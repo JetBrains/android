070490ab8b422165064f12b1ffb0b6f083bb0e20
Siva Velusamy <vsiva@google.com>
2013-03-29 11:47:59 -0700

    Setup new project wizard in IDE specific module
    Change-Id: I7bb75ce61bfe546bee75262ffb0f01855a40be5b

--- adt-branding/src/com/android/tools/idea/AndroidInitialConfigurator.java
+++ adt-branding/src/com/android/tools/idea/AndroidInitialConfigurator.java
@@ -16,18 +16,21 @@
 
 package com.android.tools.idea;
 
+import com.android.tools.idea.actions.AndroidNewProjectAction;
 import com.intellij.codeInsight.CodeInsightSettings;
+import com.intellij.ide.actions.NewProjectAction;
 import com.intellij.ide.ui.UISettings;
 import com.intellij.ide.util.PropertiesComponent;
-import com.intellij.openapi.actionSystem.KeyboardShortcut;
-import com.intellij.openapi.actionSystem.Shortcut;
+import com.intellij.openapi.actionSystem.*;
 import com.intellij.openapi.editor.ex.EditorSettingsExternalizable;
 import com.intellij.openapi.fileTypes.FileTypeManager;
 import com.intellij.openapi.keymap.Keymap;
 import com.intellij.openapi.keymap.ex.KeymapManagerEx;
 import com.intellij.util.messages.MessageBus;
 import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.Nullable;
 
+/** Customize Android IDE specific experience. */
 public class AndroidInitialConfigurator {
   @NonNls private static final String CONFIG_V1 = "AndroidConfig.V1";
   @NonNls private static final String TODO_TOOLWINDOW_ACTION_ID = "ActivateTODOToolWindow";
@@ -36,18 +39,25 @@ public class AndroidInitialConfigurator {
   public AndroidInitialConfigurator(MessageBus bus,
                                     final PropertiesComponent propertiesComponent,
                                     final FileTypeManager fileTypeManager) {
+    customizeSettings(propertiesComponent);
+
+    // change default key maps to add a activate Android ToolWindow shortcut
+    setActivateAndroidToolWindowShortcut();
+
+    // Fix New Project actions
+    fixNewProjectActions();
+  }
+
+  private static void customizeSettings(PropertiesComponent propertiesComponent) {
     if (!propertiesComponent.getBoolean(CONFIG_V1, false)) {
       propertiesComponent.setValue(CONFIG_V1, "true");
       CodeInsightSettings.getInstance().AUTO_POPUP_JAVADOC_INFO = true;
       UISettings.getInstance().SCROLL_TAB_LAYOUT_IN_EDITOR = true;
       EditorSettingsExternalizable.getInstance().setVirtualSpace(false);
     }
-
-    // change default keymaps to add a activate Android ToolWindow shortcut
-    setActivateAndroidToolWindowShortcut();
   }
 
-  private void setActivateAndroidToolWindowShortcut() {
+  private static void setActivateAndroidToolWindowShortcut() {
     for (Keymap keymap: KeymapManagerEx.getInstanceEx().getAllKeymaps()) {
       KeyboardShortcut shortcut = removeFirstKeyboardShortcut(keymap, TODO_TOOLWINDOW_ACTION_ID);
       if (shortcut != null) {
@@ -56,7 +66,8 @@ public class AndroidInitialConfigurator {
     }
   }
 
-  private KeyboardShortcut removeFirstKeyboardShortcut(Keymap keymap, String actionId) {
+  @Nullable
+  private static KeyboardShortcut removeFirstKeyboardShortcut(Keymap keymap, String actionId) {
     Shortcut[] shortcuts = keymap.getShortcuts(actionId);
     for (Shortcut each : shortcuts) {
       if (each instanceof KeyboardShortcut) {
@@ -68,4 +79,23 @@ public class AndroidInitialConfigurator {
     return null;
   }
 
+  private static void fixNewProjectActions() {
+    // TODO: This is temporary code. We should build out our own menu set and welcome screen exactly how we want.
+
+    // Register the New Project action manually since we need to unregister the platform one
+    ActionManager am = ActionManager.getInstance();
+    am.unregisterAction("NewProject");
+
+    AnAction action = new AndroidNewProjectAction();
+    am.registerAction("NewProject", action);
+
+    DefaultActionGroup ag = (DefaultActionGroup)am.getAction("WelcomeScreen.QuickStart.IDEA");
+    AnAction[] children = ag.getChildren(null);
+    for (AnAction child : children) {
+      if (child instanceof NewProjectAction) {
+        ag.remove(child);
+      }
+    }
+    ag.add(action, Constraints.FIRST);
+  }
 }
