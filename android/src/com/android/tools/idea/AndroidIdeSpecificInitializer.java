31dad0656d3de34bcb4e5b1d9cd87ff27d908fda
Siva Velusamy <vsiva@google.com>
2013-03-31 16:15:23 -0700

    Do not invoke android plugin code from core IDEA
    The adt-branding module is part of the core IDE and cannot
    refer to classes present in optional plugins such as the android plugin.
    This CL adds an extension point in the core plugin that the android
    plugin can perform to do Android IDE specific initializations.
    
    Since this extension will only be available in the Android IDE, this
    dependency has to be marked as optional and loaded only when it is
    available.
    
    Change-Id: I36c30235ce713ec8285f09c5a04d57a12e48d1af

---
+++ android/src/com/android/tools/idea/AndroidIdeSpecificInitializer.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2013 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea;
+
+import com.android.tools.idea.actions.AndroidNewProjectAction;
+import com.intellij.ide.actions.NewProjectAction;
+import com.intellij.openapi.actionSystem.*;
+
+public class AndroidIdeSpecificInitializer extends AnAction {
+  @Override
+  public void actionPerformed(AnActionEvent e) {
+    // Fix New Project actions
+    if (Boolean.getBoolean(AndroidNewProjectAction.NEW_NEW_PROJECT_WIZARD)) {
+      fixNewProjectActions();
+    }
+  }
+
+  private static void fixNewProjectActions() {
+    // TODO: This is temporary code. We should build out our own menu set and welcome screen exactly how we want.
+
+    // Register the New Project action manually since we need to unregister the platform one
+    ActionManager am = ActionManager.getInstance();
+    am.unregisterAction("NewProject");
+
+    AnAction action = new AndroidNewProjectAction();
+    am.registerAction("NewProject", action);
+
+    DefaultActionGroup ag = (DefaultActionGroup)am.getAction("WelcomeScreen.QuickStart.IDEA");
+    AnAction[] children = ag.getChildren(null);
+    for (AnAction child : children) {
+      if (child instanceof NewProjectAction) {
+        ag.remove(child);
+      }
+    }
+    ag.add(action, Constraints.FIRST);
+  }
+}
