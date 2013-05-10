1cb2e2db67c84d35c665c88e77e987a4d617864c
Alex Ruiz <alruiz@google.com>
2013-05-10 11:35:21 -0700

    Added event notification when a non-null IdeaAndroidProject is set in an AndroidFacet
    Change-Id: I19ef3c1187cdeb445c13a7d406b833d6892320ca

--- android/src/org/jetbrains/android/facet/AndroidFacet.java
+++ android/src/org/jetbrains/android/facet/AndroidFacet.java
@@ -124,6 +124,8 @@ public final class AndroidFacet extends Facet<AndroidFacetConfiguration> {
   private ProjectResources myProjectResources;
   private IdeaAndroidProject myIdeaAndroidProject;
 
+  private final List<GradleProjectAvailableWizardListener> myGradleProjectAvailableWizardListeners = Lists.newArrayList();
+
   public AndroidFacet(@NotNull Module module, String name, @NotNull AndroidFacetConfiguration configuration) {
     super(getFacetType(), module, name, configuration, null);
     configuration.setFacet(this);
@@ -858,6 +860,26 @@ public final class AndroidFacet extends Facet<AndroidFacetConfiguration> {
    */
   public void setIdeaAndroidProject(@Nullable IdeaAndroidProject project) {
     myIdeaAndroidProject = project;
+    if (project != null) {
+      for (GradleProjectAvailableWizardListener listener : myGradleProjectAvailableWizardListeners) {
+        listener.gradleProjectAvailable(project);
+      }
+    }
+  }
+
+  public void addListener(@NotNull GradleProjectAvailableWizardListener listener) {
+    if (myIdeaAndroidProject != null) {
+      listener.gradleProjectAvailable(myIdeaAndroidProject);
+    }
+    synchronized (this) {
+      myGradleProjectAvailableWizardListeners.add(listener);
+    }
+  }
+
+  public void removeListener(@NotNull GradleProjectAvailableWizardListener listener) {
+    synchronized (this) {
+      myGradleProjectAvailableWizardListeners.remove(listener);
+    }
   }
 
   /**
@@ -940,5 +962,9 @@ public final class AndroidFacet extends Facet<AndroidFacetConfiguration> {
       assert dir != null;
       return Collections.singleton(VfsUtilCore.virtualToIoFile(dir));
     }
+  }
+
+  public interface GradleProjectAvailableWizardListener {
+    void gradleProjectAvailable(@NotNull IdeaAndroidProject project);
   }
 }
---
+++ android/testSrc/org/jetbrains/android/facet/AndroidFacetTest.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) 2013 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jetbrains.android.facet;
+
+import com.android.tools.idea.gradle.IdeaAndroidProject;
+import org.jetbrains.android.AndroidTestCase;
+
+import static org.easymock.classextension.EasyMock.*;
+
+/**
+ * Tests for {@link AndroidFacet}.
+ */
+public class AndroidFacetTest extends AndroidTestCase {
+  private IdeaAndroidProject myAndroidProject;
+
+  @Override
+  public void setUp() throws Exception {
+    super.setUp();
+    myAndroidProject = createMock(IdeaAndroidProject.class);
+  }
+
+  public void testSetIdeaAndroidProject() {
+    AndroidFacet.GradleProjectAvailableWizardListener listener1 = createMock(AndroidFacet.GradleProjectAvailableWizardListener.class);
+    listener1.gradleProjectAvailable(myAndroidProject);
+    expectLastCall().once();
+
+    AndroidFacet.GradleProjectAvailableWizardListener listener2 = createMock(AndroidFacet.GradleProjectAvailableWizardListener.class);
+    listener2.gradleProjectAvailable(myAndroidProject);
+    expectLastCall().times(2);
+
+    replay(listener1, listener2);
+
+    myFacet.addListener(listener1);
+
+    // This should notify listener1.
+    myFacet.setIdeaAndroidProject(myAndroidProject);
+
+    // This should notify listener2.
+    myFacet.addListener(listener2);
+
+    myFacet.removeListener(listener1);
+
+    // This should notify listener2.
+    myFacet.setIdeaAndroidProject(myAndroidProject);
+
+    verify(listener1, listener2);
+  }
+}
