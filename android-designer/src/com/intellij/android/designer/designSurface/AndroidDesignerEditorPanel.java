7c6be037093aa021b583bf5ff2d62a81b5899a50
Alexander Lobas <Alexander.Lobas@jetbrains.com>
2013-05-14 11:08:58 +0400

    IDEA-106210

--- android-designer/src/com/intellij/android/designer/designSurface/AndroidDesignerEditorPanel.java
+++ android-designer/src/com/intellij/android/designer/designSurface/AndroidDesignerEditorPanel.java
@@ -578,7 +578,7 @@ public final class AndroidDesignerEditorPanel extends DesignerEditorPanel implem
         myHorizontalCaption.update();
         myVerticalCaption.update();
 
-        DesignerToolWindowManager.getInstance(getProject()).refresh(updateProperties);
+        DesignerToolWindowManager.getInstance(AndroidDesignerEditorPanel.this).refresh(updateProperties);
 
         if (RenderPreviewMode.getCurrent() != RenderPreviewMode.NONE) {
           RenderPreviewManager previewManager = getPreviewManager(true);
@@ -861,7 +861,7 @@ public final class AndroidDesignerEditorPanel extends DesignerEditorPanel implem
         activatePaletteItem(null);
       }
 
-      PaletteToolWindowManager.getInstance(getProject()).refresh();
+      PaletteToolWindowManager.getInstance(this).refresh();
     }
     catch (Throwable e) {
       // Pass
