a48aa7e96338a781ac31d0fe9810fd3296447237
Alex Ruiz <alruiz@google.com>
2013-06-13 15:13:03 -0700

    Unify system properties for specifying path of Android SDK to use for testing.
    Change-Id: Ie1577c0714bfbc717f1c449f5f2d1e7740dca824

--- android/testSrc/com/android/tools/idea/sdk/VersionCheckTest.java
+++ android/testSrc/com/android/tools/idea/sdk/VersionCheckTest.java
@@ -16,38 +16,37 @@
 package com.android.tools.idea.sdk;
 
 import junit.framework.TestCase;
+import org.jetbrains.android.AndroidTestCase;
 
 /**
  * Tests for {@link VersionCheck}.
  */
 public class VersionCheckTest extends TestCase {
-  private static final String MODERN_ANDROID_SDK_PATH = "android.modern.sdk.path";
-  private static final String OLD_ANDROID_SDK_PATH = "android.old.sdk.path";
+  private static final String PRE_V22_SDK_PATH = "PRE_V22_SDK_PATH";
 
-  private String myModernAndroidSdkPath;
-  private String myOldAndroidSdkPath;
+  private String mySdkPath;
+  private String myPreV22SdkPath;
 
   @Override
   protected void setUp() throws Exception {
     super.setUp();
-    myModernAndroidSdkPath = System.getProperty(MODERN_ANDROID_SDK_PATH);
-    String msg =
-      String.format("Please specify the path of a modern Android SDK (v22.0.1) with the system property '%1$s'", MODERN_ANDROID_SDK_PATH);
-    assertNotNull(msg, myModernAndroidSdkPath);
+    mySdkPath = System.getProperty(AndroidTestCase.SDK_PATH_PROPERTY);
+    String format = "Please specify the path of an Android SDK (v22.0.0) in the system property '%1$s'";
+    String msg = String.format(format, AndroidTestCase.SDK_PATH_PROPERTY);
+    assertNotNull(msg, mySdkPath);
 
-    myOldAndroidSdkPath = System.getProperty(OLD_ANDROID_SDK_PATH);
-    msg = String.format("Please specify the path of an old Android SDK (pre-22.0.1) with the system property '%1$s'", OLD_ANDROID_SDK_PATH);
-    assertNotNull(msg, myOldAndroidSdkPath);
-
+    myPreV22SdkPath = System.getProperty(PRE_V22_SDK_PATH);
+    msg = String.format("Please specify the path of an old Android SDK (pre-22.0.0) with the system property '%1$s'", PRE_V22_SDK_PATH);
+    assertNotNull(msg, myPreV22SdkPath);
   }
 
   public void testCheckVersion() {
-    VersionCheck.VersionCheckResult result = VersionCheck.checkVersion(myModernAndroidSdkPath);
+    VersionCheck.VersionCheckResult result = VersionCheck.checkVersion(mySdkPath);
     assertTrue(result.isCompatibleVersion());
   }
 
   public void testCheckVersionWithOldSdk() {
-    VersionCheck.VersionCheckResult result = VersionCheck.checkVersion(myOldAndroidSdkPath);
+    VersionCheck.VersionCheckResult result = VersionCheck.checkVersion(myPreV22SdkPath);
     assertFalse(result.isCompatibleVersion());
     assertNotNull(result.getRevision());
   }
