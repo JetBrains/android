962d0069cafa865a10c655905b658fe72b3b2c21
Eugene Kudelevsky <Eugene.Kudelevsky@jetbrains.com>
2013-07-24 17:01:59 +0400

    IDEA-102179 package renaming should not change application package

--- android/src/org/jetbrains/android/dom/converters/AndroidPackageConverter.java
+++ android/src/org/jetbrains/android/dom/converters/AndroidPackageConverter.java
@@ -15,14 +15,17 @@
  */
 package org.jetbrains.android.dom.converters;
 
+import com.intellij.openapi.util.Pair;
 import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.*;
 import com.intellij.psi.impl.source.resolve.reference.impl.providers.PackageReferenceSet;
 import com.intellij.psi.impl.source.resolve.reference.impl.providers.PsiPackageReference;
-import com.intellij.util.xml.ConvertContext;
-import com.intellij.util.xml.Converter;
-import com.intellij.util.xml.CustomReferenceConverter;
-import com.intellij.util.xml.GenericDomValue;
+import com.intellij.psi.xml.XmlAttributeValue;
+import com.intellij.psi.xml.XmlFile;
+import com.intellij.util.IncorrectOperationException;
+import com.intellij.util.Processor;
+import com.intellij.util.xml.*;
+import org.jetbrains.android.AndroidApplicationPackageRenameProcessor;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -52,20 +55,79 @@ public class AndroidPackageConverter extends Converter<String> implements Custom
   }
 
   private static class MyPackageReferenceSet extends PackageReferenceSet {
+    final int myStartInElement;
+
     public MyPackageReferenceSet(String s, PsiElement element) {
-      super(s, element, ElementManipulators.getOffsetInElement(element));
+      this(s, element, ElementManipulators.getOffsetInElement(element));
+    }
+
+    public MyPackageReferenceSet(String str, PsiElement element, int startInElement) {
+      super(str, element, startInElement);
+      myStartInElement = startInElement;
     }
 
     @NotNull
     @Override
     protected PsiPackageReference createReference(TextRange range, int index) {
-      return new PsiPackageReference(this, range, index) {
-        @NotNull
-        @Override
-        public ResolveResult[] multiResolve(boolean incompleteCode) {
-          return PsiElementResolveResult.createResults(new PsiElement[] {myElement});
+      return new MyPsiPackageReference(this, range, index);
+    }
+  }
+
+  private static class MyPsiPackageReference extends PsiPackageReference {
+    private final MyPackageReferenceSet myReferenceSet;
+    private final TextRange myTextRange;
+
+    public MyPsiPackageReference(MyPackageReferenceSet referenceSet, TextRange textRange, int index) {
+      super(referenceSet, textRange, index);
+      myReferenceSet = referenceSet;
+      myTextRange = textRange;
+    }
+
+    @NotNull
+    @Override
+    public ResolveResult[] multiResolve(boolean incompleteCode) {
+      return PsiElementResolveResult.createResults(new PsiElement[]{myElement});
+    }
+
+    @Override
+    public boolean isReferenceTo(PsiElement element) {
+      final ResolveResult[] results = doMultiResolve();
+      for (ResolveResult result : results) {
+        if (getElement().getManager().areElementsEquivalent(result.getElement(), element)) {
+          return true;
         }
-      };
+      }
+      return false;
+    }
+
+    @Override
+    public PsiElement handleElementRename(String newElementName) throws IncorrectOperationException {
+      return myElement;
+    }
+
+    @Override
+    public PsiElement bindToElement(@NotNull PsiElement element) throws IncorrectOperationException {
+      // here we only detect package movement and update relative names of components in manifest, don't change the current element
+
+      if (!(element instanceof PsiPackage) || !(myElement instanceof XmlAttributeValue)) {
+        throw new IncorrectOperationException("Cannot bind to " + element);
+      }
+      final String newPackageName = ((PsiPackage)element).getQualifiedName();
+      final String basePackage = ((XmlAttributeValue)myElement).getValue();
+      final String oldPackageName = myElement.getText().substring(myReferenceSet.myStartInElement, myTextRange.getEndOffset());
+      final PsiFile file = myElement.getContainingFile();
+
+      if (basePackage.length() > 0 && file instanceof XmlFile) {
+        AndroidApplicationPackageRenameProcessor.processAllAttributesToUpdate(
+          (XmlFile)file, basePackage, oldPackageName, newPackageName, new Processor<Pair<GenericAttributeValue, String>>() {
+          @Override
+          public boolean process(Pair<GenericAttributeValue, String> pair) {
+            pair.getFirst().setStringValue(pair.getSecond());
+            return true;
+          }
+        });
+      }
+      return myElement;
     }
   }
 }
