6e55701e7e8399e4625ae1d106c729282ff0120c
Tor Norbye <tnorbye@google.com>
2013-08-09 06:35:51 -0700

    57676: Handle R fields in custom view loading correctly
    R fields may not be final, so the custom view loading code which
    generates resource ids from the R class should not only look for
    fields that have the final modifier.
    
    Change-Id: I03a27edb6c1a6baf232832282cdfd608f05897fd

--- android/src/org/jetbrains/android/uipreview/ViewLoader.java
+++ android/src/org/jetbrains/android/uipreview/ViewLoader.java
@@ -462,7 +462,7 @@ public class ViewLoader {
 
           for (Field field : resClass.getDeclaredFields()) {
             final int modifiers = field.getModifiers();
-            if (Modifier.isStatic(modifiers) && Modifier.isFinal(modifiers)) {
+            if (Modifier.isStatic(modifiers)) { // May not be final in library projects
               final Class<?> type = field.getType();
               if (type.isArray() && type.getComponentType() == int.class) {
                 styleableId2Res.put(new IntArrayWrapper((int[])field.get(null)), field.getName());
