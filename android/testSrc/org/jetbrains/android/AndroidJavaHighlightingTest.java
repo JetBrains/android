904ff1d186708a685967dd81097e2588288da7fa
Eugene Kudelevsky <Eugene.Kudelevsky@jetbrains.com>
2013-08-19 21:30:32 +0400

    IDEA-87189 implicit assignment of "view" fields through annotations

---
+++ android/testSrc/org/jetbrains/android/AndroidJavaHighlightingTest.java
@@ -0,0 +1,23 @@
+package org.jetbrains.android;
+
+import com.intellij.codeInspection.unusedSymbol.UnusedSymbolLocalInspection;
+import com.intellij.openapi.vfs.VirtualFile;
+
+/**
+ * @author Eugene.Kudelevsky
+ */
+public class AndroidJavaHighlightingTest extends AndroidTestCase {
+  private static final String BASE_PATH = "/javaHighlighting/";
+
+  public void testInjectResourceAnnotation() throws Exception {
+    myFixture.enableInspections(UnusedSymbolLocalInspection.class);
+    myFixture.copyFileToProject(BASE_PATH + "values.xml", "res/values/values.xml");
+    myFixture.copyFileToProject(BASE_PATH + "InjectResource.java", "src/p1/p2/InjectResource.java");
+    myFixture.copyFileToProject(BASE_PATH + "SomeAnnotation.java", "src/p1/p2/SomeAnnotation.java");
+    myFixture.copyFileToProject(BASE_PATH + "R1.java", "src/p1/p2/R1.java");
+    myFixture.copyFileToProject("R.java", "src/p1/p2/R.java");
+    final VirtualFile f = myFixture.copyFileToProject(BASE_PATH + getTestName(false) + ".java", "src/p1/p2/MyActivity.java");
+    myFixture.configureFromExistingVirtualFile(f);
+    myFixture.checkHighlighting(true, false, true);
+  }
+}
