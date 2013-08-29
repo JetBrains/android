c12e831bea99610d6d2b885be6223e337764afcb
Alex Ruiz <alruiz@google.com>
2013-08-29 09:28:14 -0700

    Importing project now populate "Gradle" view with available tasks.
    Fixes issue https://code.google.com/p/android/issues/detail?id=59204
    
    As part of this CL, I merged ProjectResolver into
    AndroidGradleProjectResolver. There was no need to have 2 classes
    for importing a project.
    
    In Gerrit there will be a lot of "new code" blocks. All of them,
    except for method "addModuleTasks" were moved from
    ProjectResolver. The method "addModuleTasks" is new, and it is the
    one that populates the tasks of the imported project.
    
    Change-Id: I159c0bb2f9c00db9849a1e610adf5e1dada4c23a

--- android/testSrc/com/android/tools/idea/gradle/project/AndroidGradleProjectResolverTest.java
+++ android/testSrc/com/android/tools/idea/gradle/project/AndroidGradleProjectResolverTest.java
@@ -15,16 +15,31 @@
  */
 package com.android.tools.idea.gradle.project;
 
-import com.android.tools.idea.gradle.project.AndroidGradleProjectResolver.ProjectResolverFunctionFactory;
+import com.android.tools.idea.gradle.ContentRootSourcePaths;
+import com.android.tools.idea.gradle.TestProjects;
+import com.android.tools.idea.gradle.stubs.android.AndroidProjectStub;
+import com.android.tools.idea.gradle.stubs.gradle.IdeaModuleStub;
+import com.android.tools.idea.gradle.stubs.gradle.IdeaProjectStub;
+import com.google.common.collect.Lists;
 import com.intellij.openapi.externalSystem.model.DataNode;
+import com.intellij.openapi.externalSystem.model.ProjectKeys;
+import com.intellij.openapi.externalSystem.model.project.ContentRootData;
+import com.intellij.openapi.externalSystem.model.project.ExternalSystemSourceType;
+import com.intellij.openapi.externalSystem.model.project.ModuleData;
 import com.intellij.openapi.externalSystem.model.project.ProjectData;
 import com.intellij.openapi.externalSystem.model.task.ExternalSystemTaskId;
 import com.intellij.openapi.externalSystem.model.task.ExternalSystemTaskType;
-import com.intellij.util.Function;
+import com.intellij.openapi.externalSystem.util.ExternalSystemApiUtil;
+import com.intellij.openapi.util.io.FileUtil;
 import junit.framework.TestCase;
+import org.gradle.tooling.ModelBuilder;
 import org.gradle.tooling.ProjectConnection;
-import org.jetbrains.plugins.gradle.service.project.GradleExecutionHelper;
+import org.gradle.tooling.model.idea.IdeaProject;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.plugins.gradle.settings.GradleExecutionSettings;
+
+import java.util.Collections;
+import java.util.List;
 
 import static com.intellij.openapi.externalSystem.model.task.ExternalSystemTaskNotificationListenerAdapter.NULL_OBJECT;
 import static org.easymock.classextension.EasyMock.*;
@@ -33,38 +48,106 @@ import static org.easymock.classextension.EasyMock.*;
  * Tests for {@link AndroidGradleProjectResolver}.
  */
 public class AndroidGradleProjectResolverTest extends TestCase {
-  private GradleExecutionHelper myHelper;
-  private ProjectResolverFunctionFactory myFunctionFactory;
-  private Function<ProjectConnection, DataNode<ProjectData>> myProjectResolverFunction;
-  private ProjectImportErrorHandler myErrorHandler;
+  private ContentRootSourcePaths myExpectedSourcePaths;
+  private IdeaProjectStub myIdeaProject;
+  private AndroidProjectStub myAndroidProject;
+  private ExternalSystemTaskId myId;
+  private ProjectConnection myConnection;
+  private GradleExecutionHelperDouble myHelper;
+  private GradleExecutionSettings mySettings;
   private AndroidGradleProjectResolver myProjectResolver;
+  private IdeaModuleStub myUtilModule;
 
-  @SuppressWarnings("unchecked")
   @Override
   public void setUp() throws Exception {
     super.setUp();
-    myHelper = createMock(GradleExecutionHelper.class);
-    myFunctionFactory = createMock(ProjectResolverFunctionFactory.class);
-    myProjectResolverFunction = createMock(Function.class);
-    myErrorHandler = createMock(ProjectImportErrorHandler.class);
-    myProjectResolver = new AndroidGradleProjectResolver(myHelper, myFunctionFactory, myErrorHandler);
+    myExpectedSourcePaths = new ContentRootSourcePaths();
+    myIdeaProject = new IdeaProjectStub("multiProject");
+    myAndroidProject = TestProjects.createBasicProject(myIdeaProject.getRootDir());
+    myIdeaProject.addModule(myAndroidProject.getName(), "androidTask");
+    myUtilModule = myIdeaProject.addModule("util", "compileJava", "jar", "classes");
+    myIdeaProject.addModule("notReallyAGradleProject");
+    myId = ExternalSystemTaskId.create(ExternalSystemTaskType.RESOLVE_PROJECT, "dummy");
+    myConnection = createMock(ProjectConnection.class);
+    myHelper = GradleExecutionHelperDouble.newMock();
+    mySettings = createMock(GradleExecutionSettings.class);
+    myProjectResolver = new AndroidGradleProjectResolver(myHelper, createMock(ProjectImportErrorHandler.class));
+  }
+
+  @Override
+  protected void tearDown() throws Exception {
+    if (myIdeaProject != null) {
+      myIdeaProject.dispose();
+    }
+    super.tearDown();
   }
 
   @SuppressWarnings("unchecked")
-  public void testResolveProjectInfo() throws Exception {
-    ExternalSystemTaskId id = ExternalSystemTaskId.create(ExternalSystemTaskType.RESOLVE_PROJECT, "1");
-    String projectPath = "~/basic/build.gradle";
-    GradleExecutionSettings settings = createMock(GradleExecutionSettings.class);
+  public void testResolveProjectInfo() {
+    // Record mock expectations.
+    ModelBuilder<IdeaProject> ideaProjectModelBuilder = createMock(ModelBuilder.class);
+    myHelper.getModelBuilder(IdeaProject.class, myId, mySettings, myConnection, NULL_OBJECT, Collections.<String>emptyList());
+    expectLastCall().andReturn(ideaProjectModelBuilder);
 
-    expect(myFunctionFactory.createFunction(id, projectPath, myErrorHandler, NULL_OBJECT, settings)).andReturn(myProjectResolverFunction);
-    DataNode<ProjectData> projectInfo = createMock(DataNode.class);
-    expect(myHelper.execute(projectPath, settings, myProjectResolverFunction)).andReturn(projectInfo);
+    // Simulate retrieval of the top-level IdeaProject.
+    expect(ideaProjectModelBuilder.get()).andReturn(myIdeaProject);
 
-    replay(myFunctionFactory, myHelper);
+    // Simulate retrieval of AndroidProject from IdeaModule 'basic'
+    myHelper.setExecutionResult(myAndroidProject);
 
-    DataNode<ProjectData> resolved = myProjectResolver.resolveProjectInfo(id, projectPath, true, settings, NULL_OBJECT);
-    assertSame(projectInfo, resolved);
+    replay(myConnection, myHelper, ideaProjectModelBuilder);
 
-    verify(myFunctionFactory, myHelper);
+    // Code under test.
+    String projectPath = myIdeaProject.getBuildFile().getParentFile().getPath();
+    List<String> extraJvmArgs = Collections.emptyList();
+    DataNode<ProjectData> projectInfo =
+      myProjectResolver.resolveProjectInfo(myId, projectPath, myConnection, NULL_OBJECT, extraJvmArgs, mySettings);
+
+    // Verify mock expectations.
+    verify(myConnection, myHelper, ideaProjectModelBuilder);
+
+    // Verify project.
+    assertNotNull(projectInfo);
+    ProjectData projectData = projectInfo.getData();
+    assertEquals(myIdeaProject.getName(), projectData.getName());
+    assertEquals(FileUtil.toSystemIndependentName(myIdeaProject.getRootDir().getAbsolutePath()),
+                 projectData.getIdeProjectFileDirectoryPath());
+
+    // Verify modules.
+    List<DataNode<ModuleData>> modules = Lists.newArrayList(ExternalSystemApiUtil.getChildren(projectInfo, ProjectKeys.MODULE));
+    assertEquals("Module count", 2, modules.size());
+
+    // Verify 'basic' module.
+    DataNode<ModuleData> moduleInfo = modules.get(0);
+    ModuleData moduleData = moduleInfo.getData();
+    assertEquals(myAndroidProject.getName(), moduleData.getName());
+
+    // Verify content root in 'basic' module.
+    List<DataNode<ContentRootData>> contentRoots = Lists.newArrayList(ExternalSystemApiUtil.getChildren(moduleInfo, ProjectKeys.CONTENT_ROOT));
+    assertEquals(1, contentRoots.size());
+
+    String projectRootDirPath = FileUtil.toSystemIndependentName(myAndroidProject.getRootDir().getAbsolutePath());
+    ContentRootData contentRootData = contentRoots.get(0).getData();
+    assertEquals(projectRootDirPath, contentRootData.getRootPath());
+    myExpectedSourcePaths.storeExpectedSourcePaths(myAndroidProject);
+    assertCorrectStoredDirPaths(contentRootData, ExternalSystemSourceType.SOURCE);
+    assertCorrectStoredDirPaths(contentRootData, ExternalSystemSourceType.TEST);
+
+    // Verify 'util' module.
+    moduleInfo = modules.get(1);
+    moduleData = moduleInfo.getData();
+    assertEquals(myUtilModule.getName(), moduleData.getName());
+
+    // Verify content root in 'util' module.
+    contentRoots = Lists.newArrayList(ExternalSystemApiUtil.getChildren(moduleInfo, ProjectKeys.CONTENT_ROOT));
+    assertEquals(1, contentRoots.size());
+
+    String moduleRootDirPath = FileUtil.toSystemIndependentName(myUtilModule.getRootDir().getPath());
+    contentRootData = contentRoots.get(0).getData();
+    assertEquals(moduleRootDirPath, contentRootData.getRootPath());
+  }
+
+  private void assertCorrectStoredDirPaths(@NotNull ContentRootData contentRootData, @NotNull ExternalSystemSourceType sourceType) {
+    myExpectedSourcePaths.assertCorrectStoredDirPaths(contentRootData.getPaths(sourceType), sourceType);
   }
 }
