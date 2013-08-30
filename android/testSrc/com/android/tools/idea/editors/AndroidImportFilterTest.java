bf80e49c1294fb41c3bcff173b39e397604fb42f
Tor Norbye <tnorbye@google.com>
2013-08-30 14:04:04 -0700

    Prevent innerclass imports of application R packages
    This should fix http://youtrack.jetbrains.com/issue/IDEA-101683
    
    Change-Id: I8631fc3591851ddc1a9992657a74ee47ec5bed73

--- android/testSrc/com/android/tools/idea/editors/AndroidImportFilterTest.java
+++ android/testSrc/com/android/tools/idea/editors/AndroidImportFilterTest.java
@@ -22,12 +22,22 @@ public class AndroidImportFilterTest extends TestCase {
     AndroidImportFilter filter = new AndroidImportFilter();
     assertTrue(filter.shouldUseFullyQualifiedName("android.R"));
     assertTrue(filter.shouldUseFullyQualifiedName("android.R.anim"));
+    assertTrue(filter.shouldUseFullyQualifiedName("android.R.anything"));
     assertFalse(filter.shouldUseFullyQualifiedName("com.android.tools.R"));
-    assertFalse(filter.shouldUseFullyQualifiedName("com.android.tools.R.anim"));
+    assertTrue(filter.shouldUseFullyQualifiedName("com.android.tools.R.anim"));
+    assertTrue(filter.shouldUseFullyQualifiedName("com.android.tools.R.layout"));
+    assertTrue(filter.shouldUseFullyQualifiedName("a.R.string"));
+    assertFalse(filter.shouldUseFullyQualifiedName("my.weird.clz.R"));
+    assertFalse(filter.shouldUseFullyQualifiedName("my.weird.clz.R.bogus"));
     assertFalse(filter.shouldUseFullyQualifiedName(""));
+    assertFalse(filter.shouldUseFullyQualifiedName("."));
+    assertFalse(filter.shouldUseFullyQualifiedName("a.R"));
     assertFalse(filter.shouldUseFullyQualifiedName("android"));
     assertFalse(filter.shouldUseFullyQualifiedName("android."));
     assertFalse(filter.shouldUseFullyQualifiedName("android.r"));
     assertFalse(filter.shouldUseFullyQualifiedName("android.Random"));
+    assertFalse(filter.shouldUseFullyQualifiedName("my.R.unrelated"));
+    assertFalse(filter.shouldUseFullyQualifiedName("my.R.unrelated.to"));
+    assertFalse(filter.shouldUseFullyQualifiedName("R.string")); // R is never in the default package
   }
 }
