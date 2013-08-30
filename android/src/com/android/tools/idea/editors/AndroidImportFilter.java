b03e5f33073a7acef4c649a1ded6b1c16c08bfc0
Tor Norbye <tnorbye@google.com>
2013-08-29 19:04:25 -0700

    Never auto-import android.R
    This fixes
    57686: Lint SuspiciousImport conflicts with Studio defaults
    
    Change-Id: I01a0fdbddcdcbc4ab76371927ba28d02ebc5330e

---
+++ android/src/com/android/tools/idea/editors/AndroidImportFilter.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2013 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.editors;
+
+import com.intellij.codeInsight.ImportFilter;
+import org.jetbrains.annotations.NotNull;
+
+import static com.android.SdkConstants.CLASS_R;
+
+public class AndroidImportFilter extends ImportFilter {
+  /** Never import android.R */
+  @Override
+  public boolean shouldUseFullyQualifiedName(@NotNull String classQualifiedName) {
+    return CLASS_R.equals(classQualifiedName) ||
+           classQualifiedName.startsWith(CLASS_R) && classQualifiedName.charAt(CLASS_R.length()) == '.';
+  }
+}
