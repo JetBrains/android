36fe25554812cf243441f1d3e24523fe4e2a006a
Tor Norbye <tnorbye@google.com>
2013-09-12 11:20:11 -0700

    Extract dimension resource
    Change-Id: I3dd17493da92f7eae573e9860020c0b39c6f0b60

---
+++ android/testData/extractDimen/fromJava.java
@@ -0,0 +1,11 @@
+package p1.p2.p3;
+
+import android.content.Context;
+import android.content.res.Resources;
+
+public class Class extends Context {
+  public void f(Context resources) {
+    float f = 4<caret>2f;
+    String s = "hello";
+  }
+}
