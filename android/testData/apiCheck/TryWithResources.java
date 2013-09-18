6239a89f01e0b1eac74e5cd26a19aa6dd2e35d35
Tor Norbye <tnorbye@google.com>
2013-09-18 15:35:23 -0700

    Add try-with-resources lint check. DO NOT MERGE
    Change-Id: I6dccba4fb8d460903f6492c0ebbf6a20e76535fd

---
+++ android/testData/apiCheck/TryWithResources.java
@@ -0,0 +1,14 @@
+package p1.p2;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+
+public class Class {
+    static String readFirstLineFromFile(String path) throws IOException {
+        try <error descr="Try-with-resources requires API level 19 (current min is 1)">(BufferedReader br = new BufferedReader(new FileReader(path)))</error> {
+            return br.readLine();
+        }
+    }
+}
