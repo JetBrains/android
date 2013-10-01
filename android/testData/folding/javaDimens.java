18052644c2a585c9bb02eab7c11b92db2052dd74
Tor Norbye <tnorbye@google.com>
2013-10-01 13:40:59 -0700

    60529: Inlined resources sometimes shows obsolete/wrong contents
    This changeset fixes the resource inlining (editor folding) in three
    ways:
    
    * First, it fixes the various bugs related to element caching which
      meant that you could see obsolete contents after editing an
      expression, or that you couldn't get resource folding on an element
      after typing it in, etc.
    
    * Second, it now also inlines dimensions and integers, as requested by
      several users.
    
    * Third, it now inlines resource string references to unknown calls an
      outside call expressions. (Before this we would only inline strings
      that were calling framework methods we had annotated as pertaining
      to string lookup; it will now inline any R.string reference.)
    
    To fix the caching bug I basically removed the element-to-resolved-
    string cache, and instead made the lookup code much faster, since when
    annotating all field references we no longer need to resolve method
    calls and look up parameter annotations.  It now instead visits all
    the references and quickly determines whether they correspond to a
    resource field, and only in that case does it do the remaining
    computations.
    
    Change-Id: I0841340ccfa41aa1ae997d8c5553dca2662c8452

---
+++ android/testData/folding/javaDimens.java
@@ -0,0 +1,26 @@
+package p1.p2;
+
+import <fold text='...' expand='false'>android.app.Activity;
+import android.app.AlertDialog;</fold>
+
+public class MyActivity extends Activity {
+    public void test() <fold text='{...}' expand='true'>{
+        AlertDialog dialog = new AlertDialog.Builder(this)
+                .setView(null)
+                .setPositiveButton(<fold text='"String 3"' expand='false'>R.string.string3</fold>, null)
+                .create();
+        float dimension = <fold text='"56dip"' expand='false'>getResources().getDimension(R.dimen.action_bar_default_height)</fold>;
+        int dimension2 = <fold text='"4dip"' expand='false'>getResources().getDimensionPixelOffset(R.dimen.action_bar_icon_vertical_padding)</fold>;
+        int dimension3 = <fold text='"40dp"' expand='false'>getResources().getDimensionPixelSize(R.dimen.mydimen1)</fold>;
+        int[] strings = new int[] {
+                <fold text='"String 1"' expand='false'>R.string.string1</fold>,
+                <fold text='"String 2"' expand='false'>R.string.string2</fold>,
+                <fold text='"String 3"' expand='false'>R.string.string3</fold>
+        };
+        int[] dimensions = new int[] {
+                <fold text='"40dp"' expand='false'>R.dimen.mydimen1</fold>,
+                <fold text='"28sp"' expand='false'>R.dimen.mydimen2</fold>
+        };
+        final int maxButtons = <fold text='"5"' expand='false'>getResources().getInteger(R.integer.max_action_buttons)</fold>;
+    }</fold>
+}
