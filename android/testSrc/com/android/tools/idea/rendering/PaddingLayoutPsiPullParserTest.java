a1d6c728d695a07f2bb84a68044d9d33a1e60718
Tor Norbye <tnorbye@google.com>
2013-10-02 09:59:54 -0700

    Layout XML pull parser cleanup
    First, in the layoutlib parser factory, check whether the file we need
    a parser for is in the project, and if so, attempt to create a
    PSI-based pull parser for it.  This means that if your layout includes
    another layout in your project, and you've edited that included
    layout, we'll render the current contents of the included layout rather
    than what is saved on disk.
    
    Second, our layout pull parser had support for automatically increasing
    the padding of a given set of nodes. Most of the time it is not used.
    Therefore, I pulled all the padding related logic into a separate
    subclass which is only used when padding is enabled. This simplifies
    the logic of the PSI pull parser to really only deal with PSI pull
    parsing.
    
    I also took the opportunity to rename the class, since it's only
    suitable for layout parsing, so the much more general name
    XmlTagPullParser was a bit misleading. When we support rendering of
    more than layouts, we should extract out the generic parsing bits into
    an XmlTagPullParser class, and leave only the layout specific bits in
    LayoutPsiPullParser. Similarly, the ContextPullParser is now called
    LayoutFilePullParser, since it's like LayoutPsiPullParser but operates
    on files rather than PSI elements.
    
    Change-Id: If27355918f3bd20744ebd48c21184c20ce1aa1ae

---
+++ android/testSrc/com/android/tools/idea/rendering/PaddingLayoutPsiPullParserTest.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (C) 2013 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.rendering;
+
+import com.android.resources.Density;
+import com.google.common.collect.Sets;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.PsiManager;
+import com.intellij.psi.xml.XmlFile;
+import com.intellij.psi.xml.XmlTag;
+import org.jetbrains.android.AndroidTestCase;
+
+import java.util.Set;
+
+import static com.android.SdkConstants.*;
+import static org.xmlpull.v1.XmlPullParser.END_TAG;
+import static org.xmlpull.v1.XmlPullParser.START_TAG;
+
+@SuppressWarnings("ConstantConditions")
+public class PaddingLayoutPsiPullParserTest extends AndroidTestCase {
+  @SuppressWarnings("SpellCheckingInspection")
+  public static final String BASE_PATH = "xmlpull/";
+
+  public PaddingLayoutPsiPullParserTest() {
+  }
+
+  public void test() throws Exception {
+    String filename = "layout.xml";
+    VirtualFile file = myFixture.copyFileToProject(BASE_PATH + filename, "res/layout/" + filename);
+    assertNotNull(file);
+    PsiFile psiFile = PsiManager.getInstance(getProject()).findFile(file);
+    assertNotNull(psiFile);
+
+    assertTrue(psiFile instanceof XmlFile);
+    XmlFile xmlFile = (XmlFile)psiFile;
+
+    // Second child of the root in layout.xml is a LinearLayout of id @+id/header
+    XmlTag element = xmlFile.getRootTag().getSubTags()[1];
+    assertEquals("@+id/header", element.getAttributeValue(ATTR_ID, ANDROID_URI));
+    assertNull(element.getAttributeValue(ATTR_ID, ATTR_PADDING));
+
+    Set<XmlTag> explode = Sets.newHashSet(element);
+    LayoutPsiPullParser parser = LayoutPsiPullParser.create(xmlFile, new RenderLogger("test", myModule), explode, Density.MEDIUM);
+    assertEquals(START_TAG, parser.nextTag());
+    assertEquals("LinearLayout", parser.getName());
+    assertEquals(START_TAG, parser.nextTag());
+    assertEquals("include", parser.getName());
+    assertNull(parser.getAttributeValue(ANDROID_URI, ATTR_PADDING));
+    assertEquals(END_TAG, parser.nextTag());
+    assertEquals(START_TAG, parser.nextTag());
+    assertEquals("LinearLayout", parser.getName());
+    assertEquals("@+id/header", parser.getAttributeValue(ANDROID_URI, ATTR_ID));
+    assertEquals("20px", parser.getAttributeValue(ANDROID_URI, ATTR_PADDING));
+  }
+}
