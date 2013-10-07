f94d5acc64b61b9f82cec0e569500469d7ff457b
Alex Ruiz <alruiz@google.com>
2013-10-07 12:54:24 -0700

    Building with Gradle now uses same JDK as project.
    Change-Id: Ib85f30dfd8d7eebaa0369f03a3bfa0670c429d00

--- android/testSrc/com/android/tools/idea/gradle/compiler/AndroidGradleBuildProcessParametersProviderTest.java
+++ android/testSrc/com/android/tools/idea/gradle/compiler/AndroidGradleBuildProcessParametersProviderTest.java
@@ -15,9 +15,16 @@
  */
 package com.android.tools.idea.gradle.compiler;
 
+import com.google.common.base.Strings;
 import com.google.common.collect.Lists;
-import com.intellij.openapi.project.Project;
-import junit.framework.TestCase;
+import com.intellij.ide.impl.NewProjectUtil;
+import com.intellij.openapi.externalSystem.util.ExternalSystemApiUtil;
+import com.intellij.openapi.projectRoots.JavaSdk;
+import com.intellij.openapi.projectRoots.Sdk;
+import com.intellij.openapi.projectRoots.impl.SdkConfigurationUtil;
+import com.intellij.openapi.util.io.FileUtil;
+import com.intellij.testFramework.IdeaTestCase;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.plugins.gradle.settings.GradleExecutionSettings;
 
 import java.util.List;
@@ -27,34 +34,64 @@ import static org.easymock.classextension.EasyMock.*;
 /**
  * Tests for {@link AndroidGradleBuildProcessParametersProvider}.
  */
-public class AndroidGradleBuildProcessParametersProviderTest extends TestCase {
-  private Project myProject;
+public class AndroidGradleBuildProcessParametersProviderTest extends IdeaTestCase {
   private AndroidGradleBuildProcessParametersProvider myParametersProvider;
+  private Sdk myJdk;
 
   @Override
   public void setUp() throws Exception {
     super.setUp();
-    myProject = createMock(Project.class);
     myParametersProvider = new AndroidGradleBuildProcessParametersProvider(myProject);
+    ExternalSystemApiUtil.executeProjectChangeAction(true, new Runnable() {
+      @Override
+      public void run() {
+        NewProjectUtil.applyJdkToProject(myProject, getTestProjectJdk());
+      }
+    });
+  }
+
+  @Override
+  @NotNull
+  protected Sdk getTestProjectJdk() {
+    if (myJdk == null) {
+      String jdkHomePath = getSystemPropertyOrEnvironmentVariable("JAVA6_HOME");
+      myJdk = SdkConfigurationUtil.createAndAddSDK(jdkHomePath, JavaSdk.getInstance());
+    }
+    assertNotNull(myJdk);
+    return myJdk;
+  }
+
+  @NotNull
+  private static String getSystemPropertyOrEnvironmentVariable(@NotNull String name) {
+    String s = System.getProperty(name);
+    if (Strings.isNullOrEmpty(s)) {
+      s = System.getenv(name);
+    }
+    if (Strings.isNullOrEmpty(s)) {
+      fail(String.format("Please set the system property or environment variable '%1$s'", name));
+    }
+    return s;
   }
 
   public void testPopulateJvmArgsWithGradleExecutionSettings() {
     GradleExecutionSettings settings = createMock(GradleExecutionSettings.class);
 
-    expect(myProject.getBasePath()).andReturn("~/projects/project1");
     expect(settings.getRemoteProcessIdleTtlInMs()).andReturn(55L);
     expect(settings.getGradleHome()).andReturn("~/gradle-1.6");
     expect(settings.isVerboseProcessing()).andReturn(true);
     expect(settings.getServiceDirectory()).andReturn("~./gradle");
     expect(settings.getDaemonVmOptions()).andReturn("-Xmx2048m -XX:MaxPermSize=512m");
-    expect(settings.getJavaHome()).andReturn("~/Libraries/Java Home");
 
-    replay(myProject, settings);
+    replay(settings);
 
     List<String> jvmArgs = Lists.newArrayList();
     myParametersProvider.populateJvmArgs(settings, jvmArgs);
+
+    verify(settings);
+
     assertEquals(9, jvmArgs.size());
-    assertTrue(jvmArgs.contains("-Dcom.android.studio.gradle.project.path=~/projects/project1"));
+    String projectDirPath = FileUtil.toSystemDependentName(myProject.getBasePath());
+    assertTrue(jvmArgs.contains("-Dcom.android.studio.gradle.project.path=" + projectDirPath));
     assertTrue(jvmArgs.contains("-Dcom.android.studio.gradle.daemon.max.idle.time=55"));
     assertTrue(jvmArgs.contains("-Dcom.android.studio.gradle.home.path=~/gradle-1.6"));
     assertTrue(jvmArgs.contains("-Dcom.android.studio.gradle.use.verbose.logging=true"));
@@ -62,6 +99,9 @@ public class AndroidGradleBuildProcessParametersProviderTest extends TestCase {
     assertTrue(jvmArgs.contains("-Dcom.android.studio.daemon.gradle.vm.option.count=2"));
     assertTrue(jvmArgs.contains("-Dcom.android.studio.daemon.gradle.vm.option.0=-Xmx2048m"));
     assertTrue(jvmArgs.contains("-Dcom.android.studio.daemon.gradle.vm.option.1=-XX:MaxPermSize=512m"));
-    assertTrue(jvmArgs.contains("-Dcom.android.studio.gradle.java.home.path=~/Libraries/Java Home"));
+    String javaHomeDirPath = myJdk.getHomePath();
+    assertNotNull(javaHomeDirPath);
+    javaHomeDirPath = FileUtil.toSystemDependentName(javaHomeDirPath);
+    assertTrue(jvmArgs.contains("-Dcom.android.studio.gradle.java.home.path=" + javaHomeDirPath));
   }
 }
