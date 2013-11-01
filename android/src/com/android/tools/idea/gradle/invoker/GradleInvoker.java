aee1f5e4dd2b63ec972bfbf580f0fba8557e639e
Alex Ruiz <alruiz@google.com>
2013-10-31 21:10:20 -0700

    Enabled building selected modules.
    (By "building" we mean "making" and "compiling.")
    
    Currently, Android Studio builds all the modules
    in the project. Users have reported that they
    only want to build certain modules. This CL
    enables that.
    
    The main issue with JPS is that it is impossible
    to have a 'target builder' (in JPS terminology)
    that knows about all the selected modules in
    the IDE (JPS has no notion of an IDE.) The
    most that we can get from JPS is to have one
    'target builder' per module, and these target
    builders won't know about each other, making
    it impossible to have a single call to Gradle
    passing the tasks to execute per selected
    module (we would end with a call to Gradle
    per module.)
    
    To work around this, we have a back door: we
    have an extension point in JPS where we can pass
    JVM args to the JPS process (we requested that
    some time ago.) In this CL we pass the names
    of the selected modules (if any) to as JVM
    parameters to the JPS process.
    
    On the other hand, it is really easy to implement
    building selected modules in the new build
    strategy.
    
    In addition, this CL:
    - Fixes the "update" mechanism of the "Make
      Module(s)" and "Compile Module(s)".
    - Makes it possible to compile a module
      if any of its files is selected.
    - Unifies the way Gradle tasks to execute
      are calculated (for both JPS and
      direct Gradle invocation,) fixing bugs
      in both build approaches.
    - Does not call "assemble" on pure Java
      libraries  when generating sources.
    - Cleans up code.
    - Adds more tests.
    
    Fixes
    https://code.google.com/p/android/issues/detail?id=59915
    
    Change-Id: I4f24fbfd4773d37c0276ee7b6815d85f6b312fa0

--- android/src/com/android/tools/idea/gradle/invoker/GradleInvoker.java
+++ android/src/com/android/tools/idea/gradle/invoker/GradleInvoker.java
@@ -15,24 +15,29 @@
  */
 package com.android.tools.idea.gradle.invoker;
 
-import com.android.builder.model.ArtifactInfo;
-import com.android.builder.model.Variant;
+import com.android.tools.idea.gradle.facet.AndroidGradleFacet;
+import com.android.tools.idea.gradle.util.BuildMode;
+import com.android.tools.idea.gradle.util.GradleBuilds;
 import com.android.tools.idea.gradle.util.Projects;
-import com.google.common.base.Strings;
 import com.google.common.collect.Lists;
+import com.intellij.openapi.actionSystem.DataContext;
 import com.intellij.openapi.components.ServiceManager;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.module.ModuleManager;
 import com.intellij.openapi.project.Project;
-import com.intellij.util.Function;
 import com.intellij.util.ui.UIUtil;
 import org.jetbrains.android.facet.AndroidFacet;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import org.jetbrains.jps.android.model.impl.JpsAndroidModuleProperties;
 
 import java.util.List;
 
+/**
+ * Invokes Gradle tasks directly. Results of tasks execution are displayed in both the "Messages" tool window and the new "Gradle Console"
+ * tool window.
+ */
 public class GradleInvoker {
   private static final Logger LOG = Logger.getInstance(GradleInvoker.class);
   @NotNull private static final String CLEAN_TASK_NAME = "clean";
@@ -48,12 +53,8 @@ public class GradleInvoker {
   }
 
   public void generateSources() {
-    List<String> tasks = findTasksToExecute(new Function<ArtifactInfo, String>() {
-      @Override
-      public String fun(ArtifactInfo artifactInfo) {
-        return artifactInfo.getSourceGenTaskName();
-      }
-    }, "generate sources");
+    ModuleManager moduleManager = ModuleManager.getInstance(myProject);
+    List<String> tasks = findTasksToExecute(moduleManager.getModules(), BuildMode.SOURCE_GEN, false);
     executeTasks(tasks, new Runnable() {
       @Override
       public void run() {
@@ -62,62 +63,51 @@ public class GradleInvoker {
     });
   }
 
-  public void compileJava() {
-    List<String> tasks = findTasksToExecute(new Function<ArtifactInfo, String>() {
-      @Override
-      public String fun(ArtifactInfo artifactInfo) {
-        return artifactInfo.getJavaCompileTaskName();
-      }
-    }, "compile Java code");
+  public void compileJava(@NotNull DataContext dataContext) {
+    Module[] modules = Projects.getSelectedModules(myProject, dataContext);
+    List<String> tasks = findTasksToExecute(modules, BuildMode.COMPILE_JAVA, false);
     executeTasks(tasks, null);
   }
 
   public void make() {
-    List<String> tasks = findTasksToExecute(new Function<ArtifactInfo, String>() {
-      @Override
-      public String fun(ArtifactInfo artifactInfo) {
-        return artifactInfo.getAssembleTaskName();
-      }
-    }, "make project");
+    ModuleManager moduleManager = ModuleManager.getInstance(myProject);
+    List<String> tasks = findTasksToExecute(moduleManager.getModules(), BuildMode.MAKE, false);
+    executeTasks(tasks, null);
+  }
+
+  public void make(@NotNull DataContext dataContext) {
+    Module[] modules = Projects.getSelectedModules(myProject, dataContext);
+    List<String> tasks = findTasksToExecute(modules, BuildMode.MAKE, false);
     executeTasks(tasks, null);
   }
 
   public void rebuild() {
-    List<String> tasks = findTasksToExecute(new Function<ArtifactInfo, String>() {
-      @Override
-      public String fun(ArtifactInfo artifactInfo) {
-        return artifactInfo.getAssembleTaskName();
-      }
-    }, "rebuild project");
+    ModuleManager moduleManager = ModuleManager.getInstance(myProject);
+    List<String> tasks = findTasksToExecute(moduleManager.getModules(), BuildMode.REBUILD, false);
     if (!tasks.isEmpty()) {
       tasks.add(0, CLEAN_TASK_NAME);
     }
     executeTasks(tasks, null);
   }
 
-  @NotNull
-  private List<String> findTasksToExecute(@NotNull Function<ArtifactInfo, String> f, @NotNull String actionToPerform) {
+  private List<String> findTasksToExecute(@NotNull Module[] modules, @NotNull BuildMode buildMode, boolean buildTests) {
     List<String> tasks = Lists.newArrayList();
-
-    ModuleManager moduleManager = ModuleManager.getInstance(myProject);
-    for (Module module : moduleManager.getModules()) {
-      AndroidFacet facet = AndroidFacet.getInstance(module);
-      if (facet != null && facet.getIdeaAndroidProject() != null) {
-        Variant selectedVariant = facet.getIdeaAndroidProject().getSelectedVariant();
-        ArtifactInfo mainArtifactInfo = selectedVariant.getMainArtifactInfo();
-        String task = f.fun(mainArtifactInfo);
-        if (!Strings.isNullOrEmpty(task)) {
-          tasks.add(task);
-        }
+    for (Module module: modules) {
+      AndroidGradleFacet androidGradleFacet = AndroidGradleFacet.getInstance(module);
+      if (androidGradleFacet == null) {
+        continue;
       }
+      String gradleProjectPath = androidGradleFacet.getConfiguration().GRADLE_PROJECT_PATH;
+      AndroidFacet androidFacet = AndroidFacet.getInstance(module);
+      JpsAndroidModuleProperties properties = androidFacet != null ? androidFacet.getProperties() : null;
+      GradleBuilds.findAndAddBuildTask(module.getName(), buildMode, gradleProjectPath, properties, tasks, buildTests);
     }
 
     if (tasks.isEmpty()) {
       // Unlikely to happen.
-      String msg = String.format("Unable to find Gradle tasks that %1$s for project '%2$s'", actionToPerform, myProject.getName());
-      LOG.info(msg);
+      String format = "Unable to find Gradle tasks for project '%1$s' using BuildMode %2$s";
+      LOG.info(String.format(format, myProject.getName(), buildMode.name()));
     }
-
     return tasks;
   }
 
