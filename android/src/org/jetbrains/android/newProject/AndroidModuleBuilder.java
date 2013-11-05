47b5e9abaf3eb3a23d83d69f8b051a9532bf21bb
Dmitry Avdeev <dmitry.avdeev@jetbrains.com>
2013-11-05 12:09:44 +0400

    IDEA-115074 New Project Wizard: Grails: grails-specific settings are missing

--- android/src/org/jetbrains/android/newProject/AndroidModuleBuilder.java
+++ android/src/org/jetbrains/android/newProject/AndroidModuleBuilder.java
@@ -767,13 +767,6 @@ public class AndroidModuleBuilder extends JavaModuleBuilder {
   }
 
   @Override
-  public ModuleWizardStep[] createWizardSteps(@NotNull WizardContext wizardContext,
-                                              @NotNull ModulesProvider modulesProvider,
-                                              boolean forNewWizard) {
-    return createWizardSteps(wizardContext, modulesProvider);
-  }
-
-  @Override
   public Icon getBigIcon() {
     return AndroidIcons.Android24;
   }
