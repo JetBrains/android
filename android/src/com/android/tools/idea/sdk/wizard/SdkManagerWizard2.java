82ae37dcd6c4300131570a15756baa69d796d46f
Raphael Moll <ralf@android.com>
2013-11-08 13:55:40 -0800

    Experiment with sdk manager UI for studio.
    When enabled, an internal SDK Manager Wizard is displayed
    with 3 steps:
    1- Selection. Displays the current SDK packages and what is
       available. User can select/deselect packages.
    2- Confirmation. Displays what needs to be removed or
       installed, along with licenses.
    3- Progress. Display download/install status.
    
    This code is disabled by default.
    To enable, set an env var "STUDIO_SDK_MANAGER" to "2".
    
    Change-Id: Ia516e71458c16b80e200f811749866090f2cdcb0

---
+++ android/src/com/android/tools/idea/sdk/wizard/SdkManagerWizard2.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright (C) 2013 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.sdk.wizard;
+
+
+import com.android.annotations.Nullable;
+import com.android.tools.idea.sdk.SdkState;
+import com.intellij.ide.wizard.AbstractWizard;
+import com.intellij.openapi.Disposable;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.Disposer;
+import org.jetbrains.android.sdk.AndroidSdkData;
+import org.jetbrains.android.sdk.AndroidSdkUtils;
+
+public class SdkManagerWizard2 extends AbstractWizard<SmwStep> {
+
+  private SmwState myWizardState;
+  private SmwSelectionStep mySelectionStep;
+  private SmwConfirmationStep myConfirmationStep;
+  private SmwProgressStep myProgressStep;
+
+  public SdkManagerWizard2(@Nullable Project project) {
+    super("fake SDK Manager wizard", project);
+    init();
+  }
+
+  @Override
+  protected void init() {
+    // TODO it's possible to not have an SDK, in which case we need to either redirect
+    // to JG's "setup an SDK" wizard or include it's first page here. So right now let's
+    // be conservative and deal with the fact that SdkData or SdkState can be null.
+    AndroidSdkData sdkData = AndroidSdkUtils.tryToChooseAndroidSdk();
+    SdkState sdkState = sdkData == null ? null : SdkState.getInstance(sdkData);
+    myWizardState = new SmwState(sdkState);
+
+    // TODO even if we do have an SdkData structure, we should account for the path to
+    // be invalid (folder missing or not an SDK) and include as first step JG's page
+    // about setting up an SDK. TL;DR: this is temporary, need to adjust workflow.
+    mySelectionStep = new SmwSelectionStep(myWizardState);
+    myConfirmationStep = new SmwConfirmationStep(myWizardState);
+    myProgressStep = new SmwProgressStep(myWizardState);
+
+    addStep(mySelectionStep);
+    addStep(myConfirmationStep);
+    addStep(myProgressStep);
+
+    for (SmwStep step : mySteps) {
+      if (step instanceof Disposable) {
+        Disposer.register(getDisposable(), (Disposable)step);
+      }
+    }
+
+    super.init();
+  }
+
+  @Override
+  protected void dispose() {
+    super.dispose();
+
+  }
+
+  @Override
+  @Nullable
+  protected String getHelpID() {
+    return null;
+  }
+
+  @Override
+  protected boolean canGoNext() {
+    SmwStep step = getCurrentStepObject();
+    return step != null && step.canGoNext();
+  }
+
+}
