46cb578300cee40c52616b19c7ab6380ce39ff45
Philip Milne <pmilne@google.com>
2013-11-14 19:49:36 -0800

    Tidy
    Change-Id: Ib6bf010eacf03bf87a9332046e841ae8f0026102

--- android/src/com/android/tools/idea/editors/navigation/NavigationEditor.java
+++ android/src/com/android/tools/idea/editors/navigation/NavigationEditor.java
@@ -151,20 +151,28 @@ public class NavigationEditor implements FileEditor {
     final Map<String, ActivityState> activities = getActivities(model);
     final Map<String, MenuState> menus = getMenus(model);
 
-    final PsiMethod methodCallMacro = getMethodsByName(module, "com.android.templates.GeneralTemplates", "call")[0];
+    PsiMethod methodCallMacro = getMethodsByName(module, "com.android.templates.GeneralTemplates", "call")[0];
     final PsiMethod defineAssignment = getMethodsByName(module, "com.android.templates.GeneralTemplates", "defineAssignment")[0];
-    final PsiMethod defineInnerClassMacro = getMethodsByName(module, "com.android.templates.GeneralTemplates", "defineInnerClass")[0];
+    PsiMethod defineInnerClassMacro = getMethodsByName(module, "com.android.templates.GeneralTemplates", "defineInnerClass")[0];
 
-    final PsiMethod installMenuItemClickMacro =
+    PsiMethod installMenuItemClickMacro =
       getMethodsByName(module, "com.android.templates.InstallListenerTemplates", "installMenuItemClick")[0];
-    final PsiMethod installItemClickMacro =
+    PsiMethod installItemClickMacro =
       getMethodsByName(module, "com.android.templates.InstallListenerTemplates", "installItemClickListener")[0];
 
-    final PsiMethod getMenuItemMacro = getMethodsByName(module, "com.android.templates.MenuAccessTemplates", "getMenuItem")[0];
-    final PsiMethod launchActivityMacro = getMethodsByName(module, "com.android.templates.LaunchActivityTemplates", "launchActivity")[0];
-    final PsiMethod launchActivityMacro2 = getMethodsByName(module, "com.android.templates.LaunchActivityTemplates", "launchActivity")[1];
+    PsiMethod getMenuItemMacro = getMethodsByName(module, "com.android.templates.MenuAccessTemplates", "getMenuItem")[0];
+    PsiMethod launchActivityMacro = getMethodsByName(module, "com.android.templates.LaunchActivityTemplates", "launchActivity")[0];
+    PsiMethod launchActivityMacro2 = getMethodsByName(module, "com.android.templates.LaunchActivityTemplates", "launchActivity")[1];
 
-    //model.getTransitions().clear();
+    final MultiMatch installItemClickAndCallMacro = new MultiMatch(installItemClickMacro);
+    installItemClickAndCallMacro.addSubMacro("$f", methodCallMacro);
+
+    final MultiMatch installMenuItemOnGetMenuItemAndLaunchActivityMacro = new MultiMatch(installMenuItemClickMacro);
+    installMenuItemOnGetMenuItemAndLaunchActivityMacro.addSubMacro("$menuItem", getMenuItemMacro);
+    installMenuItemOnGetMenuItemAndLaunchActivityMacro.addSubMacro("$f", launchActivityMacro);
+
+    final MultiMatch defineInnerClassToLaunchActivityMacro = new MultiMatch(defineInnerClassMacro);
+    defineInnerClassToLaunchActivityMacro.addSubMacro("$f", launchActivityMacro2);
 
     final Collection<ActivityState> activityStates = activities.values();
 
@@ -191,10 +199,7 @@ public class NavigationEditor implements FileEditor {
               PsiStatement[] statements = onPrepareOptionsMenuMethod.getBody().getStatements();
               if (DEBUG) System.out.println("statements = " + Arrays.toString(statements));
               for (PsiStatement s : statements) {
-                Map<String, PsiMethod> constraints = new HashMap<String, PsiMethod>();
-                constraints.put("$menuItem", getMenuItemMacro);
-                constraints.put("$f", launchActivityMacro);
-                MultiMatchResult multiMatchResult = multiMatch(installMenuItemClickMacro, constraints, s.getFirstChild());
+                MultiMatchResult multiMatchResult = installMenuItemOnGetMenuItemAndLaunchActivityMacro.match(s.getFirstChild());
                 if (multiMatchResult != null) {
                   Map<String, Map<String, PsiElement>> subBindings = multiMatchResult.subBindings;
                   ActivityState activityState =
@@ -221,9 +226,7 @@ public class NavigationEditor implements FileEditor {
           PsiMethod installListenersMethod = methods[0];
           PsiStatement[] statements = installListenersMethod.getBody().getStatements();
           for (PsiStatement s : statements) {
-            Map<String, PsiMethod> constraints = new HashMap<String, PsiMethod>();
-            constraints.put("$f", methodCallMacro);
-            MultiMatchResult multiMatchResult = multiMatch(installItemClickMacro, constraints, s.getFirstChild());
+            MultiMatchResult multiMatchResult = installItemClickAndCallMacro.match(s.getFirstChild());
             if (multiMatchResult != null) {
               final Map<String, PsiElement> bindings = multiMatchResult.bindings;
               final Map<String, Map<String, PsiElement>> subBindings = multiMatchResult.subBindings;
@@ -250,9 +253,7 @@ public class NavigationEditor implements FileEditor {
                         }
                       }
                     }
-                    Map<String, PsiMethod> constraints = new HashMap<String, PsiMethod>();
-                    constraints.put("$f", launchActivityMacro2);
-                    MultiMatchResult multiMatchResult1 = multiMatch(defineInnerClassMacro, constraints, rExpression);
+                    MultiMatchResult multiMatchResult1 = defineInnerClassToLaunchActivityMacro.match(rExpression);
                     if (multiMatchResult1 != null) {
                       PsiElement activityClass = multiMatchResult1.subBindings.get("$f").get("activityClass").getFirstChild();
                       State toState = activities.get(getQualifiedName(activityClass));
@@ -274,6 +275,39 @@ public class NavigationEditor implements FileEditor {
     return model;
   }
 
+  static class MultiMatch {
+    public final PsiMethod macro;
+    public final Map<String, PsiMethod> subMacros = new LinkedHashMap<String, PsiMethod>(); // make deterministic while prototyping
+
+    MultiMatch(PsiMethod macro) {
+      this.macro = macro;
+    }
+
+    void addSubMacro(String name, PsiMethod macro) {
+      subMacros.put(name, macro);
+    }
+
+    @Nullable
+    MultiMatchResult match(PsiElement element) {
+      Map<String, PsiElement> bindings = NavigationEditor.match(macro, element);
+      if (DEBUG) System.out.println("bindings = " + bindings);
+      if (bindings == null) {
+        return null;
+      }
+      Map<String, Map<String, PsiElement>> subBindings = new HashMap<String, Map<String, PsiElement>>();
+      for (Map.Entry<String, PsiMethod> entry : subMacros.entrySet()) {
+        String name = entry.getKey();
+        PsiMethod template = entry.getValue();
+        Map<String, PsiElement> subBinding = NavigationEditor.match(template, bindings.get(name));
+        if (subBinding == null) {
+          return null;
+        }
+        subBindings.put(name, subBinding);
+      }
+      return new MultiMatchResult(bindings, subBindings);
+    }
+  }
+
   static class MultiMatchResult {
     final Map<String, PsiElement> bindings;
     final Map<String, Map<String, PsiElement>> subBindings;
@@ -282,26 +316,6 @@ public class NavigationEditor implements FileEditor {
       this.bindings = bindings;
       this.subBindings = subBindings;
     }
-  }
-
-  @Nullable
-  private static MultiMatchResult multiMatch(PsiMethod macro, Map<String, PsiMethod> subMacros, PsiElement element) {
-    Map<String, PsiElement> bindings = match(macro, element);
-    if (DEBUG) System.out.println("bindings = " + bindings);
-    if (bindings == null) {
-      return null;
-    }
-    Map<String, Map<String, PsiElement>> subBindings = new HashMap<String, Map<String, PsiElement>>();
-    for (Map.Entry<String, PsiMethod> entry : subMacros.entrySet()) {
-      String name = entry.getKey();
-      PsiMethod template = entry.getValue();
-      Map<String, PsiElement> subBinding = match(template, bindings.get(name));
-      if (subBinding == null) {
-        return null;
-      }
-      subBindings.put(name, subBinding);
-    }
-    return new MultiMatchResult(bindings, subBindings);
   }
 
   private static List<FragmentEntry> getFragmentEntries(XmlFile psiFile) {
