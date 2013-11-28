c442df5f26b19275a6f444eba47dda6c22e2971c
Philip Milne <pmilne@google.com>
2013-11-27 18:26:39 -0800

    Move the Java code recognition/generation templates from the example app into the IDE
    Change-Id: Ia8c17f56d3c3ad720fb7a622a70e8bd4e7fe2c29

--- android/src/com/android/tools/idea/editors/navigation/macros/Macros.java
+++ android/src/com/android/tools/idea/editors/navigation/macros/Macros.java
@@ -16,30 +16,64 @@
 package com.android.tools.idea.editors.navigation.macros;
 
 import com.intellij.openapi.module.Module;
+import com.intellij.psi.JavaPsiFacade;
+import com.intellij.psi.PsiClass;
+import com.intellij.psi.PsiElementFactory;
 import com.intellij.psi.PsiMethod;
 
-import static com.android.tools.idea.editors.navigation.Utilities.getMethodsByName;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class Macros {
+  private static final String TEMPLATES_PACKAGE = /*"com.android.templates.".replace('.', '/')*/ "";
+  private static final String GENERAL_TEMPLATES = TEMPLATES_PACKAGE + "GeneralTemplates";
+  private static final String LISTENER_TEMPLATES = TEMPLATES_PACKAGE + "InstallListenerTemplates";
+  private static final String MENU_ACCESS_TEMPLATES = TEMPLATES_PACKAGE + "MenuAccessTemplates";
+  private static final String LAUNCH_ACTIVITY_TEMPLATES = TEMPLATES_PACKAGE + "LaunchActivityTemplates";
+
   public final PsiMethod defineAssignment;
 
   public final MultiMatch installItemClickAndCallMacro;
   public final MultiMatch installMenuItemOnGetMenuItemAndLaunchActivityMacro;
   public final MultiMatch defineInnerClassToLaunchActivityMacro;
 
+  private static PsiMethod[] getMethodsByName(Module module, String templateName, String methodName) {
+    JavaPsiFacade facade = JavaPsiFacade.getInstance(module.getProject());
+    final PsiElementFactory factory = facade.getElementFactory();
+    ClassLoader classLoader = Macros.class.getClassLoader();
+    try {
+      InputStream inputStream = classLoader.getResourceAsStream("/navigationTemplates/" + templateName + ".java.template");
+      try {
+        int available = inputStream.available();
+        byte[] buffer = new byte[available];
+        assert available == inputStream.read(buffer);
+        String text = new String(buffer);
+        int start = text.indexOf("{");
+        int end = text.lastIndexOf("}");
+        String body = text.substring(start + 1, end);
+        PsiClass psiClass = factory.createClassFromText(body, null); //todo consider providing a context
+        return psiClass.findMethodsByName(methodName, false);
+      }
+      finally {
+        inputStream.close();
+      }
+    }
+    catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
   public Macros(Module module) {
-    PsiMethod methodCallMacro = getMethodsByName(module, "com.android.templates.GeneralTemplates", "call")[0];
-    defineAssignment = getMethodsByName(module, "com.android.templates.GeneralTemplates", "defineAssignment")[0];
-    PsiMethod defineInnerClassMacro = getMethodsByName(module, "com.android.templates.GeneralTemplates", "defineInnerClass")[0];
+    PsiMethod methodCallMacro = getMethodsByName(module, GENERAL_TEMPLATES, "call")[0];
+    defineAssignment = getMethodsByName(module, GENERAL_TEMPLATES, "defineAssignment")[0];
+    PsiMethod defineInnerClassMacro = getMethodsByName(module, GENERAL_TEMPLATES, "defineInnerClass")[0];
 
-    PsiMethod installMenuItemClickMacro =
-      getMethodsByName(module, "com.android.templates.InstallListenerTemplates", "installMenuItemClick")[0];
-    PsiMethod installItemClickMacro =
-      getMethodsByName(module, "com.android.templates.InstallListenerTemplates", "installItemClickListener")[0];
+    PsiMethod installMenuItemClickMacro = getMethodsByName(module, LISTENER_TEMPLATES, "installMenuItemClick")[0];
+    PsiMethod installItemClickMacro = getMethodsByName(module, LISTENER_TEMPLATES, "installItemClickListener")[0];
 
-    PsiMethod getMenuItemMacro = getMethodsByName(module, "com.android.templates.MenuAccessTemplates", "getMenuItem")[0];
-    PsiMethod launchActivityMacro = getMethodsByName(module, "com.android.templates.LaunchActivityTemplates", "launchActivity")[0];
-    PsiMethod launchActivityMacro2 = getMethodsByName(module, "com.android.templates.LaunchActivityTemplates", "launchActivity")[1];
+    PsiMethod getMenuItemMacro = getMethodsByName(module, MENU_ACCESS_TEMPLATES, "getMenuItem")[0];
+    PsiMethod launchActivityMacro = getMethodsByName(module, LAUNCH_ACTIVITY_TEMPLATES, "launchActivity")[0];
+    PsiMethod launchActivityMacro2 = getMethodsByName(module, LAUNCH_ACTIVITY_TEMPLATES, "launchActivity")[1];
 
     installItemClickAndCallMacro = new MultiMatch(installItemClickMacro);
     installItemClickAndCallMacro.addSubMacro("$f", methodCallMacro);
