5db40703a29026a5a05dd79756868683b80882b7
Alex Ruiz <alruiz@google.com>
2014-01-10 16:07:31 -0800

    "Open SDK Manager" action now uses IDE's default SDK path.
    Change-Id: I2b86aa19f088f4676537ad7725ae3c4748e46406

--- android/src/org/jetbrains/android/actions/AndroidRunSdkToolAction.java
+++ android/src/org/jetbrains/android/actions/AndroidRunSdkToolAction.java
@@ -1,11 +1,12 @@
 package org.jetbrains.android.actions;
 
+import com.android.tools.idea.sdk.DefaultSdks;
+import com.android.tools.idea.startup.AndroidStudioSpecificInitializer;
 import com.intellij.CommonBundle;
 import com.intellij.facet.ProjectFacetManager;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
-import com.intellij.openapi.actionSystem.PlatformDataKeys;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.Messages;
 import com.intellij.util.ArrayUtil;
@@ -15,6 +16,7 @@ import org.jetbrains.android.sdk.AndroidSdkData;
 import org.jetbrains.android.util.AndroidBundle;
 import org.jetbrains.annotations.NotNull;
 
+import java.io.File;
 import java.util.List;
 import java.util.Set;
 
@@ -28,18 +30,25 @@ public abstract class AndroidRunSdkToolAction extends AnAction {
 
   @Override
   public void update(AnActionEvent e) {
-    final Project project = e.getData(CommonDataKeys.PROJECT);
+    Project project = e.getData(CommonDataKeys.PROJECT);
     e.getPresentation().setEnabled(project != null && ProjectFacetManager.getInstance(project).getFacets(AndroidFacet.ID).size() > 0);
   }
 
   @Override
   public void actionPerformed(AnActionEvent e) {
-    final Project project = e.getData(CommonDataKeys.PROJECT);
+    Project project = e.getData(CommonDataKeys.PROJECT);
     assert project != null;
     doAction(project);
   }
 
   public void doAction(@NotNull Project project) {
+    if (AndroidStudioSpecificInitializer.isAndroidStudio()) {
+      File androidHome = DefaultSdks.getDefaultAndroidHome();
+      if (androidHome != null) {
+        doRunTool(project, androidHome.getPath());
+        return;
+      }
+    }
     List<AndroidFacet> facets = ProjectFacetManager.getInstance(project).getFacets(AndroidFacet.ID);
     assert facets.size() > 0;
     Set<String> sdkSet = new HashSet<String>();
