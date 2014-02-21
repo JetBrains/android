049fd040a6f029087868bffee4f2b9d96b5656cc
Josiah Gaskin <josiahgaskin@google.com>
2014-02-20 20:35:55 -0800

    Register MergingUpdatequeue in Disposable chain
    Fix broken leak test by properly registering disposable.
    
    Change-Id: I7d7e6f644a0cab4b0631a9727f324f6f02fd4f0f

--- android/src/com/android/tools/idea/wizard/NewProjectWizard.java
+++ android/src/com/android/tools/idea/wizard/NewProjectWizard.java
@@ -30,6 +30,7 @@ import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.Messages;
+import com.intellij.openapi.util.Disposer;
 import com.intellij.openapi.util.io.FileUtilRt;
 import com.intellij.pom.java.LanguageLevel;
 import org.jetbrains.android.sdk.AndroidSdkUtils;
@@ -104,6 +105,7 @@ public class NewProjectWizard extends TemplateWizard implements TemplateParamete
     myConfigureAndroidModuleStep = new ConfigureAndroidModuleStep(myWizardState, myProject, NewProjectSidePanel, this);
     myConfigureAndroidModuleStep.updateStep();
     myAssetSetStep = new AssetSetStep(myWizardState, myProject, null, NewProjectSidePanel, this);
+    Disposer.register(getDisposable(), myAssetSetStep);
     myAssetGenerator = new AssetStudioAssetGenerator(myWizardState);
     myAssetSetStep.finalizeAssetType(AssetStudioAssetGenerator.AssetType.LAUNCHER);
     myChooseActivityStep =
