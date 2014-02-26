182af7770b18d97879911800e2af6b79600bf9d4
Siva Velusamy <vsiva@google.com>
2014-02-25 17:13:09 -0800

    Allow customizing gradle task executed before run
    Currently the launch configuration allows users to specify that
    a "Gradle-aware make" should be executed before run. The task is
    not configurable and simply builds the bits necessary for the launch.
    
    This CL allows users to specify the specific gradle task to use,
    enabling them to run custom tasks before every launch.
    
    Fix for https://code.google.com/p/android/issues/detail?id=62258
    
    Change-Id: Ie657e4df784bb4da129496f1b32c9af41f57208a

--- android/src/com/android/tools/idea/gradle/run/MakeBeforeRunTask.java
+++ android/src/com/android/tools/idea/gradle/run/MakeBeforeRunTask.java
@@ -16,13 +16,75 @@
 package com.android.tools.idea.gradle.run;
 
 import com.intellij.execution.BeforeRunTask;
+import org.jdom.Element;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * @see MakeBeforeRunTaskProvider
  */
 public class MakeBeforeRunTask extends BeforeRunTask<MakeBeforeRunTask> {
+  private static final String SERIALIZATION_KEY = "goal";
+
+  private String myGoal;
+  private boolean myIsValid = true;
+
   protected MakeBeforeRunTask() {
     super(MakeBeforeRunTaskProvider.ID);
     setEnabled(true);
+  }
+
+  public void setGoal(String goal) {
+    myGoal = goal;
+  }
+
+  @Nullable
+  public String getGoal() {
+    return myGoal;
+  }
+
+  public void setInvalid() {
+    myIsValid = false;
+  }
+
+  public boolean isValid() {
+    return myIsValid;
+  }
+
+  @Override
+  public void writeExternal(Element element) {
+    super.writeExternal(element);
+    if (myGoal != null) {
+      element.setAttribute(SERIALIZATION_KEY, myGoal);
+    }
+  }
+
+  @Override
+  public void readExternal(Element element) {
+    super.readExternal(element);
+    myGoal = element.getAttributeValue(SERIALIZATION_KEY);
+  }
+
+  // equals and hashCode are necessary so that the infrastructure makes the necessary
+  // calls to readExternal & writeExternal above.
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    if (!super.equals(o)) return false;
+
+    MakeBeforeRunTask that = (MakeBeforeRunTask)o;
+
+    if (myIsValid != that.myIsValid) return false;
+    if (myGoal != null ? !myGoal.equals(that.myGoal) : that.myGoal != null) return false;
+
+    return true;
+  }
+
+  @Override
+  public int hashCode() {
+    int result = super.hashCode();
+    result = 31 * result + (myGoal != null ? myGoal.hashCode() : 0);
+    result = 31 * result + (myIsValid ? 1 : 0);
+    return result;
   }
 }
