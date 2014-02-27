7c53ecee2923ddb852bdc90fb830ccc624b00652
Raphael Moll <ralf@android.com>
2014-02-26 16:03:12 -0800

    Facility to send "build record" stats.
    This adds a persistence component to record build stats records,
    then changes the stats service to send them to the stats server.
    
    Change-Id: Icdf2368ba2c402da0d0b645c98168b795b944bbc

--- android/src/com/android/tools/idea/stats/KeyString.java
+++ android/src/com/android/tools/idea/stats/KeyString.java
@@ -20,7 +20,7 @@ public class KeyString {
   private final String myKey;
   private final String myValue;
 
-  KeyString(String key, String value) {
+  public KeyString(String key, String value) {
     myKey = key;
     myValue = value;
   }
@@ -31,5 +31,34 @@ public class KeyString {
 
   public String getValue() {
     return myValue;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    KeyString keyString = (KeyString)o;
+
+    if (myKey != null ? !myKey.equals(keyString.myKey) : keyString.myKey != null) return false;
+    if (myValue != null ? !myValue.equals(keyString.myValue) : keyString.myValue != null) return false;
+
+    return true;
+  }
+
+  @Override
+  public int hashCode() {
+    int result = myKey != null ? myKey.hashCode() : 0;
+    result = 31 * result + (myValue != null ? myValue.hashCode() : 0);
+    return result;
+  }
+
+  // For debugging purposes
+  @Override
+  public String toString() {
+    return "KeyString{" +
+           "myKey='" + myKey + '\'' +
+           ", myValue='" + myValue + '\'' +
+           '}';
   }
 }
