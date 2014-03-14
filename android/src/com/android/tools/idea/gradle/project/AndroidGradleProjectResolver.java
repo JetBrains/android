a06879346051aa674a4203734b01c248ded6fb7f
Alex Ruiz <alruiz@google.com>
2014-03-14 11:08:31 -0700

    Fixed bug where physical Gradle sub-project path was derived from logical Gradle path.
    Problem:
    IDEA by default, when importing a Gradle project, deduces the physical path (in the
    file system) of the IDEA module to create from the Gradle sub-project's logical path.
    For example, if the logical path is dir1:dir2:app, the deduced physical path would
    be ${projectDirPath}/dir1/dir2/app. This mechanism is not reliable, because in
    settings.gradle it is possible to map logical sub-project paths to any physical path,
    for example:
    
    include ':app'
    project(':app').projectDir = new File(rootDir, 'test/app')
    
    This mapping breaks IDEA's assumption, because :app is mapped to test/app.
    
    Solution:
    This CL fixes this problem by getting the physical path of a Gradle sub-project from
    a GradleBuild. GradleBuild is an additional model that we query from Gradle and has
    a lot more information about sub-projects, including the actual physical path.
    
    Change-Id: Ifa59c4810615fa27f5f8d5753bc89ea85374ff39

--- android/src/com/android/tools/idea/gradle/project/AndroidGradleProjectResolver.java
+++ android/src/com/android/tools/idea/gradle/project/AndroidGradleProjectResolver.java
@@ -37,17 +37,24 @@ import com.google.common.collect.Sets;
 import com.intellij.execution.configurations.SimpleJavaParameters;
 import com.intellij.openapi.externalSystem.model.DataNode;
 import com.intellij.openapi.externalSystem.model.ExternalSystemException;
+import com.intellij.openapi.externalSystem.model.ProjectKeys;
 import com.intellij.openapi.externalSystem.model.project.ModuleData;
 import com.intellij.openapi.externalSystem.model.project.ProjectData;
+import com.intellij.openapi.externalSystem.model.task.TaskData;
 import com.intellij.openapi.externalSystem.util.ExternalSystemApiUtil;
 import com.intellij.openapi.externalSystem.util.ExternalSystemConstants;
 import com.intellij.openapi.externalSystem.util.Order;
+import com.intellij.openapi.module.StdModuleTypes;
 import com.intellij.openapi.util.KeyValue;
 import com.intellij.openapi.util.io.FileUtil;
+import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.util.ExceptionUtil;
 import com.intellij.util.PathUtil;
 import com.intellij.util.containers.ContainerUtil;
 import com.intellij.util.containers.ContainerUtilRt;
+import org.gradle.tooling.model.GradleTask;
+import org.gradle.tooling.model.gradle.BasicGradleProject;
+import org.gradle.tooling.model.gradle.GradleBuild;
 import org.gradle.tooling.model.gradle.GradleScript;
 import org.gradle.tooling.model.idea.IdeaCompilerOutput;
 import org.gradle.tooling.model.idea.IdeaContentRoot;
@@ -97,7 +104,106 @@ public class AndroidGradleProjectResolver extends AbstractProjectResolverExtensi
       String msg = getUnsupportedModelVersionErrorMsg(GradleModelVersionCheck.getModelVersion(androidProject));
       throw new IllegalStateException(msg);
     }
-    return nextResolver.createModule(gradleModule, projectData);
+
+    String moduleName = gradleModule.getName();
+    if (moduleName == null) {
+      throw new IllegalStateException("Module with undefined name detected: " + gradleModule);
+    }
+
+    File moduleDirPath = getModuleDirPath(gradleModule);
+    if (moduleDirPath == null) {
+      return nextResolver.createModule(gradleModule, projectData);
+    }
+
+    String moduleConfigPath = moduleDirPath.getPath();
+
+    String gradlePath = gradleModule.getGradleProject().getPath();
+    String id = StringUtil.isEmpty(gradlePath) || ":".equals(gradlePath) ? moduleName : gradlePath;
+    ModuleData moduleData =
+      new ModuleData(id, GradleConstants.SYSTEM_ID, StdModuleTypes.JAVA.getId(), moduleName, moduleConfigPath, moduleConfigPath);
+
+    ModuleExtendedModel moduleExtendedModel = resolverCtx.getExtraProject(gradleModule, ModuleExtendedModel.class);
+    if (moduleExtendedModel != null) {
+      moduleData.setGroup(moduleExtendedModel.getGroup());
+      moduleData.setVersion(moduleExtendedModel.getVersion());
+      moduleData.setArtifacts(moduleExtendedModel.getArtifacts());
+    }
+    return moduleData;
+  }
+
+  @NotNull
+  @Override
+  public Collection<TaskData> populateModuleTasks(@NotNull IdeaModule gradleModule,
+                                                  @NotNull DataNode<ModuleData> ideModule,
+                                                  @NotNull DataNode<ProjectData> ideProject) {
+    File moduleDirPath = getModuleDirPath(gradleModule);
+    if (moduleDirPath == null) {
+      return nextResolver.populateModuleTasks(gradleModule, ideModule, ideProject);
+    }
+
+    Collection<TaskData> tasks = Lists.newArrayList();
+    String moduleConfigPath = moduleDirPath.getPath();
+
+    for (GradleTask task : gradleModule.getGradleProject().getTasks()) {
+      String taskName = task.getName();
+      if (taskName == null || taskName.trim().isEmpty() || isIdeaTask(taskName)) {
+        continue;
+      }
+      TaskData taskData = new TaskData(GradleConstants.SYSTEM_ID, taskName, moduleConfigPath, task.getDescription());
+      ideModule.createChild(ProjectKeys.TASK, taskData);
+      tasks.add(taskData);
+    }
+
+    return tasks;
+  }
+
+  private static boolean isIdeaTask(@NotNull String taskName) {
+    return taskName.toLowerCase().contains("idea");
+  }
+
+  @Nullable
+  private File getModuleDirPath(@NotNull IdeaModule gradleModule) {
+    GradleBuild build = resolverCtx.getExtraProject(gradleModule, GradleBuild.class);
+    if (build == null) {
+      // We got here only if the project is using Gradle version 1.7 or earlier. For Android projects it will never be the case.
+      return null;
+    }
+    String gradlePath = gradleModule.getGradleProject().getPath();
+    File moduleDirPath = getModuleDirPath(build, gradlePath);
+    if (moduleDirPath == null) {
+      throw new IllegalStateException(String.format("Unable to find root directory for module '%s'", gradleModule.getName()));
+    }
+    return moduleDirPath;
+  }
+
+  /**
+   * Returns the physical path of the module's root directory (the path in the file system.)
+   * <p>
+   * It is important to note that Gradle has its own "logical" path that may or may not be equal to the physical path of a Gradle project.
+   * For example, the sub-project at ${projectRootDir}/apps/app will have the Gradle path :apps:app. Gradle also allows mapping physical
+   * paths to a different logical path. For example, in settings.gradle:
+   * <pre>
+   *   include ':app'
+   *   project(':app').projectDir = new File(rootDir, 'apps/app')
+   * </pre>
+   * In this example, sub-project at ${projectRootDir}/apps/app will have the Gradle path :app.
+   * </p>
+   *
+   * @param build contains information about the root Gradle project and its sub-projects. Such information includes the physical path of
+   *              the root Gradle project and its sub-projects.
+   * @param path  the Gradle "logical" path. This path uses colon as separator, and may or may not be equal to the physical path of a
+   *              Gradle project.
+   * @return the physical path of the module's root directory.
+   */
+  @VisibleForTesting
+  @Nullable
+  static File getModuleDirPath(@NotNull GradleBuild build, @NotNull String path) {
+    for (BasicGradleProject project : build.getProjects()) {
+      if (project.getPath().equals(path)) {
+        return project.getProjectDirectory();
+      }
+    }
+    return null;
   }
 
   @Override
@@ -214,7 +320,7 @@ public class AndroidGradleProjectResolver extends AbstractProjectResolverExtensi
   @Override
   @NotNull
   public Set<Class> getExtraProjectModelClasses() {
-    return Collections.<Class>singleton(AndroidProject.class);
+    return Sets.<Class>newHashSet(AndroidProject.class, GradleBuild.class);
   }
 
 
