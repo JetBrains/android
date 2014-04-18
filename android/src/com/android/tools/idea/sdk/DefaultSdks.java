388663ec3c87dba7524a011a04478e8447915bfd
Eugene Kudelevsky <Eugene.Kudelevsky@jetbrains.com>
2014-04-18 18:16:31 +0400

    IDEA-124099 fix IAE (cherry picked from commit 7799fd7)

--- android/src/com/android/tools/idea/sdk/DefaultSdks.java
+++ android/src/com/android/tools/idea/sdk/DefaultSdks.java
@@ -463,7 +463,7 @@ public final class DefaultSdks {
     VirtualFile javaHome = null;
 
     for (String jdkPath : jdkPaths) {
-      javaHome = LocalFileSystem.getInstance().findFileByPath(jdkPath);
+      javaHome = jdkPath != null ? LocalFileSystem.getInstance().findFileByPath(jdkPath) : null;
 
       if (javaHome != null) {
         break;
