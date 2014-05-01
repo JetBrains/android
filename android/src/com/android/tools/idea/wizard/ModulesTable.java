d436b8c57e7fef9b30f13a16d1e8800ab39bbd59
Eugene Ostroukhov <eostroukhov@google.com>
2014-05-01 11:04:11 -0700

    Validate user input when wrapping archive in a subproject
    Remaining tasks:
    1. Implement moving Jars/Aars that are inside the project (create a module,
       remove original Jar, add dependency)
    2. Look into scanning modules if they already depend on jar being imported
       and suggest replacing them with dependency.
    
    Change-Id: I07d80e612af0c565ba30f68f3c38571cd96f2576

--- android/src/com/android/tools/idea/wizard/ModulesTable.java
+++ android/src/com/android/tools/idea/wizard/ModulesTable.java
@@ -37,7 +37,6 @@ import javax.swing.table.TableModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.File;
 import java.text.Collator;
 import java.util.*;
 import java.util.List;
@@ -255,30 +254,12 @@ public final class ModulesTable extends JBTable {
     }
   }
 
-  private boolean projectHasSubprojectWithPath(ModuleToImport module) {
-    if (myProject == null) {
-      return false;
-    }
-    else {
-      String name = module.name;
-      if (GradleUtil.findModuleByGradlePath(myProject, name) != null) {
-        return true;
-      }
-      File targetPath = GradleUtil.getDefaultSubprojectLocation(myProject.getBaseDir(), name);
-      if (targetPath.exists()) {
-        String[] children = targetPath.list();
-        return (children == null || children.length > 0);
-      }
-      return false;
-    }
-  }
-
   private ModuleValidationState validateModule(ModuleToImport module) {
     VirtualFile location = module.location;
     if (location == null) {
       return ModuleValidationState.NOT_FOUND;
     }
-    if (projectHasSubprojectWithPath(module)) {
+    if (GradleUtil.hasModule(myProject, module.name, true)) {
       return ModuleValidationState.ALREADY_EXISTS;
     }
     if (Objects.equal(location, myCurrentPath)) {
