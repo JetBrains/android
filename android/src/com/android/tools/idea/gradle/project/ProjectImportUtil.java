519f09ecfde22f8331adde5556e4ad59c53a1489
Eugene Ostroukhov <eostroukhov@google.com>
2014-05-13 16:41:06 -0700

    Make it easier to add project import special cases
    As users are submitting new bugs, in some cases it is easier to add new
    "importers". Previous approach of having enum of import types does not
    scale as well as needed.
    
    Change-Id: Ifc72dd3e8ac50cfb6cfb90174419cf906db2bc7e

--- android/src/com/android/tools/idea/gradle/project/ProjectImportUtil.java
+++ android/src/com/android/tools/idea/gradle/project/ProjectImportUtil.java
@@ -17,27 +17,15 @@ package com.android.tools.idea.gradle.project;
 
 import com.android.SdkConstants;
 import com.android.tools.gradle.eclipse.GradleImport;
-import com.google.common.base.Function;
-import com.google.common.base.Functions;
-import com.google.common.base.Suppliers;
-import com.google.common.collect.Maps;
-import com.google.common.collect.Sets;
-import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.diagnostic.Logger;
-import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.JDOMUtil;
-import com.intellij.openapi.vfs.VfsUtil;
-import com.intellij.openapi.vfs.VfsUtilCore;
 import com.intellij.openapi.vfs.VirtualFile;
 import org.jdom.Element;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import org.jetbrains.plugins.gradle.util.GradleConstants;
 
 import java.io.File;
-import java.io.IOException;
-import java.util.*;
 import java.util.List;
 
 /**
@@ -108,52 +96,4 @@ public final class ProjectImportUtil {
     }
     return false;
   }
-
-  @NotNull
-  public static ImportSourceKind getImportLocationKind(@NotNull VirtualFile file) {
-    VirtualFile target = findImportTarget(file);
-    if (target == null) {
-      return ImportSourceKind.NOTHING;
-    }
-    // Prioritize ADT importer
-    VirtualFile targetDir = target.isDirectory() ? target : target.getParent();
-    File targetDirFile = VfsUtilCore.virtualToIoFile(targetDir);
-    if (GradleImport.isAdtProjectDir(targetDirFile) && targetDir.findChild(SdkConstants.FN_BUILD_GRADLE) == null) {
-      return ImportSourceKind.ADT;
-    }
-    if (GradleImport.isEclipseProjectDir(targetDirFile) && targetDir.findChild(SdkConstants.FN_BUILD_GRADLE) == null) {
-      return ImportSourceKind.ECLIPSE;
-    }
-    if (GradleConstants.EXTENSION.equals(target.getExtension())) {
-      return ImportSourceKind.GRADLE;
-    }
-    return ImportSourceKind.OTHER;
-  }
-
-  public static Set<ModuleToImport> findModules(final @NotNull VirtualFile vfile,
-                                                     final @NotNull Project project) throws IOException {
-    ApplicationManager.getApplication().assertIsDispatchThread();
-    ImportSourceKind kind = getImportLocationKind(vfile);
-    if (kind == ImportSourceKind.GRADLE) {
-      return GradleProjectImporter.getInstance().getRelatedProjects(vfile, project);
-    } else if (kind == ImportSourceKind.ADT) {
-      return getAdtProjects(vfile);
-    } else {
-      return Collections.emptySet();
-    }
-  }
-
-  private static Set<ModuleToImport> getAdtProjects(VirtualFile vfile) throws IOException {
-    GradleImport gradleImport = new GradleImport();
-    gradleImport.importProjects(Collections.singletonList(VfsUtilCore.virtualToIoFile(vfile)));
-    Map<String, File> adtProjects = gradleImport.getDetectedModuleLocations();
-    Function<Object, Iterable<String>> resolver = Functions.<Iterable<String>>constant(Collections.<String>emptySet());
-    Set<ModuleToImport> modules = Sets.newHashSet();
-    for (Map.Entry<String, File> entry : adtProjects.entrySet()) {
-      VirtualFile location = VfsUtil.findFileByIoFile(entry.getValue(), false);
-      modules.add(new ModuleToImport(entry.getKey(), location, resolver));
-    }
-    return modules;
-  }
-
 }
