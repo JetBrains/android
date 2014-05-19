851683193ec0431cbdcbfa912c31e2147f0a4c85
Tor Norbye <tnorbye@google.com>
2014-05-19 14:49:47 -0700

    Add multiconfiguration preview for API levels
    For various reason we use a single layoutlib (and a single set of loaded framework
    resources), along with a special implementation of IAndroidTarget which reports an
    older API level, but delegates everything else to the new layoutlib. This helps trick
    the resource resolver into resolving best-fit resources from the given API level,
    it helps the default themes chosen from layouts to reflect the preferred theme of
    that API level (e.g. "Theme" for Gingerbread, "Theme.Holo" for IceCreamSandwich),
    and so on.
    
    It also adds id's to render preview, which lets you delete a particular preview (say
    Nexus S from the device screen previews), and as you switch layouts it remembers
    these deleted id's and excludes them there as well.
    
    Change-Id: I7db2db3384f636d7ed26212213d6b02b1b996ac5

--- android/src/com/android/tools/idea/configurations/ResourceResolverCache.java
+++ android/src/com/android/tools/idea/configurations/ResourceResolverCache.java
@@ -15,17 +15,21 @@
  */
 package com.android.tools.idea.configurations;
 
+import com.android.SdkConstants;
 import com.android.ide.common.rendering.api.ResourceValue;
 import com.android.ide.common.resources.FrameworkResources;
 import com.android.ide.common.resources.ResourceRepository;
 import com.android.ide.common.resources.ResourceResolver;
+import com.android.ide.common.resources.configuration.DensityQualifier;
 import com.android.ide.common.resources.configuration.FolderConfiguration;
+import com.android.resources.Density;
 import com.android.resources.ResourceType;
 import com.android.sdklib.IAndroidTarget;
 import com.android.tools.idea.rendering.AppResourceRepository;
 import com.android.tools.idea.rendering.LocalResourceRepository;
 import com.android.tools.idea.rendering.Locale;
 import com.android.tools.idea.rendering.ResourceHelper;
+import com.android.tools.idea.rendering.multi.CompatibilityRenderTarget;
 import com.android.utils.SparseArray;
 import com.google.common.collect.Maps;
 import com.intellij.openapi.application.Application;
@@ -39,12 +43,13 @@ import org.jetbrains.android.sdk.*;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.Map;
 
-import static com.android.SdkConstants.ANDROID_STYLE_RESOURCE_PREFIX;
-import static com.android.SdkConstants.STYLE_RESOURCE_PREFIX;
+import static com.android.SdkConstants.*;
 
 /** Cache for resolved resources */
 public class ResourceResolverCache {
@@ -130,6 +135,16 @@ public class ResourceResolverCache {
           frameworkResources = myFrameworkResourceMap.get(configurationKey);
           if (frameworkResources == null) {
             frameworkResources = frameworkRes.getConfiguredResources(fullConfiguration);
+
+            // Fix up assets. We're only doing this in limited cases for now; specifically Froyo (since the Gingerbread
+            // assets replaced the look for the same theme; that doesn't happen to the same extend for Holo)
+            if (target instanceof CompatibilityRenderTarget && target.getVersion().getApiLevel() == 8) {
+              IAndroidTarget realTarget = ((CompatibilityRenderTarget)target).getRealTarget();
+              if (realTarget != null) {
+                replaceDrawableBitmaps(frameworkResources, target, realTarget);
+              }
+            }
+
             myFrameworkResourceMap.put(configurationKey, frameworkResources);
           }
         }
@@ -173,9 +188,10 @@ public class ResourceResolverCache {
     int apiLevel = target.getVersion().getApiLevel();
     FrameworkResources resources = myFrameworkResources.get(apiLevel);
 
-    boolean reset = false;
     boolean needLocales = configuration.getLanguageQualifier() != null &&
                           !configuration.getLanguageQualifier().hasFakeValue() || myManager.getLocale() != Locale.ANY;
+
+    boolean reset = false;
     if (resources instanceof FrameworkResourceLoader.IdeFrameworkResources) {
       if (needLocales && ((FrameworkResourceLoader.IdeFrameworkResources)resources).getSkippedLocales()) {
         reset = true;
@@ -225,5 +241,90 @@ public class ResourceResolverCache {
     }
 
     return null;
+  }
+
+  /**
+   * Replaces drawable bitmaps with those from the real older target. This helps the simulated platform look more genuine,
+   * since a lot of the look comes from the nine patch assets. For example, when used to simulate Froyo, the checkboxes
+   * will look better than if we use the current classic theme assets, which look like gingerbread.
+   */
+  private static void replaceDrawableBitmaps(@NotNull Map<ResourceType, Map<String, ResourceValue>> frameworkResources,
+                                             @NotNull IAndroidTarget from,
+                                             @NotNull IAndroidTarget realTarget) {
+    // This is a bit hacky; we should be operating at the resource repository level rather than
+    // for configured resources. However, we may not need this for very long.
+    Map<String, ResourceValue> map = frameworkResources.get(ResourceType.DRAWABLE);
+    String oldPrefix = from.getPath(IAndroidTarget.RESOURCES);
+    String newPrefix = realTarget.getPath(IAndroidTarget.RESOURCES);
+
+    if (map == null || map.isEmpty() || oldPrefix == null || newPrefix == null || oldPrefix.equals(newPrefix)) {
+      return;
+    }
+
+    Collection<ResourceValue> values = map.values();
+    Map<String,String> densityDirMap = Maps.newHashMap();
+
+    // Leave XML drawable resources alone since they can reference nonexistent colors and other resources
+    // not available in the real rendering platform
+    final boolean ONLY_REPLACE_BITMAPS = true;
+    Density[] densities = Density.values();
+    for (ResourceValue value : values) {
+      String v = value.getValue();
+      //noinspection ConstantConditions,PointlessBooleanExpression
+      if (v != null && (!ONLY_REPLACE_BITMAPS || v.endsWith(DOT_PNG))) {
+        if (v.startsWith(oldPrefix)) {
+          String relative = v.substring(oldPrefix.length());
+          if (v.endsWith(DOT_PNG)) {
+            int index = relative.indexOf(File.separatorChar);
+            if (index == -1) {
+              index = relative.indexOf('/');
+            }
+            if (index == -1) {
+              continue;
+            }
+            String parent = relative.substring(0, index);
+            String replace = densityDirMap.get(parent);
+            if (replace == null) {
+              FolderConfiguration configuration = FolderConfiguration.getConfigForFolder(parent);
+              if (configuration != null) {
+                DensityQualifier densityQualifier = configuration.getDensityQualifier();
+                if (densityQualifier != null) {
+                  Density density = densityQualifier.getValue();
+                  if (!new File(newPrefix, parent).exists()) {
+                    String oldQualifier = SdkConstants.RES_QUALIFIER_SEP + density.getResourceValue();
+                    String matched = null;
+                    for (Density d : densities) {
+                      if (d.ordinal() <= density.ordinal()) {
+                        // No reason to check higher
+                        continue;
+                      }
+                      String newQualifier = SdkConstants.RES_QUALIFIER_SEP + d.getResourceValue();
+                      String newName = parent.replace(oldQualifier, newQualifier);
+                      File dir = new File(newPrefix, newName);
+                      if (dir.exists()) {
+                        matched = newName;
+                        break;
+                      }
+                    }
+                    if (matched == null) {
+                      continue;
+                    }
+                    replace = matched;
+                    densityDirMap.put(parent, replace); // This isn't right; there may be some assets only in mdpi!
+                  }
+                }
+              }
+            }
+
+            relative = replace + relative.substring(index);
+          }
+
+          File newFile = new File(newPrefix, relative);
+          if (newFile.exists()) {
+            value.setValue(newFile.getPath());
+          }
+        }
+      }
+    }
   }
 }
