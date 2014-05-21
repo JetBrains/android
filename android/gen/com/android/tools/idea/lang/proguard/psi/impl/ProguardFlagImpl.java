310d64a7fb44fd42a3e28ba2f76f8802d508e0a8
Anoop Menon <anoopm@google.com>
2014-05-21 10:01:17 -0700

    Added initial support for Proguard syntax highlighting.
    This makes use of GrammarKit for generating the parser and JFlex for
    generating the lexer. Both of these need to be generated for the build
    to succeeed. The grammar can be found in the
    com.android.tools.idea.lang.proguard.grammar package.
    
    Change-Id: I876abc35c50150080005d93dd18400585e2cfbf0

---
+++ android/gen/com/android/tools/idea/lang/proguard/psi/impl/ProguardFlagImpl.java
@@ -0,0 +1,37 @@
+// This is a generated file. Not intended for manual editing.
+package com.android.tools.idea.lang.proguard.psi.impl;
+
+import java.util.List;
+import org.jetbrains.annotations.*;
+import com.intellij.lang.ASTNode;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiElementVisitor;
+import com.intellij.psi.util.PsiTreeUtil;
+import static com.android.tools.idea.lang.proguard.psi.ProguardTypes.*;
+import com.intellij.extapi.psi.ASTWrapperPsiElement;
+import com.android.tools.idea.lang.proguard.psi.*;
+
+public class ProguardFlagImpl extends ASTWrapperPsiElement implements ProguardFlag {
+
+  public ProguardFlagImpl(ASTNode node) {
+    super(node);
+  }
+
+  public void accept(@NotNull PsiElementVisitor visitor) {
+    if (visitor instanceof ProguardVisitor) ((ProguardVisitor)visitor).visitFlag(this);
+    else super.accept(visitor);
+  }
+
+  @Override
+  @Nullable
+  public ProguardMultiLineFlag getMultiLineFlag() {
+    return findChildByClass(ProguardMultiLineFlag.class);
+  }
+
+  @Override
+  @Nullable
+  public ProguardSingleLineFlag getSingleLineFlag() {
+    return findChildByClass(ProguardSingleLineFlag.class);
+  }
+
+}
