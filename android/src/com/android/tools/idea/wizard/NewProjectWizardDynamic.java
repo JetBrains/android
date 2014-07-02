f8f27cd8534ba84f587f247e669479be5b9de393
Josiah Gaskin <josiahgaskin@google.com>
2014-07-02 14:22:24 -0700

    Use fixed version for gradle plugin
    This change switches away from the .+ syntax to a fixed version for
    the gradle plugin version which is added to the templates.
    
    Fixes https://code.google.com/p/android/issues/detail?id=72961
    
    Change-Id: Ie432d3ce678ffe6c17cd81e0649aca90e4b1f334

--- android/src/com/android/tools/idea/wizard/NewProjectWizardDynamic.java
+++ android/src/com/android/tools/idea/wizard/NewProjectWizardDynamic.java
@@ -39,6 +39,7 @@ import java.util.List;
 
 import static com.android.SdkConstants.GRADLE_LATEST_VERSION;
 import static com.android.SdkConstants.GRADLE_PLUGIN_LATEST_VERSION;
+import static com.android.SdkConstants.GRADLE_PLUGIN_RECOMMENDED_VERSION;
 import static com.android.tools.idea.templates.TemplateMetadata.*;
 import static com.android.tools.idea.wizard.ConfigureAndroidProjectStep.APPLICATION_NAME_KEY;
 import static com.android.tools.idea.wizard.ConfigureAndroidProjectStep.PROJECT_LOCATION_KEY;
@@ -85,7 +86,7 @@ public class NewProjectWizardDynamic extends DynamicWizard {
     }
     ScopedStateStore state = getState();
     state.put(GRADLE_VERSION_KEY, GRADLE_LATEST_VERSION);
-    state.put(GRADLE_PLUGIN_VERSION_KEY, GRADLE_PLUGIN_LATEST_VERSION);
+    state.put(GRADLE_PLUGIN_VERSION_KEY, GRADLE_PLUGIN_RECOMMENDED_VERSION);
     state.put(USE_PER_MODULE_REPOS_KEY, false);
     state.put(IS_NEW_PROJECT_KEY, true);
     state.put(IS_GRADLE_PROJECT_KEY, true);
