ff6216f1dacfa5e3dde7549aa33bf7992b90b5fa
Maxim.Mossienko <Maxim.Mossienko@jetbrains.com>
2014-07-03 14:25:20 +0200

    upgrade Guava to v17

--- android/testSrc/com/android/tools/idea/templates/TemplateTest.java
+++ android/testSrc/com/android/tools/idea/templates/TemplateTest.java
@@ -483,8 +483,7 @@ public class TemplateTest extends AndroidGradleTestCase {
       return;
     }
     markChecked(templateFile, createWithProject);
-    Stopwatch stopwatch = new Stopwatch();
-    stopwatch.start();
+    Stopwatch stopwatch = Stopwatch.createStarted();
     checkTemplate(templateFile, createWithProject);
     stopwatch.stop();
     System.out.println("Checked " + templateFile.getName() + " successfully in " + stopwatch.toString());
