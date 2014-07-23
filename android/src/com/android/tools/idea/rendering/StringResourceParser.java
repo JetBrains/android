abbb0b1a8a32c1732e7660389f93a3771ca6f516
Rebecca Chen <rechen@google.com>
2014-07-23 00:21:01 -0700

    Shows read-only table view of string resources in new strings editor.
    Attaches an editor to strings.xml to show a read-only table of string
    keys, default values, and translations. Table resizes as needed to fill
    its viewport.
    
    A future CL will change the entry point so the editor does not
    erroneously appear to be associated with a single file.
    
    Change-Id: Ic108459d6f6d32340b560754fa5314f5c065ff69
    Signed-off-by: Rebecca Chen <rechen@google.com>

---
+++ android/src/com/android/tools/idea/rendering/StringResourceParser.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (C) 2014 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.rendering;
+
+import com.android.ide.common.rendering.api.ResourceValue;
+import com.android.ide.common.res2.ResourceItem;
+import com.android.ide.common.resources.configuration.FolderConfiguration;
+import com.android.ide.common.resources.configuration.LanguageQualifier;
+import com.android.resources.ResourceType;
+import com.google.common.collect.*;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.*;
+
+public class StringResourceParser {
+  public static StringResourceData parse(@NotNull LocalResourceRepository repository) {
+    List<String> keys = Lists.newArrayList(repository.getItemsOfType(ResourceType.STRING));
+    Collections.sort(keys);
+
+    // Uses a tree set to sort the locales by language code
+    final Set<Locale> locales = Sets.newTreeSet(Locale.LANGUAGE_CODE_COMPARATOR);
+    Map<String,String> defaultValues = Maps.newHashMapWithExpectedSize(keys.size());
+    Table<String, Locale, String> translations = HashBasedTable.create();
+    for (String key : keys) {
+      List<ResourceItem> items = repository.getResourceItem(ResourceType.STRING, key);
+      if (items == null) {
+        continue;
+      }
+      for (ResourceItem item : items) {
+        FolderConfiguration config = item.getConfiguration();
+        LanguageQualifier languageQualifier = config.getLanguageQualifier();
+        if (languageQualifier == null) {
+          ResourceValue value = item.getResourceValue(false);
+          defaultValues.put(key, value == null ? "" : value.getRawXmlValue());
+        } else {
+          Locale locale = Locale.create(languageQualifier, config.getRegionQualifier());
+          locales.add(locale);
+          ResourceValue value = item.getResourceValue(false);
+          translations.put(key, locale, value == null ? "" : value.getRawXmlValue());
+        }
+      }
+    }
+
+    return new StringResourceData(keys, Lists.newArrayList(locales), defaultValues, translations);
+  }
+}
