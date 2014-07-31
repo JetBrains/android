fdf438dfd95867240edde53520c53fa0dd75521c
Alex Ruiz <alruiz@google.com>
2014-07-30 20:57:19 -0700

    Added FileChooserFixture to drive IDEA's 'File Chooser' dialogs.
    In this CL:
    - We force the IDE to use Swing file chooser dialogs, instead
      of native ones, to be able to use FEST to drive them
    - Created base class FileChooserFixture for all the fixtures that
      deal with IDEA file chooser dialogs
    - We find the "Open Project" dialog and simulate a user opening a
      project through the UI
    - Fixed code that finds IdeFrameImpl for the opened project (it
      only worked with new projects)
    - When IdeTestApplication is started for the first time, we remove
      the contents of the config folder, to start with clean state
    
    Change-Id: I72dd64fa1c443b6e5a97943007e87c95991c3349

---
+++ android/guiTestSrc/com/android/tools/idea/tests/gui/framework/fixture/FileChooserDialogFixture.java
@@ -0,0 +1,125 @@
+/*
+ * Copyright (C) 2014 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.tests.gui.framework.fixture;
+
+import com.intellij.openapi.fileChooser.ex.FileChooserDialogImpl;
+import com.intellij.openapi.fileChooser.ex.FileSystemTreeImpl;
+import com.intellij.openapi.ui.DialogWrapper;
+import com.intellij.openapi.vfs.VirtualFile;
+import org.fest.reflect.reference.TypeRef;
+import org.fest.swing.core.GenericTypeMatcher;
+import org.fest.swing.core.Robot;
+import org.fest.swing.core.matcher.JButtonMatcher;
+import org.fest.swing.driver.JTextComponentDriver;
+import org.fest.swing.edt.GuiActionRunner;
+import org.fest.swing.edt.GuiTask;
+import org.fest.swing.fixture.ComponentFixture;
+import org.fest.swing.timing.Condition;
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
+import java.io.File;
+import java.lang.ref.WeakReference;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import static com.android.tools.idea.tests.gui.framework.GuiTests.SHORT_TIMEOUT;
+import static com.intellij.openapi.util.io.FileUtil.splitPath;
+import static com.intellij.openapi.vfs.VfsUtil.findFileByIoFile;
+import static junit.framework.Assert.assertNotNull;
+import static org.fest.assertions.Assertions.assertThat;
+import static org.fest.reflect.core.Reflection.field;
+import static org.fest.swing.timing.Pause.pause;
+import static org.fest.util.Strings.quote;
+
+public class FileChooserDialogFixture extends ComponentFixture<JDialog> {
+  @NotNull
+  public static FileChooserDialogFixture findOpenProjectDialog(@NotNull Robot robot) {
+    JDialog dialog = robot.finder().find(new GenericTypeMatcher<JDialog>(JDialog.class) {
+      @Override
+      protected boolean isMatching(JDialog dialog) {
+        return dialog.isShowing() && "Open Project".equals(dialog.getTitle());
+      }
+    });
+    return new FileChooserDialogFixture(robot, dialog);
+  }
+
+  private FileChooserDialogFixture(@NotNull Robot robot, @NotNull JDialog target) {
+    super(robot, target);
+  }
+
+  @NotNull
+  public FileChooserDialogFixture select(@NotNull File file) {
+    final JTextField textField = robot.finder().findByType(target, JTextField.class, true);
+    JTextComponentDriver driver = new JTextComponentDriver(robot);
+    driver.deleteText(textField);
+
+    pause(new Condition("'Path' text field is not empty") {
+      @Override
+      public boolean test() {
+        return !textField.getText().isEmpty();
+      }
+    }, SHORT_TIMEOUT);
+
+    List<String> segments = splitPath(file.getPath());
+    int segmentCount = segments.size();
+    for (int i = 0; i < segmentCount; i++) {
+      String segment = segments.get(i);
+      if (segment.isEmpty()) {
+        segments.set(i, File.separator);
+      }
+    }
+
+    WeakReference<DialogWrapper> dialogWrapperRef = field("myDialogWrapper").ofType(new TypeRef<WeakReference<DialogWrapper>>() {})
+                                                                            .in(target).get();
+
+    DialogWrapper dialogWrapper = dialogWrapperRef.get();
+    assertThat(dialogWrapper).isInstanceOf(FileChooserDialogImpl.class);
+    final FileSystemTreeImpl fileSystemTree = field("myFileSystemTree").ofType(FileSystemTreeImpl.class).in(dialogWrapper).get();
+
+    final VirtualFile toSelect = findFileByIoFile(file, true);
+    assertNotNull(toSelect);
+    final AtomicBoolean fileSelected = new AtomicBoolean();
+
+    GuiActionRunner.execute(new GuiTask() {
+      @Override
+      protected void executeInEDT() throws Throwable {
+        fileSystemTree.select(toSelect, new Runnable() {
+          @Override
+          public void run() {
+            fileSelected.set(true);
+          }
+        });
+      }
+    });
+
+    pause(new Condition("File " + quote(file.getPath()) + " is selected") {
+      @Override
+      public boolean test() {
+        return fileSelected.get();
+      }
+    });
+
+    return this;
+  }
+
+  @NotNull
+  public FileChooserDialogFixture clickOK() {
+    JButton button = robot.finder().find(target, JButtonMatcher.withText("OK").andShowing());
+    robot.click(button);
+    return this;
+  }
+}
