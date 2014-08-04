cd7988a2c60d3e5f1a48aba1935f14e03835de26
Alex Ruiz <alruiz@google.com>
2014-08-04 15:01:33 -0700

    Fixed issue with closing projects in UI test suites.
    Change-Id: Ib5a84e67625733f722d28a972dbbafd983272b7d

--- android/guiTestSrc/com/android/tools/idea/tests/gui/framework/GuiTestCase.java
+++ android/guiTestSrc/com/android/tools/idea/tests/gui/framework/GuiTestCase.java
@@ -23,12 +23,18 @@ import com.android.tools.idea.tests.gui.framework.fixture.WelcomeFrameFixture;
 import com.android.tools.idea.tests.gui.framework.fixture.newProjectWizard.NewProjectWizardFixture;
 import com.intellij.openapi.application.Application;
 import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.wm.impl.IdeFrameImpl;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.project.ProjectManager;
+import com.intellij.openapi.wm.WindowManager;
+import com.intellij.openapi.wm.impl.WindowManagerImpl;
+import com.intellij.openapi.wm.impl.welcomeScreen.WelcomeFrame;
 import com.intellij.util.SystemProperties;
 import org.fest.swing.core.BasicRobot;
 import org.fest.swing.core.Robot;
 import org.fest.swing.edt.GuiActionRunner;
+import org.fest.swing.edt.GuiQuery;
 import org.fest.swing.edt.GuiTask;
+import org.fest.swing.timing.Condition;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 import org.junit.After;
@@ -43,11 +49,14 @@ import java.io.IOException;
 import static com.android.tools.idea.templates.AndroidGradleTestCase.createGradleWrapper;
 import static com.android.tools.idea.templates.AndroidGradleTestCase.updateGradleVersions;
 import static com.android.tools.idea.tests.gui.framework.GuiTestRunner.canRunGuiTests;
-import static com.android.tools.idea.tests.gui.framework.GuiTests.GUI_TESTS_RUNNING_IN_SUITE_PROPERTY;
-import static com.android.tools.idea.tests.gui.framework.GuiTests.getTestProjectsRootDirPath;
+import static com.android.tools.idea.tests.gui.framework.GuiTests.*;
+import static com.intellij.ide.impl.ProjectUtil.closeAndDispose;
 import static com.intellij.openapi.util.io.FileUtil.*;
 import static junit.framework.Assert.assertNotNull;
 import static org.fest.assertions.Assertions.assertThat;
+import static org.fest.swing.timing.Pause.pause;
+import static org.fest.util.Strings.quote;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(GuiTestRunner.class)
 public abstract class GuiTestCase {
@@ -65,7 +74,8 @@ public abstract class GuiTestCase {
     Application application = ApplicationManager.getApplication();
     assertNotNull(application); // verify that we are using the IDE's ClassLoader.
 
-    setUpRobot();
+    myRobot = BasicRobot.robotWithCurrentAwtHierarchy();
+    myRobot.settings().delayBetweenEvents(30);
   }
 
   @After
@@ -106,20 +116,50 @@ public abstract class GuiTestCase {
   @SuppressWarnings("UnusedDeclaration")
   // Called by GuiTestRunner via reflection.
   protected void closeAllProjects() {
-    setUpRobot();
-
-    for (Frame frame : Frame.getFrames()) {
-      if (frame instanceof IdeFrameImpl) {
-        IdeFrameFixture ideFrame = new IdeFrameFixture(myRobot, (IdeFrameImpl)frame);
-        ideFrame.close();
+    pause(new Condition("Close all projects") {
+      @Override
+      public boolean test() {
+        final Project[] openProjects = ProjectManager.getInstance().getOpenProjects();
+        GuiActionRunner.execute(new GuiTask() {
+          @Override
+          protected void executeInEDT() throws Throwable {
+            for (Project project : openProjects) {
+              assertTrue("Failed to close project " + quote(project.getName()), closeAndDispose(project));
+            }
+          }
+        });
+        return ProjectManager.getInstance().getOpenProjects().length == 0;
       }
-    }
-  }
+    }, SHORT_TIMEOUT);
 
-  private void setUpRobot() {
-    if (myRobot == null) {
-      myRobot = BasicRobot.robotWithCurrentAwtHierarchy();
-      myRobot.settings().delayBetweenEvents(30);
+    boolean welcomeFrameShown = GuiActionRunner.execute(new GuiQuery<Boolean>() {
+      @Override
+      protected Boolean executeInEDT() throws Throwable {
+
+        Project[] openProjects = ProjectManager.getInstance().getOpenProjects();
+        if (openProjects.length == 0) {
+          WelcomeFrame.showNow();
+
+          WindowManagerImpl windowManager = (WindowManagerImpl)WindowManager.getInstance();
+          windowManager.disposeRootFrame();
+          return true;
+        }
+        return false;
+      }
+    });
+
+    if (welcomeFrameShown) {
+      pause(new Condition("'Welcome' frame to show up") {
+        @Override
+        public boolean test() {
+          for (Frame frame : Frame.getFrames()) {
+            if (frame instanceof WelcomeFrame && frame.isShowing()) {
+              return true;
+            }
+          }
+          return false;
+        }
+      }, SHORT_TIMEOUT);
     }
   }
 
