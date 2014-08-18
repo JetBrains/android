6f0b76a616a66eb8be4c94e0ba27535b38812924
Philip Milne <pmilne@google.com>
2014-08-18 14:20:07 -0700

    Tidy
    Change-Id: I600b546beba294eb3e0803702cce6760d11fe89e

--- android/src/com/android/tools/idea/editors/navigation/NavigationEditor.java
+++ android/src/com/android/tools/idea/editors/navigation/NavigationEditor.java
@@ -75,7 +75,7 @@ public class NavigationEditor implements FileEditor {
   private static final int INITIAL_FILE_BUFFER_SIZE = 1000;
   private static final int SCROLL_UNIT_INCREMENT = 20;
   private static final NavigationModel.Event PROJECT_READ = new Event(Operation.UPDATE, Object.class);
-  public static final com.android.navigation.Dimension UNATTACHED_STRIDE = new com.android.navigation.Dimension(50, 50);
+  private static final com.android.navigation.Dimension UNATTACHED_STRIDE = new com.android.navigation.Dimension(50, 50);
 
   private final UserDataHolderBase myUserDataHolder = new UserDataHolderBase();
   @Nullable
@@ -407,7 +407,7 @@ public class NavigationEditor implements FileEditor {
     Set<State> transitionStates = getTransitionStates();
     Collection<State> unattached = getNonTransitionStates(states, transitionStates);
     visited.addAll(unattached);
-    for (final State state : states) {
+    for (State state : states) {
       if (visited.contains(state)) {
         continue;
       }
@@ -431,7 +431,7 @@ public class NavigationEditor implements FileEditor {
         }
       }.addChildrenFor(state);
     }
-    for (final State root : unattached) {
+    for (State root : unattached) {
         stateToLocation.put(root, new com.android.navigation.Point(location.x, location.y));
         location.x += UNATTACHED_STRIDE.width;
         location.y += UNATTACHED_STRIDE.height;
