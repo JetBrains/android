04672bae108de98dd4a00db70fc8a014ed7b5a4e
Tor Norbye <tnorbye@google.com>
2014-08-20 07:49:48 -0700

    74900: No Lint for ReflectiveOperationException
    The lint API check was not checking classes referenced in catch
    statements.
    
    Change-Id: Ib3d996a750890e8db28b11fef55852c00c8d9ffc

---
+++ android/testData/apiCheck/CatchClause.java
@@ -0,0 +1,41 @@
+package test.pkg;
+
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
+import android.content.OperationApplicationException;
+import android.os.Build;
+
+public class Class {
+    private void method() {
+        try {
+            willThrow();
+        } catch (<error descr="Class requires API level 5 (current min is 1): OperationApplicationException">OperationApplicationException</error> e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void willThrow() throws OperationApplicationException {
+        throw <error descr="Call requires API level 5 (current min is 1): android.content.OperationApplicationException#OperationApplicationException">new OperationApplicationException()</error>;
+    }
+
+    private class MyException extends <error descr="Class requires API level 5 (current min is 1): OperationApplicationException">OperationApplicationException</error> {
+    }
+
+    @SuppressLint("NewApi")
+    private void suppressed() {
+        try {
+            willThrow();
+        } catch (OperationApplicationException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.ECLAIR)
+    private void targetApi() {
+        try {
+            willThrow();
+        } catch (OperationApplicationException e) {
+            e.printStackTrace();
+        }
+    }
+}
