b28d5456e43399364fbdbddb82acfb4f8a204f32
Siva Velusamy <vsiva@google.com>
2014-08-25 14:49:51 -0700

    Translation Editor: Code Cleanup
    This CL refactors the code for the localization editor. At a very high
    level:
     - the StringResourceDataController class is no more, its functionality
       is moved to the ViewPanel
     - the StringResourceData is in charge of validating and reading/writing
       to files
     - adds tests for validation and editing of data
    
    Change-Id: I4d96fac0674df6f9776966a5744fe99893efeedd

--- android/src/com/android/tools/idea/editors/strings/StringResourceViewPanel.java
+++ android/src/com/android/tools/idea/editors/strings/StringResourceViewPanel.java
@@ -18,16 +18,22 @@ package com.android.tools.idea.editors.strings;
 import com.android.ide.common.res2.ResourceItem;
 import com.android.tools.idea.configurations.LocaleMenuAction;
 import com.android.tools.idea.editors.strings.table.*;
+import com.android.tools.idea.rendering.LocalResourceRepository;
 import com.android.tools.idea.rendering.Locale;
 import com.intellij.icons.AllIcons;
+import com.intellij.openapi.Disposable;
 import com.intellij.openapi.actionSystem.*;
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.editor.colors.EditorColors;
 import com.intellij.openapi.editor.colors.EditorColorsManager;
+import com.intellij.openapi.progress.ProgressIndicator;
+import com.intellij.openapi.progress.Task;
 import com.intellij.openapi.ui.TextFieldWithBrowseButton;
 import com.intellij.openapi.ui.popup.JBPopupFactory;
 import com.intellij.ui.*;
 import com.intellij.ui.components.JBLabel;
 import com.intellij.ui.components.JBList;
+import com.intellij.ui.components.JBLoadingPanel;
 import com.intellij.ui.table.JBTable;
 import com.intellij.util.ui.UIUtil;
 import icons.AndroidIcons;
@@ -35,30 +41,45 @@ import org.jetbrains.android.facet.AndroidFacet;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import javax.swing.text.JTextComponent;
 import java.awt.*;
-import java.awt.event.FocusListener;
-import java.awt.event.MouseAdapter;
+import java.awt.event.*;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class StringResourceViewPanel {
   private final AndroidFacet myFacet;
   private JPanel myContainer;
+  private JBLoadingPanel myLoadingPanel;
   private JBTable myTable;
 
   private JTextField myKey;
   private TextFieldWithBrowseButton myDefaultValueWithBrowseBtn;
   private final JTextField myDefaultValue;
   private TextFieldWithBrowseButton myTranslationWithBrowseBtn;
+  private final JTextField myTranslation;
+
   private JPanel myToolbarPanel;
+
   private JPanel myWarningPanel;
   private JBLabel myWarningLabel;
-  private final JTextField myTranslation;
-  private StringResourceDataController myController;
 
-  public StringResourceViewPanel(AndroidFacet facet) {
+
+  private LocalResourceRepository myResourceRepository;
+  private long myModificationCount;
+
+  private StringResourceData myData;
+  private final StringResourceTableModel myTableModel;
+
+  public StringResourceViewPanel(AndroidFacet facet, Disposable parentDisposable) {
     myFacet = facet;
+
+    myLoadingPanel = new JBLoadingPanel(new BorderLayout(), parentDisposable, 200);
+    myLoadingPanel.add(myContainer);
 
     Color color = EditorColorsManager.getInstance().getGlobalScheme().getColor(EditorColors.NOTIFICATION_BACKGROUND);
     if (color == null) {
@@ -77,11 +98,20 @@ public class StringResourceViewPanel {
     myDefaultValue = myDefaultValueWithBrowseBtn.getTextField();
     myTranslation = myTranslationWithBrowseBtn.getTextField();
 
-    myDefaultValueWithBrowseBtn.getButton().setVisible(false);
-    myTranslationWithBrowseBtn.getButton().setVisible(false);
-
     initEditPanel();
     initTable();
+
+    myTableModel = new StringResourceTableModel(this, myFacet, null);
+    myTable.setModel(myTableModel);
+    myLoadingPanel.setLoadingText("Loading string resource data");
+    myLoadingPanel.startLoading();
+    new ParseTask("Loading string resource data").queue();
+  }
+
+  public void reloadData() {
+    myLoadingPanel.setLoadingText("Updating string resource data");
+    myLoadingPanel.startLoading();
+    new ParseTask("Updating string resource data").queue();
   }
 
   private ActionToolbar createToolbar() {
@@ -91,7 +121,7 @@ public class StringResourceViewPanel {
     final AnAction addKeyAction = new AnAction("Add Key", "", AllIcons.ToolbarDecorator.Add) {
       @Override
       public void update(AnActionEvent e) {
-        e.getPresentation().setEnabled(myController != null);
+        e.getPresentation().setEnabled(myData != null);
       }
 
       @Override
@@ -100,7 +130,7 @@ public class StringResourceViewPanel {
         if (dialog.showAndGet()) {
           StringsWriteUtils
             .createItem(myFacet.getModule().getProject(), dialog.getResFolder(), null, dialog.getKey(), dialog.getDefaultValue(), true);
-          ((StringResourceTableModel)myTable.getModel()).getController().updateData();
+          reloadData();
         }
       }
     };
@@ -109,12 +139,12 @@ public class StringResourceViewPanel {
     final AnAction addLocaleAction = new AnAction("Add Locale", "", AndroidIcons.Globe) {
       @Override
       public void update(AnActionEvent e) {
-        e.getPresentation().setEnabled(myController != null);
+        e.getPresentation().setEnabled(myData != null);
       }
 
       @Override
       public void actionPerformed(AnActionEvent e) {
-        List<Locale> currentLocales = myController.getData().getLocales();
+        List<Locale> currentLocales = myData.getLocales();
         List<Locale> missingLocales = LocaleMenuAction.getAllLocales();
         missingLocales.removeAll(currentLocales);
         Collections.sort(missingLocales, Locale.LANGUAGE_NAME_COMPARATOR);
@@ -142,7 +172,7 @@ public class StringResourceViewPanel {
           @Override
           public void run() {
             // pick some value to add to this locale
-            Map<String, ResourceItem> defaultValues = myController.getData().getDefaultValues();
+            Map<String, ResourceItem> defaultValues = myData.getDefaultValues();
             String key = "app_name";
             ResourceItem defaultValue = defaultValues.get(key);
 
@@ -155,7 +185,7 @@ public class StringResourceViewPanel {
             Locale l = (Locale)list.getSelectedValue();
             StringsWriteUtils.createItem(myFacet.getModule().getProject(), myFacet.getPrimaryResourceDir(), l, key,
                                          StringResourceData.resourceToString(defaultValue), true);
-            ((StringResourceTableModel)myTable.getModel()).getController().updateData();
+            reloadData();
           }
         }).createPopup().show(JBPopupFactory.getInstance().guessBestPopupLocation(toolbar.getToolbarDataContext()));
       }
@@ -165,11 +195,49 @@ public class StringResourceViewPanel {
     return toolbar;
   }
 
+  public boolean dataIsCurrent() {
+    return myResourceRepository != null && myModificationCount >= myResourceRepository.getModificationCount();
+  }
+
   private void initEditPanel() {
-    FocusListener editFocusListener = new EditFocusListener(myTable, myKey, myDefaultValue, myTranslation);
+    FocusListener editFocusListener = new FocusAdapter() {
+      @Override
+      public void focusLost(FocusEvent e) {
+        ApplicationManager.getApplication().assertIsDispatchThread();
+        JTextComponent component = (JTextComponent) e.getComponent();
+        onTextFieldUpdate(component);
+      }
+    };
     myKey.addFocusListener(editFocusListener);
     myDefaultValue.addFocusListener(editFocusListener);
     myTranslation.addFocusListener(editFocusListener);
+
+    KeyListener keyListener = new KeyAdapter() {
+      @Override
+      public void keyPressed(KeyEvent e) {
+        ApplicationManager.getApplication().assertIsDispatchThread();
+        if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+          JTextComponent component = (JTextComponent) e.getComponent();
+          onTextFieldUpdate(component);
+        }
+      }
+    };
+    myKey.addKeyListener(keyListener);
+    myDefaultValue.addKeyListener(keyListener);
+    myTranslation.addKeyListener(keyListener);
+  }
+
+  private void onTextFieldUpdate(JTextComponent component) {
+    StringResourceTableModel model = (StringResourceTableModel)myTable.getModel();
+    if (myTable.getSelectedColumnCount() != 1 || myTable.getSelectedRowCount() != 1) {
+      return;
+    }
+
+    int row = myTable.getSelectedRow();
+    int column = myTable.getSelectedColumn();
+
+    String value = component.getText();
+    model.setValueAt(value, row, column);
   }
 
   private void initTable() {
@@ -180,40 +248,105 @@ public class StringResourceViewPanel {
     myTable.getTableHeader().addMouseListener(headerListener);
     myTable.getTableHeader().addMouseMotionListener(headerListener);
 
-    CellSelectionListener selectionListener = new CellSelectionListener(myTable, myKey, myDefaultValue, myTranslation);
-    myTable.getSelectionModel().addListSelectionListener(selectionListener);
-    myTable.getColumnModel().getSelectionModel().addListSelectionListener(selectionListener);
+    myTable.getSelectionModel().addListSelectionListener(new CellSelectionListener());
 
-    myTable.setDefaultEditor(String.class, new MultilineCellEditor());
+    myTable.setDefaultEditor(String.class, new StringsCellEditor());
     myTable.getParent().addComponentListener(new ResizeListener(myTable));
-    new TableSpeedSearch(myTable) {
-      @Override
-      public int getElementCount() {
-        // TableSpeedSearch computes the element count from the underlying model, which is problematic when not all cells are visible
-        return myComponent.getRowCount() * myComponent.getColumnCount();
-      }
-    };
-  }
-
-  public void initDataController(@NotNull StringResourceDataController controller) {
-    myController = controller;
-    myTable.setModel(new StringResourceTableModel(controller));
-    ColumnUtil.setColumns(myTable);
-  }
-
-  public void onDataUpdated() {
-    StringResourceTableModel model = (StringResourceTableModel) myTable.getModel();
-    model.fireTableStructureChanged();
-    ColumnUtil.setColumns(myTable);
+    new TableSpeedSearch(myTable);
   }
 
   @NotNull
   public JPanel getComponent() {
-    return myContainer;
+    return myLoadingPanel;
   }
 
   @NotNull
   public JBTable getPreferredFocusedComponent() {
     return myTable;
+  }
+
+  private void createUIComponents() {
+    myTable = new JBTable();
+  }
+
+  private class ParseTask extends Task.Backgroundable {
+    private AtomicReference<LocalResourceRepository> myResourceRepositoryRef = new AtomicReference<LocalResourceRepository>(null);
+    private AtomicReference<StringResourceData> myResourceDataRef = new AtomicReference<StringResourceData>(null);
+
+    public ParseTask(String description) {
+      super(myFacet.getModule().getProject(), description, false);
+    }
+
+    @Override
+    public void run(@NotNull ProgressIndicator indicator) {
+      indicator.setIndeterminate(true);
+      LocalResourceRepository moduleResources = myFacet.getModuleResources(true);
+      myResourceRepositoryRef.set(moduleResources);
+      myResourceDataRef.set(StringResourceParser.parse(myFacet, moduleResources));
+    }
+
+    @Override
+    public void onSuccess() {
+      myLoadingPanel.stopLoading();
+
+      myData = myResourceDataRef.get();
+      myResourceRepository = myResourceRepositoryRef.get();
+      myModificationCount = myResourceRepository.getModificationCount();
+
+      myTableModel.setData(myData);
+      myTableModel.fireTableStructureChanged();
+      ColumnUtil.setColumns(myTable);
+    }
+
+    @Override
+    public void onCancel() {
+      myLoadingPanel.stopLoading();
+    }
+  }
+
+  private class CellSelectionListener implements ListSelectionListener {
+    @Override
+    public void valueChanged(ListSelectionEvent e) {
+      if (e.getValueIsAdjusting()) {
+        return;
+      }
+
+      String key = "";
+      String defaultValue = "";
+      String translation = "";
+
+      boolean keyEditable = false;
+      boolean defaultValueEditable = false;
+      boolean translationEditable = false;
+
+      StringResourceTableModel model = (StringResourceTableModel) myTable.getModel();
+      if (myTable.getSelectedRowCount() == 1 && myTable.getSelectedColumnCount() == 1) {
+        int row = myTable.getSelectedRow();
+        int column = myTable.getSelectedColumn();
+
+        key = String.valueOf(model.getValue(row, ConstantColumn.KEY.ordinal()));
+        defaultValue = String.valueOf(model.getValue(row, ConstantColumn.DEFAULT_VALUE.ordinal()));
+        keyEditable = true;
+        defaultValueEditable = true;
+
+        if (column >= ConstantColumn.COUNT) {
+          translation = String.valueOf(model.getValue(row, column));
+          translationEditable = true;
+        }
+      }
+
+      setTextAndEditable(myKey, key, keyEditable);
+      setTextAndEditable(myDefaultValue, defaultValue, defaultValueEditable);
+      setTextAndEditable(myTranslation, translation, translationEditable);
+    }
+
+    private void setTextAndEditable(@NotNull JTextComponent component, @NotNull String text, boolean editable) {
+      component.setText(text);
+      component.setCaretPosition(0);
+      component.setEditable(editable);
+      // If a text component is not editable when it gains focus and becomes editable while still focused,
+      // the caret does not appear, so we need to set the caret visibility manually
+      component.getCaret().setVisible(editable && component.hasFocus());
+    }
   }
 }
