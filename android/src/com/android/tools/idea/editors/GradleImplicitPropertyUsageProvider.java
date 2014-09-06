1bc266e015457391e31a3273b6a7c482198b5a8e
Tor Norbye <tnorbye@google.com>
2014-09-05 19:25:23 -0700

    Add an Android-specific implicity property usage provider
    If you run Analyze > Inspect Code... on a default Android project, you
    end up with a dozen warnings for unused properties in .property
    files. Some of these are for properties in local.properties, such as
    sdk.dir and ndk.dir. These aren't really unused; they are read by the
    Gradle plugin (and the IDE) when building the project, but the IDE
    analysis doesn't know that, so they are marked as unused in the editor
    and listed in the inspections output.
    
    Similarly, all the properties in the Gradle wrapper property file
    (gradle/wrapper/gradle-wrapper.properties) such as distributionUrl are
    also all listed. These are also used; not by the gradle plugin this
    time but the gradlew wrapper code.
    
    The IDE has a facility for registering "implicit property usages", so
    we use that mechanism to mark the above properties are implicitly
    used, which removes the various unused warnings.
    
    Change-Id: Id426d010062394739457c28b71703f5486481f97

---
+++ android/src/com/android/tools/idea/editors/GradleImplicitPropertyUsageProvider.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2014 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.editors;
+
+import com.intellij.codeInspection.unused.ImplicitPropertyUsageProvider;
+import com.intellij.lang.properties.psi.Property;
+import com.intellij.openapi.util.Comparing;
+import com.intellij.openapi.util.SystemInfo;
+import com.intellij.psi.PsiFile;
+
+import static com.android.SdkConstants.*;
+
+/**
+ * Provider which defines some properties as implicitly used, such that they don't get
+ * flagged by the inspections as unused.
+ */
+public class GradleImplicitPropertyUsageProvider extends ImplicitPropertyUsageProvider {
+  @Override
+  protected boolean isUsed(Property property) {
+    PsiFile file = property.getContainingFile();
+    if (Comparing.equal(file.getName(), FN_GRADLE_WRAPPER_PROPERTIES, SystemInfo.isFileSystemCaseSensitive)) {
+      // Ignore all properties in the gradle wrapper: read by the gradle wrapper .jar code
+      return true;
+    }
+
+    // The android gradle plugin reads sdk.dir, ndk.dir and android.dir from local.properties
+    if (Comparing.equal(file.getName(), FN_LOCAL_PROPERTIES, SystemInfo.isFileSystemCaseSensitive)) {
+      String name = property.getName();
+      return SDK_DIR_PROPERTY.equals(name) || "ndk.dir".equals(name) || "android.dir".equals(name);
+    }
+
+    return false;
+  }
+}
