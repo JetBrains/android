f9a7ca1b39307f235b16f268f0a6f06cb7a9a3ba
Alex Ruiz <alruiz@google.com>
2014-09-22 17:56:03 -0700

    Now we show dialog to set JDK path when build fails due to wrong JDK.
    In this CL:
    - We show the "Select SDK" dialog (only showing the JDK path field) if
      a build fails when the IDE is using an invalid javaHome (I haven't
      been able to reproduce this issue and I don't know what
      Gradle considers as "invalid javaHome", I searched for it but I only
      got that setitng the JDK in the project settings dialog fixed it.)
    - Adds ability to UI tests to simulate a build failure.
    
    Fixes issue
    https://code.google.com/p/android/issues/detail?id=74687
    
    Change-Id: I5195eeaceb44244960b383a81c88d9229593cfc3

--- android/src/com/android/tools/idea/sdk/SelectSdkDialog.java
+++ android/src/com/android/tools/idea/sdk/SelectSdkDialog.java
@@ -75,6 +75,8 @@ public class SelectSdkDialog extends DialogWrapper {
       }
     }
 
+    mySelectJdkLabel.setLabelFor(myJdkTextFieldWithButton.getTextField());
+
     mySelectSdkDescriptionLabel.setText("Please provide the path to the Android SDK.");
     mySdkHyperlinkLabel.setHyperlinkTarget("http://d.android.com/sdk");
     mySdkHyperlinkLabel.setHyperlinkText("If you do not have the Android SDK, you can obtain it from ", "d.android.com/sdk", ".");
