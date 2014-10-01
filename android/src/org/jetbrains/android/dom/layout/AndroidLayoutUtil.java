28be5af73e8a0e3b8da341e304e5e485ffc6a378
Tor Norbye <tnorbye@google.com>
2014-10-01 15:55:52 -0700

    75702: 'Go to declaration' does not work on tools attributes
    This also fixes code completion in the value attributes, and XML
    resource reference validation, for tools attributes.
    
    Change-Id: I2b9abfefaeea960a3954f13feea30ae2bc2c1f75

--- android/src/org/jetbrains/android/dom/layout/AndroidLayoutUtil.java
+++ android/src/org/jetbrains/android/dom/layout/AndroidLayoutUtil.java
@@ -21,8 +21,7 @@ public class AndroidLayoutUtil {
     result.add("view");
     result.add("merge");
     result.add(FragmentLayoutDomFileDescription.FRAGMENT_TAG_NAME);
-    result.addAll(AndroidDomUtil.removeUnambigiousNames(
-      AndroidDomExtender.getViewClassMap(facet)));
+    result.addAll(AndroidDomUtil.removeUnambiguousNames(AndroidDomExtender.getViewClassMap(facet)));
     result.remove("View");
     return result;
   }
