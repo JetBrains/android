600e51bbf874064a87b2167ae380afcde7a03f13
Tor Norbye <tnorbye@google.com>
2014-10-01 16:01:36 -0700

    Implement test stubs for res values
    Needed by failing test ProjectResourceRepositoryTest#testParents
    
    Change-Id: I4789f669cacad0f837b36cc6a9859659cd15e604

--- android/testSrc/com/android/tools/idea/gradle/stubs/android/BuildTypeStub.java
+++ android/testSrc/com/android/tools/idea/gradle/stubs/android/BuildTypeStub.java
@@ -23,6 +23,7 @@ import org.jetbrains.annotations.Nullable;
 
 import java.io.File;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.Map;
 
 public class BuildTypeStub implements BuildType {
@@ -99,7 +100,7 @@ public class BuildTypeStub implements BuildType {
   @NonNull
   @Override
   public Map<String, ClassField> getResValues() {
-    throw new UnsupportedOperationException();
+    return Collections.emptyMap();
   }
 
   @Override
