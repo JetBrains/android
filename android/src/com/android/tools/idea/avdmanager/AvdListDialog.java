d6022d1d648da7955be9156d068ff91528cdf466
Joe Baker-Malone <jbakermalone@google.com>
2014-10-27 14:10:43 -0700

    Linking selection in AVD manager to combo box in run dialog
    Also removing popup when clicking ... in run dialog.
    https://code.google.com/p/android/issues/detail?id=78230
    https://code.google.com/p/android/issues/detail?id=78232
    
    Change-Id: If2b93ff5812a5bdb622bf9e98e2396b5bf2090b5

--- android/src/com/android/tools/idea/avdmanager/AvdListDialog.java
+++ android/src/com/android/tools/idea/avdmanager/AvdListDialog.java
@@ -99,4 +99,9 @@ public class AvdListDialog extends DialogWrapper implements AvdUiAction.AvdInfoP
     }
     return panel;
   }
+
+  @Nullable
+  public AvdInfo getSelected() {
+    return myAvdDisplayList.getAvdInfo();
+  }
 }
