f536a3b2a68900abba70c1909e5842c94580d31f
Alex Ruiz <alruiz@google.com>
2014-11-18 11:50:34 -0800

    Fixed detection of JDKs on Linux.
    Raluca and I pair-programmed.
    
    This CL fixes:
    - Detection of JDKs of a particular version
    - Detection of JDKs in Linux
    - Enables "detect JDK" button in "First Run" wizard, on Linux
    
    Fixes issue
    https://code.google.com/p/android/issues/detail?id=79694
    
    Change-Id: I8e16f98df33faea6de6be85347cc56fac3d18c3d

--- android/src/com/android/tools/idea/welcome/SetupJdkPath.java
+++ android/src/com/android/tools/idea/welcome/SetupJdkPath.java
@@ -16,13 +16,12 @@
 package com.android.tools.idea.welcome;
 
 import com.android.tools.idea.sdk.DefaultSdks;
-import com.android.tools.idea.sdk.Jdks;
 import com.android.tools.idea.wizard.DynamicWizardPath;
 import com.android.tools.idea.wizard.ScopedStateStore;
 import com.android.tools.idea.wizard.ScopedStateStore.Key;
 import com.android.tools.idea.wizard.ScopedStateStore.Scope;
+import com.intellij.openapi.projectRoots.JavaSdkVersion;
 import com.intellij.openapi.projectRoots.Sdk;
-import com.intellij.pom.java.LanguageLevel;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
@@ -36,8 +35,8 @@ public class SetupJdkPath extends DynamicWizardPath {
 
   @Override
   public boolean isPathVisible() {
-    Sdk defaultJdk = DefaultSdks.getDefaultJdk();
-    return defaultJdk == null || !Jdks.isApplicableJdk(defaultJdk, LanguageLevel.JDK_1_7);
+    Sdk defaultJdk = DefaultSdks.getDefaultJdk(JavaSdkVersion.JDK_1_7);
+    return defaultJdk == null;
   }
 
   @Override
