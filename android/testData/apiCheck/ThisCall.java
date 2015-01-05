2acd6b652a0ab6eeda03204d82725fc537055be3
Tor Norbye <tnorbye@google.com>
2015-01-05 06:57:58 -0800

    93158: Properly handle parent class lookup in the API check for this-expressions
    Change-Id: Ie7937af35fe1dd0366feb832819f57ec85125f97

---
+++ android/testData/apiCheck/ThisCall.java
@@ -0,0 +1,37 @@
+package test.pkg;
+
+import android.app.Activity;
+import android.app.Service;
+
+public class Class extends Activity {
+  public void test(final Activity activity, WebView webView) {
+    if (activity.<error descr="Call requires API level 3 (current min is 1): android.app.Activity#hasWindowFocus">hasWindowFocus</error>()) {
+      return;
+    }
+
+    webView.setWebChromeClient(new WebChromeClient() {
+      @Override
+      public void onProgressChanged(WebView view, int newProgress) {
+        if (<error descr="Call requires API level 3 (current min is 1): android.app.Activity#hasWindowFocus">hasWindowFocus</error>()) {
+          return;
+        }
+
+        if (Class.super.<error descr="Call requires API level 3 (current min is 1): android.app.Activity#hasWindowFocus">hasWindowFocus</error>()) {
+          return;
+        }
+        foo();
+      }
+    });
+  }
+
+  public void foo() {
+  }
+
+  private static abstract class WebView extends Service {
+    public abstract void setWebChromeClient(WebChromeClient client);
+  }
+
+  private static abstract class WebChromeClient {
+    public abstract void onProgressChanged(WebView view, int newProgress);
+  }
+}
