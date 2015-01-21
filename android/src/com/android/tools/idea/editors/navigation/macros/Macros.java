3ffdcda49168ee09d2dcd5f2eeaeb84cf07ac9ec
Philip Milne <pmilne@google.com>
2015-01-20 16:35:01 -0800

    Fit and finish items for Navigation Editor
    - don't penetrate destination screens
      (it's only the top level Activity that matters)
    - exclude screens from the Nav View when they are defined
       by a layout file whose root is a Layout subclass with no children
    - make the source of a transition to show the menu 'top right'
    - improve transition inspector
      (don't show both Activity and Fragment sources)
    - improve transition curve drawing.
      (Simplify tactics: just make curves as short as possible.)
    
    Change-Id: Iea2b0459d8e04a68420afffd4397ab94585548ce

--- android/src/com/android/tools/idea/editors/navigation/macros/Macros.java
+++ android/src/com/android/tools/idea/editors/navigation/macros/Macros.java
@@ -45,7 +45,7 @@ public class Macros {
     "    });" +
     "}";
 
-  private static final String INSTALL_MENU_ITEM_CLICK =
+  private static final String INSTALL_MENU_ITEM_CLICK_LISTENER =
     "void macro(android.view.MenuItem $menuItem, final Statement $f, final boolean $consume) {" +
     "    $menuItem.setOnMenuItemClickListener(new android.view.MenuItem.OnMenuItemClickListener() {" +
     "        @Override" +
@@ -66,19 +66,9 @@ public class Macros {
     "    });" +
     "}";
 
-  private static final String GET_MENU_ITEM =
+  private static final String FIND_MENU_ITEM =
     "void macro(Menu $menu, int $id) {" +
-    "    $menu.findItem($id);" +
-    "}";
-
-  private static final String LAUNCH_ACTIVITY =
-    "void macro(Context context, Class activityClass) {" +
-    "    context.startActivity(new android.content.Intent(context, activityClass));" +
-    "}";
-
-  private static final String LAUNCH_ACTIVITY_WITH_ARG =
-    "<T extends Serializable> void macro(Context context, Class activityClass, String name, T value) {" +
-    "    context.startActivity(new android.content.Intent(context, activityClass).putExtra(name, value));" +
+    "    $menu.findItem(R.id.$id);" +
     "}";
 
   private static final String FIND_VIEW_BY_ID_1 =
@@ -90,11 +80,17 @@ public class Macros {
   public static final String FIND_FRAGMENT_BY_TAG =
     "void macro(void $fragmentManager, int $tag) { $fragmentManager.findFragmentByTag($tag);}";
 
+  private static final String LAUNCH_ACTIVITY_WITH_ARG =
+    "<T extends Serializable> void macro(Context context, Class activityClass, String name, T value) {" +
+    "    context.startActivity(new android.content.Intent(context, activityClass).putExtra(name, value));" +
+    "}";
+
   public final MultiMatch createIntent;
   public final MultiMatch installClickAndCallMacro;
   public final MultiMatch installItemClickAndCallMacro;
-  public final MultiMatch installMenuItemOnGetMenuItemAndLaunchActivityMacro;
+  public final MultiMatch installMenuItemClickAndCallMacro;
   public final MultiMatch defineInnerClassToLaunchActivityMacro;
+  public final MultiMatch findMenuItem;
   public final MultiMatch findViewById1;
   public final MultiMatch findViewById2;
   public final MultiMatch findFragmentByTag;
@@ -121,17 +117,14 @@ public class Macros {
     myProject = project;
 
     createIntent = createMacro(CREATE_INTENT);
+    findMenuItem = createMacro(FIND_MENU_ITEM);
     findViewById1 = createMacro(FIND_VIEW_BY_ID_1);
     findViewById2 = createMacro(FIND_VIEW_BY_ID_2);
     findFragmentByTag = createMacro(FIND_FRAGMENT_BY_TAG);
 
     installClickAndCallMacro = createMacro(INSTALL_CLICK_LISTENER);
-
     installItemClickAndCallMacro = createMacro(INSTALL_ITEM_CLICK_LISTENER);
-
-    installMenuItemOnGetMenuItemAndLaunchActivityMacro = createMacro(INSTALL_MENU_ITEM_CLICK);
-    installMenuItemOnGetMenuItemAndLaunchActivityMacro.addSubMacro("$menuItem", getMethodFromText(GET_MENU_ITEM));
-    installMenuItemOnGetMenuItemAndLaunchActivityMacro.addSubMacro("$f", getMethodFromText(LAUNCH_ACTIVITY));
+    installMenuItemClickAndCallMacro = createMacro(INSTALL_MENU_ITEM_CLICK_LISTENER);
 
     defineInnerClassToLaunchActivityMacro = createMacro(DEFINE_INNER_CLASS);
     defineInnerClassToLaunchActivityMacro.addSubMacro("$f", getMethodFromText(LAUNCH_ACTIVITY_WITH_ARG));
