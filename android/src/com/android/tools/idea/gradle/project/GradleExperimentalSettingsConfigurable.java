44821798c033040d123ff8672dc03e4b37387465
Alex Ruiz <alruiz@google.com>
2015-01-30 05:13:02 -0800

    Added balloon notification to "Test Artifacts" combobox.
    Notifies the user that this new feature is available and explains
    how to enable it.
    
    Change-Id: I7e8af24b59c5d41a10c8c555658a85094e4d24e4
    (cherry picked from commit 1e097ae138648692edd42581e13047b854bd6bab)

--- android/src/com/android/tools/idea/gradle/project/GradleExperimentalSettingsConfigurable.java
+++ android/src/com/android/tools/idea/gradle/project/GradleExperimentalSettingsConfigurable.java
@@ -15,12 +15,9 @@
  */
 package com.android.tools.idea.gradle.project;
 
-import com.android.tools.idea.gradle.variant.view.BuildVariantView;
 import com.intellij.openapi.options.Configurable;
 import com.intellij.openapi.options.ConfigurationException;
 import com.intellij.openapi.options.SearchableConfigurable;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.project.ProjectManager;
 import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -77,20 +74,11 @@ public class GradleExperimentalSettingsConfigurable implements SearchableConfigu
   @Override
   public void apply() throws ConfigurationException {
     mySettings.SELECT_MODULES_ON_PROJECT_IMPORT = isModuleSelectionOnImportEnabled();
-    enableUnitTestingSupport();
+    mySettings.setUnitTestingSupportEnabled(isUnitTestingEnabled());
   }
 
   private boolean isModuleSelectionOnImportEnabled() {
     return myEnableModuleSelectionOnImportCheckBox.isSelected();
-  }
-
-  private void enableUnitTestingSupport() {
-    mySettings.ENABLE_UNIT_TESTING_SUPPORT = isUnitTestingEnabled();
-    for (Project project : ProjectManager.getInstance().getOpenProjects()) {
-      if (!project.isDefault() && project.isOpen() && !project.isDisposed()) {
-        BuildVariantView.getInstance(project).updateTestArtifactComboBox();
-      }
-    }
   }
 
   private boolean isUnitTestingEnabled() {
