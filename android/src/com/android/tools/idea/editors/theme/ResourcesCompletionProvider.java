7e658d6795ee414044d3d99d8455cf7a7c1ebe09
Diego Perez <diegoperez@google.com>
2015-02-25 09:28:59 +0000

    Add attributes auto-completion for references.
    Change-Id: I522f124aff0736b465aba32e2b9ff8df04dbf44d

---
+++ android/src/com/android/tools/idea/editors/theme/ResourcesCompletionProvider.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright (C) 2015 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.editors.theme;
+
+import com.android.SdkConstants;
+import com.android.ide.common.rendering.api.ResourceValue;
+import com.android.ide.common.resources.ResourceResolver;
+import com.android.resources.ResourceType;
+import com.android.tools.idea.editors.theme.attributes.editors.AttributeReferenceRendererEditor;
+import com.google.common.collect.ImmutableList;
+import org.jetbrains.android.dom.attrs.AttributeDefinition;
+import org.jetbrains.android.dom.attrs.AttributeDefinitions;
+import org.jetbrains.android.dom.attrs.AttributeFormat;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.*;
+
+class ResourcesCompletionProvider implements AttributeReferenceRendererEditor.CompletionProvider {
+  private final ArrayList<ResourceValue> myAllResources = new ArrayList<ResourceValue>();
+
+  ResourcesCompletionProvider(@NotNull ResourceResolver resourceResolver) {
+    for (Map<String, ResourceValue> resourceTypeResource : resourceResolver.getFrameworkResources().values()) {
+      myAllResources.addAll(resourceTypeResource.values());
+    }
+    for (Map<String, ResourceValue> resourceTypeResource : resourceResolver.getProjectResources().values()) {
+      myAllResources.addAll(resourceTypeResource.values());
+    }
+  }
+
+  @NotNull
+  @Override
+  public List<String> getCompletions(@NotNull EditedStyleItem value) {
+    ThemeEditorStyle selectedStyle = value.getSourceStyle();
+
+    AttributeDefinitions attributeDefinitions = selectedStyle.getResolver().getAttributeDefinitions();
+    if (attributeDefinitions == null) {
+      return Collections.emptyList();
+    }
+
+    Set<ResourceType> acceptedTypes = EnumSet.noneOf(ResourceType.class);
+    AttributeDefinition attrDefinition = attributeDefinitions.getAttrDefByName(value.getName());
+    if (ThemeEditorUtils.acceptsFormat(attrDefinition, AttributeFormat.Color)) {
+      acceptedTypes.add(ResourceType.COLOR);
+    }
+    if (ThemeEditorUtils.acceptsFormat(attrDefinition, AttributeFormat.Dimension)) {
+      acceptedTypes.add(ResourceType.DIMEN);
+    }
+    if (ThemeEditorUtils.acceptsFormat(attrDefinition, AttributeFormat.String)) {
+      acceptedTypes.add(ResourceType.STRING);
+    }
+    if (ThemeEditorUtils.acceptsFormat(attrDefinition, AttributeFormat.Reference)) {
+      acceptedTypes.addAll(ImmutableList
+                             .of(ResourceType.LAYOUT, ResourceType.COLOR, ResourceType.DRAWABLE, ResourceType.MIPMAP, ResourceType.STYLE,
+                                 ResourceType.ATTR, ResourceType.STRING, ResourceType.DIMEN, ResourceType.TRANSITION));
+    }
+
+    ArrayList<String> resourceNamesList = new ArrayList<String>(myAllResources.size());
+    for (ResourceValue resource : myAllResources) {
+      if (!acceptedTypes.contains(resource.getResourceType())) {
+        continue;
+      }
+
+      final String name = String.format("%1$s%2$s%3$s/%4$s", ResourceType.ATTR == resource.getResourceType()
+                                                             ? SdkConstants.PREFIX_THEME_REF
+                                                             : SdkConstants.PREFIX_RESOURCE_REF,
+                                        resource.isFramework() ? SdkConstants.ANDROID_NS_NAME_PREFIX : "",
+                                        resource.getResourceType().getName(), resource.getName());
+
+      resourceNamesList.add(name);
+    }
+
+    return resourceNamesList;
+  }
+}
