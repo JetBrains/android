bc05b9c3708547611bc72c9aa20a800fba32a47e
Alex Ruiz <alruiz@google.com>
2015-03-12 22:14:00 -0700

    Fixed UI testing infrastructure.
    It got broken after the syncing to the latest IDEA EAP.
    
    Change-Id: I87d3c23e87d26d135f0d7e57f10537c527e0b02c

--- android/guiTestSrc/com/android/tools/idea/tests/gui/framework/fixture/RunConfigurationComboBoxFixture.java
+++ android/guiTestSrc/com/android/tools/idea/tests/gui/framework/fixture/RunConfigurationComboBoxFixture.java
@@ -15,11 +15,10 @@
  */
 package com.android.tools.idea.tests.gui.framework.fixture;
 
-import com.intellij.execution.actions.RunConfigurationsComboBoxAction;
-import com.intellij.openapi.util.Key;
-import org.fest.reflect.reference.TypeRef;
+import com.intellij.openapi.actionSystem.impl.ActionToolbarImpl;
+import org.fest.swing.core.ComponentFinder;
+import org.fest.swing.core.GenericTypeMatcher;
 import org.fest.swing.core.Robot;
-import org.fest.swing.edt.GuiActionRunner;
 import org.fest.swing.edt.GuiQuery;
 import org.fest.swing.fixture.ComponentFixture;
 import org.jetbrains.annotations.NotNull;
@@ -27,21 +26,26 @@ import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
 
-import static org.fest.assertions.Assertions.assertThat;
-import static org.fest.reflect.core.Reflection.staticField;
+import static com.intellij.openapi.actionSystem.ActionPlaces.MAIN_TOOLBAR;
+import static org.fest.reflect.core.Reflection.field;
+import static org.fest.swing.edt.GuiActionRunner.execute;
 
 public class RunConfigurationComboBoxFixture extends ComponentFixture<JButton> {
   @NotNull
   static RunConfigurationComboBoxFixture find(@NotNull final IdeFrameFixture parent) {
-    final Key<?> key = staticField("BUTTON_KEY").ofType(new TypeRef<Key<?>>() {})
-                                                .in(RunConfigurationsComboBoxAction.class)
-                                                .get();
-    JButton button = GuiActionRunner.execute(new GuiQuery<JButton>() {
+    ComponentFinder finder = parent.robot.finder();
+    ActionToolbarImpl toolbar = finder.find(parent.target, new GenericTypeMatcher<ActionToolbarImpl>(ActionToolbarImpl.class) {
       @Override
-      protected JButton executeInEDT() throws Throwable {
-        Object runConfigurationComboBox = parent.target.getComponent().getRootPane().getClientProperty(key);
-        assertThat(runConfigurationComboBox).isInstanceOf(JButton.class);
-        return (JButton)runConfigurationComboBox;
+      protected boolean isMatching(ActionToolbarImpl toolbar) {
+        String place = field("myPlace").ofType(String.class).in(toolbar).get();
+        return MAIN_TOOLBAR.equals(place);
+      }
+    });
+
+    JButton button = finder.find(toolbar, new GenericTypeMatcher<JButton>(JButton.class) {
+      @Override
+      protected boolean isMatching(JButton button) {
+        return button.getClass().getSimpleName().equals("ComboBoxButton");
       }
     });
     return new RunConfigurationComboBoxFixture(parent.robot, button);
@@ -53,7 +57,7 @@ public class RunConfigurationComboBoxFixture extends ComponentFixture<JButton> {
 
   @Nullable
   public String getText() {
-    return GuiActionRunner.execute(new GuiQuery<String>() {
+    return execute(new GuiQuery<String>() {
       @Override
       protected String executeInEDT() throws Throwable {
         return target.getText();
