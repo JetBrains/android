e00dc7652db153c0513fdfa95b8179972b0de3af
David Herman <davidherman@google.com>
2015-04-12 07:34:37 -0700

    Developer Services and Project Structure
    This CL provides all the core classes and structure to support
    developer services, a concept of enabling APIs and associated utility
    code within a projet. The UI for developer services is shown inside
    the Project Structure dialog, and the services themselves are defined
    by a service.xml file and provided by plugins.
    
    Change-Id: Iee8136e958283c9714db7412bbca2654869364a3

---
+++ android/src/com/android/tools/idea/structure/developerServices/DeveloperServiceInitializer.java
@@ -0,0 +1,148 @@
+/*
+ * Copyright (C) 2015 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.structure.developerServices;
+
+import com.google.common.io.Files;
+import com.google.common.io.Resources;
+import com.intellij.openapi.command.WriteCommandAction;
+import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.module.Module;
+import com.intellij.openapi.util.io.FileUtil;
+import org.jetbrains.android.facet.AndroidFacet;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParserFactory;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * A class used by plugins to expose their service resources to Android Studio.
+ * <p/>
+ * Each subclass of this class should be associated with a single service, providing access to the
+ * content files (service.xml, recipe.xml, code files, etc.) that make up their service. This
+ * allows this base class to instantiate an associated {@link DeveloperService}
+ */
+public abstract class DeveloperServiceInitializer {
+  /**
+   * Reserved filename for the xml file which defines a service.
+   */
+  private static final String SERVICE_XML = "service.xml";
+
+  /**
+   * Plugins return resources via InputStreams. We create a temporary directory and create Files
+   * for each of the InputStreams, which is important because the template parsing APIs we work
+   * with require them.
+   */
+  @NotNull private final File myRootPath;
+
+  public DeveloperServiceInitializer() {
+    try {
+      // TODO: Here, we copy all resources out from the initializer and create local File copies
+      // of them, because our template code (see Template.java, RecipeXmlParser.java) requires
+      // working with them. A longer term solution is to make our template code work with
+      // InputStreams. The biggest obstacle is our template code currently supports recursive
+      // directory operations (e.g. copy src/* to dest/), and InputStreams can't point to
+      // directories or allow walking a directory.
+      myRootPath = new File(FileUtil.generateRandomTemporaryPath(), getResourceRoot());
+      myRootPath.deleteOnExit();
+
+      for (String name : getResources()) {
+        assert !name.contains("..") : "Initializer resource can't specify relative path";
+        File file = new File(myRootPath, name);
+
+        Files.createParentDirs(file);
+        assert file.createNewFile();
+        String fullName = new File(getResourceRoot(), name).getPath();
+        Resources.asByteSource(getClass().getResource(fullName)).copyTo(Files.asByteSink(file));
+      }
+    }
+    catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  /**
+   * Returns a new {@link DeveloperService}, which can be used to install the service into the
+   * associated {@link Module}. Returns {@code null} if the initialization fails for any reason.
+   */
+  @Nullable
+  public final DeveloperService createService(@NotNull Module module) {
+    final ServiceContext context = createContext();
+    if (AndroidFacet.getInstance(module) == null) {
+      throw new IllegalArgumentException(
+        String.format("Developer service cannot be associated with non-Android module %s", module.getName()));
+    }
+
+    final ServiceXmlParser serviceParser = new ServiceXmlParser(module, myRootPath, context);
+
+    try {
+      final InputStream serviceXml = new FileInputStream(new File(myRootPath, SERVICE_XML));
+      try {
+        WriteCommandAction.runWriteCommandAction(module.getProject(), new Runnable() {
+          @Override
+          public void run() {
+            try {
+              SAXParserFactory.newInstance().newSAXParser().parse(serviceXml, serviceParser);
+            }
+            catch (ParserConfigurationException e) {
+              throw new RuntimeException(e);
+            }
+            catch (SAXException e) {
+              throw new RuntimeException(e);
+            }
+            catch (IOException e) {
+              throw new RuntimeException(e);
+            }
+          }
+        });
+      }
+      finally {
+        serviceXml.close();
+      }
+    }
+    catch (Exception e) {
+      Logger.getInstance(getClass())
+        .warn(String.format("Skipping over invalid service in module %1$s: %2$s", module.getName(), e.getMessage()));
+      return null;
+    }
+
+    return new DeveloperService(serviceParser);
+  }
+
+  /**
+   * Returns the root path that all resource paths returned by {@link #getResources()} live under.
+   */
+  protected abstract String getResourceRoot();
+
+  /**
+   * Returns the path to all resources required by this service, so that the external code loading
+   * this plugin can find them. One of the resources MUST be named "service.xml".
+   */
+  // TODO: Revisit this and eliminate if possible. It exists so we can fetch all resources
+  // under getResourceRoot() as input streams and convert them to files.
+  protected abstract String[] getResources();
+
+  /**
+   * Create a context (a package of methods and variables, essentially) for the service UI to be
+   * able to bind to.
+   */
+  protected abstract ServiceContext createContext();
+}
---
+++ android/src/com/android/tools/idea/templates/FreemarkerUtils.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright (C) 2015 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.templates;
+
+import freemarker.template.Configuration;
+import freemarker.template.TemplateException;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
+/**
+ * Various utility methods to help classes interact with and initialize freemarker with Android
+ * specific extensions.
+ */
+public final class FreemarkerUtils {
+
+  /**
+   * Create a parameter map that represents a data model which should be passed into
+   * {@link freemarker.template.Template#process(Object, Writer)}. This model will be initialized
+   * with various utility methods and data useful for instantiating Android projects and modules.
+   *
+   * @param args Additional variables that should be added to this data model, if any.
+   */
+  @NotNull
+  public static Map<String, Object> createParameterMap(@NotNull Map<String, Object> args) {
+    // Create the data model.
+    final Map<String, Object> paramMap = new HashMap<String, Object>();
+
+    // Builtin conversion methods
+    paramMap.put("slashedPackageName", new FmSlashedPackageNameMethod());
+    paramMap.put("camelCaseToUnderscore", new FmCamelCaseToUnderscoreMethod());
+    paramMap.put("underscoreToCamelCase", new FmUnderscoreToCamelCaseMethod());
+    paramMap.put("activityToLayout", new FmActivityToLayoutMethod());
+    paramMap.put("layoutToActivity", new FmLayoutToActivityMethod());
+    paramMap.put("classToResource", new FmClassNameToResourceMethod());
+    paramMap.put("escapeXmlAttribute", new FmEscapeXmlAttributeMethod());
+    paramMap.put("escapeXmlText", new FmEscapeXmlStringMethod());
+    paramMap.put("escapeXmlString", new FmEscapeXmlStringMethod());
+    paramMap.put("escapePropertyValue", new FmEscapePropertyValueMethod());
+    paramMap.put("extractLetters", new FmExtractLettersMethod());
+    paramMap.put("hasDependency", new FmHasDependencyMethod(paramMap));
+    paramMap.put("truncate", new FmTruncateStringMethod());
+
+    // Dependency list
+    paramMap.put(TemplateMetadata.ATTR_DEPENDENCIES_LIST, new LinkedList<String>());
+
+    // Parameters supplied by user
+    paramMap.putAll(args);
+
+    return paramMap;
+  }
+
+  /**
+   * Helper method which processes a target file, running it through the Freemarker engine first,
+   * returning its contents as a string.
+   */
+  @NotNull
+  public static String processFreemarkerTemplate(@NotNull Configuration freemarker,
+                                                 @NotNull Map<String, Object> paramMap,
+                                                 @NotNull File file)
+    throws IOException, TemplateException {
+    freemarker.template.Template template = freemarker.getTemplate(file.getName());
+    StringWriter out = new StringWriter();
+    template.process(paramMap, out);
+    out.flush();
+    final String s = out.toString();
+    return s.replace("\r", "");
+  }
+}
