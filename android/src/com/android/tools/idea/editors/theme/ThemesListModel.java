a2955b81b0cf061934682fe0e3c7641436a9f2f4
Andrew Shulaev <ddrone@google.com>
2015-04-14 16:16:44 +0100

    Show only project and whitelisted framework themes
    Change-Id: I3fb1fd9cb72fcc065627d546d4c8ab5b26edb45e

--- android/src/com/android/tools/idea/editors/theme/ThemesListModel.java
+++ android/src/com/android/tools/idea/editors/theme/ThemesListModel.java
@@ -17,14 +17,33 @@ package com.android.tools.idea.editors.theme;
 
 import com.google.common.base.Objects;
 import com.google.common.collect.ImmutableList;
-import com.google.common.collect.Ordering;
+import com.google.common.collect.ImmutableSet;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
 import java.util.Comparator;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class ThemesListModel extends AbstractListModel implements ComboBoxModel {
+  private static final Set<String> THEME_WHITELIST = ImmutableSet
+    .of("Theme.Material.NoActionBar",
+        "Theme.Material.Light.NoActionBar",
+        "Theme.AppCompat.NoActionBar",
+        "Theme.AppCompat.Light.NoActionBar");
+
+  public static final Comparator<ThemeEditorStyle> STYLE_COMPARATOR = new Comparator<ThemeEditorStyle>() {
+    @Override
+    public int compare(ThemeEditorStyle o1, ThemeEditorStyle o2) {
+      if (o1.isProjectStyle() == o2.isProjectStyle()) {
+        return o1.getName().compareTo(o2.getName());
+      }
+
+      return o1.isProjectStyle() ? -1 : 1;
+    }
+  };
+
   private ImmutableList<ThemeEditorStyle> myThemeList;
   private Object mySelectedObject;
 
@@ -44,21 +63,14 @@ public class ThemesListModel extends AbstractListModel implements ComboBoxModel 
   public void setThemeResolver(@NotNull ThemeResolver themeResolver, @Nullable String defaultThemeName) {
     // We sort the themes, displaying the local project themes at the top sorted alphabetically. The non local themes are sorted
     // alphabetically right below the project themes.
-    myThemeList = Ordering.from(new Comparator<ThemeEditorStyle>() {
-      @Override
-      public int compare(ThemeEditorStyle o1, ThemeEditorStyle o2) {
-        if (o1.isProjectStyle() == o2.isProjectStyle()) {
-          return o1.getName().compareTo(o2.getName());
-        }
-
-        return o1.isProjectStyle() ? -1 : 1;
+    Set<ThemeEditorStyle> temporarySet = new TreeSet<ThemeEditorStyle>(STYLE_COMPARATOR);
+    for (ThemeEditorStyle style : themeResolver.getThemes()) {
+      if (style.isProjectStyle() || THEME_WHITELIST.contains(style.getSimpleName())) {
+        temporarySet.add(style);
       }
-    }).immutableSortedCopy(themeResolver.getProjectThemes());
-
-    if (myThemeList.isEmpty()) {
-      // Fallback in case we do not have project themes.
-      myThemeList = ImmutableList.copyOf(themeResolver.getFrameworkThemes());
     }
+
+    myThemeList = ImmutableList.copyOf(temporarySet);
 
     // Set the default selection to the first element.
     if (defaultThemeName != null && themeResolver.getTheme(defaultThemeName) != null) {
