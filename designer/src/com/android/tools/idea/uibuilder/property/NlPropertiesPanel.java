d39b886f30bc3a7af8d086f3e796e40b78dfcc44
Siva Velusamy <vsiva@google.com>
2015-04-27 22:09:57 -0700

    First cut of the property sheet
    This CL adds an initial implementation of the property sheet. In its
    current form, it simply displays a list of attributes applicable to the
    component currently selected in the design surface.
    
    A few noteworthy points:
     - The list of attributes are obtained using the same mechanism as used
       by the IDE in order to offer code completion.
     - The selection events from the design surface are passed via a
       MergingUpdateQueue, which provides coalescing of events, but does
       introduce a 500ms lag from the component selection to the property
       sheet showing the appropriate attributes.
     - Most of the work happens on the EDT, with just the reading the list
       of applicable attributes happening on a pooled thread holding a read
       lock.
     - In order to populate the model, a new NlProperty instance is created
       for every applicable attribute right away. However, this operation
       is pretty cheap as the NlProperty object is just a simple bean. If
       necessary, this could be moved to a later stage (model.getValueAt),
       but so far that doesn't seem necessary..
    
    Change-Id: I6d91417c04c2d396b0de6c270c4bbf1151c40c77
    (cherry picked from commit be816c2c939cbd9a962eb396a98092ed1c95701b)

---
+++ designer/src/com/android/tools/idea/uibuilder/property/NlPropertiesPanel.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright (C) 2015 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.uibuilder.property;
+
+import com.android.tools.idea.uibuilder.surface.ScreenView;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.ui.components.JBScrollPane;
+import com.intellij.ui.table.JBTable;
+import com.intellij.util.Alarm;
+import com.intellij.util.ui.UIUtil;
+import com.intellij.util.ui.update.MergingUpdateQueue;
+import com.intellij.util.ui.update.Update;
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import javax.swing.table.TableCellRenderer;
+import javax.swing.table.TableColumn;
+import java.awt.*;
+import java.util.Enumeration;
+
+public class NlPropertiesPanel extends JPanel implements ChangeListener {
+  private final JBTable myTable;
+  private final ScreenView myScreenView;
+  private final NlPropertiesModel myModel;
+  private MergingUpdateQueue myUpdateQueue;
+
+  public NlPropertiesPanel(@NotNull ScreenView screenView) {
+    super(new BorderLayout());
+    setOpaque(true);
+    setFocusable(true);
+    setRequestFocusEnabled(true);
+    setBackground(UIUtil.TRANSPARENT_COLOR);
+
+    myScreenView = screenView;
+
+    myTable = new JBTable();
+    myTable.setMaxItemsForSizeCalculation(10);
+    myTable.getTableHeader().setVisible(false);
+    myTable.getEmptyText().setText("No selected component");
+
+    myModel = new NlPropertiesModel();
+    myTable.setModel(myModel);
+
+    TableCellRenderer cellRenderer = new NlPropertyRenderer();
+    Enumeration<TableColumn> columns = myTable.getColumnModel().getColumns();
+    while (columns.hasMoreElements()) {
+      TableColumn column = columns.nextElement();
+      column.setCellRenderer(cellRenderer);
+    }
+
+    add(new JBScrollPane(myTable), BorderLayout.CENTER);
+
+    myScreenView.getSelectionModel().addListener(this);
+  }
+
+  @Override
+  public void stateChanged(ChangeEvent e) {
+    getUpdateQueue().queue(new Update("updateProperties") {
+      @Override
+      public void run() {
+        myModel.update(myScreenView.getSelectionModel().getSelection());
+      }
+
+      @Override
+      public boolean canEat(Update update) {
+        return true;
+      }
+    });
+  }
+
+  @NotNull
+  private MergingUpdateQueue getUpdateQueue() {
+    ApplicationManager.getApplication().assertIsDispatchThread();
+
+    if (myUpdateQueue == null) {
+      myUpdateQueue = new MergingUpdateQueue("android.layout.propertysheet", 500, true, null, myScreenView.getModel(), null,
+                                             Alarm.ThreadToUse.SWING_THREAD);
+    }
+    return myUpdateQueue;
+  }
+}
