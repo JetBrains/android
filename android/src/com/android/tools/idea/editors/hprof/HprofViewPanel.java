1f106c49560cb75b3cd9df200d2857669ff61595
Chester Hsieh <dunno@google.com>
2015-04-29 18:23:35 -0700

    Refactor of instance(s) tables to use IJ debug render.
    This CL refactors and consolidates the Instances table and Instance
    Detail tree table to render using IntelliJ's built-in debug tree
    infrastructure.
    * Implements com.sun.jdi interfaces which IJ's infrastructure uses to
        perform its rendering.
    * Implements additional descriptors (i.e. IJ infrastructure's data
        payload for its specialized tree nodes). This is required to
        override certain descriptor behaviors and to adapt the perflib
        data to that of IJ's infrstructure needs.
    * Display instance member variables and their values in-place inside
        the new instances tree.
    
    TODO: This still needs to be adapted to the new tabular tree that is
    simultaneously being worked on.
    
    Change-Id: I72682a3dd28d59115b68a17a7fff1b1b52729d9a

--- android/src/com/android/tools/idea/editors/hprof/HprofViewPanel.java
+++ android/src/com/android/tools/idea/editors/hprof/HprofViewPanel.java
@@ -15,69 +15,53 @@
  */
 package com.android.tools.idea.editors.hprof;
 
+import com.android.tools.idea.editors.hprof.descriptors.ContainerDescriptorImpl;
 import com.android.tools.idea.editors.hprof.tables.classtable.ClassTable;
 import com.android.tools.idea.editors.hprof.tables.classtable.ClassTableModel;
-import com.android.tools.idea.editors.hprof.tables.instancestable.InstanceDetailModel;
-import com.android.tools.idea.editors.hprof.tables.instancestable.InstanceDetailView;
-import com.android.tools.idea.editors.hprof.tables.instancestable.InstancesTreeTable;
-import com.android.tools.idea.editors.hprof.tables.instancestable.InstancesTreeTableModel;
+import com.android.tools.idea.editors.hprof.tables.instancestable.InstancesDebuggerTree;
 import com.android.tools.perflib.heap.ClassObj;
-import com.android.tools.perflib.heap.Instance;
 import com.android.tools.perflib.heap.Snapshot;
-import com.intellij.execution.ui.layout.impl.JBRunnerTabs;
+import com.intellij.debugger.ui.impl.watch.DebuggerTreeNodeImpl;
 import com.intellij.openapi.Disposable;
 import com.intellij.openapi.actionSystem.ActionManager;
 import com.intellij.openapi.actionSystem.ActionPlaces;
 import com.intellij.openapi.actionSystem.ActionToolbar;
 import com.intellij.openapi.actionSystem.DefaultActionGroup;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.wm.IdeFocusManager;
 import com.intellij.ui.JBColor;
 import com.intellij.ui.JBSplitter;
 import com.intellij.ui.components.JBPanel;
 import com.intellij.ui.components.JBScrollPane;
 import com.intellij.ui.components.panels.Wrapper;
-import com.intellij.ui.tabs.TabInfo;
-import com.intellij.util.ui.UIUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
-import javax.swing.table.TableModel;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 public class HprofViewPanel implements Disposable {
   private static final int DIVIDER_WIDTH = 4;
+  @NotNull private Project myProject;
   @NotNull private JPanel myContainer;
-  @NotNull private JBRunnerTabs myDetailTabs;
+  @NotNull private JPanel myDominatorPanel;
 
   public HprofViewPanel(@NotNull final Project project, @NotNull Snapshot snapshot) {
-    final InstancesTreeTable instancesTreeTable = createInstancesTreeTable();
-    final ClassTable classTable = createClassTable(snapshot, instancesTreeTable);
-    JBSplitter splitter = createNavigationSplitter(classTable, instancesTreeTable);
+    myProject = project;
 
-    myDetailTabs = new JBRunnerTabs(project, ActionManager.getInstance(), IdeFocusManager.findInstance(), this);
-    myDetailTabs.setBorder(new EmptyBorder(0, 2, 0, 0));
-    myDetailTabs.setPaintBorder(0, 0, 0, 0);
-    myDetailTabs.addTabMouseListener(new MouseAdapter() {
-      @Override
-      public void mousePressed(@NotNull MouseEvent e) {
-        if (UIUtil.isCloseClick(e)) {
-          final TabInfo tabInfo = myDetailTabs.findInfo(e);
-          if (tabInfo != null) {
-            myDetailTabs.removeTab(tabInfo);
-          }
-        }
-      }
-    });
+    InstancesDebuggerTree instancesDebuggerTree = createInstancesDebuggerTree();
+    final ClassTable classTable = createClassTable(snapshot, instancesDebuggerTree);
+    JBSplitter splitter = createNavigationSplitter(classTable, instancesDebuggerTree.getComponent());
+
+    myDominatorPanel = new JPanel(new BorderLayout());
+    myDominatorPanel.setBorder(new EmptyBorder(0, 2, 0, 0));
 
     JBPanel treePanel = new JBPanel(new BorderLayout());
     treePanel.setBorder(BorderFactory.createLineBorder(JBColor.border()));
     treePanel.setBackground(JBColor.background());
-    treePanel.add(myDetailTabs, BorderLayout.CENTER);
+    treePanel.add(myDominatorPanel, BorderLayout.CENTER);
 
     Wrapper treePanelWrapper = new Wrapper(treePanel);
     treePanelWrapper.setBorder(new EmptyBorder(0, 1, 0, 0));
@@ -85,12 +69,11 @@ public class HprofViewPanel implements Disposable {
     JBPanel classPanel = new JBPanel(new BorderLayout());
     classPanel.add(splitter, BorderLayout.CENTER);
 
-    DefaultActionGroup group = new DefaultActionGroup(new ComputeDominatorAction(snapshot, project){
+    DefaultActionGroup group = new DefaultActionGroup(new ComputeDominatorAction(snapshot, myProject) {
       @Override
       public void onDominatorsComputed() {
         // TODO this should be done with tables adding listeners to the snapshot, as it's the snapshot that changes.
         classTable.notifyDominatorsComputed();
-        instancesTreeTable.notifyDominatorsComputed();
       }
     });
     ActionToolbar toolbar = ActionManager.getInstance().createActionToolbar(ActionPlaces.UNKNOWN, group, true);
@@ -105,24 +88,8 @@ public class HprofViewPanel implements Disposable {
     myContainer.add(mainSplitter);
   }
 
-  @Nullable
-  public TabInfo findDetailPanel(@NotNull String title) {
-    for (TabInfo tabInfo : myDetailTabs.getTabs()) {
-      if (title.equals(tabInfo.getText())) {
-        return tabInfo;
-      }
-    }
-    return null;
-  }
-
-  public void createDetailPanel(@NotNull String title, @NotNull InstanceDetailView view, @NotNull JComponent sideComponent) {
-    TabInfo info = new TabInfo(new JBScrollPane(view)).setText(title).setSideComponent(sideComponent);
-    myDetailTabs.addTab(info);
-    myDetailTabs.select(info, false);
-  }
-
   @NotNull
-  private static ClassTable createClassTable(@NotNull Snapshot snapshot, final InstancesTreeTable instancesTreeTable) {
+  private static ClassTable createClassTable(@NotNull Snapshot snapshot, @NotNull final InstancesDebuggerTree instancesDebuggerTree) {
     final ClassTable classTable = new ClassTable(new ClassTableModel(snapshot));
     classTable.addMouseListener(new MouseAdapter() {
       @Override
@@ -132,7 +99,9 @@ public class HprofViewPanel implements Disposable {
         if (row >= 0) {
           int modelRow = classTable.getRowSorter().convertRowIndexToModel(row);
           ClassObj classObj = (ClassObj)classTable.getModel().getValueAt(modelRow, 0);
-          instancesTreeTable.setModel(new InstancesTreeTableModel(classObj.getInstances(), false));
+          instancesDebuggerTree.setRoot(DebuggerTreeNodeImpl.createNodeNoUpdate(instancesDebuggerTree.getComponent(),
+                                                                                new ContainerDescriptorImpl(classObj.getInstances())));
+
         }
       }
     });
@@ -140,36 +109,8 @@ public class HprofViewPanel implements Disposable {
   }
 
   @NotNull
-  private InstancesTreeTable createInstancesTreeTable() {
-    //// Use dummy data, since ListTreeTableModelOnColumns serves as both the column spec as well as the model for the table.
-    final InstancesTreeTable instancesTreeTable = new InstancesTreeTable(new InstancesTreeTableModel(null, true));
-
-    instancesTreeTable.addMouseListener(new MouseAdapter() {
-      @Override
-      public void mouseClicked(MouseEvent e) {
-        super.mouseClicked(e);
-
-        if (e.getClickCount() < 2) {
-          return;
-        }
-
-        int row = instancesTreeTable.getSelectedRow();
-        if (row >= 0) {
-          RowSorter<? extends TableModel> sorter = instancesTreeTable.getRowSorter();
-          int modelRow = sorter == null ? row : sorter.convertRowIndexToModel(row);
-          Instance detailsRoot = (Instance)instancesTreeTable.getModel().getValueAt(modelRow, 0);
-
-          String idString = Long.toHexString(detailsRoot.getId());
-          if (findDetailPanel(idString) != null) {
-            return;
-          }
-
-          InstanceDetailView detailTree = new InstanceDetailView(new InstanceDetailModel(detailsRoot));
-          createDetailPanel(idString, detailTree, new JPanel());
-        }
-      }
-    });
-    return instancesTreeTable;
+  private InstancesDebuggerTree createInstancesDebuggerTree() {
+    return new InstancesDebuggerTree(myProject);
   }
 
   @NotNull
---
+++ android/src/com/android/tools/idea/editors/hprof/jdi/IntegerValueImpl.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright (C) 2015 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.editors.hprof.jdi;
+
+import com.android.tools.perflib.heap.Field;
+import com.sun.jdi.IntegerValue;
+import org.jetbrains.annotations.NotNull;
+
+public class IntegerValueImpl extends PrimitiveValueImpl implements IntegerValue {
+
+  public IntegerValueImpl(@NotNull Field field, @NotNull Object value) {
+    super(field, value);
+  }
+
+  @Override
+  public int value() {
+    assert (myValue != null);
+    return (Integer)myValue;
+  }
+
+  @Override
+  public int compareTo(IntegerValue o) {
+    return value() - o.value();
+  }
+
+  @Override
+  public boolean booleanValue() {
+    return value() != 0;
+  }
+
+  @Override
+  public byte byteValue() {
+    return (byte)value();
+  }
+
+  @Override
+  public char charValue() {
+    return (char)value();
+  }
+
+  @Override
+  public short shortValue() {
+    return (short)value();
+  }
+
+  @Override
+  public int intValue() {
+    return value();
+  }
+
+  @Override
+  public long longValue() {
+    return value();
+  }
+
+  @Override
+  public float floatValue() {
+    return (float)value();
+  }
+
+  @Override
+  public double doubleValue() {
+    return (double)value();
+  }
+}
