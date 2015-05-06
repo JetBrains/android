b320990b75e619b851ab7062f05e9e697304fb2d
David Herman <davidherman@google.com>
2015-05-06 08:58:50 -0700

    Renamed package "developerServices" -> "services"
    By convention, package names should avoid capitalization.
    
    Change-Id: I4fb04a8d3e34848085c1dc4a699488baf38c5611

--- android/src/com/android/tools/idea/structure/services/InitializeXmlParser.java
+++ android/src/com/android/tools/idea/structure/services/InitializeXmlParser.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.android.tools.idea.structure.developerServices;
+package com.android.tools.idea.structure.services;
 
 import com.android.ide.common.rendering.api.ResourceValue;
 import com.android.ide.common.res2.ResourceItem;
