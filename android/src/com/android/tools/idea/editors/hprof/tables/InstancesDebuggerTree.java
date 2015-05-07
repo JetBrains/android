de7bc2ae5c3981edd3c3d7b81a4a59a6cf128ec2
Chester Hsieh <dunno@google.com>
2015-05-06 17:40:22 -0700

    Adds instance inspection tree for its references/dominators.
    This CL adds a tree to inspect the reference hierarchy of the selected
    node in the instances tree.
    
    Change-Id: I7cff6db3010e0760d7df70c2f5139307cb65d888

--- android/src/com/android/tools/idea/editors/hprof/tables/InstancesDebuggerTree.java
+++ android/src/com/android/tools/idea/editors/hprof/tables/InstancesDebuggerTree.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.android.tools.idea.editors.hprof.tables.instancestable;
+package com.android.tools.idea.editors.hprof.tables;
 
 import com.android.tools.idea.editors.hprof.descriptors.ContainerDescriptorImpl;
 import com.android.tools.idea.editors.hprof.descriptors.HprofFieldDescriptorImpl;
