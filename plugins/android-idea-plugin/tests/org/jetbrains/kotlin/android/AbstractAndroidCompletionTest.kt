gitter-badger/kotlin

Support several res/ directories

plugins/android-idea-plugin/tests/org/jetbrains/kotlin/android/AbstractAndroidCompletionTest.kt
@@ -45,11 +45,11 @@ public abstract class AbstractAndroidCompletionTest : KotlinAndroidTestCase() {
 
     private fun completionType() = CompletionType.BASIC
 
-    fun doTest(testPath: String?) {
-        myFixture.copyDirectoryToProject(getResDir()!!, "res")
-        val virtualFile = myFixture.copyFileToProject(testPath + getTestName(true) + ".kt", "src/" + getTestName(true) + ".kt");
+    fun doTest(path: String?) {
+        getResourceDirs(path).map { myFixture.copyDirectoryToProject(it.name, it.name) }
+        val virtualFile = myFixture.copyFileToProject(path + getTestName(true) + ".kt", "src/" + getTestName(true) + ".kt");
         myFixture.configureFromExistingVirtualFile(virtualFile)
-        val fileText = FileUtil.loadFile(File(testPath + getTestName(true) + ".kt"), true)
+        val fileText = FileUtil.loadFile(File(path + getTestName(true) + ".kt"), true)
         testCompletion(fileText, TargetPlatform.JVM, {
             count -> myFixture.complete(completionType())
         })

plugins/android-idea-plugin/tests/org/jetbrains/kotlin/android/AbstractAndroidFindUsagesTest.kt
@@ -25,7 +25,7 @@ public abstract class AbstractAndroidFindUsagesTest : KotlinAndroidTestCase() {
 
     public fun doTest(path: String) {
         val f = myFixture!!
-        f.copyDirectoryToProject(getResDir()!!, "res")
+        getResourceDirs(path).map { myFixture.copyDirectoryToProject(it.name, it.name) }
         val virtualFile = f.copyFileToProject(path + getTestName(true) + ".kt", "src/" + getTestName(true) + ".kt");
         f.configureFromExistingVirtualFile(virtualFile)
 

plugins/android-idea-plugin/tests/org/jetbrains/kotlin/android/AbstractAndroidGotoTest.kt
@@ -27,7 +27,7 @@ public abstract class AbstractAndroidGotoTest : KotlinAndroidTestCase() {
 
     public fun doTest(path: String) {
         val f = myFixture!!
-        f.copyDirectoryToProject(getResDir()!!, "res")
+        getResourceDirs(path).map { myFixture.copyDirectoryToProject(it.name, it.name) }
         val virtualFile = f.copyFileToProject(path + getTestName(true) + ".kt", "src/" + getTestName(true) + ".kt");
         f.configureFromExistingVirtualFile(virtualFile)
 

plugins/android-idea-plugin/tests/org/jetbrains/kotlin/android/AbstractAndroidRenameTest.kt
@@ -30,7 +30,7 @@ public abstract class AbstractAndroidRenameTest : KotlinAndroidTestCase() {
 
     public fun doTest(path: String) {
         val f = myFixture!!
-        f.copyDirectoryToProject(getResDir()!!, "res")
+        getResourceDirs(path).map { myFixture.copyDirectoryToProject(it.name, it.name) }
         val virtualFile = f.copyFileToProject(path + getTestName(true) + ".kt", "src/" + getTestName(true) + ".kt");
         f.configureFromExistingVirtualFile(virtualFile)
 

JetBrains/android

plugins/android-idea-plugin/tests/org/jetbrains/kotlin/android/AbstractAndroidCompletionTest.kt
@@ -44,11 +44,11 @@ public abstract class AbstractAndroidCompletionTest : KotlinAndroidTestCase() {
 
     private fun completionType() = CompletionType.BASIC
 
-    fun doTest(testPath: String?) {
-        myFixture.copyDirectoryToProject(getResDir()!!, "res")
-        val virtualFile = myFixture.copyFileToProject(testPath + getTestName(true) + ".kt", "src/" + getTestName(true) + ".kt");
+    fun doTest(path: String?) {
+        getResourceDirs(path).forEach { myFixture.copyDirectoryToProject(it.name, it.name) }
+        val virtualFile = myFixture.copyFileToProject(path + getTestName(true) + ".kt", "src/" + getTestName(true) + ".kt");
         myFixture.configureFromExistingVirtualFile(virtualFile)
-        val fileText = FileUtil.loadFile(File(testPath + getTestName(true) + ".kt"), true)
+        val fileText = FileUtil.loadFile(File(path + getTestName(true) + ".kt"), true)
         testCompletion(fileText, TargetPlatform.JVM, {
             count -> myFixture.complete(completionType())
         })

plugins/android-idea-plugin/tests/org/jetbrains/kotlin/android/AbstractAndroidFindUsagesTest.kt
@@ -25,7 +25,7 @@ public abstract class AbstractAndroidFindUsagesTest : KotlinAndroidTestCase() {
 
     public fun doTest(path: String) {
         val f = myFixture!!
-        f.copyDirectoryToProject(getResDir()!!, "res")
+        getResourceDirs(path).forEach { myFixture.copyDirectoryToProject(it.name, it.name) }
         val virtualFile = f.copyFileToProject(path + getTestName(true) + ".kt", "src/" + getTestName(true) + ".kt");
         f.configureFromExistingVirtualFile(virtualFile)
 

plugins/android-idea-plugin/tests/org/jetbrains/kotlin/android/AbstractAndroidGotoTest.kt
@@ -27,7 +27,7 @@ public abstract class AbstractAndroidGotoTest : KotlinAndroidTestCase() {
 
     public fun doTest(path: String) {
         val f = myFixture!!
-        f.copyDirectoryToProject(getResDir()!!, "res")
+        getResourceDirs(path).forEach { myFixture.copyDirectoryToProject(it.name, it.name) }
         val virtualFile = f.copyFileToProject(path + getTestName(true) + ".kt", "src/" + getTestName(true) + ".kt");
         f.configureFromExistingVirtualFile(virtualFile)
 

plugins/android-idea-plugin/tests/org/jetbrains/kotlin/android/AbstractAndroidRenameTest.kt
@@ -30,7 +30,7 @@ public abstract class AbstractAndroidRenameTest : KotlinAndroidTestCase() {
 
     public fun doTest(path: String) {
         val f = myFixture!!
-        f.copyDirectoryToProject(getResDir()!!, "res")
+        getResourceDirs(path).forEach { myFixture.copyDirectoryToProject(it.name, it.name) }
         val virtualFile = f.copyFileToProject(path + getTestName(true) + ".kt", "src/" + getTestName(true) + ".kt");
         f.configureFromExistingVirtualFile(virtualFile)
 