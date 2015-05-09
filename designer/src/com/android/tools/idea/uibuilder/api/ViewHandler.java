6ae1438495eaab130a5e7cf39c875fa66d6505d0
Tor Norbye <tnorbye@google.com>
2015-05-08 21:37:11 -0700

    Additional view handlers and view handler support
    This CL improves the infrastructure for view handlers, and adds new
    view handlers for more widgets and layouts.
    
    (1) Adds a RelativeLayout drag handler and resize handler, based on
        the one from the old layout editor
    
    (2) Adds a LinearLayout drag handler and resize handler, based on the
        one from ADT
    
    (3) Adds a creation handler where widgets can do custom actions when
        they are created; added handlers for <ImageView> and <ImageButton>
        to prompt on drop for the corresponding image; added handlers for
        <fragment> to ask for a fragment class, added a handler for
        <include> to ask for a corresponding layout, etc. It also now
        assigns unique id's on drop (for views that need it.)
    
    (4) Added a default resize handler which can snap to wrap bounds and
        fill bounds.
    
    (5) Added a model version such that handlers can cache data structures
        and refresh them when they are known to be stale.
    
    (6) Made the graphics handler perform coordinate transformations such
        that the view handlers can operate solely in the Android
        coordinate space without worrying about conversions.
    
    Change-Id: Ib60443ae53ac6615c66f29f3b72aa98b90407f42

--- designer/src/com/android/tools/idea/uibuilder/api/ViewHandler.java
+++ designer/src/com/android/tools/idea/uibuilder/api/ViewHandler.java
@@ -16,13 +16,46 @@
 package com.android.tools.idea.uibuilder.api;
 
 import com.android.annotations.NonNull;
+import com.android.tools.idea.uibuilder.model.FillPolicy;
 import com.android.tools.idea.uibuilder.model.NlComponent;
 import com.android.tools.idea.uibuilder.surface.ScreenView;
 
 import java.awt.*;
+import java.util.EnumSet;
 
 /** A view handler is a tool handler for a given Android view class */
 public class ViewHandler {
+  /**
+   * Called when a view for this handler is being created. This allows for the handler to
+   * customize the newly created object. Note that this method is called not just when a
+   * view is created from a palette drag, but when views are constructed via a drag-move
+   * (where views are created in the destination and then deleted from the source), and
+   * even when views are constructed programmatically from other view handlers. The
+   * {@link InsertType} parameter can be used to distinguish the context for the
+   * insertion. For example, the <code>DialerFilterHandler</code> can insert EditText children
+   * when a DialerFilter is first created, but not during a copy/paste or a move.
+   *
+   * @param editor     the associated editor
+   * @param parent     the parent of the node (which may not yet contain the newly created
+   *                   node in its child list)
+   * @param newChild   the newly created node (which will always be a View that applies to
+   *                   this {@linkplain ViewHandler})
+   * @param insertType whether this node was created as part of a newly created view, or
+   * @return true, or false if the view handler wants to cancel this component
+   * creation. This typically happens if for example the view handler tries
+   * to customize the component by for example asking the user for a specific
+   * resource (via {@link ViewEditor#displayResourceInput(EnumSet, String)}),
+   * and then the user cancels that dialog. At that point we don't want an
+   * unconfigured component lingering around, so the component create handler
+   * cancels the drop instead by returning false.
+   */
+  public boolean onCreate(@NonNull ViewEditor editor,
+                          @NonNull NlComponent parent,
+                          @NonNull NlComponent newChild,
+                          @NonNull InsertType insertType) {
+    return true;
+  }
+
   /**
    * Paints the constraints for this component. If it returns true, it has handled
    * the children as well.
@@ -33,5 +66,9 @@ public class ViewHandler {
    */
   public boolean paintConstraints(@NonNull ScreenView screenView, @NonNull Graphics2D graphics, @NonNull NlComponent component) {
     return false;
+  }
+
+  public FillPolicy getFillPolicy() {
+    return FillPolicy.WIDTH_IN_VERTICAL;
   }
 }
