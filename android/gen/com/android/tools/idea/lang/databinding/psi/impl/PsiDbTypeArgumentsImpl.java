65dc08ca1441db67177ab4b825879be293887608
Deepanshu Gupta <deepanshu@google.com>
2015-05-11 16:03:08 -0700

    Data binding XML improvments.
    - Change package name to databinding
    - Add backtick as quote support
    - Remove unused regex declaration in jflex.
    - Move utility method to get default value from expression to util file.
    - Fix per comments from previous CL.
    - Rename some production rules in bnf to provide better error messages.
    - Prevent some android plugin features from working when databinding
      expression detected: code completion, resource resolution etc.
    
    Change-Id: Ife4ad65e111136ed422768611b866b4b1e7b429e

---
+++ android/gen/com/android/tools/idea/lang/databinding/psi/impl/PsiDbTypeArgumentsImpl.java
@@ -0,0 +1,31 @@
+// This is a generated file. Not intended for manual editing.
+package com.android.tools.idea.lang.databinding.psi.impl;
+
+import java.util.List;
+import org.jetbrains.annotations.*;
+import com.intellij.lang.ASTNode;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiElementVisitor;
+import com.intellij.psi.util.PsiTreeUtil;
+import static com.android.tools.idea.lang.databinding.psi.DbTokenTypes.*;
+import com.intellij.extapi.psi.ASTWrapperPsiElement;
+import com.android.tools.idea.lang.databinding.psi.*;
+
+public class PsiDbTypeArgumentsImpl extends ASTWrapperPsiElement implements PsiDbTypeArguments {
+
+  public PsiDbTypeArgumentsImpl(ASTNode node) {
+    super(node);
+  }
+
+  public void accept(@NotNull PsiElementVisitor visitor) {
+    if (visitor instanceof PsiDbVisitor) ((PsiDbVisitor)visitor).visitTypeArguments(this);
+    else super.accept(visitor);
+  }
+
+  @Override
+  @NotNull
+  public List<PsiDbType> getTypeList() {
+    return PsiTreeUtil.getChildrenOfTypeAsList(this, PsiDbType.class);
+  }
+
+}
