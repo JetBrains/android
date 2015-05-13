bd5d24cf148660c6307f2045d4ba13d6b042f16b
Jens Ole Lauridsen <jlauridsen@google.com>
2015-05-13 15:31:24 -0700

    Nele: Add an ItemTransferable for palette/designer drag & drop.
    This is a preparation for using a ViewHandler instead of passing
    static xml for creation of a component.
    This CL will also center the image on the mouse pointer.
    
    Change-Id: If95d67f7a7ff91747575f732a93dd8dd93a8de1e

---
+++ designer/src/com/android/tools/idea/uibuilder/palette/ItemTransferable.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) 2015 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.uibuilder.palette;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.Transferable;
+import java.awt.datatransfer.UnsupportedFlavorException;
+import java.io.IOException;
+
+public class ItemTransferable implements Transferable {
+  public static final DataFlavor PALETTE_FLAVOR = new DataFlavor(DnDTransferItem.class, "Palette Item");
+
+  private final DnDTransferItem myItem;
+
+  public ItemTransferable(@NotNull DnDTransferItem item) {
+    myItem = item;
+  }
+
+  @Override
+  public DataFlavor[] getTransferDataFlavors() {
+    return new DataFlavor[]{PALETTE_FLAVOR};
+  }
+
+  @Override
+  public boolean isDataFlavorSupported(DataFlavor dataFlavor) {
+    return PALETTE_FLAVOR.equals(dataFlavor);
+  }
+
+  @Override
+  public Object getTransferData(DataFlavor dataFlavor) throws UnsupportedFlavorException {
+    if (PALETTE_FLAVOR.equals(dataFlavor)) {
+      return myItem;
+    }
+    throw new UnsupportedFlavorException(dataFlavor);
+  }
+}
