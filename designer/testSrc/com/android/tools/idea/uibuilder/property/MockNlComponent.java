dc5ad81369abb0f923482f8c91d6b7145bb42464
Siva Velusamy <vsiva@google.com>
2015-05-15 07:45:32 -0700

    Do not access attributes directly from the property sheet.
    The XML tag and/or attribute information could be invalid at a later
    point in time if the user has performed some edit. Rather than directly
    accessing the tag/attribute and checking for its validity, we query
    the component for this information. It is now the component's
    responsibility to check for validity. The component should eventually be
    updated to maintain a copy of the PSI data structure so that it can
    always return the result from a snapshot in time.
    
    Change-Id: Id002993503227412dc32968e87e84c25c5a9352c

---
+++ designer/testSrc/com/android/tools/idea/uibuilder/property/MockNlComponent.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2015 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.uibuilder.property;
+
+import com.android.annotations.NonNull;
+import com.android.tools.idea.uibuilder.model.NlComponent;
+import com.android.tools.idea.uibuilder.model.NlModel;
+import com.intellij.psi.xml.XmlTag;
+
+import static org.mockito.Mockito.mock;
+
+public class MockNlComponent extends NlComponent {
+  private MockNlComponent(@NonNull NlModel model, @NonNull XmlTag tag) {
+    super(model, tag);
+  }
+
+  public static NlComponent create(@NonNull XmlTag tag) {
+    return new MockNlComponent(mock(NlModel.class), tag);
+  }
+}
