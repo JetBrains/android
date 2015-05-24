f63937cbb46424dd657cbe975b75e01937bc6316
Tor Norbye <tnorbye@google.com>
2015-05-23 19:24:33 -0700

    Data binding DOM support
    Makes XML validation and code completion properly recognize and
    handle data binding tags in layouts.
    
    Change-Id: Iaa186999b5d648503e9ff9ca65f2cd837b28c351

--- android/src/org/jetbrains/android/dom/layout/AndroidLayoutUtil.java
+++ android/src/org/jetbrains/android/dom/layout/AndroidLayoutUtil.java
@@ -8,6 +8,8 @@ import org.jetbrains.annotations.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.android.SdkConstants.*;
+
 /**
  * @author Eugene.Kudelevsky
  */
@@ -18,11 +20,12 @@ public class AndroidLayoutUtil {
   @NotNull
   public static List<String> getPossibleRoots(@NotNull AndroidFacet facet) {
     final List<String> result = new ArrayList<String>();
-    result.add("view");
-    result.add("merge");
-    result.add(FragmentLayoutDomFileDescription.FRAGMENT_TAG_NAME);
+    result.add(VIEW_TAG);
+    result.add(VIEW_MERGE);
+    result.add(VIEW_FRAGMENT);
     result.addAll(AndroidDomUtil.removeUnambiguousNames(AndroidDomExtender.getViewClassMap(facet)));
-    result.remove("View");
+    result.remove(VIEW);
+    result.add(TAG_LAYOUT);
     return result;
   }
 }
