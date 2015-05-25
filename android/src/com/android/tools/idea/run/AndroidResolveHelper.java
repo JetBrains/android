9356c763f451d61a8c87791859e00c0895604440
Tor Norbye <tnorbye@google.com>
2015-05-25 14:37:51 -0700

    Add getter metadata and variable+call lookups for type computation
    For fields without annotations, look up the corresponding
    getter method (if any) and use its annotations. This is
    useful since fields tend to be private (and therefore not
    annotated; certainly not in framework code with external
    annotations), whereas the getter (if found) will often
    provide metadata about the field.
    
    For local variables, search through variable declarations
    and initializers (recursively, if necessary, to deal with
    intermediate variables) and use annotations from methods
    (in method calls), field assignments and variable
    declaration types.
    
    Change-Id: I3695002c6e168d680e9d0d03806e14fc71914c08

--- android/src/com/android/tools/idea/run/AndroidResolveHelper.java
+++ android/src/com/android/tools/idea/run/AndroidResolveHelper.java
@@ -26,11 +26,14 @@ import com.intellij.openapi.util.Key;
 import com.intellij.psi.*;
 import com.intellij.psi.impl.JavaConstantExpressionEvaluator;
 import com.intellij.psi.search.GlobalSearchScope;
+import com.intellij.psi.util.PropertyUtil;
+import com.intellij.psi.util.PsiTreeUtil;
 import org.jetbrains.android.inspections.ResourceTypeInspection;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
+import java.util.Collection;
 import java.util.Map;
 
 import static com.android.SdkConstants.TYPE_DEF_FLAG_ATTRIBUTE;
@@ -52,7 +55,126 @@ public class AndroidResolveHelper {
     ApplicationManager.getApplication().assertReadAccessAllowed();
 
     JavaPsiFacade facade = JavaPsiFacade.getInstance(context.getProject());
-    return getAnnotation(facade.getResolveHelper().resolveReferencedVariable(name, context));
+    PsiVariable variable = facade.getResolveHelper().resolveReferencedVariable(name, context);
+    if (variable != null) {
+      return getAnnotationForVariable(variable, 0);
+    }
+
+    return null;
+  }
+
+  @Nullable
+  private static PsiAnnotation getAnnotationForVariable(@NotNull PsiVariable variable, int depth) {
+    // Prevent deep recursion, and cycles if we try to chase assignment values from mutually assigned
+    // variables in code like this:
+    //    int var1 = 0, var2 = 0;
+    //    var1 = var2;
+    //    var2 = var1;
+    if (depth > 10) {
+      return null;
+    }
+
+    // Parameter? Those can carry annotations directly
+    if (variable instanceof PsiParameter) {
+      PsiAnnotation annotation = getAnnotation(variable);
+      if (annotation != null) {
+        return annotation;
+      }
+    }
+
+    // We're stepping through the code, and we can be far from the variable declaration
+    // location; the variable can be assigned in multiple places, so to correctly determine
+    // its current value semantics we'd need to know exactly where the debugger is (the
+    // current program counter), map that back to the AST, and then flow backwards to the most
+    // recent assignment, and then chase the value chain from there.
+    //
+    // This is necessary if we want to correctly pick @Type1 versus @Type2 based on the
+    // program location here:
+    //    void test(int var, @Type1 int type1, @Type2 int type2, @ColorInt int rgba) {
+    //       var = type1;
+    //       ...
+    //       var = type2;
+    //       ...
+    //       var = rgba;
+    //       ...
+    // However, this scenario is unlikely in real code.
+    //
+    // It's much more likely that a value will be assigned at most one typedef bound
+    // value. Therefore, by searching through the known set of assignments and picking the
+    // first match, we're likely to find a typedef which is helpful to the user.
+    PsiExpression initializer = variable.getInitializer();
+    if (initializer != null) {
+      if (initializer instanceof PsiMethodCallExpression) {
+        PsiMethodCallExpression callExpression = (PsiMethodCallExpression)initializer;
+        PsiElement resolved = callExpression.getMethodExpression().resolve();
+        if (resolved instanceof PsiMethod) {
+          PsiAnnotation annotation = getAnnotation((PsiMethod)resolved);
+          if (annotation != null) {
+            return annotation;
+          }
+        }
+      } else if (initializer instanceof PsiReferenceExpression) {
+        PsiReferenceExpression reference = (PsiReferenceExpression) initializer;
+        PsiElement resolved = reference.resolve();
+        if (resolved instanceof PsiField) {
+          PsiAnnotation annotation = getAnnotationForField((PsiField)resolved);
+          if (annotation != null) {
+            return annotation;
+          }
+        } else if (resolved instanceof PsiVariable) {
+          PsiAnnotation annotation = getAnnotationForVariable((PsiVariable)resolved, depth + 1);
+          if (annotation != null) {
+            return annotation;
+          }
+        }
+      }
+    }
+
+    PsiMethod method = PsiTreeUtil.getParentOfType(variable, PsiMethod.class, true);
+    if (method == null) {
+      return null;
+    }
+
+    // Variable not initialized; might be assigned to. Look for assignments.
+    Collection<PsiAssignmentExpression> assignments = PsiTreeUtil.findChildrenOfType(method, PsiAssignmentExpression.class);
+    for (PsiAssignmentExpression assignment : assignments) {
+      PsiExpression lhs = assignment.getLExpression();
+      if (lhs instanceof PsiReferenceExpression) {
+        PsiReferenceExpression reference = (PsiReferenceExpression) lhs;
+        PsiElement resolved = reference.resolve();
+        if (resolved == variable) {
+          // Yes, assigning to our target variable
+          // Look at the rhs to see if we can figure out the type
+          PsiExpression rhs = assignment.getRExpression();
+          if (rhs instanceof PsiMethodCallExpression) {
+            PsiMethodCallExpression callExpression = (PsiMethodCallExpression)rhs;
+            PsiElement r = callExpression.getMethodExpression().resolve();
+            if (r instanceof PsiMethod) {
+              PsiAnnotation annotation = getAnnotation((PsiMethod)r);
+              if (annotation != null) {
+                return annotation;
+              }
+            }
+          } else if (rhs instanceof PsiReferenceExpression) {
+            PsiReferenceExpression ref = (PsiReferenceExpression) rhs;
+            PsiElement r = ref.resolve();
+            if (r instanceof PsiField) {
+              PsiAnnotation annotation = getAnnotationForField((PsiField)r);
+              if (annotation != null) {
+                return annotation;
+              }
+            } else if (r instanceof PsiVariable) {
+              PsiAnnotation annotation = getAnnotationForVariable((PsiVariable)r, depth + 1);
+              if (annotation != null) {
+                return annotation;
+              }
+            }
+          }
+        }
+      }
+    }
+
+    return null;
   }
 
   @Nullable
@@ -64,7 +186,27 @@ public class AndroidResolveHelper {
       return null;
     }
 
-    return getAnnotation(psiClass.findFieldByName(fieldName, true));
+    PsiField field = psiClass.findFieldByName(fieldName, true);
+    if (field != null) {
+      return getAnnotationForField(field);
+    }
+
+    return null;
+  }
+
+  @Nullable
+  private static PsiAnnotation getAnnotationForField(PsiField field) {
+    PsiAnnotation annotation = getAnnotation(field);
+    if (annotation == null) {
+      // Fields are usually not annotated (because they are private, not part of the API).
+      // However, if a field has a getter, there is a good chance that the getter has been
+      // annotated!
+      PsiMethod getter = PropertyUtil.findGetterForField(field);
+      if (getter != null) {
+        return getAnnotation(getter);
+      }
+    }
+    return annotation;
   }
 
   @NotNull
