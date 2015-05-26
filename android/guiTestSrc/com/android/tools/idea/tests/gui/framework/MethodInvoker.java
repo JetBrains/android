8270a6772948889b41bae91ef9402638669ccf89
Alex Ruiz <alruiz@google.com>
2015-05-25 17:18:14 -0700

    [UI Testing] Consolidated UI test configuration in one place.
    This CL:
    - Moves the code that reads the configuration settings in
      @IdeGuiTest and @IdeGuiTestSetup in GuiTestConfiguration.
      This code was repeated in MethodInvoker and GuiTestRunner.
    - Consolidates reflection usage in GuiTestConfiguration.
      Reflection needs to be used to setup and access test methods
      which use the same ClassLoader as the IDE (which is not
      the same as the ClassLoader where tests are initialized.)
    
    Change-Id: I25cc115b76d806ad4f1e3853e20e9a72f61398f6

--- android/guiTestSrc/com/android/tools/idea/tests/gui/framework/MethodInvoker.java
+++ android/guiTestSrc/com/android/tools/idea/tests/gui/framework/MethodInvoker.java
@@ -15,15 +15,12 @@
  */
 package com.android.tools.idea.tests.gui.framework;
 
-import com.intellij.openapi.projectRoots.JavaSdkVersion;
 import org.fest.swing.image.ScreenshotTaker;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 import org.junit.runners.model.FrameworkMethod;
 import org.junit.runners.model.Statement;
 
 import java.io.File;
-import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
 import java.text.SimpleDateFormat;
 import java.util.GregorianCalendar;
@@ -31,14 +28,15 @@ import java.util.GregorianCalendar;
 import static com.android.tools.idea.tests.gui.framework.GuiTestRunner.canRunGuiTests;
 import static com.android.tools.idea.tests.gui.framework.IdeTestApplication.getFailedTestScreenshotDirPath;
 import static org.fest.reflect.core.Reflection.field;
-import static org.fest.reflect.core.Reflection.method;
 
 public class MethodInvoker extends Statement {
+  @NotNull private final GuiTestConfigurator myTestConfigurator;
   @NotNull private final FrameworkMethod myTestMethod;
   @NotNull private final Object myTest;
-  @Nullable private final ScreenshotTaker myScreenshotTaker;
+  @NotNull private final ScreenshotTaker myScreenshotTaker;
 
-  MethodInvoker(@NotNull FrameworkMethod testMethod, @NotNull Object test, @Nullable ScreenshotTaker screenshotTaker) {
+  MethodInvoker(@NotNull FrameworkMethod testMethod, @NotNull Object test, @NotNull ScreenshotTaker screenshotTaker) throws Throwable {
+    myTestConfigurator = GuiTestConfigurator.createNew(testMethod.getMethod(), test);
     myTestMethod = testMethod;
     myTest = test;
     myScreenshotTaker = screenshotTaker;
@@ -46,39 +44,18 @@ public class MethodInvoker extends Statement {
 
   @Override
   public void evaluate() throws Throwable {
+    if (myTestConfigurator.shouldSkipTest()) {
+      // Message already printed in console.
+      return;
+    }
+    myTestConfigurator.executeSetupTasks();
+
+    String name = myTestMethod.getName();
+    System.out.println(String.format("Executing test '%1$s'", myTestMethod.getMethod().getDeclaringClass() + "#" + name));
+
     ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
     Class<?> guiTestCaseType = Class.forName(GuiTestCase.class.getCanonicalName(), true, classLoader);
-    String name = myTestMethod.getName();
 
-    Annotation ideGuiTestAnnotation = null;
-    for (Annotation annotation : myTestMethod.getAnnotations()) {
-      if (annotation.annotationType().getCanonicalName().equals(IdeGuiTest.class.getCanonicalName())) {
-        ideGuiTestAnnotation = annotation;
-        break;
-      }
-    }
-
-    if (ideGuiTestAnnotation != null) {
-      Object minimumJdkVersion = method("runWithMinimumJdkVersion").withReturnType(Object.class)
-                                                                   .in(ideGuiTestAnnotation)
-                                                                   .invoke();
-      assert minimumJdkVersion != null;
-
-      Class<?> guiTestsClass = classLoader.loadClass(GuiTests.class.getCanonicalName());
-      Class<?> javaSdkVersionClass = classLoader.loadClass(JavaSdkVersion.class.getCanonicalName());
-      Boolean hasRequiredJdk = method("hasRequiredJdk").withReturnType(boolean.class)
-                                                       .withParameterTypes(javaSdkVersionClass)
-                                                       .in(guiTestsClass)
-                                                       .invoke(minimumJdkVersion);
-      assert hasRequiredJdk != null;
-      if (!hasRequiredJdk) {
-        String jdkVersion = method("getDescription").withReturnType(String.class).in(minimumJdkVersion).invoke();
-        System.out.println(String.format("Skipping test '%1$s'. It needs JDK %2$s or newer.", name, jdkVersion));
-        return;
-      }
-    }
-
-    System.out.println(String.format("Executing test '%1$s'", myTestMethod.getMethod().getDeclaringClass() + "#" + name));
     if (guiTestCaseType.isInstance(myTest)) {
       if (!canRunGuiTests()) {
         // We don't run tests in headless environment.
@@ -90,29 +67,32 @@ public class MethodInvoker extends Statement {
       myTestMethod.invokeExplosively(myTest);
     }
     catch (Throwable e) {
-      if (myScreenshotTaker != null) {
-        Method method = myTestMethod.getMethod();
-        String testFqn = method.getDeclaringClass().getSimpleName() + "." + method.getName();
-        String extension = ".png";
+      takeScreenshot();
+      throw e;
+    }
+  }
 
+  private void takeScreenshot() {
+    if (myTestConfigurator.shouldTakeScreenshotOnFailure()) {
+      Method method = myTestMethod.getMethod();
+      String testFqn = method.getDeclaringClass().getSimpleName() + "." + method.getName();
+      String extension = ".png";
+
+      try {
         File rootDir = getFailedTestScreenshotDirPath();
 
         File screenshotFilePath = new File(rootDir, testFqn + extension);
         if (screenshotFilePath.isFile()) {
           SimpleDateFormat format = new SimpleDateFormat("MM-dd-yyyy.HH:mm:ss");
           String now = format.format(new GregorianCalendar().getTime());
-          screenshotFilePath = new File(rootDir, testFqn  + "." + now + extension);
+          screenshotFilePath = new File(rootDir, testFqn + "." + now + extension);
         }
-
-        try {
-          myScreenshotTaker.saveDesktopAsPng(screenshotFilePath.getPath());
-          System.out.println("Screenshot of failed test taken and stored at " + screenshotFilePath.getPath());
-        } catch (Throwable ignored) {
-          System.out.println("Failed to take screenshot. Cause: " + ignored.getMessage());
-        }
+        myScreenshotTaker.saveDesktopAsPng(screenshotFilePath.getPath());
+        System.out.println("Screenshot of failed test taken and stored at " + screenshotFilePath.getPath());
       }
-
-      throw e;
+      catch (Throwable ignored) {
+        System.out.println("Failed to take screenshot. Cause: " + ignored.getMessage());
+      }
     }
   }
 }
