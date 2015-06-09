2c9ed6ee778fc405379bd2616a011c3f2f2a07e7
estebandlc <estebandlc@google.com>
2015-06-09 11:03:05 -0700

    Allocation tracker: Zooms out when grouping changes
    When "Group by" changes, zoom was kept if the node being
    zoomed into also exists in the new grouping.
    
    Change-Id: I685b609075b6cc968393a7eb51af05f8d9823368

--- android/src/com/android/tools/idea/editors/allocations/AllocationsView.java
+++ android/src/com/android/tools/idea/editors/allocations/AllocationsView.java
@@ -321,6 +321,7 @@ public class AllocationsView implements SunburstComponent.SliceSelectionListener
     myTreeNode = generateTree();
     myTreeModel.setRoot(myTreeNode);
     myLayout.setData(myTreeNode);
+    myLayout.resetZoom();
     myTreeModel.nodeStructureChanged(myTreeNode);
     myPackageFilter.setVisible(groupBy instanceof GroupByAllocator);
   }
