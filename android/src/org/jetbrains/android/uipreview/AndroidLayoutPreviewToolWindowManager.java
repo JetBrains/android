e84e96c857585012cecf854ba2f81dddc8369f38
Tor Norbye <tnorbye@google.com>
2015-07-16 16:22:35 -0700

    22519147: java.lang.Throwable in com.intellij.openapi.diagnostic.Logger
    Remove obsolete assert-dispatch-thread call from preview window manager's
    render() method; it's no longer needed (the rest of the method enqueues
    a render request in a thread safe way.)
    
    Change-Id: I9f048ffdefddbbf1110e45432d54a14a748876f0
    (cherry picked from commit beb0fa7d36d1f734ebbf6e2c7251c62c21d84733)

--- android/src/org/jetbrains/android/uipreview/AndroidLayoutPreviewToolWindowManager.java
+++ android/src/org/jetbrains/android/uipreview/AndroidLayoutPreviewToolWindowManager.java
@@ -574,8 +574,6 @@ public class AndroidLayoutPreviewToolWindowManager implements ProjectComponent {
 
   protected boolean render(final PsiFile psiFile, final AndroidFacet facet,
                         @SuppressWarnings("unused") boolean forceFullRender) {
-    ApplicationManager.getApplication().assertIsDispatchThread();
-    
     getRenderingQueue().queue(new Update("render") {
       @Override
       public void run() {
