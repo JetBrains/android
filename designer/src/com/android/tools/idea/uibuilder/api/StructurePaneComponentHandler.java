8039903b13954b7fded4fd15c195797b10c33cf9
Jens Ole Lauridsen <jlauridsen@google.com>
2015-07-29 11:49:20 -0700

    Nele: Add title, icon, and xml to ViewHandler.
    This is my suggestion for using Viewhandler for controlling the
    UI in the palette and the structure panel.
    The palette should be able to override settings by creating special
    instances of these handlers.
    
    Change-Id: I32e952d23a21bfd7bc521801a0a009a6fdf4d9e8

---
+++ designer/src/com/android/tools/idea/uibuilder/api/StructurePaneComponentHandler.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.uibuilder.api;
+
+import com.android.annotations.NonNull;
+import com.android.tools.idea.uibuilder.model.NlComponent;
+
+import javax.swing.*;
+
+/**
+ * A handler for a component in the structure pane.
+ */
+public class StructurePaneComponentHandler extends PaletteComponentHandler {
+  /**
+   * Returns the title used to identify this component in the structure pane
+   *
+   * @param component a component to get the title of
+   * @return a title of the component (usually the non qualified tag name)
+   */
+  @NonNull
+  public String getTitle(@NonNull NlComponent component) {
+    return getSimpleTagName(component.getTagName());
+  }
+
+  /**
+   * Returns the attribute values used to identify this component in the structure pane
+   *
+   * @param component a component to get the title attributes from
+   * @return a string representing important attribute values or the empty string if no attributes should be shown
+   */
+  @NonNull
+  public String getTitleAttributes(@NonNull NlComponent component) {
+    return "";
+  }
+
+  /**
+   * Returns the icon used to identify this component in the structure pane.<br>
+   * This default implementation assumes the icon is one of the builtin icons.
+   *
+   * @param component a component to get the icon for
+   * @return an icon to identify the component
+   */
+  @NonNull
+  public Icon getIcon(@NonNull NlComponent component) {
+    return loadBuiltinIcon(component.getTagName());
+  }
+}
