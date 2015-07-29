1bc396f2fcac2e77049996f1e234d004d3ffc78d
Josh Giles <joshgiles@google.com>
2015-07-29 11:57:57 -0400

    Draft a generic AndroidModel interface.
    This interface is meant to replace current usages of IdeaAndroidProject
    in non-Gradle-specific code. In particular, it abstracts away the
    details of product flavors/build types/variants and hides the
    Gradle-specific concept of the "selected variant".
    
    Note that the adoption of this interface will force the consolidation of
    the AndroidFacet#get**SourceProvider methods. Based on current usage I
    see only a couple places where this could be tricky; we are
    investigating those now.
    
    Note also that this interface pulls down some methods from
    AndroidModuleInfo (which is currently used in part as an indirection
    layer between Gradle and non-Gradle Android modules). With a bit of
    work, we might be able to collapse these layers of indirection into one.
    
    Change-Id: I065807e05febb22bf1dc6687a059981a68d2fe55

---
+++ android/src/com/android/tools/idea/model/AndroidModel.java
@@ -0,0 +1,136 @@
+/*
+ * Copyright (C) 2015 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.model;
+
+import com.android.builder.model.AndroidArtifact;
+import com.android.builder.model.BaseArtifact;
+import com.android.builder.model.SourceProvider;
+import com.android.sdklib.AndroidVersion;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * A common interface for Android module models.
+ */
+public interface AndroidModel {
+  /**
+   * @return the currently selected main Android artifact produced by this Android module.
+   * TODO: Remove this method - it exposes Gradle-specific AndroidArtifact.
+   */
+  @Deprecated
+  @NotNull
+  AndroidArtifact getMainArtifact();
+
+  /**
+   * @return the currently selected test artifact produced by this Android module.
+   * TODO: Remove this method - it exposes Gradle-specific BaseArtifact.
+   */
+  @Deprecated
+  @Nullable
+  BaseArtifact getTestArtifact();
+
+  /**
+   * @return the default source provider.
+   * TODO: To be build-system-agnostic, simplify source provider usage.
+   * {@link org.jetbrains.android.facet.AndroidFacet#getMainSourceProvider()}
+   */
+  @Deprecated
+  @NotNull
+  SourceProvider getDefaultSourceProvider();
+
+  /**
+   * @return the currently active (non-test) source providers for this Android module in
+   * overlay order (meaning that later providers override earlier providers when they redefine
+   * resources).
+   * {@link org.jetbrains.android.facet.IdeaSourceProvider#getCurrentSourceProviders}
+   */
+  @Deprecated
+  @NotNull
+  List<SourceProvider> getActiveSourceProviders();
+
+  /**
+   * @return the currently active test source providers for this Android module in overlay order.
+   * {@link org.jetbrains.android.facet.IdeaSourceProvider#getCurrentTestSourceProviders}
+   */
+  @Deprecated
+  @NotNull
+  List<SourceProvider> getTestSourceProviders();
+
+  /**
+   * @return all of the non-test source providers, including those that are not currently active.
+   * {@link org.jetbrains.android.facet.IdeaSourceProvider#getAllSourceProviders(AndroidFacet)}
+   */
+  @Deprecated
+  @NotNull
+  List<SourceProvider> getAllSourceProviders();
+
+  /**
+   * @return the current version code for this Android module, or null if not specified.
+   */
+  @Deprecated
+  @Nullable
+  Integer getVersionCode();
+
+  /**
+   * @return the current application ID.
+   * See {@link com.android.tools.idea.gradle.IdeaAndroidProject#computePackageName()}
+   */
+  @NotNull
+  String getApplicationId();
+
+  /**
+   * @return all the application IDs of artifacts this Android module could produce.
+   */
+  @NotNull
+  Collection<String> getAllApplicationIds();
+
+  /**
+   * @return whether the manifest package is overriden.
+   * TODO: Potentially dedupe with computePackageName.
+   */
+  @Deprecated
+  boolean overridesManifestPackage();
+
+  /**
+   * @return whether the application is debuggable, or null if not specified.
+   * {@link AndroidModuleInfo#isDebuggable()}
+   */
+  Boolean isDebuggable();
+
+  /**
+   * @return the minimum supported SDK version.
+   * {@link AndroidModuleInfo#getMinSdkVersion()}
+   */
+  @Nullable
+  AndroidVersion getMinSdkVersion();
+
+  /**
+   * @return the target SDK version.
+   * {@link AndroidModuleInfo#getTargetSdkVersion()}
+   */
+  @Nullable
+  AndroidVersion getTargetSdkVersion();
+
+  /**
+   * @return for test execution, the fully-qualified class name of the specified instrumentation runner if present.
+   */
+  @Nullable
+  @Deprecated
+  String getTestInstrumentationRunner();
+}
