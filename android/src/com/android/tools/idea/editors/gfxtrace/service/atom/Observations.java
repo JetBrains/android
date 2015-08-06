17c625dd088bd04aaf19a3b88fcc16a6641110a3
Ian Cottrell <iancottrell@google.com>
2015-08-06 15:13:20 +0100

    Simple import optimisation
    Change-Id: I66c2cbb28b393a7f7f837c813415790cbd1f4cf2

--- android/src/com/android/tools/idea/editors/gfxtrace/service/atom/Observations.java
+++ android/src/com/android/tools/idea/editors/gfxtrace/service/atom/Observations.java
@@ -17,14 +17,8 @@
  */
 package com.android.tools.idea.editors.gfxtrace.service.atom;
 
+import com.android.tools.rpclib.binary.*;
 import org.jetbrains.annotations.NotNull;
-
-import com.android.tools.rpclib.binary.BinaryClass;
-import com.android.tools.rpclib.binary.BinaryID;
-import com.android.tools.rpclib.binary.BinaryObject;
-import com.android.tools.rpclib.binary.Decoder;
-import com.android.tools.rpclib.binary.Encoder;
-import com.android.tools.rpclib.binary.Namespace;
 
 import java.io.IOException;
 
