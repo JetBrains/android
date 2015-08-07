740a47319fe432607d032c5a4ac984e4b9ca3301
Alex Ruiz <alruiz@google.com>
2015-08-07 11:40:25 -0700

    "Clean Project" action is now available to Gradle projects only.
    Bug: 23035304
    Change-Id: Ia9343f2cb942df46cc170e92586f3e8bbfb42630

--- android/src/com/android/tools/idea/gradle/actions/CleanProjectAction.java
+++ android/src/com/android/tools/idea/gradle/actions/CleanProjectAction.java
@@ -5,27 +5,22 @@ import com.android.tools.idea.gradle.util.Projects;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.project.Project;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * Invokes the "clean" Gradle task on a Gradle-based Android project.
  */
-public class CleanProjectAction extends AnAction {
+public class CleanProjectAction extends AndroidStudioGradleAction {
   public CleanProjectAction() {
     super("Clean Project");
   }
 
   @Override
-  public void actionPerformed(AnActionEvent e) {
-    Project project = e.getProject();
-    if (project != null) {
-      ProjectBuilder.getInstance(project).clean();
-    }
+  protected void doPerform(@NotNull AnActionEvent e, @NotNull Project project) {
+    ProjectBuilder.getInstance(project).clean();
   }
 
   @Override
-  public void update(AnActionEvent e) {
-    Project project = e.getProject();
-    boolean isGradleProject = project != null && Projects.requiresAndroidModel(project);
-    e.getPresentation().setEnabledAndVisible(isGradleProject);
+  protected void doUpdate(@NotNull AnActionEvent e, @NotNull Project project) {
   }
 }
