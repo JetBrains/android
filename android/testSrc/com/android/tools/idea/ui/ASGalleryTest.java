0419e7c046f6c0ef73ba6e237c2a50ee4c75d0cb
Renaud Paquay <rpaquay@google.com>
2015-08-17 15:38:24 -0700

    Refactor ASGallery.
    ASGallery now extends JBList instead of being of custom component
    JComponent implementation.
    
    The goal is to re-use as much code as possible from swing (JList in
    particular) so that we avoid custom drawing and we inherit proper
    implementation of accessibility.
    
    The new implementation:
    * supports resizing cells as the container is resized,
    * overrides right/left arrow to wrap around lines when reaching the
      start/end of columns,
    * provides a custom CellRenderer to the JList to allow rendering items
      according to selection/focus state,
    * Improves the look of images by by using the SMOOTH options when
      scaling down icons,
    * Supports for HiDPI when resizing images,
    * Supports for incremental search of items,
    * Has a new set of test cases, including accessibility testing.
    
    Change-Id: Iabb9934caac2729cb7aba4456ee3a390fea46c0d

--- android/testSrc/com/android/tools/idea/ui/ASGalleryTest.java
+++ android/testSrc/com/android/tools/idea/ui/ASGalleryTest.java
@@ -15,181 +15,152 @@
  */
 package com.android.tools.idea.ui;
 
-import com.android.tools.idea.ui.ASGallery;
-import com.google.common.base.Functions;
+import com.google.common.base.Function;
 import com.intellij.ui.components.JBList;
-import junit.framework.TestCase;
+import com.intellij.util.ui.UIUtil;
+import org.jetbrains.android.AndroidTestCase;
 
+import javax.accessibility.Accessible;
 import javax.swing.*;
 import javax.swing.border.Border;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.image.BufferedImage;
 
-public final class ASGalleryTest extends TestCase {
+public final class ASGalleryTest extends AndroidTestCase {
   public static final Dimension THUMBNAIL_SIZE = new Dimension(128, 128);
   public static final int COLUMNS = 5;
-  // Makes coordinates computation by gallery more complex
   public static final Border BORDER = BorderFactory.createEmptyBorder(COLUMNS, 10, 20, 40);
+
+  private ModelObject[] objects;
   private ASGallery<ModelObject> gallery;
-
-  private static Dimension adjustByInsetsValue(int width, int height, Insets cellMargin) {
-    int expectedWidth = width + cellMargin.left + cellMargin.right;
-    int expectedHeight = height + cellMargin.top + cellMargin.bottom;
-    return new Dimension(expectedWidth, expectedHeight);
-  }
-
-  private static Dimension adjustByInsetsValue(Dimension dimension, Insets borderInsets) {
-    return adjustByInsetsValue(dimension.width, dimension.height, borderInsets);
-  }
-
-  private static int computeCellWidth(ASGallery<ModelObject> gallery) {
-    return getColumnOffset(gallery, 1) - getColumnOffset(gallery, 0);
-  }
-
-  private static int getColumnOffset(ASGallery<ModelObject> gallery, int column) {
-    Insets borderInsets = BORDER.getBorderInsets(gallery);
-    int clientWidth = gallery.getWidth() - borderInsets.left - borderInsets.right;
-    return gallery.getColumnOffset(column, gallery.getColumnCount(), clientWidth);
-  }
-
-  private static void assertColumnWidthForGallerySize(ASGallery<ModelObject> gallery, int galleryWidth, int expectedCellSize) {
-    Insets borderInsets = BORDER.getBorderInsets(gallery);
-    Dimension cellSize = gallery.computeCellSize();
-    gallery.setSize(adjustByInsetsValue(galleryWidth, cellSize.height, borderInsets));
-    assertEquals(expectedCellSize, computeCellWidth(gallery));
-  }
-
-  private static void assertColumnCountForWidth(ASGallery<ModelObject> gallery, int columns, int width) {
-    gallery.setSize(adjustByInsetsValue(width, gallery.computeCellSize().height, BORDER.getBorderInsets(gallery)));
-    assertEquals(columns, gallery.getColumnCount());
-  }
 
   @Override
   protected void setUp() throws Exception {
     super.setUp();
-    ModelObject[] objects = new ModelObject[COLUMNS];
+
+    objects = new ModelObject[COLUMNS];
     for (int i = 0; i < COLUMNS; i++) {
       objects[i] = new ModelObject(i + 1);
+      if (i > 0) {
+        objects[i].myImage = UIUtil.createImage(500, 500, BufferedImage.TYPE_INT_ARGB);
+      }
+      objects[i].myLabel = "Model " + i;
     }
+
     ASGallery<ModelObject> asGallery =
-      new ASGallery<ModelObject>(JBList.createDefaultListModel(objects), Functions.<Image>constant(null), Functions.toStringFunction(),
-                                 THUMBNAIL_SIZE);
+      new ASGallery<ModelObject>(
+        JBList.createDefaultListModel(objects),
+        new Function<ModelObject, Image>() {
+          @Override
+          public Image apply(ModelObject input) {
+            return input.myImage;
+          }
+        },
+        new Function<ModelObject, String>() {
+          @Override
+          public String apply(ModelObject input) {
+            return input.myLabel;
+          }
+        },
+        THUMBNAIL_SIZE);
     asGallery.setBorder(BORDER);
     gallery = asGallery;
   }
+  public void testSelection() {
+    assertNull(gallery.getSelectedElement());
+    for (int i = 0; i < COLUMNS; i++) {
+      gallery.setSelectedIndex(i);
+      assertEquals(objects[i], gallery.getSelectedElement());
+      assertEquals(objects[i], gallery.getSelectedValue());
+      assertEquals(i, gallery.getSelectedIndex());
+    }
 
-  public void testColumnSize() {
-    Dimension cellSize = gallery.computeCellSize();
-    // 1. 5 elements, 2 columns - span the whole width
-    assertColumnWidthForGallerySize(gallery, cellSize.width * (COLUMNS + 1) / COLUMNS, cellSize.width * (COLUMNS + 1) / (COLUMNS));
-    // 2. 5 elements, 8 columns (less than 2x elements) - span
-    assertColumnWidthForGallerySize(gallery, cellSize.width * (COLUMNS + 3), cellSize.width * (COLUMNS + 3) / COLUMNS);
-    // 2. 5. elements, 12 columns (more than 2x elements) - collapse
-    assertColumnWidthForGallerySize(gallery, cellSize.width * (COLUMNS * 2 + 2), cellSize.width);
+    gallery.setSelectedElement(objects[1]);
+    assertEquals(objects[1], gallery.getSelectedElement());
+    gallery.setSelectedElement(new ModelObject(10));
+    assertEquals(objects[1], gallery.getSelectedElement());
+    assertEquals(objects[1], gallery.getSelectedValue());
+    assertEquals(1, gallery.getSelectedIndex());
+
+    gallery.clearSelection();
+    assertEquals(null, gallery.getSelectedElement());
+    assertEquals(null, gallery.getSelectedValue());
+    assertEquals(-1, gallery.getSelectedIndex());
   }
 
-  public void testCellAtForOneColumnFiveRows() {
-    Dimension cellSize = gallery.computeCellSize();
-    Insets borderInsets = BORDER.getBorderInsets(gallery);
-
-    gallery.setSize(adjustByInsetsValue(cellSize, borderInsets));
-    Dimension preferredSize = gallery.getPreferredSize();
-    gallery.setSize(preferredSize); // Multiple rows
-    // Corner cases. Literally. Note that cell height does not change with gallery height - so far there was no such requirement.
-    assertEquals(0, gallery.getCellAt(new Point(borderInsets.left + 1, borderInsets.top + 1)));
-    assertEquals(4, gallery.getCellAt(new Point(preferredSize.width - borderInsets.right - 1, borderInsets.top + cellSize.height * 5 - 1)));
-    assertEquals(-1, gallery.getCellAt(new Point(preferredSize.width - borderInsets.right - 1, borderInsets.top + cellSize.height * 5)));
-    assertEquals(-1, gallery.getCellAt(new Point(preferredSize.width - borderInsets.right, borderInsets.top + cellSize.height * 5 - 1)));
+  public void testCellRenderer() {
+    for (int i = 0; i < COLUMNS; i++) {
+      ModelObject model = objects[i];
+      Component renderer = gallery.getCellRenderer().getListCellRendererComponent(gallery, model, i, false, false);
+      assertNotNull(renderer);
+      if (model.myImage == null) {
+        assertTrue(renderer instanceof JLabel);
+        assertEquals(((JLabel)renderer).getText(), model.myLabel);
+      } else {
+        assertTrue(renderer instanceof JPanel);
+        assertTrue(((JPanel)renderer).getComponent(0) instanceof JLabel);
+        assertNotNull(((JLabel)((JPanel)renderer).getComponent(0)).getIcon());
+        assertEquals(THUMBNAIL_SIZE.width, ((JLabel)((JPanel)renderer).getComponent(0)).getIcon().getIconHeight());
+        assertEquals(THUMBNAIL_SIZE.height, ((JLabel)((JPanel)renderer).getComponent(0)).getIcon().getIconHeight());
+        assertTrue(((JPanel)renderer).getComponent(1) instanceof JLabel);
+        assertEquals(model.myLabel, ((JLabel)((JPanel)renderer).getComponent(1)).getText());
+      }
+    }
   }
 
-  public void testCellAtFor3Columns2Rows() {
-    Dimension cellSize = gallery.computeCellSize();
-    Insets borderInsets = BORDER.getBorderInsets(gallery);
-    Dimension preferredSize = setGalleryWidth(cellSize.width * 10 / 3);
-
-    assertEquals(0, gallery.getCellAt(new Point(borderInsets.left + 1, borderInsets.top + 1)));
-    assertEquals(2, gallery.getCellAt(new Point(preferredSize.width - borderInsets.right - 1, borderInsets.top + cellSize.height - 1)));
-    assertEquals(3, gallery.getCellAt(new Point(borderInsets.left + 1, borderInsets.top + cellSize.height)));
-    int clientWidth = preferredSize.width - borderInsets.left - borderInsets.right;
-    assertEquals(4, gallery.getCellAt(new Point(gallery.getColumnOffset(2, gallery.getColumnCount(), clientWidth) + borderInsets.left - 1,
-                                                borderInsets.top + cellSize.height + 1)));
-    assertEquals(4, gallery.getCellAt(new Point(gallery.getColumnOffset(2, gallery.getColumnCount(), clientWidth) + borderInsets.left - 1,
-                                                borderInsets.top + (cellSize.height * 2) - 1)));
-    assertEquals(-1, gallery.getCellAt(new Point(gallery.getColumnOffset(2, gallery.getColumnCount(), clientWidth) + borderInsets.left,
-                                                borderInsets.top + (cellSize.height * 2) - 1)));
+  public void testCellRendererCache() {
+    for (int i = 0; i < COLUMNS; i++) {
+      ModelObject model = objects[i];
+      Component renderer1 = gallery.getCellRenderer().getListCellRendererComponent(gallery, model, i, false, false);
+      assertNotNull(renderer1);
+      Component renderer2 = gallery.getCellRenderer().getListCellRendererComponent(gallery, model, i, true, false);
+      assertEquals(renderer1, renderer2);
+      Component renderer3 = gallery.getCellRenderer().getListCellRendererComponent(gallery, model, i, true, true);
+      assertEquals(renderer1, renderer3);
+    }
   }
 
-  public void testCellAtOneRowSpanning() {
-    Dimension cellSize = gallery.computeCellSize();
-    Insets borderInsets = BORDER.getBorderInsets(gallery);
-    Dimension preferredSize = setGalleryWidth(cellSize.width * (COLUMNS + 3));
-
-    assertEquals(0, gallery.getCellAt(new Point(borderInsets.left + 1, borderInsets.top + 1)));
-    assertEquals(4, gallery.getCellAt(new Point(preferredSize.width - borderInsets.right - 1, borderInsets.top + cellSize.height - 1)));
-    assertEquals(-1, gallery.getCellAt(new Point(preferredSize.width - borderInsets.right, borderInsets.top + cellSize.height - 1)));
+  public void testNextElementAction() {
+    ActionEvent event = new ActionEvent(gallery, ActionEvent.ACTION_FIRST, "test");
+    gallery.setSelectedIndex(0);
+    assertEquals(0, gallery.getSelectedIndex());
+    for (int i = 1; i < COLUMNS; i++) {
+      gallery.getActionMap().get("nextListElement").actionPerformed(event);
+      assertEquals(i, gallery.getSelectedIndex());
+    }
+    gallery.getActionMap().get("nextListElement").actionPerformed(event);
+    assertEquals(COLUMNS - 1, gallery.getSelectedIndex());
   }
 
-  public void testCellAtOneRowNonSpanning() {
-    Dimension cellSize = gallery.computeCellSize();
-    Insets borderInsets = BORDER.getBorderInsets(gallery);
-    Dimension preferredSize = setGalleryWidth(cellSize.width * (COLUMNS * 2 + 3));
-
-    assertEquals(0, gallery.getCellAt(new Point(borderInsets.left + 1, borderInsets.top + 1)));
-    assertEquals(-1, gallery.getCellAt(new Point(preferredSize.width - borderInsets.right - 1, borderInsets.top + cellSize.height - 1)));
-    assertEquals(4, gallery.getCellAt(new Point(borderInsets.left + cellSize.width * 5 - 1, borderInsets.top + cellSize.height - 1)));
-    assertEquals(-1, gallery.getCellAt(new Point(borderInsets.left + cellSize.width * 5, borderInsets.top + cellSize.height - 1)));
+  public void testPreviousElementAction() {
+    ActionEvent event = new ActionEvent(gallery, ActionEvent.ACTION_FIRST, "test");
+    gallery.setSelectedIndex(COLUMNS - 1);
+    assertEquals(COLUMNS - 1, gallery.getSelectedIndex());
+    for (int i = COLUMNS - 2; i >= 0; i--) {
+      gallery.getActionMap().get("previousListElement").actionPerformed(event);
+      assertEquals(i, gallery.getSelectedIndex());
+    }
+    gallery.getActionMap().get("previousListElement").actionPerformed(event);
+    assertEquals(0, gallery.getSelectedIndex());
   }
 
-  public void testComputeCellSize() {
-    Insets cellMargin = gallery.getCellMargin();
-    int fontSize = gallery.getFont().getSize();
-    Dimension expected = adjustByInsetsValue(THUMBNAIL_SIZE.width, THUMBNAIL_SIZE.height + (fontSize * 2), cellMargin);
-    Dimension actualCellSize = gallery.computeCellSize();
-    assertEquals(expected, actualCellSize);
-  }
-
-  public void testComputeCellSizeWithCustomMargins() {
-    Insets cellMargin = new Insets(10, 20, 30, 40);
-    gallery.setCellMargin(cellMargin);
-    int fontSize = gallery.getFont().getSize();
-    Dimension expected = adjustByInsetsValue(THUMBNAIL_SIZE.width, THUMBNAIL_SIZE.height + (fontSize * 2), cellMargin);
-    Dimension actualCellSize = gallery.computeCellSize();
-    assertEquals(expected, actualCellSize);
-  }
-
-  public void testGetColumnCount() {
-    Dimension actualCellSize = gallery.computeCellSize();
-    // Less than 2x elements - expand to fill the whole width
-    assertColumnCountForWidth(gallery, COLUMNS, actualCellSize.width * COLUMNS * 3 / 2);
-    // 2x - collapse
-    assertColumnCountForWidth(gallery, COLUMNS * 2, actualCellSize.width * COLUMNS * 2);
-    // More than 2x - collapse still
-    assertColumnCountForWidth(gallery, COLUMNS * 3, actualCellSize.width * COLUMNS * 3);
-  }
-
-  public void testPreferredSize() {
-    Dimension dimension = gallery.computeCellSize();
-    Insets borderInsets = BORDER.getBorderInsets(gallery);
-
-    // Default width - only suggests a single cell size
-    assertEquals(adjustByInsetsValue(dimension, borderInsets), gallery.getPreferredSize());
-
-    // Width greater than 2 cells - width as specified, height to show enough rows
-    int twoAndAHalfColumns = dimension.width * COLUMNS / 2;
-    gallery.setSize(new Dimension(twoAndAHalfColumns, 10));
-    Dimension expectedSize = new Dimension(twoAndAHalfColumns, dimension.height * 3 + borderInsets.top + borderInsets.bottom);
-    assertEquals(expectedSize, gallery.getPreferredSize());
-    assertEquals(expectedSize, gallery.getPreferredScrollableViewportSize());
-  }
-
-  private Dimension setGalleryWidth(int width) {
-    gallery.setSize(adjustByInsetsValue(width, gallery.computeCellSize().height, BORDER.getBorderInsets(gallery)));
-    Dimension preferredSize = gallery.getPreferredSize();
-    gallery.setSize(preferredSize);
-    return preferredSize;
+  public void testAccessible() {
+    for (int i = 0; i < COLUMNS; i++) {
+      ModelObject model = objects[i];
+      Accessible child = gallery.getAccessibleContext().getAccessibleChild(i);
+      assertNotNull(child);
+      assertEquals(model.myLabel, child.getAccessibleContext().getAccessibleName());
+      assertEquals(null, child.getAccessibleContext().getAccessibleDescription());
+      assertEquals(gallery, child.getAccessibleContext().getAccessibleParent());
+      assertEquals(i, child.getAccessibleContext().getAccessibleIndexInParent());
+    }
   }
 
   private static final class ModelObject {
     public final int myNumber;
+    public String myLabel;
+    public Image myImage;
 
     public ModelObject(int number) {
       myNumber = number;
