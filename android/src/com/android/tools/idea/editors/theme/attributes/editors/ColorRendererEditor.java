d770ce1e57a488a00308f69e90dac14dd3b948fc
Jerome Gaillard <jgaillard@google.com>
2015-08-19 17:02:46 +0100

    Fix live preview for project values
    The live preview was deciding whether attributes were framework or not
    based on the theme and not the attribute value. Now the attribute values
    are resolved before being passed to the preview.
    
    Change-Id: I7b9a61c11663631c907b8789de26c9ae0c4f4655

--- android/src/com/android/tools/idea/editors/theme/attributes/editors/ColorRendererEditor.java
+++ android/src/com/android/tools/idea/editors/theme/attributes/editors/ColorRendererEditor.java
@@ -15,29 +15,18 @@
  */
 package com.android.tools.idea.editors.theme.attributes.editors;
 
-import com.android.ide.common.resources.ResourceResolver;
 import com.android.resources.ResourceType;
 import com.android.tools.idea.editors.theme.*;
 import com.android.tools.idea.editors.theme.datamodels.EditedStyleItem;
-import com.android.tools.idea.editors.theme.attributes.AttributesTableModel;
-import com.android.tools.idea.editors.theme.MaterialColors;
-import com.android.tools.idea.editors.theme.datamodels.ThemeEditorStyle;
 import com.android.tools.idea.editors.theme.preview.AndroidThemePreviewPanel;
 import com.android.tools.idea.editors.theme.ui.ResourceComponent;
 import com.android.tools.idea.rendering.ResourceHelper;
 import com.android.tools.swing.ui.SwatchComponent;
-import com.intellij.openapi.project.DumbService;
-import com.intellij.openapi.ui.JBMenuItem;
 import org.jetbrains.android.dom.attrs.AttributeDefinition;
 import org.jetbrains.android.dom.attrs.AttributeFormat;
-import java.awt.Component;
-import javax.swing.JTable;
-import org.jetbrains.android.uipreview.ChooseResourceDialog;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 import java.awt.*;
-import java.awt.event.ActionEvent;
 import java.util.List;
 import java.util.Set;
 
@@ -45,29 +34,15 @@ import java.util.Set;
  * Class that implements a {@link javax.swing.JTable} renderer and editor for color attributes.
  */
 public class ColorRendererEditor extends GraphicalResourceRendererEditor {
-  public static final ResourceType[] COLORS_ONLY = {ResourceType.COLOR};
-  public static final ResourceType[] DRAWABLES_ONLY = {ResourceType.DRAWABLE, ResourceType.MIPMAP};
-  public static final ResourceType[] COLORS_AND_DRAWABLES = {ResourceType.COLOR, ResourceType.DRAWABLE, ResourceType.MIPMAP};
-
-  private AttributesTableModel myModel;
-
-  private final AndroidThemePreviewPanel myPreviewPanel;
-
   public ColorRendererEditor(@NotNull ThemeEditorContext context, @NotNull AndroidThemePreviewPanel previewPanel, boolean isEditor) {
-    super(context, isEditor);
-
-    if (isEditor) {
-      myComponent.addActionListener(new ColorEditorActionListener());
-    }
-    myPreviewPanel = previewPanel;
+    super(context, previewPanel, isEditor);
   }
 
   @Override
   protected void updateComponent(@NotNull ThemeEditorContext context, @NotNull ResourceComponent component, @NotNull EditedStyleItem item) {
     assert context.getResourceResolver() != null;
 
-    final List<Color> colors = ResourceHelper.resolveMultipleColors(context.getResourceResolver(), item.getSelectedValue(),
-                                                                    context.getProject());
+    final List<Color> colors = ResourceHelper.resolveMultipleColors(context.getResourceResolver(), item.getSelectedValue(), context.getProject());
     component.setSwatchIcons(SwatchComponent.colorListOf(colors));
     component.setNameText(item.getQualifiedName());
     component.setValueText(item.getValue());
@@ -96,91 +71,20 @@ public class ColorRendererEditor extends GraphicalResourceRendererEditor {
     }
   }
 
+  @NotNull
   @Override
-  public Component getEditorComponent(JTable table, EditedStyleItem value, boolean isSelected, int row, int column) {
-    myModel = (AttributesTableModel)table.getModel();
-    return super.getEditorComponent(table, value, isSelected, row, column);
-  }
+  protected ResourceType[] getAllowedResourceTypes() {
+    AttributeDefinition attrDefinition = ResolutionUtils.getAttributeDefinition(myContext.getConfiguration(), myItem.getSelectedValue());
 
-  private class ColorEditorActionListener extends DumbAwareActionListener {
-    public ColorEditorActionListener() {
-      super(myContext.getProject());
+    String attributeName = myItem.getName().toLowerCase();
+    if (attributeName.contains("color") || !ThemeEditorUtils.acceptsFormat(attrDefinition, AttributeFormat.Reference)) {
+      return COLORS_ONLY;
     }
-
-    @Override
-    public void dumbActionPerformed(ActionEvent e) {
-      DumbService.getInstance(myProject).showDumbModeNotification(DUMB_MODE_MESSAGE);
-      ColorRendererEditor.this.cancelCellEditing();
+    else if (attributeName.contains("drawable") || !ThemeEditorUtils.acceptsFormat(attrDefinition, AttributeFormat.Color)) {
+      return DRAWABLES_ONLY;
     }
-
-    @Override
-    public void smartActionPerformed(ActionEvent e) {
-      AttributeDefinition attrDefinition = ResolutionUtils.getAttributeDefinition(myContext.getConfiguration(), myItem.getSelectedValue());
-
-      ResourceType[] allowedTypes;
-      String attributeName = myItem.getName().toLowerCase();
-      if (attributeName.contains("color") || !ThemeEditorUtils.acceptsFormat(attrDefinition, AttributeFormat.Reference)) {
-        allowedTypes = COLORS_ONLY;
-      }
-      else if (attributeName.contains("drawable") || !ThemeEditorUtils.acceptsFormat(attrDefinition, AttributeFormat.Color)) {
-        allowedTypes = DRAWABLES_ONLY;
-      }
-      else {
-        allowedTypes = COLORS_AND_DRAWABLES;
-      }
-
-      ThemeEditorStyle style = myModel.getSelectedStyle();
-      ResourceResolver styleResourceResolver = myContext.getConfiguration().getResourceResolver();
-
-      assert styleResourceResolver != null;
-
-      Color primaryColor = ResourceHelper.resolveColor(styleResourceResolver, ThemeEditorUtils
-        .resolveItemFromParents(style, MaterialColors.PRIMARY_MATERIAL_ATTR, !ThemeEditorUtils.isAppCompatTheme(style)),
-                                                       myContext.getProject());
-
-      ChooseResourceDialog dialog = ThemeEditorUtils.getResourceDialog(myItem, myContext, allowedTypes);
-      if (primaryColor != null) {
-        dialog.generateColorSuggestions(primaryColor, myItem.getName());
-      }
-
-      final String oldValue = myItem.getSelectedValue().getValue();
-
-      dialog.setResourcePickerListener(new ChooseResourceDialog.ResourcePickerListener() {
-        @Override
-        public void resourceChanged(final @Nullable String resource) {
-          myItem.getSelectedValue().setValue(resource == null ? oldValue : resource);
-          myPreviewPanel.invalidateGraphicsRenderer();
-        }
-      });
-
-      if (e.getSource() instanceof JBMenuItem) {
-        // This has been triggered from the "Add variations" menu option so display location settings
-        dialog.openLocationSettings();
-      }
-
-      dialog.show();
-
-      // Restore the old value in the properties model
-      myItem.getSelectedValue().setValue(oldValue);
-
-      myEditorValue = null;
-      if (dialog.isOK()) {
-        String value = dialog.getResourceName();
-        if (value != null) {
-          myEditorValue = dialog.getResourceName();
-        }
-      }
-      else {
-        // User cancelled, clean up the preview
-        myPreviewPanel.invalidateGraphicsRenderer();
-      }
-
-      if (myEditorValue == null) {
-        ColorRendererEditor.this.cancelCellEditing();
-      }
-      else {
-        ColorRendererEditor.this.stopCellEditing();
-      }
+    else {
+      return COLORS_AND_DRAWABLES;
     }
   }
 }
