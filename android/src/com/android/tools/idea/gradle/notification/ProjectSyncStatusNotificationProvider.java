84aff4558b0d1af2cfab65c154a4f6a408253607
Alex Ruiz <alruiz@google.com>
2015-08-25 18:29:28 -0700

    Moved Gradle-related editor notifications to a *.gradle.* package.
    Bug: 23038126
    Change-Id: Ie4b29d0b77e8c30a079fcbb0559e01d84619e5a0

--- android/src/com/android/tools/idea/gradle/notification/ProjectSyncStatusNotificationProvider.java
+++ android/src/com/android/tools/idea/gradle/notification/ProjectSyncStatusNotificationProvider.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.android.tools.idea.editors;
+package com.android.tools.idea.gradle.notification;
 
 import com.android.tools.idea.gradle.GradleSyncState;
 import com.android.tools.idea.gradle.project.GradleProjectImporter;
