9799495d85f152024b1ef4493d58182a936ff2a4
Ian Cottrell <iancottrell@google.com>
2015-08-29 22:24:34 +0100

    Migrate all the CellController ui construction.
    Change-Id: I6e1877626819f82ebae99b60ee11f0496440bce4

--- android/src/com/android/tools/idea/editors/gfxtrace/controllers/ScrubberController.java
+++ android/src/com/android/tools/idea/editors/gfxtrace/controllers/ScrubberController.java
@@ -41,8 +41,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ScrubberController extends CellController<ScrubberController.Data> {
-  public static void createUI(GfxTraceEditor editor, @NotNull JBScrollPane scrollPane, @NotNull JBList list) {
-    new ScrubberController(editor, scrollPane, list);
+  public static JComponent createUI(GfxTraceEditor editor) {
+    return new ScrubberController(editor).myPanel;
   }
 
   public static class Data extends CellController.Data {
@@ -62,8 +62,8 @@ public class ScrubberController extends CellController<ScrubberController.Data> 
   @NotNull private final RenderSettings myRenderSettings = new RenderSettings();
   private boolean mDisableActivation = false;
 
-  private ScrubberController(@NotNull final GfxTraceEditor editor, @NotNull JBScrollPane scrollPane, @NotNull JBList list) {
-    super(editor, scrollPane, list);
+  private ScrubberController(@NotNull final GfxTraceEditor editor) {
+    super(editor);
     myRenderSettings.setMaxWidth(CellRenderer.MAX_WIDTH);
     myRenderSettings.setMaxHeight(CellRenderer.MAX_HEIGHT);
     myRenderSettings.setWireframeMode(WireframeMode.noWireframe());
