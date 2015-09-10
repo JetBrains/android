78869b01bb3fb1bebd392760090d1aaa798ac9a2
Vladislav.Soroka <Vladislav.Soroka@jetbrains.com>
2015-09-10 16:09:52 +0300

    external system: remove import process from edt

--- android/src/com/android/tools/idea/gradle/util/Projects.java
+++ android/src/com/android/tools/idea/gradle/util/Projects.java
@@ -125,7 +125,7 @@ public final class Projects {
                 @Override
                 public void run() {
                   ProjectDataManager dataManager = ServiceManager.getService(ProjectDataManager.class);
-                  dataManager.importData(ProjectKeys.MODULE, modules, project, true /* synchronous */);
+                  dataManager.importData(modules, project, true /* synchronous */);
                 }
               });
             }
