83a7c20d96b02fe4dc41c13993a9d64819a80eef
Chris Warrington <cmw@google.com>
2015-09-16 14:15:30 -0700

    Update manifest merger in AndroidManifestMergingBuilder
    So that the old manifest merger can be deleted.
    
    Change-Id: I67946ffced9f0b7507f2b97b9c350762200e95f9

--- android/jps-plugin/src/org/jetbrains/jps/android/AndroidManifestMergingBuilder.java
+++ android/jps-plugin/src/org/jetbrains/jps/android/AndroidManifestMergingBuilder.java
@@ -1,17 +1,18 @@
 package org.jetbrains.jps.android;
 
 import com.android.SdkConstants;
-import com.android.annotations.NonNull;
-import com.android.manifmerger.ICallback;
-import com.android.manifmerger.IMergerLog;
-import com.android.manifmerger.ManifestMerger;
-import com.android.sdklib.AndroidTargetHash;
-import com.android.sdklib.AndroidVersion;
-import com.android.sdklib.IAndroidTarget;
-import com.android.sdklib.repositoryv2.AndroidSdkHandler;
+import com.android.ide.common.blame.SourceFilePosition;
+import com.android.ide.common.blame.SourcePosition;
+import com.android.manifmerger.ManifestMerger2;
+import com.android.manifmerger.MergingReport;
+import com.android.manifmerger.XmlDocument;
 import com.android.tools.idea.jps.AndroidTargetBuilder;
+import com.android.utils.NullLogger;
+import com.android.utils.Pair;
+import com.google.common.base.Charsets;
+import com.google.common.collect.ImmutableList;
+import com.google.common.io.Files;
 import com.intellij.openapi.diagnostic.Logger;
-import com.intellij.openapi.util.io.FileUtil;
 import org.jetbrains.android.util.AndroidBuildTestingManager;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.jps.android.model.JpsAndroidModuleExtension;
@@ -28,7 +29,6 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.IllegalFormatException;
 import java.util.List;
 
 /**
@@ -104,12 +104,8 @@ public class AndroidManifestMergingBuilder
       return false;
     }
     final File outputFile = new File(outputDir, SdkConstants.FN_ANDROID_MANIFEST_XML);
-    final AndroidPlatform platform = AndroidJpsUtil.getAndroidPlatform(module, context, BUILDER_NAME);
 
-    if (platform == null) {
-      return false;
-    }
-    if (!doMergeManifests(context, platform.getSdkHandler(), manifestFile, libManifests, outputFile)) {
+    if (!doMergeManifests(context,  manifestFile, libManifests, outputFile)) {
       context.processMessage(new CompilerMessage(BUILDER_NAME, BuildMessage.Kind.ERROR,
                                                  "[" + module.getName() + "] Cannot perform manifest merging"));
       return false;
@@ -131,7 +127,6 @@ public class AndroidManifestMergingBuilder
   }
 
   private static boolean doMergeManifests(final CompileContext context,
-                                          final AndroidSdkHandler sdkHandler,
                                           File manifestFile,
                                           List<File> libManifests,
                                           File outputFile)
@@ -150,75 +145,48 @@ public class AndroidManifestMergingBuilder
       testingManager.getCommandExecutor().log(messageBuilder.toString());
     }
 
-    final ManifestMerger manifestMerger = new ManifestMerger(new IMergerLog() {
-      @Override
-      public void error(@NonNull Severity severity,
-                        @NonNull FileAndLine location,
-                        @NonNull String message,
-                        Object... msgParams) {
-        context.processMessage(new CompilerMessage(BUILDER_NAME, toBuildMessageKind(severity), formatMessage(message, msgParams),
-                                                   location.getFileName(), -1L, -1L, -1L, location.getLine(), -1L));
-      }
 
-      @Override
-      public void conflict(@NonNull Severity severity,
-                           @NonNull FileAndLine location1,
-                           @NonNull FileAndLine location2,
-                           @NonNull String message,
-                           Object... msgParams) {
-        final StringBuilder builder = new StringBuilder("Conflicts:\n");
-        final String filePath1 = location1.getFileName();
+    ImmutableList.Builder<Pair<String, File>> libraryFiles = ImmutableList.builder();
+    for (File f : libManifests) {
+      libraryFiles.add(Pair.of(f.getName(), f));
+    }
 
-        if (filePath1 != null) {
-          builder.append(FileUtil.toSystemDependentName(filePath1)).append(": line ").append(location1.getLine());
-        }
-        else {
-          builder.append("unknown");
-        }
-        builder.append('\n');
-        final String filePath2 = location2.getFileName();
+    final ManifestMerger2.Invoker manifestMergerInvoker =
+      ManifestMerger2.newMerger(manifestFile, NullLogger.getLogger(), ManifestMerger2.MergeType.APPLICATION).addLibraryManifests(libraryFiles.build());
 
-        if (filePath2 != null) {
-          builder.append(FileUtil.toSystemDependentName(filePath2)).append(": line ").append(location1.getLine());
-        }
-        else {
-          builder.append("unknown");
-        }
-        builder.append('\n').append(formatMessage(message, msgParams));
-        context.processMessage(new CompilerMessage(BUILDER_NAME, toBuildMessageKind(severity),
-                                                   builder.toString(), filePath1, -1L, -1L, -1L, location1.getLine(), -1L));
-      }
+    MergingReport mergingReport;
+    try {
+      mergingReport = manifestMergerInvoker.merge();
+    }
+    catch (ManifestMerger2.MergeFailureException e) {
+      context.processMessage(new CompilerMessage(BUILDER_NAME, BuildMessage.Kind.ERROR, e.getMessage()));
+      return false;
+    }
 
-      private String formatMessage(String message, Object... msgParams) {
-        try {
-          return String.format(message, msgParams);
-        }
-        catch (IllegalFormatException e) {
-          LOG.debug(e);
-          return message;
-        }
+    MergingReport.Result result = mergingReport.getResult();
+
+    for (MergingReport.Record record : mergingReport.getLoggingRecords()) {
+      SourceFilePosition position = record.getSourceLocation();
+      File sourceFile = position.getFile().getSourceFile();
+      String sourceFilePath = sourceFile != null ? sourceFile.getAbsolutePath() : null;
+      SourcePosition pos = position.getPosition();
+      MergingReport.Record.Severity severity = record.getSeverity();
+
+      if (severity != MergingReport.Record.Severity.INFO) {
+        context.processMessage(
+          new CompilerMessage(BUILDER_NAME, toBuildMessageKind(record.getSeverity()), record.getMessage(), sourceFilePath,
+                              pos.getStartOffset(), pos.getEndOffset(), pos.getEndOffset(), pos.getEndLine(), pos.getEndColumn()));
       }
-    }, new ICallback() {
-      @Override
-      public int queryCodenameApiLevel(@NonNull String codename) {
-        try {
-          AndroidVersion version = new AndroidVersion(codename);
-          String hashString = AndroidTargetHash.getPlatformHashString(version);
-          AndroidJpsUtil.RepoLogger log = new AndroidJpsUtil.RepoLogger();
-          IAndroidTarget t = sdkHandler.getAndroidTargetManager(log).getTargetFromHashString(hashString, log);
-          if (t != null) {
-            return t.getVersion().getApiLevel();
-          }
-        }
-        catch (AndroidVersion.AndroidVersionException ignore) {
-        }
-        return ICallback.UNKNOWN_CODENAME;
-      }
-    });
-    return manifestMerger.process(outputFile, manifestFile, libManifests.toArray(new File[libManifests.size()]), null, null);
+    }
+
+    if (!result.isError()) {
+      XmlDocument xmlDocument = mergingReport.getMergedDocument().get();
+      Files.write(xmlDocument.prettyPrint(), outputFile, Charsets.UTF_8);
+    }
+    return result.isSuccess();
   }
 
-  private static BuildMessage.Kind toBuildMessageKind(IMergerLog.Severity severity) {
+  private static BuildMessage.Kind toBuildMessageKind(MergingReport.Record.Severity severity) {
     if (severity == null) {
       return BuildMessage.Kind.INFO;
     }
