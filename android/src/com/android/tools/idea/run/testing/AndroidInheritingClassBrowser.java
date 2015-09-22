47bb64d09862b8c5c446aba9b9fa7d0c27441410
Siva Velusamy <vsiva@google.com>
2015-09-22 11:37:05 -0700

    Move classes used only in testing config to the testing package
    Change-Id: I32be36799a5e5c83a4852a15b904a5e1d1b57d53

--- android/src/com/android/tools/idea/run/testing/AndroidInheritingClassBrowser.java
+++ android/src/com/android/tools/idea/run/testing/AndroidInheritingClassBrowser.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.android.tools.idea.run;
+package com.android.tools.idea.run.testing;
 
 import com.intellij.CommonBundle;
 import com.intellij.execution.ui.ConfigurationModuleSelector;
