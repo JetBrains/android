dc562dacd99a125e6fb744db8c5860663d34f5d0
David Herman <davidherman@google.com>
2015-09-28 11:18:45 -0700

    Fix 'clear logcat' sometimes not working
    The 'clear logcat' feature worked by clearing the target device's
    logcat. However, for various reasons (adb connection, device issues?),
    this operation is not 100% reliable.
    
    To handle this, we add a feature to the Studio logcat filtering
    system so it can reject old messages. This means that even we don't
    clear the logcat buffer on the target device, Android Studio can
    still behave as expected - if you mark the filter with the current
    time, clear the console, and replay all messages, all old lines will
    be skipped.
    
    Related bug: https://code.google.com/p/android/issues/detail?id=175945
    
    Change-Id: Id1da4c6da7d864db85e247bfa477a735694af5a3

--- android/src/com/android/tools/idea/logcat/AndroidLogcatReceiver.java
+++ android/src/com/android/tools/idea/logcat/AndroidLogcatReceiver.java
@@ -16,7 +16,6 @@
 
 package com.android.tools.idea.logcat;
 
-import com.android.annotations.VisibleForTesting;
 import com.android.ddmlib.IDevice;
 import com.android.ddmlib.Log;
 import com.intellij.openapi.Disposable;
@@ -25,6 +24,9 @@ import org.jetbrains.android.util.AndroidOutputReceiver;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -136,12 +138,30 @@ public final class AndroidLogcatReceiver extends AndroidOutputReceiver implement
   }
 
   static final class LogMessageHeader {
+
+    // Parser that matches the format of the time portion of HEADER_PATTERN
+    private static SimpleDateFormat TIME_PARSER = new SimpleDateFormat("MM-dd hh:mm:ss.SSS");
+
     String myTime;
     Log.LogLevel myLogLevel;
     int myPid;
     String myTid;
     String myAppPackage;
     String myTag;
+
+    /**
+     * Returns the header time as a {@link Date} instance, which is more convenient for comparing
+     * one header's time with another.
+     */
+    @NotNull
+    Date getTimeAsDate() {
+      try {
+        return TIME_PARSER.parse(myTime);
+      }
+      catch (ParseException e) {
+        throw new RuntimeException(String.format("Could not convert %s to %s", myTime, Date.class.getSimpleName()), e);
+      }
+    }
   }
 
   private static String getFullMessage(LogMessageHeader header, String message) {
