fe5b61b1afa67a09a28c919b1dfa11e6f2333dad
Ben Clayton <bclayton@google.com>
2015-10-18 13:52:11 +0100

    Embed atom memory observations in a more generic ‘Extras’ array.
    Change-Id: Ie4e56be93f3e451aaff1c760dde52c23d50fd95a

--- android/src/com/android/tools/idea/editors/gfxtrace/service/atom/DynamicAtom.java
+++ android/src/com/android/tools/idea/editors/gfxtrace/service/atom/DynamicAtom.java
@@ -56,16 +56,20 @@ public class DynamicAtom extends Atom {
   }
 
   @Override
-  public int getObservationsIndex() {
-    return myMetadata.myObservationsIndex;
+  public int getExtrasIndex() {
+    return myMetadata.myExtrasIndex;
   }
 
   @Override
   public Observations getObservations() {
-    if (myMetadata.myObservationsIndex >= 0) {
-      Object value = getFieldValue(myMetadata.myObservationsIndex);
-      assert (value instanceof Observations);
-      return (Observations)value;
+    if (myMetadata.myExtrasIndex >= 0) {
+      Object extras = getFieldValue(myMetadata.myExtrasIndex);
+      assert (extras instanceof Object[]);
+      for (Object extra : (Object[])extras) {
+        if (extra instanceof Observations) {
+          return (Observations)extra;
+        }
+      }
     }
     return null;
   }
