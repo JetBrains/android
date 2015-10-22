cbbbb227d8fb71f4616041d651344bb4bd28feef
peter <peter@jetbrains.com>
2015-10-22 19:51:32 +0200

    gradle: change roots later in none modality state (EA-75024 - Throwable: DumbServiceImpl.scheduleCacheUpdate)

--- android/src/com/android/tools/idea/gradle/util/Projects.java
+++ android/src/com/android/tools/idea/gradle/util/Projects.java
@@ -16,8 +16,8 @@
 package com.android.tools.idea.gradle.util;
 
 import com.android.builder.model.AndroidProject;
-import com.android.tools.idea.gradle.GradleSyncState;
 import com.android.tools.idea.gradle.AndroidGradleModel;
+import com.android.tools.idea.gradle.GradleSyncState;
 import com.android.tools.idea.gradle.JavaModel;
 import com.android.tools.idea.gradle.compiler.AndroidGradleBuildConfiguration;
 import com.android.tools.idea.gradle.customizer.dependency.DependencySetupErrors;
@@ -32,9 +32,9 @@ import com.intellij.ide.projectView.ProjectView;
 import com.intellij.ide.projectView.impl.AbstractProjectViewPane;
 import com.intellij.openapi.actionSystem.DataContext;
 import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.application.ModalityState;
 import com.intellij.openapi.components.ServiceManager;
 import com.intellij.openapi.externalSystem.model.DataNode;
-import com.intellij.openapi.externalSystem.model.ProjectKeys;
 import com.intellij.openapi.externalSystem.model.project.ModuleData;
 import com.intellij.openapi.externalSystem.service.project.manage.ProjectDataManager;
 import com.intellij.openapi.module.Module;
@@ -157,7 +157,7 @@ public final class Projects {
         });
       }
     };
-    invokeAndWaitIfNeeded(task);
+    ApplicationManager.getApplication().invokeAndWait(task, ModalityState.defaultModalityState());
   }
 
   public static void setHasSyncErrors(@NotNull Project project, boolean hasSyncErrors) {
