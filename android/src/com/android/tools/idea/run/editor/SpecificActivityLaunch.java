84b533dd37f3eb104ed66ed31b7f02b1e117735c
Siva Velusamy <vsiva@google.com>
2015-10-28 16:47:43 -0700

    Add a new UI for the device chooser
    This CL adds a new UI for the device chooser dialog. Currently, this
    shows up only when cloud testing is enabled. It will become the default
    in a subsequent release.
    
    The new UI allows other plugins to contribute additional UX to the
    device chooser. Currently, this is hooked up to show the cloud test
    configuration panel if it is enabled.
    
    Change-Id: I73cd483f4f14e58e011223b67f293d76fb76a025

--- android/src/com/android/tools/idea/run/editor/SpecificActivityLaunch.java
+++ android/src/com/android/tools/idea/run/editor/SpecificActivityLaunch.java
@@ -19,37 +19,13 @@ import com.android.tools.idea.run.AndroidApplicationLauncher;
 import com.android.tools.idea.run.AndroidRunConfiguration;
 import com.android.tools.idea.run.ValidationError;
 import com.android.tools.idea.run.activity.ActivityLocator;
-import com.android.tools.idea.run.activity.ActivityLocatorUtils;
 import com.android.tools.idea.run.activity.AndroidActivityLauncher;
 import com.android.tools.idea.run.activity.SpecificActivityLocator;
 import com.google.common.collect.ImmutableList;
-import com.intellij.codeInsight.daemon.DaemonCodeAnalyzer;
-import com.intellij.execution.ExecutionBundle;
-import com.intellij.ide.util.TreeClassChooser;
-import com.intellij.ide.util.TreeClassChooserFactory;
-import com.intellij.openapi.editor.ex.EditorEx;
-import com.intellij.openapi.fileTypes.PlainTextLanguage;
-import com.intellij.openapi.module.Module;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.ui.ComponentWithBrowseButton;
-import com.intellij.openapi.ui.Messages;
-import com.intellij.openapi.util.text.StringUtil;
-import com.intellij.psi.JavaPsiFacade;
-import com.intellij.psi.PsiClass;
-import com.intellij.psi.PsiDocumentManager;
-import com.intellij.psi.PsiFile;
-import com.intellij.psi.search.ProjectScope;
-import com.intellij.ui.EditorTextField;
-import com.intellij.ui.LanguageTextField;
 import org.jetbrains.android.facet.AndroidFacet;
-import org.jetbrains.android.util.AndroidBundle;
-import org.jetbrains.android.util.AndroidUtils;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
-import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.util.List;
 
 public class SpecificActivityLaunch extends LaunchOption<SpecificActivityLaunch.State> {
