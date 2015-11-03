3f4d554600165d979ef61699254a7bfb3a91283a
Tor Norbye <tnorbye@google.com>
2015-11-02 18:49:22 -0800

    Update minimum required plugin version for 10x to alpha4
    Change-Id: I982a80e0fa15f48d2ca343e05fafa267be8c6e22

--- android/src/com/android/tools/idea/fd/FastDeployManager.java
+++ android/src/com/android/tools/idea/fd/FastDeployManager.java
@@ -105,7 +105,7 @@ import static com.android.SdkConstants.*;
  * in the IDE: determining if an app is running with the fast deploy runtime, whether it's up to date, communicating with it, etc.
  */
 public final class FastDeployManager implements ProjectComponent, BulkFileListener {
-  public static final String MINIMUM_GRADLE_PLUGIN_VERSION_STRING = "1.6.0-alpha2";
+  public static final String MINIMUM_GRADLE_PLUGIN_VERSION_STRING = "1.6.0-alpha4";
   public static final FullRevision MINIMUM_GRADLE_PLUGIN_VERSION = FullRevision.parseRevision(MINIMUM_GRADLE_PLUGIN_VERSION_STRING);
   private static final NotificationGroup NOTIFICATION_GROUP = NotificationGroup.toolWindowGroup("InstantRun", ToolWindowId.RUN);
   private static final Object INSTANCE_LOCK = new Object();
