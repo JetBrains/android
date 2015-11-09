3f2d64e483e0071b9856aa2b7dc3a2a657f9b124
Jerome Gaillard <jgaillard@google.com>
2015-11-09 15:53:45 +0000

    Fix deadlock when opening the theme editor
    The deadlock happens because the table renderer uses the same component
    as the editor, and that component requires a write lock to set the text.
    So painting the table can potentially cause a deadlock.
    We now use a simple label for the renderer so that we will no need a
    write lock anymore when simply painting the table.
    
    Bug: http://b.android.com/193673
    Change-Id: I7fcddfe251821f285650477a79ef10bb1d8f5297

--- android/src/com/android/tools/swing/ui/SwatchComponent.java
+++ android/src/com/android/tools/swing/ui/SwatchComponent.java
@@ -36,6 +36,7 @@ import org.jetbrains.annotations.Nullable;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.geom.Area;
 import java.awt.geom.RoundRectangle2D;
 import java.awt.image.BufferedImage;
 import java.util.List;
@@ -43,9 +44,9 @@ import java.util.List;
 import static com.intellij.util.ui.GraphicsUtil.setupAAPainting;
 
 /**
- * Component that displays a list of icons and a label
+ * Component that displays a clickable icon and a label or a text field with autocompletion
  */
-public class SwatchComponent extends Box {
+public class SwatchComponent extends JPanel {
   /** Padding used between the icon and the text field */
   private static final int PADDING = JBUI.scale(3);
   /** Padding around the text in the text field */
@@ -65,76 +66,82 @@ public class SwatchComponent extends Box {
     }
   };
 
-  private final TextFieldWithAutoCompletion<String> myTextField;
+  private @Nullable final TextFieldWithAutoCompletion<String> myTextField;
+  private @Nullable final JLabel myTextLabel;
   private final ClickableLabel mySwatchButton;
   private final List<ActionListener> myTextListeners = Lists.newArrayList();
 
   private Color myBorderColor;
 
   /**
-   * Constructs a SwatchComponent that is composed of a clickable icon and a text field with autocompletion
+   * Constructs a SwatchComponent that is composed of a clickable icon and a text field with autocompletion or a label
+   * @param isEditor if true, this component has a text field with autocompletion, whereas if false, it has a label
    */
-  public SwatchComponent(@NotNull Project project) {
-    super(BoxLayout.LINE_AXIS);
+  public SwatchComponent(@NotNull Project project, boolean isEditor) {
+    super(new BorderLayout(PADDING, 0));
     setBorder(null);
 
     myBorderColor = DEFAULT_BORDER_COLOR;
     mySwatchButton = new ClickableLabel();
-    //noinspection unchecked (EMPTY_COMPLETION doesn't need the generic type as it is empty)
-    myTextField = new TextFieldWithAutoCompletion<String>(project, TextFieldWithAutoCompletion.EMPTY_COMPLETION, true, null) {
-      @Override
-      protected boolean processKeyBinding(KeyStroke ks, KeyEvent e, int condition, boolean pressed) {
-        if (e.getKeyCode() == KeyEvent.VK_ENTER) {
-          ActionEvent event = new ActionEvent(myTextField, ActionEvent.ACTION_PERFORMED, null);
-          for (ActionListener listener : myTextListeners) {
-            listener.actionPerformed(event);
+    add(mySwatchButton, BorderLayout.LINE_START);
+
+    if (isEditor) {
+      //noinspection unchecked (EMPTY_COMPLETION doesn't need the generic type as it is empty)
+      myTextField = new TextFieldWithAutoCompletion<String>(project, TextFieldWithAutoCompletion.EMPTY_COMPLETION, true, null) {
+        @Override
+        protected boolean processKeyBinding(KeyStroke ks, KeyEvent e, int condition, boolean pressed) {
+          if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+            //noinspection ConstantConditions: myTextField != null since it is currently being created
+            ActionEvent event = new ActionEvent(myTextField, ActionEvent.ACTION_PERFORMED, null);
+            for (ActionListener listener : myTextListeners) {
+              listener.actionPerformed(event);
+            }
+            return true;
           }
-          return true;
+          return false;
         }
-        return false;
-      }
 
-      @Override
-      protected void updateBorder(@NotNull EditorEx editor) {
-        editor.setBorder(new RoundedLineBorder(myBorderColor, ARC_SIZE) {
-          @Override
-          public Insets getBorderInsets(Component c) {
-            return new Insets(TEXT_PADDING, TEXT_PADDING, TEXT_PADDING, TEXT_PADDING);
-          }
-
-          @Override
-          public void paintBorder(Component c, Graphics g, int x, int y, int width, int height) {
-            g.setColor(JBColor.WHITE);
-            g.fillRoundRect(x, y, width - 1, height - 1, ARC_SIZE, ARC_SIZE);
-            super.paintBorder(c, g, x, y, width, height);
-          }
-        });
-      }
-
-      @Override
-      protected EditorEx createEditor() {
-        EditorEx editor = super.createEditor();
-        editor.getScrollPane().setOpaque(false);
-        return editor;
-      }
-
-      @Override
-      public void removeNotify() {
-        // The editor needs to be removed manually because it normally is removed by invokeLater, which may happen to late
-        // and cause a crash when closing the project.
-        Editor editor = getEditor();
-        if (editor != null && !editor.isDisposed()) {
-          EditorFactory.getInstance().releaseEditor(editor);
+        @Override
+        protected void updateBorder(@NotNull EditorEx editor) {
+          editor.setBorder(new PaddedRoundedBorder(myBorderColor));
         }
-        super.removeNotify();
-      }
-    };
-    myTextField.setOneLineMode(true);
-    myTextField.setBackground(JBColor.WHITE);
 
-    add(mySwatchButton);
-    add(Box.createHorizontalStrut(PADDING));
-    add(myTextField);
+        @Override
+        protected EditorEx createEditor() {
+          EditorEx editor = super.createEditor();
+          editor.getScrollPane().setOpaque(false);
+          return editor;
+        }
+
+        @Override
+        public void removeNotify() {
+          // The editor needs to be removed manually because it normally is removed by invokeLater, which may happen to late
+          // and cause a crash when closing the project.
+          Editor editor = getEditor();
+          if (editor != null && !editor.isDisposed()) {
+            EditorFactory.getInstance().releaseEditor(editor);
+          }
+          super.removeNotify();
+        }
+      };
+      myTextField.setOneLineMode(true);
+      myTextField.setBackground(JBColor.WHITE);
+      add(myTextField, BorderLayout.CENTER);
+      myTextLabel = null;
+    }
+    else {
+      myTextLabel = new JLabel() {
+        @Override
+        protected void paintComponent(Graphics g) {
+          g.setColor(JBColor.WHITE);
+          g.fillRect(TEXT_PADDING, TEXT_PADDING, getWidth() - 2 * TEXT_PADDING, getHeight() - 2 * TEXT_PADDING);
+          super.paintComponent(g);
+        }
+      };
+      myTextLabel.setBorder(new PaddedRoundedBorder(myBorderColor));
+      add(myTextLabel, BorderLayout.CENTER);
+      myTextField = null;
+    }
   }
 
   public void setSwatchIcon(@NotNull SwatchIcon icon) {
@@ -157,6 +164,9 @@ public class SwatchComponent extends Box {
     super.setFont(font);
     if (myTextField != null) {
       myTextField.setFont(font);
+    }
+    else if (myTextLabel != null) {
+      myTextLabel.setFont(font);
     }
   }
 
@@ -233,15 +243,27 @@ public class SwatchComponent extends Box {
   }
 
   public void setText(@NotNull String text) {
-    // No need to register this action for undoing, and it conflicts with other undo actions if we do
-    myTextField.getDocument().putUserData(UndoConstants.DONT_RECORD_UNDO, true);
-    myTextField.setText(text);
-    myTextField.getDocument().putUserData(UndoConstants.DONT_RECORD_UNDO, false);
+    if (myTextField != null) {
+      // No need to register this action for undoing, and it conflicts with other undo actions if we do
+      myTextField.getDocument().putUserData(UndoConstants.DONT_RECORD_UNDO, true);
+      myTextField.setText(text);
+      myTextField.getDocument().putUserData(UndoConstants.DONT_RECORD_UNDO, false);
+    }
+    else {
+      assert myTextLabel != null;
+      myTextLabel.setText(text);
+    }
   }
 
   @NotNull
   public String getText() {
-    return myTextField.getText();
+    if (myTextField != null) {
+      return myTextField.getText();
+    }
+    else {
+      assert myTextLabel != null;
+      return myTextLabel.getText();
+    }
   }
 
   public void addSwatchListener(@NotNull ActionListener listener) {
@@ -253,10 +275,12 @@ public class SwatchComponent extends Box {
   }
 
   public void addTextDocumentListener(@NotNull final DocumentListener listener) {
+    assert myTextField != null;
     myTextField.addDocumentListener(listener);
   }
 
   public void addTextFocusListener(@NotNull final FocusListener listener) {
+    assert myTextField != null;
     myTextField.addFocusListener(listener);
   }
 
@@ -265,6 +289,7 @@ public class SwatchComponent extends Box {
   }
 
   public void setCompletionStrings(@NotNull List<String> completionStrings) {
+    assert myTextField != null;
     myTextField.setVariants(completionStrings);
   }
 
@@ -353,6 +378,35 @@ public class SwatchComponent extends Box {
 
     public void setBackgroundColor(@Nullable Color backgroundColor) {
       myBackgroundColor = backgroundColor;
+    }
+  }
+
+  private static class PaddedRoundedBorder extends RoundedLineBorder {
+    public PaddedRoundedBorder(@NotNull Color color) {
+      super(color, ARC_SIZE);
+    }
+
+    @Override
+    public Insets getBorderInsets(Component c) {
+      return new Insets(TEXT_PADDING, TEXT_PADDING, TEXT_PADDING, TEXT_PADDING);
+    }
+
+    @Override
+    public Insets getBorderInsets(Component c, Insets insets) {
+      insets.left = TEXT_PADDING;
+      insets.top = TEXT_PADDING;
+      insets.right = TEXT_PADDING;
+      insets.bottom = TEXT_PADDING;
+      return insets;
+    }
+
+    @Override
+    public void paintBorder(Component c, Graphics g, int x, int y, int width, int height) {
+      g.setColor(JBColor.WHITE);
+      Area area = new Area(new RoundRectangle2D.Double(x, y, width - 1, height - 1, ARC_SIZE, ARC_SIZE));
+      area.subtract(new Area(new Rectangle(x + TEXT_PADDING, y + TEXT_PADDING, width - 2 * TEXT_PADDING, height - 2 * TEXT_PADDING)));
+      ((Graphics2D)g).fill(area);
+      super.paintBorder(c, g, x, y, width, height);
     }
   }
 }
