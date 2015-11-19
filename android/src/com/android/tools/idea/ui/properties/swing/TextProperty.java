a18734dac8de94b465e654c90a801279273edcbe
David Herman <davidherman@google.com>
2015-11-18 16:36:51 -0800

    Add "EditorComboBox" support to TextProperty
    Change-Id: I8256376937c8183592e06ecff69f7acb7086b126

--- android/src/com/android/tools/idea/ui/properties/swing/TextProperty.java
+++ android/src/com/android/tools/idea/ui/properties/swing/TextProperty.java
@@ -19,6 +19,7 @@ import com.android.tools.idea.ui.LabelWithEditLink;
 import com.android.tools.idea.ui.properties.ObservableProperty;
 import com.android.tools.idea.ui.properties.core.StringProperty;
 import com.intellij.ui.DocumentAdapter;
+import com.intellij.ui.EditorComboBox;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
@@ -31,7 +32,8 @@ import java.beans.PropertyChangeListener;
 /**
  * {@link ObservableProperty} that wraps a Swing component and exposes its text value.
  */
-public final class TextProperty extends StringProperty implements DocumentListener, PropertyChangeListener {
+public final class TextProperty extends StringProperty implements DocumentListener, PropertyChangeListener,
+                                                                  com.intellij.openapi.editor.event.DocumentListener {
   private final JComponent myComponent;
 
   public TextProperty(JTextComponent textComponent) {
@@ -54,6 +56,11 @@ public final class TextProperty extends StringProperty implements DocumentListen
     editLabel.getDocument().addDocumentListener(this);
   }
 
+  public TextProperty(EditorComboBox editorComboBox) {
+    myComponent = editorComboBox;
+    editorComboBox.getDocument().addDocumentListener(this);
+  }
+
   @Override
   public void insertUpdate(DocumentEvent documentEvent) {
     notifyInvalidated();
@@ -74,6 +81,16 @@ public final class TextProperty extends StringProperty implements DocumentListen
     notifyInvalidated();
   }
 
+  @Override
+  public void beforeDocumentChange(com.intellij.openapi.editor.event.DocumentEvent event) {
+    // Do nothing, required interface override
+  }
+
+  @Override
+  public void documentChanged(com.intellij.openapi.editor.event.DocumentEvent event) {
+    notifyInvalidated();
+  }
+
   @NotNull
   @Override
   public String get() {
@@ -88,6 +105,9 @@ public final class TextProperty extends StringProperty implements DocumentListen
     }
     else if (myComponent instanceof LabelWithEditLink) {
       return ((LabelWithEditLink)myComponent).getText();
+    }
+    else if (myComponent instanceof EditorComboBox) {
+      return ((EditorComboBox)myComponent).getText();
     }
     else {
       throw new IllegalStateException("Unexpected text component type: " + myComponent.getClass().getSimpleName());
@@ -107,6 +127,9 @@ public final class TextProperty extends StringProperty implements DocumentListen
     }
     else if (myComponent instanceof LabelWithEditLink) {
       ((LabelWithEditLink)myComponent).setText(value);
+    }
+    else if (myComponent instanceof EditorComboBox) {
+      ((EditorComboBox)myComponent).setText(value);
     }
     else {
       throw new IllegalStateException("Unexpected text component type: " + myComponent.getClass().getSimpleName());
