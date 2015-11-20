c47094e2b3a2f43afb942038e16360f52a0df92e
Salvatore Guarnieri <salguarnieri@google.com>
2015-11-20 09:47:57 -0500

    Remove run config access to find old session
    After a recent merge, there was some regression in our usage of
    the run configuration to find old sessions. This attempts to
    undo that.
    
    The method introduced in the merge used the config name to find
    the old session. It should be more precise to use our existing
    method that uses the configuration id to find the old session.
    
    Additionally, the new method introduced added a new if
    condition. That condition was placed in the existing method
    since it makes sense for all situations.
    
    Change-Id: I8d37149430214b750afa460b62db7cc85d5e7ada

--- android/src/com/android/tools/idea/run/AndroidRunConfigurationBase.java
+++ android/src/com/android/tools/idea/run/AndroidRunConfigurationBase.java
@@ -269,7 +269,7 @@ public abstract class AndroidRunConfigurationBase extends ModuleBasedConfigurati
       return null;
     }
 
-    AndroidSessionInfo info = AndroidSessionManager.findOldSession(getProject(), getName());
+    AndroidSessionInfo info = AndroidSessionManager.findOldSession(getProject(), getUniqueID());
     if (info == null) {
       return null;
     }
@@ -302,7 +302,7 @@ public abstract class AndroidRunConfigurationBase extends ModuleBasedConfigurati
     DeployTargetState deployTargetState = getCurrentDeployTargetState();
     ProcessHandlerConsolePrinter printer = new ProcessHandlerConsolePrinter(null);
 
-    AndroidSessionInfo info = AndroidSessionManager.findOldSession(project, getName());
+    AndroidSessionInfo info = AndroidSessionManager.findOldSession(project, getUniqueID());
     if (info != null) {
       if (!info.getExecutorId().equals(executor.getId())) {
         // only run or debug of a config can be active at a time
