3aae15d3a55a625009ca5622553f9ea1d03a116a
Juan C Nuno <juancnuno@google.com>
2015-12-01 12:23:39 -0800

    Properly parameterize ImmutableList.of call
    Change-Id: I185f0274729b1ddd57f751c06c3e83b3bee0c05d

--- android/src/com/android/tools/idea/avdmanager/ConfigureAvdOptionsStep.java
+++ android/src/com/android/tools/idea/avdmanager/ConfigureAvdOptionsStep.java
@@ -968,7 +968,7 @@ public class ConfigureAvdOptionsStep extends DynamicWizardStepWithDescription {
   }
 
   private List<JComponent> getSkinComponents() {
-    return ImmutableList.of(mySkinComboBox, mySkinDefinitionLabel, myHardwareSkinHelpLabel);
+    return ImmutableList.<JComponent>of(mySkinComboBox, mySkinDefinitionLabel, myHardwareSkinHelpLabel);
   }
 
   private List<JComponent> getMemoryAndStorageComponents() {
