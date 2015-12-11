c084794e6192763d5b1d3cbc98c246a5ebd6a2f1
Tor Norbye <tnorbye@google.com>
2015-12-11 15:03:43 -0800

    Encourage updating Gradle plugin from 2.0 alpha2 to alpha3
    Change-Id: I6d3069685d6eafdacc52850d2c8d1119694ebe80

--- android/src/com/android/tools/idea/fd/FastDeployManager.java
+++ android/src/com/android/tools/idea/fd/FastDeployManager.java
@@ -1259,7 +1259,7 @@ public final class FastDeployManager implements ProjectComponent, BulkFileListen
             continue;
           }
 
-          if (modelVersion.equals("2.0.0-alpha1")) {
+          if (modelVersion.equals("2.0.0-alpha1") || modelVersion.equals("2.0.0-alpha2")) {
             showObsoleteWarning(project, modelVersion);
             break;
           }
