2e61b1ed0072f72ef1ee0d8e9ad1f6241cf30f9c
Tor Norbye <tnorbye@google.com>
2015-12-15 15:50:43 -0800

    Fix AndroidJavaDocRendererTest
    AndroidPsiUtils#getResourceReferenceType now requires elements
    to be resolvable. This was not the case for the various Android
    doc renderer tests which were referring to R symbols that are
    not part of the test. Added dummy R inner classes.
    
    Change-Id: Ic7e68e7f13c1a001c76f7e0633520fd4030558df

--- android/testData/javadoc/strings/Activity1.java
+++ android/testData/javadoc/strings/Activity1.java
@@ -8,4 +8,10 @@ public class MyActivity extends Activity {
   public void onCreate(Bundle savedInstanceState) {
     String text = getResources().getString(R.string.app_n<caret>ame);
   }
+
+  public static final class R {
+    public static final class string {
+      public static final int app_name = 0x7f0a000e;
+    }
+  }
 }
