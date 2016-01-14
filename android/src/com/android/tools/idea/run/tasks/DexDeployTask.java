9e22b8f070229a8264ecb05677d41f796287e237
Siva Velusamy <vsiva@google.com>
2016-01-14 11:11:34 -0800

    DexDeployTask: Remove unnecessary explicit launch of activity
    When a dex file is being pushed, we get a response indicating whether
    the activity needs to be launched or not. We had a small hack which then
    explicitly launched the activity from the deploy task.
    
    However, this is unnecessary since the DexDeploy task can only be
    created in the case of a cold swap, in which case we always do an
    activity launch anyway..
    
    Change-Id: I2ac55cb36fbf03642acdf7448bc0e368d04d385e

--- android/src/com/android/tools/idea/run/tasks/DexDeployTask.java
+++ android/src/com/android/tools/idea/run/tasks/DexDeployTask.java
@@ -21,20 +21,15 @@ import com.android.tools.idea.fd.InstantRunBuildInfo;
 import com.android.tools.idea.fd.InstantRunManager;
 import com.android.tools.idea.fd.InstantRunPushFailedException;
 import com.android.tools.idea.run.ConsolePrinter;
-import com.android.tools.idea.run.editor.DefaultActivityLaunch;
 import com.android.tools.idea.run.util.LaunchStatus;
 import org.jetbrains.android.facet.AndroidFacet;
 import org.jetbrains.annotations.NotNull;
 
-import java.io.IOException;
-
 public class DexDeployTask implements LaunchTask {
-  @NotNull private final String myPkgName;
   @NotNull private final AndroidFacet myFacet;
   @NotNull private final InstantRunBuildInfo myBuildInfo;
 
-  public DexDeployTask(@NotNull String pkgName, @NotNull AndroidFacet facet, @NotNull InstantRunBuildInfo buildInfo) {
-    myPkgName = pkgName;
+  public DexDeployTask(@NotNull AndroidFacet facet, @NotNull InstantRunBuildInfo buildInfo) {
     myFacet = facet;
     myBuildInfo = buildInfo;
   }
@@ -56,17 +51,9 @@ public class DexDeployTask implements LaunchTask {
         InstantRunManager.displayVerifierStatus(myFacet, myBuildInfo);
 
         InstantRunManager manager = InstantRunManager.get(myFacet.getModule().getProject());
-        boolean restart = manager.pushArtifacts(device, myFacet, UpdateMode.HOT_SWAP, myBuildInfo);
+        manager.pushArtifacts(device, myFacet, UpdateMode.HOT_SWAP, myBuildInfo);
         // Note that the above method will update the build id on the device
         // and the InstalledPatchCache, so we don't have to do it again.
-
-        if (restart) {
-          // Trigger an activity start/restart.
-          // TODO: Clean this up such that the DeployTask just specifies whether an activity
-          // launch is required afterwards.
-          LaunchTask launchTask = DefaultActivityLaunch.INSTANCE.createState().getLaunchTask(myPkgName, myFacet, false, "");
-          launchTask.perform(device, launchStatus, printer);
-        }
 
         return true;
       }
