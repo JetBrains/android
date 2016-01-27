7f09877bd1e032b68cb69f9a9cec9910e50d8547
Joshua Ganderson <jganderson@google.com>
2016-01-27 17:56:56 -0600

    Simplify side panel to merely retrieve a component for display.
    Removes much of the logic relating to tutorial content and structure
    in side panel. Further cleanup will occur later including removal of
    the pane's header such that child components can display their own
    content amongst other structural changes.
    
    Change-Id: I55cfd8d51d5b8dae2a244f570f64f1728fdcac08

--- android/src/com/android/tools/idea/structure/services/DeveloperServiceCreators.java
+++ android/src/com/android/tools/idea/structure/services/DeveloperServiceCreators.java
@@ -18,6 +18,8 @@ package com.android.tools.idea.structure.services;
 import com.intellij.openapi.extensions.ExtensionPointName;
 import org.jetbrains.annotations.NotNull;
 
+import javax.swing.*;
+import java.awt.*;
 import java.util.Collection;
 
 /**
@@ -27,11 +29,31 @@ public interface DeveloperServiceCreators {
   ExtensionPointName<DeveloperServiceCreators> EP_NAME =
     ExtensionPointName.create("com.android.tools.idea.structure.services.developerServiceCreators");
 
+  /**
+   * Retrieves {@code DeveloperServiceCreator} configurations for this
+   * grouping.  There is no strict requirement between what is included in this
+   * collection and the content rendered from {@code getPanel()}.
+   *
+   * @return Service creators.
+   */
   @NotNull
   Collection<? extends DeveloperServiceCreator> getCreators();
 
-  // TODO: Replace all of the following with parsed ServiceBundle implementation.
+  /**
+   * Get's the unique id of the bundle.
+   */
+  @NotNull
   String getBundleId();
-  String getBundleName();
-  String getBundleContentRoot();
+
+  /**
+   * Accessor to a custom root panel for the {@code DeveloperServiceSidePanel}.
+   *
+   * The component should typically use {@code BorderLayout}, filling the
+   * entire panel and try to match pre-existing patterns and styles as much as
+   * is possible. More formal guidelines are currently pending.
+   *
+   * @return the custom panel.
+   */
+  @NotNull
+  Component getPanel();
 }
