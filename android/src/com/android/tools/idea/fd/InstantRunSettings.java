9346cf76e875513e76ad2db84c3a53878c2578c5
Tor Norbye <tnorbye@google.com>
2016-01-28 16:32:04 -0800

    Add ability to customize Instant Run behavior by via system property
    If you set instant.run.expert.settings=true you can tweak coldswap
    behavior. Expert setting (hidden by default) we can use for field
    diagnosis.
    
    Change-Id: I94301e445fd4e1358035aafb7a33d1229b36de2c

--- android/src/com/android/tools/idea/fd/InstantRunSettings.java
+++ android/src/com/android/tools/idea/fd/InstantRunSettings.java
@@ -21,6 +21,8 @@ import com.intellij.openapi.project.Project;
 import org.jetbrains.annotations.NotNull;
 
 public class InstantRunSettings {
+  public static final boolean SHOW_EXPERT_OPTIONS = Boolean.getBoolean("instant.run.expert.settings");
+
   /**
    * Returns whether instant run is enabled in the given project.
    * Note: Even if instant run is enabled for the project, instant run related information should not be accessed
@@ -41,5 +43,61 @@ public class InstantRunSettings {
   public static boolean isRestartActivity(@NotNull Project project) {
     AndroidGradleBuildConfiguration buildConfiguration = AndroidGradleBuildConfiguration.getInstance(project);
     return buildConfiguration.RESTART_ACTIVITY;
+  }
+
+  /** If instant run is enabled, is cold swap mode enabled? */
+  public static boolean isColdSwapEnabled(@NotNull Project project) {
+    if (!SHOW_EXPERT_OPTIONS) {
+      // Don't keep using stored settings on disk if the settings are invisible.
+      // If users temporarily tried the flags, we don't want to keep those even
+      // after they've gone back.
+      return true;
+    }
+    AndroidGradleBuildConfiguration buildConfiguration = AndroidGradleBuildConfiguration.getInstance(project);
+    return buildConfiguration.COLD_SWAP;
+  }
+
+  /** If instant run is enabled, is cold swap mode enabled? */
+  @NotNull
+  public static ColdSwapMode getColdSwapMode(@NotNull Project project) {
+    AndroidGradleBuildConfiguration buildConfiguration = AndroidGradleBuildConfiguration.getInstance(project);
+    String value = buildConfiguration.COLD_SWAP_MODE;
+    if (value == null) {
+      return ColdSwapMode.DEFAULT;
+    }
+    return ColdSwapMode.fromValue(value, ColdSwapMode.DEFAULT);
+  }
+
+  /** Setting passed to Gradle indicating the scheme to be used for coldswap handling */
+  @SuppressWarnings("SpellCheckingInspection")
+  public enum ColdSwapMode {
+    DEFAULT("Default", "default"),
+    MULTI_APK("APK Splits", "multiapk"),
+    MULTI_DEX("Multidex", "multidex"),
+    NATIVE("Native", "native");
+
+    ColdSwapMode(String display, String value) {
+      this.display = display;
+      this.value = value;
+    }
+
+    @NotNull
+    public static ColdSwapMode fromValue(@NotNull String v, @NotNull ColdSwapMode def) {
+      for (ColdSwapMode mode : values()) {
+        if (mode.value.equals(v)) {
+          return mode;
+        }
+      }
+
+      return def;
+    }
+
+    public final String display;
+    public final String value;
+
+    @Override
+    public String toString() {
+      return display;
+    }
   }
 }
