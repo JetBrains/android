701e23b3e3b8c99de116b94e06bea164d0e8f875
Alex Ruiz <alruiz@google.com>
2016-02-01 14:00:22 -0800

    [New PSD] Implemented artifact repository search.
    This CL implements artifact repository search for:
    - Maven Central
    - Android SDK
    
    Future implementations will include JCenter and
    file system.
    
    Change-Id: Id1a6df081765e7cc2dacee014710bcd73cd7cd51

---
+++ android/src/com/android/tools/idea/gradle/structure/model/repositories/search/SearchRequest.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.gradle.structure.model.repositories.search;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class SearchRequest {
+  @NotNull private final String myArtifactName;
+
+  @Nullable private final String myGroupId;
+
+  private final int myRowCount;
+  private final int myStart;
+
+  /**
+   * Creates a new {@link SearchRequest}.
+   *
+   * @param artifactName the name of the artifact to look for.
+   * @param groupId      the group ID of the artifact (optional.)
+   * @param rowCount     number of rows to retrieve.
+   * @param start        start zero-based starting position of the search (useful when paginating search results.)
+   */
+  public SearchRequest(@NotNull String artifactName, @Nullable String groupId, int rowCount, int start) {
+    myArtifactName = artifactName;
+    myGroupId = groupId;
+    myRowCount = rowCount;
+    myStart = start;
+  }
+
+  @NotNull
+  String getArtifactName() {
+    return myArtifactName;
+  }
+
+  @Nullable
+  String getGroupId() {
+    return myGroupId;
+  }
+
+  int getRowCount() {
+    return myRowCount;
+  }
+
+  int getStart() {
+    return myStart;
+  }
+}
