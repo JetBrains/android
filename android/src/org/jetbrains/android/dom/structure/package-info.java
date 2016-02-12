3f29026a2bf7e909d6869320270947fffb896b9a
Andrew Shulaev <ddrone@google.com>
2016-02-12 12:36:43 +0000

    200525: Add a structure view for layout files
    Change-Id: Id0788e424956554f657900f8acc14f11a3aa111d

---
+++ android/src/org/jetbrains/android/dom/structure/package-info.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Classes that implement "Structure View" for different types of files available
+ * for editing in Android Studio.
+ *
+ * Can be invoked as a pop-up with Ctrl+F12 or as a sidebar with Alt+7 in
+ * default Android Studio keybindings.
+ */
+package org.jetbrains.android.dom.structure;
