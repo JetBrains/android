65e6e0398e2e944cd68bbce157e58adf41ddfd04
Deepanshu Gupta <deepanshu@google.com>
2016-02-17 11:24:30 -0800

    DataBinding Lambdas
    Change-Id: I51d10f2fa2facfca7d7b42a25a47c41a4612a71b

--- android/gen/com/android/tools/idea/lang/databinding/psi/PsiDbLambdaParameters.java
+++ android/gen/com/android/tools/idea/lang/databinding/psi/PsiDbLambdaParameters.java
@@ -5,9 +5,9 @@ import java.util.List;
 import org.jetbrains.annotations.*;
 import com.intellij.psi.PsiElement;
 
-public interface PsiDbClassExtractionExpr extends PsiDbExpr {
+public interface PsiDbLambdaParameters extends PsiElement {
 
-  @NotNull
-  PsiDbType getType();
+  @Nullable
+  PsiDbInferredFormalParameterList getInferredFormalParameterList();
 
 }
