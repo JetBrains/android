5d556eade0c0adb5a92faf96e1d9ba038457ab7c
Tor Norbye <tnorbye@google.com>
2016-02-18 08:27:59 -0800

    Update Manifest code completion tests to handle N preview
    Change-Id: Id6aa0a08a7b8977e2eba066dce0739367a76646b

--- android/testSrc/org/jetbrains/android/dom/AndroidManifestDomTest.java
+++ android/testSrc/org/jetbrains/android/dom/AndroidManifestDomTest.java
@@ -460,7 +460,7 @@ public class AndroidManifestDomTest extends AndroidDomTest {
     });
     try {
       doTestCompletionVariants(getTestName(true) + ".xml", "1", "2", "3", "4", "5", "6", "7",
-                               "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23");
+                               "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "N");
     }
     finally {
       ApplicationManager.getApplication().runWriteAction(new Runnable() {
