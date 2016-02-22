65cc328c1d6722863f50456c240d3b7d5644186a
Jimmy Lopez Portillo <jlopezportillo@google.com>
2016-02-22 12:34:47 -0800

    Clean up AvdWizardUtils Key instances
    With the AVD Wizard rewrite, a lot of the keys in AvdWizardUtils were
    only kept around for their name. In this CL, we remove the keys
    entirely, replacing them with just their names.
    
    Change-Id: Ib2dfff830c1b4745d2207a8ca8823e6a4cde0c97

--- android/src/com/android/tools/idea/avdmanager/AvdWizardUtils.java
+++ android/src/com/android/tools/idea/avdmanager/AvdWizardUtils.java
@@ -17,16 +17,11 @@ package com.android.tools.idea.avdmanager;
 
 import com.android.repository.io.FileOp;
 import com.android.repository.io.FileOpUtils;
-import com.android.resources.Navigation;
-import com.android.resources.ScreenOrientation;
-import com.android.sdklib.devices.Device;
 import com.android.sdklib.devices.Hardware;
-import com.android.sdklib.devices.Screen;
 import com.android.sdklib.devices.Storage;
 import com.android.sdklib.devices.Storage.Unit;
 import com.android.sdklib.internal.avd.AvdInfo;
 import com.android.sdklib.internal.avd.AvdManager;
-import com.android.sdklib.internal.avd.GpuMode;
 import com.android.sdklib.internal.avd.HardwareProperties;
 import com.android.sdklib.repositoryv2.IdDisplay;
 import com.android.sdklib.repositoryv2.targets.SystemImage;
@@ -50,11 +45,6 @@ import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
-import static com.android.tools.idea.wizard.dynamic.ScopedStateStore.Key;
-import static com.android.tools.idea.wizard.dynamic.ScopedStateStore.Scope.STEP;
-import static com.android.tools.idea.wizard.dynamic.ScopedStateStore.Scope.WIZARD;
-import static com.android.tools.idea.wizard.dynamic.ScopedStateStore.createKey;
-
 /**
  * State store keys for the AVD Manager wizards
  */
@@ -63,81 +53,53 @@ public class AvdWizardUtils {
   public static final String WIZARD_ONLY = "AvdManager.WizardOnly.";
 
   // Avd option keys
-  public static final Key<Device> DEVICE_DEFINITION_KEY = createKey(WIZARD_ONLY + "DeviceDefinition", WIZARD, Device.class);
-  public static final Key<SystemImageDescription> SYSTEM_IMAGE_KEY = createKey(WIZARD_ONLY + "SystemImage", WIZARD, SystemImageDescription.class);
+  public static final String DEVICE_DEFINITION_KEY = WIZARD_ONLY + "DeviceDefinition";
+  public static final String SYSTEM_IMAGE_KEY = WIZARD_ONLY + "SystemImage";
 
-  public static final Key<Storage> RAM_STORAGE_KEY = createKey(AvdManager.AVD_INI_RAM_SIZE, WIZARD, Storage.class);
-  public static final Key<Storage> VM_HEAP_STORAGE_KEY = createKey(AvdManager.AVD_INI_VM_HEAP_SIZE, WIZARD, Storage.class);
-  public static final Key<Storage> INTERNAL_STORAGE_KEY = createKey(AvdManager.AVD_INI_DATA_PARTITION_SIZE, WIZARD, Storage.class);
-  public static final Key<Storage> SD_CARD_STORAGE_KEY = createKey(AvdManager.AVD_INI_SDCARD_SIZE, WIZARD, Storage.class);
-  public static final Key<String> EXISTING_SD_LOCATION = createKey(AvdManager.AVD_INI_SDCARD_PATH, WIZARD, String.class);
+  public static final String RAM_STORAGE_KEY = AvdManager.AVD_INI_RAM_SIZE;
+  public static final String VM_HEAP_STORAGE_KEY = AvdManager.AVD_INI_VM_HEAP_SIZE;
+  public static final String INTERNAL_STORAGE_KEY = AvdManager.AVD_INI_DATA_PARTITION_SIZE;
+  public static final String SD_CARD_STORAGE_KEY = AvdManager.AVD_INI_SDCARD_SIZE;
+  public static final String EXISTING_SD_LOCATION = AvdManager.AVD_INI_SDCARD_PATH;
 
   // Keys used for display properties within the wizard. The values are derived from (and used to derive) the values for
   // SD_CARD_STORAGE_KEY and EXISTING_SD_LOCATION
-  public static final Key<Storage> DISPLAY_SD_SIZE_KEY = createKey(WIZARD_ONLY + "displaySdCardSize", WIZARD, Storage.class);
-  public static final Key<String> DISPLAY_SD_LOCATION_KEY = createKey(WIZARD_ONLY + "displaySdLocation", WIZARD, String.class);
-  public static final Key<Boolean> DISPLAY_USE_EXTERNAL_SD_KEY = createKey(WIZARD_ONLY + "displayUseExistingSd", WIZARD, Boolean.class);
+  public static final String DISPLAY_SD_SIZE_KEY = WIZARD_ONLY + "displaySdCardSize";
+  public static final String DISPLAY_SD_LOCATION_KEY = WIZARD_ONLY + "displaySdLocation";
+  public static final String DISPLAY_USE_EXTERNAL_SD_KEY = WIZARD_ONLY + "displayUseExistingSd";
 
   public static final String AVD_INI_SCALE_FACTOR = "runtime.scalefactor";
-  public static final Key<AvdScaleFactor> SCALE_SELECTION_KEY = createKey(AVD_INI_SCALE_FACTOR, WIZARD, AvdScaleFactor.class);
+  public static final String SCALE_SELECTION_KEY = AVD_INI_SCALE_FACTOR;
 
-  public static final Key<ScreenOrientation> DEFAULT_ORIENTATION_KEY = createKey(WIZARD_ONLY + "DefaultOrientation", WIZARD, ScreenOrientation.class);
+  public static final String DEFAULT_ORIENTATION_KEY = WIZARD_ONLY + "DefaultOrientation";
 
   public static final String AVD_INI_NETWORK_SPEED = "runtime.network.speed";
-  public static final Key<String> NETWORK_SPEED_KEY = createKey(AVD_INI_NETWORK_SPEED, WIZARD, String.class);
+  public static final String NETWORK_SPEED_KEY = AVD_INI_NETWORK_SPEED;
   public static final String AVD_INI_NETWORK_LATENCY = "runtime.network.latency";
-  public static final Key<String> NETWORK_LATENCY_KEY = createKey(AVD_INI_NETWORK_LATENCY, WIZARD, String.class);
+  public static final String NETWORK_LATENCY_KEY = AVD_INI_NETWORK_LATENCY;
 
-  public static final Key<String> FRONT_CAMERA_KEY = createKey(AvdManager.AVD_INI_CAMERA_FRONT, WIZARD, String.class);
-  public static final Key<String> BACK_CAMERA_KEY = createKey(AvdManager.AVD_INI_CAMERA_BACK, WIZARD, String.class);
-  public static final String CHOOSE_DEVICE_DEFINITION_STEP = "Choose Device Definition Step";
-  public static final String CHOOSE_SYSTEM_IMAGE_STEP = "Choose System Image Step";
+  public static final String FRONT_CAMERA_KEY = AvdManager.AVD_INI_CAMERA_FRONT;
+  public static final String BACK_CAMERA_KEY = AvdManager.AVD_INI_CAMERA_BACK;
 
-  public static final Key<Boolean> USE_HOST_GPU_KEY = createKey(AvdManager.AVD_INI_GPU_EMULATION, WIZARD, Boolean.class);
-  public static final Key<GpuMode> HOST_GPU_MODE_KEY = createKey(AvdManager.AVD_INI_GPU_MODE, WIZARD, GpuMode.class);
+  public static final String USE_HOST_GPU_KEY = AvdManager.AVD_INI_GPU_EMULATION;
+  public static final String HOST_GPU_MODE_KEY = AvdManager.AVD_INI_GPU_MODE;
 
-  public static final Key<Boolean> IS_IN_EDIT_MODE_KEY = createKey(WIZARD_ONLY + "isInEditMode", WIZARD, Boolean.class);
+  public static final String IS_IN_EDIT_MODE_KEY = WIZARD_ONLY + "isInEditMode";
 
-  public static final Key<File> DISPLAY_SKIN_FILE_KEY = createKey("displaySkinPath", STEP, File.class);
-  public static final Key<File> CUSTOM_SKIN_FILE_KEY = createKey(AvdManager.AVD_INI_SKIN_PATH, WIZARD, File.class);
-  public static final Key<File> BACKUP_SKIN_FILE_KEY = createKey(AvdManager.AVD_INI_BACKUP_SKIN_PATH, WIZARD, File.class);
-  public static final Key<Boolean> DEVICE_FRAME_KEY = createKey("showDeviceFrame", STEP, Boolean.class);
+  public static final String CUSTOM_SKIN_FILE_KEY = AvdManager.AVD_INI_SKIN_PATH;
+  public static final String BACKUP_SKIN_FILE_KEY = AvdManager.AVD_INI_BACKUP_SKIN_PATH;
+  public static final String DEVICE_FRAME_KEY = "showDeviceFrame";
 
-  public static final Key<String> DISPLAY_NAME_KEY = createKey(AvdManagerConnection.AVD_INI_DISPLAY_NAME, WIZARD, String.class);
+  public static final String DISPLAY_NAME_KEY = AvdManagerConnection.AVD_INI_DISPLAY_NAME;
   public static final String AVD_INI_AVD_ID = "AvdId";
-  public static final Key<String> AVD_ID_KEY = createKey(AVD_INI_AVD_ID, WIZARD, String.class);
+  public static final String AVD_ID_KEY = AVD_INI_AVD_ID;
 
-  public static final Key<Boolean> RANCHU_KEY = createKey(WIZARD_ONLY + "ranchu.emulator", WIZARD, Boolean.class);
-  public static final Key<Integer> CPU_CORES_KEY = createKey(AvdManager.AVD_INI_CPU_CORES, WIZARD, Integer.class);
+  public static final String RANCHU_KEY = WIZARD_ONLY + "ranchu.emulator";
+  public static final String CPU_CORES_KEY = AvdManager.AVD_INI_CPU_CORES;
 
   // Device definition keys
 
-  public static final Key<String> DEVICE_NAME_KEY = createKey("DeviceName", STEP, String.class);
-
-  public static final Key<Double> DIAGONAL_SCREENSIZE_KEY = createKey("DiagonalScreenSize", STEP, Double.class);
-  public static final Key<Integer> RESOLUTION_WIDTH_KEY = createKey("ResolutionWidth", STEP, Integer.class);
-  public static final Key<Integer> RESOLUTION_HEIGHT_KEY = createKey("ResolutionHeight", STEP, Integer.class);
-
-  public static final Key<Boolean> HAS_HARDWARE_BUTTONS_KEY = createKey("HasHardwareButtons", STEP, Boolean.class);
-  public static final Key<Boolean> HAS_HARDWARE_KEYBOARD_KEY = createKey(HardwareProperties.HW_KEYBOARD, WIZARD, Boolean.class);
-  public static final Key<Navigation> NAVIGATION_KEY = createKey("Navigation", STEP, Navigation.class);
-
-  public static final Key<Boolean> SUPPORTS_LANDSCAPE_KEY = createKey("SupportsLandscape", STEP, Boolean.class);
-  public static final Key<Boolean> SUPPORTS_PORTRAIT_KEY = createKey("SupportsPortrait", STEP, Boolean.class);
-
-  public static final Key<Boolean> HAS_BACK_CAMERA_KEY = createKey("HasBackCamera", STEP, Boolean.class);
-  public static final Key<Boolean> HAS_FRONT_CAMERA_KEY = createKey("HasFrontCamera", STEP, Boolean.class);
-
-  public static final Key<Boolean> HAS_ACCELEROMETER_KEY = createKey("HasAccelerometer", STEP, Boolean.class);
-  public static final Key<Boolean> HAS_GYROSCOPE_KEY = createKey("HasGyroscope", STEP, Boolean.class);
-  public static final Key<Boolean> HAS_GPS_KEY = createKey("HasGPS", STEP, Boolean.class);
-  public static final Key<Boolean> HAS_PROXIMITY_SENSOR_KEY = createKey("HasProximitySensor", STEP, Boolean.class);
-
-  public static final Key<Screen> WIP_SCREEN_KEY = createKey("ScreenUnderConstruction", STEP, Screen.class);
-  public static final Key<Hardware> WIP_HARDWARE_KEY = createKey("HardwareUnderConstruction" ,STEP, Hardware.class);
-  public static final Key<Double> WIP_SCREEN_DPI_KEY = createKey("ScreenDPI", STEP, Double.class);
-
-  public static final Key<IdDisplay> TAG_ID_KEY = createKey("TagId", STEP, IdDisplay.class);
+  public static final String HAS_HARDWARE_KEYBOARD_KEY = HardwareProperties.HW_KEYBOARD;
 
   // Defaults
   public static final AvdScaleFactor DEFAULT_SCALE = AvdScaleFactor.AUTO;
