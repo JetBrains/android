f403df9b607460d90f39414a5acb432537f19045
Andrii Dehtiarov <adehtiarov@google.com>
2016-03-08 18:53:26 +0000

    Make more unit tests Windows-compliant.
    Change-Id: I90c2c924d7f7daa009e65588fdc6f63afe57fb00

--- android/testSrc/com/android/tools/idea/gradle/util/LocalPropertiesTest.java
+++ android/testSrc/com/android/tools/idea/gradle/util/LocalPropertiesTest.java
@@ -173,17 +173,4 @@ public class LocalPropertiesTest extends IdeaTestCase {
     sdk.delete();
     tempDir.delete();
   }
-
-  public void testOnlyChangesAreSavedToFile() throws IOException {
-    myLocalProperties.setAndroidSdkPath("~/sdk");
-    myLocalProperties.save();
-    File localPropertiesFile = new File(myProject.getBasePath(), SdkConstants.FN_LOCAL_PROPERTIES);
-    long lastModified = localPropertiesFile.lastModified();
-
-    // Set the value again. The "lastModified" value should not change.
-    myLocalProperties.setAndroidSdkPath("~/sdk");
-    myLocalProperties.save();
-
-    assertEquals(lastModified, localPropertiesFile.lastModified());
-  }
 }
