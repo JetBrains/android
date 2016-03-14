27132a3f29cb2578c62e7e74719a1ac43cd28ff6
Andrew Shulaev <ddrone@google.com>
2016-03-14 11:42:49 +0000

    Add support for <data> tag inside <intent-filter>
    <intent-filter> tag can contain <data> subtag, according to documentation:
    http://developer.android.com/guide/topics/manifest/intent-filter-element.html
    
    This CL adds support for tag completion, attribute completion for
    <data> tags already due to availability of styleable definition.
    
    Change-Id: Ia8e48ed96ccb71584bc9681cec407131986bc6bd

--- android/src/org/jetbrains/android/dom/manifest/Data.java
+++ android/src/org/jetbrains/android/dom/manifest/Data.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2000-2010 JetBrains s.r.o.
+ * Copyright (C) 2016 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,19 +15,11 @@
  */
 package org.jetbrains.android.dom.manifest;
 
-import com.intellij.util.xml.SubTagList;
-
-import java.util.List;
-
-public interface IntentFilter extends ManifestElement {
-  List<Action> getActions();
-
-  List<Category> getCategories();
-
-  @SubTagList("data")
-  List<Data> getDataElements();
-
-  Action addAction();
-
-  Category addCategory();
+/**
+ * http://developer.android.com/guide/topics/manifest/data-element.html
+ * <p/>
+ * Attribute completion provided by AndroidManifestData styleable and
+ * {@link AndroidManifestUtils#getStyleableNameByTagName(String)}
+ */
+public interface Data extends ManifestElement {
 }
