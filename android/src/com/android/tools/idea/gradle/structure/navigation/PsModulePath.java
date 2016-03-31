1d3cdc74cc82fa56cfe88795b1e96fa200a93253
Alex Ruiz <alruiz@google.com>
2016-03-31 11:20:41 -0700

    [New PSD] Now modules with issues are underlined.
    Change-Id: I0225f9ef3d72e2195d20b34af2c2ce95549a7de0

---
+++ android/src/com/android/tools/idea/gradle/structure/navigation/PsModulePath.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.gradle.structure.navigation;
+
+import com.android.tools.idea.gradle.structure.model.PsModule;
+import com.google.common.base.Objects;
+import org.jetbrains.annotations.NotNull;
+
+public class PsModulePath extends PsNavigationPath {
+  @NotNull private final String myModuleName;
+
+  public PsModulePath(@NotNull PsModule module) {
+    myModuleName = module.getName();
+  }
+
+  @NotNull
+  public String getModuleName() {
+    return myModuleName;
+  }
+
+  @Override
+  @NotNull
+  public String toHtml() {
+    return "";
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    PsModulePath that = (PsModulePath)o;
+    return Objects.equal(myModuleName, that.myModuleName);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hashCode(myModuleName);
+  }
+
+  @Override
+  public String toString() {
+    return myModuleName;
+  }
+}
