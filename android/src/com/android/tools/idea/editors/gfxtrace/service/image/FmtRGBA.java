8ffc4fb10e8273e83581916ada082b6d6b7e57e7
Pascal Muetschard <pmuetschard@google.com>
2016-04-01 14:54:16 -0700

    Generated code for GL_HALF_FLOAT texture support.
    Change-Id: I564d52bf9e0336d03d3f79d950fdea6c8ffbf9d6

--- android/src/com/android/tools/idea/editors/gfxtrace/service/image/FmtRGBA.java
+++ android/src/com/android/tools/idea/editors/gfxtrace/service/image/FmtRGBA.java
@@ -23,7 +23,7 @@ import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
 
-final public class FmtRGBA extends Format {
+final class FmtRGBA extends Format {
   //<<<Start:Java.ClassBody:1>>>
 
   // Constructs a default-initialized {@link FmtRGBA}.
