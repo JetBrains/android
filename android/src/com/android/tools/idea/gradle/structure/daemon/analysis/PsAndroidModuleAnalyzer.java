3ef8c910366fb8b519d444b2aee654250a363c27
Alex Ruiz <alruiz@google.com>
2016-04-19 09:32:02 -0700

    [New PSD] Now sync issues are displayed.
    With this CL, the new "Project Structure" dialog
    displays sync errors, with the proper severity.
    
    This CL only links sync issues to library
    dependencies. It also converts URLs to hyperlinks.
    
    Change-Id: I90c84507c203ef1cedf1e951499f98ef5fe95846

--- android/src/com/android/tools/idea/gradle/structure/daemon/analysis/PsAndroidModuleAnalyzer.java
+++ android/src/com/android/tools/idea/gradle/structure/daemon/analysis/PsAndroidModuleAnalyzer.java
@@ -15,19 +15,31 @@
  */
 package com.android.tools.idea.gradle.structure.daemon.analysis;
 
+import com.android.builder.model.SyncIssue;
+import com.android.tools.idea.gradle.AndroidGradleModel;
 import com.android.tools.idea.gradle.structure.configurables.PsContext;
 import com.android.tools.idea.gradle.structure.model.PsArtifactDependencySpec;
 import com.android.tools.idea.gradle.structure.model.PsIssue;
 import com.android.tools.idea.gradle.structure.model.PsIssueCollection;
-import com.android.tools.idea.gradle.structure.model.android.PsAndroidModule;
 import com.android.tools.idea.gradle.structure.model.android.PsAndroidLibraryDependency;
+import com.android.tools.idea.gradle.structure.model.android.PsAndroidModule;
 import com.android.tools.idea.gradle.structure.navigation.PsLibraryDependencyPath;
 import com.android.tools.idea.gradle.structure.navigation.PsModulePath;
 import com.android.tools.idea.gradle.structure.navigation.PsNavigationPath;
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.Multimap;
 import org.jetbrains.annotations.NotNull;
 
-import static com.android.tools.idea.gradle.structure.model.PsIssue.Type.INFO;
-import static com.android.tools.idea.gradle.structure.model.PsIssue.Type.WARNING;
+import java.util.Collection;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static com.android.SdkConstants.GRADLE_PATH_SEPARATOR;
+import static com.android.builder.model.SyncIssue.SEVERITY_ERROR;
+import static com.android.builder.model.SyncIssue.SEVERITY_WARNING;
+import static com.android.tools.idea.gradle.structure.model.PsIssue.Type.*;
+import static com.google.common.base.Strings.nullToEmpty;
 
 public class PsAndroidModuleAnalyzer extends PsModelAnalyzer<PsAndroidModule> {
   @NotNull private final PsContext myContext;
@@ -38,11 +50,28 @@ public class PsAndroidModuleAnalyzer extends PsModelAnalyzer<PsAndroidModule> {
 
   @Override
   protected void doAnalyze(@NotNull PsAndroidModule module, @NotNull PsIssueCollection issueCollection) {
+    Multimap<String, SyncIssue> issuesByData = ArrayListMultimap.create();
+    AndroidGradleModel gradleModel = module.getGradleModel();
+    Collection<SyncIssue> syncIssues = gradleModel.getAndroidProject().getSyncIssues();
+    for (SyncIssue syncIssue : syncIssues) {
+      String data = nullToEmpty(syncIssue.getData());
+      issuesByData.put(data, syncIssue);
+    }
+
     PsModulePath modulePath = new PsModulePath(module);
     module.forEachDependency(dependency -> {
       if (dependency instanceof PsAndroidLibraryDependency && dependency.isDeclared()) {
         PsAndroidLibraryDependency libraryDependency = (PsAndroidLibraryDependency)dependency;
         PsNavigationPath path = new PsLibraryDependencyPath(myContext, libraryDependency);
+
+        PsArtifactDependencySpec resolvedSpec = libraryDependency.getResolvedSpec();
+        String issueKey = resolvedSpec.group + GRADLE_PATH_SEPARATOR + resolvedSpec.name;
+        Collection<SyncIssue> librarySyncIssues = issuesByData.get(issueKey);
+        for (SyncIssue syncIssue : librarySyncIssues) {
+          PsIssue issue = createIssueFrom(syncIssue, path);
+          issue.setExtraPath(modulePath);
+          issueCollection.add(issue);
+        }
 
         PsArtifactDependencySpec declaredSpec = libraryDependency.getDeclaredSpec();
         assert declaredSpec != null;
@@ -60,7 +89,6 @@ public class PsAndroidModuleAnalyzer extends PsModelAnalyzer<PsAndroidModule> {
         }
 
         if (libraryDependency.hasPromotedVersion()) {
-          PsArtifactDependencySpec resolvedSpec = libraryDependency.getResolvedSpec();
           String message = "Gradle promoted library version from " + declaredVersion + " to " + resolvedSpec.version;
           String description = "To resolve version conflicts, Gradle by default uses the newest version of a dependency. " +
                                "<a href='https://docs.gradle.org/current/userguide/dependency_management.html'>Open Gradle " +
@@ -71,6 +99,34 @@ public class PsAndroidModuleAnalyzer extends PsModelAnalyzer<PsAndroidModule> {
         }
       }
     });
+  }
+
+  @VisibleForTesting
+  @NotNull
+  static PsIssue createIssueFrom(@NotNull SyncIssue issue, @NotNull PsNavigationPath path) {
+    String message = issue.getMessage();
+    Pattern pattern = Pattern.compile("\\(?http://[-A-Za-z0-9+&@#/%?=~_()|!:,.;]*[-A-Za-z0-9+&@#/%=~_()|]");
+    Matcher matcher = pattern.matcher(message);
+    boolean result = matcher.find();
+    // Replace URLs with <a href='url'>url</a>.
+    while (result) {
+      String url = matcher.group();
+      message = message.replace(url, "<a href='" + url + "'>" + url + "</a>");
+      result = matcher.find();
+    }
+    return new PsIssue(message, path, getType(issue));
+  }
+
+  @NotNull
+  private static PsIssue.Type getType(@NotNull SyncIssue issue) {
+    int severity = issue.getSeverity();
+    switch (severity) {
+      case SEVERITY_ERROR:
+        return ERROR;
+      case SEVERITY_WARNING:
+        return WARNING;
+    }
+    return INFO;
   }
 
   @Override
---
+++ android/testSrc/com/android/tools/idea/gradle/structure/daemon/analysis/PsAndroidModuleAnalyzerTest.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.gradle.structure.daemon.analysis;
+
+import com.android.builder.model.SyncIssue;
+import com.android.tools.idea.gradle.structure.model.PsIssue;
+import com.android.tools.idea.gradle.structure.navigation.PsNavigationPath;
+import org.junit.Test;
+
+import static com.android.builder.model.SyncIssue.SEVERITY_ERROR;
+import static com.android.tools.idea.gradle.structure.model.PsIssue.Type.ERROR;
+import static org.fest.assertions.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ * Tests for {@link PsAndroidModuleAnalyzer}.
+ */
+public class PsAndroidModuleAnalyzerTest {
+  @Test
+  public void createIssueFrom() {
+    SyncIssue syncIssue = mock(SyncIssue.class);
+    when(syncIssue.getMessage()).thenReturn("Conflict with dependency 'com.google.guava:guava'. Resolved versions for app (16.0) " +
+                                            "and test app (16.0.1) differ. See http://g.co/androidstudio/app-test-app-conflict for " +
+                                            "details.");
+    when(syncIssue.getSeverity()).thenReturn(SEVERITY_ERROR);
+    PsNavigationPath path = mock(PsNavigationPath.class);
+
+    PsIssue issue = PsAndroidModuleAnalyzer.createIssueFrom(syncIssue, path);
+    assertThat(issue.getText()).isEqualTo("Conflict with dependency 'com.google.guava:guava'. Resolved versions for app (16.0) " +
+                                          "and test app (16.0.1) differ. See " +
+                                          "<a href='http://g.co/androidstudio/app-test-app-conflict'>http://g.co/androidstudio/app-test-app-conflict</a> " +
+                                          "for details.");
+    assertThat(issue.getType()).isEqualTo(ERROR);
+  }
+
+}
