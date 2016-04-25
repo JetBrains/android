927368a4ef88d2d119f9f37b3d577c517f5b80ab
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2016-04-25 19:04:11 +0200

    [platform] hides inactive update channel from the UI (IDEA-154552)

--- adt-branding/src/com/android/tools/idea/AndroidStudioUpdateStrategyCustomization.java
+++ adt-branding/src/com/android/tools/idea/AndroidStudioUpdateStrategyCustomization.java
@@ -1,6 +1,8 @@
 package com.android.tools.idea;
 
 import com.intellij.openapi.updateSettings.UpdateStrategyCustomization;
+import com.intellij.openapi.updateSettings.impl.ChannelStatus;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * @author nik
@@ -9,5 +11,10 @@ public class AndroidStudioUpdateStrategyCustomization extends UpdateStrategyCust
   @Override
   public boolean forceEapUpdateChannelForEapBuilds() {
     return false;
+  }
+
+  @Override
+  public boolean isChannelActive(@NotNull ChannelStatus channel) {
+    return true;
   }
 }
