1966f38b48169d35b8583ad061d1cbc2dac44b94
Madiyar Aitbayev <madiyar@google.com>
2016-05-09 17:47:50 +0100

    Shows logs in debug console on debug mode
    Change-Id: I2174d66d5c506483d025c206c954048d334f5941

---
+++ android/src/com/android/tools/idea/run/ApplicationLogListener.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.run;
+
+import com.android.ddmlib.logcat.LogCatHeader;
+import com.android.ddmlib.logcat.LogCatMessage;
+import com.android.tools.idea.logcat.AndroidLogcatService;
+import com.android.tools.idea.logcat.AndroidLogcatUtils;
+import com.google.common.base.Strings;
+import com.intellij.openapi.util.Key;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * A logcat listener base class used by the run and debug console windows
+ */
+public abstract class ApplicationLogListener implements AndroidLogcatService.LogLineListener {
+  @Nullable private LogCatHeader myActiveHeader;
+  @NotNull private String myApplicationId;
+
+  public ApplicationLogListener(@NotNull String applicationId) {
+    myApplicationId = applicationId;
+  }
+
+  @Override
+  public void receiveLogLine(@NotNull LogCatMessage line) {
+    if (!line.getHeader().getAppName().equals(myApplicationId)) {
+      myActiveHeader = null;
+      return;
+    }
+
+    String message;
+    if (!line.getHeader().equals(myActiveHeader)) {
+      myActiveHeader = line.getHeader();
+      message = formatLogLine(line);
+    } else {
+      message = Strings.repeat(" ", formatLogLine(line).indexOf(line.getMessage())) + line.getMessage();
+    }
+
+    Key key = AndroidLogcatUtils.getProcessOutputType(myActiveHeader.getLogLevel());
+    notifyTextAvailable(message + "\n", key);
+  }
+
+  protected abstract String formatLogLine(@NotNull LogCatMessage line);
+
+  protected abstract void notifyTextAvailable(@NotNull String message, @NotNull Key key);
+}
