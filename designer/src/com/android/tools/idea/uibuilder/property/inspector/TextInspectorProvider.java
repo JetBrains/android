822399872fc28c6d8122827fdc6ad72c04a4cae2
Jens Ole Lauridsen <jlauridsen@google.com>
2016-05-12 15:59:51 -0700

    Minor property changes including hardcoding exceptions.
    This CL contains:
    - Removal of app:paddingStart, app:paddingEnd, app:theme
    - Addition of android:popupBackground on AutoCompleteTextView
    - Make layout_width and layout_height a dropdown editor not edit
    - Remove fill_parent from the above dropdowns.
    - Move hint from TextView to EditText
    
    Change-Id: Iaef6578b20a6677a2b909027fb37c6a047ec3e63

--- designer/src/com/android/tools/idea/uibuilder/property/inspector/TextInspectorProvider.java
+++ designer/src/com/android/tools/idea/uibuilder/property/inspector/TextInspectorProvider.java
@@ -40,7 +40,7 @@ import static com.android.SdkConstants.*;
 import static com.android.tools.idea.uibuilder.property.editors.NlEditingListener.DEFAULT_LISTENER;
 
 public class TextInspectorProvider implements InspectorProvider {
-  private static final Set<String> TEXT_PROPERTIES = ImmutableSet.of(ATTR_TEXT, ATTR_HINT, ATTR_CONTENT_DESCRIPTION);
+  private static final Set<String> TEXT_PROPERTIES = ImmutableSet.of(ATTR_TEXT, ATTR_CONTENT_DESCRIPTION);
 
   private TextInspectorComponent myComponent;
 
@@ -67,7 +67,6 @@ public class TextInspectorProvider implements InspectorProvider {
   private static class TextInspectorComponent implements InspectorComponent {
     private final NlReferenceEditor myTextEditor;
     private final NlReferenceEditor myDesignTextEditor;
-    private final NlReferenceEditor myHintEditor;
     private final NlReferenceEditor myDescriptionEditor;
     private final NlEnumEditor myStyleEditor;
     private final NlEnumEditor myFontFamilyEditor;
@@ -88,7 +87,6 @@ public class TextInspectorProvider implements InspectorProvider {
 
     private NlProperty myText;
     private NlProperty myDesignText;
-    private NlProperty myHint;
     private NlProperty myDescription;
     private NlProperty myStyle;
     private NlProperty myFontFamily;
@@ -104,7 +102,6 @@ public class TextInspectorProvider implements InspectorProvider {
       Project project = propertiesManager.getProject();
       myTextEditor = NlReferenceEditor.createForInspectorWithBrowseButton(project, DEFAULT_LISTENER);
       myDesignTextEditor = NlReferenceEditor.createForInspectorWithBrowseButton(project, DEFAULT_LISTENER);
-      myHintEditor = NlReferenceEditor.createForInspectorWithBrowseButton(project, DEFAULT_LISTENER);
       myDescriptionEditor = NlReferenceEditor.createForInspectorWithBrowseButton(project, DEFAULT_LISTENER);
 
       myStyleEditor = NlEnumEditor.createForInspector(createEnumStyleListener());
@@ -139,7 +136,6 @@ public class TextInspectorProvider implements InspectorProvider {
     public void updateProperties(@NotNull List<NlComponent> components, @NotNull Map<String, NlProperty> properties) {
       myText = properties.get(ATTR_TEXT);
       myDesignText = myText.getDesignTimeProperty();
-      myHint = properties.get(ATTR_HINT);
       myDescription = properties.get(ATTR_CONTENT_DESCRIPTION);
       myStyle = properties.get(ATTR_TEXT_APPEARANCE);
       myFontFamily = properties.get(ATTR_FONT_FAMILY);
@@ -154,7 +150,7 @@ public class TextInspectorProvider implements InspectorProvider {
 
     @Override
     public int getMaxNumberOfRows() {
-      return 13;
+      return 12;
     }
 
     @Override
@@ -163,7 +159,6 @@ public class TextInspectorProvider implements InspectorProvider {
       inspector.addComponent(ATTR_TEXT, myText.getTooltipText(), myTextEditor.getComponent());
       JLabel designText = inspector.addComponent(ATTR_TEXT, myDesignText.getTooltipText(), myDesignTextEditor.getComponent());
       designText.setIcon(AndroidIcons.NeleIcons.DesignProperty);
-      inspector.addComponent(ATTR_HINT, myHint.getTooltipText(), myHintEditor.getComponent());
       inspector.addComponent(ATTR_CONTENT_DESCRIPTION, myDescription.getTooltipText(), myDescriptionEditor.getComponent());
 
       inspector.addExpandableComponent(ATTR_TEXT_APPEARANCE, myStyle.getTooltipText(), myStyleEditor.getComponent());
@@ -181,7 +176,6 @@ public class TextInspectorProvider implements InspectorProvider {
     public void refresh() {
       myTextEditor.setProperty(myText);
       myDesignTextEditor.setProperty(myDesignText);
-      myHintEditor.setProperty(myHint);
       myDescriptionEditor.setProperty(myDescription);
       myStyleEditor.setProperty(myStyle);
       myFontFamilyEditor.setProperty(myFontFamily);
