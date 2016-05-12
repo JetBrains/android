6e36521497f26373cae5bdf138d4b211a38320f6
Jens Ole Lauridsen <jlauridsen@google.com>
2016-05-12 10:09:38 -0700

    Sort the properties by name as well as category.
    Changed the default sorting on the property table:
    - Sort first then group.
      In this way the items in the groups are sorted as well.
    - Remote the margin group (took some of the layout_margin).
    - Remote the tag node for now.
      We will re-introduce this later.
    
    Change-Id: If2ab42a704e2facd5b639556f7eb12cbd2170ddc

--- designer/src/com/android/tools/idea/uibuilder/property/NlPropertiesGrouper.java
+++ designer/src/com/android/tools/idea/uibuilder/property/NlPropertiesGrouper.java
@@ -18,6 +18,7 @@ package com.android.tools.idea.uibuilder.property;
 import com.android.tools.idea.uibuilder.model.NlComponent;
 import com.android.tools.idea.uibuilder.property.NlPropertyAccumulator.PropertyNamePrefixAccumulator;
 import com.android.tools.idea.uibuilder.property.ptable.PTableItem;
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Lists;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -36,29 +37,25 @@ public class NlPropertiesGrouper {
     NlPropertyAccumulator themePropertiesAccumulator = new NlPropertyAccumulator(
       "Theme", p -> p != null && (p.getParentStylables().contains("Theme") || p.getName().equalsIgnoreCase("theme")));
 
+    // Disable this for now...
+    //
     // group attributes that correspond to this component together
-    NlPropertyAccumulator customViewPropertiesAccumulator = null;
-    if (className != null) {
-      customViewPropertiesAccumulator = new NlPropertyAccumulator(className, p -> p != null && p.getParentStylables().contains(className));
-    }
+    //NlPropertyAccumulator customViewPropertiesAccumulator = null;
+    //if (className != null) {
+    //  customViewPropertiesAccumulator = new NlPropertyAccumulator(className, p -> p != null && p.getParentStylables().contains(className));
+    //}
 
     // group margin, padding and layout attributes together
-    NlPropertyAccumulator marginPropertiesAccumulator = new NlMarginPropertyAccumulator("Margin", ATTR_LAYOUT_MARGIN_LEFT, ATTR_LAYOUT_MARGIN_RIGHT, ATTR_LAYOUT_MARGIN_TOP, ATTR_LAYOUT_MARGIN_BOTTOM);
     NlPropertyAccumulator paddingPropertiesAccumulator = new NlMarginPropertyAccumulator("Padding", ATTR_PADDING, ATTR_PADDING_LEFT, ATTR_PADDING_RIGHT, ATTR_PADDING_START, ATTR_PADDING_END, ATTR_PADDING_TOP, ATTR_PADDING_BOTTOM);
-    NlPropertyAccumulator layoutViewPropertiesAccumulator = new NlMarginPropertyAccumulator("Layout", ATTR_LAYOUT_MARGIN, ATTR_LAYOUT_MARGIN_LEFT, ATTR_LAYOUT_MARGIN_RIGHT, ATTR_LAYOUT_MARGIN_START, ATTR_LAYOUT_MARGIN_END, ATTR_LAYOUT_MARGIN_TOP, ATTR_LAYOUT_MARGIN_BOTTOM);
+    NlPropertyAccumulator layoutViewPropertiesAccumulator = new NlMarginPropertyAccumulator("Layout_Margin", ATTR_LAYOUT_MARGIN, ATTR_LAYOUT_MARGIN_LEFT, ATTR_LAYOUT_MARGIN_RIGHT, ATTR_LAYOUT_MARGIN_START, ATTR_LAYOUT_MARGIN_END, ATTR_LAYOUT_MARGIN_TOP, ATTR_LAYOUT_MARGIN_BOTTOM);
 
     PropertyNamePrefixAccumulator constraintPropertiesAccumulator = new PropertyNamePrefixAccumulator("Constraints", "layout_constraint");
 
     List<NlPropertyAccumulator> accumulators = Lists.newArrayList(
       themePropertiesAccumulator,
-      marginPropertiesAccumulator, // FIXME: is it needed?..
       paddingPropertiesAccumulator,
       layoutViewPropertiesAccumulator,
       constraintPropertiesAccumulator);
-
-    if (customViewPropertiesAccumulator != null) {
-      accumulators.add(customViewPropertiesAccumulator);
-    }
 
     for (NlPropertyItem p : properties) {
       boolean added = false;
@@ -74,9 +71,11 @@ public class NlPropertiesGrouper {
       }
     }
 
+    int insertionPoint = findInsertionPoint(result);
+
     for (NlPropertyAccumulator accumulator : accumulators) {
       if (accumulator.hasItems()) {
-        result.add(accumulator.getGroupNode());
+        result.add(insertionPoint, accumulator.getGroupNode());
       }
     }
 
@@ -96,5 +95,16 @@ public class NlPropertiesGrouper {
       }
     }
     return commonTagName;
+  }
+
+  private static List<String> MOST_IMPORTANT_ATTRIBUTES = ImmutableList.of(ATTR_ID, ATTR_LAYOUT_WIDTH, ATTR_LAYOUT_HEIGHT);
+
+  private static int findInsertionPoint(@NotNull List<PTableItem> properties) {
+    for (int index = 0; index < MOST_IMPORTANT_ATTRIBUTES.size(); index++) {
+      if (properties.size() < index + 1 || !properties.get(index).getName().equals(MOST_IMPORTANT_ATTRIBUTES.get(index))) {
+        return index;
+      }
+    }
+    return MOST_IMPORTANT_ATTRIBUTES.size();
   }
 }
