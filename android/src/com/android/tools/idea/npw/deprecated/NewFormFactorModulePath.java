0aeafda3b96fb6c22ffa562052c40a766a7b21d0
parentej <parentej@google.com>
2016-05-13 13:53:56 +0100

    New Module Wizard - Move deprecated code
    Change-Id: I041c065852d35803e4783012bcd1c975485b1d5e

--- android/src/com/android/tools/idea/npw/deprecated/NewFormFactorModulePath.java
+++ android/src/com/android/tools/idea/npw/deprecated/NewFormFactorModulePath.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014 The Android Open Source Project
+ * Copyright (C) 2016 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,9 +13,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.android.tools.idea.npw;
+package com.android.tools.idea.npw.deprecated;
 
 import com.android.builder.model.SourceProvider;
+import com.android.tools.idea.npw.*;
 import com.android.tools.idea.templates.*;
 import com.android.tools.idea.templates.recipe.RenderingContext;
 import com.android.tools.idea.wizard.*;
@@ -342,7 +343,8 @@ public class NewFormFactorModulePath extends DynamicWizardPath {
     }
   }
 
-  protected void setGradleSyncIfNecessary(boolean value) {
+  // TODO: Used to be protected, try to keep protected / private when code is converted over to the new system
+  public void setGradleSyncIfNecessary(boolean value) {
     myGradleSyncIfNecessary = value;
   }
 }
