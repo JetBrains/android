11277b7dc764ef227af27f650b6fe46b256ed93d
Tor Norbye <tnorbye@google.com>
2016-05-18 10:21:56 -0700

    Update @RequiresApi quickfix
    Apparently the change from api= to value= didn't make it into the
    right release branch so update the quickfix to insert api= instead.
    
    Also make the quickfixes added for the API check also show up for
    the annotation-based check.
    
    Change-Id: I8c6c7b5abac2650a1332bda29c06e8d182f76e31

--- android/src/org/jetbrains/android/inspections/lint/AndroidLintQuickFix.java
+++ android/src/org/jetbrains/android/inspections/lint/AndroidLintQuickFix.java
@@ -1,6 +1,10 @@
 package org.jetbrains.android.inspections.lint;
 
+import com.intellij.codeInspection.LocalQuickFix;
+import com.intellij.codeInspection.ProblemDescriptor;
+import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiElement;
+import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.NotNull;
 
 /**
@@ -15,4 +19,35 @@ public interface AndroidLintQuickFix {
 
   @NotNull
   String getName();
+
+  public static class LocalFixWrapper implements LocalQuickFix {
+    private final AndroidLintQuickFix myFix;
+    private final PsiElement myStart;
+    private final PsiElement myEnd;
+
+    public LocalFixWrapper(@NotNull AndroidLintQuickFix fix, @NotNull PsiElement start, @NotNull PsiElement end) {
+      myFix = fix;
+      myStart = start;
+      myEnd = end;
+    }
+
+    @Nls
+    @NotNull
+    @Override
+    public String getName() {
+      return myFix.getName();
+    }
+
+    @Nls
+    @NotNull
+    @Override
+    public String getFamilyName() {
+      return myFix.getName();
+    }
+
+    @Override
+    public void applyFix(@NotNull Project project, @NotNull ProblemDescriptor descriptor) {
+      myFix.apply(myStart, myEnd, AndroidQuickfixContexts.BatchContext.getInstance());
+    }
+  }
 }
