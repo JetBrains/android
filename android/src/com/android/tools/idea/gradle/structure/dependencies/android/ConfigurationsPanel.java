9f50b0c2b4cc15c5715278aec0c38f773a77103f
Alex Ruiz <alruiz@google.com>
2016-05-30 09:47:40 -0700

    [New PSD] Added "Speed Search" to scope panels.
    This CL:
    
    - Replaces the custom JCheckBox and JRadioButton
      lists used to display "Configuration", "Build Types"
      and "Product Flavors" panels. The result is a
      more unified look, better keyboard navigation and
      IDEA's "Speed Search"
    - Enabled dependencies for "buildable" Java modules
      (i.e. Java library modules)
    
    Change-Id: I7c3e38362aca1b316db3744e4ad74616880147ae

---
+++ android/src/com/android/tools/idea/gradle/structure/dependencies/android/ConfigurationsPanel.java
@@ -0,0 +1,108 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.gradle.structure.dependencies.android;
+
+import com.android.tools.idea.gradle.structure.configurables.ui.SelectionChangeEventDispatcher;
+import com.android.tools.idea.gradle.structure.configurables.ui.SelectionChangeListener;
+import com.google.common.collect.Lists;
+import com.intellij.openapi.Disposable;
+import com.intellij.ui.CheckBoxList;
+import com.intellij.ui.IdeBorderFactory;
+import com.intellij.ui.ListSpeedSearch;
+import com.intellij.ui.ScrollPaneFactory;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.List;
+
+import static com.android.tools.idea.gradle.structure.dependencies.android.Configuration.*;
+import static com.intellij.ui.SideBorder.*;
+
+class ConfigurationsPanel extends JPanel {
+  @NotNull private List<Configuration> mySelectedConfigurations;
+  @NotNull private final CheckBoxList<Configuration> myConfigurationsList;
+  @NotNull private final SelectionChangeEventDispatcher<List<Configuration>> myEventDispatcher = new SelectionChangeEventDispatcher<>();
+
+  ConfigurationsPanel() {
+    super(new BorderLayout());
+
+    List<Configuration> configurations = Lists.newArrayList(MAIN, ANDROID_TEST, UNIT_TEST);
+
+    myConfigurationsList = new CheckBoxList<>();
+    myConfigurationsList.setItems(configurations, null);
+
+    // Select "main" by default.
+    mySelectedConfigurations = Lists.newArrayList(MAIN);
+    for (Configuration configuration : configurations) {
+      myConfigurationsList.setItemSelected(configuration, true);
+    }
+    myConfigurationsList.setCheckBoxListListener((index, value) -> {
+      Configuration configuration = myConfigurationsList.getItemAt(index);
+      if (configuration != null) {
+        if (configuration == MAIN) {
+          if (value) {
+            myConfigurationsList.setItemSelected(ANDROID_TEST, true);
+            myConfigurationsList.setItemSelected(UNIT_TEST, true);
+          }
+        }
+        else {
+          if (!value) {
+            myConfigurationsList.setItemSelected(MAIN, false);
+          }
+        }
+      }
+      updateSelection();
+      myEventDispatcher.selectionChanged(mySelectedConfigurations);
+    });
+
+    new ListSpeedSearch(myConfigurationsList);
+
+    JScrollPane scrollPane = ScrollPaneFactory.createScrollPane(myConfigurationsList);
+    scrollPane.setBorder(IdeBorderFactory.createBorder(LEFT | RIGHT | BOTTOM));
+    add(scrollPane, BorderLayout.CENTER);
+  }
+
+  private void updateSelection() {
+    mySelectedConfigurations.clear();
+    if (myConfigurationsList.isItemSelected(MAIN)) {
+      mySelectedConfigurations.add(MAIN);
+    }
+    else {
+      if (myConfigurationsList.isItemSelected(ANDROID_TEST)) {
+        mySelectedConfigurations.add(ANDROID_TEST);
+      }
+      if (myConfigurationsList.isItemSelected(UNIT_TEST)) {
+        mySelectedConfigurations.add(UNIT_TEST);
+      }
+    }
+  }
+
+  void add(@NotNull SelectionChangeListener<List<Configuration>> listener, @NotNull Disposable parentDisposable) {
+    myEventDispatcher.addListener(listener, parentDisposable);
+  }
+
+  @Nullable
+  public JComponent getPreferredFocusedComponent() {
+    return myConfigurationsList;
+  }
+
+  @NotNull
+  List<Configuration> getSelectedConfigurations() {
+    return mySelectedConfigurations;
+  }
+}
