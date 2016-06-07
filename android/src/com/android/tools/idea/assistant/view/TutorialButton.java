a69579e592b4210dd69de26c6dff738f12997e4d
Joshua Ganderson <jganderson@google.com>
2016-06-07 17:44:03 -0500

    Make "link" buttons use standard text presentation.
    Refactors setHtml into two methods so that the generated markup
    can be used on non-editor components. Moves the offsite anchor
    treatment into the editor-only method as that's the only
    guaranteed location for offsite links.
    
    Leverages refactored method in tutorial link navigation button.
    
    Bug: 28964103
    Change-Id: If5deb7dda99ef3dabc98f471727e6ce159cb9e18

--- android/src/com/android/tools/idea/assistant/view/TutorialButton.java
+++ android/src/com/android/tools/idea/assistant/view/TutorialButton.java
@@ -36,7 +36,7 @@ public class TutorialButton extends NavigationButton {
   public TutorialButton(String label, String key, ActionListener listener) {
     // Render the button as a link to conform with the document-style presentation. We're using a button rather than a link with a
     // click handler to retain built-in accessibility benefits of a button.
-    super("<html><body><a href=\"#\">" + label + "</a></body></html>", key, listener);
+    super(UIUtils.getHtml("<a href=\"#\">" + label + "</a>"), key, listener);
     setIcon(AndroidIcons.Assistant.TutorialIndicator);
     setHorizontalAlignment(SwingConstants.LEFT);
     setMargin(new Insets(50, 10, 10, 10));
