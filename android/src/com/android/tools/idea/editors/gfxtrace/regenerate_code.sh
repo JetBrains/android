40b136b36e20af0ff394a147b76e323986a3ab9f
Pascal Muetschard <pmuetschard@google.com>
2016-06-10 10:51:36 -0700

    Small improvements to the regenerate_code script.
    - regenerate_code.sh can now be run from anywhere
    - it checks for existence of protoc and codergen first
    - can now force it to use a specific protoc by defining $PROTOC
    - invokes codergen directly rather than assuming it's in $PATH
    
    Also regenerated code that has gotten stale.
    
    Change-Id: If0cc1dccf8291d3d4c3382c613d07c008069b8ca

--- android/src/com/android/tools/idea/editors/gfxtrace/regenerate_code.sh
+++ android/src/com/android/tools/idea/editors/gfxtrace/regenerate_code.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+set -e
 
 # This script assumes a standard android studio repo checkout and refers to files from other repositories
 
@@ -6,19 +7,37 @@ absname() {
   echo "$(cd $1 && pwd)"
 }
 
-JAVA_BASE=$(absname "../../../../../..")
+PROTOC=${PROTOC:-protoc}
+
+SCRIPT_DIR=$(absname "$(dirname "${BASH_SOURCE[0]}")")
+JAVA_BASE=$(absname "$SCRIPT_DIR/../../../../../..")
 TOOLS_ROOT=$(absname "$JAVA_BASE/../../../..")
-GPU_BASE=$(absname  "$TOOLS_ROOT/gpu/src/android.googlesource.com/platform/tools/gpu")
+GPU_BASE=$(absname "$TOOLS_ROOT/gpu/src/android.googlesource.com/platform/tools/gpu")
+
+if [ ! -x $TOOLS_ROOT/gpu/bin/codergen ]
+then
+  echo "Build codergen in $TOOLS_ROOT/gpu/bin from $GPU_BASE first."
+  exit 1
+fi
+
+command -v $PROTOC >/dev/null 2>&1 || {
+  echo "$PROTOC not found in $PATH"
+  exit 1
+}
 
 # First generate all the proto based files
-protoc --proto_path $GPU_BASE --java_out $JAVA_BASE $GPU_BASE/api/snippets/snippets.proto
-protoc --proto_path $GPU_BASE --java_out $JAVA_BASE $GPU_BASE/framework/log/log.proto
-protoc --proto_path $GPU_BASE --java_out $JAVA_BASE $GPU_BASE/gapid/gfxapi/gfxapi.proto
-protoc --proto_path $GPU_BASE --java_out $JAVA_BASE $GPU_BASE/gapid/memory/memory.proto
-protoc --proto_path $GPU_BASE --java_out $JAVA_BASE $GPU_BASE/gapid/service/service.proto
-protoc --proto_path $GPU_BASE --java_out $JAVA_BASE $GPU_BASE/gapid/service/path/path.proto
-protoc --proto_path $GPU_BASE --java_out $JAVA_BASE $GPU_BASE/gapid/vertex/vertex.proto
+echo "Generating protos..."
+$PROTOC --proto_path $GPU_BASE --java_out $JAVA_BASE $GPU_BASE/api/snippets/snippets.proto
+$PROTOC --proto_path $GPU_BASE --java_out $JAVA_BASE $GPU_BASE/framework/log/log.proto
+$PROTOC --proto_path $GPU_BASE --java_out $JAVA_BASE $GPU_BASE/gapid/gfxapi/gfxapi.proto
+$PROTOC --proto_path $GPU_BASE --java_out $JAVA_BASE $GPU_BASE/gapid/memory/memory.proto
+$PROTOC --proto_path $GPU_BASE --java_out $JAVA_BASE $GPU_BASE/gapid/service/service.proto
+$PROTOC --proto_path $GPU_BASE --java_out $JAVA_BASE $GPU_BASE/gapid/service/path/path.proto
+$PROTOC --proto_path $GPU_BASE --java_out $JAVA_BASE $GPU_BASE/gapid/vertex/vertex.proto
 
 # And now run codergen for the serializers
-cd $GPU_BASE
-codergen --java $TOOLS_ROOT ./...
+echo "Running codergen..."
+pushd $GPU_BASE >/dev/null
+$TOOLS_ROOT/gpu/bin/codergen --java $TOOLS_ROOT ./...
+popd >/dev/null
+echo "All done"
