bddb4053848bfc8efefb663cf1aee485c5056afd
Vlad Albulescu <valbulescu@google.com>
2016-06-13 19:18:20 +0100

    Move back to using adb push for transferring libraries to device
    Simplifies logic, reduces the chance of deadlock, doesn't suffer
    from the problem of doing networking on the UI thread.
    
    Change-Id: I8321ae366c0091225bee29995ffabe4061d0baae
    Fixes: 29321321

--- android/src/com/android/tools/idea/editors/gfxtrace/GapiiLibraryLoader.java
+++ android/src/com/android/tools/idea/editors/gfxtrace/GapiiLibraryLoader.java
@@ -15,16 +15,10 @@
  */
 package com.android.tools.idea.editors.gfxtrace;
 
-import com.android.annotations.concurrency.Immutable;
 import com.android.ddmlib.Client;
 import com.android.ddmlib.IDevice;
+import com.android.ddmlib.NullOutputReceiver;
 import com.android.tools.idea.run.editor.AndroidJavaDebugger;
-import com.google.common.base.Charsets;
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.Lists;
-import com.google.common.io.ByteStreams;
-import com.google.common.io.Files;
-import com.google.common.util.concurrent.ListenableFuture;
 import com.intellij.debugger.engine.DebugProcessImpl;
 import com.intellij.debugger.engine.SuspendContextImpl;
 import com.intellij.debugger.engine.evaluation.*;
@@ -39,33 +33,21 @@ import com.intellij.execution.executors.DefaultDebugExecutor;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.Project;
-import com.intellij.util.Time;
 import com.intellij.util.concurrency.EdtExecutorService;
 import com.intellij.util.concurrency.FutureResult;
-import com.intellij.util.concurrency.Futures;
 import com.sun.jdi.Value;
 import com.sun.jdi.event.LocatableEvent;
 import com.sun.jdi.request.MethodEntryRequest;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.Socket;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.concurrent.*;
-import java.util.stream.Collectors;
 
 public class GapiiLibraryLoader {
 
   @NotNull private static final Logger LOG = Logger.getInstance(GapiiLibraryLoader.class);
 
-  private static final int MAX_CONNECT_ATTEMPTS = 20;
-  private static final int GAPII_INIT_PORT = 9287; // TODO(valbulescu): Find free port instead of hoping for the best.
-  private static final int TIME_BETWEEN_ATTEMPTS_MS = 250;
+  private static final String DEVICE_TMP_DIR = "/data/local/tmp";
   private static final long LIB_INSTALLATION_TIMEOUT_MS = 20000;
 
   private IDevice myDevice;
@@ -74,19 +56,6 @@ public class GapiiLibraryLoader {
   public GapiiLibraryLoader(@NotNull Project project, @NotNull IDevice device) {
     myProject = project;
     myDevice = device;
-  }
-
-  private static List<Throwable> waitForFutures(long timeout, TimeUnit unit, Future... futures) {
-    long startTime = System.nanoTime();
-    List<Throwable> errors = Lists.newArrayList();
-    for (Future future : futures) {
-      try {
-        future.get(startTime + unit.toNanos(timeout) - System.nanoTime(), TimeUnit.NANOSECONDS);
-      } catch (Throwable t) {
-        errors.add(t);
-      }
-    }
-    return errors;
   }
 
   public void connectToProcessAndInstallLibraries(@NotNull String pkg, @NotNull final File... libs) throws Exception {
@@ -103,7 +72,6 @@ public class GapiiLibraryLoader {
     DebugProcessImpl debugProcess = debuggerSession.getProcess();
 
     FutureResult<Void> evalCompletion = new FutureResult<>();
-    FutureTask<Void> senderTask = new FutureTask<>(() -> createForwardAndSendFiles(libs));
 
     FilteredRequestor appOnCreateFilter = new ClassFilterRequestor("android.app.Application") {
       @Override
@@ -115,9 +83,16 @@ public class GapiiLibraryLoader {
         LOG.debug("Application.onCreate() called, running library installation.");
 
         try {
-          ApplicationManager.getApplication().executeOnPooledThread(senderTask);
+          for (File lib : libs) {
+            String remoteFilePath = new File(DEVICE_TMP_DIR, lib.getName()).getAbsolutePath();
+            try {
+              myDevice.pushFile(lib.getAbsolutePath(), remoteFilePath);
+              evaluate(CodeFragmentKind.CODE_BLOCK, getLoaderSnippet(lib), action.getSuspendContext());
+            } finally {
+              myDevice.executeShellCommand("rm " + remoteFilePath, NullOutputReceiver.getReceiver());
+            }
+          }
 
-          evaluate(CodeFragmentKind.CODE_BLOCK, JdwpSnippets.getLoaderSnippet(libs), action.getSuspendContext());
           evalCompletion.set(null);
         } catch (Throwable t) {
           evalCompletion.setException(t);
@@ -135,29 +110,19 @@ public class GapiiLibraryLoader {
       }
     });
 
-    List<Throwable> errors = waitForFutures(LIB_INSTALLATION_TIMEOUT_MS, TimeUnit.MILLISECONDS, senderTask, evalCompletion);
-
-    if (!evalCompletion.isDone()) {
-      // evaluation could hang while e.g. waiting for a connection from the library sender.
-      LOG.warn("Attempting to terminate evaluation.");
+    try {
+      evalCompletion.get(LIB_INSTALLATION_TIMEOUT_MS, TimeUnit.MILLISECONDS);
+    } catch (TimeoutException ex) {
       debugProcess.getManagerThread().terminateAndInvoke(debugProcess.createStopCommand(true), 1000);
-    }
-    if (!senderTask.isDone()) {
-      senderTask.cancel(true);
-    }
-
-    EdtExecutorService.getInstance().submit(() -> {
-      debugProcess.getProcessHandler().detachProcess();
-      ExecutionManager.getInstance(myProject).getContentManager().removeRunContent(
-        DefaultDebugExecutor.getDebugExecutorInstance(),
-        debuggerSession.getXDebugSession().getRunContentDescriptor()
-      );
-    });
-
-    if (!errors.isEmpty()) {
-      errors.stream().forEach(LOG::error);
-      throw new RuntimeException(
-        errors.stream().map(String::valueOf).collect(Collectors.joining(", ", "Library installation failed: ", "")), errors.get(0));
+      throw ex;
+    } finally {
+      EdtExecutorService.getInstance().submit(() -> {
+        debugProcess.getProcessHandler().detachProcess();
+        ExecutionManager.getInstance(myProject).getContentManager().removeRunContent(
+          DefaultDebugExecutor.getDebugExecutorInstance(),
+          debuggerSession.getXDebugSession().getRunContentDescriptor()
+        );
+      });
     }
 
     long durationMs = TimeUnit.MILLISECONDS.convert(System.nanoTime() - startTime, TimeUnit.NANOSECONDS);
@@ -184,49 +149,26 @@ public class GapiiLibraryLoader {
     );
   }
 
-  private Void createForwardAndSendFiles(File[] files) throws Exception {
-    byte[] expectedHeader = JdwpSnippets.GAPII_HEADER.getBytes(Charsets.UTF_8);
-
-    try {
-      myDevice.createForward(GAPII_INIT_PORT, JdwpSnippets.GAPII_ABSTRACT_SOCKET, IDevice.DeviceUnixSocketNamespace.ABSTRACT);
-
-      boolean headerReceived = false;
-      for (int attempt = 1; ; attempt++) {
-        Thread.sleep(TIME_BETWEEN_ATTEMPTS_MS);
-
-        try (
-          Socket clientSocket = new Socket("localhost", GAPII_INIT_PORT);
-          OutputStream outStream = clientSocket.getOutputStream();
-          InputStream inStream = clientSocket.getInputStream();
-        ) {
-          expect(expectedHeader, inStream);
-          headerReceived = true;
-
-          for (File file : files) {
-            LOG.debug("Sending file " + file);
-            Files.copy(file, outStream);
-          }
-
-          LOG.debug("Finished sending files.");
-          return null;
-        } catch (IOException ex) {
-          if (headerReceived || attempt >= MAX_CONNECT_ATTEMPTS) {
-            throw ex;
-          }
-        }
-      }
-    } finally {
-      myDevice.removeForward(GAPII_INIT_PORT, JdwpSnippets.GAPII_ABSTRACT_SOCKET, IDevice.DeviceUnixSocketNamespace.ABSTRACT);
-    }
-  }
-
-  private void expect(byte[] expected, InputStream inStream) throws IOException {
-    byte[] actual = new byte[expected.length];
-    if (ByteStreams.read(inStream, actual, 0, expected.length) < expected.length) {
-      throw new IOException("Premature end of stream.");
-    }
-    if (!Arrays.equals(expected, actual)) {
-      throw new IllegalStateException("Unexpected response.");
-    }
+  private static String getLoaderSnippet(File lib) {
+    return
+      "  String TAG = \"gapii-init\";\n" +
+      "  String libName = \"" + lib.getName() + "\";\n" +
+      "  java.io.File inFile = new java.io.File(\"" + DEVICE_TMP_DIR + "\", libName);\n" +
+      "  java.io.File outFile = new java.io.File(((android.app.Application) this).getFilesDir(), libName);\n" +
+      "  long libSize = inFile.length();\n" +
+      "\n" +
+      "  android.util.Log.d(TAG, String.format(\"Copying %d bytes to %s.\", libSize, outFile));\n" +
+      "  java.nio.channels.FileChannel inChannel = new java.io.FileInputStream(inFile).getChannel();\n" +
+      "  java.nio.channels.FileChannel outChannel = new java.io.FileOutputStream(outFile).getChannel();\n" +
+      "  long remaining = libSize;\n" +
+      "  while (remaining > 0) {\n" +
+      "    remaining -= outChannel.transferFrom(inChannel, libSize - remaining, remaining); \n" +
+      "  }\n" +
+      "\n" +
+      "  outChannel.close();\n" +
+      "  inChannel.close();\n" +
+      "\n" +
+      "  android.util.Log.d(TAG, \"Library copied, loading.\");\n" +
+      "  System.load(outFile.toString());\n";
   }
 }
