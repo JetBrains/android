c3426caffdae0a07023b31d198fcbe44771aff8a
Yura <yura@google.com>
2016-06-14 16:50:36 +0100

    Have only 1 toString method for image texture formats.
    Change-Id: I9ccc92e2862dad910c2e84e499b0271d79df0245

--- android/src/com/android/tools/idea/editors/gfxtrace/service/image/FmtLuminanceAlpha.java
+++ android/src/com/android/tools/idea/editors/gfxtrace/service/image/FmtLuminanceAlpha.java
@@ -23,11 +23,7 @@ import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
 
-final class FmtLuminanceAlpha extends Format {  @Override
-  public String getDisplayName() {
-    return "LUMINANCE_ALPHA";
-  }
-
+final class FmtLuminanceAlpha extends Format {
   //<<<Start:Java.ClassBody:1>>>
 
   // Constructs a default-initialized {@link FmtLuminanceAlpha}.
