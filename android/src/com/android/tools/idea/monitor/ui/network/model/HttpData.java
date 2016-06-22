aec2b10ba96ec165ebfb48907c60d306f517bae9
Yuxin Yu <yuxy@google.com>
2016-06-22 16:03:22 -0700

    Add data poller for http url connections.
    1) Polls connections start and end timestamp at a fix sample rate.
    2) When a new connection and start timestamp is got, call perfd to get
       the request data like url.
    3) When a connection end timestamp is got, call perfd to get response
       boy data like image.
    4) Modifies BaseProfilerUiManager to have multiple pollers.
    
    Change-Id: If787424301d79338fea48b0df02342395588529c

---
+++ android/src/com/android/tools/idea/monitor/ui/network/model/HttpData.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.monitor.ui.network.model;
+
+/**
+ * Data of http url connection. Each {@code HttpData} object matches a http connection with a unique id, and it includes both request data
+ * and response data. Request data is filled immediately when the connection starts. Response data may be empty, it will filled when
+ * connection completes.
+ */
+public class HttpData {
+  public long myId;
+  public String myUrl;
+  public String myMethod;
+  public long myStartTimeMs;
+  public long myEndTimeMs;
+  public String myHttpResponseBodyPath;
+}
