f5e856b3c248e4cdc2deb254c92ee157fe56aaa9
Jens Ole Lauridsen <jlauridsen@google.com>
2016-06-23 14:26:27 -0700

    Add handling of TabItems in nele.
    In this CL we:
    - Add TabLayout and TabItem to the palette.
    - Add/modify the handler for these.
    - Add TabItems in the tabular AppBarLayout dialog.
    - Update the cached palette if we have a newer version available.
    
    Change-Id: I8f20da410294401942dbffdce74c22a6ccf62667
    (cherry picked from commit 1ca8479d9e7a21328d83402e1898903ecf545851)

--- designer/src/com/android/tools/idea/uibuilder/palette/NlPaletteModel.java
+++ designer/src/com/android/tools/idea/uibuilder/palette/NlPaletteModel.java
@@ -15,16 +15,18 @@
  */
 package com.android.tools.idea.uibuilder.palette;
 
-import com.android.tools.idea.uibuilder.model.NlLayoutType;
-import org.jetbrains.annotations.NotNull;
 import com.android.annotations.VisibleForTesting;
 import com.android.tools.idea.uibuilder.handlers.ViewHandlerManager;
+import com.android.tools.idea.uibuilder.model.NlLayoutType;
 import com.intellij.openapi.application.PathManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.io.FileUtil;
+import org.jetbrains.annotations.NotNull;
 
 import javax.xml.bind.JAXBException;
 import java.io.*;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.EnumMap;
 import java.util.Map;
 
@@ -62,22 +64,15 @@ public class NlPaletteModel {
       String name = type.getPaletteFileName();
       File file = new File(PathManager.getSystemPath(), ANDROID_PALETTE + File.separatorChar + PALETTE_VERSION + File.separatorChar + name);
 
-      if (!file.exists()) {
+      if (isOutOfDate(file, name)) {
         copyPredefinedPalette(file, name);
       }
 
-      Reader reader = new InputStreamReader(new FileInputStream(file));
-      try {
+      try (Reader reader = new InputStreamReader(new FileInputStream(file))) {
         loadPalette(reader, type);
       }
-      finally {
-        reader.close();
-      }
     }
-    catch (IOException e) {
-      throw new RuntimeException(e);
-    }
-    catch (JAXBException e) {
+    catch (IOException | JAXBException e) {
       throw new RuntimeException(e);
     }
   }
@@ -89,18 +84,28 @@ public class NlPaletteModel {
   }
 
   private static void copyPredefinedPalette(@NotNull File paletteFile, @NotNull String metadata) throws IOException {
-    InputStream stream = NlPaletteModel.class.getResourceAsStream(metadata);
     File folder = paletteFile.getParentFile();
     if (!folder.isDirectory() && !folder.mkdirs()) {
       throw new IOException("Could not create directory: " + folder);
     }
-    FileOutputStream output = new FileOutputStream(paletteFile);
-    try {
-      FileUtil.copy(stream, output);
+
+    try (InputStream input = NlPaletteModel.class.getResourceAsStream(metadata);
+         FileOutputStream output = new FileOutputStream(paletteFile)) {
+      FileUtil.copy(input, output);
     }
-    finally {
-      stream.close();
-      output.close();
+  }
+
+  private static boolean isOutOfDate(@NotNull File file, @NotNull String metadata) throws IOException {
+    if (!file.exists()) {
+      return true;
+    }
+    URL url = NlPaletteModel.class.getResource(metadata);
+    URLConnection connection = url.openConnection();
+
+    // The FileURLConnection creates an InputStream during url.openConnection. Make sure this is closed.
+    //noinspection unused
+    try (InputStream stream = connection.getInputStream()) {
+      return connection.getLastModified() > file.lastModified();
     }
   }
 }
