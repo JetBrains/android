b1e3e9e2d77564934744f1b2e721f970df43eb35
Jens Ole Lauridsen <jlauridsen@google.com>
2016-07-06 10:43:49 -0700

    Disable preview of menues and preferences.
    The IconPreviewFactory is unable to generate a correct preview for
    these components. Disable preview for now.
    
    Change-Id: I1798ff0d6c118427a875bbd7cdb7e11758dac735

--- designer/src/com/android/tools/idea/uibuilder/handlers/preference/CheckBoxPreferenceHandler.java
+++ designer/src/com/android/tools/idea/uibuilder/handlers/preference/CheckBoxPreferenceHandler.java
@@ -36,12 +36,20 @@ public final class CheckBoxPreferenceHandler extends PreferenceHandler {
   @NotNull
   @Override
   public String getXml(@NotNull String tagName, @NotNull XmlType xmlType) {
-    return new XmlBuilder()
-      .startTag(tagName)
-      .androidAttribute(ATTR_DEFAULT_VALUE, false)
-      .androidAttribute(ATTR_TITLE, "Check box preference")
-      .endTag(tagName)
-      .toString();
+    switch (xmlType) {
+      case COMPONENT_CREATION:
+        return new XmlBuilder()
+          .startTag(tagName)
+          .androidAttribute(ATTR_DEFAULT_VALUE, false)
+          .androidAttribute(ATTR_TITLE, "Check box preference")
+          .endTag(tagName)
+          .toString();
+      case PREVIEW_ON_PALETTE:
+      case DRAG_PREVIEW:
+        return NO_PREVIEW;
+      default:
+        throw new AssertionError(xmlType);
+    }
   }
 
   @Override
