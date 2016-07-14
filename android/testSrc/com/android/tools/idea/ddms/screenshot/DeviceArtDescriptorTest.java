a4e01000afa4b9056e89e39582db3f970e3df51d
Tor Norbye <tnorbye@google.com>
2016-07-14 14:51:56 -0700

    Add Pixel C device art
    Change-Id: I705b9ac266d20573d9b7697c470419dc282f22aa

--- android/testSrc/com/android/tools/idea/ddms/screenshot/DeviceArtDescriptorTest.java
+++ android/testSrc/com/android/tools/idea/ddms/screenshot/DeviceArtDescriptorTest.java
@@ -33,7 +33,7 @@ public class DeviceArtDescriptorTest extends TestCase {
   public void test1() throws IOException {
     List<DeviceArtDescriptor> specs = DeviceArtDescriptor.getDescriptors(null);
 
-    assertEquals(19, specs.size());
+    assertEquals(20, specs.size());
 
     DeviceArtDescriptor nexus4 = getDescriptorFor("nexus_4", specs);
     assertNotNull(nexus4);
