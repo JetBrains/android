7cb55103cc448e3302135e2cd9169acb5c8469a4
dgli (dig-lee) Li <genli@google.com>
2016-07-22 20:21:59 +0000

    Revert "Add resource usage energy profiler UI."
    This reverts commit 09f0c47266a50f9c3bf9e600ca0ae06fe48248e9.
    
    Change-Id: Idd7131c189516e3f1f32ff445d2dcf7e481150ec

--- android/src/com/android/tools/idea/monitor/ui/energy/model/EnergyPoller.java
+++ android/src/com/android/tools/idea/monitor/ui/energy/model/EnergyPoller.java
@@ -15,107 +15,148 @@
  */
 package com.android.tools.idea.monitor.ui.energy.model;
 
+import com.android.tools.adtui.model.SeriesData;
 import com.android.tools.idea.monitor.datastore.*;
-import com.android.tools.profiler.proto.EnergyProfilerService.*;
 import com.intellij.openapi.diagnostic.Logger;
-import io.grpc.StatusRuntimeException;
+import gnu.trove.TLongArrayList;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-// TODO To be done in next CL:
-// - Add support for energy status that's currently reporting 0 in poll()
+/**
+ * TODO consider putting these into maps?
+ */
 public class EnergyPoller extends Poller {
-  public static final SeriesDataType[] ENERGY_DATA_TYPES = {
+  private static final long POLL_PERIOD_NS = TimeUnit.MILLISECONDS.toNanos(250);
+  private static final SeriesDataType[] DATA_TYPES = {
     SeriesDataType.ENERGY_SCREEN,
-    SeriesDataType.ENERGY_CPU_SYSTEM,
-    SeriesDataType.ENERGY_CPU_USER,
+    SeriesDataType.ENERGY_CPU,
     SeriesDataType.ENERGY_SENSORS,
     SeriesDataType.ENERGY_CELL_NETWORK,
     SeriesDataType.ENERGY_WIFI_NETWORK,
     SeriesDataType.ENERGY_TOTAL,
   };
-  private static final long POLL_PERIOD_NS = TimeUnit.MILLISECONDS.toNanos(1000);
-  private static Logger getLog() { return Logger.getInstance(EnergyPoller.class.getCanonicalName()); }
 
   private final int myAppId;
-  private long myLatestSampleTimestampNs;
+  private long myStartPollingTimeNs = -1; // TODO remove these temporary variables when we have real data.
+  private long myDeviceStartTime = -1;    // TODO remove these temporary variables when we have real data.
+  private boolean myIsDisplayingDeltas = false;
   private HashMap<SeriesDataType, EnergyDataAdapter> myAdapters;
 
-  public EnergyPoller(@NotNull SeriesDataStore dataStore, int appId, boolean showInstantanenousByDefault) {
+  public EnergyPoller(@NotNull SeriesDataStore dataStore, int appId) {
     super(dataStore, POLL_PERIOD_NS);
     myAppId = appId;
-    myLatestSampleTimestampNs = -1;
+    myDeviceStartTime = dataStore.getLatestTimeUs();
 
     myAdapters = new HashMap<>();
-    for (SeriesDataType type : ENERGY_DATA_TYPES) {
-      EnergyDataAdapter adapter = new EnergyDataAdapter();
-      adapter.setReturnInstantaneousData(showInstantanenousByDefault);
-      dataStore.registerAdapter(type, adapter);
-      myAdapters.put(type, adapter);
+    for (SeriesDataType type : DATA_TYPES) {
+      myAdapters.put(type, new EnergyDataAdapter());
+      dataStore.registerAdapter(type, myAdapters.get(type));
     }
   }
 
-  /**
-   * Returns the adapter for given type, if it exists.
-   */
-  public EnergyDataAdapter getEnergyAdapter(SeriesDataType dataType) {
-    return myAdapters.get(dataType);
+  public void toggleDataDisplay() {
+    myIsDisplayingDeltas = !myIsDisplayingDeltas;
+
+    for (SeriesDataType type : DATA_TYPES) {
+      myAdapters.get(type).setUseCumulative(myIsDisplayingDeltas);
+    }
   }
 
   @Override
   protected void asyncInit() {
-    StartEnergyCollectionRequest request = StartEnergyCollectionRequest.newBuilder().setAppId(myAppId).build();
-    EnergyCollectionStatusResponse response = myService.getEnergyService().startCollection(request);
-    myLatestSampleTimestampNs = response.getTimestamp(); // Initially the latest sample time is same as start time.
+
   }
 
   @Override
   protected void asyncShutdown() {
-    StopEnergyCollectionRequest request = StopEnergyCollectionRequest.newBuilder().setAppId(myAppId).build();
-    myService.getEnergyService().stopCollection(request);
-    // TODO what if this fails?
+
   }
 
   @Override
   protected void poll() {
-    try {
-      EnergyDataRequest request = EnergyDataRequest.newBuilder()
-        .setAppId(myAppId)
-        .setStartTimeExcl(myLatestSampleTimestampNs)
-        .setEndTimeIncl(Long.MAX_VALUE)
-        .build();
+    // TODO change to polling real data from perfd
+    long timeOffsetNs;
+    long currentDeviceTimeUs;
+    if (myStartPollingTimeNs < 0) {
+      myStartPollingTimeNs = System.nanoTime();
+      timeOffsetNs = 0;
+    }
+    else {
+      timeOffsetNs = System.nanoTime() - myStartPollingTimeNs;
+    }
+    currentDeviceTimeUs = TimeUnit.NANOSECONDS.toMicros(timeOffsetNs) + myDeviceStartTime;
 
-      EnergyDataResponse response = myService.getEnergyService().getData(request);
-
-      List<EnergyDataResponse.EnergySample> sampleList = response.getEnergySamplesList();
-      for (EnergyDataResponse.EnergySample sample : sampleList) {
-        long timestamp = TimeUnit.NANOSECONDS.toMicros(sample.getTimestamp());
-        long total = 0;
-        myAdapters.get(SeriesDataType.ENERGY_SCREEN).update(timestamp, sample.getScreenPowerUsage());
-        myAdapters.get(SeriesDataType.ENERGY_CPU_SYSTEM).update(timestamp, sample.getCpuSystemPowerUsage());
-        myAdapters.get(SeriesDataType.ENERGY_CPU_USER).update(timestamp, sample.getCpuUserPowerUsage());
-        myAdapters.get(SeriesDataType.ENERGY_SENSORS).update(timestamp, 0);
-        myAdapters.get(SeriesDataType.ENERGY_CELL_NETWORK).update(timestamp, 0);
-        myAdapters.get(SeriesDataType.ENERGY_WIFI_NETWORK).update(timestamp, 0);
-
-        for (SeriesDataType type : ENERGY_DATA_TYPES) {
-          if (type != SeriesDataType.ENERGY_TOTAL) {
-            total += myAdapters.get(type).getLatest();
-          }
-        }
-        myAdapters.get(SeriesDataType.ENERGY_TOTAL).update(timestamp, total);
+    long totalDelta = 0;
+    for (SeriesDataType type : DATA_TYPES) {
+      if (type == SeriesDataType.ENERGY_TOTAL) {
+        continue; // Skip as it's a special stat that aggregates all other stats.
       }
+      long temp = Math.max(0, (long)(Math.random() * 100 - 90));
+      myAdapters.get(type).updateByDelta(currentDeviceTimeUs, temp);
+      totalDelta += temp;
+    }
+    myAdapters.get(SeriesDataType.ENERGY_TOTAL).updateByDelta(currentDeviceTimeUs, totalDelta);
+  }
 
-      if (myLatestSampleTimestampNs < response.getLatestSampleTimestamp()) {
-        myLatestSampleTimestampNs = response.getLatestSampleTimestamp();
+  private class EnergyDataAdapter implements DataAdapter<Long> {
+    private long myLatestTotal = 0;
+    private long myLatestDelta = 0;
+    private boolean myUseCumulative = false;
+
+    @NotNull
+    private final TLongArrayList myTimestampData = new TLongArrayList();
+
+    @NotNull
+    private final TLongArrayList myCumulativeData = new TLongArrayList();
+
+    @NotNull
+    private final TLongArrayList myDeltaData = new TLongArrayList();
+
+    public void setUseCumulative(boolean useCumulative) {
+      myUseCumulative = useCumulative;
+    }
+
+    public void updateByDelta(long timestamp, long delta) {
+      myLatestDelta = delta;
+      myLatestTotal += delta;
+      insertLatestToList(timestamp);
+    }
+
+    public void updateByTotal(long timestamp, long total) {
+      myLatestDelta = myLatestTotal - total;
+      myLatestTotal += total;
+      insertLatestToList(timestamp);
+    }
+
+    private void insertLatestToList(long timestamp) {
+      myTimestampData.add(timestamp);
+      myDeltaData.add(myLatestDelta);
+      myCumulativeData.add(myLatestTotal);
+    }
+
+    @Override
+    public int getClosestTimeIndex(long timeUs, boolean leftClosest) {
+      return DataAdapter.getClosestIndex(myTimestampData, timeUs, leftClosest);
+    }
+
+    @Override
+    public SeriesData<Long> get(int index) {
+      if (myUseCumulative) {
+        return new SeriesData<>(myTimestampData.get(index), myCumulativeData.get(index));
       }
+      return new SeriesData<>(myTimestampData.get(index), myDeltaData.get(index));
+    }
 
-    } catch (StatusRuntimeException e) {
-      getLog().info("Server most likely unreachable.");
-      cancel(true);
+    @Override
+    public void reset(long deviceStartTimeUs, long studioStartTimeUs) {
+      // TODO: implement
+    }
+
+    @Override
+    public void stop() {
+      // TODO: implement
     }
   }
 }
