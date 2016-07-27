c85b12edbfacfafaa927fde27ccae4d48e188950
Siva Velusamy <vsiva@google.com>
2016-07-26 17:32:40 -0700

    Provide better notifications when IR is disabled
    Studio does not know about all the conditions where IR could be disabled
    by the plugin. This CL uses a new API from the gradle plugin to detect
    the gradle status, and shows appropriate error messages (e.g. when using
    an external build, IR is disabled).
    
    Fixes https://code.google.com/p/android/issues/detail?id=212639
    
    Change-Id: I6c57b7f4550c6e19111cb6231f9356e0f7111945

--- android/src/com/android/tools/idea/fd/gradle/InstantRunGradleSupport.java
+++ android/src/com/android/tools/idea/fd/gradle/InstantRunGradleSupport.java
@@ -15,15 +15,76 @@
  */
 package com.android.tools.idea.fd.gradle;
 
+import com.android.builder.model.InstantRun;
+import com.android.tools.idea.fd.InstantRunManager;
+import com.android.tools.idea.gradle.AndroidGradleModel;
+import com.intellij.openapi.diagnostic.Logger;
+import org.jetbrains.android.util.AndroidBundle;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
 /** {@link InstantRunGradleSupport} indicates whether the current version of the gradle plugin supports an instant run build. */
 public enum InstantRunGradleSupport {
   SUPPORTED,
-  DISABLED,
 
   NO_GRADLE_MODEL,
-  GRADLE_PLUGIN_TOO_OLD,
-  VARIANT_DOES_NOT_SUPPORT_INSTANT_RUN,
-  LEGACY_MULTIDEX_REQUIRES_ART,
+  GRADLE_PLUGIN_TOO_OLD(AndroidBundle.message("instant.run.notification.ir.disabled.plugin.too.old",
+                                              InstantRunManager.MINIMUM_GRADLE_PLUGIN_VERSION_STRING)),
+  VARIANT_DOES_NOT_SUPPORT_INSTANT_RUN(AndroidBundle.message("instant.run.notification.ir.disabled.for.current.variant")),
+  LEGACY_MULTIDEX_REQUIRES_ART(AndroidBundle.message("instant.run.notification.ir.disabled.multidex.requires.21")),
 
-  CANNOT_BUILD_FOR_MULTIPLE_DEVICES,
+  CANNOT_BUILD_FOR_MULTIPLE_DEVICES(AndroidBundle.message("instant.run.notification.ir.disabled.multiple.devices")),
+
+  // Gradle 2.2.0-alpha6 and above can provide more fine grained info when IR is disabled.
+  // The following status messages correspond to the values returned by the gradle plugin.
+  NON_DEBUG_VARIANT(AndroidBundle.message("instant.run.notification.ir.disabled.non.debug.variant")),
+  VARIANT_USED_FOR_TESTING(AndroidBundle.message("instant.run.notification.ir.disabled.testing.variant")),
+  USES_JACK(AndroidBundle.message("instant.run.notification.ir.disabled.jack")),
+  USES_EXTERNAL_NATIVE_BUILD(AndroidBundle.message("instant.run.notification.ir.disabled.external.native.build")),
+  USES_EXPERIMENTAL_PLUGIN(AndroidBundle.message("instant.run.notification.ir.disabled.experimental.plugin")),
+  UNKNOWN_REASON(AndroidBundle.message("instant.run.notification.ir.disabled.unknown.reason"));
+
+  private final String myUserNotification;
+
+  InstantRunGradleSupport() {
+    this(null);
+  }
+
+  InstantRunGradleSupport(@Nullable String userNotification) {
+    myUserNotification = userNotification;
+  }
+
+  @Nullable
+  public String getUserNotification() {
+    return myUserNotification;
+  }
+
+  public static InstantRunGradleSupport fromModel(@NotNull AndroidGradleModel model) throws UnsupportedOperationException {
+    int modelStatus;
+    try {
+      modelStatus = model.getSelectedVariant().getMainArtifact().getInstantRun().getSupportStatus();
+    } catch (Throwable e) {
+      throw new UnsupportedOperationException(String.format("This gradle model (%1$s) does not support querying for instant run status",
+                                                            model.getModelVersion()));
+    }
+
+    switch (modelStatus) {
+      case InstantRun.STATUS_NOT_SUPPORTED_FOR_NON_DEBUG_VARIANT:
+        return NON_DEBUG_VARIANT;
+      case InstantRun.STATUS_NOT_SUPPORTED_VARIANT_USED_FOR_TESTING:
+        return VARIANT_USED_FOR_TESTING;
+      case InstantRun.STATUS_NOT_SUPPORTED_FOR_JACK:
+        return USES_JACK;
+      case InstantRun.STATUS_NOT_SUPPORTED_FOR_EXTERNAL_NATIVE_BUILD:
+        return USES_EXTERNAL_NATIVE_BUILD;
+      case InstantRun.STATUS_NOT_SUPPORTED_FOR_EXPERIMENTAL_PLUGIN:
+        return USES_EXPERIMENTAL_PLUGIN;
+      case InstantRun.STATUS_SUPPORTED:
+        return SUPPORTED;
+      default:
+        // In case Gradle adds a new option, we need to update the IDE..
+        Logger.getInstance(InstantRunGradleSupport.class).error("Unknown instant run support status reported: " + modelStatus);
+        return UNKNOWN_REASON;
+    }
+  }
 }
