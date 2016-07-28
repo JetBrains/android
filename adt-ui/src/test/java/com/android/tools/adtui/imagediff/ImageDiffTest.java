14f69052178456d50e43dbfcb9d6687d4d928440
Amaury Medeiros <amaurym@google.com>
2016-07-28 21:26:42 +0100

    Major design refactor on adt-ui imagediff tests package
    There is a necessity of automate the process of exporting baseline
    images in the imagediff package, otherwise the images of each test
    should be export manually and that slows down the test process.
    
    This refactor changes the design to insert the concept of generators,
    which aim to decouple the action of generating a text component and
    either exporting it to an image or testing it against a baseline. In
    summary, this CL does the following:
    
    * Adds an ImageDiffEntry class that contains a baseline image file, a
      method to generate an image and a similarity threshold to compare
      both.
    * Modify the current *ImageDiffTest classes to be generators, which means
      they just generate the tests by adding entries to a list. Each entry
      contains the code to generate the components of the test.
    * Adds a list constant to ImageDiffUtil containing all the entries of
      the package. This constant should be updated when a new generator
      (and, therefore, more tests/entries) is added
    * Adds a parametrized test class (ImageDiffTest) that will run all the
      tests of the package, which will basically get all the entries from
      the constant aforementioned and test if its generated image is similar
      to the baseline
    * Adds a baseline generator class (BaselineImagesGenerator), which will
      also get all the entries from the list, but will export the image as
      the baseline instead of testing one against the other.
    
    Change-Id: I8ca2f978e059d41268a1fd8342aae6e903569fb5

---
+++ adt-ui/src/test/java/com/android/tools/adtui/imagediff/ImageDiffTest.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.adtui.imagediff;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.List;
+
+/**
+ * Parametrized test class that runs all the tests of {@link com.android.tools.adtui.imagediff}.
+ */
+@RunWith(Parameterized.class)
+public class ImageDiffTest {
+
+  @Parameterized.Parameters
+  public static List<ImageDiffEntry> getImageDiffEntries() {
+    // The parameters of this test class are all the image diff entries of the imagediff package, so it can run all the tests registered.
+    return ImageDiffUtil.IMAGE_DIFF_ENTRIES;
+  }
+
+  /**
+   * Contains the parameter of the class, which means the {@link ImageDiffEntry} being used for each test that will run.
+   */
+  private ImageDiffEntry myEntry;
+
+  public ImageDiffTest(ImageDiffEntry imageDiffEntry) {
+    myEntry = imageDiffEntry;
+  }
+
+  @Test
+  public void runTest() {
+    // Asserts that a generated image is similar (within a given threshold) to a baseline image with given name.
+    ImageDiffUtil.assertImagesSimilar(myEntry.getBaselineFilename(),
+                                      myEntry.generateComponentImage(),
+                                      myEntry.getSimilarityThreshold());
+  }
+}
