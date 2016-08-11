69dccd7c98f6d87c6218977a8a3b06fe3e3c913d
David <dyu@google.com>
2016-08-11 15:07:19 -0700

    Add two Studio UI tests for GCM and refactored common code.
     -createAppEngineJavaEndpointsModule
     -createAppEngineBackendWithCloudMessaging
    
    Change-Id: Ibf9742cc2bbbe569f1909622a4c59ed971396832

--- android-uitests/testSrc/com/android/tools/idea/tests/gui/apiintegration/DeveloperServicesTest.java
+++ android-uitests/testSrc/com/android/tools/idea/tests/gui/apiintegration/DeveloperServicesTest.java
@@ -146,6 +146,57 @@ public class DeveloperServicesTest {
   }
 
   /**
+   * Common code for the Google Cloud Module tests.
+   *
+   * @param moduleSubtype The value to choose from the "Module Type" drop down.
+   * @param sourceFileName The basename of the generated Java file.
+   * @param checkForOwnerDomainAndOwnerName true if the test should check for ownerDomain and ownerName Api parameters.
+   */
+  private void createGoogleCloudModule(
+    @NotNull String moduleSubtype, @NotNull String sourceFileName, boolean checkForOwnerDomainAndOwnerName) throws Exception {
+    final String moduleName = "backend";
+    final String packageName = "com.google.sampleapp.backend";
+    final String domainName = "backend.sampleapp.google.com";
+
+    String filePath = String.format("%s/src/main/java/%s/%s", moduleName, packageName.replace(".", "/"), sourceFileName);
+
+    IdeFrameFixture ideFrame = guiTest.importSimpleApplication();
+
+    ideFrame.openFromMenu(NewModuleDialogFixture::find, "File", "New", "New Module...")
+      .chooseModuleType("Google Cloud Module")
+      .clickNextToStep("New Google Cloud Module")
+      .chooseModuleSubtype(moduleSubtype)
+      .setModuleName(moduleName)
+      .setPackageName("com.google.sampleapp.backend")
+      .chooseClientModule("app (google.simpleapplication)")
+      .clickFinish()
+      .waitForGradleProjectSyncToFinish();
+
+    ProjectViewFixture.PaneFixture pane = ideFrame.getProjectView().selectAndroidPane();
+    assertThat(pane.hasModuleRootNode(moduleName)).isTrue();
+
+    String fileContents = guiTest.ideFrame()
+      .getEditor()
+      .open(filePath)
+      .getCurrentFileContents();
+
+    assertThat(fileContents).contains("package " + packageName + ";");
+    if (checkForOwnerDomainAndOwnerName) {
+      assertThat(fileContents).contains("ownerDomain = \"" + domainName + "\"");
+      assertThat(fileContents).contains("ownerName = \"" + domainName + "\"");
+    }
+
+    ideFrame.runNonAndroidApp(moduleName);
+    ExecutionToolWindowFixture runToolWindow = ideFrame.getRunToolWindow();
+    runToolWindow
+      .findContent(moduleName)
+      .waitForOutput(new PatternTextMatcher(Pattern.compile(".*Dev App Server is now running.*", DOTALL)), 120);
+
+    assertThat(NetUtils.canConnectToRemoteSocket(NetUtils.getLocalHostString(), 8080)).isTrue();
+    runToolWindow.findContent(moduleName).stop();
+  }
+
+  /**
    * Verifies that App Engine Java Servlet Module backend is added to Android Studio and the backend starts successfully through localhost.
    * <p>
    * This is run to qualify releases. Please involve the test team in substantial changes.
@@ -173,39 +224,68 @@ public class DeveloperServicesTest {
   @Category(GuiSanityTestSuite.class)
   @Test
   public void createAppEngineJavaServletModule() throws Exception {
-    final String MODULE_NAME = "backend";
+    createGoogleCloudModule("App Engine Java Servlet Module", "MyServlet.java", false);
+  }
 
-    boolean moduleAppears = guiTest.importSimpleApplication()
-      .openFromMenu(NewModuleDialogFixture::find, "File", "New", "New Module...")
-      .chooseModuleType("Google Cloud Module")
-      .clickNextToStep("New Google Cloud Module")
-      .chooseModuleSubtype("App Engine Java Servlet Module")
-      .setModuleName(MODULE_NAME)
-      .setPackageName("com.google.sampleapp.backend")
-      .chooseClientModule("app (google.simpleapplication)")
-      .clickFinish()
-      .waitForGradleProjectSyncToFinish()
-      .getProjectView()
-      .selectAndroidPane()
-      .hasModuleRootNode(MODULE_NAME);
-    assertThat(moduleAppears).isTrue();
+  /**
+   * Verifies that App Engine Java Endpoints Module backend is added to Android Studio and the backend starts successfully through localhost.
+   * <p>
+   * This is run to qualify releases. Please involve the test team in substantial changes.
+   * <p>
+   * TR ID: C14583301
+   * <p>
+   *   <pre>
+   *   Test Steps:
+   *   1. File > New > New Module
+   *   2. Select "Google Cloud Module"
+   *   3. For Module Type, select "App Engine Java Endpoints Module"
+   *   4. For Module Name, enter in "backend"
+   *   5. For Package Name, enter in "com.google.sampleapp.backend"
+   *   6. For Client Module, select "app (google.simpleapplication)"
+   *   7. Click "Finish" button. (Verify A)
+   *   8. Edit run configurations to select "backend" (Verify B)
+   *   Verify:
+   *   A: The values entered into Module Name and Package Name fields are used in the class.
+   *      At the root of the Project Navigator, there is a module called "backend"
+   *   B: Backend starts successfully through telnet localhost 8080.
+   *      The test gets a fixture for the output window of the Run tool and waits for the text "Dev App Server is now running" to appear,
+   *      which shows that the server is running. After that it verifies the test can connect to port 8080 on localhost.
+   *   </pre>
+   */
+  @Category(GuiSanityTestSuite.class)
+  @Test
+  public void createAppEngineJavaEndpointsModule() throws Exception {
+    createGoogleCloudModule("App Engine Java Endpoints Module", "MyEndpoint.java", true);
+  }
 
-    String fileContents = guiTest.ideFrame()
-      .getEditor()
-      .open("backend/src/main/java/com/google/sampleapp/backend/MyServlet.java")
-      .getCurrentFileContents();
-    assertThat(fileContents).contains("package com.google.sampleapp.backend;");
-
-    guiTest.ideFrame()
-      .runNonAndroidApp(MODULE_NAME)
-      .getRunToolWindow()
-      .findContent(MODULE_NAME)
-      .waitForOutput(new PatternTextMatcher(Pattern.compile(".*Dev App Server is now running.*", DOTALL)), 120);
-    assertThat(NetUtils.canConnectToRemoteSocket(NetUtils.getLocalHostString(), 8080)).isTrue();
-
-    guiTest.ideFrame()
-      .getRunToolWindow()
-      .findContent(MODULE_NAME)
-      .stop();
+  /**
+   * Verifies that App Engine Backend with GCM is added to Android Studio and the backend starts successfully through localhost.
+   * <p>
+   * This is run to qualify releases. Please involve the test team in substantial changes.
+   * <p>
+   * TR ID: C14583302
+   * <p>
+   *   <pre>
+   *   Test Steps:
+   *   1. File > New > New Module
+   *   2. Select "Google Cloud Module"
+   *   3. For Module Type, select "App Engine Backend with Google Cloud Messaging"
+   *   4. For Module Name, enter in "backend"
+   *   5. For Package Name, enter in "com.google.sampleapp.backend"
+   *   6. For Client Module, select "app (google.simpleapplication)"
+   *   7. Click "Finish" button. (Verify A)
+   *   8. Edit run configurations to select "backend" (Verify B)
+   *   Verify:
+   *   A: The values entered into Module Name and Package Name fields are used in the class.
+   *      At the root of the Project Navigator, there is a module called "backend"
+   *   B: Backend starts successfully through telnet localhost 8080.
+   *      The test gets a fixture for the output window of the Run tool and waits for the text "Dev App Server is now running" to appear,
+   *      which shows that the server is running. After that it verifies the test can connect to port 8080 on localhost.
+   *   </pre>
+   */
+  @Category(GuiSanityTestSuite.class)
+  @Test
+  public void createAppEngineBackendWithCloudMessaging() throws Exception {
+    createGoogleCloudModule("App Engine Backend with Google Cloud Messaging", "MessagingEndpoint.java", true);
   }
 }
