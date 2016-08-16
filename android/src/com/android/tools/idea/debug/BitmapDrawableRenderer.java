2c417f643b15838f3e09cf5ba38045a14f0cfc1d
Egor.Ushakov <egor.ushakov@jetbrains.com>
2016-08-16 11:48:34 +0300

    made toString - default label renderer for compound renderers

--- android/src/com/android/tools/idea/debug/BitmapDrawableRenderer.java
+++ android/src/com/android/tools/idea/debug/BitmapDrawableRenderer.java
@@ -19,13 +19,13 @@ import com.intellij.debugger.engine.FullValueEvaluatorProvider;
 import com.intellij.debugger.engine.evaluation.EvaluationContextImpl;
 import com.intellij.debugger.settings.NodeRendererSettings;
 import com.intellij.debugger.ui.impl.watch.ValueDescriptorImpl;
-import com.intellij.debugger.ui.tree.render.ToStringBasedRenderer;
+import com.intellij.debugger.ui.tree.render.CompoundReferenceRenderer;
 import com.intellij.xdebugger.frame.XFullValueEvaluator;
 import org.jetbrains.annotations.Nullable;
 
 import java.awt.image.BufferedImage;
 
-public class BitmapDrawableRenderer extends ToStringBasedRenderer implements FullValueEvaluatorProvider {
+public class BitmapDrawableRenderer extends CompoundReferenceRenderer implements FullValueEvaluatorProvider {
   public static final String BITMAP_DRAWABLE_FQCN = "android.graphics.drawable.BitmapDrawable";
 
   public BitmapDrawableRenderer(NodeRendererSettings rendererSettings) {
