JetBrains/android

commit c0843b7e0077992b46df08f837b920a5f161e870

Save debugger selection in Android Chooser dialog.
Bug: b.android.com/219729
Change-Id: Icdf1ec13f83c8419469e051e73f72f8728918e44

android/src/org/jetbrains/android/actions/AndroidProcessChooserDialog.java
@@ -68,6 +68,7 @@ public class AndroidProcessChooserDialog extends DialogWrapper {
   @NonNls private static final String DEBUGGABLE_PROCESS_PROPERTY = "DEBUGGABLE_PROCESS";
   @NonNls private static final String SHOW_ALL_PROCESSES_PROPERTY = "SHOW_ALL_PROCESSES";
   @NonNls private static final String DEBUGGABLE_DEVICE_PROPERTY = "DEBUGGABLE_DEVICE";
+  @NonNls private static final String DEBUGGER_ID_PROPERTY = "DEBUGGER_ID";
 
   private final Project myProject;
   private final boolean myShowDebuggerSelection;
@@ -101,6 +102,7 @@ public class AndroidProcessChooserDialog extends DialogWrapper {
     final PropertiesComponent properties = PropertiesComponent.getInstance(myProject);
     myLastSelectedProcess = properties.getValue(DEBUGGABLE_PROCESS_PROPERTY);
     myLastSelectedDevice = properties.getValue(DEBUGGABLE_DEVICE_PROPERTY);
+    String lastSelectedDebuggerId = properties.getValue(DEBUGGER_ID_PROPERTY);
 
     final boolean showAllProcesses = Boolean.parseBoolean(properties.getValue(SHOW_ALL_PROCESSES_PROPERTY));
 
@@ -130,7 +132,7 @@ public class AndroidProcessChooserDialog extends DialogWrapper {
 
     myShowAllProcessesCheckBox.addActionListener(e -> updateTree());
 
-    setupDebuggerSelection(showDebuggerSelection);
+    setupDebuggerSelection(showDebuggerSelection, lastSelectedDebuggerId);
 
     myProcessTree.addTreeSelectionListener(e -> {
       IDevice selectedDevice = getSelectedDevice();
@@ -184,7 +186,7 @@ public class AndroidProcessChooserDialog extends DialogWrapper {
     init();
   }
 
-  private void setupDebuggerSelection(boolean showDebuggerSelection) {
+  private void setupDebuggerSelection(boolean showDebuggerSelection, @Nullable String lastSelectedDebuggerId) {
     if (!showDebuggerSelection) {
       myDebuggerLabel.setVisible(false);
       myDebuggerTypeCombo.setVisible(false);
@@ -192,14 +194,24 @@ public class AndroidProcessChooserDialog extends DialogWrapper {
     }
 
     AndroidDebugger selectedDebugger = null;
+    AndroidDebugger defaultDebugger = null;
     List<AndroidDebugger> androidDebuggers = Lists.newLinkedList();
     for (AndroidDebugger androidDebugger: AndroidDebugger.EP_NAME.getExtensions()) {
       if (androidDebugger.supportsProject(myProject)) {
         androidDebuggers.add(androidDebugger);
-        if (androidDebugger.shouldBeDefault()) {
+        if (selectedDebugger == null &&
+            lastSelectedDebuggerId != null &&
+            androidDebugger.getId().equals(lastSelectedDebuggerId)) {
           selectedDebugger = androidDebugger;
         }
+        else if (androidDebugger.shouldBeDefault()) {
+          defaultDebugger = androidDebugger;
+        }
       }
+    }
+
+    if (selectedDebugger == null) {
+      selectedDebugger = defaultDebugger;
     }
 
     androidDebuggers.sort((left, right) -> left.getId().compareTo(right.getId()));
@@ -416,6 +428,7 @@ public class AndroidProcessChooserDialog extends DialogWrapper {
     properties.setValue(DEBUGGABLE_DEVICE_PROPERTY, getPersistableName(selectedDevice));
     properties.setValue(DEBUGGABLE_PROCESS_PROPERTY, getPersistableName(mySelectedClient));
     properties.setValue(SHOW_ALL_PROCESSES_PROPERTY, Boolean.toString(myShowAllProcessesCheckBox.isSelected()));
+    properties.setValue(DEBUGGER_ID_PROPERTY, myAndroidDebugger.getId());
 
     super.doOKAction();
   }

commit 7f816c90bd21158ff2944144e4f38f2b089a1a33

Avoid NPE in select process dialog: debugger is not always visible
In some contexts where this dialog is shown, the debugger type is not
displayed. In such a case, obtaining that value will result in an NPE.
This was a recent regression introduced in c0843b7e00779.

Fixes http://b.android.com/221030

Change-Id: I451ef19ab866320673922713b269908b343a1d9f

android/src/org/jetbrains/android/actions/AndroidProcessChooserDialog.java
@@ -428,7 +428,10 @@ public class AndroidProcessChooserDialog extends DialogWrapper {
     properties.setValue(DEBUGGABLE_DEVICE_PROPERTY, getPersistableName(selectedDevice));
     properties.setValue(DEBUGGABLE_PROCESS_PROPERTY, getPersistableName(mySelectedClient));
     properties.setValue(SHOW_ALL_PROCESSES_PROPERTY, Boolean.toString(myShowAllProcessesCheckBox.isSelected()));
-    properties.setValue(DEBUGGER_ID_PROPERTY, myAndroidDebugger.getId());
+
+    if (myAndroidDebugger != null) {
+      properties.setValue(DEBUGGER_ID_PROPERTY, myAndroidDebugger.getId());
+    }
 
     super.doOKAction();
   }