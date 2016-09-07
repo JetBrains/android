2540ac3cc48e3113c83a043626a711520e3d4ae5
Illia Syniuhin <illias@google.com>
2016-09-07 12:37:11 +0100

    GLSL: Remove unused code from lexer and fix wrong '#' handling
    Change-Id: I73111c650aedfd045f7aca75e319d8e1327c6cf0

--- android/src/com/android/tools/idea/editors/gfxtrace/lang/glsl/lexer/TokenKind.java
+++ android/src/com/android/tools/idea/editors/gfxtrace/lang/glsl/lexer/TokenKind.java
@@ -17,27 +17,25 @@ package com.android.tools.idea.editors.gfxtrace.lang.glsl.lexer;
 
 public enum TokenKind {
   BINARY_OP("binary operator"),
-  KEYWORD("keyword"),
-  NUMERIC("numeric"),
+  COMPONENTS("vector or scalar components"),
   COMMA(","),
   COMMENT("comment"),
   DOT("."),
-  FALSE("false"),
   IDENTIFIER("identifier"),
   ILLEGAL("illegal character"),
+  KEYWORD("keyword"),
   LBRACE("{"),
   LBRACKET("["),
   LPAREN("("),
+  NUMERIC("numeric"),
   NEWLINE("newline"),
   PREPROCESSOR("preprocessor directive"),
-  SPECIAL("special"),
   RBRACE("}"),
   RBRACKET("]"),
   RPAREN(")"),
   SEMI(";"),
+  SPECIAL("special"),
   STRING("string"),
-  TRUE("true"),
-  COMPONENTS("vector or scalar components"),
   // Used for all tokens which should be ignored by the highlighter.
   WHITESPACE("whitespace");
 
