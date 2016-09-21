f9e42e4c55c57ad04fd5cd997cccf09bc0b8a384
David Herman <davidherman@google.com>
2016-09-20 20:52:10 -0700

    Convert AndroidSdkSourcesBrowsingTest to use prebuilt SDK
    Change-Id: I1ea4071930c278dc709f453d0b0fc67cc9555617

--- android/testData/sdkSourcesBrowsing/to_copy/MyActivity_WithCaret.java
+++ android/testData/sdkSourcesBrowsing/to_copy/MyActivity_WithCaret.java
@@ -3,6 +3,9 @@ package p1.p2;
 import android.app.Activity;
 import android.os.Bundle;
 
+/**
+ * Tests that navigation to the SDK base class will work.
+ */
 public class MyActivity extends Act<caret>ivity {
   @Override
   public void onCreate(Bundle savedInstanceState) {
