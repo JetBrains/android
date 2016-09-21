6085a26cefee5362a691d0f45ded2354ff6b5ad0
Dana Dahlstrom <dahlstrom@google.com>
2016-09-21 13:42:15 -0700

    In UI tests, use prebuilts/studio/sdk
    … instead of ANDROID_HOME, by calling TestUtils.getSdk instead of
    deprecated AndroidTestCaseHelper.getAndroidSdkPath in
    GuiTests.setUpSdks.
    
    Change-Id: Idf0c06cf95d5ec4c063fb9a8aaf048f50ee3b362

--- android-uitests/testSrc/com/android/tools/idea/tests/gui/gradle/flavors/CreateNewFlavorsTest.java
+++ android-uitests/testSrc/com/android/tools/idea/tests/gui/gradle/flavors/CreateNewFlavorsTest.java
@@ -61,8 +61,8 @@ public class CreateNewFlavorsTest {
       .selectFlavorsTab()
       .clickAddButton()
       .setFlavorName("flavor1")
-      .setMinSdkVersion("API 18: Android 4.3 (Jelly Bean)")
-      .setTargetSdkVersion("API 22: Android 5.1 (Lollipop)")
+      .setMinSdkVersion("API 24: Android 7.0 (Nougat)")
+      .setTargetSdkVersion("API 24: Android 7.0 (Nougat)")
       .clickAddButton()
       .setFlavorName("flavor2")
       .setVersionCode("2")
@@ -71,7 +71,7 @@ public class CreateNewFlavorsTest {
       .getEditor()
       .open("/app/build.gradle")
       .getCurrentFileContents();
-    assertThat(gradleFileContents).contains("flavor1 {\n            minSdkVersion 18\n            targetSdkVersion 22\n        }");
+    assertThat(gradleFileContents).contains("flavor1 {\n            minSdkVersion 24\n            targetSdkVersion 24\n        }");
     assertThat(gradleFileContents).contains("flavor2 {\n            versionCode 2\n            versionName '2.3'\n        }");
   }
 }
