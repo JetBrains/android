1744ced1f50d75a919e9ae88ad0a39e501909b0b
Dana Dahlstrom <dahlstrom@google.com>
2016-09-22 01:44:15 -0700

    In JackAndJillApp, remove dep on appcompat
    … which isn't available in prebuilts/studio/sdk.
    
    Change-Id: I000000000037593a2caf469ec555420000000000

--- android-uitests/testData/JackAndJillApp/app/src/main/java/com/example/jackandjillapp/MainActivity.java
+++ android-uitests/testData/JackAndJillApp/app/src/main/java/com/example/jackandjillapp/MainActivity.java
@@ -1,12 +1,12 @@
 package com.example.jackandjillapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
