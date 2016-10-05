c479e8f045523314b4362a72034ee594de43c140
David Herman <davidherman@google.com>
2016-10-05 15:37:29 -0700

    Remove redundant TestProjectPathsGeneratorTest blacklist entry
    Change-Id: If0c3f463a3b97e0754e301f434faf11d67d1095b

--- android/testSrc/com/android/tools/idea/IdeaTestSuite.java
+++ android/testSrc/com/android/tools/idea/IdeaTestSuite.java
@@ -93,7 +93,6 @@ import java.nio.file.Paths;
   com.android.tools.idea.templates.GradleFileSimpleMergerTest.class,
   com.android.tools.idea.templates.RepositoryUrlManagerTest.class,
   com.android.tools.idea.templates.UniqueParameterTest.class,
-  com.android.tools.idea.testing.TestProjectPathsGeneratorTest.class,
   com.android.tools.idea.uibuilder.property.editors.StyleFilterTest.class,
   com.android.tools.idea.uibuilder.structure.NlComponentTreeTest.class,
   com.android.tools.idea.uibuilder.surface.InteractionManagerTest.class,
