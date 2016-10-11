f6ce09424f29e7a6b5b4928e1e6b4b0d4e9d3b18
Billy Lam <shiufai@google.com>
2016-10-11 16:41:11 -0700

    Update profiler L1 axis usage to match mock
    - Changed the left/right axis orientations
      so they appear flushed to the edges
    - Switched the left/right content panel to be
      overlay and got rid of additional spacer
      handling logic.
    
    Change-Id: Id0cb5e50e443029db639376ca692ff8b310b71db

--- android/src/com/android/tools/idea/monitor/ui/BaseLineChartSegment.java
+++ android/src/com/android/tools/idea/monitor/ui/BaseLineChartSegment.java
@@ -74,9 +74,6 @@ public abstract class BaseLineChartSegment extends BaseSegment {
   private AxisComponent mRightAxis;
 
   @NotNull
-  private final BaseAxisFormatter mLeftAxisFormatterSimple;
-
-  @NotNull
   private final BaseAxisFormatter mLeftAxisFormatter;
 
   @Nullable
@@ -109,14 +106,12 @@ public abstract class BaseLineChartSegment extends BaseSegment {
   public BaseLineChartSegment(@NotNull String name,
                               @NotNull Range xRange,
                               @NotNull SeriesDataStore dataStore,
-                              @NotNull BaseAxisFormatter leftAxisFormatterSimple,
                               @NotNull BaseAxisFormatter leftAxisFormatter,
                               @Nullable BaseAxisFormatter rightAxisFormatter,
                               @Nullable Range leftAxisRange,
                               @Nullable Range rightAxisRange,
                               @NotNull EventDispatcher<ProfilerEventListener> dispatcher) {
     super(name, xRange, dispatcher);
-    mLeftAxisFormatterSimple = leftAxisFormatterSimple;
     mLeftAxisFormatter = leftAxisFormatter;
     mRightAxisFormatter = rightAxisFormatter;
     mLeftAxisRange = leftAxisRange != null ? leftAxisRange : new Range();
@@ -130,11 +125,10 @@ public abstract class BaseLineChartSegment extends BaseSegment {
   public BaseLineChartSegment(@NotNull String name,
                               @NotNull Range xRange,
                               @NotNull SeriesDataStore dataStore,
-                              @NotNull BaseAxisFormatter leftAxisFormatterSimple,
                               @NotNull BaseAxisFormatter leftAxisFormatter,
                               @Nullable BaseAxisFormatter rightAxisFormatter,
                               @NotNull EventDispatcher<ProfilerEventListener> dispatcher) {
-    this(name, xRange, dataStore, leftAxisFormatterSimple, leftAxisFormatter, rightAxisFormatter, null, null, dispatcher);
+    this(name, xRange, dataStore, leftAxisFormatter, rightAxisFormatter, null, null, dispatcher);
   }
 
   public abstract BaseProfilerUiManager.ProfilerType getProfilerType();
@@ -142,18 +136,19 @@ public abstract class BaseLineChartSegment extends BaseSegment {
   @Override
   public void createComponentsList(@NotNull List<Animatable> animatables) {
     // left axis
-    AxisComponent.Builder builder = new AxisComponent.Builder(mLeftAxisRange, mLeftAxisFormatterSimple, AxisComponent.AxisOrientation.LEFT)
+    AxisComponent.Builder builder = new AxisComponent.Builder(mLeftAxisRange, mLeftAxisFormatter, AxisComponent.AxisOrientation.RIGHT)
       .showMinMax(true)
+      .showAxisLine(false)
       .clampToMajorTicks(true);
     mLeftAxis = builder.build();
 
     // right axis
     if (mRightAxisFormatter != null) {
-      builder = new AxisComponent.Builder(mRightAxisRange, mRightAxisFormatter, AxisComponent.AxisOrientation.RIGHT)
+      builder = new AxisComponent.Builder(mRightAxisRange, mRightAxisFormatter, AxisComponent.AxisOrientation.LEFT)
         .showMinMax(true)
+        .showAxisLine(false)
         .setParentAxis(mLeftAxis);
       mRightAxis = builder.build();
-      mRightAxis.setParentAxis(mLeftAxis);
       mRightAxis.setVisible(false);
     }
 
@@ -212,7 +207,6 @@ public abstract class BaseLineChartSegment extends BaseSegment {
   protected void setRightContent(@NotNull JPanel panel) {
     if (mRightAxisFormatter != null) {
       panel.add(mRightAxis, BorderLayout.CENTER);
-      setRightSpacerVisible(true);
     }
   }
 
@@ -318,7 +312,6 @@ public abstract class BaseLineChartSegment extends BaseSegment {
   public void toggleView(boolean isExpanded) {
     super.toggleView(isExpanded);
     mGrid.setVisible(isExpanded);
-    mLeftAxis.setAxisFormatter(isExpanded ? mLeftAxisFormatter : mLeftAxisFormatterSimple);
     mLineChart.clearConfigs();
     updateChartLines(isExpanded);
     mLegendComponent.setLegendData(getLegendRenderDataList());
--- android/src/com/android/tools/idea/monitor/ui/BaseSegment.java
+++ android/src/com/android/tools/idea/monitor/ui/BaseSegment.java
@@ -18,7 +18,8 @@ package com.android.tools.idea.monitor.ui;
 import com.android.tools.adtui.Animatable;
 import com.android.tools.adtui.Range;
 import com.android.tools.adtui.common.AdtUiUtils;
-import com.android.tools.adtui.common.RotatedLabel;
+import com.intellij.ui.components.JBLabel;
+import com.intellij.ui.components.JBLayeredPane;
 import com.intellij.ui.components.JBPanel;
 import com.intellij.util.EventDispatcher;
 import org.jetbrains.annotations.NotNull;
@@ -29,32 +30,37 @@ import javax.swing.border.CompoundBorder;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.MatteBorder;
 import java.awt.*;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
 import java.util.List;
 
 public abstract class BaseSegment extends JComponent {
 
-  private static final int SPACER_WIDTH = 100;
-
   /**
    * Top/bottom border between segments.
    */
-  private static final Border SEGMENT_BORDER = new CompoundBorder(new MatteBorder(0, 0, 1, 0, AdtUiUtils.DEFAULT_BORDER_COLOR),
-                                                                   new EmptyBorder(0, 0, 0, 0));
-
-  private static final int LABEL_BORDER_WIDTH = 2;
+  public static final Border SEGMENT_BORDER = new CompoundBorder(new MatteBorder(0, 0, 1, 0, AdtUiUtils.DEFAULT_BORDER_COLOR),
+                                                                  new EmptyBorder(0, 0, 0, 0));
 
   /**
-   * Border around the segment label.
+   * Padding for the header labels.
    */
-  private static final Border LABEL_BORDER = new MatteBorder(0, 0, 0, LABEL_BORDER_WIDTH, AdtUiUtils.DEFAULT_BORDER_COLOR);
+  private static final Border LABEL_BORDER = BorderFactory.createEmptyBorder(4, 4, 4, 4);
 
-  private JPanel mRightPanel;
+  private static final int OVERLAY_CONTENT_WIDTH = 100;
+
+  @NotNull
+  private JBLayeredPane mLayeredPane;
+
+  private JLabel mLeftLabel;
 
   private JPanel mLeftPanel;
 
-  private JPanel mLabelPanel;
+  private JLabel mRightLabel;
 
-  private RotatedLabel mLabel;
+  private JPanel mRightPanel;
+
+  private JPanel mCenterPanel;
 
   @NotNull
   protected final String myName;
@@ -71,86 +77,103 @@ public abstract class BaseSegment extends JComponent {
     mEventDispatcher = dispatcher;
   }
 
-  public static int getSpacerWidth() {
-    return SPACER_WIDTH;
+  public static int getOverlayContentWidth() {
+    return OVERLAY_CONTENT_WIDTH;
   }
 
   public void initializeComponents() {
     setLayout(new BorderLayout());
 
-    FontMetrics metrics = getFontMetrics(AdtUiUtils.DEFAULT_FONT);
-    mLabelPanel = createSpacerPanel(metrics.getHeight() + LABEL_BORDER_WIDTH);
-    mLabelPanel.setBorder(LABEL_BORDER);
-    mLabel = new RotatedLabel();
-    mLabel.setFont(AdtUiUtils.DEFAULT_FONT);
-    mLabel.setText(myName);
-    mLabel.setBorder(SEGMENT_BORDER);
-    mLabelPanel.add(mLabel);
-    this.add(mLabelPanel, BorderLayout.WEST);
+    // The layout of a segment is a 2x3 grid, with the main content spanning the entire second row. Optional content (left/right
+    // axes) are layered on top of the main content in the first/third columns. The three columns in the first row are used to display
+    // additional info associated with the left/main/right content respectively.
+    JBPanel panel = new JBPanel();
+    panel.setLayout(new GridBagLayout());
 
-    JBPanel panels = new JBPanel();
-    panels.setBorder(SEGMENT_BORDER);
-    GridBagConstraints gbc = new GridBagConstraints();
-    gbc.fill = GridBagConstraints.BOTH;
-    panels.setLayout(new GridBagLayout());
-
-    gbc.weightx = 0;
-    gbc.weighty = 0;
-
-    //Setup the left panel
-    if (hasLeftContent()) {
-      mLeftPanel = createSpacerPanel(getSpacerWidth());
-      gbc.gridx = 0;
-      gbc.gridy = 1;
-      panels.add(mLeftPanel, gbc);
-      setLeftContent(mLeftPanel);
-    }
-
-    //Setup the top center panel.
+    // Setup the top center panel.
     JBPanel topPanel = new JBPanel();
     topPanel.setLayout(new BorderLayout());
+    GridBagConstraints gbc = new GridBagConstraints();
+    gbc.fill = GridBagConstraints.BOTH;
     gbc.gridx = 1;
     gbc.gridy = 0;
-    panels.add(topPanel, gbc);
+    gbc.weightx = 1;
+    gbc.weighty = 0;
+    panel.add(topPanel, gbc);
     setTopCenterContent(topPanel);
+
+    mLayeredPane = new JBLayeredPane();
+    //Setup the left panel
+    if (hasLeftContent()) {
+      mLeftPanel = new JBPanel();
+      mLeftPanel.setLayout(new BorderLayout());
+      mLeftPanel.setOpaque(false);
+      mLayeredPane.add(mLeftPanel);
+      setLeftContent(mLeftPanel);
+
+      mLeftLabel = new JBLabel(getLeftContentLabel());
+      mLeftLabel.setBorder(LABEL_BORDER);
+      gbc.gridx = 0;
+      gbc.gridy = 0;
+      gbc.weightx = 0;
+      gbc.weighty = 0;
+      panel.add(mLeftLabel, gbc);
+    }
 
     //Setup the right panel
     if (hasRightContent()) {
-      mRightPanel = createSpacerPanel(getSpacerWidth());
-      gbc.gridx = 2;
-      gbc.gridy = 1;
-      panels.add(mRightPanel, gbc);
+      mRightPanel = new JBPanel();
+      mRightPanel.setLayout(new BorderLayout());
+      mRightPanel.setOpaque(false);
+      mLayeredPane.add(mRightPanel);
       setRightContent(mRightPanel);
+
+      mRightLabel = new JBLabel(getRightContentLabel());
+      mRightLabel.setBorder(LABEL_BORDER);
+      gbc.gridx = 2;
+      gbc.gridy = 0;
+      gbc.weightx = 0;
+      gbc.weighty = 0;
+      panel.add(mRightLabel, gbc);
     }
 
     //Setup the center panel, the primary component.
-    //This component should consume all available space.
-    JBPanel centerPanel = new JBPanel();
-    centerPanel.setLayout(new BorderLayout());
+    mCenterPanel = new JBPanel();
+    mCenterPanel.setLayout(new BorderLayout());
+    mLayeredPane.add(mCenterPanel);
+    setCenterContent(mCenterPanel);
+
+    mLayeredPane.addComponentListener(new ComponentAdapter() {
+      @Override
+      public void componentResized(ComponentEvent e) {
+        JLayeredPane host = (JLayeredPane)e.getComponent();
+        if (host != null) {
+          Dimension dim = host.getSize();
+          for (Component c : host.getComponents()) {
+            if (c == mLeftPanel) {
+              c.setBounds(0, 0, OVERLAY_CONTENT_WIDTH, dim.height);
+            }
+            else if (c == mRightPanel) {
+              c.setBounds(dim.width - OVERLAY_CONTENT_WIDTH, 0, OVERLAY_CONTENT_WIDTH, dim.height);
+            }
+            else if (c == mCenterPanel) {
+              c.setBounds(0, 0, dim.width, dim.height);
+            }
+          }
+        }
+      }
+    });
+
+    gbc.gridx = 0;
+    gbc.gridy = 1;
+    gbc.gridwidth = 3;
     gbc.weightx = 1;
     gbc.weighty = 1;
-    gbc.gridx = 1;
-    gbc.gridy = 1;
-    panels.add(centerPanel, gbc);
-    setCenterContent(centerPanel);
+    panel.add(mLayeredPane, gbc);
+    add(panel, BorderLayout.CENTER);
 
-    add(panels, BorderLayout.CENTER);
-
-    // By default, starts in L1, this gives the Segment a chance to hide the right spacer panel and determine what it should rendered.
+    // By default, starts in L1, this gives the Segment a chance to determine what it should rendered.
     toggleView(false);
-  }
-
-  public int getLabelColumnWidth() {
-    return mLabelPanel.getPreferredSize().width;
-  }
-
-  private JPanel createSpacerPanel(int spacerWidth) {
-    JBPanel panel = new JBPanel();
-    panel.setLayout(new BorderLayout());
-    Dimension spacerDimension = new Dimension(spacerWidth, 0);
-    panel.setPreferredSize(spacerDimension);
-    panel.setMinimumSize(spacerDimension);
-    return panel;
   }
 
   /**
@@ -161,26 +184,8 @@ public abstract class BaseSegment extends JComponent {
   public void setRightSpacerVisible(boolean isVisible) {
     if (hasRightContent()) {
       mRightPanel.setVisible(isVisible);
+      mRightLabel.setVisible(isVisible);
     }
-  }
-
-  public boolean isRightSpacerVisible() {
-    return hasRightContent() && mRightPanel.isVisible();
-  }
-
-  /**
-   * This enables segments to toggle the visibility of the left panel.
-   *
-   * @param isVisible True indicates the panel is visible, false hides it.
-   */
-  public void setLeftSpacerVisible(boolean isVisible) {
-    if (hasLeftContent()) {
-      mLeftPanel.setVisible(isVisible);
-    }
-  }
-
-  public boolean isLeftSpacerVisible() {
-    return hasLeftContent() && mLeftPanel.isVisible();
   }
 
   public void toggleView(boolean isExpanded) {
@@ -190,10 +195,9 @@ public abstract class BaseSegment extends JComponent {
   public void createComponentsList(@NotNull List<Animatable> animatables) {}
 
   /**
-   * A read-only flag that indicates whether this segment has content in the left spacer. If true, the segment will construct and reverse
-   * space left of the center content, and {@link #setLeftContent(JPanel)} will be invoked. Subclasses of {@link BaseSegment} can override
-   * this to change how the segment is laid out and whether it will participate in any transitions toggling between the overview and
-   * detailed view.
+   * A read-only flag that indicates whether this segment has left overlay content. If true, {@link #setLeftContent(JPanel)} will be
+   * invoked. Subclasses of {@link BaseSegment} can override this to change how the segment is laid out and whether it will participate in
+   * any transitions toggling between the overview and detailed view.
    */
   protected boolean hasLeftContent() {
     return true;
@@ -201,19 +205,33 @@ public abstract class BaseSegment extends JComponent {
 
   protected void setLeftContent(@NotNull JPanel panel) {}
 
+  /**
+   * Label for the left overlay content which is rendered at the top left corner of the segment.
+   */
+  protected String getLeftContentLabel() {
+    return myName;
+  }
+
   protected abstract void setCenterContent(@NotNull JPanel panel);
 
   /**
-   * A read-only flag that indicates whether this segment has content in the right spacer. If true, the segment will construct and reverse
-   * space right of the center content, and {@link #setRightContent(JPanel)} will be invoked. Subclasses of {@link BaseSegment} can override
-   * this to change how the segment is laid out and whether it will participate in any transitions toggling between the overview and
-   * detailed view.
+   * A read-only flag that indicates whether this segment has right overlay content. If true, {@link #setRightContent(JPanel)} will be
+   * invoked. Subclasses of {@link BaseSegment} can override this to change how the segment is laid out and whether it will participate in
+   * any transitions toggling between the overview and detailed view.
    */
   protected boolean hasRightContent() {
     return true;
   }
 
   protected void setRightContent(@NotNull JPanel panel) {}
+
+  /**
+   * Label for the right overlay content which is rendered at the top right corner of the segment. By default, subclasses need to override
+   * this to return an non-empty string, and it is only used if {@link #hasRightContent()} returns true.
+   */
+  protected String getRightContentLabel() {
+    return "";
+  }
 
   protected void setTopCenterContent(@NotNull JPanel panel) {}
 }
