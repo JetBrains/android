dc698b06c405b66654319bea3126cf5ee12c0a7c
Billy Lam <shiufai@google.com>
2016-10-14 19:30:07 +0000

    Revert "Update profiler L1 axis usage to match mock"
    The mock is still WIP and seems like we are going back to the old scheme. Reverting this for now and waiting for the mocks to stabilize.
    
    This reverts commit d36a391805560ec46f3082a5fce1d02288aa91d2.
    
    Change-Id: Ic59229c58d1a14d17eccb41d229c16899865fbf7

--- android/src/com/android/tools/idea/monitor/tool/AndroidMonitorToolWindow.java
+++ android/src/com/android/tools/idea/monitor/tool/AndroidMonitorToolWindow.java
@@ -291,18 +291,23 @@ public class AndroidMonitorToolWindow implements Disposable {
 
     // Add Selection Overlay
     gbc.fill = GridBagConstraints.BOTH;
-    gbc.gridx = 0;
+    gbc.gridx = 2;
     gbc.gridy = 0;
+    gbc.gridwidth = 1;
     gbc.weightx = 1;
     gbc.weighty = 1;
     gridBagPanel.add(mySelection, gbc);
 
     // Add Accordion Control
+    gbc.gridx = 0;
+    gbc.gridy = 0;
+    gbc.gridwidth = 4;
+    gbc.weightx = 0;
+    gbc.weighty = 0;
     gridBagPanel.add(mySegmentsContainer, gbc);
 
-    // Add Horizontal Scrollbar
+    // Add Scrollbar
     gbc.gridy = 1;
-    gbc.weighty = 0;
     gridBagPanel.add(myScrollbar, gbc);
 
     // Setup profiler segments
@@ -318,6 +323,26 @@ public class AndroidMonitorToolWindow implements Disposable {
     timeSegment.setBorder(BorderFactory.createLineBorder(JBColor.GRAY));
     timeSegment.initializeComponents();
     mySegmentsContainer.add(timeSegment);
+
+    // Add left spacer
+    Dimension leftSpacer = new Dimension(BaseSegment.getSpacerWidth() + timeSegment.getLabelColumnWidth(), 0);
+    gbc.gridy = 0;
+    gbc.gridx = 0;
+    gbc.gridwidth = 2;
+    gbc.weightx = 0;
+    gbc.weighty = 0;
+    gridBagPanel.add(new Box.Filler(leftSpacer, leftSpacer, leftSpacer), gbc);
+
+    // Add right spacer
+    Dimension rightSpacer = new Dimension(BaseSegment.getSpacerWidth(), 0);
+    gbc.gridy = 0;
+    gbc.gridx = 3;
+    gbc.gridwidth = 1;
+    gbc.weightx = 0;
+    gbc.weighty = 0;
+    Component rightSpacerFiller = new Box.Filler(rightSpacer, rightSpacer, rightSpacer);
+    gridBagPanel.add(rightSpacerFiller, gbc);
+    rightSpacerFiller.setVisible(false);
 
     mySplitter.setFirstComponent(gridBagPanel);
     myComponent.add(mySplitter);
@@ -356,6 +381,8 @@ public class AndroidMonitorToolWindow implements Disposable {
         }
 
         if (firstExpansion) {
+          timeSegment.toggleView(true);
+          rightSpacerFiller.setVisible(true);
           myCollapseSegmentsButton.setVisible(true);
         }
       }
@@ -370,6 +397,8 @@ public class AndroidMonitorToolWindow implements Disposable {
         mySplitter.setSecondComponent(null);
         mySplitter.setProportion(1f);
 
+        timeSegment.toggleView(false);
+        rightSpacerFiller.setVisible(false);
         myCollapseSegmentsButton.setVisible(false);
         myExpandedProfiler = null;
       }
