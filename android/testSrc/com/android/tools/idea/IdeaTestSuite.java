2fe09907c8b8d040255f5b21fb0565d6c67b9bc1
Yuxin Yu <yuxy@google.com>
2016-10-20 23:42:06 +0000

    Revert "Remove passing tests from bazel blacklist."
    This reverts commit feee06c7614540a80d10289b7224af9f08750144.
    
    Change-Id: Ia139c3629c2909a76f908e54f66888ae8a5958be

--- android/testSrc/com/android/tools/idea/IdeaTestSuite.java
+++ android/testSrc/com/android/tools/idea/IdeaTestSuite.java
@@ -76,6 +76,9 @@ import java.nio.file.Paths;
   com.android.tools.idea.actions.annotations.InferSupportAnnotationsTest.class,
   org.jetbrains.android.dom.AndroidValueResourcesTest.class,
   org.jetbrains.android.dom.CreateMissingClassFixTest.class,
+
+  // Disable as it is breaking build, for linux only.
+  org.jetbrains.android.dom.AndroidLayoutDomTest.class,
 })
 public class IdeaTestSuite {
 
