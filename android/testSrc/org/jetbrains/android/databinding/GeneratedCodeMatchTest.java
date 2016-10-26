b93bbea5c055ad380787c8a3335d23b361318fcc
Yigit Boyar <yboyar@google.com>
2016-10-25 20:57:48 -0700

    disable data binding test
    Change-Id: I9d87bc4305848acf49922d7610d365c68a8163eb

--- android/testSrc/org/jetbrains/android/databinding/GeneratedCodeMatchTest.java
+++ android/testSrc/org/jetbrains/android/databinding/GeneratedCodeMatchTest.java
@@ -78,9 +78,8 @@ public class GeneratedCodeMatchTest extends AndroidGradleTestCase {
                          SdkConstants.GRADLE_PLUGIN_WH_VERSION, "25.0.0");
   }
 
-  // TODO: This test needs to be investigated as to why dataBinding = enabled is not adding
-  // databinding library dependencies to the Android Gradle model.
-  public void testGeneratedCodeMatch() throws Exception {
+  // re-enable this test after we can run tests against top of tree gradle build
+  public void ignored_testGeneratedCodeMatch() throws Exception {
     loadProject(PROJECT_WITH_DATA_BINDING);
     // temporary fix until test model can detect dependencies properly
     GradleInvocationResult assembleDebug = invokeGradleTasks(getProject(), "assembleDebug");
