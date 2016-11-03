db26d84dfdb4d27fcadbead72eccd03be5f48c68
Lucas Smaira <lsmaira@google.com>
2016-11-03 14:47:37 +0000

    Fixing search scopes in JUnit and Android Tests
    When double clicking a failed UnitTest or AndroidTest it often takes
    user to the wrong file if there are both Android and Unit test classes
    with the same name. It happens because the GlobalSearchScope is not
    correct.
    
    This CL fixes AndroidTest and JUnit scopes by implementing the method
    initScopes() in both AndroidTestConsoleProperties and
    AndroidJUnitConsoleProperties.
    
    Test: com.android.tools.idea.run.testing.DuplicateClassNameTestScopesTest
    This change is tested by creating an android application with two
    different classes with the same name (in different test scopes) and
    verifying if the locator finds only the correct one.
    
    Fixes bug: http://b.android.com/226064
    
    Change-Id: I6c46e6ad40133beada501a9bc268d22b15d8169b

--- android-test-framework/testSrc/com/android/tools/idea/testing/TestProjectPaths.java
+++ android-test-framework/testSrc/com/android/tools/idea/testing/TestProjectPaths.java
@@ -44,6 +44,7 @@ public final class TestProjectPaths {
   public static final String SPLIT_BUILD_FILES = "projects/splitBuildFiles";
   public static final String SYNC_MULTIPROJECT = "projects/sync/multiproject";
   public static final String TEST_ARTIFACTS_MULTIPROJECT = "projects/testArtifacts/multiproject";
+  public static final String TEST_ARTIFACTS_SAME_NAME_CLASSES = "projects/testArtifacts/sameNameClasses";
   public static final String TEST_ONLY_MODULE = "projects/testOnlyModule";
   public static final String TRANSITIVE_DEPENDENCIES = "projects/transitiveDependencies";
   public static final String UIBUILDER_PROPERTY = "projects/uibuilder/property";
