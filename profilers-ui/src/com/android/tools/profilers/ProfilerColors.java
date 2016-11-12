45daeb80eaca24ef78e5f577ebab62145fdb4b82
estebandlc <estebandlc@google.com>
2016-11-12 14:29:39 -0800

    Tweaks colors and fill styles
    Test: N/A
    Change-Id: Ia1fe2ebec8b5b06943b899a1289970f1f78af51f

---
+++ profilers-ui/src/com/android/tools/profilers/ProfilerColors.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.profilers;
+
+import com.intellij.ui.JBColor;
+
+import java.awt.*;
+
+public class ProfilerColors {
+
+  public static final Color CPU_USAGE = new JBColor(0x62C88E, 0x62C88E);
+
+  public static final Color TOTAL_MEMORY = new JBColor(0x62b7df, 0x62b7df);
+}
