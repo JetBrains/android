adabdb3ea950ef98d25ab7524278049f989ab4d5
estebandlc <estebandlc@google.com>
2016-11-11 21:44:35 -0800

    Adds event monitoring.
    Simple events and activity events are back!
    Rendering is place holder, and the monitor itself doesn't remain the same size, but scales along with the others.
    
    Test: New feature, tests to follow.
    Change-Id: I285872c664d4168050d931a31b599b2c49ace15f

---
+++ profilers/src/com/android/tools/profilers/event/EventProfiler.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.profilers.event;
+
+import com.android.tools.profiler.proto.EventProfiler.EventStartRequest;
+import com.android.tools.profiler.proto.EventProfiler.EventStopRequest;
+import com.android.tools.profiler.proto.Profiler;
+import com.android.tools.profilers.ProfilerMonitor;
+import com.android.tools.profilers.StudioProfiler;
+import com.android.tools.profilers.StudioProfilers;
+import org.jetbrains.annotations.NotNull;
+
+public class EventProfiler extends StudioProfiler{
+  public EventProfiler(@NotNull StudioProfilers profilers) {
+    super(profilers);
+  }
+
+  @Override
+  public ProfilerMonitor newMonitor(int processId) {
+    return new EventMonitor(myProfilers, processId);
+  }
+
+  @Override
+  public void startProfiling(Profiler.Process process) {
+    myProfilers.getClient().getEventClient().startMonitoringApp(EventStartRequest.newBuilder().setAppId(process.getPid()).build());
+  }
+
+  @Override
+  public void stopProfiling(Profiler.Process process) {
+    myProfilers.getClient().getEventClient().stopMonitoringApp(EventStopRequest.newBuilder().setAppId(process.getPid()).build());
+  }
+}
