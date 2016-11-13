7aa9cda907574894f164cfdb5fb274e053e6b5b2
Alex Ruiz <alruiz@google.com>
2016-11-12 16:13:55 -0800

    Removed code duplication in sync data services. (CL 1 of 2.)
    Now all sync data services extends a common class
    (ModuleModelDataService.) This new base class
    extract functionality that has been duplicated in
    all sync data services.
    
    Test: N/A
    Change-Id: I177dd4f4f6235984596fee42a7d1443b85bcb563

--- android/src/com/android/tools/idea/gradle/project/sync/idea/data/service/GradleModuleModelDataService.java
+++ android/src/com/android/tools/idea/gradle/project/sync/idea/data/service/GradleModuleModelDataService.java
@@ -16,23 +16,15 @@
 package com.android.tools.idea.gradle.project.sync.idea.data.service;
 
 import com.android.annotations.VisibleForTesting;
-import com.android.tools.idea.gradle.project.sync.GradleSyncState;
 import com.android.tools.idea.gradle.project.facet.gradle.GradleFacet;
 import com.android.tools.idea.gradle.project.model.GradleModuleModel;
 import com.android.tools.idea.gradle.project.sync.setup.module.GradleModuleSetup;
-import com.google.common.collect.Maps;
-import com.intellij.openapi.application.RunResult;
-import com.intellij.openapi.command.WriteCommandAction;
-import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.externalSystem.model.DataNode;
 import com.intellij.openapi.externalSystem.model.Key;
-import com.intellij.openapi.externalSystem.model.project.ProjectData;
 import com.intellij.openapi.externalSystem.service.project.IdeModifiableModelsProvider;
-import com.intellij.openapi.externalSystem.service.project.manage.AbstractProjectDataService;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.project.Project;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 import java.util.Collection;
 import java.util.Map;
@@ -43,7 +35,7 @@ import static com.android.tools.idea.gradle.util.Facets.removeAllFacetsOfType;
 /**
  * Applies Gradle settings to the modules of an Android project.
  */
-public class GradleModuleModelDataService extends AbstractProjectDataService<GradleModuleModel, Void> {
+public class GradleModuleModelDataService extends ModuleModelDataService<GradleModuleModel> {
   @NotNull private final GradleModuleSetup myModuleSetup;
 
   @SuppressWarnings("unused") // Instantiated by IDEA
@@ -63,62 +55,21 @@ public class GradleModuleModelDataService extends AbstractProjectDataService<Gra
   }
 
   @Override
-  public void importData(@NotNull Collection<DataNode<GradleModuleModel>> toImport,
-                         @Nullable ProjectData projectData,
-                         @NotNull Project project,
-                         @NotNull IdeModifiableModelsProvider modelsProvider) {
-    if (!toImport.isEmpty()) {
-      try {
-        doImport(toImport, project, modelsProvider);
+  protected void importData(@NotNull Collection<DataNode<GradleModuleModel>> toImport,
+                            @NotNull Project project,
+                            @NotNull IdeModifiableModelsProvider modelsProvider,
+                            @NotNull Map<String, GradleModuleModel> modelsByName) {
+    for (Module module : modelsProvider.getModules()) {
+      GradleModuleModel gradleModuleModel = modelsByName.get(module.getName());
+      if (gradleModuleModel == null) {
+        // This happens when there is an orphan IDEA module that does not map to a Gradle project. One way for this to happen is when
+        // opening a project created in another machine, and Gradle import assigns a different name to a module. Then, user decides
+        // not to delete the orphan module when Studio prompts to do so.
+        removeAllFacetsOfType(GradleFacet.getFacetTypeId(), modelsProvider.getModifiableFacetModel(module));
       }
-      catch (Throwable e) {
-        getLog().error(String.format("Failed to set up modules in project '%1$s'", project.getName()), e);
-        GradleSyncState.getInstance(project).syncFailed(e.getMessage());
+      else {
+        myModuleSetup.setUpModule(module, modelsProvider, gradleModuleModel);
       }
     }
-  }
-
-  @NotNull
-  private static Logger getLog() {
-    return Logger.getInstance(GradleModuleModelDataService.class);
-  }
-
-  private void doImport(@NotNull Collection<DataNode<GradleModuleModel>> toImport,
-                        @NotNull Project project,
-                        @NotNull IdeModifiableModelsProvider modelsProvider) throws Throwable {
-    RunResult result = new WriteCommandAction.Simple(project) {
-      @Override
-      protected void run() throws Throwable {
-        if (!project.isDisposed()) {
-          Map<String, GradleModuleModel> gradleProjectsByName = indexByModuleName(toImport);
-          for (Module module : modelsProvider.getModules()) {
-            GradleModuleModel gradleModuleModel = gradleProjectsByName.get(module.getName());
-            if (gradleModuleModel == null) {
-              // This happens when there is an orphan IDEA module that does not map to a Gradle project. One way for this to happen is when
-              // opening a project created in another machine, and Gradle import assigns a different name to a module. Then, user decides
-              // not to delete the orphan module when Studio prompts to do so.
-              removeAllFacetsOfType(GradleFacet.getFacetTypeId(), modelsProvider.getModifiableFacetModel(module));
-            }
-            else {
-              myModuleSetup.setUpModule(module, modelsProvider, gradleModuleModel);
-            }
-          }
-        }
-      }
-    }.execute();
-    Throwable error = result.getThrowable();
-    if (error != null) {
-      throw error;
-    }
-  }
-
-  @NotNull
-  private static Map<String, GradleModuleModel> indexByModuleName(@NotNull Collection<DataNode<GradleModuleModel>> dataNodes) {
-    Map<String, GradleModuleModel> gradleProjectsByModuleName = Maps.newHashMap();
-    for (DataNode<GradleModuleModel> d : dataNodes) {
-      GradleModuleModel gradleModuleModel = d.getData();
-      gradleProjectsByModuleName.put(gradleModuleModel.getModuleName(), gradleModuleModel);
-    }
-    return gradleProjectsByModuleName;
   }
 }
--- android/src/com/android/tools/idea/gradle/project/sync/idea/data/service/NdkModuleModelDataService.java
+++ android/src/com/android/tools/idea/gradle/project/sync/idea/data/service/NdkModuleModelDataService.java
@@ -16,20 +16,13 @@
 package com.android.tools.idea.gradle.project.sync.idea.data.service;
 
 import com.android.tools.idea.gradle.project.model.NdkModuleModel;
-import com.android.tools.idea.gradle.project.sync.GradleSyncState;
 import com.android.tools.idea.gradle.project.sync.setup.module.NdkModuleSetupStep;
 import com.android.tools.idea.gradle.project.sync.setup.module.cpp.ContentRootModuleSetupStep;
 import com.android.tools.idea.gradle.project.sync.setup.module.cpp.NdkFacetModuleSetupStep;
 import com.google.common.collect.ImmutableList;
-import com.google.common.collect.Maps;
-import com.intellij.openapi.application.RunResult;
-import com.intellij.openapi.command.WriteCommandAction;
-import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.externalSystem.model.DataNode;
 import com.intellij.openapi.externalSystem.model.Key;
-import com.intellij.openapi.externalSystem.model.project.ProjectData;
 import com.intellij.openapi.externalSystem.service.project.IdeModifiableModelsProvider;
-import com.intellij.openapi.externalSystem.service.project.manage.AbstractProjectDataService;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.project.Project;
 import org.jetbrains.annotations.NotNull;
@@ -40,63 +33,25 @@ import java.util.Map;
 
 import static com.android.tools.idea.gradle.project.sync.idea.data.service.AndroidProjectKeys.NATIVE_ANDROID_MODEL;
 
-public class NdkModuleModelDataService extends AbstractProjectDataService<NdkModuleModel, Void> {
-  private static final Logger LOG = Logger.getInstance(NdkModuleModelDataService.class);
-
+public class NdkModuleModelDataService extends ModuleModelDataService<NdkModuleModel> {
   private final ImmutableList<NdkModuleSetupStep> mySetupSteps =
     ImmutableList.of(new NdkFacetModuleSetupStep(), new ContentRootModuleSetupStep());
 
-  @NotNull
   @Override
+  @NotNull
   public Key<NdkModuleModel> getTargetDataKey() {
     return NATIVE_ANDROID_MODEL;
   }
 
   @Override
-  public void importData(@NotNull Collection<DataNode<NdkModuleModel>> toImport,
-                         @Nullable ProjectData projectData,
-                         @NotNull Project project,
-                         @NotNull IdeModifiableModelsProvider modelsProvider) {
-    try {
-      doImport(toImport, project, modelsProvider);
+  protected void importData(@NotNull Collection<DataNode<NdkModuleModel>> toImport,
+                            @NotNull Project project,
+                            @NotNull IdeModifiableModelsProvider modelsProvider,
+                            @NotNull Map<String, NdkModuleModel> modelsByName) {
+    for (Module module : modelsProvider.getModules()) {
+      NdkModuleModel ndkModuleModel = modelsByName.get(module.getName());
+      customizeModule(module, modelsProvider, ndkModuleModel);
     }
-    catch (Throwable e) {
-      LOG.error(String.format("Failed to set up Native Android Gradle modules in project '%1$s'", project.getName()), e);
-      String msg = e.getMessage();
-      if (msg == null) {
-        msg = e.getClass().getCanonicalName();
-      }
-      GradleSyncState.getInstance(project).syncFailed(msg);
-    }
-  }
-
-  private void doImport(final Collection<DataNode<NdkModuleModel>> toImport,
-                        final Project project,
-                        final IdeModifiableModelsProvider modelsProvider) throws Throwable {
-    RunResult result = new WriteCommandAction.Simple(project) {
-      @Override
-      protected void run() throws Throwable {
-        Map<String, NdkModuleModel> modelsByModuleName = indexByModuleName(toImport);
-        for (Module module : modelsProvider.getModules()) {
-          NdkModuleModel ndkModuleModel = modelsByModuleName.get(module.getName());
-          customizeModule(module, modelsProvider, ndkModuleModel);
-        }
-      }
-    }.execute();
-    Throwable error = result.getThrowable();
-    if (error != null) {
-      throw error;
-    }
-  }
-
-  @NotNull
-  private static Map<String, NdkModuleModel> indexByModuleName(@NotNull Collection<DataNode<NdkModuleModel>> nodes) {
-    Map<String, NdkModuleModel> index = Maps.newHashMap();
-    for (DataNode<NdkModuleModel> dataNode : nodes) {
-      NdkModuleModel ndkModuleModel = dataNode.getData();
-      index.put(ndkModuleModel.getModuleName(), ndkModuleModel);
-    }
-    return index;
   }
 
   private void customizeModule(@NotNull Module module,
