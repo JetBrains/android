86de41c0925f491449e7428d51ebccd5674d6712
Ben Clayton <bclayton@google.com>
2016-11-17 09:51:58 +0000

    Remove GAPID from Android Studio.
    Test: Android Studio runs with no hint of GAPID.
    Change-Id: Iaa618d0b6d65c334450a2da1a478cfc08bd94327

--- android/src/com/android/tools/idea/run/editor/AndroidProfilersPanel.java
+++ android/src/com/android/tools/idea/run/editor/AndroidProfilersPanel.java
@@ -15,8 +15,6 @@
  */
 package com.android.tools.idea.run.editor;
 
-import com.android.tools.idea.editors.gfxtrace.GfxTraceUtil;
-import com.android.tools.idea.editors.gfxtrace.gapi.GapiPaths;
 import com.intellij.openapi.project.Project;
 
 import javax.swing.*;
@@ -32,23 +30,12 @@ public class AndroidProfilersPanel {
   private JPanel myPanel;
   private JCheckBox myAdvancedProfilingCheckBox;
 
-  private JCheckBox myGapidEnabled;
-  private JCheckBox myGapidDisablePCS;
-
   public JComponent getComponent() {
     return myPanel;
   }
 
   AndroidProfilersPanel(Project project, ProfilerState state) {
     myAdvancedProfilingCheckBox.setVisible(EXPERIMENTAL_ENABLED);
-
-    myGapidEnabled.addChangeListener(e -> myGapidDisablePCS.setEnabled(myGapidEnabled.isSelected()));
-
-    myGapidEnabled.addActionListener(e -> {
-      if (!GfxTraceUtil.checkAndTryInstallGapidSdkComponent(project)) {
-        myGapidEnabled.setSelected(false);
-      }
-    });
 
     resetFrom(state);
   }
@@ -58,17 +45,6 @@ public class AndroidProfilersPanel {
    */
   void resetFrom(ProfilerState state) {
     myAdvancedProfilingCheckBox.setSelected(state.ENABLE_ADVANCED_PROFILING);
-    myGapidEnabled.setSelected(state.GAPID_ENABLED);
-    myGapidDisablePCS.setSelected(state.GAPID_DISABLE_PCS);
-    myGapidDisablePCS.setEnabled(state.GAPID_ENABLED);
-
-    if (GapiPaths.isValid()) {
-      myGapidEnabled.setToolTipText(null);
-    }
-    else {
-      myGapidEnabled.setToolTipText("GPU debugger tools not installed or out of date.");
-      myGapidEnabled.setSelected(false);
-    }
   }
 
   /**
@@ -77,8 +53,5 @@ public class AndroidProfilersPanel {
   void applyTo(ProfilerState state) {
     boolean enabled = System.getProperty(ENABLE_EXPERIMENTAL_PROFILING) != null;
     state.ENABLE_ADVANCED_PROFILING = myAdvancedProfilingCheckBox.isSelected() && enabled;
-
-    state.GAPID_ENABLED = myGapidEnabled.isSelected();
-    state.GAPID_DISABLE_PCS = myGapidDisablePCS.isSelected();
   }
 }
