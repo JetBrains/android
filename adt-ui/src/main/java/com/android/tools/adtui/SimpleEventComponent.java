67b705365eeefc526d55ae72bf58c04219c440b1
Joshua Gilpatrick <gijosh@google.com>
2016-11-20 22:59:12 -0800

    Updating the Events UI to match mock.
    This change fixes a minor bug with activities being returned for the
    wrong app id. This change also updates the UI for simple events, and
    activities to better match the mocks.
    
    Test: VisualTest, ImageDiffTest
    
    Change-Id: I15aa6c4e912f4924f870d2b59e06e3f2f9160fbf

--- adt-ui/src/main/java/com/android/tools/adtui/SimpleEventComponent.java
+++ adt-ui/src/main/java/com/android/tools/adtui/SimpleEventComponent.java
@@ -22,76 +22,49 @@ import com.android.tools.adtui.model.SeriesData;
 import com.intellij.util.containers.ImmutableList;
 import org.jetbrains.annotations.NotNull;
 
-import javax.swing.*;
 import java.awt.*;
 import java.awt.geom.AffineTransform;
-import java.awt.geom.Path2D;
 import java.util.ArrayList;
-import java.util.concurrent.TimeUnit;
+import java.util.Map;
 
 /**
  * A chart component that renders a series of events as icons.
  */
 public class SimpleEventComponent<E extends Enum<E>> extends AnimatedComponent {
 
-  public static final Color LINE_COLOR = new Color(214, 196, 228);
-  private static final int LINE_OFFSET = 3;
-  private static final int NORMALIZED_END = 1;
-
-  private static final long HOLD_DELAY_US = TimeUnit.MILLISECONDS.toMicros(150);
-
   @NotNull
   private final RangedSeries<EventAction<EventAction.Action, E>> mData;
 
   @NotNull
-  private final Icon[] mIcons;
-
-  @NotNull
-  private final ArrayList<Shape> mPaths;
+  private final Map<E, SimpleEventRenderer> mRenderers;
 
   @NotNull
   private final ArrayList<EventRenderData> mIconsToDraw;
-
-  @NotNull
-  private final int mImageWidth;
 
   /**
    * Component that renders EventActions as a series of icons.
    */
   public SimpleEventComponent(
     @NotNull RangedSeries<EventAction<EventAction.Action, E>> data,
-    @NotNull Icon[] icons) {
+    @NotNull Map<E, SimpleEventRenderer> renderers) {
     mData = data;
-    mIcons = icons;
-    mPaths = new ArrayList<Shape>();
-    mIconsToDraw = new ArrayList<EventRenderData>();
-    mImageWidth = mIcons[0].getIconWidth();
+    mRenderers = renderers;
+    mIconsToDraw = new ArrayList<>();
   }
 
   @Override
   protected void updateData() {
     //TODO Pull logic of combining events out of component and into EventHandler
-    double min = mData.getXRange().getMin();
     double max = mData.getXRange().getMax();
     mIconsToDraw.clear();
-    mPaths.clear();
     ImmutableList<SeriesData<EventAction<EventAction.Action, E>>> series = mData.getSeries();
     int size = series.size();
 
     for (int i = 0; i < size; i++) {
       SeriesData<EventAction<EventAction.Action, E>> seriesData = series.get(i);
       EventAction<EventAction.Action, ? extends Enum> data = seriesData.value;
-      Integer toDraw = data.getValueData().ordinal();
-      mIconsToDraw.add(new EventRenderData(toDraw, data.getStartUs()));
-      double endTimeUs = data.getEndUs() == 0L ? max : data.getEndUs();
-      if (endTimeUs - data.getStartUs() >= HOLD_DELAY_US) {
-        Path2D.Float path = new Path2D.Float();
-        double start = (data.getStartUs() - min) / (max - min);
-        double end = (endTimeUs - min) / (max - min);
-        path.moveTo(start, LINE_OFFSET);
-        path.lineTo(end, LINE_OFFSET);
-        mPaths.add(path);
-      }
+      long endTimeUs = data.getEndUs() == 0L ? (long)max : data.getEndUs();
+      mIconsToDraw.add(new EventRenderData(data.getValueData(), data.getStartUs(), endTimeUs));
     }
   }
 
@@ -101,27 +74,14 @@ public class SimpleEventComponent<E extends Enum<E>> extends AnimatedComponent {
     double min = mData.getXRange().getMin();
     double max = mData.getXRange().getMax();
     double scaleFactor = dim.getWidth();
-    AffineTransform scale = AffineTransform.getScaleInstance(scaleFactor, 1);
     for (int i = 0; i < mIconsToDraw.size(); i++) {
       EventRenderData data = mIconsToDraw.get(i);
-      double normalizedPosition = ((data.getTimestamp() - min) / (max - min));
-      if (data.getTimestamp() == 0) {
-        normalizedPosition = NORMALIZED_END;
-      }
-      //TODO account for image width in positioning.
+      double normalizedPositionStart = ((data.getStartTimestamp() - min) / (max - min));
+      double normalizedPositionEnd = ((data.getEndTimestamp() - min) / (max - min));
       AffineTransform translate = AffineTransform
-        .getTranslateInstance(normalizedPosition * scaleFactor - mImageWidth / 2.0, 0);
-      mIcons[data.getIndex() % mIcons.length].paintIcon(this, g2d, (int)translate.getTranslateX(), (int)translate.getTranslateY());
+        .getTranslateInstance(normalizedPositionStart * scaleFactor, 0);
+      mRenderers.get(data.getKey()).draw(this, g2d, translate, (normalizedPositionEnd - normalizedPositionStart)*scaleFactor);
     }
-    Stroke current = g2d.getStroke();
-    BasicStroke str = new BasicStroke(2.0f);
-    g2d.setStroke(str);
-    g2d.setColor(LINE_COLOR);
-    for (int i = 0; i < mPaths.size(); i++) {
-      Shape shape = scale.createTransformedShape(mPaths.get(i));
-      g2d.draw(shape);
-    }
-    g2d.setStroke(current);
   }
 
   @Override
@@ -129,22 +89,27 @@ public class SimpleEventComponent<E extends Enum<E>> extends AnimatedComponent {
     super.debugDraw(g);
   }
 
-  private static class EventRenderData {
+  private static class EventRenderData<E> {
 
-    private final int mIndex;
-    private final long mTimestamp;
+    private final E mKey;
+    private final long mStartTimestamp;
+    private final long mEndTimestamp;
 
-    public int getIndex() {
-      return mIndex;
+    public E getKey() {
+      return mKey;
     }
 
-    public long getTimestamp() {
-      return mTimestamp;
+    public long getStartTimestamp() {
+      return mStartTimestamp;
+    }
+    public long getEndTimestamp() {
+      return mEndTimestamp;
     }
 
-    public EventRenderData(int index, long timestamp) {
-      mIndex = index;
-      mTimestamp = timestamp;
+    public EventRenderData(E key, long startTimestamp, long endTimestamp) {
+      mKey = key;
+      mStartTimestamp = startTimestamp;
+      mEndTimestamp = endTimestamp;
     }
   }
 }
