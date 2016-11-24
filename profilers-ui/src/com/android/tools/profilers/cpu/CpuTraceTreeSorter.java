38806f96bccdf9cdbfab8ca14a4356c0144080c1
Joshua Gilpatrick <gijosh@google.com>
2016-11-23 16:35:55 -0800

    Adding sorting to the CpuCaptureTree
    Fixing a bug in ColumnTreeBuilder when 2 columns have the same name,
    the column look up by name can return the wrong column.
    Adding a sorting class to enable column sorting on the CpuCaptureTree.
    
    Test: None
    Change-Id: I9a80379a0da05c5da09aded5bb91bdfa05819c10

---
+++ profilers-ui/src/com/android/tools/profilers/cpu/CpuTraceTreeSorter.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.profilers.cpu;
+
+import com.android.tools.adtui.common.ColumnTreeBuilder;
+import com.android.tools.adtui.model.RangedTreeModel;
+import com.intellij.codeInspection.ui.OptionAccessor;
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.TreePath;
+import java.util.*;
+
+public class CpuTraceTreeSorter implements ColumnTreeBuilder.TreeSorter<DefaultMutableTreeNode> {
+
+  @NotNull private JTree myTree;
+  private DefaultMutableTreeNode myRoot;
+  private TopDownTreeModel myModel;
+  private Comparator<DefaultMutableTreeNode> myComparator;
+
+  public CpuTraceTreeSorter(@NotNull JTree tree) {
+    myTree = tree;
+  }
+
+  public void setModel(TopDownTreeModel model) {
+    myModel = model;
+    if (myModel != null) {
+      myRoot = (DefaultMutableTreeNode)model.getRoot();
+    }
+  }
+
+  @Override
+  public void sort(Comparator<DefaultMutableTreeNode> comparator, SortOrder order) {
+    if (myComparator != comparator && myModel != null && myRoot != null) {
+      myComparator = comparator;
+      TreePath selectionPath = myTree.getSelectionPath();
+      sortTree(myRoot);
+      myTree.collapseRow(0);
+      myTree.setSelectionPath(selectionPath);
+      myTree.scrollPathToVisible(selectionPath);
+      myModel.reload();
+    }
+  }
+  private void sortTree(@NotNull DefaultMutableTreeNode parent) {
+    if (parent.isLeaf()) {
+      return;
+    }
+    int childCount = parent.getChildCount();
+    List< DefaultMutableTreeNode> children = new ArrayList<>(childCount);
+    for (int i = 0; i < childCount; i++) {
+      children.add((DefaultMutableTreeNode) parent.getChildAt(i));
+    }
+
+    Collections.sort(children, myComparator);
+    parent.removeAllChildren();
+    for (DefaultMutableTreeNode node: children) {
+      sortTree(node);
+      parent.add(node);
+    }
+  }
+}
