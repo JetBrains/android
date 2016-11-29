346bc34eb2e6c63d70b81d93088a717cd9c5a0c8
Lucas Smaira <lsmaira@google.com>
2016-11-29 18:45:05 +0000

    Revert "Revert "Fix and reactivate test artifacts and sync tests""
    This reverts commit dccda18e27356f7817d6b9fb472f715839b226f0.
    
    Removing memory related chenges (in AndroidRunConfigurationEditor) from
    the original CL, since the build was broken because of memory problems.
    The memory leak will be treated in a separate CL.
    
    Test: look original CL message
    
    Change-Id: I9bcf5e498dad081513484a122ba539c5688a0915

--- android-tests/testSrc/com/android/tools/idea/testartifacts/TestConfigurationTesting.java
+++ android-tests/testSrc/com/android/tools/idea/testartifacts/TestConfigurationTesting.java
@@ -13,8 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.android.tools.idea.testartifacts.instrumented;
+package com.android.tools.idea.testartifacts;
 
+import com.android.tools.idea.testartifacts.instrumented.AndroidTestRunConfiguration;
 import com.android.tools.idea.testartifacts.junit.AndroidJUnitConfiguration;
 import com.intellij.execution.Location;
 import com.intellij.execution.PsiLocation;
@@ -26,8 +27,10 @@ import com.intellij.openapi.actionSystem.LangDataKeys;
 import com.intellij.openapi.module.ModuleUtilCore;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.JavaPsiFacade;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiManager;
+import com.intellij.psi.search.GlobalSearchScope;
 import com.intellij.testFramework.MapDataContext;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -40,13 +43,33 @@ import static org.junit.Assert.assertNotNull;
  */
 public class TestConfigurationTesting {
   @Nullable
-  public static AndroidJUnitConfiguration createJUnitConfiguration(@NotNull Project project, @NotNull String directory) {
+  public static AndroidTestRunConfiguration createAndroidTestConfigurationFromClass(@NotNull Project project, @NotNull String qualifiedName) {
+    return createConfigurationFromClass(project, qualifiedName, AndroidTestRunConfiguration.class);
+  }
+
+  @Nullable
+  public static AndroidJUnitConfiguration createJUnitConfigurationFromClass(@NotNull Project project, @NotNull String qualifiedName) {
+    return createConfigurationFromClass(project, qualifiedName, AndroidJUnitConfiguration.class);
+  }
+
+  @Nullable
+  public static AndroidJUnitConfiguration createJUnitConfigurationFromDirectory(@NotNull Project project, @NotNull String directory) {
     return createConfigurationFromDirectory(project, directory, AndroidJUnitConfiguration.class);
   }
 
   @Nullable
-  public static AndroidTestRunConfiguration createAndroidTestConfiguration(@NotNull Project project, @NotNull String directory) {
+  public static AndroidTestRunConfiguration createAndroidTestConfigurationFromDirectory(@NotNull Project project, @NotNull String directory) {
     return createConfigurationFromDirectory(project, directory, AndroidTestRunConfiguration.class);
+  }
+
+  @Nullable
+  private static <T extends RunConfiguration> T createConfigurationFromClass(@NotNull Project project,
+                                                                             @NotNull String qualifiedName,
+                                                                             @NotNull Class<T> expectedType) {
+    PsiElement element = JavaPsiFacade.getInstance(project).findClass(qualifiedName, GlobalSearchScope.projectScope(project));
+    assertNotNull(element);
+    RunConfiguration runConfiguration = createConfigurationFromPsiElement(project, element);
+    return expectedType.isInstance(runConfiguration) ? expectedType.cast(runConfiguration) : null;
   }
 
   @Nullable
