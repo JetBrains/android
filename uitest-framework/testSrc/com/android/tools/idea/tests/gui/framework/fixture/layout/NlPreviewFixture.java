JetBrains/android

commit 0adbc4586ae3923ec94a7ba82ae77859931b70ef

Enable the new Palette.
Also:
- rename the layout component categories to new design
- enable the 24x24 images for the palette.

Test: Not Aplicable. Pure UI changes.

Change-Id: I703a6c605c497223b5c8f05f9a3afc9c7d41af70

android/gen/icons/AndroidIcons.java
@@ -426,6 +426,8 @@ public class AndroidIcons {
     public static final Icon EditTextPhoneLarge = load("/icons/views/EditTextPhone_large.png"); // 24x24
     public static final Icon EditTextPostalAddress = load("/icons/views/EditTextPostalAddress.png"); // 16x16
     public static final Icon EditTextPostalAddressLarge = load("/icons/views/EditTextPostalAddress_large.png"); // 24x24
+    public static final Icon EditTextTime = load("/icons/views/EditTextTime.png"); // 16x16
+    public static final Icon EditTextTimeLarge = load("/icons/views/EditTextTime_large.png"); // 24x24
     public static final Icon ExpandableListView = load("/icons/views/ExpandableListView.png"); // 16x16
     public static final Icon ExpandableListViewLarge = load("/icons/views/ExpandableListView_large.png"); // 24x24
     public static final Icon FlexBoxLayout = load("/icons/views/FlexBoxLayout.png"); // 16x16

designer-tests/testSrc/com/android/tools/idea/uibuilder/palette/NlPaletteModelTest.java
@@ -38,16 +38,16 @@ public class NlPaletteModelTest extends PaletteTestCase {
     Palette palette = model.getPalette(NlLayoutType.LAYOUT);
     Iterator<Palette.BaseItem> iterator = palette.getItems().iterator();
     Palette.Group widgets = assertIsGroup(iterator.next(), "Widgets");
-    Palette.Group textFields = assertIsGroup(iterator.next(), "Text Fields (EditText)");
+    Palette.Group textFields = assertIsGroup(iterator.next(), "Text");
     Palette.Group layouts = assertIsGroup(iterator.next(), "Layouts");
     Palette.Group containers = assertIsGroup(iterator.next(), "Containers");
-    Palette.Group images = assertIsGroup(iterator.next(), "Images & Media");
-    Palette.Group times = assertIsGroup(iterator.next(), "Date & Time");
+    Palette.Group images = assertIsGroup(iterator.next(), "Images");
+    Palette.Group times = assertIsGroup(iterator.next(), "Date");
     Palette.Group transitions = assertIsGroup(iterator.next(), "Transitions");
     Palette.Group advanced = assertIsGroup(iterator.next(), "Advanced");
-    Palette.Group google = assertIsGroup(iterator.next(), "Custom - Google");
-    Palette.Group design = assertIsGroup(iterator.next(), "Custom - Design");
-    Palette.Group appcompat = assertIsGroup(iterator.next(), "Custom - AppCompat");
+    Palette.Group google = assertIsGroup(iterator.next(), "Google");
+    Palette.Group design = assertIsGroup(iterator.next(), "Design");
+    Palette.Group appcompat = assertIsGroup(iterator.next(), "AppCompat");
     assertFalse(iterator.hasNext());
 
     iterator = widgets.getItems().iterator();
@@ -58,9 +58,7 @@ public class NlPaletteModelTest extends PaletteTestCase {
     assertRadioButton(iterator.next());
     assertCheckedTextView(iterator.next());
     assertSpinner(iterator.next());
-    assertLargeProgressBarItem(iterator.next());
     assertNormalProgressBarItem(iterator.next());
-    assertSmallProgressBarItem(iterator.next());
     assertHorizontalProgressBarItem(iterator.next());
     assertSeekBar(iterator.next());
     assertDiscreteSeekBar(iterator.next());

designer-tests/testSrc/com/android/tools/idea/uibuilder/palette/PaletteTest.java
@@ -39,7 +39,6 @@ public class PaletteTest extends PaletteTestCase {
     iterator = group1.getItems().iterator();
     assertTextViewItem(iterator.next());
     assertLinearLayoutItem(iterator.next());
-    assertLargeProgressBarItem(iterator.next());
     assertNormalProgressBarItem(iterator.next());
     assertFalse(iterator.hasNext());
 
@@ -82,19 +81,6 @@ public class PaletteTest extends PaletteTestCase {
     "              android:layout_height=\"match_parent\">\n" +
     "            </LinearLayout>\n" +
     "          ]]>\n" +
-    "      </xml>\n" +
-    "    </item>\n" +
-    "    <item tag=\"ProgressBar\"\n" +
-    "          id=\"LargeProgressBar\"\n" +
-    "          title=\"ProgressBar (Large)\">\n" +
-    "      <xml reuse=\"preview,drag-preview\">\n" +
-    "        <![CDATA[\n" +
-    "              <ProgressBar\n" +
-    "                style=\"?android:attr/progressBarStyleLarge\"\n" +
-    "                android:layout_width=\"wrap_content\"\n" +
-    "                android:layout_height=\"wrap_content\"\n" +
-    "              />\n" +
-    "            ]]>\n" +
     "      </xml>\n" +
     "    </item>\n" +
     "    <item tag=\"ProgressBar\"\n" +

designer-tests/testSrc/com/android/tools/idea/uibuilder/palette/PaletteTestCase.java
@@ -113,31 +113,6 @@ public abstract class PaletteTestCase extends AndroidTestCase {
   }
 
   @Language("XML")
-  private static final String LARGE_PROGRESS_XML =
-    "<ProgressBar\n" +
-    "  style=\"?android:attr/progressBarStyleLarge\"\n" +
-    "  android:layout_width=\"wrap_content\"\n" +
-    "  android:layout_height=\"wrap_content\"\n" +
-    "/>\n";
-
-  @Language("XML")
-  private static final String LARGE_PROGRESS_PREVIEW_XML =
-    "<ProgressBar\n" +
-    "  android:id=\"@+id/LargeProgressBar\"\n" +
-    "  style=\"?android:attr/progressBarStyleLarge\"\n" +
-    "  android:layout_width=\"wrap_content\"\n" +
-    "  android:layout_height=\"wrap_content\"\n" +
-    "/>\n";
-
-  public void assertLargeProgressBarItem(@NotNull Palette.BaseItem item) {
-    checkItem(item, "ProgressBar", "ProgressBar (Large)", AndroidIcons.Views.ProgressBar, LARGE_PROGRESS_XML, LARGE_PROGRESS_PREVIEW_XML,
-              LARGE_PROGRESS_XML, IN_PLATFORM, NO_SCALE);
-    NlComponent component = createMockComponent("ProgressBar");
-    when(component.getAttribute(null, TAG_STYLE)).thenReturn(ANDROID_STYLE_RESOURCE_PREFIX + "Widget.ProgressBar.Large");
-    checkComponent(component, "ProgressBar (Large)", AndroidIcons.Views.ProgressBar);
-  }
-
-  @Language("XML")
   private static final String NORMAL_PROGRESS_XML =
     "<ProgressBar\n" +
     "  style=\"?android:attr/progressBarStyle\"\n" +
@@ -149,31 +124,6 @@ public abstract class PaletteTestCase extends AndroidTestCase {
     checkItem(item, "ProgressBar", "ProgressBar", AndroidIcons.Views.ProgressBar, NORMAL_PROGRESS_XML, NORMAL_PROGRESS_XML,
               NORMAL_PROGRESS_XML, IN_PLATFORM, NO_SCALE);
     checkComponent(createMockComponent("ProgressBar"), "ProgressBar", AndroidIcons.Views.ProgressBar);
-  }
-
-  @Language("XML")
-  private static final String SMALL_PROGRESS_XML =
-    "<ProgressBar\n" +
-    "  style=\"?android:attr/progressBarStyleSmall\"\n" +
-    "  android:layout_width=\"wrap_content\"\n" +
-    "  android:layout_height=\"wrap_content\"\n" +
-    "/>\n";
-
-  @Language("XML")
-  private static final String SMALL_PROGRESS_PREVIEW_XML =
-    "<ProgressBar\n" +
-    "  android:id=\"@+id/SmallProgressBar\"\n" +
-    "  style=\"?android:attr/progressBarStyleSmall\"\n" +
-    "  android:layout_width=\"wrap_content\"\n" +
-    "  android:layout_height=\"wrap_content\"\n" +
-    "/>\n";
-
-  public void assertSmallProgressBarItem(@NotNull Palette.BaseItem item) {
-    checkItem(item, "ProgressBar", "ProgressBar (Small)", AndroidIcons.Views.ProgressBar, SMALL_PROGRESS_XML, SMALL_PROGRESS_PREVIEW_XML,
-              SMALL_PROGRESS_XML, IN_PLATFORM, NO_SCALE);
-    NlComponent component = createMockComponent("ProgressBar");
-    when(component.getAttribute(null, TAG_STYLE)).thenReturn(ANDROID_STYLE_RESOURCE_PREFIX + "Widget.ProgressBar.Small");
-    checkComponent(component, "ProgressBar (Small)", AndroidIcons.Views.ProgressBar);
   }
 
   @Language("XML")
@@ -194,7 +144,7 @@ public abstract class PaletteTestCase extends AndroidTestCase {
     "/>\n";
 
   public void assertHorizontalProgressBarItem(@NotNull Palette.BaseItem item) {
-    checkItem(item, "ProgressBar", "ProgressBar (Horizontal)", AndroidIcons.Views.ProgressBar, HORIZONTAL_PROGRESS_XML,
+    checkItem(item, "ProgressBar", "ProgressBar (Horizontal)", AndroidIcons.Views.ProgressBarHorizontal, HORIZONTAL_PROGRESS_XML,
               HORIZONTAL_PROGRESS_PREVIEW_XML, HORIZONTAL_PROGRESS_XML, IN_PLATFORM, 2.0);
     NlComponent component = createMockComponent("ProgressBar");
     when(component.getAttribute(null, TAG_STYLE)).thenReturn(ANDROID_STYLE_RESOURCE_PREFIX + "Widget.ProgressBar.Horizontal");

designer/src/com/android/tools/idea/uibuilder/api/PaletteComponentHandler.java
@@ -16,19 +16,12 @@
 package com.android.tools.idea.uibuilder.api;
 
 import com.android.tools.idea.XmlBuilder;
-import com.android.tools.idea.rendering.ImageUtils;
 import com.intellij.openapi.util.IconLoader;
-import com.intellij.openapi.util.text.StringUtil;
-import com.intellij.util.ui.JBImageIcon;
-import com.intellij.util.ui.UIUtil;
 import icons.AndroidIcons;
 import org.intellij.lang.annotations.Language;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
-
-import java.awt.*;
-import java.awt.image.BufferedImage;
 
 import static com.android.SdkConstants.*;
 
@@ -150,20 +143,12 @@ public abstract class PaletteComponentHandler {
 
   @NotNull
   protected Icon loadBuiltinLargeIcon(@NotNull String tagName) {
-    // Temporary: Scale the 2x image to 24x24
-    // TODO: Remove this code when we have 24x24 images.
-    String path = "/icons/views/" + StringUtil.capitalize(getSimpleTagName(tagName)) + "@2x.png";
-    Icon icon = IconLoader.findIcon(path, AndroidIcons.class, false, false);
-    if (icon == null) {
-      icon = AndroidIcons.Views.Unknown;
+    String path = "AndroidIcons.Views." + getSimpleTagName(tagName) + "Large";
+    Icon icon = IconLoader.findIcon(path, getClass());
+    if (icon != null) {
+      return icon;
     }
-    int size = 24;
-    BufferedImage image = UIUtil.createImage(size, size, BufferedImage.TYPE_INT_ARGB);
-    Graphics2D g2 = (Graphics2D)image.getGraphics();
-    g2.scale(24.0 / icon.getIconWidth(), 24.0 / icon.getIconHeight());
-    icon.paintIcon(null, g2, 0, 0);
-    g2.dispose();
-    return new JBImageIcon(ImageUtils.convertToRetinaIgnoringFailures(image));
+    return AndroidIcons.Views.UnknownLarge;
   }
 
   @NotNull

designer/src/com/android/tools/idea/uibuilder/palette/DependencyManager.java
@@ -20,12 +20,12 @@ import com.android.tools.idea.gradle.dependencies.GradleDependencyManager;
 import com.android.tools.idea.gradle.project.sync.GradleSyncListener;
 import com.android.tools.idea.gradle.project.sync.GradleSyncState;
 import com.android.tools.idea.rendering.ImageUtils;
-import com.intellij.icons.AllIcons;
 import com.intellij.openapi.Disposable;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.project.Project;
 import com.intellij.util.ui.JBImageIcon;
 import com.intellij.util.ui.UIUtil;
+import icons.AndroidIcons;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
@@ -36,9 +36,6 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class DependencyManager {
-  private static final double DOWNLOAD_SCALE = 0.7;
-  private static final double DOWNLOAD_RELATIVE_OFFSET = (1.0 - DOWNLOAD_SCALE) / DOWNLOAD_SCALE;
-
   private final Project myProject;
   private final Set<String> myMissingLibraries;
   private Module myModule;
@@ -76,20 +73,19 @@ public class DependencyManager {
 
   @NotNull
   public Icon createItemIcon(@NotNull Palette.Item item, @NotNull Component componentContext) {
-    return createItemIcon(item, item.getIcon(), componentContext);
+    return createItemIcon(item, item.getIcon(), AndroidIcons.NeleIcons.Download, componentContext);
   }
 
   @NotNull
   public Icon createLargeItemIcon(@NotNull Palette.Item item, @NotNull Component componentContext) {
-    return createItemIcon(item, item.getLargeIcon(), componentContext);
+    return createItemIcon(item, item.getLargeIcon(), AndroidIcons.NeleIcons.DownloadLarge, componentContext);
   }
 
   @NotNull
-  private Icon createItemIcon(@NotNull Palette.Item item, @NotNull Icon icon, @NotNull Component componentContext) {
+  private Icon createItemIcon(@NotNull Palette.Item item, @NotNull Icon icon, @NotNull Icon download, @NotNull Component componentContext) {
     if (!needsLibraryLoad(item)) {
       return icon;
     }
-    Icon download = AllIcons.Actions.Download;
     BufferedImage image = UIUtil.createImage(icon.getIconWidth(),
                                              icon.getIconHeight(),
                                              BufferedImage.TYPE_INT_ARGB);
@@ -97,14 +93,9 @@ public class DependencyManager {
     icon.paintIcon(componentContext, g2, 0, 0);
     int x = icon.getIconWidth() - download.getIconWidth();
     int y = icon.getIconHeight() - download.getIconHeight();
-    if (x == 0 && y == 0) {
-      g2.scale(DOWNLOAD_SCALE, DOWNLOAD_SCALE);
-      x = (int)(icon.getIconWidth() * DOWNLOAD_RELATIVE_OFFSET);
-      y = (int)(icon.getIconHeight() * DOWNLOAD_RELATIVE_OFFSET);
-    }
     download.paintIcon(componentContext, g2, x, y);
     g2.dispose();
-    return new JBImageIcon(ImageUtils.convertToRetinaIgnoringFailures(image));
+    return new JBImageIcon(UIUtil.isRetina() ? ImageUtils.convertToRetinaIgnoringFailures(image) : image);
   }
 
   private boolean checkForNewMissingDependencies() {

designer/src/com/android/tools/idea/uibuilder/palette/NlPaletteDefinition.java
@@ -31,7 +31,8 @@ public class NlPaletteDefinition extends ToolWindowDefinition<DesignSurface> {
     if (project.isDisposed()) {
       return null;
     }
-    if (Boolean.getBoolean("use.new.palette")) {
+    if (!Boolean.getBoolean("use.old.palette")) {
+      // TODO: Remove the code for the old palette
       return new NlPalettePanel(project, null);
     }
     else {

designer/src/com/android/tools/idea/uibuilder/palette/Palette.java
@@ -295,6 +295,12 @@ public class Palette {
           return icon;
         }
       }
+      if (myIconName != null) {
+        Icon icon = IconLoader.findIcon(myIconName + "Large", getClass());
+        if (icon != null) {
+          return icon;
+        }
+      }
       return myHandler.getLargeIcon(myTagName);
     }
 

commit 77e8e3df3ac70c410bae7b1609402e40400aba9c

Fix NlPreviewTest.testCopyAndPaste
The test was broken by commit 0adbc4586a. This fix follows what was
done in commit 721055ec1b

Test: Run NlPreviewTest
Change-Id: I6fbd2f2de59eb8bad69238dea13bf34198b64c9b

android-uitests/testSrc/com/android/tools/idea/tests/gui/layout/NlPreviewTest.java
@@ -229,8 +229,8 @@ public class NlPreviewTest {
 
     NlPreviewFixture layout = editor.getLayoutPreview(true);
     layout
-      .dragComponentToSurface("Widgets/Button")
-      .dragComponentToSurface("Widgets/CheckBox")
+      .dragComponentToSurface("Widgets", "Button")
+      .dragComponentToSurface("Widgets", "CheckBox")
       .waitForRenderToFinish();
 
     // Find and click the first text view

uitest-framework/testSrc/com/android/tools/idea/tests/gui/framework/fixture/layout/NlPreviewFixture.java
@@ -19,6 +19,7 @@ import com.android.tools.idea.tests.gui.framework.GuiTests;
 import com.android.tools.idea.tests.gui.framework.fixture.IdeFrameFixture;
 import com.android.tools.idea.tests.gui.framework.fixture.ToolWindowFixture;
 import com.android.tools.idea.tests.gui.framework.matcher.Matchers;
+import com.android.tools.idea.uibuilder.palette.NlPaletteTreeGrid;
 import com.android.tools.idea.uibuilder.surface.DesignSurface;
 import com.intellij.openapi.actionSystem.ActionToolbar;
 import com.intellij.openapi.actionSystem.impl.ActionToolbarImpl;
@@ -27,8 +28,8 @@ import com.intellij.openapi.wm.impl.AnchoredButton;
 import org.fest.swing.core.ComponentDragAndDrop;
 import org.fest.swing.core.Robot;
 import org.fest.swing.exception.ComponentLookupException;
+import org.fest.swing.fixture.JListFixture;
 import org.fest.swing.fixture.JToggleButtonFixture;
-import org.fest.swing.fixture.JTreeFixture;
 import org.fest.swing.timing.Wait;
 import org.jetbrains.annotations.NotNull;
 
@@ -60,7 +61,7 @@ public class NlPreviewFixture extends ToolWindowFixture {
   public NlPreviewFixture openPalette() {
     // Check if the palette is already open
     try {
-      myRobot.finder().findByName("Palette Tree", JTree.class, true);
+      myRobot.finder().findByType(NlPaletteTreeGrid.class, true);
     } catch (ComponentLookupException e) {
       new JToggleButtonFixture(myRobot, GuiTests.waitUntilShowing(myRobot, Matchers.byText(AnchoredButton.class, "Palette "))).click();
     }
@@ -69,10 +70,15 @@ public class NlPreviewFixture extends ToolWindowFixture {
   }
 
   @NotNull
-  public NlPreviewFixture dragComponentToSurface(@NotNull String path) {
+  public NlPreviewFixture dragComponentToSurface(@NotNull String group, @NotNull String item) {
     openPalette();
-    JTree tree = myRobot.finder().findByName("Palette Tree", JTree.class, true);
-    new JTreeFixture(myRobot, tree).drag(path);
+    NlPaletteTreeGrid treeGrid = myRobot.finder().findByType(NlPaletteTreeGrid.class, true);
+    Wait.seconds(5).expecting("the UI to be populated").until(() -> treeGrid.getCategoryList().getModel().getSize() > 0);
+    new JListFixture(myRobot, treeGrid.getCategoryList()).selectItem(group);
+
+    // Wait until the list has been expanded in UI (eliminating flakiness).
+    JList list = GuiTests.waitUntilShowing(myRobot, treeGrid, Matchers.byName(JList.class, group));
+    new JListFixture(myRobot, list).drag(item);
     myDragAndDrop.drop(myDesignSurfaceFixture.target(), new Point(0, 0));
     return this;
   }