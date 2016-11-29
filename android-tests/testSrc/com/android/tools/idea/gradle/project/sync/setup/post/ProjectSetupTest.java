6ea8a4a6d45ff11681dc3033284b7cfcd0f327d8
Alex Ruiz <alruiz@google.com>
2016-11-29 04:08:00 -0800

    Minor code cleanup.
    Moved base classes for extension points to the parent package,
    following the same organization pattern in package
    'com.android.tools.idea.gradle.project.sync.setup.module'.
    
    Test: This CL only moves classes to a different package. All
    existing tests should pass.
    
    Change-Id: I6f77d85a9b0c8bc13c36d51053580bc658650846

--- android-tests/testSrc/com/android/tools/idea/gradle/project/sync/setup/post/ProjectSetupTest.java
+++ android-tests/testSrc/com/android/tools/idea/gradle/project/sync/setup/post/ProjectSetupTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.android.tools.idea.gradle.project.sync.setup.post.project;
+package com.android.tools.idea.gradle.project.sync.setup.post;
 
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.progress.ProgressIndicator;
