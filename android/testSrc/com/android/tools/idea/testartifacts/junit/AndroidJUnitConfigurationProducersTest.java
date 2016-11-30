d5c380a2c5ba4f19eccda93d4442283517dd85cc
Lucas Smaira <lsmaira@google.com>
2016-11-30 20:32:24 +0000

    Spliting CL ag/1657394 (CL1)
    In order to detect memory problems in gradle deamon, the original CL
    (that exposed these problems) is being split.
    
    CL1: general refactoring but keeping tests deactivated
    
    Test: included
    Change-Id: I33c8181c9f2a82c3eedfd71ab8b5806176c12319

---
+++ android/testSrc/com/android/tools/idea/testartifacts/junit/AndroidJUnitConfigurationProducersTest.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.testartifacts.junit;
+
+import com.android.tools.idea.testing.AndroidGradleTestCase;
+import com.intellij.openapi.util.SystemInfo;
+
+import static com.android.tools.idea.testartifacts.TestConfigurationTesting.createJUnitConfigurationFromClass;
+import static com.android.tools.idea.testartifacts.TestConfigurationTesting.createJUnitConfigurationFromDirectory;
+
+
+/**
+ * Tests for all the {@link AndroidJUnitConfigurationProducer}s
+ */
+public class AndroidJUnitConfigurationProducersTest extends AndroidGradleTestCase {
+
+  @Override
+  protected boolean shouldRunTest() {
+    // Do not run tests on Windows (see http://b.android.com/222904)
+    return !SystemInfo.isWindows && super.shouldRunTest();
+  }
+
+  @Override
+  public void setUp() throws Exception {
+    super.setUp();
+    loadSimpleApplication();
+  }
+
+  public void ignore_testCanCreateJUnitConfigurationFromJUnitTestClass() throws Exception {
+    assertNotNull(createJUnitConfigurationFromClass(getProject(), "google.simpleapplication.UnitTest"));
+  }
+
+  public void ignore_testCannotCreateJUnitConfigurationFromAndroidTestClass() throws Exception {
+    assertNull(createJUnitConfigurationFromClass(getProject(), "google.simpleapplication.ApplicationTest"));
+  }
+
+  public void ignore_testCanCreateJUnitConfigurationFromJUnitTestDirectory() throws Exception {
+    assertNotNull(createJUnitConfigurationFromDirectory(getProject(), "app/src/test/java"));
+  }
+
+  public void ignore_testCannotCreateJUnitConfigurationFromAndroidTestDirectory() throws Exception {
+    assertNull(createJUnitConfigurationFromDirectory(getProject(), "app/src/androidTest/java"));
+  }
+}
