42fdfd9551e642b4daff5f68c0c3e274b51eb131
Jens Ole Lauridsen <jlauridsen@google.com>
2016-11-30 09:21:29 -0800

    Only scroll property panel into view when navigating by keyboard.
    Test: Added gui test.
    
    Change-Id: If757b9437beffe6672fd4a8dfb78f109b43e4f2a

--- uitest-framework/testSrc/com/android/tools/idea/tests/gui/framework/fixture/layout/NlPropertyInspectorFixture.java
+++ uitest-framework/testSrc/com/android/tools/idea/tests/gui/framework/fixture/layout/NlPropertyInspectorFixture.java
@@ -16,20 +16,23 @@
 package com.android.tools.idea.tests.gui.framework.fixture.layout;
 
 import com.android.annotations.Nullable;
-import com.android.tools.idea.tests.gui.framework.GuiTests;
 import com.android.tools.idea.tests.gui.framework.fixture.ComponentFixture;
-import com.android.tools.idea.tests.gui.framework.fixture.IdeFrameFixture;
 import com.android.tools.idea.tests.gui.framework.matcher.Matchers;
 import com.android.tools.idea.uibuilder.property.NlPropertiesPanel;
+import com.intellij.openapi.wm.IdeFocusManager;
 import com.intellij.ui.components.JBLabel;
-import org.fest.swing.core.*;
+import org.fest.swing.core.GenericTypeMatcher;
 import org.fest.swing.core.Robot;
+import org.fest.swing.exception.WaitTimedOutError;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
 
 import static com.android.tools.idea.tests.gui.framework.GuiTests.waitUntilFound;
+import static com.google.common.truth.Truth.assertThat;
 
 /**
  * Fixture wrapping the component inspector
@@ -43,25 +46,127 @@ public class NlPropertyInspectorFixture extends ComponentFixture<NlPropertyInspe
   }
 
   public static NlPropertiesPanel create(@NotNull Robot robot) {
-    return GuiTests.waitUntilFound(robot, null, Matchers.byType(NlPropertiesPanel.class));
+    return waitUntilFound(robot, null, Matchers.byType(NlPropertiesPanel.class));
   }
 
   @Nullable
-  public NlPropertyFixture findProperty(final String name) {
-    JBLabel label = waitUntilFound(robot(), myPanel, new GenericTypeMatcher<JBLabel>(JBLabel.class) {
-      @Override
-      protected boolean isMatching(@NotNull JBLabel label) {
-        return name.equals(label.getText()) && label.getIcon() == null;
-      }
-    });
+  public NlPropertyFixture findProperty(@NotNull String name) {
+    Component component = findPropertyComponent(name, null);
+    return component != null ? new NlPropertyFixture(robot(), (JPanel)component) : null;
+  }
 
-    Container parent = label.getParent();
-    Component[] components = parent.getComponents();
-    for (int i = 0; i < components.length; i++) {
-      if (label == components[i]) {
-        return new NlPropertyFixture(robot(), (JPanel)components[i + 1]);
+  @NotNull
+  public NlPropertyInspectorFixture adjustIdeFrameHeightFor(int visiblePropertyCount, @NotNull String propertyName) {
+    Component component = findPropertyComponent(propertyName, null);
+    assertThat(component).isNotNull();
+    int height = component.getHeight();
+    Container previousParent = null;
+    Container parent = component.getParent();
+    int adjustment = 0;
+    while (parent != null) {
+      if (adjustment == 0 && parent instanceof JScrollPane) {
+        adjustment = visiblePropertyCount * height - parent.getHeight();
       }
+      previousParent = parent;
+      parent = parent.getParent();
     }
-    return null;
+    assertThat(previousParent).isNotNull();
+    Dimension size = previousParent.getSize();
+    size.height += adjustment;
+    previousParent.setSize(size);
+    return this;
+  }
+
+  @NotNull
+  public NlPropertyInspectorFixture focusAndWaitForFocusGainInProperty(@NotNull String name, @Nullable Icon icon) {
+    Component component = findPropertyComponent(name, icon);
+    assertThat(component).isNotNull();
+    driver().focusAndWaitForFocusGain(component);
+    return this;
+  }
+
+  @NotNull
+  public NlPropertyInspectorFixture tab() {
+    return pressKeyInUnknownProperty(KeyEvent.VK_TAB, 0);
+  }
+
+  @NotNull
+  public NlPropertyInspectorFixture tabBack() {
+    return pressKeyInUnknownProperty(KeyEvent.VK_TAB, InputEvent.SHIFT_MASK);
+  }
+
+  @NotNull
+  public NlPropertyInspectorFixture pressKeyInUnknownProperty(int keyCode, int... modifiers) {
+    Component component = FocusManager.getCurrentManager().getFocusOwner();
+    assertThat(component).isNotNull();
+    assertThat(SwingUtilities.isDescendingFrom(component, myPanel)).isTrue();
+    driver().pressAndReleaseKey(component, keyCode, modifiers);
+    IdeFocusManager.findInstance().doWhenFocusSettlesDown(() -> {});
+    return this;
+  }
+
+  @NotNull
+  public NlPropertyInspectorFixture assertPropertyShowing(@NotNull String name, @Nullable Icon icon) {
+    assertThat(isPropertyShowing(name, icon)).named("Property is Visible to user: " + name).isTrue();
+    return this;
+  }
+
+  @NotNull
+  public NlPropertyInspectorFixture assertPropertyNotShowing(@NotNull String name, @Nullable Icon icon) {
+    assertThat(isPropertyShowing(name, icon)).named("Property is Visible to user: " + name).isFalse();
+    return this;
+  }
+
+  @NotNull
+  public NlPropertyInspectorFixture assertFocusInProperty(@NotNull String name, @Nullable Icon icon) {
+    Component propertyComponent = findPropertyComponent(name, icon);
+    Component focusComponent = FocusManager.getCurrentManager().getFocusOwner();
+    assertThat(propertyComponent != null).named("property: " + name + " found").isTrue();
+    assertThat(SwingUtilities.isDescendingFrom(focusComponent, propertyComponent)).named("property: " + name + " has focus").isTrue();
+    return this;
+  }
+
+  public boolean isPropertyShowing(@NotNull String name, @Nullable Icon icon) {
+    Component component = findPropertyComponent(name, icon);
+    if (component == null) {
+      return false;
+    }
+    Rectangle rect = component.getBounds();
+    Container parent = component.getParent();
+    while (parent != null) {
+      Rectangle bounds = parent.getBounds();
+      if (rect.y > bounds.height || rect.y + rect.height < 0 ||
+          rect.x > bounds.width || rect.x + rect.width < 0) {
+        return false;
+      }
+      rect.x += bounds.x;
+      rect.y += bounds.y;
+      parent = parent.getParent();
+    }
+    return true;
+  }
+
+  @Nullable
+  private Component findPropertyComponent(@NotNull String name, @Nullable Icon icon) {
+    try {
+      JBLabel label = waitUntilFound(robot(), myPanel, new GenericTypeMatcher<JBLabel>(JBLabel.class) {
+        @Override
+        protected boolean isMatching(@NotNull JBLabel label) {
+          return name.equals(label.getText()) && label.getIcon() == icon;
+        }
+      });
+
+      Container parent = label.getParent();
+      Component[] components = parent.getComponents();
+      for (int i = 0; i < components.length; i++) {
+        if (label == components[i]) {
+          return components[i + 1];
+        }
+      }
+      return null;
+    }
+    catch (WaitTimedOutError ex) {
+      return null;
+    }
   }
 }
