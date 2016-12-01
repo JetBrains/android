e3fc929a9124edd31925fd4e8fab5d2aa42ec2cf
Andrii Dehtiarov <adehtiarov@google.com>
2016-12-01 18:21:01 +0000

    Implement speed search support in layout inspector properties table
    Also fix properties comparator to ensure proper sorting
    
    Bug: b.android.com/229225
    Test: Added testPropertiesOrdering and testTableModel by this CL
    Change-Id: If7d7b3804c01fd24201c524619172b62639eab3c

--- android/testSrc/com/android/tools/idea/editors/hierarchyview/model/ViewNodeTest.java
+++ android/testSrc/com/android/tools/idea/editors/hierarchyview/model/ViewNodeTest.java
@@ -15,10 +15,10 @@
  */
 package com.android.tools.idea.editors.hierarchyview.model;
 
+import com.android.tools.idea.editors.hierarchyview.ui.ViewNodeTableModel;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class ViewNodeTest {
 
@@ -40,10 +40,52 @@ public class ViewNodeTest {
     assertEquals(node.getProperty("cat:foo").getValue(), "this is a long text");
   }
 
+  @Test
+  public void testPropertiesOrdering() throws Exception {
+    ViewNode root = ViewNode.parseFlatString(getViewNodeFlatString());
+
+    ViewProperty prop1 = root.getChildAt(0).getProperty("cat:foo");
+    ViewProperty prop2 = root.getChildAt(1).getChildAt(0).getProperty("cat:foo");
+
+    assertEquals(prop1, prop2);
+    assertTrue(prop1.compareTo(prop2) == 0);
+
+    // Test comparison between properties with null and non-null category values
+    ViewProperty prop3 = root.getProperty("zoo");
+    assertEquals(prop3.compareTo(prop1), -1);
+    assertEquals(prop1.compareTo(prop3), 1);
+    assertNotEquals(prop1, prop3);
+
+    // Test same non-null category values
+    ViewProperty prop4 = root.getChildAt(0).getProperty("cow:foo");
+    ViewProperty prop5 = root.getChildAt(0).getProperty("cow:child");
+    assertNotEquals(prop4, prop5);
+    assertEquals(prop4.compareTo(prop5), 1);
+    assertEquals(prop5.compareTo(prop4), -1);
+
+    // Test non-null categories ordering with the same property name
+    assertNotEquals(prop1, prop4);
+    assertEquals(prop1.compareTo(prop4), -1);
+    assertEquals(prop4.compareTo(prop1), 1);
+  }
+
+  @Test
+  public void testViewNodeTableModel() throws Exception {
+    ViewNode node = ViewNode.parseFlatString(getViewNodeFlatString());
+    ViewNodeTableModel model = new ViewNodeTableModel();
+    model.setNode(node);
+    assertEquals(4, model.getRowCount());
+    // Arbitrarily take the second row for testing
+    assertTrue(model.getValueAt(1, 0) instanceof String);
+    assertTrue(model.getValueAt(1, 1) instanceof String);
+    assertEquals("zoo", model.getValueAt(1, 0));
+    assertEquals("baz", model.getValueAt(1, 1));
+  }
+
   private static byte[] getViewNodeFlatString() {
     String text =
-      "myroot@191 cat:foo=4,4394 cat2:foo2=5,hello mID=3,god \n" +
-      "  node1@3232 cat:foo=8,[] happy cow:child=4,calf mID=9,not-a-god \n" +
+      "myroot@191 cat:foo=4,4394 cat2:foo2=5,hello zoo=3,baz mID=3,god \n" +
+      "  node1@3232 cat:foo=8,[] happy cow:child=4,calf cow:foo=5,super mID=9,not-a-god \n" +
       "  node2@222 noun:eg=10,alpha beta mID=11,maybe-a-god \n" +
       "    node3@3333 mID=11,another-god cat:foo=19,this is a long text \n" +
       "DONE.\n";
