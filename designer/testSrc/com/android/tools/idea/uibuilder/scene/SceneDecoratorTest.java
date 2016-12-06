48504bd2d7ed5acdf88cb20e1848fd1bf2194ef2
hoford <hoford@google.com>
2016-12-06 12:24:55 -0800

    improve drawing of connections and basline connections Test: added test SceneDisplayListTest5 to cover baseline
    Change-Id: I23185011e482fae7c6b6e612e4b38e55a7725039

--- designer/testSrc/com/android/tools/idea/uibuilder/scene/SceneDecoratorTest.java
+++ designer/testSrc/com/android/tools/idea/uibuilder/scene/SceneDecoratorTest.java
@@ -108,7 +108,7 @@ public class SceneDecoratorTest extends SceneTest {
     //noinspection UndesirableClassUsage
     BufferedImage img = new BufferedImage(1000, 1000,BufferedImage.TYPE_INT_ARGB);
     disp.paint(img.createGraphics(), SceneContext.get());
-    assertEquals(66, disp.getCommands().size());
+    assertEquals(36, disp.getCommands().size());
     disp.clear();
   }
 }
--- designer/testSrc/com/android/tools/idea/uibuilder/scene/SceneDisplayListTest5.java
+++ designer/testSrc/com/android/tools/idea/uibuilder/scene/SceneDisplayListTest5.java
@@ -24,34 +24,42 @@ import java.awt.image.BufferedImage;
 import static com.android.SdkConstants.CONSTRAINT_LAYOUT;
 import static com.android.SdkConstants.TEXT_VIEW;
 
-public class SceneDisplayListTest2 extends SceneTest {
+public class SceneDisplayListTest5 extends SceneTest {
   @Override
   @NotNull
   public ModelBuilder createModel() {
     return model("constraint.xml",
                  component(CONSTRAINT_LAYOUT)
-                   .id("@id/root")
+                   .id("@+id/root")
                    .withBounds(0, 0, 1000, 1000)
                    .width("1000dp")
                    .height("1000dp")
                    .withAttribute("android:padding", "20dp")
                    .children(
                      component(TEXT_VIEW)
-                       .id("@id/button")
+                       .id("@+id/button1")
                        .withBounds(450, 490, 100, 20)
                        .width("100dp")
                        .height("20dp")
                        .withAttribute("app:layout_constraintLeft_toLeftOf", "parent")
                        .withAttribute("app:layout_constraintRight_toRightOf", "parent")
                        .withAttribute("app:layout_constraintTop_toTopOf", "parent")
-                       .withAttribute("app:layout_constraintBottom_toBottomOf", "parent")
+                       .withAttribute("app:layout_constraintBottom_toBottomOf", "parent"),
+                     component(TEXT_VIEW)
+                       .id("@+id/text1")
+                       .withBounds(450, 490, 100, 20)
+                       .width("100dp")
+                       .height("20dp")
+                       .withAttribute("app:layout_constraintLeft_toLeftOf", "@+id/button1")
+                       .withAttribute("app:layout_constraintRight_toRightOf", "@+id/button1")
+                       .withAttribute("app:layout_constraintBaseline_toBaselineOf", "@+id/button1")
                    ));
   }
 
   public void testBasicScene() {
-    myScreen.get("@id/button")
+    myScreen.get("@+id/button1")
       .expectXml("<TextView\n" +
-                 "    android:id=\"@id/button\"\n" +
+                 "    android:id=\"@+id/button1\"\n" +
                  "    android:layout_width=\"100dp\"\n" +
                  "    android:layout_height=\"20dp\"\n" +
                  "    app:layout_constraintLeft_toLeftOf=\"parent\"\n" +
@@ -67,6 +75,11 @@ public class SceneDisplayListTest2 extends SceneTest {
                         "DrawConnection,2,450x490x100x20,1,0x0x1000x1000,1,true,false,0,0.5\n" +
                         "DrawConnection,2,450x490x100x20,2,0x0x1000x1000,2,true,false,0,0.5\n" +
                         "DrawConnection,2,450x490x100x20,3,0x0x1000x1000,3,true,false,0,0.5\n" +
+                        "Rect,450,490,100,20,ff00ffff\n" +
+                        "DrawTextRegion,450,490,100,20,false,false,5,5,\"\"\n" +
+                        "DrawConnection,4,450x490x100x20,0,450x490x100x20,0,false,true,0,0.5\n" +
+                        "DrawConnection,4,450x490x100x20,1,450x490x100x20,1,false,true,0,0.5\n" +
+                        "DrawConnection,5,450x490x100x0,5,450x490x100x0,5,false,false,0,0.0\n" +
                         "UNClip\n";
 
     assertEquals(simpleList, myInteraction.getDisplayList().serialize());
@@ -75,7 +88,7 @@ public class SceneDisplayListTest2 extends SceneTest {
     //noinspection UndesirableClassUsage
     BufferedImage img = new BufferedImage(1000, 1000,BufferedImage.TYPE_INT_ARGB);
     disp.paint(img.createGraphics(), SceneContext.get());
-    assertEquals(9, disp.getCommands().size());
+    assertEquals(14, disp.getCommands().size());
     disp.clear();
   }
 }
