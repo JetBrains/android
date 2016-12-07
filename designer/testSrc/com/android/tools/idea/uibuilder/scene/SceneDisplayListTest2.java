6d668c91d36e6ca46b4b078ae278b41a417d2a18
hoford <hoford@google.com>
2016-12-06 20:49:58 -0800

    add back improved sorting Test: covered by existing SceneDisplayListTest*.java
    Change-Id: I65f29feacdfa84aae65a687aa73e685a2968e506

--- designer/testSrc/com/android/tools/idea/uibuilder/scene/SceneDisplayListTest2.java
+++ designer/testSrc/com/android/tools/idea/uibuilder/scene/SceneDisplayListTest2.java
@@ -62,7 +62,7 @@ public class SceneDisplayListTest2 extends SceneTest {
     String simpleList = "Rect,0,0,1000,1000,ff00ffff\n" +
                         "Clip,0,0,1000,1000\n" +
                         "Rect,450,490,100,20,ff00ffff\n" +
-                        "DrawTextRegion,450,490,100,20,false,false,5,5,\"\"\n" +
+                        "DrawTextRegion,450,490,100,20,0,false,false,5,5,\"\"\n" +
                         "DrawConnection,2,450x490x100x20,0,0x0x1000x1000,0,true,false,0,0.5\n" +
                         "DrawConnection,2,450x490x100x20,1,0x0x1000x1000,1,true,false,0,0.5\n" +
                         "DrawConnection,2,450x490x100x20,2,0x0x1000x1000,2,true,false,0,0.5\n" +
