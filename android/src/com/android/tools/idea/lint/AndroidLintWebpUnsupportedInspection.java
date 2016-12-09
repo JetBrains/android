95163bdde4a85af606e8af26191c4002fea966bd
Tor Norbye <tnorbye@google.com>
2016-12-09 09:18:41 -0800

    229626: Don't use webp for launcher icons
    The developer console requires PNG files for launcher icons.
    We recently switched to using WEBP for launcher icons for default
    projects if the minSdkVersion >= 18, but that means that as soon
    as you try to publish the app it will be rejected.
    
    This CL updates the convert to webp action (which recursively
    converts PNG images in a folder) to skip launcher icons.
    It also updates the resource chooser to no longer rely on
    layoutlib to render webp images since we have a direct decoder
    in the IDE.
    
    Finally, it registers the lint warning which is emitted for
    non-PNG launcher icons as an IDE inspection.
    
    Test: New unit tests added
    Change-Id: Ifc7264aaaa24296e834e67d205fadddd40e720c9

--- android/src/com/android/tools/idea/lint/AndroidLintWebpUnsupportedInspection.java
+++ android/src/com/android/tools/idea/lint/AndroidLintWebpUnsupportedInspection.java
@@ -17,8 +17,6 @@ package com.android.tools.idea.lint;
 
 import com.android.tools.idea.rendering.webp.ConvertFromWebpAction;
 import com.android.tools.lint.checks.IconDetector;
-import com.intellij.openapi.command.WriteCommandAction;
-import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiElement;
 import org.jetbrains.android.inspections.lint.AndroidLintInspectionBase;
@@ -26,16 +24,6 @@ import org.jetbrains.android.inspections.lint.AndroidLintQuickFix;
 import org.jetbrains.android.inspections.lint.AndroidQuickfixContexts;
 import org.jetbrains.android.util.AndroidBundle;
 import org.jetbrains.annotations.NotNull;
-
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
-import java.io.BufferedOutputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
-
-import static com.android.SdkConstants.DOT_PNG;
-import static com.android.SdkConstants.DOT_WEBP;
 
 public class AndroidLintWebpUnsupportedInspection extends AndroidLintInspectionBase {
   public AndroidLintWebpUnsupportedInspection() {
@@ -46,37 +34,23 @@ public class AndroidLintWebpUnsupportedInspection extends AndroidLintInspectionB
   @NotNull
   public AndroidLintQuickFix[] getQuickFixes(@NotNull PsiElement startElement, @NotNull PsiElement endElement, @NotNull String message) {
     return new AndroidLintQuickFix[]{
-      new DefaultLintQuickFix("Convert image to PNG") {
-        @Override
-        public void apply(@NotNull PsiElement startElement,
-                          @NotNull PsiElement endElement,
-                          @NotNull AndroidQuickfixContexts.Context context) {
-          VirtualFile file = startElement.getContainingFile().getVirtualFile();
-          if (file.exists()) {
-            new ConvertFromWebpAction().perform(startElement.getProject(), new VirtualFile[] {file }, true);
-            WriteCommandAction.runWriteCommandAction(startElement.getProject(), () -> {
-              try {
-                BufferedImage image = ImageIO.read(file.getInputStream());
-                if (image != null) {
-                  ByteArrayOutputStream stream = new ByteArrayOutputStream(2048);
-                  ImageIO.write(image, "PNG", stream);
-                  byte[] encoded = stream.toByteArray();
-                  VirtualFile folder = file.getParent();
-                  VirtualFile output = folder.createChildData(this, file.getNameWithoutExtension() + DOT_PNG);
-                  try (OutputStream fos = new BufferedOutputStream(output.getOutputStream(this))) {
-                    fos.write(encoded);
-                  }
-                  file.delete(this);
-                  folder.refresh(true, true);
-                }
-              }
-              catch (IOException e) {
-                Logger.getInstance(AndroidLintWebpUnsupportedInspection.class).warn(e.getMessage());
-              }
-            });
-          }
-        }
-      }
+      new ConvertWebpToPngFix()
     };
+  }
+
+  static class ConvertWebpToPngFix extends DefaultLintQuickFix {
+    public ConvertWebpToPngFix() {
+      super("Convert image to PNG");
+    }
+
+    @Override
+    public void apply(@NotNull PsiElement startElement,
+                      @NotNull PsiElement endElement,
+                      @NotNull AndroidQuickfixContexts.Context context) {
+      VirtualFile file = startElement.getContainingFile().getVirtualFile();
+      if (file.exists()) {
+        new ConvertFromWebpAction().perform(startElement.getProject(), new VirtualFile[] {file }, true);
+      }
+    }
   }
 }
