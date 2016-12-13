13e57cb12488b3e3f9f3909ab9ed76f94384c63f
Pavel Labath <labath@google.com>
2016-12-13 14:13:38 +0000

    DeployTargetPickerDialogFixture: make selectDevice more robust
    Even though the method description claims to select the device
    whose name *begins* with `text`, it was only searching for an
    exact match. This meant that we would fail to select an emulator
    which is already running as in this case it's name has additional
    text appended (API level and such). I am going to need this
    functionality in a test I am writing, which tries to deploy to an
    already-running emulator.
    
    Test: Existing tests still pass.
    Change-Id: I04b963b639980c61dacc722a50d8c048029e6e89

--- uitest-framework/testSrc/com/android/tools/idea/tests/gui/framework/fixture/DeployTargetPickerDialogFixture.java
+++ uitest-framework/testSrc/com/android/tools/idea/tests/gui/framework/fixture/DeployTargetPickerDialogFixture.java
@@ -65,10 +65,16 @@ public class DeployTargetPickerDialogFixture extends ComponentFixture<DeployTarg
    * {@code getListCellRendererComponent} method returns {@code this}, which is also a {@code SimpleColoredComponent}, whose
    * {@code toString} method returns what gets rendered to the cell. You may vomit now.
    */
-  private static final JListCellReader DEVICE_PICKER_CELL_READER = (jList, index) ->
-    jList.getCellRenderer().getListCellRendererComponent(jList, jList.getModel().getElementAt(index), index, true, true).toString();
+  private static final JListCellReader DEVICE_PICKER_CELL_READER = (jList, index) -> {
+    String item =
+      jList.getCellRenderer().getListCellRendererComponent(jList, jList.getModel().getElementAt(index), index, true, true).toString();
+    // Ignore everything after the parenthesis (typically includes stuff like API level and ABI)
+    return item.split("\\(", 2)[0].trim();
+  };
 
-  /* Selects a device whose entry in the devices list begins with {@code text}. */
+  /**
+   *  Selects a device whose entry in the devices list is {@code text}. Any characters after the first parenthesis are ignored.
+   */
   @NotNull
   public DeployTargetPickerDialogFixture selectDevice(String text) {
     JBList deviceList = robot().finder().findByType(target(), JBList.class);
@@ -76,7 +82,7 @@ public class DeployTargetPickerDialogFixture extends ComponentFixture<DeployTarg
     jListFixture.replaceCellReader(DEVICE_PICKER_CELL_READER);
     Wait.seconds(5).expecting(String.format("Deployment Target list to contain %s", text))
       .until(() -> Arrays.asList(jListFixture.contents()).contains(text));
-    jListFixture.selectItem(Pattern.compile(text + ".*"));
+    jListFixture.selectItem(text);
     return this;
   }
 
