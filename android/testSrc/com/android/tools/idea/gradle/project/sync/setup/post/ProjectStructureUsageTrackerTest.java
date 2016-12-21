e5098d5e3439ccea7b675067e6eede6ad452418c
Raymond Chiu <chiur@google.com>
2016-12-21 11:08:30 -0800

    Fix GradleWrapper version parsing to handle rc
    Also fix a few tests issues exposed from the Gradle version update.
    
    Test: GradleVersionsTest and other tests now passes when a Gradle rc is used.
    Change-Id: I2e459f96ecd3dcb1fa2e8f12b41050496c93dfe4
    (cherry picked from commit 7eff46c8cc56ebd7a4d8eef37f7910ea46eaa171)

--- android/testSrc/com/android/tools/idea/gradle/project/sync/setup/post/ProjectStructureUsageTrackerTest.java
+++ android/testSrc/com/android/tools/idea/gradle/project/sync/setup/post/ProjectStructureUsageTrackerTest.java
@@ -15,11 +15,13 @@
  */
 package com.android.tools.idea.gradle.project.sync.setup.post;
 
+import com.android.SdkConstants;
 import com.android.testutils.VirtualTimeScheduler;
 import com.android.tools.analytics.AnalyticsSettings;
 import com.android.tools.analytics.LoggedUsage;
 import com.android.tools.analytics.TestUsageTracker;
 import com.android.tools.analytics.UsageTracker;
+import com.android.tools.idea.gradle.util.GradleVersions;
 import com.android.tools.idea.stats.AndroidStudioUsageTracker;
 import com.android.tools.idea.testing.AndroidGradleTestCase;
 import com.google.wireless.android.sdk.stats.*;
@@ -67,7 +69,7 @@ public class ProjectStructureUsageTrackerTest extends AndroidGradleTestCase {
     assertEquals(AndroidStudioEvent.EventKind.GRADLE_BUILD_DETAILS, usage.getStudioEvent().getKind());
     assertEquals(GradleBuildDetails.newBuilder()
                    .setAndroidPluginVersion("2.3.0-dev")
-                   .setGradleVersion("3.2")
+                   .setGradleVersion(GradleVersions.removeTimestampFromGradleVersion(SdkConstants.GRADLE_LATEST_VERSION))
                    .setUserEnabledIr(true)
                    .setModelSupportsIr(true)
                    .setVariantSupportsIr(true)
