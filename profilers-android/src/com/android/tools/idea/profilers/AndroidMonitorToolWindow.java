fa5ca4ad06ff7d8bb497b8ab4ee7baaee5a06cb9
estebandlc <estebandlc@google.com>
2016-12-21 22:04:07 -0800

    Moves profiler android-plugin code to its own module
    The new module profilers-android now contains all the code
    that interacts with the profiler modules. This means that
    the android module doesn't need to depend on the profiler ones.
    
    Besides the obvious better abstraction, this means that
    android_tests are no longer affected by changes
    to profilers and profilers-ui. This should save 30 minutes
    of PSQ time on these CLs.
    
    Test: N/A
    Bug: 33427831
    Change-Id: I1b69d0d31b94d2863034f2410b9a25a7e8680576

--- profilers-android/src/com/android/tools/idea/profilers/AndroidMonitorToolWindow.java
+++ profilers-android/src/com/android/tools/idea/profilers/AndroidMonitorToolWindow.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.android.tools.idea.monitor.tool;
+package com.android.tools.idea.profilers;
 
 import com.android.tools.adtui.model.AspectObserver;
 import com.android.tools.idea.model.AndroidModuleInfo;
