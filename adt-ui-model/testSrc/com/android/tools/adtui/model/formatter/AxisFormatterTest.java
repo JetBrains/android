d949fe944d15a3b505dba31427514fa0f4ca9a41
Yuxin Yu <yuxy@google.com>
2017-01-03 13:58:23 -0800

    Move MockAxisFormatter and tests to the same package as BaseAxisFormatter.
    This would help other classes use the mock formatter, please see
    SeriesLegendTest.
    
    Bug: initiated by b/33484640
    
    Test: Refactor only, AxisFormatterTest is passing.
    
    Change-Id: I5407f5bac99c8da0427742545cc1b1fd51e32755

--- adt-ui-model/testSrc/com/android/tools/adtui/model/formatter/AxisFormatterTest.java
+++ adt-ui-model/testSrc/com/android/tools/adtui/model/formatter/AxisFormatterTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016 The Android Open Source Project
+ * Copyright (C) 2017 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.android.tools.adtui;
+package com.android.tools.adtui.model.formatter;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -22,12 +22,12 @@ import static com.google.common.truth.Truth.assertThat;
 
 public class AxisFormatterTest {
 
-  private MockAxisFormatter domain;
+  private MockAxisFormatter myFormatter;
 
   @Before
   public void setUp() throws Exception {
     // maxMajorTicks = 5, maxMajorTicks = 10, switchThreshold = 5;
-    domain = new MockAxisFormatter(5, 10, 5);
+    myFormatter = new MockAxisFormatter(5, 10, 5);
   }
 
   @Test
@@ -35,12 +35,12 @@ public class AxisFormatterTest {
     // maxMinorTicks is 5 so the smallest possible interval is 100 / 5 = 20
     // The axis is in base 10 which has factors: {1, 5, 10}, so the interval
     // will get round up using the factor 5, which gives us 50.
-    long interval = domain.getMinorInterval(100);
+    long interval = myFormatter.getMinorInterval(100);
     assertThat(interval).isEqualTo(50);
 
     // maxMajorTicks is 10 so the smallest possible interval is 1000 / 10 = 100
     // This essentially matches the base factor 10, so this gives exactly 100.
-    interval = domain.getMajorInterval(1000);
+    interval = myFormatter.getMajorInterval(1000);
     assertThat(interval).isEqualTo(100);
   }
 
@@ -48,23 +48,23 @@ public class AxisFormatterTest {
   public void testGetMultiplierIndex() throws Exception {
     // value is equal or less than the threshold to get to the next multiplier
     // so we are still in "mm" scale
-    int index = domain.getMultiplierIndex(50, 5);
+    int index = myFormatter.getMultiplierIndex(50, 5);
     assertThat(index).isEqualTo(0);
-    assertThat(domain.getMultiplier()).isEqualTo(1);
-    assertThat(domain.getUnit(index)).isEqualTo("mm");
+    assertThat(myFormatter.getMultiplier()).isEqualTo(1);
+    assertThat(myFormatter.getUnit(index)).isEqualTo("mm");
 
     // value is greater than the first multiplier * threshold
     // jumps to "cm"
-    index = domain.getMultiplierIndex(51, 5);
+    index = myFormatter.getMultiplierIndex(51, 5);
     assertThat(index).isEqualTo(1);
-    assertThat(domain.getMultiplier()).isEqualTo(10);
-    assertThat(domain.getUnit(index)).isEqualTo("cm");
+    assertThat(myFormatter.getMultiplier()).isEqualTo(10);
+    assertThat(myFormatter.getUnit(index)).isEqualTo("cm");
 
     // value is greater than the second multiplier * threshold
     // jumps to "m"
-    index = domain.getMultiplierIndex(5001, 5);
+    index = myFormatter.getMultiplierIndex(5001, 5);
     assertThat(index).isEqualTo(2);
-    assertThat(domain.getMultiplier()).isEqualTo(1000);
-    assertThat(domain.getUnit(index)).isEqualTo("m");
+    assertThat(myFormatter.getMultiplier()).isEqualTo(1000);
+    assertThat(myFormatter.getUnit(index)).isEqualTo("m");
   }
 }
