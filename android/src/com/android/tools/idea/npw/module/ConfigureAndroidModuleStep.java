e23513ef56e0c5d8b9741d2217d55bb74b66530b
parentej <parentej@google.com>
2017-01-10 15:49:18 +0000

    New Project Wizard: Create new Android Library
    New code to add an Android Library to and existing project, using the
    new wizard UI
    
    Test: Run tests in NewModuleTest
    Change-Id: I58d0fd94ad08fca6fa7158c435be65aa0d4f2a9a

--- android/src/com/android/tools/idea/npw/module/ConfigureAndroidModuleStep.java
+++ android/src/com/android/tools/idea/npw/module/ConfigureAndroidModuleStep.java
@@ -24,6 +24,7 @@ import com.android.tools.idea.npw.project.DomainToPackageExpression;
 import com.android.tools.idea.npw.project.NewProjectModel;
 import com.android.tools.idea.npw.template.ChooseActivityTypeStep;
 import com.android.tools.idea.npw.template.RenderTemplateModel;
+import com.android.tools.idea.npw.template.TemplateValueInjector;
 import com.android.tools.idea.npw.template.TemplateHandle;
 import com.android.tools.idea.templates.TemplateManager;
 import com.android.tools.idea.ui.properties.BindingsManager;
@@ -40,7 +41,6 @@ import com.android.tools.idea.ui.validation.ValidatorPanel;
 import com.android.tools.idea.ui.validation.validators.PathValidator;
 import com.android.tools.idea.ui.wizard.StudioWizardStepPanel;
 import com.android.tools.idea.ui.wizard.WizardUtils;
-import com.android.tools.idea.wizard.model.ModelWizard;
 import com.android.tools.idea.wizard.model.ModelWizardStep;
 import com.android.tools.idea.wizard.model.SkippableWizardStep;
 import com.android.tools.swing.util.FormScalingUtil;
@@ -57,14 +57,13 @@ import java.util.List;
 import static org.jetbrains.android.util.AndroidBundle.message;
 
 
-public final class ConfigureAndroidModuleStep extends SkippableWizardStep<NewModuleModel> {
+public class ConfigureAndroidModuleStep extends SkippableWizardStep<NewModuleModel> {
   @NotNull private final StudioWizardStepPanel myRootPanel;
   @NotNull private final FormFactor myFormFactor;
   private final BindingsManager myBindings = new BindingsManager();
   private final ListenerManager myListeners = new ListenerManager();
   private final int myMinSdkLevel;
-
-  private ChooseActivityTypeStep myChooseActivityTypeStep;
+  private final boolean myIsLibrary;
 
   private FormFactorApiComboBox mySdkControls;
   private JTextField myModuleName;
@@ -72,11 +71,15 @@ public final class ConfigureAndroidModuleStep extends SkippableWizardStep<NewMod
   private JTextField myAppName;
   private LabelWithEditLink myPackageName;
 
-  public ConfigureAndroidModuleStep(@NotNull NewModuleModel model, @NotNull FormFactor formFactor, int minSdkLevel, @NotNull String title) {
+  protected RenderTemplateModel myRenderModel;
+
+  public ConfigureAndroidModuleStep(@NotNull NewModuleModel model, @NotNull FormFactor formFactor, int minSdkLevel,
+                                    boolean isLibrary, @NotNull String title) {
     super(model, title);
 
     myFormFactor = formFactor;
     myMinSdkLevel = minSdkLevel;
+    myIsLibrary = isLibrary;
 
     StringProperty companyDomain = new StringValueProperty(NewProjectModel.getInitialDomain(false));
     TextProperty packageNameText = new TextProperty(myPackageName);
@@ -89,7 +92,8 @@ public final class ConfigureAndroidModuleStep extends SkippableWizardStep<NewMod
     myListeners.receive(packageNameText, value -> isPackageNameSynced.set(value.equals(computedPackageName.get())));
 
     // Project should never be null (we are adding a new module to an existing project)
-    Project project = getModel().getProject().getValue();
+    NewModuleModel moduleModel = getModel();
+    Project project = moduleModel.getProject().getValue();
 
     Expression<String> computedModuleName = new AppNameToModuleNameExpression(project, model.applicationName());
     BoolProperty isModuleNameSynced = new BoolValueProperty(true);
@@ -117,40 +121,33 @@ public final class ConfigureAndroidModuleStep extends SkippableWizardStep<NewMod
     validatorPanel.registerValidator(model.packageName(),
                                        value -> Validator.Result.fromNullableMessage(WizardUtils.validatePackageName(value)));
 
+    List<TemplateHandle> templateList = TemplateManager.getInstance().getTemplateList(myFormFactor);
+    AndroidSourceSet dummySourceSet = new AndroidSourceSet("main", new AndroidProjectPaths(new File("")));
+
+    myRenderModel = new RenderTemplateModel(moduleModel, templateList.get(0), dummySourceSet,
+                                            message("android.wizard.activity.add"));
+
+    // Some changes on this step model trigger changes on the Render Model
+    myListeners.listenAll(moduleModel.getProject(), moduleModel.moduleName()).withAndFire(() -> {
+      String moduleName = moduleModel.moduleName().get();
+      myRenderModel.getSourceSet().set(new AndroidSourceSet("main", new AndroidProjectPaths(new File(project.getBasePath(), moduleName))));
+    });
+
+    myBindings.bind(myRenderModel.androidSdkInfo(), new SelectedItemProperty<>(mySdkControls));
+
     myRootPanel = new StudioWizardStepPanel(validatorPanel, message("android.wizard.module.config.title"));
     FormScalingUtil.scaleComponentTree(this.getClass(), myRootPanel);
-  }
-
-  @Override
-  protected void onWizardStarting(@NotNull ModelWizard.Facade wizard) {
-    myListeners.listen(getModel().isLibrary(), sender -> {
-      myChooseActivityTypeStep.setShouldShow(!getModel().isLibrary().get());
-      wizard.updateNavigationProperties();
-    });
   }
 
   @NotNull
   @Override
   protected Collection<? extends ModelWizardStep> createDependentSteps() {
+    if (myIsLibrary) {
+      // No dependent steps for libraries (no need to choose an activity)
+      return Lists.newArrayList();
+    }
     List<TemplateHandle> templateList = TemplateManager.getInstance().getTemplateList(myFormFactor);
-    AndroidSourceSet dummySourceSet = new AndroidSourceSet("main", new AndroidProjectPaths(new File("")));
-
-    RenderTemplateModel renderModel = new RenderTemplateModel(getModel(), templateList.get(0), dummySourceSet,
-                                                              message("android.wizard.activity.add"));
-
-    // Some changes on this step model trigger changes on the Render Model
-    myListeners.listenAll(getModel().getProject(), getModel().moduleName()).withAndFire(() -> {
-      String moduleName = getModel().moduleName().get();
-      Project project = getModel().getProject().getValue();
-
-      renderModel.getSourceSet().set(new AndroidSourceSet("main", new AndroidProjectPaths(new File(project.getBasePath(), moduleName))));
-    });
-
-    myBindings.bind(renderModel.androidSdkInfo(), new SelectedItemProperty<>(mySdkControls));
-
-    myChooseActivityTypeStep = new ChooseActivityTypeStep(getModel(), renderModel, templateList, Lists.newArrayList());
-
-    return Lists.newArrayList(myChooseActivityTypeStep);
+    return Lists.newArrayList(new ChooseActivityTypeStep(getModel(), myRenderModel, templateList, Lists.newArrayList()));
   }
 
   @Override
@@ -158,13 +155,28 @@ public final class ConfigureAndroidModuleStep extends SkippableWizardStep<NewMod
     // TODO: 1 - In the old version, the combo box was initially populated with local data, and then later (asynchronously) with remote data
     // now, the init is only called when all data is loaded, so the combo box stays empty for longer
     // 2 - The old version only loaded the list of version once, and kept everything on a static field
-    // Possible solutions: Move AndroidVersionsInfo/load to the to the class that instantiates this step?
+    // Possible solutions: Move AndroidVersionsInfo/load to the class that instantiates this step?
     // Add a new method to androidVersionsInfo.ItemsLoaded interface: onDataLoadedStarted(List<VersionItem> items) that provides the already
     // loaded Local Store items?
     AndroidVersionsInfo androidVersionsInfo = new AndroidVersionsInfo();
     androidVersionsInfo.load();
 
     androidVersionsInfo.loadTargetVersions(myFormFactor, myMinSdkLevel, items -> mySdkControls.init(myFormFactor, items));
+  }
+
+  @Override
+  protected void onProceeding() {
+    if (myIsLibrary) {
+      NewModuleModel moduleModel = getModel();
+      new TemplateValueInjector(myRenderModel.getTemplateValues())
+        .setBuildVersion(myRenderModel.androidSdkInfo().getValue())
+        .setModuleRoots(myRenderModel.getSourceSet().get().getPaths(), moduleModel.packageName().get());
+
+      new TemplateValueInjector(moduleModel.getTemplateValues())
+        .setProjectDefaults(moduleModel.getProject().getValueOrNull(), moduleModel.applicationName().get());
+
+      moduleModel.getRenderTemplateValues().setValue(myRenderModel.getTemplateValues());
+    }
   }
 
   @NotNull
