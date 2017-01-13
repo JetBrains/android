01e748398bb0a89cd3a59ce1d77f7229be4244f7
Madiyar Aitbayev <madiyar@google.com>
2017-01-13 17:34:34 +0000

    Bottom up tree in CpuCaptureView
    Bug: b/33302532
    Test: BottomUpNodeTest, BottomUpTreeModelTest
    
    Change-Id: If784ab3f4e6272eb7b963a5706e1577a09d5b087

---
+++ profilers/src/com/android/tools/profilers/cpu/BottomUpTreeModel.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.profilers.cpu;
+
+import com.android.tools.adtui.model.Range;
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.tree.DefaultMutableTreeNode;
+
+public class BottomUpTreeModel extends CpuTreeModel<BottomUpNode> {
+  public BottomUpTreeModel(@NotNull Range range, @NotNull BottomUpNode node) {
+    super(range, node);
+  }
+
+  @Override
+  public void expand(@NotNull DefaultMutableTreeNode node) {
+    BottomUpNode bottomUpNode = (BottomUpNode)node.getUserObject();
+
+    for (int i = 0; i < node.getChildCount(); ++i) {
+      DefaultMutableTreeNode child = (DefaultMutableTreeNode)node.getChildAt(i);
+      BottomUpNode childBottomUp = (BottomUpNode)child.getUserObject();
+
+      if (childBottomUp.buildChildren()) {
+        loadChildren(child);
+      }
+    }
+
+    // Some children of the |bottomUpNode| may be invisible in the current range, so build their children too
+    for (BottomUpNode child: bottomUpNode.getChildren()) {
+      child.buildChildren();
+    }
+  }
+
+  private void loadChildren(@NotNull DefaultMutableTreeNode node) {
+    BottomUpNode bottomUpNode = (BottomUpNode)node.getUserObject();
+
+    for (BottomUpNode child: bottomUpNode.getChildren()) {
+      if (child.inRange(getRange())) {
+        child.update(getRange());
+        insertNodeInto(new DefaultMutableTreeNode(child), node, 0);
+      }
+      else {
+        child.reset();
+      }
+    }
+  }
+}
