b2d695ff27d9ea87e40d05b48b5daf4f5f954499
Billy Lam <shiufai@google.com>
2017-02-01 10:55:36 -0800

    Fixed an issue where LineChart renders nothing.
    Test: added unit test
    Change-Id: I98b341b8c33cce6bf3d602a8201363a3c6f62996

--- adt-ui/src/test/java/com/android/tools/adtui/chart/linechart/LineChartTest.java
+++ adt-ui/src/test/java/com/android/tools/adtui/chart/linechart/LineChartTest.java
@@ -22,6 +22,7 @@ import com.android.tools.adtui.model.RangedContinuousSeries;
 import org.junit.Test;
 
 import java.awt.*;
+import java.util.concurrent.TimeUnit;
 
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.*;
@@ -47,7 +48,33 @@ public class LineChartTest {
     Graphics2D fakeGraphics = mock(Graphics2D.class);
     when(fakeGraphics.create()).thenReturn(fakeGraphics);
     doThrow(new AssertionError()).when(fakeGraphics).draw(any(Shape.class));
-    doThrow(new AssertionError()).when(fakeGraphics).fill(any(Shape.class));
     chart.paint(fakeGraphics);
+  }
+
+  @Test
+  public void testRenderConfigNoWithData() throws Exception {
+    LineChartModel model = new LineChartModel();
+    DefaultDataSeries<Long> emptySeries = new DefaultDataSeries<>();
+    DefaultDataSeries<Long> seriesWithData = new DefaultDataSeries<>();
+    for (int i = 0; i < 11; i++) {
+      seriesWithData.add(i, (long)i);
+    }
+    RangedContinuousSeries rangedEmptySeries = new RangedContinuousSeries("emptySeries", new Range(), new Range(), emptySeries);
+    RangedContinuousSeries rangedSeriesWithData =
+      new RangedContinuousSeries("seriesWithData", new Range(0, 10), new Range(0, 0), seriesWithData);
+    model.add(rangedEmptySeries);
+    model.add(rangedSeriesWithData);
+
+    LineChart chart = new LineChart(model);
+    chart.configure(rangedEmptySeries, new LineConfig(Color.BLACK));
+    chart.configure(rangedSeriesWithData, new LineConfig(Color.WHITE));
+    model.update(TimeUnit.SECONDS.toNanos(1));
+    chart.setSize(100, 100);
+    Graphics2D fakeGraphics = mock(Graphics2D.class);
+    when(fakeGraphics.create()).thenReturn(fakeGraphics);
+    chart.paint(fakeGraphics);
+
+    // A line should be drawn for the seriesWithData, but nothing for the emptySeries.
+    verify(fakeGraphics, times(1)).draw(any(Shape.class));
   }
 }
