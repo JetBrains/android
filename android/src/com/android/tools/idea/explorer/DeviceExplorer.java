b15fcc0052333691de3e630b84b684d3374e33c0
estebandlc <estebandlc@google.com>
2017-01-31 20:57:00 -0800

    Moves conditional tool windows to be facet.toolWindow
    Removes custom ways of doing this by using the newly fixed facet.toolWindow
    
    Change-Id: I515732b7fd266235fa90ccb0505895d544d28c7e
    Test: covered by UI tests
    Bug: b.android.com/233033

--- android/src/com/android/tools/idea/explorer/DeviceExplorer.java
+++ android/src/com/android/tools/idea/explorer/DeviceExplorer.java
@@ -15,68 +15,14 @@
  */
 package com.android.tools.idea.explorer;
 
-import com.intellij.facet.ProjectFacetManager;
-import com.intellij.facet.ProjectWideFacetAdapter;
-import com.intellij.facet.ProjectWideFacetListenersRegistry;
-import com.intellij.openapi.diagnostic.Logger;
-import com.intellij.openapi.extensions.Extensions;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.util.text.StringUtil;
-import com.intellij.openapi.wm.ToolWindowEP;
-import com.intellij.openapi.wm.ex.ToolWindowManagerEx;
 import com.intellij.util.SystemProperties;
-import org.jetbrains.android.facet.AndroidFacet;
-import org.jetbrains.annotations.NotNull;
-
-import java.util.Arrays;
 
 public class DeviceExplorer {
-  private static final Logger LOGGER = Logger.getInstance(DeviceExplorer.class);
   private static final String DEVICE_EXPLORER_ENABLED = "android.device.explorer.enabled";
   private static boolean myEnabled;
 
-  public static boolean isFeatureEnabled(@NotNull Project project) {
-    boolean enabled = myEnabled || SystemProperties.getBooleanProperty(DEVICE_EXPLORER_ENABLED, true);
-    if (!enabled) {
-      return false;
-    }
-
-    if (!ProjectFacetManager.getInstance(project).hasFacets(AndroidFacet.ID)) {
-      LOGGER.info("Project has no Android facet, delaying tool window creation.");
-      // If facet is not present, register a listener in case the facet
-      // is added later (which happens when creating a new Android project)
-      registerFacetListener(project);
-      return false;
-    }
-
-    return true;
-  }
-
-  private static void registerFacetListener(@NotNull Project project) {
-    ProjectWideFacetListenersRegistry registry = ProjectWideFacetListenersRegistry.getInstance(project);
-    if (registry == null) {
-      LOGGER.warn("Cannot find ProjectWideFacetListenersRegistry instance.");
-      return;
-    }
-    ToolWindowManagerEx toolWindowManager = ToolWindowManagerEx.getInstanceEx(project);
-    if (toolWindowManager == null) {
-      LOGGER.warn("Cannot find ToolWindowManagerEx instance.");
-      return;
-    }
-
-    registry.registerListener(AndroidFacet.ID, new ProjectWideFacetAdapter<AndroidFacet>() {
-      @Override
-      public void facetAdded(@NotNull AndroidFacet facet) {
-        registry.unregisterListener(AndroidFacet.ID, this);
-        if (toolWindowManager.getToolWindow(DeviceExplorerToolWindowFactory.TOOL_WINDOW_ID) != null) {
-          return;
-        }
-        Arrays.stream(Extensions.getExtensions(ToolWindowEP.EP_NAME))
-          .filter(x -> StringUtil.equals(x.id, DeviceExplorerToolWindowFactory.TOOL_WINDOW_ID))
-          .findFirst()
-          .ifPresent(toolWindowManager::initToolWindow);
-      }
-    }, project);
+  public static boolean isFeatureEnabled() {
+    return myEnabled || SystemProperties.getBooleanProperty(DEVICE_EXPLORER_ENABLED, true);
   }
 
   public static void enableFeature(boolean enabled) {
