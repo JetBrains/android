a699b513751c0672945e3cf8c5aa26934478e5e0
Jens Ole Lauridsen <jlauridsen@google.com>
2017-02-03 08:42:44 -0800

    Add scaling for the initial width of attached tool windows.
    Bug: http://b.android.com/220375
    Test: updated
    Change-Id: I25c1c0dd20b5d5f76df2d61d464c5c2d9b110917

--- designer/src/com/android/tools/idea/uibuilder/palette/NlPaletteDefinition.java
+++ designer/src/com/android/tools/idea/uibuilder/palette/NlPaletteDefinition.java
@@ -21,16 +21,10 @@ import com.intellij.icons.AllIcons;
 import com.intellij.openapi.project.Project;
 import org.jetbrains.annotations.NotNull;
 
-import javax.swing.*;
-
-import static com.android.tools.adtui.common.AdtUiUtils.DEFAULT_FONT;
-import static com.intellij.openapi.actionSystem.ActionToolbar.DEFAULT_MINIMUM_BUTTON_SIZE;
-
 public class NlPaletteDefinition extends ToolWindowDefinition<DesignSurface> {
 
   public NlPaletteDefinition(@NotNull Project project, @NotNull Side side, @NotNull Split split, @NotNull AutoHide autoHide) {
-    super("Palette ", AllIcons.Toolwindows.ToolWindowPalette, "PALETTE", side, split, autoHide, getInitialWidth(),
-          DEFAULT_MINIMUM_BUTTON_SIZE, () -> createPalettePanel(project));
+    super("Palette ", AllIcons.Toolwindows.ToolWindowPalette, "PALETTE", side, split, autoHide, () -> createPalettePanel(project));
   }
 
   private static ToolContent<DesignSurface> createPalettePanel(@NotNull Project project) {
@@ -38,11 +32,5 @@ public class NlPaletteDefinition extends ToolWindowDefinition<DesignSurface> {
       return null;
     }
     return new NlPalettePanel(project, null);
-  }
-
-  private static int getInitialWidth() {
-    JComponent component = new JPanel();
-    return Math.max(ToolWindowDefinition.DEFAULT_SIDE_WIDTH,
-                    component.getFontMetrics(DEFAULT_FONT).stringWidth("AppCompat ConstraintLayout") + 40);
   }
 }
