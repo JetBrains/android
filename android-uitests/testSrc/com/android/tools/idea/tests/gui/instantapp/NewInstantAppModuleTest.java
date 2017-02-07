625b04d01d9146a59491c3dd15f53af417d25cfd
parentej <parentej@google.com>
2017-02-07 11:28:56 +0000

    New Project Wizard: move Instant Apps config
    This will help the new Project Wizard and the old Dynamic code, to
    share the same settings.
    
    Test: NewActivityTest, FlavoursExecutionTest, NewInstantAppTest,
          EnableInstantAppUiTest, MinSdkForInstantAppTest, NewProjectTest
    Change-Id: Ide7dde5198a0dbd33b9386c6b2c710a9e60131c4

--- android-uitests/testSrc/com/android/tools/idea/tests/gui/instantapp/NewInstantAppModuleTest.java
+++ android-uitests/testSrc/com/android/tools/idea/tests/gui/instantapp/NewInstantAppModuleTest.java
@@ -43,12 +43,12 @@ import java.io.IOException;
 
 import static com.android.tools.idea.gradle.util.BuildMode.SOURCE_GEN;
 import static com.android.tools.idea.npw.deprecated.ConfigureAndroidProjectStep.SAVED_COMPANY_DOMAIN;
-import static com.android.tools.idea.npw.deprecated.NewFormFactorModulePath.setAiaSdkLocation;
 import static com.android.tools.idea.testing.FileSubject.file;
 import static com.google.common.truth.Truth.assertAbout;
 import static com.google.common.truth.Truth.assertThat;
 import static com.intellij.openapi.util.io.FileUtil.join;
 import static java.lang.System.getenv;
+import static org.jetbrains.android.util.InstantApps.setAiaSdkLocation;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 
