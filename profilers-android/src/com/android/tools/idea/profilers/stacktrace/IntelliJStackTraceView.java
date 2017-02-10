3a4314c7aef70b54c421cb7f04ed64cc1079a94f
David Herman <davidherman@google.com>
2017-02-10 11:03:32 -0800

    Change IntelliJStackTraceView's interaction behavior
    * Press double click to jump to code, instead of single click
    * Press <ENTER> as an alternate way to jump to code
    * Removed <ESC> handling, as it was originally used in
      conjunction with single-click navigation (single-click
      would shrink the profilers to emphasize code, and ESC
      would re-expand them)
      o This makes the code navigation control more consistent
        with other controls, which do not lose their selection
        when you press ESC
    
    Bug: 65689857
    
    Test: IntelliJStackTraceViewTest updated; all new logic is
    covered.
    
    Change-Id: Ic693e24523474a4416aacf71841a4ed9ea4248df
    (cherry picked from commit ac0bd160871660b7dbbaaa29c524bd4cd1f7fcfe)

--- profilers-android/src/com/android/tools/idea/profilers/stacktrace/IntelliJStackTraceView.java
+++ profilers-android/src/com/android/tools/idea/profilers/stacktrace/IntelliJStackTraceView.java
@@ -25,6 +25,7 @@ import com.google.common.annotations.VisibleForTesting;
 import com.intellij.icons.AllIcons;
 import com.intellij.openapi.project.Project;
 import com.intellij.ui.ColoredListCellRenderer;
+import com.intellij.ui.DoubleClickListener;
 import com.intellij.ui.SimpleTextAttributes;
 import com.intellij.ui.components.JBList;
 import com.intellij.ui.components.JBScrollPane;
@@ -36,8 +37,10 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
 import java.util.List;
 import java.util.function.BiFunction;
+import java.util.function.Supplier;
 
 import static com.intellij.ui.SimpleTextAttributes.*;
 
@@ -82,25 +85,38 @@ public class IntelliJStackTraceView extends AspectObserver implements StackTrace
     myScrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
 
     myListView.addListSelectionListener(e -> {
-      int index = myListView.getSelectedIndex();
-      if (index < 0 || index >= myListView.getItemsCount() || myListView.getItemsCount() == 0) {
+      if (myListView.getSelectedValue() == null) {
         myModel.clearSelection();
-        return;
       }
-
-      myModel.setSelectedIndex(index);
     });
+
+    Supplier<Boolean> navigationHandler = () -> {
+      int index = myListView.getSelectedIndex();
+      if (index >= 0 && index < myListView.getItemsCount()) {
+        myModel.setSelectedIndex(index);
+        return true;
+      }
+      else {
+        return false;
+      }
+    };
 
     myListView.addKeyListener(new KeyAdapter() {
       @Override
       public void keyPressed(KeyEvent e) {
-        if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {
-          myListView.clearSelection();
-          e.consume();
+        if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+          if (navigationHandler.get()) {
+            e.consume();
+          }
         }
       }
     });
-
+    new DoubleClickListener() {
+      @Override
+      protected boolean onDoubleClick(MouseEvent event) {
+        return navigationHandler.get();
+      }
+    }.installOn(myListView);
 
     myModel.addDependency(this).
       onChange(StackTraceModel.Aspect.STACK_FRAMES, () -> {
