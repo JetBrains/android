ccf4218a71b425b45ae1612575f6548fa5c3478c
Joe Baker-Malone <jbakermalone@google.com>
2017-02-15 11:50:31 -0800

    Moving rendering/layout calls to SceneBuilder
    Try 3.
    As part of moving rendering out of NlModel, switching calls to the
    relevant methods to SceneBuilder (which mostly just redirect back to
    NlModel for now).
    
    Test: Just moving logic around, no new tests needed.
    Change-Id: I91d877769bd5dec47b09cfa7129b04fb4434f0e9

--- designer/src/com/android/tools/idea/uibuilder/property/NlPropertiesManager.java
+++ designer/src/com/android/tools/idea/uibuilder/property/NlPropertiesManager.java
@@ -343,10 +343,6 @@ public class NlPropertiesManager implements ToolContent<DesignSurface>, DesignSu
   }
 
   @Override
-  public void modelChanged(@NotNull NlModel model) {
-  }
-
-  @Override
   public void modelRendered(@NotNull NlModel model) {
     myPropertiesPanel.modelRendered(this);
   }
