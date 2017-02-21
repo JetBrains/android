18f1b4ce484b0b098eea2990f7ad1ba4e696a45a
Diego Perez <diegoperez@google.com>
2017-02-21 18:17:17 +0000

    Re-enabling tests broken after the merge
    Test: N/A
    Change-Id: I6492d2ab1184be644d79da4a45b542cdc186086f

--- android/testSrc/com/android/tools/idea/configurations/ResourceResolverCacheTest.java
+++ android/testSrc/com/android/tools/idea/configurations/ResourceResolverCacheTest.java
@@ -32,10 +32,9 @@ import org.jetbrains.android.facet.AndroidFacet;
 import org.jetbrains.android.sdk.FrameworkResourceLoader;
 import org.junit.Ignore;
 
-@Ignore // Broken after 2017.1 merge
 public class ResourceResolverCacheTest extends AndroidTestCase {
 
-  public void ignore_test() throws Exception {
+  public void test() throws Exception {
     VirtualFile file1 = myFixture.copyFileToProject("render/layout1.xml", "res/layout/layout1.xml");
     VirtualFile file2 = myFixture.copyFileToProject("render/layout2.xml", "res/layout/layout2.xml");
     VirtualFile file3 = myFixture.copyFileToProject("javadoc/strings/strings.xml", "res/values/strings.xml");
