77d8a9d27bb3d821939e635130315c03d45a1e4a
Diego Perez <diegoperez@google.com>
2017-02-21 12:42:47 +0000

    Add public-group public.xml parsing
    In O preview, public-group has been added to public.xml and new
    attributes are defined using this new format.
    
    Test: Added test for new functionality
    Change-Id: Id836a73c534330533d5fc06fb9d1d861496f8727

--- android/testSrc/org/jetbrains/android/sdk/AndroidTargetDataTest.java
+++ android/testSrc/org/jetbrains/android/sdk/AndroidTargetDataTest.java
@@ -20,29 +20,76 @@ import com.google.common.base.Charsets;
 import com.google.common.io.Files;
 import com.intellij.util.xml.NanoXmlUtil;
 import gnu.trove.TIntObjectHashMap;
+import net.n3.nanoxml.IXMLBuilder;
 import org.jetbrains.android.AndroidTestCase;
 
-import java.io.BufferedReader;
-import java.io.File;
+import java.io.*;
 
 public class AndroidTargetDataTest extends AndroidTestCase {
 
-  public void testResourceIdMap() throws Exception {
-    final AndroidTargetData.MyPublicResourceCacheBuilder builder = new AndroidTargetData.MyPublicResourceCacheBuilder();
-    File publicXml = TestUtils.getPlatformFile("data/res/values/public.xml");
-
-    BufferedReader reader = null;
+  private static void parseAndClose(InputStream stream, IXMLBuilder builder) throws IOException {
     try {
-      reader = Files.newReader(publicXml, Charsets.UTF_8);
-      NanoXmlUtil.parse(reader, builder);
+      NanoXmlUtil.parse(stream, builder);
     } finally {
-      assert reader != null;
-      reader.close();
+      stream.close();
     }
+  }
+
+  /**
+   * Tests that we correctly can read the platform public.xml
+   */
+  public void testPlatformResourceIdMap() throws Exception {
+    final AndroidTargetData.MyPublicResourceCacheBuilder builder = new AndroidTargetData.MyPublicResourceCacheBuilder();
+
+    parseAndClose(new FileInputStream(TestUtils.getPlatformFile("data/res/values/public.xml")), builder);
 
     TIntObjectHashMap<String> map = builder.getIdMap();
     assertEquals("@android:transition/move", map.get(0x010f0001));
     assertEquals("@android:id/widget_frame", map.get(0x01020018));
+    assertEquals("@android:attr/colorSecondary", map.get(0x01010530));
     assertNull(map.get(0));
+  }
+
+  public void testPublicGroupParsing() throws IOException {
+    final String publicXmlContent = "<?xml version=\"1.0\" encoding=\"utf-8\"?>" +
+                                    "<!-- This file defines the base public resources exported by the\n" +
+                                    "     platform, which must always exist. -->" +
+                                    "" +
+                                    "<resources>" +
+                                    "  <eat-comment />\n" +
+                                    "\n" +
+                                    "  <public type=\"attr\" name=\"theme\" id=\"0x01010000\" />\n" +
+                                    "  <public type=\"attr\" name=\"label\" id=\"0x01010001\" />\n" +
+                                    "  <public type=\"attr\" name=\"manageSpaceActivity\" id=\"0x01010004\" />\n" +
+                                    "\n" +
+                                    "  <public-group type=\"attr\" first-id=\"0x01010531\">" +
+                                    "        <public name=\"fontStyle\" />\n" +
+                                    "        <public name=\"font\" />\n" +
+                                    "        <public name=\"fontWeight\" />\n" +
+                                    "        <public name=\"tooltipText\" />\n" +
+                                    "        <public name=\"autoSizeText\" />\n" +
+                                    "  </public-group>\n" +
+                                    "  <public type=\"drawable\" name=\"btn_minus\" id=\"0x01080007\" />\n" +
+                                    "  <public type=\"drawable\" name=\"btn_plus\" id=\"0x01080008\" />" +
+                                    "  <public type=\"attr\" name=\"titleMargin\" id=\"0x010104f8\" />\n" +
+                                    "  <public type=\"attr\" name=\"titleMarginStart\" id=\"0x010104f9\" />\n" +
+                                    "  <public-group type=\"id\" first-id=\"0x01020041\">\n" +
+                                    "     <public name=\"textAssist\" />\n" +
+                                    "  </public-group>" +
+                                    "</resources>";
+    final AndroidTargetData.MyPublicResourceCacheBuilder builder = new AndroidTargetData.MyPublicResourceCacheBuilder();
+
+    parseAndClose(new ByteArrayInputStream(publicXmlContent.getBytes(Charsets.UTF_8)), builder);
+    TIntObjectHashMap<String> map = builder.getIdMap();
+
+    // Check that we handle correctly elements before and after a public-group
+    assertEquals("@android:attr/theme", map.get(0x01010000));
+    assertEquals("@android:drawable/btn_minus", map.get(0x01080007));
+    assertEquals("@android:attr/titleMargin", map.get(0x010104f8));
+
+    // Check the public group elements
+    assertEquals("@android:attr/fontStyle", map.get(0x01010531));
+    assertEquals("@android:attr/autoSizeText", map.get(0x01010531 + 4));
+    assertEquals("@android:id/textAssist", map.get(0x01020041));
   }
 }
