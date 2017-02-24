193caaca77cf62d14b8d4836328cf4e5ea9aaac1
Vadim Caen <caen@google.com>
2017-02-24 12:30:36 +0000

    Adding DragTarget for LinearLayout
    Test: To add
    Change-Id: Ie2b04d8e6b90a509f8a8acf45d045d45cfbcd775

--- designer/src/com/android/tools/idea/uibuilder/handlers/linear/LinearDragTarget.java
+++ designer/src/com/android/tools/idea/uibuilder/handlers/linear/LinearDragTarget.java
@@ -15,17 +15,99 @@
  */
 package com.android.tools.idea.uibuilder.handlers.linear;
 
+import com.android.tools.idea.uibuilder.api.InsertType;
 import com.android.tools.idea.uibuilder.model.AttributesTransaction;
+import com.android.tools.idea.uibuilder.model.NlComponent;
+import com.android.tools.idea.uibuilder.model.NlModel;
+import com.android.tools.idea.uibuilder.scene.Scene;
+import com.android.tools.idea.uibuilder.scene.SceneComponent;
 import com.android.tools.idea.uibuilder.scene.target.DragBaseTarget;
+import com.android.tools.idea.uibuilder.scene.target.Target;
+import com.google.common.collect.ImmutableList;
+import com.intellij.openapi.command.WriteCommandAction;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * Target to handle the drag of LinearLayout's children
  */
 public class LinearDragTarget extends DragBaseTarget {
 
+
+  private final LinearLayoutHandler myHandler;
+  private LinearSeparatorTarget myClosest;
+
+  public LinearDragTarget(LinearLayoutHandler handler) {
+    myHandler = handler;
+  }
+
   @Override
   protected void updateAttributes(@NotNull AttributesTransaction attributes, int x, int y) {
+    //Do nothing
+  }
 
+  @Override
+  public void mouseDown(int x, int y) {
+    super.mouseDown(x, y);
+    myComponent.setModelUpdateAuthorized(false);
+    SceneComponent parent = myComponent.getParent();
+    assert parent != null;
+    myComponent.getScene().needsLayout(Scene.ANIMATED_LAYOUT);
+    myComponent.getScene().repaint();
+  }
+
+  @Override
+  public void mouseDrag(int x, int y, @Nullable Target closestTarget) {
+    myComponent.setDragging(true);
+    SceneComponent sceneParent = myComponent.getParent();
+    assert sceneParent != null;
+    if (myHandler.isVertical(sceneParent.getNlComponent())) {
+      y -= myOffsetY;
+      myComponent.setPosition(myComponent.getDrawX(), Math.max(y, 0), false);
+    }
+    else {
+      x -= myOffsetX - sceneParent.getDrawX();
+      myComponent.setPosition(Math.max(x, 0), myComponent.getDrawY(), false);
+    }
+
+    // Reset previous closest Target
+    if (myClosest != null) {
+      myClosest.setHighlight(false);
+      myClosest = null;
+    }
+
+    if (closestTarget != null && closestTarget instanceof LinearSeparatorTarget) {
+      myClosest = (LinearSeparatorTarget)closestTarget;
+      myClosest.setHighlight(true);
+    }
+
+    myComponent.getScene().needsLayout(Scene.ANIMATED_LAYOUT);
+    myComponent.getScene().repaint();
+  }
+
+  @Override
+  public void mouseRelease(int x, int y, @Nullable Target closestTarget) {
+    super.mouseRelease(x, y, closestTarget);
+    if (myClosest != null) {
+      SceneComponent sceneParent = myComponent.getParent();
+      if (sceneParent == null) {
+        return;
+      }
+      NlComponent parent = sceneParent.getNlComponent();
+      NlModel model = parent.getModel();
+
+      Runnable swapComponent = () -> {
+        ImmutableList<NlComponent> nlComponentImmutableList = ImmutableList.of(myComponent.getNlComponent());
+        parent.getModel().addComponents(
+          nlComponentImmutableList,
+          parent,
+          !myClosest.isAtEnd() ? myClosest.getComponent().getNlComponent() : null,
+          InsertType.MOVE_WITHIN);
+      };
+      WriteCommandAction.runWriteCommandAction(model.getProject(), "Move component",
+                                               null, swapComponent, model.getFile());
+    }
+    myComponent.getScene().needsLayout(Scene.ANIMATED_LAYOUT);
+    myComponent.setModelUpdateAuthorized(true);
   }
 }
---
+++ designer/src/com/android/tools/idea/uibuilder/handlers/linear/LinearSeparatorTarget.java
@@ -0,0 +1,114 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.uibuilder.handlers.linear;
+
+import com.android.tools.idea.uibuilder.model.NlComponent;
+import com.android.tools.idea.uibuilder.scene.SceneComponent;
+import com.android.tools.idea.uibuilder.scene.SceneContext;
+import com.android.tools.idea.uibuilder.scene.ScenePicker;
+import com.android.tools.idea.uibuilder.scene.draw.DisplayList;
+import com.android.tools.idea.uibuilder.scene.draw.linearlayout.DrawLinearSeparator;
+import com.android.tools.idea.uibuilder.scene.target.BaseTarget;
+import com.intellij.ui.JBColor;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Displays a separator in between LinearLayout's children and used as a target when dropping
+ * a component in LinearLayout
+ */
+public class LinearSeparatorTarget extends BaseTarget {
+
+  private static final boolean DEBUG = false;
+  private final boolean myVertical;
+  private final boolean myAtEnd;
+  private boolean myIsHighlight;
+
+  /**
+   * Create a new separator for linear layout
+   *
+   * @param isVertical is the orientation of the parent LinearLayout
+   * @param atEnd      if true, a separator will be drawn at both ends of the component
+   */
+  public LinearSeparatorTarget(boolean isVertical, boolean atEnd) {
+    super();
+    myVertical = isVertical;
+    myAtEnd = atEnd;
+  }
+
+  @Override
+  public int getPreferenceLevel() {
+    return GUIDELINE_ANCHOR_LEVEL;
+  }
+
+  @Override
+  public boolean layout(@NotNull SceneContext context, int l, int t, int r, int b) {
+    SceneComponent parent = myComponent.getParent();
+    assert parent != null : "This target cannot be added to a root component";
+    NlComponent nlComponent = myComponent.getNlComponent();
+    if (myVertical) {
+      myLeft = parent.getDrawX();
+      myRight = myLeft + parent.getDrawWidth();
+      myTop = myBottom = context.pxToDp(nlComponent.y) + (myAtEnd ? context.pxToDp(nlComponent.h) : 0);
+    }
+    else {
+      myLeft = myRight = context.pxToDp(nlComponent.x) + (myAtEnd ? context.pxToDp(nlComponent.w) : 0 );
+      myTop = parent.getDrawY();
+      myBottom = myTop + parent.getDrawHeight();
+    }
+    return false;
+  }
+
+  @Override
+  public void render(@NotNull DisplayList list, @NotNull SceneContext sceneContext) {
+    if (myComponent.isDragging()) {
+      return;
+    }
+    DrawLinearSeparator.add(list, sceneContext, !myVertical,
+                            myIsHighlight ? DrawLinearSeparator.STATE_HIGHLIGHT : DrawLinearSeparator.STATE_DEFAULT,
+                            myLeft, myTop, myRight, myBottom);
+    if (DEBUG) {
+      drawDebug(list, sceneContext);
+    }
+  }
+
+  @Override
+  public void addHit(@NotNull SceneContext transform, @NotNull ScenePicker picker) {
+    picker.addRect(this, 10,
+                   transform.getSwingX(myLeft), transform.getSwingY(myTop),
+                   transform.getSwingX(myRight) + 1, transform.getSwingY(myBottom) + 1);
+  }
+
+  /**
+   * Draw the debug graphics
+   */
+  private void drawDebug(@NotNull DisplayList list, @NotNull SceneContext sceneContext) {
+    list.addLine(sceneContext, myLeft, myTop, myRight, myBottom, myIsHighlight ? JBColor.GREEN : JBColor.RED);
+    list.addLine(sceneContext, myLeft, myBottom, myRight, myTop, myIsHighlight ? JBColor.GREEN : JBColor.RED);
+  }
+
+  /**
+   * Visually highlight the target
+   *
+   * @param isHighlight true to highlight
+   */
+  public void setHighlight(boolean isHighlight) {
+    myIsHighlight = isHighlight;
+  }
+
+  public boolean isAtEnd() {
+    return myAtEnd;
+  }
+}
