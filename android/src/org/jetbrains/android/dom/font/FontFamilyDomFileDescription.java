8dfbebc2a1082ecb0387419d34559f1bd95afe73
Diego Perez <diegoperez@google.com>
2017-03-17 10:51:10 +0000

    Add font-family completion
    Also adds the font to the create resource dialog.
    
    Bug: 36368567
    Test: Added new completion test
    Change-Id: Iac42df04b9500e4c78dc1d78534e42976a4fca32

---
+++ android/src/org/jetbrains/android/dom/font/FontFamilyDomFileDescription.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jetbrains.android.dom.font;
+
+import com.android.resources.ResourceFolderType;
+import com.android.sdklib.AndroidVersion;
+import com.android.tools.idea.model.AndroidModuleInfo;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.psi.xml.XmlFile;
+import org.jetbrains.android.dom.AbstractSingleRootFileDescription;
+import org.jetbrains.android.facet.AndroidFacet;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Collections;
+import java.util.List;
+
+public class FontFamilyDomFileDescription extends AbstractSingleRootFileDescription<FontFamily> {
+  public static final String TAG_NAME = "font-family";
+
+  public FontFamilyDomFileDescription() {
+    super(FontFamily.class, TAG_NAME, ResourceFolderType.FONT);
+  }
+
+  public static boolean isFontFamilyFile(@NotNull XmlFile file) {
+    return doIsMyFile(file, ResourceFolderType.FONT);
+  }
+
+  @NotNull
+  public static List<String> getPossibleRoots(@NotNull AndroidFacet facet, @NotNull ResourceFolderType folderType) {
+    AndroidVersion sdkVersion = AndroidModuleInfo.getInstance(facet).getBuildSdkVersion();
+
+    if (sdkVersion == null || sdkVersion.getFeatureLevel() >= 26 || ApplicationManager.getApplication().isUnitTestMode()) {
+      return Collections.singletonList(TAG_NAME);
+    }
+
+    return Collections.emptyList();
+  }
+}
