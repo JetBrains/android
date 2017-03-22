0f8b0a2cc78dc7026a890accfb2737a5adc901f9
Amaury Medeiros <amaurym@google.com>
2017-03-22 15:29:56 +0000

    Communicate that profilers are not supported in older APIs
    Instead of not listing devices with older APIs in the dropdown, we
    should display it and update the monitor UI when the user selects it.
    
    In order to change this behavior we a few things needed to be done:
    * Instead of ignoring devices that can't run perfd, make PerfdProxy
      handle devices with API level < 21.
    * Move the logic of determining which message should be displayed in the
      "empty monitor" to NullMonitorStage. NullMonitorStageView should just
      react to the model and it's not its role to have logic to check for
      devices properties.
    * Add a new state to NullMonitorStage to represent the UI for devices
      with unsupported API.
    
    Bug: 36250632
    Test: NullMonitorStageTest
    Change-Id: If6a7c351681c8a1f565dbcf903f6d058ad085c9c

--- profilers/src/com/android/tools/profilers/NullMonitorStage.java
+++ profilers/src/com/android/tools/profilers/NullMonitorStage.java
@@ -15,15 +15,64 @@
  */
 package com.android.tools.profilers;
 
+import com.android.annotations.VisibleForTesting;
+import com.android.tools.adtui.model.AspectModel;
+import com.android.tools.profiler.proto.Profiler;
 import org.jetbrains.annotations.NotNull;
 
 /**
- * This stage only gets set when the profilers first open. It represents the stage of no-data, and no-device.
+ * This stage gets set when the profilers first open, when selecting a device with no debuggable processes,
+ * or when an unsupported device is selected. It represents the stage of no-process, no-device, and unsupported devices.
  */
 public class NullMonitorStage extends Stage {
 
+  @VisibleForTesting
+  static final String ANDROID_PROFILER_TITLE = "Android Profiler";
+  @VisibleForTesting
+  static final String DEVICE_NOT_SUPPORTED_TITLE = "Device not supported";
+
+  @VisibleForTesting
+  static final String NO_DEVICE_MESSAGE = "No device detected. Please plug in a device,<br/>or launch the emulator.";
+  @VisibleForTesting
+  static final String NO_DEBUGGABLE_PROCESS_MESSAGE = "No debuggable processes detected for<br/>the selected device.";
+  @VisibleForTesting
+  static final String DEVICE_NOT_SUPPORTED_MESSAGE = "Android Profiler requires a device with<br/>API 21 (Lollipop) or higher.";
+
+  private Type myType;
+
+  private final AspectModel<Aspect> myAspect = new AspectModel<>();
+
   public NullMonitorStage(@NotNull StudioProfilers profiler) {
     super(profiler);
+    getStudioProfilers().addDependency(this).onChange(ProfilerAspect.DEVICES, this::updateType);
+    updateType();
+  }
+
+  public AspectModel<Aspect> getAspect() {
+    return myAspect;
+  }
+
+  private void updateType() {
+    Profiler.Device device = getStudioProfilers().getDevice();
+    if (device == null) {
+      myType = Type.NO_DEVICE;
+    } else {
+      try {
+        int deviceApi = Integer.valueOf(device.getApi());
+        // Currently, we only support devices with API level 21 or higher
+        if (deviceApi < 21) {
+          myType = Type.UNSUPPORTED_DEVICE;
+        } else {
+          // If device is not null and has API level of 21 or higher,
+          // we only create a NullMonitorStage if it doesn't have debuggable processes.
+          myType = Type.NO_DEBUGGABLE_PROCESS;
+        }
+      } catch (NumberFormatException e) {
+        // API level is unknown. We don't support such device.
+        myType = Type.UNSUPPORTED_DEVICE;
+      }
+    }
+    myAspect.changed(Aspect.NULL_MONITOR_TYPE);
   }
 
   @Override
@@ -33,5 +82,41 @@ public class NullMonitorStage extends Stage {
 
   @Override
   public void exit() {
+  }
+
+  public String getMessage() {
+    switch (myType) {
+      case NO_DEVICE:
+        return NO_DEVICE_MESSAGE;
+      case UNSUPPORTED_DEVICE:
+        return DEVICE_NOT_SUPPORTED_MESSAGE;
+      case NO_DEBUGGABLE_PROCESS:
+      default:
+        return NO_DEBUGGABLE_PROCESS_MESSAGE;
+    }
+  }
+
+  public String getTitle() {
+    switch (myType) {
+      case UNSUPPORTED_DEVICE:
+        return DEVICE_NOT_SUPPORTED_TITLE;
+      case NO_DEVICE:
+      case NO_DEBUGGABLE_PROCESS:
+      default:
+        return ANDROID_PROFILER_TITLE;
+    }
+  }
+
+  enum Type {
+    // No device detected
+    NO_DEVICE,
+    // Selected device has no debuggable processes
+    NO_DEBUGGABLE_PROCESS,
+    // Selected device is unsupported
+    UNSUPPORTED_DEVICE
+  }
+
+  enum Aspect {
+    NULL_MONITOR_TYPE
   }
 }
