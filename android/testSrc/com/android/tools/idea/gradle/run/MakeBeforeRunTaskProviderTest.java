6709dc2f214a92fc764efe04c66de3675f811c5b
Chris Warrington <cmw@google.com>
2017-03-23 13:00:31 +0000

    Instant Run: Use injected api level and code name.
    This allows us to correctly handle preview versions, rather than using
    feature level, which made sense before we started instrumenting against
    the target platform.
    
    Test: TODO.
    Change-Id: I8f1ad8892f8d50d423fa8539cbe274b57f2900b7

--- android/testSrc/com/android/tools/idea/gradle/run/MakeBeforeRunTaskProviderTest.java
+++ android/testSrc/com/android/tools/idea/gradle/run/MakeBeforeRunTaskProviderTest.java
@@ -48,6 +48,10 @@ public class MakeBeforeRunTaskProviderTest {
 
     assertTrue(arguments.contains("-Pandroid.injected.build.api=20"));
     assertTrue(arguments.contains("-Pandroid.injected.build.abi=armeabi,x86"));
+    for (String argument : arguments) {
+      assertFalse("codename should not be set for a released version",
+                  argument.startsWith("-Pandroid.injected.build.codename"));
+    }
   }
 
   @Test
@@ -58,7 +62,8 @@ public class MakeBeforeRunTaskProviderTest {
 
     List<String> arguments = MakeBeforeRunTaskProvider.getDeviceSpecificArguments(Collections.singletonList(myDevice));
 
-    assertTrue(arguments.contains("-Pandroid.injected.build.api=24"));
+    assertTrue(arguments.contains("-Pandroid.injected.build.api=23"));
+    assertTrue(arguments.contains("-Pandroid.injected.build.codename=N"));
   }
 
   @Test
