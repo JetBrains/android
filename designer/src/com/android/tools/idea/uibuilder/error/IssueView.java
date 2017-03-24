93a69761a0a6276b74da2725a589c1ccc2bbc123
Vadim Caen <caen@google.com>
2017-03-24 17:01:32 +0000

    Navigation ComponentTree -> IssuePanel
     - Deleted warning layer since there issues are already shown in the
     component tree
     - When clicking on a badge in the ComponentTree, display the IssuePanel
     with the clicked issue seleced and expanded
     - Sort the issues by severity
     - Added margin to the panel header
     - behavior correction for the opening of the IssuePanel
    
    Test: covered by parent CL
    Change-Id: I1110b9c2aea2f83cf4d3a82d81090abf444ce2cb

--- designer/src/com/android/tools/idea/uibuilder/error/IssueView.java
+++ designer/src/com/android/tools/idea/uibuilder/error/IssueView.java
@@ -21,30 +21,35 @@ import com.android.utils.HtmlBuilder;
 import com.android.utils.Pair;
 import com.intellij.icons.AllIcons;
 import com.intellij.lang.annotation.HighlightSeverity;
-import com.intellij.openapi.actionSystem.ActionManager;
+import com.intellij.ui.IdeBorderFactory;
+import com.intellij.ui.RoundedLineBorder;
 import com.intellij.ui.components.JBLabel;
-import com.intellij.util.FontUtil;
 import com.intellij.util.ui.JBUI;
 import com.intellij.util.ui.UIUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
+import javax.swing.border.Border;
 import javax.swing.text.MutableAttributeSet;
 import javax.swing.text.StyleConstants;
-import javax.swing.text.StyledDocument;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.util.regex.Pattern;
 
 /**
- * Representation of a row in the {@link IssuePanel}
+ * Representation of a {@link NlIssue} in the {@link IssuePanel}
  */
 public class IssueView extends JPanel {
 
   private static final Dimension COLLAPSED_ROW_SIZE = JBUI.size(Integer.MAX_VALUE, 30);
   private static final String SUGGESTED_FIXES = "Suggested Fixes";
+  private static final RoundedLineBorder SELECTED_BORDER = IdeBorderFactory.createRoundedBorder();
+  private static final Border UNSELECTED_BORDER = IdeBorderFactory.createEmptyBorder(SELECTED_BORDER.getThickness());
+
+  static {
+    SELECTED_BORDER.setColor(UIUtil.getTreeSelectionBorderColor());
+  }
 
   private final IssuePanel myContainerIssuePanel;
   private JBLabel myExpandIcon;
@@ -58,26 +63,33 @@ public class IssueView extends JPanel {
   private JPanel myDetailPanel;
   private JBLabel mySuggestedFixLabel;
   private boolean myIsExpanded;
+  private final int myDisplayPriority;
 
-  private IssueView(@NotNull NlIssue error, @NotNull IssuePanel container) {
-    MouseAdapter mouseAdapter = createMouseListener();
+  /**
+   * Construct a new {@link IssueView} representing the provided {@link NlIssue}
+   *
+   * @param issue     The {@link NlIssue} to display
+   * @param container The {@link IssuePanel} where this view will be shown
+   */
+  IssueView(@NotNull NlIssue issue, @NotNull IssuePanel container) {
+    addMouseListener(createMouseListener());
     myContainerIssuePanel = container;
 
-    myErrorIcon.setIcon(getSeverityIcon(error.getSeverity()));
-    myExpandIcon.setIcon(AllIcons.Nodes.TreeRightArrow);
-    myExpandIcon.addMouseListener(mouseAdapter);
+    myDisplayPriority = getDisplayPriority(issue);
 
-    myErrorTitle.setText(error.getSummary());
-    myErrorTitle.addMouseListener(mouseAdapter);
+    myErrorIcon.setIcon(getSeverityIcon(issue.getSeverity()));
+    myExpandIcon.setIcon(UIUtil.getTreeCollapsedIcon());
+
+    myErrorTitle.setText(issue.getSummary());
 
     myErrorDescription.setContentType(UIUtil.HTML_MIME);
-    String description = error.getDescription();
+    String description = issue.getDescription();
     String formattedText = new HtmlBuilder().openHtmlBody().addHtml(description).closeHtmlBody().getHtml();
     myErrorDescription.setText(formattedText);
-    setErrorDescriptionStyle(myErrorDescription);
+    applyIssueDescriptionStyle(myErrorDescription);
 
-    myCategoryLabel.setText(error.getCategory());
-    NlComponent source = error.getSource();
+    myCategoryLabel.setText(issue.getCategory());
+    NlComponent source = issue.getSource();
     if (source != null) {
       String id = source.getId();
       String tag = source.getTagName();
@@ -88,7 +100,7 @@ public class IssueView extends JPanel {
     myContent.setBackground(UIUtil.getEditorPaneBackground());
 
     myFixPanel.setLayout(new BoxLayout(myFixPanel, BoxLayout.Y_AXIS));
-    error.getFixes().forEach(this::createFixEntry);
+    issue.getFixes().forEach(this::createFixEntry);
     if (myFixPanel.getComponentCount() > 0) {
       myFixPanel.setVisible(true);
       mySuggestedFixLabel.setVisible(true);
@@ -96,6 +108,21 @@ public class IssueView extends JPanel {
         mySuggestedFixLabel.setText(SUGGESTED_FIXES);
       }
     }
+  }
+
+  /**
+   * @return A int between 0 and 3 representing how high the issue should be displayed.
+   * 0 is the highest priority and 3 the lower
+   */
+  private static int getDisplayPriority(@NotNull NlIssue error) {
+    if (error.getSeverity().equals(HighlightSeverity.ERROR)) return 0;
+    if (error.getSeverity().equals(HighlightSeverity.WARNING)) return 1;
+    if (error.getSeverity().equals(HighlightSeverity.WEAK_WARNING)) return 2;
+    return 3;
+  }
+
+  int getDisplayPriority() {
+    return myDisplayPriority;
   }
 
   private void createFixEntry(Pair<String, Runnable> pair) {
@@ -107,26 +134,29 @@ public class IssueView extends JPanel {
     return new MouseAdapter() {
       @Override
       public void mouseClicked(MouseEvent e) {
-        if (e.getSource() != myErrorTitle || e.getClickCount() >= 2) {
-          toggleExpand();
+        myContainerIssuePanel.requestFocusInWindow();
+        if (e.getClickCount() == 1) {
+          myContainerIssuePanel.setSelectedIssue(IssueView.this);
+          setFocused(true);
         }
-        if (e.getSource() != myExpandIcon) {
-          setSelected(true);
+        if (e.getX() > myExpandIcon.getX() - 3 && e.getX() < myExpandIcon.getX() + myExpandIcon.getWidth() + 3
+            || e.getClickCount() >= 2) {
+          setExpanded(!myIsExpanded);
         }
       }
     };
   }
 
-  private void toggleExpand() {
-    myIsExpanded = !myIsExpanded;
+  void setExpanded(boolean expanded) {
+    myIsExpanded = expanded;
     myDetailPanel.setVisible(myIsExpanded);
-    myExpandIcon.setIcon(myIsExpanded ? AllIcons.Nodes.TreeDownArrow : AllIcons.Nodes.TreeRightArrow);
+    myExpandIcon.setIcon(myIsExpanded ? UIUtil.getTreeExpandedIcon() : UIUtil.getTreeCollapsedIcon());
   }
 
   /**
    * Returns the icon associated to the passed {@link HighlightSeverity}
    */
-  @Nullable
+  @NotNull
   public static Icon getSeverityIcon(@Nullable HighlightSeverity severity) {
     if (severity != null) {
       if (HighlightSeverity.ERROR.equals(severity)) {
@@ -139,38 +169,45 @@ public class IssueView extends JPanel {
     return AllIcons.General.Information;
   }
 
-  @NotNull
-  public static IssueView createFromNlError(@NotNull NlIssue error, IssuePanel panel) {
-    return new IssueView(error, panel);
-  }
-
-  public void setSelected(boolean selected) {
+  void setSelected(boolean selected) {
     myContent.setOpaque(selected);
     myContent.setBackground(selected ? UIUtil.getPanelBackground() : UIUtil.getEditorPaneBackground());
-    myContainerIssuePanel.setSelectedIssue(this);
+    setFocused(myContainerIssuePanel.hasFocus() && selected);
   }
 
-  public JComponent getComponent() {
-    return myContent;
-  }
-
-  public int getCategoryLabelWidth() {
+  int getCategoryLabelWidth() {
     return myCategoryLabel.getFontMetrics(myCategoryLabel.getFont()).stringWidth(myCategoryLabel.getText());
   }
 
-  public int getSourceLabelWidth() {
+  int getSourceLabelWidth() {
     return mySourceLabel.getFontMetrics(mySourceLabel.getFont()).stringWidth(mySourceLabel.getText());
   }
 
-  public void setSourceLabelSize(int sourceLabelSize) {
+  /**
+   * Set the size of the source {@link JLabel}
+   *
+   * The method is used my the {@link IssuePanel} to ensure that every {@link IssueView}'s category
+   * label has the same size.
+   *
+   * @param sourceLabelSize
+   */
+  void setSourceLabelSize(int sourceLabelSize) {
     Dimension size = mySourceLabel.getPreferredSize();
     size.width = sourceLabelSize;
     mySourceLabel.setPreferredSize(size);
   }
 
-  public void setCategoryLabelSize(int CategoryLabelSize) {
+  /**
+   * Set size of the category {@link JLabel}
+   *
+   * The method is used my the {@link IssuePanel} to ensure that every {@link IssueView}'s category
+   * label has the same size.
+   *
+   * @param categoryLabelSize
+   */
+  void setCategoryLabelSize(int categoryLabelSize) {
     Dimension size = myCategoryLabel.getPreferredSize();
-    size.width = CategoryLabelSize;
+    size.width = categoryLabelSize;
     myCategoryLabel.setPreferredSize(size);
   }
 
@@ -184,11 +221,19 @@ public class IssueView extends JPanel {
     return myErrorTitle.getText();
   }
 
+  /**
+   * Custom creation of the GUI form
+   */
   private void createUIComponents() {
     myContent = this;
   }
 
-  public static void setErrorDescriptionStyle(JTextPane textPane) {
+  /**
+   * Apply the styles to the provided {@link JTextPane}
+   *
+   * @param textPane The {@link JTextPane} to style
+   */
+  private static void applyIssueDescriptionStyle(@NotNull JTextPane textPane) {
     Font font = UIUtil.getTreeFont();
     MutableAttributeSet attrs = textPane.getInputAttributes();
 
@@ -201,6 +246,17 @@ public class IssueView extends JPanel {
       .setCharacterAttributes(0, textPane.getStyledDocument().getLength() + 1, attrs, false);
   }
 
+  /**
+   * Draw a border around this issue to show the user that it is focused.
+   * <p>
+   * This method is intended to show when the {@link IssuePanel} has the keyboard focus but
+   * won't actually give the keyboard focus to this {@link IssueView}
+   *
+   * @param focused
+   */
+  void setFocused(boolean focused) {
+    setBorder(focused ? SELECTED_BORDER : UNSELECTED_BORDER);
+  }
 
   public static class FixEntry {
     private JButton myFixButton;
