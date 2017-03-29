cd31f6d2c35c82d674c5e0308cb2e9bb8f0b386d
hoford <hoford@google.com>
2017-03-29 11:19:54 -0700

    allow live update Test: N/A added call back platform
    Change-Id: I00524b8cefd09f26b83116569c635a62d0314052

--- designer/src/com/android/tools/idea/uibuilder/scene/LayoutlibSceneManager.java
+++ designer/src/com/android/tools/idea/uibuilder/scene/LayoutlibSceneManager.java
@@ -316,6 +316,11 @@ public class LayoutlibSceneManager extends SceneManager {
         myRenderingQueue.cancelAllUpdates();
       }
     }
+
+    @Override
+    public void modelLiveUpdate(@NotNull NlModel model, boolean animate) {
+      layout(animate);
+    }
   }
 
   private class SelectionChangeListener implements SelectionListener {
