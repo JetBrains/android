b35fd9617338a035ce6ebea99d0517a656547f78
Alex Ruiz <alruiz@google.com>
2017-04-03 04:59:23 -0700

    [APK Debugging] Organized tree structure for .so files.
    Now we have a top-level library node that contains as
    children:
    - A new node representing the .so file. Double-clicking
      on this node will open an editor where users can
      configure debug symbols (to be done in the next CL.)
    - A node for each source folder found from the
      library's debug symbols.
    
    Test: JUnit tests.
    Change-Id: I3ff1524ba5c77de51b09223c6eb745210bb2b0b1

---
+++ android/src/com/android/tools/idea/navigator/nodes/apk/ndk/LibraryFileNode.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.navigator.nodes.apk.ndk;
+
+import com.android.tools.idea.apk.debugging.NativeLibrary;
+import com.intellij.ide.projectView.PresentationData;
+import com.intellij.ide.projectView.ViewSettings;
+import com.intellij.ide.util.treeView.AbstractTreeNode;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.Queryable;
+import com.intellij.ui.SimpleTextAttributes;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Collection;
+import java.util.Collections;
+
+import static com.intellij.icons.AllIcons.FileTypes.JavaClass;
+import static com.intellij.ui.JBColor.GRAY;
+import static com.intellij.ui.SimpleTextAttributes.REGULAR_ATTRIBUTES;
+import static com.intellij.ui.SimpleTextAttributes.STYLE_WAVED;
+
+public class LibraryFileNode extends LibraryNode {
+  public LibraryFileNode(@NotNull Project project, @NotNull NativeLibrary library, @NotNull ViewSettings settings) {
+    super(project, library, settings);
+  }
+
+  @Override
+  @NotNull
+  public Collection<? extends AbstractTreeNode> getChildren() {
+    return Collections.emptyList();
+  }
+
+  @Override
+  protected void update(PresentationData presentation) {
+    presentation.setIcon(JavaClass);
+    boolean hasDebugSymbols = myLibrary.hasDebugSymbols;
+    SimpleTextAttributes attributes = hasDebugSymbols ? REGULAR_ATTRIBUTES : new SimpleTextAttributes(STYLE_WAVED, null, GRAY);
+    presentation.addText(myLibrary.name, attributes);
+
+    if (!hasDebugSymbols) {
+      presentation.setTooltip("Library does not have debug symbols");
+    }
+  }
+
+  @Override
+  public int getWeight() {
+    return 10;
+  }
+
+  @Override
+  public int getTypeSortWeight(boolean sortByType) {
+    return 1; // Show library nodes before folders.
+  }
+
+  @Override
+  @Nullable
+  public Comparable getSortKey() {
+    return getTypeSortKey();
+  }
+
+  @Override
+  @Nullable
+  public Comparable getTypeSortKey() {
+    return myLibrary.name;
+  }
+
+  @Override
+  public boolean isAlwaysLeaf() {
+    return true;
+  }
+
+  @Override
+  public boolean isAlwaysShowPlus() {
+    return false;
+  }
+
+  @Nullable
+  @Override
+  public String toTestString(@Nullable Queryable.PrintInfo printInfo) {
+    return myLibrary.name;
+  }
+}
