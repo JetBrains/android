5de7835693753ff50b1093c5b7e6946d74ccf976
Madiyar Aitbayev <madiyar@google.com>
2017-04-03 19:27:15 +0100

    Add time axis guide on top of the cpu threads view.
    Test: Visual test
    Bug: 34624905
    
    Change-Id: I0d120622977f9bfd36e589115d7a48f1bc0f692b

--- profilers-ui/src/com/android/tools/profilers/ProfilerColors.java
+++ profilers-ui/src/com/android/tools/profilers/ProfilerColors.java
@@ -76,6 +76,8 @@ public class ProfilerColors {
 
   public static final Color THREAD_HOVER_OVERLAY = new JBColor(new Color(0x171650C5, true), new Color(0x0CFFFFFF, true));
 
+  public static final Color CPU_AXIS_GUIDE_COLOR = Gray._150.withAlpha(50);
+
   public static final Color THREAD_SELECTED_BACKGROUND = new JBColor(0x3476DC, 0x3476DC);
 
   public static final Color THREAD_LABEL_TEXT = new JBColor(0x545454, 0x9C9C9C);
