a967294f01149320d97489beb610d7707d52fb9a
Alex Ruiz <alruiz@google.com>
2017-04-16 20:46:13 -0700

    Fixed equals/hashCode and serialization of Android model copies (CL 4 of many).
    Given than more than 30 classes are affected, the fix is
    divided in several CLs.
    
    This CL only fixes:
    - IdeAndroidArtifactOutput
    - IdeFilterData
    - IdeJavaCompileOptions
    - IdeOutputFile
    - IdeVariantOutput
    
    Test verify the following:
    1. Each model class is serializable
    2. Model instances can be serialized and deserialized
    3. Copies contain the same information as the originals
    4. Equals/hashCode is correctly implemented (verified with
       EqualsVerifier)
    
    This work is needed to replace the proxy-based model copies and fix
    https://code.google.com/p/android/issues/detail?id=360245
    
    Test: JUnit tests, *plenty* of them.
    
    Change-Id: Ide4a9648d3a57e036c9952659bddfeb434693ca5

--- android/src/com/android/tools/idea/gradle/project/model/ide/android/IdeOutputFile.java
+++ android/src/com/android/tools/idea/gradle/project/model/ide/android/IdeOutputFile.java
@@ -20,15 +20,17 @@ import com.android.build.OutputFile;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashSet;
+import java.util.Objects;
 
 /**
- * Creates a deep copy of {@link OutputFile}.
- *
- * @see IdeAndroidProject
+ * Creates a deep copy of an {@link OutputFile}.
  */
-public class IdeOutputFile extends IdeModel implements OutputFile {
+public final class IdeOutputFile extends IdeModel implements OutputFile {
+  // Increase the value when adding/removing fields or when changing the serialization/deserialization mechanism.
+  private static final long serialVersionUID = 1L;
+
   @NotNull private final String myOutputType;
   @NotNull private final Collection<String> myFilterTypes;
   @NotNull private final Collection<FilterData> myFilters;
@@ -40,10 +42,11 @@ public class IdeOutputFile extends IdeModel implements OutputFile {
   public IdeOutputFile(@NotNull OutputFile file, @NotNull ModelCache modelCache) {
     super(file, modelCache);
     myOutputType = file.getOutputType();
-    myFilterTypes = new HashSet<>(file.getFilterTypes());
+    myFilterTypes = new ArrayList<>(file.getFilterTypes());
     myFilters = copy(file.getFilters(), modelCache, data -> new IdeFilterData(data, modelCache));
     myOutputFile = file.getOutputFile();
     myMainOutputFile = modelCache.computeIfAbsent(file.getMainOutputFile(), outputFile -> new IdeOutputFile(outputFile, modelCache));
+    //noinspection deprecation
     myOutputs = copy(file.getOutputs(), modelCache, outputFile -> new IdeOutputFile(outputFile, modelCache));
     myVersionCode = file.getVersionCode();
   }
@@ -87,5 +90,52 @@ public class IdeOutputFile extends IdeModel implements OutputFile {
   @Override
   public int getVersionCode() {
     return myVersionCode;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof IdeOutputFile)) {
+      return false;
+    }
+
+    IdeOutputFile file = (IdeOutputFile)o;
+    return myVersionCode == file.myVersionCode &&
+           Objects.equals(myOutputType, file.myOutputType) &&
+           Objects.equals(myFilterTypes, file.myFilterTypes) &&
+           Objects.equals(myFilters, file.myFilters) &&
+           Objects.equals(myOutputFile, file.myOutputFile) &&
+           Objects.equals(myOutputs, file.myOutputs)
+           && mainOutputFileEquals(file) ;
+  }
+
+  private boolean mainOutputFileEquals(@NotNull IdeOutputFile file) {
+    // Avoid stack overflow.
+    if (myMainOutputFile == this) {
+      return file.myMainOutputFile == file;
+    }
+    return myMainOutputFile.equals(file.myMainOutputFile);
+  }
+
+  @Override
+  public int hashCode() {
+    // Avoid stack overflow.
+    int mainOutputFileHashCode = myMainOutputFile != this ? Objects.hashCode(myMainOutputFile) : System.identityHashCode(myMainOutputFile);
+    return Objects.hash(myOutputType, myFilterTypes, myFilters, myOutputFile, myOutputs, myVersionCode) + mainOutputFileHashCode;
+  }
+
+  @Override
+  public String toString() {
+    return "IdeOutputFile{" +
+           "myOutputType='" + myOutputType + '\'' +
+           ", myFilterTypes=" + myFilterTypes +
+           ", myFilters=" + myFilters +
+           ", myOutputFile=" + myOutputFile +
+           ", myMainOutputFile=" + (myMainOutputFile != this ? myMainOutputFile : "this") + // Avoid stack overflow.
+           ", myOutputs=" + myOutputs +
+           ", myVersionCode=" + myVersionCode +
+           "}";
   }
 }
