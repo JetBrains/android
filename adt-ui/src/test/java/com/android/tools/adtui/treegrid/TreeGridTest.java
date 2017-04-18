45a4b8fe48b6111dca314fdb9f31020fa2b9d724
Jens Ole Lauridsen <jlauridsen@google.com>
2017-04-18 12:55:37 -0700

    Reactivate tests that were mocking UISettings.
    Test: Reactivate 4 tests.
    
    Change-Id: I1194d3ceb9c497cff1fc8cad51683e1fde6178bb

--- adt-ui/src/test/java/com/android/tools/adtui/treegrid/TreeGridTest.java
+++ adt-ui/src/test/java/com/android/tools/adtui/treegrid/TreeGridTest.java
@@ -24,7 +24,6 @@ import org.intellij.lang.annotations.MagicConstant;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -42,7 +41,6 @@ import static com.google.common.truth.Truth.assertThat;
 import static java.awt.event.KeyEvent.*;
 import static java.awt.image.BufferedImage.TYPE_INT_ARGB;
 
-@Ignore // UISettings can't be mocked yet
 @RunWith(JUnit4.class)
 public class TreeGridTest {
   @Rule
