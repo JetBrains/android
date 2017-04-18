be517fb92c6d4a6f521586e1fbed8f4675f91fe4
Dana Dahlstrom <dahlstrom@google.com>
2017-04-17 17:57:57 -0700

    Revert Change Ifd8aa4ac
    … which caused some tests to fail amid carnage from Change I3447bd19.
    
    Change-Id: I7cebe0a378df3bef65f37cab0a755c9aa1634bed

--- android-uitests/testSrc/com/android/tools/idea/tests/gui/editing/FindInPathTest.java
+++ android-uitests/testSrc/com/android/tools/idea/tests/gui/editing/FindInPathTest.java
@@ -24,6 +24,7 @@ import org.junit.runner.RunWith;
 
 import static com.google.common.truth.Truth.assertThat;
 
+@RunIn(TestGroup.UNRELIABLE)  // failing after 2017.1.2 EAP merge
 @RunWith(GuiTestRunner.class)
 public class FindInPathTest {
 
