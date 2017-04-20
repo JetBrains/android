942020366a81fdaf072f60d9736c62a98266ec0d
Kelvin Ma <kelvinhanma@google.com>
2017-04-19 17:18:31 -0700

    Group inspector properties table
    Current properties table is a dump of all properties we get from the
    framework on a node. It's disorganized and hard to read.
    
    Improve it by grouping them into categories based on the property name
    and reusing PTable element to display properties into expandable
    categories. Inspector properties panel now has a similar same look and feel
    as Layout Editor except we have no use for the "Star" feature or the
    ability to edit the properties.
    
    Those will be added in the future but requires UX (for Starring) or
    workign with framework team to figure out live editing.
    
    Fixes: http://b/37140023
    Test: Updated PropertiesTablePanelTest
    Change-Id: If698a1fefe39cd66789bf1df64c0c69a3caa623b

---
+++ android/src/com/android/tools/idea/editors/layoutInspector/ptable/LITableGroupItem.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.editors.layoutInspector.ptable;
+
+import com.android.layoutinspector.model.ViewProperty;
+import com.android.tools.adtui.ptable.PTableGroupItem;
+import com.android.tools.adtui.ptable.PTableItem;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class LITableGroupItem extends PTableGroupItem {
+  private final String myName;
+  private final List<PTableItem> myChildren;
+
+  private boolean isExpanded;
+
+  public LITableGroupItem(@NotNull String key, @NotNull List<ViewProperty> properties) {
+    super();
+    myName = key;
+    myChildren = properties.stream().map(prop -> new LITableItem(prop, this)).sorted().collect(Collectors.toList());
+    isExpanded = false;
+  }
+
+  @NotNull
+  @Override
+  public String getName() {
+    return myName;
+  }
+
+  @Nullable
+  @Override
+  public String getValue() {
+    return null;
+  }
+
+  @Override
+  public void setValue(@Nullable Object value) {
+  }
+
+  @Override
+  public boolean hasChildren() {
+    return true;
+  }
+
+  @Override
+  public List<PTableItem> getChildren() {
+    return myChildren;
+  }
+
+  @Override
+  @NotNull
+  public String getChildLabel(@NotNull PTableItem item) {
+    return item.getName();
+  }
+
+  @Override
+  public boolean isExpanded() {
+    return isExpanded;
+  }
+
+  @Override
+  public void setExpanded(boolean expanded) {
+    isExpanded = expanded;
+  }
+}
--- android/src/com/android/tools/idea/editors/layoutInspector/ui/PropertiesTablePanel.java
+++ android/src/com/android/tools/idea/editors/layoutInspector/ui/PropertiesTablePanel.java
@@ -16,19 +16,37 @@
 package com.android.tools.idea.editors.layoutInspector.ui;
 
 import com.android.annotations.Nullable;
-import com.android.layoutinspector.model.ViewNodeTableModel;
+import com.android.annotations.VisibleForTesting;
+import com.android.tools.adtui.ptable.PTable;
+import com.android.tools.adtui.ptable.PTableGroupItem;
+import com.android.tools.adtui.ptable.PTableItem;
+import com.android.tools.adtui.ptable.PTableModel;
 import com.android.tools.adtui.workbench.ToolContent;
 import com.android.tools.idea.editors.layoutInspector.LayoutInspectorContext;
-import com.intellij.ui.table.JBTable;
+import com.intellij.ui.Gray;
+import com.intellij.ui.JBColor;
+import com.intellij.ui.SpeedSearchComparator;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.TestOnly;
 
 import javax.swing.*;
 import javax.swing.table.TableRowSorter;
+import java.awt.*;
 
 public class PropertiesTablePanel extends JScrollPane implements ToolContent<LayoutInspectorContext> {
-  private JBTable myTable;
-  private TableRowSorter<ViewNodeTableModel> mySorter;
+  public static final JBColor GROUP_BACKGROUND_COLOR = new JBColor(Gray._242, new Color(60, 63, 65));
+  public static final JBColor ITEM__BACKGROUND_COLOR = new JBColor(Gray._252, new Color(49, 52, 53));
+
+  private PTable myTable;
+
+  private TableRowSorter<PTableModel> myRowSorter;
+  private MyFilter myFilter;
+  private PTableModel myModel;
+
+  public PropertiesTablePanel() {
+    myFilter = new MyFilter();
+    myRowSorter = new TableRowSorter<>();
+  }
 
   @Override
   public void dispose() {
@@ -38,16 +56,15 @@ public class PropertiesTablePanel extends JScrollPane implements ToolContent<Lay
   @Override
   public void setToolContext(@Nullable LayoutInspectorContext toolContext) {
     if (toolContext != null) {
+      myModel = toolContext.getTableModel();
       myTable = toolContext.getPropertiesTable();
-      mySorter = new TableRowSorter<>((ViewNodeTableModel)myTable.getModel());
-      myTable.setRowSorter(mySorter);
       setViewportView(myTable);
     }
   }
 
   @NotNull
   @TestOnly
-  JBTable getTable() {
+  PTable getTable() {
     return myTable;
   }
 
@@ -64,8 +81,53 @@ public class PropertiesTablePanel extends JScrollPane implements ToolContent<Lay
 
   @Override
   public void setFilter(@NotNull String filter) {
-    if (mySorter != null) {
-      mySorter.setRowFilter(RowFilter.regexFilter(filter));
+    int selectedRow = myTable.getSelectedRow();
+    PTableItem selectedItem = myTable.getSelectedItem();
+    if (filter.isEmpty()) {
+      myTable.setRowSorter(null);
+    }
+    else {
+      myFilter.setPattern(filter);
+      myRowSorter.setModel(myModel);
+      myRowSorter.setRowFilter(myFilter);
+      myRowSorter.setSortKeys(null);
+      myTable.setRowSorter(myRowSorter);
+    }
+    myTable.restoreSelection(selectedRow, selectedItem);
+  }
+
+  static class MyFilter extends RowFilter<PTableModel, Integer> {
+    private final SpeedSearchComparator myComparator = new SpeedSearchComparator(false);
+    private String myPattern = "";
+
+    @VisibleForTesting
+    void setPattern(@NotNull String pattern) {
+      myPattern = pattern;
+    }
+
+    @Override
+    public boolean include(Entry<? extends PTableModel, ? extends Integer> entry) {
+      PTableItem item = (PTableItem)entry.getValue(0);
+      if (isMatch(item.getName())) {
+        return true;
+      }
+      if (item.getParent() != null && isMatch(item.getParent().getName())) {
+        return true;
+      }
+      if (!(item instanceof PTableGroupItem)) {
+        return false;
+      }
+      PTableGroupItem group = (PTableGroupItem)item;
+      for (PTableItem child : group.getChildren()) {
+        if (isMatch(child.getName())) {
+          return true;
+        }
+      }
+      return false;
+    }
+
+    private boolean isMatch(@NotNull String text) {
+      return myComparator.matchingFragments(myPattern, text) != null;
     }
   }
 }
