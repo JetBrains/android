c13c486d2177f404d08808bd14164ffb8ab0657f
Joe Kilner <joekilner@google.com>
2017-04-27 09:41:43 +0100

    Re-enable import tests
    This test was using android modules for the import test as
    the project loading infrastructure requires each project to
    have at least one Android module.
    
    However, these Android modules were invalid. Rather than
    adding unnecessary manifests to each module, this CL
    changes the test to use java modules and adds a dummy
    Android module.
    
    Fixes: http://b/35788310 (partial)
    
    Test: re-enables existing tests
    Change-Id: I12616a4fb321782e1843132b8ef3674d4c2a3eb7

--- android/testSrc/com/android/tools/idea/npw/project/AndroidGradleModuleUtilsTest.java
+++ android/testSrc/com/android/tools/idea/npw/project/AndroidGradleModuleUtilsTest.java
@@ -16,9 +16,8 @@
 package com.android.tools.idea.npw.project;
 
 import com.android.tools.idea.testing.AndroidGradleTestCase;
-import com.intellij.openapi.module.ModuleManager;
+import com.intellij.openapi.module.Module;
 import com.intellij.openapi.project.Project;
-import org.junit.Ignore;
 
 import java.io.File;
 
@@ -26,36 +25,28 @@ import static com.android.tools.idea.npw.project.AndroidGradleModuleUtils.getCon
 import static com.android.tools.idea.testing.TestProjectPaths.IMPORTING;
 import static com.intellij.openapi.util.io.FileUtil.join;
 
-@Ignore("http://b/35788310")
 public class AndroidGradleModuleUtilsTest extends AndroidGradleTestCase {
-  public void testFake() {
-  }
 
-  private static final String ARCHIVE_JAR_PATH = join("lib", "library.jar");
-  private static final String NESTED_MODULE_NAME = "sourcemodule";
-  private static final String NESTED_MODULE_ARCHIVE_PATH = join("nested", NESTED_MODULE_NAME, ARCHIVE_JAR_PATH);
-  private static final String SIMPLE_MODULE_NAME = "simple";
-  private static final String SIMPLE_MODULE_ARCHIVE_PATH = join(SIMPLE_MODULE_NAME, ARCHIVE_JAR_PATH);
-
-  public void /*test*/GetContainingModule() throws Exception {
+  public void testGetContainingModule() throws Exception {
     Project project = getProject();
 
     loadProject(IMPORTING);
-    File archiveToImport = new File(project.getBasePath(), SIMPLE_MODULE_ARCHIVE_PATH);
+    File archiveToImport = new File(project.getBasePath(), join("simple", "lib", "library.jar"));
 
-    assertEquals(ModuleManager.getInstance(project).findModuleByName(SIMPLE_MODULE_NAME), getContainingModule(archiveToImport, project));
+    assertEquals(getModule("simple"), getContainingModule(archiveToImport, project));
   }
 
-  public void /*test*/GetContainingModuleNested() throws Exception {
+  public void testGetContainingModuleNested() throws Exception {
     Project project = getProject();
 
     loadProject(IMPORTING);
-    File archiveToImport = new File(project.getBasePath(), NESTED_MODULE_ARCHIVE_PATH);
+    File archiveToImport = new File(project.getBasePath(), join("nested", "sourcemodule", "lib", "library.jar"));
 
-    assertEquals(ModuleManager.getInstance(project).findModuleByName(NESTED_MODULE_NAME), getContainingModule(archiveToImport, project));
+    assertEquals(getModule("sourcemodule"), getContainingModule(archiveToImport, project));
   }
 
-  public void /*test*/GetContainingModuleNotInModule() throws Exception {
-    assertEquals(null, getContainingModule(new File(getTestDataPath(), join(IMPORTING, SIMPLE_MODULE_ARCHIVE_PATH)), getProject()));
+  public void testGetContainingModuleNotInModule() throws Exception {
+    Module module = getContainingModule(new File(getTestDataPath(), join(IMPORTING, "simple", "lib", "library.jar")), getProject());
+    assertEquals(null, module);
   }
 }
