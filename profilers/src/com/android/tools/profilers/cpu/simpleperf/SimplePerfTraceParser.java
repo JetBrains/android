382d29c75b0a312ddb4f2cf683519ef125b42956
Amaury Medeiros <amaurym@google.com>
2017-04-28 19:19:19 +0100

    Add parser for CPU traces obtained from simpleperf
    Simpleperf trace files have the following format:
       * LittleEndian32(record_size_0)
       * SimpleperfReport.Record (having record_size_0 bytes)
       * LittleEndian32(record_size_1)
       * message Record(record_1) (having record_size_1 bytes)
       * ...
       * LittleEndian32(record_size_N)
       * message Record(record_N) (having record_size_N bytes)
       * LittleEndian32(0)
    
    The object specs follow the protobuf API defined in
    //tools/base/profiler/proto/simplerperf_report.proto
    
    Bug: 37294023
    Test: SimplePerfTraceParserTest
    
    Change-Id: Ia1198ab2f649b69018fa15a9f45b0ba8b471fcbc

---
+++ profilers/src/com/android/tools/profilers/cpu/simpleperf/SimplePerfTraceParser.java
@@ -0,0 +1,325 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.profilers.cpu.simpleperf;
+
+import com.android.annotations.VisibleForTesting;
+import com.android.tools.profiler.proto.SimpleperfReport;
+import com.android.tools.profilers.cpu.CaptureNode;
+import com.android.tools.profilers.cpu.MethodModel;
+import com.intellij.openapi.diagnostic.Logger;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.util.*;
+
+/**
+ * Parses a trace file obtained using simpleperf to a map threadId -> {@link CaptureNode}.
+ */
+public class SimplePerfTraceParser {
+
+  /**
+   * When the name of a function (symbol) is not found in the symbol table, the symbol_id field is set to -1.
+   */
+  private static final int INVALID_SYMBOL_ID = -1;
+
+  /**
+   * Trace file to be parsed.
+   */
+  private final File myTraceFile;
+
+  /**
+   * Maps a file id to its correspondent {@link SimpleperfReport.File}.
+   */
+  private final Map<Integer, SimpleperfReport.File> myFiles;
+
+  /**
+   * List of samples containing method trace data.
+   */
+  private final List<SimpleperfReport.Sample> mySamples;
+
+  /**
+   * Maps a thread id to its correspondent method call tree.
+   */
+  private final Map<Integer, CaptureNode> myCallTrees;
+
+  /**
+   * Maps a thread id to its last callchain collected in samples.
+   */
+  private final Map<Integer, List<SimpleperfReport.Sample.CallChainEntry>> myLastCallChain;
+
+  /**
+   * Stores the {@link CaptureNode} on the top of the last call stack corresponding to a thread.
+   * Storing a {@link CaptureNode} for each thread is important, for instance,
+   * to avoid parsing the same call chain multiple times.
+   */
+  private final Map<Integer, CaptureNode> myLastCallStackTopNode;
+
+  /**
+   * Number of samples read from {@link #myTraceFile}.
+   */
+  private long mySampleCount;
+
+  /**
+   * Number of samples lost when recording the trace.
+   */
+  private long myLostSampleCount;
+
+  public SimplePerfTraceParser(File traceFile) {
+    if (!traceFile.exists()) {
+      throw new IllegalArgumentException(
+        "Trace file " + traceFile.getAbsolutePath() + " does not exist.");
+    }
+    myTraceFile = traceFile;
+    myFiles = new HashMap<>();
+    mySamples = new ArrayList<>();
+    myCallTrees = new HashMap<>();
+    myLastCallChain = new HashMap<>();
+    myLastCallStackTopNode = new HashMap<>();
+  }
+
+  /**
+   * Given Unix-like path string (e.g. /system/my-path/file.so), returns the file name (e.g. file.so).
+   */
+  private static String fileNameFromPath(String path) {
+    String[] splitPath = path.split("/");
+    return splitPath[splitPath.length - 1];
+  }
+
+  private static boolean equals(SimpleperfReport.Sample.CallChainEntry c1, SimpleperfReport.Sample.CallChainEntry c2) {
+    return c1.getVaddrInFile() == c2.getVaddrInFile() && c1.getFileId() == c2.getFileId() && c1.getSymbolId() == c2.getSymbolId();
+  }
+
+  private static Logger getLog() {
+    return Logger.getInstance(SimplePerfTraceParser.class);
+  }
+
+  private static ByteBuffer byteBufferFromFile(File f, ByteOrder byteOrder) throws IOException {
+    try (FileInputStream dataFile = new FileInputStream(f)) {
+      MappedByteBuffer buffer = dataFile.getChannel().map(FileChannel.MapMode.READ_ONLY, 0, f.length());
+      buffer.order(byteOrder);
+      return buffer;
+    }
+  }
+
+  public void parse() throws IOException {
+    parseTraceFile();
+    parseSampleData();
+  }
+
+  public Map<Integer, CaptureNode> getCallTrees() {
+    return myCallTrees;
+  }
+
+  public long getLostSampleCount() {
+    return myLostSampleCount;
+  }
+
+  public long getSampleCount() {
+    return mySampleCount;
+  }
+
+  @NotNull
+  private static CaptureNode createCaptureNode(String name, long timestamp) {
+    CaptureNode node = new CaptureNode();
+    node.setMethodModel(new MethodModel(name));
+    node.setStartGlobal(timestamp);
+    // TODO: use thread time
+    node.setStartThread(timestamp);
+    node.setDepth(0);
+    return node;
+  }
+
+  /**
+   * Parses the trace file, which should have the following format:
+   * LittleEndian32(record_size_0)
+   * SimpleperfReport.Record (having record_size_0 bytes)
+   * LittleEndian32(record_size_1)
+   * message Record(record_1) (having record_size_1 bytes)
+   * ...
+   * LittleEndian32(record_size_N)
+   * message Record(record_N) (having record_size_N bytes)
+   * LittleEndian32(0)
+   *
+   * Parsed data is stored in {@link #myFiles} and {@link #mySamples}.
+   */
+  @VisibleForTesting
+  void parseTraceFile() throws IOException {
+    ByteBuffer buffer = byteBufferFromFile(myTraceFile, ByteOrder.LITTLE_ENDIAN);
+    // Read the first record size
+    int recordSize = buffer.getInt();
+
+    // 0 is used to indicate the end of the trace
+    while (recordSize != 0) {
+      // The next recordSize bytes should represent the record
+      byte[] recordBytes = new byte[recordSize];
+      buffer.get(recordBytes);
+      SimpleperfReport.Record record = SimpleperfReport.Record.parseFrom(recordBytes);
+
+      switch (record.getRecordDataCase()) {
+        case FILE:
+          SimpleperfReport.File file = record.getFile();
+          myFiles.put(file.getId(), file);
+          break;
+        case LOST:
+          // Only one occurrence of LOST type is expected.
+          SimpleperfReport.LostSituation situation = record.getLost();
+          mySampleCount = situation.getSampleCount();
+          myLostSampleCount = situation.getLostCount();
+          break;
+        case SAMPLE:
+          SimpleperfReport.Sample sample = record.getSample();
+          mySamples.add(sample);
+          break;
+        default:
+          getLog().warn("Unexpected record data type " + record.getRecordDataCase());
+      }
+
+      // read the next record size
+      recordSize = buffer.getInt();
+    }
+
+    if (mySamples.size() != mySampleCount) {
+      // TODO: create a trace file to test this exception is thrown when it should.
+      throw new IllegalStateException("Samples count doesn't match the number of samples read.");
+    }
+  }
+
+  /**
+   * Parses the data from {@link #mySamples} into a map of tid -> {@link CaptureNode}.
+   */
+  private void parseSampleData() {
+    if (mySamples.isEmpty()) {
+      return;
+    }
+
+    // Process each sample
+    for (SimpleperfReport.Sample sample : mySamples) {
+      parseCallChain(sample.getCallchainList(), sample.getThreadId(), sample.getTime());
+    }
+
+    // Update the end timestamp of the last active call chain of each thread
+    long endTimestamp = mySamples.get(mySamples.size() - 1).getTime();
+    for (CaptureNode lastNode : myLastCallStackTopNode.values()) {
+      CaptureNode node = lastNode;
+      while (node != null && node.getEnd() == 0) {
+        node.setEndGlobal(endTimestamp);
+        // TODO: use thread time
+        node.setEndThread(endTimestamp);
+        node = (CaptureNode)node.getParent();
+      }
+    }
+  }
+
+  /**
+   * Given a {@link SimpleperfReport.Sample.CallChainEntry}, a thread id and a sample timestamp, update the {@link CaptureNode}
+   * corresponding to that thread with the information obtained from the call chain.
+   */
+  private void parseCallChain(List<SimpleperfReport.Sample.CallChainEntry> callChain, int threadId, long timestamp) {
+    if (!myCallTrees.containsKey(threadId)) {
+      // if there is no entry for threadId in the map, create one with the main node as value.
+      CaptureNode main = createCaptureNode("main", timestamp);
+      main.setDepth(0);
+      myCallTrees.put(threadId, main);
+      myLastCallStackTopNode.put(threadId, main);
+      // Initiate the previous call chain as an empty list
+      myLastCallChain.put(threadId, new LinkedList<>());
+    }
+
+    List<SimpleperfReport.Sample.CallChainEntry> previousCallChain = myLastCallChain.get(threadId);
+    // First, identify where the call chains diverge, so we update the endTime of the nodes that are not in the call chain anymore.
+    // If the last call chain is empty, there is no divergent index and no end values need to be updated.
+    int divergentIndex = 0;
+    CaptureNode divergentNodeParent = null;
+    if (!previousCallChain.isEmpty()) {
+      while (divergentIndex < previousCallChain.size() && divergentIndex < callChain.size() &&
+             equals(previousCallChain.get(divergentIndex), callChain.get(divergentIndex))) {
+        divergentIndex++;
+      }
+      divergentNodeParent = findDivergenceAndUpdateEndTime(previousCallChain.size() - divergentIndex, threadId, timestamp);
+    }
+
+    // Now, add the nodes of the new call chain to the tree
+    if (callChain.size() > divergentIndex) {
+      divergentNodeParent = divergentNodeParent == null ? myLastCallStackTopNode.get(threadId) : divergentNodeParent;
+      addNewNodes(callChain, divergentNodeParent, divergentIndex, timestamp, threadId);
+    }
+
+    // Finally, update previous call chain and previous last node
+    myLastCallChain.put(threadId, callChain);
+  }
+
+  /**
+   * Update the end timestamp of the last call chain node corresponding to a thread.
+   * Then, go backwards and do the same to the ancestors of the node until the newly read call chain
+   * matches with the previous one. When a divergence is found, return the parent of the divergent node.
+   */
+  private CaptureNode findDivergenceAndUpdateEndTime(int divergenceCount, int tid, long endTimestamp) {
+    CaptureNode node = myLastCallStackTopNode.get(tid);
+    for (int i = 0; i < divergenceCount; i++) {
+      assert node != null;
+      node.setEndGlobal(endTimestamp);
+      // TODO: use thread time
+      node.setEndThread(endTimestamp);
+      node = (CaptureNode)node.getParent();
+    }
+
+    // Node should be the parent of the first divergent node
+    return node;
+  }
+
+  /**
+   * Given a list of call chain entries and a start index, convert them to {@link CaptureNode}
+   * and add them to the call tree corresponding to the thread id passed as argument, as descendants
+   * of a given node.
+   */
+  private void addNewNodes(List<SimpleperfReport.Sample.CallChainEntry> callChain,
+                           CaptureNode node, int startIndex, long startTimestamp, int tid) {
+    assert node != null;
+    for (int i = startIndex; i < callChain.size(); i++) {
+      CaptureNode child = createCaptureNode(parseMethodName(callChain.get(i)), startTimestamp);
+      node.addChild(child);
+      child.setDepth(node.getDepth() + 1);
+      node = child;
+    }
+    // Update the pointer to the last call chain node
+    myLastCallStackTopNode.put(tid, node);
+  }
+
+  private String parseMethodName(SimpleperfReport.Sample.CallChainEntry callChainEntry) {
+    int symbolId = callChainEntry.getSymbolId();
+    SimpleperfReport.File symbolFile = myFiles.get(callChainEntry.getFileId());
+    if (symbolFile == null) {
+      throw new IllegalStateException("Symbol file with id \"" + callChainEntry.getFileId() + "\" not found.");
+    }
+    String methodName;
+    if (symbolId == INVALID_SYMBOL_ID) {
+      // if symbol_id is -1, we report the method as fileName+vAddress (e.g. program.so+0x3039)
+      String hexAddress = "0x" + Long.toHexString(callChainEntry.getVaddrInFile());
+      methodName = fileNameFromPath(symbolFile.getPath()) + "+" + hexAddress;
+    }
+    else {
+      // otherwise, read the method name from the symbol table
+      methodName = symbolFile.getSymbol(symbolId);
+    }
+    return methodName;
+  }
+}
---
+++ profilers/testSrc/com/android/tools/profilers/cpu/CpuProfilerTestUtils.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.profilers.cpu;
+
+import com.android.testutils.TestUtils;
+import com.google.protobuf3jarjar.ByteString;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+/**
+ * Common constants and methods used across CPU profiler tests.
+ * Should not be instantiated.
+ */
+public class CpuProfilerTestUtils {
+
+  private static final String CPU_TRACES_DIR = "tools/adt/idea/profilers/testData/cputraces/";
+
+  private CpuProfilerTestUtils() {}
+
+  public static ByteString traceFileToByteString(@NotNull String filename) throws IOException {
+    File traceFile = TestUtils.getWorkspaceFile(CPU_TRACES_DIR + filename);
+    return ByteString.copyFrom(Files.readAllBytes(traceFile.toPath()));
+  }
+}
