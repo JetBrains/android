b61cf79edde6f9e3e01db7c0cb8eb8af32cfb127
Amaury Medeiros <amaurym@google.com>
2017-04-28 19:19:19 +0100

    Add parser for CPU traces obtained from simpleperf
    Simpleperf trace files have the following format:
       * LittleEndian32(record_size_0)
       * SimpleperfReport.Record (having record_size_0 bytes)
       * LittleEndian32(record_size_1)
       * message Record(record_1) (having record_size_1 bytes)
       * ...
       * LittleEndian32(record_size_N)
       * message Record(record_N) (having record_size_N bytes)
       * LittleEndian32(0)
    
    The object specs follow the protobuf API defined in
    //tools/base/profiler/proto/simplerperf_report.proto
    
    Bug: 37294023
    Test: SimplePerfTraceParserTest
    
    Change-Id: Ia1198ab2f649b69018fa15a9f45b0ba8b471fcbc

---
+++ profilers/testSrc/com/android/tools/profilers/cpu/simpleperf/SimplePerfTraceParserTest.java
@@ -0,0 +1,107 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.profilers.cpu.simpleperf;
+
+import com.android.tools.adtui.model.HNode;
+import com.android.tools.profilers.cpu.CaptureNode;
+import com.android.tools.profilers.cpu.MethodModel;
+import com.google.protobuf3jarjar.ByteString;
+import com.intellij.openapi.util.io.FileUtil;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Map;
+
+import static com.android.tools.profilers.cpu.CpuProfilerTestUtils.traceFileToByteString;
+import static org.junit.Assert.*;
+
+public class SimplePerfTraceParserTest {
+
+  private SimplePerfTraceParser myParser;
+
+  @Before
+  public void setUp() throws IOException {
+    ByteString traceBytes = traceFileToByteString("simpleperf.trace");
+    File trace = FileUtil.createTempFile("cpu_trace", ".trace");
+    try (FileOutputStream out = new FileOutputStream(trace)) {
+      out.write(traceBytes.toByteArray());
+    }
+    myParser = new SimplePerfTraceParser(trace);
+  }
+
+  @Test
+  public void samplesAndLostCountShouldMatchSimpleperfReport() throws IOException {
+    myParser.parseTraceFile();
+    assertEquals(40841, myParser.getSampleCount());
+    assertEquals(644, myParser.getLostSampleCount());
+  }
+
+  @Test
+  public void allTreesShouldStartWithMain() throws IOException {
+    myParser.parse();
+    Map<Integer, CaptureNode> callTrees = myParser.getCallTrees();
+    for (CaptureNode tree : callTrees.values()) {
+      assertNotNull(tree.getData());
+      assertEquals("main", tree.getData().getName());
+    }
+  }
+
+  @Test
+  public void nodeDepthsShouldBeCoherent() throws IOException {
+    myParser.parse();
+    CaptureNode anyTree = myParser.getCallTrees().values().iterator().next();
+    assertEquals(0, anyTree.getDepth());
+
+    // Just go as deep as possible in one branch per child and check the depths of each node in the branch
+    for (HNode<MethodModel> child : anyTree.getChildren()) {
+      int depth = 1;
+      HNode<MethodModel> node = child;
+      while (node != null) {
+        assertEquals(depth++, node.getDepth());
+        node = node.getFirstChild();
+      }
+    }
+  }
+
+  @Test
+  public void mainProcessShouldBePresent() throws IOException {
+    myParser.parse();
+    CaptureNode mainThread = myParser.getCallTrees().get(16246 /* App pid */);
+    assertNotNull(mainThread);
+  }
+
+  @Test
+  public void fileIdsShouldBeMappedToAnExistingFile() throws IOException {
+    ByteString traceBytes = traceFileToByteString("simpleperf_missing_file.trace");
+    File trace = FileUtil.createTempFile("cpu_trace", ".trace");
+    try (FileOutputStream out = new FileOutputStream(trace)) {
+      out.write(traceBytes.toByteArray());
+    }
+    myParser = new SimplePerfTraceParser(trace);
+
+    try {
+      myParser.parse();
+      fail("IllegalStateException should have been thrown due to missing file.");
+    } catch (IllegalStateException e) {
+      assertTrue(e.getMessage().contains("Symbol file with id"));
+      assertTrue(e.getMessage().contains("not found"));
+      // Do nothing. Expected exception.
+    }
+  }
+}
