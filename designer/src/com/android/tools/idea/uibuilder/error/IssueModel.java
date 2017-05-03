a706b350b046b773a8f5bea914d7f1da5da69b08
Dana Dahlstrom <dahlstrom@google.com>
2017-05-03 12:35:07 -0700

    Add @GuardedBy annotations in IssueModel
    … and return an unmodifiable list instead of the mutable field value, to
    avoid concurrent modifications that have been occurring.
    
    Also replace Iterable.forEach calls with (enhanced) for loops to satisfy
    http://errorprone.info/bugpattern/GuardedByChecker.
    
    Change-Id: I359ca4abf608b390a105759406bfc9d2dca6bde3

--- designer/src/com/android/tools/idea/uibuilder/error/IssueModel.java
+++ designer/src/com/android/tools/idea/uibuilder/error/IssueModel.java
@@ -22,6 +22,7 @@ import com.intellij.lang.annotation.HighlightSeverity;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import javax.annotation.concurrent.GuardedBy;
 import java.util.*;
 
 /**
@@ -31,34 +32,35 @@ public class IssueModel {
 
   @Nullable private RenderErrorModel myRenderErrorModel;
   @Nullable private LintAnnotationsModel myLintAnnotationsModel;
-  private final List<NlIssue> myIssues = new ArrayList<>();
+  @GuardedBy("itself") private final List<NlIssue> myIssues = new ArrayList<>();
   private final List<IssueModelListener> myListeners = new ArrayList<>(1);
   private int myWarningCount;
   private int myErrorCount;
 
+  @GuardedBy("myIssues")
   public void setRenderErrorModel(@NotNull RenderErrorModel renderErrorModel) {
     myRenderErrorModel = renderErrorModel;
     updateErrorsList();
   }
 
+  @GuardedBy("myIssues")
   private void updateErrorsList() {
     myIssues.clear();
     myWarningCount = 0;
     myErrorCount = 0;
     if (myRenderErrorModel != null) {
-      myRenderErrorModel.getIssues().forEach(error -> {
+      for (RenderErrorModel.Issue error : myRenderErrorModel.getIssues()) {
         NlIssue issue = NlIssue.wrapIssue(error);
         myIssues.add(issue);
         updateIssuesCounts(issue);
-      });
+      }
     }
     if (myLintAnnotationsModel != null) {
-      myLintAnnotationsModel.getIssues().forEach(error -> {
-
+      for (LintAnnotationsModel.IssueData error : myLintAnnotationsModel.getIssues()) {
         NlIssue issue = NlIssue.wrapIssue(error);
         myIssues.add(issue);
         updateIssuesCounts(issue);
-      });
+      }
     }
 
     for (IssueModelListener myListener : myListeners) {
@@ -75,20 +77,23 @@ public class IssueModel {
     }
   }
 
+  @GuardedBy("myIssues")
   public void setLintAnnotationsModel(@NotNull LintAnnotationsModel lintAnnotationsModel) {
     myLintAnnotationsModel = lintAnnotationsModel;
     updateErrorsList();
   }
 
+  @GuardedBy("myIssues")
   @NotNull
   public List<NlIssue> getNlErrors() {
-    return myIssues;
+    return Collections.unmodifiableList(myIssues);
   }
 
   public boolean hasRenderError() {
     return myRenderErrorModel != null && !myRenderErrorModel.getIssues().isEmpty();
   }
 
+  @GuardedBy("myIssues")
   public int getIssueCount() {
     return myIssues.size();
   }
@@ -109,10 +114,12 @@ public class IssueModel {
     return myErrorCount;
   }
 
+  @GuardedBy("myIssues")
   public boolean hasIssues() {
     return !myIssues.isEmpty();
   }
 
+  @GuardedBy("myIssues")
   @Nullable
   public NlIssue findIssue(@NotNull NlComponent component) {
     for (NlIssue issue : myIssues) {
