163c80d9e487eff95811f09af25b7adcde0b9141
Diego Perez <diegoperez@google.com>
2017-05-09 17:20:01 +0100

    Move sample data handling down to SampleDataResourceItem
    It makes for a better distribution of responsabilities. Also, this will
    allow in a future CL to support other data sources that are not part of
    the project.
    
    Test: Existing tests pass
    Change-Id: I6d2db57cf4ef9b3d15bdb0fb3f067ccc6e2f408f

--- android/src/com/android/tools/idea/res/SampleDataResourceRepository.java
+++ android/src/com/android/tools/idea/res/SampleDataResourceRepository.java
@@ -19,7 +19,6 @@ import com.android.annotations.NonNull;
 import com.android.annotations.Nullable;
 import com.android.ide.common.res2.ResourceItem;
 import com.android.ide.common.res2.ResourceTable;
-import com.android.ide.common.resources.sampledata.SampleDataJsonParser;
 import com.android.resources.ResourceType;
 import com.google.common.collect.ImmutableListMultimap;
 import com.google.common.collect.ImmutableSet;
@@ -34,8 +33,6 @@ import org.jetbrains.android.facet.AndroidFacet;
 import org.jetbrains.android.facet.AndroidRootUtil;
 import org.jetbrains.annotations.NotNull;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collections;
@@ -43,7 +40,6 @@ import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Stream;
 
-import static com.android.SdkConstants.EXT_JSON;
 import static com.android.SdkConstants.FD_SAMPLE_DATA;
 
 
@@ -93,29 +89,11 @@ public class SampleDataResourceRepository extends LocalResourceRepository {
   }
 
   private static void addItems(@NotNull ImmutableListMultimap.Builder<String, ResourceItem> items, @NotNull PsiFileSystemItem sampleDataFile) {
-    if (!EXT_JSON.equals(sampleDataFile.getVirtualFile().getExtension())) {
-      // Plain file or directory
-      items.put(sampleDataFile.getName(),
-                new SampleDataResourceItem(sampleDataFile.getName(), sampleDataFile.getVirtualFile(), sampleDataFile));
-      return;
-    }
-
     try {
-      SampleDataJsonParser parser = SampleDataJsonParser.parse(new FileReader(VfsUtilCore.virtualToIoFile(sampleDataFile.getVirtualFile())));
-      if (parser == null) {
-        // Failed to parse the JSON file
-        return;
-      }
-
-      Set<String> possiblePaths = parser.getPossiblePaths();
-      for (String path : possiblePaths) {
-        String fullName = sampleDataFile.getName() + path;
-        items.put(fullName,
-                  new SampleDataResourceItem(fullName, sampleDataFile.getVirtualFile(), sampleDataFile));
-      }
+      SampleDataResourceItem.getFromPsiFileSystemItem(sampleDataFile).forEach(item -> items.put(item.getName(), item));
     }
-    catch (FileNotFoundException e) {
-      LOG.error("File not found " + sampleDataFile.getName(), e);
+    catch (IOException e) {
+      LOG.warn("Error loading sample data file " + sampleDataFile.getName(), e);
     }
   }
 
