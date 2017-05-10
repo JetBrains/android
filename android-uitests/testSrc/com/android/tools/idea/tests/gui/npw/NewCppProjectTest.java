6b8e39c5fd2fe3021a5935fee40777ff164a2349
Dana Dahlstrom <dahlstrom@google.com>
2017-05-09 19:01:38 -0700

    Move tests to TestGroup.UNRELIABLE
    … that time out waiting for Gradle sync after the new-project wizard.
    
    Bug: 37965951
    Change-Id: I6e0ca895690a430c4b6b1558ac3aa11a4180f77d

--- android-uitests/testSrc/com/android/tools/idea/tests/gui/npw/NewCppProjectTest.java
+++ android-uitests/testSrc/com/android/tools/idea/tests/gui/npw/NewCppProjectTest.java
@@ -157,7 +157,7 @@ public class NewCppProjectTest {
    * This checks that our (default) project templates are warnings-clean.
    * The test then proceeds to make a couple of edits and checks that these do not generate additional warnings either.
    */
-  @RunIn(TestGroup.PROJECT_WIZARD)
+  @RunIn(TestGroup.UNRELIABLE /* TestGroup.PROJECT_WIZARD */)  // timeout waiting for Gradle sync: b/37965951
   @Test
   public void noWarningsInNewProjectWithCpp() {
     createCppProject(false, false);
