d279f07ecb37ccbc896de9f612e9c632b28f308c
Jens Ole Lauridsen <jlauridsen@google.com>
2017-05-15 20:42:25 -0700

    Add placeholders for adding an attribute in XML property editor.
    Test: NA
    Change-Id: I32ac16998934792244c40b8e6c26eff76ca77fa9

--- designer/src/com/android/tools/idea/uibuilder/property/renderer/NlXmlNameRenderer.java
+++ designer/src/com/android/tools/idea/uibuilder/property/renderer/NlXmlNameRenderer.java
@@ -17,6 +17,7 @@ package com.android.tools.idea.uibuilder.property.renderer;
 
 import com.android.tools.adtui.ptable.PNameRenderer;
 import com.android.tools.adtui.ptable.PTableItem;
+import com.android.tools.idea.uibuilder.property.AddPropertyItem;
 import com.android.tools.idea.uibuilder.property.NlResourceHeader;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.ui.*;
@@ -33,6 +34,7 @@ public class NlXmlNameRenderer extends ColoredTableCellRenderer implements PName
   public static final JBColor NAMESPACE_COLOR = new JBColor(new Color(128, 0, 128), new Color(151, 118, 169));
   public static final JBColor ATTRIBUTE_COLOR = new JBColor(new Color(0, 0, 255), Gray._192);
   public static final JBColor TAG_COLOR = new JBColor(new Color(0, 0, 128), new Color(180, 180, 0));
+  public static final JBColor NEW_VALUE_COLOR = new JBColor(Gray._192, Gray._128);
 
   @Override
   protected void customizeCellRenderer(JTable table, @Nullable Object value, boolean selected, boolean hasFocus, int row, int column) {
@@ -41,6 +43,7 @@ public class NlXmlNameRenderer extends ColoredTableCellRenderer implements PName
       return;
     }
     Color color = ATTRIBUTE_COLOR;
+    String name = item.getName();
     if (item instanceof NlResourceHeader) {
       color = TAG_COLOR;
       if (!selected) {
@@ -50,13 +53,19 @@ public class NlXmlNameRenderer extends ColoredTableCellRenderer implements PName
           IdeBorderFactory.createBorder(SideBorder.BOTTOM),
           BorderFactory.createEmptyBorder(6, 0, 0, 0)));
     }
+    else if (item instanceof AddPropertyItem && name.isEmpty()) {
+      name = "+ name";
+      if (!selected) {
+        color = NEW_VALUE_COLOR;
+      }
+    }
     SimpleTextAttributes attr = REGULAR_ATTRIBUTES.derive(STYLE_SMALLER | STYLE_BOLD, color, null, null);
     if (!StringUtil.isEmpty(item.getNamespace())) {
       SimpleTextAttributes attrNamespace = attr.derive(-1, NAMESPACE_COLOR, null, null);
       append(getNamespaceLabel(item.getNamespace()), attrNamespace, false);
       append(":", attr, false);
     }
-    append(item.getName(), attr, true);
+    append(name, attr, true);
   }
 
   @Override
