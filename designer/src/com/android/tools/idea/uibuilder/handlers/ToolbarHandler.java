71a1cd1ebcc38e92038ea70415ffbedde33770ee
Wojtek Kaliciński <wkal@google.com>
2017-05-31 18:07:36 +0100

    Move XmlBuilder to tools/base/common
    This is part of an APK Analyzer refactor to move
    common code to tools/base.
    
    Test: moved
    Change-Id: Ie97de669544998f344a15f8a4f4216aff04c0065

--- designer/src/com/android/tools/idea/uibuilder/handlers/ToolbarHandler.java
+++ designer/src/com/android/tools/idea/uibuilder/handlers/ToolbarHandler.java
@@ -16,7 +16,7 @@
 package com.android.tools.idea.uibuilder.handlers;
 
 import com.android.tools.idea.uibuilder.api.ViewHandler;
-import com.android.tools.idea.XmlBuilder;
+import com.android.xml.XmlBuilder;
 import com.android.tools.idea.uibuilder.api.XmlType;
 import com.google.common.collect.ImmutableList;
 import org.intellij.lang.annotations.Language;
