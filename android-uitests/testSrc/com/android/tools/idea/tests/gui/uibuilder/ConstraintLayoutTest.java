3b0b53a3b25e543073c2ab5a2491d0e8adff9703
Xin Chen <xinchan@google.com>
2017-06-01 11:02:40 -0700

    Move ConstraintLayoutTest to unreliable
    b/37150579
    
    Test: N/A
    Change-Id: I2f66a60f8a48ec934ced9e06b53d09e32b672943

--- android-uitests/testSrc/com/android/tools/idea/tests/gui/uibuilder/ConstraintLayoutTest.java
+++ android-uitests/testSrc/com/android/tools/idea/tests/gui/uibuilder/ConstraintLayoutTest.java
@@ -119,7 +119,7 @@ public class ConstraintLayoutTest {
    *      for "layout_constraintBaseline_toBaselineOf"
    *   </pre>
    */
-  @RunIn(TestGroup.QA)
+  @RunIn(TestGroup.QA_UNRELIABLE)
   @Test
   public void testBaselineConstraintHandling() throws Exception {
     EditorFixture editor = guiTest.importProjectAndWaitForProjectSyncToFinish("LayoutTest")
