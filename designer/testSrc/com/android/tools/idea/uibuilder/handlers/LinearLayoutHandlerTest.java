eeb4669da1781f0ff6316b7039196d7d8d5c42a2
Diego Perez <diegoperez@google.com>
2017-06-06 17:43:18 -0700

    Handle DelegatingViewHandler in LinerarLayout actions
    Checks if the Handler is a DelegatingViewHandler and call
    the underlying LinerarLayoutHandler if it is the case.
    
    Bug: http://b/62391854
    Test: Added a test to ensure the LinearLayout's ViewAction can handle a
    DelegatingViewHandler
    
    Change-Id: I3393258ef7cf3aa1b346e3c48cbbadd00b8989c3

--- designer/testSrc/com/android/tools/idea/uibuilder/handlers/LinearLayoutHandlerTest.java
+++ designer/testSrc/com/android/tools/idea/uibuilder/handlers/LinearLayoutHandlerTest.java
@@ -17,9 +17,19 @@ package com.android.tools.idea.uibuilder.handlers;
 
 import com.android.tools.idea.uibuilder.LayoutTestCase;
 import com.android.tools.idea.uibuilder.SyncNlModel;
+import com.android.tools.idea.uibuilder.api.ViewEditor;
+import com.android.tools.idea.uibuilder.api.actions.DirectViewAction;
+import com.android.tools.idea.uibuilder.api.actions.ViewAction;
 import com.android.tools.idea.uibuilder.fixtures.ModelBuilder;
+import com.android.tools.idea.uibuilder.handlers.linear.LinearLayoutHandler;
+import com.android.tools.idea.uibuilder.model.NlComponent;
 import com.android.tools.idea.uibuilder.util.NlTreeDumper;
+import com.google.common.collect.ImmutableList;
+import com.intellij.testFramework.exceptionCases.EmptyStackExceptionCase;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.EmptyStackException;
 
 import static com.android.SdkConstants.*;
 import static com.android.tools.idea.uibuilder.model.SegmentType.*;
@@ -86,23 +96,23 @@ public class LinearLayoutHandlerTest extends LayoutTestCase {
   @NotNull
   private SyncNlModel createModel() {
     ModelBuilder builder = model("linear.xml",
-                     component(LINEAR_LAYOUT)
-                       .withBounds(0, 0, 1000, 1000)
-                       .matchParentWidth()
-                       .matchParentHeight()
-                       .children(
-                         component(TEXT_VIEW)
-                           .withBounds(100, 100, 100, 100)
-                           .id("@id/myText1")
-                           .width("100dp")
-                           .height("100dp"),
-                         component(BUTTON)
-                           .withBounds(100, 200, 100, 100)
-                           .id("@id/myText2")
-                           .width("100dp")
-                           .height("100dp")
-                           .withAttribute("android:layout_weight", "1.0")
-                     ));
+                                 component(LINEAR_LAYOUT)
+                                   .withBounds(0, 0, 1000, 1000)
+                                   .matchParentWidth()
+                                   .matchParentHeight()
+                                   .children(
+                                     component(TEXT_VIEW)
+                                       .withBounds(100, 100, 100, 100)
+                                       .id("@id/myText1")
+                                       .width("100dp")
+                                       .height("100dp"),
+                                     component(BUTTON)
+                                       .withBounds(100, 200, 100, 100)
+                                       .id("@id/myText2")
+                                       .width("100dp")
+                                       .height("100dp")
+                                       .withAttribute("android:layout_weight", "1.0")
+                                   ));
     final SyncNlModel model = builder.build();
     assertEquals(1, model.getComponents().size());
     assertEquals("NlComponent{tag=<LinearLayout>, bounds=[0,0:1000x1000}\n" +
@@ -111,5 +121,44 @@ public class LinearLayoutHandlerTest extends LayoutTestCase {
                  NlTreeDumper.dumpTree(model.getComponents()));
     format(model.getFile());
     return model;
+  }
+
+  /**
+   * Check if the LinearLayoutHandler's actions can handle a DelegatingViewHandler which delegate to a LinearLayout.
+   *
+   * This is a check for b/37946463
+   */
+  public void testDelegatedHandler() {
+    ModelBuilder builder = model("linear.xml",
+                                 component(LINEAR_LAYOUT)
+                                   .id("@id/root")
+                                   .withBounds(0, 0, 1000, 1000)
+                                   .matchParentWidth()
+                                   .matchParentHeight()
+                                   .children(
+                                     component(TEXT_VIEW)
+                                       .withBounds(100, 100, 100, 100)
+                                       .id("@id/myText1")
+                                       .width("100dp")
+                                       .height("100dp")
+                                   ));
+
+    SyncNlModel model = builder.build();
+    LinearLayoutHandler handler = new LinearLayoutHandler();
+    ArrayList<ViewAction> actions = new ArrayList<>();
+    handler.addToolbarActions(actions);
+    ViewEditor editor = editor(screen(model).getScreen());
+    DelegatingViewGroupHandler delegatingViewGroupHandler = new DelegatingViewGroupHandler(handler);
+    NlComponent component = model.find("root");
+    assertNotNull(component);
+    assertNoException(new EmptyStackExceptionCase() {
+      @Override
+      public void tryClosure() throws EmptyStackException {
+        actions.stream()
+          .filter(action -> action instanceof DirectViewAction)
+          .map(action -> (DirectViewAction)action)
+          .forEach(action -> action.perform(editor, delegatingViewGroupHandler, component, ImmutableList.of(), 0));
+      }
+    });
   }
 }
