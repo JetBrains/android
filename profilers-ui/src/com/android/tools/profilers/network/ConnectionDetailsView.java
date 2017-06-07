cd2e6fa87f7ef73138ac8a4d8af5dee5c8e7c037
    <davidherman@google.com>
2017-06-07 11:47:27 -0700

    Fix Network Details View refresh issue
    When new HttpData is selected, the contents of the connection
    details panel should refresh themselves. However, when new
    contents were not as tall as the old contents, Swing didn't know to
    clear out the old areas, so we have to tell it to do so explicilty,
    will a top-level repaint.
    
    Test: N/A, visual change only
    Bug: 37507526
    Change-Id: I3966a836a8ca04740530d8b5dc0aa2aa82fbd623

--- profilers-ui/src/com/android/tools/profilers/network/ConnectionDetailsView.java
+++ profilers-ui/src/com/android/tools/profilers/network/ConnectionDetailsView.java
@@ -183,7 +183,7 @@ public class ConnectionDetailsView extends JPanel {
       myStackTraceView.getModel().clearStackFrames();
     }
     setVisible(httpData != null);
-    revalidate();
+    repaint();
   }
 
   private static JComponent createFields(@NotNull HttpData httpData, @Nullable Dimension payloadDimension) {
