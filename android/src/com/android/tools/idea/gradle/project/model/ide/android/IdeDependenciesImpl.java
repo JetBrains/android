85ba869c9d06da2ee9ae4e550f4b32198dd89161
Fabrice de Gans-Riberi <fdegans@google.com>
2017-06-08 17:11:49 +0000

    Reland "[AIA] Remove references to atom related stuff""
    This relands commit 91409258819655769d2c06b2e975d0b67d1f4f3e.
    Reason for reland: fixed build failure
    
    Test: Existing
    Change-Id: I71e6fae168982a1a350791689693c9451ef8bbce

--- android/src/com/android/tools/idea/gradle/project/model/ide/android/IdeDependenciesImpl.java
+++ android/src/com/android/tools/idea/gradle/project/model/ide/android/IdeDependenciesImpl.java
@@ -15,7 +15,6 @@
  */
 package com.android.tools.idea.gradle.project.model.ide.android;
 
-import com.android.builder.model.AndroidAtom;
 import com.android.builder.model.AndroidLibrary;
 import com.android.builder.model.Dependencies;
 import com.android.builder.model.JavaLibrary;
@@ -25,7 +24,6 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.Collection;
-import java.util.Collections;
 import java.util.Objects;
 import java.util.function.Consumer;
 
@@ -36,46 +34,20 @@ public final class IdeDependenciesImpl extends IdeModel implements IdeDependenci
   // Increase the value when adding/removing fields or when changing the serialization/deserialization mechanism.
   private static final long serialVersionUID = 1L;
 
-  @NotNull private final Collection<AndroidAtom> myAtoms;
   @NotNull private final Collection<AndroidLibrary> myLibraries;
   @NotNull private final Collection<JavaLibrary> myJavaLibraries;
   @NotNull private final Collection<String> myProjects;
-  @Nullable private final IdeAndroidAtom myBaseAtom;
   private final int myHashCode;
 
   public IdeDependenciesImpl(@NotNull Dependencies dependencies, @NotNull ModelCache modelCache, @Nullable GradleVersion modelVersion) {
     super(dependencies, modelCache);
-    boolean atLeastTwoDotThree = modelVersion != null && modelVersion.isAtLeast(2, 3, 0);
-    if (atLeastTwoDotThree) {
-      myAtoms = copy(dependencies.getAtoms(), modelCache, atom -> new IdeAndroidAtom(atom, modelCache));
-    }
-    else {
-      myAtoms = Collections.emptyList();
-    }
 
     myLibraries = copy(dependencies.getLibraries(), modelCache, library -> new IdeAndroidLibrary(library, modelCache));
     myJavaLibraries = copy(dependencies.getJavaLibraries(), modelCache, library -> new IdeJavaLibrary(library, modelCache));
 
     myProjects = ImmutableList.copyOf(dependencies.getProjects());
 
-    AndroidAtom baseAtom = atLeastTwoDotThree ? dependencies.getBaseAtom() : null;
-    myBaseAtom = copyAtom(modelCache, baseAtom);
-
     myHashCode = calculateHashCode();
-  }
-
-  @Nullable
-  private static IdeAndroidAtom copyAtom(@NotNull ModelCache modelCache, @Nullable AndroidAtom atom) {
-    if (atom != null) {
-      return modelCache.computeIfAbsent(atom, library -> new IdeAndroidAtom(atom, modelCache));
-    }
-    return null;
-  }
-
-  @Override
-  @NotNull
-  public Collection<AndroidAtom> getAtoms() {
-    return myAtoms;
   }
 
   @Override
@@ -94,12 +66,6 @@ public final class IdeDependenciesImpl extends IdeModel implements IdeDependenci
   @NotNull
   public Collection<String> getProjects() {
     return myProjects;
-  }
-
-  @Override
-  @Nullable
-  public IdeAndroidAtom getBaseAtom() {
-    return myBaseAtom;
   }
 
   @Override
@@ -125,11 +91,9 @@ public final class IdeDependenciesImpl extends IdeModel implements IdeDependenci
       return false;
     }
     IdeDependenciesImpl that = (IdeDependenciesImpl)o;
-    return Objects.equals(myAtoms, that.myAtoms) &&
-           Objects.equals(myLibraries, that.myLibraries) &&
+    return Objects.equals(myLibraries, that.myLibraries) &&
            Objects.equals(myJavaLibraries, that.myJavaLibraries) &&
-           Objects.equals(myProjects, that.myProjects) &&
-           Objects.equals(myBaseAtom, that.myBaseAtom);
+           Objects.equals(myProjects, that.myProjects);
   }
 
   @Override
@@ -138,17 +102,15 @@ public final class IdeDependenciesImpl extends IdeModel implements IdeDependenci
   }
 
   private int calculateHashCode() {
-    return Objects.hash(myAtoms, myLibraries, myJavaLibraries, myProjects, myBaseAtom);
+    return Objects.hash(myLibraries, myJavaLibraries, myProjects);
   }
 
   @Override
   public String toString() {
     return "IdeDependencies{" +
-           "myAtoms=" + myAtoms +
-           ", myLibraries=" + myLibraries +
+           "myLibraries=" + myLibraries +
            ", myJavaLibraries=" + myJavaLibraries +
            ", myProjects=" + myProjects +
-           ", myBaseAtom=" + myBaseAtom +
            '}';
   }
 }
