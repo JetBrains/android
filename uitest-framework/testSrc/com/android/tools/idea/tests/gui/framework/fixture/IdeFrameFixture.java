67c6df6565935d11be34985c46978f841128648d
Jingjing Liu <jingjingliu@google.com>
2017-06-13 16:03:38 -0700

    Clean up fixture and test
    1) Remove identical methond and replace some codes with wrapped functions in IdeFrameFixture
    2) Remove a "Wait" inside a "Wait".
    3) In test, only check "stop" button on "RunnerToolbar" dialog.
    
    Test: ran testDeviceChooser and testWatchpoint, passed
    Change-Id: I8ed9a1284ed1917e3a8faaf6684878186cc41f35

--- uitest-framework/testSrc/com/android/tools/idea/tests/gui/framework/fixture/IdeFrameFixture.java
+++ uitest-framework/testSrc/com/android/tools/idea/tests/gui/framework/fixture/IdeFrameFixture.java
@@ -222,24 +222,14 @@ public class IdeFrameFixture extends ComponentFixture<IdeFrameFixture, IdeFrameI
 
   public DeployTargetPickerDialogFixture debugApp(@NotNull String appName) throws ClassNotFoundException {
     selectApp(appName);
-    findActionButtonByActionId("Debug").click();
+    findDebugApplicationButton().click();
     return DeployTargetPickerDialogFixture.find(robot());
   }
 
   public DeployTargetPickerDialogFixture runApp(@NotNull String appName) throws ClassNotFoundException {
     selectApp(appName);
-    getRunButton().click();
+    findRunApplicationButton().click();
     return DeployTargetPickerDialogFixture.find(robot());
-  }
-
-  @NotNull
-  public ActionButtonFixture getRunButton() {
-    return findActionButtonByActionId("Run");
-  }
-
-  @NotNull
-  public ActionButtonFixture getStopButton() {
-    return findActionButtonByActionId("Stop");
   }
 
   @NotNull
