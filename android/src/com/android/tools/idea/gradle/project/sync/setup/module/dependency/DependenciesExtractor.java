9c1f5bb5f40427f06054562cebab0321a18bd9e8
Ying Chen <chying@google.com>
2017-06-19 11:40:32 -0700

    Revert "Revert Change I9beb4d3b"
    Library.getArtifact is now equivelent to getBundle in old api.
    Add javadoc.
    
    This reverts commit a2a2ab71fb223b05abe86fc88973f2f2d320634f.
    Test: GradleSyncTest.aarSourceAttachments passes.
    
    Change-Id: I8de1363d25f20aaa29a9d7fcc8692db01e995ea3

--- android/src/com/android/tools/idea/gradle/project/sync/setup/module/dependency/DependenciesExtractor.java
+++ android/src/com/android/tools/idea/gradle/project/sync/setup/module/dependency/DependenciesExtractor.java
@@ -15,62 +15,76 @@
  */
 package com.android.tools.idea.gradle.project.sync.setup.module.dependency;
 
-import com.android.builder.model.*;
+import com.android.builder.model.level2.Library;
+import com.android.ide.common.repository.GradleCoordinate;
+import com.android.tools.idea.gradle.project.model.ide.android.IdeAndroidArtifact;
+import com.android.tools.idea.gradle.project.model.ide.android.IdeBaseArtifact;
+import com.android.tools.idea.gradle.project.model.ide.android.IdeLevel2Dependencies;
 import com.android.tools.idea.gradle.project.model.ide.android.IdeVariant;
-import com.google.common.collect.Sets;
 import com.intellij.openapi.components.ServiceManager;
 import com.intellij.openapi.roots.DependencyScope;
 import org.jetbrains.annotations.NotNull;
 
-import java.io.File;
-import java.util.Collection;
-import java.util.Set;
-
 import static com.android.tools.idea.gradle.project.sync.setup.module.dependency.LibraryDependency.PathType.BINARY;
 import static com.intellij.openapi.roots.DependencyScope.COMPILE;
 import static com.intellij.openapi.roots.DependencyScope.TEST;
-import static com.intellij.openapi.util.io.FileUtil.getNameWithoutExtension;
-import static com.intellij.openapi.util.text.StringUtil.isNotEmpty;
 
+/**
+ * Creates {@link DependencySet} from variant or artifact.
+ */
 public class DependenciesExtractor {
   @NotNull
   public static DependenciesExtractor getInstance() {
     return ServiceManager.getService(DependenciesExtractor.class);
   }
 
+  /**
+   * Get a {@link DependencySet} contains merged dependencies from main artifact and test artifacts.
+   *
+   * @param variant the variant to extract dependencies from.
+   * @return Instance of {@link DependencySet} retrieved from given variant.
+   */
   @NotNull
   public DependencySet extractFrom(@NotNull IdeVariant variant) {
     DependencySet dependencies = new DependencySet();
 
-    for (BaseArtifact testArtifact : variant.getTestArtifacts()) {
+    for (IdeBaseArtifact testArtifact : variant.getTestArtifacts()) {
       populate(dependencies, testArtifact, TEST);
     }
 
-    AndroidArtifact mainArtifact = variant.getMainArtifact();
+    IdeAndroidArtifact mainArtifact = variant.getMainArtifact();
     populate(dependencies, mainArtifact, COMPILE);
 
     return dependencies;
   }
 
+  /**
+   * @param artifact the artifact to extract dependencies from.
+   * @param scope    Scope of the dependencies, e.g. "compile" or "test".
+   * @return Instance of {@link DependencySet} retrieved from given artifact.
+   */
   @NotNull
-  public DependencySet extractFrom(@NotNull BaseArtifact artifact, @NotNull DependencyScope scope) {
+  public DependencySet extractFrom(@NotNull IdeBaseArtifact artifact, @NotNull DependencyScope scope) {
     DependencySet dependencies = new DependencySet();
     populate(dependencies, artifact, scope);
     return dependencies;
   }
 
   private static void populate(@NotNull DependencySet dependencies,
-                               @NotNull BaseArtifact artifact,
+                               @NotNull IdeBaseArtifact artifact,
                                @NotNull DependencyScope scope) {
-    Dependencies artifactDependencies = artifact.getDependencies();
-    addJavaLibraries(dependencies, artifactDependencies.getJavaLibraries(), scope);
+    IdeLevel2Dependencies artifactDependencies = artifact.getLevel2Dependencies();
 
-    Set<File> unique = Sets.newHashSet();
-    for (AndroidLibrary library : artifactDependencies.getLibraries()) {
-      addAndroidLibrary(library, dependencies, scope, unique);
+    for (Library library : artifactDependencies.getJavaLibraries()) {
+      dependencies.add(new LibraryDependency(library.getArtifact(), scope));
     }
 
-    for (String gradleProjectPath : artifactDependencies.getProjects()) {
+    for (Library library : artifactDependencies.getAndroidLibraries()) {
+      dependencies.add(createLibraryDependencyFromAndroidLibrary(library, scope));
+    }
+
+    for (Library library : artifactDependencies.getModuleDependencies()) {
+      String gradleProjectPath = library.getProjectPath();
       if (gradleProjectPath != null && !gradleProjectPath.isEmpty()) {
         ModuleDependency dependency = new ModuleDependency(gradleProjectPath, scope);
         dependencies.add(dependency);
@@ -79,91 +93,23 @@ public class DependenciesExtractor {
   }
 
   @NotNull
-  private static String getBundleName(@NotNull AndroidBundle bundle) {
-    MavenCoordinates coordinates = bundle.getResolvedCoordinates();
-    //noinspection ConstantConditions
-    if (coordinates != null) {
-      return coordinates.getArtifactId() + "-" + coordinates.getVersion();
-    }
-    File bundleFile = bundle.getBundle();
-    return getNameWithoutExtension(bundleFile);
-  }
-
-  private static boolean isAlreadySeen(@NotNull AndroidBundle bundle, @NotNull Set<File> unique) {
-    // We're using the library location as a unique handle rather than the AndroidLibrary instance itself, in case
-    // the model just blindly manufactures library instances as it's following dependencies
-    File folder = bundle.getFolder();
-    if (unique.contains(folder)) {
-      return true;
-    }
-    unique.add(folder);
-    return false;
-  }
-
-  /**
-   * Add an Android library, along with any recursive library dependencies
-   */
-  private static void addAndroidLibrary(@NotNull AndroidLibrary library,
-                                        @NotNull DependencySet dependencies,
-                                        @NotNull DependencyScope scope,
-                                        @NotNull Set<File> unique) {
-    if (isAlreadySeen(library, unique)) {
-      return;
-    }
-
-    String gradleProjectPath = library.getProject();
-    if (isNotEmpty(gradleProjectPath)) {
-      ModuleDependency dependency = new ModuleDependency(gradleProjectPath, scope);
-      // Add the aar as dependency in case there is a module dependency that cannot be satisfied (e.g. the module is outside of the
-      // project.) If we cannot set the module dependency, we set a library dependency instead.
-      dependency.setBackupDependency(createLibraryDependencyFromAndroidLibrary(library, scope));
-      dependencies.add(dependency);
-    }
-    else {
-      dependencies.add(createLibraryDependencyFromAndroidLibrary(library, scope));
-    }
-
-    addBundleTransitiveDependencies(library, dependencies, scope, unique);
-  }
-
-  private static void addBundleTransitiveDependencies(@NotNull AndroidBundle bundle,
-                                                      @NotNull DependencySet dependencies,
-                                                      @NotNull DependencyScope scope,
-                                                      @NotNull Set<File> unique) {
-    for (AndroidLibrary dependentLibrary : bundle.getLibraryDependencies()) {
-      addAndroidLibrary(dependentLibrary, dependencies, scope, unique);
-    }
-  }
-
-  @NotNull
-  private static LibraryDependency createLibraryDependencyFromAndroidLibrary(@NotNull AndroidLibrary library,
+  private static LibraryDependency createLibraryDependencyFromAndroidLibrary(@NotNull Library library,
                                                                              @NotNull DependencyScope scope) {
-    LibraryDependency dependency = new LibraryDependency(library.getBundle(), getBundleName(library), scope);
+    LibraryDependency dependency =
+      new LibraryDependency(library.getArtifact(), getDependencyName(library.getArtifactAddress()), scope);
     dependency.addPath(BINARY, library.getJarFile());
     dependency.addPath(BINARY, library.getResFolder());
 
-    for (File localJar : library.getLocalJars()) {
+    for (String localJar : library.getLocalJars()) {
       dependency.addPath(BINARY, localJar);
     }
     return dependency;
   }
 
-  private static void addJavaLibraries(@NotNull DependencySet dependencies,
-                                       @NotNull Collection<? extends JavaLibrary> libraries,
-                                       @NotNull DependencyScope scope) {
-    for (JavaLibrary library : libraries) {
-      addJavaLibrary(library, dependencies, scope);
-    }
-  }
-
-  private static void addJavaLibrary(@NotNull JavaLibrary library, @NotNull DependencySet dependencies, @NotNull DependencyScope scope) {
-    dependencies.add(createLibraryDependencyFromJavaLibrary(library, scope));
-    addJavaLibraries(dependencies, library.getDependencies(), scope);
-  }
-
   @NotNull
-  private static LibraryDependency createLibraryDependencyFromJavaLibrary(@NotNull JavaLibrary library, @NotNull DependencyScope scope) {
-    File jarFilePath = library.getJarFile();
-    return new LibraryDependency(jarFilePath, scope);
+  private static String getDependencyName(@NotNull String artifactAddress) {
+    GradleCoordinate coordinates = GradleCoordinate.parseCoordinateString(artifactAddress);
+    assert coordinates != null : String.format("'%1$s' is not a valid maven coordinate.", artifactAddress);
+    return coordinates.getArtifactId() + "-" + coordinates.getVersion();
   }
 }
