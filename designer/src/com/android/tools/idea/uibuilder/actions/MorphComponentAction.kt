0320305f4705c00193a156d7511845816539dd78
Vadim Caen <caen@google.com>
2017-06-20 14:13:13 +0100

    Add the option to remove arguments when morphing
    Test: Not testing yet since the whole class needs to be refactored in a
    more modularized way to be correctly testable and this would result in a
    too big CL
    Change-Id: I9afeac5f28712f7bdac9fd8ecc80094eaa51a042

--- designer/src/com/android/tools/idea/uibuilder/actions/MorphComponentAction.kt
+++ designer/src/com/android/tools/idea/uibuilder/actions/MorphComponentAction.kt
@@ -17,6 +17,7 @@ package com.android.tools.idea.uibuilder.actions
 
 import com.android.tools.idea.uibuilder.model.NlComponent
 import com.android.tools.idea.uibuilder.surface.DesignSurface
+import com.intellij.icons.AllIcons
 import com.intellij.ide.highlighter.XmlFileType
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
@@ -29,11 +30,7 @@ import com.intellij.openapi.editor.colors.EditorColorsManager
 import com.intellij.openapi.editor.ex.EditorEx
 import com.intellij.openapi.editor.ex.RangeHighlighterEx
 import com.intellij.openapi.editor.ex.util.LexerEditorHighlighter
-import com.intellij.openapi.editor.impl.EditorFactoryImpl
-import com.intellij.openapi.editor.markup.EffectType
-import com.intellij.openapi.editor.markup.HighlighterLayer
-import com.intellij.openapi.editor.markup.HighlighterTargetArea
-import com.intellij.openapi.editor.markup.TextAttributes
+import com.intellij.openapi.editor.markup.*
 import com.intellij.openapi.fileTypes.SyntaxHighlighterFactory
 import com.intellij.openapi.ui.popup.JBPopup
 import com.intellij.openapi.ui.popup.JBPopupFactory
@@ -41,6 +38,7 @@ import com.intellij.openapi.util.TextRange
 import com.intellij.openapi.wm.IdeFocusManager
 import org.jetbrains.android.facet.AndroidFacet
 import java.awt.Font
+import javax.swing.Icon
 
 /**
  * Action that shows a dialog to change the tag name of a component
@@ -51,10 +49,12 @@ class MorphComponentAction(component: NlComponent, designSurface: DesignSurface)
   private val myFacet: AndroidFacet = component.model.facet
   private val mySurface = designSurface
   private val myProject = component.model.project
-  private val myComponent = component
-  private val myRangesToText = mutableListOf<Pair<TextRange, String>>()
-  private var myTagNameRange: RangeHighlighterEx? = null
-  private var myNewName = ""
+  private val myNlComponent = component
+  private val myAttributes = mutableListOf<AttributeProperties>()
+  private lateinit var myTagNameTextRange: TextRange
+  private lateinit var myTagNameRange: RangeHighlighterEx
+  private var myNewName = component.tagName
+  private val myEditor = createEditor()
 
   init {
     templatePresentation.isEnabled = true
@@ -62,101 +62,101 @@ class MorphComponentAction(component: NlComponent, designSurface: DesignSurface)
   }
 
   /**
-   * Creates a new document containing a String representation of the provided component
-   * @see buildRangesAndGetString
-   */
-  private fun createDocument(nlComponent: NlComponent): Document {
-    val factory = EditorFactory.getInstance()
-    val stringRepresentation = buildRangesAndGetString(nlComponent, myRangesToText)
-    return (factory as EditorFactoryImpl).createDocument(stringRepresentation, true, false)
-  }
-
-  /**
-   *
    * Build a map from [TextRange] to the corresponding sub-[String] in the returned [String] representation of
    * [nlComponent]
    *
-   * [rangesMap] will be cleared before parsing the component.
-   * The first value of [rangesMap] corresponds to the tag name.
+   * [attributeProperties] will be cleared before parsing the component.
+   * The first value of [attributeProperties] corresponds to the tag name.
    *
    * For now, other values are useless, but they will be used to allow the user to delete some argument before
    * morphing the component
    */
-  private fun buildRangesAndGetString(nlComponent: NlComponent, rangesMap: MutableList<Pair<TextRange, String>>): String {
-    var stringRepresentation = "<"
+  private fun buildRangesAndGetString(nlComponent: NlComponent, attributeProperties: MutableList<AttributeProperties>): String {
+    var xmlString = "<"
     val indent = "    "
     val tag = nlComponent.tag
 
-    rangesMap.clear()
-    rangesMap.add(createTextRange(stringRepresentation.length, nlComponent.tagName))
-    stringRepresentation += nlComponent.tagName
+    attributeProperties.clear()
+    myTagNameTextRange = TextRange(xmlString.length, xmlString.length + nlComponent.tagName.length)
+    xmlString += nlComponent.tagName
 
-    tag.attributes.forEach { xmlAttribute ->
-      rangesMap.add(createTextRange(stringRepresentation.length, xmlAttribute.text))
-      stringRepresentation += "\n$indent" + xmlAttribute.text
+    tag.attributes.forEach { nlAttribute ->
+      xmlString += "\n$indent"
+      attributeProperties.add(AttributeProperties(xmlString.length, nlAttribute.name, nlAttribute.textLength))
+      xmlString += nlAttribute.text
     }
 
-    stringRepresentation += ">\n$indent..."
-    return stringRepresentation
+    xmlString += ">\n$indent..."
+    return xmlString
   }
-
-  /**
-   * Create a new [Pair] mapping a [TextRange] to the corresponding [text].
-   * The [TextRange] is set from [offset] to `offset + text.length + 1`
-   */
-  private fun createTextRange(offset: Int, text: String) = TextRange(offset, offset + text.length) to text
 
   /**
    * Create a read-only editor displaying the content of the provided component.
    *
    * The editor highlight the content as XML
    */
-  private fun createEditor(document: Document): EditorEx {
+  private fun createEditor(): EditorEx {
+    val factory = EditorFactory.getInstance()
+    val document = factory.createDocument(buildRangesAndGetString(myNlComponent, myAttributes))
     val editor = EditorFactory.getInstance().createEditor(document, myProject, XmlFileType.INSTANCE, false) as EditorEx
+
     val syntaxHighlighter = SyntaxHighlighterFactory.getSyntaxHighlighter(XmlFileType.INSTANCE, myProject, mySurface.model?.file?.virtualFile)
     syntaxHighlighter?.let {
       editor.highlighter = LexerEditorHighlighter(it, EditorColorsManager.getInstance().globalScheme)
     }
 
-    myTagNameRange = createTagNameMarker(editor)
+    myTagNameRange = addMarkerForTagName(editor)
+    myAttributes.forEach {
+      it.addMarker(editor)
+    }
+
     with(editor) {
       setCaretEnabled(false)
       isViewer = true
       settings.isLineNumbersShown = false
       settings.additionalLinesCount = 1
     }
+
     return editor
   }
 
   /**
    * Create the marker shown around the tag name
    */
-  private fun createTagNameMarker(editor: EditorEx): RangeHighlighterEx? {
-    if (myRangesToText.isEmpty()) {
-      return null
-    }
-    val start = myRangesToText[0].first.startOffset
-    val end = myRangesToText[0].first.endOffset
+  private fun addMarkerForTagName(editor: EditorEx): RangeHighlighterEx {
+    val start = myTagNameTextRange.startOffset
+    val end = myTagNameTextRange.endOffset
     val attributes = TextAttributes(null, null, editor.colorsScheme.getColor(EditorColors.MODIFIED_LINES_COLOR), EffectType.BOXED, Font.BOLD)
     return editor.markupModel.addRangeHighlighterAndChangeAttributes(
         start, end,
         HighlighterLayer.SELECTION, attributes,
-        HighlighterTargetArea.EXACT_RANGE, false, {
-      it.isGreedyToLeft = true
-      it.isGreedyToRight = true
-    })
+        HighlighterTargetArea.EXACT_RANGE, false,
+        { highlighter: RangeHighlighterEx ->
+          highlighter.isGreedyToLeft = true
+          highlighter.isGreedyToRight = true
+        })
   }
 
   /**
    * Apply the provided tag name to the component in the model
    */
-  private fun applyNewTagName(newTagName: String) =
+  private fun applyTagEdit(newTagName: String) {
+    val editTagRunnable = Runnable {
+      myNlComponent.tag.name = newTagName
       TransactionGuard.getInstance().submitTransactionAndWait {
-        WriteCommandAction.runWriteCommandAction(myProject, "Morph " + myComponent.tagName + " to $newTagName", null,
-            Runnable {
-              myComponent.tag.name = newTagName
-            }, myComponent.tag.containingFile)
+        myAttributes
+            .filter { !it.keep }
+            .map { it.name }
+            .forEach {
+              myNlComponent.tag.setAttribute(it, null)
+            }
       }
+    }
+
+    WriteCommandAction.runWriteCommandAction(myProject, "Morph " + myNlComponent.tagName + " to $newTagName", null,
+        editTagRunnable, myNlComponent.tag.containingFile)
+
+  }
 
   private fun createMorphPopup(morphDialog: MorphDialog): JBPopup {
     val popup = JBPopupFactory.getInstance()
@@ -167,22 +167,26 @@ class MorphComponentAction(component: NlComponent, designSurface: DesignSurface)
         .setRequestFocus(true)
         .setCancelOnClickOutside(true)
         .setAdText("Set the new type for the selected View")
+        .setCancelCallback { ->
+          if (!myEditor.isDisposed) {
+            EditorFactory.getInstance().releaseEditor(myEditor)
+          }
+          true
+        }
         .createPopup()
 
     morphDialog.setOkAction {
-      applyNewTagName(myNewName)
+      applyTagEdit(myNewName)
       popup.closeOk(null)
     }
     return popup
   }
 
   private fun showMorphPopup() {
-    val document = createDocument(myComponent)
-    val editor = createEditor(document)
-    val oldTagName = myRangesToText[0].second
-    val morphDialog = MorphDialog(myFacet, myProject, editor.component, oldTagName)
+    val oldTagName = myNlComponent.tagName
+    val morphDialog = MorphDialog(myFacet, myProject, myEditor.component, oldTagName)
 
-    morphDialog.setTagNameChangeConsumer(updateDocumentWithNewName(document))
+    morphDialog.setTagNameChangeConsumer(updateDocumentWithNewName(myEditor.document))
     createMorphPopup(morphDialog).showInFocusCenter()
     IdeFocusManager.getInstance(myProject).requestFocus(morphDialog.preferredFocusComponent, true)
   }
@@ -190,11 +194,78 @@ class MorphComponentAction(component: NlComponent, designSurface: DesignSurface)
   private fun updateDocumentWithNewName(document: Document): (String) -> Unit = {
     newName ->
     myNewName = newName
-    myTagNameRange?.let {
-      document.replaceString(it.startOffset, it.endOffset, newName)
-    }
+    document.replaceString(myTagNameRange.startOffset, myTagNameRange.endOffset, newName)
   }
 
   override fun actionPerformed(e: AnActionEvent?) = showMorphPopup()
+
+  /**
+   * Render showing a remove or add icon and running the provided [runnable] when clicking
+   * on it.
+   */
+  private class IconRenderer(keep: Boolean, val runnable: Runnable) : GutterIconRenderer() {
+    val myIcon: Icon = if (keep) AllIcons.General.Remove else AllIcons.General.Add
+
+    override fun getClickAction() = object : AnAction() {
+      override fun actionPerformed(e: AnActionEvent?) = runnable.run()
+    }
+
+    override fun getIcon(): Icon = myIcon
+
+    override fun equals(other: Any?): Boolean {
+      if (this === other) return true
+      if (other == null || this::class != other::class) return false
+      return myIcon != (other as IconRenderer).myIcon
+    }
+
+    override fun hashCode(): Int = myIcon.hashCode()
+  }
+
+  /**
+   * Data class to store useful information about an [NlComponent]'s attribute
+   */
+  private class AttributeProperties(val startOffset: Int,
+                                    val name: String,
+                                    length: Int,
+                                    var keep: Boolean = true,
+                                    var highlighter: RangeHighlighter? = null) {
+    val endOffset = startOffset + length
+
+    private val myStrikeoutTextAttributes = TextAttributes(null, null,
+        EditorColorsManager.getInstance().globalScheme.getColor(
+            EditorColors.DELETED_LINES_COLOR),
+        EffectType.STRIKEOUT,
+        Font.ITALIC)
+
+    /**
+     * Remove the old [com.intellij.openapi.editor.RangeMarker] in  [editor] for this [AttributeProperties],
+     * which will have its [AttributeProperties.keep] property inverted. A new [com.intellij.openapi.editor.RangeMarker]
+     * will be added with the updated this [AttributeProperties]
+     */
+    private fun toggleKeep(editor: EditorEx) {
+      keep = !keep
+      removeMarker(editor)
+      addMarker(editor)
+    }
+
+    private fun removeMarker(editor: EditorEx) {
+      highlighter?.let {
+        editor.markupModel.removeHighlighter(it)
+      }
+    }
+
+    internal fun addMarker(editorEx: EditorEx) {
+      highlighter = editorEx.markupModel.addRangeHighlighter(startOffset, endOffset,
+          HighlighterLayer.SELECTION,
+          if (!keep) myStrikeoutTextAttributes else null,
+          HighlighterTargetArea.EXACT_RANGE)
+
+      highlighter?.let {
+        it.gutterIconRenderer = IconRenderer(keep, Runnable {
+          toggleKeep(editorEx)
+        })
+      }
+    }
+  }
 }
 
