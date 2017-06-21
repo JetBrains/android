c666d9c2b9fb940b57f67a710efc31fb6229ce08
Justin Nieto <justinnieto@google.com>
2017-06-20 17:29:45 -0700

    Update gutter icons when resource changes
    Currently, when the user edits an image resource (e.g. changes the
    color of a vector image), the gutter icons shown next to lines which
    reference the resource do not get updated to reflect these changes.
    This issue is caused by the presence of stale entries in
    GutterIconCache.
    
    This CL fixes the issue by having GutterIconCache#getIcon check for
    changes in image resource files and create new Icons for those
    resources if their cache entries are no longer valid. Document
    contents are used to generate the new Icons if the resource is open
    in an editor so that the new Icon takes into account any unsaved
    changes in the resource XML.
    
    GutterIconRenderer#getIcon has been updated to consult GutterIconCache
    for all Icon requests, rather than using a potentially stale Icon
    stored in a local variable.
    
    Test: Covered by GutterIconCacheTest#testIsIconUpToDate_*
    Change-Id: Ibee09d7607945985484f19591a6d44378c39d5f3

--- android/testSrc/com/android/tools/idea/rendering/GutterIconCacheTest.java
+++ android/testSrc/com/android/tools/idea/rendering/GutterIconCacheTest.java
@@ -16,16 +16,42 @@
 package com.android.tools.idea.rendering;
 
 import com.android.tools.adtui.imagediff.ImageDiffUtil;
+import com.android.tools.idea.io.TestFileUtils;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.fileEditor.FileDocumentManager;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.openapi.vfs.VirtualFileManager;
+import org.jetbrains.android.AndroidResourceRenameResourceProcessor;
 import org.jetbrains.android.AndroidTestCase;
+import org.junit.Before;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.FileTime;
 
 import static com.google.common.truth.Truth.assertThat;
 
 public class GutterIconCacheTest extends AndroidTestCase {
+  private Path mySampleSvgPath;
+  private VirtualFile mySampleSvgFile;
+
+  @Override
+  protected void setUp() throws Exception {
+    super.setUp();
+
+    mySampleSvgPath = FileSystems.getDefault().getPath(myModule.getProject().getBasePath(),
+                                                       "app", "src", "main", "res", "drawable", "GutterIconCacheTest_sample.xml");
+
+    String contents = "<svg viewBox=\"0 0 50 50\"><rect width=\"50\" height=\"50\" fill=\"blue\"/></svg>";
+    mySampleSvgFile = TestFileUtils.writeFileAndRefreshVfs(mySampleSvgPath, contents);
+  }
 
   public void testCreateBitmapIcon_bigEnough() throws Exception {
     BufferedImage input = ImageIO.read(new File(getTestDataPath(), "render/imageutils/actual.png"));
@@ -51,5 +77,56 @@ public class GutterIconCacheTest extends AndroidTestCase {
 
     // Input and output should be identical.
     ImageDiffUtil.assertImageSimilar(getName(), input, output, 0);
+  }
+
+  public void testIsIconUpToDate_entryInvalidNotCached() {
+    // Use constructor instead of statically-loaded instance to ensure fresh cache
+    GutterIconCache cache = new GutterIconCache();
+
+    // If we've never requested an Icon for the path, there should be no valid cache entry.
+    assertThat(cache.isIconUpToDate(mySampleSvgPath.toString())).isFalse();
+  }
+
+  public void testIsIconUpToDate_entryValid() {
+    GutterIconCache.getInstance().getIcon(mySampleSvgPath.toString(), null);
+
+    // If we haven't modified the image since creating an Icon, the cache entry is still valid
+    assertThat(GutterIconCache.getInstance().isIconUpToDate(mySampleSvgPath.toString())).isTrue();
+  }
+
+  public void testIsIconUpToDate_entryInvalidUnsavedChanges() {
+    GutterIconCache.getInstance().getIcon(mySampleSvgPath.toString(), null);
+
+    // "Modify" Document by rewriting its contents
+    Document document = FileDocumentManager.getInstance().getDocument(mySampleSvgFile);
+    ApplicationManager.getApplication().runWriteAction(() -> document.setText(document.getText()));
+
+    // Modifying the image should have invalidated the cache entry.
+    assertThat(GutterIconCache.getInstance().isIconUpToDate(mySampleSvgPath.toString())).isFalse();
+  }
+
+  public void testIconUpToDate_entryInvalidSavedChanges() {
+    GutterIconCache.getInstance().getIcon(mySampleSvgPath.toString(), null);
+
+    // Modify image resource by adding an empty comment and then save to disk
+    Document document = FileDocumentManager.getInstance().getDocument(mySampleSvgFile);
+    ApplicationManager.getApplication().runWriteAction(() -> {
+      document.setText(document.getText() + "<!---->");
+      FileDocumentManager.getInstance().saveDocument(document);
+    });
+
+    // Modifying the image should have invalidated the cache entry.
+    assertThat(GutterIconCache.getInstance().isIconUpToDate(mySampleSvgPath.toString())).isFalse();
+  }
+
+  public void testIconUpToDate_entryInvalidDiskChanges() throws Exception {
+    GutterIconCache.getInstance().getIcon(mySampleSvgPath.toString(), null);
+
+    // "Modify" file by resetting its lastModified field
+    Files.setLastModifiedTime(mySampleSvgPath, FileTime.fromMillis(System.currentTimeMillis()));
+    mySampleSvgFile.refresh(false, false);
+
+    // Modifying the image should have invalidated the cache entry.
+    assertThat(GutterIconCache.getInstance().isIconUpToDate(mySampleSvgPath.toString())).isFalse();
   }
 }
