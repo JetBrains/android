2c01e2ac5cdbb6a27625d624cf8310351e62ce13
Diego Perez <diegoperez@google.com>
2017-06-26 12:35:08 +0200

    Updates to @tools:sample/lorem source
    Now the default @tools:sample/lorem will generate the same content every
    time. Each line will contain one extra word compared to the previous
    line. This will allow to code things like @tools:sample/lorem[30] to
    have a 31 words line.
    A new source will allow for a randomized lorem in case that the layout
    needs to support different lengths.
    
    Test: N/A
    Change-Id: I99f305c603337083e2c3bb46796fe97108ec263e

--- android/src/com/android/tools/idea/sampledata/datasource/LoremIpsumGenerator.java
+++ android/src/com/android/tools/idea/sampledata/datasource/LoremIpsumGenerator.java
@@ -19,17 +19,29 @@ import com.intellij.codeInsight.template.emmet.generators.LoremGenerator;
 
 import java.io.OutputStream;
 import java.io.PrintStream;
+import java.util.Random;
 import java.util.function.Function;
 
 public class LoremIpsumGenerator implements Function<OutputStream, Exception> {
   final LoremGenerator myLoremGenerator = new LoremGenerator();
+  /**
+   * If true, each line of the data source will contain a random length and will start at a different point.
+   * When false, each line will contain an extra word compared to the previous one.
+   */
+  private final boolean isRandom;
+
+  public LoremIpsumGenerator(boolean random) {
+    isRandom = random;
+  }
 
   @Override
   public Exception apply(OutputStream stream) {
     //noinspection IOResourceOpenedButNotSafelyClosed (Closed by the caller)
     PrintStream printStream = new PrintStream(stream);
+    Random random = new Random();
     for (int i = 0; i < 500; i++) {
-      printStream.println(myLoremGenerator.generate(10, false));
+      int wordCount = (isRandom ? random.nextInt(500) : i) + 1;
+      printStream.println(myLoremGenerator.generate(wordCount, !isRandom));
     }
 
     return null;
