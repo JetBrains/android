35d8cefe63e3069dd92db2c5d79c3835ea019251
Dana Dahlstrom <dahlstrom@google.com>
2017-06-26 19:30:59 +0000

    Revert Change Iaf73cc03
    … which caused several UI tests to fail.
    
    Bug: 62027560
    Change-Id: I62496761739e1ea081613d690db6a1948815d9bc

--- android/src/com/android/tools/idea/gradle/dependencies/GradleDependencyManager.java
+++ android/src/com/android/tools/idea/gradle/dependencies/GradleDependencyManager.java
@@ -87,8 +87,7 @@ public class GradleDependencyManager {
       return Collections.emptyList();
     }
 
-    String configurationName = GradleUtil.mapConfigurationName(COMPILE, GradleUtil.getAndroidGradleModelVersionInUse(module), false);
-    List<ArtifactDependencyModel> compileDependencies = buildModel != null ? buildModel.dependencies().artifacts(configurationName) : null;
+    List<ArtifactDependencyModel> compileDependencies = buildModel != null ? buildModel.dependencies().artifacts(COMPILE) : null;
 
     // Record current version of support library; if used, prefer that for other dependencies
     // (e.g. if you're using appcompat-v7 version 25.3.1, and you drag in a recyclerview-v7
@@ -97,7 +96,7 @@ public class GradleDependencyManager {
     if (compileDependencies != null) {
       for (ArtifactDependencyModel dependency : compileDependencies) {
         if (Objects.equal(SUPPORT_LIB_GROUP_ID, dependency.group().value()) &&
-            !Objects.equal("multidex", dependency.name().value())) {
+            Objects.equal("appcompat-v7", dependency.name().value())) {
           String s = dependency.version().value();
           if (s != null) {
             appCompatVersion = GradleVersion.tryParse(s);
@@ -125,6 +124,7 @@ public class GradleDependencyManager {
       // all consistent.
       if (appCompatVersion != null
           && coordinate.acceptsGreaterRevisions() && SUPPORT_LIB_GROUP_ID.equals(groupId)
+          && !artifactId.equals("appcompat-v7")
           // The only library in groupId=SUPPORT_LIB_GROUP_ID which doesn't follow the normal version numbering scheme
           && !artifactId.equals("multidex")) {
         resolvedCoordinate = GradleCoordinate.parseCoordinateString(groupId + ":" + artifactId + ":" + appCompatVersion.toString());
