f2ce71a6a24e0d7bf3c376bc0022d5505eaf093e
Madiyar Aitbayev <madiyar@google.com>
2017-06-29 18:22:49 +0100

    Polish ui in cpu threads view tooltip
    Test: N/A
    Bug: 63132627
    Change-Id: I041336038d2da9629f0dfdb448fe9eca7966adab

--- profilers-ui/src/com/android/tools/profilers/cpu/CpuThreadsTooltipView.java
+++ profilers-ui/src/com/android/tools/profilers/cpu/CpuThreadsTooltipView.java
@@ -18,9 +18,12 @@ package com.android.tools.profilers.cpu;
 import com.android.tools.adtui.common.AdtUiUtils;
 import com.android.tools.adtui.model.Range;
 import com.android.tools.adtui.model.formatter.TimeAxisFormatter;
+import com.android.tools.profilers.ProfilerColors;
 import com.android.tools.profilers.ProfilerLayout;
 import com.android.tools.profilers.ProfilerTimeline;
 import com.android.tools.profilers.ProfilerTooltipView;
+import com.intellij.ui.ColorUtil;
+import com.intellij.ui.JBColor;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
@@ -30,14 +33,18 @@ public class CpuThreadsTooltipView extends ProfilerTooltipView {
   @NotNull private final CpuProfilerStage.ThreadsTooltip myTooltip;
   @NotNull private final JLabel myContent;
   @NotNull private final ProfilerTimeline myTimeline;
+  private int myMaximumLabelWidth = 0;
 
   protected CpuThreadsTooltipView(@NotNull CpuProfilerStageView view, @NotNull CpuProfilerStage.ThreadsTooltip tooltip) {
     super(view.getTimeline(), "CPU");
     myLabel.setFont(AdtUiUtils.DEFAULT_FONT.deriveFont(ProfilerLayout.TOOLTIP_FONT_SIZE));
+    myLabel.setBorder(BorderFactory.createEmptyBorder(0, 0, 4, 0));
+
     myTimeline = view.getTimeline();
     myTooltip = tooltip;
     myContent = new JLabel();
-    myContent.setFont(AdtUiUtils.DEFAULT_FONT.deriveFont(ProfilerLayout.TOOLTIP_FONT_SIZE));
+    myContent.setFont(AdtUiUtils.DEFAULT_FONT);
+    myContent.setForeground(ProfilerColors.MONITORS_HEADER_TEXT);
     tooltip.addDependency(this).onChange(CpuProfilerStage.ThreadsTooltip.Aspect.THREAD_STATE, this::threadStateChanged);
   }
 
@@ -48,7 +55,12 @@ public class CpuThreadsTooltipView extends ProfilerTooltipView {
       String time = TimeAxisFormatter.DEFAULT
         .getFormattedString(myTimeline.getDataRange().getLength(), range.getMin() - myTimeline.getDataRange().getMin(), true);
       String title = myTooltip.getThreadName() != null ? myTooltip.getThreadName() : "CPU";
-      myLabel.setText(String.format("%s %s", title, time));
+      myLabel.setText(String.format("<html>%s <span style='color:#%s'>%s</span></html",
+                                    title,
+                                    ColorUtil.toHex(ProfilerColors.CPU_THREADS_TOOLTIP_TIME_COLOR),
+                                    time));
+      myMaximumLabelWidth = Math.max(myMaximumLabelWidth, myLabel.getWidth());
+      myLabel.setMinimumSize(new Dimension(myMaximumLabelWidth, 0));
     } else {
       myLabel.setText("");
     }
