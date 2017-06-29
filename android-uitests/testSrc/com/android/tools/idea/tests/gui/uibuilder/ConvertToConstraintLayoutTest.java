587b2b98d9cd1beafe8f3e57cdeee5d2c946e3f0
Dana Dahlstrom <dahlstrom@google.com>
2017-06-28 20:14:40 -0700

    Update expected XML in testConvert2
    … following Change I1b20c275, which caused this test to fail.
    
    That change also deleted a TODO comment to remove inapplicable
    attributes, citing as an example “"android:orientation" when converting
    from a LinearLayout” which is exactly what this test expected and is
    removed here to get it passing again.
    
    Change-Id: Ie2caf19da78e64c514746fc11c6039f032fec155

--- android-uitests/testSrc/com/android/tools/idea/tests/gui/uibuilder/ConvertToConstraintLayoutTest.java
+++ android-uitests/testSrc/com/android/tools/idea/tests/gui/uibuilder/ConvertToConstraintLayoutTest.java
@@ -199,8 +199,7 @@ public class ConvertToConstraintLayoutTest {
                  "    xmlns:app=\"http://schemas.android.com/apk/res-auto\"\n" +
                  "    xmlns:tools=\"http://schemas.android.com/tools\"\n" +
                  "    android:layout_width=\"match_parent\"\n" +
-                 "    android:layout_height=\"wrap_content\"\n" +
-                 "    android:orientation=\"vertical\">\n" +
+                 "    android:layout_height=\"wrap_content\">\n" +
                  "\n" +
                  "    <TextView\n" +
                  "        android:id=\"@+id/title\"\n" +
