80d152e2e1b7de5da8e650378bb2f87c9e4b6459
Michal Bendowski <bendowski@google.com>
2017-06-30 11:31:44 +0100

    Single constant for the Kotlin version used
    Test: N/A
    Change-Id: I5bfcfeb82320f1d7c69b5efa9e798efe2b03b8da

--- android/src/com/android/tools/idea/npw/template/ConvertJavaToKotlinDefaultImpl.java
+++ android/src/com/android/tools/idea/npw/template/ConvertJavaToKotlinDefaultImpl.java
@@ -15,6 +15,7 @@
  */
 package com.android.tools.idea.npw.template;
 
+import com.android.SdkConstants;
 import com.android.annotations.NonNull;
 import com.intellij.ide.plugins.IdeaPluginDescriptor;
 import com.intellij.ide.plugins.PluginManager;
@@ -38,7 +39,7 @@ public class ConvertJavaToKotlinDefaultImpl implements ConvertJavaToKotlinProvid
   @NonNull
   @Override
   public String getKotlinVersion() {
-    return "1.1.2-3";
+    return SdkConstants.KOTLIN_LATEST_VERSION;
   }
 
   @Override
