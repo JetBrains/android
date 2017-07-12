edcf3bd13a96995c57701ef90379b4045712849e
Dana Dahlstrom <dahlstrom@google.com>
2017-07-12 20:23:24 +0000

    Revert changes Ia66c552e, I42024eae, and I3d614ff2
    … because only ThemeSelectorTest.testRenameTheme continues to fail sporadically.
    
    Bug: 63048046
    Change-Id: I3b3674c46174d6d6bbd1a68641711430a1d913d2

--- android-uitests/testSrc/com/android/tools/idea/tests/gui/theme/ThemeSelectorTest.java
+++ android-uitests/testSrc/com/android/tools/idea/tests/gui/theme/ThemeSelectorTest.java
@@ -105,7 +105,6 @@ public class ThemeSelectorTest {
   /**
    * Tests the Show all themes dialog from the theme selector
    */
-  @RunIn(TestGroup.UNRELIABLE)  // b/63178811
   @Test
   public void testShowAllThemes() throws IOException {
     guiTest.importSimpleApplication();
@@ -223,7 +222,6 @@ public class ThemeSelectorTest {
    * Tests that we can remove AppCompat and the themes update correctly.
    * Test that we can open the simple application and the theme editor opens correctly.
    */
-  @RunIn(TestGroup.UNRELIABLE)  // b/63048046
   @Test
   public void testRemoveAppCompat() throws IOException {
     guiTest.importSimpleApplication();
