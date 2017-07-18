d221b38336b718c78939d4a13d0b144ee969d09c
Diego Perez <diegoperez@google.com>
2017-07-18 15:10:10 +0100

    Move sample data images to lib dir
    We can not enumerate files from within the android plugin jar file so,
    for now, moving the files for images outside to the lib dir.
    
    Bug: 63797483
    Test: Existing tests pass
    Change-Id: I04b4d236aa051f0c88bce4dd84ec84c599dc68c6

--- android/src/com/android/tools/idea/sampledata/datasource/ResourceContent.java
+++ android/src/com/android/tools/idea/sampledata/datasource/ResourceContent.java
@@ -16,28 +16,75 @@
 package com.android.tools.idea.sampledata.datasource;
 
 import com.google.common.base.Charsets;
-import com.intellij.openapi.util.io.StreamUtil;
+import com.intellij.openapi.application.PathManager;
+import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.util.io.FileUtil;
+import com.intellij.openapi.vfs.LocalFileSystem;
+import com.intellij.openapi.vfs.VfsUtilCore;
+import com.intellij.openapi.vfs.VirtualFile;
 import libcore.io.Streams;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.io.*;
-import java.net.URL;
 import java.util.function.Function;
 
 public class ResourceContent implements Function<OutputStream, Exception> {
+  private static final Logger LOG = Logger.getInstance(ResourceContent.class);
+  private static final String[] LIB_CUSTOM_PATHS = {
+    // Bundled path
+    "/plugins/android/lib/sampleData",
+    // Development path
+    "/../adt/idea/android/lib/sampleData",
+    // IDEA plugin Development path
+    "/community/android/android/lib/sampleData"
+  };
+
   byte[] myContent;
 
   private ResourceContent(@NotNull byte[] content) {
     myContent = content;
   }
 
-  @NotNull
-  public static ResourceContent fromDirectory(@Nullable URL pathUrl) {
-    StringBuilder content = new StringBuilder();
+  /**
+   * Returns the base directory for the Sample Data directory contents
+   */
+  @Nullable
+  private static File getSampleDataBaseDir() {
+    String homePath = FileUtil.toSystemIndependentName(PathManager.getHomePath());
 
-    File path = pathUrl != null && pathUrl.getFile() != null ? new File(pathUrl.getFile()) : null;
-    File[] files = path != null ? path.listFiles() : null;
+    StringBuilder notFoundPaths = new StringBuilder();
+    for (String path : LIB_CUSTOM_PATHS) {
+      String jarPath = homePath + path;
+      VirtualFile rootDir = LocalFileSystem.getInstance().findFileByPath(FileUtil.toSystemIndependentName(jarPath));
+
+      if (rootDir != null) {
+        File rootFile = VfsUtilCore.virtualToIoFile(rootDir);
+        if (rootFile.exists()) {
+          LOG.debug("Sample data base dir found at " + jarPath);
+          return rootFile;
+        }
+      }
+      else {
+        notFoundPaths.append(jarPath).append('\n');
+      }
+    }
+
+    LOG.warn("Unable to sampleData in paths:\n" + notFoundPaths.toString());
+    return null;
+  }
+
+  /**
+   * Because directories can list multiple files that we have to provide direct access to, they can not be contained
+   * within the JAR file. We always assume these directory is within the lib path.
+   */
+  @NotNull
+  public static ResourceContent fromDirectory(@NotNull String relativePath) {
+    File baseDir = getSampleDataBaseDir();
+    File sampleDataPath = baseDir != null ? new File(baseDir, relativePath) : null;
+    File[] files = sampleDataPath != null && sampleDataPath.isDirectory() ? sampleDataPath.listFiles() : null;
+
+    StringBuilder content = new StringBuilder();
 
     if (files != null) {
       for (File file : files) {
