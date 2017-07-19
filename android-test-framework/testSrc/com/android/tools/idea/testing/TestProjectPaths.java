f08d383b8caa0fb1464b0d282b9a4ac35c8f3ac8
Michal Bendowski <bendowski@google.com>
2017-07-19 15:01:48 +0100

    Don't report XML resolve issues in test files
    Currently we don't handle test resources at all and usually don't even
    recognize them as resources files. But IDE can be tricked into treating
    them as resources if a test manifest file is in the right place (no idea
    what this is supposed to achieve). For now let's just stop reporting
    resolve issues in the inspection, until we can make it correct.
    
    Bug: 62463347
    Test: TestResourcesTest
    Change-Id: I11a14b4afa804fed41f60885d8649890fc8b1f95

--- android-test-framework/testSrc/com/android/tools/idea/testing/TestProjectPaths.java
+++ android-test-framework/testSrc/com/android/tools/idea/testing/TestProjectPaths.java
@@ -64,6 +64,7 @@ public final class TestProjectPaths {
   public static final String TEST_ARTIFACTS_OLD_TESTS = "projects/testArtifacts/oldTests";
   public static final String TEST_ARTIFACTS_SAME_NAME_CLASSES = "projects/testArtifacts/sameNameClasses";
   public static final String TEST_ONLY_MODULE = "projects/testOnlyModule";
+  public static final String TEST_RESOURCES = "projects/testResources";
   public static final String TRANSITIVE_DEPENDENCIES = "projects/transitiveDependencies";
   public static final String UIBUILDER_PROPERTY = "projects/uibuilder/property";
   public static final String UNUSED_RESOURCES = "projects/unusedResources";
