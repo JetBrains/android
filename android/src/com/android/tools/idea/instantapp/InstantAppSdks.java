282014bb09b4857f0b8b7e8060f27f6650557159
Lucas Smaira <lsmaira@google.com>
2017-07-20 18:19:03 +0100

    [AIA] Fix isIntantAppEnabled method
    Instant App sdk used to be obtained either from an environment variable,
    a system property or from the local android sdk repository. This should
    not be done anymore (now that the SDK is already public) and it was
    causing problems of instant apps being considered enabled when test sdk
    environment variable was used.
    
    This CL fixes this problem. Now the SDK is obtained only though the
    SdkHandler APIs and checking if it's enabled checks if the sdk is
    downloaded.
    
    Test: existing tests should pass
    
    Bug b/63829126
    
    Change-Id: I009900eeb4af66d94f7f53a427df4871d1d29dad

--- android/src/com/android/tools/idea/instantapp/InstantAppSdks.java
+++ android/src/com/android/tools/idea/instantapp/InstantAppSdks.java
@@ -17,36 +17,29 @@ package com.android.tools.idea.instantapp;
 
 import com.android.instantapp.sdk.InstantAppSdkException;
 import com.android.instantapp.sdk.Metadata;
-import com.android.tools.idea.sdk.IdeSdks;
+import com.android.repository.api.LocalPackage;
+import com.android.sdklib.repository.AndroidSdkHandler;
+import com.android.tools.idea.sdk.AndroidSdks;
+import com.android.tools.idea.sdk.progress.StudioLoggerProgressIndicator;
 import com.android.tools.idea.wizard.model.ModelWizardDialog;
 import com.google.common.collect.ImmutableList;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.components.ServiceManager;
 import com.intellij.openapi.ui.Messages;
-import com.intellij.openapi.util.text.StringUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.io.File;
-import java.net.URI;
-import java.net.URISyntaxException;
 
 import static com.android.SdkConstants.FD_EXTRAS;
-import static com.android.repository.api.RepoPackage.PATH_SEPARATOR;
 import static com.android.tools.idea.sdk.wizard.SdkQuickfixUtils.createDialogForPaths;
-import static com.intellij.util.io.URLUtil.FILE_PROTOCOL;
 
 /**
  * Responsible for providing InstantApp SDK.
  * It is registered as a service so it can be easily mocked.
  */
 public class InstantAppSdks {
-  @NotNull private static final String INSTANT_APP_SDK_ENV_VAR = "WH_SDK";
   @NotNull private static final String INSTANT_APP_SDK_PATH = FD_EXTRAS + ";google;instantapps";
-  @NotNull public static final String INSTANT_APP_SDK_PROPERTY = "android.instant_app_sdk_location";
-
-  @Nullable private File mySdk = null;
-  private boolean myInstantAppSdkAvailable = false;
 
   @NotNull
   public static InstantAppSdks getInstance() {
@@ -55,31 +48,18 @@ public class InstantAppSdks {
 
   @Nullable
   public File getInstantAppSdk(boolean tryToInstall) {
-    if (mySdk != null && mySdk.exists() && mySdk.isDirectory()) {
-      return mySdk;
+    LocalPackage localPackage = getInstantAppLocalPackage();
+    if (localPackage == null && tryToInstall) {
+      installSdkIfNeeded();
+      localPackage = getInstantAppLocalPackage();
     }
+    return localPackage == null ? null : localPackage.getLocation();
+  }
 
-    // Try to get SDK through WH_SDK environment variable
-    mySdk = validateSdk(System.getenv(INSTANT_APP_SDK_ENV_VAR));
-
-    if (mySdk == null) {
-      // Try to get SDK through defined property
-      mySdk = validateSdk(System.getProperty(INSTANT_APP_SDK_PROPERTY));
-    }
-
-    if (mySdk == null) {
-      File androidSdk = IdeSdks.getInstance().getAndroidSdkPath();
-      if (androidSdk != null) {
-        mySdk = validateSdk(androidSdk.getAbsolutePath() + ("/" + INSTANT_APP_SDK_PATH).replace(PATH_SEPARATOR, File.separatorChar));
-        if (tryToInstall && mySdk == null) {
-          // Try to get SDK through SDK manager
-          installSdkIfNeeded();
-          mySdk = validateSdk(androidSdk.getAbsolutePath() + ("/" + INSTANT_APP_SDK_PATH).replace(PATH_SEPARATOR, File.separatorChar));
-        }
-      }
-    }
-
-    return mySdk;
+  @Nullable
+  private static LocalPackage getInstantAppLocalPackage() {
+    AndroidSdkHandler androidSdkHandler = AndroidSdks.getInstance().tryToChooseSdkHandler();
+    return androidSdkHandler.getLocalPackage(INSTANT_APP_SDK_PATH, new StudioLoggerProgressIndicator(InstantAppSdks.class));
   }
 
   private static void installSdkIfNeeded() {
@@ -95,49 +75,12 @@ public class InstantAppSdks {
     });
   }
 
-  @Nullable
-  private static File validateSdk(String sdk) {
-    if (StringUtil.isEmpty(sdk)) {
-      return null;
-    }
-    File sdkFile = new File(sdk);
-    if (!sdkFile.exists() || !sdkFile.isDirectory()) {
-      return null;
-    }
-    return sdkFile;
-  }
-
-  // Used to verify if the user has access to an Instant App SDK. This is temporary and should be deleted once the SDK becomes public.
-  // If we use #getInstantAppSdk to verify if it's available, the user will be showed a dialog each time, which we don't want during startup, for example.
+  /**
+   * Used to determine if instant app features should be shown or not.
+   * If the instant app sdk is downloaded it returns true.
+   */
   public boolean isInstantAppSdkEnabled() {
-    if (! myInstantAppSdkAvailable) {
-      String sdkUrl = System.getenv("SDK_TEST_BASE_URL");
-      if (getInstantAppSdk(false) != null) {
-        myInstantAppSdkAvailable = true;
-      }
-      else if (StringUtil.isNotEmpty(sdkUrl)) {
-        try {
-          URI sdkUri = new URI(sdkUrl);
-          if (FILE_PROTOCOL.equals(sdkUri.getScheme())) {
-            File sdkRepo = new File(sdkUri);
-            if (sdkRepo.exists() && sdkRepo.isDirectory()) {
-              String[] children = sdkRepo.list();
-              if (children != null) {
-                for (String child : children) {
-                  if (child.startsWith("whsdk")) {
-                    myInstantAppSdkAvailable = true;
-                  }
-                }
-              }
-            }
-          }
-        }
-        catch (URISyntaxException e) {
-          // Ignore
-        }
-      }
-    }
-    return myInstantAppSdkAvailable;
+    return getInstantAppLocalPackage() != null;
   }
 
   public int getMinTargetSdk() throws InstantAppSdkException {
