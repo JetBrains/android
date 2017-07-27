db3b0296f0023174523026ec2e8bb43d88c75416
Jingjing Liu <jingjingliu@google.com>
2017-07-27 13:23:17 -0700

    Update NdkHelloJni project
    Correct the sample NdkHelloJni project to use the right ndk build
    project, downloading this project from
    https://github.com/googlesamples/android-ndk.git --branch
    master-ndkbuild and doing some mini changes
    
    Test: ran DebuggerInNdkProjectTest on local Linux machine
    Change-Id: Ic7806b7ecc2f562b49540fc7bf2d4c739339e706

--- android-uitests/testData/debugger/NdkHelloJni/app/src/main/cpp/hello-jni.c
+++ android-uitests/testData/debugger/NdkHelloJni/app/src/main/cpp/hello-jni.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016 The Android Open Source Project
+ * Copyright (C) 2009 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,14 +21,14 @@
  * to return a new VM String. See the corresponding Java source
  * file located at:
  *
- *   hello-jni/app/src/main/java/com/example/hellojni/HelloJni.java
+ *   apps/samples/hello-jni/project/src/com/example/hellojni/HelloJni.java
  */
-JNIEXPORT jstring JNICALL
+jstring
 Java_com_example_hellojni_HelloJni_stringFromJNI( JNIEnv* env,
                                                   jobject thiz )
 {
 #if defined(__arm__)
-    #if defined(__ARM_ARCH_7A__)
+  #if defined(__ARM_ARCH_7A__)
     #if defined(__ARM_NEON__)
       #if defined(__ARM_PCS_VFP)
         #define ABI "armeabi-v7a/NEON (hard-float)"
@@ -46,18 +46,19 @@ Java_com_example_hellojni_HelloJni_stringFromJNI( JNIEnv* env,
    #define ABI "armeabi"
   #endif
 #elif defined(__i386__)
-#define ABI "x86"
+   #define ABI "x86"
 #elif defined(__x86_64__)
-#define ABI "x86_64"
+   #define ABI "x86_64"
 #elif defined(__mips64)  /* mips64el-* toolchain defines __mips__ too */
-#define ABI "mips64"
+   #define ABI "mips64"
 #elif defined(__mips__)
-#define ABI "mips"
+   #define ABI "mips"
 #elif defined(__aarch64__)
-#define ABI "arm64-v8a"
+   #define ABI "arm64-v8a"
 #else
-#define ABI "unknown"
+   #define ABI "unknown"
 #endif
+
     int kid_age = 3;
     return (*env)->NewStringUTF(env, "ABI " ABI ".");
 }
