895538363b4e12fcfe5d6a7df0180d83838c1abc
parentej <parentej@google.com>
2017-07-28 16:02:56 +0100

    Adding Activity to project with "compile" dependencies duplicates entry
    If we have a project with a "old" dependency like:
    compile 'com.android.support.constraint:constraint-layout:1.0.2'
    Adding a new Activity will add a "new" dependency like:
    implementation 'com.android.support.constraint:constraint-layout:1.0.2'
    
    This CL makes sure that if we already have a "compile" dependency, we
    will not add a new equivalent "implementation" dependency.
    
    Bug: 62203176
    Test: Updated NewActivityTest.createDefaultActivity to check a single
    xxx:constraint-layout instead of two. Updated GradleFileMergerTestCase.
    Change-Id: Idcc71b3bed819736b0bbd7f3aa59e91754716f0c

--- android/src/com/android/tools/idea/templates/GradleFileMergers.java
+++ android/src/com/android/tools/idea/templates/GradleFileMergers.java
@@ -17,6 +17,7 @@ package com.android.tools.idea.templates;
 
 import com.android.ide.common.repository.GradleCoordinate;
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Multimap;
 import com.google.common.collect.Ordering;
 import org.jetbrains.annotations.NotNull;
@@ -37,6 +38,11 @@ public class GradleFileMergers {
                      "testApi", "testImplementation", "testCompile",
                      "androidTestApi", "androidTestImplementation", "androidTestCompile", "androidTestUtil");
 
+  private static final ImmutableSet<ImmutableSet<String>> CONFIGURATION_GROUPS = ImmutableSet.of(
+    ImmutableSet.of("feature", "api", "implementation", "compile"),
+    ImmutableSet.of("testApi", "testImplementation", "testCompile"),
+    ImmutableSet.of("androidTestApi", "androidTestImplementation", "androidTestCompile"));
+
   /**
    * Defined an ordering on gradle configuration names.
    */
@@ -55,13 +61,26 @@ public class GradleFileMergers {
   public static void removeExistingDependencies(@NotNull Map<String, Multimap<String, GradleCoordinate>> newDependencies,
                                                 @NotNull Map<String, Multimap<String, GradleCoordinate>> existingDependencies) {
     for (String configuration : newDependencies.keySet()) {
-      if (existingDependencies.containsKey(configuration)) {
-        for (String coordinateId: existingDependencies.get(configuration).keySet()) {
-          newDependencies.get(configuration).removeAll(coordinateId);
+      // If we already have an existing "compile" dependency, the same "implementation" or "api" dependency should not be added
+      for (String possibleConfiguration : getConfigurationGroup(configuration)) {
+        if (existingDependencies.containsKey(possibleConfiguration)) {
+          for (String coordinateId: existingDependencies.get(possibleConfiguration).keySet()) {
+            newDependencies.get(configuration).removeAll(coordinateId);
+          }
         }
       }
     }
   }
 
   private GradleFileMergers() {}
+
+  private static ImmutableSet<String> getConfigurationGroup(String configuration) {
+    for (ImmutableSet<String> group : CONFIGURATION_GROUPS) {
+      if (group.contains(configuration)) {
+        return group;
+      }
+    }
+
+    return ImmutableSet.of(configuration);
+  }
 }
