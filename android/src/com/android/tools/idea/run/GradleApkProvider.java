3572bc915457194100acc961dc50ef8839fe0df8
Stefan Xenos <sxenos@google.com>
2017-08-01 10:32:19 -0700

    Create project-system and project-system-gradle modules.
    Create the AndroidProjectSystem interface, the
    com.android.project.projectsystem extension point, and the Gradle
    implementation of it.
    
    Refactor AnalyzeApkAction to make use of the new abstraction rather than
    invoking gradle-specific code directly.
    
    Change ApkParser to have it recieve the aapt path externally rather than
    have it compute it in a gradle-specific way, and refactor callers to
    supply that path.
    
    Bug: 64250896
    Test: Manually opened the APK analyzer in both a gradle and a blaze project.
    
    Change-Id: I9faac3de4288af7bbe3d093cbb6d1ce7a0cb68ba

--- android/src/com/android/tools/idea/run/GradleApkProvider.java
+++ android/src/com/android/tools/idea/run/GradleApkProvider.java
@@ -18,6 +18,8 @@ package com.android.tools.idea.run;
 import com.android.build.OutputFile;
 import com.android.builder.model.*;
 import com.android.ddmlib.IDevice;
+import com.android.sdklib.repository.AndroidSdkHandler;
+import com.android.tools.apk.analyzer.AaptInvoker;
 import com.android.tools.apk.analyzer.AndroidApplicationInfo;
 import com.android.tools.apk.analyzer.Archive;
 import com.android.tools.apk.analyzer.Archives;
@@ -29,6 +31,8 @@ import com.android.tools.idea.gradle.run.PostBuildModel;
 import com.android.tools.idea.gradle.run.PostBuildModelProvider;
 import com.android.tools.idea.gradle.structure.editors.AndroidProjectSettingsService;
 import com.android.tools.idea.gradle.util.GradleUtil;
+import com.android.tools.idea.log.LogWrapper;
+import com.android.tools.idea.sdk.AndroidSdks;
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.collect.ImmutableList;
 import com.intellij.openapi.application.ApplicationManager;
@@ -43,6 +47,7 @@ import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
@@ -152,9 +157,14 @@ public class GradleApkProvider implements ApkProvider {
     return result;
   }
 
+  private static Path getPathToAapt() {
+    AndroidSdkHandler handler = AndroidSdks.getInstance().tryToChooseSdkHandler();
+    return AaptInvoker.getPathToAapt(handler, new LogWrapper(GradleApkProvider.class));
+  }
+
   private static String getPackageId(@NotNull File fileApk) throws ApkProvisionException {
     try (Archive archive = Archives.open(fileApk.toPath())) {
-      AndroidApplicationInfo applicationInfo = ApkParser.getAppInfo(archive);
+      AndroidApplicationInfo applicationInfo = ApkParser.getAppInfo(getPathToAapt(), archive);
       if(applicationInfo == AndroidApplicationInfo.UNKNOWN) {
         throw new ApkProvisionException("Could not determine manifest package for apk: " + fileApk.getName());
       } else {
