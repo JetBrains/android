f8e1033e0fe5fe60f33df5eb5dd5cb37fcedf292
Dmitry Avdeev <dmitry.avdeev@jetbrains.com>
2017-08-01 19:02:27 +0300

    IDEA-157267 New Project Wizard: Java module tooltip is shown for Android module

--- android/src/org/jetbrains/android/newProject/AndroidWizardWrapper.java
+++ android/src/org/jetbrains/android/newProject/AndroidWizardWrapper.java
@@ -21,6 +21,7 @@ import com.intellij.openapi.roots.ModifiableRootModel;
 import com.intellij.openapi.ui.DialogEarthquakeShaker;
 import com.intellij.openapi.ui.DialogWrapper;
 import icons.AndroidIcons;
+import org.jetbrains.android.util.AndroidBundle;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -60,6 +61,11 @@ public class AndroidWizardWrapper extends ModuleBuilder implements WizardDelegat
   @Override
   public Icon getNodeIcon() {
     return AndroidIcons.Android;
+  }
+
+  @Override
+  public String getDescription() {
+    return AndroidBundle.message("android.module.type.description");
   }
 
   @Override
