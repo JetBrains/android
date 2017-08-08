60c1182ec548b07bb0dd785efbd970a7283a9c80
Charlie Tsai <chartsai@google.com>
2017-08-08 18:45:02 +0100

    Add resize target for GridLayout
    Also converted GridLayoutHandler to Kotlin file.
    
    Test: Passed the test of GridLayoutTests.
          Added and passed GridLayoutHandlerTest
    Bug: 64403087
    
    Change-Id: I9e2092fd01a0936e677dfc8900f41bd558c62693

---
+++ designer/testSrc/com/android/tools/idea/uibuilder/handlers/grid/GridLayoutV7HandlerTest.kt
@@ -0,0 +1,82 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.uibuilder.handlers.relative
+
+import com.android.tools.idea.common.util.NlTreeDumper
+
+import com.android.SdkConstants.*
+import com.android.tools.idea.uibuilder.LayoutTestUtilities.mockViewWithBaseline
+import com.android.tools.idea.common.fixtures.ModelBuilder
+import com.android.tools.idea.uibuilder.scene.SceneTest
+import com.android.tools.idea.uibuilder.scene.target.ResizeBaseTarget
+
+class GridLayoutV7HandlerTest : GridLayoutHandlerTest() {
+
+  override fun createModel(): ModelBuilder {
+    val builder = model("grid_layout_v7.xml",
+        component(GRID_LAYOUT_V7)
+            .withBounds(0, 0, 1000, 1000)
+            .matchParentWidth()
+            .matchParentHeight()
+            .children(
+                component(BUTTON)
+                    .withBounds(100, 100, 100, 100)
+                    .viewObject(mockViewWithBaseline(17))
+                    .id("@id/button")
+                    .width("100dp")
+                    .height("100dp")
+                    .withAttribute("android:layout_row", "0")
+                    .withAttribute("android:layout_column", "0"),
+
+                component(CHECK_BOX)
+                    .withBounds(300, 300, 20, 20)
+                    .viewObject(mockViewWithBaseline(17))
+                    .id("@id/checkbox")
+                    .width("20dp")
+                    .height("20dp")
+                    .withAttribute("android:layout_row", "1")
+                    .withAttribute("android:layout_column", "1")
+            ))
+    val model = builder.build()
+    assertEquals(1, model.components.size)
+    assertEquals("NlComponent{tag=<android.support.v7.widget.GridLayout>, bounds=[0,0:1000x1000}\n" +
+        "    NlComponent{tag=<Button>, bounds=[100,100:100x100}\n" +
+        "    NlComponent{tag=<CheckBox>, bounds=[300,300:20x20}",
+        NlTreeDumper.dumpTree(model.components))
+
+    format(model.file)
+    assertEquals("<android.support.v7.widget.GridLayout xmlns:android=\"http://schemas.android.com/apk/res/android\"\n" +
+        "    android:layout_width=\"match_parent\"\n" +
+        "    android:layout_height=\"match_parent\">\n" +
+        "\n" +
+        "    <Button\n" +
+        "        android:id=\"@id/button\"\n" +
+        "        android:layout_width=\"100dp\"\n" +
+        "        android:layout_height=\"100dp\"\n" +
+        "        android:layout_row=\"0\"\n" +
+        "        android:layout_column=\"0\" />\n" +
+        "\n" +
+        "    <CheckBox\n" +
+        "        android:id=\"@id/checkbox\"\n" +
+        "        android:layout_width=\"20dp\"\n" +
+        "        android:layout_height=\"20dp\"\n" +
+        "        android:layout_row=\"1\"\n" +
+        "        android:layout_column=\"1\" />\n" +
+        "\n" +
+        "</android.support.v7.widget.GridLayout>\n", model.file.text)
+    return builder
+  }
+}
