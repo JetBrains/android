2135a0a56e0c22566d34ef0db258f4598825fff9
Billy Lam <shiufai@google.com>
2017-08-08 15:16:28 -0700

    Moved live allocation tracking logic to MemoryProfiler
    Before, the logic lives in MemoryMonitor, which can potentially
    be bypassed if the user quickly enters the MemoryProfilerStage,
    resulting in MemoryMonitor being destroyed and missing the agent
    status change signal. By moving to MemoryProfiler, the signal
    always gets picked up and avoids the problematic case.
    
    Also rearranged the stopProfiling logic in setDevice so that
    it is called before the device is changed. This allows tracking
    suspension to work correctly, as it requires to retrieve the
    device version during stopProfiling.
    
    Bug: 64490742
    Test: updated unit test
    Change-Id: I21d6ee85bc51016540c78871d29d2a05821c008d

--- profilers/testSrc/com/android/tools/profilers/memory/MemoryMonitorTest.java
+++ profilers/testSrc/com/android/tools/profilers/memory/MemoryMonitorTest.java
@@ -15,20 +15,13 @@
  */
 package com.android.tools.profilers.memory;
 
-import com.android.sdklib.AndroidVersion;
-import com.android.tools.adtui.model.FakeTimer;
-import com.android.tools.profiler.proto.Common;
-import com.android.tools.profiler.proto.MemoryProfiler;
-import com.android.tools.profiler.proto.Profiler;
 import com.android.tools.profilers.*;
 import com.android.tools.profilers.cpu.FakeCpuService;
 import com.android.tools.profilers.event.FakeEventService;
 import com.android.tools.profilers.network.FakeNetworkService;
+import com.google.common.truth.Truth;
 import org.junit.Rule;
 import org.junit.Test;
-
-import static org.hamcrest.core.IsInstanceOf.instanceOf;
-import static org.junit.Assert.*;
 
 public class MemoryMonitorTest {
 
@@ -43,101 +36,15 @@ public class MemoryMonitorTest {
   @Test
   public void testName() {
     MemoryMonitor monitor = new MemoryMonitor(new StudioProfilers(myGrpcChannel.getClient(), new FakeIdeProfilerServices()));
-    assertEquals("MEMORY", monitor.getName());
+    Truth.assertThat(monitor.getName()).isEqualTo("MEMORY");
   }
 
   @Test
   public void testExpand() {
     StudioProfilers profilers = new StudioProfilers(myGrpcChannel.getClient(), new FakeIdeProfilerServices());
     MemoryMonitor monitor = new MemoryMonitor(profilers);
-    assertEquals(profilers.getStage().getClass(), NullMonitorStage.class);
+    Truth.assertThat(profilers.getStage().getClass()).isEqualTo(NullMonitorStage.class);
     monitor.expand();
-    assertThat(profilers.getStage(), instanceOf(MemoryProfilerStage.class));
-  }
-
-  @Test
-  public void testLiveAllocationTrackingOnAgentAttach() {
-    FakeIdeProfilerServices ideProfilerServices = new FakeIdeProfilerServices();
-    ideProfilerServices.enableLiveAllocationTracking(true);
-    FakeTimer timer = new FakeTimer();
-
-    // Device needs to be O+.
-    Profiler.Device device = Profiler.Device.newBuilder()
-      .setSerial("FakeDevice")
-      .setState(Profiler.Device.State.ONLINE)
-      .setFeatureLevel(AndroidVersion.VersionCodes.O)
-      .build();
-    Profiler.Process process = Profiler.Process.newBuilder()
-      .setPid(20)
-      .setState(Profiler.Process.State.ALIVE)
-      .setName("FakeProcess")
-      .build();
-    myProfilerService.addDevice(device);
-    Common.Session session = Common.Session.newBuilder()
-      .setBootId(device.getBootId())
-      .setDeviceSerial(device.getSerial())
-      .build();
-    myProfilerService.addProcess(session, process);
-
-    // Note that MemoryMonitor is created by StudioMonitorStage when the process gets selected after the fake timer tick.
-    StudioProfilers profilers = new StudioProfilers(myGrpcChannel.getClient(), ideProfilerServices, timer);
-    timer.tick(FakeTimer.ONE_SECOND_IN_NS);
-
-    assertTrue(profilers.getStage() instanceof StudioMonitorStage);
-    assertFalse(profilers.isAgentAttached());
-    assertEquals(0, myMemoryService.getTrackAllocationCount());
-
-    myProfilerService.setAgentStatus(Profiler.AgentStatusResponse.Status.ATTACHED);
-    timer.tick(FakeTimer.ONE_SECOND_IN_NS);
-    assertTrue(profilers.isAgentAttached());
-    // Expecting a stop/start trackAllocations request pair.
-    assertEquals(2, myMemoryService.getTrackAllocationCount());
-  }
-
-  @Test
-  public void testAllocationTrackingNotStartedIfInfoExists() {
-    FakeIdeProfilerServices ideProfilerServices = new FakeIdeProfilerServices();
-    ideProfilerServices.enableLiveAllocationTracking(true);
-    FakeTimer timer = new FakeTimer();
-
-    // Device needs to be O+.
-    Profiler.Device device = Profiler.Device.newBuilder()
-      .setSerial("FakeDevice")
-      .setState(Profiler.Device.State.ONLINE)
-      .setFeatureLevel(AndroidVersion.VersionCodes.O)
-      .build();
-    Profiler.Process process = Profiler.Process.newBuilder()
-      .setPid(20)
-      .setState(Profiler.Process.State.ALIVE)
-      .setName("FakeProcess")
-      .build();
-    myProfilerService.addDevice(device);
-    Common.Session session = Common.Session.newBuilder()
-      .setBootId(device.getBootId())
-      .setDeviceSerial(device.getSerial())
-      .build();
-    myProfilerService.addProcess(session, process);
-
-    // AllocationsInfo should exist for tracking to not restart.
-    myMemoryService.setMemoryData(MemoryProfiler.MemoryData.newBuilder().addAllocationsInfo(
-      MemoryProfiler.AllocationsInfo.newBuilder()
-        .setStatus(MemoryProfiler.AllocationsInfo.Status.IN_PROGRESS)
-        .setStartTime(Long.MIN_VALUE)
-        .setEndTime(Long.MAX_VALUE)
-        .setLegacy(false).build()
-    ).build());
-
-    // Note that MemoryMonitor is created by StudioMonitorStage when the process gets selected after the fake timer tick.
-    StudioProfilers profilers = new StudioProfilers(myGrpcChannel.getClient(), ideProfilerServices, timer);
-    timer.tick(FakeTimer.ONE_SECOND_IN_NS);
-
-    assertTrue(profilers.getStage() instanceof StudioMonitorStage);
-    assertFalse(profilers.isAgentAttached());
-    assertEquals(0, myMemoryService.getTrackAllocationCount());
-
-    myProfilerService.setAgentStatus(Profiler.AgentStatusResponse.Status.ATTACHED);
-    timer.tick(FakeTimer.ONE_SECOND_IN_NS);
-    assertTrue(profilers.isAgentAttached());
-    assertEquals(0, myMemoryService.getTrackAllocationCount());
+    Truth.assertThat(profilers.getStage()).isInstanceOf(MemoryProfilerStage.class);
   }
 }
