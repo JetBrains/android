2076b3335423dfa67fb9a99547b653699818620f
parentej <parentej@google.com>
2017-08-11 14:51:28 +0100

    Remove Dynamic Wizard: Remove dead fields from WizardConstants
    This is one of many CL's to remove the Dynamic Wizard.
    
    It's still no possible to remove WizardConstants completely, but there
    are now a lot of fields no longer issued.
    
    Test: N/A removing dead code
    Change-Id: I8636bdd53e51485d29660a6a6c1cd78362773cc4

--- android/src/com/android/tools/idea/wizard/WizardConstants.java
+++ android/src/com/android/tools/idea/wizard/WizardConstants.java
@@ -15,20 +15,13 @@
  */
 package com.android.tools.idea.wizard;
 
-import com.android.sdklib.repository.legacy.descriptors.IPkgDesc;
 import com.android.tools.idea.ui.wizard.deprecated.StudioWizardLayout;
-import com.google.common.collect.ImmutableSet;
-import com.google.common.collect.SetMultimap;
 import com.intellij.ui.JBColor;
 import com.intellij.util.ui.JBUI;
 
 import java.awt.*;
-import java.io.File;
-import java.util.Collection;
 import java.util.List;
-import java.util.Set;
 
-import static com.android.tools.idea.templates.TemplateMetadata.*;
 import static com.android.tools.idea.wizard.dynamic.ScopedStateStore.Key;
 import static com.android.tools.idea.wizard.dynamic.ScopedStateStore.Scope.WIZARD;
 import static com.android.tools.idea.wizard.dynamic.ScopedStateStore.createKey;
@@ -71,60 +64,11 @@ public class WizardConstants {
 
   // State Store Keys
   // TODO After the wizard migration delete as many of these keys as possible
-  public static final Key<String> APPLICATION_NAME_KEY = createKey(ATTR_APP_TITLE, WIZARD, String.class);
-  public static final Key<String> BUILD_TOOLS_VERSION_KEY = createKey(ATTR_BUILD_TOOLS_VERSION, WIZARD, String.class);
-  public static final Key<String> COMPANY_DOMAIN_KEY = createKey(ATTR_COMPANY_DOMAIN, WIZARD, String.class);
-  public static final Key<String> DEBUG_KEYSTORE_SHA_1_KEY = createKey(ATTR_DEBUG_KEYSTORE_SHA1, WIZARD, String.class);
-  @SuppressWarnings("unchecked") public static final Key<SetMultimap<String, String>> DEPENDENCIES_KEY =
-    createKey(ATTR_DEPENDENCIES_MULTIMAP, WIZARD, (Class<SetMultimap<String, String>>)(Class)SetMultimap.class);
-  public static final Key<String> GRADLE_PLUGIN_VERSION_KEY = createKey(ATTR_GRADLE_PLUGIN_VERSION, WIZARD, String.class);
-  public static final Key<String> GRADLE_VERSION_KEY = createKey(ATTR_GRADLE_VERSION, WIZARD, String.class);
   @SuppressWarnings("unchecked") public static final Key<List<String>> INSTALL_REQUESTS_KEY =
     createKey("packagesToInstall", WIZARD, (Class<List<String>>)(Class)List.class);
-  public static final Key<Boolean> IS_GRADLE_PROJECT_KEY = createKey(ATTR_IS_GRADLE, WIZARD, Boolean.class);
-  public static final Key<Boolean> IS_NEW_PROJECT_KEY = createKey(ATTR_IS_NEW_PROJECT, WIZARD, Boolean.class);
-  public static final Key<Boolean> THEME_EXISTS_KEY = createKey(ATTR_THEME_EXISTS, WIZARD, Boolean.class);
   public static final Key<String> KEY_SDK_INSTALL_LOCATION = createKey("download.sdk.location", WIZARD, String.class);
-  public static final Key<String> PACKAGE_NAME_KEY = createKey(ATTR_PACKAGE_NAME, WIZARD, String.class);
-  public static final Key<String> PROJECT_LOCATION_KEY = createKey(ATTR_TOP_OUT, WIZARD, String.class);
-  public static final Key<String> SDK_DIR_KEY = createKey(ATTR_SDK_DIR, WIZARD, String.class);
-  public static final Key<String> ESPRESSO_VERSION_KEY = createKey(ATTR_ESPRESSO_VERSION, WIZARD, String.class);
-  @SuppressWarnings("unchecked") public static final Key<List<IPkgDesc>> SKIPPED_INSTALL_REQUESTS_KEY =
-    createKey("packagesSkipped", WIZARD, (Class<List<IPkgDesc>>)(Class)List.class);
   // TODO: change this an IntProperty, see com.android.tools.idea.sdk.wizard.InstallSelectedPackagesStep#checkForUpgrades
   public static final Key<Integer> NEWLY_INSTALLED_API_KEY = createKey("newly.installed.api.level", WIZARD, Integer.class);
-  public static final Key<Boolean> IS_LIBRARY_KEY = createKey(ATTR_IS_LIBRARY_MODULE, WIZARD, Boolean.class);
-  public static final Key<Boolean> IS_LOW_MEMORY_KEY = createKey(ATTR_IS_LOW_MEMORY, WIZARD, Boolean.class);
-
-  // Instant module step keys
-  public static final Key<String> INSTANT_APP_PACKAGE_NAME_KEY = createKey(ATTR_INSTANT_APP_PACKAGE_NAME, WIZARD, String.class);
-  public static final Key<Boolean> IS_INSTANT_APP_KEY = createKey(ATTR_IS_INSTANT_APP, WIZARD, Boolean.class);
-
-  // Native support configuration in new project wizard, see ConfigureCppSupportStep for usages
-  public static final Key<Boolean> INCLUDE_CPP_SUPPORT_KEY = createKey(ATTR_CPP_SUPPORT, WIZARD, Boolean.class);
-  public static final Key<String> CPP_FLAGS_KEY = createKey(ATTR_CPP_FLAGS, WIZARD, String.class);
-
-  /**
-   * Files to open in the editor window after the Wizard is finished.
-   */
-  @SuppressWarnings("unchecked") public static final Key<List<File>> FILES_TO_OPEN_KEY =
-    createKey("files.to.open", WIZARD, (Class<List<File>>)(Class)List.class);
-
-  @SuppressWarnings("unchecked") public static final Key<Collection<File>> TARGET_FILES_KEY =
-    createKey("target.files", WIZARD, (Class<Collection<File>>)(Object)Collection.class);
-
-  // Patterns
-  /**
-   * @deprecated This field should not be publicly accessed. Use provided path validation methods instead.
-   */
-  public static final String INVALID_FILENAME_CHARS = "[/\\\\?%*:|\"<>!;]";
-  /**
-   * @deprecated This field should not be publicly accessed. Use provided path validation methods instead.
-   */
-  public static final Set<String> INVALID_WINDOWS_FILENAMES = ImmutableSet
-    .of("con", "prn", "aux", "clock$", "nul", "com1", "com2", "com3", "com4", "com5", "com6", "com7", "com8", "com9", "lpt1", "lpt2",
-        "lpt3", "lpt4", "lpt5", "lpt6", "lpt7", "lpt8", "lpt9", "$mft", "$mftmirr", "$logfile", "$volume", "$attrdef", "$bitmap", "$boot",
-        "$badclus", "$secure", "$upcase", "$extend", "$quota", "$objid", "$reparse");
 
   public static final String MODULE_TEMPLATE_NAME = "NewAndroidModule";
   public static final String PROJECT_TEMPLATE_NAME = "NewAndroidProject";
