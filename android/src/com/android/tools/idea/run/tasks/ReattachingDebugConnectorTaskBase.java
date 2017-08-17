6c919dd1b2ec12f900b84581f79262a6569f28a3
Siva Velusamy <vsiva@google.com>
2017-08-17 13:46:09 -0700

    Move ReattachingDebugConnectorTaskBase to c.a.tools.idea.run.tasks
    This class was recently added, but putting it in com.google namespace
    causes another package at the top level, which seems unnecessary.
    
    Test: no functional changes, existing tests pass
    Change-Id: I3a563e8ab6000dd2d05024f31b330c4b8bcb81b4

--- android/src/com/android/tools/idea/run/tasks/ReattachingDebugConnectorTaskBase.java
+++ android/src/com/android/tools/idea/run/tasks/ReattachingDebugConnectorTaskBase.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.google.idea.blaze.android.run.test;
+package com.android.tools.idea.run.tasks;
 
 import com.android.ddmlib.AndroidDebugBridge;
 import com.android.ddmlib.Client;
