117dad53c67442f6be3a65a6077f589060b7d0bc
Lucas Smaira <lsmaira@google.com>
2017-08-23 13:42:44 +0100

    Move InstantAppRunTest.runSimpleInstantApp to unreliable
    Failing flakily.
    Test: N/A
    
    Change-Id: I04d9b47bbec976d4e00c691445efc6aef76d6ab9
    (cherry picked from commit e0c201dcb2ae0b73f7faf65f2a684ec8553038ec)

--- android-uitests/testSrc/com/android/tools/idea/tests/gui/instantapp/InstantAppRunTest.java
+++ android-uitests/testSrc/com/android/tools/idea/tests/gui/instantapp/InstantAppRunTest.java
@@ -54,7 +54,7 @@ public class InstantAppRunTest {
    * </pre>
    */
   @Test
-  @RunIn(TestGroup.QA)
+  @RunIn(TestGroup.QA_UNRELIABLE)
   public void runSimpleInstantApp() throws Exception {
     String runConfigName = "instantapp";
     IdeFrameFixture ideFrame = guiTest.importProjectAndWaitForProjectSyncToFinish("SimpleInstantApp");
