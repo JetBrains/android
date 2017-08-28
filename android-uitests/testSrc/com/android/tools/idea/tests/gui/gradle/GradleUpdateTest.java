4b3f1ceab57ce415c684d55e14faa32ad77fca75
Xin Chen <xinchan@google.com>
2017-08-28 15:37:07 -0700

    Add a new sanity test
    Test: ran this test.
    Change-Id: Idc3aaf4d314fc26960a95a3a72f655b4b8321534

---
+++ android-uitests/testSrc/com/android/tools/idea/tests/gui/gradle/GradleUpdateTest.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.tests.gui.gradle;
+
+import com.android.tools.idea.tests.gui.framework.*;
+import com.android.tools.idea.tests.gui.framework.fixture.IdeFrameFixture;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static com.google.common.truth.Truth.assertThat;
+
+@RunWith(GuiTestRunner.class)
+public class GradleUpdateTest {
+
+  @Rule public final GuiTestRule guiTest = new GuiTestRule();
+
+  /**
+   * Verifies automatic update of gradle version
+   * <p>
+   * This is run to qualify releases. Please involve the test team in substantial changes.
+   * <p>
+   * TT ID: 8a0ddb63-18e2-4c9f-82f6-1e2fa7676bba
+   * <p>
+   *   <pre>
+   *   Test Steps:
+   *   1. Import SimpleApplication
+   *   2. After a sync, a dialog forcing to this one: https://screenshot.googleplex.com/uTcvGRzLzpQ should appear
+   *   3. Click update
+   *   4. The IDE will automatically update the Gradle plugin version in build.gradle files, and trigger a sync
+   *   Verify:
+   *   Open build.gradle files and make sure that the version of the plugin was updated to <current gradle="" version="">
+   *   </pre>
+   */
+  @RunIn(TestGroup.QA)
+  @Test
+  public void updateGradleTestProject() throws Exception {
+    IdeFrameFixture ideFrameFixture = guiTest.importSimpleApplication();
+
+    // The UI test framework will update gradle properties after import.
+    // To fullfill this test, we have to restore those old gradle propreties first.
+    ideFrameFixture.getEditor()
+      .open("build.gradle")
+      .select("gradle:(.+)['\"]")
+      .enterText("2.2.0")
+      .open("gradle/wrapper/gradle-wrapper.properties")
+      .select("gradle-(.+)\\.zip")
+      .enterText("2.14.1-all");
+
+    ideFrameFixture.requestProjectSync();
+    GuiTests.findAndClickButtonWhenEnabled(ideFrameFixture.waitForDialog("Android Gradle Plugin Update Recommended"), "Update");
+    String contents = ideFrameFixture.getEditor()
+      .open("build.gradle")
+      .getCurrentFileContents();
+    assertThat(contents).doesNotContain("gradle:2.2.0");
+  }
+}
