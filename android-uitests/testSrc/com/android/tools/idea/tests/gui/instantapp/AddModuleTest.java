d03f7ad7f39248cda22f9dc4e2bc204199733691
Xin Chen <xinchan@google.com>
2017-08-29 18:04:39 -0700

    Add a sanity test
    Test: ran this test.
    Change-Id: Idc3435a34312d6326bf1cd2faa43d6a72f2ead31

--- android-uitests/testSrc/com/android/tools/idea/tests/gui/instantapp/AddModuleTest.java
+++ android-uitests/testSrc/com/android/tools/idea/tests/gui/instantapp/AddModuleTest.java
@@ -22,9 +22,14 @@ import com.android.tools.idea.tests.gui.framework.TestGroup;
 import com.android.tools.idea.tests.gui.framework.fixture.IdeFrameFixture;
 import com.android.tools.idea.tests.gui.framework.fixture.NewModuleDialogFixture;
 import com.android.tools.idea.tests.gui.framework.fixture.ProjectViewFixture;
+import com.android.tools.idea.tests.gui.framework.fixture.projectstructure.ProjectStructureDialogFixture;
+import org.fest.swing.core.MouseButton;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import static com.android.tools.idea.tests.gui.framework.fixture.MessagesToolWindowFixture.MessageMatcher.firstLineStartingWith;
+import static com.intellij.ide.errorTreeView.ErrorTreeElementKind.ERROR;
 
 @RunWith(GuiTestRunner.class)
 public class AddModuleTest {
@@ -65,6 +70,57 @@ public class AddModuleTest {
 
     ProjectViewFixture.PaneFixture androidPane = ideFrame.getProjectView().selectAndroidPane();
     androidPane.clickPath("mylibrary");
+  }
+
+  /**
+   * Verifies that gradle sync works fine with library having shrinkResources enabled on debug and release build variants
+   *
+   * <p>TT ID: 2604a622-87f1-47dc-bc2d-55c62859b3a5
+   *
+   * <pre>
+   *   Test steps:
+   *   1. Create a Project with any Activity
+   *   2. Create a Android Library module
+   *   3. Right click on app module | Module settings | Dependencies | Add Library module as dependency to app
+   *   4. Open mylibrary | build.gradle
+   *   5. Add "shrinkResources true" to release variant
+   *   6. Gradle sync (Verify)
+   *   Verify:
+   *   Error should show up that shrinker is not allowed for library modules
+   *   "Error:Resource shrinker cannot be used for libraries."
+   * </pre>
+   */
+  @Test
+  @RunIn(TestGroup.QA)
+  public void generateApkWithReleaseVariant() throws Exception {
+    IdeFrameFixture ideFrame = guiTest.importSimpleApplication();
+
+    ideFrame.invokeMenuPath("File", "New", "New Module...");
+
+    NewModuleDialogFixture.find(ideFrame)
+      .chooseModuleType("Android Library")
+      .clickNextToStep("Configure the new module")
+      .clickFinish();
+    ideFrame.waitForGradleProjectSyncToFinish();
+
+    ideFrame.getProjectView()
+      .selectAndroidPane()
+      .clickPath(MouseButton.RIGHT_BUTTON,"app")
+      .openFromMenu(ProjectStructureDialogFixture::find, "Open Module Settings")
+      .selectDependenciesTab()
+      .addModuleDependency(":mylibrary")
+      .clickOk();
+    ideFrame.waitForGradleProjectSyncToFinish();
+
+    ideFrame.getEditor()
+      .open("mylibrary/build.gradle")
+      .moveBetween("release {", "")
+      .enterText("\nshrinkResources true");
+    ideFrame.requestProjectSync();
+
+    ideFrame.getMessagesToolWindow()
+      .getGradleSyncContent()
+      .findMessage(ERROR, firstLineStartingWith("Resource shrinker cannot be used for libraries"));
   }
 
   /**
