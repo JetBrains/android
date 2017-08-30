aa824e5b8df2c29df416048863420c18a4b0cd43
Andrii Dehtiarov <adehtiarov@google.com>
2017-08-30 15:00:59 +0100

    Populate javadoc paths for dependencies.
    This CL fixes the bug when the paths of type DOCUMENTATION were
    never populated and hence the javadoc roots were never attached
    to the libraries.
    
    Now both when AAR and JAR dependencies are constructed, the
    javadoc lookup will be performed and the paths populated within
    the dependency representation accordingly.
    
    This in turn will make gradle sync add the corresponding root
    types to the library, which has the corresponding calls in place
    but they were a no-op because the documentation paths were not
    populated in the first place.
    
    Bug: b/37125917
    Test: Added LibraryDependency.testConstructorWithAarAndJavadoc and
    adjusted Android module setup tests to make sure javadoc roots are
    set when populated in the dependency, and that the lookup itself
    is not performed during setup.
    
    Change-Id: I682cb58a5231f36e3ecd85c3869708740477f034
    (cherry picked from commit e15a6593678b5f914d1e99f9a8817821e3874abe)

--- android/testSrc/com/android/tools/idea/gradle/project/sync/setup/module/android/AndroidModuleDependenciesSetupTest.java
+++ android/testSrc/com/android/tools/idea/gradle/project/sync/setup/module/android/AndroidModuleDependenciesSetupTest.java
@@ -22,6 +22,7 @@ import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.externalSystem.service.project.IdeModifiableModelsProvider;
 import com.intellij.openapi.externalSystem.service.project.IdeModifiableModelsProviderImpl;
 import com.intellij.openapi.module.Module;
+import com.intellij.openapi.roots.JavadocOrderRootType;
 import com.intellij.openapi.roots.LibraryOrderEntry;
 import com.intellij.openapi.roots.ModuleRootManager;
 import com.intellij.openapi.roots.OrderEntry;
@@ -42,7 +43,6 @@ import static com.google.common.truth.Truth.assertThat;
 import static com.intellij.openapi.roots.DependencyScope.COMPILE;
 import static com.intellij.openapi.roots.OrderRootType.CLASSES;
 import static com.intellij.openapi.roots.OrderRootType.SOURCES;
-import static com.intellij.util.ArrayUtilRt.EMPTY_FILE_ARRAY;
 import static org.mockito.Mockito.*;
 import static org.mockito.MockitoAnnotations.initMocks;
 
@@ -75,16 +75,18 @@ public class AndroidModuleDependenciesSetupTest extends IdeaTestCase {
   }
 
   public void testSetUpLibraryWithExistingLibrary() throws IOException {
-    File binaryPath = createTempFile("fakeLibrary", "jar");
-    File sourcePath = createTempFile("fakeLibrary-src", "jar");
-    Library newLibrary = createLibrary(binaryPath, sourcePath);
+    File binaryPath = createTempFile("fakeLibrary.jar", "");
+    File sourcePath = createTempFile("fakeLibrary-sources.jar", "");
+    File javadocPath = createTempFile("fakeLibrary-javadoc.jar", "");
+    Library newLibrary = createLibrary(binaryPath, sourcePath, javadocPath);
 
     String libraryName = binaryPath.getName();
     Module module = getModule();
 
     IdeModifiableModelsProvider modelsProvider = new IdeModifiableModelsProviderImpl(getProject());
     File[] binaryPaths = {binaryPath};
-    myDependenciesSetup.setUpLibraryDependency(module, modelsProvider, libraryName, COMPILE, binaryPath, binaryPaths, EMPTY_FILE_ARRAY,
+    File[] documentationPaths = {javadocPath};
+    myDependenciesSetup.setUpLibraryDependency(module, modelsProvider, libraryName, COMPILE, binaryPath, binaryPaths, documentationPaths,
                                                false);
     ApplicationManager.getApplication().runWriteAction(modelsProvider::commit); // Apply changes before checking state.
 
@@ -94,11 +96,13 @@ public class AndroidModuleDependenciesSetupTest extends IdeaTestCase {
     assertSame(newLibrary, libraryOrderEntry.getLibrary()); // The existing library should not have been changed.
 
     verify(myLibraryRegistry).markAsUsed(newLibrary, binaryPaths);
-    verify(myLibraryFilePaths, never()).findSourceJarPath(binaryPath); // Should not attemp to look up sources for existing libraries.
+    // Should not attempt to look up sources and documentation for existing libraries.
+    verify(myLibraryFilePaths, never()).findSourceJarPath(binaryPath);
+    verify(myLibraryFilePaths, never()).findJavadocJarPath(javadocPath);
   }
 
   @NotNull
-  private Library createLibrary(@NotNull File binaryPath, @NotNull File sourcePath) {
+  private Library createLibrary(@NotNull File binaryPath, @NotNull File sourcePath, @NotNull File javadocPath) {
     LibraryTable libraryTable = ProjectLibraryTable.getInstance(getProject());
     LibraryTable.ModifiableModel libraryTableModel = libraryTable.getModifiableModel();
     Library library = libraryTableModel.createLibrary(binaryPath.getName());
@@ -109,6 +113,7 @@ public class AndroidModuleDependenciesSetupTest extends IdeaTestCase {
     Library.ModifiableModel libraryModel = library.getModifiableModel();
     libraryModel.addRoot(pathToIdeaUrl(binaryPath), CLASSES);
     libraryModel.addRoot(pathToIdeaUrl(sourcePath), SOURCES);
+    libraryModel.addRoot(pathToIdeaUrl(javadocPath), JavadocOrderRootType.getInstance());
 
     application.runWriteAction(libraryModel::commit);
 
@@ -116,8 +121,9 @@ public class AndroidModuleDependenciesSetupTest extends IdeaTestCase {
   }
 
   public void testSetUpLibraryWithNewLibrary() throws IOException {
-    File binaryPath = createTempFile("fakeLibrary", "jar");
-    File sourcePath = createTempFile("fakeLibrary-src", "jar");
+    File binaryPath = createTempFile("fakeLibrary.jar", "");
+    File sourcePath = createTempFile("fakeLibrary-sources.jar", "");
+    File javadocPath = createTempFile("fakeLibrary-javadoc.jar", "");
     when(myLibraryFilePaths.findSourceJarPath(binaryPath)).thenReturn(sourcePath);
 
     String libraryName = binaryPath.getName();
@@ -125,7 +131,8 @@ public class AndroidModuleDependenciesSetupTest extends IdeaTestCase {
 
     IdeModifiableModelsProvider modelsProvider = new IdeModifiableModelsProviderImpl(getProject());
     File[] binaryPaths = {binaryPath};
-    myDependenciesSetup.setUpLibraryDependency(module, modelsProvider, libraryName, COMPILE, binaryPath, binaryPaths, EMPTY_FILE_ARRAY,
+    File[] documentationPaths = {javadocPath};
+    myDependenciesSetup.setUpLibraryDependency(module, modelsProvider, libraryName, COMPILE, binaryPath, binaryPaths, documentationPaths,
                                                false);
     ApplicationManager.getApplication().runWriteAction(modelsProvider::commit); // Apply changes before checking state.
 
@@ -148,8 +155,14 @@ public class AndroidModuleDependenciesSetupTest extends IdeaTestCase {
     assertThat(sourceUrls).hasLength(1);
     assertEquals(pathToIdeaUrl(sourcePath), sourceUrls[0]);
 
+    String[] javadocUrls = library.getUrls(JavadocOrderRootType.getInstance());
+    assertThat(javadocUrls).hasLength(1);
+    assertEquals(pathToIdeaUrl(javadocPath), javadocUrls[0]);
+
     verify(myLibraryRegistry, never()).markAsUsed(library, binaryPaths); // Should not mark new libraries as "used"
     verify(myLibraryFilePaths).findSourceJarPath(binaryPath);
+    // Documentation paths are populated at the LibraryDependency level - no look-up to be done during setup itself
+    verify(myLibraryFilePaths, never()).findJavadocJarPath(javadocPath);
   }
 
   @NotNull
