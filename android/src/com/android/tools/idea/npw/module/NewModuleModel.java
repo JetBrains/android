22f46a4553ac2ce661816ae10ed009c727a57ba6
parentej <parentej@google.com>
2017-08-31 12:37:52 +0100

    [UI Tests] NewInstantAppModuleTest failing
    app_name String was not being created, when adding a new instant app to
    a "non instant app" project and the "Instant App Wrapper" gradle file
    was not created too.
    The copy of the Strings and creating of the "Instant App Wrapper" is
    controlled by ATTR_HAS_INSTANT_APP_WRAPPER and it needs to be true when
    a new project doesn't have a "Instant App Base Module" yet.
    
    Bug: 65237037
    Test: NewInstantAppModuleTest pass
    Change-Id: I0f2bd2cc81a5bacd283c4fd88c23441cfe1518a9

--- android/src/com/android/tools/idea/npw/module/NewModuleModel.java
+++ android/src/com/android/tools/idea/npw/module/NewModuleModel.java
@@ -222,6 +222,7 @@ public final class NewModuleModel extends WizardModel {
       if (myIsInstantApp.get()) {
         myTemplateValues.put(ATTR_INSTANT_APP_PACKAGE_NAME, myProjectPackageName.get());
 
+        boolean hasInstantAppWrapper = false;
         if (renderTemplateValues != null) {
           renderTemplateValues.put(ATTR_IS_LIBRARY_MODULE, true);
 
@@ -234,6 +235,7 @@ public final class NewModuleModel extends WizardModel {
           if (myCreateInExistingProject) {
             Module baseFeature = InstantApps.findBaseFeature(project);
             if (baseFeature == null) {
+              hasInstantAppWrapper = true;
               baseModuleRoot = new File(projectRoot, myModuleName.get());
               baseModuleResourceRoot = new File(baseModuleRoot, defaultResourceSuffix);
               renderTemplateValues.put(ATTR_IS_BASE_FEATURE, true);
@@ -262,7 +264,7 @@ public final class NewModuleModel extends WizardModel {
 
         if (myCreateInExistingProject) {
           myTemplateValues.put(ATTR_HAS_MONOLITHIC_APP_WRAPPER, false);
-          myTemplateValues.put(ATTR_HAS_INSTANT_APP_WRAPPER, false);
+          myTemplateValues.put(ATTR_HAS_INSTANT_APP_WRAPPER, hasInstantAppWrapper);
         }
       }
 
