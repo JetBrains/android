0f877ebd57e35bd2392e3588d527faf08399bdb7
alruiz <alruiz@google.com>
2017-09-01 11:58:47 -0700

    Now generated files marked as "source" are writable.
    Otherwise Android-specific refactoring won't work.
    
    Bug: 65032914
    Test: JUnit tests.
    Change-Id: Ia2f51bfed444056e6c8809583bee4d844b7bf54c
    (cherry picked from commit eb5c995bfe0d6d26ae44b19f6a21e4713bc78b2f)

--- android/src/com/android/tools/idea/gradle/editor/GeneratedFileWritingAccessProvider.java
+++ android/src/com/android/tools/idea/gradle/editor/GeneratedFileWritingAccessProvider.java
@@ -17,6 +17,7 @@ package com.android.tools.idea.gradle.editor;
 
 import com.android.tools.idea.gradle.project.GradleProjectInfo;
 import com.android.tools.idea.gradle.project.model.AndroidModuleModel;
+import com.intellij.openapi.roots.ProjectFileIndex;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.openapi.vfs.WritingAccessProvider;
 import org.jetbrains.annotations.NotNull;
@@ -34,9 +35,11 @@ import static com.intellij.openapi.vfs.VfsUtilCore.isAncestor;
  */
 public class GeneratedFileWritingAccessProvider extends WritingAccessProvider {
   @NotNull private final GradleProjectInfo myProjectInfo;
+  @NotNull private final ProjectFileIndex myProjectFileIndex;
 
-  public GeneratedFileWritingAccessProvider(@NotNull GradleProjectInfo projectInfo) {
+  public GeneratedFileWritingAccessProvider(@NotNull GradleProjectInfo projectInfo, @NotNull ProjectFileIndex projectFileIndex) {
     myProjectInfo = projectInfo;
+    myProjectFileIndex = projectFileIndex;
   }
 
   @Override
@@ -45,7 +48,7 @@ public class GeneratedFileWritingAccessProvider extends WritingAccessProvider {
     List<VirtualFile> readOnlyFiles = new ArrayList<>();
 
     for (VirtualFile file : files) {
-      if (isInBuildFolder(file)) {
+      if (isInBuildFolderAndNotSource(file)) {
         readOnlyFiles.add(file);
       }
     }
@@ -55,10 +58,15 @@ public class GeneratedFileWritingAccessProvider extends WritingAccessProvider {
 
   @Override
   public boolean isPotentiallyWritable(@NotNull VirtualFile file) {
-    return !isInBuildFolder(file);
+    return !isInBuildFolderAndNotSource(file);
   }
 
-  private boolean isInBuildFolder(@NotNull VirtualFile file) {
+  private boolean isInBuildFolderAndNotSource(@NotNull VirtualFile file) {
+    if (myProjectFileIndex.isInSource(file)) {
+      // The file might be in the "build" folder, but it can be in a folder that is marked as "generated source". Do not make read-only.
+      // See https://issuetracker.google.com/65032914
+      return false;
+    }
     AndroidModuleModel androidModel = myProjectInfo.findAndroidModelInModule(file, false /* include excluded files */);
     if (androidModel == null) {
       return false;
