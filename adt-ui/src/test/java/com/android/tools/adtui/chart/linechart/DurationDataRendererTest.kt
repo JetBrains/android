b658553352dcb02925c8c6ad6323d64af758e4cf
Billy Lam <shiufai@google.com>
2017-09-07 12:25:28 -0700

    Handle dash phase update for custom line configs
    Bug: 65452030
    Test: unit test
    Change-Id: Ic5114a969081258130dda3648e1423508ea5e042

--- adt-ui/src/test/java/com/android/tools/adtui/chart/linechart/DurationDataRendererTest.kt
+++ adt-ui/src/test/java/com/android/tools/adtui/chart/linechart/DurationDataRendererTest.kt
@@ -21,7 +21,10 @@ import org.junit.Test
 import org.mockito.Mockito.`when`
 import org.mockito.Mockito.mock
 import java.awt.Color
+import java.awt.Graphics2D
+import java.awt.geom.Path2D
 import java.awt.geom.Rectangle2D
+import java.util.*
 import javax.swing.Icon
 
 const val EPSILON: Float = 1e-6f
@@ -70,6 +73,43 @@ class DurationDataRendererTest {
     validateRegion(regions[2], 0.4f, 0.6f, 5f, 5f)
     validateRegion(regions[3], 0.6f, 0.4f, 5f, 5f)
     validateRegion(regions[4], 0.8f, 1f, 5f, 5f)   // attached series has no data after this point. y == 1.
+  }
+
+  @Test
+  fun testDashPhaseMatchingOnCustomConfig() {
+    val xRange = Range(0.0, 10.0)
+    val yRange = Range(0.0, 10.0)
+    val series1 = DefaultDataSeries<Long>()
+    val series2 = DefaultDataSeries<Long>()
+    val rangeSeries1 = RangedContinuousSeries("test1", xRange, yRange, series1)
+    val rangeSeries2 = RangedContinuousSeries("test2", xRange, yRange, series2)
+    var lineChart = LineChart(Arrays.asList(rangeSeries1, rangeSeries2))
+    lineChart.configure(rangeSeries1, LineConfig(Color.ORANGE).setStroke(LineConfig.DEFAULT_DASH_STROKE))
+    lineChart.configure(rangeSeries2, LineConfig(Color.PINK).setStroke(LineConfig.DEFAULT_DASH_STROKE))
+
+    val dataSeries = DefaultDataSeries<DurationData>()
+    val durationData = DurationDataModel(RangedSeries(xRange, dataSeries))
+    val durationDataRenderer = DurationDataRenderer.Builder(durationData, Color.BLACK).build()
+    durationDataRenderer.addCustomLineConfig(rangeSeries1, LineConfig(Color.BLUE).setStroke(LineConfig.DEFAULT_DASH_STROKE))
+    durationDataRenderer.addCustomLineConfig(rangeSeries2, LineConfig(Color.YELLOW))
+
+    // Fake a dash phase update on the default LineConfig
+    lineChart.getLineConfig(rangeSeries1).adjustedDashPhase = 0.25
+    lineChart.getLineConfig(rangeSeries2).adjustedDashPhase = 0.75
+
+    assertThat(lineChart.getLineConfig(rangeSeries1).adjustedDashPhase).isWithin(EPSILON.toDouble()).of(0.25)
+    assertThat(lineChart.getLineConfig(rangeSeries2).adjustedDashPhase).isWithin(EPSILON.toDouble()).of(0.75)
+    assertThat(durationDataRenderer.getCustomLineConfig(rangeSeries1).adjustedDashPhase).isWithin(EPSILON.toDouble()).of(0.0)
+    assertThat(durationDataRenderer.getCustomLineConfig(rangeSeries2).adjustedDashPhase).isWithin(EPSILON.toDouble()).of(0.0)
+
+    // Fake a renderLines call then check that the dash phase on the custom LineConfig has been updated.
+    durationDataRenderer.renderLines(lineChart,
+        mock(Graphics2D::class.java),
+        Collections.singletonList(Path2D.Float()) as List<Path2D>,
+        Arrays.asList(rangeSeries1, rangeSeries2))
+    assertThat(durationDataRenderer.getCustomLineConfig(rangeSeries1).adjustedDashPhase).isWithin(EPSILON.toDouble()).of(0.25)
+    // rangeSeries2 isn't updated as the custom LineConfig is not a dash stroke.
+    assertThat(durationDataRenderer.getCustomLineConfig(rangeSeries2).adjustedDashPhase).isWithin(EPSILON.toDouble()).of(0.0)
   }
 
   private fun validateRegion(rect: Rectangle2D.Float, xStart: Float, yStart: Float, width: Float, height: Float) {
