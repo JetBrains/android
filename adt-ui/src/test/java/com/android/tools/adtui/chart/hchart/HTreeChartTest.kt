f728c1f54be78c131a2aa230673d3c48be7787dc
Madiyar Aitbayev <madiyar@google.com>
2017-09-11 11:24:21 +0100

    Replace optional to lateinit in HTreeChartTest
    Test: refactoring of a test.
    Change-Id: I1290de80f123ce7fcbeb530026a8bd9dff57ad0c

--- adt-ui/src/test/java/com/android/tools/adtui/chart/hchart/HTreeChartTest.kt
+++ adt-ui/src/test/java/com/android/tools/adtui/chart/hchart/HTreeChartTest.kt
@@ -25,10 +25,9 @@ import java.awt.*
 import com.google.common.truth.Truth.assertThat
 
 class HTreeChartTest {
-
-  private var myUi: FakeUi? = null
-  private var myChart: HTreeChart<String>? = null
-  private var myRange: Range? = null
+  private lateinit var myUi: FakeUi
+  private lateinit var myChart: HTreeChart<String>
+  private lateinit var myRange: Range
 
   @Before
   fun setUp() {
@@ -38,95 +37,95 @@ class HTreeChartTest {
   private fun setUp(orientation: HTreeChart.Orientation) {
     myRange = Range(0.0, 100.0)
     myChart = HTreeChart<String>(myRange, orientation)
-    myChart!!.size = Dimension(100, 100)
-    myUi = FakeUi(myChart!!)
-    myChart!!.yRange.set(10.0, 10.0)
+    myChart.size = Dimension(100, 100)
+    myUi = FakeUi(myChart)
+    myChart.yRange.set(10.0, 10.0)
   }
 
   @Test
   fun testMouseDragToEast() {
-    assertThat(myRange!!.min).isWithin(EPSILON).of(0.0)
-    assertThat(myRange!!.max).isWithin(EPSILON).of(100.0)
+    assertThat(myRange.min).isWithin(EPSILON).of(0.0)
+    assertThat(myRange.max).isWithin(EPSILON).of(100.0)
 
-    myUi!!.mouse.press(5, 5)
-    myUi!!.mouse.dragTo(10, 5)
+    myUi.mouse.press(5, 5)
+    myUi.mouse.dragTo(10, 5)
 
-    assertThat(myRange!!.min).isWithin(EPSILON).of(-5.0)
-    assertThat(myRange!!.max).isWithin(EPSILON).of(95.0)
+    assertThat(myRange.min).isWithin(EPSILON).of(-5.0)
+    assertThat(myRange.max).isWithin(EPSILON).of(95.0)
   }
 
   @Test
   fun testMouseDragToWest() {
-    assertThat(myRange!!.min).isWithin(EPSILON).of(0.0)
-    assertThat(myRange!!.max).isWithin(EPSILON).of(100.0)
-    myUi!!.mouse.press(5, 5)
-    myUi!!.mouse.dragTo(3, 5)
-    assertThat(myRange!!.min).isWithin(EPSILON).of(2.0)
-    assertThat(myRange!!.max).isWithin(EPSILON).of(102.0)
+    assertThat(myRange.min).isWithin(EPSILON).of(0.0)
+    assertThat(myRange.max).isWithin(EPSILON).of(100.0)
+    myUi.mouse.press(5, 5)
+    myUi.mouse.dragTo(3, 5)
+    assertThat(myRange.min).isWithin(EPSILON).of(2.0)
+    assertThat(myRange.max).isWithin(EPSILON).of(102.0)
   }
 
   @Test
   fun testMouseDragToSouth() {
-    assertThat(myChart!!.yRange.min).isWithin(EPSILON).of(10.0)
-    assertThat(myChart!!.yRange.max).isWithin(EPSILON).of(10.0)
+    assertThat(myChart.yRange.min).isWithin(EPSILON).of(10.0)
+    assertThat(myChart.yRange.max).isWithin(EPSILON).of(10.0)
 
-    myUi!!.mouse.press(5, 5)
-    myUi!!.mouse.dragTo(5, 7)
+    myUi.mouse.press(5, 5)
+    myUi.mouse.dragTo(5, 7)
 
-    assertThat(myChart!!.yRange.min).isWithin(EPSILON).of(12.0)
-    assertThat(myChart!!.yRange.max).isWithin(EPSILON).of(12.0)
+    assertThat(myChart.yRange.min).isWithin(EPSILON).of(12.0)
+    assertThat(myChart.yRange.max).isWithin(EPSILON).of(12.0)
   }
 
   @Test
   fun testMouseDragToNorth() {
-    assertThat(myChart!!.yRange.min).isWithin(EPSILON).of(10.0)
-    assertThat(myChart!!.yRange.max).isWithin(EPSILON).of(10.0)
+    assertThat(myChart.yRange.min).isWithin(EPSILON).of(10.0)
+    assertThat(myChart.yRange.max).isWithin(EPSILON).of(10.0)
 
-    myUi!!.mouse.press(5, 5)
-    myUi!!.mouse.dragTo(5, 1)
+    myUi.mouse.press(5, 5)
+    myUi.mouse.dragTo(5, 1)
 
-    assertThat(myChart!!.yRange.min).isWithin(EPSILON).of(6.0)
-    assertThat(myChart!!.yRange.max).isWithin(EPSILON).of(6.0)
+    assertThat(myChart.yRange.min).isWithin(EPSILON).of(6.0)
+    assertThat(myChart.yRange.max).isWithin(EPSILON).of(6.0)
   }
 
   @Test
   fun testMouseDragEastSouth() {
-    assertThat(myRange!!.min).isWithin(EPSILON).of(0.0)
-    assertThat(myRange!!.max).isWithin(EPSILON).of(100.0)
-    assertThat(myChart!!.yRange.min).isWithin(EPSILON).of(10.0)
-    assertThat(myChart!!.yRange.max).isWithin(EPSILON).of(10.0)
+    assertThat(myRange.min).isWithin(EPSILON).of(0.0)
+    assertThat(myRange.max).isWithin(EPSILON).of(100.0)
+    assertThat(myChart.yRange.min).isWithin(EPSILON).of(10.0)
+    assertThat(myChart.yRange.max).isWithin(EPSILON).of(10.0)
 
-    myUi!!.mouse.press(5, 5)
-    myUi!!.mouse.dragTo(7, 10)
+    myUi.mouse.press(5, 5)
+    myUi.mouse.dragTo(7, 10)
 
-    assertThat(myRange!!.min).isWithin(EPSILON).of(-2.0)
-    assertThat(myRange!!.max).isWithin(EPSILON).of(98.0)
-    assertThat(myChart!!.yRange.min).isWithin(EPSILON).of(15.0)
-    assertThat(myChart!!.yRange.max).isWithin(EPSILON).of(15.0)
+    assertThat(myRange.min).isWithin(EPSILON).of(-2.0)
+    assertThat(myRange.max).isWithin(EPSILON).of(98.0)
+    assertThat(myChart.yRange.min).isWithin(EPSILON).of(15.0)
+    assertThat(myChart.yRange.max).isWithin(EPSILON).of(15.0)
   }
 
   @Test
   fun testTopDownChartDragToEast() {
     setUp(HTreeChart.Orientation.TOP_DOWN)
 
-    assertThat(myChart!!.yRange.min).isWithin(EPSILON).of(10.0)
-    assertThat(myChart!!.yRange.max).isWithin(EPSILON).of(10.0)
-    myUi!!.mouse.press(5, 5)
-    myUi!!.mouse.dragTo(5, 10)
-    assertThat(myChart!!.yRange.min).isWithin(EPSILON).of(5.0)
-    assertThat(myChart!!.yRange.max).isWithin(EPSILON).of(5.0)
+    assertThat(myChart.yRange.min).isWithin(EPSILON).of(10.0)
+    assertThat(myChart.yRange.max).isWithin(EPSILON).of(10.0)
+    myUi.mouse.press(5, 5)
+    myUi.mouse.dragTo(5, 10)
+    assertThat(myChart.yRange.min).isWithin(EPSILON).of(5.0)
+    assertThat(myChart.yRange.max).isWithin(EPSILON).of(5.0)
   }
 
   @Test
   fun testTopDownChartDragToWest() {
     setUp(HTreeChart.Orientation.TOP_DOWN)
 
-    assertThat(myChart!!.yRange.min).isWithin(EPSILON).of(10.0)
-    assertThat(myChart!!.yRange.max).isWithin(EPSILON).of(10.0)
-    myUi!!.mouse.press(5, 5)
-    myUi!!.mouse.dragTo(5, 0)
-    assertThat(myChart!!.yRange.min).isWithin(EPSILON).of(15.0)
-    assertThat(myChart!!.yRange.max).isWithin(EPSILON).of(15.0)
+    assertThat(myChart.yRange.min).isWithin(EPSILON).of(10.0)
+    assertThat(myChart.yRange.max).isWithin(EPSILON).of(10.0)
+    myUi.mouse.press(5, 5)
+    myUi.mouse.dragTo(5, 0)
+    assertThat(myChart.yRange.min).isWithin(EPSILON).of(15.0)
+    assertThat(myChart.yRange.max).isWithin(EPSILON).of(15.0)
   }
 
   companion object {
