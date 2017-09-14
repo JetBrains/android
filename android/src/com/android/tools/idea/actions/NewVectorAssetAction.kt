3941d4e9b770e7a0681ee2e022fb736469331136
Stefan Xenos <sxenos@google.com>
2017-09-14 12:48:18 -0700

    Use build-system-independent check for vector-to-PNG generation
    Currently, the vector asset generator checks whether the gradle
    plugin in use by the project supports automatic conversion of svg
    to raster assets. This CL adds a new API to the project system
    that provides this information (whether the PS supports
    svg-to-rasterization). The existing check specific to Gradle
    has been moved into project-system-gradle.
    
    Test: Manually ran the new vector asset command
    Bug: 64976974
    Change-Id: I33bf256e11c5d1c43efc31e0c546fe503ebb6847

---
+++ android/src/com/android/tools/idea/actions/NewVectorAssetAction.kt
@@ -0,0 +1,71 @@
+/*
+ * Copyright (C) 2015 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.actions
+
+import com.android.tools.idea.gradle.project.model.AndroidModuleModel
+import com.android.tools.idea.npw.assetstudio.wizard.GenerateVectorIconModel
+import com.android.tools.idea.npw.assetstudio.wizard.NewVectorAssetStep
+import com.android.tools.idea.projectsystem.CapabilityNotSupported
+import com.android.tools.idea.projectsystem.getProjectSystem
+import com.android.tools.idea.ui.wizard.WizardUtils
+import com.android.tools.idea.wizard.model.ModelWizard
+import com.intellij.openapi.ui.Messages
+import com.intellij.util.ui.JBUI
+import org.jetbrains.android.facet.AndroidFacet
+import java.awt.Dimension
+import java.net.URL
+
+/**
+ * Action to invoke the Vector Asset Studio. This will allow the user to generate icons using SVGs.
+ */
+class NewVectorAssetAction : AndroidAssetStudioAction("Vector Asset", "Open Vector Asset Studio to create an image asset") {
+
+  override fun createWizard(facet: AndroidFacet): ModelWizard? {
+    val module = facet.module
+    val status = module.project.getProjectSystem().canGeneratePngFromVectorGraphics(module)
+    if (status is CapabilityNotSupported) {
+      val androidModel = AndroidModuleModel.get(module)
+      if (androidModel != null) {
+        val minSdkVersion = androidModel.minSdkVersion
+
+        if (minSdkVersion == null || minSdkVersion.apiLevel < VECTOR_DRAWABLE_API_LEVEL) {
+          Messages.showErrorDialog(module.project, status.message, status.title)
+          return null
+        }
+      }
+    }
+
+    val wizardBuilder = ModelWizard.Builder()
+    wizardBuilder.addStep(NewVectorAssetStep(GenerateVectorIconModel(facet), facet))
+    return wizardBuilder.build()
+  }
+
+  override fun getWizardMinimumSize(): Dimension {
+    return JBUI.size(700, 500)
+  }
+
+  override fun getWizardPreferredSize(): Dimension {
+    return wizardMinimumSize
+  }
+
+  override fun getHelpUrl(): URL? {
+    return WizardUtils.toUrl("http://developer.android.com/tools/help/vector-asset-studio.html")
+  }
+
+  companion object {
+    private const val VECTOR_DRAWABLE_API_LEVEL = 21
+  }
+}
---
+++ project-system/src/com/android/tools/idea/projectsystem/CapabilityStatus.kt
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.projectsystem
+
+import org.jetbrains.annotations.Nls
+
+/**
+ * Holds the status of a project system capability
+ */
+sealed class CapabilityStatus
+
+/**
+ * Indicates that the capability is supported by the project system
+ */
+class CapabilitySupported : CapabilityStatus()
+
+/**
+ * Indicates that the capability is not supported by the project system. Includes a user-readable message
+ * explaining that the capability is unsupported.
+ */
+open class CapabilityNotSupported(@Nls val message: String = "The build system for this project does not support this feature",
+                                  @Nls(capitalization = Nls.Capitalization.Title) val title: String = "Unsupported Capability") : CapabilityStatus()
+
+/**
+ * Indicates that the capability is not supported by the current version of the build system,
+ * but would be available if it were upgraded to a newer version.
+ */
+class CapabilityUpgradeRequired(@Nls message: String,
+                                @Nls(capitalization = Nls.Capitalization.Title) title: String)
+  : CapabilityNotSupported(message, title)
