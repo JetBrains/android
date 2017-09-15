514e2ee4ba66c7eacec1180d513190dc579630dc
Kelvin Ma <kelvinhanma@google.com>
2017-09-15 13:23:22 -0700

    refactor AndroidRunConfigurationBase.getState
    The method is long (> 150 lines) and handles a lot of logic for Instant
    Run.
    
    changes to clean up the method:
    - mark variables final if possible
    - move code into helper methods with clear names explaining its purpose
    - move variable declaration to right before they are used
    - clean up code paths as much as possible
    
    While the method is still ~100 lines. The purpose of the lines are more
    obvious now:
    - if IR enabled and has existing session -> get deploy target from IR
    session
    - if no target yet, pick target
    - if IR enabled, check gradle support
    - if debug, check debug
    - create LaunchTasksProviderFactory
    
    I would like to write unit tests against AndroidRunConfigurationBase
    class but it's tricky due to the various uses of static methods.
    
    Fixes: b/62479087
    Test: existing UI tests should cover IR flows, added tests for various
    methods in AndroidRunConfigurationBase
    Change-Id: I069883657d84615f5f187a422b76ff400f977f93

---
+++ android/testSrc/com/android/tools/idea/run/AndroidTestRunConfiguration.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.run;
+
+import com.android.tools.idea.run.tasks.LaunchTask;
+import com.android.tools.idea.run.util.LaunchStatus;
+import com.intellij.execution.configurations.ConfigurationFactory;
+import com.intellij.execution.configurations.RunConfiguration;
+import com.intellij.openapi.options.SettingsEditor;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.Pair;
+import org.jetbrains.android.facet.AndroidFacet;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+/**
+ * Child class of {@link AndroidRunConfigurationBase} to test instance methods.
+ * All override methods are default genereated methods.
+ */
+public class AndroidTestRunConfiguration extends AndroidRunConfigurationBase {
+  public AndroidTestRunConfiguration(Project project,
+                                     ConfigurationFactory factory, boolean androidTests) {
+    super(project, factory, androidTests);
+  }
+
+  @Override
+  protected Pair<Boolean, String> supportsRunningLibraryProjects(@NotNull AndroidFacet facet) {
+    return null;
+  }
+
+  @NotNull
+  @Override
+  protected List<ValidationError> checkConfiguration(@NotNull AndroidFacet facet) {
+    return null;
+  }
+
+  @NotNull
+  @Override
+  protected ApkProvider getApkProvider(@NotNull AndroidFacet facet, @NotNull ApplicationIdProvider applicationIdProvider) {
+    return null;
+  }
+
+  @NotNull
+  @Override
+  protected ConsoleProvider getConsoleProvider() {
+    return null;
+  }
+
+  @Nullable
+  @Override
+  protected LaunchTask getApplicationLaunchTask(@NotNull ApplicationIdProvider applicationIdProvider,
+                                                @NotNull AndroidFacet facet,
+                                                boolean waitForDebugger,
+                                                @NotNull LaunchStatus launchStatus) {
+    return null;
+  }
+
+  @Override
+  protected boolean supportMultipleDevices() {
+    return false;
+  }
+
+  @NotNull
+  @Override
+  public SettingsEditor<? extends RunConfiguration> getConfigurationEditor() {
+    return null;
+  }
+}
