d93b53a17493dab83ab6eebde14b01839644be51
Lucas Smaira <lsmaira@google.com>
2017-09-15 14:59:53 +0100

    [AIA] Fix manifest merge for feature modules
    When merging the manifest, if the project type is "feature", its
    manifest should be merged as an application merge (libraries manifests
    should be merged in it) and not as a library merge.
    
    This CL guarantees the manifest is merged correctly for features.
    
    Test: FeatureMergedManifestTest
    Fixes bug 65366849
    
    Change-Id: I2fdfdd692bd22308ac61f9c4b95711c77ac3f64e

--- android-test-framework/testSrc/com/android/tools/idea/testing/TestProjectPaths.java
+++ android-test-framework/testSrc/com/android/tools/idea/testing/TestProjectPaths.java
@@ -14,6 +14,7 @@ public final class TestProjectPaths {
   public static final String HELLO_JNI = "projects/helloJni";
   public static final String IMPORTING = "projects/importing";
   public static final String INSTANT_APP = "projects/instantApp";
+  public static final String INSTANT_APP_LIBRARY_DEPENDENCY = "projects/instantAppLibraryDependency";
   public static final String INSTRUMENTATION_RUNNER = "projects/instrumentationRunner";
   public static final String INSTRUMENTATION_RUNNER_ANDROID_TEST_ORCHESTRATOR = "projects/instrumentationRunnerAndroidTestOrchestrator";
   public static final String JAVA_LIB = "projects/javaLib";
