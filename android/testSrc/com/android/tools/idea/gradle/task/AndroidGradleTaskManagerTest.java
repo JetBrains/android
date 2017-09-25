1dc3f9c88a60b0458e8c9d370ec65308c4e0bfa8
Vladislav.Soroka <Vladislav.Soroka@jetbrains.com>
2017-09-25 15:45:26 +0300

    Android: tests fixes

--- android/testSrc/com/android/tools/idea/gradle/task/AndroidGradleTaskManagerTest.java
+++ android/testSrc/com/android/tools/idea/gradle/task/AndroidGradleTaskManagerTest.java
@@ -2,9 +2,16 @@ package com.android.tools.idea.gradle.task;
 
 import com.android.tools.idea.gradle.project.build.compiler.AndroidGradleBuildConfiguration;
 import com.android.tools.idea.gradle.project.build.invoker.GradleBuildInvoker;
+import com.android.tools.idea.gradle.project.facet.gradle.GradleFacet;
+import com.android.tools.idea.gradle.util.GradleUtil;
 import com.android.tools.idea.project.AndroidProjectInfo;
+import com.intellij.facet.FacetManager;
+import com.intellij.openapi.externalSystem.ExternalSystemModulePropertyManager;
+import com.intellij.openapi.externalSystem.model.project.ModuleData;
 import com.intellij.openapi.externalSystem.model.task.ExternalSystemTaskId;
 import com.intellij.openapi.externalSystem.model.task.ExternalSystemTaskNotificationListenerAdapter;
+import com.intellij.openapi.module.Module;
+import com.intellij.openapi.module.ModuleManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.util.containers.ContainerUtil;
 import org.junit.Test;
@@ -38,9 +45,22 @@ public class AndroidGradleTaskManagerTest {
     when(picoContainer.getComponentInstance(AndroidGradleBuildConfiguration.class.getName())).thenReturn(androidGradleBuildConfiguration);
     when(picoContainer.getComponentInstance(GradleBuildInvoker.class.getName())).thenReturn(gradleBuildInvoker);
     when(gradleBuildInvoker.getProject()).thenReturn(project);
-
-    List<String> taskNames = ContainerUtil.list("fooTask");
+    ModuleManager moduleManager = mock(ModuleManager.class);
+    when(project.getComponent(ModuleManager.class)).thenReturn(moduleManager);
+    Module module = mock(Module.class);
+    when(moduleManager.getModules()).thenReturn(new Module[]{module});
+    when(module.getPicoContainer()).thenReturn(picoContainer);
+    when(module.getProject()).thenReturn(project);
+    FacetManager facetManager = mock(FacetManager.class);
+    when(facetManager.getFacetByType(GradleFacet.getFacetTypeId())).thenReturn(mock(GradleFacet.class));
+    when(module.getComponent(FacetManager.class)).thenReturn(facetManager);
+    ExternalSystemModulePropertyManager modulePropertyManager = new ExternalSystemModulePropertyManager(module);
+    when(picoContainer.getComponentInstance(ExternalSystemModulePropertyManager.class.getName())).thenReturn(modulePropertyManager);
     String projectPath = "projectPath";
+    modulePropertyManager.setExternalOptions(GradleUtil.GRADLE_SYSTEM_ID, new ModuleData("", GradleUtil.GRADLE_SYSTEM_ID,
+                                                                                         "", "", "", projectPath), null);
+    when(module.getOptionValue("external.linked.project.path")).thenReturn(projectPath);
+    List<String> taskNames = ContainerUtil.list("fooTask");
     ExternalSystemTaskNotificationListenerAdapter listener = new ExternalSystemTaskNotificationListenerAdapter() {};
 
     new AndroidGradleTaskManager().executeTasks(taskId, taskNames, projectPath, null, null, listener);
