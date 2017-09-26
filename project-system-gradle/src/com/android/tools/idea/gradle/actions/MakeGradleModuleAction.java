bc943be488bb4622618346146c8cc6ea8ecf433e
Stefan Xenos <sxenos@google.com>
2017-09-26 08:51:49 -0700

    Move gradle action classes to project-system-gradle
    Test: Confirmed that the actions work and appear in the correct location
    Bug: 66920262
    Change-Id: If59a41c782ef77a5a352ab2dc7e0b3b0edefd4e9

--- project-system-gradle/src/com/android/tools/idea/gradle/actions/MakeGradleModuleAction.java
+++ project-system-gradle/src/com/android/tools/idea/gradle/actions/MakeGradleModuleAction.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2013 The Android Open Source Project
+ * Copyright (C) 2017 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,17 +15,15 @@
  */
 package com.android.tools.idea.gradle.actions;
 
+import com.android.tools.idea.actions.MakeIdeaModuleAction;
 import com.android.tools.idea.gradle.project.build.invoker.GradleBuildInvoker;
 import com.android.tools.idea.gradle.project.build.invoker.TestCompileType;
 import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.actionSystem.DataContext;
-import com.intellij.openapi.actionSystem.Presentation;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.project.Project;
 import org.jetbrains.annotations.NotNull;
 
 import static com.android.tools.idea.gradle.util.GradleProjects.getModulesToBuildFromSelection;
-import static com.intellij.openapi.actionSystem.ActionPlaces.PROJECT_VIEW_POPUP;
 
 public class MakeGradleModuleAction extends AndroidStudioGradleAction {
   public MakeGradleModuleAction() {
@@ -34,42 +32,7 @@ public class MakeGradleModuleAction extends AndroidStudioGradleAction {
 
   @Override
   protected void doUpdate(@NotNull AnActionEvent e, @NotNull Project project) {
-    updatePresentation(e, project);
-  }
-
-  public static void updatePresentation(@NotNull AnActionEvent e, @NotNull Project project) {
-    DataContext dataContext = e.getDataContext();
-
-    Module[] modules = getModulesToBuildFromSelection(project, dataContext);
-    int moduleCount = modules.length;
-
-    Presentation presentation = e.getPresentation();
-    presentation.setEnabled(moduleCount > 0);
-
-    String presentationText;
-    if (moduleCount > 0) {
-      String text = "Make Module";
-      if (moduleCount > 1) {
-        text += "s";
-      }
-      for (int i = 0; i < moduleCount; i++) {
-        if (text.length() > 30) {
-          text = "Make Selected Modules";
-          break;
-        }
-        Module toMake = modules[i];
-        if (i != 0) {
-          text += ",";
-        }
-        text += " '" + toMake.getName() + "'";
-      }
-      presentationText = text;
-    }
-    else {
-      presentationText = "Make";
-    }
-    presentation.setText(presentationText);
-    presentation.setVisible(moduleCount > 0 || !PROJECT_VIEW_POPUP.equals(e.getPlace()));
+    MakeIdeaModuleAction.updatePresentation(e, project);
   }
 
   @Override
