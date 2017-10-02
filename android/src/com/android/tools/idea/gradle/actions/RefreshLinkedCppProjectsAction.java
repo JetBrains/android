fe9f475ffcb6815cf997b64159992b792d893f8f
Stefan Xenos <sxenos@google.com>
2017-10-02 10:05:39 -0700

    Revert "Move gradle action classes to project-system-gradle"
    Test failures have been reported in SceneDisplayListTest3.testBasicScene
    
    This reverts commit c3d56f8c68be83324a5cda3185b29c1c9543e19f.
    
    Test: None needed. This is a revert to a known good state.
    Change-Id: I88928214b4790cabbeed99ecfa8863e7056435c6

--- android/src/com/android/tools/idea/gradle/actions/RefreshLinkedCppProjectsAction.java
+++ android/src/com/android/tools/idea/gradle/actions/RefreshLinkedCppProjectsAction.java
@@ -16,17 +16,18 @@
 package com.android.tools.idea.gradle.actions;
 
 import com.android.tools.idea.gradle.project.model.NdkModuleModel;
-import com.android.tools.idea.gradle.project.sync.common.CommandLineArgs;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.module.ModuleManager;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.Key;
 import org.jetbrains.annotations.NotNull;
 
 /**
  * Syncs project with Gradle, with an additional argument to refresh the linked C++ projects.
  */
 public class RefreshLinkedCppProjectsAction extends SyncProjectAction {
+  public static final Key<Boolean> REFRESH_EXTERNAL_NATIVE_MODELS_KEY = Key.create("refresh.external.native.models");
 
   public RefreshLinkedCppProjectsAction() {
     super("Refresh Linked C++ Projects");
@@ -34,7 +35,7 @@ public class RefreshLinkedCppProjectsAction extends SyncProjectAction {
 
   @Override
   protected void doPerform(@NotNull AnActionEvent e, @NotNull Project project) {
-    project.putUserData(CommandLineArgs.REFRESH_EXTERNAL_NATIVE_MODELS_KEY, true);
+    project.putUserData(REFRESH_EXTERNAL_NATIVE_MODELS_KEY, true);
     super.doPerform(e, project);
   }
 
--- android/src/com/android/tools/idea/gradle/actions/StopGradleDaemonsAction.java
+++ android/src/com/android/tools/idea/gradle/actions/StopGradleDaemonsAction.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017 The Android Open Source Project
+ * Copyright (C) 2014 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,8 +15,13 @@
  */
 package com.android.tools.idea.gradle.actions;
 
+import com.android.tools.idea.gradle.util.GradleUtil;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.project.DumbAwareAction;
+import com.intellij.openapi.ui.Messages;
+
+import java.io.IOException;
 
 import static com.android.tools.idea.gradle.util.GradleUtil.stopAllGradleDaemonsAndRestart;
 
