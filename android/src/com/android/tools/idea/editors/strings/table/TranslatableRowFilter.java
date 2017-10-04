01b83220db25ed51e7da869fea078a9dcc932c7b
Juan C Nuno <juancnuno@google.com>
2017-10-04 15:52:01 -0700

    Add a TranslatableRowFilter class
    Test: Existing tests
    Change-Id: I466c849d1271f47cc4f97b9e8c98763a60eff31b

--- android/src/com/android/tools/idea/editors/strings/table/TranslatableRowFilter.java
+++ android/src/com/android/tools/idea/editors/strings/table/TranslatableRowFilter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016 The Android Open Source Project
+ * Copyright (C) 2017 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,26 +18,17 @@ package com.android.tools.idea.editors.strings.table;
 import com.intellij.openapi.actionSystem.Presentation;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.stream.IntStream;
-
-import static com.android.tools.idea.editors.strings.table.StringResourceTableModel.DEFAULT_VALUE_COLUMN;
 import static com.android.tools.idea.editors.strings.table.StringResourceTableModel.UNTRANSLATABLE_COLUMN;
 
-public final class NeedsTranslationsRowFilter extends StringResourceTableRowFilter {
+public final class TranslatableRowFilter extends StringResourceTableRowFilter {
   @Override
   public void update(@NotNull Presentation presentation) {
     presentation.setIcon(null);
-    presentation.setText("Show Keys Needing Translations");
+    presentation.setText("Show Translatable Keys");
   }
 
   @Override
   public boolean include(@NotNull Entry<? extends StringResourceTableModel, ? extends Integer> entry) {
-    if ((boolean)entry.getValue(UNTRANSLATABLE_COLUMN)) {
-      return false;
-    }
-
-    return IntStream.range(DEFAULT_VALUE_COLUMN, entry.getValueCount())
-      .mapToObj(entry::getStringValue)
-      .anyMatch(String::isEmpty);
+    return !(boolean)entry.getValue(UNTRANSLATABLE_COLUMN);
   }
 }
