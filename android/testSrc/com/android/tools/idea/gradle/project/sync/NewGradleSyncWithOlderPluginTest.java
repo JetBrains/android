030319e558c54a4dc864e98e61c1981a6bbf4845
alruiz <alruiz@google.com>
2017-10-07 19:44:32 -0700

    Enabled tests in GradleSyncWithOlderPluginTest.
    This CL separates the tests that are executed with the
    old and new Sync infrastructures to their respective test
    classes. The test code is cleaner and easier to understand.
    
    Bug: 67473609
    Test: JUnit test
    Change-Id: I4159036b319cd7006fa7fa25d2f563a46ba3fb46

---
+++ android/testSrc/com/android/tools/idea/gradle/project/sync/NewGradleSyncWithOlderPluginTest.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.gradle.project.sync;
+
+/**
+ * Integration test for Gradle Sync with old versions of Android plugin and the new Sync infrastructure.
+ */
+public class NewGradleSyncWithOlderPluginTest extends GradleSyncWithOlderPluginTest {
+  @Override
+  protected boolean useNewSyncInfrastructure() {
+    return true;
+  }
+
+  @Override
+  public void testWithInterAndroidModuleDependencies() throws Exception {
+    notifySkippedTest();
+  }
+
+  @Override
+  public void testWithInterJavaModuleDependencies() throws Exception {
+    notifySkippedTest();
+  }
+
+  @Override
+  public void testJavaLibraryDependenciesFromJavaModule() throws Exception {
+    notifySkippedTest();
+  }
+
+  @Override
+  public void testLocalJarDependenciesFromAndroidModule() throws Exception {
+    notifySkippedTest();
+  }
+
+  @Override
+  public void testJavaLibraryDependenciesFromAndroidModule() throws Exception {
+    notifySkippedTest();
+  }
+
+  @Override
+  public void testAndroidModuleDependenciesFromAndroidModule() throws Exception {
+    notifySkippedTest();
+  }
+
+  @Override
+  public void testAndroidLibraryDependenciesFromAndroidModule() throws Exception {
+    notifySkippedTest();
+  }
+
+  @Override
+  public void testWithPluginOneDotFive() throws Exception {
+    notifySkippedTest();
+  }
+
+  private void notifySkippedTest() {
+    System.out.println(String.format("Skipped '%1$s#%2$s'. See http://b/67420000 .", getClass().getSimpleName(), getName()));
+  }
+}
