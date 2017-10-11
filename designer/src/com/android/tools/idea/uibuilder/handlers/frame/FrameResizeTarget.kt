78b34fba514a6e4f4580fee64ad077642ff68f01
Charlie Tsai <chartsai@google.com>
2017-10-11 02:23:32 -0700

    Migrate FrameLayoutHandler to Target Architecture
    - Use Kotlin to re-write FrameLayoutHandler.
    - Make FrameLayoutHandler using SceneInteraction.
    - Add FrameDragTarget.
    - Re-write FrameDragHandler for using FrameDragTarget.
    - Replace ResizeHandler with ResizeTarget.
    
    Fixed: 67496846
    Test: Add and passed FrameLayoutHandlerTest
    Change-Id: I3edd89e6ecee118bece6dc6194669a411e4ee996

---
+++ designer/src/com/android/tools/idea/uibuilder/handlers/frame/FrameResizeTarget.kt
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.uibuilder.handlers.frame
+
+import com.android.tools.idea.common.model.AndroidDpCoordinate
+import com.android.tools.idea.common.model.AttributesTransaction
+import com.android.tools.idea.uibuilder.scene.target.ResizeWithSnapBaseTarget
+
+import com.android.SdkConstants.*
+
+class FrameResizeTarget(type: Type) : ResizeWithSnapBaseTarget(type) {
+
+  override fun updateAttributes(attributes: AttributesTransaction, @AndroidDpCoordinate x: Int, @AndroidDpCoordinate y: Int) {
+    when (myType) {
+      Type.LEFT, Type.RIGHT -> updateWidth(attributes, getNewWidth(x))
+      Type.TOP, Type.BOTTOM -> updateHeight(attributes, getNewHeight(y))
+      Type.LEFT_TOP, Type.LEFT_BOTTOM, Type.RIGHT_TOP, Type.RIGHT_BOTTOM -> {
+        updateWidth(attributes, getNewWidth(x))
+        updateHeight(attributes, getNewHeight(y))
+      }
+    }
+  }
+
+  private fun updateWidth(attributes: AttributesTransaction, width: String) =
+      attributes.setAttribute(ANDROID_URI, ATTR_LAYOUT_WIDTH, width)
+
+  private fun updateHeight(attributes: AttributesTransaction, height: String) =
+      attributes.setAttribute(ANDROID_URI, ATTR_LAYOUT_HEIGHT, height)
+}
