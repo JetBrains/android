95663804b8ea673715a8bda166439695b2eb212d
Nicolas Roard <nicolasroard@google.com>
2017-10-25 19:13:20 +0100

    Add a slider panel to drive the transition
    Test: UI, N/A

--- designer/src/com/android/tools/idea/uibuilder/handlers/transition/TransitionLayoutHandler.java
+++ designer/src/com/android/tools/idea/uibuilder/handlers/transition/TransitionLayoutHandler.java
@@ -15,41 +15,138 @@
  */
 package com.android.tools.idea.uibuilder.handlers.transition;
 
+import com.android.SdkConstants;
+import com.android.ide.common.rendering.api.ViewInfo;
+import com.android.tools.idea.common.model.NlComponent;
+import com.android.tools.idea.common.model.NlModel;
 import com.android.tools.idea.uibuilder.api.CustomPanel;
 import com.android.tools.idea.uibuilder.handlers.constraint.ConstraintLayoutHandler;
+import com.android.tools.idea.uibuilder.model.NlComponentHelperKt;
 import com.google.common.collect.ImmutableList;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.List;
 
 import static com.android.SdkConstants.ATTR_TRANSITION_POSITION;
 
 public class TransitionLayoutHandler extends ConstraintLayoutHandler {
 
-  class MyCustomPanel extends CustomPanel {
-    public MyCustomPanel() {
-      add(new JLabel("Hello, World!"));
-    }
-  }
+  class AnimationPositionPanel extends CustomPanel {
 
-  class MyCustomPanel2 extends CustomPanel {
-    public MyCustomPanel2() {
-      add(new JLabel("Child Hello, World!"));
+    private NlComponent myComponent;
+    private NlComponent myTransitionLayoutComponent;
+    private Method myCallSetTransitionPosition;
+    private Method myCallEvaluate;
+
+    @Override
+    public void useComponent(@NotNull NlComponent component) {
+      myComponent = component;
+      myTransitionLayoutComponent = null;
+      if (NlComponentHelperKt.isOrHasSuperclass(component, SdkConstants.TRANSITION_LAYOUT)) {
+        myTransitionLayoutComponent = component;
+      } else {
+        NlComponent parent = myComponent.getParent();
+        if (parent != null && NlComponentHelperKt.isOrHasSuperclass(parent, SdkConstants.TRANSITION_LAYOUT)) {
+          myTransitionLayoutComponent = parent;
+        }
+      }
+    }
+
+    public AnimationPositionPanel() {
+      setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+      add(new JLabel("Animation position:"));
+      JSlider slider = new JSlider();
+      slider.setValue(0);
+      slider.setMinimum(0);
+      slider.setMaximum(100);
+      slider.addChangeListener(new ChangeListener() {
+        @Override
+        public void stateChanged(ChangeEvent e) {
+          if (myTransitionLayoutComponent == null) {
+            return;
+          }
+          float value = slider.getValue() / 100f;
+          setValue(myTransitionLayoutComponent, value);
+        }
+      });
+      add(slider);
+    }
+
+    private void setTransitionPosition(Object instance, float position) {
+      if (myCallSetTransitionPosition == null) {
+        for (Method m : instance.getClass().getMethods()) {
+          if (m.getName().equalsIgnoreCase("setTransitionPosition") && m.getParameterCount() == 1) {
+            myCallSetTransitionPosition = m;
+            break;
+          }
+        }
+      }
+      if (myCallSetTransitionPosition != null) {
+        try {
+          myCallSetTransitionPosition.invoke(instance, Float.valueOf(position));
+        }
+        catch (IllegalAccessException e) {
+          e.printStackTrace();
+        }
+        catch (InvocationTargetException e) {
+          e.printStackTrace();
+        }
+      }
+    }
+
+    private void evaluate(Object instance) {
+      if (myCallEvaluate == null) {
+        for (Method m : instance.getClass().getMethods()) {
+          if (m.getName().equalsIgnoreCase("evaluate") && m.getParameterCount() == 0) {
+            myCallEvaluate = m;
+            break;
+          }
+        }
+      }
+      if (myCallEvaluate != null) {
+        try {
+          myCallEvaluate.invoke(instance);
+        }
+        catch (IllegalAccessException e) {
+          e.printStackTrace();
+        }
+        catch (InvocationTargetException e) {
+          e.printStackTrace();
+        }
+      }
+    }
+
+    public void setValue(@NotNull NlComponent component, float value) {
+      assert component != null;
+
+      ViewInfo info = NlComponentHelperKt.getViewInfo(component);
+      if (info == null) {
+        return;
+      }
+      Object instance = info.getViewObject();
+      setTransitionPosition(instance, value);
+      evaluate(instance);
+      NlModel model = component.getModel();
+      model.notifyLiveUpdate(false);
     }
   }
 
   @Override
   @Nullable
   public CustomPanel getCustomPanel() {
-    return new MyCustomPanel();
+    return new AnimationPositionPanel();
   }
 
   @Override
   @Nullable
   public CustomPanel getLayoutCustomPanel() {
-    return new MyCustomPanel2();
+    return new AnimationPositionPanel();
   }
 
   @Override
