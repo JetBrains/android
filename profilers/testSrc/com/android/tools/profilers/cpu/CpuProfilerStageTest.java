f947a65ee5cdc72cdfa55c80ab3fbeb8353a3efc
Amaury Medeiros <amaurym@google.com>
2017-10-26 15:17:55 +0100

    Select simpleperf as profiling option for native projects
    When we open the CPU profilers, we automatically select a configuration
    on the CPU profiling configurations combobox. In the general case, we
    should select ART Sampled (sampling java code), but we can make an
    educated guess to select simpleperf in case we think the user is
    profiling a native project.
    
    The current approach to guess if the project being profiled is native is
    checking the files that are currently open and being displayed (i.e. their
    tab is active) in Android Studio and checking if at least one of them is
    a native file (.c, .cc, .cpp, .h, .hpp). This approach can be changed later
    if, for instance,  we get feedback saying it's too aggressive. In that case,
    we can check if all (or half) the files being displayed are native.
    
    Test: Added a unit test to CpuProfilerStageTest
    Bug: 68014319
    Change-Id: Ib01aa6473b9641967b6eb393421961272919eb43

--- profilers/testSrc/com/android/tools/profilers/cpu/CpuProfilerStageTest.java
+++ profilers/testSrc/com/android/tools/profilers/cpu/CpuProfilerStageTest.java
@@ -732,6 +732,23 @@ public class CpuProfilerStageTest extends AspectObserver {
   }
 
   @Test
+  public void suggestedProfilingConfigurationDependsOnNativePreference() {
+    // Make sure simpleperf is supported.
+    myServices.enableSimplePerf(true);
+    addAndSetDevice(26, "Any Serial");
+
+    myServices.setNativeProfilingConfigurationPreferred(false);
+    myStage = new CpuProfilerStage(myStage.getStudioProfilers());
+    // ART Sampled should be the default configuration when there is no preference for a native config.
+    assertThat(myStage.getProfilingConfiguration().getName()).isEqualTo(ProfilingConfiguration.ART_SAMPLED);
+
+    myServices.setNativeProfilingConfigurationPreferred(true);
+    myStage = new CpuProfilerStage(myStage.getStudioProfilers());
+    // Simpleperf should be the default configuration when a native config is preferred.
+    assertThat(myStage.getProfilingConfiguration().getName()).isEqualTo(ProfilingConfiguration.SIMPLEPERF);
+  }
+
+  @Test
   public void exitingStateAndEnteringAgainShouldPreserveCaptureState() throws IOException, InterruptedException {
     assertThat(myCpuService.getProfilerType()).isEqualTo(CpuProfiler.CpuProfilerType.ART);
     ProfilingConfiguration config1 = new ProfilingConfiguration("My Config",
