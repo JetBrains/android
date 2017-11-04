0da4cfdaf26c46c481e0a61b7db0557b4411c734
Alexander Wang <afwang@google.com>
2017-11-03 17:41:10 -0700

    Add UI test: HprofAnalysisTest
    Tests if the hprof file analyzer is capable of
    detecting leaked activities and duplicate strings
    in a given hprof file.
    
    Test: HprofAnalysisTest
    Change-Id: I3c74189bd4c8e6421f6d0acc64b21db53d66057c

--- uitest-framework/testSrc/com/android/tools/idea/tests/gui/framework/fixture/HprofEditorFixture.java
+++ uitest-framework/testSrc/com/android/tools/idea/tests/gui/framework/fixture/HprofEditorFixture.java
@@ -20,6 +20,8 @@ import com.android.tools.idea.editors.hprof.HprofView;
 import com.android.tools.idea.editors.hprof.views.ClassesTreeView;
 import com.android.tools.idea.editors.hprof.views.InstanceReferenceTreeView;
 import com.android.tools.idea.editors.hprof.views.InstancesTreeView;
+import com.android.tools.idea.tests.gui.framework.matcher.Matchers;
+import com.intellij.designer.LightToolWindow;
 import com.intellij.openapi.actionSystem.impl.ActionToolbarImpl;
 import com.intellij.openapi.fileEditor.FileEditor;
 import com.intellij.openapi.fileEditor.FileEditorManager;
@@ -27,13 +29,24 @@ import com.intellij.openapi.fileEditor.impl.EditorWindow;
 import com.intellij.openapi.fileEditor.impl.EditorWithProviderComposite;
 import com.intellij.openapi.fileEditor.impl.EditorsSplitters;
 import com.intellij.openapi.fileEditor.impl.FileEditorManagerImpl;
+import com.intellij.ui.InplaceButton;
+import com.intellij.ui.treeStructure.Tree;
+import org.fest.swing.core.GenericTypeMatcher;
 import org.fest.swing.core.Robot;
 import org.fest.swing.edt.GuiQuery;
 import org.fest.swing.exception.ComponentLookupException;
+import org.fest.swing.fixture.JLabelFixture;
+import org.fest.swing.fixture.JPanelFixture;
+import org.fest.swing.fixture.JToggleButtonFixture;
 import org.fest.swing.fixture.JTreeFixture;
 import org.fest.swing.timing.Wait;
 import org.jetbrains.annotations.NotNull;
 
+import javax.accessibility.AccessibleContext;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JToggleButton;
+import javax.swing.SwingUtilities;
 import java.awt.*;
 
 public class HprofEditorFixture extends EditorFixture {
@@ -112,6 +125,79 @@ public class HprofEditorFixture extends EditorFixture {
   @NotNull
   public JTreeFixture getInstanceReferenceTree() {
     return new JTreeFixture(robot, InstanceReferenceTreeView.TREE_NAME);
+  }
+
+  @NotNull
+  public HprofEditorFixture openAnalyzerTasks() {
+    JToggleButton button = robot.finder().find(new GenericTypeMatcher<JToggleButton>(JToggleButton.class) {
+      @Override
+      protected boolean isMatching(@NotNull JToggleButton component) {
+        AccessibleContext accessibleContext = component.getAccessibleContext();
+        if (accessibleContext == null) {
+          return false;
+        }
+
+        return "Analyzer Tasks".equals(accessibleContext.getAccessibleName());
+      }
+    });
+    new JToggleButtonFixture(robot, button).click();
+    Wait.seconds(1).expecting("Analyzer Tasks window to open")
+      .until(() ->
+        !robot.finder().findAll(new GenericTypeMatcher<JLabel>(JLabel.class) {
+          @Override
+          protected boolean isMatching(@NotNull JLabel label) {
+            return "Analyzer Tasks".equals(label.getText());
+          }
+        }).isEmpty()
+      );
+    return this;
+  }
+
+  @NotNull
+  public JPanelFixture getAnalyzerTasksWindow() {
+    Component labelContainer = robot.finder().find(Matchers.byText(JLabel.class, "Analyzer Tasks")).getParent();
+    LightToolWindow analyzerWindow = (LightToolWindow) SwingUtilities.getAncestorOfClass(LightToolWindow.class, labelContainer);
+    if(analyzerWindow == null) {
+      throw new ComponentLookupException("Analyzer window not found");
+    }
+
+    return new JPanelFixture(robot, analyzerWindow);
+  }
+
+  /**
+   * Clicks on the green arrow button to start an hprof file analysis.
+   * Expects that the analysis will return results for leaked activities
+   * and duplicate strings.
+   */
+  @NotNull
+  public HprofEditorFixture performAnalysis() {
+    JPanelFixture analyzerWindow = getAnalyzerTasksWindow();
+    InplaceButton analysisButton = robot.finder()
+      .find(analyzerWindow.target(), new GenericTypeMatcher<InplaceButton>(InplaceButton.class) {
+        @Override
+        protected boolean isMatching(@NotNull InplaceButton component) {
+          return "Perform Analysis".equals(component.getToolTipText());
+        }
+      });
+
+    Tree tree = (Tree) analyzerWindow.tree().target();
+    Wait.seconds(5)
+      .expecting("hprof analysis to finish")
+      .until(() -> {
+        robot.click(analysisButton);
+        return GuiQuery.get(() -> !tree.isEmpty());
+      });
+
+    return this;
+  }
+
+  @NotNull
+  public JTreeFixture getAnalysisResultsTree() {
+    JLabelFixture analysisResults = getAnalyzerTasksWindow().label(Matchers.byText(JLabel.class, "Analysis Results"));
+
+    JPanel results = (JPanel) SwingUtilities.getAncestorOfClass(JPanel.class, analysisResults.target());
+    JPanelFixture resultsFixture = new JPanelFixture(robot, results);
+    return resultsFixture.tree();
   }
 
   private void waitForHprofEditor() {
