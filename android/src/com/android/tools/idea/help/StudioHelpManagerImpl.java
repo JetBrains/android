8470d548bdfee6f80b4e56d2bc3503a7fec8c043
Kelvin Ma <kelvinhanma@google.com>
2017-11-09 15:25:39 -0800

    Add redirect endpoint to DAC for help.
    Change StudioHelpManagerImpl to handle all helpIds so DAC can choose the
    redirect. However this means the StudioHelpManagerImpl only redirect
    other helpIds if user is on AS and not intellij with AS plugin.
    
    Fixes: b/67742106
    Test: added StudioHelpManagerImplTest
    Change-Id: I74bcca981e8ed3135d180c876fe67a96291cab6c

--- android/src/com/android/tools/idea/help/StudioHelpManagerImpl.java
+++ android/src/com/android/tools/idea/help/StudioHelpManagerImpl.java
@@ -15,13 +15,15 @@
  */
 package com.android.tools.idea.help;
 
+import com.android.annotations.VisibleForTesting;
 import com.intellij.help.impl.HelpManagerImpl;
 import com.intellij.ide.BrowserUtil;
+import com.intellij.openapi.application.ex.ApplicationInfoEx;
+import com.intellij.util.PlatformUtils;
 import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-
-import java.net.MalformedURLException;
-import java.net.URL;
+import org.jetbrains.annotations.TestOnly;
 
 /**
  * Extension to the IJ help system that allows Studio to open d.a.c URLs via an IJ help topic.
@@ -33,30 +35,66 @@ public class StudioHelpManagerImpl extends HelpManagerImpl {
    */
   public static String STUDIO_HELP_PREFIX = "android-studio:";
 
+  public static Browser myDefaultBrowser = url -> BrowserUtil.browse(url);
+
   /**
    * The domain for studio help topics.
    */
-  private static String STUDIO_HELP_URL = "https://developer.android.com/";
+  public static String STUDIO_HELP_URL = "https://developer.android.com/";
+
+  /**
+   * extension for redirecting other helpIds
+   */
+  public static String REDIRECT_URL_EXTENSION = "r/studio-help/";
+
+  interface Browser {
+    void browse(String url);
+  }
+
+  private Browser myBrowser = myDefaultBrowser;
+
+  @TestOnly
+  void setBrowser(@NotNull Browser browser) {
+    myBrowser = browser;
+  }
 
   @Override
   public void invokeHelp(@Nullable @NonNls String id) {
-    if (id != null && id.startsWith(STUDIO_HELP_PREFIX)) {
-      try {
-        URL url = new URL(STUDIO_HELP_URL + id.substring(STUDIO_HELP_PREFIX.length()));
-        BrowserUtil.browse(url);
-        return;
-      }
-      catch (MalformedURLException ignore) {
-        // nothing
-      }
-    }
+    if (id == null) return;
 
-    // The run debug configuration help topic is set deep in the platform (see SingleConfigurationConfigurable)
-    if (id != null && id.startsWith("reference.dialogs.rundebug.Android")) {
-      BrowserUtil.browse(STUDIO_HELP_URL + "/r/studio-ui/rundebugconfig.html");
+    if (id.startsWith(STUDIO_HELP_PREFIX)) {
+      openStudioHelpPage(id.substring(STUDIO_HELP_PREFIX.length()));
       return;
     }
 
-    super.invokeHelp(id);
+    // The run debug configuration help topic is set deep in the platform (see SingleConfigurationConfigurable)
+    if (id.startsWith("reference.dialogs.rundebug.Android")) {
+      openStudioHelpPage("/r/studio-ui/rundebugconfig.html");
+      return;
+    }
+
+    // open to https://developer.android.com/r/studio-help/ for all other helpIds. Including Intellij ones so map the version and redirect
+    // see b/63761943
+    if (PlatformUtils.isAndroidStudio()) {
+      openStudioHelpPage(REDIRECT_URL_EXTENSION + getVersion() + "/?" + id);
+    }
+    else {
+      super.invokeHelp(id);
+    }
+  }
+
+  private void openStudioHelpPage(@NotNull @NonNls String urlPath) {
+    myBrowser.browse(STUDIO_HELP_URL + urlPath);
+  }
+
+  @VisibleForTesting
+  String getVersion() {
+    ApplicationInfoEx info = ApplicationInfoEx.getInstanceEx();
+    String minorVersion = info.getMinorVersion();
+    int dot = minorVersion.indexOf('.');
+    if (dot != -1) {
+      minorVersion = minorVersion.substring(0, dot);
+    }
+    return info.getMajorVersion() + "." + minorVersion;
   }
 }
