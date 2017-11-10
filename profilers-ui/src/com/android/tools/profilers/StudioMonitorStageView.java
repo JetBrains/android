2d7dec7a5ce66a346a4dc96f5391c14ea7f7a89c
Chester Hsieh <dunno@google.com>
2017-11-10 14:37:58 -0800

    Fixes a framerate slowdown
    This fixes an antialiasing hint that was causing a slowdown in the profiler.
    The theory is that the setting is causing a software-based swizzle to happen,
    which is reducing rendering performance.
    
    This also includes a change that reduces the occurance of invalidations.
    
    Change-Id: I7fa8d0310b4061270b29e8acd5134d90fc4fd987
    Fixes: 68886177
    Test: N/A

--- profilers-ui/src/com/android/tools/profilers/StudioMonitorStageView.java
+++ profilers-ui/src/com/android/tools/profilers/StudioMonitorStageView.java
@@ -15,10 +15,8 @@
  */
 package com.android.tools.profilers;
 
-import com.android.tools.adtui.AxisComponent;
 import com.android.tools.adtui.TabularLayout;
 import com.android.tools.adtui.RangeTooltipComponent;
-import com.android.tools.adtui.model.Range;
 import com.android.tools.profilers.cpu.CpuMonitor;
 import com.android.tools.profilers.cpu.CpuMonitorTooltipView;
 import com.android.tools.profilers.cpu.CpuMonitorView;
