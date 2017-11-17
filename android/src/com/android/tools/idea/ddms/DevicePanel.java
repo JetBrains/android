3cee1ff2e83df1ee0c4f7e00c9e3db9681cf0a18
Juan C Nuno <juancnuno@google.com>
2017-11-17 15:22:25 -0800

    Use more specific type parameters
    Test: Not applicable
    Change-Id: I9a51936b513d19592c4322551809ca29baedd12a

--- android/src/com/android/tools/idea/ddms/DevicePanel.java
+++ android/src/com/android/tools/idea/ddms/DevicePanel.java
@@ -58,9 +58,8 @@ public class DevicePanel implements AndroidDebugBridge.IDeviceChangeListener, An
   @NotNull private final Map<String, String> myPreferredClients;
   private boolean myIgnoreActionEvents;
 
-  // TODO Use more specific type parameters
-  private JComboBox<Object> myDeviceCombo;
-  private JComboBox<Object> myClientCombo;
+  private JComboBox<IDevice> myDeviceCombo;
+  private JComboBox<Client> myClientCombo;
   private final NullableLazyValue<String> myCandidateClientName = new NullableLazyValue<String>() {
     @Nullable
     @Override
