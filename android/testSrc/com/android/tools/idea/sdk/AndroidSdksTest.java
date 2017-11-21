cabf88b5a52be38df479f71d76085b832e3efd29
parentej <parentej@google.com>
2017-11-21 16:36:12 +0000

    Android Studio can't find Add-ons when switching SDKs
    A project with an add-on
    (eg compileSdkVersion 'Google Inc.:Google APIs:24') will still not sync
    when Android Studio switches from a SDK that does not have the Add-on
    to another that has. To make it sync again, the compileSdkVersion needs
    to be changed to the add-on target API (compileSdkVersion 24), sync, and
    then back to the original value.
    
    Bug: 69606910
    Test: Run GradleSyncTest@sdkCreationForAddons() and added AndroidSdksTest@testCreateSdkForAddonCreatesItsParent
    Change-Id: I0f7cb8f9175d05146727bc929db0a76a04edc272

--- android/testSrc/com/android/tools/idea/sdk/AndroidSdksTest.java
+++ android/testSrc/com/android/tools/idea/sdk/AndroidSdksTest.java
@@ -18,6 +18,7 @@ package com.android.tools.idea.sdk;
 import com.android.sdklib.AndroidVersion;
 import com.android.sdklib.IAndroidTarget;
 import com.android.sdklib.repository.AndroidSdkHandler;
+import com.android.sdklib.repository.generated.common.v1.LibraryType;
 import com.android.tools.idea.IdeInfo;
 import com.google.common.collect.ImmutableList;
 import com.intellij.openapi.projectRoots.Sdk;
@@ -34,9 +35,7 @@ import org.jetbrains.annotations.NotNull;
 import org.mockito.Mock;
 
 import java.io.File;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 import static com.android.sdklib.AndroidTargetHash.getTargetHashString;
 import static com.android.testutils.TestUtils.getSdk;
@@ -215,6 +214,19 @@ public class AndroidSdksTest extends IdeaTestCase {
 
     Sdk sdk = myAndroidSdks.create(findLatestAndroidTarget(mySdkPath), mySdkPath, true);
     assertNull(sdk);
+  }
+
+  public void testCreateSdkForAddonCreatesItsParent() {
+    IAndroidTarget parentTarget = findLatestAndroidTarget(mySdkPath);
+    String parentHash = getTargetHashString(parentTarget);
+
+    IAndroidTarget addonTarget = mock(IAndroidTarget.class);
+    when(addonTarget.getParent()).thenReturn(parentTarget);
+    when(addonTarget.getAdditionalLibraries()).thenReturn(Collections.singletonList(new LibraryType()));
+
+    assertNull(myAndroidSdks.findSuitableAndroidSdk(parentHash)); // Parent SDK doesn't exists
+    assertNull(myAndroidSdks.create(addonTarget, mySdkPath, myJdk, false)); // Addon sdk is not created
+    assertNotNull(myAndroidSdks.findSuitableAndroidSdk(parentHash)); // Parent SDK is now created
   }
 
   public void testChooseNameForNewLibrary() {
