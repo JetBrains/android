JetBrains/android

commit 39bb590e5c4d6747a79af22b4047c836ac879dfa

NLModel: do not hold XmlFile reference
(which can be invalidated at any moment)

Test: existing
Change-Id: I217659de964afcd1bf1c394fb00cc2c6904f67f1

designer/src/com/android/tools/idea/common/model/NlModel.java
@@ -18,6 +18,7 @@ package com.android.tools.idea.common.model;
 import com.android.ide.common.resources.ResourceResolver;
 import com.android.resources.ResourceType;
 import com.android.resources.ResourceUrl;
+import com.android.tools.idea.AndroidPsiUtils;
 import com.android.tools.idea.common.command.NlWriteCommandAction;
 import com.android.tools.idea.common.lint.LintAnnotationsModel;
 import com.android.tools.idea.configurations.Configuration;
@@ -42,6 +43,7 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.Computable;
 import com.intellij.openapi.util.Disposer;
 import com.intellij.openapi.util.ModificationTracker;
+import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.psi.xml.XmlAttribute;
@@ -68,7 +70,7 @@ public class NlModel implements Disposable, ResourceChangeListener, Modification
   private final Set<String> myPendingIds = Sets.newHashSet();
 
   @NotNull private final AndroidFacet myFacet;
-  private final XmlFile myFile;
+  private final VirtualFile myFile;
 
   private final Configuration myConfiguration;
   private final ListenerCollection<ModelListener> myListeners = ListenerCollection.createWithDirectExecutor();
@@ -86,23 +88,23 @@ public class NlModel implements Disposable, ResourceChangeListener, Modification
   @NotNull
   public static NlModel create(@Nullable Disposable parent,
                                @NotNull AndroidFacet facet,
-                               @NotNull XmlFile file) {
+                               @NotNull VirtualFile file) {
     return new NlModel(parent, facet, file);
   }
 
   @VisibleForTesting
   protected NlModel(@Nullable Disposable parent,
                     @NotNull AndroidFacet facet,
-                    @NotNull XmlFile file) {
+                    @NotNull VirtualFile file) {
     myFacet = facet;
     myFile = file;
-    myConfiguration = ConfigurationManager.getOrCreateInstance(facet).getConfiguration(myFile.getVirtualFile());
+    myConfiguration = ConfigurationManager.getOrCreateInstance(facet).getConfiguration(myFile);
     myConfigurationModificationCount = myConfiguration.getModificationCount();
     myId = System.nanoTime() ^ file.getName().hashCode();
     if (parent != null) {
       Disposer.register(parent, this);
     }
-    myType = NlLayoutType.typeOf(file);
+    myType = NlLayoutType.typeOf(getFile());
   }
 
   /**
@@ -127,7 +129,7 @@ public class NlModel implements Disposable, ResourceChangeListener, Modification
         updateTheme();
       }
       ResourceNotificationManager manager = ResourceNotificationManager.getInstance(getProject());
-      manager.addListener(this, myFacet, myFile, myConfiguration);
+      manager.addListener(this, myFacet, null, null);
       myListeners.forEach(listener -> listener.modelActivated(this));
     }
   }
@@ -146,8 +148,8 @@ public class NlModel implements Disposable, ResourceChangeListener, Modification
 
   private void deactivate() {
     myListeners.forEach(listener -> listener.modelDeactivated(this));
-    ResourceNotificationManager manager = ResourceNotificationManager.getInstance(myFile.getProject());
-    manager.removeListener(this, myFacet, myFile, myConfiguration);
+    ResourceNotificationManager manager = ResourceNotificationManager.getInstance(getProject());
+    manager.removeListener(this, myFacet, null, null);
     myConfigurationModificationCount = myConfiguration.getModificationCount();
   }
 
@@ -171,7 +173,9 @@ public class NlModel implements Disposable, ResourceChangeListener, Modification
 
   @NotNull
   public XmlFile getFile() {
-    return myFile;
+    XmlFile file = (XmlFile)AndroidPsiUtils.getPsiFileSafely(getProject(), myFile);
+    assert file != null;
+    return file;
   }
 
   @NotNull
@@ -205,7 +209,7 @@ public class NlModel implements Disposable, ResourceChangeListener, Modification
       ApplicationManager.getApplication().runReadAction(() -> {
         Set<NlComponent> unique = Sets.newIdentityHashSet();
         Set<XmlTag> uniqueTags = Sets.newIdentityHashSet();
-        checkUnique(myFile.getRootTag(), uniqueTags);
+        checkUnique(getFile().getRootTag(), uniqueTags);
         uniqueTags.clear();
         if (myRootComponent != null) {
           checkUnique(myRootComponent.getTag(), uniqueTags);
@@ -706,7 +710,7 @@ public class NlModel implements Disposable, ResourceChangeListener, Modification
 
   @NotNull
   public ImmutableList<NlComponent> findByOffset(int offset) {
-    XmlTag tag = PsiTreeUtil.findElementOfClassAtOffset(myFile, offset, XmlTag.class, false);
+    XmlTag tag = PsiTreeUtil.findElementOfClassAtOffset(getFile(), offset, XmlTag.class, false);
     return (tag != null) ? findViewsByTag(tag) : ImmutableList.of();
   }
 
@@ -751,7 +755,7 @@ public class NlModel implements Disposable, ResourceChangeListener, Modification
 
   public void delete(final Collection<NlComponent> components) {
     // Group by parent and ask each one to participate
-    WriteCommandAction<Void> action = new WriteCommandAction<Void>(myFacet.getModule().getProject(), "Delete Component", myFile) {
+    WriteCommandAction<Void> action = new WriteCommandAction<Void>(myFacet.getModule().getProject(), "Delete Component", getFile()) {
       @Override
       protected void run(@NotNull Result<Void> result) {
         handleDeletion(components);
@@ -1024,7 +1028,8 @@ public class NlModel implements Disposable, ResourceChangeListener, Modification
    */
   private void transferNamespaces(@NotNull XmlTag tag) {
     // Transfer namespace attributes
-    XmlDocument xmlDocument = myFile.getDocument();
+    XmlFile file = getFile();
+    XmlDocument xmlDocument = file.getDocument();
     assert xmlDocument != null;
     XmlTag rootTag = xmlDocument.getRootTag();
     assert rootTag != null;
@@ -1041,7 +1046,7 @@ public class NlModel implements Disposable, ResourceChangeListener, Modification
       String currentPrefix = namespaceToPrefix.get(namespace);
       if (currentPrefix == null) {
         // The namespace isn't used in the document. Import it.
-        String newPrefix = AndroidResourceUtil.ensureNamespaceImported(myFile, namespace, prefix);
+        String newPrefix = AndroidResourceUtil.ensureNamespaceImported(file, namespace, prefix);
         if (!prefix.equals(newPrefix)) {
           // We imported the namespace, but the prefix used in the new document isn't available
           // so we need to update all attribute references to the new name

designer/src/com/android/tools/idea/common/editor/NlEditorPanel.java
@@ -52,7 +52,7 @@ import java.util.List;
 public class NlEditorPanel extends WorkBench<DesignSurface> {
   private final NlEditor myEditor;
   private final Project myProject;
-  private final XmlFile myFile;
+  private final VirtualFile myFile;
   private final DesignSurface mySurface;
   private final JPanel myContentPanel;
   private boolean myIsActive;
@@ -63,11 +63,10 @@ public class NlEditorPanel extends WorkBench<DesignSurface> {
 
     myEditor = editor;
     myProject = project;
-    myFile = (XmlFile)AndroidPsiUtils.getPsiFileSafely(project, file);
-    assert myFile != null : file;
+    myFile = file;
     myContentPanel = new JPanel(new BorderLayout());
 
-    if (NlLayoutType.typeOf(myFile) == NlLayoutType.NAV) {
+    if (NlLayoutType.typeOf(getFile()) == NlLayoutType.NAV) {
       mySurface = new NavDesignSurface(project, editor);
     }
     else {
@@ -96,7 +95,9 @@ public class NlEditorPanel extends WorkBench<DesignSurface> {
     if (Disposer.isDisposed(myEditor) || myContentPanel.getComponentCount() > 0) {
       return;
     }
-    AndroidFacet facet = AndroidFacet.getInstance(myFile);
+    XmlFile file = getFile();
+
+    AndroidFacet facet = AndroidFacet.getInstance(file);
     assert facet != null;
     NlModel model = NlModel.create(myEditor, facet, myFile);
     mySurface.setModel(model);
@@ -108,7 +109,7 @@ public class NlEditorPanel extends WorkBench<DesignSurface> {
 
     List<ToolWindowDefinition<DesignSurface>> tools = new ArrayList<>(4);
     // TODO: factor out tool creation
-    if (NlLayoutType.typeOf(myFile) == NlLayoutType.NAV) {
+    if (NlLayoutType.typeOf(file) == NlLayoutType.NAV) {
       tools.add(new NavPropertyPanelDefinition(facet, Side.RIGHT, Split.TOP, AutoHide.DOCKED));
       tools.add(new DestinationList.DestinationListDefinition());
     }
@@ -150,5 +151,12 @@ public class NlEditorPanel extends WorkBench<DesignSurface> {
   @NotNull
   public DesignSurface getSurface() {
     return mySurface;
+  }
+
+  @NotNull
+  public XmlFile getFile() {
+    XmlFile file = (XmlFile)AndroidPsiUtils.getPsiFileSafely(myProject, myFile);
+    assert file != null;
+    return file;
   }
 }

designer/src/com/android/tools/idea/uibuilder/editor/NlPreviewForm.java
@@ -387,7 +387,7 @@ public class NlPreviewForm implements Disposable, CaretListener {
       if (myModel != null) {
         Disposer.dispose(myModel);
       }
-      myModel = NlModel.create(null, facet, xmlFile);
+      myModel = NlModel.create(null, facet, xmlFile.getVirtualFile());
 
       mySurface.setModel(myModel);
       myPendingFile = new Pending(xmlFile, myModel);

designer/src/com/android/tools/idea/uibuilder/mockup/editor/creators/IncludeTagCreator.java
@@ -132,10 +132,10 @@ public class IncludeTagCreator extends SimpleViewCreator {
     if (rootTag == null) {
       return null;
     }
-
     SceneManager manager = getScreenView().getSurface().getSceneManager();
+
     if (manager != null) {
-      NlModel model = NlModel.create(newFile.getProject(), facet, newFile);
+      NlModel model = NlModel.create(newFile.getProject(), facet, newFile.getVirtualFile());
       manager.addRenderListener(new RenderListener() {
         @Override
         public void onRenderCompleted() {

designer/testSrc/com/android/tools/idea/common/SyncNlModel.java
@@ -20,6 +20,7 @@ import com.android.tools.idea.common.model.NlModel;
 import com.android.tools.idea.common.surface.DesignSurface;
 import com.android.tools.idea.configurations.Configuration;
 import com.intellij.openapi.Disposable;
+import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.xml.XmlFile;
 import org.jetbrains.android.facet.AndroidFacet;
 import org.jetbrains.annotations.NotNull;
@@ -38,11 +39,11 @@ public class SyncNlModel extends NlModel {
   public static SyncNlModel create(@NotNull DesignSurface surface,
                                    @Nullable Disposable parent,
                                    @NotNull AndroidFacet facet,
-                                   @NotNull XmlFile file) {
+                                   @NotNull VirtualFile file) {
     return new SyncNlModel(surface, parent, facet, file);
   }
 
-  private SyncNlModel(@NotNull DesignSurface surface, @Nullable Disposable parent, @NotNull AndroidFacet facet, @NotNull XmlFile file) {
+  private SyncNlModel(@NotNull DesignSurface surface, @Nullable Disposable parent, @NotNull AndroidFacet facet, @NotNull VirtualFile file) {
     super(parent, facet, file);
     mySurface = surface;
   }

designer/testSrc/com/android/tools/idea/common/fixtures/ModelBuilder.java
@@ -165,7 +165,7 @@ public class ModelBuilder {
       XmlDocument document = xmlFile.getDocument();
       assertNotNull(document);
 
-      SyncNlModel model = SyncNlModel.create(createSurface(mySurfaceClass), myFixture.getProject(), myFacet, xmlFile);
+      SyncNlModel model = SyncNlModel.create(createSurface(mySurfaceClass), myFixture.getProject(), myFacet, xmlFile.getVirtualFile());
       DesignSurface surface = model.getSurface();
       Disposer.register(project, surface);
       when(surface.getModel()).thenReturn(model);

designer/testSrc/com/android/tools/idea/common/model/NlModelTest.java
@@ -594,7 +594,7 @@ public class NlModelTest extends LayoutTestCase {
                                                   "     android:layout_height=\"match_parent\" />" +
                                                   "</merge>");
     DesignSurface surface = createSurface(NlDesignSurface.class);
-    NlModel model = SyncNlModel.create(surface, myFixture.getProject(), myFacet, mergeXml);
+    NlModel model = SyncNlModel.create(surface, myFixture.getProject(), myFacet, mergeXml.getVirtualFile());
     when(surface.getModel()).thenReturn(model);
 
     XmlTag parentRoot = parentXml.getRootTag();
@@ -629,7 +629,7 @@ public class NlModelTest extends LayoutTestCase {
                                                             "               android:layout_height=\"48dp\" />" +
                                                             "</LinearLayout>");
     DesignSurface surface = createSurface(NlDesignSurface.class);
-    NlModel model = SyncNlModel.create(surface, myFixture.getProject(), myFacet, modelXml);
+    NlModel model = SyncNlModel.create(surface, myFixture.getProject(), myFacet, modelXml.getVirtualFile());
     when(surface.getModel()).thenReturn(model);
 
     TagSnapshot rootSnapshot = TagSnapshot.createTagSnapshot(modelXml.getRootTag(), null);
@@ -651,7 +651,7 @@ public class NlModelTest extends LayoutTestCase {
                                                            "         android:layout_width=\"match_parent\"" +
                                                            "         android:layout_height=\"match_parent\">" +
                                                            "</LinearLayout>");
-    NlModel model = SyncNlModel.create(createSurface(NlDesignSurface.class), myFixture.getProject(), myFacet, modelXml);
+    NlModel model = SyncNlModel.create(createSurface(NlDesignSurface.class), myFixture.getProject(), myFacet, modelXml.getVirtualFile());
     ModelListener listener1 = mock(ModelListener.class);
     ModelListener remove1 = mock(ModelListener.class, invocation -> {
       model.removeListener((ModelListener)invocation.getMock());
@@ -704,7 +704,7 @@ public class NlModelTest extends LayoutTestCase {
                                                            "         android:layout_width=\"match_parent\"" +
                                                            "         android:layout_height=\"match_parent\">" +
                                                            "</LinearLayout>");
-    NlModel model = SyncNlModel.create(createSurface(NlDesignSurface.class), myFixture.getProject(), myFacet, modelXml);
+    NlModel model = SyncNlModel.create(createSurface(NlDesignSurface.class), myFixture.getProject(), myFacet, modelXml.getVirtualFile());
 
     notifyAndCheckListeners(model, NlModel::notifyListenersModelUpdateComplete, listener -> listener.modelDerivedDataChanged(any()));
     notifyAndCheckListeners(model, m -> m.notifyModified(NlModel.ChangeType.EDIT), listener -> listener.modelChanged(any()));
@@ -717,7 +717,7 @@ public class NlModelTest extends LayoutTestCase {
                                                            "         android:layout_width=\"match_parent\"" +
                                                            "         android:layout_height=\"match_parent\">" +
                                                            "</LinearLayout>");
-    NlModel model = SyncNlModel.create(createSurface(NlDesignSurface.class), myFixture.getProject(), myFacet, modelXml);
+    NlModel model = SyncNlModel.create(createSurface(NlDesignSurface.class), myFixture.getProject(), myFacet, modelXml.getVirtualFile());
     ModelListener listener1 = mock(ModelListener.class);
     ModelListener remove1 = mock(ModelListener.class, invocation -> {
       model.removeListener((ModelListener)invocation.getMock());
@@ -752,7 +752,7 @@ public class NlModelTest extends LayoutTestCase {
                                                            "         android:layout_width=\"match_parent\"" +
                                                            "         android:layout_height=\"match_parent\">" +
                                                            "</LinearLayout>");
-    NlModel model = SyncNlModel.create(createSurface(NlDesignSurface.class), myFixture.getProject(), myFacet, modelXml);
+    NlModel model = SyncNlModel.create(createSurface(NlDesignSurface.class), myFixture.getProject(), myFacet, modelXml.getVirtualFile());
     ModelListener listener1 = mock(ModelListener.class);
     model.addListener(listener1);
 
@@ -792,7 +792,7 @@ public class NlModelTest extends LayoutTestCase {
                                                            "         android:layout_width=\"match_parent\"" +
                                                            "         android:layout_height=\"match_parent\">" +
                                                            "</RelativeLayout>");
-    NlModel model = SyncNlModel.create(createSurface(NlDesignSurface.class), myFixture.getProject(), myFacet, modelXml);
+    NlModel model = SyncNlModel.create(createSurface(NlDesignSurface.class), myFixture.getProject(), myFacet, modelXml.getVirtualFile());
 
     long expectedModificationCount = model.getModificationCount();
     for (NlModel.ChangeType changeType : NlModel.ChangeType.values()) {

designer/testSrc/com/android/tools/idea/naveditor/scene/ThumbnailManagerTest.java
@@ -60,7 +60,7 @@ public class ThumbnailManagerTest extends NavigationTestCase {
     XmlFile psiFile = (XmlFile)PsiManager.getInstance(getProject()).findFile(file);
 
     DesignSurface surface = mock(NavDesignSurface.class);
-    NlModel model = NlModel.create(myRootDisposable, myFacet, psiFile);
+    NlModel model = NlModel.create(myRootDisposable, myFacet, psiFile.getVirtualFile());
     CompletableFuture<ImagePool.Image> imageFuture = manager.getThumbnail(psiFile, surface, model.getConfiguration());
     ImagePool.Image image = imageFuture.get();
     imageFuture = manager.getThumbnail(psiFile, surface, model.getConfiguration());

commit 21cca12245134a5684d2c424b0f65053e9c8d31f

There is no Psi file for models with an invalid Virtual file
Deleting the layout file of the layout currently displayed in the
Layout Editor would ask for the Psi file which does not exist anymore.
Check if the VirtualFile is invalid instead.
This was broken by commit 39bb590e5c, which switched getFile() from returning
a stored Psi file reference to recomputing the Psi file every time.

Since introducing a getVirtualFile method in NlModel, refactor all
usages of model.getFile().getVirtualFile() to model.getVirtualFile().

Test: NlPreviewTest.deletePreviewedFile
Change-Id: Ia508ad9add0de405711c053a933ce4350cc241ea

designer/src/com/android/tools/idea/common/actions/GotoComponentAction.java
@@ -80,7 +80,7 @@ public class GotoComponentAction extends DumbAwareAction {
     // If the xml file is empty, just open the text editor
     FileEditorManager editorManager = FileEditorManager.getInstance(mySurface.getProject());
     editorManager.openTextEditor(
-      new OpenFileDescriptor(model.getProject(), model.getFile().getVirtualFile(), 0), true);
+      new OpenFileDescriptor(model.getProject(), model.getVirtualFile(), 0), true);
   }
 }
 

designer/src/com/android/tools/idea/common/model/NlModel.java
@@ -172,6 +172,11 @@ public class NlModel implements Disposable, ResourceChangeListener, Modification
   }
 
   @NotNull
+  public VirtualFile getVirtualFile() {
+    return myFile;
+  }
+
+  @NotNull
   public XmlFile getFile() {
     XmlFile file = (XmlFile)AndroidPsiUtils.getPsiFileSafely(getProject(), myFile);
     assert file != null;

designer/src/com/android/tools/idea/common/surface/DesignSurface.java
@@ -229,7 +229,7 @@ public abstract class DesignSurface extends EditorDesignSurface implements Dispo
     if (myModel == null) {
       return NlLayoutType.UNKNOWN;
     }
-    return NlLayoutType.typeOf(myModel.getFile());
+    return myModel.getType();
   }
 
   @NotNull

designer/src/com/android/tools/idea/naveditor/scene/layout/ManualLayoutAlgorithm.java
@@ -114,7 +114,7 @@ public class ManualLayoutAlgorithm implements NavSceneLayoutAlgorithm {
       String id = NavComponentHelperKt.getResolvedId(current);
       if (id == null) {
         NlModel model = current.getModel();
-        Document doc = FileDocumentManager.getInstance().getDocument(model.getFile().getVirtualFile());
+        Document doc = FileDocumentManager.getInstance().getDocument(model.getVirtualFile());
         int line = -1;
         if (doc != null) {
           line = doc.getLineNumber(current.getTag().getTextRange().getStartOffset()) + 1;

designer/src/com/android/tools/idea/uibuilder/actions/MorphComponentAction.kt
@@ -99,7 +99,7 @@ class MorphComponentAction(component: NlComponent, designSurface: DesignSurface)
     val document = factory.createDocument(buildRangesAndGetString(myNlComponent, myAttributes))
     val editor = factory.createEditor(document, myProject, XmlFileType.INSTANCE, false) as EditorEx
 
-    val syntaxHighlighter = SyntaxHighlighterFactory.getSyntaxHighlighter(XmlFileType.INSTANCE, myProject, mySurface.model?.file?.virtualFile)
+    val syntaxHighlighter = SyntaxHighlighterFactory.getSyntaxHighlighter(XmlFileType.INSTANCE, myProject, mySurface.model?.virtualFile)
     syntaxHighlighter?.let {
       editor.highlighter = LexerEditorHighlighter(it, EditorColorsManager.getInstance().globalScheme)
     }
@@ -147,7 +147,7 @@ class MorphComponentAction(component: NlComponent, designSurface: DesignSurface)
               myNlComponent.tag.setAttribute(it, null)
             }
         myNlComponent.removeObsoleteAttributes()
-        myNlComponent.getChildren().forEach(NlComponent::removeObsoleteAttributes)
+        myNlComponent.children.forEach(NlComponent::removeObsoleteAttributes)
       }
     })
   }

designer/src/com/android/tools/idea/uibuilder/handlers/NavigationViewHandler.java
@@ -61,7 +61,7 @@ final class NavigationViewHandler extends FrameLayoutHandler {
     }
 
     NlModel model = component.getModel();
-    editor.openResource(model.getConfiguration(), resource, model.getFile().getVirtualFile());
+    editor.openResource(model.getConfiguration(), resource, model.getVirtualFile());
   }
 
   private static boolean contains(@NotNull View view, @AndroidCoordinate int x, @AndroidCoordinate int y) {

designer/src/com/android/tools/idea/uibuilder/property/NlXmlPropertyBuilder.java
@@ -69,7 +69,7 @@ public class NlXmlPropertyBuilder {
     }
     NlComponent component = myComponents.get(0);
     List<PTableItem> items = new ArrayList<>();
-    items.add(new NlResourceHeader(generateFileHeader(component.getModel().getFile().getVirtualFile().getPath())));
+    items.add(new NlResourceHeader(generateFileHeader(component.getModel().getVirtualFile().getPath())));
     for (AttributeSnapshot attribute : component.getAttributes()) {
       NlPropertyItem item = myProperties.get(attribute.namespace, attribute.name);
       if (item != null) {

designer/src/com/android/tools/idea/uibuilder/scene/LayoutlibSceneManager.java
@@ -497,7 +497,7 @@ public class LayoutlibSceneManager extends SceneManager {
         Project project = model.getModule().getProject();
         if (project.isOpen()) {
           DumbService.getInstance(project).waitForSmartMode();
-          if (model.getFile().isValid() && !model.getFacet().isDisposed()) {
+          if (model.getVirtualFile().isValid() && !model.getFacet().isDisposed()) {
             try {
               updateModel();
             }

designer/src/com/android/tools/idea/uibuilder/structure/BackNavigationComponent.java
@@ -72,7 +72,7 @@ public class BackNavigationComponent extends JPanel {
     NlModel model = myDesignSurface.getModel();
     if (model != null) {
       LayoutNavigationManager layoutNavigationManager = LayoutNavigationManager.getInstance(myDesignSurface.getProject());
-      VirtualFile modelFile = model.getFile().getVirtualFile();
+      VirtualFile modelFile = model.getVirtualFile();
       VirtualFile parentFile = layoutNavigationManager.get(modelFile);
       if (parentFile != null) {
         setVisible(true);

designer/testSrc/com/android/tools/idea/naveditor/scene/NavSceneTest.java
@@ -296,7 +296,7 @@ public class NavSceneTest extends NavigationTestCase {
         fragmentComponent("fragment2")
           .withLayoutAttribute("activity_main2"));
     SyncNlModel model = model("nav.xml", root).build();
-    FileEditor editor = new TestNlEditor(model.getFile().getVirtualFile(), getProject());
+    FileEditor editor = new TestNlEditor(model.getVirtualFile(), getProject());
 
     Scene scene = model.getSurface().getScene();
 