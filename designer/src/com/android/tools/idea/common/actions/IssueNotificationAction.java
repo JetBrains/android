acd6092a12d8c1667268797dfdb1127aa0c1aef6
Vadim Caen <caen@google.com>
2017-11-22 17:32:57 +0000

    Toggle Button for Issue Notification button
    Also Fix the flashing issue
    
    Bug: 69437104
    Bug: 69436958
    Test: N/A
    Change-Id: I82f15cb1007d0bbbd3c94e757033b0e528916cd9

--- designer/src/com/android/tools/idea/common/actions/IssueNotificationAction.java
+++ designer/src/com/android/tools/idea/common/actions/IssueNotificationAction.java
@@ -19,9 +19,9 @@ import com.android.tools.idea.common.analytics.NlUsageTrackerManager;
 import com.android.tools.idea.common.surface.DesignSurface;
 import com.android.tools.idea.uibuilder.error.IssueModel;
 import com.google.wireless.android.sdk.stats.LayoutEditorEvent;
-import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.Presentation;
+import com.intellij.openapi.actionSystem.ToggleAction;
 import com.intellij.util.IconUtil;
 import icons.StudioIcons;
 import org.jetbrains.annotations.NotNull;
@@ -32,22 +32,20 @@ import javax.swing.*;
  * Action which shows the current number of warnings in the layout
  * and when clicked, shows them
  */
-public class IssueNotificationAction extends AnAction {
+public class IssueNotificationAction extends ToggleAction {
   public static final String NO_ISSUE = "No Issue";
   public static final String SHOW_ISSUE = "Show Warnings and Errors";
   private static final Icon DISABLED_ICON = IconUtil.desaturate(StudioIcons.Common.ERROR);
   private final DesignSurface mySurface;
-  //private TextIcon myTextIcon = new TextIcon("", JBColor.white, null, 2);
 
   public IssueNotificationAction(@NotNull DesignSurface surface) {
     super(NO_ISSUE, NO_ISSUE, null);
     mySurface = surface;
-    setDefaultIcon(false);
-    getTemplatePresentation().setIcon(getIssueTypeIcon(mySurface.getIssueModel()));
   }
 
   @Override
   public void update(@NotNull AnActionEvent event) {
+    super.update(event);
     Presentation presentation = event.getPresentation();
     IssueModel issueModel = mySurface.getIssueModel();
     int markerCount = issueModel.getIssueCount();
@@ -57,9 +55,14 @@ public class IssueNotificationAction extends AnAction {
   }
 
   @Override
-  public void actionPerformed(@NotNull AnActionEvent event) {
+  public boolean isSelected(AnActionEvent e) {
+    return !mySurface.getIssuePanel().isMinimized();
+  }
+
+  @Override
+  public void setSelected(AnActionEvent e, boolean state) {
     NlUsageTrackerManager.getInstance(mySurface).logAction(LayoutEditorEvent.LayoutEditorEventType.SHOW_LINT_MESSAGES);
-    mySurface.setShowIssuePanel(true);
+    mySurface.setShowIssuePanel(state);
   }
 
   @NotNull
