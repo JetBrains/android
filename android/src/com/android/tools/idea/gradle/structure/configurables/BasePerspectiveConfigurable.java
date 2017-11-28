a1b104972d6f95ef2131dba5251a5d4be0ccc8f4
Yuriy Solodkyy <solodkyy@google.com>
2017-11-28 16:53:44 +0000

    [New PSD] Update the module list parent on re-initialization.
    If the BasePerspectiveConfigurable is re-initialized its panels might be
    re-created and the module list selector must be re-added.
    
    Bug: 69788672
    Test: UI test follows in a separate CL
    Change-Id: Idd6662b8cc6269b564d79909fef855e58830e21f

--- android/src/com/android/tools/idea/gradle/structure/configurables/BasePerspectiveConfigurable.java
+++ android/src/com/android/tools/idea/gradle/structure/configurables/BasePerspectiveConfigurable.java
@@ -202,14 +202,19 @@ public abstract class BasePerspectiveConfigurable extends MasterDetailsComponent
       myCenterComponent = splitter;
 
       JComponent first = splitter.getFirstComponent();
-      if (first instanceof JPanel && myHeader == null) {
+      if (first instanceof JPanel) {
         JPanel panel = (JPanel)first;
-        myHeader = ToolWindowHeader.createAndAdd("Modules", ANDROID_MODULE, panel, ToolWindowAnchor.LEFT);
-        myHeader.setPreferredFocusedComponent(myTree);
-        myHeader.addMinimizeListener(() -> {
-          modulesTreeMinimized();
-          reInitWholePanelIfNeeded();
-        });
+        if (myHeader == null) {
+          myHeader = ToolWindowHeader.createAndAdd("Modules", ANDROID_MODULE, panel, ToolWindowAnchor.LEFT);
+          myHeader.setPreferredFocusedComponent(myTree);
+          myHeader.addMinimizeListener(() -> {
+            modulesTreeMinimized();
+            reInitWholePanelIfNeeded();
+          });
+        }
+        else if (myHeader.getParent() != panel) {
+          panel.add(myHeader, BorderLayout.NORTH);
+        }
       }
     }
   }
