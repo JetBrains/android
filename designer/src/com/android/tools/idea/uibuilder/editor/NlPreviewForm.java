abb365cfae4ca6c3196658179351613470a42602
Justin Nieto <justinnieto@google.com>
2017-11-30 15:46:20 -0800

    Replace DelayedInitialization with ClearResourceCacheAfterFirstBuild
    DelayedInitialization is responsible for clearing the
    resource cache after source generation is complete and for waiting
    to initialize the designer and preview
    tabs of the layout editor until after the resource cache has been
    cleared. Because its implementation is heavily dependent on Gradle,
    DelayedInitialization is currently disabled for Blaze projects.
    
    This CL resolves the issue by introducing
    addSourceGenerationCallback(), a build system-independent
    API in ProjectSystemSyncManager that allows callers to specify a
    callback to run once the first project sync with source generation
    is complete.
    
    This CL also replaces DelayedInitialization with
    ClearResourceCacheAfterFirstBuild, which uses
    addSourceGenerationCallback() to clear the resource cache at the
    appropriate time (if necessary) and provides an API for executing
    callbacks after the cache has been validated.
    
    Bug:  65213155
    Test: GradleProjectSystemSyncManagerTest
          + ResourceCacheClearingComponentTest
    
    Change-Id: Ibca2cac11933f6daa91eab17ca167a1d6d6e53dd

--- designer/src/com/android/tools/idea/uibuilder/editor/NlPreviewForm.java
+++ designer/src/com/android/tools/idea/uibuilder/editor/NlPreviewForm.java
@@ -24,10 +24,9 @@ import com.android.tools.idea.common.model.*;
 import com.android.tools.idea.common.surface.DesignSurface;
 import com.android.tools.idea.common.surface.SceneView;
 import com.android.tools.idea.flags.StudioFlags;
-import com.android.tools.idea.gradle.project.GradleProjectInfo;
 import com.android.tools.idea.rendering.RenderResult;
-import com.android.tools.idea.startup.DelayedInitialization;
 import com.android.tools.idea.uibuilder.error.IssuePanelSplitter;
+import com.android.tools.idea.startup.ClearResourceCacheAfterFirstBuild;
 import com.android.tools.idea.uibuilder.handlers.transition.TransitionLayoutHandler;
 import com.android.tools.idea.uibuilder.model.NlComponentHelperKt;
 import com.android.tools.idea.uibuilder.model.NlModelHelperKt;
@@ -282,12 +281,7 @@ public class NlPreviewForm implements Disposable, CaretListener {
     }
 
     if (myContentPanel == null) {  // First time: Make sure we have compiled the project at least once...
-      if (GradleProjectInfo.getInstance(myProject).isBuildWithGradle()) {
-        DelayedInitialization.getInstance(myProject).runAfterBuild(this::initPreviewForm, this::buildError);
-      }
-      else {
-        initPreviewForm();
-      }
+      ClearResourceCacheAfterFirstBuild.getInstance(myProject).runWhenResourceCacheClean(this::initPreviewForm, this::buildError);
     }
     else {
       initNeleModel();
--- project-system-gradle/testSrc/com/android/tools/idea/projectsystem/GradleProjectSystemTest.java
+++ project-system-gradle/testSrc/com/android/tools/idea/projectsystem/GradleProjectSystemTest.java
@@ -18,30 +18,16 @@ package com.android.tools.idea.projectsystem;
 import com.android.tools.idea.gradle.dependencies.GradleDependencyManager;
 import com.android.tools.idea.gradle.project.GradleProjectInfo;
 import com.android.tools.idea.gradle.project.build.GradleProjectBuilder;
-import com.android.tools.idea.gradle.project.sync.GradleSyncInvoker;
-import com.android.tools.idea.projectsystem.ProjectSystemSyncManager.SyncReason;
-import com.android.tools.idea.projectsystem.ProjectSystemSyncManager.SyncResult;
 import com.android.tools.idea.projectsystem.gradle.GradleProjectSystem;
-import com.android.tools.idea.projectsystem.gradle.GradleProjectSystemSyncManager;
 import com.android.tools.idea.testing.IdeComponents;
-import com.intellij.ide.startup.StartupManagerEx;
-import com.intellij.ide.startup.impl.StartupManagerImpl;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.startup.StartupManager;
 import com.intellij.testFramework.IdeaTestCase;
-import org.jetbrains.annotations.NotNull;
-import org.mockito.Mockito;
 
-import java.util.concurrent.CountDownLatch;
-
-import static com.android.tools.idea.projectsystem.ProjectSystemSyncUtil.PROJECT_SYSTEM_SYNC_TOPIC;
 import static com.google.common.truth.Truth.assertThat;
 import static org.mockito.Mockito.*;
 
 public class GradleProjectSystemTest extends IdeaTestCase {
   private IdeComponents myIdeComponents;
   private GradleProjectInfo myGradleProjectInfo;
-  private ProjectSystemSyncManager mySyncManager;
 
   @Override
   protected void setUp() throws Exception {
@@ -52,8 +38,6 @@ public class GradleProjectSystemTest extends IdeaTestCase {
     myIdeComponents.mockProjectService(GradleProjectBuilder.class);
     myGradleProjectInfo = myIdeComponents.mockProjectService(GradleProjectInfo.class);
     when(myGradleProjectInfo.isBuildWithGradle()).thenReturn(true);
-
-    mySyncManager = new GradleProjectSystemSyncManager(myProject);
   }
 
   @Override
@@ -69,45 +53,8 @@ public class GradleProjectSystemTest extends IdeaTestCase {
     assertThat(ProjectSystemUtil.getProjectSystem(getProject())).isInstanceOf(GradleProjectSystem.class);
   }
 
-  public void testSyncProjectWithUninitializedProject() {
-    Project project = getProject();
-    StartupManagerEx startupManager = new StartupManagerImpl(project) {
-      @Override
-      public boolean startupActivityPassed() {
-        return false; // this will make Project.isInitialized return false;
-      }
-
-      @Override
-      public void runWhenProjectIsInitialized(@NotNull Runnable action) {
-        action.run();
-      }
-    };
-    myIdeComponents.replaceService(project, StartupManager.class, startupManager);
-    // http://b/62543184
-    when(myGradleProjectInfo.isImportedProject()).thenReturn(true);
-    GradleSyncInvoker mySyncInvoker = myIdeComponents.mockService(GradleSyncInvoker.class);
-
-    ProjectSystemUtil.getProjectSystem(project).getSyncManager().syncProject(SyncReason.PROJECT_LOADED, true);
-    Mockito.verify(mySyncInvoker, never()).requestProjectSync(same(project), any(), any());
-  }
-
   public void testBuildProject() {
     ProjectSystemUtil.getProjectSystem(getProject()).buildProject();
     verify(GradleProjectBuilder.getInstance(myProject)).compileJava();
-  }
-
-  public void testGetLastSyncResult_unknownIfNeverSynced() {
-    assertThat(mySyncManager.getLastSyncResult()).isSameAs(SyncResult.UNKNOWN);
-  }
-
-  public void testGetLastSyncResult_sameAsSyncResult() throws Exception {
-    CountDownLatch latch = new CountDownLatch(1);
-    myProject.getMessageBus().connect(myProject).subscribe(PROJECT_SYSTEM_SYNC_TOPIC, result -> latch.countDown());
-
-    // Emulate the completion of a successful sync.
-    myProject.getMessageBus().syncPublisher(PROJECT_SYSTEM_SYNC_TOPIC).syncEnded(SyncResult.SUCCESS);
-
-    latch.await();
-    assertThat(mySyncManager.getLastSyncResult()).isSameAs(SyncResult.SUCCESS);
   }
 }
