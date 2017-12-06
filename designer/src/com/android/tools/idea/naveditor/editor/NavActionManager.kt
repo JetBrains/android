b14d2ec8a96ca700fd92540549b3c3e4554ab161
Joe Baker-Malone <jbakermalone@google.com>
2017-12-05 17:12:13 -0800

    Fixing name/label confusion
    The "name" attribute was incorrectly populated before. Fixing it.
    
    Test: updated AddMenuWrapperTest
    Change-Id: I64c028adcab7e21b3b3160072c88fe9b60efab73

--- designer/src/com/android/tools/idea/naveditor/editor/NavActionManager.kt
+++ designer/src/com/android/tools/idea/naveditor/editor/NavActionManager.kt
@@ -105,16 +105,8 @@ class NavActionManager(surface: NavDesignSurface) : ActionManager<NavDesignSurfa
     }
 
   @VisibleForTesting
-  data class Destination(val layoutFile: XmlFile?, private val className: String, val qualifiedName: String,
+  data class Destination(val layoutFile: XmlFile?, val className: String, val qualifiedName: String,
                                             val tag: String, val thumbnail: Image?) {
-
-    val name: String
-      get() {
-        if (layoutFile != null) {
-          return FileUtil.getNameWithoutExtension(layoutFile.name)
-        }
-        return className
-      }
 
     companion object {
       fun create(layout: XmlFile?, psiClass: PsiClass, tag: String, thumbnail: Image): Destination? {
