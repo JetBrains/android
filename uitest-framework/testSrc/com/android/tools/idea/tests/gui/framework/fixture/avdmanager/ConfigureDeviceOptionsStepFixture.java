bdb6e098c87cbffa7b9e9a65031920442810e088
Dana Dahlstrom <dahlstrom@google.com>
2017-12-14 19:08:33 -0800

    Delete testCreateHardwareProfileErrors
    … because input validation should be unit-tested instead, and this
    method is failing consistently.
    
    Change-Id: I89765b6d46a73d095f5578aa379cf9b31f60f0d6

--- uitest-framework/testSrc/com/android/tools/idea/tests/gui/framework/fixture/avdmanager/ConfigureDeviceOptionsStepFixture.java
+++ uitest-framework/testSrc/com/android/tools/idea/tests/gui/framework/fixture/avdmanager/ConfigureDeviceOptionsStepFixture.java
@@ -31,22 +31,4 @@ public class ConfigureDeviceOptionsStepFixture extends AbstractWizardStepFixture
     replaceText(findTextFieldWithLabel("Device Name"), deviceName);
     return this;
   }
-
-  @NotNull
-  public ConfigureDeviceOptionsStepFixture setScreenSize(String screenSize) {
-    replaceText(findTextFieldWithLabel("Screen size:"), screenSize);
-    return this;
-  }
-
-  @NotNull
-  public ConfigureDeviceOptionsStepFixture setScreenResolutionX(String width) {
-    replaceText(findTextFieldWithLabel("Resolution:"), width);
-    return this;
-  }
-
-  @NotNull
-  public ConfigureDeviceOptionsStepFixture setScreenResolutionY(String height) {
-    replaceText(findTextFieldWithLabel("x"), height);
-    return this;
-  }
 }
