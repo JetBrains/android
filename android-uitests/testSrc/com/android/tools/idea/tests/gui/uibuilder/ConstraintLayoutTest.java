3a27dad06b100031ccc90e0ffa6e842ac70c264f
Jingjing Liu <jingjingliu@google.com>
2017-12-20 11:47:23 -0800

    Fix timed out waiting for render to finish
    testBaselineConstraintHandling failed sometimes on build bot for timed
    out waiting for render to finish. The default timeout given is 10s,
    here, use 30s since build bot is slower.
    
    Test: ran this test locally, passed, will monitor it on build bot
    Change-Id: I8545e90652cdacbabdaa03497d9610b43368949e

--- android-uitests/testSrc/com/android/tools/idea/tests/gui/uibuilder/ConstraintLayoutTest.java
+++ android-uitests/testSrc/com/android/tools/idea/tests/gui/uibuilder/ConstraintLayoutTest.java
@@ -39,6 +39,7 @@ import com.intellij.BundleBase;
 import org.fest.swing.core.GenericTypeMatcher;
 import org.fest.swing.data.TableCell;
 import org.fest.swing.fixture.JTableCellFixture;
+import org.fest.swing.timing.Wait;
 import org.intellij.lang.annotations.Language;
 import org.jetbrains.annotations.NotNull;
 import org.junit.Ignore;
@@ -164,7 +165,7 @@ public class ConstraintLayoutTest {
     NlEditorFixture layoutEditor = editor.getLayoutEditor(true);
 
     layoutEditor
-      .waitForRenderToFinish()
+      .waitForRenderToFinish(Wait.seconds(30))
       .showOnlyDesignView()
       .dragComponentToSurface("Buttons", "Button")
       .findView("Button", 0)
