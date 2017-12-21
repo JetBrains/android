1cc251866f32ead48d471eb21ea2d93f1cac5cb9
Yuriy Solodkyy <solodkyy@google.com>
2017-12-21 21:53:58 +0000

    UI property model definition and simple editor.
    * The collection of interfaces, classes and helper functions to define UI
    models of properties to be edited
    * SimplePropertyEditor implements a ComboBox based editor for simple-typed
    proeprties
    
    Bug: 69665954
    Test: SimplePropertyEditorTest
    Change-Id: Ice5003a7bad8539515ad71578a4466a44a71c5dd

---
+++ android/src/com/android/tools/idea/gradle/structure/configurables/ui/properties/SimplePropertyEditor.kt
@@ -0,0 +1,142 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.gradle.structure.configurables.ui.properties
+
+import com.android.tools.idea.gradle.structure.model.meta.*
+import com.intellij.openapi.ui.ComboBox
+import java.awt.Color
+import java.awt.Dimension
+import javax.swing.DefaultComboBoxModel
+import javax.swing.JComponent
+import javax.swing.JTextField
+
+/**
+ * A property editor [ModelPropertyEditor] for properties of simple (not complex) types.
+ *
+ * This is a [ComboBox] based editor allowing manual text entry as well as entry by selecting an item from the list of values provided by
+ * [ModelSimpleProperty.getKnownValues]. Text free text input is parsed by [ModelSimpleProperty.parse].
+ */
+class SimplePropertyEditor<ModelT, PropertyT>(
+    val model: ModelT,
+    val property: ModelSimpleProperty<ModelT, PropertyT>
+) : ComboBox<String>(), ModelPropertyEditor<ModelT, PropertyT> {
+
+  private val textToValue: Map<String, PropertyT>
+  private val valueToText: Map<PropertyT, String>
+  private var beingLoaded = false
+
+  override val component: JComponent = this
+
+  override fun getPreferredSize(): Dimension {
+    val dimensions = super.getPreferredSize()
+    return if (dimensions.width < 200) {
+      Dimension(200, dimensions.height)
+    }
+    else dimensions
+  }
+
+  private fun getParsedValue(): ParsedValue<PropertyT> {
+    val text = editor.item.toString()
+    return textToValue[text]?.let { ParsedValue.Set.Parsed(value = it) } ?: property.parse(text)
+  }
+
+  private fun setText(text: String) {
+    selectedItem = text
+  }
+
+  private fun setValue(value: PropertyT?) {
+    selectedItem = if (value == null) "" else (valueToText[value] ?: value.toString())
+  }
+
+  private fun setColorAndTooltip(toolTipText: String, background: Color) {
+    val jTextField = editor.editorComponent as? JTextField
+    if (jTextField != null) {
+      jTextField.toolTipText = toolTipText
+      jTextField.background = background
+    }
+  }
+
+  private fun loadValue(value: PropertyValue<PropertyT>) {
+    beingLoaded = true
+    try {
+      when (value.parsedValue) {
+        is ParsedValue.NotSet -> {
+          setText("")
+        }
+        is ParsedValue.Set.Parsed -> {
+          setValue(value.parsedValue.value)
+        }
+        is ParsedValue.Set.Invalid -> {
+          setText(value.parsedValue.dslText)
+        }
+      }
+      val defaultValue = property.getDefaultValue(model)
+      when {
+        value.resolved is ResolvedValue.NotResolved && value.parsedValue is ParsedValue.Set -> {
+          setColorAndTooltip(
+              toolTipText = "[Set but not resolved - not yet synced?]",
+              background = Color.GREEN
+          )
+        }
+        value.resolved is ResolvedValue.Set &&
+            (value.parsedValue is ParsedValue.Set.Parsed &&
+                value.resolved.resolved != value.parsedValue.value ||
+            value.parsedValue is ParsedValue.NotSet &&
+                value.resolved.resolved != defaultValue)
+        -> {
+          setColorAndTooltip(
+              toolTipText = "[Set does not match resolved?]",
+              background = Color.YELLOW
+          )
+        }
+        value.parsedValue is ParsedValue.Set.Invalid -> {
+          setColorAndTooltip(
+              toolTipText = "[Invalid?]",
+              background = Color.RED
+          )
+        }
+      }
+    }
+    finally {
+      beingLoaded = false
+    }
+  }
+
+  private fun applyChanges(value: ParsedValue<PropertyT>) {
+    when (value) {
+      is ParsedValue.Set.Invalid -> Unit
+      else -> property.setValue(model!!, value)
+    }
+  }
+
+  init {
+    minLength = 60
+    setEditable(true)
+
+    val possibleValues = property.getKnownValues(model) ?: listOf()
+    textToValue = possibleValues.associate { it.description to it.value }
+    valueToText = possibleValues.associate { it.value to it.description }
+    super.setModel(DefaultComboBoxModel<String>(textToValue.keys.toTypedArray()))
+
+    loadValue(property.getValue(model))
+
+    addActionListener {
+      if (!beingLoaded) {
+        applyChanges(getParsedValue())
+      }
+    }
+  }
+}
---
+++ android/src/com/android/tools/idea/gradle/structure/model/meta/PropertiesUiModel.kt
@@ -0,0 +1,23 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.gradle.structure.model.meta
+
+/**
+ * A model of the UI for editing the properties of a model of type [ModelT].
+ *
+ * This is the basic UI model defining a set of properties to be edited and their order.
+ */
+class PropertiesUiModel<in ModelT>(val properties: List<ModelProperty<ModelT, *>>)
