e7a643f4438840aa915f148ea346d55b8b654f3a
Dmitry Avdeev <dmitry.avdeev@jetbrains.com>
2017-12-27 19:01:09 +0300

    startup sync disabled for non-android projects
    (cherry picked from commit 87f2e6e)

--- android/src/com/android/tools/idea/gradle/project/AndroidGradleProjectStartupActivity.java
+++ android/src/com/android/tools/idea/gradle/project/AndroidGradleProjectStartupActivity.java
@@ -27,7 +27,9 @@ public class AndroidGradleProjectStartupActivity implements StartupActivity {
   @Override
   public void runActivity(@NotNull Project project) {
     GradleProjectInfo gradleProjectInfo = GradleProjectInfo.getInstance(project);
-    if (gradleProjectInfo.isBuildWithGradle() && !gradleProjectInfo.isImportedProject()) {
+    if (!gradleProjectInfo.getAndroidModules().isEmpty() &&
+        gradleProjectInfo.isBuildWithGradle() &&
+        !gradleProjectInfo.isImportedProject()) {
       // http://b/62543184
       // If the project was created with the "New Project" wizard or imported, there is no need to sync again.
       // This code path should only be executed when:
