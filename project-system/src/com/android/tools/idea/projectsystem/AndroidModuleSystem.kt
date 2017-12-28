f13c959ea5c929d8b7a88c706bc69d2ca197d77a
Joe Baker-Malone <jbakermalone@google.com>
2017-12-28 13:04:48 -0800

    Enable adding preview version of dependency
    This will be exercised in a soon-to-come CL.
    
    Test: GradleModuleSystemTest.testAddPreviewDependency,
    testFailToAddPreviewDependency
    
    Change-Id: Ic31552d089b16a480d0ad31ad3241f7474850b0c

--- project-system/src/com/android/tools/idea/projectsystem/AndroidModuleSystem.kt
+++ project-system/src/com/android/tools/idea/projectsystem/AndroidModuleSystem.kt
@@ -57,11 +57,13 @@ interface AndroidModuleSystem {
    * The caller may specify a specific [version] of the artifact to add.  If no [version] is passed or the passed [version] is null,
    * the artifact added will be of the latest version supported by the underlying build system of this project.
    * @param artifactId  Id of artifact needed by the dependent.
-   * @param version   Version of the artifact to add.  If left blank this parameter defaults to {@link GoogleMavenArtifact.VERSION_LATEST}
+   * @param version Version of the artifact to add.  If left blank this parameter defaults to {@link GoogleMavenArtifact.VERSION_LATEST}
+   * @param includePreview Whether preview versions should be included when looking up the latest version.
    * @throws DependencyManagementException if an error occurs when trying to add the dependency.
    */
   @Throws(DependencyManagementException::class)
-  fun addDependencyWithoutSync(artifactId: GoogleMavenArtifactId, version: GoogleMavenArtifactVersion? = null)
+  fun addDependencyWithoutSync(artifactId: GoogleMavenArtifactId, version: GoogleMavenArtifactVersion? = null,
+                               includePreview: Boolean = false)
 
   /**
    * Determines whether or not the underlying build system is capable of generating a PNG
