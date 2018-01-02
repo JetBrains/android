ab61f83363cb3ddd894b300c4144feefd669d6a9
Alexander Wang <afwang@google.com>
2018-01-02 11:27:25 -0800

    NewActivityTest.createDefaultActivity is stable
    This test has been running stably, so it should be moved
    to its reliable group.
    
    Bug: 70716383
    Test: NewActivityTest.createDefaultActivity
    Change-Id: I4c5b0fcb2de5556eaf0561e02dd4ab4130e429e0

--- android-uitests/testSrc/com/android/tools/idea/tests/gui/npw/NewActivityTest.java
+++ android-uitests/testSrc/com/android/tools/idea/tests/gui/npw/NewActivityTest.java
@@ -93,7 +93,7 @@ public class NewActivityTest {
    *   Activity class and layout.xml files are created. The activity previews correctly in layout editor.
    *   </pre>
    */
-  @RunIn(TestGroup.QA_UNRELIABLE) // http://b/70716383
+  @RunIn(TestGroup.SANITY)
   @Test
   public void createDefaultActivity() {
     myDialog.clickFinish();
