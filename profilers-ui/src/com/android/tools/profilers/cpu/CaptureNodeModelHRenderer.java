b16a441610975baa8f192340cf25a731ba037cfa
Madiyar Aitbayev <madiyar@google.com>
2018-01-09 18:26:26 +0000

    Refactor AdtUIUtils#getFittedString
    Follow up from: http://ag/3427693
    
    1. Rename to shrinkToFit
    2. Change signature order from (fontMetrics, text, ...) -> (text, ...)
    
    Test: just refactoring
    Change-Id: Ib0a72f71365316e59725e80f10bed7685a7e025a

--- profilers-ui/src/com/android/tools/profilers/cpu/CaptureNodeModelHRenderer.java
+++ profilers-ui/src/com/android/tools/profilers/cpu/CaptureNodeModelHRenderer.java
@@ -167,7 +167,7 @@ public class CaptureNodeModelHRenderer implements HRenderer<CaptureNode> {
     }
 
     // Try: toString or t...
-    return AdtUiUtils.getFittedString(textFitsPredicate, model.getName(), 1);
+    return AdtUiUtils.shrinkToFit(model.getName(), textFitsPredicate, 1);
   }
 
   public interface TextFitsPredicate {
