f0fc136b59d060ca2cfdd8179acbd935a7a7642c
Nathan Paige <npaige@google.com>
2018-01-08 19:02:48 -0800

    Fix ApkViewerTest and others
    Several UI tests have been having trouble navigating to a path in the
    project view, due to a bug in the implementation of
    ProjectViewFixture.waitForTreeToFinishLoading(). This previously tested
    for the absence of the spinning wheel; however, this icon is not
    guaranteed to be displayed, especially for short loading tasks. This
    resulted in the waitForTreeToFinishLoading() method to return
    immediately, even though the tree was not done loading.
    
    This CL replaces that behavior with waiting for the JTree's model's
    isProcessing() method to return false.
    
    Bug: b/70580328, b/70633876
    Test: ApkViewerTest and others
    Change-Id: I28df4638a3bbfbcc70442087e2d3af25e6f15af2

--- uitest-framework/testSrc/com/android/tools/idea/tests/gui/framework/fixture/ProjectViewFixture.java
+++ uitest-framework/testSrc/com/android/tools/idea/tests/gui/framework/fixture/ProjectViewFixture.java
@@ -42,6 +42,7 @@ import com.intellij.openapi.roots.JdkOrderEntry;
 import com.intellij.openapi.roots.LibraryOrSdkOrderEntry;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.openapi.wm.impl.content.BaseLabel;
+import com.intellij.ui.tree.AsyncTreeModel;
 import com.intellij.util.ui.AsyncProcessIcon;
 import com.intellij.util.ui.tree.TreeUtil;
 import org.fest.swing.core.GenericTypeMatcher;
@@ -57,6 +58,7 @@ import org.jetbrains.annotations.NotNull;
 import javax.annotation.Nonnull;
 import javax.swing.JMenuItem;
 import javax.swing.KeyStroke;
+import javax.swing.tree.TreeModel;
 import java.awt.Component;
 import java.awt.Container;
 import java.util.List;
@@ -104,7 +106,6 @@ public class ProjectViewFixture extends ToolWindowFixture {
   }
 
   private void changePane(@NotNull String paneName) {
-    waitForTreeToFinishLoading(myRobot, myToolWindow.getComponent(), 120);
     Component projectDropDown = GuiTests.waitUntilFound(myRobot, Matchers.byText(BaseLabel.class, "Project:"));
 
     myRobot.click(projectDropDown.getParent());
@@ -114,17 +115,6 @@ public class ProjectViewFixture extends ToolWindowFixture {
     myRobot.pressAndReleaseKey(firstKeyStroke.getKeyCode(), firstKeyStroke.getModifiers());
 
     GuiTests.clickPopupMenuItem("Content name=" + paneName, projectDropDown, myRobot);
-  }
-
-  private static void waitForTreeToFinishLoading(@NotNull Robot robot,
-                                                 @NotNull Container root,
-                                                 int secondsToWait) {
-    GuiTests.waitUntilGone(robot, root, new GenericTypeMatcher<AsyncProcessIcon>(AsyncProcessIcon.class) {
-      @Override
-      protected boolean isMatching(@Nonnull AsyncProcessIcon component) {
-        return component.isRunning();
-      }
-    }, secondsToWait);
   }
 
   @NotNull
@@ -140,7 +130,7 @@ public class ProjectViewFixture extends ToolWindowFixture {
       changePane(name);
     }
 
-    return new PaneFixture(ideFrameFixture, projectView.getCurrentProjectViewPane(), myRobot);
+    return new PaneFixture(ideFrameFixture, projectView.getCurrentProjectViewPane(), myRobot).waitForTreeToFinishLoading();
   }
 
   public static class PaneFixture {
@@ -157,8 +147,15 @@ public class ProjectViewFixture extends ToolWindowFixture {
     }
 
     @NotNull
+    private PaneFixture waitForTreeToFinishLoading() {
+      Wait.seconds(5).expecting("tree to load").until(() -> !(((AsyncTreeModel) myTree.target().getModel()).isProcessing()));
+      return this;
+    }
+
+    @NotNull
     public PaneFixture expand() {
       GuiTask.execute(() -> TreeUtil.expandAll(myPane.getTree()));
+      waitForTreeToFinishLoading();
       return this;
     }
 
@@ -241,7 +238,6 @@ public class ProjectViewFixture extends ToolWindowFixture {
     }
 
     public IdeFrameFixture clickPath(@NotNull MouseButton button, @NotNull final String... paths) {
-      waitForTreeToFinishLoading(myRobot, myTree.target(), 60);
       StringBuilder totalPath = new StringBuilder(paths[0]);
       for (int i = 1; i < paths.length; i++) {
         myTree.expandPath(totalPath.toString());
