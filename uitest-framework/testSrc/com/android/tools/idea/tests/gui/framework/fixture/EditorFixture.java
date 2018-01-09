6d3e6b7808276a67f91f059d573e4685b09c12ff
Jerome Gaillard <jgaillard@google.com>
2018-01-09 14:49:02 +0000

    Fix ConstraintLayoutTest.testLayoutPreviewRendering
    Change I609742b5 changed the label for API targets in the configuration
    toolbar. That caused ConstraintLayoutTest.testLayoutPreviewRendering to
    fail. This updates the test.
    
    Test: ConstraintLayoutTest.testLayoutPreviewRendering
    Change-Id: I1ad9db975f2439133a5e451ea01c51f57d5ee622

--- uitest-framework/testSrc/com/android/tools/idea/tests/gui/framework/fixture/EditorFixture.java
+++ uitest-framework/testSrc/com/android/tools/idea/tests/gui/framework/fixture/EditorFixture.java
@@ -598,7 +598,7 @@ public class EditorFixture {
       myFrame.invokeMenuPath("View", "Tool Windows", "Preview");
     }
 
-    Wait.seconds(2).expecting("Preview window to be visible")
+    Wait.seconds(10).expecting("Preview window to be visible")
       .until(() -> NlPreviewManager.getInstance(myFrame.getProject()).getPreviewForm().getSurface().isShowing());
 
     return new NlPreviewFixture(myFrame.getProject(), myFrame.robot());
