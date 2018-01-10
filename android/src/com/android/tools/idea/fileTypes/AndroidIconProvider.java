16576ba910a96fc3f4d99c6e9896cc78e6677491
Konstantin Bulenkov <kb@jetbrains.com>
2018-01-10 17:49:40 +0100

    revert temporary fix

--- android/src/com/android/tools/idea/fileTypes/AndroidIconProvider.java
+++ android/src/com/android/tools/idea/fileTypes/AndroidIconProvider.java
@@ -28,7 +28,6 @@ import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiDirectory;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.xml.XmlFile;
-import com.intellij.util.PlatformUtils;
 import icons.AndroidIcons;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -66,8 +65,7 @@ public class AndroidIconProvider extends IconProvider {
       }
     }
     // Use Andorid Studio icons for module's root
-    //todo move this code from the plugin to Android Studio specific part
-    if (!PlatformUtils.isJetBrainsProduct() && element instanceof PsiDirectory) {
+    if (element instanceof PsiDirectory) {
       PsiDirectory psiDirectory = (PsiDirectory)element;
       VirtualFile virtualDirectory = psiDirectory.getVirtualFile();
       Project project = psiDirectory.getProject();
