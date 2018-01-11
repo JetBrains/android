e23242126b81b54ffa50a68815ca51363c1259a8
Dana Dahlstrom <dahlstrom@google.com>
2018-01-11 09:26:02 -0800

    Move ThemeConfigurationTest to TestGroup.UNRELIABLE
    … since it fails consistently as of Change Id2cc7ea9.
    
    Bug: 71855356
    Change-Id: If42d9d4efe5fbf78bde111a0c027a64584a0ea4a

--- android-uitests/testSrc/com/android/tools/idea/tests/gui/theme/ThemeConfigurationTest.java
+++ android-uitests/testSrc/com/android/tools/idea/tests/gui/theme/ThemeConfigurationTest.java
@@ -17,6 +17,8 @@ package com.android.tools.idea.tests.gui.theme;
 
 import com.android.tools.idea.tests.gui.framework.GuiTestRule;
 import com.android.tools.idea.tests.gui.framework.GuiTestRunner;
+import com.android.tools.idea.tests.gui.framework.RunIn;
+import com.android.tools.idea.tests.gui.framework.TestGroup;
 import com.android.tools.idea.tests.gui.framework.fixture.ActionButtonFixture;
 import com.android.tools.idea.tests.gui.framework.fixture.ThemeSelectionDialogFixture;
 import com.android.tools.idea.tests.gui.framework.fixture.theme.AndroidThemePreviewPanelFixture;
@@ -33,6 +35,7 @@ import java.util.concurrent.TimeUnit;
 
 import static com.android.tools.idea.tests.gui.framework.fixture.theme.ThemeEditorFixture.clickPopupMenuItem;
 
+@RunIn(TestGroup.UNRELIABLE)  // b/71855356
 @RunWith(GuiTestRunner.class)
 public class ThemeConfigurationTest {
 
