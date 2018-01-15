beb5153ee4b073c4b0a7cf493188f708d80dab7a
Yuriy Solodkyy <solodkyy@google.com>
2018-01-15 16:39:00 +0000

    [New PSD] Define module level UI properties.
    This CL defines simple UI properties for:
    - andoid module
    - default config
    - signing config
    
    Bug: 69665954
    Test: new unit tests that ensure correct bindning
    Change-Id: I3c1daa620a89391fd04ee59ff87bae56c65dbd9d

--- android/src/com/android/tools/idea/gradle/structure/model/android/PsSigningConfig.kt
+++ android/src/com/android/tools/idea/gradle/structure/model/android/PsSigningConfig.kt
@@ -19,6 +19,12 @@ import com.android.builder.model.SigningConfig
 import com.android.tools.idea.gradle.dsl.api.android.SigningConfigModel
 import com.android.tools.idea.gradle.project.model.AndroidModuleModel
 import com.android.tools.idea.gradle.structure.model.PsChildModel
+import com.android.tools.idea.gradle.structure.model.helpers.parseFile
+import com.android.tools.idea.gradle.structure.model.helpers.parseString
+import com.android.tools.idea.gradle.structure.model.meta.ModelDescriptor
+import com.android.tools.idea.gradle.structure.model.meta.ModelSimpleProperty
+import com.android.tools.idea.gradle.structure.model.meta.property
+import java.io.File
 
 class PsSigningConfig(
     parent: PsAndroidModule,
@@ -32,9 +38,75 @@ class PsSigningConfig(
     else -> ""
   }
 
+  val storeFile by SigningConfigDescriptors.storeFile
+  val storePassword by SigningConfigDescriptors.storePassword
+  val storeType by SigningConfigDescriptors.storeType
+  val keyAlias by SigningConfigDescriptors.keyAlias
+  val keyPassword by SigningConfigDescriptors.keyPassword
+
   override fun getName(): String = name
   override fun getParent(): PsAndroidModule = super.getParent() as PsAndroidModule
   override fun isDeclared(): Boolean = parsedModel != null
   override fun getResolvedModel(): SigningConfig? = resolvedModel
   override fun getGradleModel(): AndroidModuleModel = parent.gradleModel
+
+  object SigningConfigDescriptors : ModelDescriptor<PsSigningConfig, SigningConfig, SigningConfigModel> {
+    override fun getResolved(model: PsSigningConfig): SigningConfig? = model.resolvedModel
+
+    override fun getParsed(model: PsSigningConfig): SigningConfigModel? = model.parsedModel
+
+    override fun setModified(model: PsSigningConfig) {
+      model.isModified = true
+    }
+
+    val storeFile: ModelSimpleProperty<PsSigningConfig, File> = property(
+        "Store File",
+        getResolvedValue = { storeFile },
+        getParsedValue = { File(storeFile().value()) },
+        getParsedRawValue = { storeFile().dslText },
+        // TODO: Store project relative path if possible.
+        setParsedValue = { setStoreFile(it.absolutePath) },
+        clearParsedValue = { removeStoreFile() },
+        parse = { parseFile(it) }
+    )
+    val storePassword: ModelSimpleProperty<PsSigningConfig, String> = property(
+        "Store Password",
+        getResolvedValue = { storePassword },
+        // TODO: Properly handle other password types.
+        getParsedValue = { storePassword().value()?.passwordText },
+        getParsedRawValue = { storePassword().dslText },
+        setParsedValue = { setStorePassword(SigningConfigModel.SigningConfigPassword.Type.PLAIN_TEXT, it) },
+        clearParsedValue = { removeStorePassword() },
+        parse = { parseString(it) }
+    )
+    val storeType: ModelSimpleProperty<PsSigningConfig, String> = property(
+        "Store Type",
+        getResolvedValue = { storeType },
+        // TODO: Properly handle other password types.
+        getParsedValue = { storeType().value() },
+        getParsedRawValue = { storeType().dslText },
+        setParsedValue = { setStoreType(it) },
+        clearParsedValue = { removeStoreType() },
+        parse = { parseString(it) }
+    )
+    val keyAlias: ModelSimpleProperty<PsSigningConfig, String> = property(
+        "Key Alias",
+        getResolvedValue = { keyAlias },
+        getParsedValue = { keyAlias().value() },
+        getParsedRawValue = { keyAlias().dslText },
+        setParsedValue = { setKeyAlias(it) },
+        clearParsedValue = { removeKeyAlias() },
+        parse = { parseString(it) }
+    )
+    val keyPassword: ModelSimpleProperty<PsSigningConfig, String> = property(
+        "Key Password",
+        getResolvedValue = { keyPassword },
+        // TODO: Properly handle other password types.
+        getParsedValue = { keyPassword().value()?.passwordText },
+        getParsedRawValue = { keyPassword().dslText },
+        setParsedValue = { setKeyPassword(SigningConfigModel.SigningConfigPassword.Type.PLAIN_TEXT, it) },
+        clearParsedValue = { removeKeyPassword() },
+        parse = { parseString(it) }
+    )
+  }
 }
---
+++ android/testSrc/com/android/tools/idea/gradle/structure/model/android/AndroidModuleDescriptorsTest.kt
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.gradle.structure.model.android
+
+import com.android.tools.idea.gradle.structure.model.PsProject
+import com.android.tools.idea.gradle.structure.model.meta.ParsedValue
+import com.android.tools.idea.gradle.structure.model.meta.ResolvedValue
+import com.android.tools.idea.testing.AndroidGradleTestCase
+import com.android.tools.idea.testing.TestProjectPaths.PSD_SAMPLE
+import com.intellij.pom.java.LanguageLevel
+import org.hamcrest.CoreMatchers.*
+import org.hamcrest.MatcherAssert.assertThat
+
+class AndroidModuleDescriptorsTest : AndroidGradleTestCase() {
+
+  private fun <T> ResolvedValue<T>.asTestValue(): T? = (this as? ResolvedValue.Set<T>)?.resolved
+  private fun <T> ParsedValue<T>.asTestValue(): T? = (this as? ParsedValue.Set.Parsed<T>)?.value
+
+  fun testProperties() {
+    loadProject(PSD_SAMPLE)
+
+    val resolvedProject = myFixture.project
+    val project = PsProject(resolvedProject)
+
+    val appModule = project.findModuleByName("app") as PsAndroidModule
+    assertThat(appModule, notNullValue())
+
+    val buildToolsVersion = AndroidModuleDescriptors.buildToolsVersion.getValue(appModule)
+    val compileSdkVersion = AndroidModuleDescriptors.compileSdkVersion.getValue(appModule)
+    val sourceCompatibility = AndroidModuleDescriptors.sourceCompatibility.getValue(appModule)
+    val targetCompatibility = AndroidModuleDescriptors.targetCompatibility.getValue(appModule)
+
+    assertThat(buildToolsVersion.resolved.asTestValue(), equalTo("27.0.3"))
+    assertThat(buildToolsVersion.parsedValue.asTestValue(), equalTo("27.0.3"))
+
+    assertThat(compileSdkVersion.resolved.asTestValue(), equalTo("26"))
+    assertThat(compileSdkVersion.parsedValue.asTestValue(), equalTo("26"))
+
+    assertThat(sourceCompatibility.resolved.asTestValue(), equalTo(LanguageLevel.JDK_1_7))
+    assertThat(sourceCompatibility.parsedValue.asTestValue(), nullValue())
+
+    assertThat(targetCompatibility.resolved.asTestValue(), equalTo(LanguageLevel.JDK_1_7))
+    assertThat(targetCompatibility.parsedValue.asTestValue(), nullValue())
+  }
+}
