fc9a1880d594ffb999087a1f2fcd820a3acf79b5
Sergio Murguia <srmurguia@google.com>
2018-01-16 15:37:16 -0800

    Enable output parsers on GradleBuildInvoker, add a AGPBI parser.
    BuildOutputInstantReaderImpl was created during a try with resource  since
    it implements a closable, However, this class creates a thread that should
    be kept runing even after createBuildTaskListener is running, but since its
    close method is called as soon as the try finishes, then none of the parsers
    are called. This causes to not report any errors from them in the Build
    window.
    
    This CL removes the try with resource to cause close to be called inside
    ExternalSystemTaskNotificationListenerAdapter#onEnd and also adds a new type
    of parser for AGPBI messages.
    
    Test: New test for AGPBIOutputParser
    Bug: 71845632
    Bug: 70299236
    
    This is how the generated messages look like (they are navigable):
    
    https://screenshot.googleplex.com/rRVHTYbehiM
    
    Change-Id: I159fa04b69141704fb78f90d3b851fb0dafe8b7c
    (cherry picked from commit 034b3b3df532caff79564c020ad96f59a3d03a9a)

---
+++ android/src/com/android/tools/idea/gradle/project/build/output/GradleBuildOutputParser.java
@@ -0,0 +1,116 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.gradle.project.build.output;
+
+import com.android.ide.common.blame.Message;
+import com.android.ide.common.blame.Message.Kind;
+import com.android.ide.common.blame.MessageJsonSerializer;
+import com.android.ide.common.blame.SourceFilePosition;
+import com.android.ide.common.blame.SourcePosition;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonParseException;
+import com.intellij.build.FilePosition;
+import com.intellij.build.events.MessageEvent;
+import com.intellij.build.events.impl.FileMessageEventImpl;
+import com.intellij.build.events.impl.MessageEventImpl;
+import com.intellij.build.output.BuildOutputInstantReader;
+import com.intellij.build.output.BuildOutputParser;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+import java.util.function.Consumer;
+
+import static com.android.ide.common.blame.parser.JsonEncodedGradleMessageParser.STDOUT_ERROR_TAG;
+
+/**
+ * Parser got errors returned by the Android Gradle Plugin in AGPBI json format.
+ */
+public class GradleBuildOutputParser implements BuildOutputParser {
+  private static final String MESSAGES_GROUP = "Android errors";
+
+  @Override
+  public boolean parse(@NotNull String line, @NotNull BuildOutputInstantReader reader, @NotNull Consumer<MessageEvent> messageConsumer) {
+    if (line.startsWith(STDOUT_ERROR_TAG)) {
+      String jsonString = line.substring(STDOUT_ERROR_TAG.length()).trim();
+      if (jsonString.isEmpty()) {
+        return false;
+      }
+      GsonBuilder gsonBuilder = new GsonBuilder();
+      MessageJsonSerializer.registerTypeAdapters(gsonBuilder);
+      Gson gson = gsonBuilder.create();
+      try {
+        Message msg = gson.fromJson(jsonString, Message.class);
+        if (msg.getSourceFilePositions().isEmpty()) {
+          messageConsumer.accept(new MessageEventImpl(reader.getBuildId(), convertKind(msg.getKind()), MESSAGES_GROUP, msg.getText()));
+        }
+        else {
+          for (SourceFilePosition sourceFilePosition : msg.getSourceFilePositions()) {
+            messageConsumer.accept(new FileMessageEventImpl(reader.getBuildId(), convertKind(msg.getKind()), MESSAGES_GROUP, msg.getText(),
+                                                            convertToFilePosition(sourceFilePosition)));
+          }
+        }
+        return true;
+      }
+      catch (JsonParseException e) {
+        return false;
+      }
+    }
+    return false;
+  }
+
+  /**
+   * Convert from {@link Message.Kind} to {@link MessageEvent.Kind}
+   * @param kind a value from {@link Message.Kind}.
+   * @return the equivalent {@link MessageEvent.Kind} or {@link MessageEvent.Kind#ERROR} if no correspondence exists.
+   */
+  @Contract(pure = true)
+  @NotNull
+  private static MessageEvent.Kind convertKind(@NotNull Kind kind) {
+    switch (kind) {
+      case ERROR:
+        return MessageEvent.Kind.ERROR;
+      case WARNING:
+        return MessageEvent.Kind.WARNING;
+      case INFO:
+        return MessageEvent.Kind.INFO;
+      case STATISTICS:
+        return MessageEvent.Kind.STATISTICS;
+      case UNKNOWN:
+        return MessageEvent.Kind.ERROR;
+      case SIMPLE:
+        return MessageEvent.Kind.SIMPLE;
+    }
+    return MessageEvent.Kind.ERROR;
+  }
+
+  /**
+   * Convert from {@link SourceFilePosition} to {@link FilePosition}
+   * @param sourceFilePosition
+   * @return converted FilePosition
+   */
+  @NotNull
+  private static FilePosition convertToFilePosition(@NotNull SourceFilePosition sourceFilePosition) {
+    File sourceFile = sourceFilePosition.getFile().getSourceFile();
+    SourcePosition position = sourceFilePosition.getPosition();
+    int startLine = position.getStartLine();
+    int endLine = position.getEndLine();
+    int startColumn = position.getStartColumn();
+    int endColumn = position.getEndColumn();
+    return new FilePosition(sourceFile, startLine, startColumn, endLine, endColumn);
+  }
+}
