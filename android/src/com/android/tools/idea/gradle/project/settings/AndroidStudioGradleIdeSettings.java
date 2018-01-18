e85deb957cc8519250e98538a8d9723b0d92c188
alruiz <alruiz@google.com>
2018-01-18 15:55:03 -0800

    Re-introduced UI setting to enable/disable offline repo.
    Bug: 71582708
    Test: JUnit tests.
    Change-Id: I6ae37862f6c57939f0c75120c3f1261e9ecff921
    (cherry picked from commit bd519ea628578d9a212ab1cb5cbdf4cc947a4d21)
    (cherry picked from commit 64fa03f15f941c235c57c7aef3057801e519bbac)

---
+++ android/src/com/android/tools/idea/gradle/project/settings/AndroidStudioGradleIdeSettings.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.gradle.project.settings;
+
+import com.intellij.openapi.components.*;
+import org.jetbrains.annotations.NotNull;
+
+import static com.intellij.util.xmlb.XmlSerializerUtil.copyBean;
+
+/**
+ * Android Studio-specific Gradle project settings.
+ */
+@State(
+  name="AndroidStudioGradleSettings",
+  storages = @Storage(file = StoragePathMacros.APP_CONFIG + "/android.gradle.studio.xml")
+)
+public class AndroidStudioGradleIdeSettings implements PersistentStateComponent<AndroidStudioGradleIdeSettings> {
+  public boolean ENABLE_EMBEDDED_MAVEN_REPO = true;
+
+  @NotNull
+  public static AndroidStudioGradleIdeSettings getInstance() {
+    return ServiceManager.getService(AndroidStudioGradleIdeSettings.class);
+  }
+
+  @Override
+  @NotNull
+  public AndroidStudioGradleIdeSettings getState() {
+    return this;
+  }
+
+  @Override
+  public void loadState(AndroidStudioGradleIdeSettings state) {
+    copyBean(state, this);
+  }
+
+  public boolean isEmbeddedMavenRepoEnabled() {
+    return ENABLE_EMBEDDED_MAVEN_REPO;
+  }
+}
