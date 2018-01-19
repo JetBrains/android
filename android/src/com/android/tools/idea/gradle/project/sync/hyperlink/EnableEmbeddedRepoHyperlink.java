b454b7feeee28417489513eb205731d1ccd3b5ef
Ying Chen <chying@google.com>
2018-01-19 13:43:48 -0800

    Provide quickfix to enable embedded repository.
    Add a new quickfix to enable embedded repository
    if there're unresolved dependency errors, and
    the dependency can be found in embedded Maven
    repository.
    
    Bug: 71582708
    Test: EnableEmbeddedRepoHyperlinkTest.
    Change-Id: Ia97f60c5ef330d2e1b2f7c4c718e573e4585f09d
    (cherry picked from commit 62819dd084dd8d5c108df699b2edf8c9f6f04467)

---
+++ android/src/com/android/tools/idea/gradle/project/sync/hyperlink/EnableEmbeddedRepoHyperlink.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.gradle.project.sync.hyperlink;
+
+import com.android.annotations.VisibleForTesting;
+import com.android.ide.common.repository.GradleCoordinate;
+import com.android.tools.idea.gradle.project.settings.AndroidStudioGradleIdeSettings;
+import com.android.tools.idea.gradle.project.sync.GradleSyncInvoker;
+import com.android.tools.idea.gradle.util.EmbeddedDistributionPaths;
+import com.android.tools.idea.project.hyperlink.NotificationHyperlink;
+import com.intellij.openapi.project.Project;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+
+import static com.google.wireless.android.sdk.stats.GradleSyncStats.Trigger.TRIGGER_PROJECT_MODIFIED;
+import static java.io.File.separator;
+
+/**
+ * A {@link NotificationHyperlink} that offers the user to enable embedded offline Maven repository.
+ */
+public class EnableEmbeddedRepoHyperlink extends NotificationHyperlink {
+  public EnableEmbeddedRepoHyperlink() {
+    super("enable.embedded.maven.repository", "Enable embedded Maven repository and sync project");
+  }
+
+  @Override
+  protected void execute(@NotNull Project project) {
+    AndroidStudioGradleIdeSettings.getInstance().ENABLE_EMBEDDED_MAVEN_REPO = true;
+    GradleSyncInvoker.getInstance().requestProjectSyncAndSourceGeneration(project, TRIGGER_PROJECT_MODIFIED);
+  }
+
+  /**
+   * @param missingDependency the unresolved dependency.
+   * @return {@code true} if embedded Maven repository is disabled, and the missing dependency can be found there.
+   */
+  public static boolean shouldEnableEmbeddedRepo(@NotNull String missingDependency) {
+    GradleCoordinate coordinate = GradleCoordinate.parseCoordinateString(missingDependency);
+    if (coordinate == null) {
+      return false;
+    }
+    if (AndroidStudioGradleIdeSettings.getInstance().isEmbeddedMavenRepoEnabled()) {
+      return false;
+    }
+
+    String subPathWithoutExtension = getPathWithoutExtensionFromCoordinate(coordinate);
+    List<String> fileExtensions = Arrays.asList(".jar", ".aar");
+    List<File> repoPaths = EmbeddedDistributionPaths.getInstance().findAndroidStudioLocalMavenRepoPaths();
+    for (File repoPath : repoPaths) {
+      for (String extension : fileExtensions) {
+        File expectedPath = new File(repoPath, subPathWithoutExtension + extension);
+        if (expectedPath.exists()) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  /**
+   * @return the relative path of a GradleCoordinate without file extension, for example, "com/android/tools/build/gradle/3.1.0-dev/gradle-3.1.0-dev".
+   */
+  @VisibleForTesting
+  @NotNull
+  static String getPathWithoutExtensionFromCoordinate(@NotNull GradleCoordinate coordinate) {
+    StringBuilder path = new StringBuilder();
+    String groupId = coordinate.getGroupId();
+    String artifactId = coordinate.getArtifactId();
+    String revision = coordinate.getRevision();
+    String fileNamePrefix = "";
+
+    if (groupId != null) {
+      path.append(groupId.replace(".", separator)).append(separator);
+    }
+    if (artifactId != null) {
+      path.append(artifactId.replace(".", separator)).append(separator);
+      fileNamePrefix = artifactId + "-";
+    }
+    path.append(revision).append(separator);
+    path.append(fileNamePrefix).append(revision);
+    return path.toString();
+  }
+}
