282c13e36255788c15767ce43b44d85f69511458
Sergio Murguia <srmurguia@google.com>
2018-01-24 12:55:21 -0800

    Change the way detailed message is generated
    After cherry picking from Jetbrains, readUntil is no longer available.
    This CL uses a different approach by buffering lines until the expected
    one is found.
    
    Test: Updated tests to also not use readUntil.
    Bug: 72180932
    Bug: 70299236
    Change-Id: Iebbb4e60ef85c2b4d70566ce89481b264c3375ba

--- android/testSrc/com/android/tools/idea/gradle/project/build/output/GradleBuildOutputParserTest.java
+++ android/testSrc/com/android/tools/idea/gradle/project/build/output/GradleBuildOutputParserTest.java
@@ -18,6 +18,7 @@ package com.android.tools.idea.gradle.project.build.output;
 import com.intellij.build.events.FileMessageEvent;
 import com.intellij.build.events.MessageEvent;
 import com.intellij.build.output.BuildOutputInstantReader;
+import com.intellij.util.SystemProperties;
 import org.jetbrains.annotations.Nullable;
 import org.junit.Before;
 import org.junit.Test;
@@ -27,6 +28,7 @@ import org.mockito.Mock;
 import java.util.List;
 import java.util.function.Consumer;
 
+import static com.android.tools.idea.gradle.project.build.output.GradleBuildOutputParser.END_DETAIL;
 import static com.google.common.truth.Truth.assertThat;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
@@ -52,15 +54,20 @@ public class GradleBuildOutputParserTest {
     when(myReader.getBuildId()).thenReturn("BUILD_ID_MOCK");
 
     ArgumentCaptor<MessageEvent> messageCaptor = ArgumentCaptor.forClass(MessageEvent.class);
-    String detailMessage = "This is a detailed message";
-    when(myReader.readUntil(any())).thenReturn(detailMessage);
-    assertTrue(myParser.parse(line, myReader, myConsumer));
+    String detailLine = "This is a detail line";
+    assertFalse(myParser.parse(line, myReader, myConsumer));
+    assertTrue(myParser.processingMessage());
+    assertFalse(myParser.parse(detailLine, myReader, myConsumer));
+    assertTrue(myParser.processingMessage());
+    assertTrue(myParser.parse(END_DETAIL, myReader, myConsumer));
+    assertFalse(myParser.processingMessage());
     verify(myConsumer).accept(messageCaptor.capture());
 
     List<MessageEvent> generatedMessages = messageCaptor.getAllValues();
     assertThat(generatedMessages).hasSize(1);
     assertThat(generatedMessages.get(0)).isInstanceOf(FileMessageEvent.class);
     FileMessageEvent fileMessageEvent = (FileMessageEvent)generatedMessages.get(0);
+    String detailMessage = line + SystemProperties.getLineSeparator() + detailLine;
     assertThat(fileMessageEvent.getResult().getDetails()).isEqualTo(detailMessage);
   }
 
@@ -70,17 +77,45 @@ public class GradleBuildOutputParserTest {
     when(myReader.getBuildId()).thenReturn("BUILD_ID_MOCK");
 
     ArgumentCaptor<MessageEvent> messageCaptor = ArgumentCaptor.forClass(MessageEvent.class);
-    assertTrue(myParser.parse(line, myReader, myConsumer));
+    String detailLine = "This is a detail line";
+    assertFalse(myParser.parse(line, myReader, myConsumer));
+    assertTrue(myParser.processingMessage());
+    assertFalse(myParser.parse(detailLine, myReader, myConsumer));
+    assertTrue(myParser.processingMessage());
+    assertTrue(myParser.parse(END_DETAIL, myReader, myConsumer));
+    assertFalse(myParser.processingMessage());
     verify(myConsumer).accept(messageCaptor.capture());
 
     List<MessageEvent> generatedMessages = messageCaptor.getAllValues();
     assertThat(generatedMessages).hasSize(1);
     assertThat(generatedMessages.get(0)).isNotInstanceOf(FileMessageEvent.class);
+    String detailMessage = line + SystemProperties.getLineSeparator() + detailLine;
+    MessageEvent messageEvent = generatedMessages.get(0);
+    assertThat(messageEvent.getResult().getDetails()).isEqualTo(detailMessage);
   }
 
   @Test
   public void parseWithoutError() {
     String line = "Non AGBPI error";
     assertFalse(myParser.parse(line, myReader, myConsumer));
+    assertFalse(myParser.processingMessage());
+  }
+
+  @Test
+  public void parseChangeBuildId() {
+    String startline = "AGPBI: {\"kind\":\"error\",\"text\":\"Warning message.\",\"sources\":[{}],\"original\":\"\",\"tool\":\"AAPT\"}";
+    String noErrorLine = "This is not an error";
+
+    when(myReader.getBuildId()).thenReturn("BUILD_ID_MOCK_1");
+    assertFalse(myParser.parse(startline, myReader, myConsumer));
+    assertTrue(myParser.processingMessage());
+
+    when(myReader.getBuildId()).thenReturn("BUILD_ID_MOCK_2");
+    assertFalse(myParser.parse(startline, myReader, myConsumer));
+    assertTrue(myParser.processingMessage());
+
+    when(myReader.getBuildId()).thenReturn("BUILD_ID_MOCK_3");
+    assertFalse(myParser.parse(noErrorLine, myReader, myConsumer));
+    assertFalse(myParser.processingMessage());
   }
 }
