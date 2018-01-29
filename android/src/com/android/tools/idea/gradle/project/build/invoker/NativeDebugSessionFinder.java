2e4acad75cda0905ca549d0fc88b09bc2ec4da8f
alruiz <alruiz@google.com>
2018-01-29 14:47:11 +0000

    Moved more functionality related to "terminate debug session" out of the UI thread.
    This CL:
    1. Only leaves displaying the message dialog in the UI thread, and everthing
       else (finding the native debug session and terminating it) in a worker
       thread.
    2. Cleans up the code by removing unused imports and moves the logic that
       finds native debug sessions to its own class.
    3. Adds unit tests and integration tests to the "terminate debug session"
       functionality.
    
    Test: JUnit tests.
    Change-Id: Ic114692c79fa7e40d067477e64cb06e2621427e0

---
+++ android/src/com/android/tools/idea/gradle/project/build/invoker/NativeDebugSessionFinder.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.gradle.project.build.invoker;
+
+import com.android.tools.idea.debug.AndroidNativeDebugProcess;
+import com.google.common.annotations.VisibleForTesting;
+import com.intellij.openapi.project.Project;
+import com.intellij.xdebugger.XDebugProcess;
+import com.intellij.xdebugger.XDebugSession;
+import com.intellij.xdebugger.XDebuggerManager;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+class NativeDebugSessionFinder {
+  @NotNull private final XDebuggerManager myDebuggerManager;
+
+  NativeDebugSessionFinder(@NotNull Project project) {
+    this(XDebuggerManager.getInstance(project));
+  }
+
+  @VisibleForTesting
+  NativeDebugSessionFinder(@NotNull XDebuggerManager debuggerManager) {
+    myDebuggerManager = debuggerManager;
+  }
+
+  /**
+   * Returns the currently-running native debug session, if it exists.
+   *
+   * @return the native debug session or {@code null} if none exists.
+   */
+  @Nullable
+  XDebugSession findNativeDebugSession() {
+    for (XDebugSession session : myDebuggerManager.getDebugSessions()) {
+      XDebugProcess debugProcess = session.getDebugProcess();
+      if (debugProcess instanceof AndroidNativeDebugProcess) {
+        return session;
+      }
+    }
+    return null;
+  }
+}
