f02e80ba693e15ce9cba6e0eef70b969a5ffc131
Liz Yu <xinruiy@google.com>
2018-01-29 17:23:52 -0800

    Update method location.
    Follow up of ag/3555369. Just to leave simpler, widely called override
    method above the other one.
    
    Bug: not a bug
    Test: All existings tests passed
    Change-Id: I4977985d3a74ae82c860e88bc13c2e64e76b89bb

--- uitest-framework/testSrc/com/android/tools/idea/tests/gui/framework/GuiTestRule.java
+++ uitest-framework/testSrc/com/android/tools/idea/tests/gui/framework/GuiTestRule.java
@@ -286,24 +286,24 @@ public class GuiTestRule implements TestRule {
     return importProjectAndWaitForProjectSyncToFinish("MultiModule");
   }
 
+  public IdeFrameFixture importProjectAndWaitForProjectSyncToFinish(@NotNull String projectDirName) throws IOException {
+    return importProjectAndWaitForProjectSyncToFinish(projectDirName, null);
+  }
+
   public IdeFrameFixture importProjectAndWaitForProjectSyncToFinish(@NotNull String projectDirName, @Nullable String buildFilePath) throws IOException {
     importProject(projectDirName, buildFilePath);
     testSystem().waitForProjectSyncToFinish(ideFrame());
     return ideFrame();
   }
 
-  public IdeFrameFixture importProjectAndWaitForProjectSyncToFinish(@NotNull String projectDirName) throws IOException {
-    return importProjectAndWaitForProjectSyncToFinish(projectDirName, null);
+  public IdeFrameFixture importProject(@NotNull String projectDirName) throws IOException {
+    return importProject(projectDirName, null);
   }
 
   public IdeFrameFixture importProject(@NotNull String projectDirName, @Nullable String buildFilePath) throws IOException {
     File testProjectDir = setUpProject(projectDirName);
     testSystem().importProject(testProjectDir, robot(), buildFilePath);
     return ideFrame();
-  }
-
-  public IdeFrameFixture importProject(@NotNull String projectDirName) throws IOException {
-    return importProject(projectDirName, null);
   }
 
   /**
