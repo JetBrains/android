31c84a1d6986c9f61c8e45d45470bddcb9ddace6
Jingjing Liu <jingjingliu@google.com>
2018-02-06 10:04:14 -0800

    Fix testDebugOnEmulator timeout flakiness
    Increase timeout to 60 seconds to wait for Gradle Sync to finish.
    Also use SimpleLocalApplication project.
    
    Bug: 72654538
    Test: Ran testDebugOnEmulator test locally
    Change-Id: I605cb38fdb47ad4524f67d8425a761f1b278852f

--- android-uitests/testSrc/com/android/tools/idea/tests/gui/emulator/LaunchAndroidApplicationTest.java
+++ android-uitests/testSrc/com/android/tools/idea/tests/gui/emulator/LaunchAndroidApplicationTest.java
@@ -125,7 +125,8 @@ public class LaunchAndroidApplicationTest {
   @RunIn(TestGroup.SANITY)
   @Test
   public void testDebugOnEmulator() throws IOException, ClassNotFoundException, EvaluateException {
-    guiTest.importSimpleApplication();
+    guiTest.importProject("SimpleLocalApplication");
+    guiTest.ideFrame().waitForGradleProjectSyncToFinish(Wait.seconds(60));
     emulator.createDefaultAVD(guiTest.ideFrame().invokeAvdManager());
 
     IdeFrameFixture ideFrameFixture = guiTest.ideFrame();
