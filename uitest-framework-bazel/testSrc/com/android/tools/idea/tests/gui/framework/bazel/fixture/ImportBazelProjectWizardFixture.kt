6787866fac7ee02c7ee83e21b413074e941caaec
Liz Yu <xinruiy@google.com>
2018-02-09 15:24:18 -0800

    More fixes to ImportBazelProjectWizardFixture
    Follow up of ag/Ifaffbe40f0c641719054b7b66c5d3ca697762d83
    * Add logs in ImportBazelProjectWizardFixture for debugging.
    * Add assertion to check text stored in component is expected
    * Change one remaining robot().click() to J...Fixture.click() which
    guarantee item get focus
    
    Bug: 73037396
    Test: manually ran the ui tests affected by this fixture change and verified they work correctly
    Change-Id: Ic1849bdac7b89eed1a70e4f41564bc7567d9b152
    Test-Strategy: ui.test.groups=DEFAULT

--- uitest-framework-bazel/testSrc/com/android/tools/idea/tests/gui/framework/bazel/fixture/ImportBazelProjectWizardFixture.kt
+++ uitest-framework-bazel/testSrc/com/android/tools/idea/tests/gui/framework/bazel/fixture/ImportBazelProjectWizardFixture.kt
@@ -17,18 +17,25 @@ package com.android.tools.idea.tests.gui.framework.bazel.fixture
 
 import com.android.tools.idea.tests.gui.framework.GuiTests.findAndClickButtonWhenEnabled
 import com.android.tools.idea.tests.gui.framework.fixture.wizard.AbstractWizardFixture
+import com.google.common.base.Verify
+import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.editor.impl.EditorComponentImpl
 import org.fest.swing.core.GenericTypeMatcher
 import org.fest.swing.core.Robot
 import org.fest.swing.finder.WindowFinder.findDialog
 import org.fest.swing.fixture.JComboBoxFixture
 import org.fest.swing.fixture.JPanelFixture
+import org.fest.swing.fixture.JRadioButtonFixture
 import javax.swing.JDialog
 import javax.swing.JRadioButton
 import javax.swing.SwingUtilities
 
 class ImportBazelProjectWizardFixture(robot: Robot, target: JDialog) :
     AbstractWizardFixture<ImportBazelProjectWizardFixture>(ImportBazelProjectWizardFixture::class.java, robot, target) {
+  val id: String
+    get() = ImportBazelProjectWizardFixture::class.java.name
+
+  private val logger = Logger.getInstance(id)
 
   companion object {
     fun find(robot: Robot): ImportBazelProjectWizardFixture {
@@ -41,28 +48,36 @@ class ImportBazelProjectWizardFixture(robot: Robot, target: JDialog) :
   }
 
   fun setBazelBinaryPath(path: String): ImportBazelProjectWizardFixture {
-    JPanelFixture(robot(), "bazel-binary-path-field").comboBox().replaceText(path)
+    logger.info("Setting bazel binary path = $path")
+    val bazelBinaryComboBox = JPanelFixture(robot(), "bazel-binary-path-field").comboBox().replaceText(path)
+    Verify.verify(bazelBinaryComboBox.target().editor.item == path)
     return this
   }
 
   fun setWorkspacePath(path: String): ImportBazelProjectWizardFixture {
-    JComboBoxFixture(robot(), "workspace-directory-field").replaceText(path)
+    logger.info("Setting workspace directory = $path")
+    val workspaceComboBox = JComboBoxFixture(robot(), "workspace-directory-field").replaceText(path)
+    Verify.verify(workspaceComboBox.target().editor.item == path)
     return this
   }
 
   fun selectGenerateFromBuildFileOptionAndSetPath(path: String): ImportBazelProjectWizardFixture {
+    logger.info("Find 'Generate from BUILD file' radio button")
     val generateFromBuildFileOption = robot().finder().find(object : GenericTypeMatcher<JRadioButton>(JRadioButton::class.java) {
       override fun isMatching(component: JRadioButton): Boolean {
         return (component.text == "Generate from BUILD file")
       }
     })
-    robot().click(generateFromBuildFileOption)
+    JRadioButtonFixture(robot(), generateFromBuildFileOption).click()
 
-    JComboBoxFixture(robot(), "build-file-path-field").replaceText(path)
+    logger.info("Setting build file path = $path")
+    val buildFileComboBox = JComboBoxFixture(robot(), "build-file-path-field").replaceText(path)
+    Verify.verify(buildFileComboBox.target().editor.item == path)
     return this
   }
 
   fun uncommentApi27(): ImportBazelProjectWizardFixture {
+    logger.info("Modifying BUILD file content")
     val editor = robot().finder().findByType(EditorComponentImpl::class.java)
 
     // Maybe let test configs select which platform to use?
@@ -71,7 +86,7 @@ class ImportBazelProjectWizardFixture(robot: Robot, target: JDialog) :
     val selectionStartIndex = editor.text.indexOf(toSelect)
 
     if (selectionStartIndex < 0) {
-      System.out.println("Assuming api 27 in bazel test run.")
+      logger.info("Assuming api 27 in bazel test run.")
     }
     else {
       val selectionEndIndex = selectionStartIndex + toSelect.length
@@ -83,11 +98,11 @@ class ImportBazelProjectWizardFixture(robot: Robot, target: JDialog) :
         editableText.replaceText(selectionStartIndex, toSelect.length, toPaste)
       }
     }
-
     return this
   }
 
   fun clickFinish(): ImportBazelProjectWizardFixture {
+    logger.info("Finish import bazel project wizard")
     findAndClickButtonWhenEnabled(this, "Finish")
     return this
   }
