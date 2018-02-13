35501a83a591142f8ae82f3ed65202cbc2321cdb
Andrii Dehtiarov <adehtiarov@google.com>
2018-02-13 15:59:30 +0000

    Do not forget APK signing key password stored with a legacy requestor.
    This CL is essentially a follow-up for the fix submitted earlier with
    ag/3265044. That CL correctly fixed the problem with APK signing key store
    password being forgotten even when 'Remember passwords' was checked.
    Also, that CL made an effort attempted to ensure smooth transition
    from the Studio versions which don't contain that fix to the versions
    which do. For that it tried to request APK passwords using in turn both
    the new and legacy requestors, thus the user wouldn't have to retype their
    key password - only the keystore password once, which was the subject of the
    original bug.
    
    However that fix itself contained a typo in the classname
    used as a legacy requestor, thus resulting in returned 'legacy' passwords
    always being null. Therefore those passwords were never populated,
    defeating the effort to ensure smooth transition to the fixed versions.
    The user would only need to retype both the key and keystore passwords in
    order to get them stored correctly (that part of the fix was working as
    expected).
    
    Therefore this CL addresses that problem by correcting the requestor
    classname, and adding a dedicated test for it. Also this CL improves the test
    infrastructure for the KeystoreStep by adding PasswordSafeMock - this allows
    replace the PasswordSafe app service for each test and not run into side
    effects of the previous tests.
    
    Bug: b/70937387
    Test: Added KeystoreStep.testRememberPasswordsUsingLegacyRequestor
    Change-Id: Idfe599b10061728c8cd95ef8d1cd0c741a3babe6
    (cherry picked from commit a021afb05420cc61b6f2451adcaa092686ef34ab)

--- android/testSrc/org/jetbrains/android/exportSignedPackage/KeystoreStepTest.kt
+++ android/testSrc/org/jetbrains/android/exportSignedPackage/KeystoreStepTest.kt
@@ -16,7 +16,13 @@
 package org.jetbrains.android.exportSignedPackage
 
 import com.android.tools.idea.testing.IdeComponents
+import com.intellij.credentialStore.CredentialAttributes
+import com.intellij.credentialStore.Credentials
+import com.intellij.ide.passwordSafe.PasswordSafe
 import com.intellij.testFramework.IdeaTestCase
+import org.jetbrains.android.exportSignedPackage.KeystoreStep.KEY_PASSWORD_KEY
+import org.jetbrains.concurrency.Promise
+import org.jetbrains.concurrency.resolvedPromise
 
 import org.junit.Assert.assertArrayEquals
 import org.mockito.Mockito.*
@@ -51,6 +57,9 @@ class KeystoreStepTest : IdeaTestCase() {
 
     ideComponents.replaceProjectService(GenerateSignedApkSettings::class.java, settings)
 
+    val passwordSafe = PasswordSafeMock()
+    ideComponents.replaceService(PasswordSafe::class.java, passwordSafe)
+
     val wizard = mock(ExportSignedPackageWizard::class.java)
     `when`(wizard.project).thenReturn(myProject)
 
@@ -71,5 +80,67 @@ class KeystoreStepTest : IdeaTestCase() {
     assertEquals(testKeyAlias, keystoreStep2.keyAliasField.text)
     assertArrayEquals(testKeyStorePassword.toCharArray(), keystoreStep2.keyStorePasswordField.password)
     assertArrayEquals(testKeyPassword.toCharArray(), keystoreStep2.keyPasswordField.password)
+  }
+
+  // See b/64995008 & b/70937387 - we want to ensure smooth transition so that the user didn't have to retype both passwords
+  fun testRememberPasswordsUsingLegacyRequestor() {
+    val testKeyStorePath = "/test/path/to/keystore"
+    val testKeyAlias = "testkey"
+    val testKeyStorePassword = "123456"
+    val testKeyPassword = "qwerty"
+    val testLegacyKeyPassword = "somestuff"
+    val legacyRequestor = KeystoreStep::class.java
+
+    val settings = GenerateSignedApkSettings()
+    settings.KEY_STORE_PATH = testKeyStorePath
+    settings.KEY_ALIAS = testKeyAlias
+    settings.REMEMBER_PASSWORDS = true
+
+    ideComponents.replaceProjectService(GenerateSignedApkSettings::class.java, settings)
+
+    val passwordSafe = PasswordSafeMock()
+    val keyPasswordKey = KeystoreStep.makePasswordKey(KEY_PASSWORD_KEY, settings.KEY_STORE_PATH, settings.KEY_ALIAS)
+    passwordSafe.setPassword(legacyRequestor, keyPasswordKey, testLegacyKeyPassword)
+    ideComponents.replaceService(PasswordSafe::class.java, passwordSafe)
+
+    val wizard = mock(ExportSignedPackageWizard::class.java)
+    `when`(wizard.project).thenReturn(myProject)
+
+    val keystoreStep = KeystoreStep(wizard, true)
+    assertEquals(testKeyStorePath, keystoreStep.keyStorePathField.text)
+    assertEquals(testKeyAlias, keystoreStep.keyAliasField.text)
+    // Yes, it's weird but before the fix for b/64995008 this was exactly the observed behavior: the keystore password would
+    // never be populated, whereas the key password would be saved as expected.
+    assertEquals(0, keystoreStep.keyStorePasswordField.password.size)
+    assertArrayEquals(testLegacyKeyPassword.toCharArray(), keystoreStep.keyPasswordField.password)
+
+    // Set passwords and commit.
+    keystoreStep.keyStorePasswordField.text = testKeyStorePassword
+    keystoreStep.keyPasswordField.text = testKeyPassword
+    keystoreStep.commitForNext()
+
+    // Now check that the old-style password is erased
+    assertEquals(null, passwordSafe.getPassword(legacyRequestor, keyPasswordKey))
+  }
+
+  class PasswordSafeMock : PasswordSafe() {
+    private val storedPasswords = HashMap<Class<*>, HashMap<String, String?>>()
+
+    override fun getAsync(attributes: CredentialAttributes): Promise<Credentials> = resolvedPromise()
+    override fun isMemoryOnly(): Boolean = false
+    override fun isPasswordStoredOnlyInMemory(attributes: CredentialAttributes, credentials: Credentials): Boolean = false
+    override fun set(attributes: CredentialAttributes, credentials: Credentials?, memoryOnly: Boolean) {}
+    override fun set(attributes: CredentialAttributes, credentials: Credentials?) {}
+    override fun get(attributes: CredentialAttributes): Credentials? = null
+
+    override fun setPassword(requestor: Class<*>, accountName: String, value: String?) {
+      val account = storedPasswords.getOrDefault(requestor, HashMap())
+      account[accountName] = value
+      storedPasswords[requestor] = account
+    }
+
+    override fun getPassword(requestor: Class<*>, accountName: String): String? {
+      return storedPasswords[requestor]?.get(accountName)
+    }
   }
 }
