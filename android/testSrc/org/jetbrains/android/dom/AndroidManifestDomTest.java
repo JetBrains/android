c8bb26ed1f92f52355aa94729091e225b14b2f4e
Tor Norbye <tnorbye@google.com>
2018-02-14 17:29:55 -0800

    Include Android P metadata
    Test: The change is in tools/base; this updates IDE tests
    Change-Id: Ie2a9705f3121ef4da7ec9b2ea9a4b32b1b01b670

--- android/testSrc/org/jetbrains/android/dom/AndroidManifestDomTest.java
+++ android/testSrc/org/jetbrains/android/dom/AndroidManifestDomTest.java
@@ -508,7 +508,7 @@ public class AndroidManifestDomTest extends AndroidDomTestCase {
     try {
       doTestCompletionVariants(getTestName(true) + ".xml", "1", "2", "3", "4", "5", "6", "7",
                                "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25",
-                               "26", "27");
+                               "26", "27", "P");
     }
     finally {
       ApplicationManager.getApplication().runWriteAction(() -> projectJdkTable.removeJdk(sdk));
