7aed35c0479d4b12565a31a84bf89eaffc3f552d
Mike Moroney <mmoro@google.com>
2018-02-19 14:26:25 -0800

    Changed caption from Kind to Type
    Fixes: 73382423
    
    Test: NavPropertyEditorsTest, NavPropertiesInspectorProvidersTest
    Change-Id: Icf4ea2891c08e1ae281c5b882928bda363a5c186

--- designer/src/com/android/tools/idea/naveditor/property/inspector/NavMainPropertiesInspectorProvider.kt
+++ designer/src/com/android/tools/idea/naveditor/property/inspector/NavMainPropertiesInspectorProvider.kt
@@ -18,7 +18,7 @@ import com.android.tools.idea.naveditor.property.TYPE_EDITOR_PROPERTY_LABEL
 import org.jetbrains.android.dom.navigation.NavigationSchema
 
 private val PROPERTY_NAME_UI_NAME_MAP = mapOf(
-    TYPE_EDITOR_PROPERTY_LABEL to "Kind",
+    TYPE_EDITOR_PROPERTY_LABEL to "Type",
     SdkConstants.ATTR_LABEL to "Title",
     SdkConstants.ATTR_ID to "ID",
     SdkConstants.ATTR_NAME to "Class",
