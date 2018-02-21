5a2d59d140a941e4281631e29cec973036c85a69
Dmitry.Krasilschikov <kradima@jetbrains.com>
2018-02-21 18:29:13 +0300

    IDEA-CR-29139 porting console foldings to new project aware model

--- android/src/com/android/tools/idea/logcat/ExceptionFolding.java
+++ android/src/com/android/tools/idea/logcat/ExceptionFolding.java
@@ -16,6 +16,7 @@
 package com.android.tools.idea.logcat;
 
 import com.intellij.execution.ConsoleFolding;
+import com.intellij.openapi.project.Project;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -24,13 +25,13 @@ import java.util.List;
 /** {@link AndroidLogcatReceiver} expands stack traces that were elided "(...N more)". This folds just those additional lines. */
 public class ExceptionFolding extends ConsoleFolding {
   @Override
-  public boolean shouldFoldLine(@NotNull String line) {
+  public boolean shouldFoldLine(@NotNull Project project, @NotNull String line) {
     return StackTraceExpander.wasLineExpanded(line);
   }
 
   @Nullable
   @Override
-  public String getPlaceholderText(@NotNull List<String> lines) {
+  public String getPlaceholderText(@NotNull Project project, @NotNull List<String> lines) {
     return " <" + lines.size() + " more...>";
   }
 }
