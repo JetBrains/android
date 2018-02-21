3953288c857f4013169a12cb0733fb8da4ac9097
Juan C Nuno <juancnuno@google.com>
2018-02-21 15:29:43 -0800

    Sort view attributes as well
    Bug: https://issuetracker.google.com/62075688
    Test: AttributeComparatorTest
    Change-Id: Ide4af98036f5f0929821f8b70464c20625eccf4b

--- android/src/org/jetbrains/android/formatter/AttributeComparator.java
+++ android/src/org/jetbrains/android/formatter/AttributeComparator.java
@@ -15,11 +15,15 @@
  */
 package org.jetbrains.android.formatter;
 
-import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Sets;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Collection;
 import java.util.Comparator;
+import java.util.EnumSet;
+import java.util.Optional;
 import java.util.function.Function;
+import java.util.regex.Pattern;
 
 /**
  * An Android XML attribute comparator.
@@ -28,152 +32,6 @@ import java.util.function.Function;
  *            constructor.
  */
 final class AttributeComparator<A> implements Comparator<A> {
-  /**
-   * A group of attributes. The sub-values of a group have their own sort order within the group.
-   */
-  private enum Group implements Comparator<String> {
-    LAYOUT_PARAM {
-      @NotNull
-      @Override
-      Object[] subValues() {
-        return LayoutParam.values();
-      }
-
-      @Override
-      public int compare(@NotNull String attribute1, @NotNull String attribute2) {
-        return LayoutParam.valueOf(attribute1).compareTo(LayoutParam.valueOf(attribute2));
-      }
-    },
-    MARGIN_LAYOUT_PARAM {
-      @NotNull
-      @Override
-      Object[] subValues() {
-        return MarginLayoutParam.values();
-      }
-
-      @Override
-      public int compare(@NotNull String attribute1, @NotNull String attribute2) {
-        return MarginLayoutParam.valueOf(attribute1).compareTo(MarginLayoutParam.valueOf(attribute2));
-      }
-    },
-    GRID_LAYOUT_PARAM {
-      @NotNull
-      @Override
-      Object[] subValues() {
-        return GridLayoutParam.values();
-      }
-
-      @Override
-      public int compare(@NotNull String attribute1, @NotNull String attribute2) {
-        return GridLayoutParam.valueOf(attribute1).compareTo(GridLayoutParam.valueOf(attribute2));
-      }
-    },
-    RELATIVE_LAYOUT_PARAM {
-      @NotNull
-      @Override
-      Object[] subValues() {
-        return RelativeLayoutParam.values();
-      }
-
-      @Override
-      public int compare(@NotNull String attribute1, @NotNull String attribute2) {
-        return RelativeLayoutParam.valueOf(attribute1).compareTo(RelativeLayoutParam.valueOf(attribute2));
-      }
-    },
-    OTHER {
-      @NotNull
-      @Override
-      Object[] subValues() {
-        return new Object[0];
-      }
-
-      @Override
-      public int compare(@NotNull String attribute1, String attribute2) {
-        return attribute1.compareTo(attribute2);
-      }
-    };
-
-    private static ImmutableMap<String, Group> ourAttributeToGroupMap;
-
-    @NotNull
-    private static ImmutableMap<String, Group> initAttributeToGroupMap() {
-      ImmutableMap.Builder<String, Group> builder = new ImmutableMap.Builder<>();
-
-      for (Group group : values()) {
-        for (Object subValue : group.subValues()) {
-          builder.put(subValue.toString(), group);
-        }
-      }
-
-      return builder.build();
-    }
-
-    @NotNull
-    abstract Object[] subValues();
-
-    @NotNull
-    private static Group getGroup(@NotNull String attribute) {
-      if (ourAttributeToGroupMap == null) {
-        ourAttributeToGroupMap = initAttributeToGroupMap();
-      }
-
-      return ourAttributeToGroupMap.getOrDefault(attribute, OTHER);
-    }
-  }
-
-  private enum LayoutParam {
-    layout_width,
-    layout_height
-  }
-
-  private enum MarginLayoutParam {
-    layout_margin,
-    layout_marginHorizontal,
-    layout_marginVertical,
-    layout_marginStart,
-    layout_marginLeft,
-    layout_marginTop,
-    layout_marginEnd,
-    layout_marginRight,
-    layout_marginBottom
-  }
-
-  private enum GridLayoutParam {
-    layout_row,
-    layout_rowSpan,
-    layout_rowWeight,
-    layout_column,
-    layout_columnSpan,
-    layout_columnWeight,
-    layout_gravity
-  }
-
-  private enum RelativeLayoutParam {
-    layout_alignWithParentIfMissing,
-    layout_toStartOf,
-    layout_toLeftOf,
-    layout_toEndOf,
-    layout_toRightOf,
-    layout_above,
-    layout_below,
-    layout_alignBaseline,
-    layout_alignStart,
-    layout_alignLeft,
-    layout_alignTop,
-    layout_alignEnd,
-    layout_alignRight,
-    layout_alignBottom,
-    layout_alignParentStart,
-    layout_alignParentLeft,
-    layout_alignParentTop,
-    layout_alignParentEnd,
-    layout_alignParentRight,
-    layout_alignParentBottom,
-    layout_centerInParent,
-    layout_centerHorizontal,
-    layout_centerVertical
-  }
-
   private final Function<A, String> myGetLocalPart;
 
   /**
@@ -185,13 +43,167 @@ final class AttributeComparator<A> implements Comparator<A> {
 
   @Override
   public int compare(A attribute1, A attribute2) {
-    String part1 = myGetLocalPart.apply(attribute1);
-    Group group1 = Group.getGroup(part1);
+    return new Attribute(myGetLocalPart.apply(attribute1)).compareTo(new Attribute(myGetLocalPart.apply(attribute2)));
+  }
 
-    String part2 = myGetLocalPart.apply(attribute2);
-    Group group2 = Group.getGroup(part2);
+  private static final class Attribute implements Comparable<Attribute> {
+    private static final Comparator<Attribute> COMPARATOR = Comparator.<Attribute, Type>comparing(attribute -> attribute.myType)
+      .thenComparing(attribute -> attribute.myLocalPartWithoutSpecifier)
+      .thenComparing(attribute -> attribute.mySpecifier);
 
-    int result = group1.compareTo(group2);
-    return result == 0 ? group1.compare(part1, part2) : result;
+    private final Type myType;
+    private final String myLocalPartWithoutSpecifier;
+    private final Specifier mySpecifier;
+
+    private Attribute(@NotNull String localPart) {
+      myType = Type.get(localPart);
+
+      Specifier specifier = Specifier.get(localPart);
+      myLocalPartWithoutSpecifier = specifier.removeFrom(localPart);
+      mySpecifier = specifier;
+    }
+
+    @Override
+    public int compareTo(@NotNull Attribute attribute) {
+      return COMPARATOR.compare(this, attribute);
+    }
+  }
+
+  private enum Type {
+    LAYOUT_WIDTH {
+      @Override
+      boolean matches(@NotNull String localPart) {
+        return localPart.equals("layout_width");
+      }
+    },
+
+    LAYOUT_HEIGHT {
+      @Override
+      boolean matches(@NotNull String localPart) {
+        return localPart.equals("layout_height");
+      }
+    },
+
+    LAYOUT_ROW {
+      @Override
+      boolean matches(@NotNull String localPart) {
+        return localPart.startsWith("layout_row");
+      }
+    },
+
+    LAYOUT_COLUMN {
+      @Override
+      boolean matches(@NotNull String localPart) {
+        return localPart.startsWith("layout_column");
+      }
+    },
+
+    LAYOUT_ALIGN_WITH_PARENT_IF_MISSING {
+      @Override
+      boolean matches(@NotNull String localPart) {
+        return localPart.equals("layout_alignWithParentIfMissing");
+      }
+    },
+
+    LAYOUT_ABOVE {
+      @Override
+      boolean matches(@NotNull String localPart) {
+        return localPart.equals("layout_above");
+      }
+    },
+
+    LAYOUT_BELOW {
+      @Override
+      boolean matches(@NotNull String localPart) {
+        return localPart.equals("layout_below");
+      }
+    },
+
+    LAYOUT {
+      @Override
+      boolean matches(@NotNull String localPart) {
+        return localPart.startsWith("layout_");
+      }
+    },
+
+    WIDTH {
+      @Override
+      boolean matches(@NotNull String localPart) {
+        return localPart.equals("width");
+      }
+    },
+
+    HEIGHT {
+      @Override
+      boolean matches(@NotNull String localPart) {
+        return localPart.equals("height");
+      }
+    },
+
+    NON_LAYOUT {
+      @Override
+      boolean matches(@NotNull String localPart) {
+        throw new UnsupportedOperationException();
+      }
+    };
+
+    private static final Collection<Type> TYPES = Sets.immutableEnumSet(EnumSet.complementOf(EnumSet.of(NON_LAYOUT)));
+
+    @NotNull
+    private static Type get(@NotNull String localPart) {
+      Optional<Type> optionalType = TYPES.stream()
+        .filter(type -> type.matches(localPart))
+        .findFirst();
+
+      return optionalType.orElse(NON_LAYOUT);
+    }
+
+    abstract boolean matches(@NotNull String localPart);
+  }
+
+  private enum Specifier {
+    NULL("") {
+      @NotNull
+      @Override
+      String removeFrom(@NotNull String localPart) {
+        return localPart;
+      }
+    },
+
+    WIDTH("Width"),
+    HEIGHT("Height"),
+    IN_PARENT("InParent"),
+    HORIZONTAL("Horizontal"),
+    VERTICAL("Vertical"),
+    BASELINE("Baseline"),
+    START("Start"),
+    LEFT("Left"),
+    TOP("Top"),
+    END("End"),
+    RIGHT("Right"),
+    BOTTOM("Bottom"),
+    UP("Up"),
+    DOWN("Down");
+
+    private static final Collection<Specifier> SPECIFIERS = Sets.immutableEnumSet(EnumSet.complementOf(EnumSet.of(NULL)));
+    private final Pattern myPattern;
+
+    Specifier(@NotNull String name) {
+      myPattern = Pattern.compile(name, Pattern.LITERAL);
+    }
+
+    @NotNull
+    private static Specifier get(@NotNull String localPart) {
+      Optional<Specifier> optionalSpecifier = SPECIFIERS.stream()
+        .filter(specifier -> localPart.contains(specifier.myPattern.toString()))
+        .findFirst();
+
+      return optionalSpecifier.orElse(NULL);
+    }
+
+    @NotNull
+    String removeFrom(@NotNull String localPart) {
+      return myPattern.matcher(localPart).replaceFirst("");
+    }
   }
 }
