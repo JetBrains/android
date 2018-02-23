286b22aa311203d60da45fc2b0ac468708bdce5a
Vadim Caen <caen@google.com>
2018-02-23 20:16:15 +0000

    Display color in ModuleResourceBrowser
    Bug: 73807046
    Test: Added unit test
    
    Change-Id: I11cdd31a43cc83acfd79a22c9a6ad43d0aebfab1

--- android/src/com/android/tools/idea/resourceExplorer/viewmodel/ModuleResourcesBrowserViewModel.kt
+++ android/src/com/android/tools/idea/resourceExplorer/viewmodel/ModuleResourcesBrowserViewModel.kt
@@ -15,23 +15,34 @@
  */
 package com.android.tools.idea.resourceExplorer.viewmodel
 
+import com.android.ide.common.rendering.api.ResourceValue
 import com.android.ide.common.resources.ResourceItem
+import com.android.ide.common.resources.ResourceResolver
+import com.android.ide.common.resources.configuration.FolderConfiguration
 import com.android.resources.ResourceFolderType
 import com.android.resources.ResourceType
+import com.android.tools.idea.configurations.ConfigurationManager
+import com.android.tools.idea.configurations.ResourceResolverCache
+import com.android.tools.idea.model.MergedManifest
+import com.android.tools.idea.res.AppResourceRepository
 import com.android.tools.idea.res.ModuleResourceRepository
+import com.android.tools.idea.resourceExplorer.importer.SynchronizationListener
+import com.android.tools.idea.resourceExplorer.importer.SynchronizationManager
 import com.android.tools.idea.resourceExplorer.model.DesignAsset
 import com.android.tools.idea.resourceExplorer.model.DesignAssetListModel
 import com.android.tools.idea.resourceExplorer.model.DesignAssetSet
-import com.android.tools.idea.resourceExplorer.importer.SynchronizationListener
-import com.android.tools.idea.resourceExplorer.importer.SynchronizationManager
 import com.android.tools.idea.resourceExplorer.plugin.DesignAssetRendererManager
 import com.android.tools.idea.resourceExplorer.view.DesignAssetExplorer
 import com.google.common.util.concurrent.ListenableFuture
+import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.vfs.VfsUtil
 import com.intellij.openapi.vfs.VirtualFile
 import org.jetbrains.android.facet.AndroidFacet
 import java.awt.Dimension
 import java.awt.Image
+
+
+private val LOG = Logger.getInstance(ModuleResourcesBrowserViewModel::class.java)
 
 /**
  * ViewModel for [com.android.tools.idea.resourceExplorer.view.ModuleResourceBrowser]
@@ -42,6 +53,7 @@ class ModuleResourcesBrowserViewModel(
   synchronizationManager: SynchronizationManager
 ) : DesignAssetExplorer {
 
+  val resourceResolver = createResourceResolver()
   override val designAssetListModel = DesignAssetListModel()
 
   init {
@@ -63,7 +75,10 @@ class ModuleResourcesBrowserViewModel(
    * Returns a list of [DesignAssetSet] representing the resources in the facet.
    */
   private fun getModuleDesignAssets(): List<DesignAssetSet> {
-    val repository = ModuleResourceRepository.getOrCreateInstance(facet)
+    // TODO use the AppResourceRepository to get all resources from all the namespace
+    // TODO Reuse the code from getColorListModel
+    val repository =
+      ModuleResourceRepository.getOrCreateInstance(facet)
     val type = ResourceType.DRAWABLE
     return repository.getItemsOfType(type)
       .flatMap { repository.getResourceItem(type, it) ?: emptyList() }
@@ -90,4 +105,52 @@ class ModuleResourcesBrowserViewModel(
   }
 
   override fun getStatusLabel(assetSet: DesignAssetSet) = ""
+
+  fun getResourceValues(
+    type: ResourceType
+  ): Collection<ResourceValue> {
+
+    // TODO see if we return one listModel per Namespace)
+    return (getModuleResources(type) + getLibrariesResources(type))
+      .mapNotNull { resourceItem -> resolveValue(resourceResolver, resourceItem) }
+  }
+
+  private fun getModuleResources(type: ResourceType): List<ResourceItem> {
+    val moduleRepository = ModuleResourceRepository.getOrCreateInstance(facet)
+    return moduleRepository.namespaces.flatMap { namespace ->
+      moduleRepository.getItemsOfType(namespace, type)
+        .flatMap { resourceItem -> moduleRepository.getResourceItems(namespace, type, resourceItem) }
+    }
+  }
+
+  private fun resolveValue(
+    resourceResolver: ResourceResolver, it: ResourceItem
+  ): ResourceValue? {
+    val resolvedValue = resourceResolver.resolveResValue(it.resourceValue)
+    if (resolvedValue == null) {
+      LOG.warn("${it.name} couldn't be resolved")
+    }
+    return resolvedValue
+  }
+
+  private fun getLibrariesResources(type: ResourceType): List<ResourceItem> {
+    val repository = AppResourceRepository.getOrCreateInstance(facet)
+    return repository.libraries.flatMap { lib ->
+      lib.namespaces.flatMap { namespace ->
+        lib.getItemsOfType(namespace, type)
+          .flatMap { resourceItem -> lib.getResourceItems(namespace, type, resourceItem) }
+      }
+    }
+  }
+
+  private fun createResourceResolver(): ResourceResolver {
+    val configurationManager = ConfigurationManager.getOrCreateInstance(facet)
+    val manifest = MergedManifest.get(facet)
+    val theme = manifest.manifestTheme ?: manifest.getDefaultTheme(null, null, null)
+    return ResourceResolverCache(configurationManager).getResourceResolver(
+      configurationManager.target,
+      theme,
+      FolderConfiguration.createDefault()
+    )
+  }
 }
---
+++ android/testSrc/com/android/tools/idea/resourceExplorer/viewmodel/ModuleResourcesBrowserViewModelTest.kt
@@ -0,0 +1,86 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.resourceExplorer.viewmodel
+
+import com.android.resources.ResourceType
+import com.android.testutils.TestUtils
+import com.android.tools.idea.res.*
+import com.android.tools.idea.resourceExplorer.getTestDataDirectory
+import com.android.tools.idea.resourceExplorer.importer.SynchronizationManager
+import com.android.tools.idea.testing.AndroidProjectRule
+import com.google.common.truth.Truth
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.roots.ModuleRootManager
+import com.intellij.openapi.util.Disposer
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.testFramework.EdtRule
+import com.intellij.testFramework.EdtTestUtil
+import com.intellij.testFramework.RuleChain
+import com.intellij.testFramework.RunsInEdt
+import org.jetbrains.android.facet.AndroidFacet
+import org.jetbrains.android.facet.AndroidFacetType
+import org.junit.After
+import org.junit.Test
+
+import org.junit.Before
+import org.junit.Rule
+import java.util.*
+
+class ModuleResourcesBrowserViewModelTest {
+  private val projectRule = AndroidProjectRule.onDisk()
+
+  private val chain = RuleChain(projectRule, EdtRule())
+  @Rule
+  fun getChain() = chain
+
+  private val disposable = Disposer.newDisposable("ModuleResourcesBrowserViewModelTest")
+
+  @Before
+  fun setUp() {
+    projectRule.fixture.testDataPath = getTestDataDirectory()
+  }
+
+  @After
+  fun tearDown() {
+    Disposer.dispose(disposable)
+  }
+
+  @Test
+  fun getResourceResolver() {
+    val viewModel = createViewModel()
+    Truth.assertThat(viewModel.resourceResolver).isNotNull()
+  }
+
+  @RunsInEdt
+  @Test
+  fun getResourceValues() {
+    projectRule.fixture.copyFileToProject("res/values/colors.xml", "res/values/colors.xml")
+    val viewModel = createViewModel()
+
+    val values = viewModel.getResourceValues(ResourceType.COLOR)
+    Truth.assertThat(values).isNotNull()
+    Truth.assertThat(values.map { it.value }).containsExactly("#3F51B5", "#303F9F", "#9dff00")
+    // TODO Test dependencies
+  }
+
+  private fun createViewModel(): ModuleResourcesBrowserViewModel {
+    val module = projectRule.module
+    val facet = AndroidFacet.getInstance(module)!!
+    val synchronizationManager = SynchronizationManager(facet)
+    Disposer.register(disposable, synchronizationManager)
+    return ModuleResourcesBrowserViewModel(facet, synchronizationManager)
+  }
+}
