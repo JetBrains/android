f5ddcfaefd7851114832418f1880ef02f791790a
Sergio Murguia <srmurguia@google.com>
2018-02-27 15:42:15 -0800

    Check if project is initialized in MissingAndroidPluginErrorHandler
    When a project is imported and it contains an AGP version that cannot be
    found, an error message is generated. This is processed by
    MissingAndroidPluginErrorHandler, but if the project is not yet initialized,
    a Logger$EmptyThrowable is generated when trying to see if google repository
    is present.
    
    This CL solves this issue by first checking if the project is initialized
    and act accordingly. If the project is initialized, we can check and
    decide if we show the quickfix. If the project is not initialized, we add
    the quickfix with a slightly modified message.
    
    Bug: 73948080
    Test: New test to confirm the correct hyperlinks are created when project is
    not initialized.
    
    Change-Id: I4c91f2613f3b35d539907ba61df46ce8097bfff2
    (cherry picked from commit 20ae7507e88f4ec54c353614f14e644d9441de6f)

--- android/src/com/android/tools/idea/gradle/project/sync/hyperlink/AddGoogleMavenRepositoryHyperlink.java
+++ android/src/com/android/tools/idea/gradle/project/sync/hyperlink/AddGoogleMavenRepositoryHyperlink.java
@@ -17,13 +17,16 @@ package com.android.tools.idea.gradle.project.sync.hyperlink;
 
 import com.android.annotations.VisibleForTesting;
 import com.android.tools.idea.gradle.dsl.api.GradleBuildModel;
+import com.android.tools.idea.gradle.plugin.AndroidPluginInfo;
 import com.android.tools.idea.gradle.project.sync.GradleSyncInvoker;
 import com.android.tools.idea.project.hyperlink.NotificationHyperlink;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import static com.android.tools.idea.gradle.dsl.api.GradleBuildModel.parseBuildFile;
+import static com.android.tools.idea.gradle.plugin.AndroidPluginInfo.searchInBuildFilesOnly;
 import static com.google.wireless.android.sdk.stats.GradleSyncStats.Trigger.TRIGGER_PROJECT_MODIFIED;
 import static com.intellij.openapi.command.WriteCommandAction.runWriteCommandAction;
 
@@ -31,30 +34,31 @@ import static com.intellij.openapi.command.WriteCommandAction.runWriteCommandAct
  * Hyperlink to add {@code google()} to the repositories in buildscript block of project build.gradle file.
  */
 public class AddGoogleMavenRepositoryHyperlink extends NotificationHyperlink {
-  @NotNull private final VirtualFile myBuildFile;
+  @Nullable private final VirtualFile myBuildFile;
   private final boolean mySyncAfterFix;
 
-  public AddGoogleMavenRepositoryHyperlink(@NotNull VirtualFile buildFile) {
+  public AddGoogleMavenRepositoryHyperlink(@Nullable VirtualFile buildFile) {
     this(buildFile, true);
   }
 
   @VisibleForTesting
-  public AddGoogleMavenRepositoryHyperlink(@NotNull VirtualFile buildFile, boolean syncAfterFix) {
-    super("add.google.maven.repository", getText(syncAfterFix));
+  public AddGoogleMavenRepositoryHyperlink(@Nullable VirtualFile buildFile, boolean syncAfterFix) {
+    super("add.google.maven.repository", getText(buildFile, syncAfterFix));
     myBuildFile = buildFile;
     mySyncAfterFix = syncAfterFix;
   }
 
   @Override
   protected void execute(@NotNull Project project) {
-    GradleBuildModel buildModel = parseBuildFile(myBuildFile, project);
-    buildModel.repositories().addGoogleMavenRepository(project);
-    runWriteCommandAction(project, buildModel::applyChanges);
-    GradleBuildModel buildModelProject = GradleBuildModel.get(project);
-    // Also add to buildscript
-    if (buildModelProject != null) {
-      buildModelProject.buildscript().repositories().addGoogleMavenRepository(project);
-      runWriteCommandAction(project, buildModelProject::applyChanges);
+    GradleBuildModel buildModel;
+    if (myBuildFile != null) {
+      buildModel = parseBuildFile(myBuildFile, project);
+    }
+    else {
+      buildModel = getBuildModelForPlugin(project);
+    }
+    if (buildModel != null) {
+      addGoogleMavenRepository(buildModel);
     }
     if (mySyncAfterFix) {
       GradleSyncInvoker.getInstance().requestProjectSyncAndSourceGeneration(project, TRIGGER_PROJECT_MODIFIED);
@@ -62,16 +66,58 @@ public class AddGoogleMavenRepositoryHyperlink extends NotificationHyperlink {
   }
 
   @NotNull
-  private static String getText(boolean syncAfterFix) {
+  private static String getText(@Nullable VirtualFile buildFile, boolean syncAfterFix) {
     String text = "Add Google Maven repository";
+    if (buildFile == null) {
+      text += " (if needed)";
+    }
     if (syncAfterFix) {
       text += " and sync project";
     }
     return text;
   }
 
-  @NotNull
+  @Nullable
   public VirtualFile getBuildFile() {
     return myBuildFile;
+  }
+
+  /**
+   * Add Google Maven Repository to the given buildModel
+   * @param buildModel
+   */
+  private static void addGoogleMavenRepository(@NotNull GradleBuildModel buildModel) {
+    Project project = buildModel.getProject();
+    buildModel.repositories().addGoogleMavenRepository(project);
+    buildModel.buildscript().repositories().addGoogleMavenRepository(project);
+    runWriteCommandAction(project, buildModel::applyChanges);
+    GradleBuildModel buildModelProject = GradleBuildModel.get(project);
+    // Also add to project's buildscript
+    if (buildModelProject != null) {
+      buildModelProject.buildscript().repositories().addGoogleMavenRepository(project);
+      runWriteCommandAction(project, buildModelProject::applyChanges);
+    }
+  }
+
+  /**
+   * Get GradleBuildModel that contains the Android Gradle Plugin, or project's build model if the plugin cannot be found.
+   * @param project
+   * @return
+   */
+  @Nullable
+  public static GradleBuildModel getBuildModelForPlugin(Project project) {
+    // Get Android Plugin info from the project, if plugin info can not be found, use project build.gradle file instead
+    AndroidPluginInfo result = searchInBuildFilesOnly(project);
+    GradleBuildModel gradleBuildModel = null;
+    if (result != null) {
+      VirtualFile buildFile = result.getPluginBuildFile();
+      if (buildFile != null) {
+        gradleBuildModel = parseBuildFile(buildFile, project);
+      }
+    }
+    if (gradleBuildModel == null) {
+      gradleBuildModel = GradleBuildModel.get(project);
+    }
+    return gradleBuildModel;
   }
 }
