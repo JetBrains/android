0d2adf0770e1773a5f5283ab8346ff0b446ac576
Sergey Prigogin <sprigogin@google.com>
2018-02-26 16:17:19 -0800

    Remove one of calling points of ResourceManager.findValueResourceInfos
    ResourceManager.findValueResourceInfos is the only method that exposes
    XML element offsets stored in android.value.resources.index. This CL is
    a step towards eliminating ResourceManager.findValueResourceInfos and
    android.value.resources.index.
    
    Test: existing tests
    Bug: N/A
    Change-Id: Ia060d5e018dedbedc6f6858002c293219a0a305d

--- android/src/org/jetbrains/android/dom/resources/ResourceNameConverter.java
+++ android/src/org/jetbrains/android/dom/resources/ResourceNameConverter.java
@@ -1,8 +1,12 @@
 package org.jetbrains.android.dom.resources;
 
 import com.android.SdkConstants;
+import com.android.ide.common.rendering.api.ResourceNamespace;
+import com.android.ide.common.rendering.api.StyleResourceValue;
+import com.android.ide.common.resources.ResourceItem;
 import com.android.ide.common.resources.ValueResourceNameValidator;
 import com.android.resources.ResourceType;
+import com.android.tools.idea.res.AppResourceRepository;
 import com.intellij.codeInspection.LocalQuickFix;
 import com.intellij.codeInspection.LocalQuickFixProvider;
 import com.intellij.openapi.module.Module;
@@ -16,8 +20,6 @@ import org.jetbrains.android.dom.converters.AndroidResourceReferenceBase;
 import org.jetbrains.android.facet.AndroidFacet;
 import org.jetbrains.android.inspections.CreateValueResourceQuickFix;
 import org.jetbrains.android.resourceManagers.LocalResourceManager;
-import org.jetbrains.android.resourceManagers.ModuleResourceManagers;
-import org.jetbrains.android.resourceManagers.ValueResourceInfoImpl;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -146,17 +148,14 @@ public class ResourceNameConverter extends ResolvingConverter<String> implements
   }
 
   public static boolean hasExplicitParent(@NotNull AndroidFacet facet, @NotNull String localStyleName) {
-    LocalResourceManager resourceManager = ModuleResourceManagers.getInstance(facet).getLocalResourceManager();
-    List<ValueResourceInfoImpl> styles = resourceManager.findValueResourceInfos(ResourceType.STYLE.getName(), localStyleName, true, false);
-
+    AppResourceRepository repository = AppResourceRepository.getOrCreateInstance(facet);
+    List<ResourceItem> styles = repository.getResourceItems(ResourceNamespace.TODO, ResourceType.STYLE, localStyleName);
     if (styles.isEmpty()) {
       return false;
     }
-    // all resolved styles have explicit parents
-    for (ValueResourceInfoImpl info : styles) {
-      final ResourceElement domElement = info.computeDomElement();
-
-      if (!(domElement instanceof Style) || ((Style)domElement).getParentStyle().getStringValue() == null) {
+    for (ResourceItem style : styles) {
+      com.android.ide.common.rendering.api.ResourceValue resourceValue = style.getResourceValue();
+      if (!(resourceValue instanceof StyleResourceValue) || ((StyleResourceValue) resourceValue).getParentStyleName() == null) {
         return false;
       }
     }
