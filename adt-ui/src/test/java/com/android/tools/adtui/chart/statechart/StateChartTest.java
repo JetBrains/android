b92ba29fe984655eea1cc330672965fca508b789
Joshua Gilpatrick <gijosh@google.com>
2018-03-06 09:30:29 -0800

    Adding StateChartTextConverter
    The state chart text converter allows us to define what text is
    rendered in the state chart without relying on the toString of
    an object. This change is required as we want to customize the
    rendering of cpu state text in the kernel view of an atrace
    
    Test: StateChartTest
    Bug: 64693095
    Change-Id: Ic2cc7560bd419002de8997241120ce5acc8c0aee

--- adt-ui/src/test/java/com/android/tools/adtui/chart/statechart/StateChartTest.java
+++ adt-ui/src/test/java/com/android/tools/adtui/chart/statechart/StateChartTest.java
@@ -15,18 +15,14 @@
  */
 package com.android.tools.adtui.chart.statechart;
 
-import com.android.tools.adtui.model.DataSeries;
-import com.android.tools.adtui.model.Range;
-import com.android.tools.adtui.model.RangedSeries;
-import com.android.tools.adtui.model.StateChartModel;
+import com.android.tools.adtui.model.*;
 import com.google.common.collect.ImmutableList;
 import org.junit.Test;
 
 import java.awt.*;
 import java.util.EnumMap;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 public class StateChartTest {
 
@@ -43,6 +39,53 @@ public class StateChartTest {
     Graphics2D fakeGraphics = mock(Graphics2D.class);
     when(fakeGraphics.create()).thenReturn(fakeGraphics);
     stateChart.paint(fakeGraphics);
+  }
+
+  @Test
+  public void testStateChartTextConverter() {
+    StateChartModel<Integer> model = new StateChartModel<>();
+    DataSeries<Integer> dataSeries = (range) -> ImmutableList.of(new SeriesData<>(0,1),
+                                                                 new SeriesData<>(1000, 2));
+
+    model.addSeries(new RangedSeries<>(new Range(0, 100), dataSeries));
+    StateChart<Integer> stateChart = new StateChart<>(model, (state) -> Color.BLACK, (value) -> "123");
+    stateChart.setSize(100, 100);
+    stateChart.setRenderMode(StateChart.RenderMode.TEXT);
+
+    Graphics2D fakeGraphics = mock(Graphics2D.class);
+    when(fakeGraphics.create()).thenReturn(fakeGraphics);
+    stateChart.paint(fakeGraphics);
+    verify(fakeGraphics, times(1)).drawString(eq("123"), anyFloat(), anyFloat());
+  }
+
+  @Test
+  public void testStateChartWithDefaultTextConverterUsesToString() {
+    StateChartModel<ToStringTestClass> model = new StateChartModel<>();
+    DataSeries<ToStringTestClass> dataSeries = (range) -> ImmutableList.of(
+      new SeriesData<>(0, new ToStringTestClass("Test")),
+      new SeriesData<>(1000, new ToStringTestClass("Test2")));
+
+    model.addSeries(new RangedSeries<>(new Range(0, 100), dataSeries));
+    StateChart<ToStringTestClass> stateChart = new StateChart<>(model, (state) -> Color.BLACK);
+    stateChart.setSize(100, 100);
+    stateChart.setRenderMode(StateChart.RenderMode.TEXT);
+
+    Graphics2D fakeGraphics = mock(Graphics2D.class);
+    when(fakeGraphics.create()).thenReturn(fakeGraphics);
+    stateChart.paint(fakeGraphics);
+    verify(fakeGraphics, times(1)).drawString(eq("Test"), anyFloat(), anyFloat());
+  }
+
+  private static class ToStringTestClass {
+    private String myString;
+    public ToStringTestClass(String string) {
+      myString = string;
+    }
+
+    @Override
+    public String toString() {
+      return myString;
+    }
   }
 
   private enum State {
