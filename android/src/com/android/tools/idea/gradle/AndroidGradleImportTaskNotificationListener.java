9c525e84a6cd6cb17801739781eccc2614a2163c
Sergio Murguia <srmurguia@google.com>
2018-03-08 15:35:35 -0800

    Add GradleSyncState start calls to refresh project button
    There is a new "Refresh Gradle Project" button on the Build View.
    Pressing this button causes indexing to became paused forever. The cause
    of that is because the refresh button calls directly to
    ExternalSystemUtil#refreshProject causing all the required changes to
    GradleSyncState to not be processed.
    
    This CL adds changes to sync state when calling
    AndroidGradleImportTaskNotificationListener#onStart. Also,
    since sync can be re rerun from the build view button, now
    GradleSyncState saves the last parameters used when calling *syncStarted
    methods to be used by onStart method.
    
    Bug: 74360539
    Test: Manually tested that indexing is not kept paused.
    Change-Id: I98bc70bfd813ae79c3d368112c5b5252f5f528ff
    (cherry picked from commit cb2fa63600b80826470c3cd5eeb7b9e5b2be51e3)

--- android/src/com/android/tools/idea/gradle/AndroidGradleImportTaskNotificationListener.java
+++ android/src/com/android/tools/idea/gradle/AndroidGradleImportTaskNotificationListener.java
@@ -15,10 +15,13 @@
  */
 package com.android.tools.idea.gradle;
 
+import com.android.tools.idea.gradle.project.sync.GradleSyncState;
 import com.intellij.openapi.externalSystem.model.task.ExternalSystemTaskId;
 import com.intellij.openapi.externalSystem.model.task.ExternalSystemTaskNotificationListenerAdapter;
 import com.intellij.openapi.externalSystem.model.task.ExternalSystemTaskType;
+import com.intellij.openapi.project.Project;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.jetbrains.plugins.gradle.util.GradleConstants;
 
 /**
@@ -28,6 +31,21 @@ import org.jetbrains.plugins.gradle.util.GradleConstants;
  * @since 10/17/13
  */
 public class AndroidGradleImportTaskNotificationListener extends ExternalSystemTaskNotificationListenerAdapter {
+  @Override
+  public void onStart(@NotNull ExternalSystemTaskId id, @Nullable String workingDir) {
+    if (GradleConstants.SYSTEM_ID.getId().equals(id.getProjectSystemId().getId())
+        && id.getType() == ExternalSystemTaskType.RESOLVE_PROJECT) {
+      Project project = id.findProject();
+      if (project != null) {
+        GradleSyncState syncState = GradleSyncState.getInstance(project);
+        if (!syncState.canRestart()) {
+          // Do nothing, sync restart should not be processed.
+          return;
+        }
+        syncState.syncReStarted();
+      }
+    }
+  }
 
   @Override
   public void onSuccess(@NotNull ExternalSystemTaskId id) {
