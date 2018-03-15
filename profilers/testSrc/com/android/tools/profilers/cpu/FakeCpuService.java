edb71dc77b8c99f3b16e1c97dc2eae18471474f8
Amaury Medeiros <amaurym@google.com>
2018-03-15 15:39:17 +0000

    Navigate to ongoing CPU captures using the sessions panel
    List ongoing captures in the sessions panel. Clicking on an ongoing
    capture should jump to its start time and adjust the zoom in a way that
    its current range fits the screen.
    
    Test: SessionsViewTest#testCpuOngoingCaptureItemMouseInteraction
    Bug: 74362035
    Change-Id: Ia7f786997ae2bb86b7c9055ef8cd16c0f7bf2bba

--- profilers/testSrc/com/android/tools/profilers/cpu/FakeCpuService.java
+++ profilers/testSrc/com/android/tools/profilers/cpu/FakeCpuService.java
@@ -73,6 +73,8 @@ public class FakeCpuService extends CpuServiceGrpc.CpuServiceImplBase {
 
   private boolean myIsAppBeingProfiled;
 
+  private long myOngoingCaptureStartTimestamp;
+
   private int myTraceId = FAKE_TRACE_ID;
 
   private CpuProfiler.CpuProfilerType myProfilerType = CpuProfiler.CpuProfilerType.ART;
@@ -146,12 +148,22 @@ public class FakeCpuService extends CpuServiceGrpc.CpuServiceImplBase {
     CpuProfiler.ProfilingStateResponse.Builder response = CpuProfiler.ProfilingStateResponse.newBuilder();
     response.setBeingProfiled(myIsAppBeingProfiled);
     if (myIsAppBeingProfiled) {
-      response.setConfiguration(myProfilerConfiguration);
+      response.setConfiguration(myProfilerConfiguration).setStartTimestamp(myOngoingCaptureStartTimestamp);
       myProfilerType = myProfilerConfiguration.getProfilerType();
     }
 
     responseObserver.onNext(response.build());
     responseObserver.onCompleted();
+  }
+
+  /**
+   * Receives a {@link CpuProfiler.CpuProfilerConfiguration} and sets the state of the service to be profiling using such configuration.
+   * If the configuration passed is null, {@link #myIsAppBeingProfiled} should be set to false.
+   */
+  public void setOngoingCaptureConfiguration(@Nullable CpuProfiler.CpuProfilerConfiguration configuration, long startTimestamp) {
+    myProfilerConfiguration = configuration;
+    myOngoingCaptureStartTimestamp = startTimestamp;
+    myIsAppBeingProfiled = configuration != null;
   }
 
   public void setStartProfilingStatus(CpuProfiler.CpuProfilingAppStartResponse.Status status) {
