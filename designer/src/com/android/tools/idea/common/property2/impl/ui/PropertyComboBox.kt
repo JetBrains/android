432c47f1b3209eb9d37f05bae9f02e0b715da0aa
Jens Ole Lauridsen <jlauridsen@google.com>
2018-03-20 12:49:32 -0700

    Fix some update problems with a ComboBox control.
    In the property inspector v2,
    We were receiving model.setSelectedItem in several situations:
    1) When the user selects an item in the dropdown.
    2) When the ComboBox control was created.
    3) When the popup was shown.
    4) When focus was lost.
    
    We need to update the property value on (1) but ignore all the
    other calls. There were no good way to determine what caused the
    model change.
    
    In this CL we eliminate (2) and (4),
    and explicitly perform (3) such that we know NOT to update the
    property value.
    
    In addition this CL adds logic to determine why a popup is being
    closed:
    1) User hits <enter>
    2) User hits <escape>
    3) User clicks on an item in the popup
    4) User clicks on the combo control (the arrow to close the popup)
    5) User clicks somewhere else
    
    We want the value of the property to change on (1) and (3).
    And the property value should be unchanged in (2), (4), and (5).
    
    This fixes several editing problems in the ComboBox.
    
    Bug: 73778668
    Test: Existing tests pass.
    
    Change-Id: Ia1a16dd6e79c3dba7eca6eb1ba3f91fbbd51b0d3

--- designer/src/com/android/tools/idea/common/property2/impl/ui/PropertyComboBox.kt
+++ designer/src/com/android/tools/idea/common/property2/impl/ui/PropertyComboBox.kt
@@ -19,12 +19,15 @@ import com.android.tools.adtui.stdui.CommonComboBox
 import com.android.tools.idea.common.property2.api.EnumValue
 import com.android.tools.idea.common.property2.impl.model.ComboBoxPropertyEditorModel
 import com.android.tools.idea.common.property2.impl.support.EditorFocusListener
+import java.awt.EventQueue
+import java.awt.event.ActionEvent
 import java.awt.event.InputEvent
 import java.awt.event.KeyEvent
-import javax.swing.JTextField
-import javax.swing.KeyStroke
+import java.awt.event.MouseEvent
+import javax.swing.*
 import javax.swing.event.PopupMenuEvent
 import javax.swing.event.PopupMenuListener
+import javax.swing.plaf.basic.ComboPopup
 
 /**
  * A standard control for editing property values with a popup list.
@@ -32,51 +35,50 @@ import javax.swing.event.PopupMenuListener
  * This control will act as a ComboBox or a DropDown depending on the model.
  */
 class PropertyComboBox(model: ComboBoxPropertyEditorModel): CommonComboBox<EnumValue>(model) {
-  private var ignoreUpdates = false
 
   init {
     registerKeyAction({ model.enterKeyPressed() }, KeyStroke.getKeyStroke(KeyEvent.VK_ENTER, 0), "enter")
-    registerKeyAction({ model.escape() }, KeyStroke.getKeyStroke(KeyEvent.VK_F1, 0), "escape")
+    registerKeyAction({ model.escapeKeyPressed() }, KeyStroke.getKeyStroke(KeyEvent.VK_F1, 0), "escape")
 
     val editor = editor.editorComponent as JTextField
-    editor.registerKeyAction({ model.enter(currentValue) }, KeyStroke.getKeyStroke(KeyEvent.VK_ENTER, 0), "enter")
-    editor.registerKeyAction({ model.escape() }, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), "escape")
+    editor.registerKeyAction({ model.enterKeyPressed(currentValue) }, KeyStroke.getKeyStroke(KeyEvent.VK_ENTER, 0), "enter")
+    editor.registerKeyAction({ model.escapeKeyPressed() }, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), "escape")
     editor.registerKeyAction({ model.f1KeyPressed() }, KeyStroke.getKeyStroke(KeyEvent.VK_F1, 0), "help")
     editor.registerKeyAction({ model.shiftF1KeyPressed() }, KeyStroke.getKeyStroke(KeyEvent.VK_F1, InputEvent.SHIFT_DOWN_MASK), "help2")
 
     addFocusListener(EditorFocusListener(model, { currentValue }))
     addPopupMenuListener(
-        object : PopupMenuListener {
-          override fun popupMenuWillBecomeInvisible(event: PopupMenuEvent) {
-            handlePopupChange(false)
-          }
+      object : PopupMenuListener {
+        override fun popupMenuWillBecomeInvisible(event: PopupMenuEvent) {
+          // Hack: We would like to determine WHY the popup is being closed.
+          // If it happened because of either:
+          //  (1) The user pressed the escape key to close the popup
+          //  (2) The user clicked somewhere other than the JList inside the popup
+          val currentEvent = EventQueue.getCurrentEvent()
+          val fromEscapeKey = (currentEvent as? KeyEvent)?.keyCode == KeyEvent.VK_ESCAPE
+          val clickOutsideList = currentEvent is MouseEvent && !isClickOnItemInPopup(currentEvent)
+          (model as? ComboBoxPropertyEditorModel)?.popupMenuWillBecomeInvisible(fromEscapeKey || clickOutsideList)
+        }
 
-          override fun popupMenuCanceled(event: PopupMenuEvent) {
-          }
+        override fun popupMenuCanceled(event: PopupMenuEvent) {
+        }
 
-          override fun popupMenuWillBecomeVisible(event: PopupMenuEvent) {
-            handlePopupChange(true)
-          }
-        })
-  }
+        override fun popupMenuWillBecomeVisible(event: PopupMenuEvent) {
+          (model as? ComboBoxPropertyEditorModel)?.popupMenuWillBecomeVisible()
+        }
 
-  private fun handlePopupChange(becomeVisible: Boolean) {
-    ignoreUpdates = true
-    try {
-      val model = model as ComboBoxPropertyEditorModel
-      model.isPopupVisible = becomeVisible
-    }
-    finally {
-      ignoreUpdates = false
-    }
+        private fun isClickOnItemInPopup(event: MouseEvent): Boolean {
+          val source = event.source as? JList<*> ?: return false
+          val popup = SwingUtilities.getAncestorOfClass(ComboPopup::class.java, source)
+          return popup != null
+        }
+      })
   }
 
   private val currentValue: String
     get() = if (isEditable) editor.item.toString() else model.selectedItem?.toString() ?: ""
 
   override fun updateFromModel() {
-    if (ignoreUpdates) return
-
     super.updateFromModel()
     val model = model as ComboBoxPropertyEditorModel
     isVisible = model.visible
@@ -84,8 +86,18 @@ class PropertyComboBox(model: ComboBoxPropertyEditorModel): CommonComboBox<EnumV
       requestFocusInWindow()
     }
     if (model.isPopupVisible != isPopupVisible) {
-      // TODO: Handle this differently:
       isPopupVisible = model.isPopupVisible
     }
   }
+
+  // Hack: This method is called to update the text editor with the content of the
+  // selected item in the dropdown. We do not want that, since the editor text can
+  // be different from any of the values in the dropdown. Instead control the text
+  // editor value with the [CommonTextFieldModel] part of the comboBox model.
+  override fun configureEditor(anEditor: ComboBoxEditor, anItem: Any?) = Unit
+
+  // Hack: JavaDoc specifies that this method should not be overridden.
+  // However this causes a value that is not represented in the popup to be
+  // overwritten when the control looses focus. This is a workaround for now.
+  override fun actionPerformed(event: ActionEvent) = Unit
 }
