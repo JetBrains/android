4956c132ae602e61088744487b96b8530e12d717
Juan C Nuno <juancnuno@google.com>
2018-03-23 14:34:30 -0700

    Abstract the device renderer away from tests
    The production renderer uses executors which complicate unit testing
    
    Bug: Not applicable
    Test: DevicePanelTest
    Change-Id: I4fd4d6b3362aa621e4bc234821b03b45ab58f214

--- android/testSrc/com/android/tools/idea/ddms/DevicePanelTest.kt
+++ android/testSrc/com/android/tools/idea/ddms/DevicePanelTest.kt
@@ -1,20 +1,24 @@
-// Copyright (C) 2017 The Android Open Source Project
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.android.tools.idea.ddms
 
 import com.android.ddmlib.AndroidDebugBridge
 import com.android.ddmlib.IDevice
+import com.android.tools.idea.ddms.DevicePanel.DeviceComboBox
+import com.google.common.util.concurrent.FutureCallback
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.Disposer
 import org.junit.After
@@ -24,7 +28,6 @@ import org.junit.Before
 import org.junit.Test
 import org.mockito.Mockito
 import javax.swing.JComboBox
-import javax.swing.plaf.basic.BasicComboBoxRenderer
 
 internal class DevicePanelTest {
   private lateinit var myProject: Project
@@ -38,10 +41,19 @@ internal class DevicePanelTest {
     myProject = Mockito.mock(Project::class.java)
     myBridge = Mockito.mock(AndroidDebugBridge::class.java)
 
-    myPanel = DevicePanel(myProject, Mockito.mock(DeviceContext::class.java), false)
-
+    myPanel = DevicePanel(myProject, Mockito.mock(DeviceContext::class.java), TestDeviceComboBox())
     myDeviceComboBox = myPanel.deviceComboBox
-    myDeviceComboBox.renderer = BasicComboBoxRenderer.UIResource()
+  }
+
+  private class TestDeviceComboBox : DeviceComboBox() {
+    override fun initRenderer(callback: FutureCallback<DeviceNameProperties>) {
+    }
+
+    override fun dispose() {
+    }
+
+    override fun setSerialNumbersVisible(visible: Boolean) {
+    }
   }
 
   @After
