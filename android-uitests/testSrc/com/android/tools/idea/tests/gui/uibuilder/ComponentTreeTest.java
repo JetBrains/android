d0d22f343a4ac4e17e0802416c367cfd7eb9060e
Dana Dahlstrom <dahlstrom@google.com>
2018-03-26 09:16:33 -0700

    Delete testDropThatOpensDialog
    Dropping an ImageView does not open the Resouce Picker anymore since
    Change I1bd2d9df.
    
    Change-Id: Ib7c202c5070dacf459eba46be7651d2dec7d82fd

--- android-uitests/testSrc/com/android/tools/idea/tests/gui/uibuilder/ComponentTreeTest.java
+++ android-uitests/testSrc/com/android/tools/idea/tests/gui/uibuilder/ComponentTreeTest.java
@@ -17,24 +17,17 @@ package com.android.tools.idea.tests.gui.uibuilder;
 
 import com.android.tools.idea.tests.gui.framework.GuiTestRule;
 import com.android.tools.idea.tests.gui.framework.GuiTestRunner;
-import com.android.tools.idea.tests.gui.framework.fixture.ChooseResourceDialogFixture;
 import com.android.tools.idea.tests.gui.framework.fixture.EditorFixture;
 import com.android.tools.idea.tests.gui.framework.fixture.EditorFixture.Tab;
 import com.android.tools.idea.tests.gui.framework.fixture.designer.NlEditorFixture;
-import com.android.tools.idea.tests.gui.framework.GuiTestFileUtils;
-import com.android.tools.idea.tests.util.WizardUtils;
 import org.fest.swing.fixture.JTreeFixture;
-import org.intellij.lang.annotations.Language;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import java.awt.event.KeyEvent;
 import java.io.IOException;
-import java.nio.file.FileSystems;
-import java.nio.file.Path;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
@@ -42,52 +35,6 @@ import static org.junit.Assert.fail;
 public final class ComponentTreeTest {
   @Rule
   public final GuiTestRule myGuiTest = new GuiTestRule();
-
-  @Test
-  public void testDropThatOpensDialog() throws IOException {
-    WizardUtils.createNewProject(myGuiTest);
-    Path activityMainXmlRelativePath = FileSystems.getDefault().getPath("app", "src", "main", "res", "layout", "activity_main.xml");
-
-    GuiTestFileUtils.writeAndReloadDocument(
-      myGuiTest.getProjectPath().toPath().resolve(activityMainXmlRelativePath),
-
-      "<android.support.constraint.ConstraintLayout xmlns:android=\"http://schemas.android.com/apk/res/android\"\n" +
-      "    android:layout_width=\"match_parent\"\n" +
-      "    android:layout_height=\"match_parent\">\n" +
-      "\n" +
-      "</android.support.constraint.ConstraintLayout>\n");
-
-    EditorFixture editor = myGuiTest.ideFrame().getEditor();
-    editor.open(activityMainXmlRelativePath);
-
-    NlEditorFixture layoutEditor = editor.getLayoutEditor(false);
-    layoutEditor.waitForRenderToFinish();
-    layoutEditor.getPalette().dragComponent("Widgets", "ImageView");
-    // TODO This step takes around 10 s when this UI test does it (not when I do it manually). Make it faster.
-    layoutEditor.getComponentTree().drop("android.support.constraint.ConstraintLayout");
-
-    ChooseResourceDialogFixture dialog = ChooseResourceDialogFixture.find(myGuiTest.robot());
-    // TODO Same here
-    dialog.expandList("Project").getList("Project").selectItem("ic_launcher");
-    dialog.clickOK();
-
-    editor.open(activityMainXmlRelativePath, Tab.EDITOR);
-
-    @Language("XML")
-    String expected = "<android.support.constraint.ConstraintLayout xmlns:android=\"http://schemas.android.com/apk/res/android\"\n" +
-                      "    xmlns:app=\"http://schemas.android.com/apk/res-auto\"\n" +
-                      "    android:layout_width=\"match_parent\"\n" +
-                      "    android:layout_height=\"match_parent\">\n" +
-                      "\n" +
-                      "    <ImageView\n" +
-                      "        android:id=\"@+id/imageView\"\n" +
-                      "        android:layout_width=\"wrap_content\"\n" +
-                      "        android:layout_height=\"wrap_content\"\n" +
-                      "        app:srcCompat=\"@mipmap/ic_launcher\" />\n" +
-                      "</android.support.constraint.ConstraintLayout>\n";
-
-    assertEquals(expected, editor.getCurrentFileContents());
-  }
 
   @Test
   public void multiSelectComponentDoNotJumpToXML() {
