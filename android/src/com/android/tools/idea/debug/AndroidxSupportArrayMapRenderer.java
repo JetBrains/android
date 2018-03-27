47f6ba76e7d41826da3bed06ddba3ba528813ba8
Diego Perez <diegoperez@google.com>
2018-03-27 11:51:39 +0100

    Register support for androidx array renderer
    This just register another debug node renderer for the new class name.
    
    Fixes: 76021626
    Test: Existing tests pass
    Change-Id: I455aed8699a91df2db22d66ea06e650d24ef521f

--- android/src/com/android/tools/idea/debug/AndroidxSupportArrayMapRenderer.java
+++ android/src/com/android/tools/idea/debug/AndroidxSupportArrayMapRenderer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015 The Android Open Source Project
+ * Copyright (C) 2018 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,12 +15,8 @@
  */
 package com.android.tools.idea.debug;
 
-import com.android.support.AndroidxName;
-
-public class SupportArrayMapRenderer extends ArrayMapRendererBase {
-  static final AndroidxName ARRAY_MAP_NAME = AndroidxName.of("android.support.v4.util.", "ArrayMap");
-
-  public SupportArrayMapRenderer() {
-    super(ARRAY_MAP_NAME.oldName());
+public class AndroidxSupportArrayMapRenderer extends ArrayMapRendererBase {
+  public AndroidxSupportArrayMapRenderer() {
+    super(SupportArrayMapRenderer.ARRAY_MAP_NAME.newName());
   }
 }
