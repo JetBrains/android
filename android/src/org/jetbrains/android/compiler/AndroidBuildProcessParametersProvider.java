8a53b1c6458b27e75b109bc9ebf1a5d45bc245fd
Dmitry Avdeev <dmitry.avdeev@jetbrains.com>
2018-03-27 19:34:06 +0300

    converting builder-model to library

--- android/src/org/jetbrains/android/compiler/AndroidBuildProcessParametersProvider.java
+++ android/src/org/jetbrains/android/compiler/AndroidBuildProcessParametersProvider.java
@@ -1,5 +1,6 @@
 package org.jetbrains.android.compiler;
 
+import com.android.builder.model.AndroidProject;
 import com.android.ide.common.build.ApkData;
 import com.android.jarutils.SignedJarBuilder;
 import com.android.manifmerger.Merger;
@@ -27,6 +28,7 @@ public class AndroidBuildProcessParametersProvider extends BuildProcessParameter
                             PathManager.getJarPathForClass(AndroidLocation.class),
                             PathManager.getJarPathForClass(Merger.class),           // manifest merger
                             PathManager.getJarPathForClass(ApkData.class),          // sdk common
+                            PathManager.getJarPathForClass(AndroidProject.class),   // builder-model
                             PathManager.getJarPathForClass(Repository.class),       // repository
                             PathManager.getJarPathForClass(BuildException.class));  // gradle tooling
   }
