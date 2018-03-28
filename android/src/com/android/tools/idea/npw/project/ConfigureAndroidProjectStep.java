01661844328f6e2d2de23eb2e6c3625db32a0920
parentej <parentej@google.com>
2018-03-28 11:04:58 +0100

    [NPW] New design - Show "Choose Api" dialog
    - During the "Configure Android Project" step, if the user
    press "Help me choose" show the "Choose Api" dialog, instead
    of launching the browser
    - Move code related with the "Minimum Api Level", such
    as "Api percentages" and "Api percentages loading" code, to
    its own class.
    
    Bug: 73812362
    Test: N/A Current UI tests will be updated to run new design
    Change-Id: Ic1d4fc4da82feadc22753e277b1b0ee828ea4851

--- android/src/com/android/tools/idea/npw/project/ConfigureAndroidProjectStep.java
+++ android/src/com/android/tools/idea/npw/project/ConfigureAndroidProjectStep.java
@@ -15,6 +15,7 @@
  */
 package com.android.tools.idea.npw.project;
 
+import com.android.SdkConstants;
 import com.android.ide.common.sdk.LoadStatus;
 import com.android.repository.api.RemotePackage;
 import com.android.repository.api.UpdatablePackage;
@@ -25,6 +26,7 @@ import com.android.tools.adtui.validation.ValidatorPanel;
 import com.android.tools.idea.npw.FormFactor;
 import com.android.tools.idea.npw.model.NewProjectModel;
 import com.android.tools.idea.npw.model.NewProjectModuleModel;
+import com.android.tools.idea.npw.model.RenderTemplateModel;
 import com.android.tools.idea.npw.module.FormFactorApiComboBox;
 import com.android.tools.idea.npw.platform.AndroidVersionsInfo;
 import com.android.tools.idea.npw.platform.Language;
@@ -35,28 +37,20 @@ import com.android.tools.idea.observable.BindingsManager;
 import com.android.tools.idea.observable.ListenerManager;
 import com.android.tools.idea.observable.core.*;
 import com.android.tools.idea.observable.expressions.Expression;
-import com.android.tools.idea.observable.ui.SelectedItemProperty;
 import com.android.tools.idea.observable.ui.SelectedProperty;
 import com.android.tools.idea.observable.ui.TextProperty;
 import com.android.tools.idea.sdk.AndroidSdks;
 import com.android.tools.idea.sdk.wizard.InstallSelectedPackagesStep;
 import com.android.tools.idea.sdk.wizard.LicenseAgreementModel;
 import com.android.tools.idea.sdk.wizard.LicenseAgreementStep;
-import com.android.tools.idea.stats.DistributionService;
 import com.android.tools.idea.ui.validation.validators.PathValidator;
 import com.android.tools.idea.ui.wizard.StudioWizardStepPanel;
 import com.android.tools.idea.ui.wizard.WizardUtils;
 import com.android.tools.idea.wizard.model.ModelWizard;
 import com.android.tools.idea.wizard.model.ModelWizardStep;
 import com.google.common.collect.Lists;
-import com.intellij.icons.AllIcons;
-import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.ui.TextFieldWithBrowseButton;
-import com.intellij.ui.HyperlinkLabel;
-import com.intellij.ui.JBColor;
-import com.intellij.ui.components.JBLabel;
 import com.intellij.ui.components.JBScrollPane;
-import com.intellij.util.ui.AsyncProcessIcon;
 import com.intellij.util.ui.ImageUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -65,7 +59,9 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -83,7 +79,6 @@ public class ConfigureAndroidProjectStep extends ModelWizardStep<NewProjectModul
 
   private final JBScrollPane myRootPanel;
   private final ValidatorPanel myValidatorPanel;
-  private final AndroidVersionsInfo myAndroidVersionsInfo = new AndroidVersionsInfo();
   private final BindingsManager myBindings = new BindingsManager();
   private final ListenerManager myListeners = new ListenerManager();
   private final List<UpdatablePackage> myInstallRequests = new ArrayList<>();
@@ -100,16 +95,8 @@ public class ConfigureAndroidProjectStep extends ModelWizardStep<NewProjectModul
   private JCheckBox myTvCheck;
   private JLabel myTemplateIconTitle;
   private JLabel myTemplateIconDetail;
-  private FormFactorApiComboBox myMinSdkCombobox;
-  private JBLabel myApiPercentIcon;
-  private JBLabel myApiPercentLabel;
-  private HyperlinkLabel myLearnMoreLink;
-  private JPanel myStatsPanel;
-  private JPanel myLoadingDataPanel;
-  private AsyncProcessIcon myLoadingDataIcon;
-  private JLabel myLoadingDataLabel;
-  private LoadStatus mySdkDataLoadingStatus;
-  private LoadStatus myStatsDataLoadingStatus;
+  private JPanel myFormFactorSdkControlsPanel;
+  private FormFactorSdkControls myFormFactorSdkControls;
 
   public ConfigureAndroidProjectStep(@NotNull NewProjectModuleModel newProjectModuleModel, @NotNull NewProjectModel projectModel) {
     super(newProjectModuleModel, message("android.wizard.project.new.configure"));
@@ -157,10 +144,7 @@ public class ConfigureAndroidProjectStep extends ModelWizardStep<NewProjectModul
     myListeners.receive(locationText, value -> isLocationSynced.set(value.equals(computedLocation.get())));
 
     OptionalProperty<AndroidVersionsInfo.VersionItem> androidSdkInfo = getModel().androidSdkInfo();
-    myBindings.bind(androidSdkInfo, new SelectedItemProperty<>(myMinSdkCombobox));
-    myListeners.receive(androidSdkInfo, value ->
-      value.ifPresent(item -> myApiPercentLabel.setText(getApiHelpText(item.getMinApiLevel())))
-    );
+    myFormFactorSdkControls.init(androidSdkInfo, this);
 
     myBindings.bindTwoWay(new SelectedProperty(myInstantAppCheck), getModel().instantApp());
 
@@ -185,12 +169,6 @@ public class ConfigureAndroidProjectStep extends ModelWizardStep<NewProjectModul
 
     myProjectLocation.addBrowseFolderListener(null, null, null, createSingleFolderDescriptor());
 
-    myLoadingDataLabel.setForeground(JBColor.GRAY);
-    myApiPercentIcon.setIcon(AllIcons.General.BalloonInformation);
-
-    myLearnMoreLink.setHyperlinkText(message("android.wizard.module.help.choose"));
-    myLearnMoreLink.setHyperlinkTarget("https://developer.android.com/about/dashboards/index.html");
-
     myProjectLanguage.setSelectedItem(myProjectModel.enableKotlinSupport().get() ? Language.KOTLIN : Language.JAVA);
 
     myListeners.listenAll(getModel().formFactor(), myProjectModel.enableCppSupport()).withAndFire(() -> {
@@ -199,7 +177,7 @@ public class ConfigureAndroidProjectStep extends ModelWizardStep<NewProjectModul
 
       myNavigationControllerCheck.setVisible(formFactor == FormFactor.MOBILE && !isCppTemplate);
       myInstantAppCheck.setVisible(formFactor == FormFactor.MOBILE && !isCppTemplate);
-      myStatsPanel.setVisible(formFactor == FormFactor.MOBILE);
+      myFormFactorSdkControls.showStatsPanel(formFactor == FormFactor.MOBILE);
       myWearCheck.setVisible(formFactor == FormFactor.WEAR);
       myTvCheck.setVisible(formFactor == FormFactor.TV);
     });
@@ -211,8 +189,7 @@ public class ConfigureAndroidProjectStep extends ModelWizardStep<NewProjectModul
     TemplateHandle templateHandle = getModel().renderTemplateHandle().getValueOrNull();
     int minSdk = templateHandle == null ? formFactor.getMinOfflineApiLevel() : templateHandle.getMetadata().getMinSdk();
 
-    myApiPercentLabel.setText(getApiHelpText(minSdk));
-    startDataLoading(formFactor, minSdk);
+    myFormFactorSdkControls.startDataLoading(formFactor, minSdk);
     setTemplateThumbnail(templateHandle);
   }
 
@@ -225,8 +202,7 @@ public class ConfigureAndroidProjectStep extends ModelWizardStep<NewProjectModul
     myInstallRequests.clear();
     myInstallLicenseRequests.clear();
 
-    AndroidVersionsInfo.VersionItem androidVersion = getModel().androidSdkInfo().getValue();
-    myInstallRequests.addAll(myAndroidVersionsInfo.loadInstallPackageList(Collections.singletonList(androidVersion)));
+    myInstallRequests.addAll(myFormFactorSdkControls.getSdkInstallPackageList());
     myInstallLicenseRequests.addAll(myInstallRequests.stream().map(UpdatablePackage::getRemote).collect(Collectors.toList()));
   }
 
@@ -269,53 +245,6 @@ public class ConfigureAndroidProjectStep extends ModelWizardStep<NewProjectModul
     return projectDirectory.getPath();
   }
 
-  private void startDataLoading(FormFactor formFactor, int minSdk) {
-    mySdkDataLoadingStatus = LoadStatus.LOADING;
-    myStatsDataLoadingStatus = myStatsPanel.isVisible() ? LoadStatus.LOADING : LoadStatus.LOADED;
-    updateLoadingProgress();
-
-    myAndroidVersionsInfo.load();
-    myAndroidVersionsInfo.loadTargetVersions(formFactor, minSdk, items -> {
-      myMinSdkCombobox.init(formFactor, items);
-      mySdkDataLoadingStatus = LoadStatus.LOADED;
-      updateLoadingProgress();
-    });
-
-    if (myStatsPanel.isVisible()) {
-      DistributionService.getInstance().refresh(
-        () -> ApplicationManager.getApplication().invokeLater(() -> {
-          myStatsDataLoadingStatus = LoadStatus.LOADED;
-          updateLoadingProgress();
-        }),
-        () -> ApplicationManager.getApplication().invokeLater(() -> {
-          myStatsDataLoadingStatus = LoadStatus.FAILED;
-          updateLoadingProgress();
-        }));
-    }
-  }
-
-  private void updateLoadingProgress() {
-    myLoadingDataPanel.setVisible(mySdkDataLoadingStatus != LoadStatus.LOADED || myStatsDataLoadingStatus != LoadStatus.LOADED);
-    myLoadingDataIcon.setVisible(mySdkDataLoadingStatus == LoadStatus.LOADING || myStatsDataLoadingStatus == LoadStatus.LOADING);
-
-    if (mySdkDataLoadingStatus == LoadStatus.LOADING) {
-      myLoadingDataLabel.setText(message("android.wizard.project.loading.sdks"));
-    }
-    else if (myStatsDataLoadingStatus == LoadStatus.LOADING) {
-      myLoadingDataLabel.setText(message("android.wizard.module.help.refreshing"));
-    }
-    else if (myStatsDataLoadingStatus == LoadStatus.FAILED) {
-      myLoadingDataLabel.setText(message("android.wizard.project.loading.stats.fail"));
-    }
-  }
-
-  private static String getApiHelpText(int selectedApi) {
-    double percentage = DistributionService.getInstance().getSupportedDistributionForApiLevel(selectedApi) * 100;
-    String percentageStr = percentage < 1 ? "<b>&lt; 1%</b>" :
-                           format("approximately <b>" + (percentage >= 10 ? "%.3g%%" : "%.2g%%") + "</b>", percentage);
-    return format("<html>Your app will run on %1$s of devices.</html>", percentageStr);
-  }
-
   private void setTemplateThumbnail(@Nullable TemplateHandle templateHandle) {
     boolean isCppTemplate = myProjectModel.enableCppSupport().get();
     Image image = ActivityGallery.getTemplateImage(templateHandle, isCppTemplate);
@@ -340,6 +269,7 @@ public class ConfigureAndroidProjectStep extends ModelWizardStep<NewProjectModul
 
   private void createUIComponents() {
     myProjectLanguage = new LanguageComboProvider().createComponent();
-    myLoadingDataIcon = new AsyncProcessIcon(message("android.wizard.module.help.loading"));
+    myFormFactorSdkControls = new FormFactorSdkControls();
+    myFormFactorSdkControlsPanel = myFormFactorSdkControls.getRoot();
   }
 }
