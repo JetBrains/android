2babe59704c0d891ce2aeeb93a814068f9bec2fa
Juan C Nuno <juancnuno@google.com>
2018-03-30 18:45:43 -0700

    Delete DelegateDeleteHandler
    Cleaning house to understand the code better and to fix a bug in here. Turns
    out that half the code was a noop in this context.
    
    Bug: Not applicable
    Test: TranslationsEditorTest
    Change-Id: I530a2024c4c4a5dcdc15cd67738f02f6c085b54c

--- android/src/com/android/tools/idea/editors/strings/RemoveKeysAction.java
+++ android/src/com/android/tools/idea/editors/strings/RemoveKeysAction.java
@@ -19,10 +19,16 @@ import com.android.tools.idea.editors.strings.table.StringResourceTable;
 import com.android.tools.idea.editors.strings.table.StringResourceTableModel;
 import com.android.tools.idea.res.LocalResourceRepository;
 import com.intellij.icons.AllIcons;
+import com.intellij.ide.util.DeleteHandler;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.project.DumbService;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiElement;
+import com.intellij.refactoring.RefactoringBundle;
+import com.intellij.refactoring.safeDelete.SafeDeleteDialog;
+import com.intellij.refactoring.safeDelete.SafeDeleteProcessor;
+import com.intellij.refactoring.util.CommonRefactoringUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -48,12 +54,58 @@ final class RemoveKeysAction extends AnAction {
     StringResourceRepository repository = model.getRepository();
     Project project = myPanel.getFacet().getModule().getProject();
 
-    PsiElement[] elements = Arrays.stream(table.getSelectedRowModelIndices())
+    PsiElement[] keys = Arrays.stream(table.getSelectedRowModelIndices())
       .mapToObj(index -> model.getStringResourceAt(index).getKey())
       .flatMap(key -> repository.getItems(key).stream())
       .map(item -> LocalResourceRepository.getItemTag(project, item))
       .toArray(PsiElement[]::new);
 
-    DelegateDeleteHandler.deletePsiElement(project, elements, myPanel.getFacet().getModule());
+    if (keys.length == 0) {
+      return;
+    }
+
+    if (!CommonRefactoringUtil.checkReadOnlyStatusRecursively(project, Arrays.asList(keys), true)) {
+      return;
+    }
+
+    if (DumbService.getInstance(project).isDumb()) {
+      DeleteHandler.deletePsiElement(keys, project);
+      return;
+    }
+
+    SafeDeleteDialogCallback callback = new SafeDeleteDialogCallback();
+    SafeDeleteDialog dialog = new MySafeDeleteDialog(project, keys, callback);
+
+    if (!dialog.showAndGet()) {
+      return;
+    }
+
+    if (callback.mySafeDeleteCheckBoxSelected) {
+      SafeDeleteProcessor.createInstance(project, null, keys, dialog.isSearchInComments(), dialog.isSearchForTextOccurences(), true).run();
+    }
+    else {
+      DeleteHandler.deletePsiElement(keys, project, false);
+    }
+  }
+
+  private static final class MySafeDeleteDialog extends SafeDeleteDialog {
+    private MySafeDeleteDialog(@NotNull Project project, @NotNull PsiElement[] keys, @NotNull SafeDeleteDialog.Callback callback) {
+      super(project, keys, callback);
+      setTitle(RefactoringBundle.message("delete.title"));
+    }
+
+    @Override
+    protected boolean isDelete() {
+      return true;
+    }
+  }
+
+  private static final class SafeDeleteDialogCallback implements SafeDeleteDialog.Callback {
+    private boolean mySafeDeleteCheckBoxSelected;
+
+    @Override
+    public void run(@NotNull SafeDeleteDialog dialog) {
+      mySafeDeleteCheckBoxSelected = true;
+    }
   }
 }
