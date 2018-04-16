b4769bdd76044f76c89b8ce24abfa66062bff911
Chris Sinco <csinco@google.com>
2018-04-16 10:14:35 -0700

    Retry - Updating module templates references to point to StudioIcons
    Test: n/a - Current UI tests should pass
    Change-Id: I94222d611e8154f5361c7ac6937f643eabe2aea3
    Test-Strategy: ui.test.groups=PROJECT_WIZARD
    Bug: n/a - Just moving code location

--- android/src/com/android/tools/idea/npw/java/NewJavaModuleDescriptionProvider.java
+++ android/src/com/android/tools/idea/npw/java/NewJavaModuleDescriptionProvider.java
@@ -22,7 +22,7 @@ import com.android.tools.idea.npw.template.TemplateHandle;
 import com.android.tools.idea.templates.Template;
 import com.android.tools.idea.templates.TemplateManager;
 import com.android.tools.idea.wizard.model.SkippableWizardStep;
-import icons.AndroidIcons;
+import icons.StudioIllustrations;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -46,7 +46,7 @@ public class NewJavaModuleDescriptionProvider implements ModuleDescriptionProvid
     @Nullable
     @Override
     public Icon getIcon() {
-      return AndroidIcons.ModuleTemplates.Android;
+      return StudioIllustrations.ModuleTemplates.ANDROID_MODULE;
     }
 
     @NotNull
