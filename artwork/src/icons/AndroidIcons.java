1c33a486b4252749a948ccd63c248b4b74d70cac
Sergey Malenkov <sergey.malenkov@jetbrains.com>
2018-04-19 13:02:24 +0300

    remove automatically added copyright

--- artwork/src/icons/AndroidIcons.java
+++ artwork/src/icons/AndroidIcons.java
@@ -1,10 +1,8 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package icons;
 
-import com.intellij.icons.AllIcons;
 import com.intellij.openapi.util.IconLoader;
 
-import javax.swing.*;
+import javax.swing.Icon;
 
 /**
  * Note: This file should be auto generated once build/scripts/icons.gant is part of CE.
