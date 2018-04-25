12321d968718a6990269f7fd981408efdf4e9992
Jens Ole Lauridsen <jlauridsen@google.com>
2018-04-25 11:59:36 -0700

    Add new version of PTable.
    The existing version of PTable was deeply connected to the old
    property panel. In order to limit the risk of impacting the old
    property panel while the new panel is being developed, I chose
    to use a separate version of PTable. Eventually the old PTable
    version can be removed.
    
    Bug: 73778668
    Test: NA
    Change-Id: I7c3f9af1de9f9010760f872b07d98daa4c83306f

---
+++ adt-ui/src/main/java/com/android/tools/adtui/ptable2/impl/PTableModelImpl.kt
@@ -0,0 +1,90 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.adtui.ptable2.impl
+
+import com.android.tools.adtui.ptable2.PTableColumn
+import com.android.tools.adtui.ptable2.PTableGroupItem
+import com.android.tools.adtui.ptable2.PTableItem
+import com.android.tools.adtui.ptable2.PTableModel
+import javax.swing.table.AbstractTableModel
+
+class PTableModelImpl(val tableModel: PTableModel) : AbstractTableModel() {
+  val items = mutableListOf<PTableItem>()
+
+  init {
+    items.addAll(tableModel.items)
+  }
+
+  override fun getRowCount() = items.count()
+
+  override fun getColumnCount() = 2
+
+  override fun getValueAt(rowIndex: Int, columnIndex: Int): PTableItem = items[rowIndex]
+
+  override fun isCellEditable(rowIndex: Int, columnIndex: Int): Boolean {
+    return tableModel.isCellEditable(items[rowIndex], PTableColumn.fromColumn(columnIndex))
+  }
+
+  fun isGroupItem(item: PTableItem): Boolean {
+    return item is PTableGroupItem
+  }
+
+  fun toggle(index: Int) {
+    val item = groupAt(index) ?: return
+    if (item.expanded) {
+      collapse(item, index)
+    }
+    else {
+      expand(item, index)
+    }
+  }
+
+  fun expand(index: Int) {
+    val item = groupAt(index) ?: return
+    expand(item, index)
+  }
+
+  fun collapse(index: Int) {
+    val item = groupAt(index) ?: return
+    collapse(item, index)
+  }
+
+  private fun groupAt(index: Int): PTableGroupItem? {
+    if (index < 0 || index >= items.size) {
+      return null
+    }
+
+    return items[index] as? PTableGroupItem
+  }
+
+  private fun expand(item: PTableGroupItem, index: Int) {
+    if (item.expanded) {
+      return
+    }
+    item.expanded = true
+    items.addAll(index + 1, item.children)
+    fireTableDataChanged()
+  }
+
+  private fun collapse(item: PTableGroupItem, row: Int) {
+    if (!item.expanded) {
+      return
+    }
+    item.expanded = false
+    items.subList(row + 1, row + 1 + item.children.size).clear()
+    fireTableDataChanged()
+  }
+}
