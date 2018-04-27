d2f234718b0e4db304963182a1bad8038c48be32
Diego Perez <diegoperez@google.com>
2018-04-27 18:43:05 +0100

    Warn if gradle is not >= 3.2 or compileSdk >= 28
    Test: N/A
    Change-Id: I4805bd6b4a1674a4bdb237d5a4586ec6d9deec20
    Fixes: 78762408

--- android/src/org/jetbrains/android/refactoring/MigrateToAndroidxAction.kt
+++ android/src/org/jetbrains/android/refactoring/MigrateToAndroidxAction.kt
@@ -16,18 +16,20 @@
 package org.jetbrains.android.refactoring
 
 import com.android.annotations.VisibleForTesting
+import com.android.ide.common.repository.GradleVersion
 import com.android.sdklib.AndroidVersion
 import com.android.support.MigrationParserVisitor
 import com.android.support.parseMigrationFile
 import com.android.tools.idea.flags.StudioFlags
 import com.android.tools.idea.gradle.dsl.api.GradleBuildModel
+import com.android.tools.idea.gradle.dsl.api.ext.GradlePropertyModel
 import com.intellij.lang.Language
 import com.intellij.openapi.actionSystem.AnActionEvent
-import com.intellij.openapi.actionSystem.CommonDataKeys
 import com.intellij.openapi.actionSystem.DataContext
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.module.ModuleManager
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.ui.popup.JBPopupFactory
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
 import com.intellij.refactoring.RefactoringActionHandler
@@ -45,11 +47,34 @@ class MigrateToAndroidxAction : BaseRefactoringAction() {
   override fun getHandler(dataContext: DataContext): RefactoringActionHandler? = MigrateToAndroidxHandler()
 
   override fun update(anActionEvent: AnActionEvent) {
-    val project = anActionEvent.getData(CommonDataKeys.PROJECT)
+    anActionEvent.presentation.isEnabledAndVisible = StudioFlags.MIGRATE_TO_ANDROID_X_REFACTORING_ENABLED.get()
+  }
 
-    if (project != null && StudioFlags.MIGRATE_TO_ANDROID_X_REFACTORING_ENABLED.get()) {
+  override fun isAvailableForLanguage(language: Language) = true
+}
+
+class MigrateToAndroidxHandler : RefactoringActionHandler {
+
+  override fun invoke(project: Project, editor: Editor?, file: PsiFile?, dataContext: DataContext?) {
+    invoke(project, arrayOf<PsiElement>(file!!), dataContext)
+  }
+
+  override fun invoke(project: Project, elements: Array<out PsiElement>, dataContext: DataContext?) {
+    if (!checkRefactoringPrerequisites(project)) {
+      return
+    }
+
+    val processor = MigrateToAndroidxProcessor(project, parseMigrationMap())
+
+    with(processor) {
+      setPreviewUsages(true)
+      run()
+    }
+  }
+
+  private fun checkRefactoringPrerequisites(project: Project): Boolean {
       @Suppress("UselessCallOnCollection")
-      val highestVersion = ModuleManager.getInstance(project).modules
+      val highestCompileSdkVersion = ModuleManager.getInstance(project).modules
         .mapNotNull { GradleBuildModel.get(it)?.android() }
         .mapNotNull { it.compileSdkVersion().toString() }
         .map { it.removePrefix("android-") }
@@ -63,34 +88,44 @@ class MigrateToAndroidxAction : BaseRefactoringAction() {
         }
         .sorted()
         .lastOrNull()
-
-      anActionEvent.presentation.isEnabled = if (highestVersion != null) {
-        AndroidVersion(28) <= highestVersion || highestVersion.codename != null
-      } else {
+      val supportedCompileSdk = if (highestCompileSdkVersion != null) {
+        AndroidVersion(28) <= highestCompileSdkVersion || highestCompileSdkVersion.codename != null
+      }
+      else {
         true // Enable by default when we can not find out the version
       }
+
+      val gradleVersionString = ModuleManager.getInstance(project).modules
+        .mapNotNull { GradleBuildModel.get(it)?.buildscript()?.dependencies() }
+        .flatMap { it.artifacts() }
+        .filter { it.name().forceString() == "gradle" && it.group().forceString() == "com.android.tools.build" }
+        .map { it.version().getValue(GradlePropertyModel.STRING_TYPE) }
+        .firstOrNull()
+      val supportedGradleVersion = if (gradleVersionString?.startsWith('$') == false) {
+        GradleVersion.tryParse(gradleVersionString)?.isAtLeastIncludingPreviews(3, 2, 0) ?: false
+      }
+      else {
+        // For now, ignore this case since the DSL parser does not seem to handle that correctly
+        true
+      }
+
+    if (supportedCompileSdk && supportedGradleVersion) {
+      return true
     }
-    else {
-      anActionEvent.presentation.isEnabledAndVisible = false
+
+    val warningContent = if (!supportedCompileSdk) {
+      "You need to have at least have compileSdk 28 set in your module build.gradle to refactor to androidx\n"
+    } else {
+      ""
+    } + if (!supportedGradleVersion) {
+      "The gradle plugin version in your project build.gradle file needs to be set to at least com.android.tools.build:gradle:3.2.0 " +
+      "in order to refactor to androidx"
+    } else {
+      ""
     }
-  }
 
-  override fun isAvailableForLanguage(language: Language) = true
-}
-
-class MigrateToAndroidxHandler : RefactoringActionHandler {
-
-  override fun invoke(project: Project, editor: Editor?, file: PsiFile?, dataContext: DataContext?) {
-    invoke(project, arrayOf<PsiElement>(file!!), dataContext)
-  }
-
-  override fun invoke(project: Project, elements: Array<out PsiElement>, dataContext: DataContext?) {
-    val processor = MigrateToAndroidxProcessor(project, parseMigrationMap())
-
-    with(processor) {
-      setPreviewUsages(true)
-      run()
-    }
+    JBPopupFactory.getInstance().createMessage(warningContent).showInFocusCenter()
+    return false
   }
 
   @VisibleForTesting
