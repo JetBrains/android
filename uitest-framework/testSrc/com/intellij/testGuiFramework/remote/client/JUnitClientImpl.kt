70138b7b8dcc2326f55e9638e40afff896f9971a
Nathan Paige <npaige@google.com>
2018-05-08 14:47:39 -0700

    Reduce log noise from UI test framework
    Note that client-side log messages will only show up on server-side
    standard output at WARN level or higher, but will be logged to idea.log
    at INFO level and higher.
    
    Change-Id: I37a49b055372c8816be4b8ed17d0a3622006b825

--- uitest-framework/testSrc/com/intellij/testGuiFramework/remote/client/JUnitClientImpl.kt
+++ uitest-framework/testSrc/com/intellij/testGuiFramework/remote/client/JUnitClientImpl.kt
@@ -98,11 +98,10 @@ class JUnitClientImpl(val host: String, val port: Int, initHandlers: Array<Clien
 
   inner class ClientReceiveThread(val connection: Socket, val objectInputStream: ObjectInputStream) : Thread(RECEIVE_THREAD) {
     override fun run() {
-      LOG.warn("Starting Client Receive Thread")
       try{
         while (!connection.isClosed) {
           val message = objectInputStream.readObject() as MessageFromServer
-          LOG.warn("Received message: $message")
+          LOG.info("Received message: $message")
           handlers
             .filter { it.accept(message) }
             .forEach { it.handle(message) }
@@ -119,10 +118,9 @@ class JUnitClientImpl(val host: String, val port: Int, initHandlers: Array<Clien
   inner class ClientSendThread(val connection: Socket, val objectOutputStream: ObjectOutputStream) : Thread(SEND_THREAD) {
     override fun run() {
       try {
-        LOG.warn("Starting Client Send Thread")
         while (!connection.isClosed) {
           val transportMessage = poolOfMessages.take()
-          LOG.warn("Sending message: $transportMessage")
+          LOG.info("Sending message: $transportMessage")
           objectOutputStream.writeObject(transportMessage)
         }
       }
