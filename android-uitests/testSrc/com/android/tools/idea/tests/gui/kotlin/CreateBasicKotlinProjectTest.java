768c08609db6d606c5837a46b6f0afa88077cecd
Alexander Wang <afwang@google.com>
2018-05-09 09:20:54 -0700

    Remove emulator from CreateBasicKotlinProjectTest
    This test doesn't need to use the emulator.
    
    Bug: 79435596
    Test: CreateBasicKotlinProjectTest
    Change-Id: I49bb05e8936c2e0abb10e462e93902d238fc7832

--- android-uitests/testSrc/com/android/tools/idea/tests/gui/kotlin/CreateBasicKotlinProjectTest.java
+++ android-uitests/testSrc/com/android/tools/idea/tests/gui/kotlin/CreateBasicKotlinProjectTest.java
@@ -15,20 +15,14 @@
  */
 package com.android.tools.idea.tests.gui.kotlin;
 
-import com.android.tools.idea.tests.gui.emulator.EmulatorGenerator;
-import com.android.tools.idea.tests.gui.emulator.EmulatorTestRule;
 import com.android.tools.idea.tests.gui.framework.GuiTestRule;
-import com.android.tools.idea.tests.gui.framework.GuiTestRunner;
 import com.android.tools.idea.tests.gui.framework.RunIn;
 import com.android.tools.idea.tests.gui.framework.TestGroup;
 import com.android.tools.idea.tests.gui.framework.fixture.IdeFrameFixture;
 import com.intellij.testGuiFramework.framework.GuiTestRemoteRunner;
-import org.fest.swing.util.PatternTextMatcher;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import java.util.regex.Pattern;
 
 import static com.android.tools.idea.tests.gui.kotlin.ProjectWithKotlinTestUtil.createNewBasicKotlinProject;
 import static com.google.common.truth.Truth.assertThat;
@@ -37,12 +31,8 @@ import static com.google.common.truth.Truth.assertThat;
 public class CreateBasicKotlinProjectTest {
 
   @Rule public final GuiTestRule guiTest = new GuiTestRule();
-  @Rule public final EmulatorTestRule emulator = new EmulatorTestRule(false);
 
-  private static final String APP_NAME = "app";
   private static final String KOTLIN_FILE = "MainActivity.kt";
-  private static final Pattern CONNECTED_TO_PROCESS_OUTPUT = Pattern.compile(
-    ".*Connected to process.*", Pattern.DOTALL);
 
   /**
    * Verifies that can creating a new project with Kotlin.
@@ -56,28 +46,19 @@ public class CreateBasicKotlinProjectTest {
    *   1. Create a basic Kotlin project following the default steps.
    *   2. Select the "include Kotlin" support checkbox [verify 1 & 2].
    *   3. Build and run project, and verify 3.
-   *   Vefify:
+   *   Verify:
    *   1. Check if build is successful.
    *   2. Mainactivity should have .kt as extension and ensure the class has Kotlin code.
-   *   3. Ensure the app is deployed on the emulator.
    *   </pre>
    */
   @RunIn(TestGroup.SANITY)
   @Test
-  public void createBasicKotlinProject() throws Exception {
+  public void createBasicKotlinProject() {
     createNewBasicKotlinProject(false, guiTest);
 
     IdeFrameFixture ideFrameFixture = guiTest.ideFrame();
     assertThat(KOTLIN_FILE).isEqualTo(ideFrameFixture.getEditor().getCurrentFileName());
 
-    String avdName = EmulatorGenerator.ensureDefaultAvdIsCreated(ideFrameFixture.invokeAvdManager());
-
-    ideFrameFixture.runApp(APP_NAME)
-                   .selectDevice(avdName)
-                   .clickOk();
-
-    ideFrameFixture.getRunToolWindow().findContent(APP_NAME)
-                   .waitForOutput(new PatternTextMatcher(CONNECTED_TO_PROCESS_OUTPUT), emulator.DEFAULT_EMULATOR_WAIT_SECONDS);
-    ideFrameFixture.stopApp();
+    assertThat(ideFrameFixture.invokeProjectMake().isBuildSuccessful()).isTrue();
   }
 }
