58243415691ab59a3e9356f89cc58fd0fdd438d8
Alexander Wang <afwang@google.com>
2018-05-10 15:13:52 -0700

    Use fake ADB server for RunOnEmulatorTest
    This UI test actually does not need to test anything involving the
    emulator, so it should use the fake ADB server. This decreases flakiness
    of the test while also decreasing the amount of time the test needs to
    run.
    
    Bug: 79541849, 77805073
    Test: RunOnEmulatorTest
    Change-Id: Id93ee7bae7fb2f6400cec09b8823bdc0bb930571

--- android-uitests/testSrc/com/android/tools/idea/tests/gui/emulator/RunOnEmulatorTest.java
+++ android-uitests/testSrc/com/android/tools/idea/tests/gui/emulator/RunOnEmulatorTest.java
@@ -15,19 +15,27 @@
  */
 package com.android.tools.idea.tests.gui.emulator;
 
+import com.android.ddmlib.AndroidDebugBridge;
+import com.android.fakeadbserver.DeviceState;
+import com.android.fakeadbserver.FakeAdbServer;
+import com.android.fakeadbserver.devicecommandhandlers.JdwpCommandHandler;
+import com.android.fakeadbserver.shellcommandhandlers.ActivityManagerCommandHandler;
 import com.android.tools.idea.fd.InstantRunSettings;
 import com.android.tools.idea.tests.gui.framework.GuiTestRule;
-import com.android.tools.idea.tests.gui.framework.GuiTestRunner;
 import com.android.tools.idea.tests.gui.framework.RunIn;
 import com.android.tools.idea.tests.gui.framework.TestGroup;
 import com.android.tools.idea.tests.gui.framework.fixture.IdeFrameFixture;
 import com.intellij.testGuiFramework.framework.GuiTestRemoteRunner;
 import org.fest.swing.util.PatternTextMatcher;
+import org.jetbrains.annotations.NotNull;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.RuleChain;
 import org.junit.runner.RunWith;
 
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.regex.Pattern;
 
@@ -36,16 +44,53 @@ public class RunOnEmulatorTest {
 
   @Rule public final GuiTestRule guiTest = new GuiTestRule().withTimeout(7, TimeUnit.MINUTES);
 
-  private final EmulatorTestRule emulator = new EmulatorTestRule(false);
-  @Rule public final RuleChain emulatorRules = RuleChain
-    .outerRule(new DeleteAvdsRule())
-    .around(emulator);
+  private FakeAdbServer fakeAdbServer;
 
   private static final String APP_NAME = "app";
   private static final String PROCESS_NAME = "google.simpleapplication";
   private static final Pattern LOCAL_PATH_OUTPUT = Pattern.compile(
     ".*adb shell am start .*google\\.simpleapplication.*", Pattern.DOTALL);
   private static final Pattern RUN_OUTPUT = Pattern.compile(".*Connected to process.*", Pattern.DOTALL);
+
+
+  @Before
+  public void setupFakeAdbServer() throws IOException, InterruptedException, ExecutionException {
+    ActivityManagerCommandHandler.ProcessStarter procStarter = new ActivityManagerCommandHandler.ProcessStarter() {
+      @NotNull
+      @Override
+      public String startProcess(@NotNull DeviceState deviceState) {
+        deviceState.startClient(1234, 1235, "google.simpleapplication", false);
+
+        return "Starting: Intent { act=android.intent.action.VIEW cat=[android.intent.category.LAUNCHER] }";
+      }
+    };
+
+    FakeAdbServer.Builder fakeAdbBuilder = new FakeAdbServer.Builder();
+    fakeAdbBuilder.installDefaultCommandHandlers()
+      .setShellCommandHandler(
+        ActivityManagerCommandHandler.COMMAND,
+        () -> new ActivityManagerCommandHandler(procStarter)
+      )
+      .setDeviceCommandHandler(
+        JdwpCommandHandler.COMMAND,
+        JdwpCommandHandler::new
+      );
+
+    fakeAdbServer = fakeAdbBuilder.build();
+
+    DeviceState fakeDevice = fakeAdbServer.connectDevice(
+      "test_device",
+      "Google",
+      "Nexus 5X",
+      "8.1",
+      "27",
+      DeviceState.HostConnectionType.LOCAL
+    ).get();
+    fakeDevice.setDeviceStatus(DeviceState.DeviceStatus.ONLINE);
+    fakeAdbServer.start();
+
+    AndroidDebugBridge.enableFakeAdbServerMode(fakeAdbServer.getPort());
+  }
 
   /**
    * Verifies that a project can be deployed on an emulator
@@ -72,11 +117,9 @@ public class RunOnEmulatorTest {
 
     IdeFrameFixture ideFrameFixture = guiTest.ideFrame();
 
-    String avdName = EmulatorGenerator.ensureDefaultAvdIsCreated(ideFrameFixture.invokeAvdManager());
-
     ideFrameFixture
       .runApp(APP_NAME)
-      .selectDevice(avdName)
+      .selectDevice("Google Nexus 5X")
       .clickOk();
 
     // Wait for background tasks to finish before requesting Run Tool Window. Otherwise Run Tool Window won't activate.
@@ -88,5 +131,12 @@ public class RunOnEmulatorTest {
 
     ideFrameFixture.getAndroidToolWindow().selectDevicesTab().selectProcess(PROCESS_NAME);
     ideFrameFixture.stopApp();
+  }
+
+  @After
+  public void shutdownFakeAdb() throws Exception {
+    AndroidDebugBridge.terminate();
+    AndroidDebugBridge.disableFakeAdbServerMode();
+    fakeAdbServer.close();
   }
 }
