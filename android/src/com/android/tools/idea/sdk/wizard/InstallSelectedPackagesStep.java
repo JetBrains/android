6e8c4de62baadd237bec440bf9154ef1060b4abf
Andrii Dehtiarov <adehtiarov@google.com>
2018-05-16 22:59:25 +0100

    Add test for redundant download optimization.
    This CL also prevents the background SDK installation from
    being executed in unit test mode (that would be most likely
    inadvertent and useless anyway, plus could introduce unforeseen
    race conditions).
    
    Test: This is a test.
    Bug: N/A
    Change-Id: I40904ee03b1b0dbd2014430c6f15e783d661022b

--- android/src/com/android/tools/idea/sdk/wizard/InstallSelectedPackagesStep.java
+++ android/src/com/android/tools/idea/sdk/wizard/InstallSelectedPackagesStep.java
@@ -37,6 +37,7 @@ import com.android.tools.idea.wizard.WizardConstants;
 import com.android.tools.idea.wizard.model.ModelWizard;
 import com.android.tools.idea.wizard.model.ModelWizardStep;
 import com.intellij.ide.util.PropertiesComponent;
+import com.intellij.openapi.application.Application;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.application.ModalityState;
 import com.intellij.openapi.diagnostic.Logger;
