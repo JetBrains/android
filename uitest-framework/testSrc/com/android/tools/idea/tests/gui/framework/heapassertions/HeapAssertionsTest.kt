6d1ebe6049830c52b936457eefbbb93f66684880
Raluca Sauciuc <ralucas@google.com>
2018-05-16 15:57:06 -0700

    Heap assertions: initial commit
    This CL adds two types of assertions based on LeakHunter's
    object traversal:
    - a check that no disposed objects are still reachable
    - a check that no weak/soft hash map leaks entries by having the
    strong reference pointing back to the weak reference.
    
    These checks are meant for stable states during UI tests, i.e. after
    flushing the AWT queue, because both invariants can temporarily be
    broken by valid code performing disposals etc.
    
    Also, Kotlin users have a nicer API to work with for new checks.
    
    Test: pending
    Bug: N/A
    Change-Id: If1376290f527038f7c3628ebb540a81dbe84472d

---
+++ uitest-framework/testSrc/com/android/tools/idea/tests/gui/framework/heapassertions/HeapAssertionsTest.kt
@@ -0,0 +1,85 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.tests.gui.framework.heapassertions
+
+import com.intellij.openapi.Disposable
+import com.intellij.openapi.util.Disposer
+import com.intellij.testFramework.PlatformTestCase
+import com.intellij.util.containers.ContainerUtil
+
+class HeapAssertionsTest : PlatformTestCase() {
+
+  override fun setUp() {
+    super.setUp()
+    extraRoots.clear()
+    extraRoots.add(this) // add object to GC roots explicitly so that companion object is reachable
+  }
+
+  fun test_reachableDisposed() {
+    instance = MyDisposable("one")
+    try {
+      checkReachableDisposed(true)
+    }
+    catch (e: HeapAssertionError) {
+      println(e)
+      fail("Nothing has been disposed yet")
+    }
+    Disposer.dispose(instance!!)
+    try {
+      checkReachableDisposed(true)
+      fail("Failed to detect disposed instance")
+    }
+    catch (e: HeapAssertionError) {
+      assert(e.message!!.contains(
+        "Found class com.android.tools.idea.tests.gui.framework.heapassertions.HeapAssertionsTest\$MyDisposable"))
+    }
+    finally {
+      instance = null
+    }
+  }
+
+  fun test_weakHashMap() {
+    val dummy = Any()
+    leakyMap = ContainerUtil.createWeakMap<Any, () -> Any>()
+    leakyMap!![dummy] = { dummy } // lambda leaks the key (the leaky listener pattern)
+
+    try {
+      checkWeakSoftRefMaps(true)
+      fail("Failed to detect leaky weak hash map")
+    }
+    catch (e: HeapAssertionError) {
+      assert(e.message!!.contains(
+        "reached via 'com.android.tools.idea.tests.gui.framework.heapassertions.HeapAssertionsTest\$test_weakHashMap\$1.\$dummy'"
+      ))
+    }
+    finally {
+      leakyMap = null
+    }
+  }
+
+  companion object { // the companion object will naturally be a GC root.
+    var instance: Disposable? = null
+    var leakyMap: MutableMap<Any, () -> Any>? = null
+  }
+
+  inner class MyDisposable(val name: String) : Disposable {
+    init {
+      Disposer.register(testRootDisposable, this)
+    }
+
+    override fun dispose() {}
+  }
+}
