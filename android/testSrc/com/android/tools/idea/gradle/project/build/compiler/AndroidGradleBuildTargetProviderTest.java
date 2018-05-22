5b7e4a9c3b3c0dbbe43d66b02c0a556703babd41
Vladislav.Soroka <Vladislav.Soroka@jetbrains.com>
2018-05-22 15:49:51 +0300

    merge-as: cleanup deprecated method usage

--- android/testSrc/com/android/tools/idea/gradle/project/build/compiler/AndroidGradleBuildTargetProviderTest.java
+++ android/testSrc/com/android/tools/idea/gradle/project/build/compiler/AndroidGradleBuildTargetProviderTest.java
@@ -42,7 +42,7 @@ public class AndroidGradleBuildTargetProviderTest extends AndroidGradleTestCase 
     CompileScope scope = new ProjectCompileScope(getProject());
 
     AndroidGradleBuildTargetScopeProvider provider = new AndroidGradleBuildTargetScopeProvider();
-    List<TargetTypeBuildScope> targetScopes = provider.getBuildTargetScopes(scope, (compiler) -> true, getProject(), true);
+    List<TargetTypeBuildScope> targetScopes = provider.getBuildTargetScopes(scope, getProject(), true);
     assertSize(1, targetScopes);
 
     TargetTypeBuildScope targetScope = targetScopes.get(0);
@@ -55,7 +55,7 @@ public class AndroidGradleBuildTargetProviderTest extends AndroidGradleTestCase 
     CompileScope scope = new ProjectCompileScope(getProject());
 
     AndroidGradleBuildTargetScopeProvider provider = new AndroidGradleBuildTargetScopeProvider();
-    List<TargetTypeBuildScope> targetScopes = provider.getBuildTargetScopes(scope, (compiler) -> true, getProject(), true);
+    List<TargetTypeBuildScope> targetScopes = provider.getBuildTargetScopes(scope, getProject(), true);
     assertSize(1, targetScopes);
 
     TargetTypeBuildScope targetScope = targetScopes.get(0);
@@ -78,7 +78,7 @@ public class AndroidGradleBuildTargetProviderTest extends AndroidGradleTestCase 
     scope.putUserData(Key.create("Test"), "RUN_CONFIGURATION");
 
     AndroidGradleBuildTargetScopeProvider provider = new AndroidGradleBuildTargetScopeProvider();
-    List<TargetTypeBuildScope> targetScopes = provider.getBuildTargetScopes(scope, (compiler) -> true, getProject(), true);
+    List<TargetTypeBuildScope> targetScopes = provider.getBuildTargetScopes(scope, getProject(), true);
     assertSize(1, targetScopes);
 
     TargetTypeBuildScope targetScope = targetScopes.get(0);
