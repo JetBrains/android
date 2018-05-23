5204155b735d4e5576119fddcef829aecdf285b6
Diego Perez <diegoperez@google.com>
2018-05-23 11:28:17 +0100

    Add ability to change listitem multiple times
    This CL also adds the changes needed to persist changes to the
    itemCount.
    If the use selects a new template for the RecyclerView and DOES NOT make
    changes to the template contents, the RecyclerViewAssistant will
    identify the template and pre-select it in the list of templates. This
    allows for a more consistent behaviour with the rest of the assistants.
    
    Bug: 79115580
    Test: Updated UI test
    Change-Id: Ib6c810b5d9aa8a4519a643cf9ba53cb6b06783a2

--- designer/src/com/android/tools/idea/uibuilder/handlers/assistant/Template.kt
+++ designer/src/com/android/tools/idea/uibuilder/handlers/assistant/Template.kt
@@ -18,12 +18,21 @@ package com.android.tools.idea.uibuilder.handlers.assistant
 import libcore.io.Streams
 import java.io.InputStream
 import java.io.InputStreamReader
+import java.security.MessageDigest
 
+private fun hash(content: String): ByteArray =
+  MessageDigest.getInstance("SHA-1").digest(content.toByteArray(Charsets.UTF_8))
 
 /**
  * Holder class for the templates information
  */
 internal data class Template(private val myTemplateName: String, val myTemplate: String, val isGrid: Boolean) {
+  private val hash: ByteArray by lazy { hash(myTemplate) }
+
+  fun hasSameContent(content: String?): Boolean {
+    return !content.isNullOrBlank() && content!!.length == myTemplate.length && hash.contentEquals(hash(content))
+  }
+
   override fun toString(): String {
     return myTemplateName
   }
@@ -38,7 +47,7 @@ internal data class Template(private val myTemplateName: String, val myTemplate:
     @JvmStatic
     @JvmOverloads
     fun fromStream(name: String, stream: InputStream, isGrid: Boolean = false): Template {
-      val content = Streams.readFully(InputStreamReader(stream))
+      val content = Streams.readFully(InputStreamReader(stream)).trim()
       return Template(name, content, isGrid)
     }
   }
