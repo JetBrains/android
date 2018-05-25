3746b44ed39afb4504dbf7e0739f1d3e6f15f7b9
Yuriy Solodkyy <solodkyy@google.com>
2018-05-25 14:40:52 +0100

    [New PSD] Declared dependencies from resolved.
    This CL makes matching declared dependencies accessible from resolved
    ones.
    
    Bug: n/a
    Test: existing unit tests
    Change-Id: I585554735eb21e3189037346c8c18bbde99fe5d2

--- android/src/com/android/tools/idea/gradle/structure/configurables/android/dependencies/treeview/AbstractDependencyNode.kt
+++ android/src/com/android/tools/idea/gradle/structure/configurables/android/dependencies/treeview/AbstractDependencyNode.kt
@@ -21,10 +21,7 @@ import com.android.tools.idea.gradle.structure.configurables.ui.treeview.Abstrac
 import com.android.tools.idea.gradle.structure.model.PsDeclaredDependency
 import com.android.tools.idea.gradle.structure.model.PsDependency
 import com.android.tools.idea.gradle.structure.model.PsResolvedDependency
-import com.android.tools.idea.gradle.structure.model.android.PsAndroidDependency
-import com.android.tools.idea.gradle.structure.model.android.PsAndroidDependencyCollection
-import com.android.tools.idea.gradle.structure.model.android.PsLibraryAndroidDependency
-import com.android.tools.idea.gradle.structure.model.android.PsModuleAndroidDependency
+import com.android.tools.idea.gradle.structure.model.android.*
 
 abstract class AbstractDependencyNode<T : PsAndroidDependency> : AbstractPsModelNode<T> {
 
@@ -35,7 +32,7 @@ abstract class AbstractDependencyNode<T : PsAndroidDependency> : AbstractPsModel
 
   companion object {
     fun createNode(parent: AbstractPsNode,
-                   collection: PsAndroidDependencyCollection?,
+                   collection: PsAndroidArtifactDependencyCollection?,
                    dependency: PsDependency): AbstractDependencyNode<*>? =
       when (dependency) {
         is PsLibraryAndroidDependency -> LibraryDependencyNode(parent, collection, dependency)
