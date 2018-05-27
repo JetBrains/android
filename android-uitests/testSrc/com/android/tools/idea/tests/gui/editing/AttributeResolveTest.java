e59328ff99156ce3f51fda5d516edc023202d2a3
Dana Dahlstrom <dahlstrom@google.com>
2018-05-27 14:00:00 -0700

    Remove test…NewlyAddedTag from TestGroup.UNRELIABLE
    Lately it's been highly reliable and hasn't shown the failure mode for
    which it was moved by Change I04375099.
    
    Issue: 73210921
    Change-Id: I4eea88346c122f22a1be1383e5c8ef14fba340b5

--- android-uitests/testSrc/com/android/tools/idea/tests/gui/editing/AttributeResolveTest.java
+++ android-uitests/testSrc/com/android/tools/idea/tests/gui/editing/AttributeResolveTest.java
@@ -40,7 +40,6 @@ public class AttributeResolveTest {
 
   @Rule public final GuiTestRule guiTest = new GuiTestRule();
 
-  @RunIn(TestGroup.UNRELIABLE)  // b/73210921
   @Test
   public void testResolveNewlyAddedTag() throws IOException {
     guiTest.importProjectAndWaitForProjectSyncToFinish("LayoutTest");
