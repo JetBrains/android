1e528b35b1acbb66c3f66d8798347676df71a534
Yuriy Solodkyy <solodkyy@google.com>
2018-05-31 18:35:46 +0100

    [New PSD] Make PsAndroidArtifactTest cleaner.
    Bug: n/a
    Test: n/a
    Change-Id: Iee69371fc304bc97a61243997f40cbbd1a2899d0

--- android/src/com/android/tools/idea/gradle/structure/model/android/PsAndroidArtifact.kt
+++ android/src/com/android/tools/idea/gradle/structure/model/android/PsAndroidArtifact.kt
@@ -22,7 +22,6 @@ import com.android.tools.idea.gradle.dsl.api.dependencies.DependencyModel
 import com.android.tools.idea.gradle.project.model.AndroidModuleModel
 import com.android.tools.idea.gradle.structure.model.PsChildModel
 import com.google.common.annotations.VisibleForTesting
-import com.google.common.collect.Lists
 import com.intellij.icons.AllIcons
 import com.intellij.icons.AllIcons.Modules.TestRoot
 import com.intellij.icons.AllIcons.Nodes.Artifact
@@ -74,64 +73,11 @@ class PsAndroidArtifact(override val parent: PsVariant, val resolvedName: String
     get() = myDependencies ?: PsAndroidArtifactDependencyCollection(this).also { myDependencies = it }
 
   val possibleConfigurationNames: List<String>
-    @VisibleForTesting
     get() {
-      val configurationNames = Lists.newArrayList<String>()
-      when (resolvedName) {
-        ARTIFACT_MAIN -> {
-          configurationNames.add(COMPILE)
-          configurationNames.add(API)
-          configurationNames.add(IMPLEMENTATION)
-        }
-        ARTIFACT_UNIT_TEST -> {
-          configurationNames.add(TEST_COMPILE)
-          configurationNames.add(TEST_API)
-          configurationNames.add(TEST_IMPLEMENTATION)
-        }
-        ARTIFACT_ANDROID_TEST -> {
-          configurationNames.add(ANDROID_TEST_COMPILE)
-          configurationNames.add(ANDROID_TEST_API)
-          configurationNames.add(ANDROID_TEST_IMPLEMENTATION)
-        }
-      }
-
       val variant = parent
-
       val buildTypeName = variant.buildType.name
-      when (resolvedName) {
-        ARTIFACT_MAIN -> {
-          configurationNames.add(buildTypeName + COMPILE_SUFFIX)
-          configurationNames.add(buildTypeName + API_SUFFIX)
-          configurationNames.add(buildTypeName + IMPLEMENTATION_SUFFIX)
-        }
-        ARTIFACT_UNIT_TEST -> {
-          configurationNames.add("test" + capitalize(buildTypeName) + COMPILE_SUFFIX)
-          configurationNames.add("test" + capitalize(buildTypeName) + API_SUFFIX)
-          configurationNames.add("test" + capitalize(buildTypeName) + IMPLEMENTATION_SUFFIX)
-        }
-      }
-
-      variant.forEachProductFlavor { productFlavor ->
-        val productFlavorName = productFlavor.name
-        when (resolvedName) {
-          ARTIFACT_MAIN -> {
-            configurationNames.add(productFlavorName + COMPILE_SUFFIX)
-            configurationNames.add(productFlavorName + API_SUFFIX)
-            configurationNames.add(productFlavorName + IMPLEMENTATION_SUFFIX)
-          }
-          ARTIFACT_UNIT_TEST -> {
-            configurationNames.add("test" + capitalize(productFlavorName) + COMPILE_SUFFIX)
-            configurationNames.add("test" + capitalize(productFlavorName) + API_SUFFIX)
-            configurationNames.add("test" + capitalize(productFlavorName) + IMPLEMENTATION_SUFFIX)
-          }
-          ARTIFACT_ANDROID_TEST -> {
-            configurationNames.add("androidTest" + capitalize(productFlavorName) + COMPILE_SUFFIX)
-            configurationNames.add("androidTest" + capitalize(productFlavorName) + API_SUFFIX)
-            configurationNames.add("androidTest" + capitalize(productFlavorName) + IMPLEMENTATION_SUFFIX)
-          }
-        }
-      }
-      return configurationNames
+      val productFlavorNames = variant.productFlavors
+      return getPossibleConfigurationNames(resolvedName, buildTypeName, productFlavorNames)
     }
 
   internal fun resetDependencies() {
@@ -146,13 +92,70 @@ class PsAndroidArtifact(override val parent: PsVariant, val resolvedName: String
   fun containsConfigurationName(configurationName: String): Boolean {
     return possibleConfigurationNames.contains(configurationName)
   }
-
-  companion object {
-    @NonNls
-    private val API_SUFFIX = "Api"
-    @NonNls
-    private val COMPILE_SUFFIX = "Compile"
-    @NonNls
-    private val IMPLEMENTATION_SUFFIX = "Implementation"
-  }
 }
+
+@NonNls
+private const val API_SUFFIX = "Api"
+@NonNls
+private const val COMPILE_SUFFIX = "Compile"
+@NonNls
+private const val IMPLEMENTATION_SUFFIX = "Implementation"
+
+@VisibleForTesting
+fun getPossibleConfigurationNames(resolvedName: String,
+                                  buildTypeName: String,
+                                  productFlavorNames: List<String>): List<String> {
+  val configurationNames = mutableListOf<String>()
+  when (resolvedName) {
+    ARTIFACT_MAIN -> {
+      configurationNames.add(COMPILE)
+      configurationNames.add(API)
+      configurationNames.add(IMPLEMENTATION)
+    }
+    ARTIFACT_UNIT_TEST -> {
+      configurationNames.add(TEST_COMPILE)
+      configurationNames.add(TEST_API)
+      configurationNames.add(TEST_IMPLEMENTATION)
+    }
+    ARTIFACT_ANDROID_TEST -> {
+      configurationNames.add(ANDROID_TEST_COMPILE)
+      configurationNames.add(ANDROID_TEST_API)
+      configurationNames.add(ANDROID_TEST_IMPLEMENTATION)
+    }
+  }
+
+  when (resolvedName) {
+    ARTIFACT_MAIN -> {
+      configurationNames.add(buildTypeName + COMPILE_SUFFIX)
+      configurationNames.add(buildTypeName + API_SUFFIX)
+      configurationNames.add(buildTypeName + IMPLEMENTATION_SUFFIX)
+    }
+    ARTIFACT_UNIT_TEST -> {
+      configurationNames.add("test" + capitalize(buildTypeName) + COMPILE_SUFFIX)
+      configurationNames.add("test" + capitalize(buildTypeName) + API_SUFFIX)
+      configurationNames.add("test" + capitalize(buildTypeName) + IMPLEMENTATION_SUFFIX)
+    }
+  }
+
+  productFlavorNames.forEach { productFlavorName ->
+    when (resolvedName) {
+      ARTIFACT_MAIN -> {
+        configurationNames.add(productFlavorName + COMPILE_SUFFIX)
+        configurationNames.add(productFlavorName + API_SUFFIX)
+        configurationNames.add(productFlavorName + IMPLEMENTATION_SUFFIX)
+      }
+      ARTIFACT_UNIT_TEST -> {
+        configurationNames.add("test" + capitalize(productFlavorName) + COMPILE_SUFFIX)
+        configurationNames.add("test" + capitalize(productFlavorName) + API_SUFFIX)
+        configurationNames.add("test" + capitalize(productFlavorName) + IMPLEMENTATION_SUFFIX)
+      }
+      ARTIFACT_ANDROID_TEST -> {
+        configurationNames.add("androidTest" + capitalize(productFlavorName) + COMPILE_SUFFIX)
+        configurationNames.add("androidTest" + capitalize(productFlavorName) + API_SUFFIX)
+        configurationNames.add("androidTest" + capitalize(productFlavorName) + IMPLEMENTATION_SUFFIX)
+      }
+    }
+  }
+  return configurationNames
+}
+
