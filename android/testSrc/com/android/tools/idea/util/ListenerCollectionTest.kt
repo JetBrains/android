5ca20263889febea58722b509185f8bc70b2707a
Diego Perez <diegoperez@google.com>
2018-06-05 16:17:03 +0100

    Convert ListenerCollection to Kotlin
    Test: No functional changes
    Bug: N/A
    Change-Id: I0629f2eaa5731b9bdae960c28d652412342ab7b3

---
+++ android/testSrc/com/android/tools/idea/util/ListenerCollectionTest.kt
@@ -0,0 +1,101 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.util
+
+import org.junit.Assert.*
+import org.junit.Test
+import java.util.*
+import java.util.concurrent.Executor
+import java.util.concurrent.atomic.AtomicBoolean
+import java.util.concurrent.atomic.AtomicInteger
+import java.util.function.Consumer
+
+typealias MyListener = () -> Unit
+
+class ListenerCollectionTest {
+  @Test
+  fun testAddRemoveSemantics() {
+    val handler = ListenerCollection.createWithDirectExecutor<MyListener>()
+    val listener1 = {}
+    val listener2 = {}
+    val listener3 = {}
+
+    assertTrue(handler.add(listener1))
+    assertFalse(handler.add(listener1))
+    handler.clear()
+    assertTrue(handler.add(listener1))
+    assertTrue(handler.add(listener2))
+    assertFalse(handler.add(listener1))
+    assertFalse(handler.remove(listener3)) // Not yet added
+    assertTrue(handler.add(listener3))
+    assertTrue(handler.remove(listener3))
+    assertFalse(handler.remove(listener3)) // Already removed
+    assertTrue(handler.remove(listener1))
+    handler.clear()
+    assertFalse(handler.remove(listener2)) // Already removed
+  }
+
+  @Test
+  fun testOnCurrentThread() {
+    val handler = ListenerCollection.createWithDirectExecutor<MyListener>()
+
+    val callCount = AtomicInteger(0)
+    handler.forEach(Consumer { callCount.incrementAndGet() })
+    assertEquals(0, callCount.get().toLong())
+
+    handler.add({})
+    handler.forEach(Consumer { callCount.incrementAndGet() })
+    assertEquals(1, callCount.get().toLong())
+
+    callCount.set(0)
+    handler.add({})
+    // Now we have two listeners, so next invocation should increment the counter by 2
+    handler.forEach(Consumer { callCount.incrementAndGet() })
+    assertEquals(2, callCount.get().toLong())
+
+    callCount.set(0)
+    handler.clear()
+    handler.forEach(Consumer { callCount.incrementAndGet() })
+    assertEquals(0, callCount.get().toLong())
+
+    // Check reentrant handler
+    handler.add({ handler.add({}) })
+    handler.forEach(Consumer { l ->
+      callCount.incrementAndGet()
+      l()
+    })
+    assertEquals(1, callCount.get().toLong())
+  }
+
+  @Test
+  fun testSeparateExecutor() {
+    val calls = LinkedList<Runnable>()
+    val fakeExecutor = Executor { calls.add(it) }
+    val handler = ListenerCollection.createWithExecutor<MyListener>(fakeExecutor)
+
+    handler.forEach(Consumer { it() })
+    assertTrue(calls.isEmpty()) // No listeners to call
+
+    val called = AtomicBoolean(false)
+    handler.add({ called.set(true) })
+    handler.forEach(Consumer { it() })
+    assertEquals(1, calls.size.toLong())
+    assertFalse(called.get()) // The call was added but the listener has not executed the method yet.
+
+    calls.forEach(Consumer<Runnable> { it.run() })
+    assertTrue(called.get()) // The call was added but the listener has not executed the method yet.
+  }
+}
