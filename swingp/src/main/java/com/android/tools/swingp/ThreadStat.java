3ee15764217207438223d3214d01e95f4c6e2019
Chester Hsieh <dunno@google.com>
2018-06-05 12:57:41 -0700

    Add Swing rendering instrumentation library.
    This CL adds:
    * A basic Swing instrumentation library.
    * Adds instrumentation around JComponent calls to
        paint[Component|Children|Immediately]
    * Adds instrumentation to capture Graphics2D transforms and JComponent bounds,
        which gives us Swing paint tree captures.
    * Adds instrumentation to the Swing Window, to capture heavy weight component
        locations.
    
    To build (the deployment jar with needed deps), call with the following:
    $ tools/base/bazel/bazel build //tools/adt/idea/swingp:swing-instrumenter_deploy.jar
    
    To run, call with the following additional VM options:
    -Xbootclasspath/a:"../../../bazel-bin/tools/adt/idea/swingp/swing-instrumenter_deploy.jar"
    -javaagent:"../../../bazel-bin/tools/adt/idea/swingp/swing-instrumenter_deploy.jar"
    
    Optionally, add the following VM option to turn on verifications (thanks
    Raluca!):
    -Xverify:all
    
    Bug: 80499904
    Test: Studio doesn't crash via the verifier + basic tests
    Change-Id: I71c38ffa14d5122a118d79d13dd6741349319615

---
+++ swingp/src/main/java/com/android/tools/swingp/ThreadStat.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.swingp;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.lang.ref.SoftReference;
+import java.util.Stack;
+import java.util.concurrent.LinkedBlockingQueue;
+
+/**
+ * Root of every call tree. As in, all {@link MethodStat} call trees are rooted at a {@link ThreadStat}.
+ */
+class ThreadStat {
+  @NotNull private final SoftReference<Thread> myThread;
+  @NotNull private final String myThreadName;
+  @NotNull private final LinkedBlockingQueue<MethodStat> myRootStats;
+  @NotNull private Stack<MethodStat> myMethodStack = new Stack<>();
+  private final long myThreadId;
+  private boolean myIsRecording;
+
+  ThreadStat() {
+    Thread currentThread = Thread.currentThread();
+    myThread = new SoftReference<>(currentThread);
+    myThreadId = currentThread.getId();
+    myThreadName = currentThread.getName();
+    myRootStats = new LinkedBlockingQueue<>();
+  }
+
+  @Nullable
+  public Thread getThread() {
+    return myThread.get();
+  }
+
+  void pushMethod(@NotNull MethodStat currentStat) {
+    // If recording is stopped, let stats go through until the stack is popped.
+    if (myMethodStack.empty() && !myIsRecording) {
+      return;
+    }
+
+    if (!myMethodStack.empty()) {
+      myMethodStack.peek().addChildStat(currentStat);
+    }
+    myMethodStack.push(currentStat);
+  }
+
+  void popMethod(@NotNull MethodStat verification) {
+    // If recording is stopped, let stats go through until the stack is popped.
+    if (myMethodStack.empty() && !myIsRecording) {
+      return;
+    }
+
+    if (myMethodStack.empty()) {
+      throw new RuntimeException("MethodStat#endMethod called more than once");
+    }
+
+    MethodStat methodStat = myMethodStack.pop();
+    if (methodStat != verification) {
+      throw new RuntimeException("Did not call MethodStat#endMethod at the end of the method or it was called more than once");
+    }
+
+    if (myMethodStack.empty()) {
+      // Add only the complete call tree at the end, so we don't get incomplete stacks during dumps.
+      myRootStats.add(methodStat);
+    }
+  }
+
+  @NotNull
+  public ThreadStat setIsRecording(boolean recording) {
+    myIsRecording = recording;
+    return this;
+  }
+}
---
+++ swingp/src/main/java/com/android/tools/swingp/instrumenter/RepaintManagerClassTransformer.java
@@ -0,0 +1,139 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.swingp.instrumenter;
+
+import com.android.tools.swingp.PaintImmediatelyMethodStat;
+import org.jetbrains.org.objectweb.asm.*;
+import org.jetbrains.org.objectweb.asm.commons.GeneratorAdapter;
+import org.jetbrains.org.objectweb.asm.commons.Method;
+
+import javax.swing.*;
+import java.lang.instrument.ClassFileTransformer;
+import java.security.ProtectionDomain;
+
+public class RepaintManagerClassTransformer implements ClassFileTransformer {
+  private static final String REPAINT_MANAGER_NAME = RepaintManager.class.getCanonicalName().replace('.', '/');
+
+  @Override
+  public byte[] transform(ClassLoader loader,
+                          String className,
+                          Class<?> classBeingRedefined,
+                          ProtectionDomain protectionDomain,
+                          byte[] classfileBuffer) {
+    if (!REPAINT_MANAGER_NAME.equals(className)) {
+      return classfileBuffer;
+    }
+
+    System.out.println("Transforming RepaintManager");
+    ClassReader reader = new ClassReader(classfileBuffer);
+    ClassWriter defaultWriter = new ClassWriter(ClassWriter.COMPUTE_FRAMES | ClassWriter.COMPUTE_MAXS);
+    ClassVisitor visitor = new RepaintManagerVisitor(defaultWriter);
+
+    try {
+      reader.accept(visitor, ClassReader.EXPAND_FRAMES);
+    }
+    catch (Exception e) {
+      e.printStackTrace();
+    }
+
+    return defaultWriter.toByteArray();
+  }
+
+  private static class RepaintManagerVisitor extends ClassVisitor {
+    private boolean myIsRepaintManager = false;
+
+    public RepaintManagerVisitor(ClassVisitor visitor) {
+      super(Opcodes.ASM5, visitor);
+    }
+
+    @Override
+    public void visit(int version, int access, String name, String signature, String superName, String[] interfaces) {
+      if (REPAINT_MANAGER_NAME.equals(name)) {
+        myIsRepaintManager = true;
+        System.out.println("Visiting RepaintManager");
+      }
+      super.visit(version, access, name, signature, superName, interfaces);
+    }
+
+    @Override
+    public MethodVisitor visitMethod(final int access, final String name,
+                                     final String desc, final String signature, final String[] exceptions) {
+      MethodVisitor defaultVisitor = super.visitMethod(access, name, desc, signature, exceptions);
+      if (!myIsRepaintManager || defaultVisitor == null) {
+        return defaultVisitor;
+      }
+
+      if ("paint".equals(name) && "(Ljavax/swing/JComponent;Ljavax/swing/JComponent;Ljava/awt/Graphics;IIII)V".equals(desc)) {
+        return new PaintMethodVisitor(defaultVisitor, access, name, desc);
+      }
+
+      return defaultVisitor;
+    }
+  }
+
+  private static class PaintMethodVisitor extends GeneratorAdapter {
+    private final Type paintImmediatelyMethodStatType = Type.getType(PaintImmediatelyMethodStat.class);
+    private int myPaintImmediatelyMethodStatIndex = -1;
+
+    public PaintMethodVisitor(MethodVisitor mv, int access, String name, String desc) {
+      super(Opcodes.ASM5, mv, access, name, desc);
+      System.out.println("Found RepaintManager " + name + ", instrumenting...");
+    }
+
+    @Override
+    public void visitCode() {
+      super.visitCode();
+
+      System.out.println("Instrumenting paint method...");
+
+      newInstance(paintImmediatelyMethodStatType);
+      dup();
+      loadThis(); // Prepare the stack for the eventual call to the constructor of PaintImmediateMethodStat.
+      super.visitVarInsn(Opcodes.ALOAD, 2); // Load the bufferComponent to capture what heavy weight component we're really painting into.
+      super.visitVarInsn(Opcodes.ALOAD, 3); // Load the Graphics parameter for the method stat.
+      super.visitVarInsn(Opcodes.ILOAD, 4); // Load x.
+      super.visitVarInsn(Opcodes.ILOAD, 5); // Load y.
+      super.visitVarInsn(Opcodes.ILOAD, 6); // Load w.
+      super.visitVarInsn(Opcodes.ILOAD, 7); // Load h.
+      invokeConstructor(paintImmediatelyMethodStatType, Method.getMethod(PaintImmediatelyMethodStat.class.getConstructors()[0]));
+      myPaintImmediatelyMethodStatIndex = newLocal(paintImmediatelyMethodStatType);
+      storeLocal(myPaintImmediatelyMethodStatIndex);
+    }
+
+    @Override
+    public void visitInsn(int opcode) {
+      System.out.println("Instrumenting exit for paint...");
+      switch (opcode) {
+        case Opcodes.IRETURN:
+        case Opcodes.LRETURN:
+        case Opcodes.FRETURN:
+        case Opcodes.DRETURN:
+        case Opcodes.ARETURN:
+        case Opcodes.RETURN:
+        case Opcodes.ATHROW:
+          assert myPaintImmediatelyMethodStatIndex > 0;
+          loadLocal(myPaintImmediatelyMethodStatIndex); // Load the PaintChildrenMethodStat local variable onto the stack (it takes no parameters).
+          invokeVirtual(paintImmediatelyMethodStatType, new Method("endMethod", "()V"));
+          break;
+
+        default:
+          // Do nothing special.
+          break;
+      }
+      super.visitInsn(opcode);
+    }
+  }
+}
