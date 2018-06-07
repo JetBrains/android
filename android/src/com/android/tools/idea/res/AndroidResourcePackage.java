83d7aa93fc422ee2b791317a436d196cd10eb9fc
Michal Bendowski <bendowski@google.com>
2018-06-07 09:23:37 +0100

    Override findClassByShortName in AndroidResourcePackage
    So that it's correct for our purposes, as opposed to the default
    optimized implementation in PsiPackageImpl.
    
    I also reworked tests for the whole light R classes feature.
    
    Bug: 77801019
    Test: LightRClassesTests
    Change-Id: Id73531fc2085a31f167924dd7d94971390002432

--- android/src/com/android/tools/idea/res/AndroidResourcePackage.java
+++ android/src/com/android/tools/idea/res/AndroidResourcePackage.java
@@ -15,9 +15,15 @@
  */
 package com.android.tools.idea.res;
 
+import com.android.SdkConstants;
 import com.google.common.base.MoreObjects;
-import com.intellij.psi.PsiManager;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.*;
 import com.intellij.psi.impl.file.PsiPackageImpl;
+import com.intellij.psi.search.GlobalSearchScope;
+import com.intellij.psi.search.PsiSearchScopeUtil;
+import com.intellij.psi.search.SearchScope;
+import org.jetbrains.annotations.NotNull;
 
 /** A generated stub PsiPackage for generated R classes. */
 public class AndroidResourcePackage extends PsiPackageImpl {
@@ -38,5 +44,27 @@ public class AndroidResourcePackage extends PsiPackageImpl {
   @Override
   public String toString() {
     return MoreObjects.toStringHelper(this).addValue(getQualifiedName()).toString();
+  }
+
+  /**
+   * Finds classes with the given short name in this package in the given scope.
+   *
+   * <p>Naive implementation that just calls {@link JavaPsiFacade} which in turn will call {@link PsiElementFinder} implementations.
+   * {@link PsiPackageImpl} maintains a cached copy of all classes in this package in the entire project (ignoring scope) and then filters
+   * it using {@link PsiSearchScopeUtil#isInScope(SearchScope, PsiElement)}. This doesn't work in our case, because R classes in this
+   * package are light and don't have corresponding {@link VirtualFile}s so {@link PsiSearchScopeUtil} always thinks they are in scope.
+   *
+   * <p>When delegating to {@link JavaPsiFacade}, the scope is handled in the implementations of {@link LightResourceClassService}.
+   *
+   * @see ProjectLightResourceClassService#getLightRClasses(String, GlobalSearchScope)
+   */
+  @NotNull
+  @Override
+  public PsiClass[] findClassByShortName(@NotNull String name, @NotNull GlobalSearchScope scope) {
+    if (SdkConstants.R_CLASS.equals(name)) {
+      return JavaPsiFacade.getInstance(getProject()).findClasses(getQualifiedName() + ".R", scope);
+    } else {
+      return PsiClass.EMPTY_ARRAY;
+    }
   }
 }
---
+++ android/testSrc/com/android/tools/idea/res/LightRClassesTests.kt
@@ -0,0 +1,225 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.res
+
+import com.android.builder.model.AndroidProject
+import com.android.tools.idea.flags.StudioFlags
+import com.android.tools.idea.testing.caret
+import com.google.common.truth.Truth.assertThat
+import com.intellij.codeInsight.TargetElementUtil
+import com.intellij.openapi.command.WriteCommandAction.runWriteCommandAction
+import com.intellij.pom.java.LanguageLevel
+import com.intellij.psi.JavaPsiFacade
+import com.intellij.testFramework.fixtures.IdeaProjectTestFixture
+import com.intellij.testFramework.fixtures.TestFixtureBuilder
+import org.jetbrains.android.AndroidTestCase
+import org.jetbrains.android.facet.AndroidFacet
+
+/**
+ * Tests for the whole setup of light, in-memory R classes.
+ *
+ * @see ProjectSystemPsiElementFinder
+ * @see ProjectLightResourceClassService
+ */
+sealed class LightRClassesTestBase : AndroidTestCase() {
+
+  override fun setUp() {
+    super.setUp()
+    StudioFlags.IN_MEMORY_R_CLASSES.override(true)
+    // No need to copy R.java into gen!
+  }
+
+  /**
+   * Sets the language level to avoid the module system visibility rules.
+   */
+  override fun getLanguageLevel() = LanguageLevel.JDK_1_8
+
+  override fun tearDown() {
+    try {
+      StudioFlags.IN_MEMORY_R_CLASSES.clearOverride()
+    }
+    finally {
+      super.tearDown()
+    }
+  }
+
+  class SingleModule : LightRClassesTestBase() {
+    override fun setUp() {
+      super.setUp()
+
+      myFixture.addFileToProject(
+        "/res/values/values.xml",
+        // language=xml
+        """
+        <resources>
+          <string name="appString">Hello from app</string>
+        </resources>
+        """.trimIndent()
+      )
+    }
+
+    fun testFindClass() {
+      val javaPsiFacade = JavaPsiFacade.getInstance(project)
+      assertThat(javaPsiFacade.findClass("p1.p2.R", myModule.moduleScope)).named("module R class").isNotNull()
+      assertThat(javaPsiFacade.findClass("p1.p2.R.string", myModule.moduleScope)).named("existing subclass").isNotNull()
+      assertThat(javaPsiFacade.findClass("p1.p2.R.color", myModule.moduleScope)).named("non-existing subclass").isNull()
+    }
+
+    fun testTopLevelClassCompletion() {
+      val activity = myFixture.addFileToProject(
+        "/src/p1/p2/MainActivity.java",
+        // language=java
+        """
+        package p1.p2;
+
+        import android.app.Activity;
+        import android.os.Bundle;
+
+        public class MainActivity extends Activity {
+            @Override
+            protected void onCreate(Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                getResources().getString(p1.p2.${caret});
+            }
+        }
+        """.trimIndent()
+      )
+
+      myFixture.configureFromExistingVirtualFile(activity.virtualFile)
+      myFixture.completeBasic()
+
+      assertThat(myFixture.lookupElementStrings).containsExactly("R", "MainActivity")
+    }
+
+    fun testInnerClassesCompletion() {
+      myFixture.configureByText(
+        "/src/p1/p2/MainActivity.java",
+        // language=java
+        """
+        package p1.p2;
+
+        import android.app.Activity;
+        import android.os.Bundle;
+
+        public class MainActivity extends Activity {
+            @Override
+            protected void onCreate(Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                getResources().getString(R.${caret});
+            }
+        }
+        """.trimIndent()
+      )
+
+      myFixture.completeBasic()
+
+      // TODO(b/80425291): remove "sample"
+      assertThat(myFixture.lookupElementStrings).containsExactly("class", "string", "sample")
+    }
+
+    fun testResourceNamesCompletion() {
+      myFixture.configureByText(
+        "/src/p1/p2/MainActivity.java",
+        // language=java
+        """
+        package p1.p2;
+
+        import android.app.Activity;
+        import android.os.Bundle;
+
+        public class MainActivity extends Activity {
+            @Override
+            protected void onCreate(Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                getResources().getString(R.string.${caret});
+            }
+        }
+        """.trimIndent()
+      )
+
+      myFixture.completeBasic()
+
+      assertThat(myFixture.lookupElementStrings).containsExactly("appString", "class")
+    }
+  }
+
+  /**
+   * Tests with a module that should not see R class from another module.
+   */
+  class UnrelatedModules : LightRClassesTestBase() {
+
+    override fun configureAdditionalModules(
+      projectBuilder: TestFixtureBuilder<IdeaProjectTestFixture>,
+      modules: MutableList<MyAdditionalModuleData>
+    ) {
+      addModuleWithAndroidFacet(projectBuilder, modules, "unrelatedLib", AndroidProject.PROJECT_TYPE_LIBRARY, false)
+    }
+
+    override fun setUp() {
+      super.setUp()
+
+      runWriteCommandAction(project) {
+        getAdditionalModuleByName("unrelatedLib")!!
+          .let(AndroidFacet::getInstance)!!
+          .manifest!!
+          .`package`!!
+          // TODO(b/109739056): Once we correctly create subpackages, we no longer need to use a common prefix.
+          .value = "p1.p2.unrelatedLib"
+      }
+
+      myFixture.addFileToProject(
+        "${getAdditionalModulePath("unrelatedLib")}/res/values/values.xml",
+        // language=xml
+        """
+        <resources>
+          <string name="libString">Hello from app</string>
+        </resources>
+        """.trimIndent()
+      )
+    }
+
+    fun testTopLevelClassCompletion() {
+      val activity = myFixture.addFileToProject(
+        "/src/p1/p2/MainActivity.java",
+        // language=java
+        """
+        package p1.p2;
+
+        import android.app.Activity;
+        import android.os.Bundle;
+
+        public class MainActivity extends Activity {
+            @Override
+            protected void onCreate(Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                getResources().getString(p1.p2.unrelatedLib.${caret}R.string.libString);
+            }
+        }
+        """.trimIndent()
+      )
+
+      myFixture.configureFromExistingVirtualFile(activity.virtualFile)
+
+      // We cannot use myFixture.elementAtCaret or TargetElementUtil.REFERENCED_ELEMENT_ACCEPTED because JavaTargetElementEvaluator doesn't
+      // consider synthetic PSI elements as "acceptable" and just returns null instead, so it wouldn't test much.
+      val rReference = TargetElementUtil.findReference(myFixture.editor)!!
+      assertThat(rReference.resolve()).isNull()
+
+      myFixture.completeBasic()
+      assertThat(myFixture.lookupElementStrings).isEmpty()
+    }
+  }
+}
