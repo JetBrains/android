424cb6953c005b06ca45b8ac93018721318d720d
Jingjing Liu <jingjingliu@google.com>
2018-06-11 14:32:21 -0700

    Add 3 new FAT tests for Live Templates
    Bug: na
    Test: Ran these tests locally with bazel
    Change-Id: I02406c7d6010c44d7e2dfda3ef77a691ccd27ca7

---
+++ android-uitests/testSrc/com/android/tools/idea/tests/gui/editors/LiveTemplatesTestUtil.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.tests.gui.editors;
+
+import com.android.tools.idea.tests.gui.framework.fixture.EditorFixture;
+import com.android.tools.idea.tests.gui.framework.fixture.IdeFrameFixture;
+import com.intellij.openapi.util.Ref;
+import com.intellij.ui.components.JBList;
+import org.fest.swing.fixture.JListFixture;
+import org.fest.swing.fixture.JListItemFixture;
+import org.fest.swing.timing.Wait;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.regex.Pattern;
+
+public class LiveTemplatesTestUtil {
+
+  protected static String JAVA_FILE = "/app/src/main/java/google/simpleapplication/MyActivity.java";
+  protected static String STATEMENT = "setContentView\\(R.layout.activity_my\\);";
+
+  protected static JListFixture clickOnCodeInsertLiveTemplate(@NotNull IdeFrameFixture ideFrame,
+                                                              @NotNull EditorFixture editorFixture) {
+    editorFixture.moveBetween("", "setContentView(R.layout.activity_my);");
+
+    ideFrame.invokeMenuPath("Code", "Insert Live Template...");
+
+    Ref<JBList> out = new Ref<>();
+    Wait.seconds(10).expecting("Live Templates list to show.").until(() -> {
+      // There is only one JBList.
+      JBList jbList = ideFrame.robot().finder().findByType(ideFrame.target(), JBList.class);
+
+      if (jbList == null) {
+        return false;
+      }
+
+      out.set(jbList);
+      return true;
+    });
+
+    JListFixture listFixture= new JListFixture(ideFrame.robot(), out.get());
+    return listFixture;
+  }
+
+  protected static void doubleTapToInsertLiveTemplate(@NotNull IdeFrameFixture ideFrame,
+                                                      @NotNull EditorFixture editorFixture,
+                                                      String itemText) {
+    JListFixture listFixture = clickOnCodeInsertLiveTemplate(ideFrame, editorFixture);
+    JListItemFixture jListItemFixture = listFixture.item(Pattern.compile(".*" + itemText + ".*", Pattern.DOTALL));
+    jListItemFixture.doubleClick();
+  }
+
+}
