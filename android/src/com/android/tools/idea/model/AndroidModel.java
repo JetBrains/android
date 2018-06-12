4bb539afb2853658a7727062506e9543f0b929b8
Stefan Xenos <sxenos@google.com>
2018-06-12 04:32:35 +0000

    Add a new getResValues() method to AndroidModel
    Move the gradle-specific logic in
    DynamicResourceValueRepository.getFullTable to a new getResValues()
    method on AndroidModel, as a first step to providing this functionality
    in a build-system-independent manner.
    
    This restores the code reverted by commit
    91528823ad1eb09bd96902932729740f2482d08e, originaly implemented in
    commit e41137575ea5793052c8c13e8d7fade44d0a6526. However this patch
    additionally includes a fix for a bug introduced in DynamicResourceValueRepository.
    
    The original patch was applying the resource overloads in an incorrect
    order, causing the flavor values to override the build type values.
    That caused a test failure NlPreviewTest.testRenderingDynamicDependencies.
    This patch reverses the order and fixes the failing test.
    
    Bug: 109665898
    Test: NlPreviewTest
    Change-Id: Ibe73f2bc38babe564ad939e2ced75a4fddbb58d3

--- android/src/com/android/tools/idea/model/AndroidModel.java
+++ android/src/com/android/tools/idea/model/AndroidModel.java
@@ -17,6 +17,7 @@ package com.android.tools.idea.model;
 
 import com.android.builder.model.AaptOptions;
 import com.android.builder.model.SourceProvider;
+import com.android.projectmodel.DynamicResourceValue;
 import com.android.sdklib.AndroidVersion;
 import com.android.tools.idea.databinding.DataBindingMode;
 import com.intellij.openapi.module.Module;
@@ -26,7 +27,9 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.io.File;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 /**
@@ -166,4 +169,12 @@ public interface AndroidModel {
 
   @NotNull
   AaptOptions.Namespacing getNamespacing();
+
+  /**
+   * Returns the set of build-system-provided resource values and overrides.
+   */
+  @NotNull
+  default Map<String, DynamicResourceValue> getResValues() {
+    return Collections.emptyMap();
+  }
 }
