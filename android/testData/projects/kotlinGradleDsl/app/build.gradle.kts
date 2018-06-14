30116e992d4a2b3e288894d5097fd65d5f01e9cf
Juan Chen <juanch@google.com>
2018-06-13 20:02:41 -0700

    Update BUILD dependencies for Kotlin 1.2.50.
    Bug: 80598993
    Test: existing tests
    Change-Id: If6c1d3ff22fc9974934ced6145afa5b5ead23825

--- android/testData/projects/kotlinGradleDsl/app/build.gradle.kts
+++ android/testData/projects/kotlinGradleDsl/app/build.gradle.kts
@@ -30,7 +30,7 @@ configure<AppExtension> {
 dependencies {
     "implementation"("com.android.support:appcompat-v7:27.1.1")
     "implementation"("com.android.support.constraint:constraint-layout:1.0.2")
-    "implementation"(kotlin("stdlib", "1.2.41"))
+    "implementation"(kotlin("stdlib", "1.2.50"))
     "implementation"(project(":lib"))
 }
 
