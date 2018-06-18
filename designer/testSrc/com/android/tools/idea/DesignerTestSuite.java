5f6ff03456962cd14f478f80204732dcac1937a0
Nathan Paige <npaige@google.com>
2018-06-18 13:54:09 -0700

    Use kotlin-m2repository maven repo in tests
    The test_deps rule will no longer contain the kotlin gradle plugin and
    its dependencies; these are moving to a separate maven repo in another
    change in this topic. This change updates BUILD files to include this
    new repository, and includes it in the setup done in the Suite classes
    for which it's necessary.
    
    Bug: 110231494
    Test-Strategy: ui.test.groups=DEFAULT
    Change-Id: Icaa61fe620b75733eeb67f4eeb8e149d372fbbb3

--- designer/testSrc/com/android/tools/idea/DesignerTestSuite.java
+++ designer/testSrc/com/android/tools/idea/DesignerTestSuite.java
@@ -55,5 +55,6 @@ public class DesignerTestSuite extends IdeaTestSuiteBase {
 
     setUpOfflineRepo("tools/base/build-system/studio_repo.zip", "out/studio/repo");
     setUpOfflineRepo("tools/adt/idea/android/test_deps.zip", "prebuilts/tools/common/m2/repository");
+    setUpOfflineRepo("tools/base/third_party/kotlin/kotlin-m2repository.zip", "prebuilts/tools/common/m2/repository");
   }
 }
