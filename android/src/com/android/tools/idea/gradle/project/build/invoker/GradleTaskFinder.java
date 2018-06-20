5c7fb60eff0f6274480a1f19259bf1abf7a0179e
Vladislav.Soroka <Vladislav.Soroka@jetbrains.com>
2018-06-20 12:06:25 +0300

    IDEA-190632 Test sources are not compiled in Android project

--- android/src/com/android/tools/idea/gradle/project/build/invoker/GradleTaskFinder.java
+++ android/src/com/android/tools/idea/gradle/project/build/invoker/GradleTaskFinder.java
@@ -229,7 +229,7 @@ public class GradleTaskFinder {
         if (gradleTaskName != null) {
           tasks.add(createBuildTask(gradlePath, gradleTaskName));
         }
-        if (TestCompileType.UNIT_TESTS.equals(testCompileType)) {
+        if (TestCompileType.UNIT_TESTS.equals(testCompileType) || TestCompileType.ALL.equals(testCompileType)) {
           tasks.add(createBuildTask(gradlePath, JavaFacet.TEST_CLASSES_TASK_NAME));
         }
       }
