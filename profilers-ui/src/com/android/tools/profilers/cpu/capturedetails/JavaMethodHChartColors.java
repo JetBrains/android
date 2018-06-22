ec6940bd4cdebfb221f144d7c24e02af667e373d
Madiyar Aitbayev <madiyar@google.com>
2018-06-22 11:41:06 +0100

    Move HTreeChart related classes to capturedetails
    Bug: 110657569
    Test: migrated tests as well
    Change-Id: Ifda9ac49e38312306ec5f9f453f76a861747a46b

--- profilers-ui/src/com/android/tools/profilers/cpu/capturedetails/JavaMethodHChartColors.java
+++ profilers-ui/src/com/android/tools/profilers/cpu/capturedetails/JavaMethodHChartColors.java
@@ -1,26 +1,29 @@
-// Copyright (C) 2017 The Android Open Source Project
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-package com.android.tools.profilers.cpu;
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.profilers.cpu.capturedetails;
 
 import com.android.tools.profilers.ProfilerColors;
+import com.android.tools.profilers.cpu.CaptureModel;
 import com.android.tools.profilers.cpu.nodemodel.CaptureNodeModel;
 import com.android.tools.profilers.cpu.nodemodel.JavaMethodModel;
 import org.jetbrains.annotations.NotNull;
 
 import java.awt.*;
 
-import static com.android.tools.profilers.cpu.CaptureNodeHRenderer.toUnmatchColor;
+import static com.android.tools.profilers.cpu.capturedetails.CaptureNodeHRenderer.toUnmatchColor;
 
 /**
  *  Defines the fill color of the rectangles used to represent {@link JavaMethodModel} nodes in a
