e01e7a58c3e11ee3475c377432663ed4a84bf9ba
Gregory.Shrago <gregory.shrago@jetbrains.com>
2018-07-04 04:15:18 +0300

    db-psi-manager api cleanup

--- ultimate/src/org/jetbrains/android/database/AndroidDbManager.java
+++ ultimate/src/org/jetbrains/android/database/AndroidDbManager.java
@@ -1,34 +1,21 @@
 package org.jetbrains.android.database;
 
-import com.intellij.database.DatabaseMessages;
 import com.intellij.database.dialects.DatabaseDialectEx;
 import com.intellij.database.dialects.SqliteDialect;
-import com.intellij.database.model.DatabaseSystem;
 import com.intellij.database.psi.BasicDbPsiManager;
-import com.intellij.database.psi.DbPsiManager;
 import com.intellij.database.util.DbSqlUtil;
 import com.intellij.facet.ProjectFacetManager;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.command.UndoConfirmationPolicy;
-import com.intellij.openapi.command.WriteCommandAction;
-import com.intellij.openapi.command.undo.GlobalUndoableAction;
-import com.intellij.openapi.command.undo.UndoManager;
-import com.intellij.openapi.command.undo.UndoableAction;
-import com.intellij.openapi.command.undo.UnexpectedUndoException;
 import com.intellij.openapi.options.Configurable;
 import com.intellij.openapi.project.DumbAwareAction;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.sql.dialects.SqlLanguageDialect;
-import com.intellij.util.PairConsumer;
+import com.intellij.util.Consumer;
 import icons.AndroidIcons;
 import org.jetbrains.android.facet.AndroidFacet;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-
-import java.util.Collection;
-import java.util.Collections;
 
 /**
  * @author Eugene.Kudelevsky
@@ -36,47 +23,51 @@ import java.util.Collections;
 public class AndroidDbManager extends BasicDbPsiManager<AndroidDataSource> {
   public static final String NOTIFICATION_GROUP_ID = "Android Data Source Manager";
 
-  public AndroidDbManager(@NotNull Project project) {
-    super(project, AndroidDataSourceStorage.getInstance(project).getDataSources());
+  private final AndroidDataSourceStorage myStorage;
+
+  public AndroidDbManager(@NotNull Project project, @NotNull AndroidDataSourceStorage storage) {
+    super(project, storage.getDataSources());
+    myStorage = storage;
   }
 
   @Nullable
   @Override
-  public DatabaseDialectEx getDatabaseDialect(@NotNull DatabaseSystem element) {
+  public DatabaseDialectEx getDatabaseDialect(@NotNull AndroidDataSource element) {
     return SqliteDialect.INSTANCE;
   }
 
   @Nullable
   @Override
-  public SqlLanguageDialect getSqlDialect(@NotNull DatabaseSystem element) {
+  public SqlLanguageDialect getSqlDialect(@NotNull AndroidDataSource element) {
     return DbSqlUtil.findSqlDialect(SqliteDialect.INSTANCE);
   }
 
   @Override
-  public void setDataSourceName(@NotNull DatabaseSystem element, String name) {
-    if (!(element instanceof AndroidDataSource)) throw new UnsupportedOperationException();
-    final AndroidDataSource dataSource = (AndroidDataSource)element;
-    dataSource.setName(name);
-    updateDataSource((AndroidDataSource)element);
+  public void renameDataSource(@NotNull AndroidDataSource element, @NotNull String name) {
+    element.setName(name);
+    updateDataSource(element);
   }
 
   @Override
-  public void removeDataSource(@NotNull DatabaseSystem element) {
-    if (!(element instanceof AndroidDataSource)) throw new UnsupportedOperationException();
-    final AndroidDataSource dataSource = (AndroidDataSource)element;
-    processAddOrRemove(dataSource, false);
+  public void addDataSource(@NotNull AndroidDataSource dataSource) {
+    myStorage.addDataSource(dataSource);
+    attachDataSource(dataSource);
+  }
+
+  @Override
+  public void removeDataSource(@NotNull AndroidDataSource element) {
+    myStorage.removeDataSource(element);
+    detachDataSource(element);
   }
 
   @NotNull
   @Override
-  public Configurable createDataSourceEditor(@NotNull DatabaseSystem element) {
-    if (!(element instanceof AndroidDataSource)) throw new UnsupportedOperationException();
-    AndroidDataSource dataSource = (AndroidDataSource)element;
-    return new AndroidDataSourceConfigurable(this, myProject, dataSource);
+  public Configurable createDataSourceEditor(@NotNull AndroidDataSource element) {
+    return new AndroidDataSourceConfigurable(this, myProject, element);
   }
 
   @Override
-  public AnAction getCreateDataSourceAction(@NotNull PairConsumer<DbPsiManager, DatabaseSystem> consumer) {
+  public AnAction getCreateDataSourceAction(@NotNull Consumer<AndroidDataSource> consumer) {
     if (!ProjectFacetManager.getInstance(myProject).hasFacets(AndroidFacet.ID)) return null;
     return new DumbAwareAction("Android SQLite", null, AndroidIcons.Android) {
       @Override
@@ -84,79 +75,18 @@ public class AndroidDbManager extends BasicDbPsiManager<AndroidDataSource> {
         AndroidDataSource result = new AndroidDataSource();
         result.setName(getTemplatePresentation().getText());
         result.resolveDriver();
-        consumer.consume(AndroidDbManager.this, result);
+        consumer.consume(result);
       }
     };
   }
 
   @NotNull
   @Override
-  public DatabaseSystem copyDataSource(@NotNull String newName, @NotNull DatabaseSystem copyFrom) {
-    AndroidDataSource result = ((AndroidDataSource)copyFrom).copy();
+  public AndroidDataSource copyDataSource(@NotNull String newName, @NotNull AndroidDataSource copyFrom) {
+    AndroidDataSource result = copyFrom.copy();
     result.setName(newName);
     result.resolveDriver();
     return result;
   }
 
-  public void processAddOrRemove(final AndroidDataSource dataSource, final boolean add) {
-    final UndoableAction action = new GlobalUndoableAction() {
-      public void undo() {
-        doIt(!add);
-      }
-
-      public void redo() {
-        doIt(add);
-      }
-
-      private void doIt(boolean add) {
-        if (add) {
-          addDataSourceInner(myProject, dataSource);
-        }
-        else {
-          removeDataSourceInner(myProject, dataSource);
-        }
-      }
-    };
-    try {
-      WriteCommandAction.writeCommandAction(myProject)
-                        .withName(add ? DatabaseMessages.message("command.name.add.data.source")
-                                      : DatabaseMessages.message("command.name.remove.data.source"))
-                        .withUndoConfirmationPolicy(UndoConfirmationPolicy.REQUEST_CONFIRMATION)
-                        .run(() -> {
-                          action.redo();
-                          UndoManager.getInstance(myProject).undoableActionPerformed(action);
-                        });
-    }
-    catch (UnexpectedUndoException e) {
-      throw new RuntimeException(e);
-    }
-  }
-
-  private void removeDataSourceInner(final Project project, final AndroidDataSource dataSource) {
-    AndroidDataSourceStorage storage = AndroidDataSourceStorage.getInstance(project);
-    storage.removeDataSource(dataSource);
-    detachDataSource(dataSource);
-  }
-
-  private void addDataSourceInner(final Project project, final AndroidDataSource dataSource) {
-    AndroidDataSourceStorage storage = AndroidDataSourceStorage.getInstance(project);
-    storage.addDataSource(dataSource);
-    attachDataSource(dataSource);
-  }
-
-  @Override
-  public boolean canCreateDataSourceByFiles(@NotNull Collection<VirtualFile> files) {
-    return false;
-  }
-
-  @NotNull
-  @Override
-  public Collection<AndroidDataSource> createDataSourceByFiles(@NotNull Collection<VirtualFile> files) {
-    return Collections.emptyList();
-  }
-
-  @Override
-  public void addDataSource(@NotNull AndroidDataSource dataSource) {
-    processAddOrRemove(dataSource, true);
-  }
 }
