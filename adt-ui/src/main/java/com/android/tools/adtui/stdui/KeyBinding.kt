f28800d48f04cea5dc9769542447d9f4e407eab4
Jens Ole Lauridsen <jlauridsen@google.com>
2018-07-06 12:19:23 -0700

    Move KeyBinding to AdtUi.
    Bug: 73778668
    Test: Existing tests should pass.
    Change-Id: I6dcb726f1ec6197fd41ade09f0e5c4a58fdc0332

--- adt-ui/src/main/java/com/android/tools/adtui/stdui/KeyBinding.kt
+++ adt-ui/src/main/java/com/android/tools/adtui/stdui/KeyBinding.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.android.tools.idea.common.property2.impl.ui
+package com.android.tools.adtui.stdui
 
 import com.intellij.ide.DataManager
 import com.intellij.openapi.actionSystem.AnAction
