2d325816a61fbf3a1480800739a73359d40d5895
estebandlc <estebandlc@google.com>
2018-07-12 16:30:37 -0700

    Hooks up the deployer with the UnifiedDeployTask
    Still doesn't work, but you can see the files that
    have changed in the console when it tries to install!
    
    Note that the installer needs to still be pushed manually
    to the device.
    
    Test: to follow up
    Bug: 111215639
    Change-Id: I7a4270b5233ed3997c24dd8d89a2f639db6ca8da

--- deploy/src/com/android/tools/idea/run/tasks/UnifiedDeployTask.java
+++ deploy/src/com/android/tools/idea/run/tasks/UnifiedDeployTask.java
@@ -17,16 +17,26 @@
 package com.android.tools.idea.run.tasks;
 
 import com.android.ddmlib.IDevice;
+import com.android.tools.deployer.Apk;
+import com.android.tools.deployer.DdmDevice;
+import com.android.tools.deployer.Deployer;
 import com.android.tools.idea.run.ApkInfo;
 import com.android.tools.idea.run.ConsolePrinter;
 import com.android.tools.idea.run.util.LaunchStatus;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
-public class UnifiedDeployTask implements LaunchTask {
+public class UnifiedDeployTask implements LaunchTask, Deployer.InstallerCallBack {
+
+  private final Collection<ApkInfo> myApks;
 
   public UnifiedDeployTask(@NotNull Collection<ApkInfo> apks) {
+    myApks = apks;
   }
 
   @NotNull
@@ -42,6 +52,25 @@ public class UnifiedDeployTask implements LaunchTask {
 
   @Override
   public boolean perform(@NotNull IDevice device, @NotNull LaunchStatus launchStatus, @NotNull ConsolePrinter printer) {
-    return false;
+    for (ApkInfo apk : myApks) {
+      List<String> paths = apk.getFiles().stream().map(
+        apkunit -> apkunit.getApkFile().getPath()).collect(Collectors.toList());
+      Deployer deployer = new Deployer(apk.getApplicationId(), paths, this, new DdmDevice(device));
+      HashMap<String, HashMap<String, Apk.ApkEntryStatus>> changes = deployer.run();
+      for (Map.Entry<String, HashMap<String, Apk.ApkEntryStatus>> entry : changes.entrySet()) {
+        System.err.println("Apk: " + entry.getKey());
+        for (Map.Entry<String, Apk.ApkEntryStatus> statusEntry : entry.getValue().entrySet()) {
+          System.err.println("  " + statusEntry.getKey() +
+                             " [" + statusEntry.getValue().toString().toLowerCase() + "]");
+        }
+      }
+    }
+
+    return true;
+  }
+
+  @Override
+  public void onInstallationFinished(boolean status) {
+    System.err.println("Installation finished");
   }
 }
