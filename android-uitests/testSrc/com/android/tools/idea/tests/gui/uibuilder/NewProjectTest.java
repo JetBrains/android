88ccb3511275bee085e9b2f91964516cabea7505
Jingjing Liu <jingjingliu@google.com>
2018-07-13 12:15:28 -0700

    Migrate fast test ChangeLibModSet...Test to bazel
    bug: 111399767
    Test: Ran this test with bazel
    Change-Id: Ie4f9220283a4ab6d2d12df9114ffff2ffbcc5602

--- android-uitests/testSrc/com/android/tools/idea/tests/gui/uibuilder/NewProjectTest.java
+++ android-uitests/testSrc/com/android/tools/idea/tests/gui/uibuilder/NewProjectTest.java
@@ -92,60 +92,6 @@ public class NewProjectTest {
     assertThat(text).contains("Test Application");
   }
 
-  /**
-   * Verify module properties can be modified.
-   * <p>
-   * This is run to qualify releases. Please involve the test team in substantial changes.
-   * <p>
-   * TT ID: 42c3202c-f2aa-4080-b94a-1a17d56d8fe2
-   * <p>
-   *   <pre>
-   *   Steps:
-   *   1. Create a new project.
-   *   2. Create a new library module and an application module.
-   *   3. Right click on the library module and select Change Module Settings.
-   *   4. Make a few changes to the properties, like build tools version.
-   *   5. Repeat with application module
-   *   Verify:
-   *   1. Module setting is updated.
-   *   </pre>
-   */
-  @RunIn(TestGroup.QA)
-  @Test
-  public void changeLibraryModuleSettings() throws Exception {
-    newProject("MyTestApp").withMinSdk("24").create(guiTest)
-      .openFromMenu(NewModuleWizardFixture::find, "File", "New", "New Module...")
-      .chooseModuleType("Android Library")
-      .clickNextToStep("Android Library")
-      .setModuleName("library_module")
-      .clickFinish()
-      .waitForGradleProjectSyncToFinish(Wait.seconds(30));
-
-    guiTest.waitForBackgroundTasks();
-
-    String gradleFileContents = guiTest.ideFrame()
-      .getProjectView()
-      .selectProjectPane()
-      .clickPath(RIGHT_BUTTON, "MyTestApp", "library_module")
-      .openFromMenu(ProjectStructureDialogFixture::find, "Open Module Settings")
-      .selectPropertiesTab()
-      .setCompileSdkVersion("API 24: Android 7.0 (Nougat)")
-      .setIgnoreAssetsPattern("TestIgnoreAssetsPattern")
-      .setIncrementalDex(false)
-      .setSourceCompatibility("1.7")
-      .setTargetCompatibility("1.7")
-      .clickOk()
-      .getEditor()
-      .open("/library_module/build.gradle")
-      .getCurrentFileContents();
-
-    assertThat(gradleFileContents).contains("compileSdkVersion 24");
-    assertThat(gradleFileContents).contains("aaptOptions {\n        ignoreAssetsPattern 'TestIgnoreAssetsPattern'\n    }");
-    assertThat(gradleFileContents).contains("dexOptions {\n        incremental false\n    }");
-    assertThat(gradleFileContents).contains(
-      "compileOptions {\n        sourceCompatibility JavaVersion.VERSION_1_7\n        targetCompatibility JavaVersion.VERSION_1_7\n    }");
-  }
-
   @RunIn(TestGroup.UNRELIABLE)  // b/66249968
   @Test
   public void testNoWarningsInNewProjects() throws IOException {
