c16c0c768a650a621f15ecdf44cc8cbb5a191748
Sergey Prigogin <sprigogin@google.com>
2018-07-20 19:58:49 -0700

    Preview rendering test with namespaces and appcompat
    Test: NamespacedRenderTestWithAppCompat
    Bug: 80301225
    Change-Id: Ie54a1c900bfbbda0084bf7694953cb3191e63cb0

--- android-test-framework/testSrc/com/android/tools/idea/testing/TestProjectPaths.java
+++ android-test-framework/testSrc/com/android/tools/idea/testing/TestProjectPaths.java
@@ -41,6 +41,7 @@ public final class TestProjectPaths {
   public static final String MOVE_WITH_RESOURCES = "projects/moveWithResources";
   public static final String MULTI_FEATURE = "projects/multiFeature";
   public static final String NAMESPACES = "projects/namespaces";
+  public static final String NAMESPACES_WITH_APPCOMPAT = "projects/namespaces_with_appcompat";
   public static final String NAVIGATION_EDITOR_BASIC = "projects/navigationEditor/basic";
   public static final String NAVIGATOR_PACKAGEVIEW_COMMONROOTS = "projects/navigator/packageview/commonroots";
   public static final String NAVIGATOR_PACKAGEVIEW_SIMPLE = "projects/navigator/packageview/simple";
