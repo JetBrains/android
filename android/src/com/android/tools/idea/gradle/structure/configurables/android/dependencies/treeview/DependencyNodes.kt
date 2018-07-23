624f93497d93140034c4047deae4ca0104a389fc
Yuriy Solodkyy <solodkyy@google.com>
2018-07-23 20:23:15 +0100

    [New PSD] Move artifact handling logic to AndroidArtifactNode
    Bug: n/a
    Test: existing unit tests
    Change-Id: I8a97b931096ce94b4533f717e91a61e3fe489a85

--- android/src/com/android/tools/idea/gradle/structure/configurables/android/dependencies/treeview/DependencyNodes.kt
+++ android/src/com/android/tools/idea/gradle/structure/configurables/android/dependencies/treeview/DependencyNodes.kt
@@ -15,33 +15,23 @@
  */
 package com.android.tools.idea.gradle.structure.configurables.android.dependencies.treeview
 
-import com.android.builder.model.AndroidProject.ARTIFACT_MAIN
 import com.android.tools.idea.gradle.structure.configurables.ui.dependencies.PsDependencyComparator
 import com.android.tools.idea.gradle.structure.configurables.ui.treeview.AbstractPsModelNode
 import com.android.tools.idea.gradle.structure.configurables.ui.treeview.AbstractPsNode
 import com.android.tools.idea.gradle.structure.model.*
-import com.android.tools.idea.gradle.structure.model.android.PsAndroidArtifact
 import com.google.common.collect.Lists
 import com.google.common.collect.Sets
 import com.intellij.util.containers.SortedList
 
-fun createNodesForResolvedDependencies(parent: AbstractPsNode,
-                                       collection: PsDependencyCollection<*, *, *>,
-                                       artifact: PsAndroidArtifact?): List<AbstractPsModelNode<*>> {
+fun createNodesForResolvedDependencies(
+  parent: AbstractPsNode,
+  collection: PsDependencyCollection<*, *, *>
+): List<AbstractPsModelNode<*>> {
   val allTransitive = Sets.newHashSet<String>()
   val children = Lists.newArrayList<AbstractPsModelNode<*>>()
 
   val declared = SortedList(PsDependencyComparator(parent.uiSettings))
   val mayBeTransitive = Lists.newArrayList<PsLibraryDependency>()
-
-  if (artifact != null && artifact.resolvedName != ARTIFACT_MAIN) {
-    val targetVariant = artifact.parent
-    val targetArtifact = targetVariant.findArtifact(ARTIFACT_MAIN)
-    if (targetArtifact != null) {
-      val artifactNode = AndroidArtifactNode(parent, targetArtifact)
-      children.add(artifactNode)
-    }
-  }
   for (dependency in collection.modules) {
     if (dependency.isDeclared) {
       declared.add(dependency)
