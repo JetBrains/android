f39471afb6482f888f7503dd8291ca81d9c6c2e2
Juan C Nuno <juancnuno@google.com>
2018-07-24 11:54:51 -0700

    Clear the header values from reused columns
    Bug: 111131641
    Test: TranslationsEditorTest.reusedColumnHeaderValuesAreCleared
    Change-Id: I5973b22971779db4e3622e81f4809885fcfcc7ce

--- android/testSrc/com/android/tools/idea/editors/strings/TranslationsEditorTest.java
+++ android/testSrc/com/android/tools/idea/editors/strings/TranslationsEditorTest.java
@@ -19,18 +19,25 @@ import com.android.tools.idea.editors.strings.table.FrozenColumnTable;
 import com.android.tools.idea.editors.strings.table.StringResourceTableModel;
 import com.android.tools.idea.editors.strings.table.StringTableCellEditor;
 import com.android.tools.idea.io.TestFileUtils;
+import com.android.tools.idea.rendering.Locale;
 import com.android.tools.idea.testing.AndroidProjectRule;
 import com.intellij.openapi.application.Application;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.util.Disposer;
 import org.intellij.lang.annotations.Language;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
-import java.nio.file.FileSystem;
-import java.nio.file.FileSystems;
+import javax.swing.table.TableColumn;
+import javax.swing.table.TableColumnModel;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import static org.junit.Assert.assertEquals;
 
@@ -38,48 +45,80 @@ public final class TranslationsEditorTest {
   @Rule
   public final AndroidProjectRule myRule = AndroidProjectRule.inMemory();
 
-  @Test
-  public void setKeyName() throws Exception {
-    FileSystem fileSystem = FileSystems.getDefault();
+  private Application myApplication;
+  private Path myRes;
+  private StringResourceEditor myEditor;
+  private StringResourceViewPanel myPanel;
+  private FrozenColumnTable myTable;
+
+  @Before
+  public void setUp() throws Exception {
+    myApplication = ApplicationManager.getApplication();
+
     Module module = myRule.getModule();
-    Path res = fileSystem.getPath(module.getProject().getBasePath(), "app", "src", "main", "res");
+    myRes = Paths.get(module.getProject().getBasePath(), "app", "src", "main", "res");
 
     @Language("XML")
     String contents = "<resources>\n" +
                       "    <string name=\"key_1\">key_1_default</string>\n" +
                       "</resources>\n";
 
-    TestFileUtils.writeFileAndRefreshVfs(fileSystem.getPath(res.toString(), "values", "strings.xml"), contents);
-    StringResourceEditor translationsEditor = new StringResourceEditor(StringsVirtualFile.getStringsVirtualFile(module));
+    TestFileUtils.writeFileAndRefreshVfs(myRes.resolve(Paths.get("values", "strings.xml")), contents);
 
-    try {
-      Application application = ApplicationManager.getApplication();
-      StringResourceViewPanel panel = translationsEditor.getPanel();
-      FrozenColumnTable table = panel.getTable();
+    myEditor = new StringResourceEditor(StringsVirtualFile.getStringsVirtualFile(module));
+    myPanel = myEditor.getPanel();
+    myTable = myPanel.getTable();
+  }
 
-      application.invokeAndWait(() -> {
-        Utils.loadResources(panel, res);
-        table.editCellAt(0, StringResourceTableModel.KEY_COLUMN);
+  @After
+  public void tearDown() {
+    Disposer.dispose(myEditor);
+  }
 
-        StringTableCellEditor cellEditor = (StringTableCellEditor)table.getCellEditor();
-        cellEditor.setCellEditorValue("key_2");
-        cellEditor.stopCellEditing();
-      });
+  @Test
+  public void reusedColumnHeaderValuesAreCleared() {
+    myApplication.invokeAndWait(() -> {
+      Utils.loadResources(myPanel, myRes);
 
-      application.invokeAndWait(() -> assertEquals("key_2", table.getValueAt(0, StringResourceTableModel.KEY_COLUMN)));
+      AddLocaleAction action = myPanel.getAddLocaleAction();
+      action.createItem(Locale.create("b+ace"));
+      Utils.loadResources(myPanel, myRes);
 
-      application.invokeAndWait(() -> {
-        table.editCellAt(0, StringResourceTableModel.DEFAULT_VALUE_COLUMN);
+      action.createItem(Locale.create("ab"));
+      Utils.loadResources(myPanel, myRes);
+    });
 
-        StringTableCellEditor cellEditor = (StringTableCellEditor)table.getCellEditor();
-        cellEditor.setCellEditorValue("key_2_default");
-        cellEditor.stopCellEditing();
-      });
+    TableColumnModel model = myTable.getScrollableTable().getColumnModel();
 
-      application.invokeAndWait(() -> assertEquals("key_2_default", table.getValueAt(0, StringResourceTableModel.DEFAULT_VALUE_COLUMN)));
-    }
-    finally {
-      Disposer.dispose(translationsEditor);
-    }
+    Object values = IntStream.range(0, model.getColumnCount())
+                             .mapToObj(model::getColumn)
+                             .map(TableColumn::getHeaderValue)
+                             .collect(Collectors.toList());
+
+    assertEquals(Arrays.asList("Untranslatable", "Default Value", "Abkhazian (ab)", "Achinese (ace)"), values);
+  }
+
+  @Test
+  public void setKeyName() {
+    myApplication.invokeAndWait(() -> {
+      Utils.loadResources(myPanel, myRes);
+      myTable.editCellAt(0, StringResourceTableModel.KEY_COLUMN);
+
+      StringTableCellEditor cellEditor = (StringTableCellEditor)myTable.getCellEditor();
+      cellEditor.setCellEditorValue("key_2");
+      cellEditor.stopCellEditing();
+    });
+
+    myApplication.invokeAndWait(() -> assertEquals("key_2", myTable.getValueAt(0, StringResourceTableModel.KEY_COLUMN)));
+
+    myApplication.invokeAndWait(() -> {
+      myTable.editCellAt(0, StringResourceTableModel.DEFAULT_VALUE_COLUMN);
+
+      StringTableCellEditor cellEditor = (StringTableCellEditor)myTable.getCellEditor();
+      cellEditor.setCellEditorValue("key_2_default");
+      cellEditor.stopCellEditing();
+    });
+
+    myApplication.invokeAndWait(() -> assertEquals("key_2_default", myTable.getValueAt(0, StringResourceTableModel.DEFAULT_VALUE_COLUMN)));
   }
 }
