087afa7d93a2fb5a2effcf67f94ea09591976255
Renaud Paquay <rpaquay@google.com>
2018-07-30 08:25:12 -0700

    Display app bundle version info in APK Viewer
    Bug: 111978531
    Test: In follow up CL (want to avoid too much code churn here)
    Change-Id: I4a32eca5f129ba6295377f3bdae1a28d6a6a2538

--- apkanalyzer/src/com/android/tools/idea/apk/viewer/ApkParser.java
+++ apkanalyzer/src/com/android/tools/idea/apk/viewer/ApkParser.java
@@ -18,7 +18,10 @@ package com.android.tools.idea.apk.viewer;
 import com.android.SdkConstants;
 import com.android.annotations.NonNull;
 import com.android.ide.common.process.ProcessException;
+import com.android.ide.common.xml.AndroidManifestParser;
+import com.android.ide.common.xml.ManifestData;
 import com.android.tools.apk.analyzer.*;
+import com.android.tools.apk.analyzer.internal.AppBundleArtifact;
 import com.android.tools.idea.log.LogWrapper;
 import com.google.common.base.Function;
 import com.google.common.util.concurrent.Futures;
@@ -30,10 +33,12 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.jetbrains.ide.PooledThreadExecutor;
 
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
 
 public class ApkParser {
@@ -135,20 +140,47 @@ public class ApkParser {
       return AndroidApplicationInfo.UNKNOWN;
     }
     try {
-      AaptInvoker invoker = new AaptInvoker(pathToAapt, new LogWrapper(ApkParser.class));
-
-      File archiveFile = archiveEntry.getArchive().getPath().toFile();
-      // Note: aapt paths don't start with a "/"
-      String entryPath = archiveEntry.getPath().toString();
-      if (entryPath.startsWith("/")) {
-        entryPath = entryPath.substring(1);
+      if (archiveEntry.getArchive() instanceof AppBundleArtifact) {
+        return getAppInfoFromAppBundle(archiveEntry);
       }
-      List<String> xmlTree = invoker.getXmlTree(archiveFile, entryPath);
-      return AndroidApplicationInfo.parse(xmlTree);
+      else {
+        return getAppInfoFromApk(pathToAapt, archiveEntry);
+      }
     }
     catch (Throwable e) {
-      Logger.getInstance(ApkViewPanel.class).warn("Unable to run aapt", e);
+      Logger.getInstance(ApkViewPanel.class).warn("Unable to retrieve application info from artifact", e);
       return AndroidApplicationInfo.UNKNOWN;
     }
+  }
+
+  private static AndroidApplicationInfo getAppInfoFromAppBundle(ArchiveEntry entry) throws Exception {
+    // Note: This code below is a little convoluted, it would be better to update AndroidManifestParser
+    //       to support ProtoXmlPullParser, which can decode xml protobuf streams automatically.
+
+    // Decode XML protobuf into XML document string representation
+    byte[] content = Files.readAllBytes(entry.getPath());
+    String doc = new ProtoXmlPrettyPrinterImpl().prettyPrint(content);
+
+    // Parse the XML string into manifest data and extract
+    byte[] decodedContent = doc.getBytes(StandardCharsets.UTF_8);
+    try (ByteArrayInputStream stream = new ByteArrayInputStream(decodedContent)) {
+      ManifestData data = AndroidManifestParser.parse(stream);
+      return new AndroidApplicationInfo(data.getPackage(), data.getVersionName(), data.getVersionCode());
+    }
+  }
+
+  @NotNull
+  private static AndroidApplicationInfo getAppInfoFromApk(@NonNull Path pathToAapt, @NotNull ArchiveEntry archiveEntry)
+    throws ProcessException {
+    AaptInvoker invoker = new AaptInvoker(pathToAapt, new LogWrapper(ApkParser.class));
+
+    File archiveFile = archiveEntry.getArchive().getPath().toFile();
+    // Note: aapt paths don't start with a "/"
+    String entryPath = archiveEntry.getPath().toString();
+    if (entryPath.startsWith("/")) {
+      entryPath = entryPath.substring(1);
+    }
+    List<String> xmlTree = invoker.getXmlTree(archiveFile, entryPath);
+    return AndroidApplicationInfo.parse(xmlTree);
   }
 }
