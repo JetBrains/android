fb929678ad1b761cc816f77d05489920c951506d
Amaury Medeiros <amaurym@google.com>
2018-07-31 18:29:32 +0100

    Introduce StatusPane to CpuCaptureView
    The new CPU profiler recording flow includes a view in the CPU details
    panel displaying the current status of the profiler (e.g. recording,
    parsing) in order to increase discoverability.
    
    In this first pass, we are introducing this panel for the recording state.
    Later, we will expand the solution to include the parsing state as well.
    
    Bug: 111779496
    Test: CpuCaptureViewTest#whenRecordingThereShouldBeInstanceOfRecordingPane.
          Updated CpuProfilerStageViewTest#recordButtonDisabledInDeadSessions as
          well.
    
    Change-Id: I87214d0b119f12062aa955dda3a7eb58fd773156

---
+++ profilers-ui/src/com/android/tools/profilers/cpu/capturedetails/StatusPane.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.profilers.cpu.capturedetails;
+
+import com.android.tools.adtui.TabularLayout;
+import com.android.tools.profilers.ProfilerColors;
+import com.android.tools.profilers.ProfilerFonts;
+import com.android.tools.profilers.cpu.CpuProfilerStageView;
+import com.intellij.util.ui.JBUI;
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
+import javax.swing.border.LineBorder;
+import java.awt.*;
+
+/**
+ * A {@link CapturePane} used to display the current state of the profiler (e.g. parsing, recording), its elapsed time, and a button to
+ * interrupt it.
+ */
+abstract class StatusPane extends CapturePane {
+
+  /**
+   * Status itself (e.g. recording, parsing).
+   */
+  @NotNull
+  private String myStatus;
+
+  /**
+   * How much time has elapsed since the profiler is in this state.
+   */
+  @NotNull
+  private JLabel myDurationLabel;
+
+  public StatusPane(@NotNull CpuProfilerStageView stageView, @NotNull String statusLabel) {
+    super(stageView);
+    myStatus = statusLabel;
+    myDurationLabel = createLabel("", false);
+    disableInteraction();
+    updateView();
+  }
+
+  @Override
+  void populateContent(@NotNull JPanel panel) {
+    // We need an outer panel, as the given panel uses BorderLayout and trying to add a fixed size box to it would make it to occupy the
+    // entire panel area.
+    JPanel mainPanel = new JPanel(new TabularLayout("*,300px,*", "*,150px,*"));
+    // TODO(b/109661512): Move vgap scale into TabularLayout
+    JPanel statusPanel = new JPanel(new TabularLayout("*,Fit,20px,Fit,*","*,Fit,Fit,Fit,*").setVGap(JBUI.scale(5)));
+    statusPanel.setBorder(new LineBorder(ProfilerColors.CPU_CAPTURE_STATUS, 1));
+
+    JLabel status = createLabel("Status", true);
+    JLabel actualStatus = createLabel(myStatus, false);
+    JLabel duration = createLabel("Duration", true);
+
+    statusPanel.add(status, new TabularLayout.Constraint(1, 1));
+    statusPanel.add(actualStatus, new TabularLayout.Constraint(1, 3));
+    statusPanel.add(duration, new TabularLayout.Constraint(2, 1));
+    statusPanel.add(myDurationLabel, new TabularLayout.Constraint(2, 3));
+    // Adds the button centralized in the 3 middle columns (2nd to 4th).
+    statusPanel.add(createButtonPanel(new TabularLayout("*,Fit,*")), new TabularLayout.Constraint(3, 1, 3));
+
+    mainPanel.add(statusPanel, new TabularLayout.Constraint(1, 1));
+    panel.add(mainPanel, BorderLayout.CENTER);
+  }
+
+  void updateDuration() {
+    myDurationLabel.setText(getDurationText());
+  }
+
+  private static JLabel createLabel(String text, boolean isRightAligned) {
+    JLabel label = new JLabel(text, isRightAligned ? SwingConstants.RIGHT : SwingConstants.LEFT);
+    label.setFont(ProfilerFonts.STANDARD_FONT);
+    label.setForeground(ProfilerColors.CPU_CAPTURE_STATUS);
+    label.setBorder(JBUI.Borders.empty());
+    return label;
+  }
+
+  private JPanel createButtonPanel(TabularLayout layout) {
+    JPanel panel = new JPanel(layout);
+    panel.add(createAbortButton(), new TabularLayout.Constraint(0, 1));
+    return panel;
+  }
+
+  /**
+   * Creates a {@link JButton} used to interrupt the current status and close the pane.
+   */
+  protected abstract JButton createAbortButton();
+
+  @NotNull
+  protected abstract String getDurationText();
+}
