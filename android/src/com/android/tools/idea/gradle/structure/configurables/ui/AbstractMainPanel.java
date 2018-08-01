97395408d38f234c38765258685ea0d806981c9a
Yuriy Solodkyy <solodkyy@google.com>
2018-08-01 15:08:47 +0100

    [New PSD] Extract module selector drop-down.
    It is not a responsibility of a module level configurable to change the
    currently selected module.
    
    This CL extracts the module selector drop-down and instantiates a single
    instance of the drop-down at the BasePerspectiveConfigurable level which
    is responsible for changing of the currently selected module.
    
    Bug: 112032657
    Bug: 79560502
    Test: (existing UI test)
    Change-Id: I456214d243f92741298b5cbe8062db8f0a851526

--- android/src/com/android/tools/idea/gradle/structure/configurables/ui/AbstractMainPanel.java
+++ android/src/com/android/tools/idea/gradle/structure/configurables/ui/AbstractMainPanel.java
@@ -18,12 +18,7 @@ package com.android.tools.idea.gradle.structure.configurables.ui;
 import com.android.tools.idea.gradle.structure.configurables.PsContext;
 import com.android.tools.idea.gradle.structure.model.PsModule;
 import com.android.tools.idea.gradle.structure.model.PsProject;
-import com.intellij.icons.AllIcons;
 import com.intellij.openapi.Disposable;
-import com.intellij.openapi.actionSystem.*;
-import com.intellij.openapi.project.DumbAwareAction;
-import com.intellij.ui.IdeBorderFactory;
-import com.intellij.ui.SideBorder;
 import com.intellij.ui.navigation.History;
 import com.intellij.ui.navigation.Place;
 import org.jetbrains.annotations.NotNull;
@@ -34,14 +29,10 @@ import java.awt.*;
 import java.util.Collections;
 import java.util.List;
 
-import static com.android.tools.idea.gradle.structure.configurables.ui.UiUtil.revalidateAndRepaint;
-
 public abstract class AbstractMainPanel extends JPanel implements Disposable, Place.Navigator, CrossModuleUiStateComponent {
   @NotNull private final PsProject myProject;
   @NotNull private final PsContext myContext;
 
-  private boolean myShowModulesDropDown;
-  private JComponent myModulesToolbar;
   private History myHistory;
 
   protected AbstractMainPanel(@NotNull PsContext context) {
@@ -52,52 +43,6 @@ public abstract class AbstractMainPanel extends JPanel implements Disposable, Pl
     super(new BorderLayout());
     myProject = context.getProject();
     myContext = context;
-
-    myShowModulesDropDown = context.getUiSettings().MODULES_LIST_MINIMIZE;
-    if (myShowModulesDropDown) {
-      createAndAddModulesAction(extraModules);
-    }
-    context.getUiSettings().addListener(settings -> {
-      if (settings.MODULES_LIST_MINIMIZE != myShowModulesDropDown) {
-        myShowModulesDropDown = settings.MODULES_LIST_MINIMIZE;
-        if (myShowModulesDropDown) {
-          createAndAddModulesAction(extraModules);
-        }
-        else {
-          removeModulesAction();
-        }
-      }
-    }, this);
-  }
-
-  private void createAndAddModulesAction(@NotNull List<PsModule> extraModules) {
-    DefaultActionGroup actions = new DefaultActionGroup();
-    actions.add(new ModulesComboBoxAction(myContext, extraModules));
-
-    AnAction restoreModuleListAction = new DumbAwareAction("Restore 'Modules' List", "", AllIcons.Actions.MoveTo2) {
-      @Override
-      public void actionPerformed(AnActionEvent e) {
-        PsUISettings settings = myContext.getUiSettings();
-        settings.MODULES_LIST_MINIMIZE = myShowModulesDropDown = false;
-        settings.fireUISettingsChanged();
-        removeModulesAction();
-      }
-    };
-    actions.add(restoreModuleListAction);
-
-    ActionToolbar toolbar = ActionManager.getInstance().createActionToolbar("TOP", actions, true);
-    myModulesToolbar = toolbar.getComponent();
-    myModulesToolbar.setBorder(IdeBorderFactory.createBorder(SideBorder.BOTTOM));
-
-    add(myModulesToolbar, BorderLayout.NORTH);
-  }
-
-  private void removeModulesAction() {
-    if (myModulesToolbar != null) {
-      remove(myModulesToolbar);
-      myModulesToolbar = null;
-      revalidateAndRepaint(this);
-    }
   }
 
   @NotNull
