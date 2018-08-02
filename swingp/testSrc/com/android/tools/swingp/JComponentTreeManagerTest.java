b6e533f30d9e12a6daf9fed8f26036f94469ef7a
Chester Hsieh <dunno@google.com>
2018-08-01 23:44:37 -0700

    Add Swing tree serialization.
    This CL adds path-to-root serialization with respect to the Swing component
    hierarchy.
    
    Note that each node only serializes a path fragment from itself to the last
    parent component that was also captured by swingp (there are exceptions to this,
    such as JComponents rendered by the InstructionsRenderer). Therefore, the
    whole swingp hierarchy is needed to determine the hierarchy of any individual
    JComponent that was captured by swingp.
    
    This CL also added example filtering using serialization to visualizer.
    
    Bug: 80499904
    Change-Id: Iabf7a21a0e0355e334ef4b61d0aae22c529f43b6
    Test: Added tests.

---
+++ swingp/testSrc/com/android/tools/swingp/JComponentTreeManagerTest.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.swingp;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.swing.*;
+
+import static com.android.tools.swingp.JComponentTreeManager.*;
+
+public class JComponentTreeManagerTest {
+  @Before
+  public void setup() {
+    setEnabled(true);
+  }
+
+  @After
+  public void teardown() {
+    clear();
+    setEnabled(false);
+  }
+
+  @Test(expected = RuntimeException.class)
+  public void testPopOnEmpty() {
+    JComponent component = new JPanel();
+    popJComponent(component);
+  }
+
+  @Test(expected = RuntimeException.class)
+  public void testDoublePopToEmpty() {
+    JComponent component = new JPanel();
+    pushJComponent(component);
+    popJComponent(component);
+    popJComponent(component);
+  }
+
+  @Test(expected = RuntimeException.class)
+  public void testDoublePop() {
+    JComponent component = new JPanel();
+    JComponent childComponent = new JPanel();
+    component.add(childComponent);
+
+    pushJComponent(component);
+    pushJComponent(childComponent);
+    popJComponent(childComponent);
+    popJComponent(childComponent);
+  }
+
+  @Test(expected = RuntimeException.class)
+  public void testMissingPop() {
+    JComponent component = new JPanel();
+    JComponent childComponent = new JPanel();
+    component.add(childComponent);
+
+    pushJComponent(component);
+    pushJComponent(childComponent);
+    popJComponent(component);
+  }
+
+  @Test
+  public void testCorrectBehavior() {
+    JComponent component = new JPanel();
+    JComponent childComponent = new JPanel();
+    component.add(childComponent);
+
+    pushJComponent(component);
+    popJComponent(component);
+
+    pushJComponent(component);
+    pushJComponent(childComponent);
+    popJComponent(childComponent);
+    popJComponent(component);
+  }
+}
