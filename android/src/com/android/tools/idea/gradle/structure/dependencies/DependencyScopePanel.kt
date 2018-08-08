eaf9020da625adcea58f5fb56d6d74281fb307b9
Yuriy Solodkyy <solodkyy@google.com>
2018-08-08 17:38:47 +0100

    [New PSD] Support completion in scope selector.
    Bug: 79566097
    Test: future UI tests
    Change-Id: Iac7681cee50968f7e5c85423867bff77fc32a12f

--- android/src/com/android/tools/idea/gradle/structure/dependencies/DependencyScopePanel.kt
+++ android/src/com/android/tools/idea/gradle/structure/dependencies/DependencyScopePanel.kt
@@ -16,31 +16,48 @@
 package com.android.tools.idea.gradle.structure.dependencies
 
 import com.android.tools.idea.gradle.structure.model.PsModule
-import com.intellij.openapi.ui.ComboBox
+import com.intellij.codeInsight.completion.PrefixMatcher
+import com.intellij.codeInsight.completion.impl.CamelHumpMatcher
+import com.intellij.openapi.fileTypes.PlainTextLanguage
+import com.intellij.openapi.fileTypes.StdFileTypes
 import com.intellij.openapi.ui.ValidationInfo
+import com.intellij.ui.EditorComboBox
+import com.intellij.ui.LanguageTextField
+import com.intellij.ui.TextFieldWithAutoCompletion
+import com.intellij.util.textCompletion.TextCompletionUtil
 
 class DependencyScopePanel(module: PsModule) : AbstractDependencyScopesPanel() {
 
-  private val instructions: String =
-    """Assign a scope to the new dependency by selecting the configurations below.<br/><a
-      |href='https://docs.gradle.org/current/userguide/artifact_dependencies_tutorial.html'>Open Documentation</a>"""
-
-  val configurations = module.getConfigurations()
-  val comboBox = ComboBox<String>().apply {
-    configurations.forEach { addItem(it) }
-  }
+  val configurations = module.getConfigurations().toSet()
+  val comboBox = createComboBox(module)
 
   init {
-    comboBox.selectedItem = configurations.firstOrNull()
-    setUpContents(comboBox, instructions)
+    setUpContents(comboBox, INSTRUCTIONS)
   }
 
   override fun validateInput(): ValidationInfo? = when {
-    (comboBox.selectedItem as? String).isNullOrEmpty() -> ValidationInfo("Please select at least one configuration", comboBox)
+    comboBox.text.isNullOrEmpty() -> ValidationInfo("Please select at least one configuration", comboBox)
+    !configurations.contains(comboBox.text) ->
+      ValidationInfo("'${comboBox.text}' is not a valid configuration name", comboBox)
     else -> null
   }
 
-  override val selectedScopeName: String get() = comboBox.selectedItem as String
-
+  override val selectedScopeName: String get() = comboBox.text
   override fun dispose() {}
+}
+
+private const val INSTRUCTIONS =
+    """Assign a scope to the new dependency by selecting the configurations below.<br/><a
+      |href='https://docs.gradle.org/current/userguide/artifact_dependencies_tutorial.html'>Open Documentation</a>"""
+
+private fun createComboBox(module: PsModule): EditorComboBox {
+  val configurations = module.getConfigurations()
+  val completionProvider = object : TextFieldWithAutoCompletion.StringsCompletionProvider(configurations, null) {
+    override fun createPrefixMatcher(prefix: String): PrefixMatcher = CamelHumpMatcher(prefix)
+  }
+  val documentCreator = TextCompletionUtil.DocumentWithCompletionCreator(completionProvider, true)
+  val initialSelection = configurations.firstOrNull()
+  val document = LanguageTextField.createDocument(initialSelection, PlainTextLanguage.INSTANCE, module.parent.ideProject, documentCreator)
+  return EditorComboBox(document, module.parent.ideProject, StdFileTypes.PLAIN_TEXT)
+    .apply { configurations.forEach { addItem(it) } }
 }
