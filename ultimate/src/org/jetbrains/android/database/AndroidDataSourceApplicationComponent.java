39d61a9fcd20f4985b142463b2f4896a13e5e961
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2018-08-08 15:06:14 +0200

    get rid of ApplicationComponent interface, applicationInitializedListener EP (part 1)
    (cherry picked from commit a57fd4c)

--- ultimate/src/org/jetbrains/android/database/AndroidDataSourceApplicationComponent.java
+++ ultimate/src/org/jetbrains/android/database/AndroidDataSourceApplicationComponent.java
@@ -1,13 +1,11 @@
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.android.database;
 
-import com.intellij.openapi.components.ApplicationComponent;
+import com.intellij.ide.ApplicationInitializedListener;
 
-/**
- * @author Eugene.Kudelevsky
- */
-public class AndroidDataSourceApplicationComponent implements ApplicationComponent {
+public class AndroidDataSourceApplicationComponent implements ApplicationInitializedListener {
   @Override
-  public void initComponent() {
+  public void componentsInitialized() {
     AndroidRemoteDataBaseManager.getInstance().processRemovedProjects();
   }
 }
