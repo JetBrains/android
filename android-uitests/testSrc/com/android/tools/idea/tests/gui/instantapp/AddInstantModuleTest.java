32ab1c8e27cfe7a0971384503fcc0f12e68cdc01
Jingjing Liu <jingjingliu@google.com>
2018-08-09 14:17:30 -0700

    Migrate a Fast test AddInstantModuleTest to bazel
    bug:na
    Test: Ran this test with bazel locally
    Change-Id: I95a749f8b9729accb19f9c9c42a8489eac845f12

--- android-uitests/testSrc/com/android/tools/idea/tests/gui/instantapp/AddInstantModuleTest.java
+++ android-uitests/testSrc/com/android/tools/idea/tests/gui/instantapp/AddInstantModuleTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017 The Android Open Source Project
+ * Copyright (C) 2018 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,7 +19,6 @@ import com.android.tools.idea.tests.gui.framework.GuiTestRule;
 import com.android.tools.idea.tests.gui.framework.RunIn;
 import com.android.tools.idea.tests.gui.framework.TestGroup;
 import com.android.tools.idea.tests.gui.framework.fixture.IdeFrameFixture;
-import com.android.tools.idea.tests.gui.framework.fixture.ProjectViewFixture;
 import com.android.tools.idea.tests.gui.framework.fixture.npw.NewModuleWizardFixture;
 import com.intellij.testGuiFramework.framework.GuiTestRemoteRunner;
 import org.junit.Rule;
@@ -29,43 +28,42 @@ import org.junit.runner.RunWith;
 import java.util.concurrent.TimeUnit;
 
 @RunWith(GuiTestRemoteRunner.class)
-public class AddModuleTest {
+public class AddInstantModuleTest {
+
   @Rule public final GuiTestRule guiTest = new GuiTestRule().withTimeout(5, TimeUnit.MINUTES);
 
   /**
-   * Verifies that user is able to add a Instant App Feature module through the
+   * Verifies that user is able to add a instant app module through the
    * new module wizard.
    *
-   * <p>TT ID: d239df75-a7fc-4327-a5af-d6b2f6caba11
+   * <p>TT ID: 6da70326-4b89-4f9b-9e08-573939bebfe5
    *
    * <pre>
    *   Test steps:
-   *   1. Import simple instant application project
+   *   1. Import simple local application project
    *   2. Go to File -> New module to open the new module dialog wizard.
-   *   3. Follow through the wizard to add a new Instant App Feature module, accepting defaults.
+   *   3. Follow through the wizard to add a new instant module, accepting defaults.
    *   4. Complete the wizard and wait for the build to complete.
    *   Verify:
-   *   1. The new Instant App Feature module's library is shown in the project explorer pane.
+   *   1. The new instant module's library is shown in the project explorer pane.
    * </pre>
    */
   @Test
-  @RunIn(TestGroup.QA)
-  public void addFeatureModule() throws Exception {
-    IdeFrameFixture ideFrame = guiTest.importProjectAndWaitForProjectSyncToFinish("TopekaInstantApp");
+  @RunIn(TestGroup.FAST_BAZEL)
+  public void addInstantModule() throws Exception {
+    IdeFrameFixture ideFrame = guiTest.importSimpleLocalApplication();
 
     ideFrame.invokeMenuPath("File", "New", "New Module...");
 
-    NewModuleWizardFixture newModDialog = NewModuleWizardFixture.find(ideFrame);
-
-    newModDialog.chooseModuleType("Instant App Feature Module")
-      .clickNextToStep("Creates a new Android Instant App Feature module.")
-      .clickNextToStep("Add an Activity to Mobile")
-      .clickNextToStep("Configure Activity")
+    NewModuleWizardFixture.find(ideFrame)
+      .chooseModuleType("Instant App")
+      .clickNextToStep("Configure your module")
       .clickFinish();
 
     ideFrame.waitForGradleProjectSyncToFinish();
 
-    ProjectViewFixture.PaneFixture androidPane = ideFrame.getProjectView().selectAndroidPane();
-    androidPane.clickPath("feature");
+    ideFrame.getProjectView()
+      .selectAndroidPane()
+      .clickPath("instantapp");
   }
 }
