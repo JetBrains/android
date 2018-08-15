484aa519c0da46ab07aa90b1c6c6a57c4d963860
develar <develar@gmail.com>
2018-08-15 18:36:14 +0200

    apply changes to before run tasks only on apply

--- android/src/com/android/tools/idea/run/editor/ApplicationRunParameters.java
+++ android/src/com/android/tools/idea/run/editor/ApplicationRunParameters.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2010 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 
 package com.android.tools.idea.run.editor;
 
@@ -42,6 +28,7 @@ import com.intellij.packaging.impl.run.BuildArtifactsBeforeRunTaskProvider;
 import com.intellij.ui.CollectionComboBoxModel;
 import com.intellij.ui.ListCellRendererWrapper;
 import com.intellij.ui.components.JBTextField;
+import com.intellij.util.SmartList;
 import org.jetbrains.android.compiler.artifact.AndroidApplicationArtifactType;
 import org.jetbrains.annotations.NotNull;
 
@@ -49,7 +36,9 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.*;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 import static com.android.builder.model.AndroidProject.PROJECT_TYPE_INSTANTAPP;
@@ -269,8 +258,7 @@ public class ApplicationRunParameters<T extends AndroidRunConfiguration> impleme
     if (editor == null) {
       return;
     }
-    final List<BeforeRunTask> makeTasks = new ArrayList<BeforeRunTask>();
-
+    final List<BeforeRunTask> makeTasks = new SmartList<>();
     for (BeforeRunTask task : editor.getStepsBeforeLaunch()) {
       if (task instanceof CompileStepBeforeRun.MakeBeforeRunTask ||
           task instanceof CompileStepBeforeRunNoErrorCheck.MakeBeforeRunTaskNoErrorCheck) {
@@ -298,7 +286,7 @@ public class ApplicationRunParameters<T extends AndroidRunConfiguration> impleme
   private Collection<? extends Artifact> getAndroidArtifacts() {
     final ArtifactManager artifactManager = ArtifactManager.getInstance(myProject);
     return artifactManager == null
-           ? Collections.<Artifact>emptyList()
+           ? Collections.emptyList()
            : artifactManager.getArtifactsByType(AndroidApplicationArtifactType.getInstance());
   }
 
