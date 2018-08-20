09e9b5de5b33b937973ecded848c846bddcbab41
Juan Chen <juanch@google.com>
2018-08-20 10:01:10 -0700

    Update Kotlin dependencies to 1.2.61.
    Bug: n/a
    Test: existing
    Change-Id: Ic7e777e5858fa7ff9172c3cb76864772ebca7188

--- android/testData/projects/kotlinGradleDsl/app/build.gradle.kts
+++ android/testData/projects/kotlinGradleDsl/app/build.gradle.kts
@@ -30,7 +30,7 @@ configure<AppExtension> {
 dependencies {
     "implementation"("com.android.support:appcompat-v7:27.1.1")
     "implementation"("com.android.support.constraint:constraint-layout:1.0.2")
-    "implementation"(kotlin("stdlib", "1.2.60"))
+    "implementation"(kotlin("stdlib", "1.2.61"))
     "implementation"(project(":lib"))
 }
 
