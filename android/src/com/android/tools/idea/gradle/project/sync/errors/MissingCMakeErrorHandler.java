2cf6f3f58ec71f0e24072498ffbf4028f09934fc
Emre Kultursay <emrekultursay@google.com>
2018-08-20 21:45:07 -0400

    Display link to install specific version of CMake.
    When the user asks for a specific version of CMake that is available in
    the SDK, but not installed locally, detect the error that gets printed,
    parse the requested CMake version, and then display a hyperlink to
    install that specific version.
    
    Do not display link if the requested version does not exist as a
    remote package. That would be useless as the install would fail
    to find the package anyways.
    
    Do not display link if the requested version exists as a local package.
    This protect against version matching errors inside gradle plugin side
    from corrupting the behavior here.
    
    Bug: 111690559
    Test: Added unit tests for MissingCmakeErrorHandler methods.
    
    Change-Id: Ice7e1ba7b20bc417e94ddcc2889b8126554c5e4c
    Signed-off-by: Emre Kultursay <emrekultursay@google.com>

--- android/src/com/android/tools/idea/gradle/project/sync/errors/MissingCMakeErrorHandler.java
+++ android/src/com/android/tools/idea/gradle/project/sync/errors/MissingCMakeErrorHandler.java
@@ -15,18 +15,28 @@
  */
 package com.android.tools.idea.gradle.project.sync.errors;
 
+import com.android.repository.Revision;
+import com.android.repository.api.LocalPackage;
+import com.android.repository.api.RemotePackage;
+import com.android.repository.api.RepoManager;
+import com.android.sdklib.repository.AndroidSdkHandler;
 import com.android.tools.idea.gradle.project.sync.hyperlink.InstallCMakeHyperlink;
 import com.android.tools.idea.project.hyperlink.NotificationHyperlink;
+import com.android.tools.idea.sdk.AndroidSdks;
+import com.android.tools.idea.sdk.progress.StudioLoggerProgressIndicator;
 import com.intellij.openapi.project.Project;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
+import static com.android.SdkConstants.FD_CMAKE;
 import static com.intellij.openapi.util.text.StringUtil.isNotEmpty;
 
 public class MissingCMakeErrorHandler extends BaseSyncErrorHandler {
+
   @Nullable
   @Override
   protected String findErrorMessage(@NotNull Throwable rootCause, @NotNull Project project) {
@@ -37,6 +47,10 @@ public class MissingCMakeErrorHandler extends BaseSyncErrorHandler {
         updateUsageTracker();
         return "Failed to find CMake.";
       }
+      else if (isCannotFindCmakeVersionError(firstLine)) {
+        updateUsageTracker();
+        return message;
+      }
     }
     return null;
   }
@@ -44,6 +58,150 @@ public class MissingCMakeErrorHandler extends BaseSyncErrorHandler {
   @Override
   @NotNull
   protected List<NotificationHyperlink> getQuickFixHyperlinks(@NotNull Project project, @NotNull String text) {
-    return Collections.singletonList(new InstallCMakeHyperlink());
+    String firstLine = getFirstLineMessage(text);
+    if (isCannotFindCmakeVersionError(firstLine)) {
+      // The user requested a specific version of CMake.
+      Revision requestedCmakeVersion = extractCmakeVersionFromError(firstLine);
+      if (requestedCmakeVersion == null) {
+        // Cannot parse the CMake version from the error string.
+        return Collections.EMPTY_LIST;
+      }
+
+      RepoManager sdkManager = getSdkManager();
+      Collection<RemotePackage> remoteCmakePackages = sdkManager.getPackages().getRemotePackagesForPrefix(FD_CMAKE);
+      Revision foundCmakeVersion = findBestMatch(remoteCmakePackages, requestedCmakeVersion);
+      if (foundCmakeVersion == null) {
+        // The requested CMake version was not found in the SDK. Adding a hyperlink is useless as it
+        // will only fail to install the package.
+        return Collections.EMPTY_LIST;
+      }
+
+      Collection<LocalPackage> localCmakePackages = sdkManager.getPackages().getLocalPackagesForPrefix(FD_CMAKE);
+      if (isAlreadyInstalled(localCmakePackages, foundCmakeVersion)) {
+        // Failed sanity check. The package is already installed.
+        return Collections.EMPTY_LIST;
+      }
+
+      // Version-specific install of CMake.
+      return Collections.singletonList(new InstallCMakeHyperlink(foundCmakeVersion));
+    }
+    else {
+      // Generic install of CMAke.
+      return Collections.singletonList(new InstallCMakeHyperlink());
+    }
+  }
+
+  /**
+   * @param firstLine the first line of the error message returned by gradle sync.
+   * @return true if input looks like it is an error about not finding a particular version of CMake.
+   **/
+  private static boolean isCannotFindCmakeVersionError(@NotNull String firstLine) {
+    return firstLine.startsWith("CMake") && firstLine.contains("was not found in PATH or by cmake.dir property");
+  }
+
+  /**
+   * @param firsLine The line inside which the cmake version will be searched.
+   * @return The cmake version string included in the error message, null if it's not found or cannot be parsed as a valid revision.
+   **/
+  @Nullable
+  static Revision extractCmakeVersionFromError(@NotNull String firstLine) {
+    int startPos = firstLine.indexOf('\'');
+    if (startPos == -1) {
+      return null;
+    }
+
+    int endPos = firstLine.indexOf('\'', startPos + 1);
+    if (endPos == -1) {
+      return null;
+    }
+
+    String version = firstLine.substring(startPos + 1, endPos);
+    try {
+      return Revision.parseRevision(version);
+    }
+    catch (NumberFormatException e) {
+      return null;
+    }
+  }
+
+  /**
+   * Finds whether the requested cmake version can be installed from the SDK.
+   *
+   * @param cmakePackages         Remote CMake packages available in the SDK.
+   * @param requestedCmakeVersion The CMake version requested by the user.
+   * @return The version that best matches the requested version, null if no match was found.
+   */
+  @Nullable
+  static Revision findBestMatch(@NotNull Collection<RemotePackage> cmakePackages, @NotNull Revision requestedCmakeVersion) {
+    int[] requestedParts = requestedCmakeVersion.toIntArray(true);
+
+    Revision foundVersion = null;
+    for (RemotePackage remotePackage : cmakePackages) {
+      Revision remoteCmakeVersion = remotePackage.getVersion();
+      int[] candidateParts = remoteCmakeVersion.toIntArray(true);
+
+      if (!versionSatisfies(candidateParts, requestedParts)) {
+        continue;
+      }
+
+      if (foundVersion == null) {
+        foundVersion = remoteCmakeVersion;
+        continue;
+      }
+
+      if (foundVersion.compareTo(remoteCmakeVersion) < 0) {
+        // Among all matching Cmake versions, use the newest one.
+        foundVersion = remoteCmakeVersion;
+        continue;
+      }
+    }
+
+    return foundVersion;
+  }
+
+  /**
+   * @param candidateParts the components of a cmake version that is available in the SDK.
+   * @param requestedParts the components of a cmake version that we are looking for.
+   * @return true if the version represented by candidateParts is a good match for the version represented by requestedParts.
+   */
+  static boolean versionSatisfies(@NotNull int[] candidateParts, @NotNull int[] requestedParts) {
+    if (candidateParts.length < requestedParts.length) {
+      // Request is more specific than the candidate: 3.10 cannot satisfy 3.10.2 request.
+      return false;
+    }
+
+    for (int i = 0; i < requestedParts.length; ++i) {
+      if (requestedParts[i] != candidateParts[i]) {
+        return false;
+      }
+    }
+
+    // Either full match, or a more specific version than the request was found (e.g., 3.10.2 satisfies 3.10).
+    return true;
+  }
+
+  /**
+   * @param cmakePackages local CMake installations available in the SDK.
+   * @param cmakeVersion  the cmake version that we are looking for.
+   * @return true if a package with the given version exists in cmakePackages.
+   */
+  private static boolean isAlreadyInstalled(@NotNull Collection<LocalPackage> cmakePackages, @NotNull Revision cmakeVersion) {
+    for (LocalPackage localCmakePackage : cmakePackages) {
+      if (localCmakePackage.getVersion().equals(cmakeVersion)) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  /**
+   * @return The currently available SDK manager.
+   */
+  @NotNull
+  protected RepoManager getSdkManager() {
+    AndroidSdkHandler sdkHandler = AndroidSdks.getInstance().tryToChooseSdkHandler();
+    StudioLoggerProgressIndicator progressIndicator = new StudioLoggerProgressIndicator(getClass());
+    return sdkHandler.getSdkManager(progressIndicator);
   }
 }
--- android/testSrc/com/android/tools/idea/gradle/project/sync/errors/MissingCMakeErrorHandlerTest.java
+++ android/testSrc/com/android/tools/idea/gradle/project/sync/errors/MissingCMakeErrorHandlerTest.java
@@ -15,12 +15,25 @@
  */
 package com.android.tools.idea.gradle.project.sync.errors;
 
+import com.android.repository.Revision;
+import com.android.repository.api.LocalPackage;
+import com.android.repository.api.RemotePackage;
+import com.android.repository.api.RepoManager;
+import com.android.repository.impl.meta.RepositoryPackages;
+import com.android.repository.testframework.FakePackage;
+import com.android.repository.testframework.FakeRepoManager;
 import com.android.tools.idea.gradle.project.sync.hyperlink.InstallCMakeHyperlink;
 import com.android.tools.idea.gradle.project.sync.messages.GradleSyncMessagesStub;
 import com.android.tools.idea.project.hyperlink.NotificationHyperlink;
 import com.android.tools.idea.testing.AndroidGradleTestCase;
+import com.intellij.openapi.command.impl.DummyProject;
+import com.intellij.openapi.externalSystem.model.ExternalSystemException;
+import org.jetbrains.annotations.NotNull;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static com.android.tools.idea.gradle.project.sync.SimulatedSyncErrors.registerSyncErrorToSimulate;
 import static com.android.tools.idea.testing.TestProjectPaths.SIMPLE_APPLICATION;
@@ -38,29 +51,205 @@ public class MissingCMakeErrorHandlerTest extends AndroidGradleTestCase {
     mySyncMessagesStub = GradleSyncMessagesStub.replaceSyncMessagesService(getProject());
   }
 
-  public void testHandleError1() throws Exception {
+  /**
+   * @param cmakeVersion The CMake version to use for the created local package.
+   * @return A fake local cmake package with the given version.
+   */
+  @NotNull
+  private LocalPackage createLocalPackage(@NotNull String cmakeVersion) {
+    Revision revision = Revision.parseRevision(cmakeVersion);
+    FakePackage.FakeLocalPackage pkg = new FakePackage.FakeLocalPackage("cmake;" + cmakeVersion);
+    pkg.setRevision(revision);
+    return pkg;
+  }
+
+  /**
+   * @param cmakeVersion The CMake version to use for the created remote package.
+   * @return A fake remote cmake package with the given version.
+   */
+  @NotNull
+  private RemotePackage createRemotePackage(@NotNull String cmakeVersion) {
+    Revision revision = Revision.parseRevision(cmakeVersion);
+    FakePackage.FakeRemotePackage pkg = new FakePackage.FakeRemotePackage("cmake;" + cmakeVersion);
+    pkg.setRevision(revision);
+    return pkg;
+  }
+
+  /**
+   * @param localPackages  The local packages to install to the fake SDK.
+   * @param remotePackages The remote packages to install to the fake SDK.
+   * @return An error handler with a fake SDK that contains the provided local and remote packages.
+   */
+  @NotNull
+  private MissingCMakeErrorHandler createHandler(@NotNull List<String> localPackages, @NotNull List<String> remotePackages) {
+    return new MissingCMakeErrorHandler() {
+      @Override
+      @NotNull
+      public RepoManager getSdkManager() {
+        return new FakeRepoManager(
+          null,
+          new RepositoryPackages(
+            localPackages.stream().map(p -> createLocalPackage(p)).collect(Collectors.toList()),
+            remotePackages.stream().map(p -> createRemotePackage(p)).collect(Collectors.toList()))
+        );
+      }
+    };
+  }
+
+  public void testIntegration() throws Exception {
+    // Verifies the integration of findErrorMessage and getQuickFixHyperlinks methods with gradle.
     String errMsg = "Failed to find CMake.";
     registerSyncErrorToSimulate(errMsg);
-    loadProjectAndExpectMissingCMakeError();
-  }
-
-  public void testHandleError2() throws Exception {
-    String errMsg = "Unable to get the CMake version located at: /Users/alruiz/Library/Android/sdk/cmake/bin";
-    registerSyncErrorToSimulate(errMsg);
-    loadProjectAndExpectMissingCMakeError();
-  }
-
-  private void loadProjectAndExpectMissingCMakeError() throws Exception {
     loadProjectAndExpectSyncError(SIMPLE_APPLICATION);
 
     GradleSyncMessagesStub.NotificationUpdate notificationUpdate = mySyncMessagesStub.getNotificationUpdate();
     assertNotNull(notificationUpdate);
 
     assertThat(notificationUpdate.getText()).isEqualTo("Failed to find CMake.");
+  }
 
-    // Verify hyperlinks are correct.
-    List<NotificationHyperlink> quickFixes = notificationUpdate.getFixes();
+  public void testFindErrorMessageNoCMakeVersion() throws Exception {
+    MissingCMakeErrorHandler handler = createHandler(Collections.emptyList(), Collections.emptyList());
+
+    String expectedMsg = "Failed to find CMake.";
+    assertEquals(
+      expectedMsg,
+      handler.findErrorMessage(
+        new ExternalSystemException("Failed to find CMake."), DummyProject.getInstance()));
+    assertEquals(
+      expectedMsg,
+      handler.findErrorMessage(
+        new ExternalSystemException("Unable to get the CMake version located at: /Users/alruiz/Library/Android/sdk/cmake/bin"),
+        DummyProject.getInstance()));
+  }
+
+  public void testFindErrorMessageWithCmakeVersion() throws Exception {
+    MissingCMakeErrorHandler handler = createHandler(Collections.emptyList(), Collections.emptyList());
+    String msg = "CMake '1.2.3' was not found in PATH or by cmake.dir property\n" +
+                 "- CMake '4.5.6' was found on PATH\n";
+    assertEquals(msg, handler.findErrorMessage(new ExternalSystemException(msg), DummyProject.getInstance()));
+  }
+
+  public void testDefaultInstall() throws Exception {
+    String errMsg = "Failed to find CMake";
+    MissingCMakeErrorHandler handler = createHandler(
+      Collections.emptyList(),
+      Collections.emptyList()
+    );
+
+    List<NotificationHyperlink> quickFixes = handler.getQuickFixHyperlinks(DummyProject.getInstance(), errMsg);
     assertThat(quickFixes).hasSize(1);
     assertThat(quickFixes.get(0)).isInstanceOf(InstallCMakeHyperlink.class);
+    assertThat(((InstallCMakeHyperlink)quickFixes.get(0)).getCmakeVersion()).isEqualTo(null);
+  }
+
+  public void testCannotParseCmakeVersion() throws Exception {
+    String errMsg = "CMake 'x.y.z' was not found in PATH or by cmake.dir property.";
+    MissingCMakeErrorHandler handler = createHandler(
+      Collections.emptyList(),
+      Collections.emptyList()
+    );
+
+    List<NotificationHyperlink> quickFixes = handler.getQuickFixHyperlinks(DummyProject.getInstance(), errMsg);
+    assertThat(quickFixes).hasSize(0);
+  }
+
+  public void testRemotePackageNotFound() throws Exception {
+    String errMsg = "CMake '3.7.0' was not found in PATH or by cmake.dir property.";
+    MissingCMakeErrorHandler handler = createHandler(
+      Collections.emptyList(),
+      Collections.emptyList()
+    );
+
+    List<NotificationHyperlink> quickFixes = handler.getQuickFixHyperlinks(DummyProject.getInstance(), errMsg);
+    assertThat(quickFixes).hasSize(0);
+  }
+
+  public void testAlreadyInstalledRemote() throws Exception {
+    String errMsg = "CMake '3.10.2' was not found in PATH or by cmake.dir property.";
+    MissingCMakeErrorHandler handler = createHandler(
+      Arrays.asList("3.10.2"),
+      Arrays.asList("3.10.2")
+    );
+
+    List<NotificationHyperlink> quickFixes = handler.getQuickFixHyperlinks(DummyProject.getInstance(), errMsg);
+    assertThat(quickFixes).hasSize(0);
+  }
+
+  public void testInstallFromRemote() throws Exception {
+    String errMsg = "CMake '3.10.2' was not found in PATH or by cmake.dir property.";
+
+    MissingCMakeErrorHandler handler = createHandler(
+      Arrays.asList("3.8"),
+      Arrays.asList("3.10.2")
+    );
+
+    List<NotificationHyperlink> quickFixes = handler.getQuickFixHyperlinks(DummyProject.getInstance(), errMsg);
+    assertThat(quickFixes).hasSize(1);
+    assertThat(quickFixes.get(0)).isInstanceOf(InstallCMakeHyperlink.class);
+    assertThat(((InstallCMakeHyperlink)quickFixes.get(0)).getCmakeVersion()).isEqualTo(Revision.parseRevision("3.10.2"));
+  }
+
+  public void testFindBestMatch() throws Exception {
+    // Requested version not found.
+    assertEquals(
+      null,
+      MissingCMakeErrorHandler.findBestMatch(
+        Arrays.asList(createRemotePackage("3.6"), createRemotePackage("3.8")),
+        Revision.parseRevision("3.7")));
+
+    // Exact match.
+    assertEquals(
+      Revision.parseRevision("3.8"),
+      MissingCMakeErrorHandler.findBestMatch(
+        Arrays.asList(createRemotePackage("3.8")),
+        Revision.parseRevision("3.8")));
+
+    // A more specific version found.
+    assertEquals(
+      Revision.parseRevision("3.8"),
+      MissingCMakeErrorHandler.findBestMatch(
+        Arrays.asList(createRemotePackage("3.8")),
+        Revision.parseRevision("3")));
+
+    // More specific match downstream.
+    assertEquals(
+      Revision.parseRevision("3.8.2"),
+      MissingCMakeErrorHandler.findBestMatch(
+        Arrays.asList(createRemotePackage("3.8"), createRemotePackage("3.8.2")),
+        Revision.parseRevision("3.8")));
+
+    // Request is too specific. Cannot satisfy.
+    assertEquals(
+      MissingCMakeErrorHandler.findBestMatch(
+        Arrays.asList(createRemotePackage("3.8"), createRemotePackage("3.10")),
+        Revision.parseRevision("3.8.2")),
+      null);
+  }
+
+  public void testVersionSatisfies() throws Exception {
+    // Exact match.
+    assertTrue(MissingCMakeErrorHandler.versionSatisfies(new int[]{3, 8, 0}, new int[]{3, 8, 0}));
+
+    // Request longer.
+    assertFalse(MissingCMakeErrorHandler.versionSatisfies(new int[]{3, 8}, new int[]{3, 8, 2}));
+
+    // Request shorter.
+    assertTrue(MissingCMakeErrorHandler.versionSatisfies(new int[]{3, 8, 0}, new int[]{3, 8}));
+
+    // Version mismatch.
+    assertFalse(MissingCMakeErrorHandler.versionSatisfies(new int[]{3, 8, 0}, new int[]{3, 10, 0}));
+  }
+
+  public void testExtractCmakeVersionFromError() throws Exception {
+    assertEquals(Revision.parseRevision("1.2.3-rc1"), MissingCMakeErrorHandler.extractCmakeVersionFromError("prefix '1.2.3-rc1' suffix"));
+    assertEquals(Revision.parseRevision("1.2.3-rc1"), MissingCMakeErrorHandler.extractCmakeVersionFromError("prefix'1.2.3-rc1'suffix"));
+    assertEquals(Revision.parseRevision("1.2.3-rc1"), MissingCMakeErrorHandler.extractCmakeVersionFromError("'1.2.3-rc1'"));
+
+    assertNull(MissingCMakeErrorHandler.extractCmakeVersionFromError(""));
+    assertNull(MissingCMakeErrorHandler.extractCmakeVersionFromError("does not have quoted substring"));
+    assertNull(MissingCMakeErrorHandler.extractCmakeVersionFromError("missing matching ' single quote"));
+    assertNull(MissingCMakeErrorHandler.extractCmakeVersionFromError("'"));
+    assertNull(MissingCMakeErrorHandler.extractCmakeVersionFromError("''"));
   }
 }
