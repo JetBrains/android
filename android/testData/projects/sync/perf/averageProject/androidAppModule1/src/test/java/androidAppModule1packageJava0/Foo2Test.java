e41ee4988aafd880af8da87c5f99fa40b52d154d
Sergio Murguia <srmurguia@google.com>
2018-08-22 18:54:30 -0700

    Add a project for single variant sync performance stats
    Test: Not needed, upcoming CL with tests will make use of it
    Bug: 113078294
    Change-Id: I0761ad56712226b97c2ff2924d3ec6cfae4d96b1

---
+++ android/testData/projects/sync/perf/averageProject/androidAppModule1/src/test/java/androidAppModule1packageJava0/Foo2Test.java
@@ -0,0 +1,30 @@
+package androidAppModule1packageJava0;
+
+import org.junit.Test;
+
+public class Foo2Test {
+  @Test
+  public void testFoo0() {
+    new Foo2().foo0();
+  }
+
+  @Test
+  public void testFoo1() {
+    new Foo2().foo1();
+  }
+
+  @Test
+  public void testFoo2() {
+    new Foo2().foo2();
+  }
+
+  @Test
+  public void testFoo3() {
+    new Foo2().foo3();
+  }
+
+  @Test
+  public void testFoo4() {
+    new Foo2().foo4();
+  }
+}
---
+++ android/testData/projects/sync/perf/averageProject/module0/src/main/java/module0packageJava0/Foo2.java
@@ -0,0 +1,23 @@
+package module0packageJava0;
+
+public class Foo2 {
+  public void foo0() {
+    new module0packageJava0.Foo1().foo4();
+  }
+
+  public void foo1() {
+    foo0();
+  }
+
+  public void foo2() {
+    foo1();
+  }
+
+  public void foo3() {
+    foo2();
+  }
+
+  public void foo4() {
+    foo3();
+  }
+}
