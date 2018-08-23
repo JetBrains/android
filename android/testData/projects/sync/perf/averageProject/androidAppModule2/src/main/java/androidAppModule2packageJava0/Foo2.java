e1d03cb02f00a815d5bc55b7a0a4fa56048c03a1
Sergio Murguia <srmurguia@google.com>
2018-08-22 18:54:30 -0700

    Add a project for single variant sync performance stats
    Test: Not needed, upcoming CL with tests will make use of it
    Bug: 113078294
    Change-Id: I0761ad56712226b97c2ff2924d3ec6cfae4d96b1

---
+++ android/testData/projects/sync/perf/averageProject/androidAppModule2/src/main/java/androidAppModule2packageJava0/Foo2.java
@@ -0,0 +1,23 @@
+package androidAppModule2packageJava0;
+
+public class Foo2 {
+  public void foo0() {
+    new androidAppModule2packageJava0.Foo1().foo4();
+  }
+
+  public void foo1() {
+    foo0();
+  }
+
+  public void foo2() {
+    foo1();
+  }
+
+  public void foo3() {
+    foo2();
+  }
+
+  public void foo4() {
+    foo3();
+  }
+}
