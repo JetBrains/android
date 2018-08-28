4d57ac6811cd55a9ea6c796399460a7b3c72806c
Gen Li <genli@google.com>
2018-08-28 14:41:15 -0700

    Let bazel use embedded JDK for bazel UI tests.
    Currently BazelGuiTestProjectSystem explicitly writes a startup flag for
    bazel during bazel project based UI tests.  A recent update of bazel has
    made bazel require java 9. Because our prebuilts java runtime is still
    java 8, the bazel server fails to start up and crashes the UI test.
    
    This CL removes "startup --host-javabase" flag from the autogenerated
    .bazelrc during UI test initialization, which lets bazel use the
    embedded java runtime instead. This fixes the UI tests.
    
    The following tests now work as they did before:
    * BazelStartupSyncTest
    * AswbGotoDeclarationTest
    * BasicLayoutEditTest[BAZEL]
    
    Bug: 113117406
    Test: Manually verified that the UI tests broken by the recent merge now
    work again.
    Test-Strategy: ui.test.groups=EDITING,DEFAULT
    
    Change-Id: If90f23902e5715b7dd14f68e1b72773dc148618d

--- android-uitests/testSrc/com/android/tools/idea/tests/gui/uibuilder/BazelStartupSyncTest.kt
+++ android-uitests/testSrc/com/android/tools/idea/tests/gui/uibuilder/BazelStartupSyncTest.kt
@@ -30,7 +30,6 @@ import org.junit.runner.RunWith
 import org.junit.runners.Parameterized
 import java.util.*
 
-@Ignore("b/113117406")
 @RunWith(Parameterized::class)
 @Parameterized.UseParametersRunnerFactory(BuildSpecificGuiTestRunner.Factory::class)
 class BazelStartupSyncTest {
