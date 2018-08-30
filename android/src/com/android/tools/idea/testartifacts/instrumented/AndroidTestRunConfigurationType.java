82f7a39f045fb7f8fe1ef94204e26465092de7eb
develar <develar@gmail.com>
2018-08-30 16:26:14 +0200

    SimpleConfigurationType

--- android/src/com/android/tools/idea/testartifacts/instrumented/AndroidTestRunConfigurationType.java
+++ android/src/com/android/tools/idea/testartifacts/instrumented/AndroidTestRunConfigurationType.java
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.android.tools.idea.testartifacts.instrumented;
 
 import com.android.tools.idea.run.AndroidRunConfigurationType;
