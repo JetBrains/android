a9dfa0a81d0f2708e209a49c22f9f01eb1ef5917
Amr Afifiy <amrmahmoud@google.com>
2018-09-07 12:43:17 -0700

    Added association between main and render thread frames
    Associated main thread frames with the related render thread frames so when
    the user hovers over one of them both are highlighted for clarity, and the
    frames tooltip shows information about both.
    
    Change-Id: Ib86cfef8cd3eb7833b846dcbe68b1378fbda3e0a
    Fixes: 112758924
    Test: Updated AtraceFrameManagerTest, CpuFrameTooltipViewTest

--- profilers-ui/src/com/android/tools/profilers/cpu/CpuFrameTooltipView.java
+++ profilers-ui/src/com/android/tools/profilers/cpu/CpuFrameTooltipView.java
@@ -25,35 +25,99 @@ import java.util.concurrent.TimeUnit;
 import javax.swing.JComponent;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JSeparator;
+import javax.swing.SwingConstants;
 import org.jetbrains.annotations.NotNull;
 
 public class CpuFrameTooltipView extends ProfilerTooltipView {
   @NotNull private final CpuFrameTooltip myTooltip;
   @NotNull private final JPanel myContent;
-  @NotNull private final JLabel myCpuText;
-  @NotNull private final JLabel myTotalTimeText;
+
+  @NotNull private final JPanel myMainFramePanel;
+  @NotNull private final JLabel myMainFrameCpuText;
+  @NotNull private final JLabel myMainFrameTotalTimeText;
+
+  @NotNull private final JPanel myRenderFramePanel;
+  @NotNull private final JLabel myRenderFrameCpuText;
+  @NotNull private final JLabel myRenderTotalTimeText;
+
+  @NotNull private final JSeparator mySeparator;
 
   protected CpuFrameTooltipView(@NotNull CpuProfilerStageView view, @NotNull CpuFrameTooltip tooltip) {
     super(view.getTimeline());
     myTooltip = tooltip;
-    myContent = new JPanel(new TabularLayout("*").setVGap(JBUI.scale(8)));
-    myCpuText = createTooltipLabel();
-    myContent.add(myCpuText, new TabularLayout.Constraint(0, 0));
-    myTotalTimeText = createTooltipLabel();
-    myContent.add(myTotalTimeText, new TabularLayout.Constraint(2, 0));
+    myContent = new JPanel(new TabularLayout("*"));
+
+    myMainFramePanel = new JPanel(new TabularLayout("*").setVGap(JBUI.scale(8)));
+
+    JLabel mainThreadLabel = createTooltipLabel();
+    mainThreadLabel.setText("Main Thread:");
+    myMainFrameCpuText = createTooltipLabel();
+    myMainFrameTotalTimeText = createTooltipLabel();
+
+    myMainFramePanel.add(mainThreadLabel, new TabularLayout.Constraint(0, 0));
+    myMainFramePanel.add(myMainFrameCpuText, new TabularLayout.Constraint(2, 0));
+    myMainFramePanel.add(myMainFrameTotalTimeText, new TabularLayout.Constraint(4, 0));
+
+    mySeparator = new JSeparator(SwingConstants.HORIZONTAL);
+    //TODO (b/77491599): Remove workaround after tabular layout no longer defaults to min size:
+    mySeparator.setMinimumSize(mySeparator.getPreferredSize());
+
+    myRenderFramePanel = new JPanel(new TabularLayout("*").setVGap(JBUI.scale(8)));
+
+    JLabel renderThreadLabel = createTooltipLabel();
+    renderThreadLabel.setText("Render Thread:");
+    myRenderFrameCpuText = createTooltipLabel();
+    myRenderTotalTimeText = createTooltipLabel();
+
+    myRenderFramePanel.add(renderThreadLabel, new TabularLayout.Constraint(0, 0));
+    myRenderFramePanel.add(myRenderFrameCpuText, new TabularLayout.Constraint(2, 0));
+    myRenderFramePanel.add(myRenderTotalTimeText, new TabularLayout.Constraint(4, 0));
+
+    myContent.add(myMainFramePanel, new TabularLayout.Constraint(0, 0));
+    myContent.add(mySeparator, new TabularLayout.Constraint(1, 0));
+    myContent.add(myRenderFramePanel, new TabularLayout.Constraint(2, 0));
+
     tooltip.addDependency(this).onChange(CpuFrameTooltip.Aspect.FRAME_CHANGED, this::timeChanged);
   }
 
+  private static void setLabelText(AtraceFrame frame, JLabel cpuText, JLabel totalTimeText) {
+    cpuText.setText(String.format("CPU Time: %s", TimeFormatter
+      .getSingleUnitDurationString((long)(TimeUnit.SECONDS.toMicros(1) * frame.getCpuTimeSeconds()))));
+    totalTimeText.setText(String.format("Total Time: %s", TimeFormatter.getSingleUnitDurationString(frame.getDurationUs())));
+  }
+
   protected void timeChanged() {
+    // hide everything then show the necessary fields later
+    myContent.setVisible(false);
+    myMainFramePanel.setVisible(false);
+    mySeparator.setVisible(false);
+    myRenderFramePanel.setVisible(false);
+
     AtraceFrame frame = myTooltip.getFrame();
     if (frame == null || frame == AtraceFrame.EMPTY) {
-      myContent.setVisible(false);
       return;
     }
     myContent.setVisible(true);
-    myCpuText.setText(String.format("CPU Time: %s", TimeFormatter
-      .getSingleUnitDurationString((long)(TimeUnit.SECONDS.toMicros(1) * frame.getCpuTimeSeconds()))));
-    myTotalTimeText.setText(String.format("Total Time: %s", TimeFormatter.getSingleUnitDurationString(frame.getDurationUs())));
+    
+    if (frame.getThread() == AtraceFrame.FrameThread.MAIN) {
+      myMainFramePanel.setVisible(true);
+      setLabelText(frame, myMainFrameCpuText, myMainFrameTotalTimeText);
+      if (frame.getAssociatedFrame() != null) {
+        mySeparator.setVisible(true);
+        myRenderFramePanel.setVisible(true);
+        setLabelText(frame.getAssociatedFrame(), myRenderFrameCpuText, myRenderTotalTimeText);
+      }
+    }
+    else if (frame.getThread() == AtraceFrame.FrameThread.RENDER) {
+      myRenderFramePanel.setVisible(true);
+      setLabelText(frame, myRenderFrameCpuText, myRenderTotalTimeText);
+      if (frame.getAssociatedFrame() != null) {
+        mySeparator.setVisible(true);
+        myMainFramePanel.setVisible(true);
+        setLabelText(frame.getAssociatedFrame(), myMainFrameCpuText, myMainFrameTotalTimeText);
+      }
+    }
   }
 
   @NotNull
