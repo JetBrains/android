d7de010b65221884cf7bd42eac0c75c8b82b5934
Dana Dahlstrom <dahlstrom@google.com>
2018-09-13 23:23:23 -0700

    Move failing tests to TestGroup.UNRELIABLE
    Issue: 115698976
    Issue: 115700446
    Change-Id: Ia951c08ff249923e901cd8266d81be45e44a2fe7

--- android-uitests/testSrc/com/android/tools/idea/tests/gui/uibuilder/DragMenuItemsFromPaletteToComponentTreeTest.java
+++ android-uitests/testSrc/com/android/tools/idea/tests/gui/uibuilder/DragMenuItemsFromPaletteToComponentTreeTest.java
@@ -16,6 +16,8 @@
 package com.android.tools.idea.tests.gui.uibuilder;
 
 import com.android.tools.idea.tests.gui.framework.GuiTestRule;
+import com.android.tools.idea.tests.gui.framework.RunIn;
+import com.android.tools.idea.tests.gui.framework.TestGroup;
 import com.android.tools.idea.tests.gui.framework.fixture.EditorFixture;
 import com.android.tools.idea.tests.gui.framework.fixture.designer.NlEditorFixture;
 import com.intellij.testGuiFramework.framework.GuiTestRemoteRunner;
@@ -34,6 +36,7 @@ public final class DragMenuItemsFromPaletteToComponentTreeTest {
   @Rule
   public final GuiTestRule myGuiTest = new GuiTestRule();
 
+  @RunIn(TestGroup.UNRELIABLE) // b/115700446
   @Test
   public void dragAndDrop() throws IOException {
     myGuiTest.importProjectAndWaitForProjectSyncToFinish("LayoutTest");
