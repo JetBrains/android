d26a024ce307b40274ed5080c46f5c8bc0936080
Vadim Caen <caen@google.com>
2018-09-19 15:36:39 +0100

    Used TimedDisposable for DrawableRenderer.
    Fixes: 113673321
    Test: N/A
    Change-Id: I121f6020f2c664d16c33c97acf783fa5498cf5be

--- android/src/com/android/tools/idea/resourceExplorer/plugin/DrawableAssetRenderer.kt
+++ android/src/com/android/tools/idea/resourceExplorer/plugin/DrawableAssetRenderer.kt
@@ -16,19 +16,19 @@
 package com.android.tools.idea.resourceExplorer.plugin
 
 import com.android.SdkConstants
-import com.android.tools.idea.layoutlib.RenderingException
 import com.android.tools.idea.npw.assetstudio.DrawableRenderer
+import com.android.tools.idea.util.TimedDisposable
 import com.google.common.util.concurrent.Futures
 import com.google.common.util.concurrent.ListenableFuture
 import com.intellij.ide.highlighter.XmlFileType
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.module.Module
-import com.intellij.openapi.util.Disposer
 import com.intellij.openapi.vfs.VirtualFile
 import org.jetbrains.android.facet.AndroidFacet
 import java.awt.Dimension
 import java.awt.Image
 import java.text.ParseException
+import java.util.concurrent.TimeUnit
 import javax.xml.parsers.DocumentBuilderFactory
 
 private val LOG = Logger.getInstance(DrawableAssetRenderer::class.java)
@@ -37,13 +37,30 @@ private val SUPPORTED_DRAWABLE_TAG = arrayOf(
   SdkConstants.TAG_VECTOR, SdkConstants.TAG_SHAPE, SdkConstants.TAG_BITMAP,
   SdkConstants.TAG_RIPPLE, SdkConstants.TAG_SELECTOR, SdkConstants.TAG_ANIMATED_SELECTOR)
 
+private const val disposeTime: Long = 5
+private val disposeTimeUnit: TimeUnit = TimeUnit.MINUTES
+
 /**
  * [DesignAssetRenderer] to display Vector Drawable.
  */
 class DrawableAssetRenderer : DesignAssetRenderer {
 
-  private var drawableRenderer: DrawableRenderer? = null
+  private var drawableRenderer: TimedDisposable<DrawableRenderer>? = null
+
   private val documentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder()
+
+  private fun getRenderer(module: Module): DrawableRenderer {
+    val renderer = drawableRenderer?.get()
+    return if (renderer == null) {
+      val facet = AndroidFacet.getInstance(module) ?: throw NullPointerException("Facet couldn't be found for use in DrawableRenderer.")
+      val newRenderer = DrawableRenderer(facet)
+      drawableRenderer = TimedDisposable(newRenderer, facet, disposeTime, disposeTimeUnit)
+      newRenderer
+    }
+    else {
+      renderer
+    }
+  }
 
   override fun isFileSupported(file: VirtualFile): Boolean {
     if (file.fileType != XmlFileType.INSTANCE || file.length == 0L) {
@@ -74,23 +91,13 @@ class DrawableAssetRenderer : DesignAssetRenderer {
         throw ParseException("${file.path} couldn't be parsed as a drawable.", 0)
       }
 
-      ensureDrawableRendererInitialized(module)
+      val renderer = getRenderer(module)
 
       val xmlContent = String(file.contentsToByteArray())
-      return drawableRenderer?.renderDrawable(xmlContent, dimension)
-             ?: throw RenderingException("${file.path} couldn't be rendered.")
+      return renderer.renderDrawable(xmlContent, dimension)
     }
     catch (ex: Exception) {
       return failedFuture(ex)
-    }
-  }
-
-  private fun ensureDrawableRendererInitialized(module: Module) {
-    if (drawableRenderer?.let { Disposer.isDisposed(it) } != false) {
-      val facet = AndroidFacet.getInstance(module) ?: throw NullPointerException("Facet couldn't be found for use in DrawableRenderer.")
-      val renderer = DrawableRenderer(facet)
-      Disposer.register(facet, renderer)
-      drawableRenderer = renderer
     }
   }
 
