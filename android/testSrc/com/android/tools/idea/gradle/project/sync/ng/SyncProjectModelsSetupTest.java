04aeab89d139cedab04afefd32ba4fecdaa1216d
Ying Chen <chying@google.com>
2018-09-20 06:58:08 -0700

    [New Sync] Setup Ndk model before Android model.
    Setup NdkModuleModel first since it removes all content
    entries during ContentEntriesSetupStep, which removes the
    entries setup by Android setup steps if Android is done
    first.
    Also this order is consistent with IDEA sync as defined
    in AndroidProjectKeys.
    
    Bug: 116006284
    Bug: 114353214
    Bug: 115775624
    Test: SyncProjectModelsSetupTest::testContentEntriesWithNativeProject
    Change-Id: I63b1c355cd6df53223b957bf303985078090e1a4

--- android/testSrc/com/android/tools/idea/gradle/project/sync/ng/SyncProjectModelsSetupTest.java
+++ android/testSrc/com/android/tools/idea/gradle/project/sync/ng/SyncProjectModelsSetupTest.java
@@ -15,32 +15,48 @@
  */
 package com.android.tools.idea.gradle.project.sync.ng;
 
+import static com.android.tools.idea.io.FilePaths.pathToIdeaUrl;
 import static com.android.tools.idea.testing.TestProjectPaths.HELLO_JNI;
+import static com.google.common.truth.Truth.assertThat;
 import static com.intellij.openapi.command.WriteCommandAction.runWriteCommandAction;
 
 import com.android.tools.idea.flags.StudioFlags;
 import com.android.tools.idea.gradle.dsl.api.ProjectBuildModel;
 import com.android.tools.idea.gradle.project.facet.ndk.NdkFacet;
 import com.android.tools.idea.testing.AndroidGradleTestCase;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.externalSystem.service.project.IdeModifiableModelsProvider;
+import com.intellij.openapi.externalSystem.service.project.IdeModifiableModelsProviderImpl;
 import com.intellij.openapi.module.Module;
+import com.intellij.openapi.roots.ContentEntry;
+import com.intellij.openapi.roots.ModifiableRootModel;
 import com.intellij.openapi.roots.ModuleRootManager;
+import com.intellij.openapi.roots.SourceFolder;
 import com.intellij.openapi.vfs.VirtualFile;
+import java.io.File;
+import java.util.Set;
+import java.util.stream.Collectors;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.jps.model.java.JavaSourceRootType;
 
 /**
  * Tests for {@link SyncProjectModelsSetup}.
  */
 public class SyncProjectModelsSetupTest extends AndroidGradleTestCase {
+  private IdeModifiableModelsProvider myModelsProvider;
+
   @Override
   public void setUp() throws Exception {
     super.setUp();
     StudioFlags.NEW_SYNC_INFRA_ENABLED.override(true);
+    myModelsProvider = new IdeModifiableModelsProviderImpl(getProject());
   }
 
   @Override
   protected void tearDown() throws Exception {
     try {
       StudioFlags.NEW_SYNC_INFRA_ENABLED.clearOverride();
+      ApplicationManager.getApplication().runWriteAction(() -> myModelsProvider.dispose());
     }
     finally {
       super.tearDown();
@@ -79,5 +95,33 @@ public class SyncProjectModelsSetupTest extends AndroidGradleTestCase {
       }
     }
     return false;
+  }
+
+  public void testContentEntriesWithNativeProject() throws Throwable {
+    loadProject(HELLO_JNI);
+
+    ModifiableRootModel rootModel = myModelsProvider.getModifiableRootModel(myModules.getAppModule());
+    ContentEntry[] entries = rootModel.getContentEntries();
+    assertThat(entries.length).isEqualTo(1);
+    ContentEntry entry = entries[0];
+    Set<String> sourceFolders = entry.getSourceFolders(JavaSourceRootType.SOURCE)
+                                     .stream()
+                                     .map(SourceFolder::toString)
+                                     .collect(Collectors.toSet());
+    Set<String> testSourceFolders = entry.getSourceFolders(JavaSourceRootType.TEST_SOURCE)
+                                         .stream()
+                                         .map(SourceFolder::toString)
+                                         .collect(Collectors.toSet());
+
+    // Verify that both of cpp and java folders are setup correctly.
+    assertThat(sourceFolders).contains(getIdeaUrl("app/src/main/cpp"));
+    assertThat(sourceFolders).contains(getIdeaUrl("app/src/main/java"));
+    assertThat(testSourceFolders).contains(getIdeaUrl("app/src/test/java"));
+    assertThat(testSourceFolders).contains(getIdeaUrl("app/src/androidTest/java"));
+  }
+
+  @NotNull
+  private String getIdeaUrl(@NotNull String relativePath) {
+    return pathToIdeaUrl(new File(getProjectFolderPath(), relativePath));
   }
 }
