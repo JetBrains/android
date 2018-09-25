3092ed5ca2081d33d7d861093a83bd864a2d73e8
Diego Perez <diegoperez@google.com>
2018-09-25 15:55:16 +0000

    Revert "Fix race condition on Layout Editor initialization"
    This reverts commit f1e5d0d797b0b57040209133dd982e39ba3841e2.
    
    Reason for revert: Breaks UI tests
    
    Change-Id: I9321968d9510d863a1b1e8c6385108add993cb51

--- android/testSrc/com/android/tools/idea/util/SyncUtilTest.kt
+++ android/testSrc/com/android/tools/idea/util/SyncUtilTest.kt
@@ -15,58 +15,21 @@
  */
 package com.android.tools.idea.util
 
-import com.android.tools.idea.concurrent.FutureUtils.executeOnPooledThread
 import com.android.tools.idea.projectsystem.PROJECT_SYSTEM_SYNC_TOPIC
-import com.android.tools.idea.projectsystem.ProjectSystemSyncManager
 import com.android.tools.idea.projectsystem.ProjectSystemSyncManager.SyncResult
 import com.android.tools.idea.projectsystem.ProjectSystemSyncManager.SyncResultListener
 import com.android.tools.idea.testing.AndroidProjectRule
-import com.google.common.util.concurrent.ListenableFuture
-import com.intellij.openapi.application.ApplicationManager
-import com.intellij.openapi.application.WriteAction
-import com.intellij.openapi.application.invokeAndWaitIfNeed
-import com.intellij.openapi.project.DumbServiceImpl
 import com.intellij.openapi.project.Project
-import com.intellij.testFramework.PlatformTestUtil
-import com.intellij.util.ThrowableRunnable
-import junit.framework.TestCase.assertFalse
-import junit.framework.TestCase.assertTrue
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
 import org.mockito.Mockito
-import java.util.concurrent.CountDownLatch
-import java.util.concurrent.atomic.AtomicInteger
-import java.util.function.Consumer
-import kotlin.test.assertEquals
-
-/**
- * Test [ProjectSystemSyncManager] that allows to manually change the [isSyncInProgress()] value
- */
-private class TestSyncManager(val project: Project) : ProjectSystemSyncManager {
-  var testIsSyncInProgress: Boolean = false
-  var testLastSyncResult: SyncResult = SyncResult.UNKNOWN
-
-  override fun syncProject(reason: ProjectSystemSyncManager.SyncReason, requireSourceGeneration: Boolean): ListenableFuture<SyncResult> {
-    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-  }
-
-  override fun isSyncInProgress(): Boolean = testIsSyncInProgress
-
-  override fun isSyncNeeded(): Boolean {
-    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-  }
-
-  override fun getLastSyncResult(): SyncResult = testLastSyncResult
-}
 
 @RunWith(JUnit4::class)
 class SyncUtilTest {
-  @Rule
-  @JvmField
-  val projectRule = AndroidProjectRule.inMemory().initAndroid(false)
+  @Rule @JvmField val projectRule = AndroidProjectRule.inMemory().initAndroid(false)
   private lateinit var project: Project
   private lateinit var listener: SyncResultListener
 
@@ -80,116 +43,20 @@ class SyncUtilTest {
 
 
   @Test
-  fun listenOneSync() {
-    project.listenUntilNextSync(listener = listener)
+  fun listenUntilNextSuccessfulSync_broadcastsUntilSuccessful() {
+    listenUntilNextSuccessfulSync(project, listener = listener)
     Mockito.verifyZeroInteractions(listener)
 
     emulateSync(SyncResult.CANCELLED)
     Mockito.verify(listener).syncEnded(SyncResult.CANCELLED)
 
     emulateSync(SyncResult.FAILURE)
-    Mockito.verifyZeroInteractions(listener)
-  }
+    Mockito.verify(listener).syncEnded(SyncResult.FAILURE)
 
-  private fun startDumbMode() {
-    WriteAction.runAndWait(ThrowableRunnable<Exception> {
-      DumbServiceImpl.getInstance(project).isDumb = true
-    })
-  }
-
-  private fun stopDumbMode() {
-    WriteAction.runAndWait(ThrowableRunnable<Exception> {
-      DumbServiceImpl.getInstance(project).isDumb = false
-    })
-  }
-
-  @Test
-  fun waitForSmartAndSyncedWhenSmartAndSynced() {
-    val callCount = AtomicInteger(0)
-    project.runWhenSmartAndSynced(callback = Consumer { callCount.incrementAndGet() })
-    assertEquals(1, callCount.get())
-  }
-
-  @Test
-  fun waitForSmartAndSyncedWhenDumbAndSynced() {
-    val callCount = AtomicInteger(0)
-    // Start dumb mode
-    startDumbMode()
-
-    project.runWhenSmartAndSynced(callback = Consumer { callCount.incrementAndGet() })
-    assertEquals(0, callCount.get())
     emulateSync(SyncResult.SUCCESS)
-    assertEquals(0, callCount.get())
-    stopDumbMode()
-    assertEquals(1, callCount.get())
-  }
+    Mockito.verify(listener).syncEnded(SyncResult.SUCCESS)
 
-  @Test
-  fun waitForSmartAndSyncedWhenSmartAndNotSynced() {
-    val callCount = AtomicInteger(0)
-    val syncManager = TestSyncManager(project)
-    syncManager.testIsSyncInProgress = true
-    project.runWhenSmartAndSynced(callback = Consumer { callCount.incrementAndGet() },
-                                  syncManager = syncManager)
-    assertEquals(0, callCount.get())
-    syncManager.testIsSyncInProgress = false
-    emulateSync(SyncResult.SUCCESS)
-    assertEquals(1, callCount.get())
-  }
-
-  @Test
-  fun waitForSmartAndSyncedWhenDumbAndNotSynced() {
-    val callCount = AtomicInteger(0)
-    val syncManager = TestSyncManager(project)
-    syncManager.testIsSyncInProgress = true
-    startDumbMode()
-    project.runWhenSmartAndSynced(
-      callback = Consumer { callCount.incrementAndGet() },
-      syncManager = syncManager)
-    assertEquals(0, callCount.get())
-    syncManager.testIsSyncInProgress = false
-    emulateSync(SyncResult.SUCCESS)
-    // Now we are in dumb mode but synced
-    assertEquals(0, callCount.get())
-
-    stopDumbMode()
-    assertEquals(1, callCount.get())
-
-    // Once the callback has been called, new syncs or dumb mode changes won't call the method
-    emulateSync(SyncResult.SUCCESS)
-    assertEquals(1, callCount.get())
-  }
-
-  @Test
-  fun waitForSmartAndSyncedAndCheckThread() {
-    val callCount = AtomicInteger(0)
-    val syncManager = TestSyncManager(project)
-
-    // The next callback won't execute immediately but it will be scheduled to run on the EDT later
-    val latch = CountDownLatch(1)
-    executeOnPooledThread {
-      project.runWhenSmartAndSyncedOnEdt(
-        callback = Consumer {
-          assertTrue(ApplicationManager.getApplication().isDispatchThread)
-          latch.await()
-          callCount.incrementAndGet()
-        },
-        syncManager = syncManager)
-    }
-    assertEquals(0, callCount.get())
-    latch.countDown()
-    invokeAndWaitIfNeed { PlatformTestUtil.dispatchAllEventsInIdeEventQueue() }
-    assertEquals(1, callCount.get())
-
-    val latch2 = CountDownLatch(1)
-    executeOnPooledThread {
-      project.runWhenSmartAndSynced(
-        callback = Consumer {
-          assertFalse(ApplicationManager.getApplication().isDispatchThread)
-          latch2.countDown()
-        },
-        syncManager = syncManager)
-    }
-    latch2.await()
+    emulateSync(SyncResult.SKIPPED)
+    Mockito.verifyNoMoreInteractions(listener)
   }
 }
