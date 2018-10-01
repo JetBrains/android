46ef3175f6da3bce75cc9a4dcb8e05289e9c263e
Charlie Tsai <chartsai@google.com>
2018-10-01 14:00:01 +0100

    Refactor DrawNlComponentFrame
    - Replace hard coded value to ordinal of the enum.
    - Remove unnecessary array creation.
    - Add Nullable/NotNull annotation.
    - Remove dead codes.
    - Remove duplicated value assignements.
    
    Test: Passed all SceneTests
    Bug: N/A
    Change-Id: Ica6a1d43a7ca9f09a89ad55443594cf331e5095f

--- designer/src/com/android/tools/idea/common/scene/draw/DrawNlComponentFrame.java
+++ designer/src/com/android/tools/idea/common/scene/draw/DrawNlComponentFrame.java
@@ -16,28 +16,25 @@
 package com.android.tools.idea.common.scene.draw;
 
 import com.android.tools.idea.common.model.AndroidDpCoordinate;
+import com.android.tools.idea.common.scene.SceneComponent;
 import com.android.tools.idea.common.scene.SceneContext;
 import com.android.tools.idea.uibuilder.handlers.constraint.drawing.ColorSet;
 
+import com.android.tools.layoutlib.annotations.NotNull;
 import java.awt.*;
 
 /**
  * Draw the frame of a SceneComponent
  */
 public class DrawNlComponentFrame extends DrawRegion {
-  public static final int SUBDUED = 0;
-  public static final int NORMAL = 1;
-  public static final int OVER = 2;
-  public static final int SELECTED = 3;
 
   static Stroke myNormalStroke = new BasicStroke(1);
-  static Stroke myProblemStroke = new BasicStroke(2);
   static Stroke myWrapStroke =  new BasicStroke(1);
   static Stroke myMatchParentStroke = new BasicStroke(1);
   static Stroke myDragReceiverStroke = new BasicStroke(3);
   static Stroke myMatchConstraintStroke = new FancyStroke(FancyStroke.Type.SPRING, 2, 2, 1);
 
-  int myMode;
+  @NotNull SceneComponent.DrawState myMode;
   int myLayoutWidth;
   int myLayoutHeight;
   int myLevel = COMPONENT_LEVEL;
@@ -46,7 +43,7 @@ public class DrawNlComponentFrame extends DrawRegion {
     String[] sp = s.split(",");
     int c = 0;
     c = super.parse(sp, c);
-    myMode = Integer.parseInt(sp[c++]);
+    myMode = SceneComponent.DrawState.values()[Integer.parseInt(sp[c++])];
     myLayoutWidth = Integer.parseInt(sp[c++]);
     myLayoutHeight = Integer.parseInt(sp[c++]);
   }
@@ -60,19 +57,68 @@ public class DrawNlComponentFrame extends DrawRegion {
                             @AndroidDpCoordinate int y,
                             @AndroidDpCoordinate int width,
                             @AndroidDpCoordinate int height,
-                            int mode,
+                            @NotNull SceneComponent.DrawState mode,
                             int layout_width,
                             int layout_height) {
     super(x, y, width, height);
     myMode = mode;
     myLayoutWidth = layout_width;
     myLayoutHeight = layout_height;
-    if (mode == SELECTED) {
+    if (mode == SceneComponent.DrawState.SELECTED) {
       myLevel = COMPONENT_SELECTED_LEVEL;
     }
   }
 
-  private Stroke getStroke(int dim) {
+  @Override
+  public void paint(Graphics2D g, SceneContext sceneContext) {
+    ColorSet colorSet = sceneContext.getColorSet();
+    Stroke previousStroke = g.getStroke();
+    Color previousColor = g.getColor();
+
+    g.setStroke(myNormalStroke);
+    g.setColor(getFrameColor(colorSet, myMode));
+    if (myLayoutWidth == myLayoutHeight || myMode == SceneComponent.DrawState.DRAG) {
+      if (myMode == SceneComponent.DrawState.DRAG) {
+        g.setStroke(myDragReceiverStroke);
+      } else {
+        g.setStroke(getStroke(myLayoutWidth));
+      }
+      g.drawRect(x, y, width, height);
+    }
+    else {
+      g.setStroke(getStroke(myLayoutHeight));
+      g.drawLine(x, y, x, y + height);
+      g.drawLine(x + width, y, x + width, y + height);
+      g.setStroke(getStroke(myLayoutWidth));
+      g.drawLine(x, y, x + width, y);
+      g.drawLine(x, y + height, x + width, y + height);
+    }
+
+    g.setColor(previousColor);
+    g.setStroke(previousStroke);
+  }
+
+  @NotNull
+  private static Color getFrameColor(@NotNull ColorSet colorSet, @NotNull SceneComponent.DrawState mode) {
+    switch (mode) {
+      case SUBDUED:
+        return colorSet.getSubduedFrames();
+      case NORMAL:
+        return colorSet.getFrames();
+      case HOVER:
+        return colorSet.getHighlightedFrames();
+      case SELECTED:
+        return colorSet.getSelectedFrames();
+      case DRAG:
+        return colorSet.getDragReceiverFrames();
+      default:
+        // Should not happen.
+        return colorSet.getFrames();
+    }
+  }
+
+  @NotNull
+  private static Stroke getStroke(int dim) {
     if (dim == 0) {
       return myMatchConstraintStroke;
     }
@@ -86,42 +132,14 @@ public class DrawNlComponentFrame extends DrawRegion {
   }
 
   @Override
-  public void paint(Graphics2D g, SceneContext sceneContext) {
-    ColorSet colorSet = sceneContext.getColorSet();
-    Color[] colorFrame = {colorSet.getSubduedFrames(), colorSet.getFrames(), colorSet.getHighlightedFrames(), colorSet.getSelectedFrames(), colorSet.getDragReceiverFrames()};
-    Stroke previousStroke = g.getStroke();
-    g.setStroke(myNormalStroke);
-    g.setColor(colorFrame[myMode]);
-    if (myLayoutWidth == myLayoutHeight || myMode == 4) {
-      if (myMode == 4) {
-        g.setStroke(myDragReceiverStroke);
-      } else {
-        g.setStroke(getStroke(myLayoutWidth));
-      }
-      g.drawRect(x, y, width, height);
-    }
-    else {
-      g.setColor(colorFrame[myMode]);
-      g.setStroke(getStroke(myLayoutHeight));
-      g.drawLine(x, y, x, y + height);
-      g.drawLine(x + width, y, x + width, y + height);
-      g.setColor(colorFrame[myMode]);
-      g.setStroke(getStroke(myLayoutWidth));
-      g.drawLine(x, y, x + width, y);
-      g.drawLine(x, y + height, x + width, y + height);
-    }
-    g.setStroke(previousStroke);
-  }
-
-  @Override
   public String serialize() {
-    return super.serialize() + "," + myMode+ "," + myLayoutHeight+ "," + myLayoutHeight;
+    return super.serialize() + "," + myMode.ordinal() + "," + myLayoutHeight+ "," + myLayoutHeight;
   }
 
   public static void add(DisplayList list,
                          SceneContext sceneContext,
                          @AndroidDpCoordinate Rectangle rect,
-                         int mode,
+                         @NotNull SceneComponent.DrawState mode,
                          int layout_width,
                          int layout_height) {
     int l = sceneContext.getSwingXDip(rect.x);
