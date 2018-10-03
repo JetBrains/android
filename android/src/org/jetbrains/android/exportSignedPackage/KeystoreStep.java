8594aef90af4643b69550a0c68c922adddab0b37
develar <develar@gmail.com>
2018-10-03 18:52:00 +0200

    remove `getPassword(@NotNull Class<?> requestor, @NotNull String accountName)`

--- android/src/org/jetbrains/android/exportSignedPackage/KeystoreStep.java
+++ android/src/org/jetbrains/android/exportSignedPackage/KeystoreStep.java
@@ -1,22 +1,9 @@
-/*
- * Copyright 2000-2012 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 
 package org.jetbrains.android.exportSignedPackage;
 
 import com.android.annotations.VisibleForTesting;
+import com.intellij.credentialStore.CredentialAttributesKt;
 import com.intellij.ide.passwordSafe.PasswordSafe;
 import com.intellij.ide.wizard.CommitStepException;
 import com.intellij.openapi.project.Project;
@@ -94,12 +81,12 @@ class KeystoreStep extends ExportSignedPackageWizardStep implements ApkSigningSe
 
   private static String retrievePassword(@NotNull Class<?> primaryRequestor, @NotNull String key) {
     final PasswordSafe passwordSafe = PasswordSafe.getInstance();
-    String password = passwordSafe.getPassword(primaryRequestor, key);
+    String password = passwordSafe.getPassword(CredentialAttributesKt.CredentialAttributes(primaryRequestor, key));
     if (password == null) {
       // Try to retrieve password previously saved with an old requestor in order to make user experience more seamless
       // while transitioning to a version which contains the fix for b/64995008, rather than having them retype all the
       // passwords at once.
-      password = passwordSafe.getPassword(KeystoreStep.class, key);
+      password = passwordSafe.getPassword(CredentialAttributesKt.CredentialAttributes(KeystoreStep.class, key));
     }
 
     return password;
