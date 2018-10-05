2536b6d60f190cf514c1bdeb5f3d454d93a4dd04
Vadim Caen <caen@google.com>
2018-10-05 11:38:44 +0100

    Handle NPE in RasterAssetRenderer
    Change-Id: I4c82737d954abb79eb4910166424b8d4e0b8d7cc
    Fixes: 115303829
    Test: N/A

--- android/src/com/android/tools/idea/resourceExplorer/plugin/RasterAssetRenderer.kt
+++ android/src/com/android/tools/idea/resourceExplorer/plugin/RasterAssetRenderer.kt
@@ -24,7 +24,6 @@ import com.intellij.openapi.vfs.VirtualFile
 import java.awt.Dimension
 import java.awt.Image
 import java.awt.image.BufferedImage
-import java.net.URL
 import java.util.concurrent.Callable
 import javax.imageio.ImageIO
 
@@ -38,28 +37,33 @@ class RasterAssetRenderer : DesignAssetRenderer {
     return JdkFutureAdapters.listenInPoolThread(
       ApplicationManager.getApplication().executeOnPooledThread(
         Callable<Image?> {
-          val image = ImageIO.read(file.inputStream)
-          val width = image.getWidth(null).toDouble()
-          val height = image.getHeight(null).toDouble()
-          val scale = if (width < height) dimension.width / width else dimension.height / height
-          val scaledImage = ImageUtils.scale(image, scale, scale, null)
-          if ((width * scale + 0.5).toInt() != dimension.width
-              || (height * scale + 0.5).toInt() != dimension.height) {
+          try {
+            val image = ImageIO.read(file.inputStream)
+            val width = image.getWidth(null).toDouble()
+            val height = image.getHeight(null).toDouble()
+            val scale = if (width < height) dimension.width / width else dimension.height / height
+            val scaledImage = ImageUtils.scale(image, scale, scale, null)
+            if ((width * scale + 0.5).toInt() != dimension.width
+                || (height * scale + 0.5).toInt() != dimension.height) {
 
-            val sx1 = (((width * scale - dimension.width) / 2.0) + 0.5).toInt()
-            val sy1 = (((height * scale - dimension.height) / 2.0) + 0.5).toInt()
-            val sx2 = sx1 + dimension.width
-            val sy2 = sy1 + dimension.height
-            val croppedImage = BufferedImage(dimension.width, dimension.height, BufferedImage.TYPE_INT_ARGB)
-            val g = croppedImage.createGraphics()
-            g.drawImage(scaledImage,
-                        0, 0, dimension.width, dimension.height,
-                        sx1, sy1, sx2, sy2,
-                        null)
-            g.dispose()
-            croppedImage
+              val sx1 = (((width * scale - dimension.width) / 2.0) + 0.5).toInt()
+              val sy1 = (((height * scale - dimension.height) / 2.0) + 0.5).toInt()
+              val sx2 = sx1 + dimension.width
+              val sy2 = sy1 + dimension.height
+              val croppedImage = BufferedImage(dimension.width, dimension.height, BufferedImage.TYPE_INT_ARGB)
+              val g = croppedImage.createGraphics()
+              g.drawImage(scaledImage,
+                          0, 0, dimension.width, dimension.height,
+                          sx1, sy1, sx2, sy2,
+                          null)
+              g.dispose()
+              croppedImage
+            }
+            else scaledImage
           }
-          else scaledImage
+          catch (e: NullPointerException) { // b/115303829
+            null
+          }
         }))
   }
 }
