14676e858f9f5d8886744173518b7cf8979d3866
Dana Dahlstrom <dahlstrom@google.com>
2018-10-08 09:11:01 -0700

    Revert Change I104af896 following Change Idcd05b12
    Issue: 113364399
    Change-Id: Ib0f1bbb53e458a2e7b6b8a0d9c90d4e53a971997

--- android-uitests/testSrc/com/android/tools/idea/tests/gui/uibuilder/NavigationViewTest.java
+++ android-uitests/testSrc/com/android/tools/idea/tests/gui/uibuilder/NavigationViewTest.java
@@ -16,8 +16,6 @@
 package com.android.tools.idea.tests.gui.uibuilder;
 
 import com.android.tools.idea.tests.gui.framework.GuiTestRule;
-import com.android.tools.idea.tests.gui.framework.RunIn;
-import com.android.tools.idea.tests.gui.framework.TestGroup;
 import com.android.tools.idea.tests.gui.framework.fixture.EditorFixture;
 import com.android.tools.idea.tests.gui.framework.fixture.designer.NlEditorFixture;
 import com.android.tools.idea.tests.util.WizardUtils;
@@ -62,7 +60,6 @@ public final class NavigationViewTest {
   }
 
 
-  @RunIn(TestGroup.UNRELIABLE)  // b/113364399
   @Test
   public void doubleClickMenu() {
     myLayoutEditor.getSurface().doubleClick(200, 600); //Android coordinates insitde the navigation drawer menu
