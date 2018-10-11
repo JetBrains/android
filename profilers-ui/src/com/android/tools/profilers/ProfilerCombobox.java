18502255dba0efc1f605f34e52e8192ead5b700f
Amaury Medeiros <amaurym@google.com>
2018-10-11 18:12:30 +0100

    Adds border to CPU profiling configurations combobox
    We were not drawing a border around CPU profiling configurations combobox
    because we are setting the JComboBox.isTableCellEditor, and IntelliJ doesn't
    draw a border around comboboxes that have this property set.
    
    JComboBox.isTableCellEditor is needed so we can be consistent with
    IntelliJ's comboboxes behavior, such as not opening a dialog when navigating
    over the combobox using the keyboard.
    
    Fixes: 116814807
    Test: Visual. Open CPU profiler and verify the combobox has a border around
          it. A sanity check can be made in Memory profiler as well, as its
          comboboxes should also have the border.
    
    Change-Id: I8cf0da4e9c1c5e749e0087681a7417ca353b3c9c
    (cherry picked from commit 7ec370cac6e1a071b4213124ab56b9e2f2e65fa4)

--- profilers-ui/src/com/android/tools/profilers/ProfilerCombobox.java
+++ profilers-ui/src/com/android/tools/profilers/ProfilerCombobox.java
@@ -18,8 +18,11 @@ package com.android.tools.profilers;
 import com.intellij.ide.ui.laf.darcula.ui.DarculaComboBoxUI;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.ui.ComboBox;
+import java.awt.Component;
+import java.awt.Graphics;
 import javax.swing.ComboBoxModel;
 import javax.swing.JComboBox;
+import javax.swing.border.Border;
 import javax.swing.border.LineBorder;
 import javax.swing.plaf.ComboBoxUI;
 import javax.swing.plaf.basic.ComboPopup;
@@ -54,11 +57,24 @@ public class ProfilerCombobox<T> extends ComboBox<T> {
    * Custom {@link ComboBoxUI} to be user by instances of {@link ProfilerCombobox}. It's essentially a {@link DarculaComboBoxUI},
    * used by IntelliJ, with a different popup border color and thickness.
    */
-  private static class ProfilerDarculaComboBoxUI extends DarculaComboBoxUI {
+  private static class ProfilerDarculaComboBoxUI extends DarculaComboBoxUI implements Border {
 
     @Override
     protected ComboPopup createPopup() {
-      return new ProfilerCustomComboPopup(this.comboBox);
+      return new ProfilerCustomComboPopup(comboBox);
+    }
+
+    @Override
+    public void paintBorder(Component c, Graphics g, int x, int y, int width, int height) {
+      // Unset the JComboBox.isTableCellEditor before calling parent's paintBorder because it checks for this property and doesn't draw the
+      // line around the border if it's set. We need this line to be drawn, and the reason to set this property to true afterwards is
+      // explained below.
+      comboBox.putClientProperty("JComboBox.isTableCellEditor", Boolean.FALSE);
+      super.paintBorder(c, g, x, y, width, height);
+      // This disables firing actions like setSelectedItem when the user is using keyboard to navigate through the combobox menu.
+      // That's the behavior of IntelliJ comboboxes. For instance, if we have a combobox entry that triggers a popup (e.g. "Settings..."),
+      // we shouldn't trigger it while navigating with the keyboard, but only when actually selecting it.
+      comboBox.putClientProperty("JComboBox.isTableCellEditor", Boolean.TRUE);
     }
 
     private static class ProfilerCustomComboPopup extends CustomComboPopup {
