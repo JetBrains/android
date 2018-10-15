24c324e950c861387f90e9f9186596a8d66f382a
Michal Bendowski <bendowski@google.com>
2018-10-15 16:31:42 +0100

    Remove a dead method in ResourceRepositoryInnerRClass
    Test: N/A
    Bug: N/A
    Change-Id: I497a769574d7c2f82e5d8ace0b400a621d5bd381

--- android/src/org/jetbrains/android/augment/ResourceRepositoryInnerRClass.java
+++ android/src/org/jetbrains/android/augment/ResourceRepositoryInnerRClass.java
@@ -1,12 +1,7 @@
 package org.jetbrains.android.augment;
 
-import com.android.builder.model.AaptOptions;
-import com.android.ide.common.rendering.api.ResourceNamespace;
 import com.android.resources.ResourceType;
-import com.android.tools.idea.res.LocalResourceRepository;
-import com.android.tools.idea.res.ResourceRepositoryManager;
 import com.android.tools.idea.res.ResourceRepositoryRClass;
-import com.android.utils.Pair;
 import com.intellij.openapi.module.Module;
 import com.intellij.psi.PsiClass;
 import com.intellij.psi.PsiField;
@@ -50,22 +45,6 @@ public class ResourceRepositoryInnerRClass extends InnerRClassBase {
                                                (type, name) -> resourceManager.isResourcePublic(type.getName(), name),
                                                resourceType,
                                                context);
-  }
-
-  @NotNull
-  private static Pair<LocalResourceRepository, ResourceNamespace> pickRepositoryAndNamespace(@NotNull AaptOptions.Namespacing namespacing,
-                                                                                             @NotNull AndroidFacet facet) {
-    ResourceRepositoryManager repositoryManager = ResourceRepositoryManager.getOrCreateInstance(facet);
-    ResourceNamespace namespace;
-    LocalResourceRepository repository;
-    if (namespacing == AaptOptions.Namespacing.DISABLED) {
-      namespace = ResourceNamespace.RES_AUTO;
-      repository = repositoryManager.getAppResources(true);
-    } else {
-      namespace = repositoryManager.getNamespace();
-      repository = repositoryManager.getModuleResources(true);
-    }
-    return Pair.of(repository, namespace);
   }
 
   @NotNull
