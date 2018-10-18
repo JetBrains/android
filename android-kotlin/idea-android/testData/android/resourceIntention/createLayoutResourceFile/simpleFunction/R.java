fe4bced2217e982bb9d1cdeb132df333aae77fd8
Michal Bendowski <bendowski@google.com>
2018-10-18 10:15:15 +0100

    Fix Kotlin tests to work with light R classes
    - Don't copy R.java files into the test projects if light R classes are
    used.
    - Create the iml file of the test module, which is required for light
    classes in Kotlin.
    - Fix res folders or various test cases to match what the R classes are
    supposed to be.
    
    Test: the tests themselves
    Fixes: 117439849
    Change-Id: If1a0c0538eb07bd5cbb8c0d855da84496f507525

--- android-kotlin/idea-android/testData/android/resourceIntention/createLayoutResourceFile/simpleFunction/R.java
+++ android-kotlin/idea-android/testData/android/resourceIntention/createLayoutResourceFile/simpleFunction/R.java
@@ -1,10 +1,7 @@
 package com.myapp;
 
 public final class R {
-    public static final class string {
-
-    }
     public static final class layout {
-
+        public static final int other_activity = 0x7f060021;
     }
 }
