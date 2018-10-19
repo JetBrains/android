84bd869a53c2b2c613440d5053fb3f4c86c13356
Anton Tarasov <anton.tarasov@jetbrains.com>
2018-10-19 22:39:46 +0300

    Improve synchronization in IconLoader and fix warnings

--- android/testSrc/com/android/tools/idea/rendering/FlagManagerTest.java
+++ android/testSrc/com/android/tools/idea/rendering/FlagManagerTest.java
@@ -192,20 +192,13 @@ public class FlagManagerTest extends TestCase {
     assertNotNull(icon);
     assertSame(AndroidIcons.EmptyFlag, icon);
 
-    boolean oldValue = IconLoader.STRICT;
-    IconLoader.STRICT = true;
-    try {
-      icon = FlagManager.get().getFlag("AQ");
-      assertNotNull(icon);
-      assertSame(AndroidIcons.EmptyFlag, icon);
+    icon = IconLoader.performStrictly(() -> FlagManager.get().getFlag("AQ"));
+    assertNotNull(icon);
+    assertSame(AndroidIcons.EmptyFlag, icon);
 
-      icon = FlagManager.get().getFlag("WO"); // Not used in ISO 3166-1
-      assertNotNull(icon);
-      assertSame(AndroidIcons.EmptyFlag, icon);
-    }
-    finally {
-      IconLoader.STRICT = oldValue;
-    }
+    icon = IconLoader.performStrictly(() -> FlagManager.get().getFlag("WO")); // Not used in ISO 3166-1
+    assertNotNull(icon);
+    assertSame(AndroidIcons.EmptyFlag, icon);
   }
 
   public void testKnownFlag() {
