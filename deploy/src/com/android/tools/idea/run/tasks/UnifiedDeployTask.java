c913721cb975fbee604d2a57a075dee05199b31d
Chester Hsieh <dunno@google.com>
2018-10-19 09:55:32 -0700

    Fix unified deploy tests for Bazel.
    This CL refactors the UnifiedDeployTask into two additional components:
    1) An application service that provides dex archive db instance.
    2) An error handler.
    
    Prior to 1), the hardcoded path was causing issues in Bazel tests.
    As for 2), this is to mainly separate out the logic for less spaghetti.
    
    Bug: 112378806
    Change-Id: Ibe47e3546bf1255e8c63da8999c1d299ca45fd6a
    Test: N/A

--- deploy/src/com/android/tools/idea/run/tasks/UnifiedDeployTask.java
+++ deploy/src/com/android/tools/idea/run/tasks/UnifiedDeployTask.java
@@ -16,12 +16,7 @@
 
 package com.android.tools.idea.run.tasks;
 
-import static com.android.tools.deployer.PreswapCheck.RESOURCE_MODIFICATION_NOT_ALLOWED;
-
 import com.android.ddmlib.IDevice;
-import com.android.tools.deploy.swapper.DexArchiveDatabase;
-import com.android.tools.deploy.swapper.SQLiteDexArchiveDatabase;
-import com.android.tools.deploy.swapper.WorkQueueDexArchiveDatabase;
 import com.android.tools.deployer.AdbClient;
 import com.android.tools.deployer.ApkDiffer;
 import com.android.tools.deployer.Deployer;
@@ -31,24 +26,14 @@ import com.android.tools.deployer.Trace;
 import com.android.tools.idea.log.LogWrapper;
 import com.android.tools.idea.run.ApkInfo;
 import com.android.tools.idea.run.ConsolePrinter;
-import com.android.tools.idea.run.ui.ApplyChangesAction;
-import com.android.tools.idea.run.ui.CodeSwapAction;
+import com.android.tools.idea.run.database.DexArchiveDatabaseService;
 import com.android.tools.idea.run.util.LaunchStatus;
-import com.google.common.annotations.VisibleForTesting;
-import com.google.common.base.Splitter;
-import com.google.common.collect.Lists;
-import com.intellij.notification.Notification;
 import com.intellij.notification.NotificationGroup;
 import com.intellij.notification.NotificationListener;
 import com.intellij.notification.NotificationType;
-import com.intellij.openapi.actionSystem.ActionManager;
-import com.intellij.openapi.actionSystem.ActionPlaces;
-import com.intellij.openapi.actionSystem.AnAction;
-import com.intellij.openapi.actionSystem.IdeActions;
 import com.intellij.openapi.application.PathManager;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.ui.playback.commands.ActionCommand;
 import com.intellij.openapi.wm.ToolWindowId;
 import java.io.File;
 import java.io.IOException;
@@ -58,7 +43,6 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
-import javax.swing.event.HyperlinkEvent;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -91,31 +75,13 @@ public class UnifiedDeployTask implements LaunchTask {
 
   private static final String ID = "UNIFIED_DEPLOY";
 
-  @NotNull
-  static final String APPLY_CHANGES_LINK = "apply_changes";
-
-  @NotNull
-  static final String RERUN_LINK = "rerun";
-
-  @VisibleForTesting
-  static final String APPLY_CHANGES_OPTION = "<a href='" + APPLY_CHANGES_LINK + "'>Apply Changes</a>";
-
-  @VisibleForTesting
-  static final String RERUN_OPTION = "<a href='" + RERUN_LINK + "'>Rerun</a>";
-
   private static final NotificationGroup NOTIFICATION_GROUP = NotificationGroup.toolWindowGroup("UnifiedDeployTask", ToolWindowId.RUN);
 
   @NotNull private final Project myProject;
 
   @NotNull private final Collection<ApkInfo> myApks;
 
-  @Nullable private String myFailureReason;
-
-  @Nullable private NotificationListener myNotificationListener;
-
-  // TODO: Move this to an an application component.
-  private static DexArchiveDatabase myDb = new WorkQueueDexArchiveDatabase(new SQLiteDexArchiveDatabase(
-    new File(Paths.get(PathManager.getSystemPath(), ".deploy.db").toString())));
+  @Nullable private DeploymentErrorHandler myDeploymentErrorHandler;
 
   public static final Logger LOG = Logger.getInstance(UnifiedDeployTask.class);
 
@@ -144,13 +110,13 @@ public class UnifiedDeployTask implements LaunchTask {
   @Nullable
   @Override
   public String getFailureReason() {
-    return myFailureReason;
+    return myDeploymentErrorHandler != null ? myDeploymentErrorHandler.getFormattedErrorString() : null;
   }
 
   @Nullable
   @Override
   public NotificationListener getNotificationListener() {
-    return myNotificationListener;
+    return myDeploymentErrorHandler != null ? myDeploymentErrorHandler.getNotificationListener() : null;
   }
 
   @Override
@@ -177,7 +143,8 @@ public class UnifiedDeployTask implements LaunchTask {
       List<String> paths = apk.getFiles().stream().map(apkunit -> apkunit.getApkFile().getPath()).collect(Collectors.toList());
       AdbClient adb = new AdbClient(device, logger);
       Installer installer = new Installer(getLocalInstaller(), adb, logger);
-      Deployer deployer = new Deployer(apk.getApplicationId(), paths, adb, myDb, installer, logger);
+      Deployer deployer = new Deployer(
+        apk.getApplicationId(), paths, adb, DexArchiveDatabaseService.getInstance().getDb(), installer, logger);
       Deployer.RunResponse response;
       try {
         switch (type) {
@@ -201,14 +168,13 @@ public class UnifiedDeployTask implements LaunchTask {
         }
       }
       catch (IOException e) {
-        myFailureReason = "Error deploying APK";
+        myDeploymentErrorHandler = new DeploymentErrorHandler("Error deploying APK");
         LOG.error("Error deploying APK", e);
         return false;
       }
 
       if (response.status == Deployer.RunResponse.Status.ERROR) {
-        myFailureReason = formatDeploymentErrors(type, response.errorMessage);
-        myNotificationListener = new DeploymentErrorNotificationListener();
+        myDeploymentErrorHandler = new DeploymentErrorHandler(type, response.errorMessage);
         LOG.info(response.errorMessage);
         return false;
       }
@@ -253,68 +219,5 @@ public class UnifiedDeployTask implements LaunchTask {
   @Override
   public String getId() {
     return ID;
-  }
-
-  @NotNull
-  @VisibleForTesting
-  static String formatDeploymentErrors(@NotNull DeployType type, @NotNull String errorsString) {
-    List<String> errors = Lists.newArrayList(Splitter.on('\n').split(errorsString));
-    errors.sort(String::compareTo); // Sort the errors so at least they're consistent over runs.
-
-    StringBuilder builder = new StringBuilder();
-
-    if (errors.isEmpty() || type == DeployType.INSTALL) {
-      builder.append("Incompatible changes.");
-    }
-    else {
-      builder.append("Recent changes are incompatible with ");
-      //noinspection EnumSwitchStatementWhichMissesCases
-      switch (type) {
-        case CODE_SWAP:
-          builder.append(CodeSwapAction.NAME);
-          break;
-        case FULL_SWAP:
-          builder.append(ApplyChangesAction.NAME);
-          break;
-      }
-      builder.append(".\n");
-    }
-
-    // TODO(b/117673388): Add "Learn More" hyperlink when we finally have the webpage up.
-    if (type == DeployType.CODE_SWAP && errors.size() == 1 && RESOURCE_MODIFICATION_NOT_ALLOWED.equals(errors.get(0))) {
-      builder.append(APPLY_CHANGES_OPTION);
-      builder.append(" | ");
-    }
-    builder.append(RERUN_OPTION);
-
-    return builder.toString();
-  }
-
-  @VisibleForTesting
-  static class DeploymentErrorNotificationListener implements NotificationListener {
-    @Override
-    public void hyperlinkUpdate(@NotNull Notification notification, @NotNull HyperlinkEvent event) {
-      if (event.getEventType() != HyperlinkEvent.EventType.ACTIVATED) {
-        return;
-      }
-      ActionManager manager = ActionManager.getInstance();
-      String actionId = null;
-      switch (event.getDescription()) {
-        case APPLY_CHANGES_LINK:
-          actionId = ApplyChangesAction.ID;
-          break;
-        case RERUN_LINK:
-          actionId = IdeActions.ACTION_DEFAULT_RUNNER;
-          break;
-      }
-      if (actionId == null) {
-        return;
-      }
-      AnAction action = manager.getAction(actionId);
-      if (action == null) {
-        return;
-      }
-      manager.tryToExecute(action, ActionCommand.getInputEvent(ApplyChangesAction.ID), null, ActionPlaces.UNKNOWN, true);
-    }
   }
 }
