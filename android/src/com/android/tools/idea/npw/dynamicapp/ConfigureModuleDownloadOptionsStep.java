81787346322d96217017ddaf507242f0cf6ac595
Renaud Paquay <rpaquay@google.com>
2018-10-31 13:52:15 -0700

    Skeleton wizard UI for conditional delivery of dynamic feature
    This CL adds the various UI components to the wizard page, without
    any specific associated behavior. The next CL will add properties
    and validation.
    
    Bug: 116788063
    Test: TBD
    Change-Id: I08193806878759dc0fcbb0d1e4e4f64a0dd3910c

--- android/src/com/android/tools/idea/npw/dynamicapp/ConfigureModuleDownloadOptionsStep.java
+++ android/src/com/android/tools/idea/npw/dynamicapp/ConfigureModuleDownloadOptionsStep.java
@@ -15,24 +15,74 @@
  */
 package com.android.tools.idea.npw.dynamicapp;
 
+import static com.android.tools.idea.ui.wizard.StudioWizardStepPanel.wrappedWithVScroll;
 import static org.jetbrains.android.util.AndroidBundle.message;
 
+import com.android.tools.adtui.util.FormScalingUtil;
+import com.android.tools.adtui.validation.ValidatorPanel;
+import com.android.tools.idea.observable.BindingsManager;
+import com.android.tools.idea.observable.ListenerManager;
+import com.android.tools.idea.observable.core.ObservableBool;
 import com.android.tools.idea.wizard.model.ModelWizardStep;
+import com.intellij.ui.components.JBLabel;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
 import javax.swing.JComponent;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public class ConfigureModuleDownloadOptionsStep extends ModelWizardStep<DynamicFeatureModel> {
+  @NotNull
+  private final ValidatorPanel myValidatorPanel;
+  @NotNull
+  private final BindingsManager myBindings = new BindingsManager();
+  @NotNull
+  private final ListenerManager myListeners = new ListenerManager();
+
   private JPanel myRootPanel;
+  @SuppressWarnings("unused") private JBLabel myFeatureTitleLabel;
+  private JTextField myFeatureTitle;
+  @SuppressWarnings("unused") private JCheckBox myFusingCheckBox;
+  private JComboBox<DownloadInstallType> myInstallationOptionCombo;
 
   public ConfigureModuleDownloadOptionsStep(@NotNull DynamicFeatureModel model) {
     super(model, message("android.wizard.module.new.dynamic.download.options"));
 
+    myInstallationOptionCombo.setModel(new DefaultComboBoxModel<>(DownloadInstallType.values()));
+
+    myValidatorPanel = new ValidatorPanel(this, wrappedWithVScroll(myRootPanel));
+    FormScalingUtil.scaleComponentTree(this.getClass(), myValidatorPanel);
   }
 
   @NotNull
   @Override
   protected JComponent getComponent() {
     return myRootPanel;
+  }
+
+  @Nullable
+  @Override
+  protected JComponent getPreferredFocusComponent() {
+    return myFeatureTitle;
+  }
+
+  @NotNull
+  @Override
+  protected ObservableBool canGoForward() {
+    return myValidatorPanel.hasErrors().not();
+  }
+
+  @Override
+  public void dispose() {
+    myBindings.releaseAll();
+    myListeners.releaseAll();
+  }
+
+  @Override
+  protected boolean shouldShow() {
+    return !getModel().instantModule().get();
   }
 }
