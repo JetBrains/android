d2902a8a624240cd7d99697ddfe134e47ecab312
Jens Ole Lauridsen <jlauridsen@google.com>
2018-11-01 21:51:39 -0700

    Change the MotionPropertiesModel to override from Nele.
    The property items will be of the type NelePropertyItem.
    This will allow us to reuse a lot of the features from Nele such
    as completions, validations, adjustable property tables, etc.
    
    Bug: 118388686
    Test: Existing tests should pass.
    
    Change-Id: Ic37f562761b34b3a3580b4c01126ac3611e76a9a

--- designer/src/com/android/tools/idea/uibuilder/handlers/motion/property2/MotionLayoutAttributesModel.java
+++ designer/src/com/android/tools/idea/uibuilder/handlers/motion/property2/MotionLayoutAttributesModel.java
@@ -15,22 +15,18 @@
  */
 package com.android.tools.idea.uibuilder.handlers.motion.property2;
 
-import com.android.tools.idea.common.model.NlComponent;
 import com.android.tools.idea.common.property2.api.PropertiesModel;
-import com.android.tools.idea.common.property2.api.PropertiesModelListener;
-import com.android.tools.idea.common.property2.api.PropertiesTable;
 import com.android.tools.idea.common.surface.DesignSurface;
 import com.android.tools.idea.uibuilder.api.AccessoryPanelInterface;
-import com.android.tools.idea.uibuilder.api.AccessorySelectionListener;
-import com.android.tools.idea.uibuilder.handlers.motion.MotionLayoutTimelineHelper;
+import com.android.tools.idea.uibuilder.handlers.motion.timeline.GanttEventListener;
+import com.android.tools.idea.uibuilder.property2.NelePropertiesModel;
+import com.android.tools.idea.uibuilder.property2.NelePropertyItem;
 import com.intellij.openapi.Disposable;
 import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.util.Disposer;
+import com.intellij.openapi.application.TransactionGuard;
+import com.intellij.openapi.command.WriteCommandAction;
 import com.intellij.psi.SmartPsiElementPointer;
 import com.intellij.psi.xml.XmlTag;
-import java.util.ArrayList;
-import java.util.List;
 import org.jetbrains.android.facet.AndroidFacet;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -38,85 +34,62 @@ import org.jetbrains.annotations.Nullable;
 /**
  * {@link PropertiesModel} for motion layout property editor.
  */
-public class MotionLayoutAttributesModel implements PropertiesModel<MotionPropertyItem>, Disposable {
-  private final AndroidFacet myFacet;
-  private final List<PropertiesModelListener> myListeners;
-  private final MotionLayoutPropertyProvider myPropertyProvider;
-  private final MotionLayoutTimelineHelper myTimelineHelper;
-  private PropertiesTable<MotionPropertyItem> myPropertiesTable;
+public class MotionLayoutAttributesModel extends NelePropertiesModel {
 
   public MotionLayoutAttributesModel(@NotNull Disposable parentDisposable, @NotNull AndroidFacet facet) {
-    myFacet = facet;
-    myListeners = new ArrayList<>();
-    myPropertyProvider = new MotionLayoutPropertyProvider(this);
-    myTimelineHelper = new MotionLayoutTimelineHelper(new TimelineListener());
-    myPropertiesTable = PropertiesTable.Companion.emptyTable();
-    Disposer.register(parentDisposable, this);
+    super(parentDisposable, new MotionLayoutPropertyProvider(facet), facet);
   }
 
   @Override
-  public void dispose() {
-    myTimelineHelper.setSurface(null);
-  }
-
-  @NotNull
-  public AndroidFacet getFacet() {
-    return myFacet;
-  }
-
-  @NotNull
-  public Project getProject() {
-    return myFacet.getModule().getProject();
-  }
-
   @Nullable
-  public DesignSurface getSurface() {
-    return myTimelineHelper.getSurface();
-  }
-
-  public void setSurface(@Nullable DesignSurface surface) {
-    myTimelineHelper.setSurface(surface);
-  }
-
-  @NotNull
-  @Override
-  public PropertiesTable<MotionPropertyItem> getProperties() {
-    return myPropertiesTable;
+  public String getPropertyValue(@NotNull NelePropertyItem property) {
+    XmlTag tag = getTag(property);
+    if (tag == null) {
+      return null;
+    }
+    return tag.getAttributeValue(property.getName(), property.getNamespace());
   }
 
   @Override
-  public void deactivate() {
-    myPropertiesTable = PropertiesTable.Companion.emptyTable();
-  }
-
-  @Override
-  public void addListener(@NotNull PropertiesModelListener listener) {
-    myListeners.add(listener);
-  }
-
-  @Override
-  public void removeListener(@NotNull PropertiesModelListener listener) {
-    myListeners.remove(listener);
-  }
-
-  private void displayItem(@NotNull SmartPsiElementPointer<XmlTag> tagPointer, @NotNull NlComponent component) {
-    ApplicationManager.getApplication().invokeLater(() -> {
-      myPropertiesTable = myPropertyProvider.getProperties(component, tagPointer);
-      List<PropertiesModelListener> copy = new ArrayList<>(myListeners);
-      //noinspection unchecked
-      copy.forEach(listener -> listener.propertiesGenerated(this));
+  public void setPropertyValue(@NotNull NelePropertyItem property, @Nullable String newValue) {
+    ApplicationManager.getApplication().assertIsDispatchThread();
+    TransactionGuard.submitTransaction(this, () -> {
+      XmlTag tag = getTag(property);
+      if (tag != null) {
+        WriteCommandAction.runWriteCommandAction(
+          getFacet().getModule().getProject(),
+          "Set $componentName.$name to $newValue",
+          null,
+          () -> tag.setAttribute(property.getName(), property.getNamespace(), newValue));
+      }
     });
   }
 
-  private class TimelineListener implements AccessorySelectionListener {
-    @Override
-    public void selectionChanged(@NotNull AccessoryPanelInterface accessoryPanel, @NotNull List<NlComponent> selection) {
-      @SuppressWarnings("unchecked")
-      SmartPsiElementPointer<XmlTag> tagPointer = (SmartPsiElementPointer<XmlTag>)accessoryPanel.getSelectedAccessory();
-      NlComponent component = selection.isEmpty() ? null : selection.get(0);
-      if (tagPointer != null && component != null) {
-        displayItem(tagPointer, component);
-      }
+  @Override
+  protected void updateDesignSurface(@Nullable DesignSurface oldSurface, @Nullable DesignSurface newSurface) {
+    setAccessoryPanelListener(oldSurface, newSurface);
+    useCurrentPanel(newSurface);
+  }
+
+  @Override
+  protected boolean wantComponentSelectionUpdate(@Nullable DesignSurface surface,
+                                                 @Nullable DesignSurface activeSurface,
+                                                 @Nullable AccessoryPanelInterface activePanel) {
+    return false;
+  }
+
+  @Override
+  protected boolean wantPanelSelectionUpdate(@NotNull AccessoryPanelInterface panel, @Nullable AccessoryPanelInterface activePanel) {
+    return panel == activePanel && panel.getSelectedAccessory() != null && panel instanceof GanttEventListener;
+  }
+
+  @Nullable
+  public static XmlTag getTag(@NotNull NelePropertyItem property) {
+    @SuppressWarnings("unchecked")
+    SmartPsiElementPointer<XmlTag> tagPointer = (SmartPsiElementPointer<XmlTag>)property.getOptionalValue();
+    if (tagPointer == null) {
+      return null;
     }
+    return tagPointer.getElement();
   }
 }
