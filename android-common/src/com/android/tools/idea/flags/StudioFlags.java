f2f53cf6630c06463e3dd69a3e250aa1cc09f95e
Juan C Nuno <juancnuno@google.com>
2018-11-05 15:38:48 -0800

    Tie the new deployment flag to the new Instant Run
    Bug: Not applicable
    Test: Not applicable
    Change-Id: I5f6ab69baf815f5de47c5decbd9b695a943432db

--- android-common/src/com/android/tools/idea/flags/StudioFlags.java
+++ android-common/src/com/android/tools/idea/flags/StudioFlags.java
@@ -307,13 +307,6 @@ public final class StudioFlags {
     "Changing the value of this flag requires restarting Android Studio.",
     true);
 
-  public static final Flag<Boolean> SELECT_DEVICE_SNAPSHOT_COMBO_BOX_VISIBLE = Flag.create(
-    RUNDEBUG,
-    "select.device.snapshot.combo.box.visible",
-    "Show the Select Device/Snapshot combo box",
-    "Show the Select Device/Snapshot combo box next to the Select Run/Debug Configuration one in the toolbar",
-    false);
-
   public static final Flag<Boolean> JVMTI_REFRESH = Flag.create(
     RUNDEBUG,
     "jvmti.refresh",
@@ -321,12 +314,19 @@ public final class StudioFlags {
     "Use JVMTI to support application refresh. This implies incremental deployment",
     false);
 
+  public static final Flag<Boolean> SELECT_DEVICE_SNAPSHOT_COMBO_BOX_VISIBLE = Flag.create(
+    RUNDEBUG,
+    "select.device.snapshot.combo.box.visible",
+    "Show the Select Device/Snapshot combo box",
+    "Show the Select Device/Snapshot combo box next to the Select Run/Debug Configuration one in the toolbar",
+    JVMTI_REFRESH.get());
+
   public static final Flag<Boolean> SELECT_DEVICE_SNAPSHOT_COMBO_BOX_SNAPSHOTS_ENABLED = Flag.create(
     RUNDEBUG,
     "select.device.snapshot.combo.box.snapshots.enabled",
     "Enable Select Device/Snapshot combo box snapshots",
     "So the new Instant Run can use the combo box",
-    JVMTI_REFRESH.get());
+    false);
 
   private static final FlagGroup GRADLE_IDE = new FlagGroup(FLAGS, "gradle.ide", "Gradle Project System");
   public static final Flag<Boolean> FIX_ANDROID_RUN_CONFIGURATIONS_ENABLED = Flag.create(
