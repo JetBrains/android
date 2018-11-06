69c2559a76749aacb0d943b2b57d8777c8222cd7
Siva Velusamy <vsiva@google.com>
2018-11-06 10:46:57 -0800

    Add a fast path for finding the PsiField given the field name
    The default implementation in AndroidLightClassBase has two flaws:
     * It needs to construct all the PsiFields before finding
       the required one.
     * In order to identify whether a field is final or not, it relies
       on AndroidCompileUtil.findCircularDependencyOnLibraryWithSamePackage,
       which is expensive due to its use of IntelliJ's XML DOM API
       to construct Manifest classes.
    
    As a result of these two issues, AndroidResourceUtil.findResourceFields
    takes over 5 minutes sometimes in large projects (> 500 modules with
    many inter dependencies). This CL adds a fast path for the specific case
    of library modules where we know that the fields are all non final. In
    such a case, we can get away with constructing just the requested
    PsiField. As a result, the running time is ~5 seconds on the first run,
    and just a few millis on subsequent invocations (compare to multiple
    minutes in both scenarios earlier).
    
    Bug: 112610798
    Test: existing tests covering find usages
    Change-Id: I355e22dc0133296f65a7e28e7a33ff240c14294a

--- android/src/org/jetbrains/android/augment/ResourceRepositoryInnerRClass.java
+++ android/src/org/jetbrains/android/augment/ResourceRepositoryInnerRClass.java
@@ -1,10 +1,14 @@
 package org.jetbrains.android.augment;
 
+import com.android.ide.common.resources.ResourceItem;
 import com.android.resources.ResourceType;
+import com.android.tools.idea.flags.StudioFlags;
 import com.android.tools.idea.res.ResourceRepositoryRClass;
 import com.intellij.openapi.module.Module;
 import com.intellij.psi.PsiClass;
 import com.intellij.psi.PsiField;
+import com.intellij.psi.PsiType;
+import java.util.List;
 import org.jetbrains.android.compiler.AndroidCompileUtil;
 import org.jetbrains.android.facet.AndroidFacet;
 import org.jetbrains.android.resourceManagers.LocalResourceManager;
@@ -15,7 +19,6 @@ import org.jetbrains.annotations.NotNull;
  * Implementation of {@link InnerRClassBase} back by a resource repository.
  */
 public class ResourceRepositoryInnerRClass extends InnerRClassBase {
-
   @NotNull private final AndroidFacet myFacet;
   @NotNull private final ResourceRepositoryRClass.ResourcesSource mySource;
 
@@ -51,5 +54,35 @@ public class ResourceRepositoryInnerRClass extends InnerRClassBase {
   @Override
   protected PsiField[] doGetFields() {
     return buildLocalResourceFields(myFacet, myResourceType, mySource, this);
+  }
+
+  /**
+   * {@inheritDoc}
+   *
+   * This implementation adds a fast path for non final resources and delegates to the super implementation in
+   * {@link AndroidLightClassBase#findFieldByName(String, boolean)} for everything else. The super implementation
+   * relies on first creating the PsiFields for *all* resources and then searching for the requested field.
+   * In addition, the logic for determining whether a field should be final or not relies on a very expensive
+   * Manifest class creation {@link AndroidCompileUtil#findCircularDependencyOnLibraryWithSamePackage(AndroidFacet)}.
+   * Both of those operations are avoided for the common case in this implementation.
+   */
+  @Override
+  public PsiField findFieldByName(String name, boolean checkBases) {
+    // bail if this is a scenario we don't fully support
+    if (!StudioFlags.IN_MEMORY_R_CLASSES.get()  // only works with full in memory R classes, not for example with "augmented" classes
+        || myResourceType == ResourceType.STYLEABLE // styleables require further modification of the name to handle sub attributes
+        || !myFacet.getConfiguration().isLibraryProject()) { // app projects use final ids, which requires assigning ids to all fields
+      return super.findFieldByName(name, checkBases);
+    }
+
+    if (!mySource.getResourceRepository().hasResources(mySource.getResourceNamespace(), myResourceType, name)) {
+      return null;
+    }
+
+    return new AndroidLightField(name,
+                                 this,
+                                 PsiType.INT,
+                                 AndroidLightField.FieldModifier.NON_FINAL,
+                                 null);
   }
 }
