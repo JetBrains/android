12d0814b064a765a88cb056d5c51af695419e301
Jens Ole Lauridsen <jlauridsen@google.com>
2018-11-08 09:10:26 -0800

    Start using the generic PTableModel
    Use the generic PTableModel in Nele for:
    - the declared properties table
    - the all properties table
    
    And in the motion editor for:
    - the general properties table
    - the custom properties table
    
    Bug: 73778668
    Test: This is a refactoring, existing tests should pass.
    Change-Id: Ia4b8fbeccf48054f71222f6000712f7552b6d359

---
+++ designer/src/com/android/tools/idea/uibuilder/handlers/motion/property2/action/AddCustomFieldAction.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.uibuilder.handlers.motion.property2.action;
+
+import com.android.tools.idea.common.property2.api.FilteredPTableModel;
+import com.android.tools.idea.uibuilder.handlers.motion.attributeEditor.NewCustomAttributePanel;
+import com.android.tools.idea.uibuilder.handlers.motion.property2.MotionLayoutAttributesModel;
+import com.android.tools.idea.uibuilder.handlers.motion.property2.MotionLayoutPropertyProvider;
+import com.android.tools.idea.uibuilder.handlers.motion.timeline.MotionSceneModel;
+import com.android.tools.idea.uibuilder.property2.NelePropertyItem;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.util.text.StringUtil;
+import com.intellij.psi.xml.XmlTag;
+import icons.StudioIcons;
+import java.util.function.Consumer;
+import org.jetbrains.annotations.NotNull;
+
+public class AddCustomFieldAction extends AnAction {
+  private final FilteredPTableModel<NelePropertyItem> myTableModel;
+  private final NelePropertyItem myProperty;
+  private final MotionLayoutAttributesModel myModel;
+
+  public AddCustomFieldAction(@NotNull FilteredPTableModel<NelePropertyItem> tableModel, @NotNull NelePropertyItem property) {
+    super(null, "Add Custom Property", StudioIcons.Common.ADD);
+    myTableModel = tableModel;
+    myProperty = property;
+    myModel = (MotionLayoutAttributesModel)myProperty.getModel();
+  }
+
+  @Override
+  public void actionPerformed(@NotNull AnActionEvent event) {
+    NewCustomAttributePanel newAttributePanel = new NewCustomAttributePanel();
+    newAttributePanel.show();
+    if (!newAttributePanel.isOK()) {
+      return;
+    }
+    String attributeName = newAttributePanel.getAttributeName();
+    String value = newAttributePanel.getInitialValue();
+    MotionSceneModel.CustomAttributes.Type type = newAttributePanel.getType();
+    if (StringUtil.isEmpty(attributeName)) {
+      return;
+    }
+    XmlTag tag = MotionLayoutAttributesModel.getTag(myProperty);
+    if (tag == null) {
+      return;
+    }
+    Consumer<XmlTag> applyToModel = newCustomTag -> {
+      NelePropertyItem newProperty = MotionLayoutPropertyProvider.createCustomProperty(
+        attributeName, type.getTagName(), newCustomTag, myProperty.getModel(), myProperty.getComponents());
+      myTableModel.addNewItem(newProperty);
+    };
+
+    myModel.createCustomXmlTag(tag, attributeName, value, type, applyToModel);
+  }
+}
---
+++ designer/src/com/android/tools/idea/uibuilder/handlers/motion/property2/action/DeleteCustomFieldAction.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.uibuilder.handlers.motion.property2.action;
+
+import com.android.tools.idea.common.property2.api.FilteredPTableModel;
+import com.android.tools.idea.common.property2.api.TableLineModel;
+import com.android.tools.idea.uibuilder.handlers.motion.property2.MotionLayoutAttributesModel;
+import com.android.tools.idea.uibuilder.property2.NelePropertyItem;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.psi.xml.XmlTag;
+import icons.StudioIcons;
+import org.jetbrains.annotations.NotNull;
+
+public class DeleteCustomFieldAction extends AnAction {
+  private final FilteredPTableModel<NelePropertyItem> myTableModel;
+  private TableLineModel myLineModel;
+
+  public DeleteCustomFieldAction(@NotNull FilteredPTableModel<NelePropertyItem> tableModel) {
+    super(null, "Remove Selected Property", StudioIcons.Common.REMOVE);
+    myTableModel = tableModel;
+  }
+
+  public void setLineModel(@NotNull TableLineModel lineModel) {
+    myLineModel = lineModel;
+  }
+
+  @Override
+  public void actionPerformed(@NotNull AnActionEvent event) {
+    if (myLineModel == null) {
+      return;
+    }
+    NelePropertyItem property = (NelePropertyItem)myLineModel.getSelectedItem();
+    if (property == null) {
+      return;
+    }
+    XmlTag tag = MotionLayoutAttributesModel.getTag(property);
+    if (tag == null) {
+      return;
+    }
+    Runnable applyToModel = () -> myTableModel.deleteItem(property);
+
+    MotionLayoutAttributesModel model = (MotionLayoutAttributesModel)property.getModel();
+    model.deleteTag(tag, applyToModel);
+  }
+}
