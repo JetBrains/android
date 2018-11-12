1145bf3865034ff20c2a257eee34519a6106c980
parentej <parentej@google.com>
2018-11-12 15:08:29 +0000

    AssertionError: Qualified name is not allowed: @android:style/Theme.DeviceDefault
    java.lang.AssertionError: Qualified name is not allowed: "@android:style/Theme.DeviceDefault"
    while adding Blank wear activity to Watch Face project.
    ThemeHelper was using a deprecated API to find the the theme style, and passing
    the wrong Theme name (was removing "android:" from the theme name but not "@android:").
    This CL refactors the code to use the new namespace api.
    
    Bug: 116684080
    Test: Added ThemeHelperTest
    Change-Id: I8c6a3a33b372a98cbd9b8e5ee0c55c2ef2e9bd73
    (cherry picked from commit c5d4aed885e35332633ba7ec9ee4a69a22979bcb)

---
+++ android/testSrc/com/android/tools/idea/configurations/ThemeHelperTest.kt
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.configurations
+
+import com.android.tools.idea.npw.ThemeHelper
+import org.jetbrains.android.AndroidTestCase
+
+class ThemeHelperTest : AndroidTestCase() {
+
+  fun testThemeExists() {
+    val layoutFile = myFixture.copyFileToProject("xmlpull/layout.xml", "res/layout/layout1.xml")
+    val configuration = ConfigurationManager.getOrCreateInstance(myModule).getConfiguration(layoutFile)
+    assertTrue(ThemeHelper.themeExists (configuration, "@android:style/Theme.DeviceDefault"))
+  }
+
+  fun testNoThemeExists() {
+    val layoutFile = myFixture.copyFileToProject("xmlpull/layout.xml", "res/layout/layout1.xml")
+    val configuration = ConfigurationManager.getOrCreateInstance(myModule).getConfiguration(layoutFile)
+    assertFalse(ThemeHelper.themeExists(configuration, "@NoExistingTheme"))
+  }
+
+  fun testThemeWithNoAtSymbol() {
+    val layoutFile = myFixture.copyFileToProject("xmlpull/layout.xml", "res/layout/layout1.xml")
+    val configuration = ConfigurationManager.getOrCreateInstance(myModule).getConfiguration(layoutFile)
+    assertFalse(ThemeHelper.themeExists (configuration, "android:style/Theme.DeviceDefault"))
+  }
+
+  fun testHasActionBar() {
+    val layoutFile = myFixture.copyFileToProject("xmlpull/layout.xml", "res/layout/layout1.xml")
+    val configuration = ConfigurationManager.getOrCreateInstance(myModule).getConfiguration(layoutFile)
+    assertTrue(ThemeHelper.hasActionBar(configuration, "@android:style/Theme.DeviceDefault"))
+  }
+}
