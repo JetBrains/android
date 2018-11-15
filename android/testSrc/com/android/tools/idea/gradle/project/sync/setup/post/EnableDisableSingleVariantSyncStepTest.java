e282c5c25f759b1ad16632a5c9054bd271496115
Ying Chen <chying@google.com>
2018-11-15 15:24:43 -0800

    Update single-variant eligibility.
    Enable the code to update NOT_ELIGIBLE_FOR_SINGLE_VARIANT_SYNC
    flag.
    
    Bug: NA
    Test: testSetSingleVariantSyncState
    Change-Id: Id69e1ccf627f93d28241009a69271c5578eb9e68

--- android/testSrc/com/android/tools/idea/gradle/project/sync/setup/post/EnableDisableSingleVariantSyncStepTest.java
+++ android/testSrc/com/android/tools/idea/gradle/project/sync/setup/post/EnableDisableSingleVariantSyncStepTest.java
@@ -17,11 +17,14 @@ package com.android.tools.idea.gradle.project.sync.setup.post;
 
 import com.android.tools.idea.flags.StudioFlags;
 import com.android.tools.idea.testing.AndroidGradleTestCase;
+import com.intellij.ide.util.PropertiesComponent;
 import com.intellij.openapi.project.Project;
 
 import static com.android.tools.idea.Projects.getBaseDirPath;
+import static com.android.tools.idea.gradle.project.sync.ng.NewGradleSync.NOT_ELIGIBLE_FOR_SINGLE_VARIANT_SYNC;
 import static com.android.tools.idea.gradle.project.sync.setup.post.EnableDisableSingleVariantSyncStep.EligibilityState.*;
 import static com.android.tools.idea.gradle.project.sync.setup.post.EnableDisableSingleVariantSyncStep.isEligibleForSingleVariantSync;
+import static com.android.tools.idea.gradle.project.sync.setup.post.EnableDisableSingleVariantSyncStep.setSingleVariantSyncState;
 import static com.android.tools.idea.testing.TestProjectPaths.HELLO_JNI;
 import static com.android.tools.idea.testing.TestProjectPaths.KOTLIN_GRADLE_DSL;
 import static com.android.tools.idea.testing.TestProjectPaths.PURE_JAVA_PROJECT;
@@ -59,5 +62,15 @@ public class EnableDisableSingleVariantSyncStepTest extends AndroidGradleTestCas
     loadProject(KOTLIN_GRADLE_DSL);
     assertEquals(HAS_KOTLIN, isEligibleForSingleVariantSync(getProject()));
     StudioFlags.NEW_SYNC_INFRA_ENABLED.clearOverride();
+  }
+
+  public void testSetSingleVariantSyncState() {
+    Project project = getProject();
+    // By default project is eligible.
+    assertFalse(PropertiesComponent.getInstance(project).getBoolean(NOT_ELIGIBLE_FOR_SINGLE_VARIANT_SYNC));
+    // Update eligibility state.
+    setSingleVariantSyncState(project);
+    // Verify the flag is updated to true.
+    assertTrue(PropertiesComponent.getInstance(project).getBoolean(NOT_ELIGIBLE_FOR_SINGLE_VARIANT_SYNC));
   }
 }
