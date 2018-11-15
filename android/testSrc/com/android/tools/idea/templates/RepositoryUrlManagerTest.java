fabbea78b291b28d0932f844e3994204e39df5b0
parentej <parentej@google.com>
2018-11-15 14:49:45 +0000

    Remove Android SDK m2repository - rm getHighestInstalledVersion()
    When installing the Android SDK, we stop installing by
    default "Android Support Repository" and "Google Repository".
    All dependencies are hosted in maven.google.com
    This CL removes calls to SdkMavenRepository@getHighestInstalledVersion()
    since even if the android/google local repos exist, they are not updated
    for a long time and can't have a valid latest version.
    
    Bug: 117207347
    Test: All existing tests should pass, as we removed a dead code path
    Change-Id: Ie51787a6422e7bbe855a115a6f80d3767a42af0a

--- android/testSrc/com/android/tools/idea/templates/RepositoryUrlManagerTest.java
+++ android/testSrc/com/android/tools/idea/templates/RepositoryUrlManagerTest.java
@@ -60,12 +60,14 @@ public class RepositoryUrlManagerTest extends AndroidGradleTestCase {
     "  <com.android.support.constraint/>\n" +
     "  <com.android.support/>\n" +
     "  <com.google.android.gms/>\n" +
+    "  <com.google.android.support/>\n" +
     "</metadata>";
 
   private static final String SUPPORT_GROUP =
    "<?xml version='1.0' encoding='UTF-8'?>\n"+
     "<com.android.support>\n"+
     "  <support-v4 versions=\"26.0.2,26.0.2\"/>\n"+
+    "  <appcompat-v7 versions=\"18.0.0,19.0.0,19.0.1,19.1.0,20.0.0,21.0.0,21.0.2,22.0.0-alpha1\"/>\n"+
     "</com.android.support>\n";
 
   private static final String CONTRAINT_GROUP =
@@ -78,13 +80,20 @@ public class RepositoryUrlManagerTest extends AndroidGradleTestCase {
   private static final String PLAY_SERVICES_GROUP =
     "<?xml version='1.0' encoding='UTF-8'?>\n" +
     "<com.google.android.gms>\n" +
-    "  <play-services versions=\"11.1.0,11.2.0-beta1\"/>\n" +
+    "  <play-services versions=\"4.4.52,11.1.0,11.2.0-beta1\"/>\n" +
     "  <play-services-ads versions=\"11.1.0,11.2.0-beta1\"/>\n" +
     "</com.google.android.gms>\n";
+
+  private static final String WEARABLE_GROUP =
+    "<?xml version='1.0' encoding='UTF-8'?>\n" +
+    "<com.google.android.support>\n" +
+    "  <wearable versions=\"1.0.0,2.0.0-alpha2\"/>\n" +
+    "</com.google.android.support>\n";
 
   private static final Map<String, String> OFFLINE_CACHE =
     ImmutableMap.of("master-index.xml", MASTER_INDEX,
                     "com/google/android/gms/group-index.xml", PLAY_SERVICES_GROUP,
+                    "com/google/android/support/group-index.xml", WEARABLE_GROUP,
                     "com/android/support/group-index.xml", SUPPORT_GROUP,
                     "com/android/support/constraint/group-index.xml", CONTRAINT_GROUP);
 
@@ -96,52 +105,6 @@ public class RepositoryUrlManagerTest extends AndroidGradleTestCase {
     mySdkHandler = new AndroidSdkHandler(SDK_DIR, ANDROID_HOME, myFileOp);
     mySdk = Mockito.mock(AndroidSdkData.class);
     Mockito.when(mySdk.getLocation()).thenReturn(SDK_DIR);
-
-    String[] paths = new String[]{
-      // Android repository
-      "extras/android/m2repository/com/android/support/appcompat-v7/18.0.0/appcompat-v7-18.0.0.aar",
-      "extras/android/m2repository/com/android/support/appcompat-v7/19.0.0/appcompat-v7-19.0.0.aar",
-      "extras/android/m2repository/com/android/support/appcompat-v7/19.0.1/appcompat-v7-19.0.1.aar",
-      "extras/android/m2repository/com/android/support/appcompat-v7/19.1.0/appcompat-v7-19.1.0.aar",
-      "extras/android/m2repository/com/android/support/appcompat-v7/20.0.0/appcompat-v7-20.0.0.aar",
-      "extras/android/m2repository/com/android/support/appcompat-v7/21.0.0/appcompat-v7-21.0.0.aar",
-      "extras/android/m2repository/com/android/support/appcompat-v7/21.0.2/appcompat-v7-21.0.2.aar",
-      "extras/android/m2repository/com/android/support/appcompat-v7/22.0.0-alpha1/appcompat-v7-22.0.0-alpha1.aar",
-      "extras/android/m2repository/com/android/support/cardview-v7/21.0.0/cardview-v7-21.0.0.aar",
-      "extras/android/m2repository/com/android/support/cardview-v7/21.0.2/cardview-v7-21.0.2.aar",
-      "extras/android/m2repository/com/android/support/support-v13/20.0.0/support-v13-20.0.0.aar",
-      "extras/android/m2repository/com/android/support/support-v13/21.0.0/support-v13-21.0.0.aar",
-      "extras/android/m2repository/com/android/support/support-v13/21.0.2/support-v13-21.0.2.aar",
-      "extras/android/m2repository/com/android/support/support-v4/13.0.0/support-v4-13.0.0.jar", // JARs were used before 19.0.0
-      "extras/android/m2repository/com/android/support/support-v4/19.0.1/support-v4-19.0.1.aar",
-      "extras/android/m2repository/com/android/support/support-v4/20.0.0/support-v4-20.0.0.aar",
-      "extras/android/m2repository/com/android/support/support-v4/20.0.0/support-v4-20.0.0-rc1.aar",
-      "extras/android/m2repository/com/android/support/support-v4/21.0.0/support-v4-21.0.0.aar",
-      "extras/android/m2repository/com/android/support/support-v4/21.0.2/support-v4-21.0.2.aar",
-      "extras/android/m2repository/com/android/support/constraint/constraint-layout/1.0.0-alpha7/constraint-layout-1.0.0-alpha7.aar",
-
-      // Google repository
-      "extras/google/m2repository/com/google/android/gms/play-services/3.1.36/play-services-3.1.36.aar",
-      "extras/google/m2repository/com/google/android/gms/play-services/3.1.59/play-services-3.1.59.aar",
-      "extras/google/m2repository/com/google/android/gms/play-services/3.2.25/play-services-3.2.25.aar",
-      "extras/google/m2repository/com/google/android/gms/play-services/3.2.65/play-services-3.2.65.aar",
-      "extras/google/m2repository/com/google/android/gms/play-services/4.0.30/play-services-4.0.30.aar",
-      "extras/google/m2repository/com/google/android/gms/play-services/4.1.32/play-services-4.1.32.aar",
-      "extras/google/m2repository/com/google/android/gms/play-services/4.2.42/play-services-4.2.42.aar",
-      "extras/google/m2repository/com/google/android/gms/play-services/4.3.23/play-services-4.3.23.aar",
-      "extras/google/m2repository/com/google/android/gms/play-services/4.4.52/play-services-4.4.52.aar",
-      "extras/google/m2repository/com/google/android/gms/play-services/5.0.89/play-services-5.0.89.aar",
-      "extras/google/m2repository/com/google/android/gms/play-services/5.2.08/play-services-5.2.08.aar",
-      "extras/google/m2repository/com/google/android/gms/play-services-wearable/5.0.77/play-services-wearable-5.0.77.aar",
-      "extras/google/m2repository/com/google/android/gms/play-services-wearable/6.1.11/play-services-wearable-6.1.11.aar",
-      "extras/google/m2repository/com/google/android/gms/play-services-wearable/6.1.71/play-services-wearable-6.1.71.aar",
-      "extras/google/m2repository/com/google/android/support/wearable/1.0.0/wearable-1.0.0.aar",
-      "extras/google/m2repository/com/google/android.wearable/wearable/2.0.0-alpha2/wearable-2.0.0-alpha2.aar",
-    };
-
-    for (String path : paths) {
-      myFileOp.createNewFile(new File(SDK_DIR, path));
-    }
   }
 
   /**
@@ -191,20 +154,6 @@ public class RepositoryUrlManagerTest extends AndroidGradleTestCase {
     // Check without metadata file.
     assertEquals("11.1.0", getLibraryRevision(GoogleMavenArtifactId.PLAY_SERVICES_ADS, false));
     assertEquals("11.2.0-beta1", getLibraryRevision(GoogleMavenArtifactId.PLAY_SERVICES_ADS, true));
-  }
-
-  public void testGetArchiveForCoordinate() throws Exception {
-    checkGetArchiveForCoordinate("com.android.support:support-v4:20.0.0",
-                                 "/extras/android/m2repository/com/android/support/support-v4/20.0.0/support-v4-20.0.0.aar");
-
-    checkGetArchiveForCoordinate("com.android.support:support-v4:13.0.0",
-                                 "/extras/android/m2repository/com/android/support/support-v4/13.0.0/support-v4-13.0.0.jar");
-
-    // Unknown version:
-    checkGetArchiveForCoordinate("com.android.support:support-v4:20.4.0", null);
-
-    // Unknown library
-    checkGetArchiveForCoordinate("com.google.guava:guava:19.0", null);
   }
 
   private void checkGetArchiveForCoordinate(String coordinateString, String path) {
@@ -257,12 +206,6 @@ public class RepositoryUrlManagerTest extends AndroidGradleTestCase {
     return myRepositoryUrlManager.resolveDynamicCoordinate(coordinate, null, mySdkHandler);
   }
 
-  public void testResolvedCoordinate_sdkMissing() throws Exception {
-    myFileOp.deleteFileOrFolder(SDK_DIR);
-    GradleCoordinate coordinate = GradleCoordinate.parseCoordinateString("com.google.android.gms:play-services:4.+");
-    assertNull(resolveDynamicCoordinateVersion(coordinate));
-  }
-
   public void testResolvedCoordinateLocalFirst() throws Exception {
     RemotePackage pkg = new FakePackage.FakeRemotePackage("extras;m2repository;com;google;android;gms;play-services;4.5.0");
     RepositoryPackages pkgs = new RepositoryPackages(ImmutableList.of(), ImmutableList.of(pkg));
@@ -271,16 +214,6 @@ public class RepositoryUrlManagerTest extends AndroidGradleTestCase {
     GradleCoordinate coordinate = GradleCoordinate.parseCoordinateString("com.google.android.gms:play-services:4.+");
     assertNotNull(coordinate);
     assertEquals("4.4.52", resolveDynamicCoordinateVersion(coordinate));
-  }
-
-  public void testResolvedCoordinateRemoteSdk() throws Exception {
-    RemotePackage pkg = new FakePackage.FakeRemotePackage("extras;m2repository;com;google;android;gms;play-services;4.5.0");
-    RepositoryPackages pkgs = new RepositoryPackages(ImmutableList.of(), ImmutableList.of(pkg));
-    RepoManager mgr = new FakeRepoManager(pkgs);
-    mySdkHandler = new AndroidSdkHandler(new File("/emptysdk"), ANDROID_HOME, myFileOp, mgr);
-    GradleCoordinate coordinate = GradleCoordinate.parseCoordinateString("com.google.android.gms:play-services:4.+");
-    assertNotNull(coordinate);
-    assertEquals("4.5.0", resolveDynamicCoordinateVersion(coordinate));
   }
 
   public void testResolveDynamicSdkDependencies() {
@@ -296,7 +229,7 @@ public class RepositoryUrlManagerTest extends AndroidGradleTestCase {
     List<GradleCoordinate> result = myRepositoryUrlManager.resolveDynamicSdkDependencies(dependencies, "23.", mySdk, myFileOp);
     result.sort(Comparator.comparing(GradleCoordinate::toString));
     assertEquals("com.android.support:appcompat-v7:19.1.0", result.get(0).toString());
-    assertEquals("com.google.android.gms:play-services:4.2.42", result.get(1).toString());
+    assertEquals("com.google.android.gms:play-services:11.1.0", result.get(1).toString());
     assertEquals("com.google.android.support:wearable:1.0.0", result.get(2).toString());
     assertEquals(3, result.size());
   }
@@ -333,7 +266,7 @@ public class RepositoryUrlManagerTest extends AndroidGradleTestCase {
     result.sort(Comparator.comparing(GradleCoordinate::toString));
     assertEquals("com.android.support.constraint:constraint-layout:1.0.2", result.get(0).toString());
     assertEquals("com.android.support:appcompat-v7:20.0.0", result.get(1).toString());
-    assertEquals("com.android.support:support-v4:20.0.0", result.get(2).toString());
+    assertEquals("com.android.support:support-v4:26.0.2", result.get(2).toString());
     assertEquals("com.google.android.support:wearable:1.0.0", result.get(3).toString());
     assertEquals(4, result.size());
   }
