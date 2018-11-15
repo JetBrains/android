61c5c685f0e5c5865ff616556ae22efe2e822747
parentej <parentej@google.com>
2018-11-15 14:38:45 +0000

    Remove Android SDK m2repository - Fix Eclipse import
    When installing the Android SDK, we stop installing by
    default "Android Support Repository" and "Google Repository".
    All dependencies are hosted in maven.google.com
    This CL replaces the use of SdkMavenRepository, when importing
    an Eclipse project, with the more generic RepositoryUrlManager
    that will check the google maven repo (or use a local cache)
    
    Bug: 117207347
    Test: Run GradleImportTest and checked new code is still hit
    Change-Id: I1054cd178047f7e2b2848bfee30dc6623a11f19d

--- android/src/com/android/tools/idea/gradle/eclipse/ImportModule.java
+++ android/src/com/android/tools/idea/gradle/eclipse/ImportModule.java
@@ -19,10 +19,10 @@ package com.android.tools.idea.gradle.eclipse;
 import com.android.annotations.NonNull;
 import com.android.annotations.Nullable;
 import com.android.ide.common.repository.GradleCoordinate;
-import com.android.ide.common.repository.SdkMavenRepository;
 import com.android.repository.io.FileOpUtils;
 import com.android.resources.ResourceFolderType;
 import com.android.sdklib.AndroidVersion;
+import com.android.tools.idea.templates.RepositoryUrlManager;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 import com.google.common.io.Files;
@@ -186,12 +186,12 @@ public abstract class ImportModule implements Comparable<ImportModule> {
 
     if (myImporter.getSdkLocation() != null) {
       int requiredVersion = compileVersion;
-      GradleCoordinate max =
-          SdkMavenRepository.ANDROID.getHighestInstalledVersion(myImporter.getSdkLocation(), SUPPORT_GROUP_ID, artifact,
-                                                                (v) -> v.getMajor() == requiredVersion, true,
-                                                                FileOpUtils.create());
+      String max =
+        RepositoryUrlManager.get().getLibraryRevision(SUPPORT_GROUP_ID, artifact,
+                                                      (v) -> v.getMajor() == requiredVersion, true,
+                                                      myImporter.getSdkLocation(), FileOpUtils.create());
       if (max != null) {
-        return max;
+        return GradleCoordinate.parseCoordinateString(SUPPORT_GROUP_ID + ':' + artifact + ':' + max);
       }
     }
 
