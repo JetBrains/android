cf7a0bb3d3314e664e7bef8bf4455b94e8c820dd
Bradley Smith <smithbradley@google.com>
2018-11-19 16:37:58 +0000

    Add test project for new Kotlin sync
    Also clean up TestProjectPaths by regenerating constants and fixing
    resulting errors.
    
    Test: N/A
    Bug: N/A
    Change-Id: I258066991b7ae162cddd648fe2c48cf1b346af62

---
+++ android/testData/projects/newSyncKotlinTest/app/src/main/java/com/example/smithbradley/newkotlinsyntestproject/FullscreenActivity.kt
@@ -0,0 +1,141 @@
+package com.example.smithbradley.newkotlinsyntestproject
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import android.widget.TextView
+import com.example.smithbradley.contentlib.AwesomeContentProvider
+import kotlinx.android.synthetic.main.activity_fullscreen.*
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+class FullscreenActivity : AppCompatActivity() {
+  private val mHideHandler = Handler()
+  private val mHidePart2Runnable = Runnable {
+    // Delayed removal of status and navigation bar
+
+    // Note that some of these constants are new as of API 16 (Jelly Bean)
+    // and API 19 (KitKat). It is safe to use them, as they are inlined
+    // at compile-time and do nothing on earlier devices.
+    fullscreen_content.systemUiVisibility =
+        View.SYSTEM_UI_FLAG_LOW_PROFILE or
+        View.SYSTEM_UI_FLAG_FULLSCREEN or
+        View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
+        View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+        View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+  }
+  private val mShowPart2Runnable = Runnable {
+    // Delayed display of UI elements
+    supportActionBar?.show()
+    fullscreen_content_controls.visibility = View.VISIBLE
+  }
+  private var mVisible: Boolean = false
+  private val mHideRunnable = Runnable { hide() }
+  /**
+   * Touch listener to use for in-layout UI controls to delay hiding the
+   * system UI. This is to prevent the jarring behavior of controls going away
+   * while interacting with activity UI.
+   */
+  private val mDelayHideTouchListener = View.OnTouchListener { _, _ ->
+    if (AUTO_HIDE) {
+      delayedHide(AUTO_HIDE_DELAY_MILLIS)
+    }
+    false
+  }
+
+  private val mContentProvider = AwesomeContentProvider()
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+
+    setContentView(R.layout.activity_fullscreen)
+    supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+    mVisible = true
+
+    // Set up the user interaction to manually show or hide the system UI.
+    fullscreen_content.setOnClickListener { toggle() }
+
+    // Upon interacting with UI controls, delay any scheduled hide()
+    // operations to prevent the jarring behavior of controls going away
+    // while interacting with the UI.
+    dummy_button.setOnTouchListener(mDelayHideTouchListener)
+
+    // Update the view with the data from the content provider.
+    val v = findViewById<TextView>(R.id.fullscreen_content);
+    v.text = mContentProvider.produceContent()
+  }
+
+  override fun onPostCreate(savedInstanceState: Bundle?) {
+    super.onPostCreate(savedInstanceState)
+
+    // Trigger the initial hide() shortly after the activity has been
+    // created, to briefly hint to the user that UI controls
+    // are available.
+    delayedHide(100)
+  }
+
+  private fun toggle() {
+    if (mVisible) {
+      hide()
+    } else {
+      show()
+    }
+  }
+
+  private fun hide() {
+    // Hide UI first
+    supportActionBar?.hide()
+    fullscreen_content_controls.visibility = View.GONE
+    mVisible = false
+
+    // Schedule a runnable to remove the status and navigation bar after a delay
+    mHideHandler.removeCallbacks(mShowPart2Runnable)
+    mHideHandler.postDelayed(mHidePart2Runnable, UI_ANIMATION_DELAY.toLong())
+  }
+
+  private fun show() {
+    // Show the system bar
+    fullscreen_content.systemUiVisibility =
+        View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+        View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+    mVisible = true
+
+    // Schedule a runnable to display UI elements after a delay
+    mHideHandler.removeCallbacks(mHidePart2Runnable)
+    mHideHandler.postDelayed(mShowPart2Runnable, UI_ANIMATION_DELAY.toLong())
+  }
+
+  /**
+   * Schedules a call to hide() in [delayMillis], canceling any
+   * previously scheduled calls.
+   */
+  private fun delayedHide(delayMillis: Int) {
+    mHideHandler.removeCallbacks(mHideRunnable)
+    mHideHandler.postDelayed(mHideRunnable, delayMillis.toLong())
+  }
+
+  companion object {
+    /**
+     * Whether or not the system UI should be auto-hidden after
+     * [AUTO_HIDE_DELAY_MILLIS] milliseconds.
+     */
+    private val AUTO_HIDE = true
+
+    /**
+     * If [AUTO_HIDE] is set, the number of milliseconds to wait after
+     * user interaction before hiding the system UI.
+     */
+    private val AUTO_HIDE_DELAY_MILLIS = 3000
+
+    /**
+     * Some older devices needs a small delay between UI widget updates
+     * and a change of the status and navigation bar.
+     */
+    private val UI_ANIMATION_DELAY = 300
+  }
+}
