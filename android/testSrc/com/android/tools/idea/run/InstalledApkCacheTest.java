ecf6c07f41bf3d3c0551ef079312ed71880385c2
Yann Cébron <yann.cebron@jetbrains.com>
2018-11-21 11:50:42 +0100

    InstalledApkCacheTest: super.tearDown() in finally

--- android/testSrc/com/android/tools/idea/run/InstalledApkCacheTest.java
+++ android/testSrc/com/android/tools/idea/run/InstalledApkCacheTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014 The Android Open Source Project
+ * Copyright (C) 2018 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -74,8 +74,12 @@ public class InstalledApkCacheTest extends TestCase {
 
   @Override
   protected void tearDown() throws Exception {
-    Disposer.dispose(myService);
-    super.tearDown();
+    try {
+      Disposer.dispose(myService);
+    }
+    finally {
+      super.tearDown();
+    }
   }
 
   public void testCacheHit() throws Exception {
