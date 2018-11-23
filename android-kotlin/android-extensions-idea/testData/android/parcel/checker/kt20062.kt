12704ee11edc74937453a227b0e436d0590c18a2
parentej <parentej@google.com>
2018-11-23 12:17:41 +0000

    Update platform to Android-28 - Fix kt20062.kt
    API 28 has annotated Parcel@readString() to be @RecentlyNullable
    
    Test: Updated failing tests
    Bug: 112467616
    Change-Id: I70bb8d5616a79f3cbddd8d2767bd217660eaf75d

--- android-kotlin/android-extensions-idea/testData/android/parcel/checker/kt20062.kt
+++ android-kotlin/android-extensions-idea/testData/android/parcel/checker/kt20062.kt
@@ -8,7 +8,7 @@ class Box(val value: String)
 @Parcelize
 class Foo(val box: Box): Parcelable {
     companion object : Parceler<Foo> {
-        override fun create(parcel: Parcel) = Foo(Box(parcel.readString()))
+        override fun create(parcel: Parcel) = Foo(Box(parcel.readString()!!))
 
         override fun Foo.write(parcel: Parcel, flags: Int) {
             parcel.writeString(box.value)
