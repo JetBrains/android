567b2f84e474217a083ee7013ad8250f6b2dafbc
parentej <parentej@google.com>
2018-11-23 13:55:09 +0000

    Update platform to Android-28 - JUnit4 Style
    android.test.ApplicationTestCase was deprecated in API-26
    and removed from API-28.
    
    Test: Updated failing tests
    Bug: 112467616
    Change-Id: Ia45773ca3c48c14def0b8d2e6841339efd4d253d

--- android/testData/projects/dynamicApp/app/src/androidTest/java/google/simpleapplication/ApplicationTest.java
+++ android/testData/projects/dynamicApp/app/src/androidTest/java/google/simpleapplication/ApplicationTest.java
@@ -1,13 +1,14 @@
 package google.simpleapplication;
 
-import android.app.Application;
-import android.test.ApplicationTestCase;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
-public class ApplicationTest extends ApplicationTestCase<Application> {
-    public ApplicationTest() {
-        super(Application.class);
+@RunWith(AndroidJUnit4.class)
+public class ApplicationTest {
+    @Test
+    public exampleTest() {
     }
 }
