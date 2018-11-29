d3e2e9701de6a52b85b06ee2ea8316f5d1ca8969
Billy Lam <shiufai@google.com>
2018-11-29 14:54:13 -0800

    Re-enable device + process event tracking with Sessions
    The tracking logic was mistakenly disabled with the Sessions feature flag.
    This change moves the logic out of StudioProfilersView to StudioProfilers to
    ensure it gets executed regardless of the flag.
    
    Bug: 120233188
    Test: tests for tracking infra to follow
    Change-Id: I5d0e469124564bcfaeac0cddf21ab424ae32bfb1

--- profilers/src/com/android/tools/profilers/StudioProfilers.java
+++ profilers/src/com/android/tools/profilers/StudioProfilers.java
@@ -213,6 +213,10 @@ public class StudioProfilers extends AspectModel<ProfilerAspect> implements Upda
                      .onChange(SessionAspect.SELECTED_SESSION, this::selectedSessionChanged)
                      .onChange(SessionAspect.PROFILING_SESSION, this::profilingSessionChanged);
 
+    addDependency(this)
+      .onChange(ProfilerAspect.PROCESSES, () -> myIdeServices.getFeatureTracker().trackChangeProcess(getProcess()))
+      .onChange(ProfilerAspect.DEVICES, () -> myIdeServices.getFeatureTracker().trackChangeDevice(getDevice()));
+
     myViewAxis = new ResizingAxisComponentModel.Builder(myTimeline.getViewRange(), TimeAxisFormatter.DEFAULT)
       .setGlobalRange(myTimeline.getDataRange()).build();
 
