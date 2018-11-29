a5e0ee38401b3277966fea65583a6660f2ed8cd3
Hannah Chu <hanchu@google.com>
2018-11-29 15:44:53 -0800

    Enable WNA load from web by default
    Bug: 80475960
    Test: N/A
    Change-Id: I2407e2b8a123c7af6adfa765ee7c0f83475f0a06

--- android-common/src/com/android/tools/idea/flags/StudioFlags.java
+++ android-common/src/com/android/tools/idea/flags/StudioFlags.java
@@ -265,7 +265,7 @@ public final class StudioFlags {
   public static final Flag<Boolean> WHATS_NEW_ASSISTANT_DOWNLOAD_CONTENT = Flag.create(
     ASSISTANT, "whats.new.download", "Downloads \"What's New\" assistant content from web",
     "If enabled, the \"What's New\" assistant will update its contents from the web whenever it is opened.",
-    false);
+    true);
 
   public static final Flag<Boolean> NELE_NEW_PROPERTY_PANEL = Flag.create(
     NELE, "new.property", "Enable the new Property Panel",
