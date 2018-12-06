e37eed89b86c155a0ce5dbda7a5bb5a1248cb9d7
Gen Li <genli@google.com>
2018-12-06 14:05:35 -0800

    Turn UserDeviceManager into a project service.
    UserDeviceManager manages non-SDK devices configurations. These devices
    are only ever defined in one location and can be shared across the
    entire android project. Currently the user's devices.xml is parsed
    everytime the list of user devices is needed which is unecessary as the
    devices.xml is very unlikely to change and should only be parsed once.
    
    This CL turns UserDeviceManager into a project service and caches the
    device list.
    
    Because UserDeviceManager now caches the list of devices, there is no
    need to cache it again in ConfigurationManager.  However, the total list
    of devices needed by ConfigurationManager includes both user devices and
    platform devices, and platform devices still need to be cached.
    
    ConfigurationManager now only caches platform devices and always fetches
    user devices directly from UserDeviceManager on the fly.
    
    Bug: 119687937
    Test: Device XML reading is covered by DeviceManagerTest. Usage of this
    class is covered by ConfigurationManagerTest.
    Change-Id: I8607c1881d666786d14544f6a9c868627950cf3a

--- android/src/org/jetbrains/android/uipreview/UserDeviceManager.java
+++ android/src/org/jetbrains/android/uipreview/UserDeviceManager.java
@@ -5,133 +5,110 @@ import com.android.prefs.AndroidLocation;
 import com.android.sdklib.devices.Device;
 import com.android.sdklib.devices.DeviceParser;
 import com.android.utils.ILogger;
-import com.intellij.openapi.Disposable;
-import com.intellij.openapi.util.io.FileUtil;
+import com.google.common.collect.ImmutableList;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.components.ServiceManager;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.startup.StartupManager;
 import com.intellij.openapi.vfs.LocalFileSystem;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.openapi.vfs.VirtualFileEvent;
-import com.intellij.openapi.vfs.VirtualFileListener;
-import com.intellij.util.containers.HashSet;
-import org.jetbrains.annotations.NotNull;
-import org.xml.sax.SAXException;
-
-import javax.xml.parsers.ParserConfigurationException;
+import com.intellij.psi.util.CachedValue;
+import com.intellij.psi.util.CachedValueProvider;
+import com.intellij.psi.util.CachedValuesManager;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Set;
+import javax.xml.parsers.ParserConfigurationException;
+import org.jetbrains.android.sdk.MessageBuildingSdkLog;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.xml.sax.SAXException;
 
 /**
- * @author Eugene.Kudelevsky
+ * Manages user created devices.  These devices come from the user's devices.xml, which is present at $HOME/.android/devices.xml.
+ * The list of devices are cached and updated automatically as the user's devices.xml changes.
  */
-// TODO: Replace with dvlib usage?
-public class UserDeviceManager implements Disposable {
-  private final VirtualFileListener myListener;
-  private boolean myUserDevicesParsed;
-  private File myUserDevicesFile;
+public class UserDeviceManager {
+  @NotNull private final ILogger logger = new MessageBuildingSdkLog();
+  @Nullable private CachedValue<ImmutableList<Device>> myDevices;
+  @Nullable private VirtualFile myDevicesFile;
 
-  private static Set<String> ourWatchedRoots = new HashSet<>();
-
-  public UserDeviceManager() {
-    myListener = new VirtualFileListener() {
-      @Override
-      public void contentsChanged(@NotNull VirtualFileEvent event) {
-        final VirtualFile file = event.getFile();
-        if (myUserDevicesFile != null && SdkConstants.FN_DEVICES_XML.equals(file.getName()) &&
-            FileUtil.pathsEqual(FileUtil.toSystemIndependentName(myUserDevicesFile.getPath()),
-                                file.getPath())) {
-          userDevicesChanged();
-        }
-      }
-    };
-    LocalFileSystem.getInstance().addVirtualFileListener(myListener);
+  public static UserDeviceManager getInstance(@NotNull Project project) {
+    return ServiceManager.getService(project, UserDeviceManager.class);
   }
 
-  protected void userDevicesChanged() {
+  private UserDeviceManager(@NotNull Project project) {
+    myDevicesFile = getUserDevicesFile(logger);
+    if (myDevicesFile != null) {
+      myDevices = CachedValuesManager.getManager(project).createCachedValue(
+        () -> new CachedValueProvider.Result<>(parseUserDevicesFile(myDevicesFile, logger), myDevicesFile));
+    }
   }
 
-  // we need this because we cannot use DeviceManager.getUserDevices():
-  // user devices are remembered in static field and cannot be reset
+  /**
+   * Returns a list of user (a.k.a non SDK) devices. An empty list is returned when there are no user devices defined or
+   * when the user devices file cannot be found.
+   */
   @NotNull
-  public List<Device> parseUserDevices(@NotNull ILogger logger) {
-    final File userDevicesFile = getUserDevicesFile(logger);
+  public ImmutableList<Device> getUserDevices() {
+    if (myDevices == null) {
+      return ImmutableList.of();
+    }
+    else {
+      return myDevices.getValue();
+    }
+  }
 
+  @NotNull
+  private static ImmutableList<Device> parseUserDevicesFile(@Nullable VirtualFile userDevicesFile, @NotNull ILogger logger) {
     if (userDevicesFile == null) {
-      return Collections.emptyList();
+      return ImmutableList.of();
     }
-    final VirtualFile vUserDeviceFile = LocalFileSystem.getInstance().findFileByIoFile(userDevicesFile);
 
-    if (vUserDeviceFile == null) {
-      return Collections.emptyList();
+    File ioFile = new File(userDevicesFile.getPath());
+    if (!ioFile.exists()) {
+      return ImmutableList.of();
     }
+
     final ArrayList<Device> userDevices = new ArrayList<>();
-
     try {
-      if (userDevicesFile.exists()) {
-        userDevices.addAll(DeviceParser.parse(userDevicesFile).values());
-      }
+      userDevices.addAll(DeviceParser.parse(ioFile).values());
     }
     catch (SAXException e) {
-      if (myUserDevicesParsed) {
-        logger.error(e, "Error parsing " + userDevicesFile.getAbsolutePath());
+      final String newName = ioFile.getAbsoluteFile() + ".old";
+      File renamedConfig = new File(newName);
+      int i = 0;
+
+      while (renamedConfig.exists()) {
+        renamedConfig = new File(newName + '.' + i);
+        i++;
       }
-      else {
-        final String newName = userDevicesFile.getAbsoluteFile() + ".old";
-        File renamedConfig = new File(newName);
-        int i = 0;
+      logger.error(e, "Error parsing " + ioFile.getAbsolutePath() +
+                      ", backing up to " + renamedConfig.getAbsolutePath());
 
-        while (renamedConfig.exists()) {
-          renamedConfig = new File(newName + '.' + i);
-          i++;
-        }
-        logger.error(e, "Error parsing " + userDevicesFile.getAbsolutePath() +
-                           ", backing up to " + renamedConfig.getAbsolutePath());
-
-        if (!userDevicesFile.renameTo(renamedConfig)) {
-          logger.error(e, "Cannot rename file " + userDevicesFile.getAbsolutePath() + " to " + renamedConfig.getAbsolutePath());
-        }
+      if (!ioFile.renameTo(renamedConfig)) {
+        logger.error(e, "Cannot rename file " + ioFile.getAbsolutePath() + " to " + renamedConfig.getAbsolutePath());
       }
     }
     catch (ParserConfigurationException e) {
-      logger.error(null, "Error parsing " + userDevicesFile.getAbsolutePath());
+      logger.error(null, "Error parsing " + ioFile.getAbsolutePath());
     }
     catch (IOException e) {
-      logger.error(null, "Error parsing " + userDevicesFile.getAbsolutePath());
+      logger.error(null, "Error parsing " + ioFile.getAbsolutePath());
     }
-    finally {
-      myUserDevicesParsed = true;
-    }
-    return userDevices;
+
+    return ImmutableList.copyOf(userDevices);
   }
 
-  private File getUserDevicesFile(ILogger logger) {
-    if (myUserDevicesFile == null) {
-      try {
-        String myFolderToStoreDevicesXml = AndroidLocation.getFolder();
-        myUserDevicesFile = new File(myFolderToStoreDevicesXml, SdkConstants.FN_DEVICES_XML);
-      }
-      catch (AndroidLocation.AndroidLocationException e) {
-        logger.warning("Couldn't load user devices: " + e.getMessage());
-        myUserDevicesFile = null;
-        return null;
-      }
-      addDevicesXmlWatchedRootIfNecessary(myUserDevicesFile);
+  @Nullable
+  private static VirtualFile getUserDevicesFile(ILogger logger) {
+    try {
+      String myFolderToStoreDevicesXml = AndroidLocation.getFolder();
+      return LocalFileSystem.getInstance().findFileByIoFile(new File(myFolderToStoreDevicesXml, SdkConstants.FN_DEVICES_XML));
     }
-    return myUserDevicesFile;
-  }
-
-  private static void addDevicesXmlWatchedRootIfNecessary(@NotNull File root) {
-    final String path = FileUtil.toSystemIndependentName(root.getPath());
-
-    if (ourWatchedRoots.add(path)) {
-      LocalFileSystem.getInstance().addRootToWatch(path, true);
+    catch (AndroidLocation.AndroidLocationException e) {
+      logger.warning("Couldn't load user devices: " + e.getMessage());
+      return null;
     }
-  }
-
-  @Override
-  public void dispose() {
-    LocalFileSystem.getInstance().removeVirtualFileListener(myListener);
   }
 }
