a1f355b5e55411082d62a83b9a223da38c161f60
Matthew Gharrity <gharrma@google.com>
2018-12-10 15:09:09 -0800

    Optimize search for binding adapter attributes
    When data binding is enabled, typing latency can be very poor because
    we search the entire project for attributes defined by @BindingAdapter
    annotations. This commit optimizes the search by:
    
    (1) Refining the search scope from project to module. (This also
        improves correctness.)
    
    (2) Using annotation stub indexes directly rather than going through
        an AnnotatedElementsSearch, thereby avoiding the need to re-find
        an annotation from the annotated element.
    
    (3) Constructing a hash set of attributes so that we're not doing
        multiple linear scans to check if attributes are defined.
    
    (4) Caching the set of attributes found. Previously we cached
        the list of annotated methods, but then had to repeat constant
        expression evaluation multiple times to reconstruct the
        corresponding attribute names.
    
    Also correctly handles non-literal constants passed to @BindingAdapter annotations,
    since you can technically write something like `@BindingAdapter(myConstField)`.
    
    Also migrates DataBindingProjectComponent to UAST, which is now the recommended
    way to handle Java and Kotlin at the same time.
    
    Local testing shows a 2-3x reduction in typing latency when editing
    a data binding expression within an XML file in the iosched app.
    Using IntelliJ's internal "Typing Latency Report" action we get:
    
    Before: avg 321 ms, max 1138 ms
    After:  avg 142 ms, max  464 ms
    
    Test: existing
    Bug: 120804574
    Change-Id: Ifd616ada4ec6f51e656420fb43ee6bc92b3709e7

--- android/src/com/android/tools/idea/databinding/DataBindingProjectComponent.kt
+++ android/src/com/android/tools/idea/databinding/DataBindingProjectComponent.kt
@@ -16,28 +16,27 @@
 package com.android.tools.idea.databinding
 
 import com.google.common.collect.Maps
+import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.module.Module
 import com.intellij.openapi.module.ModuleManager
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.ModificationTracker
-import com.intellij.psi.JavaPsiFacade
-import com.intellij.psi.PsiAnnotationMemberValue
-import com.intellij.psi.PsiArrayInitializerMemberValue
-import com.intellij.psi.PsiClass
-import com.intellij.psi.PsiLiteral
+import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiManager
-import com.intellij.psi.PsiMethod
-import com.intellij.psi.PsiModifierListOwner
 import com.intellij.psi.PsiPackage
 import com.intellij.psi.impl.file.PsiPackageImpl
-import com.intellij.psi.search.ProjectScope
-import com.intellij.psi.search.searches.AnnotatedElementsSearch
+import com.intellij.psi.impl.java.stubs.index.JavaAnnotationIndex
+import com.intellij.psi.search.GlobalSearchScope
 import com.intellij.psi.util.CachedValue
 import com.intellij.psi.util.CachedValueProvider
 import com.intellij.psi.util.CachedValuesManager
-import com.intellij.psi.util.ParameterizedCachedValue
 import com.intellij.psi.util.PsiModificationTracker
 import org.jetbrains.android.facet.AndroidFacet
+import org.jetbrains.kotlin.idea.stubindex.KotlinAnnotationsIndex
+import org.jetbrains.uast.UAnnotation
+import org.jetbrains.uast.UCallExpression
+import org.jetbrains.uast.evaluateString
+import org.jetbrains.uast.toUElementOfType
 import java.util.ArrayList
 import java.util.concurrent.atomic.AtomicLong
 
@@ -46,12 +45,8 @@ import java.util.concurrent.atomic.AtomicLong
  */
 class DataBindingProjectComponent(val project: Project) : ModificationTracker {
   private val dataBindingEnabledModules: CachedValue<Array<AndroidFacet>>
-  private val bindingAdapterAnnotations: ParameterizedCachedValue<Collection<PsiModifierListOwner>, Module>
   private val modificationCount = AtomicLong(0)
   private val dataBindingPsiPackages = Maps.newConcurrentMap<String, PsiPackage>()
-
-  val dataBindingEnabledFacets: Array<AndroidFacet>
-    get() = dataBindingEnabledModules.value
 
   init {
     dataBindingEnabledModules = CachedValuesManager.getManager(project).createCachedValue({
@@ -70,49 +65,13 @@ class DataBindingProjectComponent(val project: Project) : ModificationTracker {
         DataBindingUtil.getDataBindingEnabledTracker(),
         ModuleManager.getInstance(project))
     }, false)
-
-    bindingAdapterAnnotations = CachedValuesManager.getManager(project).createParameterizedCachedValue({ module ->
-       val androidFacet = AndroidFacet.getInstance(module)
-       val adapterClass: PsiClass?
-       if (androidFacet != null) {
-         val facade = JavaPsiFacade.getInstance(project)
-         val mode = DataBindingUtil.getDataBindingMode(androidFacet)
-         if (mode == DataBindingMode.NONE) {
-           adapterClass = null
-         }
-         else {
-           adapterClass = facade.findClass(mode.bindingAdapter, module.getModuleWithDependenciesAndLibrariesScope(false))
-         }
-       }
-       else {
-         adapterClass = null
-       }
-       val psiElements: Collection<PsiModifierListOwner>
-       if (adapterClass == null) {
-         psiElements = emptyList()
-       }
-       else {
-         // ProjectScope used. ModuleWithDependencies does not seem to work
-         psiElements = AnnotatedElementsSearch.searchElements(
-           adapterClass,
-           ProjectScope.getAllScope(project),
-           PsiMethod::class.java).findAll()
-       }
-
-       // Cached value that will be refreshed in every Java change
-       CachedValueProvider.Result.create(psiElements,
-                 PsiModificationTracker.JAVA_STRUCTURE_MODIFICATION_COUNT,
-                 ModuleManager.getInstance(project))
-     }, false)
   }
 
-  fun hasAnyDataBindingEnabledFacet(): Boolean {
-    return dataBindingEnabledFacets.isNotEmpty()
-  }
+  fun hasAnyDataBindingEnabledFacet(): Boolean = getDataBindingEnabledFacets().isNotEmpty()
 
-  override fun getModificationCount(): Long {
-    return modificationCount.toLong()
-  }
+  fun getDataBindingEnabledFacets(): Array<AndroidFacet> = dataBindingEnabledModules.value
+
+  override fun getModificationCount(): Long = modificationCount.toLong()
 
   /**
    * Returns a [PsiPackage] instance for the given package name.
@@ -131,33 +90,59 @@ class DataBindingProjectComponent(val project: Project) : ModificationTracker {
     }
   }
 
-  /**
-   * Convert the passed annotation initialization into a [Iterable] of [PsiLiteral] values
-   */
-  private fun getPsiLiterals(annotationMemberValue: PsiAnnotationMemberValue): Iterable<PsiLiteral> {
-    if (annotationMemberValue is PsiArrayInitializerMemberValue) {
-      return annotationMemberValue.initializers
-        .filter { PsiLiteral::class.java.isInstance(it) }
-        .map { PsiLiteral::class.java.cast(it) }
-    }
-
-    return if (annotationMemberValue is PsiLiteral) {
-      listOf(annotationMemberValue)
-    }
-    else listOf()
+  private fun findJavaAndKotlinAnnotations(fqName: String, scope: GlobalSearchScope, project: Project): Sequence<UAnnotation> {
+    // We avoid using AnnotatedElementsSearch here, because that returns annotated elements rather than the
+    // annotations themselves, and for Kotlin it also wraps everything in KtLightElements.
+    val shortName = fqName.substringAfterLast('.')
+    val javaAnnotations: Sequence<PsiElement> = JavaAnnotationIndex.getInstance().get(shortName, project, scope).asSequence()
+    val kotlinAnnotations: Sequence<PsiElement> = KotlinAnnotationsIndex.getInstance().get(shortName, project, scope).asSequence()
+    return (javaAnnotations + kotlinAnnotations)
+      .mapNotNull { it.toUElementOfType<UAnnotation>() }
+      .filter { it.qualifiedName == fqName }
   }
 
   /**
-   * Returns the list of attributes defined by `@BindingAdapter` annotations
+   * Find all @BindingAdapter annotations in the given module, and compute
+   * the associated set of binding adapter attribute names.
    */
-  fun getBindingAdapterAttributes(module: Module): List<String> {
-    return bindingAdapterAnnotations.getValue(module)
-      .mapNotNull { it.modifierList }
-      .flatMap { it.annotations.asIterable() }
-      .mapNotNull { it.findAttributeValue("value") }
-      .flatMap { getPsiLiterals(it) }
-      .mapNotNull { it.value }
-      .distinct()
-      .map { it.toString() }
+  private fun computeBindingAdapterAttributes(module: Module): Set<String> {
+    val androidFacet = AndroidFacet.getInstance(module) ?: return emptySet()
+    val mode = DataBindingUtil.getDataBindingMode(androidFacet)
+    if (mode == DataBindingMode.NONE) {
+      return emptySet()
+    }
+
+    val scope = androidFacet.module.getModuleWithDependenciesAndLibrariesScope(false)
+    val annotations = findJavaAndKotlinAnnotations(mode.bindingAdapter, scope, project)
+
+    val allAttributes = mutableSetOf<String>()
+    for (annotation in annotations) {
+      val value = annotation.findDeclaredAttributeValue("value") ?: continue
+      val expressions = when (value) {
+        is UCallExpression -> value.valueArguments.asSequence() // Unwraps array initializers.
+        else -> sequenceOf(value)
+      }
+      expressions
+        .mapNotNull { it.evaluateString() }
+        .forEach { allAttributes.add(it) }
+    }
+
+    return allAttributes
+  }
+
+  /**
+   * Returns the (possibly cached) set of attributes defined by `@BindingAdapter` annotations.
+   * Must be called from a read action.
+   */
+  fun getBindingAdapterAttributes(module: Module): Set<String> {
+    assert(ApplicationManager.getApplication().isReadAccessAllowed)
+    // Cache the set of binding adapter attributes for fast lookup during XML markup and autocompletion.
+    // This cache is refreshed on every Java change.
+    return CachedValuesManager.getManager(module.project).getCachedValue(module) {
+      CachedValueProvider.Result.create(
+        computeBindingAdapterAttributes(module),
+        PsiModificationTracker.MODIFICATION_COUNT
+      )
+    }
   }
 }
