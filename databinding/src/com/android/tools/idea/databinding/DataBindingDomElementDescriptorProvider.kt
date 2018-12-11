e2713fac64e59c2d99803817f6727d024b966e15
Peter Xu <peterx@google.com>
2018-12-11 10:11:12 -0800

    Fix data binding xml element autocompletion.
    Switched from using Android's default xml descriptor provider and descriptor implementation in favor of creating data binding module's own provider and descriptor. They are DataBindingDomElementDescriptorProvider and DataBindingXmlTagDescriptor. Descriptors describe what kind of xml tag it is, which dictates the type of xml completion is performed.
    
    Test: DataBindingXmlAutocompletionTest.
    
    Fixes: 37150511
    Bug: 120707822
    Change-Id: I0d0e72f6963421289b9981379f562a47552548d2

---
+++ databinding/src/com/android/tools/idea/databinding/DataBindingDomElementDescriptorProvider.kt
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.databinding
+
+import com.intellij.psi.impl.source.xml.XmlElementDescriptorProvider
+import com.intellij.psi.xml.XmlTag
+import com.intellij.util.xml.impl.DomManagerImpl
+import com.intellij.xml.XmlElementDescriptor
+import com.intellij.xml.impl.dom.DomElementXmlDescriptor
+import org.jetbrains.android.dom.layout.DataBindingElement
+
+/**
+ * [DataBindingDomElementDescriptorProvider] provides [DataBindingXmlTagDescriptor] for tags that are [DataBindingElement].
+ * Returns null, if tag is not [DataBindingElement].
+ */
+class DataBindingDomElementDescriptorProvider: XmlElementDescriptorProvider {
+  override fun getDescriptor(tag: XmlTag): XmlElementDescriptor? {
+    val domElement = DomManagerImpl.getDomManager(tag.project).getDomElement(tag)
+    if (domElement !is DataBindingElement) {
+      return null
+    }
+    return DataBindingXmlTagDescriptor(domElement)
+  }
+}
