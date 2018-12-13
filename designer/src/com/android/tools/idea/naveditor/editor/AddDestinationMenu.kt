d4d71ebfc876921a482e97fc253897f2967d67ee
Joe Baker-Malone <jbakermalone@google.com>
2018-12-13 14:00:41 -0800

    Making add destination menu keyboard navigable
    Test: coming later
    Change-Id: I16df0ab97f749860a6d46a4578cb24b855441924
    Fixes: 120297547
    (cherry picked from commit 2963d06e2ee117b5e6ca91e7ab1fdaab57b87120)

--- designer/src/com/android/tools/idea/naveditor/editor/AddDestinationMenu.kt
+++ designer/src/com/android/tools/idea/naveditor/editor/AddDestinationMenu.kt
@@ -72,6 +72,7 @@ import java.awt.BorderLayout
 import java.awt.event.HierarchyEvent
 import java.awt.event.KeyAdapter
 import java.awt.event.KeyEvent
+import java.awt.event.KeyEvent.VK_ENTER
 import java.awt.event.MouseAdapter
 import java.awt.event.MouseEvent
 import java.io.File
@@ -80,6 +81,7 @@ import javax.swing.BorderFactory
 import javax.swing.ImageIcon
 import javax.swing.JComponent
 import javax.swing.JPanel
+import javax.swing.LayoutFocusTraversalPolicy
 import javax.swing.ListModel
 import javax.swing.SwingConstants
 import javax.swing.border.CompoundBorder
@@ -293,8 +295,13 @@ open class AddDestinationMenu(surface: NavDesignSurface) :
 
     destinationsList.addKeyListener(object : KeyAdapter() {
       override fun keyTyped(e: KeyEvent?) {
-        searchField.requestFocus()
-        application.invokeLater { searchField.dispatchEvent(e) }
+        if (e?.keyChar == '\n' || e?.keyCode == VK_ENTER) {
+          destinationsList.selectedValue?.let { addDestination(it) }
+        }
+        else {
+          searchField.requestFocus()
+          application.invokeLater { searchField.dispatchEvent(e) }
+        }
       }
     })
     scrollable.add(destinationsList, BorderLayout.CENTER)
@@ -327,6 +334,9 @@ open class AddDestinationMenu(surface: NavDesignSurface) :
 
         }
       }, EmptyProgressIndicator())
+
+    result.isFocusCycleRoot = true
+    result.focusTraversalPolicy = LayoutFocusTraversalPolicy()
 
     return result
   }
