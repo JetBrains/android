8a522de3ecef65da248e4f6782074d3507ce8d51
Chris Sinco <csinco@google.com>
2018-12-18 17:55:07 -0800

    Point to new test scope icons in StudioIcons
    Test: Visual inspection of local build
    Bug: n/a
    Change-Id: Ie7908ad09e57c88f300b4e99b2037a7332cfac6b

--- android/src/com/android/tools/idea/gradle/projectView/ModuleNodeIconDecorator.java
+++ android/src/com/android/tools/idea/gradle/projectView/ModuleNodeIconDecorator.java
@@ -28,7 +28,7 @@ import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.packageDependencies.ui.PackageDependenciesNode;
 import com.intellij.psi.PsiDirectory;
 import com.intellij.ui.ColoredTreeCellRenderer;
-import icons.AndroidIcons;
+import icons.StudioIcons;
 import org.jetbrains.android.facet.AndroidFacet;
 
 import static com.android.tools.idea.gradle.util.GradleUtil.getModuleIcon;
@@ -62,7 +62,7 @@ public class ModuleNodeIconDecorator implements ProjectViewNodeDecorator {
       else if (isSourceRoot(folder, project)) {
         TestArtifactSearchScopes testScopes = TestArtifactSearchScopes.get(module);
         if (testScopes != null && testScopes.isAndroidTestSource(folder)) {
-          data.setIcon(AndroidIcons.AndroidTestRoot);
+          data.setIcon(StudioIcons.Shell.Filetree.ANDROID_TEST_ROOT);
         }
       }
     }
