979c014b980d698226442483d684442e3ec6694b
Dana Dahlstrom <dahlstrom@google.com>
2018-12-27 11:11:11 -0800

    Use Android Studio version as major version
    … when comparing build numbers in update logic.
    See http://youtrack.jetbrains.com/issue/IDEA-201860.
    
    Change-Id: I668296fc0ee8f10985ffae44050240afbaa7d9dc
    Issue: 72834269

--- adt-branding/src/com/android/tools/idea/AndroidStudioUpdateStrategyCustomization.java
+++ adt-branding/src/com/android/tools/idea/AndroidStudioUpdateStrategyCustomization.java
@@ -2,6 +2,7 @@ package com.android.tools.idea;
 
 import com.intellij.openapi.updateSettings.UpdateStrategyCustomization;
 import com.intellij.openapi.updateSettings.impl.ChannelStatus;
+import com.intellij.openapi.util.BuildNumber;
 import org.jetbrains.annotations.NotNull;
 
 /**
@@ -16,5 +17,14 @@ public class AndroidStudioUpdateStrategyCustomization extends UpdateStrategyCust
   @Override
   public boolean isChannelActive(@NotNull ChannelStatus channel) {
     return true;
+  }
+
+  @Override
+  public boolean haveSameMajorVersion(@NotNull BuildNumber build1, @NotNull BuildNumber build2) {
+    return androidStudioVersion(build1) == androidStudioVersion(build2);
+  }
+
+  private static int androidStudioVersion(BuildNumber buildNumber) {
+    return buildNumber.getComponents()[3];
   }
 }
