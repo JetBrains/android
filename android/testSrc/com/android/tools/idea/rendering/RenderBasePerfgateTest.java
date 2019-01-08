b52b4ac8341ea668a31b3519ad6b3a931baf5317
Isaac Chai <ichai@google.com>
2019-01-07 18:05:10 -0800

    Adding complex layout to perfgate
    Our perfgate doesn't show much results as it's just a simple linear
    layout. This change adds more complex layout to monitor.
    
    Bug: 117868860
    Test: bazel test
    //tools/adt/idea/android:intellij.android.core.tests_tests
    --test_filter=RenderComplexPerfgateTest
    
    Change-Id: Ie871e9eeee432d9b42ef4353a54916b99d5db743

--- android/testSrc/com/android/tools/idea/rendering/RenderBasePerfgateTest.java
+++ android/testSrc/com/android/tools/idea/rendering/RenderBasePerfgateTest.java
@@ -15,28 +15,30 @@
  */
 package com.android.tools.idea.rendering;
 
+import static com.android.tools.idea.rendering.PerfgateRenderUtil.NUMBER_OF_SAMPLES;
+import static com.android.tools.idea.rendering.PerfgateRenderUtil.NUMBER_OF_WARM_UP;
+import static com.android.tools.idea.rendering.PerfgateRenderUtil.pruneOutliers;
+import static com.android.tools.idea.rendering.PerfgateRenderUtil.sRenderMemoryBenchMark;
+import static com.android.tools.idea.rendering.PerfgateRenderUtil.sRenderTimeBenchMark;
 import static org.junit.Assert.assertNotEquals;
 import static org.mockito.Mockito.mock;
 
 import com.android.ide.common.rendering.api.Result;
 import com.android.ide.common.rendering.api.ViewInfo;
 import com.android.tools.idea.configurations.Configuration;
-import com.android.tools.perflogger.Benchmark;
 import com.android.tools.perflogger.Metric;
 import com.android.tools.perflogger.Metric.MetricSample;
 import com.google.common.util.concurrent.Futures;
 import com.intellij.openapi.util.ThrowableComputable;
 import com.intellij.openapi.vfs.VirtualFile;
-import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
 import org.intellij.lang.annotations.Language;
 import org.jetbrains.android.AndroidTestCase;
 import org.jetbrains.annotations.NotNull;
 
-public class RenderPerfgateTest extends AndroidTestCase {
+public class RenderBasePerfgateTest extends AndroidTestCase {
 
   @Language("XML")
   private static final String SIMPLE_LAYOUT = "<LinearLayout xmlns:android=\"http://schemas.android.com/apk/res/android\"\n" +
@@ -59,18 +61,6 @@ public class RenderPerfgateTest extends AndroidTestCase {
                                               "    \n" +
                                               "\n" +
                                               "</LinearLayout>";
-  private static final int NUMBER_OF_WARM_UP = 2;
-  private static final int NUMBER_OF_SAMPLES = 20;
-  private static final int MAX_PRUNED_SAMPLES = NUMBER_OF_SAMPLES / 4;
-
-  // This is the name that appears on perfgate dashboard.
-  private static final Benchmark sRenderTimeBenchMark = new Benchmark.Builder("DesignTools Render Time Benchmark")
-    .setDescription("Base line for RenderTask inflate & render time (mean) after " + NUMBER_OF_SAMPLES + " samples.")
-    .build();
-
-  private static final Benchmark sRenderMemoryBenchMark = new Benchmark.Builder("DesignTools Memory Usage Benchmark")
-    .setDescription("Base line for RenderTask memory usage (mean) after " + NUMBER_OF_SAMPLES + " samples.")
-    .build();
 
   @Override
   protected void setUp() throws Exception {
@@ -92,9 +82,9 @@ public class RenderPerfgateTest extends AndroidTestCase {
     Configuration configuration = RenderTestUtil.getConfiguration(myModule, file);
     RenderLogger logger = mock(RenderLogger.class);
 
-    ThrowableComputable<RenderMetric, Exception> computable = () -> {
+    ThrowableComputable<PerfgateRenderMetric, Exception> computable = () -> {
       RenderTask task = RenderTestUtil.createRenderTask(myFacet, file, configuration, logger);
-      RenderMetric metric = getInflateMetric(task);
+      PerfgateRenderMetric metric = getInflateMetric(task);
       task.dispose().get(5, TimeUnit.SECONDS);
       return metric;
     };
@@ -107,9 +97,9 @@ public class RenderPerfgateTest extends AndroidTestCase {
     Configuration configuration = RenderTestUtil.getConfiguration(myModule, file);
     RenderLogger logger = mock(RenderLogger.class);
 
-    ThrowableComputable<RenderMetric, Exception> computable = () -> {
+    ThrowableComputable<PerfgateRenderMetric, Exception> computable = () -> {
       RenderTask task = RenderTestUtil.createRenderTask(myFacet, file, configuration, logger);
-      RenderMetric metric = getRenderMetric(task);
+      PerfgateRenderMetric metric = getRenderMetric(task);
       task.dispose().get(5, TimeUnit.SECONDS);
       return metric;
     };
@@ -118,7 +108,7 @@ public class RenderPerfgateTest extends AndroidTestCase {
   }
 
   private static void computeAndRecordMetric(
-    String renderMetricName, String memoryMetricName, ThrowableComputable<RenderMetric, Exception> computable) throws Exception {
+    String renderMetricName, String memoryMetricName, ThrowableComputable<PerfgateRenderMetric, Exception> computable) throws Exception {
 
     System.gc();
 
@@ -133,7 +123,7 @@ public class RenderPerfgateTest extends AndroidTestCase {
     List<MetricSample> memoryUsages = new ArrayList();
     for (int i = 0; i < NUMBER_OF_SAMPLES; i++) {
 
-      RenderMetric metric = computable.compute();
+      PerfgateRenderMetric metric = computable.compute();
 
       renderTimes.add(metric.getRenderTimeMetricSample());
       memoryUsages.add(metric.getMemoryMetricSample());
@@ -150,8 +140,8 @@ public class RenderPerfgateTest extends AndroidTestCase {
     memMetric.commit();
   }
 
-  private static RenderMetric getInflateMetric(RenderTask task) {
-    RenderMetric renderMetric = new RenderMetric();
+  private static PerfgateRenderMetric getInflateMetric(RenderTask task) {
+    PerfgateRenderMetric renderMetric = new PerfgateRenderMetric();
 
     renderMetric.beforeTest();
     RenderResult result = Futures.getUnchecked(task.inflate());
@@ -161,9 +151,9 @@ public class RenderPerfgateTest extends AndroidTestCase {
     return renderMetric;
   }
 
-  private static RenderMetric getRenderMetric(RenderTask task) {
+  private static PerfgateRenderMetric getRenderMetric(RenderTask task) {
     checkSimpleLayoutResult(Futures.getUnchecked(task.inflate()));
-    RenderMetric renderMetric = new RenderMetric();
+    PerfgateRenderMetric renderMetric = new PerfgateRenderMetric();
 
     renderMetric.beforeTest();
     RenderResult result = Futures.getUnchecked(task.render());
@@ -189,77 +179,6 @@ public class RenderPerfgateTest extends AndroidTestCase {
       String currentCoordinates = String.format("%dx%d - %dx%d", view.getTop(), view.getLeft(), view.getBottom(), view.getRight());
       assertNotEquals(previousCoordinates, currentCoordinates);
       previousCoordinates = currentCoordinates;
-    }
-  }
-
-  /**
-   * Try to prune outliers based on standard deviation. No more than {@link #MAX_PRUNED_SAMPLES} samples will be pruned.
-   */
-  private static List<MetricSample> pruneOutliers(List<MetricSample> list) {
-    float average = getAverage(list);
-    float std = getStandardDeviation(list, average);
-
-    float lowerBound = average - std;
-    float upperBound = average + std;
-
-    List<MetricSample> result = new ArrayList<>();
-    for (MetricSample sample : list) {
-      if (sample.getSampleData() >= lowerBound && sample.getSampleData() <= upperBound) {
-        result.add(sample);
-      }
-    }
-
-    // Too many samples pruned (i.e. data is widely spread). Return the raw list.
-    if (result.size() < (list.size() - MAX_PRUNED_SAMPLES)) {
-      return list;
-    }
-    return result;
-  }
-
-  private static float getStandardDeviation(List<MetricSample> list, float average) {
-    float sum = 0;
-    for (MetricSample sample : list) {
-      sum += Math.pow(sample.getSampleData() - average, 2);
-    }
-    return (float) Math.sqrt(sum / list.size());
-  }
-
-  private static float getAverage(List<MetricSample> list) {
-    float sum = 0;
-    for (MetricSample sample : list) {
-      sum += sample.getSampleData();
-    }
-    return sum / list.size();
-  }
-
-  private static class RenderMetric {
-
-    private long mTimestamp;
-
-    private long mPrevUsedMem;
-    private long mMemoryUsage;
-
-    private long mStartTime;
-    private long mElapsedTime;
-
-    public void beforeTest() {
-      mPrevUsedMem = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
-      mStartTime = System.currentTimeMillis();
-    }
-
-    public void afterTest() {
-      mElapsedTime = System.currentTimeMillis() - mStartTime;
-      mMemoryUsage = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory() - mPrevUsedMem;
-
-      mTimestamp = Instant.now().toEpochMilli();
-    }
-
-    public MetricSample getRenderTimeMetricSample() {
-      return new MetricSample(mTimestamp, mElapsedTime);
-    }
-
-    public MetricSample getMemoryMetricSample() {
-      return new MetricSample(mTimestamp, mMemoryUsage);
     }
   }
 }
