37a7ce0c409be21fd1af148062ac680374dfc64b
Sergey Prigogin <sprigogin@google.com>
2019-01-09 16:55:48 -0800

    Move AAR resource repository code to a separate module
    Also move ProtoXmlPullParser to tools/base.
    
    Test: existing
    Bug: N/A
    Change-Id: Ia523fe8134c76c1d51d6c34285d30b7a67e03d18

--- android/src/org/jetbrains/android/dom/manifest/AndroidManifestUtils.java
+++ android/src/org/jetbrains/android/dom/manifest/AndroidManifestUtils.java
@@ -15,17 +15,10 @@
  */
 package org.jetbrains.android.dom.manifest;
 
-import static com.android.SdkConstants.ANDROID_MANIFEST_XML;
-
 import com.android.SdkConstants;
 import com.android.builder.model.ProductFlavor;
-import com.android.ide.common.util.PathString;
-import com.android.ide.common.util.PathStrings;
 import com.android.tools.idea.AndroidPsiUtils;
 import com.android.tools.idea.gradle.project.model.AndroidModuleModel;
-import com.android.tools.idea.res.aar.ProtoXmlPullParser;
-import com.android.tools.idea.util.FileExtensions;
-import com.android.utils.XmlUtils;
 import com.intellij.openapi.util.ModificationTracker;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.openapi.vfs.VirtualFile;
@@ -37,79 +30,16 @@ import com.intellij.psi.xml.XmlFile;
 import com.intellij.psi.xml.XmlTag;
 import com.intellij.util.xml.DomElement;
 import com.intellij.util.xml.XmlName;
-import java.io.BufferedInputStream;
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipFile;
 import org.jetbrains.android.facet.AndroidFacet;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import org.kxml2.io.KXmlParser;
-import org.xmlpull.v1.XmlPullParser;
-import org.xmlpull.v1.XmlPullParserException;
 
 /**
  * Methods for working with Android manifests.
  */
 public class AndroidManifestUtils {
-  /**
-   * Reads package name from the AndroidManifest.xml file in the given directory. The the AndroidManifest.xml
-   * file can be in either text or proto format.
-   *
-   * @param pathOpener path opener that can produce input streams for
-   * @param manifestFile the AndroidManifest.xml file
-   * @return the package name from the manifest
-   */
-  @Nullable
-  public static String getPackageNameFromManifestFile(@NotNull PathString manifestFile) throws IOException {
-    try (InputStream stream = FileExtensions.buffered(PathStrings.inputStream(manifestFile))) {
-      return getPackageName(stream);
-    }
-    catch (XmlPullParserException e) {
-      throw new IOException("File " + manifestFile + " has invalid format");
-    }
-  }
-
-  /**
-   * Reads package name from the AndroidManifest.xml stored inside the given res.apk file.
-   *
-   * @param resApk the res.apk file
-   * @return the package name from the manifest
-   */
-  @Nullable
-  public static String getPackageNameFromResApk(@NotNull ZipFile resApk) throws IOException {
-    ZipEntry zipEntry = resApk.getEntry(ANDROID_MANIFEST_XML);
-    if (zipEntry == null) {
-      throw new IOException("\"" + ANDROID_MANIFEST_XML + "\" not found in " + resApk.getName());
-    }
-
-    try (InputStream stream = new BufferedInputStream(resApk.getInputStream(zipEntry))) {
-      return getPackageName(stream);
-    } catch (XmlPullParserException e) {
-      throw new IOException("Invalid " + ANDROID_MANIFEST_XML + " in " + resApk.getName());
-    }
-  }
-
-  private static String getPackageName(@NotNull InputStream stream) throws XmlPullParserException, IOException {
-    stream.mark(1);
-    // Instantiate an XML pull parser based on the contents of the stream.
-    XmlPullParser parser;
-    if (XmlUtils.isProtoXml(stream)) {
-      parser = new ProtoXmlPullParser(); // Parser for proto XML used in AARs.
-    } else {
-      parser = new KXmlParser(); // Parser for regular text XML.
-      parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, true);
-    }
-    parser.setInput(stream, null);
-    if (parser.nextTag() == XmlPullParser.START_TAG) {
-      return parser.getAttributeValue(null, "package");
-    }
-    return null;
-  }
-
   @Nullable
   public static String getPackageName(@NotNull AndroidFacet androidFacet) {
     return CachedValuesManager.getManager(androidFacet.getModule().getProject()).getCachedValue(androidFacet, () -> {
