f3c86e75c4f1bc4e23de79324975f51969bd5872
Yuriy Solodkyy <solodkyy@google.com>
2019-01-11 16:08:09 +0000

    [New PSD] Fix handling of invalid values in the config.
    Fixes: 122439166
    Test: unit tests updated
    Change-Id: Id29f514bc1c903625e402a03e5a703ae25735b06

--- android/src/com/android/tools/idea/gradle/structure/model/meta/PropertyModelUtils.kt
+++ android/src/com/android/tools/idea/gradle/structure/model/meta/PropertyModelUtils.kt
@@ -74,7 +74,7 @@ fun ResolvedPropertyModel.setLanguageLevel(value: LanguageLevel) =
   setValue(LanguageLevelUtil.convertToGradleString(value, getValue(GradlePropertyModel.STRING_TYPE)))
 
 fun ResolvedPropertyModel.clear() = unresolvedModel.delete()
-fun ResolvedPropertyModel.dslText(): Annotated<DslText>? {
+fun ResolvedPropertyModel.dslText(effectiveValueIsNull: Boolean): Annotated<DslText>? {
   val text = getRawValue(GradlePropertyModel.OBJECT_TYPE)?.toString()
   return when {
     text == null && unresolvedModel.valueType == GradlePropertyModel.ValueType.NONE -> null
@@ -83,8 +83,9 @@ fun ResolvedPropertyModel.dslText(): Annotated<DslText>? {
         "The raw value of property '${unresolvedModel.fullyQualifiedName}' is null while its type is: ${unresolvedModel.valueType}")
     unresolvedModel.valueType == ValueType.REFERENCE && dependencies.isEmpty() -> DslText.Reference(text).annotateWithError(
       "Unresolved reference: $text")
-    unresolvedModel.valueType == ValueType.UNKNOWN -> DslText.OtherUnparsedDslText(text).annotated()
     unresolvedModel.valueType == ValueType.REFERENCE -> DslText.Reference(text).annotated()
+    unresolvedModel.valueType == ValueType.UNKNOWN || (dependencies.isEmpty() && effectiveValueIsNull)
+    -> DslText.OtherUnparsedDslText(text).annotated()
     dependencies.isEmpty() ||
     unresolvedModel.valueType == ValueType.MAP ||
     unresolvedModel.valueType == ValueType.LIST -> DslText.Literal.annotated()
@@ -105,8 +106,10 @@ fun ResolvedPropertyModel.setDslText(value: DslText) =
 
 internal fun <T : Any> ResolvedPropertyModel?.getParsedValue(
   getter: ResolvedPropertyModel.() -> T?
-): Annotated<ParsedValue<T>> =
-  makeAnnotatedParsedValue(this?.getter(), this?.dslText())
+): Annotated<ParsedValue<T>> {
+  val value = this?.getter()
+  return makeAnnotatedParsedValue(value, this?.dslText(effectiveValueIsNull = value == null))
+}
 
 internal fun <T : Any> ResolvedPropertyModel.setParsedValue(
   setter: ResolvedPropertyModel.(T) -> Unit,
