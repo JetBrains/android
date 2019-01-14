154ea8d586c444eaa38e64036e70a4e281938bbc
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2019-01-14 17:35:59 +0100

    reduce Application.saveAll() calls — project.saveDocumentsAndProjectSettings() is enough

--- android/src/org/jetbrains/android/facet/AndroidFrameworkDetector.java
+++ android/src/org/jetbrains/android/facet/AndroidFrameworkDetector.java
@@ -1,24 +1,11 @@
-/*
- * Copyright 2000-2011 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.android.facet;
 
 import com.android.SdkConstants;
 import com.android.tools.idea.gradle.project.GradleProjectInfo;
 import com.android.tools.idea.project.AndroidRunConfigurations;
 import com.android.tools.idea.run.activity.DefaultActivityLocator;
+import com.intellij.configurationStore.StoreUtil;
 import com.intellij.facet.FacetType;
 import com.intellij.framework.detection.DetectedFrameworkDescription;
 import com.intellij.framework.detection.FacetBasedFrameworkDetector;
@@ -28,7 +15,6 @@ import com.intellij.notification.Notification;
 import com.intellij.notification.NotificationGroup;
 import com.intellij.notification.NotificationListener;
 import com.intellij.notification.NotificationType;
-import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.fileTypes.FileType;
 import com.intellij.openapi.fileTypes.StdFileTypes;
 import com.intellij.openapi.module.Module;
@@ -98,7 +84,7 @@ public class AndroidFrameworkDetector extends FacetBasedFrameworkDetector<Androi
 
     StartupManager.getInstance(project).runWhenProjectIsInitialized(() -> DumbService.getInstance(project).runWhenSmart(() -> {
       doImportSdkAndFacetConfiguration(facet, model);
-      ApplicationManager.getApplication().saveAll();
+      StoreUtil.saveDocumentsAndProjectSettings(project);
     }));
   }
 
