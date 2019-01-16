ed62dd10a4a1002e38064d6b0e6db02d583b51e6
David Herman <davidherman@google.com>
2019-01-15 16:54:35 -0800

    Add "Build Output" window support for legacy data binding errors
    A recent CL added "Build Output" window support for a newer
    JSON-encoded error message format, but users of older Gradle
    plugins would not get to benefit from that, and updating
    a codebase's Gradle plugin is not always viable. Therefore,
    this CL adds support for the legacy databinding error format
    as well.
    
    Bug: 111411525
    Test: Added
    Change-Id: I64421bb1ff505bd6d03659fe3ad5558a83a06758

--- android/testSrc/com/android/tools/idea/gradle/project/build/output/DataBindingOutputParserTest.kt
+++ android/testSrc/com/android/tools/idea/gradle/project/build/output/DataBindingOutputParserTest.kt
@@ -23,11 +23,22 @@ import org.junit.Test
 class DataBindingOutputParserTest {
 
   @Test
-  fun handleMultipleDataBindingParseErrors() {
+  fun rejectUnrelatedInput() {
+    val input = "This is not data binding input"
+    val reader = TestBuildOutputInstantReader(input)
+    val consumer = TestMessageEventConsumer()
+
+    val parser = DataBindingOutputParser()
+
+    assertThat(parser.parse(input, reader, consumer)).isFalse()
+  }
+
+  @Test
+  fun handleMultipleJsonFormattedDataBindingParseErrors() {
     val input = "Found data binding error(s):\n" +
                 "\n" +
-                "[databinding] {\"msg\":\"Could not find identifier \\u0027var1\\u0027\\n\\nCheck that the identifier is spelled correctly, and that no \\u003cimport\\u003e or \\u003cvariable\\u003e tags are missing.\",\"file\":\"/usr/local/google/home/davidherman/Code/studio-master-dev/tools/data-binding/integration-tests/AppWithDataBindingInTests/app/src/main/res/layout/activity_main1.xml\",\"pos\":[{\"line0\":36,\"col0\":28,\"line1\":36,\"col1\":32}]}\n" +
-                "[databinding] {\"msg\":\"Could not find identifier \\u0027var2\\u0027\\n\\nCheck that the identifier is spelled correctly, and that no \\u003cimport\\u003e or \\u003cvariable\\u003e tags are missing.\",\"file\":\"/usr/local/google/home/davidherman/Code/studio-master-dev/tools/data-binding/integration-tests/AppWithDataBindingInTests/app/src/main/res/layout/activity_main2.xml\",\"pos\":[{\"line0\":58,\"col0\":23,\"line1\":58,\"col1\":27}]}\n"
+                "[databinding] {\"msg\":\"Could not find identifier \\u0027var1\\u0027\\n\\nCheck that the identifier is spelled correctly, and that no \\u003cimport\\u003e or \\u003cvariable\\u003e tags are missing.\",\"file\":\"/src/main/res/layout/activity_main1.xml\",\"pos\":[{\"line0\":36,\"col0\":28,\"line1\":36,\"col1\":32}]}\n" +
+                "[databinding] {\"msg\":\"Could not find identifier \\u0027var2\\u0027\\n\\nCheck that the identifier is spelled correctly, and that no \\u003cimport\\u003e or \\u003cvariable\\u003e tags are missing.\",\"file\":\"/src/main/res/layout/activity_main2.xml\",\"pos\":[{\"line0\":58,\"col0\":23,\"line1\":58,\"col1\":27}]}\n"
 
     val reader = TestBuildOutputInstantReader(input)
     val consumer = TestMessageEventConsumer()
@@ -67,17 +78,6 @@ class DataBindingOutputParserTest {
   }
 
   @Test
-  fun rejectUnrelatedInput() {
-    val input = "This is not data binding input"
-    val reader = TestBuildOutputInstantReader(input)
-    val consumer = TestMessageEventConsumer()
-
-    val parser = DataBindingOutputParser()
-
-    assertThat(parser.parse(input, reader, consumer)).isFalse()
-  }
-
-  @Test
   fun recoverFromInvalidJson() {
     val badJson = "[databinding] {\"msg"
     val reader = TestBuildOutputInstantReader(badJson)
@@ -88,4 +88,69 @@ class DataBindingOutputParserTest {
     assertThat(parser.parse(badJson, reader, consumer)).isFalse()
   }
 
+  @Test
+  fun handleMultipleLegacyFormattedDataBindingParseErrors() {
+    val input = "Found data binding errors.\n" +
+                "  ****/ data binding error ****msg:Identifiers must have user defined types from the XML file. var1 is missing it file:/src/main/res/layout/activity_main1.xml loc:36:28 - 36:32 ****\\ data binding error ****\n" +
+                "  ****/ data binding error ****msg:Identifiers must have user defined types from the XML file. var2 is missing it file://src/main/res/layout/activity_main2.xml loc:58:23 - 58:27 ****\\ data binding error ****\n"
+
+    val reader = TestBuildOutputInstantReader(input)
+    val consumer = TestMessageEventConsumer()
+
+    val parser = DataBindingOutputParser()
+
+    while (true) {
+      val line = reader.readLine() ?: break
+      assertThat(parser.parse(line, reader, consumer)).isTrue()
+    }
+
+    assertThat(consumer.messageEvents).hasSize(2)
+
+    run {
+      val firstError = consumer.messageEvents[0] as FileMessageEventImpl
+      assertThat(firstError.message).isEqualTo("Identifiers must have user defined types from the XML file. var1 is missing it")
+
+      val filePos = firstError.filePosition
+      assertThat(filePos.startLine).isEqualTo(36)
+      assertThat(filePos.startColumn).isEqualTo(28)
+      assertThat(filePos.endLine).isEqualTo(36)
+      assertThat(filePos.endColumn).isEqualTo(32)
+      assertThat(filePos.file.path).contains("activity_main1.xml")
+    }
+
+    run {
+      val secondError = consumer.messageEvents[1] as FileMessageEventImpl
+      assertThat(secondError.message).isEqualTo("Identifiers must have user defined types from the XML file. var2 is missing it")
+
+      val filePos = secondError.filePosition
+      assertThat(filePos.startLine).isEqualTo(58)
+      assertThat(filePos.startColumn).isEqualTo(23)
+      assertThat(filePos.endLine).isEqualTo(58)
+      assertThat(filePos.endColumn).isEqualTo(27)
+      assertThat(filePos.file.path).contains("activity_main2.xml")
+    }
+  }
+
+  @Test
+  fun recoverFromInvalidLegacy() {
+    run {
+      val badLegacyContents = "****/ data binding error ****invalid contents****\\ data binding error ****"
+      val reader = TestBuildOutputInstantReader(badLegacyContents)
+      val consumer = TestMessageEventConsumer()
+
+      val parser = DataBindingOutputParser()
+
+      assertThat(parser.parse(badLegacyContents, reader, consumer)).isFalse()
+    }
+
+    run {
+      val badLegacyLocation = "****/ data binding error ****msg:dummy file:/dummy loc:not-a-number ****\\ data binding error ****"
+      val reader = TestBuildOutputInstantReader(badLegacyLocation)
+      val consumer = TestMessageEventConsumer()
+
+      val parser = DataBindingOutputParser()
+
+      assertThat(parser.parse(badLegacyLocation, reader, consumer)).isFalse()
+    }
+  }
 }
