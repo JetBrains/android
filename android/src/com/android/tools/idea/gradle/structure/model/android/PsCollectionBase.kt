0422e2aa8031ba2de5d155369fc29e4e6e916ce9
Yuriy Solodkyy <solodkyy@google.com>
2019-01-21 14:55:07 +0000

    [New PSD] Handle duplicate variable names when editing.
    Fixes: 121272101
    Test: tests updated
    Change-Id: Ia6cce1d350f9d0b240f2969e4e77cd4a604eb6eb

--- android/src/com/android/tools/idea/gradle/structure/model/android/PsCollectionBase.kt
+++ android/src/com/android/tools/idea/gradle/structure/model/android/PsCollectionBase.kt
@@ -83,7 +83,7 @@ abstract class PsMutableCollectionBase<TModel : PsModel, TKey, TParent : PsModel
   protected abstract fun removeExisting(key: TKey)
 
   fun addNew(key: TKey): TModel {
-    if (entries.containsKey(key)) throw IllegalArgumentException("Duplicate key: $key")
+    if (entries.containsKey(key)) onDuplicateKey(key)
     instantiateNew(key)
     val model = create(key).also { update(key, it) }
     entries += (key to model)
@@ -91,6 +91,8 @@ abstract class PsMutableCollectionBase<TModel : PsModel, TKey, TParent : PsModel
     notifyChanged()
     return model
   }
+
+  protected open fun onDuplicateKey(key: TKey): Nothing = throw IllegalArgumentException("Duplicate key: $key")
 
   fun remove(key: TKey) {
     if (!entries.containsKey(key)) throw IllegalArgumentException("Key not found: $key")
