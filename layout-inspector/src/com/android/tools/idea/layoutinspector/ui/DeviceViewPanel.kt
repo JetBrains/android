5d81544480eee681ff0607e4e4c7e0874cff2342
Joe Baker-Malone <jbakermalone@google.com>
2019-01-24 18:44:13 -0800

    Making view fully rotatable
    Test: I should write some
    Bug: 122274789
    Change-Id: I20bd4895e1c0b6f886f6c6e94cb37bf6da61bb51

--- layout-inspector/src/com/android/tools/idea/layoutinspector/ui/DeviceViewPanel.kt
+++ layout-inspector/src/com/android/tools/idea/layoutinspector/ui/DeviceViewPanel.kt
@@ -36,22 +36,34 @@ import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.DataProvider
 import com.intellij.openapi.actionSystem.DefaultActionGroup
 import com.intellij.openapi.actionSystem.ex.CheckboxAction
+import icons.StudioIcons
 import java.awt.AlphaComposite
 import java.awt.BasicStroke
 import java.awt.BorderLayout
 import java.awt.Color
+import java.awt.Dimension
 import java.awt.Graphics
 import java.awt.Graphics2D
 import java.awt.Rectangle
 import java.awt.RenderingHints
+import java.awt.Shape
+import java.awt.event.ComponentAdapter
+import java.awt.event.ComponentEvent
 import java.awt.event.MouseAdapter
 import java.awt.event.MouseEvent
-import java.util.LinkedList
+import java.awt.geom.AffineTransform
 import javax.swing.BorderFactory
+import javax.swing.Icon
 import javax.swing.JComponent
 import javax.swing.JPanel
-import kotlin.math.cos
-import kotlin.math.sin
+import kotlin.math.PI
+import kotlin.math.abs
+import kotlin.math.atan
+import kotlin.math.hypot
+import kotlin.math.max
+import kotlin.math.min
+import kotlin.math.sign
+import kotlin.math.sqrt
 
 private const val LAYER_SPACING = 150
 
@@ -60,10 +72,19 @@ private const val LAYER_SPACING = 150
  */
 class DeviceViewPanel(private val layoutInspector: LayoutInspector) : JPanel(BorderLayout()), Zoomable, DataProvider {
 
-  override var scale: Double = 1.0
+  enum class ViewMode(val icon: Icon) {
+    FIXED(StudioIcons.LayoutEditor.Extras.ROOT_INLINE),
+    X_ONLY(StudioIcons.DeviceConfiguration.SCREEN_WIDTH),
+    XY(StudioIcons.DeviceConfiguration.SMALLEST_SCREEN_SIZE)
+  }
 
-  override val screenScalingFactor: Float
-    get() = 1f
+  var viewState = ViewMode.XY
+
+  var rootDimension: Dimension = Dimension()
+
+  override var scale: Double = .5
+
+  override val screenScalingFactor = 1f
 
   override fun zoom(type: ZoomType): Boolean {
     when (type) {
@@ -72,7 +93,7 @@ class DeviceViewPanel(private val layoutInspector: LayoutInspector) : JPanel(Bor
       ZoomType.IN -> scale += 0.1
       ZoomType.OUT -> scale -= 0.1
     }
-    repaint()
+    rebuildRects()
     return true
   }
 
@@ -82,7 +103,7 @@ class DeviceViewPanel(private val layoutInspector: LayoutInspector) : JPanel(Bor
 
   override fun canZoomToFit() = true
 
-  private var drawBorders = false
+  private var drawBorders = true
 
   private val showBordersCheckBox = object : CheckboxAction("Show borders") {
     override fun isSelected(e: AnActionEvent): Boolean {
@@ -103,14 +124,16 @@ class DeviceViewPanel(private val layoutInspector: LayoutInspector) : JPanel(Bor
     RenderingHints.KEY_STROKE_CONTROL to RenderingHints.VALUE_STROKE_PURE
   )
 
-  private var angle = 0.0
-  private val hitRects = ArrayList<Pair<Rectangle, InspectorView>>()
+  private var xOff = 0.0
+  private var yOff = 0.0
+  private val hitRects = ArrayList<Triple<Shape, AffineTransform, InspectorView>>()
 
   private val mouseListener = object : MouseAdapter() {
     override fun mouseClicked(e: MouseEvent) {
       layoutInspector.layoutInspectorModel.selection = hitRects.findLast {
-        it.first.contains(e.x / scale, e.y / scale)
-      }?.second
+        it.first.contains(e.x.toDouble(), e.y.toDouble())
+      }?.third
+      repaint()
     }
   }
 
@@ -124,14 +147,8 @@ class DeviceViewPanel(private val layoutInspector: LayoutInspector) : JPanel(Bor
       })
     }
 
-    layoutInspector.modelChangeListeners.add(this::modelChanged)
-    layoutInspector.layoutInspectorModel.selectionListeners.add(this::selectionChanged)
-
-    addMouseWheelListener { e ->
-      angle += e.preciseWheelRotation * 0.01
-      rebuildRects()
-      repaint()
-    }
+    layoutInspector.modelChangeListeners.add(::modelChanged)
+    layoutInspector.layoutInspectorModel.selectionListeners.add(::selectionChanged)
 
     add(createToolbar(), BorderLayout.NORTH)
 
@@ -139,16 +156,85 @@ class DeviceViewPanel(private val layoutInspector: LayoutInspector) : JPanel(Bor
       override fun paint(g: Graphics) {
         super.paint(g)
         (g as? Graphics2D)?.setRenderingHints(HQ_RENDERING_HINTS)
-        (g as? Graphics2D)?.scale(scale, scale)
-        draw(layoutInspector.layoutInspectorModel.root, g, 0)
+        hitRects.forEach { (rect, transform, view) ->
+          drawView(g, view, rect, transform)
+        }
       }
     }
+    val listener = object : MouseAdapter() {
+      private var x = 0
+      private var y = 0
+
+      override fun mousePressed(e: MouseEvent) {
+        x = e.x
+        y = e.y
+      }
+
+      override fun mouseDragged(e: MouseEvent) {
+        if (viewState != ViewMode.FIXED) {
+          xOff += (e.x - x) * 0.001
+          xOff = max(-1.0, min(1.0, xOff))
+          x = e.x
+        }
+        if (viewState == ViewMode.XY) {
+          yOff += (e.y - y) * 0.001
+          yOff = max(-1.0, min(1.0, yOff))
+          y = e.y
+        }
+
+        rebuildRects()
+      }
+    }
+    panel.addMouseListener(listener)
+    panel.addMouseMotionListener(listener)
+
     val borderPanel = JPanel(BorderLayout())
     borderPanel.add(panel, BorderLayout.CENTER)
-    borderPanel.border = BorderFactory.createEmptyBorder(20, 20, 20, 20)
     add(borderPanel, BorderLayout.CENTER)
     panel.addMouseListener(mouseListener)
+    panel.addComponentListener(object: ComponentAdapter() {
+      override fun componentResized(e: ComponentEvent?) {
+        rebuildRects()
+      }
+    })
     rebuildRects()
+    rootDimension = Dimension(layoutInspector.layoutInspectorModel.root.width, layoutInspector.layoutInspectorModel.root.height)
+  }
+
+  private fun drawView(g: Graphics,
+                       view: InspectorView,
+                       rect: Shape,
+                       transform: AffineTransform) {
+    val g2 = g.create() as Graphics2D
+    g2.setRenderingHints(HQ_RENDERING_HINTS)
+    if (drawBorders) {
+      if (view == layoutInspector.layoutInspectorModel.selection) {
+        g2.color = Color.RED
+        g2.stroke = BasicStroke(3f)
+      }
+      else {
+        g2.color = Color.BLUE
+        g2.stroke = BasicStroke(1f)
+      }
+      g2.draw(rect)
+    }
+
+    g2.transform = g2.transform.apply { concatenate(transform) }
+
+    val bufferedImage = view.image
+    if (bufferedImage != null) {
+      val composite = g2.composite
+      if (layoutInspector.layoutInspectorModel.selection != null && view != layoutInspector.layoutInspectorModel.selection) {
+        g2.composite = AlphaComposite.SrcOver.derive(0.6f)
+      }
+      g2.drawImage(bufferedImage, view.x, view.y, null)
+      g2.composite = composite
+    }
+    if (drawBorders && view == layoutInspector.layoutInspectorModel.selection) {
+      g2.color = Color.BLACK
+      g2.font = g2.font.deriveFont(20f)
+      g2.drawString(view.type, view.x + 5, view.y + 25)
+    }
   }
 
   override fun getData(dataId: String): Any? {
@@ -171,6 +257,24 @@ class DeviceViewPanel(private val layoutInspector: LayoutInspector) : JPanel(Bor
     panel.add(leftPanel, BorderLayout.CENTER)
 
     val rightGroup = DefaultActionGroup()
+    rightGroup.add(object: AnAction("reset") {
+      override fun actionPerformed(e: AnActionEvent) {
+        when (viewState) {
+          ViewMode.XY -> {
+            xOff = 0.0
+            yOff = 0.0
+            viewState = ViewMode.FIXED
+          }
+          ViewMode.FIXED -> viewState = ViewMode.X_ONLY
+          ViewMode.X_ONLY -> viewState = ViewMode.XY
+        }
+        rebuildRects()
+      }
+
+      override fun update(e: AnActionEvent) {
+        e.presentation.icon = viewState.icon
+      }
+    })
     rightGroup.add(ZoomOutAction)
     rightGroup.add(ZoomLabelAction)
     rightGroup.add(ZoomInAction)
@@ -183,55 +287,49 @@ class DeviceViewPanel(private val layoutInspector: LayoutInspector) : JPanel(Bor
 
   private fun rebuildRects() {
     hitRects.clear()
-    val queue = LinkedList<Pair<InspectorView, Int>>()
-    queue.add(layoutInspector.layoutInspectorModel.root to 0)
-    while (!queue.isEmpty()) {
-      val item = queue.remove()
-      val view = item.first
-      val rect = Rectangle(((view.x * cos(angle)) + (sin(angle) * item.second) * LAYER_SPACING).toInt(),
-                           (view.y + (sin(angle * 0.1) * item.second * LAYER_SPACING)).toInt(),
-                           (view.width * cos(angle)).toInt(), view.height)
-      hitRects.add(rect to view)
-      queue.addAll(view.children.map { it to item.second + 1 })
-    }
+    val transform = AffineTransform()
+    transform.translate(size.width / 2.0, size.height / 2.0)
+    transform.scale(scale, scale)
+    transform.translate(-layoutInspector.layoutInspectorModel.root.width / 2.0, -layoutInspector.layoutInspectorModel.root.height / 2.0)
+
+    val magnitude = min(1.0, hypot(xOff, yOff))
+    val angle = if (abs(xOff) < 0.00001) PI / 2.0 else atan(yOff / xOff)
+
+    transform.translate(rootDimension.width / 2.0, rootDimension.height / 2.0)
+    transform.rotate(angle)
+    val maxDepth = findMaxDepth(layoutInspector.layoutInspectorModel.root)
+    rebuildOneRect(transform, magnitude, 0, maxDepth, angle, layoutInspector.layoutInspectorModel.root)
+    repaint()
   }
 
-  private fun draw(view: InspectorView, g: Graphics, depth: Int) {
-    val g2 = g.create() as Graphics2D
-    val bufferedImage = view.image
+  private fun findMaxDepth(view: InspectorView): Int {
+    return 1 + (view.children.map { findMaxDepth(it) }.max() ?: 0)
+  }
 
-    g2.translate((sin(angle) * depth * LAYER_SPACING).toInt(), (sin(angle * 0.1) * depth * LAYER_SPACING).toInt())
-    g2.scale(cos(angle), 1.0)
-    if (bufferedImage != null) {
-      val composite = g2.composite
-      if (layoutInspector.layoutInspectorModel.selection != null && view != layoutInspector.layoutInspectorModel.selection) {
-        g2.composite = AlphaComposite.SrcOver.derive(0.6f)
-      }
-      g2.drawImage(bufferedImage, view.x, view.y, null)
-      g2.composite = composite
-    }
-    if (drawBorders) {
-      if (view == layoutInspector.layoutInspectorModel.selection) {
-        g2.color = Color.BLACK
-        g2.font = g2.font.deriveFont(20f)
-        g2.drawString(view.type, view.x + 5, view.y + 25)
-        g2.color = Color.RED
-        g2.stroke = BasicStroke(3f)
-      }
-      else {
-        g2.color = Color.BLUE
-        g2.stroke = BasicStroke(1f)
-      }
-      g2.drawRect(view.x, view.y, view.width, view.height)
-    }
-    view.children.forEach { draw(it, g, depth + 1) }
+  private fun rebuildOneRect(transform: AffineTransform,
+                             magnitude: Double,
+                             depth: Int,
+                             maxDepth: Int,
+                             angle: Double,
+                             view: InspectorView) {
+    val viewTransform = AffineTransform(transform)
+
+    val sign = if (xOff < 0) -1 else 1
+    viewTransform.translate(magnitude * (depth - maxDepth / 2) * LAYER_SPACING * sign, 0.0)
+    viewTransform.scale(sqrt(1.0 - magnitude * magnitude), 1.0)
+    viewTransform.rotate(-angle)
+    viewTransform.translate(-rootDimension.width / 2.0, -rootDimension.height / 2.0)
+
+    val rect = viewTransform.createTransformedShape(Rectangle(view.x, view.y, view.width, view.height))
+    hitRects.add(Triple(rect, viewTransform, view))
+    view.children.forEach { rebuildOneRect(transform, magnitude, depth + 1, maxDepth, angle, it) }
   }
 
   private fun modelChanged(old: InspectorModel, new: InspectorModel) {
     old.selectionListeners.remove(this::selectionChanged)
     new.selectionListeners.add(this::selectionChanged)
+    rootDimension = Dimension(new.root.width, new.root.height)
     rebuildRects()
-    repaint()
   }
 
   private fun selectionChanged(old: InspectorView?, new: InspectorView?) {
