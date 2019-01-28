a8e5302d005cb4fff26b8a66fec81658d15beb23
Alan Leung <acleung@google.com>
2019-01-28 12:58:05 -0800

    Evaluate the state of debugging App.
    Evaluate the state of the the app that is currently being debugged.
    This will be used by the Deployer to determine how exactly
    are we going to perform the swap.
    
    Bug: 121151729
    Test: Debugger swaps with O
    
    Change-Id: Ia0caf7efac3f47d31bfecdbe480ef65b99596a87

--- deploy/src/com/android/tools/idea/run/tasks/ApplyCodeChangesTask.java
+++ deploy/src/com/android/tools/idea/run/tasks/ApplyCodeChangesTask.java
@@ -56,21 +56,16 @@ public class ApplyCodeChangesTask extends AbstractDeployTask {
    * @param device The device we are deploying to.
    * @param apk The apk we want to deploy.
    */
-  private Map<Integer, ClassRedefiner> makeSpecificRedefiners(Project project, IDevice device)
+  private ImmutableMap<Integer, ClassRedefiner> makeSpecificRedefiners(Project project, IDevice device)
     throws DeployerException {
     if (!DebuggerRedefiner.hasDebuggersAttached(project)) {
       return ImmutableMap.of();
     }
 
-    if (!device.getVersion().isGreaterOrEqualThan(28)) {
-      throw DeployerException.operationNotSupported(
-        "Cannot perform Apply Code Change while debugging for API 27 or below. Please upgrade Android version.");
-    }
-
-    ClassRedefiner debugRedefiner = new DebuggerRedefiner(project);
     ImmutableMap.Builder<Integer, ClassRedefiner> debugRedefiners = ImmutableMap.builder();
     for (Client client : device.getClients()) {
       if (client.isDebuggerAttached()) {
+        ClassRedefiner debugRedefiner = new DebuggerRedefiner(project, client.getDebuggerListenPort());
         debugRedefiners.put(client.getClientData().getPid(), debugRedefiner);
       }
     }
@@ -81,7 +76,7 @@ public class ApplyCodeChangesTask extends AbstractDeployTask {
   @Override
   protected Deployer.Result perform(IDevice device, Deployer deployer, String applicationId, List<File> files) throws DeployerException {
     LOG.info("Applying code changes to application: " + applicationId);
-    Map<Integer, ClassRedefiner> redefiners = makeSpecificRedefiners(getProject(), device);
+    ImmutableMap<Integer, ClassRedefiner> redefiners = makeSpecificRedefiners(getProject(), device);
     return deployer.codeSwap(getPathsToInstall(files), redefiners);
   }
 
