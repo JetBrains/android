5f72a914eaaab3b9fc67c5d6d1f4b001d6edda29
Bruno Buss <buss@google.com>
2019-01-28 15:04:24 +0000

    Cleanup unused ResourceResolver after disabling validation.
    Bug: 112700217
    Test: existing
    Change-Id: I9b01091ac343114b2f227cb637f1db76ce9a7427

--- android/testSrc/com/android/tools/idea/sdk/install/patch/PatchInstallerTest.java
+++ android/testSrc/com/android/tools/idea/sdk/install/patch/PatchInstallerTest.java
@@ -21,14 +21,13 @@ import com.android.repository.testframework.FakeProgressIndicator;
 import com.android.repository.testframework.MockFileOp;
 import com.google.common.collect.ImmutableList;
 import com.intellij.util.PathUtil;
-import junit.framework.TestCase;
-import org.jetbrains.annotations.NotNull;
-
-import javax.xml.bind.JAXBException;
 import java.awt.*;
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.InputStream;
+import javax.xml.bind.JAXBException;
+import junit.framework.TestCase;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * Tests for {@link PatchInstallerFactory}.
@@ -111,8 +110,7 @@ public class PatchInstallerTest extends TestCase {
     throws JAXBException {
     InputStream remoteInput = new ByteArrayInputStream(REMOTE.getBytes());
     ImmutableList<SchemaModule<?>> modules = ImmutableList.of(RepoManager.getGenericModule());
-    Repository r = (Repository)SchemaModuleUtil
-      .unmarshal(remoteInput, modules, repoManager.getResourceResolver(progress), true, progress);
+    Repository r = (Repository) SchemaModuleUtil.unmarshal(remoteInput, modules, true, progress);
     RemotePackage p = r.getRemotePackage().get(0);
     ConstantSourceProvider provider = new ConstantSourceProvider("http://example.com", "dummy", modules);
     p.setSource(provider.getSources(null, progress, false).get(0));
