a7f849a6d7a4947a230927b991db59be7e22856c
Peter Xu <peterx@google.com>
2019-01-29 09:18:00 -0800

    Track data binding enabled metric
    This is the first of many CLs to track data binding usage metrics. This logs whether data binding is enabled in build.gradle file and defines a service interface that is implented with a dummy implementatio in core, and overriden with an actual implementation in data binding.
    
    TODO(b/123721754): data binding is enabled on a per module basis, and so should be tracked that way. Currently, we consider a project to be enabled if any of its module is enabled.
    
    Bug: 123029035
    Test: DataBindingDummyTrackerTest and DataBindingTrackerTest.
    Change-Id: I3378c24bc32dd5f731fd72e193f13eb5f806f4b9

---
+++ android/src/com/android/tools/idea/databinding/analytics/api/DataBindingTracker.kt
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.databinding.analytics.api
+
+import com.intellij.openapi.components.ServiceManager
+import com.intellij.openapi.project.Project
+
+/**
+ * Defines the end point through which components can report data binding related metrics, in order to help us understand how many projects
+ * is using data binding.
+ */
+interface DataBindingTracker {
+  companion object {
+    /**
+     * This will always return data binding module's implementation of [DataBindingTracker] if data binding module is enabled.
+     * Otherwise return a default dummy [DataBindingDefaultTracker] that returns a default (negative) proto.
+     */
+    fun getInstance(project: Project): DataBindingTracker {
+      return ServiceManager.getService(project, DataBindingTracker::class.java)
+    }
+  }
+
+  /**
+   * Tracks whether the user has enabled data binding in gradle file. Currently it's tracked at sync time.
+   */
+  fun trackDataBindingEnabled()
+}
---
+++ android/testSrc/com/android/tools/idea/databinding/analytics/DataBindingDefaultTrackerTest.kt
@@ -0,0 +1,69 @@
+/*
+ * Copyright (C) 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.databinding.analytics
+
+import com.android.testutils.TestUtils
+import com.android.testutils.VirtualTimeScheduler
+import com.android.tools.analytics.TestUsageTracker
+import com.android.tools.analytics.UsageTracker
+import com.android.tools.idea.gradle.project.sync.GradleSyncInvoker
+import com.android.tools.idea.gradle.project.sync.GradleSyncState
+import com.android.tools.idea.testing.AndroidProjectRule
+import com.android.tools.idea.testing.TestProjectPaths
+import com.google.common.truth.Truth.assertThat
+import com.google.wireless.android.sdk.stats.AndroidStudioEvent
+import com.google.wireless.android.sdk.stats.GradleSyncStats
+import com.intellij.openapi.command.WriteCommandAction.runWriteCommandAction
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+class DataBindingDefaultTrackerTest {
+  @JvmField
+  @Rule
+  val projectRule = AndroidProjectRule.withSdk().initAndroid(true)
+
+  @Before
+  fun setUp() {
+    projectRule.fixture.testDataPath = TestUtils.getWorkspaceFile(
+      "tools/adt/idea/android/testData/${TestProjectPaths.SIMPLE_APPLICATION}").path
+    projectRule.fixture.copyDirectoryToProject("", "")
+  }
+
+  @Test
+  fun testTrackDataBindingEnabled() {
+    val tracker = TestUsageTracker(VirtualTimeScheduler())
+    runWriteCommandAction(projectRule.project) {
+      try {
+        UsageTracker.setWriterForTest(tracker)
+        val syncState = GradleSyncState.getInstance(projectRule.project)
+        syncState.syncStarted(true, GradleSyncInvoker.Request(GradleSyncStats.Trigger.TRIGGER_TEST_REQUESTED))
+        syncState.syncEnded()
+
+        assertThat(
+          tracker.usages
+            .map { it.studioEvent }
+            .last { it.kind == AndroidStudioEvent.EventKind.DATA_BINDING }
+            .dataBindingEvent.pollMetadata.dataBindingEnabled)
+          .isFalse()
+      }
+      finally {
+        tracker.close()
+        UsageTracker.cleanAfterTesting()
+      }
+    }
+  }
+}
