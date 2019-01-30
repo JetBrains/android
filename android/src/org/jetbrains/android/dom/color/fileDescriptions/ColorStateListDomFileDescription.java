d4f69db5155be10192bdb7562399beee8999e38d
Jerome Gaillard <jgaillard@google.com>
2019-01-30 17:55:53 +0000

    Add DOM file description for color gradients
    This fixes the fact that the gradient tag in color gradient XML files
    was highlighted in red.
    
    Bug: 115396023
    Test: tests updated
    Change-Id: Ie08c64adc85383233034b529e91495586a2c4445

--- android/src/org/jetbrains/android/dom/color/fileDescriptions/ColorStateListDomFileDescription.java
+++ android/src/org/jetbrains/android/dom/color/fileDescriptions/ColorStateListDomFileDescription.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2000-2011 JetBrains s.r.o.
+ * Copyright (C) 2019 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,33 +13,22 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.jetbrains.android.dom.color;
+package org.jetbrains.android.dom.color.fileDescriptions;
 
 import com.android.resources.ResourceFolderType;
-import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.util.Computable;
-import com.intellij.psi.xml.XmlFile;
-import org.jetbrains.android.dom.AndroidResourceDomFileDescription;
+import org.jetbrains.android.dom.AbstractSingleRootFileDescription;
+import org.jetbrains.android.dom.color.ColorSelector;
 
 /**
  * Framework code: ColorStateList#createFromXml
  */
-public class ColorDomFileDescription extends AndroidResourceDomFileDescription<ColorSelector> {
-  public ColorDomFileDescription() {
+public class ColorStateListDomFileDescription extends AbstractSingleRootFileDescription<ColorSelector> {
+  public ColorStateListDomFileDescription() {
     super(ColorSelector.class, "selector", ResourceFolderType.COLOR);
   }
 
   @Override
   public boolean acceptsOtherRootTagNames() {
     return false;
-  }
-
-  public static boolean isColorResourceFile(final XmlFile file) {
-    return ApplicationManager.getApplication().runReadAction(new Computable<Boolean>() {
-      @Override
-      public Boolean compute() {
-        return new ColorDomFileDescription().isMyFile(file, null);
-      }
-    });
   }
 }
