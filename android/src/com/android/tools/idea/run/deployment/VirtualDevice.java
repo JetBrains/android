28db39270c886c35e80988808d227b193230ffa0
Juan C Nuno <juancnuno@google.com>
2019-01-29 19:11:54 -0800

    Suppress the unstable API usage warning
    So Analyze - Inspect Code... is clean
    
    Bug: Not applicable
    Test: Not applicable
    Change-Id: Idb6eb15460ab46d33c7130598bd4cc1721e04c40

--- android/src/com/android/tools/idea/run/deployment/VirtualDevice.java
+++ android/src/com/android/tools/idea/run/deployment/VirtualDevice.java
@@ -39,6 +39,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Collections;
 import java.util.Objects;
+import java.util.stream.Collector;
 import java.util.stream.Stream;
 import javax.swing.Icon;
 import org.jetbrains.annotations.NotNull;
@@ -76,12 +77,15 @@ final class VirtualDevice extends Device {
     }
 
     try (Stream<Path> stream = Files.list(snapshots)) {
+      @SuppressWarnings("UnstableApiUsage")
+      Collector<String, ?, ImmutableList<String>> collector = ImmutableList.toImmutableList();
+
       return stream
         .filter(Files::isDirectory)
         .map(VirtualDevice::getName)
         .filter(Objects::nonNull)
         .sorted()
-        .collect(ImmutableList.toImmutableList());
+        .collect(collector);
     }
     catch (IOException exception) {
       Logger.getInstance(VirtualDevice.class).warn(snapshots.toString(), exception);
