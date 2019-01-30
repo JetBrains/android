9049062f71322ac96e1afa481c2d5c65138953b5
Jerome Gaillard <jgaillard@google.com>
2019-01-30 17:55:53 +0000

    Add DOM file description for color gradients
    This fixes the fact that the gradient tag in color gradient XML files
    was highlighted in red.
    
    Bug: 115396023
    Test: tests updated
    Change-Id: Ie08c64adc85383233034b529e91495586a2c4445

--- android/src/org/jetbrains/android/dom/color/GradientColor.java
+++ android/src/org/jetbrains/android/dom/color/GradientColor.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2000-2011 JetBrains s.r.o.
+ * Copyright (C) 2019 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,14 +16,11 @@
 package org.jetbrains.android.dom.color;
 
 import com.intellij.util.xml.DefinesXml;
-
 import java.util.List;
-import org.jetbrains.android.dom.color.fileDescriptions.ColorStateListDomFileDescription;
+import org.jetbrains.android.dom.Styleable;
 
-/**
- * @see ColorStateListDomFileDescription
- */
 @DefinesXml
-public interface ColorSelector extends ColorDomElement {
-  List<ColorStateListItem> getItems();
+@Styleable("GradientColor")
+public interface GradientColor extends ColorDomElement {
+  List<GradientColorItem> getItems();
 }
