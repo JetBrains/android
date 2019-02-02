1e7f0528725e26fa9f35ae469d31bdfd7780c1ab
Sergey Prigogin <sprigogin@google.com>
2019-02-01 19:28:47 -0800

    Create id resources for all attribute values containing "@+id/..."
    Do not limit id resource creation to values of attributes belonging to android namespace.
    
    Test: AarSourceResourceRepositoryTest
    Bug: 123032845
    Change-Id: Iefe8925ccb11e5a17da588068fb301a0e004a00b

--- resources-aar/src/com/android/tools/idea/resources/aar/AarSourceResourceRepository.java
+++ resources-aar/src/com/android/tools/idea/resources/aar/AarSourceResourceRepository.java
@@ -16,7 +16,6 @@
 package com.android.tools.idea.resources.aar;
 
 import static com.android.SdkConstants.ANDROID_NS_NAME;
-import static com.android.SdkConstants.ANDROID_URI;
 import static com.android.SdkConstants.ATTR_FORMAT;
 import static com.android.SdkConstants.ATTR_ID;
 import static com.android.SdkConstants.ATTR_NAME;
@@ -759,15 +758,13 @@ public class AarSourceResourceRepository extends AbstractAarResourceRepository {
           if (event == XmlPullParser.START_TAG) {
             int numAttributes = parser.getAttributeCount();
             for (int i = 0; i < numAttributes; i++) {
-              if (ANDROID_URI.equals(parser.getAttributeNamespace(i))) {
-                String idValue = parser.getAttributeValue(i);
-                if (idValue.startsWith(NEW_ID_PREFIX) && idValue.length() > NEW_ID_PREFIX.length()) {
-                  String resourceName = idValue.substring(NEW_ID_PREFIX.length());
-                  ResourceVisibility visibility = getVisibility(ResourceType.ID, resourceName);
-                  AarValueResourceItem item = new AarValueResourceItem(ResourceType.ID, resourceName, sourceFile, visibility, null);
-                  if (!resourceAlreadyDefined(item)) { // Don't create duplicate ID resources.
-                    addValueResourceItem(item);
-                  }
+              String idValue = parser.getAttributeValue(i);
+              if (idValue.startsWith(NEW_ID_PREFIX) && idValue.length() > NEW_ID_PREFIX.length()) {
+                String resourceName = idValue.substring(NEW_ID_PREFIX.length());
+                ResourceVisibility visibility = getVisibility(ResourceType.ID, resourceName);
+                AarValueResourceItem item = new AarValueResourceItem(ResourceType.ID, resourceName, sourceFile, visibility, null);
+                if (!resourceAlreadyDefined(item)) { // Don't create duplicate ID resources.
+                  addValueResourceItem(item);
                 }
               }
             }
@@ -1402,9 +1399,7 @@ public class AarSourceResourceRepository extends AbstractAarResourceRepository {
         }
       } while (event != XmlPullParser.END_DOCUMENT);
 
-      String result = ValueXmlHelper.unescapeResourceString(text.toString(), false, true);
-      assert result != null;
-      return result;
+      return ValueXmlHelper.unescapeResourceString(text.toString(), false, true);
     }
 
     private boolean getTextInclusionState() {
