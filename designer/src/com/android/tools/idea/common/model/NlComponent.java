ac5ac2adaa5fd22363d3b07d47b423143b37d967
Isaac Chai <ichai@google.com>
2019-02-06 15:56:27 -0800

    Fix PsiInvalidElementAccessException on NlComponent#setAttribute
    Follow up from the revert 6281430.
    
    The previous fix also had an issue where one of the code calling
    setAttribute was done outside WriteActionCommand, which should have
    failed but it probably relied on the behaviour that new value was
    the same as the existing one.
    
    This fix ensures that processes are correct, but does not protect from
    IncorrectOperationException, which will be thrown when setting attribute
    outside the WriteCommandAction.
    
    Bug: 123704869
    Test: Unit test + drag n drop works
    
    Change-Id: Ib239a0fe53a3d83a3f0ae922e6ee086008d3ce0a

--- designer/src/com/android/tools/idea/common/model/NlComponent.java
+++ designer/src/com/android/tools/idea/common/model/NlComponent.java
@@ -357,8 +357,24 @@ public class NlComponent implements NlAttributesHolder {
       myDelegate.setAttribute(this, namespace, attribute, value);
       return;
     }
-    XmlTag tag = getTag();
-    if (!tag.isValid()) {
+
+    Application application = ApplicationManager.getApplication();
+    if (!application.isWriteAccessAllowed()) {
+      application.runWriteAction(() -> {
+        setAttributeImpl(namespace, attribute, value);
+      });
+    }
+    else {
+      setAttributeImpl(namespace, attribute, value);
+    }
+  }
+
+  /**
+   * Must be called from Write acess
+   */
+  private void setAttributeImpl(@Nullable String namespace, @NotNull String attribute, @Nullable String value) {
+    XmlTag tag = myBackend.getTag();
+    if (tag == null) {
       // This could happen when trying to set an attribute in a component that has been already deleted
       return;
     }
