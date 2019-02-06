6aac7c9bf48c2f497d12f1ae538a2cc8223e184a
Stefan Xenos <sxenos@google.com>
2019-02-06 09:05:03 -0800

    Combine FutureUtils.kt with FutureUtils.java
    Some of the extension methods in FutureUtils.kt are needed by additional
    modules. Since they have no dependencies outside guava, they can be
    safely moved to android common. This also allows the two "FutureUtils"
    to be combined.
    
    Test: FutureUtilsTest.kt
    Change-Id: I76a7bac22eab54f8df171c390841d98d3e4f57d8
    Fixes: 123989972

--- designer/src/com/android/tools/idea/common/model/NlDependencyManager.kt
+++ designer/src/com/android/tools/idea/common/model/NlDependencyManager.kt
@@ -17,6 +17,7 @@ package com.android.tools.idea.common.model
 
 import com.android.ide.common.repository.GradleCoordinate
 import com.android.ide.common.repository.GradleVersion
+import com.android.tools.idea.concurrent.addCallback
 import com.android.tools.idea.projectsystem.*
 import com.android.tools.idea.util.addDependencies
 import com.android.tools.idea.util.dependsOn
