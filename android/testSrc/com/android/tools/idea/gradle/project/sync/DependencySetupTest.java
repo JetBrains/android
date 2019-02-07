64656ec91503b3bbeffdead5865c85af8588a6e5
Michal Bendowski <bendowski@google.com>
2019-02-07 13:23:26 +0000

    Refresh VFS in AndroidGradleTestCase#generateSources
    To avoid flaky tests. Also moved refreshProjectFiles up to AndroidTestBase so it can be used by other tests.
    
    Bug: N/A
    Test: N/A. Will start using refreshProjectFiles in more tests as a follow-up.
    Change-Id: If658a05a9088b9735585e59cfa862d27b9d85fed

--- android/testSrc/com/android/tools/idea/gradle/project/sync/DependencySetupTest.java
+++ android/testSrc/com/android/tools/idea/gradle/project/sync/DependencySetupTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016 The Android Open Source Project
+ * Copyright (C) 2019 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -127,7 +127,7 @@ public class DependencySetupTest extends GradleSyncIntegrationTestCase {
     assertTrue(versionChanged);
 
     runWriteCommandAction(project, buildModel::applyChanges);
-    LocalFileSystem.getInstance().refresh(false /* synchronous */);
+    refreshProjectFiles();
 
     try {
       requestSyncAndWait();
