51ff199a72e6c8830a9ba9a24133861598d636a4
Dmitry Avdeev <dmitry.avdeev@jetbrains.com>
2019-02-08 17:45:10 +0300

    merging Android Studio 3.3: icons & classes updated

--- adt-ui/src/main/java/icons/AdtUIIcons.java
+++ adt-ui/src/main/java/icons/AdtUIIcons.java
@@ -8,11 +8,29 @@ import javax.swing.*;
  * NOTE THIS FILE IS AUTO-GENERATED
  * DO NOT EDIT IT BY HAND, run "Generate icon classes" configuration instead
  */
-public class AdtUIIcons {
+public final class AdtUIIcons {
   private static Icon load(String path) {
     return IconLoader.getIcon(path, AdtUIIcons.class);
   }
 
-  public static final Icon Keyboard_event = load("/icons/events/keyboard-event.png"); // 22x22
-  public static final Icon Rotate_event = load("/icons/events/rotate-event.png"); // 22x22
+  /**
+   * 22x22
+   */
+  public static final Icon Back_button = load("/icons/events/back-button.png");
+  /**
+   * 22x22
+   */
+  public static final Icon Keyboard_event = load("/icons/events/keyboard-event.png");
+  /**
+   * 22x22
+   */
+  public static final Icon Rotate_event = load("/icons/events/rotate-event.png");
+  /**
+   * 22x22
+   */
+  public static final Icon Volume_down = load("/icons/events/volume-down.png");
+  /**
+   * 22x22
+   */
+  public static final Icon Volume_up = load("/icons/events/volume-up.png");
 }
---
+++ whats-new-assistant/src/icons/WhatsNewAssistantIcons.java
@@ -0,0 +1,20 @@
+package icons;
+
+import com.intellij.openapi.util.IconLoader;
+
+import javax.swing.*;
+
+/**
+ * NOTE THIS FILE IS AUTO-GENERATED
+ * DO NOT EDIT IT BY HAND, run "Generate icon classes" configuration instead
+ */
+public final class WhatsNewAssistantIcons {
+  private static Icon load(String path) {
+    return IconLoader.getIcon(path, WhatsNewAssistantIcons.class);
+  }
+
+  /**
+   * 26x29
+   */
+  public static final Icon Whats_new_icon = load("/core/whats_new_icon.png");
+}
