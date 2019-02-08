37b31281a0cdbedba536d5b976db2fb8bd3dc1fc
Matthew Gharrity <gharrma@google.com>
2019-02-08 12:50:46 -0800

    Typing latency: native code and Kotlin scripts
    Bug: 123665991
    Test: manual
    Change-Id: Ib86c468d46fca02d236eb96386a5994da1c2339a

--- android/src/com/android/tools/idea/stats/TypingLatencyTracker.kt
+++ android/src/com/android/tools/idea/stats/TypingLatencyTracker.kt
@@ -19,10 +19,20 @@ import com.android.tools.analytics.UsageTracker
 import com.android.tools.idea.stats.TypingLatencyTracker.reportTypingLatency
 import com.google.wireless.android.sdk.stats.AndroidStudioEvent
 import com.google.wireless.android.sdk.stats.TypingLatencyStats
+import com.google.wireless.android.sdk.stats.TypingLatencyStats.FileType.GROOVY
+import com.google.wireless.android.sdk.stats.TypingLatencyStats.FileType.JAVA
+import com.google.wireless.android.sdk.stats.TypingLatencyStats.FileType.JSON
+import com.google.wireless.android.sdk.stats.TypingLatencyStats.FileType.KOTLIN
+import com.google.wireless.android.sdk.stats.TypingLatencyStats.FileType.KOTLIN_SCRIPT
+import com.google.wireless.android.sdk.stats.TypingLatencyStats.FileType.NATIVE
+import com.google.wireless.android.sdk.stats.TypingLatencyStats.FileType.PROPERTIES
+import com.google.wireless.android.sdk.stats.TypingLatencyStats.FileType.UNKNOWN
+import com.google.wireless.android.sdk.stats.TypingLatencyStats.FileType.XML
 import com.intellij.ide.toProto
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.editor.actionSystem.LatencyListener
 import com.intellij.openapi.fileEditor.FileDocumentManager
+import com.intellij.openapi.vfs.VirtualFile
 import org.HdrHistogram.SingleWriterRecorder
 import java.util.concurrent.ConcurrentHashMap
 
@@ -36,11 +46,12 @@ object TypingLatencyTracker : LatencyListener {
    * Maps file types to latency recorders.
    * We use [SingleWriterRecorder] to allow thread-safe read access from background threads.
    */
-  private val latencyRecorders = ConcurrentHashMap<String, SingleWriterRecorder>()
+  private val latencyRecorders = ConcurrentHashMap<TypingLatencyStats.FileType, SingleWriterRecorder>()
 
   override fun recordTypingLatency(editor: Editor, action: String, latencyMs: Long) {
     // This runs on the EDT, but is thread-safe with respect to a background thread running reportTypingLatency().
-    val fileType = FileDocumentManager.getInstance().getFile(editor.document)?.fileType?.name ?: return
+    val file = FileDocumentManager.getInstance().getFile(editor.document) ?: return
+    val fileType = convertFileType(file)
     val recorder = latencyRecorders.computeIfAbsent(fileType) { SingleWriterRecorder(1) }
     recorder.recordValue(latencyMs)
   }
@@ -56,9 +67,8 @@ object TypingLatencyTracker : LatencyListener {
       if (histogram.totalCount == 0L) {
         continue
       }
-      val convertedFileType = convertFileType(fileType)
       val record = TypingLatencyStats.LatencyRecord.newBuilder().also {
-        it.fileType = convertedFileType
+        it.fileType = fileType
         it.totalKeysTyped = histogram.totalCount
         it.totalLatencyMs = (histogram.totalCount * histogram.mean).toLong()
         it.maxLatencyMs = histogram.maxValue
@@ -80,15 +90,16 @@ object TypingLatencyTracker : LatencyListener {
   }
 
   /** Converts from file type name to proto enum value. */
-  private fun convertFileType(fileType: String): TypingLatencyStats.FileType = when (fileType) {
+  private fun convertFileType(file: VirtualFile): TypingLatencyStats.FileType = when (file.fileType.name) {
     // We use string literals here (rather than, e.g., JsonFileType.INSTANCE.name) to avoid unnecessary
     // dependencies on other plugins. Fortunately, these values are extremely unlikely to change.
-    "JAVA" -> TypingLatencyStats.FileType.JAVA
-    "Kotlin" -> TypingLatencyStats.FileType.KOTLIN
-    "XML" -> TypingLatencyStats.FileType.XML
-    "Groovy" -> TypingLatencyStats.FileType.GROOVY
-    "Properties" -> TypingLatencyStats.FileType.PROPERTIES
-    "JSON" -> TypingLatencyStats.FileType.JSON
-    else -> TypingLatencyStats.FileType.UNKNOWN
+    "JAVA" -> JAVA
+    "Kotlin" -> if (file.extension == "kts") KOTLIN_SCRIPT else KOTLIN
+    "XML" -> XML
+    "Groovy" -> GROOVY
+    "Properties" -> PROPERTIES
+    "JSON" -> JSON
+    "ObjectiveC" -> NATIVE
+    else -> UNKNOWN
   }
 }
