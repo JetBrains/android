6df164918e484f5e1d3df4e82b1b4e4f95470662
Michal Bendowski <bendowski@google.com>
2019-02-11 15:50:33 +0000

    First step towards supporting Androidx Preferences
    - Extended TagToClassMapper to consider an optional second hierarchy root, coming from AndroidX.
    - Fixed SubtagsProcessingUtil to not add subtags for classes that are not PreferenceGroups.
    - Changed AttributeProcessingUtil to not add attrs from widget classes to AndroidX preferences.
    - For now unqualified names are assumed to always mean framework and you can mix and match framework and
      non-framework children and parents. This is the next thing to fix.
    
    Bug: 124229368
    Test: AndroidXmlResourcesDomTest
    Change-Id: Ica8f22395908093e8232fd65a3629459ae187ab9

--- android/src/org/jetbrains/android/dom/xml/AndroidXmlResourcesUtil.java
+++ android/src/org/jetbrains/android/dom/xml/AndroidXmlResourcesUtil.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2000-2010 JetBrains s.r.o.
+ * Copyright (C) 2019 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -50,9 +50,9 @@ public class AndroidXmlResourcesUtil {
     .put("header", "PreferenceHeader")
     .build();
 
-  private static final ImmutableSet<String> ROOT_TAGS = ImmutableSet
-    .of(SdkConstants.TAG_APPWIDGET_PROVIDER, SEARCHABLE_TAG_NAME, KEYBOARD_TAG_NAME, DEVICE_ADMIN_TAG_NAME, ACCOUNT_AUTHENTICATOR_TAG_NAME,
-        PREFERENCE_HEADERS_TAG_NAME, PathsDomFileDescription.TAG_NAME);
+  private static final ImmutableSet<String> ROOT_TAGS = ImmutableSet.of(
+    SdkConstants.TAG_APPWIDGET_PROVIDER, SEARCHABLE_TAG_NAME, KEYBOARD_TAG_NAME, DEVICE_ADMIN_TAG_NAME, ACCOUNT_AUTHENTICATOR_TAG_NAME,
+    PREFERENCE_HEADERS_TAG_NAME, PathsDomFileDescription.TAG_NAME);
 
   private AndroidXmlResourcesUtil() {
   }
