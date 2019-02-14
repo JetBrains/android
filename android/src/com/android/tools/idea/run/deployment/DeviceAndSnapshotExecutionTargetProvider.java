86368016306b7a08774ee922c8ac86bb78a90a99
Chester Hsieh <dunno@google.com>
2019-02-13 17:55:19 -0800

    Dissociate ExecutionTarget from RunConfigurations.
    ExecutionTargets were unnecessarily being associated with specific
    RunConfigurations when all that's needed were the application IDs.
    
    Now whomever needs to check if an application is running on a device,
    they will need to pass in the approriate application ID. This also
    makes ExecutionTargets unaware of specific applications at creation time.
    
    Bug: N/A
    Test: N/A
    Change-Id: I1283cba0c1fbe40f9f924e38bb93f95e3a4d72e3

--- android/src/com/android/tools/idea/run/deployment/DeviceAndSnapshotExecutionTargetProvider.java
+++ android/src/com/android/tools/idea/run/deployment/DeviceAndSnapshotExecutionTargetProvider.java
@@ -17,20 +17,15 @@ package com.android.tools.idea.run.deployment;
 
 import com.android.ddmlib.IDevice;
 import com.android.tools.idea.flags.StudioFlags;
-import com.android.tools.idea.run.AndroidRunConfigurationBase;
-import com.android.tools.idea.run.ApkProvisionException;
 import com.intellij.execution.DefaultExecutionTarget;
 import com.intellij.execution.ExecutionTarget;
 import com.intellij.execution.ExecutionTargetProvider;
-import com.intellij.execution.RunManager;
 import com.intellij.execution.RunnerAndConfigurationSettings;
 import com.intellij.openapi.actionSystem.ActionManager;
-import com.intellij.openapi.module.Module;
 import com.intellij.openapi.project.Project;
 import java.util.Collections;
 import java.util.List;
 import javax.swing.Icon;
-import org.jetbrains.android.facet.AndroidFacet;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -45,31 +40,13 @@ public class DeviceAndSnapshotExecutionTargetProvider extends ExecutionTargetPro
       return Collections.singletonList(DefaultExecutionTarget.INSTANCE);
     }
 
-    RunnerAndConfigurationSettings settings = RunManager.getInstance(project).getSelectedConfiguration();
-    if (settings != null && settings.getConfiguration() instanceof AndroidRunConfigurationBase) {
-      AndroidRunConfigurationBase config = (AndroidRunConfigurationBase)settings.getConfiguration();
-      Module module = config.getConfigurationModule().getModule();
-      if (module != null) {
-        AndroidFacet facet = AndroidFacet.getInstance(module);
-        if (facet != null) {
-          // We always return a ExecutionTarget as long as we have valid config and package names.
-          // This is because we don't want to maintain listener states with ddmlib, and we don't
-          // need to keep track of online/offline states, and we let things that need the states
-          // to query on their own.
-          Device device = action.getSelectedDevice(project);
-          if (device != null) {
-            try {
-              String packageName = config.getApplicationIdProvider(facet).getPackageName();
-              return Collections.singletonList(new Target(device, packageName));
-            }
-            catch (ApkProvisionException ignored) {
-              // There's nothing much we can do if we're having issues loading or parsing the manifest.
-              // The application most likely won't run in this case anyway, so we'll fall back to the
-              // default execution target in this case.
-            }
-          }
-        }
-      }
+    // We always return an ExecutionTarget as long as we have valid config and package names.
+    // This is because we don't want to maintain listener states with ddmlib, and we don't
+    // need to keep track of online/offline states, and we let things that need the states
+    // to query on their own.
+    Device device = action.getSelectedDevice(project);
+    if (device != null) {
+      return Collections.singletonList(new Target(device));
     }
 
     return Collections.singletonList(DefaultExecutionTarget.INSTANCE);
@@ -77,12 +54,10 @@ public class DeviceAndSnapshotExecutionTargetProvider extends ExecutionTargetPro
 
   static class Target extends AndroidExecutionTarget {
     @NotNull private final Device myDevice;
-    @NotNull private final String myPackageName;
     @NotNull private final String myId;
 
-    private Target(@NotNull Device device, @NotNull String packageName) {
+    private Target(@NotNull Device device) {
       myDevice = device;
-      myPackageName = packageName;
       myId = "device_and_snapshot_execution_target|" + myDevice.getName();
     }
 
@@ -110,8 +85,8 @@ public class DeviceAndSnapshotExecutionTargetProvider extends ExecutionTargetPro
     }
 
     @Override
-    public boolean isApplicationRunning() {
-      return myDevice.isRunning(myPackageName);
+    public boolean isApplicationRunning(@NotNull String packageName) {
+      return myDevice.isRunning(packageName);
     }
 
     @Nullable
