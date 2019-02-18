9e106834dc804d998b4aafdcaec5bd1f37476c10
parentej <parentej@google.com>
2019-02-18 15:36:11 +0000

    [NPW] Allow creating modules in sub folder - Supply values to templates
    Allow module names of format :libs:lib1
    
    Templates are being injected (TemplateValueInjector) with wrong values for
    "Project Path" and "Module Name" (ATTR_PROJECT_LOCATION and ATTR_MODULE_NAME)
    
    Instead of assuming that "Project Path" is the "Module Directory Parent" and
    "Module Name" is "Module Directory Name", this CL re-factors
    TemplateValueInjector@setModuleRoots to be specified directly with a
    "Project Location" and "Module Name".
    
    This CL does not fix the problem yet, it just re-factors the code to provide
    the right data to the Wizard Templates.
    
    Bug: 124359884
    Test: TemplateTest and UI Tests
    Change-Id: I0080fe299a2cb0a92b6c1fd0ec7e0960edfff550

--- android/testSrc/com/android/tools/idea/gradle/project/library/AndroidLibraryTest.java
+++ android/testSrc/com/android/tools/idea/gradle/project/library/AndroidLibraryTest.java
@@ -80,7 +80,7 @@ public class AndroidLibraryTest extends AndroidGradleTestCase {
 
     // Create a Wizard and add an Activity to the lib module
     TemplateHandle myTemplateHandle = new TemplateHandle(TemplateManager.getInstance().getTemplateFile("Activity", "Empty Activity"));
-    NamedModuleTemplate template = GradleAndroidModuleTemplate.createDefaultTemplateAt(new File(project.getProjectFilePath()));
+    NamedModuleTemplate template = GradleAndroidModuleTemplate.createDefaultTemplateAt(project.getProjectFilePath(), "");
     RenderTemplateModel render = new RenderTemplateModel(libAndroidFacet, myTemplateHandle, "com.example", template, "command",
                                                          new ProjectSyncInvoker.DefaultProjectSyncInvoker(), true);
 
