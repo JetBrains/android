925df0e03bc5d093d8a578f3b5e7b3143d6787cd
Matthew Gharrity <gharrma@google.com>
2019-02-19 15:38:37 -0800

    Copy test project for custom Lint checks
    This copies part of the project used in the Gradle integration test for
    custom Lint checks, so that we can have an integration test on the IDE
    side as well (to make sure that custom Lint checks show up in the IDE).
    
    This is a direct copy (minus some removed files);
    modifications will come in a subsequent commit.
    
    Test: n/a
    Bug: n/a
    Change-Id: Id8412aeefbaf666275ee984f6f74d37b4c82256f

---
+++ android/testData/projects/lintCustomChecks/lint/src/main/java/com/example/google/lint/MyDetector.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.google.lint;
+
+import com.android.annotations.NonNull;
+import com.android.annotations.Nullable;
+import com.android.tools.lint.detector.api.Category;
+import com.android.tools.lint.detector.api.Detector;
+import com.android.tools.lint.detector.api.Implementation;
+import com.android.tools.lint.detector.api.Issue;
+import com.android.tools.lint.detector.api.JavaContext;
+import com.android.tools.lint.detector.api.Scope;
+import com.android.tools.lint.detector.api.Severity;
+import com.android.tools.lint.detector.api.SourceCodeScanner;
+import java.util.Collections;
+import java.util.List;
+import org.jetbrains.uast.UClass;
+import org.jetbrains.uast.UElement;
+
+public class MyDetector extends Detector implements SourceCodeScanner {
+    public static final Issue ISSUE =
+            Issue.create(
+                            "UnitTestLintCheck2",
+                            "Custom Lint Check",
+                            "This app should not have implement java.util.List.",
+                            Category.CORRECTNESS,
+                            8,
+                            Severity.ERROR,
+                            new Implementation(MyDetector.class, Scope.JAVA_FILE_SCOPE))
+                    .
+                    // Make sure other integration tests don't pick this up.
+                    // The unit test will turn it on with android.lintOptions.check <id>
+                    setEnabledByDefault(false);
+
+    public MyDetector() {}
+
+    @Nullable
+    @Override
+    public List<String> applicableSuperClasses() {
+        return Collections.singletonList("java.util.List");
+    }
+
+    @Override
+    public void visitClass(@NonNull JavaContext context, @NonNull UClass declaration) {
+        context.report(
+                ISSUE,
+                declaration,
+                context.getLocation((UElement) declaration),
+                "Do not implement java.util.List directly");
+    }
+}
