98ef748c56f99a56c7ad659f5a04068e9fc64da3
Michal Bendowski <bendowski@google.com>
2019-02-19 15:32:09 +0000

    Tweaks to annotator tests
    - Run with both values of the flag.
    - Fix handling of the R class.
    
    Test: AndroidGutterIconAnnotatorTest
    Bug: 123563685
    Change-Id: I72ac08f4b7de7762523c9e9cae9b6d443fff4456

--- android/testData/annotator/ColorTest.java
+++ android/testData/annotator/ColorTest.java
@@ -6,12 +6,4 @@ public class ColorTest {
         int color2 = R.color.color2;
         int selector = R.color.selector;
     }
-
-    public static class R {
-        public static class color {
-            public static final int color1 = 1;
-            public static final int color2 = 2;
-            public static final int selector = 3;
-        }
-    }
 }
