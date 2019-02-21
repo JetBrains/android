1f31cb537d8eb0191608048a3248bfd40a9d9f78
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2019-02-21 16:53:17 +0100

    [platform] states nullability for SdkType.suggestSdkName's 1st parameter (IDEA-CR-43328)

--- android/src/org/jetbrains/android/sdk/AndroidSdkType.java
+++ android/src/org/jetbrains/android/sdk/AndroidSdkType.java
@@ -1,6 +1,4 @@
-/*
- * Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
- */
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.android.sdk;
 
 import com.android.sdklib.AndroidVersion;
@@ -93,7 +91,7 @@ public class AndroidSdkType extends JavaDependentSdkType implements JavaSdkType 
 
   @Override
   @NotNull
-  public String suggestSdkName(String currentSdkName, String sdkHome) {
+  public String suggestSdkName(@Nullable String currentSdkName, String sdkHome) {
     return SDK_NAME;
   }
 
