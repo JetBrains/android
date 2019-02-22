14093ce25900b14de5f3946d3d63705875d02d44
David Herman <davidherman@google.com>
2019-02-21 16:52:45 -0800

    Convert LightBR classes to Kotlin
    Now that we have tests for BR files, it's a good opportunity
    to convert related files to Kotlin and clean up some verbose
    code.
    
    Bug: N/A
    Test: N/A
    Change-Id: Ica0298c738b7dad04b246bf745215a761f6ff472

---
+++ databinding/src/com/android/tools/idea/databinding/BrClassFinder.kt
@@ -0,0 +1,62 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.databinding
+
+import com.intellij.psi.PsiClass
+import com.intellij.psi.PsiElementFinder
+import com.intellij.psi.PsiPackage
+import com.intellij.psi.search.GlobalSearchScope
+import com.intellij.psi.util.CachedValue
+import com.intellij.psi.util.CachedValueProvider
+import com.intellij.psi.util.CachedValuesManager
+
+class BrClassFinder(private val component: DataBindingProjectComponent) : PsiElementFinder() {
+  private val classByPackageCache: CachedValue<Map<String, PsiClass>>
+
+  private val isEnabled: Boolean
+    get() = DataBindingCodeGenService.getInstance().isCodeGenSetToInMemoryFor(component)
+
+  init {
+    classByPackageCache = CachedValuesManager.getManager(component.project).createCachedValue(
+      {
+        val classes = component.getDataBindingEnabledFacets()
+          .associate { facet -> DataBindingUtil.getBrQualifiedName(facet) to DataBindingClassFactory.getOrCreateBrClassFor(facet) }
+
+        CachedValueProvider.Result.create<Map<String, PsiClass>>(classes, component)
+      }, false)
+  }
+
+  override fun findClass(qualifiedName: String, scope: GlobalSearchScope): PsiClass? {
+    if (!isEnabled || !qualifiedName.endsWith(DataBindingUtil.BR)) {
+      return null
+    }
+    val psiClass = classByPackageCache.value[qualifiedName]
+    return psiClass?.takeIf { psiClass.project == scope.project }
+  }
+
+  override fun findClasses(qualifiedName: String, scope: GlobalSearchScope): Array<PsiClass> {
+    if (!isEnabled) {
+      return PsiClass.EMPTY_ARRAY
+    }
+
+    val aClass = findClass(qualifiedName, scope) ?: return PsiClass.EMPTY_ARRAY
+    return arrayOf(aClass)
+  }
+
+  override fun findPackage(qualifiedName: String): PsiPackage? {
+    return null
+  }
+}
