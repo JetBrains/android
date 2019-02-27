90bf70efad5579f6b678833838cbe8f8556ab589
Juan C Nuno <juancnuno@google.com>
2019-02-26 16:42:54 -0800

    Override legacy modes with the new combo box one
    Unfortunately this breaks the Deployment Target Options drop down list in
    the Run/Debug Configurations dialog again. But we're gonna get rid of it.
    
    Bug: 123377224
    Test: DeployTargetContextTest
    Change-Id: Id558ffbafda889ade11bc13d21225d770da25d67

--- android/testSrc/com/android/tools/idea/run/editor/DeployTargetContextTest.java
+++ android/testSrc/com/android/tools/idea/run/editor/DeployTargetContextTest.java
@@ -18,9 +18,14 @@ package com.android.tools.idea.run.editor;
 import static org.junit.Assert.assertEquals;
 
 import com.android.tools.idea.run.TargetSelectionMode;
+import com.android.tools.idea.run.deployment.DeviceAndSnapshotComboBoxTargetProvider;
 import com.android.tools.idea.testing.AndroidProjectRule;
+import com.intellij.openapi.Disposable;
+import com.intellij.openapi.project.Project;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import org.jetbrains.annotations.NotNull;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -35,6 +40,70 @@ public final class DeployTargetContextTest {
   @Before
   public void initProviders() {
     myProviders = Arrays.asList(DeployTargetProvider.EP_NAME.getExtensions());
+  }
+
+  @Test
+  public void getCurrentDeployTargetProviderSelectDeviceSnapshotComboBoxIsVisible() {
+    DeployTargetContext context = new DeployTargetContext(() -> true, myProviders);
+
+    Object provider = context.getCurrentDeployTargetProvider();
+
+    assertEquals(DeployTargetProvider.EP_NAME.findExtension(DeviceAndSnapshotComboBoxTargetProvider.class), provider);
+  }
+
+  @Test
+  public void getCurrentDeployTargetProviderTargetSelectionModeEqualsFirebaseDeviceMatrix() {
+    // Arrange
+    DeployTargetProvider expectedProvider = new CloudTestMatrixTargetProvider();
+
+    myProviders = new ArrayList<>(myProviders);
+    myProviders.add(expectedProvider);
+
+    DeployTargetContext context = new DeployTargetContext(() -> true, myProviders);
+    context.setTargetSelectionMode(TargetSelectionMode.FIREBASE_DEVICE_MATRIX);
+
+    // Act
+    Object actualProvider = context.getCurrentDeployTargetProvider();
+
+    // Assert
+    assertEquals(expectedProvider, actualProvider);
+  }
+
+  private static final class CloudTestMatrixTargetProvider extends DeployTargetProvider {
+    @NotNull
+    @Override
+    public String getId() {
+      return TargetSelectionMode.FIREBASE_DEVICE_MATRIX.name();
+    }
+
+    @NotNull
+    @Override
+    public String getDisplayName() {
+      throw new UnsupportedOperationException();
+    }
+
+    @NotNull
+    @Override
+    public DeployTargetState createState() {
+      return new State();
+    }
+
+    private static final class State extends DeployTargetState {
+    }
+
+    @NotNull
+    @Override
+    public DeployTargetConfigurable createConfigurable(@NotNull Project project,
+                                                       @NotNull Disposable parent,
+                                                       @NotNull DeployTargetConfigurableContext context) {
+      throw new UnsupportedOperationException();
+    }
+
+    @NotNull
+    @Override
+    public DeployTarget getDeployTarget() {
+      throw new UnsupportedOperationException();
+    }
   }
 
   @Test
@@ -59,23 +128,10 @@ public final class DeployTargetContextTest {
   @Test
   public void getCurrentDeployTargetProviderProviderIsNotFound() {
     DeployTargetContext context = new DeployTargetContext(() -> false, myProviders);
-    context.setTargetSelectionMode(TargetSelectionMode.FIREBASE_DEVICE_MATRIX);
+    context.setTargetSelectionMode("#2-Y2Y3Ob-h72ks%");
 
     Object provider = context.getCurrentDeployTargetProvider();
 
-    assertEquals(DeployTargetProvider.EP_NAME.findExtension(ShowChooserTargetProvider.class), provider);
-  }
-
-  @Test
-  public void getCurrentDeployTargetProviderDoesntReturnDeviceAndSnapshotComboBoxTargetProviderWhenFlagIsEnabled() {
-    // Arrange
-    DeployTargetContext context = new DeployTargetContext(() -> true, myProviders);
-    context.setTargetSelectionMode(TargetSelectionMode.SHOW_DIALOG);
-
-    // Act
-    Object provider = context.getCurrentDeployTargetProvider();
-
-    // Assert
     assertEquals(DeployTargetProvider.EP_NAME.findExtension(ShowChooserTargetProvider.class), provider);
   }
 }
