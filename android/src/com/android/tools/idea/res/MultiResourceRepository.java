76073c1481238e41c80688d1e442306594207116
Michal Bendowski <bendowski@google.com>
2019-03-05 11:15:08 +0000

    Fix a leak in DynamicResourceValueRepository
    The MessageBus connection lifecycle was bound to the module instead of
    the repository, so none of the disposed repositories could be reclaimed
    after being disposed, e.g. after refreshing the layout preview.
    
    I also noticed MultiResourceRepository was causing the dispose() method
    of child repositories to be called multiple times, so added a check for
    isDisposed().
    
    Test: Manually checked that only one instance of
      DynamicResourceValueRepository is strongly reachable after refreshing
      the preview a few times. Others may be reachable via
      Disposer.ourTree.myDisposedObjects, but I assume it's WAI.
    Fixes: 126959085
    Change-Id: If1a1b40275785a75e99272cfe49c15189102202c

--- android/src/com/android/tools/idea/res/MultiResourceRepository.java
+++ android/src/com/android/tools/idea/res/MultiResourceRepository.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016 The Android Open Source Project
+ * Copyright (C) 2019 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -366,7 +366,9 @@ public abstract class MultiResourceRepository extends LocalResourceRepository {
     synchronized (ITEM_MAP_LOCK) {
       for (LocalResourceRepository child : myLocalResources) {
         child.removeParent(this);
-        Disposer.dispose(child);
+        if (!Disposer.isDisposed(child)) {
+          Disposer.dispose(child);
+        }
       }
     }
   }
