6ba7b0c01c1a9701ae9f5ee5bd74842a0b45d09c
Juan C Nuno <juancnuno@google.com>
2019-03-06 15:41:58 -0800

    Fix resetEditorFrom and applyEditorTo
    It's wrong to assume that we would never have the deployment target options
    if the select device/snapshot combo box is visible. We would still need them
    for Android instrumented tests because of Firebase.
    
    Bug: Not applicable
    Test: AndroidRunConfigurationEditorTest
    Change-Id: I0a10426c01e2f7641f7895363ae7736c504dd9b3

---
+++ android/testSrc/com/android/tools/idea/run/editor/AndroidRunConfigurationEditorTest.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright (C) 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.run.editor;
+
+import static org.junit.Assert.assertEquals;
+
+import com.android.tools.idea.run.ConfigurationSpecificEditor;
+import com.android.tools.idea.run.TargetSelectionMode;
+import com.android.tools.idea.run.deployment.DeviceAndSnapshotComboBoxTargetProvider;
+import com.android.tools.idea.testartifacts.instrumented.AndroidTestRunConfiguration;
+import com.android.tools.idea.testing.AndroidProjectRule;
+import java.util.Arrays;
+import java.util.List;
+import javax.swing.JLabel;
+import org.junit.Rule;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+public final class AndroidRunConfigurationEditorTest {
+  @Rule
+  public final AndroidProjectRule myRule = AndroidProjectRule.inMemory();
+
+  @Test
+  public void applyEditorTo() {
+    // Arrange
+    AndroidDebuggerContext androidDebuggerContext = Mockito.mock(AndroidDebuggerContext.class);
+    DeployTargetProvider provider = new CloudTestMatrixTargetProvider();
+    List<DeployTargetProvider> providers = Arrays.asList(new DeviceAndSnapshotComboBoxTargetProvider(), provider);
+
+    AndroidTestRunConfiguration configuration1 = Mockito.mock(AndroidTestRunConfiguration.class);
+    Mockito.when(configuration1.getAndroidDebuggerContext()).thenReturn(androidDebuggerContext);
+    Mockito.when(configuration1.getApplicableDeployTargetProviders()).thenReturn(providers);
+    Mockito.when(configuration1.getProfilerState()).thenReturn(new ProfilerState());
+
+    AndroidRunConfigurationEditor<AndroidTestRunConfiguration> androidRunConfigurationEditor =
+      new AndroidRunConfigurationEditor<>(myRule.getProject(), null, configuration1);
+
+    @SuppressWarnings("unchecked")
+    ConfigurationSpecificEditor<AndroidTestRunConfiguration> configurationSpecificEditor = Mockito.mock(ConfigurationSpecificEditor.class);
+    Mockito.when(configurationSpecificEditor.getComponent()).thenReturn(new JLabel());
+
+    androidRunConfigurationEditor.setConfigurationSpecificEditor(configurationSpecificEditor);
+
+    DeployTargetContext deployTargetContext = new DeployTargetContext(() -> true, providers);
+
+    AndroidTestRunConfiguration configuration2 = Mockito.mock(AndroidTestRunConfiguration.class);
+    Mockito.when(configuration2.getDeployTargetContext()).thenReturn(deployTargetContext);
+    Mockito.when(configuration2.getProfilerState()).thenReturn(new ProfilerState());
+
+    // Act
+    androidRunConfigurationEditor.getDeploymentTargetOptions().getTargetComboBox().setSelectedItem(provider);
+    androidRunConfigurationEditor.applyEditorTo(configuration2);
+
+    // Assert
+    assertEquals(TargetSelectionMode.FIREBASE_DEVICE_MATRIX, deployTargetContext.getTargetSelectionMode());
+  }
+}
