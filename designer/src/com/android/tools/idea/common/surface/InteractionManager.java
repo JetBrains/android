58341330ee4af78048453717465f0d934f5cd903
Diego Perez <diegoperez@google.com>
2019-03-07 14:23:09 +0000

    createComponents must be called with write action
    Change-Id: I42360725cffe566bdf1aaa987fe76870336f6d53
    Fixes: 127726570
    Test: Manually verified

--- designer/src/com/android/tools/idea/common/surface/InteractionManager.java
+++ designer/src/com/android/tools/idea/common/surface/InteractionManager.java
@@ -43,6 +43,7 @@ import com.android.tools.idea.uibuilder.surface.MarqueeInteraction;
 import com.android.tools.idea.uibuilder.surface.NlDesignSurface;
 import com.google.common.collect.ImmutableList;
 import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.application.WriteAction;
 import com.intellij.openapi.util.Computable;
 import com.intellij.openapi.util.SystemInfo;
 import com.intellij.openapi.util.registry.Registry;
@@ -879,7 +880,7 @@ public class InteractionManager {
         components = mySurface.getSelectionModel().getSelection();
       }
       else {
-        components = model.createComponents(item, insertType, mySurface);
+        components = WriteAction.compute(() -> model.createComponents(item, insertType, mySurface));
 
         if (components.isEmpty()) {
           return null;  // User cancelled
