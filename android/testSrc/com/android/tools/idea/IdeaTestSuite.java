6a48a738f8d6819bc8b6b59fb8a8d087faf25d6a
Chris Warrington <cmw@google.com>
2019-03-07 16:42:08 +0000

    Add Sync project comparison test for AGP 3.3.2.
    Bug: 127702568
    Test: Adding coverage for syncing projects using AGP 3.3.2.
    Change-Id: I5b3859f0f928b913858d7e9ae40ecb2a455be6cb

--- android/testSrc/com/android/tools/idea/IdeaTestSuite.java
+++ android/testSrc/com/android/tools/idea/IdeaTestSuite.java
@@ -70,6 +70,7 @@ public class IdeaTestSuite extends IdeaTestSuiteBase {
     setUpOfflineRepo("tools/adt/idea/android/test_deps.zip", "prebuilts/tools/common/m2/repository");
     setUpOfflineRepo("tools/base/third_party/kotlin/kotlin-m2repository.zip", "prebuilts/tools/common/m2/repository");
     setUpOfflineRepo("tools/adt/idea/android/android-gradle-1.5.0_repo.zip", "prebuilts/tools/common/m2/repository");
+    setUpOfflineRepo("tools/base/build-system/previous-versions/3.3.2.zip", "prebuilts/tools/common/m2/repository");
     setUpOfflineRepo("tools/data-binding/data_binding_runtime.zip", "prebuilts/tools/common/m2/repository");
 
     // Enable Kotlin plugin (see PluginManagerCore.PROPERTY_PLUGIN_PATH).
