38e218fde7c272f85f0b44990b5c0975de2b664a
Peter Xu <peterx@google.com>
2019-03-07 14:54:45 -0800

    Track observable types and classes
    We are tracking three kinds of observables in this CL:
    1. observable primitives in the likes of ObservableInt, ObservableField, etc.
    2. observable collections. only 2 exist: ObservableArrayMap and ObservableArrayList.
    3. user defined classes that directly or indirectly (through inheritance) implement Observable.
    
    For #3, although there are utility functions that determine whether a PsiType is an inheritor of another type. AFAICT they only work for java and not for KtElement (Kotlin). Therefore, I opted to stick with the strategy that I used for #1 and #2, which is to maintain a hardcoded string "Observable" for comparisons.
    
    Test: TrackObservableTest
    Bug: 123029035
    Change-Id: I62a9d68ff018376ec0b916320a798a6e16ca3779

---
+++ databinding/testData/projects/projectForTracking/app/src/main/java/com/android/example/appwithdatabinding/SimpleObservable.java
@@ -0,0 +1,12 @@
+package com.android.example.appwithdatabinding;
+import androidx.databinding.Observable;
+
+class SimpleObservable implements Observable {
+  @Override
+  public void addOnPropertyChangedCallback(OnPropertyChangedCallback callback) {
+  }
+
+  @Override
+  public void removeOnPropertyChangedCallback(OnPropertyChangedCallback callback) {
+  }
+}
