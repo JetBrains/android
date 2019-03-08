892d24043d0f7dba3071fc0257ccbd5749210ad4
Chris Warrington <cmw@google.com>
2019-03-08 17:39:51 +0000

    Move AGP 2.3.2 and 2.2.0 definition
    To move towards unifying the various different ways we refer to Android
    Gradle Plugins from tests.
    
    Bug: N/A
    Test: Existing tools/adt/idea tests.
    Change-Id: I5a90723462fb379d6bb9da5d72493cde57ce040f

--- designer/testSrc/com/android/tools/idea/DesignerTestSuite.java
+++ designer/testSrc/com/android/tools/idea/DesignerTestSuite.java
@@ -38,7 +38,6 @@ public class DesignerTestSuite extends IdeaTestSuiteBase {
     symlinkToIdeaHome(
         "prebuilts/studio/layoutlib",
         "prebuilts/studio/sdk",
-        "prebuilts/tools/common/offline-m2",
         "tools/adt/idea/android/annotations",
         "tools/adt/idea/android/lib/androidWidgets",
         "tools/adt/idea/android/lib/sampleData",
@@ -50,6 +49,7 @@ public class DesignerTestSuite extends IdeaTestSuiteBase {
 
     setUpOfflineRepo("tools/base/build-system/studio_repo.zip", "out/studio/repo");
     setUpOfflineRepo("tools/adt/idea/android/test_deps.zip", "prebuilts/tools/common/m2/repository");
+    setUpOfflineRepo("tools/base/build-system/previous-versions/2.2.0.zip", "prebuilts/tools/common/m2/repository");
     setUpOfflineRepo("tools/base/third_party/kotlin/kotlin-m2repository.zip", "prebuilts/tools/common/m2/repository");
   }
 }
