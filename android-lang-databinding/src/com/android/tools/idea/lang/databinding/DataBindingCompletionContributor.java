d2b5592dd37855f4792400d17c79ac226d3cbb32
David Herman <davidherman@google.com>
2019-03-08 18:00:22 -0800

    Convert `DataBindingExprReferenceContributor` to Kotlin
    No logic changes - just adding comments and introducing
    Kotlin idioms to the class.
    
    However, a few other cosmetic changes:
    
    * Renamed `DefinitionReference` to `DbExprReference`, as that is
      a more explicit name expressing the classes purpose.
    
    * Converted `DbExprReference` to Kotlin, since this improved
      consistency with its child classes which, because they lived
      inside `DataBindingExprReferenceContributor`, also got converted.
    
    * Renamed `ResolvesToModelClass` to `ModelClassResolvable`, to
      make the class name more "nouny" and less "verby"
    
    * Moved `ModeClassResolvable` up a level, as it is referenced
      by multiple classes and shouldn't be hidden away inside
      `DataBindingExprReferenceContributor`.
    
    This CL paves the way for further work on this class, adding
    more tests and fixing an issue with reference inconsistency
    (as reported by new IntelliJ code)
    
    Bug: N/A
    Test: Covered by existing tests
    Change-Id: Idb8123a20062a3701ba1f1ed377f62d3a501d3ea

--- android-lang-databinding/src/com/android/tools/idea/lang/databinding/DataBindingCompletionContributor.java
+++ android-lang-databinding/src/com/android/tools/idea/lang/databinding/DataBindingCompletionContributor.java
@@ -29,7 +29,7 @@ import com.android.ide.common.resources.DataBindingResourceType;
 import com.android.tools.idea.databinding.BrUtil;
 import com.android.tools.idea.databinding.DataBindingUtil;
 import com.android.tools.idea.databinding.analytics.api.DataBindingTracker;
-import com.android.tools.idea.lang.databinding.DataBindingExprReferenceContributor.ResolvesToModelClass;
+import com.android.tools.idea.lang.databinding.model.ModelClassResolvable;
 import com.android.tools.idea.lang.databinding.model.PsiModelClass;
 import com.android.tools.idea.lang.databinding.model.PsiModelField;
 import com.android.tools.idea.lang.databinding.model.PsiModelMethod;
@@ -166,7 +166,7 @@ public class DataBindingCompletionContributor extends CompletionContributor {
     ImmutableList.Builder<LookupElement> resultBuilder = new ImmutableList.Builder<>();
     PsiReference[] childReferences = referenceExpression.getReferences();
     for (PsiReference reference : childReferences) {
-      ResolvesToModelClass ref = (ResolvesToModelClass)reference;
+      ModelClassResolvable ref = (ModelClassResolvable)reference;
       PsiModelClass resolvedType = ref.getResolvedType();
       if (resolvedType != null) {
         for (ModelField modelField : resolvedType.getDeclaredFields()) {
@@ -203,8 +203,8 @@ public class DataBindingCompletionContributor extends CompletionContributor {
     ImmutableList.Builder<LookupElement> resultBuilder = new ImmutableList.Builder<>();
     PsiReference[] childReferences = referenceExpression.getReferences();
     for (PsiReference reference : childReferences) {
-      if (reference instanceof ResolvesToModelClass) {
-        ResolvesToModelClass ref = (ResolvesToModelClass)reference;
+      if (reference instanceof ModelClassResolvable) {
+        ModelClassResolvable ref = (ModelClassResolvable)reference;
         PsiModelClass resolvedType = ref.getResolvedType();
         if (resolvedType == null) {
           continue;
