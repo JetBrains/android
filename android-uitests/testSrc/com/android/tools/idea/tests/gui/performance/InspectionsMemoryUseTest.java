28638cfd11fe506c3ffc357416224d86c41fd400
Nathan Paige <npaige@google.com>
2019-03-11 09:46:02 -0700

    Add memory leak test for inspections
    Runs inspections (Analyze > Inspect Code...) on the whole project, then
    closes the inspections tool window.
    
    The implementation of ToolWindowFixture.close() is somewhat unfortunate,
    but clicking on the X on the tabs via the UI is difficult since the X
    is an icon inside a label, not a separate component. Furthermore, to
    access this icon (to determine its location) would require either
    reflectively accessing or widening the accessibility of both the
    ContentTabLabel class and the myAdditionalIcons field therein.
    
    Bug: 124256751
    Bug: 127830884
    Change-Id: I4b03f65d46119d59c507f26dadd9ebd9b9981902

---
+++ android-uitests/testSrc/com/android/tools/idea/tests/gui/performance/InspectionsMemoryUseTest.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.tests.gui.performance;
+
+import com.android.tools.idea.tests.gui.framework.GuiTestRule;
+import com.android.tools.idea.tests.gui.framework.RunIn;
+import com.android.tools.idea.tests.gui.framework.TestGroup;
+import com.android.tools.idea.tests.gui.framework.fixture.InspectCodeDialogFixture;
+import com.android.tools.idea.tests.gui.framework.heapassertions.bleak.BleakKt;
+import com.android.tools.idea.tests.gui.framework.heapassertions.bleak.UseBleak;
+import com.intellij.testGuiFramework.framework.GuiTestRemoteRunner;
+import java.io.IOException;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * Checks for memory leaks from running inspections. Leaks in this area are potentially quite severe,
+ * as they can retain many PSI elements and inspection results.
+ */
+@RunWith(GuiTestRemoteRunner.class)
+@RunIn(TestGroup.PERFORMANCE)
+public class InspectionsMemoryUseTest {
+
+  @Rule public GuiTestRule guiTest = new GuiTestRule();
+
+  @Test
+  @UseBleak
+  public void inspectCode() throws IOException {
+    guiTest.importSimpleApplication();
+    BleakKt.runWithBleak(() -> guiTest.ideFrame()
+      .openFromMenu(InspectCodeDialogFixture::find, "Analyze", "Inspect Code...")
+      .clickOk()
+      .close()
+    );
+  }
+}
