d4b3a764b2cce60fb41f1b2d3442d4afd7bf50e3
Juan C Nuno <juancnuno@google.com>
2019-03-12 15:38:09 -0700

    Add TranslationsEditorGradleTest::removeLocale
    One of the recent merges broke com.android.tools.idea.tests.gui.editors
    .translations.TranslationsEditorTest::removeLocale. Convert it to a unit
    test for ease of debugging and ignore it.
    
    Bug: 127948531
    Test: TranslationsEditorGradleTest
    Change-Id: I294940f51bb037266174f8c1bd05e128dabe4b07

---
+++ android/testSrc/com/android/tools/idea/editors/strings/TranslationsEditorGradleTest.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright (C) 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.editors.strings;
+
+import static org.junit.Assert.assertEquals;
+
+import com.android.testutils.TestUtils;
+import com.android.tools.idea.io.TestFileUtils;
+import com.android.tools.idea.rendering.Locale;
+import com.android.tools.idea.testing.AndroidGradleProjectRule;
+import com.intellij.openapi.application.Application;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.module.Module;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import org.intellij.lang.annotations.Language;
+import org.jetbrains.android.facet.AndroidFacet;
+import org.junit.Ignore;
+import org.junit.Rule;
+import org.junit.Test;
+
+public final class TranslationsEditorGradleTest {
+  @Rule
+  public final AndroidGradleProjectRule myRule = new AndroidGradleProjectRule();
+
+  @Ignore("b/127948531")
+  @Test
+  public void removeLocale() throws IOException {
+    // Arrange
+    myRule.getFixture().setTestDataPath(TestUtils.getWorkspaceFile("tools/adt/idea/android/testData").getPath());
+    myRule.load("stringsEditor/MyApplication");
+
+    String projectBasePath = myRule.getProject().getBasePath();
+    Path mainRes = Paths.get(projectBasePath, "app", "src", "main", "res");
+
+    @Language("XML")
+    String mainContents = "<resources>\n" +
+                          "    <string name=\"key_1\">key_1_main_ab</string>\n" +
+                          "</resources>\n";
+
+    TestFileUtils.writeFileAndRefreshVfs(mainRes.resolve(Paths.get("values-ab", "strings.xml")), mainContents);
+
+    Path debugRes = Paths.get(projectBasePath, "app", "src", "debug", "res");
+
+    @Language("XML")
+    String debugContents = "<resources>\n" +
+                           "    <string name=\"key_1\">key_1_debug_ab</string>\n" +
+                           "</resources>\n";
+
+    TestFileUtils.writeFileAndRefreshVfs(debugRes.resolve(Paths.get("values-ab", "strings.xml")), debugContents);
+
+    Module module = myRule.getModules().getAppModule();
+    StringResourceViewPanel panel = new StringResourceEditor(StringsVirtualFile.getStringsVirtualFile(module)).getPanel();
+
+    // Act
+    Application application = ApplicationManager.getApplication();
+    Runnable loadResources = () -> Utils.loadResources(panel, Arrays.asList(mainRes, debugRes));
+
+    application.invokeAndWait(loadResources);
+    StringsWriteUtils.removeLocale(Locale.create("ab"), AndroidFacet.getInstance(module), this);
+    application.invokeAndWait(loadResources);
+
+    // Assert
+    assertEquals(0, panel.getTable().getRowCount());
+  }
+}
