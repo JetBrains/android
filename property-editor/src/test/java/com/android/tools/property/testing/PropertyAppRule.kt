2169b9a04efd83767f28b64064b11ecc2c01bd9a
Jens Ole Lauridsen <jlauridsen@google.com>
2019-03-13 17:25:03 -0700

    Fix ComboBoxPropertyEditorModelTest after merge.
    Split up ApplicationRule into a simple rule for the application
    instance alone, and a PropertyAppRule which installs a small
    set of services.
    In this CL also remove FakeActionManager and simply replace it with
    a Mockito mock.
    
    Fixes: 128037083
    Test: This is a fix of a test failure
    Change-Id: Idbe4d6d71026d9a2db158b665de905fe7d5115e4

---
+++ property-editor/src/test/java/com/android/tools/property/testing/PropertyAppRule.kt
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.property.testing
+
+import com.android.tools.adtui.workbench.PropertiesComponentMock
+import com.intellij.ide.ui.UISettings
+import com.intellij.ide.util.PropertiesComponent
+import com.intellij.openapi.actionSystem.ActionManager
+import com.intellij.testFramework.registerServiceInstance
+import org.mockito.Mockito.mock
+
+class PropertyAppRule : ApplicationRule() {
+
+  /**
+   * Setup a test Application instance with a few common services needed for property tests.
+   */
+  override fun before() {
+    super.before()
+    testApplication.registerServiceInstance(PropertiesComponent::class.java, PropertiesComponentMock())
+    testApplication.registerServiceInstance(ActionManager::class.java, mock(ActionManager::class.java))
+    testApplication.registerService(UISettings::class.java, UISettings())
+  }
+}
