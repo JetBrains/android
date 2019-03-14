66519fd668168f94a995beb5e2e636e7995c2e40
Ying Chen <chying@google.com>
2019-03-13 18:40:04 -0700

    Refresh library roots after library setup.
    Library roots are generated by Gradle, and they're not updated
    to Virtual File system unless file system is refreshed.
    This change forced refresh for the missed library roots as part
    of library setup.
    
    Besides, update ArtifactsByConfigurationModuleSetupStep so that
    all of setup steps use the common API in ModuleDependenciesSetup
    for external libraries. The name of local jar dependency now have
    prefix "Gradle: ", which is consistent with other types of modules.
    
    Bug: 126740908
    Bug: 126795724
    Bug: 128043536
    Test: included.
    Change-Id: Ic0d283b91d4affeb1ff0a00967f225e30b30fef8

--- android/src/com/android/tools/idea/gradle/project/sync/setup/module/common/ModuleDependenciesSetup.java
+++ android/src/com/android/tools/idea/gradle/project/sync/setup/module/common/ModuleDependenciesSetup.java
@@ -15,6 +15,9 @@
  */
 package com.android.tools.idea.gradle.project.sync.setup.module.common;
 
+import static com.android.tools.idea.io.FilePaths.pathToIdeaUrl;
+
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.externalSystem.service.project.IdeModifiableModelsProvider;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.roots.DependencyScope;
@@ -22,13 +25,14 @@ import com.intellij.openapi.roots.LibraryOrderEntry;
 import com.intellij.openapi.roots.OrderEntry;
 import com.intellij.openapi.roots.OrderRootType;
 import com.intellij.openapi.roots.libraries.Library;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.openapi.vfs.VirtualFileManager;
+import java.io.File;
 import org.jetbrains.annotations.NotNull;
 
-import java.io.File;
-
-import static com.android.tools.idea.io.FilePaths.pathToIdeaUrl;
-
 public abstract class ModuleDependenciesSetup {
+  private static final Logger LOG = Logger.getInstance(ModuleDependenciesSetup.class);
+
   protected void updateLibraryRootTypePaths(@NotNull Library library,
                                             @NotNull OrderRootType pathType,
                                             @NotNull IdeModifiableModelsProvider modelsProvider,
@@ -63,5 +67,22 @@ public abstract class ModuleDependenciesSetup {
     LibraryOrderEntry orderEntry = modelsProvider.getModifiableRootModel(module).addLibraryEntry(library);
     orderEntry.setScope(scope);
     orderEntry.setExported(exported);
+    // Make sure library roots are updated in virtual file system.
+    updateLibraryRootsInFileSystem(orderEntry);
+  }
+
+  private static void updateLibraryRootsInFileSystem(@NotNull LibraryOrderEntry orderEntry) {
+    VirtualFileManager manager = VirtualFileManager.getInstance();
+    for (OrderRootType type : OrderRootType.getAllTypes()) {
+      for (String url : orderEntry.getUrls(type)) {
+        VirtualFile file = manager.findFileByUrl(url);
+        if (file == null) {
+          file = manager.refreshAndFindFileByUrl(url);
+        }
+        if (file == null && LOG.isDebugEnabled()) {
+          LOG.debug(String.format("Can't find %s of the library '%s' at path '%s'", type, orderEntry.getLibraryName(), url));
+        }
+      }
+    }
   }
 }
