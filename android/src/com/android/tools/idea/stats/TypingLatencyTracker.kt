e500b23531d753a2c6d5b289f3557ccecbdbd3bc
Matthew Gharrity <gharrma@google.com>
2019-03-20 13:57:17 -0700

    Update TypingLatencyTracker to use new proto enum
    The 'TypingLatencyStats.FileType' enum in studio_stats.proto was
    renamed to 'EditorFileType', so this change updates the relevant code
    references.
    
    Bug: n/a
    Test: existing
    Change-Id: I7b1b4e530a10986408efd7e5f8cc3b8f2580cc00

--- android/src/com/android/tools/idea/stats/TypingLatencyTracker.kt
+++ android/src/com/android/tools/idea/stats/TypingLatencyTracker.kt
@@ -18,16 +18,17 @@ package com.android.tools.idea.stats
 import com.android.tools.analytics.UsageTracker
 import com.android.tools.idea.stats.TypingLatencyTracker.reportTypingLatency
 import com.google.wireless.android.sdk.stats.AndroidStudioEvent
+import com.google.wireless.android.sdk.stats.EditorFileType
+import com.google.wireless.android.sdk.stats.EditorFileType.GROOVY
+import com.google.wireless.android.sdk.stats.EditorFileType.JAVA
+import com.google.wireless.android.sdk.stats.EditorFileType.JSON
+import com.google.wireless.android.sdk.stats.EditorFileType.KOTLIN
+import com.google.wireless.android.sdk.stats.EditorFileType.KOTLIN_SCRIPT
+import com.google.wireless.android.sdk.stats.EditorFileType.NATIVE
+import com.google.wireless.android.sdk.stats.EditorFileType.PROPERTIES
+import com.google.wireless.android.sdk.stats.EditorFileType.UNKNOWN
+import com.google.wireless.android.sdk.stats.EditorFileType.XML
 import com.google.wireless.android.sdk.stats.TypingLatencyStats
-import com.google.wireless.android.sdk.stats.TypingLatencyStats.FileType.GROOVY
-import com.google.wireless.android.sdk.stats.TypingLatencyStats.FileType.JAVA
-import com.google.wireless.android.sdk.stats.TypingLatencyStats.FileType.JSON
-import com.google.wireless.android.sdk.stats.TypingLatencyStats.FileType.KOTLIN
-import com.google.wireless.android.sdk.stats.TypingLatencyStats.FileType.KOTLIN_SCRIPT
-import com.google.wireless.android.sdk.stats.TypingLatencyStats.FileType.NATIVE
-import com.google.wireless.android.sdk.stats.TypingLatencyStats.FileType.PROPERTIES
-import com.google.wireless.android.sdk.stats.TypingLatencyStats.FileType.UNKNOWN
-import com.google.wireless.android.sdk.stats.TypingLatencyStats.FileType.XML
 import com.intellij.ide.toProto
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.editor.actionSystem.LatencyListener
@@ -46,7 +47,7 @@ object TypingLatencyTracker : LatencyListener {
    * Maps file types to latency recorders.
    * We use [SingleWriterRecorder] to allow thread-safe read access from background threads.
    */
-  private val latencyRecorders = ConcurrentHashMap<TypingLatencyStats.FileType, SingleWriterRecorder>()
+  private val latencyRecorders = ConcurrentHashMap<EditorFileType, SingleWriterRecorder>()
 
   override fun recordTypingLatency(editor: Editor, action: String, latencyMs: Long) {
     // This runs on the EDT, but is thread-safe with respect to a background thread running reportTypingLatency().
@@ -91,7 +92,7 @@ object TypingLatencyTracker : LatencyListener {
   }
 
   /** Converts from file type name to proto enum value. */
-  private fun convertFileType(file: VirtualFile): TypingLatencyStats.FileType = when (file.fileType.name) {
+  private fun convertFileType(file: VirtualFile): EditorFileType = when (file.fileType.name) {
     // We use string literals here (rather than, e.g., JsonFileType.INSTANCE.name) to avoid unnecessary
     // dependencies on other plugins. Fortunately, these values are extremely unlikely to change.
     "JAVA" -> JAVA
