49d3449bf1bc9d3cead1e810df4d89e92e685b0f
Mike Moroney <mmoro@google.com>
2019-03-21 21:55:00 -0700

    Check component type when creating editors
    Added some general hardening to prevent
    exceptions in various editors that could
    be caused by manually editing the XML.
    
    Fixes: 129078688
    
    Test: NavPropertyEditorsTest.testCreate
    Change-Id: I0cedc390c0ec92720b6cd2faa22752f17678311e

--- naveditor/testSrc/com/android/tools/idea/naveditor/property/editors/NavPropertyEditorsTest.kt
+++ naveditor/testSrc/com/android/tools/idea/naveditor/property/editors/NavPropertyEditorsTest.kt
@@ -16,27 +16,39 @@
 package com.android.tools.idea.naveditor.property.editors
 
 import com.android.SdkConstants.ANDROID_URI
+import com.android.SdkConstants.ATTR_GRAPH
 import com.android.SdkConstants.ATTR_LABEL
+import com.android.SdkConstants.ATTR_LAYOUT
+import com.android.SdkConstants.ATTR_NAME
+import com.android.SdkConstants.ATTR_START_DESTINATION
 import com.android.tools.idea.common.model.NlComponent
 import com.android.tools.idea.common.model.NlModel
 import com.android.tools.idea.common.property.editors.NonEditableEditor
+import com.android.tools.idea.naveditor.NavModelBuilderUtil
+import com.android.tools.idea.naveditor.NavTestCase
 import com.android.tools.idea.naveditor.property.TYPE_EDITOR_PROPERTY_LABEL
 import com.android.tools.idea.naveditor.property.inspector.SimpleProperty
 import com.android.tools.idea.uibuilder.property.NlPropertyItem
-import org.jetbrains.android.AndroidTestCase
 import org.jetbrains.android.dom.navigation.NavigationSchema
+import org.jetbrains.android.dom.navigation.NavigationSchema.ATTR_DESTINATION
+import org.jetbrains.android.dom.navigation.NavigationSchema.ATTR_ENTER_ANIM
+import org.jetbrains.android.dom.navigation.NavigationSchema.ATTR_POP_UP_TO
 import org.mockito.Mockito.`when`
 import org.mockito.Mockito.mock
 
-class NavPropertyEditorsTest : AndroidTestCase() {
-
+class NavPropertyEditorsTest : NavTestCase() {
   fun testCreate() {
-    val model = mock(NlModel::class.java)
-    `when`(model.project).thenReturn(project)
-    val component = mock(NlComponent::class.java)
-    `when`(component.model).thenReturn(model)
+    val model = model("nav.xml") {
+      NavModelBuilderUtil.navigation("root") {
+        fragment("f1", label = "fragment1") {
+          action("action1", destination = "f1")
+        }
+      }
+    }
 
-    val navPropertyEditors = NavPropertyEditors.Factory.getInstance(project)
+    var component = model.find("action1")!!
+
+    val navPropertyEditors = NavPropertyEditors.getInstance(project)
     var editor = navPropertyEditors.create(SimpleProperty(TYPE_EDITOR_PROPERTY_LABEL, listOf(component)))
     assertInstanceOf(editor, NonEditableEditor::class.java)
 
@@ -47,6 +59,8 @@ class NavPropertyEditorsTest : AndroidTestCase() {
     `when`(propertyItem.name).thenReturn(ATTR_LABEL)
     `when`(propertyItem.namespace).thenReturn(ANDROID_URI)
     `when`(propertyItem.model).thenReturn(model)
+    `when`(propertyItem.components).thenReturn(listOf(component))
+
     editor = navPropertyEditors.create(propertyItem)
     assertInstanceOf(editor, TextEditor::class.java)
 
@@ -58,5 +72,37 @@ class NavPropertyEditorsTest : AndroidTestCase() {
     // Try something else just to make sure it doesn't blow up
     editor = navPropertyEditors.create(SimpleProperty("foo", listOf(component)))
     assertNotNull(editor)
+
+  }
+
+  fun testInvalid() {
+    val model = model("nav.xml") {
+      NavModelBuilderUtil.navigation("root")
+    }
+
+    testInvalid(model, "fragment", ATTR_ENTER_ANIM)
+    testInvalid(model, "fragment", ATTR_GRAPH)
+    testInvalid(model, "fragment", ATTR_DESTINATION)
+    testInvalid(model, "fragment", ATTR_START_DESTINATION)
+    testInvalid(model, "fragment", ATTR_POP_UP_TO)
+    testInvalid(model, "action", ATTR_LAYOUT)
+    testInvalid(model, "action", ATTR_NAME)
+  }
+
+  private fun testInvalid(model: NlModel, tag: String, name: String) {
+    val component = mock(NlComponent::class.java)
+    `when`(component.model).thenReturn(model)
+    `when`(component.tagName).thenReturn(tag)
+
+    val propertyItem = mock(NlPropertyItem::class.java)
+    `when`(propertyItem.name).thenReturn(name)
+    `when`(propertyItem.namespace).thenReturn(ANDROID_URI)
+    `when`(propertyItem.model).thenReturn(model)
+    `when`(propertyItem.components).thenReturn(listOf(component))
+
+    val navPropertyEditors = NavPropertyEditors.getInstance(project)
+
+    val editor = navPropertyEditors.create(propertyItem)
+    assertInstanceOf(editor, TextEditor::class.java)
   }
 }
