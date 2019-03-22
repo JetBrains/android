04bbccb384a0d0528df83576ebaec51ce629ab07
Peter Xu <peterx@google.com>
2019-03-22 09:35:55 -0700

    Track number of binding adapters
    Test: TrackBindingAdaptersTest
    Bug: 123029035
    Change-Id: I92e4a9100f706ffedfc5ab030d40ba869f4dee3c

--- databinding/testData/projects/projectForTracking/app/src/main/java/com/android/example/appwithdatabinding/MyAdapter.java
+++ databinding/testData/projects/projectForTracking/app/src/main/java/com/android/example/appwithdatabinding/MyAdapter.java
@@ -1,5 +1,11 @@
 package com.android.example.appwithdatabinding;
+
 import androidx.databinding.BindingAdapter;
+import androidx.databinding.InverseBindingMethod;
+import androidx.databinding.InverseBindingMethods;
+
+@InverseBindingMethods({@InverseBindingMethod(type = android.widget.TextView.class, attribute = "android:text",
+  event = "android:textAttrChanged", method = "getText")})
 public class MyAdapter {
   @BindingAdapter("foo")
   public void bindFoo(android.view.View view, String foo) {
