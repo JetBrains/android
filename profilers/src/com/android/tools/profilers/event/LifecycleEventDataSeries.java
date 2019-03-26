27609b15b70d446b2a7ef5d3445894d526b041d1
Billy Lam <shiufai@google.com>
2019-03-26 12:12:36 -0700

    Migrate Event Profiler to use new transport data
    Bug: 128992521
    Test: Added transport pipeline tests for new data
    Change-Id: I3e5a86d7312dd0dfdf52f000b528e3b82975770a

--- profilers/src/com/android/tools/profilers/event/LifecycleEventDataSeries.java
+++ profilers/src/com/android/tools/profilers/event/LifecycleEventDataSeries.java
@@ -25,35 +25,140 @@ import com.android.tools.profiler.proto.Common;
 import com.android.tools.profiler.proto.EventProfiler;
 import com.android.tools.profiler.proto.EventServiceGrpc;
 import com.android.tools.profiler.proto.Interaction;
-import com.android.tools.profilers.ProfilerClient;
+import com.android.tools.profiler.proto.Transport;
+import com.android.tools.profilers.StudioProfilers;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 import org.jetbrains.annotations.NotNull;
 
 public class LifecycleEventDataSeries implements DataSeries<EventAction<LifecycleEvent>> {
 
-  @NotNull private ProfilerClient myClient;
+  @NotNull private StudioProfilers myProfilers;
   @NotNull private final Common.Session mySession;
   private final boolean myFragmentsOnly;
 
-  public LifecycleEventDataSeries(@NotNull ProfilerClient client, @NotNull Common.Session session, boolean fragmentOnly) {
-    myClient = client;
-    mySession = session;
+  public LifecycleEventDataSeries(@NotNull StudioProfilers profilers, boolean fragmentOnly) {
+    myProfilers = profilers;
+    mySession = profilers.getSession();
     myFragmentsOnly = fragmentOnly;
   }
 
   @Override
   public List<SeriesData<EventAction<LifecycleEvent>>> getDataForXRange(@NotNull Range timeCurrentRangeUs) {
+    if (myProfilers.getIdeServices().getFeatureConfig().isUnifiedPipelineEnabled()) {
+      return getTransportData(timeCurrentRangeUs);
+    }
+    else {
+      return getLegacyData(timeCurrentRangeUs);
+    }
+  }
+
+  @NotNull
+  private List<SeriesData<EventAction<LifecycleEvent>>> getTransportData(@NotNull Range rangeUs) {
+    List<SeriesData<EventAction<LifecycleEvent>>> series = new ArrayList<>();
+    Transport.GetEventGroupsRequest request = Transport.GetEventGroupsRequest.newBuilder()
+      .setKind(Common.Event.Kind.VIEW)
+      .setStreamId(mySession.getStreamId())
+      .setPid(mySession.getPid())
+      .setFromTimestamp(TimeUnit.MICROSECONDS.toNanos((long)rangeUs.getMin()))
+      .setToTimestamp(TimeUnit.MICROSECONDS.toNanos((long)rangeUs.getMax()))
+      .build();
+    Transport.GetEventGroupsResponse response = myProfilers.getClient().getTransportClient().getEventGroups(request);
+    for (Transport.EventGroup group : response.getGroupsList()) {
+      boolean isFragment = group.getEvents(0).getView().getParentActivityId() != 0;
+      // If we are listening to only fragments filter non-fragment types.
+      // If we only want non-fragment types then filter fragments.
+      if (myFragmentsOnly != isFragment) {
+        continue;
+      }
+
+      long actionStart = 0;
+      long actionEnd = 0;
+      boolean hasEndEvent = false;
+      LifecycleEvent lifecycleEvent = LifecycleEvent.NONE;
+      for (int i = 0; i < group.getEventsCount(); i++) {
+        Common.Event event = group.getEvents(i);
+        Interaction.ViewData data = event.getView();
+        String displayString = data.getName();
+
+        // Match start states with end states.
+        switch (data.getState()) {
+          case ADDED:
+          case RESUMED:
+            lifecycleEvent = LifecycleEvent.STARTED;
+            actionStart = TimeUnit.NANOSECONDS.toMicros(event.getTimestamp());
+            break;
+          case DESTROYED:
+            // This case is a fallthrough to set the lifecycle, and end time used in creating the proper UI event.
+
+            // In the UI we track the end of an activity when the activity gets paused.
+            // We also listen to the destroyed event here in case the app stops and we force a destroyed event.
+            // If we only listen for a destroyed event, the timeline may jump, or never get called depending on
+            // how the application is handling references.
+            // If we get a destroyed event out of order, and it is the last event we encounter then we know
+            // the activity was unexpectedly terminated.
+            if (i != group.getEventsCount() - 1) {
+              break;
+            }
+            displayString += String.format(" - %s", data.getState().toString().toLowerCase());
+            // Falls-through to REMOVED
+          case REMOVED:
+            // Remove is also a fallthrough as this is the event that gets set when we terminate a fragment.
+          case PAUSED:
+            lifecycleEvent = LifecycleEvent.COMPLETED;
+            actionEnd = TimeUnit.NANOSECONDS.toMicros(event.getTimestamp());
+            hasEndEvent = true;
+            break;
+          default:
+            break;
+        }
+
+        // Add all the states of the activity to the name until we have reached the end or the next start event.
+        if (hasEndEvent) {
+          while (++i < group.getEventsCount()) {
+            Common.Event nextEvent = group.getEvents(i);
+            if (nextEvent.getIsEnded()) {
+              displayString += String.format(" - %s", nextEvent.getView().getState().toString().toLowerCase());
+            }
+            else {
+              i--;
+              break;
+            }
+          }
+        }
+
+        // We create a UI event each time we have an end event, or if there is a start event and we have reached the end of the group.
+        if (hasEndEvent || (i == group.getEventsCount() - 1 && actionStart != 0)) {
+          series.add(new SeriesData<>(actionStart,
+                                      new LifecycleAction(actionStart, actionEnd, lifecycleEvent, displayString, event.getGroupId())));
+
+          // Reset the states for the next set of states that make up a SeriesData.
+          actionStart = 0;
+          actionEnd = 0;
+          hasEndEvent = false;
+          lifecycleEvent = LifecycleEvent.NONE;
+        }
+      }
+    }
+
+    Collections.sort(series, Comparator.comparingLong(data -> data.x));
+    return series;
+  }
+
+  @NotNull
+  private List<SeriesData<EventAction<LifecycleEvent>>> getLegacyData(@NotNull Range rangeUs) {
     List<SeriesData<EventAction<LifecycleEvent>>> seriesData = new ArrayList<>();
-    EventServiceGrpc.EventServiceBlockingStub eventService = myClient.getEventClient();
+    EventServiceGrpc.EventServiceBlockingStub eventService = myProfilers.getClient().getEventClient();
 
     // TODO: update getComponentData to accept a fragment filter. There isn't a significant amount of data here,
     // so for the first iteration performance is not a concern.
     EventProfiler.EventDataRequest.Builder dataRequestBuilder = EventProfiler.EventDataRequest.newBuilder()
       .setSession(mySession)
-      .setStartTimestamp(TimeUnit.MICROSECONDS.toNanos((long)timeCurrentRangeUs.getMin()))
-      .setEndTimestamp(TimeUnit.MICROSECONDS.toNanos((long)timeCurrentRangeUs.getMax()));
+      .setStartTimestamp(TimeUnit.MICROSECONDS.toNanos((long)rangeUs.getMin()))
+      .setEndTimestamp(TimeUnit.MICROSECONDS.toNanos((long)rangeUs.getMax()));
     EventProfiler.ActivityDataResponse response = eventService.getActivityData(dataRequestBuilder.build());
     for (EventProfiler.ActivityData data : response.getDataList()) {
       long actionStart = 0;
