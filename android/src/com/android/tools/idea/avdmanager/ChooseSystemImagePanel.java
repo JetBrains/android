ec092e5c849f7166a36be4aac7630259fc77f7dd
Roberto Perez <robertoalexis@google.com>
2019-03-27 17:02:57 -0700

    Adding generic automotive skin
    Bug: 129411293
    Test: DeviceArtDescriptorTest
    Change-Id: Iacd81cba139470ac708902223636bba4f6784a29

--- android/src/com/android/tools/idea/avdmanager/ChooseSystemImagePanel.java
+++ android/src/com/android/tools/idea/avdmanager/ChooseSystemImagePanel.java
@@ -182,7 +182,7 @@ public class ChooseSystemImagePanel extends JPanel
       // here (which will filter out system images with a non-default tag, such as the Google API
       // system images (see issue #78947), we instead deliberately skip the other form factor images
       return !imageTag.equals(SystemImage.TV_TAG) && !imageTag.equals(SystemImage.WEAR_TAG) &&
-          !imageTag.equals(SystemImage.CHROMEOS_TAG);
+          !imageTag.equals(SystemImage.CHROMEOS_TAG) && !imageTag.equals(SystemImage.AUTOMOTIVE_TAG);
     }
     return deviceTagId.equals(imageTag.getId());
   }
