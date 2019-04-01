6e55c9e3585d99bcb426309f81fc26238a9cd9a4
Michal Bendowski <bendowski@google.com>
2019-04-01 11:31:40 +0100

    Support AndroidX names in PackageClassConverter
    - Switched the converter to using its own @Options annotation instead of
    reusing @ExtendClass from IntelliJ. This way we can use more than one
    super class and don't need separate annotations for every option.
    - Added AndroidX names where necessary, e.g. ActionProvider, Fragment.
    - Changed MetadataValueConverter to be a WrappingConverter, which is
    recognized by the DOM stack, instead of forwarding all methods.
    - In MenuItem, made framework and support versions of actionViewClass
    behave the same, i.e. offer views from AARs.
    
    Bug: 80457341
    Test: existing, AndroidLayoutDomTest, AndroidMenuTest
    Change-Id: I69326e32084add6a375f92778fcaedb884b60bc0

--- android/src/org/jetbrains/android/dom/converters/MetadataValueConverter.java
+++ android/src/org/jetbrains/android/dom/converters/MetadataValueConverter.java
@@ -15,83 +15,40 @@
  */
 package org.jetbrains.android.dom.converters;
 
+import com.android.SdkConstants;
 import com.android.xml.AndroidManifest;
-import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiReference;
 import com.intellij.psi.xml.XmlTag;
-import com.intellij.util.xml.ConvertContext;
-import com.intellij.util.xml.CustomReferenceConverter;
+import com.intellij.util.xml.Converter;
 import com.intellij.util.xml.GenericDomValue;
-import com.intellij.util.xml.ResolvingConverter;
-import org.jetbrains.annotations.NonNls;
+import com.intellij.util.xml.WrappingConverter;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-
 /**
- * A {@link com.intellij.util.xml.Converter} that references the right element based on the meta-data android:name attribute value.
+ * A {@link Converter} that references the right element based on the meta-data android:name attribute value.
  *
- * <p>
- * When the android:name is "android.support.PARENT_ACTIVITY" the value is handled as a package class. The default is handling the
+ * <p>When the android:name is "android.support.PARENT_ACTIVITY" the value is handled as an activity class. The default is handling the
  * android:value of meta-data as a ResourceValue.
  */
-public class MetadataValueConverter extends ResolvingConverter<Object> implements CustomReferenceConverter<Object> {
-  private ResourceReferenceConverter myResourceReferenceConverter = new ResourceReferenceConverter();
-  private PackageClassConverter myPsiClassConverter = new PackageClassConverter();
+public class MetadataValueConverter extends WrappingConverter {
+  private ResourceReferenceConverter myResourceReferenceConverter;
+  private PackageClassConverter myActivityClassConverter;
 
   public MetadataValueConverter() {
     // Expanded suggestions list might be too long and take a while to load.
+    myResourceReferenceConverter = new ResourceReferenceConverter();
     myResourceReferenceConverter.setExpandedCompletionSuggestion(false);
-  }
-
-  private static boolean isClassContext(ConvertContext context) {
-    XmlTag xmlTag = context.getTag();
-    return (xmlTag != null && AndroidManifest.VALUE_PARENT_ACTIVITY.equals(xmlTag.getAttributeValue("android:name")));
-  }
-
-  @Nullable
-  @Override
-  public String toString(@Nullable Object object, ConvertContext context) {
-    if (object == null) {
-      return null;
-    }
-
-    return object.toString();
-  }
-
-  @NotNull
-  @Override
-  public Collection<Object> getVariants(ConvertContext context) {
-    if (isClassContext(context)) {
-      return Collections.emptyList();
-    }
-
-    ArrayList<Object> results = new ArrayList<>();
-    results.addAll(myResourceReferenceConverter.getVariants(context));
-
-    return results;
+    myActivityClassConverter = new PackageClassConverter.Builder()
+      .withExtendClassNames(SdkConstants.CLASS_ACTIVITY)
+      .build();
   }
 
   @Nullable
   @Override
-  public Object fromString(@Nullable @NonNls String s, ConvertContext context) {
-    if (isClassContext(context)) {
-      return myPsiClassConverter.fromString(s, context);
-    }
-
-    return myResourceReferenceConverter.fromString(s, context);
-  }
-
-  @NotNull
-  @Override
-  public PsiReference[] createReferences(GenericDomValue value, PsiElement element, ConvertContext context) {
-    if (isClassContext(context)) {
-      return myPsiClassConverter.createReferences(value, element, context);
-    }
-
-    return myResourceReferenceConverter.createReferences(value, element, context);
+  public Converter getConverter(@NotNull GenericDomValue domElement) {
+    XmlTag xmlTag = domElement.getXmlTag();
+    boolean isActivityClassName =
+      xmlTag != null && AndroidManifest.VALUE_PARENT_ACTIVITY.equals(xmlTag.getAttributeValue("name", SdkConstants.ANDROID_URI));
+    return isActivityClassName ? myActivityClassConverter : myResourceReferenceConverter;
   }
 }
