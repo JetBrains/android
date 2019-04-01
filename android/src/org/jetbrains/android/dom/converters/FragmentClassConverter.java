3a1b9e8c3dc4ba96f06146e7af1caa91b600c1cd
Michal Bendowski <bendowski@google.com>
2019-04-01 11:31:40 +0100

    Support AndroidX names in PackageClassConverter (attempt 2)
    (First attempt was reverted in Change-Id: I77c80bf7ff01f51b41c848d8453d5b3bd9520e64)
    
    - Switched the converter to using its own @Options annotation instead of
    reusing @ExtendClass from IntelliJ. This way we can use more than one
    super class and don't need separate annotations for every option.
    - Added AndroidX names where necessary, e.g. ActionProvider, Fragment.
    - Changed MetadataValueConverter to be a WrappingConverter, which is
    recognized by the DOM stack, instead of forwarding all methods.
    - In MenuItem, made framework and support versions of actionViewClass
    behave the same, i.e. offer views from AARs.
    
    Bug: 80457341
    Test: existing, AndroidLayoutDomTest, AndroidMenuTest
    Change-Id: I5e4476bb70c12d8245034ce989e98f7db4301833

--- android/src/org/jetbrains/android/dom/converters/FragmentClassConverter.java
+++ android/src/org/jetbrains/android/dom/converters/FragmentClassConverter.java
@@ -1,10 +1,13 @@
 package org.jetbrains.android.dom.converters;
 
+import static com.android.SdkConstants.CLASS_FRAGMENT;
+import static com.android.SdkConstants.CLASS_V4_FRAGMENT;
+
 /**
  * @author Eugene.Kudelevsky
  */
 public class FragmentClassConverter extends PackageClassConverter {
   public FragmentClassConverter() {
-    super("android.app.Fragment", "android.support.v4.app.Fragment");
+    super(CLASS_FRAGMENT, CLASS_V4_FRAGMENT.oldName(), CLASS_V4_FRAGMENT.newName());
   }
 }
