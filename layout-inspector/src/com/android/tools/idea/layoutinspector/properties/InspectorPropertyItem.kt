011fadd5a8237676c0e81a820b614d93c2a33328
Jens Ole Lauridsen <jlauridsen@google.com>
2019-04-02 17:07:55 -0700

    Read color constants from agent.
    Read color constants from agent and display the color in a color
    icon in front of the color value.
    
    Bug: 123643052
    Test: N/A
    Change-Id: I5b48ab5a7119257bf9443d14a212c22d06b5a96a

--- layout-inspector/src/com/android/tools/idea/layoutinspector/properties/InspectorPropertyItem.kt
+++ layout-inspector/src/com/android/tools/idea/layoutinspector/properties/InspectorPropertyItem.kt
@@ -16,8 +16,18 @@
 package com.android.tools.idea.layoutinspector.properties
 
 import com.android.tools.idea.layoutinspector.model.InspectorView
+import com.android.tools.idea.res.RESOURCE_ICON_SIZE
+import com.android.tools.idea.res.parseColor
+import com.android.tools.layoutinspector.proto.LayoutInspectorProto.Property.Type
+import com.android.tools.property.panel.api.ActionIconButton
 import com.android.tools.property.panel.api.PropertyItem
 import com.android.utils.HashCodes
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.util.ui.ColorIcon
+import com.intellij.util.ui.JBUI
+import icons.StudioIcons
+import java.awt.Color
+import javax.swing.Icon
 
 /**
  * A [PropertyItem] in the inspector with a snapshot of the value.
@@ -29,6 +39,9 @@ data class InspectorPropertyItem(
 
   /** The name of the attribute */
   override val name: String,
+
+  /** The type of the attribute */
+  val type: Type,
 
   /** The value of the attribute when the snapshot was taken */
   override var value: String?,
@@ -51,4 +64,20 @@ data class InspectorPropertyItem(
 
   override fun equals(other: Any?): Boolean =
     other is InspectorPropertyItem && namespace == other.namespace && name == other.name
+
+  override val colorButton = createColorButton()
+
+  private fun createColorButton(): ActionIconButton? {
+    if (type != Type.COLOR || value.isNullOrEmpty()) {
+      return null
+    }
+    val color = parseColor(value) ?: return null
+    return ColorActionIconButton(color)
+  }
+
+  private class ColorActionIconButton(color: Color): ActionIconButton {
+    override val actionButtonFocusable = false
+    override val action: AnAction? = null
+    override val actionIcon: Icon = JBUI.scale(ColorIcon(RESOURCE_ICON_SIZE, color, false))
+  }
 }
