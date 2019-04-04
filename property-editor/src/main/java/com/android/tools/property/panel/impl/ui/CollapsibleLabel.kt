9f33dfb5b3923347edf40aaa6361d74f43db7680
Jens Ole Lauridsen <jlauridsen@google.com>
2019-04-04 11:58:07 -0700

    Add keyboard access for opening titles in properties panel.
    Before this change a CollapsibleLabel was simple a JBLabel where
    the icon in the label was used for expand/collapse icons. That
    icon is not focusable. In addition the action buttons were
    "slabbed" on externally.
    
    Fix this by making the CollapsibleLabel a JPanel with a focusable
    icon, a label, and some optional action buttons.
    
    After this fix the user will be able to <TAB> to a section title
    and expand the section with <space> and <enter>.
    The action buttons on "Declared Attributes" is accessible though
    the keyboard as well. Allthough - is not that usefull from the
    keyboard.
    
    Fixes: 129868837
    Test: TODO:129977625
    Change-Id: Ie06dd327b0faccf23cf75c9e494d7d61f1c76e43

--- property-editor/src/main/java/com/android/tools/property/panel/impl/ui/CollapsibleLabel.kt
+++ property-editor/src/main/java/com/android/tools/property/panel/impl/ui/CollapsibleLabel.kt
@@ -15,15 +15,22 @@
  */
 package com.android.tools.property.panel.impl.ui
 
+import com.android.tools.adtui.common.secondaryPanelBackground
 import com.android.tools.adtui.model.stdui.ValueChangedListener
 import com.android.tools.property.panel.impl.model.CollapsibleLabelModel
 import com.google.common.html.HtmlEscapers
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.ui.ExpandableItemsHandler
 import com.intellij.ui.components.JBLabel
+import com.intellij.util.ui.JBUI
 import com.intellij.util.ui.UIUtil
-import java.awt.event.MouseAdapter
-import java.awt.event.MouseEvent
-import javax.swing.JComponent
+import java.awt.BorderLayout
+import java.awt.FlowLayout
+import java.awt.Font
+import javax.swing.Icon
+import javax.swing.JPanel
+import javax.swing.border.Border
 
 /**
  * A label to show text with or without trailing ellipsis and an optional expansion control.
@@ -34,54 +41,68 @@ import javax.swing.JComponent
  *
  * An expansion control is optionally shown and expansion logic is included.
  */
-class CollapsibleLabel(val model: CollapsibleLabelModel) : JBLabel(model.name) {
-  // A JBLabel wil automatically display ellipsis at the end of a string that is too long for the width
-  private val valueWithTrailingEllipsis = model.name
-  // In html the value will not have ellipsis at the end but simply cut off when the string is too long
-  private val valueWithoutEllipsis = toHtml(model.name)
-  private var listenerInstalled = false
+class CollapsibleLabel(
+  val model: CollapsibleLabelModel,
+  font: Font = UIUtil.getLabelFont(UIUtil.FontSize.SMALL),
+  vararg actions: AnAction
+) : JPanel(BorderLayout()) {
+  private val label = MyLabel(model.name)
 
-  private val mouseListener = object : MouseAdapter() {
-    override fun mouseClicked(event: MouseEvent) {
+  // The label wil automatically display ellipsis at the end of a string that is too long for the width
+  private val valueWithTrailingEllipsis = model.name
+  // As html the value will not have ellipsis at the end but simply cut off when the string is too long
+  private val valueWithoutEllipsis = toHtml(model.name)
+
+  private val expandAction = object : AnAction() {
+    override fun actionPerformed(event: AnActionEvent) {
       model.expanded = !model.expanded
     }
   }
 
-  var actionPanel: JComponent? = null
+  private val button = IconWithFocusBorder { if (model.expandable) expandAction else null }
+
+  val text: String?
+    get() = label.text
+
+  val icon: Icon?
+    get() = button.icon
+
+  var innerBorder: Border?
+    get() = label.border
+    set(value) { label.border = value}
 
   init {
+    background = secondaryPanelBackground
+    button.border = JBUI.Borders.emptyRight(2)
+    add(button, BorderLayout.WEST)
+    add(label, BorderLayout.CENTER)
     model.addValueChangedListener(ValueChangedListener { valueChanged() })
-    font = UIUtil.getLabelFont(UIUtil.FontSize.SMALL)
+    label.font = font
+    if (actions.isNotEmpty()) {
+      val buttons = JPanel(FlowLayout(FlowLayout.CENTER, JBUI.scale(2), 0))
+      actions.forEach { buttons.add(FocusableActionButton(it)) }
+      add(buttons, BorderLayout.EAST)
+    }
     valueChanged()
   }
 
   private fun valueChanged() {
     val revalidateParent = if (isVisible != model.visible) parent else null
     isVisible = model.visible
-    actionPanel?.isVisible = isVisible
-    text = if (model.showEllipses) valueWithTrailingEllipsis else valueWithoutEllipsis
-    icon = model.icon
-    installMouseListener(model.expandable)
+    label.isVisible = isVisible
+    label.text = if (model.showEllipses) valueWithTrailingEllipsis else valueWithoutEllipsis
+    button.icon = model.icon
     revalidateParent?.revalidate()
     revalidateParent?.repaint()
   }
 
-  override fun contains(x: Int, y: Int): Boolean {
-    return isVisible && super.contains(x, y)
-  }
-
-  private fun installMouseListener(expandable: Boolean) {
-    if (expandable && !listenerInstalled) {
-      addMouseListener(mouseListener)
-      listenerInstalled = true
-    }
-    else if (!expandable && listenerInstalled) {
-      removeMouseListener(mouseListener)
-      listenerInstalled = false
-    }
-  }
-
   private fun toHtml(text: String): String {
     return "<html>" + HtmlEscapers.htmlEscaper().escape(text) + "</html>"
+  }
+
+  private class MyLabel(label: String): JBLabel(label) {
+    override fun contains(x: Int, y: Int): Boolean {
+      return isVisible && super.contains(x, y)
+    }
   }
 }
