05c667720fe24607170694e33fa7fc80508e4e1a
Kainan Wang <knwang@google.com>
2019-04-04 12:49:21 -0700

    Fork projectWithDataBindingAndroidX and projectWithDataBindingSupport
    ... into lang-databinding so we can run gradle tests when there
    are differences between AndroidX and Support Library.
    
    Bug: 117607324
    Test: None
    Change-Id: Iab2b467bca42d2aaddb1c5cbd78faf6ffce4ea8c

---
+++ android-lang-databinding/testData/projects/projectWithDataBindingSupport/app/src/main/java/com/android/example/appwithdatabinding/MyAdapter.java
@@ -0,0 +1,18 @@
+package com.android.example.appwithdatabinding;
+import android.databinding.BindingAdapter;
+public class MyAdapter {
+  @BindingAdapter("foo")
+  public void bindFoo(android.view.View view, String foo) {
+
+  }
+
+  @BindingAdapter("my_binding_attribute")
+  public void bindTestSetter(android.view.View view, String foo) {
+
+  }
+
+  @BindingAdapter("padding")
+  public void bindPadding(android.view.View view, String foo) {
+
+  }
+}
