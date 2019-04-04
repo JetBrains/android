b29e5691b4a294b7692e3f06a074f3fd8336efeb
Kainan Wang <knwang@google.com>
2019-04-04 12:49:21 -0700

    Fork projectWithDataBindingAndroidX and projectWithDataBindingSupport
    ... into lang-databinding so we can run gradle tests when there
    are differences between AndroidX and Support Library.
    
    Bug: 117607324
    Test: None
    Change-Id: Iab2b467bca42d2aaddb1c5cbd78faf6ffce4ea8c

---
+++ android-lang-databinding/testData/projects/projectWithDataBindingAndroidX/app/src/main/java/com/android/example/appwithdatabinding/User.java
@@ -0,0 +1,29 @@
+package com.android.example.appwithdatabinding;
+
+import androidx.databinding.BaseObservable;
+import androidx.databinding.Bindable;
+
+class User extends BaseObservable {
+  private String firstName;
+  private String lastName;
+
+  @Bindable
+  public String getFirstName() {
+    return this.firstName;
+  }
+
+  @Bindable
+  public String getLastName() {
+    return this.lastName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+    notifyPropertyChanged(BR.firstName);
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+    notifyPropertyChanged(BR.lastName);
+  }
+}
---
+++ android-lang-databinding/testData/projects/projectWithDataBindingSupport/app/src/main/java/com/android/example/appwithdatabinding/DummyVo.java
@@ -0,0 +1,9 @@
+package com.android.example.appwithdatabinding;
+
+public class DummyVo {
+    public String name;
+
+    public String initialString() {
+        return "test";
+    }
+}
