d6d799e8d34f20e2b084ce46744b118aea6db51e
Matthew Gharrity <gharrma@google.com>
2019-04-08 13:49:55 -0700

    Fix AndroidExtractionTest for Kotlin 1.3.30
    AndroidExtractionTestGenerated still has some dependencies on Kotlin plugin
    test code, and it starts to fail when upgrading to Kotlin 1.3.30 due to binary
    incompatibilities between 1.3.30 and the current version of the Kotlin test jars
    in //prebuilts/tools/common/android-kotlin.
    
    There are generally two options when this happens: (1) upgrade the
    Kotlin test jars in tandem with the Kotlin plugin (by doing a custom
    build and copying the test jars into prebuilts), or (2) try to reduce
    the dependency on the Kotlin test code so that the test passes. This
    CL chooses option #2, because that route also helps pay down the
    techical debt of depending on Kotlin test code. In this case, option #2
    meant copying over some test code and modifying it to be compatible
    with 1.3.30.
    
    Test: existing
    Bug: n/a
    Change-Id: I422323107ca9eb9e3ba2b5c1bc60bd4749f6dd30
    (cherry picked from commit 65784f73c9bc8fae0061f413aebc8e80c601c438)

--- android-kotlin/android-extensions-idea/tests/org/jetbrains/kotlin/android/AbstractAndroidExtractionTest.kt
+++ android-kotlin/android-extensions-idea/tests/org/jetbrains/kotlin/android/AbstractAndroidExtractionTest.kt
@@ -16,12 +16,30 @@
 
 package org.jetbrains.kotlin.android
 
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.project.Project
+import com.intellij.testFramework.fixtures.CodeInsightTestFixture
 import org.jetbrains.kotlin.idea.refactoring.introduce.ExtractTestFiles
 import org.jetbrains.kotlin.idea.refactoring.introduce.checkExtract
-import org.jetbrains.kotlin.idea.refactoring.introduce.doExtractFunction
+import org.jetbrains.kotlin.idea.refactoring.introduce.extractFunction.EXTRACT_FUNCTION
+import org.jetbrains.kotlin.idea.refactoring.introduce.extractFunction.ExtractKotlinFunctionHandler
+import org.jetbrains.kotlin.idea.refactoring.introduce.extractionEngine.ExtractableCodeDescriptorWithConflicts
+import org.jetbrains.kotlin.idea.refactoring.introduce.extractionEngine.ExtractionData
+import org.jetbrains.kotlin.idea.refactoring.introduce.extractionEngine.ExtractionEngineHelper
+import org.jetbrains.kotlin.idea.refactoring.introduce.extractionEngine.ExtractionGeneratorConfiguration
+import org.jetbrains.kotlin.idea.refactoring.introduce.extractionEngine.ExtractionGeneratorOptions
+import org.jetbrains.kotlin.idea.refactoring.introduce.extractionEngine.ExtractionOptions
+import org.jetbrains.kotlin.idea.refactoring.introduce.extractionEngine.ExtractionResult
+import org.jetbrains.kotlin.idea.refactoring.introduce.extractionEngine.possibleReturnTypes
+import org.jetbrains.kotlin.idea.util.IdeDescriptorRenderers
 import org.jetbrains.kotlin.psi.KtFile
+import org.jetbrains.kotlin.renderer.DescriptorRenderer
+import org.jetbrains.kotlin.test.InTextDirectivesUtils
+import org.jetbrains.kotlin.test.util.findElementByCommentPrefix
+import java.util.Collections
 
 abstract class AbstractAndroidExtractionTest: KotlinAndroidTestCase() {
+
     fun doTest(path: String) {
         copyResourceDirectoryForTest(path)
         val testFilePath = path + getTestName(true) + ".kt"
@@ -30,6 +48,76 @@ abstract class AbstractAndroidExtractionTest: KotlinAndroidTestCase() {
 
         checkExtract(ExtractTestFiles("$testDataPath/$testFilePath", myFixture.file)) { file ->
             doExtractFunction(myFixture, file as KtFile)
+        }
+    }
+
+    // Test helper method originally declared in AbstractExtractionTest.kt in the Kotlin plugin.
+    fun doExtractFunction(fixture: CodeInsightTestFixture, file: KtFile) {
+        val explicitPreviousSibling = file.findElementByCommentPrefix("// SIBLING:")
+        val fileText = file.getText() ?: ""
+        val expectedNames = InTextDirectivesUtils.findListWithPrefixes(fileText, "// SUGGESTED_NAMES: ")
+        val expectedReturnTypes = InTextDirectivesUtils.findListWithPrefixes(fileText, "// SUGGESTED_RETURN_TYPES: ")
+        val expectedDescriptors =
+          InTextDirectivesUtils.findLinesWithPrefixesRemoved(fileText, "// PARAM_DESCRIPTOR: ").joinToString()
+        val expectedTypes =
+          InTextDirectivesUtils.findLinesWithPrefixesRemoved(fileText, "// PARAM_TYPES: ").map { "[$it]" }.joinToString()
+
+        val extractionOptions = InTextDirectivesUtils.findListWithPrefixes(fileText, "// OPTIONS: ").let {
+            if (it.isNotEmpty()) {
+                @Suppress("UNCHECKED_CAST")
+                val args = it.map { it.toBoolean() }.toTypedArray() as Array<Any?>
+                ExtractionOptions::class.java.constructors.first { it.parameterTypes.size == args.size }.newInstance(*args) as ExtractionOptions
+            } else ExtractionOptions.DEFAULT
+        }
+
+        val renderer = DescriptorRenderer.FQ_NAMES_IN_TYPES
+
+        val editor = fixture.editor
+        val handler = ExtractKotlinFunctionHandler(
+          helper = object : ExtractionEngineHelper(EXTRACT_FUNCTION) {
+              override fun adjustExtractionData(data: ExtractionData): ExtractionData {
+                  return data.copy(options = extractionOptions)
+              }
+
+              override fun configureAndRun(
+                project: Project,
+                editor: Editor,
+                descriptorWithConflicts: ExtractableCodeDescriptorWithConflicts,
+                onFinish: (ExtractionResult) -> Unit
+              ) {
+                  val descriptor = descriptorWithConflicts.descriptor
+                  val actualNames = descriptor.suggestedNames
+                  val actualReturnTypes = descriptor.controlFlow.possibleReturnTypes.map {
+                      IdeDescriptorRenderers.SOURCE_CODE.renderType(it)
+                  }
+                  val allParameters = listOfNotNull(descriptor.receiverParameter) + descriptor.parameters
+                  val actualDescriptors = allParameters.map { renderer.render(it.originalDescriptor) }.joinToString()
+                  val actualTypes = allParameters.map {
+                      it.getParameterTypeCandidates().map { renderer.renderType(it) }.joinToString(", ", "[", "]")
+                  }.joinToString()
+
+                  if (actualNames.size != 1 || expectedNames.isNotEmpty()) {
+                      assertEquals("Expected names mismatch.", expectedNames, actualNames)
+                  }
+                  if (actualReturnTypes.size != 1 || expectedReturnTypes.isNotEmpty()) {
+                      assertEquals("Expected return types mismatch.", expectedReturnTypes, actualReturnTypes)
+                  }
+                  assertEquals("Expected descriptors mismatch.", expectedDescriptors, actualDescriptors)
+                  assertEquals("Expected types mismatch.", expectedTypes, actualTypes)
+
+                  val newDescriptor = if (descriptor.name == "") {
+                      descriptor.copy(suggestedNames = Collections.singletonList("__dummyTestFun__"))
+                  }
+                  else {
+                      descriptor
+                  }
+
+                  doRefactor(ExtractionGeneratorConfiguration(newDescriptor, ExtractionGeneratorOptions.DEFAULT), onFinish)
+              }
+          }
+        )
+        handler.selectElements(editor, file) { elements, previousSibling ->
+            handler.doInvoke(editor, file, elements, explicitPreviousSibling ?: previousSibling)
         }
     }
 }
