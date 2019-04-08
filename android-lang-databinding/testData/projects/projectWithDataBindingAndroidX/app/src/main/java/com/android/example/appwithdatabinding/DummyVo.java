fc8975122c62eef3efcf22d0e86ef6f528b74df9
Kainan Wang <knwang@google.com>
2019-04-08 10:53:55 -0700

    Add test for observable data fields in data binding
    Gradle files are speciallyconfigured to supoort
    LiveData in AndroidX.
    
    Bug: 117607324
    Test: DataBindingObservableTest
    Change-Id: I8043ad571e745b441ba52683daa93d24c5e24455

--- android-lang-databinding/testData/projects/projectWithDataBindingAndroidX/app/src/main/java/com/android/example/appwithdatabinding/DummyVo.java
+++ android-lang-databinding/testData/projects/projectWithDataBindingAndroidX/app/src/main/java/com/android/example/appwithdatabinding/DummyVo.java
@@ -1,9 +1,24 @@
 package com.android.example.appwithdatabinding;
 
+import androidx.lifecycle.LiveData;
+import androidx.databinding.ObservableField;
+
 public class DummyVo {
     public String name;
 
-    public String initialString() {
-        return "test";
+    public String getLiveDataString() {
+        return null;
+    }
+
+    public String getObservableFieldString() {
+        return null;
+    }
+
+    public LiveData<DummyVo> getLiveData() {
+        return null;
+    }
+
+    public ObservableField<DummyVo> getObservableField() {
+        return null;
     }
 }
