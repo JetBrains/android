179c8246893bd7411c503dca927d966713c16a61
Jens Ole Lauridsen <jlauridsen@google.com>
2019-04-09 16:46:25 -0700

    Stop using mocks in property tests.
    The SupportTestUtil is using mocks for some call paths.
    Change this to use SyncNlModel and NlComponents instead.
    
    This should limit strange test results when more functionality
    is added.
    
    Bug: None
    Test: Test cleanup.
    Change-Id: Ic99875c09f73cfd8311e1ed445eaa25805ec7ea8

--- designer/testSrc/com/android/tools/idea/uibuilder/property2/inspector/IdInspectorBuilderTest.kt
+++ designer/testSrc/com/android/tools/idea/uibuilder/property2/inspector/IdInspectorBuilderTest.kt
@@ -64,7 +64,7 @@ class IdInspectorBuilderTest {
   @Test
   fun testNotAvailableForPreferenceTags() {
     for (tagName in PreferenceUtils.VALUES) {
-      val util = InspectorTestUtil(projectRule, tagName)
+      val util = InspectorTestUtil(projectRule, tagName, fileName = "${tagName.toLowerCase()}$DOT_XML")
       val builder = IdInspectorBuilder(util.editorProvider)
       util.addProperty(ANDROID_URI, ATTR_ID, NelePropertyType.ID)
       builder.attachToInspector(util.inspector, util.properties)
@@ -75,7 +75,7 @@ class IdInspectorBuilderTest {
   @Test
   fun testNotAvailableForMenuTags() {
     for (tagName in arrayOf(TAG_MENU, TAG_ITEM, TAG_GROUP)) {
-      val util = InspectorTestUtil(projectRule, tagName)
+      val util = InspectorTestUtil(projectRule, tagName, fileName = "${tagName.toLowerCase()}$DOT_XML")
       val builder = IdInspectorBuilder(util.editorProvider)
       util.addProperty(ANDROID_URI, ATTR_ID, NelePropertyType.ID)
       builder.attachToInspector(util.inspector, util.properties)
