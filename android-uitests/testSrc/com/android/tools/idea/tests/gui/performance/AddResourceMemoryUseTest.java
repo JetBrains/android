efe34f5fdf3794b8927192c934c2b46ecab22ebb
Daniel Liu <liudd@google.com>
2019-04-09 11:29:13 -0700

    Create AddResourceMemoryTest
    This test suit contains a set of test that create and delete resources
    under Android Resource File. The test suit make use of the memory leak
    checker - BLeak.
    
    Bug: 124256751
    Test: AddResourceMemoryUseTest and manual testing
    Change-Id: I3a983a29b9b29b77c5c382fa2983b72838da6c9b

---
+++ android-uitests/testSrc/com/android/tools/idea/tests/gui/performance/AddResourceMemoryUseTest.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright (C) 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.tests.gui.performance;
+
+import com.android.tools.idea.tests.gui.framework.GuiTestRule;
+import com.android.tools.idea.tests.gui.framework.RunIn;
+import com.android.tools.idea.tests.gui.framework.TestGroup;
+import com.android.tools.idea.tests.gui.framework.fixture.CreateResourceFileDialogFixture;
+import com.android.tools.idea.tests.gui.framework.fixture.IdeFrameFixture;
+import com.android.tools.idea.tests.gui.framework.heapassertions.bleak.Bleak;
+import com.android.tools.idea.tests.gui.framework.heapassertions.bleak.UseBleak;
+import com.intellij.testGuiFramework.framework.GuiTestRemoteRunner;
+import org.fest.swing.core.MouseButton;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * The test execute a basic scenario of add Android Resource File making use of BLeak - memory leak checker.
+ * BLeak repeatedly runs the test and capture memory state of each run.
+ * At the end, BLeak outputs result based on memory usage collected from each run.
+ */
+
+@RunWith(GuiTestRemoteRunner.class)
+@RunIn(TestGroup.PERFORMANCE)
+public class AddResourceMemoryUseTest {
+
+  @Rule public final GuiTestRule guiTest = new GuiTestRule();
+
+  @Test
+  @UseBleak
+  public void addNavGraphResource() throws Exception {
+    IdeFrameFixture ideFrameFixture = guiTest.importSimpleApplication();
+    Bleak.runWithBleak(() -> {
+      ideFrameFixture.getProjectView()
+        .selectAndroidPane()
+        .clickPath(MouseButton.RIGHT_BUTTON, "app")
+        .openFromMenu(CreateResourceFileDialogFixture::find, "File", "New", "Android Resource File")
+        .setFilename("menu_g")
+        .setType("menu")
+        .clickOk();
+
+      ideFrameFixture.getProjectView()
+        .selectAndroidPane()
+        .deletePath("app","res","menu", "menu_g.xml");
+    });
+  }
+
+  @Test
+  @UseBleak
+  public void addLayoutResource() throws Exception {
+    IdeFrameFixture ideFrameFixture = guiTest.importSimpleApplication();
+    Bleak.runWithBleak(() -> {
+      ideFrameFixture.getProjectView()
+        .selectAndroidPane()
+        .clickPath(MouseButton.RIGHT_BUTTON, "app")
+        .openFromMenu(CreateResourceFileDialogFixture::find, "File", "New", "Android Resource File")
+        .setFilename("layout_g")
+        .setType("layout")
+        .clickOk();
+
+      ideFrameFixture.getProjectView()
+        .selectAndroidPane()
+        .deletePath("app","res","layout", "layout_g.xml");
+    });
+  }
+}
