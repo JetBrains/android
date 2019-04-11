5c6e8873502efc820317bf0edc1c7eb533d4370e
Raluca Sauciuc <ralucas@google.com>
2019-04-11 11:29:22 -0700

    Matching IDE changes for private API Lint checks
    Bug: 123600927
    Test: done in Lint
    Change-Id: Ib2b001aa6f34cade0ffbbe985d25364dea1d3ddb

--- android/src/com/android/tools/idea/lint/AndroidLintSoonBlockedPrivateApiInspection.java
+++ android/src/com/android/tools/idea/lint/AndroidLintSoonBlockedPrivateApiInspection.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017 The Android Open Source Project
+ * Copyright (C) 2019 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,8 +19,8 @@ import com.android.tools.lint.checks.PrivateApiDetector;
 import org.jetbrains.android.inspections.lint.AndroidLintInspectionBase;
 import org.jetbrains.android.util.AndroidBundle;
 
-public class AndroidLintPrivateApiInspection extends AndroidLintInspectionBase {
-  public AndroidLintPrivateApiInspection() {
-    super(AndroidBundle.message("android.lint.inspections.private.api"), PrivateApiDetector.PRIVATE_API);
+public class AndroidLintSoonBlockedPrivateApiInspection extends AndroidLintInspectionBase {
+  public AndroidLintSoonBlockedPrivateApiInspection() {
+    super(AndroidBundle.message("android.lint.inspections.private.api"), PrivateApiDetector.SOON_BLOCKED_PRIVATE_API);
   }
 }
