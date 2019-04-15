923910c9fcde72fb2853b066274117b0f24caea4
Oscar Adame <oscarad@google.com>
2019-04-15 16:14:05 -0700

    Disable click anchor to delete constraints
    Later on, click to delete should be made while holding a key.
    
    Bug: 127371666
    Test: 'updated' tests
    Change-Id: I8774c985f466fb18dae6c00158cd9280e19a1ea6

--- designer/src/com/android/tools/idea/common/scene/target/AnchorTarget.java
+++ designer/src/com/android/tools/idea/common/scene/target/AnchorTarget.java
@@ -156,19 +156,10 @@ abstract public class AnchorTarget extends BaseTarget implements Notch.Provider 
   @Override
   public void setMouseHovered(boolean over) {
     if (over != mIsOver) {
-      changeMouseOverState(over);
+      mIsOver = over;
       myComponent.getScene().needsRebuildList();
       myComponent.getScene().repaint();
     }
-  }
-
-  private void changeMouseOverState(boolean newValue) {
-    mIsOver = newValue;
-    DecoratorUtilities.ViewStates mode = DecoratorUtilities.ViewStates.SELECTED;
-    if (mIsOver) {
-      mode = DecoratorUtilities.ViewStates.WILL_DESTROY;
-    }
-    DecoratorUtilities.setTimeChange(myComponent.getNlComponent(), myType.toString(), mode);
   }
 
   /**
@@ -176,6 +167,12 @@ abstract public class AnchorTarget extends BaseTarget implements Notch.Provider 
    */
   protected boolean isTargeted() {
     return mIsOver && !myComponent.isSelected();
+  }
+
+  /** Returns true if this anchor can disconnect itself. */
+  protected boolean canDisconnect() {
+    // TODO: Make use of this on a common function to disconnect for constraint and relative anchor targets.
+    return true;
   }
 
   //endregion
@@ -417,12 +414,12 @@ abstract public class AnchorTarget extends BaseTarget implements Notch.Provider 
         side = "Baseline";
         break;
       default:
-        return isConnected() ? "Delete Constraint" : "Create Constraint";
+        return isConnected() && canDisconnect() ? "Delete Constraint" : "Create Constraint";
     }
 
     StringBuilder builder = new StringBuilder();
     builder
-      .append(isConnected() ? "Delete " : "Create ")
+      .append(isConnected() && canDisconnect() ? "Delete " : "Create ")
       .append(side)
       .append(" Constraint");
 
