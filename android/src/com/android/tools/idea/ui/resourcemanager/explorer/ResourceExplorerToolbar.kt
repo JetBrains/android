e9d4ceecf40f81158c77f79a49b06910b811f47d
Vadim Caen <caen@google.com>
2019-04-16 16:27:14 +0100

    [resui] Dynamic size for toolbar fields
    Change the toolbar to use GroupLayout in order to dynamically
    resize the module and search fields
    
    Test: UI, manually testes
    Bug: N/A
    Change-Id: Ibf1e37ac0fa031f6ad937dd27442c48005599874

--- android/src/com/android/tools/idea/ui/resourcemanager/explorer/ResourceExplorerToolbar.kt
+++ android/src/com/android/tools/idea/ui/resourcemanager/explorer/ResourceExplorerToolbar.kt
@@ -19,26 +19,23 @@ import com.android.tools.idea.ui.resourcemanager.ResourceManagerTracking
 import com.intellij.icons.AllIcons
 import com.intellij.openapi.actionSystem.ActionGroup
 import com.intellij.openapi.actionSystem.ActionManager
-import com.intellij.openapi.actionSystem.ActionToolbar
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.DataProvider
 import com.intellij.openapi.actionSystem.DefaultActionGroup
-import com.intellij.openapi.actionSystem.Presentation
 import com.intellij.openapi.actionSystem.Separator
 import com.intellij.openapi.actionSystem.ToggleAction
 import com.intellij.openapi.actionSystem.ex.ComboBoxAction
-import com.intellij.openapi.actionSystem.ex.CustomComponentAction
+import com.intellij.openapi.actionSystem.impl.ActionButton
 import com.intellij.openapi.project.DumbAware
-import com.intellij.openapi.project.DumbAwareAction
 import com.intellij.openapi.wm.impl.content.ToolWindowContentUi
 import com.intellij.ui.DocumentAdapter
 import com.intellij.ui.JBColor
 import com.intellij.ui.SearchTextField
 import com.intellij.util.ui.JBUI
-import java.awt.BorderLayout
 import java.awt.Component
 import java.awt.event.MouseEvent
+import javax.swing.GroupLayout
 import javax.swing.Icon
 import javax.swing.JComponent
 import javax.swing.JPanel
@@ -48,34 +45,66 @@ private const val SEARCH_FIELD_LABEL = "Search resources by name"
 private const val ADD_BUTTON_LABEL = "Add resources to the module"
 private const val FILTERS_BUTTON_LABEL = "Filter displayed resources"
 
+private val MIN_FIELD_SIZE = JBUI.scale(40)
+private val PREF_FIELD_SIZE = JBUI.scale(300)
+private val MAX_FIELD_SIZE = JBUI.scale(400)
+private val BUTTON_SIZE = JBUI.size(20)
+private val GAP_SIZE = JBUI.scale(10)
+
 /**
  * Toolbar displayed at the top of the resource explorer which allows users
  * to change the module and add resources.
  */
 class ResourceExplorerToolbar(
   private val toolbarViewModel: ResourceExplorerToolbarViewModel)
-  : JPanel(BorderLayout()), DataProvider by toolbarViewModel {
+  : JPanel(), DataProvider by toolbarViewModel {
 
   private val moduleSelectionAction = ModuleSelectionAction(toolbarViewModel)
+  private val searchAction = createSearchField()
 
   init {
-    add(createLeftToolbar().component)
-    add(createRightToolbar().component, BorderLayout.EAST)
+    layout = GroupLayout(this)
+    val groupLayout = layout as GroupLayout
+    val addAction = action(AddAction(toolbarViewModel))
+    val separator = com.android.tools.idea.ui.resourcemanager.widget.Separator()
+
+    val moduleSelection = moduleSelectionAction.createCustomComponent(moduleSelectionAction.templatePresentation, "").apply {
+      border = JBUI.Borders.empty(2, 0)
+    }
+    val filterAction = action(FilterAction(toolbarViewModel))
+
+    val sequentialGroup = groupLayout.createSequentialGroup()
+      .addFixedSizeComponent(addAction, true)
+      .addFixedSizeComponent(separator)
+      .addComponent(moduleSelection, MIN_FIELD_SIZE, PREF_FIELD_SIZE, MAX_FIELD_SIZE)
+      .addComponent(searchAction, MIN_FIELD_SIZE, PREF_FIELD_SIZE, MAX_FIELD_SIZE)
+      .addGap(GAP_SIZE, GAP_SIZE, Int.MAX_VALUE) // Align the rest of the components to the right
+      .addFixedSizeComponent(filterAction)
+
+    val verticalGroup = groupLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
+      .addComponent(addAction)
+      .addComponent(separator)
+      .addComponent(moduleSelection)
+      .addComponent(searchAction)
+      .addComponent(filterAction)
+
+    groupLayout.setHorizontalGroup(sequentialGroup)
+    groupLayout.setVerticalGroup(verticalGroup)
+
     border = JBUI.Borders.merge(JBUI.Borders.empty(4, 2), JBUI.Borders.customLine(JBColor.border(), 0, 0, 1, 0), true)
   }
 
-  private fun createLeftToolbar() = ActionManager.getInstance().createActionToolbar(
-    "resourceExplorer",
-    DefaultActionGroup(
-      AddAction(toolbarViewModel),
-      Separator(),
-      moduleSelectionAction,
-      SearchAction(toolbarViewModel)
-    ), true).apply { layoutPolicy = ActionToolbar.NOWRAP_LAYOUT_POLICY }
-
-
-  private fun createRightToolbar() = ActionManager.getInstance().createActionToolbar(
-    "resourceExplorer", DefaultActionGroup(FilterAction(toolbarViewModel)), true)
+  private fun createSearchField() = SearchTextField(true).apply {
+    isFocusable = true
+    toolTipText = SEARCH_FIELD_LABEL
+    accessibleContext.accessibleName = SEARCH_FIELD_LABEL
+    textEditor.columns = GAP_SIZE
+    textEditor.document.addDocumentListener(object : DocumentAdapter() {
+      override fun textChanged(e: DocumentEvent) {
+        toolbarViewModel.searchString = e.document.getText(0, e.document.length)
+      }
+    })
+  }
 }
 
 /**
@@ -84,7 +113,8 @@ class ResourceExplorerToolbar(
 private class ModuleSelectionAction(val viewModel: ResourceExplorerToolbarViewModel) : ComboBoxAction(), DumbAware {
 
   init {
-    this.templatePresentation.text = viewModel.getSelectedModuleText()
+    templatePresentation.text = viewModel.getSelectedModuleText()
+    isSmallVariant = true
   }
 
   override fun update(e: AnActionEvent) {
@@ -97,7 +127,7 @@ private class ModuleSelectionAction(val viewModel: ResourceExplorerToolbarViewMo
 /**
  * Button to add new resources
  */
-abstract class PopupAction internal constructor(val icon: Icon?, description: String)
+private abstract class PopupAction internal constructor(val icon: Icon?, description: String)
   : AnAction(description, description, icon), DumbAware {
 
   override fun actionPerformed(e: AnActionEvent) {
@@ -131,10 +161,9 @@ private class AddAction internal constructor(val viewModel: ResourceExplorerTool
       addAll(importersActions)
     }
   }
-
 }
 
-class FilterAction internal constructor(val viewModel: ResourceExplorerToolbarViewModel)
+private class FilterAction internal constructor(val viewModel: ResourceExplorerToolbarViewModel)
   : PopupAction(AllIcons.General.Filter, FILTERS_BUTTON_LABEL) {
   override fun createAddPopupGroup() = DefaultActionGroup().apply {
     add(ShowDependenciesAction(viewModel))
@@ -150,27 +179,14 @@ private class ShowDependenciesAction internal constructor(val viewModel: Resourc
   }
 }
 
-private class SearchAction internal constructor(val viewModel: ResourceExplorerToolbarViewModel)
-  : DumbAwareAction(), CustomComponentAction {
+private fun action(addAction: AnAction) =
+  ActionButton(addAction, addAction.templatePresentation, "", BUTTON_SIZE)
 
-  override fun update(e: AnActionEvent) {
-    e.presentation.isEnabledAndVisible = e.isFromActionToolbar
-  }
-
-  val searchField = SearchTextField(true).apply {
-    isFocusable = true
-    toolTipText = SEARCH_FIELD_LABEL
-    accessibleContext.accessibleName = SEARCH_FIELD_LABEL
-    textEditor.columns = 10
-    textEditor.document.addDocumentListener(object : DocumentAdapter() {
-      override fun textChanged(e: DocumentEvent) {
-        viewModel.searchString = e.document.getText(0, e.document.length)
-      }
-    })
-  }
-
-  override fun actionPerformed(e: AnActionEvent) {
-  }
-
-  override fun createCustomComponent(presentation: Presentation): JComponent = searchField
+private fun GroupLayout.SequentialGroup.addFixedSizeComponent(
+  jComponent: JComponent,
+  baseline: Boolean = false
+): GroupLayout.SequentialGroup {
+  val width = jComponent.preferredSize.width
+  this.addComponent(baseline, jComponent, width, width, width)
+  return this
 }
