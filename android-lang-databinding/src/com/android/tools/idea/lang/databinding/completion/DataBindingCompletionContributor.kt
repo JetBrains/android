cfd7b614157a996f7717565b7f8ea3ec50d03246
Peter Xu <peterx@google.com>
2019-04-18 15:19:22 -0700

    Make it easier to test completion by specifying a test plugin.xml
    Currently, we need to append a call to
    DataBindingCompletionContributor.attachTracker for any crafted
    LookupElement. With this CL, there is no need to do that anymore,
    because tests will refer to an overriden completion contributor with
    stubbed out tracking code.
    
    Test: N/A
    Bug: N/A
    Change-Id: I8a566aba3d82a2ac7be0b56bf71feca0490d37e6

--- android-lang-databinding/src/com/android/tools/idea/lang/databinding/completion/DataBindingCompletionContributor.kt
+++ android-lang-databinding/src/com/android/tools/idea/lang/databinding/completion/DataBindingCompletionContributor.kt
@@ -22,7 +22,6 @@ import com.android.tools.idea.databinding.analytics.api.DataBindingTracker
 import com.android.tools.idea.lang.databinding.config.DbFile
 import com.android.tools.idea.lang.databinding.getDataBindingLayoutInfo
 import com.android.tools.idea.lang.databinding.model.ModelClassResolvable
-import com.android.tools.idea.lang.databinding.psi.PsiDbExpr
 import com.android.tools.idea.lang.databinding.psi.PsiDbFunctionRefExpr
 import com.android.tools.idea.lang.databinding.psi.PsiDbRefExpr
 import com.google.common.annotations.VisibleForTesting
@@ -36,10 +35,10 @@ import com.intellij.codeInsight.completion.CompletionParameters
 import com.intellij.codeInsight.completion.CompletionProvider
 import com.intellij.codeInsight.completion.CompletionResultSet
 import com.intellij.codeInsight.completion.CompletionType
+import com.intellij.codeInsight.completion.InsertHandler
 import com.intellij.codeInsight.completion.JavaLookupElementBuilder
 import com.intellij.codeInsight.lookup.LookupElement
 import com.intellij.codeInsight.lookup.LookupElementBuilder
-import com.intellij.openapi.util.text.StringUtil
 import com.intellij.patterns.PlatformPatterns
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiSubstitutor
@@ -54,7 +53,21 @@ import com.intellij.util.ProcessingContext
  * Completion for everything under `<data>` tag is in
  * [org.jetbrains.android.AndroidXmlCompletionContributor.completeDataBindingTypeAttr].
  */
-class DataBindingCompletionContributor : CompletionContributor() {
+open class DataBindingCompletionContributor : CompletionContributor() {
+
+  @VisibleForTesting
+  open val onCompletionHandler: InsertHandler<LookupElement>? = InsertHandler { context, _ ->
+    val tracker = DataBindingTracker.getInstance(context.project)
+
+    val childElement = context.file.findElementAt(context.startOffset)!!
+    when (childElement.parent.parent) {
+      is PsiDbFunctionRefExpr -> tracker.trackDataBindingCompletion(DATA_BINDING_COMPLETION_ACCEPTED,
+                                                                    DATA_BINDING_CONTEXT_METHOD_REFERENCE)
+      is PsiDbRefExpr -> tracker.trackDataBindingCompletion(DATA_BINDING_COMPLETION_ACCEPTED, DATA_BINDING_CONTEXT_LAMBDA)
+      else -> tracker.trackDataBindingCompletion(DATA_BINDING_COMPLETION_ACCEPTED, UNKNOWN_CONTEXT)
+    }
+  }
+
   init {
     extend(CompletionType.BASIC, PlatformPatterns.psiElement(), object : CompletionProvider<CompletionParameters>() {
       override fun addCompletions(parameters: CompletionParameters, context: ProcessingContext, result: CompletionResultSet) {
@@ -99,116 +112,101 @@ class DataBindingCompletionContributor : CompletionContributor() {
     })
   }
 
-  companion object {
+  private fun getFile(element: PsiElement): DbFile {
+    var element = element
+    while (element !is DbFile) {
+      element = element.parent ?: throw IllegalArgumentException()
+    }
+    return element
+  }
 
-    private fun getFile(element: PsiElement): DbFile {
-      var element = element
-      while (element !is DbFile) {
-        element = element.parent ?: throw IllegalArgumentException()
+  private fun autoCompleteVariablesAndUnqualifiedFunctions(file: DbFile, result: CompletionResultSet) {
+    autoCompleteUnqualifiedFunctions(result)
+
+    val dataBindingLayoutInfo = getDataBindingLayoutInfo(file) ?: return
+    for ((name, _, xmlTag) in dataBindingLayoutInfo.getItems(DataBindingResourceType.VARIABLE).values) {
+      val elementBuilder = LookupElementBuilder.create(xmlTag,
+                                                       DataBindingUtil.convertToJavaFieldName(name))
+        .withInsertHandler(onCompletionHandler)
+      result.addElement(elementBuilder)
+    }
+  }
+
+  private fun autoCompleteUnqualifiedFunctions(result: CompletionResultSet) {
+    result.addElement(LookupElementBuilder.create("safeUnbox").withInsertHandler(onCompletionHandler))
+  }
+
+  /**
+   * Given a data binding expression, return a list of [LookupElement] which are the field references of the given expression.
+   * If onlyValidCompletions is false, private and mismatched context fields are also suggested.
+   */
+  private fun populateFieldReferenceCompletions(referenceExpression: PsiElement, onlyValidCompletions: Boolean): List<LookupElement> {
+    val completionSuggestionsList = mutableListOf<LookupElement>()
+    val childReferences = referenceExpression.references
+    for (reference in childReferences) {
+      val ref = reference as ModelClassResolvable
+      val resolvedType = ref.resolvedType?.unwrapped ?: continue
+      for (psiModelField in resolvedType.allFields) {
+        if (onlyValidCompletions) {
+          if (!psiModelField.isPublic || ref.isStatic != psiModelField.isStatic) {
+            continue
+          }
+        }
+        // Pass resolvedType.getPsiClass into JavaLookupElementBuilder.forField as qualifierClass
+        // so that only fields declared in current class are bold.
+        val lookupBuilder = JavaLookupElementBuilder
+          .forField(psiModelField.psiField, psiModelField.psiField.name,
+                    resolvedType.psiClass)
+          .withTypeText(
+            PsiFormatUtil.formatVariable(psiModelField.psiField, PsiFormatUtilBase.SHOW_TYPE, PsiSubstitutor.EMPTY))
+          .withInsertHandler(onCompletionHandler)
+        completionSuggestionsList.add(lookupBuilder)
       }
-      return element
     }
+    return completionSuggestionsList
+  }
 
-    private fun autoCompleteVariablesAndUnqualifiedFunctions(file: DbFile, result: CompletionResultSet) {
-      autoCompleteUnqualifiedFunctions(result)
-
-      val dataBindingLayoutInfo = getDataBindingLayoutInfo(file) ?: return
-      for ((name, _, xmlTag) in dataBindingLayoutInfo.getItems(DataBindingResourceType.VARIABLE).values) {
-        val elementBuilder = LookupElementBuilder.create(xmlTag,
-                                                         DataBindingUtil.convertToJavaFieldName(name))
-        result.addElement(attachTracker(elementBuilder))
-      }
-    }
-
-    private fun autoCompleteUnqualifiedFunctions(result: CompletionResultSet) {
-      result.addElement(attachTracker(LookupElementBuilder.create("safeUnbox")))
-    }
-
-    /**
-     * Given a data binding expression, return a list of [LookupElement] which are the field references of the given expression.
-     * If onlyValidCompletions is false, private and mismatched context fields are also suggested.
-     */
-    private fun populateFieldReferenceCompletions(referenceExpression: PsiElement, onlyValidCompletions: Boolean): List<LookupElement> {
-      val completionSuggestionsList = mutableListOf<LookupElement>()
-      val childReferences = referenceExpression.references
-      for (reference in childReferences) {
+  /**
+   * Given a data binding expression, return a list of [LookupElement] which are method references of the given expression.
+   * If onlyValidCompletions is false, private and mismatched context fields are also suggested.
+   */
+  private fun populateMethodReferenceCompletions(referenceExpression: PsiElement,
+                                                 onlyValidCompletions: Boolean,
+                                                 completeBrackets: Boolean = true): List<LookupElement> {
+    val completionSuggestionsList = mutableListOf<LookupElement>()
+    val childReferences = referenceExpression.references
+    for (reference in childReferences) {
+      if (reference is ModelClassResolvable) {
         val ref = reference as ModelClassResolvable
-        val resolvedType = ref.resolvedType?.unwrapped ?: continue
-        for (psiModelField in resolvedType.allFields) {
-          if (onlyValidCompletions) {
-            if (!psiModelField.isPublic || ref.isStatic != psiModelField.isStatic) {
+        var resolvedType = ref.resolvedType?.unwrapped ?: continue
+        for (psiModelMethod in resolvedType.allMethods) {
+          val psiMethod = psiModelMethod.psiMethod
+          if (psiMethod.isConstructor) {
+            continue
+          }
+          else if (onlyValidCompletions) {
+            if (ref.isStatic != psiModelMethod.isStatic || !psiModelMethod.isPublic) {
               continue
             }
           }
-          // Pass resolvedType.getPsiClass into JavaLookupElementBuilder.forField as qualifierClass
-          // so that only fields declared in current class are bold.
-          val lookupBuilder = JavaLookupElementBuilder
-            .forField(psiModelField.psiField, psiModelField.psiField.name,
-                      resolvedType.psiClass)
-            .withTypeText(
-              PsiFormatUtil.formatVariable(psiModelField.psiField, PsiFormatUtilBase.SHOW_TYPE, PsiSubstitutor.EMPTY))
-          completionSuggestionsList.add(attachTracker(lookupBuilder))
-        }
-      }
-      return completionSuggestionsList
-    }
-
-    /**
-     * Given a data binding expression, return a list of [LookupElement] which are method references of the given expression.
-     * If onlyValidCompletions is false, private and mismatched context fields are also suggested.
-     */
-    private fun populateMethodReferenceCompletions(referenceExpression: PsiElement,
-                                                   onlyValidCompletions: Boolean,
-                                                   completeBrackets: Boolean = true): List<LookupElement> {
-      val completionSuggestionsList = mutableListOf<LookupElement>()
-      val childReferences = referenceExpression.references
-      for (reference in childReferences) {
-        if (reference is ModelClassResolvable) {
-          val ref = reference as ModelClassResolvable
-          var resolvedType = ref.resolvedType?.unwrapped ?: continue
-          for (psiModelMethod in resolvedType.allMethods) {
-            val psiMethod = psiModelMethod.psiMethod
-            if (psiMethod.isConstructor) {
-              continue
-            } else if (onlyValidCompletions) {
-              if (ref.isStatic != psiModelMethod.isStatic || !psiModelMethod.isPublic) {
-                continue
-              }
+          var name = psiModelMethod.name
+          if (completeBrackets) {
+            name += "()"
+            if (BrUtil.isGetter(psiMethod)) {
+              name = psiModelMethod.name.substring(3).decapitalize()
             }
-            var name = psiModelMethod.name
-            if (completeBrackets) {
-              name += "()"
-              if (BrUtil.isGetter(psiMethod)) {
-                name = psiModelMethod.name.substring(3).decapitalize()
-              }
-              else if (BrUtil.isBooleanGetter(psiMethod)) {
-                name = psiModelMethod.name.substring(2).decapitalize()
-              }
+            else if (BrUtil.isBooleanGetter(psiMethod)) {
+              name = psiModelMethod.name.substring(2).decapitalize()
             }
-            // Pass resolvedType.getPsiClass into JavaLookupElementBuilder.forMethod as qualifierClass
-            // so that only methods declared in current class are bold.
-            val lookupBuilder = JavaLookupElementBuilder.forMethod(psiMethod, name, PsiSubstitutor.EMPTY, resolvedType.psiClass)
-            completionSuggestionsList.add(attachTracker(lookupBuilder))
           }
-        }
-      }
-      return completionSuggestionsList
-    }
-
-    @VisibleForTesting
-    fun attachTracker(element: LookupElementBuilder): LookupElement {
-      // Attach a completion handler to each look up element so we can track when user accepts a suggestion.
-      return element.withInsertHandler { context, _ ->
-        val tracker = DataBindingTracker.getInstance(context.project)
-
-        val childElement = context.file.findElementAt(context.startOffset)!!
-        when (childElement.parent.parent) {
-          is PsiDbFunctionRefExpr -> tracker.trackDataBindingCompletion(DATA_BINDING_COMPLETION_ACCEPTED,
-                                                                        DATA_BINDING_CONTEXT_METHOD_REFERENCE)
-          is PsiDbRefExpr -> tracker.trackDataBindingCompletion(DATA_BINDING_COMPLETION_ACCEPTED, DATA_BINDING_CONTEXT_LAMBDA)
-          else -> tracker.trackDataBindingCompletion(DATA_BINDING_COMPLETION_ACCEPTED, UNKNOWN_CONTEXT)
+          // Pass resolvedType.getPsiClass into JavaLookupElementBuilder.forMethod as qualifierClass
+          // so that only methods declared in current class are bold.
+          val lookupBuilder = JavaLookupElementBuilder.forMethod(psiMethod, name, PsiSubstitutor.EMPTY,
+                                                                 resolvedType.psiClass).withInsertHandler(onCompletionHandler)
+          completionSuggestionsList.add(lookupBuilder)
         }
       }
     }
+    return completionSuggestionsList
   }
 }
