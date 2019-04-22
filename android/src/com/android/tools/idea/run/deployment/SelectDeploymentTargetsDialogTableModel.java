4b363ca3947f216873db15f83188328e2b962a3f
Juan C Nuno <juancnuno@google.com>
2019-04-22 14:11:21 -0700

    Tweak the Select Deployment Targets dialog
    Set the table row height to thirty pixels
    Add a table header
    Add a Cancel button
    Rename the OK button to Run
    Disable the Run button if no devices are selected
    
    Bug: 129948043
    Test: Not applicable
    Change-Id: Id6fce5e4f20bbc17895c13b555f12f7bef3488e2

--- android/src/com/android/tools/idea/run/deployment/SelectDeploymentTargetsDialogTableModel.java
+++ android/src/com/android/tools/idea/run/deployment/SelectDeploymentTargetsDialogTableModel.java
@@ -95,6 +95,21 @@ final class SelectDeploymentTargetsDialogTableModel extends AbstractTableModel i
 
   @NotNull
   @Override
+  public String getColumnName(int modelColumnIndex) {
+    switch (modelColumnIndex) {
+      case SELECTED_MODEL_COLUMN_INDEX:
+        return "";
+      case ICON_MODEL_COLUMN_INDEX:
+        return "Type";
+      case NAME_MODEL_COLUMN_INDEX:
+        return "Device name";
+      default:
+        throw new AssertionError(modelColumnIndex);
+    }
+  }
+
+  @NotNull
+  @Override
   public Class<?> getColumnClass(int modelColumnIndex) {
     switch (modelColumnIndex) {
       case SELECTED_MODEL_COLUMN_INDEX:
