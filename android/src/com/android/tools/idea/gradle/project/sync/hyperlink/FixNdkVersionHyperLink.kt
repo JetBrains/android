f5a7a391ab8be1a4ba06b2b55097fa92eb5f4918
Jomo Fisher <jomof@google.com>
2019-04-24 13:16:11 -0700

    NDK side-by-side: offer hotlink to update ndkVersion in build.gradle
    Before, if there was a failure to locate NDK then Android Studio
    would offer to update ndk.dir. Now, if there is already an NDK
    installed then Android Studio will offer to set android.ndkVersion
    to the highest version currently installed. If there is no NDK
    installed then we will offer a hyperlink to install the latest
    NDK and also set android.ndkVersion to that version.
    
    Not in this CL but considering next:
    - If there is no android.ndkVersion set then have a balloon that
      suggests adding one (with hyperlink to do it automatically)
    - If there is a more recent NDK available then have a balloon
      with a hyperlink to download and set android.ndkVersion.
      Generally, the goal is to get people away from using ndk.dir and
      to get more people to have android.ndkVersion in their
      build.gradle.
    
    Bug: 130363042
    Test: Unitested
    Change-Id: I5a76a00d9ff6c944eb19ef45d743d3d5d51461e9

---
+++ android/src/com/android/tools/idea/gradle/project/sync/hyperlink/FixNdkVersionHyperLink.kt
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.gradle.project.sync.hyperlink
+
+import com.android.tools.idea.gradle.project.sync.issues.processor.FixNdkVersionProcessor
+import com.android.tools.idea.project.hyperlink.NotificationHyperlink
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VirtualFile
+
+class FixNdkVersionHyperlink(
+  private val version: String,
+  private val buildFiles: List<VirtualFile>) : NotificationHyperlink(
+    "fix.ndk.version",
+   "Update NDK version to '$version' and sync project") {
+
+  override fun execute(project: Project) {
+    val processor = FixNdkVersionProcessor(project, buildFiles, version)
+    processor.run()
+  }
+}
--- android/src/com/android/tools/idea/gradle/project/sync/hyperlink/InstallNdkHyperlink.java
+++ android/src/com/android/tools/idea/gradle/project/sync/hyperlink/InstallNdkHyperlink.java
@@ -22,14 +22,17 @@ import static com.android.tools.idea.sdk.wizard.SdkQuickfixUtils.createDialogFor
 import static com.google.wireless.android.sdk.stats.GradleSyncStats.Trigger.TRIGGER_QF_NDK_INSTALLED;
 
 import com.android.repository.Revision;
+import com.android.repository.api.LocalPackage;
 import com.android.repository.api.RemotePackage;
 import com.android.repository.api.RepoManager;
 import com.android.sdklib.repository.AndroidSdkHandler;
 import com.android.tools.idea.flags.StudioFlags;
 import com.android.tools.idea.gradle.project.sync.GradleSyncInvoker;
+import com.android.tools.idea.gradle.project.sync.issues.processor.FixNdkVersionProcessor;
 import com.android.tools.idea.gradle.util.LocalProperties;
 import com.android.tools.idea.project.hyperlink.NotificationHyperlink;
 import com.android.tools.idea.sdk.AndroidSdks;
+import com.android.tools.idea.sdk.IdeSdks;
 import com.android.tools.idea.sdk.SelectNdkDialog;
 import com.android.tools.idea.sdk.StudioDownloader;
 import com.android.tools.idea.sdk.StudioSettingsController;
@@ -42,18 +45,22 @@ import com.intellij.openapi.application.ModalityState;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.Messages;
+import com.intellij.openapi.vfs.VirtualFile;
 import java.io.File;
 import java.io.IOException;
 import java.util.Collection;
+import java.util.List;
 import java.util.Map;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class InstallNdkHyperlink extends NotificationHyperlink {
   private static final String ERROR_TITLE = "Gradle Sync Error";
+  private final List<VirtualFile> buildFiles;
 
-  public InstallNdkHyperlink() {
-    super("install.ndk", "Install NDK");
+  public InstallNdkHyperlink(List<VirtualFile> buildFiles) {
+    super("install.ndk", "Install latest NDK and sync project");
+    this.buildFiles = buildFiles;
   }
 
   @Override
@@ -103,7 +110,12 @@ public class InstallNdkHyperlink extends NotificationHyperlink {
         if (ndkPath != null) {
           ModelWizardDialog dialog = createDialogForPaths(project, ImmutableList.of(ndkPath), true);
           if (dialog != null && dialog.showAndGet()) {
-            GradleSyncInvoker.getInstance().requestProjectSyncAndSourceGeneration(project, TRIGGER_QF_NDK_INSTALLED);
+            LocalPackage highestLocalNdk = IdeSdks.getInstance().getHighestLocalNdkPackage();
+            if (highestLocalNdk != null) {
+              new FixNdkVersionProcessor(project, buildFiles, highestLocalNdk.getVersion().toString()).run();
+            } else {
+              GradleSyncInvoker.getInstance().requestProjectSyncAndSourceGeneration(project, TRIGGER_QF_NDK_INSTALLED);
+            }
           }
           return;
         }
