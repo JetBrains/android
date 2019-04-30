f8c551aa25e079643d1e340bf100551b39f8de63
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2019-04-30 14:04:06 +0200

    introduce URLUtil.decode as a solution for non-modern JDK API
    To avoid ugly code in each client of URLDecoder.decode
    
    GitOrigin-RevId: 290792e688fc8d7a126305196a0b621701e097eb

--- profilers/src/com/android/tools/profilers/network/httpdata/HttpData.java
+++ profilers/src/com/android/tools/profilers/network/httpdata/HttpData.java
@@ -19,11 +19,11 @@ import com.google.common.annotations.VisibleForTesting;
 import com.google.common.collect.ImmutableSortedMap;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.openapi.vfs.CharsetToolkit;
+import com.intellij.util.io.URLUtil;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URI;
-import java.net.URLDecoder;
 import java.net.URLEncoder;
 import java.util.*;
 
@@ -167,7 +167,7 @@ public class HttpData {
       String lastName;
       do {
         lastName = name;
-        name = URLDecoder.decode(name, CharsetToolkit.UTF8);
+        name = URLUtil.decode(name);
       }
       while (!name.equals(lastName));
       return name;
