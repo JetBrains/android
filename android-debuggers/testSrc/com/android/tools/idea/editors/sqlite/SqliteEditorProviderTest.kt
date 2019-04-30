9c9eaf3b1944c278e966c9fbfc94876f3047b53e
Nathan Paige <npaige@google.com>
2019-04-30 23:37:10 +0000

    Revert "SQLite Editor WorkBench window"
    This reverts commit fc7ce997902a2d4e9fedadb868d13b15f6a38f4f.
    
    Reason for revert: Test failures on Windows
    
    Issue: 131697245
    Change-Id: I3ee9ece69fcf96d368783f1e8488a8634b9e4e29

--- android-debuggers/testSrc/com/android/tools/idea/editors/sqlite/SqliteEditorProviderTest.kt
+++ android-debuggers/testSrc/com/android/tools/idea/editors/sqlite/SqliteEditorProviderTest.kt
@@ -15,44 +15,21 @@
  */
 package com.android.tools.idea.editors.sqlite
 
-import com.android.tools.adtui.workbench.DetachedToolWindowManager
-import com.android.tools.adtui.workbench.PropertiesComponentMock
-import com.android.tools.adtui.workbench.WorkBench
-import com.android.tools.adtui.workbench.WorkBenchManager
 import com.google.common.truth.Truth.assertThat
-import com.intellij.ide.util.PropertiesComponent
-import com.intellij.openapi.application.ApplicationManager
-import com.intellij.openapi.components.impl.ComponentManagerImpl
-import com.intellij.openapi.util.Disposer
 import com.intellij.testFramework.LightPlatformTestCase
 import com.intellij.testFramework.fixtures.IdeaTestFixtureFactory
-import org.jetbrains.android.ComponentStack
-import org.mockito.Mock
-import org.mockito.MockitoAnnotations.initMocks
+import javax.swing.JPanel
 
 class SqliteEditorProviderTest : LightPlatformTestCase() {
   private lateinit var sqliteUtil: SqliteTestUtil
   private var previouslyEnabled: Boolean = false
-  @Mock
-  private lateinit var myWorkBenchManager: WorkBenchManager
-  @Mock
-  private lateinit var myFloatingToolWindowManager: DetachedToolWindowManager
-  private lateinit var myApplicationComponentStack: ComponentStack
-  private lateinit var myProjectComponentStack: ComponentStack
 
   @Throws(Exception::class)
   override fun setUp() {
     super.setUp()
-    initMocks(this)
-    myApplicationComponentStack = ComponentStack(ApplicationManager.getApplication())
-    myProjectComponentStack = ComponentStack(getProject())
     sqliteUtil = SqliteTestUtil(IdeaTestFixtureFactory.getFixtureFactory().createTempDirTestFixture())
     sqliteUtil.setUp()
     previouslyEnabled = SqliteViewer.enableFeature(true)
-
-    registerApplicationComponent(WorkBenchManager::class.java, myWorkBenchManager)
-    registerApplicationComponent(PropertiesComponent::class.java, PropertiesComponentMock())
-    registerProjectComponentImplementation(DetachedToolWindowManager::class.java, myFloatingToolWindowManager)
   }
 
   @Throws(Exception::class)
@@ -60,21 +37,10 @@ class SqliteEditorProviderTest : LightPlatformTestCase() {
     try {
       sqliteUtil.tearDown()
       SqliteViewer.enableFeature(previouslyEnabled)
-      myApplicationComponentStack.restoreComponents()
-      myProjectComponentStack.restoreComponents()
     }
     finally {
       super.tearDown()
     }
-  }
-
-  private fun <T> registerApplicationComponent(key: Class<T>, instance: T) {
-    myApplicationComponentStack.registerComponentInstance(key, instance)
-  }
-
-  private fun <T> registerProjectComponentImplementation(key: Class<T>, instance: T) {
-    (getProject() as ComponentManagerImpl).registerComponentImplementation(key, key)
-    myProjectComponentStack.registerComponentInstance(key, instance)
   }
 
   @Throws(Exception::class)
@@ -142,13 +108,12 @@ class SqliteEditorProviderTest : LightPlatformTestCase() {
 
     // Act
     val editor = provider.createEditor(getProject(), file)
-    Disposer.register(testRootDisposable, editor)
 
     // Assert
     assertThat(editor).isNotNull()
     assertThat(editor).isInstanceOf(SqliteEditor::class.java)
     assertThat(editor.isValid).isTrue()
     assertThat(editor.isModified).isFalse()
-    assertThat(editor.component).isInstanceOf(WorkBench::class.java)
+    assertThat(editor.component).isInstanceOf(JPanel::class.java)
   }
 }
