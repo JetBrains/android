40be89d204c9ba150027b7f7a7727f3c2abc1714
Charlie Tsai <chartsai@google.com>
2019-05-02 16:18:01 +0100

    Always checking draggable in ConstraintLayout
    ConstraintLayout checks the constraints to
    place the component after dragging. So the
    actual position after dragging may be different
    than the mouse position.
    
    Before this CL, it checks the position when
    Live Rendering is enable only. This CL makes
    ConstraintLayout checks the draggable area
    no matter Live Rendering is enable or not.
    
    Fixes: 129681462
    Test: Updated CommonDragTarget
    Change-Id: I3b66edea1b3fd7b9cedeea60592ee6a99bc523cd

--- designer/src/com/android/tools/idea/uibuilder/handlers/constraint/ConstraintPlaceholder.kt
+++ designer/src/com/android/tools/idea/uibuilder/handlers/constraint/ConstraintPlaceholder.kt
@@ -29,6 +29,8 @@ class ConstraintPlaceholder(host: SceneComponent) : Placeholder(host) {
 
   private val delegator = ViewGroupPlaceholder(host)
 
+  override val isLiveUpdatable = true
+
   override val dominate = false
 
   override val region = delegator.region
@@ -36,11 +38,11 @@ class ConstraintPlaceholder(host: SceneComponent) : Placeholder(host) {
   override fun snap(left: Int, top: Int, right: Int, bottom: Int, retPoint: Point) = delegator.snap(left, top, right, bottom, retPoint)
 
   override fun updateAttribute(sceneComponent: SceneComponent, attributes: NlAttributesHolder) =
-    updateAttribute(sceneComponent, attributes, sceneComponent.drawX, sceneComponent.drawY)
+    updateLiveAttribute(sceneComponent, attributes, sceneComponent.drawX, sceneComponent.drawY)
 
   /**
    * Position of [SceneComponent] is not set yet when live rendering is enabled, the [x] and [y] argument should be passed in.
    */
-  fun updateAttribute(sceneComponent: SceneComponent, attributes: NlAttributesHolder, x: Int, y: Int) =
+  override fun updateLiveAttribute(sceneComponent: SceneComponent, attributes: NlAttributesHolder, x: Int, y: Int) =
     ConstraintDragTarget.ConstraintDropHandler(sceneComponent).updateAttributes(attributes, host, x, y)
 }
