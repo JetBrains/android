058144b90fbc854fa716a2e834bbc3b4e787c004
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2019-05-03 09:22:40 +0200

    deprecate ContainerUtil.list
    (cherry picked from commit 0ae1b01b12479298e3f7285e5b036bf1992e7edb)
    
    Change-Id: I0ae1b01b12479298e3f7285e5b036bf1992e7edb

--- android/src/com/android/tools/idea/gradle/run/AndroidGradleTestTasksProvider.java
+++ android/src/com/android/tools/idea/gradle/run/AndroidGradleTestTasksProvider.java
@@ -4,10 +4,10 @@ import com.android.tools.idea.gradle.project.facet.gradle.GradleFacet;
 import com.android.tools.idea.gradle.project.model.AndroidModuleModel;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.util.text.StringUtil;
-import com.intellij.util.containers.ContainerUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.plugins.gradle.execution.test.runner.GradleTestTasksProvider;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -20,7 +20,7 @@ public class AndroidGradleTestTasksProvider implements GradleTestTasksProvider {
       if (androidModel != null) {
         final String variant = androidModel.getSelectedVariant().getName();
         final String testTask = "test" + StringUtil.capitalize(variant) + "UnitTest";
-        return ContainerUtil.list("clean" + StringUtil.capitalize(testTask), testTask);
+        return Arrays.asList("clean" + StringUtil.capitalize(testTask), testTask);
       }
     }
     return Collections.emptyList();
