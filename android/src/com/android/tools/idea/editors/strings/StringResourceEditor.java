61cab11bc78b612c8fc4f0789a7890203958bc4e
Diego Perez <diegoperez@google.com>
2019-05-02 18:35:27 -0700

    Remove Strings editor preview feature
    The feature depends on the AndroidPreviewPanel that was used by the
    theme editor. This class is not supported for previews anymore so we are
    removing usages.
    
    Test: Existing tests pass
    Change-Id: I44ced1b7a4256e31f7afd0ae3811199367da52b9
    Fixes: 131644642

--- android/src/com/android/tools/idea/editors/strings/StringResourceEditor.java
+++ android/src/com/android/tools/idea/editors/strings/StringResourceEditor.java
@@ -15,22 +15,13 @@
  */
 package com.android.tools.idea.editors.strings;
 
-import com.android.tools.idea.project.AndroidProjectInfo;
 import com.intellij.codeHighlighting.BackgroundEditorHighlighter;
 import com.intellij.ide.structureView.StructureViewBuilder;
 import com.intellij.openapi.fileEditor.FileEditor;
 import com.intellij.openapi.fileEditor.FileEditorLocation;
 import com.intellij.openapi.fileEditor.FileEditorState;
 import com.intellij.openapi.fileEditor.FileEditorStateLevel;
-import com.intellij.openapi.module.Module;
-import com.intellij.openapi.project.DumbService;
-import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.UserDataHolderBase;
-import com.intellij.openapi.wm.ToolWindow;
-import com.intellij.openapi.wm.ToolWindowAnchor;
-import com.intellij.openapi.wm.ToolWindowManager;
-import com.intellij.ui.DocumentAdapter;
-import com.intellij.ui.content.ContentFactory;
 import com.intellij.util.ui.UIUtil;
 import icons.StudioIcons;
 import org.jetbrains.android.facet.AndroidFacet;
@@ -38,51 +29,19 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
-import javax.swing.event.DocumentEvent;
-import javax.swing.text.BadLocationException;
-import javax.swing.text.Document;
-import javax.swing.text.JTextComponent;
-import java.awt.event.FocusAdapter;
-import java.awt.event.FocusEvent;
 import java.beans.PropertyChangeListener;
 
 public class StringResourceEditor extends UserDataHolderBase implements FileEditor {
 
   public static final Icon ICON = StudioIcons.LayoutEditor.Toolbar.LANGUAGE;
   public static final String NAME = "String Resource Editor";
-  private static final String TOOL_WINDOW_ID = "StringPreview";
 
   private StringResourceViewPanel myPanel;
-
-  private final PreviewFeature myPreviewFeature;
 
   StringResourceEditor(@NotNull StringsVirtualFile file) {
     AndroidFacet facet = file.getFacet();
     // Post startup activities (such as when reopening last open editors) are run from a background thread
     UIUtil.invokeAndWaitIfNeeded(() -> myPanel = new StringResourceViewPanel(facet, this));
-
-    myPreviewFeature = new PreviewFeature(this, facet.getModule());
-
-    myPanel.addDocumentListener(new DocumentAdapter() {
-      @Override
-      protected void textChanged(@NotNull DocumentEvent event) {
-        Document document = event.getDocument();
-
-        try {
-          myPreviewFeature.setText(document.getText(0, document.getLength()));
-        }
-        catch (BadLocationException exception) {
-          myPreviewFeature.setText("");
-        }
-      }
-    });
-
-    myPanel.addFocusListener(new FocusAdapter() {
-      @Override
-      public void focusGained(@NotNull FocusEvent event) {
-        myPreviewFeature.setText(((JTextComponent)event.getSource()).getText());
-      }
-    });
   }
 
   @NotNull
@@ -130,12 +89,10 @@ public class StringResourceEditor extends UserDataHolderBase implements FileEdit
 
   @Override
   public void selectNotify() {
-    myPreviewFeature.selectNotify();
   }
 
   @Override
   public void deselectNotify() {
-    myPreviewFeature.deselectNotify();
   }
 
   @Override
@@ -172,53 +129,5 @@ public class StringResourceEditor extends UserDataHolderBase implements FileEdit
   @Override
   public String toString() {
     return "StringResourceEditor " + myPanel.getFacet() + " " + System.identityHashCode(this);
-  }
-
-  static class PreviewFeature extends SelectedEditorFeature {
-    private final Module myModule;
-    private StringPreview myPreview;
-    private String myPreviewString;
-
-    public PreviewFeature(@NotNull FileEditor editor, @NotNull Module module) {
-      super(editor, module.getProject());
-      myModule = module;
-    }
-
-    @Override
-    public boolean isReady() {
-      return !DumbService.getInstance(myModule.getProject()).isDumb() &&
-             !AndroidProjectInfo.getInstance(myModule.getProject()).requiredAndroidModelMissing() &&
-             ToolWindowManager.getInstance(myModule.getProject()).getToolWindow(TOOL_WINDOW_ID) == null;
-    }
-
-    @Override
-    public void open() {
-      myPreview = new StringPreview(myModule);
-      if (myPreviewString != null) {
-        myPreview.setText(myPreviewString);
-      }
-
-      Project project = myModule.getProject();
-      ToolWindowManager manager = ToolWindowManager.getInstance(project);
-
-      ToolWindow toolWindow = manager.registerToolWindow(TOOL_WINDOW_ID, false, ToolWindowAnchor.RIGHT, project, true);
-
-      toolWindow.setIcon(StudioIcons.Shell.ToolWindows.ANDROID_PREVIEW);
-      toolWindow.setStripeTitle("Preview");
-      toolWindow.getContentManager().addContent(ContentFactory.SERVICE.getInstance().createContent(myPreview.getComponent(), "", false));
-    }
-
-    @Override
-    public void close() {
-      ToolWindowManager.getInstance(myModule.getProject()).unregisterToolWindow(TOOL_WINDOW_ID);
-      myPreview = null;
-    }
-
-    public void setText(@NotNull String text) {
-      myPreviewString = text;
-      if (myPreview != null) {
-        myPreview.setText(text);
-      }
-    }
   }
 }
