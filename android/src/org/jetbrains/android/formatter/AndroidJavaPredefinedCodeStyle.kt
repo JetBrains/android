2820a63e47e6c5482d8f208ff0cd89dc96a2804d
Raluca Sauciuc <ralucas@google.com>
2019-05-07 21:29:05 -0700

    Code style settings fixes
    Android Studio now provides predefined code styles for XML and Java (which can
    be selected via Set From -> Predefined Style -> Android, and are available in
    the Android plugin as well) and properly adds the custom XML arrangement
    settings in the common XML code style.
    
    Previously, these changes were applied during startup on the Default scheme, but
    that is insufficient because applying other code styles (such as the official
    Kotlin code style) in turn changes the settings to a Project scheme which
    doesn't inherit our customizations -- most likely changed after 2017.3:
    https://github.com/JetBrains/intellij-community/commit/414025cbc1ce3609d18b59f258074eb2b0bebc02
    
    With this change we ensure CodeStyleSettings() has the Android code style
    applied, so that even opening existing projects with a non-Default scheme now
    has the right XML arrangement settings (if they haven't been explicitly modified
    already).
    
    Bug: 131706579
    Bug: 117642392
    Test: added (also, manual)
    Change-Id: I3b41f3dacbfecfb5c0dff41b8f4af0b2cc193d8b
    (cherry picked from commit 5ae27adfa8931f1697336c9880f4c5ee9171420e)

---
+++ android/src/org/jetbrains/android/formatter/AndroidJavaPredefinedCodeStyle.kt
@@ -0,0 +1,82 @@
+/*
+ * Copyright (C) 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jetbrains.android.formatter
+
+import com.intellij.lang.java.JavaLanguage
+import com.intellij.psi.codeStyle.CodeStyleSettings
+import com.intellij.psi.codeStyle.JavaCodeStyleSettings
+import com.intellij.psi.codeStyle.PackageEntry
+import com.intellij.psi.codeStyle.PackageEntryTable
+import com.intellij.psi.codeStyle.PredefinedCodeStyle
+
+class AndroidJavaPredefinedCodeStyle : PredefinedCodeStyle("Android", JavaLanguage.INSTANCE) {
+
+  override fun apply(settings: CodeStyleSettings) {
+    val javaSettings = settings.getCustomSettings(JavaCodeStyleSettings::class.java)
+
+    // Set Import order
+    javaSettings.IMPORT_LAYOUT_TABLE.copyFrom(getAndroidImportOrder())
+
+    // Don't use wildcard imports
+    javaSettings.PACKAGES_TO_USE_IMPORT_ON_DEMAND.copyFrom(PackageEntryTable())
+    javaSettings.CLASS_COUNT_TO_USE_IMPORT_ON_DEMAND = 99 // Class count to use import with "*"
+    javaSettings.NAMES_COUNT_TO_USE_IMPORT_ON_DEMAND = 99 // Name count to use static import with "*"
+
+    // set margin to 100 columns
+    settings.defaultRightMargin = 100
+  }
+
+  private fun getAndroidImportOrder(): PackageEntryTable {
+    val table = PackageEntryTable()
+    // From http://s.android.com/source/code-style.html, the order should be:
+    //  1. Android imports
+    //  2. Imports from 3rd parties (com, junit, net, org)
+    //  3. java and javax
+    table.addEntry(PackageEntry(false, "android", true))
+    table.addEntry(PackageEntry.BLANK_LINE_ENTRY)
+
+    table.addEntry(PackageEntry(false, "androidx", true))
+    table.addEntry(PackageEntry.BLANK_LINE_ENTRY)
+
+    table.addEntry(PackageEntry(false, "com", true))
+    table.addEntry(PackageEntry.BLANK_LINE_ENTRY)
+
+    table.addEntry(PackageEntry(false, "junit", true))
+    table.addEntry(PackageEntry.BLANK_LINE_ENTRY)
+
+    table.addEntry(PackageEntry(false, "net", true))
+    table.addEntry(PackageEntry.BLANK_LINE_ENTRY)
+
+    table.addEntry(PackageEntry(false, "org", true))
+    table.addEntry(PackageEntry.BLANK_LINE_ENTRY)
+
+    table.addEntry(PackageEntry(false, "java", true))
+    table.addEntry(PackageEntry.BLANK_LINE_ENTRY)
+
+    table.addEntry(PackageEntry(false, "javax", true))
+    table.addEntry(PackageEntry.BLANK_LINE_ENTRY)
+
+    // handle any other conditions
+    table.addEntry(PackageEntry.ALL_OTHER_IMPORTS_ENTRY)
+    table.addEntry(PackageEntry.BLANK_LINE_ENTRY)
+
+    // location of static imports is not defined, we put it last
+    table.addEntry(PackageEntry.ALL_OTHER_STATIC_IMPORTS_ENTRY)
+    table.addEntry(PackageEntry.BLANK_LINE_ENTRY)
+
+    return table
+  }
+}
