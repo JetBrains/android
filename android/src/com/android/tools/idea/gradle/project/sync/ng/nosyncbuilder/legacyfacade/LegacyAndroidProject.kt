e4cb901f09445603bcba7bb1fa9e40c2156b9105
Chris Warrington <cmw@google.com>
2019-05-09 14:37:47 +0100

    Split IdeLintOptions from LintOptions
    As part of untangling the AGP Gradle tooling API interfaces from other
    parts of the codebase.
    
    Bug: 131735732
    Test: Covered by existing tests.
    Change-Id: I3f415db4938c65fdd4206a9e7d502d9d82b5de61

--- android/src/com/android/tools/idea/gradle/project/sync/ng/nosyncbuilder/legacyfacade/LegacyAndroidProject.kt
+++ android/src/com/android/tools/idea/gradle/project/sync/ng/nosyncbuilder/legacyfacade/LegacyAndroidProject.kt
@@ -16,6 +16,7 @@
 package com.android.tools.idea.gradle.project.sync.ng.nosyncbuilder.legacyfacade
 
 import com.android.builder.model.*
+import com.android.ide.common.gradle.model.IdeLintOptions
 import com.android.ide.common.gradle.model.IdeVariant
 import com.android.ide.common.gradle.model.UnusedModelMethodException
 import com.android.ide.common.gradle.model.level2.IdeDependenciesFactory
@@ -24,7 +25,6 @@ import com.android.tools.idea.gradle.project.sync.ng.nosyncbuilder.interfaces.an
 import com.android.tools.idea.gradle.project.sync.ng.nosyncbuilder.interfaces.variant.Variant
 import com.android.tools.idea.gradle.project.sync.ng.nosyncbuilder.legacyfacade.stubs.BuildTypeContainerStub
 import com.android.tools.idea.gradle.project.sync.ng.nosyncbuilder.legacyfacade.stubs.BuildTypeStub
-import com.android.tools.idea.gradle.project.sync.ng.nosyncbuilder.legacyfacade.stubs.LintOptionsStub
 import com.android.tools.idea.gradle.project.sync.ng.nosyncbuilder.legacyfacade.stubs.ProductFlavorContainerStub
 import com.android.tools.idea.gradle.project.sync.ng.nosyncbuilder.misc.*
 import java.io.File
@@ -56,7 +56,7 @@ open class LegacyAndroidProject(private val androidProject: AndroidProject, priv
   override fun getBuildTypes(): Collection<BuildTypeContainer> = throw UnusedModelMethodException("getBuildTypes")
   override fun getProductFlavors(): Collection<ProductFlavorContainer> = throw UnusedModelMethodException("getProductFlavors")
   override fun getDefaultConfig(): ProductFlavorContainer = throw UnusedModelMethodException("getDefaultConfig")
-  override fun getLintOptions(): LintOptions = throw UnusedModelMethodException("getLintOptions") // will be replaced with a separate model
+  override fun getLintOptions(): IdeLintOptions = throw UnusedModelMethodException("getLintOptions") // will be replaced with a separate model
   override fun getFlavorDimensions(): Collection<String> = throw UnusedModelMethodException("getFlavorDimensions")
   override fun getPluginGeneration(): Int = throw UnusedModelMethodException("getPluginGeneration")
   override fun getResourcePrefix(): String? = throw UnusedModelMethodException("getResourcePrefix") // use namespacing instead
@@ -97,7 +97,7 @@ class LegacyAndroidProjectStub(
 ) : LegacyAndroidProject(androidProject, variant) {
   override fun getAaptOptions(): OldAaptOptions = LegacyAaptOptionsStub(androidProject.aaptOptions)
   override fun getSigningConfigs(): Collection<OldSigningConfig> = androidProject.signingConfigs.map { LegacySigningConfigStub(it) }
-  override fun getLintOptions(): LintOptions = LintOptionsStub()
+  override fun getLintOptions(): IdeLintOptions = IdeLintOptions()
   override fun getVariants(): Collection<com.android.builder.model.Variant> = listOf(LegacyVariantStub(variant))
 
   override fun getDefaultConfig(): ProductFlavorContainer = ProductFlavorContainerStub(
