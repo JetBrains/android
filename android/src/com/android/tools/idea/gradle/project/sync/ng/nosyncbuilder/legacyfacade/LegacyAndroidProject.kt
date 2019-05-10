d2c906c3c67079abedab2efc3983f32ca5a2dccc
Chris Warrington <cmw@google.com>
2019-05-10 12:16:07 +0100

    Remove some unused methods from IdeAndroidProject.
    As part of untangling the AGP Gradle tooling API interfaces from other
    parts of the codebase.
    
    Bug: 131735732
    Test: Covered by existing tests.
    Change-Id: Ifa0fd041160db19af7c2ce742c80c7d71df69467

--- android/src/com/android/tools/idea/gradle/project/sync/ng/nosyncbuilder/legacyfacade/LegacyAndroidProject.kt
+++ android/src/com/android/tools/idea/gradle/project/sync/ng/nosyncbuilder/legacyfacade/LegacyAndroidProject.kt
@@ -48,17 +48,11 @@ open class LegacyAndroidProject(private val androidProject: AndroidProject, priv
   override fun getDynamicFeatures(): Collection<String> = androidProject.dynamicFeatures
   override fun getVariants(): Collection<com.android.builder.model.Variant> = listOf(LegacyVariant(variant))
 
-  @Deprecated("use project type", ReplaceWith("getProjectType()"))
-  override fun isLibrary(): Boolean = androidProject.projectType == AndroidProject.ProjectType.LIBRARY
-
-  override fun getNativeToolchains(): Collection<NativeToolchain> = TODO("native model support")
-
   override fun getBuildTypes(): Collection<BuildTypeContainer> = throw UnusedModelMethodException("getBuildTypes")
   override fun getProductFlavors(): Collection<ProductFlavorContainer> = throw UnusedModelMethodException("getProductFlavors")
   override fun getDefaultConfig(): ProductFlavorContainer = throw UnusedModelMethodException("getDefaultConfig")
   override fun getLintOptions(): IdeLintOptions = throw UnusedModelMethodException("getLintOptions") // will be replaced with a separate model
   override fun getFlavorDimensions(): Collection<String> = throw UnusedModelMethodException("getFlavorDimensions")
-  override fun getPluginGeneration(): Int = throw UnusedModelMethodException("getPluginGeneration")
   override fun getResourcePrefix(): String? = throw UnusedModelMethodException("getResourcePrefix") // use namespacing instead
   override fun getBuildToolsVersion(): String = throw UnusedModelMethodException("getBuildToolsVersion")
   @Deprecated("use sync issues", ReplaceWith("getSyncIssues()"))
@@ -117,9 +111,7 @@ class LegacyAndroidProjectStub(
   override fun getProductFlavors(): Collection<ProductFlavorContainer> = listOf()
   override fun getBuildToolsVersion(): String = "buildToolsVersion" // TODO(qumeric) Get actual value instead
   override fun getFlavorDimensions(): Collection<String> = listOf()
-  override fun getNativeToolchains(): Collection<NativeToolchain> = listOf() // TODO(qumeric): remove when native libraries are supported
   override fun getResourcePrefix(): String? = null
-  override fun getPluginGeneration(): Int = com.android.builder.model.AndroidProject.GENERATION_ORIGINAL
   @Deprecated("use sync issues instead", ReplaceWith("getSyncIssues()"))
   override fun getUnresolvedDependencies(): Collection<String> = listOf()
 }
