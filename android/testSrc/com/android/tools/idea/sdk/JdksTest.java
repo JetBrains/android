ebe49c79d15a45184df344e07024885b9cf42621
Sergio Murguia <srmurguia@google.com>
2019-05-13 13:15:57 -0700

    Show UseJavaHomeAsJdkHyperlink only when valid
    When we detect that the Jdk is not configured correctly we show a
    message and a couple of hyperlinks to help users solve this issue,
    giving priority to using JAVA_HOME. We are currently offering it without
    checking if the JDK version pointed by JAVA_HOME is valid or not.
    
    This CL also checks that the JDK in JAVA_HOME is a valid JDK before
    trying to offer it.
    
    Bug: 124680713
    Test: new tests in JdksTest
    Change-Id: I30e4a1dd2d1a3993abf370170643d9fdbcd4a2d5

--- android/testSrc/com/android/tools/idea/sdk/JdksTest.java
+++ android/testSrc/com/android/tools/idea/sdk/JdksTest.java
@@ -15,14 +15,40 @@
  */
 package com.android.tools.idea.sdk;
 
+import static com.google.common.truth.Truth.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.spy;
+
+import com.android.tools.idea.gradle.project.sync.hyperlink.DownloadAndroidStudioHyperlink;
+import com.android.tools.idea.gradle.project.sync.hyperlink.DownloadJdk8Hyperlink;
+import com.android.tools.idea.gradle.project.sync.hyperlink.UseEmbeddedJdkHyperlink;
+import com.android.tools.idea.gradle.project.sync.hyperlink.UseJavaHomeAsJdkHyperlink;
+import com.android.tools.idea.gradle.util.EmbeddedDistributionPaths;
+import com.android.tools.idea.project.hyperlink.NotificationHyperlink;
+import com.android.tools.idea.testing.IdeComponents;
 import com.intellij.openapi.projectRoots.JavaSdkVersion;
 import com.intellij.pom.java.LanguageLevel;
 import com.intellij.testFramework.IdeaTestCase;
+import java.util.List;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * Tests for {@link Jdks}.
  */
 public class JdksTest extends IdeaTestCase {
+  private IdeSdks mySpyIdeSdks;
+  private EmbeddedDistributionPaths mySpyEmbeddedDistributionPaths;
+
+  @Override
+  protected void setUp() throws Exception {
+    super.setUp();
+    IdeComponents ideComponents = new IdeComponents(myProject);
+    mySpyIdeSdks = spy(IdeSdks.getInstance());
+    mySpyEmbeddedDistributionPaths = spy(EmbeddedDistributionPaths.getInstance());
+    ideComponents.replaceApplicationService(IdeSdks.class, mySpyIdeSdks);
+    ideComponents.replaceApplicationService(EmbeddedDistributionPaths.class, mySpyEmbeddedDistributionPaths);
+  }
 
   // These tests verify that LanguageLevel#isAtLeast does what we think it does (this is IntelliJ code.) Leaving these tests here as a way
   // ensure that regressions are not introduced later.
@@ -36,5 +62,47 @@ public class JdksTest extends IdeaTestCase {
 
   public void testHasMatchingLangLevelWithLangLevel1dot7AndJdk6() {
     assertFalse(Jdks.hasMatchingLangLevel(JavaSdkVersion.JDK_1_6, LanguageLevel.JDK_1_7));
+  }
+
+  /**
+   * Confirm {@link UseJavaHomeAsJdkHyperlink} is offered when JavaHome is not used
+   */
+  public void testGetWrongJdkQuickFixesNotUsingJavaHome() {
+    doReturn(false).when(mySpyIdeSdks).isUsingJavaHomeJdk();
+    verifyGetWrongJdkQuickFixes(UseJavaHomeAsJdkHyperlink.class);
+  }
+
+  /**
+   * Confirm {@link UseEmbeddedJdkHyperlink} is offered when JavaHome is not used but is not valid
+   */
+  public void testGetWrongJdkQuickFixesNotUsingJavaHomeInvalid() {
+    doReturn(false).when(mySpyIdeSdks).isUsingJavaHomeJdk();
+    doReturn(null).when(mySpyIdeSdks).validateJdkPath(any());
+    verifyGetWrongJdkQuickFixes(UseEmbeddedJdkHyperlink.class);
+  }
+
+  /**
+   * Confirm {@link UseEmbeddedJdkHyperlink} is offered when JavaHome is already in use
+   */
+  public void testGetWrongJdkQuickFixesUsingJavaHome() {
+    doReturn(true).when(mySpyIdeSdks).isUsingJavaHomeJdk();
+    verifyGetWrongJdkQuickFixes(UseEmbeddedJdkHyperlink.class);
+  }
+
+  /**
+   * Confirm {@link DownloadAndroidStudioHyperlink} is offered when JavaHome and Embedded jdk cannot be used
+   */
+  public void testGetWrongJdkQuickFixesUsingJavaHomeWithOutEmbedded() {
+    doReturn(true).when(mySpyIdeSdks).isUsingJavaHomeJdk();
+    doReturn(null).when(mySpyEmbeddedDistributionPaths).tryToGetEmbeddedJdkPath();
+    verifyGetWrongJdkQuickFixes(DownloadAndroidStudioHyperlink.class);
+  }
+
+  private void verifyGetWrongJdkQuickFixes(@NotNull Class<? extends NotificationHyperlink> hyperlinkClass) {
+    Jdks jdks = Jdks.getInstance();
+    List<NotificationHyperlink> quickFixes = jdks.getWrongJdkQuickFixes(myProject);
+    assertThat(quickFixes).hasSize(2);
+    assertThat(quickFixes.get(0)).isInstanceOf(hyperlinkClass);
+    assertThat(quickFixes.get(1)).isInstanceOf(DownloadJdk8Hyperlink.class);
   }
 }
