4f41b659908026303c4b8f8c3910430e0b80e743
Peter Xu <peterx@google.com>
2019-05-14 13:28:41 -0700

    Revert part of "Revert data binding class names referenced by Kotlin compiler"
    This CL moves the following files back:
    Move DataBindingClassFinder -> finders.LayoutBindingClassFinder
    Move DataBindingComponentClassFinder ->
    finders.BindingComponentClassFinder
    
    The original (reverted) move causes IDE to not find DataBinding light
    classes, because of https://youtrack.jetbrains.com/issue/KT-12402. The
    good news it doesn't seem to be related to the deadlock issue, so I
    think these two files are safe to be moved back.
    
    Test: N/A
    Fixes: 131889243
    Change-Id: I49fbdfcdd7659839e9647b25eaf13405a76708d2
    (cherry picked from commit 613ab0f8e24dd59797fb403662dbf21f3971544e)

--- databinding/src/com/android/tools/idea/databinding/finders/BindingComponentClassFinder.java
+++ databinding/src/com/android/tools/idea/databinding/finders/BindingComponentClassFinder.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.android.tools.idea.databinding;
+package com.android.tools.idea.databinding.finders;
 
 import com.android.tools.idea.databinding.DataBindingProjectComponent;
 import com.android.tools.idea.databinding.psiclass.LightBindingComponentClass;
@@ -37,16 +37,12 @@ import java.util.List;
  * A finder responsible for finding all the generated DataBindingComponents in this project.
  *
  * See {@link LightBindingComponentClass}
- *
- * TODO(b/129543644): This class cannot change its name or package location until we remove
- *  hardcoded references to it from the Kotlin plugin.
- *  Move back to: finders.BindingComponentCacheFinder
  */
-public class DataBindingComponentClassFinder extends PsiElementFinder {
+public class BindingComponentClassFinder extends PsiElementFinder {
   private final DataBindingProjectComponent myComponent;
   private CachedValue<List<PsiClass>> myClasses;
 
-  public DataBindingComponentClassFinder(final DataBindingProjectComponent component) {
+  public BindingComponentClassFinder(final DataBindingProjectComponent component) {
     myComponent = component;
     myClasses = CachedValuesManager.getManager(component.getProject()).createCachedValue(
       () -> {
