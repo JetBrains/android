75a2edc1b0bc70ee702a1247770f1c187f8d0e39
Amaury Medeiros <amaurym@google.com>
2019-05-21 14:52:27 +0100

    Check if some stacktraces can be removed from the baseline
    Run a test to compare the aspects agent baseline with the stack traces
    from aspects_active_stack_traces_log.txt, which should be the ones hit
    when running the UI tests. The test logs every stack trace on the baseline
    that is not hit, meaning it can be removed.
    
    Test: added a unit test
    Bug: 133301342
    Change-Id: I55431ba9aa0140ebaa0c9345b5b53a8ffcb7f0b8

--- uitest-framework/testSrc/com/android/tools/idea/tests/gui/framework/aspects/AspectsAgentLogger.java
+++ uitest-framework/testSrc/com/android/tools/idea/tests/gui/framework/aspects/AspectsAgentLogger.java
@@ -13,17 +13,14 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.android.tools.idea.tests.gui.framework;
+package com.android.tools.idea.tests.gui.framework.aspects;
 
-import com.android.testutils.TestUtils;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.util.io.FileUtil;
 import com.intellij.testGuiFramework.framework.RestartUtilsKt;
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Paths;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 import org.junit.rules.TestWatcher;
 import org.junit.runner.Description;
 
@@ -35,40 +32,17 @@ public class AspectsAgentLogger extends TestWatcher {
 
   private static final Logger LOGGER = Logger.getInstance(AspectsAgentLogger.class);
 
-  @Nullable
-  private static File ourAspectsAgentLog;
-
-  static {
-    if (!TestUtils.runningFromBazel()) { // When running from bazel, we don't generate the aspects agent log.
-      try {
-        String logPath = Paths.get(GuiTests.getGuiTestRootDirPath().getAbsolutePath(), "system", "log", "aspects_agent_log.txt").toString();
-        ourAspectsAgentLog = new File(logPath);
-        FileUtil.ensureExists(ourAspectsAgentLog.getParentFile());
-        boolean created = ourAspectsAgentLog.createNewFile();
-        if (!created) {
-          LOGGER.warn("Could not create the aspects agent log.");
-        }
-      }
-      catch (IOException e) {
-        LOGGER.warn("Error while creating the aspects agent output log", e);
-      }
-    }
-  }
-
-  @Nullable
-  public static File getAspectsAgentLog() {
-    return ourAspectsAgentLog;
-  }
 
   @Override
   protected void starting(@NotNull Description description) {
-    if (ourAspectsAgentLog == null) {
+    File aspectsAgentLog = AspectsAgentLogUtil.getAspectsAgentLog();
+    if (aspectsAgentLog == null) {
       return;
     }
     if (RestartUtilsKt.isFirstRun()) {
       String testName = String.format("%s.%s", description.getClassName(), description.getMethodName());
       try {
-        FileUtil.writeToFile(ourAspectsAgentLog, String.format("STARTED %s\n", testName), true);
+        FileUtil.writeToFile(aspectsAgentLog, String.format("STARTED %s\n", testName), true);
       }
       catch (IOException e) {
         LOGGER.warn(String.format("Error while writing STARTED tag of test %s to aspects agent log.", testName));
@@ -78,13 +52,14 @@ public class AspectsAgentLogger extends TestWatcher {
 
   @Override
   protected void finished(@NotNull Description description) {
-    if (ourAspectsAgentLog == null) {
+    File aspectsAgentLog = AspectsAgentLogUtil.getAspectsAgentLog();
+    if (aspectsAgentLog == null) {
       return;
     }
     if (RestartUtilsKt.isLastRun()) {
       String testName = String.format("%s.%s", description.getClassName(), description.getMethodName());
       try {
-        FileUtil.writeToFile(ourAspectsAgentLog, String.format("FINISHED %s\n", testName), true);
+        FileUtil.writeToFile(aspectsAgentLog, String.format("FINISHED %s\n", testName), true);
       }
       catch (IOException e) {
         LOGGER.warn(String.format("Error while writing FINISHED tag of test %s to aspects agent log.", testName));
