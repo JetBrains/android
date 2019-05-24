a88bfb6e14a4dae60ed2942e12e86417e101d006
Valery Cherepanov <qumeric@google.com>
2019-05-24 12:55:16 +0100

    Remove ResetParameterAction
    I believe that it makes more harm than good. A few reasons:
    1. Probably almost nobody knows about it.
    2. It doesn't work when we change values ourselves. An example:
        Layout name may be changed as a result of changing activity name.
        Action is disabled in this case and we cannot revert it to default.
    3. It doesn't work well with "Undo" action (Ctrl-Z) which is
       much more common and convenient in my opinion.
    
    Bug: N/A
    Test: Existing tests
    Change-Id: I7ac5c01854f8f82e0a2ed5af7cdbd27790c3cfc4

--- android-uitests/testSrc/com/android/tools/idea/tests/gui/npw/NewActivityTest.java
+++ android-uitests/testSrc/com/android/tools/idea/tests/gui/npw/NewActivityTest.java
@@ -100,7 +100,7 @@ public class NewActivityTest {
   }
 
   @Test
-  public void createActivityWithNonDefaultPackage() throws Exception {
+  public void createActivityWithNonDefaultPackage() {
     myConfigActivity.enterTextFieldValue(ActivityTextField.PACKAGE_NAME, "google.test2");
     myDialog.clickFinish();
 
@@ -111,7 +111,7 @@ public class NewActivityTest {
   }
 
   @Test
-  public void createActivityWithKotlin() throws Exception {
+  public void createActivityWithKotlin() {
     myConfigActivity.setSourceLanguage("Kotlin");
     assertThat(getSavedRenderSourceLanguage()).isEqualTo(Language.KOTLIN);
     assertThat(getSavedKotlinSupport()).isFalse(); // Changing the Render source language should not affect the project default
@@ -160,84 +160,47 @@ public class NewActivityTest {
   }
 
   @Test
-  public void changeActivityNameUndo() throws Exception {
-    // Changing "Activity Name" causes "Title" and "Layout Name" to change
-    myConfigActivity.enterTextFieldValue(ActivityTextField.NAME, "MainActivityTest");
-    assertTextFieldValues("MainActivityTest", "activity_main_test", "MainActivityTest");
-
-    // Undo "Activity Name" (Right-click and choose "Restore default value") should revert all changes
-    myConfigActivity.undoTextFieldValue(ActivityTextField.NAME);
-    assertTextFieldValues(DEFAULT_ACTIVITY_NAME, DEFAULT_LAYOUT_NAME, DEFAULT_ACTIVITY_TITLE);
-
-    myDialog.clickCancel();
-  }
-
-  @Test
-  public void changeLayoutNameUndo() throws Exception {
+  public void changeLayoutName() {
     // Changing "Layout Name" causes "Activity Name" and "Title" to change
     myConfigActivity.enterTextFieldValue(ActivityTextField.LAYOUT, "activity_main_test1");
     assertTextFieldValues("MainTest1Activity", "activity_main_test1", "MainTest1Activity");
 
-    // Undo "Layout Name", should revert all changes
-    myConfigActivity.undoTextFieldValue(ActivityTextField.LAYOUT);
-    assertTextFieldValues(DEFAULT_ACTIVITY_NAME, DEFAULT_LAYOUT_NAME, DEFAULT_ACTIVITY_TITLE);
-
     myDialog.clickCancel();
   }
 
   @Test
-  public void changeTitleNameUndo() throws Exception {
+  public void changeTitleName() {
     // Changing "Title" does not change "Activity Name" or "Layout Name"
     myConfigActivity.enterTextFieldValue(ActivityTextField.TITLE, "Main Activity Test3");
     assertTextFieldValues(DEFAULT_ACTIVITY_NAME, DEFAULT_LAYOUT_NAME, "Main Activity Test3");
 
-    // Undo "Title"
-    myConfigActivity.undoTextFieldValue(ActivityTextField.TITLE);
-    assertTextFieldValues(DEFAULT_ACTIVITY_NAME, DEFAULT_LAYOUT_NAME, DEFAULT_ACTIVITY_TITLE);
-
     myDialog.clickCancel();
   }
 
   @Test
-  public void changeActivityThenLayoutNameUndo() throws Exception {
+  public void changeActivityThenLayoutName() {
     // Changing "Activity Name" causes "Title" and "Layout Name" to change, after that
     // "Activity Name" should be "locked", changing LAYOUT should not update any other field
     myConfigActivity.enterTextFieldValue(ActivityTextField.NAME, "MainActivityTest1");
     myConfigActivity.enterTextFieldValue(ActivityTextField.LAYOUT, "main_activity2");
     assertTextFieldValues("MainActivityTest1", "main_activity2", "MainActivityTest1");
 
-    // Undo "Layout Name" should only undo that field
-    myConfigActivity.undoTextFieldValue(ActivityTextField.LAYOUT);
-    assertTextFieldValues("MainActivityTest1", "activity_main_test1", "MainActivityTest1");
-
-    // Undo "Activity Name" should revert all changes
-    myConfigActivity.undoTextFieldValue(ActivityTextField.NAME);
-    assertTextFieldValues(DEFAULT_ACTIVITY_NAME, DEFAULT_LAYOUT_NAME, DEFAULT_ACTIVITY_TITLE);
-
     myDialog.clickCancel();
   }
 
   @Test
-  public void changeActivityThenTitleNameUndo() throws Exception {
+  public void changeActivityThenTitleName() {
     // Changing "Activity Name", then "Title", then "Activity Name" again. "Title" should not update since it's been manually modified.
     myConfigActivity.enterTextFieldValue(ActivityTextField.NAME, "MainActivityTest1");
     myConfigActivity.enterTextFieldValue(ActivityTextField.TITLE, "Main Activity Test3");
     myConfigActivity.enterTextFieldValue(ActivityTextField.NAME, "MainActivityTest123");
     assertTextFieldValues("MainActivityTest123", "activity_main_test123", "Main Activity Test3");
 
-    // Undo "Activity Name", should update "Activity Name" and LAYOUT, but not the title
-    myConfigActivity.undoTextFieldValue(ActivityTextField.NAME);
-    assertTextFieldValues(DEFAULT_ACTIVITY_NAME, DEFAULT_LAYOUT_NAME, "Main Activity Test3");
-
-    // And undo of "Title" should bring everything to its default values
-    myConfigActivity.undoTextFieldValue(ActivityTextField.TITLE);
-    assertTextFieldValues(DEFAULT_ACTIVITY_NAME, DEFAULT_LAYOUT_NAME, DEFAULT_ACTIVITY_TITLE);
-
     myDialog.clickCancel();
   }
 
   @Test
-  public void projectViewPaneNotChanged() throws Exception {
+  public void projectViewPaneNotChanged() {
     // Verify that after creating a new activity, the current pane on projectView does not change, assumes initial pane is ProjectView
     myDialog.clickFinish();
 
