3195e13f36f88bfc4111099e6367235f249bf347
Jerome Gaillard <jgaillard@google.com>
2019-05-24 18:13:02 +0100

    Create heapdump before and after LayoutEditorMemoryUseTest
    This test seems to be the first one to encounter low memory warning when
    running UI tests. We temporarily add the creation of heapdump at the
    start and end of it to try and figure out what is happening.
    
    Bug: 132898557
    Test: Test code
    Change-Id: I4c1f2ebf7aa78fc79a02cedb2952afc998a67bf7

--- android-uitests/testSrc/com/android/tools/idea/tests/gui/performance/LayoutEditorMemoryUseTest.java
+++ android-uitests/testSrc/com/android/tools/idea/tests/gui/performance/LayoutEditorMemoryUseTest.java
@@ -23,6 +23,7 @@ import com.android.tools.idea.tests.gui.framework.fixture.IdeFrameFixture;
 import com.android.tools.idea.tests.gui.framework.heapassertions.bleak.Bleak;
 import com.android.tools.idea.tests.gui.framework.heapassertions.bleak.UseBleak;
 import com.google.common.collect.ImmutableSet;
+import com.intellij.openapi.application.PathManager;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.testFramework.LeakHunter;
 import com.intellij.testGuiFramework.framework.GuiTestRemoteRunner;
@@ -100,7 +101,7 @@ public class LayoutEditorMemoryUseTest {
     IdeFrameFixture fixture = guiTest.importProjectAndWaitForProjectSyncToFinish("LayoutTest");
 
     warmUp(fixture);
-
+    createHprofDump(PathManager.getLogPath() + "heap-dump/before.hprof");
     if (CAPTURE_HEAP_DUMPS) {
       createHprofDump("/tmp/LayoutEditorMemoryUseTest-before.hprof");
     }
@@ -123,6 +124,8 @@ public class LayoutEditorMemoryUseTest {
       currentCountsInstances = copyMapWithSizeOnly(instancesTracker.getCurrentLeakCounts());
       LOG.info("[Pass " + (i + 1) + "] Potential leaked classes count: " + currentCountsInstances.size());
     }
+
+    createHprofDump(PathManager.getLogPath() + "heap-dump/after.hprof");
 
     if (!currentCountsInstances.isEmpty()) {
       // Leaks have been found. Create a report.
