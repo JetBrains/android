f1484fd9fc4586b7451b4b39356ab4f6ed159fe1
Yuriy Solodkyy <solodkyy@google.com>
2019-05-29 20:24:04 +0100

    Remove shipped sync.
    Bug: n/a
    Test: n/a
    Change-Id: I9dea17374768c6ab578f99ca90803b58770e207d

--- android-test-framework/testSrc/com/android/tools/idea/testing/TestProjectPaths.java
+++ android-test-framework/testSrc/com/android/tools/idea/testing/TestProjectPaths.java
@@ -87,7 +87,6 @@ public final class TestProjectPaths {
   public static final String TEST_ARTIFACTS_MULTIPROJECT = "projects/testArtifacts/multiproject";
   public static final String TEST_ARTIFACTS_OLD_TESTS = "projects/testArtifacts/oldTests";
   public static final String TEST_ARTIFACTS_SAME_NAME_CLASSES = "projects/testArtifacts/sameNameClasses";
-  public static final String TEST_NOSYNCBUILDER = "projects/testNosyncbuilder";
   public static final String TEST_ONLY_MODULE = "projects/testOnlyModule";
   public static final String TEST_RESOURCES = "projects/testResources";
   public static final String TRANSITIVE_DEPENDENCIES = "projects/transitiveDependencies";
