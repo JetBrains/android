da00edf0f425b5ea538024bcb3756c2d46b941ac
Kainan Wang <knwang@google.com>
2019-05-29 11:21:54 -0700

    Search data binding layout across modules for include tag
    This CL goes through the facet itself and all facets it depends
    to find the binding layout under include tag.
    This CL also includes a new test case that can run data binding
    through different modules.
    
    Fixes: 132367955
    Test: DataBindingCrossModuleTest
    Change-Id: Ie967ecfbffb8898a346b412592befe13553c98ab

--- android-lang-databinding/testData/projects/projectWithDataBindingAndroidX/app/src/main/java/com/android/example/appwithdatabinding/MainActivity.java
+++ android-lang-databinding/testData/projects/projectWithDataBindingAndroidX/app/src/main/java/com/android/example/appwithdatabinding/MainActivity.java
@@ -16,6 +16,7 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         binding.regularView.getId();
+        binding.included.setStr("x");
     }
 
     @Override
