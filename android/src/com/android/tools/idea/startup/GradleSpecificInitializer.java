86d5875b322541aa6f4e01fca924a62724ebf35d
Dmitry Jemerov <yole@jetbrains.com>
2019-05-30 18:49:18 +0200

    Show single progress indicator when reopening project on startup
    GitOrigin-RevId: 485c41338e3ca62f0372321ad1fa461e30b6c23f

--- android/src/com/android/tools/idea/startup/GradleSpecificInitializer.java
+++ android/src/com/android/tools/idea/startup/GradleSpecificInitializer.java
@@ -19,13 +19,10 @@ import com.android.annotations.VisibleForTesting;
 import com.android.prefs.AndroidLocation;
 import com.android.sdklib.IAndroidTarget;
 import com.android.tools.idea.actions.*;
-import com.android.tools.idea.fd.actions.HotswapAction;
-import com.android.tools.idea.flags.StudioFlags;
 import com.android.tools.idea.gradle.actions.AndroidTemplateProjectSettingsGroup;
 import com.android.tools.idea.gradle.actions.AndroidTemplateProjectStructureAction;
 import com.android.tools.idea.npw.PathValidationResult;
 import com.android.tools.idea.npw.PathValidationResult.WritableCheckMode;
-import com.android.tools.idea.run.ApplyChangesAction;
 import com.android.tools.idea.sdk.AndroidSdks;
 import com.android.tools.idea.sdk.IdeSdks;
 import com.android.tools.idea.sdk.wizard.SdkQuickfixUtils;
@@ -50,6 +47,7 @@ import com.intellij.openapi.projectRoots.Sdk;
 import com.intellij.openapi.projectRoots.SdkModificator;
 import com.intellij.openapi.roots.OrderRootType;
 import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.openapi.wm.IdeFrame;
 import com.intellij.psi.codeStyle.CodeStyleScheme;
 import com.intellij.psi.codeStyle.CodeStyleSchemes;
 import com.intellij.psi.codeStyle.CommonCodeStyleSettings;
@@ -126,7 +124,7 @@ public class GradleSpecificInitializer implements Runnable {
 
       app.getMessageBus().connect(app).subscribe(AppLifecycleListener.TOPIC, new AppLifecycleListener() {
         @Override
-        public void appStarting(Project project) {
+        public void appStarting(Project project, IdeFrame frame) {
           app.invokeLater(() -> {
             String message = String.format("%1$s must not be installed in a path containing '!' or Gradle sync will fail!",
                                            ApplicationNamesInfo.getInstance().getProductName());
@@ -239,7 +237,7 @@ public class GradleSpecificInitializer implements Runnable {
 
     app.getMessageBus().connect(app).subscribe(AppLifecycleListener.TOPIC, new AppLifecycleListener() {
       @Override
-      public void appStarting(Project project) {
+      public void appStarting(Project project, IdeFrame frame) {
         app.invokeLater(() -> {
           Notification notification =
             getNotificationGroup().createNotification("SDK Validation", message, NotificationType.WARNING, listener);
