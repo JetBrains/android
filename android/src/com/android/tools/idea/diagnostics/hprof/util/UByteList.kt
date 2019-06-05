ba2ae9dac4dc3753e8b17169196f93a3b8b23214
Radomir Malaczek <radomirm@google.com>
2019-06-05 13:44:01 -0700

    HProf analyzer improvements
    * Introduced AnalysisConfig class which contains all options for
      analysis and report generation.
    
    * Introduced AnalysisContext, which gives access to hprof data and
      holds mutable state of the analysis.
    
    * Refactored report generation.
    
    * Graph traversal now has 5 phases:
      1. Visits graph of all strong references from non stack frame
         GC-roots. References from such objects are more important
         from the object ownership perspective.
      2. Visits strong references from stack frame GC-roots. In the report
         any object visited from such root is known to be solely
         referenced from stack frames.
      3. Visits soft refereced objects.
      4. Visits weak referencd objects.
      5. Visits Cleaner/Finalizer references.
    
    * Report can now show field name for parent-child relationship.
      However, the feature is currently disabled in AnalysisConfig as
      it requires slight tweaking of report generation.
    
    * Reports will now include DirectByteBuffer objects. In that section,
      size of the object includes the size of the native memory buffers.
    
    * During aux files generation, DirectByteBuffer objects will include
      extra information on the size of the native buffer. This is set
      to non-zero only for non-memory mapped file buffers to track
      commited rather than reserved memory use.
    
    * References from soft/weak reference object are now annotated in the
      report.
    
    * New status "!" indicates report lines with a disposed object or
      a soft/weak references.
    
    * Tweaked how object size is calculated.
    
    * Stack frame GC-roots will now include full frame information
      (class/method name).
    
    * Refactored initial HProf passes. Removed a separate pass to get
      string information. Now all hprof strings are loaded, but
      only ones with a strong reference at the end of hprof passes
      will be kept alive.
      This allowed for some code simplification.
    
    * HProf visitors now assume string in HProf file is defined
      before it is referenced. Same for thread information - roots
      referencing a thread should show up after THREAD_INFO field.
    
    * HProfWriter now generates valid HProf files
    
    * Added DebugVisitor that dumps hrpof content in human readable form.
    
    * New HProfBuilder class helps with creation of small HProf files
      with a serialized data of given live instances. See
      HeapAnalysisTest.testPathsThroughDifferentFields test
      for a typical use of the builder.
    
    Test: Added HeapAnalysisTest class
    Fixes: N/A
    Change-Id: I4ce2c4872a91e34d8e910e19b15cf23bce2c53a9

--- android/src/com/android/tools/idea/diagnostics/hprof/util/UByteList.kt
+++ android/src/com/android/tools/idea/diagnostics/hprof/util/UByteList.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018 The Android Open Source Project
+ * Copyright (C) 2019 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,8 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.android.tools.idea.diagnostics.hprof.classstore
+package com.android.tools.idea.diagnostics.hprof.util
 
-import com.android.tools.idea.diagnostics.hprof.parser.Type
-
-class InstanceField(val name: String, val offset: Int, val type: Type)
+interface UByteList {
+  operator fun get(index: Int): Int
+  operator fun set(index: Int, value: Int)
+}
