3de00848fcfca75e1ffd6a058110e2dcb75c5f38
Dmitry Jemerov <yole@jetbrains.com>
2019-06-12 18:58:55 +0200

    Don't show "Experimental" configurable on top level in IDEA
    GitOrigin-RevId: af264c12af07c273e6120db01bceb21a65e3e742

---
+++ android/src/com/android/tools/idea/flags/StudioExperimentalConfigurableProvider.kt
@@ -0,0 +1,27 @@
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.android.tools.idea.flags
+
+import com.android.tools.idea.IdeInfo
+import com.intellij.openapi.options.Configurable
+import com.intellij.openapi.options.ConfigurableProvider
+import com.intellij.openapi.project.Project
+
+// In Android Studio, "Experimental" is a top-level configurable
+class StudioExperimentalConfigurableProvider(private val myProject: Project) : ConfigurableProvider() {
+
+  override fun createConfigurable(): Configurable? {
+    return ExperimentalSettingsConfigurable(myProject)
+  }
+
+  override fun canCreateConfigurable() = IdeInfo.getInstance().isAndroidStudio
+}
+
+// In Android plugin, "Experimental" is shown under "Languages and Frameworks"
+class PluginExperimentalConfigurableProvider(private val myProject: Project) : ConfigurableProvider() {
+
+  override fun createConfigurable(): Configurable? {
+    return ExperimentalSettingsConfigurable(myProject)
+  }
+
+  override fun canCreateConfigurable() = !IdeInfo.getInstance().isAndroidStudio
+}
