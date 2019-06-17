98b00177c48187af1bceb1e1e830349d68a7f60e
Michal Bendowski <bendowski@google.com>
2019-06-17 10:44:49 +0100

    Decrease memory usage for AAR light R classes
    - Only compute the available fields when the class is actually used.
    Most projects don't use R classes from libraries at all, so that's an
    obvious optimization to make.
    - Don't hold on to the full SymbolTable once parsed, instead extract
    just the field names and release the SymbolTable, which stores more
    information than we need.
    - Synchronize in getInnerClasses, to make sure a given symbol table is
    only parsed and allocated once. This is something CachedValuesManager
    doesn't guarantee, which means all threads that try to resolve the light
    field end up doing IO and allocating MB of memory.
    
    Test: existing tests should still pass, no new functionality
    Bug: 134703700
    Change-Id: Idc1dfd5cba883c5dde2054f46ece6568d2530d6c

--- android/src/com/android/tools/idea/res/AndroidClassWithOnlyInnerClassesBase.java
+++ android/src/com/android/tools/idea/res/AndroidClassWithOnlyInnerClassesBase.java
@@ -93,7 +93,7 @@ public abstract class AndroidClassWithOnlyInnerClassesBase extends AndroidLightC
 
   @NotNull
   @Override
-  public final PsiClass[] getInnerClasses() {
+  public PsiClass[] getInnerClasses() {
     return myClassCache.getValue();
   }
 
