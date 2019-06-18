24fd6d5672b5d78068827a2d02d6d351948633fd
Ying Chen <chying@google.com>
2019-06-17 18:02:22 -0700

    Rename and move Java Library Plugin
    Java library plugin will soon be deprecated due to merging
    of sync infrastructures, but some model builers such as
    GradlePluginModel are still useful, also new model builers
    for sources and javadoc will be added.
    
    So these modules are renamed to android.gradle-tooling.api/impl
    to reflect that it's a general purpose tooling extension used
    by studio.
    Also move the modules to tools/adt/idea since they will always
    be bundled with Studio.
    
    Bug: 129008025
    Test: No code change, existing tests.
    Change-Id: Iff5d45087f038bcae25b05f1d815f46edec6b631

---
+++ gradle-tooling/studio-gradle-tooling-impl/src/com/android/java/model/builder/JavaLibraryPlugin.kt
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.java.model.builder
+
+import javax.inject.Inject
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.tooling.provider.model.ToolingModelBuilderRegistry
+
+/** Custom plugin for Java Library.  */
+class JavaLibraryPlugin @Inject
+internal constructor(private val registry: ToolingModelBuilderRegistry) : Plugin<Project> {
+
+  override fun apply(project: Project) {
+    registry.register(JavaModelBuilder())
+    registry.register(ArtifactModelBuilder())
+    registry.register(GradlePluginModelBuilder())
+  }
+}
