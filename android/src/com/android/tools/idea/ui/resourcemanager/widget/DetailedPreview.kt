0379a4c7957e84d87aa32f29b475f7bd309d3171
Oscar Adame <oscarad@google.com>
2019-06-21 10:37:31 -0700

    [Res Picker] Add DetailedPreview to the ResourcePickerDialog
    Adds DetailedPreview to the ResourceExplorerView when it's used for
    resource-picking.
    
    This detailed preview includes some information about the
    ResourceAssetSet being picked. E.g: may display a preview, basic
    metadata like resource name and reference and the configuration/values
    availbale for that resource set.
    
    Changed image cache logic for drawables/mipmap/layout, it should now
    request a new render for higher resolutions, and does a high quality
    scale down for lower resolutions. Uses more memory but reduces the
    jankinnes and stutter when changing grid/list view or zomming-in/out,
    should also make it possible to eventually share just one instance of
    ImageCache for ResourceManager and ResourcePicker (+DetailedPreview).
    As it stands, with this CL, there's a chance to use up to 410MB if
    ResourceManager and ResourcePicker are both open.
    
    Bug: 133323508
    Test: ResourceExplorerViewTest
    Change-Id: Iee18b9c1892735c47fb9678f6e6ff23d838149ff

--- android/src/com/android/tools/idea/ui/resourcemanager/widget/DetailedPreview.kt
+++ android/src/com/android/tools/idea/ui/resourcemanager/widget/DetailedPreview.kt
@@ -17,45 +17,89 @@ package com.android.tools.idea.ui.resourcemanager.widget
 
 import com.intellij.ui.JBColor
 import com.intellij.ui.components.JBLabel
+import com.intellij.ui.components.JBScrollPane
 import com.intellij.ui.table.JBTable
+import com.intellij.util.ui.JBDimension
 import com.intellij.util.ui.JBUI
 import com.intellij.util.ui.UIUtil
 import java.awt.BorderLayout
+import java.awt.Dimension
 import java.util.Vector
+import javax.swing.BorderFactory
+import javax.swing.Box
 import javax.swing.BoxLayout
 import javax.swing.Icon
 import javax.swing.JPanel
+import javax.swing.SwingConstants
+import javax.swing.table.DefaultTableCellRenderer
 import javax.swing.table.DefaultTableModel
+import javax.swing.table.TableModel
+import kotlin.properties.Delegates
+
+private const val PREVIEW_BOTTOM_MARGIN = 10
 
 class DetailedPreview : JPanel(null) {
+  companion object {
+    const val PREVIEW_ICON_SIZE = 200
+  }
 
   private val label = JBLabel(null, JBLabel.CENTER)
+  private val valuesTableModel = object : DefaultTableModel(0, 2) {
+    override fun isCellEditable(row: Int, column: Int) = false
+  }
   private val tableModel = object : DefaultTableModel(0, 2) {
     override fun isCellEditable(row: Int, column: Int) = false
   }
 
-  var data: Map<String, String> = emptyMap()
-    set(value) {
-      field = value
-      tableModel.rowCount = value.size
-      tableModel.setDataVector(
-        value
-          .map { (key, value) ->
-            Vector<String>(2).apply {
-              addElement("$key:")
-              addElement(value)
-            }
-          }
-          .toCollection(Vector<Vector<String>>(value.size)), Vector<String>(listOf("", "")))
+  /**
+   * A metadata map. Displays the values in the form of ["Key:" "Value"].
+   */
+  var data: Map<String, String> by Delegates.observable(emptyMap()) { _, _, newValue ->
+      tableModel.rowCount = newValue.size
+      tableModel.setTableData(newValue)
       metadataTable.revalidate()
       metadataTable.repaint()
-    }
+  }
 
+  /**
+   * A configuration/value map. Displays the map on a table with a header, reads "Configuration" for the keys, and "Value" for the values.
+   */
+  var values: Map<String, String> by Delegates.observable(emptyMap()) { _, _, newValue ->
+      valuesContainer.isVisible = newValue.isNotEmpty()
+      valuesTableModel.setTableData(newValue, "Configuration", "Value")
+
+      // Make the scrollpane take the size of its content, instead of trying to take available space.
+      // Once the layout takes the all the space, the scrollpane should allow to scroll.
+      // TODO: Find a way to achieve this with layout managers instead.
+      val tableHeight = valuesTable.tableHeader.preferredSize.height + (valuesTable.rowHeight * valuesTable.rowCount)
+      valuesContainer.maximumSize = Dimension(Int.MAX_VALUE, tableHeight)
+      valuesContainer.preferredSize = Dimension(valuesTable.preferredScrollableViewportSize.width, tableHeight)
+      valuesContainer.revalidate()
+      valuesContainer.repaint()
+  }
+
+  /**
+   * An icon to preview. Will try to paint the icon centered over a chessboard with size: [PREVIEW_ICON_SIZE] (won't attempt to scale).
+   */
   var icon: Icon? = null
     set(value) {
       field = value
       label.icon = value
+      iconPreviewContainer.isVisible = value != null
     }
+
+  private val iconPreviewContainer = ChessBoardPanel(BorderLayout()).apply {
+    isVisible = false
+    alignmentX = LEFT_ALIGNMENT
+    border = BorderFactory.createCompoundBorder(JBUI.Borders.emptyBottom(PREVIEW_BOTTOM_MARGIN),
+                                                JBUI.Borders.customLine(JBColor.border(), 1))
+    showChessboard = true
+    val previewContainerHeight = PREVIEW_ICON_SIZE + PREVIEW_BOTTOM_MARGIN
+    preferredSize = JBUI.size(PREVIEW_ICON_SIZE, previewContainerHeight)
+    minimumSize = JBUI.size(0, previewContainerHeight)
+    maximumSize = JBUI.size(2000, previewContainerHeight)
+    add(label)
+  }
 
   private val metadataTable = JBTable(tableModel).apply {
     alignmentX = LEFT_ALIGNMENT
@@ -65,26 +109,49 @@ class DetailedPreview : JPanel(null) {
     setShowGrid(false)
   }
 
+  private val valuesTable = JBTable(valuesTableModel).apply {
+    alignmentX = LEFT_ALIGNMENT
+    fillsViewportHeight = false
+    tableHeader.reorderingAllowed = false
+    (tableHeader.defaultRenderer as? DefaultTableCellRenderer)?.let { headerRenderer ->
+      headerRenderer.horizontalAlignment = SwingConstants.LEFT
+    }
+    rowHeight = JBUI.scale(28)
+    rowMargin = JBUI.scale(8)
+    background = JBColor.white
+    showVerticalLines = true
+    showHorizontalLines = false
+  }
+
+  private val valuesContainer =
+    JBScrollPane(valuesTable).apply {
+      alignmentX = LEFT_ALIGNMENT
+      border = JBUI.Borders.empty()
+      isVisible = false
+      isOpaque = false
+    }
+
   init {
     layout = BoxLayout(this, BoxLayout.Y_AXIS)
-
     add(JBLabel("Preview").apply {
       horizontalAlignment = JBLabel.LEFT
       alignmentX = LEFT_ALIGNMENT
       border = JBUI.Borders.empty(8, 0)
     })
-
-    add(ChessBoardPanel(BorderLayout()).apply {
-      alignmentX = LEFT_ALIGNMENT
-      border = JBUI.Borders.customLine(JBColor.border(), 1)
-      showChessboard = true
-      preferredSize = JBUI.size(300, 300)
-      minimumSize = JBUI.size(0, 200)
-      maximumSize = JBUI.size(2000, 200)
-      add(label)
-    })
-
+    add(iconPreviewContainer)
     add(metadataTable)
+    add(Box.createRigidArea(JBDimension(0, 10)))
+    add(valuesContainer)
   }
+}
 
+private fun DefaultTableModel.setTableData(dataMap: Map<String, String>, firstColumnName: String = "", secondColumnName: String = "") {
+  this.setDataVector(
+    dataMap
+      .map { (key, value) ->
+        Vector<String>(2).apply {
+          addElement("$key:")
+          addElement(value)
+        }
+      }.toCollection(Vector<Vector<String>>(dataMap.size)), Vector<String>(listOf(firstColumnName, secondColumnName)))
 }
