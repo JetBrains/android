9301e357b7b63da7eaf095525a1a20b1c3f5d867
diegoperez <diegoperez@google.com>
2019-06-24 13:29:27 +0100

    Compose preview prototype
    Bug: 136635026
    Test: Manually checked. Tests will be added separately
    Change-Id: I0f3029d07a0a857eca863e8a38be8311fabf88b7

--- android-common/src/com/android/tools/idea/flags/StudioFlags.java
+++ android-common/src/com/android/tools/idea/flags/StudioFlags.java
@@ -679,5 +679,13 @@ public final class StudioFlags {
     "If enabled, a notification will be shown",true);
   //endregion
 
-  private StudioFlags() {}
+  //region Compose
+  private static final FlagGroup COMPOSE = new FlagGroup(FLAGS, "compose", "Compose");
+  public static final Flag<Boolean> COMPOSE_PREVIEW = Flag.create(
+    COMPOSE, "compose.preview.enabled", "Enable the Compose preview",
+    "If enabled, a visual preview will be available for Compose.",
+    false);
+  //endregion
+
+  private StudioFlags() { }
 }
