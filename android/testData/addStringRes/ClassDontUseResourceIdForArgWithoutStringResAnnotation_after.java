b884f50054d2d49e6eea371f75b569420006350c
kovalp <kovalp@google.com>
2019-06-25 17:42:49 +0100

    Handle @StringRes from AndroidX in AndroidAddStringResourceAction correctly
    Fixes: 130261280
    Test: AndroidAddStringResourceActionTest
    Change-Id: Ia877f399cd3cb3e01a454d6c5e162e0b142c1c8b

--- android/testData/addStringRes/ClassDontUseResourceIdForArgWithoutStringResAnnotation_after.java
+++ android/testData/addStringRes/ClassDontUseResourceIdForArgWithoutStringResAnnotation_after.java
@@ -1,12 +1,13 @@
 package p1.p2.p3;
 
+import java.lang.StringBuilder
 import android.content.Context;
-import android.widget.Toast;
 
 import p1.p2.R;
 
 public class Class {
   public void f(Context context) {
-    Toast.makeText(context, R.string.hello, Toast.LENGTH_SHORT).show();
+    StringBuilder sb = new StringBuilder()
+    sb.append(context.getString(R.string.hello))
   }
 }
