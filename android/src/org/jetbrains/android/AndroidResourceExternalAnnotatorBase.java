2826403749a4ac5e803c68e0f52f7b908c923fb4
Charlie Tsai <chartsai@google.com>
2019-06-25 12:21:59 +0100

    Add Resource tab to Color Picker
    This is a prototyping feature which provides the
    resources in color picker.
    
    Bug: 128634180
    Test: manually and ColorResourceModelTest
    Change-Id: I4c74fec73c2f60f41c100a303b2c9ce1adcb0cc2
    Exempt-From-Owner-Approval: This CL doesn't relate to owners.

--- android/src/org/jetbrains/android/AndroidResourceExternalAnnotatorBase.java
+++ android/src/org/jetbrains/android/AndroidResourceExternalAnnotatorBase.java
@@ -104,7 +104,7 @@ public abstract class AndroidResourceExternalAnnotatorBase
         // Inline color
         assert (element.getColor() != null);
         Color color = element.getColor();
-        gutterIconRenderer = new AndroidAnnotatorUtil.ColorRenderer(element.getPsiElement(), color, true);
+        gutterIconRenderer = new AndroidAnnotatorUtil.ColorRenderer(element.getPsiElement(), color, true, configuration);
       }
       if (gutterIconRenderer != null) {
         rendererMap.put(element.getPsiElement(), gutterIconRenderer);
@@ -121,7 +121,7 @@ public abstract class AndroidResourceExternalAnnotatorBase
                                                                 @NotNull Configuration configuration) {
     ResourceType type = reference.getResourceType();
     if (type == ResourceType.COLOR) {
-      return getColorGutterIconRenderer(resolver, reference, facet, element);
+      return getColorGutterIconRenderer(resolver, reference, facet, element, configuration);
     }
     else {
       assert type == ResourceType.DRAWABLE || type == ResourceType.MIPMAP;
@@ -153,7 +153,8 @@ public abstract class AndroidResourceExternalAnnotatorBase
   private static GutterIconRenderer getColorGutterIconRenderer(@NotNull ResourceResolver resourceResolver,
                                                                @NotNull ResourceReference reference,
                                                                @NotNull AndroidFacet facet,
-                                                               @NotNull PsiElement element) {
+                                                               @NotNull PsiElement element,
+                                                               @NotNull Configuration configuration) {
     ResourceValue colorValue = resourceResolver.getResolvedResource(reference);
     Color color = ResourceHelper.resolveColor(resourceResolver, colorValue, facet.getModule().getProject());
     if (color == null) {
@@ -164,7 +165,7 @@ public abstract class AndroidResourceExternalAnnotatorBase
     // For java and kotlin files, we should open color resource picker only and set R.color.[resource_name] to the field.
     // TODO: Open color resource picker for java and kotlin files.
     boolean isClickable = AndroidAnnotatorUtil.getFileType(element) == XmlFileType.INSTANCE;
-    return new AndroidAnnotatorUtil.ColorRenderer(element, color, isClickable);
+    return new AndroidAnnotatorUtil.ColorRenderer(element, color, isClickable, configuration);
   }
 
   @Override
