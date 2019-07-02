45b42543144a10781d544d6d44230a6b75df093f
Teodora Tica <teodoratica@google.com>
2019-07-02 13:51:17 +0100

    Generate Java migration class.
    Added JavaMigrationClassGenerator which generates a file containing the
    Migration class that produces the update between two versions of a
    Room database.
    The generation is performed by selecting two JSON
    files which contain the description of the two versions of the database
    and by selecting the GenerateRoomMigration action from the actions menu.
    
    Test: Will add tests for JavaMigrationClassGenerator in a future CL.
    Bug: 136048932
    Change-Id: I70b014929ca8213e59617548d1ef2df03f1f5475

--- room/src/com/android/tools/idea/room/GenerateMigrationAction.kt
+++ room/src/com/android/tools/idea/room/GenerateMigrationAction.kt
@@ -15,12 +15,33 @@
  */
 package com.android.tools.idea.room
 
+import com.android.tools.idea.room.bundle.SchemaBundle
+import com.android.tools.idea.room.generators.JavaMigrationClassGenerator
+import com.android.tools.idea.room.update.DatabaseUpdate
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.command.WriteCommandAction
+import com.intellij.openapi.module.Module
+import com.intellij.openapi.module.ModuleUtilCore
+import com.intellij.openapi.project.rootManager
 import com.intellij.openapi.ui.Messages
+import com.intellij.psi.PsiManager
 
 class GenerateRoomMigrationAction : AnAction("Generate a Room migration") {
+
   override fun actionPerformed(e: AnActionEvent) {
     Messages.showInfoMessage(e.project, "Generating migration", "Room migration generator")
+    val project = e.project ?: return
+    val fileArray = e.getData(CommonDataKeys.VIRTUAL_FILE_ARRAY) ?: return
+    if (fileArray.size == 2 && fileArray[0] != null && fileArray[1] != null) {
+      val module = ModuleUtilCore.findModuleForFile(fileArray[0], project) ?: return
+      val directory = PsiManager.getInstance(project).findDirectory(module.rootManager.sourceRoots[0]) ?: return
+      val oldSchema = SchemaBundle.deserialize(fileArray[0].inputStream)
+      val newSchema = SchemaBundle.deserialize(fileArray[1].inputStream)
+      WriteCommandAction.runWriteCommandAction(project) {
+        JavaMigrationClassGenerator.createMigrationClass(project, directory, DatabaseUpdate(oldSchema.database, newSchema.database))
+      }
+    }
   }
 }
