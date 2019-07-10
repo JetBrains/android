1f4608c68a84e8dddc9e90d10f6aa243d1cee587
Amaury Medeiros <amaurym@google.com>
2019-07-10 14:41:55 +0100

    Enable Go to XML action when using the split editor
    Enables the "Go to XML" action when using the split editor to behave the
    following way:
      * If we're in design mode, change mode to text and move the caret to the
        desired component.
      * If we're in split mode, preserve the mode and updates the caret position
        similarly to above.
    
    Additionally, this CL restores the current "text + preview" behavior of
    updating the cursor of the XML editor, without giving focus to it, when single
    clicking an element in the design portion of the split view. Double-clicking
    an element (layout-editor only) while in split view should update the cursor
    and also give the focus to the text editor.
    
    Bug: 136174865
    Test: NlEditorTest#gotoAction UI test should pass with NELE_SPLIT_EDITOR
          enabled or disabled
    Change-Id: I016f3585a8a7a2a72cb3ecaae5f2b3b8676b41c6

--- designer/src/com/android/tools/idea/common/editor/SplitEditor.java
+++ designer/src/com/android/tools/idea/common/editor/SplitEditor.java
@@ -15,27 +15,42 @@
  */
 package com.android.tools.idea.common.editor;
 
-import com.intellij.openapi.actionSystem.ActionToolbar;
-import com.intellij.openapi.fileEditor.FileEditor;
+import com.intellij.icons.AllIcons;
+import com.intellij.ide.DataManager;
+import com.intellij.openapi.actionSystem.ActionManager;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.Presentation;
+import com.intellij.openapi.actionSystem.ToggleAction;
+import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.fileEditor.FileEditorManager;
 import com.intellij.openapi.fileEditor.TextEditor;
 import com.intellij.openapi.fileEditor.TextEditorWithPreview;
 import com.intellij.openapi.project.Project;
+import com.intellij.pom.Navigatable;
 import com.intellij.util.ArrayUtil;
+import javax.swing.Icon;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 /**
  * {@link TextEditorWithPreview} in which {@link #myPreview} is a {@link DesignerEditor} and {@link #myEditor} contains the corresponding
  * XML file being displayed in the preview.
  */
-public class SplitEditor extends TextEditorWithPreview {
+public class SplitEditor extends TextEditorWithPreview implements TextEditor {
 
   @NotNull
   private final Project myProject;
 
   @NotNull
   private final DesignerEditor myDesignerEditor;
+
+  private final MyToolBarAction myTextViewAction =
+    new MyToolBarAction("Text", AllIcons.General.LayoutEditorOnly, super.getShowEditorAction());
+
+  private final MyToolBarAction myDesignViewAction =
+    new MyToolBarAction("Design", AllIcons.General.LayoutPreviewOnly, super.getShowPreviewAction());
+
+  private final MyToolBarAction mySplitViewAction =
+    new MyToolBarAction("Split", AllIcons.General.LayoutEditorPreview, super.getShowEditorAndPreviewAction());
 
   public SplitEditor(@NotNull TextEditor textEditor,
                      @NotNull DesignerEditor designerEditor,
@@ -47,19 +62,49 @@ public class SplitEditor extends TextEditorWithPreview {
   }
 
   @NotNull
-  public TextEditor getTextEditor() {
-    return myEditor;
-  }
-
-  @NotNull
   public DesignerEditor getDesignerEditor() {
     return myDesignerEditor;
   }
 
-  @Nullable
+  @NotNull
   @Override
-  protected ActionToolbar createToolbar() {
-    return super.createToolbar();
+  protected ToggleAction getShowPreviewAction() {
+    return myDesignViewAction;
+  }
+
+  @NotNull
+  @Override
+  protected ToggleAction getShowEditorAction() {
+    return myTextViewAction;
+  }
+
+  @NotNull
+  @Override
+  protected ToggleAction getShowEditorAndPreviewAction() {
+    return mySplitViewAction;
+  }
+
+  public boolean isSplitMode() {
+    return mySplitViewAction.isSelected(getDummyActionEvent());
+  }
+
+  public boolean isDesignMode() {
+    return myDesignViewAction.isSelected(getDummyActionEvent());
+  }
+
+  public boolean isTextMode() {
+    return myTextViewAction.isSelected(getDummyActionEvent());
+  }
+
+  public void selectTextMode() {
+    myTextViewAction.setSelected(getDummyActionEvent(), true);
+  }
+
+  @NotNull
+  private AnActionEvent getDummyActionEvent() {
+    return new AnActionEvent(null, DataManager.getInstance().getDataContext(getComponent()), "", new Presentation(),
+                             ActionManager.getInstance(),
+                             0);
   }
 
   @Override
@@ -79,6 +124,42 @@ public class SplitEditor extends TextEditorWithPreview {
     // If we are still visible but the user deselected us, do not deactivate the model since we still need to receive updates.
     if (!ArrayUtil.contains(this, FileEditorManager.getInstance(myProject).getSelectedEditors())) {
       myDesignerEditor.getComponent().deactivate();
+    }
+  }
+
+  @NotNull
+  @Override
+  public Editor getEditor() {
+    return myEditor.getEditor();
+  }
+
+  @Override
+  public boolean canNavigateTo(@NotNull Navigatable navigatable) {
+    return myEditor.canNavigateTo(navigatable);
+  }
+
+  @Override
+  public void navigateTo(@NotNull Navigatable navigatable) {
+    myEditor.navigateTo(navigatable);
+  }
+
+  private static class MyToolBarAction extends ToggleAction {
+    @NotNull private final ToggleAction myDelegate;
+
+    MyToolBarAction(@NotNull String name, @NotNull Icon icon, @NotNull ToggleAction delegate) {
+      super(name, name, icon);
+      myDelegate = delegate;
+    }
+
+    @Override
+    public boolean isSelected(@NotNull AnActionEvent e) {
+      return myDelegate.isSelected(e);
+    }
+
+    @Override
+    public void setSelected(@NotNull AnActionEvent e, boolean state) {
+      myDelegate.setSelected(e, state);
+      // TODO(b/136174865): track selection change and hide editor tool windows (e.g. palette) depending on the mode selected.
     }
   }
 }
