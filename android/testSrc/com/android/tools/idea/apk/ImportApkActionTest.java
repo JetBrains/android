48655493079e32575fe33181d2db85038aec21df
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2019-07-10 19:48:57 +0200

    IDEA-217764 Window bounds not saved when closing project
    GitOrigin-RevId: 18e5ff6ddfc48780a4c8ce03425249ebadf8cdf5

--- android/testSrc/com/android/tools/idea/apk/ImportApkActionTest.java
+++ android/testSrc/com/android/tools/idea/apk/ImportApkActionTest.java
@@ -148,11 +148,6 @@ public class ImportApkActionTest extends JavaProjectTestCase {
     }
 
     @Override
-    public String getLastProjectPath() {
-      return null;
-    }
-
-    @Override
     public void removePath(@Nullable String path) {
     }
 
