52bc3a0689ebdabc6f9f6a9004c6df26c4671ac7
David Herman <davidherman@google.com>
2019-07-12 14:59:30 -0700

    Rename BindingComponent to DataBindingComponent
    In a previous CL, we were aggressive about removing "Data" from our
    various "DataBindingXyz" classes, as we were upgrading our codebase to
    be ViewBinding aware.
    
    However, DataBindingComponents are unique to data binding and NOT used
    in view binding at all, so removing "Data" from those class names was
    premature.
    
    Bug: N/A
    Test: N/A, rename only
    Change-Id: Iba1e667e2aaf63414b3ec7097b13367b7f49d4c1

--- databinding/src/com/android/tools/idea/databinding/DataBindingComponentShortNamesCache.java
+++ databinding/src/com/android/tools/idea/databinding/DataBindingComponentShortNamesCache.java
@@ -17,8 +17,8 @@ package com.android.tools.idea.databinding;
 
 import com.android.SdkConstants;
 import com.android.support.AndroidxName;
-import com.android.tools.idea.databinding.finders.BindingComponentClassFinder;
-import com.android.tools.idea.databinding.psiclass.LightBindingComponentClass;
+import com.android.tools.idea.databinding.finders.DataBindingComponentClassFinder;
+import com.android.tools.idea.databinding.psiclass.LightDataBindingComponentClass;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiClass;
 import com.intellij.psi.PsiElementFinder;
@@ -35,7 +35,7 @@ import org.jetbrains.annotations.NotNull;
 /**
  * Cache that stores the DataBindingComponent instances associated with each module.
  *
- * See {@link LightBindingComponentClass}
+ * See {@link LightDataBindingComponentClass}
  *
  * TODO(b/129543644): This class cannot change its name or package location until we remove
  *  hardcoded references to it from the Kotlin plugin.
@@ -57,7 +57,7 @@ final class DataBindingComponentShortNamesCache extends PsiShortNamesCache {
       return PsiClass.EMPTY_ARRAY;
     }
 
-    BindingComponentClassFinder classFinder = PsiElementFinder.EP.findExtensionOrFail(BindingComponentClassFinder.class, project);
+    DataBindingComponentClassFinder classFinder = PsiElementFinder.EP.findExtensionOrFail(DataBindingComponentClassFinder.class, project);
     // we need to search for both old and new. Class finder knows which one to generate.
     final AndroidxName componentClass = SdkConstants.CLASS_DATA_BINDING_COMPONENT;
     final PsiClass[] support = classFinder.findClasses(componentClass.oldName(), scope);
