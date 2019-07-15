4655d49333d99fea08b95694458e1184d667617d
Yuriy Solodkyy <solodkyy@google.com>
2019-07-15 17:09:28 +0100

    Fix editor tab clicking in UI tests.
    Bug: n/a
    Test: ApkViewerTest and NlPreviewTest depend on this.
    Change-Id: Id4da32a91fc0982fcfdf0bfb7d1533af8dfae965

--- uitest-framework/testSrc/com/android/tools/idea/tests/gui/framework/fixture/EditorFixture.java
+++ uitest-framework/testSrc/com/android/tools/idea/tests/gui/framework/fixture/EditorFixture.java
@@ -78,7 +78,7 @@ import com.intellij.ui.components.JBList;
 import com.intellij.ui.components.JBLoadingPanel;
 import com.intellij.ui.tabs.TabInfo;
 import com.intellij.ui.tabs.impl.JBEditorTabs;
-import com.intellij.ui.tabs.impl.TabLabel;
+import com.intellij.ui.tabs.newImpl.TabLabel;
 import java.awt.Component;
 import java.awt.Point;
 import java.io.IOException;
