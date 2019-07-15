40de88b2a34d0fde8791de2f2e468067df8de98a
Teodora Tica <teodoratica@google.com>
2019-07-15 16:27:28 +0100

    Generate a migration class which compiles.
    The migration class is now generated inside the Java sources
    and has a constructor.
    
    Test: Will add a test for JavaMigrationClassGEnerator in a future CL.
    Bug: 136048932
    Change-Id: I89080a4bd5aac7be13903032790992722e3aa3c7

--- room/src/com/android/tools/idea/room/migrations/GenerateMigrationAction.kt
+++ room/src/com/android/tools/idea/room/migrations/GenerateMigrationAction.kt
@@ -22,10 +22,16 @@ import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
 import com.intellij.openapi.command.WriteCommandAction
+import com.intellij.openapi.module.Module
 import com.intellij.openapi.module.ModuleUtilCore
+import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.rootManager
+import com.intellij.openapi.roots.SourceFolder
 import com.intellij.openapi.ui.Messages
+import com.intellij.psi.PsiDirectory
 import com.intellij.psi.PsiManager
+import org.jetbrains.jps.model.java.JavaModuleSourceRootTypes
+import org.jetbrains.jps.model.java.JavaSourceRootType
 
 class GenerateRoomMigrationAction : AnAction("Generate a Room migration") {
 
@@ -36,12 +42,27 @@ class GenerateRoomMigrationAction : AnAction("Generate a Room migration") {
     if (files.size == 2) {
       files.sortBy { it.nameWithoutExtension }
       val module = ModuleUtilCore.findModuleForFile(files[0], project) ?: return
-      val directory = PsiManager.getInstance(project).findDirectory(module.rootManager.sourceRoots[0]) ?: return
+      val directory = getTargetDirectory(project, module) ?: return
       val oldSchema = SchemaBundle.deserialize(files[0].inputStream)
       val newSchema = SchemaBundle.deserialize(files[1].inputStream)
       WriteCommandAction.runWriteCommandAction(project) {
         JavaMigrationClassGenerator.createMigrationClass(project, directory, DatabaseUpdate(oldSchema.database, newSchema.database))
       }
     }
+  }
+
+  private fun getTargetDirectory(project: Project, module: Module): PsiDirectory? {
+    return module.rootManager.contentEntries.asSequence()
+      .flatMap { it.getSourceFolders(JavaSourceRootType.SOURCE).asSequence() }
+      .filterNot { isForGeneratedSources(it) }
+      .mapNotNull { it.file }
+      .firstOrNull()
+      ?.let { PsiManager.getInstance(project).findDirectory(it) }
+  }
+
+  private fun isForGeneratedSources(sourceFolder: SourceFolder): Boolean {
+    val properties = sourceFolder.jpsElement.getProperties(JavaModuleSourceRootTypes.SOURCES)
+    val resourceProperties = sourceFolder.jpsElement.getProperties(JavaModuleSourceRootTypes.RESOURCES)
+    return (properties != null && properties.isForGeneratedSources) || (resourceProperties != null && resourceProperties.isForGeneratedSources)
   }
 }
