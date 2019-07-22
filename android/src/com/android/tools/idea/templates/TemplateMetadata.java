219281dc093fe1b5cfc3e270bfcdb074752df919
Valery Cherepanov <qumeric@google.com>
2019-07-22 18:10:59 +0100

    Remove Kotlin EAP repo from templates
    Because now we ship Kotlin with Studio and it's never EAP
    
    Bug: 132943124
    Test: N/A
    Change-Id: Id82b5e34f09bd9b56279a4225b5c0129d9aba2c8

--- android/src/com/android/tools/idea/templates/TemplateMetadata.java
+++ android/src/com/android/tools/idea/templates/TemplateMetadata.java
@@ -118,8 +118,6 @@ public class TemplateMetadata {
   public static final String ATTR_LANGUAGE = "language"; // Java vs Kotlin
   public static final String ATTR_KOTLIN_VERSION = "kotlinVersion";
   public static final String ATTR_KOTLIN_EAP_REPO = "includeKotlinEapRepo";
-  public static final String ATTR_KOTLIN_EAP_REPO_URL = "kotlinEapRepoUrl";
-  public static final String KOTLIN_EAP_REPO_URL = "https://dl.bintray.com/kotlin/kotlin-eap";
 
   public static final String ATTR_APP_THEME = "applicationTheme";
   public static final String ATTR_APP_THEME_NAME = "name";
