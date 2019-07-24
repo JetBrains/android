6921e59add12db8f84325c4c07aa8b4426084620
Oscar Adame <oscarad@google.com>
2019-07-24 12:41:14 -0700

    [Res Explorer] Update resource creation flow
    Updated the flow to add resources from the resources manager and the
    resource picker. The add [+] button will now display actions to create
    resources specific to the current selected resource tab.
    E.g: If the Drawable resource tab is selected, it will show the options:
    'Drawable resource file', 'Image Asset', 'Vector Asset', 'Import
    Drawables'. But if the Color tab is selected, it will show 'Color
    resource file', 'Color Value'.
    
    Bug: 137756873
    Bug: 138257967
    Fixes: 129947614
    Test: unit tests for resourceExplorerToolbarViewModel
    Change-Id: I0583d0840517e1e7ef497426a4fa122720ee6821

--- android/testSrc/com/android/tools/idea/ui/resourcemanager/explorer/ResourceExplorerToolbarViewModelTest.kt
+++ android/testSrc/com/android/tools/idea/ui/resourcemanager/explorer/ResourceExplorerToolbarViewModelTest.kt
@@ -15,17 +15,29 @@
  */
 package com.android.tools.idea.ui.resourcemanager.explorer
 
+import com.android.resources.ResourceType
 import com.android.tools.idea.testing.AndroidProjectRule
+import com.android.tools.idea.ui.resourcemanager.actions.NewResourceValueAction
 import com.android.tools.idea.ui.resourcemanager.getTestDataDirectory
 import com.android.tools.idea.ui.resourcemanager.importer.ImportersProvider
 import com.android.tools.idea.ui.resourcemanager.model.FilterOptions
 import com.android.tools.idea.util.androidFacet
 import com.google.common.truth.Truth.assertThat
+import com.intellij.openapi.actionSystem.ActionManager
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.CommonDataKeys
 import com.intellij.openapi.actionSystem.LangDataKeys
+import com.intellij.psi.PsiElement
+import com.intellij.psi.impl.file.PsiDirectoryFactory
+import com.intellij.testFramework.runInEdtAndWait
+import org.jetbrains.android.actions.CreateResourceActionBase
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
 
 class ResourceExplorerToolbarViewModelTest {
 
@@ -36,7 +48,8 @@ class ResourceExplorerToolbarViewModelTest {
 
   @Before
   fun setUp() {
-    viewModel = ResourceExplorerToolbarViewModel(rule.module.androidFacet!!, ImportersProvider(), FilterOptions.createDefault())
+    viewModel = ResourceExplorerToolbarViewModel(
+      rule.module.androidFacet!!, ResourceType.DRAWABLE, ImportersProvider(), FilterOptions.createDefault())
     rule.fixture.testDataPath = getTestDataDirectory()
   }
 
@@ -60,5 +73,53 @@ class ResourceExplorerToolbarViewModelTest {
   @Test
   fun getData() {
     assertThat(viewModel.getData(LangDataKeys.MODULE.name)).isEqualTo(rule.module)
+    assertThat(viewModel.getData(CommonDataKeys.PROJECT.name)).isEqualTo(rule.project)
+
+    val resFolder = rule.fixture.copyDirectoryToProject("res/", "res")
+    val drawableDir = resFolder.findChild("drawable")
+    val psiDrawableDir = PsiDirectoryFactory.getInstance(rule.project).createDirectory(drawableDir!!)
+    runInEdtAndWait { assertThat((viewModel.getData(CommonDataKeys.PSI_ELEMENT.name) as PsiElement)).isEqualTo(psiDrawableDir) }
+  }
+
+  @Test
+  fun resourceValueAction() {
+    // For Drawable
+    assertFalse(viewModel.addActions.getChildren(null).hasValueActionOfType(ResourceType.DRAWABLE))
+
+    // For Color
+    viewModel = ResourceExplorerToolbarViewModel(
+      rule.module.androidFacet!!, ResourceType.COLOR, ImportersProvider(), FilterOptions.createDefault())
+    assertTrue(viewModel.addActions.getChildren(null).hasValueActionOfType(ResourceType.COLOR))
+  }
+
+  @Test
+  fun enabledResourceFileAction() {
+    rule.fixture.copyDirectoryToProject("res/", "res")
+    val actions = viewModel.addActions.getChildren(null).toMutableList()
+    // Filter out non create resource actions.
+    actions.removeIf { it !is CreateResourceActionBase }
+    // FIXME: Have to remove the default CreateResourceFileAction (assumed to be at the first index), in reality it should not be enabled
+    //  but it returns as such in this test.
+    actions.removeAt(0)
+    assertThat(actions.size).isGreaterThan(1)
+    val event = AnActionEvent(
+      null, viewModel::getData, "", viewModel.addActions.templatePresentation.clone(), ActionManager.getInstance(), 0)
+    var hasDrawableAction = false
+    runInEdtAndWait {
+      actions.forEach { action ->
+        action.update(event)
+        if (action.templatePresentation.text?.startsWith(ResourceType.DRAWABLE.displayName) == true) {
+          // Search for the create resource action for Drawables and confirm that is enabled.
+          hasDrawableAction = true
+          assertTrue(event.presentation.isEnabled)
+        } else {
+          assertFalse(event.presentation.isEnabled)
+        }
+      }
+    }
+    assertTrue(hasDrawableAction)
   }
 }
+
+private fun Array<AnAction>.hasValueActionOfType(type: ResourceType): Boolean
+  = any { (it is NewResourceValueAction) && (it.templatePresentation.text.startsWith(type.displayName)) }
