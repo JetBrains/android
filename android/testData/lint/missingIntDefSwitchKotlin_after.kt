f4d53ca3675216965f9ba2a307a63540e6560d70
Tor Norbye <tnorbye@google.com>
2019-07-25 14:51:46 -0700

    66890777: no quick-fix for missing "when" cases (works for "switch" cases)
    This CL updates the quickfix which adds case statements to
    switch statements for missing @IntDef constants to also
    handle when statements in Kotlin.
    
    Fixes: 66890777
    Test: Unit test included
    Change-Id: I59042416e2fcd44161d1930ebc21c4f0b1eb4fd7

---
+++ android/testData/lint/missingIntDefSwitchKotlin_after.kt
@@ -0,0 +1,20 @@
+package p1.p2
+
+import android.view.View
+
+class WhenTest {
+    fun measure(mode: Int) {
+        val `val` = View.MeasureSpec.getMode(mode)
+        when (`val`) {
+            View.MeasureSpec.AT_MOST -> {
+            // something
+        }
+            View.MeasureSpec.EXACTLY -> {
+                TODO()
+            }
+            View.MeasureSpec.UNSPECIFIED -> {
+                TODO()
+            }
+        }
+    }
+}
