43ae5e2cbdff1ed6cc6cf0f2a508e137d72c3808
Oscar Adame <oscarad@google.com>
2019-07-25 16:09:39 -0700

    [Res Explorer] Add MoreFontsDialog action to toolbar
    Adds the 'MoreFontsDialog' present in the LayoutEditor property panel to the
    toolbar in the resourceExplorer.
    
    For the resource explorer it will only show Downloadable fonts, since
    it's the only relevant information to add 'new' fonts.
    
    Bug: 138257967
    Test: n/a not much is really being done here
    Change-Id: Idda275d3b19ab4441b0b34e8e2c0c7f1f1dd1ebc

---
+++ android/src/com/android/tools/idea/ui/resourcemanager/actions/AddFontAction.kt
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.ui.resourcemanager.actions
+
+import com.android.tools.idea.fonts.MoreFontsDialog
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import org.jetbrains.android.facet.AndroidFacet
+
+/**
+ * [AnAction] wrapper that calls the MoreFontsDialog to add new font resources.
+ */
+class AddFontAction(private val facet: AndroidFacet): AnAction(MoreFontsDialog.ACTION_NAME, "Add font resources to project", null) {
+  override fun actionPerformed(e: AnActionEvent) {
+    val dialog = MoreFontsDialog(facet, null, false)
+    if (dialog.showAndGet()) {
+      // TODO: Select resource in resource manager / resource picker.
+    }
+  }
+}
