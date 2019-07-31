f5f2429640b4250bab6c7350ef227af7bd53caf7
Valery Cherepanov <qumeric@google.com>
2019-07-31 19:07:51 +0100

    Simplify ImportUIUtil
    Bug: N/A
    Test: tested manually
    Change-Id: I786e2b48726259f63b87d099b85746d3fc1a7748

--- android/src/com/android/tools/idea/npw/importing/ImportUIUtil.kt
+++ android/src/com/android/tools/idea/npw/importing/ImportUIUtil.kt
@@ -18,12 +18,8 @@
 
 package com.android.tools.idea.npw.importing
 
-import com.google.common.base.Functions
-import com.google.common.base.Joiner
-import com.google.common.base.Splitter
-import com.google.common.base.Strings
+import com.android.tools.idea.util.toIoFile
 import com.google.common.collect.Iterables
-import com.intellij.openapi.util.text.StringUtil
 import com.intellij.openapi.vfs.VirtualFile
 
 /**
@@ -39,73 +35,29 @@ import com.intellij.openapi.vfs.VirtualFile
  * @param moreThenThreeElementsMessage message format for over 3 values. Should accept one string and one number.
  * @return formatted message string
  */
-fun formatElementListString(values: Iterable<String>,
-                            oneElementMessage: String,
-                            twoOrThreeElementsMessage: String,
-                            moreThenThreeElementsMessage: String): String {
+fun formatElementListString(
+  values: Iterable<String>, oneElementMessage: String, twoOrThreeElementsMessage: String, moreThenThreeElementsMessage: String
+): String {
   val size = Iterables.size(values)
   return when {
     // If there's 0 elements, some error happened
-    size <= 1 -> String.format(oneElementMessage, Iterables.getFirst(values, "<validation error>"))
-    size <= 3 -> String.format(twoOrThreeElementsMessage, atMostTwo(values, size), Iterables.getLast(values))
-    else -> String.format(moreThenThreeElementsMessage, atMostTwo(values, size), size - 2)
+    size == 0 -> "<validation error>"
+    size == 1 -> oneElementMessage.format(values.first())
+    size <= 3 -> twoOrThreeElementsMessage.format(atMostTwo(values, size), values.last())
+    else -> moreThenThreeElementsMessage.format(atMostTwo(values, size), size - 2)
   }
 }
 
-private fun atMostTwo(names: Iterable<String>, size: Int): String? =
-  Joiner.on(", ").join(Iterables.limit(names, (size - 1).coerceAtMost(2)))
-
-@Deprecated("Replaced by {@link com.android.tools.idea.ui.wizard.WizardUtils#toHtmlString(String)}")
-fun makeHtmlString(templateDescription: String?): String? {
-  var templateDescription = templateDescription
-  if (!StringUtil.isEmpty(templateDescription) && !templateDescription!!.startsWith("<html>")) {
-    templateDescription = String.format("<html>%1\$s</html>", templateDescription.trim())
-  }
-  return templateDescription
-}
+private fun atMostTwo(names: Iterable<String>, size: Int): String =
+  names.take((size - 1).coerceAtMost(2)).joinToString(", ")
 
 /**
- * Returns a relative path string to be shown in the UI. Wizard logic
- * operates with VirtualFile's so these paths are only for user. The paths
- * shown are relative to the file system location user specified, showing
- * relative paths will be easier for the user to read.
+ * Returns a relative path string to be shown in the UI. Wizard logic operates with VirtualFile's so these paths are only for user.
+ * The paths shown are relative to the file system location user specified, showing relative paths will be easier for the user to read.
  */
-internal fun getRelativePath(baseFile: VirtualFile?,
-                             file: VirtualFile?): String {
-  if (file == null) {
-    return ""
-  }
-  val path = file.path
-  return if (baseFile == null) {
-    path
-  }
-  else if (file == baseFile) {
-    "."
-  }
-  else if (!baseFile.isDirectory) {
-    getRelativePath(baseFile.parent, file)
-  }
-  else {
-    val basePath = baseFile.path
-    if (path.startsWith("$basePath/")) {
-      path.substring(basePath.length + 1)
-    }
-    else if (file.fileSystem == baseFile.fileSystem) {
-      val builder = StringBuilder(basePath.length)
-      var prefix: String? = Strings.commonPrefix(path, basePath)
-      if (!prefix!!.endsWith("/")) {
-        prefix = prefix.substring(0, prefix.lastIndexOf('/') + 1)
-      }
-      if (!path.startsWith(basePath)) {
-        val segments: MutableIterable<String?>? = Splitter.on("/").split( basePath.substring(prefix.length))
-        Joiner.on("/").appendTo(builder, Iterables.transform(segments, Functions.constant("..")))
-        builder.append("/")
-      }
-      builder.append(path.substring(prefix.length))
-      builder.toString()
-    }
-    else {
-      path
-    }
-  }
+internal tailrec fun VirtualFile.relativeTo(baseFile: VirtualFile?): String = when {
+  baseFile == null -> path
+  !baseFile.isDirectory -> this.relativeTo(baseFile.parent)
+  fileSystem == baseFile.fileSystem -> toIoFile().relativeTo(baseFile.toIoFile()).path
+  else -> path
 }
