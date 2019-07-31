efe384a612c46663757c23c8aac2778fd438c863
Valery Cherepanov <qumeric@google.com>
2019-07-31 10:35:19 +0100

    Simplify NewBenchmarkModule
    Bug: N/A
    Test: existing
    Change-Id: I37f78517514858d1356bcaf7fea7c2c4fc2405be

--- android/src/com/android/tools/idea/npw/benchmark/NewBenchmarkModuleModel.kt
+++ android/src/com/android/tools/idea/npw/benchmark/NewBenchmarkModuleModel.kt
@@ -26,85 +26,68 @@ import com.android.tools.idea.observable.core.OptionalProperty
 import com.android.tools.idea.observable.core.OptionalValueProperty
 import com.android.tools.idea.observable.core.StringProperty
 import com.android.tools.idea.observable.core.StringValueProperty
-import com.android.tools.idea.templates.TemplateMetadata
+import com.android.tools.idea.templates.TemplateMetadata.ATTR_APP_TITLE
+import com.android.tools.idea.templates.TemplateMetadata.ATTR_IS_LIBRARY_MODULE
+import com.android.tools.idea.templates.TemplateMetadata.ATTR_IS_NEW_PROJECT
 import com.android.tools.idea.templates.TemplateUtils.openEditors
 import com.android.tools.idea.templates.recipe.RenderingContext.Builder
 import com.android.tools.idea.wizard.model.WizardModel
-import com.google.common.collect.Maps
 import com.intellij.openapi.progress.ProgressIndicator
 import com.intellij.openapi.progress.Task.Modal
 import com.intellij.openapi.project.DumbService
 import com.intellij.openapi.project.Project
 import org.jetbrains.android.util.AndroidBundle.message
 import java.io.File
-import java.util.ArrayList
 
 class NewBenchmarkModuleModel(
   val project: Project,
-  private val myTemplateHandle: TemplateHandle,
-  private val myProjectSyncInvoker: ProjectSyncInvoker) : WizardModel() {
-  private val myModuleName: StringProperty = StringValueProperty("benchmark")
-  private val myPackageName: StringProperty = StringValueProperty()
-  private val myLanguage: OptionalProperty<Language> = OptionalValueProperty(getInitialSourceLanguage(project))
-  private val myMinSdk: OptionalProperty<VersionItem> = OptionalValueProperty()
-
-  fun moduleName(): StringProperty {
-    return myModuleName
-  }
-
-  fun packageName(): StringProperty {
-    return myPackageName
-  }
-
-  fun language(): OptionalProperty<Language> {
-    return myLanguage
-  }
-
-  fun minSdk(): OptionalProperty<VersionItem> {
-    return myMinSdk
-  }
+  private val templateHandle: TemplateHandle,
+  private val projectSyncInvoker: ProjectSyncInvoker) : WizardModel() {
+  @JvmField val moduleName: StringProperty = StringValueProperty("benchmark")
+  @JvmField val packageName: StringProperty = StringValueProperty()
+  @JvmField val language: OptionalProperty<Language> = OptionalValueProperty(getInitialSourceLanguage(project))
+  @JvmField val minSdk: OptionalProperty<VersionItem> = OptionalValueProperty()
 
   override fun handleFinished() {
     object : Modal(project, message(
       "android.compile.messages.generating.r.java.content.name"), false) {
       override fun run(indicator: ProgressIndicator) {
-        val modulePaths = createDefaultTemplateAt(myProject.basePath!!, moduleName().get()).paths
-        val myTemplateValues: MutableMap<String, Any>? = Maps.newHashMap()
-        TemplateValueInjector(myTemplateValues!!)
+        val modulePaths = createDefaultTemplateAt(myProject.basePath!!, moduleName.get()).paths
+        val templateValues: MutableMap<String, Any> = mutableMapOf()
+        TemplateValueInjector(templateValues)
           .setProjectDefaults(myProject)
-          .setModuleRoots(modulePaths, myProject.basePath!!, moduleName().get(), packageName().get())
+          .setModuleRoots(modulePaths, myProject.basePath!!, moduleName.get(), packageName.get())
           .setJavaVersion(myProject)
-          .setLanguage(myLanguage.value)
-          .setBuildVersion(myMinSdk.value, myProject)
-        myTemplateValues[TemplateMetadata.ATTR_APP_TITLE] = moduleName().get()
-        myTemplateValues[TemplateMetadata.ATTR_IS_NEW_PROJECT] = false
-        myTemplateValues[TemplateMetadata.ATTR_IS_LIBRARY_MODULE] = true
+          .setLanguage(language.value)
+          .setBuildVersion(minSdk.value, myProject)
+        templateValues[ATTR_APP_TITLE] = moduleName.get()
+        templateValues[ATTR_IS_NEW_PROJECT] = false
+        templateValues[ATTR_IS_LIBRARY_MODULE] = true
         val moduleRoot = modulePaths.moduleRoot!!
-        if (doDryRun(moduleRoot, myTemplateValues)) {
-          render(moduleRoot, myTemplateValues)
+        if (doDryRun(moduleRoot, templateValues)) {
+          render(moduleRoot, templateValues)
         }
       }
     }.queue()
   }
 
-  private fun doDryRun(moduleRoot: File, templateValues: Map<String, Any>): Boolean {
-    return renderTemplate(true, project, moduleRoot, templateValues, null)
-  }
+  private fun doDryRun(moduleRoot: File, templateValues: Map<String, Any>): Boolean =
+    renderTemplate(true, project, moduleRoot, templateValues, null)
 
   private fun render(moduleRoot: File, templateValues: Map<String, Any>) {
-    val filesToOpen: List<File> = ArrayList()
+    val filesToOpen = mutableListOf<File>()
     val success = renderTemplate(false, project, moduleRoot, templateValues, filesToOpen)
     if (success) {
       // calling smartInvokeLater will make sure that files are open only when the project is ready
       DumbService.getInstance(project).smartInvokeLater { openEditors(project, filesToOpen, true) }
-      myProjectSyncInvoker.syncProject(project)
+      projectSyncInvoker.syncProject(project)
     }
   }
 
   private fun renderTemplate(
-    dryRun: Boolean, project: Project, moduleRoot: File, templateValues: Map<String, Any>, filesToOpen: List<File>?
+    dryRun: Boolean, project: Project, moduleRoot: File, templateValues: Map<String, Any>, filesToOpen: MutableList<File>?
   ): Boolean {
-    val template = myTemplateHandle.template
+    val template = templateHandle.template
 
     val context = Builder.newContext(template, project)
       .withCommandName(message("android.wizard.module.new.module.menu.description"))
