6987742833077c1fe096dc79667804e2d25246a1
Christophe Rhodes <xof@google.com>
2019-08-01 13:05:35 +0100

    [Dsl,.kts] add some more now-working tests
    direct translations from Groovy; no parser changes required
    
    Bug: 130224064
    Test: in this CL
    Change-Id: Ic512562af92c642646372c9d7248ffb99c228b08

---
+++ android/testData/parser/buildTypeModel/buildTypeApplicationStatements.gradle.kts
@@ -0,0 +1,25 @@
+android {
+  buildTypes {
+    create("xyz") {
+    }
+  }
+}
+android.buildTypes.getByName("xyz").applicationIdSuffix("mySuffix")
+android.buildTypes.getByName("xyz").buildConfigField("abcd", "efgh", "ijkl")
+android.buildTypes.getByName("xyz").consumerProguardFiles("proguard-android.txt", "proguard-rules.pro")
+android.buildTypes.getByName("xyz").debuggable(true)
+android.buildTypes.getByName("xyz").embedMicroApp(true)
+android.buildTypes.getByName("xyz").jniDebuggable(true)
+android.buildTypes.getByName("xyz").manifestPlaceholders(mapOf("activityLabel1" to "defaultName1", "activityLabel2" to "defaultName2"))
+android.buildTypes.getByName("xyz").minifyEnabled(true)
+android.buildTypes.getByName("xyz").multiDexEnabled(true)
+android.buildTypes.getByName("xyz").proguardFiles("proguard-android.txt", "proguard-rules.pro")
+android.buildTypes.getByName("xyz").pseudoLocalesEnabled(true)
+android.buildTypes.getByName("xyz").renderscriptDebuggable(true)
+android.buildTypes.getByName("xyz").renderscriptOptimLevel(1)
+android.buildTypes.getByName("xyz").resValue("mnop", "qrst", "uvwx")
+android.buildTypes.getByName("xyz").shrinkResources(true)
+android.buildTypes.getByName("xyz").testCoverageEnabled(true)
+android.buildTypes.getByName("xyz").useJack(true)
+android.buildTypes.getByName("xyz").versionNameSuffix("abc")
+android.buildTypes.getByName("xyz").zipAlignEnabled(true)
