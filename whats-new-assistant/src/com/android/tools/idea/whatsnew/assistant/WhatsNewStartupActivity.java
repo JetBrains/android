7ff1e421ba5ce16eee4514bd8ef89b21905993dd
Hannah Chu <hanchu@google.com>
2019-08-02 15:19:45 -0700

    Fix another WNA metrics tool window corner case
    Fix case in which 2 listeners were registered for the same What's New
    Assistant tool window because they were created by different instances
    of WhatsNewAssistantSidePanelAction, which would only occur when the
    panel was auto-opened and then the user opens one again manually.
    
    Bug: 136210409
    Test: N/A
    Change-Id: I4275449d47ba972dc9b5d95bd033f9d5f836be8b

--- whats-new-assistant/src/com/android/tools/idea/whatsnew/assistant/WhatsNewStartupActivity.java
+++ whats-new-assistant/src/com/android/tools/idea/whatsnew/assistant/WhatsNewStartupActivity.java
@@ -15,32 +15,27 @@
  */
 package com.android.tools.idea.whatsnew.assistant;
 
+import com.android.repository.Revision;
 import com.android.tools.analytics.UsageTracker;
+import com.android.tools.idea.IdeInfo;
 import com.android.tools.idea.assistant.AssistantBundleCreator;
 import com.android.tools.idea.flags.StudioFlags;
+import com.android.tools.idea.ui.GuiTestingService;
+import com.google.common.annotations.VisibleForTesting;
 import com.google.wireless.android.sdk.stats.AndroidStudioEvent;
 import com.google.wireless.android.sdk.stats.WhatsNewAssistantEvent;
 import com.intellij.ide.GeneralSettings;
-import com.intellij.openapi.actionSystem.ActionPlaces;
-import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.actionSystem.CommonDataKeys;
-import com.intellij.openapi.actionSystem.DataContext;
-import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.diagnostic.Logger;
-import com.intellij.openapi.startup.StartupActivity;
-
-
-import com.google.common.annotations.VisibleForTesting;
-import com.android.repository.Revision;
-import com.android.tools.idea.IdeInfo;
-import com.android.tools.idea.ui.GuiTestingService;
+import com.intellij.openapi.actionSystem.ActionManager;
 import com.intellij.openapi.application.ApplicationInfo;
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.components.PersistentStateComponent;
 import com.intellij.openapi.components.ServiceManager;
 import com.intellij.openapi.components.State;
 import com.intellij.openapi.components.Storage;
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.DumbAware;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.startup.StartupActivity;
 import com.intellij.util.xmlb.annotations.Tag;
 import org.apache.http.concurrent.FutureCallback;
 import org.jetbrains.annotations.NotNull;
@@ -122,17 +117,8 @@ public class WhatsNewStartupActivity implements StartupActivity, DumbAware {
                                                      .setKind(AndroidStudioEvent.EventKind.WHATS_NEW_ASSISTANT_EVENT)
                                                      .setWhatsNewAssistantEvent(WhatsNewAssistantEvent.newBuilder().setType(
                                                        WhatsNewAssistantEvent.WhatsNewAssistantEventType.AUTO_OPEN)));
-    new WhatsNewAssistantSidePanelAction()
-      .actionPerformed(AnActionEvent.createFromDataContext(ActionPlaces.UNKNOWN, null, new DataContext() {
-        @Nullable
-        @Override
-        public Object getData(@NotNull String dataId) {
-          if (dataId.equalsIgnoreCase(CommonDataKeys.PROJECT.getName())) {
-            return project;
-          }
-          return null;
-        }
-      }));
+    ((WhatsNewAssistantSidePanelAction)ActionManager.getInstance().getAction("WhatsNewAction"))
+      .openWhatsNewSidePanel(project);
   }
 
   @VisibleForTesting
