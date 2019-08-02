64cbc520fe511756b07f15209e15c6d6d531baa9
Dana Dahlstrom <dahlstrom@google.com>
2019-08-02 11:00:00 -0700

    Return Project from GuiTestRule.openProject
    Change-Id: Ice07adbfea1cd2ceac724c755da47296c45e0dbf

--- android-uitests/testSrc/com/android/tools/idea/tests/gui/deploy/DeploymentTest.java
+++ android-uitests/testSrc/com/android/tools/idea/tests/gui/deploy/DeploymentTest.java
@@ -105,7 +105,7 @@ public class DeploymentTest {
     // Start ADB with fake server and its port.
     AndroidDebugBridge.enableFakeAdbServerMode(myAdbServer.getPort());
 
-    myProject = myGuiTest.openProject(PROJECT_NAME).getProject();
+    myProject = myGuiTest.openProject(PROJECT_NAME);
 
     // Get the bridge synchronously, since we're in test mode.
     myBridge = AdbService.getInstance().getDebugBridge(AndroidSdkUtils.getAdb(myProject)).get();
