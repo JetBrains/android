a6972de25286427718c2331f42e3793e0f4f920d
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2019-08-05 16:50:53 +0300

    make RenameHandler#isRenaming default
    GitOrigin-RevId: 669b842e8ef40006b72f826570b8ec8fcbcb9b80

--- android/src/org/jetbrains/android/AndroidRenameHandler.java
+++ android/src/org/jetbrains/android/AndroidRenameHandler.java
@@ -1,3 +1,4 @@
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.android;
 
 import com.android.resources.ResourceFolderType;
@@ -97,11 +98,6 @@ public class AndroidRenameHandler implements RenameHandler, TitledHandler {
     ArrayList<PsiElement> elementList = new ArrayList<>(elements);
     Collections.sort(elementList, AndroidResourceUtil.RESOURCE_ELEMENT_COMPARATOR);
     return elementList.get(0);
-  }
-
-  @Override
-  public boolean isRenaming(@NotNull DataContext dataContext) {
-    return isAvailableOnDataContext(dataContext);
   }
 
   @Override
