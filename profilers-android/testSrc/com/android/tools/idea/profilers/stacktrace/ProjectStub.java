4c113d09fe4dc8270197b352f8cc4f9626ea38e6
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2019-08-08 18:17:01 +0200

    IDEA-CR-51231 introduce getServiceIfCreated
    GitOrigin-RevId: f1558e5fa350303ac92a938776ff02825520b4bb

--- profilers-android/testSrc/com/android/tools/idea/profilers/stacktrace/ProjectStub.java
+++ profilers-android/testSrc/com/android/tools/idea/profilers/stacktrace/ProjectStub.java
@@ -141,7 +141,7 @@ final class ProjectStub extends UserDataHolderBase implements Project {
   }
 
   @Override
-  public <T> T getService(@NotNull Class<T> serviceClass, boolean isCreate) {
+  public <T> T getService(@NotNull Class<T> serviceClass, boolean createIfNeeded) {
     return null;
   }
 
