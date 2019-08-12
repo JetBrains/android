daaa6c9e7c26995c01b6f8ce78788e930ea9ada7
Joshua Gilpatrick <gijosh@google.com>
2019-08-12 11:56:32 -0700

    Add support for Analysis panel and tabs.
    This change adds an analysis panel after a capture has been parsed.
    the panel contains a single analysis of "Full trace". This analysis
    consist of a single tab "Summary". In its current implementation the
    Summary tab is a placeholder.
    
    Test: added.
    Bug: 138408518
    Bug: 138408838
    Change-Id: I1618f48a28b1bf35a7b56d1079df310f8b5d6bf1

--- profilers-ui/src/com/android/tools/profilers/cpu/CpuCaptureStageView.java
+++ profilers-ui/src/com/android/tools/profilers/cpu/CpuCaptureStageView.java
@@ -29,6 +29,8 @@ import java.awt.BorderLayout;
 import java.awt.Component;
 import java.util.HashMap;
 import java.util.Map;
+import com.android.tools.profilers.cpu.analysis.CpuAnalysisPanel;
+import com.intellij.ui.JBSplitter;
 import javax.swing.JComponent;
 import javax.swing.JList;
 import javax.swing.JPanel;
@@ -42,16 +44,23 @@ import org.jetbrains.annotations.NotNull;
 public class CpuCaptureStageView extends StageView<CpuCaptureStage> {
   private static final ProfilerTrackRendererFactory TRACK_RENDERER_FACTORY = new ProfilerTrackRendererFactory();
 
-  @NotNull
   private final JList<TrackGroupModel> myTrackGroupList;
+  private final CpuAnalysisPanel myAnalysisPanel;
+  private final JBSplitter mySplitter = new JBSplitter(false, 0.5f);
 
   public CpuCaptureStageView(@NotNull StudioProfilersView view, @NotNull CpuCaptureStage stage) {
     super(view, stage);
     myTrackGroupList = createTrackGroups(stage.getTrackGroupListModel());
+    myAnalysisPanel = new CpuAnalysisPanel(stage);
     stage.getAspect().addDependency(this).onChange(CpuCaptureStage.Aspect.STATE, this::updateComponents);
     stage.getMinimapModel().getRangeSelectionModel().addDependency(this)
       .onChange(RangeSelectionModel.Aspect.SELECTION, this::updateTrackGroupList);
     updateComponents();
+  }
+
+  @Override
+  public JComponent getToolbar() {
+    return new JPanel();
   }
 
   private void updateComponents() {
@@ -67,17 +76,14 @@ public class CpuCaptureStageView extends StageView<CpuCaptureStage> {
     JPanel container = new JPanel(new BorderLayout());
     container.add(new CpuCaptureMinimapView(getStage().getMinimapModel()).getComponent(), BorderLayout.NORTH);
     container.add(myTrackGroupList, BorderLayout.CENTER);
-    return container;
+    mySplitter.setFirstComponent(container);
+    mySplitter.setSecondComponent(myAnalysisPanel.getComponent());
+    return mySplitter;
   }
 
   private void updateTrackGroupList() {
     // Force JList cell renderer to validate.
     myTrackGroupList.updateUI();
-  }
-
-  @Override
-  public JComponent getToolbar() {
-    return new JPanel();
   }
 
   /**
@@ -105,5 +111,11 @@ public class CpuCaptureStageView extends StageView<CpuCaptureStage> {
   @NotNull
   protected final JList<TrackGroupModel> getTrackGroupList() {
     return myTrackGroupList;
+  }
+
+  @VisibleForTesting
+  @NotNull
+  protected final CpuAnalysisPanel getAnalysisPanel() {
+    return myAnalysisPanel;
   }
 }
