e672c90116162f0bf7138ecbad01fa154270012d
Arsala Bangash <arsala@google.com>
2019-08-12 10:41:15 -0700

    Added model definitions for the Render Audit
    This change introduces data classes for the components that make up a
    Render Audit model. The functions that generate these models will be
    implemented in followup changes.
    
    AtraceFrames now keep track of their slices so that we can access their
    main and render thread slices for RenderPipeline breakdowns.
    
    Bug: 137131316
    Test: Added testStoringSlices in AtraceFrameTest
    
    Change-Id: I74b41567a348ae9e64739f314963a3e6b8ccd36f

--- profilers/src/com/android/tools/profilers/cpu/audits/RenderAuditModel.kt
+++ profilers/src/com/android/tools/profilers/cpu/audits/RenderAuditModel.kt
@@ -15,9 +15,49 @@
  */
 package com.android.tools.profilers.cpu.audits
 
+import com.android.tools.adtui.model.Range
 import com.android.tools.profilers.cpu.atrace.AtraceCpuCapture
 
 /**
  * Model for the RenderAudit UI Component. Holds state and dispatches events to other models.
  */
-class RenderAuditModel(capture: AtraceCpuCapture)
+class RenderAuditModel(capture: AtraceCpuCapture) {
+
+  // Render Audit Data
+  val tripleBuffers: List<TripleBuffer> = listOf()
+  val longFrames: List<LongFrame> = listOf()
+  val skippedFrames: List<SkippedFrame> = listOf()
+  val auditFrames: List<AuditFrame> = listOf()
+  val renderStageStats: Map<RenderStage, RenderStageStats> = mapOf()
+
+  private val mainThreadId: Int = capture.mainThreadId
+  private val renderThreadId: Int = capture.renderThreadId
+
+  // The timeline range for a frame in Us. Used to inspect the frame under other tabs in the capture pane
+  private val frameRange = Range(Double.MAX_VALUE, Double.MIN_VALUE)
+
+  // Used by the view when selecting to inspect the main or render thread for a frame
+  var frameThread: FrameThread = FrameThread.MAIN
+
+  /**
+   * Called by the {@link CpuProfilerStage} to set the {@link CaptureModel}'s selected thread id
+   */
+  fun getFrameThreadId(): Int = if (frameThread == FrameThread.MAIN) mainThreadId else renderThreadId
+
+  fun setFrameRange(minUs: Double, maxUs: Double) {
+    frameRange.set(minUs, maxUs)
+  }
+
+  /**
+   * Returns a copy of the frame range so that we can ensure only this model can mutate the range
+   */
+  fun getFrameRange(): Range {
+    return Range(frameRange)
+  }
+
+  enum class FrameThread {
+    MAIN,
+    RENDER
+  }
+
+}
