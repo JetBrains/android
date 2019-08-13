589bc6ad918e3152640d19d06d137850bcfa1117
Pierfrancesco Soffritti <psoffritti@google.com>
2019-08-13 17:42:36 +0100

    Add support for selecting database from gutter icon.
    If multiple database are open, show a dropdown to select a database when the gutter icon is clicked.
    
    Bug: NA
    Test: RunSqlQueryAnnotatorTest.kt
    Change-Id: Ie4acf575b958443689c248d088d4df83cc787507

--- android-debuggers/src/com/android/tools/idea/RunSqlQueryAnnotator.kt
+++ android-debuggers/src/com/android/tools/idea/RunSqlQueryAnnotator.kt
@@ -17,6 +17,7 @@ package com.android.tools.idea
 
 import com.android.tools.idea.editors.sqlite.SqliteViewer
 import com.android.tools.idea.lang.androidSql.AndroidSqlLanguage
+import com.android.tools.idea.sqlite.model.SqliteDatabase
 import com.android.tools.idea.sqliteExplorer.SqliteExplorerProjectService
 import com.intellij.icons.AllIcons
 import com.intellij.lang.annotation.AnnotationHolder
@@ -25,9 +26,18 @@ import com.intellij.lang.injection.InjectedLanguageManager
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.editor.markup.GutterIconRenderer
+import com.intellij.openapi.ui.popup.JBPopupFactory
 import com.intellij.psi.PsiElement
+import com.intellij.ui.awt.RelativePoint
 import com.intellij.util.ui.EmptyIcon
+import com.intellij.util.ui.JBUI
+import java.awt.Component
+import java.awt.event.MouseEvent
+import javax.swing.DefaultListCellRenderer
 import javax.swing.Icon
+import javax.swing.JList
+import javax.swing.border.Border
+import javax.swing.border.EmptyBorder
 
 /**
  * Annotator that adds a run action to instances of SQL language in the editor, so that they can be executed in the Sqlite Inspector.
@@ -82,20 +92,74 @@ private data class SqliteQueryGutterIconRenderer(private val element: PsiElement
   override fun getClickAction(): AnAction? {
     if (!sqliteExplorerProjectService.hasOpenDatabase()) return null
 
-    return object : AnAction() {
-      override fun actionPerformed(e: AnActionEvent) {
-        val injectedPsiFile = InjectedLanguageManager.getInstance(element.project).getInjectedPsiFiles(element)
-                                .orEmpty()
-                                .map { it.first }
-                                .firstOrNull { it.language == AndroidSqlLanguage.INSTANCE } ?: return
+    return SqliteQueryGutterIconAction()
+  }
 
-        SqliteExplorerProjectService.getInstance(element.project).runQuery(injectedPsiFile.text)
-      }
+  private inner class SqliteQueryGutterIconAction : AnAction() {
+    override fun actionPerformed(actionEvent: AnActionEvent) {
+      val openDatabases = SqliteExplorerProjectService.getInstance(element.project).getOpenDatabases()
 
-      override fun update(e: AnActionEvent) {
-        super.update(e)
-        e.presentation.isEnabledAndVisible = sqliteExplorerProjectService.hasOpenDatabase()
+      if (openDatabases.isEmpty())
+        return
+
+      val injectedPsiFile = InjectedLanguageManager.getInstance(element.project).getInjectedPsiFiles(element)
+                              .orEmpty()
+                              .firstOrNull { it.first.language == AndroidSqlLanguage.INSTANCE }?.first ?: return
+
+      if (openDatabases.size == 1) {
+        SqliteExplorerProjectService.getInstance(element.project).runQuery(openDatabases.first(), injectedPsiFile.text)
+      } else if (openDatabases.size > 1) {
+        val popupChooserBuilder = JBPopupFactory.getInstance().createPopupChooserBuilder(openDatabases.toList())
+        val popup = popupChooserBuilder
+          .setTitle("Choose database")
+          .setMovable(true)
+          .setRenderer(SqliteQueryListCellRenderer())
+          .withHintUpdateSupply()
+          .setResizable(true)
+          .setItemChosenCallback {
+            SqliteExplorerProjectService.getInstance(element.project).runQuery(it, injectedPsiFile.text)
+          }
+          .createPopup()
+
+        if (actionEvent.inputEvent is MouseEvent) {
+          val point = RelativePoint(actionEvent.inputEvent as MouseEvent)
+          popup.show(point)
+        } else {
+          popup.showInFocusCenter()
+        }
       }
     }
+
+    override fun update(e: AnActionEvent) {
+      super.update(e)
+      e.presentation.isEnabledAndVisible = sqliteExplorerProjectService.hasOpenDatabase()
+    }
+  }
+}
+
+private class SqliteQueryListCellRenderer : DefaultListCellRenderer() {
+  companion object {
+    private val cellInsets = JBUI.insets(2, 6)
+  }
+
+  private val border = EmptyBorder(cellInsets)
+
+  override fun getBorder(): Border = border
+
+  override fun getIconTextGap(): Int = cellInsets.left
+
+  override fun getIcon(): Icon = AllIcons.RunConfigurations.TestState.Run
+
+  override fun getListCellRendererComponent(list: JList<*>,
+                                            value: Any?,
+                                            index: Int,
+                                            isSelected: Boolean,
+                                            cellHasFocus: Boolean): Component {
+    val component = super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus)
+    if (value is SqliteDatabase) {
+      text = value.name
+    }
+
+    return component
   }
 }
