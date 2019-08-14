b8df7a1dca9bfb7fd90ae744f8f01a9f6267d06b
Sergey Ignatov <sergey.ignatov@jetbrains.com>
2019-08-14 14:26:49 +0300

    speedup action manager a bit: don't load gradle sync invoker
    GitOrigin-RevId: d38871390605b0fd002617b435e56e97a1cbaa89

--- android/src/com/android/tools/idea/gradle/actions/SyncProjectAction.java
+++ android/src/com/android/tools/idea/gradle/actions/SyncProjectAction.java
@@ -22,6 +22,7 @@ import com.google.common.annotations.VisibleForTesting;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.Presentation;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.NotNullLazyValue;
 import org.jetbrains.annotations.NotNull;
 
 import static com.google.wireless.android.sdk.stats.GradleSyncStats.Trigger.TRIGGER_USER_REQUEST;
@@ -30,20 +31,21 @@ import static com.google.wireless.android.sdk.stats.GradleSyncStats.Trigger.TRIG
  * Re-imports (syncs) an Android-Gradle project, without showing the "Import Project" wizard.
  */
 public class SyncProjectAction extends AndroidStudioGradleAction {
-  @NotNull private final GradleSyncInvoker mySyncInvoker;
+  private final NotNullLazyValue<GradleSyncInvoker> mySyncInvoker;
 
   public SyncProjectAction() {
     this("Sync Project with Gradle Files");
   }
 
   protected SyncProjectAction(@NotNull String text) {
-    this(text, GradleSyncInvoker.getInstance());
+    super(text);
+    mySyncInvoker = NotNullLazyValue.createValue(() -> GradleSyncInvoker.getInstance());
   }
 
   @VisibleForTesting
   SyncProjectAction(@NotNull String text, @NotNull GradleSyncInvoker syncInvoker) {
     super(text);
-    mySyncInvoker = syncInvoker;
+    mySyncInvoker = NotNullLazyValue.createConstantValue(syncInvoker);
   }
 
   @Override
@@ -52,7 +54,7 @@ public class SyncProjectAction extends AndroidStudioGradleAction {
     Presentation presentation = e.getPresentation();
     presentation.setEnabled(false);
     try {
-      mySyncInvoker.requestProjectSyncAndSourceGeneration(project, TRIGGER_USER_REQUEST);
+      mySyncInvoker.getValue().requestProjectSyncAndSourceGeneration(project, TRIGGER_USER_REQUEST);
     }
     finally {
       presentation.setEnabled(true);
