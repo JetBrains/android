4270616b087f396f5a33100430324def691f52a5
Karima Ichou <karimai@google.com>
2019-08-14 18:46:24 +0100

    [Kotlin Dsl] support dependencies with blocks.
    This change adds support to dependencies having blocks (ex : for exclude
    statements). When parsing KtCalExpressions, we need to check if a method
    has a blockExpression whether it should be parsed as a block (ex :
    buildType) or not.
    The change also provides support to dslLiterals having a configBlock at
    both the parser and writer stages.
    
    Bug: N/A
    Test: testFiles added for existing tests, more tests should be added in
    future CL for dslMethodCalls with closureBlocks.
    
    Change-Id: I2b164cba8c4f711da16ae3b0a1117977d5bc040e

---
+++ android/testData/parser/moduleDependency/multiTypeApplicationStatementDoesNotThrowException.gradle.kts
@@ -0,0 +1,5 @@
+dependencies {
+  implementation(mapOf("group" to "my.test.dep", "name" to "artifact", "version" to "version")), {
+    exclude(mapOf("module" to "module1"))
+    exclude(mapOf("module" to "module2"))
+  }
