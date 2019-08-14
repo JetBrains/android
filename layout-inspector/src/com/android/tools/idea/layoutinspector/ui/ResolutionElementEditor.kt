6735247d593e3592170f7d1d8eee27c7e0a0cbde
Jens Ole Lauridsen <jlauridsen@google.com>
2019-08-14 12:57:24 -0700

    Change the icons used in the resolution stack.
    Fixes: 137661568
    Test: N/A
    Change-Id: I209ed6866fdb58b343a94e639eba54b92a6c046b

--- layout-inspector/src/com/android/tools/idea/layoutinspector/ui/ResolutionElementEditor.kt
+++ layout-inspector/src/com/android/tools/idea/layoutinspector/ui/ResolutionElementEditor.kt
@@ -24,14 +24,11 @@ import com.android.tools.idea.layoutinspector.resource.SourceLocation
 import com.android.tools.property.panel.api.PropertyEditorModel
 import com.android.tools.property.ptable2.PTableGroupItem
 import com.android.tools.property.ptable2.PTableVariableHeightCellEditor
-import com.intellij.icons.AllIcons
 import com.intellij.ide.ui.laf.darcula.DarculaUIUtil
 import com.intellij.ui.JBColor
 import com.intellij.ui.components.JBLabel
-import com.intellij.util.ui.EmptyIcon
 import com.intellij.util.ui.JBUI
 import com.intellij.util.ui.UIUtil
-import icons.StudioIcons
 import java.awt.BorderLayout
 import java.awt.Component
 import java.awt.Font
@@ -41,7 +38,6 @@ import java.awt.event.MouseAdapter
 import java.awt.event.MouseEvent
 import java.awt.font.TextAttribute
 import javax.swing.BoxLayout
-import javax.swing.Icon
 import javax.swing.JComponent
 import javax.swing.JPanel
 
@@ -87,10 +83,10 @@ class ResolutionElementEditor(
       linkPanel.removeAll()
       val isSelected = editorModel.isUsedInRendererWithSelection
       val isOverridden = property is ResolutionStackItem
-      classLocation?.let { linkPanel.add(SourceLocationLink(it, isSelected, false, null)) }
+      classLocation?.let { linkPanel.add(SourceLocationLink(it, isSelected, false)) }
       when (locations.size) {
         0 -> {}
-        1 -> linkPanel.add(SourceLocationLink(locations.first(), isSelected, isOverridden, null))
+        1 -> linkPanel.add(SourceLocationLink(locations.first(), isSelected, isOverridden))
         else -> linkPanel.add(ExpansionPanel(model, editorModel, property, locations, isSelected, isOverridden))
       }
     }
@@ -112,27 +108,27 @@ class ResolutionElementEditor(
       val mainPanel = JPanel()
       mainPanel.layout = BoxLayout(mainPanel, BoxLayout.X_AXIS)
       mainPanel.background = UIUtil.TRANSPARENT_COLOR
+      mainPanel.border = JBUI.Borders.emptyLeft(8)
       val isExtraPanelVisible = model.isExpanded(property)
       val extraPanel = JPanel()
       extraPanel.layout = BoxLayout(extraPanel, BoxLayout.Y_AXIS)
       extraPanel.background = UIUtil.TRANSPARENT_COLOR
       extraPanel.isVisible = isExtraPanelVisible
-      val expandIcon = JBLabel(if (isExtraPanelVisible) AllIcons.Ide.Notification.Collapse else AllIcons.Ide.Notification.Expand)
-      expandIcon.addMouseListener(object : MouseAdapter() {
+      extraPanel.border = JBUI.Borders.emptyLeft(24)
+      val expandIcon = UIUtil.getTreeNodeIcon(isExtraPanelVisible, isSelected, isSelected)
+      val expandLabel = JBLabel(expandIcon)
+      expandLabel.addMouseListener(object : MouseAdapter() {
         override fun mousePressed(event: MouseEvent?) {
           model.toggle(property)
           editorModel.refresh()
         }
       })
       val mainLocation = locations.first()
-      mainPanel.add(SourceLocationLink(mainLocation, isSelected, isOverridden, null))
-      mainPanel.add(expandIcon)
+      mainPanel.add(expandLabel)
+      mainPanel.add(SourceLocationLink(mainLocation, isSelected, isOverridden))
 
-      // TODO: Replace StudioIcons.LayoutEditor.Palette.INCLUDE with a LayoutInspector icon for first indented source location:
-      var icon = StudioIcons.LayoutEditor.Palette.INCLUDE
       for (index in 1 until locations.size) {
-        extraPanel.add(SourceLocationLink(locations[index], isSelected, isOverridden, icon))
-        icon = EmptyIcon.ICON_16
+        extraPanel.add(SourceLocationLink(locations[index], isSelected, isOverridden))
       }
       add(mainPanel, BorderLayout.CENTER)
       add(extraPanel, BorderLayout.SOUTH)
@@ -145,10 +141,10 @@ class ResolutionElementEditor(
    * Allow the link to be clickable in the table even though the editor is only used for rendering.
    *
    * @param [location] the source location. If [SourceLocation.navigatable] is missing then show the link as normal text.
-   * @param [isOverridden] then the font will use strikeout to indicate the value is overridden.
    * @param [isSelected] then the font color will use the table foreground for selected and focused.
+   * @param [isOverridden] then the font will use strikeout to indicate the value is overridden.
    */
-  private class SourceLocationLink(location: SourceLocation, isSelected: Boolean, isOverridden: Boolean, icon: Icon?): JBLabel(icon) {
+  private class SourceLocationLink(location: SourceLocation, isSelected: Boolean, isOverridden: Boolean): JBLabel() {
 
     init {
       val showAsLink = location.navigatable != null
