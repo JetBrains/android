f7ebe0d85304dcade7d47cd3a74314a248368f26
Ying Chen <chying@google.com>
2019-08-15 15:42:04 -0700

    Fix and simplify Sdk update steps.
    There are multiple places in code base to update Sdks, including
    initial setup, sdk update after Sync, and new packages installed
    by Sdk manager.
    The code are slightly differnt in all of the places.
    This CL simplifies this process by always use the same code to
    update Sdk.
    
    This also fixes some problems with original code, that when sources
    becomes available after the initial setup, javadoc should not keep
    the remote location. Otherwise quick documentation tries to
    fetch from remote location everytime.
    Also, refresh file system when searching for sources and javadoc.
    
    Bug: 130257706
    Test: SdksCleanupStepTest
    Change-Id: If16b5ca7db2135db0149d6f7184c13a14078c6ad

--- android/src/com/android/tools/idea/gradle/project/sync/setup/post/cleanup/SdksCleanupStep.java
+++ android/src/com/android/tools/idea/gradle/project/sync/setup/post/cleanup/SdksCleanupStep.java
@@ -15,14 +15,22 @@
  */
 package com.android.tools.idea.gradle.project.sync.setup.post.cleanup;
 
+import static com.android.SdkConstants.FN_FRAMEWORK_LIBRARY;
+import static com.android.tools.idea.io.FilePaths.toSystemDependentPath;
+import static com.android.tools.idea.project.messages.MessageType.ERROR;
+import static com.android.tools.idea.startup.ExternalAnnotationsSupport.attachJdkAnnotations;
+import static com.intellij.openapi.roots.OrderRootType.CLASSES;
+import static com.intellij.openapi.roots.OrderRootType.SOURCES;
+import static java.util.Arrays.asList;
+
 import com.android.sdklib.AndroidTargetHash;
 import com.android.sdklib.AndroidVersion;
 import com.android.sdklib.IAndroidTarget;
 import com.android.sdklib.repository.AndroidSdkHandler;
 import com.android.tools.idea.gradle.project.sync.hyperlink.InstallPlatformHyperlink;
-import com.android.tools.idea.project.messages.SyncMessage;
 import com.android.tools.idea.gradle.project.sync.messages.GradleSyncMessages;
 import com.android.tools.idea.gradle.project.sync.setup.post.ProjectCleanupStep;
+import com.android.tools.idea.project.messages.SyncMessage;
 import com.android.tools.idea.sdk.AndroidSdks;
 import com.android.tools.idea.sdk.progress.StudioLoggerProgressIndicator;
 import com.google.common.annotations.VisibleForTesting;
@@ -37,28 +45,21 @@ import com.intellij.openapi.projectRoots.Sdk;
 import com.intellij.openapi.projectRoots.SdkModificator;
 import com.intellij.openapi.roots.JavadocOrderRootType;
 import com.intellij.openapi.roots.ModuleRootManager;
+import com.intellij.openapi.roots.OrderRootType;
 import com.intellij.openapi.roots.libraries.ui.OrderRoot;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.openapi.vfs.ex.http.HttpFileSystem;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import org.jetbrains.android.facet.AndroidFacet;
 import org.jetbrains.android.sdk.AndroidSdkAdditionalData;
 import org.jetbrains.android.sdk.AndroidSdkData;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
-import static com.android.SdkConstants.FN_ANNOTATIONS_JAR;
-import static com.android.SdkConstants.FN_FRAMEWORK_LIBRARY;
-import static com.android.tools.idea.project.messages.MessageType.ERROR;
-import static com.android.tools.idea.io.FilePaths.toSystemDependentPath;
-import static com.android.tools.idea.startup.ExternalAnnotationsSupport.attachJdkAnnotations;
-import static com.intellij.openapi.roots.OrderRootType.CLASSES;
-import static com.intellij.openapi.roots.OrderRootType.SOURCES;
-import static org.jetbrains.android.sdk.AndroidSdkType.DEFAULT_EXTERNAL_DOCUMENTATION_PATH;
 
 public class SdksCleanupStep extends ProjectCleanupStep {
   @NotNull private final AndroidSdks myAndroidSdks;
@@ -94,56 +95,71 @@ public class SdksCleanupStep extends ProjectCleanupStep {
       return;
     }
     Sdk sdk = ModuleRootManager.getInstance(module).getSdk();
-    if (sdk != null && !invalidSdks.contains(sdk) && !fixedSdks.contains(sdk)) {
-      AndroidSdkAdditionalData additionalData = myAndroidSdks.getAndroidSdkAdditionalData(sdk);
-      AndroidSdkData sdkData = AndroidSdkData.getSdkData(sdk);
-      if (additionalData != null && sdkData != null) {
-        IAndroidTarget target = additionalData.getBuildTarget(sdkData);
-        if (target == null) {
-          AndroidSdkHandler sdkHandler = sdkData.getSdkHandler();
-          StudioLoggerProgressIndicator logger = new StudioLoggerProgressIndicator(getClass());
-          sdkHandler.getSdkManager(logger).loadSynchronously(0, logger, null, null);
-          target = sdkHandler.getAndroidTargetManager(logger).getTargetFromHashString(additionalData.getBuildTargetHashString(), logger);
-        }
-        if (target != null) {
-          SdkModificator sdkModificator = null;
-          if (isMissingAndroidLibrary(sdk) || shouldRemoveAnnotationsJar(sdk)) {
-            // First try to recreate SDK; workaround for issue 78072
-            sdkModificator = sdk.getSdkModificator();
-            sdkModificator.removeAllRoots();
-            for (OrderRoot orderRoot : myAndroidSdks.getLibraryRootsForTarget(target, toSystemDependentPath(sdk.getHomePath()), true)) {
-              sdkModificator.addRoot(orderRoot.getFile(), orderRoot.getType());
-            }
-            attachJdkAnnotations(sdkModificator);
-          }
-          if (!myAndroidSdks.hasValidDocs(sdk, target)) {
-            if (sdkModificator == null) {
-              sdkModificator = sdk.getSdkModificator();
-            }
-            VirtualFile documentationPath = HttpFileSystem.getInstance().findFileByPath(DEFAULT_EXTERNAL_DOCUMENTATION_PATH);
-            sdkModificator.addRoot(documentationPath, JavadocOrderRootType.getInstance());
-          }
-          if (!hasSources(sdk)) {
-            // See https://code.google.com/p/android/issues/detail?id=233392
-            if (sdkModificator == null) {
-              sdkModificator = sdk.getSdkModificator();
-            }
-            myAndroidSdks.findAndSetPlatformSources(target, sdkModificator);
-          }
-          if (sdkModificator != null) {
-            ApplicationManager.getApplication().invokeAndWait(sdkModificator::commitChanges);
-            fixedSdks.add(sdk);
-          }
-        }
-      }
+    if (sdk == null || invalidSdks.contains(sdk) || fixedSdks.contains(sdk)) {
+      return;
+    }
+    updateSdkIfNeeded(sdk, myAndroidSdks);
+    fixedSdks.add(sdk);
+    // If attempting to fix up the roots in the SDK fails, install the target over again
+    // (this is a truly corrupt install, as opposed to an incorrectly synced SDK which the
+    // above workaround deals with)
+    if (isMissingAndroidLibrary(sdk)) {
+      invalidSdks.add(sdk);
+    }
+  }
 
-      // If attempting to fix up the roots in the SDK fails, install the target over again
-      // (this is a truly corrupt install, as opposed to an incorrectly synced SDK which the
-      // above workaround deals with)
-      if (isMissingAndroidLibrary(sdk)) {
-        invalidSdks.add(sdk);
+  // If the sdk is outdated, then all of its roots will be recreated.
+  // An Sdk is considered outdated if any of the roots are different from expected roots.
+  public static void updateSdkIfNeeded(@NotNull Sdk sdk, @NotNull AndroidSdks androidSdks) {
+    IAndroidTarget target = getTarget(sdk, androidSdks);
+    if (target != null) {
+      updateSdkIfNeeded(sdk, androidSdks, target);
+    }
+  }
+
+  @VisibleForTesting
+  static void updateSdkIfNeeded(@NotNull Sdk sdk, @NotNull AndroidSdks androidSdks, @NotNull IAndroidTarget target) {
+    List<OrderRoot> expectedRoots = androidSdks.getLibraryRootsForTarget(target, toSystemDependentPath(sdk.getHomePath()), true);
+    Map<OrderRootType, Set<String>> urlsByRootType = new HashMap<>();
+    for (OrderRoot root : expectedRoots) {
+      urlsByRootType.computeIfAbsent(root.getType(), k -> new HashSet<>()).add(root.getFile().getUrl());
+    }
+
+    for (OrderRootType type : asList(CLASSES, SOURCES, JavadocOrderRootType.getInstance())) {
+      List<String> urlInSdk = asList(sdk.getRootProvider().getUrls(type));
+      Set<String> expectedUrls = urlsByRootType.getOrDefault(type, Collections.emptySet());
+      if (urlInSdk.size() != expectedUrls.size() || urlInSdk.stream().anyMatch(url -> !expectedUrls.contains(url))) {
+        updateSdk(sdk, expectedRoots);
+        return;
       }
     }
+  }
+
+  @Nullable
+  private static IAndroidTarget getTarget(@NotNull Sdk sdk, @NotNull AndroidSdks androidSdks) {
+    AndroidSdkAdditionalData additionalData = androidSdks.getAndroidSdkAdditionalData(sdk);
+    AndroidSdkData sdkData = AndroidSdkData.getSdkData(sdk);
+    if (additionalData == null || sdkData == null) {
+      return null;
+    }
+    IAndroidTarget target = additionalData.getBuildTarget(sdkData);
+    if (target == null) {
+      AndroidSdkHandler sdkHandler = sdkData.getSdkHandler();
+      StudioLoggerProgressIndicator logger = new StudioLoggerProgressIndicator(SdksCleanupStep.class);
+      sdkHandler.getSdkManager(logger).loadSynchronously(0, logger, null, null);
+      target = sdkHandler.getAndroidTargetManager(logger).getTargetFromHashString(additionalData.getBuildTargetHashString(), logger);
+    }
+    return target;
+  }
+
+  private static void updateSdk(@NotNull Sdk sdk, @NotNull List<OrderRoot> expectedRoots) {
+    SdkModificator sdkModificator = sdk.getSdkModificator();
+    sdkModificator.removeAllRoots();
+    for (OrderRoot orderRoot : expectedRoots) {
+      sdkModificator.addRoot(orderRoot.getFile(), orderRoot.getType());
+    }
+    attachJdkAnnotations(sdkModificator);
+    ApplicationManager.getApplication().invokeAndWait(sdkModificator::commitChanges);
   }
 
   private boolean isMissingAndroidLibrary(@NotNull Sdk sdk) {
@@ -157,38 +173,6 @@ public class SdksCleanupStep extends ProjectCleanupStep {
       }
     }
     return true;
-  }
-
-  /*
-   * Indicates whether annotations.jar should be removed from the given SDK (if it is an Android SDK.)
-   * There are 2 issues:
-   * 1. annotations.jar is not needed for API level 16 and above. The annotations are already included in android.jar. Until recently, the
-   *    IDE added annotations.jar to the IDEA Android SDK definition unconditionally.
-   * 2. Because annotations.jar is in the classpath, the IDE locks the file on Windows making automatic updates of SDK Tools fail. The
-   *    update not only fails, it corrupts the 'tools' folder in the SDK.
-   * From now on, creating IDEA Android SDKs will not include annotations.jar if API level is 16 or above, but we still need to remove
-   * this jar from existing IDEA Android SDKs.
-   */
-  private boolean shouldRemoveAnnotationsJar(@NotNull Sdk sdk) {
-    if (myAndroidSdks.isAndroidSdk(sdk)) {
-      AndroidSdkAdditionalData additionalData = myAndroidSdks.getAndroidSdkAdditionalData(sdk);
-      AndroidSdkData sdkData = AndroidSdkData.getSdkData(sdk);
-      boolean needsAnnotationsJar = false;
-      if (additionalData != null && sdkData != null) {
-        IAndroidTarget target = additionalData.getBuildTarget(sdkData);
-        if (target != null) {
-          needsAnnotationsJar = myAndroidSdks.needsAnnotationsJarInClasspath(target);
-        }
-      }
-      for (VirtualFile library : sdk.getRootProvider().getFiles(CLASSES)) {
-        // This code does not through the classes in the Android SDK. It iterates through a list of 3 files in the IDEA SDK: android.jar,
-        // annotations.jar and res folder.
-        if (library.getName().equals(FN_ANNOTATIONS_JAR) && library.exists() && !needsAnnotationsJar) {
-          return true;
-        }
-      }
-    }
-    return false;
   }
 
   private void reinstallMissingPlatforms(@NotNull Set<Sdk> invalidSdks, @NotNull Project project) {
@@ -215,10 +199,5 @@ public class SdksCleanupStep extends ProjectCleanupStep {
       msg.add(new InstallPlatformHyperlink(versionsToInstall));
       GradleSyncMessages.getInstance(project).report(msg);
     }
-  }
-
-  private static boolean hasSources(@NotNull Sdk sdk) {
-    String[] urls = sdk.getRootProvider().getUrls(SOURCES);
-    return urls.length > 0;
   }
 }
