c6c5044bef66fe02a438badd79591bc73a13a3d8
Amaury Medeiros <amaurym@google.com>
2019-08-16 17:54:07 +0100

    Add animation toolbar for motion layout and animated-vector
    When opening a motion layout file in split editor, we should display an
    animation toolbar (guarded by the NELE_MOTION_LAYOUT_ANIMATIONS flag) on
    the bottom of the visual portion of the editor. The animation toolbar
    should have a seek bar so we can jump to specific portions of the
    animation.
    
    Similarly, when inspecting vector drawables, we should display an animation
    bar as well. The toolbar is guarded by the NELE_ANIMATIONS_PREVIEW flag and
    does not have a seek bar.
    
    This reapplies change I547b2842dc50f6807b46816c8aa5d67fa6a744c9, which
    made GuiTestRuleTest#makeSimpleApplication fail, since it adds a motion
    layout XML file to the SimpleApplication test project. The problem is we
    actually try to make this project, which does not supportmotion/constraint
    layout. In this CL, we moved motion_layout.xml to SimpleAndroidXApplication,
    which is enough to make the tests pass.
    
    Bug: 139007163
    Test: AnimationToolbarTest
    Change-Id: Ib1e57fe71efd862a6ee67469cc444b6c7c79f36c

---
+++ android-uitests/testSrc/com/android/tools/idea/tests/gui/uibuilder/AnimationToolbarTest.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright (C) 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.tests.gui.uibuilder;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.junit.Assert.fail;
+
+import com.android.tools.idea.common.editor.DesignerEditorPanel;
+import com.android.tools.idea.flags.StudioFlags;
+import com.android.tools.idea.tests.gui.framework.GuiTestRule;
+import com.android.tools.idea.tests.gui.framework.fixture.EditorFixture;
+import com.android.tools.idea.tests.gui.framework.fixture.IdeFrameFixture;
+import com.android.tools.idea.uibuilder.editor.AnimationToolbar;
+import com.intellij.testGuiFramework.framework.GuiTestRemoteRunner;
+import org.fest.swing.exception.ComponentLookupException;
+import org.junit.After;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(GuiTestRemoteRunner.class)
+public class AnimationToolbarTest {
+  @Rule public final GuiTestRule guiTest = new GuiTestRule();
+  @Rule public final RenderTaskLeakCheckRule renderTaskLeakCheckRule = new RenderTaskLeakCheckRule();
+
+  @Test
+  public void toolbarDisplayedForAnimatedVectors() throws Exception {
+    StudioFlags.NELE_ANIMATIONS_PREVIEW.override(true);
+    guiTest.importSimpleApplication();
+    IdeFrameFixture frame = guiTest.ideFrame();
+
+    EditorFixture editor = frame.getEditor().open("app/src/main/res/drawable/animated_vector.xml", EditorFixture.Tab.DESIGN);
+    DesignerEditorPanel panel = editor.getLayoutEditor(false).waitForRenderToFinish().target();
+    AnimationToolbar toolbar = frame.robot().finder().findByType(panel, AnimationToolbar.class);
+    assertThat(toolbar).isNotNull();
+
+    // Other drawables should not display the toolbar.
+    frame.getEditor().open("app/src/main/res/drawable/vector.xml", EditorFixture.Tab.DESIGN);
+
+    panel = editor.getLayoutEditor(false).waitForRenderToFinish().target();
+    try {
+      frame.robot().finder().findByType(panel, AnimationToolbar.class);
+      fail("We shouldn't find an AnimationToolbar when opening vector files.");
+    }
+    catch (ComponentLookupException expected) {
+      // We expect to throw ComponentLookupException as we shouldn't find an AnimationToolbar
+    }
+  }
+
+  @Test
+  public void toolbarDisplayedForMotionLayout() throws Exception {
+    StudioFlags.NELE_MOTION_LAYOUT_ANIMATIONS.override(true);
+    guiTest.importProjectAndWaitForProjectSyncToFinish("SimpleAndroidxApplication");
+    IdeFrameFixture frame = guiTest.ideFrame();
+    EditorFixture editor = frame.getEditor().open("app/src/main/res/layout/motion_layout.xml", EditorFixture.Tab.DESIGN);
+
+    DesignerEditorPanel panel = editor.getLayoutEditor(false).waitForRenderToFinish().target();
+    AnimationToolbar toolbar = frame.robot().finder().findByType(panel, AnimationToolbar.class);
+    assertThat(toolbar).isNotNull();
+
+    // Open a non-motion layout. Toolbar should not be displayed.
+    editor = frame.getEditor().open("app/src/main/res/layout/activity_main.xml", EditorFixture.Tab.DESIGN);
+
+    panel = editor.getLayoutEditor(false).waitForRenderToFinish().target();
+    try {
+      frame.robot().finder().findByType(panel, AnimationToolbar.class);
+      fail("We shouldn't find an AnimationToolbar when opening layouts other than MotionLayout.");
+    }
+    catch (ComponentLookupException expected) {
+      // We expect to throw ComponentLookupException as we shouldn't find an AnimationToolbar
+    }
+  }
+
+  @After
+  public void tearDown() {
+    StudioFlags.NELE_ANIMATIONS_PREVIEW.clearOverride();
+    StudioFlags.NELE_MOTION_LAYOUT_ANIMATIONS.clearOverride();
+  }
+}
