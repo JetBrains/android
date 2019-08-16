df0fd3a2537da9fd04b314800847b0d133549009
Jens Ole Lauridsen <jlauridsen@google.com>
2019-08-16 10:29:58 -0700

    Add the bounds of each view to the layout inspector.
    Bug: 123643052
    Test: N/A
    Change-Id: Ia9cfaff94adc0c0fe968454a4083f2aaa68a2448

--- layout-inspector/src/com/android/tools/idea/layoutinspector/properties/InspectorPropertiesView.kt
+++ layout-inspector/src/com/android/tools/idea/layoutinspector/properties/InspectorPropertiesView.kt
@@ -50,6 +50,7 @@ class InspectorPropertiesView(model: InspectorPropertiesModel) : PropertiesView<
     watermark = Watermark(WATERMARK_MESSAGE, WATERMARK_ACTION_MESSAGE, "")
     main.builders.add(SelectedViewBuilder())
     val tab = addTab("")
+    tab.builders.add(DimensionBuilder())
     tab.builders.add(InspectorTableBuilder("Declared Attributes", { it.isDeclared }, model, controlTypeProvider, declaredEditorProvider))
     tab.builders.add(InspectorTableBuilder("All Attributes", { true }, model, controlTypeProvider, allEditorProvider))
   }
