d547226c160241beab31110e374de9660840a9da
David Herman <davidherman@google.com>
2019-08-19 12:54:23 -0700

    Delete LocalResourceRepository#getBindingLayoutGroups
    ... and BindingLayoutGroup
    
    Bug: 139751928
    Fixes: 139468057
    Test: N/A, deleted code only (although ResourceFolderRepositoryTest
          was modified in one place to use getBindingLayoutInfo instead)
    
    Change-Id: If82f7d9f4dd5692099b747cc9c550ec115055be7

--- android/src/com/android/tools/idea/res/MultiResourceRepository.java
+++ android/src/com/android/tools/idea/res/MultiResourceRepository.java
@@ -21,7 +21,6 @@ import com.android.ide.common.resources.ResourceRepository;
 import com.android.ide.common.resources.ResourceTable;
 import com.android.ide.common.resources.SingleNamespaceResourceRepository;
 import com.android.resources.ResourceType;
-import com.android.tools.idea.res.binding.BindingLayoutGroup;
 import com.android.tools.idea.res.binding.BindingLayoutInfo;
 import com.android.tools.idea.resources.aar.AarResourceRepository;
 import com.google.common.base.Stopwatch;
@@ -29,7 +28,6 @@ import com.google.common.collect.ArrayListMultimap;
 import com.google.common.collect.HashMultimap;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableListMultimap;
-import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.ListMultimap;
 import com.google.common.collect.SetMultimap;
@@ -39,11 +37,9 @@ import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiFile;
 import java.util.Collection;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Locale;
-import java.util.Map;
 import java.util.Set;
 import javax.annotation.concurrent.GuardedBy;
 import org.jetbrains.annotations.NotNull;
@@ -85,12 +81,6 @@ public abstract class MultiResourceRepository extends LocalResourceRepository im
 
   @GuardedBy("ITEM_MAP_LOCK")
   private final ResourceTable myCachedMaps = new ResourceTable();
-
-  @GuardedBy("ITEM_MAP_LOCK")
-  private Map<String, BindingLayoutGroup> myBindingLayoutGroups = new HashMap<>();
-
-  @GuardedBy("ITEM_MAP_LOCK")
-  private long myBindingLayoutGroupsModificationCount = -1;
 
   MultiResourceRepository(@NotNull String displayName) {
     super(displayName);
@@ -239,26 +229,6 @@ public abstract class MultiResourceRepository extends LocalResourceRepository im
         }
       }
       return ImmutableList.of();
-    }
-  }
-
-  @Override
-  @NotNull
-  public Map<String, BindingLayoutGroup> getBindingLayoutGroups() {
-    synchronized (ITEM_MAP_LOCK) {
-      long modificationCount = getModificationCount();
-      if (myBindingLayoutGroupsModificationCount == modificationCount) {
-        return myBindingLayoutGroups;
-      }
-
-      ImmutableMap.Builder<String, BindingLayoutGroup> groups = ImmutableMap.builder();
-      for (LocalResourceRepository child : myLocalResources) {
-        Map<String, BindingLayoutGroup> childGroups = child.getBindingLayoutGroups();
-        groups.putAll(childGroups);
-      }
-      myBindingLayoutGroups = groups.build();
-      myBindingLayoutGroupsModificationCount = modificationCount;
-      return myBindingLayoutGroups;
     }
   }
 
