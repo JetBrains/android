b8f97daf9d929228abf1f0ebbb390d4c3c749459
Bradley Smith <smithbradley@google.com>
2019-08-22 14:17:27 -0700

    [Build Model] Disable Kotlin parser in production
    This change disables the Kotlin parser in projection and adds a flag to
    StudioFlags to enable or disable it. This will prevent exceptions from
    this parser being used before it is compelte.
    
    Bug: 139481725
    Test: added unit test to ensure disabled
    Change-Id: I8fa364a05dc3083ca5fc91f0fb22117ef8568046

---
+++ android/testSrc/com/android/tools/idea/gradle/dsl/KotlinParserTest.kt
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.gradle.dsl
+
+import com.android.SdkConstants.FN_BUILD_GRADLE_KTS
+import com.android.tools.idea.flags.StudioFlags
+import com.android.tools.idea.gradle.dsl.api.ProjectBuildModel
+import com.intellij.openapi.application.runWriteAction
+import com.intellij.openapi.vfs.VfsUtil
+import com.intellij.testFramework.PlatformTestCase
+import junit.framework.TestCase
+import org.junit.Test
+import java.io.File
+
+class KotlinParserTest : PlatformTestCase() {
+  @Test
+  fun testKotlinParserDisabledByDefault() {
+    runWriteAction {
+      val basePath = myProject.basePath
+      TestCase.assertNotNull(basePath)
+      val projectBasePath = VfsUtil.findFile(File(basePath!!).toPath(), true)!!
+      TestCase.assertTrue(projectBasePath.isDirectory)
+      val modulePath = myModule.moduleFile!!.parent
+      TestCase.assertTrue(modulePath.isDirectory)
+      val buildFile = modulePath.createChildData(this, FN_BUILD_GRADLE_KTS)
+      VfsUtil.saveText(buildFile, """
+        dependencies {
+          testImplementation(project(":"))
+        }
+      """.trimIndent())
+      TestCase.assertTrue(buildFile.isWritable)
+    }
+
+    val buildModel = ProjectBuildModel.get(myProject).getModuleBuildModel(myModule)
+    // Check nothing has been parsed.
+    val dependencies = buildModel!!.dependencies().all()
+    assertEmpty(dependencies)
+  }
+}
