54e16cda83a18bfe9a2e1d634bda6670cdf7fbcb
Kainan Wang <knwang@google.com>
2019-08-26 09:41:52 -0700

    Use BindingConversion annotation to match attribute's type
    Fixes: 140026884
    Test: DataBindingInspectionTest
    Change-Id: Ibffd9f70d1c816521eda4fe8fc852226750a9ceb

--- android-lang-databinding/testSrc/com/android/tools/idea/lang/databinding/DataBindingInspectionTest.kt
+++ android-lang-databinding/testSrc/com/android/tools/idea/lang/databinding/DataBindingInspectionTest.kt
@@ -56,8 +56,10 @@ class DataBindingInspectionTest(private val dataBindingMode: DataBindingMode) {
     // easier than finding a way to add a real dependency on the data binding library, which
     // usually requires Gradle plugin support.
     val databindingPackage = dataBindingMode.packageName.removeSuffix(".") // Without trailing '.'
+    val databindingSrcPath = "src/${databindingPackage.replace('.', '/')}"
+
     with(fixture.addFileToProject(
-      "src/${databindingPackage.replace('.', '/')}/BindingAdapter.java",
+      "$databindingSrcPath/BindingAdapter.java",
       // language=java
       """
         package $databindingPackage;
@@ -71,6 +73,23 @@ class DataBindingInspectionTest(private val dataBindingMode: DataBindingMode) {
         }
       """.trimIndent())) {
       // The following line is needed or else we get an error for referencing a file out of bounds
+      fixture.allowTreeAccessForFile(this.virtualFile)
+    }
+
+    with(fixture.addFileToProject(
+      "$databindingSrcPath/BindingConversion.java",
+      // language=java
+      """
+        package $databindingPackage;
+
+        import java.lang.annotation.ElementType;
+        import java.lang.annotation.Target;
+
+        @Target({ElementType.METHOD})
+        public @interface BindingConversion {
+        }
+
+      """.trimIndent())) {
       fixture.allowTreeAccessForFile(this.virtualFile)
     }
 
@@ -798,6 +817,46 @@ class DataBindingInspectionTest(private val dataBindingMode: DataBindingMode) {
             android:layout_height="120dp"
             android:gravity="center"
             android:onClick2="@{<error descr="Listener class 'android.view.View.OnClickListener' with method 'onClick' did not match signature of any method 'android:onClick2'">model::testClick</error>}"/>
+      </layout>
+    """.trimIndent())
+    fixture.configureFromExistingVirtualFile(file.virtualFile)
+    fixture.checkHighlighting()
+  }
+
+  @Test
+  fun testDataBindingInspection_functionExpressionMatchedWithBindingConversion() {
+    fixture.addClass(
+      // language=java
+      """
+      package test.langdb;
+
+      import android.view.View;
+      import ${dataBindingMode.bindingAdapter};
+      import ${dataBindingMode.bindingConversion};
+
+      public class Model {
+        @BindingAdapter("android:onClick2")
+        public void bindDummyValue(View view, View.OnClickListener s) {}
+        @BindingConversion
+        public static View.OnClickListener convertColorToOnClickListener(int num) {}
+        public int getNumber() {}
+      }
+    """.trimIndent())
+
+    val file = fixture.addFileToProject("res/layout/test_layout.xml", """
+      <?xml version="1.0" encoding="utf-8"?>
+      <layout xmlns:android="http://schemas.android.com/apk/res/android"
+              xmlns:app="http://schemas.android.com/apk/res-auto">
+        <data>
+          <import type="test.langdb.Model"/>
+          <variable name="model" type="Model" />
+        </data>
+        <TextView
+            android:id="@+id/c_0_0"
+            android:layout_width="120dp"
+            android:layout_height="120dp"
+            android:gravity="center"
+            android:onClick2="@{model.number}"/>
       </layout>
     """.trimIndent())
     fixture.configureFromExistingVirtualFile(file.virtualFile)
