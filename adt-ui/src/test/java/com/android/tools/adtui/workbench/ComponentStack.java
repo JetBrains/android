704145b1dafb3c79888f162a6ad3cdf95c26e413
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2019-08-29 10:45:09 +0200

    move ComponentManagerImpl to service-container module, reduce dependency on ComponentManagerImpl in tests
    GitOrigin-RevId: ab762249006e28a56caf2813d6348e43cb49c404

--- adt-ui/src/test/java/com/android/tools/adtui/workbench/ComponentStack.java
+++ adt-ui/src/test/java/com/android/tools/adtui/workbench/ComponentStack.java
@@ -13,24 +13,8 @@
 // limitations under the License.
 package com.android.tools.adtui.workbench;
 
-/*
- * Copyright (C) 2017 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 import com.intellij.openapi.components.ComponentManager;
-import com.intellij.openapi.components.impl.ComponentManagerImpl;
+import com.intellij.testFramework.ServiceContainerUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.picocontainer.MutablePicoContainer;
@@ -39,13 +23,13 @@ import java.util.ArrayDeque;
 import java.util.Deque;
 
 // TODO: Move this to a place to be shared with AndroidTestCase
-public class ComponentStack {
-  private final ComponentManagerImpl myComponentManager;
+public final class ComponentStack {
+  private final ComponentManager myComponentManager;
   private final MutablePicoContainer myContainer;
   private final Deque<ComponentItem> myComponents;
 
   public ComponentStack(@NotNull ComponentManager manager) {
-    myComponentManager = (ComponentManagerImpl)manager;
+    myComponentManager = manager;
     myContainer = (MutablePicoContainer)manager.getPicoContainer();
     myComponents = new ArrayDeque<>();
   }
@@ -61,7 +45,7 @@ public class ComponentStack {
   public <T> void registerComponentImplementation(@NotNull Class<T> key, @NotNull T instance) {
     Object old = myComponentManager.getComponent(key);
     myComponents.push(new ComponentItem(key, old));
-    myComponentManager.registerComponentInstance(key, instance);
+    ServiceContainerUtil.registerComponentInstance(myComponentManager, key, instance);
   }
 
   public void restoreComponents() {
@@ -69,7 +53,7 @@ public class ComponentStack {
       ComponentItem component = myComponents.pop();
       if (component.key instanceof Class) {
         //noinspection unchecked
-        myComponentManager.registerComponentInstance((Class)component.key, component.instance);
+        ServiceContainerUtil.registerComponentInstance(myComponentManager, (Class)component.key, component.instance);
       }
       else {
         myContainer.unregisterComponent(component.key.toString());
@@ -80,7 +64,7 @@ public class ComponentStack {
     }
   }
 
-  private static class ComponentItem {
+  private static final class ComponentItem {
     private final Object key;
     private final Object instance;
 
