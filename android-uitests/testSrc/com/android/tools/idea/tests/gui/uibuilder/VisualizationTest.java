4232bd67e530ffebe771bf62487dbe861a9320a1
Charlie Tsai <chartsai@google.com>
2019-08-30 15:41:53 +0100

    Add UI-test for Visualization Tool
    Bug: 140168045
    Test: N/A (This CL adds test)
    Change-Id: Ia9e1b01150a57dff6ae7c7362dac5bba72c26f25

---
+++ android-uitests/testSrc/com/android/tools/idea/tests/gui/uibuilder/VisualizationTest.java
@@ -0,0 +1,170 @@
+/*
+ * Copyright (C) 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.tests.gui.uibuilder;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import com.android.tools.idea.flags.StudioFlags;
+import com.android.tools.idea.tests.gui.framework.GuiTestRule;
+import com.android.tools.idea.tests.gui.framework.fixture.DeleteDialogFixture;
+import com.android.tools.idea.tests.gui.framework.fixture.EditorFixture;
+import com.android.tools.idea.tests.gui.framework.fixture.IdeFrameFixture;
+import com.android.tools.idea.tests.gui.framework.fixture.TextEditorFixture;
+import com.android.tools.idea.tests.gui.framework.fixture.designer.layout.VisualizationFixture;
+import com.android.tools.idea.uibuilder.visual.VisualizationManager;
+import com.intellij.testGuiFramework.framework.GuiTestRemoteRunner;
+import org.fest.swing.core.MouseButton;
+import org.fest.swing.timing.Wait;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * UI test for the visualization tool window
+ */
+@RunWith(GuiTestRemoteRunner.class)
+public class VisualizationTest {
+
+  @Rule public final GuiTestRule guiTest = new GuiTestRule();
+  @Rule public final RenderTaskLeakCheckRule renderTaskLeakCheckRule = new RenderTaskLeakCheckRule();
+
+  @Before
+  public void setUp() {
+    StudioFlags.NELE_SPLIT_EDITOR.override(true);
+  }
+
+  @After
+  public void tearDown() {
+    if (guiTest.ideFrame().getEditor().isVisualizationToolVisible()) {
+      guiTest.ideFrame().getEditor().getVisualizationTool().close();
+      VisualizationManager.getInstance(guiTest.ideFrame().getProject()).getVisualizationForm().dispose();
+    }
+    StudioFlags.NELE_SPLIT_EDITOR.clearOverride();
+  }
+
+  @Test
+  public void visualizationToolAvailableForLayoutFiles() throws Exception {
+    EditorFixture editor = guiTest.importSimpleApplication().getEditor();
+    VisualizationFixture visualizationTool = editor.open("app/src/main/res/layout/activity_my.xml").getVisualizationTool().waitForRenderToFinish();
+    assertTrue(editor.isVisualizationToolVisible());
+
+    editor.open("app/src/main/java/google/simpleapplication/MyActivity.java");
+    visualizationTool.waitForRenderToFinish();
+    assertFalse(editor.isVisualizationToolVisible());
+
+    editor.open("app/src/main/res/layout/frames.xml").getVisualizationTool().waitForRenderToFinish();
+    assertTrue(editor.isVisualizationToolVisible());
+  }
+
+  @Test
+  public void deletePreviewedFileShouldCloseVisualizationTool() throws Exception {
+    EditorFixture editor = guiTest.importSimpleApplication().getEditor();
+    editor.open("app/src/main/res/layout/activity_my.xml")
+      .getVisualizationTool()
+      .waitForRenderToFinish();
+    guiTest.ideFrame()
+      .getProjectView()
+      .selectAndroidPane()
+      .clickPath(MouseButton.RIGHT_BUTTON, "app", "res", "layout", "activity_my.xml")
+      .openFromMenu(DeleteDialogFixture::find, "Delete...")
+      .unsafeDelete();
+    assertFalse(editor.isVisualizationToolVisible());
+  }
+
+  @Test
+  public void openAnotherLayoutFileShouldChangePreviewedLayoutInVisualizationTool() throws Exception {
+    guiTest.importProjectAndWaitForProjectSyncToFinish("LayoutTest");
+    IdeFrameFixture ideFrame = guiTest.ideFrame();
+    EditorFixture editor = ideFrame.getEditor();
+
+    editor.open("app/src/main/res/layout/layout2.xml");
+    assertEquals(editor.getCurrentFile(), editor.getVisualizationTool().waitForRenderToFinish().getCurrentFile());
+
+    editor.open("app/src/main/res/layout-sw600dp/layout2.xml");
+    assertThat(editor.getCurrentFile().getParent().getName()).isEqualTo("layout-sw600dp");
+    assertEquals(editor.getCurrentFile(), editor.getVisualizationTool().waitForRenderToFinish().getCurrentFile());
+
+    editor.open("app/src/main/res/layout/layout2.xml");
+    assertThat(editor.getCurrentFile().getParent().getName()).isEqualTo("layout");
+    assertEquals(editor.getCurrentFile(), editor.getVisualizationTool().waitForRenderToFinish().getCurrentFile());
+  }
+
+  @Test
+  public void closeLayoutShouldNotCloseVisualizationForAnotherLayout() throws Exception {
+    EditorFixture editor = guiTest.importSimpleApplication()
+      .getEditor()
+      .open("app/src/main/res/layout/activity_my.xml")
+      .open("app/src/main/res/layout/frames.xml");
+    editor
+      .getVisualizationTool()
+      .waitForRenderToFinish();
+    int updateCountBeforeClose = editor.getPreviewUpdateCount();
+    editor.closeFile("app/src/main/res/layout/frames.xml");
+
+    Wait.seconds(2).expecting("visualization tool to update")
+      .until(() -> editor.getVisualizationToolUpdateCount() > updateCountBeforeClose);
+    assertTrue(editor.isVisualizationToolShowing());
+  }
+
+  @Test
+  public void closeSplitLayoutShouldMoveVisualizationToolToCorrectFile() throws Exception {
+    EditorFixture editor = guiTest.importSimpleApplication()
+      .getEditor()
+      .open("app/src/main/res/layout/frames.xml")
+      .open("app/src/main/res/layout/activity_my.xml")
+      .invokeAction(EditorFixture.EditorAction.SPLIT_HORIZONTALLY);
+
+    TextEditorFixture frames = editor.getVisibleTextEditor("frames.xml");
+    TextEditorFixture activity = editor.getVisibleTextEditor("activity_my.xml");
+
+    frames.select();
+    assertEquals(frames.getEditor().getFile(), editor.getVisualizationTool().getCurrentFile());
+    activity.select();
+    assertEquals(activity.getEditor().getFile(), editor.getVisualizationTool().getCurrentFile());
+
+    int updateCountBeforeClose = editor.getVisualizationToolUpdateCount();
+    activity.closeFile();
+    Wait.seconds(3).expecting("visualization tool to update")
+      .until(() -> editor.getVisualizationToolUpdateCount() > updateCountBeforeClose);
+    assertTrue(editor.isVisualizationToolVisible());
+    assertEquals(frames.getEditor().getFile(), editor.getVisualizationTool().getCurrentFile());
+  }
+
+  @Test
+  public void closeAllFileShouldCloseVisualizationTool() throws Exception {
+    EditorFixture editor = guiTest.importSimpleApplication()
+      .waitForGradleProjectSyncToFinish()
+      .getEditor()
+      .open("app/src/main/res/layout/activity_my.xml")
+      .open("app/src/main/java/google/simpleapplication/MyActivity.java");
+
+    editor.getVisibleTextEditor("activity_my.xml").select();
+    editor.getVisualizationTool().waitForRenderToFinish();
+    Wait.seconds(3).expecting("visualization tool to open").until(() -> editor.isVisualizationToolVisible());
+
+    editor.invokeAction(EditorFixture.EditorAction.CLOSE_ALL);
+    Wait.seconds(3).expecting("visualization tool to close").until(() -> !editor.isVisualizationToolVisible());
+
+    assertNull(editor.getCurrentFile());
+    assertFalse(editor.isVisualizationToolVisible());
+  }
+}
---
+++ uitest-framework/testSrc/com/android/tools/idea/tests/gui/framework/fixture/designer/layout/VisualizationFixture.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.tests.gui.framework.fixture.designer.layout;
+
+import com.android.tools.idea.common.model.NlModel;
+import com.android.tools.idea.tests.gui.framework.GuiTests;
+import com.android.tools.idea.tests.gui.framework.fixture.ToolWindowFixture;
+import com.android.tools.idea.tests.gui.framework.matcher.Matchers;
+import com.android.tools.idea.uibuilder.handlers.motion.editor.adapters.Annotations.Nullable;
+import com.android.tools.idea.uibuilder.surface.NlDesignSurface;
+import com.android.tools.idea.uibuilder.visual.VisualizationForm;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.openapi.wm.ToolWindowManager;
+import org.fest.swing.core.Robot;
+import org.jetbrains.android.util.AndroidBundle;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Fixture for the Visualization tool window
+ */
+public class VisualizationFixture extends ToolWindowFixture {
+  private final NlDesignSurfaceFixture myDesignSurfaceFixture;
+
+  public VisualizationFixture(@NotNull Project project, @NotNull Robot robot) {
+    super(AndroidBundle.message("android.layout.visual.tool.window.title"), project, robot);
+    myDesignSurfaceFixture = new NlDesignSurfaceFixture(
+      robot, GuiTests.waitUntilShowing(robot, Matchers.byName(NlDesignSurface.class, VisualizationForm.VISUALIZATION_DESIGN_SURFACE)));
+  }
+
+  public VisualizationFixture waitForRenderToFinish() {
+    myDesignSurfaceFixture.waitForRenderToFinish();
+    return this;
+  }
+
+  @Nullable
+  public VirtualFile getCurrentFile() {
+    NlModel model = myDesignSurfaceFixture.target().getModel();
+    return model != null ? model.getVirtualFile() : null;
+  }
+}
