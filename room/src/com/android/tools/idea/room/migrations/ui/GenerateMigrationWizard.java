8db89b650a9f3f51c15a8f70ba7566a998b8ad9d
Teodora Tica <teodoratica@google.com>
2019-09-04 16:45:33 +0100

    Introduces a wizard step for renaming columns
    It currently does not handle the case in which only renamed tables
    contain renamed columns.
    
    Test: Will add a test later
    Bug: 136048932
    Change-Id: I4ce19f866c26454b905cbd80c7185a678da749ef

--- room/src/com/android/tools/idea/room/migrations/ui/GenerateMigrationWizard.java
+++ room/src/com/android/tools/idea/room/migrations/ui/GenerateMigrationWizard.java
@@ -15,12 +15,26 @@
  */
 package com.android.tools.idea.room.migrations.ui;
 
+import static com.android.tools.idea.room.migrations.ui.GenerateMigrationWizard.RenamePanelElement.*;
+
 import com.android.tools.idea.room.migrations.update.DatabaseUpdate;
 import com.intellij.ide.wizard.AbstractWizard;
-import com.intellij.ide.wizard.Step;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.ComboBox;
 import com.intellij.psi.PsiDirectory;
 import com.intellij.psi.PsiPackage;
+import com.intellij.ui.components.JBLabel;
+import com.intellij.ui.components.JBPanel;
+import com.intellij.ui.components.JBScrollPane;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.swing.BoxLayout;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -30,10 +44,13 @@ import org.jetbrains.annotations.Nullable;
  * <p>Provides steps for selecting the destination folders for the new Migration class and test and collecting user input needed in order
  * to decide which tables have been renamed.</p>
  */
-public class GenerateMigrationWizard extends AbstractWizard<Step> {
+public class GenerateMigrationWizard extends AbstractWizard<GenerateMigrationWizardStep> {
   private static final String WIZARD_TITLE = "Generate a Room Migration";
 
   private GenerateMigrationWizardData myWizardData;
+  private GenerateMigrationWizardStep selectDestinationStep;
+  private GenerateMigrationWizardStep renameTablesStep;
+  private GenerateMigrationWizardStep renameColumnsStep;
 
   public GenerateMigrationWizard(@NotNull Project project,
                                  @NotNull PsiPackage targetPackage,
@@ -42,12 +59,52 @@ public class GenerateMigrationWizard extends AbstractWizard<Step> {
                                  @NotNull DatabaseUpdate databaseUpdate) {
     super(WIZARD_TITLE,project);
     myWizardData = new GenerateMigrationWizardData(project, targetPackage, migrationClassDirectory, migrationTestDirectory, databaseUpdate);
-    GenerateMigrationWizardSelectDestinationStep selectDestinationStep = new GenerateMigrationWizardSelectDestinationStep(myWizardData);
-    GenerateMigrationWizardRenameTablesStep renameStep = new GenerateMigrationWizardRenameTablesStep(myWizardData);
+    selectDestinationStep = new GenerateMigrationWizardSelectDestinationStep(myWizardData);
+    renameTablesStep = new GenerateMigrationWizardRenameTablesStep(myWizardData);
+    renameColumnsStep = new GenerateMigrationWizardRenameColumnsStep(myWizardData);
 
     addStep(selectDestinationStep);
-    addStep(renameStep);
+    if (!renameTablesStep.shouldBeSkipped()) {
+      addStep(renameTablesStep);
+    }
+    if (!renameColumnsStep.shouldBeSkipped()) {
+      addStep(renameColumnsStep);
+    }
+
     init();
+  }
+
+  @Override
+  protected boolean isLastStep() {
+    return super.isLastStep() || allNextStepsCanBeSkipped(getCurrentStep());
+  }
+
+  @Override
+  protected int getNextStep(int step) {
+    if (step == getStepCount() - 1 || allNextStepsCanBeSkipped(step)) {
+      return step;
+    }
+
+    int nextStep = step + 1;
+    while (nextStep < getStepCount() - 1 && mySteps.get(nextStep).shouldBeSkipped()) {
+      nextStep++;
+    }
+
+    return nextStep;
+  }
+
+  @Override
+  protected int getPreviousStep(int step) {
+    if (step == 0) {
+      return step;
+    }
+
+    int previousStep = step - 1;
+    while (previousStep > 0 && mySteps.get(previousStep).shouldBeSkipped()) {
+      previousStep --;
+    }
+
+    return previousStep;
   }
 
   @Nullable
@@ -69,5 +126,166 @@ public class GenerateMigrationWizard extends AbstractWizard<Step> {
   @NotNull
   public DatabaseUpdate getUserReviewedDatabaseUpdate() {
     return myWizardData.getUserReviewedDatabaseUpdate();
+  }
+
+  private boolean allNextStepsCanBeSkipped(int step) {
+    for (int futureStep = step + 1; futureStep < getStepCount(); futureStep++) {
+      if (!mySteps.get(futureStep).shouldBeSkipped()) {
+        return false;
+      }
+    }
+
+    return false;
+  }
+
+  /**
+   * Builds a panel for renaming either a table or a column.
+   *
+   * The panel provides a label with the old name of the element to be renamed, a combo box for specifying whether the element was deleted or
+   * renamed and another combo box for specifying the new name of the element, if applicable.
+   */
+  public static class RenamePanelElement {
+    public static final int MAX_PANEL_WIDTH = 900;
+    public static final int MAX_PANEL_HEIGHT = 35;
+    public static final int MAX_PANEL_COMPONENT_WIDTH = 300;
+    public static final int MAX_PANEL_COMPONENT_HEIGHT = 30;
+
+    private static final String DELETED = "deleted";
+    private static final String RENAMED = "renamed";
+    private static final String[] STATUSES = {DELETED, RENAMED};
+
+    private JBLabel initialName;
+    private ComboBox<String> statusComboBox;
+    private ComboBox<String> newNamesComboBox;
+    private JBPanel renameStepElementPanel;
+
+    public RenamePanelElement(@NotNull String initialName, @NotNull List<String> newNames) {
+      this.initialName = new JBLabel(initialName);
+      this.initialName.setMaximumSize(new Dimension(MAX_PANEL_COMPONENT_WIDTH, MAX_PANEL_COMPONENT_HEIGHT));
+
+      this.statusComboBox = new ComboBox<>(STATUSES);
+      this.statusComboBox.setMaximumSize(new Dimension(MAX_PANEL_COMPONENT_WIDTH, MAX_PANEL_COMPONENT_HEIGHT));
+
+      this.newNamesComboBox = new ComboBox<>();
+      newNames.forEach(newName -> newNamesComboBox.addItem(newName));
+      this.newNamesComboBox.setMaximumSize(new Dimension(MAX_PANEL_COMPONENT_WIDTH, MAX_PANEL_COMPONENT_HEIGHT));
+      this.newNamesComboBox.setEnabled(false);
+
+      renameStepElementPanel = new JBPanel<>();
+      renameStepElementPanel.setLayout(new BoxLayout(renameStepElementPanel, BoxLayout.X_AXIS));
+      renameStepElementPanel.add(this.initialName);
+      renameStepElementPanel.add(this.statusComboBox);
+      renameStepElementPanel.add(this.newNamesComboBox);
+      renameStepElementPanel.setMaximumSize(new Dimension(MAX_PANEL_WIDTH, MAX_PANEL_HEIGHT));
+
+      statusComboBox.addItemListener(new ItemListener() {
+        @Override
+        public void itemStateChanged(ItemEvent e) {
+          if (e.getItem().equals(RENAMED)) {
+            if (e.getStateChange() == ItemEvent.SELECTED) {
+              newNamesComboBox.setEnabled(true);
+            } else {
+              newNamesComboBox.setEnabled(false);
+            }
+          }
+        }
+      });
+    }
+
+    @NotNull
+    public String getInitialName() {
+      return initialName.getText();
+    }
+
+    @NotNull String getStatus() {
+      return statusComboBox.getItemAt(statusComboBox.getSelectedIndex());
+    }
+
+    @NotNull String getNewName() {
+      return newNamesComboBox.getItemAt(newNamesComboBox.getSelectedIndex());
+    }
+
+    @NotNull
+    public JBPanel getRenameStepElementPanel() {
+      return renameStepElementPanel;
+    }
+
+    public boolean markedAsRenamed() {
+      return getStatus().equals(RENAMED);
+    }
+  }
+
+  /**
+   * Builds a panel for renaming either all tables from a database or all columns of a table.
+   *
+   * It provides a {@link RenamePanelElement} for each table or column which could be renamed.
+   */
+  public static class RenamePanel {
+    private static final String HEADER_NAME_LABEL = "Old Name";
+    private static final String HEADER_OPERATION_LABEL = "Operation";
+    private static final String HEADER_NEW_NAME_LABEL = "New Name";
+
+    private List<RenamePanelElement> renameStepElements;
+    private JBPanel renameStepPanel;
+
+    public RenamePanel(@NotNull String label,
+                       @NotNull List<String> oldNames,
+                       @NotNull List<String> newNames) {
+      renameStepElements = new ArrayList<>();
+      renameStepPanel = new JBPanel(new BorderLayout());
+
+      JBLabel renameStepPanelLabel = new JBLabel(label);
+      renameStepPanel.add(renameStepPanelLabel, BorderLayout.NORTH);
+      renameStepPanelLabel.setLabelFor(renameStepPanel);
+
+      JBPanel listPanel = new JBPanel();
+      listPanel.setLayout(new BoxLayout(listPanel, BoxLayout.Y_AXIS));
+
+      JBPanel headerPanel = new JBPanel();
+      headerPanel.setLayout(new BoxLayout(headerPanel, BoxLayout.X_AXIS));
+      headerPanel.setMaximumSize(new Dimension(MAX_PANEL_WIDTH, MAX_PANEL_HEIGHT));
+
+      JBLabel tableNameLabel = new JBLabel(HEADER_NAME_LABEL);
+      tableNameLabel.setMaximumSize(new Dimension(MAX_PANEL_COMPONENT_WIDTH, MAX_PANEL_COMPONENT_HEIGHT));
+      JBLabel operationLabel = new JBLabel(HEADER_OPERATION_LABEL);
+      operationLabel.setMaximumSize(new Dimension(MAX_PANEL_COMPONENT_WIDTH, MAX_PANEL_COMPONENT_HEIGHT));
+      JBLabel newNameLabel = new JBLabel(HEADER_NEW_NAME_LABEL);
+      newNameLabel.setMaximumSize(new Dimension(MAX_PANEL_COMPONENT_WIDTH, MAX_PANEL_COMPONENT_HEIGHT));
+
+      headerPanel.add(tableNameLabel);
+      headerPanel.add(operationLabel);
+      headerPanel.add(newNameLabel);
+
+      listPanel.add(headerPanel);
+
+      for (String oldName : oldNames) {
+        RenamePanelElement renameStepElement =
+          new RenamePanelElement(oldName, newNames);
+        renameStepElements.add(renameStepElement);
+        listPanel.add(renameStepElement.getRenameStepElementPanel());
+      }
+
+      JBScrollPane renameStepScrollPane = new JBScrollPane();
+      renameStepScrollPane.getViewport().setView(listPanel);
+
+      renameStepPanel.add(renameStepScrollPane, BorderLayout.CENTER);
+    }
+
+    @NotNull
+    public JBPanel getRenameStepPanel() {
+      return renameStepPanel;
+    }
+
+    @NotNull
+    public Map<String, String> getOldToNewNamesMapping() {
+      Map<String, String> oldToNewNamesMapping = new HashMap<>();
+      for (RenamePanelElement renameStepElement : renameStepElements) {
+        if (renameStepElement.markedAsRenamed()) {
+          oldToNewNamesMapping.put(renameStepElement.getInitialName(), renameStepElement.getNewName());
+        }
+      }
+
+      return oldToNewNamesMapping;
+    }
   }
 }
--- room/src/com/android/tools/idea/room/migrations/ui/GenerateMigrationWizardData.java
+++ room/src/com/android/tools/idea/room/migrations/ui/GenerateMigrationWizardData.java
@@ -16,10 +16,13 @@
 package com.android.tools.idea.room.migrations.ui;
 
 import com.android.tools.idea.room.migrations.update.DatabaseUpdate;
+import com.android.tools.idea.room.migrations.update.EntityUpdate;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiDirectory;
 import com.intellij.psi.PsiPackage;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import org.jetbrains.annotations.NotNull;
 
@@ -36,7 +39,9 @@ public class GenerateMigrationWizardData {
   private PsiDirectory migrationTestDirectory;
 
   private DatabaseUpdate databaseUpdate;
+  private List<EntityUpdate> userIdentifiedEntityUpdates;
   private Map<String, String> renamedTables;
+  private Map<String, Map<String, String>> tableToRenamedColumnsMapping;
 
   GenerateMigrationWizardData(@NotNull Project project,
                               @NotNull PsiPackage targetPackage,
@@ -49,19 +54,9 @@ public class GenerateMigrationWizardData {
     this.migrationTestDirectory = migrationTestDirectory;
 
     this.databaseUpdate = databaseUpdate;
+    this.userIdentifiedEntityUpdates = new ArrayList<>();
     this.renamedTables = new HashMap<>();
-  }
-
-  public void updateTargetPackage(@NotNull PsiPackage targetPackage) {
-    this.targetPackage = targetPackage;
-  }
-
-  public void updateMigrationClassDirectory(@NotNull PsiDirectory migrationClassDirectory) {
-    this.migrationClassDirectory = migrationClassDirectory;
-  }
-
-  public void updateMigrationTestDirectory(PsiDirectory migrationTestDirectory) {
-    this.migrationTestDirectory = migrationTestDirectory;
+    this.tableToRenamedColumnsMapping = new HashMap<>();
   }
 
   @NotNull
@@ -89,16 +84,64 @@ public class GenerateMigrationWizardData {
     return databaseUpdate;
   }
 
-  public DatabaseUpdate getUserReviewedDatabaseUpdate() {
-    databaseUpdate.applyRenameMapping(renamedTables);
-    return databaseUpdate;
+  /**
+   * Returns a list of entity updates which were identified after processing user input regarding renamed tables.
+   */
+  @NotNull
+  public List<EntityUpdate> getUserIdentifiedEntityUpdates() {
+    return userIdentifiedEntityUpdates;
   }
 
   /**
-   * Updates the old table name to new table name mapping based on user input
+   * Modifies the DatabaseUpdate based on user provided information. It should only be called after completing all the steps of the wizard.
+   */
+  @NotNull
+  public DatabaseUpdate getUserReviewedDatabaseUpdate() {
+    databaseUpdate.applyRenameMapping(renamedTables);
+    for (EntityUpdate entityUpdate : databaseUpdate.getModifiedEntities().values()) {
+      Map<String, String> renamedColumns = tableToRenamedColumnsMapping.get(entityUpdate.getNewTableName());
+      if (renamedColumns != null) {
+        entityUpdate.applyRenameMapping(renamedColumns);
+      }
+    }
+
+    return databaseUpdate;
+  }
+
+  public void updateTargetPackage(@NotNull PsiPackage targetPackage) {
+    this.targetPackage = targetPackage;
+  }
+
+  public void updateMigrationClassDirectory(@NotNull PsiDirectory migrationClassDirectory) {
+    this.migrationClassDirectory = migrationClassDirectory;
+  }
+
+  public void updateMigrationTestDirectory(PsiDirectory migrationTestDirectory) {
+    this.migrationTestDirectory = migrationTestDirectory;
+  }
+
+  /**
+   * Updates the old table name to new table name mapping based on user input.
    * @param userInput a user provided mapping between the old table names and new table names.
    */
   public void updateRenamedTables(@NotNull Map<String, String> userInput) {
     renamedTables = userInput;
+  }
+
+  /**
+   * Updates the table name to renamed columns (which also represents a mapping of old column names to new column names)
+   * mapping based on user input.
+   * @param tableToRenamedColumnsMapping a user provided mapping between tables names and their renamed columns.
+   */
+  public void updateTableToRenamedColumnsMapping(@NotNull Map<String, Map<String, String>> tableToRenamedColumnsMapping) {
+    this.tableToRenamedColumnsMapping = tableToRenamedColumnsMapping;
+  }
+
+  /**
+   * Updates the list of entity updates which are identified based on the user input from the renaming tables step.
+   * @param userIdentifiedEntityUpdates a list of entity updates identified after the table renaming step.
+   */
+  public void updateUserIdentifiedEntityUpdates(@NotNull List<EntityUpdate> userIdentifiedEntityUpdates) {
+    this.userIdentifiedEntityUpdates = userIdentifiedEntityUpdates;
   }
 }
