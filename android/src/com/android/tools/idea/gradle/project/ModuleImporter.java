91c148c67148e344fcbb027e55235c84b988f175
Gregory.Shrago <gregory.shrago@jetbrains.com>
2019-09-19 02:19:45 +0300

    cleanup: make LOG fields final
    (cherry picked from commit 451e5062c08d757e44ddcb734e81caea09220a13)
    
    Change-Id: I451e5062c08d757e44ddcb734e81caea09220a13

--- android/src/com/android/tools/idea/gradle/project/ModuleImporter.java
+++ android/src/com/android/tools/idea/gradle/project/ModuleImporter.java
@@ -36,6 +36,7 @@ import java.util.Set;
  * Importers for different project types, e.g. ADT, Gradle.
  */
 public abstract class ModuleImporter {
+  private static final Logger LOG = Logger.getInstance(ModuleImporter.class);
   private static final Key<ModuleImporter[]> KEY_IMPORTERS = new Key<>("com.android.tools.importers");
   private static final Key<ModuleImporter> KEY_CURRENT_IMPORTER = new Key<>("com.android.tools.currentImporter");
   private static final ModuleImporter NONE = new ModuleImporter() {
@@ -71,7 +72,6 @@ public abstract class ModuleImporter {
       return Collections.emptySet();
     }
   };
-  private static Logger LOG = Logger.getInstance(ModuleImporter.class);
 
   /**
    * Importers live in the wizard context. This method lazily creates importers if they are
