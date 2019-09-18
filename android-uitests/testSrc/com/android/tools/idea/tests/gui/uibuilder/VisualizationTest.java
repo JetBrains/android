be0ad4288c7a33a2690e5a35a671598f5df38b50
Charlie Tsai <chartsai@google.com>
2019-09-18 17:09:24 +0100

    Move visualization test into unreliable group
    Bug: N/A
    Test: N/A
    Change-Id: I709af037b9cb529e0d1e3d5d456d23e9e49212da

--- android-uitests/testSrc/com/android/tools/idea/tests/gui/uibuilder/VisualizationTest.java
+++ android-uitests/testSrc/com/android/tools/idea/tests/gui/uibuilder/VisualizationTest.java
@@ -23,6 +23,8 @@ import static org.junit.Assert.assertTrue;
 
 import com.android.tools.idea.flags.StudioFlags;
 import com.android.tools.idea.tests.gui.framework.GuiTestRule;
+import com.android.tools.idea.tests.gui.framework.RunIn;
+import com.android.tools.idea.tests.gui.framework.TestGroup;
 import com.android.tools.idea.tests.gui.framework.fixture.DeleteDialogFixture;
 import com.android.tools.idea.tests.gui.framework.fixture.EditorFixture;
 import com.android.tools.idea.tests.gui.framework.fixture.IdeFrameFixture;
@@ -41,6 +43,7 @@ import org.junit.runner.RunWith;
 /**
  * UI test for the visualization tool window
  */
+@RunIn(TestGroup.UNRELIABLE)
 @RunWith(GuiTestRemoteRunner.class)
 public class VisualizationTest {
 
