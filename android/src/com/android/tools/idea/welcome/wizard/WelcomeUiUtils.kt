c875afbc7a5839f26141461e846e2516f4fed2cd
Valery Cherepanov <qumeric@google.com>
2019-09-19 12:35:43 +0100

    Add VerticalPanel UI helper
    It has some naming problems etc. but I have a very long chain which uses
    it, so I want to keep it as it is for now and refactor later to avoid
    major rebasing pain.
    
    Bug: 76009501
    Test: manual
    Change-Id: I58c30a23a1235b356a11d0b5c47d1e6549cf3887

--- android/src/com/android/tools/idea/welcome/wizard/WelcomeUiUtils.kt
+++ android/src/com/android/tools/idea/welcome/wizard/WelcomeUiUtils.kt
@@ -22,8 +22,17 @@
 package com.android.tools.idea.welcome.wizard
 
 import com.android.sdklib.devices.Storage
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.application.ModalityState
+import com.intellij.ui.components.JBLabel
+import com.intellij.uiDesigner.core.GridConstraints
+import com.intellij.uiDesigner.core.GridLayoutManager
+import com.intellij.uiDesigner.core.Spacer
+import java.awt.Component
+import java.awt.Dimension
 import java.math.RoundingMode
 import java.text.NumberFormat
+import javax.swing.JPanel
 
 /**
  * Returns string describing the [size].
@@ -66,3 +75,44 @@ fun getMessageWithDetails(message: String, details: String?): String =
     val dotIfNeeded = if (details.trim().endsWith(".")) "" else "."
     "$message: $details$dotIfNeeded"
   }
+
+internal fun invokeLater(modalityState: ModalityState = ModalityState.defaultModalityState(), block: () -> Unit): Unit =
+  ApplicationManager.getApplication().invokeLater(block, modalityState)
+
+// TODO(qumeric): rename, refactor, add more capabilities
+// * do not let to add two things in the same cell
+// * add support for button groups?
+// * add support for default spacing with spacers?
+// * dynamic number of rows
+// * multicolumn/multirow support
+// * add JavaDoc
+// * move to different file
+class VerticalPanel(row: Int, col: Int, isHorizontal: Boolean = false, private val block: VerticalPanel.() -> Unit) {
+  private val panel = JPanel(GridLayoutManager(row, col))
+  private val any = Dimension(-1, -1)
+  private var curRow = 0
+  private var curCol = 0
+  private val rowInc = if (isHorizontal) 0 else 1
+  private val colInc = if (isHorizontal) 1 else 0
+
+  fun elem(c: Component, anchor: Int = 0, fill: Int = 0, hPolicy: Int = 0, vPolicy: Int = 0) {
+    panel.add(c, GridConstraints(curRow, curCol, 1, 1, anchor, fill, hPolicy, vPolicy, any, any, any))
+    curRow += rowInc
+    curCol += colInc
+  }
+
+  fun label(text: String, anchor: Int = 0, fill: Int = 0, hPolicy: Int = 0, vPolicy: Int = 0) =
+    elem(JBLabel(text), anchor, fill, hPolicy, vPolicy)
+
+  fun html(text: String, anchor: Int = 0, fill: Int = 0, hPolicy: Int = 0, vPolicy: Int = 0) =
+    label("<html>$text</html>", anchor, fill, hPolicy, vPolicy)
+
+  // TODO(qumeric): should it be hspacer/vspacer instead?
+  fun spacer(anchor: Int = 0, fill: Int = 0, hPolicy: Int = 0, vPolicy: Int = 0) =
+    elem(Spacer(), anchor, fill, hPolicy, vPolicy)
+
+  fun build(): JPanel {
+    block()
+    return panel
+  }
+}
