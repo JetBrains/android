393127c3a2e430c0f1af899aa4082373a69efb6f
Peter Xu <peterx@google.com>
2019-09-19 19:01:32 -0700

    Implement studio inspector connection client
    This new inspection client will make it easier for developers to send
    commands and receive events via the transport pipeline.
    
    Test: AppInspectorConnectionTest
    Bug: 140135544
    Change-Id: I16875ca9c44902f5df6d1dec63105785d86261b3

---
+++ app-inspection/src/com/android/tools/idea/appinspection/transport/AppInspectorConnection.kt
@@ -0,0 +1,151 @@
+/*
+ * Copyright (C) 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.appinspection.transport
+
+import com.android.tools.app.inspection.AppInspection.AppInspectionCommand
+import com.android.tools.app.inspection.AppInspection.DisposeInspectorCommand
+import com.android.tools.app.inspection.AppInspection.RawCommand
+import com.android.tools.app.inspection.AppInspection.ServiceResponse
+import com.android.tools.idea.protobuf.ByteString
+import com.android.tools.profiler.proto.Common.Event
+import com.android.tools.profiler.proto.Common.Event.Kind.PROCESS
+import com.google.common.util.concurrent.Futures
+import com.google.common.util.concurrent.ListenableFuture
+import com.google.common.util.concurrent.MoreExecutors
+import com.google.common.util.concurrent.SettableFuture
+import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.atomic.AtomicBoolean
+
+/**
+ * Two-way connection for the [AppInspectorClient] which implements [AppInspectorClient.CommandMessenger] and dispatches events for the
+ * [AppInspectorClient.EventListener].
+ */
+internal class AppInspectorConnection(
+  private val transport: AppInspectionTransport,
+  private val inspectorId: String
+) : AppInspectorClient.CommandMessenger {
+  private val pendingCommands = ConcurrentHashMap<Int, SettableFuture<ByteArray>>()
+  private val connectionClosedMessage = "Failed to send a command because the $inspectorId connection is already closed."
+  private val disposeCalled = AtomicBoolean(false)
+  private var isDisposed = AtomicBoolean(false)
+  private val disposeFuture = SettableFuture.create<ServiceResponse>()
+
+  var clientEventListener: AppInspectorClient.EventListener? = null
+    set(value) {
+      field = value
+      transport.poller.registerListener(inspectorEventListener)
+    }
+
+  private val inspectorEventListener = transport.createEventListener(
+    executor = MoreExecutors.directExecutor(),
+    filter = { event: Event ->
+      (event.appInspectionEvent.hasRawEvent() && event.appInspectionEvent.rawEvent.inspectorId == inspectorId)
+      || (event.appInspectionEvent.hasCrashEvent() && event.appInspectionEvent.crashEvent.inspectorId == inspectorId)
+    }
+  ) { event ->
+    val appInspectionEvent = event.appInspectionEvent
+    when {
+      appInspectionEvent.hasRawEvent() -> {
+        val content = appInspectionEvent.rawEvent.content.toByteArray()
+        clientEventListener!!.onRawEvent(content)
+      }
+      appInspectionEvent.hasCrashEvent() -> {
+        // Remove inspector's listener if it crashes
+        cleanup("Inspector $inspectorId has crashed.")
+        clientEventListener!!.onCrashEvent(appInspectionEvent.crashEvent.errorMessage)
+      }
+    }
+    false
+  }
+
+  private val responsesListener = transport.createEventListener(
+    executor = MoreExecutors.directExecutor(),
+    filter = { it.commandId != 0 && it.appInspectionEvent.hasRawEvent() }
+  ) { event ->
+    pendingCommands.remove(event.commandId)?.set(event.appInspectionEvent.rawEvent.content.toByteArray())
+    false
+  }
+
+  private val processEndListener = transport.createEventListener(
+    eventKind = PROCESS,
+    executor = MoreExecutors.directExecutor()) {
+    if (it.isEnded) {
+      cleanup("Inspector $inspectorId was disposed, because app process terminated.")
+    }
+    it.isEnded
+  }
+
+  init {
+    transport.poller.registerListener(responsesListener)
+    transport.poller.registerListener(processEndListener)
+  }
+
+  override fun disposeInspector(): ListenableFuture<ServiceResponse> {
+    return disposeFuture.also {
+      if (disposeCalled.compareAndSet(false, true)) {
+        val disposeInspectorCommand = DisposeInspectorCommand.newBuilder().setInspectorId(inspectorId).build()
+        val appInspectionCommand = AppInspectionCommand.newBuilder().setDisposeInspectorCommand(disposeInspectorCommand).build()
+        val response = transport.executeCommand(appInspectionCommand)
+        val listener = transport.registerEventListener(MoreExecutors.directExecutor(), { it.commandId == response.commandId }) {
+          disposeFuture.set(it.appInspectionEvent.response)
+          cleanup("Inspector $inspectorId was disposed.")
+          // we manually call unregister, because future can be completed from other places, so we clean up the listeners there
+          false
+        }
+        disposeFuture.addListener(Runnable {
+          transport.poller.unregisterListener(listener)
+        }, MoreExecutors.directExecutor())
+      }
+    }
+  }
+
+  override fun sendRawCommand(rawData: ByteArray): ListenableFuture<ByteArray> {
+    if (isDisposed.get()) {
+      return Futures.immediateFailedFuture(IllegalStateException(connectionClosedMessage))
+    }
+    val settableFuture = SettableFuture.create<ByteArray>()
+    val rawCommand = RawCommand.newBuilder().setInspectorId(inspectorId).setContent(ByteString.copyFrom(rawData)).build()
+    val appInspectionCommand = AppInspectionCommand.newBuilder().setRawInspectorCommand(rawCommand).build()
+    val response = transport.executeCommand(appInspectionCommand)
+    pendingCommands[response.commandId] = settableFuture
+
+    // cleanup() might have gotten called from a different thread, so we double-check if connection is disposed by now.
+    // if it is disposed, then there is a race between pendingCommands.clear / future completion in cleanup method and
+    // "pendingCommands[commandId] =" in this method. To make sure that a future isn't leaked, we remove it ourselves from the map
+    // and complete it with an exception.
+    // if it isn't disposed, then cleanup didn't happen yet and it will be able properly clear [pendingCommands].
+    if (isDisposed.get()) {
+      pendingCommands.remove(response.commandId)
+      settableFuture.setException(IllegalStateException(connectionClosedMessage))
+    }
+
+    return settableFuture
+  }
+
+  private fun cleanup(message: String) {
+    if (isDisposed.compareAndSet(false, true)) {
+      transport.poller.unregisterListener(inspectorEventListener)
+      transport.poller.unregisterListener(processEndListener)
+      transport.poller.unregisterListener(responsesListener)
+      pendingCommands.values.forEach { it.setException(RuntimeException(message)) }
+      pendingCommands.clear()
+      if (!disposeFuture.isDone) {
+        disposeFuture.setException(RuntimeException(message))
+      }
+      clientEventListener!!.onDispose()
+    }
+  }
+}
---
+++ app-inspection/testSrc/com/android/tools/idea/appinspection/transport/AppInspectionPipelineConnectionTest.kt
@@ -0,0 +1,95 @@
+/*
+ * Copyright (C) 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.appinspection.transport
+
+import com.android.tools.adtui.model.FakeTimer
+import com.android.tools.app.inspection.AppInspection.AppInspectionEvent
+import com.android.tools.app.inspection.AppInspection.ServiceResponse
+import com.android.tools.app.inspection.AppInspection.ServiceResponse.Status.SUCCESS
+import com.android.tools.idea.transport.TransportClient
+import com.android.tools.idea.transport.faketransport.FakeGrpcServer
+import com.android.tools.idea.transport.faketransport.FakeTransportService
+import com.android.tools.idea.transport.faketransport.commands.CommandHandler
+import com.android.tools.profiler.proto.Commands.Command
+import com.android.tools.profiler.proto.Commands.Command.CommandType.ATTACH_AGENT
+import com.android.tools.profiler.proto.Common
+import com.android.tools.profiler.proto.Common.AgentData
+import com.android.tools.profiler.proto.Common.AgentData.Status.ATTACHED
+import com.android.tools.profiler.proto.Common.Event
+import com.android.tools.profiler.proto.Common.Event.Kind.AGENT
+import com.android.tools.profiler.proto.Common.Event.Kind.APP_INSPECTION
+import com.google.common.truth.Truth.assertThat
+import com.google.common.util.concurrent.AsyncFunction
+import com.google.common.util.concurrent.Futures
+import com.google.common.util.concurrent.MoreExecutors
+import org.junit.Rule
+import org.junit.Test
+import java.nio.file.Paths
+import java.util.concurrent.TimeUnit
+import com.android.tools.profiler.proto.Commands.Command.CommandType.APP_INSPECTION as APP_INSPECTION_COMMAND
+
+class AppInspectionPipelineConnectionTest {
+  private val timer = FakeTimer()
+  private val transportService = FakeTransportService(timer)
+
+  @get:Rule
+  val gRpcServerRule = FakeGrpcServer.createFakeGrpcServer("InspectorPipelineConnectionTest", transportService, transportService)!!
+
+  @Test
+  fun launchInspector() {
+    transportService.setCommandHandler(
+      ATTACH_AGENT,
+      object : CommandHandler(timer) {
+        override fun handleCommand(command: Command, events: MutableList<Event>) {
+          events.add(
+            Event.newBuilder()
+              .setKind(AGENT)
+              .setAgentData(AgentData.newBuilder().setStatus(ATTACHED).build())
+              .build()
+          )
+        }
+      })
+    transportService.setCommandHandler(
+      APP_INSPECTION_COMMAND,
+      object : CommandHandler(timer) {
+        override fun handleCommand(command: Command, events: MutableList<Event>) {
+          events.add(
+            Event.newBuilder()
+              .setKind(APP_INSPECTION)
+              .setPid(command.pid)
+              .setTimestamp(timer.currentTimeNs)
+              .setCommandId(command.commandId)
+              .setIsEnded(true)
+              .setAppInspectionEvent(AppInspectionEvent.newBuilder()
+                                       .setResponse(ServiceResponse.newBuilder()
+                                                      .setStatus(SUCCESS)
+                                                      .build())
+                                       .build())
+              .build()
+          )
+        }
+      })
+    val connection = AppInspectionPipelineConnection.attach(
+       Common.Stream.getDefaultInstance(), Common.Process.getDefaultInstance(), TransportClient(gRpcServerRule.name))
+    val clientFuture = Futures.transformAsync(connection, AsyncFunction<AppInspectionPipelineConnection, TestInspectorClient> {
+      it!!.launchInspector(
+        "test.inspector",
+        Paths.get("path", "to", "inspector", "dex")) { TestInspectorClient(it) }
+    }, MoreExecutors.directExecutor())
+    assertThat(clientFuture.get(1, TimeUnit.SECONDS)).isNotNull()
+  }
+}
+
