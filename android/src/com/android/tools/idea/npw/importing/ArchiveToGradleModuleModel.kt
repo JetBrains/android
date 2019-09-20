82b13a8253f4a0fa3c2b9bbb8638f4d1b77f6465
Bradley Smith <smithbradley@google.com>
2019-09-19 17:46:54 -0700

    Refactor ArchiveToGradleModuleModel...
    ... and some minor cleanup
    
    This change performs the following:
     1 - merges functionality from CreateModuleFromArchiveAction to
     ArchiveToGradleModuleModel.
     2 - use guessProjectDir before project.baseDir
     3 - Make ArchiveToGradleModuleModel the LargeFileWriteRequestor
    
    Test: existing
    Bug: N/A
    Change-Id: I190d56ebdb3fbbdcda08ab7a578bccb1fe0c1cbb

--- android/src/com/android/tools/idea/npw/importing/ArchiveToGradleModuleModel.kt
+++ android/src/com/android/tools/idea/npw/importing/ArchiveToGradleModuleModel.kt
@@ -15,7 +15,14 @@
  */
 package com.android.tools.idea.npw.importing
 
+import com.android.SdkConstants
 import com.android.SdkConstants.GRADLE_PATH_SEPARATOR
+import com.android.internal.annotations.VisibleForTesting
+import com.android.tools.idea.gradle.parser.BuildFileKey
+import com.android.tools.idea.gradle.parser.Dependency
+import com.android.tools.idea.gradle.parser.GradleBuildFile
+import com.android.tools.idea.gradle.parser.GradleSettingsFile
+import com.android.tools.idea.gradle.util.GradleUtil
 import com.android.tools.idea.npw.model.ProjectSyncInvoker
 import com.android.tools.idea.npw.project.getContainingModule
 import com.android.tools.idea.observable.core.BoolProperty
@@ -23,15 +30,33 @@ import com.android.tools.idea.observable.core.BoolValueProperty
 import com.android.tools.idea.observable.core.StringProperty
 import com.android.tools.idea.observable.core.StringValueProperty
 import com.android.tools.idea.observable.expressions.bool.BooleanExpression
+import com.android.tools.idea.util.toIoFile
 import com.android.tools.idea.wizard.model.WizardModel
 import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.command.UndoConfirmationPolicy
+import com.intellij.openapi.command.WriteCommandAction.writeCommandAction
+import com.intellij.openapi.diagnostic.Logger
+import com.intellij.openapi.diagnostic.logger
+import com.intellij.openapi.module.Module
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.project.guessProjectDir
+import com.intellij.openapi.util.io.FileUtil
+import com.intellij.openapi.vfs.LargeFileWriteRequestor
+import com.intellij.openapi.vfs.VfsUtil
+import com.intellij.openapi.vfs.VfsUtilCore
 import java.io.File
+import java.io.IOException
+
+@VisibleForTesting
+fun getBuildGradleText(jarName: File) = "configurations.maybeCreate(\"default\")\nartifacts.add(\"default\", file('${jarName.name}'))"
 
 /**
  * Model that represents the import of an existing library (.jar or .aar) into a Gradle project as a new Module
  */
-class ArchiveToGradleModuleModel(val project: Project, private val projectSyncInvoker: ProjectSyncInvoker) : WizardModel() {
+class ArchiveToGradleModuleModel(
+  val project: Project,
+  private val projectSyncInvoker: ProjectSyncInvoker
+) : WizardModel(), LargeFileWriteRequestor {
   @JvmField val archive: StringProperty = StringValueProperty()
   @JvmField val gradlePath: StringProperty = StringValueProperty()
   @JvmField val moveArchive: BoolProperty = BoolValueProperty()
@@ -58,5 +83,68 @@ class ArchiveToGradleModuleModel(val project: Project, private val projectSyncIn
       assert(ApplicationManager.getApplication().isDispatchThread)
       projectSyncInvoker.syncProject(project)
     }
+  }
+
+  /** Wraps archive in a Gradle module. */
+  private fun createModuleFromArchive(
+    project: Project, gradlePath: String, archivePath: File, move: Boolean, containingModule: Module?
+  ) {
+    writeCommandAction(project)
+      .withName("create module $gradlePath")
+      .withGlobalUndo()
+      .withUndoConfirmationPolicy(UndoConfirmationPolicy.REQUEST_CONFIRMATION).run<Throwable> {
+        @Suppress("DEPRECATION") // TODO: maybe we need to look into asking the user for where they want the module to be placed.
+        val projectBaseDir = project.guessProjectDir() ?: project.baseDir
+        val moduleLocation = GradleUtil.getModuleDefaultPath(projectBaseDir, gradlePath)
+        try {
+          val moduleRoot = VfsUtil.createDirectoryIfMissing(moduleLocation.absolutePath)
+          val sourceFile = VfsUtil.findFileByIoFile(archivePath, true)
+          if (sourceFile != null && moduleRoot != null) {
+            if (move) {
+              sourceFile.move(this, moduleRoot)
+            }
+            else {
+              sourceFile.copy(this, moduleRoot, sourceFile.name)
+            }
+            val buildGradle = moduleRoot.createChildData(null, SdkConstants.FN_BUILD_GRADLE)
+            VfsUtil.saveText(buildGradle, getBuildGradleText(archivePath))
+            GradleSettingsFile.getOrCreate(project).addModule(gradlePath, VfsUtilCore.virtualToIoFile(moduleRoot))
+            if (move && containingModule != null) {
+              addDependency(containingModule, gradlePath, archivePath)
+            }
+          }
+        }
+        catch (e: IOException) {
+          logger<ArchiveToGradleModuleModel>().error(e)
+        }
+      }
+  }
+
+  private fun filterDependencyStatement(dependency: Dependency, moduleRoot: File, archivePath: File): Dependency? {
+    val rawArguments: Any? = dependency.data
+    if (dependency.type == Dependency.Type.FILES && rawArguments != null) {
+      val data = if (rawArguments is Array<*>) rawArguments as Array<String>
+      else arrayOf(rawArguments.toString())
+      val list = data.filterNot { jarFile ->
+        val path = File(jarFile).takeIf { it.isAbsolute } ?: File(moduleRoot, jarFile)
+        FileUtil.filesEqual(path, archivePath)
+      }
+      return when {
+        list.isEmpty() -> null
+        list.size == 1 -> Dependency(dependency.scope, dependency.type, list[0])
+        else -> Dependency(dependency.scope, dependency.type, list.toTypedArray())
+      }
+    }
+    return dependency
+  }
+
+  private fun addDependency(module: Module, gradlePath: String, archivePath: File) {
+    // TODO(qumeric): use GradleBuildModel instead
+    val buildFile = GradleBuildFile.get(module) ?: throw IOException("Missing " + SdkConstants.FN_BUILD_GRADLE)
+    val moduleRoot = buildFile.file.parent.toIoFile()
+    val newDeps = buildFile.dependencies.mapNotNull { filterDependencyStatement(it as Dependency, moduleRoot, archivePath) }.toMutableList()
+    val scope = Dependency.Scope.getDefaultScope(module.project)
+    newDeps.add(Dependency(scope, Dependency.Type.MODULE, gradlePath))
+    buildFile.setValue(BuildFileKey.DEPENDENCIES, newDeps)
   }
 }
