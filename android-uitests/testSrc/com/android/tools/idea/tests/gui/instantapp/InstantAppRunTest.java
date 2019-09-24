aa73c0181c607965e04bab77277774a919ad427c
Jingjing Liu <jingjingliu@google.com>
2019-09-24 09:57:11 -0700

    Migrate InstantAppRunFromCmdLineTest to Bazel
    Todo: Will migrate InstantAppRunTest.java shortly after this.
    
    Bug: NA
    Test: Ran this test with bazel, passed
    QA_ANDROID_SDK_ROOT=<Your-local-directory>/Android/Sdk
    tools/base/bazel/bazel test --nocache_test_results
    //tools/adt/idea/android-uitests:InstantAppRunFromCmdLineTest
    --define external_emulator=true --config=remote
    
    Change-Id: If9dce45ead00e1719945af21d9f6c4c0dc475390

--- android-uitests/testSrc/com/android/tools/idea/tests/gui/instantapp/InstantAppRunTest.java
+++ android-uitests/testSrc/com/android/tools/idea/tests/gui/instantapp/InstantAppRunTest.java
@@ -15,16 +15,6 @@
  */
 package com.android.tools.idea.tests.gui.instantapp;
 
-import static com.android.tools.idea.tests.gui.instantapp.InstantAppRunTestHelpersKt.isActivityWindowOnTop;
-import static com.android.tools.idea.tests.gui.instantapp.InstantAppRunTestHelpersKt.prepareAdbInstall;
-import static com.android.tools.idea.tests.gui.instantapp.InstantAppRunTestHelpersKt.prepareAdbInstantAppLaunchIntent;
-import static com.android.tools.idea.tests.gui.instantapp.InstantAppRunTestHelpersKt.waitForAppInstalled;
-
-import com.android.SdkConstants;
-import com.android.ddmlib.AndroidDebugBridge;
-import com.android.ddmlib.IDevice;
-import com.android.sdklib.repository.AndroidSdkHandler;
-import com.android.tools.idea.sdk.AndroidSdks;
 import com.android.tools.idea.tests.gui.emulator.EmulatorTestRule;
 import com.android.tools.idea.tests.gui.framework.GuiTestRule;
 import com.android.tools.idea.tests.gui.framework.RunIn;
@@ -33,14 +23,10 @@ import com.android.tools.idea.tests.gui.framework.emulator.AvdSpec;
 import com.android.tools.idea.tests.gui.framework.emulator.EmulatorGenerator;
 import com.android.tools.idea.tests.gui.framework.fixture.ExecutionToolWindowFixture;
 import com.android.tools.idea.tests.gui.framework.fixture.IdeFrameFixture;
-import com.android.tools.idea.tests.gui.framework.fixture.avdmanager.AvdManagerDialogFixture;
 import com.android.tools.idea.tests.gui.framework.fixture.avdmanager.ChooseSystemImageStepFixture.SystemImage;
-import com.intellij.openapi.util.io.FileUtil;
 import com.intellij.testGuiFramework.framework.GuiTestRemoteRunner;
-import java.io.File;
 import java.util.concurrent.TimeUnit;
 import java.util.regex.Pattern;
-import org.fest.swing.timing.Wait;
 import org.fest.swing.util.PatternTextMatcher;
 import org.junit.Rule;
 import org.junit.Test;
@@ -90,75 +76,5 @@ public class InstantAppRunTest {
     runWindow.waitForOutput(new PatternTextMatcher(CONNECTED_APP_PATTERN), TimeUnit.MINUTES.toSeconds(2));
 
     runWindow.waitForStopClick();
-  }
-
-  /**
-   * Verifies instant apps can be launched from the command line
-   *
-   * <p>TT ID: 769cb342-c858-43f7-91c1-f4117e4f7627
-   *
-   * <pre>
-   *   Test steps:
-   *   1. Create and start an emulator running API 26 or newer.
-   *   2. Retrieve prebuilt instant APKs from test project directory
-   *   3. Install prebuilt APKs
-   *   4. Launch instant app by using an implicit intent through the command line
-   *   Verify:
-   *   1. Dump emulator window information using adb to check if the
-   *      instant app's activity is on top.
-   * </pre>
-   */
-  @Test
-  @RunIn(TestGroup.QA_UNRELIABLE) // b/114304149, fast
-  public void runFromCmdLine() throws Exception {
-    IdeFrameFixture ideFrame = guiTest.importProject("TopekaInstantApp")
-      .waitForGradleProjectSyncToFinish();
-
-    String avdName = EmulatorGenerator.ensureAvdIsCreated(
-      ideFrame.invokeAvdManager(),
-      new AvdSpec.Builder()
-        .setSystemImageGroup(AvdSpec.SystemImageGroups.X86)
-        .setSystemImageSpec(O_AVD_IMAGE)
-        .build()
-    );
-
-    AvdManagerDialogFixture avdManager = ideFrame.invokeAvdManager();
-    avdManager.startAvdWithName(avdName);
-    avdManager.close();
-
-    // TODO: Move these adb commands over to DeviceQueries and AndroidDebugBridgeUtils
-    AndroidSdkHandler sdkHandler = AndroidSdks.getInstance().tryToChooseSdkHandler();
-    File adbBinary = new File(sdkHandler.getLocation(), FileUtil.join(SdkConstants.OS_SDK_PLATFORM_TOOLS_FOLDER, SdkConstants.FN_ADB));
-    File prebuiltApks = new File(ideFrame.getProjectPath(), "prebuilt");
-
-    AndroidDebugBridge adb = AndroidDebugBridge.createBridge(adbBinary.getAbsolutePath(), false);
-    Wait.seconds(120)
-      .expecting("emulator to start")
-      .until(() -> adb.getDevices().length > 0);
-
-    IDevice[] devices = adb.getDevices();
-
-    Wait.seconds(120)
-      .expecting("emulator to finish booting")
-      .until(() -> devices[0].isOnline() && devices[0].getProperty("dev.bootcomplete") != null);
-
-    ProcessBuilder installCommand = prepareAdbInstall(adbBinary, prebuiltApks.listFiles());
-    installCommand.inheritIO();
-    installCommand.start().waitFor(10, TimeUnit.SECONDS);
-
-    String expectedAppId = "com.google.samples.apps.topeka";
-    waitForAppInstalled(devices[0], expectedAppId);
-
-    ProcessBuilder launchCommand = prepareAdbInstantAppLaunchIntent(adbBinary);
-    launchCommand.inheritIO();
-    launchCommand.start().waitFor(10, TimeUnit.SECONDS);
-
-    Wait.seconds(10)
-      .expecting("instant app activity to be launched and shown")
-      .until(() ->
-        isActivityWindowOnTop(
-          devices[0],
-          expectedAppId + "/.activity.SignInActivity")
-      );
   }
 }
