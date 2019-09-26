5a9db11e3479c046a02b3d41c0c0193c7785360c
kovalp <kovalp@google.com>
2019-09-26 15:23:27 +0100

    Regenerate parser code androidSql.bnf with new version of Grammar-Kit
    Fixes: n/a
    Test: existing
    Change-Id: Ia11d04de982023e616aa04237f7c673fdd3838f9

--- android-lang/gen/com/android/tools/idea/lang/androidSql/psi/AndroidSqlBindParameter.java
+++ android-lang/gen/com/android/tools/idea/lang/androidSql/psi/AndroidSqlBindParameter.java
@@ -18,6 +18,7 @@
 
 package com.android.tools.idea.lang.androidSql.psi;
 
+import java.util.List;
 import org.jetbrains.annotations.*;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiReference;
