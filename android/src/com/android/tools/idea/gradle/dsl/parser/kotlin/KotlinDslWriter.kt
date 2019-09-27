d11c6aa8b4484e47387c488bb91bf0d93b1c85db
Christophe Rhodes <xof@google.com>
2019-09-27 13:28:59 +0000

    Revert "[Dsl,.kts] fix writing to the front of blocks"
    This reverts commit 23017760b7be8e328ffba216f0ba13c9ae23d1a0.
    
    Reason for revert: breaks ArtifactDependencyTest[Kotlin]
    
    Change-Id: I8908e621c064a5c81954cf6e1025346fe5b0e5c9

--- android/src/com/android/tools/idea/gradle/dsl/parser/kotlin/KotlinDslWriter.kt
+++ android/src/com/android/tools/idea/gradle/dsl/parser/kotlin/KotlinDslWriter.kt
@@ -220,12 +220,11 @@ class KotlinDslWriter : KotlinDslNameConverter, GradleDslWriter {
         parentPsiElement.addBefore(lineTerminator, addedElement)
       }
       is KtBlockExpression -> {
+        addedElement = parentPsiElement.addAfter(statement, anchor)
         if (anchorAfter != null) {
-          addedElement = parentPsiElement.addAfter(statement, anchor)
           parentPsiElement.addBefore(lineTerminator, addedElement)
         }
         else {
-          addedElement = parentPsiElement.addBefore(statement, anchor)
           parentPsiElement.addAfter(lineTerminator, addedElement)
         }
       }
