a4990855696ac0bbeb8aafa7328ee275a046b57c
Oscar Adame <oscarad@google.com>
2019-09-27 15:22:18 -0700

    [Res Manager] Grid/list/zoom buttons should be part of the list view
    Currently, these buttons are part of ResourceExplorerView but should be
    part of ResourceExplorerListView, when opening a resource with multiple
    configurations, these buttons should not be available, but because they
    were not part of the correct panel, they didn't go away.
    
    Bug: 141776568
    Test: manually, ui-tweak
    Change-Id: Ibae21d806109e091431fdaf27a14da021f22fa1b
    (cherry picked from commit cab9408d29af75533208de9f6e87f4c719005b7c)

--- android/src/com/android/tools/idea/ui/resourcemanager/explorer/ResourceExplorerListView.kt
+++ android/src/com/android/tools/idea/ui/resourcemanager/explorer/ResourceExplorerListView.kt
@@ -35,9 +35,15 @@ import com.intellij.ide.dnd.DnDManager
 import com.intellij.openapi.Disposable
 import com.intellij.openapi.actionSystem.ActionGroup
 import com.intellij.openapi.actionSystem.ActionManager
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.DataProvider
+import com.intellij.openapi.actionSystem.DefaultActionGroup
+import com.intellij.openapi.actionSystem.Separator
+import com.intellij.openapi.actionSystem.ToggleAction
 import com.intellij.openapi.application.ModalityState
 import com.intellij.openapi.module.ModuleManager
+import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.util.SystemInfo
 import com.intellij.openapi.util.text.StringUtil
 import com.intellij.openapi.vfs.VirtualFile
@@ -52,6 +58,7 @@ import com.intellij.ui.speedSearch.NameFilteringListModel
 import com.intellij.util.concurrency.EdtExecutorService
 import com.intellij.util.ui.JBUI
 import com.intellij.util.ui.UIUtil
+import icons.StudioIcons
 import java.awt.BorderLayout
 import java.awt.Component
 import java.awt.Container
@@ -81,10 +88,12 @@ import kotlin.math.min
 import kotlin.math.roundToInt
 import kotlin.properties.Delegates
 
+private const val DEFAULT_GRID_MODE = false
+
+private val DEFAULT_LIST_MODE_WIDTH get() = JBUI.scale(60)
 private val MAX_CELL_WIDTH get() = JBUI.scale(300)
 private val LIST_CELL_SIZE get() = JBUI.scale(60)
 private val MIN_CELL_WIDTH get() = JBUI.scale(150)
-private const val DEFAULT_GRID_MODE = false
 private val DEFAULT_CELL_WIDTH get() = LIST_CELL_SIZE
 private val SECTION_HEADER_SECONDARY_COLOR get() = JBColor.border()
 
@@ -144,7 +153,7 @@ class ResourceExplorerListView(
   private var fileToSelect: VirtualFile? = null
   private var resourceToSelect: String? = null
 
-  var previewSize = DEFAULT_CELL_WIDTH
+  private var previewSize = DEFAULT_CELL_WIDTH
     set(value) {
       if (value != field) {
         field = value
@@ -154,7 +163,7 @@ class ResourceExplorerListView(
       }
     }
 
-  var gridMode: Boolean by Delegates.observable(
+  private var gridMode: Boolean by Delegates.observable(
     DEFAULT_GRID_MODE) { _, _, newValue ->
     val backgroundColor = if (newValue) GRID_MODE_BACKGROUND else LIST_MODE_BACKGROUND
     centerPanel.background = backgroundColor
@@ -210,10 +219,19 @@ class ResourceExplorerListView(
     }
   }
 
+  private val footerPanel = JPanel(BorderLayout()).apply {
+    border = JBUI.Borders.customLine(JBColor.border(), 1, 0, 0, 0)
+
+    add(ActionManager.getInstance().createActionToolbar(
+      "resourceExplorer",
+      createBottomActions(), true).component, BorderLayout.EAST)
+  }
+
   private val contentPanel: JPanel =
     JPanel(BorderLayout()).apply {
       add(topActionsPanel, BorderLayout.NORTH)
       add(centerPanel)
+      add(footerPanel, BorderLayout.SOUTH)
     }
 
   /**
@@ -668,5 +686,79 @@ class ResourceExplorerListView(
     DnDManager.getInstance().unregisterTarget(resourceImportDragTarget, this)
     populateResourcesFuture?.cancel(true)
     searchFuture?.cancel(true)
+  }
+
+  private fun createBottomActions(): DefaultActionGroup {
+    return DefaultActionGroup(
+      ListModeButton(),
+      GridModeButton(),
+      Separator(),
+      ZoomMinus(),
+      ZoomPlus()
+    )
+  }
+
+  /**
+   * Button to enable the list view
+   */
+  private inner class ListModeButton
+    : ToggleAction("List mode", "Switch to list mode", StudioIcons.Common.LIST_VIEW),
+      DumbAware {
+
+    override fun isSelected(e: AnActionEvent) = !gridMode
+
+    override fun setSelected(e: AnActionEvent, state: Boolean) {
+      if (state) {
+        ResourceManagerTracking.logSwitchToListMode()
+        gridMode = false
+        previewSize = DEFAULT_LIST_MODE_WIDTH
+      }
+    }
+  }
+
+  /**
+   * Button to enable the grid view
+   */
+  private inner class GridModeButton
+    : ToggleAction("Grid mode", "Switch to grid mode", StudioIcons.Common.GRID_VIEW),
+      DumbAware {
+
+    override fun isSelected(e: AnActionEvent) = gridMode
+
+    override fun setSelected(e: AnActionEvent, state: Boolean) {
+      if (state) {
+        ResourceManagerTracking.logSwitchToGridMode()
+        gridMode = true
+        previewSize = MIN_CELL_WIDTH
+      }
+    }
+  }
+
+  /**
+   * Button to scale down the icons. It is only enabled in grid mode.
+   */
+  private inner class ZoomMinus : AnAction("Zoom Out", "Decrease thumbnail size", AllIcons.General.ZoomOut), DumbAware {
+
+    override fun actionPerformed(e: AnActionEvent) {
+      previewSize = max(MIN_CELL_WIDTH, (previewSize * 0.9).roundToInt())
+    }
+
+    override fun update(e: AnActionEvent) {
+      e.presentation.isEnabled = gridMode && previewSize > MIN_CELL_WIDTH
+    }
+  }
+
+  /**
+   * Button to scale up the icons. It is only enabled in grid mode.
+   */
+  private inner class ZoomPlus : AnAction("Zoom In", "Increase thumbnail size", AllIcons.General.ZoomIn), DumbAware {
+
+    override fun actionPerformed(e: AnActionEvent) {
+      previewSize = min(MAX_CELL_WIDTH, (previewSize * 1.1).roundToInt())
+    }
+
+    override fun update(e: AnActionEvent) {
+      e.presentation.isEnabled = gridMode && previewSize < MAX_CELL_WIDTH
+    }
   }
 }
