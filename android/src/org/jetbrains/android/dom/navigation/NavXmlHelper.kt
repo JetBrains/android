042de5c2610cd6193c7f809edef9a8288f2a4932
Mike Moroney <mmoro@google.com>
2019-09-30 12:31:21 -0700

    Recursion for rendering NavHostFragment
    When determining the layout to be used for rendering
    a NavHostFragment, recurse into the start destinations
    until a non-navigation destination is found.
    
    Fixes: 140953238
    Test: testNavigationNestedStartDestination
    Change-Id: I241ab360ab85532f7d65057188cbbee40dc932f1
    (cherry picked from commit b28b13f9bd7ecc7b46c19220295d77fbd98e5e6c)

--- android/src/org/jetbrains/android/dom/navigation/NavXmlHelper.kt
+++ android/src/org/jetbrains/android/dom/navigation/NavXmlHelper.kt
@@ -15,9 +15,12 @@
  */
 package org.jetbrains.android.dom.navigation
 
+import com.android.SdkConstants.ANDROID_URI
 import com.android.SdkConstants.ATTR_ID
 import com.android.SdkConstants.ATTR_LAYOUT
 import com.android.SdkConstants.ATTR_START_DESTINATION
+import com.android.SdkConstants.AUTO_URI
+import com.android.SdkConstants.TAG_NAVIGATION
 import com.android.SdkConstants.TOOLS_URI
 import com.android.ide.common.resources.ResourceResolver
 import com.android.ide.common.resources.stripPrefixFromId
@@ -37,15 +40,25 @@ fun getStartDestLayoutId(navResourceId: String, project: Project, resourceResolv
   val file = LocalFileSystem.getInstance().findFileByPath(fileName) ?: return null
   val psiFile = AndroidPsiUtils.getPsiFileSafely(project, file) as? XmlFile ?: return null
   return ApplicationManager.getApplication().runReadAction(Computable<String> {
-    val startDestId = psiFile.rootTag?.attributes?.firstOrNull { it.localName == ATTR_START_DESTINATION }?.value?.let(::stripPrefixFromId)
-    if (startDestId != null) {
-      val startDest = psiFile.rootTag
-        ?.children
-        ?.filterIsInstance(XmlTag::class.java)
-        ?.firstOrNull { it.attributes.firstOrNull { tag -> tag.localName == ATTR_ID }?.value?.let(::stripPrefixFromId) == startDestId }
-      startDest?.getAttributeValue(ATTR_LAYOUT, TOOLS_URI)
-    } else {
-      null
-    }
+    findStartDestination(psiFile.rootTag)?.getAttributeValue(ATTR_LAYOUT, TOOLS_URI)
   })
+}
+
+/*
+Start at the given tag and recurse into the start destination attribute
+until we reach a tag that is not a navigation element. Return the tag or null
+if the attribute or the tag is missing.
+ */
+private fun findStartDestination(root: XmlTag?): XmlTag? {
+  var current = root
+  while (current?.name == TAG_NAVIGATION) {
+    val startDestId = current.getAttributeValue(ATTR_START_DESTINATION, AUTO_URI)?.let(::stripPrefixFromId) ?: return null
+
+    current = current
+      .children
+      .filterIsInstance(XmlTag::class.java)
+      .firstOrNull { it.getAttributeValue(ATTR_ID, ANDROID_URI)?.let(::stripPrefixFromId) == startDestId }
+  }
+
+  return current
 }
