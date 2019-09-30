61a3e2025df86842829318941e677b10bb583cf3
Oscar Adame <oscarad@google.com>
2019-09-30 11:27:41 -0700

    [Asset Studio] Provider class for new material icons in IconPickerDialog
    Provider class that call MaterialIconsLoader asynchronously and handles
    getting the metadata file for it. In general this class will handle the
    logic to provide material icons to the ui (IconPickerDialog).
    
    Also added necessary Gson parser for the icons' metadata.
    
    Moved and added some useful interfaces for testing. As well as some
    utility methods.
    
    Bug: 141629663
    Test: MaterialVdIconsProviderTest, MaterialIconsMetadataTest
    Change-Id: I54d9fa5209db5ce1ed364f94ab4e8994d23797c0

--- android/src/com/android/tools/idea/material/icons/MaterialIconsMetadata.kt
+++ android/src/com/android/tools/idea/material/icons/MaterialIconsMetadata.kt
@@ -15,6 +15,13 @@
  */
 package com.android.tools.idea.material.icons
 
+import com.google.gson.GsonBuilder
+import com.google.gson.JsonDeserializationContext
+import com.google.gson.JsonDeserializer
+import com.google.gson.JsonElement
+import java.io.Reader
+import java.lang.reflect.Type
+
 /**
  * Metadata for the Material design icons, based on the metadata file obtained from http://fonts.google.com/metadata/icons.
  */
@@ -23,15 +30,70 @@ class MaterialIconsMetadata(
   val urlPattern: String,
   val families: Array<String>,
   val icons: Array<MaterialMetadataIcon>
-)
+) {
+  companion object {
+    fun parse(reader: Reader): MaterialIconsMetadata {
+      return with(GsonBuilder()
+                    .registerTypeAdapter(MaterialIconsMetadata::class.java, MetadataDeserializer())
+                    .registerTypeAdapter(MaterialMetadataIcon::class.java, IconDeserializer())
+                    .generateNonExecutableJson()
+                    .create()) {
+        fromJson<MaterialIconsMetadata>(reader, MaterialIconsMetadata::class.java)
+      }
+    }
+  }
+}
 
 /**
  * Metadata of each icon within [MaterialIconsMetadata.icons].
  */
-class MaterialMetadataIcon (
+class MaterialMetadataIcon(
   val name: String,
   val version: Int,
   val unsupportedFamilies: Array<String>,
   val categories: Array<String>,
   val tags: Array<String>
 )
+
+/**
+ * [JsonDeserializer] for [MaterialIconsMetadata].
+ */
+private class MetadataDeserializer : JsonDeserializer<MaterialIconsMetadata?> {
+  private val hostKey = "host"
+  private val urlPatternKey = "asset_url_pattern"
+  private val familiesKey = "families"
+  private val iconsKey = "icons"
+
+  override fun deserialize(json: JsonElement?, typeOfT: Type?, context: JsonDeserializationContext?): MaterialIconsMetadata? {
+    if (json == null || typeOfT == null || context == null) return null
+    val jsonObject = json.asJsonObject
+    val host = jsonObject[hostKey].asString
+    val urlPattern = jsonObject[urlPatternKey].asString
+    val families = context.deserialize<Array<String>>(jsonObject[familiesKey], Array<String>::class.java)
+    val icons = context.deserialize<Array<MaterialMetadataIcon>>(jsonObject[iconsKey], Array<MaterialMetadataIcon>::class.java)
+    return MaterialIconsMetadata(host, urlPattern, families, icons)
+  }
+}
+
+/**
+ * [JsonDeserializer] for [MaterialMetadataIcon].
+ */
+private class IconDeserializer : JsonDeserializer<MaterialMetadataIcon?> {
+  private val nameKey = "name"
+  private val versionKey = "version"
+  private val unsupportedFamiliesKey = "unsupported_families"
+  private val categoriesKey = "categories"
+  private val tagsKey = "tags"
+
+  override fun deserialize(json: JsonElement?, typeOfT: Type?, context: JsonDeserializationContext?): MaterialMetadataIcon? {
+    if (json == null || typeOfT == null || context == null) return null
+    val jsonObject = json.asJsonObject
+    val name = jsonObject[nameKey].asString
+    val version = jsonObject[versionKey].asInt
+    val unsupportedFamilies = context.deserialize<Array<String>>(jsonObject[unsupportedFamiliesKey], Array<String>::class.java)
+    val categories = context.deserialize<Array<String>>(jsonObject[categoriesKey], Array<String>::class.java)
+    val tags = context.deserialize<Array<String>>(jsonObject[tagsKey], Array<String>::class.java)
+
+    return MaterialMetadataIcon(name, version, unsupportedFamilies, categories, tags)
+  }
+}
