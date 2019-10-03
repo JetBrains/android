3448970be9c29fb67531fa80fe269331bb40d9f4
Jens Ole Lauridsen <jlauridsen@google.com>
2019-10-03 09:46:44 -0700

    Update selection colors in MEJTable
    - the selection background should depend on the table having focus
    - the icons should be converted to white on blue background
    
    Bug: N/A
    Test: N/A
    Change-Id: I00fff80e4f8da3cd4cc4549873e29f6c5f3bd3ed

--- designer/src/com/android/tools/idea/uibuilder/handlers/motion/editor/adapters/MEJTable.java
+++ designer/src/com/android/tools/idea/uibuilder/handlers/motion/editor/adapters/MEJTable.java
@@ -15,15 +15,60 @@
  */
 package com.android.tools.idea.uibuilder.handlers.motion.editor.adapters;
 
-import com.intellij.openapi.ui.ComboBox;
+import com.android.tools.adtui.common.AdtSecondaryPanel;
+import com.android.tools.adtui.common.ColoredIconGenerator;
+import com.intellij.ui.components.JBLabel;
 import com.intellij.ui.table.JBTable;
+import com.intellij.util.ui.UIUtil;
+import java.awt.Color;
+import java.awt.Component;
+import javax.swing.Icon;
+import javax.swing.JPanel;
+import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableCellRenderer;
 
 /**
  * Abstraction of a JTable/JBTable
  */
-public class MEJTable extends JBTable{
+public class MEJTable extends JBTable {
   public MEJTable(DefaultTableModel model) {
     super(model);
+    setDefaultRenderer(Icon.class, new DefaultIconRenderer());
+  }
+
+  @Override
+  public Color getSelectionForeground() {
+    return UIUtil.getTableSelectionForeground(isFocusOwner());
+  }
+
+  @Override
+  public Color getSelectionBackground() {
+    return UIUtil.getTableSelectionBackground(isFocusOwner());
+  }
+
+  private static class DefaultIconRenderer implements TableCellRenderer {
+    private final JPanel myPanel;
+    private final JBLabel myLabel;
+
+    private DefaultIconRenderer() {
+      myPanel = new AdtSecondaryPanel();
+      myLabel = new JBLabel();
+      myPanel.add(myLabel);
+    }
+
+    @Override
+    public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+      Icon icon = (Icon)value;
+      boolean showFocus = table.isFocusOwner() && isSelected;
+      if (showFocus && icon != null) {
+        myLabel.setIcon(ColoredIconGenerator.INSTANCE.generateWhiteIcon(icon));
+      }
+      else {
+        myLabel.setIcon(icon);
+      }
+      myPanel.setBackground(isSelected ? table.getSelectionBackground() : table.getBackground());
+      return myPanel;
+    }
   }
 }
