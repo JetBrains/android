f8d892b6e4788b31554522d40d2a95408a424b36
Joe Baker-Malone <jbakermalone@google.com>
2019-10-03 18:08:29 -0700

    Moving tree loading code to LayoutInspector
    Right now LayoutInspector is responsibile for holding on to the
    LayoutInspectorModel, which is where the tree lives. Move the tree
    updating code to LayoutInspector.
    
    Test: none, just moving code
    Bug: none
    Change-Id: I725c439ea16a9d3980ee2b8d3d09a0f678f5c8fa

--- layout-inspector/src/com/android/tools/idea/layoutinspector/tree/LayoutInspectorTreePanel.kt
+++ layout-inspector/src/com/android/tools/idea/layoutinspector/tree/LayoutInspectorTreePanel.kt
@@ -21,33 +21,19 @@ import com.android.tools.componenttree.api.ComponentTreeModel
 import com.android.tools.componenttree.api.ComponentTreeSelectionModel
 import com.android.tools.componenttree.api.ViewNodeType
 import com.android.tools.idea.layoutinspector.LayoutInspector
-import com.android.tools.idea.layoutinspector.SkiaParser
-import com.android.tools.idea.layoutinspector.common.StringTable
-import com.android.tools.idea.layoutinspector.model.InspectorView
 import com.android.tools.idea.layoutinspector.model.ViewNode
-import com.android.tools.idea.layoutinspector.resource.ResourceLookup
 import com.android.tools.idea.layoutinspector.transport.InspectorClient
-import com.android.tools.layoutinspector.proto.LayoutInspectorProto.ComponentTreeEvent
-import com.android.tools.layoutinspector.proto.LayoutInspectorProto.LayoutInspectorEvent
-import com.android.tools.layoutinspector.proto.LayoutInspectorProto.View
-import com.android.tools.profiler.proto.Common
 import com.google.common.annotations.VisibleForTesting
 import com.intellij.openapi.actionSystem.ActionManager
 import com.intellij.openapi.actionSystem.IdeActions
 import com.intellij.openapi.actionSystem.KeyboardShortcut
 import com.intellij.openapi.application.ApplicationManager
-import com.intellij.openapi.diagnostic.Logger
-import com.intellij.util.ui.UIUtil
 import icons.StudioIcons
 import org.jetbrains.android.dom.AndroidDomElementDescriptorProvider
-import java.awt.Image
 import java.util.Collections
-import java.util.concurrent.TimeUnit
-import java.util.concurrent.atomic.AtomicLong
 import javax.swing.Icon
 import javax.swing.JComponent
 
-private val LOAD_TIMEOUT = TimeUnit.SECONDS.toMillis(20)
 const val GOTO_DEFINITION_ACTION_KEY = "gotoDefinition"
 
 class LayoutInspectorTreePanel : ToolContent<LayoutInspector> {
@@ -58,9 +44,6 @@ class LayoutInspectorTreePanel : ToolContent<LayoutInspector> {
 
   @VisibleForTesting
   val componentTreeSelectionModel: ComponentTreeSelectionModel
-
-  private val loadStartTime = AtomicLong(-1)
-  private val latestLoadTime = AtomicLong(-1)
 
   init {
     val builder = ComponentTreeBuilder()
@@ -84,8 +67,6 @@ class LayoutInspectorTreePanel : ToolContent<LayoutInspector> {
     layoutInspector = toolContext
     layoutInspector?.layoutInspectorModel?.modificationListeners?.add(this::modelModified)
     client = layoutInspector?.client
-    client?.register(Common.Event.EventGroupIds.COMPONENT_TREE, ::loadComponentTree)
-    client?.registerProcessChanged(::clearComponentTreeWhenProcessEnds)
     toolContext?.layoutInspectorModel?.selectionListeners?.add(this::selectionChanged)
   }
 
@@ -99,125 +80,6 @@ class LayoutInspectorTreePanel : ToolContent<LayoutInspector> {
     val node = componentTreeSelectionModel.selection.singleOrNull() as? ViewNode ?: return
     val location = resourceLookup.findFileLocation(node) ?: return
     location.navigatable?.navigate(true)
-  }
-
-  private fun clearComponentTreeWhenProcessEnds() {
-    if (client?.isConnected == true) {
-      return
-    }
-    val application = ApplicationManager.getApplication()
-    application.invokeLater {
-      layoutInspector?.layoutInspectorModel?.update(null)
-    }
-  }
-
-  private fun loadComponentTree(event: LayoutInspectorEvent) {
-    val time = System.currentTimeMillis()
-    if (time - loadStartTime.get() < LOAD_TIMEOUT) {
-      return
-    }
-    val root = try {
-      val loader = ComponentTreeLoader(event.tree, layoutInspector?.layoutInspectorModel?.resourceLookup)
-      val rootView = loader.loadRootView()
-      val bytes = client?.getPayload(event.tree.payloadId) ?: return
-      var viewRoot: InspectorView? = null
-      if (bytes.isNotEmpty()) {
-        try {
-          viewRoot = SkiaParser.getViewTree(bytes)
-          if (viewRoot != null && viewRoot.id.isEmpty()) {
-            // We were unable to parse the skia image. Allow the user to interact with the component tree.
-            viewRoot = null
-          }
-        }
-        catch (ex: Exception) {
-          Logger.getInstance(LayoutInspectorTreePanel::class.java).warn(ex)
-        }
-      }
-      if (viewRoot != null) {
-        val imageLoader = ComponentImageLoader(rootView, viewRoot)
-        imageLoader.loadImages()
-      }
-      rootView
-    }
-    finally {
-      loadStartTime.set(0)
-    }
-
-    ApplicationManager.getApplication().invokeLater {
-      synchronized(latestLoadTime) {
-        if (latestLoadTime.get() > time) {
-          return@invokeLater
-        }
-        latestLoadTime.set(time)
-        layoutInspector?.layoutInspectorModel?.update(root)
-      }
-    }
-  }
-
-  class ComponentImageLoader(root: ViewNode, viewRoot: InspectorView) {
-    private val nodeMap = root.flatten().associateBy { it.drawId }
-    private val viewMap = viewRoot.flatten().associateBy { it.id.toLong() }
-
-    fun loadImages() {
-      for ((drawId, node) in nodeMap) {
-        val view = viewMap[drawId] ?: continue
-        node.imageBottom = view.image
-        addChildNodeImages(node, view)
-      }
-    }
-
-    private fun addChildNodeImages(node: ViewNode, view: InspectorView) {
-      var beforeChildren = true
-      for (child in view.children.values) {
-        val isChildNode = view.id != child.id && nodeMap.containsKey(child.id.toLong())
-        when {
-          isChildNode -> beforeChildren = false
-          beforeChildren -> node.imageBottom = combine(node.imageBottom, child)
-          else -> node.imageTop = combine(node.imageTop, child)
-        }
-        if (!isChildNode) {
-          // Some Skia views are several levels deep:
-          addChildNodeImages(node, child)
-        }
-      }
-    }
-
-    private fun combine(image: Image?, view: InspectorView): Image? =
-      when {
-        view.image == null -> image
-        image == null -> view.image
-        else -> {
-          // Combine the images...
-          val g = image.graphics
-          UIUtil.drawImage(g, view.image!!, 0, 0, null)
-          g.dispose()
-          image
-        }
-      }
-  }
-
-  private class ComponentTreeLoader(private val tree: ComponentTreeEvent, private val resourceLookup: ResourceLookup?) {
-    val stringTable = StringTable(tree.stringList)
-
-    fun loadRootView(): ViewNode {
-      resourceLookup?.updateConfiguration(tree.resources, stringTable)
-      return loadView(tree.root, null)
-    }
-
-    fun loadView(view: View, parent: ViewNode?): ViewNode {
-      val qualifiedName = "${stringTable[view.packageName]}.${stringTable[view.className]}"
-      val viewId = stringTable[view.viewId]
-      val textValue = stringTable[view.textValue]
-      val layout = stringTable[view.layout]
-      val x = view.x + (parent?.x ?: 0)
-      val y = view.y + (parent?.y ?: 0)
-      val node = ViewNode(view.drawId, qualifiedName, layout, x, y, view.width, view.height, viewId, textValue)
-      view.subViewList.map { loadView(it, node) }.forEach {
-        node.children.add(it)
-        it.parent = node
-      }
-      return node
-    }
   }
 
   @Suppress("UNUSED_PARAMETER")
