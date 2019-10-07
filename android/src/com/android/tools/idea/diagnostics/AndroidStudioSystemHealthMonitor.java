27d39647a75d7fffe4696ba52915efa2d664553d
Radomir Malaczek <radomirm@google.com>
2019-10-07 14:26:20 -0700

    Fix NPE when action is in unnamed package
    Class.getPackage() returns null if the class is in unnamed package.
    
    Fixes: 130785855
    Test: Manual
    Change-Id: I3223d2ebd2ae5233f8162be80ea72bfac6a45073

--- android/src/com/android/tools/idea/diagnostics/AndroidStudioSystemHealthMonitor.java
+++ android/src/com/android/tools/idea/diagnostics/AndroidStudioSystemHealthMonitor.java
@@ -1034,7 +1034,8 @@ public class AndroidStudioSystemHealthMonitor implements BaseComponent {
     while (currentClass.isAnonymousClass()) {
       currentClass = currentClass.getEnclosingClass();
     }
-    String packageName = currentClass.getPackage().getName();
+    Package classPackage = currentClass.getPackage();
+    String packageName = classPackage != null ? classPackage.getName() : "";
     if (packageName.startsWith("com.android.") || packageName.startsWith("com.intellij.") || packageName.startsWith("org.jetbrains.") ||
         packageName.startsWith("or.intellij.") || packageName.startsWith("com.jetbrains.") || packageName.startsWith("git4idea.")) {
 
