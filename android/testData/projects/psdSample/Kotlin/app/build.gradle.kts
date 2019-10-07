765470523e5f695737f935809efb317e0eb8b6c7
Christophe Rhodes <xof@google.com>
2019-10-07 15:56:26 +0100

    [PSD] duplicate sample project in Kotlin
    Exactly like the current psdSample/Groovy project, but with some build
    scripts expressed in Kotlin rather than Groovy.
    
    At present, this copy only has the root and app scripts converted to
    build.gradle.kts, and those conversions are slightly lossy:
    
    - I do not understand the evaluation model for extra properties.  The
      root build.gradle.kts (and the Groovy on which this is based) had an
      extra property initialization from a bare `variable', defined
      textually later on in the buildscript closure as an extra property.
      Clearly the evaluation model is not as I expect, because this syncs
      fine, even though I would have expected the initialization of
      rootBool to have occurred before scriptBool is even defined as an
      extra property...
    
    - the call to setTestFunctionalTest() within the "paid" product flavor
      requires an explicit type coercion.  There might be a way to avoid
      it, perhaps by defining a variable with a type by delegation?
    
    Override the KOTLIN_DSL_PARSING StudioFlag in AndroidGradleTestCase
    setUp() (and clear it in tearDown()).  It may end up being deleted
    before this change is submitted, but just in case let's do things
    right.
    
    Bug: 130224064
    Test: DefaultConfig and ProductFlavor descriptor tests
    Change-Id: Id3954bdd17d8d5a75c759be84321f04b498d4f1a

---
+++ android/testData/projects/psdSample/Kotlin/app/build.gradle.kts
@@ -0,0 +1,96 @@
+plugins {
+    id("com.android.application")
+}
+
+val myVariable by extra("26.1.0")
+val variable1 by extra("1.3")
+val anotherVariable by extra("3.0.1")
+val varInt by extra(1)
+val varBool by extra(true)
+val varRefString by extra(variable1)
+val varProGuardFiles by extra(listOf("proguard-rules.txt", "proguard-rules2.txt"))
+
+android {
+    compileSdkVersion(19)
+    buildToolsVersion("19.1.0")
+
+    dynamicFeatures = mutableSetOf(":dyn_feature")
+
+    signingConfigs {
+        create("myConfig") {
+            storeFile = file("debug.keystore")
+            storePassword = "android"
+            keyAlias = "androiddebugkey"
+            keyPassword = "android"
+        }
+    }
+
+    defaultConfig {
+        applicationId = "com.example.psd.sample.app.default"
+        applicationIdSuffix = "defaultSuffix"
+        testApplicationId = "com.example.psd.sample.app.default.test"
+        maxSdkVersion(26)
+        minSdkVersion(9)
+        targetSdkVersion(19)
+        versionCode = 1
+        versionName = "1.0"
+        versionNameSuffix = "vns"
+        manifestPlaceholders = mapOf("aa" to "aaa", "bb" to "bbb", "cc" to true)
+        setTestFunctionalTest(false)
+    }
+    buildTypes {
+        getByName("release") {
+            applicationIdSuffix = "suffix"
+            versionNameSuffix = "vsuffix"
+            isDebuggable = false
+            isJniDebuggable = false
+            isMinifyEnabled = false
+            renderscriptOptimLevel = 2
+            signingConfig = signingConfigs.getByName("myConfig")
+            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.txt", "proguard-rules2.txt")
+        }
+        create("specialRelease") {
+            matchingFallbacks = listOf("release", "debug")
+            versionNameSuffix = "vnsSpecial"
+        }
+    }
+    flavorDimensions("foo", "bar")
+    productFlavors {
+        create("basic") {
+            setDimension("foo")
+            applicationId = "com.example.psd.sample.app"
+        }
+        create("paid") {
+            setDimension("foo")
+            applicationId = "com.example.psd.sample.app.paid"
+            testApplicationId = "com.example.psd.sample.app.paid.test"
+            maxSdkVersion(25)
+            minSdkVersion(10)
+            targetSdkVersion(20)
+            versionCode = 2
+            versionName = "2.0"
+            versionNameSuffix = "vnsFoo"
+            testInstrumentationRunnerArguments = mapOf("a" to "AAA", "b" to "BBB", "c" to "CCC")
+            setTestHandleProfiling(varBool)
+            setTestFunctionalTest(rootProject.extra["rootBool"] as Boolean)
+        }
+        create("bar") {
+            setDimension("bar")
+            setApplicationIdSuffix("barSuffix")
+        }
+        create("otherBar") {
+            setDimension("bar")
+            matchingFallbacks = listOf("bar")
+            resConfig("en")
+            resConfigs("hdpi", "xhdpi")
+        }
+    }
+}
+
+val moreVariable by extra("1234")
+val mapVariable by extra(mapOf("a" to "\"double\" quotes", "b" to "'single' quotes"))
+
+dependencies {
+    api("com.android.support:appcompat-v7:+")
+    api(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+}
