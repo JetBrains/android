cacfccf9a0261e2c751d39a6e8f645c7f3c7ab23
Yuriy Solodkyy <solodkyy@google.com>
2019-10-07 17:34:38 +0100

    Add a test project created and synced in 3.6.
    This is to enable backward compatibility testing in newer versions.
    
    Bug: 142082563
    Test: n/a
    Change-Id: I6d5b193b83e4117bb2e1552b15fabadf31a91c13

--- android-test-framework/testSrc/com/android/tools/idea/testing/TestProjectPaths.java
+++ android-test-framework/testSrc/com/android/tools/idea/testing/TestProjectPaths.java
@@ -10,6 +10,7 @@ public final class TestProjectPaths {
   public static final String BUDDY_APKS = "projects/buddyApks";
   public static final String CENTRAL_BUILD_DIRECTORY = "projects/centralBuildDirectory";
   public static final String CIRCULAR_MODULE_DEPS = "projects/circularModuleDeps";
+  public static final String COMPATIBILITY_TESTS_AS_36 = "projects/compatibility_tests/AS36";
   public static final String COMPOSITE_BUILD = "projects/compositeBuild";
   public static final String DEPENDENT_MODULES = "projects/dependentModules";
   public static final String DEPENDENT_NATIVE_MODULES = "projects/dependentNativeModules";
---
+++ android/testData/projects/compatibility_tests/AS36/libs/android_library/src/main/java/com/example/android_library/SomeActivity.java
@@ -0,0 +1,14 @@
+package com.example.android_library;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_some);
+    }
+}
