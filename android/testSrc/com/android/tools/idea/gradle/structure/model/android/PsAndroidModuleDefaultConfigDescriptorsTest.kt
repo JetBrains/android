5a5a9fa8daaeb9ae89c7b0b01262944533ce6418
Christophe Rhodes <xof@google.com>
2019-10-08 18:19:45 +0100

    [PSD] test setting properties in Kotlin psdSample
    Test: PsAndroidModuleDefaultConfig testSetProperties for Kotlin
    Test: PsProductFlavor testSetProperties for Kotlin
    Bug: 130224064
    Change-Id: If04a7bb09d8ddfe91eeef59334c8402bcf233b75

--- android/testSrc/com/android/tools/idea/gradle/structure/model/android/PsAndroidModuleDefaultConfigDescriptorsTest.kt
+++ android/testSrc/com/android/tools/idea/gradle/structure/model/android/PsAndroidModuleDefaultConfigDescriptorsTest.kt
@@ -30,6 +30,8 @@ import org.hamcrest.CoreMatchers.equalTo
 import org.hamcrest.CoreMatchers.notNullValue
 import org.hamcrest.CoreMatchers.nullValue
 import org.hamcrest.MatcherAssert.assertThat
+import org.jetbrains.kotlin.idea.KotlinLanguage
+import org.jetbrains.plugins.groovy.GroovyLanguage
 
 class PsAndroidModuleDefaultConfigDescriptorsTest : AndroidGradleTestCase() {
 
@@ -176,9 +178,7 @@ class PsAndroidModuleDefaultConfigDescriptorsTest : AndroidGradleTestCase() {
     doTestProperties()
   }
 
-  fun testSetProperties() {
-    loadProject(TestProjectPaths.PSD_SAMPLE_GROOVY)
-
+  private fun doTestSetProperties() {
     val resolvedProject = myFixture.project
     var project = PsProjectImpl(resolvedProject)
 
@@ -202,8 +202,13 @@ class PsAndroidModuleDefaultConfigDescriptorsTest : AndroidGradleTestCase() {
     // defaultConfig.versionCode = "3".asParsed()
     defaultConfig.versionName = "3.0".asParsed()
     defaultConfig.versionNameSuffix = "newVns".asParsed()
+    val mySigningConfigDslText = when (appModule.parsedModel?.psiFile?.language) {
+      is GroovyLanguage -> "signingConfigs.myConfig"
+      is KotlinLanguage -> "signingConfigs.getByName(\"myConfig\")"
+      else -> "***unknown language for signingConfig Dsl text***"
+    }
     PsAndroidModuleDefaultConfigDescriptors.signingConfig.bind(defaultConfig).setParsedValue(
-      ParsedValue.Set.Parsed(Unit, DslText.Reference("signingConfigs.myConfig")))
+      ParsedValue.Set.Parsed(Unit, DslText.Reference(mySigningConfigDslText)))
     PsAndroidModuleDefaultConfigDescriptors.manifestPlaceholders.bind(defaultConfig).run {
       getEditableValues()["aa"]?.setParsedValue("EEE".asParsed())
       changeEntryKey("aa", "ee")
@@ -243,7 +248,7 @@ class PsAndroidModuleDefaultConfigDescriptorsTest : AndroidGradleTestCase() {
       assertThat(signingConfig.resolved.asTestValue(), nullValue())
       assertThat(
         signingConfig.parsedValue,
-        equalTo<Annotated<ParsedValue<Unit>>>(ParsedValue.Set.Parsed(null, DslText.Reference("signingConfigs.myConfig")).annotated()))
+        equalTo<Annotated<ParsedValue<Unit>>>(ParsedValue.Set.Parsed(null, DslText.Reference(mySigningConfigDslText)).annotated()))
       assertThat(targetSdkVersion.parsedValue.asTestValue(), equalTo("21"))
       assertThat(testApplicationId.parsedValue.asTestValue(), equalTo("com.example.psd.sample.app.unpaid.failed_test"))
       assertThat(testInstrumentationRunner.parsedValue.asTestValue(), equalTo("com.runner"))
@@ -282,6 +287,16 @@ class PsAndroidModuleDefaultConfigDescriptorsTest : AndroidGradleTestCase() {
     appModule = project.findModuleByName("app") as PsAndroidModule
     // Verify nothing bad happened to the values after the re-parsing.
     verifyValues(appModule.defaultConfig, afterSync = true)
+  }
+
+  fun testSetPropertiesGroovy() {
+    loadProject(TestProjectPaths.PSD_SAMPLE_GROOVY)
+    doTestSetProperties()
+  }
+
+  fun testSetPropertiesKotlin() {
+    loadProject(TestProjectPaths.PSD_SAMPLE_KOTLIN)
+    doTestSetProperties()
   }
 
   fun testMapProperties_delete() {
