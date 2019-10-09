78f32e73a916fb0416546d92723677699c87f8ab
Christophe Rhodes <xof@google.com>
2019-10-09 12:04:43 +0100

    [PSD,.kts] Enable Kotlin DSL and turn off related warnings
    1. enable the KOTLIN_DSL_PARSING Studio Flag;
    
    2. (re-)enable all the tabs in the PSD, even when we have Kotlinscript
       build files, unless the KOTLIN_DSL_PARSING flag is disabled;
    
    3. display a different warning text in the balloon if the
       KOTLIN_DSL_PARSING flag is enabled, to make it clear that support
       is experimental but any problems encoutered should be reported to
       the issue tracker (rather than ignored).
    
    4. invert the sense of the unit test checking the default status of
       the Kotlin Dsl parser
    
    Bug: 130224064
    Test: updated existing unit tests; manual testing
    Change-Id: I026bba77cafb892fef6a1898dee0e98f6804648a

--- android/testSrc/com/android/tools/idea/project/AndroidKtsSupportNotificationTest.java
+++ android/testSrc/com/android/tools/idea/project/AndroidKtsSupportNotificationTest.java
@@ -15,8 +15,9 @@
  */
 package com.android.tools.idea.project;
 
+import static com.android.tools.idea.project.AndroidKtsSupportNotification.KTS_DISABLED_WARNING_MSG;
+import static com.android.tools.idea.project.AndroidKtsSupportNotification.KTS_ENABLED_WARNING_MSG;
 import static com.android.tools.idea.project.AndroidKtsSupportNotification.KTS_NOTIFICATION_GROUP;
-import static com.android.tools.idea.project.AndroidKtsSupportNotification.KTS_WARNING_MSG;
 import static com.android.tools.idea.project.AndroidKtsSupportNotification.KTS_WARNING_TITLE;
 import static com.google.common.truth.Truth.assertThat;
 import static com.intellij.notification.NotificationType.WARNING;
@@ -25,6 +26,8 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.MockitoAnnotations.initMocks;
 
+import com.android.tools.idea.flags.StudioFlags;
+import com.android.tools.idea.gradle.project.sync.hyperlink.FileBugHyperlink;
 import com.android.tools.idea.project.AndroidKtsSupportNotification.DisableAndroidKtsNotificationHyperlink;
 import com.android.tools.idea.project.hyperlink.NotificationHyperlink;
 import com.android.tools.idea.testing.IdeComponents;
@@ -46,10 +49,28 @@ public class AndroidKtsSupportNotificationTest extends PlatformTestCase {
     new IdeComponents(myProject).replaceProjectService(AndroidNotification.class, myAndroidNotification);
   }
 
+  @Override
+  public void tearDown() throws Exception {
+    try {
+      StudioFlags.KOTLIN_DSL_PARSING.clearOverride();
+    }
+    finally {
+      super.tearDown();
+    }
+  }
+
   /**
    * Verify an AndroidNotification is created with the expected parameters
    */
-  public void testShowWarningIfNotShown() {
+  public void testShowEnabledWarningIfNotShown() {
+    StudioFlags.KOTLIN_DSL_PARSING.override(true);
+    AndroidKtsSupportNotification myKtsNotification = new AndroidKtsSupportNotification(myProject);
+    myKtsNotification.showWarningIfNotShown();
+    verifyBalloon();
+  }
+
+  public void testShowDisabledWarningIfNotShown() {
+    StudioFlags.KOTLIN_DSL_PARSING.override(false);
     AndroidKtsSupportNotification myKtsNotification = new AndroidKtsSupportNotification(myProject);
     myKtsNotification.showWarningIfNotShown();
     verifyBalloon();
@@ -58,7 +79,16 @@ public class AndroidKtsSupportNotificationTest extends PlatformTestCase {
   /**
    * Verify that warning is generated only once even when called multiple times
    */
-  public void testShowWarningIfNotShownTwice() {
+  public void testShowEnabledWarningIfNotShownTwice() {
+    StudioFlags.KOTLIN_DSL_PARSING.override(true);
+    AndroidKtsSupportNotification myKtsNotification = new AndroidKtsSupportNotification(myProject);
+    myKtsNotification.showWarningIfNotShown();
+    myKtsNotification.showWarningIfNotShown();
+    verifyBalloon();
+  }
+
+  public void testShowDisabledWarningIfNotShownTwice() {
+    StudioFlags.KOTLIN_DSL_PARSING.override(false);
     AndroidKtsSupportNotification myKtsNotification = new AndroidKtsSupportNotification(myProject);
     myKtsNotification.showWarningIfNotShown();
     myKtsNotification.showWarningIfNotShown();
@@ -66,10 +96,19 @@ public class AndroidKtsSupportNotificationTest extends PlatformTestCase {
   }
 
   private void verifyBalloon() {
+    boolean enabled = StudioFlags.KOTLIN_DSL_PARSING.get();
     ArgumentCaptor<NotificationHyperlink> hyperlinkCaptor = ArgumentCaptor.forClass(NotificationHyperlink.class);
-    verify(myAndroidNotification, times(1)).showBalloon(same(KTS_WARNING_TITLE), same(KTS_WARNING_MSG), same(WARNING), same(KTS_NOTIFICATION_GROUP), hyperlinkCaptor.capture());
+    verify(myAndroidNotification, times(1))
+      .showBalloon(same(KTS_WARNING_TITLE),
+                   enabled ? same(KTS_ENABLED_WARNING_MSG) : same(KTS_DISABLED_WARNING_MSG),
+                   same(WARNING),
+                   same(KTS_NOTIFICATION_GROUP),
+                   hyperlinkCaptor.capture());
     List<NotificationHyperlink> hyperlinks = hyperlinkCaptor.getAllValues();
-    assertThat(hyperlinks).hasSize(1);
+    assertThat(hyperlinks).hasSize(enabled ? 2 : 1);
     assertThat(hyperlinks.get(0)).isInstanceOf(DisableAndroidKtsNotificationHyperlink.class);
+    if (enabled) {
+      assertThat(hyperlinks.get(1)).isInstanceOf(FileBugHyperlink.class);
+    }
   }
 }
