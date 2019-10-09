bb650331ca1b01e60559a7fedcbd66e2c11f04ee
Michal Bendowski <bendowski@google.com>
2019-10-09 13:54:10 +0100

    Add a new package for compose support in the editor
    Bug: None
    Test: N/A
    Change-Id: I1c527377fc1f787c6f4f9da5368dd3ca6e15d173

---
+++ android/src/org/jetbrains/android/compose/AndroidComposePsiUtils.kt
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+@file:JvmName("AndroidComposablePsiUtils")
+
+package org.jetbrains.android.compose
+
+import com.android.tools.idea.kotlin.getQualifiedName
+import com.intellij.psi.PsiElement
+import com.intellij.psi.util.parentOfType
+import org.jetbrains.kotlin.idea.KotlinLanguage
+import org.jetbrains.kotlin.psi.KtNamedFunction
+
+const val COMPOSABLE = "androidx.compose.Composable"
+
+fun PsiElement.isComposableFunction(): Boolean {
+  return this is KtNamedFunction && annotationEntries.any { it.getQualifiedName() == COMPOSABLE }
+}
+
+fun PsiElement.isInsideComposableCode(): Boolean {
+  // TODO: also handle composable lambdas.
+  return language == KotlinLanguage.INSTANCE && parentOfType<KtNamedFunction>()?.isComposableFunction() == true
+}
