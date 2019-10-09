a7076645286e1ccae40a0ace6383e3ba0fd8ec3b
Isaac Chai <ichai@google.com>
2019-10-09 14:55:12 -0700

    [motion editor] Adding metrics for some editor events
    As titled
    
    Bug: N/A
    Test: Override AnalyticsSettings.kt instance?.optedIn @ runtime and
    tested to ensure UsageTracker is logging the correct type.
    
    Change-Id: Ie5d4fcc60f705eeb6868c5e0505eb3dbe6617d87

--- designer/src/com/android/tools/idea/uibuilder/handlers/motion/editor/adapters/Track.java
+++ designer/src/com/android/tools/idea/uibuilder/handlers/motion/editor/adapters/Track.java
@@ -15,47 +15,69 @@
  */
 package com.android.tools.idea.uibuilder.handlers.motion.editor.adapters;
 
-import com.android.tools.idea.common.analytics.DesignerAnalyticsManager;
+import com.android.tools.idea.common.surface.DesignSurface;
+import com.google.wireless.android.sdk.stats.MotionLayoutEditorEvent.MotionLayoutEditorEventType;
 
 /**
  * This is the place holder class for Analytics
- * TODO add calls to NLAnalyticsManager
  */
 public class Track {
-  public static void init(DesignerAnalyticsManager manager) {
+
+  // TODO: Make sure to clean this so we don't leak surface.
+  private static DesignSurface ourSurface;
+
+  // TODO: We need to know when editor is open / closed so I can track that too.
+
+  public static void init(DesignSurface surface) {
+    ourSurface = surface;
+  }
+
+  private static void track(MotionLayoutEditorEventType motionType) {
+    InternalMotionTrackerFactory.Companion.getInstance(ourSurface).track(motionType);
   }
 
   public static void createConstraintSet() {
+    // TODO: Don't have it.
   }
 
   public static void motionEditorEdit() {
+    // TODO: Don't have it
   }
 
   public static void createTransition() {
+    track(MotionLayoutEditorEventType.CREATE_TRANSITION);
   }
 
   public static void createOnClick() {
+    track(MotionLayoutEditorEventType.CREATE_ONCLICK);
   }
 
   public static void createOnSwipe() {
+    // TODO: Don't have it
   }
 
   public static void createKeyCycle() {
+    track(MotionLayoutEditorEventType.CREATE_KEY_CYCLE);
   }
 
   public static void createKeyTimeCycle() {
+    track(MotionLayoutEditorEventType.CREATE_KEY_TIME_CYCLE);
   }
 
   public static void createKeyTrigger() {
+    track(MotionLayoutEditorEventType.CREATE_KEY_TRIGGER);
   }
 
   public static void createKeyPosition() {
+    track(MotionLayoutEditorEventType.CREATE_KEY_POSITION);
   }
 
   public static void createKeyAttribute() {
+    track(MotionLayoutEditorEventType.CREATE_KEY_ATTRIBUTES);
   }
 
   public static void playAnimation() {
+    track(MotionLayoutEditorEventType.MOTION_PLAY);
   }
 }
 
