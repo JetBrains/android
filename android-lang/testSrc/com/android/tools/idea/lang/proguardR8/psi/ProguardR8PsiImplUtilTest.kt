3435280913998a8a6d2b9d609d34a8fe509c0aa1
kovalp <kovalp@google.com>
2019-10-14 17:38:09 +0100

    [Psi, R8] Support multiple classes in keep rule header
    Fixes: 141291678
    Test: ProguardR8PsiImplUtilTest.kt, ProguardR8ParserTest.kt
    Change-Id: Icfd7cdd1fb95706cd9687643420a0950d97eaa50

--- android-lang/testSrc/com/android/tools/idea/lang/proguardR8/psi/ProguardR8PsiImplUtilTest.kt
+++ android-lang/testSrc/com/android/tools/idea/lang/proguardR8/psi/ProguardR8PsiImplUtilTest.kt
@@ -397,6 +397,35 @@ class ProguardR8PsiImplUtilTest : ProguardR8TestCase() {
     assertThat(header.resolvePsiClasses()).containsExactly(myClass, superClass)
   }
 
+  fun testResolveToMultiplePsiClasses() {
+    val class1 = myFixture.addClass(
+      """
+      package p1.p2
+
+      class MyClass1 {}
+    """.trimIndent()
+    )
+
+    val class2 = myFixture.addClass(
+      """
+      package p1.p2
+
+      class MyClass2 {}
+    """.trimIndent()
+    )
+
+    myFixture.configureByText(
+      ProguardR8FileType.INSTANCE,
+      """
+          -keep class $caret p1.p2.MyClass1, p1.p2.MyClass2 
+      """.trimIndent()
+    )
+
+    val header = myFixture.file.findElementAt(myFixture.caretOffset)!!.parentOfType(ProguardR8ClassSpecificationHeader::class)!!
+    assertThat(header.resolvePsiClasses()).containsExactly(class1, class2)
+    assertThat(header.resolvePsiClasses().map { it.qualifiedName }).containsExactly("p1.p2.MyClass1", "p1.p2.MyClass2")
+  }
+
   fun testGetTypeForMethod() {
 
     myFixture.configureByText(
