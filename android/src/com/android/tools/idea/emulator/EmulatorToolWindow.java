27e346a2d65f8cfc6125c0dbd466381cf34766ff
Tor Norbye <tnorbye@google.com>
2019-10-15 15:51:14 -0700

    Update embedded emulator window
    (1) Look up aspect ratio from device and use that
        in the IDE (center-fit)
    
    (2) Handle class loading such that only a single instance
        of the library is loaded (which allows the emulator
        tool window to be shown in multiple project windows
        at the same time)
    
    (3) Add a keyboard action, alt-shift-e, to toggle on/off
        the emulator tools window
    
    (4) Add a flag enabling/disabling emulator window
    
    Test: Manual
    Change-Id: Iaa5b0e4c6716b52c1c8367cfd3aed776cc4d72b5

--- android/src/com/android/tools/idea/emulator/EmulatorToolWindow.java
+++ android/src/com/android/tools/idea/emulator/EmulatorToolWindow.java
@@ -17,25 +17,82 @@ package com.android.tools.idea.emulator;
 
 import com.intellij.openapi.project.DumbAware;
 import com.intellij.openapi.project.Project;
-import javax.swing.*;
+import com.intellij.openapi.wm.ToolWindow;
+import com.intellij.openapi.wm.ToolWindowManager;
+import com.intellij.openapi.wm.ex.ToolWindowManagerListener;
+import java.awt.BorderLayout;
+import java.awt.LayoutManager;
+import javax.swing.JComponent;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class EmulatorToolWindow implements DumbAware {
+  public static final boolean SHUTDOWN_CAPABLE = false;
+  public static final String ID = "Emulator";
 
   private JPanel myToolWindowContent;
+  private boolean myInitialized;
 
   public EmulatorToolWindow(Project project) {
-    try {
-      // TODO: Well we should probably fetch the proper emulator port from somewhere.
-      myToolWindowContent = EmulatorJarLoader.createView(5554);
-    } catch (Exception e) {
-      myToolWindowContent = new JPanel();
-      myToolWindowContent.add(new JLabel("Unable to load emulator view"));
-    }
+    myToolWindowContent = new JPanel(new BorderLayout());
 
+    // Lazily initialize content since we can only have one frame
+    project.getMessageBus().connect().subscribe(ToolWindowManagerListener.TOPIC, new ToolWindowManagerListener() {
+      @Override
+      public void stateChanged() {
+        if (!SHUTDOWN_CAPABLE && myInitialized) {
+          return;
+        }
+
+        // We need to query the tool window again, because it might have been unregistered when closing the project.
+        ToolWindow window = ToolWindowManager.getInstance(project).getToolWindow(ID);
+        if (window == null) {
+          return;
+        }
+
+        if (window.isVisible()) {
+          // TODO: How do I unsubscribe? This will keep notifying me of all tool windows, forever.
+          if (myInitialized) {
+            return;
+          }
+
+          myInitialized = true;
+          createContent(window);
+        }
+        else if (SHUTDOWN_CAPABLE && myInitialized) {
+          destroyContent();
+        }
+      }
+    });
   }
 
   public JComponent getComponent() {
     return myToolWindowContent;
   }
 
+  private void createContent(ToolWindow window) {
+    myInitialized = true;
+    try {
+      // TODO: Well we should probably fetch the proper emulator port from somewhere.
+      JPanel emulatorPanel = EmulatorJarLoader.createView(5554);
+
+      // Tor modifications: wrap in another JPanel to keep aspect ratio
+      LayoutManager layoutManager = new EmulatorLayoutManager(emulatorPanel);
+      myToolWindowContent.add(emulatorPanel);
+      myToolWindowContent.setLayout(layoutManager);
+      myToolWindowContent.repaint();
+      window.setTitle(EmulatorJarLoader.getCurrentAvdName(emulatorPanel));
+
+    }
+    catch (Exception ex) {
+      String label = "Unable to load emulator view: " + ex.toString();
+      myToolWindowContent.add(new JLabel(label), BorderLayout.CENTER);
+    }
+  }
+
+  private void destroyContent() {
+    myInitialized = false;
+    myToolWindowContent.setLayout(new BorderLayout());
+    myToolWindowContent.removeAll();
+  }
 }
