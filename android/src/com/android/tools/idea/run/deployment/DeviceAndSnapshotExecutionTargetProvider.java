c38289c46db5b3b8eb161b37c530aea1b9300af4
Dmitry Avdeev <dmitry.avdeev@jetbrains.com>
2019-10-16 17:16:54 +0300

    fixed API usage
    (cherry picked from commit d7a46a18084b263f846f8ee535c5771a9a261645)
    
    GitOrigin-RevId: 80ec0689861b046ef01b881280d7b2fbcadecb01

--- android/src/com/android/tools/idea/run/deployment/DeviceAndSnapshotExecutionTargetProvider.java
+++ android/src/com/android/tools/idea/run/deployment/DeviceAndSnapshotExecutionTargetProvider.java
@@ -20,7 +20,6 @@ import com.android.tools.idea.flags.StudioFlags;
 import com.intellij.execution.DefaultExecutionTarget;
 import com.intellij.execution.ExecutionTarget;
 import com.intellij.execution.ExecutionTargetProvider;
-import com.intellij.execution.RunnerAndConfigurationSettings;
 import com.intellij.execution.configurations.RunConfiguration;
 import com.intellij.openapi.actionSystem.ActionManager;
 import com.intellij.openapi.project.Project;
@@ -35,7 +34,7 @@ import java.util.List;
 public class DeviceAndSnapshotExecutionTargetProvider extends ExecutionTargetProvider {
   @NotNull
   @Override
-  public List<ExecutionTarget> getTargets(@NotNull Project project, @NotNull RunnerAndConfigurationSettings configuration) {
+  public List<ExecutionTarget> getTargets(@NotNull Project project, @NotNull RunConfiguration configuration) {
     ActionManager manager = ActionManager.getInstance();
     DeviceAndSnapshotComboBoxAction action = (DeviceAndSnapshotComboBoxAction)manager.getAction("DeviceAndSnapshotComboBox");
 
