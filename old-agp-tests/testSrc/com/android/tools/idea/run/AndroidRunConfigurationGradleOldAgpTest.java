9530bbdb831a5b3906aa7161dff16c85354a09fc
Sergio Murguia <srmurguia@google.com>
2019-10-21 16:47:45 -0700

    Remove dependency on AGP 3.0.0 for core tests
    Tests that use it were moved to old-agp-tests. "3.0.0" can still be
    found but only on stubs and unit tests that do not depend on having the
    actual version available.
    
    Added dependency to commons-logging 1.1.1, this was included in 3.0.0
    and is still needed by some tests.
    
    Bug: 141628806
    Test: Manually run tests that have a 3.0.0 version in them from core.
    Existing tests should cover this.
    
    Change-Id: Id66b8aac3e40f500d5b359c14e8fc52d0b75e1a9

--- old-agp-tests/testSrc/com/android/tools/idea/run/AndroidRunConfigurationGradleOldAgpTest.java
+++ old-agp-tests/testSrc/com/android/tools/idea/run/AndroidRunConfigurationGradleOldAgpTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018 The Android Open Source Project
+ * Copyright (C) 2019 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,17 +15,21 @@
  */
 package com.android.tools.idea.run;
 
-import static com.android.tools.idea.testing.TestProjectPaths.DYNAMIC_APP;
+import static com.android.tools.idea.testing.TestProjectPaths.SIMPLE_APPLICATION_PRE30;
 import static com.google.common.truth.Truth.assertThat;
 
 import java.util.List;
 
-public class AndroidRunConfigurationGradleTest extends AndroidRunConfigurationGradleTestCase {
-  public void testNoErrorIfGradlePluginVersionIsUpToDate() throws Exception {
-    loadProject(DYNAMIC_APP);
+public class AndroidRunConfigurationGradleOldAgpTest extends AndroidRunConfigurationGradleTestCase {
+  public void testErrorIfGradlePluginVersionIsOutdated() throws Exception {
+    loadProject(SIMPLE_APPLICATION_PRE30, "app", "4.5", "3.0.0");
+
+    // Verifies there is a validation error (since bundle tasks are not available)
     myRunConfiguration.DEPLOY = true;
     myRunConfiguration.DEPLOY_APK_FROM_BUNDLE = true;
     List<ValidationError> errors = myRunConfiguration.checkConfiguration(myAndroidFacet);
-    assertThat(errors).isEmpty();
+    assertThat(errors).hasSize(1);
+    assertThat(errors.get(0).getMessage()).isEqualTo("This option requires a newer version of the Android Gradle Plugin");
   }
+
 }
