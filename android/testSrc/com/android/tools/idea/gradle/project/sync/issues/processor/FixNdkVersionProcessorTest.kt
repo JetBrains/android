f5592cf836fd392b396d67dabf353385a42940f0
Dan Albert <danalbert@google.com>
2019-10-23 09:11:27 -0700

    Move testing to NDK r21.
    Test: SHELL=/bin/bash tools/base/bazel/test_studio.sh
    Bug: None
    Change-Id: I71b4223d5cb42269cb9f148180a0407268f17ad5

--- android/testSrc/com/android/tools/idea/gradle/project/sync/issues/processor/FixNdkVersionProcessorTest.kt
+++ android/testSrc/com/android/tools/idea/gradle/project/sync/issues/processor/FixNdkVersionProcessorTest.kt
@@ -56,7 +56,7 @@ class FixNdkVersionProcessorTest : AndroidGradleTestCase() {
     val processor = FixNdkVersionProcessor(project, ImmutableList.of(file), "77.7.7")
     val usages = processor.findUsages()
     assertSize(1, usages)
-    assertEquals("\"20.0.5594570\"", usages[0].element!!.text)
+    assertEquals("\"21.0.5935234\"", usages[0].element!!.text)
   }
 
   @Test
