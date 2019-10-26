9226c1224fb7a4fc57594d45616caac0d1febef2
Dana Dahlstrom <dahlstrom@google.com>
2019-10-25 18:00:00 -0700

    Cast to ComponentManagerImpl only where needed
    Change-Id: I114d17496111022e377f50b6543f4e36802882fd

--- adt-ui/src/test/java/com/android/tools/adtui/workbench/ComponentStack.java
+++ adt-ui/src/test/java/com/android/tools/adtui/workbench/ComponentStack.java
@@ -24,12 +24,12 @@ import java.util.Deque;
 
 // TODO: Move this to a place to be shared with AndroidTestCase
 public class ComponentStack {
-  private final ComponentManagerImpl myComponentManager;
+  private final ComponentManager myComponentManager;
   private final MutablePicoContainer myContainer;
   private final Deque<ComponentItem> myComponents;
 
   public ComponentStack(@NotNull ComponentManager manager) {
-    myComponentManager = (ComponentManagerImpl)manager;
+    myComponentManager = manager;
     myContainer = (MutablePicoContainer)manager.getPicoContainer();
     myComponents = new ArrayDeque<>();
   }
@@ -45,7 +45,7 @@ public class ComponentStack {
   public <T> void registerComponentInstance(@NotNull Class<T> key, @NotNull T instance) {
     Object old = myComponentManager.getComponent(key);
     myComponents.push(new ComponentItem(key, old));
-    myComponentManager.registerComponentInstance(key, instance);
+    ((ComponentManagerImpl)myComponentManager).registerComponentInstance(key, instance);
   }
 
   public void restoreComponents() {
@@ -53,7 +53,7 @@ public class ComponentStack {
       ComponentItem component = myComponents.pop();
       if (component.key instanceof Class) {
         //noinspection unchecked
-        myComponentManager.registerComponentInstance((Class)component.key, component.instance);
+        ((ComponentManagerImpl)myComponentManager).registerComponentInstance((Class)component.key, component.instance);
       }
       else {
         myContainer.unregisterComponent(component.key.toString());
