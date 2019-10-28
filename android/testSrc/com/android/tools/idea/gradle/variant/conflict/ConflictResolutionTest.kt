73b6e5772d022741a5dece4d821f782ed3c951bf
Yuriy Solodkyy <solodkyy@google.com>
2019-10-28 15:11:44 +0000

    Migrate variant conflict tests to light sync test framework.
    This is to eliminate fragile AndroidModel.set() tests which rely on
    implementation details.
    
    Bug: n/a
    Test: n/a
    Change-Id: I63d4a3d663d4026c78391c993cf6174844fe21f7

---
+++ android/testSrc/com/android/tools/idea/gradle/variant/conflict/ConflictResolutionTest.kt
@@ -0,0 +1,74 @@
+/*
+ * Copyright (C) 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.gradle.variant.conflict
+
+import com.android.tools.idea.gradle.project.model.AndroidModuleModel
+import com.android.tools.idea.testing.JavaModuleModelBuilder.Companion.rootModuleBuilder
+import com.android.tools.idea.testing.setupTestProjectFromAndroidModel
+import com.google.common.truth.Truth.assertThat
+import com.google.common.truth.TruthJUnit.assume
+import com.intellij.openapi.module.ModuleManager
+import java.io.File
+
+
+class ConflictResolutionTest : ConflictsTestCase() {
+  fun testSolveSelectionConflict() {
+    setupTestProjectFromAndroidModel(
+      project,
+      File(myFixture.tempDirPath),
+      rootModuleBuilder,
+      appModuleBuilder(selectedVariant = "release", dependOnVariant = "release"),
+      libModuleBuilder()
+    )
+
+    val appModule = ModuleManager.getInstance(project).findModuleByName("app")!!
+    val libModule = ModuleManager.getInstance(project).findModuleByName("lib")!!
+
+    assume().that(AndroidModuleModel.get(appModule)!!.selectedVariant.name).isEqualTo("release")
+    assume().that(AndroidModuleModel.get(libModule)!!.selectedVariant.name).isEqualTo("debug")
+
+    var conflicts = ConflictSet.findConflicts(project).selectionConflicts
+    assume().that(conflicts).hasSize(1)
+
+    // Source is the :lib module, which has "debug".
+    val conflict = conflicts[0]
+    assume().that(conflict.source).isSameAs(libModule)
+    assume().that(conflict.selectedVariant).isEqualTo("debug")
+
+    val affectedModules = conflict.affectedModules
+    assume().that(affectedModules).hasSize(1)
+
+    // Affected is the :app module, which has "release".
+    val affectedModule = affectedModules[0]
+    assume().that(affectedModule.target).isSameAs(appModule)
+    assume().that(affectedModule.expectedVariant).isEqualTo("release")
+
+    // We should fix the "source" (i.e., ":lib") and make it "release".
+    assertThat(ConflictResolution.solveSelectionConflict(conflict)).isTrue()
+
+    // After fixing the conflict, the selected variants match.
+    assertThat(AndroidModuleModel.get(appModule)!!.selectedVariant.name).isEqualTo("release")
+    assertThat(AndroidModuleModel.get(libModule)!!.selectedVariant.name).isEqualTo("release")
+
+    // After fixing the conflict, there are no more conflicts left.
+    conflicts = ConflictSet.findConflicts(project).selectionConflicts
+    assertThat(conflicts).hasSize(0)
+  }
+
+  fun testSolveSelectionConflictWithABIs() {
+    // TODO: Add tests for modules with NdkModuleModels.
+  }
+}
