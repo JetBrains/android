8ccbd507b1b10a4edb8214e5c905ee3c1cd4ccd8
Renaud Paquay <rpaquay@google.com>
2019-10-30 10:42:21 -0700

    Catch errors in test suite static constructor
    This is to workaround a bug in Bazel (see b/143359533).
    
    Test: This is it
    Bug: 143359533
    Change-Id: I023aff99d249c0b3aeaec331694dc55571a916e6

--- android/testSrc/com/android/tools/idea/IdeaTestSuite.java
+++ android/testSrc/com/android/tools/idea/IdeaTestSuite.java
@@ -46,7 +46,8 @@ public class IdeaTestSuite extends IdeaTestSuiteBase {
   @ClassRule public static GradleDaemonsRule gradle = new GradleDaemonsRule();
 
   static {
-    symlinkToIdeaHome(
+    try {
+      symlinkToIdeaHome(
         "prebuilts/studio/jdk",
         "prebuilts/studio/layoutlib",
         "prebuilts/studio/sdk",
@@ -58,14 +59,20 @@ public class IdeaTestSuite extends IdeaTestSuiteBase {
         "tools/base/templates",
         "tools/idea/java");
 
-    setUpOfflineRepo("tools/base/build-system/studio_repo.zip", "out/studio/repo");
-    setUpOfflineRepo("tools/adt/idea/android/test_deps.zip", "prebuilts/tools/common/m2/repository");
-    setUpOfflineRepo("tools/base/third_party/kotlin/kotlin-m2repository.zip", "prebuilts/tools/common/m2/repository");
-    setUpOfflineRepo("tools/base/build-system/previous-versions/1.5.0.zip", "prebuilts/tools/common/m2/repository");
-    setUpOfflineRepo("tools/base/build-system/previous-versions/2.2.0.zip", "prebuilts/tools/common/m2/repository");
-    setUpOfflineRepo("tools/base/build-system/previous-versions/3.0.0.zip", "prebuilts/tools/common/m2/repository");
-    setUpOfflineRepo("tools/base/build-system/previous-versions/3.3.2.zip", "prebuilts/tools/common/m2/repository");
-    setUpOfflineRepo("tools/base/build-system/previous-versions/3.5.0.zip", "prebuilts/tools/common/m2/repository");
-    setUpOfflineRepo("tools/data-binding/data_binding_runtime.zip", "prebuilts/tools/common/m2/repository");
+      setUpOfflineRepo("tools/base/build-system/studio_repo.zip", "out/studio/repo");
+      setUpOfflineRepo("tools/adt/idea/android/test_deps.zip", "prebuilts/tools/common/m2/repository");
+      setUpOfflineRepo("tools/base/third_party/kotlin/kotlin-m2repository.zip", "prebuilts/tools/common/m2/repository");
+      setUpOfflineRepo("tools/base/build-system/previous-versions/1.5.0.zip", "prebuilts/tools/common/m2/repository");
+      setUpOfflineRepo("tools/base/build-system/previous-versions/2.2.0.zip", "prebuilts/tools/common/m2/repository");
+      setUpOfflineRepo("tools/base/build-system/previous-versions/3.0.0.zip", "prebuilts/tools/common/m2/repository");
+      setUpOfflineRepo("tools/base/build-system/previous-versions/3.3.2.zip", "prebuilts/tools/common/m2/repository");
+      setUpOfflineRepo("tools/base/build-system/previous-versions/3.5.0.zip", "prebuilts/tools/common/m2/repository");
+      setUpOfflineRepo("tools/data-binding/data_binding_runtime.zip", "prebuilts/tools/common/m2/repository");
+    }
+    catch (Throwable e) {
+      // See b/143359533 for why we are handling errors here
+      System.err.println("ERROR: Error initializing test suite, tests will likely fail following this error");
+      e.printStackTrace();
+    }
   }
 }
