acb8f17eea51d2a72e924b13020af959ae65177e
Dana Dahlstrom <dahlstrom@google.com>
2019-10-30 13:00:00 -0700

    Remove unnecessary MyPluginVersionUpgradeStep
    This code has conflicted with upstream commits before
    (see Change Ibe8b55f5) and again conflicts with commit b3aaec2.
    
    Change-Id: I6677f79dca3a719b0ad1e4cba1a80f4903ad2d49

--- android/testSrc/com/android/tools/idea/run/AndroidRunConfigurationGradleTestCase.java
+++ android/testSrc/com/android/tools/idea/run/AndroidRunConfigurationGradleTestCase.java
@@ -16,14 +16,8 @@
 package com.android.tools.idea.run;
 
 import com.android.tools.idea.flags.StudioFlags;
-import com.android.tools.idea.gradle.plugin.AndroidPluginInfo;
-import com.android.tools.idea.gradle.project.sync.setup.post.upgrade.RecommendedPluginVersionUpgradeStep;
 import com.android.tools.idea.testing.AndroidGradleTestCase;
 import com.intellij.execution.configurations.ConfigurationFactory;
-import com.intellij.openapi.project.Project;
-import com.intellij.testFramework.PlatformTestUtil;
-import java.util.Collections;
-import org.jetbrains.annotations.NotNull;
 
 public abstract class AndroidRunConfigurationGradleTestCase extends AndroidGradleTestCase {
   protected AndroidRunConfiguration myRunConfiguration;
@@ -38,10 +32,6 @@ public abstract class AndroidRunConfigurationGradleTestCase extends AndroidGradl
 
     ConfigurationFactory configurationFactory = AndroidRunConfigurationType.getInstance().getFactory();
     myRunConfiguration = new AndroidRunConfiguration(getProject(), configurationFactory);
-
-    // We override the default extension point to prevent the "Gradle Update" UI to show during the test
-    PlatformTestUtil.maskExtensions(RecommendedPluginVersionUpgradeStep.EXTENSION_POINT_NAME, Collections
-      .singletonList(new AndroidRunConfigurationGradleTestCase.MyPluginVersionUpgradeStep()), getTestRootDisposable());
   }
 
   @Override
@@ -51,21 +41,6 @@ public abstract class AndroidRunConfigurationGradleTestCase extends AndroidGradl
     }
     finally {
       super.tearDown();
-    }
-  }
-
-  private static class MyPluginVersionUpgradeStep extends RecommendedPluginVersionUpgradeStep {
-
-    @Override
-    public boolean checkUpgradable(@NotNull Project project, @NotNull AndroidPluginInfo pluginInfo) {
-      // Returning {@code false} means "project is all good, no update needed".
-      return false;
-    }
-
-    @Override
-    public boolean performUpgradeAndSync(@NotNull Project project, @NotNull AndroidPluginInfo pluginInfo) {
-      // Returning {@code false} means "project is all good, no update needed or performed".
-      return false;
     }
   }
 }
