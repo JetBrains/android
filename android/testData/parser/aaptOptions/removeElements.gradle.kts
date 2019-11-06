c1a6fd709ab9db40e4df242bf499536f13cdadbd
Christophe Rhodes <xof@google.com>
2019-11-06 10:33:29 +0000

    [Dsl] extend AaptOptionsModel tests
    Implement all the current AaptOptionsModel tests for Kotlinscript.
    Additionally, systematically check the contents of files after
    applyChanges().
    
    Note that the current tests do not follow Kotlin Dsl syntax; fixing
    this fully depends on implementing semantics tables for the
    AaptOptionsDslElement class, and choosing appropriately between method
    call and assignment for setting properties.
    
    Bug: 130224064
    Bug: 142114586
    Test: new, stronger AaptOptionsModel tests
    Change-Id: I71c542b2e59d96b28d89a168267673be24ff8415

--- android/testData/parser/aaptOptions/removeElements.gradle.kts
+++ android/testData/parser/aaptOptions/removeElements.gradle.kts
@@ -1,6 +1,6 @@
 android {
   aaptOptions {
-    additionalParameters("abcd")
+    additionalParameters("abcd", "efgh")
     cruncherEnabled(true)
     cruncherProcesses(1)
     failOnMissingConfigEntry(false)
