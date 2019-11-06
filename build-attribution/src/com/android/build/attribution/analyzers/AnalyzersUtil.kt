f17e3d55161663ece5f94f046fd37fdbc0f45880
Amr Afifiy <amrmahmoud@google.com>
2019-11-06 16:31:14 +0000

    [build attribution] Add methods to identify gradle and kotlin plugins
    Bug: 144016414
    Test: N/A
    Change-Id: I3ab7ea6c87349ada1559de90851fa0495081d45b

--- build-attribution/src/com/android/build/attribution/analyzers/AnalyzersUtil.kt
+++ build-attribution/src/com/android/build/attribution/analyzers/AnalyzersUtil.kt
@@ -29,3 +29,18 @@ fun isAndroidPlugin(plugin: PluginData): Boolean {
          plugin.displayName == "com.android.dynamic-feature" ||
          plugin.displayName == "com.android.test"
 }
+
+fun isKotlinPlugin(plugin: PluginData): Boolean {
+  return plugin.displayName == "kotlin-android" ||
+         plugin.displayName == "kotlin-android-extensions" ||
+         plugin.displayName == "kotlin-multiplatform" ||
+         plugin.displayName.startsWith("org.jetbrains.kotlin")
+}
+
+fun isGradlePlugin(plugin: PluginData): Boolean {
+  return plugin.displayName == "org.gradle.language.base.plugins.LifecycleBasePlugin" ||
+         plugin.displayName == "org.gradle.build-init" ||
+         plugin.displayName == "org.gradle.wrapper" ||
+         plugin.displayName == "org.gradle.help-tasks" ||
+         plugin.displayName.startsWith("org.gradle.api.plugins")
+}
