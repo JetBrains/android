74654f4e5f7599ff8aae1697b361785f87d1341e
Christophe Rhodes <xof@google.com>
2019-11-06 16:15:56 +0000

    [Dsl] generalize dataBinding support
    The Kotlinscript DSL has properties but no setter method of the same
    name for dataBinding properties addDefaultAdapters and version, and
    the property that is enabled in Groovy is isEnabled in Kotlinscript.
    Implement model property maps to make this happen.
    
    Bug: 130224064
    Bug: 141842964
    Bug: 142114586
    Test: stronger existing tests, also run on .kts build files
    Change-Id: I5ced8ac3d8eb63f609235a6fdf8837b90d05d7d5

--- android/src/com/android/tools/idea/gradle/dsl/parser/android/DataBindingDslElement.java
+++ android/src/com/android/tools/idea/gradle/dsl/parser/android/DataBindingDslElement.java
@@ -15,16 +15,74 @@
  */
 package com.android.tools.idea.gradle.dsl.parser.android;
 
+import static com.android.tools.idea.gradle.dsl.model.android.DataBindingModelImpl.*;
+import static com.android.tools.idea.gradle.dsl.parser.semantics.ArityHelper.*;
+import static com.android.tools.idea.gradle.dsl.parser.semantics.MethodSemanticsDescription.SET;
+import static com.android.tools.idea.gradle.dsl.parser.semantics.PropertySemanticsDescription.VAR;
+import static com.google.common.collect.ImmutableMap.toImmutableMap;
+
+import com.android.tools.idea.gradle.dsl.parser.GradleDslNameConverter;
 import com.android.tools.idea.gradle.dsl.parser.elements.GradleDslBlockElement;
 import com.android.tools.idea.gradle.dsl.parser.elements.GradleDslElement;
 import com.android.tools.idea.gradle.dsl.parser.elements.GradleNameElement;
+import com.android.tools.idea.gradle.dsl.parser.groovy.GroovyDslNameConverter;
+import com.android.tools.idea.gradle.dsl.parser.kotlin.KotlinDslNameConverter;
+import com.android.tools.idea.gradle.dsl.parser.semantics.SemanticsDescription;
+import com.google.common.collect.ImmutableMap;
+import java.util.stream.Stream;
+import kotlin.Pair;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 
 public class DataBindingDslElement extends GradleDslBlockElement {
   @NonNls public static final String DATA_BINDING_BLOCK_NAME = "dataBinding";
 
+  @NotNull
+  public static final ImmutableMap<Pair<String,Integer>, Pair<String, SemanticsDescription>> ktsToModelNameMap = Stream.of(new Object[][]{
+    {"addDefaultAdapters", property, ADD_DEFAULT_ADAPTERS, VAR},
+    {"isEnabled", property, ENABLED, VAR},
+    {"version", property, VERSION, VAR},
+  }).collect(toImmutableMap(data -> new Pair<>((String) data[0], (Integer) data[1]),
+                            data -> new Pair<>((String) data[2], (SemanticsDescription) data[3])));
+
+  @NotNull
+  public static final ImmutableMap<Pair<String,Integer>, Pair<String,SemanticsDescription>> groovyToModelNameMap = Stream.of(new Object[][]{
+    {"addDefaultAdapters", property, ADD_DEFAULT_ADAPTERS, VAR},
+    {"addDefaultAdapters", exactly(1), ADD_DEFAULT_ADAPTERS, SET},
+    {"enabled", property, ENABLED, VAR},
+    {"enabled", exactly(1), ENABLED, SET},
+    {"version", property, VERSION, VAR},
+    {"version", exactly(1), VERSION, SET},
+  }).collect(toImmutableMap(data -> new Pair<>((String) data[0], (Integer) data[1]),
+                            data -> new Pair<>((String) data[2], (SemanticsDescription) data[3])));
+
+  @Override
+  @NotNull
+  public ImmutableMap<Pair<String,Integer>, Pair<String,SemanticsDescription>> getExternalToModelMap(@NotNull GradleDslNameConverter converter) {
+    if (converter instanceof KotlinDslNameConverter) {
+      return ktsToModelNameMap;
+    }
+    else if (converter instanceof GroovyDslNameConverter) {
+      return groovyToModelNameMap;
+    }
+    else {
+      return super.getExternalToModelMap(converter);
+    }
+  }
+
   public DataBindingDslElement(@NotNull GradleDslElement parent) {
     super(parent, GradleNameElement.create(DATA_BINDING_BLOCK_NAME));
+  }
+
+  @Override
+  public void addParsedElement(@NotNull GradleDslElement element) {
+    super.addParsedElement(element);
+    maybeRenameElement(element);
+  }
+
+  @Override
+  public void setParsedElement(@NotNull GradleDslElement element) {
+    super.setParsedElement(element);
+    maybeRenameElement(element);
   }
 }
