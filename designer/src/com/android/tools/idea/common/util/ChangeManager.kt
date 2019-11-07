0b79e73bae90281d44d52f32bb45bea22bab8728
Fedor Kudasov <kudasov@google.com>
2019-11-07 13:41:58 +0000

    Move ChangeManager to designer
    This allows us to reuse it for e.g. custom view preview
    
    Bug: 143067434
    Test: ChangeManagerTest
    Change-Id: Icb4d8e865df84b2ce8138ed51180127c97c4a937

--- designer/src/com/android/tools/idea/common/util/ChangeManager.kt
+++ designer/src/com/android/tools/idea/common/util/ChangeManager.kt
@@ -13,13 +13,12 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.android.tools.idea.compose.preview
+package com.android.tools.idea.common.util
 
 import com.intellij.openapi.Disposable
 import com.intellij.openapi.editor.event.DocumentEvent
 import com.intellij.openapi.editor.event.DocumentListener
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiDocumentManager
 import com.intellij.psi.PsiFile
 import com.intellij.util.ui.update.MergingUpdateQueue
