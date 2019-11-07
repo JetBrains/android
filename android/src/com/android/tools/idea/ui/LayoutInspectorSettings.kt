bd545ac72acfd9957903bbdacad350223244df5e
Joe Baker-Malone <jbakermalone@google.com>
2019-11-07 13:43:36 -0800

    Move settings code to inspector-specific place
    There was a bunch of layout inspector-specific code in
    ExperimenalSettingsConfigurable. Moving it to LayoutInspectorSettings.
    Also actually hiding the inspector when you turn the setting off.
    
    Test: none
    Bug: none
    Change-Id: Ie47a9df2128031d6c46ba83ece462af97436852c

--- android/src/com/android/tools/idea/ui/LayoutInspectorSettings.kt
+++ android/src/com/android/tools/idea/ui/LayoutInspectorSettings.kt
@@ -16,13 +16,61 @@
 package com.android.tools.idea.ui
 
 import com.android.tools.idea.flags.StudioFlags
+import com.intellij.facet.ui.FacetDependentToolWindow
 import com.intellij.ide.util.PropertiesComponent
+import com.intellij.openapi.fileEditor.FileEditorManager
+import com.intellij.openapi.project.ProjectManager
+import com.intellij.openapi.wm.ToolWindow
+import com.intellij.openapi.wm.ToolWindowManager
+import com.intellij.openapi.wm.ex.ToolWindowManagerEx
 
 private const val PREFERENCE_KEY = "live.layout.inspector.enabled"
+
+// Should match LayoutInspectorFileType.getName()
+private const val LAYOUT_INSPECTOR_FILE_TYPE_NAME = "Layout Inspector"
+
+// Should match LayoutInspectorToolWindowFactory.TOOL_WINDOW_ID
+private const val LAYOUT_INSPECTOR_TOOL_WINDOW_ID = "Layout Inspector"
+
 
 var enableLiveLayoutInspector
   get() = PropertiesComponent.getInstance().getBoolean(PREFERENCE_KEY, false) ||
           StudioFlags.DYNAMIC_LAYOUT_INSPECTOR_LEGACY_DEVICE_SUPPORT.get()
-  set(value) = PropertiesComponent.getInstance().setValue(PREFERENCE_KEY, value)
+  set(value) {
+    if (value != enableLiveLayoutInspector) {
+      if (value) {
+        for (windowEp in FacetDependentToolWindow.EXTENSION_POINT_NAME.extensionList) {
+          if (windowEp.id == LAYOUT_INSPECTOR_TOOL_WINDOW_ID) {
+            for (project in ProjectManager.getInstance().openProjects) {
+              val windowManager = ToolWindowManager.getInstance(project) as ToolWindowManagerEx
+              var window: ToolWindow? = windowManager.getToolWindow(LAYOUT_INSPECTOR_TOOL_WINDOW_ID)
+              if (window == null) {
+                windowManager.initToolWindow(windowEp)
+              }
+              window = windowManager.getToolWindow(LAYOUT_INSPECTOR_TOOL_WINDOW_ID)
+              window!!.activate(null)
+            }
+          }
+        }
+        for (project in ProjectManager.getInstance().openProjects) {
+          val editorManager = FileEditorManager.getInstance(project)
+          for (vf in editorManager.openFiles) {
+            if (vf.fileType.name == LAYOUT_INSPECTOR_FILE_TYPE_NAME) {
+              editorManager.closeFile(vf)
+            }
+          }
+        }
+      }
+      else {
+        for (project in ProjectManager.getInstance().openProjects) {
+          val windowManager = ToolWindowManager.getInstance(project) as ToolWindowManagerEx
+          val window = windowManager.getToolWindow(LAYOUT_INSPECTOR_TOOL_WINDOW_ID)
+          window.setAvailable(false, null)
+          window.hide(null)
+        }
+      }
+    }
+    PropertiesComponent.getInstance().setValue(PREFERENCE_KEY, value)
+  }
 
 
