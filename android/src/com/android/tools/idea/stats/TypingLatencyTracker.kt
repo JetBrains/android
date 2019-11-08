062444943f9b7736b81bb4797f9d04c13cb15a91
Matthew Gharrity <gharrma@google.com>
2019-11-08 10:33:36 -0800

    Collect analytics for completion latency
    We measure three things:
    
    (1) Time to display the popup containing initial completion items.
    
    (2) Time to compute *all* completion items.
    
    (3) Time to insert a selected completion item into the editor.
    
    Code completion is a complex process involving frequent cancellations,
    restarts, and incremental updates while typing. Hence the code to
    measure performance is not very simple. Fortunately, we are using
    similar techniques that the Kotlin plugin is using; see
    LookupCancelWatcher and CompletionFUSCollector in the Kotlin plugin.
    
    Test: manual (via the internal usage statistics action)
    Bug: 144098445
    Change-Id: I4fcb6f1345a55c405cdaf205a9dc7960cf88cbf9

--- android/src/com/android/tools/idea/stats/TypingLatencyTracker.kt
+++ android/src/com/android/tools/idea/stats/TypingLatencyTracker.kt
@@ -19,20 +19,10 @@ import com.android.tools.analytics.UsageTracker
 import com.android.tools.idea.stats.TypingLatencyTracker.reportTypingLatency
 import com.google.wireless.android.sdk.stats.AndroidStudioEvent
 import com.google.wireless.android.sdk.stats.EditorFileType
-import com.google.wireless.android.sdk.stats.EditorFileType.GROOVY
-import com.google.wireless.android.sdk.stats.EditorFileType.JAVA
-import com.google.wireless.android.sdk.stats.EditorFileType.JSON
-import com.google.wireless.android.sdk.stats.EditorFileType.KOTLIN
-import com.google.wireless.android.sdk.stats.EditorFileType.KOTLIN_SCRIPT
-import com.google.wireless.android.sdk.stats.EditorFileType.NATIVE
-import com.google.wireless.android.sdk.stats.EditorFileType.PROPERTIES
-import com.google.wireless.android.sdk.stats.EditorFileType.UNKNOWN
-import com.google.wireless.android.sdk.stats.EditorFileType.XML
 import com.google.wireless.android.sdk.stats.TypingLatencyStats
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.editor.actionSystem.LatencyListener
 import com.intellij.openapi.fileEditor.FileDocumentManager
-import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.util.analytics.toProto
 import org.HdrHistogram.SingleWriterRecorder
 import java.util.concurrent.ConcurrentHashMap
@@ -53,7 +43,7 @@ object TypingLatencyTracker : LatencyListener {
     // This runs on the EDT, but is thread-safe with respect to a background thread running reportTypingLatency().
     if (latencyMs < 0) return // Can happen due to non-monotonic system time, for example.
     val file = FileDocumentManager.getInstance().getFile(editor.document) ?: return
-    val fileType = convertFileType(file)
+    val fileType = getEditorFileTypeForAnalytics(file)
     val recorder = latencyRecorders.computeIfAbsent(fileType) { SingleWriterRecorder(1) }
     recorder.recordValue(latencyMs)
   }
@@ -89,19 +79,5 @@ object TypingLatencyTracker : LatencyListener {
         typingLatencyStats = allStats.build()
       }
     )
-  }
-
-  /** Converts from file type name to proto enum value. */
-  private fun convertFileType(file: VirtualFile): EditorFileType = when (file.fileType.name) {
-    // We use string literals here (rather than, e.g., JsonFileType.INSTANCE.name) to avoid unnecessary
-    // dependencies on other plugins. Fortunately, these values are extremely unlikely to change.
-    "JAVA" -> JAVA
-    "Kotlin" -> if (file.extension == "kts") KOTLIN_SCRIPT else KOTLIN
-    "XML" -> XML
-    "Groovy" -> GROOVY
-    "Properties" -> PROPERTIES
-    "JSON" -> JSON
-    "ObjectiveC" -> NATIVE
-    else -> UNKNOWN
   }
 }
