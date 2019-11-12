f199b1610024e7b7018bac77ae5a67858eb0f3ab
Valery Cherepanov <qumeric@google.com>
2019-11-12 14:49:07 +0000

    Set Java versions and Kotlin JVM programmatically
    Bug: 138935015
    Test: TemplateTest.test*ComposeActivity
    Change-Id: Ie8a7006eeabc33cdd9f003b98a7e6812dfd0042a

--- android/src/com/android/tools/idea/templates/recipe/DefaultRecipeExecutor.kt
+++ android/src/com/android/tools/idea/templates/recipe/DefaultRecipeExecutor.kt
@@ -28,6 +28,9 @@ import com.android.tools.idea.Projects.getBaseDirPath
 import com.android.tools.idea.gradle.dsl.api.dependencies.ArtifactDependencyModel
 import com.android.tools.idea.gradle.dsl.api.dependencies.ArtifactDependencySpec
 import com.android.tools.idea.gradle.dsl.api.ext.GradlePropertyModel
+import com.android.tools.idea.gradle.dsl.api.ext.GradlePropertyModel.STRING_TYPE
+import com.android.tools.idea.gradle.dsl.api.java.LanguageLevelPropertyModel
+import com.android.tools.idea.gradle.dsl.api.util.LanguageLevelUtil
 import com.android.tools.idea.gradle.util.GradleUtil.getGradleBuildFile
 import com.android.tools.idea.templates.FmGetConfigurationNameMethod
 import com.android.tools.idea.templates.FreemarkerUtils.TemplateProcessingException
@@ -386,13 +389,57 @@ class DefaultRecipeExecutor(private val context: RenderingContext, dryRun: Boole
       return // we do not override value if it exists. TODO(qumeric): ask user?
     }
 
-    val boolValue = when(value) {
-      "true" -> true
-      "false" -> false
-      else -> throw IllegalArgumentException("buildFeature may be 'true' or 'false' but '$value' was given")
-    }
+    val boolValue = parseBoolean(value, "buildFeature")
 
     feature.setValue(boolValue)
+    io.applyChanges(buildModel)
+  }
+
+  /**
+   * Sets sourceCompatibility and targetCompatibility in compileOptions and (if needed) jvmTarget in kotlinOptions.
+   */
+  override fun requireJavaVersion(version: String, kotlinSupport: String) {
+    infix fun String.compareWith(other: String): Int {
+      val validTargets = listOf("1.6", "1.7", "1.8", "9", "10", "11", "12", "13")
+
+      return validTargets.indexOf(this) - validTargets.indexOf(other)
+    }
+
+    val buildFile = getBuildFilePath(context)
+
+    // TODO(qumeric) handle it in a better way?
+    val buildModel = getBuildModel(buildFile, context.project) ?: return
+
+    val compileOptions = buildModel.android().compileOptions()
+    val kotlinOptions = buildModel.android().kotlinOptions()
+
+    val currentSourceCompatibility = compileOptions.sourceCompatibility()
+    val currentTargetCompatibility = compileOptions.targetCompatibility()
+
+    val languageLevel = LanguageLevelUtil.parseFromGradleString(version)!!
+
+    fun updateCompatibility(current: LanguageLevelPropertyModel) {
+      if (current.valueType == GradlePropertyModel.ValueType.NONE ||
+          languageLevel.isAtLeast(LanguageLevelUtil.parseFromGradleString(current.getValue(STRING_TYPE)!!)!!)) {
+        current.setLanguageLevel(languageLevel)
+      }
+    }
+
+    updateCompatibility(currentSourceCompatibility)
+    updateCompatibility(currentTargetCompatibility)
+
+    if (!parseBoolean(kotlinSupport, "kotlinSupport")) {
+      io.applyChanges(buildModel)
+      return
+    }
+
+    val currentKotlinJvmTarget = kotlinOptions.jvmTarget()
+
+    if (currentKotlinJvmTarget.valueType == GradlePropertyModel.ValueType.NONE ||
+        currentKotlinJvmTarget.getValue(STRING_TYPE)!! compareWith version <= 0) {
+      currentKotlinJvmTarget.setValue(version)
+    }
+
     io.applyChanges(buildModel)
   }
 
@@ -642,4 +689,10 @@ private fun getBuildFilePath(context: RenderingContext): File {
   val module = context.module
   val moduleBuildFile = if (module == null) null else getGradleBuildFile(module)
   return moduleBuildFile?.let { virtualToIoFile(it) } ?: findGradleBuildFile(context.moduleRoot)
+}
+
+private fun parseBoolean(value: String, name: String = "value") = when(value) {
+  "true" -> true
+  "false" -> false
+  else -> throw IllegalArgumentException("$name may be 'true' or 'false' but '$value' was given")
 }
