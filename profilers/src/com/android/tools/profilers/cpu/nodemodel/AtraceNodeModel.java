fbf8d7f0e3140bd297b5cff26d78afb0b4aa0c91
Joshua Gilpatrick <gijosh@google.com>
2019-11-19 17:28:13 -0800

    Migrate Atrace Callchart nodes to use Data Vis colors
    Test: Updated
    Bug: 144805176
    Change-Id: I0440ce9a04a9ed032e24cd50f34031ba46458d7b

--- profilers/src/com/android/tools/profilers/cpu/nodemodel/AtraceNodeModel.java
+++ profilers/src/com/android/tools/profilers/cpu/nodemodel/AtraceNodeModel.java
@@ -15,14 +15,52 @@
  */
 package com.android.tools.profilers.cpu.nodemodel;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import org.jetbrains.annotations.NotNull;
 
 /**
  * This model represents an Atrace node, used for determining the color to draw for the
  * FlameChart, and CallChart.
  */
-public class AtraceNodeModel extends SingleNameModel {
+public class AtraceNodeModel implements CaptureNodeModel {
+
+  // Pattern to match names with the format Letters Number. Eg: Frame 1234
+  private static final Pattern ID_GROUP = Pattern.compile("^([A-Za-z\\s]*)(\\d+)");
+  private final String myId;
+  @NotNull private final String myName;
+
   public AtraceNodeModel(@NotNull String name) {
-    super(name);
+    // We match the numbers at the end of a tag so the UI can group elements that have an incrementing number at the end as the same thing.
+    // This means that "Frame 1", and "Frame 2" will appear as a single element "Frame ###". This allows us to collect the stats, and
+    // colorize these elements as if they represent the same thing.
+    Matcher matches = ID_GROUP.matcher(name);
+    // If we have a group 0 that is not the name then something went wrong. Fallback to the name.
+    if (matches.matches() && matches.group(0).equals(name)) {
+      // If we find numbers in the group then instead of using the numbers use "###"
+      myId = matches.group(1);
+      myName = matches.group(1) + "###";
+    } else {
+      myId = name;
+      myName = name;
+    }
+  }
+
+  @Override
+  @NotNull
+  public String getName() {
+    return myName;
+  }
+
+  @Override
+  @NotNull
+  public String getFullName() {
+    return myName;
+  }
+
+  @Override
+  @NotNull
+  public String getId() {
+    return myId;
   }
 }
