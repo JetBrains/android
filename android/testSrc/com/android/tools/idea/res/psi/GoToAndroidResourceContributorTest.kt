86e0afa212cb13ab1732be7896c50504b629d1d4
Sergey Prigogin <sprigogin@google.com>
2019-11-21 15:27:31 -0800

    Use leaf resource repositories in GoToAndroidResourceContributor
    Using leaf resource repositories is more semantically correct since it allows navigation to all resource definitions when resource with the same name is defined in resource folders for different build flavors. It also has the benefit of being faster because it doesn't involve merging resources from multiple repositories.
    
    Test: GoToAndroidResourceContributorTest
    Bug: N/A
    Change-Id: Ib0ee85c19fd1abbf4e48af3b0271bfde871e57b1

--- android/testSrc/com/android/tools/idea/res/psi/GoToAndroidResourceContributorTest.kt
+++ android/testSrc/com/android/tools/idea/res/psi/GoToAndroidResourceContributorTest.kt
@@ -15,6 +15,8 @@
  */
 package com.android.tools.idea.res.psi
 
+import com.android.tools.idea.testing.AndroidProjectRule
+import com.android.tools.idea.testing.createAndroidProjectBuilder
 import com.google.common.truth.Truth
 import com.intellij.ide.util.gotoByName.GotoSymbolModel2
 import com.intellij.navigation.NavigationItem
@@ -24,21 +26,32 @@ import com.intellij.psi.PsiElement
 import com.intellij.psi.xml.XmlTag
 import com.intellij.psi.xml.XmlToken
 import com.intellij.testFramework.EditorTestUtil
+import com.intellij.testFramework.EdtRule
+import com.intellij.testFramework.RuleChain
+import com.intellij.testFramework.RunsInEdt
 import com.intellij.util.ui.UIUtil
-import org.jetbrains.android.AndroidTestCase
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
 
 /**
  * Tests for [GoToAndroidResourceContributor].
  */
-class GoToAndroidResourceContributorTest : AndroidTestCase() {
-  override fun setUp() {
-    super.setUp()
-    myFixture.addFileToProject("res/values/strings.xml", """
+@RunsInEdt
+class GoToAndroidResourceContributorTest {
+  val projectRule = AndroidProjectRule.withAndroidModel(createAndroidProjectBuilder())
+
+  @get:Rule
+  val chain = RuleChain(projectRule, EdtRule())
+
+  @Before
+  fun setUp() {
+    projectRule.fixture.addFileToProject("src/main/res/values/strings.xml", """
         <resources>
           <string name="my_string">My string</string>
         </resources>
         """.trimIndent())
-    myFixture.addFileToProject("res/layout/my_layout.xml", """
+    projectRule.fixture.addFileToProject("src/main/res/layout/my_layout.xml", """
         <FrameLayout xmlns:android="http://schemas.android.com/apk/res/android"
             xmlns:tools="http://schemas.android.com/tools"
             android:id="@+id/item_detail_container"
@@ -55,21 +68,21 @@ class GoToAndroidResourceContributorTest : AndroidTestCase() {
         """.trimIndent())
   }
 
-  private fun navigate(name: String, pattern: String): PsiElement {
-    val model = GotoSymbolModel2(myFixture.project)
+  private fun navigate(name: String, pattern: String, expectedNumberOfResults: Int = 1, selectResult: Int = 0): PsiElement {
+    val model = GotoSymbolModel2(projectRule.project)
     val searchResults = model.getElementsByName(name, false, pattern)
-    Truth.assertThat(searchResults).hasLength(1)
-    val result = searchResults[0]
+    Truth.assertThat(searchResults).hasLength(expectedNumberOfResults)
+    val result = searchResults[selectResult]
     Truth.assertThat(result).isInstanceOf(NavigationItem::class.java)
     Truth.assertThat((result as NavigationItem).presentation!!.getIcon(false)).isNotNull()
     UIUtil.dispatchAllInvocationEvents()
     result.navigate(true)
-    val editorManager = FileEditorManager.getInstance(myFixture.project)
+    val editorManager = FileEditorManager.getInstance(projectRule.project)
     val editor = editorManager.selectedTextEditor
     EditorTestUtil.waitForLoading(editor)
     val offset = editor!!.caretModel.offset
     val document = editor.document
-    val file = PsiDocumentManager.getInstance(myFixture.project).getPsiFile(document)
+    val file = PsiDocumentManager.getInstance(projectRule.project).getPsiFile(document)
     Truth.assertThat(file).isNotNull()
     var element = file!!.findElementAt(offset)
     if (element is XmlToken) {
@@ -79,17 +92,32 @@ class GoToAndroidResourceContributorTest : AndroidTestCase() {
     return element!!
   }
 
+  @Test
   fun testGoToString() {
     val element = navigate("my_string", "my_s")
     Truth.assertThat(element.text).isEqualTo("\"my_string\"")
     Truth.assertThat(element.parent.parent.text).isEqualTo("<string name=\"my_string\">My string</string>")
   }
 
+  @Test
+  fun testGoToStringDefinedInTwoPlaces() {
+    projectRule.fixture.addFileToProject("src/debug/res/values/strings.xml", """
+        <resources>
+          <string name="my_string">My debug string</string>
+        </resources>
+        """.trimIndent())
+    val element = navigate("my_string", "my_s", expectedNumberOfResults = 2, selectResult = 0)
+    Truth.assertThat(element.text).isEqualTo("\"my_string\"")
+    Truth.assertThat(element.parent.parent.text).isEqualTo("<string name=\"my_string\">My debug string</string>")
+  }
+
+  @Test
   fun testGoToId() {
     val element = navigate("my_button", "my_b")
     Truth.assertThat(element.text).isEqualTo("\"@+id/my_button\"")
   }
 
+  @Test
   fun testGoToLayout() {
     val element = navigate("my_layout", "my_l")
     Truth.assertThat(element).isInstanceOf(XmlTag::class.java)
@@ -100,11 +128,12 @@ class GoToAndroidResourceContributorTest : AndroidTestCase() {
    * Tries to emulate what [com.intellij.ide.actions.searcheverywhere.TrivialElementsEqualityProvider] is doing to
    * deduplicate the result list. Unfortunately some of the types involved are not public, so we cannot do exactly the same.
    */
+  @Test
   fun testEquality() {
     val contributor = GoToAndroidResourceContributor()
     val result: List<NavigationItem> = mutableListOf()
-    contributor.addItems(myModule, "my_layout", result)
-    contributor.addItems(myModule, "my_layout", result)
+    contributor.addItems(projectRule.module, "my_layout", result)
+    contributor.addItems(projectRule.module, "my_layout", result)
     Truth.assertThat(result).hasSize(2)
     Truth.assertThat(result.toSet()).hasSize(1)
   }
