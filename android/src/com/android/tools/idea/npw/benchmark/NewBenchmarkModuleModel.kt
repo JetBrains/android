6e7ba9456ea95ccacd48c51e1585542b2bbefbcd
Valery Cherepanov <qumeric@google.com>
2019-11-22 12:55:55 +0000

    Add mechanisms for new benchmark module
    Bug: 135397531
    Test: manual, will add TemplateTest later
    Change-Id: I13b43d734ccf527d8d33d98b16d171d029a1c09f

--- android/src/com/android/tools/idea/npw/benchmark/NewBenchmarkModuleModel.kt
+++ android/src/com/android/tools/idea/npw/benchmark/NewBenchmarkModuleModel.kt
@@ -15,10 +15,16 @@
  */
 package com.android.tools.idea.npw.benchmark
 
+import com.android.sdklib.SdkVersionInfo.LOWEST_ACTIVE_API
+import com.android.tools.idea.flags.StudioFlags
 import com.android.tools.idea.gradle.npw.project.GradleAndroidModuleTemplate.createDefaultTemplateAt
+import com.android.tools.idea.npw.FormFactor
 import com.android.tools.idea.npw.model.ProjectSyncInvoker
 import com.android.tools.idea.npw.model.RenderTemplateModel.Companion.getInitialSourceLanguage
+import com.android.tools.idea.npw.model.doRender
 import com.android.tools.idea.npw.module.ModuleModel
+import com.android.tools.idea.npw.module.getModuleRoot
+import com.android.tools.idea.npw.platform.AndroidVersionsInfo
 import com.android.tools.idea.npw.platform.AndroidVersionsInfo.VersionItem
 import com.android.tools.idea.npw.template.TemplateHandle
 import com.android.tools.idea.npw.template.TemplateValueInjector
@@ -27,7 +33,13 @@ import com.android.tools.idea.observable.core.StringValueProperty
 import com.android.tools.idea.templates.TemplateAttributes.ATTR_APP_TITLE
 import com.android.tools.idea.templates.TemplateAttributes.ATTR_IS_LIBRARY_MODULE
 import com.android.tools.idea.templates.TemplateAttributes.ATTR_IS_NEW_MODULE
+import com.android.tools.idea.templates.recipe.DefaultRecipeExecutor2
+import com.android.tools.idea.templates.recipe.FindReferencesRecipeExecutor2
+import com.android.tools.idea.templates.recipe.RenderingContext2
+import com.android.tools.idea.wizard.template.Recipe
+import com.android.tools.idea.wizard.template.TemplateData
 import com.intellij.openapi.project.Project
+import com.intellij.util.lang.JavaVersion
 
 class NewBenchmarkModuleModel(
   project: Project, templateHandle: TemplateHandle, projectSyncInvoker: ProjectSyncInvoker
@@ -55,6 +67,42 @@ class NewBenchmarkModuleModel(
         .setLanguage(language.value)
 
       templateValues.putAll(newValues)
+
+      if (StudioFlags.NPW_NEW_MODULE_TEMPLATES.get()) {
+        moduleTemplateDataBuilder.apply {
+          projectTemplateDataBuilder.apply {
+            setProjectDefaults(project)
+            language = this@NewBenchmarkModuleModel.language.value
+            javaVersion = JavaVersion.parse("1.8")
+          }
+          // TODO(qumeric): will it fail if there are no SDKs installed?
+          val anyTargetVersion = AndroidVersionsInfo().apply { loadLocalVersions() }
+            .getKnownTargetVersions(FormFactor.MOBILE, LOWEST_ACTIVE_API)
+            .first() // we dot'tn care which one do we use, we just have to pass something, it is not going to be used
+          setBuildVersion(anyTargetVersion, project)
+          isLibrary = true
+          setModuleRoots(modulePaths, project.basePath!!, moduleName.get(), this@NewBenchmarkModuleModel.packageName.get())
+        }
+      }
+    }
+    // TODO(qumeric): move it to ModuleModel when all modules will support the new system
+    override fun renderTemplate(dryRun: Boolean, project: Project, runFromTemplateRenderer: Boolean): Boolean {
+      val moduleRoot = getModuleRoot(project.basePath!!, moduleName.get())
+      if (StudioFlags.NPW_NEW_MODULE_TEMPLATES.get()) {
+        val context = RenderingContext2(
+          project = project,
+          module = null,
+          commandName = "New Benchmark Module",
+          templateData = moduleTemplateDataBuilder.build(),
+          moduleRoot = moduleRoot,
+          dryRun = dryRun,
+          showErrors = true
+        )
+        val executor = if (dryRun) FindReferencesRecipeExecutor2(context) else DefaultRecipeExecutor2(context)
+        val recipe: Recipe = { td: TemplateData -> /* TODO: generateBenchmarkModule(...) */ }
+        return recipe.doRender(context, executor)
+      }
+      return super.renderTemplate(dryRun, project, runFromTemplateRenderer)
     }
   }
 }
