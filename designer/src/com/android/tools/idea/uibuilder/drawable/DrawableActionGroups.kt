1ed1ac170148f73483cbdff4982c0a734170badc
Oscar Adame <oscarad@google.com>
2019-11-25 12:14:48 -0800

    Add the Theme selector menu for Drawable files
    Sometimes, files can be opened in the 'Material.Light' theme. Likely due
    to b/73332530.
    
    For layout files, the user can simply select the correct theme in the
    Layout Editor, but the same cannot be done for Drawables.
    
    This adds the Theme selector menu for Drawable files, so that it's
    possible not only to fix Drawable previews in the Layout Editor but also
    preview Drawables under a different theme.
    
    Bug: 145026814
    Test: manual
    Change-Id: I072ebd97196691706871afe4e93ab9e2c915be73

---
+++ designer/src/com/android/tools/idea/uibuilder/drawable/DrawableActionGroups.kt
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.uibuilder.drawable
+
+import com.android.tools.idea.common.editor.ToolbarActionGroups
+import com.android.tools.idea.common.surface.DesignSurface
+import com.android.tools.idea.configurations.ThemeMenuAction
+import com.intellij.openapi.actionSystem.ActionGroup
+import com.intellij.openapi.actionSystem.DefaultActionGroup
+
+/**
+ * The default [ToolbarActionGroups] for Drawable files.
+ */
+class DrawableActionGroups(surface: DesignSurface) : ToolbarActionGroups(surface) {
+
+  override fun getNorthGroup(): ActionGroup {
+    // TODO(b/136258816): Update to support multi-model
+    return DefaultActionGroup().apply { add(ThemeMenuAction(mySurface::getConfiguration)) }
+  }
+
+  override fun getNorthEastGroup(): ActionGroup {
+    return DefaultActionGroup().apply { addAll(getZoomActions()) }
+  }
+}
