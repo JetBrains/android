57c0cef9f7d0697d4ba3ff6b51e80c47f334f8ac
Valery Cherepanov <qumeric@google.com>
2019-11-28 10:10:20 +0000

    Split SourceImportModuleGalleryEntry into two
    And make them less dependent on template file
    
    Bug: 145335961
    Test: existing
    Change-Id: I0086b33b7831242573d47c12f1d1a269cd43195d

--- android/src/com/android/tools/idea/npw/module/ChooseModuleTypeStep.kt
+++ android/src/com/android/tools/idea/npw/module/ChooseModuleTypeStep.kt
@@ -93,7 +93,7 @@ fun sortModuleEntries(moduleTypesProviders: List<ModuleGalleryEntry>): List<Modu
     message("android.wizard.module.new.things"),
     message("android.wizard.module.import.gradle.title"),
     message("android.wizard.module.import.eclipse.title"),
-    message("android.wizard.module.import.title"),
+    message("android.wizard.module.import.archive.title"),
     message("android.wizard.module.new.java.or.kotlin.library"),
     message("android.wizard.module.new.google.cloud"),
     message("android.wizard.module.new.benchmark.module.app"))
