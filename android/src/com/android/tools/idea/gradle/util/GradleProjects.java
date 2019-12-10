ac546f7ed82f8f34e6634d2a2330e57cd4b30330
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2019-12-10 12:40:53 +0100

    open maven project under progress
    GitOrigin-RevId: 95eb83ec86c5999eaf2edd050622d26555ef1bb3

--- android/src/com/android/tools/idea/gradle/util/GradleProjects.java
+++ android/src/com/android/tools/idea/gradle/util/GradleProjects.java
@@ -15,12 +15,6 @@
  */
 package com.android.tools.idea.gradle.util;
 
-import static com.android.tools.idea.gradle.project.ProjectImportUtil.findImportTarget;
-import static com.android.tools.idea.io.FilePaths.toSystemDependentPath;
-import static com.intellij.ide.impl.ProjectUtil.updateLastProjectLocation;
-import static com.intellij.openapi.externalSystem.util.ExternalSystemApiUtil.isExternalSystemAwareModule;
-import static com.intellij.openapi.util.io.FileUtil.pathsEqual;
-
 import com.android.tools.idea.gradle.project.GradleProjectInfo;
 import com.android.tools.idea.gradle.project.facet.gradle.GradleFacet;
 import com.android.tools.idea.gradle.project.facet.java.JavaFacet;
@@ -36,17 +30,23 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.roots.ex.ProjectRootManagerEx;
 import com.intellij.openapi.util.Key;
 import com.intellij.openapi.vfs.VirtualFile;
-import java.io.File;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Objects;
-
 import com.intellij.platform.PlatformProjectOpenProcessor;
 import org.jetbrains.android.facet.AndroidFacet;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.jetbrains.plugins.gradle.settings.GradleSettings;
 import org.jetbrains.plugins.gradle.util.GradleConstants;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Objects;
+
+import static com.android.tools.idea.gradle.project.ProjectImportUtil.findImportTarget;
+import static com.android.tools.idea.io.FilePaths.toSystemDependentPath;
+import static com.intellij.ide.impl.ProjectUtil.updateLastProjectLocation;
+import static com.intellij.openapi.externalSystem.util.ExternalSystemApiUtil.isExternalSystemAwareModule;
+import static com.intellij.openapi.util.io.FileUtil.pathsEqual;
 
 /**
  * Utility methods for {@link Project}s.
@@ -79,10 +79,8 @@ public final class GradleProjects {
   public static void open(@NotNull Project project) {
     updateLastProjectLocation(project.getBasePath());
 
-    OpenProjectTask options = new OpenProjectTask();
-    options.setProject(project);
     Path projectDir = Paths.get(Objects.requireNonNull(project.getBasePath()));
-    PlatformProjectOpenProcessor.openExistingProject(projectDir, projectDir, options);
+    PlatformProjectOpenProcessor.openExistingProject(projectDir, projectDir, new OpenProjectTask(project));
   }
 
   public static boolean isOfflineBuildModeEnabled(@NotNull Project project) {
