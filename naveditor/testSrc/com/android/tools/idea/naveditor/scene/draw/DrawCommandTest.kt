f0c8715f71f6b476fe105ccfd9126721715e85b5
Mike Moroney <mmoro@google.com>
2019-12-15 10:35:56 -0800

    Test draw commands using mocks
    Convert the tests for placeholder, nav screen, and fragment
    draw commands to use a mock of the drawing context.
    
    Bug: 146392974
    Test: DrawCommandTest
    Change-Id: I74cb35137fbbd1ca5d27addf1548adbd28cc764c

---
+++ naveditor/testSrc/com/android/tools/idea/naveditor/scene/draw/DrawCommandTest.kt
@@ -0,0 +1,110 @@
+/*
+ * Copyright (C) 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.naveditor.scene.draw
+
+import com.android.tools.adtui.common.SwingRectangle
+import com.android.tools.idea.common.model.Scale
+import com.android.tools.idea.common.scene.SceneContext
+import com.android.tools.idea.common.scene.draw.DrawCommand
+import com.android.tools.idea.naveditor.NavTestCase
+import com.android.tools.idea.naveditor.scene.RefinableImage
+import com.intellij.util.ui.ImageUtil
+import org.mockito.InOrder
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.inOrder
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.verifyNoMoreInteractions
+import java.awt.Color
+import java.awt.FontMetrics
+import java.awt.Graphics2D
+import java.awt.geom.AffineTransform
+import java.awt.geom.Rectangle2D
+import java.awt.image.BufferedImage
+import java.awt.image.BufferedImage.TYPE_INT_ARGB
+import java.util.concurrent.CompletableFuture
+
+private val RECTANGLE = Rectangle2D.Float(10f, 20f, 80f, 120f)
+private val SCALE = Scale(1.5)
+@Suppress("UndesirableClassUsage")
+private val IMAGE = BufferedImage(5, 5, TYPE_INT_ARGB)
+
+class DrawCommandTest : NavTestCase() {
+  fun testDrawPlaceholder() {
+    val command = DrawPlaceholder(SwingRectangle(RECTANGLE))
+    verifyDrawCommand(command) { inOrder, g -> verifyDrawPlaceholder(inOrder, g, RECTANGLE) }
+  }
+
+  fun testDrawNavScreenWithImage() {
+    val image = RefinableImage(IMAGE)
+    val command = DrawNavScreen(SwingRectangle(RECTANGLE), image)
+    verifyDrawCommand(command) { inOrder, g -> verifyDrawNavScreenImage(inOrder, g, RECTANGLE, IMAGE) }
+  }
+
+  fun testDrawNavScreenWithLoading() {
+    val image = RefinableImage(null, CompletableFuture())
+    val command = DrawNavScreen(SwingRectangle(RECTANGLE), image)
+    verifyDrawCommand(command) { inOrder, g -> verifyDrawNavScreenLoading(inOrder, g, RECTANGLE) }
+  }
+
+  fun testDrawNavScreenWithPreviewUnavailable() {
+    val image = RefinableImage()
+    val command = DrawNavScreen(SwingRectangle(RECTANGLE), image)
+    verifyDrawCommand(command) { inOrder, g -> verifyDrawNavScreenPreviewUnavailable(inOrder, g, RECTANGLE) }
+  }
+
+  fun testDrawFragmentWithPlaceholder() {
+    val command = DrawFragment(SwingRectangle(RECTANGLE), SCALE, null)
+    verifyDrawCommand(command) { inOrder, g -> verifyDrawFragment(inOrder, g, RECTANGLE, SCALE) }
+  }
+
+  fun testDrawHighlightedFragment() {
+    val highlightColor = Color.RED
+    val command = DrawFragment(SwingRectangle(RECTANGLE), SCALE, highlightColor)
+    verifyDrawCommand(command) { inOrder, g -> verifyDrawFragment(inOrder, g, RECTANGLE, SCALE, highlightColor) }
+  }
+
+  fun testDrawFragmentWithPreviewUnavailable() {
+    val image = RefinableImage()
+    val command = DrawFragment(SwingRectangle(RECTANGLE), SCALE, null, image)
+    verifyDrawCommand(command) { inOrder, g -> verifyDrawFragment(inOrder, g, RECTANGLE, SCALE, null, image) }
+  }
+
+  fun testDrawFragmentWithLoading() {
+    val image = RefinableImage(null, CompletableFuture())
+    val command = DrawFragment(SwingRectangle(RECTANGLE), SCALE, null, image)
+    verifyDrawCommand(command) { inOrder, g -> verifyDrawFragment(inOrder, g, RECTANGLE, SCALE, null, image) }
+  }
+
+  fun testDrawFragmentWithImage() {
+    val image = RefinableImage(IMAGE)
+    val command = DrawFragment(SwingRectangle(RECTANGLE), SCALE, null, image)
+    verifyDrawCommand(command) { inOrder, g -> verifyDrawFragment(inOrder, g, RECTANGLE, SCALE, null, image) }
+  }
+
+  private fun verifyDrawCommand(command: DrawCommand, verifier: (InOrder, Graphics2D) -> Unit) {
+    val graphics = mock(Graphics2D::class.java)
+    `when`(graphics.create()).thenReturn(graphics)
+    val metrics = mock(FontMetrics::class.java)
+    `when`(graphics.fontMetrics).thenReturn(metrics)
+
+    val inOrder = inOrder(graphics)
+    val context = SceneContext.get()
+
+    command.paint(graphics, context)
+    verifier(inOrder, graphics)
+    verifyNoMoreInteractions(graphics)
+  }
+}
---
+++ naveditor/testSrc/com/android/tools/idea/naveditor/scene/draw/DrawCommandVerifier.kt
@@ -0,0 +1,147 @@
+/*
+ * Copyright (C) 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.naveditor.scene.draw
+
+import com.android.tools.idea.common.model.Scale
+import com.android.tools.idea.naveditor.scene.RefinableImage
+import org.mockito.ArgumentMatchers.any
+import org.mockito.ArgumentMatchers.anyFloat
+import org.mockito.ArgumentMatchers.anyInt
+import org.mockito.ArgumentMatchers.argThat
+import org.mockito.ArgumentMatchers.eq
+import org.mockito.InOrder
+import org.mockito.Mockito.times
+import org.mockito.internal.verification.AtLeast
+import java.awt.BasicStroke
+import java.awt.Color
+import java.awt.Graphics2D
+import java.awt.Shape
+import java.awt.Stroke
+import java.awt.geom.Line2D
+import java.awt.geom.Rectangle2D
+import java.awt.geom.RoundRectangle2D
+import java.awt.image.BufferedImage
+
+private val PLACEHOLDER_FILL = Color(0xfdfdfd)
+private val PLACEHOLDER_COLOR = Color(0xcccccc)
+private val PLACEHOLDER_TEXT = Color(0xcccccc)
+private val PLACEHOLDER_STROKE = BasicStroke(1f)
+
+private val FRAME_COLOR = Color(0xa7a7a7)
+private val FRAME_STROKE = BasicStroke(1f)
+private val HIGHLIGHTED_STROKE = BasicStroke(2f)
+
+private const val SPACING = 2f
+
+fun verifyDrawFragment(inOrder: InOrder,
+                       g: Graphics2D,
+                       rectangle: Rectangle2D.Float,
+                       scale: Scale,
+                       highlightColor: Color? = null,
+                       image: RefinableImage? = null) {
+  verifyDrawShape(inOrder, g, rectangle, FRAME_COLOR, FRAME_STROKE)
+  val imageRectangle = Rectangle2D.Float(rectangle.x + 1f, rectangle.y + 1f, rectangle.width - 2f, rectangle.height - 2f)
+  verifyDrawImage(inOrder, g, imageRectangle, image)
+
+  if (highlightColor != null) {
+    val spacing = 2 * SPACING * scale.value.toFloat()
+    val roundRectangle = RoundRectangle2D.Float(rectangle.x - spacing, rectangle.y - spacing,
+                                                rectangle.width + 2 * spacing, rectangle.height + 2 * spacing,
+                                                spacing, spacing)
+
+    verifyDrawShape(inOrder, g, roundRectangle, highlightColor, HIGHLIGHTED_STROKE)
+  }
+}
+
+fun verifyDrawImage(inOrder: InOrder, g: Graphics2D, rectangle: Rectangle2D.Float, image: RefinableImage?) {
+  if (image == null) {
+    verifyDrawPlaceholder(inOrder, g, rectangle)
+    return
+  }
+
+  val lastCompleted = image.lastCompleted
+  val bufferedImage = lastCompleted.image
+
+  when {
+    bufferedImage != null -> verifyDrawNavScreenImage(inOrder, g, rectangle, bufferedImage)
+    lastCompleted.refined == null -> verifyDrawNavScreenPreviewUnavailable(inOrder, g, rectangle)
+    else -> verifyDrawNavScreenLoading(inOrder, g, rectangle)
+  }
+}
+
+fun verifyDrawNavScreenImage(inOrder: InOrder, g: Graphics2D, rectangle: Rectangle2D.Float, image: BufferedImage) {
+  inOrder.verify(g).create()
+  inOrder.verify(g).setRenderingHints(any())
+  inOrder.verify(g).clip(argThat(ShapeArgumentMatcher(rectangle)))
+  inOrder.verify(g).drawImage(eq(image), eq(rectangle.x.toInt()), eq(rectangle.y.toInt()), anyInt(), anyInt(), eq(null))
+  inOrder.verify(g).dispose()
+}
+
+fun verifyDrawNavScreenLoading(inOrder: InOrder, g: Graphics2D, rectangle: Rectangle2D.Float) {
+  inOrder.verify(g).create()
+  inOrder.verify(g).setRenderingHints(any())
+  inOrder.verify(g).clip(argThat(ShapeArgumentMatcher(rectangle)))
+  inOrder.verify(g).color = PLACEHOLDER_FILL
+  inOrder.verify(g).fill(argThat(ShapeArgumentMatcher(rectangle)))
+  inOrder.verify(g).color = PLACEHOLDER_TEXT
+  inOrder.verify(g).font = any()
+  inOrder.verify(g).fontMetrics
+  inOrder.verify(g).drawString(eq("Loading..."), anyFloat(), anyFloat())
+  inOrder.verify(g).dispose()
+}
+
+fun verifyDrawNavScreenPreviewUnavailable(inOrder: InOrder, g: Graphics2D, rectangle: Rectangle2D.Float) {
+  inOrder.verify(g).create()
+  inOrder.verify(g).setRenderingHints(any())
+  inOrder.verify(g).clip(argThat(ShapeArgumentMatcher(rectangle)))
+  inOrder.verify(g).color = PLACEHOLDER_FILL
+  inOrder.verify(g).fill(argThat(ShapeArgumentMatcher(rectangle)))
+  inOrder.verify(g).color = PLACEHOLDER_TEXT
+  inOrder.verify(g).font = any()
+  inOrder.verify(g).fontMetrics
+  inOrder.verify(g).drawString(eq("Preview"), anyFloat(), anyFloat())
+  inOrder.verify(g, times(2)).fontMetrics
+  inOrder.verify(g).drawString(eq("Unavailable"), anyFloat(), anyFloat())
+  inOrder.verify(g).dispose()
+}
+
+fun verifyDrawPlaceholder(inOrder: InOrder, g: Graphics2D, rectangle: Rectangle2D.Float) {
+  verifyFillShape(inOrder, g, rectangle, PLACEHOLDER_FILL)
+  val x1 = rectangle.x
+  val x2 = x1 + rectangle.width
+  val y1 = rectangle.y
+  val y2 = y1 + rectangle.height
+
+  verifyDrawShape(inOrder, g, Line2D.Float(x1, y1, x2, y2), PLACEHOLDER_COLOR, PLACEHOLDER_STROKE)
+  verifyDrawShape(inOrder, g, Line2D.Float(x1, y2, x2, y1), PLACEHOLDER_COLOR, PLACEHOLDER_STROKE)
+}
+
+fun verifyFillShape(inOrder: InOrder, g: Graphics2D, shape: Shape, color: Color) {
+  inOrder.verify(g).create()
+  inOrder.verify(g).setRenderingHints(any())
+  inOrder.verify(g).color = color
+  inOrder.verify(g).fill(shape)
+  inOrder.verify(g).dispose()
+}
+
+fun verifyDrawShape(inOrder: InOrder, g: Graphics2D, shape: Shape, color: Color, stroke: Stroke) {
+  inOrder.verify(g).create()
+  inOrder.verify(g).setRenderingHints(any())
+  inOrder.verify(g).color = color
+  inOrder.verify(g).stroke = stroke
+  inOrder.verify(g).draw(argThat(ShapeArgumentMatcher(shape)))
+  inOrder.verify(g).dispose()
+}
