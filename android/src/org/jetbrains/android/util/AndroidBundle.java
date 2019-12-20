9ea33287aab1a13d6f59b40dba882ce9b46a822f
Sergey Ignatov <sergey.ignatov@jetbrains.com>
2019-12-21 00:22:55 +0300

    android bundles
    GitOrigin-RevId: 84d132ae6e155aea46fe9cbca1c1f8944aff7d80

--- android/src/org/jetbrains/android/util/AndroidBundle.java
+++ android/src/org/jetbrains/android/util/AndroidBundle.java
@@ -1,38 +1,19 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.android.util;
 
-import com.intellij.CommonBundle;
+import com.intellij.DynamicBundle;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.PropertyKey;
 
-import java.lang.ref.Reference;
-import java.lang.ref.SoftReference;
-import java.util.ResourceBundle;
+public class AndroidBundle extends DynamicBundle {
+  @NonNls private static final String BUNDLE = "messages.AndroidBundle";
+  private static final AndroidBundle INSTANCE = new AndroidBundle();
 
-/**
- * Messages bundle.
- *
- * @author Alexey Efimov
- */
-public final class AndroidBundle {
-  @NonNls
-  private static final String BUNDLE_NAME = "messages.AndroidBundle";
-  private static Reference<ResourceBundle> ourBundle;
+  private AndroidBundle() { super(BUNDLE); }
 
-  private static ResourceBundle getBundle() {
-    ResourceBundle bundle = com.intellij.reference.SoftReference.dereference(ourBundle);
-    if (bundle == null) {
-      bundle = ResourceBundle.getBundle(BUNDLE_NAME);
-      ourBundle = new SoftReference<>(bundle);
-    }
-    return bundle;
-  }
-
-  private AndroidBundle() {
-  }
-
-  public static String message(@NotNull @PropertyKey(resourceBundle = BUNDLE_NAME) String key, @NotNull Object... params) {
-    return CommonBundle.message(getBundle(), key, params);
+  @NotNull
+  public static String message(@NotNull @PropertyKey(resourceBundle = BUNDLE) String key, @NotNull Object... params) {
+    return INSTANCE.getMessage(key, params);
   }
 }
