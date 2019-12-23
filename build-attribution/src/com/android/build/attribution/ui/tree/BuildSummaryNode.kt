2a04f3411bf15e6a37a17ecd91bbbf7565473e7d
Maksim Lazeba <mlazeba@google.com>
2019-12-23 15:55:55 +0000

    Show chart in Build Attribution summary page
    Bug: 144393368
    Test: N/A
    Change-Id: I1da743a2b8b75a83cfc016a2841c2acb50eb567a

--- build-attribution/src/com/android/build/attribution/ui/tree/BuildSummaryNode.kt
+++ build-attribution/src/com/android/build/attribution/ui/tree/BuildSummaryNode.kt
@@ -16,14 +16,16 @@
 package com.android.build.attribution.ui.tree
 
 import com.android.build.attribution.ui.data.BuildSummary
+import com.android.build.attribution.ui.data.TimeWithPercentage
 import com.android.build.attribution.ui.durationString
 import com.android.build.attribution.ui.panels.AbstractBuildAttributionInfoPanel
+import com.android.build.attribution.ui.panels.ChartBuildAttributionInfoPanel
+import com.android.build.attribution.ui.panels.CriticalPathChartLegend
+import com.android.build.attribution.ui.panels.CriticalPathChartLegend.categoricalGooglePalette
+import com.android.build.attribution.ui.panels.TimeDistributionChart
 import com.android.build.attribution.ui.panels.headerLabel
-import com.android.utils.HtmlBuilder
 import com.google.wireless.android.sdk.stats.BuildAttributionUiEvent
 import com.intellij.ui.components.JBLabel
-import com.intellij.ui.components.JBPanel
-import com.intellij.ui.components.panels.VerticalLayout
 import com.intellij.ui.treeStructure.SimpleNode
 import com.intellij.util.text.DateFormatUtil
 import javax.swing.Icon
@@ -34,35 +36,42 @@ class BuildSummaryNode(
   parent: ControllersAwareBuildAttributionNode
 ) : AbstractBuildAttributionNode(parent, "Build:") {
 
+  private val chartItems: List<TimeDistributionChart.ChartDataItem<String>> = listOf(
+    SimpleChartItem(buildSummary.configurationDuration, "Build configuration", categoricalGooglePalette[0]),
+    SimpleChartItem(buildSummary.criticalPathDuration, "Tasks execution", categoricalGooglePalette[1]),
+    MiscGradleStepsChartItem(buildSummary.miscStepsTime)
+  )
   override val presentationIcon: Icon? = null
   override val issuesCountsSuffix = "finished at ${buildFinishedTime()}"
   override val timeSuffix = buildSummary.totalBuildDuration.durationString()
   override fun buildChildren() = emptyArray<SimpleNode>()
-  override fun createComponent(): AbstractBuildAttributionInfoPanel = object : AbstractBuildAttributionInfoPanel() {
+  override fun createComponent(): AbstractBuildAttributionInfoPanel = object : ChartBuildAttributionInfoPanel() {
 
     override fun createHeader(): JComponent {
       return headerLabel("Build finished at ${buildFinishedTime()}")
     }
 
-    override fun createBody(): JComponent {
-      return JBPanel<JBPanel<*>>(VerticalLayout(6)).apply {
-        add(JBLabel().apply {
-          setAllowAutoWrapping(true)
-          setCopyable(true)
-          text = HtmlBuilder()
-            .openHtmlBody()
-            .add("Total build duration was ${buildSummary.totalBuildDuration.durationString()}, it includes:")
-            .newline()
-            .add("Build configuration: ${buildSummary.configurationDuration.durationString()}")
-            .newline()
-            .add("Critical path tasks execution: ${buildSummary.criticalPathDuration.durationString()}")
-            .closeHtmlBody().html
-        })
-      }
-    }
+    override fun createChart(): JComponent = TimeDistributionChart(chartItems, null, true)
+
+    override fun createRightInfoPanel(): JComponent? = null
+    override fun createLegend(): JComponent? = null
+    override fun createDescription(): JComponent =
+      JBLabel("Total build duration was ${buildSummary.totalBuildDuration.durationString()}.")
   }
 
   override val pageType = BuildAttributionUiEvent.Page.PageType.BUILD_SUMMARY
 
   private fun buildFinishedTime(): String = DateFormatUtil.formatDateTime(buildSummary.buildFinishedTimestamp)
+
+  class SimpleChartItem(
+    private val time: TimeWithPercentage,
+    private val text: String,
+    private val color: CriticalPathChartLegend.ChartColor
+  ) : TimeDistributionChart.ChartDataItem<String> {
+    override fun time(): TimeWithPercentage = time
+    override fun text(): String = text
+    override fun getLegendColor(): CriticalPathChartLegend.ChartColor = color
+    override fun getTableIcon(): Icon? = null
+    override fun chartBoxText(): String? = null
+  }
 }
