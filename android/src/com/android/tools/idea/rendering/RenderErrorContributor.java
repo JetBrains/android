783553e9f77fada562fbf9dda2667f63181ebfba
Dmitry.Krasilschikov <kradima@jetbrains.com>
2020-01-01 22:10:26 +0200

    LAB-62 get rid of static *Bundle usages: compilation fix
    GitOrigin-RevId: e5f2a40836d300088e1cd5179cf0584e4bb1f6c7

--- android/src/com/android/tools/idea/rendering/RenderErrorContributor.java
+++ android/src/com/android/tools/idea/rendering/RenderErrorContributor.java
@@ -1478,7 +1478,7 @@ public class RenderErrorContributor {
       final String moduleToSelect = !myProblemModules.isEmpty()
                                     ? myProblemModules.iterator().next().getName()
                                     : null;
-      if (ModulesConfigurator.showDialog(myProject, moduleToSelect, ClasspathEditor.getNAME())) {
+      if (ModulesConfigurator.showDialog(myProject, moduleToSelect, ClasspathEditor.getName())) {
         askAndRebuild(myProject);
       }
     }
