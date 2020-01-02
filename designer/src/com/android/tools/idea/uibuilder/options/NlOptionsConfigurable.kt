76ee8e49dcb72aa4c1036608b65ec757955afd0c
Amaury Medeiros <amaurym@google.com>
2020-01-02 18:44:34 +0000

    Add default split editor mode comboboxes to settings panel
    Add two comboboxes to the Layout Editor section of the settings panel: one
    corresponding to drawable files, and another one for other resources. These
    comboboxes can be used to set the default mode of the split editor when
    opening resource files.
    
    Files opened for the first time, and consequently new files, will be open in
    the mode determined by the comboboxes. Files will continue be reopened in
    the mode they were before being closed last time.
    
    Bug: 147066397
    Bug: 144513660
    Test: Manual by changing the combobox value and opening new resource files.
    Change-Id: I3413c4117ca2470f86156513f4efa3299fdabb09

--- designer/src/com/android/tools/idea/uibuilder/options/NlOptionsConfigurable.kt
+++ designer/src/com/android/tools/idea/uibuilder/options/NlOptionsConfigurable.kt
@@ -9,21 +9,33 @@ import com.intellij.ui.components.JBCheckBox
 import com.intellij.ui.layout.panel
 import org.jetbrains.android.uipreview.AndroidEditorSettings
 import org.jetbrains.annotations.Nls
+import javax.swing.JComboBox
 
 class NlOptionsConfigurable : SearchableConfigurable, Configurable.NoScroll {
+
+  private class EditorModeComboBox : JComboBox<AndroidEditorSettings.EditorMode>(AndroidEditorSettings.EditorMode.values())
+
   private val preferXmlEditor = JBCheckBox("Prefer XML editor")
   private val showLint = JBCheckBox("Show lint icons on design surface")
   private val hideForNonLayoutFiles = JBCheckBox("Hide preview window when editing non-layout files")
+  private val preferredDrawablesEditorMode = EditorModeComboBox()
+  private val preferredEditorMode = EditorModeComboBox()
 
   private val state = AndroidEditorSettings.getInstance().globalState
 
   override fun getId() = "nele.options"
 
   override fun createComponent() = panel {
-    row { preferXmlEditor() }
     row { showLint() }
     if (!StudioFlags.NELE_SPLIT_EDITOR.get()) {
+      row { preferXmlEditor() }
       row { hideForNonLayoutFiles() }
+    }
+    else {
+      titledRow("Default Editor Mode") {
+        row("Drawables:") { preferredDrawablesEditorMode() }
+        row("Other Resources (e.g. Layout, Menu, Navigation):") { preferredEditorMode() }
+      }
     }
   }
 
@@ -31,18 +43,40 @@ class NlOptionsConfigurable : SearchableConfigurable, Configurable.NoScroll {
     preferXmlEditor.isSelected != state.isPreferXmlEditor
     || showLint.isSelected != state.isShowLint
     || hideForNonLayoutFiles.isSelected != state.isHideForNonLayoutFiles
+    || preferredDrawablesEditorMode.selectedItem != state.preferredDrawableEditorMode
+    || preferredEditorMode.selectedItem != state.preferredEditorMode
 
   @Throws(ConfigurationException::class)
   override fun apply() {
     state.isPreferXmlEditor = preferXmlEditor.isSelected
     state.isShowLint = showLint.isSelected
     state.isHideForNonLayoutFiles = hideForNonLayoutFiles.isSelected
+    state.preferredDrawableEditorMode = preferredDrawablesEditorMode.selectedItem as AndroidEditorSettings.EditorMode
+    state.preferredEditorMode = preferredEditorMode.selectedItem as AndroidEditorSettings.EditorMode
   }
 
   override fun reset() {
     preferXmlEditor.isSelected = state.isPreferXmlEditor
     showLint.isSelected = state.isShowLint
     hideForNonLayoutFiles.isSelected = state.isHideForNonLayoutFiles
+
+    // Handle the case where preferredDrawableEditorMode and preferredEditorMode were not set for the first time yet.
+    if (state.preferredDrawableEditorMode == null && state.preferredEditorMode == null) {
+      if (state.isPreferXmlEditor) {
+        // Preserve the user preference if they had set the old "Prefer XML editor" option.
+        preferredDrawablesEditorMode.selectedItem = AndroidEditorSettings.EditorMode.CODE
+        preferredEditorMode.selectedItem = AndroidEditorSettings.EditorMode.CODE
+      }
+      else {
+        // Otherwise default drawables to SPLIT and other resource types to DESIGN
+        preferredDrawablesEditorMode.selectedItem = AndroidEditorSettings.EditorMode.SPLIT
+        preferredEditorMode.selectedItem = AndroidEditorSettings.EditorMode.DESIGN
+      }
+    }
+    else {
+      preferredDrawablesEditorMode.selectedItem = state.preferredDrawableEditorMode
+      preferredEditorMode.selectedItem = state.preferredEditorMode
+    }
   }
 
   @Nls
