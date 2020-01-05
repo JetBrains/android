6049dc3d483c73dc22f394e386ceeefb11609f55
David Herman <davidherman@google.com>
2020-01-05 11:34:56 -0800

    Add action methods to the Directions class
    Bug: 147008436
    Test: Added
    Change-Id: If9ec4d72be2747676423e841431da18a0c7d6221

---
+++ nav/safeargs/src/com/android/tools/idea/nav/safeargs/psi/SafeArgsLightClassUtils.kt
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.nav.safeargs.psi
+
+import com.intellij.lang.java.JavaLanguage
+import com.intellij.psi.PsiClass
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiElementFactory
+import com.intellij.psi.PsiModifier
+import com.intellij.psi.PsiType
+import com.intellij.psi.impl.light.LightMethodBuilder
+import com.intellij.util.IncorrectOperationException
+
+internal val MODIFIERS_PUBLIC_METHOD = arrayOf(PsiModifier.PUBLIC, PsiModifier.FINAL)
+internal val MODIFIERS_STATIC_PUBLIC_METHOD = MODIFIERS_PUBLIC_METHOD + arrayOf(PsiModifier.STATIC)
+
+private const val STRING_FQCN = "java.lang.String"
+private const val FALLBACK_TYPE = STRING_FQCN
+
+private val NAV_TO_JAVA_TYPE_MAP = mapOf(
+  "string" to STRING_FQCN,
+  "integer" to "int"
+)
+
+internal fun parsePsiType(modulePackage: String, typeStr: String, context: PsiElement): PsiType {
+  @Suppress("NAME_SHADOWING") // Shadowing to convert val to var
+  var typeStr = typeStr.takeUnless { it.isEmpty() } ?: FALLBACK_TYPE
+  typeStr = NAV_TO_JAVA_TYPE_MAP.getOrDefault(typeStr, typeStr)
+  typeStr = if (!typeStr.startsWith('.')) typeStr else "$modulePackage$typeStr"
+  return try {
+    PsiElementFactory.getInstance(context.project).createTypeFromText(typeStr, context)
+  }
+  catch (e: IncorrectOperationException) {
+    PsiElementFactory.getInstance(context.project).createTypeFromText(FALLBACK_TYPE, context)
+  }
+}
+
+internal fun PsiClass.createMethod(name: String,
+                          modifiers: Array<String> = MODIFIERS_PUBLIC_METHOD,
+                          returnType: PsiType = PsiType.VOID): LightMethodBuilder {
+  return LightMethodBuilder(manager, JavaLanguage.INSTANCE, name)
+    .setContainingClass(this)
+    .setMethodReturnType(returnType)
+    .addModifiers(*modifiers)
+}
--- nav/safeargs/testSrc/com/android/tools/idea/nav/safeargs/psi/LightDirectionsClassTest.kt
+++ nav/safeargs/testSrc/com/android/tools/idea/nav/safeargs/psi/LightDirectionsClassTest.kt
@@ -20,6 +20,8 @@ import com.android.tools.idea.res.ResourceRepositoryManager
 import com.android.tools.idea.testing.findClass
 import com.google.common.truth.Truth.assertThat
 import com.intellij.psi.JavaPsiFacade
+import com.intellij.psi.PsiPrimitiveType
+import com.intellij.psi.impl.source.PsiClassReferenceType
 import com.intellij.psi.search.GlobalSearchScope
 import com.intellij.testFramework.RunsInEdt
 import org.junit.Rule
@@ -65,5 +67,97 @@ class LightDirectionsClassTest {
     // ... but cannot be found without context
     val psiFacade = JavaPsiFacade.getInstance(safeArgsRule.project)
     assertThat(psiFacade.findClass("test.safeargs.Fragment1Directions", GlobalSearchScope.allScope(safeArgsRule.project))).isNull()
+  }
+
+  @Test
+  fun actionMethodsGeneratedWithArgs() {
+    safeArgsRule.fixture.addFileToProject(
+      "res/navigation/main.xml",
+      //language=XML
+      """
+        <?xml version="1.0" encoding="utf-8"?>
+        <navigation xmlns:android="http://schemas.android.com/apk/res/android"
+            xmlns:app="http://schemas.android.com/apk/res-auto" android:id="@+id/main"
+            app:startDestination="@id/fragment1">
+
+          <action
+            android:id="@+id/action_to_nested"
+            app:destination="@id/nested"/>
+
+          <fragment
+              android:id="@+id/fragment1"
+              android:name="test.safeargs.Fragment1"
+              android:label="Fragment1" >
+            <action
+              android:id="@+id/action_fragment1_to_fragment2"
+              app:destination="@id/fragment2" />
+            <action
+              android:id="@+id/action_fragment1_to_fragment3"
+              app:destination="@id/fragment3" />
+          </fragment>
+
+          <fragment
+              android:id="@+id/fragment2"
+              android:name="test.safeargs.Fragment2"
+              android:label="Fragment2" >
+            <argument
+                android:name="arg1"
+                app:argType="string" />
+            <argument
+                android:name="arg2"
+                app:argType="float" />
+          </fragment>
+
+          <navigation
+              android:id="@+id/nested"
+              app:startDestination="@id/fragment3">
+
+            <fragment
+                android:id="@+id/fragment3"
+                android:name="test.safeargs.Fragment3"
+                android:label="Fragment3">
+              <argument
+                  android:name="arg"
+                  app:argType="integer" />
+            </fragment>
+
+          </navigation>
+        </navigation>
+      """.trimIndent())
+
+    // Initialize repository after creating resources, needed for codegen to work
+    ResourceRepositoryManager.getInstance(safeArgsRule.androidFacet).moduleResources
+
+    val context = safeArgsRule.fixture.addClass("package test.safeargs; public class Fragment1 {}")
+
+    val mainDirections = safeArgsRule.fixture.findClass("test.safeargs.MainDirections", context) as LightDirectionsClass
+    val fragment1directions = safeArgsRule.fixture.findClass("test.safeargs.Fragment1Directions", context) as LightDirectionsClass
+
+    mainDirections.findMethodsByName("actionToNested").first().let { action ->
+      assertThat(action.parameters).isEmpty()
+      assertThat((action.returnType as PsiClassReferenceType).className).isEqualTo("NavDirections")
+    }
+
+    fragment1directions.findMethodsByName("actionFragment1ToFragment2").first().let { action ->
+      assertThat(action.parameters.size).isEqualTo(2)
+      action.parameters[0].let { arg1 ->
+        assertThat((arg1.type as PsiClassReferenceType).className).isEqualTo("String")
+        assertThat(arg1.name).isEqualTo("arg1")
+      }
+      action.parameters[1].let { arg2 ->
+        assertThat((arg2.type as PsiPrimitiveType).name).isEqualTo("float")
+        assertThat(arg2.name).isEqualTo("arg2")
+      }
+      assertThat((action.returnType as PsiClassReferenceType).className).isEqualTo("NavDirections")
+    }
+    fragment1directions.findMethodsByName("actionFragment1ToFragment3").first().let { action ->
+      assertThat(action.parameters.size).isEqualTo(1)
+      action.parameters[0].let { arg1 ->
+        assertThat((arg1.type as PsiPrimitiveType).name).isEqualTo("int")
+        assertThat(arg1.name).isEqualTo("arg")
+      }
+
+      assertThat((action.returnType as PsiClassReferenceType).className).isEqualTo("NavDirections")
+    }
   }
 }
