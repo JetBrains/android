36d926fa3c6bd4f4c9004398164a0dbf902f844d
Amaury Medeiros <amaurym@google.com>
2020-01-07 18:41:45 +0000

    Add RunConfiguration to launch Compose Previews to a device
    Create ComposePreviewRunConfigurationType and ComposePreviewRunConfiguration
    to represent configurations to launch the Compose tooling PreviewActivity to
    a device/emulator passing a @Composable to be set as its main content.
    
    This allows running Compose Previews directly on a device/emulator, speeding
    up the process of checking how your @Composable's will look like when used
    in specific targets.
    
    We don't register ComposePreviewRunConfigurationType in compose-designer.xml
    yet, otherwise it would show up in the list of RunConfiguration. That should
    only happen when the feature is ready to be enabled.
    
    Bug: 144500928
    Test: ComposePreviewRunConfigurationTest
    Change-Id: Ic6ec6cb9b4848897e030a08f70c54393ac6ea7fc

---
+++ compose-designer/testSrc/com/android/tools/idea/compose/runconfiguration/ComposePreviewRunConfigurationTest.kt
@@ -0,0 +1,67 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.compose.runconfiguration
+
+import com.android.ddmlib.IDevice
+import com.android.tools.idea.compose.preview.runconfiguration.ComposePreviewRunConfiguration
+import com.android.tools.idea.compose.preview.runconfiguration.ComposePreviewRunConfigurationType
+import com.android.tools.idea.run.ApplicationIdProvider
+import com.android.tools.idea.run.ConsolePrinter
+import com.android.tools.idea.run.tasks.ActivityLaunchTask
+import com.android.tools.idea.run.tasks.LaunchTask
+import com.android.tools.idea.run.util.LaunchStatus
+import com.intellij.execution.configurations.ConfigurationFactory
+import com.intellij.openapi.project.Project
+import org.jetbrains.android.AndroidTestCase
+import org.jetbrains.android.facet.AndroidFacet
+import org.mockito.Mockito.mock
+
+class ComposePreviewRunConfigurationTest : AndroidTestCase() {
+
+  fun testAmStartOptionsWithComposableMethod() {
+    val runConfigurationFactory = ComposePreviewRunConfigurationType().configurationFactories[0]
+    val runConfiguration = TestComposePreviewRunConfiguration(project, runConfigurationFactory)
+    runConfiguration.setComposableMethod("com.mycomposeapp.SomeClass.SomeComposable")
+
+    val status = mock(LaunchStatus::class.java)
+    val task = runConfiguration.getApplicationLaunchTask(FakeApplicationIdProvider(), myFacet, "", false, status) as ActivityLaunchTask
+    assertEquals("am start -n \"com.example.myapp/androidx.ui.tooling.preview.PreviewActivity\" " +
+                 "-a android.intent.action.MAIN -c android.intent.category.LAUNCHER " +
+                 "--es composable com.mycomposeapp.SomeClass.SomeComposable",
+                 task.getStartActivityCommand(mock(IDevice::class.java), status, mock(ConsolePrinter::class.java)))
+  }
+
+  private class FakeApplicationIdProvider : ApplicationIdProvider {
+    override fun getPackageName() = "com.example.myapp"
+
+    override fun getTestPackageName(): Nothing? {
+      return null
+    }
+  }
+
+  private class TestComposePreviewRunConfiguration(project: Project, factory: ConfigurationFactory)
+    : ComposePreviewRunConfiguration(project, factory) {
+
+    // Relax visibility to call the super method (which has protected visibility) in this test
+    public override fun getApplicationLaunchTask(applicationIdProvider: ApplicationIdProvider,
+                                                 facet: AndroidFacet,
+                                                 contributorsAmStartOptions: String,
+                                                 waitForDebugger: Boolean,
+                                                 launchStatus: LaunchStatus): LaunchTask? {
+      return super.getApplicationLaunchTask(applicationIdProvider, facet, contributorsAmStartOptions, waitForDebugger, launchStatus)
+    }
+  }
+}
