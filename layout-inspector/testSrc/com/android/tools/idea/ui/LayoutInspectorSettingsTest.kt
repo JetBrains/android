86f530b3bde8209ef945c4814b39ebe293b7db01
Joe Baker-Malone <jbakermalone@google.com>
2020-01-09 10:59:32 -0800

    Fix experimental setting
    The PropertiesComponent API apparently isn't great, leading to us not
    actually persist the setting when we called setValue().
    Also fixing a problem where we were trying to initialize the tool window
    before setting the flag, causing it not to be initialized until restart.
    
    Fixes: 147382931
    Test: Added a test
    Change-Id: Ie4e602bd262b5da14d547dc86ea0ad45804660aa

---
+++ layout-inspector/testSrc/com/android/tools/idea/ui/LayoutInspectorSettingsTest.kt
@@ -0,0 +1,114 @@
+/*
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.ui
+
+import com.android.tools.idea.layoutinspector.LAYOUT_INSPECTOR_TOOL_WINDOW_ID
+import com.android.tools.idea.testing.AndroidProjectRule
+import com.google.common.truth.Truth.assertThat
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.wm.ToolWindow
+import com.intellij.openapi.wm.ToolWindowEP
+import com.intellij.openapi.wm.ToolWindowManager
+import com.intellij.openapi.wm.impl.ToolWindowHeadlessManagerImpl
+import com.intellij.testFramework.registerComponentInstance
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+class LayoutInspectorSettingsTest {
+
+  @get:Rule
+  val projectRule = AndroidProjectRule.inMemory()
+  private lateinit var windowManager: ToolWindowManager
+
+  private var originalValue = true
+
+  @Before
+  fun setUp() {
+    originalValue = enableLiveLayoutInspector
+    val project = projectRule.project
+    windowManager = MyToolWindowManager(project)
+    project.registerComponentInstance(ToolWindowManager::class.java, windowManager, project)
+  }
+
+
+  @After
+  fun tearDown() {
+    ApplicationManager.getApplication().invokeAndWait {
+      enableLiveLayoutInspector = originalValue
+    }
+  }
+
+  @Test
+  fun testSettingPersists() {
+    ApplicationManager.getApplication().invokeAndWait {
+      enableLiveLayoutInspector = false
+      assertThat(enableLiveLayoutInspector).isFalse()
+      assertThat(windowManager.getToolWindow("Layout Inspector")).isNull()
+
+      enableLiveLayoutInspector = true
+      assertThat(enableLiveLayoutInspector).isTrue()
+      assertThat(windowManager.getToolWindow("Layout Inspector")).isNotNull()
+      assertThat(windowManager.getToolWindow("Layout Inspector")!!.isAvailable).isTrue()
+
+      enableLiveLayoutInspector = false
+      assertThat(enableLiveLayoutInspector).isFalse()
+      assertThat(windowManager.getToolWindow("Layout Inspector")!!.isAvailable).isFalse()
+    }
+  }
+
+  private class MyToolWindowManager(val project: Project) : ToolWindowHeadlessManagerImpl(project) {
+    var toolWindow: ToolWindow? = null
+
+    override fun initToolWindow(bean: ToolWindowEP) {
+      assertThat(bean.condition!!.value(project)).isTrue()
+      assertThat(bean.id).isEqualTo(LAYOUT_INSPECTOR_TOOL_WINDOW_ID)
+      toolWindow = MyMockToolWindow(project)
+    }
+
+    override fun getToolWindow(id: String?): ToolWindow? {
+      assertThat(id).isEqualTo(LAYOUT_INSPECTOR_TOOL_WINDOW_ID)
+      return toolWindow
+    }
+  }
+
+  private class MyMockToolWindow(project: Project) : ToolWindowHeadlessManagerImpl.MockToolWindow(project) {
+    var available = true
+    var visible = true
+
+    override fun setAvailable(available: Boolean, runnable: Runnable?) {
+      this.available = available
+    }
+
+    override fun isAvailable(): Boolean {
+      return available
+    }
+
+    override fun show(runnable: Runnable?) {
+      visible = true
+    }
+
+    override fun hide(runnable: Runnable?) {
+      visible = false
+    }
+
+    override fun isVisible(): Boolean {
+      return visible
+    }
+  }
+}
