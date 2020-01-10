bf4421f878aa81bb8e132a62a1df809dd79fa8a7
Yi Yang <yiyiyang@google.com>
2020-01-10 13:17:12 -0800

    Fix a bug where filter is not applied
    A previous CL introduced the bug where filtered result is not being used
    when creating the capture detail view.
    
    Fixes: 146379332
    Test: CpuAnalysisChartTest
    Change-Id: I43423096768faccb1369b63ff3225d5e312a0075
    (cherry picked from commit 4051f522721d7f02a09f0a261e11d4f230313d06)

---
+++ profilers-ui/testSrc/com/android/tools/profilers/cpu/analysis/CpuAnalysisChartTest.kt
@@ -0,0 +1,63 @@
+/*
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.profilers.cpu.analysis
+
+import com.android.tools.adtui.model.FakeTimer
+import com.android.tools.adtui.model.Range
+import com.android.tools.adtui.model.filter.Filter
+import com.android.tools.idea.transport.faketransport.FakeGrpcChannel
+import com.android.tools.idea.transport.faketransport.FakeTransportService
+import com.android.tools.profilers.FakeIdeProfilerComponents
+import com.android.tools.profilers.FakeIdeProfilerServices
+import com.android.tools.profilers.FakeProfilerService
+import com.android.tools.profilers.ProfilerClient
+import com.android.tools.profilers.StudioProfilers
+import com.android.tools.profilers.StudioProfilersView
+import com.android.tools.profilers.cpu.CaptureNode
+import com.android.tools.profilers.cpu.CpuCapture
+import com.android.tools.profilers.cpu.CpuProfilerUITestUtils
+import com.android.tools.profilers.cpu.FakeCpuService
+import com.google.common.truth.Truth.assertThat
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+class CpuAnalysisChartTest {
+  private val timer = FakeTimer()
+
+  @get:Rule
+  val grpcChannel = FakeGrpcChannel("CPuAnalysisChartTest", FakeCpuService(), FakeTransportService(timer), FakeProfilerService(timer))
+  private lateinit var profilersView: StudioProfilersView
+
+  @Before
+  fun setUp() {
+    val profilers = StudioProfilers(ProfilerClient(grpcChannel.name), FakeIdeProfilerServices(), timer)
+    profilersView = StudioProfilersView(profilers, FakeIdeProfilerComponents())
+
+  }
+
+  @Test
+  fun filterIsApplied() {
+    val capture = CpuProfilerUITestUtils.validCapture()
+    val model = CpuAnalysisChartModel<CpuCapture>(CpuAnalysisTabModel.Type.TOP_DOWN, Range(capture.range), capture) { capture.captureNodes }
+    model.dataSeries.add(capture)
+    val chart = CpuAnalysisChart(profilersView, model)
+
+    // Apply text filter "main". The main thread node should be an exact match.
+    chart.filterComponent.model.filter = Filter("main")
+    assertThat(capture.captureNodes.filter { node -> node.filterType == CaptureNode.FilterType.EXACT_MATCH }).isNotEmpty()
+  }
+}
