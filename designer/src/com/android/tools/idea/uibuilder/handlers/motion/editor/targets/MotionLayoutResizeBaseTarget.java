b8b9f3a6d3cd983e787678c8714e25bdb43105f1
Nicolas Roard <nro@google.com>
2020-01-14 21:32:05 -0800

    Fix incorrect attribute loading in base layout
    Fixes: 147437931, 147562817, 147451555, 147765898
    Test: added targets/resize/drag tests
    
    Change-Id: I29b610ea298f86c99baf622b49b9704646a4fb81

--- designer/src/com/android/tools/idea/uibuilder/handlers/motion/editor/targets/MotionLayoutResizeBaseTarget.java
+++ designer/src/com/android/tools/idea/uibuilder/handlers/motion/editor/targets/MotionLayoutResizeBaseTarget.java
@@ -33,6 +33,7 @@ import com.android.tools.idea.uibuilder.handlers.motion.editor.adapters.MTag;
 import com.android.tools.idea.uibuilder.handlers.motion.editor.adapters.MotionSceneAttrs;
 import com.android.tools.idea.uibuilder.handlers.motion.editor.ui.Utils;
 import com.android.tools.idea.uibuilder.scene.draw.DrawResize;
+import com.android.tools.idea.uibuilder.scene.target.ResizeBaseTarget;
 import com.intellij.openapi.util.text.StringUtil;
 import java.awt.Cursor;
 import java.util.List;
@@ -45,162 +46,17 @@ import org.jetbrains.annotations.Nullable;
  *
  * TODO: refactor with ResizeBaseTarget
  */
-public abstract class MotionLayoutResizeBaseTarget extends BaseTarget {
-
-  protected final Type myType;
-
-  @AndroidDpCoordinate protected int myStartX1;
-  @AndroidDpCoordinate protected int myStartY1;
-  @AndroidDpCoordinate protected int myStartX2;
-  @AndroidDpCoordinate protected int myStartY2;
-
-  public Type getType() {
-    return myType;
-  }
-
-  // Type of possible resize handles
-  public enum Type {
-    LEFT, LEFT_TOP, LEFT_BOTTOM, TOP, BOTTOM, RIGHT, RIGHT_TOP, RIGHT_BOTTOM
-  }
+public abstract class MotionLayoutResizeBaseTarget extends ResizeBaseTarget {
 
   /////////////////////////////////////////////////////////////////////////////
   //region Constructor
   /////////////////////////////////////////////////////////////////////////////
 
   public MotionLayoutResizeBaseTarget(@NotNull Type type) {
-    myType = type;
+    super(type);
   }
 
   //endregion
-  /////////////////////////////////////////////////////////////////////////////
-  //region Layout
-  /////////////////////////////////////////////////////////////////////////////
-
-  @Override
-  public boolean layout(@NotNull SceneContext sceneTransform,
-                        @AndroidDpCoordinate int l,
-                        @AndroidDpCoordinate int t,
-                        @AndroidDpCoordinate int r,
-                        @AndroidDpCoordinate int b) {
-    int width = r - l;
-    int height = b - t;
-    int horizontalCenter = l + width / 2;
-    int verticalCenter = t + height / 2;
-    switch (myType) {
-      case LEFT: {
-        myLeft = l;
-        myTop = verticalCenter;
-      }
-      break;
-      case TOP: {
-        myLeft = horizontalCenter;
-        myTop = t;
-      }
-      break;
-      case RIGHT: {
-        myLeft = r;
-        myTop = verticalCenter;
-      }
-      break;
-      case BOTTOM: {
-        myLeft = horizontalCenter;
-        myTop = b;
-      }
-      break;
-      case LEFT_TOP: {
-        myLeft = l;
-        myTop = t;
-      }
-      break;
-      case LEFT_BOTTOM: {
-        myLeft = l;
-        myTop = b;
-      }
-      break;
-      case RIGHT_TOP: {
-        myLeft = r;
-        myTop = t;
-      }
-      break;
-      case RIGHT_BOTTOM: {
-        myLeft = r;
-        myTop = b;
-      }
-      break;
-    }
-    myRight = myLeft;
-    myBottom = myTop;
-    return false;
-  }
-
-  //endregion
-  /////////////////////////////////////////////////////////////////////////////
-  //region Display
-  /////////////////////////////////////////////////////////////////////////////
-
-  @Override
-  public Cursor getMouseCursor(@JdkConstants.InputEventMask int modifiersEx) {
-    switch (myType) {
-      case LEFT:
-        return Cursor.getPredefinedCursor(Cursor.W_RESIZE_CURSOR);
-      case RIGHT:
-        return Cursor.getPredefinedCursor(Cursor.E_RESIZE_CURSOR);
-      case TOP:
-        return Cursor.getPredefinedCursor(Cursor.N_RESIZE_CURSOR);
-      case BOTTOM:
-        return Cursor.getPredefinedCursor(Cursor.S_RESIZE_CURSOR);
-      case LEFT_TOP:
-        return Cursor.getPredefinedCursor(Cursor.NW_RESIZE_CURSOR);
-      case LEFT_BOTTOM:
-        return Cursor.getPredefinedCursor(Cursor.SW_RESIZE_CURSOR);
-      case RIGHT_TOP:
-        return Cursor.getPredefinedCursor(Cursor.NE_RESIZE_CURSOR);
-      case RIGHT_BOTTOM:
-        return Cursor.getPredefinedCursor(Cursor.SE_RESIZE_CURSOR);
-      default:
-        return Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR);
-    }
-  }
-
-  @Override
-  public void render(@NotNull DisplayList list, @NotNull SceneContext sceneContext) {
-    if (isHittable()) {
-      DrawResize.add(list, sceneContext, myLeft, myTop, mIsOver ? DrawResize.OVER : DrawResize.NORMAL);
-    }
-  }
-
-  @Override
-  public void addHit(@NotNull SceneContext transform,
-                     @NotNull ScenePicker picker,
-                     @JdkConstants.InputEventMask int modifiersEx) {
-    if (isHittable()) {
-      int halfSize = DrawResize.SIZE / 2;
-      picker.addRect(this, 0, transform.getSwingXDip(myLeft) - halfSize,
-                     transform.getSwingYDip(myTop) - halfSize,
-                     transform.getSwingXDip(myRight) + halfSize,
-                     transform.getSwingYDip(myBottom) + halfSize);
-    }
-  }
-
-  @Override
-  protected boolean isHittable() {
-    SceneComponent component = getComponent();
-    if (component.getScene().getSelection().size() > 1) {
-      // Disable resize target when selecting multiple components.
-      return false;
-    }
-    if (component.isSelected()) {
-      if (component.canShowBaseline()) {
-        return true;
-      }
-      return !component.isDragging();
-    }
-    Scene.FilterType filterType = component.getScene().getFilterType();
-    if (filterType == Scene.FilterType.RESIZE || filterType == Scene.FilterType.ALL) {
-      return true;
-    }
-    return false;
-  }
 
   protected abstract void updateAttributes(@NotNull NlAttributesHolder attributes,
                                            @AndroidDpCoordinate int x,
@@ -210,36 +66,9 @@ public abstract class MotionLayoutResizeBaseTarget extends BaseTarget {
                                            @AndroidDpCoordinate int x,
                                            @AndroidDpCoordinate int y);
 
-  //endregion
   /////////////////////////////////////////////////////////////////////////////
   //region Mouse Handling
   /////////////////////////////////////////////////////////////////////////////
-
-  @Override
-  public int getPreferenceLevel() {
-    return Target.RESIZE_LEVEL;
-  }
-
-  class Attributes implements NlAttributesHolder {
-    @Override
-    public void setAttribute(@Nullable String namespace, @NotNull String attribute, @Nullable String value) {
-
-    }
-
-    @Nullable
-    @Override
-    public String getAttribute(@Nullable String namespace, @NotNull String attribute) {
-      return null;
-    }
-  }
-
-  @Override
-  public void mouseDown(@AndroidDpCoordinate int x, @AndroidDpCoordinate int y) {
-    myStartX1 = myComponent.getDrawX();
-    myStartY1 = myComponent.getDrawY();
-    myStartX2 = myComponent.getDrawX() + myComponent.getDrawWidth();
-    myStartY2 = myComponent.getDrawY() + myComponent.getDrawHeight();
-  }
 
   @Override
   public void mouseDrag(@AndroidDpCoordinate int x, @AndroidDpCoordinate int y, @NotNull List<Target> closestTargets) {
@@ -257,13 +86,13 @@ public abstract class MotionLayoutResizeBaseTarget extends BaseTarget {
   public void mouseRelease(@AndroidDpCoordinate int x, @AndroidDpCoordinate int y, @NotNull List<Target> closestTargets) {
     MotionLayoutComponentHelper motionLayout = MotionLayoutComponentHelper.create(myComponent.getNlComponent().getParent());
     if (!motionLayout.isInTransition()) {
-      String state = motionLayout.getState();
-      if (state == null) {
+      if (MotionUtils.isInBaseState(motionLayout)) {
         NlComponent component = myComponent.getAuthoritativeNlComponent();
         ComponentModification modification = new ComponentModification(component, "Resize " + StringUtil.getShortName(component.getTagName()));
         updateAttributes(modification, x, y);
         modification.commit();
       } else {
+        String state = motionLayout.getState();
         MTag motionScene = MotionUtils.getMotionScene(myComponent.getNlComponent());
         MTag[] cSet = motionScene.getChildTags("ConstraintSet");
         for (int i = 0; i < cSet.length; i++) {
@@ -307,10 +136,6 @@ public abstract class MotionLayoutResizeBaseTarget extends BaseTarget {
     myComponent.getScene().markNeedsLayout(Scene.IMMEDIATE_LAYOUT);
   }
 
-  @Override
-  public String getToolTipText() {
-    return "Resize View";
-  }
   //endregion
   /////////////////////////////////////////////////////////////////////////////
 }
---
+++ designer/testSrc/com/android/tools/idea/uibuilder/handlers/motion/MotionBasicConnectionsTest.java
@@ -0,0 +1,176 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.uibuilder.handlers.motion;
+
+import static com.android.SdkConstants.CONSTRAINT_LAYOUT;
+import static com.android.SdkConstants.MOTION_LAYOUT;
+import static com.android.SdkConstants.TEXT_VIEW;
+import static com.google.common.truth.Truth.assertThat;
+
+import com.android.tools.idea.common.fixtures.ModelBuilder;
+import com.android.tools.idea.common.model.NlComponent;
+import com.android.tools.idea.common.scene.SceneComponent;
+import com.android.tools.idea.common.scene.target.AnchorTarget;
+import com.android.tools.idea.uibuilder.api.actions.ViewAction;
+import com.android.tools.idea.uibuilder.handlers.constraint.targets.BaseLineToggleViewAction;
+import com.android.tools.idea.uibuilder.scene.SceneTest;
+import java.util.function.Predicate;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Test basic connection interactions
+ */
+public class MotionBasicConnectionsTest extends SceneTest {
+
+  private static final Predicate<ViewAction> BASELINE_ACTION_SELECTOR = target -> target instanceof BaseLineToggleViewAction;
+
+  public void testConnectLeft() {
+    myInteraction.select("button", true);
+    myInteraction.mouseDown("button", AnchorTarget.Type.LEFT);
+    myInteraction.mouseRelease("root", AnchorTarget.Type.LEFT);
+    myScreen.get("@id/button")
+      .expectXml("<TextView\n" +
+                 "        android:id=\"@id/button\"\n" +
+                 "        android:layout_width=\"100dp\"\n" +
+                 "        android:layout_height=\"20dp\"\n" +
+                 "        app:layout_constraintStart_toStartOf=\"parent\"\n" +
+                 "        tools:layout_editor_absoluteY=\"200dp\" />");
+  }
+
+  public void testConnectTop() {
+    myInteraction.select("button", true);
+    NlComponent button = myScene.getSceneComponent("button").getNlComponent();
+    assertEquals(1, myScreen.getScreen().getSelectionModel().getSelection().size());
+    assertEquals(button, myScreen.getScreen().getSelectionModel().getPrimary());
+    myInteraction.mouseDown("button", AnchorTarget.Type.TOP);
+    myInteraction.mouseRelease("root", AnchorTarget.Type.TOP);
+    myScreen.get("@id/button")
+      .expectXml("<TextView\n" +
+                 "        android:id=\"@id/button\"\n" +
+                 "        android:layout_width=\"100dp\"\n" +
+                 "        android:layout_height=\"20dp\"\n" +
+                 "        app:layout_constraintTop_toTopOf=\"parent\"\n" +
+                 "        tools:layout_editor_absoluteX=\"100dp\" />");
+    assertEquals(button, myScreen.getScreen().getSelectionModel().getPrimary());
+    assertEquals(1, myScreen.getScreen().getSelectionModel().getSelection().size());
+  }
+
+  public void testConnectRight() {
+    myInteraction.select("button", true);
+    myInteraction.mouseDown("button", AnchorTarget.Type.RIGHT);
+    myInteraction.mouseRelease("root", AnchorTarget.Type.RIGHT);
+    myScreen.get("@id/button")
+      .expectXml("<TextView\n" +
+                 "        android:id=\"@id/button\"\n" +
+                 "        android:layout_width=\"100dp\"\n" +
+                 "        android:layout_height=\"20dp\"\n" +
+                 "        app:layout_constraintEnd_toEndOf=\"parent\"\n" +
+                 "        tools:layout_editor_absoluteY=\"200dp\" />");
+  }
+
+  public void testConnectBottom() {
+    myInteraction.select("button", true);
+    myInteraction.mouseDown("button", AnchorTarget.Type.BOTTOM);
+    myInteraction.mouseRelease("root", AnchorTarget.Type.BOTTOM);
+    myScreen.get("@id/button")
+      .expectXml("<TextView\n" +
+                 "        android:id=\"@id/button\"\n" +
+                 "        android:layout_width=\"100dp\"\n" +
+                 "        android:layout_height=\"20dp\"\n" +
+                 "        app:layout_constraintBottom_toBottomOf=\"parent\"\n" +
+                 "        tools:layout_editor_absoluteX=\"100dp\" />");
+  }
+
+  public void testConnectBaseline() {
+    myInteraction.select("button2", true);
+    myInteraction.performViewAction("button2", BASELINE_ACTION_SELECTOR);
+    myInteraction.mouseDown("button2", AnchorTarget.Type.BASELINE);
+    myInteraction.mouseRelease("button", AnchorTarget.Type.BASELINE);
+    myScreen.get("@id/button2")
+      .expectXml("<TextView\n" +
+                 "        android:id=\"@id/button2\"\n" +
+                 "        android:layout_width=\"100dp\"\n" +
+                 "        android:layout_height=\"20dp\"\n" +
+                 "        app:layout_constraintBaseline_toBaselineOf=\"@+id/button\"\n" +
+                 "        tools:layout_editor_absoluteX=\"300dp\" />");
+    myInteraction.select("button2", false);
+    myInteraction.select("button2", true);
+    myInteraction.mouseDown("button2", AnchorTarget.Type.LEFT);
+    myInteraction.mouseRelease("button", AnchorTarget.Type.RIGHT);
+    myScreen.get("@id/button2")
+      .expectXml("<TextView\n" +
+                 "        android:id=\"@id/button2\"\n" +
+                 "        android:layout_width=\"100dp\"\n" +
+                 "        android:layout_height=\"20dp\"\n" +
+                 "        app:layout_constraintBaseline_toBaselineOf=\"@+id/button\"\n" +
+                 "        app:layout_constraintStart_toEndOf=\"@+id/button\" />");
+  }
+
+  public void testConnectBaselineWithNoId() {
+    SceneComponent noIdComponent = null;
+    for (SceneComponent component : myScene.getSceneComponents()) {
+      if (component.getId() == null) {
+        noIdComponent = component;
+      }
+    }
+    assertThat(noIdComponent).isNotNull();
+    myInteraction.select(noIdComponent, true);
+    myInteraction.performViewAction(noIdComponent, BASELINE_ACTION_SELECTOR);
+    myInteraction.mouseDown(noIdComponent, AnchorTarget.Type.BASELINE);
+    myInteraction.mouseRelease("button", AnchorTarget.Type.BASELINE);
+    assertThat(noIdComponent.getNlComponent().getTagDeprecated().getText())
+      .isEqualTo("<TextView\n" +
+                 "        android:layout_width=\"100dp\"\n" +
+                 "        android:layout_height=\"20dp\"\n" +
+                 "        app:layout_constraintBaseline_toBaselineOf=\"@+id/button\"\n" +
+                 "        tools:layout_editor_absoluteX=\"500dp\" />");
+  }
+
+  @Override
+  @NotNull
+  public ModelBuilder createModel() {
+    ModelBuilder builder = model("constraint.xml",
+                                 component(MOTION_LAYOUT.defaultName())
+                                   .id("@id/root")
+                                   .withBounds(0, 0, 1000, 1000)
+                                   .width("1000dp")
+                                   .height("1000dp")
+                                   .withAttribute("android:padding", "20dp")
+                                   .children(
+                                     component(TEXT_VIEW)
+                                       .id("@id/button")
+                                       .withBounds(100, 200, 100, 20)
+                                       .width("100dp")
+                                       .height("20dp")
+                                       .withAttribute("tools:layout_editor_absoluteX", "100dp")
+                                       .withAttribute("tools:layout_editor_absoluteY", "200dp"),
+                                     component(TEXT_VIEW)
+                                       .id("@id/button2")
+                                       .withBounds(300, 200, 100, 20)
+                                       .width("100dp")
+                                       .height("20dp")
+                                       .withAttribute("tools:layout_editor_absoluteX", "300dp")
+                                       .withAttribute("tools:layout_editor_absoluteY", "200dp"),
+                                     component(TEXT_VIEW)
+                                       .withBounds(300, 200, 100, 20)
+                                       .width("100dp")
+                                       .height("20dp")
+                                       .withAttribute("tools:layout_editor_absoluteX", "500dp")
+                                       .withAttribute("tools:layout_editor_absoluteY", "200dp")
+                                   ));
+    return builder;
+  }
+}
