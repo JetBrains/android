cbf2705c6fde8add17766946bcf4a996a0064227
kunshen <kunshen@google.com>
2020-01-17 13:43:49 -0800

    Query package name from index for default module system
    Bug: 132248837
    Test: n/a
    Change-Id: I314400ebc38436912f79da162d85310df101b8ff

---
+++ android/testSrc/com/android/tools/idea/project/DefaultModuleSystemTest.kt
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.project
+
+import com.android.tools.idea.model.MergedManifestModificationListener
+import com.android.tools.idea.projectsystem.getModuleSystem
+import com.google.common.truth.Truth
+import org.jetbrains.android.AndroidTestCase
+
+class DefaultModuleSystemTest : AndroidTestCase() {
+  override fun setUp() {
+    super.setUp()
+    MergedManifestModificationListener.ensureSubscribed(project)
+  }
+
+  fun testGetPackageName_noOverrides() {
+    val packageName = (myModule.getModuleSystem() as DefaultModuleSystem).getPackageName()
+    Truth.assertThat(packageName).isEqualTo("p1.p2")
+  }
+}
