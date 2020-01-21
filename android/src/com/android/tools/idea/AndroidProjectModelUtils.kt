fe20dac0d79729024555df6ab47ac795e447ebb5
Sergey Prigogin <sprigogin@google.com>
2020-01-21 11:15:12 -0800

    Remove an unused method
    Test: existing
    Bug: N/A
    Change-Id: If5ad6103ea8c28e0835562a52480bfe3fa81ae62

--- android/src/com/android/tools/idea/AndroidProjectModelUtils.kt
+++ android/src/com/android/tools/idea/AndroidProjectModelUtils.kt
@@ -24,21 +24,15 @@
 
 package com.android.tools.idea
 
-import com.android.SdkConstants.ANDROID_MANIFEST_XML
-import com.android.SdkConstants.DOT_AAR
-import com.android.SdkConstants.FD_JARS
-import com.android.SdkConstants.LIBS_FOLDER
 import com.android.builder.model.AaptOptions
 import com.android.projectmodel.AndroidSubmodule
 import com.android.projectmodel.ExternalLibrary
 import com.android.tools.idea.model.AndroidModel
-import com.android.tools.idea.projectsystem.FilenameConstants
 import com.android.tools.idea.projectsystem.getModuleSystem
 import com.intellij.openapi.module.Module
 import com.intellij.openapi.module.ModuleManager
 import com.intellij.openapi.project.Project
 import org.jetbrains.android.facet.AndroidFacet
-import java.io.File
 
 /**
  * Returns information about all [ExternalLibrary] dependencies that contribute resources in the project, indexed by
@@ -67,51 +61,6 @@ fun findDependenciesWithResources(module: Module): Map<String, ExternalLibrary> 
     .filterIsInstance<ExternalLibrary>()
     .filter { it.hasResources }
     .associateBy { library -> library.address }
-}
-
-/**
- * Tries to find the resources folder corresponding to a given `classes.jar` file extracted from an AAR.
- *
- * TODO: make it private and part of building the model for legacy projects where guessing is the best we can do.
- */
-@Deprecated("Use AndroidProjectModelUtils.findAllLibrariesWithResources instead of processing jar files and looking for resources.")
-fun findResFolder(jarFile: File): File? {
-  // We need to figure out the layout of the resources relative to the jar file. This changed over time, so we check for different
-  // layouts until we find one we recognize.
-  var resourcesDirectory: File? = null
-
-  var aarDir = jarFile.parentFile
-  if (aarDir.path.endsWith(DOT_AAR) || aarDir.path.contains(FilenameConstants.EXPLODED_AAR)) {
-    if (aarDir.path.contains(FilenameConstants.EXPLODED_AAR)) {
-      if (aarDir.path.endsWith(LIBS_FOLDER)) {
-        // Some libraries recently started packaging jars inside a sub libs folder inside jars
-        aarDir = aarDir.parentFile
-      }
-      // Gradle plugin version 1.2.x and later has classes in aar-dir/jars/
-      if (aarDir.path.endsWith(FD_JARS)) {
-        aarDir = aarDir.parentFile
-      }
-    }
-    val path = aarDir.path
-    if (path.endsWith(DOT_AAR) || path.contains(FilenameConstants.EXPLODED_AAR)) {
-      resourcesDirectory = aarDir
-    }
-  }
-
-  if (resourcesDirectory == null) {
-    // Build cache? We need to compute the package name in a slightly different way.
-    val parentFile = aarDir.parentFile
-    if (parentFile != null) {
-      val manifest = File(parentFile, ANDROID_MANIFEST_XML)
-      if (manifest.exists()) {
-        resourcesDirectory = parentFile
-      }
-    }
-    if (resourcesDirectory == null) {
-      return null
-    }
-  }
-  return resourcesDirectory
 }
 
 /**
