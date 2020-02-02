e055e8551a4692a3b735a71332e5c74a94021296
Dmitry.Krasilschikov <kradima@jetbrains.com>
2020-02-02 15:06:50 +0200

    API for lazy localized messages
    Use `XBundle.lazyMessage()` API a string to be changed to its localized variant on a language plugin enabling w/o restart
    
    `public static Supplier<String> lazyMessage() {...}` added into many bundle classes
    
    GitOrigin-RevId: 634d1673bd3af5b35452702bea9637822e209bfc

--- layoutlib-loader/src/com/android/tools/idea/layoutlib/LayoutlibBundle.java
+++ layoutlib-loader/src/com/android/tools/idea/layoutlib/LayoutlibBundle.java
@@ -19,6 +19,7 @@ import com.intellij.DynamicBundle;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.PropertyKey;
+import java.util.function.Supplier;
 
 public class LayoutlibBundle extends DynamicBundle {
   @NonNls private static final String BUNDLE = "messages.LayoutlibBundle";
@@ -29,5 +30,10 @@ public class LayoutlibBundle extends DynamicBundle {
   @NotNull
   public static String message(@NotNull @PropertyKey(resourceBundle = BUNDLE) String key, @NotNull Object... params) {
     return INSTANCE.getMessage(key, params);
+  }
+
+  @NotNull
+  public static Supplier<String> lazyMessage(@NotNull @PropertyKey(resourceBundle = BUNDLE) String key, @NotNull Object... params) {
+    return INSTANCE.getLazyMessage(key, params);
   }
 }
