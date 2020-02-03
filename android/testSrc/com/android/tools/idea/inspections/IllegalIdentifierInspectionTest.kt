3b9e7cfa724e70bb957051dfd4b2ac70dd733678
Christophe Rhodes <xof@google.com>
2020-02-03 16:07:16 +0000

    Test for IllegalIdentifierInspection
    A simple minimal test that an identifier with spaces is correctly
    detected in a Kotlin file
    
    Bug: none
    Test: included
    Change-Id: Id0e0d0af03a71be221c8ba37a326699623a5b7e5

---
+++ android/testSrc/com/android/tools/idea/inspections/IllegalIdentifierInspectionTest.kt
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.inspections
+
+import com.android.tools.idea.testing.AndroidGradleTestCase
+import com.android.tools.idea.testing.TestProjectPaths.TEST_ARTIFACTS_ILLEGAL_IDENTIFIERS
+import com.google.common.truth.Truth.assertThat
+import com.intellij.lang.annotation.HighlightSeverity
+import com.intellij.openapi.project.guessProjectDir
+import org.jetbrains.kotlin.android.inspection.IllegalIdentifierInspection
+
+class IllegalIdentifierInspectionTest : AndroidGradleTestCase() {
+  override fun setUp() {
+    super.setUp()
+    loadProject(TEST_ARTIFACTS_ILLEGAL_IDENTIFIERS)
+    myFixture.enableInspections(IllegalIdentifierInspection::class.java)
+  }
+
+  fun testInspectionInRegularFile() {
+    val file = project.guessProjectDir()!!
+      .findFileByRelativePath("app/src/main/java/com/google/studio/android/test/Test.kt")!!
+    myFixture.openFileInEditor(file)
+    val highlightInfo = myFixture.doHighlighting(HighlightSeverity.ERROR)
+    assertThat(highlightInfo).isNotEmpty()
+    assertThat(highlightInfo.any { it.description == "Identifier not allowed in Android projects" }).isTrue()
+  }
+}
