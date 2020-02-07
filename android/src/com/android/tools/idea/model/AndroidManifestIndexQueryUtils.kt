49d6d9a9823d096218d9e96c39272222a40e5e2e
Kun Shen <kunshen@google.com>
2020-02-07 15:51:56 +0000

    Revert "Query services from Android Manifest Index"
    This reverts commit 3d84c2deef360591f025aa07a8c163d4eb3954d7.
    
    Reason for revert: it fails in CreateNewProjectWithNoActivityTest
    and other ui tests involve creating template projects
    
    Change-Id: Iba9f99569e5292ff83a8acfabbd9457c4540e1fd

--- android/src/com/android/tools/idea/model/AndroidManifestIndexQueryUtils.kt
+++ android/src/com/android/tools/idea/model/AndroidManifestIndexQueryUtils.kt
@@ -206,18 +206,6 @@ fun AndroidFacet.queryPackageNameFromManifestIndex(): String? {
 }
 
 /**
- * Returns the union set of services, instead of merged results with merging rules applied.
- *
- * Must be called in a smart read action.
- */
-fun AndroidFacet.queryServicesFromManifestIndex() = queryManifestIndex { overrides, contributors ->
-  contributors.asSequence()
-    .flatMap { it.services.asSequence()}
-    .mapNotNull { ServiceWrapper(it.intentFilters, overrides) }
-    .toSet()
-}
-
-/**
  * To track in crash analytics when EAP
  */
 fun logManifestIndexQueryError(e: Exception) {
