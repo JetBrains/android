4dc0a45cd33a1f60004755cff65ad51e6d72da9e
Charlie Tsai <chartsai@google.com>
2020-02-10 15:45:46 +0000

    Add Android Design Tools section in keymap setting
    Also migrate "Force Refresh" and "ToggleDeviceOrientation" actions
    into keymap setting.
    
    Bug: 149212539
    Test: Add DesignerKeymapExtensionTest
    Change-Id: Ia66664a2a5c7d3376302de1f36428437391da86e

---
+++ designer/src/com/android/tools/idea/actions/DesignerActions.kt
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.actions
+
+/**
+ * List all the Action Ids for Android Designer Tools.
+ * The registered Actions in designer/src/META-INF/designer.xml should use the Ids listed in this class.
+ *
+ * The action system doesn't allow ID duplications, so all the below Group and Action Ids should have "Android.Designer." prefix.
+ */
+object DesignerActions {
+  const val GROUP_COMMON = "Android.Designer.CommonActions"
+  const val GROUP_TOOLS = "Android.Designer.ToolsActions"
+  const val GROUP_LAYOUT_EDITOR = "Android.Designer.LayoutEditorActions"
+
+  //<editor-fold desc="Common Actions">
+  const val ACTION_FORCE_REFRESH_PREVIEW = "Android.Designer.ForceRefreshPreview"
+  //</editor-fold>
+
+  //<editor-fold desc="Layout Editor Actions">
+  const val ACTION_TOGGLE_DEVICE_ORIENTATION = "Android.Designer.ToggleDeviceOrientation"
+  //</editor-fold>
+}
