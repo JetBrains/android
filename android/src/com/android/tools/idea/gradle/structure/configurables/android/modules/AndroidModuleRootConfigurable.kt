8527b43d5c2aeac29b707c7ecf79f3464d605b6a
Christophe Rhodes <xof@google.com>
2020-02-10 09:40:59 +0000

    [PSD] allow setting dependenciesInfo.includeInApk
    Conditionalizing when to display the property combo box does not quite
    work as expected: the configurables are not refreshed when the plugin
    version (or indeed anything else) is changed, so a change from
    e.g. 3.5.3 to 4.0.0-dev would need the user to exit the PSD and
    re-open it to see the includeInApk combo box in the Modules
    configurable.
    
    Bug: 145728802
    Test: manual
    Change-Id: If9e7163b4381f26870453cb6dffd607041b9f7c9

--- android/src/com/android/tools/idea/gradle/structure/configurables/android/modules/AndroidModuleRootConfigurable.kt
+++ android/src/com/android/tools/idea/gradle/structure/configurables/android/modules/AndroidModuleRootConfigurable.kt
@@ -15,6 +15,7 @@
  */
 package com.android.tools.idea.gradle.structure.configurables.android.modules
 
+import com.android.ide.common.repository.GradleVersion
 import com.android.tools.idea.gradle.structure.configurables.BasePerspectiveConfigurable
 import com.android.tools.idea.gradle.structure.configurables.PsContext
 import com.android.tools.idea.gradle.structure.configurables.createTreeModel
@@ -24,9 +25,11 @@ import com.android.tools.idea.gradle.structure.configurables.ui.mapPropertyEdito
 import com.android.tools.idea.gradle.structure.configurables.ui.modules.ModulePanel
 import com.android.tools.idea.gradle.structure.configurables.ui.simplePropertyEditor
 import com.android.tools.idea.gradle.structure.configurables.ui.uiProperty
+import com.android.tools.idea.gradle.structure.model.PsModuleType
 import com.android.tools.idea.gradle.structure.model.android.AndroidModuleDescriptors
 import com.android.tools.idea.gradle.structure.model.android.PsAndroidModule
 import com.android.tools.idea.gradle.structure.model.android.PsAndroidModuleDefaultConfigDescriptors
+import com.android.tools.idea.gradle.structure.model.meta.maybeValue
 import com.google.wireless.android.sdk.stats.PSDEvent
 import com.intellij.openapi.Disposable
 import com.intellij.openapi.util.Disposer
@@ -45,9 +48,15 @@ class AndroidModuleRootConfigurable(
   override fun dispose() = Unit
 }
 
-fun androidModulePropertiesModel() =
+// TODO(b/142099752): this could be a general mechanism attached to the descriptors
+private fun includeDependenciesInfoInApkPresent(context: PsContext, module: PsAndroidModule) =
+  module.projectType == PsModuleType.ANDROID_APP &&
+  context.project.androidGradlePluginVersion.maybeValue
+    ?.let { GradleVersion.tryParse(it) }?.isAtLeastIncludingPreviews(4, 0, 0) ?: false
+
+fun androidModulePropertiesModel(context: PsContext, module: PsAndroidModule) =
   PropertiesUiModel(
-    listOf(
+    listOfNotNull(
       uiProperty(AndroidModuleDescriptors.compileSdkVersion, ::simplePropertyEditor,
                  PSDEvent.PSDField.PROJECT_STRUCTURE_DIALOG_FIELD_MODULE_PROPERTIES_COMPILE_SDK_VERSION),
       uiProperty(AndroidModuleDescriptors.buildToolsVersion, ::simplePropertyEditor,
@@ -55,11 +64,18 @@ fun androidModulePropertiesModel() =
       uiProperty(AndroidModuleDescriptors.sourceCompatibility, ::simplePropertyEditor,
                  PSDEvent.PSDField.PROJECT_STRUCTURE_DIALOG_FIELD_MODULE_PROPERTIES_SOURCE_COMPATIBILITY),
       uiProperty(AndroidModuleDescriptors.targetCompatibility, ::simplePropertyEditor,
-                 PSDEvent.PSDField.PROJECT_STRUCTURE_DIALOG_FIELD_MODULE_PROPERTIES_TARGET_COMPATIBILITY)
+                 PSDEvent.PSDField.PROJECT_STRUCTURE_DIALOG_FIELD_MODULE_PROPERTIES_TARGET_COMPATIBILITY),
+      when (includeDependenciesInfoInApkPresent(context, module)) {
+        true -> uiProperty(AndroidModuleDescriptors.includeDependenciesInfoInApk, ::simplePropertyEditor, null)
+        false -> null
+      }
       // TODO(b/142099752): Properly configure condition when it is available and enable.
       /*,
       uiProperty(AndroidModuleDescriptors.viewBindingEnabled, ::simplePropertyEditor,
-                 null)*/))
+                 null)*/
+    )
+  )
+
 
 fun defaultConfigPropertiesModel(isLibrary: Boolean) =
   PropertiesUiModel(
