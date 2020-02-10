e90695d7da4f6aec200c21459f221d8a62c373cf
Tor Norbye <tnorbye@google.com>
2020-02-10 07:18:22 -0800

    149203366 Fix Crash report: AssertionError at LintIdeClient.report-39094cea
    The root cause was that there are cases where for example the
    layout editor reuses lint infrastructure (in particular, ApiLookup)
    which requires for example a LintClient and a LintResult. However,
    the default LintResult class does not expect to receive error messages,
    which can happen if for example the ApiLookup cannot find the
    configured SDK Home (perhaps because the user has not configured it).
    
    This CL makes this more explicit. Instead of just removing the
    assertion, I switched the result classes to a sealed class hierarchy
    and created an explicit ignored result class.
    
    Test: Existing
    Fixes: 149203366
    Change-Id: Ib64f47869e440196a4758d0c8fedbc45f9405d74

--- android/testSrc/org/jetbrains/android/AndroidLintTest.java
+++ android/testSrc/org/jetbrains/android/AndroidLintTest.java
@@ -119,9 +119,9 @@ import com.android.tools.idea.lint.AndroidLintWrongCaseInspection;
 import com.android.tools.idea.lint.AndroidLintWrongViewCastInspection;
 import com.android.tools.idea.lint.common.AndroidLintGradleDynamicVersionInspection;
 import com.android.tools.idea.lint.common.AndroidLintInspectionBase;
+import com.android.tools.idea.lint.common.LintIgnoredResult;
 import com.android.tools.idea.lint.common.LintExternalAnnotator;
 import com.android.tools.idea.lint.common.LintIdeIssueRegistry;
-import com.android.tools.idea.lint.common.LintResult;
 import com.android.tools.idea.lint.common.SuppressLintIntentionAction;
 import com.android.tools.idea.projectsystem.GoogleMavenArtifactId;
 import com.android.tools.idea.projectsystem.TestProjectSystem;
@@ -837,7 +837,7 @@ public class AndroidLintTest extends AndroidTestCase {
 
   public void testOldTargetApi() throws Exception {
     deleteManifest();
-    String expectedTarget = Integer.toString(new AndroidLintIdeClient(getProject(), new LintResult()).getHighestKnownApiLevel());
+    String expectedTarget = Integer.toString(new AndroidLintIdeClient(getProject(), new LintIgnoredResult()).getHighestKnownApiLevel());
     doTestWithFix(new AndroidLintOldTargetApiInspection(),
                   "Update targetSdkVersion to " + expectedTarget, "AndroidManifest.xml", "xml");
   }
