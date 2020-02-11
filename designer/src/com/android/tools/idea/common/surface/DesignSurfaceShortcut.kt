5acaa1c44c0a25866b3e23c73f2780e8bdae6451
Charlie Tsai <chartsai@google.com>
2020-02-11 16:17:08 +0000

    Migrate toggle issue panel action to designer shortcut
    To make this action searchable and its shortcut configurable.
    
    Bug: 149212539
    Test: Update DesignerKeymapExtensionTest
    Change-Id: I9bf431286b66684c9d530f3e29eff03e84aaaf8f

--- designer/src/com/android/tools/idea/common/surface/DesignSurfaceShortcut.kt
+++ designer/src/com/android/tools/idea/common/surface/DesignSurfaceShortcut.kt
@@ -37,7 +37,6 @@ import javax.swing.KeyStroke
  * @See KeyEvent
  */
 enum class DesignSurfaceShortcut(val keyCode: Int, private val keyChar: Char? = null) {
-  TOGGLE_ISSUE_PANEL(KeyEvent.VK_E),
   NEXT_DEVICE(KeyEvent.VK_D),
   DESIGN_MODE(KeyEvent.VK_B),
 
