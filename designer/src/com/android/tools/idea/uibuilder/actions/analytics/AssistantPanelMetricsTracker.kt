21ccd3650a3a7984ae2d991337f63a873c918897
Isaac Chai <ichai@google.com>
2020-02-13 10:23:25 -0800

    Adding metrics to Help Assistant layout editor
    As title. Also added unit tests
    
    Bug: 146177091
    Test: Tested manually + through unit test
    Change-Id: I520379d048651a9dfc5d2b556247cbbb46d6747a

---
+++ designer/src/com/android/tools/idea/uibuilder/actions/analytics/AssistantPanelMetricsTracker.kt
@@ -0,0 +1,85 @@
+/*
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.uibuilder.actions.analytics
+
+import com.android.tools.analytics.UsageTracker.log
+import com.android.tools.idea.uibuilder.actions.LayoutEditorHelpAssistantAction
+import com.google.common.annotations.VisibleForTesting
+import com.google.common.base.Stopwatch
+import com.google.wireless.android.sdk.stats.AndroidStudioEvent
+import com.google.wireless.android.sdk.stats.DesignEditorHelpPanelEvent
+import com.google.wireless.android.sdk.stats.DesignEditorHelpPanelEvent.HelpPanelAction
+import com.google.wireless.android.sdk.stats.DesignEditorHelpPanelEvent.HelpPanelType
+
+open class AssistantPanelMetricsTracker(actionType: LayoutEditorHelpAssistantAction.Type) {
+
+  private val type = convertType(actionType)
+  @VisibleForTesting val timer: Stopwatch = Stopwatch.createUnstarted()
+
+  fun logOpen() {
+    if (timer.isRunning) {
+      timer.stop().reset()
+    }
+
+    val event = createEventBuilder(type, HelpPanelAction.OPEN)
+    timer.start()
+    logEvent(event)
+  }
+
+  fun logClose() {
+    if (timer.isRunning) {
+      timer.stop()
+    }
+
+    val event = createEventBuilder(type, HelpPanelAction.CLOSE)
+    event.timeToCloseMs = timer.elapsed().toMillis()
+    logEvent(event)
+  }
+
+  fun logButtonClicked() {
+    val event = createEventBuilder(type, HelpPanelAction.BUTTON_CLICKED)
+    logEvent(event)
+  }
+
+  fun logReachedEnd() {
+    val event = createEventBuilder(type, HelpPanelAction.REACHED_END)
+    logEvent(event)
+  }
+
+  @VisibleForTesting
+  open fun logEvent(event: DesignEditorHelpPanelEvent.Builder) {
+    log(AndroidStudioEvent.newBuilder()
+          .setKind(AndroidStudioEvent.EventKind.DESIGN_EDITOR_HELP_PANEL_EVENT)
+          .setDesignEditorHelpPanelEvent(event))
+  }
+
+  private fun createEventBuilder(type: HelpPanelType, action: HelpPanelAction): DesignEditorHelpPanelEvent.Builder {
+    val event: DesignEditorHelpPanelEvent.Builder = DesignEditorHelpPanelEvent.newBuilder()
+    event.helpPanelType = type
+    event.action = action
+    return event
+  }
+
+  private fun convertType(type: LayoutEditorHelpAssistantAction.Type): HelpPanelType {
+    return when(type) {
+      LayoutEditorHelpAssistantAction.Type.NONE -> HelpPanelType.UNKNOWN_PANEL_TYPE
+      LayoutEditorHelpAssistantAction.Type.CONSTRAINT_LAYOUT -> HelpPanelType.CONSTRAINT_LAYOUT
+      LayoutEditorHelpAssistantAction.Type.MOTION_LAYOUT -> HelpPanelType.MOTION_LAYOUT
+      LayoutEditorHelpAssistantAction.Type.FULL -> HelpPanelType.FULL_ALL
+    }
+  }
+
+}
