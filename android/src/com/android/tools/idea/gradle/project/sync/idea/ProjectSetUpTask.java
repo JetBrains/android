b447abe6f3a2773f92f1e4ab04c9f946d5199635
Bradley Smith <smithbradley@google.com>
2020-02-13 14:31:25 -0800

    [Gradle Sync] Ensure stack traces are preserved in the error messages
    Test: Unit test added
    Bug: N/A
    Change-Id: I67ed45f2cf5c02e2495e4a14b4c4d7a6f714dfe4

--- android/src/com/android/tools/idea/gradle/project/sync/idea/ProjectSetUpTask.java
+++ android/src/com/android/tools/idea/gradle/project/sync/idea/ProjectSetUpTask.java
@@ -118,7 +118,7 @@ class ProjectSetUpTask implements ExternalProjectRefreshCallback {
 
     String exceptionMessage =
       (errorDetails == null || errorMessage.contains(errorDetails)) ? errorMessage : errorMessage + "\n" + errorDetails;
-    String messageWithGuide = ExternalSystemBundle.message("error.resolve.with.reason", errorMessage);
+    String messageWithGuide = ExternalSystemBundle.message("error.resolve.with.reason", exceptionMessage);
     // Remove cache data to force a sync next time the project is open. This is necessary when checking MD5s is not enough. For example,
     // when sync failed because the SDK being used by the project was accidentally removed in the SDK Manager. The state of the project did
     // not change, and if we don't force a sync, the project will use the cached state and it would look like there are no errors.
