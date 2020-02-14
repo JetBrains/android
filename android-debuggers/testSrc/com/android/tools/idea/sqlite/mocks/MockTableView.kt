b16e2911ad024239fa7d33e7f3d17c6a9ee3e08c
Pierfrancesco Soffritti <psoffritti@google.com>
2020-02-14 20:00:14 +0000

    Hide table actions in evaluator panel.
    Do not show table actions (row above the table)
    in evaluator view.
    
    Test: SqliteEvaluatorViewImplTest.kt
    Bug: 148049132
    Change-Id: Ifbe9328e635fb93fc26e4cf2e320faef17a446c9

--- android-debuggers/testSrc/com/android/tools/idea/sqlite/mocks/MockTableView.kt
+++ android-debuggers/testSrc/com/android/tools/idea/sqlite/mocks/MockTableView.kt
@@ -24,6 +24,7 @@ import javax.swing.JComponent
 open class MockTableView : TableView {
 
   val listeners = mutableListOf<TableView.Listener>()
+  override var isTableActionsRowVisible = true
 
   override val component = mock(JComponent::class.java)
 
