8b34bdd149192a28c5ce2f9feac6ddd90acc3c20
nik <Nikolay.Chashnikov@jetbrains.com>
2020-02-17 11:32:05 +0300

    [i18n] move properties used in intellij.xml.analysis* modules to a separate bundle (IDEA-209382)
    (cherry picked from commit baed74a3962e1249886b969780f6bcd7d5fe5404)
    
    Change-Id: Ibaed74a3962e1249886b969780f6bcd7d5fe5404

--- android/src/org/jetbrains/android/dom/inspections/AndroidElementNotAllowedInspection.java
+++ android/src/org/jetbrains/android/dom/inspections/AndroidElementNotAllowedInspection.java
@@ -2,7 +2,6 @@ package org.jetbrains.android.dom.inspections;
 
 import com.android.resources.ResourceFolderType;
 import com.android.tools.idea.res.IdeResourcesUtil;
-import com.intellij.codeInsight.daemon.XmlErrorBundle;
 import com.intellij.codeInspection.InspectionManager;
 import com.intellij.codeInspection.LocalInspectionTool;
 import com.intellij.codeInspection.LocalQuickFix;
@@ -14,6 +13,7 @@ import com.intellij.psi.xml.XmlFile;
 import com.intellij.psi.xml.XmlTag;
 import com.intellij.psi.xml.XmlToken;
 import com.intellij.xml.XmlElementDescriptor;
+import com.intellij.xml.analysis.XmlAnalysisBundle;
 import com.intellij.xml.util.XmlTagUtil;
 import java.util.ArrayList;
 import java.util.List;
@@ -85,14 +85,14 @@ public class AndroidElementNotAllowedInspection extends LocalInspectionTool {
         if (descriptor instanceof AndroidAnyTagDescriptor) {
           final XmlToken startTagNameElement = XmlTagUtil.getStartTagNameElement(tag);
           if (startTagNameElement != null && !isUnknownCustomView(tag)) {
-            myResult.add(myInspectionManager.createProblemDescriptor(startTagNameElement, XmlErrorBundle.message(
+            myResult.add(myInspectionManager.createProblemDescriptor(startTagNameElement, XmlAnalysisBundle.message(
               "element.is.not.allowed.here", tag.getName()), myOnTheFly, LocalQuickFix.EMPTY_ARRAY,
                                                                      ProblemHighlightType.GENERIC_ERROR_OR_WARNING));
           }
 
           final XmlToken endTagNameElement = XmlTagUtil.getEndTagNameElement(tag);
           if (endTagNameElement != null && !isUnknownCustomView(tag)) {
-            myResult.add(myInspectionManager.createProblemDescriptor(endTagNameElement, XmlErrorBundle.message(
+            myResult.add(myInspectionManager.createProblemDescriptor(endTagNameElement, XmlAnalysisBundle.message(
               "element.is.not.allowed.here", tag.getName()), myOnTheFly, LocalQuickFix.EMPTY_ARRAY,
                                                                      ProblemHighlightType.GENERIC_ERROR_OR_WARNING));
           }
