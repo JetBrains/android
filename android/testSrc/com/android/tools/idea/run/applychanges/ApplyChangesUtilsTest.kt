fd8b9a47c1305a6d7c1584d5600e4377bfec0f07
Egor Ushakov <egor.ushakov@jetbrains.com>
2020-02-18 12:20:36 +0300

    convert to services pt.2
    Change-Id: I6f13abdd4329fbc7add79ba8498f7470a3530681

--- android/testSrc/com/android/tools/idea/run/applychanges/ApplyChangesUtilsTest.kt
+++ android/testSrc/com/android/tools/idea/run/applychanges/ApplyChangesUtilsTest.kt
@@ -76,7 +76,7 @@ class ApplyChangesUtilsTest {
     `when`(mockEnv.runProfile).thenReturn(mockRunProfile)
     `when`(mockEnv.executionTarget).thenReturn(mockExecutionTarget)
     `when`(mockProject.getService(eq(ExecutionManager::class.java))).thenReturn(mockExecutionManager)
-    `when`(mockProject.getComponent(eq(DebuggerManager::class.java))).thenReturn(mockDebugManager)
+    `when`(mockProject.getService(eq(DebuggerManager::class.java))).thenReturn(mockDebugManager)
     `when`(mockDebugManager.sessions).thenReturn(listOf())
     `when`(mockProject.getService(eq(RunContentManager::class.java))).thenReturn(mockRunContentManager)
   }
