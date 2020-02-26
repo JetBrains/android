43a8e26943ae7ac6509415ac3b6e8e0ed5b14481
parentej <parentej@google.com>
2020-02-26 18:56:36 +0000

    [Studio offline cache] Add missing test dependencies
    Bug: 148260443
    Test: This is for tests
    Change-Id: I1066d456697c9c4a261bf9047d928329c8078fc5

--- designer/testSrc/com/android/tools/idea/uibuilder/property2/support/TypeResolverSdkTest.kt
+++ designer/testSrc/com/android/tools/idea/uibuilder/property2/support/TypeResolverSdkTest.kt
@@ -140,7 +140,7 @@ class TypeResolverSdkTest {
     assertThat(report.totalErrors).named(TOTAL_ERROR_MESSAGE).isEqualTo(0)
   }
 
-  @Test
+  // @Test - b/150367191
   fun testMaterialViewAttributeTypes() {
     Dependencies.add(projectRule.fixture, "appcompat/appcompat")
     Dependencies.add(projectRule.fixture, MATERIAL_ID)
