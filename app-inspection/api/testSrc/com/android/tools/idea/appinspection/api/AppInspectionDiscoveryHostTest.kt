4b34555b33897aaf5319803e1ff5ca29f0f66582
Peter Xu <peterx@google.com>
2020-02-26 11:04:50 -0800

    Discovery service to distinguish processes running on same device model
    We now use the device serial number to distinguish processes running on
    the same device model.
    
    Test: new AppInspectionDiscoveryHostTest
    Fixes: 150017926
    Change-Id: Iff5be48e482763b986d2f80ea3e21c45b348e6a0

--- app-inspection/api/testSrc/com/android/tools/idea/appinspection/api/AppInspectionDiscoveryHostTest.kt
+++ app-inspection/api/testSrc/com/android/tools/idea/appinspection/api/AppInspectionDiscoveryHostTest.kt
@@ -77,6 +77,7 @@ class AppInspectionDiscoveryHostTest {
       LaunchedProcessDescriptor(
         device.manufacturer,
         device.model,
+        device.serial,
         process.name,
         AppInspectionTestUtils.TestTransportJarCopier
       )
@@ -231,6 +232,37 @@ class AppInspectionDiscoveryHostTest {
 
     // Launch process with same pid in stream 2
     val fakeDevice2 = FakeTransportService.FAKE_DEVICE.toBuilder().setDeviceId(2).setModel("fakeModel2").setManufacturer("fakeMan2").build()
+    val fakeProcess2 = FakeTransportService.FAKE_PROCESS.toBuilder().setDeviceId(2).build()
+    launchFakeProcess(discoveryHost, fakeDevice2, fakeProcess2)
+
+    latch.await()
+  }
+
+  @Test
+  fun processesRunningOnTwoIdenticalDeviceModels() {
+    // Setup
+    val executor = MoreExecutors.listeningDecorator(Executors.newScheduledThreadPool(1))
+    val discoveryHost = AppInspectionDiscoveryHost(executor, TransportClient(grpcServerRule.name))
+
+    val latch = CountDownLatch(2)
+    discoveryHost.addProcessListener(executor, object : AppInspectionDiscoveryHost.ProcessListener {
+      override fun onProcessConnected(descriptor: ProcessDescriptor) {
+        latch.countDown()
+      }
+
+      override fun onProcessDisconnected(descriptor: ProcessDescriptor) {
+      }
+    })
+
+    // Launch process in stream 1
+    val fakeDevice1 =
+      FakeTransportService.FAKE_DEVICE.toBuilder().setDeviceId(1).setModel("fakeModel").setManufacturer("fakeMan").setSerial("1").build()
+    val fakeProcess1 = FakeTransportService.FAKE_PROCESS.toBuilder().setDeviceId(1).build()
+    launchFakeProcess(discoveryHost, fakeDevice1, fakeProcess1)
+
+    // Launch process with same pid in stream 2
+    val fakeDevice2 =
+      FakeTransportService.FAKE_DEVICE.toBuilder().setDeviceId(2).setModel("fakeModel").setManufacturer("fakeMan").setSerial("2").build()
     val fakeProcess2 = FakeTransportService.FAKE_PROCESS.toBuilder().setDeviceId(2).build()
     launchFakeProcess(discoveryHost, fakeDevice2, fakeProcess2)
 
