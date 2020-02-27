297458a3f766937cfe73f055e7d46b3cd7df6034
Gregory.Shrago <gregory.shrago@jetbrains.com>
2020-02-27 21:06:58 +0300

    unify setAsUsualTopToolbar usages 2
    GitOrigin-RevId: 2f68319e3edda18e297328cdd754df8001686fc4

--- profilers-android/src/com/android/tools/idea/profilers/profilingconfig/CpuProfilingConfigurationsDialog.java
+++ profilers-android/src/com/android/tools/idea/profilers/profilingconfig/CpuProfilingConfigurationsDialog.java
@@ -23,6 +23,7 @@ import com.android.tools.profilers.ProfilerColors;
 import com.android.tools.profilers.analytics.FeatureTracker;
 import com.android.tools.profilers.cpu.CpuProfilerConfigModel;
 import com.android.tools.profilers.cpu.ProfilingConfiguration;
+import com.intellij.openapi.actionSystem.ActionToolbarPosition;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.options.Configurable;
@@ -40,6 +41,7 @@ import com.intellij.ui.ToolbarDecorator;
 import com.intellij.ui.components.JBList;
 import com.intellij.util.IconUtil;
 import com.intellij.util.ui.JBDimension;
+import com.intellij.util.ui.JBUI;
 import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.Dimension;
@@ -215,7 +217,8 @@ public class CpuProfilingConfigurationsDialog extends SingleConfigurableEditor {
       MyMoveAction moveDownAction = new MyMoveAction(MOVE_DOWN, 1, IconUtil.getMoveUpIcon());
 
       ToolbarDecorator toolbarDecorator = ToolbarDecorator.createDecorator(myConfigurations)
-        .setAsUsualTopToolbar()
+        .setToolbarPosition(ActionToolbarPosition.TOP)
+        .setPanelBorder(JBUI.Borders.empty())
         .setMoveUpAction(moveUpAction).setMoveUpActionUpdater(moveUpAction).setMoveUpActionName(MOVE_UP)
         .setMoveDownAction(moveDownAction).setMoveDownActionUpdater(moveDownAction).setMoveDownActionName(MOVE_DOWN)
         .setRemoveAction(removeAction).setRemoveActionUpdater(removeAction).setRemoveActionName(REMOVE)
