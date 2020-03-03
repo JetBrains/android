1201e27d2ecdf22aec8a6871dec249d9dcb4bb2c
Yuriy Solodkyy <solodkyy@google.com>
2020-03-03 12:08:46 +0000

    Move Android project initialization to the project system
    Extract initializers to their own classes and, for now, leave them
    in `android.core` module.
    
    This is to allow initialization steps to be moved out of
    `android.core` as we split the module into smaller modules.
    
    Bug: 147586218
    Test: n/a
    Change-Id: Ic9f23d318e9ad0f0a7ed93fe0adfb93ea406c0ff

---
+++ android/src/com/android/tools/idea/templates/DynamicTemplateMenuAndroidStartupActivity.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.templates;
+
+import com.android.tools.idea.AndroidStartupActivity;
+import com.intellij.openapi.Disposable;
+import com.intellij.openapi.actionSystem.ActionGroup;
+import com.intellij.openapi.actionSystem.ActionManager;
+import com.intellij.openapi.actionSystem.Anchor;
+import com.intellij.openapi.actionSystem.Constraints;
+import com.intellij.openapi.actionSystem.DefaultActionGroup;
+import com.intellij.openapi.progress.PerformInBackgroundOption;
+import com.intellij.openapi.progress.ProgressIndicator;
+import com.intellij.openapi.progress.Task;
+import com.intellij.openapi.project.Project;
+import org.jetbrains.annotations.NotNull;
+
+class DynamicTemplateMenuAndroidStartupActivity implements AndroidStartupActivity {
+  private static boolean ourDynamicTemplateMenuCreated;
+
+  @Override
+  public void runActivity(@NotNull Project project, @NotNull Disposable disposable) {
+    createDynamicTemplateMenu();
+  }
+
+  private static void createDynamicTemplateMenu() {
+    if (ourDynamicTemplateMenuCreated) {
+      return;
+    }
+    ourDynamicTemplateMenuCreated = true;
+
+    new Task.Backgroundable(null, "Loading Dynamic Templates", false, PerformInBackgroundOption.ALWAYS_BACKGROUND) {
+      ActionGroup menu;
+
+      @Override
+      public void run(@NotNull ProgressIndicator indicator) {
+        indicator.setIndeterminate(true);
+        menu = TemplateManager.getInstance().getTemplateCreationMenu();
+      }
+
+      @Override
+      public void onFinished() {
+        DefaultActionGroup newGroup = (DefaultActionGroup)ActionManager.getInstance().getAction("NewGroup");
+        newGroup.addSeparator();
+        if (menu != null) {
+          newGroup.add(menu, new Constraints(Anchor.AFTER, "NewFromTemplate"));
+        }
+      }
+    }.queue();
+  }
+}
---
+++ android/src/org/jetbrains/android/CompilerManagerAndroidStartupActivity.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jetbrains.android;
+
+import com.android.tools.idea.AndroidStartupActivity;
+import com.intellij.openapi.Disposable;
+import com.intellij.openapi.compiler.CompilerManager;
+import com.intellij.openapi.project.Project;
+import org.jetbrains.android.compiler.AndroidPrecompileTask;
+import org.jetbrains.annotations.NotNull;
+
+// TODO(b/150625066): Move to JPS project system if possible.
+public class CompilerManagerAndroidStartupActivity implements AndroidStartupActivity {
+
+  @Override
+  public void runActivity(@NotNull Project project, @NotNull Disposable disposable) {
+    final CompilerManager manager = CompilerManager.getInstance(project);
+    manager.addBeforeTask(new AndroidPrecompileTask());
+  }
+}
