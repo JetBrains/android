1120dd0760ec57390b2ee1c853f852f7aef8c5dc
Yuriy Solodkyy <solodkyy@google.com>
2020-03-03 13:04:50 +0000

    Remove unused import of ThemeHelper
    and a reference from a comment where ThemeHelper is not used.
    
    Bug: 147586218
    Test: n/a
    Change-Id: I65c6ece435aca602b064a18ecec4cb7a74601ddb

--- android/src/com/android/tools/idea/ui/resourcemanager/explorer/ResourceExplorerViewModel.kt
+++ android/src/com/android/tools/idea/ui/resourcemanager/explorer/ResourceExplorerViewModel.kt
@@ -25,7 +25,6 @@ import com.android.tools.idea.configurations.Configuration
 import com.android.tools.idea.configurations.ConfigurationManager
 import com.android.tools.idea.configurations.getAppThemeName
 import com.android.tools.idea.configurations.getDefaultTheme
-import com.android.tools.idea.npw.ThemeHelper
 import com.android.tools.idea.res.ResourceNotificationManager
 import com.android.tools.idea.res.getFolderType
 import com.android.tools.idea.ui.resourcemanager.MANAGER_SUPPORTED_RESOURCES
@@ -441,7 +440,7 @@ private fun getResourceResolver(
 }
 
 /**
- *  Try to get application theme from [ThemeHelper]. And it falls back to the default theme if necessary.
+ *  Try to get application theme from the manifest. And it falls back to the default theme if necessary.
  */
 private fun getApplicationTheme(facet: AndroidFacet): String {
   val module = facet.module
