bf47004a191713f0ac49b109394fb502c3472c75
Joshua Gilpatrick <gijosh@google.com>
2020-03-09 16:41:14 -0700

    Add support for "Jump to source"
    Test: Updated
    Bug: 149541949
    Change-Id: Ia07da8e50d912303a456859b87f74f63a61c0063

--- profilers-android/src/com/android/tools/idea/profilers/perfetto/traceprocessor/HeapProfdConverter.kt
+++ profilers-android/src/com/android/tools/idea/profilers/perfetto/traceprocessor/HeapProfdConverter.kt
@@ -32,14 +32,18 @@ import java.util.HashMap
 class HeapProfdConverter(private val abi: String,
                          private val symbolizer: NativeFrameSymbolizer,
                          private val memorySet: NativeMemoryHeapSet) {
+  companion object {
+    private val UNKNOWN_FRAME = Memory.AllocationStack.StackFrame.newBuilder().setMethodName("unknown").build()
+  }
 
   /**
    * Given a {@link Memory.StackFrame} from the trace processor we attempt to gather symbolized data. If we cannot get symbolized data
-   * we return the original name if one was found. If no name was found then we return "unknown"
-   * When we have a symbolized frame we return the results in the form of
+   * we return a frame with the original name if one was provided. If no name was found then we return {@link UNKNOWN_FRAME}
+   * When we have a symbolized frame we return a frame with a method name in the form of
    * Symbol (File:Line) eg.. operator new (new.cpp:256)
+   * The file name and line number are also populated if available.
    */
-  private fun toBestAvailableName(rawFrame: StackFrame): String {
+  private fun toBestAvailableStackFrame(rawFrame: StackFrame): Memory.AllocationStack.StackFrame {
     val symbolizedFrame = symbolizer.symbolize(abi, Memory.NativeCallStack.NativeFrame.newBuilder()
       .setModuleName(rawFrame.module)
       // +1 because the common symbolizer does -1 accounting for an offset heapprofd does not have.
@@ -48,10 +52,18 @@ class HeapProfdConverter(private val abi: String,
       .build())
     if (symbolizedFrame.symbolName.startsWith("0x")) {
       // Raw name is better than PC offset.
-      return rawFrame.name.ifBlank { "unknown" }
+      if (rawFrame.name.isNullOrBlank()) {
+        return UNKNOWN_FRAME
+      }
+      return Memory.AllocationStack.StackFrame.newBuilder().setMethodName(rawFrame.name).build()
     }
     val file = File(symbolizedFrame.fileName).name
-    return "${symbolizedFrame.symbolName} (${file}:${symbolizedFrame.lineNumber})"
+    val formattedName = "${symbolizedFrame.symbolName} (${file}:${symbolizedFrame.lineNumber})"
+    return Memory.AllocationStack.StackFrame.newBuilder()
+      .setMethodName(formattedName)
+      .setFileName(symbolizedFrame.fileName)
+      .setLineNumber(symbolizedFrame.lineNumber)
+      .build()
   }
 
   /**
@@ -59,16 +71,16 @@ class HeapProfdConverter(private val abi: String,
    * a count > 0 it will be added as an allocation. If the count is <= 0 it will be added as a free.
    */
   fun populateHeapSet(context: NativeAllocationContext) {
-    val frameIdToName: MutableMap<Long, String> = HashMap()
+    val frameIdToFrame: MutableMap<Long, Memory.AllocationStack.StackFrame> = HashMap()
     val stackPointerIdToParentId: MutableMap<Long, Long> = HashMap()
-    val stackPointerIdToFrameName: MutableMap<Long, String> = HashMap()
+    val stackPointerIdToFrameName: MutableMap<Long, Memory.AllocationStack.StackFrame> = HashMap()
     val callSites: Map<Long, NativeAllocationInstanceObject?> = HashMap()
     val classDb = ClassDb()
     context.framesList.forEach {
-      frameIdToName[it.id] = toBestAvailableName(it)
+      frameIdToFrame[it.id] = toBestAvailableStackFrame(it)
     }
     context.pointersList.forEach {
-      stackPointerIdToFrameName[it.id] = frameIdToName[it.frameId] ?: "unknown"
+      stackPointerIdToFrameName[it.id] = frameIdToFrame[it.frameId] ?: UNKNOWN_FRAME
       stackPointerIdToParentId[it.id] = it.parentId
     }
     context.allocationsList.forEach {
@@ -76,19 +88,18 @@ class HeapProfdConverter(private val abi: String,
       var callSiteId = stackPointerIdToParentId[it.stackId]
       if (!callSites.containsKey(it.stackId)) {
         while (callSiteId != null && callSiteId != 0L) {
-          val name = stackPointerIdToFrameName[callSiteId] ?: "unknown"
-          fullStack.addFrames(Memory.AllocationStack.StackFrame.newBuilder().setMethodName(name).build())
+          fullStack.addFrames(stackPointerIdToFrameName[callSiteId] ?: UNKNOWN_FRAME)
           callSiteId = stackPointerIdToParentId[callSiteId]
         }
         val event = Memory.AllocationEvent.Allocation.newBuilder()
           .setSize(Math.abs(it.size))
           .build()
-        val name = stackPointerIdToFrameName[it.stackId] ?: "unknown"
+        val frame = stackPointerIdToFrameName[it.stackId] ?: UNKNOWN_FRAME
         val stack = Memory.AllocationStack.newBuilder()
           .setFullStack(fullStack)
           .build()
         val instanceObject = NativeAllocationInstanceObject(
-          event, classDb.registerClass(0, 0, name), stack, it.count)
+          event, classDb.registerClass(0, 0, frame.methodName), stack, it.count)
         if (it.count > 0) {
           memorySet.addDeltaInstanceObject(instanceObject)
         }
