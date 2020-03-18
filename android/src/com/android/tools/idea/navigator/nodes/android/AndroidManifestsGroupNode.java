dcf055fab0fbec384d24f565f731ac310d26e819
Dana Dahlstrom <dahlstrom@google.com>
2020-03-17 18:00:00 -0700

    Don't static-import PsiDirectory.EMPTY_ARRAY
    This makes the type and provenance clear where it's used
    and avoids a compilation error after the update to 2020.1
    probably due to something in the inheritance hierarchy.
    
    Change-Id: I1e46e32b68b5a1ae8b5afbb4273ed8ecbac17b2d

--- android/src/com/android/tools/idea/navigator/nodes/android/AndroidManifestsGroupNode.java
+++ android/src/com/android/tools/idea/navigator/nodes/android/AndroidManifestsGroupNode.java
@@ -41,7 +41,6 @@ import java.util.List;
 import java.util.Set;
 
 import static com.android.SdkConstants.FD_MAIN;
-import static com.intellij.psi.PsiDirectory.EMPTY_ARRAY;
 import static com.intellij.ui.SimpleTextAttributes.REGULAR_ATTRIBUTES;
 
 public class AndroidManifestsGroupNode extends ProjectViewNode<AndroidFacet> implements FolderGroupNode {
@@ -149,7 +148,7 @@ public class AndroidManifestsGroupNode extends ProjectViewNode<AndroidFacet> imp
   @Override
   @NotNull
   public PsiDirectory[] getFolders() {
-    return EMPTY_ARRAY;
+    return PsiDirectory.EMPTY_ARRAY;
   }
 
   @Override
