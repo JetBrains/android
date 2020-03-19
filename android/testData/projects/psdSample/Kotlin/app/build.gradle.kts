c9678ddf799ed02e3647362b6b5d090d995470ce
Ivan Gavrilovic <gavra@google.com>
2020-03-19 18:20:54 +0000

    Remove workaround used in Gradle tests
    Bug: 144281891
    Test: existing
    Change-Id: I3d948f9ae725f1a553fa7eaf411eec0173dae3d4

--- android/testData/projects/psdSample/Kotlin/app/build.gradle.kts
+++ android/testData/projects/psdSample/Kotlin/app/build.gradle.kts
@@ -99,6 +99,5 @@ dependencies {
     implementation("com.android.support.constraint:constraint-layout:1.1.0")
     implementation("com.android.support.test:runner:1.0.2")
     implementation("com.android.support.test.espresso:espresso-core:3.0.2")
-    // This is to work around the issue in Gradle 6.0 RC, where the line below fails. See b/144281891.
-    //api(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+    api(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
 }
