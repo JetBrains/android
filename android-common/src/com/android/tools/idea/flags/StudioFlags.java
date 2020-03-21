65493bddd3955b2d3915258967b1ca503c518b6c
Mike Moroney <mmoro@google.com>
2020-03-20 21:30:20 -0700

    Enable nav component tree
    Bug: 141201121
    Test: None
    Change-Id: Id45074792abf49747ffb6a4ed05c0eb1a55e56b3

--- android-common/src/com/android/tools/idea/flags/StudioFlags.java
+++ android-common/src/com/android/tools/idea/flags/StudioFlags.java
@@ -348,7 +348,8 @@ public final class StudioFlags {
   public static final Flag<Boolean> NAV_NEW_COMPONENT_TREE = Flag.create(
     NAV_EDITOR, "new.component", "Enable the new Component Tree",
     "Enable the new Component Tree",
-    false);
+    true);
+
   public static final Flag<Boolean> NAV_DYNAMIC_SUPPORT = Flag.create(
     NAV_EDITOR, "dynamic.support", "Support for Dynamic Feature Modules",
     "Support for Dynamic Feature Modules",
