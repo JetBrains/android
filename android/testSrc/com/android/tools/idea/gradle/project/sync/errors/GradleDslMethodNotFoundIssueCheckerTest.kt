ff0a823f37d2c7ef8c8449fbadf5d9582d3f9353
Karima Ichou <karimai@google.com>
2020-03-23 13:00:50 +0000

    [Sync Error Handlers] convert GradleDslMethodNotFoundErrorHandler to IssueChecker.
    Bug: 150060053
    Bug: 149822714
    Test: Added
    Change-Id: I0905be963cfca4ff4d846ed0c9f2a1422d0cb5aa

---
+++ android/testSrc/com/android/tools/idea/gradle/project/sync/errors/GradleDslMethodNotFoundIssueCheckerTest.kt
@@ -0,0 +1,65 @@
+/*
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.gradle.project.sync.errors
+
+import com.android.SdkConstants
+import com.android.SdkConstants.FN_SETTINGS_GRADLE
+import com.android.tools.idea.testing.AndroidGradleTestCase
+import com.google.common.truth.Truth.assertThat
+import com.intellij.build.FilePosition
+import org.jetbrains.plugins.gradle.issue.GradleIssueData
+import java.io.File
+
+class GradleDslMethodNotFoundIssueCheckerTest : AndroidGradleTestCase() {
+  private val gradleDslMethodNotFoundIssueChecker = GradleDslMethodNotFoundIssueChecker()
+
+  fun testCheckIssueWithMethodNotFoundInSettingsFile() {
+    loadSimpleApplication()
+
+    val settingsFile = File(project.basePath, FN_SETTINGS_GRADLE)
+    val issueData = GradleIssueData(projectFolderPath.path, Throwable("Gradle DSL method not found. \nCould not find method abdd()"),
+                                    null, FilePosition(settingsFile, 2, 0))
+    val buildIssue = gradleDslMethodNotFoundIssueChecker.check(issueData)
+
+    // Check error handled correctly.
+    assertThat(buildIssue).isNotNull()
+    assertThat(buildIssue!!.description).contains("Gradle DSL method not found")
+    // Verify quickFixes.
+    assertThat(buildIssue.quickFixes).hasSize(1)
+    val quickFix = buildIssue.quickFixes[0]
+    assertThat(quickFix).isInstanceOf(GradleDslMethodNotFoundIssueChecker.OpenFileAtLocationQuickFix::class.java)
+    assertThat((quickFix as GradleDslMethodNotFoundIssueChecker.OpenFileAtLocationQuickFix).myFilePosition.file.path).isEqualTo(settingsFile.path)
+  }
+
+  fun testCheckIssueWithMethodNotFoundInBuildFile() {
+    loadSimpleApplication()
+
+    val buildFile = File(project.basePath, SdkConstants.FN_BUILD_GRADLE)
+    val issueData = GradleIssueData(projectFolderPath.path, Throwable("Gradle DSL method not found. \nCould not find method abdd()"),
+                                    null, FilePosition(buildFile, 0, 0))
+    val buildIssue = gradleDslMethodNotFoundIssueChecker.check(issueData)
+
+    // Check error handled correctly.
+    assertThat(buildIssue).isNotNull()
+    assertThat(buildIssue!!.description).contains("Your project may be using a version of the Android Gradle plug-in that does not contain the " +
+                                                  "method (e.g. 'testCompile' was added in 1.1.0).")
+    // Verify quickFixes.
+    assertThat(buildIssue.quickFixes).hasSize(3)
+    assertThat(buildIssue.quickFixes[0]).isInstanceOf(GradleDslMethodNotFoundIssueChecker.FixAndroidGradlePluginVersionQuickFix::class.java)
+    assertThat(buildIssue.quickFixes[1]).isInstanceOf(GradleDslMethodNotFoundIssueChecker.GetGradleSettingsQuickFix::class.java)
+    assertThat(buildIssue.quickFixes[2]).isInstanceOf(GradleDslMethodNotFoundIssueChecker.ApplyGradlePluginQuickFix::class.java)
+  }
+}
