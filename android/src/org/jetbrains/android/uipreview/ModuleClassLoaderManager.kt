cce61342a6844afaf40374210df36f5e972eb4c1
Fedor Kudasov <kudasov@google.com>
2020-03-24 13:37:25 +0000

    Clear ThreadLocals to be able to dispose ClassLoader
    If the ClassLoader is retained by the ThreadLocals
    it can not be garbage collected. Clear the known
    compose ThreadLocals to increase probability of
    successful memory freeing.
    
    Bug: 146552571
    Test: manually tested with profiler
    Change-Id: If71e40742ac387cda6e7c5ccc9a5a856120299c5

--- android/src/org/jetbrains/android/uipreview/ModuleClassLoaderManager.kt
+++ android/src/org/jetbrains/android/uipreview/ModuleClassLoaderManager.kt
@@ -15,16 +15,17 @@
  */
 package org.jetbrains.android.uipreview
 
+import com.android.SdkConstants.CLASS_COMPOSE_VIEW_ADAPTER
 import com.android.tools.idea.LogAnonymizerUtil.anonymize
+import com.android.tools.idea.rendering.RenderService
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.diagnostic.debug
 import com.intellij.openapi.module.Module
 import java.util.WeakHashMap
-import java.util.concurrent.locks.ReentrantReadWriteLock
-import kotlin.concurrent.read
-import kotlin.concurrent.write
 
+private const val CLASS_COMPOSE_RECOMPOSER = "androidx.compose.Recomposer"
+private const val CLASS_COMPOSE_FRAMES = "androidx.compose.frames.FramesKt"
 /**
  * A [ClassLoader] for the [Module] dependencies.
  */
@@ -39,13 +40,16 @@ class ModuleClassLoaderManager {
     var moduleClassLoader =
       cache[module]
 
+    var oldClassLoader: ModuleClassLoader? = null
     if (moduleClassLoader != null) {
       if (moduleClassLoader.parent != parent) {
         LOG.debug("Parent has changed, discarding ModuleClassLoader")
+        oldClassLoader = moduleClassLoader
         moduleClassLoader = null
       }
       else if (!moduleClassLoader.isUpToDate) {
         LOG.debug("Files have changed, discarding ModuleClassLoader")
+        oldClassLoader = moduleClassLoader
         moduleClassLoader = null
       }
       else {
@@ -57,6 +61,7 @@ class ModuleClassLoaderManager {
       LOG.debug { "Loading new class loader for module ${anonymize(module)}" }
       moduleClassLoader = ModuleClassLoader(parent, module)
       cache[module] = moduleClassLoader
+      oldClassLoader?.let { disposeClassLoaderThreadLocals(it) }
     }
 
     return moduleClassLoader
@@ -64,18 +69,30 @@ class ModuleClassLoaderManager {
 
   @Synchronized
   fun clearCache() {
-    cache.clear();
+    cache.values.forEach { disposeClassLoaderThreadLocals(it) }
+    cache.clear()
   }
 
   @Synchronized
   fun clearCache(module: Module) {
-    cache.remove(module)
+    cache.remove(module)?.let { disposeClassLoaderThreadLocals(it) }
   }
 
+  @Synchronized
+  private fun isManaged(moduleClassLoader: ModuleClassLoader) = cache.values.contains(moduleClassLoader)
+
   /**
-   * Manually disposes the cache.
+   * Inform [ModuleClassLoaderManager] that [ModuleClassLoader] is not used anymore and therefore can be
+   * disposed if no longer managed.
    */
-  fun disposeCache() = cache.clear()
+  fun release(moduleClassLoader: ModuleClassLoader) {
+    if (isManaged(moduleClassLoader)) {
+      // We are still managing this ClassLoader, cannot dispose yet
+      return
+    }
+
+    disposeClassLoaderThreadLocals(moduleClassLoader)
+  }
 
   companion object {
     @JvmStatic
@@ -84,5 +101,43 @@ class ModuleClassLoaderManager {
     @JvmStatic
     fun get(): ModuleClassLoaderManager =
       ApplicationManager.getApplication().getService(ModuleClassLoaderManager::class.java);
+
+    // TODO(b/152947285): Remove all ThreadLocals that retain ModuleClassLoader from the user code
+    //
+    // Current approach, where we are searching for particular ThreadLocals with reflection is not
+    // sustainable. We will not only fail in case compose changes its internals, but also if the user
+    // code (which we have no control on) defines static ThreadLocals as well.
+    private fun disposeClassLoaderThreadLocals(moduleClassLoader: ModuleClassLoader) {
+      // If Compose has not been loaded, we do not need to care about disposing it
+      if (!moduleClassLoader.isClassLoaded(CLASS_COMPOSE_VIEW_ADAPTER)) {
+        return
+      }
+
+      try {
+        val framesKtClass = moduleClassLoader.loadClass(CLASS_COMPOSE_FRAMES)
+        val recomposerClass = moduleClassLoader.loadClass(CLASS_COMPOSE_RECOMPOSER)
+
+        val threadLocalFields = listOf(
+          framesKtClass.getDeclaredField("threadFrame"),
+          framesKtClass.getDeclaredField("threadReadObservers"),
+          recomposerClass.getDeclaredField("threadRecomposer"))
+
+        threadLocalFields.forEach { it.isAccessible = true }
+
+        // Because we are clearing-up ThreadLocals, the code must run on the Layoutlib Thread
+        RenderService.runAsyncRenderAction {
+          threadLocalFields.forEach {
+            try {
+              (it[null] as ThreadLocal<*>).remove()
+            } catch (e: IllegalAccessException) {
+              LOG.warn(e) // Failure detected here will most probably cause a memory leak
+            }
+          }
+        }
+      }
+      catch (t: Throwable) {
+        LOG.warn(t) // Failure detected here will most probably cause a memory leak
+      }
+    }
   }
 }
