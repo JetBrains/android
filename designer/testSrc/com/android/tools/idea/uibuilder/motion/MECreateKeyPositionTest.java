2909acad89f2b916725172fe2b92dbd7acfddf6b
Gregory Arias <gregoryarias@google.com>
2020-03-24 09:42:47 -0700

    [MotionLayoutUX] Add divider to keyframe creation dialog
    - Added divider between Tag/ID and rest of fields in the transition key frame create dialogs
    
    Fixes: 151865771
    Test: Existing (no functional change)/Visual Verfiication
    Change-Id: I22746ff2ac5fd0bc51d69690a9399da55f58682c

--- designer/testSrc/com/android/tools/idea/uibuilder/motion/MECreateKeyPositionTest.java
+++ designer/testSrc/com/android/tools/idea/uibuilder/motion/MECreateKeyPositionTest.java
@@ -45,19 +45,20 @@ public class MECreateKeyPositionTest extends BaseMotionEditorTest {
     CreatorAccess panel = new CreatorAccess();
     String layout = "0,CreatorAccess      ,0,0,99,99\n" +
                     "1,JLabel             ,4,2,90,4\n" +
-                    "1,JSeparator         ,0,9,99,0\n" +
-                    "1,JRadioButton       ,4,12,42,6\n" +
+                    "1,JSeparator         ,0,8,99,0\n" +
+                    "1,JRadioButton       ,4,11,42,6\n" +
                     "1,JRadioButton       ,57,11,37,6\n" +
-                    "1,JPanel             ,5,20,89,7\n" +
+                    "1,JPanel             ,5,19,89,6\n" +
                     "2,PromptedTextField  ,0,0,0,0\n" +
                     "2,MEComboBox         ,0,0,0,0\n" +
-                    "1,JLabel             ,4,29,90,4\n" +
-                    "1,PromptedTextField  ,5,35,89,5\n" +
-                    "1,JLabel             ,4,43,90,4\n" +
-                    "1,MEComboBox         ,5,49,89,7\n" +
-                    "1,JLabel             ,4,58,90,4\n" +
-                    "1,PromptedTextField  ,5,64,89,5\n" +
-                    "1,JLabel             ,4,72,90,4\n" +
+                    "1,JSeparator         ,0,28,99,0\n" +
+                    "1,JLabel             ,4,31,90,4\n" +
+                    "1,PromptedTextField  ,5,37,89,5\n" +
+                    "1,JLabel             ,4,45,90,4\n" +
+                    "1,MEComboBox         ,5,50,89,6\n" +
+                    "1,JLabel             ,4,60,90,4\n" +
+                    "1,PromptedTextField  ,5,65,89,5\n" +
+                    "1,JLabel             ,4,73,90,4\n" +
                     "1,PromptedTextField  ,5,78,89,5\n" +
                     "1,JButton            ,4,88,90,7\n";
     Dimension size = panel.getPreferredSize();
@@ -81,22 +82,23 @@ public class MECreateKeyPositionTest extends BaseMotionEditorTest {
     panel.access_populateDialog();
 
     String info = "0,CreatorAccess,\n" +
-      "1,JLabel,CREATE KEY POSITION\n" +
-      "1,JSeparator,\n" +
-      "1,JRadioButton,\n" +
-      "1,JRadioButton,\n" +
-      "1,JPanel,\n" +
-      "2,PromptedTextField,tag or regex\n" +
-      "2,MEComboBox,number,dial_pad,dialtitle,button1,button2,button3,button4,button5,button6,button7,button8,button9,button10,button11,button12,people_pad,people_title,people1,people2,people3,people4,people5,people6,people7,people8\n" +
-      "1,JLabel,Position\n" +
-      "1,PromptedTextField,0-100\n" +
-      "1,JLabel,Type\n" +
-      "1,MEComboBox,deltaRelative,pathRelative,parentRelative\n" +
-      "1,JLabel,PercentX\n" +
-      "1,PromptedTextField,float\n" +
-      "1,JLabel,PercentY\n" +
-      "1,PromptedTextField,float\n" +
-      "1,JButton,Add\n";
+                  "1,JLabel,CREATE KEY POSITION\n" +
+                  "1,JSeparator,\n" +
+                  "1,JRadioButton,\n" +
+                  "1,JRadioButton,\n" +
+                  "1,JPanel,\n" +
+                  "2,PromptedTextField,tag or regex\n" +
+                  "2,MEComboBox,number,dial_pad,dialtitle,button1,button2,button3,button4,button5,button6,button7,button8,button9,button10,button11,button12,people_pad,people_title,people1,people2,people3,people4,people5,people6,people7,people8\n" +
+                  "1,JSeparator,\n" +
+                  "1,JLabel,Position\n" +
+                  "1,PromptedTextField,0-100\n" +
+                  "1,JLabel,Type\n" +
+                  "1,MEComboBox,deltaRelative,pathRelative,parentRelative\n" +
+                  "1,JLabel,PercentX\n" +
+                  "1,PromptedTextField,float\n" +
+                  "1,JLabel,PercentY\n" +
+                  "1,PromptedTextField,float\n" +
+                  "1,JButton,Add\n";
     assertEquals(info, componentFieldsString(panel, 0));
     panel.fillAttributes();
     MTag tag = panel.create();
