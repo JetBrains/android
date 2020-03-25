768d3fa4fc39bcba705e67b783b131c3973f8ebe
kunshen <kunshen@google.com>
2020-03-24 22:56:00 -0700

    [safeArg] Use SafeArgComponent in light class findAll
    Since class finders are not guarded with safe arg enabling
    checking logic, previously stored light classes are provided
    by mistake. In this cl, when iterating facets, we rely on
    SafeArgComponent info, that we only iteratate facets whose
    safe arg mode is turned on.
    
    Also some refactoring work is included in this cl, that
    SafeArgsClassFinderBase is introduced, and subclasses
    can simply override 'findAll' method and return relevant set
    of safe args light classes.
    
    Bug: 149835298
    Test: Existing
    Change-Id: I25cb46915a68a024213f3abd946a5aeb1c762294

--- nav/safeargs/src/com/android/tools/idea/nav/safeargs/finder/DirectionsClassFinder.kt
+++ nav/safeargs/src/com/android/tools/idea/nav/safeargs/finder/DirectionsClassFinder.kt
@@ -17,55 +17,15 @@ package com.android.tools.idea.nav.safeargs.finder
 
 import com.android.tools.idea.nav.safeargs.module.SafeArgsCacheModuleService
 import com.android.tools.idea.nav.safeargs.psi.LightDirectionsClass
-import com.android.tools.idea.util.androidFacet
-import com.intellij.openapi.module.ModuleManager
+import com.android.tools.idea.nav.safeargs.psi.SafeArgsLightBaseClass
 import com.intellij.openapi.project.Project
-import com.intellij.psi.PsiClass
-import com.intellij.psi.PsiElementFinder
-import com.intellij.psi.PsiPackage
-import com.intellij.psi.search.GlobalSearchScope
-import com.intellij.psi.search.PsiSearchScopeUtil
 import org.jetbrains.android.facet.AndroidFacet
 
 /**
  * A finder that can find instances of [LightDirectionsClass] by qualified name / package.
  */
-class DirectionsClassFinder(private val project: Project) : PsiElementFinder() {
-  companion object {
-    fun findAll(project: Project): List<LightDirectionsClass> {
-      return ModuleManager.getInstance(project).modules
-        .mapNotNull { module -> module.androidFacet }
-        .flatMap { facet -> findAll(facet) }
-    }
-
-    fun findAll(facet: AndroidFacet): List<LightDirectionsClass> {
-      return SafeArgsCacheModuleService.getInstance(facet).directions
-    }
-  }
-
-  override fun findClass(qualifiedName: String, scope: GlobalSearchScope): PsiClass? {
-    return findAll(project)
-      .firstOrNull { directionsClass ->
-        directionsClass.qualifiedName == qualifiedName
-        && PsiSearchScopeUtil.isInScope(scope, directionsClass)
-      }
-  }
-
-  override fun findClasses(qualifiedName: String, scope: GlobalSearchScope): Array<PsiClass> {
-    val psiClass = findClass(qualifiedName, scope) ?: return PsiClass.EMPTY_ARRAY
-    return arrayOf(psiClass)
-  }
-
-  override fun getClasses(psiPackage: PsiPackage, scope: GlobalSearchScope): Array<PsiClass> {
-    if (psiPackage.project != scope.project) {
-      return PsiClass.EMPTY_ARRAY
-    }
-
-    return findAll(psiPackage.project)
-      .filter { directionsClass ->
-        psiPackage.qualifiedName == directionsClass.qualifiedName.substringBeforeLast('.')
-        && PsiSearchScopeUtil.isInScope(scope, directionsClass)
-      }
-      .toTypedArray()
+class DirectionsClassFinder(project: Project) : SafeArgsClassFinderBase(project) {
+  override fun findAll(facet: AndroidFacet): List<SafeArgsLightBaseClass> {
+    return SafeArgsCacheModuleService.getInstance(facet).directions
   }
 }
