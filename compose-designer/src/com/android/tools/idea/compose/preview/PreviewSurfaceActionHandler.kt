d4cbc814a2654a6571e9c620661ef925627b8346
Diego Perez <diegoperez@google.com>
2020-03-27 14:18:53 +0000

    Disable copy/paste and delete in Compose Preview
    The surface has an XML underneath but it does not support copy/paste or
    deleting. The model is virtual and all changes will be disposed.
    
    Fixes: 152604650
    Test: Manually tested
    Change-Id: I4153f39645ff87dc43f1f2361cf049bf12be685c

---
+++ compose-designer/src/com/android/tools/idea/compose/preview/PreviewSurfaceActionHandler.kt
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.compose.preview
+
+import com.android.tools.idea.common.model.ItemTransferable.DESIGNER_FLAVOR
+import com.android.tools.idea.common.model.NlComponent
+import com.android.tools.idea.common.surface.DesignSurface
+import com.android.tools.idea.common.surface.DesignSurfaceActionHandler
+import com.intellij.openapi.actionSystem.DataContext
+import java.awt.datatransfer.DataFlavor
+
+/**
+ * A [DesignSurfaceActionHandler] that disables the copy/paste and delete functionality.
+ */
+class PreviewSurfaceActionHandler(surface: DesignSurface): DesignSurfaceActionHandler(surface) {
+  override fun getPasteTarget(): NlComponent? = null
+  override fun canHandleChildren(component: NlComponent, pasted: MutableList<NlComponent>): Boolean = false
+  override fun getFlavor(): DataFlavor = DESIGNER_FLAVOR
+  override fun canDeleteElement(dataContext: DataContext): Boolean = false
+  override fun isPasteEnabled(dataContext: DataContext): Boolean = false
+  override fun isCopyEnabled(dataContext: DataContext): Boolean = false
+  override fun isCopyVisible(dataContext: DataContext): Boolean = false
+  override fun isCutVisible(dataContext: DataContext): Boolean = false
+  override fun isPastePossible(dataContext: DataContext): Boolean = false
+}
