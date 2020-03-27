4f30696041a794bae741d1578e926099c9f8c15e
kunshen <kunshen@google.com>
2020-03-27 13:13:24 -0700

    Inject the list of enabled dynamic features
    Bug: 146497275
    Test: MakeBeforeRunTaskProviderTest
    Change-Id: Ibe23102a881bae0028f8a5ac29ea7f8decc5465c

--- android/src/com/android/tools/idea/gradle/run/MakeBeforeRunTaskProvider.java
+++ android/src/com/android/tools/idea/gradle/run/MakeBeforeRunTaskProvider.java
@@ -22,6 +22,7 @@ import static com.android.builder.model.AndroidProject.PROPERTY_BUILD_API_CODENA
 import static com.android.builder.model.AndroidProject.PROPERTY_BUILD_DENSITY;
 import static com.android.builder.model.AndroidProject.PROPERTY_DEPLOY_AS_INSTANT_APP;
 import static com.android.builder.model.AndroidProject.PROPERTY_EXTRACT_INSTANT_APK;
+import static com.android.builder.model.AndroidProject.PROPERTY_INJECTED_DYNAMIC_MODULES_LIST;
 import static com.android.tools.idea.gradle.util.AndroidGradleSettings.createProjectProperty;
 import static com.android.tools.idea.run.editor.ProfilerState.ANDROID_ADVANCED_PROFILING_TRANSFORMS;
 import static com.google.wireless.android.sdk.stats.GradleSyncStats.Trigger.TRIGGER_RUN_SYNC_NEEDED_BEFORE_RUNNING;
@@ -88,9 +89,11 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Properties;
+import java.util.Set;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
@@ -437,15 +440,23 @@ public class MakeBeforeRunTaskProvider extends BeforeRunTaskProvider<MakeBeforeR
       return Collections.emptyList();
     }
 
-    List<String> properties = new ArrayList<>(2);
+    List<String> properties = new ArrayList<>(3);
     if (useSelectApksFromBundleBuilder(modules, configuration, devices)) {
       // For the bundle tool, we create a temporary json file with the device spec and
       // pass the file path to the gradle task.
       File deviceSpecFile = deviceSpec.writeToJsonTempFile();
       properties.add(createProjectProperty(PROPERTY_APK_SELECT_CONFIG, deviceSpecFile.getAbsolutePath()));
       if (configuration instanceof AndroidAppRunConfigurationBase) {
+        AndroidAppRunConfigurationBase androidRunConfigurationbase = (AndroidAppRunConfigurationBase)configuration;
         if (((AndroidAppRunConfigurationBase)configuration).DEPLOY_AS_INSTANT) {
           properties.add(createProjectProperty(PROPERTY_EXTRACT_INSTANT_APK, true));
+        }
+
+        if (androidRunConfigurationbase.DEPLOY_APK_FROM_BUNDLE) {
+          String featureList = getEnabledDynamicFeatureList(modules, androidRunConfigurationbase);
+          if (!featureList.isEmpty()) {
+            properties.add(createProjectProperty(PROPERTY_INJECTED_DYNAMIC_MODULES_LIST, featureList));
+          }
         }
       }
     }
@@ -469,6 +480,22 @@ public class MakeBeforeRunTaskProvider extends BeforeRunTaskProvider<MakeBeforeR
       }
     }
     return properties;
+  }
+
+  @NotNull
+  private static String getEnabledDynamicFeatureList(@NotNull Module[] modules,
+                                                     @NotNull AndroidAppRunConfigurationBase configuration) {
+    Set<String> disabledFeatures = new HashSet<>(configuration.getDisabledDynamicFeatures());
+    return Arrays.stream(modules)
+      .flatMap(module -> DynamicAppUtils.getDependentFeatureModulesForBase(module).stream())
+      .map(Module::getName)
+      .filter(name -> !disabledFeatures.contains(name))
+      .map(moduleName -> {
+        // e.g name = "MyApplication.dynamicfeature"
+        int index = moduleName.lastIndexOf('.');
+        return index < 0 ? moduleName : moduleName.substring(index + 1);
+      })
+      .collect(Collectors.joining(","));
   }
 
   @NotNull
