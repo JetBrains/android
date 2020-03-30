88ae964164521cfe2ccbd9404f8834fff6f6b9ea
David Herman <davidherman@google.com>
2020-03-30 21:53:12 +0000

    Revert "Implement LiveStore inspector client and UI"
    This reverts commit bdc446f683111f612bf8140d9a18dc37626db259.
    
    Reason for revert: Causes compile issue for the team
    
    Change-Id: Ife9d2e4d7ed3fbbfde2c9ea9676b1eff9a739344

--- app-inspection/inspectors/livestore/model/src/com/android/tools/idea/appinspection/inspectors/livestore/model/LiveStoreInspectorClient.kt
+++ app-inspection/inspectors/livestore/model/src/com/android/tools/idea/appinspection/inspectors/livestore/model/LiveStoreInspectorClient.kt
@@ -15,87 +15,10 @@
  */
 package com.android.tools.idea.appinspection.inspectors.livestore.model
 
-import com.android.tools.idea.concurrency.transform
-import com.android.tools.appinspection.livestore.protocol.LiveStoreCommand
-import com.android.tools.appinspection.livestore.protocol.LiveStoreCommand.FetchAll
-import com.android.tools.appinspection.livestore.protocol.LiveStoreDefinition
-import com.android.tools.appinspection.livestore.protocol.LiveStoreEvent
-import com.android.tools.appinspection.livestore.protocol.LiveStoreResponse
 import com.android.tools.idea.appinspection.inspector.api.AppInspectorClient
-import com.google.gson.Gson
 
 class LiveStoreInspectorClient(messenger: CommandMessenger) : AppInspectorClient(messenger) {
-  private val gson = Gson()
-
-  private val _enumDefinitions = mutableMapOf<String, List<String>>()
-  val enumDefinitions: Map<String, List<String>> get() = _enumDefinitions
-
-  private val _stores = mutableListOf<LiveStoreDefinition>()
-  val stores: List<LiveStoreDefinition> get() = _stores
-
-  private val _storesChangedListeners = mutableListOf<() -> Unit>()
-  fun addStoresChangedListener(listener: () -> Unit) = _storesChangedListeners.add(listener)
-
-  private val _valueChangedListeners = mutableListOf<(ValueChangedEventArgs) -> Unit>()
-  fun addValueChangedListener(listener: (ValueChangedEventArgs) -> Unit) = _valueChangedListeners.add(listener)
-
-  init {
-    val command = LiveStoreCommand(fetchAll = FetchAll())
-    messenger.sendRawCommand(gson.toJson(command).toByteArray()).transform { response -> handleFetchAllResponse(response) }
-  }
-
   override val eventListener = object : EventListener {
-    override fun onRawEvent(eventData: ByteArray) {
-      val event = gson.fromJson(String(eventData), LiveStoreEvent::class.java)!!
-      event.valueUpdated?.let { valueUpdatedEvent ->
-        handleValueChanged(valueUpdatedEvent.store, valueUpdatedEvent.key, valueUpdatedEvent.value)
-      }
-    }
-
-    override fun onDispose() {
-      _storesChangedListeners.clear()
-      _valueChangedListeners.clear()
-    }
-  }
-
-  /**
-   * Make a request to the device to update a key/value pair on some target livestore.
-   *
-   * This request is asynchronous. If successful, listeners which subscribed to
-   * [addValueChangedListener] will be notified.
-   */
-  fun requestValueUpdate(storeName: String, keyName: String, newValue: String) {
-    val command = LiveStoreCommand(updateValue = LiveStoreCommand.UpdateValue(storeName, keyName, newValue))
-    messenger.sendRawCommand(gson.toJson(command).toByteArray()).transform { response -> handleUpdateValueResponse(response, newValue) }
-  }
-
-  private fun handleFetchAllResponse(responseBytes: ByteArray) {
-    val response: LiveStoreResponse = gson.fromJson(String(responseBytes), LiveStoreResponse::class.java)
-    response.fetchAll?.let { fetchAllResponse ->
-      fetchAllResponse.enums.forEach { _enumDefinitions[it.fqcn] = it.values }
-      _stores.addAll(fetchAllResponse.stores)
-      _storesChangedListeners.forEach { listener -> listener() }
-    }
-  }
-
-  private fun handleUpdateValueResponse(responseBytes: ByteArray, newValue: String) {
-    val response: LiveStoreResponse = gson.fromJson(String(responseBytes), LiveStoreResponse::class.java)
-    response.updateValue?.let { updateValueResponse ->
-      if (updateValueResponse.succeeded) {
-        // Only update our model after the device confirmed it was set
-        handleValueChanged(updateValueResponse.store, updateValueResponse.key, newValue)
-      }
-    }
-  }
-
-  private fun handleValueChanged(storeName: String, keyName: String, value: String) {
-    val storeDefinition = stores.firstOrNull { it.name == storeName } ?: return
-    val keyDefinition = storeDefinition.keyValues.firstOrNull { it.name == keyName } ?: return
-    val valueDefinition = keyDefinition.value
-
-    // Only update our model after the device confirmed it was set
-    valueDefinition.value = value
-    val args = ValueChangedEventArgs(storeName, keyName, value)
-    _valueChangedListeners.forEach { listener -> listener(args) }
+    // TODO: Implement handlers
   }
 }
