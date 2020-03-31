0114d084f6758337e9932c941c6a275ed37323c5
Yuriy Solodkyy <solodkyy@google.com>
2020-03-31 12:47:19 +0100

    Save documents after applying formatting while generating a project.
    Bug: 148087864
    Test: n/a
    Change-Id: Idd8b0ebf420331726960eedb1f31e4a822c12dcb

--- android/src/com/android/tools/idea/npw/model/NewTemplateRenderer.kt
+++ android/src/com/android/tools/idea/npw/model/NewTemplateRenderer.kt
@@ -68,7 +68,7 @@ fun Recipe.render(c: RenderingContext, e: RecipeExecutor, loggingEvent: Template
 
   if (!c.dryRun) {
     ApplicationManager.getApplication().invokeAndWait { PsiDocumentManager.getInstance(c.project).commitAllDocuments() }
-    EditorUtil.reformatAndRearrange(c.project, c.targetFiles)
+    EditorUtil.reformatRearrangeAndSave(c.project, c.targetFiles)
   }
 
   return success
