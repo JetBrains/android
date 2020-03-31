81a37d4cd2220ada98286d49b901494096808fce
Kainan Wang <knwang@google.com>
2020-03-31 16:51:45 +0000

    Restore device compatibility back to O+ devices.
    This is a revert of
    "Temporarily restrict device compatibility to Q+ devices"
    
    This reverts commit d36e3e139091b3e71c3f29900673ca1f008d27bf.
    
    Reason for revert: database inspector works on O devices
    
    Change-Id: Ib4a34fa60232ea52377aa98f44943f53362bf4bf

--- app-inspection/api/src/com/android/tools/idea/appinspection/api/AppInspectionDiscovery.kt
+++ app-inspection/api/src/com/android/tools/idea/appinspection/api/AppInspectionDiscovery.kt
@@ -185,8 +185,7 @@ class AppInspectionDiscoveryHost(
    * guaranteed to be true because transport pipeline only provides debuggable processes, so there is no need to check.
    */
   private fun ProcessDescriptor.isInspectable(): Boolean {
-    // TODO(b/152216552): Support O+ (API 26+) devices. Due to device-side issues, we only support API 29+ devices for now.
-    return stream.device.apiLevel >= AndroidVersion.VersionCodes.Q
+    return stream.device.apiLevel >= AndroidVersion.VersionCodes.O
   }
 
   /**
