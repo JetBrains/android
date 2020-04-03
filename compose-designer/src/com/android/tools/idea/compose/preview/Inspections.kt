a2f6d93e63886e30b2d6a0cbcd026d06da76d573
Diego Perez <diegoperez@google.com>
2020-04-03 10:47:00 +0100

    Refactor inspection to support @PreviewParameter
    Parameters with @PreviewParameter are supported and will not be flagged
    by the inspection.
    
    Test: Tests pass
    Bug: 150783335
    Change-Id: I135af904b4d9ca99e83c44e6af746c7206e679a4

--- compose-designer/src/com/android/tools/idea/compose/preview/Inspections.kt
+++ compose-designer/src/com/android/tools/idea/compose/preview/Inspections.kt
@@ -20,6 +20,7 @@ import com.android.tools.idea.compose.preview.util.HEIGHT_PARAMETER
 import com.android.tools.idea.compose.preview.util.MAX_HEIGHT
 import com.android.tools.idea.compose.preview.util.MAX_WIDTH
 import com.android.tools.idea.compose.preview.util.PREVIEW_ANNOTATION_FQN
+import com.android.tools.idea.compose.preview.util.PREVIEW_PARAMETER_FQN
 import com.android.tools.idea.compose.preview.util.WIDTH_PARAMETER
 import com.android.tools.idea.compose.preview.util.isValidPreviewLocation
 import com.android.tools.idea.flags.StudioFlags
@@ -36,6 +37,7 @@ import org.jetbrains.kotlin.idea.inspections.AbstractKotlinInspection
 import org.jetbrains.kotlin.psi.KtAnnotationEntry
 import org.jetbrains.kotlin.psi.KtImportDirective
 import org.jetbrains.kotlin.psi.KtNamedFunction
+import org.jetbrains.kotlin.psi.KtParameter
 import org.jetbrains.kotlin.psi.KtValueArgument
 import org.jetbrains.kotlin.psi.KtVisitorVoid
 
@@ -98,6 +100,15 @@ abstract class BasePreviewAnnotationInspection : AbstractKotlinInspection() {
 }
 
 /**
+ * Returns whether the [KtParameter] can be used in the preview. This will return true if the parameter
+ * has a default value or a value provider.
+ */
+private fun KtParameter.isAcceptableForPreview(): Boolean =
+  hasDefaultValue() ||
+  // When data sources are enabled, we also accept parameters with the @PreviewParameter annotation
+  (StudioFlags.COMPOSE_PREVIEW_DATA_SOURCES.get() && annotationEntries.any { it.fqNameMatches(PREVIEW_PARAMETER_FQN) })
+
+/**
  * Inspection that checks that any function annotated with `@Preview` does not have parameters.
  */
 class PreviewAnnotationInFunctionWithParametersInspection : BasePreviewAnnotationInspection() {
@@ -106,9 +117,13 @@ class PreviewAnnotationInFunctionWithParametersInspection : BasePreviewAnnotatio
   override fun visitPreviewAnnotatedFunction(holder: ProblemsHolder,
                                              function: KtNamedFunction,
                                              previewAnnotation: KtAnnotationEntry) {
-    if (function.valueParameters.any { !it.hasDefaultValue() }) {
+    if (function.valueParameters.any { !it.isAcceptableForPreview() }) {
+      val description = if (StudioFlags.COMPOSE_PREVIEW_DATA_SOURCES.get())
+        message("inspection.no.parameters.or.provider.description")
+      else
+        message("inspection.no.parameters.description")
       holder.registerProblem(previewAnnotation.psiOrParent as PsiElement,
-                             message("inspection.no.parameters.description"),
+                             description,
                              ProblemHighlightType.ERROR)
     }
   }
