e93cfa60526d1c5c6eb81b2db10314b8dfb67624
kovalp <kovalp@google.com>
2020-04-07 11:12:25 +0100

    [Dagger] Makes serializeAttrValueToString return null if we can't reliably serialize value.
    If serializeAttrValueToString returns null for any attribute
    qualifier is considered invalid and will not be returned.
    It's follow up to change I9c33d406ecfffc83b470d3240872180a58fd724d.
    
    Bug: 152481333
    Test: n/a
    Change-Id: I3a6147719f6199b3ef7e509cc75bc7da6728bdf9

--- dagger/src/com/android/tools/idea/dagger/DaggerQualifierUtil.kt
+++ dagger/src/com/android/tools/idea/dagger/DaggerQualifierUtil.kt
@@ -36,21 +36,32 @@ import org.jetbrains.kotlin.psi.KtAnnotationEntry
 import org.jetbrains.kotlin.resolve.BindingContext
 import org.jetbrains.kotlin.resolve.constants.AnnotationValue
 import org.jetbrains.kotlin.resolve.constants.ArrayValue
+import org.jetbrains.kotlin.resolve.constants.BooleanValue
 import org.jetbrains.kotlin.resolve.constants.ConstantValue
+import org.jetbrains.kotlin.resolve.constants.DoubleValue
 import org.jetbrains.kotlin.resolve.constants.EnumValue
+import org.jetbrains.kotlin.resolve.constants.FloatValue
+import org.jetbrains.kotlin.resolve.constants.IntegerValueConstant
 import org.jetbrains.kotlin.resolve.constants.KClassValue
+import org.jetbrains.kotlin.resolve.constants.NullValue
+import org.jetbrains.kotlin.resolve.constants.StringValue
 import org.jetbrains.kotlin.resolve.descriptorUtil.annotationClass
 import org.jetbrains.kotlin.resolve.lazy.BodyResolveMode
 
-const val QUALIFIER_ANNOTATION = "javax.inject.Qualifier"
+const val QUALIFIER_ANNOTATION_CLASS_FQ = "javax.inject.Qualifier"
 
 /**
- * Contains an info about a qualifier that assigned to Dagger consumer/provider.
+ * Contains a serialized representation of a qualifier annotation.
  *
- * Qualifier is an annotation that has [QUALIFIER_ANNOTATION] annotation.
+ * Includes qualifier's fqcn and serialized representation of attribute values.
+ *
+ * Qualifier is an annotation that has [QUALIFIER_ANNOTATION_CLASS_FQ] annotation.
  *
  * [fqName] - fq name of qualifier
  * [attributes] - map from attribute name to serialized value
+ *
+ * We need to store the annotations values in their serialized form because we don't have a common PSI representation between Kotlin
+ * and Java PSIs.
  *
  * Serialization convention:
  * enum -> "enum-fqn.enum-fieldName"
@@ -59,19 +70,30 @@ const val QUALIFIER_ANNOTATION = "javax.inject.Qualifier"
  * primitive Types/String -> "x.toString()"
  * array -> "QualifierInfo.attrValueToString(array[0]), "QualifierInfo.attrValueToString(array[1]) .."
  *
- * @see [attrValueToString]
+ * @see [serializeAttrValueToString]
  */
 internal data class QualifierInfo(val fqName: String, val attributes: Map<String, String>)
+
+fun isConstantValueSerializableAsString(constant: ConstantValue<*>): Boolean {
+  return constant is StringValue ||
+         constant is BooleanValue ||
+         constant is DoubleValue ||
+         constant is FloatValue ||
+         constant is IntegerValueConstant<*> ||
+         constant is NullValue
+}
 
 /**
  * Converts a [ConstantValue] to a [String].
  *
  * [ConstantValue] can be an enum, primitive type or an annotation, String, or Class object.
  * It can also be an array of these types.
+ *
+ * Returns `null` if we can't reliably serialize value.
  */
-private fun attrValueToString(value: ConstantValue<*>): String =
+private fun serializeAttrValueToString(value: ConstantValue<*>): String? =
   when (value) {
-    is ArrayValue -> value.value.joinToString { attrValueToString(it) }
+    is ArrayValue -> value.value.map { serializeAttrValueToString(it) ?: return null }.joinToString()
     is KClassValue -> {
       val kotlinFqName = (value.value as? KClassValue.Value.NormalClass)?.value?.classId?.asSingleFqName()?.asString()
       // Try to map Kotlin fqcn to Java fqcn, e.g kotlin.String -> java.lang.String.
@@ -79,9 +101,8 @@ private fun attrValueToString(value: ConstantValue<*>): String =
     }
     is AnnotationValue -> value.value.fqName?.asString()
     is EnumValue -> "${value.enumClassId.asSingleFqName().asString()}.${value.enumEntryName}"
-    // Brunch for all primitive types and String, e.g [BooleanValue]
-    else -> value.value.toString()
-  } ?: ""
+    else -> if (isConstantValueSerializableAsString(value)) value.value.toString() else null
+  }
 
 
 /**
@@ -89,20 +110,24 @@ private fun attrValueToString(value: ConstantValue<*>): String =
  *
  * [JvmAnnotationAttributeValue] can be an enum, primitive type or an annotation, String, or Class object.
  * It can also be an array of these types.
+ *
+ * Returns `null` if we can't reliably serialize value.
  */
-private fun attrValueToString(value: JvmAnnotationAttributeValue?): String =
+private fun serializeAttrValueToString(value: JvmAnnotationAttributeValue?): String? =
   when (value) {
-    is JvmAnnotationArrayValue -> value.values.joinToString { attrValueToString(it) }
+    is JvmAnnotationArrayValue -> value.values.map { serializeAttrValueToString(it) ?: return null }.joinToString()
     is JvmAnnotationConstantValue -> value.constantValue.toString()
     is JvmAnnotationClassValue -> value.qualifiedName
     is JvmNestedAnnotationValue -> value.value.qualifiedName
     is JvmAnnotationEnumFieldValue -> "${value.containingClassName}.${value.fieldName}"
-    else -> ""
-  } ?: ""
+    else -> null
+  }
 
 
 /**
- * Returns a [QualifierInfo] for a given [PsiElement] if a qualifier presents and it's only one otherwise returns null.
+ * Returns a [QualifierInfo] for a given [PsiElement] if a qualifier presents and it's only one otherwise returns `null`.
+ *
+ * Returns `null` if any attr value of Qualifier can't be serialized value. See [serializeAttrValueToString]
  */
 internal fun PsiElement.getQualifierInfo(): QualifierInfo? =
   when (this) {
@@ -127,10 +152,13 @@ internal fun <T : PsiModifierListOwner> Collection<T>.filterByQualifier(
 private fun KtAnnotated.getQualifierInfoFromKtAnnotated(): QualifierInfo? {
   val annotationDescriptors = annotationEntries.mapNotNull { it.getDescriptor() }
   val qualifiers = annotationDescriptors.filter { it.isQualifier }
+  // It is always an error to apply multiple qualifiers. Qualifier is valid only if it's single.
   if (qualifiers.size == 1) {
-    val qualifier = qualifiers.first()
+    val qualifier = qualifiers.single()
     val qualifierFqName = qualifier.fqName?.asString() ?: return null
-    val qualifierAttributes = qualifier.allValueArguments.map { it.key.asString() to attrValueToString(it.value) }.toMap()
+    val qualifierAttributes = qualifier.allValueArguments.map {
+      it.key.asString() to (serializeAttrValueToString(it.value) ?: return@getQualifierInfoFromKtAnnotated null)
+    }.toMap()
     return QualifierInfo(qualifierFqName, qualifierAttributes)
   }
   return null
@@ -138,9 +166,12 @@ private fun KtAnnotated.getQualifierInfoFromKtAnnotated(): QualifierInfo? {
 
 private fun PsiModifierListOwner.getQualifierInfoFromPsiModifierListOwner(): QualifierInfo? {
   val qualifiers = annotations.filter { it.isQualifier }
+  // It is always an error to apply multiple qualifiers. Qualifier is valid only if it's single.
   if (qualifiers.size == 1) {
     val qualifierFqName = qualifiers.first().qualifiedName ?: return null
-    val qualifierAttributes = qualifiers.first().attributes.map { it.attributeName to attrValueToString(it.attributeValue) }.toMap()
+    val qualifierAttributes = qualifiers.first().attributes.map {
+      it.attributeName to (serializeAttrValueToString(it.attributeValue) ?: return@getQualifierInfoFromPsiModifierListOwner null)
+    }.toMap()
     return QualifierInfo(qualifierFqName, qualifierAttributes)
   }
   return null
@@ -149,10 +180,10 @@ private fun PsiModifierListOwner.getQualifierInfoFromPsiModifierListOwner(): Qua
 private fun KtAnnotationEntry.getDescriptor() = analyze(BodyResolveMode.PARTIAL).get(BindingContext.ANNOTATION, this)
 
 private val AnnotationDescriptor.isQualifier: Boolean
-  get() = annotationClass?.annotations?.hasAnnotation(FqName(QUALIFIER_ANNOTATION)) == true
+  get() = annotationClass?.annotations?.hasAnnotation(FqName(QUALIFIER_ANNOTATION_CLASS_FQ)) == true
 
 private val PsiAnnotation.isQualifier: Boolean
   get() {
     val cls = (this.nameReferenceElement?.resolve() as? PsiClass) ?: return false
-    return cls.isAnnotationType && cls.hasAnnotation(QUALIFIER_ANNOTATION)
+    return cls.isAnnotationType && cls.hasAnnotation(QUALIFIER_ANNOTATION_CLASS_FQ)
   }
