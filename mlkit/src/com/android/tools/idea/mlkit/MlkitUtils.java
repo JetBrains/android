913c9132ac521851db6ab5b927dd704e5d5cb1af
Yi Zhou <deanzhou@google.com>
2020-04-08 14:08:06 -0700

    Fix some ml folder issues related to build variants
    It includes:
    1. Auto moving model file may direct it to the ml folder from differnt build variant
    2. Model file in inactive build variant is not properly recognized
    
    Deletes MlkitUtilsTest as existing tests are outdated after this and
    we can add the test class back once we have meaningful tests added.
    
    Bug: 153573353
    Test: manual
    Change-Id: Iba97aee2b28f4b31693a797db7368d61a4f8445f

--- mlkit/src/com/android/tools/idea/mlkit/MlkitUtils.java
+++ mlkit/src/com/android/tools/idea/mlkit/MlkitUtils.java
@@ -18,25 +18,20 @@ package com.android.tools.idea.mlkit;
 import com.android.ide.common.repository.GradleCoordinate;
 import com.android.tools.idea.mlkit.lightpsi.LightModelClass;
 import com.android.tools.idea.projectsystem.AndroidModuleSystem;
-import com.android.tools.idea.projectsystem.NamedModuleTemplate;
 import com.android.tools.idea.projectsystem.ProjectSystemUtil;
 import com.android.tools.idea.projectsystem.SourceProviders;
 import com.google.common.collect.ImmutableList;
-import com.google.common.collect.Sets;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.module.ModuleUtilCore;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VfsUtilCore;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiClass;
-import java.io.File;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import org.jetbrains.android.facet.AndroidFacet;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 /**
  * Provides common utility methods.
@@ -52,18 +47,14 @@ public class MlkitUtils {
 
   public static boolean isModelFileInMlModelsFolder(@NotNull Module module, @NotNull VirtualFile file) {
     AndroidFacet androidFacet = AndroidFacet.getInstance(module);
-    if (androidFacet == null || file.getFileType() != TfliteModelFileType.INSTANCE) {
-      return false;
-    }
-
-    Collection<VirtualFile> mlModelsDirectories = SourceProviders.getInstance(androidFacet).getSources().getMlModelsDirectories();
-    if (VfsUtilCore.isUnder(file, Sets.newHashSet(mlModelsDirectories))) {
-      return true;
-    }
-
-    return false;
+    return androidFacet != null &&
+           file.getFileType() == TfliteModelFileType.INSTANCE &&
+           SourceProviders.getInstance(androidFacet).getCurrentAndSomeFrequentlyUsedInactiveSourceProviders().stream()
+             .flatMap(sourceProvider -> sourceProvider.getMlModelsDirectories().stream())
+             .anyMatch(mlDir -> VfsUtilCore.isAncestor(mlDir, file, true));
   }
 
+  @NotNull
   public static PsiClass[] getLightModelClasses(@NotNull Project project, @NotNull Map<VirtualFile, MlModelMetadata> modelFileMap) {
     List<PsiClass> lightModelClassList = new ArrayList<>();
     for (Map.Entry<VirtualFile, MlModelMetadata> metadata : modelFileMap.entrySet()) {
@@ -84,6 +75,7 @@ public class MlkitUtils {
   /**
    * Returns the set of missing dependencies that are required by the auto-generated model classes.
    */
+  @NotNull
   public static List<GradleCoordinate> getMissingDependencies(@NotNull Module module, @NotNull VirtualFile modelFile) {
     AndroidModuleSystem moduleSystem = ProjectSystemUtil.getModuleSystem(module);
     List<GradleCoordinate> pendingDeps = new ArrayList<>();
@@ -97,6 +89,7 @@ public class MlkitUtils {
     return pendingDeps;
   }
 
+  @NotNull
   public static ImmutableList<String> getRequiredDependencies() {
     // TODO(148887002): calculate required deps based on the given model file and figure out how to handle versions.
     return ImmutableList.of(
@@ -104,21 +97,5 @@ public class MlkitUtils {
       "org.tensorflow:tensorflow-lite:2.1.0",
       "org.tensorflow:tensorflow-lite-support:0.0.0-nightly"
     );
-  }
-
-  @Nullable
-  public static File getModuleMlDirectory(@NotNull List<NamedModuleTemplate> namedModuleTemplates) {
-    if (namedModuleTemplates.isEmpty()) {
-      return null;
-    }
-
-    // Find target ml path given module templates. If there are multiple, select the main template.
-    // TODO(b/150616631): Add drop down UI to let user select the flavour.
-    List<File> mlDirectories = namedModuleTemplates.get(0).getPaths().getMlModelsDirectories();
-    if (mlDirectories.isEmpty()) {
-      return null;
-    }
-
-    return mlDirectories.get(0);
   }
 }
