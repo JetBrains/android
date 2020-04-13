0825ae536f7fc35f329d34a49d7d8b3e5f773bfe
Bradley Smith <smithbradley@google.com>
2020-04-13 14:33:17 -0700

    [Gradle Sync] Move notification disable to resolver
    Bug: N/A
    Test: moved
    Change-Id: I8fd93fd4d341e1a155eb3b519b57ab1f38174b09

--- android/src/com/android/tools/idea/gradle/project/sync/cleanup/PreSyncProjectCleanUp.java
+++ android/src/com/android/tools/idea/gradle/project/sync/cleanup/PreSyncProjectCleanUp.java
@@ -29,7 +29,7 @@ public class PreSyncProjectCleanUp {
   }
 
   public PreSyncProjectCleanUp() {
-    this(new ProjectNotificationsCleanUpTask(), new ProjectPreferencesCleanUpTask(),
+    this(new ProjectPreferencesCleanUpTask(),
          new HttpProxySettingsCleanUpTask(), new GradleDistributionCleanUpTask());
   }
 
