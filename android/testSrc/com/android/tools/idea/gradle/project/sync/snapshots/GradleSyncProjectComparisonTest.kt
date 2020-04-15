d56b90185d82c3c408282b24b40fbc0b196f4d68
Yuriy Solodkyy <solodkyy@google.com>
2020-04-15 18:36:43 +0100

    Move Gradle sync project-snapshot-comparison-tests to their own target
    to avoid interference with other tests via Gradle cache. The tests
    will be moved to `project-system-gradle-sync-tests` module after.
    
    Bug: 147586218
    Test: n/a
    Change-Id: I679c2a459902fd5c1e07315dfc635058167e2cee

--- android/testSrc/com/android/tools/idea/gradle/project/sync/snapshots/GradleSyncProjectComparisonTest.kt
+++ android/testSrc/com/android/tools/idea/gradle/project/sync/snapshots/GradleSyncProjectComparisonTest.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2019 The Android Open Source Project
+ * Copyright (C) 2020 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,11 +13,13 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.android.tools.idea.gradle.project.sync
+package com.android.tools.idea.gradle.project.sync.snapshots
 
 import com.android.SdkConstants.FN_SETTINGS_GRADLE
 import com.android.builder.model.SyncIssue
 import com.android.tools.idea.gradle.project.importing.GradleProjectImporter
+import com.android.tools.idea.gradle.project.sync.GradleSyncIntegrationTestCase
+import com.android.tools.idea.gradle.project.sync.GradleSyncInvoker
 import com.android.tools.idea.gradle.structure.model.PsProjectImpl
 import com.android.tools.idea.gradle.structure.model.android.asParsed
 import com.android.tools.idea.gradle.variant.view.BuildVariantUpdater
