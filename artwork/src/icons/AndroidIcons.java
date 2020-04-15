39eebe0c7cd58bdfd848b05cf4e6270d53c91979
Parentej <parentej@google.com>
2020-04-15 15:13:50 +0100

    [NPW] Remove un-used Template icons
    Bug: N/A
    Test: UI tests
    Change-Id: Ia2a73018c00cd1d47371dac7ab9dc5a2ba91ac7e

--- artwork/src/icons/AndroidIcons.java
+++ artwork/src/icons/AndroidIcons.java
@@ -33,9 +33,6 @@ public class AndroidIcons {
     public static final Icon ThingsModule = load("/icons/wizards/things_module.png"); // 256x256
     public static final Icon TvModule = load("/icons/wizards/tv_module.png"); // 256x256
     public static final Icon WearModule = load("/icons/wizards/wear_module.png"); // 256x256
-    public static final Icon NavigationDrawer = load("/icons/wizards/navigation/navigation_drawer.png"); // 256x256
-    public static final Icon BottomNavigation = load("/icons/wizards/navigation/bottom_navigation.png"); // 256x256
-    public static final Icon NavigationTabs = load("/icons/wizards/navigation/navigation_tabs.png"); // 256x256
     public static final Icon CppConfiguration = load("/icons/wizards/cpp_configure.png"); // 256x256
     public static final Icon NoActivity = load("/icons/wizards/no_activity.png"); // 256x256
   }
