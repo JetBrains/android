ea8dcb8b5f3a0dd2aa01e4cbf4e66297db0e9aec
jedo <jedo@google.com>
2020-04-21 20:46:45 -0700

    resValues was removed from AndroidArtifact interface
    the method is still in the stub for compatibility reasons, it's
    been returning an empty map before so no functional changes.
    
    Test: Exisiting
    Bug: N/A
    Change-Id: I116b3fa319a972430e9baab0dfbc60c20caf2b1a

--- android/testSrc/com/android/tools/idea/gradle/stubs/android/AndroidArtifactStub.java
+++ android/testSrc/com/android/tools/idea/gradle/stubs/android/AndroidArtifactStub.java
@@ -117,7 +117,10 @@ public class AndroidArtifactStub extends BaseArtifactStub implements IdeAndroidA
     return myNativeLibraries;
   }
 
-  @Override
+  /**
+   * Removed from the model in 4.1.0
+   */
+  @Deprecated
   @NotNull
   public Map<String, ClassField> getResValues() {
     return Collections.emptyMap();
