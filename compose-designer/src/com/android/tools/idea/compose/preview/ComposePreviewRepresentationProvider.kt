a5d656ffb36aa8891998d7f2cf0b2bd705046713
Diego Perez <diegoperez@google.com>
2020-04-24 17:05:06 +0100

    Move Compose test to use MultiRepresentation
    The old path is not used so move the test to test the new path for
    instantiating editors.
    
    Bug: N/A
    Test: Updates test
    Change-Id: I18fc20723a6f8983760c245e7be66c806f40717b

--- compose-designer/src/com/android/tools/idea/compose/preview/ComposePreviewRepresentationProvider.kt
+++ compose-designer/src/com/android/tools/idea/compose/preview/ComposePreviewRepresentationProvider.kt
@@ -15,11 +15,25 @@
  */
 package com.android.tools.idea.compose.preview
 
+import com.android.tools.idea.common.actions.IssueNotificationAction
+import com.android.tools.idea.common.editor.ToolbarActionGroups
+import com.android.tools.idea.common.model.NlComponent
+import com.android.tools.idea.common.surface.DesignSurface
+import com.android.tools.idea.common.type.DesignerTypeRegistrar
+import com.android.tools.idea.compose.preview.actions.ForceCompileAndRefreshAction
+import com.android.tools.idea.compose.preview.actions.GroupSwitchAction
+import com.android.tools.idea.compose.preview.actions.ShowDebugBoundaries
+import com.android.tools.idea.compose.preview.actions.ToggleAutoBuildAction
+import com.android.tools.idea.compose.preview.util.ComposeAdapterLightVirtualFile
 import com.android.tools.idea.compose.preview.util.FilePreviewElementFinder
 import com.android.tools.idea.compose.preview.util.PreviewElement
 import com.android.tools.idea.compose.preview.util.isKotlinFileType
 import com.android.tools.idea.flags.StudioFlags
 import com.android.tools.idea.uibuilder.editor.multirepresentation.PreviewRepresentationProvider
+import com.android.tools.idea.uibuilder.type.LayoutEditorFileType
+import com.google.wireless.android.sdk.stats.LayoutEditorState
+import com.intellij.openapi.actionSystem.ActionGroup
+import com.intellij.openapi.actionSystem.DefaultActionGroup
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.project.DumbService
 import com.intellij.openapi.project.IndexNotReadyException
@@ -28,12 +42,52 @@ import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.PsiFile
 
 /**
+ * [ToolbarActionGroups] that includes the [ForceCompileAndRefreshAction]
+ */
+private class ComposePreviewToolbar(private val surface: DesignSurface) :
+  ToolbarActionGroups(surface) {
+
+  override fun getNorthGroup(): ActionGroup = DefaultActionGroup(
+    listOfNotNull(
+      GroupSwitchAction(),
+      if (StudioFlags.COMPOSE_PREVIEW_AUTO_BUILD.get()) ToggleAutoBuildAction() else null,
+      ForceCompileAndRefreshAction(surface),
+      if (StudioFlags.COMPOSE_DEBUG_BOUNDS.get()) ShowDebugBoundaries() else null
+    )
+  )
+
+  override fun getNorthEastGroup(): ActionGroup = DefaultActionGroup().apply {
+    addAll(getZoomActionsWithShortcuts(surface, this@ComposePreviewToolbar))
+    add(IssueNotificationAction.getInstance())
+  }
+}
+
+/**
  * A [PreviewRepresentationProvider] coupled with [ComposePreviewRepresentation].
  */
 class ComposePreviewRepresentationProvider(
   private val filePreviewElementProvider: () -> FilePreviewElementFinder = ::defaultFilePreviewElementFinder
 ) : PreviewRepresentationProvider {
   private val LOG = Logger.getInstance(ComposePreviewRepresentationProvider::class.java)
+
+  private object ComposeEditorFileType : LayoutEditorFileType() {
+    override fun getLayoutEditorStateType() = LayoutEditorState.Type.COMPOSE
+
+    override fun isResourceTypeOf(file: PsiFile): Boolean =
+      file.virtualFile is ComposeAdapterLightVirtualFile
+
+    override fun getToolbarActionGroups(surface: DesignSurface): ToolbarActionGroups =
+      ComposePreviewToolbar(surface)
+
+    override fun getSelectionContextToolbar(surface: DesignSurface, selection: List<NlComponent>): DefaultActionGroup =
+      DefaultActionGroup()
+  }
+
+  init {
+    if (StudioFlags.COMPOSE_PREVIEW.get()) {
+      DesignerTypeRegistrar.register(ComposeEditorFileType)
+    }
+  }
 
   /**
    * Checks if the input [virtualFile] contains compose previews and therefore can be provided with the [PreviewRepresentation] of them.
