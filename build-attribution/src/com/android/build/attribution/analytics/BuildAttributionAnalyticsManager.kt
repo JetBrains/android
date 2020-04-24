ff2fd8a23e4bf4007f8304397514e2c0645358e1
Amr Afifiy <amrmahmoud@google.com>
2020-04-24 14:54:32 +0100

    [Build Analyzer] Differentiate between buildSrc and binary plugins
    The list of plugins defined in the buildSrc is sent in the build
    attribution report sent from agp
    
    Fixes: 144849108
    Test: testAlwaysRunTasksForBuildSrcPlugin
    Change-Id: I424cd633669d5d35d6e5004ddf87443e29e62f0b

--- build-attribution/src/com/android/build/attribution/analytics/BuildAttributionAnalyticsManager.kt
+++ build-attribution/src/com/android/build/attribution/analytics/BuildAttributionAnalyticsManager.kt
@@ -125,7 +125,9 @@ class BuildAttributionAnalyticsManager(
   private fun transformPluginType(pluginType: PluginData.PluginType) =
     when (pluginType) {
       PluginData.PluginType.UNKNOWN -> BuildAttributionPluginIdentifier.PluginType.UNKNOWN_TYPE
-      PluginData.PluginType.PLUGIN -> BuildAttributionPluginIdentifier.PluginType.BINARY_PLUGIN
+      // In 4.0 and before adding the distinction between buildSrc and binary plugins, both of them were treated as binary plugins
+      // TODO: update the proto to add two new fields for buildSrc and binary plugins
+      PluginData.PluginType.BINARY_PLUGIN, PluginData.PluginType.BUILDSRC_PLUGIN -> BuildAttributionPluginIdentifier.PluginType.BINARY_PLUGIN
       PluginData.PluginType.SCRIPT -> BuildAttributionPluginIdentifier.PluginType.BUILD_SCRIPT
     }
 
