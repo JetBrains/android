e2ffd26a44e386c3b8c2ce88671064d5e7ddb905
Yuriy Solodkyy <solodkyy@google.com>
2020-04-27 21:46:09 +0100

    Fix open project in various places
    Bug: n/a
    Test: UI tests
    Change-Id: I622fbdca9177d81e02b0c4600b5b25e4052f55bc

--- android/src/com/android/tools/idea/gradle/project/importing/AndroidGradleProjectImportProvider.java
+++ android/src/com/android/tools/idea/gradle/project/importing/AndroidGradleProjectImportProvider.java
@@ -78,7 +78,7 @@ public class AndroidGradleProjectImportProvider extends ProjectImportProvider {
                                ModifiableModuleModel model,
                                ModulesProvider modulesProvider,
                                ModifiableArtifactModel artifactModel) {
-      GradleProjectImporter.getInstance().importProjectCore(project.getBaseDir());
+      GradleProjectImporter.getInstance().importAndOpenProjectCore(null, true, project.getBaseDir());
       return Collections.emptyList();
     }
   }
