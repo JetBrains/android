c2a21db1d437d0e639d83b30c43b6d0505662b7c
Matthew Gharrity <gharrma@google.com>
2020-04-28 16:07:59 -0400

    Create fewer annotations in AidlClassNameAnnotator
    Test: existing
    Change-Id: Ia3f2b36883ffaf6466297692bfed9a507d6a5ec8

--- android-lang/src/com/android/tools/idea/lang/aidl/highlight/AidlClassNameAnnotator.java
+++ android-lang/src/com/android/tools/idea/lang/aidl/highlight/AidlClassNameAnnotator.java
@@ -20,7 +20,6 @@ import com.android.tools.idea.lang.aidl.psi.AidlDeclarationName;
 import com.android.tools.idea.lang.aidl.psi.AidlInterfaceDeclaration;
 import com.android.tools.idea.lang.aidl.psi.AidlMethodDeclaration;
 import com.android.tools.idea.lang.aidl.psi.AidlParcelableDeclaration;
-import com.intellij.lang.annotation.AnnotationBuilder;
 import com.intellij.lang.annotation.AnnotationHolder;
 import com.intellij.lang.annotation.Annotator;
 import com.intellij.lang.annotation.HighlightSeverity;
@@ -35,20 +34,23 @@ public class AidlClassNameAnnotator implements Annotator {
 
   @Override
   public void annotate(@NotNull PsiElement element, @NotNull AnnotationHolder holder) {
-    AnnotationBuilder builder = holder.newSilentAnnotation(HighlightSeverity.INFORMATION);
-
     if (element instanceof AidlClassOrInterfaceType) {
-      builder = builder.textAttributes(DefaultLanguageHighlighterColors.CLASS_REFERENCE);
+      holder.newSilentAnnotation(HighlightSeverity.INFORMATION)
+        .textAttributes(DefaultLanguageHighlighterColors.CLASS_REFERENCE)
+        .create();
     }
     else if (element instanceof AidlDeclarationName) {
       PsiElement component = element.getParent();
       if (component instanceof AidlInterfaceDeclaration || component instanceof AidlParcelableDeclaration) {
-        builder = builder.textAttributes(DefaultLanguageHighlighterColors.CLASS_NAME);
+        holder.newSilentAnnotation(HighlightSeverity.INFORMATION)
+          .textAttributes(DefaultLanguageHighlighterColors.CLASS_NAME)
+          .create();
       }
       else if (element instanceof AidlMethodDeclaration) {
-        builder = builder.textAttributes(DefaultLanguageHighlighterColors.FUNCTION_DECLARATION);
+        holder.newSilentAnnotation(HighlightSeverity.INFORMATION)
+          .textAttributes(DefaultLanguageHighlighterColors.FUNCTION_DECLARATION)
+          .create();
       }
     }
-    builder.create();
   }
 }
