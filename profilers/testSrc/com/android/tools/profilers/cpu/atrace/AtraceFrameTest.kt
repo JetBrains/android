201f1ed7c5f1fc5b4c72965133bc4fc0d1564909
Bruno Buss <buss@google.com>
2020-04-29 16:51:01 +0100

    Simplify AtraceFrame as we only add one Slice per Frame
    A lot of AtraceFrame class logic was dynamic, based on the fact
    that a bunch of properties could change when addSlice() was called.
    
    But right now we only add one slice per frame, so we can simplify a
    lot of the logic inside the class. The only mutability inside the class
    right now is when we set an associated frame.
    
    Bug: cleanup
    Test: existing
    Change-Id: I92149f19326de22509de5b1ce0a50d1047fb97cc

--- profilers/testSrc/com/android/tools/profilers/cpu/atrace/AtraceFrameTest.kt
+++ profilers/testSrc/com/android/tools/profilers/cpu/atrace/AtraceFrameTest.kt
@@ -15,76 +15,36 @@
  */
 package com.android.tools.profilers.cpu.atrace
 
-import com.android.tools.adtui.model.Range
-import com.android.tools.profilers.cpu.CpuProfilerTestUtils
-import com.android.tools.profilers.cpu.atrace.AtraceTestUtils.Companion.DELTA
 import com.android.tools.profilers.cpu.atrace.AtraceTestUtils.Companion.SECONDS_TO_US
-import com.android.tools.profilers.cpu.atrace.AtraceTestUtils.Companion.TEST_PID
 import com.android.tools.profilers.cpu.atrace.AtraceTestUtils.Companion.convertTimeStamps
 import com.google.common.truth.Truth.assertThat
-import org.junit.Before
 import org.junit.Test
-import trebuchet.model.Model
-import trebuchet.task.ImportTask
-import trebuchet.util.PrintlnImportFeedback
+import trebuchet.model.SchedSlice
+import trebuchet.model.base.SliceGroup
 
 class AtraceFrameTest {
 
-  private lateinit var myModel: Model;
-
-  @Before
-  fun setup() {
-    val file = CpuProfilerTestUtils.getTraceFile("atrace.ctrace")
-    val reader = AtraceProducer()
-    assertThat(reader.parseFile(file)).isTrue()
-    val task = ImportTask(PrintlnImportFeedback())
-    myModel = task.importBuffer(reader)
+  @Test
+  fun testFramePerformance_Good() {
+    val goodSlice = TestSliceGroup(SECONDS_TO_US * 0.25, SECONDS_TO_US * 0.75, SECONDS_TO_US * 0.6)
+    val frame = AtraceFrame(goodSlice, ::convertTimeStamps, SECONDS_TO_US.toLong() * 1, AtraceFrame.FrameThread.MAIN)
+    assertThat(frame.perfClass).isEqualTo(AtraceFrame.PerfClass.GOOD)
   }
 
   @Test
-  fun testFramePerformance() {
-    val bitmapsProcess = myModel.processes[TEST_PID]!!
-    val frame = AtraceFrame(bitmapsProcess.threads[0].id, ::convertTimeStamps, SECONDS_TO_US.toLong() * 1, AtraceFrame.FrameThread.MAIN)
-    val goodFrameRange = Range(1.0, 1.01)
-    val badFrameRange = Range(1.0, 10.0)
-    frame.addSlice(bitmapsProcess.threads[0].slices[0], goodFrameRange)
-    assertThat(frame.perfClass).isEqualTo(AtraceFrame.PerfClass.GOOD)
-    frame.addSlice(bitmapsProcess.threads[0].slices[1], badFrameRange)
+  fun testFramePerformance_Bad() {
+    val badSlice = TestSliceGroup(SECONDS_TO_US * 0.25, SECONDS_TO_US * 1.5, SECONDS_TO_US * 0.9)
+    val frame = AtraceFrame(badSlice, ::convertTimeStamps, SECONDS_TO_US.toLong() * 1, AtraceFrame.FrameThread.MAIN)
     assertThat(frame.perfClass).isEqualTo(AtraceFrame.PerfClass.BAD)
   }
 
   @Test
-  fun testRangeValues() {
-    val bitmapsProcess = myModel.processes[TEST_PID]!!
-    val frame = AtraceFrame(bitmapsProcess.threads[0].id, ::convertTimeStamps, 0, AtraceFrame.FrameThread.MAIN)
-    val smallRange = Range(1.0 + myModel.beginTimestamp, 2.0 + myModel.beginTimestamp)
-    val smallUserRange = Range(convertTimeStamps(smallRange.min).toDouble(), convertTimeStamps(smallRange.max).toDouble())
-    frame.addSlice(bitmapsProcess.threads[0].slices[0], smallRange)
-    validateRange(smallRange, frame.totalRangeSeconds)
-    assertThat(smallUserRange.min.toLong()).isEqualTo(frame.startUs)
-    assertThat(smallUserRange.max.toLong()).isEqualTo(frame.endUs)
-    assertThat(bitmapsProcess.threads[0].slices[0].cpuTime).isWithin(DELTA).of(frame.cpuTimeSeconds)
-
-    val largeRange = Range(3.0 + myModel.beginTimestamp, 6.0 + myModel.beginTimestamp)
-    val largeUserRange = Range(convertTimeStamps(largeRange.min).toDouble(), convertTimeStamps(largeRange.max).toDouble())
-    val totalRange = Range(smallRange.min, largeRange.max)
-    frame.addSlice(bitmapsProcess.threads[0].slices[1], largeRange)
-    assertThat(smallUserRange.min.toLong()).isEqualTo(frame.startUs)
-    assertThat(largeUserRange.max.toLong()).isEqualTo(frame.endUs)
-    assertThat(frame.durationUs).isEqualTo((totalRange.length * SECONDS_TO_US).toLong())
-  }
-
-  @Test
   fun associatedFramesPerfClass() {
-    val bitmapsProcess = myModel.processes[TEST_PID]!!
-    val goodFrame = AtraceFrame(bitmapsProcess.threads[0].id, ::convertTimeStamps, SECONDS_TO_US.toLong() * 1, AtraceFrame.FrameThread.MAIN)
-    val badFrame = AtraceFrame(bitmapsProcess.threads[0].id, ::convertTimeStamps, SECONDS_TO_US.toLong() * 1, AtraceFrame.FrameThread.MAIN)
-    val goodFrameRange = Range(1.0, 1.01)
-    val badFrameRange = Range(1.0, 10.0)
-    goodFrame.addSlice(bitmapsProcess.threads[0].slices[0], goodFrameRange)
-    assertThat(goodFrame.perfClass).isEqualTo(AtraceFrame.PerfClass.GOOD)
-    badFrame.addSlice(bitmapsProcess.threads[0].slices[0], badFrameRange)
-    assertThat(badFrame.perfClass).isEqualTo(AtraceFrame.PerfClass.BAD)
+    val goodSlice = TestSliceGroup(SECONDS_TO_US * 0.25, SECONDS_TO_US * 0.75, SECONDS_TO_US * 0.6)
+    val goodFrame = AtraceFrame(goodSlice, ::convertTimeStamps, SECONDS_TO_US.toLong() * 1, AtraceFrame.FrameThread.MAIN)
+
+    val badSlice = TestSliceGroup(SECONDS_TO_US * 0.25, SECONDS_TO_US * 1.5, SECONDS_TO_US * 0.9)
+    val badFrame = AtraceFrame(badSlice, ::convertTimeStamps, SECONDS_TO_US.toLong() * 1, AtraceFrame.FrameThread.MAIN)
 
     goodFrame.associatedFrame = badFrame
     badFrame.associatedFrame = goodFrame
@@ -94,18 +54,13 @@ class AtraceFrameTest {
     assertThat(AtraceFrame.EMPTY.totalPerfClass).isEqualTo(AtraceFrame.PerfClass.NOT_SET)
   }
 
-  fun validateRange(expected: Range, actual: Range) {
-    assertThat(expected.min).isWithin(DELTA).of(actual.min)
-    assertThat(expected.max).isWithin(DELTA).of(actual.max)
-  }
+  private inner class TestSliceGroup(override val startTime: Double,
+                                     override val endTime: Double,
+                                     override val cpuTime: Double) : SliceGroup {
+    override val children: List<SliceGroup> = listOf()
+    override val scheduledSlices: List<SchedSlice> = listOf()
+    override val name: String = ""
+    override val didNotFinish: Boolean = false
 
-  @Test
-  fun testStoringSlices() {
-    val bitmapsProcess = myModel.processes[TEST_PID] ?: error("No process for pid $TEST_PID")
-    val frame = AtraceFrame(bitmapsProcess.threads[0].id, ::convertTimeStamps, SECONDS_TO_US.toLong() * 1, AtraceFrame.FrameThread.MAIN)
-    val frameRange = Range(1.0, 2.0)
-    val slice = bitmapsProcess.threads[0].slices[0]
-    frame.addSlice(slice, frameRange)
-    assertThat(frame.slices.contains(slice))
   }
 }
