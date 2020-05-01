9dec2377c5bc083b1d0086b695b5453f8b7b1c10
Yuriy Solodkyy <solodkyy@google.com>
2020-05-01 17:36:35 +0100

    Re-attach cached build models if and only if all modules are available
    Bug: 155408330
    Test: com.android.tools.idea.tests.gui.gradle.GradleSyncTest
    Test: com.android.tools.idea.gradle.project.sync.OpenProjectIntegrationTest
    Change-Id: Ib5e140ddf8245d3291c0b6343e0b398896bc3f4e

--- android/src/com/android/tools/idea/gradle/project/AndroidGradleProjectStartupActivity.kt
+++ android/src/com/android/tools/idea/gradle/project/AndroidGradleProjectStartupActivity.kt
@@ -23,12 +23,15 @@ import com.android.tools.idea.gradle.project.sync.GradleSyncInvoker
 import com.android.tools.idea.gradle.project.sync.GradleSyncState.Companion.getInstance
 import com.android.tools.idea.gradle.project.sync.idea.GradleSyncExecutor
 import com.android.tools.idea.gradle.project.sync.idea.data.DataNodeCaches
-import com.android.tools.idea.gradle.project.sync.idea.data.service.AndroidProjectKeys
+import com.android.tools.idea.gradle.project.sync.idea.data.service.AndroidProjectKeys.ANDROID_MODEL
+import com.android.tools.idea.gradle.project.sync.idea.data.service.AndroidProjectKeys.GRADLE_MODULE_MODEL
+import com.android.tools.idea.gradle.project.sync.idea.data.service.AndroidProjectKeys.JAVA_MODULE_MODEL
+import com.android.tools.idea.gradle.project.sync.idea.data.service.AndroidProjectKeys.NDK_MODEL
 import com.android.tools.idea.gradle.project.sync.setup.post.setUpModules
 import com.android.tools.idea.gradle.util.AndroidStudioPreferences
 import com.android.tools.idea.gradle.variant.conflict.ConflictSet
 import com.android.tools.idea.model.AndroidModel
-import com.google.wireless.android.sdk.stats.GradleSyncStats
+import com.google.wireless.android.sdk.stats.GradleSyncStats.Trigger
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.externalSystem.model.DataNode
 import com.intellij.openapi.externalSystem.model.Key
@@ -51,20 +54,26 @@ import org.jetbrains.plugins.gradle.util.GradleConstants
 class AndroidGradleProjectStartupActivity : StartupActivity {
   override fun runActivity(project: Project) {
     val gradleProjectInfo = GradleProjectInfo.getInstance(project)
-    if (!gradleProjectInfo.isSkipStartupActivity &&
 
-        // We only request sync if we know this is an Android project.
-        (
-          // Opening an IDEA project with Android modules (AS and IDEA - i.e. previously synced).
-          gradleProjectInfo.androidModules.isNotEmpty()
+    fun shouldSyncOrAttachModels(): Boolean {
+      if (gradleProjectInfo.isSkipStartupActivity) return false
 
-          // Opening a Gradle project with .idea but no .iml files or facets (Typical for AS but not in IDEA)
-          || IdeInfo.getInstance().isAndroidStudio && gradleProjectInfo.isBuildWithGradle
+      // Opening an IDEA project with Android modules (AS and IDEA - i.e. previously synced).
+      if (gradleProjectInfo.androidModules.isNotEmpty()) return true
 
-          // Opening a project without .idea directory (including a newly created).
-          || gradleProjectInfo.isImportedProject)) {
+      // Opening a Gradle project with .idea but no .iml files or facets (Typical for AS but not in IDEA)
+      if (IdeInfo.getInstance().isAndroidStudio && gradleProjectInfo.isBuildWithGradle) return true
+
+      // Opening a project without .idea directory (including a newly created).
+      if (gradleProjectInfo.isImportedProject) return true
+
+      return false
+    }
+
+    if (shouldSyncOrAttachModels()) {
       attachCachedModelsOrTriggerSync(project, gradleProjectInfo)
     }
+
     gradleProjectInfo.isSkipStartupActivity = false
 
     // Disable all settings sections that we don't want to be present in Android Studio.
@@ -73,67 +82,105 @@ class AndroidGradleProjectStartupActivity : StartupActivity {
   }
 }
 
+private val LOG = Logger.getInstance(AndroidGradleProjectStartupActivity::class.java)
+
 /**
  * Attempts to see if the models cached by IDEAs external system are valid, if they are then we attach them to the facet,
  * if they are not then we request a project sync in order to ensure that the IDE has access to all of models it needs to function.
  */
 private fun attachCachedModelsOrTriggerSync(project: Project, gradleProjectInfo: GradleProjectInfo) {
-  val linkedProjectPaths = GradleSettings.getInstance(project).linkedProjectsSettings.mapNotNull { it.externalProjectPath }.toSet()
-  val projectDataManager = ProjectDataManager.getInstance()
-  val projectsData: Collection<DataNode<ProjectData>> =
-    linkedProjectPaths.mapNotNull {
-      projectDataManager.getExternalProjectData(project, GradleConstants.SYSTEM_ID, it)?.externalProjectStructure
-    }
-  val trigger =
-    if (gradleProjectInfo.isNewProject) GradleSyncStats.Trigger.TRIGGER_PROJECT_NEW else GradleSyncStats.Trigger.TRIGGER_PROJECT_REOPEN
-  val request = GradleSyncInvoker.Request(trigger)
-  val caches = DataNodeCaches.getInstance(project)
-  // If we don't have any cached data then do a full re-sync
-  if (projectsData.isEmpty() ||
-      projectsData.any { caches.isCacheMissingModels(it) } ||
-      GradleSyncExecutor.areCachedFilesMissing(project)) {
-    GradleSyncInvoker.getInstance().requestProjectSync(project, request)
-  }
-  else {
-    // Otherwise attach the models we found from the cache.
-    Logger.getInstance(AndroidGradleProjectStartupActivity::class.java)
-      .info("Up-to-date models found in the cache. Not invoking Gradle sync.")
-    projectsData.forEach { attachModelsToFacets(project, it) }
 
-    ConflictSet.findConflicts(project).showSelectionConflicts()
-    ProjectStructure.getInstance(project).analyzeProjectStructure()
-    setUpModules(project)
-
-    getInstance(project).syncSkipped(null)
-  }
-}
-
-private fun attachModelsToFacets(project: Project, projectData: DataNode<ProjectData>) {
   val moduleManager = ModuleManager.getInstance(project)
-  val moduleDataNodes = ExternalSystemApiUtil.findAllRecursively(projectData, ProjectKeys.MODULE)
+  val projectDataManager = ProjectDataManager.getInstance()
+  val caches = DataNodeCaches.getInstance(project)
 
-  moduleDataNodes.forEach { moduleDataNode: DataNode<ModuleData> ->
-    val module = moduleManager.findModuleByName(moduleDataNode.data.internalName)
-    if (module != null) {
-      attachModelsToFacets(module, moduleDataNode)
+  fun findProjectDataNode(externalProjectPath: String): DataNode<ProjectData>? =
+    projectDataManager.getExternalProjectData(project, GradleConstants.SYSTEM_ID, externalProjectPath)?.externalProjectStructure
+
+  fun findModule(internalName: String): Module? =
+    moduleManager.findModuleByName(internalName)
+
+  fun DataNode<ProjectData>.modules(): Collection<DataNode<ModuleData>> =
+    ExternalSystemApiUtil.findAllRecursively(this, ProjectKeys.MODULE)
+
+  // TODO(b/155467517): Reconsider the way we launch sync when GradleSyncInvoker is deleted. We may want to handle each external project
+  //                    path individually.
+  fun requestSync(reason: String) {
+    LOG.info("Requesting Gradle sync ($reason).")
+    val trigger = if (gradleProjectInfo.isNewProject) Trigger.TRIGGER_PROJECT_NEW else Trigger.TRIGGER_PROJECT_REOPEN
+    GradleSyncInvoker.getInstance().requestProjectSync(project, GradleSyncInvoker.Request(trigger))
+  }
+
+  val projectDataNodes: Collection<DataNode<ProjectData>> =
+    GradleSettings.getInstance(project)
+      .linkedProjectsSettings
+      .mapNotNull { it.externalProjectPath }
+      .toSet()
+      .map {
+        findProjectDataNode(it) ?: run { requestSync("DataNode<ProjectData> not found for $it"); return }
+      }
+
+  if (projectDataNodes.isEmpty()) {
+    requestSync("No linked projects found")
+    return
+  }
+
+  if (projectDataNodes.any { caches.isCacheMissingModels(it) }) {
+    requestSync("Some models are missing")  // TODO(solodkyy): Merge with attaching and report details.
+    return
+  }
+
+  if (GradleSyncExecutor.areCachedFilesMissing(project)) {
+    requestSync("Some .jar files missing")
+    return
+  }
+
+  val moduleToModelPairs: Collection<Pair<Module, DataNode<ModuleData>>> =
+    projectDataNodes.flatMap { projectData ->
+      projectData
+        .modules()
+        .map { node ->
+          val internalName = node.data.internalName
+          val module = findModule(internalName) ?: run { requestSync("Module $internalName not found"); return }
+          module to node
+        }
     }
+
+  val attachModelActions = moduleToModelPairs.flatMap { (module, moduleDataNode) ->
+
+    /** Returns `null` if validation fails. */
+    fun <T, V> prepare(dataKey: Key<T>, getFacet: Module.() -> V?, attach: V.(T) -> Unit): (() -> Unit)? {
+      val model =
+        ExternalSystemApiUtil
+          .getChildren(moduleDataNode, dataKey)
+          .singleOrNull() // None or one node is expected here.
+          ?.data
+        ?: return { /* Nothing to do if no model present. */ }
+      val facet = module.getFacet() ?: run {
+        requestSync("no facet found for $dataKey in ${module.name} module")
+        return null  // Missing facet detected, triggering sync.
+      }
+      return { facet.attach(model) }
+    }
+
+    listOf(
+      prepare(ANDROID_MODEL, AndroidFacet::getInstance, AndroidModel::set) ?: return,
+      prepare(JAVA_MODULE_MODEL, JavaFacet::getInstance, JavaFacet::setJavaModuleModel) ?: return,
+      prepare(GRADLE_MODULE_MODEL, GradleFacet::getInstance, GradleFacet::setGradleModuleModel) ?: return,
+      prepare(NDK_MODEL, NdkFacet::getInstance, NdkFacet::setNdkModuleModel) ?: return
+    )
   }
+
+  LOG.info("Up-to-date models found in the cache. Not invoking Gradle sync.")
+  attachModelActions.forEach { it() }
+
+  additionalProjectSetup(project)
+
+  getInstance(project).syncSkipped(null)
 }
 
-private fun attachModelsToFacets(module: Module, moduleDataNode: DataNode<ModuleData>) {
-  fun <T, V> attachModelToFacet(dataKey: Key<T>, facet: Module.() -> V?, attach: V.(T) -> Unit) {
-    val model =
-      ExternalSystemApiUtil
-        .getChildren(moduleDataNode, dataKey)
-        .singleOrNull() // None or one node is expected here.
-        ?.data
-      ?: return
-    module.facet()?.attach(model)
-  }
-
-  attachModelToFacet(AndroidProjectKeys.ANDROID_MODEL, AndroidFacet::getInstance, AndroidModel::set)
-  attachModelToFacet(AndroidProjectKeys.JAVA_MODULE_MODEL, JavaFacet::getInstance, JavaFacet::setJavaModuleModel)
-  attachModelToFacet(AndroidProjectKeys.GRADLE_MODULE_MODEL, GradleFacet::getInstance, GradleFacet::setGradleModuleModel)
-  attachModelToFacet(AndroidProjectKeys.NDK_MODEL, NdkFacet::getInstance, NdkFacet::setNdkModuleModel)
+private fun additionalProjectSetup(project: Project) {
+  ConflictSet.findConflicts(project).showSelectionConflicts()
+  ProjectStructure.getInstance(project).analyzeProjectStructure()
+  setUpModules(project)
 }
-
