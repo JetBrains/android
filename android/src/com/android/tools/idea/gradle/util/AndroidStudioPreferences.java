3cdd42edcf6edf43cf436bfa6d20edb2833c1558
Bradley Smith <smithbradley@google.com>
2020-05-04 17:33:21 -0700

    Ensure configurables are not removed when they shouldn't be
    Bug: 155677192
    Test: manually tested
    Change-Id: Id3d03bfdfddc5dee3f77ed2548553c77317eb7f9

--- android/src/com/android/tools/idea/gradle/util/AndroidStudioPreferences.java
+++ android/src/com/android/tools/idea/gradle/util/AndroidStudioPreferences.java
@@ -55,6 +55,6 @@ public final class AndroidStudioPreferences {
       }
     }
 
-    projectConfigurable.unregisterExtensions(ep -> !nonStudioExtensions.contains(ep));
+    projectConfigurable.unregisterExtensions(ep -> nonStudioExtensions.contains(ep));
   }
 }
