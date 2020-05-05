JetBrains/android

commit e2c04469f5dd213f1a93dd2ccffb2d380baf4227

Add fabricated attributes for name & dimension.
Also convert DimensionBuilder and SelectedViewBuilder from classes
to objects.

Bug: 149999705
Test: Unit test added.
Change-Id: Ie1073446e0baa48eeb7f37a5dd3c316d02e48669

layout-inspector/src/com/android/tools/idea/layoutinspector/model/SelectedViewModel.kt
@@ -15,6 +15,7 @@
  */
 package com.android.tools.idea.layoutinspector.model
 
+import com.android.tools.idea.layoutinspector.properties.InspectorPropertyItem
 import com.android.tools.property.panel.api.SelectedComponentModel
 import icons.StudioIcons
 import org.jetbrains.android.dom.AndroidDomElementDescriptorProvider
@@ -28,12 +29,12 @@ private const val UNNAMED_COMPONENT = "<unnamed>"
  * For displaying which component is being edited.
  * Intended for being shown at the top of the properties panel.
  */
-class SelectedViewModel(view: ViewNode?) : SelectedComponentModel {
+class SelectedViewModel(name: InspectorPropertyItem, id: InspectorPropertyItem?) : SelectedComponentModel {
 
-  override val icon: Icon? = view?.unqualifiedName?.let { AndroidDomElementDescriptorProvider.getIconForViewTag(it) }
+  override val icon: Icon? = name.value?.let { AndroidDomElementDescriptorProvider.getIconForViewTag(it) }
                              ?: StudioIcons.LayoutEditor.Palette.UNKNOWN_VIEW
 
-  override val id: String = view?.viewId?.name ?: UNNAMED_COMPONENT
+  override val id: String = id?.value ?: UNNAMED_COMPONENT
 
-  override val description: String = view?.unqualifiedName ?: ""
+  override val description: String = name.value ?: ""
 }

layout-inspector/src/com/android/tools/idea/layoutinspector/legacydevice/LegacyPropertiesProvider.kt
@@ -24,6 +24,7 @@ import com.android.tools.idea.layoutinspector.model.ViewNode
 import com.android.tools.idea.layoutinspector.properties.InspectorPropertyItem
 import com.android.tools.idea.layoutinspector.properties.PropertiesProvider
 import com.android.tools.idea.layoutinspector.properties.PropertySection
+import com.android.tools.idea.layoutinspector.properties.addInternalProperties
 import com.android.tools.property.panel.api.PropertiesTable
 import com.google.common.collect.HashBasedTable
 import com.google.common.util.concurrent.Futures
@@ -114,6 +115,8 @@ class LegacyPropertiesProvider : PropertiesProvider {
       table.remove(SdkConstants.ANDROID_URI, ATTR_Z)
       table.remove(SdkConstants.ANDROID_URI, ATTR_BOTTOM)
       table.remove(SdkConstants.ANDROID_URI, ATTR_RIGHT)
+
+      addInternalProperties(table, view)
 
       temp[view.drawId] = PropertiesTable.create(table)
     }

layout-inspector/src/com/android/tools/idea/layoutinspector/properties/DimensionBuilder.kt
@@ -15,10 +15,10 @@
  */
 package com.android.tools.idea.layoutinspector.properties
 
-import com.android.tools.idea.layoutinspector.model.ViewNode
+import com.android.SdkConstants.ATTR_HEIGHT
+import com.android.SdkConstants.ATTR_WIDTH
 import com.android.tools.property.panel.api.InspectorBuilder
 import com.android.tools.property.panel.api.InspectorPanel
-import com.android.tools.property.panel.api.PropertiesModel
 import com.android.tools.property.panel.api.PropertiesTable
 import com.android.tools.property.ptable2.PTable
 import com.android.tools.property.ptable2.PTableCellRenderer
@@ -37,22 +37,16 @@ import javax.swing.Icon
 import javax.swing.JComponent
 import javax.swing.JTable
 
-private const val X = "x"
-private const val Y = "y"
-private const val HEIGHT = "height"
-private const val WIDTH = "width"
-
 /**
  * Adds the bounds of a view to the layout inspectors property table.
  *
  * Currently displayed are: x, y, width, height where the position is relative to the top
  * left of the device.
  */
-class DimensionBuilder(val model: InspectorPropertiesModel) : InspectorBuilder<InspectorPropertyItem> {
+object DimensionBuilder : InspectorBuilder<InspectorPropertyItem> {
 
   override fun attachToInspector(inspector: InspectorPanel, properties: PropertiesTable<InspectorPropertyItem>) {
-    val view = model.layoutInspector?.layoutInspectorModel?.selection ?: return
-    val table = PTable.create(DimensionTableModel(view), null, RendererProvider())
+    val table = PTable.create(DimensionTableModel(properties), null, RendererProvider())
     table.component.addFocusListener(object : FocusAdapter() {
       override fun focusGained(event: FocusEvent) {
         scrollSelectedRowIntoView(table)
@@ -74,16 +68,16 @@ class DimensionBuilder(val model: InspectorPropertiesModel) : InspectorBuilder<I
     return table.getCellRect(row, 1, true)
   }
 
-  private class DimensionTableModel(view: ViewNode): PTableModel {
-    override val items = createDimensionItems(view)
+  private class DimensionTableModel(properties: PropertiesTable<InspectorPropertyItem>): PTableModel {
+    override val items = createDimensionItems(properties)
     override var editedItem: PTableItem? = null
 
-    private fun createDimensionItems(view: ViewNode): List<PTableItem> {
-      return listOf(
-        Item(X, view.bounds.x.toString()),
-        Item(Y, view.bounds.y.toString()),
-        Item(WIDTH, view.bounds.width.toString()),
-        Item(HEIGHT, view.bounds.height.toString()))
+    private fun createDimensionItems(properties: PropertiesTable<InspectorPropertyItem>): List<PTableItem> {
+      return listOfNotNull(
+        properties.getOrNull(NAMESPACE_INTERNAL, ATTR_X),
+        properties.getOrNull(NAMESPACE_INTERNAL, ATTR_Y),
+        properties.getOrNull(NAMESPACE_INTERNAL, ATTR_WIDTH),
+        properties.getOrNull(NAMESPACE_INTERNAL, ATTR_HEIGHT))
     }
 
     override fun addItem(item: PTableItem): PTableItem {
@@ -95,8 +89,6 @@ class DimensionBuilder(val model: InspectorPropertiesModel) : InspectorBuilder<I
       // Not supported
     }
   }
-
-  private class Item(override val name: String, override val value: String) : PTableItem
 
   private class RendererProvider : PTableCellRendererProvider {
     private val renderer = Renderer()

layout-inspector/src/com/android/tools/idea/layoutinspector/properties/InspectorPropertiesView.kt
@@ -53,13 +53,14 @@ class InspectorPropertiesView(model: InspectorPropertiesModel) : PropertiesView<
 
   init {
     watermark = Watermark(WATERMARK_MESSAGE, WATERMARK_ACTION_MESSAGE, "")
-    main.builders.add(SelectedViewBuilder(model))
+    main.builders.add(SelectedViewBuilder)
     val tab = addTab("")
-    tab.builders.add(DimensionBuilder(model))
+    tab.builders.add(DimensionBuilder)
     tab.builders.add(InspectorTableBuilder("Declared Attributes", { it.group == PropertySection.DECLARED },
                                            model, enumSupportProvider, controlTypeProvider))
     tab.builders.add(InspectorTableBuilder("Layout", { it.group == PropertySection.LAYOUT },
                                            model, enumSupportProvider, controlTypeProvider, androidSortOrder))
-    tab.builders.add(InspectorTableBuilder("All Attributes", { true }, model, enumSupportProvider, controlTypeProvider, searchable = true))
+    tab.builders.add(InspectorTableBuilder("All Attributes", { it.group != PropertySection.DIMENSION && it.group != PropertySection.VIEW },
+                                           model, enumSupportProvider, controlTypeProvider, searchable = true))
   }
 }

layout-inspector/src/com/android/tools/idea/layoutinspector/properties/InspectorPropertyItem.kt
@@ -64,6 +64,10 @@ open class InspectorPropertyItem(
 
 ) : PropertyItem {
 
+  constructor(namespace: String, attrName: String, type: Type, value: String?, group: PropertySection, source: ResourceReference?,
+              view: ViewNode, resourceLookup: ResourceLookup?) :
+    this(namespace, attrName, attrName, type, value, group, source, view, resourceLookup)
+
   override fun hashCode(): Int = HashCodes.mix(namespace.hashCode(), attrName.hashCode(), source?.hashCode() ?: 0)
 
   override fun equals(other: Any?): Boolean =

layout-inspector/src/com/android/tools/idea/layoutinspector/properties/PropertiesProvider.kt
@@ -8,17 +8,29 @@
  *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,ndroid
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
 package com.android.tools.idea.layoutinspector.properties
 
+import com.android.SdkConstants.ATTR_NAME
+import com.android.SdkConstants.ATTR_HEIGHT
+import com.android.SdkConstants.ATTR_WIDTH
 import com.android.tools.idea.layoutinspector.model.ViewNode
+import com.android.tools.idea.layoutinspector.properties.PropertySection.DIMENSION
+import com.android.tools.idea.layoutinspector.properties.PropertySection.VIEW
+import com.android.tools.layoutinspector.proto.LayoutInspectorProto.Property.Type
 import com.android.tools.property.panel.api.PropertiesTable
+import com.google.common.collect.Table
 import com.google.common.util.concurrent.Futures
 import java.util.concurrent.Future
+
+// Constants for fabricated internal properties
+const val NAMESPACE_INTERNAL = "internal"
+const val ATTR_X = "x"
+const val ATTR_Y = "y"
 
 /**
  * A [PropertiesProvider] provides properties to registered listeners..
@@ -46,4 +58,19 @@ object EmptyPropertiesProvider : PropertiesProvider {
   override fun requestProperties(view: ViewNode): Future<*> {
     return Futures.immediateFuture(null)
   }
+}
+
+/**
+ * Add a few fabricated internal attributes.
+ */
+internal fun addInternalProperties(table: Table<String, String, InspectorPropertyItem>, view: ViewNode) {
+  add(table, InspectorPropertyItem(NAMESPACE_INTERNAL, ATTR_NAME, Type.STRING, view.qualifiedName, VIEW, null, view, null))
+  add(table, InspectorPropertyItem(NAMESPACE_INTERNAL, ATTR_X, Type.INT32, view.x.toString(), DIMENSION, null, view, null))
+  add(table, InspectorPropertyItem(NAMESPACE_INTERNAL, ATTR_Y, Type.INT32, view.y.toString(), DIMENSION, null, view, null))
+  add(table, InspectorPropertyItem(NAMESPACE_INTERNAL, ATTR_WIDTH, Type.INT32, view.width.toString(), DIMENSION, null, view, null))
+  add(table, InspectorPropertyItem(NAMESPACE_INTERNAL, ATTR_HEIGHT, Type.INT32, view.height.toString(), DIMENSION, null, view, null))
+}
+
+private fun add(table: Table<String, String, InspectorPropertyItem>, item: InspectorPropertyItem) {
+  table.put(item.namespace, item.name, item)
 }

layout-inspector/src/com/android/tools/idea/layoutinspector/properties/PropertySection.kt
@@ -22,4 +22,6 @@ enum class PropertySection {
   DEFAULT,    // Use this value if an item is not in any of the groups mentioned below
   DECLARED,   // This attribute was specified by the user in a layout file in the application
   LAYOUT,     // This attribute is a layout attribute i.e. defined by the parent view
+  DIMENSION,  // This attribute is intended for the Dimension section only
+  VIEW,       // This attribute is intended for the SelectedView section only
 }

layout-inspector/src/com/android/tools/idea/layoutinspector/properties/SelectedViewBuilder.kt
@@ -15,17 +15,21 @@
  */
 package com.android.tools.idea.layoutinspector.properties
 
+import com.android.SdkConstants.ANDROID_URI
+import com.android.SdkConstants.ATTR_ID
+import com.android.SdkConstants.ATTR_NAME
 import com.android.tools.idea.layoutinspector.model.SelectedViewModel
 import com.android.tools.property.panel.api.InspectorBuilder
 import com.android.tools.property.panel.api.InspectorPanel
 import com.android.tools.property.panel.api.PropertiesTable
 import com.android.tools.property.panel.api.SelectedComponentPanel
 
-class SelectedViewBuilder(val model: InspectorPropertiesModel) : InspectorBuilder<InspectorPropertyItem> {
+object SelectedViewBuilder : InspectorBuilder<InspectorPropertyItem> {
 
   override fun attachToInspector(inspector: InspectorPanel, properties: PropertiesTable<InspectorPropertyItem>) {
-    val view = model.layoutInspector?.layoutInspectorModel?.selection ?: return
-    val panel = SelectedComponentPanel(SelectedViewModel(view))
+    val name = properties.getOrNull(NAMESPACE_INTERNAL, ATTR_NAME) ?: return
+    val id = properties.getOrNull(ANDROID_URI, ATTR_ID)
+    val panel = SelectedComponentPanel(SelectedViewModel(name, id))
     inspector.addComponent(panel, null)
   }
 }

commit d22663c8d9ca2e1861fc28718ada276d3c82aba2

Show the unqualified name in SelectedView
This bug was introduced with change e2c0446

Bug: 149999705
Test: Added a unit test.
Change-Id: I1ee67674368f67bae16a11827c75006850cb2bc2

layout-inspector/src/com/android/tools/idea/layoutinspector/model/SelectedViewModel.kt
@@ -30,11 +30,12 @@ private const val UNNAMED_COMPONENT = "<unnamed>"
  * Intended for being shown at the top of the properties panel.
  */
 class SelectedViewModel(name: InspectorPropertyItem, id: InspectorPropertyItem?) : SelectedComponentModel {
+  private val unqualifiedName = name.value?.substringAfterLast('.') ?: ""
 
-  override val icon: Icon? = name.value?.let { AndroidDomElementDescriptorProvider.getIconForViewTag(it) }
+  override val icon: Icon? = unqualifiedName?.let { AndroidDomElementDescriptorProvider.getIconForViewTag(it) }
                              ?: StudioIcons.LayoutEditor.Palette.UNKNOWN_VIEW
 
   override val id: String = id?.value ?: UNNAMED_COMPONENT
 
-  override val description: String = name.value ?: ""
+  override val description: String = unqualifiedName
 }

layout-inspector/testSrc/com/android/tools/idea/layoutinspector/model/SelectedViewModelTest.kt
@@ -1,0 +1,78 @@
+/*
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.layoutinspector.model
+
+import com.android.SdkConstants.ANDROID_URI
+import com.android.SdkConstants.ATTR_ID
+import com.android.SdkConstants.ATTR_NAME
+import com.android.SdkConstants.BUTTON
+import com.android.SdkConstants.FQCN_BUTTON
+import com.android.SdkConstants.FQCN_TEXT_VIEW
+import com.android.SdkConstants.TEXT_VIEW
+import com.android.testutils.MockitoKt.mock
+import com.android.tools.idea.layoutinspector.properties.InspectorPropertyItem
+import com.android.tools.idea.layoutinspector.properties.PropertySection
+import com.android.tools.idea.layoutinspector.resource.ResourceLookup
+import com.android.tools.layoutinspector.proto.LayoutInspectorProto.Property.Type
+import com.google.common.truth.Truth.assertThat
+import icons.StudioIcons
+import org.junit.Test
+
+class SelectedViewModelTest {
+
+  @Test
+  fun testButtonWithId() {
+    val name = nameOf(FQCN_BUTTON)
+    val id = idOf("button1")
+    val model = SelectedViewModel(name, id)
+    assertThat(model.id).isEqualTo("@id/button1")
+    assertThat(model.icon).isEqualTo(StudioIcons.LayoutEditor.Palette.BUTTON)
+    assertThat(model.description).isEqualTo(BUTTON)
+  }
+
+  @Test
+  fun testTextViewWithoutId() {
+    val name = nameOf(FQCN_TEXT_VIEW)
+    val id = idOf(null)
+    val model = SelectedViewModel(name, id)
+    assertThat(model.id).isEqualTo("<unnamed>")
+    assertThat(model.icon).isEqualTo(StudioIcons.LayoutEditor.Palette.TEXT_VIEW)
+    assertThat(model.description).isEqualTo(TEXT_VIEW)
+  }
+
+  @Test
+  fun testDecorView() {
+    val name = nameOf("com.android.internal.policy.DecorView")
+    val id = idOf(null)
+    val model = SelectedViewModel(name, id)
+    assertThat(model.id).isEqualTo("<unnamed>")
+    assertThat(model.icon).isEqualTo(StudioIcons.LayoutEditor.Palette.UNKNOWN_VIEW)
+    assertThat(model.description).isEqualTo("DecorView")
+  }
+
+  private fun nameOf(name: String): InspectorPropertyItem {
+    val lookup: ResourceLookup = mock()
+    val view: ViewNode = mock()
+    return InspectorPropertyItem(ANDROID_URI, ATTR_NAME, Type.STRING, name, PropertySection.VIEW, null, view, lookup)
+  }
+
+  private fun idOf(id: String?): InspectorPropertyItem {
+    val lookup: ResourceLookup = mock()
+    val view: ViewNode = mock()
+    val value = id?.let { "@id/$id" }
+    return InspectorPropertyItem(ANDROID_URI, ATTR_ID, Type.STRING, value, PropertySection.VIEW, null, view, lookup)
+  }
+}