66d8946df5fde18fa101024d13f9c065c2cfd2e1
Christophe Rhodes <xof@google.com>
2020-05-06 12:39:42 +0100

    [Dsl] update references to bugs
    Some bugs are fixed and the comment can go away; most, sadly, are
    fixed but the comment is still relevant, so file new bugs as
    appropriate and update the references.
    
    Bug: n/a
    Test: no code change
    Change-Id: I7183129c558c4a24cffbbc420fe2b247e55651bd

--- gradle-dsl/src/com/android/tools/idea/gradle/dsl/model/android/FlavorTypeModelImpl.java
+++ gradle-dsl/src/com/android/tools/idea/gradle/dsl/model/android/FlavorTypeModelImpl.java
@@ -310,7 +310,7 @@ public abstract class FlavorTypeModelImpl extends GradleDslBlockModel implements
                                                                           @NotNull String name,
                                                                           @NotNull String value) {
     GradleNameElement nameElement = GradleNameElement.create(elementName);
-    // TODO(b/141842964): Groovy expects buildConfigField and resValue to be expressed with GradleDslExpressionList, while Kotlin expresses
+    // TODO(b/155853837): Groovy expects buildConfigField and resValue to be expressed with GradleDslExpressionList, while Kotlin expresses
     //  them with a GradleDslMethodCall.  This method of detecting which to produce is unsound given the possibility of multi-language build
     //  files, and should in any case be replaced by the two language backends producing a common representation.
     boolean forKotlin = myDslElement.getDslFile().getPsiElement() instanceof KtFile;
