345ed99fcbd3602c1c89da316bc463bc1cf629fb
John Hoford <hoford@google.com>
2020-05-07 14:28:58 -0700

    change the rendering of Motion paths during editing bug: 156023052 Test: N/A UI rendering change
    Change-Id: I04610ebb484356ac0719a30c87fc1953c2652290

--- designer/src/com/android/tools/idea/uibuilder/handlers/motion/DrawMotionPath.java
+++ designer/src/com/android/tools/idea/uibuilder/handlers/motion/DrawMotionPath.java
@@ -18,12 +18,16 @@ package com.android.tools.idea.uibuilder.handlers.motion;
 import com.android.tools.idea.common.scene.SceneContext;
 import com.android.tools.idea.common.scene.draw.DisplayList;
 import com.android.tools.idea.common.scene.draw.DrawCommand;
+import com.intellij.ui.JBColor;
 import java.awt.BasicStroke;
 import java.awt.Color;
+import java.awt.FontMetrics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 import java.awt.geom.AffineTransform;
 import java.awt.geom.GeneralPath;
+import java.awt.geom.Rectangle2D;
+import java.text.DecimalFormat;
 import java.util.Arrays;
 
 public class DrawMotionPath implements DrawCommand {
@@ -34,24 +38,46 @@ public class DrawMotionPath implements DrawCommand {
   private AffineTransform at = new AffineTransform();
   private static BasicStroke ourBasicStroke = new BasicStroke(1f);
   private static BasicStroke ourShadowStroke = new BasicStroke(1f);
-  private int mViewX;
-  private int mViewY;
-  private int mViewWidth;
-  private int mViewHeight;
+  private static BasicStroke ourAxis = new BasicStroke(2f);
+  private static BasicStroke ourMajorLines = new BasicStroke(1f);
+  private static BasicStroke ourMinorLines = new BasicStroke(1f, BasicStroke.CAP_BUTT,BasicStroke.JOIN_ROUND,BasicStroke.JOIN_MITER,new float[]{4,2},0);
+  private float mViewX;
+  private float mViewY;
+  private float mViewWidth;
+  private float mViewHeight;
   int[] xpath = new int[4];
   int[] ypath = new int[4];
   boolean mSelected = false;
+  int mSelectedKey;
+  private GeneralPath mGridAxis = new GeneralPath();
+  private GeneralPath mGridMajor = new GeneralPath();
+  private GeneralPath mGridMinor = new GeneralPath();
+  public static final int TYPE_SCREEN = 2;
+  public static final int TYPE_PATH = 1;
+  public static final int TYPE_CARTESIAN = 0;
+  String mPercent;
+  DecimalFormat myPercentFormat = new DecimalFormat("#.000");
+  JBColor myPointBackground = new JBColor(new Color(0x884322ff, true), new Color(0x884322ff,true));
 
   DrawMotionPath(boolean selected,
+                 int selected_key,
+                 int key_pos_type,
                  float[] path,
                  int pathSize,
                  int[] keyFramesType,
                  float[] keyFramePos,
                  int keyFramesSize,
-                 int vx,
-                 int vy,
-                 int vw,
-                 int vh) {
+                 float vx,
+                 float vy,
+                 float vw,
+                 float vh,
+                 float cbx,
+                 float cby,
+                 float cbw,
+                 float cbh,
+                 float percentX,
+                 float percentY) {
+    mSelectedKey = selected_key;
     mPath = Arrays.copyOf(path, pathSize);
     ourPath.reset();
     mViewX = vx;
@@ -59,6 +85,29 @@ public class DrawMotionPath implements DrawCommand {
     mViewWidth = vw;
     mViewHeight = vh;
     mSelected = selected;
+    mGridAxis.reset();
+    mGridMajor.reset();
+    mGridMinor.reset();
+    if (!Float.isNaN(percentX)) {
+      mPercent = "(" + myPercentFormat.format(percentX) + "," + myPercentFormat.format(percentY) + ")";
+    } else {
+      mPercent = "";
+    }
+
+    if (pathSize >= 4 && selected_key >= 0) {
+       switch (key_pos_type) {
+         case TYPE_CARTESIAN:
+         default:
+         graphDeltaXY();
+          break;
+         case TYPE_PATH:
+           graphPathRelative();
+           break;
+         case TYPE_SCREEN:
+           graphDeltaParent(cbx, cby, cbw, cbh);
+           break;
+      }
+    }
 
     for (int i = 0; i < pathSize; i += 2) {
       float x = mPath[i];
@@ -81,6 +130,97 @@ public class DrawMotionPath implements DrawCommand {
     }
   }
 
+  private void graphDeltaXY() {
+    float mStartX, mStartY;
+    float mEndX, mEndY;
+    mStartX = mPath[0];
+    mStartY = mPath[1];
+    mEndX = mPath[mPath.length - 2];
+    mEndY = mPath[mPath.length - 1];
+    for (int i = 0; i <= 100; i += 10) {
+      float x = mStartX + (mEndX - mStartX) * i / 100f;
+      float y = mStartY + (mEndY - mStartY) * i / 100f;
+      if (i == 0) {
+        mGridAxis.moveTo(x, mStartY);
+        mGridAxis.lineTo(x, mEndY);
+        mGridAxis.moveTo(mStartX, y);
+        mGridAxis.lineTo(mEndX, y);
+      } else {
+        mGridMajor.moveTo(x, mStartY);
+        mGridMajor.lineTo(x, mEndY);
+        mGridMajor.moveTo(mStartX, y);
+        mGridMajor.lineTo(mEndX, y);
+      }
+    }
+  }
+
+  private void graphPathRelative() {
+    float mStartX, mStartY;
+    float mEndX, mEndY;
+    mStartX = mPath[0];
+    mStartY = mPath[1];
+    mEndX = mPath[mPath.length - 2];
+    mEndY = mPath[mPath.length - 1];
+    for (int x = 0; x <= 100; x += 10) {
+      float y = 0;
+      float xp1 = mStartX + (mEndX - mStartX) * x / 100f;
+      float yp1 = mStartY + (mEndY - mStartY) * x / 100f;
+      y = 100;
+      float xp2 = xp1 + (mEndY - mStartY) * y / 100f;
+      float yp2 = yp1 - (mEndX - mStartX) * y / 100f;
+
+      y = -100;
+      float xp3 = xp1 + (mEndY - mStartY) * y / 100f;
+      float yp3 = yp1 - (mEndX - mStartX) * y / 100f;
+      if ( x == 0 || x == 100) {
+        mGridAxis.moveTo(xp2, yp2);
+        mGridAxis.lineTo(xp3, yp3);
+      } else {
+        mGridMajor.moveTo(xp2, yp2);
+        mGridMajor.lineTo(xp3, yp3);
+      }
+
+    }
+    for (int y = -100; y <= 100; y += 10) {
+
+      float xp1 = mStartX +  (mEndY - mStartY) * y / 100f;
+      float yp1 = mStartY - (mEndX - mStartX) * y / 100f;
+      float x =  100;
+
+      float xp2 = xp1+    (mEndX - mStartX) * x / 100f;
+      float yp2 = yp1+  (mEndY - mStartY) * x / 100f;
+      if (y == -100 || y == 0 || y == 100) {
+        mGridAxis.moveTo(xp1, yp1);
+        mGridAxis.lineTo(xp2, yp2);
+      } else {
+        mGridMajor.moveTo(xp1, yp1);
+        mGridMajor.lineTo(xp2, yp2);
+      }
+    }
+  }
+
+  private void graphDeltaParent(float vx,
+                                float vy,
+                                float vw,
+                                float vh) {
+
+    for (int i = 0; i <= 100; i += 10) {
+      float x = vx + (vw) * i / 100f;
+      float y = vy + (vh) * i / 100f;
+      if (i == 0) {
+        mGridAxis.moveTo(x, vy);
+        mGridAxis.lineTo(x, vy + vh);
+        mGridAxis.moveTo(vx, y);
+        mGridAxis.lineTo(vx + vw, y);
+      } else {
+        mGridMajor.moveTo(x, vy);
+        mGridMajor.lineTo(x, vy + vh);
+        mGridMajor.moveTo(vx, y);
+        mGridMajor.lineTo(vx + vw, y);
+      }
+    }
+  }
+
 
   @Override
   public int getLevel() {
@@ -96,7 +236,7 @@ public class DrawMotionPath implements DrawCommand {
     at.setToIdentity();
     at.translate(vx, vy);
     at.scale(scale, scale);
-
+    g2.setFont(g2.getFont().deriveFont(12/(float)scale));
     g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
     float lineWidth = (2f / (float)scale);
     g2.transform(at);
@@ -108,14 +248,29 @@ public class DrawMotionPath implements DrawCommand {
     g2.setStroke(ourShadowStroke);
     g2.setColor(Color.BLACK);
     g2.draw(ourPath);
+    g2.setColor(Color.GRAY);
+    g2.setStroke(ourAxis);
+    g2.draw(mGridAxis);
+    g2.setStroke(ourMajorLines);
+    g2.draw(mGridMajor);
     int diamond = (int)(1 + 5 / scale);
     g2.setStroke(ourBasicStroke);
+    g2.setColor(Color.LIGHT_GRAY);
+    g2.setStroke(ourAxis);
+    g2.draw(mGridAxis);
+    g2.setStroke(ourMajorLines);
+    g2.draw(mGridMajor);
+    g2.setStroke(ourMinorLines);
+    g2.draw(mGridMinor);
+
     g2.setColor(Color.white);
     g2.draw(ourPath);
+
+
     // Only draw keyframes if the component is selected
     if (mKeyFramesPos != null && mSelected) {
       for (int i = 0; i < mKeyFramesPos.length; i += 2) {
-        int type = mKeyFramesType[i/2];
+        int type = mKeyFramesType[i / 2];
         if (type / 1000 != 2) {
           // For now, only draw keyframes positions
           continue;
@@ -123,22 +278,36 @@ public class DrawMotionPath implements DrawCommand {
         int posx = (int)mKeyFramesPos[i];
         int posy = (int)mKeyFramesPos[i + 1];
         xpath[0] = posx;
-        xpath[1] = posx + diamond+1;
+        xpath[1] = posx + diamond + 1;
         xpath[2] = posx;
-        xpath[3] = posx - diamond-1;
+        xpath[3] = posx - diamond - 1;
 
-        ypath[0] = posy - diamond-1;
+        ypath[0] = posy - diamond - 1;
         ypath[1] = posy;
-        ypath[2] = posy + diamond+1;
+        ypath[2] = posy + diamond + 1;
         ypath[3] = posy;
-
+        int stringOffset = 10;
         g2.setColor(Color.BLACK);
         g2.fillPolygon(xpath, ypath, 4);
+
+        if (mSelectedKey == i/2 && mPercent.length() > 0) {
+          FontMetrics fm = g2.getFontMetrics();
+          Rectangle2D bounds =fm.getStringBounds(mPercent, g2);
+          g2.setColor(myPointBackground);
+          g2.fillRect( stringOffset + posx, posy - fm.getAscent(),(int) bounds.getWidth(), (int) bounds.getHeight());
+        }
         xpath[1]--;
         ypath[2]--;
         xpath[3]++;
         ypath[0]++;
-        g2.setColor(Color.WHITE);
+
+        if (mSelectedKey == i/2) {
+          g2.setColor(Color.BLACK);
+          g2.drawString(mPercent, 10+ posx,  posy);
+          g2.setColor(Color.BLUE);
+        } else {
+          g2.setColor(Color.WHITE);
+        }
         g2.fillPolygon(xpath, ypath, 4);
       }
     }
@@ -150,16 +319,24 @@ public class DrawMotionPath implements DrawCommand {
   }
 
   public static void buildDisplayList(boolean selected, DisplayList list,
+                                      int selected_key,
+                                      int key_pos_type,
                                       float[] path,
                                       int pathSize,
                                       int[] keyFrameType,
                                       float[] keyFramePos,
                                       int keyFrameSize,
-                                      int x,
-                                      int y,
-                                      int w,
-                                      int h) {
+                                      float x,
+                                      float y,
+                                      float w,
+                                      float h,
+                                      float cbx,
+                                      float cby,
+                                      float cbw,
+                                      float cbh,
+                                      float percentX,
+                                      float percentY) {
 
-    list.add(new DrawMotionPath(selected, path, pathSize, keyFrameType, keyFramePos, keyFrameSize, x, y, w, h));
+    list.add(new DrawMotionPath(selected, selected_key, key_pos_type, path, pathSize, keyFrameType, keyFramePos, keyFrameSize, x, y, w, h,cbx, cby, cbw, cbh,percentX, percentY));
   }
 }
