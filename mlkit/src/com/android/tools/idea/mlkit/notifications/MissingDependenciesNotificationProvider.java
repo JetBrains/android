e875fee8f0ca896e054eb59763e7658a00fa7938
Yi Zhou <deanzhou@google.com>
2020-05-11 18:32:15 -0700

    Add sub package .viewer to host model viewer related classes
    Bug: n/a
    Test: package refactor only
    Change-Id: I5bbed45994bdf595f8fbc7b3ff5cab27b2c0b56a

--- mlkit/src/com/android/tools/idea/mlkit/notifications/MissingDependenciesNotificationProvider.java
+++ mlkit/src/com/android/tools/idea/mlkit/notifications/MissingDependenciesNotificationProvider.java
@@ -17,7 +17,7 @@ package com.android.tools.idea.mlkit.notifications;
 
 import com.android.ide.common.repository.GradleCoordinate;
 import com.android.tools.idea.mlkit.MlUtils;
-import com.android.tools.idea.mlkit.TfliteModelFileEditor;
+import com.android.tools.idea.mlkit.viewer.TfliteModelFileEditor;
 import com.android.tools.idea.projectsystem.AndroidModuleSystem;
 import com.android.tools.idea.projectsystem.ProjectSystemSyncManager;
 import com.android.tools.idea.projectsystem.ProjectSystemUtil;
--- mlkit/src/com/android/tools/idea/mlkit/viewer/TfliteModelFileEditor.java
+++ mlkit/src/com/android/tools/idea/mlkit/viewer/TfliteModelFileEditor.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2019 The Android Open Source Project
+ * Copyright (C) 2020 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,8 +13,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.android.tools.idea.mlkit;
+package com.android.tools.idea.mlkit.viewer;
 
+import com.android.tools.idea.mlkit.LoggingUtils;
+import com.android.tools.idea.mlkit.MlModuleService;
 import com.android.tools.idea.mlkit.lightpsi.ClassNames;
 import com.android.tools.idea.mlkit.lightpsi.LightModelClass;
 import com.android.tools.mlkit.MetadataExtractor;
