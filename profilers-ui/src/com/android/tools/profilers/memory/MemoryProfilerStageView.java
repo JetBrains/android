aefa05f366406b728385fe456835136ce6c5bcb6
Joshua Gilpatrick <gijosh@google.com>
2020-05-18 15:15:20 -0700

    Disable record native allocation for x86 devices.
    Test: updated
    Fixes: 156973931
    Change-Id: I8ced70b8da5aec30b7f97dec8d1e4e4743b8b563

--- profilers-ui/src/com/android/tools/profilers/memory/MemoryProfilerStageView.java
+++ profilers-ui/src/com/android/tools/profilers/memory/MemoryProfilerStageView.java
@@ -117,6 +117,8 @@ public class MemoryProfilerStageView extends BaseMemoryProfilerStageView<MemoryP
   @VisibleForTesting
   static final String RECORD_NATIVE_TEXT = "Record native allocations";
   @VisibleForTesting
+  static final String X86_RECORD_NATIVE_TOOLTIP = "Native memory recording is unavailable on x86 or x86_64 devices";
+  @VisibleForTesting
   static final String STOP_NATIVE_TEXT = "Stop recording";
 
   private final MemoryProfilerStageLayout myLayout;
@@ -377,7 +379,7 @@ public class MemoryProfilerStageView extends BaseMemoryProfilerStageView<MemoryP
       myForceGarbageCollectionButton.setEnabled(isAlive);
       myHeapDumpButton.setEnabled(isAlive);
       myAllocationButton.setEnabled(isAlive);
-      myNativeAllocationButton.setEnabled(isAlive);
+      myNativeAllocationButton.setEnabled(isAlive && !isSelectedSessionDeviceX86OrX64());
       myAllocationSamplingRateDropDown.setEnabled(isAlive);
     };
     profilers.getSessionsManager().addDependency(this).onChange(SessionAspect.SELECTED_SESSION, toggleButtons);
@@ -457,6 +459,11 @@ public class MemoryProfilerStageView extends BaseMemoryProfilerStageView<MemoryP
     return myLayout.getCapturePanel().getCaptureInfoMessage();
   }
 
+  private boolean isSelectedSessionDeviceX86OrX64() {
+    String abi = getStage().getStudioProfilers().getSessionsManager().getSelectedSessionMetaData().getProcessAbi();
+    return abi.equalsIgnoreCase("x86") || abi.equalsIgnoreCase("x86_64");
+  }
+
   private void allocationTrackingChanged() {
     if (getStage().isTrackingAllocations()) {
       myAllocationButton.setText(STOP_TEXT);
@@ -468,13 +475,14 @@ public class MemoryProfilerStageView extends BaseMemoryProfilerStageView<MemoryP
       myCaptureElapsedTime.setText(TimeFormatter.getSemiSimplifiedClockString(0));
     }
     else {
+      boolean isX86OrX64Device = isSelectedSessionDeviceX86OrX64();
       myCaptureElapsedTime.setText("");
       myAllocationButton.setText(RECORD_TEXT);
       myAllocationButton.setDisabledIcon(IconLoader.getDisabledIcon(StudioIcons.Profiler.Toolbar.RECORD));
       myAllocationButton.setToolTipText("Record memory allocations");
       myNativeAllocationButton.setText(RECORD_NATIVE_TEXT);
       myNativeAllocationButton.setDisabledIcon(IconLoader.getDisabledIcon(StudioIcons.Profiler.Toolbar.RECORD));
-      myNativeAllocationButton.setToolTipText(RECORD_NATIVE_TEXT);
+      myNativeAllocationButton.setToolTipText(isX86OrX64Device ? X86_RECORD_NATIVE_TOOLTIP : RECORD_NATIVE_TEXT);
     }
   }
 
@@ -819,8 +827,9 @@ public class MemoryProfilerStageView extends BaseMemoryProfilerStageView<MemoryP
     myCaptureObject = getStage().getCaptureSelection().getSelectedCapture();
     if (myCaptureObject == null) {
       boolean isAlive = getStage().getStudioProfilers().getSessionsManager().isSessionAlive();
+      boolean isX86OrX64Device = isSelectedSessionDeviceX86OrX64();
       myAllocationButton.setEnabled(isAlive);
-      myNativeAllocationButton.setEnabled(isAlive);
+      myNativeAllocationButton.setEnabled(isAlive && !isX86OrX64Device);
       myHeapDumpButton.setEnabled(isAlive);
       myLayout.setShowingCaptureUi(false);
       return;
@@ -840,8 +849,9 @@ public class MemoryProfilerStageView extends BaseMemoryProfilerStageView<MemoryP
 
   private void captureObjectFinishedLoading() {
     boolean isAlive = getStage().getStudioProfilers().getSessionsManager().isSessionAlive();
+    boolean isX86OrX64Device = isSelectedSessionDeviceX86OrX64();
     myAllocationButton.setEnabled(isAlive);
-    myNativeAllocationButton.setEnabled(isAlive);
+    myNativeAllocationButton.setEnabled(isAlive && !isX86OrX64Device);
     // If the capture is an imported file, myRangeSelectionComponent is null.
     // If it is part of a profiler session, myRangeSelectionComponent is not null and should obtain the focus.
     if (myRangeSelectionComponent != null) {
