a8f4062e3123b50383ef129a5cb5815eed3681e0
parentej <parentej@google.com>
2020-05-18 15:58:44 +0100

    [NPW] Move ConfigureModuleStep.bytecodeCombo field
    ... to ConfigureAndroidModuleStep, since only
    "New Android Library" needs to show the dropdown
    for Bytecode Level.
    bytecodeCombo was made private.
    
    Bug: 148367249
    Test: Run TemplateTests
    Change-Id: Ic95b3ab1ad1d066013cfb74e64d997ecfc238d7a

--- android/src/com/android/tools/idea/npw/module/ConfigureAndroidModuleStep.kt
+++ android/src/com/android/tools/idea/npw/module/ConfigureAndroidModuleStep.kt
@@ -22,16 +22,18 @@ import com.android.tools.idea.npw.model.NewAndroidModuleModel
 import com.android.tools.idea.npw.model.RenderTemplateModel
 import com.android.tools.idea.npw.model.RenderTemplateModel.Companion.fromModuleModel
 import com.android.tools.idea.npw.template.ChooseActivityTypeStep
-import com.android.tools.idea.npw.validator.PackageNameValidator
+import com.android.tools.idea.npw.template.components.BytecodeLevelComboProvider
 import com.android.tools.idea.npw.validator.ProjectNameValidator
+import com.android.tools.idea.observable.ui.SelectedItemProperty
 import com.android.tools.idea.observable.ui.TextProperty
 import com.android.tools.idea.ui.wizard.StudioWizardStepPanel
 import com.android.tools.idea.wizard.model.ModelWizardStep
+import com.android.tools.idea.wizard.template.BytecodeLevel
 import com.intellij.openapi.ui.DialogPanel
 import com.intellij.ui.components.JBTextField
 import com.intellij.ui.layout.panel
-import org.jetbrains.android.util.AndroidBundle
 import org.jetbrains.android.util.AndroidBundle.message
+import javax.swing.JComboBox
 import javax.swing.JComponent
 import javax.swing.JTextField
 
@@ -43,6 +45,7 @@ class ConfigureAndroidModuleStep(
   title: String
 ) : ConfigureModuleStep<NewAndroidModuleModel>(model, formFactor, minSdkLevel, basePackage, title) {
   private val appName: JTextField = JBTextField(model.applicationName.get())
+  private val bytecodeCombo: JComboBox<BytecodeLevel> = BytecodeLevelComboProvider().createComponent()
 
   private val panel: DialogPanel = panel {
     row {
@@ -83,13 +86,14 @@ class ConfigureAndroidModuleStep(
 
   init {
     bindings.bindTwoWay(TextProperty(appName), model.applicationName)
+    bindings.bindTwoWay(SelectedItemProperty(bytecodeCombo), model.bytecodeLevel)
     validatorPanel.registerValidator(model.applicationName, ProjectNameValidator())
   }
 
   override fun createDependentSteps(): Collection<ModelWizardStep<*>> {
     val commonSteps = super.createDependentSteps()
 
-    val renderModel: RenderTemplateModel = fromModuleModel(model, AndroidBundle.message("android.wizard.activity.add", formFactor.id))
+    val renderModel: RenderTemplateModel = fromModuleModel(model, message("android.wizard.activity.add", formFactor.id))
     // Note: MultiTemplateRenderer needs that all Models constructed (ie myRenderModel) are inside a Step, so handleSkipped() is called
     val chooseActivityStep = ChooseActivityTypeStep.forNewModule(renderModel, formFactor, model.androidSdkInfo)
     chooseActivityStep.setShouldShow(!model.isLibrary)
