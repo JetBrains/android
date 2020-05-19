3b58006116fe2dd192258dd797c88c65ad5a8c0d
Phil Nguyen <philnguyen@google.com>
2020-05-19 11:43:07 -0700

    Fix potential `ConcurrentModificationException`
    Fixes: 157022496
    Test: n/a
    Change-Id: Id55b757b7e91db16d7cd8a5af5489e19ed4a613f

--- adt-ui-model/src/main/java/com/android/tools/adtui/model/RangeSelectionModel.java
+++ adt-ui-model/src/main/java/com/android/tools/adtui/model/RangeSelectionModel.java
@@ -104,7 +104,8 @@ public class RangeSelectionModel extends AspectModel<RangeSelectionModel.Aspect>
     }
 
     if (myEventToFire != null) {
-      myListeners.forEach(myEventToFire);
+      // `myEventToFire` may indirectly modify `myListeners`, so we iterate over a copy instead, e.g., b/157022496
+      new ArrayList<>(myListeners).forEach(myEventToFire);
       myEventToFire = null;
     }
   }
