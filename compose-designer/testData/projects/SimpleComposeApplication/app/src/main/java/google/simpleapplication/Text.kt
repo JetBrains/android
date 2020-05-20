e9a20c3ec7826aac15f2bf91248835e69e64d6c9
diegoperez <diegoperez@google.com>
2020-05-20 16:03:42 +0100

    Fix navigation when duplicate files are found
    When clicking on androidx.ui.foundation.Text we could end up navigating
    to a Text.kt within the current project. This is to the filename index
    lookup.
    
    This CL refactors the code to make sure we are navigating to the same
    file and also reduces the stop of the index search to just the current
    module with dependencies.
    
    Fixes: 157129712
    Test: New regression test
    Change-Id: I6bde2d1ee40f3860e99b7b8a12bf0c8ecd2bf0f4

---
+++ compose-designer/testData/projects/SimpleComposeApplication/app/src/main/java/google/simpleapplication/Text.kt
@@ -0,0 +1,6 @@
+// This file is just provided to confuse the file navigation and to test that
+// it does not navigate here when trying to navigate to androidx.ui.foundation.Text
+
+fun nothing() {
+
+}
