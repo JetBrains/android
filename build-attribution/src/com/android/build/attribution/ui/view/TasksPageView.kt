d1455e8f4abfddd68f33163f94dd252953a8796c
Maksim Lazeba <mlazeba@google.com>
2020-05-26 17:31:49 +0100

    [BA] visually separate navigation toolbar from the content
    Separate Navigation toolbar from the content using 1px line.
    Also add vertical margin for the details half.
    
    Bug: 155802637
    Test: visually verified
    Change-Id: I6e8269fc0a2da00422720d4605d09e7271ba93a7

--- build-attribution/src/com/android/build/attribution/ui/view/TasksPageView.kt
+++ build-attribution/src/com/android/build/attribution/ui/view/TasksPageView.kt
@@ -118,7 +118,7 @@ class TasksPageView(
     val detailsHalf: JPanel = JPanel().apply {
       val dimension = JBUI.size(20, 5)
       layout = BorderLayout(dimension.width(), dimension.height())
-      border = JBUI.Borders.emptyLeft(20)
+      border = JBUI.Borders.empty(5, 20)
       add(detailsPanel, BorderLayout.CENTER)
       add(chartsPanel, BorderLayout.WEST)
     }
