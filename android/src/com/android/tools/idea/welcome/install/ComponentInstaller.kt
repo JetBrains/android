dcd387f1f4c202e310d0991b01dd2f9ab8e98553
Valery Cherepanov <qumeric@google.com>
2020-05-27 12:46:14 +0100

    Move ComponentInstaller to Kotlin
    Bug: 76009501
    Test: existing
    Change-Id: I8db9046279b13cbc0f248092dcfd2debf7283b70
    
    GitOrigin-RevId: 2f65d2f66acc79ae1639332f812a3f8edec8d10c

--- android/src/com/android/tools/idea/welcome/install/ComponentInstaller.kt
+++ android/src/com/android/tools/idea/welcome/install/ComponentInstaller.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014 The Android Open Source Project
+ * Copyright (C) 2020 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,109 +13,95 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.android.tools.idea.welcome.install;
+package com.android.tools.idea.welcome.install
 
-import com.android.repository.api.*;
-import com.android.repository.impl.installer.BasicInstallerFactory;
-import com.android.repository.impl.meta.RepositoryPackages;
-import com.android.sdklib.repository.AndroidSdkHandler;
-import com.android.tools.idea.sdk.progress.StudioLoggerProgressIndicator;
-import com.android.tools.idea.sdk.progress.ThrottledProgressWrapper;
-import com.android.tools.idea.sdk.wizard.SdkQuickfixUtils;
-import com.google.common.collect.Lists;
-import com.google.common.collect.Sets;
-import org.jetbrains.annotations.NotNull;
-
-import java.util.*;
+import com.android.repository.api.Downloader
+import com.android.repository.api.InstallerFactory
+import com.android.repository.api.LocalPackage
+import com.android.repository.api.ProgressIndicator
+import com.android.repository.api.RemotePackage
+import com.android.repository.api.RepoManager
+import com.android.repository.impl.installer.BasicInstallerFactory
+import com.android.sdklib.repository.AndroidSdkHandler
+import com.android.tools.idea.sdk.progress.StudioLoggerProgressIndicator
+import com.android.tools.idea.sdk.progress.ThrottledProgressWrapper
+import com.android.tools.idea.sdk.wizard.SdkQuickfixUtils
+import com.android.tools.idea.sdk.wizard.SdkQuickfixUtils.PackageResolutionException
 
 /**
  * Installs SDK components.
  */
-public final class ComponentInstaller {
-  private final AndroidSdkHandler mySdkHandler;
-
-  public ComponentInstaller(@NotNull AndroidSdkHandler sdkHandler) {
-    mySdkHandler = sdkHandler;
-  }
-
-  public List<RemotePackage> getPackagesToInstall(@NotNull Iterable<? extends InstallableComponent> components)
-    throws SdkQuickfixUtils.PackageResolutionException {
+class ComponentInstaller(private val sdkHandler: AndroidSdkHandler) {
+  @Throws(PackageResolutionException::class)
+  fun getPackagesToInstall(components: Iterable<InstallableComponent>): List<RemotePackage> {
     // TODO: Prompt about connection in handoff case?
-    Set<UpdatablePackage> requests = Sets.newHashSet();
-    StudioLoggerProgressIndicator progress = new StudioLoggerProgressIndicator(getClass());
-    RepoManager sdkManager = mySdkHandler.getSdkManager(progress);
-    for (InstallableComponent component : components) {
-      requests.addAll(component.getPackagesToInstall());
-    }
-    List<UpdatablePackage> resolved = Lists.newArrayList();
-    resolved.addAll(SdkQuickfixUtils.resolve(requests, sdkManager.getPackages()));
-
-    List<RemotePackage> result = Lists.newArrayList();
-    for (UpdatablePackage p : resolved) {
-      result.add(p.getRemote());
-    }
-    return result;
+    val progress = StudioLoggerProgressIndicator(javaClass)
+    val sdkManager = sdkHandler.getSdkManager(progress)
+    val requests = components.flatMap { it.packagesToInstall }
+    return SdkQuickfixUtils.resolve(requests, sdkManager.packages).map { it.remote!! }
   }
 
-  public void installPackages(@NotNull List<RemotePackage> packages, @NotNull Downloader downloader, @NotNull ProgressIndicator progress)
-    throws WizardException {
-    progress = new ThrottledProgressWrapper(progress);
-    RepoManager sdkManager = mySdkHandler.getSdkManager(progress);
-    double progressMax = 0;
-    double progressIncrement = 0.9 / (packages.size() * 2.);
-    for (RemotePackage request : packages) {
+  fun installPackages(packages: List<RemotePackage>, downloader: Downloader, progress: ProgressIndicator) {
+    val throttledProgress = ThrottledProgressWrapper(progress)
+    val sdkManager = sdkHandler.getSdkManager(throttledProgress)
+    var progressMax = 0.0
+    val progressIncrement = 0.9 / (packages.size * 2.0)
+    val factory = BasicInstallerFactory()
+
+    packages.map {
+      factory.createInstaller(it, sdkManager, downloader, sdkHandler.fileOp)
+    }.forEach { installer ->
       // Intentionally don't register any listeners on the installer, so we don't recurse on haxm
       // TODO: This is a hack. Any future rewrite of this shouldn't require this behavior.
-      InstallerFactory factory = new BasicInstallerFactory();
-      Installer installer = factory.createInstaller(request, sdkManager, downloader, mySdkHandler.getFileOp());
-      progressMax += progressIncrement;
-      if (installer.prepare(progress.createSubProgress(progressMax))) {
-        installer.complete(progress.createSubProgress(progressMax + progressIncrement));
+      progressMax += progressIncrement
+      if (installer.prepare(throttledProgress.createSubProgress(progressMax))) {
+        installer.complete(throttledProgress.createSubProgress(progressMax + progressIncrement))
       }
-      progressMax += progressIncrement;
-      progress.setFraction(progressMax);
+      progressMax += progressIncrement
+      throttledProgress.fraction = progressMax
     }
-    sdkManager.loadSynchronously(RepoManager.DEFAULT_EXPIRATION_PERIOD_MS, progress.createSubProgress(1), null, null);
+
+    sdkManager.loadSynchronously(RepoManager.DEFAULT_EXPIRATION_PERIOD_MS, throttledProgress.createSubProgress(1.0), null, null)
   }
 
-  public void ensureSdkPackagesUninstalled(@NotNull Collection<String> packageNames, @NotNull ProgressIndicator progress) {
-    RepoManager sdkManager = mySdkHandler.getSdkManager(progress);
-    RepositoryPackages packages = sdkManager.getPackages();
-    Map<String, LocalPackage> localPackages = packages.getLocalPackages();
-    List<LocalPackage> packagesToUninstall = new ArrayList<>();
-
-    for (String packageName : packageNames) {
-      LocalPackage p = localPackages.get(packageName);
+  fun ensureSdkPackagesUninstalled(packageNames: Collection<String>, progress: ProgressIndicator) {
+    val sdkManager = sdkHandler.getSdkManager(progress)
+    val localPackages = sdkManager.packages.localPackages
+    val packagesToUninstall = mutableListOf<LocalPackage>()
+    for (packageName in packageNames) {
+      val p = localPackages[packageName]
       if (p != null) {
-        packagesToUninstall.add(p);
+        packagesToUninstall.add(p)
       }
       else {
-        progress.logInfo(String.format("Package '%1$s' does not appear to be installed - ignoring", packageName));
+        progress.logInfo("Package '$packageName' does not appear to be installed - ignoring")
       }
     }
-    double progressMax = 0;
-    double progressIncrement = 0.9 / (packagesToUninstall.size() * 2.);
-    for (LocalPackage request : packagesToUninstall) {
-      // This is pretty much symmetric to the installPackages() method above, so the same comments apply.
-      // Should we have registered listeners, HaxmInstallListener would have invoked another instance of HaxmWizard.
-      // The good news is that as of writing this,
-      // this class is used in Welcome and Haxm wizards only, and plays the role of a utility class.
-      // If we have more packages which require custom pre- and post-installation steps like Haxm,
-      // then we might still need a way to invoke non-recursive / listener-free uninstall operations for cleanup purposes
-      // It's possible that a change in packaging API would make sense to support that later -
-      // there is already some cleanup() support in operation chain implementation, but its limitation is that cleanup()
-      // is executed unconditionally, whereas in most cases it should be dependent on the next operation success status -
-      // like stack unwinding after an exception.
-      InstallerFactory factory = new BasicInstallerFactory();
-      Uninstaller uninstaller = factory.createUninstaller(request, sdkManager, mySdkHandler.getFileOp());
-      progressMax += progressIncrement;
+    var progressMax = 0.0
+    val progressIncrement = 0.9 / (packagesToUninstall.size * 2.0)
+    val factory: InstallerFactory = BasicInstallerFactory()
+
+    // This is pretty much symmetric to the installPackages() method above, so the same comments apply.
+    // Should we have registered listeners, HaxmInstallListener would have invoked another instance of HaxmWizard.
+    // The good news is that as of writing this,
+    // this class is used in Welcome and Haxm wizards only, and plays the role of a utility class.
+    // If we have more packages which require custom pre- and post-installation steps like Haxm,
+    // then we might still need a way to invoke non-recursive / listener-free uninstall operations for cleanup purposes
+    // It's possible that a change in packaging API would make sense to support that later -
+    // there is already some cleanup() support in operation chain implementation, but its limitation is that cleanup()
+    // is executed unconditionally, whereas in most cases it should be dependent on the next operation success status -
+    // like stack unwinding after an exception.
+    packagesToUninstall.map {
+      factory.createUninstaller(it, sdkManager, sdkHandler.fileOp)
+    }.forEach { uninstaller ->
+      progressMax += progressIncrement
       if (uninstaller.prepare(progress.createSubProgress(progressMax))) {
-        uninstaller.complete(progress.createSubProgress(progressMax + progressIncrement));
+        uninstaller.complete(progress.createSubProgress(progressMax + progressIncrement))
       }
-      progressMax += progressIncrement;
-      progress.setFraction(progressMax);
+      progressMax += progressIncrement
+      progress.fraction = progressMax
     }
-    sdkManager.loadSynchronously(RepoManager.DEFAULT_EXPIRATION_PERIOD_MS, progress.createSubProgress(1), null, null);
-    progress.setFraction(1);
+    sdkManager.loadSynchronously(RepoManager.DEFAULT_EXPIRATION_PERIOD_MS, progress.createSubProgress(1.0), null, null)
+    progress.fraction = 1.0
   }
 }
