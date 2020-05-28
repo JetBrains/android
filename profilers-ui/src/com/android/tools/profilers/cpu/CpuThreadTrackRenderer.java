ba718d66ad70f895382c93d3d598308a816eac8f
yiyiyang <yiyiyang@google.com>
2020-05-28 09:57:35 -0700

    Refresh thread call charts when filter is applied
    ...to the root node (likely from Top Down or Bottom Up trees) so
    that we can see instantly filter matches in the thread tracks.
    
    We're doing two things in order for the FILTER_APPLIED aspect to
    propgate to UI:
    - Update the HTreeChart in the track to mark the model dirty
    - Refresh the track groups in CpuCaptureStageView to repaint
    the JLists
    
    Fixes: 156646780
    Test: CaptureNodeTest
    Change-Id: I7d9515dac6c4b7c5ff406284d0f43e11bc28b88d

--- profilers-ui/src/com/android/tools/profilers/cpu/CpuThreadTrackRenderer.java
+++ profilers-ui/src/com/android/tools/profilers/cpu/CpuThreadTrackRenderer.java
@@ -27,7 +27,6 @@ import com.android.tools.adtui.model.StateChartModel;
 import com.android.tools.adtui.model.trackgroup.TrackModel;
 import com.android.tools.adtui.trackgroup.TrackRenderer;
 import com.android.tools.adtui.util.SwingUtil;
-import com.android.tools.profiler.proto.Cpu;
 import com.android.tools.profilers.ProfilerColors;
 import com.android.tools.profilers.ProfilerTrackRendererType;
 import com.android.tools.profilers.StudioProfilersView;
@@ -175,6 +174,11 @@ public class CpuThreadTrackRenderer implements TrackRenderer<CpuThreadTrackModel
       chart.addMouseListener(handler);
       myProfilersView.getIdeProfilerComponents().createContextMenuInstaller()
         .installNavigationContextMenu(chart, navigator, handler::getCodeLocation);
+    }
+    if (node != null) {
+      // Force the call chart to update when a filter is applied to the root node. By setting the root to the same node we're not changing
+      // the tree model but just triggering a model-changed event.
+      node.getAspectModel().addDependency(myObserver).onChange(CaptureNode.Aspect.FILTER_APPLIED, () -> chart.setHTree(node));
     }
     return chart;
   }
