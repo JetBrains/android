f90cb70cb1d5eeb1ab15214bf12139692d6c9ab4
diegoperez <diegoperez@google.com>
2020-05-28 10:14:42 +0100

    Create androidx integration tests
    Bug: 160271731
    Test: Executed test in the androidx-studio-integration branch
    Change-Id: I5afd7d870770d6e0788f92a0f4bd6f651a556b5b
    
    GitOrigin-RevId: 7a9639b27622f2aa109186c919b84de0e9918483

---
+++ androidx-integration-tests/testData/projects/SimpleComposeApplication/app/src/main/java/google/simpleapplication/MainActivity.kt
@@ -0,0 +1,58 @@
+package google.simpleapplication
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Text
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.setContent
+import androidx.ui.tooling.preview.Preview
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MaterialTheme {
+                Greeting("Android")
+            }
+        }
+    }
+}
+
+/**
+ * Greeting element.
+ *
+ * @sample DefaultPreview
+ */
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview
+@Composable
+fun DefaultPreview() {
+    MaterialTheme {
+        Greeting("Android")
+    }
+}
+
+@Preview
+@Composable
+fun TwoElementsPreview() {
+    MaterialTheme {
+        Column {
+            Text("Hello 2")
+            Button(onClick = {}) {
+                Text("Hello World")
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun NavigatablePreview() {
+  PreviewInOtherFile()
+}
