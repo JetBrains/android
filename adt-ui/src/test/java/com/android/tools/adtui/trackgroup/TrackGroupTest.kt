713a3a83ceb74ca4097d1471e89cd2546be087a2
yiyiyang <yiyiyang@google.com>
2020-05-29 18:43:20 -0700

    Track group help tooltip refactoring
    - Switched to IDEA's HelpTooltip
    - Added link text and URL to TrackGroupModel
    - Added help text and link to the Dispaly track group
    - Disabled tests that use ReferenceWalker due to StackOverflow
    with ApplicationRule
    
    Fixes: 156378304
    Test: visual verification (HelpTooltip is not found in componenet
          tree and doesn't provide easy getters)
    Change-Id: I568b8d2c495c01f527dc18e2b6c75a69b9e5008d
    (cherry picked from commit 797857e3b6e21f45f08c2880d53b2aab67af5152)

--- adt-ui/src/test/java/com/android/tools/adtui/trackgroup/TrackGroupTest.kt
+++ adt-ui/src/test/java/com/android/tools/adtui/trackgroup/TrackGroupTest.kt
@@ -27,6 +27,7 @@ import com.android.tools.adtui.swing.FakeKeyboard
 import com.android.tools.adtui.swing.FakeUi
 import com.android.tools.adtui.swing.laf.HeadlessListUI
 import com.google.common.truth.Truth.assertThat
+import com.intellij.testFramework.ApplicationRule
 import com.intellij.testFramework.EdtRule
 import com.intellij.testFramework.RunsInEdt
 import org.junit.Rule
@@ -41,6 +42,12 @@ class TrackGroupTest {
 
   @get:Rule
   val edtRule = EdtRule()
+
+  /**
+   * For initializing [com.intellij.ide.HelpTooltip].
+   */
+  @get:Rule
+  val appRule = ApplicationRule()
 
   @Test
   fun createTrackGroup() {
@@ -102,10 +109,9 @@ class TrackGroupTest {
     assertThat(noInfoTrackGroup.titleInfoIcon.isVisible).isFalse()
     assertThat(noInfoTrackGroup.titleInfoIcon.toolTipText).isNull()
 
-    val infoTrackGroupModel = TrackGroupModel.newBuilder().setTitle("Bar").setTitleInfo("Information").build()
+    val infoTrackGroupModel = TrackGroupModel.newBuilder().setTitle("Bar").setTitleHelpText("Information").build()
     val infoTrackGroup = TrackGroup(infoTrackGroupModel, TestTrackRendererFactory())
     assertThat(infoTrackGroup.titleInfoIcon.isVisible).isTrue()
-    assertThat(infoTrackGroup.titleInfoIcon.toolTipText).isEqualTo("Information")
   }
 
   @Test
