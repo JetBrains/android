12360029495cc6354ae257bc487db40edfaff30d
Yuriy Solodkyy <solodkyy@google.com>
2020-05-31 20:43:48 +0100

    Enable support for different types of project dumps
    Bug: n/a
    Test: n/a
    Change-Id: Iae33b580222a020e68c22c12870c022848b9ce48
    
    GitOrigin-RevId: c75ed85b4a26fc61e3c5d3940b76e366df09458e

--- android-test-framework/testSrc/com/android/tools/idea/testing/SnaphotComparisonTestHelpers.kt
+++ android-test-framework/testSrc/com/android/tools/idea/testing/SnaphotComparisonTestHelpers.kt
@@ -17,21 +17,27 @@ package com.android.tools.idea.testing
 
 import com.android.testutils.TestUtils
 import com.android.tools.idea.gradle.project.sync.internal.ProjectDumper
+import com.android.tools.idea.gradle.project.sync.internal.dumpProject
 import com.android.tools.idea.gradle.util.EmbeddedDistributionPaths
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.io.FileUtil
 import java.io.File
 
+typealias ProjectDumpAction = (project: Project, projectDumper: ProjectDumper) -> Unit
+
 /**
  * Returns a human readable environment independent stable representation of the current structure of the project that can be used in tests
  * to ensure that no unintended changes are accidentally introduced to projects set up by sync/import/etc.
  */
-fun Project.saveAndDump(additionalRoots: Map<String, File> = emptyMap()): String {
+fun Project.saveAndDump(
+  additionalRoots: Map<String, File> = emptyMap(),
+  dumpToAction: ProjectDumpAction = { project, projectDumper -> projectDumper.dumpProject(project) }
+): String {
   save()
   ApplicationManager.getApplication().saveAll()
   val dumper = ProjectDumper(androidSdk = TestUtils.getSdk(), offlineRepos = getOfflineM2Repositories(), additionalRoots = additionalRoots)
-  dumper.dump(this)
+  dumpToAction(this, dumper)
   return dumper.toString()
 }
 
