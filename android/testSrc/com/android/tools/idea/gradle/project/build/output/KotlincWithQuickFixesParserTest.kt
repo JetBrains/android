82fa0f1769de6685c20a7a8985f58800c55d4829
Sergio Murguia <srmurguia@google.com>
2020-06-02 10:32:46 -0700

    Wrap KotlincOuputParser in a new parser
    ... so we can add more information and quick fixes when there are errors
    from the Kotlin compiler.
    
    This cl also adds a quickfix to set java language level and jvmTarget to
    1.8 when there are error messages similar to this one:
    
    "Cannot inline bytecode built with JVM target 1.8 into bytecode that is
    being built with JVM target 1.6. Please specify proper '-jvm-target'
    option"
    
    and a link to more information, pointing to this address:
    
    https://developer.android.com/studio/write/java8-support
    
    Test: new test in KotlincWithQuickFixesParserTest.kt
    Bug: 147776458
    Change-Id: I0cfcb9b38ecb7f9838ab1e961fc8d69871ca97b6
    (cherry picked from commit dc59d66d13ac4a31538c57d40d6ff1b84343adf2)

---
+++ android/testSrc/com/android/tools/idea/gradle/project/build/output/KotlincWithQuickFixesParserTest.kt
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.gradle.project.build.output
+
+import com.android.tools.idea.gradle.project.sync.quickFixes.AbstractSetLanguageLevel8QuickFix
+import com.android.tools.idea.gradle.project.sync.quickFixes.OpenLinkQuickFix
+import com.google.common.truth.Truth.assertThat
+import com.intellij.build.events.BuildEvent
+import com.intellij.build.events.BuildIssueEvent
+import com.intellij.build.output.BuildOutputInstantReader
+import org.junit.Before
+import org.junit.Test
+import org.mockito.ArgumentCaptor
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.MockitoAnnotations
+import java.util.function.Consumer
+
+class KotlincWithQuickFixesParserTest {
+  @Mock
+  lateinit var reader: BuildOutputInstantReader
+  @Mock
+  lateinit var messageConsumer: Consumer<in BuildEvent?>
+
+  private lateinit var parser: KotlincWithQuickFixesParser
+
+  @Before
+  fun setUp() {
+    MockitoAnnotations.initMocks(this)
+    parser = KotlincWithQuickFixesParser()
+    Mockito.`when`(reader.parentEventId).thenReturn("testId")
+  }
+
+  @Test
+  fun `Cannot inline bytecode with JVM target 8 has correct quickfixes`() {
+    val errorLine = "e: /path/file.kt: (42, 47): Cannot inline bytecode built with JVM target 1.8 into bytecode that is being built with JVM target 1.6. Please specify proper '-jvm-target' option"
+    val captor = ArgumentCaptor.forClass(BuildEvent::class.java)
+    assertThat(parser.parse(errorLine, reader, messageConsumer)).isTrue()
+    Mockito.verify(messageConsumer).accept(captor.capture())
+    val events = captor.allValues
+    assertThat(events).hasSize(1)
+    assertThat(events[0]).isInstanceOf(BuildIssueEvent::class.java)
+    val quickFixes = (events[0] as BuildIssueEvent).issue.quickFixes
+    assertThat(quickFixes).hasSize(2)
+    assertThat(quickFixes.filterIsInstance(AbstractSetLanguageLevel8QuickFix::class.java)).isNotEmpty()
+    assertThat(quickFixes.filterIsInstance(OpenLinkQuickFix::class.java)).isNotEmpty()
+  }
+}
