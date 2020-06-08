dd33cb44bd3c52db278cb82ba52dd237ff443d48
Yi Zhou <deanzhou@google.com>
2020-06-08 11:35:31 -0700

    Notify user when AGP version lower than required
    Bug: 156673459
    Test: unit test
    Change-Id: I48b1fb32b19d1b48d223dd2ce09872ddfc51d290
    (cherry picked from commit d9ae1cb044836de72e0ee0e25b83c1393972e277)

---
+++ mlkit/testSrc/com/android/tools/idea/mlkit/notifications/LowAgpVersionNotificationProviderTest.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.mlkit.notifications;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import com.android.ide.common.gradle.model.IdeAndroidProject;
+import com.android.ide.common.repository.GradleVersion;
+import com.android.tools.idea.gradle.project.model.AndroidModuleModel;
+import com.android.tools.idea.mlkit.viewer.TfliteModelFileEditor;
+import com.android.tools.idea.model.AndroidModel;
+import com.android.tools.idea.testing.AndroidProjectRule;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
+import org.jetbrains.android.facet.AndroidFacet;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+/**
+ * Unit tests for {@link LowAgpVersionNotificationProvider}.
+ */
+public class LowAgpVersionNotificationProviderTest {
+
+  @Rule
+  public AndroidProjectRule projectRule = AndroidProjectRule.inMemory();
+
+  @Mock private AndroidModuleModel myMockAndroidModuleModel;
+  @Mock private VirtualFile myMockVirtualFile;
+  @Mock private TfliteModelFileEditor myMockEditor;
+  private Project myProject;
+  private LowAgpVersionNotificationProvider myNotificationProvider;
+
+  @Before
+  public void setUp() {
+    MockitoAnnotations.initMocks(this);
+
+    AndroidFacet androidFacet = AndroidFacet.getInstance(projectRule.getModule());
+    when(myMockAndroidModuleModel.getAndroidProject()).thenReturn(mock(IdeAndroidProject.class));
+    AndroidModel.set(androidFacet, myMockAndroidModuleModel);
+
+    myProject = projectRule.getProject();
+    myNotificationProvider = new LowAgpVersionNotificationProvider();
+  }
+
+  @Test
+  public void lowAgpVersion_hasNotification() {
+    when(myMockAndroidModuleModel.getModelVersion()).thenReturn(GradleVersion.parse("4.0.0"));
+    assertThat(myNotificationProvider.createNotificationPanel(myMockVirtualFile, myMockEditor, myProject)).isNotNull();
+  }
+
+  @Test
+  public void qualifiedAgpVersion_noNotification() {
+    when(myMockAndroidModuleModel.getModelVersion()).thenReturn(GradleVersion.parse("4.2.0"));
+    assertThat(myNotificationProvider.createNotificationPanel(myMockVirtualFile, myMockEditor, myProject)).isNull();
+  }
+}
