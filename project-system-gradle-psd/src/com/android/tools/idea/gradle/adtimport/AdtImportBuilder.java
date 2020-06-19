78438d89f66a0a07ad0e3588b46c8a041d521c8a
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2020-06-19 15:35:06 +0200

    test code should use regular API to load project and not a custom one — migrate createProjectAndUseInLoadComponentStateMode ProjectRule — dispose project after each test class, do not reuse for all tests setPath — use Path to have a more strict code (system dependent vs independent paths) and ability to use in-memory fs in tests
    Change-Id: I4c6969e135d14c5ee670110a04e8d1a7239ad8bc

--- project-system-gradle-psd/src/com/android/tools/idea/gradle/adtimport/AdtImportBuilder.java
+++ project-system-gradle-psd/src/com/android/tools/idea/gradle/adtimport/AdtImportBuilder.java
@@ -172,7 +172,7 @@ public class AdtImportBuilder extends ProjectImportBuilder<String> {
         request.isNewProject = true;
         importer.importProjectNoSync(request);
         Path projectDir = getBaseDirPath(request.project).getAbsoluteFile().toPath();
-        PlatformProjectOpenProcessor.openExistingProject(projectDir, projectDir, new OpenProjectTask(true, null, false, false, project, null, false, true, null, null, null, null, null, -1, -1, true, false, true, false));
+        PlatformProjectOpenProcessor.openExistingProject(projectDir, projectDir, new OpenProjectTask(true, null, false, false, project, null, true, null, null, -1, -1, true, false, true, null, false, false, null, null, null));
       }
       else {
         GradleSyncInvoker.getInstance().requestProjectSync(project, TRIGGER_IMPORT_ADT_MODULE, syncListener);
