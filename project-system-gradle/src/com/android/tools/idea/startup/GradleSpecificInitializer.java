4ab13fb39c02d818e5c75ad94bb399fbf5467db4
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2020-06-22 21:28:51 +0200

    [android] omitting unneeded app lifecycle listeners
    The platform collects early notifications and displays them as soon as the UI becomes ready.
    
    (cherry picked from commit 526920efa435a92289e5ba47f27504fde6b15d21)
    
    Change-Id: I526920efa435a92289e5ba47f27504fde6b15d21
    
    GitOrigin-RevId: b905f77b200af0b843fb912dd5e2aada56bb45d1

--- project-system-gradle/src/com/android/tools/idea/startup/GradleSpecificInitializer.java
+++ project-system-gradle/src/com/android/tools/idea/startup/GradleSpecificInitializer.java
@@ -34,7 +34,6 @@ import com.android.tools.idea.ui.validation.validators.PathValidator;
 import com.android.tools.idea.welcome.config.FirstRunWizardMode;
 import com.android.tools.idea.welcome.wizard.AndroidStudioWelcomeScreenProvider;
 import com.android.utils.Pair;
-import com.intellij.ide.AppLifecycleListener;
 import com.intellij.ide.projectView.actions.MarkRootGroup;
 import com.intellij.ide.projectView.impl.MoveModuleToGroupTopLevel;
 import com.intellij.notification.Notification;
@@ -113,20 +112,12 @@ public class GradleSpecificInitializer implements ActionConfigurationCustomizer 
    */
   private static void checkInstallPath() {
     if (PathManager.getHomePath().contains("!")) {
-      final Application app = ApplicationManager.getApplication();
-
-      app.getMessageBus().connect(app).subscribe(AppLifecycleListener.TOPIC, new AppLifecycleListener() {
-        @Override
-        public void appStarting(Project project) {
-          app.invokeLater(() -> {
-            String message = String.format("%1$s must not be installed in a path containing '!' or Gradle sync will fail!",
-                                           ApplicationNamesInfo.getInstance().getProductName());
-            Notification notification = getNotificationGroup().createNotification(message, NotificationType.ERROR);
-            notification.setImportant(true);
-            Notifications.Bus.notify(notification);
-          });
-        }
-      });
+      String message = String.format(
+        "%1$s must not be installed in a path containing '!' or Gradle sync will fail!",
+        ApplicationNamesInfo.getInstance().getProductName());
+      Notification notification = getNotificationGroup().createNotification(message, NotificationType.ERROR);
+      notification.setImportant(true);
+      Notifications.Bus.notify(notification);
     }
   }
 
@@ -206,20 +197,9 @@ public class GradleSpecificInitializer implements ActionConfigurationCustomizer 
   }
 
   private static void addStartupWarning(@NotNull final String message, @Nullable final NotificationListener listener) {
-    final Application app = ApplicationManager.getApplication();
-
-    app.getMessageBus().connect(app).subscribe(AppLifecycleListener.TOPIC, new AppLifecycleListener() {
-      @Override
-      public void appStarting(Project project) {
-        app.invokeLater(() -> {
-          Notification notification =
-            getNotificationGroup().createNotification("SDK Validation", message, NotificationType.WARNING);
-          if (listener != null) notification.setListener(listener);
-          notification.setImportant(true);
-          Notifications.Bus.notify(notification);
-        });
-      }
-    });
+    Notification notification = getNotificationGroup().createNotification("SDK Validation", message, NotificationType.WARNING, listener);
+    notification.setImportant(true);
+    Notifications.Bus.notify(notification);
   }
 
   private static NotificationGroup getNotificationGroup() {
