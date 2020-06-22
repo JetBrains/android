a3eb4e8884a8979c22e9b7df6e8a578967341191
Christophe Rhodes <xof@google.com>
2020-06-22 12:30:58 +0100

    [AUA] test Java8Default on application modules
    Bug: 149565531
    Bug: 159612385
    Test: added
    Change-Id: Iddb4600e01b1f0d0ae0f7df9e6af4d4f4085e93f
    
    GitOrigin-RevId: 0b968007fd5a63fab5a1496bc370eab246f40d2e

---
+++ android/testData/upgrade/Java8Default/SimpleApplicationNoLanguageLevelExpected.gradle.kts
@@ -0,0 +1,41 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    compileSdkVersion(29)
+    buildToolsVersion = "29.0.3"
+
+    defaultConfig {
+        applicationId = "com.example.myapplication"
+        minSdkVersion(16)
+        targetSdkVersion(29)
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_7
+        targetCompatibility = JavaVersion.VERSION_1_7
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core:1.2.0")
+    implementation("androidx.appcompat:appcompat:1.1.0")
+    implementation("com.google.android.material:material:1.1.0")
+    implementation("androidx.constraintlayout:constraintlayout:1.1.3")
+    implementation("androidx.navigation:navigation-fragment:2.2.1")
+    implementation("androidx.navigation:navigation-ui:2.2.1")
+    testImplementation("junit:junit:4.13")
+    androidTestImplementation("androidx.test.ext:junit:1.1.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.2.0")
+}
