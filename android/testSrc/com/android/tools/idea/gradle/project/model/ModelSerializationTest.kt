2b4a79e5cb0f0fd7c9fd2f00ee68e33d410513ac
Yuriy Solodkyy <solodkyy@google.com>
2020-06-26 12:43:48 +0100

    Delete deprecated IdeDependencies
    Bug: n/a
    Test: n/a
    Change-Id: I834c3f223e7b23c27b37ccfd3d7f49be5e729787
    
    GitOrigin-RevId: c38f0cc46ce8f84ce196022df0c40a60cdfcb13f

--- android/testSrc/com/android/tools/idea/gradle/project/model/ModelSerializationTest.kt
+++ android/testSrc/com/android/tools/idea/gradle/project/model/ModelSerializationTest.kt
@@ -25,7 +25,6 @@ import com.android.ide.common.gradle.model.IdeApiVersion
 import com.android.ide.common.gradle.model.IdeBuildType
 import com.android.ide.common.gradle.model.IdeBuildTypeContainer
 import com.android.ide.common.gradle.model.IdeClassField
-import com.android.ide.common.gradle.model.IdeDependenciesImpl
 import com.android.ide.common.gradle.model.IdeDependencyGraphs
 import com.android.ide.common.gradle.model.IdeFilterData
 import com.android.ide.common.gradle.model.IdeGraphItem
@@ -56,7 +55,9 @@ import com.android.ide.common.gradle.model.IdeVariantImpl
 import com.android.ide.common.gradle.model.IdeVectorDrawablesOptions
 import com.android.ide.common.gradle.model.IdeViewBindingOptions
 import com.android.ide.common.gradle.model.ModelCache
+import com.android.ide.common.gradle.model.level2.IdeDependencies
 import com.android.ide.common.gradle.model.level2.IdeDependenciesFactory
+import com.android.ide.common.gradle.model.level2.IdeDependenciesImpl
 import com.android.ide.common.gradle.model.level2.IdeModuleLibrary
 import com.android.ide.common.gradle.model.stubs.AaptOptionsStub
 import com.android.ide.common.gradle.model.stubs.AndroidArtifactOutputStub
@@ -224,9 +225,7 @@ class ModelSerializationTest {
   fun level2Dependencies() = assertSerializable {
     // We use a local one to avoid changing the global one that is used for other tests.
     val localDependenciesFactory = IdeDependenciesFactory()
-    localDependenciesFactory.create(
-      IdeAndroidArtifactImpl(AndroidArtifactStub(), modelCache, dependenciesFactory, gradleVersion))
-      as com.android.ide.common.gradle.model.level2.IdeDependenciesImpl
+    localDependenciesFactory.create(AndroidArtifactStub()) as IdeDependenciesImpl
   }
 
   /*
@@ -276,10 +275,6 @@ class ModelSerializationTest {
   @Test
   fun classField() =
     assertSerializable { IdeClassField(ClassFieldStub()) }
-
-  @Test
-  fun dependencies() =
-    assertSerializable { IdeDependenciesImpl(DependenciesStub(), modelCache) }
 
   @Test
   fun dependencyGraphs() =
