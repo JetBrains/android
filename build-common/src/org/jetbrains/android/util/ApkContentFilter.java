f9de4013bee78d3f997dbf901c2798e8e1e5ab94
Andrei Kuznetsov <andrei.kuznetsov@jetbrains.com>
2020-06-28 01:02:26 +0300

    IDEA-CR-64296: IDEA-241862: Android JPS build (i.e. non-gradle) is not compatible with Java 11
    Get rid of deprecated `com.android.jarutils.SignedJarBuilder` (which uses `sun.misc.BASE64Encoder`), and re-write code to use apkzlib instead.
    
    (cherry picked from commit 6a21f4817c7d099b76a6e0c2b5224bb60766efa5)
    
    GitOrigin-RevId: 72fc6d0ccce7e8a8649f959a2f8b5806dddc023a

---
+++ build-common/src/org/jetbrains/android/util/ApkContentFilter.java
@@ -0,0 +1,47 @@
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+
+package org.jetbrains.android.util;
+
+public class ApkContentFilter {
+  public ApkContentFilter() {
+  }
+
+  // TODO: make it consistent with com.android.build.gradle.internal.dsl.PackagingOptions
+  public boolean checkEntry(String name) {
+    String[] segments = name.split("/");
+    if (segments.length == 0) {
+      return false;
+    } else {
+      for(int i = 0; i < segments.length - 1; ++i) {
+        if (!checkFolderForPackaging(segments[i])) {
+          return false;
+        }
+      }
+
+      String fileName = segments[segments.length - 1];
+      return checkFileForPackaging(fileName);
+    }
+  }
+
+  public static boolean checkFolderForPackaging(String folderName) {
+    return !folderName.equals("CVS") && !folderName.equals(".svn") && !folderName.equals("SCCS") && !folderName.equals("META-INF") && !folderName.startsWith("_");
+  }
+
+  public static boolean checkFileForPackaging(String fileName) {
+    String[] fileSegments = fileName.split("\\.");
+    String fileExt = "";
+    if (fileSegments.length > 1) {
+      fileExt = fileSegments[fileSegments.length - 1];
+    }
+
+    return checkFileForPackaging(fileName, fileExt);
+  }
+
+  public static boolean checkFileForPackaging(String fileName, String extension) {
+    if (fileName.charAt(0) == '.') {
+      return false;
+    } else {
+      return !"aidl".equalsIgnoreCase(extension) && !"java".equalsIgnoreCase(extension) && !"class".equalsIgnoreCase(extension) && !"scc".equalsIgnoreCase(extension) && !"swp".equalsIgnoreCase(extension) && !"package.html".equalsIgnoreCase(fileName) && !"overview.html".equalsIgnoreCase(fileName) && !".cvsignore".equalsIgnoreCase(fileName) && !".DS_Store".equals(fileName) && fileName.charAt(fileName.length() - 1) != '~';
+    }
+  }
+}
