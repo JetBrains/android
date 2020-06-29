16f65f2f424204bb607aebcf011baa82b74c9939
Andrei Kuznetsov <andrei.kuznetsov@jetbrains.com>
2020-06-29 16:37:21 +0300

    IDEA-CR-64296: IDEA-244744,IDEA-244767: missing libraries in Android JPS build
    `<compileServer.plugin classpath="..."` expects that all the jars are listed relative to plugin's `lib` dir. Use AndroidBuildProcessParametersProvider for libraries provided by the platform (i.e. not by the plugin) in order to avoid unsafe paths like "../../lib/javaee.jar"
    
    (cherry picked from commit 365a107b0b6149d30ff40f726b7893f59387b242)
    
    GitOrigin-RevId: a1310526b0ce9facc9645214ae26030585d3336f

--- android/src/org/jetbrains/android/compiler/AndroidBuildProcessParametersProvider.java
+++ android/src/org/jetbrains/android/compiler/AndroidBuildProcessParametersProvider.java
@@ -2,10 +2,17 @@ package org.jetbrains.android.compiler;
 
 import com.android.prefs.AndroidLocation;
 import com.google.common.collect.ImmutableList;
+import com.google.common.util.concurrent.internal.InternalFutureFailureAccess;
 import com.google.gson.Gson;
 import com.intellij.compiler.server.BuildProcessParametersProvider;
 import com.intellij.openapi.application.PathManager;
+import com.sun.istack.XMLStreamReaderToContentHandler;
+import com.sun.xml.bind.v2.runtime.JAXBContextImpl;
 import java.util.List;
+import javax.activation.DataSource;
+import javax.xml.bind.JAXBContext;
+import org.bouncycastle.cert.X509CertificateHolder;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.gradle.tooling.BuildException;
 import org.jetbrains.annotations.NotNull;
 
@@ -14,7 +21,22 @@ public class AndroidBuildProcessParametersProvider extends BuildProcessParameter
   @Override
   public List<String> getClassPath() {
     return ImmutableList.of(PathManager.getJarPathForClass(Gson.class),
+
+                            // guava
                             PathManager.getJarPathForClass(ImmutableList.class),    // guava
+                            PathManager.getJarPathForClass(InternalFutureFailureAccess.class), // guava/failureaccess (for apkzlib)
+
+                            // bouncycastle (for apksig)
+                            PathManager.getJarPathForClass(BouncyCastleProvider.class),  // bcprov
+                            PathManager.getJarPathForClass(X509CertificateHolder.class), // bcpkix
+
+                            //JAXB for studio.android.sdktools.sdklib on Java 11
+                            PathManager.getJarPathForClass(JAXBContext.class),                        // jaxb-api
+                            PathManager.getJarPathForClass(JAXBContextImpl.class),                    // jaxb-runtime
+                            PathManager.getJarPathForClass(XMLStreamReaderToContentHandler.class),    // istack-commons-runtime
+                            PathManager.getJarPathForClass(DataSource.class),                         // javaee.jar
+
+
                             PathManager.getJarPathForClass(AndroidLocation.class),
                             PathManager.getJarPathForClass(BuildException.class)); // gradle tooling
   }
