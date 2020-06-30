31172484bf188b803b2ee5941f965a33d10505ec
Andrei Kuznetsov <andrei.kuznetsov@jetbrains.com>
2020-06-30 23:36:52 +0300

    IDEA-CR-64428: IDEA-243103: Android: Fix "Cannot create Project ... because container is already disposed" at .AndroidProjectViewPane
    Project might have been disposed while the task was waiting for execution
    
    (cherry picked from commit 687dff66ed9e62b1ee8bde169f439ecc21982f73)
    
    Change-Id: I687dff66ed9e62b1ee8bde169f439ecc21982f73

--- android/src/com/android/tools/idea/navigator/AndroidProjectViewPane.java
+++ android/src/com/android/tools/idea/navigator/AndroidProjectViewPane.java
@@ -127,7 +127,7 @@ public class AndroidProjectViewPane extends AbstractProjectViewPSIPane {
             finally {
               isProcessingChanges.set(false);
             }
-          });
+          }, project.getDisposed());
         }
       }
     });
