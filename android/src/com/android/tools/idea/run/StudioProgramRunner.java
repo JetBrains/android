b8970b8b7179fe5b357e2f6b119726d361fe4056
Juan C Nuno <juancnuno@google.com>
2020-07-01 15:24:10 -0700

    Throw an exception
    Throw an exception if there are multiple devices in the execution target and
    the executor is not the default run executor
    
    Bug: 158601281
    Test: DefaultStudioProgramRunnerTest
    Change-Id: I37fbe3849527f66e028df1c1bfaa49f0eb97af53

--- android/src/com/android/tools/idea/run/StudioProgramRunner.java
+++ android/src/com/android/tools/idea/run/StudioProgramRunner.java
@@ -16,16 +16,20 @@
 package com.android.tools.idea.run;
 
 import com.android.tools.idea.gradle.project.sync.GradleSyncState;
+import com.android.tools.idea.run.deployment.AndroidExecutionTarget;
 import com.android.tools.idea.run.util.SwapInfo;
 import com.android.tools.idea.testartifacts.instrumented.AndroidTestRunConfiguration;
 import com.google.common.annotations.VisibleForTesting;
 import com.intellij.execution.ExecutionException;
 import com.intellij.execution.ExecutionResult;
 import com.intellij.execution.ExecutionTarget;
+import com.intellij.execution.Executor;
 import com.intellij.execution.RunnerAndConfigurationSettings;
 import com.intellij.execution.configurations.RunConfiguration;
 import com.intellij.execution.configurations.RunProfile;
 import com.intellij.execution.configurations.RunProfileState;
+import com.intellij.execution.executors.DefaultDebugExecutor;
+import com.intellij.execution.executors.DefaultRunExecutor;
 import com.intellij.execution.process.ProcessHandler;
 import com.intellij.execution.runners.DefaultProgramRunnerKt;
 import com.intellij.execution.runners.ExecutionEnvironment;
@@ -81,6 +85,13 @@ public abstract class StudioProgramRunner extends AndroidProgramRunner {
   @Nullable
   protected RunContentDescriptor doExecute(@NotNull final RunProfileState state, @NotNull final ExecutionEnvironment env)
     throws ExecutionException {
+
+    Project project = env.getProject();
+    Executor executor = env.getExecutor();
+    String executorId = executor.getId();
+
+    throwExceptionIfExecutorIdIsNotSupported(project, executorId);
+
     boolean showRunContent = env.getRunProfile() instanceof AndroidTestRunConfiguration;
     RunnerAndConfigurationSettings runnerAndConfigurationSettings = env.getRunnerAndConfigurationSettings();
 
@@ -91,15 +102,15 @@ public abstract class StudioProgramRunner extends AndroidProgramRunner {
     }
 
     FileDocumentManager.getInstance().saveAllDocuments();
-    ExecutionResult result = state.execute(env.getExecutor(), this);
+    ExecutionResult result = state.execute(executor, this);
 
     RunContentDescriptor descriptor = null;
     if (swapInfo != null && result != null) {
       // If we're hotswapping, we want to use the currently-running ContentDescriptor,
       // instead of making a new one (which "show"RunContent actually does).
-      RunContentManager manager = RunContentManager.getInstance(env.getProject());
+      RunContentManager manager = RunContentManager.getInstance(project);
       // Note we may still end up with a null descriptor since the user could close the tool tab after starting a hotswap.
-      descriptor = manager.findContentDescriptor(env.getExecutor(), result.getProcessHandler());
+      descriptor = manager.findContentDescriptor(executor, result.getProcessHandler());
     }
 
     if (descriptor == null || descriptor.getAttachedContent() == null) {
@@ -125,12 +136,26 @@ public abstract class StudioProgramRunner extends AndroidProgramRunner {
 
       RunProfile runProfile = env.getRunProfile();
       RunConfiguration runConfiguration = runProfile instanceof RunConfiguration ? (RunConfiguration)runProfile : null;
-      AndroidSessionInfo.create(processHandler, descriptor, runConfiguration, env.getExecutor().getId(), env.getExecutor().getActionName(),
-                                env.getExecutionTarget()
-      );
+      AndroidSessionInfo.create(processHandler, descriptor, runConfiguration, executorId, executor.getActionName(),
+                                env.getExecutionTarget());
     }
 
     return descriptor;
+  }
+
+  private void throwExceptionIfExecutorIdIsNotSupported(@NotNull Project project, @NotNull String executorId) throws ExecutionException {
+    if (((AndroidExecutionTarget)myGetActiveTarget.apply(project)).getAvailableDeviceCount() <= 1) {
+      return;
+    }
+
+    switch (executorId) {
+      case DefaultRunExecutor.EXECUTOR_ID:
+        return;
+      case DefaultDebugExecutor.EXECUTOR_ID:
+        throw new ExecutionException("Debugging is not supported on multiple devices");
+      default:
+        throw new ExecutionException("The " + executorId + " executor is not supported on multiple devices");
+    }
   }
 
   @VisibleForTesting
