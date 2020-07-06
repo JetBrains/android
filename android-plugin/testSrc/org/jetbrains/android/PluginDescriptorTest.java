c4a45419f4b247ff935a6f677edb8ef5d9e5307f
Andrei Kuznetsov <andrei.kuznetsov@jetbrains.com>
2020-07-07 00:05:06 +0300

    Fix Android Plugin description
    Which appears in plugin manager as "IDE support for running Android Lint..." (from lint-plugin.xml) instead of "Supports the development of Android applications..." (from plugin.xml), because plugin.xml includes lint-plugin.xml which has <description> tag.
    
    GitOrigin-RevId: 4ca6452e40007f121e1305f5f04e520da723b9ec

---
+++ android-plugin/testSrc/org/jetbrains/android/PluginDescriptorTest.java
@@ -0,0 +1,19 @@
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package org.jetbrains.android;
+
+import com.intellij.ide.plugins.IdeaPluginDescriptor;
+import com.intellij.ide.plugins.PluginManager;
+import com.intellij.openapi.extensions.PluginId;
+import com.intellij.testFramework.LightPlatformTestCase;
+import org.jetbrains.annotations.Nullable;
+
+public class PluginDescriptorTest extends LightPlatformTestCase {
+  public void testPluginDescription() {
+    @Nullable IdeaPluginDescriptor androidPlugin =
+      PluginManager.getInstance().findEnabledPlugin(PluginId.getId("org.jetbrains.android"));
+
+    assertNotNull(androidPlugin);
+    assertTrue(androidPlugin.getDescription().startsWith("Supports the development of"));
+    assertFalse(androidPlugin.getDescription().startsWith("IDE support for running Android Lint"));
+  }
+}
