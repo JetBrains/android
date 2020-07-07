7e46df28ba1fd5456fe095d1a5bef2f4d115e731
Chris Warrington <cmw@google.com>
2020-07-07 15:09:12 +0100

    DSL: Make matchingFallbacks implementation val Mutable
    To allow e.g.
    matchingFallbacks += "a"
    in kotlin scripts.
    
    See I07034f1be81ff5e387a056a7fe98a32b90f3258c
    
    Updates the test projects to be compatible with this change.
    
    Bug: 140406102
    Bug: 156169950
    Test: ExtractVariableWorkerTest
    Test: PsBuildTypeTest
    Test: AndroidModuleDescriptorsTest
    Test: PsAndroidModuleDefaultConfigDescriptorsTest
    Test: PsProductFlavorTest
    Change-Id: Ibe8da6ef9763040d5fc5c73768a5fe5ea0d4f097

--- android/testData/projects/psdSample/Kotlin/app/build.gradle.kts
+++ android/testData/projects/psdSample/Kotlin/app/build.gradle.kts
@@ -54,7 +54,7 @@ android {
             proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.txt", "proguard-rules2.txt")
         }
         create("specialRelease") {
-            matchingFallbacks = mutableListOf("release", "debug")
+            setMatchingFallbacks(listOf("release", "debug"))
             versionNameSuffix = "vnsSpecial"
         }
     }
@@ -84,7 +84,7 @@ android {
         }
         create("otherBar") {
             setDimension("bar")
-            matchingFallbacks = mutableListOf("bar")
+            setMatchingFallbacks(listOf("bar"))
             resConfig("en")
             resConfigs("hdpi", "xhdpi")
         }
--- gradle-dsl/testData/parser/productFlavorModel/deleteMatchingFallbacks.gradle.kts
+++ gradle-dsl/testData/parser/productFlavorModel/deleteMatchingFallbacks.gradle.kts
@@ -3,7 +3,7 @@ android {
   productFlavors {
     create("demo") {
       setDimension("tier")
-      matchingFallbacks = mutableListOf("trial")
+      setMatchingFallbacks(listOf("trial"))
     }
   }
 }
