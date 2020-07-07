038ee7b1406f4bee0a029a5ff6284c6f26fa7ae5
Yuriy Solodkyy <solodkyy@google.com>
2020-07-07 17:44:00 +0100

    Use reliable robot when dealing when open projects
    Bug: n/a
    Test: n/a
    Change-Id: I3ab6729967cda8fbec925f746bc18ca4553616be

--- uitest-framework/testSrc/com/android/tools/idea/tests/gui/framework/GuiTestRule.java
+++ uitest-framework/testSrc/com/android/tools/idea/tests/gui/framework/GuiTestRule.java
@@ -18,6 +18,7 @@ package com.android.tools.idea.tests.gui.framework;
 import static com.android.testutils.TestUtils.getWorkspaceFile;
 import static com.android.tools.idea.testing.FileSubject.file;
 import static com.android.tools.idea.tests.gui.framework.GuiTests.refreshFiles;
+import static com.android.tools.idea.tests.gui.framework.UiTestUtilsKt.fixupWaiting;
 import static com.android.tools.idea.tests.gui.framework.fixture.IdeFrameFixture.actAndWaitForGradleProjectSyncToFinish;
 import static com.google.common.base.Charsets.UTF_8;
 import static com.google.common.io.Files.asCharSource;
@@ -352,7 +353,7 @@ public class GuiTestRule implements TestRule {
   public IdeFrameFixture openProjectAndWaitForProjectSyncToFinish(@NotNull File projectDir, @NotNull Wait waitForSync) {
     ApplicationManager.getApplication().invokeAndWait(() -> ProjectUtil.openOrImport(projectDir.getAbsolutePath(), null, true));
     Wait.seconds(5).expecting("Project to be open").until(() -> ProjectManager.getInstance().getOpenProjects().length != 0);
-    return actAndWaitForGradleProjectSyncToFinish(waitForSync, () -> ideFrame());
+    return actAndWaitForGradleProjectSyncToFinish(waitForSync, () -> reliableIdeFrame());
   }
 
   /**
@@ -504,6 +505,13 @@ public class GuiTestRule implements TestRule {
       myIdeFrameFixture.requestFocusIfLost();
     }
     return myIdeFrameFixture;
+  }
+
+  @NotNull
+  public IdeFrameFixture reliableIdeFrame() {
+    IdeFrameFixture result = IdeFrameFixture.find(fixupWaiting(robot()));
+    result.requestFocusIfLost();
+    return result;
   }
 
   @NotNull
