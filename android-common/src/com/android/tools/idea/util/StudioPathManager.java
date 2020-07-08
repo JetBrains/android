555218d6d15d9e3f60347593a502c6c05d4b5617
Emre Kultursay <emrekultursay@google.com>
2020-07-08 14:18:14 -0700

    Adds StudioPathManager...
    ...to use instead of PathManager#isRunningFromSources()
    
    PathManager#isRunningFromSources() does not work for unbundled builds,
    nor bazel tests.
    
    StudioPathManager.getRepoRoot() also eliminates the need to add leading
    ../../ (or ../../../../../../ for unbundled) to find prebuilts.
    
    Test: n/a
    Bug: 156497102
    Change-Id: I9f612d11e8a1a43814e728d33776a8cf9be3eff7
    
    GitOrigin-RevId: 8403ad11b6416316946a26a5151450a786a4a488

---
+++ android-common/src/com/android/tools/idea/util/StudioPathManager.java
@@ -0,0 +1,55 @@
+package com.android.tools.idea.util;
+
+import com.intellij.ide.plugins.PluginManagerCore;
+import com.intellij.openapi.application.PathManager;
+import java.nio.file.Paths;
+
+/**
+ * Code inside Android Studio (as opposed to code inside studio-sdk), should use the methods in this class
+ * instead of {@link PathManager#getHomePath()} or {@link PluginManagerCore#isRunningFromSources()}.
+ */
+public class StudioPathManager {
+  /**
+   * The name of the property that contains the relative path from {@link PathManager#getHomePath()} to repo root.
+   */
+  @SuppressWarnings("FieldCanBeLocal")
+  private static final String STUDIO_SOURCE_ROOT = "studio.source.root";
+
+  /**
+   * @return true if Android Studio is currently running from sources (works for both bundled and unbundled builds), as opposed to
+   * packaged release build.
+   */
+  public static boolean isRunningFromSources() {
+    return PluginManagerCore.isRunningFromSources() || isUnBundledDevBuild() || isRunningInBazelTest();
+  }
+
+  /**
+   * @return returns the root of the Android repo when Android Studio is running from sources (works for both
+   * bundled or unbundled builds).
+   */
+  public static String getRepoRoot() {
+    assert isRunningFromSources();
+    if (isUnBundledDevBuild()) {
+      String studioSourceRoot = System.getProperty(STUDIO_SOURCE_ROOT);
+      return Paths.get(PathManager.getHomePath(), studioSourceRoot).normalize().toString();
+    }
+
+    // Bundled dev build, or bazel test.
+    //     PathManager.getHomePath(): $SRC/tools/idea
+    return Paths.get(PathManager.getHomePath(), "../..").normalize().toString();
+  }
+
+  /**
+   * @return true if running Android Studio from sources and this is an unbundled build.
+   */
+  private static boolean isUnBundledDevBuild() {
+    return System.getProperties().containsKey(STUDIO_SOURCE_ROOT);
+  }
+
+  /**
+   * Returns true if Android Studio is running inside a Bazel test environment.
+   */
+  private static boolean isRunningInBazelTest() {
+    return System.getenv().containsKey("TEST_WORKSPACE");
+  }
+}
