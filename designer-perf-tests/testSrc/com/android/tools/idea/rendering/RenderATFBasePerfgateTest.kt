d0fd372db7d869a00e813b65176c8f08ef7f4fbd
Isaac Chai <ichai@google.com>
2020-07-08 16:02:13 -0700

    [atf] add perfgate metrics
    Adding metrics for render so it can be directly compared against the
    benchmark we have of the studio.
    
    Bug: 158119426
    Test: Ran locally and seems to work. Once verified and landed, will need
    to check the perfgate server dashboard
    
    Change-Id: Ie8cf6da292bca024c5c786e5d1b3308b6644b9d1
    
    GitOrigin-RevId: c9c9c9b2c283e69802ff2adb384551d9eee0b4b0

---
+++ designer-perf-tests/testSrc/com/android/tools/idea/rendering/RenderATFBasePerfgateTest.kt
@@ -0,0 +1,70 @@
+/*
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.rendering
+
+import com.android.tools.idea.configurations.Configuration
+import com.android.tools.idea.res.FrameworkResourceRepositoryManager
+import com.android.tools.idea.validator.ValidatorResult
+import com.intellij.openapi.util.ThrowableComputable
+import com.intellij.openapi.vfs.VirtualFile
+import junit.framework.TestCase
+import org.jetbrains.android.AndroidTestCase
+
+class RenderATFBasePerfgateTest : AndroidTestCase() {
+
+  private lateinit var layoutFile: VirtualFile
+  private lateinit var layoutConfiguration: Configuration
+
+  @Throws(Exception::class)
+  override fun setUp() {
+    super.setUp()
+    RenderTestUtil.beforeRenderTestCase()
+
+    layoutFile = myFixture.addFileToProject("res/layout/layout.xml", SIMPLE_LAYOUT).virtualFile
+    layoutConfiguration = RenderTestUtil.getConfiguration(myModule, layoutFile)
+  }
+
+  @Throws(Exception::class)
+  override fun tearDown() {
+    try {
+      RenderTestUtil.afterRenderTestCase()
+    }
+    finally {
+      FrameworkResourceRepositoryManager.getInstance().clearCache()
+      super.tearDown()
+    }
+  }
+
+  @Throws(Exception::class)
+  fun testATFBaseRender() {
+    val computable = ThrowableComputable<PerfgateRenderMetric, Exception> {
+      var metric: PerfgateRenderMetric? = null
+      RenderTestUtil.withRenderTask(myFacet, layoutFile, layoutConfiguration, true) {
+        metric = getRenderMetric(it, {_ -> }, ::checkATFSimpleLayoutResult)
+      }
+      metric!!
+    }
+    computeAndRecordMetric("render_time_atf_base", "render_memory_atf_base", computable)
+  }
+
+  /**
+   * Asserts that the given result matches the [.SIMPLE_LAYOUT] structure
+   */
+  private fun checkATFSimpleLayoutResult(result: RenderResult) {
+    checkSimpleLayoutResult(result)
+    verifyValidatorResult(result)
+  }
+}
