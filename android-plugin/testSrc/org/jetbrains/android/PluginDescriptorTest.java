81c23d397994ba0b5fbaa1e5c8735fe7d4fa1197
Andrei Kuznetsov <andrei.kuznetsov@jetbrains.com>
2020-07-10 20:39:54 +0300

    IDEA-245623: restore missing Android action group (AndroidToolsGroup)
    LintBundle accidentally overrides AndroidBundle. Android's plugin.xml includes android-plugin.xml and lint-plugin.xml. Both declare `<resource-bundle>`, but inclusion of lint-plugin.xml appears later in the text, so LintBundle wins, and warning is displayed in IDEA logs. As a result, some messages were not found, action groups didn't have titles. Action groups with no title didn't appear in UI therefore AVD Manager action completely disappears from IDEA.
    
    (cherry picked from commit b1887a6025e7bf9a277f0b7646ee709a73fc0730)
    
    Change-Id: Ieba4fbabf1b1716cad97d8615cbbdbf712852609
    
    GitOrigin-RevId: 8baef71f59f02320dcb6856aae768a70410aa9ba

--- android-plugin/testSrc/org/jetbrains/android/PluginDescriptorTest.java
+++ android-plugin/testSrc/org/jetbrains/android/PluginDescriptorTest.java
@@ -6,6 +6,7 @@ import com.intellij.ide.plugins.PluginManager;
 import com.intellij.openapi.extensions.PluginId;
 import com.intellij.testFramework.LightPlatformTestCase;
 import org.jetbrains.annotations.Nullable;
+import org.junit.Assert;
 
 public class PluginDescriptorTest extends LightPlatformTestCase {
   public void testPluginDescription() {
@@ -15,5 +16,14 @@ public class PluginDescriptorTest extends LightPlatformTestCase {
     assertNotNull(androidPlugin);
     assertTrue(androidPlugin.getDescription().startsWith("Supports the development of"));
     assertFalse(androidPlugin.getDescription().startsWith("IDE support for running Android Lint"));
+  }
+
+  public void testPluginBundle() {
+    @Nullable IdeaPluginDescriptor androidPlugin =
+      PluginManager.getInstance().findEnabledPlugin(PluginId.getId("org.jetbrains.android"));
+
+    assertNotNull(androidPlugin);
+    assertEquals("messages.AndroidBundle", androidPlugin.getResourceBundleBaseName());
+    Assert.assertNotEquals("messages.LintBundle", androidPlugin.getResourceBundleBaseName());
   }
 }
