ab95b90cc5c41eecbe8601fa4f3a2319a73f09ab
Raluca Sauciuc <ralucas@google.com>
2020-07-10 19:36:24 -0700

    [JDK11] Fix new compilation errors
    The signature for javax.swing.table.DefaultTableModel's getDataVector
    changes from Vector to Vector<Vector>. With this change, the code can
    be compiled with either JDK.
    
    Bug: 135535322
    Test: N/A
    Change-Id: I23c954988b4d381653360d50ea179fe30d938016
    
    GitOrigin-RevId: dc12bc810ef74f36192feca1906033310e97efff

--- designer/src/com/android/tools/idea/uibuilder/handlers/motion/editor/ui/LayoutPanel.java
+++ designer/src/com/android/tools/idea/uibuilder/handlers/motion/editor/ui/LayoutPanel.java
@@ -181,9 +181,11 @@ class LayoutPanel extends JPanel {
       .filter(Objects::nonNull)
       .collect(Collectors.toSet());
 
+    // As of JDK 11 DefaultTableModel.getDataVector has generic type Vector<Vector>, so we need to cast the resulting element to String,
+    // such that the outer unchecked cast to Set<String> succeeds.
     //noinspection unchecked
     Set<String> found = (Set<String>)mConstraintSetModel.getDataVector().stream()
-      .map(row -> ((Vector)row).get(1))
+      .map(row -> (String)((Vector)row).get(1))
       .collect(Collectors.toSet());
 
     if (!ids.equals(found)) {
