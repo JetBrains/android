abc795c095845e73a61db17385bd2f6cb76de661
Jose Parente <parentej@google.com>
2020-07-14 16:47:08 +0100

    UI Tests for Left nav wizards
    Bug: 154564222
    Test: NewModuleTest
    Change-Id: Idfeea888186788b4f962b3da0930da5e12494f6c
    
    GitOrigin-RevId: 3d50346f8e5153ac306029076588f92974b0ce3d

--- uitest-framework/testSrc/com/android/tools/idea/tests/gui/framework/fixture/npw/NewModuleWizardFixture.java
+++ uitest-framework/testSrc/com/android/tools/idea/tests/gui/framework/fixture/npw/NewModuleWizardFixture.java
@@ -19,11 +19,12 @@ import static com.android.tools.idea.tests.gui.framework.GuiTests.waitUntilShowi
 import static org.jetbrains.android.util.AndroidBundle.message;
 
 import com.android.tools.adtui.ASGallery;
+import com.android.tools.idea.flags.StudioFlags;
 import com.android.tools.idea.npw.module.ModuleGalleryEntry;
-import com.android.tools.idea.tests.gui.framework.GuiTests;
 import com.android.tools.idea.tests.gui.framework.fixture.IdeFrameFixture;
 import com.android.tools.idea.tests.gui.framework.fixture.wizard.AbstractWizardFixture;
 import com.android.tools.idea.tests.gui.framework.matcher.Matchers;
+import com.intellij.ui.components.JBList;
 import javax.swing.JDialog;
 import org.fest.swing.core.matcher.JLabelMatcher;
 import org.fest.swing.fixture.JListFixture;
@@ -123,14 +124,16 @@ public class NewModuleWizardFixture extends AbstractWizardFixture<NewModuleWizar
   }
 
   private void clickNextToStep(String moduleName, String nextStepTitle) {
-    JListFixture listFixture = new JListFixture(robot(), robot().finder().findByType(target(), ASGallery.class));
+    JListFixture listFixture = new JListFixture(robot(), robot().finder().findByType(target(), JBList.class));
     listFixture.replaceCellReader((list, index) -> ((ModuleGalleryEntry)list.getModel().getElementAt(index)).getName());
     listFixture.clickItem(moduleName);
 
-    clickNext();
+    if (!StudioFlags.NPW_NEW_MODULE_WITH_SIDE_BAR.get()) {
+      clickNext();
 
-    Wait.seconds(5).expecting("next step to appear").until(
-      () -> robot().finder().findAll(target(), JLabelMatcher.withText(nextStepTitle).andShowing()).size() == 1);
+      Wait.seconds(5).expecting("next step to appear").until(
+        () -> robot().finder().findAll(target(), JLabelMatcher.withText(nextStepTitle).andShowing()).size() == 1);
+    }
   }
 
   private void clickFinish() {
