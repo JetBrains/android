c43b4af7221b546dd841ae15733294a08076f731
David Herman <davidherman@google.com>
2020-07-14 12:17:57 -0700

    Update AppInspectorTabProvider API to take a process descriptor
    Bug: N/A
    Test: Covered by existing
    Change-Id: I9c27926f0c4c34249d2b975722bf62dd2a03dd86

--- app-inspection/inspectors/workmanager/ide/src/com/android/tools/idea/appinspection/inspectors/workmanager/ide/WorkManagerInspectorTabProvider.kt
+++ app-inspection/inspectors/workmanager/ide/src/com/android/tools/idea/appinspection/inspectors/workmanager/ide/WorkManagerInspectorTabProvider.kt
@@ -18,6 +18,7 @@ package com.android.tools.idea.appinspection.inspectors.workmanager.ide
 import com.android.tools.idea.appinspection.inspector.api.AppInspectorClient
 import com.android.tools.idea.appinspection.inspector.api.AppInspectorJar
 import com.android.tools.idea.appinspection.inspector.api.AppInspectionIdeServices
+import com.android.tools.idea.appinspection.inspector.api.process.ProcessDescriptor
 import com.android.tools.idea.appinspection.inspector.ide.AppInspectorTab
 import com.android.tools.idea.appinspection.inspector.ide.AppInspectorTabProvider
 import com.android.tools.idea.appinspection.inspectors.workmanager.model.WorkManagerInspectorClient
@@ -38,8 +39,9 @@ class WorkManagerInspectorTabProvider : AppInspectorTabProvider {
   }
 
   override fun createTab(project: Project,
-                         messenger: AppInspectorClient.CommandMessenger,
-                         ideServices: AppInspectionIdeServices): AppInspectorTab {
+                         ideServices: AppInspectionIdeServices,
+                         processDescriptor: ProcessDescriptor,
+                         messenger: AppInspectorClient.CommandMessenger): AppInspectorTab {
     return object : AppInspectorTab {
       override val client = WorkManagerInspectorClient(messenger)
 
