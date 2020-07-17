c06d9f546cbb3af6367898d83588b926a6e6e739
Christophe Rhodes <xof@google.com>
2020-07-17 15:24:21 +0100

    [AUA] usages in KotlinScript Gradle build files
    We currently display a usage type of "... in Gradle build script" for
    Groovy usages in build files, but usage types corresponding to Kotlin
    semantics in KotlinScript build files.
    
    Fix the inconsistency by checking whether we are in a Gradle build
    file for Kotlin elements too, and further place the
    GradleUsageTypeProvider at the front of the list where it will take
    priority over the Kotlin plugin's UsageTypeProvider.  (Ideally these
    usage type providers would have ids in the plugin definitions so that
    we could use order="before <id>", but they don't).
    
    Bug: n/a
    Test: existing, added
    Change-Id: I6a0f1ec351c39483caba30f8cf1b70b3afcfd836
    
    GitOrigin-RevId: 898e90c580e1bc23e68ed6e1ba14e00993a1da02

--- android/testSrc/org/jetbrains/android/refactoring/UsageTypeProvidersTest.kt
+++ android/testSrc/org/jetbrains/android/refactoring/UsageTypeProvidersTest.kt
@@ -116,7 +116,7 @@ class UsageTypeProvidersTest : AndroidTestCase() {
   }
 
   /**
-   * Test for [GradleUsageTypeProvider]
+   * Tests for [GradleUsageTypeProvider]
    */
   fun testGroovyElement() {
     val file = myFixture.addFileToProject("Foo.gradle", "class F${caret}oo {}")
@@ -125,6 +125,36 @@ class UsageTypeProvidersTest : AndroidTestCase() {
     val usageType = getUsageType(elementAtCaret!!)
     assertThat(usageType).isNotNull()
     assertThat(usageType.toString()).isEqualTo("{0} in Gradle build script")
+  }
+
+  fun testKotlinScriptElement() {
+    val file = myFixture.addFileToProject("Foo.gradle.kts", "class F${caret}oo {}")
+    myFixture.configureFromExistingVirtualFile(file.virtualFile)
+    val elementAtCaret = file.findElementAt(myFixture.caretOffset)
+    val usageType = getUsageType(elementAtCaret!!)
+    assertThat(usageType).isNotNull()
+    assertThat(usageType.toString()).isEqualTo("{0} in Gradle build script")
+  }
+
+  // whatever is returned on a normal Kotlin file, it should not be related to Gradle (a null result is OK)
+  fun testKotlinClassNameElement() {
+    val file = myFixture.addFileToProject("Foo.kt", "class F${caret}oo {}")
+    myFixture.configureFromExistingVirtualFile(file.virtualFile)
+    val elementAtCaret = file.findElementAt(myFixture.caretOffset)
+    val usageType = getUsageType(elementAtCaret!!)
+    if (usageType != null) {
+      assertThat(usageType.toString()).doesNotContainMatch("Gradle")
+    }
+  }
+
+  fun testKotlinClassReferenceElement() {
+    val file = myFixture.addFileToProject("Foo.kt", "class Foo {}\n\nclass Bar : F${caret}oo {}")
+    myFixture.configureFromExistingVirtualFile(file.virtualFile)
+    val elementAtCaret = file.findElementAt(myFixture.caretOffset)
+    val usageType = getUsageType(elementAtCaret!!)
+    if (usageType != null) {
+      assertThat(usageType.toString()).doesNotContainMatch("Gradle")
+    }
   }
 
   /**
