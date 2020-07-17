fc7af5877d12f9349c27a322e73fee1a8967623c
Raluca Sauciuc <ralucas@google.com>
2020-07-16 17:06:39 -0700

    Fix tearDown sequence in tests written in Kotlin
    This pattern is flagged in the IDE but only in Java.
    
    Change-Id: Idb486c422dcca42d84bf447fb954ac0afa40398f
    
    GitOrigin-RevId: c8761e84872421c0e8ff09d2b0f481a23280cc6d

--- dagger/testSrc/com/android/tools/idea/dagger/DaggerUtilTest.kt
+++ dagger/testSrc/com/android/tools/idea/dagger/DaggerUtilTest.kt
@@ -1160,8 +1160,13 @@ class DaggerCrossModuleTest : UsefulTestCase() {
   }
 
   override fun tearDown() {
-    super.tearDown()
-    myFixture.tearDown()
+    try {
+      myFixture.tearDown()
+    } catch (t: Throwable) {
+      addSuppressedException(t)
+    } finally {
+      super.tearDown()
+    }
   }
 
   private fun newModule(projectBuilder: TestFixtureBuilder<IdeaProjectTestFixture>, contentRoot: String): ModuleFixture {
