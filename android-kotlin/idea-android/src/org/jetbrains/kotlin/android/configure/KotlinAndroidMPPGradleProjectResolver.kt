10372105a806359d232beb2f6ca186ff5fe8097b
Bradley Smith <smithbradley@google.com>
2020-07-23 07:03:46 -0700

    Do not call into KotlinMPPGradleProjectResolver
    This resolver is already run as part of project import, calling into it
    from here caused multiple mpp setup methods to be re-run.
    
    Bug: 160909456
    Bug: 156686357
    Test: MPP project to be added to snapshots
    Change-Id: Ia023e64d97b2c3b7b574658a3f56f493e2de19ff

--- android-kotlin/idea-android/src/org/jetbrains/kotlin/android/configure/KotlinAndroidMPPGradleProjectResolver.kt
+++ android-kotlin/idea-android/src/org/jetbrains/kotlin/android/configure/KotlinAndroidMPPGradleProjectResolver.kt
@@ -7,7 +7,7 @@ package org.jetbrains.kotlin.android.configure
 
 import com.android.builder.model.AndroidProject
 import com.android.builder.model.NativeAndroidProject
-import com.android.tools.idea.IdeInfo
+
 import com.intellij.openapi.externalSystem.model.DataNode
 import com.intellij.openapi.externalSystem.model.project.ModuleData
 import com.intellij.openapi.externalSystem.model.project.ProjectData
@@ -39,32 +39,15 @@ class KotlinAndroidMPPGradleProjectResolver : AbstractProjectResolverExtension()
 
     override fun createModule(gradleModule: IdeaModule, projectDataNode: DataNode<ProjectData>): DataNode<ModuleData> {
         return super.createModule(gradleModule, projectDataNode)!!.also {
-            initializeModuleData(gradleModule, it, projectDataNode)
-        }
-    }
-
-    override fun populateModuleContentRoots(gradleModule: IdeaModule, ideModule: DataNode<ModuleData>) {
-        super.populateModuleContentRoots(gradleModule, ideModule)
-        if (IdeInfo.getInstance().isAndroidStudio || isAndroidProject) {
-            KotlinMPPGradleProjectResolver.populateContentRoots(gradleModule, ideModule, resolverCtx)
-        }
-    }
-
-    override fun populateModuleDependencies(gradleModule: IdeaModule, ideModule: DataNode<ModuleData>, ideProject: DataNode<ProjectData>) {
-        super.populateModuleDependencies(gradleModule, ideModule, ideProject)
-        if (isAndroidProject) {
-            KotlinMPPGradleProjectResolver.populateModuleDependencies(gradleModule, ideProject, ideModule, resolverCtx)
+            initializeModuleData(gradleModule, it)
         }
     }
 
     private fun initializeModuleData(
         gradleModule: IdeaModule,
-        mainModuleData: DataNode<ModuleData>,
-        projectDataNode: DataNode<ProjectData>
+        mainModuleData: DataNode<ModuleData>
     ) {
         if (!isAndroidProject) return
-
-        KotlinMPPGradleProjectResolver.initializeModuleData(gradleModule, mainModuleData, projectDataNode, resolverCtx)
 
         val mppModel = resolverCtx.getMppModel(gradleModule) ?: return
 
