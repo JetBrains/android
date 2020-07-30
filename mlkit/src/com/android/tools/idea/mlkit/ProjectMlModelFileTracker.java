bbd4d60a697694df045ab81fa1b97402b34b8631
Lei Yu <jackqdyulei@google.com>
2020-07-29 20:12:16 -0700

    Add project sync listener in MlModelFileTracker
    Bug: 161140683
    Test: ProjectMlModelFileTrackerTest
    Change-Id: I2cfa64f41eb794555962ee0fdb7e78c9456fef7e
    
    GitOrigin-RevId: 37538de0d72a4d79f684a3278380bee71209d8e3

--- mlkit/src/com/android/tools/idea/mlkit/ProjectMlModelFileTracker.java
+++ mlkit/src/com/android/tools/idea/mlkit/ProjectMlModelFileTracker.java
@@ -15,9 +15,16 @@
  */
 package com.android.tools.idea.mlkit;
 
+import static com.android.tools.idea.projectsystem.ProjectSystemSyncUtil.PROJECT_SYSTEM_SYNC_TOPIC;
+
+import com.android.ide.common.repository.GradleVersion;
+import com.android.tools.idea.gradle.plugin.AndroidPluginInfo;
 import com.android.tools.idea.mlkit.viewer.TfliteModelFileType;
+import com.android.tools.idea.projectsystem.ProjectSystemSyncManager;
+import com.google.common.annotations.VisibleForTesting;
 import com.intellij.openapi.components.Service;
 import com.intellij.openapi.components.ServiceManager;
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.SimpleModificationTracker;
 import com.intellij.openapi.vfs.VirtualFile;
@@ -27,15 +34,21 @@ import com.intellij.openapi.vfs.newvfs.events.VFileContentChangeEvent;
 import com.intellij.openapi.vfs.newvfs.events.VFileEvent;
 import com.intellij.util.indexing.FileBasedIndex;
 import java.util.List;
+import java.util.Objects;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * Tracks the modification of model files in the whole project.
  */
 @Service
 public final class ProjectMlModelFileTracker extends SimpleModificationTracker {
+  @Nullable
+  private GradleVersion myGradleVersion;
 
   public ProjectMlModelFileTracker(@NotNull Project project) {
+    myGradleVersion = getGradleVersionFromProject(project);
+
     project.getMessageBus().connect(project).subscribe(VirtualFileManager.VFS_CHANGES, new BulkFileListener() {
       @Override
       public void after(@NotNull List<? extends VFileEvent> events) {
@@ -58,10 +71,38 @@ public final class ProjectMlModelFileTracker extends SimpleModificationTracker {
         }
       }
     });
+
+    project.getMessageBus().connect(project).subscribe(PROJECT_SYSTEM_SYNC_TOPIC, new ProjectSystemSyncManager.SyncResultListener() {
+      @Override
+      public void syncEnded(@NotNull ProjectSystemSyncManager.SyncResult result) {
+        if (result.isSuccessful()) {
+          GradleVersion gradleVersion = getGradleVersionFromProject(project);
+          if (!Objects.equals(myGradleVersion, gradleVersion)) {
+            Logger.getInstance(ProjectMlModelFileTracker.class).info("AGP version changed, refresh all light classes cache.");
+            myGradleVersion = gradleVersion;
+            incModificationCount();
+          }
+        }
+      }
+    });
+  }
+
+  @Nullable
+  private static GradleVersion getGradleVersionFromProject(@NotNull Project project) {
+    AndroidPluginInfo androidPluginInfo = AndroidPluginInfo.findFromModel(project);
+    if (androidPluginInfo != null) {
+      return androidPluginInfo.getPluginVersion();
+    }
+    return null;
   }
 
   @NotNull
   public static ProjectMlModelFileTracker getInstance(@NotNull Project project) {
     return ServiceManager.getService(project, ProjectMlModelFileTracker.class);
+  }
+
+  @VisibleForTesting
+  void setGradleVersion(@Nullable GradleVersion gradleVersion) {
+    myGradleVersion = gradleVersion;
   }
 }
