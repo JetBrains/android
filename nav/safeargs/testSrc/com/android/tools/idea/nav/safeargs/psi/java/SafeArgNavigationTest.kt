59140a7e41dda537f0be7517a36e72fe1fcf2edd
Kun Shen <kunshen@google.com>
2020-07-30 16:16:42 -0700

    [safeArg] Fix navigation for getters and setters
    ...of inner action builder classes. We first check
    if the corresponding argument tag is located inside the
    current action tag or not. If not we try to locate this
    tag in the target destination tag.
    
    Bug: 161369564
    Test: Updated existing tests
    Change-Id: I64a439f4f346a44dc797c54ab15bc6fb0fcd1a98

--- nav/safeargs/testSrc/com/android/tools/idea/nav/safeargs/psi/java/SafeArgNavigationTest.kt
+++ nav/safeargs/testSrc/com/android/tools/idea/nav/safeargs/psi/java/SafeArgNavigationTest.kt
@@ -128,7 +128,7 @@ class SafeArgNavigationTest {
   }
 
   @Test
-  fun canNavigateToXmlTagFromInnerBuilderClass() {
+  fun canNavigateToXmlTagFromInnerArgsBuilderClass() {
     safeArgsRule.fixture.addFileToProject(
       "res/navigation/main.xml",
       //language=XML
@@ -305,6 +305,117 @@ class SafeArgNavigationTest {
                   app:destination="@id/fragment3" />
             """.trimIndent())
         }
+      }
+    }
+  }
+
+  @Test
+  fun canNavigateToXmlTagFromInnerActionsBuilderClass() {
+    safeArgsRule.fixture.addFileToProject(
+      "res/navigation/main.xml",
+      //language=XML
+      """
+        <?xml version="1.0" encoding="utf-8"?>
+        <navigation xmlns:android="http://schemas.android.com/apk/res/android"
+            xmlns:app="http://schemas.android.com/apk/res-auto" android:id="@+id/main"
+            app:startDestination="@id/fragment1">
+
+          <fragment
+              android:id="@+id/fragment1"
+              android:name="test.safeargs.Fragment1"
+              android:label="Fragment1" >
+            <action
+                android:id="@+id/action_fragment1_to_fragment2"
+                app:destination="@id/fragment2" >
+                <argument
+                  android:name="arg"
+                  app:argType="string"
+                  android:defaultValue="defaultString" />
+                <argument
+                  android:name="argInAction"
+                  app:argType="string" />
+            </action>
+          </fragment>
+
+          <fragment
+              android:id="@+id/fragment2"
+              android:name="test.safeargs.Fragment2"
+              android:label="Fragment2" >
+            <argument
+              android:name="arg"
+              app:argType="string" />
+            <argument
+                android:name="argInDestination"
+                app:argType="string" />
+          </fragment>
+        </navigation>
+      """.trimIndent())
+
+    // Initialize repository after creating resources, needed for codegen to work
+    ResourceRepositoryManager.getInstance(safeArgsRule.androidFacet).moduleResources
+
+    val context = safeArgsRule.fixture.addClass("package test.safeargs; public class Fragment1 {}")
+
+    val editors = FileEditorManager.getInstance(safeArgsRule.fixture.project)
+    assertThat(editors.selectedFiles).isEmpty()
+
+    // check actionBuilderClass navigation
+    val actionBuilderClass = safeArgsRule.fixture
+      .findClass("test.safeargs.Fragment1Directions.ActionFragment1ToFragment2", context) as LightActionBuilderClass
+    actionBuilderClass.let {
+      it.navigate(true)
+      assertThat(editors.selectedFiles[0].name).isEqualTo("main.xml")
+      assertThat(it.navigationElement).isInstanceOf(XmlTag::class.java)
+      assertThat(it.navigationElement.text).isEqualTo(
+        """
+          <action
+                  android:id="@+id/action_fragment1_to_fragment2"
+                  app:destination="@id/fragment2" >
+                  <argument
+                    android:name="arg"
+                    app:argType="string"
+                    android:defaultValue="defaultString" />
+                  <argument
+                    android:name="argInAction"
+                    app:argType="string" />
+              </action>
+        """.trimIndent())
+    }
+
+    // check methods navigation of actionBuilderClass
+    actionBuilderClass.methods.forEach {
+      it.navigate(true)
+      assertThat(editors.selectedFiles[0].name).isEqualTo("main.xml")
+      assertThat(it.navigationElement).isInstanceOf(XmlTag::class.java)
+
+      // check getters and setters
+      when (it.name) {
+        "getArg", "setArg" -> {
+          assertThat(it.navigationElement.text).isEqualTo(
+            """
+              <argument
+                        android:name="arg"
+                        app:argType="string"
+                        android:defaultValue="defaultString" />
+            """.trimIndent())
+        }
+        "getArgInAction", "setArgInAction" -> {
+          assertThat(it.navigationElement.text).isEqualTo(
+            """
+              <argument
+                        android:name="argInAction"
+                        app:argType="string" />
+            """.trimIndent())
+        }
+        "getArgInDestination", "setArgInDestination" -> {
+          assertThat(it.navigationElement.text).isEqualTo(
+            """
+              <argument
+                      android:name="argInDestination"
+                      app:argType="string" />
+            """.trimIndent())
+        }
+        else -> throw IllegalAccessError("This should never happen!")
       }
     }
   }
