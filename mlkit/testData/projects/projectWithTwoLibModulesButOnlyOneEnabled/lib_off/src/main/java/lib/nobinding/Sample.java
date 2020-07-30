a107f960b6ee8986d76187ffca2146c3819b9e38
Tor Norbye <tnorbye@google.com>
2020-07-29 17:13:47 -0700

    Update language to comply with Android's inclusive language guidance
    See https://source.android.com/setup/contribute/respectful-code for reference
    
    BUG=161896447
    Test: Existing
    
    Change-Id: Ic8af5bbb70145be955a3dee06379728db4e0f565
    
    GitOrigin-RevId: 1f3a6f659219f01c837fa55a0871cb96348aa17d

--- mlkit/testData/projects/projectWithTwoLibModulesButOnlyOneEnabled/lib_off/src/main/java/lib/nobinding/Sample.java
+++ mlkit/testData/projects/projectWithTwoLibModulesButOnlyOneEnabled/lib_off/src/main/java/lib/nobinding/Sample.java
@@ -2,5 +2,5 @@ package lib.nobinding;
 
 // A concrete class is currently necessary to get an enlarged search scope that includes light
 // model classes. This class isn't used outside of that purpose.
-public class Dummy {
+public class Sample {
 }
