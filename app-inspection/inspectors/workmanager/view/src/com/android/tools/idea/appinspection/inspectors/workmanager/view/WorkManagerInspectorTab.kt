41dcf493b4d5d0f698d4dd3826d953a916d9604a
Kainan Wang <knwang@google.com>
2020-08-10 08:05:56 -0700

    Match table formats with Specs for WorkManager Inspector
    This commit includes a few minor changes to the table UI:
    Adding a column for orders sorting by time added.
    Renaming column "Retry Count" to "Retries"
    Formatting column "Time Started" with "hh:mm:ss"
    
    Bug: 140135398
    Test: Manual Test
    
    Change-Id: I7b83c8b842a91c1db77784de31f98dcb1a265637

--- app-inspection/inspectors/workmanager/view/src/com/android/tools/idea/appinspection/inspectors/workmanager/view/WorkManagerInspectorTab.kt
+++ app-inspection/inspectors/workmanager/view/src/com/android/tools/idea/appinspection/inspectors/workmanager/view/WorkManagerInspectorTab.kt
@@ -15,23 +15,53 @@
  */
 package com.android.tools.idea.appinspection.inspectors.workmanager.view
 
+import androidx.work.inspection.WorkManagerInspectorProtocol
 import com.android.tools.adtui.HoverRowTable
 import com.android.tools.idea.appinspection.inspectors.workmanager.model.WorkManagerInspectorClient
 import com.android.tools.idea.appinspection.inspectors.workmanager.model.WorksTableModel
 import com.intellij.ui.components.panels.VerticalLayout
 import com.intellij.ui.table.JBTable
+import java.awt.Component
 import java.awt.event.ComponentAdapter
 import java.awt.event.ComponentEvent
-import java.awt.event.MouseAdapter
-import java.awt.event.MouseEvent
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 import javax.swing.JComponent
 import javax.swing.JPanel
 import javax.swing.JScrollPane
+import javax.swing.JTable
+import javax.swing.table.DefaultTableCellRenderer
 
 /**
  * View class for the WorkManger Inspector Tab with a table of all active works.
  */
 class WorkManagerInspectorTab(private val client: WorkManagerInspectorClient) {
+
+  private class WorksTableCellRenderer : DefaultTableCellRenderer() {
+    override fun getTableCellRendererComponent(table: JTable?,
+                                               value: Any?,
+                                               isSelected: Boolean,
+                                               hasFocus: Boolean,
+                                               row: Int,
+                                               column: Int): Component =
+      when (table?.convertColumnIndexToModel(column)) {
+        // TODO(163343710): Add icons on the left of state text
+        WorksTableModel.Column.STATE.ordinal -> {
+          val text = WorkManagerInspectorProtocol.WorkInfo.State.forNumber(value as Int).name
+          val capitalizedText = text[0] + text.substring(1).toLowerCase(Locale.getDefault())
+          super.getTableCellRendererComponent(table, capitalizedText, isSelected, hasFocus, row, column)
+        }
+        WorksTableModel.Column.TIME_STARTED.ordinal -> {
+          val formatter = SimpleDateFormat("h:mm:ss a", Locale.getDefault())
+          val time = value as Long
+          val timeText = if (time == -1L) "-" else formatter.format(Date(value))
+          super.getTableCellRendererComponent(table, timeText, isSelected, hasFocus, row, column)
+        }
+        else -> super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column)
+      }
+  }
+
   private val root = JPanel(VerticalLayout(0))
 
   val component: JComponent = root
@@ -45,6 +75,7 @@ class WorkManagerInspectorTab(private val client: WorkManagerInspectorClient) {
     val table: JBTable = HoverRowTable(model)
 
     table.autoCreateRowSorter = true
+    table.setDefaultRenderer(Object::class.java, WorksTableCellRenderer())
 
     // Adjusts width for each column.
     table.addComponentListener(object : ComponentAdapter() {
