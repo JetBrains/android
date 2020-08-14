a890178473692bcd891198b59dca8663d05f522f
Esteban de la Canal <estebandlc@google.com>
2020-08-14 09:23:42 -0700

    Fix expected files per distribution
    Test: this is a test
    Bug: N/A
    Change-Id: Ia2a79965cfb0c805ba4cd5cb31767db73edff7d1
    
    GitOrigin-RevId: c19cae93889c334ab6e702a127da029569c25826

--- studio/tests/test_studio.py
+++ studio/tests/test_studio.py
@@ -17,28 +17,32 @@ class StudioTests(unittest.TestCase):
 
   def test_linux_files(self):
 
-    for platform in ["linux", "win", "mac"]:
+    platforms = ["linux", "win", "mac"]
+    actual = {}
+    for platform in platforms:
       name = "tools/adt/idea/studio/android-studio.%s.zip" % platform
-      actual = []
       with zipfile.ZipFile(name) as file:
-        actual = sorted(file.namelist())
+        actual[platform] = sorted(file.namelist())
 
-      expected = []
+    expected = {}
+    for platform in platforms:
       with open("tools/adt/idea/studio/tests/expected_%s.txt" % platform, "r") as txt:
-        expected = [line.strip() for line in sorted(txt.readlines())]
+        expected[platform] = [line.strip() for line in sorted(txt.readlines())]
 
+    for platform in platforms:
       if expected != actual:
         undeclared_dir = os.getenv("TEST_UNDECLARED_OUTPUTS_DIR")
         with open("%s/expected_%s.txt" % (undeclared_dir, platform), "w") as new_ex:
-          new_ex.writelines([line + "\n" for line in actual])
+          new_ex.writelines([line + "\n" for line in actual[platform]])
         print("You can find the newly expected file in the undeclared output directory.")
 
+    for platform in platforms:
       i = 0
-      while i < len(actual) and i < len(expected):
-        self.assertEqual(actual[i], expected[i], "#%d - Expected \"%s\", got \"%s\"" % (i, expected[i], actual[i]))
+      while i < len(actual[platform]) and i < len(expected[platform]):
+        self.assertEqual(actual[platform][i], expected[platform][i], "#%d - Expected \"%s\", got \"%s\"" % (i, expected[platform][i], actual[platform][i]))
         i += 1
-      self.assertEqual(i, len(expected), "Expected item did not appear")
-      self.assertEqual(i, len(actual), "Unexpected item")
+      self.assertEqual(i, len(expected[platform]), "Expected item did not appear")
+      self.assertEqual(i, len(actual[platform]), "Unexpected item")
 
 if __name__ == "__main__":
   unittest.main()
