f45afe3110af5e2663ee2c1e84c17213616426e2
Yuriy Solodkyy <solodkyy@google.com>
2020-08-26 16:47:35 +0100

    Encapsulate extraction of the dependency list in AndroidModule
    This is to make it possible to transition to v2 and Ide models.
    
    Since this was the last usage of `androidProject` property make it
    private.
    
    Bug: n/a
    Test: n/a
    Change-Id: I655d8d04451c1e788d32cf9dddbf8cb1fd893bf5

--- android/src/com/android/tools/idea/gradle/project/sync/idea/AdditionalClassifierArtifactsModelCollector.kt
+++ android/src/com/android/tools/idea/gradle/project/sync/idea/AdditionalClassifierArtifactsModelCollector.kt
@@ -15,12 +15,8 @@
  */
 package com.android.tools.idea.gradle.project.sync.idea
 
-import com.android.builder.model.BaseArtifact
-import com.android.builder.model.Library
-import com.android.builder.model.Variant
 import com.android.ide.gradle.model.AdditionalClassifierArtifactsModelParameter
 import com.android.ide.gradle.model.ArtifactIdentifier
-import com.android.ide.gradle.model.ArtifactIdentifierImpl
 import com.android.ide.gradle.model.artifacts.AdditionalClassifierArtifactsModel
 import com.android.tools.idea.gradle.project.sync.idea.svs.AndroidModule
 import org.gradle.tooling.BuildController
@@ -35,43 +31,22 @@ fun getAdditionalClassifierArtifactsModel(
   inputModules.forEach { module ->
     if (module.modelVersion?.isAtLeast(3, 5, 0) != true) return@forEach
 
-    // Get variants from AndroidProject if it's not empty, otherwise get from VariantGroup.
-    // The first case indicates full-variants sync and the later single-variant sync.
-    val variants = if (module.androidProject.variants.isNotEmpty()) module.androidProject.variants else module.variantGroup.variants
     // Collect the library identifiers to download sources and javadoc for, and filter out the cached ones and local jar/aars.
-    val identifiers = collectIdentifiers(variants).filter { !cachedLibraries.contains(idToString(it)) && it.version != "unspecified" }
+    val identifiers = module.getLibraryDependencies().filter { !cachedLibraries.contains(idToString(it)) && it.version != "unspecified" }
 
     // Query for AdditionalClassifierArtifactsModel model.
     if (identifiers.isNotEmpty()) {
-      module.additionalClassifierArtifacts = controller.findModel(module.findModelRoot, AdditionalClassifierArtifactsModel::class.java,
-                           AdditionalClassifierArtifactsModelParameter::class.java) { parameter ->
-        parameter.artifactIdentifiers = identifiers
-        parameter.downloadAndroidxUISamplesSources = downloadAndroidxUISamplesSources
+      module.additionalClassifierArtifacts =
+        controller.findModel(
+          module.findModelRoot,
+          AdditionalClassifierArtifactsModel::class.java,
+          AdditionalClassifierArtifactsModelParameter::class.java
+        ) { parameter ->
+          parameter.artifactIdentifiers = identifiers
+          parameter.downloadAndroidxUISamplesSources = downloadAndroidxUISamplesSources
       }
     }
   }
-}
-
-@UsedInBuildAction
-private fun collectIdentifiers(
-  variants: Collection<Variant>
-): List<ArtifactIdentifier> {
-  val libraries = mutableListOf<Library>()
-  // Collect libraries from all artifacts of all variants.
-  @Suppress("DEPRECATION")
-  variants.forEach { variant ->
-    val artifacts = mutableListOf<BaseArtifact>(variant.mainArtifact)
-    artifacts.addAll(variant.extraAndroidArtifacts)
-    artifacts.addAll(variant.extraJavaArtifacts)
-    artifacts.forEach {
-      libraries.addAll(it.compileDependencies.javaLibraries)
-      libraries.addAll(it.compileDependencies.libraries)
-    }
-  }
-
-  return libraries.filter { it.project == null }.map { it.resolvedCoordinates }.map {
-    ArtifactIdentifierImpl(it.groupId, it.artifactId, it.version)
-  }.distinct()
 }
 
 @UsedInBuildAction
