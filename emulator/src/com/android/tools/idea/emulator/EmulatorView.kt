43a00d7786a99f616359354097fd9efb1b18198d
Sergey Prigogin <sprigogin@google.com>
2020-08-31 12:38:57 -0700

    Minor performance improvement
    Test: existing
    Bug: N/A
    Change-Id: I3c2ff0fa68cff86d200c9c6b469ef5bfdf2c89ba
    
    GitOrigin-RevId: f1632c8af796bc1fd995cce154e58f5ed939238d

--- emulator/src/com/android/tools/idea/emulator/EmulatorView.kt
+++ emulator/src/com/android/tools/idea/emulator/EmulatorView.kt
@@ -792,12 +792,13 @@ class EmulatorView(
     private fun getPixels(imageBytes: ByteString, width: Int, height: Int): IntArray {
       val pixels = IntArray(width * height)
       val byteIterator = imageBytes.iterator()
+      val alpha = 0xFF shl 24
       for (i in pixels.indices) {
         val red = byteIterator.nextByte().toInt() and 0xFF
         val green = byteIterator.nextByte().toInt() and 0xFF
         val blue = byteIterator.nextByte().toInt() and 0xFF
         byteIterator.nextByte() // Alpha is ignored since the screenshots are always opaque.
-        pixels[i] = 0xFF000000.toInt() or (red shl 16) or (green shl 8) or blue
+        pixels[i] = alpha or (red shl 16) or (green shl 8) or blue
       }
       return pixels
     }
