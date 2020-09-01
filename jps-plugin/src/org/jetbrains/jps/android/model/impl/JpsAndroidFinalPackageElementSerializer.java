5b83f4f30a1ee4f113add33cedd596af6998e00b
Nikolay Chashnikov <Nikolay.Chashnikov@jetbrains.com>
2020-09-01 20:42:21 +0300

    [jps model] cleanup: remove code which implements save for JPS model
    JpsModel is used inside the build process (except small part related to custom source root types which is also used inside IDE) and the build process doesn't save the project configuration. And we don't have plans to use JpsModel inside the IDE process anymore (the new workspace model is used instead), so it makes no sense to maintain that code.
    
    (cherry picked from commit 060008f99ec41acaaf7cb406a23abaa6f4ef337c)
    
    Change-Id: I060008f99ec41acaaf7cb406a23abaa6f4ef337c
    
    GitOrigin-RevId: 73792688603480fb8294620973762f09eb5d1e80

--- jps-plugin/src/org/jetbrains/jps/android/model/impl/JpsAndroidFinalPackageElementSerializer.java
+++ jps-plugin/src/org/jetbrains/jps/android/model/impl/JpsAndroidFinalPackageElementSerializer.java
@@ -2,7 +2,6 @@ package org.jetbrains.jps.android.model.impl;
 
 import org.jdom.Element;
 import org.jetbrains.annotations.NonNls;
-import org.jetbrains.jps.android.AndroidJpsUtil;
 import org.jetbrains.jps.model.module.JpsModuleReference;
 import org.jetbrains.jps.model.serialization.artifact.JpsPackagingElementSerializer;
 import org.jetbrains.jps.model.serialization.facet.JpsFacetSerializer;
@@ -20,13 +19,5 @@ public class JpsAndroidFinalPackageElementSerializer extends JpsPackagingElement
     final JpsModuleReference moduleReference = JpsFacetSerializer.createModuleReference(
       element.getAttributeValue(PACKAGING_FACET_ATTRIBUTE));
     return new JpsAndroidFinalPackageElement(moduleReference);
-  }
-
-  @Override
-  public void save(JpsAndroidFinalPackageElement element, Element tag) {
-    final String id = JpsFacetSerializer.getFacetId(element.getModuleReference(),
-                                                    AndroidJpsUtil.ANDROID_FACET_TYPE_ID,
-                                                    AndroidJpsUtil.ANDROID_FACET_NAME);
-    tag.setAttribute(PACKAGING_FACET_ATTRIBUTE, id);
   }
 }
