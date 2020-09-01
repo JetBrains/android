8aef4ad2703c30f72e0416dfd685dbea164f0f64
Charlie Tsai <chartsai@google.com>
2020-09-01 14:47:49 +0100

    Keep same zoom level when re-open resource files
    Use the last zoom level when opening the same resouorce files in
    DesignSurface, which includes layout and drawable files.
    This works even when project is re-open.
    
    Bug: b/158344314 , b/155215268
    Test: Add Unit test and manually tested
    Change-Id: Ic53f3191c1bbc299edfa111ac715994cae2d900d
    
    GitOrigin-RevId: 0342c2e44a31b63efe4e7ca0a92bdd06a0761a97

--- designer/src/com/android/tools/idea/common/surface/DesignSurface.java
+++ designer/src/com/android/tools/idea/common/surface/DesignSurface.java
@@ -351,9 +351,14 @@ public abstract class DesignSurface extends EditorDesignSurface implements Dispo
       public void componentResized(ComponentEvent componentEvent) {
         if (componentEvent.getID() == ComponentEvent.COMPONENT_RESIZED) {
           if (!myIsInitialZoomLevelDetermined && isShowing() && getWidth() > 0 && getHeight() > 0) {
-            // Zoom-to-fit as default size of content when DesignSurface becomes visible at first time.
-            zoomToFit();
+            // Set previous scale when DesignSurface becomes visible at first time.
+            NlModel model = Iterables.getFirst(getModels(), null);
+            if (model == null) {
+              // No model is attached, ignore the setup of initial zoom level.
+              return;
+            }
 
+            restorePreviousZoomLevel(model);
             // The default size is defined, enable the flag.
             myIsInitialZoomLevelDetermined = true;
           }
@@ -657,12 +662,11 @@ public abstract class DesignSurface extends EditorDesignSurface implements Dispo
     }
 
     addModel(model);
-    zoomToFit();
 
     return requestRender()
       .whenCompleteAsync((result, ex) -> {
         reactivateInteractionManager();
-        zoomToFit();
+        restorePreviousZoomLevel(model);
         revalidateScrollArea();
 
         // TODO: The listeners have the expectation of the call happening in the EDT. We need
@@ -1106,7 +1110,7 @@ public abstract class DesignSurface extends EditorDesignSurface implements Dispo
    */
   @VisibleForTesting(visibility = VisibleForTesting.Visibility.PROTECTED)
   public boolean setScale(@SurfaceScale double scale, @SwingCoordinate int x, @SwingCoordinate int y) {
-    @SurfaceScale double newScale = Math.min(Math.max(scale, getMinScale()), getMaxScale());
+    @SurfaceScale final double newScale = Math.min(Math.max(scale, getMinScale()), getMaxScale());
     if (Math.abs(newScale - myScale) < SCALING_THRESHOLD / getScreenScalingFactor()) {
       return false;
     }
@@ -1128,6 +1132,10 @@ public abstract class DesignSurface extends EditorDesignSurface implements Dispo
     }
 
     myScale = newScale;
+    NlModel model = Iterables.getFirst(getModels(), null);
+    if (model != null) {
+      storeCurrentZoomLevel(model);
+    }
 
     if (view != null) {
       @SwingCoordinate int shiftedX = Coordinates.getSwingXDip(view, androidX);
@@ -1138,6 +1146,30 @@ public abstract class DesignSurface extends EditorDesignSurface implements Dispo
     revalidateScrollArea();
     notifyScaleChanged();
     return true;
+  }
+
+  /**
+   * Save the current zoom level from the file of the given {@link NlModel}.
+   */
+  private void storeCurrentZoomLevel(@NotNull NlModel model) {
+    SurfaceState state = DesignSurfaceSettings.getInstance().getSurfaceState();
+    state.saveFileScale(model.getFile(), myScale);
+  }
+
+  /**
+   * Load the saved zoom level from the file of the given {@link NlModel}.
+   * If there is no saved zoom level, {@link #zoomToFit()} is invoked.
+   */
+  private void restorePreviousZoomLevel(@NotNull NlModel model) {
+    SurfaceState state = DesignSurfaceSettings.getInstance().getSurfaceState();
+    Double previousScale = state.loadFileScale(model.getFile());
+    if (previousScale != null) {
+      setScale(previousScale);
+    }
+    else {
+      // No previous scale, use zoom-to-fit as default.
+      zoomToFit();
+    }
   }
 
   /**
