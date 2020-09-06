768a614c8306374245612fd9482e3cc680a60c72
Pierfrancesco Soffritti <psoffritti@google.com>
2020-09-06 14:57:16 +0100

    Address security concerns with offline mode
    SQLite can be exploited to run malicious code,
    to avoid this from happening on the host machine
    this change disables offline mode if:
    
    1. the process being inspected does not correspond
       to the project open in studio.
    2. The project has been generated from a prebuilt apk.
    
    [Second attempt]
    
    Test: OfflineDatabaseManagerTest.kt
    Bug: 167814794
    Change-Id: I86d0307617d63d821e2b7a8a490921164301095e

--- android-debuggers/src/com/android/tools/idea/sqlite/OfflineDatabaseManager.kt
+++ android-debuggers/src/com/android/tools/idea/sqlite/OfflineDatabaseManager.kt
@@ -15,14 +15,17 @@
  */
 package com.android.tools.idea.sqlite
 
+import com.android.tools.idea.ApkFacetChecker
 import com.android.tools.idea.appinspection.inspector.api.process.ProcessDescriptor
 import com.android.tools.idea.device.fs.DeviceFileDownloaderService
 import com.android.tools.idea.device.fs.DownloadProgress
+import com.android.tools.idea.projectsystem.ProjectSystemService
 import com.android.tools.idea.sqlite.model.DatabaseFileData
 import com.android.tools.idea.sqlite.model.SqliteDatabaseId
 import com.intellij.openapi.Disposable
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.Disposer
+import com.intellij.psi.search.GlobalSearchScope
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.guava.await
 import java.io.FileNotFoundException
@@ -55,6 +58,14 @@ class OfflineDatabaseManagerImpl(
     processDescriptor: ProcessDescriptor,
     databaseToDownload: SqliteDatabaseId.LiveSqliteDatabaseId
   ): DatabaseFileData {
+    if (!isFileDownloadAllowed(processDescriptor)) {
+      throw OfflineDatabaseException(
+        """For security reasons offline mode is disabled when 
+        the process being inspected does not correspond to the project open in studio 
+        or when the project has been generated from a prebuilt apk."""
+      )
+    }
+
     val path = databaseToDownload.path
     // Room uses write-ahead-log, so we need to download these additional files to open the db
     val pathsToDownload = listOf(path, "$path-shm", "$path-wal")
@@ -65,10 +76,10 @@ class OfflineDatabaseManagerImpl(
     val files = try {
       deviceFileDownloaderService.downloadFiles(processDescriptor.serial, pathsToDownload, disposableDownloadProgress).await()
     } catch (e: IllegalArgumentException) {
-      throw IOException("Device '${processDescriptor.model} ${processDescriptor.serial}' not found.", e)
+      throw OfflineDatabaseException("Device '${processDescriptor.model} ${processDescriptor.serial}' not found.", e)
     }
 
-    val mainFile = files[path] ?: throw FileNotFoundException("Can't download database '${databaseToDownload.path}'")
+    val mainFile = files[path] ?: throw OfflineDatabaseException("Can't download database '${databaseToDownload.path}'")
     val shmFile = files["$path-shm"]
     val walFile = files["$path-wal"]
 
@@ -83,6 +94,23 @@ class OfflineDatabaseManagerImpl(
       .await()
   }
 
+  /**
+   * File download is not allowed if:
+   * 1. the file belongs to an app different from the one open in the studio project
+   * 2. the project comes from a prebuilt apk
+   */
+  private fun isFileDownloadAllowed(processDescriptor: ProcessDescriptor): Boolean {
+    val androidFacetsForInspectedProcess = ProjectSystemService.getInstance(project).projectSystem.getAndroidFacetsWithPackageName(
+      project,
+      processDescriptor.processName,
+      GlobalSearchScope.projectScope(project)
+    )
+
+    val hasApkFacet = androidFacetsForInspectedProcess.any { ApkFacetChecker.hasApkFacet(it.module) }
+
+    return androidFacetsForInspectedProcess.isNotEmpty() && !hasApkFacet
+  }
+
   private class DisposableDownloadProgress(private val coroutineJob: Job) : DownloadProgress, Disposable {
     private var isDisposed = false
 
@@ -95,3 +123,5 @@ class OfflineDatabaseManagerImpl(
     }
   }
 }
+
+class OfflineDatabaseException(override val message: String?, override val cause: Throwable? = null) : RuntimeException()
