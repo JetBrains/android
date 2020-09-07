aec97ff45374837f579fc20f2dceb41afa1b49c2
Esteban de la Canal <estebandlc@google.com>
2020-09-07 14:44:14 -0700

    Change dagger tests to use custom mock JDK
    This allows the tests to work against the prebuilts
    
    Test: this is for tests
    Bug: N/A
    Change-Id: I3e97b12a79acc067ccbeda44a568f5f915a06a9c

--- dagger/testSrc/com/android/tools/idea/dagger/DaggerTestCase.kt
+++ dagger/testSrc/com/android/tools/idea/dagger/DaggerTestCase.kt
@@ -16,9 +16,9 @@
 package com.android.tools.idea.dagger
 
 import com.android.tools.idea.flags.StudioFlags
-import com.intellij.testFramework.fixtures.JavaCodeInsightFixtureTestCase
+import org.jetbrains.android.JavaCodeInsightFixtureAdtTestCase
 
-abstract class DaggerTestCase : JavaCodeInsightFixtureTestCase() {
+abstract class DaggerTestCase : JavaCodeInsightFixtureAdtTestCase() {
   override fun setUp() {
     StudioFlags.DAGGER_SUPPORT_ENABLED.override(true)
     super.setUp()
