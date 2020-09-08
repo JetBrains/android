deb344dd884f08f45af7ebbd36c3e3dcb6472124
Dmitry Batkovich <dmitry.batkovich@jetbrains.com>
2020-09-08 10:57:00 +0300

    optimize imports
    GitOrigin-RevId: 2cb4f65b83b4fd77162821599bc3fb3e9e102cb3

--- apkanalyzer/src/com/android/tools/idea/apk/viewer/ApkFileSystem.java
+++ apkanalyzer/src/com/android/tools/idea/apk/viewer/ApkFileSystem.java
@@ -24,7 +24,6 @@ import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.util.Key;
 import com.intellij.openapi.util.io.FileUtil;
 import com.intellij.openapi.util.text.StringUtil;
-import com.intellij.openapi.vfs.DiskQueryRelay;
 import com.intellij.openapi.vfs.VfsUtil;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.openapi.vfs.VirtualFileManager;
