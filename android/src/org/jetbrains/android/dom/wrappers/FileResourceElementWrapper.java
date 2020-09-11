40f8927e94b0652b018c02480cc0c6bb00e69c84
Alexey Kudravtsev <cdr@intellij.com>
2020-09-11 16:39:04 +0200

    cleanup: notnull and fix Processor type parameter types
    GitOrigin-RevId: 694f1cf4f8b2602e5a0e199c92490e68ec303c1b

--- android/src/org/jetbrains/android/dom/wrappers/FileResourceElementWrapper.java
+++ android/src/org/jetbrains/android/dom/wrappers/FileResourceElementWrapper.java
@@ -411,7 +411,7 @@ public class FileResourceElementWrapper implements PsiFile, ResourceElementWrapp
   }
 
   @Override
-  public boolean processChildren(@NotNull PsiElementProcessor<PsiFileSystemItem> processor) {
+  public boolean processChildren(@NotNull PsiElementProcessor<? super PsiFileSystemItem> processor) {
     return myWrappedElement.processChildren(processor);
   }
 
