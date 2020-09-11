40918d54e6b56656b765aafb8dcd5b00bbfe6383
Eugene Zhuravlev <jeka@intellij.com>
2020-09-11 08:16:08 +0200

    support dynamic message bundles inside JPS build process (IDEA-240279)
    GitOrigin-RevId: 13b9d19489baf62c4e8f921d7c7077583a302db4

--- jps-plugin/src/org/jetbrains/jps/android/AndroidJpsBundle.java
+++ jps-plugin/src/org/jetbrains/jps/android/AndroidJpsBundle.java
@@ -1,14 +1,15 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.jps.android;
 
-import com.intellij.AbstractBundle;
-import java.util.function.Supplier;
 import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.PropertyKey;
+import org.jetbrains.jps.api.JpsDynamicBundle;
 
-public final class AndroidJpsBundle extends AbstractBundle {
+import java.util.function.Supplier;
+
+public final class AndroidJpsBundle extends JpsDynamicBundle {
   @NonNls private static final String BUNDLE = "messages.AndroidJpsBundle";
   private static final AndroidJpsBundle INSTANCE = new AndroidJpsBundle();
 
