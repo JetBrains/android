9e4553e6337fd175909885d0f6a6ba3ca5cf467d
Renaud Paquay <rpaquay@google.com>
2020-09-22 14:59:16 -0700

    Wireless ADB: Update QR code success screen
    * Displays a scaled "V" SVG icon when pairing succeeds
    
    Bug: 150796676
    Test: Existing
    Change-Id: I07aff627e48c4dad80b6424eee1b6681b2c4de3b

--- adt-ui/src/main/java/com/android/tools/adtui/ui/ImagePanel.kt
+++ adt-ui/src/main/java/com/android/tools/adtui/ui/ImagePanel.kt
@@ -19,6 +19,7 @@ import com.android.tools.adtui.ImageUtils
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.scale.JBUIScale
 import com.intellij.util.ui.ImageUtil
+import com.intellij.util.ui.StartupUiUtil
 import java.awt.AlphaComposite
 import java.awt.Composite
 import java.awt.Graphics
@@ -46,51 +47,59 @@ class ImagePanel(val highFidelityScaling: Boolean = false) : JBPanel<ImagePanel>
 
   override fun paintComponent(g: Graphics) {
     super.paintComponent(g)
-    val insets = this.insets
-    val rect = Rectangle(insets.left, insets.top, width - insets.left - insets.right, height - insets.bottom - insets.top)
+    paintPanelImage(g, image, active, highFidelityScaling)
+  }
+}
 
-    // Paint background.
-    g.color = background
-    g.fillRect(rect.x, rect.y, rect.width, rect.height)
+/**
+ * Draws an image inside a [JBPanel], preserving aspect ratio.
+ */
+fun <T: JBPanel<T>> JBPanel<T>.paintPanelImage(g: Graphics, image: Image?, active: Boolean, highFidelityScaling: Boolean) {
+  val insets = this.insets
+  val rect = Rectangle(insets.left, insets.top, width - insets.left - insets.right, height - insets.bottom - insets.top)
 
-    // Set Alpha to "light" if component is not active.
-    var prevComposite: Composite? = null
-    if (!active) {
-      (g as Graphics2D?)?.apply {
-        prevComposite = composite
-        composite = AlphaComposite.SrcOver.derive(0.3f)
-      }
-    }
+  // Paint background.
+  g.color = background
+  g.fillRect(rect.x, rect.y, rect.width, rect.height)
 
-    // Draw image, centered, preserving the aspect ratio.
-    image?.let { img ->
-      val imageWidth = img.getWidth(this)
-      val imageHeight = img.getHeight(this)
-      if (imageWidth > 0 && imageHeight > 0) {
-        val scale = (rect.getWidth() / imageWidth).coerceAtMost(rect.getHeight() / imageHeight)
-        val w = (imageWidth * scale).roundToInt()
-        val h = (imageHeight * scale).roundToInt()
-        val xOffset = rect.x + (rect.width - w) / 2
-        val yOffset = rect.y + (rect.height - h) / 2
-        if (highFidelityScaling) {
-          val bufferedImage = ImageUtil.toBufferedImage(img)
-          val graphicsScale = JBUIScale.sysScale(g as Graphics2D).toDouble()
-          val scaleX = graphicsScale * w / imageWidth
-          val scaleY = graphicsScale * h / imageHeight
-          // ImageUtils.scale uses a two-step scaling algorithm that produces better quality results
-          // than the standard AWT scaling.
-          val scaledImage = ImageUtils.scale(bufferedImage, scaleX, scaleY)
-          g.drawImage(scaledImage, xOffset, yOffset, w, h, null)
-        }
-        else {
-          g.drawImage(img, xOffset, yOffset, w, h, null)
-        }
-      }
-    }
-
-    // Restore composite.
+  // Set Alpha to "light" if component is not active.
+  var prevComposite: Composite? = null
+  if (!active) {
     (g as Graphics2D?)?.apply {
-      prevComposite?.let { composite = it }
+      prevComposite = composite
+      composite = AlphaComposite.SrcOver.derive(0.3f)
     }
+  }
+
+  // Draw image, centered, preserving the aspect ratio.
+  image?.let { img ->
+    val imageWidth = img.getWidth(this)
+    val imageHeight = img.getHeight(this)
+    if (imageWidth > 0 && imageHeight > 0) {
+      val scale = (rect.getWidth() / imageWidth).coerceAtMost(rect.getHeight() / imageHeight)
+      val w = (imageWidth * scale).roundToInt()
+      val h = (imageHeight * scale).roundToInt()
+      val xOffset = rect.x + (rect.width - w) / 2
+      val yOffset = rect.y + (rect.height - h) / 2
+      if (highFidelityScaling) {
+        val bufferedImage = ImageUtil.toBufferedImage(img)
+        val graphicsScale = JBUIScale.sysScale(g as Graphics2D).toDouble()
+        val scaleX = graphicsScale * w / imageWidth
+        val scaleY = graphicsScale * h / imageHeight
+        // ImageUtils.scale uses a two-step scaling algorithm that produces better quality results
+        // than the standard AWT scaling.
+        val scaledImage = ImageUtils.scale(bufferedImage, scaleX, scaleY)
+        g.drawImage(scaledImage, xOffset, yOffset, w, h, null)
+      }
+      else {
+        // Draw the image using IJ wrapper so that rendering is optimized for JBHiDPIScaledImage
+        StartupUiUtil.drawImage(g, img, Rectangle(xOffset, yOffset, w, h), null, null)
+      }
+    }
+  }
+
+  // Restore composite.
+  (g as Graphics2D?)?.apply {
+    prevComposite?.let { composite = it }
   }
 }
