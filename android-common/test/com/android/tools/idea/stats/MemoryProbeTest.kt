6a34e3c7cc61563c48d13833b9c8d90a70e32705
Tianyu Geng <tgeng@google.com>
2020-09-22 09:49:13 -0700

    Move MemoryProbe to intellij.android.common
    Bug: 168810387
    Test: existing
    Change-Id: Ib9760e039e3aaff2ba52eb1e323ea7850eabd214
    
    GitOrigin-RevId: d4bb5481a36aa2d27f62355e5943629c9c56e94b

--- android-common/test/com/android/tools/idea/stats/MemoryProbeTest.kt
+++ android-common/test/com/android/tools/idea/stats/MemoryProbeTest.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.android.tools.idea.layoutinspector.memory
+package com.android.tools.idea.stats
 
 import com.google.common.collect.Range
 import com.google.common.truth.Truth.assertThat
