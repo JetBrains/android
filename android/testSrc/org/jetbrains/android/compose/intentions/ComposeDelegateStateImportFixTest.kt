eb1749926b039130d5b6758095762dc0a0d9544a
Polina Koval <kovalp@google.com>
2020-09-23 11:03:43 +0200

    [Compose] Move all intentions under org.jetbrains.android.compose.intentions
    Bug: n/a
    Test: n/a
    Change-Id: Ia2bc96577da87d7ec11bd26a19187fdc6fb7bc8d
    
    GitOrigin-RevId: d0064ece9d6cc7c40c3801fb91b4a6807b5cd903

--- android/testSrc/org/jetbrains/android/compose/intentions/ComposeDelegateStateImportFixTest.kt
+++ android/testSrc/org/jetbrains/android/compose/intentions/ComposeDelegateStateImportFixTest.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.jetbrains.android.compose
+package org.jetbrains.android.compose.intentions
 
 import com.google.common.truth.Truth.assertThat
 import com.intellij.testFramework.fixtures.JavaCodeInsightFixtureTestCase
