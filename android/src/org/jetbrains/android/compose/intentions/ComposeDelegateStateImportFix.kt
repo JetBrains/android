5eb04c49e1bcd69b424be759d29468351ac44827
Polina Koval <kovalp@google.com>
2020-09-23 11:03:43 +0200

    [Compose] Move all intentions under org.jetbrains.android.compose.intentions
    Bug: n/a
    Test: n/a
    Change-Id: Ia2bc96577da87d7ec11bd26a19187fdc6fb7bc8d

--- android/src/org/jetbrains/android/compose/intentions/ComposeDelegateStateImportFix.kt
+++ android/src/org/jetbrains/android/compose/intentions/ComposeDelegateStateImportFix.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.jetbrains.android.compose
+package org.jetbrains.android.compose.intentions
 
 import com.intellij.codeInsight.daemon.QuickFixBundle
 import com.intellij.codeInsight.intention.IntentionAction
