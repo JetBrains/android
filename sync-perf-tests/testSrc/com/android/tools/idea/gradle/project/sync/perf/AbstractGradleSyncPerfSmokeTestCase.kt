43396fcee3f01d3e5c1dc05091457d0c875e41f2
Sergio Murguia <srmurguia@google.com>
2020-09-29 10:58:18 -0700

    Check sync perf tests can be synced and built
    ... in pre submit.
    
    The tests runs sync and build, failing if either of them fail. The test
    projects used are the same as those used in perfgate for sync. Due to
    the time it takes to build only Average and SantaTracker are build
    and only Average is run in Windows for presubmit.
    
    This change also adds Santa Tracker for perfgate.
    
    Bug: 169436495
    Test: New tests to be run in presubmit
    Change-Id: Ie8bab9e40b9d6781f23db0799fbd262862913dd5

---
+++ sync-perf-tests/testSrc/com/android/tools/idea/gradle/project/sync/perf/AbstractGradleSyncPerfSmokeTestCase.kt
@@ -0,0 +1,110 @@
+/*
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.gradle.project.sync.perf
+
+import com.android.tools.idea.testing.AndroidGradleProjectRule
+import com.android.tools.idea.testing.AndroidGradleTests
+import com.android.tools.idea.testing.AndroidGradleTests.overrideJdkTo8
+import com.android.tools.idea.testing.AndroidGradleTests.restoreJdk
+import com.intellij.openapi.Disposable
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.application.impl.ApplicationInfoImpl
+import com.intellij.openapi.util.Disposer
+import com.intellij.testFramework.EdtRule
+import com.intellij.testFramework.RunsInEdt
+import com.intellij.testFramework.fixtures.CodeInsightTestFixture
+import org.jetbrains.android.AndroidTestBase
+import org.jetbrains.plugins.gradle.settings.DistributionType
+import org.jetbrains.plugins.gradle.settings.GradleProjectSettings
+import org.jetbrains.plugins.gradle.settings.GradleSettings
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import java.io.File
+
+@RunsInEdt
+abstract class AbstractGradleSyncPerfSmokeTestCase {
+  abstract val relativePath: String
+  protected open val buildTask: String? = "assembleDebug"
+
+  protected val projectRule = AndroidGradleProjectRule()
+  @get:Rule
+  val ruleChain = org.junit.rules.RuleChain.outerRule(projectRule).around(EdtRule())!!
+
+  @Before
+  @Throws(Exception::class)
+  open fun setUp() {
+    val projectSettings = GradleProjectSettings()
+    projectSettings.distributionType = DistributionType.DEFAULT_WRAPPED
+    GradleSettings.getInstance(projectRule.project).linkedProjectsSettings = listOf(projectSettings)
+
+    projectRule.fixture.testDataPath = AndroidTestBase.getModulePath("sync-perf-tests") + File.separator + "testData"
+    disableExpensivePlatformAssertions(projectRule.fixture)
+    AndroidGradleTests.overrideWaitForSourceFolderTimeout(5000)
+  }
+
+  /**
+   * Verify that the test project is able to open and sync without errors.
+   * @throws Exception
+   */
+  @Throws(java.lang.Exception::class)
+  @Test
+  open fun testSyncsAndBuilds() {
+    verifySync()
+    verifyBuild()
+  }
+
+  /**
+   * Verify that the test project is able to open and sync without errors using JDK8.
+   * @throws Exception
+   */
+  @Throws(java.lang.Exception::class)
+  @Test
+  open fun testSyncsAndBuildsJdk8() {
+    overrideJdkTo8()
+    try {
+      verifySync();
+      verifyBuild()
+    }
+    finally {
+      restoreJdk()
+    }
+  }
+
+  private fun verifySync() {
+    // Load also syncs, this should be enough to confirm sync is successful
+    projectRule.loadProject(relativePath)
+  }
+
+  private fun verifyBuild() {
+    if (buildTask == null) return
+    ApplicationManager.getApplication().invokeAndWait {
+      projectRule.invokeTasks(buildTask!!).apply {
+        buildError?.printStackTrace()
+        assertTrue("The project must compile correctly for the test to pass", isBuildSuccessful)
+      }
+    }
+  }
+
+  private fun disableExpensivePlatformAssertions(fixture: CodeInsightTestFixture) {
+    ApplicationInfoImpl.setInStressTest(true)
+    Disposer.register(fixture.testRootDisposable, Disposable {
+      ApplicationInfoImpl.setInStressTest(false)
+      AndroidGradleTests.resetWaitForSourceFolderTimeout()
+    })
+  }
+}
