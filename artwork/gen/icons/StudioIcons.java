a3010ac282dcbbc2700e9abc309bb22c10f174dc
Chris Sinco <csinco@google.com>
2020-09-30 01:35:39 -0700

    New module wizard icons - Refactor into consistent set in StudioIcons - Adds new icon for Native library template
    Test: Visual inspection of local build
    Bug: 161532618
    Fixes: 168820589
    Change-Id: I0c7d6f7510b25d88b44cce44aedee55fceebfe33

--- artwork/gen/icons/StudioIcons.java
+++ artwork/gen/icons/StudioIcons.java
@@ -744,4 +744,19 @@ public class StudioIcons {
   public static class Test {
     public static final Icon RECORD_ESPRESSO_TEST = load("/studio/icons/test/record-espresso-test.svg"); // 16x16
   }
+
+  public static class Wizards {
+    public static class Modules {
+      public static final Icon ANDROID_LIBRARY = load("/studio/icons/wizards/modules/android-library.svg"); // 16x16
+      public static final Icon ANDROID_THINGS = load("/studio/icons/wizards/modules/android-things.svg"); // 16x16
+      public static final Icon ANDROID_TV = load("/studio/icons/wizards/modules/android-tv.svg"); // 16x16
+      public static final Icon AUTOMOTIVE = load("/studio/icons/wizards/modules/automotive.svg"); // 16x16
+      public static final Icon BENCHMARK = load("/studio/icons/wizards/modules/benchmark.svg"); // 16x16
+      public static final Icon DYNAMIC_FEATURE = load("/studio/icons/wizards/modules/dynamic-feature.svg"); // 16x16
+      public static final Icon INSTANT_DYNAMIC_FEATURE = load("/studio/icons/wizards/modules/instant-dynamic-feature.svg"); // 16x16
+      public static final Icon NATIVE = load("/studio/icons/wizards/modules/native.svg"); // 16x16
+      public static final Icon PHONE_TABLET = load("/studio/icons/wizards/modules/phone-tablet.svg"); // 16x16
+      public static final Icon WEAR_OS = load("/studio/icons/wizards/modules/wear-os.svg"); // 16x16
+    }
+  }
 }
