0ff6b922b1d09180236d994739b1d095c8b27c9b
Elena.Korotkova <elena.korotkova@jetbrains.com>
2020-10-07 11:31:44 +0300

    Fix speed search filter
    GitOrigin-RevId: 72c77c1f6508891aea7702b165acabb3ef2aedb1

--- android/src/com/android/tools/idea/gradle/structure/configurables/ui/treeview/TreeBuilderSpeedSearch.java
+++ android/src/com/android/tools/idea/gradle/structure/configurables/ui/treeview/TreeBuilderSpeedSearch.java
@@ -427,4 +427,9 @@ public final class TreeBuilderSpeedSearch extends SpeedSearchSupply {
       }
     }
   }
+
+  @Override
+  public boolean isObjectFilteredOut(Object o) {
+    return false;
+  }
 }
