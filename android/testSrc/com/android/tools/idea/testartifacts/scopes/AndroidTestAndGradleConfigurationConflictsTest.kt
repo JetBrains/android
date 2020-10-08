c5025fd06d03b23983cb39d7dc12c9c6f4a6d134
Karima Ichou <karimai@google.com>
2020-10-08 15:20:39 +0100

    Fix creating AndroidTest package run configuration when using GRADLE runner.
    When using GRADLE test runner for unit test, and when AndroidTest and
    unit tests have the same package name, creating a "all in package"
    androidTest run configuration does run unit tests instead.
    This is due to the fact that GRADLE finds that both androidTest and
    unitTests have the same test tasks.
    As we use GRADLE only for unit tests, it doesn't make sense generating
    test tasks for androidTest artifacts.
    This change adds a fix to only create gradle test task for unit tests
    sources.
    
    bug: 173106394
    bug: 173107350
    Test: added
    Change-Id: I54a458f672f889acb5eddc4d7aa9a61ac5be13cb
    
    GitOrigin-RevId: c999128ba84c75c382bdd246ca4fce9ffbadd9c0

---
+++ android/testSrc/com/android/tools/idea/testartifacts/scopes/AndroidTestAndGradleConfigurationConflictsTest.kt
@@ -0,0 +1,72 @@
+/*
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.testartifacts.scopes
+
+import com.android.tools.idea.flags.StudioFlags
+import com.android.tools.idea.testartifacts.TestConfigurationTesting
+import com.android.tools.idea.testartifacts.createAndroidGradleConfigurationFromDirectory
+import com.android.tools.idea.testing.AndroidGradleTestCase
+import com.intellij.execution.configurations.RunConfiguration
+import com.intellij.openapi.project.Project
+import junit.framework.TestCase
+
+
+/**
+ * Tests for verifying that there is no conflict creating an instrumented test after a Gradle unit test.
+ */
+class AndroidTestAndGradleConfigurationConflictsTest : AndroidGradleTestCase() {
+  // See: http://b.android.com/173106394
+  override fun setUp() {
+    super.setUp()
+    StudioFlags.GRADLE_UNIT_TESTING.override(true)
+  }
+
+  @Throws(Exception::class)
+  override fun tearDown() {
+    super.tearDown()
+    StudioFlags.GRADLE_UNIT_TESTING.clearOverride()
+  }
+
+  @Throws(Exception::class)
+  fun testCanCreateInstrumentedTestConfiguration() {
+    loadSimpleApplication()
+    val gradleRunConfiguration = TestCase.assertNotNull(createAndroidGradleConfigurationFromDirectory(
+      project, "app/src/test/java/google/simpleapplication"))
+    TestCase.assertNotNull(gradleRunConfiguration)
+
+    // Verify that that no configuration is created from the context of AndroidTest artifact.
+    // This follows the workflow on AS when trying create a AndroidTest configuration, where we first check if any existing configuration
+    // was created from the AndroidTest context, and return it.
+    // The Gradle Configuration producer fails in detecting that the created unit test configuration isn't from the context of AndroidTest
+    // because it doesn't check for the unique PSI location.
+    var androidTestRunConfiguration = findExistingAndroidTestConfigurationFromDirectory(
+      project, "app/src/androidTest/java/google/simpleapplication")
+    TestCase.assertNull(androidTestRunConfiguration)
+
+    // Verify that we can successfully create an AndroidTest run configuration.
+    androidTestRunConfiguration = TestConfigurationTesting.createAndroidTestConfigurationFromDirectory(
+      project, "app/src/androidTest/java/google/simpleapplication")
+    TestCase.assertNotNull(androidTestRunConfiguration)
+  }
+
+  private fun findExistingAndroidTestConfigurationFromDirectory(project: Project, directory: String): RunConfiguration? {
+    val element = TestConfigurationTesting.getPsiElement(project, directory, true)
+    val context = TestConfigurationTesting.createContext(project, element)
+    // Search for any existing run configuration that was created from this context.
+    val existing = context.findExisting() ?: return null
+    return existing.configuration
+  }
+}
