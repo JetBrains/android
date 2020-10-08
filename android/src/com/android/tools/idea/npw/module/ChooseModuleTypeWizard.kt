c0f30197446369a659495e4efafce286ca025628
Jose Parente <parentej@google.com>
2020-10-08 15:21:45 +0100

    [New Module] Sort "Android Native Library" entry
    Bug: 156915925
    Test: ChooseModuleTypeStepTest
    Change-Id: I5cd7f7b6f9984be1452d1d4cc010952587f2d7d5
    
    GitOrigin-RevId: fd2e7d09734238e89ae3e2f644ce938c8e69c3a3

--- android/src/com/android/tools/idea/npw/module/ChooseModuleTypeWizard.kt
+++ android/src/com/android/tools/idea/npw/module/ChooseModuleTypeWizard.kt
@@ -239,6 +239,7 @@ fun sortModuleEntries(moduleTypeProviders: List<ModuleGalleryEntry>): List<Modul
     message("android.wizard.module.new.things"),
     message("android.wizard.module.import.gradle.title"),
     message("android.wizard.module.import.eclipse.title"),
+    message("android.wizard.module.new.native.library"),
     message("android.wizard.module.new.java.or.kotlin.library"),
     message("android.wizard.module.new.google.cloud"),
     message("android.wizard.module.new.benchmark.module.app"))
