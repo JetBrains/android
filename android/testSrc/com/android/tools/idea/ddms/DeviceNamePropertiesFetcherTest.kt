4fe0d4bc6ff0e35ba1ad426b9d20d70bac109d40
Juan C Nuno <juancnuno@google.com>
2020-10-12 13:34:31 -0700

    Clean up DeviceNamePropertiesProviderTest
    Bug: Not applicable
    Test: DeviceNamePropertiesFetcherTest
    Change-Id: I39275d8b0e72515e37bbd5ec96e52480ea793655
    
    GitOrigin-RevId: 9de0858eeb52accd34c069cf4fc92ef33eb9d531

--- android/testSrc/com/android/tools/idea/ddms/DeviceNamePropertiesFetcherTest.kt
+++ android/testSrc/com/android/tools/idea/ddms/DeviceNamePropertiesFetcherTest.kt
@@ -18,14 +18,15 @@ package com.android.tools.idea.ddms
 import com.android.ddmlib.IDevice
 import com.android.testutils.VirtualTimeScheduler
 import com.android.tools.idea.ddms.DeviceNamePropertiesFetcher.DefaultCallback
-import com.google.common.truth.Truth
 import com.google.common.util.concurrent.FutureCallback
 import com.google.common.util.concurrent.Futures
 import com.intellij.openapi.util.Disposer
-import junit.framework.TestCase
 import org.junit.After
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNull
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
 import org.mockito.Mockito
 import java.util.concurrent.Callable
 import java.util.concurrent.CountDownLatch
@@ -38,7 +39,8 @@ enum class ResultType {
   FAIL
 }
 
-class DeviceNamePropertiesProviderTest {
+@RunWith(JUnit4::class)
+internal class DeviceNamePropertiesFetcherTest {
   private val myDisposable = Disposer.newDisposable()
   private val manufacturer = "google"
   private val model = "nexus 4"
@@ -80,14 +82,14 @@ class DeviceNamePropertiesProviderTest {
   }
 
   @Test
-  fun testGet_DefaultValue() {
+  fun getDefaultValue() {
     val d = createDevice(
-        Futures.immediateFuture(manufacturer),
-        Futures.immediateFuture(model),
-        Futures.immediateFuture(buildVersion),
-        Futures.immediateFuture(apiLevel))
+      Futures.immediateFuture(manufacturer),
+      Futures.immediateFuture(model),
+      Futures.immediateFuture(buildVersion),
+      Futures.immediateFuture(apiLevel))
     val deviceNamePropertiesProvider = createDeviceNamePropertiesProvider(AtomicReference(), listOf(CountDownLatch(1)))
-    Truth.assertThat(deviceNamePropertiesProvider.get(d).model).isNull()
+    assertNull(deviceNamePropertiesProvider.get(d).model)
   }
 
   @Test
@@ -104,136 +106,124 @@ class DeviceNamePropertiesProviderTest {
   }
 
   @Test
-  fun testGet_ValueAvailableImmediate() {
+  fun getValueAvailableImmediate() {
     val d = createDevice(
-        Futures.immediateFuture(manufacturer),
-        Futures.immediateFuture(model),
-        Futures.immediateFuture(buildVersion),
-        Futures.immediateFuture(apiLevel))
+      Futures.immediateFuture(manufacturer),
+      Futures.immediateFuture(model),
+      Futures.immediateFuture(buildVersion),
+      Futures.immediateFuture(apiLevel))
     val result = AtomicReference<ResultType>()
     val countDownLatch = CountDownLatch(1)
     val deviceNamePropertiesProvider = createDeviceNamePropertiesProvider(result, listOf(countDownLatch))
     var deviceProperties = deviceNamePropertiesProvider.get(d)
-    Truth.assertThat(deviceProperties.manufacturer).isNull()
+    assertNull(deviceProperties.manufacturer)
     countDownLatch.await(2, TimeUnit.SECONDS)
     deviceProperties = deviceNamePropertiesProvider.get(d)
-    TestCase.assertEquals(manufacturer, deviceProperties.manufacturer)
-    TestCase.assertEquals(model, deviceProperties.model)
-    TestCase.assertEquals(buildVersion, deviceProperties.buildVersion)
-    TestCase.assertEquals(apiLevel, deviceProperties.apiLevel)
-    TestCase.assertEquals(ResultType.SUCCESS, result.get())
+    assertEquals(DeviceNameProperties(model, manufacturer, buildVersion, apiLevel), deviceProperties)
+    assertEquals(ResultType.SUCCESS, result.get())
   }
 
   @Test
-  fun testGet_DefaultValueUntilValueAvailable() {
+  fun getDefaultValueUntilValueAvailable() {
     val virtualTimeScheduler = VirtualTimeScheduler()
     val d = createDevice(
-        virtualTimeScheduler.schedule(Callable<String> { manufacturer }, 5, TimeUnit.SECONDS),
-        virtualTimeScheduler.schedule(Callable<String> { model }, 5, TimeUnit.SECONDS),
-        virtualTimeScheduler.schedule(Callable<String> { buildVersion }, 5, TimeUnit.SECONDS),
-        virtualTimeScheduler.schedule(Callable<String> { apiLevel }, 5, TimeUnit.SECONDS))
+      virtualTimeScheduler.schedule(Callable { manufacturer }, 5, TimeUnit.SECONDS),
+      virtualTimeScheduler.schedule(Callable { model }, 5, TimeUnit.SECONDS),
+      virtualTimeScheduler.schedule(Callable { buildVersion }, 5, TimeUnit.SECONDS),
+      virtualTimeScheduler.schedule(Callable { apiLevel }, 5, TimeUnit.SECONDS))
     val result = AtomicReference<ResultType>()
     val countDownLatch = CountDownLatch(1)
     val deviceNamePropertiesProvider = createDeviceNamePropertiesProvider(result, listOf(countDownLatch))
     var deviceProperties = deviceNamePropertiesProvider.get(d)
-    Truth.assertThat(deviceProperties.manufacturer).isNull()
+    assertNull(deviceProperties.manufacturer)
     virtualTimeScheduler.advanceBy(5, TimeUnit.SECONDS)
     countDownLatch.await(2, TimeUnit.SECONDS)
     deviceProperties = deviceNamePropertiesProvider.get(d)
-    TestCase.assertEquals(manufacturer, deviceProperties.manufacturer)
-    TestCase.assertEquals(model, deviceProperties.model)
-    TestCase.assertEquals(buildVersion, deviceProperties.buildVersion)
-    TestCase.assertEquals(apiLevel, deviceProperties.apiLevel)
-    TestCase.assertEquals(ResultType.SUCCESS, result.get())
+    assertEquals(DeviceNameProperties(model, manufacturer, buildVersion, apiLevel), deviceProperties)
+    assertEquals(ResultType.SUCCESS, result.get())
   }
 
   @Test
-  fun testGet_ExceptionIsThrownDuringRetrieve() {
+  fun getExceptionIsThrownDuringRetrieve() {
     val d = createDevice(
-        Futures.immediateFailedFuture(Exception("Fail")),
-        Futures.immediateFailedFuture(Exception("Fail")),
-        Futures.immediateFailedFuture(Exception("Fail")),
-        Futures.immediateFailedFuture(Exception("Fail")))
+      Futures.immediateFailedFuture(Exception("Fail")),
+      Futures.immediateFailedFuture(Exception("Fail")),
+      Futures.immediateFailedFuture(Exception("Fail")),
+      Futures.immediateFailedFuture(Exception("Fail")))
     val result = AtomicReference<ResultType>()
     val successLatch = CountDownLatch(1)
     val failureLatch = CountDownLatch(1)
     val deviceNamePropertiesProvider = createDeviceNamePropertiesProvider(result, listOf(successLatch), failureLatch)
     var deviceProperties = deviceNamePropertiesProvider.get(d)
-    Truth.assertThat(deviceProperties.manufacturer).isNull()
+    assertNull(deviceProperties.manufacturer)
     successLatch.await(2, TimeUnit.SECONDS)
     deviceProperties = deviceNamePropertiesProvider.get(d)
     failureLatch.await(2, TimeUnit.SECONDS)
-    Truth.assertThat(deviceProperties.manufacturer).isNull()
-    TestCase.assertEquals(ResultType.FAIL, result.get())
+    assertNull(deviceProperties.manufacturer)
+    assertEquals(ResultType.FAIL, result.get())
   }
 
   @Test
-  fun testGet_DeviceUnauthorized() {
+  fun getDeviceUnauthorized() {
     val d = createDevice(
-        Futures.immediateFuture(null),
-        Futures.immediateFuture(null),
-        Futures.immediateFuture(null),
-        Futures.immediateFuture(null))
+      Futures.immediateFuture(null),
+      Futures.immediateFuture(null),
+      Futures.immediateFuture(null),
+      Futures.immediateFuture(null))
     val result = AtomicReference<ResultType>()
     val countDownLatch = CountDownLatch(1)
     val deviceNamePropertiesProvider = createDeviceNamePropertiesProvider(result, listOf(countDownLatch))
     var deviceProperties = deviceNamePropertiesProvider.get(d)
-    Truth.assertThat(deviceProperties.buildVersion).isNull()
+    assertNull(deviceProperties.buildVersion)
     countDownLatch.await(2, TimeUnit.SECONDS)
     deviceProperties = deviceNamePropertiesProvider.get(d)
-    Truth.assertThat(deviceProperties.buildVersion).isNull()
-    TestCase.assertEquals(ResultType.SUCCESS, result.get())
+    assertNull(deviceProperties.buildVersion)
+    assertEquals(ResultType.SUCCESS, result.get())
   }
 
   @Test
-  fun testGet_DeviceUnauthorizedUntilAuthorized() {
+  fun getDeviceUnauthorizedUntilAuthorized() {
     val d = Mockito.mock(IDevice::class.java)
     Mockito.`when`(
-        d.getSystemProperty(IDevice.PROP_DEVICE_MANUFACTURER))
-        .thenReturn(Futures.immediateFuture(null))
-        .thenReturn(Futures.immediateFuture(null))
-        .thenReturn(Futures.immediateFuture(manufacturer))
+      d.getSystemProperty(IDevice.PROP_DEVICE_MANUFACTURER))
+      .thenReturn(Futures.immediateFuture(null))
+      .thenReturn(Futures.immediateFuture(null))
+      .thenReturn(Futures.immediateFuture(manufacturer))
     Mockito.`when`(
-        d.getSystemProperty(IDevice.PROP_DEVICE_MODEL))
-        .thenReturn(Futures.immediateFuture(null))
-        .thenReturn(Futures.immediateFuture(model))
-        .thenReturn(Futures.immediateFuture(model))
+      d.getSystemProperty(IDevice.PROP_DEVICE_MODEL))
+      .thenReturn(Futures.immediateFuture(null))
+      .thenReturn(Futures.immediateFuture(model))
+      .thenReturn(Futures.immediateFuture(model))
     Mockito.`when`(
-        d.getSystemProperty(IDevice.PROP_BUILD_VERSION))
-        .thenReturn(Futures.immediateFuture(null))
-        .thenReturn(Futures.immediateFuture(null))
-        .thenReturn(Futures.immediateFuture(buildVersion))
+      d.getSystemProperty(IDevice.PROP_BUILD_VERSION))
+      .thenReturn(Futures.immediateFuture(null))
+      .thenReturn(Futures.immediateFuture(null))
+      .thenReturn(Futures.immediateFuture(buildVersion))
     Mockito.`when`(
-        d.getSystemProperty(IDevice.PROP_BUILD_API_LEVEL))
-        .thenReturn(Futures.immediateFuture(null))
-        .thenReturn(Futures.immediateFuture(null))
-        .thenReturn(Futures.immediateFuture(apiLevel))
+      d.getSystemProperty(IDevice.PROP_BUILD_API_LEVEL))
+      .thenReturn(Futures.immediateFuture(null))
+      .thenReturn(Futures.immediateFuture(null))
+      .thenReturn(Futures.immediateFuture(apiLevel))
     val result = AtomicReference<ResultType>()
     val successLatch1 = CountDownLatch(1)
     val successLatch2 = CountDownLatch(1)
     val successLatch3 = CountDownLatch(1)
     val deviceNamePropertiesProvider = createDeviceNamePropertiesProvider(result, listOf(successLatch1, successLatch2, successLatch3))
     var deviceProperties = deviceNamePropertiesProvider.get(d)
-    Truth.assertThat(deviceProperties.buildVersion).isNull()
+    assertNull(deviceProperties.buildVersion)
     successLatch1.await(2, TimeUnit.SECONDS)
     deviceProperties = deviceNamePropertiesProvider.get(d)
-    Truth.assertThat(deviceProperties.buildVersion).isNull()
-    TestCase.assertEquals(ResultType.SUCCESS, result.get())
+    assertNull(deviceProperties.buildVersion)
+    assertEquals(ResultType.SUCCESS, result.get())
 
     successLatch2.await(2, TimeUnit.SECONDS)
     deviceProperties = deviceNamePropertiesProvider.get(d)
-    TestCase.assertEquals(model, deviceProperties.model)
-    Truth.assertThat(deviceProperties.manufacturer).isNull()
-    Truth.assertThat(deviceProperties.buildVersion).isNull()
-    Truth.assertThat(deviceProperties.apiLevel).isNull()
-    TestCase.assertEquals(ResultType.SUCCESS, result.get())
+    assertEquals(DeviceNameProperties(model, null, null, null), deviceProperties)
+    assertEquals(ResultType.SUCCESS, result.get())
 
     successLatch3.await(2, TimeUnit.SECONDS)
     deviceProperties = deviceNamePropertiesProvider.get(d)
-    TestCase.assertEquals(manufacturer, deviceProperties.manufacturer)
-    TestCase.assertEquals(model, deviceProperties.model)
-    TestCase.assertEquals(buildVersion, deviceProperties.buildVersion)
-    TestCase.assertEquals(apiLevel, deviceProperties.apiLevel)
-    TestCase.assertEquals(ResultType.SUCCESS, result.get())
+    assertEquals(DeviceNameProperties(model, manufacturer, buildVersion, apiLevel), deviceProperties)
+    assertEquals(ResultType.SUCCESS, result.get())
   }
 }
