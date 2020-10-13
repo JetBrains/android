abed8df9fe9650f515bfb89e504d9f2bba9afe8c
Christophe Rhodes <xof@google.com>
2020-10-13 14:57:39 +0100

    [AUA] adjust integration test for 4.1.0 release
    Now that AGP 4.1.0 is released, we can not only upgrade the build
    files from 4.0.0 to 4.1.0 but also successfully sync with the upgraded
    version (provided it and all its dependencies are available to the
    running system).
    
    Bug: n/a
    Test: improved
    Change-Id: I163ebcb6b51c33e50950f3d382f7074360546068
    
    GitOrigin-RevId: 727220ab09af989ca66445441b9d4611295ae971

--- android/testSrc/com/android/tools/idea/gradle/project/upgrade/AgpUpgradeAssistantIntegrationTest.kt
+++ android/testSrc/com/android/tools/idea/gradle/project/upgrade/AgpUpgradeAssistantIntegrationTest.kt
@@ -36,11 +36,8 @@ class AgpUpgradeAssistantIntegrationTest : AndroidGradleTestCase() {
     val processor = AgpUpgradeRefactoringProcessor(project, GradleVersion.parse("4.0.0"), GradleVersion.parse("4.1.0"))
     processor.run()
 
-    // TODO(xof): assert that the post-sync was successful once 4.1.0 is released and added to prebuilts.  (For now this effectively
-    //  asserts that Sync was attempted after running the processor).
-    assertThat(ProjectSystemService.getInstance(project).projectSystem.getSyncManager().getLastSyncResult()).isEqualTo(FAILURE)
-    // TODO(xof): assert that the post-sync actually used AGP 4.1.0 once it is released and added to prebuilts.
-    //    assertThat(GradleFacet.getInstance(appModule)?.configuration?.LAST_SUCCESSFUL_SYNC_AGP_VERSION).isEqualTo("4.1.0")
+    assertThat(ProjectSystemService.getInstance(project).projectSystem.getSyncManager().getLastSyncResult()).isEqualTo(SUCCESS)
+    assertThat(GradleFacet.getInstance(appModule)?.configuration?.LAST_SUCCESSFUL_SYNC_AGP_VERSION).isEqualTo("4.1.0")
 
     // We can't straightforwardly assert the exact contents of build.gradle, because the test infrastructure patches it with repository
     // declarations to make it work in the test environment
