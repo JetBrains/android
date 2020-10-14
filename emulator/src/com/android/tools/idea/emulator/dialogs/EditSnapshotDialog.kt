6dfe34d1f0bd226353969d6fa2406be54fde6b9a
Sergey Prigogin <sprigogin@google.com>
2020-10-14 11:00:56 -0700

    Move the "dialogs" package one level up
    Test: existing
    Bug: N/A
    Change-Id: I69361aa74a8e81b85e8dbf6e066970c083e55014

--- emulator/src/com/android/tools/idea/emulator/dialogs/EditSnapshotDialog.kt
+++ emulator/src/com/android/tools/idea/emulator/dialogs/EditSnapshotDialog.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.android.tools.idea.emulator.actions.dialogs
+package com.android.tools.idea.emulator.dialogs
 
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.DialogPanel
