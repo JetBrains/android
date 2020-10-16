b99592561cc1b41953bcb12a01fa501d6224907a
Diego Perez <diegoperez@google.com>
2020-10-16 17:34:07 +0100

    Move change and build listeners to android core
    This will allow for these utilities to be reused in live literals that
    are also moving to a common place to allow both the Compose preview and
    Apply changes to use them.
    
    Bug: 171053266
    Test: Moved tests pass
    Change-Id: I6191375b9552f91b4f4f23cdc38d295bb40f6359
    
    GitOrigin-RevId: a9c346d8f8f8236841ac663cc2165756c97822eb

--- designer/testSrc/com/android/tools/idea/gradle/util/BuildListenerTest.kt
+++ designer/testSrc/com/android/tools/idea/gradle/util/BuildListenerTest.kt
@@ -1,30 +1,14 @@
-/*
- * Copyright (C) 2019 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-package com.android.tools.idea.common.util
+package com.android.tools.idea.gradle.util
 
 import com.android.tools.idea.gradle.project.build.BuildContext
 import com.android.tools.idea.gradle.project.build.BuildStatus
 import com.android.tools.idea.gradle.project.build.GradleBuildState
-import com.android.tools.idea.gradle.util.BuildMode
 import com.intellij.testFramework.PlatformTestCase
 import org.mockito.Mock
 import org.mockito.Mockito
-import org.mockito.MockitoAnnotations.initMocks
+import org.mockito.MockitoAnnotations
 
-class ChangeManagerBuildTest : PlatformTestCase() {
+class BuildListenerTest : PlatformTestCase() {
   private lateinit var buildContext: BuildContext
   private lateinit var buildState: GradleBuildState
 
@@ -33,7 +17,7 @@ class ChangeManagerBuildTest : PlatformTestCase() {
 
   override fun setUp() {
     super.setUp()
-    initMocks(this)
+    MockitoAnnotations.initMocks(this)
 
     setupBuildListener(project, buildListener, project)
     buildState = GradleBuildState.getInstance(project)
