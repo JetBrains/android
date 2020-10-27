4d1134ff59565d2fafec4fcf2dd569334f987716
Andrei Kuznetsov <andrei.kuznetsov@jetbrains.com>
2020-10-27 20:55:25 +0300

    IDEA-252775 [merge-android-41] re-generate icons
    GitOrigin-RevId: ea010ba97399ecf7494525194543929c8e6a2a8a

---
+++ whats-new-assistant/src/icons/WhatsNewAssistantIcons.java
@@ -0,0 +1,24 @@
+package icons;
+
+import com.intellij.ui.IconManager;
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
+
+/**
+ * NOTE THIS FILE IS AUTO-GENERATED
+ * DO NOT EDIT IT BY HAND, run "Generate icon classes" configuration instead
+ */
+public final class WhatsNewAssistantIcons {
+  private static @NotNull Icon load(@NotNull String path, long cacheKey, int flags) {
+    return IconManager.getInstance().loadRasterizedIcon(path, WhatsNewAssistantIcons.class, cacheKey, flags);
+  }
+
+  public static final class Preview {
+    /** 29x29 */ public static final @NotNull Icon Whats_new_icon = load("/preview/whats_new_icon.png", 0L, 1);
+  }
+
+  public static final class Stable {
+    /** 29x29 */ public static final @NotNull Icon Whats_new_icon = load("/stable/whats_new_icon.png", 0L, 1);
+  }
+}
