a1e64b8be4114956109a0826e8ba28f6292e1cd9
Esteban de la Canal <estebandlc@google.com>
2020-10-29 09:16:36 -0700

    Validate plugin dependency at build time
    Plugin dependencies are now explicit in build files. We
    validate at build time that what is declared matches
    what is written in plugin.xml files.
    
    Note this covers only basic cases and forces them to be
    in sync. Follow up change will validate that what modules
    depend on matches what the plugin structure needs.
    
    Test: This introduces changes at build time
    Bug: N/A
    Change-Id: Ia7bef21285247b3611b02d500a072733175d6513

--- studio/check_plugin.py
+++ studio/check_plugin.py
@@ -7,7 +7,7 @@ import xml.etree.ElementTree as ET
 import zipfile
 import sys
 
-def check_plugin(plugin_id, files, out):
+def check_plugin(plugin_id, files, deps, out):
   xmls = {}
   for file in files:
     if file.endswith(".jar"):
@@ -18,7 +18,7 @@ def check_plugin(plugin_id, files, out):
 
   if len(xmls) != 1:
     msg = "\n".join(xmls.keys())
-    print("Plugin %s should have exactly one plugin.xml file (found %d)" % (plugin_id, len(xmls)))
+    print("Plugin should have exactly one plugin.xml file (found %d)" % len(xmls))
     print(msg)
     sys.exit(1)
 
@@ -26,11 +26,44 @@ def check_plugin(plugin_id, files, out):
   element = ET.fromstring(xml)
   ids = [id.text for id in element.findall("id")]
 
-  if ids != [plugin_id]:
-    print("Expected exactly one id of the form \"%s\", but found [%s]" % (plugin_id, ",".join(ids)))
+  if len(ids) != 1:
+      print("Expected exactly one id, but found [%s]" % ",".join(ids))
+      sys.exit(1)
+  found_id = ids[0]
+
+  if plugin_id and found_id != plugin_id:
+    print("Expected plugin id to be %d, but found %s" % (plugin_id, found_id))
     sys.exit(1)
 
-  open(out, "w").close()
+  if deps != None:
+    depends_xml = set()
+    for e in element.findall("depends"):
+      # We only validate plugin dependencies not module ones
+      if e.text in [
+          "com.intellij.modules.java",
+          "com.intellij.modules.lang",
+          "com.intellij.modules.platform",
+          "com.intellij.modules.vcs",
+          "com.intellij.modules.xdebugger",
+          "com.intellij.modules.xml",
+      ]:
+        continue
+      depends_xml.add(e.text)
+
+    depends_build = set()
+    for d in deps:
+      with open(d, "r") as info:
+        depends_build.add(info.read())
+    if depends_build != depends_xml:
+      print("Error while checking plugin dependencies")
+      for d in depends_build - depends_xml:
+        print("The build depends on plugin \"%s\", but this dependency is not declared in the plugin.xml." % d)
+      for d in depends_xml - depends_build:
+        print("The plugin.xml declares a dependency on \"%s\", but it's not declared in the build." % d)
+      sys.exit(1)
+
+  with open(out, "w") as info:
+    info.write(found_id)
 
 
 if __name__ == "__main__":
@@ -41,13 +74,18 @@ if __name__ == "__main__":
       nargs="+",
       help="Path to files included in the plugin.")
   parser.add_argument(
+      "--deps",
+      dest="deps",
+      default=None,
+      nargs="*",
+      help="Ids of the plugins this plugin depends on.")
+  parser.add_argument(
       "--plugin_id",
       dest="plugin_id",
-      required=True,
-      help="The expected plugin id.")
+      help="The expected id of this plugin.")
   parser.add_argument(
       "--out",
       dest="out",
-      help="Path to a log file where to save the result of the analysis.")
+      help="Path to a file where to save the plugin information.")
   args = parser.parse_args()
-  check_plugin(args.plugin_id, args.files, args.out)
+  check_plugin(args.plugin_id, args.files, args.deps, args.out)
