f828b1baf61c2ea0390b45c51b1cf93470e3b837
Andrei Kuznetsov <andrei.kuznetsov@jetbrains.com>
2020-10-30 18:36:52 +0300

    IDEA-252775 [merge-android-41] re-apply commit 3b9865377945e958b49a9615f8a25913a084f788 Author: Dmitry Avdeev Date: 09.10.2019, 13:16
    do not throw exception in IDEA plugin (which has no embedded JDK)
    
    GitOrigin-RevId: 4d234d7b71fe5b6fe99f1ae4bfa693752702a9db

--- android/src/com/android/tools/idea/welcome/wizard/deprecated/InstallComponentsPath.java
+++ android/src/com/android/tools/idea/welcome/wizard/deprecated/InstallComponentsPath.java
@@ -186,7 +186,11 @@ public class InstallComponentsPath extends DynamicWizardPath implements LongRunn
     assert location != null;
 
     myState.put(WizardConstants.KEY_SDK_INSTALL_LOCATION, location.getAbsolutePath());
-    myState.put(WizardConstants.KEY_JDK_LOCATION, EmbeddedDistributionPaths.getInstance().getEmbeddedJdkPath().getPath());
+    File file = EmbeddedDistributionPaths.getInstance().tryToGetEmbeddedJdkPath();
+    if (file != null) {
+      // There is no embedded JDK in IDEA
+      myState.put(WizardConstants.KEY_JDK_LOCATION, file.getPath());
+    }
 
     myComponentTree = createComponentTree(myMode, !isChromeOSAndIsNotHWAccelerated() && myMode.shouldCreateAvd());
     myComponentTree.init(myProgressStep);
