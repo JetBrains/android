21672f88b57bf0ebd816b4d77dd66d687769038c
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2020-11-02 07:41:53 +0100

    IDEA-250250 on type mismatch, report thread dump not as part of message, but as an attachment
    GitOrigin-RevId: 587eda6c482f8223b12cc340c2de11a78e0b43e6

--- profilers-android/testSrc/com/android/tools/idea/profilers/stacktrace/ProjectStub.java
+++ profilers-android/testSrc/com/android/tools/idea/profilers/stacktrace/ProjectStub.java
@@ -17,11 +17,14 @@ package com.android.tools.idea.profilers.stacktrace;
 
 import com.intellij.openapi.extensions.ExtensionsArea;
 import com.intellij.openapi.extensions.PluginDescriptor;
+import com.intellij.openapi.extensions.PluginId;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.Condition;
 import com.intellij.openapi.util.UserDataHolderBase;
 import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.util.ExceptionUtilRt;
 import com.intellij.util.messages.MessageBus;
+import java.util.Map;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -147,5 +150,26 @@ final class ProjectStub extends UserDataHolderBase implements Project {
 
   @Override
   public void dispose() {
+  }
+
+  @Override
+  public @NotNull
+  RuntimeException createError(@NotNull @NonNls String message, @NotNull PluginId pluginId) {
+    return new RuntimeException(message);
+  }
+
+  @Override
+  public @NotNull
+  RuntimeException createError(@NotNull @NonNls String message,
+                               @NotNull PluginId pluginId,
+                               @Nullable Map<String, String> attachments) {
+    return new RuntimeException(message);
+  }
+
+  @Override
+  public @NotNull
+  RuntimeException createError(@NotNull Throwable error, @NotNull PluginId pluginId) {
+    ExceptionUtilRt.rethrowUnchecked(error);
+    return new RuntimeException(error);
   }
 }
