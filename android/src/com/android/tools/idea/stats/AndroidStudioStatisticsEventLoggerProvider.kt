a026a551acff173957e67bb1b7a4f8d6bd96c0a5
Mike Moroney <mmoro@google.com>
2020-11-01 19:43:37 -0800

    Add an event logger provider for file usage events
    This is an initial checkin of a logger that will allow us to capture
    file usage events raised by IntelliJ and log them to
    Android Studio metrics.
    
    Bug: 112586100
    Test: None
    
    Change-Id: I6f6a1c42dd2f94bf553a6dda04022b2e8cb9690d

---
+++ android/src/com/android/tools/idea/stats/AndroidStudioStatisticsEventLoggerProvider.kt
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.stats
+
+import com.intellij.internal.statistic.eventLog.StatisticsEventLogger
+import com.intellij.internal.statistic.eventLog.StatisticsEventLoggerProvider
+import com.intellij.internal.statistic.utils.StatisticsUploadAssistant
+import com.intellij.openapi.application.ApplicationManager
+
+@Suppress("UnstableApiUsage")
+class AndroidStudioStatisticsEventLoggerProvider : StatisticsEventLoggerProvider("FUS", 1) {
+  override val logger: StatisticsEventLogger by lazy { createLogger() }
+
+  override fun isRecordEnabled(): Boolean {
+    // This logic is needed to ensure we initialize the usage tracker and
+    // IJ settings for data collection interaction properly.
+    return !ApplicationManager.getApplication().isHeadlessEnvironment &&
+           StatisticsUploadAssistant.isCollectAllowed()
+  }
+
+  override fun isSendEnabled(): Boolean {
+    return isRecordEnabled() && StatisticsUploadAssistant.isSendAllowed()
+  }
+
+  private fun createLogger(): StatisticsEventLogger {
+    if (!isRecordEnabled()) {
+      return super.logger
+    }
+    return AndroidStudioEventLogger
+  }
+}
