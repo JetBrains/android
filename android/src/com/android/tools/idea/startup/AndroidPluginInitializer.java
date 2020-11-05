2444304943ad46a626e29d45ce3f765e15fa8f1e
Sergey Prigogin <sprigogin@google.com>
2020-11-05 12:39:51 -0800

    Move AndroidStudioLoadListener to its own file and rename it to AndroidPluginLoadListener
    Test: existing
    Bug: 172582232
    Change-Id: I8cb265daa09df5b563fb25ff503ff8fb72711631
    
    GitOrigin-RevId: f64f4e15852a212ea0e8b46588a77e72b052e5f0

---
+++ android/src/com/android/tools/idea/startup/AndroidPluginInitializer.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.startup;
+
+import com.android.tools.idea.progress.StudioProgressManagerAdapter;
+import com.intellij.analytics.AndroidStudioAnalytics;
+import com.intellij.ide.ApplicationLoadListener;
+import com.intellij.openapi.application.Application;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Initialization code common between Android Studio and Android plugin in IntelliJ.
+ */
+public class AndroidPluginInitializer implements ApplicationLoadListener {
+  @Override
+  public void beforeApplicationLoaded(@NotNull Application application, @NotNull String configPath) {
+    AndroidStudioAnalytics.initialize(new AndroidStudioAnalyticsImpl());
+    StudioProgressManagerAdapter.initialize();
+  }
+}
