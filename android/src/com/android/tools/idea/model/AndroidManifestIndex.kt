399304d66ddf86a7993137c8a365779d2bd103a9
Sergey Prigogin <sprigogin@google.com>
2020-11-10 18:14:12 -0800

    Use FileBasedIndex.getFileData instead of getAllKeys/processValues
    The main difference between getFileData and getAllKeys is that with getFileData FileBasedIndexImpl.ensureUpToDate is called with the restrictedFile parameter pointing to AndroidManifest.xml, while with getAllKeys the restrictedFile parameter is null. This means that getFileData forces indexing of only the manifest file while getAllKeys forced indexing of all out of date files.
    
    Test: existing
    Bug: 166625311
    Bug: 166334891
    Change-Id: I41b118bb9894c3dc2fe0ca0ac0c430129f41b70b
    
    GitOrigin-RevId: b6afbfb713f4f3d4e4e4e88e357a72a5f8a0b4e1

--- android/src/com/android/tools/idea/model/AndroidManifestIndex.kt
+++ android/src/com/android/tools/idea/model/AndroidManifestIndex.kt
@@ -211,20 +211,9 @@ class AndroidManifestIndex : FileBasedIndexExtension<String, AndroidManifestRawT
     @JvmStatic
     private fun doGetDataForManifestFile(project: Project, manifestFile: VirtualFile): AndroidManifestRawText? {
       ProgressManager.checkCanceled()
-      val index = FileBasedIndex.getInstance()
-      val scope = GlobalSearchScope.fileScope(project, manifestFile)
-      val values = mutableListOf<AndroidManifestRawText>()
-
-      // TODO (b/166625311) use FileBasedIndex#getFileData
-      for (key in index.getAllKeys(NAME, project)) {
-        index.processValues(NAME, key, manifestFile, { _, value ->
-          values.add(value)
-          true
-        }, scope)
-      }
-      // It's guaranteed that there's at most one entry: <package name, android manifest raw text>.
-      check(values.size <= 1)
-      return values.firstOrNull()
+      val data: MutableMap<String, AndroidManifestRawText> = FileBasedIndex.getInstance().getFileData(NAME, manifestFile, project)
+      check(data.values.size <= 1)
+      return data.values.firstOrNull()
     }
   }
 
