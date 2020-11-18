99f03ef2c989a7751d0ddf934572bc751a149fb1
Matthew Gharrity <gharrma@google.com>
2020-11-18 14:41:55 -0500

    Fix android-kotlin parcelize tests
    * Update test dependencies to reflect the new location of Parcelize.
    
    * Update isParcelize() to recognize both the old and the new fq names.
    
    Fixes: 173568465
    Test: fixes android-kotlin tests
    Change-Id: I004582a495b8b2cc1a90a8365e0eef376a7061b8

--- android-kotlin/idea-android/tests/org/jetbrains/kotlin/android/intention/AbstractAndroidIntentionTest.kt
+++ android-kotlin/idea-android/tests/org/jetbrains/kotlin/android/intention/AbstractAndroidIntentionTest.kt
@@ -40,7 +40,7 @@ abstract class AbstractAndroidIntentionTest : KotlinAndroidTestCase() {
         try {
             val kotlinPlugin = TestUtils.getWorkspaceFile("prebuilts/tools/common/kotlin-plugin/Kotlin")
             val compilerLib = "$kotlinPlugin/kotlinc/lib"
-            ConfigLibraryUtil.addLibrary(myModule, "androidExtensionsRuntime", compilerLib, arrayOf("android-extensions-runtime.jar"))
+            ConfigLibraryUtil.addLibrary(myModule, "parcelizeCompiler", compilerLib, arrayOf("parcelize-compiler.jar"))
             ConfigLibraryUtil.addLibrary(myModule, "kotlinStdlib", compilerLib, arrayOf("kotlin-stdlib.jar"))
 
             if (withRuntime) {
@@ -79,7 +79,7 @@ abstract class AbstractAndroidIntentionTest : KotlinAndroidTestCase() {
             }
         }
         finally {
-            ConfigLibraryUtil.removeLibrary(myModule, "androidExtensionsRuntime")
+            ConfigLibraryUtil.removeLibrary(myModule, "parcelizeCompiler")
             ConfigLibraryUtil.removeLibrary(myModule, "kotlinStdlib")
             if (withRuntime) {
                 ConfigLibraryUtil.unConfigureKotlinRuntime(myFixture.module)
