5f14a03dec5e5ce69a0207a31b9cd22137bcee98
Jens Ole Lauridsen <jlauridsen@google.com>
2020-11-23 13:29:34 -0800

    Treat layout_constraintDimensionRatio as a string
    This was accidentally mapped to a float for properties panel.
    
    Fixes: 146702651
    Test: Unit test updated
    Change-Id: Ibd7e09e4189b6d755846454da5ae61d3492b65a6
    
    GitOrigin-RevId: c0fe8d25051f2910c153f466441905d6b6682488

--- designer/testSrc/com/android/tools/idea/uibuilder/property2/support/TypeResolverSdkTest.kt
+++ designer/testSrc/com/android/tools/idea/uibuilder/property2/support/TypeResolverSdkTest.kt
@@ -83,7 +83,7 @@ class TypeResolverSdkTest {
     val report = Report(ANDROID_VIEWS_HEADER)
     psiPackage.classes.filter { it.isInheritor(psiViewClass, true) }.forEach { checkViewAttributes(it.name!!, report) }
     psiPackage.classes.filter { it.isInheritor(psiViewGroupClass, true) }.forEach { checkViewLayoutAttributes(it.name!!, report) }
-    report.dumpReport()
+    report.dumpReport(report.totalErrors > 0)
     assertThat(report.totalErrors).named(TOTAL_ERROR_MESSAGE).isEqualTo(0)
   }
 
@@ -94,7 +94,7 @@ class TypeResolverSdkTest {
     val psiPackage = psiFacade.findPackage(PREFERENCE_PACKAGE)!!
     val report = Report(ANDROID_PREFERENCES_HEADER)
     psiPackage.classes.filter { it.isInheritor(psiPreferenceClass, true) }.forEach { checkViewAttributes(it.name!!, report) }
-    report.dumpReport()
+    report.dumpReport(report.totalErrors > 0)
     assertThat(report.totalErrors).named(TOTAL_ERROR_MESSAGE).isEqualTo(0)
   }
 
@@ -108,7 +108,7 @@ class TypeResolverSdkTest {
     val report = Report(APPCOMPAT_VIEWS_HEADER)
     psiPackage.classes.filter { it.isInheritor(psiViewClass, true) }.forEach { checkViewAttributes(it.qualifiedName!!, report) }
     psiPackage.classes.filter { it.isInheritor(psiViewGroupClass, true) }.forEach { checkViewLayoutAttributes(it.qualifiedName!!, report) }
-    report.dumpReport()
+    report.dumpReport(report.totalErrors > 0)
     assertThat(report.totalErrors).named(TOTAL_ERROR_MESSAGE).isEqualTo(0)
   }
 
@@ -122,7 +122,7 @@ class TypeResolverSdkTest {
     val report = Report(CONSTRAINT_LAYOUT_HEADER)
     psiPackage.classes.filter { it.isInheritor(psiViewClass, true) }.forEach { checkViewAttributes(it.qualifiedName!!, report) }
     psiPackage.classes.filter { it.isInheritor(psiViewGroupClass, true) }.forEach { checkViewLayoutAttributes(it.qualifiedName!!, report) }
-    report.dumpReport()
+    report.dumpReport(report.totalErrors > 0)
     assertThat(report.totalErrors).named(TOTAL_ERROR_MESSAGE).isEqualTo(0)
   }
 
@@ -136,7 +136,7 @@ class TypeResolverSdkTest {
     val report = Report(DESIGN_HEADER)
     psiPackage.classes.filter { it.isInheritor(psiViewClass, true) }.forEach { checkViewAttributes(it.qualifiedName!!, report) }
     psiPackage.classes.filter { it.isInheritor(psiViewGroupClass, true) }.forEach { checkViewLayoutAttributes(it.qualifiedName!!, report) }
-    report.dumpReport()
+    report.dumpReport(report.totalErrors > 0)
     assertThat(report.totalErrors).named(TOTAL_ERROR_MESSAGE).isEqualTo(0)
   }
 
@@ -153,7 +153,7 @@ class TypeResolverSdkTest {
       folder.classes.filter { it.isInheritor(psiViewClass, true) }.forEach { checkViewAttributes(it.qualifiedName!!, report) }
       folder.classes.filter { it.isInheritor(psiViewGroupClass, true) }.forEach { checkViewLayoutAttributes(it.qualifiedName!!, report) }
     }
-    report.dumpReport()
+    report.dumpReport(report.totalErrors > 0)
     assertThat(report.totalErrors).named(TOTAL_ERROR_MESSAGE).isEqualTo(0)
   }
 
@@ -171,7 +171,7 @@ class TypeResolverSdkTest {
     psiPackage.classes.filter { it.isInheritor(psiViewGroupClass, true) }.forEach {
       checkViewLayoutAttributes(it.qualifiedName!!, report)
     }
-    report.dumpReport()
+    report.dumpReport(report.totalErrors > 0)
     assertThat(report.totalErrors).named(TOTAL_ERROR_MESSAGE).isEqualTo(0)
   }
 
@@ -248,8 +248,8 @@ class TypeResolverSdkTest {
       logError(mismatch.tag)
     }
 
-    fun dumpReport() {
-      if (!DEBUG_DUMP) {
+    fun dumpReport(hasErrors: Boolean) {
+      if (!DEBUG_DUMP && !hasErrors) {
         return
       }
       System.err.println("\n==============================================================>")
