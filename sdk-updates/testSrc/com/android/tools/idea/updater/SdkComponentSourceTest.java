3bbdd4c77b993d20cb34b7c9781d46b0729b3d51
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2020-11-25 23:31:27 +0100

    [platform] dropping obsolete non-thread-safe code (IDEA-254409)
    GitOrigin-RevId: 6d6057c11fca7efb4c4fdec82deda84e815e50c0

--- sdk-updates/testSrc/com/android/tools/idea/updater/SdkComponentSourceTest.java
+++ sdk-updates/testSrc/com/android/tools/idea/updater/SdkComponentSourceTest.java
@@ -359,16 +359,6 @@ public class SdkComponentSourceTest extends UsefulTestCase {
       .maskExtensions(ExternalComponentSource.EP_NAME, Collections.singletonList(myTestComponentSource), myDisposable);
     UpdateSettings settings = new UpdateSettings() {
       @Override
-      public List<String> getKnownExternalUpdateSources() {
-        return getEnabledExternalUpdateSources();
-      }
-
-      @Override
-      public List<String> getEnabledExternalUpdateSources() {
-        return ImmutableList.of(myTestComponentSource.getName());
-      }
-
-      @Override
       @NotNull
       public List<String> getIgnoredBuildNumbers() {
         return ImmutableList.of(id.get());
@@ -391,17 +381,7 @@ public class SdkComponentSourceTest extends UsefulTestCase {
       .maskExtensions(ExternalComponentSource.EP_NAME, Collections.singletonList(myTestComponentSource), myDisposable);
 
     ProgressIndicator progress = new StudioProgressIndicatorAdapter(new FakeProgressIndicator(), null);
-    UpdateSettings settings = new UpdateSettings() {
-      @Override
-      public List<String> getKnownExternalUpdateSources() {
-        return getEnabledExternalUpdateSources();
-      }
-
-      @Override
-      public List<String> getEnabledExternalUpdateSources() {
-        return ImmutableList.of(myTestComponentSource.getName());
-      }
-    };
+    UpdateSettings settings = new UpdateSettings();
 
     Collection<ExternalUpdate> updates = UpdateChecker.INSTANCE.checkExternalUpdates(true, settings, progress);
     assertEquals(1, updates.size());
@@ -424,17 +404,7 @@ public class SdkComponentSourceTest extends UsefulTestCase {
       .maskExtensions(ExternalComponentSource.EP_NAME, Collections.singletonList(myTestComponentSource), myDisposable);
 
     ProgressIndicator progress = new StudioProgressIndicatorAdapter(new FakeProgressIndicator(), null);
-    UpdateSettings settings = new UpdateSettings() {
-      @Override
-      public List<String> getKnownExternalUpdateSources() {
-        return getEnabledExternalUpdateSources();
-      }
-
-      @Override
-      public List<String> getEnabledExternalUpdateSources() {
-        return ImmutableList.of(myTestComponentSource.getName());
-      }
-    };
+    UpdateSettings settings = new UpdateSettings();
 
     Collection<ExternalUpdate> updates = UpdateChecker.INSTANCE.checkExternalUpdates(true, settings, progress);
     assertEquals(1, updates.size());
