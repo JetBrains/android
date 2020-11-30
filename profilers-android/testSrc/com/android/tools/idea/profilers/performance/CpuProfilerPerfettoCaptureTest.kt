3fa02ae66fdc6d0cdf8d261a1831dcd40cc22898
Bruno Buss <buss@google.com>
2020-11-30 16:44:34 +0000

    Add 60s and 120s Perfetto benchmarks
    Adds 60 and 120 seconds benchmarks tests to Perfetto (TPD-only) to track
    how our backend scales with bigger traces than the 10s one we have been
    using.
    
    Bug: n/a
    Test: n/a
    Change-Id: I8c8631759a1d674ab2beed6fd772ea47732e1cbf
    
    GitOrigin-RevId: f093ecfb4badcc44fe21b0b4f923354161fc4065

--- profilers-android/testSrc/com/android/tools/idea/profilers/performance/CpuProfilerPerfettoCaptureTest.kt
+++ profilers-android/testSrc/com/android/tools/idea/profilers/performance/CpuProfilerPerfettoCaptureTest.kt
@@ -44,9 +44,30 @@ class CpuProfilerPerfettoCaptureTest : CpuProfilerMemoryLoadTestBase() {
   }
 
   @Test
-  fun measureMemoryOfImportPerfettoWithTPD() {
+  fun measureMemoryOfImportPerfettoWithTPD_10s() {
     myIdeServices.enableUseTraceProcessor(true)
-    loadCaptureAndReport("Perfetto-TPD-10-sec", getTraceFile("performance/perfetto_10s_tanks.trace"))
+    loadCaptureAndReport(
+      "Perfetto-TPD-10-sec",
+      getTraceFile("performance/perfetto_10s_tanks.trace"),
+      "system_server", 1193)
+  }
+
+  @Test
+  fun measureMemoryOfImportPerfettoWithTPD_60s() {
+    myIdeServices.enableUseTraceProcessor(true)
+    loadCaptureAndReport(
+      "Perfetto-TPD-60-sec",
+      getTraceFile("performance/perfetto_60s_tanks.trace"),
+      "com.google.android.tanks", 9796)
+  }
+
+  @Test
+  fun measureMemoryOfImportPerfettoWithTPD_120s() {
+    myIdeServices.enableUseTraceProcessor(true)
+    loadCaptureAndReport(
+      "Perfetto-TPD-120-sec",
+      getTraceFile("performance/perfetto_120s_tanks.trace"),
+      "com.google.android.tanks", 10679)
   }
 }
 
