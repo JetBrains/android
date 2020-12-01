5935a06df713f2a6c54508b410041a1053d7286f
Peter Xu <peterx@google.com>
2020-12-01 15:41:45 -0800

    Use http to resolve inspectors for gradle projects
    With the introduction of http artifact resolver, there is really no need
    for a gradle resolver. The caching that was delegated to gradle is now
    handled by the IDE.
    
    With the deletion of gradle resolver, we no longer need to maintain two
    different resolver implementations between Studio and ASwB.
    
    Test: covered by existing tests.
    Bug: 165319681
    Change-Id: Ib69843a6cedbbb6d75da5c6dae2fcedb9e4a6f12

--- app-inspection/ide/src/com/android/tools/idea/appinspection/ide/resolver/ArtifactResolverFactory.kt
+++ app-inspection/ide/src/com/android/tools/idea/appinspection/ide/resolver/ArtifactResolverFactory.kt
@@ -16,7 +16,6 @@
 package com.android.tools.idea.appinspection.ide.resolver
 
 import com.android.tools.idea.appinspection.ide.resolver.blaze.BlazeArtifactResolver
-import com.android.tools.idea.appinspection.ide.resolver.gradle.GradleArtifactResolver
 import com.android.tools.idea.appinspection.ide.resolver.http.HttpArtifactResolver
 import com.android.tools.idea.appinspection.inspector.api.io.FileService
 import com.android.tools.idea.appinspection.inspector.ide.io.IdeFileService
@@ -37,16 +36,12 @@ class ArtifactResolverFactory @NonInjectable @TestOnly constructor(
   private val jarPaths = AppInspectorJarPaths(fileService)
 
   override fun getArtifactResolver(project: Project): ArtifactResolver {
-    return when {
-      GradleProjectInfo.getInstance(project).isBuildWithGradle -> {
-        GradleArtifactResolver(jarPaths)
-      }
-      AndroidProjectInfo.getInstance(project).isApkProject -> {
-        HttpArtifactResolver(fileService, jarPaths)
-      }
-      else -> {
-        BlazeArtifactResolver(fileService, jarPaths)
-      }
+    return if (GradleProjectInfo.getInstance(project).isBuildWithGradle
+               || AndroidProjectInfo.getInstance(project).isApkProject) {
+      HttpArtifactResolver(fileService, jarPaths)
+    }
+    else {
+      BlazeArtifactResolver(fileService, jarPaths)
     }
   }
 }
