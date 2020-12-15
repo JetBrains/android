a4b207cc0eb6b4021c440a8fe81da699da0333a9
Sergey Prigogin <sprigogin@google.com>
2020-12-15 15:00:36 -0800

    Check error handling in the KeyValueFileUtils.updateKeyValueFile method
    Test: KeyValueFileUtilsTest
    Bug: N/A
    Change-Id: I388b2fa2557ada77c83019a7dafccbe88ab44068

--- emulator/testSrc/com/android/tools/idea/emulator/KeyValueFileUtilsTest.kt
+++ emulator/testSrc/com/android/tools/idea/emulator/KeyValueFileUtilsTest.kt
@@ -15,22 +15,30 @@
  */
 package com.android.tools.idea.emulator
 
+import com.android.testutils.file.DelegatingFileSystemProvider
 import com.android.testutils.file.createInMemoryFileSystem
 import com.android.testutils.file.getExistingFiles
 import com.android.testutils.file.someRoot
 import com.android.testutils.truth.PathSubject.assertThat
 import com.google.common.truth.Truth.assertThat
 import org.junit.Test
+import java.io.IOException
+import java.io.OutputStream
+import java.nio.file.FileSystem
 import java.nio.file.Files
+import java.nio.file.OpenOption
+import java.nio.file.Path
+import kotlin.test.fail
 
 /**
  * Test for functions defined in `KeyValueFileUtils.kt`.
  */
 class KeyValueFileUtilsTest {
+  private var exception: IOException? = null
 
   @Test
   fun testUpdateKeyValueFile() {
-    val fileSystem = createInMemoryFileSystem()
+    val fileSystem = MockFileSystemProvider(createInMemoryFileSystem()).fileSystem
     val file = fileSystem.someRoot.resolve("test.ini")
     Files.write(file, listOf("AvdId = Pixel_4_XL_API_30",
                              "PlayStore.enabled = false",
@@ -38,6 +46,7 @@ class KeyValueFileUtilsTest {
                              "fastboot.chosenSnapshotFile = snapshot42",
                              "fastboot.forceChosenSnapshotBoot = yes",
                              "hw.sensors.orientation = yes"))
+    // Check normal update.
     updateKeyValueFile(file, mapOf("PlayStore.enabled" to "true",
                                    "fastboot.chosenSnapshotFile" to null,
                                    "fastboot.forceChosenSnapshotBoot" to "no",
@@ -49,5 +58,33 @@ class KeyValueFileUtilsTest {
                                  "fastboot.forceFastBoot=yes",
                                  "hw.sensors.orientation=yes")
     assertThat(fileSystem.getExistingFiles()).isEqualTo(arrayOf("$file")) // No extra files left behind.
+
+    // Check with I/O errors.
+    exception = IOException()
+    try {
+      updateKeyValueFile(file, mapOf("PlayStore.enabled" to "false"))
+      fail("Expected an exception to be logged")
+    }
+    catch (e: AssertionError) {
+      assertThat(e.message).isEqualTo("Error writing $file")
+    }
+    assertThat(fileSystem.getExistingFiles()).isEqualTo(arrayOf("$file")) // No extra files left behind.
+
+    exception = IOException("something horrible happened")
+    try {
+      updateKeyValueFile(file, mapOf("PlayStore.enabled" to "false"))
+      fail("Expected an exception to be logged")
+    }
+    catch (e: AssertionError) {
+      assertThat(e.message).isEqualTo("Error writing $file - something horrible happened")
+    }
+    assertThat(fileSystem.getExistingFiles()).isEqualTo(arrayOf("$file")) // No extra files left behind.
+  }
+
+  private inner class MockFileSystemProvider(fileSystem: FileSystem) : DelegatingFileSystemProvider(fileSystem) {
+    override fun newOutputStream(path: Path, vararg options: OpenOption): OutputStream {
+      exception?.let { throw it }
+      return super.newOutputStream(path, *options)
+    }
   }
 }
