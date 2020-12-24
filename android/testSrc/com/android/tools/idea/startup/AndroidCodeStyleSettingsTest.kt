ad81fec6fe5c396a107febc3461199a7e4113732
Raluca Sauciuc <ralucas@google.com>
2020-12-23 20:52:16 -0800

    Centralize Android code style defaults
    Bug: 175998141
    Test: AndroidCodeStyleSettingsTest
    Change-Id: I01bc84524bd0b67f593b689bd29d2ac112d72feb

--- android/testSrc/com/android/tools/idea/startup/AndroidCodeStyleSettingsTest.kt
+++ android/testSrc/com/android/tools/idea/startup/AndroidCodeStyleSettingsTest.kt
@@ -29,7 +29,6 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
 
-@org.junit.Ignore("b/175998141")
 @RunWith(JUnit4::class)
 class AndroidCodeStyleSettingsTest {
   @Rule
