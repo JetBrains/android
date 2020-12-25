825d2759dc4a58799c8d55d848455386b592231b
Andrei Kuznetsov <andrei.kuznetsov@jetbrains.com>
2020-12-26 02:21:05 +0300

    Android Tests: add more logging (investigating flaky test)
    Current hypothesis: `ApplicationTest.java` does not have filetype assigned sometimes => stub indexes are not built for this file => it cannot be found in FQN cache
    
    GitOrigin-RevId: 41c9af3b57480f25aef5e02f4715da18f682dbbc

--- android/testSrc/com/android/tools/idea/testartifacts/instrumented/AndroidTestRunConfigurationTest.java
+++ android/testSrc/com/android/tools/idea/testartifacts/instrumented/AndroidTestRunConfigurationTest.java
@@ -25,6 +25,14 @@ import com.android.tools.idea.gradle.project.model.AndroidModuleModel;
 import com.android.tools.idea.gradle.project.sync.internal.ProjectDumper;
 import com.android.tools.idea.run.ValidationError;
 import com.android.tools.idea.testing.AndroidGradleTestCase;
+import com.intellij.openapi.project.DumbService;
+import com.intellij.openapi.vfs.VirtualFileManager;
+import com.intellij.openapi.vfs.VirtualFileSystem;
+import com.intellij.psi.PsiClass;
+import com.intellij.psi.impl.java.stubs.index.JavaFullClassNameIndex;
+import com.intellij.psi.search.GlobalSearchScope;
+import com.intellij.psi.stubs.StubIndex;
+import java.nio.file.Paths;
 import java.util.List;
 
 /**
@@ -38,6 +46,10 @@ public class AndroidTestRunConfigurationTest extends AndroidGradleTestCase {
    * @throws Exception
    */
   public void testCheckConfigurationNoErrors() throws Exception {
+    System.out.println("isDumb: " + DumbService.isDumb(getProject()));
+    System.out.println("JavaFullClassNameIndex: " + JavaFullClassNameIndex.getInstance().get(TEST_APP_CLASS_NAME.hashCode(), getProject(), GlobalSearchScope
+      .projectScope(getProject())));
+
     loadProject(DYNAMIC_APP);
 try {
     AndroidTestRunConfiguration androidTestRunConfiguration =
@@ -52,9 +64,25 @@ try {
     List<ValidationError> errors = androidTestRunConfiguration.checkConfiguration(myAndroidFacet);
     assertThat(errors).isEmpty();
 } finally {
-    ProjectDumper dumper = new ProjectDumper();
-    dumper.dump(getProject());
-    System.out.println(dumper);
+    System.out.println("isDumb: " + DumbService.isDumb(getProject()));
+    System.out.println("JavaFullClassNameIndex: " + JavaFullClassNameIndex.getInstance().get(TEST_APP_CLASS_NAME.hashCode(), getProject(), GlobalSearchScope
+      .projectScope(getProject())));
+    System.out.println("JavaFullClassNameIndex: " + JavaFullClassNameIndex.getInstance().get(TEST_APP_CLASS_NAME.hashCode(), getProject(), GlobalSearchScope
+      .projectScope(getProject())));
+    System.out.println("JavaFullClassNameIndex.containsKey: " + JavaFullClassNameIndex.getInstance().getAllKeys(getProject()).contains(TEST_APP_CLASS_NAME.hashCode()));
+
+    System.out.println("findFileByNioPath: " + VirtualFileManager.getInstance().findFileByNioPath(Paths.get(
+      getProject().getBasePath() + "/app/src/androidTest/java/google/simpleapplication/ApplicationTest.java")
+    ));
+    System.out.println("refreshAndFindFileByNioPath: " + VirtualFileManager.getInstance().refreshAndFindFileByNioPath(Paths.get(
+      getProject().getBasePath() + "/app/src/androidTest/java/google/simpleapplication/ApplicationTest.java")
+    ));
+    System.out.println("findFileByNioPath.filetype: " + VirtualFileManager.getInstance().findFileByNioPath(Paths.get(
+      getProject().getBasePath() + "/app/src/androidTest/java/google/simpleapplication/ApplicationTest.java")
+    ).getFileType());
+    //ProjectDumper dumper = new ProjectDumper();
+    //dumper.dump(getProject());
+    //System.out.println(dumper);
 }
   }
 
