0aa028d52164886098e3473e9dcb807445506c50
Karima Ichou <karimai@google.com>
2021-01-14 16:21:37 +0000

    Do not create test tasks for KMP modules.
    test tasks creation should be delegated to
    KotlinMPPGradleTestTasksProvider instead.
    
    Bug: 176999821
    Test: verified manually
    Change-Id: If89aa954cdabbca9730299aa519237cfec387df3
    
    GitOrigin-RevId: 8799f668bf480afdd61314a0fb7b86fe2776f09d

--- android/src/com/android/tools/idea/gradle/run/AndroidGradleTestTasksProvider.java
+++ android/src/com/android/tools/idea/gradle/run/AndroidGradleTestTasksProvider.java
@@ -8,6 +8,8 @@ import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.openapi.vfs.VirtualFile;
 import java.util.Objects;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.kotlin.idea.facet.KotlinFacet;
+import org.jetbrains.kotlin.idea.facet.KotlinFacetConfiguration;
 import org.jetbrains.plugins.gradle.execution.test.runner.GradleTestTasksProvider;
 import com.android.utils.StringHelper;
 
@@ -20,11 +22,19 @@ public class AndroidGradleTestTasksProvider implements GradleTestTasksProvider {
   @Override
   public @NotNull List<String> getTasks(@NotNull Module module) {
     // TODO(b/174357889): When running 'all Tests' in module, it will run unit tests only.
-    if (GradleFacet.isAppliedTo(module)) {
-      AndroidModuleModel androidModel = AndroidModuleModel.get(module);
-      if (androidModel != null) {
-        return getTasksFromAndroidModule(module, androidModel);
-      }
+    AndroidModuleModel androidModel = getAndroidModelIfPossible(module);
+    if (androidModel == null) return Collections.emptyList();
+    return getTasksFromAndroidModule(module, androidModel);
+  }
+
+  @NotNull
+  @Override
+  public List<String> getTasks(@NotNull Module module, @NotNull VirtualFile source) {
+    AndroidModuleModel androidModel = getAndroidModelIfPossible(module);
+    if (androidModel == null) return Collections.emptyList();
+    // Filter out non-unit tests artifacts, because this task provider is only for unit tests artifacts.
+    if (Objects.requireNonNull(GradleTestArtifactSearchScopes.getInstance(module)).isUnitTestSource(source)) {
+      return getTasksFromAndroidModule(module, androidModel);
     }
     return Collections.emptyList();
   }
@@ -40,17 +50,14 @@ public class AndroidGradleTestTasksProvider implements GradleTestTasksProvider {
     return Arrays.asList(StringHelper.appendCapitalized(taskNamePrefix + "clean", testTask), taskNamePrefix + testTask);
   }
 
-  @NotNull
-  @Override
-  public List<String> getTasks(@NotNull Module module, @NotNull VirtualFile source) {
+  private AndroidModuleModel getAndroidModelIfPossible(@NotNull Module module) {
     if (GradleFacet.isAppliedTo(module)) {
       AndroidModuleModel androidModel = AndroidModuleModel.get(module);
-      if (androidModel != null) {
-        if (Objects.requireNonNull(GradleTestArtifactSearchScopes.getInstance(module)).isUnitTestSource(source)) {
-          return getTasks(module);
-        }
-      }
+      // If the module is a KMP, we shouldn't provide the test tasks because kotlin has a specific tasks provider: KotlinMPPGradleTestTasksProvider.
+      KotlinFacet kotlinFacet = KotlinFacet.Companion.get(module);
+      boolean isKmpModule = kotlinFacet != null && kotlinFacet.getConfiguration().getSettings().getMppVersion() != null;
+      return (androidModel != null && !isKmpModule) ? androidModel : null;
     }
-    return Collections.emptyList();
+    return null;
   }
 }
