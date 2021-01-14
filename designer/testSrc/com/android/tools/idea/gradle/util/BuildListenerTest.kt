62d1c1f1d10c1b1d9ce9fb646ceb06d393e8faad
Diego Perez <diegoperez@google.com>
2021-01-14 13:00:53 +0000

    Migrate BuildListenerTest
    Remove the use of mocks and the deprecated testing infra.
    
    Bug: N/A
    Test: BuildListenerTest
    Change-Id: Ifb541b6d66780855441739620bc93d862a5b632e
    
    GitOrigin-RevId: 72d17f0a0563f3660b0e931f4ccc7f95ed7856ab

--- designer/testSrc/com/android/tools/idea/gradle/util/BuildListenerTest.kt
+++ designer/testSrc/com/android/tools/idea/gradle/util/BuildListenerTest.kt
@@ -3,80 +3,119 @@ package com.android.tools.idea.gradle.util
 import com.android.tools.idea.gradle.project.build.BuildContext
 import com.android.tools.idea.gradle.project.build.BuildStatus
 import com.android.tools.idea.gradle.project.build.GradleBuildState
+import com.android.tools.idea.testing.AndroidProjectRule
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.Disposer
 import com.intellij.testFramework.PlatformTestCase
+import com.intellij.util.ui.UIUtil
+import junit.framework.Assert.assertEquals
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
 import org.mockito.Mock
 import org.mockito.Mockito
 import org.mockito.MockitoAnnotations
+import java.lang.StringBuilder
 
-class BuildListenerTest : PlatformTestCase() {
+private class TestBuildListener: BuildListener {
+  private val log = StringBuilder()
+
+  override fun buildStarted() {
+    log.append("Build Started\n")
+  }
+
+  override fun buildSucceeded() {
+    log.append("Build Succeeded\n")
+  }
+
+  override fun buildFailed() {
+    log.append("Build Failed\n")
+  }
+
+  fun getLog(): String = log.toString().trimEnd()
+}
+
+class BuildListenerTest {
+  @get:Rule
+  val projectRule = AndroidProjectRule.inMemory()
+  private val project: Project
+    get() = projectRule.project
   private lateinit var buildContext: BuildContext
   private lateinit var buildState: GradleBuildState
 
-  @Mock
-  private lateinit var buildListener: BuildListener
+  private val buildListener: TestBuildListener = TestBuildListener()
 
-  override fun setUp() {
-    super.setUp()
-    MockitoAnnotations.initMocks(this)
-
-    setupBuildListener(project, buildListener, project)
+  @Before
+  fun setUp() {
+    setupBuildListener(project, buildListener, projectRule.fixture.testRootDisposable)
     buildState = GradleBuildState.getInstance(project)
   }
 
-  override fun tearDown() {
-    try {
-      buildState.clear()
-    }
-    finally {
-      super.tearDown()
-    }
+  @After
+  fun tearDown() {
+    buildState.clear()
   }
 
   private fun createContext(buildMode: BuildMode): BuildContext {
     return BuildContext(project, listOf("task1", "task2"), buildMode)
   }
 
+  private fun processEvents() = UIUtil.invokeAndWaitIfNeeded(Runnable {
+    UIUtil.dispatchAllInvocationEvents()
+  })
+
+  @Test
   fun testBuildSuccessful() {
     buildContext = createContext(BuildMode.ASSEMBLE)
 
     buildState.buildStarted(buildContext)
-
-    Mockito.verify(buildListener).buildStarted()
+    processEvents()
+    assertEquals("Build Started", buildListener.getLog())
 
     buildState.buildFinished(BuildStatus.SUCCESS)
-
-    Mockito.verify(buildListener).buildSucceeded()
+    processEvents()
+    assertEquals("""
+      Build Started
+      Build Succeeded
+    """.trimIndent(), buildListener.getLog())
   }
 
+  @Test
   fun testBuildFailed() {
     buildContext = createContext(BuildMode.ASSEMBLE)
 
     buildState.buildStarted(buildContext)
     buildState.buildFinished(BuildStatus.FAILED)
-
-    Mockito.verify(buildListener).buildFailed()
+    processEvents()
+    assertEquals("""
+      Build Started
+      Build Failed
+    """.trimIndent(), buildListener.getLog())
   }
 
+  @Test
   fun testBuildCancelled() {
     buildContext = createContext(BuildMode.ASSEMBLE)
 
     buildState.buildStarted(buildContext)
-
     buildState.buildFinished(BuildStatus.FAILED)
-
-    Mockito.verify(buildListener).buildFailed()
+    processEvents()
+    assertEquals("""
+      Build Started
+      Build Failed
+    """.trimIndent(), buildListener.getLog())
   }
 
+  @Test
   fun testCleanBuild() {
     buildContext = createContext(BuildMode.CLEAN)
-
     buildState.buildStarted(buildContext)
-
-    Mockito.verify(buildListener, Mockito.never()).buildStarted()
+    processEvents()
+    assertEquals("", buildListener.getLog())
 
     buildState.buildFinished(BuildStatus.SUCCESS)
-
-    Mockito.verify(buildListener, Mockito.never()).buildSucceeded()
-    Mockito.verify(buildListener).buildFailed()
+    processEvents()
+    assertEquals("Build Failed", buildListener.getLog())
   }
 }
