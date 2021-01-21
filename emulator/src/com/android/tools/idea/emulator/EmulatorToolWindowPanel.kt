4c1094e80865e8d4a4c5acd31793dd5212f5f0ae
Sergey Prigogin <sprigogin@google.com>
2021-01-20 17:30:17 -0800

    Add support for dragging and dropping of non-APK files
    Non-APK files are copied to the /sdcard/Download directory of the AVD.
    
    Test: EmulatorToolWindowPanelTest.testDnD
    Bug: 177860386
    Change-Id: Iefd6da432fef7131c27d7f887cdf2497acc214c5
    
    GitOrigin-RevId: 48cebdabb8bf53a21ceb58aefe4786d6523316c0

--- emulator/src/com/android/tools/idea/emulator/EmulatorToolWindowPanel.kt
+++ emulator/src/com/android/tools/idea/emulator/EmulatorToolWindowPanel.kt
@@ -15,35 +15,17 @@
  */
 package com.android.tools.idea.emulator
 
-import com.android.ddmlib.AndroidDebugBridge
-import com.android.ddmlib.IDevice
 import com.android.emulator.control.ExtendedControlsStatus
 import com.android.tools.adtui.ZOOMABLE_KEY
 import com.android.tools.adtui.common.primaryPanelBackground
 import com.android.tools.adtui.util.ActionToolbarUtil.makeToolbarNavigable
-import com.android.tools.deployer.AdbClient
-import com.android.tools.deployer.ApkInstaller
-import com.android.tools.deployer.InstallStatus
-import com.android.tools.idea.adb.AdbService
-import com.android.tools.idea.concurrency.addCallback
-import com.android.tools.idea.concurrency.transform
-import com.android.tools.idea.concurrency.transformNullable
 import com.android.tools.idea.emulator.EmulatorController.ConnectionState
 import com.android.tools.idea.emulator.actions.findManageSnapshotDialog
 import com.android.tools.idea.emulator.actions.showExtendedControls
 import com.android.tools.idea.emulator.actions.showManageSnapshotsDialog
 import com.android.tools.idea.flags.StudioFlags
-import com.android.tools.idea.log.LogWrapper
-import com.google.common.util.concurrent.Futures
-import com.google.common.util.concurrent.ListenableFuture
 import com.intellij.execution.runners.ExecutionUtil
-import com.intellij.ide.dnd.DnDDropHandler
-import com.intellij.ide.dnd.DnDEvent
-import com.intellij.ide.dnd.DnDSupport
-import com.intellij.ide.dnd.FileCopyPasteUtil.getFileListFromAttachedObject
-import com.intellij.ide.dnd.FileCopyPasteUtil.isFileListFlavorAvailable
 import com.intellij.ide.ui.customization.CustomActionsSchema
-import com.intellij.notification.NotificationType
 import com.intellij.openapi.actionSystem.ActionManager
 import com.intellij.openapi.actionSystem.ActionToolbar
 import com.intellij.openapi.actionSystem.DataProvider
@@ -53,11 +35,8 @@ import com.intellij.openapi.ui.DialogWrapper
 import com.intellij.ui.IdeBorderFactory
 import com.intellij.ui.JBColor
 import com.intellij.ui.SideBorder
-import com.intellij.util.concurrency.AppExecutorUtil.getAppExecutorService
-import com.intellij.util.concurrency.EdtExecutorService
 import com.intellij.util.ui.components.BorderLayoutPanel
 import icons.StudioIcons
-import org.jetbrains.android.sdk.AndroidSdkUtils
 import java.awt.BorderLayout
 import java.awt.EventQueue
 import javax.swing.JComponent
@@ -105,8 +84,6 @@ class EmulatorToolWindowPanel(
     background = primaryPanelBackground
 
     mainToolbar = createToolbar(EMULATOR_MAIN_TOOLBAR_ID, isToolbarHorizontal)
-
-    installDnD()
   }
 
   fun getPreferredFocusableComponent(): JComponent {
@@ -126,20 +103,6 @@ class EmulatorToolWindowPanel(
     }
   }
 
-  private fun installDnD() {
-    DnDSupport.createBuilder(this)
-      .enableAsNativeTarget()
-      .setTargetChecker { event ->
-        if (isFileListFlavorAvailable(event)) {
-          event.isDropPossible = true
-          return@setTargetChecker false
-        }
-        return@setTargetChecker true
-      }
-      .setDropHandler(ApkFileDropHandler())
-      .install()
-  }
-
   fun setDeviceFrameVisible(visible: Boolean) {
     emulatorPanel.setDeviceFrameVisible(visible)
   }
@@ -153,8 +116,10 @@ class EmulatorToolWindowPanel(
 
       addToolbar()
 
-      if (connected) {
-        emulatorView?.let { emulatorView ->
+      emulatorView?.let { emulatorView ->
+        installFileDropHandler(this, emulatorView, project)
+
+        if (connected) {
           if (uiState.manageSnapshotsDialogShown) {
             showManageSnapshotsDialog(emulatorView, project)
           }
@@ -207,61 +172,5 @@ class EmulatorToolWindowPanel(
     toolbar.layoutPolicy = ActionToolbar.AUTO_LAYOUT_POLICY
     makeToolbarNavigable(toolbar)
     return toolbar
-  }
-
-  private inner class ApkFileDropHandler : DnDDropHandler {
-
-    override fun drop(event: DnDEvent) {
-      val files = getFileListFromAttachedObject(event.attachedObject)
-      val fileNames = files.joinToString(", ") { it.name }
-      emulatorView?.showLongRunningOperationIndicator("Installing $fileNames")
-
-      val resultFuture: ListenableFuture<AdbClient.InstallResult?> = findDevice().transformNullable(getAppExecutorService()) { device ->
-        if (device == null) return@transformNullable null
-        val adbClient = AdbClient(device, LogWrapper(EmulatorToolWindowPanel::class.java))
-        val filePaths = files.asSequence().map { it.path }.toList()
-        return@transformNullable adbClient.install(filePaths, listOf("-t", "--user", "current", "--full", "--dont-kill"), true)
-      }
-      resultFuture.addCallback(
-          EdtExecutorService.getInstance(),
-          success = { installResult ->
-            if (installResult?.status == InstallStatus.OK) {
-              notifyOfSuccess("$fileNames installed")
-            }
-            else {
-              val message = if (installResult == null) {
-                "Unable to find the device to install the app"
-              }
-              else {
-                installResult.reason ?: ApkInstaller.message(installResult)
-              }
-              notifyOfError(message)
-            }
-            emulatorView?.hideLongRunningOperationIndicator()
-          },
-          failure = { throwable ->
-            val message = throwable?.message ?: "Installation failed"
-            notifyOfError(message)
-            emulatorView?.hideLongRunningOperationIndicator()
-          })
-    }
-
-    private fun notifyOfSuccess(message: String) {
-      EMULATOR_NOTIFICATION_GROUP.createNotification(message, NotificationType.INFORMATION).notify(project)
-    }
-
-    private fun notifyOfError(message: String) {
-      EMULATOR_NOTIFICATION_GROUP.createNotification(message, NotificationType.WARNING).notify(project)
-    }
-
-    private fun findDevice(): ListenableFuture<IDevice?> {
-      val serialNumber = "emulator-${emulator.emulatorId.serialPort}"
-      val adbFile = AndroidSdkUtils.getAdb(project) ?:
-                    return Futures.immediateFailedFuture(RuntimeException("Could not find adb executable"))
-      val bridgeFuture: ListenableFuture<AndroidDebugBridge> = AdbService.getInstance().getDebugBridge(adbFile)
-      return bridgeFuture.transform(getAppExecutorService()) { debugBridge ->
-        debugBridge.devices.find { it.isEmulator && it.serialNumber == serialNumber }
-      }
-    }
   }
 }
