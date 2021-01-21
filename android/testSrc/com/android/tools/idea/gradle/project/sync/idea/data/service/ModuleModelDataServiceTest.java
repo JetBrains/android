341db2026e21559d1bf5b406ca3bc07e49a85e94
Andrei Kuznetsov <andrei.kuznetsov@jetbrains.com>
2021-01-21 20:04:50 +0300

    IDEA-259235: android facets are lost if two android-gradle projects are linked
    If two or more gradle projects with android modules are linked to idea project, only one of them have android-related facts after sync completion.
    
    GitOrigin-RevId: 0e3d3b96c49da17fb043aae615c51789ddf6666a

---
+++ android/testSrc/com/android/tools/idea/gradle/project/sync/idea/data/service/ModuleModelDataServiceTest.java
@@ -0,0 +1,50 @@
+// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.android.tools.idea.gradle.project.sync.idea.data.service;
+
+import static com.android.tools.idea.testing.TestProjectPaths.SIMPLE_APPLICATION;
+
+import com.android.tools.idea.gradle.project.facet.gradle.GradleFacet;
+import com.android.tools.idea.gradle.project.facet.java.JavaFacet;
+import com.android.tools.idea.testing.AndroidGradleTestCase;
+import com.intellij.facet.ProjectFacetManager;
+import java.io.File;
+import java.util.List;
+import org.jetbrains.android.facet.AndroidFacet;
+import org.jetbrains.plugins.gradle.service.project.open.GradleProjectImportUtil;
+import org.jetbrains.plugins.gradle.util.GradleImportingTestUtil;
+
+public class ModuleModelDataServiceTest extends AndroidGradleTestCase {
+  public void testLinkedProjectsDoNotRemoveEachOtherFacets() throws Exception {
+    File linkedProject1 = new File(myFixture.getTempDirFixture().findOrCreateDir("linked1").getPath());
+    File linkedProject2 = new File(myFixture.getTempDirFixture().findOrCreateDir("linked2").getPath());
+
+    prepareProjectForImport(SIMPLE_APPLICATION, linkedProject1);
+    prepareProjectForImport(SIMPLE_APPLICATION, linkedProject2);
+
+    GradleImportingTestUtil.waitForProjectReload(
+      () -> {
+        // FIXME-ank: there is a defect in new workspace model which fails concurrent import of two linked gradle projects
+        GradleProjectImportUtil.linkAndRefreshGradleProject(linkedProject1.getAbsolutePath(), getProject());
+        //GradleProjectImportUtil.linkAndRefreshGradleProject(linkedProject2.getAbsolutePath(), getProject());
+        return null;
+      }
+    );
+
+    GradleImportingTestUtil.waitForProjectReload(
+      () -> {
+        //GradleProjectImportUtil.linkAndRefreshGradleProject(linkedProject1.getAbsolutePath(), getProject());
+        GradleProjectImportUtil.linkAndRefreshGradleProject(linkedProject2.getAbsolutePath(), getProject());
+        return null;
+      }
+    );
+
+    List<AndroidFacet> androidFacets = ProjectFacetManager.getInstance(getProject()).getFacets(AndroidFacet.ID);
+    assertEquals(2, androidFacets.size());
+
+    List<GradleFacet> androidGradleFacets = ProjectFacetManager.getInstance(getProject()).getFacets(GradleFacet.getFacetTypeId());
+    assertEquals(2, androidGradleFacets.size());
+
+    List<JavaFacet> androidJavaFacets = ProjectFacetManager.getInstance(getProject()).getFacets(JavaFacet.getFacetTypeId());
+    assertEquals(2, androidJavaFacets.size());
+  }
+}
