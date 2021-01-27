63c1de01e2e034dcf97ffb97fe3afc8bbd7ddfde
Matthew Gharrity <gharrma@google.com>
2021-01-26 20:57:04 -0500

    Use facet index to get Android facets
    Bug: 92730296
    Test: existing
    Change-Id: I29c941b1bad40e8ef216f6b6c2cfb589b1d89b05
    
    GitOrigin-RevId: 601f8fce48783e2dc56c43a73df6b14e054ece84

--- android/src/com/android/tools/idea/databinding/LayoutBindingEnabledFacetsProvider.kt
+++ android/src/com/android/tools/idea/databinding/LayoutBindingEnabledFacetsProvider.kt
@@ -18,6 +18,7 @@ package com.android.tools.idea.databinding
 import com.android.tools.idea.databinding.util.DataBindingUtil
 import com.android.tools.idea.databinding.util.getViewBindingEnabledTracker
 import com.android.tools.idea.databinding.util.isViewBindingEnabled
+import com.intellij.facet.ProjectFacetManager
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.module.ModuleManager
 import com.intellij.openapi.project.Project
@@ -48,13 +49,13 @@ class LayoutBindingEnabledFacetsProvider(val project: Project) : ModificationTra
   init {
     val cachedValuesManager = CachedValuesManager.getManager(project)
     val moduleManager = ModuleManager.getInstance(project)
+    val facetManager = ProjectFacetManager.getInstance(project)
     val dataBindingTracker = DataBindingUtil.getDataBindingEnabledTracker()
     val viewBindingTracker = project.getViewBindingEnabledTracker()
 
     allBindingEnabledModules = cachedValuesManager.createCachedValue(
       {
-        val facets = moduleManager.modules
-            .mapNotNull { module -> AndroidFacet.getInstance(module) }
+        val facets = facetManager.getFacets(AndroidFacet.ID)
             .filter { facet -> DataBindingUtil.isDataBindingEnabled(facet) || facet.isViewBindingEnabled() }
 
         CachedValueProvider.Result.create(facets, dataBindingTracker, viewBindingTracker, moduleManager)
