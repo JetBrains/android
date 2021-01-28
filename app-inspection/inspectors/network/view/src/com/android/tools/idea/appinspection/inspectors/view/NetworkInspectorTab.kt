d7766cba3a57a0f633791ac0ef91d7a759417422
Peter Xu <peterx@google.com>
2021-01-27 17:00:05 -0800

    Remove proto usage for now
    Because intellij is not building the proto at the moment. Something in
    the setup is wrong.
    
    Test: n/a
    Bug: n/a
    Change-Id: If94f139a1b2a40bd86e57ecbc71d1744b7f499e8
    
    GitOrigin-RevId: 5612e3d61b89d9a96f828b7034af616208c815ad

--- app-inspection/inspectors/network/view/src/com/android/tools/idea/appinspection/inspectors/view/NetworkInspectorTab.kt
+++ app-inspection/inspectors/network/view/src/com/android/tools/idea/appinspection/inspectors/view/NetworkInspectorTab.kt
@@ -16,17 +16,16 @@
 package com.android.tools.idea.appinspection.inspectors.view
 
 import com.android.tools.idea.appinspection.inspectors.network.model.NetworkInspectorClient
-import kotlinx.coroutines.runBlocking
 import javax.swing.JButton
 
 class NetworkInspectorTab(private val client: NetworkInspectorClient) {
   val component = JButton("Click here")
 
   init {
-    component.addActionListener {
-      runBlocking {
-        component.text = client.send("Hello World!")
-      }
-    }
+    //component.addActionListener {
+    //  runBlocking {
+    //    component.text = client.send("Hello World!")
+    //  }
+    //}
   }
 }
