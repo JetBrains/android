80a228ef65914704409cc66bd553484f9a26facb
Chris Sinco <csinco@google.com>
2021-01-28 14:38:17 -0800

    Updating icons to exactly match new Welcome screen
    Test: Visual inspection of local build
    Fixes: 178463983
    Change-Id: Ie591c76a21a81f22795bcec64bcdf2a61b6c896b
    
    GitOrigin-RevId: 2a8b9ab5ca762bb3e88e6be4b63b563eb4a44263

--- android/src/com/android/tools/idea/actions/AndroidNewProjectAction.kt
+++ android/src/com/android/tools/idea/actions/AndroidNewProjectAction.kt
@@ -22,6 +22,7 @@ import com.android.tools.idea.sdk.wizard.SdkQuickfixUtils
 import com.android.tools.idea.ui.wizard.SimpleStudioWizardLayout
 import com.android.tools.idea.ui.wizard.StudioWizardDialogBuilder
 import com.android.tools.idea.wizard.model.ModelWizard.Builder
+import com.intellij.icons.AllIcons
 import com.intellij.idea.ActionsBundle.actionText
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
@@ -34,7 +35,8 @@ import org.jetbrains.android.sdk.AndroidSdkUtils
 class AndroidNewProjectAction @JvmOverloads constructor(text: String = actionText("NewDirectoryProject")) : AnAction(text), DumbAware {
   override fun update(e: AnActionEvent) {
     if (NewWelcomeScreen.isNewWelcomeScreen(e)) {
-      e.presentation.icon = StudioIcons.Common.ADD
+      e.presentation.icon = AllIcons.Welcome.CreateNewProjectTab
+      e.presentation.selectedIcon = AllIcons.Welcome.CreateNewProjectTabSelected
     }
   }
 
