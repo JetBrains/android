3ebf042e57a7f6a05f210947ddf535b8570e87c2
Matthew Gharrity <gharrma@google.com>
2021-01-28 15:01:53 -0500

    Remove use of JUnit3RunnerWithInners
    JUnit3RunnerWithInners was created to make it easier to run the tests
    from all inner classes of an outer class, but (1) it does not seem
    to work for us in the IDE, and (2) it does not affect which tests
    end up running in Bazel.
    
    So, it is safe to remove. Removing it helps reduce our dependency
    on the Kotlin plugin test framework.
    
    Bug: 178713991
    Test: existing android-kotlin tests
    Change-Id: I0948629ee7cdeeda401ece4280d6a7bc99cf9d95
    
    GitOrigin-RevId: f69ba9b8d87d45f30cf2769d22ba1527ed357313

--- android-kotlin/idea-android/tests/org/jetbrains/kotlin/android/annotator/AndroidGutterIconTestGenerated.java
+++ android-kotlin/idea-android/tests/org/jetbrains/kotlin/android/annotator/AndroidGutterIconTestGenerated.java
@@ -6,13 +6,11 @@
 package org.jetbrains.kotlin.android.annotator;
 
 import com.intellij.testFramework.TestDataPath;
-import org.jetbrains.kotlin.test.JUnit3RunnerWithInners;
 import org.jetbrains.kotlin.android.KotlinTestUtils;
 import org.junit.runner.RunWith;
 
 @SuppressWarnings("all")
 @TestDataPath("$PROJECT_ROOT")
-@RunWith(JUnit3RunnerWithInners.class)
 public class AndroidGutterIconTestGenerated extends AbstractAndroidGutterIconTest {
 
     public void testColor() throws Exception {
