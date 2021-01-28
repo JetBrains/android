c73511c37d5fba9a5ba68143604e0f7080b05d02
Matthew Gharrity <gharrma@google.com>
2021-01-28 15:41:21 -0500

    Migrate ConfigLibraryUtil
    Bug: 178713991
    Test: existing android-kotlin tests
    Change-Id: If72a53a58cd1b87fe714ce4bf19c0bfda6d53ec9

--- android-kotlin/idea-android/tests/org/jetbrains/kotlin/android/annotator/AbstractAndroidGutterIconTest.kt
+++ android-kotlin/idea-android/tests/org/jetbrains/kotlin/android/annotator/AbstractAndroidGutterIconTest.kt
@@ -22,7 +22,7 @@ import com.intellij.util.PathUtil
 import com.intellij.util.ui.ColorIcon
 import junit.framework.TestCase
 import org.jetbrains.kotlin.android.KotlinAndroidTestCase
-import org.jetbrains.kotlin.idea.test.ConfigLibraryUtil
+import org.jetbrains.kotlin.android.ConfigLibraryUtil
 import org.jetbrains.kotlin.android.InTextDirectivesUtils
 import java.awt.Color
 import java.io.File
