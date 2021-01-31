fe3ef072786cd1b1981ac74836321cef126d6019
Yuriy Solodkyy <solodkyy@google.com>
2021-01-31 17:24:07 +0000

    Remove mergedFlavor from IdeVariant
    It is not used anymore. Its usages have been replaced with usages of
    the properties added to the `IdeVariant`.
    
    Bug: 37797956
    Test: n/a
    Change-Id: I777c51919d02e997e8f15b547e91da0d97bef182
    
    GitOrigin-RevId: 4ea37f395477154db4cbdc1ed9ee076a953901cd

--- android/src/com/android/tools/idea/gradle/project/model/AndroidModuleModel.java
+++ android/src/com/android/tools/idea/gradle/project/model/AndroidModuleModel.java
@@ -81,7 +81,7 @@ public class AndroidModuleModel implements AndroidModel, ModuleModel {
   // Placeholder application id if the project is never built before, there is no way to get application id.
   public static final String UNINITIALIZED_APPLICATION_ID = "uninitialized.application.id";
   private static final AndroidVersion NOT_SPECIFIED = new AndroidVersion(0, null);
-  private final static String ourAndroidSyncVersion = "2021-01-31/2";
+  private final static String ourAndroidSyncVersion = "2021-01-31/3";
 
   @Nullable public transient Object lintModuleModelCache;
   @Nullable private transient Module myModule;
