54aa2b59b3f0302596a157d6dccb4627701618c4
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2021-02-03 19:23:47 +0100

    cleanup
    GitOrigin-RevId: 4bed85d2ab8d0a138370632f14266944d2766e33

--- android/src/org/jetbrains/android/inspections/ResourceTypeCompletionContributor.java
+++ android/src/org/jetbrains/android/inspections/ResourceTypeCompletionContributor.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2014 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.android.inspections;
 
 import static com.android.SdkConstants.INT_DEF_ANNOTATION;
@@ -28,8 +14,6 @@ import static com.android.tools.lint.detector.api.ResourceEvaluator.PX_ANNOTATIO
 import static com.android.tools.lint.detector.api.ResourceEvaluator.RES_SUFFIX;
 
 import com.android.resources.ResourceType;
-import com.android.tools.idea.model.MergedManifestManager;
-import com.google.common.collect.Lists;
 import com.intellij.codeInsight.AnnotationUtil;
 import com.intellij.codeInsight.ExpectedTypeInfo;
 import com.intellij.codeInsight.completion.CompletionContributor;
@@ -78,15 +62,12 @@ import com.intellij.util.Consumer;
 import gnu.trove.THashSet;
 import gnu.trove.TObjectHashingStrategy;
 import java.util.ArrayList;
-import org.jetbrains.android.dom.manifest.AndroidManifestUtils;
-import org.jetbrains.android.facet.AndroidFacet;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import org.jetbrains.android.dom.manifest.AndroidManifestUtils;
 import org.jetbrains.android.facet.AndroidFacet;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -271,7 +252,9 @@ public class ResourceTypeCompletionContributor extends CompletionContributor {
       if (constraint == null) {
         PsiClass aClass = annotation.resolveAnnotationType();
         if (aClass == null) continue;
-        if (visited == null) visited = new THashSet<>();
+        if (visited == null) {
+          visited = new HashSet<>();
+        }
         if (!visited.add(aClass)) continue;
         constraint = getAllowedValues(aClass, type, visited);
       }
