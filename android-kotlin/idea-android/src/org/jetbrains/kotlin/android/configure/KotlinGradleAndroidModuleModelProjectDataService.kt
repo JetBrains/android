9a0b5051fb2991983979387640e00dc21afa47f4
Bradley Smith <smithbradley@google.com>
2021-02-09 14:18:19 -0800

    [MPSS] Fix Kotlin facet configuration
    This ensures that facets are configured on modules that have been
    generated from android artifacts.
    
    Test: manually tested - other tests coming soon
    Bug: N/A
    Change-Id: I9c5a7be8b6a6528ea8ebee6ebd9a3aeedbdea75f

--- android-kotlin/idea-android/src/org/jetbrains/kotlin/android/configure/KotlinGradleAndroidModuleModelProjectDataService.kt
+++ android-kotlin/idea-android/src/org/jetbrains/kotlin/android/configure/KotlinGradleAndroidModuleModelProjectDataService.kt
@@ -18,6 +18,7 @@ package org.jetbrains.kotlin.android.configure
 
 import com.android.tools.idea.gradle.project.model.AndroidModuleModel
 import com.android.tools.idea.gradle.project.sync.idea.data.service.AndroidProjectKeys.*
+import com.android.utils.usLocaleCapitalize
 import com.intellij.openapi.externalSystem.model.DataNode
 import com.intellij.openapi.externalSystem.model.ProjectKeys
 import com.intellij.openapi.externalSystem.model.project.ProjectData
@@ -26,7 +27,10 @@ import com.intellij.openapi.externalSystem.service.project.manage.AbstractProjec
 import com.intellij.openapi.externalSystem.util.ExternalSystemApiUtil
 import com.intellij.openapi.project.Project
 import org.jetbrains.kotlin.idea.configuration.GradleProjectImportHandler
+import org.jetbrains.kotlin.idea.configuration.compilerArgumentsBySourceSet
 import org.jetbrains.kotlin.idea.configuration.configureFacetByGradleModule
+import org.jetbrains.plugins.gradle.model.data.GradleSourceSetData
+import java.util.Locale
 
 class KotlinGradleAndroidModuleModelProjectDataService : AbstractProjectDataService<AndroidModuleModel, Void>() {
     override fun getTargetDataKey() = ANDROID_MODEL
@@ -41,6 +45,22 @@ class KotlinGradleAndroidModuleModelProjectDataService : AbstractProjectDataServ
         for (moduleModelNode in toImport) {
             val moduleNode = ExternalSystemApiUtil.findParent(moduleModelNode, ProjectKeys.MODULE) ?: continue
             val moduleData = moduleNode.data
+            val sourceSetNodes = ExternalSystemApiUtil.findAll(moduleNode, GradleSourceSetData.KEY)
+
+            // If module per source set is enabled then we will have source set data nodes
+            if (sourceSetNodes.isNotEmpty()) {
+              sourceSetNodes.forEach { sourceSetNode ->
+                val ideModule = modelsProvider.findIdeModule(sourceSetNode.data) ?: return@forEach
+                ideModule.compilerArgumentsBySourceSet = moduleNode.compilerArgumentsBySourceSet
+                val kotlinSourceSetName = moduleModelNode.data.selectedVariantName +
+                                          (sourceSetNode.data.moduleName.takeUnless { it == "main" }?.usLocaleCapitalize() ?: "")
+                val kotlinFacet = configureFacetByGradleModule(ideModule, modelsProvider, moduleNode, sourceSetNode, kotlinSourceSetName)
+                                  ?: return@forEach
+                GradleProjectImportHandler.getInstances(project).forEach { it.importBySourceSet(kotlinFacet, sourceSetNode) }
+              }
+              return
+            }
+
             val ideModule = modelsProvider.findIdeModule(moduleData) ?: continue
             val sourceSetName = moduleModelNode.data.selectedVariant.name
             val kotlinFacet = configureFacetByGradleModule(moduleNode, sourceSetName, ideModule, modelsProvider) ?: continue
