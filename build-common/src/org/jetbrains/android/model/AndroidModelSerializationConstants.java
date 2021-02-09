1950a6c52140941a4f0f6a85ef51a816136d186d
Andrei Kuznetsov <andrei.kuznetsov@jetbrains.com>
2021-02-09 17:45:45 +0300

    Cleanup: delete unneeded class AndroidModelSerializationConstants
    (cherry picked from commit bb94f431efd3720176db5e5eee42b9a5412ae878)
    
    Change-Id: Ibb94f431efd3720176db5e5eee42b9a5412ae878

--- build-common/src/org/jetbrains/android/model/AndroidModelSerializationConstants.java
+++
@@ -1,6 +0,0 @@
-package org.jetbrains.android.model;
-
-public class AndroidModelSerializationConstants {
-  public static final String ANDROID_GRADLE_FACET_ID = "android-gradle";
-  public static final String ANDROID_GRADLE_FACET_NAME = "Android-Gradle";
-}
