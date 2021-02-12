83f8e7ccbd00b32ffbc65f481516cd4cc1583569
Christophe Rhodes <xof@google.com>
2021-02-12 13:51:51 +0000

    [Dsl] Support editing existing mutable-collection properties
    If we have an implicitly-created mutable collection (from augmenting a
    not-yet-present Dsl element using a helper function or augmented
    assignment), it is not necessarily legal to change the structure of that
    collection by changing the argument list.  For example, some properties
    have distinct 1-arg and vararg helper functions, and an attempt to
    modify a call which happens to use a 1-arg function to add a new element
    would lead to invalid Dsl.  In the case of changed structure, then, we
    delete and re-create the element, allowing the backends to make a
    suitable choice of representation.
    
    In the case where the structure of the collection is unchanged, though
    elements themselves are modified, continue to use the old codepath.
    This is a workaround for the fact that the Dsl writer is not yet correct
    enough to avoid writing invalid Dsl for some property creation,
    particularly those elements of packagingOptions which only have 1-arg
    helper functions (and no vararg equivalent); we still cannot in fact
    write correct Dsl in general, but at least we don't make the editing
    case any worse through this change.
    
    One existing test changes output as a result of this change; the output
    remains legal.
    
    Bug: 144280051
    Bug: 179483658
    Test: added; one test updated
    Change-Id: I7be323001625251dd6493d1246880c4cc3e43cd0

--- gradle-dsl/testSrc/com/android/tools/idea/gradle/dsl/model/android/packagingOptions/JniLibsModelTest.kt
+++ gradle-dsl/testSrc/com/android/tools/idea/gradle/dsl/model/android/packagingOptions/JniLibsModelTest.kt
@@ -58,10 +58,58 @@ class JniLibsModelTest : GradleFileModelTestCase() {
     verifyListProperty("keepDebugSymbols", jniLibsModel.keepDebugSymbols(), listOf("a", "b", "c"))
   }
 
+  @Test
+  fun testAddElementsAndApply() {
+    writeToBuildFile(TestFile.ADD_ELEMENTS_AND_APPLY)
+    val buildModel = gradleBuildModel
+    val jniLibsModel = buildModel.android().packagingOptions().jniLibs()
+
+    jniLibsModel.excludes().addListValue().setValue("excludes2")
+    jniLibsModel.pickFirsts().addListValue().setValue("pickFirsts2")
+    jniLibsModel.keepDebugSymbols().addListValue().setValue("keepDebugSymbols1")
+
+    applyChangesAndReparse(buildModel)
+    verifyFileContents(myBuildFile, TestFile.ADD_ELEMENTS_AND_APPLY_EXPECTED)
+  }
+
+  @Test
+  fun testRemoveElementsAndApply() {
+    writeToBuildFile(TestFile.REMOVE_ELEMENTS_AND_APPLY)
+    val buildModel = gradleBuildModel
+    val jniLibsModel = buildModel.android().packagingOptions().jniLibs()
+
+    jniLibsModel.excludes().getListValue("excludes1")?.delete()
+    jniLibsModel.pickFirsts().getListValue("pickFirsts1")?.delete()
+    jniLibsModel.keepDebugSymbols().getListValue("keepDebugSymbols1")?.delete()
+
+    applyChangesAndReparse(buildModel)
+    verifyFileContents(myBuildFile, TestFile.REMOVE_ELEMENTS_AND_APPLY_EXPECTED)
+  }
+
+  @Test
+  fun testEditElementsAndApply() {
+    writeToBuildFile(TestFile.EDIT_ELEMENTS_AND_APPLY)
+    val buildModel = gradleBuildModel
+    val jniLibsModel = buildModel.android().packagingOptions().jniLibs()
+
+    jniLibsModel.excludes().getListValue("excludes1")?.setValue("excludesX")
+    jniLibsModel.pickFirsts().getListValue("pickFirsts1")?.setValue("pickFirstsX")
+    jniLibsModel.keepDebugSymbols().getListValue("keepDebugSymbols1")?.setValue("keepDebugSymbolsX")
+
+    applyChangesAndReparse(buildModel)
+    verifyFileContents(myBuildFile, TestFile.EDIT_ELEMENTS_AND_APPLY_EXPECTED)
+  }
+
   enum class TestFile(val path: @SystemDependent String) : TestFileName {
     PARSE("parse"),
     ADD_AND_APPLY("addAndApply"),
     ADD_AND_APPLY_EXPECTED("addAndApplyExpected"),
+    ADD_ELEMENTS_AND_APPLY("addElementsAndApply"),
+    ADD_ELEMENTS_AND_APPLY_EXPECTED("addElementsAndApplyExpected"),
+    REMOVE_ELEMENTS_AND_APPLY("removeElementsAndApply"),
+    REMOVE_ELEMENTS_AND_APPLY_EXPECTED("removeElementsAndApplyExpected"),
+    EDIT_ELEMENTS_AND_APPLY("editElementsAndApply"),
+    EDIT_ELEMENTS_AND_APPLY_EXPECTED("editElementsAndApplyExpected"),
     ;
 
     override fun toFile(basePath: String, extension: String): File = super.toFile("$basePath/jniLibsModel/$path", extension)
