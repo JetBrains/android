b62a644d1cd8d53dac30d2b3feaaa5a062287cfc
Maksim Lazeba <mlazeba@google.com>
2021-02-16 13:16:06 +0000

    [BA] Known Gradle plugins data service
    Introduce a service that parses information about known gradle plugins
    form a json file.
    There is a local copy of the file for a backup, service will try
    to download most recent version from the server.
    Plugin information currently includes:
     - Plugin common name.
     - Plugin class (classes to be more precise as we may want to group).
     - Plugin artifact.
     - Configuration caching compatibility.
    
    Bug: 173784161
    Test: added
    Change-Id: Ieb4c60b5df1cae73553ad955903647c4e9b3d0da
    
    GitOrigin-RevId: 1c24010c74ca61b7630aeb24c481eb07b88fdfeb

---
+++ build-attribution/testSrc/com/android/build/attribution/data/GradlePluginsDataTest.kt
@@ -0,0 +1,102 @@
+/*
+ * Copyright (C) 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.build.attribution.data
+
+import com.android.ide.common.repository.GradleVersion
+import com.google.common.truth.Truth
+import org.junit.Test
+
+class GradlePluginsDataTest {
+
+  @Test
+  fun testJsonV1Parsed() {
+    val fileContent = """
+    {"pluginsInfo":[{
+      "name": "MyPlugin",
+      "pluginClasses": ["my.plugin.pluginA","my.plugin.pluginB"],
+      "pluginArtifact": {"group":"org.my", "name":"gradle-plugin"},
+      "configurationCachingCompatibleFrom": "1.0.0"
+    }]}
+    """.trimIndent()
+
+    val parsedData = GradlePluginsData.loadFromJson(fileContent)
+
+    Truth.assertThat(parsedData).isEqualTo(
+      GradlePluginsData(
+        listOf(
+          GradlePluginsData.PluginInfo(
+            pluginClasses = listOf("my.plugin.pluginA", "my.plugin.pluginB"),
+            name = "MyPlugin",
+            pluginArtifact = GradlePluginsData.DependencyCoordinates("org.my", "gradle-plugin"),
+            configurationCachingCompatibleFrom = GradleVersion.parse("1.0.0")
+          )
+        )
+      )
+    )
+  }
+
+  @Test
+  fun testJsonParsedWithoutSomeFields() {
+    val fileContent = """
+    {"pluginsInfo":[{
+      "pluginClasses": ["my.plugin.pluginA","my.plugin.pluginB"],
+      "name": "MyPlugin"
+    }]}
+    """.trimIndent()
+
+    val parsedData = GradlePluginsData.loadFromJson(fileContent)
+
+    Truth.assertThat(parsedData).isEqualTo(
+      GradlePluginsData(
+        listOf(
+          GradlePluginsData.PluginInfo(
+            pluginClasses = listOf("my.plugin.pluginA", "my.plugin.pluginB"),
+            name = "MyPlugin"
+          )
+        )
+      )
+    )
+  }
+
+  @Test
+  fun testJsonParsedWithExtraFields() {
+    val fileContent = """
+    { "pluginsInfo": [{
+        "name": "MyPlugin",
+        "pluginClasses": ["my.plugin.pluginA","my.plugin.pluginB"],
+        "pluginArtifact": {"group":"org.my", "name":"gradle-plugin"},
+        "configurationCachingCompatibleFrom": "1.0.0",
+        "newField": "newValue"
+      }]}
+    """.trimIndent()
+
+
+    val parsedData = GradlePluginsData.loadFromJson(fileContent)
+
+    Truth.assertThat(parsedData).isEqualTo(
+      GradlePluginsData(
+        listOf(
+          GradlePluginsData.PluginInfo(
+            pluginClasses = listOf("my.plugin.pluginA", "my.plugin.pluginB"),
+            name = "MyPlugin",
+            pluginArtifact = GradlePluginsData.DependencyCoordinates("org.my", "gradle-plugin"),
+            configurationCachingCompatibleFrom = GradleVersion.parse("1.0.0")
+          )
+        )
+      )
+    )
+  }
+}
