2aa60c901e41435d52aa0d0e2cb674c935713780
Charlie Tsai <chartsai@google.com>
2021-02-25 14:45:37 +0000

    Fix cannot pan Layout Validation Tool
    The interaction manager stops to listen the events and never activate
    back even when a new model is added.
    
    Fixes: 180292432
    Test: manually
    Change-Id: Ica8d8a5f4ed30ec705d3a46232430c695b874dc3

--- designer/src/com/android/tools/idea/common/surface/DesignSurface.java
+++ designer/src/com/android/tools/idea/common/surface/DesignSurface.java
@@ -689,6 +689,7 @@ public abstract class DesignSurface extends EditorDesignSurface implements Dispo
         listener.modelChanged(this, model);
       }
     });
+    reactivateInteractionManager();
     return manager;
   }
 
