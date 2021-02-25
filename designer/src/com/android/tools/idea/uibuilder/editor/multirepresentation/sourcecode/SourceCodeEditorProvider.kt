d0f31382d00c85c0e3d3ea95b4fa60a3ef5c7569
Andrei Kuznetsov <andrei.kuznetsov@jetbrains.com>
2021-02-25 23:25:07 +0300

    IDEA-262075: Android's SourceCodeEditorProvider should not affect projects with no Android modules in IDEA
    GitOrigin-RevId: 7452cbd467e3e3b79f56df3e9a23f72bbba1f02a

--- designer/src/com/android/tools/idea/uibuilder/editor/multirepresentation/sourcecode/SourceCodeEditorProvider.kt
+++ designer/src/com/android/tools/idea/uibuilder/editor/multirepresentation/sourcecode/SourceCodeEditorProvider.kt
@@ -16,6 +16,7 @@
 package com.android.tools.idea.uibuilder.editor.multirepresentation.sourcecode
 
 import com.android.tools.idea.flags.StudioFlags.NELE_SOURCE_CODE_EDITOR
+import com.intellij.facet.ProjectFacetManager
 import com.intellij.ide.lightEdit.LightEdit
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.fileEditor.FileEditor
@@ -24,10 +25,13 @@ import com.intellij.openapi.fileEditor.FileEditorProvider
 import com.intellij.openapi.fileEditor.TextEditor
 import com.intellij.openapi.fileEditor.impl.text.QuickDefinitionProvider
 import com.intellij.openapi.fileEditor.impl.text.TextEditorProvider
+import com.intellij.openapi.module.ModuleUtil
 import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VfsUtil
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.PsiManager
+import org.jetbrains.android.facet.AndroidFacet
 
 /**
  * [FileEditorProvider] intended to be used with all source code files universally and therefore accepts all source code files. Creates
@@ -37,6 +41,7 @@ class SourceCodeEditorProvider : FileEditorProvider, QuickDefinitionProvider, Du
   private val LOG = Logger.getInstance(SourceCodeEditorProvider::class.java)
 
   override fun accept(project: Project, file: VirtualFile): Boolean =
+    ProjectFacetManager.getInstance(project).hasFacets(AndroidFacet.ID) &&
     !LightEdit.owns(project) && NELE_SOURCE_CODE_EDITOR.get() && file.hasSourceFileExtension()
 
   override fun createEditor(project: Project, file: VirtualFile): FileEditor {
