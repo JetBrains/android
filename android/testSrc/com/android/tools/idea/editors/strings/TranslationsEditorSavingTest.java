d1883099c54e14887ca0379f7141dae0a644d325
Chester Hsieh <dunno@google.com>
2021-02-25 08:39:08 -0800

    Special case single translation file with irregular path name
    When the translation editor creates a new, non-preexisting, translation
    for a particular key, the only piece of information it has is its target
    translation language. Therefore, by default it just creates a file at a
    specific location (i.e. "values-<lang_code>/strings.xml") if it doesn't
    already exist and serializes the newly translated value in that file.
    
    This causes a user expectation mismatch: if the user provided a
    translation file that doesn't match the directory format as indicated
    above, a new file will be created in the location indicated above and
    all newly added translations will be serialized there, instead of the
    file the user provided.
    
    This fix works around this issue when there is only one file that's
    used as the source translation for a particular locale. It does so by
    crawling through all translated keys and checking their translation
    source file. When there is only one unique file, the translation editor
    will use this file as the destination file to serialize to.
    
    Test: TranslationsEditorSavingTest.java
    Fixes: 128106334
    Change-Id: Ic00ec378f79512fc901f8eb6dccaf3dc31754519

---
+++ android/testSrc/com/android/tools/idea/editors/strings/TranslationsEditorSavingTest.java
@@ -0,0 +1,121 @@
+/*
+ * Copyright (C) 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.editors.strings;
+
+import static org.junit.Assert.assertTrue;
+
+import com.android.tools.idea.editors.strings.table.FrozenColumnTable;
+import com.android.tools.idea.editors.strings.table.StringResourceTableModel;
+import com.android.tools.idea.editors.strings.table.StringTableCellEditor;
+import com.android.tools.idea.io.TestFileUtils;
+import com.android.tools.idea.testing.AndroidProjectRule;
+import com.intellij.openapi.application.Application;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.module.Module;
+import com.intellij.openapi.util.Disposer;
+import com.intellij.openapi.vfs.LocalFileSystem;
+import com.intellij.openapi.vfs.VirtualFile;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Collections;
+import org.intellij.lang.annotations.Language;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class TranslationsEditorSavingTest {
+  @Rule
+  public final AndroidProjectRule myRule = AndroidProjectRule.inMemory();
+
+  private Application myApplication;
+  private Path myRes;
+  private StringResourceEditor myEditor;
+  private StringResourceViewPanel myPanel;
+  private FrozenColumnTable myTable;
+
+  @Before
+  public void setUp() throws Exception {
+    myApplication = ApplicationManager.getApplication();
+
+    Module module = myRule.getModule();
+    myRes = Paths.get(module.getProject().getBasePath(), "app", "src", "main", "res");
+
+    @Language("XML")
+    String contents = "<resources>\n" +
+                      "    <string name=\"key_1\">key_1_default</string>\n" +
+                      "    <string name=\"key_2\">key_2_default</string>\n" +
+                      "    <string name=\"key_3\">key_3_default</string>\n" +
+                      "</resources>\n";
+    TestFileUtils.writeFileAndRefreshVfs(myRes.resolve(Paths.get("values", "strings.xml")), contents);
+
+    @Language("XML")
+    String translatedContents = "<resources>\n" +
+                                "    <string name=\"key_1\">key_1_translated</string>\n" +
+                                "</resources>\n";
+    TestFileUtils.writeFileAndRefreshVfs(myRes.resolve(Paths.get("values-zh-v11", "strings.xml")), translatedContents);
+
+    myEditor = new StringResourceEditor(StringsVirtualFile.getStringsVirtualFile(module));
+    myPanel = myEditor.getPanel();
+    myTable = myPanel.getTable();
+  }
+
+  @After
+  public void tearDown() {
+    Disposer.dispose(myEditor);
+  }
+
+  @Test
+  public void saveNewTranslationSingleSource() {
+    myApplication.invokeAndWait(() -> {
+      Utils.loadResources(myPanel, Collections.singletonList(myRes));
+
+      myTable.editCellAt(1, StringResourceTableModel.DEFAULT_VALUE_COLUMN + 1);
+      StringTableCellEditor cellEditor = (StringTableCellEditor)myTable.getCellEditor();
+      cellEditor.setCellEditorValue("key_2_translated");
+      cellEditor.stopCellEditing();
+    });
+
+    VirtualFile vFile =
+      LocalFileSystem.getInstance().refreshAndFindFileByIoFile(myRes.resolve(Paths.get("values-zh", "strings.xml")).toFile());
+    assertTrue(vFile == null || !vFile.exists());
+  }
+
+  @Test
+  public void saveNewTranslationMultipleSources() throws Exception {
+    @Language("XML")
+    String translatedContents = "<resources>\n" +
+                                "    <string name=\"key_2\">key_2_translated</string>\n" +
+                                "</resources>\n";
+    TestFileUtils.writeFileAndRefreshVfs(myRes.resolve(Paths.get("values-zh-v12", "strings.xml")), translatedContents);
+
+    myApplication.invokeAndWait(() -> {
+      Utils.loadResources(myPanel, Collections.singletonList(myRes));
+
+      myTable.editCellAt(2, StringResourceTableModel.DEFAULT_VALUE_COLUMN + 1);
+      StringTableCellEditor cellEditor = (StringTableCellEditor)myTable.getCellEditor();
+      cellEditor.setCellEditorValue("key_3_translated");
+      cellEditor.stopCellEditing();
+    });
+
+    VirtualFile vFile =
+      LocalFileSystem.getInstance().refreshAndFindFileByIoFile(myRes.resolve(Paths.get("values-zh", "strings.xml")).toFile());
+    assertTrue(vFile != null && vFile.exists());
+  }
+}
