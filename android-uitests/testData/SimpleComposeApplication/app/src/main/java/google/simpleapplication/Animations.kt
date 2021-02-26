000227400e1b5936bbf0c30520a692f35f1fd0b2
Amaury Medeiros <amaurym@google.com>
2021-02-26 19:36:54 +0000

    Fix SimpleComposeApplication to use updated Compose APIs
    Update the APIs used in SimpleComposeApplication so the project doesn't
    rely on deprecated or already removed methors. This change makes the
    build step succeed in ComposePreviewTest UI tests.
    
    Bug: 172894609
    Test: ComposePreviewTest
    Change-Id: Ia75b8895a0906071cb53fe42464b1f7340552f4c

--- android-uitests/testData/SimpleComposeApplication/app/src/main/java/google/simpleapplication/Animations.kt
+++ android-uitests/testData/SimpleComposeApplication/app/src/main/java/google/simpleapplication/Animations.kt
@@ -1,63 +1,104 @@
 package google.simpleapplication
 
-import androidx.compose.animation.core.FloatPropKey
-import androidx.compose.animation.core.transitionDefinition
+import androidx.compose.animation.animateColor
+import androidx.compose.animation.core.Transition
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.spring
 import androidx.compose.animation.core.tween
-import androidx.compose.animation.transition
-import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.Text
+import androidx.compose.animation.core.updateTransition
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
-private const val halfSize = 200f
-
-private val scale = FloatPropKey()
-
-private val transDef = transitionDefinition<String> {
-  state("start") {
-    this[scale] = 1f
-  }
-  state("end") {
-    this[scale] = 3f
-  }
-  transition {
-    scale using tween(
-      durationMillis = 10000
-    )
-  }
-}
 
 @Preview
 @Composable
-fun AnimationDemo() {
-  val state = transition(definition = transDef, initState = "start", toState = "end")
-  Scaffold {
-    ScaledColorRect(scale = state[scale])
+fun GestureAnimationSample() {
+  // enum class ComponentState { Pressed, Released }
+  var useRed by remember { mutableStateOf(false) }
+  var toState by remember { mutableStateOf(ComponentState.Released) }
+  val modifier = Modifier.pointerInput(Unit) {
+    detectTapGestures(
+      onPress = {
+        toState = ComponentState.Pressed
+        tryAwaitRelease()
+        toState = ComponentState.Released
+      }
+    )
+  }
+
+  val transition: Transition<ComponentState> = updateTransition(targetState = toState)
+
+  val scale: Float by transition.animateFloat(
+    label = "Scale",
+    transitionSpec = { spring(stiffness = 50f) }
+  ) { state ->
+    if (state == ComponentState.Pressed) 3f else 1f
+  }
+
+  val color: Color by transition.animateColor(
+    label = "Background Color",
+    transitionSpec = {
+      when {
+        ComponentState.Pressed isTransitioningTo ComponentState.Released ->
+          spring(stiffness = 50f)
+        else ->
+          tween(durationMillis = 500)
+      }
+    }
+  ) { state ->
+    when (state) {
+      ComponentState.Pressed -> MaterialTheme.colors.primary
+      ComponentState.Released -> if (useRed) Color(0xFFFF0000) else MaterialTheme.colors.secondary
+    }
+  }
+
+  Column {
+    Button(
+      modifier = Modifier
+        .padding(10.dp)
+        .align(Alignment.CenterHorizontally),
+      onClick = { useRed = !useRed }
+    ) {
+      Text("Change Color")
+    }
+
+    Box(
+      modifier
+        .fillMaxSize()
+        .wrapContentSize(Alignment.CenterStart)
+        .size((100 * scale).dp)
+        .background(color)
+    )
+
   }
 }
+
+enum class ComponentState { Pressed, Released }
 
 @Preview
 @Composable
 fun NotAnAnimation() {
   MaterialTheme {
     Text(text = "Not an animation!")
-  }
-}
-
-@Composable
-private fun ScaledColorRect(scale: Float) {
-  Canvas(Modifier.fillMaxSize()) {
-    drawRect(
-      Color.Red,
-      topLeft = Offset(center.x - halfSize * scale, center.y - halfSize * scale),
-      size = Size(halfSize * 2 * scale, halfSize * 2 * scale)
-    )
   }
 }
