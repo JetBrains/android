d1a4da676f0557764a1f84787ae3a0bf1591c6d8
Andrey Akinshin <andrey.akinshin@jetbrains.com>
2021-03-04 17:42:08 +0300

    Send duration_ms to FUS for action.invoked
    (cherry picked from commit 7166d8ee7e41cab2d2c6821c2df6ab87b123a33d)
    
    IJ-MR-6661
    
    GitOrigin-RevId: 2adaab92847ee2d31f2b90c839dd6b018ad208b6

--- android/src/com/android/tools/idea/gradle/util/ui/Header.java
+++ android/src/com/android/tools/idea/gradle/util/ui/Header.java
@@ -23,6 +23,7 @@ import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.DataContext;
 import com.intellij.openapi.actionSystem.Separator;
 import com.intellij.openapi.actionSystem.ex.ActionManagerEx;
+import com.intellij.openapi.actionSystem.ex.ActionUtil;
 import com.intellij.openapi.util.SystemInfo;
 import com.intellij.openapi.wm.impl.content.BaseLabel;
 import com.intellij.ui.InplaceButton;
@@ -222,7 +223,7 @@ public class Header extends JPanel {
       InputEvent inputEvent = e.getSource() instanceof InputEvent ? (InputEvent)e.getSource() : null;
       AnActionEvent event = AnActionEvent.createFromAnAction(myAction, inputEvent, UNKNOWN, dataContext);
       actionManager.fireBeforeActionPerformed(myAction, dataContext, event);
-      myAction.actionPerformed(event);
+      ActionUtil.performAction(myAction, dataContext, event);
     }
   }
 
