67f0e19ea3fffd29c4a8f0a95459c20597aa54e3
Alan Leung <acleung@google.com>
2021-03-03 18:14:21 -0800

    LL: Don't mention LL monitor in status.
    Since it is short lived and it might not always start LL
    monitoring so let us not confuse the user.
    
    Bug: 181807399
    Test: None
    Change-Id: I4f5d2d3dc9ba07db33318c0d9260384341157d10
    
    GitOrigin-RevId: fd38dff357c18518a3b91f8751e1991325cc4d02

--- deploy/src/com/android/tools/idea/run/tasks/StartLiveLiteralMonitoringTask.java
+++ deploy/src/com/android/tools/idea/run/tasks/StartLiveLiteralMonitoringTask.java
@@ -31,7 +31,10 @@ public class StartLiveLiteralMonitoringTask implements LaunchTask {
 
   @Override
   public @NotNull String getDescription() {
-    return "Starting Live Literal Monitoring";
+    // This task only start LL under the right conditions so lets not
+    // lets not mention anything about LL on the status because
+    // users might not expect it in non-compose related projects.
+    return "";
   }
 
   @Override
