07701096e1086226a6c86262712f6d383a54ce93
Isaac Chai <ichai@google.com>
2021-03-08 16:49:51 -0800

    [atf] Refactor parser
    Simply move all layout parsing logic to its own class.
    
    We need to expand information stored from parsing the layout. This cl
    helps:
    - Make future feature to be impl'd without complicating scanner logic
    - Make the code more readable
    - Make the testing more independent (parser tested without depending on
    scanner)
    
    Bug: N/A
    Test: Added unit tests that no longer depends on scanner. Also passes
    all other existing tests.
    
    Change-Id: Ia1412a549cdd3103043ce57968ea3eedb500b731

--- designer/testSrc/com/android/tools/idea/uibuilder/surface/NlLayoutScannerTest.kt
+++ designer/testSrc/com/android/tools/idea/uibuilder/surface/NlLayoutScannerTest.kt
@@ -15,49 +15,34 @@
  */
 package com.android.tools.idea.uibuilder.surface
 
-import android.view.View
 import com.android.tools.idea.common.error.IssueModel
-import com.android.tools.idea.common.model.NlComponent
 import com.android.tools.idea.rendering.RenderResult
-import com.android.tools.idea.uibuilder.LayoutTestCase
-import com.android.tools.idea.uibuilder.model.viewInfo
+import com.android.tools.idea.testing.AndroidProjectRule
 import com.android.tools.idea.validator.ValidatorData
 import com.android.tools.idea.validator.ValidatorResult
-import com.intellij.openapi.Disposable
-import com.intellij.openapi.util.Disposer
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertNotNull
+import org.junit.Assert.assertTrue
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
 import org.mockito.Mockito
 
-class NlLayoutScannerTest : LayoutTestCase() {
-
-  @Volatile
-  private var disposable: Disposable? = null
-
-  override fun setUp() {
-    super.setUp()
-    disposable = object : Disposable {
-      // If using a lambda, it can be reused by the JVM and causing an exception
-      // because the Disposable is already disposed.
-      //noinspection Convert2Lambda
-      override fun dispose() { }
-    }
-  }
-
-  override fun tearDown() {
-    try {
-      Disposer.dispose(disposable!!)
-      disposable = null
-    } finally {
-      super.tearDown()
-    }
-  }
+@RunWith(JUnit4::class)
+class NlLayoutScannerTest {
+  @get:Rule
+  val projectRule = AndroidProjectRule.withSdk()
 
   private fun createScanner(): NlLayoutScanner {
     val issueModel: IssueModel = Mockito.mock(IssueModel::class.java)
     val metricTracker = NlLayoutScannerMetricTrackerTest.createMetricTracker()
-    return NlLayoutScanner(issueModel, disposable!!, metricTracker)
+    return NlLayoutScanner(issueModel, projectRule.fixture.testRootDisposable!!, metricTracker)
   }
 
-  fun testAddListener() {
+  @Test
+  fun addListener() {
     val scanner = createScanner()
     val listener = object : NlLayoutScanner.Listener {
       override fun lintUpdated(result: ValidatorResult?) { }
@@ -68,7 +53,8 @@ class NlLayoutScannerTest : LayoutTestCase() {
     assertTrue(scanner.listeners.contains(listener))
   }
 
-  fun testRemoveListener() {
+  @Test
+  fun removeListener() {
     val scanner = createScanner()
     val listener = object : NlLayoutScanner.Listener {
       override fun lintUpdated(result: ValidatorResult?) { }
@@ -80,7 +66,8 @@ class NlLayoutScannerTest : LayoutTestCase() {
     assertFalse(scanner.listeners.contains(listener))
   }
 
-  fun testRemoveListenerInCallback() {
+  @Test
+  fun removeListenerInCallback() {
     val scanner = createScanner()
     val model = ScannerTestHelper().buildModel(0)
     val renderResult = Mockito.mock(RenderResult::class.java)
@@ -96,79 +83,8 @@ class NlLayoutScannerTest : LayoutTestCase() {
     assertFalse(scanner.listeners.contains(listener))
   }
 
-  fun testBuildViewToComponentMap() {
-    val scanner = createScanner()
-    val helper = ScannerTestHelper()
-    val component = helper.buildNlComponent()
-
-    // Test building component to view mapping.
-    scanner.buildViewToComponentMap(component)
-
-    assertNotNull(component.viewInfo)
-    assertNotNull(component.viewInfo?.viewObject)
-    assertTrue(component.viewInfo?.viewObject is View)
-
-    assertEquals(1, scanner.viewToComponent.size)
-    assertEquals(1, scanner.idToComponent.size)
-    assertTrue(scanner.viewToComponent.values.contains(component))
-    assertTrue(scanner.idToComponent.keys.contains(helper.lastUsedViewId))
-  }
-
-  fun testBuildViewToComponentMapMultipleComponents() {
-    val scanner = createScanner()
-    val helper = ScannerTestHelper()
-    val componentSize = 4
-    val component = helper.buildModel(componentSize).components[0]
-
-    scanner.buildViewToComponentMap(component)
-
-    assertEquals(componentSize, scanner.viewToComponent.size)
-    assertEquals(componentSize, scanner.idToComponent.size)
-    assertTrue(scanner.viewToComponent.values.contains(component))
-    component.children.forEach {
-      assertTrue(scanner.viewToComponent.values.contains(it))
-    }
-    assertTrue(scanner.idToComponent.keys.contains(helper.lastUsedViewId))
-  }
-
-  fun testFindComponent() {
-    val scanner = createScanner()
-    val helper = ScannerTestHelper()
-    val component = helper.buildNlComponent()
-    scanner.buildViewToComponentMap(component)
-
-    val result = helper.generateResult(component).build()
-    val issue = ScannerTestHelper.createTestIssueBuilder()
-      .setSrcId(helper.lastUsedIssueId).build()
-
-    val found = scanner.findComponent(issue, result.srcMap)
-
-    assertNotNull(found)
-    assertEquals(component, found)
-  }
-
-  fun testFindComponentThruId() {
-    val scanner = createScanner()
-    val helper = ScannerTestHelper()
-    val component = helper.buildNlComponent()
-    scanner.buildViewToComponentMap(component)
-
-    val result = helper.generateResult(component).build()
-    val issue = ScannerTestHelper.createTestIssueBuilder()
-      .setSrcId(helper.lastUsedIssueId).build()
-
-    // Simulate render out of sync. View to component map no longer useful.
-    // When render is out of sync with error update, new view instance is created
-    // per component. Force components to find its View thru id match.
-    scanner.viewToComponent.clear()
-
-    val found = scanner.findComponent(issue, result.srcMap)
-
-    assertNotNull(found)
-    assertEquals(component, found)
-  }
-
-  fun testValidateNoResult() {
+  @Test
+  fun validateNoResult() {
     val scanner = createScanner()
     val helper = ScannerTestHelper()
     val componentSize = 0
@@ -187,11 +103,11 @@ class NlLayoutScannerTest : LayoutTestCase() {
 
     assertTrue(listenerTriggered)
     assertEquals(0, scanner.issues.size)
-    assertTrue(scanner.viewToComponent.isEmpty())
-    assertTrue(scanner.idToComponent.isEmpty())
+    assertTrue(scanner.isParserCleaned())
   }
 
-  fun testValidateEmpty() {
+  @Test
+  fun validateEmpty() {
     val scanner = createScanner()
     val helper = ScannerTestHelper()
     val componentSize = 0
@@ -210,11 +126,11 @@ class NlLayoutScannerTest : LayoutTestCase() {
 
     assertTrue(listenerTriggered)
     assertEquals(0, scanner.issues.size)
-    assertTrue(scanner.viewToComponent.isEmpty())
-    assertTrue(scanner.idToComponent.isEmpty())
+    assertTrue(scanner.isParserCleaned())
   }
 
-  fun testValidateMultipleIssues() {
+  @Test
+  fun validateMultipleIssues() {
     val scanner = createScanner()
     val helper = ScannerTestHelper()
     val componentSize = 5
@@ -234,11 +150,11 @@ class NlLayoutScannerTest : LayoutTestCase() {
     assertNotNull(validatorResult)
     assertEquals(componentSize, validatorResult!!.issues.size)
     assertEquals(componentSize, scanner.issues.size)
-    assertTrue(scanner.viewToComponent.isEmpty())
-    assertTrue(scanner.idToComponent.isEmpty())
+    assertTrue(scanner.isParserCleaned())
   }
 
-  fun testValidateFiltersInternalIssues() {
+  @Test
+  fun validateFiltersInternalIssues() {
     val scanner = createScanner()
     val helper = ScannerTestHelper()
     val model = helper.buildModel(1)
@@ -276,45 +192,6 @@ class NlLayoutScannerTest : LayoutTestCase() {
     assertNotNull(validatorResult)
     assertEquals( 3, validatorResult!!.issues.size)
     assertTrue("Issue from Validator Result must be filtered.", scanner.issues.isEmpty())
-    assertTrue("Maps must be cleaned after the scan.", scanner.viewToComponent.isEmpty())
-    assertTrue("Maps must be cleaned after the scan.", scanner.idToComponent.isEmpty())
-  }
-
-  fun testFindRootWithViewLikeDataBinding() {
-
-    // Build component tree with root, c1, c2 with only c2 with view info
-    val helper = ScannerTestHelper()
-    val root = helper.buildNlComponent()
-    val child1 = helper.buildNlComponent()
-    val child2 = helper.buildNlComponent()
-    root.viewInfo = null
-    child1.viewInfo = null
-    val children = ArrayList<NlComponent>(2)
-    children.add(child1)
-    children.add(child2)
-    Mockito.`when`(root.children).thenReturn(children)
-
-    val scanner = createScanner()
-    val result = scanner.tryFindingRootWithViewInfo(root)
-
-    assertEquals(child2, result)
-  }
-
-  fun testFindRootWithViewInfoInRoot() {
-
-    // Build component tree with root, c1, c2.
-    val helper = ScannerTestHelper()
-    val root = helper.buildNlComponent()
-    val child1 = helper.buildNlComponent()
-    val child2 = helper.buildNlComponent()
-    val children = ArrayList<NlComponent>(2)
-    children.add(child1)
-    children.add(child2)
-    Mockito.`when`(root.children).thenReturn(children)
-
-    val scanner = createScanner()
-    val result = scanner.tryFindingRootWithViewInfo(root)
-
-    assertEquals(root, result)
+    assertTrue("Maps must be cleaned after the scan.", scanner.isParserCleaned())
   }
 }
