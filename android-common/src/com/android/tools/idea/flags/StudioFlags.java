d28a82d4b57c3cf7360e9dab95e5322585a7ec6b
David Herman <davidherman@google.com>
2021-03-10 09:32:53 -0800

    Enable the flag for the layout inspector app inspection backend
    Bug: 170895806
    Test: Covered by existing
    Change-Id: I68a8fd790a8bbbdc03d9634d1dd05cf13462a831
    
    GitOrigin-RevId: 94ad3c685dfd6751b18dac0f98257393cac79a22

--- android-common/src/com/android/tools/idea/flags/StudioFlags.java
+++ android-common/src/com/android/tools/idea/flags/StudioFlags.java
@@ -649,7 +649,7 @@ public final class StudioFlags {
     "If enabled the component tree will include Composable nodes if they are wrapped in an Inspectable.", true);
   public static final Flag<Boolean> DYNAMIC_LAYOUT_INSPECTOR_USE_INSPECTION = Flag.create(
     LAYOUT_INSPECTOR, "dynamic.layout.inspector.use.inspection", "Use app inspection client",
-    "If enabled, use a client built on the app inspection pipeline instead of the transport pipeline.", false);
+    "If enabled, use a client built on the app inspection pipeline instead of the transport pipeline.", true);
   public static final Flag<Boolean> DYNAMIC_LAYOUT_INSPECTOR_COMPONENT_TREE_OPTIONS = Flag.create(
     LAYOUT_INSPECTOR, "dynamic.layout.inspector.tree.options", "Add view options to component tree",
     "If enabled, the component tree will have extra options under the gear dropdown.", false);
