d0c0fa3927aa2a969a19b1eade265d25d80c9cc2
Amaury Medeiros <amaurym@google.com>
2021-03-10 11:05:10 +0000

    Show weak warnings for transition properties label param
    Similar to what we do with updateTransition calls, we need to show weak
    warnings if the user does not set the label field when defining transition
    properties. If they don't explicitly set the label, a default value will be
    used to represent the property, and that might make it harder to inspect it
    in the animation preview. For example, it would be better for the user to
    see "Corner Radius" and "Scale" instead of "FloatAnimation" twice.
    
    Fixes: 181986045
    Test: AnimationInspectionsTest
    Change-Id: Ia329401e079544121e3d2b996114c382eef2ad2e
    
    GitOrigin-RevId: a42e896bb6dbb172edf0a3d76c59ed5caced881c

--- compose-designer/src/com/android/tools/idea/compose/preview/animation/AnimationInspections.kt
+++ compose-designer/src/com/android/tools/idea/compose/preview/animation/AnimationInspections.kt
@@ -28,14 +28,19 @@ import com.intellij.psi.PsiElementVisitor
 import com.intellij.psi.PsiFile
 import org.jetbrains.kotlin.idea.caches.resolve.resolveToCall
 import org.jetbrains.kotlin.idea.inspections.AbstractKotlinInspection
+import org.jetbrains.kotlin.idea.refactoring.fqName.fqName
 import org.jetbrains.kotlin.psi.KtCallExpression
 import org.jetbrains.kotlin.psi.KtPsiFactory
 import org.jetbrains.kotlin.psi.KtVisitorVoid
 import org.jetbrains.kotlin.resolve.calls.callUtil.getParameterForArgument
 import org.jetbrains.kotlin.resolve.descriptorUtil.fqNameOrNull
+import org.jetbrains.kotlin.types.SimpleType
 
 private const val LABEL_PARAMETER = "label"
-private const val UPDATE_TRANSITION_FQN = "androidx.compose.animation.core.updateTransition"
+private const val COMPOSE_ANIMATION_PACKAGE_PREFIX = "androidx.compose.animation"
+private const val UPDATE_TRANSITION_FQN = "$COMPOSE_ANIMATION_PACKAGE_PREFIX.core.updateTransition"
+private const val TRANSITION_FQN = "$COMPOSE_ANIMATION_PACKAGE_PREFIX.core.Transition"
+private const val ANIMATE_PREFIX = "animate" // e.g. animateColor, animateFloat, animateValue
 
 /**
  * Inspection to verify that the `label` parameter is set for `updateTransition` calls that create Compose transition animations. This
@@ -58,7 +63,7 @@ class UpdateTransitionLabelInspection : AbstractKotlinInspection() {
           }
           holder.registerProblem(
             expression,
-            message("inspection.no.label.parameter.set.description"),
+            message("inspection.update.transition.no.label.parameter.set.description"),
             ProblemHighlightType.WEAK_WARNING,
             AddLabelFieldQuickFix(expression))
         }
@@ -67,20 +72,64 @@ class UpdateTransitionLabelInspection : AbstractKotlinInspection() {
     else {
       PsiElementVisitor.EMPTY_VISITOR
     }
+}
 
-  /**
-   * Add the `label` parameter to an updateTransition expression. For example:
-   * `updateTransition(targetState = state)` -> `updateTransition(targetState = state, label = "")`
-   */
-  private class AddLabelFieldQuickFix(updateTransition: KtCallExpression) : LocalQuickFixOnPsiElement(updateTransition) {
-    override fun getFamilyName() = message("inspection.group.name")
+/**
+ * Inspection to verify that the `label` parameter is set for `animate*` (e.g. animateFloat, animateColor) calls that create Compose
+ * transition properties. This parameter is used by the animation tooling to identify the transition property when inspecting animations in
+ * the Animation Preview. Otherwise, a default name will be used (e.g. FloatProperty, ColorProperty).
+ */
+class TransitionPropertiesLabelInspection : AbstractKotlinInspection() {
+  override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean, session: LocalInspectionToolSession): PsiElementVisitor =
+    if (StudioFlags.COMPOSE_ANIMATION_PREVIEW_LABEL_INSPECTION.get() && session.file.androidFacet != null) {
+      object : KtVisitorVoid() {
+        override fun visitCallExpression(expression: KtCallExpression) {
+          super.visitCallExpression(expression)
+          val resolvedExpression = expression.resolveToCall() ?: return
+          // First, check we're visiting an extension method of Transition<T>
+          if ((resolvedExpression.extensionReceiver?.type as? SimpleType)?.fqName?.asString() != TRANSITION_FQN) return
 
-    override fun getText() = message("inspection.no.label.parameter.set.quick.fix.text")
+          // Now, check that we're visiting a method named animate* (e.g. animateFloat, animateValue, animateColor) defined on a compose
+          // animation (sub-)package (e.g. androidx.compose.animation, androidx.compose.animation.core).
+          val animateCall = resolvedExpression.resultingDescriptor.fqNameOrNull() ?: return
+          val shortName = animateCall.shortNameOrSpecial()
+          if (shortName.isSpecial
+              || !shortName.asString().startsWith(ANIMATE_PREFIX)
+              || !animateCall.parent().asString().startsWith(COMPOSE_ANIMATION_PACKAGE_PREFIX)) {
+            return
+          }
 
-    override fun invoke(project: Project, file: PsiFile, startElement: PsiElement, endElement: PsiElement) {
-      val psiFactory = KtPsiFactory(project)
-      val statementText = "$LABEL_PARAMETER = \"\""
-      (startElement as KtCallExpression).valueArgumentList?.addArgument(psiFactory.createArgument(statementText))
+          // Finally, verify the animate call has the `label` parameter set, otherwise show a weak warning.
+          if (expression.valueArguments.any { resolvedExpression.getParameterForArgument(it)?.name?.asString() == LABEL_PARAMETER }) {
+            // This Transition<T>.animate* call already has the label parameter set.
+            return
+          }
+          holder.registerProblem(
+            expression,
+            message("inspection.transition.properties.no.label.parameter.set.description"),
+            ProblemHighlightType.WEAK_WARNING,
+            AddLabelFieldQuickFix(expression))
+        }
+      }
     }
+    else {
+      PsiElementVisitor.EMPTY_VISITOR
+    }
+}
+
+/**
+ * Add the `label` parameter to a call expression. For example:
+ * `updateTransition(targetState = state)` -> `updateTransition(targetState = state, label = "")`
+ * `animateFloat(transitionSpec = ...)` -> `animateFloat(transitionSpec = ..., label = "")`
+ */
+private class AddLabelFieldQuickFix(updateTransition: KtCallExpression) : LocalQuickFixOnPsiElement(updateTransition) {
+  override fun getFamilyName() = message("inspection.group.name")
+
+  override fun getText() = message("inspection.no.label.parameter.set.quick.fix.text")
+
+  override fun invoke(project: Project, file: PsiFile, startElement: PsiElement, endElement: PsiElement) {
+    val psiFactory = KtPsiFactory(project)
+    val statementText = "$LABEL_PARAMETER = \"\""
+    (startElement as KtCallExpression).valueArgumentList?.addArgument(psiFactory.createArgument(statementText))
   }
 }
