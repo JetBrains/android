706d5328f574eab3d2c19c6e3e673e1e6e4eea41
Sergey Prigogin <sprigogin@google.com>
2021-03-12 15:30:31 -0800

    Replace a modal task by a modeless dialog
    Display progress of device screen recording using a modeless dialog instead of a modal task. This allows user to interact with embedded emulator during recording.
    
    Test: manual
    Bug: 180474805
    Change-Id: I336f51e68e168ee3c6ab1b160d90e3987618a28e
    
    GitOrigin-RevId: 550662ace90c1bb704603ccf72b10cd47b39562c

--- android/src/com/android/tools/idea/ddms/actions/ScreenRecorderTask.java
+++ android/src/com/android/tools/idea/ddms/actions/ScreenRecorderTask.java
@@ -15,26 +15,32 @@
  */
 package com.android.tools.idea.ddms.actions;
 
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
+
+import com.android.annotations.concurrency.Slow;
+import com.android.annotations.concurrency.UiThread;
 import com.android.ddmlib.CollectingOutputReceiver;
 import com.android.ddmlib.EmulatorConsole;
 import com.android.ddmlib.IDevice;
+import com.android.ddmlib.ScreenRecorderOptions;
+import com.google.common.annotations.VisibleForTesting;
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.fileChooser.FileChooserFactory;
 import com.intellij.openapi.fileChooser.FileSaverDescriptor;
 import com.intellij.openapi.fileChooser.FileSaverDialog;
-import com.intellij.openapi.progress.ProgressIndicator;
-import com.intellij.openapi.progress.Task;
+import com.intellij.openapi.progress.ProcessCanceledException;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.DialogWrapper;
 import com.intellij.openapi.ui.Messages;
 import com.intellij.openapi.util.SystemInfo;
-import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.openapi.vfs.VfsUtil;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.openapi.vfs.VirtualFileWrapper;
+import java.awt.EventQueue;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.StandardCopyOption;
 import java.util.Calendar;
 import java.util.Locale;
 import java.util.concurrent.CountDownLatch;
@@ -42,129 +48,223 @@ import java.util.concurrent.TimeUnit;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-final class ScreenRecorderTask extends Task.Modal {
+final class ScreenRecorderTask implements Runnable {
   private static final CharSequence MEDIA_UNSUPPORTED_ERROR = "-1010";
+  private static final long MAX_RECORDING_TIME_MILLIS = TimeUnit.MINUTES.toMillis(3);
 
+  private final Project myProject;
   private final IDevice myDevice;
-  private final CountDownLatch myCompletionLatch;
-  private final CollectingOutputReceiver myReceiver;
-  private final Path mHostTmpFileName;
+  private final Path myHostRecordingFile;
+  private final ScreenRecorderOptions myOptions;
+  private DialogWrapper myDialogWrapper;
 
   public ScreenRecorderTask(@NotNull Project project,
                             @NotNull IDevice device,
-                            @NotNull CountDownLatch completionLatch,
-                            @NotNull CollectingOutputReceiver receiver,
-                            @Nullable Path hostTmpFileName) {
-    super(project, ScreenRecorderAction.TITLE, true);
-
+                            @Nullable Path hostRecordingFile,
+                            @NotNull ScreenRecorderOptions options) {
+    myProject = project;
     myDevice = device;
-    myCompletionLatch = completionLatch;
-    myReceiver = receiver;
-    mHostTmpFileName = hostTmpFileName;
+    myHostRecordingFile = hostRecordingFile;
+    myOptions = options;
   }
 
+  @Slow
   @Override
-  public void run(@NotNull ProgressIndicator indicator) {
-    int elapsedTime = 0; // elapsed time in seconds
-    indicator.setIndeterminate(true);
-    while (true) {
+  public void run() {
+    CountDownLatch completionLatch = null;
+    CollectingOutputReceiver receiver = null;
+
+    EmulatorConsole console = null;
+    if (myHostRecordingFile != null) { // Using emulator screen recording.
+      console = EmulatorConsole.getConsole(myDevice);
+      if (console == null) {
+        return; // Emulator was terminated.
+      }
       try {
-        if (myCompletionLatch.await(1, TimeUnit.SECONDS)) {
-          break;
-        }
-
-        // update elapsed time in seconds
-        elapsedTime++;
-        indicator.setText(
-          String.format(Locale.US, "Recording...%1$d %2$s elapsed", elapsedTime, StringUtil.pluralize("second", elapsedTime)));
-
-        // If using emulator screen recording feature, stop the recording if the emulator dies
-        EmulatorConsole console = null;
-
-        if (mHostTmpFileName != null) { // Using emulator screen recording
-          // getConsole() will check if the emulator is alive
-          console = EmulatorConsole.getConsole(myDevice);
-          if (console == null) {
-            indicator.cancel();
-          }
-        }
-        // Emulator recording has a max recording time of 3 min, so explicitly stop the recording when
-        // the time limit is reached.
-        if (indicator.isCanceled() || elapsedTime >= 180) {
-          // explicitly cancel the running task
-          if (mHostTmpFileName != null) { // Using emulator screen recording
-            if (console != null) {
-              console.stopScreenRecording();
-            }
-          }
-          else {
-            myReceiver.cancel();
-          }
-
-          indicator.setText("Stopping...");
-
-          // Wait for an additional second to make sure that the command
-          // completed and screen recorder finishes writing the output
-          myCompletionLatch.await(1, TimeUnit.SECONDS);
-          break;
-        }
+        console.startEmulatorScreenRecording(getEmulatorScreenRecorderOptions(myHostRecordingFile, myOptions));
       }
-      catch (InterruptedException ignored) {
+      catch (Exception e) {
+        EventQueue.invokeLater(() -> showError("Unexpected error while launching screen recording", e));
+        return;
       }
-    }
-  }
-
-  @Override
-  public void onFinished() {
-    if (mHostTmpFileName != null) {
-      pullEmulatorRecording();
     }
     else {
-      pullRecording();
+      completionLatch = new CountDownLatch(1);
+      receiver = new CollectingOutputReceiver(completionLatch);
+      startDeviceRecording(receiver, completionLatch);
+    }
+
+    long start = System.currentTimeMillis();
+
+    CountDownLatch stoppingLatch = new CountDownLatch(1);
+    ScreenRecorderDialog dialog = new ScreenRecorderDialog("Screen Recorder", stoppingLatch::countDown);
+
+    EventQueue.invokeLater(() -> {
+      myDialogWrapper = dialog.createWrapper(myProject);
+      myDialogWrapper.show();
+    });
+
+    try {
+      while (!stoppingLatch.await(millisUntilNextSecondTick(start), TimeUnit.MILLISECONDS) &&
+             System.currentTimeMillis() - start < MAX_RECORDING_TIME_MILLIS &&
+             (completionLatch == null || completionLatch.getCount() > 0)) {
+        EventQueue.invokeLater(() -> dialog.setRecordingTimeMillis(System.currentTimeMillis() - start));
+
+        // If using emulator screen recording feature, stop the recording if the emulator dies.
+        if (console != null) { // Using emulator screen recording.
+          // Check if the emulator is still alive.
+          console = EmulatorConsole.getConsole(myDevice);
+          if (console == null) {
+            break; // Emulator has been terminated.
+          }
+        }
+      }
+
+      EventQueue.invokeLater(() -> dialog.setRecordingLabelText("Stopping..."));
+
+      stopRecording(receiver, completionLatch, console);
+    }
+    catch (InterruptedException e) {
+      stopRecording(receiver, null, console);
+      throw new ProcessCanceledException();
+    }
+    finally {
+      EventQueue.invokeLater(() -> myDialogWrapper.close(DialogWrapper.CLOSE_EXIT_CODE));
+    }
+
+    if (receiver != null) {
+      pullRecording(receiver);
+    }
+    else {
+      pullEmulatorRecording();
+    }
+  }
+
+  private long millisUntilNextSecondTick(long start) {
+    return 1000 - (System.currentTimeMillis() - start) % 1000;
+  }
+
+  private void startDeviceRecording(@NotNull CollectingOutputReceiver receiver, @NotNull CountDownLatch completionLatch) {
+    // The IDevice.startScreenRecorder method is blocking, so execute it asynchronously.
+    ApplicationManager.getApplication().executeOnPooledThread(() -> {
+      // Store the temp media file in the respective AVD folder.
+      try {
+        myDevice.startScreenRecorder(ScreenRecorderAction.REMOTE_PATH, myOptions, receiver);
+      }
+      catch (Exception e) {
+        completionLatch.countDown();
+        EventQueue.invokeLater(() -> showError("Unexpected error while launching screen recording", e));
+      }
+    });
+  }
+
+  private void stopRecording(@Nullable CollectingOutputReceiver receiver, @Nullable CountDownLatch completionLatch,
+                             @Nullable EmulatorConsole console) {
+    if (receiver != null) {
+      receiver.cancel();
+      if (completionLatch != null) {
+        try {
+          // Wait for an additional second to make sure that the command
+          // completed and screen recorder finishes writing the output.
+          completionLatch.await(1, TimeUnit.SECONDS);
+        }
+        catch (InterruptedException e) {
+          throw new ProcessCanceledException();
+        }
+      }
+    }
+    else if (console != null) {
+      console.stopScreenRecording();
     }
   }
 
   private void pullEmulatorRecording() {
-    FileSaverDescriptor descriptor = new FileSaverDescriptor("Save As", "", "webm");
-    FileSaverDialog saveFileDialog = FileChooserFactory.getInstance().createSaveFileDialog(descriptor, myProject);
-    VirtualFile baseDir = VfsUtil.getUserHomeDir();
-    VirtualFileWrapper fileWrapper = saveFileDialog.save(baseDir, getDefaultFileName(".webm"));
-    if (fileWrapper == null) {
+    assert myHostRecordingFile != null;
+    if (!Files.exists(myHostRecordingFile)) {
       return;
     }
 
-    try {
-      assert mHostTmpFileName != null;
-      Files.copy(mHostTmpFileName, fileWrapper.getFile().toPath(), StandardCopyOption.COPY_ATTRIBUTES, StandardCopyOption.REPLACE_EXISTING);
-    }
-    catch (IOException e) {
-      ScreenRecorderAction.showError(myProject, "Unable to copy file to destination", e);
-    }
+    EventQueue.invokeLater(() -> {
+      VirtualFileWrapper fileWrapper = getTargetFile("webm");
+      if (fileWrapper == null) {
+        return;
+      }
+
+      try {
+        Files.move(myHostRecordingFile, fileWrapper.getFile().toPath(), REPLACE_EXISTING);
+      }
+      catch (IOException e) {
+        showError("Unable to copy file to destination", e);
+      }
+    });
   }
 
-  private void pullRecording() {
-    // If the receiver failed to record due to unsupported screen resolution
-    if (myReceiver.getOutput().contains(MEDIA_UNSUPPORTED_ERROR)) {
-      Messages.showErrorDialog(myReceiver.getOutput(), "Screen Recorder Error");
-      return;
-    }
-    FileSaverDescriptor descriptor = new FileSaverDescriptor("Save As", "", "mp4");
-    FileSaverDialog saveFileDialog = FileChooserFactory.getInstance().createSaveFileDialog(descriptor, myProject);
-    VirtualFile baseDir = VfsUtil.getUserHomeDir();
-    VirtualFileWrapper fileWrapper = saveFileDialog.save(baseDir, getDefaultFileName(".mp4"));
-    if (fileWrapper == null) {
-      return;
-    }
+  private void pullRecording(@NotNull CollectingOutputReceiver receiver) {
+    EventQueue.invokeLater(() -> {
+      // If the receiver failed to record due to unsupported screen resolution.
+      if (receiver.getOutput().contains(MEDIA_UNSUPPORTED_ERROR)) {
+        Messages.showErrorDialog(receiver.getOutput(), "Screen Recorder Error");
+        return;
+      }
+      VirtualFileWrapper fileWrapper = getTargetFile("mp4");
+      if (fileWrapper == null) {
+        return;
+      }
 
-    File f = fileWrapper.getFile();
-
-    new PullRecordingTask(myProject, myDevice, f.getAbsolutePath()).queue();
+      File file = fileWrapper.getFile();
+      new PullRecordingTask(myProject, myDevice, file.getAbsolutePath()).queue();
+    });
   }
 
-  private static String getDefaultFileName(String extension) {
+  private @Nullable VirtualFileWrapper getTargetFile(@NotNull String extension) {
+    FileSaverDescriptor descriptor = new FileSaverDescriptor("Save As", "", extension);
+    FileSaverDialog saveFileDialog = FileChooserFactory.getInstance().createSaveFileDialog(descriptor, myProject);
+    VirtualFile baseDir = VfsUtil.getUserHomeDir();
+    return saveFileDialog.save(baseDir, getDefaultFileName(extension));
+  }
+
+  @UiThread
+  private void showError(@NotNull String message, @Nullable Throwable throwable) {
+    ScreenRecorderAction.showError(myProject, message, throwable);
+  }
+
+  private static @NotNull String getDefaultFileName(@NotNull String extension) {
     Calendar now = Calendar.getInstance();
     String fileName = "device-%tF-%tH%tM%tS";
-    // add extension to filename on Mac only see: b/38447816
-    return String.format(Locale.US, SystemInfo.isMac ? fileName + extension : fileName, now, now, now, now);
+    // Add extension to filename on Mac only see: b/38447816.
+    return String.format(Locale.US, SystemInfo.isMac ? fileName + '.' + extension : fileName, now, now, now, now);
+  }
+
+  @VisibleForTesting
+  static @NotNull String getEmulatorScreenRecorderOptions(@NotNull Path filePath, @NotNull ScreenRecorderOptions options) {
+    StringBuilder sb = new StringBuilder();
+
+    if (options.width > 0 && options.height > 0) {
+      sb.append("--size ");
+      sb.append(options.width);
+      sb.append('x');
+      sb.append(options.height);
+      sb.append(' ');
+    }
+
+    if (options.bitrateMbps > 0) {
+      sb.append("--bit-rate ");
+      sb.append(options.bitrateMbps * 1000000);
+      sb.append(' ');
+    }
+
+    if (options.timeLimit > 0) {
+      sb.append("--time-limit ");
+      long seconds = TimeUnit.SECONDS.convert(options.timeLimit, options.timeLimitUnits);
+      if (seconds > 180) {
+        seconds = 180;
+      }
+      sb.append(seconds);
+      sb.append(' ');
+    }
+
+    sb.append(filePath);
+
+    return sb.toString();
   }
 }
