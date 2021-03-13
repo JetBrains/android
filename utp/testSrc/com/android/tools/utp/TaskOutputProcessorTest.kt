4ac6990f31b730fc2d6c63477fa192db309b9a0d
Yuki Hamada <hummer@google.com>
2021-03-12 18:38:55 -0800

    Propagate UTP test status event into the Test Matrix view
    Bug: n/a
    Test: unit tests and tested manually
    Change-Id: Ibbf71cc4ca956199b96380b34bd99ff41392294c

--- utp/testSrc/com/android/tools/utp/TaskOutputProcessorTest.kt
+++ utp/testSrc/com/android/tools/utp/TaskOutputProcessorTest.kt
@@ -15,7 +15,14 @@
  */
 package com.android.tools.utp
 
+import com.android.testutils.MockitoKt.eq
+import com.android.tools.utp.plugins.result.listener.gradle.proto.GradleAndroidTestResultListenerProto
 import com.google.common.truth.Truth.assertThat
+import com.google.protobuf.Any
+import com.google.protobuf.GeneratedMessageV3
+import com.google.testing.platform.proto.api.core.TestCaseProto
+import com.google.testing.platform.proto.api.core.TestResultProto
+import com.google.testing.platform.proto.api.core.TestSuiteResultProto
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -25,6 +32,7 @@ import org.mockito.Mockito.inOrder
 import org.mockito.Mockito.verifyNoInteractions
 import org.mockito.junit.MockitoJUnit
 import org.mockito.junit.MockitoRule
+import java.util.Base64
 
 /**
  * Unit tests for [TaskOutputProcessor].
@@ -44,7 +52,7 @@ class TaskOutputProcessorTest {
       There are no UTP test result tag in this test input.
       So the input text should be returned as-is.
       """.trimIndent()
-    val processor = TaskOutputProcessor(listOf(mockListener))
+    val processor = TaskOutputProcessor(mapOf("" to mockListener))
 
     val processed = processor.process(input)
 
@@ -56,14 +64,14 @@ class TaskOutputProcessorTest {
   fun processWithUtpTag() {
     val input = """
       Connected to process 6763 on device 'Pixel_3a_XL_API_28 [emulator-5554]'.
-      <UTP_TEST_RESULT_ON_TEST_RESULT_EVENT>CgAqDWVtdWxhdG9yLTU1NTQ=</UTP_TEST_RESULT_ON_TEST_RESULT_EVENT>
-      <UTP_TEST_RESULT_ON_TEST_RESULT_EVENT>EgAqDWVtdWxhdG9yLTU1NTQ=</UTP_TEST_RESULT_ON_TEST_RESULT_EVENT>
-      <UTP_TEST_RESULT_ON_TEST_RESULT_EVENT>GgAqDWVtdWxhdG9yLTU1NTQ=</UTP_TEST_RESULT_ON_TEST_RESULT_EVENT>
-      <UTP_TEST_RESULT_ON_TEST_RESULT_EVENT>IgAqDWVtdWxhdG9yLTU1NTQ=</UTP_TEST_RESULT_ON_TEST_RESULT_EVENT>
+      ${testSuiteStartedEvent()}
+      ${testCaseStartedEvent()}
+      ${testCaseFinishedEvent()}
+      ${testSuiteFinishedEvent()}
       <UTP_TEST_RESULT_ON_COMPLETED />
       > Task :app:connectedDebugAndroidTest
       """.trimIndent()
-    val processor = TaskOutputProcessor(listOf(mockListener))
+    val processor = TaskOutputProcessor(mapOf("" to mockListener))
 
     val processed = processor.process(input)
 
@@ -73,12 +81,59 @@ class TaskOutputProcessorTest {
     """.trimIndent())
 
     inOrder(mockListener).apply {
-      verify(mockListener).onTestSuiteStarted()
-      verify(mockListener).onTestCaseStarted()
-      verify(mockListener).onTestCaseFinished()
-      verify(mockListener).onTestSuiteFinished()
+      verify(mockListener).onTestSuiteStarted(eq(TestSuiteResultProto.TestSuiteMetaData.newBuilder().apply {
+        scheduledTestCaseCount = 1
+      }.build()))
+      verify(mockListener).onTestCaseStarted(eq(TestCaseProto.TestCase.getDefaultInstance()))
+      verify(mockListener).onTestCaseFinished(eq(TestResultProto.TestResult.getDefaultInstance()))
+      verify(mockListener).onTestSuiteFinished(eq(TestSuiteResultProto.TestSuiteResult.getDefaultInstance()))
       verify(mockListener).onComplete()
       verifyNoMoreInteractions()
     }
+  }
+
+  private fun testSuiteStartedEvent(): String {
+    return GradleAndroidTestResultListenerProto.TestResultEvent.newBuilder().apply {
+      testSuiteStartedBuilder.apply {
+        testSuiteMetadata = Any.pack(TestSuiteResultProto.TestSuiteMetaData.newBuilder().apply {
+          scheduledTestCaseCount = 1
+        }.build())
+      }
+    }.build().toXml()
+  }
+
+  private fun testCaseStartedEvent(): String {
+    return GradleAndroidTestResultListenerProto.TestResultEvent.newBuilder().apply {
+      testCaseStartedBuilder.apply {
+        testCase = Any.pack(TestCaseProto.TestCase.newBuilder().apply {
+        }.build())
+      }
+    }.build().toXml()
+  }
+
+  private fun testCaseFinishedEvent(): String {
+    return GradleAndroidTestResultListenerProto.TestResultEvent.newBuilder().apply {
+      testCaseFinishedBuilder.apply {
+        testCaseResult = Any.pack(TestResultProto.TestResult.newBuilder().apply {
+        }.build())
+      }
+    }.build().toXml()
+  }
+
+  private fun testSuiteFinishedEvent(): String {
+    return GradleAndroidTestResultListenerProto.TestResultEvent.newBuilder().apply {
+      testSuiteFinishedBuilder.apply {
+        testSuiteResult = Any.pack(TestSuiteResultProto.TestSuiteResult.newBuilder().apply {
+        }.build())
+      }
+    }.build().toXml()
+  }
+
+  private fun GradleAndroidTestResultListenerProto.TestResultEvent.toXml(): String {
+    return "<UTP_TEST_RESULT_ON_TEST_RESULT_EVENT>${toBase64String()}</UTP_TEST_RESULT_ON_TEST_RESULT_EVENT>"
+  }
+
+  private fun GeneratedMessageV3.toBase64String(): String {
+    return Base64.getEncoder().encodeToString(toByteArray())
   }
 }
