e75d9795bc5edfabb7b9098aa5dc6dc3cfb40169
Juan C Nuno <juancnuno@google.com>
2021-03-12 19:49:15 -0800

    Add PhysicalDeviceTableModel
    Bug: 157851487
    Test: PhysicalDeviceTableModelTest
    Change-Id: I919bf5f7705104e4a54100072da4dc51f16355b9
    
    GitOrigin-RevId: 38322845cb5dae3aaa1e200aac07d7799069fff9

---
+++ android/src/com/android/tools/idea/deviceManager/physicaltab/PhysicalDeviceTableModel.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright (C) 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.deviceManager.physicaltab;
+
+import java.util.List;
+import javax.swing.table.AbstractTableModel;
+import org.jetbrains.annotations.NotNull;
+
+final class PhysicalDeviceTableModel extends AbstractTableModel {
+  private static final int DEVICE_MODEL_COLUMN_INDEX = 0;
+  private static final int API_MODEL_COLUMN_INDEX = 1;
+  private static final int TYPE_MODEL_COLUMN_INDEX = 2;
+  private static final int ACTIONS_MODEL_COLUMN_INDEX = 3;
+
+  private final @NotNull List<@NotNull PhysicalDevice> myDevices;
+
+  PhysicalDeviceTableModel(@NotNull List<@NotNull PhysicalDevice> devices) {
+    myDevices = devices;
+  }
+
+  @Override
+  public int getRowCount() {
+    return myDevices.size();
+  }
+
+  @Override
+  public int getColumnCount() {
+    return 4;
+  }
+
+  @Override
+  public @NotNull String getColumnName(int modelColumnIndex) {
+    switch (modelColumnIndex) {
+      case DEVICE_MODEL_COLUMN_INDEX:
+        return "Device";
+      case API_MODEL_COLUMN_INDEX:
+        return "API";
+      case TYPE_MODEL_COLUMN_INDEX:
+        return "Type";
+      case ACTIONS_MODEL_COLUMN_INDEX:
+        return "Actions";
+      default:
+        throw new AssertionError(modelColumnIndex);
+    }
+  }
+
+  @Override
+  public @NotNull Object getValueAt(int modelRowIndex, int modelColumnIndex) {
+    switch (modelColumnIndex) {
+      case DEVICE_MODEL_COLUMN_INDEX:
+        return myDevices.get(modelRowIndex);
+      case API_MODEL_COLUMN_INDEX:
+        return "API";
+      case TYPE_MODEL_COLUMN_INDEX:
+        return "Type";
+      case ACTIONS_MODEL_COLUMN_INDEX:
+        // TODO You can probably throw an exception here too
+        return "Actions";
+      default:
+        throw new AssertionError(modelColumnIndex);
+    }
+  }
+}
---
+++ android/testSrc/com/android/tools/idea/deviceManager/physicaltab/PhysicalDeviceTableModelTest.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.deviceManager.physicaltab;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Collections;
+import javax.swing.table.TableModel;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public final class PhysicalDeviceTableModelTest {
+  private static final PhysicalDevice DEVICE = new PhysicalDevice("86UX00F4R");
+  private final TableModel myModel = new PhysicalDeviceTableModel(Collections.singletonList(DEVICE));
+
+  @Test
+  public void getRowCount() {
+    // Act
+    int count = myModel.getRowCount();
+
+    // Assert
+    assertEquals(1, count);
+  }
+
+  @Test
+  public void getValueAt() {
+    // Act
+    Object value = myModel.getValueAt(0, 0);
+
+    // Assert
+    assertEquals(DEVICE, value);
+  }
+}
