c960bc159d09ebaac905f13c73715147ffd33df0
Leonid Bushuev <leonidos@gmail.com>
2021-03-18 00:06:26 +0300

    Refactor database context functions: separate them from DatabaseView
    GitOrigin-RevId: ecc29be21f5f01219149a7590cbd9071ea83328b

--- ultimate/src/org/jetbrains/android/database/AndroidUploadDatabaseAction.java
+++ ultimate/src/org/jetbrains/android/database/AndroidUploadDatabaseAction.java
@@ -1,7 +1,7 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.android.database;
 
-import static com.intellij.database.view.DatabaseView.getSelectedElements;
+import static com.intellij.database.view.DatabaseContextFun.getSelectedElements;
 
 import com.android.ddmlib.AndroidDebugBridge;
 import com.android.ddmlib.IDevice;
