657ee76430e1d3c2a376a803fb2190143c5e2a18
Christophe Rhodes <xof@google.com>
2021-03-18 16:44:13 +0000

    [AUA] Test the initial state of the treeModel
    In order to test the state meaningfully, we need some active refactoring
    processors, which means we need some non-empty files in the project.
    Change the way we set up these tests to make that a bit more
    straightforward, using AndroidProjectRule rather than
    LightFixtureTestCase.  Then test that if we have a build.gradle file in
    the root of the project with an upgradable AGP classpath declaration, we
    see an active MANDATORY_CODEPENDENT processor with the right title in
    the treeModel.
    
    Bug: 183107211
    Test: added here
    Change-Id: Ib35a25d2046eb69ef1a3d46ac22e1c62661cd51d
    
    GitOrigin-RevId: cae455d01d7eac0db7a8732df6ada05835c46eea

--- android/testSrc/com/android/tools/idea/gradle/project/upgrade/ContentManagerTest.kt
+++ android/testSrc/com/android/tools/idea/gradle/project/upgrade/ContentManagerTest.kt
@@ -17,52 +17,105 @@ package com.android.tools.idea.gradle.project.upgrade
 
 import com.android.ide.common.repository.GradleVersion
 import com.android.tools.idea.gradle.plugin.LatestKnownPluginVersionProvider
+import com.android.tools.idea.gradle.project.upgrade.AgpUpgradeComponentNecessity.MANDATORY_CODEPENDENT
+import com.android.tools.idea.testing.AndroidProjectRule
+import com.android.tools.idea.testing.onEdt
 import com.google.common.truth.Truth.assertThat
 import com.intellij.testFramework.LightPlatformTestCase
+import com.intellij.testFramework.RunsInEdt
+import com.intellij.ui.CheckedTreeNode
+import org.apache.commons.io.FileSystem
+import org.jetbrains.kotlin.idea.util.application.runWriteAction
+import org.junit.Rule
+import org.junit.Test
 
-class ContentManagerTest : LightPlatformTestCase() {
+@RunsInEdt
+class ContentManagerTest {
   val currentAgpVersion by lazy { GradleVersion.parse("4.1.0") }
-  val latestAgpVersion  by lazy { GradleVersion.parse(LatestKnownPluginVersionProvider.INSTANCE.get()) }
+  val latestAgpVersion by lazy { GradleVersion.parse(LatestKnownPluginVersionProvider.INSTANCE.get()) }
 
+  @get:Rule
+  val projectRule = AndroidProjectRule.withSdk().onEdt()
+
+  val project by lazy { projectRule.project }
+
+  @Test
   fun testContentManagerConstructable() {
     val contentManager = ContentManager(project)
   }
 
+  @Test
   fun testToolWindowModelConstructable() {
     val toolWindowModel = ToolWindowModel(project, currentAgpVersion)
   }
 
+  @Test
   fun testToolWindowModelStartsWithLatestAgpVersionSelected() {
     val toolWindowModel = ToolWindowModel(project, currentAgpVersion)
     assertThat(toolWindowModel.selectedVersion.valueOrNull).isEqualTo(latestAgpVersion)
   }
 
+  @Test
   fun testToolWindowModelStartsWithValidProcessor() {
     val toolWindowModel = ToolWindowModel(project, currentAgpVersion)
     assertThat(toolWindowModel.processor?.current).isEqualTo(currentAgpVersion)
     assertThat(toolWindowModel.processor?.new).isEqualTo(latestAgpVersion)
   }
 
+  @Test
   fun testToolWindowModelStartsEnabled() {
     val toolWindowModel = ToolWindowModel(project, currentAgpVersion)
     assertThat(toolWindowModel.runEnabled.get()).isTrue()
     assertThat(toolWindowModel.runDisabledTooltip.get()).isEmpty()
   }
 
+  @Test
   fun testToolWindowModelIsNotLoading() {
     val toolWindowModel = ToolWindowModel(project, currentAgpVersion)
     assertThat(toolWindowModel.showLoadingState.get()).isFalse()
   }
 
+  @Test
   fun testToolWindowModelIsNotEnabledForNullSelectedVersion() {
     val toolWindowModel = ToolWindowModel(project, currentAgpVersion)
     toolWindowModel.selectedVersion.clear()
     assertThat(toolWindowModel.runEnabled.get()).isFalse()
   }
 
+  @Test
   fun testToolWindowModelIsNotLoadingForNullSelectedVersion() {
     val toolWindowModel = ToolWindowModel(project, currentAgpVersion)
     toolWindowModel.selectedVersion.clear()
     assertThat(toolWindowModel.showLoadingState.get()).isFalse()
+  }
+
+  @Test
+  fun testTreeModelInitialState() {
+    projectRule.fixture.addFileToProject(
+      "build.gradle",
+      """
+        buildscript {
+          dependencies {
+            classpath 'com.android.tools.build:gradle:$currentAgpVersion'
+          }
+        }
+      """.trimIndent()
+    )
+    val toolWindowModel = ToolWindowModel(project, currentAgpVersion)
+    val treeModel = toolWindowModel.treeModel
+    val root = treeModel.root as? CheckedTreeNode
+    assertThat(root).isInstanceOf(CheckedTreeNode::class.java)
+    assertThat(root!!.childCount).isEqualTo(1)
+    val mandatoryCodependent = root.firstChild as CheckedTreeNode
+    assertThat(mandatoryCodependent.userObject).isEqualTo(MANDATORY_CODEPENDENT)
+    assertThat(mandatoryCodependent.isEnabled).isTrue()
+    assertThat(mandatoryCodependent.isChecked).isTrue()
+    assertThat(mandatoryCodependent.childCount).isEqualTo(1)
+    val step = mandatoryCodependent.firstChild as CheckedTreeNode
+    assertThat(step.isEnabled).isFalse()
+    assertThat(step.isChecked).isTrue()
+    val stepPresentation = step.userObject as ToolWindowModel.DefaultStepPresentation
+    assertThat(stepPresentation.processor).isInstanceOf(AgpClasspathDependencyRefactoringProcessor::class.java)
+    assertThat(stepPresentation.treeText).contains("Upgrade AGP dependency from $currentAgpVersion to $latestAgpVersion")
   }
 }
