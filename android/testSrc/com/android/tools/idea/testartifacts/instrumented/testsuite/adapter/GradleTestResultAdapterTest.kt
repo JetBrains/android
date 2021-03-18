ca70759d847c486cebd009f863a6d3bd26bb7fbe
Yahan Zhou <yahan@google.com>
2021-03-18 15:19:32 -0700

    Parse retention results in GradleTestResultAdapter
    Bug: 182812960
    Test: GradleTestResultAdapterTest
    Change-Id: I006eae42b6579fc8f7c1dafffff32c8c449a4abd
    
    GitOrigin-RevId: baac2c097839f610c5c36829ebfcbcce186a96d2

--- android/testSrc/com/android/tools/idea/testartifacts/instrumented/testsuite/adapter/GradleTestResultAdapterTest.kt
+++ android/testSrc/com/android/tools/idea/testartifacts/instrumented/testsuite/adapter/GradleTestResultAdapterTest.kt
@@ -26,6 +26,9 @@ import com.android.tools.idea.testartifacts.instrumented.testsuite.model.Android
 import com.android.tools.idea.testartifacts.instrumented.testsuite.model.AndroidTestCaseResult
 import com.android.tools.idea.testartifacts.instrumented.testsuite.model.AndroidTestSuiteResult
 import com.google.common.truth.Truth.assertThat
+import com.google.testing.platform.proto.api.core.LabelProto
+import com.google.testing.platform.proto.api.core.PathProto
+import com.google.testing.platform.proto.api.core.TestArtifactProto
 import com.google.testing.platform.proto.api.core.TestCaseProto
 import com.google.testing.platform.proto.api.core.TestResultProto
 import com.google.testing.platform.proto.api.core.TestStatusProto
@@ -172,4 +175,79 @@ class GradleTestResultAdapterTest {
       it.id.isNotBlank() && it.name == "testName" && it.testCaseCount == 1 && it.result == AndroidTestSuiteResult.FAILED
     })
   }
+
+
+  @Test
+  fun runTestSuiteWithRetention() {
+    val adapter = GradleTestResultAdapter(mockDevice1, "testName", mockListener)
+    val iceboxInfoPath = "icebox-info.pb"
+    val iceboxSnapshotPath = "icebox-snapshot.tar"
+
+    verify(mockListener).onTestSuiteScheduled(eq(adapter.device))
+
+    adapter.onTestSuiteStarted(TestSuiteResultProto.TestSuiteMetaData.newBuilder().apply {
+      scheduledTestCaseCount = 1
+    }.build())
+
+    verify(mockListener).onTestSuiteStarted(eq(adapter.device), argThat {
+      it.id.isNotBlank() && it.name == "testName" && it.testCaseCount == 1 && it.result == null
+    })
+
+    adapter.onTestCaseStarted(TestCaseProto.TestCase.newBuilder().apply {
+      testPackage = "com.example.test"
+      testClass = "ExampleTest"
+      testMethod = "testExample"
+    }.build())
+
+    verify(mockListener).onTestCaseStarted(eq(adapter.device), any(), argThat {
+      it.packageName == "com.example.test" && it.className == "ExampleTest" &&
+      it.methodName == "testExample" && it.result == AndroidTestCaseResult.IN_PROGRESS
+    })
+
+    adapter.onTestCaseFinished(TestResultProto.TestResult.newBuilder().apply {
+      testCaseBuilder.apply {
+        testPackage = "com.example.test"
+        testClass = "ExampleTest"
+        testMethod = "testExample"
+      }
+      testStatus = TestStatusProto.TestStatus.FAILED
+      errorBuilder.apply {
+        errorMessage = "ErrorStackTrace"
+      }
+      addOutputArtifact(TestArtifactProto.Artifact.newBuilder().apply {
+        label = LabelProto.Label.newBuilder().apply {
+          namespace = "android"
+          label = "icebox.info"
+        }.build()
+        sourcePath = PathProto.Path.newBuilder().apply {
+          path = iceboxInfoPath
+        }.build()
+      })
+      addOutputArtifact(TestArtifactProto.Artifact.newBuilder().apply {
+        label = LabelProto.Label.newBuilder().apply {
+          namespace = "android"
+          label = "icebox.snapshot"
+        }.build()
+        sourcePath = PathProto.Path.newBuilder().apply {
+          path = iceboxSnapshotPath
+        }.build()
+      })
+    }.build())
+
+    verify(mockListener).onTestCaseFinished(eq(adapter.device), any(), argThat {
+      it.packageName == "com.example.test" && it.className == "ExampleTest" &&
+      it.methodName == "testExample" && it.result == AndroidTestCaseResult.FAILED &&
+      it.errorStackTrace == "ErrorStackTrace" &&
+      it.retentionInfo?.path == iceboxInfoPath && it.retentionSnapshot?.path == iceboxSnapshotPath
+    })
+
+    adapter.onTestSuiteFinished(TestSuiteResultProto.TestSuiteResult.newBuilder().apply {
+      testStatus = TestStatusProto.TestStatus.FAILED
+    }.build())
+
+    verify(mockListener).onTestSuiteFinished(eq(adapter.device), argThat {
+      it.id.isNotBlank() && it.name == "testName" && it.testCaseCount == 1 && it.result == AndroidTestSuiteResult.FAILED
+    })
+  }
+
 }
