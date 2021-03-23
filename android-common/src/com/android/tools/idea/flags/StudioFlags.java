ef39a696a809fdb9d9e9c3b48aa318f9334ed2f2
Charlie Tsai <chartsai@google.com>
2021-03-23 13:07:59 +0000

    Enable animated selector preview
    Bug: 157454678
    Test: N/A
    Change-Id: I0f64abfb07c470d9523f7cf6b9be1444c405a331
    
    GitOrigin-RevId: 8a9b6bfbeb2389ac0c0814f6eb2b563b67588420

--- android-common/src/com/android/tools/idea/flags/StudioFlags.java
+++ android-common/src/com/android/tools/idea/flags/StudioFlags.java
@@ -198,7 +198,7 @@ public final class StudioFlags {
   public static final Flag<Boolean> NELE_ANIMATED_SELECTOR_PREVIEW = Flag.create(
     NELE, "animated.selector.preview", "Show preview animations toolbar for animated selector",
     "Show an animations bar that allows playback of transitions in animated selector.",
-    false);
+    true);
   public static final Flag<Boolean> NELE_MOTION_LAYOUT_ANIMATIONS = Flag.create(
     NELE, "animated.motion.layout", "Show preview animations toolbar for MotionLayout",
     "Show an animations bar that allows playback of MotionLayout animations.",
