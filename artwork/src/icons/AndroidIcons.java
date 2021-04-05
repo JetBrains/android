e56f3a9e8992f0693001f8adccbdb4f80a36307e
Chris Sinco <csinco@google.com>
2021-04-04 22:31:04 -0700

    Remove unused assets from AndroidIcons
    Test: Visual inspection of local build
    Bug: n/a
    Change-Id: If8879eece067c302faa2e061392052b418e14e08

--- artwork/src/icons/AndroidIcons.java
+++ artwork/src/icons/AndroidIcons.java
@@ -16,16 +16,6 @@ public class AndroidIcons {
     public static final Icon StudioProductIcon = load("/icons/wizards/studio_product.svg"); // 64x64
     // Template thumbnails
     public static final Icon AndroidModule = load("/icons/wizards/android_module.png"); // 256x256
-    public static final Icon AndroidNativeModule = load("/icons/wizards/cpp_configure.png"); // 256x256
-    public static final Icon AutomotiveModule = load("/icons/wizards/automotive_module.png"); // 256x256
-    public static final Icon BenchmarkModule = load("/icons/wizards/benchmark_module.png"); // 256x256
-    public static final Icon DynamicFeatureModule = load("/icons/wizards/dynamic_feature_module.png"); // 256x256
-    public static final Icon EclipseModule = load("/icons/wizards/eclipse_module.png"); // 256x256
-    public static final Icon GradleModule = load("/icons/wizards/gradle_module.png"); // 256x256
-    public static final Icon InstantDynamicFeatureModule = load("/icons/wizards/instant_dynamic_feature_module.png"); // 256x256
-    public static final Icon MobileModule = load("/icons/wizards/mobile_module.png"); // 256x256
-    public static final Icon TvModule = load("/icons/wizards/tv_module.png"); // 256x256
-    public static final Icon WearModule = load("/icons/wizards/wear_module.png"); // 256x256
     public static final Icon NoActivity = load("/icons/wizards/no_activity.png"); // 256x256
   }
 
