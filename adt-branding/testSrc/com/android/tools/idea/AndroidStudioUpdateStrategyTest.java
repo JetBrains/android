f2f6b379912523687f2fae68cafc911390ee5195
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2021-04-06 22:41:26 +0200

    [platform] skipping irrelevant IDE update data
    GitOrigin-RevId: 91c4baaeb3a1b12bd854579e748d1f21a861d64e

--- adt-branding/testSrc/com/android/tools/idea/AndroidStudioUpdateStrategyTest.java
+++ adt-branding/testSrc/com/android/tools/idea/AndroidStudioUpdateStrategyTest.java
@@ -16,13 +16,13 @@
 package com.android.tools.idea;
 
 import static com.google.common.truth.Truth.assertThat;
+import static com.intellij.openapi.updateSettings.impl.UpdateData.parseUpdateData;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import com.intellij.openapi.updateSettings.impl.ChannelStatus;
-import com.intellij.openapi.updateSettings.impl.UpdateStrategy;
-import com.intellij.openapi.updateSettings.impl.UpdatesInfo;
 import com.intellij.openapi.updateSettings.impl.UpdateSettings;
+import com.intellij.openapi.updateSettings.impl.UpdateStrategy;
 import com.intellij.openapi.util.BuildNumber;
 import com.intellij.openapi.util.JDOMUtil;
 import com.intellij.testFramework.LightIdeaTestCase;
@@ -48,7 +48,7 @@ public final class AndroidStudioUpdateStrategyTest extends LightIdeaTestCase {
     UpdateSettings settings = mock(UpdateSettings.class);
     when(settings.getSelectedChannelStatus()).thenReturn(ChannelStatus.EAP);
 
-    assertThat(new UpdateStrategy(BuildNumber.fromString("AI-182.4505.22.34.5070326"), new UpdatesInfo(JDOMUtil.load(updatesXml)), settings)
+    assertThat(new UpdateStrategy(BuildNumber.fromString("AI-182.4505.22.34.5070326"), parseUpdateData(JDOMUtil.load(updatesXml), "AI"), settings)
       .checkForUpdates().getNewBuild().getNumber().asString())
       .isEqualTo("AI-183.2153.8.34.5078398");  // not AI-182.4892.20.33.5078385. This scenario is taken directly from b/117996392.
   }
@@ -67,7 +67,7 @@ public final class AndroidStudioUpdateStrategyTest extends LightIdeaTestCase {
     UpdateSettings settings = mock(UpdateSettings.class);
     when(settings.getSelectedChannelStatus()).thenReturn(ChannelStatus.EAP);
 
-    assertThat(new UpdateStrategy(BuildNumber.fromString("AI-191.7479.19.36.5721125"), new UpdatesInfo(JDOMUtil.load(updatesXml)), settings)
+    assertThat(new UpdateStrategy(BuildNumber.fromString("AI-191.7479.19.36.5721125"), parseUpdateData(JDOMUtil.load(updatesXml), "AI"), settings)
       .checkForUpdates().getNewBuild()).isNull();  // not AI-191.8026.42.35.5781497. This scenario is taken directly from b/139118534.
   }
 
@@ -89,7 +89,7 @@ public final class AndroidStudioUpdateStrategyTest extends LightIdeaTestCase {
     UpdateSettings settings = mock(UpdateSettings.class);
     when(settings.getSelectedChannelStatus()).thenReturn(ChannelStatus.EAP);
 
-    assertThat(new UpdateStrategy(BuildNumber.fromString("AI-191.7479.19.35.5717577"), new UpdatesInfo(JDOMUtil.load(updatesXml)), settings)
+    assertThat(new UpdateStrategy(BuildNumber.fromString("AI-191.7479.19.35.5717577"), parseUpdateData(JDOMUtil.load(updatesXml), "AI"), settings)
       .checkForUpdates().getNewBuild().getNumber().asString())
       .isEqualTo("AI-191.7479.19.35.5763348");  // not AI-191.7479.19.36.5721125
   }
