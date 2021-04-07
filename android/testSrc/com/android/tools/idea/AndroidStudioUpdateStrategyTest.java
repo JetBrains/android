19a3a49766508d91d5f34d34aa610db8142e7aa4
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2021-04-07 10:02:52 +0200

    [android] making ADT branding tests pass in IDEA configurations
    (cherry picked from commit 05c0e8e8ba9ebd6dcc3ada061ba2ee2e40a051cd)
    
    Change-Id: I05c0e8e8ba9ebd6dcc3ada061ba2ee2e40a051cd

--- android/testSrc/com/android/tools/idea/AndroidStudioUpdateStrategyTest.java
+++ android/testSrc/com/android/tools/idea/AndroidStudioUpdateStrategyTest.java
@@ -25,12 +25,13 @@ import com.intellij.openapi.updateSettings.impl.UpdateStrategy;
 import com.intellij.openapi.updateSettings.impl.UpdateSettings;
 import com.intellij.openapi.util.BuildNumber;
 import com.intellij.openapi.util.JDOMUtil;
-import com.intellij.testFramework.PlatformTestCase;
 import org.intellij.lang.annotations.Language;
+import org.junit.Test;
 
 /** Test of {@link UpdateStrategy} with {@link AndroidStudioUpdateStrategyCustomization}. */
-public final class AndroidStudioUpdateStrategyTest extends PlatformTestCase {
+public final class AndroidStudioUpdateStrategyTest {
 
+  @Test
   public void testUpdateStrategyDoesNotPreferSamePlatformVersion() throws Exception {
     @Language("XML") String updatesXml =
       "<products>" +
@@ -48,11 +49,12 @@ public final class AndroidStudioUpdateStrategyTest extends PlatformTestCase {
     UpdateSettings settings = mock(UpdateSettings.class);
     when(settings.getSelectedChannelStatus()).thenReturn(ChannelStatus.EAP);
 
-    assertThat(new UpdateStrategy(BuildNumber.fromString("AI-182.4505.22.34.5070326"), UpdateData.parseUpdateData(JDOMUtil.load(updatesXml), "AI"), settings)
+    assertThat(updateStrategy("AI-182.4505.22.34.5070326", updatesXml, settings)
       .checkForUpdates().getNewBuild().getNumber().asString())
       .isEqualTo("AI-183.2153.8.34.5078398");  // not AI-182.4892.20.33.5078385. This scenario is taken directly from b/117996392.
   }
 
+  @Test
   public void testUpdateStrategyIgnoresLowerAndroidStudioVersion() throws Exception {
     @Language("XML") String updatesXml =
       "<products>" +
@@ -67,11 +69,12 @@ public final class AndroidStudioUpdateStrategyTest extends PlatformTestCase {
     UpdateSettings settings = mock(UpdateSettings.class);
     when(settings.getSelectedChannelStatus()).thenReturn(ChannelStatus.EAP);
 
-    assertThat(new UpdateStrategy(BuildNumber.fromString("AI-191.7479.19.36.5721125"), UpdateData.parseUpdateData(JDOMUtil.load(updatesXml), "AI"), settings)
+    assertThat(updateStrategy("AI-191.7479.19.36.5721125", updatesXml, settings)
       .checkForUpdates().getNewBuild()).isNull();  // not AI-191.8026.42.35.5781497. This scenario is taken directly from b/139118534.
   }
 
   // It's not clear this is important, but it's what overriding haveSameMajorVersion still does now that we also override isNewerVersion.
+  @Test
   public void testUpdateStrategyPrefersSameAndroidStudioVersion() throws Exception {
     @Language("XML") String updatesXml =
       "<products>" +
@@ -89,8 +92,16 @@ public final class AndroidStudioUpdateStrategyTest extends PlatformTestCase {
     UpdateSettings settings = mock(UpdateSettings.class);
     when(settings.getSelectedChannelStatus()).thenReturn(ChannelStatus.EAP);
 
-    assertThat(new UpdateStrategy(BuildNumber.fromString("AI-191.7479.19.35.5717577"), UpdateData.parseUpdateData(JDOMUtil.load(updatesXml), "AI"), settings)
+    assertThat(updateStrategy("AI-191.7479.19.35.5717577", updatesXml, settings)
       .checkForUpdates().getNewBuild().getNumber().asString())
       .isEqualTo("AI-191.7479.19.35.5763348");  // not AI-191.7479.19.36.5721125
+  }
+
+  private static UpdateStrategy updateStrategy(String version, String updatesXml, UpdateSettings settings) throws Exception {
+    return new UpdateStrategy(
+      BuildNumber.fromString(version),
+      UpdateData.parseUpdateData(JDOMUtil.load(updatesXml), "AI"),
+      settings,
+      new AndroidStudioUpdateStrategyCustomization());
   }
 }
