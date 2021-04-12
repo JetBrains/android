7611bbdb11f2c5cd8aa5c5d367498a8b24c831ea
Jerome Gaillard <jgaillard@google.com>
2021-04-12 16:13:27 +0100

    Add Visual linting to Layout Validation
    After updating its NlModels, Layout Validation performs an analysis of
    the RenderResults and display the issues found in its error panel.
    At the moment, the only analysis performed is checking that all views
    are fully contained within the bounds of their parents.
    
    Bug: 184734874
    Test: Checked manually, tests to be added in a subsequent CL
    Change-Id: I422175a3ed0730b58f37613ea9a73f49bd6e5c26
    
    GitOrigin-RevId: 68619445cc06d40b796fa25564962222e6deed47

---
+++ designer/src/com/android/tools/idea/uibuilder/visual/visuallint/VisualLintAnalysis.kt
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.uibuilder.visual.visuallint
+
+import com.android.ide.common.rendering.api.ViewInfo
+import com.android.tools.idea.rendering.RenderResult
+import com.android.tools.idea.rendering.errors.ui.RenderErrorModel
+import com.google.common.collect.ImmutableList
+
+/**
+ * Returns all the [RenderErrorModel.Issue] found when analyzing the given [RenderResult].
+ */
+fun analyze(result: RenderResult): ImmutableList<RenderErrorModel.Issue> {
+  return ImmutableList.copyOf(analyzeBounds(result))
+}
+
+/**
+ * Analyze the given [RenderResult] for issues where a child view is not fully contained within
+ * the bounds of its parent, and return all such issues.
+ */
+private fun analyzeBounds(renderResult: RenderResult): List<RenderErrorModel.Issue> {
+  val issues = mutableListOf<RenderErrorModel.Issue>()
+  for (root in renderResult.rootViews) {
+    findBoundIssues(root, issues)
+  }
+  return issues
+}
+
+private fun findBoundIssues(root: ViewInfo, issues: MutableList<RenderErrorModel.Issue>) {
+  val bottom = root.bottom
+  val top = root.top
+  val left = root.left
+  val right = root.right
+  for (child in root.children) {
+    if (child.bottom > bottom || child.top < top || child.left < left || child.right > right) {
+      issues.add(RenderErrorModel.Issue.builder().setSummary("$child is not fully visible in layout").build())
+    }
+    findBoundIssues(child, issues)
+  }
+}
