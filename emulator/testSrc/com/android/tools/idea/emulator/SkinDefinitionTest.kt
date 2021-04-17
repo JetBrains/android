a65f462fcdc0ab2ac5fce873761459686a089723
Sergey Prigogin <sprigogin@google.com>
2021-04-16 17:06:06 -0700

    Add comprehensive checks of skin images
    Test: SkinDefinitionTest
    Bug: N/A
    Change-Id: Icc0b0300d6c6e9a83965df4ca8f50514d6a06a29
    
    GitOrigin-RevId: 7b94e6f7947287165f312ad50369533cafb76169

--- emulator/testSrc/com/android/tools/idea/emulator/SkinDefinitionTest.kt
+++ emulator/testSrc/com/android/tools/idea/emulator/SkinDefinitionTest.kt
@@ -16,19 +16,28 @@
 package com.android.tools.idea.emulator
 
 import com.android.emulator.control.Rotation.SkinRotation
+import com.android.io.readImage
 import com.android.testutils.ImageDiffUtil
 import com.android.testutils.TestUtils.getWorkspaceRoot
 import com.android.tools.adtui.ImageUtils
 import com.android.tools.adtui.webp.WebpMetadata
+import com.android.tools.idea.avdmanager.SkinLayoutDefinition
 import com.android.tools.idea.emulator.FakeEmulator.Companion.getSkinFolder
 import com.google.common.truth.Truth.assertThat
+import org.junit.Assert.fail
 import org.junit.Before
 import org.junit.Ignore
 import org.junit.Test
 import java.awt.Dimension
 import java.awt.Rectangle
+import java.awt.image.BufferedImage
 import java.awt.image.BufferedImage.TYPE_INT_ARGB
+import java.nio.charset.StandardCharsets
+import java.nio.file.Files
+import java.nio.file.NoSuchFileException
 import java.nio.file.Path
+import java.util.function.Consumer
+import java.util.function.Predicate
 
 /**
  * Tests for [SkinDefinition] and related classes.
@@ -178,8 +187,9 @@ class SkinDefinitionTest {
 
     // Check the skin layout and consistency of its images.
     val layout = skin.layout
+    validateLayoutConsistency(layout, folder)
     assertThat(layout.displaySize).isEqualTo(Dimension(1080, 2340))
-    assertThat(layout.frameRectangle.size).isEqualTo(Dimension(1195, 2473))
+    assertThat(layout.frameRectangle.size).isEqualTo(Dimension(1204, 2491))
     assertThat(layout.frameImages).hasSize(8)
     assertThat(layout.maskImages).hasSize(4) // Four round corners, one with a camera hole.
   }
@@ -192,8 +202,9 @@ class SkinDefinitionTest {
 
     // Check the skin layout and consistency of its images.
     val layout = skin.layout
+    validateLayoutConsistency(layout, folder)
     assertThat(layout.displaySize).isEqualTo(Dimension(1080, 2340))
-    assertThat(layout.frameRectangle.size).isEqualTo(Dimension(1196, 2441))
+    assertThat(layout.frameRectangle.size).isEqualTo(Dimension(1211, 2474))
     assertThat(layout.frameImages).hasSize(8)
     assertThat(layout.maskImages).hasSize(4) // Four round corners, one with a camera hole.
   }
@@ -218,18 +229,148 @@ class SkinDefinitionTest {
     assertSkinAppearance(layout, "wear_round")
   }
 
-  private fun assertSkinAppearance(layout: SkinLayout, goldenImageName: String) {
-    val frameRectangle = layout.frameRectangle
+  private fun validateLayoutConsistency(skinLayout: SkinLayout, skinFolder: Path) {
+    val image = skinLayout.draw()
+    val backgroundImage = readBackgroundImage(skinFolder)
+    val problems = mutableListOf<String>()
+    if (backgroundImage != null && (backgroundImage.width != image.width || backgroundImage.height != image.height)) {
+      problems.add("The background image can be cropped without loosing any information")
+    }
+
+    val start = Point(skinLayout.displaySize.width / 2 - skinLayout.frameRectangle.x,
+                      skinLayout.displaySize.height / 2 - skinLayout.frameRectangle.y)
+    if (!image.isTransparent(start)) {
+      fail("The skin image is not transparent near the center of the display")
+    }
+    val transparentAreaBounds = findBoundsOfContiguousArea(image, start, image::isTransparent)
+    if (transparentAreaBounds.width != skinLayout.displaySize.width) {
+      problems.add("The width of the display area in the skin image (${transparentAreaBounds.width})" +
+                   " doesn't match the layout file (${skinLayout.displaySize.width})")
+    }
+    if (transparentAreaBounds.height != skinLayout.displaySize.height) {
+      problems.add("The height of the display area in the skin image (${transparentAreaBounds.height})" +
+                   " doesn't match the layout file (${skinLayout.displaySize.height})")
+    }
+    val nonOpaqueAreaBounds = findBoundsOfContiguousArea(image, start, image::isNotOpaque)
+    if (nonOpaqueAreaBounds.x != transparentAreaBounds.x) {
+      problems.add("Partially transparent pixels near the left edge of the display area")
+    }
+    if (nonOpaqueAreaBounds.x + nonOpaqueAreaBounds.width != transparentAreaBounds.x +  + transparentAreaBounds.width) {
+      problems.add("Partially transparent pixels near the right edge of the display area")
+    }
+    if (nonOpaqueAreaBounds.y != transparentAreaBounds.y) {
+      problems.add("Partially transparent pixels near the top edge of the display area")
+    }
+    if (nonOpaqueAreaBounds.y + nonOpaqueAreaBounds.height != transparentAreaBounds.y +  + transparentAreaBounds.height) {
+      problems.add("Partially transparent pixels near the bottom edge of the display area")
+    }
+    if (transparentAreaBounds.x != -skinLayout.frameRectangle.x || transparentAreaBounds.y != -skinLayout.frameRectangle.y) {
+      problems.add("Display offset in the layout file (${-skinLayout.frameRectangle.x}, ${-skinLayout.frameRectangle.y})" +
+                   " doesn't match the skin image (${transparentAreaBounds.x}, ${transparentAreaBounds.y})")
+    }
+    if (problems.isNotEmpty()) {
+      fail(problems.joinToString("\n"))
+    }
+  }
+
+  private fun readBackgroundImage(skinFolder: Path): BufferedImage? {
+    val layoutFile = skinFolder.resolve("layout")
+    try {
+      val contents = Files.readAllBytes(layoutFile).toString(StandardCharsets.UTF_8)
+      val layoutDefinition = SkinLayoutDefinition.parseString(contents)
+      val backroundFileName = layoutDefinition.getValue("parts.portrait.background.image") ?: return null
+      val backgroundFile = skinFolder.resolve(backroundFileName)
+      return backgroundFile.readImage()
+    }
+    catch (e: NoSuchFileException) {
+      return null
+    }
+  }
+
+  private fun findBoundsOfContiguousArea(image: BufferedImage, start: Point, predicate: Predicate<Point>): Rectangle {
+    var minX = start.x
+    var maxX = start.x
+    var minY = start.y
+    var maxY = start.y
+
+    visitContiguousArea(image, start, predicate) { point ->
+      minX = minX.coerceAtMost(point.x)
+      maxX = maxX.coerceAtLeast(point.x)
+      minY = minY.coerceAtMost(point.y)
+      maxY = maxY.coerceAtLeast(point.y)
+    }
+    return Rectangle(minX, minY, maxX + 1 - minX, maxY + 1 - minY)
+  }
+
+  /**
+   * Calls [visitor] for every point of the contiguous area of the [image] where every pixel satisfies
+   * the [predicate] and containing the [start] point.
+   */
+  private fun visitContiguousArea(image: BufferedImage, start: Point, predicate: Predicate<Point>, visitor: Consumer<Point>) {
+    if (!predicate.test(start)) {
+      return
+    }
+    visitor.accept(start)
+    val width = image.width
+    val height = image.height
+    // Use the flood fill algorithm to explore the contiguous area.
+    var front = mutableSetOf(start)
+    var previousFront = mutableSetOf<Point>()
+    while (front.isNotEmpty()) {
+      var newFront = mutableSetOf<Point>()
+      for (p1 in front) {
+        for (neighbor in NEIGHBORS) {
+          val p2 = Point(p1.x + neighbor.x, p1.y + neighbor.y)
+          if (p2.x in 0 until width && p2.y in 0 until height &&
+              !newFront.contains(p2) && !front.contains(p2) && !previousFront.contains(p2) && predicate.test(p2)) {
+            visitor.accept(p2)
+            newFront.add(p2)
+          }
+        }
+      }
+      val temp = previousFront
+      previousFront = front
+      front = newFront
+      newFront = temp
+      newFront.clear()
+    }
+  }
+
+  private fun assertSkinAppearance(skinLayout: SkinLayout, goldenImageName: String) {
+    val image = skinLayout.draw()
+    ImageDiffUtil.assertImageSimilar(getGoldenFile(goldenImageName), image, 0.0)
+  }
+
+  private fun SkinLayout.draw(): BufferedImage {
     val image = ImageUtils.createDipImage(frameRectangle.width, frameRectangle.height, TYPE_INT_ARGB)
     val g = image.createGraphics()
-    layout.drawFrameAndMask(g, Rectangle(-frameRectangle.x, -frameRectangle.y, layout.displaySize.width, layout.displaySize.height))
+    drawFrameAndMask(g, Rectangle(-frameRectangle.x, -frameRectangle.y, displaySize.width, displaySize.height))
     g.dispose()
-    ImageDiffUtil.assertImageSimilar(getGoldenFile(goldenImageName), image, 0.0)
+    return image
   }
 
   private fun getGoldenFile(name: String): Path {
     return getWorkspaceRoot().resolve("${GOLDEN_FILE_PATH}/${name}.png")
   }
 }
+
+private fun BufferedImage.isTransparent(point: Point): Boolean {
+  return getRGB(point.x, point.y) and ALPHA_MASK == 0
+}
+
+private fun BufferedImage.isOpaque(point: Point): Boolean {
+  return getRGB(point.x, point.y) and ALPHA_MASK == ALPHA_MASK
+}
+
+private fun BufferedImage.isNotOpaque(point: Point): Boolean {
+  return !isOpaque(point)
+}
+
+data class Point(val x: Int, val y: Int) // Unlike java.awt.Point this class has an efficient hashCode method.
+
+private val NEIGHBORS = listOf(Point(-1, -1), Point(-1, 0), Point(-1, 1), Point(0, 1),
+                               Point(1, 1), Point(1, 0), Point(1, -1), Point(0, -1))
+
+private const val ALPHA_MASK = 0xFF shl 24
 
 private const val GOLDEN_FILE_PATH = "tools/adt/idea/emulator/testData/SkinDefinitionTest/golden"
