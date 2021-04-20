e3721e91b99ad4427439b885f82c97a6775ec9fa
Christophe Rhodes <xof@google.com>
2021-04-20 14:50:46 +0100

    [AUA] Update suggested versions when current changes
    If the Upgrade Assistant needs to refresh its view of what the current
    version of AGP in the project is, then it also needs to refresh the list
    of possible AGP versions to upgrade to.
    
    Fixes: 185875654
    Test: manual
    Change-Id: I7ff3005f30aa7b77901fa2297b4ce22ce1659aba
    
    GitOrigin-RevId: 8bce31b73b98b423267eca4630a21fd99837c3f0

--- android/src/com/android/tools/idea/gradle/project/upgrade/ContentManager.kt
+++ android/src/com/android/tools/idea/gradle/project/upgrade/ContentManager.kt
@@ -92,6 +92,7 @@ class ToolWindowModel(
   val runEnabled = BoolValueProperty(true)
   val showFinishedMessage = BoolValueProperty(false)
 
+  val knownVersions = OptionalValueProperty<Set<GradleVersion>>()
   val suggestedVersions = OptionalValueProperty<List<GradleVersion>>()
 
   val treeModel = DefaultTreeModel(CheckedTreeNode(null))
@@ -149,8 +150,8 @@ class ToolWindowModel(
     // Request known versions.
     ProgressManager.getInstance().run(object : Task.Backgroundable(project, "Looking for known versions", false) {
       override fun run(indicator: ProgressIndicator) {
-        val gMavenVersions = knownVersionsRequester()
-        val suggestedVersionsList = suggestedVersionsList(gMavenVersions)
+        knownVersions.value = knownVersionsRequester()
+        val suggestedVersionsList = suggestedVersionsList(knownVersions.value)
         invokeLater(ModalityState.NON_MODAL) { suggestedVersions.value = suggestedVersionsList }
       }
     })
@@ -177,7 +178,10 @@ class ToolWindowModel(
     showFinishedMessage.set(false)
     treeModel.nodeStructureChanged(root)
 
-    if (refindPlugin) { current = AndroidPluginInfo.find(project)?.pluginVersion }
+    if (refindPlugin) {
+      current = AndroidPluginInfo.find(project)?.pluginVersion
+      suggestedVersions.value = suggestedVersionsList(knownVersions.valueOrNull ?: setOf())
+    }
     val newVersion = selectedVersion.valueOrNull
     // TODO(xof/mlazeba): should we somehow preserve the existing uuid of the processor?
     val newProcessor = newVersion?.let {
