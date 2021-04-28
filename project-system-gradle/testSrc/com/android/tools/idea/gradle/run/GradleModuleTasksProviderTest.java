26b30dcb17a6edf51e4870b7b2e3e7cf00159f79
Yuriy Solodkyy <solodkyy@google.com>
2021-04-28 11:25:36 +0100

    Move GradleModuleTasksProvider to Gradle project system
    Bug: 147586218
    Test: n/a
    Change-Id: Ic4905f0b6e157203da77498d0522adf34c6a8b98
    
    GitOrigin-RevId: 318f2f53ec2f95623eb7b2bc9cb62987da27e0e2

--- project-system-gradle/testSrc/com/android/tools/idea/gradle/run/GradleModuleTasksProviderTest.java
+++ project-system-gradle/testSrc/com/android/tools/idea/gradle/run/GradleModuleTasksProviderTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017 The Android Open Source Project
+ * Copyright (C) 2021 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,19 +15,17 @@
  */
 package com.android.tools.idea.gradle.run;
 
+import static com.android.tools.idea.testing.TestProjectPaths.JAVA_LIB;
+
 import com.android.tools.idea.gradle.util.BuildMode;
 import com.android.tools.idea.testing.AndroidGradleTestCase;
 import com.android.tools.idea.testing.TestModuleUtil;
 import com.google.common.collect.ListMultimap;
 import com.intellij.openapi.externalSystem.util.ExternalSystemApiUtil;
 import com.intellij.openapi.module.Module;
-import com.intellij.openapi.module.ModuleManager;
-
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
-
-import static com.android.tools.idea.testing.TestProjectPaths.JAVA_LIB;
 
 public class GradleModuleTasksProviderTest extends AndroidGradleTestCase {
 
