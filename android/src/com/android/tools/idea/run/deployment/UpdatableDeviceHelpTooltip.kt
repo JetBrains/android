2fbedd35f127b1fe165c2368a2ef40caf8c37f31
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2021-04-28 09:39:56 +0200

    fix early access to registry
    Change-Id: I67d064f29d2854bfe0c2f7217e4f21a7886e8215

--- android/src/com/android/tools/idea/run/deployment/UpdatableDeviceHelpTooltip.kt
+++ android/src/com/android/tools/idea/run/deployment/UpdatableDeviceHelpTooltip.kt
@@ -28,7 +28,6 @@ internal open class UpdatableDeviceHelpTooltip : HelpTooltip() {
   private var myCompatibility: LaunchCompatibility? = null
 
   init {
-    initDismissDelay()
     createMouseListeners()
   }
 
