757d4591dcc4240a6b87ce8ee621cff86709dc6e
Yuriy Solodkyy <solodkyy@google.com>
2021-04-28 14:08:57 +0100

    Delete unused code
    Bug: 147586218
    Test: n/a
    Change-Id: I51d0e7f18aebf706ae568b6a2ba6a1339a54401f

--- build-common/src/com/android/tools/idea/gradle/compiler/AndroidGradleBuildTargetConstants.java
+++
@@ -1,6 +0,0 @@
-package com.android.tools.idea.gradle.compiler;
-
-public class AndroidGradleBuildTargetConstants {
-  public static final String TARGET_ID = "android_gradle_build_target";
-  public static final String TARGET_TYPE_ID = "android_gradle_build_target_type";
-}
