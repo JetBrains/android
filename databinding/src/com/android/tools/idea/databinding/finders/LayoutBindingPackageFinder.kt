2e3ecceef6073ebbcfff7c420be84ed215034b9d
David Herman <davidherman@google.com>
2021-04-29 15:05:21 -0700

    Move LayoutBindingPackageFactory out of core module
    Bug: N/A
    Test: Covered by existing
    Change-Id: I668b22172290defcc0bad15e3f698d84bc5e78e3

--- databinding/src/com/android/tools/idea/databinding/finders/LayoutBindingPackageFinder.kt
+++ databinding/src/com/android/tools/idea/databinding/finders/LayoutBindingPackageFinder.kt
@@ -16,7 +16,6 @@
 package com.android.tools.idea.databinding.finders
 
 import com.android.tools.idea.databinding.LayoutBindingEnabledFacetsProvider
-import com.android.tools.idea.databinding.LayoutBindingPackageFactory
 import com.android.tools.idea.databinding.module.LayoutBindingModuleCache
 import com.android.tools.idea.databinding.project.ProjectLayoutResourcesModificationTracker
 import com.intellij.openapi.project.Project
