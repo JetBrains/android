8009baad336c33f23e11f7d1274fad34526489ec
Sergio Murguia <srmurguia@google.com>
2021-04-30 17:30:01 -0700

    Cause validateJdk to consider JDK per project
    When a Gradle project is opened we validate if the JDK used for Gradle
    is valid and if not we show an issue with some quickfixes. This check
    validates the JDK returned by IdeSdks.getInstance().jdk, but after JDK
    per project was introduced, this no longer represents the project JDK.
    
    This change uses AndroidStudioGradleInstallationManager to find the JDK
    used by the project when calling validateJdk.
    
    Bug: 186868652
    Test: JdkImportCheckTest
    Change-Id: I4511ffa13e28f0bd32d3b8960ad9deeb6ccad834
    
    GitOrigin-RevId: 037e2b9ecb6f85b19f4452143c19c6320136c412

--- android/testSrc/com/android/tools/idea/gradle/project/sync/idea/issues/JdkImportCheckTest.java
+++ android/testSrc/com/android/tools/idea/gradle/project/sync/idea/issues/JdkImportCheckTest.java
@@ -15,26 +15,23 @@
  */
 package com.android.tools.idea.gradle.project.sync.idea.issues;
 
-import static com.android.tools.idea.gradle.project.sync.SimulatedSyncErrors.registerSyncErrorToSimulate;
-import static com.android.tools.idea.testing.TestProjectPaths.SIMPLE_APPLICATION;
 import static com.google.common.truth.Truth.assertThat;
-import static com.google.wireless.android.sdk.stats.AndroidStudioEvent.GradleSyncFailure.JDK8_REQUIRED;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-import com.android.tools.idea.IdeInfo;
 import com.android.tools.idea.flags.StudioFlags;
-import com.android.tools.idea.gradle.project.sync.issues.TestSyncIssueUsageReporter;
 import com.android.tools.idea.sdk.IdeSdks;
 import com.android.tools.idea.sdk.Jdks;
 import com.android.tools.idea.testing.AndroidGradleTestCase;
 import com.android.tools.idea.testing.IdeComponents;
+import com.intellij.openapi.project.Project;
 import com.intellij.openapi.projectRoots.JavaSdkVersion;
 import com.intellij.openapi.projectRoots.Sdk;
 import java.io.File;
+import org.jetbrains.annotations.Nullable;
 
 /**
- * Tests for {@link JdkImportCheck#validateJdk()}.
+ * Tests for {@link JdkImportCheck#validateProjectGradleJdk(Project, String)} and {@link JdkImportCheck#validateDefaultGradleJdk()}.
  */
 public class JdkImportCheckTest extends AndroidGradleTestCase {
   private IdeSdks myMockIdeSdks;
@@ -43,8 +40,6 @@ public class JdkImportCheckTest extends AndroidGradleTestCase {
   @Override
   public void setUp() throws Exception {
     super.setUp();
-
-    loadSimpleApplication();
 
     myMockIdeSdks = new IdeComponents(getProject()).mockApplicationService(IdeSdks.class);
     myMockJdks = new IdeComponents(getProject()).mockApplicationService(Jdks.class);
@@ -60,30 +55,25 @@ public class JdkImportCheckTest extends AndroidGradleTestCase {
   }
 
   public void testDoCheckCanSyncWithNullJdk() {
-    when(myMockIdeSdks.getJdk()).thenReturn(null);
-
-    assertThat(runValidateJdkAndGetMessage()).startsWith("Jdk location is not set");
+    assertThat(runCheckJdkErrorMessage(null)).startsWith("Jdk location is not set");
   }
 
   public void testDoCheckWithJdkWithoutHomePath() {
     Sdk jdk = mock(Sdk.class);
-
-    when(myMockIdeSdks.getJdk()).thenReturn(jdk);
     when(jdk.getHomePath()).thenReturn(null);
 
-    assertThat(runValidateJdkAndGetMessage()).startsWith("Could not find valid Jdk home from the selected Jdk location");
+    assertThat(runCheckJdkErrorMessage(jdk)).startsWith("Could not find valid Jdk home from the selected Jdk location");
   }
 
   public void testDoCheckWithJdkWithIncompatibleVersion() {
     Sdk jdk = mock(Sdk.class);
     String pathToJdk10 = "/path/to/jdk10";
 
-    when(myMockIdeSdks.getJdk()).thenReturn(jdk);
     when(jdk.getHomePath()).thenReturn(pathToJdk10);
     when(myMockIdeSdks.getRunningVersionOrDefault()).thenReturn(JavaSdkVersion.JDK_1_8);
     when(myMockJdks.findVersion(new File(pathToJdk10))).thenReturn(JavaSdkVersion.JDK_10);
 
-    assertThat(runValidateJdkAndGetMessage()).startsWith(
+    assertThat(runCheckJdkErrorMessage(jdk)).startsWith(
       "The version of selected Jdk doesn't match the Jdk used by Studio. Please choose a valid Jdk 8 directory.\n" +
       "Selected Jdk location is /path/to/jdk10."
     );
@@ -93,13 +83,11 @@ public class JdkImportCheckTest extends AndroidGradleTestCase {
     StudioFlags.ALLOW_DIFFERENT_JDK_VERSION.override(true);
     Sdk jdk = mock(Sdk.class);
     String pathToJdk10 = "/path/to/jdk10";
-
-    when(myMockIdeSdks.getJdk()).thenReturn(jdk);
     when(jdk.getHomePath()).thenReturn(pathToJdk10);
     when(myMockIdeSdks.getRunningVersionOrDefault()).thenReturn(JavaSdkVersion.JDK_1_8);
     when(myMockJdks.findVersion(new File(pathToJdk10))).thenReturn(JavaSdkVersion.JDK_10);
 
-    assertThat(runValidateJdkAndGetMessage()).startsWith(
+    assertThat(runCheckJdkErrorMessage(jdk)).startsWith(
       "The Jdk installation is invalid.\n" +
       "Selected Jdk location is /path/to/jdk10."
     );
@@ -108,23 +96,19 @@ public class JdkImportCheckTest extends AndroidGradleTestCase {
   public void testDoCheckWithJdkWithInvalidJdkInstallation() {
     Sdk jdk = mock(Sdk.class);
     String pathToJdk8 = "/path/to/jdk8";
-
-    when(myMockIdeSdks.getJdk()).thenReturn(jdk);
     when(jdk.getHomePath()).thenReturn(pathToJdk8);
     when(myMockIdeSdks.getRunningVersionOrDefault()).thenReturn(JavaSdkVersion.JDK_1_8);
     when(myMockJdks.findVersion(new File(pathToJdk8))).thenReturn(JavaSdkVersion.JDK_1_8);
-    when(myMockIdeSdks.getJdk()).thenReturn(jdk);
-    when(jdk.getHomePath()).thenReturn("/path/to/jdk8");
 
-    assertThat(runValidateJdkAndGetMessage()).startsWith(
+    assertThat(runCheckJdkErrorMessage(jdk)).startsWith(
       "The Jdk installation is invalid.\n" +
       "Selected Jdk location is /path/to/jdk8."
     );
   }
 
-  private static String runValidateJdkAndGetMessage() {
+  private static String runCheckJdkErrorMessage(@Nullable Sdk jdk) {
     try {
-      JdkImportCheck.validateJdk();
+      JdkImportCheck.checkJdkErrorMessage(jdk);
       return ""; // No error
     } catch (JdkImportCheckException e) {
       return e.getMessage();
