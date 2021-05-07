3709a04ea933e5729af2194ba6597316906e3ad4
Yi Yang <yiyiyang@google.com>
2021-05-06 18:31:12 -0700

    Fix freeze when stopping CPU recording on pre-O devices
    For pre-O CPU recording, ddmlib only supports one handler so when
    multiple devices are connected, each of them will register their
    own handler, overwriting each other. This resuls in freezes when
    the STOP_TRACE command waits on a CountDownLatch that is no
    longer in scope.
    
    This CL makes LegacyCpuProfilingHandler a singleton that manages
    the device to metadata mapping properly.
    
    Fixes: 177612469
    Test: added
    Change-Id: Ib743bee1a7bb9aa3a87d1bc0e02ee3064dda767d

--- profilers-android/src/com/android/tools/idea/profilers/LegacyCpuProfilingHandler.kt
+++ profilers-android/src/com/android/tools/idea/profilers/LegacyCpuProfilingHandler.kt
@@ -17,32 +17,57 @@ package com.android.tools.idea.profilers
 
 import com.android.ddmlib.Client
 import com.android.ddmlib.ClientData
+import com.android.ddmlib.IDevice
+import com.android.tools.idea.profilers.LegacyCpuProfilingHandler.registerDevice
 import com.android.tools.idea.protobuf.ByteString
 import com.android.tools.profiler.proto.Cpu
 
-internal class LegacyCpuProfilingHandler(private val profilingRecords: Map<Int, LegacyCpuTraceRecord>,
-                                         private val byteCache: MutableMap<String, ByteString>) : ClientData.IMethodProfilingHandler {
+/**
+ * A singleton handler that implements [ClientData.setMethodProfilingHandler] since ddmlib only supports one such handler at a time.
+ *
+ * This handler keeps track of all devices by [registerDevice] and handles callbacks depending on the device from the given [Client].
+ */
+internal object LegacyCpuProfilingHandler : ClientData.IMethodProfilingHandler {
+  /**
+   * A device-to-(traceRecords, byteCache) map to for routing callbacks based on [Client]'s device.
+   */
+  private val deviceToProfilingMetadata = mutableMapOf<IDevice, Pair<Map<Int, LegacyCpuTraceRecord>, MutableMap<String, ByteString>>>()
+
+  init {
+    // ddmlib only supports one method profiling handler at a time.
+    ClientData.setMethodProfilingHandler(this)
+  }
+
+  /**
+   * Register a device with its associated trace records and byte cache so the handler knows where to route the callbacks.
+   */
+  fun registerDevice(device: IDevice, profilingRecords: Map<Int, LegacyCpuTraceRecord>, byteCache: MutableMap<String, ByteString>) {
+    deviceToProfilingMetadata[device] = Pair(profilingRecords, byteCache)
+  }
 
   override fun onSuccess(remoteFilePath: String, client: Client) {
-    val record = profilingRecords[client.clientData.pid]
-    if (record != null) {
-      // Devices older than API 10 don't return profile results via JDWP. Instead they save the results on the
-      // sdcard. We don't support this.
-      val status = Cpu.TraceStopStatus.newBuilder()
-      .setStatus(Cpu.TraceStopStatus.Status.CANNOT_COPY_FILE)
-        .setErrorMessage("Method profiling: Older devices (API level < 10) are not supported. Please use DDMS.")
-        .build()
-      record.traceInfo!!.stopStatus = status
-      record.stopLatch.countDown()
+    deviceToProfilingMetadata[client.device]?.let { (traceRecords, _) ->
+      traceRecords[client.clientData.pid]?.let { record ->
+        // Devices older than API 10 don't return profile results via JDWP. Instead they save the results on the
+        // sdcard. We don't support this.
+        val status = Cpu.TraceStopStatus.newBuilder()
+          .setStatus(Cpu.TraceStopStatus.Status.CANNOT_COPY_FILE)
+          .setErrorMessage("Method profiling: Older devices (API level < 10) are not supported. Please use DDMS.")
+          .build()
+        record.traceInfo!!.stopStatus = status
+        record.stopLatch.countDown()
+      }
     }
   }
 
   override fun onSuccess(data: ByteArray, client: Client) {
-    val record = profilingRecords[client.clientData.pid]
-    if (record != null) {
-      record.traceInfo!!.stopStatus = Cpu.TraceStopStatus.newBuilder().setStatus(Cpu.TraceStopStatus.Status.SUCCESS).build()
-      byteCache[record.traceInfo!!.traceId.toString()] = ByteString.copyFrom(data)
-      record.stopLatch.countDown()
+    deviceToProfilingMetadata[client.device]?.let { (traceRecords, byteCache) ->
+      traceRecords[client.clientData.pid]?.let { record ->
+        val traceInfo = record.traceInfo!!
+        traceInfo.stopStatus = Cpu.TraceStopStatus.newBuilder().setStatus(Cpu.TraceStopStatus.Status.SUCCESS).build()
+        byteCache[traceInfo.traceId.toString()] = ByteString.copyFrom(data)
+        record.stopLatch.countDown()
+      }
     }
   }
 
@@ -65,19 +90,20 @@ internal class LegacyCpuProfilingHandler(private val profilingRecords: Map<Int, 
    * @param message an optional (`null` ok) error message to be displayed.
    */
   override fun onEndFailure(client: Client, message: String) {
-    val record = profilingRecords[client.clientData.pid]
-    if (record != null) {
-      if (record.startLatch.getCount() > 0) {
-        record.startFailureMessage = message
-        record.startLatch.countDown()
-      }
-      else {
-        val status = Cpu.TraceStopStatus.newBuilder()
-          .setStatus(Cpu.TraceStopStatus.Status.STOP_COMMAND_FAILED)
-          .setErrorMessage("Failed to stop profiling: $message")
-          .build()
-        record.traceInfo!!.stopStatus = status
-        record.stopLatch.countDown()
+    deviceToProfilingMetadata[client.device]?.let { (traceRecords, _) ->
+      traceRecords[client.clientData.pid]?.let { record ->
+        if (record.startLatch.count > 0) {
+          record.startFailureMessage = message
+          record.startLatch.countDown()
+        }
+        else {
+          val status = Cpu.TraceStopStatus.newBuilder()
+            .setStatus(Cpu.TraceStopStatus.Status.STOP_COMMAND_FAILED)
+            .setErrorMessage("Failed to stop profiling: $message")
+            .build()
+          record.traceInfo!!.stopStatus = status
+          record.stopLatch.countDown()
+        }
       }
     }
   }
