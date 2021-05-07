c7e018d3125e876f80371b8c1052fba4138ef33e
Christophe Rhodes <xof@google.com>
2021-05-07 11:06:09 +0100

    [AUA] Ensure that there is a usage target
    In the new interface to the Upgrade Assistant, users can (and in some
    sense we hope they do) disable the classpath processor itself,
    particularly in order to do component pre-upgrade / prerequisite steps
    on their own.
    
    However, this broke the implicit assumption that the classpath processor
    is always on, which was used to populate the processor's target for the
    preview window.  (Without a target, the preview window behaves more
    erratically).
    
    Workaround by unconditionally installing a fake Psi element as the
    target for this refactoring.  This fake element doesn't have a
    navigation target, but this is probably preferable to pointing at the
    AGP version declaration when the user might have removed that from the
    set of refactorings to execute.
    
    Bug: 163020008
    Test: added
    Change-Id: I4fa2df1b162b53b52ca90a4d850bf1f26c0a9983
    
    GitOrigin-RevId: c850f0efc77d67806ee59d6eaa24404221929b45

--- android/src/com/android/tools/idea/gradle/project/upgrade/AgpUpgradeRefactoringProcessor.kt
+++ android/src/com/android/tools/idea/gradle/project/upgrade/AgpUpgradeRefactoringProcessor.kt
@@ -116,6 +116,7 @@ import com.intellij.psi.PsiDocumentManager
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
 import com.intellij.psi.PsiManager
+import com.intellij.psi.impl.FakePsiElement
 import com.intellij.psi.util.PsiUtilCore
 import com.intellij.refactoring.BaseRefactoringProcessor
 import com.intellij.refactoring.RefactoringBundle
@@ -317,10 +318,17 @@ class AgpUpgradeRefactoringProcessor(
     val usages = ArrayList<UsageInfo>()
 
     usages.addAll(classpathRefactoringProcessor.findUsages())
-    targets.ifEmpty { targets.apply { usages.firstNotNullResult { it.element }?.let { add(it) } } }
-
     componentRefactoringProcessors.forEach { processor ->
       usages.addAll(processor.findUsages())
+    }
+    targets.clear()
+    projectBuildModel.projectBuildModel?.let {
+      targets.add(object : FakePsiElement() {
+        override fun getParent() = it.psiElement
+        override fun canNavigate() = false
+        override fun getContainingFile() = it.psiFile
+        override fun getName() = "Upgrading Project Build Configuration"
+      })
     }
 
     foundUsages = usages.size > 0
