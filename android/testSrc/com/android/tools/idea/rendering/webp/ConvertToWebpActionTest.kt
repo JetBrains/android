40a89cc96717c855d69d711679777baa3dd03d6d
Sergey Prigogin <sprigogin@google.com>
2021-05-07 18:34:46 -0700

    Convert ConvertToWebpActionTest to Kotlin
    Test: this
    Bug: N/A
    Change-Id: I529a4ba5c07d56c21ede276a9bb33ab0894ab7fc
    
    GitOrigin-RevId: a4220d40f63302c92c4b5cdc6b52d4690fb8612e

--- android/testSrc/com/android/tools/idea/rendering/webp/ConvertToWebpActionTest.kt
+++ android/testSrc/com/android/tools/idea/rendering/webp/ConvertToWebpActionTest.kt
@@ -13,63 +13,55 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.android.tools.idea.rendering.webp;
+package com.android.tools.idea.rendering.webp
 
-import com.intellij.openapi.vfs.VirtualFile;
-import org.jetbrains.android.AndroidTestCase;
+import com.google.common.truth.Truth
+import org.jetbrains.android.AndroidTestCase
 
-import java.util.Arrays;
-
-import static com.google.common.truth.Truth.assertThat;
-
-public class ConvertToWebpActionTest extends AndroidTestCase {
-  public void testConvert() throws Exception {
+class ConvertToWebpActionTest : AndroidTestCase() {
+  fun testConvert() {
     // Regression test for issue 226893
     // Ensure that images that are too large to encode are encoded anyway if the user asked for it
-    WebpConversionSettings settings = new WebpConversionSettings();
-    settings.skipTransparentImages = false;
-    settings.skipLargerImages = true;
-    settings.quality = 75;
-
-    VirtualFile mdpi = myFixture.copyFileToProject("webp/ic_action_name-mdpi.png", "res/drawable-mdpi/ic_action_name.png");
-    VirtualFile xhdpi = myFixture.copyFileToProject("webp/ic_action_name-xhdpi.png", "res/drawable-xhdpi/ic_action_name.png");
-    VirtualFile mdpiFolder = mdpi.getParent();
-    VirtualFile xhdpiFolder = xhdpi.getParent();
-
-    ConvertToWebpAction action = new ConvertToWebpAction();
-    action.convert(getProject(), settings, true, Arrays.asList(mdpi, xhdpi));
+    val settings = WebpConversionSettings()
+    settings.skipTransparentImages = false
+    settings.skipLargerImages = true
+    settings.quality = 75
+    val mdpi = myFixture.copyFileToProject("webp/ic_action_name-mdpi.png", "res/drawable-mdpi/ic_action_name.png")
+    val xhdpi = myFixture.copyFileToProject("webp/ic_action_name-xhdpi.png", "res/drawable-xhdpi/ic_action_name.png")
+    val mdpiFolder = mdpi.parent
+    val xhdpiFolder = xhdpi.parent
+    val action = ConvertToWebpAction()
+    action.convert(project, settings, true, listOf(mdpi, xhdpi))
 
     // Check that we only converted the xhdpi image (the mdpi image encodes to a larger image)
-    assertThat(xhdpiFolder.findChild("ic_action_name.png")).isNull();
-    assertThat(xhdpiFolder.findChild("ic_action_name.webp")).isNotNull();
-    assertThat(mdpiFolder.findChild("ic_action_name.png")).isNotNull();
-    assertThat(mdpiFolder.findChild("ic_action_name.webp")).isNull();
+    Truth.assertThat(xhdpiFolder.findChild("ic_action_name.png")).isNull()
+    Truth.assertThat(xhdpiFolder.findChild("ic_action_name.webp")).isNotNull()
+    Truth.assertThat(mdpiFolder.findChild("ic_action_name.png")).isNotNull()
+    Truth.assertThat(mdpiFolder.findChild("ic_action_name.webp")).isNull()
   }
 
-  public void testIncludeLargerImages() throws Exception {
+  fun testIncludeLargerImages() {
     // Regression test for issue 226893
     // Ensure that images that are too large to encode are encoded anyway if the user asked for it
-    WebpConversionSettings settings = new WebpConversionSettings();
-    settings.skipTransparentImages = false;
-    settings.skipLargerImages = false;
-    settings.quality = 75;
-
-    VirtualFile mdpi = myFixture.copyFileToProject("webp/ic_action_name-mdpi.png", "res/drawable-mdpi/ic_action_name.png");
+    val settings = WebpConversionSettings()
+    settings.skipTransparentImages = false
+    settings.skipLargerImages = false
+    settings.quality = 75
+    val mdpi = myFixture.copyFileToProject("webp/ic_action_name-mdpi.png", "res/drawable-mdpi/ic_action_name.png")
     // test conversion of a transparent gray issue
-    VirtualFile gray = myFixture.copyFileToProject("webp/ic_arrow_back.png", "res/drawable-mdpi/ic_arrow_back.png");
-    VirtualFile xhdpi = myFixture.copyFileToProject("webp/ic_action_name-xhdpi.png", "res/drawable-xhdpi/ic_action_name.png");
-    VirtualFile mdpiFolder = mdpi.getParent();
-    VirtualFile xhdpiFolder = xhdpi.getParent();
-
-    ConvertToWebpAction action = new ConvertToWebpAction();
-    action.convert(getProject(), settings, true, Arrays.asList(mdpi, xhdpi, gray));
+    val gray = myFixture.copyFileToProject("webp/ic_arrow_back.png", "res/drawable-mdpi/ic_arrow_back.png")
+    val xhdpi = myFixture.copyFileToProject("webp/ic_action_name-xhdpi.png", "res/drawable-xhdpi/ic_action_name.png")
+    val mdpiFolder = mdpi.parent
+    val xhdpiFolder = xhdpi.parent
+    val action = ConvertToWebpAction()
+    action.convert(project, settings, true, listOf(mdpi, xhdpi, gray))
 
     // Check that we converted both images
-    assertThat(xhdpiFolder.findChild("ic_action_name.png")).isNull();
-    assertThat(xhdpiFolder.findChild("ic_action_name.webp")).isNotNull();
-    assertThat(mdpiFolder.findChild("ic_action_name.png")).isNull();
-    assertThat(mdpiFolder.findChild("ic_action_name.webp")).isNotNull();
-    assertThat(mdpiFolder.findChild("ic_arrow_back.webp")).isNotNull();
-    assertThat(mdpiFolder.findChild("ic_arrow_back.png")).isNull();
+    Truth.assertThat(xhdpiFolder.findChild("ic_action_name.png")).isNull()
+    Truth.assertThat(xhdpiFolder.findChild("ic_action_name.webp")).isNotNull()
+    Truth.assertThat(mdpiFolder.findChild("ic_action_name.png")).isNull()
+    Truth.assertThat(mdpiFolder.findChild("ic_action_name.webp")).isNotNull()
+    Truth.assertThat(mdpiFolder.findChild("ic_arrow_back.webp")).isNotNull()
+    Truth.assertThat(mdpiFolder.findChild("ic_arrow_back.png")).isNull()
   }
 }
