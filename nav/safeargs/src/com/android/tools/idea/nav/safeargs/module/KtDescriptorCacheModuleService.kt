bc337c1d1573eb03e229206c4a129abd85d7fcf2
David Herman <davidherman@google.com>
2021-05-11 09:33:56 -0700

    Stop caching Kotlin descriptors
    It turns out that Kotlin has its own, extremely complex system for
    caching and recalculating descriptors. We were adding our own cache on
    top of this, which kept strong references to descriptors (specifically
    a list of PackageFragmentDescriptors). It turns out this is something
    their architecture does not want you to do - keeping descriptors alive
    can add a ton of memory pressure due to not being able to GC classes
    with a heavy footprint (e.g. trace objects which keep the whole
    descriptor tree alive)
    
    By removing our change, it's possible that we'll see a performance
    regression in large projects using safe args, but it seems at the moment
    that after removing our cache, safe args caches are recalculated every
    time a user stops typing. This may be OK, unlike, say, our previous
    approach with light classes in Java, which would attempt to refetch values
    much more frequently, e.g. multiple times when triggering autocomplete.
    
    Fixes: 187095111
    Test: This is an implementation tweak that I don't even know how I would
      test, but at least existing test coverage ensures functionality isn't
      broken by it.
    Change-Id: Ia2e80de823b9f72b383b3d707497f5f542d3bd6e

--- nav/safeargs/src/com/android/tools/idea/nav/safeargs/module/KtDescriptorCacheModuleService.kt
+++ nav/safeargs/src/com/android/tools/idea/nav/safeargs/module/KtDescriptorCacheModuleService.kt
@@ -21,7 +21,6 @@ import com.android.resources.ResourceType
 import com.android.tools.idea.nav.safeargs.SafeArgsMode
 import com.android.tools.idea.nav.safeargs.index.NavXmlData
 import com.android.tools.idea.nav.safeargs.index.NavXmlIndex
-import com.android.tools.idea.nav.safeargs.psi.GRADLE_VERSION_ZERO
 import com.android.tools.idea.nav.safeargs.psi.findNavigationVersion
 import com.android.tools.idea.nav.safeargs.psi.kotlin.KtArgsPackageDescriptor
 import com.android.tools.idea.nav.safeargs.psi.kotlin.KtDirectionsPackageDescriptor
@@ -42,7 +41,6 @@ import com.intellij.psi.PsiManager
 import com.intellij.psi.impl.source.xml.XmlTagImpl
 import com.intellij.psi.xml.XmlFile
 import com.intellij.util.PlatformIcons
-import net.jcip.annotations.GuardedBy
 import org.jetbrains.android.dom.manifest.getPackageName
 import org.jetbrains.android.facet.AndroidFacet
 import org.jetbrains.kotlin.descriptors.ModuleDescriptor
@@ -64,13 +62,6 @@ import org.jetbrains.kotlin.storage.StorageManager
  */
 class KtDescriptorCacheModuleService(val module: Module) {
   private val LOG get() = Logger.getInstance(KtDescriptorCacheModuleService::class.java)
-  private val lock = Any()
-
-  @GuardedBy("lock")
-  private var lastModificationCount = -1L
-
-  @GuardedBy("lock")
-  private var lastVersion = GRADLE_VERSION_ZERO
 
   private data class QualifiedDescriptor(val fqName: FqName, val descriptor: PackageFragmentDescriptor)
 
@@ -84,9 +75,6 @@ class KtDescriptorCacheModuleService(val module: Module) {
     fun getInstance(module: Module) = module.getService(KtDescriptorCacheModuleService::class.java)!!
   }
 
-  @GuardedBy("lock")
-  var descriptorsCache = emptyMap<FqName, List<PackageFragmentDescriptor>>()
-
   fun getDescriptors(moduleDescriptor: ModuleDescriptor): Map<FqName, List<PackageFragmentDescriptor>> {
     ProgressManager.checkCanceled()
 
@@ -94,45 +82,31 @@ class KtDescriptorCacheModuleService(val module: Module) {
     val facet = module.androidFacet!!
 
     if (DumbService.isDumb(module.project)) {
-      LOG.warn("Safe Args classes may be temporarily stale due to indices not being ready right now.")
-      return descriptorsCache
+      LOG.warn("Safe Args classes may be temporarily unavailable due to indices not being ready right now.")
+      return emptyMap()
     }
 
     val packageFqName = getPackageName(facet)?.let { packageName -> FqName(packageName) }
-                        ?: return descriptorsCache
+                        ?: return emptyMap()
 
-    synchronized(lock) {
-      val now = ModuleNavigationResourcesModificationTracker.getInstance(module).modificationCount
-      val currVersion = facet.findNavigationVersion()
+    val currVersion = facet.findNavigationVersion()
 
-      if (lastModificationCount != now || lastVersion != currVersion) {
-        val moduleNavResources = getNavResourceFromIndex()
+    val moduleNavResources = getNavResourceFromIndex()
+    val packageResourceData = SafeArgSyntheticPackageResourceData(moduleNavResources)
+    // TODO(b/159950623): Consolidate with SafeArgsCacheModuleService
+    return packageResourceData.moduleNavResource
+      .asSequence()
+      .flatMap { navEntry ->
+        val backingXmlFile = PsiManager.getInstance(module.project).findFile(navEntry.file)
+        val sourceElement = backingXmlFile?.let { XmlSourceElement(it) } ?: SourceElement.NO_SOURCE
 
-        val packageResourceData = SafeArgSyntheticPackageResourceData(moduleNavResources)
+        val packages =
+          createArgsPackages(moduleDescriptor, currVersion, navEntry, sourceElement, packageFqName.asString()) +
+          createDirectionsPackages(moduleDescriptor, navEntry, sourceElement, packageFqName.asString())
 
-
-        val packageDescriptors = packageResourceData.moduleNavResource
-          .asSequence()
-          .flatMap { navEntry ->
-            val backingXmlFile = PsiManager.getInstance(module.project).findFile(navEntry.file)
-            val sourceElement = backingXmlFile?.let { XmlSourceElement(it) } ?: SourceElement.NO_SOURCE
-
-            val packages =
-              createArgsPackages(moduleDescriptor, currVersion, navEntry, sourceElement, packageFqName.asString()) +
-              createDirectionsPackages(moduleDescriptor, navEntry, sourceElement, packageFqName.asString())
-
-            packages.asSequence()
-          }
-          .groupBy({ it.fqName }, { it.descriptor })
-
-        descriptorsCache = packageDescriptors
-        lastModificationCount = now
-        lastVersion = currVersion
+        packages.asSequence()
       }
-
-      // TODO(b/159950623): Consolidate with SafeArgsCacheModuleService
-      return descriptorsCache
-    }
+      .groupBy({ it.fqName }, { it.descriptor })
   }
 
   private fun createDirectionsPackages(
