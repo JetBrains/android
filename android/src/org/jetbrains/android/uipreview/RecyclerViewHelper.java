e300caafb038964e8e6ae268cbbbf111b74ff996
Diego Perez <diegoperez@google.com>
2021-05-12 15:29:11 +0100

    Do not rely on gradle deps on ModuleClassLoader
    When jetifier is enabled, a project could be using classes in the
    androidx namespace but not have the androidx dependencies. This break
    the heuristic to determine which adapters should be used in
    ModuleClassLoader.
    This CL removes that check and creates the adapters based on the
    namespace of the RecyclerView being loaded.
    
    Fixes: 173155464
    Test: Manually verified in a project with support deps and jetifier
    enabled.
    
    Change-Id: Ib7e99f296613bae875d16ad746003d7c6e2c29b7

--- android/src/org/jetbrains/android/uipreview/RecyclerViewHelper.java
+++ android/src/org/jetbrains/android/uipreview/RecyclerViewHelper.java
@@ -15,64 +15,164 @@
  */
 package org.jetbrains.android.uipreview;
 
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.org.objectweb.asm.*;
+import static com.android.SdkConstants.CLASS_RECYCLER_VIEW_ADAPTER;
+import static com.android.SdkConstants.CLASS_RECYCLER_VIEW_V7;
+import static com.android.SdkConstants.CLASS_RECYCLER_VIEW_VIEW_HOLDER;
+import static org.jetbrains.org.objectweb.asm.Opcodes.ACC_ABSTRACT;
+import static org.jetbrains.org.objectweb.asm.Opcodes.ACC_PRIVATE;
+import static org.jetbrains.org.objectweb.asm.Opcodes.ACC_PUBLIC;
+import static org.jetbrains.org.objectweb.asm.Opcodes.ACC_STATIC;
+import static org.jetbrains.org.objectweb.asm.Opcodes.ACC_SUPER;
+import static org.jetbrains.org.objectweb.asm.Opcodes.ALOAD;
+import static org.jetbrains.org.objectweb.asm.Opcodes.ARETURN;
+import static org.jetbrains.org.objectweb.asm.Opcodes.ASTORE;
+import static org.jetbrains.org.objectweb.asm.Opcodes.BIPUSH;
+import static org.jetbrains.org.objectweb.asm.Opcodes.CHECKCAST;
+import static org.jetbrains.org.objectweb.asm.Opcodes.DUP;
+import static org.jetbrains.org.objectweb.asm.Opcodes.GETFIELD;
+import static org.jetbrains.org.objectweb.asm.Opcodes.GOTO;
+import static org.jetbrains.org.objectweb.asm.Opcodes.ICONST_0;
+import static org.jetbrains.org.objectweb.asm.Opcodes.IFEQ;
+import static org.jetbrains.org.objectweb.asm.Opcodes.IFLE;
+import static org.jetbrains.org.objectweb.asm.Opcodes.IFNE;
+import static org.jetbrains.org.objectweb.asm.Opcodes.IF_ICMPGE;
+import static org.jetbrains.org.objectweb.asm.Opcodes.ILOAD;
+import static org.jetbrains.org.objectweb.asm.Opcodes.INSTANCEOF;
+import static org.jetbrains.org.objectweb.asm.Opcodes.INVOKEINTERFACE;
+import static org.jetbrains.org.objectweb.asm.Opcodes.INVOKESPECIAL;
+import static org.jetbrains.org.objectweb.asm.Opcodes.INVOKESTATIC;
+import static org.jetbrains.org.objectweb.asm.Opcodes.INVOKEVIRTUAL;
+import static org.jetbrains.org.objectweb.asm.Opcodes.IRETURN;
+import static org.jetbrains.org.objectweb.asm.Opcodes.ISTORE;
+import static org.jetbrains.org.objectweb.asm.Opcodes.NEW;
+import static org.jetbrains.org.objectweb.asm.Opcodes.POP;
+import static org.jetbrains.org.objectweb.asm.Opcodes.PUTFIELD;
+import static org.jetbrains.org.objectweb.asm.Opcodes.RETURN;
+import static org.jetbrains.org.objectweb.asm.Opcodes.V1_6;
+import static org.jetbrains.org.objectweb.asm.Opcodes.V1_7;
 
+import com.android.support.AndroidxName;
 import java.util.HashMap;
 import java.util.Map;
-
-import static org.jetbrains.org.objectweb.asm.Opcodes.*;
+import java.util.function.Function;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.org.objectweb.asm.AnnotationVisitor;
+import org.jetbrains.org.objectweb.asm.ClassWriter;
+import org.jetbrains.org.objectweb.asm.FieldVisitor;
+import org.jetbrains.org.objectweb.asm.Label;
+import org.jetbrains.org.objectweb.asm.MethodVisitor;
+import org.jetbrains.org.objectweb.asm.Opcodes;
 
 /**
  * Some constants that are needed to render the RecyclerView correctly.
  */
 @SuppressWarnings("SpellCheckingInspection")
 public class RecyclerViewHelper {
+  private static final String SUPPORT_PACKAGE_NAME = "com.android.layoutlib.bridge.android.support";
+  public static final String CN_SUPPORT_CUSTOM_ADAPTER = SUPPORT_PACKAGE_NAME + ".Adapter";
+  private static final String CN_SUPPORT_CUSTOM_VIEW_HOLDER = SUPPORT_PACKAGE_NAME + ".Adapter$ViewHolder";
 
-  public static final String PACKAGE_NAME = "com.android.layoutlib.bridge.android.support";
-  public static final String CN_CUSTOM_ADAPTER = PACKAGE_NAME + ".Adapter";
-  public static final String CN_CUSTOM_VIEW_HOLDER = PACKAGE_NAME + ".Adapter$ViewHolder";
+  private static final String ANDROIDX_PACKAGE_NAME = "com.android.layoutlib.bridge.android.androidx";
+  public static final String CN_ANDROIDX_CUSTOM_ADAPTER = ANDROIDX_PACKAGE_NAME + ".Adapter";
+  private static final String CN_ANDROIDX_CUSTOM_VIEW_HOLDER = ANDROIDX_PACKAGE_NAME + ".Adapter$ViewHolder";
 
-  // Lazily initialized.
-  private static Map<String, byte[]> ourClassesCache = new HashMap<>();
-
-  public static byte[] getAdapterClass(@NotNull String recyclerViewName,
-                                       @NotNull String viewHolderName,
-                                       @NotNull String adapterName) {
-    byte[] adapterClass = ourClassesCache.get(adapterName);
-    if (adapterClass == null) {
-      recyclerViewName = recyclerViewName.replaceAll("\\.", "/");
-      viewHolderName = viewHolderName.replaceAll("\\.", "/");
-      adapterName = adapterName.replaceAll("\\.", "/");
-
-      adapterClass = getAdapterClassDump(recyclerViewName,
-                                            viewHolderName,
-                                            adapterName);
-      ourClassesCache.put(adapterName, adapterClass);
-    }
-    return adapterClass;
+  public enum AdapterNamespace {
+    ANDROIDX,
+    SUPPORT
   }
 
-  public static byte[] getViewHolder(@NotNull String recyclerViewName,
-                                     @NotNull String viewHolderName,
-                                     @NotNull String adapterName) {
-    byte[] viewHolderClass = ourClassesCache.get(viewHolderName);
-    if (viewHolderClass == null) {
-      recyclerViewName = recyclerViewName.replaceAll("\\.", "/");
-      viewHolderName = viewHolderName.replaceAll("\\.", "/");
-      adapterName = adapterName.replaceAll("\\.", "/");
+  // Lazily initialized.
+  private static final Map<String, byte[]> ourClassesCache = new HashMap<>();
 
-      viewHolderClass = getViewHolderDump(recyclerViewName,
-                                        viewHolderName,
-                                        adapterName);
-      ourClassesCache.put(viewHolderName, viewHolderClass);
+  /**
+   * Converts the given {@link AndroidxName} to the VM binary class name representation using slashes.
+   *
+   * @param nameFunction One of either {@link AndroidxName#newName()} or One of either {@link AndroidxName#oldName()} depending on whether
+   *                     the androidx or support namespaces repectively need to be used.
+   * @param androidxName The {@link AndroidxName} to convert.
+   */
+  @NotNull
+  private static String nameToBinaryRepresentation(@NotNull Function<AndroidxName, String> nameFunction, @NotNull AndroidxName androidxName) {
+    return nameFunction.apply(androidxName).replaceAll("\\.", "/");
+  }
+
+  /**
+   * If the name matches any of the adapter helper classes (adapter or view holder), it will return the class definition for that helper
+   * class. This method will return null if the name does not match any of the helper classes.
+   */
+  public static byte[] getAdapterHelperClass(@NotNull String name) {
+    byte[] clazz = null;
+    switch (name) {
+      case CN_ANDROIDX_CUSTOM_ADAPTER:
+        clazz = getAdapterClass(CN_ANDROIDX_CUSTOM_ADAPTER, AdapterNamespace.ANDROIDX);
+        break;
+      case CN_ANDROIDX_CUSTOM_VIEW_HOLDER:
+        clazz = getViewHolder(CN_ANDROIDX_CUSTOM_ADAPTER, AdapterNamespace.ANDROIDX);
+        break;
+      case CN_SUPPORT_CUSTOM_ADAPTER:
+        clazz = getAdapterClass(CN_SUPPORT_CUSTOM_ADAPTER, AdapterNamespace.SUPPORT);
+        break;
+      case CN_SUPPORT_CUSTOM_VIEW_HOLDER:
+        clazz = getViewHolder(CN_SUPPORT_CUSTOM_ADAPTER, AdapterNamespace.SUPPORT);
+        break;
     }
-    return viewHolderClass;
+    return clazz;
+  }
+
+  /**
+   * Generates the custom RecyclerView adapter that allows using sample data from the Layout Editor.
+   *
+   * @param customAdapterName the name of the custom adapter. Different combinations of recyclerViewName/viewHolderName/adapterName must
+   *                          have a different customAdapterName so they do not collide. Usually there will be one for androidx and one for
+   *                          the old support library.
+   * @param adapterNamespace defined the namespaced that must be used for the generated adapter class (either androidx or support).
+   * @return the byte array containing the new class.
+   */
+  private static byte[] getAdapterClass(@NotNull final String customAdapterName,
+                                        @NotNull RecyclerViewHelper.AdapterNamespace adapterNamespace) {
+    return ourClassesCache.computeIfAbsent(customAdapterName, (adapterClassName) -> {
+      Function<AndroidxName, String> nameFunction =
+        adapterNamespace == AdapterNamespace.ANDROIDX ? AndroidxName::newName : AndroidxName::oldName;
+      String recyclerViewName = nameToBinaryRepresentation(nameFunction, CLASS_RECYCLER_VIEW_V7);
+      String viewHolderName = nameToBinaryRepresentation(nameFunction, CLASS_RECYCLER_VIEW_VIEW_HOLDER);
+      String adapterName = nameToBinaryRepresentation(nameFunction, CLASS_RECYCLER_VIEW_ADAPTER);
+
+      return getAdapterClassDump(adapterClassName.replaceAll("\\.", "/"),
+                                 recyclerViewName,
+                                 viewHolderName,
+                                 adapterName);
+    });
+  }
+
+  /**
+   * Generates the custom RecyclerView adapter ViewHolder that allows using sample data from the Layout Editor.
+   *
+   * @param customAdapterName the name of the custom adapter. Different combinations of recyclerViewName/viewHolderName/adapterName must
+   *                          have a different customAdapterName so they do not collide. Usually there will be one for androidx and one for
+   *                          the old support library.
+   * @param adapterNamespace defined the namespaced that must be used for the generated adapter class (either androidx or support).
+   * @return the byte array containing the new class.
+   */
+  private static byte[] getViewHolder(@NotNull final String customAdapterName,
+                                      @NotNull RecyclerViewHelper.AdapterNamespace adapterNamespace) {
+    return ourClassesCache.computeIfAbsent(customAdapterName + "$ViewHolder", (ignore) -> {
+      Function<AndroidxName, String> nameFunction =
+        adapterNamespace == AdapterNamespace.ANDROIDX ? AndroidxName::newName : AndroidxName::oldName;
+      String recyclerViewName = nameToBinaryRepresentation(nameFunction, CLASS_RECYCLER_VIEW_V7);
+      String viewHolderName = nameToBinaryRepresentation(nameFunction, CLASS_RECYCLER_VIEW_VIEW_HOLDER);
+      String adapterName = nameToBinaryRepresentation(nameFunction, CLASS_RECYCLER_VIEW_ADAPTER);
+
+      return getViewHolderDump(customAdapterName.replaceAll("\\.", "/"),
+                               recyclerViewName,
+                               viewHolderName,
+                               adapterName);
+    });
   }
 
   // See comment at the end of the file for how this method was generated.
   @SuppressWarnings("unused")  // Generated code
-  private static byte[] getAdapterClassDump(@NotNull String recyclerViewName,
+  private static byte[] getAdapterClassDump(@NotNull String customAdapterName,
+                                            @NotNull String recyclerViewName,
                                             @NotNull String viewHolderName,
                                             @NotNull String adapterName) {
     ClassWriter cw = new ClassWriter(0);
@@ -83,9 +183,9 @@ public class RecyclerViewHelper {
     String signature = String.format("L%1$s<L%2$s;>;",
                                      adapterName,
                                      viewHolderName);
-    cw.visit(V1_7, ACC_PUBLIC + ACC_SUPER, "com/android/layoutlib/bridge/android/support/Adapter", signature, adapterName, null);
+    cw.visit(V1_7, ACC_PUBLIC + ACC_SUPER, customAdapterName, signature, adapterName, null);
 
-    cw.visitInnerClass("com/android/layoutlib/bridge/android/support/Adapter$ViewHolder", "com/android/layoutlib/bridge/android/support/Adapter", "ViewHolder", ACC_PRIVATE + ACC_STATIC);
+    cw.visitInnerClass(customAdapterName + "$ViewHolder", customAdapterName, "ViewHolder", ACC_PRIVATE + ACC_STATIC);
 
     cw.visitInnerClass(viewHolderName, recyclerViewName, "ViewHolder", ACC_PUBLIC + ACC_STATIC + ACC_ABSTRACT);
 
@@ -106,7 +206,7 @@ public class RecyclerViewHelper {
       mv.visitMethodInsn(INVOKESPECIAL, adapterName, "<init>", "()V", false);
       mv.visitVarInsn(ALOAD, 0);
       mv.visitIntInsn(BIPUSH, 10);
-      mv.visitFieldInsn(PUTFIELD, "com/android/layoutlib/bridge/android/support/Adapter", "mItemCount", "I");
+      mv.visitFieldInsn(PUTFIELD, customAdapterName, "mItemCount", "I");
       mv.visitInsn(RETURN);
       mv.visitMaxs(2, 1);
       mv.visitEnd();
@@ -116,14 +216,14 @@ public class RecyclerViewHelper {
       mv = cw.visitMethod(ACC_PUBLIC, "onCreateViewHolder", desc, null, null);
       mv.visitCode();
       mv.visitVarInsn(ALOAD, 0);
-      mv.visitFieldInsn(GETFIELD, "com/android/layoutlib/bridge/android/support/Adapter", "mId", "I");
+      mv.visitFieldInsn(GETFIELD, customAdapterName, "mId", "I");
       Label l0 = new Label();
       mv.visitJumpInsn(IFLE, l0);
       mv.visitVarInsn(ALOAD, 1);
       mv.visitMethodInsn(INVOKEVIRTUAL, "android/view/ViewGroup", "getContext", "()Landroid/content/Context;", false);
       mv.visitMethodInsn(INVOKESTATIC, "android/view/LayoutInflater", "from", "(Landroid/content/Context;)Landroid/view/LayoutInflater;", false);
       mv.visitVarInsn(ALOAD, 0);
-      mv.visitFieldInsn(GETFIELD, "com/android/layoutlib/bridge/android/support/Adapter", "mId", "I");
+      mv.visitFieldInsn(GETFIELD, customAdapterName, "mId", "I");
       mv.visitVarInsn(ALOAD, 1);
       mv.visitInsn(ICONST_0);
       mv.visitMethodInsn(INVOKEVIRTUAL, "android/view/LayoutInflater", "inflate", "(ILandroid/view/ViewGroup;Z)Landroid/view/View;", false);
@@ -140,10 +240,10 @@ public class RecyclerViewHelper {
       mv.visitVarInsn(ASTORE, 3);
       mv.visitLabel(l1);
       mv.visitFrame(Opcodes.F_APPEND,1, new Object[] {"android/view/View"}, 0, null);
-      mv.visitTypeInsn(NEW, "com/android/layoutlib/bridge/android/support/Adapter$ViewHolder");
+      mv.visitTypeInsn(NEW, customAdapterName + "$ViewHolder");
       mv.visitInsn(DUP);
       mv.visitVarInsn(ALOAD, 3);
-      mv.visitMethodInsn(INVOKESPECIAL, "com/android/layoutlib/bridge/android/support/Adapter$ViewHolder", "<init>", "(Landroid/view/View;)V", false);
+      mv.visitMethodInsn(INVOKESPECIAL, customAdapterName + "$ViewHolder", "<init>", "(Landroid/view/View;)V", false);
       mv.visitInsn(ARETURN);
       mv.visitMaxs(4, 4);
       mv.visitEnd();
@@ -165,7 +265,7 @@ public class RecyclerViewHelper {
       mv.visitTypeInsn(NEW, "java/util/LinkedList");
       mv.visitInsn(DUP);
       mv.visitMethodInsn(INVOKESPECIAL, "java/util/LinkedList", "<init>", "()V", false);
-      mv.visitMethodInsn(INVOKESPECIAL, "com/android/layoutlib/bridge/android/support/Adapter", "findTextViews", "(Landroid/view/View;Ljava/util/ArrayList;Ljava/util/LinkedList;)V", false);
+      mv.visitMethodInsn(INVOKESPECIAL, customAdapterName, "findTextViews", "(Landroid/view/View;Ljava/util/ArrayList;Ljava/util/LinkedList;)V", false);
       mv.visitTypeInsn(NEW, "java/lang/StringBuilder");
       mv.visitInsn(DUP);
       mv.visitMethodInsn(INVOKESPECIAL, "java/lang/StringBuilder", "<init>", "()V", false);
@@ -179,7 +279,7 @@ public class RecyclerViewHelper {
       mv.visitVarInsn(ISTORE, 6);
       Label l0 = new Label();
       mv.visitLabel(l0);
-      mv.visitFrame(Opcodes.F_FULL, 7, new Object[] {"com/android/layoutlib/bridge/android/support/Adapter", viewHolderName, Opcodes.INTEGER, "android/view/View", "java/util/ArrayList", "java/lang/String", Opcodes.INTEGER}, 0, new Object[] {});
+      mv.visitFrame(Opcodes.F_FULL, 7, new Object[] {customAdapterName, viewHolderName, Opcodes.INTEGER, "android/view/View", "java/util/ArrayList", "java/lang/String", Opcodes.INTEGER}, 0, new Object[] {});
       mv.visitVarInsn(ILOAD, 6);
       mv.visitVarInsn(ALOAD, 4);
       mv.visitMethodInsn(INVOKEVIRTUAL, "java/util/ArrayList", "size", "()I", false);
@@ -212,7 +312,7 @@ public class RecyclerViewHelper {
       mv = cw.visitMethod(ACC_PUBLIC, "getItemCount", "()I", null, null);
       mv.visitCode();
       mv.visitVarInsn(ALOAD, 0);
-      mv.visitFieldInsn(GETFIELD, "com/android/layoutlib/bridge/android/support/Adapter", "mItemCount", "I");
+      mv.visitFieldInsn(GETFIELD, customAdapterName, "mItemCount", "I");
       mv.visitInsn(IRETURN);
       mv.visitMaxs(1, 1);
       mv.visitEnd();
@@ -222,7 +322,7 @@ public class RecyclerViewHelper {
       mv.visitCode();
       mv.visitVarInsn(ALOAD, 0);
       mv.visitVarInsn(ILOAD, 1);
-      mv.visitFieldInsn(PUTFIELD, "com/android/layoutlib/bridge/android/support/Adapter", "mId", "I");
+      mv.visitFieldInsn(PUTFIELD, customAdapterName, "mId", "I");
       mv.visitInsn(RETURN);
       mv.visitMaxs(2, 2);
       mv.visitEnd();
@@ -232,7 +332,7 @@ public class RecyclerViewHelper {
       mv.visitCode();
       mv.visitVarInsn(ALOAD, 0);
       mv.visitVarInsn(ILOAD, 1);
-      mv.visitFieldInsn(PUTFIELD, "com/android/layoutlib/bridge/android/support/Adapter", "mItemCount", "I");
+      mv.visitFieldInsn(PUTFIELD, customAdapterName, "mItemCount", "I");
       mv.visitInsn(RETURN);
       mv.visitMaxs(2, 2);
       mv.visitEnd();
@@ -292,7 +392,7 @@ public class RecyclerViewHelper {
       mv.visitTypeInsn(CHECKCAST, "android/view/View");
       mv.visitVarInsn(ALOAD, 2);
       mv.visitVarInsn(ALOAD, 3);
-      mv.visitMethodInsn(INVOKESPECIAL, "com/android/layoutlib/bridge/android/support/Adapter", "findTextViews", "(Landroid/view/View;Ljava/util/ArrayList;Ljava/util/LinkedList;)V", false);
+      mv.visitMethodInsn(INVOKESPECIAL, customAdapterName, "findTextViews", "(Landroid/view/View;Ljava/util/ArrayList;Ljava/util/LinkedList;)V", false);
       mv.visitLabel(l3);
       mv.visitFrame(Opcodes.F_SAME, 0, null, 0, null);
       mv.visitInsn(RETURN);
@@ -306,7 +406,8 @@ public class RecyclerViewHelper {
 
   // See comment at the end of the file for how this method was generated.
   @SuppressWarnings("unused")  // Generated code.
-  private static byte[] getViewHolderDump(@NotNull String recyclerViewClass,
+  private static byte[] getViewHolderDump(@NotNull String customAdapterName,
+                                          @NotNull String recyclerViewClass,
                                           @NotNull String viewHolderClass,
                                           @NotNull String viewAdapterClass) {
     ClassWriter cw = new ClassWriter(0);
@@ -314,11 +415,11 @@ public class RecyclerViewHelper {
     MethodVisitor mv;
     AnnotationVisitor av0;
 
-    cw.visit(V1_6, ACC_SUPER, "com/android/layoutlib/bridge/android/support/Adapter$ViewHolder", null,
+    cw.visit(V1_6, ACC_SUPER, customAdapterName + "$ViewHolder", null,
              viewHolderClass, null);
 
-    cw.visitInnerClass("com/android/layoutlib/bridge/android/support/Adapter$ViewHolder",
-                       "com/android/layoutlib/bridge/android/support/Adapter", "ViewHolder", ACC_PRIVATE + ACC_STATIC);
+    cw.visitInnerClass(customAdapterName + "$ViewHolder",
+                       customAdapterName, "ViewHolder", ACC_PRIVATE + ACC_STATIC);
 
     cw.visitInnerClass(viewHolderClass, recyclerViewClass, "ViewHolder",
                        ACC_PUBLIC + ACC_STATIC + ACC_ABSTRACT);
