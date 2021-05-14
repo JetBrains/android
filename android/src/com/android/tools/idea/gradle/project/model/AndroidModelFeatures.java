71b8417eaeeceb84d208811896820b027c88fe5a
Yuriy Solodkyy <solodkyy@google.com>
2021-05-14 14:02:56 +0100

    Simplify model feature support detection knowning AGP is at least 3.1
    Bug: 187822637
    Test: AndroidModelFeaturesTest
    Change-Id: I2a03bb65ce170f62e33c2133c67663392c4dfcfb
    
    GitOrigin-RevId: 07adf851c86508466c8d2a5a72dc737f88c5b949

--- android/src/com/android/tools/idea/gradle/project/model/AndroidModelFeatures.java
+++ android/src/com/android/tools/idea/gradle/project/model/AndroidModelFeatures.java
@@ -22,49 +22,22 @@ import org.jetbrains.annotations.Nullable;
 public class AndroidModelFeatures {
   @Nullable private final GradleVersion myModelVersion;
 
-  private final boolean myIssueReportingSupported;
-  private final boolean myShadersSupported;
-  private final boolean myTestedTargetVariantsSupported;
-  private final boolean myProductFlavorVersionSuffixSupported;
-  private final boolean myExternalBuildSupported;
-  private final boolean myConstraintLayoutSdkLocationSupported;
-  private final boolean myLayoutRenderingIssuePresent;
   private final boolean myBuildOutputFileSupported;
   private final boolean myPostBuildSyncSupported;
-  // Should current module export its module/library dependencies.
-  private final boolean myExportDependencies;
-  private final boolean myVfsRefreshRequired;
   private final boolean mySingleVariantSyncSupported;
 
   public AndroidModelFeatures(@Nullable GradleVersion modelVersion) {
     myModelVersion = modelVersion;
-    myIssueReportingSupported = modelVersionIsAtLeast("1.1.0");
-    myShadersSupported = modelVersionIsAtLeast("2.1.0");
-    myConstraintLayoutSdkLocationSupported = myModelVersion != null && myModelVersion.compareTo("2.1.0") > 0;
-    myTestedTargetVariantsSupported = myProductFlavorVersionSuffixSupported = myExternalBuildSupported = modelVersionIsAtLeast("2.2.0");
     myBuildOutputFileSupported = modelVersionIsAtLeast("4.1.0");
 
-    // https://code.google.com/p/android/issues/detail?id=170841
     if (modelVersion != null) {
-      myLayoutRenderingIssuePresent = modelVersion.getMajor() == 1 && modelVersion.getMinor() == 2 && modelVersion.getMicro() <= 2;
       // Enable post build sync if AGP is between AGP[3.0, 4.0].
       // For AGP prior to 3.0, post build sync is not supported. For 4.1 and higher, build output listing file should be used.
       myPostBuildSyncSupported = modelVersionIsAtLeast("3.0.0") && !modelVersionIsAtLeast("4.1.0");
     }
     else {
-      myLayoutRenderingIssuePresent = false;
       myPostBuildSyncSupported = false;
     }
-    boolean isPre3dot0Version = !modelVersionIsAtLeast("3.0.0");
-    // Dependencies exported should be true if android plugin version is less than 3.0.
-    // Although with IdeDependencies, each module manages their own list of dependencies, this list is not
-    // complete for pre-3.0 plugins, where java library dependencies of dependent module are always empty.
-    // See b/64521930.
-    myExportDependencies = isPre3dot0Version;
-    // With pre-3.0 AGP, AARs are exploded AFTER Gradle Sync, and IDE does not know when these files becomes available,
-    // which causes "unresolved symbol" problem. Refresh file system to make IDE pick up exploded AARs.
-    // It is not needed with AGP 3.0+, because AARs are exploded DURING Gradle Sync with 3.0+.
-    myVfsRefreshRequired = isPre3dot0Version;
     mySingleVariantSyncSupported = modelVersionIsAtLeast("3.3.0");
   }
 
@@ -73,31 +46,31 @@ public class AndroidModelFeatures {
   }
 
   public boolean isIssueReportingSupported() {
-    return myIssueReportingSupported;
+    return true;
   }
 
   public boolean isShadersSupported() {
-    return myShadersSupported;
+    return true;
   }
 
   public boolean isTestedTargetVariantsSupported() {
-    return myTestedTargetVariantsSupported;
+    return true;
   }
 
   public boolean isProductFlavorVersionSuffixSupported() {
-    return myProductFlavorVersionSuffixSupported;
+    return true;
   }
 
   public boolean isExternalBuildSupported() {
-    return myExternalBuildSupported;
+    return true;
   }
 
   public boolean isConstraintLayoutSdkLocationSupported() {
-    return myConstraintLayoutSdkLocationSupported;
+    return true;
   }
 
   public boolean isLayoutRenderingIssuePresent() {
-    return myLayoutRenderingIssuePresent;
+    return false;
   }
 
   public boolean isPostBuildSyncSupported() {
@@ -109,11 +82,11 @@ public class AndroidModelFeatures {
   }
 
   public boolean shouldExportDependencies() {
-    return myExportDependencies;
+    return false;
   }
 
   public boolean isVfsRefreshAfterBuildRequired() {
-    return myVfsRefreshRequired;
+    return false;
   }
 
   public boolean isSingleVariantSyncSupported() {
