4cefb5b052430f0f73cf0cfbae98eac8bc7f34f9
Isaac Chai <ichai@google.com>
2021-05-14 16:48:10 -0700

    [visual lint] highlight atf issues as well
    atf issues were missing highlighting before. Make sure they behave the
    same way as visual lint render issues.
    
    Bug: 184734874
    Test: Manually tested
    Change-Id: I350907d2a1dabc3457bae91805a9de0ffb3f933e
    
    GitOrigin-RevId: b06256168d32bffdd160d63a3f408967d22ef8a2

--- designer/src/com/android/tools/idea/uibuilder/visual/visuallint/VisualLintIssueProvider.kt
+++ designer/src/com/android/tools/idea/uibuilder/visual/visuallint/VisualLintIssueProvider.kt
@@ -38,13 +38,17 @@ class VisualLintIssueProvider(
     atfIssues.forEach { issue -> issueListBuilder.add(issue) }
   }
 
-  class VisualLintRenderIssueWrapper(myIssue: RenderErrorModel.Issue, val sourceModel: NlModel) : Issue() {
+  class VisualLintRenderIssueWrapper(myIssue: RenderErrorModel.Issue, val sourceModel: NlModel) : Issue(), VisualLintHighlightingIssue {
     override val source = VisualLintIssueSource(sourceModel)
     override val summary = myIssue.summary
     override val description = myIssue.htmlContent
     override val severity = myIssue.severity
     override val category = "Visual Lint Issue"
     override val hyperlinkListener = myIssue.hyperlinkListener
+    override fun shouldHighlight(model: NlModel): Boolean {
+      return sourceModel == model
+    }
+
   }
 
   data class VisualLintIssueSource(private val model: NlModel) : IssueSource {
@@ -55,4 +59,14 @@ class VisualLintIssueProvider(
       it.scrollToVisible(model, false)
     }
   }
+}
+
+/** Issue that highlights */
+interface VisualLintHighlightingIssue {
+
+  /**
+   * return true if the issue should be highlighting when selected.
+   * @param model Currently displaying model.
+   * */
+  fun shouldHighlight(model: NlModel): Boolean
 }
