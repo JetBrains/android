09c245b823121b57c718ff239b8b6f35aea925dd
Alon Albert <aalbert@google.com>
2021-05-14 14:56:15 -0700

    Extract SafeAlarm To Outer Level Class
    And add a public addRequest() method.
    
    Change-Id: Ib0d23f75643bf34e99c3bd9837e2ce81e0691fd4

--- android/testSrc/com/android/tools/idea/logcat/SafeAlarmTest.kt
+++ android/testSrc/com/android/tools/idea/logcat/SafeAlarmTest.kt
@@ -13,51 +13,74 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.android.tools.idea.logcat;
+package com.android.tools.idea.logcat
 
+import com.intellij.openapi.Disposable
+import com.intellij.openapi.util.Disposer
+import com.intellij.testFramework.EdtRule
+import com.intellij.util.Alarm.ThreadToUse.SWING_THREAD
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.junit.MockitoJUnit
+import org.mockito.junit.MockitoRule
+import java.util.concurrent.CountDownLatch
 
-import static com.intellij.util.Alarm.ThreadToUse.SWING_THREAD;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+class SafeAlarmTest {
+  @get:Rule
+  var edtRule = EdtRule()
 
-import com.intellij.openapi.Disposable;
-import com.intellij.openapi.util.Disposer;
-import com.intellij.testFramework.EdtRule;
-import java.util.concurrent.CountDownLatch;
-import org.junit.Rule;
-import org.junit.Test;
-import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnit;
-import org.mockito.junit.MockitoRule;
+  @get:Rule
+  var mockitoRule: MockitoRule = MockitoJUnit.rule()
 
-public class SafeAlarmTest {
-
-  @Rule public EdtRule myEdtRule = new EdtRule();
-  @Rule public MockitoRule myMockitoRule = MockitoJUnit.rule();
-
-  @Mock Runnable myMockRequest;
+  @Mock
+  private lateinit var mockRequest: Runnable
 
   @Test
-  public void addRequestIfNotEmpty_afterDispose_doesNotCrash() {
-    Disposable disposable = () -> {};
-    ViewListener.SafeAlarm safeAlarm = new ViewListener.SafeAlarm(disposable, SWING_THREAD);
+  fun addRequestIfNotEmpty_afterDispose_doesNotCrash() {
+    val disposable = Disposable {}
+    val safeAlarm = SafeAlarm(SWING_THREAD, disposable)
 
-    Disposer.dispose(disposable);
-    safeAlarm.addRequestIfNotEmpty(() -> {}, 0);
+    Disposer.dispose(disposable)
+    safeAlarm.addRequestIfNotEmpty({}, 0)
   }
 
   @Test
-  public void addRequestIfNotEmpty() throws Exception {
-    Disposable disposable = () -> {};
-    ViewListener.SafeAlarm safeAlarm = new ViewListener.SafeAlarm(disposable, SWING_THREAD);
+  fun addRequestIfNotEmpty() {
+    val disposable = Disposable {}
+    val safeAlarm = SafeAlarm(SWING_THREAD, disposable)
 
-    safeAlarm.addRequestIfNotEmpty(myMockRequest, 500);
-    safeAlarm.addRequestIfNotEmpty(myMockRequest, 500);
+    safeAlarm.addRequestIfNotEmpty(mockRequest, 500)
+    safeAlarm.addRequestIfNotEmpty(mockRequest, 500)
 
-    CountDownLatch latch = new CountDownLatch(1);
-    safeAlarm.addRequest(latch::countDown, 500);
-    latch.await();
-    verify(myMockRequest, times(1)).run();
-    Disposer.dispose(disposable);
+    val latch = CountDownLatch(1)
+    safeAlarm.addRequest({ latch.countDown() }, 500)
+    latch.await()
+    Mockito.verify(mockRequest, Mockito.times(1)).run()
+    Disposer.dispose(disposable)
+  }
+
+  @Test
+  fun addRequest_afterDispose_doesNotCrash() {
+    val disposable = Disposable {}
+    val safeAlarm = SafeAlarm(SWING_THREAD, disposable)
+
+    Disposer.dispose(disposable)
+    safeAlarm.addRequest({}, 0)
+  }
+
+  @Test
+  fun addRequest() {
+    val disposable = Disposable {}
+    val safeAlarm = SafeAlarm(SWING_THREAD, disposable)
+
+    safeAlarm.addRequest(mockRequest, 500)
+
+    val latch = CountDownLatch(1)
+    safeAlarm.addRequest({ latch.countDown() }, 500)
+    latch.await()
+    Mockito.verify(mockRequest, Mockito.times(1)).run()
+    Disposer.dispose(disposable)
   }
 }
