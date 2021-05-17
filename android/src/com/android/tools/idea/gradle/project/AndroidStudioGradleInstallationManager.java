7d3f5ea5184964836ae18b633456d933f0d88031
Nikolay Chashnikov <Nikolay.Chashnikov@jetbrains.com>
2021-05-17 14:43:15 +0300

    [android] get rid of method overriding deprecated 'getGradleJdk' method
    The base method isn't called anymore.
    
    GitOrigin-RevId: ba026d3af5b95459cb1971e14645572e43ad1c3b

--- android/src/com/android/tools/idea/gradle/project/AndroidStudioGradleInstallationManager.java
+++ android/src/com/android/tools/idea/gradle/project/AndroidStudioGradleInstallationManager.java
@@ -8,11 +8,6 @@ import org.jetbrains.annotations.Nullable;
 import org.jetbrains.plugins.gradle.service.GradleInstallationManager;
 
 public class AndroidStudioGradleInstallationManager extends GradleInstallationManager {
-  @Nullable
-  @Override
-  public Sdk getGradleJdk(@Nullable Project project, @NotNull String linkedProjectPath) {
-    return IdeSdks.getInstance().getJdk();
-  }
 
   @Nullable
   @Override
