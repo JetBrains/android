3aa17f80faf0d91f0bdab7bb895889f9c833e614
Christophe Rhodes <xof@google.com>
2021-05-18 14:41:30 +0100

    [Dsl] Handle Int/String Sdk properties
    Historically fooSdk properties were all set by an overloaded method
    fooSdk(), handling the fact that the argument could be an integer (for a
    released Sdk version) or a string (for a preview).  Now that these
    properties are split into two, we need some way to model the property,
    and in particular to do the right thing if client code wishes to set the
    Sdk version from one kind to the other (e.g. in a project with declared
    minSdk 30, to change to using a preview for "T" one can no longer simply
    change the argument to the application statement).
    
    PropertyTransforms allow making non-local changes to the Dsl tree in
    response to changing the values of Dsl properties.  Define a new
    transform, SdkOrPreviewTransform, which examines the type of the value
    being set and determines which of the Integer, String or generic method
    to use, optionally also constrained by version considerations.
    
    Unfortunately, this doesn't quite work, in that after carefully
    constructing a Dsl element with the right form, model effect and name,
    the name conversion mechanism kicks in at a lower level and rewrites the
    element's name to something wrong: the Dsl parser/writer do not have
    enough information to be able to determine the correct syntactic form
    for this semantic operation.  This might be indicative that there's a
    layering violation going on here, but it doesn't seem obvious to me that
    there is: it might just be that we need to be able to indicate to the
    lower level that we have already taken into consideration issues around
    syntax.
    
    In any case, the form we have is unique: a Dsl element with a
    ModelEffectDescription acting on a property, whose name is not the same
    as the GradleElementName of the underlying element, and whose effect is
    CREATE_WITH_VALUE (which is never generated when parsed).  Handle this
    case specially when generating an external name.
    
    Bug: 148657110
    Bug: 184935026
    Test: existing, more in forthcoming commits
    Change-Id: I40b4f552517d46bac57f7702ae126bf723ded4fb
    
    GitOrigin-RevId: 64ca09dfc3caad54daf9d1cfacb3b1e96afe3958

---
+++ gradle-dsl/src/com/android/tools/idea/gradle/dsl/model/ext/transforms/SdkOrPreviewTransform.java
@@ -0,0 +1,134 @@
+/*
+ * Copyright (C) 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.gradle.dsl.model.ext.transforms;
+
+import static com.android.tools.idea.gradle.dsl.model.ext.PropertyUtil.createBasicExpression;
+
+import com.android.tools.idea.gradle.dsl.api.ext.RawText;
+import com.android.tools.idea.gradle.dsl.api.ext.ReferenceTo;
+import com.android.tools.idea.gradle.dsl.parser.ExternalNameInfo;
+import com.android.tools.idea.gradle.dsl.parser.elements.GradleDslElement;
+import com.android.tools.idea.gradle.dsl.parser.elements.GradleDslExpression;
+import com.android.tools.idea.gradle.dsl.parser.elements.GradleDslSimpleExpression;
+import com.android.tools.idea.gradle.dsl.parser.elements.GradleNameElement;
+import com.android.tools.idea.gradle.dsl.parser.elements.GradlePropertiesDslElement;
+import com.android.tools.idea.gradle.dsl.parser.groovy.GroovyDslNameConverter;
+import com.android.tools.idea.gradle.dsl.parser.semantics.ModelEffectDescription;
+import com.android.tools.idea.gradle.dsl.parser.semantics.ModelPropertyDescription;
+import com.android.tools.idea.gradle.dsl.parser.semantics.ModelSemanticsDescription;
+import com.android.tools.idea.gradle.dsl.parser.semantics.VersionConstraint;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * In AGP 7.0.0, some properties which previously had been set using a single operator with
+ * overloads for multiple types (Integer or String) had their setters split into two distinct
+ * operators, one for the Integer and one for the String.  The new operators are preferred, but
+ * are only available from a certain version, and if there is uncertainty over the type of the
+ * value we should probably continue to use the older generic setter.
+ *
+ * Note: despite its name, the MultiTypePropertyModel is not suitable for this, because that assumes
+ * a single operator with multiple syntactic forms for the value.
+ */
+public class SdkOrPreviewTransform extends PropertyTransform {
+  @NotNull private ModelPropertyDescription propertyDescription;
+  @NotNull private String genericSetter;
+  @NotNull private String sdkSetter;
+  @NotNull private String previewSetter;
+  /**
+   * The constraint on versions for which the sdk- and previewSetters are available, or null (meaning
+   * no constraint).
+   */
+  @Nullable private VersionConstraint versionConstraint;
+
+  public SdkOrPreviewTransform(
+    @NotNull ModelPropertyDescription propertyDescription,
+    @NotNull String genericSetter,
+    @NotNull String sdkSetter,
+    @NotNull String previewSetter,
+    @Nullable VersionConstraint versionConstraint
+  ) {
+    super();
+    this.propertyDescription = propertyDescription;
+    this.genericSetter = genericSetter;
+    this.sdkSetter = sdkSetter;
+    this.previewSetter = previewSetter;
+    this.versionConstraint = versionConstraint;
+  }
+
+  @Override
+  public boolean test(@Nullable GradleDslElement e, @NotNull GradleDslElement holder) {
+    return e == null || e.getModelProperty() == propertyDescription;
+  }
+
+  @Override
+  public @Nullable GradleDslElement transform(@Nullable GradleDslElement e) {
+    if (e == null) return null;
+    return e;
+  }
+
+  @Override
+  public @NotNull GradleDslExpression bind(
+    @NotNull GradleDslElement holder,
+    @Nullable GradleDslElement oldElement,
+    @NotNull Object value,
+    @NotNull String name
+  ) {
+    String operatorName = genericSetter;
+    ExternalNameInfo.ExternalNameSyntax syntax = ExternalNameInfo.ExternalNameSyntax.METHOD;
+    if (versionConstraint == null || versionConstraint.isOkWith(holder.getDslFile().getContext().getAgpVersion())) {
+      if (value instanceof Integer) {
+        operatorName = sdkSetter;
+        syntax = holder.getDslFile().getWriter() instanceof GroovyDslNameConverter
+                 ? ExternalNameInfo.ExternalNameSyntax.METHOD
+                 : ExternalNameInfo.ExternalNameSyntax.ASSIGNMENT;
+      }
+      else if (value instanceof String) {
+        operatorName = previewSetter;
+        syntax = holder.getDslFile().getWriter() instanceof GroovyDslNameConverter
+                 ? ExternalNameInfo.ExternalNameSyntax.METHOD
+                 : ExternalNameInfo.ExternalNameSyntax.ASSIGNMENT;
+      }
+      else if (value instanceof ReferenceTo || value instanceof RawText) {
+        // TODO(xof): maybe we need to be cleverer here?  Resolve references?
+        operatorName = genericSetter;
+        syntax = ExternalNameInfo.ExternalNameSyntax.METHOD;
+      }
+    }
+    GradleDslSimpleExpression expression = createBasicExpression(holder, value, GradleNameElement.create(operatorName));
+    expression.setModelEffect(new ModelEffectDescription(propertyDescription, ModelSemanticsDescription.CREATE_WITH_VALUE));
+    expression.setExternalSyntax(syntax);
+    return expression;
+  }
+
+  @Override
+  public @NotNull GradleDslElement replace(
+    @NotNull GradleDslElement holder,
+    @Nullable GradleDslElement oldElement,
+    @NotNull GradleDslExpression newElement,
+    @NotNull String name
+  ) {
+    // TODO(xof): shouldn't holder always be a GradlePropertiesDslElement?
+    GradlePropertiesDslElement propertiesHolder = (GradlePropertiesDslElement) holder;
+    if (oldElement != null) {
+      propertiesHolder.replaceElement(oldElement, newElement);
+    }
+    else {
+      propertiesHolder.setNewElement(newElement);
+    }
+    return newElement;
+  }
+}
