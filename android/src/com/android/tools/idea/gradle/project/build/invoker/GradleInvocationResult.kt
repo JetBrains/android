5adba248fded2e13c7d4ba9abc027f69b738930f
Yuriy Solodkyy <solodkyy@google.com>
2021-05-18 15:39:55 +0100

    Convert GradleInvocationResult to Koltin
    before combining results from multi-build invocations such as
    building independent apps from from composite builds etc. into one
    `GradleInvocationResult`.
    
    This is required to make it possible to return an `ApkProvider` directly
    from `assemble`/`bundle` calls on `GradleBuildInvoker`. When done we
    can replace outdated usages of `IdeAndroidArtifact.outputs` usages with
    `ApkProviders` and remove the outdated property.
    
    Bug: 37797956
    Test: n/a
    Change-Id: Iffe49c1b8ae88d8b56e4dd03ed7ced243a7e52f6
    
    GitOrigin-RevId: b44c1382ef1a542117790a991d7a8f211465eb1f

---
+++ android/src/com/android/tools/idea/gradle/project/build/invoker/GradleInvocationResult.kt
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2013 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.gradle.project.build.invoker
+
+import com.android.tools.idea.gradle.util.GradleUtil
+import org.gradle.tooling.BuildCancelledException
+import org.jetbrains.annotations.TestOnly
+
+class GradleInvocationResult @JvmOverloads constructor (
+  val tasks: List<String>,
+
+  /**
+   * In production, the build error is intentionally wrapped, with relevant information exposed
+   * only through a public API, but for tests it could be useful to access it directly.
+   */
+  @get:TestOnly val buildError: Throwable?,
+
+  val model: Any? = null
+) {
+
+  val isBuildCancelled: Boolean get() = buildError != null && GradleUtil.hasCause(buildError, BuildCancelledException::class.java)
+  val isBuildSuccessful: Boolean get() = buildError == null
+}
