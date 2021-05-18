b203c0b2251673b0212703175d865f828edf3997
Dustin Lam <dustinlam@google.com>
2021-05-18 14:56:24 -0700

    Fix build.gradle.kts for benchmark modules
    Added a unit test for basic application of new benchmark module
    template covering both micro and macro benchmarks in both .gradle and
    .kts variations.
    
    Fixes: 188574828
    Test: BenchmarkModuleTest
    Change-Id: Ia7fda2447f465fc642e4948ddae60ffef4cc8489

--- android/src/com/android/tools/idea/npw/module/recipes/macrobenchmarkModule/src/main/exampleMacrobenchmarkKt.kt
+++ android/src/com/android/tools/idea/npw/module/recipes/macrobenchmarkModule/src/main/exampleMacrobenchmarkKt.kt
@@ -29,7 +29,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-class ExampleStartupBenchmark() {
+class ExampleStartupBenchmark {
     @get:Rule
     val benchmarkRule = MacrobenchmarkRule()
 
