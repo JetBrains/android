1ad85048a6155f4c2bae9a5b296b0998609e5b40
Alexey Kudravtsev <cdr@intellij.com>
2021-05-20 14:54:36 +0200

    call hierarchy: cleanup
    GitOrigin-RevId: b011f25e47f0d6c7a28927baaa3913ef961627ba

--- android/src/com/android/tools/idea/experimental/callgraph/CallGraphAction.kt
+++ android/src/com/android/tools/idea/experimental/callgraph/CallGraphAction.kt
@@ -107,7 +107,7 @@ open class ContextualCallPathBrowser(
     return ContextualCallPathTreeStructure(myProject, graph, psiElement, reverseEdges)
   }
 
-  override fun createTrees(typeToTreeMap: MutableMap<String, JTree>) {
+  override fun createTrees(typeToTreeMap: MutableMap<in String, in JTree>) {
     val group = ActionManager.getInstance().getAction(IdeActions.GROUP_CALL_HIERARCHY_POPUP) as ActionGroup
     val baseOnThisMethodAction = BaseOnThisMethodAction()
     val kinds = arrayOf(getCalleeType(), getCallerType())
