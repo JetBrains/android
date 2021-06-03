02cbda8ae9a4e827704d990c254ef0d545f5291f
Christophe Rhodes <xof@google.com>
2021-06-03 17:34:31 +0100

    [AUA] Fix another test
    On the Mac builder, there currently is apparently no version of AGP
    available later than 4.2.0.  This makes the test checking that a
    third-party plugin is marked for upgrade (and a separate entry in the
    tree) fail, because in an upgrade to 4.2 it does not need that.  Make
    the version of the plugin used in the test even earlier, such that even
    upgrades to 4.2.0 require an upgrade to the plugin version.
    
    Fixes: 189890821
    Test: this is it
    Change-Id: I00cd4bdbdb16ade1197e126ae9dc7d59cc12f010
    
    GitOrigin-RevId: c3ada804a0366103f7b9705c04d0d2aca6a560f5

--- project-system-gradle-upgrade/testSrc/com/android/tools/idea/gradle/project/upgrade/ContentManagerTest.kt
+++ project-system-gradle-upgrade/testSrc/com/android/tools/idea/gradle/project/upgrade/ContentManagerTest.kt
@@ -283,8 +283,8 @@ class ContentManagerTest {
       """
         buildscript {
           dependencies {
-            classpath 'com.android.tools.build:gradle:4.1.0'
-            classpath 'org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.20'
+            classpath 'com.android.tools.build:gradle:3.6.0'
+            classpath 'org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.10'
           }
         }
       """.trimIndent()
