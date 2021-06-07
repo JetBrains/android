2460e7979ff9102f6d5b8afc470240326971470f
Christophe Rhodes <xof@google.com>
2021-06-07 11:24:52 +0100

    Adjust LatestKnownPluginVersionProvider
    When running under bazel, the offline maven repository can be placed at
    an path relative to the (possibly absolute) temporary directory found
    from `java.io.tmpdir` -- in particular, this is how it is set up in
    presubmit.
    
    This location was completely unknown to
    LatestKnownPluginVersionProvider, which led to confusing results, as the
    -dev version of AGP would typically be in the offline repository, but
    other versions might be in other locations actually found by the
    LKPVProvider, leading to spurious return of old versions of "latest".
    
    Bug: 148081564
    Test: existing test adjusted
    Change-Id: Ic432ea0034c1ab451066064019042783acf81784

--- android/testSrc/com/android/tools/idea/gradle/util/EmbeddedDistributionPathsTest.java
+++ android/testSrc/com/android/tools/idea/gradle/util/EmbeddedDistributionPathsTest.java
@@ -36,7 +36,8 @@ public class EmbeddedDistributionPathsTest {
     File rootDir = new File(StudioPathManager.getSourcesRoot());
     List<File> expectedRepo = Arrays.asList(new File(rootDir, "out/repo"),
                                             new File(rootDir, "out/studio/repo"),
-                                            new File(rootDir, "prebuilts/tools/common/m2/repository"));
+                                            new File(rootDir, "prebuilts/tools/common/m2/repository"),
+                                            new File(System.getProperty("java.io.tmpdir"), "offline-maven-repo"));
     expectedRepo = expectedRepo.stream().filter(File::isDirectory).collect(Collectors.toList());
     // Invoke the method to test.
     List<File> paths = doFindAndroidStudioLocalMavenRepoPaths();
