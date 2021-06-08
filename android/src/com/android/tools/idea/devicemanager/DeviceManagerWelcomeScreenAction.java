3f3ffc53a05c1a578ea2f621a23e457e558bc39e
Hannah Chu <hanchu@google.com>
2021-06-08 13:00:59 -0700

    [DM] Add welcome screen action
    Bug: 157696461
    Test: DeviceManagerWelcomeScreenActionTest
    Change-Id: I27e8768ab5df8e407bd810f60e01901ee9c729f0
    
    GitOrigin-RevId: 5088888d33737a608026ada457e70d807aaa1b4b

---
+++ android/src/com/android/tools/idea/devicemanager/DeviceManagerWelcomeScreenAction.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright (C) 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.devicemanager;
+
+import com.android.tools.idea.avdmanager.HardwareAccelerationCheck;
+import com.android.tools.idea.flags.StudioFlags;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.Presentation;
+import com.intellij.openapi.project.DumbAwareAction;
+import com.intellij.serviceContainer.NonInjectable;
+import java.util.function.BooleanSupplier;
+import org.jetbrains.android.sdk.AndroidSdkUtils;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.VisibleForTesting;
+
+final class DeviceManagerWelcomeScreenAction extends DumbAwareAction {
+  private final @NotNull BooleanSupplier myEnableNewDeviceManagerPanelGet;
+  private final @NotNull BooleanSupplier myIsChromeOSAndIsNotHWAccelerated;
+  private final @NotNull BooleanSupplier myIsAndroidSdkAvailable;
+
+  @SuppressWarnings("unused")
+  private DeviceManagerWelcomeScreenAction() {
+    this(StudioFlags.ENABLE_NEW_DEVICE_MANAGER_PANEL::get,
+         HardwareAccelerationCheck::isChromeOSAndIsNotHWAccelerated,
+         AndroidSdkUtils::isAndroidSdkAvailable);
+  }
+
+  @VisibleForTesting
+  @NonInjectable
+  DeviceManagerWelcomeScreenAction(@NotNull BooleanSupplier enableNewDeviceManagerPanelGet,
+                                   @NotNull BooleanSupplier isChromeOSAndIsNotHWAccelerated,
+                                   @NotNull BooleanSupplier isAndroidSdkAvailable) {
+    myEnableNewDeviceManagerPanelGet = enableNewDeviceManagerPanelGet;
+    myIsChromeOSAndIsNotHWAccelerated = isChromeOSAndIsNotHWAccelerated;
+    myIsAndroidSdkAvailable = isAndroidSdkAvailable;
+  }
+
+  @Override
+  public void update(@NotNull AnActionEvent event) {
+    Presentation presentation = event.getPresentation();
+
+    if (myIsChromeOSAndIsNotHWAccelerated.getAsBoolean() || !myEnableNewDeviceManagerPanelGet.getAsBoolean()) {
+      presentation.setVisible(false);
+      return;
+    }
+
+    presentation.setVisible(true);
+    presentation.setEnabled(myIsAndroidSdkAvailable.getAsBoolean());
+  }
+
+  @Override
+  public void actionPerformed(@NotNull AnActionEvent event) {
+    new DeviceManagerWelcomeScreenFrame(event.getProject()).show();
+  }
+}
