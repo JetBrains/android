be707c4b5fe7492451a8905f2638c279eac6217c
Sergey Prigogin <sprigogin@google.com>
2021-06-08 18:06:58 -0700

    Build the libimage_converter native library from source
    When ImageConverter.c is changed, the prebuilt libimage_converter native library has to be updated. To update the libimage_converter library for the current platform, run
    
     bazel run //tools/adt/idea/emulator:update_libimage_converter
    
    Test: existing
    Bug: N/A
    Change-Id: I808a46535a6444e63d234267b4f578e7c41889d0
    
    GitOrigin-RevId: 55b2471477bfb1e9f1991bed72929089881ac848

--- emulator/testSrc/com/android/emulator/ImageConverterTest.kt
+++ emulator/testSrc/com/android/emulator/ImageConverterTest.kt
@@ -27,11 +27,6 @@ class ImageConverterTest {
   private val testImage = createTestImage()
 
   @Test
-  fun testLoadNativeLibrary() {
-    ImageConverter.loadNativeLibrary()
-  }
-
-  @Test
   fun testUnpackRgb888Slow() {
     val pixels = IntArray(IMAGE_SIZE)
     val t = runBenchmark { ImageConverter.unpackRgb888Slow(testImage, pixels) }
