58d497fb63de79b57c0d1a7389f6ed14b4c849cc
Yi Yang <yiyiyang@google.com>
2021-06-15 17:49:45 -0700

    Add frame events table to the summary panel
    - Transform frame event proto to rows grouped by frame number
    - Added paginated table to the frames tab
    
    Bug: 186899372
    Test: added
    Change-Id: I763b81371bbd3aecb3c803e89447c595377c3b8f
    
    GitOrigin-RevId: bd36a1774b79cd3983a790a9a4defed61937d349

--- profilers-ui/src/com/android/tools/profilers/cpu/analysis/CpuAnalysisFramesTab.kt
+++ profilers-ui/src/com/android/tools/profilers/cpu/analysis/CpuAnalysisFramesTab.kt
@@ -15,6 +15,7 @@
  */
 package com.android.tools.profilers.cpu.analysis
 
+import com.android.tools.adtui.PaginatedTableView
 import com.android.tools.profilers.StudioProfilersView
 import java.awt.BorderLayout
 
@@ -23,5 +24,22 @@ class CpuAnalysisFramesTab(profilersView: StudioProfilersView,
 ) : CpuAnalysisTab<CpuAnalysisFramesTabModel>(profilersView, model) {
   init {
     layout = BorderLayout()
+    // TODO(b/186899372): support multi-layer apps.
+    val tableView = PaginatedTableView(model.layerToTableModel.values.first(), PAGE_SIZE_VALUES)
+    tableView.table.apply {
+      showVerticalLines = true
+      showHorizontalLines = true
+      emptyText.text = "No frames in the selected range"
+      columnModel.getColumn(FrameEventTableColumn.FRAME_NUMBER.ordinal).cellRenderer = CustomBorderTableCellRenderer()
+      columnModel.getColumn(FrameEventTableColumn.TOTAL_TIME.ordinal).cellRenderer = DurationRenderer()
+      columnModel.getColumn(FrameEventTableColumn.APP.ordinal).cellRenderer = DurationRenderer()
+      columnModel.getColumn(FrameEventTableColumn.GPU.ordinal).cellRenderer = DurationRenderer()
+      columnModel.getColumn(FrameEventTableColumn.COMPOSITION.ordinal).cellRenderer = DurationRenderer()
+    }
+    add(tableView.component)
+  }
+
+  companion object {
+    val PAGE_SIZE_VALUES = arrayOf(10, 25, 50, 100)
   }
 }
--- profilers/src/com/android/tools/profilers/cpu/analysis/CpuAnalysisFramesTabModel.kt
+++ profilers/src/com/android/tools/profilers/cpu/analysis/CpuAnalysisFramesTabModel.kt
@@ -15,12 +15,112 @@
  */
 package com.android.tools.profilers.cpu.analysis
 
+import com.android.tools.adtui.model.AbstractPaginatedTableModel
 import com.android.tools.adtui.model.Range
 import com.android.tools.profilers.cpu.CpuCapture
+import java.util.concurrent.TimeUnit
+import javax.swing.RowSorter
+import javax.swing.SortOrder
 
 /**
  * Tab model for the Frames tab in the Analysis Panel.
- *
- * TODO(b/186899372): expose frame events as table model.
  */
-class CpuAnalysisFramesTabModel(val captureRange: Range) : CpuAnalysisTabModel<CpuCapture>(Type.FRAMES)
+class CpuAnalysisFramesTabModel(val captureRange: Range) : CpuAnalysisTabModel<CpuCapture>(Type.FRAMES) {
+  /**
+   * Frame table data by layer. This is lazily initialized because at the time of construction, the [CpuCapture] data is not yet available.
+   */
+  val layerToTableModel: Map<String, FrameEventTableModel> by lazy {
+    val layers = dataSeries.flatMap { it.systemTraceData?.getAndroidFrameLayers() ?: listOf() }
+    // Transform frame event proto (grouped by phase) into table model (grouped by frame number).
+    layers.associate { layer ->
+      val frameNumberToRow = mutableMapOf<Int, FrameEventRow>().also {
+        layer.phaseList.forEach { phase ->
+          phase.frameEventList.forEach { frameEvent ->
+            val frameEventRow = it.getOrPut(frameEvent.frameNumber) {
+              // Initialize start and end timestamps with capture range for the incomplete edge frames.
+              FrameEventRow(frameEvent.frameNumber, startTimeUs = captureRange.min.toLong(), endTimeUs = captureRange.max.toLong())
+            }
+            val timestampUs = TimeUnit.NANOSECONDS.toMicros(frameEvent.timestampNanoseconds)
+            val durationUs = TimeUnit.NANOSECONDS.toMicros(frameEvent.durationNanoseconds)
+            when (phase.phaseName) {
+              "App" -> {
+                frameEventRow.startTimeUs = timestampUs
+                frameEventRow.appDurationUs = durationUs
+              }
+              "GPU" -> frameEventRow.gpuDurationUs = durationUs
+              "Composition" -> frameEventRow.compositionDurationUs = durationUs
+              "Display" -> frameEventRow.endTimeUs = timestampUs
+            }
+          }
+        }
+      }
+      Pair(layer.layerName, FrameEventTableModel(frameNumberToRow.values.toMutableList()))
+    }
+  }
+}
+
+/**
+ * Data class to represent a single row in the frame events table.
+ */
+data class FrameEventRow(val frameNumber: Int,
+                         var startTimeUs: Long,
+                         var endTimeUs: Long,
+                         var appDurationUs: Long = 0L,
+                         var gpuDurationUs: Long = 0L,
+                         var compositionDurationUs: Long = 0L) {
+  val totalDurationUs: Long
+    get() = endTimeUs - startTimeUs
+}
+
+/**
+ * Table model for the frame events table.
+ */
+class FrameEventTableModel(val frameEvents: MutableList<FrameEventRow>) : AbstractPaginatedTableModel(25) {
+  override fun getDataSize(): Int {
+    return frameEvents.size
+  }
+
+  override fun getDataValueAt(dataIndex: Int, columnIndex: Int): Any {
+    return FrameEventTableColumn.values()[columnIndex].getValueFrom(frameEvents[dataIndex])
+  }
+
+  override fun sortData(sortKeys: List<RowSorter.SortKey>) {
+    if (sortKeys.isNotEmpty()) {
+      val sortKey = sortKeys[0]
+      val comparator = FrameEventTableColumn.values()[sortKey.column].getComparator()
+      if (sortKey.sortOrder == SortOrder.ASCENDING) {
+        frameEvents.sortWith(comparator)
+      }
+      else {
+        frameEvents.sortWith(comparator.reversed())
+      }
+    }
+  }
+
+  override fun getColumnCount(): Int {
+    return FrameEventTableColumn.values().size
+  }
+
+  override fun getColumnClass(columnIndex: Int): Class<*> {
+    return FrameEventTableColumn.values()[columnIndex].type
+  }
+
+  override fun getColumnName(column: Int): String {
+    return FrameEventTableColumn.values()[column].displayName
+  }
+}
+
+/**
+ * Column definition for the frame events table.
+ *
+ * @param type use Java number classes (e.g. [java.lang.Long]) to ensure proper sorting in JTable
+ */
+enum class FrameEventTableColumn(val displayName: String, val type: Class<*>, val getValueFrom: (FrameEventRow) -> Comparable<*>) {
+  FRAME_NUMBER("Frame #", java.lang.Integer::class.java, FrameEventRow::frameNumber),
+  TOTAL_TIME("Total Time", java.lang.Long::class.java, FrameEventRow::totalDurationUs),
+  APP("Application", java.lang.Long::class.java, FrameEventRow::appDurationUs),
+  GPU("Wait for GPU", java.lang.Long::class.java, FrameEventRow::gpuDurationUs),
+  COMPOSITION("Composition", java.lang.Long::class.java, FrameEventRow::compositionDurationUs);
+
+  fun getComparator(): Comparator<FrameEventRow> = compareBy(getValueFrom)
+}
