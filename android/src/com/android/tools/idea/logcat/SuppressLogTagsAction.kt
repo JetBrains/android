e7c50ab65d90f94ae8076a3f78dd12dff68ac57d
Alon Albert <aalbert@google.com>
2021-06-22 08:12:31 -0700

    Disable SuppressLogTagsAction When Not Connected to A Device
    Bug: 191722954
    Test: Manually
    Change-Id: Ibb453b0832c8122fc280f0a3e1ec48295f2b1163
    
    GitOrigin-RevId: 7794f200a67c068494686b22199db89fb5cd93a4

--- android/src/com/android/tools/idea/logcat/SuppressLogTagsAction.kt
+++ android/src/com/android/tools/idea/logcat/SuppressLogTagsAction.kt
@@ -15,19 +15,21 @@
  */
 package com.android.tools.idea.logcat
 
+import com.android.ddmlib.IDevice
+import com.android.tools.idea.ddms.DeviceContext
+import com.android.tools.idea.ddms.actions.AbstractDeviceAction
 import com.intellij.icons.AllIcons
-import com.intellij.openapi.actionSystem.AnActionEvent
-import com.intellij.openapi.project.DumbAwareAction
 import com.intellij.openapi.project.Project
 import org.jetbrains.android.util.AndroidBundle
 
-class SuppressLogTagsAction(val project: Project, val logConsole: AndroidLogConsole) :
-  DumbAwareAction(
+internal class SuppressLogTagsAction(val project: Project, context: DeviceContext, val logConsole: AndroidLogConsole) :
+  AbstractDeviceAction(
+    context,
     AndroidBundle.message("android.configure.logcat.suppress.tags.text"),
     AndroidBundle.message("android.configure.logcat.suppress.tags.description"),
     AllIcons.RunConfigurations.ShowIgnored) {
 
-  override fun actionPerformed(e: AnActionEvent) {
+  override fun performAction(device: IDevice) {
     val preferences = AndroidLogcatGlobalPreferences.getInstance()
     val dialog = SuppressLogTagsActionDialog(project, logConsole, preferences.suppressedLogTags)
     dialog.setSize(preferences.suppressedLogTagsDialogDimension.width, preferences.suppressedLogTagsDialogDimension.height)
