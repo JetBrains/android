ecceccf29dc00133c50a9eef30d0eb031aaad880
Jerome Gaillard <jgaillard@google.com>
2021-06-24 17:01:26 +0100

    Highlight problematic view in Visual Linting
    When selecting an issue, highlight the component with the problem.
    
    Bug: 190758480
    Test: checked manually
    Change-Id: I00a7b7b6258ddf56fd1f7e77f6abfd40e3197a32
    
    GitOrigin-RevId: 14d7c26862bd3d10bb73bf9c787331a57eb2fbaa

--- designer/src/com/android/tools/idea/uibuilder/visual/visuallint/VisualLintIssueProvider.kt
+++ designer/src/com/android/tools/idea/uibuilder/visual/visuallint/VisualLintIssueProvider.kt
@@ -18,6 +18,7 @@ package com.android.tools.idea.uibuilder.visual.visuallint
 import com.android.tools.idea.common.error.Issue
 import com.android.tools.idea.common.error.IssueProvider
 import com.android.tools.idea.common.error.IssueSource
+import com.android.tools.idea.common.model.NlComponent
 import com.android.tools.idea.common.model.NlModel
 import com.android.tools.idea.common.surface.DesignSurface
 import com.android.tools.idea.rendering.errors.ui.RenderErrorModel
@@ -25,39 +26,39 @@ import com.google.common.collect.ImmutableCollection
 import com.google.common.collect.ImmutableList
 
 class VisualLintIssueProvider(
-  private val sourceModel: NlModel,
-  private val renderErrorModel: RenderErrorModel,
+  private val renderErrorModel: ImmutableList<VisualLintRenderIssue>,
   private val atfIssues: ImmutableList<VisualLintAtfIssue>
 ) : IssueProvider() {
 
   override fun collectIssues(issueListBuilder: ImmutableCollection.Builder<Issue>) {
-    renderErrorModel.issues.forEach { issue: RenderErrorModel.Issue ->
-      issueListBuilder.add(VisualLintRenderIssueWrapper(issue, sourceModel))
-    }
-
+    renderErrorModel.forEach { issue -> issueListBuilder.add(issue) }
     atfIssues.forEach { issue -> issueListBuilder.add(issue) }
   }
 
-  class VisualLintRenderIssueWrapper(myIssue: RenderErrorModel.Issue, val sourceModel: NlModel) : Issue(), VisualLintHighlightingIssue {
-    override val source = VisualLintIssueSource(sourceModel)
-    override val summary = myIssue.summary
-    override val description = myIssue.htmlContent
-    override val severity = myIssue.severity
-    override val category = "Visual Lint Issue"
-    override val hyperlinkListener = myIssue.hyperlinkListener
-    override fun shouldHighlight(model: NlModel): Boolean {
-      return sourceModel == model
-    }
-
-  }
-
-  data class VisualLintIssueSource(private val model: NlModel) : IssueSource {
+  data class VisualLintIssueSource(private val model: NlModel, val component: NlComponent?) : IssueSource {
     override val displayText: String = model.modelDisplayName.orEmpty()
     override val onIssueSelected: (DesignSurface) -> Unit = {
+      if (component != null) {
+        it.selectionModel.setSelection(listOf(component))
+      }
       // Repaint DesignSurface when issue is selected to update visibility of WarningLayer
       it.repaint()
       it.scrollToVisible(model, false)
     }
+  }
+}
+
+class VisualLintRenderIssue(myIssue: RenderErrorModel.Issue,
+                            val sourceModel: NlModel,
+                            val component: NlComponent?) : Issue(), VisualLintHighlightingIssue {
+  override val source = VisualLintIssueProvider.VisualLintIssueSource(sourceModel, component)
+  override val summary = myIssue.summary
+  override val description = myIssue.htmlContent
+  override val severity = myIssue.severity
+  override val category = "Visual Lint Issue"
+  override val hyperlinkListener = myIssue.hyperlinkListener
+  override fun shouldHighlight(model: NlModel): Boolean {
+    return sourceModel == model
   }
 }
 
