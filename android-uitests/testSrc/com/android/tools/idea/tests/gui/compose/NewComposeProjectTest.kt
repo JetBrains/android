a7062293dc9e53638a6a07c786680e2ca71c05df
Diego Perez <diegoperez@google.com>
2021-07-01 16:02:28 +0100

    Update Compose to 1.0.0-rc01
    Bug: 192549450
    Test: Existing tests pass
    Change-Id: I48f9b6d3a3133879ff41938ca575406699cbe26e

--- android-uitests/testSrc/com/android/tools/idea/tests/gui/compose/NewComposeProjectTest.kt
+++ android-uitests/testSrc/com/android/tools/idea/tests/gui/compose/NewComposeProjectTest.kt
@@ -61,7 +61,8 @@ class NewComposeProjectTest {
     guiTest.getProjectFileText("app/build.gradle").run {
       assertThat(this).contains("implementation \"androidx.compose.ui:ui:")
       assertThat(this).contains("implementation \"androidx.compose.material:material:")
-      assertThat(this).contains("implementation \"androidx.compose.ui:ui-tooling:")
+      assertThat(this).contains("implementation \"androidx.compose.ui:ui-tooling-preview:")
+      assertThat(this).contains("debugImplementation \"androidx.compose.ui:ui-tooling:")
     }
     guiTest.getProjectFileText("app/src/main/java/com/google/myapplication/MainActivity.kt").run {
       assertThat(this).contains("@Composable")
