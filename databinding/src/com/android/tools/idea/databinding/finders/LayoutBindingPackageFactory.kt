11b2634947e8ae3210a98964459260ddc629fa6b
David Herman <davidherman@google.com>
2021-07-02 17:32:07 -0700

    Fix "databinding" package not resolving with JDK11
    Fixes: 180946610
    Test: Added
    Change-Id: I5a35c60cf570a23132b8340e40f11bc5c1beea04
    
    GitOrigin-RevId: b3a8ad6a42b49a5a4efa4d38a6ea4f6da706c9d0

--- databinding/src/com/android/tools/idea/databinding/finders/LayoutBindingPackageFactory.kt
+++ databinding/src/com/android/tools/idea/databinding/finders/LayoutBindingPackageFactory.kt
@@ -15,12 +15,20 @@
  */
 package com.android.tools.idea.databinding.finders
 
+import com.android.tools.idea.util.toIoFile
 import com.google.common.collect.Maps
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiDirectory
 import com.intellij.psi.PsiManager
 import com.intellij.psi.PsiPackage
+import com.intellij.psi.impl.file.PsiDirectoryFactory
 import com.intellij.psi.impl.file.PsiPackageImpl
+import com.intellij.psi.search.GlobalSearchScope
+import com.intellij.testFramework.LightVirtualFileBase
+import org.jetbrains.android.facet.AndroidFacet
+import org.jetbrains.android.facet.SourceProviderManager
+import java.io.File
 
 @Service
 class LayoutBindingPackageFactory(val project: Project) {
@@ -31,19 +39,40 @@ class LayoutBindingPackageFactory(val project: Project) {
 
   private val layoutBindingPsiPackages = Maps.newConcurrentMap<String, PsiPackage>()
 
+  private class FakeDirectory(file: File) : LightVirtualFileBase(file.absolutePath, null, -1) {
+    override fun getOutputStream(requestor: Any?, newModificationStamp: Long, newTimeStamp: Long) = throw NotImplementedError()
+    override fun getInputStream() = throw NotImplementedError()
+    override fun contentsToByteArray() = ByteArray(0)
+  }
+
   /**
    * Returns a [PsiPackage] instance for the given package name.
    *
    * If it does not exist in the cache, a new one is created.
    *
+   * @param facet The facet within which we'll be creating this package, used for finding a root
+   *   source directory.
    * @param packageName The qualified package name
    * @return A [PsiPackage] that represents the given qualified name
    */
   @Synchronized
-  fun getOrCreatePsiPackage(packageName: String): PsiPackage {
+  fun getOrCreatePsiPackage(facet: AndroidFacet, packageName: String): PsiPackage {
     return layoutBindingPsiPackages.computeIfAbsent(packageName) {
       object : PsiPackageImpl(PsiManager.getInstance(project), packageName) {
         override fun isValid(): Boolean = true
+        override fun getDirectories(scope: GlobalSearchScope): Array<PsiDirectory> {
+          // Hack alert: Since JDK9, IntelliJ APIs expect a package to be associated with a corresponding directory.
+          // However, for data binding classes, the folder *will* exist at some point but not until compilation time.
+          // It seems we can fake out the IntelliJ APIs for now with a pretend directory. If we don't do this, then
+          // when users upgrade past JDK9, all the "com.xyz.databinding" packages will look like they aren't resolving.
+          // It doesn't matter to us that this won't be the actual, final generated directory; it's just enough to fool
+          // the resolution system.
+          // See also: https://issuetracker.google.com/180946610
+          val srcDir = SourceProviderManager.getInstance(facet).sources.javaDirectories.firstOrNull()?.toIoFile() ?: return emptyArray()
+          val databindingDir = FakeDirectory(File(srcDir, packageName.replace('.', '/')))
+          // The following line creates a PsiDirectory but doesn't actually create a directory on disk
+          return arrayOf(PsiDirectoryFactory.getInstance(project).createDirectory(databindingDir))
+        }
       }
     }
   }
---
+++ databinding/testSrc/com/android/tools/idea/databinding/gradle/DataBindingJdk11Test.kt
@@ -0,0 +1,81 @@
+/*
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.databinding.gradle
+
+import com.android.tools.idea.databinding.TestDataPaths
+import com.android.tools.idea.databinding.util.isViewBindingEnabled
+import com.android.tools.idea.gradle.project.sync.GradleSyncState
+import com.android.tools.idea.testing.AndroidGradleProjectRule
+import com.android.tools.idea.testing.AndroidProjectRule
+import com.google.common.truth.Truth.assertThat
+import com.intellij.openapi.vfs.VirtualFileManager
+import com.intellij.testFramework.EdtRule
+import com.intellij.testFramework.RunsInEdt
+import com.intellij.testFramework.fixtures.JavaCodeInsightTestFixture
+import com.intellij.util.ui.UIUtil
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.RuleChain
+
+/**
+ * In JDK9, the IntelliJ logic around resolving packages changed, requiring a backing directory.
+ * This test makes sure our changes to support that work.
+ *
+ * See the project's build.gradle file, which targets JDK11.
+ */
+@RunsInEdt
+class DataBindingJdk11Test {
+  private val projectRule = AndroidGradleProjectRule()
+
+  // The tests need to run on the EDT thread but we must initialize the project rule off of it
+  @get:Rule
+  val ruleChain = RuleChain.outerRule(projectRule).around(EdtRule())!!
+
+  /**
+   * Expose the underlying project rule fixture directly.
+   *
+   * We know that the underlying fixture is a [JavaCodeInsightTestFixture] because our
+   * [AndroidProjectRule] is initialized to use the disk.
+   */
+  private val fixture
+    get() = projectRule.fixture as JavaCodeInsightTestFixture
+
+  @Before
+  fun setUp() {
+    fixture.testDataPath = TestDataPaths.TEST_DATA_ROOT
+  }
+
+  // See also: LayoutbindingPackageFactory#getOrCreatePsiPackage
+  @Test
+  fun noResolutionErrorsWhenTargetingJdk11() {
+    projectRule.load(TestDataPaths.PROJECT_USING_JDK11)
+    val syncState = GradleSyncState.getInstance(projectRule.project)
+    assertThat(syncState.isSyncNeeded().toBoolean()).isFalse()
+
+    // Make sure that all file system events up to this point have been processed.
+    VirtualFileManager.getInstance().syncRefresh()
+    UIUtil.dispatchAllInvocationEvents()
+
+    val facet = projectRule.androidFacet(":app")
+    assertThat(facet.isViewBindingEnabled()).isTrue()
+
+    val mainActivityFile = fixture.findClass("com.android.example.viewbinding.MainActivity").containingFile.virtualFile
+
+    fixture.configureFromExistingVirtualFile(mainActivityFile)
+    fixture.checkHighlighting(false, false, false)
+  }
+}
