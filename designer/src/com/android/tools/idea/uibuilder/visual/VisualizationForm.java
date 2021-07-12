e78d7a16088814da04b31278ad4faf10a443f1c6
Jerome Gaillard <jgaillard@google.com>
2021-07-12 15:47:29 +0100

    Expand horizontal space between tablets in layout validation
    This is to ensure there is enough space to draw the warning icon that
    appears when a visual lint error is present.
    
    Bug: 184734874
    Test: checked manually
    Change-Id: I978261ecd5083a5c89c4209fb48daf6c2a734884

--- designer/src/com/android/tools/idea/uibuilder/visual/VisualizationForm.java
+++ designer/src/com/android/tools/idea/uibuilder/visual/VisualizationForm.java
@@ -119,7 +119,7 @@ public class VisualizationForm
   /**
    * custom horizontal gap between different previews for {@link TabletModelsProvider}.
    */
-  @SwingCoordinate private static final int TABLET_HORIZONTAL_SCREEN_DELTA = 40;
+  @SwingCoordinate private static final int TABLET_HORIZONTAL_SCREEN_DELTA = 50;
 
   /**
    * vertical gap between different previews
