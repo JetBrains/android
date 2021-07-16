10cee54ce23b8a34393bf09c2db0c2da6fb9cf99
Ruiqi Mao <ruiqimao@google.com>
2021-07-16 15:26:28 -0700

    Split compose-designer test deps off from //tools/adt/idea/android:test_deps
    This change creates a new test_deps target for compose-designer that
    consists only of the artifacts used by its tests.
    
    Bug: 191383051
    Test: Existing tests
    Change-Id: I06e68106fd21f9d1279e20605a41b3aae9c67b81
    
    GitOrigin-RevId: 45ccd184fdf694a5a3b18123451d1e8fbf9c4162

--- compose-designer/testSrc/com/android/tools/idea/compose/preview/ComposePreviewTestSuite.kt
+++ compose-designer/testSrc/com/android/tools/idea/compose/preview/ComposePreviewTestSuite.kt
@@ -25,7 +25,7 @@ import org.junit.runner.RunWith
 class ComposePreviewTestSuite : IdeaTestSuiteBase() {
   companion object {
     init {
-      linkIntoOfflineMavenRepo("tools/adt/idea/android/test_deps.manifest")
+      linkIntoOfflineMavenRepo("tools/adt/idea/compose-designer/test_deps.manifest")
       linkIntoOfflineMavenRepo("tools/base/build-system/studio_repo.manifest")
       linkIntoOfflineMavenRepo("tools/base/third_party/kotlin/kotlin-m2repository.manifest")
     }
