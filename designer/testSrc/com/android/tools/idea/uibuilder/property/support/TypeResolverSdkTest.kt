65c931e097c29812a524a0256c8c1f5f1c8e0df4
Jens Ole Lauridsen <jlauridsen@google.com>
2021-07-16 10:29:25 -0700

    Add 1.4.0 material attribute names to AndroidAttributeTypeLookup
    Fixes: 193894599
    Test: TypeResolverSdkTest
    Change-Id: Id9a5df778bc2eaa33acb0f294e7672098900c3c3
    
    GitOrigin-RevId: 5c0d1aba5f9360c157d37612bc581e16615ad13a

--- designer/testSrc/com/android/tools/idea/uibuilder/property/support/TypeResolverSdkTest.kt
+++ designer/testSrc/com/android/tools/idea/uibuilder/property/support/TypeResolverSdkTest.kt
@@ -140,7 +140,7 @@ class TypeResolverSdkTest {
     assertThat(report.totalErrors).named(TOTAL_ERROR_MESSAGE).isEqualTo(0)
   }
 
-  // b/193894599 @Test
+  @Test
   fun testMaterialViewAttributeTypes() {
     Dependencies.add(projectRule.fixture, "appcompat/appcompat")
     Dependencies.add(projectRule.fixture, MATERIAL_ID)
