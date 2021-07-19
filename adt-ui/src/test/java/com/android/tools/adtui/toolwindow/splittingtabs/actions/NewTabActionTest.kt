06425c7ba080ad5ebff594a7897081a08d383e5d
Alon Albert <aalbert@google.com>
2021-07-19 10:14:55 -0700

    Create a ToolWindowFactory For Tabbed Tool Windows
    Will eventually also support splitting the content similar to the Terminal tool window.
    
    The code is based Terminal tool window code. Cleaned up a refactored to be a general purpose library.
    
    The Logcat V2 tool will use this component. Dependant cl will include the logcat integration.
    
    Bug: 191880898
    Test: Added
    Change-Id: Ibc427ce4d3ad2cb34ad2e37427354abc55e1d120
    
    GitOrigin-RevId: 8bd32b43039890ad3f7afa4cdbeed3eb809e477b

---
+++ adt-ui/src/test/java/com/android/tools/adtui/toolwindow/splittingtabs/actions/NewTabActionTest.kt
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.adtui.toolwindow.splittingtabs.actions
+
+import com.android.testutils.MockitoKt.mock
+import com.google.common.truth.Truth.assertThat
+import com.intellij.icons.AllIcons
+import com.intellij.openapi.project.DumbAware
+import org.junit.Test
+import org.mockito.Mockito.verify
+
+class NewTabActionTest {
+  @Test
+  fun isDumbAware() {
+    assertThat(NewTabAction(textSupplier = { "" }, createNewTab = {})).isInstanceOf(DumbAware::class.java)
+  }
+
+  @Test
+  fun testPresentation() {
+    val newTabAction = NewTabAction(textSupplier = { "Label" }, createNewTab = {})
+
+    assertThat(newTabAction.templatePresentation.text).isEqualTo("Label")
+    assertThat(newTabAction.templatePresentation.icon).isEqualTo(AllIcons.General.Add)
+  }
+
+  @Test
+  fun actionPerformed() {
+    val createNewTab = mock<() -> Unit>()
+    val newTabAction = NewTabAction(textSupplier = { "" }, createNewTab = createNewTab)
+
+    newTabAction.actionPerformed(mock())
+
+    verify(createNewTab).invoke()
+  }
+}
