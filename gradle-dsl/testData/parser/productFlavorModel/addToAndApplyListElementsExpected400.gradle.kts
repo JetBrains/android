7ea95ed56cfe8bc07df4873d3535ea0005215fe6
Christophe Rhodes <xof@google.com>
2021-07-21 17:16:18 +0100

    [Dsl] Add resourceConfigurations operator entry
    And mark resConfig and resConfig methods as being removed in AGP 8.0.0.
    
    Bug: 194192720
    Test: added AGP 4.0.0-syntax tests, adjusted existing tests
    Change-Id: I742bd8a1a4d6d73aabd1afd01cde0ccc3612b98e

--- gradle-dsl/testData/parser/productFlavorModel/addToAndApplyListElementsExpected400.gradle.kts
+++ gradle-dsl/testData/parser/productFlavorModel/addToAndApplyListElementsExpected400.gradle.kts
@@ -2,7 +2,7 @@ android {
   defaultConfig {
     consumerProguardFiles("proguard-android.txt", "proguard-rules.pro", "proguard-android-1.txt")
     proguardFiles("proguard-android.txt", "proguard-rules.pro", "proguard-android-1.txt")
-    resourceConfigurations += setOf("abcd", "efgh", "xyz")
+    resConfigs("abcd", "efgh", "xyz")
     resValue("abcd", "efgh", "ijkl")
     resValue("mnop", "qrst", "uvwx")
   }
