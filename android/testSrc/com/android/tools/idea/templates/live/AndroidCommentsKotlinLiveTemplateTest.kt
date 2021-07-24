1918bd7c0727a21c97982a260c9f0a5d6d6d3184
Dana Dahlstrom <dahlstrom@google.com>
2021-07-23 19:00:00 -0700

    Remove dates from comment templates
    Change-Id: Ie40e28d1eceae114245615b7ada1146e291e5a09
    Fixes: 157176656
    
    GitOrigin-RevId: 50507f586c0efd4ca50a2e83a9f9df8c36b9823f

--- android/testSrc/com/android/tools/idea/templates/live/AndroidCommentsKotlinLiveTemplateTest.kt
+++ android/testSrc/com/android/tools/idea/templates/live/AndroidCommentsKotlinLiveTemplateTest.kt
@@ -21,8 +21,6 @@ import com.android.tools.idea.templates.live.LiveTemplateTestCase.Location.EXPRE
 import com.android.tools.idea.templates.live.LiveTemplateTestCase.Location.OBJECT_DECLARATION
 import com.android.tools.idea.templates.live.LiveTemplateTestCase.Location.STATEMENT
 import com.android.tools.idea.templates.live.LiveTemplateTestCase.Location.TOP_LEVEL
-import com.intellij.openapi.util.Clock
-import com.intellij.util.text.DateFormatUtil
 
 class AndroidCommentsKotlinLiveTemplateTest : LiveTemplateTestCase() {
 
@@ -81,8 +79,7 @@ class AndroidCommentsKotlinLiveTemplateTest : LiveTemplateTestCase() {
     myFixture.type("something\n")
 
     // Then:
-    val today = DateFormatUtil.formatDate(Clock.getTime())
-    myFixture.checkResult(insertIntoPsiFileAt(location, content = "// ${templateName.toUpperCase()}: $today something "))
+    myFixture.checkResult(insertIntoPsiFileAt(location, content = "// ${templateName.toUpperCase()}: something "))
   }
 
   private val TEMPLATE_TODO = "todo"
