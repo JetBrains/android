c1b61fe2c02791ce4a7c614aaddf3e953168dee1
Ruiqi Mao <ruiqimao@google.com>
2021-07-26 11:35:07 -0700

    Split android-uitests from android:test_deps
    This change creates a new test_deps target that is a copy of the
    android:test_deps maven_repo to remove external dependencies on
    android:test_deps.
    
    Bug: 191383051
    Test: Existing tests
    Change-Id: Iad065a87d461f6b0135b8c2fde01aa91d5eedf77

--- android-uitests/testSrc/com/android/tools/idea/tests/gui/GuiJarTestSuite.java
+++ android-uitests/testSrc/com/android/tools/idea/tests/gui/GuiJarTestSuite.java
@@ -38,7 +38,7 @@ public class GuiJarTestSuite extends IdeaTestSuiteBase {
 
   static {
     linkIntoOfflineMavenRepo("tools/base/build-system/studio_repo.manifest");
-    linkIntoOfflineMavenRepo("tools/adt/idea/android/test_deps.manifest");
+    linkIntoOfflineMavenRepo("tools/adt/idea/android-uitests/test_deps.manifest");
     linkIntoOfflineMavenRepo("tools/base/third_party/kotlin/kotlin-m2repository.manifest");
     unzipIntoOfflineMavenRepo("tools/data-binding/data_binding_runtime.zip");
 
