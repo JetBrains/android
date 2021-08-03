201b9ec6ed832e87e19f3d456b65b79d6afdea25
Sergey Prigogin <sprigogin@google.com>
2021-08-02 18:42:14 -0700

    Remove unnecessary blank lines
    Test: existing
    Bug: N/A
    Change-Id: I3fb73eaf2a4e40b6ea5a3e23d1c19b0f46f2af6a
    
    GitOrigin-RevId: 289e9fd1e4b594c3df9209e44011031564620bae

--- emulator/src/com/android/tools/idea/emulator/NotificationHolderPanel.kt
+++ emulator/src/com/android/tools/idea/emulator/NotificationHolderPanel.kt
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.android.tools.idea.emulator
 
 import com.intellij.codeInsight.hint.HintUtil
