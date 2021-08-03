3a8744095b1b0e2b9e17c7a512abc2294ac42f32
Sergio Murguia <srmurguia@google.com>
2021-08-03 14:48:49 -0700

    Disable bundle signing when opening APK project
    We currently show a GenerateSignedApkAction in the build menu, showing
    users a wizard where they can select if they want to generate a signed
    APK or bundle.
    
    This change modifies the action to say only sign APK if the project
    opened is an APK and in that case the wizards jumps directly to the APK
    signing step.
    
    Bug: 186124820
    Test: GenerateSignedApkActionTest
    Test: Manually confirmed the menu and wizars change accordingly.
    Change-Id: I077f55909c3072059f5c803b3dde9097b4275c53
    
    GitOrigin-RevId: 020b05cdd65224b2d535587216c11b1032244cb6

--- android/src/org/jetbrains/android/actions/GenerateSignedApkAction.java
+++ android/src/org/jetbrains/android/actions/GenerateSignedApkAction.java
@@ -1,25 +1,32 @@
 package org.jetbrains.android.actions;
 
+import static org.jetbrains.android.util.AndroidUtils.getApplicationFacets;
+
 import com.android.tools.idea.flags.StudioFlags;
 import com.android.tools.idea.gradle.project.GradleProjectInfo;
 import com.android.tools.idea.project.AndroidProjectInfo;
-
 import com.google.wireless.android.vending.developer.signing.tools.extern.export.ExportEncryptedPrivateKeyTool;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.project.Project;
+import java.util.List;
 import org.jetbrains.android.exportSignedPackage.ExportSignedPackageWizard;
 import org.jetbrains.android.facet.AndroidFacet;
 import org.jetbrains.android.util.AndroidBundle;
 import org.jetbrains.annotations.NotNull;
-
-import java.util.List;
-
-import static org.jetbrains.android.util.AndroidUtils.getApplicationFacets;
+import org.jetbrains.annotations.Nullable;
+import org.jetbrains.annotations.VisibleForTesting;
 
 public class GenerateSignedApkAction extends AnAction {
   public GenerateSignedApkAction() {
     super(AndroidBundle.message(StudioFlags.RUNDEBUG_ANDROID_BUILD_BUNDLE_ENABLED.get() ? "android.generate.signed.apk.action.bundle.text" : "android.generate.signed.apk.action.text"));
+  }
+
+  @VisibleForTesting
+  static boolean allowBundleSigning(@Nullable Project project) {
+    return project != null &&
+           StudioFlags.RUNDEBUG_ANDROID_BUILD_BUNDLE_ENABLED.get() &&
+           !AndroidProjectInfo.getInstance(project).isApkProject();
   }
 
   @Override
@@ -31,8 +38,7 @@ public class GenerateSignedApkAction extends AnAction {
     assert !facets.isEmpty();
 
     ExportSignedPackageWizard wizard =
-      new ExportSignedPackageWizard(project, facets, true, StudioFlags.RUNDEBUG_ANDROID_BUILD_BUNDLE_ENABLED.get(),
-                                    new ExportEncryptedPrivateKeyTool());
+      new ExportSignedPackageWizard(project, facets, true, allowBundleSigning(project), new ExportEncryptedPrivateKeyTool());
     wizard.show();
   }
 
@@ -44,5 +50,10 @@ public class GenerateSignedApkAction extends AnAction {
                       (GradleProjectInfo.getInstance(project).isBuildWithGradle() ||
                        !AndroidProjectInfo.getInstance(project).requiresAndroidModel());
     e.getPresentation().setEnabledAndVisible(enabled);
+    if (enabled) {
+      String actionText = allowBundleSigning(project) ?  "android.generate.signed.apk.action.bundle.text"
+                                                      : "android.generate.signed.apk.action.text";
+      e.getPresentation().setText(AndroidBundle.message(actionText));
+    }
   }
 }
