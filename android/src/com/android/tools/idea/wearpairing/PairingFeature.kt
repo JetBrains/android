ea7ae0336255bdb58c79bcfb0ad4b309ed192323
Mohammad Saboorian <msab@google.com>
2021-08-13 14:26:10 +0100

    Add feature detection logic for pairing
    Bug: 196549974
    Test: PairingFeaturesTest
    
    Change-Id: I88918231cb5d98dc77da8216921fa534ae1a7836

---
+++ android/src/com/android/tools/idea/wearpairing/PairingFeature.kt
@@ -0,0 +1,75 @@
+/*
+ * Copyright (C) 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.wearpairing
+
+import com.android.ddmlib.IDevice
+import com.google.common.annotations.VisibleForTesting
+
+enum class PairingFeature(val minVersion: Int) {
+  // Applicable to phone
+  COMPANION_EMULATOR_ACTIVITY(773179735),
+
+  // Applicable to phone/watch
+  REFRESH_EMULATOR_CONNECTION(213013000),
+
+  // Applicable to phone
+  MULTI_WATCH_SINGLE_PHONE_PAIRING(213204000),
+
+  // Applicable to watch
+  REVERSE_PORT_FORWARD(210915000)
+}
+
+private const val GMSCORE_APP_ID = "com.google.android.gms"
+private val VERSION_CODE_PATTERN = "versionCode=(\\d+)".toRegex()
+private const val OEM_COMPANION_SETTING_KEY = "cw.oem_companion_package"
+private const val OEM_COMPANION_PROPERTY_KEY = "ro.oem.companion_package"
+
+@VisibleForTesting
+const val OEM_COMPANION_FALLBACK_APP_ID = "com.google.android.wearable.app"
+
+private suspend fun IDevice.getVersionCode(appId: String) =
+  VERSION_CODE_PATTERN.find(runShellCommand("dumpsys package $appId | grep versionCode | head -n1"))?.groupValues?.get(1)?.toInt()
+
+suspend fun IDevice.getCompanionAppIdForWatch(): String {
+  val settingValue = getSecureSetting(OEM_COMPANION_SETTING_KEY)
+  if (settingValue.isNotBlank() && settingValue != "null") {
+    return settingValue
+  }
+  val propertyValue = getSystemProperty(OEM_COMPANION_PROPERTY_KEY).await()
+  if (!propertyValue.isNullOrBlank() && propertyValue != "null") {
+    return propertyValue
+  }
+  return OEM_COMPANION_FALLBACK_APP_ID
+}
+
+suspend fun IDevice.hasPairingFeature(pairingFeature: PairingFeature, companionAppId: String?): Boolean {
+  return when (pairingFeature) {
+    PairingFeature.COMPANION_EMULATOR_ACTIVITY ->
+      if (companionAppId == OEM_COMPANION_FALLBACK_APP_ID) {
+        (getVersionCode(companionAppId) ?: 0) >= PairingFeature.COMPANION_EMULATOR_ACTIVITY.minVersion
+      }
+      else {
+        true
+      }
+    PairingFeature.REFRESH_EMULATOR_CONNECTION,
+    PairingFeature.MULTI_WATCH_SINGLE_PHONE_PAIRING,
+    PairingFeature.REVERSE_PORT_FORWARD ->
+      (getVersionCode(GMSCORE_APP_ID) ?: 0) >= pairingFeature.minVersion
+  }
+}
+
+private suspend fun IDevice.getSecureSetting(settingKey: String) =
+  runShellCommand("settings get secure $settingKey")
---
+++ android/testSrc/com/android/tools/idea/wearpairing/PairingFeaturesTest.kt
@@ -0,0 +1,108 @@
+/*
+ * Copyright (C) 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.wearpairing
+
+import com.android.ddmlib.IDevice
+import com.android.ddmlib.IShellOutputReceiver
+import com.google.common.truth.Truth.assertThat
+import com.google.common.util.concurrent.Futures
+import com.intellij.testFramework.LightPlatform4TestCase
+import kotlinx.coroutines.runBlocking
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
+import org.mockito.ArgumentMatchers
+import org.mockito.ArgumentMatchers.anyString
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.Mockito.doAnswer
+import org.mockito.MockitoAnnotations.openMocks
+import org.mockito.invocation.InvocationOnMock
+import org.mockito.Mockito.`when` as whenever
+
+private const val OEM_COMPANION_APP_ID = "com.example"
+
+class PairingFeaturesTest : LightPlatform4TestCase() {
+  @Test
+  fun onGettingCompanionAppId_settingsIsReadFirst() {
+    val device = createDeviceWithShellCommandResult(OEM_COMPANION_APP_ID)
+
+    runBlocking {
+      assertThat(device.getCompanionAppIdForWatch()).isEqualTo(OEM_COMPANION_APP_ID)
+    }
+  }
+
+  @Test
+  fun onGettingCompanionAppId_nothingIsNotSet_systemPropertyIsRead() {
+    val device = createDeviceWithShellCommandResult("null")
+    whenever(device.getSystemProperty(anyString())).thenReturn(Futures.immediateFuture(OEM_COMPANION_APP_ID))
+
+    runBlocking {
+      assertThat(device.getCompanionAppIdForWatch()).isEqualTo(OEM_COMPANION_APP_ID)
+    }
+  }
+
+  @Test
+  fun onGettingCompanionAppId_settingIsNotSet_returnsWear2CompanionAppId() {
+    val device = createDeviceWithShellCommandResult("null")
+    whenever(device.getSystemProperty(anyString())).thenReturn(Futures.immediateFuture(""))
+
+    runBlocking {
+      assertThat(device.getCompanionAppIdForWatch()).isEqualTo(OEM_COMPANION_FALLBACK_APP_ID)
+    }
+  }
+}
+
+@RunWith(Parameterized::class)
+class HasPairingFeatureTest(private val pairingFeature: PairingFeature) : LightPlatform4TestCase() {
+  companion object {
+    @JvmStatic
+    @Parameterized.Parameters(name = "{0}")
+    fun data(): Array<PairingFeature> {
+      return PairingFeature.values()
+    }
+  }
+
+  @Test
+  fun onHasPairingFeature_lowerVersions_fail() {
+    val device = createDeviceWithShellCommandResult("    versionCode=1 minSdk=23 targetSdk=30")
+
+    runBlocking {
+      assertThat(device.hasPairingFeature(pairingFeature, OEM_COMPANION_FALLBACK_APP_ID)).isFalse()
+    }
+  }
+
+  @Test
+  fun onHasPairingFeature_higherVersions_succeed() {
+    val device = createDeviceWithShellCommandResult("    versionCode=${pairingFeature.minVersion} minSdk=23 targetSdk=30")
+
+    runBlocking {
+      assertThat(device.hasPairingFeature(pairingFeature, OEM_COMPANION_FALLBACK_APP_ID)).isTrue()
+    }
+  }
+}
+
+private fun createDeviceWithShellCommandResult(result: String): IDevice {
+  val device = Mockito.mock(IDevice::class.java)
+  doAnswer { invocation: InvocationOnMock ->
+    val outputReceiver = invocation.getArgument<IShellOutputReceiver>(1)
+    val data = result.toByteArray()
+    outputReceiver.addOutput(data, 0, data.size)
+    null
+  }.`when`(device).executeShellCommand(anyString(),
+                                       ArgumentMatchers.any())
+  return device
+}
