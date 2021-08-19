f1bcb0529180a937af06ff4d2b0ca7f9b9edcd8c
Jens Ole Lauridsen <jlauridsen@google.com>
2021-08-19 13:27:25 -0700

    Add LaunchTask that implements new run config setting
    The new Run configuration setting for the Layout Inspector:
    - "Enable layout inspection without activity restart"
    
    Is implemented in a LaunchTask from the Layout Inspector.
    When a process is starting we set the device global attribute:
    - debug_view_attributes_application_package.
    
    The LaunchTask should also clear this attribute when the process
    ends.
    
    Fixes: 197143966
    Test: Unit test added
    Change-Id: I6d7bebe1c88685e4d2607f28dc18e5413768f1fe

---
+++ layout-inspector/src/com/android/tools/idea/layoutinspector/LayoutInspectorLaunchTaskContributor.kt
@@ -0,0 +1,95 @@
+/*
+ * Copyright (C) 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.layoutinspector
+
+import com.android.ddmlib.IDevice
+import com.android.tools.idea.appinspection.inspector.api.process.DeviceDescriptor
+import com.android.tools.idea.appinspection.inspector.api.process.ProcessDescriptor
+import com.android.tools.idea.layoutinspector.pipeline.adb.AdbUtils
+import com.android.tools.idea.layoutinspector.pipeline.appinspection.DebugViewAttributes
+import com.android.tools.idea.run.AndroidLaunchTaskContributor
+import com.android.tools.idea.run.LaunchOptions
+import com.android.tools.idea.run.tasks.LaunchContext
+import com.android.tools.idea.run.tasks.LaunchResult
+import com.android.tools.idea.run.tasks.LaunchTask
+import com.android.tools.idea.run.tasks.LaunchTaskDurations
+import com.intellij.execution.process.ProcessAdapter
+import com.intellij.execution.process.ProcessEvent
+import com.intellij.openapi.module.Module
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.text.StringUtil
+import org.jetbrains.annotations.VisibleForTesting
+
+@VisibleForTesting
+interface LayoutInspectorLaunchTask : LaunchTask
+
+/**
+ * Layout Inspector specific logic that runs when the user presses "Run" or "Debug"
+ */
+class LayoutInspectorLaunchTaskContributor : AndroidLaunchTaskContributor {
+  override fun getTask(module: Module, applicationId: String, launchOptions: LaunchOptions) = object : LayoutInspectorLaunchTask {
+    override fun getId() = LAYOUT_INSPECTOR_TOOL_WINDOW_ID
+    override fun getDescription() = "Launching Layout Inspector"
+    override fun getDuration() = LaunchTaskDurations.LAUNCH_ACTIVITY
+    override fun run(launchContext: LaunchContext): LaunchResult {
+      if (launchOptions.isInspectionWithoutActivityRestart && launchContext.device.version.apiLevel >= 29) {
+        enableDebugViewAttributes(module.project, applicationId, launchContext)
+      }
+      return LaunchResult.success()
+    }
+
+    private fun enableDebugViewAttributes(project: Project, packageName: String, launchContext: LaunchContext) {
+      val device = launchContext.device
+      val descriptor = object : DeviceDescriptor {
+        override val manufacturer: String = "" // unused
+        override val model: String = device.model
+        override val serial: String = device.serialNumber
+        override val isEmulator: Boolean = device.isEmulator
+        override val apiLevel: Int = device.version.apiLevel
+        override val version: String = "" // unused
+        override val codename: String = "" // unused
+      }
+      val process = object : ProcessDescriptor {
+        override val device: DeviceDescriptor = descriptor
+        override val abiCpuArch: String = ""
+        override val name: String = packageName
+        override val isRunning: Boolean = true
+        override val pid: Int = 0
+        override val streamId: Long = 0L
+      }
+      val adb = AdbUtils.getAdbFuture(project).get()
+      val attrs = DebugViewAttributes(adb, project, process)
+      attrs.set()
+      launchContext.processHandler.addProcessListener(object : ProcessAdapter() {
+        override fun processWillTerminate(event: ProcessEvent, willBeDestroyed: Boolean) {
+          // TODO(b/195152579) Consider creating a proper API on ProcessListener for debugger attachment
+          // This is a workaround:
+          if (event.source != launchContext.launchStatus.processHandler) {
+            launchContext.launchStatus.processHandler.addProcessListener(this)
+          }
+          else {
+            attrs.clear()
+          }
+        }
+      })
+    }
+  }
+
+  private val IDevice.model get() = when {
+    isEmulator -> avdName.takeIf { !it.isNullOrBlank() }
+    else -> getProperty(IDevice.PROP_DEVICE_MODEL)?.let { StringUtil.capitalizeWords(it, true) }
+  } ?: "Unknown"
+}
