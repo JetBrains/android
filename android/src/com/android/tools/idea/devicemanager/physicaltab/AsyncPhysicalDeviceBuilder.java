3ba586364e909ba763619c4d062402e5b222cfae
Juan C Nuno <juancnuno@google.com>
2021-08-23 14:44:59 -0700

    Enable the Pair device item
    Enable the Pair device item if the device is online and a phone or
    tablet
    
    Bug: 193702583
    Test: Not applicable
    Change-Id: Id2c4516e52a8c49cf8c3b85b0c7b6dd1db669235
    
    GitOrigin-RevId: f41063991ac3c34dfeed6a8679dacc36841a8462

--- android/src/com/android/tools/idea/devicemanager/physicaltab/AsyncPhysicalDeviceBuilder.java
+++ android/src/com/android/tools/idea/devicemanager/physicaltab/AsyncPhysicalDeviceBuilder.java
@@ -15,17 +15,23 @@
  */
 package com.android.tools.idea.devicemanager.physicaltab;
 
+import com.android.annotations.concurrency.UiThread;
+import com.android.annotations.concurrency.WorkerThread;
 import com.android.ddmlib.IDevice;
+import com.android.ddmlib.IDevice.HardwareFeature;
 import com.android.sdklib.AndroidVersion;
 import com.android.tools.idea.concurrency.FutureUtils;
 import com.android.tools.idea.ddms.DeviceNameProperties;
 import com.android.tools.idea.util.Targets;
 import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.ListeningExecutorService;
 import com.google.common.util.concurrent.MoreExecutors;
 import com.intellij.util.concurrency.AppExecutorUtil;
 import com.intellij.util.concurrency.EdtExecutorService;
 import java.time.Instant;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.concurrent.CancellationException;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
@@ -40,23 +46,59 @@ final class AsyncPhysicalDeviceBuilder {
   private final @NotNull ListenableFuture<@NotNull AndroidVersion> myVersionFuture;
   private final @NotNull ListenableFuture<@NotNull String> myModelFuture;
   private final @NotNull ListenableFuture<@NotNull String> myManufacturerFuture;
+  private final @NotNull ListenableFuture<@NotNull Boolean> myPhoneOrTabletFuture;
 
+  @UiThread
   AsyncPhysicalDeviceBuilder(@NotNull IDevice device, @NotNull Key key, @Nullable Instant lastOnlineTime) {
     myDevice = device;
     myKey = key;
     myLastOnlineTime = lastOnlineTime;
 
-    myVersionFuture = MoreExecutors.listeningDecorator(AppExecutorUtil.getAppExecutorService()).submit(device::getVersion);
+    ListeningExecutorService service = MoreExecutors.listeningDecorator(AppExecutorUtil.getAppExecutorService());
+
+    myVersionFuture = service.submit(device::getVersion);
     myModelFuture = device.getSystemProperty(IDevice.PROP_DEVICE_MODEL);
     myManufacturerFuture = device.getSystemProperty(IDevice.PROP_DEVICE_MANUFACTURER);
+    myPhoneOrTabletFuture = service.submit(this::isPhoneOrTablet);
   }
 
+  /**
+   * Called by an application pool thread
+   */
+  @WorkerThread
+  private boolean isPhoneOrTablet() {
+    String string = myDevice.getProperty(IDevice.PROP_BUILD_CHARACTERISTICS);
+
+    if (string == null) {
+      return true;
+    }
+
+    Collection<String> collection = Arrays.asList(string.split(","));
+
+    if (collection.contains(HardwareFeature.WATCH.getCharacteristic())) {
+      return false;
+    }
+
+    if (collection.contains(HardwareFeature.TV.getCharacteristic())) {
+      return false;
+    }
+
+    // noinspection RedundantIfStatement
+    if (collection.contains(HardwareFeature.AUTOMOTIVE.getCharacteristic())) {
+      return false;
+    }
+
+    return true;
+  }
+
+  @UiThread
   @NotNull ListenableFuture<@NotNull PhysicalDevice> buildAsync() {
     // noinspection UnstableApiUsage
-    return Futures.whenAllComplete(myVersionFuture, myModelFuture, myManufacturerFuture)
+    return Futures.whenAllComplete(myVersionFuture, myModelFuture, myManufacturerFuture, myPhoneOrTabletFuture)
       .call(this::build, EdtExecutorService.getInstance());
   }
 
+  @UiThread
   private @NotNull PhysicalDevice build() {
     AndroidVersion version = getDoneOrElse(myVersionFuture, AndroidVersion.DEFAULT);
 
@@ -68,12 +110,15 @@ final class AsyncPhysicalDeviceBuilder {
       .setApi(version.getApiString());
 
     if (myDevice.isOnline()) {
-      builder.addConnectionType(myKey.getConnectionType());
+      builder
+        .addConnectionType(myKey.getConnectionType())
+        .setPhoneOrTablet(getDoneOrElse(myPhoneOrTabletFuture, true));
     }
 
     return builder.build();
   }
 
+  @UiThread
   private static <V> @NotNull V getDoneOrElse(@NotNull Future<@NotNull V> future, @NotNull V defaultValue) {
     assert future.isDone();
 
