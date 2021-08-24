9a533973dfd4946d420ad75a8667449b74647c64
Christophe Rhodes <xof@google.com>
2021-08-24 15:27:00 +0100

    [AUA] Implement new upgrade rules
    Corresponding to the AGP Upgrade Assistant Product Requirements Document
    for Bumblebee.  This implementation is not yet hooked in, but is
    included here along with its tests.
    
    Bug: 189388326
    Test: unit tests added
    Change-Id: I8e76950a0b6a2504efc00ea45ace0b4d4b389f19

--- android/src/com/android/tools/idea/gradle/project/upgrade/GradlePluginUpgrade.kt
+++ android/src/com/android/tools/idea/gradle/project/upgrade/GradlePluginUpgrade.kt
@@ -28,6 +28,9 @@ import com.android.tools.idea.gradle.project.facet.gradle.GradleFacet
 import com.android.tools.idea.gradle.project.sync.hyperlink.SearchInBuildFilesHyperlink
 import com.android.tools.idea.gradle.project.sync.messages.GradleSyncMessages
 import com.android.tools.idea.gradle.project.sync.setup.post.TimeBasedReminder
+import com.android.tools.idea.gradle.project.upgrade.GradlePluginUpgradeState.Importance.FORCE
+import com.android.tools.idea.gradle.project.upgrade.GradlePluginUpgradeState.Importance.NO_UPGRADE
+import com.android.tools.idea.gradle.project.upgrade.GradlePluginUpgradeState.Importance.RECOMMEND
 import com.android.tools.idea.project.messages.MessageType.ERROR
 import com.android.tools.idea.project.messages.SyncMessage
 import com.google.common.annotations.VisibleForTesting
@@ -323,6 +326,48 @@ fun displayForceUpdatesDisabledMessage(project: Project) {
             "'${DISABLE_FORCED_UPGRADES.displayName}' to 'Off'."
   val notification = AGP_UPGRADE_NOTIFICATION_GROUP.createNotification(msg, MessageType.WARNING)
   notification.notify(project)
+}
+
+data class GradlePluginUpgradeState(
+  val importance: Importance,
+  val target: GradleVersion,
+) {
+  enum class Importance {
+    NO_UPGRADE,
+    RECOMMEND,
+    FORCE,
+  }
+}
+
+fun computeGradlePluginUpgradeState(
+  current: GradleVersion,
+  latestKnown: GradleVersion,
+  published: Set<GradleVersion>
+): GradlePluginUpgradeState {
+  if (current >= latestKnown) return GradlePluginUpgradeState(NO_UPGRADE, current)
+
+  if (!current.isPreview || current.previewType == "rc") {
+    // If our latestKnown is stable, recommend it.
+    if (!latestKnown.isPreview || latestKnown.previewType == "rc") return GradlePluginUpgradeState(RECOMMEND, latestKnown)
+    // Otherwise, look for a newer published stable.
+    val laterStable = published.filter { !it.isPreview }.filter { it > current }.maxOrNull()
+                      ?: return GradlePluginUpgradeState(NO_UPGRADE, current)
+    return GradlePluginUpgradeState(RECOMMEND, laterStable)
+  }
+  else if (current.previewType == "alpha" || current.previewType == "beta") {
+    if (latestKnown.isSnapshot) {
+      // If latestKnown is -dev and current is in the same series, leave it alone.
+      if (latestKnown.compareIgnoringQualifiers(current) == 0) return GradlePluginUpgradeState(NO_UPGRADE, current)
+      // If latestKnown is -dev and current is a preview from an earlier series, recommend an upgrade.
+      return GradlePluginUpgradeState(RECOMMEND, latestKnown)
+    }
+    // In all other cases where latestKnown is later than an alpha or beta current, force an upgrade.
+    return GradlePluginUpgradeState(FORCE, latestKnown)
+  }
+  else {
+    // Current is a snapshot, probably -dev, and is less than latestKnown.  Force an upgrade to latestKnown.
+    return GradlePluginUpgradeState(FORCE, latestKnown)
+  }
 }
 
 fun AndroidPluginInfo.maybeRecommendPluginUpgrade(project: Project) {
