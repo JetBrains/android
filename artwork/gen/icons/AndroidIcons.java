1c9a16bedad4240d3a336b5e73badf87fe962be5
Andrei Kuznetsov <andrei.kuznetsov@jetbrains.com>
2021-08-25 12:04:48 +0300

    IDEA-275522 [merge-4.3] restore android logo (used from android-ultimate)
    GitOrigin-RevId: c61c684fb703474dc24646bf6d0d1fbc55be6f86

--- artwork/gen/icons/AndroidIcons.java
+++ artwork/gen/icons/AndroidIcons.java
@@ -1,12 +1,15 @@
 package icons;
 
 import com.intellij.openapi.util.IconLoader;
+import org.jetbrains.annotations.NotNull;
 import javax.swing.Icon;
 
 public class AndroidIcons {
   private static Icon load(String path) {
     return IconLoader.getIcon(path, AndroidIcons.class);
   }
+
+  /** 16x16 */ public static final @NotNull Icon Android = load("icons/android.svg");
 
   public static class DeviceExplorer {
     public static final Icon DevicesLineup = load("/icons/explorer/devices-lineup.png"); // 300x150
