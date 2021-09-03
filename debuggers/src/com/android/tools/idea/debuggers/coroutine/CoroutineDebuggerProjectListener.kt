d1f46350ff63bc0089cd69a2595387c5866314fc
Pierfrancesco Soffritti <psoffritti@google.com>
2021-09-03 13:28:49 +0100

    Refactor logic that create coroutine debugger panel
    The current implementation has some timing issues between
    our ExecutionListener and the XDebugManagerListener inside
    DebuggerConnection.
    
    More in detail, when an app is launched twice, the second time
    this happens:
    
    1. ExecutionListener#processStarting is called, which creates
    a new DebuggerConnection (at this moment both the old and new
    one exist)
    2. XDebugManagerListener#processStopped is called, which
    calls dispose on both DebuggerConnection.
    
    This results in no panel being showed any app run after
    the first.
    
    This change adds a XDebugManagerListener, which is responsible
    of creating DebuggerConnection and of forwarding the
    processStarted call to DebuggerConnection#processStarted.
    
    Test: NA
    Bug: 18202318
    Change-Id: Ie729d715113fc081fb05c7df5dfa000fc69d9a10

--- debuggers/src/com/android/tools/idea/debuggers/coroutine/CoroutineDebuggerProjectListener.kt
+++ debuggers/src/com/android/tools/idea/debuggers/coroutine/CoroutineDebuggerProjectListener.kt
@@ -15,71 +15,54 @@
  */
 package com.android.tools.idea.debuggers.coroutine
 
-import com.android.tools.idea.run.AndroidRunConfigurationBase
-import com.intellij.execution.ExecutionListener
-import com.intellij.execution.ExecutionManager
-import com.intellij.execution.Executor
-import com.intellij.execution.configurations.ConfigurationFactory
-import com.intellij.execution.configurations.ConfigurationType
-import com.intellij.execution.configurations.RunConfiguration
-import com.intellij.execution.executors.DefaultDebugExecutor
-import com.intellij.execution.runners.ExecutionEnvironment
-import com.intellij.openapi.externalSystem.model.ProjectSystemId
-import com.intellij.openapi.externalSystem.service.execution.ExternalSystemRunConfiguration
+import com.android.tools.idea.run.deployable.SwappableProcessHandler
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import com.intellij.util.messages.MessageBusConnection
 import com.intellij.xdebugger.XDebugProcess
 import com.intellij.xdebugger.XDebuggerManager
 import com.intellij.xdebugger.XDebuggerManagerListener
 import org.jetbrains.kotlin.idea.debugger.coroutine.DebuggerConnection
 
 /**
- * Class responsible for setting up to coroutine debugger [DebuggerConnection]
+ * Class responsible for setting up the coroutine debugger panel
  */
 // TODO(b/182023182) disable when running api lower than 28
 class CoroutineDebuggerProjectListener : ProjectManagerListener {
-  private var connection: MessageBusConnection? = null
-  private var executionListener = CoroutineDebuggerExecutionListener()
+  private var associatedProject: Project? = null
 
   override fun projectOpened(project: Project) {
     if (!FlagController.isCoroutineDebuggerEnabled) {
       return
     }
 
-    // multiple projects can be opened, which causes multiple ProjectManagerListeners to be created.
+    // multiple projects can be opened at the same time, which causes multiple ProjectManagerListeners to be created.
     // ProjectManagerListeners#projectOpened is called on every listener every time a project is opened.
     // by checking this flag we prevent the same listener to register the execution listener multiple times.
-    if (executionListener.project != null) {
+    if (associatedProject != null) {
+      return
+    }
+    associatedProject = project
+
+    val connection = project.messageBus.connect(project)
+
+    val executionListener = CoroutineDebuggerListener(project)
+    connection.subscribe(XDebuggerManager.TOPIC, executionListener)
+  }
+}
+
+private class CoroutineDebuggerListener(private val project: Project) : XDebuggerManagerListener {
+  override fun processStarted(debugProcess: XDebugProcess) {
+    // we check the process handler to differentiate between regular JVM processes and Android processes.
+    // we don't want to create the panel if the process is regular JVM.
+    if (debugProcess.processHandler !is SwappableProcessHandler) {
       return
     }
 
-    connection = project.messageBus.connect(project)
+    val debuggerConnection = DebuggerConnection(project, null, null, false, alwaysShowPanel = true)
 
-    // Using an ExecutionListener to hook into the launch of an android app instead of an AndroidLaunchTaskContributor
-    // has the advantage that we need to set this up only once, when the project is opened, instead of every time an app is
-    // launched.
-    connection!!.subscribe(ExecutionManager.EXECUTION_TOPIC, executionListener)
-    executionListener.project = project
-  }
-
-  override fun projectClosed(project: Project) {
-    if (executionListener.project == project) {
-      connection?.disconnect()
-    }
-  }
-
-  private class CoroutineDebuggerExecutionListener : ExecutionListener {
-    var project: Project? = null
-
-    override fun processStarting(executorId: String, env: ExecutionEnvironment) {
-      // Checking for AndroidRunConfigurationBase makes sure that the code is executed only for Android apps, not regular JVM or
-      // kotlin. It also works for hybrid debugging on Android (native + jvm).
-      if (executorId == DefaultDebugExecutor.EXECUTOR_ID && env.runProfile is AndroidRunConfigurationBase) {
-        // The creation of DebuggerConnection registers an event listener to DebuggerManager.
-        // When a debug process is started, it triggers a callback that creates the coroutine debugger panel UI.
-        DebuggerConnection(env.project, null, null, false, alwaysShowPanel = true)
-      }
-    }
+    // creating the [DebuggerConnection] object does nothing on its own. In order for the panel to be created
+    // we need to forward the "processStarted" call to the Kotlin plugin DebuggerConnection component,
+    // which is responsible for creating the Coroutines Debugger panel
+    debuggerConnection.processStarted(debugProcess)
   }
 }
