bcf78ec7ca71ce37e41d4eee3ab7d8ea13415c59
Alexey Kudravtsev <cdr@intellij.com>
2021-09-07 17:29:02 +0200

    replace error-prone LoggerErrorProcessor.setInstance() with pull-style executeWith(); fix broken usages; port to the new method
    (cherry picked from commit a936011554b0653991ac98ee2a4e41a31ab8300f)
    
    Change-Id: Ia936011554b0653991ac98ee2a4e41a31ab8300f
    
    GitOrigin-RevId: d4d0c7a39e4d647f098571acbb83ac8039fb870f

--- android/testSrc/com/android/tools/idea/concurrency/CoroutineUtilsTest.kt
+++ android/testSrc/com/android/tools/idea/concurrency/CoroutineUtilsTest.kt
@@ -169,15 +169,12 @@ class CoroutineUtilsTest {
 
     val messages = mutableListOf<String>()
 
-    val defaultProcessor = LoggedErrorProcessor.getInstance()
-    try {
-      LoggedErrorProcessor.setNewInstance(object : LoggedErrorProcessor() {
+    LoggedErrorProcessor.executeWith<RuntimeException>(object : LoggedErrorProcessor() {
         override fun processError(category: String, message: String, t: Throwable?, details: Array<out String>): Boolean {
           messages.add(message)
           return false
         }
-      })
-
+    }) {
       val fooManager = FooManager()
       Disposer.register(projectRule.project, fooManager)
 
@@ -187,9 +184,6 @@ class CoroutineUtilsTest {
       workerExecutor.shutdown()
       workerExecutor.awaitTermination(2, TimeUnit.SECONDS)
       assertThat(messages).containsExactly("expected failure", "computing")
-    }
-    finally {
-      LoggedErrorProcessor.setNewInstance(defaultProcessor)
     }
   }
 
