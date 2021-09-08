c4a04a314a32afe1258f52089ca39e1e0f43470a
Diego Perez <diegoperez@google.com>
2021-09-08 12:51:50 +0100

    Handle the reflection case for transformed classes
    This CL stops user code from loading kotlin. and kotlinx. classes from
    the Studio plugin. It also ensures, that when called via reflection, the
    DelegatingClassLoader handles renaming of classes correctly.
    
    This fixes the problem where some user or library code uses reflection
    to load JDK specific implementations.
    
    Fixes: 198331505
    Test: Added tests
    
    Change-Id: I00906222693b4c075dbfe2491f61b2fd3acf85b3
    
    GitOrigin-RevId: 53fc27fda883e9df84e3e5ad5f39ce8efaf8c890

---
+++ android/testSrc/com/android/tools/idea/rendering/classloading/FilteringClassLoaderTest.kt
@@ -0,0 +1,113 @@
+/*
+ * Copyright (C) 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.rendering.classloading
+
+import com.android.tools.idea.rendering.classloading.loaders.DelegatingClassLoader
+import com.android.tools.idea.rendering.classloading.loaders.StaticLoader
+import org.junit.Assert.fail
+import org.junit.Test
+
+class A1
+class A2
+class B1
+class B2
+class Prefix {
+  class A1
+  class A2
+}
+
+class FilteringClassLoaderTest {
+  private val parentClassLoader = DelegatingClassLoader(null,
+                                                        StaticLoader(
+                                                          A1::class.java.name to loadClassBytes(A1::class.java),
+                                                          A2::class.java.name to loadClassBytes(A2::class.java),
+                                                          B1::class.java.name to loadClassBytes(B1::class.java),
+                                                          B2::class.java.name to loadClassBytes(B2::class.java),
+                                                          Prefix.A1::class.java.name to loadClassBytes(Prefix.A1::class.java),
+                                                          Prefix.A2::class.java.name to loadClassBytes(Prefix.A2::class.java),
+                                                        ))
+
+  @Test
+  fun `test some classes are filtered`() {
+    val allow1ClassLoader = FilteringClassLoader(parentClassLoader) {
+      // Only allow classes ending in 1
+      it.endsWith("1")
+    }
+    val allow2ClassLoader = FilteringClassLoader(parentClassLoader) {
+      // Only allow classes ending in 1
+      it.endsWith("2")
+    }
+
+    allow1ClassLoader.loadClass(A1::class.java.name)
+    allow1ClassLoader.loadClass(B1::class.java.name)
+    allow1ClassLoader.loadClass(Prefix.A1::class.java.name)
+    allow2ClassLoader.loadClass(A2::class.java.name)
+    allow2ClassLoader.loadClass(B2::class.java.name)
+    allow2ClassLoader.loadClass(Prefix.A2::class.java.name)
+
+    // The following will not be found
+    listOf(
+      A2::class.java.name,
+      B2::class.java.name,
+      Prefix::A2::class.java.name,
+    ).forEach {
+      try {
+        allow1ClassLoader.loadClass(it)
+        fail("ClassNotFoundException expected for '$it'")
+      }
+      catch (_: ClassNotFoundException) {
+      }
+    }
+
+    listOf(
+      A1::class.java.name,
+      B1::class.java.name,
+      Prefix::A1::class.java.name,
+    ).forEach {
+      try {
+        allow2ClassLoader.loadClass(it)
+        fail("ClassNotFoundException expected for '$it'")
+      }
+      catch (_: ClassNotFoundException) {
+      }
+    }
+  }
+
+  @Test
+  fun `test prefix filtering`() {
+    val filteringClassLoader = FilteringClassLoader.disallowedPrefixes(
+      parentClassLoader, listOf(
+      "com.android.tools.idea.rendering.classloading.Prefix.",
+      "androidx.test."
+    ))
+
+    filteringClassLoader.loadClass(A1::class.java.name)
+    filteringClassLoader.loadClass(B1::class.java.name)
+
+    // The following will not be found
+    listOf(
+      Prefix::A1::class.java.name,
+      Prefix::A2::class.java.name,
+    ).forEach {
+      try {
+        filteringClassLoader.loadClass(it)
+        fail("ClassNotFoundException expected for '$it'")
+      }
+      catch (_: ClassNotFoundException) {
+      }
+    }
+  }
+}
---
+++ android/testSrc/com/android/tools/idea/rendering/classloading/loaders/DelegatingClassLoaderTest.kt
@@ -0,0 +1,65 @@
+/*
+ * Copyright (C) 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.rendering.classloading.loaders
+
+import com.android.tools.idea.rendering.classloading.loadClassBytes
+import org.junit.Assert.assertNotNull
+import org.junit.Assert.fail
+import org.junit.Test
+
+private class Test
+
+internal class DelegatingClassLoaderTest {
+  @Test
+  fun `check loader successfully finds class`() {
+    val classLoader = DelegatingClassLoader(null, StaticLoader(
+      Test::class.java.canonicalName to loadClassBytes(Test::class.java)
+    ))
+
+    assertNotNull(classLoader.loadClass(Test::class.java.canonicalName))
+
+    // Check that a nonexistent class throws ClassNotFound
+    try {
+      classLoader.loadClass("class.do.not.exist")
+      fail("ClassNotFoundException expected")
+    } catch (_: ClassNotFoundException) {}
+
+    try {
+      classLoader.loadClass("")
+      fail("ClassNotFoundException expected")
+    } catch (_: ClassNotFoundException) {}
+  }
+
+  @Test
+  fun `check class renaming`() {
+    val classLoader = DelegatingClassLoader(null, StaticLoader(
+      "private.Test" to loadClassBytes(Test::class.java)
+    ))
+
+    assertNotNull(classLoader.loadClass("private.Test"))
+
+    // Check that a nonexistent class throws ClassNotFound
+    try {
+      classLoader.loadClass("class.do.not.exist")
+      fail("ClassNotFoundException expected")
+    } catch (_: ClassNotFoundException) {}
+
+    try {
+      classLoader.loadClass("")
+      fail("ClassNotFoundException expected")
+    } catch (_: ClassNotFoundException) {}
+  }
+}
