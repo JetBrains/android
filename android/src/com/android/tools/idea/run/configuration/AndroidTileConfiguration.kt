0a2fff8bacc433baee4d8c59d27855f7888c2dda
Polina Koval <kovalp@google.com>
2021-09-08 20:23:16 +0100

    Add configuration for Tile Service
    Add ability to run and debug Tile services
    
    Bug: 199303699
    Test: manually
    Change-Id: I5222d79992adb7d4d6d4d27805e86cc77fade576
    
    GitOrigin-RevId: 4eded1c925204099541b27a1c3b8e1d1a16b45f5

---
+++ android/src/com/android/tools/idea/run/configuration/AndroidTileConfiguration.kt
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.run.configuration
+
+import com.android.SdkConstants
+import com.android.tools.deployer.model.component.ComponentType
+import com.android.tools.idea.run.configuration.executors.AndroidTileConfigurationExecutor
+import com.intellij.execution.Executor
+import com.intellij.execution.configurations.ConfigurationFactory
+import com.intellij.execution.configurations.ConfigurationTypeBase
+import com.intellij.execution.runners.ExecutionEnvironment
+import com.intellij.openapi.project.Project
+import icons.StudioIcons
+import org.jetbrains.android.util.AndroidBundle
+
+class AndroidTileConfigurationType :
+  ConfigurationTypeBase(
+    ID,
+    AndroidBundle.message("android.tile.configuration.type.name"),
+    AndroidBundle.message("android.run.configuration.type.description"),
+    StudioIcons.Shell.Filetree.ANDROID_PROJECT
+  ) {
+  companion object {
+    const val ID = "AndroidTileConfigurationType"
+  }
+
+  init {
+    addFactory(object : ConfigurationFactory(this) {
+      override fun getId() = "AndroidTileConfigurationFactory"
+      override fun createTemplateConfiguration(project: Project) = AndroidTileConfiguration(project, this)
+    })
+  }
+}
+
+class AndroidTileConfiguration(project: Project, factory: ConfigurationFactory) : AndroidWearConfiguration(project, factory) {
+  override val componentType = ComponentType.TILE
+  override val userVisibleComponentTypeName = AndroidBundle.message("android.run.configuration.tile")
+  override val componentBaseClassesFqNames = arrayOf(SdkConstants.CLASS_TILE_SERVICE)
+
+  override fun getState(executor: Executor, environment: ExecutionEnvironment) = AndroidTileConfigurationExecutor(environment)
+}
+
