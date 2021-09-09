ce3073bac1b95fcbcb341912441e93f15828e6e9
Dustin Lam <dustinlam@google.com>
2021-09-08 17:01:24 -0700

    Add comments to macrobenchmark template
    Fixes: 194525110
    Test: AddBenchmarkModuleTest, BenchmarkModuleTest
    Change-Id: Ifc0d81bde6e25274239a4c79317ba81a4a2901e7
    
    GitOrigin-RevId: be3e1493d2ffcb36e1b3ca7aa85f20d4b0172fb0

--- android-npw/src/com/android/tools/idea/npw/module/recipes/macrobenchmarkModule/src/main/exampleMacrobenchmarkJava.kt
+++ android-npw/src/com/android/tools/idea/npw/module/recipes/macrobenchmarkModule/src/main/exampleMacrobenchmarkJava.kt
@@ -31,6 +31,18 @@ import org.junit.runner.RunWith;
 
 import java.util.Collections;
 
+/**
+ * This is an example startup benchmark.
+ *
+ * It navigates to the device's home screen, and launches the default activity.
+ *
+ * Before running this benchmark:
+ * 1) switch your app's active build variant in the Studio (affects Studio runs only)
+ * 2) add `<profileable shell=true>` to your app's manifest, within the `<application>` tag
+ *
+ * Run this benchmark from Studio to see startup measurements, and captured system traces
+ * for investigating your app's performance.
+ */
 @RunWith(AndroidJUnit4.class)
 public class ExampleStartupBenchmark {
 
