0b85220f2002726104cac8cdf309eff5b8bec4d7
Dustin Lam <dustinlam@google.com>
2021-09-08 17:01:24 -0700

    Add comments to macrobenchmark template
    Fixes: 194525110
    Test: AddBenchmarkModuleTest, BenchmarkModuleTest
    Change-Id: Ifc0d81bde6e25274239a4c79317ba81a4a2901e7

--- android-npw/src/com/android/tools/idea/npw/module/recipes/macrobenchmarkModule/buildGradle.kt
+++ android-npw/src/com/android/tools/idea/npw/module/recipes/macrobenchmarkModule/buildGradle.kt
@@ -81,6 +81,9 @@ android {
     }
 
     buildTypes {
+        // This benchmark buildType is used for benchmarking, and should function like your
+        // release build (for example, with minification on). It's signed with a debug key
+        // for easy local/CI testing.
         $benchmarkBuildType {
             debuggable = true
             signingConfig = $debugSigningConfig
