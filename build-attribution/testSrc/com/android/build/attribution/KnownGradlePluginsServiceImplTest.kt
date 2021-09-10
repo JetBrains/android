8922223103e2e2e2e9d0feca0efce1ab5b69478f
Maksim Lazeba <mlazeba@google.com>
2021-09-10 15:06:31 +0100

    [BA] Add more plugins to configuration cache compatibility matrix
    This CL adds configuration cache compatibility info on plugins:
     * org.sonarqube
     * realm-android
     * com.gorylenko.gradle-git-properties
     * androidx.benchmark
     * com.bugsnag.android.gradle
     * de.fayard.refreshVersions
     * org.jetbrains.intellij
     * com.google.android.gms.oss-licenses-plugin
    
    Bug: 185471133
    Test: N/A
    Change-Id: I1cf537b6a5579d78a4e618e04ccc9c5b7b319835

--- build-attribution/testSrc/com/android/build/attribution/KnownGradlePluginsServiceImplTest.kt
+++ build-attribution/testSrc/com/android/build/attribution/KnownGradlePluginsServiceImplTest.kt
@@ -42,8 +42,8 @@ class KnownGradlePluginsServiceImplTest {
     val data = LocalKnownGradlePluginsServiceImpl().gradlePluginsData
 
     assertThat(data).isNotEqualTo(GradlePluginsData.emptyData)
-    assertThat(data.pluginsInfo).hasSize(25)
-    assertThat(data.pluginsInfo.filter { it.configurationCachingCompatibleFrom == null }).hasSize(3)
+    assertThat(data.pluginsInfo).hasSize(32)
+    assertThat(data.pluginsInfo.filter { it.configurationCachingCompatibleFrom == null }).hasSize(6)
     assertThat(data.pluginsInfo.filter { it.pluginArtifact == null }).isEmpty()
   }
 
