58ed5625f1547e62db3344352048b53b2dfdda49
karimai <karimai@google.com>
2021-09-10 11:51:45 +0100

    Add Support for KMP tests execution.
    We are now able to distinguish between unit tests and android tests
    test sources when KMP is applied.
    
    minimum requirements for tests execution in KMP are:
    
    AGP: 7.0.0-alpha08
    Gradle: 6.8.2
    Kotlin plugin: 1.4.30
    Android Studio : BB canary 13 ( this change will be cherry-picked to BB branch )
    
    Bug: 176999821
    Test: Added
    
    Change-Id: If947274ece70811ac24bb4c99335ed3f2c46b6a4
    
    GitOrigin-RevId: 2ac36939525556108e2f3dbc4393551f0251ada2

--- android/testSrc/com/android/tools/idea/testartifacts/gradle/AndroidGradleConfigurationProducersTest.kt
+++ android/testSrc/com/android/tools/idea/testartifacts/gradle/AndroidGradleConfigurationProducersTest.kt
@@ -15,16 +15,29 @@
  */
 package com.android.tools.idea.testartifacts.gradle
 
-import com.android.tools.idea.flags.StudioFlags
+import com.android.tools.idea.gradle.run.AndroidGradleTestTasksProvider
+import com.android.tools.idea.testartifacts.TestConfigurationTesting
 import com.android.tools.idea.testartifacts.createAndroidGradleConfigurationFromDirectory
 import com.android.tools.idea.testartifacts.createAndroidGradleConfigurationFromFile
 import com.android.tools.idea.testartifacts.createAndroidGradleTestConfigurationFromClass
+import com.android.tools.idea.testartifacts.getPsiElement
 
 import com.android.tools.idea.testing.AndroidGradleTestCase
 import com.android.tools.idea.testing.TestProjectPaths.TEST_ARTIFACTS_KOTLIN
+import com.android.tools.idea.testing.TestProjectPaths.TEST_ARTIFACTS_KOTLIN_MULTIPLATFORM
 import com.android.tools.idea.testing.TestProjectPaths.UNIT_TESTING
 import com.google.common.truth.Truth
+import com.intellij.execution.actions.ConfigurationFromContextImpl
+import com.intellij.openapi.module.ModuleManager
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiFile
+import com.intellij.psi.impl.file.PsiJavaDirectoryImpl
 import junit.framework.TestCase
+import org.jetbrains.plugins.gradle.execution.test.runner.AllInPackageGradleConfigurationProducer
+import org.jetbrains.plugins.gradle.execution.test.runner.GradleTestTasksProvider
+import org.jetbrains.plugins.gradle.execution.test.runner.TestClassGradleConfigurationProducer
+import org.jetbrains.plugins.gradle.service.execution.GradleRunConfiguration
 
 /**
  * Tests for producing Gradle Run Configuration for Android unit test.
@@ -77,6 +90,42 @@ class AndroidGradleConfigurationProducersTest : AndroidGradleTestCase() {
   }
 
   @Throws(Exception::class)
+  fun testCanCreateGradleConfigurationFromAndroidTestKotlinClass() {
+    loadProject(TEST_ARTIFACTS_KOTLIN_MULTIPLATFORM)
+    val psiFile = getPsiElement(project, "module2/src/androidTest/kotlin/com/example/library/TestUnitTest.kt", false) as PsiFile
+    // Create a runConfiguration context based on the testClass.
+    val configurationFromContext = createConfigurationFromContext(psiFile)
+    TestCase.assertNotNull(configurationFromContext)
+    // Make sure that the configuration is created by the testClass gradle provider.
+    assertTrue(configurationFromContext!!.configurationProducer is TestClassGradleConfigurationProducer)
+
+    // Make sure that the runConfiguration test tasks we set are expected when provided by AndroidGradleTestTasksProvider.
+    checkConfigurationTasksAreAsExpected(
+      configurationFromContext,
+      psiFile.virtualFile,
+      "kotlinMultiPlatform.module2"
+    )
+  }
+
+  @Throws(Exception::class)
+  fun testCanCreateGradleConfigurationFromAndroidTestKotlinDirectory() {
+    loadProject(TEST_ARTIFACTS_KOTLIN_MULTIPLATFORM)
+    val psiFile = getPsiElement(project, "module2/src/androidTest/kotlin/com/example/library", true) as PsiJavaDirectoryImpl
+    // Create a runConfiguration context based on the testClass.
+    val configurationFromContext = createConfigurationFromContext(psiFile)
+    TestCase.assertNotNull(configurationFromContext)
+    // Make sure that the configuration is created by the AllInPackage gradle provider.
+    assertTrue(configurationFromContext!!.configurationProducer is AllInPackageGradleConfigurationProducer)
+
+    // Check that the run Configuration is a GradleRunConfiguration, and has tasks to run tests.
+    checkConfigurationTasksAreAsExpected(
+      configurationFromContext,
+      psiFile.virtualFile,
+      "kotlinMultiPlatform.module2"
+    )
+  }
+
+  @Throws(Exception::class)
   fun testAndroidGradleTestTasksProviderDoesntCreateJavaModulesTestTasks() {
     loadProject(UNIT_TESTING)
     // Here we test to verify that the configuration tasks aren't provided by the AndroidGradleTestTasksProvider.
@@ -85,5 +134,26 @@ class AndroidGradleConfigurationProducersTest : AndroidGradleTestCase() {
       project, "com.example.javalib.JavaLibJavaTest")
     TestCase.assertNotNull(gradleJavaConfiguration)
     Truth.assertThat(gradleJavaConfiguration!!.settings.taskNames).isEqualTo(listOf(":javalib:test"))
+  }
+
+  private fun createConfigurationFromContext(psiFile: PsiElement): ConfigurationFromContextImpl? {
+    val context = TestConfigurationTesting.createContext(project, psiFile)
+    return context.configurationsFromContext?.firstOrNull() as ConfigurationFromContextImpl?
+  }
+
+  private fun checkConfigurationTasksAreAsExpected(
+    configurationFromContext: ConfigurationFromContextImpl,
+    file: VirtualFile,
+    moduleName: String) {
+    val configuration = configurationFromContext.configuration as? GradleRunConfiguration
+    assertNotNull(configuration)
+    // Make sure that the tasks we set are expected when provided by AndroidGradleTestTasksProvider.
+    val module2 = ModuleManager.getInstance(project).modules
+      .first { module ->  module.name == moduleName }
+    TestCase.assertNotNull(module2)
+    val androidGradleTestTasksProvider = GradleTestTasksProvider.EP_NAME.extensions.filterIsInstance<AndroidGradleTestTasksProvider>().first()
+    TestCase.assertNotNull(androidGradleTestTasksProvider)
+
+    TestCase.assertEquals(configuration!!.settings.taskNames, androidGradleTestTasksProvider.getTasks(module2, file))
   }
 }
