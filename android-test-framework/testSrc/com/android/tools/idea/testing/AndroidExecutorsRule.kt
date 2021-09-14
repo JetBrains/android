f7cf52c31e67bedf7596c6b15ccef237b84a3fc5
Alon Albert <aalbert@google.com>
2021-09-14 14:22:43 -0700

    Allow Tests to Inject Custom Executors
    Instead of using AndroidExecutors directly, wrap it in a Mockito spy() and return optional custom executors when provided.
    
    Bug: n/a
    Test: n/a
    Change-Id: Ic355b4145e4831070c8b97fae87443108ecf1be2
    
    GitOrigin-RevId: c60accd1facbeeca7cac09615e9ed842e5c9dc69

--- android-test-framework/testSrc/com/android/tools/idea/testing/AndroidExecutorsRule.kt
+++ android-test-framework/testSrc/com/android/tools/idea/testing/AndroidExecutorsRule.kt
@@ -18,23 +18,43 @@ package com.android.tools.idea.testing
 import com.android.tools.idea.concurrency.AndroidExecutors
 import com.android.tools.idea.concurrency.AndroidIoManager
 import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.application.ModalityState
 import org.junit.rules.TestRule
 import org.junit.runner.Description
 import org.junit.runners.model.Statement
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.spy
+import java.util.concurrent.Executor
 
 /**
  * A rule that allows testing code that depends on [AndroidExecutors].
  *
  * Registers AndroidExecutors with its dependency [AndroidIoManager].
+ *
+ * This rule also allows tests to inject custom executors by passing optional replacements in the constructor.
  */
-class AndroidExecutorsRule : TestRule {
+class AndroidExecutorsRule(
+  private val workerThreadExecutor: Executor? = null,
+  private val ioThreadExecutor: Executor? = null,
+  private val uiThreadExecutor: ((ModalityState, Runnable) -> Unit)? = null,
+) : TestRule {
   override fun apply(base: Statement, description: Description): Statement {
     return object : Statement() {
       override fun evaluate() {
         TempDisposable().use {
           val application = ApplicationManager.getApplication()
           application.registerServiceInstance(AndroidIoManager::class.java, AndroidIoManager(), it)
-          application.registerServiceInstance(AndroidExecutors::class.java, AndroidExecutors(), it)
+          val androidExecutors = spy(AndroidExecutors())
+          if (workerThreadExecutor != null) {
+            `when`(androidExecutors.workerThreadExecutor).thenReturn(workerThreadExecutor)
+          }
+          if (ioThreadExecutor != null) {
+            `when`(androidExecutors.ioThreadExecutor).thenReturn(ioThreadExecutor)
+          }
+          if (uiThreadExecutor != null) {
+            `when`(androidExecutors.uiThreadExecutor).thenReturn(uiThreadExecutor)
+          }
+          application.registerServiceInstance(AndroidExecutors::class.java, androidExecutors, it)
           base.evaluate()
         }
       }
