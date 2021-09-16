ef01d9ea6e6da5719190ea3d1a8c301789e25b54
karimai <karimai@google.com>
2021-09-16 16:02:22 +0100

    Add a KMP test project
    Bug: N/A
    Test: N/A
    Change-Id: Ifbf7bc99ad6a622419cb4091e8df18927b6a1250
    
    GitOrigin-RevId: 80dfd6c7300a8adbdc862f311f8999ca39d1683d

---
+++ android/testData/projects/testArtifacts/kotlinMultiplatform/module2/src/androidTest/kotlin/com/example/library/TestUnitTest.kt
@@ -0,0 +1,12 @@
+package com.example.library
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class TestUnitTest {
+
+    @Test
+    fun test() {
+        assertEquals(4, 2 + 2)
+    }
+}
