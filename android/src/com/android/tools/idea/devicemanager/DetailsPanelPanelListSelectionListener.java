msft-mirror-aosp/platform.tools.adt.idea

commit 89049e11535be1e63e341e0cf9a32ef7d3d01366

Do not let right clicks bring up the details panel
And remove the right click menu from the Virtual tab

Bug: 200134983
Test: Not applicable
Change-Id: I8864d50a63541270eb2ba0caf7340422d35a5c19

android/src/com/android/tools/idea/devicemanager/physicaltab/PhysicalDevicePanel.java
@@ -20,7 +20,7 @@ import com.android.tools.idea.adb.wireless.PairDevicesUsingWiFiService;
 import com.android.tools.idea.concurrency.FutureUtils;
 import com.android.tools.idea.devicemanager.DetailsPanel;
 import com.android.tools.idea.devicemanager.DetailsPanelPanel;
-import com.android.tools.idea.devicemanager.DetailsPanelPanelListSelectionListener;
+import com.android.tools.idea.devicemanager.DetailsPanelPanelMouseListener;
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.util.concurrent.FutureCallback;
 import com.intellij.icons.AllIcons;
@@ -155,7 +155,7 @@ public final class PhysicalDevicePanel extends JBPanel<PhysicalDevicePanel> impl
 
   private void initTable(@NotNull Function<@NotNull PhysicalDevicePanel, @NotNull PhysicalDeviceTable> newPhysicalDeviceTable) {
     myTable = newPhysicalDeviceTable.apply(this);
-    myTable.getSelectionModel().addListSelectionListener(new DetailsPanelPanelListSelectionListener<>(this));
+    myTable.addMouseListener(new DetailsPanelPanelMouseListener<>(this));
   }
 
   private @NotNull List<@NotNull PhysicalDevice> addOfflineDevices(@NotNull List<@NotNull PhysicalDevice> onlineDevices) {

android/src/com/android/tools/idea/devicemanager/virtualtab/VirtualDevicePanel.java
@@ -21,7 +21,7 @@ import com.android.tools.idea.avdmanager.AvdUiAction.AvdInfoProvider;
 import com.android.tools.idea.avdmanager.CreateAvdAction;
 import com.android.tools.idea.devicemanager.DetailsPanel;
 import com.android.tools.idea.devicemanager.DetailsPanelPanel;
-import com.android.tools.idea.devicemanager.DetailsPanelPanelListSelectionListener;
+import com.android.tools.idea.devicemanager.DetailsPanelPanelMouseListener;
 import com.android.tools.idea.flags.StudioFlags;
 import com.intellij.icons.AllIcons;
 import com.intellij.ide.BrowserUtil;
@@ -104,7 +104,7 @@ public final class VirtualDevicePanel extends JBPanel<VirtualDevicePanel> implem
 
     table.setShowGrid(false);
     table.getTableHeader().setReorderingAllowed(false);
-    table.getSelectionModel().addListSelectionListener(new DetailsPanelPanelListSelectionListener<>(this));
+    table.addMouseListener(new DetailsPanelPanelMouseListener<>(this));
   }
 
   private @NotNull GroupLayout createGroupLayout() {

android/src/com/android/tools/idea/devicemanager/DetailsPanelPanelMouseListener.java
@@ -15,21 +15,31 @@
  */
 package com.android.tools.idea.devicemanager;
 
+import java.awt.Component;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.util.Optional;
-import javax.swing.event.ListSelectionEvent;
-import javax.swing.event.ListSelectionListener;
+import javax.swing.SwingUtilities;
 import org.jetbrains.annotations.NotNull;
 
-public final class DetailsPanelPanelListSelectionListener<D> implements ListSelectionListener {
+public final class DetailsPanelPanelMouseListener<D> extends MouseAdapter {
   private final @NotNull DetailsPanelPanel<@NotNull D> myPanel;
 
-  public DetailsPanelPanelListSelectionListener(@NotNull DetailsPanelPanel<@NotNull D> panel) {
+  public DetailsPanelPanelMouseListener(@NotNull DetailsPanelPanel<@NotNull D> panel) {
     myPanel = panel;
   }
 
   @Override
-  public void valueChanged(@NotNull ListSelectionEvent event) {
-    if (event.getValueIsAdjusting()) {
+  public void mousePressed(@NotNull MouseEvent event) {
+    if (!((Component)event.getSource()).isEnabled()) {
+      return;
+    }
+
+    if (!SwingUtilities.isLeftMouseButton(event)) {
+      return;
+    }
+
+    if (event.isConsumed()) {
       return;
     }
 

android/src/com/android/tools/idea/devicemanager/virtualtab/VirtualDisplayList.kt
@@ -18,7 +18,6 @@ package com.android.tools.idea.devicemanager.virtualtab
 import com.android.sdklib.internal.avd.AvdInfo
 import com.android.tools.idea.avdmanager.AccelerationErrorCode
 import com.android.tools.idea.avdmanager.AccelerationErrorNotificationPanel
-import com.android.tools.idea.avdmanager.AvdActionPanel
 import com.android.tools.idea.avdmanager.AvdActionPanel.AvdRefreshProvider
 import com.android.tools.idea.avdmanager.AvdDisplayList
 import com.android.tools.idea.avdmanager.AvdManagerConnection
@@ -42,8 +41,6 @@ import com.intellij.util.ui.ListTableModel
 import org.jetbrains.annotations.TestOnly
 import java.awt.BorderLayout
 import java.awt.event.KeyEvent
-import java.awt.event.MouseAdapter
-import java.awt.event.MouseEvent
 import java.util.concurrent.CountDownLatch
 import java.util.concurrent.atomic.AtomicBoolean
 import javax.swing.BoxLayout
@@ -93,14 +90,9 @@ class VirtualDisplayList @TestOnly constructor(
       selectionModel.selectionMode = ListSelectionModel.SINGLE_SELECTION
       selectionModel.addListSelectionListener(this)
 
-      val adapter = avdActionPanelMouseAdapter()
-
-      addMouseListener(adapter)
       addMouseMotionListener(ActionsTableCellEditorMouseMotionListener(table))
-      addMouseMotionListener(adapter)
 
       val map = getInputMap(JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT)
-
       map.put(KeyStroke.getKeyStroke(KeyEvent.VK_DELETE, 0), "deleteAvd")
       map.put(KeyStroke.getKeyStroke(KeyEvent.VK_BACK_SPACE, 0), "deleteAvd")
     }
@@ -171,34 +163,6 @@ class VirtualDisplayList @TestOnly constructor(
   @VisibleForTesting
   fun getTableItems(): List<AvdInfo> {
     return tableModel.items
-  }
-
-  private fun avdActionPanelMouseAdapter(): MouseAdapter = object : MouseAdapter() {
-    override fun mousePressed(e: MouseEvent) {
-      possiblyShowPopup(e)
-    }
-
-    override fun mouseReleased(e: MouseEvent) {
-      possiblyShowPopup(e)
-    }
-  }
-
-  private fun possiblyShowPopup(e: MouseEvent) {
-    if (!e.isPopupTrigger) {
-      return
-    }
-    val p = e.point
-    val row = table.rowAtPoint(p)
-    val col = table.columnAtPoint(p)
-    if (row != -1 && col != -1) {
-      val lastColumn = table.columnCount - 1
-      val maybeActionPanel = table.getCellRenderer(row, lastColumn).getTableCellRendererComponent(
-        table, table.getValueAt(row, lastColumn), false, true, row, lastColumn
-      )
-      if (maybeActionPanel is AvdActionPanel) {
-        maybeActionPanel.showPopup(table, e)
-      }
-    }
   }
 
   // needs an initialized table

JetBrains/android

commit 2f03feec11db8941b49f5695f13b6ffab8cb12f3

Partially revert 89049e11535
89049e11535be1e63e341e0cf9a32ef7d3d01366 broke the way the details panel
worked with the keyboard. We want it to be driven by changes in the
table selection, regardless if the selection was changed with the mouse
or keyboard.

As the right click menu in the Virtual table is still gone, this is a
good enough mitigation for Issue 200134983

Bug: 200134983
Test: Not applicable
Change-Id: I54d6e6880c3a92d5c062d7e087b21a7ead2c9cfa

GitOrigin-RevId: 6e16fc23ab832e6c807bb93ffb761c47361e9dc9

android/src/com/android/tools/idea/devicemanager/DetailsPanelPanelListSelectionListener.java
@@ -15,31 +15,21 @@
  */
 package com.android.tools.idea.devicemanager;
 
-import java.awt.Component;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 import java.util.Optional;
-import javax.swing.SwingUtilities;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 import org.jetbrains.annotations.NotNull;
 
-public final class DetailsPanelPanelMouseListener<D> extends MouseAdapter {
+public final class DetailsPanelPanelListSelectionListener<D> implements ListSelectionListener {
   private final @NotNull DetailsPanelPanel<@NotNull D> myPanel;
 
-  public DetailsPanelPanelMouseListener(@NotNull DetailsPanelPanel<@NotNull D> panel) {
+  public DetailsPanelPanelListSelectionListener(@NotNull DetailsPanelPanel<@NotNull D> panel) {
     myPanel = panel;
   }
 
   @Override
-  public void mousePressed(@NotNull MouseEvent event) {
-    if (!((Component)event.getSource()).isEnabled()) {
-      return;
-    }
-
-    if (!SwingUtilities.isLeftMouseButton(event)) {
-      return;
-    }
-
-    if (event.isConsumed()) {
+  public void valueChanged(@NotNull ListSelectionEvent event) {
+    if (event.getValueIsAdjusting()) {
       return;
     }
 

android/src/com/android/tools/idea/devicemanager/physicaltab/PhysicalDevicePanel.java
@@ -20,7 +20,7 @@ import com.android.tools.idea.adb.wireless.PairDevicesUsingWiFiService;
 import com.android.tools.idea.concurrency.FutureUtils;
 import com.android.tools.idea.devicemanager.DetailsPanel;
 import com.android.tools.idea.devicemanager.DetailsPanelPanel;
-import com.android.tools.idea.devicemanager.DetailsPanelPanelMouseListener;
+import com.android.tools.idea.devicemanager.DetailsPanelPanelListSelectionListener;
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.util.concurrent.FutureCallback;
 import com.intellij.icons.AllIcons;
@@ -155,7 +155,7 @@ public final class PhysicalDevicePanel extends JBPanel<PhysicalDevicePanel> impl
 
   private void initTable(@NotNull Function<@NotNull PhysicalDevicePanel, @NotNull PhysicalDeviceTable> newPhysicalDeviceTable) {
     myTable = newPhysicalDeviceTable.apply(this);
-    myTable.addMouseListener(new DetailsPanelPanelMouseListener<>(this));
+    myTable.getSelectionModel().addListSelectionListener(new DetailsPanelPanelListSelectionListener<>(this));
   }
 
   private @NotNull List<@NotNull PhysicalDevice> addOfflineDevices(@NotNull List<@NotNull PhysicalDevice> onlineDevices) {

android/src/com/android/tools/idea/devicemanager/virtualtab/VirtualDevicePanel.java
@@ -21,7 +21,7 @@ import com.android.tools.idea.avdmanager.AvdUiAction.AvdInfoProvider;
 import com.android.tools.idea.avdmanager.CreateAvdAction;
 import com.android.tools.idea.devicemanager.DetailsPanel;
 import com.android.tools.idea.devicemanager.DetailsPanelPanel;
-import com.android.tools.idea.devicemanager.DetailsPanelPanelMouseListener;
+import com.android.tools.idea.devicemanager.DetailsPanelPanelListSelectionListener;
 import com.android.tools.idea.flags.StudioFlags;
 import com.intellij.icons.AllIcons;
 import com.intellij.ide.BrowserUtil;
@@ -104,7 +104,7 @@ public final class VirtualDevicePanel extends JBPanel<VirtualDevicePanel> implem
 
     table.setShowGrid(false);
     table.getTableHeader().setReorderingAllowed(false);
-    table.addMouseListener(new DetailsPanelPanelMouseListener<>(this));
+    table.getSelectionModel().addListSelectionListener(new DetailsPanelPanelListSelectionListener<>(this));
   }
 
   private @NotNull GroupLayout createGroupLayout() {