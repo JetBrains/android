746317b0ee6d0154030092e7cf3652267847df5e
Aleksey Pivovarov <AMPivovarov@gmail.com>
2021-09-17 15:05:23 +0300

    platform: restore OpenProjectTask flag values after refactorings and merge
    Restore original flag values from 43cc4934dbbcf1ad06cd1a89d3e8bb2b5cf6d5ed
    
    (cherry picked from commit 0fdf66e405f5792795df1c164927b423cedd6252)
    
    Change-Id: I0fdf66e405f5792795df1c164927b423cedd6252
    
    GitOrigin-RevId: 0179e022abd372a386c89edf370e5fc5b89a4518

--- project-system-gradle-psd/src/com/android/tools/idea/gradle/adtimport/AdtImportBuilder.java
+++ project-system-gradle-psd/src/com/android/tools/idea/gradle/adtimport/AdtImportBuilder.java
@@ -187,7 +187,7 @@ public class AdtImportBuilder extends ProjectImportBuilder<String> {
         request.isNewProject = true;
         importer.importProjectNoSync(request);
         Path projectDir = getBaseDirPath(request.project).getAbsoluteFile().toPath();
-        ProjectManagerEx.getInstanceEx().openProject(projectDir, new OpenProjectTask(true, null, false, false, project, null, true, null, null, -1, -1, true, false, true, null, false, true, null, null, null));
+        ProjectManagerEx.getInstanceEx().openProject(projectDir, OpenProjectTask.withProjectToClose(project, null, true));
       }
       else {
         GradleSyncInvoker.getInstance().requestProjectSync(project, TRIGGER_IMPORT_ADT_MODULE, syncListener);
