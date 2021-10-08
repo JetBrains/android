eb27107a30429fb9844e076ae081996e3e13f083
Christophe Rhodes <xof@google.com>
2021-10-08 10:23:01 +0100

    [AUA] Give the okButton a default action look
    This should make it a bit more obvious that further action is needed by
    the user.
    
    Bug: 200975232
    Test: manual (all three built-in themes)
    Change-Id: I723476505656cd4da3cbcf07378b77e7e5dbe7aa
    
    GitOrigin-RevId: 4ed1073b0ab20596a09be3d53238b560777ce352

--- android/src/com/android/tools/idea/gradle/project/upgrade/ContentManager.kt
+++ android/src/com/android/tools/idea/gradle/project/upgrade/ContentManager.kt
@@ -31,6 +31,7 @@ import com.android.tools.idea.observable.core.OptionalValueProperty
 import com.google.wireless.android.sdk.stats.UpgradeAssistantEventInfo.UpgradeAssistantEventKind.FAILURE_PREDICTED
 import com.intellij.icons.AllIcons
 import com.intellij.ide.plugins.newui.HorizontalLayout
+import com.intellij.ide.ui.laf.darcula.ui.DarculaButtonUI.DEFAULT_STYLE_KEY
 import com.intellij.openapi.Disposable
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.application.ModalityState
@@ -594,6 +595,7 @@ class ContentManager(val project: Project) {
         toolTipText = uiState.runTooltip
         isEnabled = uiState.runEnabled
       }
+      putClientProperty(DEFAULT_STYLE_KEY, true)
     }
     val previewButton = JButton("Show Usages").apply {
       addActionListener { this@View.model.runUpgrade(true) }
@@ -679,7 +681,6 @@ class ContentManager(val project: Project) {
       add(versionTextField)
       // TODO(xof): make these buttons come in a platform-dependent order
       add(refreshButton)
-      // TODO(xof): make this look like a default button
       add(okButton)
       add(previewButton)
       add(messageLabel)
