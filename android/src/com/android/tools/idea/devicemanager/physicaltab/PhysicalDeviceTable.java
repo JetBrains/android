3459812f9341ee3e14bd986d0bb2151027ef1467
Juan C Nuno <juancnuno@google.com>
2021-10-18 15:39:10 -0700

    Switch to the split actions
    Bug: 202412314
    Test: Not applicable
    Change-Id: I62cc05fe57762b531f3ebb7355401091f8b74f5b
    
    GitOrigin-RevId: 9942f3e8e642beffafc2877d2c0cb80ea7135437

--- android/src/com/android/tools/idea/devicemanager/physicaltab/PhysicalDeviceTable.java
+++ android/src/com/android/tools/idea/devicemanager/physicaltab/PhysicalDeviceTable.java
@@ -16,11 +16,8 @@
 package com.android.tools.idea.devicemanager.physicaltab;
 
 import com.android.tools.idea.avdmanager.ApiLevelComparator;
-import com.android.tools.idea.devicemanager.ActionsTableCellEditorMouseMotionListener;
 import com.android.tools.idea.devicemanager.Device;
-import com.android.tools.idea.devicemanager.Table;
 import com.android.tools.idea.devicemanager.Tables;
-import com.android.tools.idea.devicemanager.physicaltab.PhysicalDeviceTableModel.Actions;
 import com.android.tools.idea.devicemanager.physicaltab.PhysicalDeviceTableModel.ActivateDeviceFileExplorerWindowValue;
 import com.android.tools.idea.devicemanager.physicaltab.PhysicalDeviceTableModel.PopUpMenuValue;
 import com.android.tools.idea.devicemanager.physicaltab.PhysicalDeviceTableModel.RemoveValue;
@@ -28,7 +25,6 @@ import com.google.common.annotations.VisibleForTesting;
 import com.intellij.icons.AllIcons;
 import com.intellij.openapi.project.Project;
 import com.intellij.ui.table.JBTable;
-import java.awt.Point;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
@@ -36,7 +32,6 @@ import java.util.Comparator;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-import javax.swing.ActionMap;
 import javax.swing.DefaultRowSorter;
 import javax.swing.ListSelectionModel;
 import javax.swing.RowSorter;
@@ -50,7 +45,7 @@ import javax.swing.table.TableModel;
 import javax.swing.table.TableRowSorter;
 import org.jetbrains.annotations.NotNull;
 
-final class PhysicalDeviceTable extends JBTable implements Table {
+final class PhysicalDeviceTable extends JBTable {
   PhysicalDeviceTable(@NotNull PhysicalDevicePanel panel) {
     this(panel, new PhysicalDeviceTableModel());
   }
@@ -60,44 +55,23 @@ final class PhysicalDeviceTable extends JBTable implements Table {
     super(model);
     model.addTableModelListener(event -> sizeApiTypeAndActionsColumnWidthsToFit());
 
-    if (PhysicalDeviceTableModel.SPLIT_ACTIONS_ENABLED) {
-      Project project = panel.getProject();
-      assert project != null;
+    Project project = panel.getProject();
+    assert project != null;
 
-      setDefaultEditor(ActivateDeviceFileExplorerWindowValue.class, new ActivateDeviceFileExplorerWindowButtonTableCellEditor(project));
-      setDefaultEditor(RemoveValue.class, new RemoveButtonTableCellEditor(panel));
-      setDefaultEditor(PopUpMenuValue.class, new PhysicalDevicePopUpMenuButtonTableCellEditor(project));
-
-      setDefaultRenderer(ActivateDeviceFileExplorerWindowValue.class, new ActivateDeviceFileExplorerWindowButtonTableCellRenderer());
-      setDefaultRenderer(RemoveValue.class, new RemoveButtonTableCellRenderer());
-      setDefaultRenderer(PopUpMenuValue.class, new IconButtonTableCellRenderer(AllIcons.Actions.More));
-    }
-    else {
-      setDefaultEditor(Actions.class, new ActionsTableCellEditor(panel));
-      setDefaultRenderer(Actions.class, new ActionsTableCellRenderer());
-    }
+    setDefaultEditor(ActivateDeviceFileExplorerWindowValue.class, new ActivateDeviceFileExplorerWindowButtonTableCellEditor(project));
+    setDefaultEditor(RemoveValue.class, new RemoveButtonTableCellEditor(panel));
+    setDefaultEditor(PopUpMenuValue.class, new PhysicalDevicePopUpMenuButtonTableCellEditor(project));
 
     setDefaultRenderer(Device.class, new PhysicalDeviceTableCellRenderer());
+    setDefaultRenderer(ActivateDeviceFileExplorerWindowValue.class, new ActivateDeviceFileExplorerWindowButtonTableCellRenderer());
+    setDefaultRenderer(RemoveValue.class, new RemoveButtonTableCellRenderer());
+    setDefaultRenderer(PopUpMenuValue.class, new IconButtonTableCellRenderer(AllIcons.Actions.More));
+
     setRowSorter(newRowSorter(model));
     setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
     setShowGrid(false);
 
-    if (!PhysicalDeviceTableModel.SPLIT_ACTIONS_ENABLED) {
-      ActionMap map = getActionMap();
-
-      map.put("selectNextColumn", new SelectNextColumnAction());
-      map.put("selectNextColumnCell", new SelectNextColumnCellAction());
-      map.put("selectNextRow", new SelectNextRowAction());
-      map.put("selectPreviousColumn", new SelectPreviousColumnAction());
-      map.put("selectPreviousColumnCell", new SelectPreviousColumnCellAction());
-      map.put("selectPreviousRow", new SelectPreviousRowAction());
-    }
-
     getEmptyText().setText("No physical devices added. Connect a device via USB cable.");
-
-    if (!PhysicalDeviceTableModel.SPLIT_ACTIONS_ENABLED) {
-      addMouseMotionListener(new ActionsTableCellEditorMouseMotionListener(this));
-    }
   }
 
   private void sizeApiTypeAndActionsColumnWidthsToFit() {
@@ -105,15 +79,9 @@ final class PhysicalDeviceTable extends JBTable implements Table {
 
     Tables.sizeWidthToFit(this, apiViewColumnIndex());
     Tables.sizeWidthToFit(this, typeViewColumnIndex());
-
-    if (PhysicalDeviceTableModel.SPLIT_ACTIONS_ENABLED) {
-      Tables.sizeWidthToFit(this, activateDeviceFileExplorerWindowViewColumnIndex(), 0);
-      Tables.sizeWidthToFit(this, removeViewColumnIndex(), 0);
-      Tables.sizeWidthToFit(this, popUpMenuViewColumnIndex(), 0);
-    }
-    else {
-      Tables.sizeWidthToFit(this, actionsViewColumnIndex());
-    }
+    Tables.sizeWidthToFit(this, activateDeviceFileExplorerWindowViewColumnIndex(), 0);
+    Tables.sizeWidthToFit(this, removeViewColumnIndex(), 0);
+    Tables.sizeWidthToFit(this, popUpMenuViewColumnIndex(), 0);
   }
 
   private static @NotNull RowSorter<@NotNull TableModel> newRowSorter(@NotNull TableModel model) {
@@ -122,7 +90,9 @@ final class PhysicalDeviceTable extends JBTable implements Table {
     sorter.setComparator(PhysicalDeviceTableModel.DEVICE_MODEL_COLUMN_INDEX, Comparator.comparing(PhysicalDevice::getName));
     sorter.setComparator(PhysicalDeviceTableModel.API_MODEL_COLUMN_INDEX, new ApiLevelComparator().reversed());
     sorter.setComparator(PhysicalDeviceTableModel.TYPE_MODEL_COLUMN_INDEX, Comparator.naturalOrder().reversed());
-    sorter.setSortable(PhysicalDeviceTableModel.ACTIONS_MODEL_COLUMN_INDEX, false);
+    sorter.setSortable(PhysicalDeviceTableModel.ACTIVATE_DEVICE_FILE_EXPLORER_WINDOW_MODEL_COLUMN_INDEX, false);
+    sorter.setSortable(PhysicalDeviceTableModel.REMOVE_MODEL_COLUMN_INDEX, false);
+    sorter.setSortable(PhysicalDeviceTableModel.POP_UP_MENU_MODEL_COLUMN_INDEX, false);
     sorter.setSortKeys(Collections.singletonList(new SortKey(PhysicalDeviceTableModel.DEVICE_MODEL_COLUMN_INDEX, SortOrder.ASCENDING)));
 
     return sorter;
@@ -142,10 +112,6 @@ final class PhysicalDeviceTable extends JBTable implements Table {
     return (PhysicalDevice)getValueAt(viewRowIndex, deviceViewColumnIndex());
   }
 
-  @NotNull ActionsTableCellEditor getActionsCellEditor() {
-    return (ActionsTableCellEditor)getCellEditor();
-  }
-
   @VisibleForTesting
   @NotNull Object getData() {
     return IntStream.range(0, getRowCount())
@@ -162,26 +128,23 @@ final class PhysicalDeviceTable extends JBTable implements Table {
 
   @Override
   protected @NotNull JTableHeader createDefaultTableHeader() {
+    TableColumnModel model = new DefaultTableColumnModel();
+
+    model.addColumn(columnModel.getColumn(deviceViewColumnIndex()));
+    model.addColumn(columnModel.getColumn(apiViewColumnIndex()));
+    model.addColumn(columnModel.getColumn(typeViewColumnIndex()));
+
+    Collection<TableColumn> columns = Arrays.asList(columnModel.getColumn(activateDeviceFileExplorerWindowViewColumnIndex()),
+                                                    columnModel.getColumn(removeViewColumnIndex()),
+                                                    columnModel.getColumn(popUpMenuViewColumnIndex()));
+
+    TableColumn column = new MergedTableColumn(columns);
+    column.setHeaderValue("Actions");
+
+    model.addColumn(column);
+
     JTableHeader header = super.createDefaultTableHeader();
-
-    if (PhysicalDeviceTableModel.SPLIT_ACTIONS_ENABLED) {
-      TableColumnModel model = new DefaultTableColumnModel();
-
-      model.addColumn(columnModel.getColumn(deviceViewColumnIndex()));
-      model.addColumn(columnModel.getColumn(apiViewColumnIndex()));
-      model.addColumn(columnModel.getColumn(typeViewColumnIndex()));
-
-      Collection<TableColumn> columns = Arrays.asList(columnModel.getColumn(activateDeviceFileExplorerWindowViewColumnIndex()),
-                                                      columnModel.getColumn(removeViewColumnIndex()),
-                                                      columnModel.getColumn(popUpMenuViewColumnIndex()));
-
-      TableColumn column = new MergedTableColumn(columns);
-      column.setHeaderValue("Actions");
-
-      model.addColumn(column);
-      header.setColumnModel(model);
-    }
-
+    header.setColumnModel(model);
     header.setReorderingAllowed(false);
     header.setResizingAllowed(false);
 
@@ -200,10 +163,6 @@ final class PhysicalDeviceTable extends JBTable implements Table {
     return convertColumnIndexToView(PhysicalDeviceTableModel.TYPE_MODEL_COLUMN_INDEX);
   }
 
-  private int actionsViewColumnIndex() {
-    return convertColumnIndexToView(PhysicalDeviceTableModel.ACTIONS_MODEL_COLUMN_INDEX);
-  }
-
   private int activateDeviceFileExplorerWindowViewColumnIndex() {
     return convertColumnIndexToView(PhysicalDeviceTableModel.ACTIVATE_DEVICE_FILE_EXPLORER_WINDOW_MODEL_COLUMN_INDEX);
   }
@@ -219,25 +178,5 @@ final class PhysicalDeviceTable extends JBTable implements Table {
   @Override
   public @NotNull PhysicalDeviceTableModel getModel() {
     return (PhysicalDeviceTableModel)dataModel;
-  }
-
-  @Override
-  public boolean isActionsColumn(int viewColumnIndex) {
-    return getColumnClass(viewColumnIndex).equals(Actions.class);
-  }
-
-  @Override
-  public int viewRowIndexAtPoint(@NotNull Point point) {
-    return rowAtPoint(point);
-  }
-
-  @Override
-  public int viewColumnIndexAtPoint(@NotNull Point point) {
-    return columnAtPoint(point);
-  }
-
-  @Override
-  public int getEditingViewRowIndex() {
-    return editingRow;
   }
 }
