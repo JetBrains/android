c4ec150a7483824e35bffea281525e363d761dac
Mike Moroney <mmoro@google.com>
2021-11-02 11:01:09 -0700

    Update visuals for new consent dialog
    Bug: 204225849
    Test: None
    Change-Id: I75ed10878cc07e3ad71e96cc85790374fdbbbf6f
    
    GitOrigin-RevId: 3838a9c7b59b8e72acbea310133ce07370eb662c

--- android/src/com/android/tools/idea/stats/ConsentDialog.kt
+++ android/src/com/android/tools/idea/stats/ConsentDialog.kt
@@ -15,49 +15,120 @@
  */
 package com.android.tools.idea.stats
 
+import com.android.tools.adtui.ImageUtils.iconToImage
 import com.google.common.base.Predicates
 import com.intellij.ide.gdpr.Consent
 import com.intellij.ide.gdpr.ConsentOptions
 import com.intellij.openapi.ui.DialogWrapper
-import com.intellij.openapi.util.text.StringUtil
 import com.intellij.ui.AppUIUtil
-import com.intellij.ui.scale.JBUIScale
+import icons.StudioIllustrations
 import java.awt.Color
+import java.awt.Desktop
+import java.awt.Dimension
 import java.awt.EventQueue
+import java.awt.Font
+import java.awt.GridBagConstraints
+import java.awt.GridBagConstraints.NORTHWEST
+import java.awt.GridBagLayout
+import java.awt.Image
+import java.awt.Insets
 import java.lang.reflect.InvocationTargetException
 import javax.swing.Action
-import javax.swing.Box
+import javax.swing.ImageIcon
 import javax.swing.JCheckBox
 import javax.swing.JComponent
-import javax.swing.JTextArea
+import javax.swing.JEditorPane
+import javax.swing.JLabel
+import javax.swing.JPanel
+import javax.swing.SwingConstants.TOP
+import javax.swing.event.HyperlinkEvent
+
+private const val LABEL_TEXT =
+  "<html>Help make Android Studio better by automatically sending usage statistics and crash reports to Google</html>"
 
 class ConsentDialog(private val consent: Consent) : DialogWrapper(null) {
-  private val checkBox = JCheckBox()
+  private val checkBox = JCheckBox(null, null, true)
   override fun createActions(): Array<Action> {
     return arrayOf(okAction)
   }
 
-  // TODO: update visuals
-  val content: JComponent = Box.createVerticalBox().apply {
-    background = Color.pink
-    foreground = Color.orange
+  val content: JComponent = JPanel(GridBagLayout()).apply {
+    val image = iconToImage(StudioIllustrations.Common.PRODUCT_ICON)
+    val scaled = image.getScaledInstance(80, 80, Image.SCALE_SMOOTH)
+    val icon = JLabel(ImageIcon(scaled))
 
-    val textArea = JTextArea()
-    textArea.text = consent.text;
-    textArea.lineWrap = true;
-    textArea.wrapStyleWord = true
-    add(textArea)
+    val constraints = GridBagConstraints().apply {
+      gridx = 0
+      gridy = 0
+      gridheight = 3
+      anchor = NORTHWEST
+      insets = Insets(10, 15, 5, 20)
+    }
 
-    checkBox.text = "I agree to the conditions."
-    add(checkBox)
+    add(icon, constraints)
+
+    val title = JLabel().apply {
+      text = "Help improve Android Studio"
+      font = Font(font.name, Font.BOLD, font.size + 2)
+    }
+
+    constraints.apply {
+      gridx = 1
+      gridwidth = 2
+      gridheight = 1
+      insets = Insets(10, 0, 15, 0)
+    }
+
+    add(title, constraints)
+
+    constraints.apply {
+      gridy = 1
+      gridwidth = 1
+      insets = Insets(0, 0, 0, 5)
+    }
+
+    add(checkBox, constraints)
+
+    val label = JLabel(LABEL_TEXT).apply {
+      verticalAlignment = TOP
+      preferredSize = Dimension(480, 40)
+    }
+
+    constraints.apply {
+      constraints.gridx = 2
+      constraints.insets = Insets(0, 0, 0, 0)
+    }
+
+    add(label, constraints)
+
+    val message = JEditorPane("text/html", consent.text).apply {
+      isEditable = false
+      background = Color(0, 0, 0, 0)
+      foreground = Color.GRAY
+      font = Font(font.name, Font.PLAIN, checkBox.font.size - 2)
+      putClientProperty(JEditorPane.HONOR_DISPLAY_PROPERTIES, true)
+      preferredSize = Dimension(480, 80)
+
+      addHyperlinkListener { e ->
+        if (e.eventType == HyperlinkEvent.EventType.ACTIVATED) {
+          if (Desktop.isDesktopSupported()) {
+            Desktop.getDesktop().browse(e.url.toURI());
+          }
+        }
+      }
+    }
+
+    constraints.apply {
+      gridy = 2
+    }
+
+    add(message, constraints)
   }
 
   init {
-    isAutoAdjustable = true
-    isResizable = true
-    title = "Data Sharing"
+    isResizable = false
     isModal = true
-    checkBox.isSelected = true
+    setUndecorated(true)
     init()
   }
 
