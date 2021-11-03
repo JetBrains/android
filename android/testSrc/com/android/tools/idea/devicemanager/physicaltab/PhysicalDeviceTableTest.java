ec20fef99d41972a75582d0a0465747c5cdf5704
Juan C Nuno <juancnuno@google.com>
2021-11-03 15:14:47 -0700

    Test PhysicalDeviceTable
    Bug: 196437170
    Test: PhysicalDeviceTableTest
    Change-Id: I3e1623795f2c695e5d1308252fa481eb7f811644
    
    GitOrigin-RevId: 3c4a5418c8e9937685a1835ce197a22a44a1e66a

---
+++ android/testSrc/com/android/tools/idea/devicemanager/physicaltab/PhysicalDeviceTableTest.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.devicemanager.physicaltab;
+
+import static org.junit.Assert.assertEquals;
+
+import com.intellij.openapi.project.Project;
+import java.util.Collections;
+import java.util.Optional;
+import org.jetbrains.annotations.NotNull;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.mockito.Mockito;
+
+@RunWith(JUnit4.class)
+public final class PhysicalDeviceTableTest {
+  private final @NotNull PhysicalDeviceTable myTable;
+
+  public PhysicalDeviceTableTest() {
+    PhysicalDevicePanel panel = Mockito.mock(PhysicalDevicePanel.class);
+    Mockito.when(panel.getProject()).thenReturn(Mockito.mock(Project.class));
+
+    myTable = new PhysicalDeviceTable(panel, new PhysicalDeviceTableModel(Collections.singletonList(TestPhysicalDevices.GOOGLE_PIXEL_3)));
+  }
+
+  @Test
+  public void getSelectedDeviceViewRowIndexEqualsNegativeOne() {
+    // Act
+    Object device = myTable.getSelectedDevice();
+
+    // Assert
+    assertEquals(Optional.empty(), device);
+  }
+
+  @Test
+  public void getSelectedDevice() {
+    // Arrange
+    myTable.setRowSelectionInterval(0, 0);
+
+    // Act
+    Object device = myTable.getSelectedDevice();
+
+    // Assert
+    assertEquals(Optional.of(TestPhysicalDevices.GOOGLE_PIXEL_3), device);
+  }
+}
