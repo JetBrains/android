beac9a1ee5a8fcf94b2b004208990a9f082d2718
Chris Sinco <csinco@google.com>
2021-11-15 17:08:47 -0800

    New Send Feedback icon
    Test: n/a
    Fixes: 199767141
    Change-Id: I6b6fd2c959770e190cf60e70eeb8278c72cee2ba
    
    GitOrigin-RevId: b1c7375d71dd4e377edecc7dc7957acf35b679f5

--- artwork/gen/icons/StudioIcons.java
+++ artwork/gen/icons/StudioIcons.java
@@ -805,6 +805,7 @@ public class StudioIcons {
     }
 
     public static class Telemetry {
+      public static final Icon SEND_FEEDBACK = load("/studio/icons/shell/telemetry/send-feedback.svg"); // 16x16
       public static final Icon SENTIMENT_DISSATISFIED = load("/studio/icons/shell/telemetry/sentiment-dissatisfied.svg"); // 24x24
       public static final Icon SENTIMENT_NEGATIVE = load("/studio/icons/shell/telemetry/sentiment-negative.svg"); // 16x16
       public static final Icon SENTIMENT_NEUTRAL = load("/studio/icons/shell/telemetry/sentiment-neutral.svg"); // 24x24
