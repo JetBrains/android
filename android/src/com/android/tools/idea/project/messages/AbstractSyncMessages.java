3b3d61dd2f7d8079fc31ebc4b0519c3b382a3302
Yuriy Solodkyy <solodkyy@google.com>
2021-11-18 21:25:28 +0000

    Do not ignore sync errors
    Most of sync errors have been converted to warnings but a few that
    remain still need to be displayed. Since we do not have a special
    treatment of errors anymore we will simply display them alongside
    any other sync issues.
    
    Also, if sync crashes (i.e. reaches its final state) without calling
    `onSucceed` report a sync issue to let users know at least some
    details about the problem.
    
    Bug: 205744568
    Test: n/a
    Change-Id: I7a660572e1b15eb62262feb0323cf9986c23c81d
    
    GitOrigin-RevId: 002ef41c31435fa020889df695aa9608c25fdbf4

--- android/src/com/android/tools/idea/project/messages/AbstractSyncMessages.java
+++ android/src/com/android/tools/idea/project/messages/AbstractSyncMessages.java
@@ -213,19 +213,7 @@ public abstract class AbstractSyncMessages implements Disposable {
       issueEvent = new AndroidSyncIssueEvent(taskId, notification, title, quickFixes);
     }
 
-    // Only include errors in the summary text output
-    // This has the side effect of not opening the right hand bar if there are no failures
-    if (issueEvent.getKind() == MessageEvent.Kind.ERROR) {
-      synchronized (myLock) {
-        myShownFailures.computeIfAbsent(taskId, key -> new ArrayList<>())
-          .addAll(((AndroidSyncIssueEventResult)issueEvent.getResult()).getFailures());
-      }
-    }
-    else {
-      // Only emit the event if it's not an error. Errors are saved in myShownFailures and will be emitted at the end of sync as part of FinishBuildEvent.
-      // FinishBuildEvent is better to emit errors since the failures in FinishBuildEvent has better format of simplified node titles and clickable hyperlinks.
-      myProject.getService(SyncViewManager.class).onEvent(taskId, issueEvent);
-    }
+    myProject.getService(SyncViewManager.class).onEvent(taskId, issueEvent);
   }
 
   @NotNull
