d5fa9c433ea775256e2f35857ca8b320d704053a
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2021-11-18 14:47:18 +0100

    use murmur 3 32 instead of xxhash 64 for icon digests - reduce icon db size (mostly in memory, because on disk compressed using LZ4) and avoid loading extra classes on start-up (murmur is already used in classloader)
    no collisions so far
    
    GitOrigin-RevId: 1f41a238729772b4b3c4faa5de5cd1ccb9ec3f65

--- artwork/gen/icons/AndroidIcons.java
+++ artwork/gen/icons/AndroidIcons.java
@@ -1,3 +1,4 @@
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package icons;
 
 import com.intellij.ui.IconManager;
@@ -10,69 +11,69 @@ import javax.swing.*;
  * DO NOT EDIT IT BY HAND, run "Generate icon classes" configuration instead
  */
 public final class AndroidIcons {
-  private static @NotNull Icon load(@NotNull String path, long cacheKey, int flags) {
+  private static @NotNull Icon load(@NotNull String path, int cacheKey, int flags) {
     return IconManager.getInstance().loadRasterizedIcon(path, AndroidIcons.class.getClassLoader(), cacheKey, flags);
   }
-  /** 16x16 */ public static final @NotNull Icon Android = load("icons/android.svg", 5186374359974655776L, 0);
+  /** 16x16 */ public static final @NotNull Icon Android = load("icons/android.svg", -364876, 0);
 
   public static final class Explorer {
-    /** 220x125 */ public static final @NotNull Icon DevicesLineup = load("icons/explorer/devices-lineup.png", 0L, 7);
+    /** 220x125 */ public static final @NotNull Icon DevicesLineup = load("icons/explorer/devices-lineup.png", 0, 7);
   }
 
   public static final class Flags {
-    /** 16x11 */ public static final @NotNull Icon Fi = load("icons/flags/fi.png", 0L, 0);
-    /** 16x11 */ public static final @NotNull Icon Fj = load("icons/flags/fj.png", 0L, 0);
-    /** 16x11 */ public static final @NotNull Icon Fk = load("icons/flags/fk.png", 0L, 0);
-    /** 16x16 */ public static final @NotNull Icon FlagEmpty = load("icons/flags/flag_empty.png", 0L, 0);
-    /** 16x11 */ public static final @NotNull Icon Fm = load("icons/flags/fm.png", 0L, 0);
-    /** 16x11 */ public static final @NotNull Icon Fo = load("icons/flags/fo.png", 0L, 0);
-    /** 16x11 */ public static final @NotNull Icon Fr = load("icons/flags/fr.png", 0L, 0);
-    /** 16x11 */ public static final @NotNull Icon Hk = load("icons/flags/hk.png", 0L, 0);
-    /** 16x11 */ public static final @NotNull Icon Hm = load("icons/flags/hm.png", 0L, 0);
-    /** 16x11 */ public static final @NotNull Icon Hn = load("icons/flags/hn.png", 0L, 0);
-    /** 16x11 */ public static final @NotNull Icon Hr = load("icons/flags/hr.png", 0L, 0);
-    /** 16x11 */ public static final @NotNull Icon Ht = load("icons/flags/ht.png", 0L, 0);
-    /** 16x11 */ public static final @NotNull Icon Hu = load("icons/flags/hu.png", 0L, 0);
-    /** 16x11 */ public static final @NotNull Icon Jm = load("icons/flags/jm.png", 0L, 0);
-    /** 16x11 */ public static final @NotNull Icon Jo = load("icons/flags/jo.png", 0L, 0);
-    /** 16x11 */ public static final @NotNull Icon Jp = load("icons/flags/jp.png", 0L, 0);
-    /** 16x11 */ public static final @NotNull Icon Ke = load("icons/flags/ke.png", 0L, 0);
-    /** 16x11 */ public static final @NotNull Icon Kg = load("icons/flags/kg.png", 0L, 0);
-    /** 16x11 */ public static final @NotNull Icon Kh = load("icons/flags/kh.png", 0L, 0);
-    /** 16x11 */ public static final @NotNull Icon Ki = load("icons/flags/ki.png", 0L, 0);
-    /** 16x11 */ public static final @NotNull Icon Km = load("icons/flags/km.png", 0L, 0);
-    /** 16x11 */ public static final @NotNull Icon Kn = load("icons/flags/kn.png", 0L, 0);
-    /** 16x11 */ public static final @NotNull Icon Kp = load("icons/flags/kp.png", 0L, 0);
-    /** 16x11 */ public static final @NotNull Icon Kr = load("icons/flags/kr.png", 0L, 0);
-    /** 16x11 */ public static final @NotNull Icon Kw = load("icons/flags/kw.png", 0L, 0);
-    /** 16x11 */ public static final @NotNull Icon Ky = load("icons/flags/ky.png", 0L, 0);
-    /** 16x11 */ public static final @NotNull Icon Kz = load("icons/flags/kz.png", 0L, 0);
+    /** 16x11 */ public static final @NotNull Icon Fi = load("icons/flags/fi.png", 0, 0);
+    /** 16x11 */ public static final @NotNull Icon Fj = load("icons/flags/fj.png", 0, 0);
+    /** 16x11 */ public static final @NotNull Icon Fk = load("icons/flags/fk.png", 0, 0);
+    /** 16x16 */ public static final @NotNull Icon FlagEmpty = load("icons/flags/flag_empty.png", 0, 0);
+    /** 16x11 */ public static final @NotNull Icon Fm = load("icons/flags/fm.png", 0, 0);
+    /** 16x11 */ public static final @NotNull Icon Fo = load("icons/flags/fo.png", 0, 0);
+    /** 16x11 */ public static final @NotNull Icon Fr = load("icons/flags/fr.png", 0, 0);
+    /** 16x11 */ public static final @NotNull Icon Hk = load("icons/flags/hk.png", 0, 0);
+    /** 16x11 */ public static final @NotNull Icon Hm = load("icons/flags/hm.png", 0, 0);
+    /** 16x11 */ public static final @NotNull Icon Hn = load("icons/flags/hn.png", 0, 0);
+    /** 16x11 */ public static final @NotNull Icon Hr = load("icons/flags/hr.png", 0, 0);
+    /** 16x11 */ public static final @NotNull Icon Ht = load("icons/flags/ht.png", 0, 0);
+    /** 16x11 */ public static final @NotNull Icon Hu = load("icons/flags/hu.png", 0, 0);
+    /** 16x11 */ public static final @NotNull Icon Jm = load("icons/flags/jm.png", 0, 0);
+    /** 16x11 */ public static final @NotNull Icon Jo = load("icons/flags/jo.png", 0, 0);
+    /** 16x11 */ public static final @NotNull Icon Jp = load("icons/flags/jp.png", 0, 0);
+    /** 16x11 */ public static final @NotNull Icon Ke = load("icons/flags/ke.png", 0, 0);
+    /** 16x11 */ public static final @NotNull Icon Kg = load("icons/flags/kg.png", 0, 0);
+    /** 16x11 */ public static final @NotNull Icon Kh = load("icons/flags/kh.png", 0, 0);
+    /** 16x11 */ public static final @NotNull Icon Ki = load("icons/flags/ki.png", 0, 0);
+    /** 16x11 */ public static final @NotNull Icon Km = load("icons/flags/km.png", 0, 0);
+    /** 16x11 */ public static final @NotNull Icon Kn = load("icons/flags/kn.png", 0, 0);
+    /** 16x11 */ public static final @NotNull Icon Kp = load("icons/flags/kp.png", 0, 0);
+    /** 16x11 */ public static final @NotNull Icon Kr = load("icons/flags/kr.png", 0, 0);
+    /** 16x11 */ public static final @NotNull Icon Kw = load("icons/flags/kw.png", 0, 0);
+    /** 16x11 */ public static final @NotNull Icon Ky = load("icons/flags/ky.png", 0, 0);
+    /** 16x11 */ public static final @NotNull Icon Kz = load("icons/flags/kz.png", 0, 0);
   }
 
   public static final class Versions {
-    /** 128x128 */ public static final @NotNull Icon Default = load("icons/versions/Default.png", 0L, 0);
-    /** 32x32 */ public static final @NotNull Icon Default32 = load("icons/versions/Default_32.png", 0L, 0);
-    /** 128x128 */ public static final @NotNull Icon Gingerbread = load("icons/versions/Gingerbread.png", 0L, 0);
-    /** 32x32 */ public static final @NotNull Icon Gingerbread32 = load("icons/versions/Gingerbread_32.png", 0L, 1);
-    /** 128x128 */ public static final @NotNull Icon Honeycomb = load("icons/versions/Honeycomb.png", 0L, 0);
-    /** 32x32 */ public static final @NotNull Icon Honeycomb32 = load("icons/versions/Honeycomb_32.png", 0L, 1);
-    /** 128x128 */ public static final @NotNull Icon IceCreamSandwich = load("icons/versions/IceCreamSandwich.png", 0L, 0);
-    /** 32x32 */ public static final @NotNull Icon IceCreamSandwich32 = load("icons/versions/IceCreamSandwich_32.png", 0L, 1);
-    /** 128x128 */ public static final @NotNull Icon Jelly_Bean = load("icons/versions/Jelly Bean.png", 0L, 0);
-    /** 32x32 */ public static final @NotNull Icon Jelly_Bean32 = load("icons/versions/Jelly Bean_32.png", 0L, 1);
-    /** 128x128 */ public static final @NotNull Icon KitKat_Wear = load("icons/versions/KitKat Wear.png", 0L, 0);
-    /** 32x32 */ public static final @NotNull Icon KitKat_Wear32 = load("icons/versions/KitKat Wear_32.png", 0L, 1);
-    /** 128x128 */ public static final @NotNull Icon KitKat = load("icons/versions/KitKat.png", 0L, 0);
-    /** 32x32 */ public static final @NotNull Icon KitKat32 = load("icons/versions/KitKat_32.png", 0L, 1);
-    /** 128x128 */ public static final @NotNull Icon Lollipop = load("icons/versions/Lollipop.png", 0L, 0);
-    /** 32x32 */ public static final @NotNull Icon Lollipop32 = load("icons/versions/Lollipop_32.png", 0L, 1);
-    /** 128x128 */ public static final @NotNull Icon Marshmallow = load("icons/versions/Marshmallow.png", 0L, 0);
-    /** 32x32 */ public static final @NotNull Icon Marshmallow32 = load("icons/versions/Marshmallow_32.png", 0L, 1);
-    /** 128x128 */ public static final @NotNull Icon Nougat = load("icons/versions/Nougat.png", 0L, 0);
-    /** 32x32 */ public static final @NotNull Icon Nougat32 = load("icons/versions/Nougat_32.png", 0L, 1);
-    /** 128x128 */ public static final @NotNull Icon Oreo = load("icons/versions/Oreo.png", 0L, 0);
-    /** 32x32 */ public static final @NotNull Icon Oreo32 = load("icons/versions/Oreo_32.png", 0L, 1);
-    /** 128x128 */ public static final @NotNull Icon Pie = load("icons/versions/Pie.png", 0L, 0);
-    /** 32x32 */ public static final @NotNull Icon Pie32 = load("icons/versions/Pie_32.png", 0L, 1);
+    /** 128x128 */ public static final @NotNull Icon Default = load("icons/versions/Default.png", 0, 0);
+    /** 32x32 */ public static final @NotNull Icon Default32 = load("icons/versions/Default_32.png", 0, 0);
+    /** 128x128 */ public static final @NotNull Icon Gingerbread = load("icons/versions/Gingerbread.png", 0, 0);
+    /** 32x32 */ public static final @NotNull Icon Gingerbread32 = load("icons/versions/Gingerbread_32.png", 0, 1);
+    /** 128x128 */ public static final @NotNull Icon Honeycomb = load("icons/versions/Honeycomb.png", 0, 0);
+    /** 32x32 */ public static final @NotNull Icon Honeycomb32 = load("icons/versions/Honeycomb_32.png", 0, 1);
+    /** 128x128 */ public static final @NotNull Icon IceCreamSandwich = load("icons/versions/IceCreamSandwich.png", 0, 0);
+    /** 32x32 */ public static final @NotNull Icon IceCreamSandwich32 = load("icons/versions/IceCreamSandwich_32.png", 0, 1);
+    /** 128x128 */ public static final @NotNull Icon Jelly_Bean = load("icons/versions/Jelly Bean.png", 0, 0);
+    /** 32x32 */ public static final @NotNull Icon Jelly_Bean32 = load("icons/versions/Jelly Bean_32.png", 0, 1);
+    /** 128x128 */ public static final @NotNull Icon KitKat_Wear = load("icons/versions/KitKat Wear.png", 0, 0);
+    /** 32x32 */ public static final @NotNull Icon KitKat_Wear32 = load("icons/versions/KitKat Wear_32.png", 0, 1);
+    /** 128x128 */ public static final @NotNull Icon KitKat = load("icons/versions/KitKat.png", 0, 0);
+    /** 32x32 */ public static final @NotNull Icon KitKat32 = load("icons/versions/KitKat_32.png", 0, 1);
+    /** 128x128 */ public static final @NotNull Icon Lollipop = load("icons/versions/Lollipop.png", 0, 0);
+    /** 32x32 */ public static final @NotNull Icon Lollipop32 = load("icons/versions/Lollipop_32.png", 0, 1);
+    /** 128x128 */ public static final @NotNull Icon Marshmallow = load("icons/versions/Marshmallow.png", 0, 0);
+    /** 32x32 */ public static final @NotNull Icon Marshmallow32 = load("icons/versions/Marshmallow_32.png", 0, 1);
+    /** 128x128 */ public static final @NotNull Icon Nougat = load("icons/versions/Nougat.png", 0, 0);
+    /** 32x32 */ public static final @NotNull Icon Nougat32 = load("icons/versions/Nougat_32.png", 0, 1);
+    /** 128x128 */ public static final @NotNull Icon Oreo = load("icons/versions/Oreo.png", 0, 0);
+    /** 32x32 */ public static final @NotNull Icon Oreo32 = load("icons/versions/Oreo_32.png", 0, 1);
+    /** 128x128 */ public static final @NotNull Icon Pie = load("icons/versions/Pie.png", 0, 0);
+    /** 32x32 */ public static final @NotNull Icon Pie32 = load("icons/versions/Pie_32.png", 0, 1);
   }
 }
--- whats-new-assistant/src/icons/WhatsNewAssistantIcons.java
+++ whats-new-assistant/src/icons/WhatsNewAssistantIcons.java
@@ -1,3 +1,4 @@
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package icons;
 
 import com.intellij.ui.IconManager;
@@ -10,15 +11,15 @@ import javax.swing.*;
  * DO NOT EDIT IT BY HAND, run "Generate icon classes" configuration instead
  */
 public final class WhatsNewAssistantIcons {
-  private static @NotNull Icon load(@NotNull String path, long cacheKey, int flags) {
+  private static @NotNull Icon load(@NotNull String path, int cacheKey, int flags) {
     return IconManager.getInstance().loadRasterizedIcon(path, WhatsNewAssistantIcons.class.getClassLoader(), cacheKey, flags);
   }
 
   public static final class Preview {
-    /** 30x30 */ public static final @NotNull Icon Whats_new_icon = load("preview/whats_new_icon.png", 0L, 1);
+    /** 30x30 */ public static final @NotNull Icon Whats_new_icon = load("preview/whats_new_icon.png", 0, 1);
   }
 
   public static final class Stable {
-    /** 30x30 */ public static final @NotNull Icon Whats_new_icon = load("stable/whats_new_icon.png", 0L, 1);
+    /** 30x30 */ public static final @NotNull Icon Whats_new_icon = load("stable/whats_new_icon.png", 0, 1);
   }
 }
