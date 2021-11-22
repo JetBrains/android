ca5794089ee6a65185fdc06dec30d05e8734d516
kovalp <kovalp@google.com>
2021-11-22 17:47:20 +0000

    Better error handling for device coming online
    Bug: n/a
    Test: n/a
    Change-Id: I8cf56aadc0bf6658465922a76b305f925aeedba9
    
    GitOrigin-RevId: 3259cbb9c5d7a55b4b81d39f1a1026d594d5bc15

--- android/src/com/android/tools/idea/run/configuration/execution/AndroidConfigurationExecutorBase.kt
+++ android/src/com/android/tools/idea/run/configuration/execution/AndroidConfigurationExecutorBase.kt
@@ -25,8 +25,8 @@ import com.android.tools.idea.run.configuration.isDebug
 import com.android.tools.idea.run.deployment.DeviceAndSnapshotComboBoxTargetProvider
 import com.android.tools.idea.run.util.LaunchUtils
 import com.android.tools.idea.stats.RunStats
-import com.android.tools.idea.wearpairing.await
 import com.google.common.annotations.VisibleForTesting
+import com.google.common.util.concurrent.ListenableFuture
 import com.intellij.execution.DefaultExecutionResult
 import com.intellij.execution.ExecutionException
 import com.intellij.execution.ExecutionResult
@@ -41,9 +41,10 @@ import com.intellij.execution.ui.RunContentDescriptor
 import com.intellij.openapi.application.invokeAndWaitIfNeeded
 import com.intellij.openapi.progress.ProgressIndicatorProvider
 import com.intellij.openapi.progress.ProgressManager
-import kotlinx.coroutines.runBlocking
 import org.jetbrains.android.facet.AndroidFacet
 import org.jetbrains.android.util.AndroidBundle
+import java.util.concurrent.TimeUnit
+import java.util.concurrent.TimeoutException
 
 abstract class AndroidConfigurationExecutorBase(protected val environment: ExecutionEnvironment) : RunProfileState {
   abstract val configuration: ComponentSpecificConfiguration
@@ -79,27 +80,45 @@ abstract class AndroidConfigurationExecutorBase(protected val environment: Execu
 
   private fun getDevices(stats: RunStats): List<IDevice> {
     val facet = AndroidFacet.getInstance(configuration.module!!)!!
-    val devices = runBlocking {
-      val provider = DeviceAndSnapshotComboBoxTargetProvider()
-      val deployTarget = if (provider.requiresRuntimePrompt(project)) invokeAndWaitIfNeeded {
-        provider.showPrompt(facet)
-      }
-      else provider.getDeployTarget(project)
-      val deviceFutureList = deployTarget?.getDevices(facet) ?: return@runBlocking emptyList()
+    val provider = DeviceAndSnapshotComboBoxTargetProvider()
+    val deployTarget = if (provider.requiresRuntimePrompt(project)) invokeAndWaitIfNeeded { provider.showPrompt(facet) }
+    else provider.getDeployTarget(project)
+    val deviceFutureList = deployTarget?.getDevices(facet) ?: throw ExecutionException(AndroidBundle.message("deployment.target.not.found"))
 
-      // Record stat if we launched a device.
-      stats.setLaunchedDevices(deviceFutureList.devices.any { it is LaunchableAndroidDevice })
-      return@runBlocking deviceFutureList.get().map {
-        stats.beginWaitForDevice()
-        val device = it.await()
-        stats.endWaitForDevice(device)
-        device
-      }
+    // Record stat if we launched a device.
+    stats.setLaunchedDevices(deviceFutureList.devices.any { it is LaunchableAndroidDevice })
+    val devices = deviceFutureList.get().map {
+      ProgressManager.checkCanceled()
+      stats.beginWaitForDevice()
+      val device = waitForDevice(it)
+      stats.endWaitForDevice(device)
+      device
     }
     if (devices.isEmpty()) {
       throw ExecutionException(AndroidBundle.message("deployment.target.not.found"))
     }
     return devices
+  }
+
+  private fun waitForDevice(deviceFuture: ListenableFuture<IDevice>): IDevice {
+    val start = System.currentTimeMillis()
+    val timeoutMs = TimeUnit.MINUTES.toMillis(1)
+    while (System.currentTimeMillis() - start < timeoutMs) {
+      try {
+        return deviceFuture.get(1, TimeUnit.SECONDS)
+      }
+      catch (ignored: TimeoutException) {
+        // Let's check the cancellation request then continue to wait for a device again.
+        ProgressManager.checkCanceled()
+      }
+      catch (e: InterruptedException) {
+        throw ExecutionException("Interrupted while waiting for device", e)
+      }
+      catch (e: java.util.concurrent.ExecutionException) {
+        throw ExecutionException("Error while waiting for device: " + e.cause!!.message, e)
+      }
+    }
+    throw ExecutionException("Device didn't come online")
   }
 
   fun getApplicationInstaller(): ApplicationInstaller {
