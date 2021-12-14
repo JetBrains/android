454f7194f6f9d9f25ea32aa711846c2b0c999d0f
Andrei Kuznetsov <andrei.kuznetsov@jetbrains.com>
2021-12-14 12:34:40 +0300

    IDEA-284832: Android Layout Inspector does not work in IDEA
    GitOrigin-RevId: 7fd00488f035d421549e0582313256a963a846e7

--- android/src/org/jetbrains/android/download/AndroidProfilerDownloader.java
+++ android/src/org/jetbrains/android/download/AndroidProfilerDownloader.java
@@ -1,14 +1,17 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.android.download;
 
+import com.intellij.openapi.diagnostic.Logger;
 import org.jetbrains.annotations.NotNull;
 
 public class AndroidProfilerDownloader extends AndroidComponentDownloader {
+  private static final Logger LOG = Logger.getInstance(AndroidProfilerDownloader.class);
+
   private static class Holder {
     private static final AndroidProfilerDownloader INSTANCE = new AndroidProfilerDownloader();
   }
 
-  private AndroidProfilerDownloader(){
+  private AndroidProfilerDownloader() {
     // singleton. Use "getInstance"
   }
 
@@ -20,5 +23,11 @@ public class AndroidProfilerDownloader extends AndroidComponentDownloader {
 
   public static AndroidProfilerDownloader getInstance() {
     return Holder.INSTANCE;
+  }
+
+  @Override
+  protected @NotNull String getVersion() {
+    LOG.assertTrue(super.getVersion().startsWith("27.3.0."), "Obsolete version override.");
+    return "27.3.0.1";
   }
 }
