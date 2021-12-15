f6d97afc510cfff6649152634000965df0fac3c2
Yuriy Solodkyy <solodkyy@google.com>
2021-12-15 13:38:32 +0000

    Move build analyzer out of android.core
    Bug: 147586218
    Test: n/a
    Change-Id: I90504b2727c63a584e952a5c421a16b5ab6057d9
    
    GitOrigin-RevId: 91d80f147e122f1a51723fb076782db8f9a26481

--- project-system-gradle/src/com/android/tools/idea/gradle/project/build/attribution/BuildAttributionManager.kt
+++ project-system-gradle/src/com/android/tools/idea/gradle/project/build/attribution/BuildAttributionManager.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2019 The Android Open Source Project
+ * Copyright (C) 2021 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,7 +17,6 @@ package com.android.tools.idea.gradle.project.build.attribution
 
 import com.android.tools.idea.gradle.project.build.invoker.GradleBuildInvoker
 import org.gradle.tooling.events.ProgressListener
-import java.io.File
 
 interface BuildAttributionManager : ProgressListener {
   fun onBuildStart()
