6e58696aec4725947ceddfb9d38dd3d7ad3126bb
Andrey.Cherkasov <andrey.cherkasov@jetbrains.com>
2022-01-12 14:11:07 +0300

    [java-inspections] Java8CollectionRemoveIf: highlight only for/while keyword instead of the whole statement
    Change-Id: I205cf1eb2b62843f410920ddaa0f4ff6fdca4883
    
    GitOrigin-RevId: 868ff6819f907cb55b9b2ff06a7e5252fd54ca27

--- android/testSrc/org/jetbrains/android/inspections/AndroidJava8CollectionRemoveIfInspectionTest.java
+++ android/testSrc/org/jetbrains/android/inspections/AndroidJava8CollectionRemoveIfInspectionTest.java
@@ -62,7 +62,7 @@ public class AndroidJava8CollectionRemoveIfInspectionTest extends AndroidInspect
            "public class X {\n" +
            "\n" +
            "    public void test(List<String> strings) {\n" +
-           "        /*The loop can be replaced with 'Collection.removeIf'*/for (Iterator<String> it = strings.iterator(); it.hasNext(); )/**/ {\n" +
+           "        /*The loop can be replaced with 'Collection.removeIf'*/for/**/ (Iterator<String> it = strings.iterator(); it.hasNext(); ) {\n" +
            "            String aValue = it.next();\n" +
            "            if(shouldBeRemoved(aValue)) {\n" +
            "                it.remove();\n" +
