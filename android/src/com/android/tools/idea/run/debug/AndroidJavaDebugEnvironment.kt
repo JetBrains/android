5686450bfb130bd500f0446d35197077bff89d6d
Polina Koval <kovalp@google.com>
2022-01-14 11:28:28 +0000

    Introduce AndroidJavaDebugEnvironment
    AndroidJavaDebugEnvironment describes debug process
    we are about to start.
    
    DebugEnvironment is only thing that is needed to start debug process, but we used to create instances of many classes that we don't actually use (Runners/ExecutionEnvironment/RunProfiles) and pretend we start configuration( AndroidJavaDebugger.attachToClient) or that we have an ExecutionEnvironment (ConnectJavaDebuggerTask.launchDebugger) in order to make platform indirectly create DebugEnvironment for us.
    
    It's much easier and cleaner to directly create DebugEnvironment and use it directly with
    `DebuggerManager.attachVirtualMachine(debugEnvironment)` rather than defer it to the platform.
    
    Additionally, it allows us to decouple code from run configurations as we don't need any to create AndroidJavaDebugEnvironment.
    
    See go/decouple-android-debuggers-from-android-configurations
    
    Bug: 214519121
    Test: tested within next CL
    Change-Id: I7fa7fe578ada26e24036459ada6cc987e25c4024
    
    GitOrigin-RevId: 5f1d8d337fe21cd1d444a9551634ea8cb2c4b168

---
+++ android/src/com/android/tools/idea/run/debug/AndroidJavaDebugEnvironment.kt
@@ -0,0 +1,74 @@
+/*
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.run.debug
+
+import com.android.ddmlib.Client
+import com.android.tools.idea.run.AndroidRemoteDebugProcessHandler
+import com.android.tools.idea.run.editor.AndroidJavaDebugger
+import com.intellij.debugger.DebugEnvironment
+import com.intellij.debugger.DebuggerGlobalSearchScope
+import com.intellij.debugger.DebuggerManagerEx
+import com.intellij.execution.DefaultExecutionResult
+import com.intellij.execution.ExecutionResult
+import com.intellij.execution.configurations.RemoteConnection
+import com.intellij.execution.filters.TextConsoleBuilderFactory
+import com.intellij.execution.ui.ConsoleView
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.roots.ProjectRootManager
+import com.intellij.openapi.util.Disposer
+import com.intellij.psi.search.GlobalSearchScope
+
+/**
+ * Describes Java debug process that we are about to start for any [Client]. It is used by Java debugger [AndroidJavaDebugger].
+ *
+ * This class is needed by the platform to create a debug session. See [DebuggerManagerEx.attachVirtualMachine]
+ */
+internal class AndroidJavaDebugEnvironment(
+  private val project: Project,
+  private val client: Client,
+  private val mySessionName: String,
+  private val consoleViewToReuse: ConsoleView?,
+  private val onDebugProcessStarted: (() -> Unit)?,
+  private val onDebugProcessDestroyed: ((Client) -> Unit)?,
+  private val detachIsDefault: Boolean
+) : DebugEnvironment {
+
+  private val myRemoteConnection = RemoteConnection(true, "localhost", client.debuggerListenPort.toString(), false)
+
+  private val searchScope = DebuggerGlobalSearchScope(GlobalSearchScope.allScope(project), project)
+  override fun getSearchScope() = searchScope
+
+  override fun createExecutionResult(): ExecutionResult {
+    val console = consoleViewToReuse ?: TextConsoleBuilderFactory.getInstance().createBuilder(project).console
+    Disposer.register(project, console)
+    val debugProcessHandler = AndroidRemoteDebugProcessHandler(project, client, detachIsDefault, onDebugProcessDestroyed)
+    console.attachToProcess(debugProcessHandler)
+
+    onDebugProcessStarted?.let { it() }
+
+    return DefaultExecutionResult(console, debugProcessHandler)
+  }
+
+  override fun isRemote() = true
+
+  override fun getRemoteConnection() = myRemoteConnection
+
+  override fun getPollTimeout() = 0L
+
+  override fun getSessionName() = mySessionName
+
+  override fun getRunJre() = ProjectRootManager.getInstance(project).projectSdk
+}
