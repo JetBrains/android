4f3c10e7964ce1332123b150df60b683e824652c
Karima Ichou <karimai@google.com>
2022-01-17 15:48:10 +0000

    [Unit testing] Update tests.
    With the ongoing platform merge, some details have changed about how the
    Gradle Run configurations are setup. This change updates the tests to
    take into consideration the API updates.
    This change also reworks a test to reflect these changes, but updates
    how we verify the outcome by using a better project.
    
    Fixes: 214640804
    Test: updated
    Change-Id: I227a7e34a2fab295068428bc59c5b23b2931b6bf
    
    GitOrigin-RevId: 6063b21e6ba6af67132c08eb1adab3d9587773b6

--- android/testSrc/com/android/tools/idea/testartifacts/gradle/AndroidGradleConfigurationProducersTest.kt
+++ android/testSrc/com/android/tools/idea/testartifacts/gradle/AndroidGradleConfigurationProducersTest.kt
@@ -15,7 +15,6 @@
  */
 package com.android.tools.idea.testartifacts.gradle
 
-import com.android.tools.idea.gradle.run.AndroidGradleTestTasksProvider
 import com.android.tools.idea.gradle.task.AndroidGradleTaskManager
 import com.android.tools.idea.testartifacts.TestConfigurationTesting
 import com.android.tools.idea.testartifacts.createAndroidGradleConfigurationFromDirectory
@@ -25,27 +24,24 @@ import com.android.tools.idea.testartifacts.getPsiElement
 import com.android.tools.idea.testing.AndroidGradleTestCase
 import com.android.tools.idea.testing.TestProjectPaths.TEST_ARTIFACTS_KOTLIN
 import com.android.tools.idea.testing.TestProjectPaths.TEST_ARTIFACTS_KOTLIN_MULTIPLATFORM
-import com.android.tools.idea.testing.TestProjectPaths.TEST_ARTIFACTS_SAME_NAME_CLASSES
 import com.android.tools.idea.testing.TestProjectPaths.TEST_RESOURCES
-import com.android.tools.idea.testing.TestProjectPaths.TRANSITIVE_DEPENDENCIES
 import com.android.tools.idea.testing.TestProjectPaths.UNIT_TESTING
 import com.google.common.truth.Truth.assertThat
 import com.intellij.execution.actions.ConfigurationFromContextImpl
-import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.externalSystem.model.task.ExternalSystemTaskId
 import com.intellij.openapi.externalSystem.model.task.ExternalSystemTaskNotificationListenerAdapter
 import com.intellij.openapi.externalSystem.model.task.ExternalSystemTaskType
 import com.intellij.openapi.module.ModuleManager
 import com.intellij.openapi.util.Pair
-import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.JavaPsiFacade
 import com.intellij.psi.PsiDirectory
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
 import com.intellij.psi.search.GlobalSearchScope
+import junit.framework.TestCase
+import org.jetbrains.kotlin.daemon.common.trimQuotes
 import org.jetbrains.plugins.gradle.GradleManager
 import org.jetbrains.plugins.gradle.execution.test.runner.AllInPackageGradleConfigurationProducer
-import org.jetbrains.plugins.gradle.execution.test.runner.GradleTestTasksProvider
 import org.jetbrains.plugins.gradle.execution.test.runner.TestClassGradleConfigurationProducer
 import org.jetbrains.plugins.gradle.service.execution.GradleRunConfiguration
 import org.jetbrains.plugins.gradle.util.GradleConstants
@@ -61,7 +57,6 @@ class AndroidGradleConfigurationProducersTest : AndroidGradleTestCase() {
     verifyCanCreateClassGradleRunConfigurationFromTestScope()
     verifyCannotCreateClassGradleRunConfigurationFromAndroidTestScope()
     verifyCannotCreateDirectoryGradleRunConfigurationFromAndroidTestDirectory()
-    verifyAndroidGradleTestTasksProviderDoesntCreateTestTasksForJavaModule()
     verifyCanCreateGradleConfigurationFromTestDirectory()
   }
 
@@ -103,9 +98,9 @@ class AndroidGradleConfigurationProducersTest : AndroidGradleTestCase() {
     val psiElement = JavaPsiFacade.getInstance(project).findClass("com.example.app.ExampleUnitTest", GlobalSearchScope.projectScope(project))
     val configurationFromContext = createConfigurationFromContext(psiElement!!)
     val gradleRunConfiguration = configurationFromContext!!.configuration as GradleRunConfiguration
-/* b/214640804
-    assertThat(gradleRunConfiguration.settings.taskNames).containsExactly(":app:testDebugUnitTest")
-b/214640804 */
+    // Starting from IDEA 2021.3, both the task names and script parameters are merged into taskNames as a list of separate tasks
+    // that is passed to the Gradle executor as such.
+    assertThat(gradleRunConfiguration.settings.taskNames).isEqualTo(listOf(":app:testDebugUnitTest", "--tests", "\"com.example.app.ExampleUnitTest\""))
     // Set the execution settings using the runConfiguration parameters.
     val executionSettings = GradleManager()
       .executionSettingsProvider
@@ -114,23 +109,21 @@ b/214640804 */
 
     AndroidGradleTaskManager().executeTasks(
       ExternalSystemTaskId.create(GradleConstants.SYSTEM_ID, ExternalSystemTaskType.EXECUTE_TASK, project),
-      gradleRunConfiguration.settings.taskNames,
+      gradleRunConfiguration.settings.taskNames.map { it.trimQuotes()},
       project.basePath!!,
       executionSettings,
       null,
       listener
     )
 
-/* b/214640804
     assertThat(listener.finalMessage.lines()).contains("> Task :app:testDebugUnitTest")
-b/214640804 */
 
     // Clear the logged messages.
     listener.messagesLog = StringBuilder()
 
     AndroidGradleTaskManager().executeTasks(
       ExternalSystemTaskId.create(GradleConstants.SYSTEM_ID, ExternalSystemTaskType.EXECUTE_TASK, project),
-      gradleRunConfiguration.settings.taskNames,
+      gradleRunConfiguration.settings.taskNames.map { it.trimQuotes()},
       project.basePath!!,
       executionSettings,
       null,
@@ -139,9 +132,17 @@ b/214640804 */
 
     // Check that the test task was re-executed, and not marked as UP-TO-DATE.
     assertThat(listener.messagesLog.lines()).doesNotContain("> Task :app:testDebugUnitTest UP-TO-DATE")
-/* b/214640804
     assertThat(listener.messagesLog.lines()).contains("> Task :app:testDebugUnitTest")
-b/214640804 */
+  }
+
+  @Throws(Exception::class)
+  fun testJavaModulesTestTasksAreCreated() {
+    loadProject(UNIT_TESTING)
+    val gradleJavaConfiguration = createAndroidGradleTestConfigurationFromClass(
+      project, "com.example.javalib.JavaLibJavaTest")
+    TestCase.assertNotNull(gradleJavaConfiguration)
+    // See above comment about the changes to task names.
+    assertThat(gradleJavaConfiguration!!.settings.taskNames).isEqualTo(listOf(":javalib:test", "--tests", "\"com.example.javalib.JavaLibJavaTest\""))
   }
 
   private fun createConfigurationFromContext(psiFile: PsiElement): ConfigurationFromContextImpl? {
@@ -151,17 +152,15 @@ b/214640804 */
 
   private fun checkConfigurationTasksAreAsExpected(
     configurationFromContext: ConfigurationFromContextImpl,
-    file: VirtualFile,
-    moduleName: String) {
+    configurationTasks: List<String>
+  ) {
     val configuration = configurationFromContext.configuration as? GradleRunConfiguration
     // Make sure that the tasks we set are expected when provided by AndroidGradleTestTasksProvider.
     val module2 = ModuleManager.getInstance(project).modules
-      .first { module ->  module.name == moduleName }
+      .first { module ->  module.name == "kotlinMultiPlatform.module2" }
     assertThat(module2).isNotNull()
 
-/* b/214640804
-    assertThat(configuration!!.settings.taskNames).containsExactly(":module2:testDebugUnitTest")
-b/214640804 */
+    assertThat(configuration!!.settings.taskNames).isEqualTo(configurationTasks)
   }
   
   private fun verifyCannotCreateGradleConfigurationFromAndroidTestDirectory() {
@@ -182,16 +181,6 @@ b/214640804 */
     assertThat(createAndroidGradleTestConfigurationFromClass(project, "google.simpleapplication.ApplicationTest")).isNull()
   }
 
-  private fun verifyAndroidGradleTestTasksProviderDoesntCreateTestTasksForJavaModule() {
-    // Here we test to verify that the configuration tasks aren't provided by the AndroidGradleTestTasksProvider.
-    // We do that by verifying the tasks value.
-    val gradleJavaConfiguration = createAndroidGradleTestConfigurationFromClass(
-      project, "google.simpleapplication.UnitTest")
-/* b/214640804
-    assertThat(gradleJavaConfiguration!!.settings.taskNames).containsExactly(":app:testDebugUnitTest")
-b/214640804 */
-  }
-
   private fun verifyCannotCreateDirectoryGradleRunConfigurationFromAndroidTestDirectory() {
     assertThat(createAndroidGradleConfigurationFromDirectory(project, "app/src/androidTest/java")).isNull()
   }
@@ -206,8 +195,8 @@ b/214640804 */
     // Make sure that the runConfiguration test tasks we set are expected when provided by AndroidGradleTestTasksProvider.
     checkConfigurationTasksAreAsExpected(
       configurationFromContext,
-      psiFile.virtualFile,
-      "kotlinMultiPlatform.module2"
+      // See above comment about the changes to task names.
+      listOf(":module2:testDebugUnitTest", "--tests", "\"com.example.library.TestUnitTest\"")
     )
   }
 
@@ -222,8 +211,8 @@ b/214640804 */
     (psiFile as? PsiDirectory)?.virtualFile?.let {
       checkConfigurationTasksAreAsExpected(
         configurationFromContext,
-        it,
-        "kotlinMultiPlatform.module2"
+        // See above comment about the changes to task names.
+        listOf(":module2:testDebugUnitTest", "--tests", "\"com.example.library.*\"")
       )
     }
   }
