5b86a3a06b7e17e4dfd766d9fec792aa0418f785
Sergio Murguia <srmurguia@google.com>
2022-01-21 16:37:49 -0800

    [SDK Index] Add functionality flags
    Added flags in new group GOOGLE_PLAY_SDK_INDEX:
    
    SHOW_SDK_INDEX_MESSAGES: Whether to show or not messages related to
    Google Play SDK Index
    INCLUDE_LINKS_TO_SDK_INDEX: Whether or not such messages should include
    links to the SDK Index
    SHOW_SDK_INDEX_CRTITICAL_ISSUES: Whether or not to include critical
    issues
    SHOW_SDK_INDEX_POLICY_ISSUES: Whether or not to include policy issues
    
    Bug: 219998752
    Test: N/A no functionality changed
    Change-Id: Ic05f9c89d81d9293d9afba9c98918867efd942fe
    
    GitOrigin-RevId: e7ed5850a4df71ab863903b02e4dea59778e5d82

--- android-common/src/com/android/tools/idea/flags/StudioFlags.java
+++ android-common/src/com/android/tools/idea/flags/StudioFlags.java
@@ -1428,5 +1428,30 @@ public final class StudioFlags {
     Flag.create(APP_LINKS_ASSISTANT, "kotlin.intent.handling", "Kotlin Intent Handling",
                 "Support adding logic for intent handling in Kotlin.", false);
   // endregion App Links Assistant
+
+  // region GOOGLE_PLAY_SDK_INDEX
+  private static final FlagGroup GOOGLE_PLAY_SDK_INDEX = new FlagGroup(FLAGS, "google.play.sdk.index", "Google Play SDK Index");
+  public static final Flag<Boolean> SHOW_SDK_INDEX_MESSAGES = Flag.create(
+    GOOGLE_PLAY_SDK_INDEX, "show.sdk.index.messages", "Show SDK Index messages",
+    "Show messages related to Google Play SDK Index",
+    true
+  );
+  public static final Flag<Boolean> INCLUDE_LINKS_TO_SDK_INDEX = Flag.create(
+    GOOGLE_PLAY_SDK_INDEX, "include.links.to.sdk.index", "Include links to SDK Index",
+    "Whether or not links to Google Play SDK Index should be included in the SDK Index messages",
+    true
+  );
+  public static final Flag<Boolean> SHOW_SDK_INDEX_CRITICAL_ISSUES = Flag.create(
+    GOOGLE_PLAY_SDK_INDEX, "show.sdk.critical.issues", "Show SDK Index critical issues",
+    "Whether or not critical issues from library authors should be shown",
+    true
+  );
+  public static final Flag<Boolean> SHOW_SDK_INDEX_POLICY_ISSUES = Flag.create(
+    GOOGLE_PLAY_SDK_INDEX, "show.sdk.policy.issues", "Show SDK Index policy issues",
+    "Whether or not show issues when libraries are not policy complaint",
+    true
+  );
+  // endregion GOOGLE_PLAY_SDK_INDEX
+
   private StudioFlags() { }
 }
