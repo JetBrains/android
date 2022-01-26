e9283a7429c3e614b1a22be51b72a06b613e44a9
Kun Shen <kunshen@google.com>
2022-01-26 12:45:16 -0800

    Move `VectorImageComponent` to `intellij.android.newProjectWizard` module
    Bug: 197015670
    Test: n/a
    Change-Id: Ifd819770835cc1678881512ec968c0ae1787eee2
    
    GitOrigin-RevId: 9ef48d0cace0837a4bead59eb696be319e787c55

--- android-npw/src/com/android/tools/idea/npw/assetstudio/ui/VectorImageComponent.java
+++ android-npw/src/com/android/tools/idea/npw/assetstudio/ui/VectorImageComponent.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015 The Android Open Source Project
+ * Copyright (C) 2022 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.android.tools.idea.ui;
+package com.android.tools.idea.npw.assetstudio.ui;
 
 import com.android.ide.common.util.AssetUtil;
 import com.android.tools.adtui.ImageComponent;
