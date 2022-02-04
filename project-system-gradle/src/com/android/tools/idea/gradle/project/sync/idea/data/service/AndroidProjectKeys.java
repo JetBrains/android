dcbb01c8b9ffcdc8bc4e4b5d14b05d7b16340bdc
Christophe Rhodes <xof@google.com>
2022-02-04 12:28:21 +0000

    [Sync] Clarify a comment
    That the ANDROID_MODEL and JAVA_MODULE_MODEL processing weights are
    the same is not a problem as those models are mutually exclusive.
    
    Bug: n/a
    Test: n/a
    Change-Id: Ib2fd878b36104619d32035cc8d966f84a8854813
    
    GitOrigin-RevId: f0ff8e77acc2bd04ac6f9167ecc618045525e4b3

--- project-system-gradle/src/com/android/tools/idea/gradle/project/sync/idea/data/service/AndroidProjectKeys.java
+++ project-system-gradle/src/com/android/tools/idea/gradle/project/sync/idea/data/service/AndroidProjectKeys.java
@@ -26,6 +26,7 @@ import com.android.tools.idea.gradle.project.model.JavaModuleModel;
 import com.android.tools.idea.gradle.project.model.NdkModuleModel;
 import com.android.tools.idea.gradle.project.sync.idea.IdeAndroidNativeVariantsModelsWrapper;
 import com.android.tools.idea.gradle.project.sync.idea.data.model.ProjectCleanupModel;
+import com.android.tools.idea.projectsystem.gradle.sync.AndroidModuleDataService;
 import com.intellij.openapi.externalSystem.model.Key;
 import org.jetbrains.annotations.NotNull;
 
@@ -34,8 +35,7 @@ import org.jetbrains.annotations.NotNull;
  * <ol>
  * <li>{@link GradleModuleModelDataService}</li>
  * <li>{@link NdkModuleModelDataService}</li>
- * <li>{@link AndroidModuleDataService}</li>
- * <li>{@link JavaModuleModelDataService}</li>
+ * <li>{@link AndroidModuleDataService} or {@link JavaModuleModelDataService} (mutually exclusive)</li>
  * <li>{@link ProjectCleanupDataService}</li>
  * </ol>
  * <br/>
