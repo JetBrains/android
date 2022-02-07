2121426f4be57feeef46574239fa572cb665bc43
Yuriy Solodkyy <solodkyy@google.com>
2022-02-07 16:32:42 +0000

    Fix dependency and facet setup
    of Android source set modules in Kotlin multiplatform projects.
    
    (1) Attach KotlinSourceSetData nodes to Android Gradle sourceSet modules
        in the data ode tree;
    
    (2) Add module dependencies to Android Gradle sourceSet modules
        according to data in `KotlinMPPGradleModel`.
    
    Bug: KTIJ-20137
    Bug: 218315443
    Test: kmp.txt
    Change-Id: Iedb4c2d5e14d0f8736a33f54bd137ff191575223
    
    GitOrigin-RevId: f0e9ec6b3459cb97008cc8a59d6c3df39cddf013

--- project-system-gradle/src/org/jetbrains/kotlin/android/configure/KotlinAndroidMPPGradleProjectResolver.kt
+++ project-system-gradle/src/org/jetbrains/kotlin/android/configure/KotlinAndroidMPPGradleProjectResolver.kt
@@ -5,57 +5,126 @@
 
 package org.jetbrains.kotlin.android.configure
 
+import com.android.tools.idea.gradle.model.IdeModuleSourceSet
 import com.android.tools.idea.gradle.project.sync.IdeAndroidModels
+import com.android.utils.appendCapitalized
 import com.intellij.openapi.externalSystem.model.DataNode
+import com.intellij.openapi.externalSystem.model.ProjectKeys
 import com.intellij.openapi.externalSystem.model.project.ModuleData
+import com.intellij.openapi.externalSystem.model.project.ModuleDependencyData
 import com.intellij.openapi.externalSystem.model.project.ProjectData
+import com.intellij.openapi.externalSystem.util.ExternalSystemApiUtil
 import com.intellij.openapi.externalSystem.util.ExternalSystemConstants
 import com.intellij.openapi.externalSystem.util.Order
 import org.gradle.tooling.model.idea.IdeaModule
-import org.jetbrains.kotlin.idea.gradle.configuration.KotlinAndroidSourceSetData
-import org.jetbrains.kotlin.idea.gradleTooling.KotlinMPPGradleModelBuilder
+import org.jetbrains.kotlin.idea.gradle.configuration.KotlinSourceSetData
 import org.jetbrains.kotlin.idea.gradleJava.configuration.KotlinMPPGradleProjectResolver
 import org.jetbrains.kotlin.idea.gradleJava.configuration.getMppModel
 import org.jetbrains.kotlin.idea.gradleTooling.KotlinMPPGradleModel
+import org.jetbrains.kotlin.idea.gradleTooling.KotlinMPPGradleModelBuilder
+import org.jetbrains.kotlin.idea.projectModel.KotlinCompilation
 import org.jetbrains.kotlin.idea.projectModel.KotlinPlatform
+import org.jetbrains.kotlin.idea.projectModel.KotlinSourceSet
 import org.jetbrains.kotlin.idea.projectModel.KotlinTarget
+import org.jetbrains.plugins.gradle.model.data.GradleSourceSetData
 import org.jetbrains.plugins.gradle.service.project.AbstractProjectResolverExtension
 
 @Order(ExternalSystemConstants.UNORDERED - 1)
 class KotlinAndroidMPPGradleProjectResolver : AbstractProjectResolverExtension() {
-    private val isAndroidProject by lazy {
-      resolverCtx.hasModulesWithModel(IdeAndroidModels::class.java)
+  override fun getToolingExtensionsClasses(): Set<Class<out Any>> {
+    return setOf(KotlinMPPGradleModelBuilder::class.java, KotlinTarget::class.java, Unit::class.java)
+  }
+
+  override fun getExtraProjectModelClasses(): Set<Class<out Any>> {
+    return setOf(KotlinMPPGradleModel::class.java, KotlinTarget::class.java)
+  }
+
+  override fun createModule(gradleModule: IdeaModule, projectDataNode: DataNode<ProjectData>): DataNode<ModuleData> {
+    return super.createModule(gradleModule, projectDataNode)!!.also {
+      maybeAttachKotlinSourceSetData(gradleModule, it)
     }
+  }
 
-    override fun getToolingExtensionsClasses(): Set<Class<out Any>> {
-        return setOf(KotlinMPPGradleModelBuilder::class.java, KotlinTarget::class.java, Unit::class.java)
+  private fun maybeAttachKotlinSourceSetData(
+    gradleModule: IdeaModule,
+    ideModule: DataNode<ModuleData>
+  ) {
+    val mppModel = resolverCtx.getMppModel(gradleModule) ?: return
+    val androidModels = resolverCtx.getExtraProject(gradleModule, IdeAndroidModels::class.java) ?: return
+    val selectedVariantName = androidModels.selectedVariantName
+    val sourceSetByName = ideModule.sourceSetsByName()
+
+    for ((sourceSetDesc, compilation) in mppModel.androidCompilationsForVariant(selectedVariantName)) {
+      val kotlinSourceSetInfo = KotlinMPPGradleProjectResolver.createSourceSetInfo(compilation, gradleModule, resolverCtx) ?: continue
+      val androidGradleSourceSetDataNode = sourceSetByName[sourceSetDesc.sourceSetName] ?: continue
+
+      androidGradleSourceSetDataNode.createChild(KotlinSourceSetData.KEY, KotlinSourceSetData(kotlinSourceSetInfo))
     }
+  }
 
-    override fun getExtraProjectModelClasses(): Set<Class<out Any>> {
-        return setOf(KotlinMPPGradleModel::class.java, KotlinTarget::class.java)
+  override fun populateModuleDependencies(gradleModule: IdeaModule, ideModule: DataNode<ModuleData>, ideProject: DataNode<ProjectData>) {
+    super.populateModuleDependencies(gradleModule, ideModule, ideProject)
+
+    val mppModel = resolverCtx.getMppModel(gradleModule) ?: return
+    val androidModels = resolverCtx.getExtraProject(gradleModule, IdeAndroidModels::class.java) ?: return
+    val selectedVariantName = androidModels.selectedVariantName
+    val sourceSetByName = ideModule.sourceSetsByName()
+
+    for ((sourceSetDesc, compilation) in mppModel.androidCompilationsForVariant(selectedVariantName)) {
+      val androidGradleSourceSetDataNode = sourceSetByName[sourceSetDesc.sourceSetName] ?: continue
+      val kotlinSourceSet = sourceSetDesc.getRootKotlinSourceSet(compilation) ?: continue
+
+      for (dependsOn in kotlinSourceSet.declaredDependsOnSourceSets) {
+        val dependsOnGradleSourceSet = sourceSetByName[dependsOn] ?: continue
+        androidGradleSourceSetDataNode.createChild(
+          ProjectKeys.MODULE_DEPENDENCY,
+          ModuleDependencyData(androidGradleSourceSetDataNode.data, dependsOnGradleSourceSet.data).also {
+            // Set up dependencies as exported since when an Android module depends on MPP module it depends on its `main` module only.
+            it.isExported = true
+          }
+        )
+      }
     }
+  }
+}
 
-    override fun createModule(gradleModule: IdeaModule, projectDataNode: DataNode<ProjectData>): DataNode<ModuleData> {
-        return super.createModule(gradleModule, projectDataNode)!!.also {
-            initializeModuleData(gradleModule, it)
-        }
+/**
+ * Returns all Android compilations for the given [variant].
+ */
+private fun KotlinMPPGradleModel.androidCompilationsForVariant(
+  variant: String
+): List<Pair<IdeModuleSourceSet, KotlinCompilation>> {
+  return targets
+    .asSequence()
+    .flatMap { it.compilations.asSequence() }
+    .filter { it.platform == KotlinPlatform.ANDROID }
+    .mapNotNull { androidKotlinCompilation ->
+      val sourceSet =
+        IdeModuleSourceSet.values().find { variant + it.androidCompilationNameSuffix() == androidKotlinCompilation.name }
+          ?: return@mapNotNull null
+      sourceSet to androidKotlinCompilation
     }
+    .toList()
+}
 
-    private fun initializeModuleData(
-        gradleModule: IdeaModule,
-        mainModuleData: DataNode<ModuleData>
-    ) {
-        if (!isAndroidProject) return
+private fun DataNode<ModuleData>.sourceSetsByName(): Map<String, DataNode<GradleSourceSetData>> {
+  return ExternalSystemApiUtil.findAll(this, GradleSourceSetData.KEY).associateBy { it.data.moduleName }
+}
 
-        val mppModel = resolverCtx.getMppModel(gradleModule) ?: return
+/**
+ * Returns the main [KotlinSourceSet] representing this [IdeModuleSourceSet] in [compilation].
+ *
+ * Usually there are multiple Kotlin source sets representing one [IdeModuleSourceSet]. For example, for [IdeModuleSourceSet.ANDROID_TEST]
+ * there might be `androidAndroidTest` and `androidAndroidTestDebug`.
+ */
+private fun IdeModuleSourceSet.getRootKotlinSourceSet(compilation: KotlinCompilation): KotlinSourceSet? {
+  val sourceSetName = compilation.disambiguationClassifier.orEmpty().appendCapitalized(sourceSetName)
+  return compilation.declaredSourceSets.singleOrNull { it.name == sourceSetName }
+}
 
-        val androidSourceSets = mppModel
-            .targets
-            .asSequence()
-            .flatMap { it.compilations.asSequence() }
-            .filter { it.platform == KotlinPlatform.ANDROID }
-            .mapNotNull { KotlinMPPGradleProjectResolver.createSourceSetInfo(it, gradleModule, resolverCtx) }
-            .toList()
-        mainModuleData.createChild(KotlinAndroidSourceSetData.KEY, KotlinAndroidSourceSetData(androidSourceSets))
-    }
+private fun IdeModuleSourceSet.androidCompilationNameSuffix() = when (this) {
+  IdeModuleSourceSet.MAIN -> ""
+  IdeModuleSourceSet.ANDROID_TEST -> "AndroidTest"
+  IdeModuleSourceSet.UNIT_TEST -> "UnitTest"
+  IdeModuleSourceSet.TEST_FIXTURES -> "TestFixtures"
 }
