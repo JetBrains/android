5fb9d7150fa1f92ea48ffa606e919d9bed7efa9d
Fisher Jomo <jomof@google.com>
2022-02-09 09:00:00 -0800

    Factor out function to create NDK release name
    This is so that it can also be used in tests to adjust
    baseline snapshots to not have fixed NDK versions
    
    Bug: NA
    Test: NA
    Change-Id: I4f9a760c251ca350efbdfce027d3b569181fa04d
    
    GitOrigin-RevId: c8717a15cdac9aaf37b9e18a8c4cbc88a0e3d5d4

--- android-navigator/src/com/android/tools/idea/navigator/nodes/ndk/includes/resolver/IndexedRegularExpressionIncludeResolver.java
+++ android-navigator/src/com/android/tools/idea/navigator/nodes/ndk/includes/resolver/IndexedRegularExpressionIncludeResolver.java
@@ -15,6 +15,8 @@
  */
 package com.android.tools.idea.navigator.nodes.ndk.includes.resolver;
 
+import static com.android.tools.idea.navigator.nodes.ndk.includes.utils.NdkVersionUtilsKt.getNdkVersionName;
+
 import com.android.repository.Revision;
 import com.android.tools.idea.navigator.nodes.ndk.includes.model.PackageType;
 import com.android.tools.idea.navigator.nodes.ndk.includes.model.SimpleIncludeValue;
@@ -72,15 +74,7 @@ public class IndexedRegularExpressionIncludeResolver extends RegularExpressionIn
       String description = myKind.myDescription;
       String version = groupOrNull(match, "ndk");
       if (version != null) {
-        // Convert NDK revision like 19.2.5345600 to standard NDK release name like r19c
-        Revision revision = Revision.parseRevision(version);
-        if (revision.getMinor() == 0) {
-          // Don't show 'a' in the NDK version. It should be r20 not r20a
-          description += String.format(" r%s", revision.getMajor());
-        } else {
-          char minor = (char)((int)'a' + revision.getMinor());
-          description += String.format(" r%s%s", revision.getMajor(), minor);
-        }
+          description += getNdkVersionName(version);
       }
       return new SimpleIncludeValue(myKind, description, libraryName, relativeFolder, includeFolder, new File(homeFolder));
     } catch (IllegalArgumentException e) {
