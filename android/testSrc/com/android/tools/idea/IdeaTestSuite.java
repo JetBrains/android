e9d3b1f8e4c9f3e10b8c4deb8f45606dddd8fb3a
Chris Warrington <cmw@google.com>
2022-02-16 14:47:47 +0000

    Use AGP-as-built-by-gradle in android tests
    Bug: 143864616
    Test: existing
    Change-Id: Iead57e8807f13262a51168252e42e72ccc0c58c0
    
    GitOrigin-RevId: bdba87b17cab3cefc12a2118f3f3f6d2a9412402

--- android/testSrc/com/android/tools/idea/IdeaTestSuite.java
+++ android/testSrc/com/android/tools/idea/IdeaTestSuite.java
@@ -47,7 +47,8 @@ public class IdeaTestSuite extends IdeaTestSuiteBase {
 
   static {
     try {
-      linkIntoOfflineMavenRepo("tools/base/build-system/studio_repo.manifest");
+      unzipIntoOfflineMavenRepo("tools/base/build-system/android_gradle_plugin.zip");
+      linkIntoOfflineMavenRepo("tools/base/build-system/android_gradle_plugin_runtime_dependencies.manifest");
       linkIntoOfflineMavenRepo("tools/adt/idea/android/test_deps.manifest");
       linkIntoOfflineMavenRepo("tools/base/build-system/integration-test/kotlin_gradle_plugin_prebuilts.manifest");
       // When using iml_module's split_test_target attribute, not all bazel targets will include this dependency.
