e2c68a0f561d9335129e01cfce5351de75b5a78e
Diego Perez <diegoperez@google.com>
2022-02-16 15:50:53 +0000

    Make locating output roots lazy
    The GradleClassFileFinder would eagerly convern File to VirtualFile even
    for paths that do not needed it. This change, makes the first conversion
    lazy.
    
    Bug: N/A
    Test: GradleClassFileFinderTest
    Change-Id: Ic1825e810fc018a3107594bad97c070c1338b20a
    
    GitOrigin-RevId: 4de5ce9f612c59f8ae4e0c425110e01056497ab1

---
+++ project-system-gradle/src/com/android/tools/idea/projectsystem/gradle/GradleClassFinderUtil.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.projectsystem.gradle;
+
+import com.android.tools.idea.gradle.model.IdeAndroidArtifact;
+import com.android.tools.idea.gradle.project.model.GradleAndroidModel;
+import com.intellij.openapi.compiler.CompilerPaths;
+import com.intellij.openapi.module.Module;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+import org.jetbrains.annotations.NotNull;
+
+public class GradleClassFinderUtil {
+  private GradleClassFinderUtil() {};
+
+  @NotNull
+  private static Stream<File> getCompilerOutputRoots(@NotNull GradleAndroidModel model, boolean includeAndroidTests) {
+    List<IdeAndroidArtifact> artifacts = new ArrayList<>();
+    artifacts.add(model.getMainArtifact());
+    if (includeAndroidTests) {
+      artifacts.add(model.getArtifactForAndroidTest());
+    }
+    return artifacts.stream().flatMap((artifactInfo) -> artifactInfo.getClassesFolder().stream());
+  }
+
+  /**
+   * Returns all the compiler output roots for the given {@link Module}. If {@code includeAndroidTests} is true, it will include
+   * also the output for tests.
+   */
+  @NotNull
+  public static Stream<File> getModuleCompileOutputs(@NotNull Module module, boolean includeAndroidTests) {
+    GradleAndroidModel androidModel = GradleAndroidModel.get(module);
+    if (androidModel != null) {
+      return getCompilerOutputRoots(androidModel, includeAndroidTests);
+    }
+
+    // The module is not an Android module. Check for regular Java outputs.
+    Module[] modules = {module};
+    return Stream.of(CompilerPaths.getOutputPaths(modules))
+      .map(File::new);
+  }
+}
