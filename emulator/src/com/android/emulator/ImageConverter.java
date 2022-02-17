69aec708df3bd3f6b44f89581eeaf9b48d619529
Sergey Prigogin <sprigogin@google.com>
2022-02-16 17:12:54 -0800

    Fix the path of the native image_converter library
    Test: existing
    Bug: 220022493
    Change-Id: I29aa1f9361580c3f270c15d5b58bd7486c0f5676
    
    GitOrigin-RevId: 6fd92c2026c94619046e55b67312e79161965abd

--- emulator/src/com/android/emulator/ImageConverter.java
+++ emulator/src/com/android/emulator/ImageConverter.java
@@ -106,7 +106,7 @@ public class ImageConverter {
     String libName = getLibName();
     Path homePath = Paths.get(PathManager.getHomePath());
     // Installed Studio.
-    Path libFile = homePath.resolve("plugins/android/native").resolve(libName);
+    Path libFile = homePath.resolve("plugins/android/resources/native").resolve(libName);
     if (Files.exists(libFile)) {
       return libFile;
     }
