468aba65bd9d1df098e20b73f68b4830b21b9076
Charlie Tsai <chartsai@google.com>
2022-02-25 01:41:38 +0000

    Update the test cases in VisualizationToolWindowFactoryTest
    Fixes the tests after using the new API of ToolWindowFactory.
    
    Bug: N/A
    Test: covered
    Change-Id: I4220bea940d0b16c3b719534f58cb0fc1a220c01
    
    GitOrigin-RevId: d08cb73f06c4af2d2ae1fe0141d1766a164e6743

--- designer/testSrc/com/android/tools/idea/uibuilder/visual/VisualizationToolWindowFactoryTest.kt
+++ designer/testSrc/com/android/tools/idea/uibuilder/visual/VisualizationToolWindowFactoryTest.kt
@@ -47,34 +47,34 @@ class VisualizationToolWindowFactoryTest {
 
   @Test
   fun testAvailableWhenOpeningProject() {
-    val toolWindow = VisualizationTestToolWindow(projectRule.project)
-
     val factory = VisualizationToolWindowFactory()
-    factory.isApplicable(projectRule.project)
 
+    // Not available when no file is opened.
+    assertFalse(factory.shouldBeAvailable(projectRule.project))
+
+    // Available when a layout file is opened.
     val layoutFile = projectRule.fixture.addFileToProject("res/layout/my_layout.xml", LAYOUT_FILE_TEXT)
     WriteCommandAction.runWriteCommandAction(projectRule.project) {
       projectRule.fixture.openFileInEditor(layoutFile.virtualFile)
-      factory.init(toolWindow)
     }
-/* fails after Change Ia4a9b92f
-    assertTrue(toolWindow.isAvailable)
-fails after Change Ia4a9b92f */
+    assertTrue(factory.shouldBeAvailable(projectRule.project))
 
+    WriteCommandAction.runWriteCommandAction(projectRule.project) {
+      FileEditorManager.getInstance(projectRule.project).closeFile(layoutFile.virtualFile)
+    }
 
+    // Not available when there is no opened layout file.
     val ktFile = projectRule.fixture.addFileToProject("src/my_test_project/SomeFile.kt", KT_FILE_TEXT)
     WriteCommandAction.runWriteCommandAction(projectRule.project) {
       projectRule.fixture.openFileInEditor(ktFile.virtualFile)
-      factory.init(toolWindow)
     }
-    assertFalse(toolWindow.isAvailable)
+    assertFalse(factory.shouldBeAvailable(projectRule.project))
   }
 
   @Test
   fun testAvailableWhenSwitchingFile() {
     val toolWindow = VisualizationTestToolWindow(projectRule.project)
     val factory = VisualizationToolWindowFactory()
-    factory.isApplicable(projectRule.project)
     WriteCommandAction.runWriteCommandAction(projectRule.project) { factory.init(toolWindow) }
 
     val layoutFile = projectRule.fixture.addFileToProject("res/layout/my_layout.xml", LAYOUT_FILE_TEXT)
@@ -101,17 +101,13 @@ fails after Change Ia4a9b92f */
   fun testAvailableWhenClosingFile() {
     val toolWindow = VisualizationTestToolWindow(projectRule.project)
     val factory = VisualizationToolWindowFactory()
-    factory.isApplicable(projectRule.project)
+    factory.init(toolWindow)
 
     val layoutFile = projectRule.fixture.addFileToProject("res/layout/my_layout.xml", LAYOUT_FILE_TEXT)
     WriteCommandAction.runWriteCommandAction(projectRule.project) {
       projectRule.fixture.openFileInEditor(layoutFile.virtualFile)
-      factory.init(toolWindow)
     }
-/* fails after Change Ia4a9b92f
     assertTrue(toolWindow.isAvailable)
-fails after Change Ia4a9b92f */
-
 
     WriteCommandAction.runWriteCommandAction(projectRule.project) {
       FileEditorManager.getInstance(projectRule.project).closeFile(layoutFile.virtualFile)
