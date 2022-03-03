56920b8e20fbf57f4622d9d4928c1b64d5f8d318
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2022-03-03 14:56:02 +0100

    [platform] dropping obsolete implementations of macOS message dialogs; wiring clients to `MessagesService`
    (cherry picked from commit ba20ae9c3103066e341c51c3ea527fc837d2a73f)
    
    IJ-CR-21641
    
    GitOrigin-RevId: 5a869d12f139d6636e06a45a3deebb443ee8f392

--- uitest-framework/testSrc/com/android/tools/idea/tests/gui/framework/fixture/MessagesFixture.java
+++ uitest-framework/testSrc/com/android/tools/idea/tests/gui/framework/fixture/MessagesFixture.java
@@ -18,7 +18,6 @@ package com.android.tools.idea.tests.gui.framework.fixture;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.ui.Messages;
 import com.intellij.openapi.util.JDOMUtil;
-import com.intellij.ui.messages.SheetController;
 import org.fest.swing.core.GenericTypeMatcher;
 import org.fest.swing.core.Robot;
 import org.fest.swing.fixture.ContainerFixture;
