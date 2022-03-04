0836147171f1cb30e04ee8ff12bd07c033d49c4d
Charlie Tsai <chartsai@google.com>
2022-03-04 14:31:34 +0000

    Removed file issues from issue panel when its file is not selected
    In Compose, the IssueModel.deactivate() may not be caleld immediately
    when selected editor is changed. By listening the file editor event,
    shared issue panel can remove the file issues when their editors are no
    longer selected.
    
    Bug: 222110455
    Test: manually (UI-related)
    Change-Id: I9de27bab996c0863e2d0b7f97b3a1f7ab93f0e3a
    
    GitOrigin-RevId: ca1e8973932b105623f46815976e1aea71cbb149

--- designer/src/com/android/tools/idea/common/error/DesignerCommonIssueProvider.kt
+++ designer/src/com/android/tools/idea/common/error/DesignerCommonIssueProvider.kt
@@ -15,9 +15,14 @@
  */
 package com.android.tools.idea.common.error
 
+import com.intellij.notebook.editor.BackedVirtualFile
 import com.intellij.openapi.Disposable
+import com.intellij.openapi.fileEditor.FileEditorManager
+import com.intellij.openapi.fileEditor.FileEditorManagerEvent
+import com.intellij.openapi.fileEditor.FileEditorManagerListener
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.Disposer
+import com.intellij.openapi.vfs.VirtualFile
 
 interface DesignerCommonIssueProvider<T> : Disposable {
   var filter: (Issue) -> Boolean
@@ -26,6 +31,8 @@ interface DesignerCommonIssueProvider<T> : Disposable {
 }
 
 class DesignToolsIssueProvider(project: Project) : DesignerCommonIssueProvider<Any?> {
+
+  private val fileEditorManager: FileEditorManager
 
   private val sourceToIssueMap = mutableMapOf<Any, List<Issue>>()
 
@@ -39,17 +46,66 @@ class DesignToolsIssueProvider(project: Project) : DesignerCommonIssueProvider<A
 
   init {
     Disposer.register(project, this)
+    fileEditorManager = FileEditorManager.getInstance(project)
     messageBusConnection.subscribe(IssueProviderListener.TOPIC, object : IssueProviderListener {
       override fun issueUpdated(source: Any, issues: List<Issue>) {
-        if (issues.isEmpty()) {
+        val filteredIssues = filterSelectedOrNoFileIssues(issues)
+        if (filteredIssues.isEmpty()) {
           sourceToIssueMap.remove(source)
         }
         else {
-          sourceToIssueMap[source] = issues
+          sourceToIssueMap[source] = filteredIssues
         }
         listeners.forEach { it.run() }
       }
     })
+
+    // This is a workaround to remove the issues if [IssueModel.deactivate()] is not called when the selected editor is changed.
+    // This may happen in compose preview, which calls [DesignSurface.deactivate()] delayed when editor is changed.
+    // TODO(b/222110455): Make [DesignSurface] deactivate the IssueModel when it is no longer visible.
+    messageBusConnection.subscribe(FileEditorManagerListener.FILE_EDITOR_MANAGER, object : FileEditorManagerListener {
+      override fun fileClosed(source: FileEditorManager, file: VirtualFile) {
+        if (!source.hasOpenFiles()) {
+          cleanUpFileIssues()
+        }
+      }
+
+      override fun selectionChanged(event: FileEditorManagerEvent) {
+        cleanUpFileIssues()
+      }
+
+      /**
+       * Remove the file issues if the file is not visible.
+       */
+      private fun cleanUpFileIssues() {
+        for ((source, issues) in sourceToIssueMap.toMap()) {
+          val filteredIssues = filterSelectedOrNoFileIssues(issues)
+          if (filteredIssues.isEmpty()) {
+            sourceToIssueMap.remove(source)
+          }
+          else {
+            sourceToIssueMap[source] = filteredIssues
+          }
+        }
+      }
+    })
+  }
+
+  /**
+   * Remove the file issues if their editors are not selected.
+   * If an issue is not from the file (which its [Issue.source.file] is null), it will NOT be removed.
+   */
+  @Suppress("UnstableApiUsage")
+  private fun filterSelectedOrNoFileIssues(issues: List<Issue>): List<Issue> {
+    val files = fileEditorManager.selectedEditors.mapNotNull { it.file }
+    val ret = mutableListOf<Issue>()
+    for (issue in issues) {
+      val issueFile = issue.source.file?.let { BackedVirtualFile.getOriginFileIfBacked(it) }
+      if (issueFile == null || files.contains(issueFile)) {
+        ret.add(issue)
+      }
+    }
+    return ret
   }
 
   override fun getFilteredIssues(): List<Issue> = sourceToIssueMap.values.flatten().filter(filter).toList()
