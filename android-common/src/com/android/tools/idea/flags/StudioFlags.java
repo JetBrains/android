83af17e1c1d5764a90d10b74a4400a14ae3b8f93
Sergio Murguia <srmurguia@google.com>
2022-03-08 03:44:16 -0800

    [SDK Index] Change default flag values
    ... so only messages for outdated libraries are shown during first
    canary.
    
    Bug: 219998659
    Test: GradleDetectorTest
    Change-Id: I77430b04874f63bc575d55d725ea299bd7d31b66
    
    GitOrigin-RevId: fbfc85bf4cedbddf02c25b92f3aefd32ccb56772

--- android-common/src/com/android/tools/idea/flags/StudioFlags.java
+++ android-common/src/com/android/tools/idea/flags/StudioFlags.java
@@ -1439,17 +1439,17 @@ public final class StudioFlags {
   public static final Flag<Boolean> INCLUDE_LINKS_TO_SDK_INDEX = Flag.create(
     GOOGLE_PLAY_SDK_INDEX, "include.links.to.sdk.index", "Include links to SDK Index",
     "Whether or not links to Google Play SDK Index should be included in the SDK Index messages",
-    true
+    false
   );
   public static final Flag<Boolean> SHOW_SDK_INDEX_CRITICAL_ISSUES = Flag.create(
     GOOGLE_PLAY_SDK_INDEX, "show.sdk.critical.issues", "Show SDK Index critical issues",
     "Whether or not critical issues from library authors should be shown",
-    true
+    false
   );
   public static final Flag<Boolean> SHOW_SDK_INDEX_POLICY_ISSUES = Flag.create(
     GOOGLE_PLAY_SDK_INDEX, "show.sdk.policy.issues", "Show SDK Index policy issues",
     "Whether or not show issues when libraries are not policy complaint",
-    true
+    false
   );
   // endregion GOOGLE_PLAY_SDK_INDEX
 
