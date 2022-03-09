64a35d47b043a2bce87262f3e1846949a67f3de5
Sergey Prigogin <sprigogin@google.com>
2022-03-09 11:50:36 -0800

    Add an explicit exit call
    Test: later
    Bug: N/A
    Change-Id: Ie43071c43c153223f482e9012f2b6e935c692439
    
    GitOrigin-RevId: 23930c423c32134caf00e931f2d946a8bf88d67b

--- emulator/screen-sharing-agent/app/src/main/cpp/main.cc
+++ emulator/screen-sharing-agent/app/src/main/cpp/main.cc
@@ -34,6 +34,10 @@ Java_com_android_tools_screensharing_Main_nativeMain(JNIEnv* jni_env, jclass thi
     args.push_back(arg.GetValue());
   }
 
-  Agent agent(args);
-  agent.Run();
+  {
+    Agent agent(args);
+    agent.Run();
+  }
+  // Exit explicitly to bypass the final JVM cleanup that for some unclear reason sometimes crashes with SIGSEGV.
+  exit(EXIT_SUCCESS);
 }
