2e0e587d159c0c93aab9e31a9a41a53590539a35
Charlie Tsai <chartsai@google.com>
2022-03-24 12:35:26 +0000

    Add ui test for shared issue panel
    Bug: N/A
    Test: N/A
    Change-Id: Ic948fd1d5a2d92876030599241423c184901921a
    
    GitOrigin-RevId: f44df4dd264fa056954fd47a26613e24fb74c9e9

---
+++ android-uitests/testSrc/com/android/tools/idea/tests/gui/uibuilder/SharedIssuePanelTest.kt
@@ -0,0 +1,87 @@
+/*
+ * Copyright (C) 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.tests.gui.uibuilder
+
+import com.intellij.testGuiFramework.framework.GuiTestRemoteRunner
+import com.android.tools.idea.tests.gui.framework.GuiTestRule
+import com.android.tools.idea.bleak.UseBleak
+import com.android.tools.idea.flags.StudioFlags
+import com.android.tools.idea.tests.gui.framework.RunIn
+import com.android.tools.idea.tests.gui.framework.TestGroup
+import com.android.tools.idea.tests.gui.framework.fixture.ProblemsPaneFixture
+import com.android.tools.idea.tests.gui.framework.fixture.EditorFixture
+import com.google.common.truth.Truth.assertThat
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.lang.Exception
+import java.util.concurrent.TimeUnit
+
+/**
+ * UI test for the visualization tool window
+ */
+@RunWith(GuiTestRemoteRunner::class)
+class SharedIssuePanelTest {
+  @JvmField
+  @Rule
+  val guiTest = GuiTestRule().withTimeout(5, TimeUnit.MINUTES)
+
+  @JvmField
+  @Rule
+  val renderTaskLeakCheckRule = RenderTaskLeakCheckRule()
+
+  private val title = "Layout and Qualifiers"
+
+  @Before
+  fun setup() {
+    StudioFlags.NELE_USE_SHARED_ISSUE_PANEL_FOR_DESIGN_TOOLS.override(true)
+  }
+
+  @After
+  fun tearDown() {
+    StudioFlags.NELE_USE_SHARED_ISSUE_PANEL_FOR_DESIGN_TOOLS.clearOverride()
+  }
+
+  @Test
+  @UseBleak
+  @RunIn(TestGroup.UNRELIABLE)
+  @Throws(Exception::class)
+  fun checkSharedIssuePanelVisibilityWithBleak() {
+    val ideFixture = guiTest.importSimpleApplication()
+    guiTest.runWithBleak { checkSharedIssuePanelVisibility(ideFixture.editor, ProblemsPaneFixture(ideFixture)) }
+  }
+
+  private fun checkSharedIssuePanelVisibility(editor: EditorFixture, problemsPane: ProblemsPaneFixture) {
+    val layoutFile = "app/src/main/res/layout/frames.xml"
+    val javaFile = "app/src/main/java/google/simpleapplication/MyActivity.java"
+
+    problemsPane.activate()
+
+    editor.open(layoutFile).layoutEditor.waitForRenderToFinish()
+    assertThat(problemsPane.isTabSelected(title)).isTrue()
+
+    editor.open(javaFile)
+    assertThat(problemsPane.isTabExist(title)).isFalse()
+
+    // Test switching back.
+    editor.open(layoutFile)
+    assertThat(problemsPane.isTabSelected(title)).isTrue()
+
+    editor.closeFile(layoutFile).closeFile(javaFile)
+  }
+}
---
+++ uitest-framework/testSrc/com/android/tools/idea/tests/gui/framework/fixture/ProblemsPaneFixture.kt
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) 2014 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.tests.gui.framework.fixture
+
+import com.intellij.analysis.problemsView.toolWindow.ProblemsView
+import com.intellij.openapi.util.text.StringUtil
+import org.fest.swing.util.TextMatcher
+
+/**
+ * Fixture for the ProblemsPane in the IDE
+ */
+class ProblemsPaneFixture(ideFrameFixture: IdeFrameFixture) :
+  ToolWindowFixture(ProblemsView.ID, ideFrameFixture.project, ideFrameFixture.robot()) {
+
+  fun isTabExist(tabTitle: String): Boolean {
+    val nameMatcher = TabNameMatcher(tabTitle)
+    return contents.any { nameMatcher.isMatching(it.displayName) }
+  }
+
+  fun isTabSelected(tabTitle: String) = getContent(TabNameMatcher(tabTitle))?.isSelected ?: false
+}
+
+/**
+ * The tab name of problems pane can be a html text, and may have an issue count at the tail.
+ * So we check the prefix of content string.
+ */
+private class TabNameMatcher(private val tabName: String) : TextMatcher {
+  override fun isMatching(text: String?): Boolean {
+    val textWithoutHtml = text?.let { StringUtil.removeHtmlTags(it) } ?: return false
+    return textWithoutHtml.trim().startsWith(tabName)
+  }
+
+  override fun description() = "Cannot find the tab $tabName"
+
+  override fun formattedValues() = "\"$tabName\"";
+}
