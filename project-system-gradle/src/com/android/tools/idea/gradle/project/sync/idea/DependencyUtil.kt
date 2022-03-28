61b311c299a536db5909f109d93669b2dff9820b
Bradley Smith <smithbradley@google.com>
2022-03-28 09:16:56 -0700

    Don't crash when we can't match a module dependency
    Instead log a warning with the missing dependency and ignore it.
    We still error here in tests.
    
    Bug: 208357458
    Test: manually tested sync no loner crashs in production
    Change-Id: I0aa2be734aa164674f17999a5de5b4aa5b5d11e1
    
    GitOrigin-RevId: 45faa8e7d934b765573120b7dbcc13a40104f6c5

--- project-system-gradle/src/com/android/tools/idea/gradle/project/sync/idea/DependencyUtil.kt
+++ project-system-gradle/src/com/android/tools/idea/gradle/project/sync/idea/DependencyUtil.kt
@@ -212,9 +212,12 @@ private class AndroidDependenciesSetupContext(
   private fun createModuleLibraryWorkItem(library: IdeModuleDependency): ModuleLibraryWorkItem? {
     if (library.projectPath.isEmpty()) return null
     val targetModuleGradlePath = computeModuleIdForLibraryTarget(library)
-    val targetData =
-      (gradleProjectPathToModuleData(targetModuleGradlePath)
-       ?: error("Cannot find module with id: $targetModuleGradlePath")) // uncomment and see what we gte in IdeaProject
+    val targetData = gradleProjectPathToModuleData(targetModuleGradlePath)
+    if (targetData == null) {
+      // TODO(b/208357458): Once we correct source set matching we need to revisit whether this should produce an error
+      LOG.warnInProduction(ExternalSystemException("Cannot find module with id: $targetModuleGradlePath"))
+      return null;
+    }
     return ModuleLibraryWorkItem(targetModuleGradlePath, targetData)
   }
 
