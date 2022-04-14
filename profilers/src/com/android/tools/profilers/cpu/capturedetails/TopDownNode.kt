e604015c3ecb2a7e06f59c5b7a39045f49ea9179
Phil Nguyen <philnguyen@google.com>
2022-04-14 14:04:21 -0700

    Convert `TopDownNode` to Kotlin
    Bug: n/a
    Test: existing
    Change-Id: I7d3f81da7e0aa15ee2c472a5aef8838491204587
    
    GitOrigin-RevId: 28529ad8b848a909febf28687bb9abf07fe33641

---
+++ profilers/src/com/android/tools/profilers/cpu/capturedetails/TopDownNode.kt
@@ -0,0 +1,73 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.profilers.cpu.capturedetails
+
+import com.android.tools.profilers.cpu.CaptureNode
+import com.android.tools.profilers.cpu.nodemodel.CaptureNodeModel
+
+/**
+ * A top-down CPU usage tree. This is a node on that tree and represents all the calls that share the same callstack upto a point.
+ * It's created from an execution tree by merging the nodes with the same path from the root.
+ */
+class TopDownNode(node: CaptureNode) : CpuTreeNode<TopDownNode>(node.data.id) {
+  init {
+    addNode(node)
+
+    // We're adding unmatched children separately, because we don't want to merge unmatched with matched,
+    // i.e all merged children should have the same {@link CaptureNode.FilterType}.
+    addChildren(node, false)
+    addChildren(node, true)
+  }
+
+  /**
+   * Adds children of {@param node} whose filter type matches to the flag {@param unmatched}.
+   */
+  private fun addChildren(node: CaptureNode, unmatched: Boolean) {
+    val children = HashMap<String, TopDownNode>()
+    for (child in node.children) {
+      if (unmatched == child.isUnmatched) {
+        val other = TopDownNode(child)
+        when (val prev = children[child.data.id]) {
+          null -> {
+            children[child.data.id] = other
+            addChild(other)
+          }
+          else -> prev.merge(other)
+        }
+      }
+    }
+  }
+
+  private fun merge(other: TopDownNode) {
+    addNodes(other.nodes)
+
+    // We use a separate map for unmatched children, because we can not merge unmatched with matched,
+    // i.e all merged children should have the same {@link CaptureNode.FilterType};
+    val matchedChildren = hashMapOf<String, TopDownNode>()
+    val unmatchedChildren = hashMapOf<String, TopDownNode>()
+    fun childrenMap(unmatched: Boolean) = if (unmatched) unmatchedChildren else matchedChildren
+    getChildren().forEach { childrenMap(it.isUnmatched)[it.id] = it }
+    for (otherChild in other.children) {
+      when (val existing = childrenMap(otherChild.isUnmatched)[otherChild.id]) {
+        null -> addChild(otherChild)
+        else -> existing.merge(otherChild)
+      }
+    }
+  }
+
+  override fun getMethodModel(): CaptureNodeModel = nodes[0].data
+  override fun getFilterType(): CaptureNode.FilterType = nodes[0].filterType
+}
