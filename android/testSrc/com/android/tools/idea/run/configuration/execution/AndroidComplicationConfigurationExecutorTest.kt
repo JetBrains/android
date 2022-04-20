4034291d46f6338bf759746691a7d719bb742baf
kovalp <kovalp@google.com>
2022-04-20 12:12:37 +0100

    Increase timeout in tests
    Fixes: 229795220
    Test: existing tests passing
    Change-Id: Ib7521efcee73e8563a59843ffd26aad7a0582786
    
    GitOrigin-RevId: c1e69afd3a5a0e85dc06cf52213c47b0af54a8d9

--- android/testSrc/com/android/tools/idea/run/configuration/execution/AndroidComplicationConfigurationExecutorTest.kt
+++ android/testSrc/com/android/tools/idea/run/configuration/execution/AndroidComplicationConfigurationExecutorTest.kt
@@ -143,7 +143,7 @@ class AndroidComplicationConfigurationExecutorTest : AndroidConfigurationExecuto
       consoleViewImpl.flushDeferredText()
       consoleOutputPromise.complete(consoleViewImpl.editor.document.text)
     }
-    val consoleOutput = consoleOutputPromise.get(1, TimeUnit.SECONDS)
+    val consoleOutput = consoleOutputPromise.get(10, TimeUnit.SECONDS)
     assertThat(consoleOutput)
       .contains("Warning: The chosen Wear device may kill background services if they take too long to respond, which can " +
                 "affect debugging. To avoid this, please update the Wear OS app on your device to the latest version.")
