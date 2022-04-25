65b37f5f250d9f7487c77b35bf5897fbec470d32
Alon Albert <aalbert@google.com>
2022-04-25 15:03:37 -0700

    Add LogcatMessageAssembler Class
    Assembles lines into LogCatMessage's
    
    Bug: 191880898
    Test: Added
    Change-Id: I0c7239f5e1ab6ac80148f59034ab4cbb55e8301b
    
    GitOrigin-RevId: 0c7d53eef1d1f9f0e007c777ba6be4a9ad280e13

---
+++ logcat/src/com/android/tools/idea/logcat/service/LogcatMessageAssembler.kt
@@ -0,0 +1,199 @@
+/*
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.logcat.service
+
+import com.android.ddmlib.logcat.LogCatHeader
+import com.android.ddmlib.logcat.LogCatHeaderParser
+import com.android.ddmlib.logcat.LogCatMessage
+import com.android.tools.idea.adb.processnamemonitor.ProcessNameMonitor
+import com.android.tools.idea.concurrency.AndroidCoroutineScope
+import com.android.tools.idea.logcat.SYSTEM_HEADER
+import com.android.tools.idea.logcat.folding.StackTraceExpander
+import com.intellij.openapi.Disposable
+import com.intellij.openapi.project.Project
+import kotlinx.coroutines.channels.SendChannel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import org.jetbrains.annotations.TestOnly
+import java.util.concurrent.atomic.AtomicReference
+import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.EmptyCoroutineContext
+
+private const val SYSTEM_LINE_PREFIX = "--------- beginning of "
+
+/**
+ * Last message in batch will be posted after a delay, to allow for more log lines if another batch is pending.
+ */
+private const val DELAY_MILLIS = 100L
+
+/**
+ * Receives batches of lines from an `adb logcat -v long` process and assembles them into complete [LogCatMessage]'s.
+ *
+ * A logcat entry starts with a header:
+ *
+ * `[          1650901603.644  1505: 1539 W/BroadcastQueue ]`
+ *
+ * And is followed by one or more lines. The entry is terminated by an empty line but there is nothing preventing users from including an
+ * empty line in a proper message so there is no reliable way to determine the end of a Logcat entry. The only reliable trigger that an
+ * entry has ended is when a new header is parsed.
+ *
+ * This means that the last entry cannot be assembled until the next entry is emitted, which could cause an unwanted delay.
+ *
+ * To avoid this delay, after finishing precessing a batch of lines, we schedule a delayed task to flush the last message if nothing arrives
+ * in time.
+ *
+ * This class is derived from [com.android.tools.idea.logcat.AndroidLogcatReceiver]
+ */
+internal class LogcatMessageAssembler @TestOnly internal constructor(
+  project: Project,
+  val serialNumber: String,
+  private val channel: SendChannel<List<LogCatMessage>>,
+  private val headerParser: LogCatHeaderParser,
+  private val processNameMonitor: ProcessNameMonitor,
+  coroutineContext: CoroutineContext,
+) : Disposable {
+  constructor(project: Project, serialNumber: String, channel: SendChannel<List<LogCatMessage>>)
+    : this(project, serialNumber, channel, LogCatHeaderParser(), ProcessNameMonitor.getInstance(project), EmptyCoroutineContext)
+
+  private val coroutineScope = AndroidCoroutineScope(this, coroutineContext)
+
+
+  /**
+   * Keep a lambda se we don't allocate a new one for each parsed line
+   */
+  private val applicationIdGetter = this::getApplicationId
+
+  private val previousState = AtomicReference<PartialMessage?>()
+
+
+  /**
+   * Parse a batch of new lines.
+   *
+   * Lines are expected to arrive in the following structure:
+   * ```
+   *   [ header 1 ]
+   *   Line 1
+   *   Line 2
+   *   ...
+   *
+   *   [ header 2]
+   *   Line 1
+   *   Line 2
+   *   ...
+   * ```
+   *
+   * There seems to be no way to deterministically detect the end of a log entry because the EOM is indicated by an empty line but an empty
+   * line can be also be a valid part of a log entry. A valid header is a good indication that the previous entry has ended, but we can't
+   * just hold on to an entry until we get a new header because there could be a long pause between entries.
+   *
+   * We address this issue by posting a delayed job that will flush the last entry in a batch. If we get another batch before the delayed
+   * job executes, we cancel it.
+   */
+  suspend fun processNewLines(newLines: List<String>) {
+    // New batch has arrived so effectively cancel the pending job by resetting previousState
+    val state = previousState.getAndSet(null)
+
+    // Parse new lines and send log messages
+    val batch: Batch = parseNewLines(state, newLines)
+    if (batch.messages.isNotEmpty()) {
+      channel.send(batch.messages)
+    }
+
+    // Save the last header/lines to handle in the next batch or in the delayed job
+    previousState.set(PartialMessage(batch.lastHeader, batch.lastLines))
+
+    // If there is a valid last message in the batch, queue it for sending in case there is no imminent next batch coming
+    if (batch.lastHeader != null && batch.lastLines.isNotEmpty()) {
+      coroutineScope.launch {
+        delay(DELAY_MILLIS)
+        val pendingState = previousState.getAndSet(null)
+        if (pendingState?.header != null) {
+          channel.send(listOf(LogCatMessage(pendingState.header, pendingState.lines.toMessage())))
+        }
+      }
+    }
+  }
+
+  override fun dispose() {}
+
+  private fun parseNewLines(
+    state: PartialMessage?, newLines: List<String>): Batch {
+
+    var lastHeader = state?.header
+    val lastLines = state?.lines?.toMutableList() ?: mutableListOf()
+    val batchMessages = mutableListOf<LogCatMessage>()
+
+    for (line in newLines.map { it.fixLine() }) {
+      if (line.isSystemLine()) {
+        batchMessages.add(LogCatMessage(SYSTEM_HEADER, line))
+        continue
+      }
+      val header = headerParser.parseHeader(line, applicationIdGetter)
+      if (header != null) {
+        // It's a header, flush active lines.
+        if (lastHeader != null && lastLines.isNotEmpty()) {
+          batchMessages.add(LogCatMessage(lastHeader, lastLines.toMessage()))
+        }
+        // previous lines without a previous header are discarded
+        lastLines.clear()
+        lastHeader = header
+      }
+      else {
+        lastLines.add(line)
+      }
+    }
+    return Batch(batchMessages, lastHeader, lastLines)
+  }
+
+  private fun getApplicationId(pid: Int): String {
+    return processNameMonitor.getProcessNames(serialNumber, pid)?.applicationId ?: "pid-$pid"
+  }
+
+  /**
+   * A batch consists of the first n-1 log entries in a batch. The last entry can be incomplete and is stored as a header and a list of
+   * lines.
+   */
+  private class Batch(val messages: List<LogCatMessage>, val lastHeader: LogCatHeader?, val lastLines: List<String>)
+
+  /**
+   * A header and lines of a possibly unfinished message.
+   */
+  private class PartialMessage(val header: LogCatHeader?, val lines: List<String>)
+}
+
+private fun String.isSystemLine(): Boolean {
+  return startsWith(SYSTEM_LINE_PREFIX)
+
+}
+
+/**
+ * Really, the user's log should never put any system characters in it ever - that will cause
+ * it to get filtered by our strict regex patterns (see AndroidLogcatFormatter). The reason
+ * this might happen in practice is due to a bug where either adb or logcat (not sure which)
+ * is too aggressive about converting \n's to \r\n's, including those that are quoted. This
+ * means that a user's log, if it uses \r\n itself, is converted to \r\r\n. Then, when
+ * MultiLineReceiver, which expects valid input, strips out \r\n, it leaves behind an extra \r.
+ *
+ * Unfortunately this isn't a case where we can fix the root cause because adb and logcat are
+ * both external to Android Studio. In fact, the latest adb/logcat versions have already fixed
+ * this issue! But we still need to run properly with older versions. Also, putting this fix in
+ * MultiLineReceiver isn't right either because it is used for more than just receiving logcat.
+ */
+private fun String.fixLine(): String {
+  return replace("\r", "")
+}
+
+private fun List<String>.toMessage(): String = StackTraceExpander.process(this).joinToString("\n").trimEnd('\n')
---
+++ logcat/testSrc/com/android/tools/idea/logcat/service/LogcatMessageAssemblerTest.kt
@@ -0,0 +1,468 @@
+/*
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.logcat.service
+
+import com.android.ddmlib.Log.LogLevel
+import com.android.ddmlib.Log.LogLevel.DEBUG
+import com.android.ddmlib.Log.LogLevel.INFO
+import com.android.ddmlib.logcat.LogCatHeader
+import com.android.ddmlib.logcat.LogCatHeaderParser
+import com.android.ddmlib.logcat.LogCatMessage
+import com.android.testutils.TestResources
+import com.android.tools.idea.adb.processnamemonitor.ProcessNameMonitor
+import com.android.tools.idea.adb.processnamemonitor.testing.FakeProcessNameMonitor
+import com.android.tools.idea.logcat.SYSTEM_HEADER
+import com.google.common.truth.Truth.assertThat
+import com.intellij.testFramework.ProjectRule
+import com.intellij.testFramework.RuleChain
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.Channel.Factory.UNLIMITED
+import kotlinx.coroutines.channels.SendChannel
+import kotlinx.coroutines.channels.toList
+import kotlinx.coroutines.test.TestCoroutineScope
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import java.io.BufferedReader
+import java.io.File
+import java.io.InputStreamReader
+import java.nio.CharBuffer
+import java.time.Instant
+import java.time.ZoneId
+import java.util.concurrent.TimeUnit.MILLISECONDS
+import kotlin.text.Charsets.UTF_8
+
+/**
+ * Tests for [LogcatMessageAssembler]
+ */
+@Suppress("OPT_IN_USAGE") // runBlockingTest is experimental
+class LogcatMessageAssemblerTest {
+  private val projectRule = ProjectRule()
+
+  @get:Rule
+  val rule = RuleChain(projectRule)
+
+  private val processNameMonitor = FakeProcessNameMonitor()
+
+  private val channel = Channel<List<LogCatMessage>>(UNLIMITED)
+
+  @Before
+  fun setUp() {
+    processNameMonitor.addProcessName("device1", 1, "app-1.1", "process-1.1")
+    processNameMonitor.addProcessName("device1", 2, "app-1.2", "process-1.2")
+    processNameMonitor.addProcessName("device2", 1, "app-2.1", "process-2.1")
+    processNameMonitor.addProcessName("device2", 2, "app-2.2", "process-2.2")
+  }
+
+  @Test
+  fun singleCompleteLogMessage() = runBlockingTest {
+    val assembler = logcatMessageAssembler("device1", channel)
+
+    assembler.processNewLines(
+      """
+        [          1619900000.123  1: 2000 D/Tag  ]
+        Message 1
+
+      """
+    )
+
+    advanceUntilIdle()
+    channel.close()
+    assertThat(channel.toList()).containsExactly(listOf(logCatMessage(DEBUG, 1, 2000, "app-1.1", "Tag", 1619900000L, 123L, "Message 1")))
+  }
+
+  @Test
+  fun multipleCompleteLogMessage() = runBlockingTest {
+    val assembler = logcatMessageAssembler("device1", channel)
+
+    assembler.processNewLines(
+      """
+        [          1619900000.101  1: 2000 D/Tag  ]
+        Message 1
+
+        [          1619900000.102  1: 2000 D/Tag  ]
+        Message 2
+
+        [          1619900000.103  1: 2000 D/Tag  ]
+        Message 3
+
+      """
+    )
+
+    advanceUntilIdle()
+    channel.close()
+    assertThat(channel.toList()).containsExactly(
+      listOf(
+        logCatMessage(DEBUG, 1, 2000, "app-1.1", "Tag", 1619900000L, 101L, "Message 1"),
+        logCatMessage(DEBUG, 1, 2000, "app-1.1", "Tag", 1619900000L, 102L, "Message 2"),
+      ),
+      listOf(logCatMessage(DEBUG, 1, 2000, "app-1.1", "Tag", 1619900000L, 103L, "Message 3")),
+    )
+  }
+
+  @Test
+  fun twoBatches_messageSplit() = runBlockingTest {
+    val assembler = logcatMessageAssembler("device2", channel)
+
+    assembler.processNewLines(
+      """
+        [          1619900000.123  2:2000 D/Tag1  ]
+        Message 1
+
+        [          1619900000.123  2:2000 D/Tag2  ]
+        Message 2 Line 1
+      """
+    )
+    assembler.processNewLines(
+      """
+        Message 2 Line 2
+
+        [          1619900000.123  1:2000 D/Tag3  ]
+        Message 3
+
+      """
+    )
+
+    advanceUntilIdle()
+    channel.close()
+    assertThat(channel.toList()).containsExactly(
+      listOf(logCatMessage(DEBUG, 2, 2000, "app-2.2", "Tag1", 1619900000L, 123L, "Message 1")),
+      listOf(logCatMessage(DEBUG, 2, 2000, "app-2.2", "Tag2", 1619900000L, 123L, "Message 2 Line 1\nMessage 2 Line 2")),
+      listOf(logCatMessage(DEBUG, 1, 2000, "app-2.1", "Tag3", 1619900000L, 123L, "Message 3")),
+    )
+  }
+
+  @Test
+  fun twoBatches_messageNotSplit() = runBlockingTest {
+    val assembler = logcatMessageAssembler("device2", channel)
+
+    assembler.processNewLines(
+      """
+        [          1619900000.123  2:2000 D/Tag1  ]
+        Message 1
+
+        [          1619900000.123  2:2000 D/Tag2  ]
+        Message 2
+        
+      """
+    )
+    assembler.processNewLines(
+      """
+        [          1619900000.123  1:2000 D/Tag3  ]
+        Message 3
+
+        [          1619900000.123  1:2000 D/Tag4  ]
+        Message 4
+
+      """
+    )
+
+    advanceUntilIdle()
+    channel.close()
+    assertThat(channel.toList()).containsExactly(
+      listOf(logCatMessage(DEBUG, 2, 2000, "app-2.2", "Tag1", 1619900000L, 123L, "Message 1")),
+      listOf(
+        logCatMessage(DEBUG, 2, 2000, "app-2.2", "Tag2", 1619900000L, 123L, "Message 2"),
+        logCatMessage(DEBUG, 1, 2000, "app-2.1", "Tag3", 1619900000L, 123L, "Message 3"),
+      ),
+      listOf(logCatMessage(DEBUG, 1, 2000, "app-2.1", "Tag4", 1619900000L, 123L, "Message 4")),
+    )
+  }
+
+  @Test
+  fun twoBatchesSplitOnUserEmittedEmptyLine() = runBlockingTest {
+    val assembler = logcatMessageAssembler("device1", channel)
+
+    assembler.processNewLines(
+      """
+        [          1619900000.123  1:2000 D/Tag1  ]
+        Message 1
+
+        [          1619900000.123  1:2000 D/Tag2  ]
+        Message 2 Line 1
+
+      """
+    )
+    assembler.processNewLines(
+      """
+        Message 2 Line 3
+
+        [          1619900000.123  1:2000 D/Tag3  ]
+        Message 3
+
+      """
+    )
+
+    advanceUntilIdle()
+    channel.close()
+    assertThat(channel.toList()).containsExactly(
+      listOf(logCatMessage(DEBUG, 1, 2000, "app-1.1", "Tag1", 1619900000L, 123L, "Message 1")),
+      listOf(logCatMessage(DEBUG, 1, 2000, "app-1.1", "Tag2", 1619900000L, 123L, "Message 2 Line 1\n\nMessage 2 Line 3")),
+      listOf(logCatMessage(DEBUG, 1, 2000, "app-1.1", "Tag3", 1619900000L, 123L, "Message 3")),
+    )
+  }
+
+  @Test
+  fun messageSplitAcrossThreeBatches() = runBlockingTest {
+    val assembler = logcatMessageAssembler("device1", channel)
+
+    assembler.processNewLines(
+      """
+        [          1619900000.123  1:2000 I/Tag1  ]
+        Message 1 Line 1
+      """
+    )
+    assembler.processNewLines(
+      """
+        Message 1 Line 2
+      """
+    )
+
+    assembler.processNewLines(
+      """
+        Message 1 Line 3
+
+        [          1619900000.123  1:2000 I/Tag2  ]
+        Message 2
+
+      """
+    )
+
+    advanceUntilIdle()
+    channel.close()
+    assertThat(channel.toList()).containsExactly(
+      listOf(logCatMessage(INFO, 1, 2000, "app-1.1", "Tag1", 1619900000L, 123L, "Message 1 Line 1\nMessage 1 Line 2\nMessage 1 Line 3")),
+      listOf(logCatMessage(INFO, 1, 2000, "app-1.1", "Tag2", 1619900000L, 123L, "Message 2")),
+    )
+  }
+
+  @Test
+  fun systemLines() = runBlockingTest {
+    val assembler = logcatMessageAssembler("device1", channel)
+
+    assembler.processNewLines(
+      """
+        --------- beginning of crash
+        [          1619900001.123  1:1000 I/Tag1  ]
+        Message 1
+        
+        --------- beginning of system
+        [          1619900001.123  1:1000 I/Tag2  ]
+        Message 2
+        
+      """
+    )
+
+    advanceUntilIdle()
+    channel.close()
+    assertThat(channel.toList()).containsExactly(
+      listOf(
+        LogCatMessage(SYSTEM_HEADER, "--------- beginning of crash"),
+        LogCatMessage(SYSTEM_HEADER, "--------- beginning of system"),
+        logCatMessage(INFO, 1, 1000, "app-1.1", "Tag1", 1619900001L, 123L, "Message 1"),
+      ),
+      listOf(logCatMessage(INFO, 1, 1000, "app-1.1", "Tag2", 1619900001L, 123L, "Message 2")),
+    )
+  }
+
+  @Test
+  fun linesWithoutHeader_dropped() = runBlockingTest {
+    val assembler = logcatMessageAssembler("device1", channel)
+
+    assembler.processNewLines(
+      """
+        Message 1
+        
+        [          1619900001.123  1:1000 I/Tag2  ]
+        Message 2
+        
+      """
+    )
+
+    advanceUntilIdle()
+    channel.close()
+    assertThat(channel.toList()).containsExactly(
+      listOf(logCatMessage(INFO, 1, 1000, "app-1.1", "Tag2", 1619900001L, 123L, "Message 2")),
+    )
+  }
+
+  /**
+   * This test sends 3 small batches with a small interval between them simulating a running Logcat process that emits data periodically.
+   *
+   * In contrast to the other tests in this file, it asserts the state of the channel after each batch rather than at the end.
+   */
+  @Test
+  fun multipleBatchesWithIntervals() = runBlockingTest {
+    val assembler = logcatMessageAssembler("device1", channel)
+
+    assembler.processNewLines(
+      """
+        [          1619900000.123  1:2000 D/Tag1  ]
+        Message 1
+
+        [          1619900000.123  1:2000 D/Tag2  ]
+        Message 2
+
+      """
+    )
+    assertThat(channel.receive()).containsExactly(logCatMessage(DEBUG, 1, 2000, "app-1.1", "Tag1", 1619900000L, 123L, "Message 1"))
+    assertThat(channel.isEmpty)
+    advanceTimeBy(100)
+    assertThat(channel.receive()).containsExactly(logCatMessage(DEBUG, 1, 2000, "app-1.1", "Tag2", 1619900000L, 123L, "Message 2"))
+
+    assembler.processNewLines(
+      """
+        [          1619900000.123  1:2000 D/Tag3  ]
+        Message 3
+
+        [          1619900000.123  1:2000 D/Tag4  ]
+        Message 4
+
+      """
+    )
+    assertThat(channel.receive()).containsExactly(logCatMessage(DEBUG, 1, 2000, "app-1.1", "Tag3", 1619900000L, 123L, "Message 3"))
+    assertThat(channel.isEmpty)
+    advanceTimeBy(100)
+    assertThat(channel.receive()).containsExactly(logCatMessage(DEBUG, 1, 2000, "app-1.1", "Tag4", 1619900000L, 123L, "Message 4"))
+
+    assembler.processNewLines(
+      """
+        [          1619900000.123  1:2000 D/Tag5  ]
+        Message 5
+
+        [          1619900000.123  1:2000 D/Tag6  ]
+        Message 6
+
+      """
+    )
+    assertThat(channel.receive()).containsExactly(logCatMessage(DEBUG, 1, 2000, "app-1.1", "Tag5", 1619900000L, 123L, "Message 5"))
+    assertThat(channel.isEmpty)
+    advanceTimeBy(100)
+    assertThat(channel.receive()).containsExactly(logCatMessage(DEBUG, 1, 2000, "app-1.1", "Tag6", 1619900000L, 123L, "Message 6"))
+    channel.close()
+    advanceUntilIdle()
+    assertThat(channel.isEmpty)
+  }
+
+  @Test
+  fun realLogcat_oneBatch() = runBlockingTest {
+    val assembler = logcatMessageAssembler("device1", channel)
+
+    assembler.processNewLines(TestResources.getFile("/logcatFiles/real-logcat-from-device.txt").readLines())
+
+    advanceUntilIdle()
+    channel.close()
+    val actualLines = channel.toList().flatten().joinToString("\n") { it.toString() }.split('\n')
+    val expectedLines = TestResources.getFile("/logcatFiles/real-logcat-from-device-expected.txt").readLines()
+    assertThat(actualLines).hasSize(expectedLines.size)
+    actualLines.zip(expectedLines).forEachIndexed { index, (actual, expected) ->
+      assertThat(actual).named("Line $index").isEqualTo(expected)
+    }
+  }
+
+  @Test
+  fun realLogcat_smallBatches() = runBlockingTest {
+    val assembler = logcatMessageAssembler("device1", channel)
+
+    TestResources.getFile("/logcatFiles/real-logcat-from-device.txt").readLinesInBatches(50).forEach {
+      assembler.processNewLines(it)
+    }
+
+    advanceUntilIdle()
+    channel.close()
+    val actualLines = channel.toList().flatten().joinToString("\n") { it.toString() }.split('\n')
+    val expectedLines = TestResources.getFile("/logcatFiles/real-logcat-from-device-expected.txt").readLines()
+    assertThat(actualLines).hasSize(expectedLines.size)
+    actualLines.zip(expectedLines).forEachIndexed { index, (actual, expected) ->
+      assertThat(actual).named("Line $index").isEqualTo(expected)
+    }
+  }
+
+  @Test
+  fun realLogcat_largeBatches() = runBlockingTest {
+    val assembler = logcatMessageAssembler("device1", channel)
+
+    TestResources.getFile("/logcatFiles/real-logcat-from-device.txt").readLinesInBatches(8192).forEach {
+      assembler.processNewLines(it)
+    }
+
+    advanceUntilIdle()
+    channel.close()
+    val actualLines = channel.toList().flatten().joinToString("\n") { it.toString() }.split('\n')
+    val expectedLines = TestResources.getFile("/logcatFiles/real-logcat-from-device-expected.txt").readLines()
+    assertThat(actualLines).hasSize(expectedLines.size)
+    actualLines.zip(expectedLines).forEachIndexed { index, (actual, expected) ->
+      assertThat(actual).named("Line $index").isEqualTo(expected)
+    }
+  }
+
+  private fun TestCoroutineScope.logcatMessageAssembler(
+    serialNumber: String,
+    channel: SendChannel<List<LogCatMessage>>,
+    processNameMonitor: ProcessNameMonitor = this@LogcatMessageAssemblerTest.processNameMonitor,
+  ) =
+    LogcatMessageAssembler(
+      projectRule.project,
+      serialNumber,
+      channel,
+      LogCatHeaderParser(2022, ZoneId.of("America/Los_Angeles")),
+      processNameMonitor,
+      coroutineContext)
+}
+
+private fun logCatMessage(level: LogLevel, pid: Int, tid: Int, appId: String, tag: String, seconds: Long, millis: Long, message: String) =
+  LogCatMessage(LogCatHeader(level, pid, tid, appId, tag, Instant.ofEpochSecond(seconds, MILLISECONDS.toNanos(millis))), message)
+
+private suspend fun LogcatMessageAssembler.processNewLines(lines: String) = processNewLines(lines.replaceIndent().split("\n").toList())
+
+/**
+ * Reads lines from a file in batches.
+ *
+ * Inspired by com.android.adblib.utils.MultiLineShellCollector
+ */
+private fun File.readLinesInBatches(bufferSize: Int): List<List<String>> {
+  val stream = BufferedReader(InputStreamReader(inputStream(), UTF_8))
+  val batches = mutableListOf<List<String>>()
+  val previousString = StringBuilder()
+
+  while (true) {
+    val charBuffer = CharBuffer.allocate(bufferSize)
+    val len = stream.read(charBuffer)
+    if (len < 0) {
+      break
+    }
+    charBuffer.rewind()
+    val lines = mutableListOf<String>()
+    var currentOffset = 0
+    while (currentOffset < len) {
+      val index = charBuffer.indexOf("\n", currentOffset)
+      if (index < 0) {
+        previousString.append(charBuffer.substring(currentOffset))
+        break
+      }
+      previousString.append(charBuffer.substring(currentOffset, index))
+      lines.add(previousString.toString())
+      previousString.clear()
+      currentOffset = index + 1
+    }
+    batches.add(lines)
+  }
+  if (previousString.isNotEmpty()) {
+    batches.add(listOf(previousString.toString()))
+  }
+  return batches
+}
