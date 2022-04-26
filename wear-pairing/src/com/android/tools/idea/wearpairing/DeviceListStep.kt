7866d3dba1104e181b400cc8c5ed6445f4308a88
Jose Parente <parentej@google.com>
2022-04-26 13:45:16 +0000

    [Wear Pairing] Add unavailable devices header
    Fixes: 225775427
    Test: Visual inspection in light/dark mode
    Change-Id: I3c4e9ef091fd898b7fd082200dd248309c4dc9f7
    
    GitOrigin-RevId: b8abbabf43518e9c82a6ef07371959dbb473a88b

--- wear-pairing/src/com/android/tools/idea/wearpairing/DeviceListStep.kt
+++ wear-pairing/src/com/android/tools/idea/wearpairing/DeviceListStep.kt
@@ -194,7 +194,22 @@ class DeviceListStep(model: WearDevicePairingModel, private val project: Project
 
         JPanel().apply {
           layout = GridBagLayout()
-          if (index > 0 && value.isDisabled() && !model.getElementAt(index - 1).isDisabled()) {
+          if (value.isDisabled() && (index == 0 || !model.getElementAt(index - 1).isDisabled())) {
+            if (index == 0) {
+              add(
+                JBLabel("No compatible devices are available to pair.", SwingConstants.CENTER).apply {
+                  isOpaque = true
+                  foreground = UIUtil.getLabelDisabledForeground()
+                  background = UIUtil.getListBackground()
+                  border = empty(32, 16)
+                },
+                GridBagConstraints().apply {
+                  gridwidth = GridBagConstraints.REMAINDER
+                  fill = GridBagConstraints.HORIZONTAL
+                  gridy = 0
+                }
+              )
+            }
             add(
               JBLabel("Unavailable devices").apply {
                 isOpaque = true
@@ -204,6 +219,7 @@ class DeviceListStep(model: WearDevicePairingModel, private val project: Project
               GridBagConstraints().apply {
                 gridwidth = GridBagConstraints.REMAINDER
                 fill = GridBagConstraints.HORIZONTAL
+                gridy = 1
               }
             )
           }
@@ -213,7 +229,7 @@ class DeviceListStep(model: WearDevicePairingModel, private val project: Project
             },
             GridBagConstraints().apply {
               gridx = 0
-              gridy = 1
+              gridy = 2
             }
           )
           add(
@@ -243,7 +259,7 @@ class DeviceListStep(model: WearDevicePairingModel, private val project: Project
               fill = GridBagConstraints.HORIZONTAL
               weightx = 1.0
               gridx = 1
-              gridy = 1
+              gridy = 2
             }
           )
 
@@ -259,7 +275,7 @@ class DeviceListStep(model: WearDevicePairingModel, private val project: Project
               },
               GridBagConstraints().apply {
                 gridx = 2
-                gridy = 1
+                gridy = 2
               }
             )
           }
