ba714090940e476b422c17edca6cafe5aa6fae06
Fedor Kudasov <kudasov@google.com>
2022-04-29 08:02:49 +0000

    Change SurfaceInteractable implementation from static to dynamic delegation
    Bug: 194482298
    Test: all tests should pass
    Change-Id: Ibe95388b04efdc6f4473f9325b92f75d7ade1441
    
    GitOrigin-RevId: 684106ce8f6b5703ec5b941181908a6d66cf7420

--- designer/src/com/android/tools/idea/common/surface/Interactable.kt
+++ designer/src/com/android/tools/idea/common/surface/Interactable.kt
@@ -31,9 +31,9 @@ interface Interactable {
 }
 
 class SurfaceInteractable(private val surface: DesignSurface): Interactable {
-  override val pannable: Pannable = surface.getData(PANNABLE_KEY.name) as? Pannable ?: surface
-  override val interactionPane: JComponent = surface.interactionPane
-  override val cursorReceiver: Component? = DataManager.getInstance().getDataContext(surface).getData(CURSOR_RECEIVER)
+  override val pannable: Pannable get() = surface.getData(PANNABLE_KEY.name) as? Pannable ?: surface
+  override val interactionPane: JComponent get() = surface.interactionPane
+  override val cursorReceiver: Component? get() = DataManager.getInstance().getDataContext(surface).getData(CURSOR_RECEIVER)
   override fun repaintComponent() {
     surface.repaint()
   }
