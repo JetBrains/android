f826f62a2e75188f6f3677de3a2f3896270bd8da
Christophe Rhodes <xof@google.com>
2022-05-23 17:06:01 +0100

    [Dsl,.toml] Start testing the Toml writer
    For now, get the testing infrastructure working and start looking at
    basic data structures.  The specific case of the
    GradleDslVersionCatalog special handling of `version.ref` as
    references will need more thought.
    
    Bug: 184090375
    Bug: 200280395
    Bug: 219640783
    Test: added
    Change-Id: Ic3cc706666482fa653df1434f31b479abd1f7150
    
    GitOrigin-RevId: dc3314802c3cc00b66de2049b1e05943b3ec1324

---
+++ gradle-dsl/testSrc/com/android/tools/idea/gradle/dsl/parser/toml/TomlDslWriterTest.kt
@@ -0,0 +1,83 @@
+/*
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.gradle.dsl.parser.toml
+
+import com.android.testutils.MockitoKt
+import com.android.tools.idea.flags.StudioFlags
+import com.android.tools.idea.gradle.dsl.model.BuildModelContext
+import com.android.tools.idea.gradle.dsl.parser.elements.GradlePropertiesDslElement
+import com.android.tools.idea.gradle.dsl.parser.files.GradleDslFile
+import com.intellij.openapi.application.runWriteAction
+import com.intellij.openapi.command.WriteCommandAction.runWriteCommandAction
+import com.intellij.openapi.vfs.VfsUtil
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.testFramework.PlatformTestCase
+
+class TomlDslWriterTest : PlatformTestCase() {
+  override fun setUp() {
+    super.setUp()
+    StudioFlags.GRADLE_DSL_TOML_SUPPORT.override(true)
+    StudioFlags.GRADLE_DSL_TOML_WRITE_SUPPORT.override(true)
+  }
+
+  override fun tearDown() {
+    StudioFlags.GRADLE_DSL_TOML_WRITE_SUPPORT.clearOverride()
+    StudioFlags.GRADLE_DSL_TOML_SUPPORT.clearOverride()
+    super.tearDown()
+  }
+
+  fun testSingleLiteral() {
+    val contents = mapOf("foo" to "bar")
+    val expected = """
+      foo = "bar"
+    """.trimIndent()
+
+    doTest(contents, expected)
+  }
+
+  private fun doTest(contents: Map<String,Any>, expected: String) {
+    val libsTomlFile = writeLibsTomlFile("")
+    val dslFile = object : GradleDslFile(libsTomlFile, project, ":", BuildModelContext.create(project, MockitoKt.mock())) {}
+    dslFile.parse()
+    mapToProperties(contents, dslFile)
+    runWriteCommandAction(project) {
+      dslFile.applyChanges()
+      dslFile.saveAllChanges()
+    }
+    val text = VfsUtil.loadText(libsTomlFile).replace("\r", "")
+    assertEquals(expected, text)
+  }
+
+  private fun writeLibsTomlFile(text: String): VirtualFile {
+    lateinit var libsTomlFile: VirtualFile
+    runWriteAction {
+      val baseDir = getOrCreateProjectBaseDir()
+      val gradlePath = VfsUtil.createDirectoryIfMissing(baseDir, "gradle")
+      libsTomlFile = gradlePath.createChildData(this, "libs.versions.toml")
+      VfsUtil.saveText(libsTomlFile, text)
+    }
+    return libsTomlFile
+  }
+
+  private fun mapToProperties(map: Map<String,Any>, dslFile: GradleDslFile) {
+    fun populate(key: String, value: Any, element: GradlePropertiesDslElement) {
+      when (value) {
+        is String -> element.setNewLiteral(key, value)
+      }
+    }
+    map.forEach { (k, v) -> populate(k, v, dslFile) }
+  }
+}
