83884cb7b0a9c520ffaa16aa15705b576d6c5755
Kurt Dresner <kdresner@google.com>
2022-05-26 14:04:28 -0700

    Pre-move AddKeyAction in preparation for refactor to be a PanelAction.
    Bug: N/A
    Test: N/A
    Change-Id: I049ba9f246c0c0bd77f02c503a75dcd3ffb17167
    
    GitOrigin-RevId: 6401bc836efa6d4645d3264fb7de2707bb240059

--- android/src/com/android/tools/idea/editors/strings/StringResourceViewPanel.java
+++ android/src/com/android/tools/idea/editors/strings/StringResourceViewPanel.java
@@ -17,6 +17,7 @@ package com.android.tools.idea.editors.strings;
 
 import com.android.ide.common.resources.Locale;
 import com.android.tools.idea.actions.BrowserHelpAction;
+import com.android.tools.idea.editors.strings.action.AddKeyAction;
 import com.android.tools.idea.editors.strings.action.RemoveKeysAction;
 import com.android.tools.idea.editors.strings.model.StringResourceKey;
 import com.android.tools.idea.editors.strings.table.FrozenColumnTableEvent;
