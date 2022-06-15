904d99e84021513bc35f290c27efccb4e5913258
Christophe Rhodes <xof@google.com>
2022-06-15 13:22:55 +0100

    [AUA] Smoke test for AgpUpgradeAction
    Writing this test reveals the perhaps overstrong requirement that the
    project contain an app module for the ContentManager to work
    correctly; this comes from the use of AndroidPluginInfo.find()
    extension function, which not only finds the plugin version but
    specifically the plugin version associated with "the" application
    module.
    
    Bug: n/a
    Test: added
    Change-Id: I5406876f3cd9837d066f01e8cad0ee0dd60c8f0c
    
    GitOrigin-RevId: dea5f519e2a0f7e51e9f6a498ca573eb8213a254

---
+++ project-system-gradle-upgrade/testSrc/com/android/tools/idea/gradle/project/upgrade/AgpUpgradeActionTest.kt
@@ -0,0 +1,65 @@
+/*
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.gradle.project.upgrade
+
+import com.android.testutils.MockitoKt.whenever
+import com.android.tools.idea.testing.AndroidProjectRule
+import com.android.tools.idea.testing.onEdt
+import com.google.common.truth.Truth.assertThat
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.DataContext
+import com.intellij.openapi.wm.ToolWindowManager
+import com.intellij.testFramework.PlatformTestUtil
+import com.intellij.testFramework.RunsInEdt
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.Mockito.mock
+
+@RunsInEdt
+class AgpUpgradeActionTest {
+
+  @get:Rule
+  val projectRule = AndroidProjectRule.withSdk().onEdt()
+
+  val project by lazy { projectRule.project }
+
+  @Test
+  fun testAgpUpgradeAction() {
+    projectRule.fixture.addFileToProject("build.gradle", """
+      buildscript {
+        dependencies {
+          classpath 'com.android.tools.build:gradle:4.2.0'
+        }
+      }
+    """.trimIndent())
+    projectRule.fixture.addFileToProject("settings.gradle", "include ':app'")
+    projectRule.fixture.addFileToProject("app/build.gradle", """
+      plugins {
+        id 'com.android.application'
+      }
+    """.trimIndent())
+    val event = mock(AnActionEvent::class.java)
+    val dataContext = mock(DataContext::class.java)
+    whenever(event.project).thenReturn(project)
+    whenever(event.dataContext).thenReturn(dataContext)
+    AgpUpgradeAction().actionPerformed(event)
+    fun ready(): Boolean {
+      return ToolWindowManager.getInstance(project).getToolWindow("Upgrade Assistant")?.contentManager?.contents?.isNotEmpty() ?: false
+    }
+    PlatformTestUtil.waitWithEventsDispatching("Upgrade Assistant content not provided", ::ready, 5)
+    assertThat(ToolWindowManager.getInstance(project).getToolWindow("Upgrade Assistant")!!.contentManager.contents).hasLength(1)
+  }
+}
