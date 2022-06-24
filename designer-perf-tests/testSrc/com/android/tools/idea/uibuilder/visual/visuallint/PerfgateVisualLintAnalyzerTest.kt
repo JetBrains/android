4693a81a17e5d986f98c5a7382bfd476164d8066
Jerome Gaillard <jgaillard@google.com>
2022-06-24 18:49:57 +0100

    Add Perfgate tests for Visual Linting
    Bug: 232544714
    Test: Perfgate tests
    Change-Id: I83ef8113d069726797672d52b3f74f015f92d08d
    
    GitOrigin-RevId: 4ed83e8d6a725bf51df45b82a77ef84c0f509bb6

---
+++ designer-perf-tests/testSrc/com/android/tools/idea/uibuilder/visual/visuallint/PerfgateVisualLintAnalyzerTest.kt
@@ -0,0 +1,161 @@
+/*
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.uibuilder.visual.visuallint
+
+import com.android.testutils.TestUtils
+import com.android.tools.idea.AndroidPsiUtils
+import com.android.tools.idea.common.SyncNlModel
+import com.android.tools.idea.common.model.NlModel
+import com.android.tools.idea.rendering.ElapsedTimeMeasurement
+import com.android.tools.idea.rendering.MemoryUseMeasurement
+import com.android.tools.idea.rendering.RenderResult
+import com.android.tools.idea.rendering.RenderTask
+import com.android.tools.idea.rendering.RenderTestUtil
+import com.android.tools.idea.rendering.measureOperation
+import com.android.tools.idea.testing.AndroidGradleProjectRule
+import com.android.tools.idea.uibuilder.model.NlComponentRegistrar
+import com.android.tools.idea.uibuilder.visual.visuallint.analyzers.BottomAppBarAnalyzer
+import com.android.tools.idea.uibuilder.visual.visuallint.analyzers.BottomAppBarAnalyzerInspection
+import com.android.tools.idea.uibuilder.visual.visuallint.analyzers.BottomNavAnalyzer
+import com.android.tools.idea.uibuilder.visual.visuallint.analyzers.BottomNavAnalyzerInspection
+import com.android.tools.idea.uibuilder.visual.visuallint.analyzers.BoundsAnalyzer
+import com.android.tools.idea.uibuilder.visual.visuallint.analyzers.BoundsAnalyzerInspection
+import com.android.tools.idea.uibuilder.visual.visuallint.analyzers.ButtonSizeAnalyzer
+import com.android.tools.idea.uibuilder.visual.visuallint.analyzers.ButtonSizeAnalyzerInspection
+import com.android.tools.idea.uibuilder.visual.visuallint.analyzers.LocaleAnalyzer
+import com.android.tools.idea.uibuilder.visual.visuallint.analyzers.LongTextAnalyzer
+import com.android.tools.idea.uibuilder.visual.visuallint.analyzers.LongTextAnalyzerInspection
+import com.android.tools.idea.uibuilder.visual.visuallint.analyzers.OverlapAnalyzer
+import com.android.tools.idea.uibuilder.visual.visuallint.analyzers.OverlapAnalyzerInspection
+import com.android.tools.idea.uibuilder.visual.visuallint.analyzers.TextFieldSizeAnalyzer
+import com.android.tools.idea.uibuilder.visual.visuallint.analyzers.TextFieldSizeAnalyzerInspection
+import com.android.tools.idea.uibuilder.visual.visuallint.analyzers.WearMarginAnalyzerInspection
+import com.android.tools.perflogger.Metric
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.psi.xml.XmlFile
+import org.jetbrains.android.facet.AndroidFacet
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+class PerfgateVisualLintAnalyzerTest {
+
+  @get:Rule
+  val projectRule = AndroidGradleProjectRule()
+
+  @Before
+  fun setup() {
+    projectRule.fixture.testDataPath = TestUtils.resolveWorkspacePath("tools/adt/idea/designer-perf-tests/testData").toString()
+    RenderTestUtil.beforeRenderTestCase()
+    val visualLintInspections = arrayOf(BoundsAnalyzerInspection, BottomNavAnalyzerInspection, BottomAppBarAnalyzerInspection,
+                                        TextFieldSizeAnalyzerInspection, OverlapAnalyzerInspection, LongTextAnalyzerInspection,
+                                        ButtonSizeAnalyzerInspection, WearMarginAnalyzerInspection)
+    projectRule.fixture.enableInspections(*visualLintInspections)
+  }
+
+  @After
+  fun tearDown() {
+    ApplicationManager.getApplication().invokeAndWait {
+      RenderTestUtil.afterRenderTestCase()
+    }
+  }
+
+  @Test
+  fun bottomAppBarAnalyzerRun() {
+    visualLintAnalyzerRun(BottomAppBarAnalyzer)
+  }
+
+  @Test
+  fun bottomNavAnalyzerRun() {
+    visualLintAnalyzerRun(BottomNavAnalyzer)
+  }
+
+  @Test
+  fun boundsAnalyzerRun() {
+    visualLintAnalyzerRun(BoundsAnalyzer)
+  }
+
+  @Test
+  fun buttonSizeAnalyzerRun() {
+    visualLintAnalyzerRun(ButtonSizeAnalyzer)
+  }
+
+  @Test
+  fun localeAnalyzerRun() {
+    visualLintAnalyzerRun(LocaleAnalyzer(VisualLintBaseConfigIssues()))
+  }
+
+  @Test
+  fun longTextAnalyzerRun() {
+    visualLintAnalyzerRun(LongTextAnalyzer)
+  }
+
+  @Test
+  fun overlapAnalyzerRun() {
+    visualLintAnalyzerRun(OverlapAnalyzer)
+  }
+
+  @Test
+  fun textFieldSizeAnalyzerRun() {
+    visualLintAnalyzerRun(TextFieldSizeAnalyzer)
+  }
+
+  private fun visualLintAnalyzerRun(analyzer: VisualLintAnalyzer) {
+    projectRule.load("projects/visualLintApplication")
+    projectRule.requestSyncAndWait()
+
+    val module = projectRule.getModule("app")
+    val facet = AndroidFacet.getInstance(module)!!
+    val activityLayout = projectRule.project.baseDir.findFileByRelativePath("app/src/main/res/layout/activity_main.xml")!!
+    val dashboardLayout = projectRule.project.baseDir.findFileByRelativePath("app/src/main/res/layout/fragment_dashboard.xml")!!
+    val notificationsLayout = projectRule.project.baseDir.findFileByRelativePath("app/src/main/res/layout/fragment_notifications.xml")!!
+    val homeLayout = projectRule.project.baseDir.findFileByRelativePath("app/src/main/res/layout/fragment_home.xml")!!
+    val filesToAnalyze = listOf(activityLayout, dashboardLayout, notificationsLayout, homeLayout)
+    val phoneConfiguration = RenderTestUtil.getConfiguration(module, activityLayout, "_device_class_phone")
+    phoneConfiguration.setTheme("Theme.MaterialComponents.DayNight.DarkActionBar")
+    val foldableConfiguration = RenderTestUtil.getConfiguration(module, activityLayout, "_device_class_foldable")
+    foldableConfiguration.setTheme("Theme.MaterialComponents.DayNight.DarkActionBar")
+    val tabletConfiguration = RenderTestUtil.getConfiguration(module, activityLayout, "_device_class_tablet")
+    tabletConfiguration.setTheme("Theme.MaterialComponents.DayNight.DarkActionBar")
+    val desktopConfiguration = RenderTestUtil.getConfiguration(module, activityLayout, "_device_class_desktop")
+    desktopConfiguration.setTheme("Theme.MaterialComponents.DayNight.DarkActionBar")
+    val configurations = listOf(phoneConfiguration, foldableConfiguration, tabletConfiguration, desktopConfiguration)
+
+    val modelResultMap = mutableMapOf<NlModel, RenderResult>()
+    configurations.forEach { configuration ->
+      filesToAnalyze.forEach { file ->
+        val nlModel = SyncNlModel.create(projectRule.project, NlComponentRegistrar, null, null, facet, file, configuration)
+        val psiFile = AndroidPsiUtils.getPsiFileSafely(projectRule.project, file) as XmlFile
+        nlModel.syncWithPsi(AndroidPsiUtils.getRootTagSafely(psiFile)!!, emptyList<NlModel.TagSnapshotTreeNode>())
+        RenderTestUtil.withRenderTask(facet, file, configuration) { task: RenderTask ->
+          task.setDecorations(false)
+          try {
+            val result = task.inflate().get()
+            modelResultMap[nlModel] = result
+          }
+          catch (ex: Exception) {
+            throw RuntimeException(ex)
+          }
+        }
+      }
+    }
+    visualLintingBenchmark.measureOperation(measures = listOf(ElapsedTimeMeasurement(Metric("${analyzer.type}_run_time")),
+                                                              MemoryUseMeasurement(Metric("${analyzer.type}_memory_use")))) {
+      modelResultMap.forEach { (nlModel, renderResult) -> analyzer.findIssues(renderResult, nlModel) }
+    }
+  }
+}
---
+++ designer-perf-tests/testSrc/com/android/tools/idea/uibuilder/visual/visuallint/PerfgateVisualLintTest.kt
@@ -0,0 +1,121 @@
+/*
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.uibuilder.visual.visuallint
+
+import com.android.testutils.TestUtils
+import com.android.tools.idea.common.SyncNlModel
+import com.android.tools.idea.common.error.IssueModel
+import com.android.tools.idea.common.surface.DesignSurface
+import com.android.tools.idea.common.type.DesignerTypeRegistrar
+import com.android.tools.idea.rendering.ElapsedTimeMeasurement
+import com.android.tools.idea.rendering.MemoryUseMeasurement
+import com.android.tools.idea.rendering.NoSecurityManagerRenderService
+import com.android.tools.idea.rendering.RenderService
+import com.android.tools.idea.rendering.RenderTestUtil
+import com.android.tools.idea.rendering.measureOperation
+import com.android.tools.idea.testing.AndroidGradleProjectRule
+import com.android.tools.idea.uibuilder.model.NlComponentRegistrar
+import com.android.tools.idea.uibuilder.type.LayoutFileType
+import com.android.tools.idea.uibuilder.visual.visuallint.analyzers.BottomAppBarAnalyzerInspection
+import com.android.tools.idea.uibuilder.visual.visuallint.analyzers.BottomNavAnalyzerInspection
+import com.android.tools.idea.uibuilder.visual.visuallint.analyzers.BoundsAnalyzerInspection
+import com.android.tools.idea.uibuilder.visual.visuallint.analyzers.ButtonSizeAnalyzerInspection
+import com.android.tools.idea.uibuilder.visual.visuallint.analyzers.LongTextAnalyzerInspection
+import com.android.tools.idea.uibuilder.visual.visuallint.analyzers.OverlapAnalyzerInspection
+import com.android.tools.idea.uibuilder.visual.visuallint.analyzers.TextFieldSizeAnalyzerInspection
+import com.android.tools.idea.uibuilder.visual.visuallint.analyzers.WearMarginAnalyzerInspection
+import com.android.tools.perflogger.Benchmark
+import com.android.tools.perflogger.Metric
+import com.google.common.util.concurrent.MoreExecutors
+import com.intellij.openapi.application.ApplicationManager
+import org.jetbrains.android.facet.AndroidFacet
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.Mockito
+
+private const val NUMBER_OF_SAMPLES = 40
+
+val visualLintingBenchmark = Benchmark.Builder("Visual Linting Benchmark")
+  .setProject("Design Tools")
+  .setDescription("Benchmark for measuring performance of Visual Linting, using $NUMBER_OF_SAMPLES samples.")
+  .build()
+
+class PerfgateVisualLintTest {
+  private lateinit var surface: DesignSurface<*>
+
+  @get:Rule
+  val projectRule = AndroidGradleProjectRule()
+
+  @Before
+  fun setup() {
+    projectRule.fixture.testDataPath = TestUtils.resolveWorkspacePath("tools/adt/idea/designer-perf-tests/testData").toString()
+    RenderTestUtil.beforeRenderTestCase()
+    RenderService.setForTesting(projectRule.project, NoSecurityManagerRenderService(projectRule.project))
+    val issueModel = IssueModel.createForTesting(projectRule.fixture.testRootDisposable, projectRule.project)
+    surface = Mockito.mock(DesignSurface::class.java)
+    Mockito.`when`(surface.issueModel).thenReturn(issueModel)
+    DesignerTypeRegistrar.register(LayoutFileType)
+    val visualLintInspections = arrayOf(
+      BoundsAnalyzerInspection, BottomNavAnalyzerInspection, BottomAppBarAnalyzerInspection,
+      TextFieldSizeAnalyzerInspection, OverlapAnalyzerInspection, LongTextAnalyzerInspection,
+      ButtonSizeAnalyzerInspection, WearMarginAnalyzerInspection
+    )
+    projectRule.fixture.enableInspections(*visualLintInspections)
+  }
+
+  @After
+  fun tearDown() {
+    ApplicationManager.getApplication().invokeAndWait {
+      RenderTestUtil.afterRenderTestCase()
+    }
+    RenderService.setForTesting(projectRule.project, null)
+  }
+
+  @Test
+  fun backgroundLintingTimeForPhone() {
+    projectRule.load("projects/visualLintApplication")
+    projectRule.requestSyncAndWait()
+    val module = projectRule.getModule("app")
+    val facet = AndroidFacet.getInstance(module)!!
+
+    val dashboardLayout = projectRule.project.baseDir.findFileByRelativePath("app/src/main/res/layout/fragment_dashboard.xml")!!
+    val nlModel = SyncNlModel.create(projectRule.project, NlComponentRegistrar, null, null, facet, dashboardLayout)
+    visualLintingBenchmark.measureOperation(measures = listOf(ElapsedTimeMeasurement(Metric("phone_background_linting_time")),
+                                                              MemoryUseMeasurement(Metric("phone_background_linting_memory_use")))) {
+      VisualLintService.getInstance(projectRule.project)
+        .runVisualLintAnalysis(listOf(nlModel), surface, MoreExecutors.newDirectExecutorService())
+    }
+  }
+
+  @Test
+  fun backgroundLintingTimeForWear() {
+    projectRule.load("projects/visualLintApplication")
+    projectRule.requestSyncAndWait()
+    val module = projectRule.getModule("app")
+    val facet = AndroidFacet.getInstance(module)!!
+
+    val wearLayout = projectRule.project.baseDir.findFileByRelativePath("app/src/main/res/layout/wear_layout.xml")!!
+    val wearConfiguration = RenderTestUtil.getConfiguration(module, wearLayout, "wearos_small_round")
+    val wearModel = SyncNlModel.create(projectRule.project, NlComponentRegistrar, null, null, facet, wearLayout, wearConfiguration)
+    visualLintingBenchmark.measureOperation(measures = listOf(ElapsedTimeMeasurement(Metric("wear_background_linting_time")),
+                                                              MemoryUseMeasurement(Metric("wear_background_linting_memory_use")))) {
+      VisualLintService.getInstance(projectRule.project)
+        .runVisualLintAnalysis(listOf(wearModel), surface, MoreExecutors.newDirectExecutorService())
+    }
+  }
+}
