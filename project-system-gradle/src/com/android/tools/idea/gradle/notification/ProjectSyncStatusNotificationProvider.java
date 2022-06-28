e7c31204dc819b1951c38f4ef58b664994e8c4ed
Pavel Porvatov <pavel.porvatov@jetbrains.com>
2022-06-28 13:29:33 +0200

    Use proper color in Android-Studio notifications background.
    It should fix background color when new UI is enabled IDEA-297683 IJ-CR-27719
    
    (cherry picked from commit 357baae3f2411de448761e9fa10c30bf5ad78fc3)
    
    Change-Id: I357baae3f2411de448761e9fa10c30bf5ad78fc3
    
    GitOrigin-RevId: 74ca830e564b09ce7d1b3d76ae28a2350e997853

--- project-system-gradle/src/com/android/tools/idea/gradle/notification/ProjectSyncStatusNotificationProvider.java
+++ project-system-gradle/src/com/android/tools/idea/gradle/notification/ProjectSyncStatusNotificationProvider.java
@@ -56,6 +56,7 @@ import com.intellij.serviceContainer.NonInjectable;
 import com.intellij.ui.EditorNotificationPanel;
 import com.intellij.ui.EditorNotifications;
 import com.intellij.util.ThreeState;
+import com.intellij.util.ui.JBUI;
 import com.intellij.util.ui.UIUtil;
 import java.awt.Color;
 import java.io.File;
@@ -217,7 +218,7 @@ public class ProjectSyncStatusNotificationProvider extends EditorNotifications.P
     @NotNull private final Type type;
 
     NotificationPanel(@NotNull Type type, @NotNull String text) {
-      super(EditorColors.READONLY_BACKGROUND_COLOR);
+      super(JBUI.CurrentTheme.Banner.WARNING_BACKGROUND);
       this.type = type;
       setText(text);
     }
