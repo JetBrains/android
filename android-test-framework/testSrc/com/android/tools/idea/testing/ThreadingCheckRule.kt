8f4b8c0673e8e60e8e91d2574cd55d9f562f0365
Alexander Demurov <ademurov@google.com>
2022-07-06 20:06:24 -0700

    JUnit test rule for verifying thread correctness
    Creates a JUnit test rule that can be used to verify that the methods annotated with threading annotations like @WorkerThread/@UiThread were called on a corresponding thread.
    
    Bug: 228841408
    Test: Unit tests
    Change-Id: I7cb8a13a169063089a49f362b8145ff8fd884f82
    
    GitOrigin-RevId: e56b56efead369996e90d8579b9a61994b9ec7e4

---
+++ android-test-framework/testSrc/com/android/tools/idea/testing/ThreadingCheckRule.kt
@@ -0,0 +1,70 @@
+/*
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.testing
+
+import com.android.annotations.concurrency.WorkerThread
+import com.android.annotations.concurrency.UiThread
+import com.android.tools.instrumentation.threading.agent.callback.ThreadingCheckerHook
+import com.android.tools.instrumentation.threading.agent.callback.ThreadingCheckerTrampoline
+import org.junit.rules.TestRule
+import org.junit.runner.Description
+import org.junit.runners.model.Statement
+import javax.swing.SwingUtilities
+
+/**
+ * ThreadingCheck test rule works in conjunction with a threading checker java agent and will fail
+ * a test if a method annotated with a threading annotation (see [UiThread] and [WorkerThread])
+ * is called on a wrong thread.
+ */
+class ThreadingCheckRule : TestRule {
+  override fun apply(base: Statement, description: Description): Statement {
+    return object : Statement() {
+      override fun evaluate() {
+        val hook = object : ThreadingCheckerHook {
+          var hasPerformedThreadingChecks = false
+          var hasThreadingViolation = false
+
+          override fun verifyOnUiThread() {
+            hasPerformedThreadingChecks = true
+            if (!SwingUtilities.isEventDispatchThread()) {
+              hasThreadingViolation = true
+            }
+          }
+
+          override fun verifyOnWorkerThread() {
+            hasPerformedThreadingChecks = true
+            if (SwingUtilities.isEventDispatchThread()) {
+              hasThreadingViolation = true
+            }
+          }
+        }
+
+        ThreadingCheckerTrampoline.installHook(hook)
+        base.evaluate()
+        ThreadingCheckerTrampoline.removeHook(hook)
+
+        if (!hook.hasPerformedThreadingChecks) {
+          // Either the test code doesn't run any code annotated with threading annotations or
+          // threading java agent is not running.
+          println("No threading checks were performed when running ${description.className}#${description.methodName} test")
+        }
+        if (hook.hasThreadingViolation) {
+          throw RuntimeException("Method called on a wrong thread. See the test output log for details.")
+        }
+      }
+    }
+  }
+}
