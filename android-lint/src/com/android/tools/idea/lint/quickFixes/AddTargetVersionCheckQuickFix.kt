4053524e8857e9ad887511171fc8ed00cfb621cb
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2022-07-12 12:38:18 +0200

    [kotlin] move findExistingEditor util to kotlin.code-insight.utils module
    GitOrigin-RevId: 83c3e221421bc9d79ac56dcd5d8090f9007602dc

--- android-lint/src/com/android/tools/idea/lint/quickFixes/AddTargetVersionCheckQuickFix.kt
+++ android-lint/src/com/android/tools/idea/lint/quickFixes/AddTargetVersionCheckQuickFix.kt
@@ -40,7 +40,7 @@ import org.jetbrains.kotlin.idea.KotlinLanguage
 import org.jetbrains.kotlin.idea.caches.resolve.analyze
 import org.jetbrains.kotlin.idea.codeInsight.surroundWith.statement.KotlinIfSurrounder
 import org.jetbrains.kotlin.idea.core.ShortenReferences
-import org.jetbrains.kotlin.idea.codeinsight.api.classic.inspections.findExistingEditor
+import org.jetbrains.kotlin.idea.codeinsight.utils.findExistingEditor
 import org.jetbrains.kotlin.psi.KtBlockExpression
 import org.jetbrains.kotlin.psi.KtClassInitializer
 import org.jetbrains.kotlin.psi.KtContainerNode
