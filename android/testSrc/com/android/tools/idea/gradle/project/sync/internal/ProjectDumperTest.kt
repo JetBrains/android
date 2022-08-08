e98382251509ba971b802661889fe51253e99c43
Andrei Kuznetsov <andrei.kuznetsov@jetbrains.com>
2022-08-08 17:34:50 +0300

    Android: mask JDK vendor in snapshot comparison tests
    GitOrigin-RevId: d90b1e6d20973abef81e950bef565b7ff9d21784

--- android/testSrc/com/android/tools/idea/gradle/project/sync/internal/ProjectDumperTest.kt
+++ android/testSrc/com/android/tools/idea/gradle/project/sync/internal/ProjectDumperTest.kt
@@ -6,19 +6,40 @@ import org.junit.Test
 import java.io.File
 
 class ProjectDumperTest {
-  private val SAMPLES: Map<String, String> = mutableMapOf(
-    Pair(
-      "<GRADLE>/caches/modules-2/files-2.1/org.jetbrains.kotlin/kotlin-stdlib-common/<KOTLIN_VERSION>/xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx/kotlin-stdlib-common-<KOTLIN_VERSION>.jar",
-      "<M2>/org/jetbrains/kotlin/kotlin-stdlib-common/<KOTLIN_VERSION>/kotlin-stdlib-common-<KOTLIN_VERSION>.jar"
-    )
-  )
-
-
   @Test
   fun testConvertToMavenPath() {
+    val samples: Map<String, String> = mutableMapOf(
+      Pair(
+        "<GRADLE>/caches/modules-2/files-2.1/org.jetbrains.kotlin/kotlin-stdlib-common/<KOTLIN_VERSION>/xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx/kotlin-stdlib-common-<KOTLIN_VERSION>.jar",
+        "<M2>/org/jetbrains/kotlin/kotlin-stdlib-common/<KOTLIN_VERSION>/kotlin-stdlib-common-<KOTLIN_VERSION>.jar"
+      )
+    )
+
     val dumper = ProjectDumper(offlineRepos = emptyList(), androidSdk = File("/nowhere"))
-    for (entry in SAMPLES) {
+    for (entry in samples) {
       assertEquals(dumper.convertToMaskedMavenPath(entry.key), entry.value)
+    }
+  }
+
+  @Test
+  fun testJavaVersionMask() {
+    val samples = listOf("jbr-11", "corretto-11")
+    fun ProjectDumper.test(src: String) = src.replaceJavaVersion()
+
+    val dumper = ProjectDumper(offlineRepos = emptyList(), androidSdk = File("/nowhere"))
+    for (sample in samples) {
+      assertEquals("<JAVA_VERSION>", dumper.test(sample))
+    }
+  }
+
+  @Test
+  fun testJDKVersionMask() {
+    val samples = listOf("JetBrains Runtime version 11.0.8", "Amazon Corretto version 11.0.8")
+    fun ProjectDumper.test(src: String) = src.replaceJdkVersion()
+
+    val dumper = ProjectDumper(offlineRepos = emptyList(), androidSdk = File("/nowhere"))
+    for (sample in samples) {
+      assertEquals("<JDK_VERSION>", dumper.test(sample))
     }
   }
 }
