e6e1da5bc4958304c2828071ba6b75ec841a857f
James Rose <rosej@google.com>
2022-08-10 13:39:53 -0700

    Add local emulator plugin to Studio
    This is glue code that instantiates the LocalEmulatorProvisionerPlugin
    within Studio by injecting the AvdManagerConnection.
    
    Bug: 241591222
    Test: none
    Change-Id: If17c36aa639ae0600069e64b7b4e815d74f014e9
    
    GitOrigin-RevId: 0b3e39cf263e38710dd118fbc6d09e3280b786bc

---
+++ android/src/com/android/tools/idea/avdmanager/LocalEmulatorProvisionerFactory.kt
@@ -0,0 +1,59 @@
+/*
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.avdmanager
+
+import com.android.sdklib.deviceprovisioner.LocalEmulatorProvisionerPlugin
+import com.android.sdklib.internal.avd.AvdInfo
+import com.android.tools.idea.adblib.AdbLibService
+import com.android.tools.idea.concurrency.AndroidDispatchers
+import com.android.tools.idea.concurrency.AndroidDispatchers.uiThread
+import com.android.tools.idea.concurrency.AndroidDispatchers.workerThread
+import com.android.tools.idea.deviceprovisioner.DeviceProvisionerFactory
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.Disposer
+import kotlinx.coroutines.withContext
+
+/** Builds a LocalEmulatorProvisionerPlugin with its dependencies provided by Studio. */
+class LocalEmulatorProvisionerFactory : DeviceProvisionerFactory {
+  override fun create(project: Project): LocalEmulatorProvisionerPlugin {
+    val avdManagerConnection = AvdManagerConnection.getDefaultAvdManagerConnection()
+    val avdManager =
+      object : LocalEmulatorProvisionerPlugin.AvdManager {
+        override suspend fun rescanAvds() =
+          withContext(AndroidDispatchers.diskIoThread) { avdManagerConnection.getAvds(true) }
+
+        override suspend fun createAvd(): Boolean =
+          withContext(uiThread) {
+            AvdWizardUtils.createAvdWizard(null, project, AvdOptionsModel(null)).showAndGet()
+          }
+
+        override suspend fun editAvd(avdInfo: AvdInfo): Boolean =
+          withContext(uiThread) {
+            AvdWizardUtils.createAvdWizard(null, project, avdInfo).showAndGet()
+          }
+
+        override suspend fun startAvd(avdInfo: AvdInfo) {
+          withContext(workerThread) { avdManagerConnection.startAvd(project, avdInfo) }
+        }
+
+        override suspend fun stopAvd(avdInfo: AvdInfo) {
+          withContext(workerThread) { avdManagerConnection.stopAvd(avdInfo) }
+        }
+      }
+
+    return LocalEmulatorProvisionerPlugin(AdbLibService.getSession(project), avdManager)
+  }
+}
