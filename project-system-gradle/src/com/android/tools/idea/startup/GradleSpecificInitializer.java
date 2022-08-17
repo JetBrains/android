41887dc7b69057679c9f0b1aee0e432be8705e64
Matthew Gharrity <gharrma@google.com>
2022-08-17 16:12:46 -0700

    Move action customization to separate class
    Action customization code should not be mixed with general
    application initialization code---mainly because the time at which
    ActionConfigurationCustomizer runs during application startup is not
    well-defined.
    
    This commit simply moves the ActionManager-specific code to
    a separate class, verbatim. Upcoming commits will add tweaks.
    
    Bug: 200037669
    Bug: might help with b/221488058
    Test: verified that GradleSpecificActionCustomizer runs at startup
    Change-Id: If1c744fe99df5c76ce12d0c7818ea21651dcf02d
    
    GitOrigin-RevId: db80c6da1aa9e1a3152e44500780c029cdf20b2b

--- project-system-gradle/src/com/android/tools/idea/startup/GradleSpecificInitializer.java
+++ project-system-gradle/src/com/android/tools/idea/startup/GradleSpecificInitializer.java
@@ -15,22 +15,12 @@
  */
 package com.android.tools.idea.startup;
 
-import static com.intellij.openapi.actionSystem.Anchor.AFTER;
-import static com.intellij.openapi.actionSystem.IdeActions.ACTION_COMPILE;
-import static com.intellij.openapi.actionSystem.IdeActions.ACTION_COMPILE_PROJECT;
-import static com.intellij.openapi.actionSystem.IdeActions.ACTION_MAKE_MODULE;
 import static org.jetbrains.android.sdk.AndroidSdkUtils.DEFAULT_JDK_NAME;
 import static org.jetbrains.android.sdk.AndroidSdkUtils.createNewAndroidPlatform;
 
 import com.android.sdklib.IAndroidTarget;
 import com.android.tools.adtui.validation.Validator;
 import com.android.tools.idea.IdeInfo;
-import com.android.tools.idea.actions.AndroidActionGroupRemover;
-import com.android.tools.idea.actions.AndroidOpenFileAction;
-import com.android.tools.idea.actions.CreateLibraryFromFilesAction;
-import com.android.tools.idea.flags.StudioFlags;
-import com.android.tools.idea.gradle.actions.AndroidTemplateProjectStructureAction;
-import com.android.tools.idea.gradle.actions.MakeIdeaModuleAction;
 import com.android.tools.idea.io.FilePaths;
 import com.android.tools.idea.projectsystem.gradle.IdeGooglePlaySdkIndex;
 import com.android.tools.idea.sdk.AndroidSdks;
@@ -40,20 +30,13 @@ import com.android.tools.idea.ui.validation.validators.PathValidator;
 import com.android.tools.idea.welcome.config.FirstRunWizardMode;
 import com.android.tools.idea.welcome.wizard.AndroidStudioWelcomeScreenProvider;
 import com.android.tools.lint.checks.GradleDetector;
-import com.android.utils.Pair;
-import com.intellij.ide.projectView.actions.MarkRootGroup;
-import com.intellij.ide.projectView.impl.MoveModuleToGroupTopLevel;
 import com.intellij.notification.Notification;
 import com.intellij.notification.NotificationDisplayType;
 import com.intellij.notification.NotificationGroup;
 import com.intellij.notification.NotificationListener;
 import com.intellij.notification.NotificationType;
 import com.intellij.notification.Notifications;
-import com.intellij.openapi.actionSystem.ActionGroup;
 import com.intellij.openapi.actionSystem.ActionManager;
-import com.intellij.openapi.actionSystem.AnAction;
-import com.intellij.openapi.actionSystem.Constraints;
-import com.intellij.openapi.actionSystem.DefaultActionGroup;
 import com.intellij.openapi.actionSystem.impl.ActionConfigurationCustomizer;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.application.ApplicationNamesInfo;
@@ -66,8 +49,6 @@ import com.intellij.openapi.projectRoots.SdkModificator;
 import com.intellij.openapi.roots.OrderRootType;
 import com.intellij.openapi.vfs.VirtualFile;
 import java.io.File;
-import java.util.ArrayDeque;
-import java.util.Deque;
 import java.util.List;
 import javax.swing.event.HyperlinkEvent;
 import org.jetbrains.android.sdk.AndroidPlatform;
@@ -86,12 +67,6 @@ public class GradleSpecificInitializer implements ActionConfigurationCustomizer 
 
   @Override
   public void customize(@NotNull ActionManager actionManager) {
-    setUpNewProjectActions(actionManager);
-    setUpWelcomeScreenActions(actionManager);
-    disableUnsupportedAction(actionManager);
-    replaceProjectPopupActions(actionManager);
-    setUpMakeActions(actionManager);
-    setUpGradleViewToolbarActions(actionManager);
     checkInstallPath();
 
     if (AndroidSdkUtils.isAndroidSdkManagerEnabled()) {
@@ -114,31 +89,6 @@ public class GradleSpecificInitializer implements ActionConfigurationCustomizer 
     useIdeGooglePlaySdkIndexInGradleDetector();
   }
 
-  private void disableUnsupportedAction(ActionManager actionManager) {
-    actionManager.unregisterAction("LoadUnloadModules"); // private LoadUnloadModulesActionKt.ACTION_ID
-  }
-
-  // The original actions will be visible only on plain IDEA projects.
-  private static void setUpMakeActions(ActionManager actionManager) {
-    // 'Build' > 'Make Project' action
-    Actions.hideAction(actionManager, "CompileDirty");
-
-    // 'Build' > 'Make Modules' action
-    // We cannot simply hide this action, because of a NPE.
-    Actions.replaceAction(actionManager, ACTION_MAKE_MODULE, new MakeIdeaModuleAction());
-
-    // 'Build' > 'Rebuild' action
-    Actions.hideAction(actionManager, ACTION_COMPILE_PROJECT);
-
-    // 'Build' > 'Compile Modules' action
-    Actions.hideAction(actionManager, ACTION_COMPILE);
-
-    // Additional 'Build' action from com.jetbrains.cidr.execution.build.CidrBuildTargetAction
-    Actions.hideAction(actionManager, "Build");
-    Actions.hideAction(actionManager, "Groovy.CheckResources.Rebuild");
-    Actions.hideAction(actionManager, "Groovy.CheckResources.Make");
-  }
-
   /**
    * Gradle has an issue when the studio path contains ! (http://b.android.com/184588)
    */
@@ -150,66 +100,6 @@ public class GradleSpecificInitializer implements ActionConfigurationCustomizer 
       Notification notification = getNotificationGroup().createNotification(message, NotificationType.ERROR);
       notification.setImportant(true);
       Notifications.Bus.notify(notification);
-    }
-  }
-
-  private static void setUpGradleViewToolbarActions(ActionManager actionManager) {
-    Actions.hideAction(actionManager, "ExternalSystem.RefreshAllProjects");
-    Actions.hideAction(actionManager, "ExternalSystem.SelectProjectDataToImport");
-    Actions.hideAction(actionManager, "ExternalSystem.ToggleAutoReload");
-    Actions.hideAction(actionManager, "ExternalSystem.DetachProject");
-    Actions.hideAction(actionManager, "ExternalSystem.AttachProject");
-  }
-
-  private static void setUpNewProjectActions(ActionManager actionManager) {
-    // Unregister IntelliJ's version of the project actions and manually register our own.
-    Actions.replaceAction(actionManager, "OpenFile", new AndroidOpenFileAction());
-    Actions.replaceAction(actionManager, "CreateLibraryFromFile", new CreateLibraryFromFilesAction());
-
-    Actions.hideAction(actionManager, "AddFrameworkSupport");
-    Actions.hideAction(actionManager, "BuildArtifact");
-    Actions.hideAction(actionManager, "RunTargetAction");
-  }
-
-  private static void setUpWelcomeScreenActions(ActionManager actionManager) {
-    // Update the Welcome Screen actions
-    Actions.replaceAction(actionManager, "WelcomeScreen.OpenProject", new AndroidOpenFileAction("Open"));
-    Actions.replaceAction(actionManager, "WelcomeScreen.Configure.ProjectStructure", new AndroidTemplateProjectStructureAction("Default Project Structure..."));
-    Actions.replaceAction(actionManager, "TemplateProjectStructure", new AndroidTemplateProjectStructureAction("Default Project Structure..."));
-
-    Actions.moveAction(actionManager, "WelcomeScreen.ImportProject", "WelcomeScreen.QuickStart.IDEA",
-               "WelcomeScreen.QuickStart", new Constraints(AFTER, "Vcs.VcsClone"));
-
-    AnAction getFromVcsAction = actionManager.getAction("Vcs.VcsClone");
-    if (getFromVcsAction != null) {
-      getFromVcsAction.getTemplatePresentation().setText("Get project from Version Control");
-    }
-  }
-
-  private static void replaceProjectPopupActions(ActionManager actionManager) {
-    Deque<Pair<DefaultActionGroup, AnAction>> stack = new ArrayDeque<>();
-    stack.add(Pair.of(null, actionManager.getAction("ProjectViewPopupMenu")));
-    while (!stack.isEmpty()) {
-      Pair<DefaultActionGroup, AnAction> entry = stack.pop();
-      DefaultActionGroup parent = entry.getFirst();
-      AnAction action = entry.getSecond();
-      if (action instanceof DefaultActionGroup) {
-        DefaultActionGroup actionGroup = (DefaultActionGroup)action;
-        for (AnAction child : actionGroup.getChildActionsOrStubs()) {
-          stack.push(Pair.of(actionGroup, child));
-        }
-      }
-
-      if (action instanceof MoveModuleToGroupTopLevel) {
-        parent.remove(action, actionManager);
-        parent.add(new AndroidActionGroupRemover((ActionGroup)action, "Move Module to Group"),
-                   new Constraints(AFTER, "OpenModuleSettings"), actionManager);
-      }
-      else if (action instanceof MarkRootGroup) {
-        parent.remove(action, actionManager);
-        parent.add(new AndroidActionGroupRemover((ActionGroup)action, "Mark Directory As"),
-                   new Constraints(AFTER, "OpenModuleSettings"), actionManager);
-      }
     }
   }
 
