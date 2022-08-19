9b2ca7c2dcd07fb06211c8d5495224538c124ec0
Sergio Murguia <srmurguia@google.com>
2022-08-19 13:40:53 -0700

    Use ActionMenu, ActionMenuItem in invokeComponent
    ... so tests can call menu items.
    
    Some actions (like AndroidShowStructureSettingsAction) cause modal
    dialogs to appear, causing E2E tests to not be able to use them since
    the test cannot control it once the dialog is shown (the action starts
    and finishes until the dialog is closed). If the
    same action can be called using a menu option, then the test can
    continue since invokeComponent does not wait for modal dialogs.
    
    This change adds ActionMenu and ActionMenuItem handling into
    invokeComponent so test can call actions from the menu.
    
    Because of b/243561571, this change filters out ActionMenu components
    that have height = 0.
    
    Bug: 233789469, 243561571
    Test: E2E test will use this.
    Change-Id: Ic758af05f284a7efacd7d3dee4fbeb56ddeaa54c
    
    GitOrigin-RevId: fc1064b3cbf8c25437e0a875aa3a8dc70a8cc541

--- as-driver/agent/src/com/android/tools/asdriver/inject/StudioInteractionService.java
+++ as-driver/agent/src/com/android/tools/asdriver/inject/StudioInteractionService.java
@@ -25,8 +25,11 @@ import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.DataContext;
 import com.intellij.openapi.actionSystem.impl.ActionButton;
+import com.intellij.openapi.actionSystem.impl.ActionMenu;
+import com.intellij.openapi.actionSystem.impl.ActionMenuItem;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.util.IconLoader;
+import com.intellij.openapi.util.SystemInfo;
 import com.intellij.openapi.wm.IdeFocusManager;
 import com.intellij.ui.LayeredIcon;
 import com.intellij.ui.components.labels.ActionLink;
@@ -197,6 +200,12 @@ public class StudioInteractionService {
     } else if (component instanceof ActionButton) {
       log("Invoking ActionButton: " + component);
       ((ActionButton)component).click();
+    } else if (component instanceof ActionMenu) {
+      log("Invoking ActionMenu: " + component);
+      ((ActionMenu)component).doClick();
+    } else if (component instanceof ActionMenuItem) {
+      log("Invoking ActionMenuItem: " + component);
+      ((ActionMenuItem)component).doClick();
     } else {
       throw new IllegalArgumentException(String.format("Don't know how to invoke a component of class \"%s\"", component.getClass()));
     }
@@ -235,6 +244,11 @@ public class StudioInteractionService {
       } else {
         throw new IllegalArgumentException("ComponentMatcher doesn't have a recognized matcher");
       }
+    }
+
+    // b/243561571: Filter out ActionMenu components that have height 0 on Windows
+    if (SystemInfo.isWindows) {
+      componentsFound.removeIf((c) -> (c instanceof ActionMenu) && (c.getHeight() == 0));
     }
 
     int numComponentsFound = componentsFound.size();
