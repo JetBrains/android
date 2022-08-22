29b7d778667ce960153852ae758f257ac08be327
Polina Koval <kovalp@google.com>
2022-08-22 20:10:56 +0100

    Fix closing console bug
    Delete detachExistingSessionForColdSwap because its implementation led to the bug that console was closed on configuration restart. For the correct work of tabs it's enough to detach process.
    
    Fixes: 242296010
    Test: manually AC/ACC with run/debug/native debug/process started from device/
    Change-Id: Ide1a47496ddd0c4e9f156d273d0e187335645117
    
    GitOrigin-RevId: 570c699e743c6870cfa80c0fdcc741392b79af72

--- android/src/com/android/tools/idea/run/applychanges/ApplyChangesUtils.kt
+++ android/src/com/android/tools/idea/run/applychanges/ApplyChangesUtils.kt
@@ -15,17 +15,13 @@
  */
 package com.android.tools.idea.run.applychanges
 
-import com.android.tools.idea.run.AndroidRunState
 import com.android.tools.idea.run.ui.BaseAction
 import com.android.tools.idea.run.util.SwapInfo
 import com.intellij.execution.configurations.RunConfiguration
 import com.intellij.execution.process.ProcessHandler
 import com.intellij.execution.runners.ExecutionEnvironment
 import com.intellij.execution.ui.ExecutionConsole
-import com.intellij.execution.ui.RunContentDescriptor
 import com.intellij.execution.ui.RunContentManager
-import com.intellij.execution.ui.RunContentManagerImpl
-import com.intellij.openapi.diagnostic.Logger
 
 /**
  * Finds the existing session and returns them as [ExistingSession] if present.
@@ -37,14 +33,13 @@ import com.intellij.openapi.diagnostic.Logger
 fun findExistingSessionAndMaybeDetachForColdSwap(env: ExecutionEnvironment): ExistingSession {
   val prevHandler = BaseAction.findRunningProcessHandler(env.project, env.runProfile as RunConfiguration, env.executionTarget)
                     ?: return ExistingSession()
-  val manager = RunContentManager.getInstance(env.project)
-  val descriptor = manager.allDescriptors
-    .find { descriptor -> descriptor.processHandler === prevHandler }
   val swapInfo = env.getUserData(SwapInfo.SWAP_INFO_KEY)
   if (swapInfo == null) {
-    detachExistingSessionForColdSwap(prevHandler, descriptor, manager)
+    prevHandler.detachProcess()
     return ExistingSession()
   }
+  val descriptor = RunContentManager.getInstance(env.project).allDescriptors
+    .find { descriptor -> descriptor.processHandler === prevHandler }
   return ExistingSession(prevHandler, descriptor?.executionConsole)
 }
 
@@ -55,30 +50,3 @@ data class ExistingSession(
   val processHandler: ProcessHandler? = null,
   val executionConsole: ExecutionConsole? = null
 )
-
-/**
- * Detaches the existing session so that we don't end up with having 2 run tabs for the same launch (the existing one and the new one).
- */
-private fun detachExistingSessionForColdSwap(handler: ProcessHandler,
-                                             descriptor: RunContentDescriptor?,
-                                             manager: RunContentManager) {
-  // Detach the process first so that removeRunContent doesn't try to prompt the user to disconnect.
-  handler.detachProcess()
-  if (descriptor != null) {
-    val previousContent = descriptor.attachedContent
-    if (previousContent == null) {
-      Logger.getInstance(AndroidRunState::class.java).warn("Descriptor without content.")
-    }
-    else {
-      val previousExecutor = RunContentManagerImpl.getExecutorByContent(previousContent)
-      if (previousExecutor == null) {
-        Logger.getInstance(AndroidRunState::class.java).warn("No executor found for content")
-      }
-      else {
-        // We ignore the return value because in most cases it should return true since we already detached the process above.
-        // In all other cases the Content isn't valid, in which case we just ignore it since it's already in the intended state.
-        manager.removeRunContent(previousExecutor, descriptor)
-      }
-    }
-  }
-}
