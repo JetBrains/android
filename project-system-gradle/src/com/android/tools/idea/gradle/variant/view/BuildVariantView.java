61100ac5c4053fd0b570936cda6287d72bbaa30b
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2022-08-22 15:12:56 +0200

    mark inappropriate constructor as NonInjectable
    GitOrigin-RevId: 1c101a5da9a871ebdcdda9c1e751e0721095471c

--- project-system-gradle/src/com/android/tools/idea/gradle/variant/view/BuildVariantView.java
+++ project-system-gradle/src/com/android/tools/idea/gradle/variant/view/BuildVariantView.java
@@ -43,6 +43,7 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.ComboBox;
 import com.intellij.openapi.ui.MessageType;
 import com.intellij.openapi.wm.ToolWindow;
+import com.intellij.serviceContainer.NonInjectable;
 import com.intellij.ui.TableSpeedSearch;
 import com.intellij.ui.content.Content;
 import com.intellij.ui.content.ContentFactory;
@@ -104,6 +105,7 @@ public class BuildVariantView {
     this(project, BuildVariantUpdater.getInstance(project));
   }
 
+  @NonInjectable
   private BuildVariantView(@NotNull Project project, @NotNull BuildVariantUpdater updater) {
     myProject = project;
     myUpdater = updater;
