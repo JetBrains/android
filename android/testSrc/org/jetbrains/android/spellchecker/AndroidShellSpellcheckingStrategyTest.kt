1e81e2bcdeb60144b9e4f63769ae06ae2cd4cf4f
Dana Dahlstrom <dahlstrom@google.com>
2022-08-22 22:00:00 -0700

    Remove @Ignore from AndroidShellSpellcheckingStrategyTest
    … which passes with the Kotlin plugin updated for IntelliJ 221.
    
    Change-Id: Ie161ee535f39829849b640ac81c4d01dcd122448
    Fixes: 233921382
    Issue: 232541564
    
    GitOrigin-RevId: b1f5bf717a6d992f6c4a246311a47c4e2322ca06

--- android/testSrc/org/jetbrains/android/spellchecker/AndroidShellSpellcheckingStrategyTest.kt
+++ android/testSrc/org/jetbrains/android/spellchecker/AndroidShellSpellcheckingStrategyTest.kt
@@ -25,7 +25,6 @@ import org.jetbrains.android.AndroidTestCase
  *
  * Verifies that gradlew files are the only shell files ignore by spellcheck.
  */
-@org.junit.Ignore("b/233921382")
 class AndroidShellSpellcheckingStrategyTest : AndroidTestCase() {
   override fun setUp() {
     super.setUp()
