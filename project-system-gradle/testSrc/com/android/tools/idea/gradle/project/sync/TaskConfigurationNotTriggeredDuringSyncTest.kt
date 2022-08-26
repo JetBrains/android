95c058cd5e526e4291be5826353fda0eb31d2eff
Karima Ichou <karimai@google.com>
2022-08-26 17:48:06 +0100

    Use utility method to capture events during Sync
    Do not rely on using a listener to capture the events created during
    Sync as we do have a utility method that logs output messages.
    
    Bug: 243767844
    Test: this
    Change-Id: I5357e3eb0a90e0ab2916722e093a7d0468e67ee9
    
    GitOrigin-RevId: 10f94777677ddb8014c1bea1be402e95b1bb9464

--- project-system-gradle/testSrc/com/android/tools/idea/gradle/project/sync/TaskConfigurationNotTriggeredDuringSyncTest.kt
+++ project-system-gradle/testSrc/com/android/tools/idea/gradle/project/sync/TaskConfigurationNotTriggeredDuringSyncTest.kt
@@ -18,16 +18,13 @@ package com.android.tools.idea.gradle.project.sync
 import com.android.tools.idea.gradle.project.GradleExperimentalSettings
 import com.android.tools.idea.testing.AndroidProjectRule
 import com.android.tools.idea.testing.GradleIntegrationTest
+import com.android.tools.idea.testing.OpenPreparedProjectOptions
 import com.android.tools.idea.testing.TestProjectToSnapshotPaths.SIMPLE_APPLICATION
 import com.android.tools.idea.testing.onEdt
 import com.android.tools.idea.testing.openPreparedProject
 import com.android.tools.idea.testing.prepareGradleProject
 import com.google.common.truth.Expect
 import com.google.common.truth.Truth.assertThat
-import com.intellij.openapi.externalSystem.model.task.ExternalSystemTaskId
-import com.intellij.openapi.externalSystem.model.task.ExternalSystemTaskNotificationEvent
-import com.intellij.openapi.externalSystem.model.task.ExternalSystemTaskNotificationListenerAdapter
-import com.intellij.openapi.externalSystem.service.notification.ExternalSystemProgressNotificationManager
 import com.intellij.testFramework.RunsInEdt
 import com.intellij.util.PathUtil
 import org.jetbrains.android.AndroidTestBase
@@ -48,35 +45,24 @@ class TaskConfigurationNotTriggeredDuringSyncTest : GradleIntegrationTest {
   fun testTasksAreNotConfiguredDuringSync() {
     // We do not allow Gradle tasks configuration by default on AS.
     assertThat(GradleExperimentalSettings.getInstance().SKIP_GRADLE_TASKS_LIST).isEqualTo(true)
-    val listener = object : ExternalSystemTaskNotificationListenerAdapter() {
-      var messagesLog = StringBuilder()
-      var finalMessage = ""
-
-      override fun onEnd(id: ExternalSystemTaskId) {
-        super.onEnd(id)
-        finalMessage = messagesLog.toString()
-      }
-
-      override fun onStatusChange(event: ExternalSystemTaskNotificationEvent) {
-        super.onStatusChange(event)
-        messagesLog.append(event.toString())
-      }
-    }
-    ExternalSystemProgressNotificationManager.getInstance().addNotificationListener(listener)
 
     val prepared = prepareGradleProject(SIMPLE_APPLICATION, "project")
 
+    val failureMessage = "task should not be configured"
     val buildFile = prepared.resolve("app").resolve("build.gradle")
     buildFile.writeText(
       buildFile.readText() + """
           tasks.register("shouldNotBeConfigured", Test.class).configure {
-              throw RuntimeException("task should not be configured")
+              println($failureMessage)
           }
         """.trimIndent()
     )
-    openPreparedProject("project") { project ->
-      assertThat(listener.finalMessage.contains("Can not load tasks for project ':app'")).isFalse()
-    }
+    val outputLog = StringBuilder()
+    openPreparedProject(
+      "project",
+      options = OpenPreparedProjectOptions(outputHandler = { outputLog.append(it) })
+    ) {}
+    assertThat(outputLog.toString()).doesNotContain(failureMessage)
   }
 
   override fun getBaseTestPath(): String = projectRule.fixture.tempDirPath
