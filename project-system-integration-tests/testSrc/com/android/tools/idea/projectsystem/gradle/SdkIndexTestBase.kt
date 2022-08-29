a9de840fe263b9580c7b39b27d53acc486b9a159
Sergio Murguia <srmurguia@google.com>
2022-08-29 11:51:29 -0700

    [SDK Index] E2E test for policy issues
    This change adds a new test to replace manual testing for policy
    issues. The test sets true to the flag that causes policy issues to be
    shown and then verifies that those issues are shown. This change also
    modifies SdkIndexPsdTest to verify that the default flag setting causes
    the issue to be logged but not shown in PSD.
    
    The snapshot.gz was modified to include only the set of issues needed by
    the tests (including a policy issue), as opposed to use a public
    available snapshot.
    
    Bug: 233789469
    Test: PolicyIssuesPsdTest, SdkIndexPsdTest
    Change-Id: Ib0fab4ac050087b9da1ac55cbd035f2caecf5c41
    
    GitOrigin-RevId: 65fc5b23c0b32bd038f63b45bb3039908aea5f4b

--- project-system-integration-tests/testSrc/com/android/tools/idea/projectsystem/gradle/SdkIndexTestBase.kt
+++ project-system-integration-tests/testSrc/com/android/tools/idea/projectsystem/gradle/SdkIndexTestBase.kt
@@ -27,6 +27,7 @@ import com.intellij.util.io.exists
 import org.junit.Rule
 import java.util.concurrent.TimeUnit
 import java.util.regex.Pattern
+import kotlin.concurrent.thread
 
 open class SdkIndexTestBase {
   private val snapshotPath = "system/sdk_index/snapshot.gz"
@@ -34,6 +35,7 @@ open class SdkIndexTestBase {
   private val testProjectPath = "tools/adt/idea/project-system-integration-tests/testData/sdkindexapp"
   private val testRepoManifest = "tools/adt/idea/project-system-integration-tests/sdkindexproject_deps.manifest"
   private val snapshotTimeoutSeconds: Long = 60
+  private val timeoutPsdShowSeconds: Long = 60
   // TODO(b/243691427): Change the way we confirm no more issues were created, on test runs it is around 10 ms between issues
   private val timeoutBetweenIssuesSeconds: Long = 2
 
@@ -42,7 +44,7 @@ open class SdkIndexTestBase {
   val system: AndroidSystem = AndroidSystem.standard()
 
   fun verifySdkIndexIsInitializedAndUsedWhen(showFunction: ((studio: AndroidStudio, project: AndroidProject) -> Unit)?,
-                                             closeFunction: ((studio: AndroidStudio, project: AndroidProject) -> Unit)?,
+                                             beforeClose: (() -> Unit)?,
                                              expectedIssues: Set<String>) {
     val project = AndroidProject(testProjectPath)
     project.setDistribution("tools/external/gradle/gradle-7.3.3-bin.zip")
@@ -84,8 +86,43 @@ open class SdkIndexTestBase {
           break
         }
       }
-      closeFunction?.invoke(studio, project)
+      beforeClose?.invoke()
       assertThat(foundIssues).isEqualTo(expectedIssues)
     }
+  }
+
+  protected fun verifyPsdIssues(numErrors: Int) {
+    val summaryRegex = ".*PsAnalyzerDaemon - Issues recreated: (.*)$"
+    // The test project uses the following libraries:
+    //  - com.startapp:inapp-sdk: 3.9.1 error (blocking critical)
+    //  - com.stripe:stripe-android:9.3.2 error (if policy issues are enabled)
+    //  - com.mopub:mopub-sdk:4.16.0 warning (outdated)
+    //  - com.snowplowanalytics:snowplow-android-tracker:1.4.1 info (critical, but not blocking)
+    val expectedSummary = "$numErrors errors, 1 warnings, 1 information, 0 updates, 0 other"
+    val foundSummary: String
+    try {
+      val matcher = system.installation.ideaLog.waitForMatchingLine(summaryRegex, timeoutBetweenIssuesSeconds, TimeUnit.SECONDS)
+      foundSummary = matcher.group(1)
+    }
+    catch (exc: InterruptedException) {
+      print("Test failed because no summary message was found")
+      throw exc
+    }
+    assertThat(foundSummary).isEqualTo(expectedSummary)
+  }
+
+  protected fun openAndClosePSD(studio: AndroidStudio) {
+    thread(start = true) {
+      try {
+        // Wait for summary to appear
+        val summaryRegex = ".*PsAnalyzerDaemon - Issues recreated: (.*)$"
+        system.installation.ideaLog.waitForMatchingLine(summaryRegex, null,true, timeoutPsdShowSeconds, TimeUnit.SECONDS)
+      }
+      finally {
+        // Close PSD even if summary could not be found so executeAction("ShowProjectStructureSettings") is finished
+        studio.invokeComponent("OK")
+      }
+    }
+    studio.executeAction("ShowProjectStructureSettings")
   }
 }
