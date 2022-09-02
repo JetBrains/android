a307efc8f2eb8bb3452bda9e0439dfcf359db806
James Rose <rosej@google.com>
2022-09-02 12:20:13 -0700

    Add DeviceHandleRenderer
    This is like DeviceRenderer but for DeviceHandles.
    
    Bug: 241591222
    Test: Included
    Change-Id: I2ac0fd56a0999a7901262d3c89c57cabe38a4c77
    
    GitOrigin-RevId: 9a75ac5a01cf45cd32e7b3eff90da9619d62a2a5

---
+++ android-adb/src/com/android/tools/idea/deviceprovisioner/DeviceHandleRenderer.kt
@@ -0,0 +1,118 @@
+/*
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.deviceprovisioner
+
+import com.android.adblib.DeviceState
+import com.android.adblib.deviceInfo
+import com.android.sdklib.SdkVersionInfo
+import com.android.sdklib.deviceprovisioner.DeviceHandle
+import com.intellij.ui.ColoredListCellRenderer
+import com.intellij.ui.ColoredTextContainer
+import com.intellij.ui.SimpleTextAttributes
+import javax.swing.JList
+import javax.swing.ListModel
+
+/** Utilities for rendering a [DeviceHandle] to a [ColoredTextContainer]. */
+object DeviceHandleRenderer {
+  /** Renders the given [DeviceHandle] to a [ColoredTextContainer]. */
+  @JvmStatic
+  fun renderDevice(
+    component: ColoredTextContainer,
+    device: DeviceHandle,
+  ) {
+    renderDevice(component, device, device.state.properties.title(), false)
+  }
+
+  /**
+   * Renders the given [DeviceHandle] to a [ColoredTextContainer], attempting to disambiguate it if
+   * it has the same label as another device in the given list.
+   */
+  @JvmStatic
+  fun renderDevice(
+    component: ColoredTextContainer,
+    device: DeviceHandle,
+    allDevices: Iterable<DeviceHandle>
+  ) {
+    val name = device.state.properties.title()
+    val isDuplicated = allDevices.any { it != device && it.state.properties.title() == name }
+
+    renderDevice(component, device, name, isDuplicated)
+  }
+
+  private fun renderDevice(
+    component: ColoredTextContainer,
+    device: DeviceHandle,
+    name: String,
+    isDuplicated: Boolean
+  ) {
+    component.append(name, SimpleTextAttributes.REGULAR_ATTRIBUTES)
+
+    when (val deviceState = device.state.connectedDevice?.deviceInfo?.deviceState) {
+      null ->
+        component.append(
+          " [${device.state.javaClass.simpleName}]",
+          SimpleTextAttributes.GRAYED_BOLD_ATTRIBUTES
+        )
+      DeviceState.ONLINE -> {}
+      else ->
+        component.append(
+          " [${titleCase(deviceState.toString())}]",
+          SimpleTextAttributes.GRAYED_BOLD_ATTRIBUTES
+        )
+    }
+
+    if (isDuplicated) {
+      device.state.properties.disambiguator?.let {
+        component.append(" [$it]", SimpleTextAttributes.GRAYED_BOLD_ATTRIBUTES)
+      }
+    }
+
+    // E.g. "Android 5.1 (Lollipop)"
+    device.state.properties.androidVersion?.let {
+      component.append(
+        " ${SdkVersionInfo.getVersionWithCodename(it)}",
+        SimpleTextAttributes.GRAY_ATTRIBUTES
+      )
+    }
+  }
+}
+
+/** Renderer for device names in lists and combo boxes. */
+class DeviceHandleListCellRenderer : ColoredListCellRenderer<DeviceHandle>() {
+  override fun customizeCellRenderer(
+    list: JList<out DeviceHandle>,
+    value: DeviceHandle?,
+    index: Int,
+    selected: Boolean,
+    hasFocus: Boolean
+  ) {
+    value ?: return
+
+    DeviceHandleRenderer.renderDevice(this, value, list.model.toIterable())
+  }
+}
+
+fun <T> ListModel<T>.toIterable(): Iterable<T> =
+  object : Iterable<T> {
+    override fun iterator(): Iterator<T> =
+      object : Iterator<T> {
+        var index = 0
+        override fun hasNext(): Boolean = index < size
+        override fun next(): T = getElementAt(index++)
+      }
+  }
+
+fun titleCase(s: String) = if (s.isEmpty()) "" else s[0].titlecase() + s.substring(1).lowercase()
---
+++ android-adb/testSrc/com/android/tools/idea/deviceprovisioner/DeviceHandleRendererTest.kt
@@ -0,0 +1,123 @@
+/*
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.deviceprovisioner
+
+import com.android.adblib.AdbSession
+import com.android.adblib.SOCKET_CONNECT_TIMEOUT_MS
+import com.android.adblib.testingutils.CloseablesRule
+import com.android.adblib.testingutils.CoroutineTestUtils.runBlockingWithTimeout
+import com.android.adblib.testingutils.FakeAdbServerProvider
+import com.android.adblib.testingutils.TestingAdbSessionHost
+import com.android.fakeadbserver.DeviceState
+import com.android.sdklib.AndroidVersion
+import com.android.sdklib.deviceprovisioner.Connected
+import com.android.sdklib.deviceprovisioner.DeviceProperties
+import com.android.sdklib.deviceprovisioner.DeviceProvisioner
+import com.android.sdklib.deviceprovisioner.testing.FakeAdbDeviceProvisionerPlugin
+import com.google.common.truth.Truth.assertThat
+import com.intellij.ui.SimpleColoredText
+import java.time.Duration
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.takeWhile
+import org.junit.Rule
+import org.junit.Test
+
+class DeviceHandleRendererTest {
+
+  @JvmField @Rule val closeables = CloseablesRule()
+
+  val fakeAdb = closeables.register(FakeAdbServerProvider().buildDefault().start())
+  val host = closeables.register(TestingAdbSessionHost())
+  val session =
+    closeables.register(
+      AdbSession.create(
+        host,
+        fakeAdb.createChannelProvider(host),
+        Duration.ofMillis(SOCKET_CONNECT_TIMEOUT_MS)
+      )
+    )
+
+  val plugin = FakeAdbDeviceProvisionerPlugin(fakeAdb)
+  val provisioner = DeviceProvisioner.create(session, listOf(plugin))
+
+  private fun DeviceProperties.Builder.baseProperties() {
+    manufacturer = "Google"
+    model = "Pixel 6"
+    androidRelease = "12.0"
+    androidVersion = AndroidVersion(31)
+  }
+  val properties1 =
+    DeviceProperties.build {
+      baseProperties()
+      disambiguator = "SN1"
+    }
+  val properties2 =
+    DeviceProperties.build {
+      baseProperties()
+      disambiguator = "SN2"
+    }
+  val device1 = plugin.addNewDevice(properties = properties1)
+  val device2 = plugin.addNewDevice(properties = properties2)
+
+  @Test
+  fun disconnected() = runBlockingWithTimeout {
+    val text = SimpleColoredText()
+    DeviceHandleRenderer.renderDevice(text, device1)
+
+    assertThat(text.toString()).isEqualTo("Google Pixel 6 [Disconnected] Android 12.0 (S)")
+  }
+
+  @Test
+  fun unauthorized() = runBlockingWithTimeout {
+    activate(device1)
+    device1.fakeAdbDevice?.deviceStatus = DeviceState.DeviceStatus.UNAUTHORIZED
+    checkNotNull(device1.state.connectedDevice)
+      .deviceInfoFlow
+      .takeWhile { it.deviceState != com.android.adblib.DeviceState.UNAUTHORIZED }
+      .collect()
+
+    val text = SimpleColoredText()
+    DeviceHandleRenderer.renderDevice(text, device1)
+
+    assertThat(text.toString()).isEqualTo("Google Pixel 6 [Unauthorized] Android 12.0 (S)")
+  }
+
+  @Test
+  fun connected() = runBlockingWithTimeout {
+    activate(device1)
+
+    val text = SimpleColoredText()
+    DeviceHandleRenderer.renderDevice(text, device1)
+
+    assertThat(text.toString()).isEqualTo("Google Pixel 6 Android 12.0 (S)")
+  }
+
+  @Test
+  fun duplicate() = runBlockingWithTimeout {
+    val allDevices = listOf(device1, device2)
+    allDevices.forEach { activate(it) }
+
+    val text = SimpleColoredText()
+    DeviceHandleRenderer.renderDevice(text, device1, allDevices)
+
+    assertThat(text.toString()).isEqualTo("Google Pixel 6 [SN1] Android 12.0 (S)")
+  }
+
+  private suspend fun activate(deviceHandle: FakeAdbDeviceProvisionerPlugin.FakeDeviceHandle) {
+    deviceHandle.activationAction.activate()
+    deviceHandle.stateFlow.takeWhile { it !is Connected }.collect()
+  }
+}
