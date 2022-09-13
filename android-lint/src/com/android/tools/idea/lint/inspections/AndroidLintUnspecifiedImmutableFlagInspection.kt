5bfdb4ae3538c25e944bf682f48f8fde2629fb74
mattgilbride <mattgilbride@google.com>
2022-09-13 16:24:14 +0000

    Update generated files for UnspecifiedImmutableFlag lint
    Bug: 219022225
    Test: PendingIntentMutableFlagDetectorTest
    Change-Id: Ia04f9a3fc561430ee4a7a773cc729acf8068a498
    
    GitOrigin-RevId: 28c02e4c08aacf70bc3217ac1f100352f0068614

--- android-lint/src/com/android/tools/idea/lint/inspections/AndroidLintUnspecifiedImmutableFlagInspection.kt
+++ android-lint/src/com/android/tools/idea/lint/inspections/AndroidLintUnspecifiedImmutableFlagInspection.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2020 The Android Open Source Project
+ * Copyright (C) 2022 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,10 +13,11 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.android.tools.idea.lint.common
+package com.android.tools.idea.lint.inspections
 
-import com.android.tools.idea.lint.common.LintBundle.Companion.message
+import com.android.tools.idea.lint.common.AndroidLintInspectionBase
 import com.android.tools.lint.checks.PendingIntentMutableFlagDetector
+import com.android.tools.idea.lint.AndroidLintBundle.Companion.message
 
 class AndroidLintUnspecifiedImmutableFlagInspection : AndroidLintInspectionBase(
   message("android.lint.inspections.unspecified.immutable.flag"), PendingIntentMutableFlagDetector.ISSUE
