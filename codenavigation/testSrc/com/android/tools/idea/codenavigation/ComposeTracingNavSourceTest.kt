d3a6efc1fe064c0ae9310983bd9c594a718622de
Jakub Gielzak <jgielzak@google.com>
2022-09-20 20:29:44 +0100

    Deduplicating click-to-navigate using version
    Deduplicating click-to-navigate using package version. Helps in cases
    where multiple source versions for the same library are returned by file
    index search.
    
    See diff (before -> after):
    https://diff.googleplex.com/#key=9kA8y9QA95d9
    
    Bug: 244437735
    Test: ComposeTracingNavSourceTest
    
    Change-Id: I79de0cfa74abcf333d85c6b2e96710ee7e05e836
    
    GitOrigin-RevId: 8c824443c8710f0f4c78bc6bc049eb9a5408b5b2

--- codenavigation/testSrc/com/android/tools/idea/codenavigation/ComposeTracingNavSourceTest.kt
+++ codenavigation/testSrc/com/android/tools/idea/codenavigation/ComposeTracingNavSourceTest.kt
@@ -15,11 +15,15 @@
  */
 package com.android.tools.idea.codenavigation
 
+import com.android.tools.idea.codenavigation.ComposeTracingNavSource.Companion.filterByMaxLibraryVersion
+import com.android.tools.idea.codenavigation.ComposeTracingNavSource.Companion.filterByMaxPackageNameLength
+import com.android.tools.idea.codenavigation.ComposeTracingNavSource.LibrarySignature
 import com.google.common.truth.Truth.assertThat
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.pom.Navigatable
 import com.intellij.psi.PsiClassOwner
 import com.intellij.psi.PsiFile
+import org.apache.maven.artifact.versioning.ComparableVersion
 import org.junit.Test
 import org.mockito.Mockito
 import org.mockito.Mockito.mock
@@ -32,15 +36,16 @@ class ComposeTracingNavSourceTest {
     // given
     val navSource = ComposeTracingNavSource(
       getFilesByName = { emptyList() },
-      createNavigatable = { files, lineNumber -> FakeNavigatable(files, lineNumber) }
+      createNavigatable = { files, lineNumber -> FakeNavigatable(files, lineNumber) },
+      mavenSignatureResolver = { null }
     )
 
     // when
     val codeLocation = "androidx.compose.foundation.lazy.grid.LazyVerticalGrid (LazyGridDsl.kt:62)".toCodeLocation()!!
-    val actual = navSource.lookUp(codeLocation, null)
+    val actual = navSource.lookUp(codeLocation, null) as FakeNavigatable
 
     // then
-    assertThat(actual).isEqualTo(null)
+    assertThat(actual.files).isEmpty()
   }
 
   @Test
@@ -49,10 +54,11 @@ class ComposeTracingNavSourceTest {
     val fileName = "LazyGridDsl.kt"
     val packageName = "androidx.compose.foundation.lazy.grid"
     val lineNumber = 62
-    val targetFile = CodeFile("/path/to/file/$fileName", packageName, lineNumber).toPsiClassOwner()
+    val targetFile = FakeCodeFile("/path/to/file/$fileName", lineNumber, packageName)
     val navSource = ComposeTracingNavSource(
       getFilesByName = { listOf(targetFile) },
-      createNavigatable = { files, line -> FakeNavigatable(files, line) }
+      createNavigatable = { files, line -> FakeNavigatable(files, line) },
+      mavenSignatureResolver = { null }
     )
 
     // when
@@ -69,11 +75,12 @@ class ComposeTracingNavSourceTest {
     val fileName = "LazyGridDsl.kt"
     val packageName = "androidx.compose.foundation.lazy.grid"
     val lineNumber = 62
-    val targetFile = CodeFile("/path/to/file/$fileName", packageName, lineNumber).toPsiClassOwner()
-    val otherFile = CodeFile("/path/to/file/$fileName", "wrong-package", lineNumber).toPsiClassOwner()
+    val targetFile = FakeCodeFile("/path/to/file/$fileName", lineNumber, packageName)
+    val otherFile = FakeCodeFile("/path/to/file/$fileName", lineNumber, "wrong-package")
     val navSource = ComposeTracingNavSource(
       getFilesByName = { listOf(targetFile, otherFile) },
-      createNavigatable = { files, line -> FakeNavigatable(files, line) }
+      createNavigatable = { files, line -> FakeNavigatable(files, line) },
+      mavenSignatureResolver = { null }
     )
 
     // when
@@ -90,11 +97,12 @@ class ComposeTracingNavSourceTest {
     val fileName = "LazyGridDsl.kt"
     val packageName = "androidx.compose.foundation.lazy.grid"
     val lineNumber = 62
-    val targetFile = CodeFile("/path/to/file/$fileName", packageName, lineNumber).toPsiClassOwner()
-    val otherFile = CodeFile("/path/to/file/$fileName", packageName, lineNumber - 1).toPsiClassOwner()
+    val targetFile = FakeCodeFile("/path/to/file/$fileName", lineNumber, packageName)
+    val otherFile = FakeCodeFile("/path/to/file/$fileName", lineNumber - 1, packageName)
     val navSource = ComposeTracingNavSource(
       getFilesByName = { listOf(targetFile, otherFile) },
-      createNavigatable = { files, line -> FakeNavigatable(files, line) }
+      createNavigatable = { files, line -> FakeNavigatable(files, line) },
+      mavenSignatureResolver = { null }
     )
 
     // when
@@ -112,11 +120,12 @@ class ComposeTracingNavSourceTest {
     val packageNameTarget = "androidx.compose.foundation.lazy.grid"
     val packageNameShorter = "androidx.compose.foundation.lazy"
     val lineNumber = 62
-    val targetFile = CodeFile("/path/to/file/$fileName", packageNameTarget, lineNumber).toPsiClassOwner()
-    val otherFile = CodeFile("/path/to/file/$fileName", packageNameShorter, lineNumber).toPsiClassOwner()
+    val targetFile = FakeCodeFile("/path/to/file/$fileName", lineNumber, packageNameTarget)
+    val otherFile = FakeCodeFile("/path/to/file/$fileName", lineNumber, packageNameShorter)
     val navSource = ComposeTracingNavSource(
       getFilesByName = { listOf(targetFile, otherFile) },
-      createNavigatable = { files, line -> FakeNavigatable(files, line) }
+      createNavigatable = { files, line -> FakeNavigatable(files, line) },
+      mavenSignatureResolver = { null }
     )
 
     // when
@@ -133,11 +142,12 @@ class ComposeTracingNavSourceTest {
     val fileName = "LazyGridDsl.kt"
     val packageName = "androidx.compose.foundation.lazy.grid"
     val lineNumber = 62
-    val targetFile1 = CodeFile("/path/to/file/1/$fileName", packageName, lineNumber).toPsiClassOwner()
-    val targetFile2 = CodeFile("/path/to/file/2/$fileName", packageName, lineNumber).toPsiClassOwner()
+    val targetFile1 = FakeCodeFile("/path/to/file/1/$fileName", lineNumber, packageName)
+    val targetFile2 = FakeCodeFile("/path/to/file/2/$fileName", lineNumber, packageName)
     val navSource = ComposeTracingNavSource(
       getFilesByName = { listOf(targetFile1, targetFile2) },
-      createNavigatable = { files, line -> FakeNavigatable(files, line) }
+      createNavigatable = { files, line -> FakeNavigatable(files, line) },
+      mavenSignatureResolver = { null }
     )
 
     // when
@@ -147,6 +157,103 @@ class ComposeTracingNavSourceTest {
     // then
     assertThat((actual as FakeNavigatable).files).isEqualTo(listOf(targetFile1, targetFile2))
   }
+
+  @Test
+  fun test_maxPackageNameLength() {
+    // given
+    val files: List<PsiClassOwner> = listOf(
+      FakeCodeFile("", 1, packageName = "abcd"),
+      FakeCodeFile("", 1, packageName = "abc"),
+      FakeCodeFile("", 1, packageName = "abcs"),
+      FakeCodeFile("", 1, packageName = "ab"),
+      FakeCodeFile("", 1, packageName = "bbaa"),
+    )
+
+    // when
+    val actual = files.filterByMaxPackageNameLength()
+
+    // then
+    assertThat(actual).isEqualTo(listOf(files.first(), files[2], files.last()))
+  }
+
+  @Test
+  fun test_maxLibraryVersion_oneLibraryDuplicated() {
+    // given
+    val files: List<PsiClassOwner> = listOf(
+      FakeCodeFile(path = "", lineCount = 99, packageName = "", librarySignatureString = "g:a:1.3.1-beta02"),
+      FakeCodeFile(path = "", lineCount = 99, packageName = "", librarySignatureString = "g:a:1.0.1")
+    )
+
+    // when
+    val actual = files.filterByMaxLibraryVersion { (it as FakeCodeFile).librarySignature }
+
+    // then
+    assertThat(actual).isEqualTo(listOf(files.first()))
+  }
+
+  @Test
+  fun test_maxLibraryVersion_twoLibrariesDuplicated() {
+    // given
+    val files: List<PsiClassOwner> = listOf(
+      FakeCodeFile(path = "", lineCount = 99, packageName = "", librarySignatureString = "group1:artifact1:1.3.1-beta02"), // 0
+      FakeCodeFile(path = "", lineCount = 99, packageName = "", librarySignatureString = "group1:artifact1:1.0.1"), // 1
+      FakeCodeFile(path = "", lineCount = 99, packageName = "", librarySignatureString = "group2:artifact2:1.1.0-alpha01"), // 2
+      FakeCodeFile(path = "", lineCount = 99, packageName = "", librarySignatureString = "group2:artifact2:1.0.1"), // 3
+    )
+
+    // when
+    val actual = files.filterByMaxLibraryVersion { (it as FakeCodeFile).librarySignature }
+
+    // then
+    assertThat(actual).isEqualTo(listOf(0, 2).map { files[it] })
+  }
+
+  @Test
+  fun test_maxLibraryVersion_twoLibrariesDuplicated_nonLibraryFilesPresent() {
+    // given
+    val files: List<PsiClassOwner> = listOf(
+      FakeCodeFile(path = "", lineCount = 99, packageName = "", librarySignatureString = null), // 0
+      FakeCodeFile(path = "", lineCount = 99, packageName = "", librarySignatureString = "group1:artifact1:1.3.1-beta02"), // 1
+      FakeCodeFile(path = "", lineCount = 99, packageName = "", librarySignatureString = "group1:artifact1:1.0.1"), // 2
+      FakeCodeFile(path = "", lineCount = 99, packageName = "", librarySignatureString = "group2:artifact2:1.1.0-alpha01"), // 3
+      FakeCodeFile(path = "", lineCount = 99, packageName = "", librarySignatureString = "group2:artifact2:1.0.1"), // 4
+      FakeCodeFile(path = "", lineCount = 99, packageName = "", librarySignatureString = null), // 5
+    )
+
+    // when
+    val actual = files.filterByMaxLibraryVersion { (it as FakeCodeFile).librarySignature }
+
+    // then
+    assertThat(actual).isEqualTo(listOf(0, 1, 3, 5).map { files[it] })
+  }
+
+  @Test
+  fun test_allRules_integration() {
+    // given
+    val codeLocation = "pn.LazyVerticalGrid (F.kt:50)".toCodeLocation()!! // package=pn, lineNumber=50
+    val files: List<PsiClassOwner> = listOf(
+      FakeCodeFile(path = "/p/F.kt", lineCount = 99, packageName = "p", librarySignatureString = null), // 0 packageName
+      FakeCodeFile(path = "/p/F.kt", lineCount = 99, packageName = "pn", librarySignatureString = null), // 1 OK
+      FakeCodeFile(path = "/p/F.kt", lineCount = 99, packageName = "pn", librarySignatureString = "group1:artifact1:1.3.1-beta02"), // 2 OK
+      FakeCodeFile(path = "/p/F.kt", lineCount = 99, packageName = "pn", librarySignatureString = "group1:artifact1:1.0.1"), // 3 version
+      FakeCodeFile(path = "/p/F.kt", lineCount = 99, packageName = "pn", librarySignatureString = "group2:artifact2:1.1.0-alpha01"), // 4 OK
+      FakeCodeFile(path = "/p/F.kt", lineCount = 99, packageName = "pn", librarySignatureString = "group2:artifact2:1.0.1"), // 5 version
+      FakeCodeFile(path = "/p/F.kt", lineCount = 99, packageName = "pn", librarySignatureString = null), // 6 OK
+      FakeCodeFile(path = "/p/F.kt", lineCount = 10, packageName = "pn", librarySignatureString = null), // 7 lineCount
+      FakeCodeFile(path = "/p/F.kt", lineCount = 99, packageName = "qwerty", librarySignatureString = null), // 8 packageName
+    )
+    val navSource = ComposeTracingNavSource(
+      getFilesByName = { files },
+      createNavigatable = { fs, line -> FakeNavigatable(fs, line) },
+      mavenSignatureResolver = { (it as FakeCodeFile).librarySignature }
+    )
+
+    // when
+    val actual = navSource.lookUp(codeLocation, null)
+
+    // then
+    assertThat(actual).isEqualTo(FakeNavigatable(listOf(1, 2, 4, 6).map { files[it] }, 50))
+  }
 }
 
 private data class FakeNavigatable(val files: List<PsiFile>, val lineNumber: Int) : Navigatable {
@@ -155,17 +262,25 @@ private data class FakeNavigatable(val files: List<PsiFile>, val lineNumber: Int
   override fun canNavigateToSource(): Boolean = throw IllegalStateException("Not implemented")
 }
 
-private data class CodeFile(val path: String, val packageName: String, val lineCount: Int)
-
-private fun CodeFile.toPsiClassOwner(): PsiClassOwner = this.run {
-  val result = mock(PsiClassOwner::class.java)
-  Mockito.`when`(result.packageName).thenReturn(packageName)
-  val fakeText = List(lineCount) { "" }.joinToString(separator = "\n") { it }
-  Mockito.`when`(result.text).thenReturn(fakeText)
-  val fakeVirtualFile = mock(VirtualFile::class.java)
-  Mockito.`when`(result.virtualFile).thenReturn(fakeVirtualFile)
-  Mockito.`when`(fakeVirtualFile.path).thenReturn(path)
-  return result
+private class FakeCodeFile private constructor(
+  private val virtualFile: VirtualFile,
+  private val fileContents: String,
+  private val packageName: String,
+  val librarySignature: LibrarySignature?,
+  mock: PsiClassOwner = mock(PsiClassOwner::class.java),
+) : PsiClassOwner by mock {
+  constructor(path: String, lineCount: Int, packageName: String, librarySignatureString: String? = null) : this(
+    virtualFile = mock(VirtualFile::class.java).also { Mockito.`when`(it.path).thenReturn(path) },
+    fileContents = (1..lineCount).joinToString(separator = "\n") { "" },
+    packageName = packageName,
+    librarySignature = librarySignatureString?.let {
+      val (groupId, artifactId, version) = it.split(":")
+      LibrarySignature(groupId, artifactId, ComparableVersion(version))
+    }
+  )
+  override fun getPackageName() = packageName
+  override fun getVirtualFile() = virtualFile
+  override fun getText(): String = fileContents
 }
 
 /**
