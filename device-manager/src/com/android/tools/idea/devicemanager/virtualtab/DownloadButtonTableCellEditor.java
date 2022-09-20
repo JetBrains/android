d9a874e1cb83d4994379fab72adb0e8a782fc57e
Hannah Chu <hanchu@google.com>
2022-09-20 16:44:28 -0700

    [DM] Add basic download button
    It's not yet being used, and it just brings up the SDK Manager dialog.
    
    Bug: 237701028
    Test: None
    Change-Id: Ic98973959a86003e3871b66fe42e0432233ff5f9
    
    GitOrigin-RevId: 7f79ce18ad29c1484ff1d311e3173f428e915c68

--- device-manager/src/com/android/tools/idea/devicemanager/virtualtab/DownloadButtonTableCellEditor.java
+++ device-manager/src/com/android/tools/idea/devicemanager/virtualtab/DownloadButtonTableCellEditor.java
@@ -15,12 +15,42 @@
  */
 package com.android.tools.idea.devicemanager.virtualtab;
 
+import com.android.tools.idea.avdmanager.AvdManagerConnection;
 import com.android.tools.idea.devicemanager.IconButtonTableCellEditor;
+import com.android.tools.idea.sdk.wizard.SdkQuickfixUtils;
+import com.intellij.icons.AllIcons;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.DialogWrapper;
 import java.awt.Component;
+import java.util.List;
 import javax.swing.JTable;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-final class DeviceIconButtonTableCellEditor extends IconButtonTableCellEditor {
+final class DownloadButtonTableCellEditor extends IconButtonTableCellEditor {
+  private VirtualDevice myDevice;
+  private VirtualDeviceTable myTable;
+
+  DownloadButtonTableCellEditor(@Nullable Project project) {
+    super(DownloadValue.INSTANCE, AllIcons.Actions.Download, "Download system image");
+
+    myButton.addActionListener(event -> {
+      String path = AvdManagerConnection.getRequiredSystemImagePath(myDevice.getAvdInfo());
+      assert path != null;
+
+      DialogWrapper dialog = SdkQuickfixUtils.createDialogForPaths(project, List.of(path));
+
+      if (dialog == null) {
+        return;
+      }
+
+      dialog.show();
+
+      // Multiple AVDs can have the same missing system image, so refresh all
+      myTable.refreshAvds();
+    });
+  }
+
   @Override
   public @NotNull Component getTableCellEditorComponent(@NotNull JTable table,
                                                         @NotNull Object value,
@@ -28,7 +58,9 @@ final class DeviceIconButtonTableCellEditor extends IconButtonTableCellEditor {
                                                         int viewRowIndex,
                                                         int viewColumnIndex) {
     super.getTableCellEditorComponent(table, value, selected, viewRowIndex, viewColumnIndex);
-    myButton.setDefaultIcon(((VirtualDeviceTable)table).getDeviceAt(viewRowIndex).getIcon());
+
+    myTable = (VirtualDeviceTable)table;
+    myDevice = myTable.getDeviceAt(viewRowIndex);
 
     return myButton;
   }
