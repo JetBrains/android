bd99471a2a983aaf2a73be2c878b17f5abf5d83c
Vihasith Patnana <vihasith@google.com>
2022-09-27 21:33:59 -0700

    To stable / fix flakiness for CreateNewAppModuleWithDefaultsTest To add namespace to SimpleApplication
    Bug: b/248635660, b/249420959
    Test: https://fusion2.corp.google.com/invocations/da5c0566-1e1d-4913-94c2-fae471f69fac
    Change-Id: I531014023cd169a1627bb932eada65eba55b008b
    
    GitOrigin-RevId: 65b402600550ace11e5b6eabf02dcefa151037bf

--- android-uitests/testSrc/com/android/tools/idea/tests/gui/gradle/CreateNewAppModuleWithDefaultsTest.java
+++ android-uitests/testSrc/com/android/tools/idea/tests/gui/gradle/CreateNewAppModuleWithDefaultsTest.java
@@ -15,12 +15,13 @@
  */
 package com.android.tools.idea.tests.gui.gradle;
 
+import com.android.tools.idea.gradle.project.build.BuildStatus;
 import com.android.tools.idea.tests.gui.framework.GuiTestRule;
 import com.android.tools.idea.tests.gui.framework.RunIn;
 import com.android.tools.idea.tests.gui.framework.TestGroup;
+import com.android.tools.idea.tests.gui.framework.fixture.IdeFrameFixture;
 import com.android.tools.idea.tests.gui.framework.fixture.npw.NewModuleWizardFixture;
 import com.intellij.testGuiFramework.framework.GuiTestRemoteRunner;
-import org.fest.swing.timing.Wait;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -29,17 +30,22 @@ import java.util.concurrent.TimeUnit;
 
 import static com.android.tools.idea.testing.FileSubject.file;
 import static com.google.common.truth.Truth.assertAbout;
-import static com.google.common.truth.Truth.assertThat;
+import static junit.framework.TestCase.assertTrue;
 
 @RunWith(GuiTestRemoteRunner.class)
 public class CreateNewAppModuleWithDefaultsTest {
 
-  @Rule public final GuiTestRule guiTest = new GuiTestRule().withTimeout(5, TimeUnit.MINUTES);
+  @Rule
+  public final GuiTestRule guiTest = new GuiTestRule().withTimeout(8, TimeUnit.MINUTES);
 
   /**
    * Verifies addition of new application module to application.
-   * <p>This is run to qualify releases. Please involve the test team in substantial changes.
-   * <p>TT ID: fd583b0a-bedd-4ec8-9207-70e4994ed761
+   * <p>
+   * This is run to qualify releases. Please involve the test team in substantial
+   * changes.
+   * <p>
+   * TT ID: fd583b0a-bedd-4ec8-9207-70e4994ed761
+   *
    * <pre>
    *   Test Steps
    *   1. Import 'SimpleApplication project and File -> New -> new module
@@ -52,27 +58,40 @@ public class CreateNewAppModuleWithDefaultsTest {
    *   3. Build is successful
    * </pre>
    */
+
   @RunIn(TestGroup.SANITY_BAZEL)
   @Test
   public void createNewAppModuleWithDefaults() throws Exception {
-    guiTest.importSimpleApplication()
-           .openFromMenu(NewModuleWizardFixture::find, "File", "New", "New Module\u2026")
-           .clickNextPhoneAndTabletModule()
-           .enterModuleName("application_module")
-           .wizard()
-           .clickNext()
-           .chooseActivity("Empty Activity")
-           .clickNext()
-           .clickFinishAndWaitForSyncToFinish(Wait.seconds(30));
+    guiTest.importSimpleApplication();
+    guiTest.waitForAllBackgroundTasksToBeCompleted();
+
+    IdeFrameFixture ideFrame = guiTest.ideFrame();
+
+    ideFrame.invokeMenuPath("File", "New", "New Module\u2026");
+
+    NewModuleWizardFixture createNewModule = NewModuleWizardFixture.find(ideFrame);
+    createNewModule.clickNextPhoneAndTabletModule()
+      .enterModuleName("application_module")
+      .wizard()
+      .clickNext()
+      .chooseActivity("Empty Activity")
+      .clickNext()
+      .clickFinishAndWaitForSyncToFinish();
+    guiTest.waitForAllBackgroundTasksToBeCompleted();
+
     assertAbout(file()).that(guiTest.getProjectPath("application_module")).isDirectory();
 
-    guiTest.ideFrame().getProjectView().assertFilesExist(
-      "/application_module/src/main/res/layout/activity_main.xml"
-    );
-    guiTest.ideFrame().getProjectView().assertFilesExist(
-      "/application_module/src/main/java/com/example/application_module/MainActivity.java"
-    );
+    ideFrame.getProjectView()
+      .assertFilesExist("/application_module/src/main/res/layout/activity_main.xml");
 
-    assertThat(guiTest.ideFrame().invokeProjectMake().isBuildSuccessful()).isTrue();
+    ideFrame.getProjectView()
+      .assertFilesExist("/application_module/src/main/java/com/example/application_module/MainActivity.java");
+
+    guiTest.waitForAllBackgroundTasksToBeCompleted();
+    ideFrame.requestFocusIfLost();
+
+    BuildStatus result = ideFrame.invokeProjectMake();
+    guiTest.waitForAllBackgroundTasksToBeCompleted();
+    assertTrue(result.isBuildSuccessful());
   }
 }
