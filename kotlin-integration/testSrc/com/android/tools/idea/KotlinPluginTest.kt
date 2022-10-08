fa89cc17e5a8c6e5fe82405138df238a9f8a8ea2
Matthew Gharrity <gharrma@google.com>
2022-10-07 17:28:01 -0700

    Add tests to verify our Kotlin IDE plugin version
    We assert the same invariants that JetBrains maintains
    for IntelliJ releases:
    
    * standaloneCompilerVersion is a release version
        (so that new projects use a stable Kotlin CLI compiler by default)
    
    * ideCompilerVersion >= standaloneCompilerVersion
        (so that the IDE understands at least as much as the CLI compiler)
    
    * The user-visible Kotlin IDE plugin version corresponds to
      standaloneCompilerVersion
    
    Bug: not filed
    Test: KotlinPluginTest
    Change-Id: Ib62967aa38fc6da695301f656993bbdcb308d5aa
    
    GitOrigin-RevId: 3095b96a9e3f65170dc64b04c2a29ffcfbeb9206

--- kotlin-integration/testSrc/com/android/tools/idea/KotlinPluginTest.kt
+++ kotlin-integration/testSrc/com/android/tools/idea/KotlinPluginTest.kt
@@ -15,16 +15,43 @@
  */
 package com.android.tools.idea
 
-import org.jetbrains.android.AndroidTestCase
+import com.google.common.truth.Truth.assertThat
+import com.intellij.openapi.application.ApplicationInfo
+import com.intellij.testFramework.ApplicationRule
+import org.jetbrains.kotlin.idea.compiler.configuration.KotlinIdePlugin
+import org.jetbrains.kotlin.idea.compiler.configuration.KotlinIdePluginVersion
+import org.jetbrains.kotlin.idea.compiler.configuration.KotlinPluginLayout
+import org.junit.ClassRule
+import org.junit.Test
 
-class KotlinPluginTest : AndroidTestCase() {
-  
-  fun testKotlinBundledVersionFormat() {
-    // This is a regression test for when Jetbrains accidentally released a Kotlin plugin with version 1.2.60-release-76,
-    // where the "-release-76" suffix should not have been there. This broke some code in Studio; for example, Studio would
-    // populate build.gradle with the wrong Kotlin version string, thereby breaking the search for Kotlin plugin artifacts.
+class KotlinPluginTest {
 
-    // Kotlin-Compose has dash in the version number
-    //assert(!bundledRuntimeVersion().contains('-'))
+  companion object {
+    @ClassRule
+    @JvmField
+    val appRule = ApplicationRule()
+  }
+
+  @Test
+  fun testKotlinVersion() {
+    val kotlinLayout = KotlinPluginLayout.instance
+
+    // The 'standalone' compiler version is defined by Kotlin/kotlinc/build.txt.
+    // This version is the default for new projects created in the IDE. So, it should generally be a release version.
+    val standaloneCompilerVersion = kotlinLayout.standaloneCompilerVersion
+    assertThat(standaloneCompilerVersion.isRelease).isTrue()
+
+    // The 'IDE' compiler version is defined by kotlin-plugin.jar!/META-INF/compiler.version.
+    // This version corresponds to the Kotlin compiler used for IDE analysis.
+    val ideCompilerVersion = kotlinLayout.ideCompilerVersion
+    assertThat(ideCompilerVersion.isSnapshot).isFalse()
+    assertThat(ideCompilerVersion.isDev).isFalse()
+    assertThat(ideCompilerVersion.kotlinVersion).isAtLeast(standaloneCompilerVersion.kotlinVersion)
+
+    // The Kotlin IDE plugin version is defined by the <version> tag in kotlin-plugin.jar!/META-INF/plugin.xml.
+    val idePluginVersion = KotlinIdePluginVersion.parse(KotlinIdePlugin.version).getOrThrow()
+    assertThat(idePluginVersion.isAndroidStudio).isTrue()
+    assertThat(idePluginVersion.kotlinCompilerVersion).isEqualTo(standaloneCompilerVersion)
+    assertThat(idePluginVersion.platformVersion).isEqualTo(ApplicationInfo.getInstance().shortVersion)
   }
 }
