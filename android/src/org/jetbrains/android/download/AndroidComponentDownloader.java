300b8c0cac5c8a0010cd0a99e9f7a9d3a199fd70
andrei.kuznetsov <andrei.kuznetsov@jetbrains.com>
2022-10-15 19:25:33 +0200

    Update android's downloadable components versions
    GitOrigin-RevId: 1dda02f3188e86002bdded822afeddc7ea3fa22b

--- android/src/org/jetbrains/android/download/AndroidComponentDownloader.java
+++ android/src/org/jetbrains/android/download/AndroidComponentDownloader.java
@@ -32,7 +32,7 @@ import org.jetbrains.annotations.NotNull;
 public abstract class AndroidComponentDownloader {
 
   private static final Logger LOG = Logger.getInstance(AndroidComponentDownloader.class);
-  private static final String VERSION = "221.0.9.0";
+  private static final String VERSION = "221.0.12.0";
   public static final String BINTRAY_ANDROID_TOOLS_BASE =
     "https://cache-redirector.jetbrains.com/intellij-dependencies/org/jetbrains/intellij/deps/android/tools/base/";
   public static final String ZIP = "zip";
