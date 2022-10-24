4189fdbe6dc546383e16786fb0343930125a6dd2
Diego Perez <diegoperez@google.com>
2022-10-24 10:04:06 +0000

    Re-enable LiveLiteralsServiceTest
    Bug: 194631917
    Test: bazel test --config=remote --runs_per_test=3000 --notest_keep_going //tools/adt/idea/android:intellij.android.core.tests_tests__editors.literals
    Change-Id: I2b46a5b0e316cd883143240bd9fc671b6a77d17e
    
    GitOrigin-RevId: 0be17327d7fdd9cbc5b9b927cc927117384d48c3

--- android/testSrc/com/android/tools/idea/editors/literals/LiveLiteralsServiceTest.kt
+++ android/testSrc/com/android/tools/idea/editors/literals/LiveLiteralsServiceTest.kt
@@ -22,7 +22,6 @@ import org.junit.Assert.assertEquals
 import org.junit.Assert.assertFalse
 import org.junit.Assert.assertTrue
 import org.junit.Before
-import org.junit.Ignore
 import org.junit.Rule
 import org.junit.Test
 import java.util.concurrent.CountDownLatch
@@ -107,7 +106,6 @@ internal class LiveLiteralsServiceTest {
     }
   }
 
-  @Ignore("b/194631917")
   @Test
   fun `check that already open editors register constants`() {
     projectRule.fixture.configureFromExistingVirtualFile(file1.virtualFile)
@@ -121,7 +119,6 @@ internal class LiveLiteralsServiceTest {
     assertEquals(9, liveLiteralsService.allConstants().size)
   }
 
-  @Ignore("b/194631917")
   @Test
   fun `check that constants are registered after a new editor is opened`() {
     val liveLiteralsService = getTestLiveLiteralsService()
@@ -146,7 +143,6 @@ internal class LiveLiteralsServiceTest {
     assertEquals(9, liveLiteralsService.allConstants().size)
   }
 
-  @Ignore("b/194631917")
   @Test
   fun `listener notification`() {
     // Setup
