b4c2cd6314516c97792a864ed172eec7a1dac19e
Andy Magee <andymagee@google.com>
2022-10-24 12:22:28 -0700

    Set composable function icon as default
    Updated screenshot with icon: https://screenshot.googleplex.com/55MU6yPzrvnc8gT
    
    This is a follow-on to ag/20260444, making the menu items for
    Composables a bit more distinct by using the same icon we already use
    for the methods in auto-suggest.
    
    Bug: 254887357
    Test: ComposableIconProviderTest
    Change-Id: I6a854ed18a1190931c81d1e386e6aff5fed01b00
    
    GitOrigin-RevId: 601c3187cef4b68f7910863eb6b9d1dda3300db6

---
+++ compose-ide-plugin/src/com/android/tools/compose/ComposableIconProvider.kt
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.compose
+
+import com.intellij.openapi.util.Iconable
+import com.intellij.psi.PsiElement
+import com.intellij.ui.RowIcon
+import icons.StudioIcons.Compose.Editor.COMPOSABLE_FUNCTION
+import org.jetbrains.kotlin.idea.KotlinIconProviderBase
+import org.jetbrains.kotlin.idea.search.usagesSearch.descriptor
+import org.jetbrains.kotlin.psi.KtFunction
+import javax.swing.Icon
+
+/**
+ * Returns Composable function icon for [KtFunction] elements that are composable, and fall back to the Kotlin plugin's default
+ * [KotlinIconProviderBase] otherwise. This may be used in various places across the IDE; one example is in the "Add Import" menu.
+ */
+class ComposableIconProvider : KotlinIconProviderBase() {
+
+  override fun getIcon(psiElement: PsiElement, flags: Int): Icon? {
+    if (psiElement is KtFunction && psiElement.descriptor?.hasComposableAnnotation() == true) {
+      if (flags and Iconable.ICON_FLAG_VISIBILITY > 0) {
+        return createRowIcon(COMPOSABLE_FUNCTION, getVisibilityIcon(psiElement.modifierList))
+      }
+
+      return COMPOSABLE_FUNCTION
+    }
+
+    return super.getIcon(psiElement, flags)
+  }
+
+  private fun createRowIcon(baseIcon: Icon, visibilityIcon: Icon): RowIcon = RowIcon(2).apply {
+    setIcon(baseIcon, /* layer = */ 0)
+    setIcon(visibilityIcon, /* layer = */ 1)
+  }
+}
---
+++ compose-ide-plugin/testSrc/com/android/tools/compose/ComposableIconProviderTest.kt
@@ -0,0 +1,137 @@
+/*
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.compose
+
+import com.android.tools.idea.testing.AndroidProjectRule
+import com.google.common.truth.Truth.assertThat
+import com.intellij.icons.AllIcons
+import com.intellij.openapi.application.runReadAction
+import com.intellij.openapi.util.Iconable
+import com.intellij.ui.RowIcon
+import icons.StudioIcons.Compose.Editor.COMPOSABLE_FUNCTION
+import org.jetbrains.kotlin.idea.KotlinFileType
+import org.jetbrains.kotlin.idea.KotlinIconProviderBase
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@RunWith(JUnit4::class)
+class ComposableIconProviderTest {
+  @get:Rule
+  var projectRule = AndroidProjectRule.inMemory()
+
+  private val provider = ComposableIconProvider()
+
+  @Before
+  fun setup() {
+    // Allow @Composable attribute to be used in snippets below.
+    projectRule.fixture.addFileToProject("androidx/compose/runtime/Composable.kt", """
+package androidx.compose.runtime
+
+annotation class Composable""")
+  }
+
+  @Test
+  fun getPresentation_notAFunction() {
+    projectRule.fixture.configureByText(KotlinFileType.INSTANCE, """
+package com.example
+
+val fo<caret>o = 1234
+
+""")
+
+    runReadAction {
+      val element = projectRule.fixture.elementAtCaret
+
+      // Code should fall back to KotlinIconProviderBase. Rather than hardcoding specific icon traits that might change, just verify it
+      // matches a direct call.
+      val expectedIcon = KotlinIconProviderBase().getIcon(element, flags = 0)
+      val actualIcon = provider.getIcon(element, flags = 0)
+
+      assertThat(actualIcon).isEqualTo(expectedIcon)
+      assertThat(actualIcon).isNotEqualTo(COMPOSABLE_FUNCTION)
+    }
+  }
+
+  @Test
+  fun getPresentation_notComposeFunction() {
+    projectRule.fixture.configureByText(KotlinFileType.INSTANCE, """
+package com.example
+
+fun testFun<caret>ction() {}
+
+""")
+
+    runReadAction {
+      val element = projectRule.fixture.elementAtCaret
+
+      // Code should fall back to KotlinIconProviderBase. Rather than hardcoding specific icon traits that might change, just verify it
+      // matches a direct call.
+      val expectedIcon = KotlinIconProviderBase().getIcon(element, flags = 0)
+      val actualIcon = provider.getIcon(element, flags = 0)
+
+      assertThat(actualIcon).isEqualTo(expectedIcon)
+      assertThat(actualIcon).isNotEqualTo(COMPOSABLE_FUNCTION)
+    }
+  }
+
+  @Test
+  fun getPresentation_composeFunctionWithVisibility() {
+    projectRule.fixture.configureByText(KotlinFileType.INSTANCE, """
+package com.example
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun testFun<caret>ction() {}
+
+""")
+
+    runReadAction {
+      val element = projectRule.fixture.elementAtCaret
+
+      val icon = provider.getIcon(element, flags = Iconable.ICON_FLAG_VISIBILITY)
+
+      assertThat(icon).isInstanceOf(RowIcon::class.java)
+      val rowIcon = icon as RowIcon
+      assertThat(rowIcon.iconCount).isEqualTo(2)
+      assertThat(rowIcon.getIcon(0)).isEqualTo(COMPOSABLE_FUNCTION)
+      assertThat(rowIcon.getIcon(1)).isEqualTo(AllIcons.Nodes.C_public)
+    }
+  }
+
+  @Test
+  fun getPresentation_composeFunctionWithoutVisibility() {
+    projectRule.fixture.configureByText(KotlinFileType.INSTANCE, """
+package com.example
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun testFun<caret>ction() {}
+
+""")
+
+    runReadAction {
+      val element = projectRule.fixture.elementAtCaret
+
+      val icon = provider.getIcon(element, flags = 0)
+      assertThat(icon).isEqualTo(COMPOSABLE_FUNCTION)
+    }
+  }
+}
