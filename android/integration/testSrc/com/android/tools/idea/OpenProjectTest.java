1289add5abb9933a3e88f8166406920a7ab80f5e
Alexander Demurov <ademurov@google.com>
2022-10-28 15:52:28 -0700

    Add perfgate benchmarks for threading agent
    Uses OpenProject integration test to measure how long it takes to open an android studio project with the threading agent disabled vs threading agent enabled. Adds this data to perfgate.
    
    Bug: 228841408
    Test: tested that perfgate json files are added as test artifacts
    Change-Id: Ic33b3566f933d6cf54de50e2f6f61d026c5760b8
    
    GitOrigin-RevId: 4d4a24e6620938f488fcb2d739acdee6de30ab2f

--- android/integration/testSrc/com/android/tools/idea/OpenProjectTest.java
+++ android/integration/testSrc/com/android/tools/idea/OpenProjectTest.java
@@ -17,17 +17,42 @@ package com.android.tools.idea;
 
 import com.android.tools.asdriver.tests.AndroidProject;
 import com.android.tools.asdriver.tests.AndroidStudio;
+import com.android.tools.asdriver.tests.AndroidStudioInstallation;
 import com.android.tools.asdriver.tests.AndroidSystem;
 import com.android.tools.asdriver.tests.MavenRepo;
+import com.android.tools.perflogger.Benchmark;
+import com.android.tools.perflogger.PerfData;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+
 
 public class OpenProjectTest {
   @Rule
   public AndroidSystem system = AndroidSystem.standard();
 
+  private Benchmark benchmark;
+  private long startTimeMs;
+
+  @Before
+  public void setUp() throws Exception {
+    benchmark = createBenchmark(shouldDisableThreadingAgent());
+    startTimeMs = System.currentTimeMillis();
+  }
+
+  @After
+  public void tearDown() {
+    benchmark.log("total_time", System.currentTimeMillis() - startTimeMs);
+  }
+
   @Test
   public void openProjectTest() throws Exception {
+    AndroidStudioInstallation installation = system.getInstallation();
+    if (shouldDisableThreadingAgent()) {
+      installation.addVmOption("-Dandroid.studio.instrumentation.threading.agent.disable=true");
+    }
+
     // Create a new android project, and set a fixed distribution
     AndroidProject project = new AndroidProject("tools/adt/idea/android/integration/testData/minapp");
     project.setDistribution("tools/external/gradle/gradle-7.2-bin.zip");
@@ -38,5 +63,23 @@ public class OpenProjectTest {
     try (AndroidStudio studio = system.runStudio(project)) {
       studio.waitForSync();
     }
+  }
+
+  private static Benchmark createBenchmark(boolean threadingAgentDisabled) throws Exception {
+    String benchmarkName = threadingAgentDisabled ? "OpenProject-withAgentDisabled" : "OpenProject-withAgentEnabled";
+    PerfData perfData = new PerfData();
+
+    Benchmark benchmark =
+      new Benchmark.Builder(benchmarkName)
+        .setProject("Android Studio Threading Agent")
+        .build();
+    perfData.addBenchmark(benchmark);
+    perfData.commit();
+
+    return benchmark;
+  }
+
+  private static boolean shouldDisableThreadingAgent() {
+    return "true".equalsIgnoreCase(System.getProperty("threading_agent.disabled"));
   }
 }
