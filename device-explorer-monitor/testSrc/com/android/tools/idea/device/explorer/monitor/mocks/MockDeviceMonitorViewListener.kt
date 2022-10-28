4fc71d4da9af3f0d22c61b878dd67c9a52a2b93e
Chrystian Bueno <chrysbueno@google.com>
2022-10-28 13:55:17 -0700

    Add test module for Device Monitor and copy over tests.
    Fixes: N/A
    Test: DeviceMonitorTestSuite
    Change-Id: Ic68262112a39b7fecf8560189ba58810010ab346
    
    GitOrigin-RevId: f72cdecd2740748c3abc2286cb748943b21a0695

---
+++ device-explorer-monitor/testSrc/com/android/tools/idea/device/explorer/monitor/mocks/MockDeviceMonitorViewListener.kt
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.device.explorer.monitor.mocks
+
+import com.android.tools.idea.FutureValuesTracker
+import com.android.tools.idea.device.explorer.monitor.DeviceMonitorViewListener
+import com.android.tools.idea.device.explorer.monitor.ProcessTreeNode
+import com.android.tools.idea.device.explorer.monitor.processes.Device
+
+class MockDeviceMonitorViewListener : DeviceMonitorViewListener {
+  val deviceNotSelectedTracker = FutureValuesTracker<Unit>()
+  val deviceSelectedTracker = FutureValuesTracker<Device>()
+  val treeNodeExpandingTracker = FutureValuesTracker<ProcessTreeNode>()
+
+  override fun noDeviceSelected() {
+    deviceNotSelectedTracker.produce(null)
+  }
+
+  override fun deviceSelected(device: Device) {
+    deviceSelectedTracker.produce(device)
+  }
+
+  override fun treeNodeExpanding(treeNode: ProcessTreeNode) {
+    treeNodeExpandingTracker.produce(treeNode)
+  }
+
+  override fun refreshInvoked() {}
+
+  override fun killNodesInvoked(nodes: List<ProcessTreeNode>) {}
+
+  override fun forceStopNodesInvoked(nodes: List<ProcessTreeNode>) {}
+}
