9db44edfebd0d447dc2a1492d4ae36c222154095
Andy Magee <andymagee@google.com>
2022-10-28 10:34:43 -0700

    Adjust "Add Imports" list order for Composables
    When in a Compose-enabled module, we should bump Composable functions up
    the "Add Imports" list.
    
    Additionally, "Modifier" is not a Composable, but should be promoted up
    the list as well. Adding a mechanism to manually promote specific items
    up the import list.
    
    Bug: N/A
    Test: ComposeProximityWeigherTest
    Change-Id: I73cbb4758ebd8863b440eb22ecb19c3f714eefdc
    
    GitOrigin-RevId: 7cc4fbf69b69f51ae35bf5ef2d2b226f07cb6d6f

---
+++ compose-ide-plugin/src/com/android/tools/compose/code/actions/ComposeProximityWeigher.kt
@@ -0,0 +1,59 @@
+/*
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.compose.code.actions
+
+import com.android.tools.compose.isComposableFunction
+import com.android.tools.idea.projectsystem.getModuleSystem
+import com.intellij.psi.PsiElement
+import com.intellij.psi.util.ProximityLocation
+import com.intellij.psi.util.proximity.ProximityWeigher
+import org.jetbrains.kotlin.idea.KotlinLanguage
+import org.jetbrains.kotlin.psi.KtNamedDeclaration
+
+/** This weigher is used in determining the order of items in Kotlin's "Add Imports" list. */
+class ComposeProximityWeigher: ProximityWeigher() {
+  override fun weigh(element: PsiElement, location: ProximityLocation): Int? {
+    if (location.position?.language != KotlinLanguage.INSTANCE) return null
+    if (location.positionModule?.getModuleSystem()?.usesCompose != true) return null
+
+    // In the "Add Import" case, `location` unfortunately points only to the file we're in -- so we can't do anything smart that would take
+    // into account the position of the code that needs the import.
+
+    // If we've manually weighted this element, use that weight.
+    element.manualWeight()?.let { return it }
+
+    if (element.isComposableFunction()) return COMPOSABLE_METHOD_WEIGHT
+
+    return DEFAULT_WEIGHT
+  }
+}
+
+// Order imports as follows:
+// [Weight 2] Any FQ we are promoting
+// [Weight 1] Any Composable method
+// [Weight 0] Default case
+private const val PROMOTED_WEIGHT = 2
+private const val COMPOSABLE_METHOD_WEIGHT = 1
+private const val DEFAULT_WEIGHT = 0
+
+private fun PsiElement.manualWeight(): Int? {
+  val fqName = (this as? KtNamedDeclaration)?.fqName?.asString() ?: return null
+  return MANUALLY_WEIGHTED_FQ_NAMES[fqName]
+}
+
+private val MANUALLY_WEIGHTED_FQ_NAMES = mapOf(
+  "androidx.compose.ui.Modifier" to PROMOTED_WEIGHT,
+)
---
+++ compose-ide-plugin/testSrc/com/android/tools/compose/code/actions/ComposeProximityWeigherTest.kt
@@ -0,0 +1,187 @@
+/*
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.compose.code.actions
+
+import com.android.tools.compose.ComposeFqNames
+import com.android.tools.idea.project.DefaultModuleSystem
+import com.android.tools.idea.projectsystem.getModuleSystem
+import com.android.tools.idea.testing.AndroidProjectRule
+import com.android.tools.idea.testing.caret
+import com.android.tools.idea.testing.getIntentionAction
+import com.android.tools.idea.testing.loadNewFile
+import com.google.common.truth.Truth.assertThat
+import com.intellij.openapi.application.runInEdt
+import com.intellij.openapi.application.runReadAction
+import com.intellij.psi.PsiElement
+import com.intellij.psi.util.ProximityLocation
+import com.intellij.psi.util.PsiTreeUtil
+import com.intellij.testFramework.fixtures.CodeInsightTestFixture
+import org.jetbrains.android.compose.stubComposableAnnotation
+import org.jetbrains.kotlin.psi.KtNamedFunction
+import org.jetbrains.kotlin.psi.KtObjectDeclaration
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+/**
+ * Tests for [ComposeProximityWeigher].
+ *
+ * Unfortunately it's not possible to validate the order of items in the "Add Imports" list end to end in a unit test, because most of the
+ * logic involved is internal in the Kotlin plugin. We can execute the correct intention, but in a unit test it will not pop up a dialog,
+ * instead just selecting the first item.
+ *
+ * This file contains one test that validates that the Weigher is correctly wired up, by validating that the intention results in an import
+ * being added that wouldn't have been used if [ComposeProximityWeigher] isn't running. The remaining tests are more traditional unit tests,
+ * working directly with [ComposeProximityWeigher] outside the context of the intention.
+ */
+@RunWith(JUnit4::class)
+class ComposeProximityWeigherTest {
+  @get:Rule
+  val projectRule = AndroidProjectRule.inMemory()
+
+  private val myFixture: CodeInsightTestFixture by lazy { projectRule.fixture }
+
+  @Before
+  fun setUp() {
+    (myFixture.module.getModuleSystem() as DefaultModuleSystem).usesCompose = true
+    myFixture.stubComposableAnnotation(ComposeFqNames.root)
+  }
+
+  @Test
+  fun validateWeigherIsWiredUp() {
+    // When two imports have equal weight (which will be the default in the unit test), the Kotlin plugin's logic falls back to using
+    // lexicographic ordering. This "Modifier" class will begin with "aaa", and thus would be listed first absent [ComposeProximityWeigher]
+    // changing the order.
+    myFixture.addFileToProject(
+      "src/aaa/example/foo/Modifier.kt",
+      // language=kotlin
+      """
+      package aaa.example.foo
+
+      interface Modifier
+      """)
+
+    myFixture.addFileToProject(
+      "src/androidx/compose/ui/Modifier.kt",
+      // language=kotlin
+      """
+      package androidx.compose.ui
+
+      interface Modifier
+      """)
+
+    val psiFile = myFixture.loadNewFile(
+      "src/com/example/Test.kt",
+      // language=kotlin
+      """
+      package com.example
+
+      import androidx.compose.runtime.Composable
+
+      @Composable
+      fun HomeScreen(modifier: Mod${caret}ifier) {}
+      """.trimIndent()
+    )
+
+    val action = myFixture.getIntentionAction("Import")!!
+    runInEdt {
+      action.invoke(myFixture.project, myFixture.editor, psiFile)
+    }
+
+    myFixture.checkResult(
+      // language=kotlin
+      """
+      package com.example
+
+      import androidx.compose.runtime.Composable
+      import androidx.compose.ui.Modifier
+
+      @Composable
+      fun HomeScreen(modifier: Mod${caret}ifier) {}
+      """.trimIndent()
+    )
+  }
+
+  @Test
+  fun validateBasicOrdering() {
+    val composableFunction = addFileAndFindElement(
+      "src/com/example/composable/ComposableFunction.kt",
+      // language=kotlin
+      """
+      package com.example.composable
+
+      import androidx.compose.runtime.Composable
+
+      @Composable
+      fun ComposableFunction()
+      """.trimIndent(),
+      "ComposableFunction",
+      KtNamedFunction::class.java
+    )
+
+    val nonComposableFunction = addFileAndFindElement(
+      "src/com/example/noncomposable/NonComposableFunction.kt",
+      // language=kotlin
+      """
+      package com.example.noncomposable
+
+      fun NonComposableFunction()
+      """.trimIndent(),
+      "NonComposableFunction",
+      KtNamedFunction::class.java
+    )
+
+    val manuallyWeightedElement = addFileAndFindElement(
+      "src/androidx/compose/ui/Modifier.kt",
+      // language=kotlin
+      """
+      package androidx.compose.ui
+
+      object Modifier
+      """.trimIndent(),
+      "Modifier",
+      KtObjectDeclaration::class.java
+    )
+
+    val locationFile = myFixture.loadNewFile(
+      "src/com/example/Test.kt",
+      // language=kotlin
+      """
+      package com.example
+      """.trimIndent()
+    )
+
+    val proximityLocation = ProximityLocation(locationFile, myFixture.module)
+    val sortedList = runReadAction {
+      listOf(nonComposableFunction, composableFunction, manuallyWeightedElement)
+        .sortedByDescending { element -> ComposeProximityWeigher().weigh(element, proximityLocation) }
+    }
+
+    assertThat(sortedList).containsExactly(manuallyWeightedElement, composableFunction, nonComposableFunction).inOrder()
+  }
+
+  fun <T : PsiElement> addFileAndFindElement(relativePath: String,
+                                             fileText: String,
+                                             targetElementText: String,
+                                             targetElementClass: Class<T>): T {
+    val psiFile = myFixture.addFileToProject(relativePath, fileText)
+    return runReadAction {
+      PsiTreeUtil.getParentOfType(psiFile.findElementAt(psiFile.text.indexOf(targetElementText)), targetElementClass)!!
+    }
+  }
+}
