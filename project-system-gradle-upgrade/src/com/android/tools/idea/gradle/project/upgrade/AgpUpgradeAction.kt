4b69e64bec370c6617688dc6da7bc4cde9e6fd40
Christophe Rhodes <xof@google.com>
2022-10-31 17:05:31 +0000

    [AUA] Simplify the AGP Upgrade Assistant toolbar entry
    Although it is a refactoring action in some sense, it's not a
    refactoring action in the sense of operating on its direct context,
    which is apparently what BaseRefactoringAction is attempting to
    simplify (to identify contexts in which the action should or should
    not be offered).  Instead, simply inherit from AnAction.
    
    Fixes: 256229057
    Test: manual
    Change-Id: Ie1ed484056d7971cf33079fffec770fc60c6d5c8
    
    GitOrigin-RevId: f20c7bb43ac0b28af70a3f0607357468279d1f27

--- project-system-gradle-upgrade/src/com/android/tools/idea/gradle/project/upgrade/AgpUpgradeAction.kt
+++ project-system-gradle-upgrade/src/com/android/tools/idea/gradle/project/upgrade/AgpUpgradeAction.kt
@@ -18,29 +18,15 @@ package com.android.tools.idea.gradle.project.upgrade
 import com.android.ide.common.repository.AgpVersion
 import com.android.tools.idea.gradle.plugin.LatestKnownPluginVersionProvider
 import com.android.tools.idea.gradle.repositories.IdeGoogleMavenRepository
-import com.intellij.openapi.actionSystem.DataContext
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.application.ModalityState
 import com.intellij.openapi.application.invokeLater
-import com.intellij.openapi.editor.Editor
-import com.intellij.openapi.project.Project
-import com.intellij.psi.PsiElement
-import com.intellij.psi.PsiFile
-import com.intellij.refactoring.RefactoringActionHandler
-import com.intellij.refactoring.actions.BaseRefactoringAction
 
-class AgpUpgradeAction: BaseRefactoringAction() {
-  override fun isAvailableInEditorOnly() = false
-  override fun isEnabledOnDataContext(dataContext: DataContext) = true
-  override fun isEnabledOnElements(elements: Array<out PsiElement>) = true
-  override fun getHandler(dataContext: DataContext): RefactoringActionHandler? = AgpUpgradeActionHandler()
-}
-
-class AgpUpgradeActionHandler: RefactoringActionHandler {
-  override fun invoke(project: Project, editor: Editor?, file: PsiFile?, dataContext: DataContext?) = invoke(project)
-  override fun invoke(project: Project, elements: Array<out PsiElement>, dataContext: DataContext?)  = invoke(project)
-
-  fun invoke(project: Project) {
+class AgpUpgradeAction: AnAction() {
+  override fun actionPerformed(e: AnActionEvent) {
+    val project = e.project ?: return
     val current = project.findPluginInfo()?.pluginVersion ?: return
     val latestKnown = AgpVersion.parse(LatestKnownPluginVersionProvider.INSTANCE.get())
     ApplicationManager.getApplication().executeOnPooledThread {
