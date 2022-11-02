a31afd7aa838c585cef0ebacd265ddd6339b7cff
Diego Perez <diegoperez@google.com>
2022-11-02 18:46:55 +0000

    Increase Component search timeout
    Test: ComposePreviewKotlinTest
    Bug: 247532990
    Change-Id: Ibfeb69a12220732163de9ec7993bc75c2ba16cd4
    
    GitOrigin-RevId: 7d847f9e04979a420cbbed9018d392146035db68

--- as-driver/agent/src/com/android/tools/asdriver/inject/StudioInteractionService.java
+++ as-driver/agent/src/com/android/tools/asdriver/inject/StudioInteractionService.java
@@ -160,9 +160,9 @@ public class StudioInteractionService {
   public void waitForComponent(List<ASDriver.ComponentMatcher> matchers) throws InterruptedException, TimeoutException, InvocationTargetException {
     log("Attempting to wait for a component with matchers: " + matchers);
     // TODO(b/234067246): consider this timeout when addressing b/234067246. This particular
-    // timeout is so high because ComposePreviewKotlin performs a Gradle build, and that takes >4m
+    // timeout is so high because ComposePreviewKotlin performs a Gradle build, and that takes >10m
     // sometimes.
-    int timeoutMillis = 360000;
+    final long timeoutMillis = java.time.Duration.ofMinutes(10).toMillis();
     long msBetweenRetries = 300;
     long startTime = System.currentTimeMillis();
     long elapsedTime = 0;
