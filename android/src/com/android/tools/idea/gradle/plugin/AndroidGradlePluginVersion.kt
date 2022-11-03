79d12e35d63b7df7de6d27569c1f451f5d925f0a
Nebojsa Vuksic <nebojsa.vuksic@jetbrains.com>
2022-11-03 15:28:03 +0100

    IDEA-305252: Ensure the latest stable AGP version is used in stable IntelliJ Idea builds
    Always use the latest stable AGP version when creating Android project in Intellij Idea. To ensure this, LastKnownPluginVersionProvider know checks if the run build is Intellij Idea, and if that is the case we always use the latest stable AGP version from AndroidGradlePluginVersion class.
    
    GitOrigin-RevId: b6a6a5b4f0eb058073dbc221d857385cd2482c52

---
+++ android/src/com/android/tools/idea/gradle/plugin/AndroidGradlePluginVersion.kt
@@ -0,0 +1,6 @@
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.android.tools.idea.gradle.plugin
+
+object AndroidGradlePluginVersion {
+  const val LATEST_STABLE_VERSION = "7.3.0"
+}
