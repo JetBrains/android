4ce6592bc664c12359b10eaee97e4d0d9ed2bc1d
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2022-11-14 05:58:05 +0100

    default implementation of disposeWidget
    (cherry picked from commit e1ccfc29b191dfcdf9c2ccf0073b46b66cf2089e)
    
    Change-Id: Ie1ccfc29b191dfcdf9c2ccf0073b46b66cf2089e
    
    GitOrigin-RevId: 879aede17fff35336644dd4896261911707c6d9a

--- android/src/com/android/tools/idea/stats/SendFeedbackWidgetFactory.kt
+++ android/src/com/android/tools/idea/stats/SendFeedbackWidgetFactory.kt
@@ -16,7 +16,6 @@
 package com.android.tools.idea.stats
 
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.util.Disposer
 import com.intellij.openapi.wm.StatusBar
 import com.intellij.openapi.wm.StatusBarWidget
 import com.intellij.openapi.wm.StatusBarWidgetFactory
@@ -28,7 +27,6 @@ class SendFeedbackWidgetFactory
   override fun getDisplayName() = "Send Feedback to Google"
   override fun isAvailable(project: Project) = true
   override fun createWidget(project: Project): StatusBarWidget = SendFeedbackPanel()
-  override fun disposeWidget(widget: StatusBarWidget) = Disposer.dispose(widget)
   override fun canBeEnabledOn(statusBar: StatusBar) = true
 }
 
