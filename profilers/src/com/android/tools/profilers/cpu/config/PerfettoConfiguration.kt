87eefeda7b7f00705bca6283f787dde12b767eea
Yasser Dbeis <ydbeis@google.com>
2022-11-15 12:06:59 -0800

    Add back slider to perfetto profiling config
    In ag/20404069, PerfettoConfiguration was
    removed as a child of AtraceConfiguration and thus
    lost some inherited behavior. One of which was that
    the buffer size would be determined via a slider in
    the custom profiling configuration settings. This is
    now added back so the user may customize the system
    trace buffer size.
    
    Fixes: 259285311
    Test: manual
    Change-Id: I1df7fea720fda0f4cefab4ac813637b9f78c7efe
    
    GitOrigin-RevId: 64bc5efe2ed6489eb66381b9bb2fa34fc0ab6e1a

--- profilers/src/com/android/tools/profilers/cpu/config/PerfettoConfiguration.kt
+++ profilers/src/com/android/tools/profilers/cpu/config/PerfettoConfiguration.kt
@@ -16,6 +16,8 @@
 package com.android.tools.profilers.cpu.config
 
 import com.android.sdklib.AndroidVersion
+import com.android.tools.adtui.model.options.OptionsProperty
+import com.android.tools.adtui.model.options.Slider
 import com.android.tools.profiler.proto.Trace
 import perfetto.protos.PerfettoConfig.AndroidPowerConfig
 import perfetto.protos.PerfettoConfig.DataSourceConfig
@@ -26,6 +28,10 @@ import perfetto.protos.PerfettoConfig.TraceConfig
  * Configuration for Perfetto traces.
  */
 class PerfettoConfiguration(name: String) : ProfilingConfiguration(name) {
+  @Slider(min = 1, max = 32, step = 1)
+  @OptionsProperty(group = TRACE_CONFIG_GROUP, order = 100, name = "Buffer size limit:",
+                   description = "In memory buffer size for capturing trace events.",
+                   unit = "Mb")
   var profilingBufferSizeInMb = DEFAULT_BUFFER_SIZE_MB
   var appPkgName: String = ""
 
