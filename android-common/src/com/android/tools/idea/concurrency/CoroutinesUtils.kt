9c13c8c07ac19accf0425c02dcfa87ba504491ef
Diego Perez <diegoperez@google.com>
2022-11-21 12:15:33 +0000

    Add new flow to detect PSI file changes
    Test: FlowsTest
    Bug: N/A
    Change-Id: I932b468af128d9540eacae3800d151ff1a37f5bd
    
    GitOrigin-RevId: 5f993affcc94f9e47d266b4417f5d1730b5031b0

--- android-common/src/com/android/tools/idea/concurrency/CoroutinesUtils.kt
+++ android-common/src/com/android/tools/idea/concurrency/CoroutinesUtils.kt
@@ -39,6 +39,7 @@ import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.openapi.wm.ex.ProgressIndicatorEx
 import com.intellij.psi.PsiFile
 import com.intellij.psi.PsiManager
+import com.intellij.psi.PsiTreeAnyChangeAbstractAdapter
 import com.intellij.util.concurrency.AppExecutorUtil
 import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.CompletableDeferred
@@ -60,6 +61,7 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.ensureActive
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.distinctUntilChangedBy
 import kotlinx.coroutines.isActive
 import kotlinx.coroutines.job
 import kotlinx.coroutines.launch
@@ -533,3 +535,23 @@ fun smartModeFlow(project: Project, parentDisposable: Disposable, logger: Logger
  */
 fun smartModeFlow(project: Project, parentDisposable: Disposable, logger: Logger? = null): Flow<Unit> =
   smartModeFlow(project, parentDisposable, logger, null)
+
+
+/**
+ * A [callbackFlow] that produces an element when a [PsiFile] changes.
+ */
+fun psiFileChangeFlow(project: Project, parentDisposable: Disposable, logger: Logger? = null, onConnected: (() -> Unit)? = null): Flow<PsiFile> =
+  disposableCallbackFlow<PsiFile>(debugName = "PsiFileChangeFlow", parentDisposable = parentDisposable, logger = logger) {
+    PsiManager.getInstance(project).addPsiTreeChangeListener(
+      object : PsiTreeAnyChangeAbstractAdapter() {
+        override fun onChange(changedFile: PsiFile?) {
+          if (changedFile == null) return
+          trySend(changedFile)
+        }
+      },
+      this.disposable
+    )
+
+    onConnected?.let { onConnected -> launch(workerThread) { onConnected() } }
+  }
+    .distinctUntilChangedBy { file -> file.modificationStamp }
---
+++ android/testSrc/com/android/tools/idea/concurrency/FlowsTest.kt
@@ -0,0 +1,86 @@
+/*
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.concurrency
+
+import com.android.tools.idea.concurrency.AndroidDispatchers.workerThread
+import com.android.tools.idea.testing.AndroidProjectRule
+import com.android.tools.idea.testing.executeAndSave
+import com.android.tools.idea.testing.insertText
+import com.android.tools.idea.ui.ApplicationUtils
+import com.intellij.openapi.application.ModalityState
+import com.intellij.openapi.command.WriteCommandAction
+import com.intellij.psi.PsiManager
+import kotlinx.coroutines.CompletableDeferred
+import kotlinx.coroutines.TimeoutCancellationException
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.take
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withTimeout
+import org.assertj.core.api.Assertions
+import org.junit.Rule
+import org.junit.Test
+import kotlin.time.Duration.Companion.seconds
+import kotlin.time.ExperimentalTime
+
+class FlowsTest {
+
+  @get:Rule
+  val projectRule = AndroidProjectRule.inMemory()
+
+  @OptIn(ExperimentalTime::class)
+  @Test
+  fun `test psiFileChangeFlow`() {
+    val psiFile = projectRule.fixture.addFileToProject("src/Test.kt", """
+      fun test() {
+      }
+    """.trimIndent())
+
+    runBlocking {
+      val flowReady = CompletableDeferred<Unit>()
+      launch(workerThread) {
+        withTimeout(20.seconds) {
+          try {
+            psiFileChangeFlow(projectRule.project, projectRule.testRootDisposable) { flowReady.complete(Unit) }
+              .take(3)
+              .filter { PsiManager.getInstance(projectRule.project).areElementsEquivalent(psiFile, it) }
+              .collect()
+          } catch (_: TimeoutCancellationException) {
+            Assertions.fail("Timeout waiting for the changes")
+          }
+        }
+      }
+
+      flowReady.await()
+      ApplicationUtils.invokeWriteActionAndWait(ModalityState.defaultModalityState()) {
+        projectRule.fixture.openFileInEditor(psiFile.virtualFile)
+      }
+
+      // Make 3 changes that should trigger *at least* 3 flow elements
+      WriteCommandAction.runWriteCommandAction(projectRule.project) {
+        projectRule.fixture.editor.executeAndSave { insertText(" // Comment\n") }
+      }
+      WriteCommandAction.runWriteCommandAction(projectRule.project) {
+        projectRule.fixture.editor.executeAndSave { insertText("fun test2() {}\n") }
+      }
+      WriteCommandAction.runWriteCommandAction(projectRule.project) {
+        projectRule.fixture.editor.executeAndSave { insertText("Broken") }
+      }
+    }
+  }
+}
