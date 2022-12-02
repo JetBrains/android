ae2faa9f9e810b64ce57aa00f8bb700614d1acec
Nickolay Viuginov <viuginick@google.com>
2022-12-02 16:25:21 +0000

    Lightweight memory reporting agent added
    The agent can be attached to external processes and will collect reports
    on memory usage of the process. Currently it only provides the total
    number of objects and their total size in bytes, but new metrics can be
    added if needed.
    
    Bug: 261589078
    Test: LightweightHeapTraverseTest
    Change-Id: I902dbd2d5dc972902ca3368d75a7bd86fdde26f8
    
    GitOrigin-RevId: e7858c440a57175e3b0863e3c193e9c39c3cedc9

---
+++ memory-usage/src/main/java/com/android/tools/memory/usage/LightweightTraverseResult.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.memory.usage;
+
+public class LightweightTraverseResult {
+  private final int totalObjectsNumber;
+  private final long totalObjectsSizeBytes;
+  private final int totalStrongReferencedObjectsNumber;
+  private final long totalStrongReferencedObjectsSizeBytes;
+
+  public LightweightTraverseResult(int totalObjectsNumber,
+                                   long totalObjectsSize,
+                                   int totalStrongReferencedObjectsNumber,
+                                   long totalStrongReferencedObjectsSize) {
+    this.totalObjectsNumber = totalObjectsNumber;
+    this.totalObjectsSizeBytes = totalObjectsSize;
+
+    this.totalStrongReferencedObjectsNumber = totalStrongReferencedObjectsNumber;
+    this.totalStrongReferencedObjectsSizeBytes = totalStrongReferencedObjectsSize;
+  }
+
+  public int getTotalObjectsNumber() {
+    return totalObjectsNumber;
+  }
+
+  public long getTotalObjectsSizeBytes() {
+    return totalObjectsSizeBytes;
+  }
+
+  public long getTotalStrongReferencedObjectsSizeBytes() {
+    return totalStrongReferencedObjectsSizeBytes;
+  }
+
+  public int getTotalStrongReferencedObjectsNumber() {
+    return totalStrongReferencedObjectsNumber;
+  }
+}
