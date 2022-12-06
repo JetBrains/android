cfabc81a1e1f487b2cd93fde5714e2b1143bb94e
Alexander Demurov <ademurov@google.com>
2022-12-06 15:55:41 -0800

    Support attaching threading agent dynamically
    Bug: 228841408
    Test: Unit tests
    Change-Id: Ib10aa605f0f59af84265d88d2c5bdb7eb0c4b83c
    
    GitOrigin-RevId: 597250479adfa39840ea6be692e0ca223c642ec3

--- android/src/com/android/tools/idea/instrumentation/threading/ThreadingChecker.kt
+++ android/src/com/android/tools/idea/instrumentation/threading/ThreadingChecker.kt
@@ -15,14 +15,88 @@
  */
 package com.android.tools.idea.instrumentation.threading
 
+import com.android.tools.idea.util.StudioPathManager.isRunningFromSources
 import com.android.tools.instrumentation.threading.agent.callback.ThreadingCheckerTrampoline
+import com.intellij.execution.process.OSProcessUtil
 import com.intellij.ide.ApplicationInitializedListener
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.application.PathManager
 import com.intellij.openapi.diagnostic.thisLogger
+import com.sun.tools.attach.VirtualMachine
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
+
 
 class ThreadingChecker : ApplicationInitializedListener {
+
   /** Start receiving notifications from the threading agent. */
   override fun componentsInitialized() {
-    ThreadingCheckerTrampoline.installHook(ThreadingCheckerHookImpl())
-    thisLogger().debug("ThreadingChecker listener has been installed.")
+    val agentLoadedAtStartup = try {
+      Class.forName("com.android.tools.instrumentation.threading.agent.Agent", false, null)
+      true
+    }
+    catch (e: ClassNotFoundException) {
+      false
+    }
+
+    if (agentLoadedAtStartup) {
+      ThreadingCheckerTrampoline.installHook(ThreadingCheckerHookImpl())
+      thisLogger().debug("ThreadingChecker listener has been installed.")
+    } else {
+      // Attempt to load the threading agent dynamically when running an EAP build of Android Studio.
+      // As for the tests and running Android Studio from IntelliJ during development use the -javaagent JVM option instead.
+      maybeAttachThreadingAgent()
+    }
+  }
+
+  private fun maybeAttachThreadingAgent() {
+    if (!ApplicationManager.getApplication().isEAP) {
+      // We only allow dynamic attachment of the threading agent in EAP builds
+      return
+    }
+
+    if (isRunningFromSources()) {
+      // Dynamic attachment of the threading agent is disallowed when running from sources
+      return
+    }
+
+    if (ApplicationManager.getApplication().isUnitTestMode) {
+      // Dynamic attachment of the threading agent is disallowed when running from sources
+      return
+    }
+
+    ApplicationManager.getApplication().executeOnPooledThread { attachThreadingAgent() }
+  }
+
+  private fun attachThreadingAgent() {
+    var vm: VirtualMachine? = null
+    try {
+      val threadingAgentJarPath = getThreadingAgentJarLocation()
+      if (threadingAgentJarPath == null) {
+        thisLogger().warn("Couldn't locate threading_agent.jar.")
+        return
+      }
+      vm = VirtualMachine.attach(OSProcessUtil.getApplicationPid())
+      vm.loadAgent(threadingAgentJarPath.toString())
+      ThreadingCheckerTrampoline.installHook(ThreadingCheckerHookImpl())
+      thisLogger().debug("ThreadingChecker listener has been installed (after threading agent was dynamically loaded).")
+    }
+    catch (e: Exception) {
+      thisLogger().error(e)
+    }
+    finally {
+      vm?.detach()
+    }
+  }
+
+  private fun getThreadingAgentJarLocation(): Path? {
+    // Path for the installed Studio.
+    val homePath: Path = Paths.get(PathManager.getHomePath())
+    val jarFile: Path = homePath.resolve("plugins/android/resources/threading_agent.jar")
+    if (!Files.exists(jarFile)) {
+      return null
+    }
+    return jarFile
   }
 }
