7d57e9500770cddb268070524c9fbb79d5fca772
Diego Perez <diegoperez@google.com>
2022-12-06 11:03:24 +0000

    Complete the LayoutEditorPreviewTest
    - Open different layouts of different complexity and check for successful
    render.
    - Remove obsolete unused test code in the project.
    - Update dependencies.
    
    Bug: 256150093
    Test: LayoutEditorPreviewTest
    Change-Id: I05363e31dac1aa801b5e3cf0878eff12255cc413
    
    GitOrigin-RevId: 765ecc37b2e7c360b57b60689b0c712660168934

--- designer/integrationTestSrc/com/android/tools/idea/designer/LayoutEditorPreviewTest.kt
+++ designer/integrationTestSrc/com/android/tools/idea/designer/LayoutEditorPreviewTest.kt
@@ -16,6 +16,7 @@
 package com.android.tools.idea.designer
 
 import com.android.tools.asdriver.tests.AndroidProject
+import com.android.tools.asdriver.tests.AndroidStudio
 import com.android.tools.asdriver.tests.AndroidSystem
 import com.android.tools.asdriver.tests.MavenRepo
 import com.android.tools.asdriver.tests.MemoryDashboardNameProviderWatcher
@@ -33,11 +34,29 @@ class LayoutEditorPreviewTest {
   @get:Rule
   var watcher = MemoryDashboardNameProviderWatcher()
 
+  private fun AndroidStudio.waitForSuccessfulRender(xmlName: String) {
+    system.installation.ideaLog
+      .waitForMatchingLine(
+        ".*RenderResult\\{renderResult=Result\\{status=SUCCESS, errorMessage=null, throwable=null, data=null\\}, psiFile=XmlFile:$xmlName.*",
+        10, TimeUnit.SECONDS)
+  }
+
+  private fun AndroidStudio.openAndWaitForRender(path: Path) {
+    openFile("simpleApplication", path.toString())
+    waitForComponentByClass("DesignSurfaceScrollPane", "JBViewport", "SceneViewPanel")
+    waitForSuccessfulRender(path.fileName.toString())
+  }
+
   @Test
   fun layoutEditorPreviewBasicTest() {
     // Create a new android project, and set a fixed distribution
     val project = AndroidProject("tools/adt/idea/designer/testData/projects/simpleApplication")
     project.setDistribution("tools/external/gradle/gradle-7.3.3-bin.zip")
+
+    // Enable additional logging
+    system.installation.addVmOption(
+      "-Didea.log.debug.categories=#com.android.tools.idea.rendering.RenderResult"
+    )
 
     // Create a maven repo and set it up in the installation and environment
     system.installRepo(MavenRepo("tools/adt/idea/designer/layout_preview_deps.manifest"))
@@ -49,10 +68,15 @@ class LayoutEditorPreviewTest {
           studio.executeAction("MakeGradleProject")
           studio.waitForBuild()
 
-          val path: Path =
-            project.targetProject.resolve("app/src/main/res/layout/activity_my.xml")
-          studio.openFile("simpleApplication", path.toString())
-          studio.waitForComponentByClass("DesignSurfaceScrollPane", "JBViewport", "SceneViewPanel")
+          studio.openAndWaitForRender(project.targetProject.resolve("app/src/main/res/layout/simple_layout.xml"))
+          studio.executeAction("CloseAllEditors")
+          studio.openAndWaitForRender(project.targetProject.resolve("app/src/main/res/layout/normal_layout.xml"))
+          studio.executeAction("CloseAllEditors")
+          val complexLayoutFile = project.targetProject.resolve("app/src/main/res/layout/complex_layout.xml")
+          studio.openAndWaitForRender(complexLayoutFile)
+          studio.editFile(complexLayoutFile.toString(), "\\s*<!--EASY TEXT FIND", "")
+          studio.editFile(complexLayoutFile.toString(), "\\s*EASY TEXT FIND-->", "")
+          studio.waitForSuccessfulRender("complex_layout.xml")
 
           studio.executeAction("Run")
           system.installation.ideaLog.waitForMatchingLine(
