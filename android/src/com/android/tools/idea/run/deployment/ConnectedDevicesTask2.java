9ca9ac7c4848d17dea46fe33607c1889772e083e
Juan C Nuno <juancnuno@google.com>
2022-12-15 10:05:57 -0800

    Start on ConnectedDevicesTask2
    This will eventually replace ConnectedDevicesTask for two reasons:
    
    1. NameGetter calls the deprecated Device::getDdmlibDevice method. I
       will replace that name computation with the way we do it in the
       device manager in ConnectedDevicesTask2.
    2. While I'm here I might as well fix the calls to the deprecated
       getAvdName and getAvdPath methods
    
    Bug: 258484267
    Test: Not applicable
    Change-Id: I5f4178bb74dfd63738456a01d3604855b631ddbb
    
    GitOrigin-RevId: bcb5a9e8671739b5e29472a980feca92a68bf8c3

---
+++ android/src/com/android/tools/idea/run/deployment/ConnectedDevicesTask2.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.run.deployment;
+
+import com.android.ddmlib.IDevice;
+import com.android.tools.idea.run.ConnectedAndroidDevice;
+import com.android.tools.idea.run.LaunchCompatibility;
+import com.android.tools.idea.run.deployment.Device.Type;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.intellij.util.concurrency.EdtExecutorService;
+import java.util.Collection;
+import org.jetbrains.annotations.NotNull;
+
+// TODO Add the thread annotations
+final class ConnectedDevicesTask2 implements AsyncSupplier<Collection<ConnectedDevice>> {
+  private final @NotNull AndroidDebugBridge myAndroidDebugBridge;
+
+  private ConnectedDevicesTask2(@NotNull AndroidDebugBridge androidDebugBridge) {
+    myAndroidDebugBridge = androidDebugBridge;
+  }
+
+  @Override
+  public @NotNull ListenableFuture<@NotNull Collection<@NotNull ConnectedDevice>> get() {
+    // noinspection UnstableApiUsage
+    return Futures.transform(myAndroidDebugBridge.getConnectedDevices(), ConnectedDevicesTask2::toList, EdtExecutorService.getInstance());
+  }
+
+  private static @NotNull Collection<@NotNull ConnectedDevice> toList(@NotNull Collection<@NotNull IDevice> devices) {
+    return devices.stream()
+      .filter(IDevice::isOnline)
+      .map(ConnectedDevicesTask2::build)
+      .toList();
+  }
+
+  private static @NotNull ConnectedDevice build(@NotNull IDevice device) {
+    return new ConnectedDevice.Builder()
+      // TODO
+      .setName("Pixel_6_API_33")
+      // TODO
+      .setKey(new VirtualDevicePath("/usr/local/google/home/user/.android/avd/Pixel_6_API_33.avd"))
+      .setAndroidDevice(new ConnectedAndroidDevice(device))
+      // TODO
+      .setType(Type.PHONE)
+      // TODO
+      .setLaunchCompatibility(LaunchCompatibility.YES)
+      .build();
+  }
+}
