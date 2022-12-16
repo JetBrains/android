8433c0d6cad645151f1d6d0ac4caa2099d5f513e
Vihasith Patnana <vihasith@google.com>
2022-12-15 16:49:18 -0800

    Updated P0: GotoDeclarationTest
    Added validations for declarations and navigation using control + mouse clicks.
    
    Bug: b/262626273
    Test: N/A
    Change-Id: I0ab0123720beee2a77581be0b069450b8babfac6
    
    GitOrigin-RevId: 81df82bfa451a7148dd6344cf85a06a4cc09e7dc

--- android-uitests/testSrc/com/android/tools/idea/tests/gui/intellijplatform/GotoDeclarationTest.java
+++ android-uitests/testSrc/com/android/tools/idea/tests/gui/intellijplatform/GotoDeclarationTest.java
@@ -25,11 +25,17 @@ import com.android.tools.idea.tests.gui.framework.RunIn;
 import com.android.tools.idea.tests.gui.framework.TestGroup;
 import com.android.tools.idea.tests.gui.framework.fixture.EditorFixture;
 import com.android.tools.idea.tests.gui.framework.fixture.IdeFrameFixture;
+import com.intellij.openapi.util.SystemInfo;
 import com.intellij.testGuiFramework.framework.GuiTestRemoteRunner;
+import java.awt.MouseInfo;
+import java.awt.Point;
+import java.awt.event.KeyEvent;
 import java.util.concurrent.TimeUnit;
+import org.fest.swing.core.MouseButton;
 import org.fest.swing.fixture.DialogFixture;
 import org.fest.swing.fixture.JButtonFixture;
 import org.fest.swing.timing.Wait;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -37,7 +43,11 @@ import org.junit.runner.RunWith;
 @RunWith(GuiTestRemoteRunner.class)
 public class GotoDeclarationTest {
 
-  @Rule public final GuiTestRule guiTest = new GuiTestRule().withTimeout(5, TimeUnit.MINUTES);
+  @Rule public final GuiTestRule guiTest = new GuiTestRule().withTimeout(7, TimeUnit.MINUTES);
+
+  private EditorFixture editorFixture;
+  private IdeFrameFixture ideFrame;
+  private Point pointerLocation;
 
   private static final String ACTIVITY_FILE_PATH =
     "app/src/main/java/com/codegeneration/MainActivity.java";
@@ -65,15 +75,20 @@ public class GotoDeclarationTest {
    *   2. Studio will open "AppCompatActivity" file
    *   </pre>
    */
+
+  @Before
+  public void setup() throws Exception {
+    ideFrame = guiTest.importProjectAndWaitForProjectSyncToFinish("CodeGeneration", Wait.seconds(120));
+    guiTest.waitForAllBackgroundTasksToBeCompleted();
+
+    ideFrame.clearNotificationsPresentOnIdeFrame();
+    editorFixture = ideFrame.getEditor();
+  }
+
   @RunIn(TestGroup.SANITY_BAZEL)
   @Test
-  public void gotoDeclaration() throws Exception  {
-    IdeFrameFixture ideFrame =
-      guiTest.importProjectAndWaitForProjectSyncToFinish("CodeGeneration", Wait.seconds(120));
-    guiTest.waitForBackgroundTasks();
-
-    EditorFixture editorFixture = ideFrame.getEditor();
-
+  public void testgotoDeclaration() throws Exception  {
+    //Test go to declarations using keyboard shortcuts.
     editorFixture
       .open(ACTIVITY_FILE_PATH)
       .select(String.format("(setContentView)"))
@@ -111,5 +126,56 @@ public class GotoDeclarationTest {
 
     Wait.seconds(30).expecting("XML file is opened for navigating to definition")
       .until(() -> "activity_main.xml".equals(ideFrame.getEditor().getCurrentFileName()));
+
+    //Test goto declarations using control + mouse click
+    editorFixture
+      .open(ACTIVITY_FILE_PATH)
+      .moveBetween("setCont", "entView");
+
+    pointerLocation = MouseInfo.getPointerInfo().getLocation();
+    pressControlKeyAndClick(pointerLocation);
+
+    Wait.seconds(20).expecting("File is opened for navigating to definition")
+      .until(() -> COMPAT_ACTIVITY_FILE.equals(ideFrame.getEditor().getCurrentFileName()));
+
+    editorFixture
+      .open(ACTIVITY_FILE_PATH)
+      .moveBetween("import ", "")
+      .moveBetween("AppCompat", "Activity");
+
+    pointerLocation = MouseInfo.getPointerInfo().getLocation();
+    pressControlKeyAndClick(pointerLocation);
+
+    Wait.seconds(10).expecting("File is opened for navigating to definition")
+      .until(() -> COMPAT_ACTIVITY_FILE.equals(ideFrame.getEditor().getCurrentFileName()));
+
+    editorFixture
+      .open(ACTIVITY_FILE_PATH)
+      .moveBetween("activi", "ty_main");
+
+    pointerLocation = MouseInfo.getPointerInfo().getLocation();
+    pressControlKeyAndClick(pointerLocation);
+
+    Wait.seconds(30).expecting("XML file is opened for navigating to definition")
+      .until(() -> "activity_main.xml".equals(ideFrame.getEditor().getCurrentFileName()));
+  }
+
+  private void pressControlKeyAndClick(Point pointerLocation) {
+    if (SystemInfo.isMac) {
+      ideFrame.robot().pressKey(KeyEvent.META_MASK);
+      ideFrame.robot().click(editorFixture.frame().target(),
+                             pointerLocation,
+                             MouseButton.LEFT_BUTTON,
+                             1);
+      ideFrame.robot().releaseKey(KeyEvent.META_MASK);
+    }
+    else {
+      ideFrame.robot().pressKey(KeyEvent.VK_CONTROL);
+      ideFrame.robot().click(editorFixture.frame().target(),
+                             pointerLocation,
+                             MouseButton.LEFT_BUTTON,
+                             1);
+      ideFrame.robot().releaseKey(KeyEvent.VK_CONTROL);
+    }
   }
 }
