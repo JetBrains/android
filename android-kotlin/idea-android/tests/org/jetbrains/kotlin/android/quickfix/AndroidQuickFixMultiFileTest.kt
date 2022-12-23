da551c41ecb11d8fa3a7632f44576d32bce2f16f
Dana Dahlstrom <dahlstrom@google.com>
2022-12-23 15:00:00 -0800

    Apply @Ignore to failing test class AutoImports
    Change-Id: Ia052bfffb60e20b340b75e18a9f1fb011f15b93a
    Issue: 263890870
    
    GitOrigin-RevId: 757a63fd92324c36fedc77b810285a359c928bf2

--- android-kotlin/idea-android/tests/org/jetbrains/kotlin/android/quickfix/AndroidQuickFixMultiFileTest.kt
+++ android-kotlin/idea-android/tests/org/jetbrains/kotlin/android/quickfix/AndroidQuickFixMultiFileTest.kt
@@ -21,6 +21,7 @@ abstract class AndroidQuickFixMultiFileTest : AbstractAndroidQuickFixMultiFileTe
     private const val TEST_DIR = "idea-android/testData/android/quickfix"
   }
 
+  @org.junit.Ignore("b/263890870")
   class AutoImports : AndroidQuickFixMultiFileTest() {
     fun testAndroidRImport() = doTest("$TEST_DIR/autoImports/androidRImport")
   }
