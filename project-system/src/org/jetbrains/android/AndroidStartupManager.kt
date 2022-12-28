50ff158ca781cbb990942178e1d2efd8d93adb41
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2022-12-28 13:38:22 +0100

    AndroidStartupManager - migrate to ProjectPostStartupActivity
    GitOrigin-RevId: 0667742f5c4310b1f89cd3b7c2da6a50ac33aba0

--- project-system/src/org/jetbrains/android/AndroidStartupManager.kt
+++ project-system/src/org/jetbrains/android/AndroidStartupManager.kt
@@ -5,65 +5,79 @@ import com.android.tools.idea.AndroidStartupActivity
 import com.intellij.ProjectTopics
 import com.intellij.facet.Facet
 import com.intellij.facet.FacetManager
-import com.intellij.facet.FacetManagerAdapter
+import com.intellij.facet.FacetManagerListener
 import com.intellij.facet.ProjectFacetManager
 import com.intellij.openapi.Disposable
 import com.intellij.openapi.application.ApplicationManager
-import com.intellij.openapi.application.invokeAndWaitIfNeeded
+import com.intellij.openapi.application.EDT
 import com.intellij.openapi.components.Service
+import com.intellij.openapi.components.service
+import com.intellij.openapi.extensions.ExtensionNotApplicableException
 import com.intellij.openapi.module.Module
 import com.intellij.openapi.project.ModuleListener
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.startup.StartupActivity
-import com.intellij.util.messages.MessageBusConnection
+import com.intellij.openapi.startup.ProjectPostStartupActivity
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import org.jetbrains.android.facet.AndroidFacet
 
-class AndroidStartupManager : StartupActivity {
-
+private class AndroidStartupManager : ProjectPostStartupActivity {
   // A project level service to be used as a parent disposable. Future implementation may reduce the life-time of such disposable to the
   // moment when the last Android facet is removed (from a non-Gradle project).
-  @Service
+  @Service(Service.Level.PROJECT)
   class ProjectDisposableScope : Disposable {
-    override fun dispose() = Unit
+    override fun dispose() {
+    }
   }
 
-  override fun runActivity(project: Project) {
-    fun runAndroidStartupActivities() {
+  init {
+    if (ApplicationManager.getApplication().isUnitTestMode || ApplicationManager.getApplication().isHeadlessEnvironment) {
+      throw ExtensionNotApplicableException.create()
+    }
+  }
+
+  override suspend fun execute(project: Project) {
+    suspend fun runAndroidStartupActivities() {
       for (activity in AndroidStartupActivity.STARTUP_ACTIVITY.extensionList) {
-        invokeAndWaitIfNeeded {
-          activity.runActivity(project, project.getService(ProjectDisposableScope::class.java))
+        withContext(Dispatchers.EDT) {
+          activity.runActivity(project, project.service<ProjectDisposableScope>())
         }
       }
     }
 
-    if (!ApplicationManager.getApplication().isUnitTestMode &&
-        !ApplicationManager.getApplication().isHeadlessEnvironment) {
-      if (ProjectFacetManager.getInstance(project).hasFacets(AndroidFacet.ID)) {
-        runAndroidStartupActivities()
-      }
-      else {
-        val connection: MessageBusConnection = project.messageBus.connect()
-        connection.subscribe(FacetManager.FACETS_TOPIC, object : FacetManagerAdapter() {
-          override fun facetAdded(facet: Facet<*>) {
-            if (facet is AndroidFacet) {
-              runAndroidStartupActivities()
-              connection.disconnect()
-            }
-          }
-        })
-        // [facetAdded] is not invoked for a facet if it is added together with a new module that holds it.
-        connection.subscribe(ProjectTopics.MODULES, object : ModuleListener {
-          override fun modulesAdded(project: Project, modules: List<Module>) {
-            for (module in modules) {
-              if (AndroidFacet.getInstance(module) != null) {
-                runAndroidStartupActivities()
-                connection.disconnect()
-                break
-              }
-            }
-          }
-        })
-      }
+    if (ProjectFacetManager.getInstance(project).hasFacets(AndroidFacet.ID)) {
+      runAndroidStartupActivities()
+      return
     }
+
+    val connection = project.messageBus.simpleConnect()
+    connection.subscribe(FacetManager.FACETS_TOPIC, object : FacetManagerListener {
+      override fun facetAdded(facet: Facet<*>) {
+        if (facet is AndroidFacet) {
+          @Suppress("DEPRECATION")
+          project.coroutineScope.launch {
+            runAndroidStartupActivities()
+          }
+          connection.disconnect()
+        }
+      }
+    })
+
+    // [facetAdded] is not invoked for a facet if it is added together with a new module that holds it.
+    connection.subscribe(ProjectTopics.MODULES, object : ModuleListener {
+      override fun modulesAdded(project: Project, modules: List<Module>) {
+        for (module in modules) {
+          if (AndroidFacet.getInstance(module) != null) {
+            @Suppress("DEPRECATION")
+            project.coroutineScope.launch {
+              runAndroidStartupActivities()
+            }
+            connection.disconnect()
+            break
+          }
+        }
+      }
+    })
   }
 }
