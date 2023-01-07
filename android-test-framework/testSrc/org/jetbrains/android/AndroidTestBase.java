4c7e44186ba78debf24ab67102b7e33bdf1babbd
Sergey Prigogin <sprigogin@google.com>
2023-01-06 21:38:59 -0800

    Add UndisposedAndroidObjectsCheckerRule
    Test: N/A
    Bug: N/A
    Change-Id: I2dac3ea726c4d5e7b02cf0b10cb0b6a0cd4dbf1b
    
    GitOrigin-RevId: 0e7ccfe438adf7a4b6b96ca11f9b4bc1da8712ad

--- android-test-framework/testSrc/org/jetbrains/android/AndroidTestBase.java
+++ android-test-framework/testSrc/org/jetbrains/android/AndroidTestBase.java
@@ -15,11 +15,12 @@
  */
 package org.jetbrains.android;
 
+import static org.jetbrains.android.UndisposedAndroidObjectsCheckerRule.checkUndisposedAndroidRelatedObjects;
+
 import com.android.testutils.MockitoThreadLocalsCleaner;
 import com.android.testutils.TestUtils;
 import com.android.tools.idea.res.IdeResourcesUtil;
 import com.android.tools.idea.sdk.AndroidSdks;
-import com.android.tools.idea.testing.DisposerExplorer;
 import com.android.tools.idea.testing.Sdks;
 import com.intellij.analysis.AnalysisScope;
 import com.intellij.codeInspection.CommonProblemDescriptor;
@@ -34,11 +35,8 @@ import com.intellij.openapi.Disposable;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.application.WriteAction;
 import com.intellij.openapi.application.ex.PathManagerEx;
-import com.intellij.openapi.module.Module;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.project.impl.ProjectImpl;
 import com.intellij.openapi.projectRoots.Sdk;
-import com.intellij.openapi.util.Ref;
 import com.intellij.openapi.util.Segment;
 import com.intellij.openapi.util.TextRange;
 import com.intellij.openapi.util.io.FileUtil;
@@ -46,21 +44,17 @@ import com.intellij.openapi.vfs.LocalFileSystem;
 import com.intellij.openapi.vfs.newvfs.impl.VfsRootAccess;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
-import com.intellij.psi.PsiReferenceContributor;
 import com.intellij.testFramework.InspectionTestUtil;
 import com.intellij.testFramework.InspectionsKt;
-import com.intellij.testFramework.LightProjectDescriptor;
 import com.intellij.testFramework.UsefulTestCase;
 import com.intellij.testFramework.fixtures.JavaCodeInsightTestFixture;
 import com.intellij.testFramework.fixtures.impl.GlobalInspectionContextForTests;
-import com.intellij.util.containers.ContainerUtil;
 import com.intellij.util.ui.UIUtil;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
-import java.util.Set;
 import java.util.stream.Collectors;
 import org.jetbrains.android.sdk.AndroidPlatform;
 import org.jetbrains.android.sdk.AndroidSdkAdditionalData;
@@ -77,8 +71,6 @@ import org.jetbrains.annotations.Nullable;
  */
 @SuppressWarnings({"JUnitTestCaseWithNonTrivialConstructors"})
 public abstract class AndroidTestBase extends UsefulTestCase {
-  // Keep track of each leaked disposable so that we can fail just the *first* test that leaks it.
-  private static final Set<Disposable> allLeakedDisposables = ContainerUtil.createWeakSet();
 
   protected JavaCodeInsightTestFixture myFixture;
   private final MockitoThreadLocalsCleaner mockitoCleaner = new MockitoThreadLocalsCleaner();
@@ -104,42 +96,6 @@ public abstract class AndroidTestBase extends UsefulTestCase {
       mockitoCleaner.cleanupAndTearDown();
     }
     checkUndisposedAndroidRelatedObjects();
-  }
-
-  /**
-   * Checks that there are no undisposed Android-related objects.
-   */
-  public static void checkUndisposedAndroidRelatedObjects() {
-    Ref<Disposable> firstLeak = new Ref<>();
-    DisposerExplorer.visitTree(disposable -> {
-      if (allLeakedDisposables.contains(disposable) ||
-          disposable.getClass().getName().startsWith("com.android.tools.analytics.HighlightingStats") ||
-          (disposable instanceof ProjectImpl && (((ProjectImpl)disposable).isDefault() || ((ProjectImpl)disposable).isLight())) ||
-          disposable.toString().startsWith("services of ") || // See ComponentManagerImpl.serviceParentDisposable.
-          (disposable instanceof Module && ((Module)disposable).getName().equals(LightProjectDescriptor.TEST_MODULE_NAME)) ||
-          disposable instanceof PsiReferenceContributor) {
-        // Ignore application services and light projects and modules that are not disposed by tearDown.
-        return DisposerExplorer.VisitResult.SKIP_CHILDREN;
-      }
-      if (disposable.getClass().getName().startsWith("com.android.") ||
-          disposable.getClass().getName().startsWith("org.jetbrains.android.")) {
-        firstLeak.setIfNull(disposable);
-        allLeakedDisposables.add(disposable);
-      }
-      return DisposerExplorer.VisitResult.CONTINUE;
-    });
-    if (!firstLeak.isNull()) {
-      Disposable disposable = firstLeak.get();
-      Disposable parent = DisposerExplorer.getParent(disposable);
-      String baseMsg = "Undisposed object '" + disposable + "' of type '" + disposable.getClass().getName() + "'";
-      if (parent == null) {
-        throw new RuntimeException(
-          baseMsg + ", registered as a root disposable (see cause for creation trace)",
-          DisposerExplorer.getTrace(disposable));
-      } else {
-        throw new RuntimeException(baseMsg + ", with parent '" + parent + "' of type '" + parent.getClass().getName() + "'");
-      }
-    }
   }
 
   public static void refreshProjectFiles() {
