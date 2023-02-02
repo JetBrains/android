38a94a881f42e1a6dc06b6647b43e0a2d1dc3c1c
Ahmet Kerim Senol <akerim@google.com>
2023-02-02 10:46:54 +0000

    Create one test suite per benchmark test
    Test: Test change
    Bug: 260381662
    Change-Id: Ib9eceb67ac7ef1d31bbcddfcc7395815663c4d6b
    
    GitOrigin-RevId: bf92cc5d4e052702682420ea39ec7a8f9992ed19

--- sync-memory-tests/testSrc/com/android/tools/idea/gradle/project/sync/memory/AbstractGradleSyncMemoryUsageTestCase.kt
+++ sync-memory-tests/testSrc/com/android/tools/idea/gradle/project/sync/memory/AbstractGradleSyncMemoryUsageTestCase.kt
@@ -27,11 +27,16 @@ import com.android.tools.memory.usage.LightweightHeapTraverseConfig
 import com.android.tools.perflogger.Benchmark
 import com.android.tools.perflogger.Metric
 import com.android.tools.perflogger.Metric.MetricSample
+import com.android.tools.tests.GradleDaemonsRule
+import com.android.tools.tests.IdeaTestSuiteBase
+import com.android.tools.tests.LeakCheckerRule
 import com.intellij.testFramework.RunsInEdt
+import com.intellij.util.containers.map2Array
 import org.gradle.tooling.internal.consumer.DefaultGradleConnector
 import org.jetbrains.android.AndroidTestBase
 import org.junit.After
 import org.junit.Before
+import org.junit.ClassRule
 import org.junit.Rule
 import org.junit.Test
 import java.io.File
@@ -44,15 +49,8 @@ import kotlin.io.path.createDirectory
 import kotlin.system.measureTimeMillis
 
 @RunsInEdt
-abstract class AbstractGradleSyncMemoryUsageTestCase {
-  companion object {
-    val BENCHMARK = Benchmark.Builder("Retained heap size")
-      .setProject("Android Studio Sync Test")
-      .build()
-    val TEST_DATA: Path = Paths.get(AndroidTestBase.getModulePath("sync-memory-tests")).resolve("testData")
-  }
+abstract class AbstractGradleSyncMemoryUsageTestCase : IdeaTestSuiteBase() {
 
-  abstract val relativePath: String
   abstract val projectName: String
   abstract val memoryLimitMb: Int
 
@@ -89,7 +87,7 @@ abstract class AbstractGradleSyncMemoryUsageTestCase {
   open fun testSyncMemory() {
     setJvmArgs()
     projectRule.openTestProject(testProjectTemplateFromPath(
-        path = relativePath,
+        path = DIRECTORY,
         testDataPath = TEST_DATA.toString())) {
       // Free up some memory by closing the Gradle Daemon
       DefaultGradleConnector.close()
@@ -135,10 +133,37 @@ abstract class AbstractGradleSyncMemoryUsageTestCase {
   }
 
   private fun setJvmArgs() {
-    GradleProperties(TEST_DATA.toFile().resolve(relativePath).resolve(SdkConstants.FN_GRADLE_PROPERTIES)).apply {
+    GradleProperties(TEST_DATA.resolve(DIRECTORY).resolve(SdkConstants.FN_GRADLE_PROPERTIES).toFile()).apply {
       setJvmArgs(jvmArgs.orEmpty().replace("-Xmx60g", "-Xmx${memoryLimitMb}m"))
       setJvmArgs("$jvmArgs -agentpath:${File(memoryAgentPath).absolutePath}")
       save()
     }
+  }
+
+  companion object {
+    val DIRECTORY = "benchmark"
+    val BENCHMARK = Benchmark.Builder("Retained heap size")
+      .setProject("Android Studio Sync Test")
+      .build()
+    val TEST_DATA: Path = Paths.get(AndroidTestBase.getModulePath("sync-memory-tests")).resolve("testData")
+
+    @JvmField @ClassRule val checker = LeakCheckerRule()
+    @JvmField @ClassRule val gradle = GradleDaemonsRule()
+
+    @JvmStatic
+    protected fun setUpProject(vararg diffSpecs: String) {
+      setUpSourceZip(
+        "prebuilts/studio/buildbenchmarks/extra-large.2022.9/src.zip",
+        "tools/adt/idea/sync-memory-tests/testData/$DIRECTORY",
+        DiffSpec("prebuilts/studio/buildbenchmarks/extra-large.2022.9/diff-properties", 0),
+        *(diffSpecs.map2Array { it.toSpec() })
+      )
+      unzipIntoOfflineMavenRepo("prebuilts/studio/buildbenchmarks/extra-large.2022.9/repo.zip")
+      unzipIntoOfflineMavenRepo("tools/base/build-system/android_gradle_plugin.zip")
+      unzipIntoOfflineMavenRepo("tools/data-binding/data_binding_runtime.zip")
+      linkIntoOfflineMavenRepo("tools/base/build-system/android_gradle_plugin_runtime_dependencies.manifest")
+      linkIntoOfflineMavenRepo("tools/base/build-system/integration-test/kotlin_gradle_plugin_prebuilts.manifest")
+    }
+    private fun String.toSpec() = DiffSpec("prebuilts/studio/buildbenchmarks/extra-large.2022.9/$this", 0)
   }
 }
--- sync-memory-tests/testSrc/com/android/tools/idea/gradle/project/sync/memory/SyncMemoryTestSuite.kt
+++ sync-memory-tests/testSrc/com/android/tools/idea/gradle/project/sync/memory/SyncMemoryTestSuite.kt
@@ -15,84 +15,64 @@
  */
 package com.android.tools.idea.gradle.project.sync.memory
 
-import com.android.testutils.JarTestSuiteRunner
-import com.android.testutils.JarTestSuiteRunner.ExcludeClasses
-import com.android.tools.tests.GradleDaemonsRule
-import com.android.tools.tests.IdeaTestSuiteBase
-import com.android.tools.tests.LeakCheckerRule
-import org.junit.ClassRule
-import org.junit.runner.RunWith
-
 class Benchmark50MemoryTest : AbstractGradleSyncMemoryUsageTestCase() {
-  override val relativePath = "benchmark-50"
   override val projectName = "50Modules"
   override val memoryLimitMb = 400
+
+  init {
+    setUpProject("diff-50")
+  }
 }
 class Benchmark100MemoryTest : AbstractGradleSyncMemoryUsageTestCase() {
-  override val relativePath = "benchmark-100"
   override val projectName = "100Modules"
   override val memoryLimitMb = 600
+
+  init {
+    setUpProject("diff-100")
+  }
 }
 
 class Benchmark200MemoryTest : AbstractGradleSyncMemoryUsageTestCase() {
-  override val relativePath = "benchmark-200"
   override val projectName = "200Modules"
   override val memoryLimitMb = 1300
+
+  init {
+    setUpProject("diff-200")
+  }
 }
 
 class Benchmark500MemoryTest : AbstractGradleSyncMemoryUsageTestCase() {
-  override val relativePath = "benchmark-500"
   override val projectName = "500Modules"
   override val memoryLimitMb = 4000
+
+  init {
+    setUpProject("diff-500")
+  }
 }
 
 class Benchmark1000MemoryTest : AbstractGradleSyncMemoryUsageTestCase() {
-  override val relativePath = "benchmark-1000"
   override val projectName = "1000Modules"
   override val memoryLimitMb = 9000
+
+  init {
+    setUpProject("diff-1000")
+  }
 }
 
 class Benchmark2000MemoryTest : AbstractGradleSyncMemoryUsageTestCase() {
-  override val relativePath = "benchmark-2000"
   override val projectName = "2000Modules"
   override val memoryLimitMb = 20000
+
+  init {
+    setUpProject("diff-app")
+  }
 }
 
 class BenchmarkXLMemoryTest : AbstractGradleSyncMemoryUsageTestCase() {
-  override val relativePath = "benchmark-xl"
   override val projectName = "BenchmarkXL"
   override val memoryLimitMb = 40000
-}
-
-@RunWith(JarTestSuiteRunner::class)
-@ExcludeClasses(SyncMemoryTestSuite::class)
-object SyncMemoryTestSuite : IdeaTestSuiteBase() {
-  @JvmField @ClassRule  val checker = LeakCheckerRule()
-  @JvmField @ClassRule  val gradle = GradleDaemonsRule()
 
   init {
-    setUpProject("benchmark-50", "diff-50".toSpec())
-    setUpProject("benchmark-100", "diff-100".toSpec())
-    setUpProject("benchmark-200", "diff-200".toSpec())
-    setUpProject("benchmark-500", "diff-500".toSpec())
-    setUpProject("benchmark-1000", "diff-1000".toSpec())
-    setUpProject("benchmark-2000", "diff-app".toSpec())
-    setUpProject("benchmark-xl")
-
-    unzipIntoOfflineMavenRepo("prebuilts/studio/buildbenchmarks/extra-large.2022.9/repo.zip")
-    unzipIntoOfflineMavenRepo("tools/base/build-system/android_gradle_plugin.zip")
-    unzipIntoOfflineMavenRepo("tools/data-binding/data_binding_runtime.zip")
-    linkIntoOfflineMavenRepo("tools/base/build-system/android_gradle_plugin_runtime_dependencies.manifest")
-    linkIntoOfflineMavenRepo("tools/base/build-system/integration-test/kotlin_gradle_plugin_prebuilts.manifest")
+    setUpProject() // no diff
   }
-
-  private fun setUpProject(directory: String, vararg diffSpecs: DiffSpec) {
-    setUpSourceZip(
-      "prebuilts/studio/buildbenchmarks/extra-large.2022.9/src.zip",
-      "tools/adt/idea/sync-memory-tests/testData/$directory",
-      DiffSpec("prebuilts/studio/buildbenchmarks/extra-large.2022.9/diff-properties", 0),
-      *diffSpecs
-    )
-  }
-  private fun String.toSpec() = DiffSpec("prebuilts/studio/buildbenchmarks/extra-large.2022.9/$this", 0)
 }
