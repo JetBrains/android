d6473c43560b8703bf9295f67ededd9d1c9f478f
Amr Afifiy <amrmahmoud@google.com>
2023-02-08 16:11:03 +0000

    Remove resource validation warning from build analyzer
    The optional flag doesn't affect much and doesn't do any extra
    processing.
    
    Bug: 267779313
    Test: existing
    Change-Id: Ib0acc4fca5f2f67746fc2742452c6082f5b48678
    
    GitOrigin-RevId: b591f5654b2af3cc6757442fe6c1b5cb1e3b9db5

--- build-attribution/testSrc/com/android/build/attribution/analyzers/TaskCategoryWarningsAnalyzerTest.kt
+++ build-attribution/testSrc/com/android/build/attribution/analyzers/TaskCategoryWarningsAnalyzerTest.kt
@@ -57,7 +57,6 @@ class TaskCategoryWarningsAnalyzerTest {
       ).containsExactly(
         TaskCategoryIssue.NON_FINAL_RES_IDS_DISABLED,
         TaskCategoryIssue.NON_TRANSITIVE_R_CLASS_DISABLED,
-        TaskCategoryIssue.RESOURCE_VALIDATION_ENABLED,
         TaskCategoryIssue.MINIFICATION_ENABLED_IN_DEBUG_BUILD
       )
     }
