1b9288a11ad889afa48d7b0240540bbbceaf5bac
Fabien Sanglard <sanglardf@google.com>
2023-02-09 00:29:08 +0000

    LE: Delete dead code
    Test: go/ab
    Bug: NA
    Change-Id: I23f62ba30c4c30a651157c829d3a8f426231d593
    
    GitOrigin-RevId: 6d3fa75e7d1c0c3668abb32c9115674cf0907e76

--- android/src/com/android/tools/idea/run/deployment/liveedit/LiveEditCompilerOutput.kt
+++ android/src/com/android/tools/idea/run/deployment/liveedit/LiveEditCompilerOutput.kt
@@ -19,8 +19,6 @@ data class LiveEditCompilerOutput private constructor (val classes: Map<String, 
                                                        val supportClasses: Map<String, ByteArray>,
                                                        val groupIds: List<Int>,
                                                        val resetState: Boolean) {
-  fun hasGroupIds() = groupIds.isNotEmpty()
-
   class Builder(
     var classes: HashMap<String, ByteArray> = HashMap(),
     var supportClasses: HashMap<String, ByteArray> = HashMap(),
