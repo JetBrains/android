5b135ace41d6bb88e152af22b79f4368e7c14ace
Alex Plate <aleksei.plate@jetbrains.com>
2023-02-16 17:25:16 +0200

    Refactoring TreeSpeedSearch to avoid constructors with side effects
    GitOrigin-RevId: 8a7362a937bcb95b07c5c1649076a67c3f4389bd

--- apkanalyzer/src/com/android/tools/idea/apk/viewer/dex/DexFileViewer.java
+++ apkanalyzer/src/com/android/tools/idea/apk/viewer/dex/DexFileViewer.java
@@ -138,7 +138,7 @@ public class DexFileViewer extends UserDataHolderBase implements ApkFileEditorCo
     myTree.setRootVisible(true);
     myTree.setShowsRootHandles(true);
 
-    new TreeSpeedSearch(myTree, true, path -> {
+    TreeSpeedSearch.installOn(myTree, true, path -> {
       Object o = path.getLastPathComponent();
       if (!(o instanceof DexElementNode)) {
         return "";
