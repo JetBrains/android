92c40d3a48b512746030b44250a61c92655f29fa
Chester Hsieh <dunno@google.com>
2023-02-23 11:55:20 -0800

    Adjust LE button border size on HiDPI devices
    Fixes: 269521174
    Test: visual inspection
    Change-Id: Ie43b88da2354a699e436158358936f975880c7df
    
    GitOrigin-RevId: 4a68fe955d1cc0ee1c2293155911cac1a2a6d6d4

--- adt-ui/src/main/java/com/android/tools/adtui/compose/IssueNotificationAction.kt
+++ adt-ui/src/main/java/com/android/tools/adtui/compose/IssueNotificationAction.kt
@@ -35,6 +35,7 @@ import com.intellij.openapi.util.text.StringUtil
 import com.intellij.ui.JBColor
 import com.intellij.ui.RoundedLineBorder
 import com.intellij.ui.components.AnActionLink
+import com.intellij.ui.scale.JBUIScale
 import com.intellij.util.Alarm
 import com.intellij.util.ui.JBUI
 import com.intellij.util.ui.UIUtil
@@ -59,9 +60,8 @@ val REFRESH_BUTTON =
     JBColor(0x59A869, 0x499C54)
   )
 
-internal fun chipBorder(color: Color): Border = RoundedLineBorder(UIUtil.toAlpha(color, ACTION_BORDER_ALPHA),
-                                                                 ACTION_BORDER_ARC_SIZE,
-                                                                 ACTION_BORDER_THICKNESS)
+internal fun chipBorder(color: Color): Border = RoundedLineBorder(
+  UIUtil.toAlpha(color, ACTION_BORDER_ALPHA), ACTION_BORDER_ARC_SIZE, (ACTION_BORDER_THICKNESS * JBUIScale.sysScale()).toInt())
 
 interface ComposeStatus {
   val icon: Icon?
