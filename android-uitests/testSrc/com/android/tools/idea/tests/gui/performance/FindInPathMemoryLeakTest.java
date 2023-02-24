f11c9d1c4674e71a1c88fbe73093e2ed1dccb07d
Suryanarayana Mudunuri <smudunuri@google.com>
2023-02-23 17:11:55 -0800

    Fix UI-test failure for Bleak performance tests.
    Fix EditorTabsMemoryUseTest, FindInPathMemoryLeakTest and NavEditorMemoryUseTest UI test failures.
    
    Bug: 269668833
    Test: N/A
    Change-Id: Id640095274150a23c6b9566a0a3e44045a61c0f1
    
    GitOrigin-RevId: dd9ac1aee4de5ea39086ed60ed599773167e9731

--- android-uitests/testSrc/com/android/tools/idea/tests/gui/performance/FindInPathMemoryLeakTest.java
+++ android-uitests/testSrc/com/android/tools/idea/tests/gui/performance/FindInPathMemoryLeakTest.java
@@ -22,6 +22,8 @@ import com.android.tools.idea.tests.gui.framework.fixture.FindPopupPanelFixture;
 import com.android.tools.idea.tests.gui.framework.fixture.IdeFrameFixture;
 import com.android.tools.idea.bleak.UseBleak;
 import com.intellij.testGuiFramework.framework.GuiTestRemoteRunner;
+import java.util.concurrent.TimeUnit;
+import org.fest.swing.fixture.DialogFixture;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -32,16 +34,18 @@ import org.junit.runner.RunWith;
 @RunWith(GuiTestRemoteRunner.class)
 @RunIn(TestGroup.PERFORMANCE)
 public class FindInPathMemoryLeakTest {
-  @Rule public final GuiTestRule guiTest = new GuiTestRule();
+  @Rule public final GuiTestRule guiTest = new GuiTestRule().withTimeout(6, TimeUnit.MINUTES);
 
   @Test
   @UseBleak
   public void openAndClosePopup() throws Exception {
     IdeFrameFixture ideFrame = guiTest.importSimpleApplication();
     guiTest.runWithBleak(() -> {
-      ideFrame.openFromMenu(FindPopupPanelFixture::find, "Edit", "Find", "Find in Path...")
-          .dialog()
-          .close();
+      ideFrame.focus()
+        .invokeMenuPath("Edit", "Find", "Find in Files...");
+      FindPopupPanelFixture.find(ideFrame)
+        .dialog()
+        .close();
     });
   }
 }
