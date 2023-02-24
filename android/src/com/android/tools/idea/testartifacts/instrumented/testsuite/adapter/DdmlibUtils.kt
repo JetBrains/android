72bdaf214efb244d27ea2773b558ec6009fda089
Polina Koval <kovalp@google.com>
2023-02-24 17:40:35 +0000

    Change threading model for convertIDeviceToAndroidDevice
    Make convertIDeviceToAndroidDevice blocking operation and @WorkerThread.
    It will throw exceptions during shell operations to invokers of
    convertIDeviceToAndroidDevice instead of leave them unchecked in pool
    thread
    
    Remove executeShellCommandSync wrapper because device.executeShellCommand
    is a sync operation
    
    Bug: n/a
    Test: GradleTestResultAdapterTest + run test with gradle
    Change-Id: Iab7e450ef6167e765bd74a84127aeb348adda477
    
    GitOrigin-RevId: 91c4e030a133327d398f3abf6fc719edda1b7015

--- android/src/com/android/tools/idea/testartifacts/instrumented/testsuite/adapter/DdmlibUtils.kt
+++ android/src/com/android/tools/idea/testartifacts/instrumented/testsuite/adapter/DdmlibUtils.kt
@@ -16,21 +16,17 @@
 package com.android.tools.idea.testartifacts.instrumented.testsuite.adapter
 
 import com.android.annotations.concurrency.WorkerThread
-import com.android.ddmlib.CollectingOutputReceiver
 import com.android.ddmlib.IDevice
-import com.android.tools.idea.concurrency.AndroidDispatchers
-import com.android.tools.idea.concurrency.androidCoroutineExceptionHandler
+import com.android.ddmlib.MultiLineReceiver
 import com.android.tools.idea.testartifacts.instrumented.testsuite.model.AndroidDevice
 import com.android.tools.idea.testartifacts.instrumented.testsuite.model.AndroidDeviceType
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.launch
 import java.util.Collections
-import java.util.concurrent.CountDownLatch
 import java.util.concurrent.TimeUnit
 
 /**
  * Converts the given IDevice into an AndroidDevice
  */
+@WorkerThread
 fun convertIDeviceToAndroidDevice(device: IDevice): AndroidDevice {
   return AndroidDevice(device.serialNumber,
                        device.avdName ?: "",
@@ -44,62 +40,61 @@ fun convertIDeviceToAndroidDevice(device: IDevice): AndroidDevice {
                        device.version,
                        Collections.synchronizedMap(LinkedHashMap())).apply {
     additionalInfo["SerialNumber"] = device.serialNumber
-    CoroutineScope(AndroidDispatchers.workerThread + androidCoroutineExceptionHandler).launch {
-      executeShellCommandSync(device, "cat /proc/meminfo") { output ->
-        output.lineSequence().map {
-          val (key, value) = it.split(':', ignoreCase = true, limit = 2) + listOf("", "")
-          if (key.trim() == "MemTotal") {
-            val (ramSize, unit) = value.trim().split(' ', ignoreCase = true, limit = 2)
-            val ramSizeFloat = ramSize.toFloatOrNull() ?: return@map null
-            when (unit) {
-              "kB" -> String.format("%.1f GB", ramSizeFloat / 1000 / 1000)
-              else -> null
-            }
+    executeShellCommandAndProcessOutput(device, "cat /proc/meminfo").let { output ->
+      output.map {
+        val (key, value) = it.split(':', ignoreCase = true, limit = 2) + listOf("", "")
+        if (key.trim() == "MemTotal") {
+          val (ramSize, unit) = value.trim().split(' ', ignoreCase = true, limit = 2)
+          val ramSizeFloat = ramSize.toFloatOrNull() ?: return@map null
+          when (unit) {
+            "kB" -> String.format("%.1f GB", ramSizeFloat / 1000 / 1000)
+            else -> null
           }
-          else {
-            null
-          }
-        }.filterNotNull().firstOrNull()
-      }?.let { additionalInfo["RAM"] = it }
-
-      executeShellCommandSync(device, "cat /proc/cpuinfo") { output ->
-        val cpus = output.lineSequence().map {
-          val (key, value) = it.split(':', ignoreCase = true, limit = 2) + listOf("", "")
-          if (key.trim() == "model name") {
-            value.trim()
-          }
-          else {
-            null
-          }
-        }.filterNotNull().toSet()
-        if (cpus.isEmpty()) {
-          null
         }
         else {
-          cpus.joinToString("\n")
+          null
         }
-      }?.let { additionalInfo["Processor"] = it }
-
-      executeShellCommandSync(device, "getprop ro.product.manufacturer")?.let { additionalInfo["Manufacturer"] = it }
-      executeShellCommandSync(device, "getprop ro.product.model")?.let { additionalInfo["Model"] = it }
+      }.filterNotNull().firstOrNull()?.let {
+        additionalInfo["RAM"] = it
+      }
     }
+
+    executeShellCommandAndProcessOutput(device, "cat /proc/cpuinfo").let { output ->
+      val cpus = output.mapNotNull {
+        val (key, value) = it.split(':', ignoreCase = true, limit = 2) + listOf("", "")
+        if (key.trim() == "model name") {
+          value.trim()
+        }
+        else {
+          null
+        }
+      }.toSet()
+
+      if (cpus.isNotEmpty()) {
+        additionalInfo["Processor"] = cpus.joinToString("\n")
+      }
+    }
+
+    executeShellCommandAndProcessOutput(device, "getprop ro.product.manufacturer").let {
+      it.getOrNull(0)?.let { additionalInfo["Manufacturer"] = it }
+    }
+    executeShellCommandAndProcessOutput(device, "getprop ro.product.model").let { it.getOrNull(0)?.let { additionalInfo["Model"] = it } }
   }
 }
 
 /**
- * Executes a given shell command on a given device. This function blocks caller
- * until the command finishes or times out and returns output in string.
- *
- * @param device a target device to run a command
- * @param command a command to be executed
- * @param postProcessOutput a function which post processes the command output
+ * Executes a given shell command on a given device and returns output.
  */
 @WorkerThread
-private fun executeShellCommandSync(device: IDevice, command: String,
-                                    postProcessOutput: (output: String) -> String? = { it }): String? {
-  val latch = CountDownLatch(1)
-  val receiver = CollectingOutputReceiver(latch)
+private fun executeShellCommandAndProcessOutput(device: IDevice, command: String): MutableList<String> {
+  val receiver = object : MultiLineReceiver() {
+    val output = mutableListOf<String>()
+    override fun isCancelled() = false
+
+    override fun processNewLines(lines: Array<out String>) {
+      output.addAll(lines)
+    }
+  }
   device.executeShellCommand(command, receiver, 10, TimeUnit.SECONDS)
-  latch.await(10, TimeUnit.SECONDS)
-  return postProcessOutput(receiver.output)
+  return receiver.output
 }
