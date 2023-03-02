a3f5121fe4057f70d14c78361b1e6dd511cb9501
Nikita.Skvortsov <Nikita.Skvortsov@jetbrains.com>
2023-03-02 12:10:35 +0100

    [android] restore comptibility with Gradle 3.0 IDEA-314166
    GitOrigin-RevId: 6cc72b5afe26b6f1ba3b0176cfbfb0d3f25b6698

--- gradle-tooling/studio-gradle-tooling-impl/src/com/android/ide/gradle/model/composites/BuildMapModelBuilder.kt
+++ gradle-tooling/studio-gradle-tooling-impl/src/com/android/ide/gradle/model/composites/BuildMapModelBuilder.kt
@@ -2,6 +2,7 @@ package com.android.ide.gradle.model.composites
 
 import org.gradle.api.Project
 import org.gradle.tooling.provider.model.ToolingModelBuilder
+import org.gradle.util.GradleVersion
 import java.io.File
 
 class BuildMapModelBuilder : ToolingModelBuilder {
@@ -19,6 +20,7 @@ class BuildMapModelBuilder : ToolingModelBuilder {
   private fun buildBuildMap(project: Project): BuildMap = BuildMapImpl(getBuildMap(project))
 
   private fun getBuildMap(project: Project): Map<String, File> {
+    if (GradleVersion.version(project.gradle.gradleVersion) < GradleVersion.version("3.1")) return emptyMap()
     return project.gradle.includedBuilds.associate { it.name to it.projectDir }
   }
 }
