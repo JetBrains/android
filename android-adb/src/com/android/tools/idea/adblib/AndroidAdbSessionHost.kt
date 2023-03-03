fe8e58fbcfe57df8a2255e5c9b51b75e977cb106
Renaud Paquay <rpaquay@google.com>
2023-03-03 10:34:46 -0800

    adblib: Add "boost" support for JDWP processes
    Given the `WAIT` packet behavior (see b/271572555 for
    context) and the goal to eventually allow multiple instances
    of Android Studio (or IntelliJ + Android plugin) running
    concurrently when acquiring JDWP sessions on Android
    device, this change implements "boosting" JDWP process
    discovery when the Android Studio application is activated,
    and "un-boosting" when de-activated.
    
    This ensures the currently active Android Studio instance
    will always have priority accessing the JDWP session
    of new processes, which is critical for debugging (and
    useful for other use cases, such are running an app).
    
    The goal is to make sure we get to a state where having
    multiple instances of Studio (or IJ+Android plugin)
    running at the same time "just works" (*) from an
    end-user perspective.
    
    (*) "just works": most of the time, given that we
    can't rely build a fool proof solution given the current
    limitations of the Android Debugger and the way JDWP
    sessions are contrained.
    
    Bug: 271572555
    Test: Included
    Change-Id: I899bd8dafd0f5fb6e9c6e883aad7bc2aae5d7564
    
    GitOrigin-RevId: fa4f8dc7d1921d313041700081f2c5b5622a5816

--- android-adb/src/com/android/tools/idea/adblib/AndroidAdbSessionHost.kt
+++ android-adb/src/com/android/tools/idea/adblib/AndroidAdbSessionHost.kt
@@ -16,8 +16,17 @@
 package com.android.tools.idea.adblib
 
 import com.android.adblib.AdbSessionHost
+import com.android.adblib.tools.AdbLibToolsProperties
 import com.android.tools.idea.concurrency.AndroidDispatchers
 import com.android.tools.idea.concurrency.androidCoroutineExceptionHandler
+import com.intellij.application.subscribe
+import com.intellij.openapi.application.ApplicationActivationListener
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.diagnostic.debug
+import com.intellij.openapi.diagnostic.thisLogger
+import com.intellij.openapi.util.Disposer
+import com.intellij.openapi.wm.IdeFrame
+import java.util.concurrent.ConcurrentHashMap
 
 /**
  * Implementation of [AdbSessionHost] that integrates with the IntelliJ/Android Studio platform.
@@ -25,11 +34,59 @@ import com.android.tools.idea.concurrency.androidCoroutineExceptionHandler
  * See also [AndroidAdbLoggerFactory] and [AndroidDispatchers].
  */
 internal class AndroidAdbSessionHost : AdbSessionHost() {
+  private val log = thisLogger()
+
+  private val overriddenProperties = ConcurrentHashMap<Property<*>, Any>()
+
+  private val myActivationListener = MyActivationListener()
+
+  private val disposable = Disposer.newDisposable("AndroidAdbSessionHost disposable")
+
   override val loggerFactory = AndroidAdbLoggerFactory()
 
   override val parentContext = androidCoroutineExceptionHandler
 
+  init {
+    ApplicationActivationListener.TOPIC.subscribe(disposable, myActivationListener)
+    myActivationListener.boostJdwpProcessPropertiesCollector(ApplicationManager.getApplication().isActive)
+  }
+
+  fun <T: Any> overridePropertyValue(property: Property<T>, value: T) {
+    if (!property.isVolatile) {
+      throw IllegalArgumentException("Non volatile property value cannot be changed at runtime")
+    }
+    overriddenProperties[property] = value
+  }
+
+  override fun <T : Any> getPropertyValue(property: Property<T>): T {
+    @Suppress("UNCHECKED_CAST")
+    val result = overriddenProperties[property] as? T
+    return result ?: super.getPropertyValue(property)
+  }
+
   override fun close() {
-    // Nothing to do
+    Disposer.dispose(disposable)
+  }
+
+  inner class MyActivationListener : ApplicationActivationListener {
+    /**
+     * Notifies `adblib` whether to use the shortest possible delay to track
+     * and monitor new processes, so that this instance of Studio will get priority
+     * when running/debugging new JDWP processes.
+     *
+     * See b/271572555 for more context.
+     */
+    fun boostJdwpProcessPropertiesCollector(value: Boolean) {
+      log.debug { "boostJdwpProcessPropertiesCollector($value)" }
+      overridePropertyValue(AdbLibToolsProperties.PROCESS_PROPERTIES_COLLECTOR_DELAY_USE_SHORT, value)
+    }
+
+    override fun applicationActivated(ideFrame: IdeFrame) {
+      boostJdwpProcessPropertiesCollector(true)
+    }
+
+    override fun applicationDeactivated(ideFrame: IdeFrame) {
+      boostJdwpProcessPropertiesCollector(false)
+    }
   }
 }
