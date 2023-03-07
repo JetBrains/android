66c89f6dd4ebeeb6c69b552267961a6acd4385e8
Jinseong Jeon <jsjeon@google.com>
2023-03-06 17:35:00 -0800

    Adjust test expectation for call graph (only for .kt)
    Last change was tested with testKotlinCallGraph
    
    Bug: 269234399
    Test: //tools/adt/idea/kotlin-integration:kotlin-integration-tests_tests
        w/ and w/o --jvmopt='-Didea.kotlin.plugin.use.k2=true'
    Change-Id: Iecafdf29fc8e1d50a2dd15c74d53be10eac6e434
    
    GitOrigin-RevId: ecf5fde7020ab4392fac6c8cef04f79df3b03b9d

--- kotlin-integration/testSrc/com/android/tools/idea/callgraph/CallGraphTest.kt
+++ kotlin-integration/testSrc/com/android/tools/idea/callgraph/CallGraphTest.kt
@@ -101,7 +101,7 @@ class CallGraphTest : AndroidTestCase() {
     // In K1, `it` is of `It`, the explicit type, since assignments of `Impl` and `SubImpl` are aggregated.
     // Therefore, the resolution of `it.f()` started from `It#f` and traced all the overrides: `Impl#f` and `SubImpl#f`.
     // In contrast, in K2, the last assignment is tracked properly in DFA, so the resolution goes directly to `SubImpl#f`.
-    if (isK2Plugin()) {
+    if (isK2Plugin() && ext == ".kt") {
       "SimpleLocal#typeEvidencedBoth".assertCalls("Impl#Impl", "SubImpl#SubImpl", "SubImpl#f")
     } else {
       "SimpleLocal#typeEvidencedBoth".assertCalls("Impl#Impl", "SubImpl#SubImpl", "SubImpl#f", "Impl#f")
