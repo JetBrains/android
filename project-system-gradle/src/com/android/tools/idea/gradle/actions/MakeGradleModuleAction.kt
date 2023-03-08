f0edd23d0d64b4419eb4f2ec091b79696af7f07c
Ivan Gavrilovic <gavra@google.com>
2023-03-08 21:51:13 +0000

    Convert MakeGradleModuleAction to Kotlin
    ... to reduce diff in next changes.
    
    Bug: 272338755
    Test: manual and existing
    Change-Id: I29d8620a57a9c2b7e3515cb3e1d7b214390601e9
    
    GitOrigin-RevId: fdbda2cffbba8e552cde7ada10b2b0cbc1e7bbff

---
+++ project-system-gradle/src/com/android/tools/idea/gradle/actions/MakeGradleModuleAction.kt
@@ -0,0 +1,68 @@
+/*
+ * Copyright (C) 2013 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.gradle.actions
+
+import com.android.tools.idea.gradle.project.GradleProjectInfo
+import com.android.tools.idea.gradle.project.build.invoker.GradleBuildInvoker.Companion.getInstance
+import com.android.tools.idea.gradle.project.build.invoker.TestCompileType
+import com.intellij.openapi.actionSystem.ActionPlaces
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.project.Project
+
+class MakeGradleModuleAction : AndroidStudioGradleAction("Make Module(s)") {
+  override fun doUpdate(e: AnActionEvent, project: Project) {
+    updatePresentation(e, project)
+  }
+
+  public override fun doPerform(e: AnActionEvent, project: Project) {
+    val modules = GradleProjectInfo.getInstance(project).getModulesToBuildFromSelection(e.dataContext)
+    getInstance(project).assemble(modules, TestCompileType.ALL)
+  }
+
+  companion object {
+    @JvmStatic
+    fun updatePresentation(e: AnActionEvent, project: Project) {
+      val dataContext = e.dataContext
+      val modules = GradleProjectInfo.getInstance(project).getModulesToBuildFromSelection(dataContext)
+      val moduleCount = modules.size
+      val presentation = e.presentation
+      presentation.isEnabled = moduleCount > 0
+      val presentationText: String
+      if (moduleCount > 0) {
+        var text = StringBuilder("Make Module")
+        if (moduleCount > 1) {
+          text.append("s")
+        }
+        for (i in 0 until moduleCount) {
+          if (text.length > 30) {
+            text = StringBuilder("Make Selected Modules")
+            break
+          }
+          val toMake = modules[i]
+          if (i != 0) {
+            text.append(",")
+          }
+          text.append(" '").append(toMake.name).append("'")
+        }
+        presentationText = text.toString()
+      } else {
+        presentationText = "Make"
+      }
+      presentation.text = presentationText
+      presentation.isVisible = moduleCount > 0 || ActionPlaces.PROJECT_VIEW_POPUP != e.place
+    }
+  }
+}
