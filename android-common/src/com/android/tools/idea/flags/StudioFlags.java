3a1712e30a63c8847692941e0e7e1e6f6c0dc793
Diego Perez <diegoperez@google.com>
2023-03-15 09:47:52 +0000

    Enable allocation tracker in dev builds
    Test: N/A
    Bug: N/A
    Change-Id: If1665b3fd13c4662b1bba24b6ab18c46dc86f628
    
    GitOrigin-RevId: 6171ca56ada4a07c3742802a37e86332a8cfffab

--- android-common/src/com/android/tools/idea/flags/StudioFlags.java
+++ android-common/src/com/android/tools/idea/flags/StudioFlags.java
@@ -1346,7 +1346,7 @@ public final class StudioFlags {
   public static final Flag<Boolean> COMPOSE_ALLOCATION_LIMITER = Flag.create(
     COMPOSE, "allocation.limiter", "If enabled, limits allocations per render",
     "If enabled, limits the number of allocations that user code can do in a single render action",
-    false);
+    java.lang.Boolean.getBoolean("idea.is.internal"));
   public static final Flag<Boolean> COMPOSE_PREVIEW_SELECTION = Flag.create(
     COMPOSE, "compose.preview.selection", "Enable the select/deselect interaction with Previews",
     "If enabled, Previews will be selectable, and some interactions will only be enabled for selected Previews",
