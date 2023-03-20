25470c861318dac06c7cba57142c7f1b90626ab0
Peter Xu <peterx@google.com>
2023-03-20 12:12:23 -0700

    Copy CrashlyticsService to insights module
    Renamed it AppInsightsConfigurationManager.
    
    Test: n/a
    Bug: 263259654
    Change-Id: I7070b9474ad6d944e132c587ab23c473b735b71c
    
    GitOrigin-RevId: 61df80293bbac18de46264b790a7ae3de0fccb85

---
+++ app-quality-insights/api/src/com/android/tools/idea/insights/AppInsightsModel.kt
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.insights
+
+/**
+ * Models App Insight states.
+ *
+ * App Insights functionality currently has a hard dependency on the user being logged into Android
+ * Studio with their Google account. Without it, none of the functionality is meaningful since we
+ * are unable to make any API calls. To express this dependency the model provides [Unauthenticated]
+ * and [Authenticated] states.
+ */
+sealed class AppInsightsModel {
+  /** The user is not signed in, App Insights will not work until they do. */
+  object Unauthenticated : AppInsightsModel()
+
+  /**
+   * When the user is signed in, we pass in [AppInsightsProjectLevelController] which manages App
+   * Insights state data.
+   */
+  class Authenticated(val controller: AppInsightsProjectLevelController) : AppInsightsModel()
+}
