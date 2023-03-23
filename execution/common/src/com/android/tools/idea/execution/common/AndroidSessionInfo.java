acc7759c0af5c96cfb182e43e957c17e014788f9
Polina Koval <kovalp@google.com>
2023-03-23 16:57:38 +0000

    Use getProcessHandlersForDevices in TestRecorderAction
    ... instead of AndroidSessionInfo
    
    Remove AndroidSessionInfo.findOldSession as it not used anymore.
    
    Bug: 274103920
    Test: start recording when process is running (btw test recorder is
    broken later in code)
    
    Change-Id: I9f74e9a4663eb2c08820178e55aef593ec5c19ba
    
    GitOrigin-RevId: db00d428ad37af9e8bd35545ee17b1f38a092c93

--- execution/common/src/com/android/tools/idea/execution/common/AndroidSessionInfo.java
+++ execution/common/src/com/android/tools/idea/execution/common/AndroidSessionInfo.java
@@ -17,7 +17,6 @@
 package com.android.tools.idea.execution.common;
 
 import com.android.sdklib.AndroidVersion;
-import com.android.tools.idea.run.deployable.SwappableProcessHandler;
 import com.intellij.execution.ExecutionManager;
 import com.intellij.execution.ExecutionTarget;
 import com.intellij.execution.Executor;
@@ -37,7 +36,6 @@ public class AndroidSessionInfo {
   public static final Key<AndroidSessionInfo> KEY = new Key<>("KEY");
   public static final Key<AndroidVersion> ANDROID_DEVICE_API_LEVEL = new Key<>("ANDROID_DEVICE_API_LEVEL");
 
-  @NotNull private final ProcessHandler myProcessHandler;
   @NotNull private final String myExecutorId;
   @Nullable private final RunConfiguration myRunConfiguration;
   @NotNull private final ExecutionTarget myExecutionTarget;
@@ -56,15 +54,9 @@ public class AndroidSessionInfo {
                              @Nullable RunConfiguration runConfiguration,
                              @NotNull String executorId,
                              @NotNull ExecutionTarget executionTarget) {
-    myProcessHandler = processHandler;
     myRunConfiguration = runConfiguration;
     myExecutorId = executorId;
     myExecutionTarget = executionTarget;
-  }
-
-  @NotNull
-  public ProcessHandler getProcessHandler() {
-    return myProcessHandler;
   }
 
   @NotNull
@@ -94,77 +86,5 @@ public class AndroidSessionInfo {
   public static List<AndroidSessionInfo> findActiveSession(@NotNull Project project) {
     return Arrays.stream(ExecutionManager.getInstance(project).getRunningProcesses()).map(
       handler -> handler.getUserData(KEY)).filter(Objects::nonNull).collect(Collectors.toList());
-  }
-
-  @Nullable
-  public static AndroidSessionInfo findOldSession(@NotNull Project project,
-                                                  @Nullable Executor executor,
-                                                  @NotNull RunConfiguration runConfiguration,
-                                                  @NotNull ExecutionTarget executionTarget) {
-    // Note: There are 2 alternatives here:
-    //    1. RunContentManager.getInstance(project).getAllDescriptors()
-    //    2. ExecutionManagerImpl.getInstance(project).getRunningDescriptors
-    // The 2nd one doesn't work since its implementation relies on the same run descriptor to be alive as the one that is launched,
-    // but that doesn't work for android debug sessions where we have 2 process handlers (one while installing and another while debugging)
-    List<AndroidSessionInfo> infos = Arrays.stream(ExecutionManager.getInstance(project).getRunningProcesses())
-      .filter(handler -> !handler.isProcessTerminating() && !handler.isProcessTerminated())
-      .map(handler -> handler.getUserData(KEY))
-      .filter(info -> info != null &&
-                      runConfiguration == info.getRunConfiguration() &&
-                      (executor == null || executor.getId().equals(info.getExecutorId())))
-      .collect(Collectors.toList());
-
-    // There are many scenarios to check here. Given the assumption that:
-    // 1) If executionTarget is the DefaultExecutionTarget, then we assume that it's either an old-style run, or a dropdown run with
-    //    multiple devices.
-    // 2) Otherwise, it should be an AndroidExecutionTarget.
-    //
-    // Then given the above two scenarios, we have the following cross product of cases [executionTarget x myExecutionTarget]:
-    // [AndroidExecutionTarget, AndroidExecutionTarget]: Just check if they are the same.
-    // [AndroidExecutionTarget, DefaultExecutionTarget]: Peek into myProcessHandler and check if it contains executionTarget.
-    // [DefaultExecutionTarget, AndroidExecutionTarget]: Multi-device launch, so check if myExecutionTarget is contained in executionTarget.
-    // [DefaultExecutionTarget, DefaultExecutionTarget]: Multi-device or old-style launch, then just return true (only 1 allowed to exist).
-    if (executionTarget instanceof AndroidExecutionTarget) {
-      for (AndroidSessionInfo info : infos) {
-        ExecutionTarget sessionExecutionTarget = info.getExecutionTarget();
-        if (sessionExecutionTarget instanceof AndroidExecutionTarget) {
-          if (sessionExecutionTarget.getId().equals(executionTarget.getId())) {
-            return info;
-          }
-        }
-        else {
-          if (checkIfIDeviceRunningInProcessHandler(runConfiguration, (AndroidExecutionTarget)executionTarget, info.getProcessHandler())) {
-            return info;
-          }
-        }
-      }
-      return null;
-    }
-    else {
-      for (AndroidSessionInfo info : infos) {
-        ExecutionTarget sessionExecutionTarget = info.getExecutionTarget();
-        if (sessionExecutionTarget instanceof AndroidExecutionTarget) {
-          assert info.myRunConfiguration != null;
-          if (checkIfIDeviceRunningInProcessHandler(info.myRunConfiguration, (AndroidExecutionTarget)sessionExecutionTarget,
-                                                    info.getProcessHandler())) {
-            return info;
-          }
-        }
-        else {
-          return info;
-        }
-      }
-      return null;
-    }
-  }
-
-  private static boolean checkIfIDeviceRunningInProcessHandler(@NotNull RunConfiguration runConfiguration,
-                                                               @NotNull AndroidExecutionTarget executionTarget,
-                                                               @NotNull ProcessHandler processHandler) {
-    if (processHandler instanceof SwappableProcessHandler) {
-      return ((SwappableProcessHandler)processHandler).isRunningWith(runConfiguration, executionTarget);
-    }
-
-    return false;
   }
 }
