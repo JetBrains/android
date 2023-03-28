f11a848a3046971add03457df41d7de4990e853d
Tor Norbye <tnorbye@google.com>
2023-03-28 06:46:08 -0700

    Switch targetSdkVersion warning quickfixes over to assistant
    The quickfixes for lint's "not using latest targetSdkVersion" warning,
    and "edited targetSdkVersion in editor" warning, are both pointing
    to the TargetSdkVersion Assistant. However, there were two other
    lint checks which offered quickfixes to just replace the value with
    the latest; this CL also replaces these warnings' quickfixes with
    the assistant launcher instead.
    
    Test: Manual
    Bug: Not filed
    Change-Id: Id9a5426b6698931ff721076c6a3453d05441eeed
    
    GitOrigin-RevId: e6cf5a13e9c606a36bd3af6d160dccfc25600296

--- android-lint/src/com/android/tools/idea/lint/inspections/AndroidLintExpiringTargetSdkVersionInspection.java
+++ android-lint/src/com/android/tools/idea/lint/inspections/AndroidLintExpiringTargetSdkVersionInspection.java
@@ -15,12 +15,20 @@
  */
 package com.android.tools.idea.lint.inspections;
 
+import com.android.annotations.NonNull;
 import com.android.tools.idea.lint.common.AndroidLintInspectionBase;
 import com.android.tools.idea.lint.AndroidLintBundle;
+import com.android.tools.idea.lint.common.LintIdeQuickFix;
 import com.android.tools.lint.checks.GradleDetector;
 
 public class AndroidLintExpiringTargetSdkVersionInspection extends AndroidLintInspectionBase {
   public AndroidLintExpiringTargetSdkVersionInspection() {
     super(AndroidLintBundle.message("android.lint.inspections.expiring.target.sdk.version"), GradleDetector.EXPIRING_TARGET_SDK_VERSION);
+  }
+
+  @NonNull
+  @Override
+  public LintIdeQuickFix[] getQuickFixes(@NonNull String message) {
+    return new LintIdeQuickFix[] { new LaunchTargetSdkVersionAssistantFix() };
   }
 }
