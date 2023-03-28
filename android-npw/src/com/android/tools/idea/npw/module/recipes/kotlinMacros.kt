37677fae494a142a198edcd6848e447fa3808157
Takeshi Hagikura <thagikura@google.com>
2023-03-28 18:13:49 +0900

    Adds kotlin-bom dependency to avoid the duplicated kotlin-std classes
    That happened when:
    1.
    - The version of kotlin plugin is smaller than 1.8.0
    - When the app has a transitive dependency to kotlin-stdlib:1.8.0
    
    OR
    
    2.
    - The version of kotlin plugin is greater or equal to 1.8.0
    - When the app has a dependency that has an direct dependency to
      kotlin-stdlib:1.7.2 (< 1.8)
    - When the app has a transitive dependency to kotlin-stdlib:1.8.0
    
    But we are not adding direct kotlin-stdlib dependency from the
    templates, we care the #1 scenario.
    
    This CL adds a kotlin-bom:1.8.10 platform dependency for a new Kotlin
    module if the kotlin gradle plugin version is < 1.8 to prevent the
    duplicated kotlin-std classes as explained in the Kotlin doc
    
    Fixes: 274526042
    Test: KotlinBomPlatformDependencyTest
    Change-Id: I665e414d39b31a8c967657460cb049a4a27c8dc0
    
    GitOrigin-RevId: 11fa54b67c1f25a951ff68a552bcbe3a51a39eb4

--- android-npw/src/com/android/tools/idea/npw/module/recipes/kotlinMacros.kt
+++ android-npw/src/com/android/tools/idea/npw/module/recipes/kotlinMacros.kt
@@ -18,7 +18,7 @@ package com.android.tools.idea.npw.module.recipes
 import com.android.tools.idea.wizard.template.Language
 import com.android.tools.idea.wizard.template.ProjectTemplateData
 import com.android.tools.idea.wizard.template.RecipeExecutor
-
+import org.jetbrains.kotlin.config.ApiVersion
 
 fun RecipeExecutor.addKotlinDependencies(androidX: Boolean, targetApi: Int) {
   if (androidX) {
@@ -37,5 +37,16 @@ fun RecipeExecutor.addKotlinIfNeeded(data: ProjectTemplateData, targetApi: Int, 
     setKotlinVersion(data.kotlinVersion)
     applyPlugin("org.jetbrains.kotlin.android", data.kotlinVersion)
     addKotlinDependencies(data.androidXSupport && !noKtx, targetApi)
+
+    val kotlinVersion = ApiVersion.parse(data.kotlinVersion)
+    if (kotlinVersion != null && kotlinVersion < ApiVersion.KOTLIN_1_8) {
+      // This is to avoid the class duplication from different kotlin-stdlib versions
+      // See https://kotlinlang.org/docs/whatsnew18.html#usage-of-the-latest-kotlin-stdlib-version-in-transitive-dependencies
+      // for more details.
+      // Adding this solves duplicated classes issues in kotlin-stdlib in following possible scenario
+      // 1. - When Kotlin gradle plugin < 1.8 (e.g. 1.7.20)
+      //    - When the app has transitive dependency to kotlin-stdlib:1.8.0 (e.g. "androidx.lifecycle:lifecycle-livedata-ktx:2.6.1")
+      addPlatformDependency("org.jetbrains.kotlin:kotlin-bom:1.8.0")
+    }
   }
 }
---
+++ android-templates/testSrc/com/android/tools/idea/templates/KotlinBomPlatformDependencyTest.kt
@@ -0,0 +1,151 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.templates
+
+import com.android.sdklib.SdkVersionInfo
+import com.android.testutils.MockitoKt
+import com.android.tools.idea.gradle.npw.project.GradleAndroidModuleTemplate
+import com.android.tools.idea.lint.common.getModuleDir
+import com.android.tools.idea.npw.model.NewAndroidModuleModel
+import com.android.tools.idea.npw.model.ProjectSyncInvoker
+import com.android.tools.idea.npw.module.ModuleModel
+import com.android.tools.idea.npw.platform.AndroidVersionsInfo
+import com.android.tools.idea.npw.template.ProjectTemplateDataBuilder
+import com.android.tools.idea.templates.recipe.DefaultRecipeExecutor
+import com.android.tools.idea.templates.recipe.RenderingContext
+import com.android.tools.idea.testing.AgpVersionSoftwareEnvironmentDescriptor
+import com.android.tools.idea.testing.AgpVersionSoftwareEnvironmentDescriptor.Companion.AGP_CURRENT
+import com.android.tools.idea.testing.AndroidGradleProjectRule
+import com.android.tools.idea.testing.TestProjectPaths
+import com.android.tools.idea.testing.withKotlin
+import com.android.tools.idea.util.toIoFile
+import com.android.tools.idea.wizard.template.Category
+import com.android.tools.idea.wizard.template.FormFactor
+import com.android.tools.idea.wizard.template.Language
+import com.android.tools.idea.wizard.template.ModuleTemplateData
+import com.android.utils.FileUtils
+import com.intellij.openapi.command.WriteCommandAction.runWriteCommandAction
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.project.guessProjectDir
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import java.io.File
+
+class KotlinBomPlatformDependencyTest {
+
+  @get:Rule
+  val projectRule = AndroidGradleProjectRule()
+  private val module by lazy { projectRule.getModule("app") }
+  private val mockModuleTemplateData = MockitoKt.mock<ModuleTemplateData>()
+  private val myModule = "mymodule"
+
+  private val renderingContext by lazy {
+    RenderingContext(
+      projectRule.project,
+      module,
+      "KotlinBomPlatformDependencyTest",
+      mockModuleTemplateData,
+      moduleRoot = module.getModuleDir(),
+      dryRun = false,
+      showErrors = true
+    )
+  }
+
+  private val projectTemplateDataBuilder by lazy {
+    ProjectTemplateDataBuilder(true).apply {
+      androidXSupport = true
+      setProjectDefaults(projectRule.project)
+      language = Language.Kotlin
+      topOut = projectRule.project.guessProjectDir()!!.toIoFile()
+      debugKeyStoreSha1 = KeystoreUtils.sha1(KeystoreUtils.getOrCreateDefaultDebugKeystore())
+      applicationPackage = "com.example"
+      overridePathCheck = true // To disable android plugin checking for ascii in paths (windows tests)
+    }
+  }
+
+  private val moduleModel by lazy {
+    NewAndroidModuleModel.fromExistingProject(
+      project = projectRule.project,
+      moduleParent = ":",
+      projectSyncInvoker = emptyProjectSyncInvoker,
+      formFactor = FormFactor.Mobile,
+      category = Category.Activity,
+      isLibrary = false
+    )
+  }
+
+  private val recipeExecutor by lazy {
+    DefaultRecipeExecutor(renderingContext)
+  }
+
+  // Ignore project sync (to speed up test), if later we are going to perform a gradle build anyway.
+  private val emptyProjectSyncInvoker = object: ProjectSyncInvoker {
+    override fun syncProject(project: Project) { }
+  }
+
+  @Before
+  fun setUp() {
+    MockitoKt.whenever(mockModuleTemplateData.projectTemplateData).thenReturn(projectTemplateDataBuilder.build())
+  }
+
+  @Test
+  fun kgp1_7_20_hasTransitiveDepTo_kotlinStdLib1_8_0() {
+    projectRule.load(TestProjectPaths.KOTLIN_WITH_VERSION_CATALOG, AGP_CURRENT.withKotlin("1.7.20"))
+
+    generateModuleFiles(projectRule.project, moduleModel)
+    val moduleDir = FileUtils.join(projectRule.project.basePath, myModule)
+    // Adds a dependency that has a transitive dependency to kotlin-stdlib:1.8.0
+    // to intentionally reproduce duplicated class without kotlin-bom as in
+    // https://kotlinlang.org/docs/whatsnew18.html#bumping-the-minimum-supported-versions
+    recipeExecutor.addDependency("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1", moduleDir = File(moduleDir))
+    runWriteCommandAction(projectRule.project) { recipeExecutor.applyChanges() }
+
+    assembleDebugProject()
+  }
+
+  @Test
+  fun kgp1_8_10_hasTransitiveDepToKotlinStdLib1_8_0() {
+    projectRule.load(TestProjectPaths.KOTLIN_WITH_VERSION_CATALOG, AGP_CURRENT.withKotlin("1.8.10"))
+
+    generateModuleFiles(projectRule.project, moduleModel)
+    val moduleDir = FileUtils.join(projectRule.project.basePath, myModule)
+    // Adds a dependency that has a transitive dependency to kotlin-stdlib:1.8.0
+    recipeExecutor.addDependency("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1", moduleDir = File(moduleDir))
+    runWriteCommandAction(projectRule.project) { recipeExecutor.applyChanges() }
+
+    // kotlin-bom isn't added because kotlinVersion is > 1.8. We still verify the app compiles
+    assembleDebugProject()
+  }
+
+  private fun assembleDebugProject() {
+    projectRule.invokeTasks("assembleDebug").apply {
+      buildError?.printStackTrace()
+      Assert.assertTrue("Project didn't compile correctly", isBuildSuccessful)
+    }
+  }
+
+  private fun generateModuleFiles(project: Project, model: ModuleModel) {
+    model.androidSdkInfo.value = AndroidVersionsInfo.VersionItem.fromStableVersion(SdkVersionInfo.HIGHEST_KNOWN_STABLE_API)
+    model.moduleName.set(myModule)
+    model.template.set(GradleAndroidModuleTemplate.createDefaultModuleTemplate(project, myModule))
+    model.packageName.set("com.example")
+    model.useGradleKts.set(true)
+
+    model.handleFinished() // Generate module files
+  }
+}
