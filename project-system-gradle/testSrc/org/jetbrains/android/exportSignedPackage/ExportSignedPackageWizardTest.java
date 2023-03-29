ed2a5543ba991fcb29e9261c850671ae1f2f51b0
Sergio Murguia <srmurguia@google.com>
2023-03-29 11:33:13 -0700

    Move code in ExportSignedPackageWizard
    ... so we can test signing in isolation.
    
    This change also adds tests to verify that signing is successful when a
    correct store, key alias and passwords are used and it fails when any
    of those parameters is incorrect.
    
    Bug: 249277725
    Test: ExportSignedPackageWizardTest
    Change-Id: Ia8437c6cecb95c4f2522ce133d56ac54893cf22f
    
    GitOrigin-RevId: 5cdaf647df7f8ee4287333da2422f021901ef406

--- project-system-gradle/testSrc/org/jetbrains/android/exportSignedPackage/ExportSignedPackageWizardTest.java
+++ project-system-gradle/testSrc/org/jetbrains/android/exportSignedPackage/ExportSignedPackageWizardTest.java
@@ -17,19 +17,90 @@ package org.jetbrains.android.exportSignedPackage;
 
 import static com.android.tools.idea.testing.TestProjectPaths.SIGNAPK_MULTIFLAVOR;
 import static com.android.tools.idea.testing.TestProjectPaths.SIGNAPK_NO_FLAVORS;
+import static com.android.tools.idea.testing.TestProjectPaths.SIMPLE_APPLICATION;
 import static com.intellij.openapi.util.io.FileUtil.toSystemDependentName;
 import static java.util.Arrays.asList;
 import static java.util.Collections.singletonList;
 import static org.jetbrains.android.exportSignedPackage.ExportSignedPackageWizard.getTaskNamesFromSelectedVariant;
 
 import com.android.tools.idea.gradle.model.IdeAndroidProject;
+import com.android.tools.idea.gradle.project.build.invoker.AssembleInvocationResult;
+import com.android.tools.idea.gradle.project.build.invoker.GradleInvocationResult;
+import com.android.tools.idea.gradle.project.model.GradleAndroidModel;
 import com.android.tools.idea.gradle.util.BuildMode;
+import com.android.tools.idea.projectsystem.ProjectSystemUtil;
 import com.android.tools.idea.testing.AndroidGradleTestCase;
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Sets;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.intellij.openapi.module.Module;
+import com.intellij.openapi.project.Project;
+import com.intellij.util.Consumer;
+import java.io.File;
 import java.util.List;
 import java.util.Set;
+import java.util.concurrent.ExecutionException;
+import org.jetbrains.android.facet.AndroidFacet;
+import org.jetbrains.annotations.NotNull;
 
 public class ExportSignedPackageWizardTest extends AndroidGradleTestCase {
+  private final char[] testKeystorePassword = "android".toCharArray();
+  private final String testKeyAlias = "androiddebugkey";
+  private final char[] testKeyPassword = "android".toCharArray();
+  private final Consumer<ListenableFuture<AssembleInvocationResult>> successHandler = future -> {
+    try {
+      AssembleInvocationResult result = future.get();
+      if (!result.isBuildSuccessful()) {
+        fail(getInvocationErrorsCause(result));
+      }
+    }
+    catch (InterruptedException | ExecutionException e) {
+      throw new RuntimeException(e);
+    }
+  };
+  private final Consumer<ListenableFuture<AssembleInvocationResult>> badStorePassHandler = future -> {
+    try {
+      AssembleInvocationResult result = future.get();
+      if (result.isBuildSuccessful()) {
+        fail("Build was successful even with a bad store password");
+      }
+      String buildErrorCause = getInvocationErrorsCause(result);
+      assertTrue("Build failed but without the expected cause: " + buildErrorCause,
+                 buildErrorCause.contains("Keystore was tampered with, or password was incorrect"));
+    }
+    catch (InterruptedException | ExecutionException e) {
+      throw new RuntimeException(e);
+    }
+  };
+  private final Consumer<ListenableFuture<AssembleInvocationResult>> badKeyAliasHandler = future -> {
+    try {
+      AssembleInvocationResult result = future.get();
+      if (result.isBuildSuccessful()) {
+        fail("Build was successful even with a bad key alias");
+      }
+      String buildErrorCause = getInvocationErrorsCause(result);
+      assertTrue("Build failed but without the expected cause: " + buildErrorCause,
+                 buildErrorCause.contains("No key with alias 'badKeyAlias' found in keystore"));
+    }
+    catch (InterruptedException | ExecutionException e) {
+      throw new RuntimeException(e);
+    }
+  };
+  private final Consumer<ListenableFuture<AssembleInvocationResult>> badKeyPassHandler = future -> {
+    try {
+      AssembleInvocationResult result = future.get();
+      if (result.isBuildSuccessful()) {
+        fail("Build was successful even with a bad key alias");
+      }
+      String buildErrorCause = getInvocationErrorsCause(result);
+      assertTrue("Build failed but without the expected cause: " + buildErrorCause,
+                 buildErrorCause.contains("Failed to read key androiddebugkey from store"));
+    }
+    catch (InterruptedException | ExecutionException e) {
+      throw new RuntimeException(e);
+    }
+  };
+
   public void testNoFlavors() throws Exception {
     loadProject(SIGNAPK_NO_FLAVORS);
     IdeAndroidProject androidProject = getModel().getAndroidProject();
@@ -163,5 +234,101 @@ public class ExportSignedPackageWizardTest extends AndroidGradleTestCase {
 
   public void testBuildModeFromBundleTargetType() {
     assertEquals(BuildMode.BUNDLE, ExportSignedPackageWizard.getBuildModeFromTarget(ExportSignedPackageWizard.BUNDLE));
+  }
+
+  /**
+   * Perform a sign for a bundle and confirm no errors were caused.
+   */
+  public void testSignBundle() throws Exception {
+    verifySigning(ExportSignedPackageWizard.BUNDLE, testKeystorePassword, testKeyAlias, testKeyPassword, successHandler);
+  }
+
+  /**
+   * Perform a sign for a bundle and confirm no errors were caused.
+   */
+  public void testSignApk() throws Exception {
+    verifySigning(ExportSignedPackageWizard.APK, testKeystorePassword, testKeyAlias, testKeyPassword, successHandler);
+  }
+
+  /**
+   * Sign bundle with bad store password should fail
+   */
+  public void testSignBundleBadStorePass() throws Exception {
+    verifySigning(ExportSignedPackageWizard.BUNDLE, "badStorePass".toCharArray(), testKeyAlias, testKeyPassword, badStorePassHandler);
+  }
+
+  /**
+   * Sign apk with bad store password should fail
+   */
+  public void testSignApkBadStorePass() throws Exception {
+    verifySigning(ExportSignedPackageWizard.APK, "badStorePass".toCharArray(), testKeyAlias, testKeyPassword, badStorePassHandler);
+  }
+
+  /**
+   * Sign bundle with bad key alias should fail
+   */
+  public void testSignBundleBadKeyAlias() throws Exception {
+    verifySigning(ExportSignedPackageWizard.BUNDLE, testKeystorePassword, "badKeyAlias", testKeyPassword, badKeyAliasHandler);
+  }
+
+  /**
+   * Sign apk with bad key alias should fail
+   */
+  public void testSignApkBadKeyAlias() throws Exception {
+    verifySigning(ExportSignedPackageWizard.APK, testKeystorePassword, "badKeyAlias", testKeyPassword, badKeyAliasHandler);
+  }
+
+  /**
+   * Sign bundle with bad key password should fail
+   */
+  public void testSignBundleBadKeyPass() throws Exception {
+    verifySigning(ExportSignedPackageWizard.BUNDLE, testKeystorePassword, testKeyAlias, "badKeyPass".toCharArray(), badKeyPassHandler);
+  }
+
+  /**
+   * Sign apk with bad key password should fail
+   */
+  public void testSignApkBadKeyPass() throws Exception {
+    verifySigning(ExportSignedPackageWizard.APK, testKeystorePassword, testKeyAlias, "badKeyPass".toCharArray(), badKeyPassHandler);
+  }
+
+  private void verifySigning(@NotNull String targetType,
+                             char[] storePass,
+                             String keyAlias,
+                             char[] keyPass,
+                             Consumer<ListenableFuture<AssembleInvocationResult>> buildResultHandler) throws Exception {
+    loadProject(SIMPLE_APPLICATION);
+    Project project = getProject();
+    List<AndroidFacet> facets =
+      ProjectSystemUtil.getAndroidFacets(project).stream().filter(facet -> facet.getConfiguration().isAppProject()).toList();
+    assertFalse(facets.isEmpty());
+    AndroidFacet facet = facets.get(0);
+    GradleAndroidModel androidModel = GradleAndroidModel.get(facet);
+    assertNotNull(androidModel);
+    List<String> variants = singletonList("release");
+    String keyStorePath = getTestDataPath() + File.separator + "signingKey" + File.separator + "debug.keystore";
+    GradleSigningInfo signingInfo = new GradleSigningInfo(keyStorePath, storePass, keyAlias, keyPass);
+    String apkPath = androidModel.getRootDirPath().getPath();
+    List<Module> modules = ImmutableList.of(facet.getMainModule());
+    ExportSignedPackageWizard.doBuildAndSignGradleProject(getProject(), facet, variants, modules, signingInfo, apkPath, targetType,
+                                                          buildResultHandler);
+  }
+
+  private String getInvocationErrorsCause(AssembleInvocationResult result) {
+    StringBuilder errorMessages = new StringBuilder("The build was not successful\n");
+    for (GradleInvocationResult invocation : result.getInvocationResult().getInvocations()) {
+      Throwable buildError = invocation.getBuildError();
+      String prefix = "";
+      while (buildError != null) {
+        prefix += "  ";
+        errorMessages.append(prefix).append(buildError.getMessage()).append("\n");
+        Throwable cause = buildError.getCause();
+        if (cause == buildError) {
+          break;
+        }
+        buildError = cause;
+      }
+    }
+    return errorMessages.toString();
   }
 }
