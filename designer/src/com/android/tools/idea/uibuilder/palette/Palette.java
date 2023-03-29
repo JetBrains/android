35429b43154516b2bd6db65ab14685c3e6463c7c
Jerome Gaillard <jgaillard@google.com>
2023-03-29 15:27:59 +0100

    Remove suggested attribute in palette
    It is unused.
    
    Bug: N/A
    Test: N/A
    Change-Id: Ib3a81de2de64183c65ad85c6b3f405ede16745f0
    
    GitOrigin-RevId: b5d157f90ad83135af9790574520f087a75d9e5c

--- designer/src/com/android/tools/idea/uibuilder/palette/Palette.java
+++ designer/src/com/android/tools/idea/uibuilder/palette/Palette.java
@@ -241,10 +241,6 @@ public class Palette {
     @Nullable
     private String myHandlerClass;
 
-    @XmlAttribute(name = "suggested")
-    @Nullable
-    private Boolean mySuggested;
-
     @XmlAttribute(name = "meta")
     @Nullable
     private String myMeta;
@@ -320,13 +316,6 @@ public class Palette {
         return myGradleCoordinateId;
       }
       return myHandler.getGradleCoordinateId(myTagName);
-    }
-
-    public boolean isSuggested() {
-      if (mySuggested != null) {
-        return mySuggested;
-      }
-      return false;
     }
 
     @NotNull
