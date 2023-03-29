45501d9fedfc4f64212061755faec4a0bf54ecac
Fedor Kudasov <kudasov@google.com>
2023-03-29 10:53:24 +0100

    Replace constant from ComposeLibraryNamespace
    with the more generic one from SdkConstants.
    
    Bug: 267588800
    Test: all tests should pass
    Change-Id: Icf7a34e4e76c6a6fcf7cfd24b50c58c54a74f725
    
    GitOrigin-RevId: d776fb46d63e4c15639fb5290d0eb5c8995fc995

--- android/src/com/android/tools/idea/rendering/parsers/TagSnapshot.java
+++ android/src/com/android/tools/idea/rendering/parsers/TagSnapshot.java
@@ -20,8 +20,8 @@ import static com.android.SdkConstants.ATTR_ATTR;
 import static com.android.SdkConstants.ATTR_NAME;
 import static com.android.SdkConstants.ATTR_USE_TAG;
 import static com.android.SdkConstants.CLASS_COMPOSE_VIEW;
+import static com.android.SdkConstants.CLASS_COMPOSE_VIEW_ADAPTER;
 import static com.android.SdkConstants.TOOLS_URI;
-import static com.android.tools.compose.ComposeLibraryNamespaceKt.COMPOSE_VIEW_ADAPTER_FQN;
 import static com.google.common.base.Charsets.UTF_8;
 
 import com.google.common.collect.Lists;
@@ -94,7 +94,7 @@ public class TagSnapshot {
     // ComposeView gets replaced with ComposeViewAdapter so it can be rendered correctly within the Layout Editor.
     // The ComposeView requires a ViewTreeLifecycleOwner but, since the Layout Editor does not run within an activity, there is not one.
     // ComposeViewAdapter provides that logic allowing for transparently replace it.
-    return CLASS_COMPOSE_VIEW.equals(tagName) ? COMPOSE_VIEW_ADAPTER_FQN : tagName;
+    return CLASS_COMPOSE_VIEW.equals(tagName) ? CLASS_COMPOSE_VIEW_ADAPTER : tagName;
   }
 
   /**
