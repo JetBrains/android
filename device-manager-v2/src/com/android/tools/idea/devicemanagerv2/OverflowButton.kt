969e55766ed61c6e028db79b857f182ff06a99d0
James Rose <rosej@google.com>
2023-04-03 10:59:57 -0700

    Add extend reservation actions to overflow menu
    Bug: 276780581
    Test: n/a
    Change-Id: Id4e96d97667ad7b65f34cefa33c68fb2d70357b2
    
    GitOrigin-RevId: 60bd409408412d803711c58d635d2f644f89ca72

--- device-manager-v2/src/com/android/tools/idea/devicemanagerv2/OverflowButton.kt
+++ device-manager-v2/src/com/android/tools/idea/devicemanagerv2/OverflowButton.kt
@@ -17,14 +17,28 @@ package com.android.tools.idea.devicemanagerv2
 
 import com.android.tools.adtui.categorytable.IconButton
 import com.intellij.ide.DataManager
+import com.intellij.ide.ui.customization.CustomActionsSchema
 import com.intellij.openapi.actionSystem.DefaultActionGroup
+import com.intellij.openapi.actionSystem.Separator
 import com.intellij.openapi.ui.popup.JBPopupFactory
 import icons.StudioIcons
 
 class OverflowButton : IconButton(StudioIcons.Common.OVERFLOW) {
 
   companion object {
-    val actions = DefaultActionGroup(EditDeviceAction())
+    private val reservationActions =
+      DefaultActionGroup(
+        CustomActionsSchema.getInstance()
+          .getCorrectedAction("android.device.reservation.extend.half.hour"),
+        CustomActionsSchema.getInstance()
+          .getCorrectedAction("android.device.reservation.extend.one.hour"),
+      )
+    val actions =
+      DefaultActionGroup(
+        EditDeviceAction(),
+        Separator.create(),
+        reservationActions,
+      )
   }
 
   init {
