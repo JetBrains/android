106edfddd1723ba189a1db81ce0bcffbb07ecb63
Fedor Kudasov <kudasov@google.com>
2023-04-05 16:43:49 +0100

    Free TagSnapshotTest of intellij dependency
    There is no reason to use intellij project to parse XML to test
    TagSnapshot. Instead, XML can be parsed with some helpers based on
    KXmlParser.
    
    Bug: 267588800
    Test: TagSnapshotTest
    Change-Id: Iaa38e6bfb294804c111d5f5bdfb5a5b77c09879c
    
    GitOrigin-RevId: 881204f7879bed9601ecfee66e2820152e67541c

--- android/testSrc/com/android/tools/idea/rendering/parsers/TagSnapshotTest.java
+++ android/testSrc/com/android/tools/idea/rendering/parsers/TagSnapshotTest.java
@@ -23,23 +23,17 @@ import static com.android.SdkConstants.VALUE_VERTICAL;
 import static com.android.SdkConstants.VALUE_WRAP_CONTENT;
 import static com.google.common.truth.Truth.assertThat;
 
-import com.android.tools.idea.testing.AndroidProjectRule;
 import com.android.tools.rendering.parsers.RenderXmlTag;
-import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.util.Computable;
-import com.intellij.psi.XmlElementFactory;
 import java.util.Collections;
 import java.util.concurrent.atomic.AtomicInteger;
 import org.intellij.lang.annotations.Language;
-import org.junit.Rule;
 import org.junit.Test;
 
 public class TagSnapshotTest {
-  @Rule
-  public AndroidProjectRule myProjectRule = AndroidProjectRule.inMemory();
 
   @Test
   public void testSnapshotCreatingIf() {
+
     @Language("XML") final String layoutString = "<LinearLayout xmlns:android=\"http://schemas.android.com/apk/res/android\"\n" +
                                                  "  android:orientation=\"vertical\">\"\n" +
                                                  "  <Button\n" +
@@ -52,30 +46,26 @@ public class TagSnapshotTest {
 
     AtomicInteger callCount = new AtomicInteger(0);
 
-    TagSnapshot snapshot = ApplicationManager.getApplication().runReadAction((Computable<TagSnapshot>)() -> {
-      RenderXmlTag linearLayout = new PsiXmlTag(XmlElementFactory.getInstance(myProjectRule.getProject()).createTagFromText(layoutString));
-      return TagSnapshot.createTagSnapshot(linearLayout, (tag) -> {
-        callCount.incrementAndGet();
+    RenderXmlTag linearLayout = TestXmlParser.getRootTag(layoutString);
+    TagSnapshot snapshot = TagSnapshot.createTagSnapshot(linearLayout, (tag) -> {
+      callCount.incrementAndGet();
 
-        if ("LinearLayout".equals(tag.tagName)) {
-          assertThat(tag.getAttribute(ATTR_ORIENTATION, ANDROID_URI)).isEqualTo(VALUE_VERTICAL);
-          assertThat(tag.children.size()).isEqualTo(2);
-        }
-        else {
-          assertThat(tag.getAttribute(ATTR_LAYOUT_WIDTH, ANDROID_URI)).isEqualTo(VALUE_WRAP_CONTENT);
-          assertThat(tag.getAttribute(ATTR_LAYOUT_HEIGHT, ANDROID_URI)).isEqualTo(VALUE_WRAP_CONTENT);
-          assertThat(tag.children.size()).isEqualTo(0);
-        }
-      });
+      if ("LinearLayout".equals(tag.tagName)) {
+        assertThat(tag.getAttribute(ATTR_ORIENTATION, ANDROID_URI)).isEqualTo(VALUE_VERTICAL);
+        assertThat(tag.children.size()).isEqualTo(2);
+      }
+      else {
+        assertThat(tag.getAttribute(ATTR_LAYOUT_WIDTH, ANDROID_URI)).isEqualTo(VALUE_WRAP_CONTENT);
+        assertThat(tag.getAttribute(ATTR_LAYOUT_HEIGHT, ANDROID_URI)).isEqualTo(VALUE_WRAP_CONTENT);
+        assertThat(tag.children.size()).isEqualTo(0);
+      }
     });
 
     // Once per element
     assertThat(callCount.get()).isEqualTo(3);
-    TagSnapshot synthetic = ApplicationManager.getApplication().runReadAction((Computable<TagSnapshot>)() -> {
-      RenderXmlTag tag = new PsiXmlTag(XmlElementFactory.getInstance(myProjectRule.getProject()).createTagFromText("<transformed />"));
-      return TagSnapshot.createSyntheticTag(tag, "synthetic", null, null,
-                                            Collections.emptyList(), Collections.emptyList(), null);
-    });
+    RenderXmlTag tag = TestXmlParser.getRootTag("<transformed />");
+    TagSnapshot synthetic = TagSnapshot.createSyntheticTag(tag, "synthetic", null, null,
+                                                           Collections.emptyList(), Collections.emptyList(), null);
     synthetic.children = Collections.singletonList(snapshot);
     assertThat(synthetic.toString())
       .isEqualTo("TagSnapshot{synthetic, attributes=[], children=\n" +
@@ -101,10 +91,8 @@ public class TagSnapshotTest {
                                                 "  </aapt:attr>\n" +
                                                 "</ImageView>";
 
-    TagSnapshot root = ApplicationManager.getApplication().runReadAction((Computable<TagSnapshot>)() -> {
-      RenderXmlTag image = new PsiXmlTag(XmlElementFactory.getInstance(myProjectRule.getProject()).createTagFromText(imageString));
-      return TagSnapshot.createTagSnapshot(image, null);
-    });
+    RenderXmlTag image = TestXmlParser.getRootTag(imageString);
+    TagSnapshot root = TagSnapshot.createTagSnapshot(image, null);
     String expectedId = Long.toString(AaptAttrAttributeSnapshot.ourUniqueId.get() - 1);
     assertThat(root.toString()).isEqualTo(
       "TagSnapshot{ImageView, attributes=[AttributeSnapshot{layout_width=\"wrap_content\"}, AttributeSnapshot{layout_height=\"wrap_content\"}, AttributeSnapshot{src=\"@aapt:_aapt/aapt" +
@@ -123,10 +111,8 @@ public class TagSnapshotTest {
                                                 "  android:layout_height=\"wrap_content\"\n" +
                                                 "  tools:useTag=\"Button\" />";
 
-    TagSnapshot button = ApplicationManager.getApplication().runReadAction((Computable<TagSnapshot>)() -> {
-      RenderXmlTag image = new PsiXmlTag(XmlElementFactory.getInstance(myProjectRule.getProject()).createTagFromText(imageString));
-      return TagSnapshot.createTagSnapshot(image, null);
-    });
+    RenderXmlTag image = TestXmlParser.getRootTag(imageString);
+    TagSnapshot button = TagSnapshot.createTagSnapshot(image, null);
     assertThat(button.toString()).isEqualTo(
       "TagSnapshot{Button, attributes=[AttributeSnapshot{layout_width=\"wrap_content\"}, AttributeSnapshot{layout_height=\"wrap_content\"}, AttributeSnapshot{useTag=\"Button\"}], children=\n" +
       "[]\n" +
@@ -139,10 +125,8 @@ public class TagSnapshotTest {
                                                 "  android:layout_width=\"wrap_content\"\n" +
                                                 "  android:layout_height=\"wrap_content\" />";
 
-    TagSnapshot button = ApplicationManager.getApplication().runReadAction((Computable<TagSnapshot>)() -> {
-      RenderXmlTag image = new PsiXmlTag(XmlElementFactory.getInstance(myProjectRule.getProject()).createTagFromText(imageString));
-      return TagSnapshot.createTagSnapshot(image, null);
-    });
+    RenderXmlTag image = TestXmlParser.getRootTag(imageString);
+    TagSnapshot button = TagSnapshot.createTagSnapshot(image, null);
     assertThat(button.toString()).isEqualTo(
       "TagSnapshot{androidx.compose.ui.tooling.ComposeViewAdapter, attributes=[AttributeSnapshot{layout_width=\"wrap_content\"}, AttributeSnapshot{layout_height=\"wrap_content\"}], children=\n" +
       "[]\n" +
