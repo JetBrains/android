1f6fb091a274f6066176601dedfcd433e37cdbcf
Madalin Valceleanu <vmadalin@google.com>
2023-04-06 11:55:15 +0100

    Disable all "unknown.sdk" registries to avoid displaying suggestions
    The platform UnknownSdkTracker is strictly linked with provided
    Project JDK under project-jdk-name on .idea/misc.xml, displaying a
    banner message only on specific files like java or kt, when users
    opened the project with an invalid jdk.table entry.
    Those are some messages:
     - JDK "X" is missing (no jdk entry)
     - JDK "Y" is not found on the disc or corrupted (wrong jdk path)
    
    This creates confusion for the user that just opened the project, but
    at the same time is somehow conflicting with the recent logic we have
    in place about updating the Project JDK after a successful sync.
    Meaning that in any case, any user selection is going to be
    overriding with the specified Gradle JDK for consistency reasons but
    also to guarantee a valid JDK will be used to resolve project symbols
    
    Bug: 236055498
    Test: UnknownSdkTrackerTest
    Change-Id: I878b9e409e69df491d4ed62b0cfa9ca1759e4a0b
    
    GitOrigin-RevId: a8164a8d1bbccb38567991c496106ce33224d114

---
+++ project-system-gradle/testSrc/com/android/tools/idea/gradle/project/sync/jdk/UnknownSdkTrackerTest.kt
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.gradle.project.sync.jdk
+
+import com.intellij.openapi.application.impl.NonBlockingReadActionImpl
+import com.intellij.openapi.application.runWriteActionAndWait
+import com.intellij.openapi.projectRoots.impl.UnknownSdkEditorNotification
+import com.intellij.openapi.projectRoots.impl.UnknownSdkTracker
+import com.intellij.openapi.roots.ProjectRootManager
+import com.intellij.testFramework.IdeaTestUtil
+import com.intellij.testFramework.LightPlatformTestCase
+import com.intellij.util.ui.UIUtil
+import org.junit.Test
+
+class UnknownSdkTrackerTest: LightPlatformTestCase() {
+
+  @Test
+  fun `test Given disabled UnknownSdkTracker When project JDK is invalid Then no notification was triggered`() {
+    runWriteActionAndWait {
+      ProjectRootManager.getInstance(project).projectSdk = IdeaTestUtil.getMockJdk17()
+    }
+    UnknownSdkTracker.getInstance(project).updateUnknownSdks()
+
+    NonBlockingReadActionImpl.waitForAsyncTaskCompletion()
+    UIUtil.dispatchAllInvocationEvents()
+    assertEmpty(UnknownSdkEditorNotification.getInstance(project).notifications)
+  }
+}
