0875d585ac4d6182c185962f18cd5bc5b34dabcb
Joe Baker-Malone <jbakermalone@google.com>
2023-04-21 14:54:58 -0700

    Make SdkQuickfixUtilsTest a non-gradle test
    Hopefully this will fix observed flakiness
    
    Test: this is a test fix
    Bug: 271928654
    Change-Id: If4f7c41348324e52067df24c66738bc8ad73a3f2
    
    GitOrigin-RevId: a8f3fbe39772377a923a0961829ac4fe36bb38c6

--- android/testSrc/com/android/tools/idea/sdk/wizard/SdkQuickfixUtilsTest.java
+++ android/testSrc/com/android/tools/idea/sdk/wizard/SdkQuickfixUtilsTest.java
@@ -16,13 +16,15 @@
 package com.android.tools.idea.sdk.wizard;
 
 import static com.google.common.truth.Truth.assertThat;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertSame;
 import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.eq;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
-import static org.mockito.MockitoAnnotations.initMocks;
 
 import com.android.repository.api.LocalPackage;
 import com.android.repository.api.RepoManager;
@@ -32,26 +34,33 @@ import com.android.repository.testframework.FakeProgressIndicator;
 import com.android.repository.testframework.FakeRepoManager;
 import com.android.sdklib.repository.AndroidSdkHandler;
 import com.android.testutils.file.InMemoryFileSystems;
-import com.android.tools.idea.testing.AndroidGradleTestCase;
+import com.android.tools.idea.testing.AndroidProjectRule;
 import com.android.tools.idea.wizard.model.ModelWizardDialog;
 import com.google.common.collect.ImmutableList;
 import com.intellij.openapi.util.Disposer;
+import com.intellij.testFramework.EdtRule;
+import com.intellij.testFramework.RunsInEdt;
 import java.nio.file.Path;
 import java.util.Collections;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
 
 /**
  * Tests for {@link SdkQuickfixUtils}.
  */
-public class SdkQuickfixUtilsTest extends AndroidGradleTestCase {
+public class SdkQuickfixUtilsTest {
+  @Rule
+  public AndroidProjectRule androidProjectRule = AndroidProjectRule.withSdk();
+  @Rule
+  public EdtRule edtRule = new EdtRule();
+
   RepoManager myRepoManager;
   AndroidSdkHandler mySdkHandler;
   private final Path sdkRoot = InMemoryFileSystems.createInMemoryFileSystemAndFolder("sdk");
 
-  @Override
+  @Before
   public void setUp() throws Exception {
-    super.setUp();
-    initMocks(this.getClass());
-
     RepositoryPackages packages = new RepositoryPackages();
     myRepoManager = spy(new FakeRepoManager(sdkRoot, packages));
     mySdkHandler = new AndroidSdkHandler(sdkRoot, sdkRoot.getRoot().resolve("avd"), myRepoManager);
@@ -60,12 +69,14 @@ public class SdkQuickfixUtilsTest extends AndroidGradleTestCase {
     assertSame(myRepoManager, mySdkHandler.getSdkManager(progress));
   }
 
+  @Test
   public void testCreateDialogForPathsNoOpMessageNull() {
     ModelWizardDialog dialog;
     dialog = SdkQuickfixUtils.createDialogForPaths(null, Collections.emptyList(), null);
     assertNull(dialog);
   }
 
+  @Test
   public void testCreateDialogForPathsNoOpMessage() {
     String errorMessage = "This is an error from testCreateDialogNoOpMessage";
     // createDialogForPaths will cause a RuntimeException instead of creating a dialog when it is headless or in testing mode
@@ -73,7 +84,7 @@ public class SdkQuickfixUtilsTest extends AndroidGradleTestCase {
     boolean causedException = false;
     try {
       ModelWizardDialog dialog = SdkQuickfixUtils.createDialogForPaths(null, Collections.emptyList(), errorMessage);
-      Disposer.register(getTestRootDisposable(), dialog.getDisposable());
+      Disposer.register(androidProjectRule.getTestRootDisposable(), dialog.getDisposable());
     }
     catch (RuntimeException error) {
       causedException = true;
@@ -82,6 +93,8 @@ public class SdkQuickfixUtilsTest extends AndroidGradleTestCase {
     assertThat(causedException).isTrue();
   }
 
+  @Test
+  @RunsInEdt
   public void testCreateDialogNoRepoReloadsWhenUninstallsOnly() {
     LocalPackage localPackage = new FakePackage.FakeLocalPackage("some;sdk;package", sdkRoot.resolve("p"));
 
@@ -89,12 +102,13 @@ public class SdkQuickfixUtilsTest extends AndroidGradleTestCase {
                                                              Collections.emptyList(), ImmutableList.of(localPackage), mySdkHandler,
                                                              null, false);
     assertNotNull(dialog);
-    Disposer.register(getTestRootDisposable(), dialog.getDisposable());
+    Disposer.register(androidProjectRule.getTestRootDisposable(), dialog.getDisposable());
     // We're fine with non-zero cache expiration values, as those inherently optimize the redundant downloads.
     // One such call is currently made from the wizard, which starts SDK installation once built - we will accept that.
     verify(myRepoManager, never()).loadSynchronously(eq(0), any(), any(), any(), any(), any(), any());
   }
 
+  @Test
   public void testCreateDialogNoUncachedRepoReloads() {
     LocalPackage localPackage = new FakePackage.FakeLocalPackage("some;sdk;package", sdkRoot.resolve("p"));
     try {
