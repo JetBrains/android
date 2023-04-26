e549c21aa4283811d29256f98b8d0d22d929bfe5
Sergey Prigogin <sprigogin@google.com>
2023-04-26 14:26:54 -0700

    Support more event types in event logger
    Test: ScreenSharingAgentTest still passes
    Bug: N/A
    Change-Id: I5293606d0c385b2ddb2e886326070fb81bd895e9
    
    GitOrigin-RevId: 04d531b8f72e4ac1e0f25a9652ae67ed3bdd71cb

--- streaming/integration/event-logger/app/src/main/java/com/android/tools/eventlogger/EventLoggingActivity.java
+++ streaming/integration/event-logger/app/src/main/java/com/android/tools/eventlogger/EventLoggingActivity.java
@@ -43,62 +43,80 @@ public class EventLoggingActivity extends Activity {
 
   @Override
   public boolean onKeyDown(int keyCode, KeyEvent event) {
+    Log.d(TAG, event.toString());
     Log.i(TAG, "KEY DOWN: " + keyCode);
     return true;
   }
 
   @Override
   public boolean onKeyLongPress(int keyCode, KeyEvent event) {
+    Log.d(TAG, event.toString());
     Log.i(TAG, "KEY LONG PRESS: " + keyCode);
     return true;
   }
 
   @Override
   public boolean onKeyMultiple(int keyCode, int count, KeyEvent event) {
+    Log.d(TAG, event.toString());
     Log.i(TAG, "KEY MULTIPLE: " + keyCode + " (count=" + count + ")");
     return true;
   }
 
   @Override
+  public boolean onKeyShortcut(int keyCode, KeyEvent event) {
+    Log.d(TAG, event.toString());
+    Log.i(TAG, "KEY SHORTCUT: " + keyCode);
+    return true;
+  }
+
+  @Override
   public boolean onKeyUp(int keyCode, KeyEvent event) {
+    Log.d(TAG, event.toString());
     Log.i(TAG, "KEY UP: " + keyCode);
     return true;
   }
 
   @Override
   public boolean onTouchEvent(MotionEvent event) {
-    StringBuilder pointerStringBuilder = new StringBuilder();
-    for (int i = 0; i < event.getPointerCount(); ++i) {
-      pointerStringBuilder.append(" (").append(event.getX(i)).append(",").append(event.getY(i)).append(")");
-    }
-    Log.i(TAG, "TOUCH EVENT: " + MotionEvent.actionToString(event.getAction()) + pointerStringBuilder);
+    Log.d(TAG, event.toString());
+    Log.i(TAG, "TOUCH EVENT: " + MotionEvent.actionToString(event.getAction()) + pointersToString(event));
     return true;
   }
 
   @Override
   public boolean onGenericMotionEvent(MotionEvent event) {
-    StringBuilder stringBuilder = new StringBuilder("MOTION EVENT:");
+    Log.d(TAG, event.toString());
+    StringBuilder buf = new StringBuilder("MOTION EVENT: ");
+    buf.append(MotionEvent.actionToString(event.getAction()));
     switch (event.getAction()) {
       case MotionEvent.ACTION_SCROLL:
-        processScrollEvent(event, stringBuilder);
+        processScrollEvent(event, buf);
         break;
       default:
-        stringBuilder.append(" UNSUPPORTED");
+        buf.append(pointersToString(event));
+        break;
     }
-    Log.i(TAG, stringBuilder.toString());
+    Log.i(TAG, buf.toString());
     return true;
   }
 
-  private void processScrollEvent(MotionEvent event, StringBuilder stringBuilder) {
-    stringBuilder.append(" ACTION_SCROLL");
+  private void processScrollEvent(MotionEvent event, StringBuilder buf) {
     for (int i = 0; i < event.getPointerCount(); ++i) {
-      stringBuilder
+      buf
         // Coordinates of the pointer at the time of the scrolling.
         .append(" (").append(event.getX(i)).append(",").append(event.getY(i)).append(")")
-        // Vertical scroll component
+        // Vertical scroll component.
         .append(" v=").append(event.getAxisValue(MotionEvent.AXIS_VSCROLL, i))
-        // Horizontal scroll component
+        // Horizontal scroll component.
         .append(" h=").append(event.getAxisValue(MotionEvent.AXIS_HSCROLL, i));
     }
+  }
+
+  private String pointersToString(MotionEvent event) {
+    StringBuilder result = new StringBuilder();
+    for (int i = 0; i < event.getPointerCount(); ++i) {
+      result.append(" (").append(event.getX(i)).append(",").append(event.getY(i)).append(")");
+    }
+    return result.toString();
   }
 }
