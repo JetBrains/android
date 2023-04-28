77da770ea5c8a9fd9464b816ab56921819751f7f
Sasha Blashenkov <Sasha.Blashenkov@jetbrains.com>
2023-04-28 13:51:47 +0200

    Updates to import settings related to Rider wizard
    (cherry picked from commit c207a8c69d23dea0965b3176e3092ceead5c97cf)
    
    GitOrigin-RevId: b172f568cbd1e63193f4f47b3840a6de5ceeb988

--- android/src/com/android/tools/idea/welcome/wizard/CustomizeUIThemeStepPanel.java
+++ android/src/com/android/tools/idea/welcome/wizard/CustomizeUIThemeStepPanel.java
@@ -1,102 +1,73 @@
-// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.android.tools.idea.welcome.wizard;
 
-import com.intellij.CommonBundle;
 import com.intellij.ide.IdeBundle;
 import com.intellij.ide.cloudConfig.CloudConfigProvider;
+import com.intellij.ide.customize.AbstractCustomizeWizardStep;
 import com.intellij.ide.ui.LafManager;
 import com.intellij.ide.ui.laf.IntelliJLaf;
+import com.intellij.ide.ui.laf.LafManagerImpl;
 import com.intellij.ide.ui.laf.darcula.DarculaLaf;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.util.IconLoader;
 import com.intellij.openapi.util.NlsSafe;
 import com.intellij.openapi.util.SystemInfo;
 import com.intellij.ui.AppUIUtil;
-import com.intellij.ui.ClickListener;
-import com.intellij.ui.ColorUtil;
 import com.intellij.util.IconUtil;
 import com.intellij.util.containers.ContainerUtil;
 import com.intellij.util.ui.StartupUiUtil;
 import com.intellij.util.ui.UIUtil;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
-import javax.swing.BorderFactory;
-import javax.swing.ButtonGroup;
-import javax.swing.Icon;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JRadioButton;
-import javax.swing.JToggleButton;
-import javax.swing.SwingConstants;
-import javax.swing.SwingUtilities;
-import javax.swing.UIManager;
-import javax.swing.border.Border;
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Component;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.GridLayout;
-import java.awt.LayoutManager;
-import java.awt.Window;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
-import java.awt.event.MouseEvent;
+
+import javax.swing.*;
+import java.awt.*;
 import java.util.Collection;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
-public class CustomizeUIThemeStepPanel extends JPanel {
+@Deprecated
+public class CustomizeUIThemeStepPanel extends AbstractCustomizeWizardStep {
+  public static class ThemeInfo {
+    public final @NonNls String name;
+    public final @NonNls String previewFileName;
+    public final @NonNls String laf;
+
+    private Icon icon;
+
+    public ThemeInfo(@NonNls String name, @NonNls String previewFileName, @NonNls String laf) {
+      this.name = name;
+      this.previewFileName = SystemInfo.isMac && "IntelliJ".equals(previewFileName) ? "Aqua" : previewFileName;
+      this.laf = laf;
+    }
+
+    private Icon getIcon() {
+      if (icon == null) {
+        String selector;
+        if (SystemInfo.isMac) {
+          selector = "OSX";
+        }
+        else if (SystemInfo.isWindows) {
+          selector = "Windows";
+        }
+        else {
+          selector = "Linux";
+        }
+        icon = IconLoader.getIcon("lafs/" + selector + previewFileName + ".png", CustomizeUIThemeStepPanel.class.getClassLoader());
+      }
+      return icon;
+    }
+
+    public void apply() {
+    }
+  }
+
   protected static final ThemeInfo DARCULA = new ThemeInfo("Darcula", "Darcula", DarculaLaf.class.getName());
   protected static final ThemeInfo INTELLIJ = new ThemeInfo("Light", "IntelliJ", IntelliJLaf.class.getName());
+
   private final boolean myColumnMode;
   private final JLabel myPreviewLabel;
   private final Set<ThemeInfo> myThemes = new LinkedHashSet<>();
-
-  protected static final int SMALL_GAP = 10;
-
-  public static Border createSmallEmptyBorder() {
-    return BorderFactory.createEmptyBorder(SMALL_GAP, SMALL_GAP, SMALL_GAP, SMALL_GAP);
-  }
-
-  public static BorderLayout createSmallBorderLayout() {
-    return new BorderLayout(SMALL_GAP, SMALL_GAP);
-  }
-
-  @NotNull
-  protected static Color getSelectionBackground() {
-    return ColorUtil.mix(UIUtil.getListSelectionBackground(true), UIUtil.getLabelBackground(), StartupUiUtil.isUnderDarcula() ? .5 : .75);
-  }
-
-  protected static JPanel createBigButtonPanel(LayoutManager layout, final JToggleButton anchorButton, final Runnable action) {
-    final JPanel panel = new JPanel(layout) {
-      @Override
-      public Color getBackground() {
-        return anchorButton.isSelected() ? getSelectionBackground() : super.getBackground();
-      }
-    };
-    panel.setOpaque(anchorButton.isSelected());
-    new ClickListener() {
-      @Override
-      public boolean onClick(@NotNull MouseEvent event, int clickCount) {
-        anchorButton.setSelected(true);
-        return true;
-      }
-    }.installOn(panel);
-    anchorButton.addItemListener(new ItemListener() {
-      boolean curState = anchorButton.isSelected();
-      @Override
-      public void itemStateChanged(ItemEvent e) {
-        if (e.getStateChange() == ItemEvent.SELECTED && curState != anchorButton.isSelected()) {
-          action.run();
-        }
-        curState = anchorButton.isSelected();
-        panel.setOpaque(curState);
-        panel.repaint();
-      }
-    });
-    return panel;
-  }
 
   public CustomizeUIThemeStepPanel() {
     setLayout(createSmallBorderLayout());
@@ -162,6 +133,11 @@ public class CustomizeUIThemeStepPanel extends JPanel {
   }
 
   @NotNull
+  protected static ThemeInfo getDefaultLafOnMac() {
+    return INTELLIJ;
+  }
+
+  @NotNull
   private ThemeInfo getDefaultTheme() {
     if (ApplicationManager.getApplication() != null) {
       if (StartupUiUtil.isUnderDarcula()) return DARCULA;
@@ -187,6 +163,21 @@ public class CustomizeUIThemeStepPanel extends JPanel {
     return size;
   }
 
+  @Override
+  public String getTitle() {
+    return "";
+  }
+
+  @Override
+  public String getHTMLHeader() {
+    return "";
+  }
+
+  @Override
+  public String getHTMLFooter() {
+    return "";
+  }
+
   private void applyLaf(ThemeInfo theme, Component component) {
     UIManager.LookAndFeelInfo info = new UIManager.LookAndFeelInfo(theme.name, theme.laf);
     try {
@@ -194,7 +185,6 @@ public class CustomizeUIThemeStepPanel extends JPanel {
       UIManager.setLookAndFeel(info.getClassName());
       AppUIUtil.updateForDarcula(StartupUiUtil.isUnderDarcula());
       String className = info.getClassName();
-      // TODO: WelcomeWizardUtil.setWizardLAF(className);
       Window window = SwingUtilities.getWindowAncestor(component);
       if (window != null) {
         if (SystemInfo.isMac) {
@@ -205,7 +195,12 @@ public class CustomizeUIThemeStepPanel extends JPanel {
       if (ApplicationManager.getApplication() != null) {
         LafManager lafManager = LafManager.getInstance();
         lafManager.setCurrentLookAndFeel(info);
-        lafManager.updateUI();
+        if (lafManager instanceof LafManagerImpl) {
+          //((LafManagerImpl)lafManager).updateWizardLAF(wasUnderDarcula);//Actually updateUI would be called inside EditorColorsManager
+        }
+        else {
+          lafManager.updateUI();
+        }
       }
       if (myColumnMode) {
         myPreviewLabel.setIcon(theme.getIcon());
@@ -214,45 +209,6 @@ public class CustomizeUIThemeStepPanel extends JPanel {
     }
     catch (Exception e) {
       e.printStackTrace();
-    }
-  }
-
-  @NotNull
-  protected static ThemeInfo getDefaultLafOnMac() {
-    return INTELLIJ;
-  }
-
-  public static class ThemeInfo {
-    public final @NonNls String name;
-    public final @NonNls String previewFileName;
-    public final @NonNls String laf;
-
-    private Icon icon;
-
-    public ThemeInfo(@NonNls String name, @NonNls String previewFileName, @NonNls String laf) {
-      this.name = name;
-      this.previewFileName = SystemInfo.isMac && "IntelliJ".equals(previewFileName) ? "Aqua" : previewFileName;
-      this.laf = laf;
-    }
-
-    private Icon getIcon() {
-      if (icon == null) {
-        String selector;
-        if (SystemInfo.isMac) {
-          selector = "OSX";
-        }
-        else if (SystemInfo.isWindows) {
-          selector = "Windows";
-        }
-        else {
-          selector = "Linux";
-        }
-        icon = IconLoader.getIcon("lafs/" + selector + previewFileName + ".png", CustomizeUIThemeStepPanel.class.getClassLoader());
-      }
-      return icon;
-    }
-
-    public void apply() {
     }
   }
 }
