40e6cbfd93176e2f4fc4055c0bcb0ba3e9da2cd2
Andy Magee <andymagee@google.com>
2023-05-01 11:00:50 -0700

    Update GMaven offline artifact index
    I haven't yet made any updates to the index format (b/155235180), so
    this change is just refreshing the current offline list to reflect
    up-to-date index.
    
    Index updated by running:
    ```
    tools/base/bazel/bazel run \
    //tools/adt/idea/android:update_offline_gmaven_index_main ~/studio-main
    ```
    
    Bug: 155235180
    Test: MavenClassRegistryTest
    Change-Id: Id605fb8af2d070af7714c73e29d294c41e9c1703
    
    GitOrigin-RevId: bd652a028fc50ed34bf556846cc022771e730bad

--- android/testSrc/com/android/tools/idea/imports/MavenClassRegistryTest.kt
+++ android/testSrc/com/android/tools/idea/imports/MavenClassRegistryTest.kt
@@ -386,7 +386,7 @@ class MavenClassRegistryTest {
             MavenClassRegistryBase.Library(
               artifact = "androidx.activity:activity",
               packageName = "androidx.activity",
-              version = "1.6.1"
+              version = "1.7.1"
             )
           )
         )
@@ -396,7 +396,7 @@ class MavenClassRegistryTest {
             MavenClassRegistryBase.Library(
               artifact = "androidx.activity:activity",
               packageName = "androidx.activity",
-              version = "1.6.1"
+              version = "1.7.1"
             )
           )
         )
