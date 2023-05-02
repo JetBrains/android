2f2fa3a442a905eed0bccf19b4c377fecd301aeb
Chris Warrington <cmw@google.com>
2023-05-02 12:46:36 +0100

    Rewrite NonTransitiveResourcesLoaderTest
    To use AndroidProjectRule.withAndroidModels
    so it is not brittle to removal of the fallback
    for finding project namespaces in the Gradle module system.
    
    Bug: 275635046
    Bug: 273768010
    Test: included
    Change-Id: I5a0ea4b7b4ce088c5928e9bb7687a722dbb8724d
    
    GitOrigin-RevId: 6958c3141f8c0348e93689d0398fcaa4691ba181

--- android/testSrc/org/jetbrains/android/uipreview/NonTransitiveResourcesLoaderTest.kt
+++ android/testSrc/org/jetbrains/android/uipreview/NonTransitiveResourcesLoaderTest.kt
@@ -15,76 +15,82 @@
  */
 package org.jetbrains.android.uipreview
 
-import com.android.AndroidProjectTypes
 import com.android.ide.common.rendering.api.ResourceNamespace
 import com.android.ide.common.rendering.api.ResourceReference
 import com.android.resources.ResourceType
 import com.android.testutils.MockitoKt.mock
+import com.android.tools.idea.gradle.model.IdeAndroidProjectType
+import com.android.tools.idea.gradle.structure.model.getModuleByGradlePath
 import com.android.tools.idea.layoutlib.LayoutLibrary
 import com.android.tools.idea.rendering.AndroidFacetRenderModelModule
-import com.android.tools.rendering.IRenderLogger
 import com.android.tools.idea.rendering.classloading.loadClassBytes
 import com.android.tools.idea.rendering.classloading.loaders.NameRemapperLoader
-import com.android.tools.idea.testing.ProjectFiles
+import com.android.tools.idea.testing.AndroidModuleDependency
+import com.android.tools.idea.testing.AndroidModuleModelBuilder
+import com.android.tools.idea.testing.AndroidProjectBuilder
+import com.android.tools.idea.testing.AndroidProjectRule
+import com.android.tools.idea.testing.JavaModuleModelBuilder
+import com.android.tools.idea.util.androidFacet
+import com.android.tools.rendering.IRenderLogger
 import com.android.tools.rendering.classloading.loaders.DelegatingClassLoader
 import com.android.tools.rendering.classloading.loaders.StaticLoader
 import com.android.tools.res.ids.ResourceIdManager.Companion.get
-import com.intellij.openapi.application.runUndoTransparentWriteAction
+import com.google.common.truth.Truth.assertThat
 import com.intellij.openapi.roots.ModuleRootModificationUtil
-import com.intellij.testFramework.fixtures.IdeaProjectTestFixture
-import com.intellij.testFramework.fixtures.TestFixtureBuilder
-import org.jetbrains.android.AndroidTestCase
-import org.jetbrains.android.dom.manifest.Manifest
-import org.jetbrains.android.facet.AndroidFacet
 import org.jetbrains.android.uipreview.nontransitive.app.R
+import org.junit.Rule
+import org.junit.Test
+import kotlin.test.fail
 
-class NonTransitiveResourcesLoaderTest : AndroidTestCase() {
-  override fun configureAdditionalModules(projectBuilder: TestFixtureBuilder<IdeaProjectTestFixture>,
-                                          modules: MutableList<MyAdditionalModuleData>) {
-    addModuleWithAndroidFacet(
-      projectBuilder,
-      modules,
-      "lib",
-      AndroidProjectTypes.PROJECT_TYPE_LIBRARY,
-      false
-    )
-  }
+class NonTransitiveResourcesLoaderTest() {
+  @get:Rule
+  val androidProject = AndroidProjectRule.withAndroidModels(
+    { root ->
+      root.resolve("lib/src/main/res/values/strings.xml")
+        .also { it.parentFile.mkdirs() }
+        .writeText(
+          // language=xml
+          """
+          <resources>
+            <string name="lib_name">lib Name</string>
+          </resources>
+          """.trimIndent()
+        )
+      root.resolve("app/src/main/res/values/strings.xml")
+        .also { it.parentFile.mkdirs() }
+        .writeText(
+          // language=xml
+          """
+          <resources>
+            <string name="app_name">app name</string>
+          </resources>
+          """.trimIndent()
+        )
+    },
+    JavaModuleModelBuilder.rootModuleBuilder,
+    AndroidModuleModelBuilder(
+      gradlePath = ":lib",
+      selectedBuildVariant = "debug",
+      projectBuilder = AndroidProjectBuilder(
+        namespace = { "com.example.lib" },
+        projectType = { IdeAndroidProjectType.PROJECT_TYPE_LIBRARY },
+      )
+    ),
+    AndroidModuleModelBuilder(
+      gradlePath = ":app",
+      selectedBuildVariant = "debug",
+      projectBuilder = AndroidProjectBuilder(
+        projectType = { IdeAndroidProjectType.PROJECT_TYPE_APP },
+        namespace = { "com.example.app" },
+        androidModuleDependencyList = {
+          listOf(
+            AndroidModuleDependency(moduleGradlePath = ":lib", variant = "debug"),
+          )
+        }
+      ),
+    ),
+  )
 
-  override fun setUp() {
-    super.setUp()
-
-    val appModule = myFixture.module
-    val libModule = getAdditionalModuleByName("lib")!!
-    ModuleRootModificationUtil.addDependency(appModule, libModule)
-
-    myFixture.addFileToProject(
-      "${getAdditionalModulePath("lib")}/res/values/strings.xml",
-      // language=xml
-      """
-        <resources>
-          <string name="lib_name">lib Name</string>
-        </resources>
-      """.trimIndent()
-    )
-
-    myFixture.addFileToProject(
-      "res/values/strings.xml",
-      // language=xml
-      """
-        <resources>
-          <string name="app_name">app name</string>
-        </resources>
-      """.trimIndent())
-
-    // A settings.gradle file is needed to trick the IDE into thinking this is a project built using gradle, necessary for removing
-    // generated build files from search scope.
-    ProjectFiles.createFileInProjectRoot(project, "settings.gradle")
-
-    runUndoTransparentWriteAction {
-      Manifest.getMainManifest(myFacet)!!.`package`.value = "com.example.app"
-      Manifest.getMainManifest(AndroidFacet.getInstance(libModule)!!)!!.`package`.value = "com.example.lib"
-    }
-  }
 
   /**
    * Regression test for b/206862224.
@@ -95,7 +101,13 @@ class NonTransitiveResourcesLoaderTest : AndroidTestCase() {
    * non-final. This is incorrect as, with non transitive R classes enabled, the library is still treated as part of the project and the
    * ids inlined (because they are final). The difference is just that the library now has its own R class, but the ids are still inlined.
    */
+  @Test
   fun testNonTransitiveRClassesAreInitializedCorrectly() {
+    val app = androidProject.project.getModuleByGradlePath(":app") ?: fail("Could not find app")
+    val lib = androidProject.project.getModuleByGradlePath(":lib") ?: fail("Could not find lib")
+    // TODO(b/280427949): Should this be done by the withAndroidModels factory?
+    ModuleRootModificationUtil.addDependency(app, lib)
+
     val staticLoader = StaticLoader(
       R::class.java.name to loadClassBytes(
         R::class.java),
@@ -114,15 +126,16 @@ class NonTransitiveResourcesLoaderTest : AndroidTestCase() {
 
     // We do not need any of the services offered by LayoutLibrary in this test so just mock it.
     val layoutlib = mock<LayoutLibrary>()
-    val viewLoader = ViewLoader(layoutlib, AndroidFacetRenderModelModule(myFacet), IRenderLogger.NULL_LOGGER, null, delegateClassLoader)
+    val facet = app.androidFacet ?: fail(":app does not have an android facet")
+    val viewLoader = ViewLoader(layoutlib, AndroidFacetRenderModelModule(facet), IRenderLogger.NULL_LOGGER, null, delegateClassLoader)
     viewLoader.loadAndParseRClassSilently()
-    val idManager = get(myModule)
-    assertNotNull(idManager)
+    val idManager = get(app)
+    assertThat(idManager).isNotNull()
 
-    assertEquals(0x7f011111,
-                 idManager.getCompiledId(ResourceReference(ResourceNamespace.RES_AUTO, ResourceType.STRING, "app_name"))!!.toInt())
-    assertEquals(0x7f022222,
-                 idManager.getCompiledId(ResourceReference(ResourceNamespace.RES_AUTO, ResourceType.STRING, "lib_name"))!!.toInt())
+    assertThat(idManager.getCompiledId(ResourceReference(ResourceNamespace.RES_AUTO, ResourceType.STRING, "app_name")))
+      .isEqualTo(0x7f011111)
+    assertThat(idManager.getCompiledId(ResourceReference(ResourceNamespace.RES_AUTO, ResourceType.STRING, "lib_name")))
+      .isEqualTo(0x7f022222)
 
   }
 }
