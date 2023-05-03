2b38b232b1e210d5ce0932eb41cda5471011f5e4
James Rose <rosej@google.com>
2023-05-03 14:24:49 -0700

    Implement local AVD actions
    Show on Disk, Duplicate, Wipe Data, and Delete are implemented by
    delegating to the AvdManager interface (which then delegates to the
    AvdManagerConnection class).
    
    Bug: 276974127
    Test: n/a, just glue code
    Change-Id: I6f0aef4f1164232860c4b199c6e4570d93ec1bea
    
    GitOrigin-RevId: 60fe3420cafb5ca79623e2956dba8e1d66ec9bbc

--- android/src/com/android/tools/idea/avdmanager/LocalEmulatorProvisionerFactory.kt
+++ android/src/com/android/tools/idea/avdmanager/LocalEmulatorProvisionerFactory.kt
@@ -18,14 +18,19 @@ package com.android.tools.idea.avdmanager
 import com.android.sdklib.deviceprovisioner.DeviceProvisionerPlugin
 import com.android.sdklib.deviceprovisioner.LocalEmulatorProvisionerPlugin
 import com.android.sdklib.internal.avd.AvdInfo
+import com.android.sdklib.internal.avd.AvdManager
 import com.android.tools.idea.adblib.AdbLibService
 import com.android.tools.idea.avdmanager.AvdLaunchListener.RequestType
 import com.android.tools.idea.concurrency.AndroidDispatchers
+import com.android.tools.idea.concurrency.AndroidDispatchers.diskIoThread
 import com.android.tools.idea.concurrency.AndroidDispatchers.uiThread
 import com.android.tools.idea.concurrency.AndroidDispatchers.workerThread
 import com.android.tools.idea.deviceprovisioner.DeviceProvisionerFactory
 import com.android.tools.idea.deviceprovisioner.StudioDefaultDeviceActionPresentation
+import com.intellij.ide.actions.RevealFileAction
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.ui.MessageDialogBuilder
+import com.intellij.openapi.ui.Messages
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.withContext
 
@@ -61,6 +66,53 @@ class LocalEmulatorProvisionerFactory : DeviceProvisionerFactory {
 
         override suspend fun stopAvd(avdInfo: AvdInfo) {
           withContext(workerThread) { avdManagerConnection.stopAvd(avdInfo) }
+        }
+
+        override suspend fun showOnDisk(avdInfo: AvdInfo) {
+          RevealFileAction.openDirectory(avdInfo.dataFolderPath)
+        }
+
+        override suspend fun duplicateAvd(avdInfo: AvdInfo) {
+          withContext(uiThread) {
+            AvdWizardUtils.createAvdWizardForDuplication(null, project, AvdOptionsModel(avdInfo))
+              .showAndGet()
+          }
+        }
+
+        override suspend fun wipeData(avdInfo: AvdInfo) {
+          withContext(diskIoThread) {
+            if (!avdManagerConnection.wipeUserData(avdInfo)) {
+              withContext(uiThread) {
+                Messages.showErrorDialog(
+                  project,
+                  "Failed to wipe data. Please check that the emulator and its files are not in use and try again.",
+                  "Wipe Data Error"
+                )
+              }
+            }
+          }
+        }
+
+        override suspend fun deleteAvd(avdInfo: AvdInfo) {
+          withContext(diskIoThread) {
+            if (!avdManagerConnection.deleteAvd(avdInfo)) {
+              withContext(uiThread) {
+                if (
+                  MessageDialogBuilder.okCancel(
+                      "Could Not Delete All AVD Files",
+                      "There may be additional files remaining in the AVD directory. Open the directory, " +
+                        "manually delete the files, and refresh AVD list."
+                    )
+                    .yesText("Open Directory")
+                    .noText("OK")
+                    .icon(Messages.getInformationIcon())
+                    .ask(project)
+                ) {
+                  showOnDisk(avdInfo)
+                }
+              }
+            }
+          }
         }
       }
 
