826f1cec356e739cdbb82086cd6b603a1ba91356
Amaury Medeiros <amaurym@google.com>
2023-05-03 15:09:15 +0100

    Fix formatting of PreviewPowerSaveManager
    Bug: N/A
    Test: N/A
    Change-Id: I508a5c681c06b594e9e819d29f61508917b227df
    
    GitOrigin-RevId: c2ce9c95ac0b5b73b259335a92d7a907e968d5cd

--- android/src/com/android/tools/idea/editors/powersave/PreviewPowerSaveManager.kt
+++ android/src/com/android/tools/idea/editors/powersave/PreviewPowerSaveManager.kt
@@ -24,5 +24,5 @@ object PreviewPowerSaveManager {
    * Same as [PowerSaveMode] but obeys to the [DESIGN_TOOLS_POWER_SAVE_MODE_SUPPORT] to allow disabling the functionality.
    */
   val isInPowerSaveMode: Boolean
-    get() = DESIGN_TOOLS_POWER_SAVE_MODE_SUPPORT.get() && (PowerSaveMode.isEnabled() ||  EssentialHighlightingMode.isEnabled ())
+    get() = DESIGN_TOOLS_POWER_SAVE_MODE_SUPPORT.get() && (PowerSaveMode.isEnabled() || EssentialHighlightingMode.isEnabled())
 }
