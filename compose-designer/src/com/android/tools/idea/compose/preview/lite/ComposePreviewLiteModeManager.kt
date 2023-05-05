239691a83e6880f6f538ba936e877ead5df6e217
Amaury Medeiros <amaurym@google.com>
2023-05-05 14:47:50 +0100

    Add manager to control Compose Preview Lite Mode state
    Fixes: 280580937
    Test: ComposePreviewLiteModeManagerTest
    Change-Id: If972d6274e74f841c88467d0cdf250db0c719048
    
    GitOrigin-RevId: ea396a532edc68d2ad57ce5df61f8fc2a5e9b85c

---
+++ compose-designer/src/com/android/tools/idea/compose/preview/lite/ComposePreviewLiteModeManager.kt
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.compose.preview.lite
+
+import com.android.tools.idea.flags.StudioFlags
+import org.jetbrains.android.uipreview.AndroidEditorSettings
+
+/**
+ * Service to handle and query the state of Compose Preview Lite Mode. The state can be set via
+ * settings.
+ */
+object ComposePreviewLiteModeManager {
+
+  val isLiteModeEnabled: Boolean
+    get() =
+      StudioFlags.COMPOSE_PREVIEW_LITE_MODE.get() &&
+        AndroidEditorSettings.getInstance().globalState.isComposePreviewLiteModeEnabled
+}
