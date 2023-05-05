23c13c0bba182e4756312274b2bc41f8e653e8b8
Andy Magee <andymagee@google.com>
2023-05-05 13:20:02 -0700

    Split lang test targets
    This will hopefully address the lang target timing out.
    
    Fixes: 279769850
    Test: Test fix
    Change-Id: Id78ffc45d917697ab6d092d2c9c85e11b02344a7
    
    GitOrigin-RevId: e05c241dd96b52b0b467970a09a7bd30e0423239

--- android-lang/testSrc/com/android/tools/idea/lang/androidSql/refactoring/UsagesTest.kt
+++ android-lang/testSrc/com/android/tools/idea/lang/androidSql/refactoring/UsagesTest.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.android.tools.idea.lang.com.android.tools.idea.lang.androidSql.refactoring
+package com.android.tools.idea.lang.androidSql.refactoring
 
 import com.android.testutils.TestUtils.resolveWorkspacePath
 import com.android.tools.idea.lang.androidSql.createStubRoomClasses
