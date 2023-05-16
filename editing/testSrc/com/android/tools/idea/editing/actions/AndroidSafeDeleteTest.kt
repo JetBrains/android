097c71efefca57baeb131fcc92ff292075bbb811
Andy Magee <andymagee@google.com>
2023-05-16 10:08:50 -0700

    Create intellij.android.editing.tests module
    Splitting out some tests from core. For now, just moving a single
    stabilized test to ensure everything's hooked up correctly; will follow
    up with more changes to port more tests over.
    
    Fixes: 281709998
    Test: test change
    Change-Id: Ia30fe07d6c2763aeb3642946bfabc43185285691
    
    GitOrigin-RevId: b355007b59b5f7a532b0f58fcc795277bbc098e2

--- editing/testSrc/com/android/tools/idea/editing/actions/AndroidSafeDeleteTest.kt
+++ editing/testSrc/com/android/tools/idea/editing/actions/AndroidSafeDeleteTest.kt
@@ -13,7 +13,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-package org.jetbrains.android.actions
+package com.android.tools.idea.editing.actions
 
 import com.android.tools.idea.testing.AndroidProjectRule
 import com.google.common.truth.Truth.assertThat
