8b2860a7facf1ed6dde6dadc02b020bbca778305
Andy Magee <andymagee@google.com>
2023-05-16 10:45:41 -0700

    Convert AndroidFindStyleApplicationsTest
    Converting to use AndroidProjectRule instead of AndroidTestCase, and
    changing to Kotlin at the same time.
    
    b/281709998 is technically for a different test, but this change is
    related since this test starts failing as soon as the other test is
    moved out of the suite. Too bad the tests aren't already hermetic...
    
    Bug: 281709998
    Test: AndroidFindStyleApplicationsTest
    Change-Id: I19a017f2de95c5e95ee8a530911776c4c8365bc3
    
    GitOrigin-RevId: 76849191c5f381c734afce2f69e8d3b502856511

--- editing/testSrc/org/jetbrains/android/refactoring/AndroidFindStyleApplicationsTest.kt
+++ editing/testSrc/org/jetbrains/android/refactoring/AndroidFindStyleApplicationsTest.kt
@@ -1,97 +1,133 @@
-package org.jetbrains.android.refactoring;
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jetbrains.android.refactoring
 
-import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.psi.PsiFile;
-import com.intellij.psi.util.PsiTreeUtil;
-import com.intellij.psi.xml.XmlFile;
-import com.intellij.psi.xml.XmlTag;
-import com.intellij.usageView.UsageInfo;
-import com.intellij.util.xml.DomManager;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import org.jetbrains.android.AndroidTestCase;
-import org.jetbrains.android.dom.layout.LayoutViewElementDomFileDescription;
+import com.android.testutils.TestUtils
+import com.android.tools.idea.testing.AndroidProjectRule
+import com.google.common.truth.Truth.assertThat
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.application.runReadAction
+import com.intellij.psi.util.PsiTreeUtil
+import com.intellij.psi.xml.XmlFile
+import com.intellij.psi.xml.XmlTag
+import com.intellij.usageView.UsageInfo
+import com.intellij.util.xml.DomManager
+import org.jetbrains.android.dom.layout.LayoutViewElementDomFileDescription
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+import kotlin.test.assertFailsWith
 
-public class AndroidFindStyleApplicationsTest extends AndroidTestCase {
-  private static final String BASE_PATH = "refactoring/findPossibleUsages/";
+private const val BASE_PATH = "refactoring/findStyleApplication/"
 
-  public void test1() throws Exception {
-    doTest();
+@RunWith(JUnit4::class)
+class AndroidFindStyleApplicationsTest {
+  @get:Rule
+  var androidProjectRule: AndroidProjectRule = AndroidProjectRule.onDisk()
+
+  private val myFixture by lazy { androidProjectRule.fixture }
+
+  @Before
+  fun setUp() {
+    myFixture.testDataPath = TestUtils.resolveWorkspacePath("tools/adt/idea/editing/testData").toString()
   }
 
-  public void testGranular1() throws Exception {
-    myFixture.copyFileToProject(BASE_PATH + "1_layout.xml", "res/layout/layout.xml");
-    final VirtualFile f = myFixture.copyFileToProject(BASE_PATH + "1.xml", "res/values/styles.xml");
-    myFixture.configureFromExistingVirtualFile(f);
-    XmlTag tag = PsiTreeUtil.getParentOfType(myFixture.getFile().findElementAt(myFixture.getCaretOffset()), XmlTag.class);
-    AndroidFindStyleApplicationsAction.MyStyleData styleData = AndroidFindStyleApplicationsAction.getStyleData(tag);
-    assertNotNull(styleData);
-    AndroidFindStyleApplicationsProcessor processor =
-      AndroidFindStyleApplicationsAction.createFindStyleApplicationsProcessor(tag, styleData, null);
-    processor.configureScope(AndroidFindStyleApplicationsProcessor.MyScope.PROJECT, null);
-    Collection<PsiFile> files = processor.collectFilesToProcess();
-    assertEquals(1, files.size());
-    XmlFile layoutFile = (XmlFile)files.iterator().next();
-    assertInstanceOf(DomManager.getDomManager(myFixture.getProject()).getDomFileDescription(layoutFile),
-                     LayoutViewElementDomFileDescription.class);
-    final List<UsageInfo> usages = new ArrayList<>();
-    processor.collectPossibleStyleApplications(layoutFile, usages);
-    assertEquals(2, usages.size());
+  @Test
+  fun test1() {
+    doTest("1")
   }
 
-  public void test2() throws Exception {
-    doTest();
+  @Test
+  fun testGranular1() {
+    myFixture.copyFileToProject(BASE_PATH + "1_layout.xml", "res/layout/layout.xml")
+    val styleVirtualFile = myFixture.copyFileToProject(BASE_PATH + "1.xml", "res/values/styles.xml")
+    myFixture.configureFromExistingVirtualFile(styleVirtualFile)
+
+    val tag = runReadAction { PsiTreeUtil.getParentOfType(myFixture.file.findElementAt(myFixture.caretOffset), XmlTag::class.java) }
+    assertThat(tag).isNotNull()
+
+    val styleData = runReadAction { AndroidFindStyleApplicationsAction.getStyleData(tag!!) }
+    assertThat(styleData).isNotNull()
+
+    val processor = runReadAction { AndroidFindStyleApplicationsAction.createFindStyleApplicationsProcessor(tag, styleData, null) }
+    processor.configureScope(AndroidFindStyleApplicationsProcessor.MyScope.PROJECT, null)
+
+    val files = runReadAction { processor.collectFilesToProcess() }
+    assertThat(files).hasSize(1)
+
+    val layoutFile = files.single() as XmlFile
+    val domFileDescription = runReadAction { DomManager.getDomManager(myFixture.project).getDomFileDescription(layoutFile) }
+    assertThat(domFileDescription)
+      .isInstanceOf(LayoutViewElementDomFileDescription::class.java)
+
+    val usages: List<UsageInfo> = ArrayList()
+    runReadAction { processor.collectPossibleStyleApplications(layoutFile, usages) }
+    assertThat(usages).hasSize(2)
   }
 
-  public void test3() throws Exception {
-    doTest();
+  @Test
+  fun test2() {
+    doTest("2")
   }
 
-  public void test4() throws Exception {
-    doTest();
+  @Test
+  fun test3() {
+    doTest("3")
   }
 
-  public void test5() throws Exception {
-    doTest();
+  @Test
+  fun test4() {
+    doTest("4")
   }
 
-  public void test6() throws Exception {
-    final String testName = getTestName(true);
-    myFixture.copyFileToProject(BASE_PATH + testName + "_layout.xml", "res/layout/layout1.xml");
-    myFixture.copyFileToProject(BASE_PATH + testName + "_layout.xml", "res/layout/layout2.xml");
-
-    doTest1();
-
-    myFixture.checkResultByFile("res/layout/layout1.xml", BASE_PATH + testName + "_layout_after.xml", true);
-    myFixture.checkResultByFile("res/layout/layout2.xml", BASE_PATH + testName + "_layout_after.xml", true);
+  @Test
+  fun test5() {
+    doTest("5")
   }
 
-  public void test7() throws Exception {
-    try {
-      doTest();
-      fail();
-    }
-    catch (RuntimeException e) {
-      assertEquals("IDEA has not found any possible applications of style 'style1'", e.getMessage());
-    }
+  @Test
+  fun test6() {
+    myFixture.copyFileToProject(BASE_PATH + "6_layout.xml", "res/layout/layout1.xml")
+    myFixture.copyFileToProject(BASE_PATH + "6_layout.xml", "res/layout/layout2.xml")
+    doTest1("6")
+    myFixture.checkResultByFile("res/layout/layout1.xml", BASE_PATH + "6_layout_after.xml", true)
+    myFixture.checkResultByFile("res/layout/layout2.xml", BASE_PATH + "6_layout_after.xml", true)
   }
 
-  private void doTest() {
-    final String testName = getTestName(true);
-    myFixture.copyFileToProject(BASE_PATH + testName + "_layout.xml", "res/layout/layout.xml");
-
-    doTest1();
-
-    myFixture.checkResultByFile("res/layout/layout.xml", BASE_PATH + testName + "_layout_after.xml", true);
+  @Test
+  fun test7() {
+    assertFailsWith<RuntimeException>("IDEA has not found any possible applications of style 'style1'") { doTest("7") }
   }
 
-  private void doTest1() {
-    final String testName = getTestName(true);
-    final VirtualFile f = myFixture.copyFileToProject(BASE_PATH + testName + ".xml", "res/values/styles.xml");
-    myFixture.configureFromExistingVirtualFile(f);
-    myFixture.testAction(new AndroidFindStyleApplicationsAction(new AndroidFindStyleApplicationsAction.MyTestConfig(
-      AndroidFindStyleApplicationsProcessor.MyScope.PROJECT)));
-    myFixture.checkResultByFile(BASE_PATH + testName + ".xml");
+  private fun doTest(testName: String) {
+    myFixture.copyFileToProject("$BASE_PATH${testName}_layout.xml", "res/layout/layout.xml")
+    doTest1(testName)
+    myFixture.checkResultByFile("res/layout/layout.xml", "$BASE_PATH${testName}_layout_after.xml", true)
+  }
+
+  private fun doTest1(testName: String) {
+    val f = myFixture.copyFileToProject("$BASE_PATH$testName.xml", "res/values/styles.xml")
+    myFixture.configureFromExistingVirtualFile(f)
+
+    val myTestConfig = AndroidFindStyleApplicationsAction.MyTestConfig(AndroidFindStyleApplicationsProcessor.MyScope.PROJECT)
+    val action = AndroidFindStyleApplicationsAction(myTestConfig)
+    ApplicationManager.getApplication().invokeAndWait { myFixture.testAction (action) }
+
+    myFixture.checkResultByFile("$BASE_PATH$testName.xml")
   }
 }
