b5b7319be76f91b3d6c38a6a77eac8162050e698
Alon Albert <aalbert@google.com>
2023-06-08 15:23:35 -0700

    Add Compose Method Breakpoint Extensions
    The issue is that `@Composable` functions have a different signature in the JVM as they in the PSI because the Compose compiler plugin adds a few parameters to the function.
    
    This is solved by allowing this variance when comparing JVM signature to PSI.
    
    Bug: 286154452
    Test: Manually
    Change-Id: Ifd4d067e2b64120732622e122b67587db37fa8b0
    
    GitOrigin-RevId: ed568a9b92d6ccff39b4b3bc18135359d949f5f7

---
+++ compose-ide-plugin/src/com/android/tools/compose/debug/ComposeFunctionBreakpoint.kt
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.compose.debug
+
+import com.intellij.debugger.engine.DebugProcessImpl
+import com.intellij.openapi.project.Project
+import com.intellij.xdebugger.breakpoints.XBreakpoint
+import com.sun.jdi.Method
+import org.jetbrains.kotlin.idea.debugger.core.breakpoints.KotlinFunctionBreakpoint
+import org.jetbrains.kotlin.idea.debugger.core.breakpoints.SourcePositionRefiner
+
+/**
+ * A [com.intellij.debugger.ui.breakpoints.MethodBreakpoint] that supports `@Composable` function breakpoints
+ */
+internal class ComposeFunctionBreakpoint(
+  project: Project,
+  breakpoint: XBreakpoint<*>
+) : KotlinFunctionBreakpoint(project, breakpoint), SourcePositionRefiner {
+  override fun isMethodMatch(method: Method, debugProcess: DebugProcessImpl) =
+    method.name() == methodName && method.signature().withoutComposeArgs() == mySignature?.getName(debugProcess)
+}
+
+private fun String.withoutComposeArgs(): String {
+  val start = indexOf("Landroidx/compose/runtime/Composer;")
+  val end = lastIndexOf(')')
+  if (start < 0 || end < 0) {
+    return this
+  }
+  return substring(0, start) + substring(end)
+}
---
+++ compose-ide-plugin/src/com/android/tools/compose/debug/ComposeFunctionBreakpointType.kt
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.compose.debug
+
+import com.android.tools.compose.ComposeBundle
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.xdebugger.breakpoints.XBreakpoint
+import org.jetbrains.java.debugger.breakpoints.properties.JavaMethodBreakpointProperties
+import org.jetbrains.kotlin.idea.debugger.core.breakpoints.ApplicabilityResult
+import org.jetbrains.kotlin.idea.debugger.core.breakpoints.KotlinFunctionBreakpoint
+import org.jetbrains.kotlin.idea.debugger.core.breakpoints.KotlinFunctionBreakpointType
+import org.jetbrains.kotlin.idea.debugger.core.breakpoints.isBreakpointApplicable
+import org.jetbrains.kotlin.idea.debugger.core.breakpoints.isComposable
+import org.jetbrains.kotlin.idea.debugger.core.breakpoints.isInlineOnly
+import org.jetbrains.kotlin.psi.KtFunction
+import org.jetbrains.kotlin.psi.KtPsiUtil
+
+/**
+ * A [com.intellij.debugger.ui.breakpoints.JavaMethodBreakpointType] for `@Composable` functions
+ */
+internal class ComposeFunctionBreakpointType :
+  KotlinFunctionBreakpointType("compose-function", ComposeBundle.message("compose.breakpoint.description")) {
+
+  override fun createJavaBreakpoint(project: Project, breakpoint: XBreakpoint<JavaMethodBreakpointProperties>): KotlinFunctionBreakpoint =
+    ComposeFunctionBreakpoint(project, breakpoint)
+
+  override fun isFunctionBreakpointApplicable(file: VirtualFile, line: Int, project: Project): Boolean =
+    isBreakpointApplicable(file, line, project) { element ->
+      when (element) {
+        is KtFunction ->
+          ApplicabilityResult.maybe(
+            !KtPsiUtil.isLocal(element) &&
+            !element.isInlineOnly() &&
+            element.isComposable())
+        else -> ApplicabilityResult.UNKNOWN
+      }
+    }
+}
