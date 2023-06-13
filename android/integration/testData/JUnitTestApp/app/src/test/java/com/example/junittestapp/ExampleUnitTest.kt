16bd85c195acb427a01d3ccfbe1c2c78c270f81e
Yuchen Yao <yaoyuchen@google.com>
2023-06-12 17:08:55 -0700

    Added E2E test for Debugging JUnit tests
    This test is similar to debugging a regular app but the run target
    is a JUnit test.
    
    Currently test is disabled due to its flakiness. See b/286943866
    
    Bug: 286945666
    Test: N/A
    Change-Id: I980001e16fdad73cd8420519edfc4ca885022af3
    
    GitOrigin-RevId: 8f85e45e003aee924132a6f212d013da7d700a4b

---
+++ android/integration/testData/JUnitTestApp/app/src/test/java/com/example/junittestapp/ExampleUnitTest.kt
@@ -0,0 +1,11 @@
+package com.example.junittestapp
+
+import org.junit.Test
+import org.junit.Assert.*
+
+class ExampleUnitTest {
+  @Test
+  fun addition_isCorrect() {
+    assertEquals(4, 2 + 2)
+  }
+}
