228b0a8e353a6a0996430daa5d3951e7077daf99
Jens Ole Lauridsen <jlauridsen@google.com>
2023-06-14 12:07:39 -0700

    Update color used for SelectedComponent in attributes
    Fixes: 286930172
    Test: N/A
    Change-Id: I01d0330f653806d580f7029978685cf5980ecf18
    
    GitOrigin-RevId: f22458f9fde851ee96125790c7c85b22a4a5c50a

--- layout-ui/src/main/java/com/android/tools/property/panel/api/SelectedComponentPanel.kt
+++ layout-ui/src/main/java/com/android/tools/property/panel/api/SelectedComponentPanel.kt
@@ -18,8 +18,7 @@ package com.android.tools.property.panel.api
 import com.android.tools.adtui.common.secondaryPanelBackground
 import com.android.tools.property.panel.impl.ui.ExpandableLabel
 import com.android.tools.property.panel.impl.ui.PropertyTextField
-import com.intellij.ui.Gray
-import com.intellij.ui.JBColor
+import com.intellij.util.ui.UIUtil
 import java.awt.BorderLayout
 import javax.swing.JPanel
 
@@ -36,7 +35,7 @@ class SelectedComponentPanel(private val model: SelectedComponentModel) : JPanel
   init {
     background = secondaryPanelBackground
     PropertyTextField.addBorderAtTextFieldBorderSize(this)
-    left.foreground = JBColor(Gray._192, Gray._128)
+    left.foreground = UIUtil.getInactiveTextColor()
     add(left, BorderLayout.WEST)
     add(right, BorderLayout.EAST)
     updateAfterModelChange()
