9a50904d5cf34344620109ed6ee88b3b24fbd12b
Yasser Dbeis <ydbeis@google.com>
2023-06-14 14:18:22 -0700

    Add start system trace and stop cpu capture actions
    ...to be used for E2E test for starting a profiler session,
    starting a system trace, ending the current CPU capture, and
    then stopping the profiling session. Their respective ids:
    
    "Android.StartSystemTrace"
    "Android.StopCpuCapture"
    
    Bug: 270582414
    Test: this change is for automatic testing
    Change-Id: Ie1491ac4404844a3cbef9f6c71191d1b959b81de
    
    GitOrigin-RevId: 915792501bbc3f1e4c13845a47492905e93792e2

---
+++ profilers-android/src/com/android/tools/idea/profilers/StopCpuCaptureAction.kt
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.profilers
+
+import android.annotation.SuppressLint
+import com.android.tools.idea.flags.StudioFlags
+import com.android.tools.profilers.cpu.CpuProfilerStage
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.project.DumbAwareAction
+
+class StopCpuCaptureAction : DumbAwareAction(
+  "Stop CPU Capture",
+  "Stop a CPU capture in the current profiling session",
+  null
+) {
+  override fun update(e: AnActionEvent) {
+    val project = e.project
+    val profilers = project?.let { AndroidProfilerToolWindowFactory.getProfilerToolWindow(it)?.profilers }
+    e.presentation.isEnabled =
+      StudioFlags.PROFILER_TESTING_MODE.get() == true &&
+      project != null &&
+      profilers != null &&
+      profilers.sessionsManager.isSessionAlive == true &&
+      profilers.stage is CpuProfilerStage &&
+      (profilers.stage as CpuProfilerStage).recordingModel.canStop()
+  }
+
+  @SuppressLint("VisibleForTests")
+  override fun actionPerformed(e: AnActionEvent) {
+    val project = e.project!!
+    val profilers = AndroidProfilerToolWindowFactory.getProfilerToolWindow(project)!!.profilers
+    (profilers.stage as CpuProfilerStage).recordingModel.stop()
+  }
+}
