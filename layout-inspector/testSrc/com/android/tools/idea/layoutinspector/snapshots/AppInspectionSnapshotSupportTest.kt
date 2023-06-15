5d1f9817ee1574bf211def88c3ed6badd485429c
Jens Ole Lauridsen <jlauridsen@google.com>
2023-06-15 13:30:12 -0700

    Add test of resources present after save/load
    Bug: 287265444
    Test: AppInspectionSnapshotSupportTest
    Change-Id: Ic6669023cc7b77a2467d0f88373d75b41bc692ae
    
    GitOrigin-RevId: de5c2c0faa292f8bdd8d235ebe4d57b101d67b29

--- layout-inspector/testSrc/com/android/tools/idea/layoutinspector/snapshots/AppInspectionSnapshotSupportTest.kt
+++ layout-inspector/testSrc/com/android/tools/idea/layoutinspector/snapshots/AppInspectionSnapshotSupportTest.kt
@@ -37,7 +37,22 @@ import com.android.tools.idea.layoutinspector.pipeline.appinspection.dsl.ViewNod
 import com.android.tools.idea.layoutinspector.pipeline.appinspection.dsl.ViewResource
 import com.android.tools.idea.layoutinspector.pipeline.appinspection.dsl.ViewString
 import com.android.tools.idea.layoutinspector.pipeline.appinspection.inspectors.sendEvent
+import com.android.tools.idea.layoutinspector.pipeline.appinspection.view.GRAMMATICAL_GENDER_FEMININE
 import com.android.tools.idea.layoutinspector.properties.PropertyType.INT32
+import com.android.tools.idea.layoutinspector.resource.COLOR_MODE_HDR_YES
+import com.android.tools.idea.layoutinspector.resource.COLOR_MODE_WIDE_COLOR_GAMUT_YES
+import com.android.tools.idea.layoutinspector.resource.KEYBOARDHIDDEN_NO
+import com.android.tools.idea.layoutinspector.resource.KEYBOARD_QWERTY
+import com.android.tools.idea.layoutinspector.resource.NAVIGATIONHIDDEN_NO
+import com.android.tools.idea.layoutinspector.resource.NAVIGATION_WHEEL
+import com.android.tools.idea.layoutinspector.resource.ORIENTATION_PORTRAIT
+import com.android.tools.idea.layoutinspector.resource.SCREENLAYOUT_LAYOUTDIR_RTL
+import com.android.tools.idea.layoutinspector.resource.SCREENLAYOUT_LONG_YES
+import com.android.tools.idea.layoutinspector.resource.SCREENLAYOUT_ROUND_YES
+import com.android.tools.idea.layoutinspector.resource.SCREENLAYOUT_SIZE_SMALL
+import com.android.tools.idea.layoutinspector.resource.TOUCHSCREEN_STYLUS
+import com.android.tools.idea.layoutinspector.resource.UI_MODE_NIGHT_NO
+import com.android.tools.idea.layoutinspector.resource.UI_MODE_TYPE_NORMAL
 import com.android.tools.idea.layoutinspector.view
 import com.android.tools.idea.layoutinspector.view.inspection.LayoutInspectorViewProtocol
 import com.android.tools.idea.testing.AndroidProjectRule
@@ -113,6 +128,10 @@ class AppInspectionSnapshotSupportTest {
       inspectorRule.inspectorClient.stats
     )
     checkSnapshot(newModel, snapshotLoader)
+    assertThat(newModel.resourceLookup.dpi).isEqualTo(640)
+    assertThat(newModel.resourceLookup.fontScale).isEqualTo(1.2f)
+    assertThat(newModel.resourceLookup.screenDimension).isEqualTo(Dimension(800, 1600))
+    assertThat(newModel.resourceLookup.hasResolver).isTrue()
   }
 
   @Test
@@ -182,6 +201,10 @@ class AppInspectionSnapshotSupportTest {
       inspectorRule.inspectorClient.stats
     )
     checkSnapshot(newModel, snapshotLoader)
+    assertThat(newModel.resourceLookup.dpi).isEqualTo(640)
+    assertThat(newModel.resourceLookup.fontScale).isEqualTo(1.2f)
+    assertThat(newModel.resourceLookup.screenDimension).isEqualTo(Dimension(800, 1600))
+    assertThat(newModel.resourceLookup.hasResolver).isTrue()
   }
 
   @Test
@@ -341,7 +364,7 @@ class AppInspectionSnapshotSupportTest {
       ViewString(9, "title")
       ViewString(10, "android")
       ViewString(11, "AppTheme")
-      ViewString(12, "http://schemas.android.com/apk/res/myapp")
+      ViewString(12, "com.example")
       ViewString(13, "style")
 
       Root {
@@ -375,7 +398,35 @@ class AppInspectionSnapshotSupportTest {
         }
       }
 
-      appContextBuilder.apply { theme = ViewResource(13, 12, 11) }
+      appContextBuilder.apply {
+        configurationBuilder.apply {
+          fontScale = 1.2f
+          countryCode = 310
+          networkCode = 410
+          screenLayout =
+            SCREENLAYOUT_SIZE_SMALL or
+              SCREENLAYOUT_LONG_YES or
+              SCREENLAYOUT_LAYOUTDIR_RTL or
+              SCREENLAYOUT_ROUND_YES
+          colorMode = COLOR_MODE_WIDE_COLOR_GAMUT_YES or COLOR_MODE_HDR_YES
+          touchScreen = TOUCHSCREEN_STYLUS
+          keyboard = KEYBOARD_QWERTY
+          keyboardHidden = KEYBOARDHIDDEN_NO
+          hardKeyboardHidden = KEYBOARDHIDDEN_NO
+          navigation = NAVIGATION_WHEEL
+          navigationHidden = NAVIGATIONHIDDEN_NO
+          uiMode = UI_MODE_TYPE_NORMAL or UI_MODE_NIGHT_NO
+          smallestScreenWidthDp = 200
+          density = 640
+          orientation = ORIENTATION_PORTRAIT
+          screenWidthDp = 480
+          screenHeightDp = 800
+          grammaticalGender = GRAMMATICAL_GENDER_FEMININE
+        }
+        screenWidth = 800
+        screenHeight = 1600
+        theme = ViewResource(13, 12, 11)
+      }
     }
   }
 
