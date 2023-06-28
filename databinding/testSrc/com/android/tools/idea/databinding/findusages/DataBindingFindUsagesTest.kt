61372fa756d5354842ba65328f4fb8aa63b3bd7e
Andy Magee <andymagee@google.com>
2023-06-28 15:40:22 -0700

    Filter databinding results for duplicate IDs
    Fixes: 288834116
    Test: DataBindingFindUsagesTest
    Change-Id: Ifc2c5053e4f4a976ab1250009b11dad11d0352bc
    
    GitOrigin-RevId: 5588e956aec557fa24f7d191af5f54be0c454dab

--- databinding/testSrc/com/android/tools/idea/databinding/findusages/DataBindingFindUsagesTest.kt
+++ databinding/testSrc/com/android/tools/idea/databinding/findusages/DataBindingFindUsagesTest.kt
@@ -20,6 +20,7 @@ import com.android.tools.idea.databinding.TestDataPaths
 import com.android.tools.idea.databinding.module.LayoutBindingModuleCache
 import com.android.tools.idea.testing.AndroidProjectRule
 import com.android.tools.idea.testing.caret
+import com.android.tools.idea.testing.moveCaret
 import com.android.tools.idea.util.androidFacet
 import com.google.common.truth.Truth.assertThat
 import com.intellij.openapi.application.ApplicationManager
@@ -213,6 +214,113 @@ class DataBindingFindUsagesTest() {
              13System.out.println(binding.button.getId());
 
     """.trimIndent())
+  }
+
+  @Test
+  fun duplicateIdInTwoLayoutFiles() {
+    val layoutFile1 = fixture.addFileToProject(
+      "res/layout/activity_main.xml",
+      // language=XML
+      """
+      <?xml version="1.0" encoding="utf-8"?>
+      <layout xmlns:android="http://schemas.android.com/apk/res/android">
+        <LinearLayout
+            android:layout_width="fill_parent"
+            android:layout_height="fill_parent">
+            <Button
+                android:id="@+id/button"
+                android:layout_width="fill_parent"
+                android:layout_height="fill_parent" />
+        </LinearLayout>
+      </layout>
+    """.trimIndent())
+
+    val layoutFile2 = fixture.addFileToProject(
+      "res/layout/activity_main2.xml",
+      // language=XML
+      """
+      <?xml version="1.0" encoding="utf-8"?>
+      <layout xmlns:android="http://schemas.android.com/apk/res/android">
+        <LinearLayout
+            android:layout_width="fill_parent"
+            android:layout_height="fill_parent">
+            <Button
+                android:id="@+id/button"
+                android:layout_width="fill_parent"
+                android:layout_height="fill_parent" />
+        </LinearLayout>
+      </layout>
+    """.trimIndent())
+
+    fixture.addFileToProject(
+      "src/java/test/db/MainActivity.java",
+      // language=JAVA
+      """
+      package test.db;
+
+      import android.app.Activity;
+      import android.os.Bundle;
+
+      import test.db.databinding.ActivityMainBinding;
+
+      public class MainActivity extends Activity {
+          @Override
+          protected void onCreate(Bundle savedInstanceState) {
+              super.onCreate(savedInstanceState);
+              ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+              System.out.println(binding.button.getId());
+              setContentView(binding.getRoot());
+          }
+      }
+    """.trimIndent()
+    )
+
+    // Find usages for layout 1 should have three; both xml files (because they both reference the same id), and the binding class usage.
+    fixture.configureFromExistingVirtualFile(layoutFile1.virtualFile)
+    ApplicationManager.getApplication().invokeAndWait { fixture.moveCaret("@+id/but|ton") }
+    val presentationLayout1 = getUsagePresentationAtCursor()
+
+    assertThat(presentationLayout1).contains("<root> (3)")
+    assertThat(presentationLayout1).contains(
+      """
+      |  Resource declaration in Android resources XML (2)
+      |   app (2)
+      |    res/layout (2)
+      |     activity_main.xml (1)
+      |      7android:id="@+id/button"
+      |     activity_main2.xml (1)
+      |      7android:id="@+id/button"
+      """.trimMargin()
+    )
+    assertThat(presentationLayout1).contains(
+      """
+      |  Unclassified (1)
+      |   app (1)
+      |    java.test.db (1)
+      |     MainActivity (1)
+      |      onCreate(Bundle) (1)
+      |       13System.out.println(binding.button.getId());
+      """.trimMargin()
+    )
+
+    // Find usages for layout 2 should not include the binding class reference
+    fixture.configureFromExistingVirtualFile(layoutFile2.virtualFile)
+    ApplicationManager.getApplication().invokeAndWait { fixture.moveCaret("@+id/but|ton") }
+    val presentationLayout2 = getUsagePresentationAtCursor()
+
+    assertThat(presentationLayout2).contains("<root> (2)")
+    assertThat(presentationLayout2).contains(
+      """
+      |  Resource declaration in Android resources XML (2)
+      |   app (2)
+      |    res/layout (2)
+      |     activity_main.xml (1)
+      |      7android:id="@+id/button"
+      |     activity_main2.xml (1)
+      |      7android:id="@+id/button"
+      """.trimMargin()
+    )
+    assertThat(presentationLayout2).doesNotContain("binding.button")
   }
 
   private fun getUsagePresentationAtCursor(): String {
