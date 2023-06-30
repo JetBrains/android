0e534c6cdc14493183a38c621a40f31ecb2f6122
Andy Magee <andymagee@google.com>
2023-06-30 09:25:51 -0700

    Migrate FindUsagesTest from UI test framework
    The test data is copied verbatim from the existing UI test. It's copied
    rather than moved since the same test data is still used in other UI
    tests.
    
    Fixes: 279969556
    Test: test code
    Change-Id: Id24464e2d0a99b4321ed33f40ce7c392f4506e3d
    
    GitOrigin-RevId: 86705c5b3448598aa6c75ff2e13297ec44f9b974

---
+++ android/testData/findUsagesJavaKotlin/MainActivity.java
@@ -0,0 +1,28 @@
+package com.codegeneration;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    String hello = "hello";
+    addNewCar();
+    new Inventory().addNewCar();
+  }
+
+  private void addNewCar() {
+    Car car = Car.getInstance();
+    car.setMake("BMW");
+    car.setModel("M5");
+    car.setYear(2019);
+
+    String make = car.getMake();
+    String model = car.getModel();
+    int year = car.getYear();
+  }
+}
