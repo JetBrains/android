ae848b75b4a0a2f9de52bbe149d624e267c3c160
Andy Magee <andymagee@google.com>
2023-07-11 09:41:21 -0700

    Listen to PROJECT_SYSTEM_SYNC_TOPIC
    AndroidProjectRootListener needs to notify consumers when module roots
    change, or more specifically when the resource folders associated with a
    module may change. The current event in use does not fire at all times
    when sync completes, so this change is adding in a second event that
    should help capture more cases.
    
    The consumers that are invalidated try to be smart about only
    invalidating folders that have actually changed, so I don't anticipate a
    larger perf impact to invalidating more frequently than we used to.
    
    Fixes: 286877308
    Test: AndroidProjectRootListenerTest
    Change-Id: I357f9f25c733b74add2829c870437763a23987d9
    
    GitOrigin-RevId: ad73fa8f2f31a01ca3b40af50597edf5778167ac

--- android/src/com/android/tools/idea/res/AndroidProjectRootListener.java
+++ android/src/com/android/tools/idea/res/AndroidProjectRootListener.java
@@ -16,8 +16,12 @@
 package com.android.tools.idea.res;
 
 import com.android.tools.idea.model.AndroidModel;
+import com.android.tools.idea.projectsystem.ProjectSystemSyncManager.SyncResultListener;
+import com.android.tools.idea.projectsystem.ProjectSystemSyncManager.SyncResult;
+import com.android.tools.idea.projectsystem.ProjectSystemSyncUtil;
 import com.intellij.ProjectTopics;
 import com.intellij.openapi.Disposable;
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.module.ModuleManager;
 import com.intellij.openapi.progress.ProgressIndicator;
@@ -25,6 +29,7 @@ import com.intellij.openapi.project.DumbModeTask;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.roots.ModuleRootEvent;
 import com.intellij.openapi.roots.ModuleRootListener;
+import com.intellij.util.messages.MessageBusConnection;
 import org.jetbrains.android.facet.AndroidFacet;
 import org.jetbrains.android.facet.ResourceFolderManager;
 import org.jetbrains.annotations.NotNull;
@@ -45,10 +50,22 @@ public class AndroidProjectRootListener implements Disposable.Default {
   }
 
   private AndroidProjectRootListener(@NotNull Project project) {
-    project.getMessageBus().connect(this).subscribe(ProjectTopics.PROJECT_ROOTS, new ModuleRootListener() {
+    MessageBusConnection messageBusConnection = project.getMessageBus().connect(this);
+
+    messageBusConnection.subscribe(ProjectTopics.PROJECT_ROOTS, new ModuleRootListener() {
       @Override
       public void rootsChanged(@NotNull ModuleRootEvent event) {
         moduleRootsOrDependenciesChanged(project);
+      }
+    });
+
+    messageBusConnection.subscribe(ProjectSystemSyncUtil.PROJECT_SYSTEM_SYNC_TOPIC, new SyncResultListener() {
+      @Override
+      public void syncEnded(@NotNull SyncResult result) {
+        // This event is called on the EDT. Calling `moduleRootsOrDependenciesChanged` directly ends up executing the DumbModeTask
+        // synchronously, which has leads to failures due to the state we're in from higher up the stack. Executing this on the EDT later
+        // avoids that situation.
+        ApplicationManager.getApplication().invokeLater(() -> moduleRootsOrDependenciesChanged(project));
       }
     });
   }
@@ -59,7 +76,9 @@ public class AndroidProjectRootListener implements Disposable.Default {
    * @param project the project whose module roots changed
    */
   private static void moduleRootsOrDependenciesChanged(@NotNull Project project) {
-    new MyDumbModeTask(project).queue(project);
+    if (!project.isDisposed()) {
+      new MyDumbModeTask(project).queue(project);
+    }
   }
 
   /**
