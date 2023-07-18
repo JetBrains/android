c17c14bc64ff770ce4ade08544bb2b55a7b34c90
Paul Thomson <paulthomson@google.com>
2023-07-18 15:03:19 +0100

    Convert LintInspectionDescriptionLinkHandler.kt
    Convert LintInspectionDescriptionLinkHandler to Kotlin.
    
    Bug: N/A
    Test: N/A
    Change-Id: I5fb0490a0f0a7ff728b566a918df67ac1f959ad5
    
    GitOrigin-RevId: 7600b659b8f21d3a5677c375509f8ee74f2577e8

--- lint/src/com/android/tools/idea/lint/common/LintInspectionDescriptionLinkHandler.kt
+++ lint/src/com/android/tools/idea/lint/common/LintInspectionDescriptionLinkHandler.kt
@@ -13,64 +13,48 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.android.tools.idea.lint.common;
+package com.android.tools.idea.lint.common
 
-import com.android.tools.lint.checks.BuiltinIssueRegistry;
-import com.android.tools.lint.client.api.IssueRegistry;
-import com.android.tools.lint.client.api.Vendor;
-import com.android.tools.lint.detector.api.Issue;
-import com.android.tools.lint.detector.api.Option;
-import com.android.tools.lint.detector.api.TextFormat;
-import com.intellij.codeInsight.highlighting.TooltipLinkHandler;
-import com.intellij.openapi.editor.Editor;
-import java.util.List;
-import org.jetbrains.annotations.NotNull;
+import com.android.tools.lint.checks.BuiltinIssueRegistry
+import com.android.tools.lint.detector.api.Option
+import com.android.tools.lint.detector.api.TextFormat
+import com.intellij.codeInsight.highlighting.TooltipLinkHandler
+import com.intellij.openapi.editor.Editor
 
-public class LintInspectionDescriptionLinkHandler extends TooltipLinkHandler {
-  @Override
-  public String getDescription(@NotNull final String refSuffix, @NotNull final Editor editor) {
-    Issue issue = new BuiltinIssueRegistry().getIssue(refSuffix);
-    if (issue == null) {
-      issue = AndroidLintInspectionBase.findIssueByShortName(editor.getProject(), refSuffix);
-    }
+class LintInspectionDescriptionLinkHandler : TooltipLinkHandler() {
+  override fun getDescription(refSuffix: String, editor: Editor): String? {
+    val issue =
+      BuiltinIssueRegistry().getIssue(refSuffix)
+        ?: AndroidLintInspectionBase.findIssueByShortName(editor.project, refSuffix) ?: return null
+    val html = issue.getExplanation(TextFormat.HTML)
 
-    if (issue != null) {
-      String html = issue.getExplanation(TextFormat.HTML);
-      StringBuilder sb = new StringBuilder(html);
+    val description = buildString {
+      append(html)
+      append("<br><br>Issue id: ${issue.id}")
 
-      sb.append("<br><br>Issue id: ").append(issue.getId());
-
-      List<Option> options = issue.getOptions();
-      if (!options.isEmpty()) {
-        sb.append("<br><br>");
-        String optionsHtml = Option.Companion.describe(options, TextFormat.HTML, true);
-        sb.append(optionsHtml);
+      val options = issue.getOptions()
+      if (options.isNotEmpty()) {
+        append("<br><br>${Option.describe(options, TextFormat.HTML, true)}")
       }
 
-      List<String> urls = issue.getMoreInfo();
-      if (!urls.isEmpty()) {
-        sb.append("<br><br>More info:<br>");
-        for (String url : urls) {
-          sb.append("<a href=\"").append(url).append("\">").append(url).append("</a><br>");
+      val urls = issue.moreInfo
+      if (urls.isNotEmpty()) {
+        append("<br><br>More info:<br>")
+        for (url in urls) {
+          append("<a href=\"${url}\">${url}</a><br>")
         }
       }
 
-      Vendor vendor = issue.getVendor();
-      IssueRegistry registry = issue.getRegistry();
-      if (vendor == null && registry != null) {
-        vendor = registry.getVendor();
-      }
+      val vendor = issue.vendor ?: issue.registry?.vendor
       if (vendor != null) {
-        sb.append("<br>\n");
-        vendor.describeInto(sb, TextFormat.HTML, "");
+        append("<br>")
+        vendor.describeInto(this, TextFormat.HTML, "")
       }
-
-      // IntelliJ seems to treat newlines in the HTML as needing to also be converted to <br> (whereas
-      // Lint includes these for HTML readability but they shouldn't add additional lines since it has
-      // already added <br> as well) so strip these out
-      return sb.toString().replace("\n", "");
     }
 
-    return null;
+    // IntelliJ seems to treat newlines in the HTML as needing to also be converted to <br> (whereas
+    // Lint includes these for HTML readability but they shouldn't add additional lines since it has
+    // already added <br> as well) so strip these out
+    return description.replace("\n", "")
   }
 }
