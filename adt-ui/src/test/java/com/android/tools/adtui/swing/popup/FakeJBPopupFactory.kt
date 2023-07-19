165da03ef8b75dbe3c4edbe8a4388c09c68480df
Andrii Rublov <andrii.rublov@jetbrains.com>
2023-07-19 04:53:40 +0200

    RIDER-7033: Add new overload for `JBPopupFactory.createHtmlTextBalloonBuilder` to accept `Html` instance instead of `String htmlContent`
    (cherry picked from commit 7f0c3a4681ec499d4634188b6ada6d4c47e0e7d0)
    
    IJ-MR-115584
    
    GitOrigin-RevId: b3b6255ab893f90522125c8ba717dae3a71ad053

--- adt-ui/src/test/java/com/android/tools/adtui/swing/popup/FakeJBPopupFactory.kt
+++ adt-ui/src/test/java/com/android/tools/adtui/swing/popup/FakeJBPopupFactory.kt
@@ -37,6 +37,7 @@ import com.intellij.openapi.ui.popup.TreePopup
 import com.intellij.openapi.ui.popup.TreePopupStep
 import com.intellij.openapi.util.Condition
 import com.intellij.ui.awt.RelativePoint
+import com.intellij.util.ui.Html
 import java.awt.Color
 import java.awt.Component
 import java.awt.Point
@@ -225,6 +226,14 @@ class FakeJBPopupFactory : JBPopupFactory() {
   }
 
   override fun createHtmlTextBalloonBuilder(htmlContent: String,
+                                            icon: Icon?,
+                                            textColor: Color?,
+                                            fillColor: Color?,
+                                            listener: HyperlinkListener?): BalloonBuilder {
+    TODO("Not yet implemented")
+  }
+
+  override fun createHtmlTextBalloonBuilder(html: Html,
                                             icon: Icon?,
                                             textColor: Color?,
                                             fillColor: Color?,
