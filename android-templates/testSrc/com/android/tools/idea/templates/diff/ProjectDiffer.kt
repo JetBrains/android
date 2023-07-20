169e33f92e9baa09259115f8d4b25dd4b8be1185
Hannah Chu <hanchu@google.com>
2023-07-20 12:03:30 -0700

    Remove the string replacements in ProjectDiffer
    They haven't been useful since we don't check in Gradle-synced golden
    files anymore.
    
    Fixes: 278937427
    Test: TemplateDiffTest
    Change-Id: I7371c837247559b9ae80a7d24099f4779b4a5ef6
    
    GitOrigin-RevId: 47fd20e7eb6f9af3e33aa102ce9220382a48d060

--- android-templates/testSrc/com/android/tools/idea/templates/diff/ProjectDiffer.kt
+++ android-templates/testSrc/com/android/tools/idea/templates/diff/ProjectDiffer.kt
@@ -15,7 +15,6 @@
  */
 package com.android.tools.idea.templates.diff
 
-import com.android.tools.idea.templates.diff.TemplateDiffTestUtils.smartDiffAgpVersion
 import com.android.tools.idea.wizard.template.Template
 import com.google.common.truth.Truth
 import com.intellij.util.containers.isEmpty
@@ -74,8 +73,8 @@ private fun diffDirectories(goldenDir: Path, projectDir: Path, printPrefix: Stri
     // it's just for human readability, so
     // diffing all lines and only reading bytes if text fails is simpler.
     try {
-      val goldenLines = Files.readAllLines(goldenFile).map { replaceVariables(it, printPrefix) }
-      val projectLines = Files.readAllLines(projectFile).map { replaceVariables(it, printPrefix) }
+      val goldenLines = Files.readAllLines(goldenFile)
+      val projectLines = Files.readAllLines(projectFile)
       Truth.assertThat(projectLines).isEqualTo(goldenLines)
     } catch (error: MalformedInputException) {
       println("${printPrefix}reading lines failed, compare bytes instead")
@@ -118,117 +117,4 @@ private fun isDirectoryEffectivelyEmpty(dir: Path, printPrefix: String = ""): Bo
     println("${printPrefix}Skipping empty $dir")
   }
   return empty
-}
-
-private fun replaceVariables(text: String, printPrefix: String = ""): String {
-  replaceDates(text, printPrefix).also { dateReplaced ->
-    replaceDistributionUrl(dateReplaced, printPrefix).also { distributionReplaced ->
-      replaceMavenRepoUrl(distributionReplaced, printPrefix).also { mavenReplaced ->
-        return replaceLatestGradleVersion(mavenReplaced, printPrefix)
-      }
-    }
-  }
-}
-
-// TODO: check what localization it is that we generate the files with, date format could be
-// different...
-// This applies to gradle-wrapper.properties and local.properties.
-// Example: #Tue Mar 21 15:26:47 PDT 2023
-// According to Wikipedia's list of time zone abbreviations, some of them just use +03, etc. instead
-// of being letters, hence the \S+
-val PROPERTIES_DATE =
-  Regex(
-    "(Mon|Tue|Wed|Thu|Fri|Sat|Sun) (Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec) \\d{2} \\d{2}:\\d{2}:\\d{2} \\S+ \\d{4}"
-  )
-
-// This string replaces dates in template-generated files. See replaceDates.
-const val PROPERTIES_DATE_REPLACEMENT = "DATE_REPLACED"
-
-/**
- * Some metadata files contain the generated date/time, which we need to replace in order to diff
- * the whole file
- */
-private fun replaceDates(text: String, printPrefix: String = ""): String {
-  val replacedText = PROPERTIES_DATE.replace(text, PROPERTIES_DATE_REPLACEMENT)
-  if (replacedText != text) {
-    println("${printPrefix}Replaced date in $text")
-  }
-  return replacedText
-}
-
-// The distributionUrl value in gradle-wrapper.properties of similar format to:
-// distributionUrl=file\:/C\:/src/studio-main/tools/external/gradle/gradle-8.0-bin.zip
-val GRADLE_DISTRIBUTION_URL = Regex("distributionUrl=.*[\\\\/]")
-
-// This string replaces the distributionUrl path in template-generated files. See
-// replaceDistributionUrl.
-// It replaces the path such that we can still diff the gradle version:
-// distributionUrl=DISTRIBUTION_URL_REPLACEDgradle-8.0-bin.zip
-const val GRADLE_DISTRIBUTION_URL_REPLACEMENT = "distributionUrl=DISTRIBUTION_URL_REPLACED"
-
-/**
- * gradle-wrapper.properties files contain the generated Gradle distribution path, which we need to
- * replace in order to diff the whole file
- */
-private fun replaceDistributionUrl(text: String, printPrefix: String = ""): String {
-  val replacedText = GRADLE_DISTRIBUTION_URL.replace(text, GRADLE_DISTRIBUTION_URL_REPLACEMENT)
-  if (replacedText != text) {
-    println("${printPrefix}Replaced distribution URL in $text")
-  }
-  return replacedText
-}
-
-// The url value in build.gradle under the maven section. When run from IDEA it may be of similar
-// format to:
-// url "file:/C:/src/studio-main/prebuilts/tools/common/m2/repository/"
-// When run from Bazel it may be of similar format to:
-// url "file:/C:/botcode/w40/_tmp/64cb621d51aeb63b419ca295dc73be0b/offline-maven-repo/"
-val MAVEN_REPO_URL = Regex("url \".*[\\\\/]\"")
-
-// This string replaces the Maven repo path in template-generated files. See replaceMavenRepoUrl.
-const val MAVEN_REPO_URL_REPLACEMENT = "url \"MAVEN_REPO_PATH\""
-
-/**
- * build.gradle files contain the generated Maven repo path, which we need to replace in order to
- * diff the whole file
- */
-private fun replaceMavenRepoUrl(text: String, printPrefix: String = ""): String {
-  val replacedText = MAVEN_REPO_URL.replace(text, MAVEN_REPO_URL_REPLACEMENT)
-  if (replacedText != text) {
-    println("${printPrefix}Replaced Maven repo URL in $text")
-  }
-  return replacedText
-}
-
-// The dependency in build.gradle of similar format to:
-// com.android.tools.build:gradle:8.0.0-beta04
-val AGP_VERSION_CLASSPATH = Regex("com\\.android\\.tools\\.build:gradle:.*")
-
-// This string replaces the dependency classpath in template-generated build.gradle. See
-// replaceLatestGradleVersion. It is only replaced when we test the latest AGP version.
-const val AGP_VERSION_CLASSPATH_REPLACEMENT = "LATEST_GRADLE_VERSION"
-
-// The Gradle version in gradle-wrapper.properties of similar format to:
-// distributionUrl=file\:/C\:/src/studio-main/tools/external/gradle/gradle-8.0-bin.zip
-val GRADLE_VERSION_ZIP = Regex("gradle-\\d\\.\\d(\\.\\d)?-bin\\.zip")
-
-// This string replaces the distributionUrl file path in template-generated files. See
-// replaceLatestGradleVersion. It is only replaced when we test the latest AGP version.
-const val GRADLE_VERSION_ZIP_REPLACEMENT = "gradle-LATEST_VERSION-bin.zip"
-
-/**
- * When testing the latest AGP version, we replace the version string because it changes ~weekly, to
- * avoid constantly updating golden files.
- */
-private fun replaceLatestGradleVersion(text: String, printPrefix: String = ""): String {
-  if (!smartDiffAgpVersion()) {
-    return text
-  }
-
-  var replacedText = AGP_VERSION_CLASSPATH.replace(text, AGP_VERSION_CLASSPATH_REPLACEMENT)
-  replacedText = GRADLE_VERSION_ZIP.replace(replacedText, GRADLE_VERSION_ZIP_REPLACEMENT)
-  if (replacedText != text) {
-    println("${printPrefix}Replaced AGP and/or Gradle version in $text")
-  }
-  return replacedText
 }
