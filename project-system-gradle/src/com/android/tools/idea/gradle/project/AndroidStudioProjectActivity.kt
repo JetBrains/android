9ce134f9a7d8bc34bbe4bd8637fe116ce720e741
Bradley Smith <smithbradley@google.com>
2023-07-25 13:05:51 +0000

    Move notifications directly to where they are used
    And remove ProjectNotificationUtils.
    
    Bug: N/A
    Test: N/A
    Change-Id: I93487985bc8d9c7ecd0b4198dd1bbea0a08e0f12
    
    GitOrigin-RevId: ece4dc38d8f2d76a8143d8e167685d4635b06b3a

--- project-system-gradle/src/com/android/tools/idea/gradle/project/AndroidStudioProjectActivity.kt
+++ project-system-gradle/src/com/android/tools/idea/gradle/project/AndroidStudioProjectActivity.kt
@@ -15,9 +15,22 @@
  */
 package com.android.tools.idea.gradle.project
 
+import com.android.tools.idea.gradle.project.sync.hyperlink.SelectJdkFromFileSystemHyperlink
+import com.android.tools.idea.gradle.project.sync.jdk.GradleJdkValidationManager
 import com.android.tools.idea.gradle.util.AndroidStudioPreferences
+import com.android.tools.idea.project.AndroidNotification
+import com.android.tools.idea.project.AndroidProjectInfo
+import com.android.tools.idea.sdk.IdeSdks
+import com.intellij.notification.NotificationType
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.application.EDT
+import com.intellij.openapi.application.runWriteAction
+import com.intellij.openapi.externalSystem.service.execution.ExternalSystemJdkUtil
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.ProjectActivity
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import org.jetbrains.plugins.gradle.settings.GradleSettings
 
 /**
  * Project startup activity run ONLY in Android Studio. It should not be registered as part of the Android IntelliJ plugin.
@@ -30,7 +43,61 @@ class AndroidStudioProjectActivity : ProjectActivity {
     // Disable all settings sections that we don't want to be present in Android Studio.
     // See AndroidStudioPreferences for a full list.
     AndroidStudioPreferences.cleanUpPreferences(project)
+
     // Custom notifications for Android Studio, un-wanted or un-needed when running as the Android IntelliJ plugin
-    showNeededNotifications(project)
+    notifyOnLegacyAndroidProject(project)
+    notifyOnInvalidGradleJDKEnv(project)
+    // Many JDK tests rely on this not running in order to test invalid JDK messages.
+    // This is not an ideal solution but allows it to work for now.
+    if (!ApplicationManager.getApplication().isUnitTestMode) {
+      notifyOnInvalidGradleJdk(project)
+    } else {
+      // This is only run in tests!
+      // The reason we need this is otherwise we get sporadic un-disposed pointers in tests.
+      // It seems calling this ensures that the tests correctly dispose the internal JDK along with all roots.
+      // This is a temporary solution until we can figure out something better.
+      ExternalSystemJdkUtil.getAvailableJdk(project)
+    }
   }
+}
+
+private fun notifyOnLegacyAndroidProject(project: Project) {
+  val legacyAndroidProjects = LegacyAndroidProjects(project)
+  if (AndroidProjectInfo.getInstance(project).isLegacyIdeaAndroidProject
+      && !AndroidProjectInfo.getInstance(project).isApkProject) {
+    legacyAndroidProjects.trackProject()
+    if (!GradleProjectInfo.getInstance(project).isBuildWithGradle) {
+      // Suggest that Android Studio users use Gradle instead of IDEA project builder.
+      legacyAndroidProjects.showMigrateToGradleWarning()
+    }
+  }
+}
+
+private fun notifyOnInvalidGradleJDKEnv(project: Project) {
+  val ideSdks = IdeSdks.getInstance()
+  if (ideSdks.isJdkEnvVariableDefined && !ideSdks.isJdkEnvVariableValid) {
+    val msg = IdeSdks.JDK_LOCATION_ENV_VARIABLE_NAME +
+              " is being ignored since it is set to an invalid JDK Location:\n" +
+              ideSdks.envVariableJdkValue
+    val hyperlinks = listOfNotNull(SelectJdkFromFileSystemHyperlink.create(project, project.basePath))
+    AndroidNotification.getInstance(project).showBalloon(
+      "", msg, NotificationType.WARNING, *hyperlinks.toTypedArray()
+    )
+  }
+}
+
+private suspend fun notifyOnInvalidGradleJdk(project: Project) {
+  GradleSettings.getInstance(project).linkedProjectsSettings
+    .mapNotNull { it.externalProjectPath }
+    .plus(project.basePath)
+    .filterNotNull()
+    .toSet()
+    .forEach {
+      val gradleJdkException = GradleJdkValidationManager.getInstance(project).validateProjectGradleJvmPath(project, it)
+      withContext(Dispatchers.EDT) {
+        runWriteAction {
+          gradleJdkException?.recover()
+        }
+      }
+    }
 }
