5886bb275ae25206af8ac9e3b50f0c3539f454d3
Kurt Dresner <kdresner@google.com>
2023-07-26 12:46:05 -0700

    Convert ResourceRepositoryInnerRClass from Java -> Kotlin (3/3)
    Bug: N/A
    Test: N/A
    Change-Id: I5e9e60656249f8aba8aa2595b14cc2d2c5bcb371
    
    GitOrigin-RevId: fb4ec9e00bd9e0e48049fb5b283abe96184496b6

--- android/src/org/jetbrains/android/augment/ResourceRepositoryInnerRClass.kt
+++ android/src/org/jetbrains/android/augment/ResourceRepositoryInnerRClass.kt
@@ -8,66 +8,63 @@ import com.intellij.psi.PsiClass
 import com.intellij.psi.PsiField
 import com.intellij.psi.PsiTypes
 
-/**
- * Implementation of [InnerRClassBase] backed by a resource repository.
- */
+/** Implementation of [InnerRClassBase] backed by a `ResourceRepository`. */
 class ResourceRepositoryInnerRClass(
   resourceType: ResourceType,
-  private val mySource: ResourcesSource,
+  private val resourcesSource: ResourcesSource,
   parentClass: PsiClass
 ) : InnerRClassBase(parentClass, resourceType) {
-  override fun doGetFields(): Array<PsiField> {
-    return buildLocalResourceFields(myResourceType, mySource, this)
-  }
+  override fun doGetFields() = buildLocalResourceFields(myResourceType, resourcesSource, this)
 
   /**
-   * {@inheritDoc}
-   *
-   * This implementation adds a fast path for non final resources and delegates to the super implementation in
-   * [AndroidLightClassBase.findFieldByName] for everything else. The super implementation
-   * relies on first creating the PsiFields for *all* resources and then searching for the requested field.
+   * This implementation adds a fast path for non-final resources and delegates to the super implementation in
+   * [AndroidLightClassBase.findFieldByName] for everything else. The `super` implementation
+   * relies on first creating the [PsiField]s for *all* resources and then searching for the requested field.
    * In addition, the logic for determining whether a field should be final or not relies on a very expensive
-   * Manifest class creation [AndroidCompileUtil.findCircularDependencyOnLibraryWithSamePackage].
+   * Manifest class creation `AndroidCompileUtil.findCircularDependencyOnLibraryWithSamePackage`.
    * Both of those operations are avoided for the common case in this implementation.
    */
   override fun findFieldByName(name: String, checkBases: Boolean): PsiField? {
-    // bail if this is a scenario we don't fully support
-    if ( // app projects use final ids, which requires assigning ids to all fields
-      myResourceType == ResourceType.STYLEABLE || mySource.fieldModifier === AndroidLightField.FieldModifier.FINAL || name.contains("_")) { // Resource fields with underscores are flattened resources, for which ResourceRepository.hasResources will not find the correct resource.
-      return super.findFieldByName(name, checkBases)
-    }
-    return if (!mySource.resourceRepository.hasResources(mySource.resourceNamespace, myResourceType, name)) {
-      null
-    } else ResourceLightField(
+    // Bail if this is a scenario we don't fully support.
+    if (scenarioUnsupported(name)) return super.findFieldByName(name, checkBases)
+
+    if (!resourcesSource.resourceRepository.hasResources(resourcesSource.resourceNamespace, myResourceType, name)) return null
+
+    return ResourceLightField(
       name,
-      this,
+      myContext = this,
       PsiTypes.intType(),
       AndroidLightField.FieldModifier.NON_FINAL,
-      null,
+      myConstantValue = null,
       ResourceVisibility.PUBLIC
     )
   }
 
-  override fun getFieldsDependencies(): ModificationTracker {
-    return ModificationTracker { mySource.resourceRepository.modificationCount }
-  }
+  override fun getFieldsDependencies() = ModificationTracker { resourcesSource.resourceRepository.modificationCount }
+
+  /**
+   * Returns whether this scenario is unsupported. This can be due to one of three reasons:
+   * * The [ResourceType] is [ResourceType.STYLEABLE] - styleables require further modification of the name to handle sub attributes.
+   * * The ID is `final` - App projects use final IDs, which requires assigning IDs to all fields.
+   * * The [name] contains an underscore - resource fields with underscores are flattened resources, for which
+   *   `ResourceRepository.hasResources` will not find the correct resource.
+   */
+  private fun scenarioUnsupported(name: String) =
+    myResourceType == ResourceType.STYLEABLE
+    || resourcesSource.fieldModifier == AndroidLightField.FieldModifier.FINAL
+    || name.contains("_")
 
   companion object {
-    fun buildLocalResourceFields(
-      resourceType: ResourceType,
-      resourcesSource: ResourcesSource,
-      context: PsiClass
-    ): Array<PsiField> {
-      val modifier = resourcesSource.fieldModifier
-      return buildResourceFields(
-        resourcesSource.resourceRepository,
-        resourcesSource.resourceNamespace,
-        resourcesSource.resourceRepositoryManager,
-        modifier,
-        ACCESSIBLE_RESOURCE_FILTER,
-        resourceType,
-        context
-      )
-    }
+    fun buildLocalResourceFields(resourceType: ResourceType, resourcesSource: ResourcesSource, context: PsiClass): Array<PsiField> =
+      with(resourcesSource) {
+        buildResourceFields(
+          resourceRepository,
+          resourceNamespace,
+          resourceRepositoryManager,
+          fieldModifier,
+          ACCESSIBLE_RESOURCE_FILTER,
+          resourceType,
+          context)
+      }
   }
 }
