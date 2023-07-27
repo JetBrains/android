88d1588acf337a90236224d91b4cb3989ada09cc
Kurt Dresner <kdresner@google.com>
2023-07-27 13:01:39 -0700

    Convert AndroidInternalRClass from Java -> Kotlin (3/3)
    Bug: N/A
    Test: N/A
    Change-Id: I2d848ef8a716024c20e5373f18b111f77ee15a81
    
    GitOrigin-RevId: c99ff4295792631a32128452f50c7615563d1b17

--- android/src/org/jetbrains/android/augment/AndroidInternalRClass.kt
+++ android/src/org/jetbrains/android/augment/AndroidInternalRClass.kt
@@ -2,7 +2,6 @@ package org.jetbrains.android.augment
 
 import com.android.annotations.concurrency.Slow
 import com.android.ide.common.rendering.api.ResourceNamespace
-import com.android.ide.common.resources.ResourceItem
 import com.android.resources.ResourceType
 import com.android.tools.idea.res.AndroidInternalRClassFinder
 import com.android.tools.sdk.AndroidPlatform
@@ -21,75 +20,46 @@ import com.intellij.psi.PsiFileFactory
 import com.intellij.psi.PsiManager
 import com.intellij.psi.PsiModifier
 
-class AndroidInternalRClass(psiManager: PsiManager, platform: AndroidPlatform, sdk: Sdk) : AndroidLightClassBase(
-  psiManager, ImmutableSet.of(
-    PsiModifier.PUBLIC, PsiModifier.STATIC, PsiModifier.FINAL
-  )
+class AndroidInternalRClass(psiManager: PsiManager, private val platform: AndroidPlatform, sdk: Sdk) : AndroidLightClassBase(
+  psiManager,
+  ImmutableSet.of(PsiModifier.PUBLIC, PsiModifier.STATIC, PsiModifier.FINAL)
 ) {
-  private val myFile: PsiFile
-  private val myPlatform: AndroidPlatform
-  private val myInnerClasses: Array<PsiClass>
+  private val file: PsiFile = PsiFileFactory.getInstance(myManager.project).createFileFromText("R.java", JavaFileType.INSTANCE, "")
+  private val innerClasses: Array<PsiClass>
 
   init {
-    myFile = PsiFileFactory.getInstance(myManager.project).createFileFromText("R.java", JavaFileType.INSTANCE, "")
-    myFile.viewProvider.virtualFile.putUserData(ANDROID_INTERNAL_R, sdk)
+    file.viewProvider.virtualFile.putUserData(ANDROID_INTERNAL_R, sdk)
     setModuleInfo(sdk)
-    myPlatform = platform
-    val types = ResourceType.values()
-    myInnerClasses = arrayOfNulls(types.size)
-    for (i in types.indices) {
-      myInnerClasses[i] = MyInnerClass(types[i])
-    }
+    innerClasses = ResourceType.values().map(::MyInnerClass).toTypedArray()
   }
 
-  override fun getQualifiedName(): String? {
-    return AndroidInternalRClassFinder.INTERNAL_R_CLASS_QNAME
-  }
-
-  override fun getName(): String {
-    return "R"
-  }
-
-  override fun getContainingClass(): PsiClass? {
-    return null
-  }
-
-  override fun getContainingFile(): PsiFile? {
-    return myFile
-  }
-
-  override fun getTextRange(): TextRange {
-    return TextRange.EMPTY_RANGE
-  }
-
-  override fun getInnerClasses(): Array<PsiClass> {
-    return myInnerClasses
-  }
+  override fun getQualifiedName() = AndroidInternalRClassFinder.INTERNAL_R_CLASS_QNAME
+  override fun getName() = "R"
+  override fun getContainingClass() = null
+  override fun getContainingFile() = file
+  override fun getTextRange(): TextRange = TextRange.EMPTY_RANGE
+  override fun getInnerClasses() = innerClasses
 
   private inner class MyInnerClass(resourceType: ResourceType) : InnerRClassBase(this@AndroidInternalRClass, resourceType) {
     @Slow
     override fun doGetFields(): Array<PsiField> {
-      val targetData = AndroidTargetData.get(myPlatform.sdkData, myPlatform.target)
-      val repository = targetData.getFrameworkResources(ImmutableSet.of())
-        ?: return PsiField.EMPTY_ARRAY
+      val repository = AndroidTargetData.get(platform.sdkData, platform.target).getFrameworkResources(ImmutableSet.of())
+                       ?: return PsiField.EMPTY_ARRAY
       return buildResourceFields(
-        repository, ResourceNamespace.ANDROID, null,
+        repository, ResourceNamespace.ANDROID, studioResourceRepositoryManager = null,
         AndroidLightField.FieldModifier.FINAL,
-        { resource: ResourceItem? -> true },
+        resourceFilter = { true },
         resourceType,
-        this@AndroidInternalRClass
+        context = this@AndroidInternalRClass
       )
     }
 
-    override val fieldsDependencies: ModificationTracker
-      protected get() = ModificationTracker.NEVER_CHANGED
+    override val fieldsDependencies: ModificationTracker = ModificationTracker.NEVER_CHANGED
   }
 
   companion object {
     private val ANDROID_INTERNAL_R = Key.create<Sdk>("ANDROID_INTERNAL_R")
     @JvmStatic
-    fun isAndroidInternalR(file: VirtualFile, sdk: Sdk): Boolean {
-      return sdk == file.getUserData(ANDROID_INTERNAL_R)
-    }
+    fun isAndroidInternalR(file: VirtualFile, sdk: Sdk) = sdk == file.getUserData(ANDROID_INTERNAL_R)
   }
 }
