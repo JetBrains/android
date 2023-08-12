106f43de6a74447ae0e7275414e6ff3d24101b2e
Alexander Demurov <ademurov@google.com>
2023-08-12 15:11:58 -0700

    Remove unused code
    Bug: n/a
    Test: n/a
    Change-Id: I69797e10678c4d944e3ce8d0605aa35827447eb4
    
    GitOrigin-RevId: 4bf827db7088d49c020f064bbe4e34cf898975ff

--- deploy/src/com/android/tools/idea/run/DeploymentApplicationServiceImpl.java
+++ deploy/src/com/android/tools/idea/run/DeploymentApplicationServiceImpl.java
@@ -17,36 +17,25 @@ package com.android.tools.idea.run;
 
 import com.android.ddmlib.Client;
 import com.android.ddmlib.IDevice;
-import com.android.sdklib.AndroidVersion;
 import com.android.tools.idea.run.deployable.ApplicationIdResolver;
-import com.android.tools.idea.run.deployable.DeviceVersion;
-import com.google.common.util.concurrent.ListenableFuture;
 import com.intellij.openapi.Disposable;
 import java.util.List;
 import org.jetbrains.annotations.NotNull;
 
 public class DeploymentApplicationServiceImpl implements Disposable, DeploymentApplicationService {
   private final ApplicationIdResolver myApplicationIdResolver;
-  private final DeviceVersion myDeviceVersion;
 
   private DeploymentApplicationServiceImpl() {
     myApplicationIdResolver = new ApplicationIdResolver();
-    myDeviceVersion = new DeviceVersion();
   }
 
   @Override
   public void dispose() {
-    myDeviceVersion.dispose();
     myApplicationIdResolver.dispose();
   }
 
   @NotNull
   public List<Client> findClient(@NotNull IDevice iDevice, @NotNull String applicationId) {
     return myApplicationIdResolver.resolve(iDevice, applicationId);
-  }
-
-  @NotNull
-  public ListenableFuture<AndroidVersion> getVersion(@NotNull IDevice iDevice) {
-    return myDeviceVersion.get(iDevice);
   }
 }
