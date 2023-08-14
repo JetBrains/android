a9998b78dc0ba8408ecd77d958c02250be5209ae
Pierfrancesco Soffritti <psoffritti@google.com>
2023-08-14 17:48:09 +0100

    Synchronize access to _selectedProcessListeners
    Test: NA
    Bug: NA
    Change-Id: I1115b95ced5d8e6c0a299792fddfd509ff04fd0c
    
    GitOrigin-RevId: 8db76913fb8782fb16111c656dc093c02bf69b90

--- app-inspection/api/src/com/android/tools/idea/appinspection/api/process/ProcessesModel.kt
+++ app-inspection/api/src/com/android/tools/idea/appinspection/api/process/ProcessesModel.kt
@@ -66,7 +66,7 @@ class ProcessesModel(
   @GuardedBy("lock") private val _selectedProcessListeners = mutableMapOf<() -> Unit, Executor>()
 
   val selectedProcessListeners: Map<() -> Unit, Executor>
-    get() = _selectedProcessListeners
+    get() = synchronized(lock) { _selectedProcessListeners }
 
   @GuardedBy("lock") private val _processes = mutableSetOf<ProcessDescriptor>()
 
