714d3217b9fe2ae51d9b0df8bcba221a4ef7588f
Yasser Dbeis <ydbeis@google.com>
2023-08-16 17:38:46 -0700

    Add CpuTaskArgs class
    ...to serve as a wrapper for all arguments/data
    passed to a CPU task handler. The wrapped data
    can provide configuration data to customize how
    the task is started/launched, and/or provide the
    data to load a past task.
    
    Bug: 277798877
    Test: none
    Change-Id: I3ce74ae0cf9c3ee9e09799acf04e44849513fe73
    
    GitOrigin-RevId: dcd1c64f506cad218118772daa89ea4559366fa8

---
+++ profilers/src/com/android/tools/profilers/tasks/args/singleartifact/cpu/CpuTaskArgs.kt
@@ -0,0 +1,26 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.profilers.tasks.args.singleartifact.cpu
+
+import com.android.tools.profilers.cpu.CpuCaptureSessionArtifact
+import com.android.tools.profilers.tasks.args.singleartifact.SingleArtifactTaskArgs
+
+/**
+ * The following class serves as a wrapper for all arguments/data passed to a CPU task handler.
+ */
+class CpuTaskArgs(val artifact: CpuCaptureSessionArtifact): SingleArtifactTaskArgs {
+  fun getCpuCaptureArtifact() = artifact
+}
