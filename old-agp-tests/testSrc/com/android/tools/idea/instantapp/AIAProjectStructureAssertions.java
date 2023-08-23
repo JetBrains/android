39c45848160fa71ef6eb4e05b8da9f51efc75cb3
Christophe Rhodes <xof@google.com>
2023-08-23 14:38:21 +0100

    Remove deprecated usage
    This was the last usage of getModuleDependencies().
    
    Bug: 262058500
    Test: existing
    Change-Id: Ibe8508776a30e697da7453bb4adf543a3483c729
    
    GitOrigin-RevId: fce5a4f5116a7301d43130949a93723c4b0b4ec5

--- old-agp-tests/testSrc/com/android/tools/idea/instantapp/AIAProjectStructureAssertions.java
+++ old-agp-tests/testSrc/com/android/tools/idea/instantapp/AIAProjectStructureAssertions.java
@@ -19,11 +19,10 @@ import static com.google.common.truth.Truth.assertThat;
 
 import com.android.tools.idea.gradle.model.IdeAndroidProjectType;
 import com.android.tools.idea.gradle.model.IdeDependencies;
-import com.android.tools.idea.gradle.model.IdeDependencyKt;
+import com.android.tools.idea.gradle.model.IdeModuleLibrary;
 import com.android.tools.idea.gradle.project.model.GradleAndroidModel;
 import com.google.common.collect.ImmutableList;
 import com.intellij.openapi.module.Module;
-import com.intellij.util.containers.ContainerUtil;
 import java.util.Collection;
 import java.util.List;
 import org.jetbrains.annotations.NotNull;
@@ -80,7 +79,11 @@ public class AIAProjectStructureAssertions {
 
     IdeDependencies dependencies = model.getSelectedMainCompileDependencies();
     List<String> libraries =
-      ContainerUtil.map(dependencies.getModuleDependencies(), IdeDependencyKt::getProjectPath);
+      dependencies.getLibraries().stream()
+        .filter((x) -> x instanceof IdeModuleLibrary)
+        .map((x) -> (IdeModuleLibrary)x)
+        .map(IdeModuleLibrary::getProjectPath)
+        .toList();
     assertThat(libraries).containsExactlyElementsIn(expectedDependencies);
   }
 }
