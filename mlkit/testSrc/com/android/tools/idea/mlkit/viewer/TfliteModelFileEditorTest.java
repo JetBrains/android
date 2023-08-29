de72e1b95e4b4bc907edbf3cbb2e74a13a9b1e22
Alon Albert <aalbert@google.com>
2023-08-29 15:26:48 -0700

    Read Contents Directly From VirtualFile
    Will make it work if the file is in an APK or any other non physical
    file
    
    Fixes: 297027924
    Test: Added
    Change-Id: I28f02416e3596292a5a6729e47e8f9c23812fd5a
    
    GitOrigin-RevId: 9efa4af204629743a7a415499aae129a945be676

--- mlkit/testSrc/com/android/tools/idea/mlkit/viewer/TfliteModelFileEditorTest.java
+++ mlkit/testSrc/com/android/tools/idea/mlkit/viewer/TfliteModelFileEditorTest.java
@@ -27,10 +27,12 @@ import com.android.tools.idea.mlkit.MlProjectTestUtil;
 import com.google.wireless.android.sdk.stats.AndroidStudioEvent.EventKind;
 import com.google.wireless.android.sdk.stats.MlModelBindingEvent;
 import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.testFramework.BinaryLightVirtualFile;
 import com.intellij.ui.EditorTextField;
 import com.intellij.ui.components.JBLabel;
 import com.intellij.ui.components.JBTabbedPane;
 import com.intellij.util.WaitFor;
+import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
@@ -196,6 +198,18 @@ public class TfliteModelFileEditorTest extends AndroidTestCase {
     assertThat(contentPanel.getComponentCount()).isEqualTo(2);
     verifySectionPanelContainsLabel((JPanel)contentPanel.getComponent(0), "Model");
     verifySectionPanelContainsLabel((JPanel)contentPanel.getComponent(1), "Sample Code");
+  }
+
+  public void testNonPhysicalFile_doesNotThrow() throws IOException {
+    VirtualFile modelFile = setupProject("mobilenet_quant_no_metadata.tflite", "ml/my_model.tflite");
+    BinaryLightVirtualFile virtualFile = new BinaryLightVirtualFile("ml/my_model.tflite", modelFile.contentsToByteArray());
+    TfliteModelFileEditor editor = new TfliteModelFileEditor(myFixture.getProject(), virtualFile);
+
+    try {
+      editor.getComponent();
+    } catch (Exception e) {
+      fail("Should not throw");
+    }
   }
 
   private static void verifySectionPanelContainsLabel(@NonNull JPanel sectionPanel, @NonNull String labelText) {
