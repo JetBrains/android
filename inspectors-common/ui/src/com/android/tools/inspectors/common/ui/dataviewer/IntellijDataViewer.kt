8fdae2ea494d37c468f250a8096f0e931471f66c
Alon Albert <aalbert@google.com>
2023-08-30 09:00:28 -0700

    Convert to Kotlin
    Motivation is to explore b/259650988 and potentially fix with coroutines
    
    Bug: 259650988
    Test: Existing
    Change-Id: I7d490768c7a7befb58b70703a714ad49e8f63b69
    
    GitOrigin-RevId: e9a207cc38f49fe6527ad2307eae7a79158de605

--- inspectors-common/ui/src/com/android/tools/inspectors/common/ui/dataviewer/IntellijDataViewer.kt
+++ inspectors-common/ui/src/com/android/tools/inspectors/common/ui/dataviewer/IntellijDataViewer.kt
@@ -13,165 +13,137 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.android.tools.inspectors.common.ui.dataviewer;
+package com.android.tools.inspectors.common.ui.dataviewer
 
-import com.intellij.codeInsight.actions.ReformatCodeProcessor;
-import com.intellij.codeInsight.folding.CodeFoldingManager;
-import com.intellij.lang.Language;
-import com.intellij.openapi.Disposable;
-import com.intellij.openapi.editor.Document;
-import com.intellij.openapi.editor.EditorFactory;
-import com.intellij.openapi.editor.EditorSettings;
-import com.intellij.openapi.editor.ex.EditorEx;
-import com.intellij.openapi.editor.highlighter.EditorHighlighterFactory;
-import com.intellij.openapi.fileTypes.FileType;
-import com.intellij.openapi.fileTypes.LanguageFileType;
-import com.intellij.openapi.fileTypes.PlainTextLanguage;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.util.Disposer;
-import com.intellij.psi.PsiDocumentManager;
-import com.intellij.psi.PsiFile;
-import com.intellij.psi.PsiFileFactory;
-import com.intellij.util.ui.JBFont;
-import java.util.Collections;
-import javax.swing.JComponent;
-import javax.swing.JLabel;
-import javax.swing.JTextArea;
-import javax.swing.SwingConstants;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import com.android.tools.inspectors.common.ui.dataviewer.DataViewer.Style
+import com.android.tools.inspectors.common.ui.dataviewer.DataViewer.Style.PRETTY
+import com.android.tools.inspectors.common.ui.dataviewer.DataViewer.Style.RAW
+import com.intellij.codeInsight.actions.ReformatCodeProcessor
+import com.intellij.codeInsight.folding.CodeFoldingManager
+import com.intellij.openapi.Disposable
+import com.intellij.openapi.editor.Document
+import com.intellij.openapi.editor.EditorFactory
+import com.intellij.openapi.editor.ex.EditorEx
+import com.intellij.openapi.editor.highlighter.EditorHighlighterFactory
+import com.intellij.openapi.fileTypes.FileType
+import com.intellij.openapi.fileTypes.LanguageFileType
+import com.intellij.openapi.fileTypes.PlainTextLanguage
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.Disposer
+import com.intellij.psi.PsiDocumentManager
+import com.intellij.psi.PsiFileFactory
+import com.intellij.util.ui.JBFont
+import javax.swing.JComponent
+import javax.swing.JLabel
+import javax.swing.JTextArea
+import javax.swing.SwingConstants
+import kotlin.math.min
 
-public class IntellijDataViewer implements DataViewer {
-  private static final int RAW_VIEWER_MAX_STRING_LENGTH = 500;
+private const val RAW_VIEWER_MAX_STRING_LENGTH = 500
+private val editorFactory = EditorFactory.getInstance()
 
-  @NotNull
-  private final JComponent myComponent;
-  @NotNull
-  private final Style myStyle;
-
-  /**
-   * Create a data viewer that renders its content as is, without any attempt to clean it up.
-   * <p>
-   * Note: to prevent UI from being frozen by large text, the content will be truncated.
-   */
-  public static IntellijDataViewer createRawTextViewer(@NotNull byte[] content, Boolean isEditable) {
-    JTextArea textArea = new JTextArea(new String(content, 0, Math.min(content.length, RAW_VIEWER_MAX_STRING_LENGTH)));
-    textArea.setLineWrap(true);
-    textArea.setEditable(isEditable);
-    textArea.setBackground(null);
-    textArea.setOpaque(false);
-    return new IntellijDataViewer(textArea, Style.RAW);
+class IntellijDataViewer private constructor(private val component: JComponent, private val style: Style) : DataViewer {
+  override fun getComponent(): JComponent {
+    return component
   }
 
-  /**
-   * Create an editable data viewer that renders its content as is, without any attempt to clean it up.
-   * <p>
-   * Note: to prevent UI from being frozen by large text, the content will be truncated.
-   */
-  public static IntellijDataViewer createRawTextViewer(@NotNull byte[] content) {
-    return createRawTextViewer(content, false);
+  override fun getStyle(): Style {
+    return style
   }
 
-  /**
-   * Create a data viewer that automatically formats the content it receives. In cases where it is
-   * not able to do this, or it is not desirable to do this (e.g. plain text), it returns a
-   * {@link Style#RAW} viewer instead. Be sure to check {@link IntellijDataViewer#getStyle()} if
-   * this matters for your use-case.
-   *
-   * @param fileType An optional file type that can be associated with this content, which,
-   *                 if provided, hints to the editor how it should format it.
-   */
-  @NotNull
-  public static IntellijDataViewer createPrettyViewerIfPossible(@NotNull Project project,
-                                                                byte[] content,
-                                                                @Nullable FileType fileType,
-                                                                boolean formatted,
-                                                                Disposable parentDisposable) {
-    try {
-      EditorFactory editorFactory = EditorFactory.getInstance();
+  companion object {
+    /**
+     * Create a data viewer that renders its content as is, without any attempt to clean it up.
+     *
+     *
+     * Note: to prevent UI from being frozen by large text, the content will be truncated.
+     */
+    /**
+     * Create an editable data viewer that renders its content as is, without any attempt to clean it up.
+     *
+     *
+     * Note: to prevent UI from being frozen by large text, the content will be truncated.
+     */
+    @JvmOverloads
+    fun createRawTextViewer(content: ByteArray, isEditable: Boolean = false): IntellijDataViewer {
+      val textArea = JTextArea(content.decodeToString(0, min(content.size, RAW_VIEWER_MAX_STRING_LENGTH)))
+      textArea.setLineWrap(true)
+      textArea.isEditable = isEditable
+      textArea.setBackground(null)
+      textArea.setOpaque(false)
+      return IntellijDataViewer(textArea, RAW)
+    }
 
-      // We need to support documents with \r newlines in them (since network payloads can contain
-      // data from any OS); however, Document will assert if it finds a \r as a line ending in its
-      // content and the user will see a mysterious "NO PREVIEW" message without any information
-      // on why. The Document class allows you to change a setting to allow \r, but this breaks
-      // soft wrapping in the editor.
-      String contentStr = new String(content).replace("\r\n", "\n");
+    /**
+     * Create a data viewer that automatically formats the content it receives. In cases where it is
+     * not able to do this, or it is not desirable to do this (e.g. plain text), it returns a
+     * [RAW] viewer instead. Be sure to check [IntellijDataViewer.getStyle] if
+     * this matters for your use-case.
+     *
+     * @param fileType An optional file type that can be associated with this content, which,
+     * if provided, hints to the editor how it should format it.
+     */
+    fun createPrettyViewerIfPossible(
+      project: Project,
+      content: ByteArray,
+      fileType: FileType?,
+      formatted: Boolean,
+      parentDisposable: Disposable
+    ): IntellijDataViewer {
+      return try {
 
-      Style style = Style.RAW;
-      Document document = null;
-      if (fileType instanceof LanguageFileType) {
-        Language language = ((LanguageFileType)fileType).getLanguage();
-        // Creating a "pretty" editor for plain text ends up feeling redundant, as none of the
-        // features matter in that case. Just create a regular editor.
-        if (!language.is(PlainTextLanguage.INSTANCE)) {
-          PsiFile psiFile = PsiFileFactory.getInstance(project).createFileFromText(language, contentStr);
-          if (psiFile != null) {
-            if (formatted) {
-              ReformatCodeProcessor processor = new ReformatCodeProcessor(psiFile, false);
-              processor.run();
-            }
+        // We need to support documents with \r newlines in them (since network payloads can contain
+        // data from any OS); however, Document will assert if it finds a \r as a line ending in its
+        // content and the user will see a mysterious "NO PREVIEW" message without any information
+        // on why. The Document class allows you to change a setting to allow \r, but this breaks
+        // soft wrapping in the editor.
+        val (document, style) = createDocument(project, content.decodeToString().replace("\r\n", "\n"), fileType, formatted)
 
-            document = PsiDocumentManager.getInstance(project).getDocument(psiFile);
-            if (document != null) {
-              style = Style.PRETTY;
-            }
-          }
+        val editor = editorFactory.createViewer(document) as EditorEx
+        editor.setCaretVisible(false)
+        val settings = editor.settings
+        settings.isLineNumbersShown = false
+        settings.isLineMarkerAreaShown = false
+        settings.isUseSoftWraps = true
+        settings.setSoftMargins(emptyList())
+        settings.isRightMarginShown = false
+        settings.isFoldingOutlineShown = true
+        CodeFoldingManager.getInstance(project).updateFoldRegions(editor)
+        if (fileType != null) {
+          editor.highlighter = EditorHighlighterFactory.getInstance().createEditorHighlighter(project, fileType)
         }
+        Disposer.register(parentDisposable) { editorFactory.releaseEditor(editor) }
+        IntellijDataViewer(editor.component, style)
+      } catch (e: Exception) {
+        // Exceptions and AssertionErrors can be thrown by editorFactory.createDocument and editorFactory.createViewer
+        createInvalidViewer()
+      } catch (e: AssertionError) {
+        createInvalidViewer()
       }
-
-      if (document == null) {
-        document = editorFactory.createDocument(contentStr.toCharArray());
-      }
-
-      EditorEx editor = (EditorEx)editorFactory.createViewer(document);
-      editor.setCaretVisible(false);
-      EditorSettings settings = editor.getSettings();
-
-      settings.setLineNumbersShown(false);
-      settings.setLineMarkerAreaShown(false);
-      settings.setUseSoftWraps(true);
-
-      settings.setSoftMargins(Collections.emptyList());
-      settings.setRightMarginShown(false);
-
-      settings.setFoldingOutlineShown(true);
-      CodeFoldingManager.getInstance(project).updateFoldRegions(editor);
-
-      if (fileType != null) {
-        editor.setHighlighter(EditorHighlighterFactory.getInstance().createEditorHighlighter(project, fileType));
-      }
-
-      Disposer.register(parentDisposable, () -> editorFactory.releaseEditor(editor));
-
-      return new IntellijDataViewer(editor.getComponent(), style);
     }
-    catch (Exception | AssertionError e) {
-      // Exceptions and AssertionErrors can be thrown by editorFactory.createDocument and editorFactory.createViewer
-      return createInvalidViewer();
+
+    fun createInvalidViewer(): IntellijDataViewer {
+      val component: JComponent = JLabel("No preview available", SwingConstants.CENTER)
+      component.setFont(JBFont.label().asPlain())
+      return IntellijDataViewer(component, Style.INVALID)
     }
-  }
 
-  @NotNull
-  public static IntellijDataViewer createInvalidViewer() {
-    JComponent component = new JLabel("No preview available", SwingConstants.CENTER);
-    component.setFont(JBFont.label().asPlain());
-    return new IntellijDataViewer(component, Style.INVALID);
-  }
+    private fun createDocument(project: Project, content: String, fileType: FileType?, formatted: Boolean): Pair<Document, Style> {
+      fun createRawDocument() = editorFactory.createDocument(content) to RAW
 
-  private IntellijDataViewer(@NotNull JComponent component, @NotNull Style style) {
-    myComponent = component;
-    myStyle = style;
-  }
+      val language = (fileType as? LanguageFileType)?.language ?: return createRawDocument()
+      if (language === PlainTextLanguage.INSTANCE) {
+        return createRawDocument()
+      }
 
-  @NotNull
-  @Override
-  public JComponent getComponent() {
-    return myComponent;
-  }
+      val psiFile = PsiFileFactory.getInstance(project).createFileFromText(language, content) ?: return createRawDocument()
 
-  @NotNull
-  @Override
-  public Style getStyle() {
-    return myStyle;
+      if (formatted) {
+        val processor = ReformatCodeProcessor(psiFile, false)
+        processor.run()
+      }
+      val psiDocument = PsiDocumentManager.getInstance(project).getDocument(psiFile) ?: return createRawDocument()
+      return psiDocument to PRETTY
+    }
   }
 }
