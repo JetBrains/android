1179d0586e514b9eab2cf472ad743a6914c8afcb
Andy Magee <andymagee@google.com>
2023-08-30 11:29:49 -0700

    Fix formatter used for inspection on layout xml
    AndroidXmlFormattingModelBuilder should be used in all cases dealing
    with formatting for Android layout xml files (as well as a few others).
    
    In some cases, it's not correctly identifying layout files.
    Specifically, this happens when the file passed in is a virtual copy of
    a the original file, as is the case when running formatting to detect
    changes but not actually make changes to the original file.
    
    This change adds logic to detect such virtual files and make decisions
    based upon the original file's type instead.
    
    Fixes: 297790370
    Test: AndroidXmlFormatterTest.testLayoutNoChangesNeeded
    Change-Id: Id161e0a12210e6a84abae0fa62fbe9efbd7bc182
    
    GitOrigin-RevId: 04e74c10afe7d58cc7d9873f56a66c4b342e067b

--- android/src/org/jetbrains/android/formatter/AndroidXmlFormattingModelBuilder.java
+++ android/src/org/jetbrains/android/formatter/AndroidXmlFormattingModelBuilder.java
@@ -9,6 +9,7 @@ import com.intellij.lang.xml.XmlFormattingModelBuilder;
 import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
+import com.intellij.psi.PsiFileFactory;
 import com.intellij.psi.codeStyle.CodeStyleSettings;
 import com.intellij.psi.formatter.xml.XmlBlock;
 import com.intellij.psi.formatter.xml.XmlPolicy;
@@ -26,13 +27,10 @@ public class AndroidXmlFormattingModelBuilder implements CustomFormattingModelBu
 
   @Override
   public boolean isEngagedToFormat(@NotNull PsiElement context) {
-    Object psiFile = context.getContainingFile();
-
-    if (!(psiFile instanceof XmlFile)) {
+    XmlFile xmlFile = getOriginalContainingXmlFile(context);
+    if (xmlFile == null) {
       return false;
     }
-
-    XmlFile xmlFile = (XmlFile)psiFile;
 
     return new ColorStateListDomFileDescription().isMyFile(xmlFile, null) ||
            new DrawableStateListDomFileDescription().isMyFile(xmlFile, null) ||
@@ -87,13 +85,10 @@ public class AndroidXmlFormattingModelBuilder implements CustomFormattingModelBu
 
   @Nullable
   private static MySettings getContextSpecificSettings(@NotNull PsiElement context, @NotNull AndroidXmlCodeStyleSettings settings) {
-    Object psiFile = context.getContainingFile();
-
-    if (!(psiFile instanceof XmlFile)) {
+    XmlFile xmlFile = getOriginalContainingXmlFile(context);
+    if (xmlFile == null) {
       return null;
     }
-
-    XmlFile xmlFile = (XmlFile)psiFile;
 
     if (new ColorStateListDomFileDescription().isMyFile(xmlFile, null) || new DrawableStateListDomFileDescription().isMyFile(xmlFile, null)) {
       return settings.VALUE_RESOURCE_FILE_SETTINGS;
@@ -132,5 +127,23 @@ public class AndroidXmlFormattingModelBuilder implements CustomFormattingModelBu
       default:
         return null;
     }
+  }
+
+  @Nullable
+  private static XmlFile getOriginalContainingXmlFile(PsiElement context) {
+    Object psiFile = context.getContainingFile();
+    if (!(psiFile instanceof XmlFile xmlFile)) {
+      return null;
+    }
+
+    // In some cases (eg, calls originating from FormattingChanges.detectFormattingChanges), the given context is in a virtual file that's a
+    // copy of an actual source file. That copy doesn't have a parent directory, which is needed to determine which files are layout files.
+    // Redirecting to use the original source file allows this builder to correctly determine if it should run on the given file.
+    PsiFile originalFile = xmlFile.getUserData(PsiFileFactory.ORIGINAL_FILE);
+    if (originalFile instanceof XmlFile originalXmlFile) {
+      return originalXmlFile;
+    }
+
+    return xmlFile;
   }
 }
