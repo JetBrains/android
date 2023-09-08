32de38634e9783a1b1f4a15a2fae0bc0b071dd4d
Jinseong Jeon <jsjeon@google.com>
2023-09-08 16:42:10 -0700

    Refactor Compose diagnostics tests
    to be aligned with Compose compiler plugin:
    https://cs.android.com/androidx/platform/frameworks/support/+/androidx-main:compose/compiler/compiler-hosted/integration-tests/src/test/java/androidx/compose/compiler/plugins/kotlin/analysis/
    
    Bug: 298695444
    Test: Composable*CheckerTest relocated/renamed
    Change-Id: I7db202fc03c4b876891b0939edcd25d9cf240e31
    
    GitOrigin-RevId: 0eeddc10606aa0df1a45f68b9d9c911cd5ed2229

--- compose-ide-plugin/testSrc/com/android/tools/compose/TestUtils.kt
+++ compose-ide-plugin/testSrc/com/android/tools/compose/TestUtils.kt
@@ -15,44 +15,12 @@
  */
 package com.android.tools.compose
 
-import androidx.compose.compiler.plugins.kotlin.ComposeCommandLineProcessor
 import com.android.testutils.TestUtils.resolveWorkspacePath
-import com.android.tools.idea.testing.AndroidProjectRule
 import com.intellij.openapi.application.ex.PathManagerEx
-import com.intellij.openapi.project.Project
-import org.jetbrains.kotlin.idea.base.plugin.isK2Plugin
-import org.jetbrains.kotlin.idea.compiler.configuration.KotlinCommonCompilerArgumentsHolder
 import java.nio.file.Files
 
 fun getComposePluginTestDataPath():String {
   val adtPath = resolveWorkspacePath("tools/adt/idea/compose-ide-plugin/testData")
   return if (Files.exists(adtPath)) adtPath.toString()
          else PathManagerEx.findFileUnderCommunityHome("plugins/android-compose-ide-plugin").path
-}
-
-// Kotlin compiler diagnostics to suppress
-// When testing Compose compiler plugin checkers, let's test diagnostics from plugin checkers,
-// not those from built-in compiler checkers.
-private val SUPPRESSION = listOf("UNUSED_PARAMETER", "UNUSED_VARIABLE")
-
-internal val suppressAnnotation: String
-  get() = SUPPRESSION.joinToString(prefix = "@file:Suppress(", postfix = ")") { "\"$it\"" }
-
-private val composeCompilerPluginPath by lazy {
-  resolveWorkspacePath("tools/adt/idea/compose-ide-plugin/lib/compiler-hosted.jar")
-}
-
-private val suppressKotlinVersionCheckOption = "plugin:${
-  ComposeCommandLineProcessor.PLUGIN_ID
-}:${
-  ComposeCommandLineProcessor.SUPPRESS_KOTLIN_VERSION_CHECK_ENABLED_OPTION.optionName
-}=true"
-
-internal fun setUpCompilerArgumentsForComposeCompilerPlugin(project: Project) {
-  if (isK2Plugin()) {
-    KotlinCommonCompilerArgumentsHolder.getInstance(project).update {
-      this.pluginClasspaths = arrayOf(composeCompilerPluginPath.toString())
-      this.pluginOptions = arrayOf(suppressKotlinVersionCheckOption)
-    }
-  }
 }
--- compose-ide-plugin/testSrc/com/android/tools/compose/analysis/ComposableDeclarationCheckerTests.kt
+++ compose-ide-plugin/testSrc/com/android/tools/compose/analysis/ComposableDeclarationCheckerTests.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2020 The Android Open Source Project
+ * Copyright (C) 2023 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.android.tools.compose
+package com.android.tools.compose.analysis
 
 import com.android.tools.idea.testing.AndroidProjectRule
 import org.jetbrains.android.compose.stubComposeRuntime
@@ -25,7 +25,7 @@ import org.junit.Test
 /**
  * Tests for [ComposableDeclarationChecker]
  */
-class ComposableDeclarationCheckerTest {
+class ComposableDeclarationCheckerTests {
   @get:Rule
   val androidProject = AndroidProjectRule.inMemory()
 
