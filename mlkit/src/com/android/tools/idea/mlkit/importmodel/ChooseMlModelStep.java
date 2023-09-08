6ac32d10dc2a5bc7655898972ddeb2f4a5b37e80
Oleg Baskakov <baskakov@google.com>
2023-09-08 14:27:07 -0700

    Remove a check when create tflite model
    ...which isn't needed because studio will create a directory anyway
    
    Fixes: 299351995
    Test: exising
    Change-Id: I329248ddbb59ce1ef0961400ca3fd0a5112fb01f
    
    GitOrigin-RevId: 7a9f5c69e07792b815b5cd4f2581be7cb8f217d9

--- mlkit/src/com/android/tools/idea/mlkit/importmodel/ChooseMlModelStep.java
+++ mlkit/src/com/android/tools/idea/mlkit/importmodel/ChooseMlModelStep.java
@@ -129,7 +129,6 @@ public class ChooseMlModelStep extends ModelWizardStep<MlWizardModel> {
     myValidatorPanel = new ValidatorPanel(this, myPanel);
 
     SelectedItemProperty<NamedModuleTemplate> selectedFavor = new SelectedItemProperty<>(myFlavorBox);
-    myValidatorPanel.registerValidator(ObjectProperty.wrap(selectedFavor), value -> checkFlavor(value));
 
     Expression<File> locationFile = model.sourceLocation.transform(File::new);
     myValidatorPanel.registerValidator(locationFile, value -> checkPath(value), selectedFavor);
@@ -263,15 +262,6 @@ public class ChooseMlModelStep extends ModelWizardStep<MlWizardModel> {
     }
 
     return true;
-  }
-
-  @NotNull
-  private static Validator.Result checkFlavor(@NotNull NamedModuleTemplate flavor) {
-    if (flavor.getPaths().getMlModelsDirectories().isEmpty()) {
-      return new Validator.Result(Validator.Severity.ERROR, "No valid ml directory in checkFlavor.");
-    }
-
-    return Validator.Result.OK;
   }
 
   @NotNull
