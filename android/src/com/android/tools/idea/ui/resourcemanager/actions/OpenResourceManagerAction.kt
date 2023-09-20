537e0d0c1fe5853b207b531d6c91d74a0f8977ec
Oleg Baskakov <baskakov@google.com>
2023-09-20 12:03:45 -0700

    Set ActionUpdateThread on OpenResourceManagerAction
    Fixes: 300535509
    Test: existing
    Change-Id: Ieaa33c3db2b3a2e20ff264fe8b98c3b934ecf1e6
    
    GitOrigin-RevId: f25b630dc37a5514081c5ffcb4b96f8bd93d2d68

--- android/src/com/android/tools/idea/ui/resourcemanager/actions/OpenResourceManagerAction.kt
+++ android/src/com/android/tools/idea/ui/resourcemanager/actions/OpenResourceManagerAction.kt
@@ -17,6 +17,7 @@ package com.android.tools.idea.ui.resourcemanager.actions
 
 import com.android.tools.idea.ui.resourcemanager.RESOURCE_EXPLORER_TOOL_WINDOW_ID
 import com.intellij.facet.ProjectFacetManager
+import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
 import com.intellij.openapi.project.DumbAwareAction
@@ -37,6 +38,8 @@ class OpenResourceManagerAction : DumbAwareAction("Resource Manager", "Open the 
                                          && ProjectFacetManager.getInstance(project).hasFacets(AndroidFacet.ID)
                                          && ToolWindowManager.getInstance(project).getToolWindow(RESOURCE_EXPLORER_TOOL_WINDOW_ID) != null
   }
+
+  override fun getActionUpdateThread() = ActionUpdateThread.BGT
 
   override fun actionPerformed(e: AnActionEvent) {
     val project = e.getData(CommonDataKeys.PROJECT) ?: return
