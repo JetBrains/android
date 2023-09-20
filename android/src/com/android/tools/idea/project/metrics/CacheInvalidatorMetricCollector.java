8d7a7442accea6409cab5801c9719e7fb4c35c64
Christophe Rhodes <xof@google.com>
2023-09-20 15:54:55 +0100

    Move the metric collector for cache invalidation
    It is not particularly Gradle-specific, so move it to a more
    generically-named package.
    
    Bug: 147586218
    Test: existing
    Change-Id: I762062fc720a67f4071834d9bb3cec1754ee9dbe
    
    GitOrigin-RevId: 8ab0f35c653cfddf98f206c13cc63cec5616d620

--- android/src/com/android/tools/idea/project/metrics/CacheInvalidatorMetricCollector.java
+++ android/src/com/android/tools/idea/project/metrics/CacheInvalidatorMetricCollector.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.android.tools.idea.gradle.project.metrics;
+package com.android.tools.idea.project.metrics;
 
 import com.android.tools.analytics.UsageTracker;
 import com.google.wireless.android.sdk.stats.AndroidStudioEvent;
