72cb077e6fbc3ff05722c2cafb18d3bfb519b215
Diego Perez <diegoperez@google.com>
2023-09-20 11:05:28 +0000

    Add missing setAnchorView to Kotlin Basic Activity Template
    This was already part of the Java template but missing for Kotlin.
    
    Fixes: 271958206
    Test: TemplateTests
    Change-Id: I1e1efb74ffe7c262ed0b6546e3ba4cc334fc2960
    
    GitOrigin-RevId: ee94ed10a24b161bacb7ede89f2d0683092c0405

--- android-templates/testData/golden/testNewBasicActivityMaterial3/Template test module/src/main/java/template/test/pkg/MainActivity.kt
+++ android-templates/testData/golden/testNewBasicActivityMaterial3/Template test module/src/main/java/template/test/pkg/MainActivity.kt
@@ -31,7 +31,8 @@ class MainActivity : AppCompatActivity() {
 
         binding.fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+                    .setAction("Action", null)
+                    .setAnchorView(R.id.fab).show()
         }
     }
 
--- android-templates/testData/golden/testNewScrollingActivity/Template test module/src/main/java/template/test/pkg/ScrollingActivity.java
+++ android-templates/testData/golden/testNewScrollingActivity/Template test module/src/main/java/template/test/pkg/ScrollingActivity.java
@@ -37,7 +37,8 @@ public class ScrollingActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                        .setAction("Action", null)
+                        .setAnchorView(R.id.fab).show();
             }
         });
     }
