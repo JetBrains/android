a2d294243b68f957dc171c92eda526596bc51257
Alon Albert <aalbert@google.com>
2023-09-22 16:51:59 -0700

    Convert StreamingScrollbar To Kotlin
    The goal is to make StreamingScrollbar work with any `Timeline`, not
    just `StreamingTimeline`.
    
    Since I'm going to rename it `TimelineScrollbar` in a subsequent cl, I
    might as well convert to Kotlin.
    
    Bug: NA
    Test: Existing
    Change-Id: I6de57fcf8122e5460bb2ce1edf6422bab81462e0
    
    GitOrigin-RevId: 1e382d1f6c8d12877d27898bbda65c9311aeca46

--- adt-ui/src/main/java/com/android/tools/adtui/stdui/StreamingScrollbar.kt
+++ adt-ui/src/main/java/com/android/tools/adtui/stdui/StreamingScrollbar.kt
@@ -13,165 +13,146 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.android.tools.adtui.stdui;
+package com.android.tools.adtui.stdui
 
-import static com.android.tools.adtui.stdui.StandardColors.DEFAULT_CONTENT_BACKGROUND_COLOR;
+import com.android.tools.adtui.RangeScrollBarUI
+import com.android.tools.adtui.common.AdtUiUtils.isActionKeyDown
+import com.android.tools.adtui.model.AspectObserver
+import com.android.tools.adtui.model.Range.Aspect.RANGE
+import com.android.tools.adtui.model.StreamingTimeline
+import com.google.common.annotations.VisibleForTesting
+import com.intellij.ui.components.JBScrollBar
+import java.awt.Graphics
+import java.awt.Rectangle
+import java.util.concurrent.TimeUnit
+import javax.swing.JComponent
+import kotlin.math.max
+import kotlin.math.min
 
-import com.android.tools.adtui.RangeScrollBarUI;
-import com.android.tools.adtui.common.AdtUiUtils;
-import com.android.tools.adtui.model.AspectObserver;
-import com.android.tools.adtui.model.Range;
-import com.android.tools.adtui.model.StreamingTimeline;
-import com.google.common.annotations.VisibleForTesting;
-import com.intellij.ui.components.JBScrollBar;
-import java.awt.Graphics;
-import java.awt.Rectangle;
-import java.util.concurrent.TimeUnit;
-import javax.swing.BoundedRangeModel;
-import javax.swing.JComponent;
-import org.jetbrains.annotations.NotNull;
+/** The percentage of the current view range's length to zoom per mouse wheel click. */
+private const val VIEW_ZOOM_PER_MOUSE_WHEEL_FACTOR = 0.125f
+
+/** The percentage of the current view range's length to pan per mouse wheel click. */
+private const val VIEW_PAN_PERCENTAGE_PER_MOUSE_WHEEL_FACTOR = 0.005f
 
 /**
- * A custom toolbar that synchronizes with the data+view ranges from the {@link StreamingTimeline}.
- * This control sets the timeline into streaming mode if users drags the thumb all the way to the right.
+ * Work in ms to keep things compatible with scrollbar's integer api. This should cover a long
+ * enough time period for us in terms of profiling.
  */
-public final class StreamingScrollbar extends JBScrollBar {
-  /**
-   * The percentage of the current view range's length to zoom per mouse wheel click.
-   */
-  private static final float VIEW_ZOOM_PER_MOUSEWHEEL_FACTOR = 0.125f;
+private val MS_TO_US = TimeUnit.MILLISECONDS.toMicros(1)
 
-  /**
-   * The percentage of the current view range's length to pan per mouse wheel click.
-   */
-  private static final float VIEW_PAN_PERCENTAGE_PER_MOUSEHWEEL_FACTOR = 0.005f;
+/** Pixel threshold to switch [.myTimeline] to streaming mode. */
+private const val STREAMING_POSITION_THRESHOLD_PX = 10f
 
-  /**
-   * Work in ms to keep things compatible with scrollbar's integer api.
-   * This should cover a long enough time period for us in terms of profiling.
-   */
-  private static final long MS_TO_US = TimeUnit.MILLISECONDS.toMicros(1);
+/**
+ * A custom toolbar that synchronizes with the data+view ranges from the [StreamingTimeline]. This
+ * control sets the timeline into streaming mode if users drags the thumb all the way to the right.
+ */
+class StreamingScrollbar(val timeline: StreamingTimeline, zoomPanComponent: JComponent) :
+  JBScrollBar(HORIZONTAL) {
 
-  /**
-   * Pixel threshold to switch {@link #myTimeline} to streaming mode.
-   */
-  private static final float STREAMING_POSITION_THRESHOLD_PX = 10;
+  private val aspectObserver = AspectObserver()
+  private var updating = false
+  private var checkStream = false
 
-  @NotNull private final StreamingTimeline myTimeline;
-  private final AspectObserver myAspectObserver;
-
-  private boolean myUpdating;
-  private boolean myCheckStream;
-
-  public StreamingScrollbar(@NotNull StreamingTimeline timeline,
-                            @NotNull JComponent zoomPanComponent) {
-    super(HORIZONTAL);
-
-    myAspectObserver = new AspectObserver();
-    myTimeline = timeline;
-    myTimeline.getViewRange().addDependency(myAspectObserver).onChange(Range.Aspect.RANGE, this::modelChanged);
-    myTimeline.getDataRange().addDependency(myAspectObserver).onChange(Range.Aspect.RANGE, this::modelChanged);
-
-    StreamingScrollbarUi scrollbarUi = new StreamingScrollbarUi();
-    setUI(scrollbarUi);
-    addPropertyChangeListener(evt -> {
-      // preserve RangeScrollbarUI always, otherwise it reverts back to the default UI when switching themes.
-      if (evt.getPropertyName().equals("UI") && !(evt.getNewValue() instanceof RangeScrollBarUI)) {
-        setUI(scrollbarUi);
-      }
-    });
-
-    addAdjustmentListener(e -> {
-      if (!myUpdating) {
-        updateModel();
-        if (!e.getValueIsAdjusting()) {
-          myCheckStream = true;
-        }
-      }
-    });
-    zoomPanComponent.addMouseWheelListener(e -> {
-      double count = e.getPreciseWheelRotation();
-      boolean isMenuKeyDown = AdtUiUtils.isActionKeyDown(e);
-      if (isMenuKeyDown) {
-        double anchor = ((float)e.getX() / e.getComponent().getWidth());
-        myTimeline.zoom(getZoomWheelDelta() * count, anchor);
-      }
-      else if (isScrollable()) {
-        myTimeline.panView(getPanWheelDelta() * count);
-      }
-      myCheckStream = count > 0;
-    });
-
-    // Ensure the scrollbar is set to the correct initial state.
-    modelChanged();
-  }
-
-  @VisibleForTesting
-  public double getZoomWheelDelta() {
-    return myTimeline.getViewRange().getLength() * VIEW_ZOOM_PER_MOUSEWHEEL_FACTOR;
-  }
-
-  @VisibleForTesting
-  public double getPanWheelDelta() {
-    return myTimeline.getViewRange().getLength() * VIEW_PAN_PERCENTAGE_PER_MOUSEHWEEL_FACTOR;
-  }
-
-  private void modelChanged() {
-    myUpdating = true;
-    Range dataRangeUs = myTimeline.getDataRange();
-    Range viewRangeUs = myTimeline.getViewRange();
-    int dataExtentMs = (int)((dataRangeUs.getLength()) / MS_TO_US);
-    int viewExtentMs = Math.min(dataExtentMs, (int)(viewRangeUs.getLength() / MS_TO_US));
-    int viewRelativeMinMs = Math.max(0, (int)((viewRangeUs.getMin() - dataRangeUs.getMin()) / MS_TO_US));
-
-    setValues(viewRelativeMinMs, viewExtentMs, 0, dataExtentMs);
-    setBlockIncrement(viewExtentMs);
-    myUpdating = false;
-  }
-
-  private void updateModel() {
-    myTimeline.setStreaming(false);
-    Range dataRangeUs = myTimeline.getDataRange();
-    Range viewRangeUs = myTimeline.getViewRange();
-    int valueMs = getValue();
-    int viewRelativeMinMs = Math.max(0, (int)((viewRangeUs.getMin() - dataRangeUs.getMin()) / MS_TO_US));
-    double deltaUs = (valueMs - viewRelativeMinMs) * MS_TO_US;
-    viewRangeUs.shift(deltaUs);
-  }
-
-  @Override
-  protected void paintComponent(Graphics g) {
-    super.paintComponent(g);
-
-    // Change back to streaming mode as needed
-    // Note: isCloseToMax() checks for pixel proximity which relies on the scrollbar's dimension, which is why this code snippet
-    // is here instead of animate/postAnimate - we wouldn't get the most current size in those places.
-    if (myCheckStream) {
-      if (!myTimeline.isStreaming() && isCloseToMax() && myTimeline.canStream()) {
-        myTimeline.setStreaming(true);
+  init {
+    val onChange = { modelChanged() }
+    timeline.viewRange.addDependency(aspectObserver).onChange(RANGE, onChange)
+    timeline.dataRange.addDependency(aspectObserver).onChange(RANGE, onChange)
+    val scrollbarUi = StreamingScrollbarUi()
+    setUI(scrollbarUi)
+    addPropertyChangeListener {
+      // preserve RangeScrollbarUI always, otherwise it reverts back to the default UI when
+      // switching themes.
+      if (it.propertyName == "UI" && it.newValue !is RangeScrollBarUI) {
+        setUI(scrollbarUi)
       }
     }
-    myCheckStream = false;
+    addAdjustmentListener {
+      if (!updating) {
+        updateModel()
+        if (!it.valueIsAdjusting) {
+          checkStream = true
+        }
+      }
+    }
+    zoomPanComponent.addMouseWheelListener {
+      val count = it.preciseWheelRotation
+      val isMenuKeyDown = isActionKeyDown(it)
+      if (isMenuKeyDown) {
+        val anchor = (it.x.toFloat() / it.component.width).toDouble()
+        timeline.zoom(getZoomWheelDelta() * count, anchor)
+      } else if (isScrollable()) {
+        timeline.panView(getPanWheelDelta() * count)
+      }
+      checkStream = count > 0
+    }
+
+    // Ensure the scrollbar is set to the correct initial state.
+    modelChanged()
   }
 
   @VisibleForTesting
-  public boolean isScrollable() {
-    Range viewRange = myTimeline.getViewRange();
-    Range dataRange = myTimeline.getDataRange();
-    return viewRange.getMin() >= dataRange.getMin() && viewRange.getMax() <= dataRange.getMax();
+  fun getZoomWheelDelta() = timeline.viewRange.length * VIEW_ZOOM_PER_MOUSE_WHEEL_FACTOR
+
+  @VisibleForTesting
+  fun getPanWheelDelta() = timeline.viewRange.length * VIEW_PAN_PERCENTAGE_PER_MOUSE_WHEEL_FACTOR
+
+  private fun modelChanged() {
+    updating = true
+    val dataRangeUs = timeline.dataRange
+    val viewRangeUs = timeline.viewRange
+    val dataExtentMs = (dataRangeUs.length / MS_TO_US).toInt()
+    val viewExtentMs = min(dataExtentMs, (viewRangeUs.length / MS_TO_US).toInt())
+    val viewRelativeMinMs = max(0, ((viewRangeUs.min - dataRangeUs.min) / MS_TO_US).toInt())
+    setValues(viewRelativeMinMs, viewExtentMs, 0, dataExtentMs)
+    setBlockIncrement(viewExtentMs)
+    updating = false
   }
 
-  private boolean isCloseToMax() {
-    BoundedRangeModel model = getModel();
-    float snapPercentage = 1 - (STREAMING_POSITION_THRESHOLD_PX / (float)getWidth());
-    return (model.getValue() + model.getExtent()) / (float)model.getMaximum() >= snapPercentage;
+  private fun updateModel() {
+    timeline.setStreaming(false)
+    val dataRangeUs = timeline.dataRange
+    val viewRangeUs = timeline.viewRange
+    val valueMs = value
+    val viewRelativeMinMs = max(0, ((viewRangeUs.min - dataRangeUs.min) / MS_TO_US).toInt())
+    val deltaUs = ((valueMs - viewRelativeMinMs) * MS_TO_US).toDouble()
+    viewRangeUs.shift(deltaUs)
   }
 
-  private class StreamingScrollbarUi extends RangeScrollBarUI {
-    @Override
-    protected void doPaintTrack(Graphics g, JComponent c, Rectangle bounds) {
-      g.setColor(DEFAULT_CONTENT_BACKGROUND_COLOR);
-      g.fillRect(bounds.x, bounds.y, bounds.width, bounds.height);
+  public override fun paintComponent(g: Graphics) {
+    super.paintComponent(g)
+
+    // Change back to streaming mode as needed
+    // Note: isCloseToMax() checks for pixel proximity which relies on the scrollbar's dimension,
+    // which is why this code snippet
+    // is here instead of animate/postAnimate - we wouldn't get the most current size in those
+    // places.
+    if (checkStream) {
+      if (!timeline.isStreaming && isCloseToMax() && timeline.canStream()) {
+        timeline.setStreaming(true)
+      }
+    }
+    checkStream = false
+  }
+
+  @VisibleForTesting
+  fun isScrollable(): Boolean {
+    val viewRange = timeline.viewRange
+    val dataRange = timeline.dataRange
+    return viewRange.min >= dataRange.min && viewRange.max <= dataRange.max
+  }
+
+  private fun isCloseToMax(): Boolean {
+    val model = getModel()
+    val snapPercentage = 1 - STREAMING_POSITION_THRESHOLD_PX / width.toFloat()
+    return (model.value + model.extent) / model.maximum.toFloat() >= snapPercentage
+  }
+
+  private inner class StreamingScrollbarUi : RangeScrollBarUI() {
+    override fun doPaintTrack(g: Graphics, c: JComponent, bounds: Rectangle) {
+      g.color = StandardColors.DEFAULT_CONTENT_BACKGROUND_COLOR
+      g.fillRect(bounds.x, bounds.y, bounds.width, bounds.height)
     }
   }
 }
