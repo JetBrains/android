d6b8ca65a4487c1adc90597904469b6c1c6e87ce
Yuriy Artamonov <yuriy.artamonov@jetbrains.com>
2023-09-24 16:20:02 +0200

    [android] IDEA-333195 Android: excessive memory allocation from AndroidWearRunMarkerContributor
    GitOrigin-RevId: 82feae0c67b7083e43b139e76b9c04a036224285

--- android/src/com/android/tools/idea/run/configuration/AndroidWearRunMarkerContributor.kt
+++ android/src/com/android/tools/idea/run/configuration/AndroidWearRunMarkerContributor.kt
@@ -24,6 +24,7 @@ import com.intellij.psi.JavaTokenType
 import com.intellij.psi.PsiElement
 import com.intellij.psi.tree.java.IJavaElementType
 import org.jetbrains.android.util.AndroidBundle
+import org.jetbrains.android.util.AndroidUtils
 import org.jetbrains.kotlin.lexer.KtToken
 import org.jetbrains.kotlin.lexer.KtTokens
 
@@ -39,6 +40,8 @@ class AndroidWearRunMarkerContributor : RunLineMarkerContributor() {
         && !(elementType is IJavaElementType && elementType == JavaTokenType.CLASS_KEYWORD)) {
       return null
     }
+
+    if (!AndroidUtils.hasAndroidFacets(e.project)) return null
 
     val psiClass = e.getPsiClass() ?: return null
     if (psiClass.isValidWatchFaceService() || psiClass.isValidTileService() || psiClass.isValidComplicationService()) {
