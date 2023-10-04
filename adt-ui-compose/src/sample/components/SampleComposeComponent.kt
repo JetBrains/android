72f79cf96e4fde5b8df66c6400b690ad961ca58a
Yasser Dbeis <ydbeis@google.com>
2023-10-04 01:48:14 -0700

    Add compose tests to adt-ui-compose module
    Added the dependenices for testing compose specifically and general
    testing to the adt-ui-module. This allowed for the addition of a sample
    composable with a corresponding test to show how one can test desktop
    compose composable content and behavior.
    
    Due to a bug (github.com/JetBrains/compose-multiplatform/issues/3243)
    in skiko causing the instantiation of the ComposeContentTestRule to
    crash on Windows, this test class must be ignored on windows (by
    configuring the BUILD file) in order to pass pre-submit.
    
    Bug: 272822954
    Test: added
    Change-Id: Ibc37d8b9c69711ef66e362c2ff5a60e212d84639
    
    GitOrigin-RevId: 8d58c60ac03f68cd5511eb9fb2a04e8983f27100

---
+++ adt-ui-compose/src/sample/components/SampleComposeComponent.kt
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package sample.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.window.singleWindowApplication
+import org.jetbrains.jewel.DefaultButton
+import org.jetbrains.jewel.Text
+import org.jetbrains.jewel.intui.standalone.IntUiTheme
+
+/**
+ * This composable is used by the SampleComposeComponentTest to show how we can test the content and behavior of compose components.
+ * For theming and swing compatibility, we hardcode the values for simplicity.
+ */
+@Composable
+fun SampleComposeComponent() {
+  var displayText by remember { mutableStateOf(false) }
+  IntUiTheme(IntUiTheme.darkThemeDefinition(), true) {
+    Column {
+      DefaultButton(onClick = { displayText = !displayText }) {
+        Text("Hello Compose")
+      }
+      if (displayText) {
+        Text("Displayed Text", color = Color.Red)
+      }
+    }
+  }
+}
+
+fun main() {
+  singleWindowApplication {
+    SampleComposeComponent()
+  }
+}
