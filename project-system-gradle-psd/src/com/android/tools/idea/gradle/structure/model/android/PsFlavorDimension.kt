058d69d8b5340b0f4fdffd22743b9a8270991a92
Christophe Rhodes <xof@google.com>
2023-10-05 15:04:03 +0100

    [PSD] Treat invalid flavor dimensions as equal
    At least if they are children of the same PsAndroidModule.  This
    ensures that we do not delete and recreate the "(invalid)" node in the
    Product Flavor tab of the PSD when we refresh after resolved models
    are available.
    
    Bug: 264013609
    Test: manual
    Change-Id: Ibc25447edf3951d00b47a0f4d42ae6d6f3842226
    
    GitOrigin-RevId: f52f2ff99c6c4fae47280d90763590e0bff7a23f

--- project-system-gradle-psd/src/com/android/tools/idea/gradle/structure/model/android/PsFlavorDimension.kt
+++ project-system-gradle-psd/src/com/android/tools/idea/gradle/structure/model/android/PsFlavorDimension.kt
@@ -19,6 +19,7 @@ import com.android.tools.idea.gradle.structure.model.PsChildModel
 import com.android.tools.idea.gradle.structure.model.PsPath
 import com.android.tools.idea.gradle.structure.navigation.PsFlavorDimensionNavigationPath
 import icons.StudioIcons.Misc.PRODUCT_FLAVOR_DIMENSION
+import java.util.Objects
 import javax.swing.Icon
 
 class PsFlavorDimension(
@@ -35,4 +36,10 @@ class PsFlavorDimension(
   override val path: PsFlavorDimensionNavigationPath get() = PsFlavorDimensionNavigationPath(parent.path.productFlavorsPath, name)
   override val isDeclared: Boolean get() = parsedName != null
   override val icon: Icon = PRODUCT_FLAVOR_DIMENSION
+
+  override fun equals(other: Any?) = when(other) {
+    is PsFlavorDimension -> if (this.isInvalid && other.isInvalid) this.parent == other.parent else super.equals(other)
+    else -> false
+  }
+  override fun hashCode() = if (isInvalid) Objects.hash(42, parent) else super.hashCode()
 }
