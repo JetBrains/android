4066cee18b694dbe55c83f99a8ea6d130652d477
Gianni <gmod@google.com>
2023-10-06 16:26:24 +0100

    Shows alpha colors in resource manager
    Resource manager didn't parse the alpha color when changing colors
    in the colors.xml file. The issue was caused by the color parser having
    the alpha disabled.
    
    A customised Color.toHex function needed to be implemented as the format
    of java.awt.Color is #RGBA but we needed to show #ARGB as we define
    in our resources.
    
    Fixes: 278844165
    Test: AssetDataProviderTest
    Change-Id: I19fb881a22e52ccb23f4cadcbf32fe5ccb28ab18
    
    GitOrigin-RevId: 7401bef9200b279a66507b7f0c2fa629cd8b8ac5

--- android/ui.resourcemanager/testSrc/com/android/tools/idea/ui/resourcemanager/rendering/AssetDataProviderTest.kt
+++ android/ui.resourcemanager/testSrc/com/android/tools/idea/ui/resourcemanager/rendering/AssetDataProviderTest.kt
@@ -81,7 +81,7 @@ class AssetDataProviderTest {
   fun testColorDataProvider() {
     val resName = "primary_color"
     val fileName = "colors.xml"
-    val resValue = "#012345"
+    val resValue = "#FF012345"
 
     val project = Mockito.mock(Project::class.java)
     val resourceResolver = Mockito.mock(ResourceResolver::class.java)
