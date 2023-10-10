b01d48fb5073e917788a9099e5879241c68c1aec
Yasser Dbeis <ydbeis@google.com>
2023-10-10 13:00:18 -0700

    Add TaskGridModel as the UI model for the grid of tasks
    This class serves as the model for the grid/list of Profiler tasks
    a user can select from. Each task is represented via a TaskGridItem
    which shows the name and icon of the respective task.
    
    Bug: 277798754
    Test: none
    Change-Id: I6a7536c545a0947b1e1f10e7cd6d05ed98981457
    
    GitOrigin-RevId: 7be1f2061b6733d7dbaacb2a428b6a83139ee947

---
+++ profilers/src/com/android/tools/profilers/taskbased/tasks/TaskListModel.kt
@@ -0,0 +1,80 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.profilers.taskbased.tasks
+
+import com.android.tools.profilers.tasks.ProfilerTaskType
+import icons.StudioIcons
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+/**
+ * This class serves as the model for the list/grid of Profiler tasks a user can select from. Each task is represented via a TaskGridItem
+ * which shows the name and icon of the respective task.
+ */
+class TaskGridModel {
+  val tasks: List<TaskGridItem>
+  private val _selectedTaskType = MutableStateFlow(ProfilerTaskType.UNSPECIFIED)
+  val selectedTaskType = _selectedTaskType.asStateFlow()
+
+  fun getSelectedTaskType() = selectedTaskType.value
+
+  fun onTaskSelection(taskType: ProfilerTaskType) {
+    _selectedTaskType.value = taskType
+  }
+
+  fun resetTaskSelection() {
+    onTaskSelection(ProfilerTaskType.UNSPECIFIED)
+  }
+
+  fun getTaskGridItem(taskType: ProfilerTaskType): TaskGridItem? = tasks.firstOrNull { it.type == taskType }
+
+  init {
+    tasks = listOf(
+      TaskGridItem(
+        type = ProfilerTaskType.CALLSTACK_SAMPLE,
+        icon = StudioIcons.Profiler.Sessions.CPU,
+      ),
+      TaskGridItem(
+        type = ProfilerTaskType.SYSTEM_TRACE,
+        icon = StudioIcons.Profiler.Sessions.CPU,
+      ),
+      TaskGridItem(
+        type = ProfilerTaskType.JAVA_KOTLIN_METHOD_TRACE,
+        icon = StudioIcons.Profiler.Sessions.CPU,
+      ),
+      TaskGridItem(
+        type = ProfilerTaskType.JAVA_KOTLIN_METHOD_SAMPLE,
+        icon = StudioIcons.Profiler.Sessions.CPU,
+      ),
+      TaskGridItem(
+        type = ProfilerTaskType.HEAP_DUMP,
+        icon = StudioIcons.Profiler.Sessions.HEAP,
+      ),
+      TaskGridItem(
+        type = ProfilerTaskType.NATIVE_ALLOCATIONS,
+        icon = StudioIcons.Profiler.Sessions.ALLOCATIONS,
+      ),
+      TaskGridItem(
+        type = ProfilerTaskType.JAVA_KOTLIN_ALLOCATIONS,
+        icon = StudioIcons.Profiler.Sessions.ALLOCATIONS,
+      ),
+    )
+  }
+
+  companion object {
+    val DISABLED_TASK_ICON = StudioIcons.Profiler.Sidebar.ISSUE
+  }
+}
