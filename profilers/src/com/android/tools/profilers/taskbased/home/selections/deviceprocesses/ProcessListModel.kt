ee249363d1701a396a87e25e0f2adab405a3e7c8
Yasser Dbeis <ydbeis@google.com>
2023-10-09 22:08:26 -0700

    Add ProcessListModel for device process list in Task-Based UX
    ...to manage the state of the device processes (combination of
    device + process) to render in the Compose-based device process
    UI.
    
    Bug: 277798461
    Test: added
    Change-Id: Ib8ebd95225daee41922abca4a6a660c3a4b955f1
    
    GitOrigin-RevId: 67839f94abbdf686b90b385f135d1cf1d90d5dd3

---
+++ profilers/src/com/android/tools/profilers/taskbased/home/selections/deviceprocesses/ProcessListModel.kt
@@ -0,0 +1,103 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.profilers.taskbased.home.selections.deviceprocesses
+
+import com.android.tools.adtui.model.AspectObserver
+import com.android.tools.profiler.proto.Common
+import com.android.tools.profilers.ProfilerAspect
+import com.android.tools.profilers.StudioProfilers
+import com.android.tools.profilers.SupportLevel
+import com.google.common.annotations.VisibleForTesting
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class ProcessListModel(val profilers: StudioProfilers) : AspectObserver() {
+  private val _deviceProcessList = MutableStateFlow(listOf<DeviceProcess>())
+  val deviceProcessList = _deviceProcessList.asStateFlow()
+
+  private val _selectedDeviceProcess = MutableStateFlow(
+    DeviceProcess(Common.Device.getDefaultInstance(), Common.Process.getDefaultInstance()))
+  val selectedDeviceProcess = _selectedDeviceProcess.asStateFlow()
+
+  private var preferredProcessName: String? = null
+
+  data class DeviceProcess(
+    val device: Common.Device,
+    val process: Common.Process
+  )
+
+  init {
+    profilers.addDependency(this)
+      .onChange(ProfilerAspect.PROCESSES) { deviceProcessListUpdated() }
+      .onChange(ProfilerAspect.PREFERRED_PROCESS) { preferredProcessUpdated() }
+  }
+
+  private fun deviceProcessListUpdated() {
+    val newDeviceProcessList = mutableListOf<DeviceProcess>()
+    profilers.deviceProcessMap.map { (device, deviceProcesses) ->
+      if (device.state != Common.Device.State.ONLINE) {
+        return@map
+      }
+      for (process in deviceProcesses) {
+        if (process.state == Common.Process.State.ALIVE) {
+          newDeviceProcessList.add(DeviceProcess(device = device, process = process))
+        }
+      }
+    }
+
+    _deviceProcessList.value = newDeviceProcessList
+    reorderProcessList()
+  }
+
+  /**
+   * Reorders the device process list by process name in increasing lexicographic order. One exception to this sort/order is that the
+   * preferred process will always be the first entry in the list.
+   */
+  private fun reorderProcessList() {
+    val reorderedProcessList = mutableListOf<DeviceProcess>()
+    reorderedProcessList.addAll(_deviceProcessList.value)
+    reorderedProcessList.sortBy { it.process.name }
+
+    // Move the preferred process, if it exists, to the front of the device process list.
+    if (!preferredProcessName.isNullOrBlank()){
+      val preferredProcessIdx = reorderedProcessList.indexOfFirst { it.process.name ==  preferredProcessName}
+      if (preferredProcessIdx != -1) {
+        val preferredProcess = reorderedProcessList.removeAt(preferredProcessIdx)
+        reorderedProcessList.add(0, preferredProcess)
+      }
+    }
+
+    _deviceProcessList.value = reorderedProcessList
+  }
+
+  fun onDeviceProcessSelection(newDeviceProcess: DeviceProcess) {
+    _selectedDeviceProcess.value = newDeviceProcess
+  }
+
+  private fun preferredProcessUpdated() {
+    preferredProcessName = profilers.preferredProcessName
+    // Force update of process list ordering.
+    reorderProcessList()
+  }
+
+  @VisibleForTesting
+  fun getPreferredProcessName() = preferredProcessName
+
+  companion object {
+    // Utility function to determine if selected process is profileable or not.
+    fun Common.Process.isProfileable() = SupportLevel.of(this.exposureLevel) == SupportLevel.PROFILEABLE
+  }
+}
---
+++ profilers/testSrc/com/android/tools/profilers/taskbased/selections/deviceprocesses/ProcessListModelTest.kt
@@ -0,0 +1,131 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.profilers.taskbased.selections.deviceprocesses
+
+import com.android.tools.adtui.model.FakeTimer
+import com.android.tools.idea.transport.faketransport.FakeGrpcChannel
+import com.android.tools.idea.transport.faketransport.FakeTransportService
+import com.android.tools.profiler.proto.Common
+import com.android.tools.profiler.proto.Common.Process.ExposureLevel
+import com.android.tools.profilers.FakeIdeProfilerServices
+import com.android.tools.profilers.ProfilerClient
+import com.android.tools.profilers.StudioProfilers
+import com.android.tools.profilers.event.FakeEventService
+import com.android.tools.profilers.sessions.SessionsManager
+import com.android.tools.profilers.taskbased.home.selections.deviceprocesses.ProcessListModel
+import com.google.common.truth.Truth.assertThat
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+class ProcessListModelTest {
+  private val myTimer = FakeTimer()
+  private val myTransportService = FakeTransportService(myTimer, false)
+
+  @get:Rule
+  var myGrpcChannel = FakeGrpcChannel("ProcessListModelTestChannel", myTransportService, FakeEventService())
+
+  private lateinit var myProfilers: StudioProfilers
+  private lateinit var myManager: SessionsManager
+  private lateinit var ideProfilerServices: FakeIdeProfilerServices
+  private lateinit var processListModel: ProcessListModel
+
+  @Before
+  fun setup() {
+    ideProfilerServices = FakeIdeProfilerServices()
+    myProfilers = StudioProfilers(
+      ProfilerClient(myGrpcChannel.channel),
+      ideProfilerServices,
+      myTimer
+    )
+    myManager = myProfilers.sessionsManager
+    processListModel = ProcessListModel(myProfilers)
+    ideProfilerServices.enableTaskBasedUx(true)
+  }
+
+  @Test
+  fun `online devices with alive processes show in device process list`() {
+    assertThat(processListModel.deviceProcessList.value).isEmpty()
+    addDeviceWithProcess("FakeDevice1", Common.Device.State.ONLINE, 20, "FakeProcess1", Common.Process.State.ALIVE)
+    addDeviceWithProcess("FakeDevice2", Common.Device.State.ONLINE, 40, "FakeProcess2", Common.Process.State.ALIVE)
+    assertThat(processListModel.deviceProcessList.value).isNotEmpty()
+    assertThat(processListModel.deviceProcessList.value.size).isEqualTo(2)
+  }
+
+  @Test
+  fun `offline devices with alive processes do not show in device process list`() {
+    assertThat(processListModel.deviceProcessList.value).isEmpty()
+    addDeviceWithProcess("FakeDevice1", Common.Device.State.OFFLINE, 20, "FakeProcess1", Common.Process.State.ALIVE)
+    addDeviceWithProcess("FakeDevice2", Common.Device.State.OFFLINE, 40, "FakeProcess2", Common.Process.State.ALIVE)
+    assertThat(processListModel.deviceProcessList.value).isEmpty()
+  }
+
+  @Test
+  fun `online devices with dead processes do not show in device process list`() {
+    assertThat(processListModel.deviceProcessList.value).isEmpty()
+    addDeviceWithProcess("FakeDevice1", Common.Device.State.ONLINE, 20, "FakeProcess1", Common.Process.State.DEAD)
+    addDeviceWithProcess("FakeDevice2", Common.Device.State.ONLINE, 40, "FakeProcess2", Common.Process.State.DEAD)
+    assertThat(processListModel.deviceProcessList.value).isEmpty()
+  }
+
+  @Test
+  fun `test preferred process selection is at the top of device process list`() {
+    assertThat(processListModel.deviceProcessList.value).isEmpty()
+
+    addDeviceWithProcess("FakeDevice1", Common.Device.State.ONLINE, 20, "FakeProcess1", Common.Process.State.ALIVE)
+    // PREFERRED_PROCESS aspect should be fired via the call to set the preferred process.
+    myProfilers.setPreferredProcess("FakeProcess1", "FakeProcess1", null)
+    myTimer.tick(FakeTimer.ONE_SECOND_IN_NS)
+
+    assertThat(processListModel.getPreferredProcessName()).isEqualTo("FakeProcess1")
+
+    addDeviceWithProcess("FakeDevice2", Common.Device.State.ONLINE, 40, "FakeProcess2", Common.Process.State.ALIVE)
+
+    // PREFERRED_PROCESS aspect should be fired via the call to set the preferred process.
+    myProfilers.setPreferredProcess("FakeProcess2", "FakeProcess2", null)
+    myTimer.tick(FakeTimer.ONE_SECOND_IN_NS)
+
+    assertThat(processListModel.getPreferredProcessName()).isEqualTo("FakeProcess2")
+
+    assertThat(processListModel.deviceProcessList.value).isNotEmpty()
+    assertThat(processListModel.deviceProcessList.value.size).isEqualTo(2)
+    // Make sure that despite being lexicographically greater than the "FakeProcess1", because it is the preferred process, "FakeProcess2"
+    // is the first device process listed.
+    assertThat(processListModel.deviceProcessList.value.first().process.name).isEqualTo("FakeProcess2")
+  }
+
+  private fun addDeviceWithProcess(deviceName: String,
+                                   deviceState: Common.Device.State,
+                                   processId: Int,
+                                   processName: String,
+                                   processState: Common.Process.State) {
+
+    val device = createDevice(deviceName, deviceState)
+    val process = createProcess(processId, processName, processState, device.deviceId)
+    myTransportService.addDevice(device)
+    myTransportService.addProcess(device, process)
+    myTimer.tick(FakeTimer.ONE_SECOND_IN_NS)
+  }
+
+  private fun createDevice(deviceName: String, deviceState: Common.Device.State) = Common.Device.newBuilder().setDeviceId(
+    deviceName.hashCode().toLong()).setSerial(deviceName).setState(deviceState).build()
+
+  private fun createProcess(pid: Int,
+                            processName: String,
+                            processState: Common.Process.State,
+                            deviceId: Long) = Common.Process.newBuilder().setDeviceId(deviceId).setPid(pid).setName(processName).setState(
+    processState).setExposureLevel(ExposureLevel.DEBUGGABLE).build()
+}
