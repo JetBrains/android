0983692278144fcb347938feb0e4f8bba711c625
Ivan Gavrilovic <gavra@google.com>
2023-10-12 10:58:02 +0100

    Re-enable AndroidJavaDocWithGradleTest on windows
    Bug: 228880357
    Test: updating it
    Change-Id: I15227063b9307ffb5fa993577a874a2bede8e32f
    
    GitOrigin-RevId: 00379ba6257bdc29ab4b94c72e3aa2108eea8398

--- android/agpIntegrationTestSrc/com/android/tools/idea/javadoc/AndroidJavaDocWithGradleTest.java
+++ android/agpIntegrationTestSrc/com/android/tools/idea/javadoc/AndroidJavaDocWithGradleTest.java
@@ -28,7 +28,6 @@ import com.intellij.codeInsight.documentation.DocumentationManager;
 import com.intellij.lang.documentation.DocumentationProvider;
 import com.intellij.openapi.project.ProjectUtil;
 import com.intellij.openapi.util.io.FileUtil;
-import com.intellij.openapi.util.SystemInfoRt;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiElement;
 import java.io.File;
@@ -58,9 +57,6 @@ public class AndroidJavaDocWithGradleTest extends AndroidGradleTestCase implemen
   }
 
   public void testResource() throws Exception {
-    if (SystemInfoRt.isWindows) {
-      return; // TODO(b/228880357) fix high failure rate on Windows
-    }
     loadProject(DEPENDENT_MODULES);
 
     checkJavadoc("/app/src/main/res/values/colors.xml"
