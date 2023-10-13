b92b98e8f640309917d583740eba962adf7ac48d
Christophe Rhodes <xof@google.com>
2023-10-13 16:01:35 +0100

    Use generic sync manager in DefaultStudioProgramRunner
    Bug: 305009551
    Test: existing tests adjusted
    Change-Id: Ic8b35bdae5c15da4281e29f4d723704777b4116f
    
    GitOrigin-RevId: 253e7f9e5f2c43c3b0dbda6c489f76509f2d5adb

--- android/run/testSrc/com/android/tools/idea/run/DefaultStudioProgramRunnerTest.java
+++ android/run/testSrc/com/android/tools/idea/run/DefaultStudioProgramRunnerTest.java
@@ -16,12 +16,13 @@
 package com.android.tools.idea.run;
 
 import com.android.tools.idea.execution.common.AndroidExecutionTarget;
-import com.android.tools.idea.gradle.project.sync.GradleSyncState;
+import com.android.tools.idea.projectsystem.ProjectSystemSyncManager;
+import com.google.common.util.concurrent.ListenableFuture;
 import com.intellij.execution.configurations.RunProfile;
 import com.intellij.execution.executors.DefaultRunExecutor;
 import com.intellij.testFramework.ProjectRule;
-import com.intellij.util.ThreeState;
 import java.util.Collections;
+import org.jetbrains.annotations.NotNull;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Rule;
@@ -36,56 +37,75 @@ public class DefaultStudioProgramRunnerTest {
   @Mock private AndroidExecutionTarget target;
   private AndroidRunConfiguration runConfig;
   @Mock private RunProfile runProfile;
-  @Mock private GradleSyncState syncState;
-
+  private ProjectSystemSyncManager syncManager;
+  public boolean isSyncInProgress = false;
+  public boolean isSyncNeeded = false;
   private DefaultStudioProgramRunner myRunner;
 
   @Before
   public void before() {
     MockitoAnnotations.initMocks(this);
+    syncManager = new ProjectSystemSyncManager() {
+      @NotNull
+      @Override
+      public SyncResult getLastSyncResult() {
+        throw(new IllegalStateException("getLastSyncResult unimplemented"));
+      }
+
+      @NotNull
+      @Override
+      public ListenableFuture<SyncResult> syncProject(@NotNull SyncReason reason) {
+        throw(new IllegalStateException("syncProject unimplemented"));
+      }
+
+      @Override
+      public boolean isSyncInProgress() {
+        return isSyncInProgress;
+      }
+
+      @Override
+      public boolean isSyncNeeded() {
+        return isSyncNeeded;
+      }
+    };
     runConfig = new AndroidRunConfiguration(myProjectRule.getProject(), AndroidRunConfigurationType.getInstance().getFactory());
   }
 
   @Before
   public void newDefaultStudioProgramRunner() {
-    myRunner = new DefaultStudioProgramRunner(project -> syncState, (project, profile) -> target);
+    myRunner = new DefaultStudioProgramRunner(project -> syncManager, (project, profile) -> target);
   }
 
   /**
-   * Checks if the program runner is disabled when gradle is or needs syncing.
+   * Checks if the program runner is disabled when the project is being synced or needs syncing.
    */
   @Test
-  public void gradleCanRun() {
+  public void canRun() {
     Mockito.when(target.getRunningDevices()).thenReturn(Collections.emptyList());
 
     // Check that the program runner doesn't support non-AndroidRunConfigurationBase profiles.
-    Mockito.when(syncState.isSyncInProgress()).thenReturn(false);
-    Mockito.when(syncState.isSyncNeeded()).thenReturn(ThreeState.NO);
+    isSyncInProgress = false;
+    isSyncNeeded = false;
     Assert.assertFalse(myRunner.canRun(DefaultRunExecutor.EXECUTOR_ID, runProfile));
 
     // Check that the program runner can run when Gradle is ready.
-    Mockito.when(syncState.isSyncInProgress()).thenReturn(false);
-    Mockito.when(syncState.isSyncNeeded()).thenReturn(ThreeState.NO);
+    isSyncInProgress = false;
+    isSyncNeeded = false;
     Assert.assertTrue(myRunner.canRun(DefaultRunExecutor.EXECUTOR_ID, runConfig));
 
     // Check that the program runner cannot run when Gradle is syncing.
-    Mockito.when(syncState.isSyncInProgress()).thenReturn(true);
-    Mockito.when(syncState.isSyncNeeded()).thenReturn(ThreeState.NO);
+    isSyncInProgress = true;
+    isSyncNeeded = false;
     Assert.assertFalse(myRunner.canRun(DefaultRunExecutor.EXECUTOR_ID, runConfig));
 
     // Check that the program runner cannot run when Gradle needs syncing.
-    Mockito.when(syncState.isSyncInProgress()).thenReturn(false);
-    Mockito.when(syncState.isSyncNeeded()).thenReturn(ThreeState.YES);
-    Assert.assertFalse(myRunner.canRun(DefaultRunExecutor.EXECUTOR_ID, runConfig));
-
-    // Check that the program runner cannot run when Gradle isn't sure if it needs to sync.
-    Mockito.when(syncState.isSyncInProgress()).thenReturn(false);
-    Mockito.when(syncState.isSyncNeeded()).thenReturn(ThreeState.UNSURE);
+    isSyncInProgress = false;
+    isSyncNeeded = true;
     Assert.assertFalse(myRunner.canRun(DefaultRunExecutor.EXECUTOR_ID, runConfig));
 
     // Check that the program runner cannot run when Gradle is completely out of whack.
-    Mockito.when(syncState.isSyncInProgress()).thenReturn(true);
-    Mockito.when(syncState.isSyncNeeded()).thenReturn(ThreeState.YES);
+    isSyncInProgress = true;
+    isSyncNeeded = true;
     Assert.assertFalse(myRunner.canRun(DefaultRunExecutor.EXECUTOR_ID, runConfig));
   }
 }
