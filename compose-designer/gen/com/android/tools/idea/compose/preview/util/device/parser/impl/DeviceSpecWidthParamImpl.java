e541965e8c7922690567f5b20068461e9c80fab7
Kseniia Grey <kseniia@google.com>
2023-10-13 13:58:15 +0100

    Delete unused imports in compose-designer
    Test: n/a
    Bug: n/a
    Change-Id: Id4fded7531b50bc7fd38bbdc7300b4022e52437a
    
    GitOrigin-RevId: 42fabf22659ea110c2843a4d777a60877f9557c9

--- compose-designer/gen/com/android/tools/idea/compose/preview/util/device/parser/impl/DeviceSpecWidthParamImpl.java
+++ compose-designer/gen/com/android/tools/idea/compose/preview/util/device/parser/impl/DeviceSpecWidthParamImpl.java
@@ -1,13 +1,9 @@
 // This is a generated file. Not intended for manual editing.
 package com.android.tools.idea.compose.preview.util.device.parser.impl;
 
-import java.util.List;
 import org.jetbrains.annotations.*;
 import com.intellij.lang.ASTNode;
-import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiElementVisitor;
-import com.intellij.psi.util.PsiTreeUtil;
-import static com.android.tools.idea.compose.preview.util.device.parser.DeviceSpecTypes.*;
 import com.android.tools.idea.compose.preview.util.device.parser.*;
 
 public class DeviceSpecWidthParamImpl extends DeviceSpecParamImpl implements DeviceSpecWidthParam {
