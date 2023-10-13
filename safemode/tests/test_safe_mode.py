ac2e188a5a83827e6af09b6659b599bbfeae1364
vangjush <vangjush@google.com>
2023-10-13 15:16:14 -0400

    Add safe mode scripts and unit test to safe mode module
    Test: N/A
    Bug: N/A
    Change-Id: Ie14ae534ddce0f3a0901bb53892de8f70141dd10
    
    GitOrigin-RevId: 4bf5e1f752cd2c0990bd9ea604c3ab477fcb0029

---
+++ safemode/tests/test_safe_mode.py
@@ -0,0 +1,63 @@
+import os
+import re
+import unittest
+import zipfile
+
+class SafeModeTests(unittest.TestCase):
+  """Performs basic tests on safe mode artifacts.
+  """
+
+  def test_jars_parity(self):
+    mac_jars = set()
+    linux_jars = set()
+    win_jars = set()
+    mac_safe_jars = set()
+    linux_safe_jars = set()
+    win_safe_jars = set()
+
+    file = zipfile.ZipFile("tools/adt/idea/studio/android-studio.linux.zip")
+    for f in file.namelist():
+      m = re.search("studio_safe.sh", f)
+      if m:
+        lines = open(file.extract(f), encoding="utf8").readlines()
+        for line in lines:
+          if line.lower().startswith("class_path"):
+            linux_safe_jars.add(line)
+      m = re.search("studio.sh", f)
+      if m:
+        lines = open(file.extract(f), encoding="utf8").readlines()
+        for line in lines:
+          if line.lower().startswith("class_path"):
+            linux_jars.add(line)
+            mac_jars.add(line)
+
+    file = zipfile.ZipFile("tools/adt/idea/studio/android-studio.win.zip")
+    for f in file.namelist():
+      m = re.search("studio_safe.bat", f)
+      if m:
+        lines = open(file.extract(f), encoding="utf8").readlines()
+        for line in lines:
+          if "class_path=" in line.lower():
+            win_safe_jars.add(line)
+      m = re.search("studio.bat", f)
+      if m:
+        lines = open(file.extract(f), encoding="utf8").readlines()
+        for line in lines:
+          if "class_path=" in line.lower():
+            win_jars.add(line)
+
+    file = zipfile.ZipFile("tools/adt/idea/studio/android-studio.mac.zip")
+    for f in file.namelist():
+      m = re.search("studio_safe.sh", f)
+      if m:
+        lines = open(file.extract(f), encoding="utf8").readlines()
+        for line in lines:
+          if line.lower().startswith("class_path"):
+            mac_safe_jars.add(line)
+    self.assertEqual(mac_jars, mac_safe_jars)
+    self.assertEqual(linux_jars, linux_safe_jars)
+    self.assertEqual(win_jars, win_safe_jars)
+
+
+if __name__ == "__main__":
+  unittest.main()
