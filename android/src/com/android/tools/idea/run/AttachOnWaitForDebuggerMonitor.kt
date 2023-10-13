8ae12725ab7f6108898d5a3a9a0097375313c89b
Christophe Rhodes <xof@google.com>
2023-10-13 16:01:06 +0100

    Use generic sync manager in waiting for debugger monitor
    Bug: 305009551
    Test: existing
    Change-Id: Ief4840fb6f9687e42fef1e00f78625f61e391880
    
    GitOrigin-RevId: 21d293ca7b7fa35ac652fa113b2aa8848a47e997

--- android/src/com/android/tools/idea/run/AttachOnWaitForDebuggerMonitor.kt
+++ android/src/com/android/tools/idea/run/AttachOnWaitForDebuggerMonitor.kt
@@ -24,7 +24,7 @@ import com.android.tools.idea.execution.common.debug.AndroidDebuggerState
 import com.android.tools.idea.execution.common.debug.utils.AndroidConnectDebugger
 import com.android.tools.idea.execution.common.processhandler.AndroidRemoteDebugProcessHandler
 import com.android.tools.idea.flags.StudioFlags
-import com.android.tools.idea.gradle.project.sync.GradleSyncState
+import com.android.tools.idea.projectsystem.getSyncManager
 import com.google.common.annotations.VisibleForTesting
 import com.intellij.execution.ExecutionManager
 import com.intellij.execution.RunManager
@@ -33,7 +33,6 @@ import com.intellij.execution.runners.ProgramRunner
 import com.intellij.openapi.Disposable
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.project.Project
-import com.intellij.util.ThreeState
 
 @Service(Service.Level.PROJECT)
 class AttachOnWaitForDebuggerMonitor(val host: DebuggerHost) : Disposable {
@@ -59,7 +58,7 @@ class AttachOnWaitForDebuggerMonitor(val host: DebuggerHost) : Disposable {
 
     open fun enabled(config: AndroidRunConfigurationBase, debugger: AndroidDebugger<out AndroidDebuggerState>): Boolean {
       return config.androidDebuggerContext.getAndroidDebuggerState<AndroidDebuggerState>(debugger.id)?.ATTACH_ON_WAIT_FOR_DEBUGGER == true &&
-             GradleSyncState.getInstance(project).isSyncNeeded() == ThreeState.NO
+             !project.getSyncManager().isSyncNeeded()
     }
 
     open fun canDebugRun(project: Project, config: AndroidRunConfigurationBase): Boolean {
