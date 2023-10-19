3b65e61ba2c1951fec021f35eb1f8f25746a402f
Juan C Nuno <juancnuno@google.com>
2023-10-19 13:59:46 -0700

    Add the tabs
    Bug: Not applicable
    Test: Not applicable
    Change-Id: I4e2a713a1ed2e953db5a1b38c59d6c917a04e0fd
    
    GitOrigin-RevId: f22cf93ec8c979a10085cbf9077370c52f227e00

--- intellij.android.device/src/com/android/tools/idea/adddevicedialog/compose/ConfigureDevicePanel.kt
+++ intellij.android.device/src/com/android/tools/idea/adddevicedialog/compose/ConfigureDevicePanel.kt
@@ -15,12 +15,39 @@
  */
 package com.android.tools.idea.adddevicedialog.compose
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import org.jetbrains.jewel.ExperimentalJewelApi
+import org.jetbrains.jewel.TabData
+import org.jetbrains.jewel.TabStrip
 import org.jetbrains.jewel.Text
 import org.jetbrains.jewel.bridge.SwingBridgeTheme
 
 @Composable
 internal fun ConfigureDevicePanel() {
-  @OptIn(ExperimentalJewelApi::class) SwingBridgeTheme { Text("Hello, Compose for Desktop!") }
+  @OptIn(ExperimentalJewelApi::class)
+  SwingBridgeTheme {
+    Column {
+      Text("Configure device")
+      Text("Add a device to device manager")
+      Tabs()
+    }
+  }
 }
+
+@Composable
+private fun Tabs() {
+  var selectedTab by remember { mutableStateOf(Tab.DEVICE_AND_API) }
+  TabStrip(Tab.values().map { tab -> TabData.Default(selectedTab == tab, tab.text, onClick = { selectedTab = tab }) })
+
+  when (selectedTab) {
+    Tab.DEVICE_AND_API -> DeviceAndApiPanel()
+    Tab.ADDITIONAL_SETTINGS -> AdditionalSettingsPanel()
+  }
+}
+
+private enum class Tab(val text: String) { DEVICE_AND_API("Device and API"), ADDITIONAL_SETTINGS("Additional settings") }
