29935ea97aac1bc6ddc780e5fdf42234f46f1ad0
Christophe Rhodes <xof@google.com>
2023-10-23 12:09:32 +0100

    Improve getId() in ExperimentalSettingsConfigurable
    The contract requires it to return the same value as is specified in
    the plugin.xml, which was changed by
    I28d0076ed743263e917d3fcaaa3c56b61fe12b51.
    
    This only fixes getId() for Android Studio; it is still wrong when we
    are running as the Android plugin within IntelliJ IDEA.
    
    Bug: 307237467
    Test: existing, manual
    Change-Id: I258eff5c41bb46df08b3e369e142d29aaf490e99
    
    GitOrigin-RevId: c54d4e1a5dff48808b8d419127bdee8d3a7b88ca

--- android/src/com/android/tools/idea/flags/ExperimentalSettingsConfigurable.java
+++ android/src/com/android/tools/idea/flags/ExperimentalSettingsConfigurable.java
@@ -100,7 +100,7 @@ public class ExperimentalSettingsConfigurable implements SearchableConfigurable,
   @Override
   @NotNull
   public String getId() {
-    return "gradle.experimental";
+    return "experimental";
   }
 
   @Override
