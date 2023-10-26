6cf60cd8d947d938c5b1ebfb819940c7b26c52a1
Matthew Gharrity <gharrma@google.com>
2023-10-26 21:34:31 +0000

    Migrate from deprecated ExternalSystemApiUtil APIs
    These APIs are removed in IntelliJ 2023.3.
    
    Change-Id: I2fab830f9dbcb513ea3ec2457ac22f91d48322c4
    
    GitOrigin-RevId: 5db386444bc12c160c4fa6baa6dbe2ab817e97f5

--- project-system-gradle/src/com/android/tools/idea/gradle/project/sync/issues/SyncIssues.kt
+++ project-system-gradle/src/com/android/tools/idea/gradle/project/sync/issues/SyncIssues.kt
@@ -25,9 +25,9 @@ import com.intellij.openapi.externalSystem.model.project.ProjectData
 import com.intellij.openapi.externalSystem.service.project.IdeModifiableModelsProvider
 import com.intellij.openapi.externalSystem.service.project.manage.AbstractProjectDataService
 import com.intellij.openapi.externalSystem.util.ExternalSystemApiUtil
-import com.intellij.openapi.externalSystem.util.ExternalSystemApiUtil.find
 import com.intellij.openapi.externalSystem.util.ExternalSystemApiUtil.findAll
-import com.intellij.openapi.externalSystem.util.ExternalSystemApiUtil.findProjectData
+import com.intellij.openapi.externalSystem.util.ExternalSystemApiUtil.findChild
+import com.intellij.openapi.externalSystem.util.ExternalSystemApiUtil.findProjectNode
 import com.intellij.openapi.module.Module
 import com.intellij.openapi.project.Project
 import org.jetbrains.plugins.gradle.util.GradleConstants
@@ -41,8 +41,8 @@ class SyncIssues(private val issues: List<IdeSyncIssue>) : List<IdeSyncIssue> by
     @JvmStatic
     fun Module.syncIssues(): SyncIssues {
       val linkedProjectPath = ExternalSystemApiUtil.getExternalRootProjectPath(this) ?: return EMPTY
-      val projectDataNode = findProjectData(project, GradleConstants.SYSTEM_ID, linkedProjectPath) ?: return EMPTY
-      val moduleDataNode = find(projectDataNode, ProjectKeys.MODULE) { node ->
+      val projectDataNode = findProjectNode(project, GradleConstants.SYSTEM_ID, linkedProjectPath) ?: return EMPTY
+      val moduleDataNode = findChild(projectDataNode, ProjectKeys.MODULE) { node ->
         node.data.internalName == name
       } ?: return EMPTY
       return SyncIssues(findAll(moduleDataNode, SYNC_ISSUE).map { dataNode -> dataNode.data })
