c22fcacd2544c76481ebce1b736aef7fe0e7b3ae
Fedor <kudasov@google.com>
2023-10-31 13:51:43 +0000

    Change formatting
    Bug: N/A
    Test: N/A
    Change-Id: I0961941de0bc3b3d978b6944c391e026e7e359a9
    
    GitOrigin-RevId: fd77dfb3223fbbe711ab2eaac926bab3a5c9b1e0

--- android/src/org/jetbrains/android/uipreview/ClassBinaryCacheManager.kt
+++ android/src/org/jetbrains/android/uipreview/ClassBinaryCacheManager.kt
@@ -26,44 +26,35 @@ import java.util.WeakHashMap
 import java.util.concurrent.locks.ReentrantLock
 import kotlin.concurrent.withLock
 
-private const val MAX_WEIGHT_BYTES = 100_000_000L // We will store no more than 100Mb of cached classes
+private const val MAX_WEIGHT_BYTES =
+  100_000_000L // We will store no more than 100Mb of cached classes
 private const val EXPIRE_MINUTES = 30L // We will store cached classes for no longer than 30 minutes
-/**
- * A class binary representation cache.
- */
-class ClassBinaryCacheManager private constructor(ticker: Ticker, maxWeight: Long, expireMinutes: Long) {
-  @GuardedBy("this")
-  private val moduleCaches = WeakHashMap<Module, ModuleClassCache>()
+/** A class binary representation cache. */
+class ClassBinaryCacheManager
+private constructor(ticker: Ticker, maxWeight: Long, expireMinutes: Long) {
+  @GuardedBy("this") private val moduleCaches = WeakHashMap<Module, ModuleClassCache>()
   private var lock = ReentrantLock()
-  /**
-   * A mapping from a library path to all the classes (fqcn) cached from this library.
-   */
-  @GuardedBy("lock")
-  private val libraryPath2ClassFqns = mutableMapOf<String, MutableSet<String>>()
-  /**
-   * A mapping from a fqcn to a library (path) that contains the class.
-   */
-  @GuardedBy("lock")
-  private val classFqn2LibraryPath = mutableMapOf<String, String>()
+  /** A mapping from a library path to all the classes (fqcn) cached from this library. */
+  @GuardedBy("lock") private val libraryPath2ClassFqns = mutableMapOf<String, MutableSet<String>>()
+  /** A mapping from a fqcn to a library (path) that contains the class. */
+  @GuardedBy("lock") private val classFqn2LibraryPath = mutableMapOf<String, String>()
 
-  /**
-   * A binary representation class cache (fqcn -> bytes).
-   */
-  private val globalCache = CacheBuilder
-    .newBuilder()
-    .ticker(ticker)
-    .concurrencyLevel(1)
-    .maximumWeight(maxWeight)
-    .weigher { _: String, value: ByteArray -> value.size }
-    .expireAfterAccess(Duration.ofMinutes(expireMinutes))
-    .removalListener<String, ByteArray> {
-      lock.withLock {
-        classFqn2LibraryPath.remove(it.key)?.let { url ->
-          libraryPath2ClassFqns[url]?.remove(it.key)
+  /** A binary representation class cache (fqcn -> bytes). */
+  private val globalCache =
+    CacheBuilder.newBuilder()
+      .ticker(ticker)
+      .concurrencyLevel(1)
+      .maximumWeight(maxWeight)
+      .weigher { _: String, value: ByteArray -> value.size }
+      .expireAfterAccess(Duration.ofMinutes(expireMinutes))
+      .removalListener<String, ByteArray> {
+        lock.withLock {
+          classFqn2LibraryPath.remove(it.key)?.let { url ->
+            libraryPath2ClassFqns[url]?.remove(it.key)
+          }
         }
       }
-    }
-    .build<String, ByteArray>()
+      .build<String, ByteArray>()
 
   /**
    * Returns a module specific cache that will only return classes if they belong to the module dependencies, the cache will also invalidate
@@ -120,12 +111,14 @@ class ClassBinaryCacheManager private constructor(ticker: Ticker, maxWeight: Lon
   }
 
   companion object {
-    private val globalManager = ClassBinaryCacheManager(Ticker.systemTicker(), MAX_WEIGHT_BYTES, EXPIRE_MINUTES)
+    private val globalManager =
+      ClassBinaryCacheManager(Ticker.systemTicker(), MAX_WEIGHT_BYTES, EXPIRE_MINUTES)
 
     @JvmStatic
     fun getInstance() = globalManager
 
     @TestOnly
-    fun getTestInstance(ticker: Ticker, maxWeight: Long, expireMinutes: Long) = ClassBinaryCacheManager(ticker, maxWeight, expireMinutes)
+    fun getTestInstance(ticker: Ticker, maxWeight: Long, expireMinutes: Long) =
+      ClassBinaryCacheManager(ticker, maxWeight, expireMinutes)
   }
 }
