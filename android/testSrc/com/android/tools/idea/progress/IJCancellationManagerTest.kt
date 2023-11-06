42ddc47e1d79705baa6130c930336a851cfe51c2
Fedor <kudasov@google.com>
2023-11-06 14:23:16 +0000

    Add IJ specific CancellationManager
    Bug: 306121377
    Test: IJCancellationManagerTest
    Change-Id: I1a6d9c9ccbb69050e7e12b540abbc006fac65f1a
    
    GitOrigin-RevId: 1d4fcecbb614f892d0d7357773acc631439391b0

---
+++ android/testSrc/com/android/tools/idea/progress/IJCancellationManagerTest.kt
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.progress
+
+import com.android.tools.environment.CancellationManager
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.progress.ProcessCanceledException
+import com.intellij.openapi.progress.ProgressManager
+import com.intellij.openapi.progress.impl.BackgroundableProcessIndicator
+import com.intellij.openapi.util.Disposer
+import com.intellij.testFramework.ApplicationRule
+import org.junit.Assert.assertThrows
+import org.junit.Rule
+import org.junit.Test
+
+class IJCancellationManagerTest {
+  @get:Rule
+  val rule = ApplicationRule()
+
+  @Test
+  fun test() {
+    val indicator = BackgroundableProcessIndicator(null, "", "", "", true)
+    try {
+      indicator.cancel()
+
+      assertThrows(ProcessCanceledException::class.java) {
+        ProgressManager.getInstance().executeProcessUnderProgress({
+                                                                    CancellationManager.throwIfCancelled()
+                                                                  }, indicator)
+      }
+    } finally {
+      ApplicationManager.getApplication().invokeAndWait {
+        Disposer.dispose(indicator)
+      }
+    }
+  }
+}
