6332eee75fbacc2edb9e5315b0263ca2eba82b63
Polina Koval <kovalp@google.com>
2023-11-10 13:33:06 +0000

    Add test to ApplyChangesActionTest
    Bug: 310179271
    Test: ApplyChangesActionTest
    Change-Id: I2477d8a35e7f6e0696bc48989c56a695399ae890
    
    GitOrigin-RevId: b135c5a82c00dfd3141b8f3d834610161a61e1d9

--- execution/common/testSrc/com/android/tools/idea/execution/common/applychanges/ApplyChangesActionTest.kt
+++ execution/common/testSrc/com/android/tools/idea/execution/common/applychanges/ApplyChangesActionTest.kt
@@ -18,20 +18,22 @@ package com.android.tools.idea.execution.common.applychanges
 import com.android.ddmlib.Client
 import com.android.ddmlib.IDevice
 import com.android.sdklib.AndroidVersion
+import com.android.testutils.MockitoKt
 import com.android.testutils.MockitoKt.mock
 import com.android.testutils.MockitoKt.whenever
-import com.android.tools.idea.execution.common.AndroidExecutionTarget
 import com.android.tools.idea.execution.common.AndroidSessionInfo
 import com.android.tools.idea.execution.common.debug.createFakeExecutionEnvironment
 import com.android.tools.idea.execution.common.processhandler.AndroidRemoteDebugProcessHandler
 import com.android.tools.idea.gradle.project.sync.snapshots.AndroidCoreTestProject
 import com.android.tools.idea.run.DeploymentApplicationService
+import com.android.tools.idea.run.util.SwapInfo
 import com.android.tools.idea.testing.AndroidProjectRule
 import com.android.tools.idea.testing.onEdt
 import com.google.common.truth.Truth.assertThat
-import com.intellij.execution.ExecutionTargetManager
+import com.intellij.execution.ExecutionManager
 import com.intellij.execution.RunManager
-import com.intellij.execution.configurations.RunConfiguration
+import com.intellij.execution.impl.ExecutionManagerImpl
+import com.intellij.execution.runners.ExecutionEnvironment
 import com.intellij.openapi.actionSystem.CommonDataKeys
 import com.intellij.openapi.actionSystem.impl.SimpleDataContext
 import com.intellij.openapi.application.ApplicationManager
@@ -43,6 +45,7 @@ import com.intellij.xdebugger.XDebugProcessStarter
 import com.intellij.xdebugger.XDebugSession
 import com.intellij.xdebugger.XDebuggerManager
 import com.intellij.xdebugger.evaluation.XDebuggerEditorsProvider
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
@@ -51,30 +54,40 @@ class ApplyChangesActionTest  {
   @get:Rule
   val projectRule = AndroidProjectRule.testProject(AndroidCoreTestProject.SIMPLE_APPLICATION).onEdt()
 
+  val project get() = projectRule.project
+
+  private val device = createMockDevice()
+
   private val APPLICATION_ID = "google.simpleapplication"
+
+  @Before
+  fun setup() {
+    val service = mock<DeploymentApplicationService>().also {
+      whenever(it.findClient(device, APPLICATION_ID)).thenReturn(listOf(mock<Client>()))
+    }
+    ApplicationManager.getApplication().replaceService(DeploymentApplicationService::class.java, service, projectRule.testRootDisposable)
+    setExecutionTargetForConfiguration(
+      project,
+      device,
+      RunManager.getInstance(project).selectedConfiguration!!.configuration,
+      projectRule.testRootDisposable
+    )
+  }
 
   @Test
   @RunsInEdt
   fun disabledDuringDebugSession() {
     // Set up
-    val device = createMockDevice()
-    val service = mock<DeploymentApplicationService>().also {
-      whenever(it.findClient(device, APPLICATION_ID)).thenReturn(listOf(mock<Client>()))
-    }
-    ApplicationManager.getApplication()
-      .replaceService(DeploymentApplicationService::class.java, service, projectRule.testRootDisposable)
-    setTarget(device, RunManager.getInstance(projectRule.project).selectedConfiguration!!.configuration)
-
     val client = mock<Client>()
     whenever(client.device).thenReturn(device)
 
-    val debugProcessHandler = AndroidRemoteDebugProcessHandler(projectRule.project, client, false)
+    val debugProcessHandler = AndroidRemoteDebugProcessHandler(project, client, false)
     AndroidSessionInfo.create(debugProcessHandler, listOf(device), APPLICATION_ID)
 
-    val executionEnvironment = createFakeExecutionEnvironment(projectRule.project, "fake env")
+    val executionEnvironment = createFakeExecutionEnvironment(project, "fake env")
 
     // Start debug session
-    XDebuggerManager.getInstance(projectRule.project).startSession(executionEnvironment, object : XDebugProcessStarter() {
+    XDebuggerManager.getInstance(project).startSession(executionEnvironment, object : XDebugProcessStarter() {
       override fun start(session: XDebugSession) = object : XDebugProcess(session) {
         override fun getEditorsProvider() = mock<XDebuggerEditorsProvider>()
         override fun doGetProcessHandler() = debugProcessHandler
@@ -83,7 +96,7 @@ class ApplyChangesActionTest  {
 
     // Update
     val action = ApplyChangesAction()
-    val event = TestActionEvent.createTestEvent(SimpleDataContext.getSimpleContext(CommonDataKeys.PROJECT, projectRule.project))
+    val event = TestActionEvent.createTestEvent(SimpleDataContext.getSimpleContext(CommonDataKeys.PROJECT, project))
     action.update(event)
 
     // Assert
@@ -91,26 +104,30 @@ class ApplyChangesActionTest  {
     assertThat(event.presentation.description).isEqualTo("Apply Changes and Restart Activity is disabled for this device because it is currently not allowed during debugging.")
   }
 
-  private fun setTarget(device: IDevice, config: RunConfiguration) {
-    val executionTarget = object : AndroidExecutionTarget() {
-      override fun getId() = "Test Target"
+  @Test
+  @RunsInEdt
+  fun applyChangesAction_executesApplyChanges() {
+    // Set up
+    var applyChangesExecuted = false
+    val executionServiceMock = mock<ExecutionManagerImpl>()
 
-      override fun getDisplayName() = "Test Target"
-
-      override fun getIcon() = null
-
-      override fun getAvailableDeviceCount() = 1
-
-      override fun getRunningDevices() = listOf(device)
+    whenever(executionServiceMock.executeConfiguration(MockitoKt.any(), MockitoKt.any(), MockitoKt.any())).thenAnswer {
+      val swapInfo = (it.getArgument(0) as ExecutionEnvironment).getUserData(SwapInfo.SWAP_INFO_KEY)
+      applyChangesExecuted = swapInfo?.type == SwapInfo.SwapType.APPLY_CHANGES
+      return@thenAnswer Unit
     }
 
-    val executionTargetManager = mock<ExecutionTargetManager>()
-      .also {
-        whenever(it.activeTarget).thenReturn(executionTarget)
-        whenever(it.getTargetsFor(config)).thenReturn(listOf(executionTarget))
-      }
-    projectRule.project.replaceService(ExecutionTargetManager::class.java, executionTargetManager, projectRule.testRootDisposable)
+    project.replaceService(ExecutionManager::class.java, executionServiceMock, projectRule.testRootDisposable)
+
+    // Perform
+    val action = ApplyChangesAction()
+    val event = TestActionEvent.createTestEvent(SimpleDataContext.getSimpleContext(CommonDataKeys.PROJECT, project))
+    action.actionPerformed(event)
+
+    // Assert
+    assertThat(applyChangesExecuted).isTrue()
   }
+
 
   private fun createMockDevice(): IDevice {
     val mockDevice = mock<IDevice>()
