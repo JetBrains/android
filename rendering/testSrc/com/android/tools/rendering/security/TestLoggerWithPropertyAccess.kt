402dbd8167efb438d142b768fcb7da0da25e5772
Matthew Gharrity <gharrma@google.com>
2023-11-14 19:34:26 -0800

    Simplify code by using DelegatingLogger
    As a side effect this makes TestLoggerWithPropertyAccess
    source-compatible with both IntelliJ 2023.2 and 2023.3.
    
    Change-Id: Id02922b0d36a89865dd99c2a9b3cda031ac24f25
    
    GitOrigin-RevId: 0aa7111194afbc8f680a0c3a947bbf0d5d2de0be

--- rendering/testSrc/com/android/tools/rendering/security/TestLoggerWithPropertyAccess.kt
+++ rendering/testSrc/com/android/tools/rendering/security/TestLoggerWithPropertyAccess.kt
@@ -15,33 +15,17 @@
  */
 package com.android.tools.rendering.security
 
+import com.intellij.openapi.diagnostic.DelegatingLogger
 import com.intellij.openapi.diagnostic.Logger
-import org.apache.log4j.Level
 
 /**
  * A [Logger] that is used for testing sandboxing. This will simulate a property access during error
  * logging that does not happen in the unit test logging but does happen in production.
  */
-class TestLoggerWithPropertyAccess(private val delegate: Logger) : Logger() {
-  override fun isDebugEnabled(): Boolean = delegate.isDebugEnabled
-
-  override fun debug(message: String?) = delegate.debug(message)
-
-  override fun debug(t: Throwable?) = delegate.debug(t)
-
-  override fun debug(message: String?, t: Throwable?) = delegate.debug(message, t)
-
-  override fun info(message: String?) = delegate.info(message)
-
-  override fun info(message: String?, t: Throwable?) = delegate.info(message, t)
-
-  override fun warn(message: String?, t: Throwable?) = delegate.warn(message, t)
-
+class TestLoggerWithPropertyAccess(delegate: Logger) : DelegatingLogger<Logger>(delegate) {
   override fun error(message: String?, t: Throwable?, vararg details: String?) {
     // Simulate a properties access as the IdeaLogger does.
     System.getProperties()
-    delegate.error(message, t, *details)
+    super.error(message, t, *details)
   }
-
-  @Suppress("UnstableApiUsage") override fun setLevel(level: Level) = delegate.setLevel(level)
 }
