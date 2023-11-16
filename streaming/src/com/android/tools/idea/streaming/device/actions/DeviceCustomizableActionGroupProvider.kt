89aa8a2c39d2b58e330955c1dccfafb5d188a8d7
Sergey Prigogin <sprigogin@google.com>
2023-11-15 19:57:41 -0800

    Add physical device toolbar customization
    Bug: N/A
    Test: existing
    Change-Id: I128b46e36fa2077b2a9487c0e4ba87eaaac5499c
    
    GitOrigin-RevId: 3a2bfd6a9debb39b656e6b4348263f05087bc093

--- streaming/src/com/android/tools/idea/streaming/device/actions/DeviceCustomizableActionGroupProvider.kt
+++ streaming/src/com/android/tools/idea/streaming/device/actions/DeviceCustomizableActionGroupProvider.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2020 The Android Open Source Project
+ * Copyright (C) 2023 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,14 +13,14 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.android.tools.idea.streaming.emulator.actions
+package com.android.tools.idea.streaming.device.actions
 
-import com.android.tools.idea.streaming.emulator.EMULATOR_MAIN_TOOLBAR_ID
+import com.android.tools.idea.streaming.device.DEVICE_MAIN_TOOLBAR_ID
 import com.intellij.ide.ui.customization.CustomizableActionGroupProvider
 
-class EmulatorCustomizableActionGroupProvider : CustomizableActionGroupProvider() {
+class DeviceCustomizableActionGroupProvider : CustomizableActionGroupProvider() {
 
   override fun registerGroups(registrar: CustomizableActionGroupRegistrar) {
-    registrar.addCustomizableActionGroup(EMULATOR_MAIN_TOOLBAR_ID, "Virtual Device Toolbar")
+    registrar.addCustomizableActionGroup(DEVICE_MAIN_TOOLBAR_ID, "Physical Device Toolbar")
   }
 }
