564ca4730042004ca023e419dea1f157bbb428b2
Yuichi Araki <yaraki@google.com>
2023-11-17 13:55:23 +0900

    Update test dependencies for Empty Compose Activity
    Bug: 303767219
    Test: TemplateDiffTest
    Change-Id: I5e25fac0684562ee3978a2e4e11fec86816e4816
    
    GitOrigin-RevId: 6f67fab253acbea18b57ed1c6dda0c6f4624f2f2

--- android-templates/testData/golden/testComposeActivityMaterial3/Template test module/src/main/java/template/test/pkg/ui/theme/Theme.kt
+++ android-templates/testData/golden/testComposeActivityMaterial3/Template test module/src/main/java/template/test/pkg/ui/theme/Theme.kt
@@ -9,11 +9,7 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalView
-import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
         primary = Purple80,
@@ -52,14 +48,6 @@ fun AppTheme(
 
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
-    }
-    val view = LocalView.current
-    if (!view.isInEditMode) {
-        SideEffect {
-            val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
-        }
     }
 
     MaterialTheme(
