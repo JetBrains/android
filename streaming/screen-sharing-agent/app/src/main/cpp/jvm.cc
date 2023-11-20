cb5215d23979be5485de7770b4970b4d7f6b3a2b
Sergey Prigogin <sprigogin@google.com>
2023-11-20 12:47:06 -0800

    Improve error messages
    Bug: N/A
    Test: existing
    Change-Id: I30739b85ddd93bb4f677ada2c5ca7bf1b89d142b
    
    GitOrigin-RevId: 64327c237e4fd8d3cfa8a5669d4c17bf9dd5fa03

--- streaming/screen-sharing-agent/app/src/main/cpp/jvm.cc
+++ streaming/screen-sharing-agent/app/src/main/cpp/jvm.cc
@@ -175,7 +175,7 @@ void JObject::IllegalGlobalReferenceUse() {
 jfieldID JClass::GetStaticFieldId(JNIEnv* jni_env, const char* name, const char* signature) const {
   auto field = jni_env->GetStaticFieldID(ref(), name, signature);
   if (field == nullptr) {
-    Log::Fatal(FIELD_NOT_FOUND, "Unable to find the static %s.%s field with signature %s", GetName(jni_env).c_str(), name, signature);
+    Log::Fatal(FIELD_NOT_FOUND, "Unable to find the static %s.%s field with signature \"%s\"", GetName(jni_env).c_str(), name, signature);
   }
   return field;
 }
@@ -183,7 +183,7 @@ jfieldID JClass::GetStaticFieldId(JNIEnv* jni_env, const char* name, const char*
 jfieldID JClass::GetFieldId(JNIEnv* jni_env, const char* name, const char* signature) const {
   auto field = jni_env->GetFieldID(ref(), name, signature);
   if (field == nullptr) {
-    Log::Fatal(FIELD_NOT_FOUND, "Unable to find the %s.%s field with signature %s", GetName(jni_env).c_str(), name, signature);
+    Log::Fatal(FIELD_NOT_FOUND, "Unable to find the %s.%s field with signature \"%s\"", GetName(jni_env).c_str(), name, signature);
   }
   return field;
 }
@@ -191,7 +191,7 @@ jfieldID JClass::GetFieldId(JNIEnv* jni_env, const char* name, const char* signa
 jmethodID JClass::GetStaticMethod(JNIEnv* jni_env, const char* name, const char* signature) const {
   auto method = jni_env->GetStaticMethodID(ref(), name, signature);
   if (method == nullptr) {
-    Log::Fatal(METHOD_NOT_FOUND, "Unable to find the static %s.%s method with signature %s", GetName(jni_env).c_str(), name, signature);
+    Log::Fatal(METHOD_NOT_FOUND, "Unable to find the static %s.%s method with signature \"%s\"", GetName(jni_env).c_str(), name, signature);
   }
   return method;
 }
@@ -199,7 +199,7 @@ jmethodID JClass::GetStaticMethod(JNIEnv* jni_env, const char* name, const char*
 jmethodID JClass::GetMethod(JNIEnv* jni_env, const char* name, const char* signature) const {
   auto method = jni_env->GetMethodID(ref(), name, signature);
   if (method == nullptr) {
-    Log::Fatal(METHOD_NOT_FOUND, "Unable to find the %s.%s method with signature %s", GetName(jni_env).c_str(), name, signature);
+    Log::Fatal(METHOD_NOT_FOUND, "Unable to find the %s.%s method with signature \"%s\"", GetName(jni_env).c_str(), name, signature);
   }
   return method;
 }
@@ -207,7 +207,7 @@ jmethodID JClass::GetMethod(JNIEnv* jni_env, const char* name, const char* signa
 jmethodID JClass::GetConstructor(JNIEnv* jni_env, const char* signature) const {
   auto constructor = jni_env->GetMethodID(ref(), "<init>", signature);
   if (constructor == nullptr) {
-    Log::Fatal(CONSTRUCTOR_NOT_FOUND, "Unable to find the %s constructor with signature %s", GetName(jni_env).c_str(), signature);
+    Log::Fatal(CONSTRUCTOR_NOT_FOUND, "Unable to find the %s constructor with signature \"%s\"", GetName(jni_env).c_str(), signature);
   }
   return constructor;
 }
@@ -225,7 +225,7 @@ jmethodID JClass::GetDeclaredOrInheritedMethod(JNIEnv* jni_env, const char* name
     }
     jni_env->ExceptionClear();
   }
-  Log::Fatal(METHOD_NOT_FOUND, "Unable to find the declared or inherited %s.%s method with signature %s",
+  Log::Fatal(METHOD_NOT_FOUND, "Unable to find the declared or inherited %s.%s method with signature \"%s\"",
              GetName(jni_env).c_str(), name, signature);
 }
 
