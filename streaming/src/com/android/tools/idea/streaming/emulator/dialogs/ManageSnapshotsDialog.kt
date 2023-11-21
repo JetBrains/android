486161844f430f9aa190f6ad589da97a5acd36a7
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2023-11-21 10:07:20 +0100

    [platform] unifying date/time formatting API (clients)
    GitOrigin-RevId: 991586c6d453753cb75733740d301e538fa1cd1a

--- streaming/src/com/android/tools/idea/streaming/emulator/dialogs/ManageSnapshotsDialog.kt
+++ streaming/src/com/android/tools/idea/streaming/emulator/dialogs/ManageSnapshotsDialog.kt
@@ -74,7 +74,7 @@ import com.intellij.ui.scale.JBUIScale
 import com.intellij.ui.table.TableView
 import com.intellij.util.IconUtil
 import com.intellij.util.concurrency.AppExecutorUtil.createBoundedApplicationPoolExecutor
-import com.intellij.util.text.JBDateFormat
+import com.intellij.util.text.DateFormatUtil
 import com.intellij.util.ui.ColumnInfo
 import com.intellij.util.ui.EmptyIcon
 import com.intellij.util.ui.JBImageIcon
@@ -243,7 +243,7 @@ internal class ManageSnapshotsDialog(
     val htmlEscaper = HtmlEscapers.htmlEscaper()
     val name = htmlEscaper.escape(snapshot.displayName)
     val size = getHumanizedSize(snapshot.sizeOnDisk)
-    val creationTime = JBDateFormat.getFormatter().formatDateTime(snapshot.creationTime)
+    val creationTime = DateFormatUtil.formatDateTime(snapshot.creationTime)
     val folderName = htmlEscaper.escape(snapshot.snapshotFolder.fileName.toString())
     val attributeSection = if (snapshot.creationTime == 0L) message("manage.snapshots.create.time.none") else message("manage.snapshots.created.info", creationTime, size)
     val fileSection = if (snapshot.isQuickBoot) "" else "<br>${message("manage.snapshots.file.name", folderName)}"
@@ -1132,13 +1132,11 @@ internal class ManageSnapshotsDialog(
   }
 }
 
-private fun formatPrettySnapshotDateTime(time: Long): String {
-  return if (time > 0) JBDateFormat.getFormatter().formatPrettyDateTime(time).replace(",", "") else "-"
-}
+private fun formatPrettySnapshotDateTime(time: Long): String =
+  if (time > 0) DateFormatUtil.formatPrettyDateTime(time).replace(",", "") else "-"
 
-private fun formatSnapshotSize(size: Long): String {
-  return if (size > 0) getHumanizedSize(size) else "-"
-}
+private fun formatSnapshotSize(size: Long): String =
+  if (size > 0) getHumanizedSize(size) else "-"
 
 private fun composeSnapshotId(existingSnapshots: Collection<SnapshotInfo>): String {
   val timestamp = TIMESTAMP_FORMAT.format(Date())
