c921b47980bc18f433d1e5a433f14f8aa6403895
Kurt Dresner <kdresner@google.com>
2023-11-22 10:02:35 -0800

    Fix up AndroidJavaDocExternalFilter
    This replaces some hard to understand/maintain/change code
    involving a bunch of state variables into a well-defined
    state machine that is easy to modify and reason about.
    
    Bug: 308441456
    Test: AndroidJavaDocExternalFilterTest.kt
    Change-Id: I6e502bb75c2e5569e244a19be6887ba995efef93
    
    GitOrigin-RevId: c82a83225017cdfa8b04e08e3450e7424d3ea058

--- android/src/org/jetbrains/android/AndroidJavaDocExternalFilter.kt
+++ android/src/org/jetbrains/android/AndroidJavaDocExternalFilter.kt
@@ -15,9 +15,16 @@
  */
 package org.jetbrains.android
 
+import com.android.tools.idea.util.fsm.StateMachine.Companion.stateMachine
 import com.intellij.codeInsight.javadoc.JavaDocExternalFilter
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.project.Project
+import org.jetbrains.android.AndroidJavaDocExternalFilter.Companion.State.ABORTED
+import org.jetbrains.android.AndroidJavaDocExternalFilter.Companion.State.CONSUMING_CLASS_DATA
+import org.jetbrains.android.AndroidJavaDocExternalFilter.Companion.State.CONSUMING_DESCRIPTION
+import org.jetbrains.android.AndroidJavaDocExternalFilter.Companion.State.MOVING_TO_CLASS_DATA
+import org.jetbrains.android.AndroidJavaDocExternalFilter.Companion.State.MOVING_TO_DESCRIPTION
+import org.jetbrains.android.AndroidJavaDocExternalFilter.Companion.State.SUCCESS
 import org.jetbrains.annotations.NonNls
 import java.io.BufferedReader
 import java.io.Reader
@@ -25,80 +32,9 @@ import java.io.Reader
 internal class AndroidJavaDocExternalFilter(project: Project?) : JavaDocExternalFilter(project) {
   public override fun doBuildFromStream(url: String, input: Reader, data: StringBuilder) {
     try {
-      // Looking up a method, field or constructor? If so we can use the
-      // builtin support -- it works.
-      if (ourAnchorSuffix.matcher(url).find()) {
-        super.doBuildFromStream(url, input, data)
-        return
-      }
-
-      BufferedReader(input).use { buf ->
-        // Pull out the javadoc section.
-        // The format has changed over time, so we need to look for different formats.
-        // The document begins with a bunch of stuff we don't want to include (e.g.
-        // page navigation etc); in all formats this seems to end with the following marker:
-        @NonNls val startSection = "<!-- ======== START OF CLASS DATA ======== -->"
-        // This doesn't appear anywhere in recent documentation,
-        // but presumably was needed at one point; left for now
-        // for users who have old documentation installed locally.
-        @NonNls val skipHeader = "<!-- END HEADER -->"
-        data.append(HTML)
-        var read: String?
-        do {
-          read = buf.readLine().trimEnd()
-        } while (read != null && !read.contains(startSection))
-        if (read == null) {
-          data.delete(0, data.length)
-          return
-        }
-        if (read.isNotBlank()) data.append(read).append("\n")
-
-        // Read until we reach the class overview (if present); copy everything until we see the
-        // optional marker skipHeader.
-        var skip = false
-        while (
-          buf.readLine().also { read = it.trimEnd() } !=
-            null && // Old format: class description follows <h2>Class Overview</h2>
-            !read!!.startsWith(
-              "<h2>Class Overview"
-            ) && // New format: class description follows just a <br><hr>. These
-            // are luckily not present in the older docs.
-            read != "<br><hr>"
-        ) {
-          if (read!!.contains("<table class=")) {
-            // Skip all tables until the beginning of the class description
-            skip = true
-          } else if (read!!.startsWith("<h2 class=\"api-section\"")) {
-            // Done; we've reached the section after the class description already.
-            // Newer docs have no marker section or class attribute marking the
-            // beginning of the class doc.
-            read = null
-            break
-          }
-          if (!skip && read!!.isNotBlank()) {
-            data.append(read).append("\n")
-          }
-          if (read!!.contains(skipHeader)) {
-            skip = true
-          }
-        }
-
-        // Now copy lines until the next <h2> section.
-        // In older versions of the docs format, this was a "<h2>", but in recent
-        // revisions (N+) it's <h2 class="api-section">
-        if (read != null) {
-          data.append("<br><div>\n")
-          while (
-            buf.readLine().also { read = it.trimEnd() } != null &&
-              !read!!.startsWith("<h2>") &&
-              !read!!.startsWith("<h2 ")
-          ) {
-            if (read!!.isNotBlank()) data.append(read).append("\n")
-          }
-          data.append("</div>\n")
-        }
-        data.append(HTML_CLOSE)
-      }
+      // Looking up a method, field or constructor? If so we can use the builtin support.
+      if (ourAnchorSuffix.matcher(url).find()) super.doBuildFromStream(url, input, data)
+      else BufferedReader(input).filterTo(data)
     } catch (e: Exception) {
       LOG.error(e.message, e, "URL: $url")
     }
@@ -106,5 +42,92 @@ internal class AndroidJavaDocExternalFilter(project: Project?) : JavaDocExternal
 
   companion object {
     private val LOG = Logger.getInstance("#org.jetbrains.android.AndroidDocumentationProvider")
+    // Pull out the javadoc section.
+    // The format has changed over time, so we need to look for different formats.
+    // The document begins with a bunch of stuff we don't want to include (e.g.
+    // page navigation etc); in all formats this seems to end with the following marker:
+    @NonNls private const val START_SECTION = "<!-- ======== START OF CLASS DATA ======== -->"
+    // This doesn't appear anywhere in recent documentation,
+    // but presumably was needed at one point; left for now
+    // for users who have old documentation installed locally.
+    @NonNls private const val SKIP_HEADER = "<!-- END HEADER -->"
+
+    private fun String.isClassDescriptionStart() =
+      startsWith("<h2>Class Overview") || equals("<br><hr>")
+
+    /**
+     * Returns true if we've clearly reached the section after the class description. Newer docs
+     * have no marker section or class attribute marking the beginning of the class doc.
+     */
+    private fun String.isClearlyBeyondClassDescription() = startsWith("<h2 class=\"api-section\"")
+
+    private enum class State {
+      MOVING_TO_CLASS_DATA,
+      CONSUMING_CLASS_DATA,
+      MOVING_TO_DESCRIPTION,
+      CONSUMING_DESCRIPTION,
+      SUCCESS,
+      ABORTED,
+    }
+
+    private fun createStateMachine(sb: StringBuilder) =
+      stateMachine(initialState = MOVING_TO_CLASS_DATA) {
+        MOVING_TO_CLASS_DATA.transitionsTo(CONSUMING_CLASS_DATA, ABORTED)
+        CONSUMING_CLASS_DATA {
+          onEnter { sb.append(HTML) }
+          transitionsTo(MOVING_TO_DESCRIPTION, CONSUMING_DESCRIPTION, SUCCESS)
+        }
+        MOVING_TO_DESCRIPTION.transitionsTo(CONSUMING_DESCRIPTION)
+        CONSUMING_DESCRIPTION {
+          onEnter { sb.append("<br><div>\n") }
+          onExit { sb.append("</div>\n") }
+          transitionsTo(SUCCESS)
+        }
+        SUCCESS.onEnter { sb.append(HTML_CLOSE) }
+        ABORTED.onEnter { sb.clear() }
+      }
+
+    private fun BufferedReader.filterTo(sb: StringBuilder) {
+      use { buf ->
+        var state by createStateMachine(sb)::state
+        while (state != ABORTED && state != SUCCESS) {
+          val line: String? = buf.readLine()?.trimEnd()
+          if (line == null) {
+            state = if (state == MOVING_TO_CLASS_DATA) ABORTED else SUCCESS
+            continue
+          }
+          if (line.isBlank()) continue // Blank lines don't change anything
+          when (state) {
+            SUCCESS,
+            ABORTED -> throw RuntimeException("Unreachable") // Compiler not smart enough.
+            MOVING_TO_CLASS_DATA -> {
+              if (line.contains(START_SECTION)) {
+                state = CONSUMING_CLASS_DATA
+                sb.append(line).append("\n")
+              }
+            }
+            CONSUMING_CLASS_DATA ->
+              when {
+                line.isClassDescriptionStart() -> state = CONSUMING_DESCRIPTION
+                line.contains("<table class=") -> state = MOVING_TO_DESCRIPTION
+                line.isClearlyBeyondClassDescription() -> state = SUCCESS
+                else -> {
+                  sb.append(line).append("\n")
+                  if (line.contains(SKIP_HEADER)) state = MOVING_TO_DESCRIPTION
+                }
+              }
+            MOVING_TO_DESCRIPTION ->
+              when {
+                line.isClearlyBeyondClassDescription() -> state = SUCCESS
+                line.isClassDescriptionStart() -> state = CONSUMING_DESCRIPTION
+              }
+            CONSUMING_DESCRIPTION -> {
+              if (line.startsWith("<h2>") || line.startsWith("<h2 ")) state = SUCCESS
+              else sb.append(line).append("\n")
+            }
+          }
+        }
+      }
+    }
   }
 }
