ec7bfc00e87b86e33e9bb75829a45ce6456364dd
Eugene Yakavets <yakavets@google.com>
2023-12-18 13:19:21 -0800

    Remove patch for altering FlatWelcomeFrame.
    Default welcome frame is now resizable, so code for adjusting it
    behaviour could be removed.
    
    Bug: 68295805
    Change-Id: I4c5be0ad1ccba41dee73c1f9b7788da584d3ef91
    
    GitOrigin-RevId: 4ccf753e9bc3b16d6d6375ac04687d1e92fff0c6

--- android/src/com/android/tools/idea/welcome/wizard/FirstRunWizardFrameProvider.java
+++ android/src/com/android/tools/idea/welcome/wizard/FirstRunWizardFrameProvider.java
@@ -16,18 +16,10 @@
 package com.android.tools.idea.welcome.wizard;
 
 import com.android.tools.idea.welcome.wizard.deprecated.FirstRunWizard;
-import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.wm.IdeFrame;
 import com.intellij.openapi.wm.WelcomeFrameProvider;
 import com.intellij.openapi.wm.WelcomeScreenProvider;
-import com.intellij.openapi.wm.impl.welcomeScreen.FlatWelcomeFrame;
 import com.intellij.openapi.wm.impl.welcomeScreen.WelcomeFrame;
-import com.intellij.ui.ScreenUtil;
-import org.jetbrains.annotations.Nullable;
-
-import java.awt.*;
-
-import static com.intellij.util.ui.update.UiNotifyConnector.doWhenFirstShown;
 
 /**
  * {@link WelcomeFrameProvider} for the {@link FirstRunWizard}.
@@ -35,51 +27,13 @@ import static com.intellij.util.ui.update.UiNotifyConnector.doWhenFirstShown;
 public class FirstRunWizardFrameProvider implements WelcomeFrameProvider {
   @Override
   public IdeFrame createFrame() {
-    WelcomeFrame frame = WelcomeScreenProvider.EP_NAME.computeSafeIfAny(provider -> {
+    return WelcomeScreenProvider.EP_NAME.computeSafeIfAny(provider -> {
       if (provider instanceof AndroidStudioWelcomeScreenProvider && provider.isAvailable()) {
-        // If we need to show the first run wizard, return a normal WelcomeFrame (which will initialize the wizard via the
+        // If we need to show the first run wizard, return an old WelcomeFrame (which will initialize the wizard via the
         // WelcomeScreenProvider extension point).
         return new WelcomeFrame();
       }
       return null;
-    });
-    return frame != null ? frame : customizeFlatWelcomeFrame();
-  }
-
-  /**
-   * Customizes the platform {@link FlatWelcomeFrame} so that it is resizable
-   * and fits the screen.
-   * <p>Note that this behavior is specific to Android Studio, as there are more
-   * actions displayed in the middle panel, making the whole welcome frame
-   * too big on low resolution screen with HiDPI. See
-   * <a href="https://issuetracker.google.com/issues/68295805">bug 68295805</a>.
-   */
-  @Nullable
-  private IdeFrame customizeFlatWelcomeFrame() {
-    return WelcomeFrame.EP.computeSafeIfAny(provider -> {
-      if (provider == this) {
-        // Avoid infinite recursion, since we are one of the providers.
-        return null;
-      }
-
-      IdeFrame frame = provider.createFrame();
-      if (frame == null) {
-        return null;
-      }
-
-      // Customize if FlatWelcomeFrame
-      if (frame instanceof FlatWelcomeFrame) {
-        FlatWelcomeFrame welcomeFrame = (FlatWelcomeFrame)frame;
-        doWhenFirstShown(welcomeFrame, () -> {
-          Logger.getInstance(this.getClass()).info("Overriding welcome frame to be resizable");
-          welcomeFrame.setResizable(true);
-          Rectangle newBounds = welcomeFrame.getBounds();
-          ScreenUtil.fitToScreen(newBounds);
-          welcomeFrame.setBounds(newBounds);
-        });
-      }
-      // Always return the first available frame
-      return frame;
     });
   }
 }
