6df31787f15f89685b5872bc86c4815fc6f2fcf0
Gianni <gmod@google.com>
2024-01-09 17:34:27 +0000

    Fix Ui Check feature flag
    Feature flag for Ui Check mode should be enabled in every channel
    instead it was true.
    
    Bug: 291874189
    Test: N/A
    Change-Id: I2c0401660eb9df4aa0eae0ff6580325e1a8117bf
    
    GitOrigin-RevId: 7adb362e3981822c44da3ecdcce376cb2203e429

--- android-common/src/com/android/tools/idea/flags/StudioFlags.java
+++ android-common/src/com/android/tools/idea/flags/StudioFlags.java
@@ -268,7 +268,7 @@ public final class StudioFlags {
   public static final Flag<Boolean> NELE_COMPOSE_UI_CHECK_MODE = new BooleanFlag(
     NELE, "compose.ui.check.mode", "Enable UI Check mode for Compose preview",
     "Enable UI Check mode in Compose preview for running ATF checks and Visual Linting",
-    ChannelDefault.of(false).withOverride(true, DEV, NIGHTLY, CANARY));
+    true);
 
   public static final Flag<Boolean> NELE_COMPOSE_UI_CHECK_COLORBLIND_MODE = Flag.create(
     NELE, "compose.ui.check.mode.colorblind", "Enable colorblind mode in UI Check for Compose preview",
