945da64754f73ebd943aeb882fabdcf7d4900f47
Chris Warrington <cmw@google.com>
2024-01-11 16:53:31 +0000

    Update model consumer version for Jellyfish
    Use of this value is gated behind experimental property
    gradle.ide.support.future.agp.versions=true
    
    Bug: N/A
    Test: None
    Change-Id: I22c11e5328dd8c191d76ae3f45758abe54c76965
    
    GitOrigin-RevId: 5d60115ed7ae3875c9391acc8bd01b747925a517

--- project-system-gradle-sync/src/com/android/tools/idea/gradle/project/sync/AgpCompatibility.kt
+++ project-system-gradle-sync/src/com/android/tools/idea/gradle/project/sync/AgpCompatibility.kt
@@ -24,7 +24,7 @@ import com.android.tools.idea.gradle.project.upgrade.computeAndroidGradlePluginC
 internal val LATEST_KNOWN_ANDROID_GRADLE_PLUGIN_VERSION = AgpVersion.parse(Version.ANDROID_GRADLE_PLUGIN_VERSION)
 internal val MINIMUM_SUPPORTED_AGP_VERSION = AgpVersion.parse(SdkConstants.GRADLE_PLUGIN_MINIMUM_VERSION)
 
-internal val MODEL_CONSUMER_VERSION = ModelConsumerVersion(66, 1, "Android Studio Iguana")
+internal val MODEL_CONSUMER_VERSION = ModelConsumerVersion(67, 1, "Android Studio Jellyfish")
 
 
 internal fun checkAgpVersionCompatibility(minimumModelConsumerVersion: ModelConsumerVersion?, agpVersion: AgpVersion, syncOptions: SyncActionOptions) {
