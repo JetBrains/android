aaeec9b16d78c11f40cf546f9ecf00e39fc67d80
Matthew Gharrity <gharrma@google.com>
2024-01-19 15:50:30 -0800

    Move test logic into sole subclass
    Change-Id: Ib5608a80aa213d6a1dc52b54b902546297addfa9
    
    GitOrigin-RevId: fdf4875af80151c392bc0d1a603161758ff994eb

--- android-kotlin/idea-android/tests/org/jetbrains/kotlin/android/quickfix/FindViewByIdQuickFixTest.kt
+++ android-kotlin/idea-android/tests/org/jetbrains/kotlin/android/quickfix/FindViewByIdQuickFixTest.kt
@@ -4,11 +4,18 @@
  */
 package org.jetbrains.kotlin.android.quickfix
 
+import com.intellij.codeInspection.InspectionProfileEntry
+import com.intellij.openapi.util.io.FileUtil
 import com.intellij.testFramework.TestDataFile
 import com.intellij.testFramework.TestDataPath
+import com.intellij.util.PathUtil
+import org.jetbrains.kotlin.android.DirectiveBasedActionUtils
+import org.jetbrains.kotlin.android.KotlinAndroidTestCase
+import org.jetbrains.kotlin.idea.base.plugin.isK2Plugin
+import java.io.File
 
-@TestDataPath("\$PROJECT_ROOT")
-class FindViewByIdQuickFixTest : AbstractAndroidLintQuickfixTest() {
+@TestDataPath("\$PROJECT_ROOT/android-kotlin")
+class FindViewByIdQuickFixTest : KotlinAndroidTestCase() {
     fun testAlreadyHasTypeArgument() {
         doTest(testFile("idea-android/testData/android/lintQuickfix/findViewById/alreadyHasTypeArgument.kt"))
     }
@@ -79,6 +86,46 @@ class FindViewByIdQuickFixTest : AbstractAndroidLintQuickfixTest() {
 
     fun testVariableTypeAlreadyExists() {
         doTest(testFile("idea-android/testData/android/lintQuickfix/findViewById/variableTypeAlreadyExists.kt"))
+    }
+
+    private fun doTest(path: String) {
+        val fileText = FileUtil.loadFile(File(testDataPath, path), true)
+        val intentionText = DirectiveBasedActionUtils.findStringWithPrefixesByFrontend(fileText, "// INTENTION_TEXT: ")
+                            ?: error("Empty intention text")
+        val mainInspectionClassName = DirectiveBasedActionUtils.findStringWithPrefixesByFrontend(fileText, "// INSPECTION_CLASS: ")
+                                      ?: error("No inspection class specified")
+        val dependency = DirectiveBasedActionUtils.findStringWithPrefixesByFrontend(fileText, "// DEPENDENCY: ")
+        val intentionAvailable = !DirectiveBasedActionUtils.isDirectiveDefinedForFrontend(fileText, "// INTENTION_NOT_AVAILABLE")
+
+        val inspection = Class.forName(mainInspectionClassName).newInstance() as InspectionProfileEntry
+        myFixture.enableInspections(inspection)
+
+        val sourceFile = myFixture.copyFileToProject(path, "src/${PathUtil.getFileName(path)}")
+        myFixture.configureFromExistingVirtualFile(sourceFile)
+
+        if (dependency != null) {
+            val (dependencyFile, dependencyTargetPath) = dependency.split(" -> ").map(String::trim)
+            myFixture.copyFileToProject("${PathUtil.getParentPath(path)}/$dependencyFile", "src/$dependencyTargetPath")
+        }
+
+        if (intentionAvailable) {
+            val oldLabel = intentionText
+              .replace(": Add @SuppressLint(\"", " ")
+              .replace("\") annotation", " with an annotation")
+            val intention = myFixture.getAvailableIntention(intentionText)
+                            ?: myFixture.getAvailableIntention(oldLabel)
+                            ?: error("Failed to find intention")
+            myFixture.launchAction(intention)
+            if (isK2Plugin() && File(testDataPath, "$path.k2.expected").isFile) {
+                myFixture.checkResultByFile("$path.k2.expected")
+            }
+            else {
+                myFixture.checkResultByFile("$path.expected")
+            }
+        }
+        else {
+            assertNull("Intention should not be available", myFixture.availableIntentions.find { it.text == intentionText })
+        }
     }
 
     private fun testFile(@TestDataFile testFile: String): String {
