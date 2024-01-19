7f5f402d85b0072232297de731aae4f932e9fbe3
Matthew Gharrity <gharrma@google.com>
2024-01-19 15:36:39 -0800

    Convert to Kotlin (part 2)
    Test: AndroidLintQuickfixTestGenerated
    Change-Id: I5fe5f3fdd6ec44f41c1ab6184e5f1ab77ac6120d
    
    GitOrigin-RevId: a0417b50d73dfb4e54de7b5f22257c5f04017685

--- android-kotlin/idea-android/tests/org/jetbrains/kotlin/android/quickfix/AndroidLintQuickfixTestGenerated.kt
+++ android-kotlin/idea-android/tests/org/jetbrains/kotlin/android/quickfix/AndroidLintQuickfixTestGenerated.kt
@@ -2,107 +2,104 @@
  * Copyright 2010-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license
  * that can be found in the license/LICENSE.txt file.
  */
+package org.jetbrains.kotlin.android.quickfix
 
-package org.jetbrains.kotlin.android.quickfix;
+import com.intellij.testFramework.TestDataPath
+import org.jetbrains.kotlin.android.KotlinTestUtils
 
-import com.intellij.testFramework.TestDataPath;
-import org.jetbrains.kotlin.android.KotlinTestUtils;
-
-@SuppressWarnings("all")
-@TestDataPath("$PROJECT_ROOT")
-public class AndroidLintQuickfixTestGenerated {
-    // This isn't actually lint, but TypeParameterFindViewByIdInspection
-    @TestDataPath("$PROJECT_ROOT")
-    public static class FindViewById extends AbstractAndroidLintQuickfixTest {
-
-        public void testAlreadyHasTypeArgument() throws Exception {
-            String fileName = KotlinTestUtils.navigationMetadata("idea-android/testData/android/lintQuickfix/findViewById/alreadyHasTypeArgument.kt");
-            doTest(fileName);
-        }
-
-        public void testCastDoesNotSatisfyTypeParameterBounds() throws Exception {
-            String fileName = KotlinTestUtils.navigationMetadata("idea-android/testData/android/lintQuickfix/findViewById/castDoesNotSatisfyTypeParameterBounds.kt");
-            doTest(fileName);
-        }
-
-        public void testFindViewWithTag() throws Exception {
-            String fileName = KotlinTestUtils.navigationMetadata("idea-android/testData/android/lintQuickfix/findViewById/findViewWithTag.kt");
-            doTest(fileName);
-        }
-
-        public void testNotReturningTypeParameter() throws Exception {
-            String fileName = KotlinTestUtils.navigationMetadata("idea-android/testData/android/lintQuickfix/findViewById/notReturningTypeParameter.kt");
-            doTest(fileName);
-        }
-
-        public void testNoTypeParameter() throws Exception {
-            String fileName = KotlinTestUtils.navigationMetadata("idea-android/testData/android/lintQuickfix/findViewById/noTypeParameter.kt");
-            doTest(fileName);
-        }
-
-        public void testNullableType() throws Exception {
-            String fileName = KotlinTestUtils.navigationMetadata("idea-android/testData/android/lintQuickfix/findViewById/nullableType.kt");
-            doTest(fileName);
-        }
-
-        public void testParenthesized() throws Exception {
-            String fileName = KotlinTestUtils.navigationMetadata("idea-android/testData/android/lintQuickfix/findViewById/parenthesized.kt");
-            doTest(fileName);
-        }
-
-        public void testPlatformNotNullExpression() throws Exception {
-            String fileName = KotlinTestUtils.navigationMetadata("idea-android/testData/android/lintQuickfix/findViewById/platformNotNullExpression.kt");
-            doTest(fileName);
-        }
-
-        public void testPlatformNullableExpression() throws Exception {
-            String fileName = KotlinTestUtils.navigationMetadata("idea-android/testData/android/lintQuickfix/findViewById/platformNullableExpression.kt");
-            doTest(fileName);
-        }
-
-        public void testQualifiedRequireView() throws Exception {
-            String fileName = KotlinTestUtils.navigationMetadata("idea-android/testData/android/lintQuickfix/findViewById/qualifiedRequireView.kt");
-            doTest(fileName);
-        }
-
-        public void testRequireView() throws Exception {
-            String fileName = KotlinTestUtils.navigationMetadata("idea-android/testData/android/lintQuickfix/findViewById/requireView.kt");
-            doTest(fileName);
-        }
-
-        public void testRequireViewNullable() throws Exception {
-            String fileName = KotlinTestUtils.navigationMetadata("idea-android/testData/android/lintQuickfix/findViewById/requireViewNullable.kt");
-            doTest(fileName);
-        }
-
-        public void testSafeCallOfNotNullFunction() throws Exception {
-            String fileName = KotlinTestUtils.navigationMetadata("idea-android/testData/android/lintQuickfix/findViewById/safeCallOfNotNullFunction.kt");
-            doTest(fileName);
-        }
-
-        public void testSafeCallOfNotNullFunctionWithNonNullCast() throws Exception {
-            String fileName = KotlinTestUtils.navigationMetadata("idea-android/testData/android/lintQuickfix/findViewById/safeCallOfNotNullFunctionWithNonNullCast.kt");
-            doTest(fileName);
-        }
-
-        public void testSafeCast() throws Exception {
-            String fileName = KotlinTestUtils.navigationMetadata("idea-android/testData/android/lintQuickfix/findViewById/safeCast.kt");
-            doTest(fileName);
-        }
-
-        public void testSimple() throws Exception {
-            String fileName = KotlinTestUtils.navigationMetadata("idea-android/testData/android/lintQuickfix/findViewById/simple.kt");
-            doTest(fileName);
-        }
-
-        public void testTooManyTypeParameters() throws Exception {
-            String fileName = KotlinTestUtils.navigationMetadata("idea-android/testData/android/lintQuickfix/findViewById/tooManyTypeParameters.kt");
-            doTest(fileName);
-        }
-
-        public void testVariableTypeAlreadyExists() throws Exception {
-            String fileName = KotlinTestUtils.navigationMetadata("idea-android/testData/android/lintQuickfix/findViewById/variableTypeAlreadyExists.kt");
-            doTest(fileName);
-        }
+@TestDataPath("\$PROJECT_ROOT")
+class AndroidLintQuickfixTestGenerated {
+  // This isn't actually lint, but TypeParameterFindViewByIdInspection
+  @TestDataPath("\$PROJECT_ROOT")
+  class FindViewById : AbstractAndroidLintQuickfixTest() {
+    fun testAlreadyHasTypeArgument() {
+      val fileName = KotlinTestUtils.navigationMetadata("idea-android/testData/android/lintQuickfix/findViewById/alreadyHasTypeArgument.kt")
+      doTest(fileName)
     }
+
+    fun testCastDoesNotSatisfyTypeParameterBounds() {
+      val fileName = KotlinTestUtils.navigationMetadata("idea-android/testData/android/lintQuickfix/findViewById/castDoesNotSatisfyTypeParameterBounds.kt")
+      doTest(fileName)
+    }
+
+    fun testFindViewWithTag() {
+      val fileName = KotlinTestUtils.navigationMetadata("idea-android/testData/android/lintQuickfix/findViewById/findViewWithTag.kt")
+      doTest(fileName)
+    }
+
+    fun testNotReturningTypeParameter() {
+      val fileName = KotlinTestUtils.navigationMetadata("idea-android/testData/android/lintQuickfix/findViewById/notReturningTypeParameter.kt")
+      doTest(fileName)
+    }
+
+    fun testNoTypeParameter() {
+      val fileName = KotlinTestUtils.navigationMetadata("idea-android/testData/android/lintQuickfix/findViewById/noTypeParameter.kt")
+      doTest(fileName)
+    }
+
+    fun testNullableType() {
+      val fileName = KotlinTestUtils.navigationMetadata("idea-android/testData/android/lintQuickfix/findViewById/nullableType.kt")
+      doTest(fileName)
+    }
+
+    fun testParenthesized() {
+      val fileName = KotlinTestUtils.navigationMetadata("idea-android/testData/android/lintQuickfix/findViewById/parenthesized.kt")
+      doTest(fileName)
+    }
+
+    fun testPlatformNotNullExpression() {
+      val fileName = KotlinTestUtils.navigationMetadata("idea-android/testData/android/lintQuickfix/findViewById/platformNotNullExpression.kt")
+      doTest(fileName)
+    }
+
+    fun testPlatformNullableExpression() {
+      val fileName = KotlinTestUtils.navigationMetadata("idea-android/testData/android/lintQuickfix/findViewById/platformNullableExpression.kt")
+      doTest(fileName)
+    }
+
+    fun testQualifiedRequireView() {
+      val fileName = KotlinTestUtils.navigationMetadata("idea-android/testData/android/lintQuickfix/findViewById/qualifiedRequireView.kt")
+      doTest(fileName)
+    }
+
+    fun testRequireView() {
+      val fileName = KotlinTestUtils.navigationMetadata("idea-android/testData/android/lintQuickfix/findViewById/requireView.kt")
+      doTest(fileName)
+    }
+
+    fun testRequireViewNullable() {
+      val fileName = KotlinTestUtils.navigationMetadata("idea-android/testData/android/lintQuickfix/findViewById/requireViewNullable.kt")
+      doTest(fileName)
+    }
+
+    fun testSafeCallOfNotNullFunction() {
+      val fileName = KotlinTestUtils.navigationMetadata("idea-android/testData/android/lintQuickfix/findViewById/safeCallOfNotNullFunction.kt")
+      doTest(fileName)
+    }
+
+    fun testSafeCallOfNotNullFunctionWithNonNullCast() {
+      val fileName = KotlinTestUtils.navigationMetadata("idea-android/testData/android/lintQuickfix/findViewById/safeCallOfNotNullFunctionWithNonNullCast.kt")
+      doTest(fileName)
+    }
+
+    fun testSafeCast() {
+      val fileName = KotlinTestUtils.navigationMetadata("idea-android/testData/android/lintQuickfix/findViewById/safeCast.kt")
+      doTest(fileName)
+    }
+
+    fun testSimple() {
+      val fileName = KotlinTestUtils.navigationMetadata("idea-android/testData/android/lintQuickfix/findViewById/simple.kt")
+      doTest(fileName)
+    }
+
+    fun testTooManyTypeParameters() {
+      val fileName = KotlinTestUtils.navigationMetadata("idea-android/testData/android/lintQuickfix/findViewById/tooManyTypeParameters.kt")
+      doTest(fileName)
+    }
+
+    fun testVariableTypeAlreadyExists() {
+      val fileName = KotlinTestUtils.navigationMetadata("idea-android/testData/android/lintQuickfix/findViewById/variableTypeAlreadyExists.kt")
+      doTest(fileName)
+    }
+  }
 }
