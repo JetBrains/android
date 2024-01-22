cbdc0f8024d67697e47992c671c5f6ce598e5ac7
Jenna Himawan <jhimawan@google.com>
2024-01-22 15:32:43 -0800

    Add flag for deep links DAL validation API key.
    Bug: 321807114
    Test: N/A
    Change-Id: I530cbba74de976d0828c2c503424542ef5d95aa5
    
    GitOrigin-RevId: c8396b22b59dd9c19dc3c330cbaa89b094f4b3d0

--- android-common/src/com/android/tools/idea/flags/StudioFlags.java
+++ android-common/src/com/android/tools/idea/flags/StudioFlags.java
@@ -1645,6 +1645,9 @@ public final class StudioFlags {
     Flag.create(APP_LINKS_ASSISTANT, "deeplinks.grpc.server", "Deep links gRPC server address",
                 "Deep links gRPC server address. Use a non-default value for testing purposes.",
                 "deeplinkassistant-pa.googleapis.com");
+  public static final Flag<String> DEEPLINKS_DAL_VALIDATION_API_KEY =
+    Flag.create(APP_LINKS_ASSISTANT, "deeplinks.dal.validation.api.key", "Deep links DAL validation API key",
+                "API key for deep links JSON validation. Put an API key here to use it. (The default is blank to prevent API usage.)", "");
   public static final Flag<Boolean> CREATE_APP_LINKS_V2 =
     Flag.create(APP_LINKS_ASSISTANT, "create.app.links.v2", "Create App Links V2",
                 "Improvements to the Create App Links functionalities.", false);
