9fce3dfc0362ea411f8f5345fd7e9db68e7ce81b
Kurt Dresner <kdresner@google.com>
2024-01-22 15:02:05 -0800

    Fix warnings in AndroidTestConfigurationProducerTest.java
    Bug: N/A
    Test: N/A
    Change-Id: I9b5d7a18aba7c75aa2c4ef44b7e693e6425c4048
    
    GitOrigin-RevId: 1aae77ceb86964e9e0129025de9f264484c73756

--- android/testartifacts/testSrc/com/android/tools/idea/testartifacts/instrumented/AndroidTestConfigurationProducerTest.java
+++ android/testartifacts/testSrc/com/android/tools/idea/testartifacts/instrumented/AndroidTestConfigurationProducerTest.java
@@ -36,8 +36,10 @@ import com.intellij.execution.PsiLocation;
 import com.intellij.execution.actions.ConfigurationContext;
 import com.intellij.execution.actions.ConfigurationFromContext;
 import com.intellij.execution.configurations.RunConfiguration;
+import com.intellij.openapi.actionSystem.ActionPlaces;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.actionSystem.PlatformCoreDataKeys;
+import com.intellij.openapi.actionSystem.impl.SimpleDataContext;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.module.ModuleUtilCore;
 import com.intellij.openapi.util.SystemInfo;
@@ -47,7 +49,6 @@ import com.intellij.psi.JavaPsiFacade;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiMethod;
 import com.intellij.psi.search.GlobalSearchScope;
-import com.intellij.testFramework.MapDataContext;
 import com.intellij.testFramework.PlatformTestUtil;
 import java.io.File;
 import java.nio.charset.StandardCharsets;
@@ -224,17 +225,17 @@ public class AndroidTestConfigurationProducerTest extends AndroidGradleTestCase 
   public void testCanCreateAndroidTestConfigurationWhenOriginalConfigExists() throws Exception {
     loadProject(TEST_ARTIFACTS_KOTLIN);
 
-    MapDataContext dataContext = new MapDataContext();
-    dataContext.put(CommonDataKeys.PROJECT, getProject());
+    SimpleDataContext.Builder dataContext = SimpleDataContext.builder()
+      .add(CommonDataKeys.PROJECT, getProject());
 
     PsiElement element = JavaPsiFacade.getInstance(getProject()).findClass(
       "com.example.android.kotlin.ExampleInstrumentedTest",
       GlobalSearchScope.projectScope(getProject()));
     assertNotNull(element);
-    dataContext.put(Location.DATA_KEY, PsiLocation.fromPsiElement(element));
+    dataContext.add(Location.DATA_KEY, PsiLocation.fromPsiElement(element));
 
     Module module = ModuleUtilCore.findModuleForPsiElement(element);
-    dataContext.put(PlatformCoreDataKeys.MODULE, module);
+    dataContext.add(PlatformCoreDataKeys.MODULE, module);
 
     // This test really is concerned with the case where the context data has an original
     // configuration with module information in it, to simulate the SMTRunnerConsole context
@@ -242,9 +243,10 @@ public class AndroidTestConfigurationProducerTest extends AndroidGradleTestCase 
     AndroidTestRunConfiguration original =
       new AndroidTestRunConfiguration(getProject(), AndroidTestRunConfigurationType.getInstance().getFactory());
     original.setModule(module);
-    dataContext.put(RunConfiguration.DATA_KEY, original);
+    dataContext.add(RunConfiguration.DATA_KEY, original);
 
-    ConfigurationContext context = ConfigurationContext.getFromContext(dataContext);
+    ConfigurationContext context =
+      ConfigurationContext.getFromContext(dataContext.build(), ActionPlaces.UNKNOWN);
     assertNotNull(context.getOriginalConfiguration(AndroidTestRunConfigurationType.getInstance()));
 
     AndroidTestConfigurationProducer producer = new AndroidTestConfigurationProducer();
@@ -258,7 +260,7 @@ public class AndroidTestConfigurationProducerTest extends AndroidGradleTestCase 
     File projectDir = VfsUtilCore.virtualToIoFile(PlatformTestUtil.getOrCreateProjectBaseDir(myFixture.getProject()));
     File newTestFile = new File(projectDir, "app/src/androidTest/java/google/simpleapplication/SomeTest.java");
     Files.createParentDirs(newTestFile);
-    Files.write(
+    Files.asCharSink(newTestFile, StandardCharsets.UTF_8).write(
       "package google.simpleapplication;\n" +
       "\n" +
       "import org.junit.Test;\n" +
@@ -272,9 +274,7 @@ public class AndroidTestConfigurationProducerTest extends AndroidGradleTestCase 
       "    }\n" +
       "    \n" +
       "  }\n" +
-      "}",
-      newTestFile,
-      StandardCharsets.UTF_8);
+      "}");
 
     LocalFileSystem.getInstance().refreshAndFindFileByIoFile(newTestFile);
 
