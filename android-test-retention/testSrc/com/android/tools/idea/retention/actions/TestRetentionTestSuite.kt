0a38bc4ca889d97e8a8cf72a41fa5e8110dbf970
Alexander Demurov <ademurov@google.com>
2024-02-02 10:59:25 -0800

    Remove no longer needed Android Test Retention
    Test: n/a
    Bug: 315008271
    Change-Id: I1401cab6fdf50f2183c2a25c35555892a09a9a43
    
    GitOrigin-RevId: 128392d058f9a4b735f532fff2367127b9c2bbd1

--- android-test-retention/testSrc/com/android/tools/idea/retention/actions/TestRetentionTestSuite.kt
+++
@@ -1,8 +0,0 @@
-package com.android.tools.idea.retention.actions
-
-import com.android.testutils.JarTestSuiteRunner
-import com.android.tools.tests.IdeaTestSuiteBase
-import org.junit.runner.RunWith
-
-@RunWith(JarTestSuiteRunner::class)
-class TestRetentionTestSuite : IdeaTestSuiteBase()
