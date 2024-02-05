bd01f0aadfd6dfe6bb95a149328308ce18b526d1
Blake Gallay <blakegallay@google.com>
2024-02-05 14:54:42 -0800

    Change configuration of Studio Bot flags
    Flag for custom transformation now only on for dev builds
    
    Flag for experiment using facts for project context on for dev, nightly,
    and canary
    
    Bug: none
    Test: none
    Change-Id: Idb803ba484d762bcf32a9d5f40d07c3c769f657b
    
    GitOrigin-RevId: 42006c5ff5c0e6034d67b9b0a6693e39e79cf3d3

--- android-common/src/com/android/tools/idea/flags/StudioFlags.java
+++ android-common/src/com/android/tools/idea/flags/StudioFlags.java
@@ -1798,13 +1798,13 @@ public final class StudioFlags {
     new BooleanFlag(STUDIOBOT, "editor.ai.custom.transform.enabled",
                     "Enable the custom transform action in the editor.",
                     "When enabled, the custom transform action, which allows users to send custom prompts to modify and iterate on code, is enabled.",
-                    ChannelDefault.of(false).withOverride(true, CANARY));
+                    ChannelDefault.of(false).withOverride(true, DEV));
 
   public static final Flag<Boolean> STUDIOBOT_USE_FACTS_FOR_CONTEXT =
     new BooleanFlag(STUDIOBOT, "chat.use.facts.for.context",
                     "Use retrieval augmentation facts to attach context to queries.",
                     "When enabled, project context is attached to queries using facts, instead of prepending them directly to the query string.",
-                    ChannelDefault.of(false).withOverride(true, CANARY));
+                    ChannelDefault.of(false).withOverride(true, DEV, NIGHTLY, CANARY));
 
   // endregion STUDIO_BOT
 
