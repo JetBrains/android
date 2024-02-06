085ac4cba944f622625fed68e4a6c55f301b86de
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2024-02-06 07:21:10 -0800

    IJPL-594 update fastutil 8.5.11 -> 8.5.13 (part 2)
    GitOrigin-RevId: 4f55be9e64dc4a484750762590056d478b746c97
    
    (cherry picked from commit 2b256e9a049d2f1efe628ee4025aa0be1faab331)
    Change-Id: Ic0d12043c016eef252f79b76e34fd9167eb9230e
    
    GitOrigin-RevId: 088ec9afeae6323ae413c6b2818147e02b43879b

--- android-common/src/com/android/tools/idea/io/FilePaths.java
+++ android-common/src/com/android/tools/idea/io/FilePaths.java
@@ -15,29 +15,26 @@
  */
 package com.android.tools.idea.io;
 
-import com.intellij.openapi.vfs.StandardFileSystems;
+import static com.android.SdkConstants.EXT_JAR;
+import static com.android.SdkConstants.EXT_ZIP;
+import static com.intellij.openapi.util.io.FileUtil.extensionEquals;
+import static com.intellij.openapi.vfs.StandardFileSystems.FILE_PROTOCOL;
+import static com.intellij.openapi.vfs.StandardFileSystems.JAR_PROTOCOL;
+import static com.intellij.openapi.vfs.StandardFileSystems.JAR_PROTOCOL_PREFIX;
+import static com.intellij.util.PathUtil.toSystemIndependentName;
+import static com.intellij.util.io.URLUtil.JAR_SEPARATOR;
+
 import com.intellij.openapi.vfs.VirtualFileManager;
+import java.io.File;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.io.File;
-
-import static com.android.SdkConstants.EXT_JAR;
-import static com.android.SdkConstants.EXT_ZIP;
-import static com.intellij.openapi.util.io.FileUtil.extensionEquals;
-import static com.intellij.openapi.util.io.FileUtil.toSystemIndependentName;
-import static com.intellij.openapi.util.io.FileUtilRt.toSystemDependentName;
-import static com.intellij.util.io.URLUtil.JAR_SEPARATOR;
-
 public final class FilePaths {
-  private FilePaths() {
-  }
-
   /**
-   * Converts the given path to an URL. The underlying implementation "cheats": it doesn't encode spaces and it just adds the "file"
+   * Converts the given path to a URL. The underlying implementation "cheats": it doesn't encode spaces, and it just adds the "file"
    * protocol at the beginning of this path. We use this method when creating URLs for file paths that will be included in a module's
    * content root, because converting a URL back to a path expects the path to be constructed the way this method does. To obtain a real
    * URL from a file path, use {@link com.android.utils.SdkUtils#fileToUrl(File)}.
@@ -45,12 +42,11 @@ public final class FilePaths {
    * @param path the given path.
    * @return the created URL.
    */
-  @NotNull
-  public static String pathToIdeaUrl(@NotNull File path) {
+  public static @NotNull String pathToIdeaUrl(@NotNull File path) {
     String name = path.getName();
     boolean isJarFile = extensionEquals(name, EXT_JAR) || extensionEquals(name, EXT_ZIP);
     // .jar files require an URL with "jar" protocol.
-    String protocol = isJarFile ? StandardFileSystems.JAR_PROTOCOL : StandardFileSystems.FILE_PROTOCOL;
+    String protocol = isJarFile ? JAR_PROTOCOL : FILE_PROTOCOL;
     String url = VirtualFileManager.constructUrl(protocol, toSystemIndependentName(path.getPath()));
     if (isJarFile) {
       url += JAR_SEPARATOR;
@@ -58,13 +54,12 @@ public final class FilePaths {
     return url;
   }
 
-  @Nullable
-  public static Path getJarFromJarUrl(@NotNull String url) {
+  public static @Nullable Path getJarFromJarUrl(@NotNull String url) {
     // URLs for jar file start with "jar://" and end with "!/".
-    if (!url.startsWith(StandardFileSystems.JAR_PROTOCOL_PREFIX)) {
+    if (!url.startsWith(JAR_PROTOCOL_PREFIX)) {
       return null;
     }
-    String path = url.substring(StandardFileSystems.JAR_PROTOCOL_PREFIX.length());
+    String path = url.substring(JAR_PROTOCOL_PREFIX.length());
     int index = path.lastIndexOf(JAR_SEPARATOR);
     if (index != -1) {
       path = path.substring(0, index);
@@ -76,8 +71,7 @@ public final class FilePaths {
    * Converts the given {@code String} path to a system-dependent path (as {@link File}.)
    */
   @Contract("!null -> !null")
-  @Nullable
-  public static Path toSystemDependentPath(@Nullable String path) {
+  public static @Nullable Path toSystemDependentPath(@Nullable String path) {
     return path == null ? null : Paths.get(path);
   }
 
@@ -85,8 +79,10 @@ public final class FilePaths {
    * Converts the given {@code String} path to a system-dependent path (as {@link File}.)
    */
   @Contract("!null -> !null")
-  @Nullable
-  public static File stringToFile(@Nullable String path) {
+  public static @Nullable File stringToFile(@Nullable String path) {
     return path == null ? null : new File(path);
+  }
+
+  private FilePaths() {
   }
 }
