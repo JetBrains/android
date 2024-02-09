1e22c2b291945d3bdb7b22a8952cf6abbc6f61e6
Paul Milian <paulmilian@google.com>
2024-02-09 12:01:39 +0000

    Introduce a findAllAnnotationsInGraph function that returns
    ... a list of NodeInfos for all UAnnotations that match a given
    predicate.
    
    Bug: 228294269
    Bug: 238749730
    Bug: 297200842
    Test: AnnotationsGraphFinderTest
    Change-Id: Icaaeac728d805cb28cda68658927cfba20cf7967
    
    GitOrigin-RevId: c2f9cb3b46508be6a82fc0dd8131b072f98fda9b

---
+++ designer/src/com/android/tools/idea/annotations/AnnotationsGraphFinder.kt
@@ -0,0 +1,111 @@
+/*
+ * Copyright (C) 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.annotations
+
+import com.android.annotations.concurrency.Slow
+import com.intellij.util.containers.sequenceOfNotNull
+import org.jetbrains.uast.UAnnotation
+import org.jetbrains.uast.UElement
+
+/**
+ * Data class containing graph traversal information for a [UAnnotation].
+ *
+ * Note: the [children] field gets populated as the graph is traversed.
+ */
+data class UAnnotationSubtreeInfo(
+  val depth: Int,
+  val topLevelAnnotation: UAnnotation?,
+  val children: MutableList<NodeInfo<UAnnotationSubtreeInfo>> = mutableListOf(),
+)
+
+/**
+ * Specific implementation of [NodeInfo] that populates a [UAnnotationSubtreeInfo.children] field
+ * before each child is traversed.
+ */
+private class UAnnotationNodeInfo(
+  override val parent: NodeInfo<UAnnotationSubtreeInfo>?,
+  override val element: UElement,
+  override val subtreeInfo: UAnnotationSubtreeInfo,
+) : NodeInfo<UAnnotationSubtreeInfo> {
+
+  override fun onSkippedChildTraversal(child: NodeInfo<UAnnotationSubtreeInfo>) {}
+
+  override fun onAfterChildTraversal(child: NodeInfo<UAnnotationSubtreeInfo>) {}
+
+  override fun onBeforeChildTraversal(child: NodeInfo<UAnnotationSubtreeInfo>) {
+    subtreeInfo.children += child
+  }
+}
+
+/** [NodeInfoFactory] implementation that creates [UAnnotationNodeInfo] for each node traversed. */
+private object UAnnotationNodeInfoFactory : NodeInfoFactory<UAnnotationSubtreeInfo> {
+  override fun create(
+    parent: NodeInfo<UAnnotationSubtreeInfo>?,
+    curElement: UElement,
+  ): NodeInfo<UAnnotationSubtreeInfo> {
+    return UAnnotationNodeInfo(
+      parent = parent,
+      element = curElement,
+      subtreeInfo =
+        UAnnotationSubtreeInfo(
+          depth = parent?.subtreeInfo?.depth?.let { it + 1 } ?: 0,
+          topLevelAnnotation = parent?.subtreeInfo?.topLevelAnnotation ?: curElement as? UAnnotation,
+        ),
+    )
+  }
+}
+
+/**
+ * [ResultFactory] that creates a [NodeInfo] of type [UAnnotationSubtreeInfo] for each annotation
+ * that matches a given [filter] predicate.
+ */
+private class UAnnotationResultFactory(private val filter: (UAnnotation) -> Boolean) :
+  ResultFactory<UAnnotationSubtreeInfo, NodeInfo<UAnnotationSubtreeInfo>> {
+  override fun create(
+    node: NodeInfo<UAnnotationSubtreeInfo>
+  ): Sequence<NodeInfo<UAnnotationSubtreeInfo>> {
+    return sequenceOfNotNull(
+      node.takeIf {
+        val element = it.element
+        element is UAnnotation && filter(element)
+      }
+    )
+  }
+}
+
+/**
+ * Given an [UElement] used as the root element to search from, this method searches all reachable
+ * annotations in the annotation graph and returns a list of [NodeInfo] of type
+ * [UAnnotationSubtreeInfo] containing all annotations matching the [filter] predicate.
+ *
+ * The parameter [shouldTraverse] can be provided to determine which [UAnnotation]s in the graph
+ * should be traversed. By default, all [UAnnotation]s are traversed.
+ *
+ * Note: [UAnnotation]s matching the [filter] predicate act as leaves when traversing the graph.
+ */
+@Slow
+fun UElement.findAllAnnotationsInGraph(
+  shouldTraverse: (UAnnotation) -> Boolean = { true },
+  filter: (UAnnotation) -> Boolean,
+): Sequence<NodeInfo<UAnnotationSubtreeInfo>> {
+  val annotationsGraph =
+    AnnotationsGraph(UAnnotationNodeInfoFactory, UAnnotationResultFactory { filter(it) })
+  return annotationsGraph.traverse(
+    listOf(this),
+    annotationFilter = { _, annotation -> shouldTraverse(annotation) },
+    isLeafAnnotation = { filter(it) },
+  )
+}
---
+++ designer/testSrc/com/android/tools/idea/annotations/AnnotationsGraphFinderTest.kt
@@ -0,0 +1,267 @@
+/*
+ * Copyright (C) 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.annotations
+
+import com.android.tools.idea.testing.AndroidProjectRule
+import com.google.common.truth.Truth.assertThat
+import com.intellij.openapi.application.runReadAction
+import com.intellij.psi.PsiClass
+import com.intellij.psi.PsiFile
+import org.intellij.lang.annotations.Language
+import org.jetbrains.kotlin.idea.KotlinFileType
+import org.jetbrains.uast.UAnnotation
+import org.jetbrains.uast.evaluateString
+import org.jetbrains.uast.tryResolve
+import org.junit.Assert.assertEquals
+import org.junit.Rule
+import org.junit.Test
+
+class AnnotationsGraphFinderTest {
+  @get:Rule val projectRule: AndroidProjectRule = AndroidProjectRule.inMemory()
+
+  private val project
+    get() = projectRule.project
+
+  private val fixture
+    get() = projectRule.fixture
+
+  @Test
+  fun testTraverse_backEdge() {
+    @Language("kotlin")
+    val fileContent =
+      """
+      // Graph illustration:
+      // rootMethod --> 0 --> 1 --> 2 --> 3
+      //                      ^-----------'
+      // Note that all edges are tree edges, except 3->1, that is a back edge.
+      @node2
+      annotation class node1
+
+      @node3
+      annotation class node2
+
+      @node1
+      annotation class node3
+
+      @node1
+      annotation class node0
+
+      @node0
+      fun rootMethod(){}
+    """
+        .trimIndent()
+
+    val psiFile = fixture.configureByText(KotlinFileType.INSTANCE, fileContent)
+    val rootMethod = psiFile.getMethodAnnotatedBy("node0")
+
+    val annotations =
+      rootMethod
+        .findAllAnnotationsInGraph { runReadAction { it.qualifiedName == "node3" } }
+        .toList()
+    assertThat(annotations.map { (it.element as UAnnotation).qualifiedName })
+      .containsExactly("node3")
+  }
+
+  @Test
+  fun testTraverse_forwardEdge() {
+    @Language("kotlin")
+    val fileContent =
+      """
+      // Graph illustration:
+      // rootMethod --> 0 --> 1 --> 2 --> 3
+      //                      '-----------^
+      // Note that all edges are tree edges, except 1->3, that is a forward edge.
+      @node2
+      @node3
+      annotation class node1
+
+      @node3
+      annotation class node2
+
+      annotation class node3
+
+      @node1
+      annotation class node0
+
+      @node0
+      fun rootMethod(){}
+    """
+        .trimIndent()
+
+    val psiFile = fixture.configureByText(KotlinFileType.INSTANCE, fileContent)
+    val rootMethod = psiFile.getMethodAnnotatedBy("node0")
+
+    val annotations =
+      rootMethod
+        .findAllAnnotationsInGraph { runReadAction { it.qualifiedName == "node3" } }
+        .toList()
+    assertThat(annotations.map { (it.element as UAnnotation).qualifiedName })
+      .containsExactly("node3", "node3")
+  }
+
+  @Test
+  fun testTraverse_crossEdge() {
+    @Language("kotlin")
+    val fileContent =
+      """
+      // Graph illustration:
+      // rootMethod --> 0 --> 1 --> 2 --> 3
+      //                      '---> 4 ----^
+      // Note that all edges are tree edges, except 4->3, that is a cross edge.
+      @node2
+      @node4
+      annotation class node1
+
+      @node3
+      annotation class node2
+
+      annotation class node3
+
+      @node3
+      annotation class node4
+
+      @node1
+      annotation class node0
+
+      @node0
+      fun rootMethod(){}
+    """
+        .trimIndent()
+
+    val psiFile = fixture.configureByText(KotlinFileType.INSTANCE, fileContent)
+    val rootMethod = psiFile.getMethodAnnotatedBy("node0")
+
+    val annotations =
+      rootMethod
+        .findAllAnnotationsInGraph { runReadAction { it.qualifiedName == "node3" } }
+        .toList()
+    assertThat(annotations.map { (it.element as UAnnotation).qualifiedName })
+      .containsExactly("node3", "node3")
+  }
+
+  @Test
+  fun testTraverse_differentParameters() {
+    @Language("kotlin")
+    val fileContent =
+      """
+      // Graph illustration:
+      // rootMethod --> 0 --> 1 --> 2 --> 3(name"1")
+      //                      '---> 3(name="2")
+
+      @node2
+      @node3(name="2")
+      annotation class node1
+
+      @node3(name="1")
+      annotation class node2
+
+      annotation class node3(name: String)
+
+      @node3
+      annotation class node4
+
+      @node1
+      annotation class node0
+
+      @node0
+      fun rootMethod(){}
+    """
+        .trimIndent()
+
+    val psiFile = fixture.configureByText(KotlinFileType.INSTANCE, fileContent)
+    val rootMethod = psiFile.getMethodAnnotatedBy("node0")
+
+    val annotations =
+      rootMethod
+        .findAllAnnotationsInGraph { runReadAction { it.qualifiedName == "node3" } }
+        .toList()
+    runReadAction {
+      assertThat(
+          annotations
+            .map { it.element as UAnnotation }
+            .map { it.qualifiedName to it.findAttributeValue("name")?.evaluateString() }
+        )
+        .containsExactly("node3" to "1", "node3" to "2")
+    }
+  }
+
+  @Test
+  fun testFindMultiPreviewsExample() {
+    @Language("kotlin")
+    val fileContent =
+      """
+      // This annotation class is the "Preview" in this context (see TestMultiPreviewNodeInfo.isPreview)
+      annotation class MyTestPreview
+
+      // Using this annotation shouldn't have any effect
+      annotation class EmptyAnnotation
+
+      @MyTestPreview
+      annotation class NotReachableFromSourceElements
+
+      @MyTestPreview
+      @EmptyAnnotation
+      annotation class Intermediate1 // with 1 direct Preview
+
+      @MyTestPreview
+      @MyTestPreview
+      annotation class Intermediate2 // with 2 direct Previews
+
+      @MyTestPreview // direct preview
+      @Intermediate1
+      @Intermediate2
+      @EmptyAnnotation
+      fun rootMethod(){}
+    """
+        .trimIndent()
+
+    val psiFile = fixture.configureByText(KotlinFileType.INSTANCE, fileContent)
+    val rootMethod = psiFile.getMethodAnnotatedBy("Intermediate1")
+
+    val previews =
+      rootMethod
+        .findAllAnnotationsInGraph {
+          runReadAction { (it.tryResolve() as PsiClass).name == "MyTestPreview" }
+        }
+        .toList()
+
+    assertThat(previews).hasSize(4)
+
+    val topLevelAnnotationsToDepth =
+      previews.map {
+        val topLevelAnnotationName = runReadAction {
+          (it.subtreeInfo?.topLevelAnnotation?.tryResolve() as PsiClass).name
+        }
+        val depth = it.subtreeInfo?.depth ?: -1
+        topLevelAnnotationName to depth
+      }
+    assertEquals(
+      listOf(
+        "Intermediate1" to 2,
+        "Intermediate2" to 2,
+        "Intermediate2" to 2,
+        "MyTestPreview" to 1,
+      ),
+      topLevelAnnotationsToDepth.sortedBy { it.first },
+    )
+  }
+
+  private fun PsiFile.getMethodAnnotatedBy(annotationShortName: String) = runReadAction {
+    findAnnotations(project, virtualFile, annotationShortName).firstNotNullOf {
+      it.qualifiedName?.let { qualifiedName -> it.getContainingUMethodAnnotatedWith(qualifiedName) }
+    }
+  }
+}
