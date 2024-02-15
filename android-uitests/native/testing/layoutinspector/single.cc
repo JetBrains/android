9f3b4d0bb67cfb830e746416fde721ce73b0d369
Jens Ole Lauridsen <jlauridsen@google.com>
2024-02-15 16:15:49 +0000

    Adapt skia changes to old UI test
    The test willl not pass but at least build.
    This test was failing before this change see b/325457879.
    
    Bug: 319896964
    Test: BasicLayoutInspectorUITest
    Change-Id: I4d3c40cb7fd6f8fcd18d236d0a9eddbb0ef0f202
    
    GitOrigin-RevId: 2e8a5f019426dc319b9600668a94ccae125b92fc

--- android-uitests/native/testing/layoutinspector/single.cc
+++ android-uitests/native/testing/layoutinspector/single.cc
@@ -1,11 +1,14 @@
 #include <fstream>
 #include <jni.h>
 #include "include/core/SkData.h"
+#include "include/core/SkPicture.h"
 #include "include/core/SkPictureRecorder.h"
 #include "include/core/SkRRect.h"
+#include "include/core/SkSerialProcs.h"
 #include "include/core/SkCanvas.h"
 #include "include/core/SkPaint.h"
 #include "include/core/SkRect.h"
+#include "include/encode/SkPngEncoder.h"
 
 /**
  * Tool to generate single.skp used in testing the layout inspector.
@@ -30,7 +33,11 @@ Java_com_android_tools_idea_tests_gui_layoutinspector_FakeBoxes_generateSingleBo
     canvas->drawAnnotation(skRect1, "/RenderNode(id=1, name='LinearLayout')", nullptr);
 
     sk_sp<SkPicture> picture = recorder.finishRecordingAsPicture();
-    sk_sp<SkData> data = picture->serialize();
+    SkSerialProcs procs;
+    procs.fImageProc = [](SkImage *img, void *) -> sk_sp<SkData> {
+      return SkPngEncoder::Encode(nullptr, img, SkPngEncoder::Options{});
+    };
+    sk_sp<SkData> data = picture->serialize(&procs);
     jbyteArray array = env->NewByteArray(data->size());
     env->SetByteArrayRegion (array, 0, data->size(), (const jbyte *)data->data());
     return array;
