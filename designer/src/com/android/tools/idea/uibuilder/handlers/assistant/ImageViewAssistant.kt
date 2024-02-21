697b0873602f38325a6450f59eedc81abe808b1c
Diego Perez <diegoperez@google.com>
2024-02-21 15:45:49 +0000

    Fix ImageViewAssistant
    so the ResourceManagerExplorerTest works correctly.
    
    This also replaces the layout for the AssistantPopupPanel with a Box
    layout since the VerticalFlowLayout would break under UI testing.
    
    Fixes: 325906794
    Fixes: 325906929
    Test: ResourceExplorerTest
    Change-Id: I1c7c1cc1095779aef84f2aeb094975e6cbfc3041
    
    GitOrigin-RevId: a53eda2ad1be39f9c9752cae53bb2980535af753

--- designer/src/com/android/tools/idea/uibuilder/handlers/assistant/ImageViewAssistant.kt
+++ designer/src/com/android/tools/idea/uibuilder/handlers/assistant/ImageViewAssistant.kt
@@ -30,10 +30,11 @@ import com.android.tools.idea.ui.resourcechooser.util.createResourcePickerDialog
 import com.android.tools.idea.uibuilder.assistant.AssistantPopupPanel
 import com.android.tools.idea.uibuilder.assistant.ComponentAssistantFactory
 import com.android.tools.idea.uibuilder.handlers.ImageViewHandler
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.application.ModalityState
 import com.intellij.openapi.util.text.StringUtil
 import com.intellij.ui.components.JBCheckBox
 import com.intellij.util.concurrency.AppExecutorUtil
-import com.intellij.util.concurrency.EdtExecutorService
 import com.intellij.util.ui.JBUI
 import com.intellij.util.ui.JBUI.Borders
 import com.intellij.util.ui.JBUI.scale
@@ -118,17 +119,19 @@ class ImageViewAssistant(
     // Get SampleData drawables in background thread, then, update the widget on the EDT
     sampleDataLoaded =
       CompletableFuture.supplyAsync(
-          {
+        {
+          val sampleDataItems =
             StudioResourceRepositoryManager.getAppResources(nlComponent.model.facet)
               .getSampleDataOfType(IMAGE)
               .toList()
-          },
-          AppExecutorUtil.getAppExecutorService(),
-        )
-        .whenCompleteAsync(
-          { sampleDataItems, _ -> populateWidget(sampleDataItems) },
-          EdtExecutorService.getScheduledExecutorInstance(),
-        )
+
+          ApplicationManager.getApplication()
+            .invokeAndWait({ populateWidget(sampleDataItems) }, ModalityState.nonModal())
+
+          sampleDataItems
+        },
+        AppExecutorUtil.getAppExecutorService(),
+      )
   }
 
   private fun isSampleValueAll(value: String?) = value?.endsWith(']')?.not() ?: false
