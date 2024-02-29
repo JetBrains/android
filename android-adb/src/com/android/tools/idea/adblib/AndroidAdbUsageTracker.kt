a92b9b488b8046c76059bf1a6ee9b2f2b82dc6b2
Alexander Demurov <ademurov@google.com>
2024-02-28 18:23:52 -0800

    Use Android Studio Usage Stats to track adblib events
    Log information about success/failures in
    `JdwpProcessPropertiesCollector` in Android Studio usage stats.
    
    Test: added
    Bug: 325514914
    Change-Id: Ied9dbbb0fbee68694bcf2e9a96f6ac93475987a5
    
    GitOrigin-RevId: 0b8438df4c342be46ecec34533e0d11044c79119

--- android-adb/src/com/android/tools/idea/adblib/AndroidAdbUsageTracker.kt
+++ android-adb/src/com/android/tools/idea/adblib/AndroidAdbUsageTracker.kt
@@ -16,9 +16,46 @@
 package com.android.tools.idea.adblib
 
 import com.android.adblib.AdbUsageTracker
+import com.android.tools.analytics.UsageTracker
+import com.google.wireless.android.sdk.stats.AdbUsageEvent
+import com.google.wireless.android.sdk.stats.AndroidStudioEvent
 
 class AndroidAdbUsageTracker : AdbUsageTracker {
+
   override fun logUsage(event: AdbUsageTracker.Event) {
-    // TODO(b/325514914): Create a `AndroidStudioEvent` and log using `UsageTracker`
+    val androidStudioEvent = AndroidStudioEvent.newBuilder().setKind(AndroidStudioEvent.EventKind.ADB_USAGE_EVENT)
+
+    event.jdwpProcessPropertiesCollector?.let {
+      androidStudioEvent.adbUsageEventBuilder.processPropertiesEventBuilder
+        .setSuccess(it.isSuccess)
+        .setPreviouslyFailedCount(it.previouslyFailedCount)
+
+      val failureType = it.failureType?.toProtoEnum()
+      if (failureType != null) {
+        androidStudioEvent.adbUsageEventBuilder.processPropertiesEventBuilder.failureType = failureType
+      }
+      val previousFailureType = it.previousFailureType?.toProtoEnum()
+      if (previousFailureType != null) {
+        androidStudioEvent.adbUsageEventBuilder.processPropertiesEventBuilder.previousFailureType = previousFailureType
+      }
+    }
+
+    UsageTracker.log(androidStudioEvent)
+  }
+
+  private fun AdbUsageTracker.JdwpProcessPropertiesCollectorFailureType.toProtoEnum(): AdbUsageEvent.JdwpProcessPropertiesCollectorEvent.FailureType {
+    return when (this) {
+      AdbUsageTracker.JdwpProcessPropertiesCollectorFailureType.NO_RESPONSE ->
+        AdbUsageEvent.JdwpProcessPropertiesCollectorEvent.FailureType.NO_RESPONSE
+
+      AdbUsageTracker.JdwpProcessPropertiesCollectorFailureType.CLOSED_CHANNEL_EXCEPTION ->
+        AdbUsageEvent.JdwpProcessPropertiesCollectorEvent.FailureType.CLOSED_CHANNEL_EXCEPTION
+
+      AdbUsageTracker.JdwpProcessPropertiesCollectorFailureType.CONNECTION_CLOSED_ERROR ->
+        AdbUsageEvent.JdwpProcessPropertiesCollectorEvent.FailureType.CONNECTION_CLOSED_ERROR
+
+      AdbUsageTracker.JdwpProcessPropertiesCollectorFailureType.OTHER_ERROR ->
+        AdbUsageEvent.JdwpProcessPropertiesCollectorEvent.FailureType.OTHER_ERROR
+    }
   }
 }
