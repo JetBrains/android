d563ad5ebf11d954cd58e22fcf89d813e2b52dcf
Andy Magee <andymagee@google.com>
2024-02-29 15:58:34 -0800

    Add multi-module test for BindingScopeEnlarger
    Test: BindingScopeEnlargerMultiModuleTest
    Bug: 327677623
    Change-Id: I42566b7fa6bad0e01cf8f98ab0b6a213d7cedf34
    
    GitOrigin-RevId: bae491b6cb2906fe469dc63e2c88a346f86678a8

--- databinding/testSrc/com/android/tools/idea/databinding/finders/BindingScopeEnlargerTest.kt
+++ databinding/testSrc/com/android/tools/idea/databinding/finders/BindingScopeEnlargerTest.kt
@@ -17,21 +17,45 @@ package com.android.tools.idea.databinding.finders
 
 import com.android.tools.idea.databinding.DataBindingMode
 import com.android.tools.idea.databinding.module.LayoutBindingModuleCache
+import com.android.tools.idea.gradle.model.IdeAndroidProjectType
+import com.android.tools.idea.gradle.model.IdeModuleWellKnownSourceSet
+import com.android.tools.idea.testing.AndroidModuleDependency
+import com.android.tools.idea.testing.AndroidModuleModelBuilder
+import com.android.tools.idea.testing.AndroidProjectBuilder
 import com.android.tools.idea.testing.AndroidProjectRule
+import com.android.tools.idea.testing.JavaModuleModelBuilder
+import com.android.tools.idea.testing.gradleModule
 import com.android.tools.idea.util.androidFacet
 import com.google.common.truth.Truth.assertThat
+import com.intellij.openapi.application.runReadAction
 import com.intellij.openapi.project.DumbService
+import com.intellij.psi.PsiFile
+import com.intellij.psi.PsiJavaFile
 import com.intellij.psi.search.PsiSearchScopeUtil
 import com.intellij.testFramework.DumbModeTestUtils
 import com.intellij.testFramework.EdtRule
 import com.intellij.testFramework.RunsInEdt
 import com.intellij.testFramework.fixtures.JavaCodeInsightTestFixture
+import org.jetbrains.android.facet.AndroidFacet
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.RuleChain
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+private val EMPTY_LAYOUT_FILE =
+  // language=xml
+  """
+  <?xml version="1.0" encoding="utf-8"?>
+  <layout xmlns:android="http://schemas.android.com/apk/res/android">
+    <LinearLayout />
+  </layout>
+  """
+    .trimIndent()
 
 @RunsInEdt
+@RunWith(JUnit4::class)
 class BindingScopeEnlargerTest {
   private val projectRule = AndroidProjectRule.onDisk()
 
@@ -46,7 +70,7 @@ class BindingScopeEnlargerTest {
    * [AndroidProjectRule] is initialized to use the disk.
    */
   private val fixture
-    get() = projectRule.fixture as JavaCodeInsightTestFixture
+    get() = projectRule.fixture
 
   private val facet
     get() = projectRule.module.androidFacet!!
@@ -79,17 +103,7 @@ class BindingScopeEnlargerTest {
     // stale value while in dumb mode, but should return an updated value after dumb mode completes.
     val (activityClass, dumbScope) =
       DumbModeTestUtils.computeInDumbModeSynchronously(project) {
-        fixture.addFileToProject(
-          "res/layout/activity_main.xml",
-          // language=XML
-          """
-          <?xml version="1.0" encoding="utf-8"?>
-          <layout xmlns:android="http://schemas.android.com/apk/res/android">
-            <LinearLayout />
-          </layout>
-        """
-            .trimIndent(),
-        )
+        fixture.addFileToProject("res/layout/activity_main.xml", EMPTY_LAYOUT_FILE)
         val activityClass = fixture.addClass("public class MainActivity {}")
         val dumbScope = activityClass.resolveScope
         Pair(activityClass, dumbScope)
@@ -109,4 +123,186 @@ class BindingScopeEnlargerTest {
       }
     }
   }
+}
+
+@RunWith(JUnit4::class)
+class BindingScopeEnlargerMultiModuleTest {
+  @get:Rule
+  val projectRule =
+    AndroidProjectRule.withAndroidModels(
+        { dir ->
+          assertThat(dir.resolve("app/src").mkdirs()).isTrue()
+          assertThat(dir.resolve("app/res").mkdirs()).isTrue()
+          assertThat(dir.resolve("lib/src").mkdirs()).isTrue()
+          assertThat(dir.resolve("lib/res").mkdirs()).isTrue()
+        },
+        JavaModuleModelBuilder.rootModuleBuilder,
+        AndroidModuleModelBuilder(
+          ":app",
+          "debug",
+          AndroidProjectBuilder(
+            androidModuleDependencyList = { _ -> listOf(AndroidModuleDependency(":lib", "debug")) },
+            namespace = { "com.example.app" },
+          ),
+        ),
+        AndroidModuleModelBuilder(
+          ":lib",
+          "debug",
+          AndroidProjectBuilder(
+            projectType = { IdeAndroidProjectType.PROJECT_TYPE_LIBRARY },
+            namespace = { "com.example.lib" },
+          ),
+        ),
+      )
+      .initAndroid(true)
+
+  private val fixture by lazy { projectRule.fixture }
+
+  private val project by lazy { projectRule.project }
+
+  private val appModule by lazy {
+    requireNotNull(project.gradleModule(":app", IdeModuleWellKnownSourceSet.MAIN))
+  }
+  private val libModule by lazy {
+    requireNotNull(project.gradleModule(":lib", IdeModuleWellKnownSourceSet.MAIN))
+  }
+
+  private val appFacet by lazy { requireNotNull(appModule.androidFacet) }
+  private val libFacet by lazy { requireNotNull(libModule.androidFacet) }
+
+  @Before
+  fun setUp() {
+    LayoutBindingModuleCache.getInstance(appFacet).dataBindingMode = DataBindingMode.ANDROIDX
+    LayoutBindingModuleCache.getInstance(libFacet).dataBindingMode = DataBindingMode.ANDROIDX
+  }
+
+  @Test
+  fun appScopeContainsLayoutFromLib() {
+    fixture.addFileToProject("app/src/main/res/layout/activity_app.xml", EMPTY_LAYOUT_FILE)
+    fixture.addFileToProject("lib/src/main/res/layout/activity_lib.xml", EMPTY_LAYOUT_FILE)
+
+    // Validate that light binding classes are generated for both layouts
+    val appLightBindingClasses = appFacet.getLightBindingClasses()
+    val libLightBindingClasses = libFacet.getLightBindingClasses()
+
+    assertThat(appLightBindingClasses).hasSize(1)
+    assertThat(libLightBindingClasses).hasSize(1)
+
+    val appLightBindingClass = appLightBindingClasses.single()
+    val libLightBindingClass = libLightBindingClasses.single()
+    assertThat(appLightBindingClass.qualifiedName)
+      .isEqualTo("com.example.app.databinding.ActivityAppBinding")
+    assertThat(libLightBindingClass.qualifiedName)
+      .isEqualTo("com.example.lib.databinding.ActivityLibBinding")
+
+    // The app module should have both binding classes in scope, whereas the lib module should only
+    // have its own class.
+    val appActivityClass =
+      fixture
+        .addFileToProject("app/src/main/src/AppActivity.java", "public class AppActivity {}")
+        .getFirstJavaClass()
+    val libActivityClass =
+      fixture
+        .addFileToProject("lib/src/main/src/LibActivity.java", "public class LibActivity {}")
+        .getFirstJavaClass()
+
+    assertThat(PsiSearchScopeUtil.isInScope(appActivityClass.resolveScope, appLightBindingClass))
+      .isTrue()
+    assertThat(PsiSearchScopeUtil.isInScope(appActivityClass.resolveScope, libLightBindingClass))
+      .isTrue()
+    assertThat(PsiSearchScopeUtil.isInScope(libActivityClass.resolveScope, appLightBindingClass))
+      .isFalse()
+    assertThat(PsiSearchScopeUtil.isInScope(libActivityClass.resolveScope, libLightBindingClass))
+      .isTrue()
+  }
+
+  @Test
+  fun scopeValuesUpdateAfterDumbMode() {
+    val appActivityClass =
+      fixture
+        .addFileToProject("app/src/main/src/AppActivity.java", "public class AppActivity {}")
+        .getFirstJavaClass()
+    val libActivityClass =
+      fixture
+        .addFileToProject("lib/src/main/src/LibActivity.java", "public class LibActivity {}")
+        .getFirstJavaClass()
+
+    fixture.addFileToProject("app/src/main/res/layout/activity1_app.xml", EMPTY_LAYOUT_FILE)
+    fixture.addFileToProject("lib/src/main/res/layout/activity1_lib.xml", EMPTY_LAYOUT_FILE)
+
+    var appActivity1LightClass =
+      appFacet.getLightBindingClass("com.example.app.databinding.Activity1AppBinding")
+    var libActivity1LightClass =
+      libFacet.getLightBindingClass("com.example.lib.databinding.Activity1LibBinding")
+    assertThat(PsiSearchScopeUtil.isInScope(appActivityClass.resolveScope, appActivity1LightClass))
+      .isTrue()
+    assertThat(PsiSearchScopeUtil.isInScope(libActivityClass.resolveScope, appActivity1LightClass))
+      .isFalse()
+    assertThat(PsiSearchScopeUtil.isInScope(appActivityClass.resolveScope, libActivity1LightClass))
+      .isTrue()
+    assertThat(PsiSearchScopeUtil.isInScope(libActivityClass.resolveScope, libActivity1LightClass))
+      .isTrue()
+
+    // In dumb mode, add a resource and then request the current scope. The enlarger will return a
+    // stale value while in dumb mode, but should return an updated value after dumb mode completes.
+    DumbModeTestUtils.computeInDumbModeSynchronously(project) {
+      fixture.addFileToProject("app/src/main/res/layout/activity2_app.xml", EMPTY_LAYOUT_FILE)
+      fixture.addFileToProject("lib/src/main/res/layout/activity2_lib.xml", EMPTY_LAYOUT_FILE)
+
+      // TODO(b/327677623): Uncomment this assertion when the bug is fixed.
+      // assertThat(PsiSearchScopeUtil.isInScope(appActivityClass.resolveScope,
+      // appActivity1LightClass)).isTrue()
+      assertThat(
+          PsiSearchScopeUtil.isInScope(libActivityClass.resolveScope, appActivity1LightClass)
+        )
+        .isFalse()
+      assertThat(
+          PsiSearchScopeUtil.isInScope(appActivityClass.resolveScope, libActivity1LightClass)
+        )
+        .isTrue()
+      assertThat(
+          PsiSearchScopeUtil.isInScope(libActivityClass.resolveScope, libActivity1LightClass)
+        )
+        .isTrue()
+    }
+
+    // Light classes will be regenerated now, so need to refetch the first two.
+    appActivity1LightClass =
+      appFacet.getLightBindingClass("com.example.app.databinding.Activity1AppBinding")
+    libActivity1LightClass =
+      libFacet.getLightBindingClass("com.example.lib.databinding.Activity1LibBinding")
+    val appActivity2LightClass =
+      appFacet.getLightBindingClass("com.example.app.databinding.Activity2AppBinding")
+    val libActivity2LightClass =
+      libFacet.getLightBindingClass("com.example.lib.databinding.Activity2LibBinding")
+
+    assertThat(PsiSearchScopeUtil.isInScope(appActivityClass.resolveScope, appActivity1LightClass))
+      .isTrue()
+    assertThat(PsiSearchScopeUtil.isInScope(libActivityClass.resolveScope, appActivity1LightClass))
+      .isFalse()
+    assertThat(PsiSearchScopeUtil.isInScope(appActivityClass.resolveScope, libActivity1LightClass))
+      .isTrue()
+    assertThat(PsiSearchScopeUtil.isInScope(libActivityClass.resolveScope, libActivity1LightClass))
+      .isTrue()
+
+    assertThat(PsiSearchScopeUtil.isInScope(appActivityClass.resolveScope, appActivity2LightClass))
+      .isTrue()
+    assertThat(PsiSearchScopeUtil.isInScope(libActivityClass.resolveScope, appActivity2LightClass))
+      .isFalse()
+    assertThat(PsiSearchScopeUtil.isInScope(appActivityClass.resolveScope, libActivity2LightClass))
+      .isTrue()
+    assertThat(PsiSearchScopeUtil.isInScope(libActivityClass.resolveScope, libActivity2LightClass))
+      .isTrue()
+  }
+
+  private fun AndroidFacet.getLightBindingClasses() = runReadAction {
+    LayoutBindingModuleCache.getInstance(this).let { cache ->
+      cache.bindingLayoutGroups.flatMap { cache.getLightBindingClasses(it) }
+    }
+  }
+
+  private fun AndroidFacet.getLightBindingClass(qualifiedName: String) =
+    getLightBindingClasses().single { it.qualifiedName == qualifiedName }
+
+  private fun PsiFile.getFirstJavaClass() = runReadAction { (this as PsiJavaFile).classes.first() }
 }
