b3f917f1b0159cdd86825e20e1d157835c1d661f
Jens Ole Lauridsen <jlauridsen@google.com>
2024-03-07 23:34:45 +0000

    Revert "Adapt skia changes to old UI test"
    Revert submission 26216455-update-skia-to-104
    
    Reason for revert: This change depends on a version of skia that is newer than the version in aosp. We need to wait until the framework skia version is caught up.
    
    Reverted changes: /q/submissionid:26216455-update-skia-to-104
    
    Change-Id: Id4a89e9fadc7a391cd5a7587bd82816c1569a9a1
    
    GitOrigin-RevId: a7ca53915bd32a974d7d0001763285ed74b91cc5

--- android-uitests/native/testing/layoutinspector/single.cc
+++ android-uitests/native/testing/layoutinspector/single.cc
@@ -1,14 +1,11 @@
 #include <fstream>
 #include <jni.h>
 #include "include/core/SkData.h"
-#include "include/core/SkPicture.h"
 #include "include/core/SkPictureRecorder.h"
 #include "include/core/SkRRect.h"
-#include "include/core/SkSerialProcs.h"
 #include "include/core/SkCanvas.h"
 #include "include/core/SkPaint.h"
 #include "include/core/SkRect.h"
-#include "include/encode/SkPngEncoder.h"
 
 /**
  * Tool to generate single.skp used in testing the layout inspector.
@@ -33,11 +30,7 @@ Java_com_android_tools_idea_tests_gui_layoutinspector_FakeBoxes_generateSingleBo
     canvas->drawAnnotation(skRect1, "/RenderNode(id=1, name='LinearLayout')", nullptr);
 
     sk_sp<SkPicture> picture = recorder.finishRecordingAsPicture();
-    SkSerialProcs procs;
-    procs.fImageProc = [](SkImage *img, void *) -> sk_sp<SkData> {
-      return SkPngEncoder::Encode(nullptr, img, SkPngEncoder::Options{});
-    };
-    sk_sp<SkData> data = picture->serialize(&procs);
+    sk_sp<SkData> data = picture->serialize();
     jbyteArray array = env->NewByteArray(data->size());
     env->SetByteArrayRegion (array, 0, data->size(), (const jbyte *)data->data());
     return array;
