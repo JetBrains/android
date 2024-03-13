9d2f5a327e72e78bfbd32814ce12eb62c85011d2
Christophe Rhodes <xof@google.com>
2024-03-13 18:51:25 +0000

    Hook Something parser into the Android plugin
    Define File types, token sets, and so on just enough to get
    something that might integrate into the final Android Studio
    build.
    
    Bug: 328257084
    Bug: 329227053
    Test: none yet
    Change-Id: I137f33595abaf8ef671c7100e8e53f135d11f0cb
    
    GitOrigin-RevId: f28e8257a84a3242fc6f876019b0cc7e86983429

---
+++ gradle-dsl-something/src/com/android/tools/idea/gradle/something/SomethingFileType.kt
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.gradle.something
+
+import com.intellij.openapi.fileTypes.LanguageFileType
+import icons.GradleIcons
+import javax.swing.Icon
+
+class SomethingFileType : LanguageFileType(SomethingLanguage.INSTANCE) {
+  override fun getName(): String = "Something (Gradle Declarative)"
+  override fun getDescription(): String = "Something (Gradle Declarative Build DSL)"
+  override fun getDefaultExtension(): String = "gradle.something"
+  override fun getIcon(): Icon = GradleIcons.GradleFile
+
+  companion object {
+    @JvmStatic
+    val INSTANCE = SomethingFileType()
+  }
+}
