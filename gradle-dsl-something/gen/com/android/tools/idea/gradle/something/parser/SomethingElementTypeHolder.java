12caf64c318b25e29831ee501bcd99a4f6a18e27
Christophe Rhodes <xof@google.com>
2024-03-13 18:00:10 +0000

    Add generated files for Gradle "Something" Dsl
    The _SomethingLexer.flex is a generated file despite it being in src/;
    it is apparently GrammarKit convention to put it there, where it is used
    by JFlex to generate _SomethingLexer.java.
    
    Bug: 328257084
    Bug: 329227053
    Test: none yet
    Change-Id: Ic414ae2086688691185909729f0e5957cbdc1e95
    
    GitOrigin-RevId: ed8be0314044ebc40aa8a2ac88309b0a340123ef

---
+++ gradle-dsl-something/gen/com/android/tools/idea/gradle/something/parser/SomethingElementTypeHolder.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright (C) 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// ATTENTION: This file has been automatically generated from something.bnf. Do not edit it manually.
+package com.android.tools.idea.gradle.something.parser;
+
+import com.intellij.psi.tree.IElementType;
+import com.intellij.psi.PsiElement;
+import com.intellij.lang.ASTNode;
+import com.android.tools.idea.gradle.something.psi.SomethingElementType;
+import com.android.tools.idea.gradle.something.psi.impl.*;
+
+public interface SomethingElementTypeHolder {
+
+  IElementType ASSIGNMENT = new SomethingElementType("ASSIGNMENT");
+  IElementType BARE = new SomethingElementType("BARE");
+  IElementType BLOCK = new SomethingElementType("BLOCK");
+  IElementType ENTRY = new SomethingElementType("ENTRY");
+  IElementType FACTORY = new SomethingElementType("FACTORY");
+  IElementType IDENTIFIER = new SomethingElementType("IDENTIFIER");
+  IElementType LVALUE = new SomethingElementType("LVALUE");
+  IElementType PROPERTY = new SomethingElementType("PROPERTY");
+  IElementType QUALIFIED = new SomethingElementType("QUALIFIED");
+  IElementType RVALUE = new SomethingElementType("RVALUE");
+
+  IElementType BOOLEAN = new SomethingTokenType("boolean");
+  IElementType LINE_COMMENT = new SomethingTokenType("line_comment");
+  IElementType NULL = new SomethingTokenType("null");
+  IElementType NUMBER = new SomethingTokenType("number");
+  IElementType OP_DOT = new SomethingTokenType(".");
+  IElementType OP_EQ = new SomethingTokenType("=");
+  IElementType OP_LBRACE = new SomethingTokenType("{");
+  IElementType OP_LPAREN = new SomethingTokenType("(");
+  IElementType OP_RBRACE = new SomethingTokenType("}");
+  IElementType OP_RPAREN = new SomethingTokenType(")");
+  IElementType STRING = new SomethingTokenType("string");
+  IElementType TOKEN = new SomethingTokenType("token");
+
+  class Factory {
+    public static PsiElement createElement(ASTNode node) {
+      IElementType type = node.getElementType();
+      if (type == ASSIGNMENT) {
+        return new SomethingAssignmentImpl(node);
+      }
+      else if (type == BARE) {
+        return new SomethingBareImpl(node);
+      }
+      else if (type == BLOCK) {
+        return new SomethingBlockImpl(node);
+      }
+      else if (type == ENTRY) {
+        return new SomethingEntryImpl(node);
+      }
+      else if (type == FACTORY) {
+        return new SomethingFactoryImpl(node);
+      }
+      else if (type == IDENTIFIER) {
+        return new SomethingIdentifierImpl(node);
+      }
+      else if (type == LVALUE) {
+        return new SomethingLvalueImpl(node);
+      }
+      else if (type == QUALIFIED) {
+        return new SomethingQualifiedImpl(node);
+      }
+      else if (type == RVALUE) {
+        return new SomethingRvalueImpl(node);
+      }
+      throw new AssertionError("Unknown element type: " + type);
+    }
+  }
+}
---
+++ gradle-dsl-something/src/com/android/tools/idea/gradle/something/parser/_SomethingLexer.flex
@@ -0,0 +1,54 @@
+package com.android.tools.idea.gradle.something.parser;
+
+import com.intellij.lexer.FlexLexer;
+import com.intellij.psi.tree.IElementType;
+
+import static com.intellij.psi.TokenType.BAD_CHARACTER;
+import static com.intellij.psi.TokenType.WHITE_SPACE;
+import static com.android.tools.idea.gradle.something.parser.SomethingElementTypeHolder.*;
+
+%%
+
+%{
+  public _SomethingLexer() {
+    this((java.io.Reader)null);
+  }
+%}
+
+%public
+%class _SomethingLexer
+%implements FlexLexer
+%function advance
+%type IElementType
+%unicode
+
+EOL=\R
+WHITE_SPACE=\s+
+
+LINE_COMMENT="//".*
+NUMBER=[0-9]+
+STRING=\"[^\"]*\"
+BOOLEAN=(true|false)
+TOKEN=[a-z][a-zA-Z0-9]*
+
+%%
+<YYINITIAL> {
+  {WHITE_SPACE}       { return WHITE_SPACE; }
+
+  "="                 { return OP_EQ; }
+  "."                 { return OP_DOT; }
+  "{"                 { return OP_LBRACE; }
+  "}"                 { return OP_RBRACE; }
+  "("                 { return OP_LPAREN; }
+  ")"                 { return OP_RPAREN; }
+  "null"              { return NULL; }
+
+  {LINE_COMMENT}      { return LINE_COMMENT; }
+  {NUMBER}            { return NUMBER; }
+  {STRING}            { return STRING; }
+  {BOOLEAN}           { return BOOLEAN; }
+  {TOKEN}             { return TOKEN; }
+
+}
+
+[^] { return BAD_CHARACTER; }
