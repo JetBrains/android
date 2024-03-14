22dbc7535414f8d2e0b46690d74ecc1eefc7bcaf
Andy Magee <andymagee@google.com>
2024-03-14 09:57:51 -0700

    Convert LightDataBindingComponentClass to kt (2/3)
    Automated conversion by IDE + ktfmt
    
    Bug: n/a
    Test: n/a
    Change-Id: Ia546100881ad2db266b3b7c22b49625bcb8516ef
    
    GitOrigin-RevId: 6fe320de0672de550374ad74004e1a9295271d3a

--- databinding/src/com/android/tools/idea/databinding/psiclass/LightDataBindingComponentClass.kt
+++ databinding/src/com/android/tools/idea/databinding/psiclass/LightDataBindingComponentClass.kt
@@ -13,53 +13,45 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.android.tools.idea.databinding.psiclass;
+package com.android.tools.idea.databinding.psiclass
 
-import com.android.SdkConstants;
-import com.android.tools.idea.AndroidPsiUtils;
-import com.android.tools.idea.databinding.DataBindingMode;
-import com.android.tools.idea.databinding.module.LayoutBindingModuleCache;
-import com.android.tools.idea.projectsystem.ProjectSystemUtil;
-import com.android.tools.idea.projectsystem.ScopeType;
-import com.google.common.collect.ImmutableSet;
-import com.google.common.collect.Iterables;
-import com.google.common.collect.Maps;
-import com.intellij.lang.java.JavaLanguage;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.util.AtomicNotNullLazyValue;
-import com.intellij.openapi.util.ModificationTracker;
-import com.intellij.psi.JavaPsiFacade;
-import com.intellij.psi.PsiClass;
-import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiElementFactory;
-import com.intellij.psi.PsiField;
-import com.intellij.psi.PsiFile;
-import com.intellij.psi.PsiFileFactory;
-import com.intellij.psi.PsiIdentifier;
-import com.intellij.psi.PsiManager;
-import com.intellij.psi.PsiMethod;
-import com.intellij.psi.PsiModifier;
-import com.intellij.psi.PsiModifierListOwner;
-import com.intellij.psi.PsiType;
-import com.intellij.psi.impl.light.LightIdentifier;
-import com.intellij.psi.impl.light.LightMethod;
-import com.intellij.psi.search.GlobalSearchScope;
-import com.intellij.psi.search.searches.AnnotatedElementsSearch;
-import com.intellij.psi.util.CachedValue;
-import com.intellij.psi.util.CachedValueProvider;
-import com.intellij.psi.util.CachedValuesManager;
-import com.intellij.psi.util.PsiUtil;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.TreeSet;
-import org.jetbrains.android.augment.AndroidLightClassBase;
-import org.jetbrains.android.facet.AndroidFacet;
-import org.jetbrains.annotations.NonNls;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import com.android.SdkConstants
+import com.android.tools.idea.AndroidPsiUtils
+import com.android.tools.idea.databinding.module.LayoutBindingModuleCache.Companion.getInstance
+import com.android.tools.idea.projectsystem.ScopeType
+import com.android.tools.idea.projectsystem.getModuleSystem
+import com.google.common.collect.ImmutableSet
+import com.google.common.collect.Iterables
+import com.google.common.collect.Maps
+import com.intellij.lang.java.JavaLanguage
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.AtomicNotNullLazyValue
+import com.intellij.openapi.util.ModificationTracker
+import com.intellij.psi.JavaPsiFacade
+import com.intellij.psi.PsiClass
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiElementFactory
+import com.intellij.psi.PsiField
+import com.intellij.psi.PsiFile
+import com.intellij.psi.PsiFileFactory
+import com.intellij.psi.PsiIdentifier
+import com.intellij.psi.PsiManager
+import com.intellij.psi.PsiMethod
+import com.intellij.psi.PsiModifier
+import com.intellij.psi.PsiModifierListOwner
+import com.intellij.psi.PsiType
+import com.intellij.psi.impl.light.LightIdentifier
+import com.intellij.psi.impl.light.LightMethod
+import com.intellij.psi.search.GlobalSearchScope
+import com.intellij.psi.search.searches.AnnotatedElementsSearch
+import com.intellij.psi.util.CachedValue
+import com.intellij.psi.util.CachedValueProvider
+import com.intellij.psi.util.CachedValuesManager
+import com.intellij.psi.util.PsiUtil
+import java.util.TreeSet
+import org.jetbrains.android.augment.AndroidLightClassBase
+import org.jetbrains.android.facet.AndroidFacet
+import org.jetbrains.annotations.NonNls
 
 /**
  * In-memory PSI for the generated DataBindingComponent class.
@@ -71,185 +63,187 @@ import org.jetbrains.annotations.Nullable;
  *
  * See also: https://developer.android.com/reference/android/databinding/DataBindingComponent
  */
-public class LightDataBindingComponentClass extends AndroidLightClassBase implements ModificationTracker {
-  private final AndroidFacet myFacet;
-  private final CachedValue<PsiMethod[]> myMethodCache;
-  private final AtomicNotNullLazyValue<PsiFile> myContainingFile;
-  private final DataBindingMode myMode;
+class LightDataBindingComponentClass(psiManager: PsiManager, private val myFacet: AndroidFacet) :
+  AndroidLightClassBase(psiManager, ImmutableSet.of<String>(PsiModifier.PUBLIC)),
+  ModificationTracker {
+  private val myMethodCache: CachedValue<Array<PsiMethod>>
+  private val myContainingFile: AtomicNotNullLazyValue<PsiFile>
+  private val myMode = getInstance(myFacet).dataBindingMode
 
-  public LightDataBindingComponentClass(@NotNull PsiManager psiManager, final AndroidFacet facet) {
-    super(psiManager, ImmutableSet.of(PsiModifier.PUBLIC));
-    myMode = LayoutBindingModuleCache.getInstance(facet).getDataBindingMode();
-    myFacet = facet;
-
-    Project project = facet.getModule().getProject();
-    ModificationTracker modificationTracker = AndroidPsiUtils.getPsiModificationTrackerIgnoringXml(project);
+  init {
+    val project = myFacet.module.project
+    val modificationTracker = AndroidPsiUtils.getPsiModificationTrackerIgnoringXml(project)
 
     myMethodCache =
-      CachedValuesManager.getManager(project).createCachedValue(
-        () -> {
-          Map<String, Set<String>> instanceAdapterClasses = Maps.newHashMap();
-          JavaPsiFacade facade = JavaPsiFacade.getInstance(myFacet.getModule().getProject());
-          GlobalSearchScope moduleScope = ProjectSystemUtil.getModuleSystem(myFacet).getResolveScope(ScopeType.MAIN);
-          PsiClass aClass = facade.findClass(myMode.bindingAdapter, moduleScope);
-          if (aClass == null) {
-            return CachedValueProvider.Result.create(PsiMethod.EMPTY_ARRAY, modificationTracker);
-          }
+      CachedValuesManager.getManager(project)
+        .createCachedValue<Array<PsiMethod>>(
+          CachedValueProvider<Array<PsiMethod?>> {
+            val instanceAdapterClasses: MutableMap<String, MutableSet<String?>> = Maps.newHashMap()
+            val facade = JavaPsiFacade.getInstance(myFacet.module.project)
+            val moduleScope = myFacet.getModuleSystem().getResolveScope(ScopeType.MAIN)
+            val aClass =
+              facade.findClass(myMode.bindingAdapter, moduleScope)
+                ?: return@createCachedValue CachedValueProvider.Result.create<Array<PsiMethod>>(
+                  PsiMethod.EMPTY_ARRAY,
+                  modificationTracker,
+                )
 
-          @SuppressWarnings("unchecked")
-          final Collection<? extends PsiModifierListOwner> psiElements =
-            AnnotatedElementsSearch.searchElements(aClass, myFacet.getModule().getModuleScope(), PsiMethod.class).findAll();
-          int methodCount = 0;
+            val psiElements: Collection<PsiModifierListOwner> =
+              AnnotatedElementsSearch.searchElements(
+                  aClass,
+                  myFacet.module.moduleScope,
+                  PsiMethod::class.java,
+                )
+                .findAll()
+            var methodCount = 0
 
-          for (PsiModifierListOwner owner : psiElements) {
-            if (owner instanceof PsiMethod && !owner.hasModifierProperty(PsiModifier.STATIC)) {
-              PsiClass containingClass = ((PsiMethod)owner).getContainingClass();
-              if (containingClass == null) {
-                continue;
-              }
-              String className = containingClass.getName();
-              Set<String> set = instanceAdapterClasses.get(className);
-              if (set == null) {
-                set = new TreeSet<>();
-                instanceAdapterClasses.put(className, set);
-              }
-              if (set.add(containingClass.getQualifiedName())) {
-                methodCount++;
+            for (owner in psiElements) {
+              if (owner is PsiMethod && !owner.hasModifierProperty(PsiModifier.STATIC)) {
+                val containingClass = owner.containingClass ?: continue
+                val className = containingClass.name!!
+                var set = instanceAdapterClasses[className]
+                if (set == null) {
+                  set = TreeSet()
+                  instanceAdapterClasses[className] = set
+                }
+                if (set.add(containingClass.qualifiedName)) {
+                  methodCount++
+                }
               }
             }
-          }
-          if (methodCount == 0) {
-            return CachedValueProvider.Result.create(PsiMethod.EMPTY_ARRAY, modificationTracker);
-          }
-          PsiElementFactory elementFactory = PsiElementFactory.getInstance(project);
-          PsiMethod[] result = new PsiMethod[methodCount];
-          int methodIndex = 0;
-          GlobalSearchScope scope = GlobalSearchScope.allScope(project);
-          for (Map.Entry<String, Set<String>> methods : instanceAdapterClasses.entrySet()) {
-            if (methods.getValue().size() == 1) {
-              result[methodIndex] =
-                createPsiMethod(elementFactory, "get" + methods.getKey(), Iterables.getFirst(methods.getValue(), ""), project, scope);
-              methodIndex ++;
+            if (methodCount == 0) {
+              return@createCachedValue CachedValueProvider.Result.create<Array<PsiMethod>>(
+                PsiMethod.EMPTY_ARRAY,
+                modificationTracker,
+              )
             }
-            else {
-              int suffix = 1;
-              for (String item : methods.getValue()) {
-                final String name = "get" + methods.getKey() + suffix;
-                result[methodIndex] = createPsiMethod(elementFactory, name, item, project, scope);
-                suffix++;
-                methodIndex ++;
+            val elementFactory = PsiElementFactory.getInstance(project)
+            val result = arrayOfNulls<PsiMethod>(methodCount)
+            var methodIndex = 0
+            val scope = GlobalSearchScope.allScope(project)
+            for ((key, value) in instanceAdapterClasses) {
+              if (value.size == 1) {
+                result[methodIndex] =
+                  createPsiMethod(
+                    elementFactory,
+                    "get$key",
+                    Iterables.getFirst(value, ""),
+                    project,
+                    scope,
+                  )
+                methodIndex++
+              } else {
+                var suffix = 1
+                for (item in value) {
+                  val name = "get$key$suffix"
+                  result[methodIndex] = createPsiMethod(elementFactory, name, item, project, scope)
+                  suffix++
+                  methodIndex++
+                }
               }
             }
-          }
-          return CachedValueProvider.Result.create(result, modificationTracker);
+            CachedValueProvider.Result.create<Array<PsiMethod?>>(result, modificationTracker)
+          },
+          false,
+        )
+
+    myContainingFile =
+      AtomicNotNullLazyValue.createValue {
+        var packageName = myMode.packageName
+        if (packageName.endsWith(".")) {
+          packageName = packageName.substring(0, packageName.length - 1)
         }
-      , false);
-
-    myContainingFile = AtomicNotNullLazyValue.createValue(() -> {
-      String packageName = myMode.packageName;
-      if (packageName.endsWith(".")) {
-        packageName = packageName.substring(0, packageName.length() - 1);
+        PsiFileFactory.getInstance(myFacet.module.project)
+          .createFileFromText(
+            SdkConstants.CLASS_NAME_DATA_BINDING_COMPONENT + ".java",
+            JavaLanguage.INSTANCE,
+            """
+          package $packageName;
+          public interface DataBindingComponent {}
+          """
+              .trimIndent(),
+            false,
+            true,
+            true,
+          )
       }
-      return PsiFileFactory.getInstance(myFacet.getModule().getProject()).createFileFromText(
-        SdkConstants.CLASS_NAME_DATA_BINDING_COMPONENT + ".java", JavaLanguage.INSTANCE,
-        "package " + packageName + ";\n"
-        + "public interface DataBindingComponent {}"
-        , false, true, true);
-    });
 
-    setModuleInfo(facet.getModule(), false);
+    setModuleInfo(myFacet.module, false)
   }
 
-  @Override
-  public boolean isInterface() {
-    return true;
+  override fun isInterface(): Boolean {
+    return true
   }
 
-  @Override
-  public boolean isAnnotationType() {
-    return false;
+  override fun isAnnotationType(): Boolean {
+    return false
   }
 
-  private PsiMethod createPsiMethod(PsiElementFactory factory, String name, String type, Project project, GlobalSearchScope scope) {
-    PsiMethod method = factory.createMethod(name, PsiType.getTypeByName(type, project, scope));
-    PsiUtil.setModifierProperty(method, PsiModifier.PUBLIC, true);
-    PsiUtil.setModifierProperty(method, PsiModifier.ABSTRACT, true);
-    return new LightMethod(PsiManager.getInstance(project), method, this);
+  private fun createPsiMethod(
+    factory: PsiElementFactory,
+    name: String,
+    type: String?,
+    project: Project,
+    scope: GlobalSearchScope,
+  ): PsiMethod {
+    val method = factory.createMethod(name, PsiType.getTypeByName(type!!, project, scope))
+    PsiUtil.setModifierProperty(method, PsiModifier.PUBLIC, true)
+    PsiUtil.setModifierProperty(method, PsiModifier.ABSTRACT, true)
+    return LightMethod(PsiManager.getInstance(project), method, this)
   }
 
-  @Nullable
-  @Override
-  public String getQualifiedName() {
-    return myMode.dataBindingComponent;
+  override fun getQualifiedName(): String? {
+    return myMode.dataBindingComponent
   }
 
-  @Override
-  public String getName() {
-    return SdkConstants.CLASS_NAME_DATA_BINDING_COMPONENT;
+  override fun getName(): String? {
+    return SdkConstants.CLASS_NAME_DATA_BINDING_COMPONENT
   }
 
-  @Nullable
-  @Override
-  public PsiClass getContainingClass() {
-    return null;
+  override fun getContainingClass(): PsiClass? {
+    return null
   }
 
-  @NotNull
-  @Override
-  public PsiField[] getFields() {
-    return PsiField.EMPTY_ARRAY;
+  override fun getFields(): Array<PsiField> {
+    return PsiField.EMPTY_ARRAY
   }
 
-  @NotNull
-  @Override
-  public PsiField[] getAllFields() {
-    return getFields();
+  override fun getAllFields(): Array<PsiField> {
+    return fields
   }
 
-  @NotNull
-  @Override
-  public PsiMethod[] getMethods() {
-    return myMethodCache.getValue();
+  override fun getMethods(): Array<PsiMethod> {
+    return myMethodCache.value
   }
 
-  @NotNull
-  @Override
-  public PsiMethod[] getAllMethods() {
-    return getMethods();
+  override fun getAllMethods(): Array<PsiMethod> {
+    return methods
   }
 
-  @NotNull
-  @Override
-  public PsiMethod[] findMethodsByName(@NonNls String name, boolean checkBases) {
-    List<PsiMethod> result = new ArrayList<>();
-    for (PsiMethod method : myMethodCache.getValue()) {
-      if (method.getName().equals(name)) {
-        result.add(method);
+  override fun findMethodsByName(name: @NonNls String?, checkBases: Boolean): Array<PsiMethod> {
+    val result: MutableList<PsiMethod> = ArrayList()
+    for (method in myMethodCache.value) {
+      if (method.name == name) {
+        result.add(method)
       }
     }
-    return result.isEmpty() ? PsiMethod.EMPTY_ARRAY : result.toArray(PsiMethod.EMPTY_ARRAY);
+    return if (result.isEmpty()) PsiMethod.EMPTY_ARRAY else result.toArray(PsiMethod.EMPTY_ARRAY)
   }
 
-  @Nullable
-  @Override
-  public PsiFile getContainingFile() {
-    return myContainingFile.getValue();
+  override fun getContainingFile(): PsiFile? {
+    return myContainingFile.value
   }
 
-  @Override
-  public PsiIdentifier getNameIdentifier() {
-    return new LightIdentifier(getManager(), getName());
+  override fun getNameIdentifier(): PsiIdentifier? {
+    return LightIdentifier(manager, name)
   }
 
-  @NotNull
-  @Override
-  public PsiElement getNavigationElement() {
-    PsiFile containingFile = getContainingFile();
-    return containingFile == null ? super.getNavigationElement() : containingFile;
+  override fun getNavigationElement(): PsiElement {
+    val containingFile = containingFile
+    return containingFile ?: super.getNavigationElement()
   }
 
-  @Override
-  public long getModificationCount() {
-    return 0;
+  override fun getModificationCount(): Long {
+    return 0
   }
 }
