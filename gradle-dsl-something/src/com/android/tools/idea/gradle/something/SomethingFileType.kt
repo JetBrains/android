a4083afea94db65f9fb5437673e7f19cb3ce0265
Christophe Rhodes <xof@google.com>
2024-03-14 09:24:34 +0000

    Adjust declared extension for Something files
    Use just bare `something`, as the platform treats the extension as
    what follows the final dot, not simply a (delimited) suffix.
    
    Now `.something` files have the (Something) Gradle Declarative language
    associated in the editor.
    
    Bug: 328257084
    Bug: 329227053
    Test: manual
    Change-Id: I6fb63d2364b2b54f7424813fa509d1db9ab26e93
    
    GitOrigin-RevId: 6d2f8c507b59b6dc40a423ebf2f9b754ee8058eb

--- gradle-dsl-something/src/com/android/tools/idea/gradle/something/SomethingFileType.kt
+++ gradle-dsl-something/src/com/android/tools/idea/gradle/something/SomethingFileType.kt
@@ -22,7 +22,7 @@ import javax.swing.Icon
 class SomethingFileType : LanguageFileType(SomethingLanguage.INSTANCE) {
   override fun getName(): String = "Something (Gradle Declarative)"
   override fun getDescription(): String = "Something (Gradle Declarative Build DSL)"
-  override fun getDefaultExtension(): String = "gradle.something"
+  override fun getDefaultExtension(): String = "something"
   override fun getIcon(): Icon = GradleIcons.GradleFile
 
   companion object {
