0f4be4afac43bd8f067699b461e44673de729a91
Sebastiano Poggi <sebp@google.com>
2024-03-18 12:25:39 +0100

    Embed a copy of Inter for use in Jewel tests
    This fixes the "missing font" issues we've been having on some platforms
    
    Bug: none
    Test: n/a
    Change-Id: Ia63618748e98fdf93146e8fe7ddf89706503868b
    
    GitOrigin-RevId: 8af3727dbbacfcca5668b16976ca93000609f2f6

--- adt-ui-compose/testSrc/com/android/tools/adtui/compose/JewelTestUtil.kt
+++ adt-ui-compose/testSrc/com/android/tools/adtui/compose/JewelTestUtil.kt
@@ -16,21 +16,43 @@
 package com.android.tools.adtui.compose
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.platform.Font
 import org.jetbrains.jewel.foundation.theme.JewelTheme
 import org.jetbrains.jewel.intui.standalone.theme.IntUiTheme
 import org.jetbrains.jewel.intui.standalone.theme.dark
 import org.jetbrains.jewel.intui.standalone.theme.darkThemeDefinition
+import org.jetbrains.jewel.intui.standalone.theme.defaultTextStyle
 import org.jetbrains.jewel.intui.standalone.theme.light
 import org.jetbrains.jewel.intui.standalone.theme.lightThemeDefinition
 import org.jetbrains.jewel.ui.ComponentStyling
 
+@Suppress("TestFunctionName") // It's a composable
 @Composable
 fun JewelTestTheme(darkMode: Boolean = false, content: @Composable () -> Unit) {
+  val fontFamily =
+    FontFamily(
+      Font(resource = "fonts/inter/Inter-Thin.ttf", weight = FontWeight.Thin),
+      Font(resource = "fonts/inter/Inter-ThinItalic.ttf", weight = FontWeight.Thin, style = FontStyle.Italic),
+      Font(resource = "fonts/inter/Inter-ExtraLight.ttf", weight = FontWeight.ExtraLight),
+      Font(resource = "fonts/inter/Inter-Light.ttf", weight = FontWeight.Light),
+      Font(resource = "fonts/inter/Inter-Regular.ttf", weight = FontWeight.Normal),
+      Font(resource = "fonts/inter/Inter-Medium.ttf", weight = FontWeight.Medium),
+      Font(resource = "fonts/inter/Inter-SemiBold.ttf", weight = FontWeight.SemiBold),
+      Font(resource = "fonts/inter/Inter-Bold.ttf", weight = FontWeight.Bold),
+      Font(resource = "fonts/inter/Inter-ExtraBold.ttf", weight = FontWeight.ExtraBold),
+      Font(resource = "fonts/inter/Inter-Black.ttf", weight = FontWeight.Black),
+    )
+
+  val textStyle = JewelTheme.defaultTextStyle.copy(fontFamily = fontFamily)
+
   val themeDefinition =
     if (darkMode) {
-      JewelTheme.darkThemeDefinition()
+      JewelTheme.darkThemeDefinition(defaultTextStyle = textStyle)
     } else {
-      JewelTheme.lightThemeDefinition()
+      JewelTheme.lightThemeDefinition(defaultTextStyle = textStyle)
     }
 
   val componentStyling =
