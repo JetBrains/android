2225448b6d7bff13b8522298b1dccfb4b5166050
Diego Perez <diegoperez@google.com>
2024-03-22 09:16:29 +0000

    Add new background scene generation
    Scene generation currently has a number of cases that require blocking
    operations (like obtaining Configuration or ResourceResolver). This
    change moves the display list building to a background thread.
    This also adds a flag that allows reverting to the previous behaviour.
    
    Test: Existing and manual
    Fixes: 357609848
    Fixes: 321676532
    Change-Id: I53c82859b5998229e8f0f9ba21627bd698939c16
    
    GitOrigin-RevId: 6f194a2b0b4666c82459c9811079e6ea456b8450

--- designer/src/com/android/tools/idea/common/scene/SyncDisplay.java
+++ designer/src/com/android/tools/idea/common/scene/SyncDisplay.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017 The Android Open Source Project
+ * Copyright (C) 2024 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,24 +15,27 @@
  */
 package com.android.tools.idea.common.scene;
 
+import com.android.annotations.concurrency.UiThread;
 import com.android.tools.idea.common.scene.draw.DisplayList;
 import com.android.tools.idea.common.surface.DesignSurface;
+import java.awt.Graphics2D;
 import org.jetbrains.annotations.NotNull;
-
-import java.awt.*;
 
 /**
  * Display a layout Scene
  */
-public class Display {
+@Deprecated // Use AsyncDisplay instead
+class SyncDisplay implements Display {
   private final DisplayList myDisplayList = new DisplayList();
   private long myDisplayListVersion = 0;
   double myScale = 0;
 
-  public void reLayout() {
+  @Override
+  public void relayout() {
     myDisplayListVersion = 0;
   }
 
+  @UiThread
   public void draw(@NotNull SceneContext sceneContext, @NotNull Graphics2D g, @NotNull Scene scene) {
     long mTime = System.currentTimeMillis();
     boolean needsRebuild = scene.getDisplayListVersion() > myDisplayListVersion;
@@ -63,5 +66,10 @@ public class Display {
   public void draw(@NotNull SceneContext sceneContext, @NotNull Graphics2D g, @NotNull DisplayList list) {
     sceneContext.setTime(System.currentTimeMillis());
     list.paint(g, sceneContext);
+  }
+
+  @Override
+  public boolean hasPendingPaints() {
+    return false;
   }
 }
