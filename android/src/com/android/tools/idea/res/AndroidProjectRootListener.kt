70afff919b71de032fdfc5a523e1e86c7791dd30
Andy Magee <andymagee@google.com>
2024-04-05 15:04:08 -0700

    Convert AndroidProjectRootListener to kt (2/3)
    Automated conversion by IDE and ktfmt
    
    Bug: n/a
    Test: n/a
    Change-Id: I28e97803c884f269c0cfd871b27c074874c8fec7
    
    GitOrigin-RevId: 5897a2d21ce0e5c04e0feda1e89108d40a19f9a5

--- android/src/com/android/tools/idea/res/AndroidProjectRootListener.kt
+++ android/src/com/android/tools/idea/res/AndroidProjectRootListener.kt
@@ -13,122 +13,124 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.android.tools.idea.res;
+package com.android.tools.idea.res
 
-import com.android.tools.idea.model.AndroidModel;
-import com.android.tools.idea.projectsystem.ProjectSystemSyncManager.SyncResult;
-import com.android.tools.idea.projectsystem.ProjectSystemSyncManager.SyncResultListener;
-import com.android.tools.idea.projectsystem.ProjectSystemSyncUtil;
-import com.intellij.openapi.Disposable;
-import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.application.ReadAction;
-import com.intellij.openapi.module.Module;
-import com.intellij.openapi.module.ModuleManager;
-import com.intellij.openapi.progress.ProgressIndicator;
-import com.intellij.openapi.project.DumbModeTask;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.roots.ModuleRootEvent;
-import com.intellij.openapi.roots.ModuleRootListener;
-import com.intellij.openapi.util.Disposer;
-import com.intellij.util.messages.MessageBusConnection;
-import org.jetbrains.android.facet.AndroidFacet;
-import org.jetbrains.android.facet.ResourceFolderManager;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import com.android.tools.idea.model.AndroidModel
+import com.android.tools.idea.projectsystem.ProjectSystemSyncManager
+import com.intellij.openapi.Disposable
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.application.ReadAction
+import com.intellij.openapi.module.Module
+import com.intellij.openapi.module.ModuleManager
+import com.intellij.openapi.progress.ProgressIndicator
+import com.intellij.openapi.project.DumbModeTask
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.roots.ModuleRootEvent
+import com.intellij.openapi.roots.ModuleRootListener
+import com.intellij.openapi.util.Disposer
+import org.jetbrains.android.facet.AndroidFacet
+import org.jetbrains.android.facet.ResourceFolderManager
+import org.jetbrains.android.facet.ResourceFolderManager.Companion.getInstance
 
 /**
- * Service that subscribes to project root changes in order to invalidate {@link AndroidDependenciesCache},
- * the {@link ResourceFolderManager} cache, and to update resource repositories.
+ * Service that subscribes to project root changes in order to invalidate
+ * [AndroidDependenciesCache], the [ResourceFolderManager] cache, and to update resource
+ * repositories.
  */
-public class AndroidProjectRootListener implements Disposable.Default {
-  /**
-   * Makes AndroidProjectRootListener listen to the {@link ModuleRootListener#TOPIC} events if it has not been listening already.
-   *
-   * @param project the project to listen on
-   */
-  public static void ensureSubscribed(@NotNull Project project) {
-    project.getService(AndroidProjectRootListener.class);
+class AndroidProjectRootListener private constructor(project: Project) : Disposable.Default {
+  init {
+    val messageBusConnection = project.messageBus.connect(this)
+
+    messageBusConnection.subscribe(
+      ModuleRootListener.TOPIC,
+      object : ModuleRootListener {
+        override fun rootsChanged(event: ModuleRootEvent) {
+          moduleRootsOrDependenciesChanged(project, this@AndroidProjectRootListener)
+        }
+      },
+    )
+
+    messageBusConnection.subscribe<ProjectSystemSyncManager.SyncResultListener>(
+      PROJECT_SYSTEM_SYNC_TOPIC,
+      ProjectSystemSyncManager
+        .SyncResultListener { // This event is called on the EDT. Calling
+                              // `moduleRootsOrDependenciesChanged` directly ends up executing the
+                              // DumbModeTask
+          // synchronously, which has leads to failures due to the state we're in from higher up the
+          // stack. Executing this on the EDT later
+          // avoids that situation.
+          ApplicationManager.getApplication().invokeLater {
+            moduleRootsOrDependenciesChanged(project, this@AndroidProjectRootListener)
+          }
+        },
+    )
   }
 
-  private AndroidProjectRootListener(@NotNull Project project) {
-    MessageBusConnection messageBusConnection = project.getMessageBus().connect(this);
-
-    messageBusConnection.subscribe(ModuleRootListener.TOPIC, new ModuleRootListener() {
-      @Override
-      public void rootsChanged(@NotNull ModuleRootEvent event) {
-        moduleRootsOrDependenciesChanged(project, AndroidProjectRootListener.this);
-      }
-    });
-
-    messageBusConnection.subscribe(ProjectSystemSyncUtil.PROJECT_SYSTEM_SYNC_TOPIC, new SyncResultListener() {
-      @Override
-      public void syncEnded(@NotNull SyncResult result) {
-        // This event is called on the EDT. Calling `moduleRootsOrDependenciesChanged` directly ends up executing the DumbModeTask
-        // synchronously, which has leads to failures due to the state we're in from higher up the stack. Executing this on the EDT later
-        // avoids that situation.
-        ApplicationManager.getApplication().invokeLater(
-          () -> moduleRootsOrDependenciesChanged(project, AndroidProjectRootListener.this));
-      }
-    });
-  }
-
-  /**
-   * Called when module roots have changed in the given project.
-   *
-   * @param project the project whose module roots changed
-   */
-  private static void moduleRootsOrDependenciesChanged(@NotNull Project project, Disposable parentDisposable) {
-    ReadAction.run(() -> {
-      if (!project.isDisposed()) {
-        new MyDumbModeTask(project, parentDisposable).queue(project);
-      }
-    });
-  }
-
-  /**
-   * Called when module roots have changed in the given module.
-   *
-   * @param module the module whose roots changed
-   */
-  private static void moduleRootsOrDependenciesChanged(@NotNull Module module) {
-    AndroidFacet facet = AndroidFacet.getInstance(module);
-    if (facet != null) {
-      if (AndroidModel.isRequired(facet) && AndroidModel.get(facet) == null) {
-        // Project not yet fully initialized. No need to do a sync now because our
-        // GradleProjectAvailableListener will be called as soon as it is and do a proper sync.
-        return;
-      }
-
-      AndroidDependenciesCache.getInstance(module).dropCache();
-      ResourceFolderManager.getInstance(facet).checkForChanges();
-      StudioResourceRepositoryManager.getInstance(facet).updateRootsAndLibraries();
-    }
-  }
-
-  private static class MyDumbModeTask extends DumbModeTask {
-    private final @NotNull Project myProject;
-
-    private MyDumbModeTask(@NotNull Project project, @NotNull Disposable parent) {
-      myProject = project;
-      Disposer.register(parent, this);
+  private class MyDumbModeTask(private val myProject: Project, parent: Disposable) :
+    DumbModeTask() {
+    init {
+      Disposer.register(parent, this)
     }
 
-    @Override
-    public void performInDumbMode(@NotNull ProgressIndicator indicator) {
-      if (!myProject.isDisposed()) {
-        indicator.setText("Updating resource repository roots");
-        ModuleManager moduleManager = ModuleManager.getInstance(myProject);
-        for (Module module : moduleManager.getModules()) {
-          moduleRootsOrDependenciesChanged(module);
+    override fun performInDumbMode(indicator: ProgressIndicator) {
+      if (!myProject.isDisposed) {
+        indicator.text = "Updating resource repository roots"
+        val moduleManager: ModuleManager = getInstance.getInstance(myProject)
+        for (module in moduleManager.modules) {
+          moduleRootsOrDependenciesChanged(module)
         }
       }
     }
 
-    @Nullable
-    @Override
-    public DumbModeTask tryMergeWith(@NotNull DumbModeTask taskFromQueue) {
-      if (taskFromQueue instanceof MyDumbModeTask && ((MyDumbModeTask)taskFromQueue).myProject.equals(myProject)) return this;
-      return null;
+    override fun tryMergeWith(taskFromQueue: DumbModeTask): DumbModeTask? {
+      if ((taskFromQueue is MyDumbModeTask) && taskFromQueue.myProject == myProject) return this
+      return null
+    }
+  }
+
+  companion object {
+    /**
+     * Makes AndroidProjectRootListener listen to the [ModuleRootListener.TOPIC] events if it has
+     * not been listening already.
+     *
+     * @param project the project to listen on
+     */
+    @JvmStatic
+    fun ensureSubscribed(project: Project) {
+      project.getService(AndroidProjectRootListener::class.java)
+    }
+
+    /**
+     * Called when module roots have changed in the given project.
+     *
+     * @param project the project whose module roots changed
+     */
+    private fun moduleRootsOrDependenciesChanged(project: Project, parentDisposable: Disposable) {
+      ReadAction.run<RuntimeException> {
+        if (!project.isDisposed) {
+          MyDumbModeTask(project, parentDisposable).queue(project)
+        }
+      }
+    }
+
+    /**
+     * Called when module roots have changed in the given module.
+     *
+     * @param module the module whose roots changed
+     */
+    private fun moduleRootsOrDependenciesChanged(module: Module) {
+      val facet = AndroidFacet.getInstance(module)
+      if (facet != null) {
+        if (AndroidModel.isRequired(facet) && AndroidModel.get(facet) == null) {
+          // Project not yet fully initialized. No need to do a sync now because our
+          // GradleProjectAvailableListener will be called as soon as it is and do a proper sync.
+          return
+        }
+
+        AndroidDependenciesCache.getInstance(module).dropCache()
+        getInstance(facet).checkForChanges()
+        StudioResourceRepositoryManager.getInstance(facet).updateRootsAndLibraries()
+      }
     }
   }
 }
