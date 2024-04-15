3f8f9bc523d586fba469a09bb51deb04be3816bf
Kun Shen <kunshen@google.com>
2024-04-15 11:07:51 -0700

    [new combined login] Add a feature flag
    Bug: 334152239
    Test: manual
    Change-Id: I2e4f33281714ec8148e435e70a97a1eba616a760
    
    GitOrigin-RevId: ab351a6ce34aba731edf984cce23bb39f33328e4

--- android-common/src/com/android/tools/idea/flags/StudioFlags.java
+++ android-common/src/com/android/tools/idea/flags/StudioFlags.java
@@ -1943,6 +1943,9 @@ public final class StudioFlags {
   public static final Flag<Boolean> ENABLE_SETTINGS_ACCOUNT_UI =
     new BooleanFlag(GOOGLE_LOGIN, "enabled", "Enable new login settings UI",
                 "When enabled, a login settings page will replace the popup from the login action in the top right.", true);
+  public static final Flag<Boolean> ENABLE_COMBINED_LOGIN_UI =
+    new BooleanFlag(GOOGLE_LOGIN, "combined.login.enabled", "Enable combined login",
+                    "When enabled, a combined login page will show when logging in for a new user.", false);
   // endregion GOOGLE_LOGIN
 
   public static Boolean isBuildOutputShowsDownloadInfo() {
