7ccb87eee4cac988ecd5a5048154d3c98b41dfa8
Jens Ole Lauridsen <jlauridsen@google.com>
2024-04-16 11:11:32 +0200

    Reenable SkiaParserTest
    Disable this test on Mac since it requires a manual accept in the
    security settings. There is now a flag that can be enabled to run
    the tests locally on Mac.
    
    Fixes: 329646345
    Test: SkiaParserTest
    Change-Id: I0f79c73fe66221833e0e3e43bde8ae6f0607b445
    
    GitOrigin-RevId: 8e2aefe3250382890dd00c88fc7a350b35ec4761

--- layout-inspector/testSrc/com/android/tools/idea/layoutinspector/skia/SkiaParserTest.kt
+++ layout-inspector/testSrc/com/android/tools/idea/layoutinspector/skia/SkiaParserTest.kt
@@ -15,12 +15,12 @@
  */
 package com.android.tools.idea.layoutinspector.skia
 
-import com.android.flags.junit.FlagRule
+import com.android.SdkConstants
+import com.android.testutils.AssumeUtil.assumeNotMac
 import com.android.testutils.ImageDiffUtil
 import com.android.testutils.MockitoKt.mock
 import com.android.testutils.TestUtils
 import com.android.tools.idea.FakeSdkRule
-import com.android.tools.idea.flags.StudioFlags
 import com.android.tools.idea.layoutinspector.proto.SkiaParser.InspectorView
 import com.android.tools.idea.layoutinspector.proto.SkiaParser.RequestedNodeInfo
 import com.android.tools.idea.protobuf.ByteString
@@ -32,12 +32,12 @@ import com.google.common.truth.Truth.assertThat
 import java.awt.image.BufferedImage
 import kotlin.test.assertTrue
 import org.junit.Assert.fail
-import org.junit.Ignore
 import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.RuleChain
 
 private const val TEST_DATA_PATH = "tools/adt/idea/layout-inspector/testData"
+private const val RUN_MAC_INTEGRATION_TESTS = false
 
 // TODO(152816022): testBuildTree
 class SkiaParserTest {
@@ -146,13 +146,11 @@ class SkiaParserWithSdkTest {
   }
 }
 
-@Ignore("b/329646345")
 class SkiaParserIntegrationTest {
-  @get:Rule
-  val flagRule = FlagRule(StudioFlags.DYNAMIC_LAYOUT_INSPECTOR_USE_DEVBUILD_SKIA_SERVER, true)
 
   @Test
   fun testRunServer() {
+    if (!RUN_MAC_INTEGRATION_TESTS) assumeNotMac()
     val server = SkiaParserServerConnection(mock())
     // Call createGrpcClient directly to skip running the server binary
     val port = server.createGrpcClient()
@@ -203,6 +201,7 @@ class SkiaParserIntegrationTest {
    */
   @Test
   fun testThreadSafety() {
+    if (!RUN_MAC_INTEGRATION_TESTS) assumeNotMac()
     val iterations = 100
     var hasErrors = false
     val parser =
@@ -275,7 +274,14 @@ class SkiaParserIntegrationTest {
 
   companion object {
     init {
-      System.loadLibrary("skiaparser-test")
+      if (
+        RUN_MAC_INTEGRATION_TESTS || SdkConstants.currentPlatform() != SdkConstants.PLATFORM_DARWIN
+      ) {
+        // On Mac the dylib must be manually accepted to run in the "Privacy and Security" settings.
+        // Do not run these test on mac servers.
+        // Set RUN_MAC_INTEGRATION_TESTS to tru to run locally on mac.
+        System.loadLibrary("skiaparser-test")
+      }
     }
 
     @JvmStatic private external fun generateBoxes(): ByteArray
