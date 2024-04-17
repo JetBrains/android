565a3400777bb2699a7200116e838e7718beaa87
Alan Leung <acleung@google.com>
2024-04-16 17:00:55 -0700

    LE: Extend Live ETEs to cover import / UI changes
    1000/1000 Success rate with --config=remote
    
    Fixes: 319303113
    Test: This CL adds test
    Change-Id: Ifa0982b56a39238375e16eb4fbb9db8d83de9daa
    
    GitOrigin-RevId: e97c0850187657cc45475ba51d8a0e1ea7ca8ff9

--- android/integration/testSrc/com/android/tools/idea/LiveEditTest.kt
+++ android/integration/testSrc/com/android/tools/idea/LiveEditTest.kt
@@ -93,9 +93,14 @@ class LiveEditTest {
             waitForLog(".*Before editing.*", 600, TimeUnit.SECONDS);
           }
 
-          val newContents = "Log.i(\"MainActivity\", \"After editing\")\n" +
-                            "Text(text = \"Hello, Live Edit and \$name!\")";
-          studio.editFile(path.toString(), "(?s)// EASILY SEARCHABLE LINE.*?// END SEARCH", newContents)
+          val newContents00 = "import androidx.compose.material.Button"
+          studio.editFile(path.toString(), "(?s)// EASILY SEARCHABLE LINE 00.*?// END SEARCH 00", newContents00)
+
+          val newContents01 = "Button(onClick = {}) {\n" +
+                              "  Log.i(\"MainActivity\", \"After editing\")\n" +
+                              "  Text(text = \"Hello, Live Edit and \$name!\")" +
+                              "}\n"
+          studio.editFile(path.toString(), "(?s)// EASILY SEARCHABLE LINE 01.*?// END SEARCH 01", newContents01)
 
           adb.runCommand("logcat", emulator = emulator) {
             waitForLog(".*After editing.*", 600, TimeUnit.SECONDS);
