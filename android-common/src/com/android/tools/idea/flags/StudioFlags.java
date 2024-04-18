35a44211262788864a9b87cb95daaef870c19d73
Amaury Medeiros <amaurym@google.com>
2024-04-18 15:36:50 +0100

    Create action to generate Compose Previews for composables
    The action builds a prompt that explains an LLM how to create a Compose
    Preview corresponding to a simple composable. Then, it asks the model
    to generate a Compose Preview corresponding to the composable that's
    being right-clicked.
    
    When the model query is received, we show it in a diff view, so users
    can accept it entirely or partially.
    
    Bug: 335418850
    Test: TBD
    Change-Id: I035b9fe78427a4ce78602e950ff8755728da6577
    
    GitOrigin-RevId: 5361b67e36e3b9831dc55d6040e60cd15beef162

--- android-common/src/com/android/tools/idea/flags/StudioFlags.java
+++ android-common/src/com/android/tools/idea/flags/StudioFlags.java
@@ -1385,6 +1385,11 @@ public final class StudioFlags {
     "Enable a Gemini context-menu action that generates sample data for a given Composable function",
     false);
 
+  public static final Flag<Boolean> COMPOSE_PREVIEW_GENERATE_PREVIEW = new BooleanFlag(
+    COMPOSE, "preview.generate.preview.action", "Enable editor action for generating Compose Previews",
+    "Enable a context-menu action that can generate a Compose Previews corresponding to the selected @Composable",
+    false);
+
   public static final Flag<Boolean> COMPOSE_UI_CHECK_MODE = new BooleanFlag(
     COMPOSE, "ui.check.mode", "Enable UI Check mode for Compose preview",
     "Enable UI Check mode in Compose preview for running ATF checks and Visual Linting",
