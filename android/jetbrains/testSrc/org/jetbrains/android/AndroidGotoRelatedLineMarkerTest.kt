869dec57b8c1aeb3c8815f90cfd54c7a468ae795
Alexey Kudravtsev <cdr@intellij.com>
2024-04-18 14:20:38 +0200

    notnull, cleanup
    GitOrigin-RevId: 04cf8a716196d0c05031edc810a44bb5e1a19e38

--- android/jetbrains/testSrc/org/jetbrains/android/AndroidGotoRelatedLineMarkerTest.kt
+++ android/jetbrains/testSrc/org/jetbrains/android/AndroidGotoRelatedLineMarkerTest.kt
@@ -14,6 +14,7 @@ import com.intellij.codeInsight.daemon.impl.DaemonCodeAnalyzerImpl
 import com.intellij.ide.actions.GotoRelatedSymbolAction
 import com.intellij.navigation.GotoRelatedItem
 import com.intellij.openapi.actionSystem.ex.ActionUtil
+import com.intellij.openapi.actionSystem.impl.SimpleDataContext
 import com.intellij.openapi.util.io.FileUtil
 import com.intellij.openapi.vfs.VfsUtil.findFileByIoFile
 import com.intellij.openapi.vfs.VirtualFile
@@ -254,7 +255,7 @@ class AndroidGotoRelatedLineMarkerTest {
     val e = TestActionEvent.createTestEvent(action)
     ActionUtil.performDumbAwareUpdate(action, e, false)
     assertThat(e.presentation.isEnabledAndVisible).isTrue()
-    return GotoRelatedSymbolAction.getItems(myFixture.file, myFixture.editor, null)
+    return GotoRelatedSymbolAction.getItems(myFixture.file, myFixture.editor)
   }
 
   private fun doCheckLineMarkers(expectedTargetFiles: Set<VirtualFile>,
