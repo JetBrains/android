b4da8fc7ac677d7c6fd1ff3eb0b7a297efee3a2a
Juan C Nuno <juancnuno@google.com>
2024-05-06 15:38:51 -0700

    Remove the ABI drop down
    Bug: 335263078
    Test: Not applicable
    Change-Id: Ia1fe43c5fd38a45c4181ceb88e45fbdbc7d01fcd
    
    GitOrigin-RevId: b271b1c96cb4f1feb6c1144fae7badb923a589ef

--- intellij.android.device/src/com/android/tools/idea/adddevicedialog/localavd/DevicePanel.kt
+++ intellij.android.device/src/com/android/tools/idea/adddevicedialog/localavd/DevicePanel.kt
@@ -21,7 +21,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import com.android.sdklib.devices.Abi
 import kotlinx.collections.immutable.ImmutableCollection
 import org.jetbrains.jewel.ui.component.Dropdown
 import org.jetbrains.jewel.ui.component.Text
@@ -33,11 +32,8 @@ internal fun DevicePanel(
   device: VirtualDevice,
   selectedServices: Services?,
   servicesCollection: ImmutableCollection<Services>,
-  selectedAbi: Abi?,
-  abis: ImmutableCollection<Abi>,
   onDeviceChange: (VirtualDevice) -> Unit,
   onSelectedServicesChange: (Services?) -> Unit,
-  onSelectedAbiChange: (Abi?) -> Unit,
 ) {
   Text("Name", Modifier.padding(bottom = Padding.SMALL))
 
@@ -54,16 +50,7 @@ internal fun DevicePanel(
     Modifier.padding(bottom = Padding.SMALL_MEDIUM),
   )
 
-  Row {
-    ServicesDropdown(
-      selectedServices,
-      servicesCollection,
-      onSelectedServicesChange,
-      Modifier.padding(end = Padding.LARGE),
-    )
-
-    AbiDropdown(selectedAbi, abis, onSelectedAbiChange)
-  }
+  ServicesDropdown(selectedServices, servicesCollection, onSelectedServicesChange)
 }
 
 @Composable
@@ -94,40 +81,6 @@ private fun ServicesDropdown(
         },
       ) {
         Text(selectedServices?.toString() ?: "Show All")
-      }
-
-      InfoOutlineIcon(Modifier.align(Alignment.CenterVertically))
-    }
-  }
-}
-
-@Composable
-private fun AbiDropdown(
-  selectedAbi: Abi?,
-  abis: ImmutableCollection<Abi>,
-  onSelectedAbiChange: (Abi?) -> Unit,
-) {
-  Column {
-    Text("ABI", Modifier.padding(bottom = Padding.SMALL))
-
-    Row {
-      Dropdown(
-        Modifier.padding(end = Padding.MEDIUM),
-        menuContent = {
-          abis.forEach {
-            selectableItem(selectedAbi == it, onClick = { onSelectedAbiChange(it) }) {
-              Text(it.toString())
-            }
-          }
-
-          separator()
-
-          selectableItem(selectedAbi == null, onClick = { onSelectedAbiChange(null) }) {
-            Text("Show All")
-          }
-        },
-      ) {
-        Text(selectedAbi?.toString() ?: "Show All")
       }
 
       InfoOutlineIcon(Modifier.align(Alignment.CenterVertically))
