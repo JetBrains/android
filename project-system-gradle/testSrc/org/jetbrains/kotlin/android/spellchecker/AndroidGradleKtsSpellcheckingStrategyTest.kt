285f65564b5f33fbcf9c79b217206c19a87d0ce0
Christophe Rhodes <xof@google.com>
2024-05-09 13:15:02 +0000

    Add a SpellcheckingStrategy for Kotlin Script build files
    By analogy to Groovy build files, only highlight typos in strings if
    they are known to be arguments to print-like functions.
    
    Bug: 337338043
    Test: added
    Change-Id: Ic41b9e1bf7d6190970822945f3effcb49401e5ee
    
    GitOrigin-RevId: 622dfc93905b1d7c99f21df91d4ec7299c5317be

---
+++ project-system-gradle/testSrc/org/jetbrains/kotlin/android/spellchecker/AndroidGradleKtsSpellcheckingStrategyTest.kt
@@ -0,0 +1,62 @@
+/*
+ * Copyright (C) 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jetbrains.kotlin.android.spellchecker
+
+import com.google.common.truth.Truth
+import com.intellij.codeInsight.daemon.ProblemHighlightFilter
+import com.intellij.spellchecker.SpellCheckerSeveritiesProvider
+import com.intellij.spellchecker.inspections.SpellCheckingInspection
+import com.intellij.testFramework.ExtensionTestUtil.maskExtensions
+import org.jetbrains.android.AndroidTestCase
+import org.junit.Before
+
+class AndroidGradleKtsSpellcheckingStrategyTest : AndroidTestCase() {
+  override fun setUp() {
+    super.setUp()
+    maskExtensions(ProblemHighlightFilter.EP_NAME, listOf(), myFixture.projectDisposable)
+  }
+
+  fun testNoTypoInDependencyCallExpression() {
+    myFixture.enableInspections(setOf(SpellCheckingInspection::class.java))
+    val virtualFile = myFixture.addFileToProject(
+      "build.gradle.kts",
+      //language=kotlin
+      """
+        dependencies {
+          implementation("com.example:xyzzy:1.0")
+        }
+      """.trimIndent()).virtualFile
+    myFixture.configureFromExistingVirtualFile(virtualFile)
+    val typos = myFixture.doHighlighting(SpellCheckerSeveritiesProvider.TYPO)
+    Truth.assertThat(typos).isEmpty()
+  }
+
+  fun testTypoInPrintCallExpression() {
+    myFixture.enableInspections(setOf(SpellCheckingInspection::class.java))
+    val virtualFile = myFixture.addFileToProject(
+      "build.gradle.kts",
+      //language=kotlin
+      """
+        dependencies {
+          print("com.example:xyzzy:1.0")
+        }
+      """.trimIndent()).virtualFile
+    myFixture.configureFromExistingVirtualFile(virtualFile)
+    val typos = myFixture.doHighlighting(SpellCheckerSeveritiesProvider.TYPO)
+    Truth.assertThat(typos).hasSize(1)
+    Truth.assertThat(typos[0].description).isEqualTo("Typo: In word 'xyzzy'")
+  }
+}
