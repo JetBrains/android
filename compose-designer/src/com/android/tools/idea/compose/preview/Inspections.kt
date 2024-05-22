e74048ca576d6ba3fd11f2e8f3fed9369fd3fa16
Marco Pennekamp <marco.pennekamp@jetbrains.com>
2024-05-23 00:19:18 +0200

    [kotlin] Adapt to rename of `KaCallableSymbol.callableIdIfNonLocal` to `callableId`
    ^KT-62924
    
    (cherry picked from commit c48f01406a5e3040950f4fdeec579cbeba33f1aa)
    
    GitOrigin-RevId: c6b2dc1d9047e19721fe337f0548e6e81446be8d
    Change-Id: I8553915444ee0cd20b8fdf5ba688aff0d8addb89

--- compose-designer/src/com/android/tools/idea/compose/preview/Inspections.kt
+++ compose-designer/src/com/android/tools/idea/compose/preview/Inspections.kt
@@ -317,7 +317,7 @@ class PreviewShouldNotBeCalledRecursively : AbstractKotlinInspection() {
           if (KotlinPluginModeProvider.isK2Mode()) {
             analyze(this) {
               val functionSymbol = resolveCall()?.singleFunctionCallOrNull()?.symbol
-              functionSymbol?.callableIdIfNonLocal?.callableName
+              functionSymbol?.callableId?.callableName
             }
           } else {
             val resolvedExpression = resolveToCall()
