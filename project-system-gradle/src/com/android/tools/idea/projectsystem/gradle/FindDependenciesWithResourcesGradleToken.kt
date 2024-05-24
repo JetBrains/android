05efadfe0a1ea5dd764fe8c9f1fe01b3f2cbeeab
Christophe Rhodes <xof@google.com>
2024-05-24 13:40:58 +0000

    Use a project system token for finding dependencies with resources
    The current entry point has a module as the unit of enquiry, which
    cannot be right, but its wrongness should not additionally mean
    transitive support of the superset of things one can ask of a module.
    Move the Gradle-specific implementation to project-system-gradle
    through a token, while preserving the existing behaviour for all
    other project systems
    
    This is a part of removing some Gradle-specific module-per-source-set
    interfaces from ModuleSystemUtils.
    
    Bug: 147586218
    Test: existing
    Change-Id: Ia5b07b874f73e88a03116a28b894f3889a480a12
    
    GitOrigin-RevId: cee1212e0ea45b0ffc5fe9c178681da91def076d

---
+++ project-system-gradle/src/com/android/tools/idea/projectsystem/gradle/FindDependenciesWithResourcesGradleToken.kt
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.projectsystem.gradle
+
+import com.android.projectmodel.ExternalAndroidLibrary
+import com.android.tools.idea.projectsystem.DependencyScopeType
+import com.android.tools.idea.projectsystem.FindDependenciesWithResourcesToken
+import com.android.tools.idea.projectsystem.GradleToken
+import com.android.tools.idea.projectsystem.isAndroidTestModule
+import com.android.tools.idea.projectsystem.isMainModule
+import com.android.tools.idea.projectsystem.isScreenshotTestModule
+import com.android.tools.idea.projectsystem.isTestFixturesModule
+import com.android.tools.idea.projectsystem.isUnitTestModule
+import com.intellij.openapi.module.Module
+
+class FindDependenciesWithResourcesGradleToken : FindDependenciesWithResourcesToken<GradleModuleSystem>, GradleToken {
+  override fun findDependencies(moduleSystem: GradleModuleSystem, module: Module): Collection<ExternalAndroidLibrary> {
+    val dependencyScope = when {
+      moduleSystem.isRClassTransitive -> DependencyScopeType.MAIN
+      module.isMainModule() -> DependencyScopeType.MAIN
+      module.isUnitTestModule() -> DependencyScopeType.UNIT_TEST
+      module.isAndroidTestModule() -> DependencyScopeType.ANDROID_TEST
+      module.isTestFixturesModule() -> DependencyScopeType.TEST_FIXTURES
+      module.isScreenshotTestModule() -> DependencyScopeType.SCREENSHOT_TEST
+      else -> DependencyScopeType.MAIN
+    }
+    return moduleSystem.getAndroidLibraryDependencies(dependencyScope)
+  }
+}
