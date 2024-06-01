97b94e96847c3453906c690504a227481079ddb1
Hakan Mehmed <hmehmed@google.com>
2024-06-01 16:55:48 +0000

    [NPW] Add multiplatform module recipe
    Bug: 341025359
    Test: na
    Change-Id: I5d4c7f10534cce7a2b0ca86002fd2fbb04067dfe
    
    GitOrigin-RevId: 457e2b59add04c510f206d93d7c3c2896ab54a3b

--- android-npw/src/com/android/tools/idea/npw/multiplatform/ConfigureKotlinMultiplatformLibraryModuleStep.kt
+++ android-npw/src/com/android/tools/idea/npw/multiplatform/ConfigureKotlinMultiplatformLibraryModuleStep.kt
@@ -19,11 +19,14 @@ import com.android.sdklib.SdkVersionInfo
 import com.android.tools.adtui.device.FormFactor
 import com.android.tools.idea.npw.contextLabel
 import com.android.tools.idea.npw.module.ConfigureModuleStep
+import com.android.tools.idea.observable.ui.SelectedItemProperty
+import com.android.tools.idea.wizard.template.Language
 import com.intellij.openapi.ui.DialogPanel
 import com.intellij.ui.dsl.builder.AlignX
 import com.intellij.ui.dsl.builder.panel
 import com.intellij.util.ui.JBUI.Borders.empty
 import org.jetbrains.android.util.AndroidBundle
+import java.util.Optional
 
 class ConfigureKotlinMultiplatformLibraryModuleStep(
   model: NewKotlinMultiplatformLibraryModuleModel,
@@ -31,6 +34,10 @@ class ConfigureKotlinMultiplatformLibraryModuleStep(
 ) : ConfigureModuleStep<NewKotlinMultiplatformLibraryModuleModel>(
   model, FormFactor.MOBILE, SdkVersionInfo.LOWEST_ACTIVE_API, title = title
 ) {
+
+  init {
+    model.language.set(Optional.of(Language.Kotlin))
+  }
 
   override fun createMainPanel(): DialogPanel = panel {
     row(contextLabel("Module name", AndroidBundle.message("android.wizard.module.help.name"))) {
