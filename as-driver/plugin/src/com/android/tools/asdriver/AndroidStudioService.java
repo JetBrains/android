091458aa6bb2fd47e24d2655c659874cb89d6c74
Nickolay Viuginov <viuginick@google.com>
2024-06-04 14:04:57 +0000

    Refactor as-driver agent to be a plugin
    Test: All E2E tests
    Bug: 341759143
    Change-Id: Idd5f032c00cbff9eb0aed57dde5e3e61b6495617
    
    GitOrigin-RevId: 80705f5172525f031bd2f26b7232f42d91612deb

--- as-driver/plugin/src/com/android/tools/asdriver/AndroidStudioService.java
+++ as-driver/plugin/src/com/android/tools/asdriver/AndroidStudioService.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.android.tools.asdriver.inject;
+package com.android.tools.asdriver;
 
 import com.android.tools.asdriver.proto.ASDriver;
 import com.android.tools.asdriver.proto.AndroidStudioGrpc;
