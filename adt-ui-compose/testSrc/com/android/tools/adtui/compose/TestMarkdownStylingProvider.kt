bb3726ac05efbc61f0ada5252d89fb5aaa909f7f
Sebastiano Poggi <sebp@google.com>
2024-06-07 16:56:40 +0200

    Fix build breakage in adt-ui-compose
    After the Jewel 0.19.5 update, some API changed but for some reason the
    presubmit didn't catch it.
    
    This CL fixes the issue and lets the code compile again.
    
    Bug: none
    Test: none, fixed compilation error.
    Change-Id: I1e499de9ae31d8ba5bc465c1877ff55d754c7ea0
    
    GitOrigin-RevId: 1562ccb1489d11da3be21fd604dc793092036b0d

--- adt-ui-compose/testSrc/com/android/tools/adtui/compose/TestMarkdownStylingProvider.kt
+++ adt-ui-compose/testSrc/com/android/tools/adtui/compose/TestMarkdownStylingProvider.kt
@@ -17,9 +17,10 @@ package com.android.tools.adtui.compose
 
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.Dp
-import org.jetbrains.jewel.foundation.theme.JewelTheme.Companion.isDark
+import org.jetbrains.jewel.foundation.theme.JewelTheme
 import org.jetbrains.jewel.intui.markdown.standalone.styling.dark
 import org.jetbrains.jewel.intui.markdown.standalone.styling.light
+import org.jetbrains.jewel.intui.standalone.theme.createEditorTextStyle
 import org.jetbrains.jewel.markdown.rendering.InlinesStyling
 import org.jetbrains.jewel.markdown.rendering.MarkdownStyling
 
@@ -40,39 +41,59 @@ class TestMarkdownStylingProvider(private val isDark: Boolean) : MarkdownStyling
   ): MarkdownStyling {
     val defaults = createDefault()
 
-    // TODO restore commented out lines when Jewel 0.19.5 is merged
-    //val defaultInlinesStyling = defaults.paragraph.inlinesStyling
+    val defaultInlinesStyling = defaults.paragraph.inlinesStyling
+    val defaultTextSize = defaultInlinesStyling.textStyle.fontSize
+    val defaultEditorTextStyle = JewelTheme.createEditorTextStyle(
+      fontSize = defaultTextSize,
+      lineHeight = defaultTextSize * 1.2,
+    )
 
     return if (isDark) {
       MarkdownStyling.dark(
-        //baseTextStyle ?: defaultInlinesStyling.textStyle,
-        //editorTextStyle ?: JewelTheme.editorTextStyle,
-        blockVerticalSpacing ?: defaults.blockVerticalSpacing,
-        paragraph ?: defaults.paragraph,
-        heading ?: defaults.heading,
-        blockQuote ?: defaults.blockQuote,
-        code ?: defaults.code,
-        list ?: defaults.list,
-        image ?: defaults.image,
-        thematicBreak ?: defaults.thematicBreak,
-        htmlBlock ?: defaults.htmlBlock,
+        baseTextStyle = defaultInlinesStyling.textStyle.merge(baseTextStyle),
+        editorTextStyle = defaultEditorTextStyle.merge(editorTextStyle),
+        inlinesStyling = defaultInlinesStyling.merge(inlinesStyling),
+        blockVerticalSpacing = blockVerticalSpacing ?: defaults.blockVerticalSpacing,
+        paragraph = paragraph ?: defaults.paragraph,
+        heading = heading ?: defaults.heading,
+        blockQuote = blockQuote ?: defaults.blockQuote,
+        code = code ?: defaults.code,
+        list = list ?: defaults.list,
+        image = image ?: defaults.image,
+        thematicBreak = thematicBreak ?: defaults.thematicBreak,
+        htmlBlock = htmlBlock ?: defaults.htmlBlock,
       )
     }
     else {
       MarkdownStyling.light(
-        //baseTextStyle ?: defaultInlinesStyling.textStyle,
-        //editorTextStyle ?: JewelTheme.editorTextStyle,
-        blockVerticalSpacing ?: defaults.blockVerticalSpacing,
-        paragraph ?: defaults.paragraph,
-        heading ?: defaults.heading,
-        blockQuote ?: defaults.blockQuote,
-        code ?: defaults.code,
-        list ?: defaults.list,
-        image ?: defaults.image,
-        thematicBreak ?: defaults.thematicBreak,
-        htmlBlock ?: defaults.htmlBlock,
+        baseTextStyle = defaultInlinesStyling.textStyle.merge(baseTextStyle),
+        editorTextStyle = defaultEditorTextStyle.merge(editorTextStyle),
+        inlinesStyling = defaultInlinesStyling.merge(inlinesStyling),
+        blockVerticalSpacing = blockVerticalSpacing ?: defaults.blockVerticalSpacing,
+        paragraph = paragraph ?: defaults.paragraph,
+        heading = heading ?: defaults.heading,
+        blockQuote = blockQuote ?: defaults.blockQuote,
+        code = code ?: defaults.code,
+        list = list ?: defaults.list,
+        image = image ?: defaults.image,
+        thematicBreak = thematicBreak ?: defaults.thematicBreak,
+        htmlBlock = htmlBlock ?: defaults.htmlBlock,
       )
     }
+  }
+
+  private fun InlinesStyling.merge(other: InlinesStyling?): InlinesStyling {
+    if (other == null) return this
+
+    return InlinesStyling(
+      textStyle = textStyle.merge(other.textStyle),
+      inlineCode = inlineCode.merge(other.inlineCode),
+      link = link.merge(other.link),
+      emphasis = emphasis.merge(other.emphasis),
+      strongEmphasis = strongEmphasis.merge(other.strongEmphasis),
+      inlineHtml = inlineHtml.merge(other.inlineHtml),
+      renderInlineHtml = renderInlineHtml,
+    )
   }
 
   override fun createDefault(): MarkdownStyling =
