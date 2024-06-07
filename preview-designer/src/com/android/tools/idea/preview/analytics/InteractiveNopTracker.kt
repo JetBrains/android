b7a0ba81eed17baa0ed1463eb9518693f9baaa89
Luis Santiago Re <lsantire@google.com>
2024-06-07 15:20:49 +0000

    Move interactive preview from designer to preview-designer
    Bug: N/A
    Test: existing, also moved.
    Change-Id: Ia5766595c785a16351ec18582370e450b3b3ed1a
    
    GitOrigin-RevId: 2b864930369be5d17109ddc2c96c44598d2e5ad5

--- preview-designer/src/com/android/tools/idea/preview/analytics/InteractiveNopTracker.kt
+++ preview-designer/src/com/android/tools/idea/preview/analytics/InteractiveNopTracker.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2020 The Android Open Source Project
+ * Copyright (C) 2024 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.android.tools.idea.preview.interactive.analytics
+package com.android.tools.idea.preview.analytics
 
 class InteractiveNopTracker : InteractivePreviewUsageTracker {
   override fun logInteractiveSession(fps: Int, durationMs: Int, userInteractions: Int) {}
