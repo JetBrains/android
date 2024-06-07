26e072bb5c17d12164d58772d9b17bfab1bd56d0
Luis Santiago Re <lsantire@google.com>
2024-06-07 15:20:49 +0000

    Move interactive preview from designer to preview-designer
    Bug: N/A
    Test: existing, also moved.
    Change-Id: Ia5766595c785a16351ec18582370e450b3b3ed1a
    
    GitOrigin-RevId: 5e86ed5df9d025e3b9f2c63a40e20a8885162171

--- compose-designer/src/com/android/tools/idea/compose/preview/Preview.kt
+++ compose-designer/src/com/android/tools/idea/compose/preview/Preview.kt
@@ -60,6 +60,7 @@ import com.android.tools.idea.preview.PreviewRefreshManager
 import com.android.tools.idea.preview.RenderQualityManager
 import com.android.tools.idea.preview.SimpleRenderQualityManager
 import com.android.tools.idea.preview.actions.BuildAndRefresh
+import com.android.tools.idea.preview.analytics.InteractivePreviewUsageTracker
 import com.android.tools.idea.preview.analytics.PreviewRefreshEventBuilder
 import com.android.tools.idea.preview.annotations.findAnnotatedMethodsValues
 import com.android.tools.idea.preview.essentials.PreviewEssentialsModeManager
@@ -72,7 +73,6 @@ import com.android.tools.idea.preview.gallery.GalleryMode
 import com.android.tools.idea.preview.getDefaultPreviewQuality
 import com.android.tools.idea.preview.groups.PreviewGroupManager
 import com.android.tools.idea.preview.interactive.InteractivePreviewManager
-import com.android.tools.idea.preview.interactive.analytics.InteractivePreviewUsageTracker
 import com.android.tools.idea.preview.interactive.fpsLimitFlow
 import com.android.tools.idea.preview.lifecycle.PreviewLifecycleManager
 import com.android.tools.idea.preview.modes.CommonPreviewModeManager
