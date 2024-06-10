700d5f9d450574d34c431a9e1712633a4dd141c6
Alon Albert <aalbert@google.com>
2024-06-10 09:10:39 -0700

    Automatically Reload Logcat File After Filter Updates
    File is reloaded after a certain time of inactivity.
    
    Notes:
    1. This change required changing `parseLogcatFile` to not use
    `MessageBacklog` to collect the log entries because we need to collect
     all the messages. `MessageBacklog` has a built-in cap.
    2. Added a `loadFilter` parameter to `loadLogcatFile()` that allows
    loading the file without touching the filter. We don't want to touch
    the filter when reloading the file.
    
    Test: Manually
    Bug: NA
    Change-Id: I4463c23911e27db5c9a0bbb001bd970fb012735c
    
    GitOrigin-RevId: 526d37657dcd5133e4771a85933302d673f7043d

--- logcat/src/com/android/tools/idea/logcat/files/LogcatFileParser.kt
+++ logcat/src/com/android/tools/idea/logcat/files/LogcatFileParser.kt
@@ -19,8 +19,6 @@ import com.android.tools.idea.logcat.SYSTEM_HEADER
 import com.android.tools.idea.logcat.message.LogLevel
 import com.android.tools.idea.logcat.message.LogcatHeader
 import com.android.tools.idea.logcat.message.LogcatMessage
-import com.android.tools.idea.logcat.messages.MessageBacklog
-import com.android.tools.idea.logcat.settings.AndroidLogcatSettings
 import com.google.common.collect.Iterators
 import com.google.common.collect.PeekingIterator
 import java.io.BufferedReader
@@ -41,7 +39,6 @@ private const val BUGREPORT_LOGCAT_START = "------ SYSTEM LOG "
 
 internal class LogcatFileParser(
   private val headerRegex: Regex,
-  private val maxBufferSize: Int = AndroidLogcatSettings.getInstance().bufferSize,
   private val zoneId: ZoneId = ZoneId.systemDefault(),
 ) {
 
@@ -68,7 +65,6 @@ internal class LogcatFileParser(
     year: Int,
     isBugreport: Boolean,
   ): List<LogcatMessage> {
-    val messageBacklog = MessageBacklog(maxBufferSize)
     var currentHeader: LogcatHeader? = null
     val currentMessage = StringBuilder()
     val iterator = Iterators.peekingIterator(reader.lineSequence().iterator().withIndex())
@@ -78,50 +74,51 @@ internal class LogcatFileParser(
         throw IllegalStateException("SYSTEM LOG not found")
       }
     }
-    run {
-      iterator.forEach {
-        val line = it.value
-        if (isBugreport) {
-          if (line.isEndOfLogcat()) {
-            // Bugreport runs a second logcat command for logs generated during the bugreport.
-            when (iterator.consumeUntilLogcat()) {
-              true -> return@forEach
-              false -> return@run
+    return buildList {
+      run {
+        iterator.forEach {
+          val line = it.value
+          if (isBugreport) {
+            if (line.isEndOfLogcat()) {
+              // Bugreport runs a second logcat command for logs generated during the bugreport.
+              when (iterator.consumeUntilLogcat()) {
+                true -> return@forEach
+                false -> return@run
+              }
             }
           }
-        }
-        if (line.startsWith(SYSTEM_LOG_PREFIX)) {
-          messageBacklog.addAll(listOf(LogcatMessage(SYSTEM_HEADER, line)))
-          currentHeader = null
-          currentMessage.clear()
-          return@forEach
-        }
-
-        val result =
-          headerRegex.find(line)
-            ?: throw IllegalArgumentException(
-              "Error parsing [$filename:${it.index + 1}]. Invalid logcat line: $line"
-            )
-        val header = result.toLogcatHeader(year)
-        if (header != currentHeader) {
-          currentHeader?.let { logcatHeader ->
-            messageBacklog.addAll(listOf(LogcatMessage(logcatHeader, currentMessage.toString())))
+          if (line.startsWith(SYSTEM_LOG_PREFIX)) {
+            add(LogcatMessage(SYSTEM_HEADER, line))
+            currentHeader = null
+            currentMessage.clear()
+            return@forEach
           }
-          currentHeader = header
-          currentMessage.clear()
+
+          val result =
+            headerRegex.find(line)
+              ?: throw IllegalArgumentException(
+                "Error parsing [$filename:${it.index + 1}]. Invalid logcat line: $line"
+              )
+          val header = result.toLogcatHeader(year)
+          if (header != currentHeader) {
+            currentHeader?.let { logcatHeader ->
+              add(LogcatMessage(logcatHeader, currentMessage.toString()))
+            }
+            currentHeader = header
+            currentMessage.clear()
+          }
+          if (currentMessage.isNotEmpty()) {
+            currentMessage.append('\n')
+          }
+          currentMessage.append(result.getGroup("message"))
         }
+      }
+      currentHeader?.let {
         if (currentMessage.isNotEmpty()) {
-          currentMessage.append('\n')
+          add(LogcatMessage(it, currentMessage.toString()))
         }
-        currentMessage.append(result.getGroup("message"))
       }
     }
-    currentHeader?.let {
-      if (currentMessage.isNotEmpty()) {
-        messageBacklog.addAll(listOf(LogcatMessage(it, currentMessage.toString())))
-      }
-    }
-    return messageBacklog.messages
   }
 
   companion object {
