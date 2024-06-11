34d19a3f3b70babf32b73bbef485aa409f970a5f
Raluca Sauciuc <ralucas@google.com>
2024-06-10 19:11:24 -0700

    Switch ReplaceCallFix to ModCommand
    Bug: 319287252
    Test: existing
    Change-Id: Ia31c04025441c030c607b188d900908402570c82
    
    GitOrigin-RevId: 8eba25432e23d4782e0d47c3117d3569e5dacdc8

--- lint/src/com/android/tools/idea/lint/common/ReplaceCallFix.kt
+++ lint/src/com/android/tools/idea/lint/common/ReplaceCallFix.kt
@@ -15,18 +15,21 @@
  */
 package com.android.tools.idea.lint.common
 
-import com.intellij.codeInsight.intention.preview.IntentionPreviewUtils.prepareElementForWrite
 import com.intellij.lang.java.JavaLanguage
+import com.intellij.modcommand.ActionContext
+import com.intellij.modcommand.ModCommand
+import com.intellij.modcommand.Presentation
+import com.intellij.modcommand.PsiBasedModCommandAction
 import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiMethodCallExpression
 import com.intellij.psi.util.PsiTreeUtil
-import org.jetbrains.annotations.Nls
 import org.jetbrains.kotlin.idea.KotlinLanguage
 import org.jetbrains.kotlin.psi.KtCallExpression
 import org.jetbrains.kotlin.psi.KtNameReferenceExpression
 
-class ReplaceCallFix(private val mySuggest: String) : DefaultLintQuickFix(null) {
+class ReplaceCallFix(private val mySuggest: String, element: PsiElement) :
+  PsiBasedModCommandAction<PsiElement>(element) {
   private val methodName: String
     get() {
       val start = if (mySuggest.startsWith("#")) 1 else 0
@@ -37,35 +40,26 @@ class ReplaceCallFix(private val mySuggest: String) : DefaultLintQuickFix(null) 
       return mySuggest.substring(start, parameters).trim()
     }
 
-  @Nls
-  override fun getName(): String {
-    return "Call $methodName instead"
-  }
+  override fun getPresentation(context: ActionContext, element: PsiElement) =
+    Presentation.of("Call $methodName instead")
 
-  override fun apply(
-    startElement: PsiElement,
-    endElement: PsiElement,
-    context: AndroidQuickfixContexts.Context,
-  ) {
-    if (!startElement.isValid) {
-      return
-    }
+  override fun getFamilyName() = "ReplaceCallFix"
 
-    if (!prepareElementForWrite(startElement)) {
-      return
-    }
-
-    when (startElement.language) {
-      JavaLanguage.INSTANCE -> handleJava(startElement, context)
-      KotlinLanguage.INSTANCE -> handleKotlin(startElement, context)
+  @Suppress("UnstableApiUsage")
+  override fun perform(context: ActionContext, element: PsiElement): ModCommand {
+    return ModCommand.psiUpdate(element) { e, _ ->
+      when (element.language) {
+        JavaLanguage.INSTANCE -> handleJava(e)
+        KotlinLanguage.INSTANCE -> handleKotlin(e)
+      }
     }
   }
 
-  private fun handleJava(element: PsiElement, context: AndroidQuickfixContexts.Context) {
+  private fun handleJava(element: PsiElement) {
     val methodCall =
       PsiTreeUtil.getParentOfType(element, PsiMethodCallExpression::class.java, false) ?: return
     val file = methodCall.containingFile ?: return
-    val document = context.getDocument(file) ?: return
+    val document = file.fileDocument
     val methodExpression = methodCall.methodExpression
     val referenceNameElement = methodExpression.referenceNameElement ?: return
     val range = referenceNameElement.textRange ?: return
@@ -88,16 +82,16 @@ class ReplaceCallFix(private val mySuggest: String) : DefaultLintQuickFix(null) 
     document.replaceString(range.startOffset, range.endOffset, name)
   }
 
-  private fun handleKotlin(element: PsiElement, context: AndroidQuickfixContexts.Context) {
+  private fun handleKotlin(element: PsiElement) {
     val methodCall =
       PsiTreeUtil.getParentOfType(element, KtCallExpression::class.java, false) ?: return
-    val methodExpression = methodCall.getCalleeExpression()
+    val methodExpression = methodCall.calleeExpression
     if (methodExpression is KtNameReferenceExpression) {
       val identifier: PsiElement? = methodExpression.getIdentifier()
       if (identifier != null) {
         val range = identifier.textRange
         val file = methodCall.containingFile ?: return
-        val document = context.getDocument(file) ?: return
+        val document = file.fileDocument
 
         // Also need to insert a message parameter
         // Currently hardcoded for the check*Permission to enforce*Permission code path. It's
