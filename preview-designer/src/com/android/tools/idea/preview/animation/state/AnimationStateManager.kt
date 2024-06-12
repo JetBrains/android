01ceabe87ced5a25d8651d2192970c11c76e1854
Polina Koval <kovalp@google.com>
2024-06-12 14:14:48 +0100

    Move files to correct packages
    Bug: 346751584
    Test: n/a
    Change-Id: I7ee70266a308df311d34fc50c547d729d8060199
    
    GitOrigin-RevId: 6c85602bde4934390ac34f73541c71e5f57679f2

--- preview-designer/src/com/android/tools/idea/preview/animation/state/AnimationStateManager.kt
+++ preview-designer/src/com/android/tools/idea/preview/animation/state/AnimationStateManager.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.android.tools.idea.preview.animation
+package com.android.tools.idea.preview.animation.state
 
 import com.intellij.openapi.actionSystem.AnAction
 
