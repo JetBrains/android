f0183517f6d3f2dda0a8e980486e9915d489cf6e
Luis Santiago Re <lsantire@google.com>
2024-06-13 16:31:04 +0000

    Remove SUPPORTS_LOCKING and dead code around it
    This boolean "flag" has been disabled for quite some time.
    
    Bug: 335424569
    Test: N/A
    Change-Id: I4491ebd5732b41da18c727398885308e0ed587a5
    
    GitOrigin-RevId: 1cb29ec877bb2cf3050ee91744a389749f984254

--- designer/src/com/android/tools/idea/uibuilder/structure/NlTreeBadgeHandler.java
+++ designer/src/com/android/tools/idea/uibuilder/structure/NlTreeBadgeHandler.java
@@ -15,18 +15,14 @@
  */
 package com.android.tools.idea.uibuilder.structure;
 
-import com.android.SdkConstants;
-import com.android.tools.idea.common.command.NlWriteCommandActionUtil;
 import com.android.tools.idea.common.error.Issue;
 import com.android.tools.idea.common.error.IssueModel;
 import com.android.tools.idea.common.error.IssuePanelService;
 import com.android.tools.idea.common.model.NlComponent;
 import com.android.tools.idea.common.model.NlModel;
-import com.android.tools.idea.common.scene.SceneManager;
 import com.android.tools.idea.common.surface.DesignSurface;
 import com.android.utils.SparseIntArray;
 import com.intellij.ui.LightweightHint;
-import icons.StudioIcons;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -36,7 +32,6 @@ import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
-import static com.android.tools.idea.common.scene.SceneManager.SUPPORTS_LOCKING;
 
 /**
  * Handler class responsible for drawing badges for each
@@ -45,20 +40,13 @@ import static com.android.tools.idea.common.scene.SceneManager.SUPPORTS_LOCKING;
 public class NlTreeBadgeHandler {
 
   private static final int BADGE_MARGIN = 5;
-  private static final int BADGE_ICON = 0;
-  private static final int LOCK_ICON = 1;
-
-  private static final String TOGGLE_LOCK_MESSAGE = "Toggle Tool Locking";
 
   private final BadgeMouseMotionListener myBadgeMouseMotionListener = new BadgeMouseMotionListener();
   @Nullable private LightweightHint myTooltipHint;
   @Nullable private TreePath myHoveredPath;
-  private int myHoveredIcon = BADGE_ICON;
-  private NlComponent myHoveredComponent;
 
   @Nullable private NlModel myNlModel;
   private int myBadgeX;
-  private int myLockIconX;
   @Nullable private DesignSurface<?> mySurface;
 
   /**
@@ -96,9 +84,8 @@ public class NlTreeBadgeHandler {
       }
       if (pathBounds != null) {
         int y = pathBounds.y + pathBounds.height / 2;
-        Icon firstIcon = null;
         if (issue != null) {
-          firstIcon = IssueModel.getIssueIcon(issue.getSeverity(), tree.isRowSelected(i) && tree.hasFocus());
+          Icon firstIcon = IssueModel.getIssueIcon(issue.getSeverity(), tree.isRowSelected(i) && tree.hasFocus());
           if (firstIcon != null) {
             int x = tree.getWidth() - firstIcon.getIconWidth() - BADGE_MARGIN;
             int iy = y - firstIcon.getIconHeight() / 2;
@@ -106,29 +93,6 @@ public class NlTreeBadgeHandler {
             myBadgeWidthForRows.put(i, firstIcon.getIconWidth());
             myBadgeX = Math.min(x, myBadgeX);
           }
-        }
-        if (SUPPORTS_LOCKING) {
-          Icon unlockIcon = StudioIcons.LayoutEditor.Toolbar.UNLOCK;
-          Icon lockIcon = StudioIcons.LayoutEditor.Toolbar.LOCK;
-          if (firstIcon != null) {
-            myLockIconX = myBadgeX - BADGE_MARGIN - lockIcon.getIconWidth();
-          }
-          else {
-            myLockIconX = tree.getWidth() - BADGE_MARGIN - lockIcon.getIconWidth();
-          }
-          boolean isLocked = SceneManager.isComponentLocked(component);
-          boolean isOver = myHoveredComponent == component && myHoveredIcon == LOCK_ICON;
-          if (!isOver && !isLocked) {
-            // if we are not over the icon, we only draw it when the component is invisible
-            // (as visible is the default state)
-            continue;
-          }
-          Icon lockingIcon = isLocked ? lockIcon : unlockIcon;
-          if (isOver) {
-            // in this case, show instead the icon of the state we would end up if we click on it
-            lockingIcon = isLocked ? unlockIcon : lockIcon;
-          }
-          lockingIcon.paintIcon(tree, g, myLockIconX, y - lockingIcon.getIconHeight() / 2);
         }
       }
     }
@@ -178,7 +142,6 @@ public class NlTreeBadgeHandler {
         myTooltipHint.hide(true);
         myTooltipHint = null;
       }
-      myHoveredComponent = null;
     }
 
     @Override
@@ -194,9 +157,6 @@ public class NlTreeBadgeHandler {
 
     private void handleMouseClicked(MouseEvent event, JTree tree) {
       int limit = myBadgeX;
-      if (SUPPORTS_LOCKING) {
-        limit = myLockIconX;
-      }
       IssueModel issueModel = mySurface != null ? mySurface.getIssueModel() : null;
       if (event.getX() < limit || issueModel == null) {
         // We only show the tooltip if the mouse id hovering the badge
@@ -219,30 +179,13 @@ public class NlTreeBadgeHandler {
       if (event.getX() > myBadgeX) {
         IssuePanelService.getInstance(component.getModel().getProject()).showIssueForComponent(mySurface, component);
       }
-      else {
-        if (SUPPORTS_LOCKING) {
-          toggleLocking(component);
-        }
-      }
 
-    }
-
-    private void toggleLocking(@NotNull NlComponent component) {
-      boolean isLocked = SceneManager.isComponentLocked(component);
-      if (component.getParent() == null) {
-        // Don't do anything for the root component
-        return;
-      }
-
-      NlWriteCommandActionUtil.run(component, "", () ->
-        component.setAttribute(SdkConstants.TOOLS_URI, SdkConstants.ATTR_LOCKED, isLocked ? null : SdkConstants.VALUE_TRUE));
     }
 
     private void handleShowBadge(@NotNull MouseEvent event, @NotNull JTree tree) {
       if (event.getX() < myBadgeX) {
         // We only show the tooltip if the mouse id hovering the badge
         myHoveredPath = null;
-        myHoveredComponent = null;
         hideTooltip();
         return;
       }
@@ -254,7 +197,6 @@ public class NlTreeBadgeHandler {
           // Ensure the tooltip is not displayed if the mouse is below the last row
           event.getY() < bounds.y || event.getY() > bounds.y + bounds.height) {
         myHoveredPath = null;
-        myHoveredComponent = null;
         hideTooltip();
         return;
       }
@@ -263,37 +205,14 @@ public class NlTreeBadgeHandler {
       if (!(last instanceof NlComponent)) {
         return;
       }
-      myHoveredComponent = (NlComponent)last;
-
-      boolean badgeIcon = true;
-      if (SUPPORTS_LOCKING) {
-        if (event.getX() < myBadgeX) {
-          badgeIcon = false;
-        }
-      }
 
       // Don't do anything if the path under the mouse is still the same
       if (myHoveredPath == path) {
-        if (badgeIcon && myHoveredIcon == BADGE_ICON) {
-          return;
-        }
-        if (!badgeIcon && myHoveredIcon == LOCK_ICON) {
-          return;
-        }
+        return;
       }
       myHoveredPath = path;
 
-      String message = null;
-      if (badgeIcon) {
-        message = getIssueMessage(myHoveredPath);
-        myHoveredIcon = BADGE_ICON;
-      }
-      else if (SUPPORTS_LOCKING) {
-        if (myHoveredComponent.getParent() != null) {
-          message = TOGGLE_LOCK_MESSAGE;
-          myHoveredIcon = LOCK_ICON;
-        }
-      }
+      String message = getIssueMessage(myHoveredPath);
       if (message != null) {
         tree.setToolTipText(message);
       }
