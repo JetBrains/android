fa3afbc27130af540478d8bcfa4274b178c061e2
Christophe Rhodes <xof@google.com>
2024-06-14 14:14:49 +0000

    Type signature changes for test to sourceProvider maps
    Define HostTest and DeviceTest interfaces for use in type signatures,
    distinct from the implementation enum classes.
    
    Propagate the new HostTest and DeviceTest interfaces through the
    system, so that it should no longer be possible to accidentally
    attempt to look up a host test source provider from a device test map,
    or the reverse.
    
    Fixes: 343189827
    Test: existing (no change in functionality)
    Change-Id: I431077e1c697098c357b181cdf3b7e53a3c1e7e0
    
    GitOrigin-RevId: af589811b06209d502952260eac071897a117509

--- project-system/src/com/android/tools/idea/projectsystem/SourceProvidersImpl.kt
+++ project-system/src/com/android/tools/idea/projectsystem/SourceProvidersImpl.kt
@@ -20,28 +20,28 @@ import kotlinx.collections.immutable.toImmutableMap
 class SourceProvidersImpl(
   override val mainIdeaSourceProvider: NamedIdeaSourceProvider,
   override val currentSourceProviders: List<NamedIdeaSourceProvider>,
-  override val currentHostTestSourceProviders: Map<TestComponentType, List<NamedIdeaSourceProvider>>,
-  override val currentDeviceTestSourceProviders: Map<TestComponentType, List<NamedIdeaSourceProvider>>,
+  override val currentHostTestSourceProviders: Map<TestComponentType.HostTest, List<NamedIdeaSourceProvider>>,
+  override val currentDeviceTestSourceProviders: Map<TestComponentType.DeviceTest, List<NamedIdeaSourceProvider>>,
   override val currentTestFixturesSourceProviders: List<NamedIdeaSourceProvider>,
   override val currentAndSomeFrequentlyUsedInactiveSourceProviders: List<NamedIdeaSourceProvider>,
 
   @Suppress("OverridingDeprecatedMember")
   override val mainAndFlavorSourceProviders: List<NamedIdeaSourceProvider>,
   override val generatedSources: IdeaSourceProvider,
-  override val generatedHostTestSources: Map<TestComponentType, IdeaSourceProvider>,
-  override val generatedDeviceTestSources: Map<TestComponentType, IdeaSourceProvider>,
+  override val generatedHostTestSources: Map<TestComponentType.HostTest, IdeaSourceProvider>,
+  override val generatedDeviceTestSources: Map<TestComponentType.DeviceTest, IdeaSourceProvider>,
   override val generatedTestFixturesSources: IdeaSourceProvider
 ) : SourceProviders {
   override val sources: IdeaSourceProvider =
     createMergedSourceProvider(ScopeType.MAIN, currentSourceProviders)
-  override val hostTestSources: Map<TestComponentType, IdeaSourceProvider> =
-    mutableMapOf<TestComponentType, IdeaSourceProvider>().apply {
+  override val hostTestSources: Map<TestComponentType.HostTest, IdeaSourceProvider> =
+    mutableMapOf<TestComponentType.HostTest, IdeaSourceProvider>().apply {
        currentHostTestSourceProviders.forEach {
          put(it.key, createMergedSourceProvider(it.key.scopeTypeByName(), it.value))
        }
      }.toImmutableMap()
-  override val deviceTestSources: Map<TestComponentType, IdeaSourceProvider> =
-    mutableMapOf<TestComponentType, IdeaSourceProvider>().apply {
+  override val deviceTestSources: Map<TestComponentType.DeviceTest, IdeaSourceProvider> =
+    mutableMapOf<TestComponentType.DeviceTest, IdeaSourceProvider>().apply {
       currentDeviceTestSourceProviders.forEach {
         put(it.key, createMergedSourceProvider(it.key.scopeTypeByName(), it.value))
       }
