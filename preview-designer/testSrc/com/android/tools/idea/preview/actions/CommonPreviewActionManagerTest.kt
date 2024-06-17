73838fdef633543121d9af800cac8cca1e11ca5c
Paul Milian <paulmilian@google.com>
2024-06-17 20:10:17 +0100

    Disable context toolbar actions when a build is needed
    Fixes: 346911154
    Test: added CommonPreviewActionManagerTest and updated
      PreviewSurfaceActionManagerTest and also tested manually
    Change-Id: I0209d52c023f60290e605e9d9980cea53fe9fa86
    
    GitOrigin-RevId: 310f76dee3f0e055305307a625a554bcaff1f0fa

--- preview-designer/testSrc/com/android/tools/idea/preview/actions/CommonPreviewActionManagerTest.kt
+++ preview-designer/testSrc/com/android/tools/idea/preview/actions/CommonPreviewActionManagerTest.kt
@@ -1,4 +1,19 @@
-package com.android.tools.idea.compose.preview.actions
+/*
+ * Copyright (C) 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.preview.actions
 
 import com.android.testutils.MockitoKt.mock
 import com.android.testutils.MockitoKt.whenever
@@ -18,18 +33,20 @@ import kotlin.test.assertNotEquals
 import org.junit.Rule
 import org.junit.Test
 
-// EnableUiCheckAction(),
 // AnimationInspectorAction(),
 // EnableInteractiveAction(),
-// DeployToDeviceAction()
 // in wrappers
-private const val EXPECTED_NUMBER_OF_ACTIONS = 4
+private const val EXPECTED_NUMBER_OF_ACTIONS = 2
 
-class PreviewSurfaceActionManagerTest {
-
+class CommonPreviewActionManagerTest {
   @get:Rule val projectRule = AndroidProjectRule.inMemory()
 
-  private val actionManager = PreviewSurfaceActionManager(mock(), mock())
+  private val actionManager =
+    CommonPreviewActionManager(
+      mock(),
+      supportAnimationPreview = true,
+      supportInteractivePreview = true,
+    )
 
   @Test
   fun testToolbarActionsDisabledWhenPreviewHasErrors() {
