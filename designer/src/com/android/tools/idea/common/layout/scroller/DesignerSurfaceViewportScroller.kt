06c140b66302c5d95ac19ccd5ec76ecfc61c7e43
Kseniia Grey <kseniia@google.com>
2024-06-20 13:54:05 +0100

    Move scrollers to its package
    Test: n/a
    Bug: n/a
    Change-Id: I89a5a183957d1cad1c1b8f1160b7343961bd3ea8
    
    GitOrigin-RevId: 605381f5294ce27f3832952ec65437c37adf3d58

--- designer/src/com/android/tools/idea/common/layout/scroller/DesignerSurfaceViewportScroller.kt
+++ designer/src/com/android/tools/idea/common/layout/scroller/DesignerSurfaceViewportScroller.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2023 The Android Open Source Project
+ * Copyright (C) 2024 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.android.tools.idea.common.surface.layout
+package com.android.tools.idea.common.layout.scroller
+
+import com.android.tools.idea.common.surface.layout.DesignSurfaceViewport
 
 /**
  * An interface which scroll the [DesignSurfaceViewport]. Can be used to calculate the viewport
--- designer/testSrc/com/android/tools/idea/common/layout/scroller/ReferencePointScrollerTest.kt
+++ designer/testSrc/com/android/tools/idea/common/layout/scroller/ReferencePointScrollerTest.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2023 The Android Open Source Project
+ * Copyright (C) 2024 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,10 +13,11 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.android.tools.idea.common.surface.layout
+package com.android.tools.idea.common.layout.scroller
 
 import com.android.testutils.MockitoKt
 import com.android.tools.idea.common.surface.SceneView
+import com.android.tools.idea.common.surface.layout.TestDesignSurfaceViewport
 import java.awt.Component
 import java.awt.Dimension
 import java.awt.Point
