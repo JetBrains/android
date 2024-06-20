8f0e1a7ac012b7a20529b5dae23ee47794b2b403
Kseniia Grey <kseniia@google.com>
2024-06-20 14:50:01 +0100

    Move MatchParentLayoutManager to manager package
    Test: n/a
    Bug: n/a
    Change-Id: I07ce5617f85d3804dfdcf9a7708d2f1e7aac2d0f
    
    GitOrigin-RevId: a2dbf5b9dd387db338cb2a06defee6f193d9f9cb

--- designer/src/com/android/tools/idea/common/layout/manager/MatchParentLayoutManager.kt
+++ designer/src/com/android/tools/idea/common/layout/manager/MatchParentLayoutManager.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.android.tools.idea.common.surface.layout
+package com.android.tools.idea.common.layout.manager
 
 import java.awt.Component
 import java.awt.Container
