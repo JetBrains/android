dada352a021dc6028a9e077453efcc19876fd23d
Luis Santiago Re <lsantire@google.com>
2024-06-20 17:58:00 +0000

    Fix some dispose executions in designer
    The documentation of the Disposable interface specifies that the
    dispose method should never be called directly. Instead, it should
    be done using Disposer.dispose(Disposable).
    
    Bug: N/A
    Test: N/A
    Change-Id: I5a2a867e87916fde758c54d691fd9ed90c9d7b5f
    
    GitOrigin-RevId: b3ab0442b052909360607a2de2a29ba4ccd111aa

--- designer/src/com/android/tools/idea/editors/layeredimage/LayeredImageEditor.java
+++ designer/src/com/android/tools/idea/editors/layeredimage/LayeredImageEditor.java
@@ -105,7 +105,7 @@ class LayeredImageEditor extends UserDataHolderBase implements FileEditor {
   @Override
   public void dispose() {
     Disposer.dispose(myEditor);
-    getComponent().dispose();
+    Disposer.dispose(getComponent());
   }
 
   @NotNull
