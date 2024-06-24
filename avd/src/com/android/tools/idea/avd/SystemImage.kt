70ab89cf1ffa250816e58f16f90717664aea68d8
James Rose <rosej@google.com>
2024-06-24 16:40:51 -0700

    Add device matching logic
    We need to know which Devices and system images are compatible even
    before we create VirtualDeviceProfiles, so that we know which API levels
    are available.
    
    Bug: 348437638
    Test: SystemImageTest
    Change-Id: I78329a3282cd95fe4a97637e5e0dbc30364fb948
    
    GitOrigin-RevId: 019c0b753c7b73e331eed357972d03e69be9a8e6

--- avd/src/com/android/tools/idea/avd/SystemImage.kt
+++ avd/src/com/android/tools/idea/avd/SystemImage.kt
@@ -23,7 +23,9 @@ import com.android.repository.api.UpdatablePackage
 import com.android.sdklib.AndroidVersion
 import com.android.sdklib.SystemImageTags
 import com.android.sdklib.devices.Abi
+import com.android.sdklib.devices.Device
 import com.android.sdklib.devices.Storage
+import com.android.sdklib.repository.IdDisplay
 import com.android.sdklib.repository.meta.DetailsTypes.ApiDetailsType
 import com.android.tools.idea.progress.StudioLoggerProgressIndicator
 import com.android.tools.idea.sdk.AndroidSdks
@@ -31,6 +33,8 @@ import com.android.tools.idea.sdk.StudioDownloader
 import com.android.tools.idea.sdk.StudioSettingsController
 import com.google.common.annotations.VisibleForTesting
 import kotlinx.collections.immutable.ImmutableCollection
+import kotlinx.collections.immutable.ImmutableList
+import kotlinx.collections.immutable.toImmutableList
 import kotlinx.collections.immutable.toImmutableSet
 
 @Immutable
@@ -44,8 +48,43 @@ internal constructor(
   internal val services: Services,
   internal val abis: ImmutableCollection<Abi>,
   internal val translatedAbis: ImmutableCollection<Abi>,
+  internal val tags: ImmutableList<IdDisplay>,
   private val size: Storage,
 ) {
+  internal fun matches(device: Device): Boolean {
+    // TODO(b/326294450) - Try doing this in device and system image declarations
+    if (!Device.isTablet(device)) {
+      if (tags.contains(SystemImageTags.TABLET_TAG)) {
+        return false
+      }
+    }
+
+    // Unknown/generic device?
+    if (device.tagId == null || device.tagId == SystemImageTags.DEFAULT_TAG.id) {
+      // If so include all system images, except those we *know* not to match this type
+      // of device. Rather than just checking
+      // "imageTag.getId().equals(SystemImage.DEFAULT_TAG.getId())"
+      // here (which will filter out system images with a non-default tag, such as the Google API
+      // system images (see issue #78947), we instead deliberately skip the other form factor images
+      return !SystemImageTags.isTvImage(tags) &&
+             !SystemImageTags.isWearImage(tags) &&
+             !SystemImageTags.isAutomotiveImage(tags) &&
+             !SystemImageTags.isDesktopImage(tags) &&
+             !tags.contains(SystemImageTags.CHROMEOS_TAG)
+    }
+
+    // Android TV / Google TV and vice versa
+    if (
+      device.tagId == SystemImageTags.ANDROID_TV_TAG.id ||
+      device.tagId == SystemImageTags.GOOGLE_TV_TAG.id
+    ) {
+      return SystemImageTags.isTvImage(tags);
+    }
+
+    // The device has a tag; the system image must have a corresponding tag
+    return tags.any { it.id == device.tagId }
+  }
+
   internal fun matches(device: VirtualDevice): Boolean {
     // TODO: http://b/347053479
     if (androidVersion.isPreview) {
@@ -107,6 +146,7 @@ internal constructor(
         repoPackage.getServices(details.androidVersion),
         details.abis.map(::valueOfString).toImmutableSet(),
         details.translatedAbis.map(::valueOfString).toImmutableSet(),
+        SystemImageTags.getTags(repoPackage).toImmutableList(),
         Storage(size),
       )
     }
