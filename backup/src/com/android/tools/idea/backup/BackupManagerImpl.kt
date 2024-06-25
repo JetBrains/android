936849bc8ffd7aa7af1198f68050c10b6b2a4aef
Alon Albert <aalbert@google.com>
2024-06-25 08:29:15 -0700

    Add File Chooser Dialogs
    Bug: 348406593
    Test: Manually
    Change-Id: Id4c03fabaec792f9496b0ab6fe1a9f4738b4e0eb
    
    GitOrigin-RevId: 4cefc515e8ad47cc8d9cb42cbd3a6a39683b9af8

--- backup/src/com/android/tools/idea/backup/BackupManagerImpl.kt
+++ backup/src/com/android/tools/idea/backup/BackupManagerImpl.kt
@@ -20,11 +20,23 @@ import com.android.backup.BackupProgressListener
 import com.android.backup.RestoreHandler
 import com.android.tools.environment.Logger
 import com.android.tools.idea.adblib.AdbLibService
+import com.android.tools.idea.concurrency.AndroidDispatchers
+import com.intellij.ide.util.PropertiesComponent
+import com.intellij.openapi.fileChooser.FileChooserDescriptor
+import com.intellij.openapi.fileChooser.FileChooserFactory
+import com.intellij.openapi.fileChooser.FileSaverDescriptor
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.project.guessProjectDir
+import com.intellij.openapi.vfs.LocalFileSystem
 import java.nio.file.Path
+import kotlin.io.path.pathString
+import kotlinx.coroutines.withContext
+
+private const val BACKUP_PATH_KEY = "Backup.Path"
+private const val BACKUP_EXT = ".backup"
 
 /** Implementation of [BackupManager] */
-internal class BackupManagerImpl(project: Project) : BackupManager {
+internal class BackupManagerImpl(private val project: Project) : BackupManager {
   private val adbSession = AdbLibService.getSession(project)
   private val logger: Logger = Logger.getInstance(this::class.java)
 
@@ -46,5 +58,50 @@ internal class BackupManagerImpl(project: Project) : BackupManager {
   ) {
     logger.debug("Restoring from $backupFile on '${serialNumber}'")
     RestoreHandler(adbSession, logger, serialNumber, progressListener, backupFile).restore()
+  }
+
+  override suspend fun chooseBackupFile(nameHint: String): Path? {
+    val dialog =
+      FileChooserFactory.getInstance()
+        .createSaveFileDialog(
+          FileSaverDescriptor(
+            BackupBundle.message("backup.choose.backup.file.dialog.title"),
+            "",
+            BACKUP_EXT,
+          ),
+          project,
+        )
+    val path = dialog.save(getBackupPath(), nameHint)?.file?.toPath()
+    if (path != null) {
+      setBackupPath(path)
+    }
+    return path
+  }
+
+  override fun chooseRestoreFile(): Path? {
+    val descriptor =
+      FileChooserDescriptor(true, false, true, true, false, false)
+        .withTitle(BackupBundle.message("backup.choose.restore.file.dialog.title"))
+        .withFileFilter { it.name.endsWith(BACKUP_EXT) }
+    return FileChooserFactory.getInstance()
+      .createFileChooser(descriptor, project, null)
+      .choose(project)
+      .firstOrNull()
+      ?.toNioPath()
+      ?.normalize()
+  }
+
+  private suspend fun getBackupPath(): Path? {
+    val properties = PropertiesComponent.getInstance(project)
+    return withContext(AndroidDispatchers.diskIoThread) {
+      when (val lastPath = properties.getValue(BACKUP_PATH_KEY)) {
+        null -> project.guessProjectDir()?.toNioPath()
+        else -> LocalFileSystem.getInstance().findFileByPath(lastPath)?.toNioPath()
+      }
+    }
+  }
+
+  private fun setBackupPath(path: Path) {
+    PropertiesComponent.getInstance(project).setValue(BACKUP_PATH_KEY, path.pathString)
   }
 }
