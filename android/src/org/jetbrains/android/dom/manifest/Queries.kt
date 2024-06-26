86eab64e934276a4719a218ae6f9b7f362ef44d9
Andy Magee <andymagee@google.com>
2024-06-26 13:59:30 -0700

    Fix XML attributes for <queries>
    The <data> element that is a descendant of <queries> is restricted in
    which attributes it will accept; this change updates it to not allow
    them.
    
    Additionally, the <action> tag is required underneath <intent> in this
    context.
    
    See documentation at
    https://developer.android.com/training/package-visibility/declaring#intent-filter-signature
    
    Fixes: 213256139
    Test: AndroidManifestDomTest
    Change-Id: I183d22bc0771f23c3ff24905f63fef48cfb89a4b
    
    GitOrigin-RevId: 5a5ab642a43bd704c84d62908d495566d25174fc

--- android/src/org/jetbrains/android/dom/manifest/Queries.kt
+++ android/src/org/jetbrains/android/dom/manifest/Queries.kt
@@ -17,10 +17,13 @@ package org.jetbrains.android.dom.manifest
 
 import com.intellij.util.xml.Attribute
 import com.intellij.util.xml.Convert
+import com.intellij.util.xml.DefinesXml
+import com.intellij.util.xml.Required
 import org.jetbrains.android.dom.AndroidAttributeValue
 import org.jetbrains.android.dom.AndroidDomElement
 import org.jetbrains.android.dom.converters.AndroidPackageConverter
 
+@DefinesXml
 interface Queries : ManifestElement {
   fun getProviders(): List<Provider>
 
@@ -28,20 +31,53 @@ interface Queries : ManifestElement {
 
   fun getPackages(): List<Package>
 
+  @DefinesXml
   interface Package : AndroidDomElement {
     @Attribute("name")
     @Convert(AndroidPackageConverter::class)
     fun getName(): AndroidAttributeValue<String>
   }
 
+  @DefinesXml
   interface Intent : AndroidDomElement {
-    fun getAction(): Action
+    @Required fun getAction(): Action
 
     fun getData(): Data
 
     fun getCategory(): Category
+
+    /**
+     * Data is redefined here because some of the "AndroidManifestData" styleable's attributes are
+     * not applicable in this context per
+     * https://developer.android.com/training/package-visibility/declaring#intent-filter-signature.
+     */
+    @DefinesXml
+    interface Data : AndroidDomElement {
+      @Attribute("mimeType") fun getMimeType(): AndroidAttributeValue<String>
+
+      fun getScheme(): AndroidAttributeValue<String>
+
+      fun getSsp(): AndroidAttributeValue<String>
+
+      @Attribute("sspPrefix") fun getSspPrefix(): AndroidAttributeValue<String>
+
+      @Attribute("sspPattern") fun getSspPattern(): AndroidAttributeValue<String>
+
+      @Attribute("sspAdvancedPattern") fun getSspAdvancedPattern(): AndroidAttributeValue<String>
+
+      @Attribute("sspSuffix") fun getSspSuffix(): AndroidAttributeValue<String>
+
+      fun getHost(): AndroidAttributeValue<String>
+
+      @Attribute("pathPattern") fun getPathPattern(): AndroidAttributeValue<String>
+
+      @Attribute("pathAdvancedPattern") fun getPathAdvancedPattern(): AndroidAttributeValue<String>
+
+      @Attribute("pathSuffix") fun getPathSuffix(): AndroidAttributeValue<String>
+    }
   }
 
+  @DefinesXml
   interface Provider : AndroidDomElement {
     @Attribute("authorities")
     @Convert(AndroidPackageConverter::class)
