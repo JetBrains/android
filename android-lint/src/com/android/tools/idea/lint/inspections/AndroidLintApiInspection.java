2254a6d7f12e9cdb081c1bbc4b243b286bd19885
Tor Norbye <tnorbye@google.com>
2024-07-01 10:25:28 -0700

    Handle SequentialCollection in ApiDetector
    Warn about upcoming rebinding of extension functions, and when there
    is a new API warning (when using API level 35) add quickfix to rewrite
    to safer alternative.
    
    (The IDE-side fix of this is basically updating the API-relate
    inspection, which adds various IDE-implemented quickfixes related
    to API constraints, to not only handle a single LintFix map descriptor
    which supplies the API constraint associated with a particular warning,
    but to also allow a normal quickfix to be attached. We use that here
    to offer a quickfix for these two new methods to replace the new
    platform SequentialCollection methods to be replaced by safe Kotlin
    extension replacements.)
    
    Test: Unit test included
    Bug: 320460092
    Change-Id: I9fbd4478f81843171d0f3a925253c3260d1af0c2
    
    GitOrigin-RevId: 2e82d48df39cb837679f7ed133ccd286236aec88

--- android-lint/src/com/android/tools/idea/lint/inspections/AndroidLintApiInspection.java
+++ android-lint/src/com/android/tools/idea/lint/inspections/AndroidLintApiInspection.java
@@ -30,6 +30,7 @@ import com.android.tools.idea.lint.quickFixes.AddTargetVersionCheckQuickFix;
 import com.android.tools.idea.res.IdeResourcesUtil;
 import com.android.tools.lint.checks.ApiDetector;
 import com.android.tools.lint.detector.api.ApiConstraint;
+import com.android.tools.lint.detector.api.Incident;
 import com.android.tools.lint.detector.api.Issue;
 import com.android.tools.lint.detector.api.LintFix;
 import com.intellij.openapi.application.ApplicationManager;
@@ -51,73 +52,125 @@ public abstract class AndroidLintApiInspection extends AndroidLintInspectionBase
     super(displayName, issue);
   }
 
-  @NotNull
   @Override
-  public LintIdeQuickFix[] getQuickFixes(@NotNull PsiElement startElement,
-                                         @NotNull PsiElement endElement,
-                                         @NotNull String message,
-                                         @Nullable LintFix fixData) {
-    ApiConstraint requirement = LintFix.getApiConstraint(fixData, ApiDetector.KEY_REQUIRES_API, ApiConstraint.UNKNOWN);
-    if (requirement != ApiConstraint.UNKNOWN) {
-      int api = requirement.min();
-      List<LintIdeQuickFix> list = new ArrayList<>();
-      PsiFile file = startElement.getContainingFile();
-      boolean isXml = false;
-      if (file instanceof XmlFile) {
-        isXml = true;
-        ResourceFolderType folderType = IdeResourcesUtil.getFolderType(file);
-        if (folderType != null) {
-          FolderConfiguration config = IdeResourcesUtil.getFolderConfiguration(file);
-          if (config != null) {
-            config.setVersionQualifier(new VersionQualifier(api));
-            String folder = config.getFolderName(folderType);
-            list.add(OverrideResourceAction.createFix(folder));
-          }
+  public @NotNull LintIdeQuickFix[] getQuickFixes(@NotNull PsiElement startElement,
+                                                  @NotNull PsiElement endElement,
+                                                  @NotNull Incident incident) {
+    LintFix fixData = incident.getFix();
+    if (fixData != null) {
+      ApiConstraint requirement = LintFix.getApiConstraint(fixData, ApiDetector.KEY_REQUIRES_API, ApiConstraint.UNKNOWN);
+      if (requirement != ApiConstraint.UNKNOWN) {
+        List<LintIdeQuickFix> list = getApiConstraintFixes(startElement, requirement, fixData);
+        return list.toArray(LintIdeQuickFix.EMPTY_ARRAY);
+      }
+      else {
+        LintIdeQuickFix[] fixes = getCombinedFixes(startElement, incident, fixData);
+        if (fixes == null) {
+          assert false : "Unexpected API fix data: " + fixData;
+        } else {
+          return fixes;
         }
       }
+    }
+    return super.getQuickFixes(startElement, endElement, incident);
+  }
 
-      // Is the API fix limited to applying to (for example) just classes?
-      boolean requireClass = LintFix.getBoolean(fixData, ApiDetector.KEY_REQUIRE_CLASS, false);
-      List<ApiConstraint.SdkApiConstraint> constraints = requirement.getConstraints();
-      Project project = startElement.getProject();
-      if (!requireClass && !isXml) {
-        ApiConstraint minSdk = LintFix.getApiConstraint(fixData, ApiDetector.KEY_MIN_API, ApiConstraint.UNKNOWN);
-        for (ApiConstraint constraint : constraints) {
-          int version = constraint.min();
-          int sdk = constraint.getSdk();
-          list.add(new AddTargetVersionCheckQuickFix(project, version, sdk, minSdk));
-        }
+  private static LintIdeQuickFix @Nullable [] getCombinedFixes(@NotNull PsiElement startElement, @NotNull Incident incident, @NotNull LintFix fixData) {
+    ApiConstraint requirement;
+    if (fixData instanceof LintFix.LintFixGroup) {
+      // We support exactly two types of quickfixes for API errors:
+      // (1) Just an API constraint in a data map -- this implies that we should
+      //     offer IDE-side quickfixes related to the API level, such as adding
+      //     a RequiresApi annotation, suppressing, etc. (This was handled
+      //     above.)
+      // (2) A single regular quickfix descriptor, *and* a constraint data map, combined
+      //     in that order in a fix alternatives group. We'll offer
+      //     the fix followed by the API constraints fixes.
+      LintFix.LintFixGroup group = (LintFix.LintFixGroup)fixData;
+      if (group.getType() != LintFix.GroupType.ALTERNATIVES) {
+        return null;
+      }
+      List<LintFix> fixes = group.getFixes();
+      if (fixes.size() != 2) {
+        return null;
       }
 
-      ApplicationManager.getApplication().assertReadAccessAllowed();
-      ApiConstraint.SdkApiConstraint first = constraints.get(0);
-      if (constraints.size() > 1) {
-        // Just pick the first one: the order is significant and the first item is supposed
-        // to be the recommended one. We don't currently have an annotation mechanism to
-        // declare "you must have *all* of these APIs".
-        if (requiresExtensionAvailable(project)) {
-          list.add(new ModCommandLintQuickFix(new AddTargetApiQuickFix(Collections.singletonList(first), true, startElement, requireClass)));
+      LintIdeQuickFix[] quickFixes = createFixes(startElement.getProject(), startElement.getContainingFile(), incident, fixes.get(0));
+
+      requirement = LintFix.getApiConstraint(fixes.get(1), ApiDetector.KEY_REQUIRES_API, ApiConstraint.UNKNOWN);
+      if (requirement != ApiConstraint.UNKNOWN) {
+        List<LintIdeQuickFix> list = getApiConstraintFixes(startElement, requirement, fixData);
+        LintIdeQuickFix[] joined = new LintIdeQuickFix[quickFixes.length + list.size()];
+        System.arraycopy(quickFixes, 0, joined, 0, quickFixes.length);
+        for (int i = 0; i < list.size(); i++) {
+          joined[i + quickFixes.length] = list.get(i);
         }
-      } else {
-        int sdkId = first.getSdkId();
-        if (sdkId == ANDROID_SDK_ID) {
-          if (!isXml && requiresApiAvailable(project)) {
-            list.add(new ModCommandLintQuickFix(new AddTargetApiQuickFix(constraints, true, startElement, requireClass)));
-          }
-          else {
-            // Discourage use of @TargetApi if @RequiresApi is available; see for example
-            // https://android-review.googlesource.com/c/platform/frameworks/support/+/843915/
-            list.add(new ModCommandLintQuickFix(new AddTargetApiQuickFix(constraints, false, startElement, requireClass)));
-          }
-        } else if (!isXml && requiresExtensionAvailable(project)) {
-          list.add(new ModCommandLintQuickFix(new AddTargetApiQuickFix(constraints, true, startElement, requireClass)));
+        return joined;
+      }
+      return quickFixes;
+    } else {
+      return createFixes(startElement.getProject(), startElement.getContainingFile(), incident, fixData);
+    }
+  }
+
+  private static @NotNull List<LintIdeQuickFix> getApiConstraintFixes(@NotNull PsiElement startElement,
+                                                                      @NotNull ApiConstraint requirement,
+                                                                      @NotNull LintFix fixData) {
+    int api = requirement.min();
+    List<LintIdeQuickFix> list = new ArrayList<>();
+    PsiFile file = startElement.getContainingFile();
+    boolean isXml = false;
+    if (file instanceof XmlFile) {
+      isXml = true;
+      ResourceFolderType folderType = IdeResourcesUtil.getFolderType(file);
+      if (folderType != null) {
+        FolderConfiguration config = IdeResourcesUtil.getFolderConfiguration(file);
+        if (config != null) {
+          config.setVersionQualifier(new VersionQualifier(api));
+          String folder = config.getFolderName(folderType);
+          list.add(OverrideResourceAction.createFix(folder));
         }
       }
-
-      return list.toArray(LintIdeQuickFix.EMPTY_ARRAY);
     }
 
-    return super.getQuickFixes(startElement, endElement, message, fixData);
+    // Is the API fix limited to applying to (for example) just classes?
+    boolean requireClass = LintFix.getBoolean(fixData, ApiDetector.KEY_REQUIRE_CLASS, false);
+    List<ApiConstraint.SdkApiConstraint> constraints = requirement.getConstraints();
+    Project project = startElement.getProject();
+    if (!requireClass && !isXml) {
+      ApiConstraint minSdk = LintFix.getApiConstraint(fixData, ApiDetector.KEY_MIN_API, ApiConstraint.UNKNOWN);
+      for (ApiConstraint constraint : constraints) {
+        int version = constraint.min();
+        int sdk = constraint.getSdk();
+        list.add(new AddTargetVersionCheckQuickFix(project, version, sdk, minSdk));
+      }
+    }
+
+    ApplicationManager.getApplication().assertReadAccessAllowed();
+    ApiConstraint.SdkApiConstraint first = constraints.get(0);
+    if (constraints.size() > 1) {
+      // Just pick the first one: the order is significant and the first item is supposed
+      // to be the recommended one. We don't currently have an annotation mechanism to
+      // declare "you must have *all* of these APIs".
+      if (requiresExtensionAvailable(project)) {
+        list.add(new ModCommandLintQuickFix(new AddTargetApiQuickFix(Collections.singletonList(first), true, startElement, requireClass)));
+      }
+    } else {
+      int sdkId = first.getSdkId();
+      if (sdkId == ANDROID_SDK_ID) {
+        if (!isXml && requiresApiAvailable(project)) {
+          list.add(new ModCommandLintQuickFix(new AddTargetApiQuickFix(constraints, true, startElement, requireClass)));
+        }
+        else {
+          // Discourage use of @TargetApi if @RequiresApi is available; see for example
+          // https://android-review.googlesource.com/c/platform/frameworks/support/+/843915/
+          list.add(new ModCommandLintQuickFix(new AddTargetApiQuickFix(constraints, false, startElement, requireClass)));
+        }
+      } else if (!isXml && requiresExtensionAvailable(project)) {
+        list.add(new ModCommandLintQuickFix(new AddTargetApiQuickFix(constraints, true, startElement, requireClass)));
+      }
+    }
+    return list;
   }
 
   public static boolean requiresApiAvailable(Project project) {
