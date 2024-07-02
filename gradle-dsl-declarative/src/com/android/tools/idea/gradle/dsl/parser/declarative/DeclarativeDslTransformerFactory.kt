42854dd83c324ce1b42c95854475200a1bead773
Sebastian Sellmair <sebastian.sellmair@jetbrains.com>
2024-07-02 15:39:49 +0200

    [android] (Koala) Merge AS 2024.1.2 Canary 7: Move back gradle-dsl-declarative entities to AOSP locations
    GitOrigin-RevId: 94c898d2d515c7ce4f7f265db7eab997418811f1

--- gradle-dsl-declarative/src/com/android/tools/idea/gradle/dsl/parser/declarative/DeclarativeDslTransformerFactory.kt
+++ gradle-dsl-declarative/src/com/android/tools/idea/gradle/dsl/parser/declarative/DeclarativeDslTransformerFactory.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.android.tools.idea.gradle.declarative.dsl.parser.declarative
+package com.android.tools.idea.gradle.dsl.parser.declarative
 
 import com.android.tools.idea.gradle.dsl.model.BuildModelContext
 import com.android.tools.idea.gradle.dsl.parser.GradleDslTransformerFactory
