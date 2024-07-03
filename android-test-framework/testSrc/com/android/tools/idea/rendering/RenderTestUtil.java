44de456655ac08c018991af3514cdd7b2de79b93
Sebastian Sellmair <sebastian.sellmair@jetbrains.com>
2024-07-03 10:03:44 +0200

    [android] (Koala) Merge AS 2024.1.2 Canary 7: RenderTestUtil: Increase leniency for IntelliJ
    GitOrigin-RevId: 711c0a8bcd153989a8e8b482603b46d3fe1e0d34

--- android-test-framework/testSrc/com/android/tools/idea/rendering/RenderTestUtil.java
+++ android-test-framework/testSrc/com/android/tools/idea/rendering/RenderTestUtil.java
@@ -71,7 +71,9 @@ import org.jetbrains.annotations.Nullable;
 public class RenderTestUtil {
   public static final String DEFAULT_DEVICE_ID = "Nexus 4";
   public static final String HOLO_THEME = "@android:style/Theme.Holo";
-  private static final float MAX_PERCENT_DIFFERENT = 1.0f;
+
+  // AS Koala 2024.1.2 Canary 7 Merge: Increased rendering margin for IntelliJ
+  private static final float MAX_PERCENT_DIFFERENT = 2.0f;
 
   /**
    * Method to be called before every render test case. If you are using JUnit 4, use the {@link RenderTestRule} instead.
