0b71fb5d95c79ea7fd1c40d9f0e6ef2f3315457e
Sergio Murguia <srmurguia@google.com>
2024-07-09 11:12:49 -0700

    [SDK Index] Add flag to control notes from developers
    Bug: 336568007
    Test: IdeGooglePlaySdkIndexTest, PsAnalyzerDaemonKtTest
    Test: SdkIndexLintTest, SdkIndexLintWithoutNotesTest
    Test: SdkIndexPsdTest, SdkIndexPsdWithoutNotesTest
    Change-Id: I63c3d4f9d666d58f021368cfa3c9b1f8a72f9952
    
    GitOrigin-RevId: d1fe72d54073c584ba69ce263e740a524acd244d

--- project-system-integration-tests/testSrc/com/android/tools/idea/projectsystem/gradle/SdkIndexPsdWithoutNotesTest.kt
+++ project-system-integration-tests/testSrc/com/android/tools/idea/projectsystem/gradle/SdkIndexPsdWithoutNotesTest.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2022 The Android Open Source Project
+ * Copyright (C) 2024 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,11 +17,11 @@ package com.android.tools.idea.projectsystem.gradle
 
 import org.junit.Test
 
-class SdkIndexPsdTest : SdkIndexTestBase() {
+class SdkIndexPsdWithoutNotesTest : SdkIndexTestBase() {
   @Test
-  fun `Snapshot used by PSD`() {
+  fun `Snapshot used by PSD without notes`() {
     system.installation.addVmOption("-Didea.log.debug.categories=#com.android.tools.idea.gradle.structure.daemon.PsAnalyzerDaemon")
-    system.installation.addVmOption("-Dgoogle.play.sdk.index.show.sdk.index.notes=true")
+    system.installation.addVmOption("-Dgoogle.play.sdk.index.show.sdk.index.notes=false")
     verifySdkIndexIsInitializedAndUsedWhen(
       showFunction = { studio, _ ->
         openAndClosePSD(studio)
@@ -32,8 +32,7 @@ class SdkIndexPsdTest : SdkIndexTestBase() {
       expectedIssues = listOf(
         // Error
         listOf(
-          "**[Prevents app release in Google Play Console]** com.google.android.gms:play-services-ads-lite version 19.4.0 has been reported as problematic by its author and will block publishing of your app to Play Console.",
-          "**Note:** As of June 30th 2023, this version is sunset. For more information, please visit https://developers.google.com/admob/android/deprecation.",
+          "**[Prevents app release in Google Play Console]** com.google.android.gms:play-services-ads-lite version 19.4.0 has been reported as problematic by its author and will block publishing of your app to Play Console",
         ),
         // Error
         listOf(
@@ -41,8 +40,7 @@ class SdkIndexPsdTest : SdkIndexTestBase() {
         ),
         // Error
         listOf(
-          "**[Prevents app release in Google Play Console]** com.startapp:inapp-sdk version 3.9.1 has been reported as problematic by its author and will block publishing of your app to Play Console.",
-          "**Note:** Critical issue has been identified which causes intensive battery consumption.",
+          "**[Prevents app release in Google Play Console]** com.startapp:inapp-sdk version 3.9.1 has been reported as problematic by its author and will block publishing of your app to Play Console",
         ),
         // Error
         listOf(
@@ -66,13 +64,11 @@ class SdkIndexPsdTest : SdkIndexTestBase() {
         ),
         // Info
         listOf(
-          "com.google.android.gms:play-services-safetynet version 10.0.0 has an associated message from its author.",
-          "**Note:** The SafetyNet Attestation API is being discontinued and replaced by the new Play Integrity API. Begin migration as soon as possible to avoid user disruption. The Play Integrity API includes all the integrity signals that SafetyNet Attestation offers and more, like Google Play licensing and better error messaging. Learn more and start migrating at https://developer.android.com/training/safetynet/deprecation-timeline",
+          "com.google.android.gms:play-services-safetynet version 10.0.0 has an associated message from its author",
         ),
         // Info
         listOf(
-          "io.objectbox:objectbox-android version 2.5.1 has an associated message from its author.",
-          "**Note:** This version has known issues that have been fixed in more recent versions. We also have made improvements to the API, performance and compatibility with devices. See https://docs.objectbox.io for details. Please upgrade to version 3.3.1 or newer (compatible with Android Plugin 3.4.0 or newer and Gradle 6.1 or newer). Note that newer versions are available on Maven Central.",
+          "io.objectbox:objectbox-android version 2.5.1 has an associated message from its author",
         ),
       )
     )
