c980c16329f1606912ff8a6e22f4433855597267
Matthew Gharrity <gharrma@google.com>
2024-07-08 20:38:44 -0700

    ModuleCompileTest: use read action
    getPsiValidationState() is annotated with @RequiresReadLock,
    and it throws an error if called without a read action in
    IntelliJ 2024.2.
    
    Test: ModuleCompileTest
    Change-Id: I393f3f62c1d7bb7d96247a71e8a6b888b9e50ffe
    
    GitOrigin-RevId: 536ee885c440904a36eba17fe000c3cec5d5edc5

--- android/run/testSrc/com/android/tools/idea/run/deployment/liveedit/ModuleCompileTest.kt
+++ android/run/testSrc/com/android/tools/idea/run/deployment/liveedit/ModuleCompileTest.kt
@@ -19,8 +19,10 @@ import com.android.tools.idea.run.deployment.liveedit.analysis.directApiCompile
 import com.android.tools.idea.run.deployment.liveedit.analysis.directApiCompileIr
 import com.android.tools.idea.testing.AndroidProjectRule
 import com.android.tools.tests.AdtTestProjectDescriptors
+import com.intellij.openapi.application.runReadAction
 import com.intellij.openapi.command.WriteCommandAction
 import com.intellij.openapi.module.JavaModuleType
+import com.intellij.psi.PsiFile
 import com.intellij.testFramework.PsiTestUtil
 import com.intellij.util.containers.stream
 import junit.framework.Assert
@@ -64,7 +66,7 @@ class ModuleCompileTest {
     val compiler = LiveEditCompiler(projectRule.project, cache, object : ApkClassProvider {
       override fun getClass(ktFile: KtFile, className: String) = apk[className]
     })
-    var output = compile(listOf(LiveEditCompilerInput(file, getPsiValidationState(file))), compiler)
+    var output = compile(listOf(LiveEditCompilerInput(file, readPsiValidationState(file))), compiler)
     var clazz = loadClass(output)
     Assert.assertTrue(clazz.declaredMethods.stream().anyMatch {it.name.contains("foo\$$libModule1Name")})
   }
@@ -82,8 +84,8 @@ class ModuleCompileTest {
     val compiler = LiveEditCompiler(projectRule.project, cache, object : ApkClassProvider {
       override fun getClass(ktFile: KtFile, className: String) = apk[className]
     })
-    val output = compile(listOf(LiveEditCompilerInput(file1, getPsiValidationState(file1)),
-                                LiveEditCompilerInput(file2, getPsiValidationState(file2))), compiler)
+    val output = compile(listOf(LiveEditCompilerInput(file1, readPsiValidationState(file1)),
+                                LiveEditCompilerInput(file2, readPsiValidationState(file2))), compiler)
 
     var clazzA = loadClass(output, "A")
     Assert.assertTrue(clazzA.declaredMethods.stream().anyMatch {it.name.contains("foo\$$libModule1Name")})
@@ -103,6 +105,12 @@ class ModuleCompileTest {
       // TODO: This test is wrong. We should *NOT* be getting a null pointer exception if you allow files of two different module to be
       //  compiled at once. I suspect we are not setting up the modules correctly for it to mirror an actual Android project set up.
       Assert.assertTrue(l.cause is NullPointerException)
+    }
+  }
+
+  private fun readPsiValidationState(file: PsiFile): PsiState {
+    return runReadAction {
+      getPsiValidationState(file)
     }
   }
 }
