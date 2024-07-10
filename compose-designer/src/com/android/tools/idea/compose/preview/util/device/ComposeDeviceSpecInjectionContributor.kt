342018e54872e947735dfc209fb1c29cbb59613e
Jaebaek Seo <jaebaek@google.com>
2024-07-10 16:52:46 -0700

    Allow K2 ComposeDeviceSpecInjectionContributor to call AA on write-action
    ComposeDeviceSpecInjectionContributor is used as a part of code-format
    that can be also a part of template execution. Since the user can
    update PSIs for the template, the code-format can happen right after the
    PSI updates. In that case, we cannot run analysis APIs (AA) for the
    updated PSIs in advance on a background thread, which means we cannot
    avoid running analysis APIs on the UI-thread. In addition, the
    code-format runs on a write-action. This commit allows K2
    ComposeDeviceSpecInjectionContributor to call AA on write-action, which
    is unavoidable.
    
    Bug: 345812375
    Test: PreviewPickerTests
    Change-Id: Ic0a2005a291b2cf0b25c82a57a6e45038435e6d7
    
    GitOrigin-RevId: 4b90faab3bc58576c185007f9f734543096352f1

--- compose-designer/src/com/android/tools/idea/compose/preview/util/device/ComposeDeviceSpecInjectionContributor.kt
+++ compose-designer/src/com/android/tools/idea/compose/preview/util/device/ComposeDeviceSpecInjectionContributor.kt
@@ -15,15 +15,33 @@
  */
 package com.android.tools.idea.compose.preview.util.device
 
+import com.android.tools.compose.COMPOSE_PREVIEW_ANNOTATION_FQN
 import com.android.tools.idea.compose.preview.ComposePreviewAnnotationChecker
+import com.android.tools.idea.kotlin.getFullyQualifiedNameOnWriteActionForK2
 import com.android.tools.idea.preview.util.device.DeviceSpecInjectionContributor
 import com.intellij.psi.PsiElement
 import com.intellij.psi.util.parentOfType
+import org.jetbrains.kotlin.analysis.api.analyze
+import org.jetbrains.kotlin.idea.base.plugin.KotlinPluginModeProvider
 import org.jetbrains.kotlin.psi.KtAnnotationEntry
 
 class ComposeDeviceSpecInjectionContributor : DeviceSpecInjectionContributor() {
+  /**
+   * This function is called as a part of code-format that can be also a part of template execution.
+   * Since it happens right after the user updates PSIs, we cannot run analysis APIs for the PSIs in
+   * advance on a background thread. In this case, we cannot avoid running analysis APIs on the
+   * UI-thread. In addition, the code-format runs on a write-action. This is why this function calls
+   * [KtAnnotationEntry.getFullyQualifiedNameOnWriteAction]. It is known that calling analysis APIs
+   * on a write-action can cause the UI-freeze, but this is a case we cannot avoid it.
+   */
   override fun isInPreviewAnnotation(psiElement: PsiElement): Boolean =
     psiElement.parentOfType<KtAnnotationEntry>()?.let { parent ->
-      ComposePreviewAnnotationChecker.isPreview(parent)
+      if (KotlinPluginModeProvider.isK2Mode()) {
+        analyze(parent) {
+          parent.getFullyQualifiedNameOnWriteActionForK2(this) == COMPOSE_PREVIEW_ANNOTATION_FQN
+        }
+      } else {
+        ComposePreviewAnnotationChecker.isPreview(parent)
+      }
     } ?: false
 }
