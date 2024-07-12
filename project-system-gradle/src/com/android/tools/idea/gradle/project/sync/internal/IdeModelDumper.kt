bb9ecc664c1018f539333475b7561e7b0f4adcca
Hung Nguyen <hungnv@google.com>
2024-07-12 14:28:59 +0100

    Update to KGP 2.0.20-Beta2 for tests (tools/adt/idea)
    Bug: N/A (Regular update)
    Test: Updated
    Change-Id: I299662cab8c160a17048691d3cc845c8d73c1abb
    
    GitOrigin-RevId: 22146142b63eaee589338008ddedac1a757c9607

--- project-system-gradle/src/com/android/tools/idea/gradle/project/sync/internal/IdeModelDumper.kt
+++ project-system-gradle/src/com/android/tools/idea/gradle/project/sync/internal/IdeModelDumper.kt
@@ -231,7 +231,7 @@ private val jbModelDumpers = listOf(
   },
 )
 
-const val KOTLIN_VERSION_FOR_TESTS = "2.0.20-Beta1"
+const val KOTLIN_VERSION_FOR_TESTS = "2.0.20-Beta2"
 
 fun String.replaceKotlinVersionForTests(): String = replace(KOTLIN_VERSION_FOR_TESTS, "<KOTLIN_VERSION_FOR_TESTS>")
 
