334e5c53a834f214455127b1849054f2506a7bc7
bingran <bingran@google.com>
2024-07-17 11:20:57 -0700

    Replace allprojects with lifecycle.beforeProject
    This new API is available from 8.8 which is introduced to address the
    issue of allprojects block not compatible with project isolation.
    
    This change adds a test to verify no project isolation issues related to
    "Initialization script '/xxx/sync.studio.tooling.gradle'"
    
    Bug: 253076415
    Test: ConfigurationCacheFailureTest
    Change-Id: Iefa9e08f2e39b14e397cbde684e2c03b64e46a54
    
    GitOrigin-RevId: ea0cf04c99358d2807bf7e13ff2c3fdff3fece93

---
+++ project-system-gradle/testSrc/com/android/tools/idea/gradle/project/sync/runsGradle/ProjectIsolationTest.kt
@@ -0,0 +1,63 @@
+/*
+ * Copyright (C) 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.gradle.project.sync.runsGradle
+
+import com.android.SdkConstants
+import com.android.tools.idea.gradle.project.sync.snapshots.AndroidCoreTestProject
+import com.android.tools.idea.gradle.project.sync.snapshots.TestProjectDefinition.Companion.prepareTestProject
+import com.android.tools.idea.testing.AndroidProjectRule
+import com.android.tools.idea.testing.IntegrationTestEnvironmentRule
+import com.android.tools.idea.testing.OpenPreparedProjectOptions
+import com.android.tools.idea.testing.openPreparedProject
+import com.google.common.truth.Expect
+import com.google.common.truth.Truth
+import org.junit.Rule
+import org.junit.Test
+
+class ProjectIsolationTest {
+
+  @get:Rule
+  val expect: Expect = Expect.createAndEnableStackTrace()
+
+  @get:Rule
+  val projectRule: IntegrationTestEnvironmentRule = AndroidProjectRule.withIntegrationTestEnvironment()
+
+  @Test
+  fun testProjectIsolationIssues() {
+    val preparedProject = projectRule.prepareTestProject(AndroidCoreTestProject.SIMPLE_APPLICATION)
+    preparedProject.root.resolve(SdkConstants.FN_GRADLE_PROPERTIES).let {
+      it.appendText("\norg.gradle.unsafe.isolated-projects=true")
+    }
+
+    val stdout = StringBuilder()
+    projectRule.openPreparedProject(
+      "project",
+      OpenPreparedProjectOptions(
+        outputHandler = {message ->
+          stdout.append(message)
+        },
+      )
+    ) {
+      Truth.assertThat(stdout.toString()).contains("""
+        5 problems were found storing the configuration cache, 3 of which seem unique.
+      """.trimIndent())
+      // check no issue caused by applying AndroidStudioToolingPlugin plugin in all projects in initialization script '/xxx/sync.studio.tooling.gradle'
+      Truth.assertThat(stdout.toString()).doesNotContain("""
+        Project ':' cannot access 'Project.apply' functionality on subprojects via 'allprojects'
+      """.trimIndent())
+    }
+  }
+}
