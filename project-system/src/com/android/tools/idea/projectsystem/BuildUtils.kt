63d9e9eefd59a600dd466a15350ce32db544ccb7
Ilia Bogdanovich <ilia.bogdanovich@jetbrains.com>
2024-07-18 11:52:59 +0200

    [FL-22892][Android Compose Previews] Removed workaround in KtFileClassProviderImpl.
    GitOrigin-RevId: d562ebaaab8ec390019dcc18a88fff36e0dba802

--- project-system/src/com/android/tools/idea/projectsystem/BuildUtils.kt
+++ project-system/src/com/android/tools/idea/projectsystem/BuildUtils.kt
@@ -20,8 +20,16 @@ import com.intellij.openapi.application.ReadAction
 import com.intellij.openapi.application.runReadAction
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.psi.PsiClass
 import com.intellij.psi.PsiClassOwner
+import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
+import com.intellij.psi.impl.light.AbstractLightClass
+import org.jetbrains.kotlin.asJava.KotlinAsJavaSupport
+import org.jetbrains.kotlin.fileClasses.isJvmMultifileClassFile
+import org.jetbrains.kotlin.psi.KtClassOrObject
+import org.jetbrains.kotlin.psi.KtFile
+import org.jetbrains.kotlin.psi.analysisContext
 
 fun Project.requestBuild(file: VirtualFile) {
   requestBuild(listOf(file))
@@ -42,11 +50,47 @@ fun hasExistingClassFile(psiFile: PsiFile?) = if (psiFile is PsiClassOwner) {
       psiFile.getModuleSystem()
     }
   }
-  runReadAction { psiFile.classes.mapNotNull { it.qualifiedName } }
+  runReadAction { psiFile.findClassesFqNames() }
     .mapNotNull { androidModuleSystem?.getClassFileFinderForSourceFile(runReadAction { psiFile.virtualFile })?.findClassFile(it) }
     .firstOrNull() != null
 }
 else false
+
+private fun PsiFile.findClassesFqNames(): List<String> {
+  return when (this) {
+    is KtFile -> asClassOwner().classes
+    is PsiClassOwner -> classes
+    else -> arrayOf()
+  }.mapNotNull { it.qualifiedName }
+}
+
+private fun KtFile.topLevelDeclarationsAsClasses(): Array<PsiClass> {
+  val result = mutableListOf<PsiClass>()
+  if (!isJvmMultifileClassFile && !hasTopLevelCallables()) return result.toTypedArray()
+
+  val kotlinAsJavaSupport = KotlinAsJavaSupport.getInstance(project)
+  if (analysisContext == null) {
+    kotlinAsJavaSupport.getLightFacade(this)?.let(result::add)
+  } else {
+    result.add(kotlinAsJavaSupport.createFacadeForSyntheticFile(this))
+  }
+
+  return result.toTypedArray()
+}
+
+fun KtFile.asClassOwner(): PsiClassOwner = object : PsiClassOwner by this {
+  override fun getClasses(): Array<out PsiClass?> {
+    val regularClasses = declarations.filterIsInstance<KtClassOrObject>().map { ktClass ->
+      object : AbstractLightClass(manager) {
+        override fun getQualifiedName(): String? = ktClass.fqName?.asString()
+        override fun getDelegate(): PsiClass = error("Not implemented")
+        override fun copy(): PsiElement = error("Not implemented")
+      }
+    }
+    val topLevelDeclarations = topLevelDeclarationsAsClasses()
+    return (regularClasses + topLevelDeclarations).toTypedArray()
+  }
+}
 
 @Suppress("UnstableApiUsage")
 private fun VirtualFile.getSourceFile(): VirtualFile = if (!this.isInLocalFileSystem && this is BackedVirtualFile) {
