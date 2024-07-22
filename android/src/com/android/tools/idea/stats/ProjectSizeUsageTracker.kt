f6720eeac4714a18c6d02d451d21eb091a25a5fd
Madalin Valceleanu <vmadalin@google.com>
2024-07-22 08:55:59 +0000

    Fix 'smart mode' not waiting for UnindexedFilesScannerExecutor to finish
    With IntelliJ 2024.2 waiting for smart mode doesn't necessary guarantee
    that UnindexedFilesScannerExecutor finished to execute indexes files
    tasks since doesn't wait for queued tasks or in progress ones to finish.
    This is causing issues on ReportProjectSizeTask not being able to report
    prover analytics results when requesting FileTypeIndex.processFiles.
    
    For this reason a workaround was added to explicitly wait for file
    indexes tasks to finish while IDEA-356331 get properly addressed.
    However, is important to mention that ReportProjectSizeTask must
    run async during tests to avoid waiting for smart mode when the
    EDT is blocked causing deadlock since dumb mode will never change
    to smart mode due to scanning tasks being processed before or not
    before the EDT was blocked b/354210253.
    
    Bug: 354210253
    Test: SyncedProjectTest
    Change-Id: Ieecbc24051933b115656a1ea62daa0c6bb8c31e7
    
    GitOrigin-RevId: 3a1901249e3fea80800010b93135b740b08f6c72

--- android/src/com/android/tools/idea/stats/ProjectSizeUsageTracker.kt
+++ android/src/com/android/tools/idea/stats/ProjectSizeUsageTracker.kt
@@ -18,6 +18,7 @@ package com.android.tools.idea.stats
 import com.android.tools.analytics.AnalyticsSettings
 import com.android.tools.analytics.UsageTracker
 import com.android.tools.analytics.withProjectId
+import com.android.tools.idea.concurrency.coroutineScope
 import com.android.tools.idea.projectsystem.ProjectSystemSyncManager.SyncResult
 import com.android.tools.idea.projectsystem.ProjectSystemSyncManager.SyncResultListener
 import com.android.tools.idea.serverflags.ServerFlagService
@@ -26,40 +27,43 @@ import com.google.wireless.android.sdk.stats.IntellijProjectSizeStats
 import com.intellij.ide.highlighter.JavaClassFileType
 import com.intellij.ide.highlighter.JavaFileType
 import com.intellij.ide.highlighter.XmlFileType
-import com.intellij.openapi.application.ReadAction
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.application.smartReadAction
 import com.intellij.openapi.fileTypes.FileTypeRegistry
 import com.intellij.openapi.fileTypes.PlainTextFileType
-import com.intellij.openapi.progress.PerformInBackgroundOption
-import com.intellij.openapi.progress.ProgressIndicator
 import com.intellij.openapi.progress.ProgressManager
-import com.intellij.openapi.progress.Task
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.project.UnindexedFilesScannerExecutor
 import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.platform.ide.progress.withBackgroundProgress
 import com.intellij.psi.search.FileTypeIndex
 import com.intellij.psi.search.ProjectScope
 import com.intellij.util.Processor
-import java.util.concurrent.Callable
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import org.jetbrains.annotations.VisibleForTesting
 
 // Upper limit on the number of files we will count. Only 1% of projects have more files than this
 private const val FILE_CAP = 60000
 
 class ProjectSizeUsageTrackerListener(private val project: Project) : SyncResultListener {
+
   override fun syncEnded(result: SyncResult) {
     if (!result.isSuccessful) {
       return
     }
     if (AnalyticsSettings.optedIn) {
-      object : Task.Backgroundable(project, "Computing project size", true, PerformInBackgroundOption.ALWAYS_BACKGROUND) {
-        override fun run(indicator: ProgressIndicator) {
-          ReportProjectSizeTask(project).run()
-        }
-
-      }.queue()
+      project.getService(ReportProjectSizeTask::class.java).run()
     }
   }
 }
 
 class ReportProjectSizeTask(val project: Project) : Runnable {
+
+  @VisibleForTesting
+  val repostStatsJobs = mutableListOf<Job>()
+
   private enum class FileType(private val fileType: com.intellij.openapi.fileTypes.FileType,
                               private val statsFileType: IntellijProjectSizeStats.FileType) {
     JAVA(JavaFileType.INSTANCE, IntellijProjectSizeStats.FileType.JAVA),
@@ -80,36 +84,49 @@ class ReportProjectSizeTask(val project: Project) : Runnable {
   }
 
   override fun run() {
-    val builder = AndroidStudioEvent
-      .newBuilder()
-      .setKind(AndroidStudioEvent.EventKind.INTELLIJ_PROJECT_SIZE_STATS)
-      .withProjectId(project)
-
-    for (fileType in FileType.values()) {
-      val fileCount =
-        try {
-          ReadAction.nonBlocking(Callable {
-            fileCount(fileType)
-          })
-            .inSmartMode(project)
-            .expireWith(project)
-            .executeSynchronously()
-        } catch (e: Exception) {
-          // in the case of any exception (project disposed, or ProcessCanceledException, etc)
-          // we just send an impossible value so that we can track how often such scenarios
-          // occur in the backend
-          -1
-        }
-      builder.addIntellijProjectSizeStats(
-        IntellijProjectSizeStats
+    project.coroutineScope().launch {
+      withBackgroundProgress(project, "Computing project size", true) {
+        val builder = AndroidStudioEvent
           .newBuilder()
-          .setScope(IntellijProjectSizeStats.Scope.PROJECT)
-          .setType(fileType.statsFileType())
-          .setCount(fileCount)
-      )
-    }
+          .setKind(AndroidStudioEvent.EventKind.INTELLIJ_PROJECT_SIZE_STATS)
+          .withProjectId(project)
 
-    UsageTracker.log(builder)
+        FileType.entries.forEach { fileType ->
+          var waitSmartModeWithFileIndexes = true
+          // Smart mode doesn't wait for UnindexedFilesScannerExecutorImpl queued tasks to finish
+          // so this was added to explicitly wait for them until IDEA-356331 gets fixed.
+          while (waitSmartModeWithFileIndexes) {
+            waitSmartModeWithFileIndexes = smartReadAction(project) {
+              if (UnindexedFilesScannerExecutor.getInstance(project).hasQueuedTasks
+                  || UnindexedFilesScannerExecutor.getInstance(project).isRunning.value) {
+                true
+              } else {
+                builder.addIntellijProjectSizeStatsForFileType(fileType)
+                false
+              }
+            }
+            if (waitSmartModeWithFileIndexes) delay(1000)
+          }
+        }
+
+        UsageTracker.log(builder)
+      }
+    }.also {
+      if (ApplicationManager.getApplication().isUnitTestMode) {
+        repostStatsJobs.removeIf { it.isCompleted }
+        repostStatsJobs.add(it)
+      }
+    }
+  }
+
+  private fun AndroidStudioEvent.Builder.addIntellijProjectSizeStatsForFileType(fileType: FileType) {
+    addIntellijProjectSizeStats(
+      IntellijProjectSizeStats
+        .newBuilder()
+        .setScope(IntellijProjectSizeStats.Scope.PROJECT)
+        .setType(fileType.statsFileType())
+        .setCount(fileCount(fileType))
+    )
   }
 
   private fun fileCount(fileType: FileType): Int {
