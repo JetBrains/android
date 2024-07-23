b2b3991cf4d4c7863b5734098cdaa1f3eb094b24
Andy Magee <andymagee@google.com>
2024-07-23 11:06:18 -0700

    Use coroutines instead of pooled threads
    This change is meant to be functionally identical to the previous logic.
    All the actions that were previously run on an executor are now being
    done in a coroutine context, which avoids allocating a lot of extra
    threads. The `Channel` queues are used to ensure only a single action is
    run at a time for each queue, and that the actions are run in the order
    they're submitted.
    
    Switching back to an indicator process via `coroutineToIndicator` is
    technically unsupported (it's an @Internal API meant for helping with
    platform migration). Technically this is a similar migration - it is
    allowing us to migrate the submission of actions to coroutines without
    currently modifying the internal scanning logic. That can still be done
    at any point in the future, but is not necessary for this bug to fix the
    thread allocation issue.
    
    Fixes: 354682619
    Test: ResourceFolderRepositoryTest
    Test: ResourceFolderRepositoryBackgroundActionsTest
    Change-Id: I21e9ffca733d3d10b3768fda0b85d69685cb0760
    
    GitOrigin-RevId: cc8c71f2ce0cef478ba7f9e9d4f254c648616dd4

--- android/src/com/android/tools/idea/res/ResourceFolderRepositoryBackgroundActions.kt
+++ android/src/com/android/tools/idea/res/ResourceFolderRepositoryBackgroundActions.kt
@@ -19,70 +19,110 @@ import com.android.utils.TraceUtils.getStackTrace
 import com.android.utils.TraceUtils.simpleId
 import com.intellij.openapi.Disposable
 import com.intellij.openapi.application.ReadAction
+import com.intellij.openapi.components.Service
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.module.Module
 import com.intellij.openapi.progress.ProcessCanceledException
+import com.intellij.openapi.progress.blockingContext
+import com.intellij.openapi.progress.coroutineToIndicator
 import com.intellij.util.application
-import com.intellij.util.concurrency.AppExecutorUtil
-import java.util.concurrent.RejectedExecutionException
+import com.intellij.util.cancelOnDispose
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.consumeEach
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.supervisorScope
 
 /**
  * Module service responsible for managing background actions taken by [ResourceFolderRepository].
  */
-class ResourceFolderRepositoryBackgroundActions : Disposable {
+class ResourceFolderRepositoryBackgroundActions : Disposable.Default {
 
-  private val updateExecutor =
-    AppExecutorUtil.createBoundedApplicationPoolExecutor("ResourceFolderRepository", 1)
+  private val updateChannel: Channel<Pair<String, Runnable>> = Channel(Channel.UNLIMITED)
 
-  private val wolfQueue =
-    AppExecutorUtil.createBoundedApplicationPoolExecutor("ResourceFolderRepositoryWolfQueue", 1)
+  private val wolfChannel: Channel<Runnable> = Channel(Channel.UNLIMITED)
+
+  init {
+    coroutineScope
+      .launch(Dispatchers.Default) {
+        supervisorScope {
+          updateChannel.consumeEach { (repositorySimpleId, action) ->
+            launch { doRunInUpdateQueue(repositorySimpleId, action) }.join()
+          }
+        }
+      }
+      .cancelOnDispose(this)
+
+    coroutineScope
+      .launch(Dispatchers.Default) {
+        supervisorScope {
+          wolfChannel.consumeEach { launch { blockingContext { it.run() } }.join() }
+        }
+      }
+      .cancelOnDispose(this)
+  }
 
   /**
    * Runs the given update action on [updateExecutor] in a read action. All update actions are
    * executed in the same order they were scheduled.
    */
-  fun scheduleUpdate(repository: Any, updateAction: Runnable) {
+  fun runInUpdateQueue(repository: Any, action: Runnable) {
     val repositorySimpleId = repository.simpleId
-    ResourceUpdateTracer.log { "$repositorySimpleId.scheduleUpdate scheduling $updateAction" }
+    ResourceUpdateTracer.log { "$repositorySimpleId.scheduleUpdate scheduling $action" }
 
-    try {
-      updateExecutor.execute {
-        ResourceUpdateTracer.log { "$repositorySimpleId: Update $updateAction started" }
-        try {
-          ReadAction.nonBlocking(updateAction).expireWith(this).executeSynchronously()
-          ResourceUpdateTracer.log { "$repositorySimpleId: Update $updateAction finished" }
-        } catch (e: ProcessCanceledException) {
-          ResourceUpdateTracer.log { "$repositorySimpleId: Update $updateAction was canceled" }
-          // The current update action has been canceled. Proceed to the next one in the queue.
-        } catch (e: Throwable) {
+    // trySend always succeeds since this channel is created with UNLIMITED.
+    updateChannel.trySend(Pair(repositorySimpleId, action))
+  }
+
+  fun runInWolfQueue(action: Runnable) {
+    // trySend always succeeds since this channel is created with UNLIMITED.
+    wolfChannel.trySend(action)
+  }
+
+  private suspend fun doRunInUpdateQueue(repositorySimpleId: String, action: Runnable) {
+    ResourceUpdateTracer.log { "$repositorySimpleId: Update $action started" }
+    // coroutineToIndicator is needed because ResourceFolderRepository.scheduleScan puts an action
+    // on the queue to run here that internally uses a progress indicator. The logic it runs can be
+    // interrupted without going back to an indicator context, which leads to logical errors (ie,
+    // resource files don't get scanned).
+    coroutineToIndicator {
+      try {
+        ReadAction.nonBlocking(action).expireWith(this).executeSynchronously()
+        ResourceUpdateTracer.log { "$repositorySimpleId: Update $action finished" }
+      } catch (e: Throwable) {
+        if (e is ProcessCanceledException) {
+          ResourceUpdateTracer.log { "$repositorySimpleId: Update $action was canceled" }
+        } else {
           ResourceUpdateTracer.log {
-            "$repositorySimpleId: Update $updateAction finished with exception $e\n${getStackTrace(e)}"
+            "$repositorySimpleId: Update $action finished with exception $e\n${getStackTrace(e)}"
           }
           thisLogger().error(e)
         }
       }
-    } catch (ignore: RejectedExecutionException) {
-      // The executor has been shut down.
     }
   }
 
-  fun submitToWolfQueue(task: Runnable) {
-    wolfQueue.submit(task)
-  }
-
-  override fun dispose() {
-    updateExecutor.shutdownNow()
-    wolfQueue.shutdownNow()
-  }
-
   companion object {
+    private val coroutineScope
+      get() = application.service<ScopeService>().coroutineScope
+
     @JvmStatic
     fun getInstance(module: Module) = module.service<ResourceFolderRepositoryBackgroundActions>()
 
     @JvmStatic
-    fun executeOnPooledThread(action: Runnable) {
-      application.executeOnPooledThread(action)
+    fun runInBackground(action: Runnable) {
+      coroutineScope.launch(Dispatchers.Default) { supervisorScope { launch { action.run() } } }
     }
   }
+
+  /**
+   * Service that provides a coroutine scope to [ResourceFolderRepositoryBackgroundActions].
+   *
+   * The platform does not provide coroutine scopes to module services, so one can't be sent
+   * directly to [ResourceFolderRepositoryBackgroundActions]. This service can be used instead,
+   * since each module service can share the same scope and just use separate [Channel]s.
+   */
+  @Service private class ScopeService(val coroutineScope: CoroutineScope)
 }
