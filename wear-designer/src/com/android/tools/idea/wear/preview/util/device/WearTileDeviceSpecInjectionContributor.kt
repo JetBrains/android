0725697d3b7b8a802858f7df891cc6bbebe4c304
Paul Milian <paulmilian@google.com>
2024-07-29 17:34:01 +0100

    Fix WearTilePreviewDeviceSpecInspectionTest for k2
    The issue was coming from WearTileDeviceSpecInjectionContributor when
    applying the fix. WearTileDeviceSpecInjectionContributor was failing
    due to K2, for the same reason as ComposeDeviceSpecInjectionContributor
    in Ic0a2005a291b2cf0b25c82a: we need to allow running an analysis in
    the EDT and write action as there is no other choice for the
    injection contributor.
    
    This CL also introduces a test for
    WearTileDeviceSpecInjectionContributor to make it easier to catch the
    issue.
    
    Fixes: 355606883
    Test: WearTilePreviewDeviceSpecInspectionTest and
     WearTileDeviceSpecInjectionContributorTest with k2 enabled
    Change-Id: Id06929fef9bc749ee0b71a48718b72675bef9f62
    
    GitOrigin-RevId: 46ac1fe7612e5c0652d8bf18c3646fba9d7e9285

--- wear-designer/src/com/android/tools/idea/wear/preview/util/device/WearTileDeviceSpecInjectionContributor.kt
+++ wear-designer/src/com/android/tools/idea/wear/preview/util/device/WearTileDeviceSpecInjectionContributor.kt
@@ -16,6 +16,7 @@
 package com.android.tools.idea.wear.preview.util.device
 
 import com.android.tools.idea.kotlin.fqNameMatches
+import com.android.tools.idea.kotlin.getFullyQualifiedNameOnWriteActionForK2
 import com.android.tools.idea.preview.util.device.DeviceSpecInjectionContributor
 import com.android.tools.idea.wear.preview.TILE_PREVIEW_ANNOTATION_FQ_NAME
 import com.intellij.lang.java.JavaLanguage
@@ -23,14 +24,30 @@ import com.intellij.psi.PsiAnnotation
 import com.intellij.psi.PsiElement
 import com.intellij.psi.util.parentOfType
 import org.jetbrains.kotlin.idea.KotlinLanguage
+import org.jetbrains.kotlin.idea.base.plugin.KotlinPluginModeProvider
 import org.jetbrains.kotlin.psi.KtAnnotationEntry
 
 class WearTileDeviceSpecInjectionContributor : DeviceSpecInjectionContributor() {
+  /**
+   * This function is called as a part of code-format that can be also a part of template execution.
+   * Since it happens right after the user updates PSIs, we cannot run analysis APIs for the PSIs in
+   * advance on a background thread. In this case, we cannot avoid running analysis APIs on the
+   * UI-thread. In addition, the code-format runs on a write-action. This is why this function calls
+   * [KtAnnotationEntry.getFullyQualifiedNameOnWriteActionForK2]. It is known that calling analysis
+   * APIs on a write-action can cause the UI-freeze, but this is a case we cannot avoid it.
+   */
   override fun isInPreviewAnnotation(psiElement: PsiElement): Boolean =
     when (psiElement.language) {
       is KotlinLanguage -> {
-        psiElement.parentOfType<KtAnnotationEntry>()?.fqNameMatches(TILE_PREVIEW_ANNOTATION_FQ_NAME)
-          ?: false
+        psiElement.parentOfType<KtAnnotationEntry>()?.let { parent ->
+          if (KotlinPluginModeProvider.isK2Mode()) {
+            parent.getFullyQualifiedNameOnWriteActionForK2() == TILE_PREVIEW_ANNOTATION_FQ_NAME
+          } else {
+            psiElement
+              .parentOfType<KtAnnotationEntry>()
+              ?.fqNameMatches(TILE_PREVIEW_ANNOTATION_FQ_NAME)
+          }
+        } ?: false
       }
       is JavaLanguage -> {
         psiElement.parentOfType<PsiAnnotation>()?.hasQualifiedName(TILE_PREVIEW_ANNOTATION_FQ_NAME)
