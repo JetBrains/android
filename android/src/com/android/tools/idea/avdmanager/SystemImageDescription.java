cafea56138523125d54e69a7b331f617ff1a544a
Juan C Nuno <juancnuno@google.com>
2024-07-29 15:28:06 -0700

    Move RemoteSystemImage
    Bug: Not applicable
    Test: Not applicable
    Change-Id: I5946a6c32d4b01e4ad89a4d0577c5d087677700f
    
    GitOrigin-RevId: 30c632bd88b07bddcee1a520d2fa856ec6f52039

--- android/src/com/android/tools/idea/avdmanager/SystemImageDescription.java
+++ android/src/com/android/tools/idea/avdmanager/SystemImageDescription.java
@@ -18,24 +18,21 @@ package com.android.tools.idea.avdmanager;
 import static com.android.sdklib.SystemImageTags.ANDROID_TV_TAG;
 import static com.android.sdklib.SystemImageTags.GOOGLE_TV_TAG;
 
-import com.android.annotations.NonNull;
 import com.android.repository.Revision;
 import com.android.repository.api.RemotePackage;
 import com.android.repository.api.RepoPackage;
 import com.android.repository.impl.meta.TypeDetails;
 import com.android.sdklib.AndroidVersion;
 import com.android.sdklib.ISystemImage;
+import com.android.sdklib.RemoteSystemImage;
 import com.android.sdklib.SdkVersionInfo;
 import com.android.sdklib.SystemImageTags;
 import com.android.sdklib.repository.IdDisplay;
 import com.android.sdklib.repository.meta.DetailsTypes;
-import com.android.sdklib.repository.meta.DetailsTypes.ApiDetailsType;
 import com.android.sdklib.repository.targets.PlatformTarget;
 import com.google.common.base.Objects;
-import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
 import java.util.Set;
 import org.jetbrains.annotations.NotNull;
@@ -187,124 +184,5 @@ public final class SystemImageDescription {
 
   public ISystemImage getSystemImage() {
     return mySystemImage;
-  }
-
-  private static class RemoteSystemImage implements ISystemImage {
-    private final RemotePackage myRemotePackage;
-    private final ImmutableList<IdDisplay> myTags;
-    private final IdDisplay myVendor;
-    private final List<String> myAbis;
-    private final List<String> myTranslatedAbis;
-    private final AndroidVersion myAndroidVersion;
-
-    public RemoteSystemImage(RemotePackage p) {
-      var details = p.getTypeDetails();
-      assert details instanceof ApiDetailsType;
-
-      IdDisplay vendor = null;
-      var apiDetailsType = (ApiDetailsType)details;
-
-      if (details instanceof DetailsTypes.AddonDetailsType) {
-        vendor = ((DetailsTypes.AddonDetailsType)details).getVendor();
-      }
-      if (details instanceof DetailsTypes.SysImgDetailsType) {
-        vendor = ((DetailsTypes.SysImgDetailsType)details).getVendor();
-      }
-
-      myRemotePackage = p;
-      myTags = SystemImageTags.getTags(p);
-      myVendor = vendor;
-      myAbis = apiDetailsType.getAbis();
-      myTranslatedAbis = apiDetailsType.getTranslatedAbis();
-      myAndroidVersion = apiDetailsType.getAndroidVersion();
-    }
-
-    @NonNull
-    @Override
-    public Path getLocation() {
-      assert false : "Can't get location for remote image";
-      return Paths.get("");
-    }
-
-    @NonNull
-    @Override
-    public List<IdDisplay> getTags() {
-      return myTags;
-    }
-
-    @com.android.annotations.Nullable
-    @Override
-    public IdDisplay getAddonVendor() {
-      return myVendor;
-    }
-
-    @NotNull
-    @Override
-    public String getPrimaryAbiType() {
-      return myAbis.get(0);
-    }
-
-    @NonNull
-    @Override
-    public List<String> getAbiTypes() {
-      return myAbis;
-    }
-
-    @NotNull
-    @Override
-    public List<String> getTranslatedAbiTypes() {
-      return myTranslatedAbis;
-    }
-
-    @NonNull
-    @Override
-    public List<Path> getSkins() {
-      return ImmutableList.of();
-    }
-
-    @NonNull
-    @Override
-    public Revision getRevision() {
-      return myRemotePackage.getVersion();
-    }
-
-    @NonNull
-    @Override
-    public AndroidVersion getAndroidVersion() {
-      return myAndroidVersion;
-    }
-
-    @NotNull
-    @Override
-    public RepoPackage getPackage() {
-      return myRemotePackage;
-    }
-
-    @Override
-    public boolean obsolete() {
-      return myRemotePackage.obsolete();
-    }
-
-    @Override
-    public int compareTo(ISystemImage o) {
-      if (o instanceof RemoteSystemImage) {
-        return myRemotePackage.compareTo(((RemoteSystemImage)o).myRemotePackage);
-      }
-      return 1;
-    }
-
-    @Override
-    public int hashCode() {
-      return myRemotePackage.hashCode();
-    }
-
-    @Override
-    public boolean equals(Object o) {
-      if (!(o instanceof RemoteSystemImage)) {
-        return false;
-      }
-      RemoteSystemImage other = (RemoteSystemImage) o;
-      return myRemotePackage.equals(other.myRemotePackage);
-    }
   }
 }
