821c685084080fa3760c389aa60c01ad2c17b2a6
Nickolay Viuginov <viuginick@google.com>
2024-07-30 16:20:05 +0000

    Extract FrameInfo and ThreadCallTree in the util package
    Merging stack traces is a logic that may also be useful outside of
    ThreadSamplingReportContributor, for instance in the typing latency
    monitoring.
    
    Bug: N/A
    Test: Existing tests
    Change-Id: Ibccce58e560dbb8feff43666ffaa3846751eb4e3
    
    GitOrigin-RevId: 6b9035cc49e8909010f87686111641dc6dbc7af7

---
+++ android/src/com/android/tools/idea/diagnostics/util/ThreadCallTree.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.diagnostics.util;
+
+import java.lang.management.ThreadInfo;
+
+public class ThreadCallTree {
+  private long myThreadId;
+  private final String myThreadName;
+
+  public String getThreadName() {
+    return myThreadName;
+  }
+
+  public FrameInfo getRootFrame() {
+    return myRootFrame;
+  }
+
+  private final FrameInfo myRootFrame;
+
+  public ThreadCallTree(long threadId, String threadName) {
+    myThreadId = threadId;
+    myThreadName = threadName;
+    myRootFrame = new FrameInfo(null);
+  }
+
+  public void addThreadInfo(ThreadInfo ti, long timeSpent) {
+    myRootFrame.addThreadInfo(ti, timeSpent);
+  }
+
+  public String getReportString(long frameTimeIgnoreThresholdMs) {
+    return myThreadName + ", TID: " + myThreadId + myRootFrame.getReportString(frameTimeIgnoreThresholdMs);
+  }
+
+}
