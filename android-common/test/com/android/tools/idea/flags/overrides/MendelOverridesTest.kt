2087099638d56317b27f2a76bc7a0b159b92a51c
Blake Gallay <blakegallay@google.com>
2024-07-31 16:49:13 -0700

    Add infrastructure for overriding flags with Mendel experiments
    (referring to the previous commits in this chain):
    
    To get tests to work, the android-common tests had to be extracted
    to a new module, and a dependency on intellij.android.testFramework
    has been added. Without these changes, the new extension point in
    server-flags.xml doesn't get correctly exposed during tests.
    
    I think that this is the case because the Android plugin is
    only loaded if all of its dependencies are available, and the transitive
    closure of e.g. intellij.android.testFramework covers all of the
    necessary dependencies.
    
    Bug: 342183628
    Test: MendelOverridesTest
    Change-Id: I522f3d4d7657167c689acb2608ac8bd780eeddb2
    
    GitOrigin-RevId: 77e1322668b23636f6355c1768342a7cf8bc103b

---
+++ android-common/test/com/android/tools/idea/flags/overrides/MendelOverridesTest.kt
@@ -0,0 +1,71 @@
+/*
+ * Copyright (C) 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.flags.overrides
+
+import com.android.flags.BooleanFlag
+import com.android.flags.FlagGroup
+import com.android.flags.Flags
+import com.android.flags.ImmutableFlagOverrides
+import com.android.flags.MendelFlag
+import com.android.testutils.MockitoKt.whenever
+import com.android.tools.idea.mendel.MendelFlagsProvider
+import com.google.common.truth.Truth.assertThat
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.testFramework.ApplicationRule
+import com.intellij.testFramework.DisposableRule
+import com.intellij.testFramework.registerExtension
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.Mockito.mock
+
+private const val TEST_GROUP = "testgroup"
+
+class MendelOverridesTest {
+  @get:Rule
+  val appRule = ApplicationRule()
+
+  @get:Rule
+  val disposableRule = DisposableRule()
+
+  private val service = mock(MendelFlagsProvider::class.java)
+
+  @Before
+  fun setUp() {
+    ApplicationManager.getApplication().registerExtension(
+      MendelFlagsProvider.EP_NAME, service, disposableRule.disposable
+    )
+  }
+
+  @Test
+  fun testMendelOverrides() {
+    val overrides: ImmutableFlagOverrides = MendelOverrides()
+    val flags = Flags(overrides)
+    val group = FlagGroup(flags, TEST_GROUP, "display")
+    val flagA = MendelFlag(group, "a", 1, "name_a", "description_a", false)
+    val flagB = MendelFlag(group, "b", 2, "name_b", "description_b", true)
+    val flagC = BooleanFlag(group, "c", "name_c", "description_c", true)
+    assertThat(overrides.get(flagA)).isEqualTo("false")
+    assertThat(overrides.get(flagB)).isEqualTo("false")
+    assertThat(overrides.get(flagC)).isNull()
+
+    whenever(service.getActiveExperimentIds()).thenReturn(listOf(1))
+
+    assertThat(overrides.get(flagA)).isEqualTo("true")
+    assertThat(overrides.get(flagB)).isEqualTo("false")
+    assertThat(overrides.get(flagC)).isNull()
+  }
+}
