8b7adcf8c5fa3dc7fbf9b92852945cce05d5cae4
Chris Sinco <csinco@google.com>
2024-08-07 15:22:29 -0700

    Reapply "Quick fix to address new icon loading in IJP 242"
    This reverts commit a4598a8ee5e10c476d590c5fe219887666416171.
    
    Test: n/a
    Bug: n/a
    Change-Id: I36b88e30efe2bed33a1e204d7f064af21e7f7eee
    
    GitOrigin-RevId: 8c82bbdcaa54c7444439a2da86997e35eee8727e

--- profilers-ui/src/com/android/tools/profilers/taskbased/common/icons/DeviceIconUtils.kt
+++ profilers-ui/src/com/android/tools/profilers/taskbased/common/icons/DeviceIconUtils.kt
@@ -16,34 +16,34 @@
 package com.android.tools.profilers.taskbased.common.icons
 
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.painter.Painter
 import icons.StudioIcons
 import icons.StudioIconsCompose
+import org.jetbrains.jewel.ui.icon.IntelliJIconKey
 import javax.swing.Icon
 
 object DeviceIconUtils {
   @Composable
-  fun getDeviceIconPainter(swingIcon: Icon?): Painter? {
-    val resourcePainterProvider = when (swingIcon) {
+  fun getDeviceIconKey(swingIcon: Icon?): IntelliJIconKey? {
+    val iconKey = when (swingIcon) {
       // Phone + Tablet
-      StudioIcons.DeviceExplorer.VIRTUAL_DEVICE_PHONE -> StudioIconsCompose.DeviceExplorer.VirtualDevicePhone()
-      StudioIcons.DeviceExplorer.PHYSICAL_DEVICE_PHONE -> StudioIconsCompose.DeviceExplorer.PhysicalDevicePhone()
-      StudioIcons.DeviceExplorer.FIREBASE_DEVICE_PHONE -> StudioIconsCompose.DeviceExplorer.FirebaseDevicePhone()
+      StudioIcons.DeviceExplorer.VIRTUAL_DEVICE_PHONE -> StudioIconsCompose.DeviceExplorer.VirtualDevicePhone
+      StudioIcons.DeviceExplorer.PHYSICAL_DEVICE_PHONE -> StudioIconsCompose.DeviceExplorer.PhysicalDevicePhone
+      StudioIcons.DeviceExplorer.FIREBASE_DEVICE_PHONE -> StudioIconsCompose.DeviceExplorer.FirebaseDevicePhone
       // Watch
-      StudioIcons.DeviceExplorer.VIRTUAL_DEVICE_WEAR -> StudioIconsCompose.DeviceExplorer.VirtualDeviceWear()
-      StudioIcons.DeviceExplorer.PHYSICAL_DEVICE_WEAR -> StudioIconsCompose.DeviceExplorer.PhysicalDeviceWear()
-      StudioIcons.DeviceExplorer.FIREBASE_DEVICE_WEAR -> StudioIconsCompose.DeviceExplorer.FirebaseDeviceWear()
+      StudioIcons.DeviceExplorer.VIRTUAL_DEVICE_WEAR -> StudioIconsCompose.DeviceExplorer.VirtualDeviceWear
+      StudioIcons.DeviceExplorer.PHYSICAL_DEVICE_WEAR -> StudioIconsCompose.DeviceExplorer.PhysicalDeviceWear
+      StudioIcons.DeviceExplorer.FIREBASE_DEVICE_WEAR -> StudioIconsCompose.DeviceExplorer.FirebaseDeviceWear
       // TV
-      StudioIcons.DeviceExplorer.VIRTUAL_DEVICE_TV -> StudioIconsCompose.DeviceExplorer.VirtualDeviceTv()
-      StudioIcons.DeviceExplorer.PHYSICAL_DEVICE_TV -> StudioIconsCompose.DeviceExplorer.PhysicalDeviceTv()
-      StudioIcons.DeviceExplorer.FIREBASE_DEVICE_TV -> StudioIconsCompose.DeviceExplorer.FirebaseDeviceTv()
+      StudioIcons.DeviceExplorer.VIRTUAL_DEVICE_TV -> StudioIconsCompose.DeviceExplorer.VirtualDeviceTv
+      StudioIcons.DeviceExplorer.PHYSICAL_DEVICE_TV -> StudioIconsCompose.DeviceExplorer.PhysicalDeviceTv
+      StudioIcons.DeviceExplorer.FIREBASE_DEVICE_TV -> StudioIconsCompose.DeviceExplorer.FirebaseDeviceTv
       // Auto
-      StudioIcons.DeviceExplorer.VIRTUAL_DEVICE_CAR -> StudioIconsCompose.DeviceExplorer.VirtualDeviceCar()
-      StudioIcons.DeviceExplorer.PHYSICAL_DEVICE_CAR -> StudioIconsCompose.DeviceExplorer.PhysicalDeviceCar()
-      StudioIcons.DeviceExplorer.FIREBASE_DEVICE_CAR -> StudioIconsCompose.DeviceExplorer.FirebaseDeviceCar()
+      StudioIcons.DeviceExplorer.VIRTUAL_DEVICE_CAR -> StudioIconsCompose.DeviceExplorer.VirtualDeviceCar
+      StudioIcons.DeviceExplorer.PHYSICAL_DEVICE_CAR -> StudioIconsCompose.DeviceExplorer.PhysicalDeviceCar
+      StudioIcons.DeviceExplorer.FIREBASE_DEVICE_CAR -> StudioIconsCompose.DeviceExplorer.FirebaseDeviceCar
       // Icon not found
       else -> null
     }
-    return resourcePainterProvider?.getPainter()?.value
+    return iconKey
   }
 }
--- profilers-ui/src/com/android/tools/profilers/taskbased/common/icons/TaskIconUtils.kt
+++ profilers-ui/src/com/android/tools/profilers/taskbased/common/icons/TaskIconUtils.kt
@@ -16,10 +16,10 @@
 package com.android.tools.profilers.taskbased.common.icons
 
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.painter.Painter
 import com.android.tools.profilers.tasks.ProfilerTaskType
 import icons.StudioIcons
 import icons.StudioIconsCompose
+import org.jetbrains.jewel.ui.icon.IntelliJIconKey
 import javax.swing.Icon
 
 object TaskIconUtils {
@@ -27,18 +27,18 @@ object TaskIconUtils {
    * Return the corresponding large task icon painter for a task type. To be used in the task selection grid.
    */
   @Composable
-  fun getLargeTaskIconPainter(taskType: ProfilerTaskType): Painter {
-    val resourcePainterProvider = when (taskType) {
+  fun getLargeTaskIconKey(taskType: ProfilerTaskType): IntelliJIconKey {
+    val iconKey = when (taskType) {
       ProfilerTaskType.UNSPECIFIED -> throw IllegalStateException("No task icon is available for the UNSPECIFIED task type.")
-      ProfilerTaskType.CALLSTACK_SAMPLE -> StudioIconsCompose.Profiler.Taskslarge.CallstackSampleLarge()
-      ProfilerTaskType.SYSTEM_TRACE -> StudioIconsCompose.Profiler.Taskslarge.SystemTraceLarge()
-      ProfilerTaskType.JAVA_KOTLIN_METHOD_RECORDING -> StudioIconsCompose.Profiler.Taskslarge.JavaKotlinMethodTraceLarge()
-      ProfilerTaskType.HEAP_DUMP -> StudioIconsCompose.Profiler.Taskslarge.HeapDumpLarge()
-      ProfilerTaskType.NATIVE_ALLOCATIONS -> StudioIconsCompose.Profiler.Taskslarge.NativeAllocationsLarge()
-      ProfilerTaskType.JAVA_KOTLIN_ALLOCATIONS -> StudioIconsCompose.Profiler.Taskslarge.JavaKotlinAllocationsLarge()
-      ProfilerTaskType.LIVE_VIEW -> StudioIconsCompose.Profiler.Taskslarge.LiveViewLarge()
+      ProfilerTaskType.CALLSTACK_SAMPLE -> StudioIconsCompose.Profiler.Taskslarge.CallstackSampleLarge
+      ProfilerTaskType.SYSTEM_TRACE -> StudioIconsCompose.Profiler.Taskslarge.SystemTraceLarge
+      ProfilerTaskType.JAVA_KOTLIN_METHOD_RECORDING -> StudioIconsCompose.Profiler.Taskslarge.JavaKotlinMethodTraceLarge
+      ProfilerTaskType.HEAP_DUMP -> StudioIconsCompose.Profiler.Taskslarge.HeapDumpLarge
+      ProfilerTaskType.NATIVE_ALLOCATIONS -> StudioIconsCompose.Profiler.Taskslarge.NativeAllocationsLarge
+      ProfilerTaskType.JAVA_KOTLIN_ALLOCATIONS -> StudioIconsCompose.Profiler.Taskslarge.JavaKotlinAllocationsLarge
+      ProfilerTaskType.LIVE_VIEW -> StudioIconsCompose.Profiler.Taskslarge.LiveViewLarge
     }
-    return resourcePainterProvider.getPainter().value
+    return iconKey
   }
 
   /**
