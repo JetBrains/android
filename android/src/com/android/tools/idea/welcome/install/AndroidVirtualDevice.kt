7be9476038ef167b5371023628a11fb81da48f76
James Rose <rosej@google.com>
2024-08-09 12:11:55 -0700

    Use AvdBuilder in initial device creation
    Remove check for doesSystemImageSupportQemu2, which has been true at
    least since API 10.
    
    Bug: 339875083
    Test: AndroidVirtualDeviceTest
    Change-Id: I56fe1f3cb3b4c080f2fe03e04a4e418e6ad4b6aa
    
    GitOrigin-RevId: 0157b4837fe9c81d50cf759d4ae65403c73b5826

--- android/src/com/android/tools/idea/welcome/install/AndroidVirtualDevice.kt
+++ android/src/com/android/tools/idea/welcome/install/AndroidVirtualDevice.kt
@@ -17,18 +17,20 @@ package com.android.tools.idea.welcome.install
 
 import com.android.SdkConstants
 import com.android.repository.api.RemotePackage
-import com.android.resources.ScreenOrientation
 import com.android.sdklib.AndroidVersion
 import com.android.sdklib.devices.Abi
 import com.android.sdklib.devices.Device
-import com.android.sdklib.devices.DeviceManager
 import com.android.sdklib.devices.Storage
+import com.android.sdklib.internal.avd.AvdCamera
 import com.android.sdklib.internal.avd.AvdInfo
-import com.android.sdklib.internal.avd.ConfigKey
+import com.android.sdklib.internal.avd.AvdNames
 import com.android.sdklib.internal.avd.EmulatedProperties
 import com.android.sdklib.internal.avd.GpuMode
-import com.android.sdklib.internal.avd.HardwareProperties
 import com.android.sdklib.internal.avd.InternalSdCard
+import com.android.sdklib.internal.avd.OnDiskSkin
+import com.android.sdklib.internal.avd.defaultGenericSkin
+import com.android.sdklib.internal.avd.uniquifyAvdName
+import com.android.sdklib.internal.avd.uniquifyDisplayName
 import com.android.sdklib.repository.AndroidSdkHandler
 import com.android.sdklib.repository.IdDisplay
 import com.android.sdklib.repository.meta.DetailsTypes
@@ -37,9 +39,9 @@ import com.android.tools.idea.avdmanager.AvdManagerConnection
 import com.android.tools.idea.avdmanager.DeviceManagerConnection
 import com.android.tools.idea.avdmanager.DeviceSkinUpdaterService
 import com.android.tools.idea.avdmanager.SystemImageDescription
-import com.android.tools.idea.avdmanager.ui.AvdOptionsModel
 import com.android.tools.idea.avdmanager.ui.AvdWizardUtils
 import com.android.tools.idea.progress.StudioLoggerProgressIndicator
+import com.android.tools.idea.sdk.IdeAvdManagers
 import com.android.tools.idea.welcome.wizard.deprecated.InstallComponentsPath.findLatestPlatform
 import com.android.tools.idea.welcome.wizard.deprecated.ProgressStep
 import com.google.wireless.android.sdk.stats.ProductDetails
@@ -93,28 +95,36 @@ class AndroidVirtualDevice(private val androidVersion: AndroidVersion?, installU
 
   @Throws(WizardException::class)
   fun createAvd(sdkHandler: AndroidSdkHandler): AvdInfo? {
-    val avdManager = AvdManagerConnection.getAvdManagerConnection(sdkHandler)
-    val d = getDevice(sdkHandler.location!!)
+    val avdManager = IdeAvdManagers.getAvdManager(sdkHandler)
+    val device = getDevice(sdkHandler.location!!)
     val systemImageDescription = getSystemImageDescription(sdkHandler)
-    val sdCard = InternalSdCard(EmulatedProperties.DEFAULT_INTERNAL_STORAGE.size)
-    val hardwareSkinPath = d.defaultHardware.skinFile?.let { sdkHandler.toCompatiblePath(it) }
-      ?.let { defaultHardwareSkin ->
-        DeviceSkinUpdaterService.getInstance().updateSkins(defaultHardwareSkin, systemImageDescription).get()
-      }
 
-    val displayName = avdManager.getDefaultDeviceDisplayName(d, systemImageDescription.version)
-    val internalName = avdManager.cleanAvdName(displayName, true)
-    val abi = Abi.getEnum(systemImageDescription.primaryAbiType)
-    val useRanchu = AvdManagerConnection.doesSystemImageSupportQemu2(systemImageDescription)
-    val supportsSmp = abi != null && abi.supportsMultipleCpuCores() && AvdWizardUtils.getMaxCpuCores() > 1
-    val settings = getAvdSettings(displayName, internalName, d)
-    if (useRanchu) {
-      settings[ConfigKey.CPU_CORES] = if (supportsSmp) AvdWizardUtils.getMaxCpuCores().toString() else "1"
+    val avdBuilder = avdManager.createAvdBuilder(device)
+    with(avdBuilder) {
+      displayName = avdManager.uniquifyDisplayName(AvdNames.getDefaultDeviceDisplayName(device, systemImageDescription.version))
+      avdName = avdManager.uniquifyAvdName(AvdNames.cleanAvdName(displayName))
+      systemImage = systemImageDescription.systemImage
+      sdCard = InternalSdCard(EmulatedProperties.DEFAULT_INTERNAL_STORAGE.size)
+      skin = device.defaultHardware.skinFile?.let { sdkHandler.toCompatiblePath(it) }?.let { defaultHardwareSkin ->
+        OnDiskSkin(DeviceSkinUpdaterService.getInstance().updateSkins(defaultHardwareSkin, systemImageDescription).get())
+      } ?: device.defaultGenericSkin()
+
+      gpuMode = GpuMode.AUTO
+      backCamera = AvdCamera.VIRTUAL_SCENE
+      frontCamera = AvdCamera.EMULATED
+      enableKeyboard = true
+      networkLatency = EmulatedProperties.DEFAULT_NETWORK_LATENCY
+      networkSpeed = EmulatedProperties.DEFAULT_NETWORK_SPEED
+      ram = DEFAULT_RAM_SIZE
+      internalStorage = DEFAULT_RAM_SIZE
+      vmHeap = DEFAULT_HEAP_SIZE
     }
-    return avdManager.createOrUpdateAvd(
-      null, internalName, d, systemImageDescription, ScreenOrientation.PORTRAIT, false, sdCard,
-      hardwareSkinPath, settings, null, true
-    )
+
+    val abi = Abi.getEnum(systemImageDescription.primaryAbiType)
+    val supportsSmp = abi != null && abi.supportsMultipleCpuCores() && AvdWizardUtils.getMaxCpuCores() > 1
+    avdBuilder.cpuCoreCount = if (supportsSmp) AvdWizardUtils.getMaxCpuCores() else 1
+
+    return avdManager.createAvd(avdBuilder)
   }
 
   @VisibleForTesting
@@ -186,48 +196,11 @@ class AndroidVirtualDevice(private val androidVersion: AndroidVersion?, installU
     private val ID_VENDOR_GOOGLE = IdDisplay.create("google", "Google LLC")
     private val DEFAULT_RAM_SIZE = Storage(2, Storage.Unit.GiB)
     private val DEFAULT_HEAP_SIZE = Storage(336, Storage.Unit.MiB)
-    private val ENABLED_HARDWARE = setOf(
-        HardwareProperties.HW_ACCELEROMETER, HardwareProperties.HW_AUDIO_INPUT, HardwareProperties.HW_BATTERY,
-        HardwareProperties.HW_GPS, HardwareProperties.HW_KEYBOARD, HardwareProperties.HW_ORIENTATION_SENSOR,
-        HardwareProperties.HW_PROXIMITY_SENSOR, HardwareProperties.HW_SDCARD,
-        ConfigKey.GPU_EMULATION)
-    private val DISABLED_HARDWARE = setOf(
-      HardwareProperties.HW_DPAD, HardwareProperties.HW_MAINKEYS, HardwareProperties.HW_TRACKBALL,
-      ConfigKey.SNAPSHOT_PRESENT
-    )
 
     @Throws(WizardException::class)
     private fun getDevice(sdkPath: Path): Device {
       return DeviceManagerConnection.getDeviceManagerConnection(sdkPath).devices.find { it.id == DEFAULT_DEVICE_ID } ?:
           throw WizardException("No device definition with \"$DEFAULT_DEVICE_ID\" ID found")
-    }
-
-    private fun getAvdSettings(displayName: String, internalName: String, device: Device): MutableMap<String, String> {
-      val result: MutableMap<String, String> = hashMapOf()
-      // First, initialize AVD settings based on the device definition
-      result.putAll(DeviceManager.getHardwareProperties(device))
-      // Then, override device definition defaults and fill in remaining fields
-      result[ConfigKey.GPU_MODE] = GpuMode.AUTO.gpuSetting
-      for (key in ENABLED_HARDWARE) {
-        result[key] = HardwareProperties.BOOLEAN_YES
-      }
-      for (key in DISABLED_HARDWARE) {
-        result[key] = HardwareProperties.BOOLEAN_NO
-      }
-      result[ConfigKey.CAMERA_BACK] = "virtualscene"
-      result[ConfigKey.CAMERA_FRONT] = "emulated"
-      result[ConfigKey.NETWORK_LATENCY] = EmulatedProperties.DEFAULT_NETWORK_LATENCY.asParameter
-      result[ConfigKey.NETWORK_SPEED] = EmulatedProperties.DEFAULT_NETWORK_SPEED.asParameter
-      result[ConfigKey.AVD_ID] = internalName
-      result[ConfigKey.DISPLAY_NAME] = displayName
-      setStorageSizeKey(result, ConfigKey.RAM_SIZE, DEFAULT_RAM_SIZE, false)
-      setStorageSizeKey(result, ConfigKey.DATA_PARTITION_SIZE, DEFAULT_RAM_SIZE, false)
-      setStorageSizeKey(result, ConfigKey.VM_HEAP_SIZE, DEFAULT_HEAP_SIZE, true)
-      return result
-    }
-
-    private fun setStorageSizeKey(result: MutableMap<String, String>, key: String, size: Storage, convertToMb: Boolean) {
-      result[key] = AvdOptionsModel.toIniString(size, convertToMb)
     }
   }
 }
