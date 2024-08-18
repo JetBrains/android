bddf60df9ab887d30e2480e2da60f94d9e055458
Alex Golubev <alexgolubev@google.com>
2024-08-18 00:06:01 -0400

    [DCL] Move dcl language to separate module
    Test: existing
    Bug: 336267864
    Change-Id: I17ed68b99bd7c164c119851563b6c3bcb76e3b92
    
    GitOrigin-RevId: 0f3e1ddc4282a7a2b3160cbb2e948472e1d87628

---
+++ gradle-declarative-lang/src/com/android/tools/idea/gradle/dcl/lang/psi/PsiImplUtil.kt
@@ -0,0 +1,123 @@
+/*
+ * Copyright (C) 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.gradle.dcl.lang.psi
+
+import com.intellij.openapi.util.text.StringUtil
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiReference
+import com.intellij.psi.impl.source.resolve.reference.ReferenceProvidersRegistry
+import com.intellij.psi.util.childLeafs
+import com.intellij.psi.util.childrenOfType
+
+class PsiImplUtil {
+  companion object {
+    @JvmStatic
+    fun getReceiver(property: DeclarativeProperty): DeclarativeProperty? = when (property) {
+      is DeclarativeBare -> null
+      is DeclarativeQualified -> property.property
+      else -> error("foo")
+    }
+
+    @JvmStatic
+    fun getField(property: DeclarativeProperty): DeclarativeIdentifier = when (property) {
+      is DeclarativeBare -> property.identifier
+      is DeclarativeQualified -> property.identifier!!
+      else -> error("foo")
+    }
+
+    @JvmStatic
+    fun getReference(property: DeclarativeProperty): PsiReference? = getReferences(property).firstOrNull()
+
+    @JvmStatic
+    fun getReferences(property: DeclarativeProperty): Array<PsiReference> =
+      ReferenceProvidersRegistry.getReferencesFromProviders(property)
+
+    @JvmStatic
+    fun getName(property: DeclarativeIdentifier): String? {
+      return StringUtil.unescapeStringCharacters(property.text)
+    }
+
+    @JvmStatic
+    fun getFactory(block: DeclarativeBlock): DeclarativeFactory? {
+      return block.firstChild as? DeclarativeFactory
+    }
+
+    @JvmStatic
+    fun getValue(assignment: DeclarativeAssignment): DeclarativeValue? {
+      return assignment.children.firstNotNullOfOrNull { child -> (child as? DeclarativeValue).takeIf { it != null } }
+    }
+
+    @JvmStatic
+    fun getBlockEntriesStart(block: DeclarativeBlock): PsiElement? {
+      return block.childLeafs().find { it.text == "{" }
+    }
+
+    @JvmStatic
+    fun getEntries(block: DeclarativeBlock): List<DeclarativeEntry> {
+      return block.blockGroup.entries
+    }
+
+
+    @JvmStatic
+    fun getBlockEntriesStart(blockGroup: DeclarativeBlockGroup): PsiElement? {
+      return blockGroup.childLeafs().find { it.text == "{" }
+    }
+
+    @JvmStatic
+    fun getArguments(list: DeclarativeArgumentsList): List<DeclarativeValue> {
+      return list.childrenOfType<DeclarativeValue>().toList()
+    }
+
+    @JvmStatic
+    fun getValue(literal: DeclarativeLiteral): Any? = when {
+      literal.boolean != null -> literal.boolean?.text == "true"
+      literal.multilineStringLiteral != null -> literal.multilineStringLiteral?.text?.unTripleQuote()?.unescape()
+      literal.oneLineStringLiteral != null -> literal.oneLineStringLiteral?.text?.unquote()?.unescape()
+      literal.longLiteral != null -> literal.longLiteral?.text?.toIntegerOrNull()
+      literal.integerLiteral != null -> literal.integerLiteral?.text?.toIntegerOrNull()
+      literal.unsignedLong != null -> literal.unsignedLong?.text?.toIntegerOrNull()
+      literal.unsignedInteger != null -> literal.unsignedInteger?.text?.toIntegerOrNull()
+      else -> null
+    }
+
+    private fun String.unquote() = this.removePrefix("\"").removeSuffixIfPresent("\"")
+    private fun String.unTripleQuote() = this.removePrefix("\"\"\"").removeSuffixIfPresent("\"\"\"")
+    private fun String.removeSuffixIfPresent(suffix: String) = if (this.endsWith(suffix)) this.dropLast(suffix.length) else this
+    private fun String.toIntegerOrNull(): Any? {
+      if (isEmpty()) return null
+      var str = this.replace("_", "")
+      val longIndicator = last().lowercaseChar() == 'l'
+      if (longIndicator) {
+        str = dropLast(1)
+        return if (str.last().lowercaseChar() == 'u') str.dropLast(1).toULong()
+        else str.toLongOrNull()
+      }
+      return if (str.last().lowercaseChar() == 'u') {
+        when (val answer = str.dropLast(1).toULong()) {
+          null -> null
+          in UInt.MIN_VALUE..UInt.MAX_VALUE -> answer.toUInt()
+          else -> answer
+        }
+      }
+      else
+        when (val answer = str.toLongOrNull()) {
+          null -> null
+          in Int.MIN_VALUE..Int.MAX_VALUE -> answer.toInt()
+          else -> answer
+        }
+    }
+  }
+}
