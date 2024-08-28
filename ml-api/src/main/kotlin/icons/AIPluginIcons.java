c7081f30d8af21f61f56de8463278fa30260d7b2
Nebojsa Vuksic <nebojsa.vuksic@jetbrains.com>
2024-08-28 14:38:28 +0200

    [android] (Ladybug) Merge AS 2024.2.1 Canary 7: Run `Generate icons classes` to update Android plugin icons
    GitOrigin-RevId: 86b1a9e40441f4b492c664706ae55dc3c21cf843

---
+++ ml-api/src/main/kotlin/icons/AIPluginIcons.java
@@ -0,0 +1,18 @@
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package icons;
+
+import com.intellij.ui.IconManager;
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
+
+/**
+ * NOTE THIS FILE IS AUTO-GENERATED
+ * DO NOT EDIT IT BY HAND, run "Generate icon classes" configuration instead
+ */
+public final class AIPluginIcons {
+  private static @NotNull Icon load(@NotNull String path, int cacheKey, int flags) {
+    return IconManager.getInstance().loadRasterizedIcon(path, AIPluginIcons.class.getClassLoader(), cacheKey, flags);
+  }
+  /** 16x16 */ public static final @NotNull Icon GeminiLogo = load("icons/gemini-logo.svg", 351137742, 0);
+}
