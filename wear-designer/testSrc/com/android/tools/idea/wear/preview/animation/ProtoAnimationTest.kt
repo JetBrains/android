d0e56ee1b669d5f09099efc3e288b7ebe8082203
Polina Koval <kovalp@google.com>
2024-08-28 17:16:52 +0100

    Update DynamicTypeAnimator according to changes in androidx
    Bug: n/a
    Test: ProtoAnimationTest
    Change-Id: Icace6b58ae9d41811feec46aed7bd51945f5a00b
    
    GitOrigin-RevId: 6082a94ad826ff8e4ef830fa278c8b433a400c2d

--- wear-designer/testSrc/com/android/tools/idea/wear/preview/animation/ProtoAnimationTest.kt
+++ wear-designer/testSrc/com/android/tools/idea/wear/preview/animation/ProtoAnimationTest.kt
@@ -15,7 +15,9 @@
  */
 package com.android.tools.idea.wear.preview.animation
 
+import com.android.tools.idea.wear.preview.animation.TestDynamicTypeAnimator.Unknowm
 import junit.framework.TestCase.assertEquals
+import org.junit.Assert.assertArrayEquals
 import org.junit.Assert.assertThrows
 import org.junit.Test
 
@@ -36,5 +38,87 @@ class ProtoAnimationTest {
     val exception =
       assertThrows(IllegalArgumentException::class.java) { ProtoAnimation(invalidAnimator) }
     assertEquals("Animator must implement DynamicTypeAnimator interface", exception.message)
+  }
+
+  @Test
+  fun testGetType() {
+    val animator = TestDynamicTypeAnimator()
+
+    // Set up the evaluator to be an ArgbEvaluator (simulating a color animation)
+    animator.typeEvaluator = TestDynamicTypeAnimator.ArgbEvaluator()
+    assertEquals(ProtoAnimation.TYPE.COLOR, ProtoAnimation(animator).type)
+
+    // Set up the evaluator to be an IntEvaluator
+    animator.typeEvaluator = TestDynamicTypeAnimator.IntEvaluator()
+    assertEquals(ProtoAnimation.TYPE.INT, ProtoAnimation(animator).type)
+
+    // Set up the evaluator to be a FloatEvaluator
+    animator.typeEvaluator = TestDynamicTypeAnimator.FloatEvaluator()
+    assertEquals(ProtoAnimation.TYPE.FLOAT, ProtoAnimation(animator).type)
+
+    animator.typeEvaluator = Unknowm()
+    assertEquals(ProtoAnimation.TYPE.UNKNOWN, ProtoAnimation(animator).type)
+  }
+
+  @Test
+  fun testGetName() {
+    val animator = TestDynamicTypeAnimator()
+
+    // Set up the evaluator to be an ArgbEvaluator
+    animator.typeEvaluator = TestDynamicTypeAnimator.ArgbEvaluator()
+    assertEquals("COLOR Animation", ProtoAnimation(animator).name)
+
+    // Set up the evaluator to be an IntEvaluator
+    animator.typeEvaluator = TestDynamicTypeAnimator.IntEvaluator()
+    assertEquals("INT Animation", ProtoAnimation(animator).name)
+
+    // Set up the evaluator to be a FloatEvaluator
+    animator.typeEvaluator = TestDynamicTypeAnimator.FloatEvaluator()
+    assertEquals("FLOAT Animation", ProtoAnimation(animator).name)
+
+    animator.typeEvaluator = Unknowm()
+    assertEquals("UNKNOWN Animation", ProtoAnimation(animator).name)
+  }
+
+  @Test
+  fun testDelegateMethodCall() {
+    val animator = TestDynamicTypeAnimator()
+    val protoAnimation = ProtoAnimation(animator)
+
+    // Test a method with no arguments
+    val duration = protoAnimation.durationMs
+    assertEquals(animator.getDurationMs(), duration)
+
+    // Test a method with arguments
+    val newTime = 500L
+    protoAnimation.setTime(newTime)
+    assertEquals(newTime, animator.getAnimationFrameTime())
+
+    // Test a method that returns a value
+    animator.setCurrentValue(123) // Set a current value
+    val value = protoAnimation.value
+    assertEquals(123, value)
+  }
+
+  @Test
+  fun testSetFloatValues() {
+    val animator = TestDynamicTypeAnimator()
+    val protoAnimation = ProtoAnimation(animator)
+
+    val values = floatArrayOf(0.2f, 0.5f, 0.8f)
+    protoAnimation.setFloatValues(*values)
+
+    assertArrayEquals(values, animator.getFloatValues(), 0.001f)
+  }
+
+  @Test
+  fun testSetIntValues() {
+    val animator = TestDynamicTypeAnimator()
+    val protoAnimation = ProtoAnimation(animator)
+
+    val values = intArrayOf(10, 50, 90)
+    protoAnimation.setIntValues(*values)
+
+    assertArrayEquals(values, animator.getIntValues())
   }
 }
