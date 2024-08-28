8938afd8fe64930a2f9fa370d89e1bd2c0dca5d2
Esteban de la Canal <estebandlc@google.com>
2024-08-28 08:09:43 -0700

    Add runtime_deps to jps_test
    ... to be able to inject classes.
    
    Bug: NA
    Test: added
    Change-Id: If4dbbe69a8956858721d72f5058a3f04641b9b0f
    
    GitOrigin-RevId: 0b0d0a5a3c06200ccd14c0df2661f03cb546511f

--- jps-build/jps_test.sh
+++ jps-build/jps_test.sh
@@ -16,7 +16,7 @@ echo "-Dbazel.test_suite=$TEST_SUITE" >> .test/args.txt
 echo "-Didea.root=$PWD" >> .test/args.txt
 echo "-Djava.security.manager=allow" >> .test/args.txt
 echo "-Dmaven.repo.for.testing=file://$JPS_WORKSPACE/maven" >> .test/args.txt
-echo -n "-cp $BAZEL_RUNNER:" >> .test/args.txt
+echo -n "-cp $RUNTIME_DEPS:" >> .test/args.txt
 cat out/studio/artifacts/module-tests/$TEST_MODULE.classpath.txt | sed "s#^#../../#" | tr '\n' ':' >> .test/args.txt
 echo '' >> .test/args.txt
 
