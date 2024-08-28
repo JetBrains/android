46db9db4dcd6f4cbc1de7cb94c5eafc7cebb6663
Nebojsa Vuksic <nebojsa.vuksic@jetbrains.com>
2024-08-28 16:26:54 +0200

    [android] (Ladybug) Merge AS 2024.2.1 Canary 7: Update intellij.android.ml-api icon class
    GitOrigin-RevId: 2f71cead0e0d3e6d1807d95e3810a225d102fccf

--- ml-api/src/main/kotlin/com/android/tools/idea/studiobot/icons/AndroidAIPluginIcons.java
+++ ml-api/src/main/kotlin/com/android/tools/idea/studiobot/icons/AndroidAIPluginIcons.java
@@ -1,5 +1,5 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package icons;
+package com.android.tools.idea.studiobot.icons;
 
 import com.intellij.ui.IconManager;
 import org.jetbrains.annotations.NotNull;
@@ -10,9 +10,9 @@ import javax.swing.*;
  * NOTE THIS FILE IS AUTO-GENERATED
  * DO NOT EDIT IT BY HAND, run "Generate icon classes" configuration instead
  */
-public final class AIPluginIcons {
+public final class AndroidAIPluginIcons {
   private static @NotNull Icon load(@NotNull String path, int cacheKey, int flags) {
-    return IconManager.getInstance().loadRasterizedIcon(path, AIPluginIcons.class.getClassLoader(), cacheKey, flags);
+    return IconManager.getInstance().loadRasterizedIcon(path, AndroidAIPluginIcons.class.getClassLoader(), cacheKey, flags);
   }
   /** 16x16 */ public static final @NotNull Icon GeminiLogo = load("icons/gemini-logo.svg", 351137742, 0);
 }
