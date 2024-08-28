fd850e4e4043a0a0b30dc15af5d45abe98d93e63
Amaury Medeiros <amaurym@google.com>
2024-08-28 16:15:34 +0100

    Add dialog to Generate All Previews action
    Also modify the availability of the action. The new agreed behavior is:
    
    * The action will only be available if the file has Composables, but
      doesn't have any previews
    * The dialog will then list all the Composables in the file as
      checkboxes (V1, subject to change in future iterations)
    * Once the user presses "OK", previews will be generated only for the
      functions whose corresponding checkboxes are checked.
    
    Bug: 357060508
    Test: TBD. Tracked by issue 362526526
    Change-Id: Iae353c47c3a51bb4b9716fb7315275bf48d7eaf1
    
    GitOrigin-RevId: 607737223487b069e02e73776a007a7e0516c142

--- compose-designer/src/com/android/tools/idea/compose/preview/actions/ml/GenerateComposePreviewAction.kt
+++ compose-designer/src/com/android/tools/idea/compose/preview/actions/ml/GenerateComposePreviewAction.kt
@@ -15,8 +15,13 @@
  */
 package com.android.tools.idea.compose.preview.actions.ml
 
+import com.android.tools.compose.COMPOSABLE_ANNOTATION_FQ_NAME
+import com.android.tools.compose.isValidPreviewLocation
+import com.android.tools.idea.compose.preview.isMultiPreviewAnnotation
+import com.android.tools.idea.compose.preview.isPreviewAnnotation
 import com.android.tools.idea.compose.preview.message
 import com.android.tools.idea.flags.StudioFlags
+import com.android.tools.idea.kotlin.fqNameMatches
 import com.intellij.openapi.actionSystem.ActionPlaces
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
@@ -25,6 +30,8 @@ import com.intellij.psi.util.parentOfType
 import icons.AipluginIcons
 import org.jetbrains.kotlin.psi.KtFile
 import org.jetbrains.kotlin.psi.KtNamedFunction
+import org.jetbrains.uast.UAnnotation
+import org.jetbrains.uast.toUElement
 
 /** Action to generate a Compose Preview for a target Composable function. */
 class GenerateComposePreviewAction :
@@ -40,6 +47,10 @@ class GenerateComposePreviewAction :
     else emptyList()
   }
 
+  override fun actionPerformed(e: AnActionEvent) {
+    generateComposePreviews(e)
+  }
+
   private fun getContainingFunctionAtCaret(e: AnActionEvent): KtNamedFunction? {
     val caret = e.getData(CommonDataKeys.CARET) ?: return null
     val psiFile = e.getData(CommonDataKeys.PSI_FILE) as? KtFile ?: return null
@@ -52,5 +63,23 @@ class GenerateComposePreviewAction :
     if (e.place != ActionPlaces.EDITOR_POPUP) {
       e.presentation.icon = AipluginIcons.Logo
     }
+  }
+
+  /**
+   * Whether this [KtNamedFunction] is a valid Composable function, i.e. a function annotated
+   * with @Composable that's not yet annotated with a @Preview or a MultiPreview annotation. It also
+   * must be in a valid preview location (see [isValidPreviewLocation]).
+   */
+  private fun KtNamedFunction.isValidComposableFunction(): Boolean {
+    if (annotationEntries.none { it.fqNameMatches(COMPOSABLE_ANNOTATION_FQ_NAME) }) return false
+    if (!isValidPreviewLocation()) return false
+    if (
+      annotationEntries.any {
+        val uAnnotation = (it.toUElement() as? UAnnotation) ?: return@any false
+        return@any uAnnotation.isPreviewAnnotation() || uAnnotation.isMultiPreviewAnnotation()
+      }
+    )
+      return false
+    return true
   }
 }
--- compose-designer/src/com/android/tools/idea/compose/preview/actions/ml/GenerateComposePreviewsForFileAction.kt
+++ compose-designer/src/com/android/tools/idea/compose/preview/actions/ml/GenerateComposePreviewsForFileAction.kt
@@ -15,13 +15,27 @@
  */
 package com.android.tools.idea.compose.preview.actions.ml
 
+import com.android.tools.adtui.TabularLayout
+import com.android.tools.compose.COMPOSABLE_ANNOTATION_FQ_NAME
+import com.android.tools.idea.compose.preview.isMultiPreviewAnnotation
+import com.android.tools.idea.compose.preview.isPreviewAnnotation
 import com.android.tools.idea.compose.preview.message
 import com.android.tools.idea.flags.StudioFlags
+import com.android.tools.idea.kotlin.fqNameMatches
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.ui.DialogWrapper
+import com.intellij.ui.components.JBCheckBox
+import com.intellij.ui.components.JBLabel
+import javax.swing.JComponent
+import javax.swing.JPanel
+import javax.swing.JSeparator
 import org.jetbrains.kotlin.psi.KtFile
 import org.jetbrains.kotlin.psi.KtNamedFunction
 import org.jetbrains.kotlin.psi.psiUtil.collectDescendantsOfType
+import org.jetbrains.uast.UAnnotation
+import org.jetbrains.uast.toUElement
 
 class GenerateComposePreviewsForFileAction :
   GenerateComposePreviewBaseAction(message("action.generate.previews.for.file")) {
@@ -29,7 +43,110 @@ class GenerateComposePreviewsForFileAction :
 
   override fun getTargetComposableFunctions(e: AnActionEvent): List<KtNamedFunction> {
     val psiFile = e.getData(CommonDataKeys.PSI_FILE) as? KtFile ?: return emptyList()
-    // Collect all the valid Composables from this file
-    return psiFile.collectDescendantsOfType<KtNamedFunction> { it.isValidComposableFunction() }
+    val functionsInFile = psiFile.collectDescendantsOfType<KtNamedFunction>()
+    val composableFunctions = mutableListOf<KtNamedFunction>()
+    functionsInFile.forEach { function ->
+      if (function.annotationEntries.any { it.fqNameMatches(COMPOSABLE_ANNOTATION_FQ_NAME) }) {
+        composableFunctions.add(function)
+      }
+      if (
+        function.annotationEntries.any {
+          val uAnnotation = (it.toUElement() as? UAnnotation) ?: return@any false
+          return@any uAnnotation.isPreviewAnnotation() || uAnnotation.isMultiPreviewAnnotation()
+        }
+      ) {
+        // If the file has previews, return an empty list because the action should not be shown
+        return@getTargetComposableFunctions emptyList()
+      }
+    }
+    return composableFunctions
+  }
+
+  override fun actionPerformed(e: AnActionEvent) {
+    val project = e.project ?: return
+    SelectComposablesDialog(project, getTargetComposableFunctions(e)) { composables ->
+        generateComposePreviews(e) { composables }
+      }
+      .showAndGet()
+  }
+
+  /**
+   * Dialog to list Composable functions of this file as checkboxes, so the user can select which
+   * previews they want to generate.
+   */
+  private class SelectComposablesDialog(
+    project: Project,
+    private val composablesInFile: List<KtNamedFunction>,
+    private val generatePreviewsForComposableFunctions: (List<KtNamedFunction>) -> Unit,
+  ) : DialogWrapper(project) {
+
+    /**
+     * Maps function name to its corresponding [JBCheckBox]. It's used to determine which
+     * Composables will have previews generated for.
+     */
+    private val checkBoxes = mutableMapOf<String?, JBCheckBox>()
+
+    init {
+      title = message("action.generate.previews.for.file.dialog.title")
+      init()
+    }
+
+    override fun createCenterPanel(): JComponent {
+      // [Composables][padding][Horizontal separator]
+      // [Empty Panel] or [Checkboxes panel]
+      val mainPanel = JPanel(TabularLayout("Fit,5px,*", "Fit,*"))
+
+      mainPanel.add(
+        JBLabel(message("action.generate.previews.for.file.dialog.composables.label")),
+        TabularLayout.Constraint(0, 0),
+      )
+      // Centralize the separator vertically
+      val separatorPanel = JPanel(TabularLayout("*", "*,Fit,*"))
+      separatorPanel.add(JSeparator(), TabularLayout.Constraint(1, 0))
+      mainPanel.add(separatorPanel, TabularLayout.Constraint(0, 2))
+
+      val composablesPanel = createComposablesPanel(composablesInFile)
+      mainPanel.add(composablesPanel, TabularLayout.Constraint(1, 0, 3))
+
+      return mainPanel
+    }
+
+    override fun doOKAction() {
+      generatePreviewForCheckedFunctions()
+      super.doOKAction()
+    }
+
+    /** Creates a panel that will list Composables in the file as checkboxes. */
+    private fun createComposablesPanel(composableCandidates: List<KtNamedFunction>): JComponent {
+      if (composableCandidates.isEmpty()) {
+        return JBLabel(message("action.generate.previews.for.file.dialog.empty"))
+      }
+      // Checkboxes are horizontally aligned to the left.
+      // Vertical alignment is Glue, checkboxes (n * Fit), Glue.
+      val checkBoxesPanel =
+        JPanel(TabularLayout("Fit,*", "*,${"Fit,".repeat(composableCandidates.size)}*"))
+      for ((index, composable) in composableCandidates.withIndex()) {
+        // Skip the first row, as it will be a vertical glue
+        val row = index + 1
+        val checkbox = JBCheckBox(composable.name, true)
+        checkBoxesPanel.add(checkbox, TabularLayout.Constraint(row, 0))
+        checkBoxes[composable.name] = (checkbox)
+      }
+      return checkBoxesPanel
+    }
+
+    /**
+     * Invokes the dialog callback, passing the composable functions whose corresponding checkboxes
+     * are checked. The callback will generate a Compose Preview per checked function.
+     */
+    private fun generatePreviewForCheckedFunctions() {
+      val targetComposableFunctions = mutableListOf<KtNamedFunction>()
+      for (candidate in composablesInFile) {
+        if (checkBoxes[candidate.name]?.isSelected == true) {
+          targetComposableFunctions.add(candidate)
+        }
+      }
+      generatePreviewsForComposableFunctions(targetComposableFunctions)
+    }
   }
 }
