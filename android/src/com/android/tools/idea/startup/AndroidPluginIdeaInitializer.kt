f0f56cc202e41747740862f54f612c69ca8e3483
Nebojsa Vuksic <nebojsa.vuksic@jetbrains.com>
2024-09-02 11:04:47 +0200

    [android] (Ladybug) Merge AS 2024.2.1 Canary 7: Disable UsageTracker in IntelliJ IDEA
    GitOrigin-RevId: b4ca75e46b464d89692f81f4627361873076538b

--- android/src/com/android/tools/idea/startup/AndroidPluginIdeaInitializer.kt
+++ android/src/com/android/tools/idea/startup/AndroidPluginIdeaInitializer.kt
@@ -1,15 +1,19 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.android.tools.idea.startup
 
+import com.android.tools.analytics.AnalyticsSettings
 import com.android.tools.analytics.UsageTracker
+import com.google.wireless.android.sdk.stats.AndroidStudioEvent
 import com.intellij.ide.ApplicationInitializedListener
 
 /**
  * Initializer that is run only when the Android plugin is executed in IntelliJ IDEA.
  * This initializer shouldn't be run in Android Studio.
  */
-private class AndroidPluginIdeaInitializer: ApplicationInitializedListener {
+private class AndroidPluginIdeaInitializer : ApplicationInitializedListener {
   override suspend fun execute() {
-      UsageTracker.disable()
+    AnalyticsSettings.disable();
+    UsageTracker.disable();
+    UsageTracker.ideBrand = AndroidStudioEvent.IdeBrand.INTELLIJ
   }
 }
