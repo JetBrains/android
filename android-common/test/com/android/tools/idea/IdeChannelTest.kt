08b0039c21d7ebb7d19cbc872856515d40849d1c
Nebojsa Vuksic <nebojsa.vuksic@jetbrains.com>
2024-09-04 09:28:28 +0200

    [android] (Ladybug) Merge AS 2024.2.1 Canary 7: Replace usage of org.jetbrains.kotlin.backend.common.push with simple `add` function call
    GitOrigin-RevId: 25f84fedf8ff0ca91832c9ff86b0722e07dd3358

--- android-common/test/com/android/tools/idea/IdeChannelTest.kt
+++ android-common/test/com/android/tools/idea/IdeChannelTest.kt
@@ -1,7 +1,6 @@
 package com.android.tools.idea
 
 import com.intellij.testFramework.ProjectRule
-//import org.jetbrains.kotlin.backend.common.push
 import org.junit.Assert.assertArrayEquals
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertFalse
@@ -62,8 +61,8 @@ class IdeChannelTest {
       alreadyVisited.forEach { lessStableChannel ->
         assertFalse(lessStableChannel.isAtLeast(channel))
       }
-      // TODO Android Studio (Ladybug Canary 7) Merge - resolve push API
-      //alreadyVisited.push(channel)
+
+      alreadyVisited.add(channel)
     }
   }
 }
