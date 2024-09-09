6517adeec40d5d801bcc7cd09dcb8b08d2ff7e79
Shireesha Bongarala <shiree@google.com>
2024-09-09 15:14:25 +0100

    Add 'Ask Gemini' link on MessageEvents with level ERROR or above
    To add the link, previous implementation depended on the BuildEvent
    object's parentId to be an instance of ExternalSystemTaskId and deriving
    the task type from this instance. However, this only works for top level
    messages. Many errors are not on the top level but related to a
    task, these have task name (String) as the parent ID.
    
    However,as parentId can also be a
    String, this causes the link to not be added, and resulting in bugs like
    b/330441116, b/295887979.
    
    We are now relying on the message's severity being ERROR in-order to add
    the Ask Gemini link.
    
    bug: 330441116, b/295887979
    fixes: 330441116, b/295887979
    Test: BuildOutputParserWrapperTest
    Change-Id: I8aa54e5bcc9019c981195d2a5aa4ddf7594da77e
    
    GitOrigin-RevId: fed711275f7e09233bc38d0eec781bd381705c09

--- project-system-gradle/src/com/android/tools/idea/gradle/project/build/output/BuildOutputParserWrapper.kt
+++ project-system-gradle/src/com/android/tools/idea/gradle/project/build/output/BuildOutputParserWrapper.kt
@@ -18,12 +18,10 @@ package com.android.tools.idea.gradle.project.build.output
 import com.android.tools.idea.gradle.project.sync.issues.SyncIssuesReporter.consoleLinkWithSeparatorText
 import com.android.tools.idea.studiobot.StudioBot
 import com.intellij.build.events.BuildEvent
-import com.intellij.build.events.FileMessageEvent
-import com.intellij.build.events.impl.FileMessageEventImpl
+import com.intellij.build.events.MessageEvent
+import com.intellij.build.events.impl.MessageEventImpl
 import com.intellij.build.output.BuildOutputInstantReader
 import com.intellij.build.output.BuildOutputParser
-import com.intellij.openapi.externalSystem.model.task.ExternalSystemTaskId
-import com.intellij.openapi.externalSystem.model.task.ExternalSystemTaskType
 import java.util.function.Consumer
 
 
@@ -37,10 +35,13 @@ class BuildOutputParserWrapper(val parser: BuildOutputParser) : BuildOutputParse
 
   override fun parse(line: String?, reader: BuildOutputInstantReader?, messageConsumer: Consumer<in BuildEvent>?): Boolean {
     return parser.parse(line, reader) {
-      val taskId = it.parentId as? ExternalSystemTaskId
+      val messageEvent = it as? MessageEvent
+
       val event =
         if (explainerAvailable
-            && ExternalSystemTaskType.RESOLVE_PROJECT == taskId?.type
+            // We are only adding links for build events that are of severity ERROR. All other types such as warnings and info messages are
+            // excluded.
+            && messageEvent != null && messageEvent.kind == MessageEvent.Kind.ERROR
             && !it.message.startsWith("Unresolved reference:"))
         {
           it.injectExplanationText()
@@ -56,7 +57,7 @@ class BuildOutputParserWrapper(val parser: BuildOutputParser) : BuildOutputParse
    * ExplainBuildErrorFilter
    */
   private fun BuildEvent.injectExplanationText(): BuildEvent {
-    if (this is FileMessageEventImpl) {
+    if (this is MessageEventImpl) {
       this.description = (description?.trimEnd()?.plus("\n\n") ?: "") +
                           consoleLinkWithSeparatorText + message
     }
