12ad648812748fcff5fbe0ca8f7611e35cac63be
Karima Ichou <karimai@google.com>
2024-09-09 15:42:49 +0100

    Update comparison benchmark Kotlin version
    to 2.1.0-dev-6784
    
    Bug: 365509858
    Test: N/A
    Change-Id: Idcae7783d62d93c0464da34314a27dd6b79a036e
    
    GitOrigin-RevId: f2217aaba7ac6f7c43d2406528f658ab3576fbc0

--- android-test-framework/testSrc/com/android/tools/idea/testing/AgpIntegrationTests.kt
+++ android-test-framework/testSrc/com/android/tools/idea/testing/AgpIntegrationTests.kt
@@ -25,7 +25,7 @@ import com.intellij.openapi.projectRoots.JavaSdkVersion.JDK_1_8
 
 const val GRADLE_SNAPSHOT_VERSION = "8.11-20240906125531+0000"
 // For available versions: https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev/org/jetbrains/kotlin/kotlin-compiler/maven-metadata.xml
-const val KOTLIN_SNAPSHOT_VERSION = "2.0.20-Beta2-1"
+const val KOTLIN_SNAPSHOT_VERSION = "2.1.0-dev-6784"
 
 /**
  * An AGP Version definition to be used in AGP integration tests.
