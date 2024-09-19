65f8b55eb9adbc1a8334aeeeae3ab29147a4732e
Hung Nguyen <hungnv@google.com>
2024-09-19 16:46:19 +0100

    Update to KGP 2.1.0-Beta1 for tests (tools/adt/idea)
    Bug: N/A (Regular update)
    Test: Updated
    Change-Id: Ie9940ec92c6c2457ce13817d8e3f7e359b4383e8
    
    GitOrigin-RevId: 06a55f397fa7f608a56889e08ef3913d47a81576

--- android/testData/snapshots/projects/kotlinGradleDsl/build.gradle.kts
+++ android/testData/snapshots/projects/kotlinGradleDsl/build.gradle.kts
@@ -12,7 +12,11 @@ allprojects {
   }
 }
 
-
-// Do not remove. This is used to ensure that the class which is searched for in SyncedProjectTest still exists.
-
-println(org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompilerArgumentsProvider::class.java.simpleName)
+// ag/19457336 says that the following line should not be removed
+// ("This is used to ensure that the class which is searched for in SyncedProjectTest still exists.")
+// However, the KotlinJvmCompilerArgumentsProvider class has been removed in KGP 2.1.0-Beta1
+// (https://github.com/JetBrains/kotlin/commit/1c95429c4), so the following line no longer compiles.
+// Therefore, we need to comment out this line
+// for SyncedProjectsAllAgpTest.testKotlinGradleDsl[Agp(8.0.2, g=8.0, k=2.1.0-Beta1 , m=V2)] to pass.
+//
+// println(org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompilerArgumentsProvider::class.java.simpleName)
