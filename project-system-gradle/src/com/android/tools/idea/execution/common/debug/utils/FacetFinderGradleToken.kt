adbb4c60d9d253a5454e07508176d640e6e4d826
Christophe Rhodes <xof@google.com>
2024-09-19 15:31:29 +0000

    Apply the project system token hammer to the facet finder nail
    When trying to find a suitable project entity (facet, module, or
    something of that ilk) for a global process for which we do not have a
    known applicationId, we examine source providers of android facets for
    their manifests to find candidates that might have launched a process
    of that name.
    
    We then need to map that manifest to a module.  For Gradle projects,
    we need to provide the sourceSet modules, because the result will be
    more accurate if we know whether the thing that we have is known to be
    directly deployable or not, and whether it has test nature or not.
    Non-Gradle projects at present have a best-effort equivalent, which
    may fail if we find a device test manifest in a mixed module for which
    a Facet is not the right unit of abstraction.
    
    Bug: 147586218
    Test: existing
    Change-Id: I1da93e7ddf4ff9c64b09c706dfb7cdeaa01e12a6
    
    GitOrigin-RevId: ce6d321ffe1fcb7a91bad997540664ba687e7347

---
+++ project-system-gradle/src/com/android/tools/idea/execution/common/debug/utils/FacetFinderGradleToken.kt
@@ -0,0 +1,62 @@
+/*
+ * Copyright (C) 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.execution.common.debug.utils
+
+import com.android.tools.idea.projectsystem.AndroidModuleSystem.Type.TYPE_LIBRARY
+import com.android.tools.idea.projectsystem.AndroidModuleSystem.Type.TYPE_TEST
+import com.android.tools.idea.projectsystem.CommonTestType
+import com.android.tools.idea.projectsystem.GradleToken
+import com.android.tools.idea.projectsystem.getAndroidFacets
+import com.android.tools.idea.projectsystem.getAndroidTestModule
+import com.android.tools.idea.projectsystem.getMainModule
+import com.android.tools.idea.projectsystem.getModuleSystem
+import com.android.tools.idea.projectsystem.gradle.GradleProjectSystem
+import com.android.tools.idea.projectsystem.isAndroidTestModule
+import com.android.tools.idea.projectsystem.sourceProviders
+import com.intellij.openapi.module.Module
+import com.intellij.openapi.project.Project
+
+class FacetFinderGradleToken : FacetFinderToken<GradleProjectSystem>, GradleToken {
+  override fun findGlobalProcessDefinition(projectSystem: GradleProjectSystem, project: Project, processName: String): Module? {
+    // Iterate over holder modules, looking into candidate source providers of
+    // those holders and mapping to sourceSet modules "by hand".
+    for (facet in project.getAndroidFacets()) {
+      for (sourceProvider in facet.sourceProviders.currentSourceProviders) {
+        for (manifestFile in sourceProvider.manifestFiles) {
+          val globalProcessNames = ProcessNameReader.readGlobalProcessNames(project, manifestFile)
+          if (globalProcessNames.contains(processName)) {
+            return facet.module.getMainModule()
+          }
+        }
+      }
+      for (sourceProvider in facet.sourceProviders.currentDeviceTestSourceProviders[CommonTestType.ANDROID_TEST] ?: emptyList()) {
+        for (manifestFile in sourceProvider.manifestFiles) {
+          val globalProcessNames = ProcessNameReader.readGlobalProcessNames(project, manifestFile)
+          if (globalProcessNames.contains(processName)) {
+            return facet.module.getAndroidTestModule()
+          }
+        }
+      }
+    }
+    return null
+  }
+
+  override fun isDirectlyDeployable(projectSystem: GradleProjectSystem, project: Project, module: Module): Boolean =
+    module.isAndroidTestModule() || module.getModuleSystem().type != TYPE_LIBRARY
+
+  override fun hasTestNature(projectSystem: GradleProjectSystem, project: Project, module: Module): Boolean =
+    module.isAndroidTestModule() || module.getModuleSystem().type == TYPE_TEST
+}
