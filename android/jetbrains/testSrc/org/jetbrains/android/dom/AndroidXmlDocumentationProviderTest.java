7cf54a0a53ebc6463467681f21c1f3da3307b996
Andy Magee <andymagee@google.com>
2024-09-26 10:05:24 -0700

    Remove super class from test
    This is in preparation for moving the test to a different project.
    `AndroidTestCase` requires access to the central
    `tools/adt/idea/android/testData` directory, which I would like to
    avoid. By rewriting this without the super class, the dependency is no
    longer absolutely required after the move.
    
    Bug: n/a
    Test: AndroidXmlDocumentationProviderTest
    Change-Id: I406806a1da455eec41f86b161925d2c7475d6aac
    
    GitOrigin-RevId: f37f78812c60e0ced0c87aaa376935853a87ab3d

--- android/jetbrains/testSrc/org/jetbrains/android/dom/AndroidXmlDocumentationProviderTest.java
+++ android/jetbrains/testSrc/org/jetbrains/android/dom/AndroidXmlDocumentationProviderTest.java
@@ -17,43 +17,71 @@ package org.jetbrains.android.dom;
 
 import static com.google.common.truth.Truth.assertThat;
 
+import com.android.testutils.TestUtils;
+import com.android.tools.idea.testing.AndroidProjectRule;
 import com.intellij.codeInsight.documentation.DocumentationManager;
 import com.intellij.codeInsight.navigation.CtrlMouseHandler;
 import com.intellij.lang.documentation.DocumentationProvider;
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiReference;
-import org.jetbrains.android.AndroidTestCase;
+import com.intellij.testFramework.fixtures.CodeInsightTestFixture;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
 /**
  * Tests for {@link AndroidXmlDocumentationProvider}.
  */
-public class AndroidXmlDocumentationProviderTest extends AndroidTestCase {
+@RunWith(JUnit4.class)
+public class AndroidXmlDocumentationProviderTest {
   private static final String BASE_PATH = "documentation/";
 
-  public void testValueResourceReferenceQuickDoc() {
-    myFixture.copyFileToProject(BASE_PATH + "strings.xml", "res/values/strings.xml");
-    final VirtualFile f = myFixture.copyFileToProject(BASE_PATH + getTestName(true) + ".xml", "res/layout/test.xml");
-    myFixture.configureFromExistingVirtualFile(f);
-    final PsiReference ref = myFixture.getFile().findReferenceAt(myFixture.getEditor().getCaretModel().getOffset());
-    assertThat(ref).isNotNull();
-    assertThat(CtrlMouseHandler.getInfo(ref.resolve(), ref.getElement())).isEqualTo("@string/myString");
+  @Rule
+  public AndroidProjectRule projectRule = AndroidProjectRule.withSdk();
+
+  private CodeInsightTestFixture myFixture;
+
+  @Before
+  public void setUp() {
+    myFixture = projectRule.getFixture();
+    myFixture.setTestDataPath(TestUtils.resolveWorkspacePath("tools/adt/idea/android/testData").toString());
   }
 
-  public void testAndroidAttributeDocumentation() {
-    final VirtualFile f = myFixture.copyFileToProject(BASE_PATH + getTestName(true) + ".xml", "res/layout/test.xml");
+  @Test
+  public void valueResourceReferenceQuickDoc() {
+    myFixture.copyFileToProject(BASE_PATH + "strings.xml", "res/values/strings.xml");
+    final VirtualFile f = myFixture.copyFileToProject(BASE_PATH + "valueResourceReferenceQuickDoc.xml", "res/layout/test.xml");
     myFixture.configureFromExistingVirtualFile(f);
-    final PsiReference ref = myFixture.getFile().findReferenceAt(myFixture.getEditor().getCaretModel().getOffset());
-    assertThat(ref).isNotNull();
-    // There aren't really mouse-hover quick docs for attributes.
-    assertThat(CtrlMouseHandler.getInfo(ref.resolve(), ref.getElement())).isEqualTo("Attribute \"foreground\"");
-    // However, one can get the more detailed docs.
-    PsiElement docTargetElement = DocumentationManager.getInstance(getProject()).findTargetElement(
-      myFixture.getEditor(), myFixture.getFile(), ref.getElement());
-    DocumentationProvider documentationProvider = DocumentationManager.getProviderFromElement(docTargetElement);
-    assertThat(documentationProvider.generateDoc(docTargetElement, ref.getElement())).isEqualTo(
-      "<html><body>Formats: color, reference<br><br>Defines the drawable to draw over the content. This can be used as an overlay.\n" +
-      "             The foreground drawable participates in the padding of the content if the gravity\n" +
-      "             is set to fill.</body></html>");
+
+    ApplicationManager.getApplication().runReadAction(() -> {
+      final PsiReference ref = myFixture.getFile().findReferenceAt(myFixture.getEditor().getCaretModel().getOffset());
+      assertThat(ref).isNotNull();
+      assertThat(CtrlMouseHandler.getInfo(ref.resolve(), ref.getElement())).isEqualTo("@string/myString");
+    });
+  }
+
+  @Test
+  public void androidAttributeDocumentation() {
+    final VirtualFile f = myFixture.copyFileToProject(BASE_PATH + "androidAttributeDocumentation.xml", "res/layout/test.xml");
+    myFixture.configureFromExistingVirtualFile(f);
+
+    ApplicationManager.getApplication().runReadAction(() -> {
+      final PsiReference ref = myFixture.getFile().findReferenceAt(myFixture.getEditor().getCaretModel().getOffset());
+      assertThat(ref).isNotNull();
+      // There aren't really mouse-hover quick docs for attributes.
+      assertThat(CtrlMouseHandler.getInfo(ref.resolve(), ref.getElement())).isEqualTo("Attribute \"foreground\"");
+      // However, one can get the more detailed docs.
+      PsiElement docTargetElement = DocumentationManager.getInstance(myFixture.getProject()).findTargetElement(
+        myFixture.getEditor(), myFixture.getFile(), ref.getElement());
+      DocumentationProvider documentationProvider = DocumentationManager.getProviderFromElement(docTargetElement);
+      assertThat(documentationProvider.generateDoc(docTargetElement, ref.getElement())).isEqualTo(
+        "<html><body>Formats: color, reference<br><br>Defines the drawable to draw over the content. This can be used as an overlay.\n" +
+        "             The foreground drawable participates in the padding of the content if the gravity\n" +
+        "             is set to fill.</body></html>");
+    });
   }
 }
