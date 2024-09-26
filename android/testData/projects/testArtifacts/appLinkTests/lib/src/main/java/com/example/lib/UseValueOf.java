b66563d219d16897138216fd09dee26fa849217f
Scott Pollom <spollom@google.com>
2024-09-26 05:09:54 +0000

    Revert "Add AppLinksValidDetectorGradleTest"
    This reverts commit 2c1a41f43751c2e1526f32f3e93d80c7c73e164a.
    
    Reason for revert: Performance regression (b/369730729)
    
    Change-Id: I8aa9f0f9db472680de9524a95ff7c7d97148116a
    
    GitOrigin-RevId: e1391b11f64f686155d1c10a507e7789db9dbe11

--- android/testData/projects/testArtifacts/appLinkTests/lib/src/main/java/com/example/lib/UseValueOf.java
+++
@@ -1,9 +0,0 @@
-package com.example.lib;
-
-public class UseValueOf {
-    @SuppressWarnings("UnnecessaryBoxing")
-    public void useValueOf() {
-        Integer myInt = new Integer(5);
-    }
-}
-
