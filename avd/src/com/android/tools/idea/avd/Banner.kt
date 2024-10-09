3ffcc2d782adeaca8f2d58ed8e0e106bea912008
Juan C Nuno <juancnuno@google.com>
2024-10-09 15:24:32 -0700

    Show a warning if the device has Play
    Bug: 364711876
    Test: Not applicable
    Change-Id: I3443332f685419e1b66ca7e45ee1fda75a06b8a4
    
    GitOrigin-RevId: c7f233d6561fcc354e002a9d25297e1ee1e5ced6

--- avd/src/com/android/tools/idea/avd/Banner.kt
+++ avd/src/com/android/tools/idea/avd/Banner.kt
@@ -38,7 +38,13 @@ import org.jetbrains.jewel.ui.component.CircularProgressIndicator
 import org.jetbrains.jewel.ui.component.Divider
 import org.jetbrains.jewel.ui.component.Icon
 import org.jetbrains.jewel.ui.component.Text
+import org.jetbrains.jewel.ui.icon.IconKey
 import org.jetbrains.jewel.ui.theme.colorPalette
+
+@Composable
+internal fun WarningBanner(text: String) {
+  Banner(BannerUi.Warning, StudioIconsCompose.Common.Warning, "Warning", text)
+}
 
 @Composable
 internal fun ErrorBanner(
@@ -46,19 +52,31 @@ internal fun ErrorBanner(
   modifier: Modifier = Modifier,
   rightContent: @Composable () -> Unit = {},
 ) {
+  Banner(BannerUi.Error, StudioIconsCompose.Common.Error, "Error", text, modifier, rightContent)
+}
+
+@Composable
+private fun Banner(
+  colors: Colors,
+  key: IconKey,
+  contentDescription: String,
+  text: String,
+  modifier: Modifier = Modifier,
+  rightContent: @Composable () -> Unit = {},
+) {
   Column(modifier.fillMaxWidth()) {
-    Divider(orientation = Orientation.Horizontal, color = BannerUi.Error.border)
+    Divider(orientation = Orientation.Horizontal, color = colors.border)
     Row(
-      modifier = Modifier.background(BannerUi.Error.background).padding(10.dp),
+      modifier = Modifier.background(colors.background).padding(10.dp),
       verticalAlignment = Alignment.CenterVertically,
     ) {
-      Icon(StudioIconsCompose.Common.Error, contentDescription = "Error")
+      Icon(key, contentDescription = contentDescription)
       Spacer(modifier = Modifier.width(8.dp))
-      Text(text = text, color = BannerUi.Error.foreground)
+      Text(text = text, color = colors.foreground)
       Spacer(modifier = Modifier.weight(1f))
       rightContent()
     }
-    Divider(orientation = Orientation.Horizontal, color = BannerUi.Error.border)
+    Divider(orientation = Orientation.Horizontal, color = colors.border)
   }
 }
 
@@ -91,30 +109,30 @@ internal fun ErrorPanel(modifier: Modifier = Modifier, error: String) {
 }
 
 private object BannerUi {
-  object Error {
-    val border: Color
+  object Warning : Colors() {
+    override val border: Color
       @Composable
       get() =
         rememberColor(
-          key = "Banner.Error.borderColor",
-          darkFallbackKey = "ColorPalette.Red3",
-          darkDefault = JewelTheme.colorPalette.red(3),
-          lightFallbackKey = "ColorPalette.Red9",
-          lightDefault = JewelTheme.colorPalette.red(9),
+          key = "Banner.Warning.borderColor",
+          darkFallbackKey = "ColorPalette.Yellow3",
+          darkDefault = JewelTheme.colorPalette.yellow(3),
+          lightFallbackKey = "ColorPalette.Yellow9",
+          lightDefault = JewelTheme.colorPalette.yellow(9),
         )
 
-    val background: Color
+    override val background: Color
       @Composable
       get() =
         rememberColor(
-          key = "Banner.Error.background",
-          darkFallbackKey = "ColorPalette.Red1",
-          darkDefault = JewelTheme.colorPalette.red(1),
-          lightFallbackKey = "ColorPalette.Red12",
-          lightDefault = JewelTheme.colorPalette.red(12),
+          key = "Banner.Warning.background",
+          darkFallbackKey = "ColorPalette.Warning1",
+          darkDefault = JewelTheme.colorPalette.yellow(1),
+          lightFallbackKey = "ColorPalette.Warning10",
+          lightDefault = JewelTheme.colorPalette.yellow(10),
         )
 
-    val foreground: Color
+    override val foreground: Color
       @Composable
       get() =
         rememberColor(
@@ -125,4 +143,45 @@ private object BannerUi {
           lightDefault = JewelTheme.globalColors.text.normal,
         )
   }
+
+  object Error : Colors() {
+    override val border: Color
+      @Composable
+      get() =
+        rememberColor(
+          key = "Banner.Error.borderColor",
+          darkFallbackKey = "ColorPalette.Red3",
+          darkDefault = JewelTheme.colorPalette.red(3),
+          lightFallbackKey = "ColorPalette.Red9",
+          lightDefault = JewelTheme.colorPalette.red(9),
+        )
+
+    override val background: Color
+      @Composable
+      get() =
+        rememberColor(
+          key = "Banner.Error.background",
+          darkFallbackKey = "ColorPalette.Red1",
+          darkDefault = JewelTheme.colorPalette.red(1),
+          lightFallbackKey = "ColorPalette.Red12",
+          lightDefault = JewelTheme.colorPalette.red(12),
+        )
+
+    override val foreground: Color
+      @Composable
+      get() =
+        rememberColor(
+          key = "Banner.Error.foreground",
+          darkFallbackKey = "ColorPalette.Gray12",
+          darkDefault = JewelTheme.globalColors.text.normal,
+          lightFallbackKey = "ColorPalette.Gray1",
+          lightDefault = JewelTheme.globalColors.text.normal,
+        )
+  }
+}
+
+private abstract class Colors {
+  @get:Composable abstract val border: Color
+  @get:Composable abstract val background: Color
+  @get:Composable abstract val foreground: Color
 }
