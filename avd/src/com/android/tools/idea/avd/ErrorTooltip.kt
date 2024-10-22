234e04b25c9b45efdf7a3952ee0e8d4e46696d2e
James Rose <rosej@google.com>
2024-10-22 16:08:47 -0700

    Use Jewel tooltips directly
    Now that Jewel 0.26 Tooltip has an "enabled" flag, we don't need our own
    fork of it.
    
    Bug: n/a
    Test: n/a
    Change-Id: I94a22fb8d4c9fabb331162a6d85b05b3eead578a
    
    GitOrigin-RevId: 1c1e28dc3b15afcbbd5d181735f4ae9ae13b8a77

--- avd/src/com/android/tools/idea/avd/ErrorTooltip.kt
+++ avd/src/com/android/tools/idea/avd/ErrorTooltip.kt
@@ -16,38 +16,24 @@
 package com.android.tools.idea.avd
 
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.TooltipArea
 import androidx.compose.foundation.TooltipPlacement
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.dp
 import kotlin.time.Duration.Companion.milliseconds
 import org.jetbrains.jewel.foundation.theme.JewelTheme
-import org.jetbrains.jewel.foundation.theme.LocalContentColor
 import org.jetbrains.jewel.ui.component.Text
+import org.jetbrains.jewel.ui.component.Tooltip
 import org.jetbrains.jewel.ui.component.styling.TooltipColors
 import org.jetbrains.jewel.ui.component.styling.TooltipMetrics
 import org.jetbrains.jewel.ui.component.styling.TooltipStyle
 import org.jetbrains.jewel.ui.theme.tooltipStyle
 
-// This whole file is mostly a fork of Jewel's Tooltip to add support for optional
-// tooltips. The obvious approach is to just invoke the content without the Tooltip wrapper, but
-// this works poorly with focus: if the error state changes, the node structure changes, and
-// focus is lost.
-
 /**
- * Displays a tooltip when hovering over the given [content] if [errorMessage] is not null;
- * otherwise, just displays the content.
+ * Displays a tooltip with error styling when hovering over the given [content] if [errorMessage] is
+ * not null; otherwise, just displays the content.
  */
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
@@ -58,7 +44,14 @@ internal fun ErrorTooltip(
   tooltipPlacement: TooltipPlacement = style.metrics.placement,
   content: @Composable () -> Unit,
 ) {
-  Tooltip(tooltip = errorMessage?.let { { Text(it) } }, modifier, style, tooltipPlacement, content)
+  Tooltip(
+    tooltip = { Text(errorMessage ?: "") },
+    modifier,
+    enabled = errorMessage != null,
+    style,
+    tooltipPlacement,
+    content,
+  )
 }
 
 @OptIn(ExperimentalFoundationApi::class)
@@ -86,49 +79,5 @@ internal fun validationErrorTooltipStyle(): TooltipStyle {
           offset = DpOffset(0.dp, -8.dp),
         ),
     ),
-  )
-}
-
-/** A variant of org.jetbrains.jewel.ui.component.Tooltip that makes [tooltip] optional. */
-@OptIn(ExperimentalFoundationApi::class)
-@Composable
-internal fun Tooltip(
-  tooltip: (@Composable () -> Unit)?,
-  modifier: Modifier = Modifier,
-  style: TooltipStyle = JewelTheme.tooltipStyle,
-  tooltipPlacement: TooltipPlacement = style.metrics.placement,
-  content: @Composable () -> Unit,
-) {
-  TooltipArea(
-    tooltip = {
-      if (tooltip != null) {
-        CompositionLocalProvider(LocalContentColor provides style.colors.content) {
-          Box(
-            modifier =
-              Modifier.shadow(
-                  elevation = style.metrics.shadowSize,
-                  shape = RoundedCornerShape(style.metrics.cornerSize),
-                  ambientColor = style.colors.shadow,
-                  spotColor = Color.Transparent,
-                )
-                .background(
-                  color = style.colors.background,
-                  shape = RoundedCornerShape(style.metrics.cornerSize),
-                )
-                .border(
-                  width = style.metrics.borderWidth,
-                  color = style.colors.border,
-                  shape = RoundedCornerShape(style.metrics.cornerSize),
-                )
-                .padding(style.metrics.contentPadding)
-          ) {
-            tooltip()
-          }
-        }
-      }
-    },
-    modifier = modifier,
-    tooltipPlacement = tooltipPlacement,
-    content = content,
   )
 }
