d02cea026e3fa429af0335d6c22e9f2a60b4c8db
Jenna Himawan <jhimawan@google.com>
2024-10-24 17:29:48 +0000

    Add textfield for specified Activity to DeepLink run configuration
    Followup to ag/29468159
    
    Bug: 367369341
    Test: DeepLinkConfigurableTest
    Change-Id: I3086f4f61d4f5b76195812e17ac2e1fea6283e2e
    
    GitOrigin-RevId: 8cc9d176c4476b8d36c27fc170129d47a3c7da0f

--- android/src/com/android/tools/idea/run/activity/launch/DeepLinkConfigurable.kt
+++ android/src/com/android/tools/idea/run/activity/launch/DeepLinkConfigurable.kt
@@ -15,15 +15,22 @@
  */
 package com.android.tools.idea.run.activity.launch
 
+import com.android.tools.idea.run.activity.ActivityLocatorUtils
 import com.android.tools.idea.run.editor.DeepLinkChooserDialog
 import com.intellij.execution.ExecutionBundle
+import com.intellij.ide.util.TreeClassChooser
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.ComponentWithBrowseButton
 import com.intellij.openapi.ui.Messages
 import com.intellij.openapi.util.text.StringUtil
+import com.intellij.psi.JavaPsiFacade
+import com.intellij.psi.search.ProjectScope
 import com.intellij.ui.components.JBTextField
 import com.intellij.ui.dsl.builder.AlignX
 import com.intellij.ui.dsl.builder.panel
+import org.jetbrains.android.util.AndroidBundle
+import org.jetbrains.android.util.AndroidTreeClassChooserFactory.createInheritanceClassChooser
+import org.jetbrains.android.util.AndroidUtils
 
 class DeepLinkConfigurable(project: Project, context: LaunchOptionConfigurableContext) : LaunchOptionConfigurable<DeepLinkLaunch.State?> {
   private val deepLinkField = ComponentWithBrowseButton(JBTextField(), null).apply {
@@ -46,10 +53,41 @@ class DeepLinkConfigurable(project: Project, context: LaunchOptionConfigurableCo
     }
     childComponent.emptyText.text = "Specify URL declared in the manifest"
   }
+  private val activityField = ComponentWithBrowseButton(JBTextField(), null).apply {
+    addActionListener {
+      if (!project.isInitialized) {
+        return@addActionListener
+      }
+      val facade = JavaPsiFacade.getInstance(project)
+      val activityBaseClass = facade.findClass(AndroidUtils.ACTIVITY_BASE_CLASS_NAME, ProjectScope.getAllScope(project))
+      if (activityBaseClass == null) {
+        Messages.showErrorDialog(project, AndroidBundle.message("cant.find.activity.class.error"), "Specific Activity Launcher")
+        return@addActionListener
+      }
+      val module = context.getModule()
+      if (module == null) {
+        Messages.showErrorDialog(project, ExecutionBundle.message("module.not.specified.error.text"), "Specific Activity Launcher")
+        return@addActionListener
+      }
+      val initialSelection = facade.findClass(childComponent.text, module.moduleWithDependenciesScope)
+      val chooser: TreeClassChooser = createInheritanceClassChooser(
+        project, "Select Activity Class", module.moduleWithDependenciesScope, activityBaseClass, initialSelection, null
+      )
+      chooser.showDialog()
+      val selClass = chooser.selected
+      if (selClass != null) {
+        childComponent.text = ActivityLocatorUtils.getQualifiedActivityName(selClass)
+      }
+    }
+  }
   private val panel = panel {
     row {
       label("URL:")
       cell(deepLinkField).resizableColumn().align(AlignX.FILL)
+    }
+    row {
+      label("Activity (optional):")
+      cell(activityField).resizableColumn().align(AlignX.FILL)
     }
   }
 
@@ -57,9 +95,11 @@ class DeepLinkConfigurable(project: Project, context: LaunchOptionConfigurableCo
 
   override fun resetFrom(state: DeepLinkLaunch.State) {
     deepLinkField.childComponent.setText(StringUtil.notNullize(state.DEEP_LINK))
+    activityField.childComponent.setText(StringUtil.notNullize(state.ACTIVITY))
   }
 
   override fun applyTo(state: DeepLinkLaunch.State) {
     state.DEEP_LINK = StringUtil.notNullize(deepLinkField.childComponent.getText())
+    state.ACTIVITY = StringUtil.notNullize(activityField.childComponent.getText())
   }
 }
