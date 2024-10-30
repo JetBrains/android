27ec00cf6ae0780f5cf0a5c520e59234a1dccccf
David Belcher <davidbelcher@google.com>
2024-10-30 13:15:49 +0000

    Clear metadata cache when project is unloaded
    The `RecipeUtils.getRecipeMetadata()` function converts a `Recipe` to a
    `RecipeMetadata`. It caches the result in a hashmap, which is stored as
    a static field of the `RecipeUtils` class. Since the `Project` object is
    used as a cache key and cache entries are never evicted, the project is
    leaked.
    
    In this CL, we configure a Disposer to ensure the cache entries
    associated with a project are cleared from the cache when the project is
    closed.
    
    The function has also been refactored to help avoid race condition
    issues.
    
    Bug: 288940571
    Test: Added new unit test
    Change-Id: I000c27808d59899b224dd46be0ba0f78d99f609b
    
    GitOrigin-RevId: 37a91e18d0a02aaa594dd4da8d60bd39062c33b4

--- assistant/testSrc/com/android/tools/idea/assistant/RecipeUtilsTest.kt
+++ assistant/testSrc/com/android/tools/idea/assistant/RecipeUtilsTest.kt
@@ -17,10 +17,17 @@ package com.android.tools.idea.assistant
 
 import com.android.SdkConstants
 import com.android.tools.idea.templates.recipe.RenderingContext
+import com.android.tools.idea.wizard.template.RecipeExecutor
+import com.android.tools.idea.wizard.template.TemplateData
 import com.google.common.collect.LinkedHashMultimap
 import com.google.common.collect.SetMultimap
+import com.intellij.openapi.project.ProjectManager
+import com.intellij.testFramework.ProjectRule
+import com.intellij.testFramework.runInEdtAndWait
+import org.junit.Assert.assertEquals
 import org.junit.Assert.assertFalse
 import org.junit.Assert.assertTrue
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
@@ -29,6 +36,9 @@ import org.mockito.kotlin.whenever
 
 @RunWith(JUnit4::class)
 class RecipeUtilsTest {
+
+  @get:Rule val projectRule = ProjectRule()
+
   @Test
   fun metadataDependenciesHaveImplementationAndApiConfigurations() {
     val dependencies: SetMultimap<String, String> = LinkedHashMultimap.create()
@@ -50,5 +60,29 @@ class RecipeUtilsTest {
     assertTrue(metadata.dependencies.contains("my.implementation.dependency"))
     assertTrue(metadata.dependencies.contains("my.api.dependency"))
     assertFalse(metadata.dependencies.contains("my.test.dependency"))
+  }
+
+  @Test
+  fun getRecipeMetadata_returnsCachedMetadata() {
+    val recipe = { _: RecipeExecutor, _: TemplateData -> }
+    val project = projectRule.project
+    val metadataFirst = RecipeUtils.getRecipeMetadata(recipe, project)
+    val metadataSecond = RecipeUtils.getRecipeMetadata(recipe, project)
+
+    assertEquals(metadataFirst, metadataSecond)
+    assertTrue(RecipeUtils.getRecipeMetadataCacheSize() == 1)
+  }
+
+  @Test
+  fun getRecipeMetadata_clearsCacheWhenProjectIsDisposed() {
+    val recipe = { _: RecipeExecutor, _: TemplateData -> }
+    val project = projectRule.project
+    RecipeUtils.getRecipeMetadata(recipe, project)
+
+    assertTrue(RecipeUtils.getRecipeMetadataCacheSize() > 0)
+
+    runInEdtAndWait { ProjectManager.getInstance().closeAndDispose(project) }
+
+    assertTrue(RecipeUtils.getRecipeMetadataCacheSize() == 0)
   }
 }
