c31feebfcee12c5e041137e746bfc61da85520f4
Nikita Biriukov <nikita.biriukov@jetbrains.com>
2024-10-30 13:49:18 +0100

    [android][gradle] IDEA-361677 support version catalogs in included builds without a settings file.
    Before this commit, version catalogs in an included build of a composite build were supported only if the included build had a settings file. If an included build didn't have its own settings file, BuildModel used a settings file from upper directories. This would be correct if the build was a subproject, but incorrect for an included build of a composite build.
    
    There is a difference between a "subproject" and an "included build of a composite build":
    1) In the root settings file:
      - `include("subproject")` registers subproject of a multi-project build.
      - `includeBuild("includedBuild")` registers an included build of a composite build.
    2) A subproject uses version catalogs from the root settings file. An included build doesn't because it could have its own settings file and its own version catalogs.
    
    GitOrigin-RevId: 0a9490348f6dac5330bf77a57708d9c9244d3504

--- gradle-dsl/src/com/android/tools/idea/gradle/dsl/model/BuildModelContext.java
+++ gradle-dsl/src/com/android/tools/idea/gradle/dsl/model/BuildModelContext.java
@@ -54,6 +54,7 @@ import com.intellij.openapi.module.Module;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.io.FileUtil;
 import com.intellij.openapi.vfs.StandardFileSystems;
+import com.intellij.openapi.vfs.VfsUtilCore;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.openapi.vfs.VirtualFileSystem;
 import java.io.File;
@@ -284,8 +285,7 @@ public final class BuildModelContext {
   private void populateVersionCatalogFiles(@Nullable GradleSettingsModel gradleSettingsModel) {
     if (!GradleDslModelExperimentalSettings.getInstance().isVersionCatalogEnabled()) return;
     if (gradleSettingsModel == null) {
-      checkVersionCatalog(VersionCatalogModel.DEFAULT_CATALOG_FILE, VersionCatalogModel.DEFAULT_CATALOG_NAME)
-        .ifPresent(myVersionCatalogFiles::add);
+      addDefaultVersionCatalogIfExists();
       return;
     }
     for (VersionCatalogModel versionCatalogModel : gradleSettingsModel.dependencyResolutionManagement().versionCatalogs()) {
@@ -302,6 +302,14 @@ public final class BuildModelContext {
           .ifPresent(myVersionCatalogFiles::add);
       }
     }
+  }
+
+  private void addDefaultVersionCatalogIfExists() {
+    if (myRootProjectFile == null) return;
+    VirtualFile buildDirectory = myRootProjectFile.getFile().getParent();
+    VirtualFile tomlFile = buildDirectory.findFileByRelativePath(VersionCatalogModel.DEFAULT_CATALOG_FILE);
+    checkVersionCatalog(tomlFile, VersionCatalogModel.DEFAULT_CATALOG_NAME)
+      .ifPresent(myVersionCatalogFiles::add);
   }
 
   private Optional<GradleVersionCatalogFile> checkVersionCatalog(String filePath, String name) {
@@ -353,7 +361,33 @@ public final class BuildModelContext {
       return null;
     }
     GradleSettingsFile settingsFile = getOrCreateSettingsFile(maybeSettingsFile);
-    return new GradleSettingsModelImpl(settingsFile);
+    GradleSettingsModel model = new GradleSettingsModelImpl(settingsFile);
+    if (isSettingsFileUnsuitable(buildDslFile, maybeSettingsFile, model)) {
+      return null;
+    }
+    return model;
+  }
+
+  /**
+   * @return `true` if the build file and the settings file are located in different directories, but a module of the build file is not a
+   * subproject of the build the settings file belongs to. In such a case, the module could be an included build and cannot use
+   * a settings file of its composite build.
+   */
+  private static boolean isSettingsFileUnsuitable(
+    @NotNull GradleBuildFile buildFile,
+    @NotNull VirtualFile settingsFile,
+    @NotNull GradleSettingsModel model
+  ) {
+    VirtualFile buildDirectory = buildFile.getFile().getParent();
+    VirtualFile settingsDirectory = settingsFile.getParent();
+    if (!buildDirectory.equals(settingsDirectory)) {
+      String modulePath = ":" + VfsUtilCore.getRelativePath(buildDirectory, settingsDirectory, ':');
+      // the settings file is unsuitable if it doesn't contain `include("$modulePath")` call
+      if (!model.modulePaths().contains(modulePath)) {
+        return true;
+      }
+    }
+    return false;
   }
 
   private void populateWithParentModuleSubProjectsProperties(@NotNull GradleBuildFile buildDslFile,
