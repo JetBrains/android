2a5879f2d32706b0414c58ae5c4c6dc077ff41dc
Jakub Gielzak <jgielzak@google.com>
2024-10-30 01:46:58 +0000

    Benchmark Deep Links: Perfetto Web Zoom
    Adds support for Benchmark deep links to support zooming-in on a process
    and time range.
    Query data is passed on without changes to the Perfetto Web Loader.
    
    Follow-up work: support for zooming in for Studio Profiler.
    
    Bug: 364596588
    Test: BenchmarkLinkListenerTest
    Change-Id: I7fa6e24e69aca9228a6cb28333b380f1ae360c03
    
    GitOrigin-RevId: dc0b97f865f598cb61566d77ff4daf5059b1e5ab

--- android/src/com/android/tools/idea/testartifacts/instrumented/testsuite/model/benchmark/BenchmarkLinkListener.kt
+++ android/src/com/android/tools/idea/testartifacts/instrumented/testsuite/model/benchmark/BenchmarkLinkListener.kt
@@ -15,22 +15,37 @@
  */
 package com.android.tools.idea.testartifacts.instrumented.testsuite.model.benchmark
 
+import com.android.tools.idea.perfetto.PerfettoTraceWebLoader
 import com.android.tools.idea.project.AndroidNotification
+import com.android.tools.idea.util.toIoFile
 import com.intellij.ide.browsers.BrowserLauncher
 import com.intellij.notification.NotificationType
 import com.intellij.openapi.fileEditor.FileEditorManager
 import com.intellij.openapi.fileEditor.OpenFileDescriptor
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.io.FileUtil
+import com.intellij.openapi.util.registry.Registry
 import com.intellij.openapi.vfs.LocalFileSystem
 import java.io.File
 import java.net.URI
 
-class BenchmarkLinkListener(private val project: Project) : HyperlinkListener {
+private val BENCHMARK_TRACE_FILE_PREFIX_V2 = BenchmarkOutput.BENCHMARK_TRACE_FILE_PREFIX
+private val BENCHMARK_TRACE_FILE_PREFIX_V3 = "uri://"
+
+class BenchmarkLinkListener(
+  private val project: Project,
+  private val isPerfettoWebLoaderEnabled: Boolean = Registry.`is`(PerfettoTraceWebLoader.FEATURE_REGISTRY_KEY, false),
+  private val openTraceInPerfettoWebLoader: (file: File, queryParams: String?) -> Unit = PerfettoTraceWebLoader::loadTrace
+) : HyperlinkListener {
   override fun hyperlinkClicked(link: String) {
     when {
-      link.startsWith(BenchmarkOutput.BENCHMARK_TRACE_FILE_PREFIX) -> {
-        val localFile = File(FileUtil.getTempDirectory() + link.replace(BenchmarkOutput.BENCHMARK_TRACE_FILE_PREFIX, File.separator))
+      link.startsWith(BENCHMARK_TRACE_FILE_PREFIX_V2) || link.startsWith(BENCHMARK_TRACE_FILE_PREFIX_V3) -> {
+        val link = convertLinkToV3Format(link) // replace the V2 prefix with a V3 prefix (V3 is backwards compatible)
+        check(link.startsWith(BENCHMARK_TRACE_FILE_PREFIX_V3)) // from this point we only deal with the V3 format
+
+        // check if the file exists
+        val fileName = link.drop(BENCHMARK_TRACE_FILE_PREFIX_V3.length).replace(Regex("\\?.*"), "") // drop query params (and the prefix)
+        val localFile = File(FileUtil.getTempDirectory() + File.separator + fileName)
         if (!localFile.exists()) {
           AndroidNotification.getInstance(project).showBalloon("Benchmark file not found", "Unable to open trace file (${localFile.name})",
                                                                NotificationType.WARNING)
@@ -39,12 +54,33 @@ class BenchmarkLinkListener(private val project: Project) : HyperlinkListener {
         }
         val virtualFileTrace = LocalFileSystem.getInstance().refreshAndFindFileByIoFile(localFile) ?: return
         val fd = OpenFileDescriptor(project, virtualFileTrace)
-        FileEditorManager.getInstance(project).openEditor(fd, true)
+
+        if (isPerfettoWebLoaderEnabled && isUiPerfettoDevSupportedFile(fileName)) {
+          // open the trace in Perfetto Web UI (if applicable)
+          val queryParams = Regex("\\?(.*)$").find(link)?.groupValues?.getOrNull(1)
+          openTraceInPerfettoWebLoader(fd.file.toIoFile(), queryParams)
+        } else {
+          // open the trace in the Studio Profiler
+          // TODO(b/364596134): pass selection parameters to PerfettoParser to zoom-in on a relevant part of the trace
+          FileEditorManager.getInstance(project).openEditor(fd, true)
+        }
       }
       link.startsWith("http://") || link.startsWith("https://") -> {
         BrowserLauncher.instance.browse(URI.create(link))
       }
-      else -> { /* ignore unrecognized links */ }
+
+      else -> {
+        /* ignore unrecognized links */
+      }
     }
+  }
+
+  // TODO(b/b/376667704): confirm if perf traces are also supported (and add to supported extensions)
+  private fun isUiPerfettoDevSupportedFile(fileName: String) = fileName.endsWith(".perfetto-trace")
+
+  private fun convertLinkToV3Format(link: String): String = when {
+    link.startsWith(BENCHMARK_TRACE_FILE_PREFIX_V2) -> BENCHMARK_TRACE_FILE_PREFIX_V3 + link.drop(BENCHMARK_TRACE_FILE_PREFIX_V2.length)
+    link.startsWith(BENCHMARK_TRACE_FILE_PREFIX_V3) -> link // no-op
+    else -> error("Unsupported Benchmark link format: $link")
   }
 }
--- android/testartifacts/testSrc/com/android/tools/idea/testartifacts/instrumented/testsuite/model/benchmark/BenchmarkLinkListenerTest.kt
+++ android/testartifacts/testSrc/com/android/tools/idea/testartifacts/instrumented/testsuite/model/benchmark/BenchmarkLinkListenerTest.kt
@@ -39,6 +39,7 @@ import org.mockito.kotlin.verify
 import org.mockito.kotlin.verifyNoInteractions
 import org.mockito.kotlin.whenever
 import java.net.URI
+import java.io.File
 
 class BenchmarkLinkListenerTest {
   private val projectRule = AndroidProjectRule.inMemory()
@@ -66,12 +67,54 @@ class BenchmarkLinkListenerTest {
   }
 
   @Test
-  fun listenerOpensFileLink() {
+  fun listenerOpensV2FileLink() {
     val listener = BenchmarkLinkListener(projectRule.project)
     val traceFile = FileUtil.createTempFile("traceFile", ".trace")
     traceFile.deleteOnExit()
     listener.hyperlinkClicked("file://${traceFile.name}")
     assertThat(fileCapture.value.file.name).isEqualTo(traceFile.name)
+  }
+
+  @Test
+  fun listenerOpensV3FileLink() {
+    val listener = BenchmarkLinkListener(projectRule.project)
+    val traceFile = FileUtil.createTempFile("traceFile", ".trace")
+    traceFile.deleteOnExit()
+    listener.hyperlinkClicked("uri://${traceFile.name}?param1=value1&param2=value2")
+    assertThat(fileCapture.value.file.name).isEqualTo(traceFile.name)
+  }
+
+  @Test
+  fun listenerOpensV2FileLinkInPerfettoWeb() {
+    val fakePerfettoLoader = FakePerfettoLoader()
+    assertThat(fakePerfettoLoader.callCount).isEqualTo(0)
+    val listener =
+      BenchmarkLinkListener(projectRule.project, isPerfettoWebLoaderEnabled = true, openTraceInPerfettoWebLoader = fakePerfettoLoader::load)
+    val traceFile = FileUtil.createTempFile("traceFile", ".perfetto-trace")
+    traceFile.deleteOnExit()
+    listener.hyperlinkClicked("file://${traceFile.name}")
+    with(fakePerfettoLoader) {
+      assertThat(callCount).isEqualTo(1)
+      assertThat(capturedFile.name).isEqualTo(traceFile.name)
+      assertThat(capturedQuery).isEqualTo(null)
+    }
+  }
+
+  @Test
+  fun listenerOpensV3FileLinkInPerfettoWeb() {
+    val fakePerfettoLoader = FakePerfettoLoader()
+    assertThat(fakePerfettoLoader.callCount).isEqualTo(0)
+    val listener =
+      BenchmarkLinkListener(projectRule.project, isPerfettoWebLoaderEnabled = true, openTraceInPerfettoWebLoader = fakePerfettoLoader::load)
+    val traceFile = FileUtil.createTempFile("traceFile", ".perfetto-trace")
+    traceFile.deleteOnExit()
+    val query = "param1=value1&param%202=value%202"
+    listener.hyperlinkClicked("uri://${traceFile.name}?$query")
+    with(fakePerfettoLoader) {
+      assertThat(callCount).isEqualTo(1)
+      assertThat(capturedFile.name).isEqualTo(traceFile.name)
+      assertThat(capturedQuery).isEqualTo(query)
+    }
   }
 
   @Test
@@ -99,5 +142,20 @@ class BenchmarkLinkListenerTest {
     listener.hyperlinkClicked("vnc://192.168.1.100")
     verifyNoInteractions(mockBrowserService)
     verifyNoInteractions(mockEditorService)
+  }
+}
+
+private class FakePerfettoLoader {
+  lateinit var capturedFile: File
+    private set
+  var capturedQuery: String? = null
+    private set
+  var callCount = 0
+    private set
+
+  fun load(file: File, query: String?) {
+    callCount++
+    this.capturedFile = file
+    this.capturedQuery = query
   }
 }
