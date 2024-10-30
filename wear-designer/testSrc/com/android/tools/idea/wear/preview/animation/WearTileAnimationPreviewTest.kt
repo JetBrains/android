d1b04daedd9325aad53f9d45e3a38c37af6db107
Matthew Gharrity <gharrma@google.com>
2024-10-29 20:18:18 -0700

    Fix lazy init for mock
    This is a follow-up to Change Id184f812f.
    
    Using 'by lazy' apparently results in nested mock initialization
    in some cases (depending on test order), leading to errors such
    as: org.mockito.exceptions.misusing.WrongTypeOfReturnValue.
    
    Test: WearTileAnimationPreviewTest
    Change-Id: I2861db5d186b779ee545d6b0e6a7f2478885eb70
    
    GitOrigin-RevId: c9ffd993edd054552d61b0ae2523da920c7e5902

--- wear-designer/testSrc/com/android/tools/idea/wear/preview/animation/WearTileAnimationPreviewTest.kt
+++ wear-designer/testSrc/com/android/tools/idea/wear/preview/animation/WearTileAnimationPreviewTest.kt
@@ -60,14 +60,13 @@ class WearTileAnimationPreviewTest {
 
   private lateinit var animationPreview: WearTileAnimationPreview
 
-  private val renderResultMock by lazy {
+  private val renderResultMock =
     Mockito.mock(RenderResult::class.java).apply {
       whenever(this.sourceFile).thenReturn(mock(PsiFile::class.java))
       whenever(this.logger).thenReturn(RenderLogger())
       whenever(this.renderResult).thenReturn(Status.SUCCESS.createResult())
-      whenever(this.module).thenReturn(projectRule.module)
+      whenever(this.module).doAnswer { projectRule.module }
     }
-  }
 
   private val wearTilePreviewElement: PsiWearTilePreviewElement =
     WearTilePreviewElement(
