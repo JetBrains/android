0d3ef6fb7dcc4e4a53b472e10f2869071abd3455
Noah Zimmt <noahz@google.com>
2024-11-15 11:26:56 -0800

    Remove output builder w/out bytecode analysis
    First of two CLs; rename in the second CL to prevent a large diff from
    occurring/blame issues.
    
    Bug: None
    Test: Existing
    Change-Id: I3b8234ef149feffc7c590e473ce67670274d6524
    
    GitOrigin-RevId: d7db6486718691ac946af43a3b5db13dab025e2f

--- android/src/com/android/tools/idea/run/deployment/liveedit/LiveEditOutputBuilderWithBytecodeAnalysis.kt
+++ android/src/com/android/tools/idea/run/deployment/liveedit/LiveEditOutputBuilderWithBytecodeAnalysis.kt
@@ -45,12 +45,9 @@ import org.jetbrains.kotlin.name.SpecialNames
 import org.jetbrains.kotlin.psi.KtFile
 import java.util.concurrent.TimeUnit
 
-private val logger = LogWrapper(Logger.getInstance(LiveEditOutputBuilder::class.java))
+private val logger = LogWrapper(Logger.getInstance(LiveEditOutputBuilderWithBytecodeAnalysis ::class.java))
 private val debug = LiveEditLogger("LiveEditOutputBuilder")
 
-// TODO: This is a direct copy of LiveEditOutputBuilder modified to use bytecode analysis for group selection
-//  This has been done to allow for a clean break between the paths that is toggled by a flag. When we are confident
-//  in this new path, the old version should be deleted and this duplication should be cleaned up
 internal class LiveEditOutputBuilderWithBytecodeAnalysis(private val apkClassProvider: ApkClassProvider) {
   // The outputs builder is *cumulative* and will include the outputs from *all previously compiled files* during this LiveEdit operation
   // Be extremely careful if you use the state inside the outputs object for any reason (or better yet, don't) - it's very easy to
