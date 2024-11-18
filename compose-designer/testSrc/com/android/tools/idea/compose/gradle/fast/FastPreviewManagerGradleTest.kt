0224d3c7aa7c63c05793815d5933530f8d71671f
Christophe Rhodes <xof@google.com>
2024-11-18 14:28:14 +0000

    Remove OutOfProcessCompilerDaemonClient
    Its only reference was from a (suite of) disabled tests.
    
    Bug: 328608974
    Test: existing
    Change-Id: I3e3e17b653cf15b1e9ed499dc00d8de5963f3006
    
    GitOrigin-RevId: df298e6ad80c49a685b26b311535c48b6f6c8e41

--- compose-designer/testSrc/com/android/tools/idea/compose/gradle/fast/FastPreviewManagerGradleTest.kt
+++ compose-designer/testSrc/com/android/tools/idea/compose/gradle/fast/FastPreviewManagerGradleTest.kt
@@ -23,7 +23,6 @@ import com.android.tools.idea.compose.gradle.renderer.renderPreviewElement
 import com.android.tools.idea.compose.preview.SIMPLE_COMPOSE_PROJECT_PATH
 import com.android.tools.idea.compose.preview.SimpleComposeAppPaths
 import com.android.tools.idea.concurrency.AndroidDispatchers.diskIoThread
-import com.android.tools.idea.editors.fast.CompilerDaemonClient
 import com.android.tools.idea.editors.fast.FastPreviewConfiguration
 import com.android.tools.idea.editors.fast.FastPreviewManager
 import com.android.tools.idea.editors.fast.toFileNameSet
@@ -47,7 +46,6 @@ import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.fileEditor.FileDocumentManager
 import com.intellij.openapi.module.ModuleUtilCore
 import com.intellij.openapi.project.guessProjectDir
-import com.intellij.openapi.util.Disposer
 import com.intellij.psi.PsiDocumentManager
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
@@ -64,7 +62,6 @@ import java.util.concurrent.CountDownLatch
 import java.util.concurrent.atomic.AtomicLong
 import kotlin.concurrent.thread
 import kotlin.time.Duration.Companion.seconds
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withContext
 import org.jetbrains.android.uipreview.ModuleClassLoaderOverlays
@@ -77,33 +74,8 @@ import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
-import org.junit.runner.RunWith
-import org.junit.runners.Parameterized
 
-/**
- * This factory will instantiate [OutOfProcessCompilerDaemonClientImpl] for the given version. This
- * factory will try to find a daemon for the given specific version or fallback to a stable one if
- * the specific one is not found. For example, for `1.1.0-alpha02`, this factory will try to locate
- * the jar for the daemon `kotlin-compiler-daemon-1.1.0-alpha02.jar`. If not found, it will
- * alternatively try `kotlin-compiler-daemon-1.1.0.jar` since it should be compatible.
- */
-private fun defaultDaemonFactory(
-  version: String,
-  log: Logger,
-  scope: CoroutineScope,
-): CompilerDaemonClient {
-  return OutOfProcessCompilerDaemonClientImpl(version, scope, log)
-}
-
-@RunWith(Parameterized::class)
-class FastPreviewManagerGradleTest(private val useEmbeddedCompiler: Boolean) {
-  companion object {
-    @Suppress("unused") // Used by JUnit via reflection
-    @JvmStatic
-    @get:Parameterized.Parameters(name = "useEmbeddedCompiler = {0}")
-    val useEmbeddedCompilerValues = listOf(true) // TODO: add "false" back after compose 1.3 release
-  }
-
+class FastPreviewManagerGradleTest {
   @get:Rule val projectRule = ComposeGradleProjectRule(SIMPLE_COMPOSE_PROJECT_PATH)
 
   private lateinit var psiMainFile: PsiFile
@@ -117,14 +89,7 @@ class FastPreviewManagerGradleTest(private val useEmbeddedCompiler: Boolean) {
         .guessProjectDir()!!
         .findFileByRelativePath(SimpleComposeAppPaths.APP_MAIN_ACTIVITY.path)!!
     psiMainFile = runReadAction { PsiManager.getInstance(projectRule.project).findFile(mainFile)!! }
-    fastPreviewManager =
-      if (useEmbeddedCompiler) FastPreviewManager.getInstance(projectRule.project)
-      else
-        FastPreviewManager.getTestInstance(
-            projectRule.project,
-            { version, _, log, scope -> defaultDaemonFactory(version, log, scope) },
-          )
-          .also { Disposer.register(projectRule.fixture.testRootDisposable, it) }
+    fastPreviewManager = FastPreviewManager.getInstance(projectRule.project)
     runInEdtAndWait {
       projectRule.buildAndAssertIsSuccessful()
       projectRule.fixture.openFileInEditor(mainFile)
@@ -284,10 +249,6 @@ class FastPreviewManagerGradleTest(private val useEmbeddedCompiler: Boolean) {
   // Regression test for b/228168101
   @Test
   fun `test parallel compilations`() {
-    // This tests is only to verify the interaction of the internal compiler with the Live Edit
-    // on device compilation.
-    if (!useEmbeddedCompiler) return
-
     var compile = true
     val startCountDownLatch = CountDownLatch(1)
 
