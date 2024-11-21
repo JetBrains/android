6ca04d13b8906a1c191f4cb762501b67987bb14c
Paul Milian <paulmilian@google.com>
2024-11-21 12:36:56 +0000

    Remove unnecessary SlowOperations
    The write code is being run on the EDT and the call to
    WorkspaceFileIndexDataImpl.ensureIsUpToDate no longer throws a
    SlowOperation exception.
    
    Fixes: 342566476
    Test: existing tests should still pass
    Change-Id: Ic69c079a4201d5727ca56896e31d861ba6c476e8
    
    GitOrigin-RevId: dcf5f3a66d140ad934a1f5ccd16e26537dff9f58

--- designer/src/com/android/tools/idea/uibuilder/property/NlPropertiesModel.kt
+++ designer/src/com/android/tools/idea/uibuilder/property/NlPropertiesModel.kt
@@ -54,11 +54,9 @@ import com.intellij.openapi.application.runReadAction
 import com.intellij.openapi.extensions.ExtensionPointName
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.Disposer
-import com.intellij.openapi.util.ThrowableComputable
 import com.intellij.pom.Navigatable
 import com.intellij.psi.xml.XmlTag
 import com.intellij.util.Alarm
-import com.intellij.util.SlowOperations
 import com.intellij.util.concurrency.AppExecutorUtil
 import com.intellij.util.ui.update.MergingUpdateQueue
 import com.intellij.util.ui.update.Update
@@ -254,43 +252,38 @@ open class NlPropertiesModel(
     ApplicationManager.getApplication()
       .invokeLater(
         {
-          SlowOperations.allowSlowOperations(
-            ThrowableComputable {
-              NlWriteCommandActionUtil.run(
-                property.components,
-                "Set $componentName.${property.name} to $newValue",
-              ) {
-                property.components.forEach {
-                  it.setAttribute(property.namespace, property.name, newValue)
-                }
-                val compatibleAttribute = compatibleMarginAttribute(property)
-                if (compatibleAttribute != null) {
-                  property.components.forEach {
-                    it.setAttribute(property.namespace, compatibleAttribute, newValue)
-                  }
-                }
-                logPropertyValueChanged(property)
-                if (property.namespace == TOOLS_URI) {
-                  if (newValue != null) {
-                    // A tools property may not be in the current set of possible properties. So add
-                    // it
-                    // now:
-                    if (properties.isEmpty) {
-                      properties = provider.createEmptyTable()
-                    }
-                    properties.put(property)
-                  }
-
-                  if (property.name == ATTR_PARENT_TAG) {
-                    // When the "parentTag" attribute is set on a <merge> tag,
-                    // we may have a different set of available properties available,
-                    // since the attributes of the "parentTag" are included if set.
-                    firePropertiesGenerated()
-                  }
-                }
+          NlWriteCommandActionUtil.run(
+            property.components,
+            "Set $componentName.${property.name} to $newValue",
+          ) {
+            property.components.forEach {
+              it.setAttribute(property.namespace, property.name, newValue)
+            }
+            val compatibleAttribute = compatibleMarginAttribute(property)
+            if (compatibleAttribute != null) {
+              property.components.forEach {
+                it.setAttribute(property.namespace, compatibleAttribute, newValue)
               }
             }
-          )
+            logPropertyValueChanged(property)
+            if (property.namespace == TOOLS_URI) {
+              if (newValue != null) {
+                // A tools property may not be in the current set of possible properties. So add
+                // it now:
+                if (properties.isEmpty) {
+                  properties = provider.createEmptyTable()
+                }
+                properties.put(property)
+              }
+
+              if (property.name == ATTR_PARENT_TAG) {
+                // When the "parentTag" attribute is set on a <merge> tag,
+                // we may have a different set of available properties available,
+                // since the attributes of the "parentTag" are included if set.
+                firePropertiesGenerated()
+              }
+            }
+          }
         },
         { Disposer.isDisposed(this) },
       )
