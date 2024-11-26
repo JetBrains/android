1288685878081fd1b46b3b02b60c154353ea0b68
Alexey Kudravtsev <cdr@intellij.com>
2024-11-26 12:47:39 +0100

    annotate @Unmodifiable collections to which unmodifiable data from ContainerUtil is assigned, to make it easier for data-flow inspection to spot mutability-related errors
    GitOrigin-RevId: 8ef93cefdb73dbdec1ac29f038cb8edac5d7a27f
    (cherry picked from commit 7f9fbf0c9fb9b77d3e163da66ca3ab05e46a9463)
    Change-Id: I0458954289b14fd5011072f902080990de19a3fe

--- gradle-dsl/src/com/android/tools/idea/gradle/dsl/api/ext/GradlePropertyModel.java
+++ gradle-dsl/src/com/android/tools/idea/gradle/dsl/api/ext/GradlePropertyModel.java
@@ -18,12 +18,12 @@ import com.android.tools.idea.gradle.dsl.api.util.GradleDslElementModel;
 import com.android.tools.idea.gradle.dsl.api.util.TypeReference;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiElement;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.Map;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.jetbrains.annotations.Unmodifiable;
 
 /**
  * This class represents a property or variable declared or referenced by the ExtraPropertiesExtension
@@ -298,6 +298,7 @@ public interface GradlePropertyModel extends DeletablePsiElementHolder, GradleDs
    * @return the value of the property as a List or null if no value exists or is of an incorrect type.
    */
   @Nullable
+  @Unmodifiable
   List<GradlePropertyModel> toList();
 
   /**
