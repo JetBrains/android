b714e1a27154ceb9dead6495eac29a4cabd6cb47
Vidish Naik <vidish@google.com>
2024-12-04 17:09:12 -0800

    Make Vitals SDK insights panel scrollable
    SDK insights received from Vitals can be extremely long to fit in the
    issue details space. This leads to the insight getting truncated on the
    right side. Move it to a scroll pane to make it horizontally scrollable.
    
    Fixes: 373816200
    Test: Existing
    Change-Id: I628ead3aa870132d37d2354c8596e380a6e05d52
    
    GitOrigin-RevId: 7fbf33711053120483cefb3303a6c94bf73eb18a

--- app-quality-insights/play-vitals/view/src/com/android/tools/idea/vitals/ui/SdkInsightsPanel.kt
+++ app-quality-insights/play-vitals/view/src/com/android/tools/idea/vitals/ui/SdkInsightsPanel.kt
@@ -21,13 +21,20 @@ import com.android.tools.idea.insights.ui.transparentPanel
 import com.intellij.icons.AllIcons
 import com.intellij.ui.HyperlinkLabel
 import com.intellij.ui.components.JBLabel
+import com.intellij.ui.components.JBScrollPane
 import com.intellij.util.ui.JBFont
 import com.intellij.util.ui.JBUI
 import java.awt.BorderLayout
+import java.awt.CardLayout
 import java.awt.FlowLayout
 import java.awt.Graphics
+import java.awt.event.ComponentAdapter
+import java.awt.event.ComponentEvent
 import javax.swing.JPanel
 import javax.swing.text.html.HTMLDocument
+
+private const val SEE_MORE = "more"
+private const val EXPANDED = "expanded"
 
 class SdkInsightsPanel(category: String, title: String, private val body: String) :
   JPanel(BorderLayout()) {
@@ -51,7 +58,23 @@ class SdkInsightsPanel(category: String, title: String, private val body: String
   // The url wrapped with () parentheses and is located at the end of the body.
   private val urlRegex = Regex("\\(([^)]+)\\)\\s*$", RegexOption.MULTILINE)
 
-  private val expandedLabel = HtmlLabel()
+  private val expandedLabel = HtmlLabel().apply { text = replaceUrlsWithHtmlLinks(body) }
+
+  private val scrollPane =
+    JBScrollPane(expandedLabel).apply {
+      verticalScrollBarPolicy = JBScrollPane.VERTICAL_SCROLLBAR_NEVER
+      horizontalScrollBarPolicy = JBScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED
+      border = JBUI.Borders.empty()
+    }
+
+  private val expandPanel =
+    transparentPanel(BorderLayout()).apply { add(scrollPane, BorderLayout.CENTER) }
+
+  private val cardPanel =
+    transparentPanel(CardLayout()).apply {
+      add(seeMorePanel, SEE_MORE)
+      add(expandPanel, EXPANDED)
+    }
 
   init {
     border = JBUI.Borders.empty(7)
@@ -64,16 +87,24 @@ class SdkInsightsPanel(category: String, title: String, private val body: String
     seeMorePanel.add(seeMoreLinkLabel, TabularLayout.Constraint(0, 1))
 
     add(topPanel, BorderLayout.NORTH)
-    add(seeMorePanel, BorderLayout.CENTER)
-    add(expandedLabel, BorderLayout.SOUTH)
-    expandedLabel.isVisible = false
+    add(cardPanel, BorderLayout.CENTER)
+    cardPanel.preferredSize = seeMorePanel.preferredSize
   }
 
   private fun expand() {
-    HtmlLabel.setUpAsHtmlLabel(expandedLabel)
-    expandedLabel.text = replaceUrlsWithHtmlLinks(body)
-    seeMorePanel.isVisible = false
-    expandedLabel.isVisible = true
+    (cardPanel.layout as CardLayout).show(cardPanel, EXPANDED)
+    cardPanel.preferredSize = expandPanel.preferredSize
+    scrollPane.horizontalScrollBar.value = 0
+    // Resizing studio after the expanded label is visible will leave a blank space
+    // between this panel and the tabbed pane.
+    // Resize cardPanel to expandPanel's preferred size to leave no gap in between.
+    expandPanel.addComponentListener(
+      object : ComponentAdapter() {
+        override fun componentResized(e: ComponentEvent?) {
+          cardPanel.preferredSize = expandPanel.preferredSize
+        }
+      }
+    )
   }
 
   private fun replaceUrlsWithHtmlLinks(body: String) =
