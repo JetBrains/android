2607b6aad78270851e6ac149dc17846bc2b89f6a
James Rose <rosej@google.com>
2024-12-05 10:40:37 -0800

    Use extension filter instead of file filter
    The latter doesn't actually do anything with the native file chooser
    (at least on Mac).
    
    Fixes: 369275230
    Test: n/a
    Change-Id: I21c72e7276700d98dc8d746f441802c94630f2a1
    
    GitOrigin-RevId: 93520ed9f5482d8497f0b4ba9a299080998219dd

--- avd/src/com/android/tools/idea/avd/StorageGroup.kt
+++ avd/src/com/android/tools/idea/avd/StorageGroup.kt
@@ -280,7 +280,7 @@ private fun chooseFile(parent: Component, project: Project?): Path? {
         /* chooseJarContents= */ false,
         /* chooseMultiple= */ false,
       )
-      .withFileFilter { it.name.endsWith(".img", ignoreCase = true) }
+      .withExtensionFilter(".img")
 
   val virtualFile = FileChooser.chooseFile(descriptor, parent, project, null) ?: return null
 
