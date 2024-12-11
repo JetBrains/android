00f7c1a080255a88414ea851f8724182412846af
Kurt Dresner <kdresner@google.com>
2024-12-11 15:26:49 -0800

    Convert CreateTypedResourceFileActionTest to Kotlin [3/3]
    Manual fixes.
    
    Bug: N/A
    Test: N/A
    Change-Id: I9ea151167179c8f152b81c3ca0baa56779f01145
    
    GitOrigin-RevId: d7a01e0e6bfdb177fb298220de1621d98a3d0250

--- android/jetbrains/testSrc/org/jetbrains/android/actions/CreateTypedResourceFileActionTest.kt
+++ android/jetbrains/testSrc/org/jetbrains/android/actions/CreateTypedResourceFileActionTest.kt
@@ -16,42 +16,59 @@
 package org.jetbrains.android.actions
 
 import com.android.ide.common.repository.GoogleMavenArtifactId
-import com.android.ide.common.repository.GradleCoordinate
-import com.android.ide.common.repository.GradleVersion
 import com.android.resources.ResourceFolderType
-import com.android.tools.idea.projectsystem.TestProjectSystem
+import com.android.testutils.TestUtils
+import com.android.tools.idea.projectsystem.ProjectSystemSyncManager
+import com.android.tools.idea.projectsystem.getModuleSystem
+import com.android.tools.idea.projectsystem.getSyncManager
+import com.android.tools.idea.testing.AndroidProjectRule
+import com.android.tools.idea.util.toIoFile
+import com.google.common.truth.Truth.assertThat
+import com.google.common.truth.Truth.assertWithMessage
 import com.intellij.openapi.actionSystem.CommonDataKeys
 import com.intellij.openapi.actionSystem.impl.SimpleDataContext
-import com.intellij.psi.PsiDirectory
-import junit.framework.TestCase
-import org.jetbrains.android.AndroidTestCase
+import com.intellij.openapi.vfs.LocalFileSystem
+import com.intellij.testFramework.PsiTestUtil
+import com.intellij.util.io.ZipUtil
 import org.jetbrains.android.actions.CreateTypedResourceFileAction.Companion.doIsAvailable
 import org.jetbrains.android.actions.CreateTypedResourceFileAction.Companion.getDefaultRootTagByResourceType
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+import java.io.File
 
-class CreateTypedResourceFileActionTest : AndroidTestCase() {
-  fun testDoIsAvailableForTypedResourceDirectory() {
+@RunWith(JUnit4::class)
+class CreateTypedResourceFileActionTest {
+  @get:Rule val projectRule = AndroidProjectRule.onDisk()
+  val project by lazy { projectRule.project }
+  val fixture by lazy { projectRule.fixture }
+  val module by lazy { projectRule.module }
+
+  @Test
+  fun doIsAvailableForTypedResourceDirectory() {
     val dataContext = SimpleDataContext.builder()
     for (folderType in ResourceFolderType.entries) {
-      val filePath = "res/" + folderType.getName() + "/my_" + folderType.getName() + ".xml"
-      dataContext.add<PsiDirectory?>(CommonDataKeys.PSI_ELEMENT, myFixture.addFileToProject(filePath, "").getParent())
-      assertTrue("Failed for " + folderType.name, doIsAvailable(dataContext.build(), folderType.getName()))
+      val filePath = "res/" + folderType.name + "/my_" + folderType.name + ".xml"
+      dataContext.add(CommonDataKeys.PSI_ELEMENT, fixture.addFileToProject(filePath, "").parent)
+      assertWithMessage("Failed for ${folderType.name}").that(doIsAvailable(dataContext.build(), folderType.name)).isTrue()
     }
 
-    val resDir = myFixture.findFileInTempDir("res")
-    val psiResDir = myFixture.getPsiManager().findDirectory(resDir)
-    dataContext.add<PsiDirectory?>(CommonDataKeys.PSI_ELEMENT, psiResDir)
+    val resDir = fixture.findFileInTempDir("res")
+    val psiResDir = fixture.psiManager.findDirectory(resDir)
+    dataContext.add(CommonDataKeys.PSI_ELEMENT, psiResDir)
     // Should fail when the directory is not a type specific resource directory (e.g: res/drawable).
-    assertFalse(doIsAvailable(dataContext.build(), ResourceFolderType.DRAWABLE.getName()))
+    assertThat(doIsAvailable(dataContext.build(), ResourceFolderType.DRAWABLE.name)).isFalse()
   }
 
-  fun testAddPreferencesScreenAndroidxPreferenceLibraryHandling() {
+  @Test
+  fun addPreferencesScreenAndroidxPreferenceLibraryHandling() {
     // First check without having androidx.preference as a library
-    TestCase.assertEquals("PreferenceScreen", getDefaultRootTagByResourceType(myModule, ResourceFolderType.XML))
+    assertThat(getDefaultRootTagByResourceType(module, ResourceFolderType.XML)).isEqualTo("PreferenceScreen")
+
+    module.getModuleSystem().registerDependency(GoogleMavenArtifactId.ANDROIDX_PREFERENCE.getCoordinate("+"))
 
     // Now with the dependency, the handler should return "androidx.preference.PreferenceScreen"
-    val testProjectSystem = TestProjectSystem(getProject(), mutableListOf<GradleCoordinate>())
-    testProjectSystem.useInTests()
-    testProjectSystem.addDependency(GoogleMavenArtifactId.ANDROIDX_PREFERENCE, myFacet.getModule(), GradleVersion(1, 1))
-    TestCase.assertEquals("androidx.preference.PreferenceScreen", getDefaultRootTagByResourceType(myModule, ResourceFolderType.XML))
+    assertThat(getDefaultRootTagByResourceType(module, ResourceFolderType.XML)).isEqualTo("androidx.preference.PreferenceScreen")
   }
 }
