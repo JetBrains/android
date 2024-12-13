ae9d781bd4328c79672fcc96471488690e42e694
Kurt Dresner <kdresner@google.com>
2024-12-13 13:20:16 -0800

    Add tests for new code in CreateTypedResourceFileAction
    Fixes: 375550832
    Test: CreateTypedResourceFileActionTest.kt
    Change-Id: I6f1fd0b0608aa7b497756816724284910494979b
    
    GitOrigin-RevId: c4b8d83e9f3ecf8c92fb39551957e9a4fd818e45

--- android/jetbrains/testSrc/org/jetbrains/android/actions/CreateTypedResourceFileActionTest.kt
+++ android/jetbrains/testSrc/org/jetbrains/android/actions/CreateTypedResourceFileActionTest.kt
@@ -17,33 +17,34 @@ package org.jetbrains.android.actions
 
 import com.android.ide.common.repository.GoogleMavenArtifactId
 import com.android.resources.ResourceFolderType
-import com.android.testutils.TestUtils
-import com.android.tools.idea.projectsystem.ProjectSystemSyncManager
 import com.android.tools.idea.projectsystem.getModuleSystem
-import com.android.tools.idea.projectsystem.getSyncManager
 import com.android.tools.idea.testing.AndroidProjectRule
-import com.android.tools.idea.util.toIoFile
+import com.android.tools.idea.testing.onEdt
 import com.google.common.truth.Truth.assertThat
 import com.google.common.truth.Truth.assertWithMessage
 import com.intellij.openapi.actionSystem.CommonDataKeys
 import com.intellij.openapi.actionSystem.impl.SimpleDataContext
-import com.intellij.openapi.vfs.LocalFileSystem
-import com.intellij.testFramework.PsiTestUtil
-import com.intellij.util.io.ZipUtil
+import com.intellij.openapi.fileEditor.FileEditorManager
+import com.intellij.openapi.vfs.VirtualFileManager
+import com.intellij.psi.PsiFile
+import com.intellij.psi.PsiManager
+import com.intellij.testFramework.RunsInEdt
+import java.nio.file.Path
+import kotlin.jvm.java
+import kotlin.test.assertNotNull
 import org.jetbrains.android.actions.CreateTypedResourceFileAction.Companion.doIsAvailable
 import org.jetbrains.android.actions.CreateTypedResourceFileAction.Companion.getDefaultRootTagByResourceType
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
-import java.io.File
 
 @RunWith(JUnit4::class)
 class CreateTypedResourceFileActionTest {
-  @get:Rule val projectRule = AndroidProjectRule.onDisk()
+  @get:Rule val projectRule = AndroidProjectRule.onDisk().onEdt()
   val project by lazy { projectRule.project }
   val fixture by lazy { projectRule.fixture }
-  val module by lazy { projectRule.module }
+  val module by lazy { projectRule.projectRule.module }
 
   @Test
   fun doIsAvailableForTypedResourceDirectory() {
@@ -51,7 +52,9 @@ class CreateTypedResourceFileActionTest {
     for (folderType in ResourceFolderType.entries) {
       val filePath = "res/" + folderType.name + "/my_" + folderType.name + ".xml"
       dataContext.add(CommonDataKeys.PSI_ELEMENT, fixture.addFileToProject(filePath, "").parent)
-      assertWithMessage("Failed for ${folderType.name}").that(doIsAvailable(dataContext.build(), folderType.name)).isTrue()
+      assertWithMessage("Failed for ${folderType.name}")
+        .that(doIsAvailable(dataContext.build(), folderType.name))
+        .isTrue()
     }
 
     val resDir = fixture.findFileInTempDir("res")
@@ -64,11 +67,71 @@ class CreateTypedResourceFileActionTest {
   @Test
   fun addPreferencesScreenAndroidxPreferenceLibraryHandling() {
     // First check without having androidx.preference as a library
-    assertThat(getDefaultRootTagByResourceType(module, ResourceFolderType.XML)).isEqualTo("PreferenceScreen")
+    assertThat(getDefaultRootTagByResourceType(module, ResourceFolderType.XML))
+      .isEqualTo("PreferenceScreen")
 
-    module.getModuleSystem().registerDependency(GoogleMavenArtifactId.ANDROIDX_PREFERENCE.getCoordinate("+"))
+    module
+      .getModuleSystem()
+      .registerDependency(GoogleMavenArtifactId.ANDROIDX_PREFERENCE.getCoordinate("+"))
 
     // Now with the dependency, the handler should return "androidx.preference.PreferenceScreen"
-    assertThat(getDefaultRootTagByResourceType(module, ResourceFolderType.XML)).isEqualTo("androidx.preference.PreferenceScreen")
+    assertThat(getDefaultRootTagByResourceType(module, ResourceFolderType.XML))
+      .isEqualTo("androidx.preference.PreferenceScreen")
+  }
+
+  @RunsInEdt
+  @Test
+  fun doCreateAndNavigate_rawResource_navigateTrue() {
+    val filename = "new_resource_file.json"
+    val action =
+      CreateTypedResourceFileAction("Unimportant string", ResourceFolderType.RAW, false, false)
+    val virtualFileDir =
+      assertNotNull(
+        VirtualFileManager.getInstance().findFileByNioPath(Path.of(fixture.tempDirPath))
+      )
+    val psiDirectory = assertNotNull(PsiManager.getInstance(project).findDirectory(virtualFileDir))
+    val elements =
+      action.doCreateAndNavigate(
+        filename,
+        psiDirectory,
+        rootTagName = "",
+        chooseTagName = false,
+        navigate = true,
+      )
+    assertThat(elements).hasLength(1)
+    val addedFile = elements.single()
+    assertThat(addedFile).isInstanceOf(PsiFile::class.java)
+    addedFile as PsiFile
+    val editor = assertNotNull(FileEditorManager.getInstance(project).selectedTextEditor)
+    assertThat(editor.virtualFile).isEqualTo(addedFile.virtualFile)
+    assertThat(editor.virtualFile.name).isEqualTo(filename)
+    assertThat(editor.document.text).isEmpty()
+  }
+
+  @RunsInEdt
+  @Test
+  fun doCreateAndNavigate_rawResource_navigateFalse() {
+    val filename = "new_resource_file.json"
+    val action =
+      CreateTypedResourceFileAction("Unimportant string", ResourceFolderType.RAW, false, false)
+    val virtualFileDir =
+      assertNotNull(
+        VirtualFileManager.getInstance().findFileByNioPath(Path.of(fixture.tempDirPath))
+      )
+    val psiDirectory = assertNotNull(PsiManager.getInstance(project).findDirectory(virtualFileDir))
+    val elements =
+      action.doCreateAndNavigate(
+        filename,
+        psiDirectory,
+        rootTagName = "",
+        chooseTagName = false,
+        navigate = false,
+      )
+    assertThat(elements).hasLength(1)
+    val addedFile = elements.single()
+    assertThat(addedFile).isInstanceOf(PsiFile::class.java)
+    addedFile as PsiFile
+    assertThat(addedFile.virtualFile.name).isEqualTo(filename)
+    assertThat(FileEditorManager.getInstance(project).selectedTextEditor).isNull()
   }
 }
