43aac013f92a6ed3d7e2135da69c774d559bab3a
Alex Golubev <alexgolubev@google.com>
2024-12-20 20:02:57 -0500

    Reformat some recipes
    Bug: NA
    Test: NA
    Change-Id: I50c10569698ffeb6c38d36a078b077b50462cc19
    
    GitOrigin-RevId: 8eee32e5a95a4dbae5406cf470d5ba5dee45f55a

--- android-npw/src/com/android/tools/idea/npw/module/recipes/baselineProfilesModule/BaselineProfilesMacrobenchmarkCommon.kt
+++ android-npw/src/com/android/tools/idea/npw/module/recipes/baselineProfilesModule/BaselineProfilesMacrobenchmarkCommon.kt
@@ -23,16 +23,16 @@ import com.android.tools.idea.wizard.template.ModuleTemplateData
 import com.android.tools.idea.wizard.template.RecipeExecutor
 import java.util.Locale
 
-/**
- * Common class that serves functionality for Macrobenchmark and Baseline Profiles templates.
- */
+/** Common class that serves functionality for Macrobenchmark and Baseline Profiles templates. */
 object BaselineProfilesMacrobenchmarkCommon {
-  const val FILTER_INSTR_ARG = "android.testInstrumentationRunnerArguments.androidx.benchmark.enabledRules"
+  const val FILTER_INSTR_ARG =
+    "android.testInstrumentationRunnerArguments.androidx.benchmark.enabledRules"
   const val FILTER_ARG_BASELINE_PROFILE = "baselineprofile"
   const val FILTER_ARG_MACROBENCHMARK = "macrobenchmark"
 
   /**
    * Creates a new Gradle module for Macrobenchmark / Baseline Profiles module.
+   *
    * @param buildGradleContent represents build.gradle(.kts) string
    * @param customizeModule is meant for creating benchmarks and optional setup
    */
@@ -46,7 +46,8 @@ object BaselineProfilesMacrobenchmarkCommon {
     addIncludeToSettings(newModule.name)
 
     // Create build.gradle(.kts) with the content from [buildGradle] lambda
-    val buildFile = if (useGradleKts) SdkConstants.FN_BUILD_GRADLE_KTS else SdkConstants.FN_BUILD_GRADLE
+    val buildFile =
+      if (useGradleKts) SdkConstants.FN_BUILD_GRADLE_KTS else SdkConstants.FN_BUILD_GRADLE
     save(buildGradleContent, newModule.rootDir.resolve(buildFile))
 
     // Apply all required dependencies
@@ -55,14 +56,22 @@ object BaselineProfilesMacrobenchmarkCommon {
     addDependency("androidx.test.ext:junit:+", "implementation")
     addDependency("androidx.test.espresso:espresso-core:+", "implementation")
     addDependency("androidx.test.uiautomator:uiautomator:+", "implementation")
-    addDependency("androidx.benchmark:benchmark-macro-junit4:+", "implementation", macrobenchmarkMinRev)
+    addDependency(
+      "androidx.benchmark:benchmark-macro-junit4:+",
+      "implementation",
+      macrobenchmarkMinRev,
+    )
 
     // Add empty android manifest to be proper Android module
     save("<manifest />", newModule.rootDir.resolve("src/main/AndroidManifest.xml"))
     // Add gitignore with build/
     save(gitignore(), newModule.rootDir.resolve(".gitignore"))
 
-    addKotlinIfNeeded(newModule.projectTemplateData, targetApi = newModule.apis.targetApi.api, noKtx = true)
+    addKotlinIfNeeded(
+      newModule.projectTemplateData,
+      targetApi = newModule.apis.targetApi.api,
+      noKtx = true,
+    )
 
     // Create (and open) the content test classes (benchmarks / baseline profile generator)
     customizeModule()
@@ -70,7 +79,7 @@ object BaselineProfilesMacrobenchmarkCommon {
 
   fun flavorsConfigurationsBuildGradle(
     flavors: ProductFlavorsWithDimensions,
-    useGradleKts: Boolean
+    useGradleKts: Boolean,
   ): String {
     return buildString {
       if (flavors.dimensions.isNotEmpty()) {
@@ -78,8 +87,7 @@ object BaselineProfilesMacrobenchmarkCommon {
 
         if (useGradleKts) {
           appendLine("flavorDimensions += listOf(${dimenString})")
-        }
-        else {
+        } else {
           appendLine("flavorDimensions += [$dimenString]")
         }
       }
@@ -96,16 +104,18 @@ object BaselineProfilesMacrobenchmarkCommon {
   }
 
   /**
-   * Retrieves the product flavors from [GradleAndroidModel].
-   * We don't retrieve it from the DSL model, because it doesn't support loading flavors that aren't set in the build.gradle directly,
+   * Retrieves the product flavors from [GradleAndroidModel]. We don't retrieve it from the DSL
+   * model, because it doesn't support loading flavors that aren't set in the build.gradle directly,
    * for example, when using convention plugins.
    */
-  fun getTargetModelProductFlavors(targetModuleGradleModel: GradleAndroidModel): ProductFlavorsWithDimensions =
+  fun getTargetModelProductFlavors(
+    targetModuleGradleModel: GradleAndroidModel
+  ): ProductFlavorsWithDimensions =
     ProductFlavorsWithDimensions(
       targetModuleGradleModel.productFlavorNamesByFlavorDimension.keys,
       targetModuleGradleModel.productFlavorNamesByFlavorDimension.flatMap { (dimension, flavors) ->
         flavors.map { flavorName -> ProductFlavorsWithDimensions.Item(flavorName, dimension) }
-      }
+      },
     )
 }
 
@@ -116,20 +126,14 @@ data class GmdSpec(val deviceName: String, val apiLevel: Int, val systemImageSou
     append(deviceName.replace(" ", "").replaceFirstChar { it.lowercase(Locale.getDefault()) })
     append("Api$apiLevel")
   }
-
 }
 
-class ProductFlavorsWithDimensions(
-  val dimensions: Collection<String>,
-  val flavors: List<Item>
-) {
+class ProductFlavorsWithDimensions(val dimensions: Collection<String>, val flavors: List<Item>) {
 
   data class Item(val name: String, val dimension: String?)
 
-  val flavorNamesGroupedByDimension = dimensions.map { dimensionName ->
-    flavors
-      .filter { it.dimension == dimensionName }
-      .map { it.name }
-  }
-
+  val flavorNamesGroupedByDimension =
+    dimensions.map { dimensionName ->
+      flavors.filter { it.dimension == dimensionName }.map { it.name }
+    }
 }
--- android-npw/src/com/android/tools/idea/npw/module/recipes/baselineProfilesModule/generateBaselineProfilesModule.kt
+++ android-npw/src/com/android/tools/idea/npw/module/recipes/baselineProfilesModule/generateBaselineProfilesModule.kt
@@ -15,8 +15,8 @@
  */
 package com.android.tools.idea.npw.module.recipes.baselineProfilesModule
 
-import com.android.tools.idea.gradle.model.IdeBasicVariant
 import com.android.ide.common.repository.AgpVersion
+import com.android.tools.idea.gradle.model.IdeBasicVariant
 import com.android.tools.idea.gradle.project.model.GradleAndroidModel
 import com.android.tools.idea.npw.module.recipes.baselineProfilesModule.BaselineProfilesMacrobenchmarkCommon.FILTER_ARG_BASELINE_PROFILE
 import com.android.tools.idea.npw.module.recipes.baselineProfilesModule.BaselineProfilesMacrobenchmarkCommon.FILTER_ARG_MACROBENCHMARK
@@ -35,9 +35,9 @@ import com.android.tools.idea.wizard.template.RecipeExecutor
 import com.google.common.annotations.VisibleForTesting
 import com.intellij.execution.RunManager
 import com.intellij.openapi.module.Module
+import java.io.File
 import org.jetbrains.android.facet.AndroidRootUtil
 import org.jetbrains.kotlin.idea.gradleTooling.capitalize
-import java.io.File
 
 const val GMD_DEVICE = "Pixel 6"
 const val GMD_API = 34
@@ -56,24 +56,33 @@ fun RecipeExecutor.generateBaselineProfilesModule(
   targetModule: Module,
   useGmd: Boolean,
   useVersionCatalog: Boolean = true,
-  useConfigurationCaching: Boolean = true
+  useConfigurationCaching: Boolean = true,
 ) {
   val targetModuleGradleModel = GradleAndroidModel.get(targetModule) ?: return
 
-  // For agp 8.3.0-alpha10 and above, the target application id is passed as instrumentation runner argument.
+  // For agp 8.3.0-alpha10 and above, the target application id is passed as instrumentation runner
+  // argument.
   // This is because TestVariant#testedApks was introduced in that version.
-  val useInstrumentationArgumentForAppId = targetModuleGradleModel.agpVersion >= AgpVersion.parse("8.3.0-alpha10")
-  val targetApplicationId = chooseReleaseTargetApplicationId(
+  val useInstrumentationArgumentForAppId =
+    targetModuleGradleModel.agpVersion >= AgpVersion.parse("8.3.0-alpha10")
+  val targetApplicationId =
+    chooseReleaseTargetApplicationId(
       basicVariants = targetModuleGradleModel.androidProject.basicVariants,
-      defaultValue = targetModuleGradleModel.applicationId
+      defaultValue = targetModuleGradleModel.applicationId,
     )
 
-  addClasspathDependency("androidx.benchmark:benchmark-baseline-profile-gradle-plugin:+", BASELINE_PROFILES_PLUGIN_MIN_REV)
+  addClasspathDependency(
+    "androidx.benchmark:benchmark-baseline-profile-gradle-plugin:+",
+    BASELINE_PROFILES_PLUGIN_MIN_REV,
+  )
 
   val gmdSpec = if (useGmd) GmdSpec(GMD_DEVICE, GMD_API, GMD_SYSTEM_IMAGE_SOURCE) else null
 
   if (useConfigurationCaching) {
-    append("org.gradle.configuration-cache=true", newModule.projectTemplateData.rootDir.resolve("gradle.properties"))
+    append(
+      "org.gradle.configuration-cache=true",
+      newModule.projectTemplateData.rootDir.resolve("gradle.properties"),
+    )
   }
 
   val flavors = getTargetModelProductFlavors(targetModuleGradleModel)
@@ -82,14 +91,15 @@ fun RecipeExecutor.generateBaselineProfilesModule(
     newModule = newModule,
     useGradleKts = useGradleKts,
     macrobenchmarkMinRev = MACROBENCHMARK_MIN_REV,
-    buildGradleContent = baselineProfilesBuildGradle(
-      newModule = newModule,
-      flavors = flavors,
-      useGradleKts = useGradleKts,
-      targetModule = targetModule,
-      useGmd = gmdSpec,
-      useInstrumentationArgumentForAppId = useInstrumentationArgumentForAppId
-    ),
+    buildGradleContent =
+      baselineProfilesBuildGradle(
+        newModule = newModule,
+        flavors = flavors,
+        useGradleKts = useGradleKts,
+        targetModule = targetModule,
+        useGmd = gmdSpec,
+        useInstrumentationArgumentForAppId = useInstrumentationArgumentForAppId,
+      ),
     customizeModule = {
       applyPlugin("androidx.baselineprofile", BASELINE_PROFILES_PLUGIN_MIN_REV)
 
@@ -97,9 +107,9 @@ fun RecipeExecutor.generateBaselineProfilesModule(
         targetModule = targetModule,
         newModule = newModule,
         targetApplicationId = targetApplicationId,
-        useInstrumentationArgumentForAppId = useInstrumentationArgumentForAppId
+        useInstrumentationArgumentForAppId = useInstrumentationArgumentForAppId,
       )
-    }
+    },
   )
 
   updateTargetModule(newModule, targetModule)
@@ -114,8 +124,13 @@ fun RecipeExecutor.generateBaselineProfilesModule(
 fun RecipeExecutor.updateTargetModule(newModule: ModuleTemplateData, targetModule: Module) {
   val targetModuleDir = AndroidRootUtil.getModuleDirPath(targetModule)?.let { File(it) } ?: return
 
-  // This needs to be added, because many projects don't define this plugin in plugins { } block, and therefore it fails with unknown version.
-  applyPluginInModule("com.android.application", targetModule, newModule.projectTemplateData.agpVersion)
+  // This needs to be added, because many projects don't define this plugin in plugins { } block,
+  // and therefore it fails with unknown version.
+  applyPluginInModule(
+    "com.android.application",
+    targetModule,
+    newModule.projectTemplateData.agpVersion,
+  )
 
   applyPluginInModule("androidx.baselineprofile", targetModule, BASELINE_PROFILES_PLUGIN_MIN_REV)
 
@@ -123,71 +138,75 @@ fun RecipeExecutor.updateTargetModule(newModule: ModuleTemplateData, targetModul
     mavenCoordinate = "androidx.profileinstaller:profileinstaller:+",
     configuration = "implementation",
     minRev = PROFILE_INSTALLER_MIN_REV,
-    moduleDir = targetModuleDir
+    moduleDir = targetModuleDir,
   )
 
   addModuleDependency("baselineProfile", newModule.name, targetModuleDir)
 }
 
 /**
- * Add BaselineProfile generator
- * Add StartupBenchmark for measuring effectiveness
+ * - Add BaselineProfile generator
+ * - Add StartupBenchmark for measuring effectiveness
  */
 @VisibleForTesting
 fun RecipeExecutor.createTestClasses(
   targetModule: Module,
   newModule: ModuleTemplateData,
   targetApplicationId: String,
-  useInstrumentationArgumentForAppId: Boolean
+  useInstrumentationArgumentForAppId: Boolean,
 ) {
   val language = newModule.projectTemplateData.language
   val pluginTaskName = baselineProfileTaskName("release")
 
-  val (generatorContent, benchmarksContent) = when (language) {
-    Language.Kotlin -> {
-      // Create Baseline Profile Generator class
-      val generatorContent = baselineProfileGeneratorKt(
-        targetModuleName = targetModule.getModuleNameForGradleTask(),
-        pluginTaskName = pluginTaskName,
-        className = GENERATOR_CLASS_NAME,
-        packageName = newModule.packageName,
-        targetPackageName = targetApplicationId,
-        useInstrumentationArgumentForAppId = useInstrumentationArgumentForAppId
-      )
-      // Create Macrobenchmark tests
-      val benchmarksContent = baselineProfileBenchmarksKt(
-        newModuleName = newModule.name,
-        className = MACROBENCHMARKS_CLASS_NAME,
-        packageName = newModule.packageName,
-        targetPackageName = targetApplicationId,
-        useInstrumentationArgumentForAppId = useInstrumentationArgumentForAppId
-      )
+  val (generatorContent, benchmarksContent) =
+    when (language) {
+      Language.Kotlin -> {
+        // Create Baseline Profile Generator class
+        val generatorContent =
+          baselineProfileGeneratorKt(
+            targetModuleName = targetModule.getModuleNameForGradleTask(),
+            pluginTaskName = pluginTaskName,
+            className = GENERATOR_CLASS_NAME,
+            packageName = newModule.packageName,
+            targetPackageName = targetApplicationId,
+            useInstrumentationArgumentForAppId = useInstrumentationArgumentForAppId,
+          )
+        // Create Macrobenchmark tests
+        val benchmarksContent =
+          baselineProfileBenchmarksKt(
+            newModuleName = newModule.name,
+            className = MACROBENCHMARKS_CLASS_NAME,
+            packageName = newModule.packageName,
+            targetPackageName = targetApplicationId,
+            useInstrumentationArgumentForAppId = useInstrumentationArgumentForAppId,
+          )
 
-      generatorContent to benchmarksContent
+        generatorContent to benchmarksContent
+      }
+      Language.Java -> {
+        // Create Baseline Profile Generator class
+        val generatorContent =
+          baselineProfileGeneratorJava(
+            targetModuleName = targetModule.getModuleNameForGradleTask(),
+            pluginTaskName = pluginTaskName,
+            className = GENERATOR_CLASS_NAME,
+            packageName = newModule.packageName,
+            targetPackageName = targetApplicationId,
+            useInstrumentationArgumentForAppId = useInstrumentationArgumentForAppId,
+          )
+        // Create Macrobenchmark tests
+        val benchmarksContent =
+          baselineProfileBenchmarksJava(
+            newModuleName = newModule.name,
+            className = MACROBENCHMARKS_CLASS_NAME,
+            packageName = newModule.packageName,
+            targetPackageName = targetApplicationId,
+            useInstrumentationArgumentForAppId = useInstrumentationArgumentForAppId,
+          )
+
+        generatorContent to benchmarksContent
+      }
     }
-
-    Language.Java -> {
-      // Create Baseline Profile Generator class
-      val generatorContent = baselineProfileGeneratorJava(
-        targetModuleName = targetModule.getModuleNameForGradleTask(),
-        pluginTaskName = pluginTaskName,
-        className = GENERATOR_CLASS_NAME,
-        packageName = newModule.packageName,
-        targetPackageName = targetApplicationId,
-        useInstrumentationArgumentForAppId = useInstrumentationArgumentForAppId
-      )
-      // Create Macrobenchmark tests
-      val benchmarksContent = baselineProfileBenchmarksJava(
-        newModuleName = newModule.name,
-        className = MACROBENCHMARKS_CLASS_NAME,
-        packageName = newModule.packageName,
-        targetPackageName = targetApplicationId,
-        useInstrumentationArgumentForAppId = useInstrumentationArgumentForAppId
-      )
-
-      generatorContent to benchmarksContent
-    }
-  }
   // Save benchmarks + open it
   val benchmarksFile = newModule.srcDir.resolve("$BENCHMARKS_CLASS_NAME.${language.extension}")
   save(benchmarksContent, benchmarksFile)
@@ -199,26 +218,25 @@ fun RecipeExecutor.createTestClasses(
   open(generatorFile)
 }
 
-/**
- * Creates run configurations for each build flavor of the target module.
- */
+/** Creates run configurations for each build flavor of the target module. */
 @VisibleForTesting
 fun setupRunConfigurations(
   targetModule: Module,
-  runManager: RunManager = RunManager.getInstance(targetModule.project)
+  runManager: RunManager = RunManager.getInstance(targetModule.project),
 ) {
   val project = targetModule.project
 
   val configFactory = AndroidBaselineProfileRunConfigurationType.getInstance().factory
 
-  val runConfigForSelected = AndroidBaselineProfileRunConfiguration(
-    project = project,
-    factory = configFactory,
-    name = "$RUN_CONFIGURATION_NAME for ${targetModule.getModuleNameForGradleTask()}"
-  ).also {
-    it.setModule(targetModule)
-  }
-  val runConfigSettingsForSelected = runManager.createConfiguration(runConfigForSelected, configFactory)
+  val runConfigForSelected =
+    AndroidBaselineProfileRunConfiguration(
+        project = project,
+        factory = configFactory,
+        name = "$RUN_CONFIGURATION_NAME for ${targetModule.getModuleNameForGradleTask()}",
+      )
+      .also { it.setModule(targetModule) }
+  val runConfigSettingsForSelected =
+    runManager.createConfiguration(runConfigForSelected, configFactory)
   // Persists in .idea folder
   runConfigSettingsForSelected.storeInDotIdeaFolder()
   runManager.addConfiguration(runConfigSettingsForSelected)
@@ -230,34 +248,33 @@ fun Module.getModuleNameForGradleTask(): String {
   // name of the whole project replacing spaces, because that's not allowed for Gradle modules
   val projectName = project.name.replace(' ', '_')
 
-  // module name contains also the whole project name, so we need to remove it, so we can use it to a Gradle task
+  // module name contains also the whole project name,
+  // so we need to remove it, so we can use it to a Gradle task
   return name.removePrefix("$projectName.")
 }
 
-/**
- * [filterArgument] can be one of [FILTER_ARG_BASELINE_PROFILE], [FILTER_ARG_MACROBENCHMARK]
- */
+/** [filterArgument] can be one of [FILTER_ARG_BASELINE_PROFILE], [FILTER_ARG_MACROBENCHMARK] */
 @VisibleForTesting
-fun runConfigurationGradleTask(
-  moduleName: String,
-  flavorName: String?,
-  filterArgument: String?,
-) = buildString {
-  append(":${moduleName}:")
-  append(baselineProfileTaskName(flavorName))
-  // Allows running only Baseline Profile generators (in case Macrobenchmarks are in the same module)
-  if (filterArgument != null) {
-    append(" -P${BaselineProfilesMacrobenchmarkCommon.FILTER_INSTR_ARG}=$filterArgument")
+fun runConfigurationGradleTask(moduleName: String, flavorName: String?, filterArgument: String?) =
+  buildString {
+    append(":${moduleName}:")
+    append(baselineProfileTaskName(flavorName))
+    // Allows running only Baseline Profile generators (in case Macrobenchmarks are in the same
+    // module)
+    if (filterArgument != null) {
+      append(" -P${BaselineProfilesMacrobenchmarkCommon.FILTER_INSTR_ARG}=$filterArgument")
+    }
   }
-}
 
 @VisibleForTesting
-fun chooseReleaseTargetApplicationId(basicVariants: Collection<IdeBasicVariant>, defaultValue: String) =
+fun chooseReleaseTargetApplicationId(
+  basicVariants: Collection<IdeBasicVariant>,
+  defaultValue: String,
+) =
   basicVariants
     .sortedBy { it.name }
     .firstOrNull { it.name.endsWith("Release") || it.name == "release" }
-    ?.applicationId
-  ?: defaultValue
+    ?.applicationId ?: defaultValue
 
 @VisibleForTesting
 fun baselineProfileTaskName(variantName: String?): String =
