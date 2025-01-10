e5c7db7124f901224907ca099df6b0088e4594af
Alex Vanyo <vanyo@google.com>
2025-01-10 00:14:49 +0000

    Add Compose navigation UI template to renderer
    Bug: 359910572
    Test: Updated TemplateDiffTest in linked CL
    
    Change-Id: I8fa74cb758bd43393c432b87c0021342ece117d9
    
    GitOrigin-RevId: f6833c260a7ac983bdcf8d5e95b4b78b17111181

--- android-npw/src/com/android/tools/idea/npw/model/NewTemplateRenderer.kt
+++ android-npw/src/com/android/tools/idea/npw/model/NewTemplateRenderer.kt
@@ -253,6 +253,7 @@ fun titleToTemplateRenderer(title: String, formFactor: FormFactor): TemplateRend
     "Values XML File" -> TemplateRenderer.VALUES_XML_FILE
     "Gemini API Starter" -> TemplateRenderer.EXPERIMENTAL_ACTIVITY
     "Basic Headset Activity" -> TemplateRenderer.EXPERIMENTAL_ACTIVITY_2
+    "Navigation UI Activity" -> TemplateRenderer.COMPOSE_NAVIGATION_UI_ACTIVITY
     else -> TemplateRenderer.CUSTOM_TEMPLATE_RENDERER
   }
 
@@ -325,6 +326,7 @@ fun titleToTemplateType(title: String, formFactor: FormFactor): TemplateType {
     TemplateRenderer.GOOGLE_WALLET_ACTIVITY -> TemplateType.GOOGLE_WALLET_ACTIVITY
     TemplateRenderer.EXPERIMENTAL_ACTIVITY -> TemplateType.EXPERIMENTAL_ACTIVITY
     TemplateRenderer.EXPERIMENTAL_ACTIVITY_2 -> TemplateType.EXPERIMENTAL_ACTIVITY_2
+    TemplateRenderer.COMPOSE_NAVIGATION_UI_ACTIVITY -> TemplateType.COMPOSE_NAVIGATION_UI_ACTIVITY
     TemplateRenderer.BLANK_ACTIVITY,
     TemplateRenderer.ANDROID_MODULE,
     TemplateRenderer.ANDROID_PROJECT,
