474b758410ec35fbceae767e3504a58115d3e86f
Alex Golubev <alexgolubev@google.com>
2025-01-12 17:35:00 -0500

    Create  DeclarativeAndroidGradleProjectRule
    To have flags enabled and usage of new Gradle snapshot
    
    Bug: NA
    Test: NA
    Change-Id: If1b92bf06d1804658f4e498c7aa0c01fed8a917a
    
    GitOrigin-RevId: 5f9b65af09db717b4ef261befb6df1a124308823

---
+++ project-system-gradle/testSrc/com/android/tools/idea/testing/DeclarativeAndroidGradleProjectRule.kt
@@ -0,0 +1,72 @@
+/*
+ * Copyright (C) 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.testing
+
+import com.android.testutils.TestUtils
+import com.android.tools.idea.flags.DeclarativeStudioSupport
+import com.android.tools.idea.gradle.dcl.lang.ide.DeclarativeIdeSupport
+import com.android.tools.idea.gradle.util.GradleWrapper
+import com.intellij.openapi.command.WriteCommandAction
+import com.intellij.openapi.project.Project
+import com.intellij.testFramework.fixtures.CodeInsightTestFixture
+import org.junit.runner.Description
+import java.io.File
+
+class DeclarativeAndroidGradleProjectRule(val projectRule: AndroidGradleProjectRule) :
+  NamedExternalResource() {
+  val project: Project
+    get() = projectRule.project
+
+  val fixture: CodeInsightTestFixture
+    get() = projectRule.fixture
+
+  override fun before(description: Description) {
+    projectRule.before(description)
+    DeclarativeStudioSupport.override(true)
+    DeclarativeIdeSupport.override(true)
+  }
+
+  override fun after(description: Description) {
+    projectRule.after(description)
+    DeclarativeStudioSupport.clearOverride()
+    DeclarativeIdeSupport.clearOverride()
+  }
+
+  @JvmOverloads
+  fun loadProject(
+    projectPath: String,
+    chosenModuleName: String? = null,
+    agpVersion: AgpVersionSoftwareEnvironmentDescriptor =
+      AgpVersionSoftwareEnvironmentDescriptor.AGP_CURRENT,
+    ndkVersion: String? = null,
+    preLoad: ((projectRoot: File) -> Unit)? = null
+  ) = projectRule.loadProject(projectPath, chosenModuleName, agpVersion, ndkVersion) { projectRoot ->
+    WriteCommandAction.runWriteCommandAction(project) {
+      setupGradleSnapshotToWrapper(project)
+    }
+    preLoad?.invoke(projectRoot)
+  }
+
+  private fun setupGradleSnapshotToWrapper(project: Project) {
+    val distribution = TestUtils.resolveWorkspacePath("tools/external/gradle")
+    val gradle = distribution.resolve("gradle-8.12-20241105002153+0000-bin.zip")
+    val wrapper = GradleWrapper.find(project)!!
+    wrapper.updateDistributionUrl(gradle.toFile())
+  }
+}
+
+fun AndroidGradleProjectRule.withDeclarative(): DeclarativeAndroidGradleProjectRule =
+  DeclarativeAndroidGradleProjectRule(this)
