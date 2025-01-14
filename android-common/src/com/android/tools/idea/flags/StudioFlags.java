985b402dd97d9c89521889448f7e520cb1bf327d
Hannah Chu <hanchu@google.com>
2025-01-14 10:16:11 -0800

    Enable KMP module template StudioFlag
    Bug: 359626184
    Test: N/A
    Change-Id: I8c9d34da75be73566cc0dcb8af4c904f35679fcf
    
    GitOrigin-RevId: 37358218c00cc7e0c3be7e1462ee259943a015cb

--- android-common/src/com/android/tools/idea/flags/StudioFlags.java
+++ android-common/src/com/android/tools/idea/flags/StudioFlags.java
@@ -182,7 +182,7 @@ public final class StudioFlags {
   public static final Flag<Boolean> NPW_NEW_KOTLIN_MULTIPLATFORM_MODULE = new BooleanFlag(
     NPW, "new.kotlin.multiplatform.module", "New Kotlin Multiplatform Module",
     "Show template to create a new Kotlin Multiplatform module in the new module wizard.",
-    enabledUpTo(CANARY));
+    true);
 
   public static final Flag<Integer> NPW_COMPILE_SDK_VERSION = new IntFlag(
     NPW, "new.project.compile.sdk", "New project Compile SDK version",
