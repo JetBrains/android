ae704b55f32b027ca3f20a51899c7e7ebe32961d
Kajus Kuncaitis <Kajus.Kuncaitis@jetbrains.com>
2025-01-21 13:04:51 +0000

    [gradle][IDEA-365458] Added an interface
    GitOrigin-RevId: d1c4bb3f6138106c2f1cd472272d81852badf177
    (cherry picked from commit c6ddc3d8df20d041d5a2d2f0818a9ebd39cef172)
    Change-Id: I3300970ba3d6385fc366edbfd45d02de64931c73
    
    Space-RevId: ae6e4239175976b026df0df8339513d674eec212
    (cherry picked from commit f485480654e8ca58f516d6d1895bb6b3c30a8fed)

--- gradle-dsl/src/com/android/tools/idea/gradle/dsl/api/GradleBuildModel.java
+++ gradle-dsl/src/com/android/tools/idea/gradle/dsl/api/GradleBuildModel.java
@@ -24,7 +24,6 @@ import com.android.tools.idea.gradle.dsl.api.kotlin.KotlinModel;
 import com.android.tools.idea.gradle.dsl.api.repositories.RepositoriesModel;
 import com.android.tools.idea.gradle.dsl.api.util.GradleDslContextModel;
 import com.android.tools.idea.gradle.dsl.api.util.GradleDslModel;
-import com.android.tools.idea.gradle.dsl.model.java.JavaDeclarativeModelImpl;
 import com.intellij.openapi.diagnostic.ControlFlowException;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.module.Module;
