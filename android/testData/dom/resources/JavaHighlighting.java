df3e20d00a53a66ee07ef34b0cde9219cce08333
Tagir Valeev <tagir.valeev@jetbrains.com>
2025-01-29 10:52:08 +0100

    [android] error anchors adjusted in testdata after recent updates of Java highlighter (see IDEA-365344)
    GitOrigin-RevId: f0d3cd8ed8293bf367c8afccf3cbf39eb838dde3

--- android/testData/dom/resources/JavaHighlighting.java
+++ android/testData/dom/resources/JavaHighlighting.java
@@ -8,7 +8,7 @@ public class JavaCompletion1 {
   public void f() {
     int n1 = R.string.my_string;
     int n2 = R.string.<error>unknown</error>;
-    <error>int n3 = R.styleable.MyStyleable;</error>
+    int n3 = R.styleable.<error>MyStyleable</error>;
     int[] n4 = R.styleable.MyStyleable;
     int n5 = R.attr.myAttr1;
     n5 = R.attr.myAttr2;
