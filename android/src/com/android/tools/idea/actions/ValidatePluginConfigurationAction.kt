bc442ef05efd917f00595bfe579b330fa5414bdd
Matthew Gharrity <gharrma@google.com>
2025-02-21 12:27:13 -0800

    ValidatePluginConfiguration: handle v2 modules
    Since PluginManagerCore.getNonOptionalDependenciesIds() does not
    handle v2 module dependencies correctly, we need to do it ourselves.
    
    Main idea: traverse v2 module edges recursively until we reach actual
    plugins (including the host plugins of the v2 modules themselves).
    
    Fixes: 398240000
    Test: StartUpTest + manual testing with Change I60abac608
    Change-Id: I29300851087e863e3d0a1c3a4fb39e0d27374ef9
    
    GitOrigin-RevId: d146e74215e4cd62c712cef30cdde45e00db257a

--- android/src/com/android/tools/idea/actions/ValidatePluginConfigurationAction.kt
+++ android/src/com/android/tools/idea/actions/ValidatePluginConfigurationAction.kt
@@ -15,15 +15,17 @@
  */
 package com.android.tools.idea.actions
 
-import com.intellij.ide.plugins.PluginManagerCore.findPlugin
-import com.intellij.ide.plugins.PluginManagerCore.getNonOptionalDependenciesIds
+import com.intellij.ide.plugins.IdeaPluginDescriptorImpl
+import com.intellij.ide.plugins.PluginManagerCore
 import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.application.ex.ApplicationInfoEx
 import com.intellij.openapi.diagnostic.thisLogger
+import com.intellij.openapi.extensions.PluginId
 
 /** Validates IDE plugins and their dependencies (for use in E2E tests). */
+@Suppress("UnstableApiUsage")
 class ValidatePluginConfigurationAction : AnAction() {
 
   override fun actionPerformed(e: AnActionEvent) {
@@ -31,7 +33,6 @@ class ValidatePluginConfigurationAction : AnAction() {
     thisLogger().info("Done validating plugins")
   }
 
-  @Suppress("UnstableApiUsage")
   private fun validateEssentialPlugins() {
     // Until JetBrains fixes https://youtrack.jetbrains.com/issue/IJPL-6075 upstream, we want to ensure all Android plugin
     // dependencies are marked 'essential' so they cannot be disabled (see b/202048599, b/365493089). If this assertion fails,
@@ -40,13 +41,9 @@ class ValidatePluginConfigurationAction : AnAction() {
     val essentialPlugins = appInfoEx.essentialPluginIds.toSet()
     val errors = mutableListOf<String>()
     for (plugin in essentialPlugins) {
-      val descriptor = findPlugin(plugin) ?: error("Failed to find descriptor for essential plugin: $plugin")
-      for (dependency in getNonOptionalDependenciesIds(descriptor)) {
-        // No need to worry about V1 modules of the form "com.intellij.modules.*". These are not true plugins; they cannot be disabled.
-        val dependencyDescriptor = checkNotNull(findPlugin(dependency)) { "Failed to find plugin descriptor for: $dependency" }
-        val isV1Module = dependency.idString.startsWith("com.intellij.modules.") && dependency != dependencyDescriptor.pluginId
-        if (dependency !in essentialPlugins && !isV1Module) {
-          errors.add("Essential plugin '$plugin' depends on non-essential plugin '$dependency'")
+      for (dependency in getRequiredPluginDependencies(plugin)) {
+        if (dependency !in essentialPlugins && dependency != PluginManagerCore.CORE_ID) {
+          errors.add("Essential plugin '$plugin' requires non-essential plugin '$dependency'")
         }
       }
     }
@@ -54,6 +51,51 @@ class ValidatePluginConfigurationAction : AnAction() {
       val msg = "The essential plugins in AndroidStudioApplicationInfo.xml do not form a transitive closure:\n" + errors.joinToString("\n")
       throw AssertionError(msg)
     }
+  }
+
+  /**
+   * Returns the required plugin dependencies of [plugin], including those implied by v2 module dependencies
+   * (unlike [PluginManagerCore.getNonOptionalDependenciesIds], which ignores v2 module edges).
+   */
+  private fun getRequiredPluginDependencies(plugin: PluginId): Collection<PluginId> {
+    val pluginSet = PluginManagerCore.getPluginSet()
+    val plugins = mutableSetOf<PluginId>()
+    val modules = mutableSetOf<String>()
+
+    // Using 'canonical' plugin IDs ensures we handle v1 modules correctly.
+    fun getCanonicalPluginId(id: PluginId): PluginId = checkNotNull(pluginSet.findEnabledPlugin(id)).pluginId
+
+    fun collectDependencies(descriptor: IdeaPluginDescriptorImpl) {
+      // v1 dependencies.
+      for (dep in descriptor.pluginDependencies) {
+        if (!dep.isOptional) {
+          plugins.add(getCanonicalPluginId(dep.pluginId))
+        }
+      }
+      // v2 dependencies.
+      for (dep in descriptor.dependencies.plugins) {
+        plugins.add(getCanonicalPluginId(dep.id))
+      }
+      for (dep in descriptor.dependencies.modules) {
+        val moduleDescriptor = checkNotNull(pluginSet.findEnabledModule(dep.name))
+        if (modules.add(dep.name)) {
+          // Traverse v2 module edges recursively (until we reach actual plugins).
+          collectDependencies(moduleDescriptor)
+        }
+        // Add an implicit dependency on the v2 module 'host' plugin.
+        plugins.add(getCanonicalPluginId(moduleDescriptor.pluginId))
+      }
+    }
+
+    val descriptor = checkNotNull(pluginSet.findEnabledPlugin(plugin))
+    collectDependencies(descriptor)
+
+    val baseline = PluginManagerCore.getNonOptionalDependenciesIds(descriptor).map(::getCanonicalPluginId)
+    check(plugins.containsAll(baseline)) {
+      "The result of getRequiredPluginDependencies() appears to be inconsistent with getNonOptionalDependenciesIds() from IntelliJ"
+    }
+
+    return plugins
   }
 
   override fun getActionUpdateThread(): ActionUpdateThread = ActionUpdateThread.BGT
