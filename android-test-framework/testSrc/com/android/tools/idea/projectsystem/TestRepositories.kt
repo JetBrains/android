c29521b1cbe9eb5aba90d45ba02f1e56fef2341c
Christophe Rhodes <xof@google.com>
2025-02-24 16:46:08 +0000

    Explicitly list a set of libraries for PLATFORM_SUPPORT_LIBS
    Bug: none filed
    Test: existing
    Change-Id: I3c5224468a32725857b41b1ba894bf24b717c732
    
    GitOrigin-RevId: af588730987d5ed3006d2135b2319967319f2aae

--- android-test-framework/testSrc/com/android/tools/idea/projectsystem/TestRepositories.kt
+++ android-test-framework/testSrc/com/android/tools/idea/projectsystem/TestRepositories.kt
@@ -17,17 +17,46 @@
 
 package com.android.tools.idea.projectsystem
 
-import com.android.ide.common.repository.GoogleMavenArtifactId
 import com.android.ide.common.repository.GoogleMavenArtifactId.*
 
 /**
  * A collection of repos for dependencies used during testing.
  */
 @JvmField
-val PLATFORM_SUPPORT_LIBS = GoogleMavenArtifactId.values()
-  .filter { it.isPlatformSupportLibrary }
-  .map { it.getCoordinate("+") }
-
+val PLATFORM_SUPPORT_LIBS =
+  // This list isn't particularly principled, but instead reflects the state of GoogleMavenArtifactId at the point that
+  // that class stopped keeping track of whether (by some unknown metric) the library was a platform support library.
+  // This list should probably be viewed as a tasteful set of available libraries for testing dependency management in
+  // both original and androidx projects.
+  listOf(
+    SUPPORT_ANNOTATIONS,
+    ANDROIDX_SUPPORT_ANNOTATIONS,
+    SUPPORT_V4,
+    ANDROIDX_SUPPORT_V4,
+    SUPPORT_V13,
+    ANDROIDX_SUPPORT_V13,
+    APP_COMPAT_V7,
+    ANDROIDX_APP_COMPAT_V7,
+    SUPPORT_VECTOR_DRAWABLE,
+    ANDROIDX_SUPPORT_VECTOR_DRAWABLE,
+    DESIGN,
+    ANDROIDX_DESIGN,
+    GRID_LAYOUT_V7,
+    ANDROIDX_GRID_LAYOUT_V7,
+    MEDIA_ROUTER_V7,
+    ANDROIDX_MEDIA_ROUTER_V7,
+    CARDVIEW_V7,
+    ANDROIDX_CARDVIEW_V7,
+    PALETTE_V7,
+    ANDROIDX_PALETTE_V7,
+    LEANBACK_V17,
+    ANDROIDX_LEANBACK_V17,
+    RECYCLERVIEW_V7,
+    ANDROIDX_RECYCLERVIEW_V7,
+    EXIF_INTERFACE,
+    ANDROIDX_EXIF_INTERFACE,
+    ANDROIDX_PREFERENCE,
+  ).map { it.getCoordinate("+") }
 @JvmField
 val NON_PLATFORM_SUPPORT_LAYOUT_LIBS = listOf(CONSTRAINT_LAYOUT, ANDROIDX_CONSTRAINT_LAYOUT, FLEXBOX_LAYOUT)
   .map { it.getCoordinate("+") }
