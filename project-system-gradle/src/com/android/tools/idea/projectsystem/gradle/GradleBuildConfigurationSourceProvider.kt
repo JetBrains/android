6328cc613b0044691b921eb0cd5e7a35edc333a4
Christophe Rhodes <xof@google.com>
2025-02-25 16:20:43 +0000

    In the Android view, use external project data for version catalogs
    Since the external project data tells us what files Gradle itself
    considered to be version catalogs, use that rather than filesystem
    interrogation.
    
    This means we can't test version catalog files showing up without
    running a full sync: the module/project builders aren't of high-enough
    fidelity.  (We could in principle extract this information into our
    own models and indirect that way, but for now simply not testing the
    version catalog nodes in the unit-ish test is probably OK, as it's
    covered by snapshots).
    
    Bug: 271914036
    Test: manual, adjusted, snapshot updated
    Change-Id: I05270769a1108940170889bd44a4cfe3eb217b64
    
    GitOrigin-RevId: bbef278fb023ef03b66de28be4dc47267a54e2f5

--- project-system-gradle/src/com/android/tools/idea/projectsystem/gradle/GradleBuildConfigurationSourceProvider.kt
+++ project-system-gradle/src/com/android/tools/idea/projectsystem/gradle/GradleBuildConfigurationSourceProvider.kt
@@ -30,6 +30,7 @@ import com.intellij.openapi.vfs.VfsUtil
 import com.intellij.openapi.vfs.VirtualFile
 import org.jetbrains.android.facet.AndroidRootUtil
 import org.jetbrains.kotlin.utils.yieldIfNotNull
+import org.jetbrains.plugins.gradle.service.resolve.getVersionCatalogFiles
 import java.util.Comparator
 
 private const val BUILD_ORDER_BASE = 1_000_000
@@ -156,11 +157,10 @@ class GradleBuildConfigurationSourceProvider(private val project: Project) : Bui
         )
       }
 
-      projectRootFolder.findChild("gradle")?.takeIf { it.isDirectory }?.let { gradle ->
-        gradle.children.filter { !it.isDirectory && it.name.endsWith(SdkConstants.DOT_VERSIONS_DOT_TOML) }.forEach {
-          yield(it.describe("Version Catalog", BUILD_WIDE_ORDER_BASE))
-        }
+      ModuleManager.getInstance(project).modules.map { getVersionCatalogFiles(it) }.flatMap { it.entries }.toSet().forEach {
+        yield(it.value.describe("Version Catalog \"${it.key}\"", BUILD_WIDE_ORDER_BASE))
       }
+
       yieldIfNotNull(
         projectRootFolder.findChild(SdkConstants.FN_LOCAL_PROPERTIES)
           ?.describe("SDK Location", BUILD_WIDE_ORDER_BASE)
