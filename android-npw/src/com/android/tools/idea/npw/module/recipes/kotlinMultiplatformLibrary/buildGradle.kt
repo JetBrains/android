602f86c2fc6729bea75ee2ee8fc210b9749cfe1b
Hannah Chu <hanchu@google.com>
2025-02-26 16:03:39 -0800

    Fix comment indentation in generated build.gradle
    If the comment already starts at the beginning of the line, the auto
    indentation doesn't indent it.
    
    Bug: N/A
    Test: KotlinMultiplatformModuleTest
    Change-Id: I2416e0cb38a3a87ee1f094e3648d02837d530748
    
    GitOrigin-RevId: abbece57c1216c295299cb3dfa8f1a937c9c31fa

--- android-npw/src/com/android/tools/idea/npw/module/recipes/kotlinMultiplatformLibrary/buildGradle.kt
+++ android-npw/src/com/android/tools/idea/npw/module/recipes/kotlinMultiplatformLibrary/buildGradle.kt
@@ -39,11 +39,11 @@ fun buildKmpGradle(
 
   val sourceSetConfigurationsBlock =
     """
-    // Source set declarations.
-    // Declaring a target automatically creates a source set with the same name. By default, the
-    // Kotlin Gradle Plugin creates additional source sets that depend on each other, since it is
-    // common to share sources between related targets.
-    // See: https://kotlinlang.org/docs/multiplatform-hierarchy.html
+      // Source set declarations.
+      // Declaring a target automatically creates a source set with the same name. By default, the
+      // Kotlin Gradle Plugin creates additional source sets that depend on each other, since it is
+      // common to share sources between related targets.
+      // See: https://kotlinlang.org/docs/multiplatform-hierarchy.html
     sourceSets {
       commonMain {
         dependencies {
@@ -107,9 +107,9 @@ private fun androidTargetConfig(
   minApi: String,
 ): String {
   return """
-    // Target declarations - add or remove as needed below. These define
-    // which platforms this KMP module supports.
-    // See: https://kotlinlang.org/docs/multiplatform-discover-project.html#targets
+      // Target declarations - add or remove as needed below. These define
+      // which platforms this KMP module supports.
+      // See: https://kotlinlang.org/docs/multiplatform-discover-project.html#targets
     androidLibrary {
       namespace '$packageName'
       ${toAndroidFieldVersion("compileSdk", compileApiString, agpVersion)}
@@ -129,13 +129,13 @@ private fun androidTargetConfig(
 
 private fun iosTargetConfig(name: String): String {
   return """
-    // For iOS targets, this is also where you should
-    // configure native binary output. For more information, see:
-    // https://kotlinlang.org/docs/multiplatform-build-native-binaries.html#build-xcframeworks
+      // For iOS targets, this is also where you should
+      // configure native binary output. For more information, see:
+      // https://kotlinlang.org/docs/multiplatform-build-native-binaries.html#build-xcframeworks
 
-    // A step-by-step guide on how to include this library in an XCode
-    // project can be found here:
-    // https://developer.android.com/kotlin/multiplatform/migrate
+      // A step-by-step guide on how to include this library in an XCode
+      // project can be found here:
+      // https://developer.android.com/kotlin/multiplatform/migrate
     val xcfName = "${name}Kit"
 
     iosX64 {
--- android-npw/testSrc/com/android/tools/idea/npw/module/KotlinMultiplatformModuleTest.kt
+++ android-npw/testSrc/com/android/tools/idea/npw/module/KotlinMultiplatformModuleTest.kt
@@ -198,9 +198,9 @@ plugins {
 
     kotlin {
 
-// Target declarations - add or remove as needed below. These define
-// which platforms this KMP module supports.
-// See: https://kotlinlang.org/docs/multiplatform-discover-project.html#targets
+  // Target declarations - add or remove as needed below. These define
+  // which platforms this KMP module supports.
+  // See: https://kotlinlang.org/docs/multiplatform-discover-project.html#targets
 androidLibrary {
   namespace = "com.kmplib.packagename"
   compileSdk = ${StudioFlags.NPW_COMPILE_SDK_VERSION.get()}
@@ -216,13 +216,13 @@ androidLibrary {
   }
 }
 
-// For iOS targets, this is also where you should
-// configure native binary output. For more information, see:
-// https://kotlinlang.org/docs/multiplatform-build-native-binaries.html#build-xcframeworks
+  // For iOS targets, this is also where you should
+  // configure native binary output. For more information, see:
+  // https://kotlinlang.org/docs/multiplatform-build-native-binaries.html#build-xcframeworks
 
-// A step-by-step guide on how to include this library in an XCode
-// project can be found here:
-// https://developer.android.com/kotlin/multiplatform/migrate
+  // A step-by-step guide on how to include this library in an XCode
+  // project can be found here:
+  // https://developer.android.com/kotlin/multiplatform/migrate
 val xcfName = "sharedKit"
 
 iosX64 {
@@ -243,11 +243,11 @@ iosSimulatorArm64 {
   }
 }
 
-// Source set declarations.
-// Declaring a target automatically creates a source set with the same name. By default, the
-// Kotlin Gradle Plugin creates additional source sets that depend on each other, since it is
-// common to share sources between related targets.
-// See: https://kotlinlang.org/docs/multiplatform-hierarchy.html
+  // Source set declarations.
+  // Declaring a target automatically creates a source set with the same name. By default, the
+  // Kotlin Gradle Plugin creates additional source sets that depend on each other, since it is
+  // common to share sources between related targets.
+  // See: https://kotlinlang.org/docs/multiplatform-hierarchy.html
 sourceSets {
   commonMain {
     dependencies {
