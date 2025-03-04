3b73a7551c0e9b93af48167d2099203fa448ec17
Luis Santiago Re <lsantire@google.com>
2025-03-04 21:40:23 +0000

    Create toolbar in sceneViewTopPanel only when needed
    This means that the toolbar won't be created if there are no actions
    to populate the pop up menu action of the toolbar.
    
    Bug: 387944407
    Test: SceneViewPeerPanelTest
    Change-Id: I0050d6e09a6ab3fa1b10215855fa1aeb523d833b
    
    GitOrigin-RevId: 2ca5278f76cf77f777e67f01b8a223c56173d582

--- designer/src/com/android/tools/idea/common/surface/sceneview/SceneViewTopPanel.kt
+++ designer/src/com/android/tools/idea/common/surface/sceneview/SceneViewTopPanel.kt
@@ -66,11 +66,14 @@ class SceneViewTopPanel(
       statusIcon.isVisible = true
     }
     add(labelPanel, BorderLayout.CENTER)
-    val toolbarActionGroup = DefaultActionGroup(toolbarActions)
     val toolbar =
-      createToolbar(listOf(ShowActionGroupInPopupAction(toolbarActionGroup)))?.also {
-        add(it, BorderLayout.LINE_END)
-      }
+      toolbarActions
+        .takeIf { it.isNotEmpty() }
+        ?.let {
+          createToolbar(listOf(ShowActionGroupInPopupAction(DefaultActionGroup(it))))?.also {
+            add(it, BorderLayout.LINE_END)
+          }
+        }
     // The space of name label is sacrificed when there is no enough width to display the toolbar.
     // When it happens, the label will be trimmed and show the ellipsis at its tail.
     // User can still hover it to see the full label in the tooltips.
