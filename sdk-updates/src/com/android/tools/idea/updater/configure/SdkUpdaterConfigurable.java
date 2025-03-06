21268080392c8a1ef2b23f778a821081a2e45863
James Rose <rosej@google.com>
2025-03-06 15:40:03 -0800

    Remove unnecessary synchronous loads
    Some of these calls occurred very frequently (on every UI tree node).
    Most importantly, the synchronous call immediately after an asynchronous
    call on StudioProgressRunner leads to a hang.
    
    Bug: 401342995
    Test: verified changing the SDK path works
    Change-Id: I81e3cdf043442d0ee059bd691207f0b05a3df975
    
    GitOrigin-RevId: 600383d691f878eb164c7755a466fd8add665c66

--- sdk-updates/src/com/android/tools/idea/updater/configure/SdkUpdaterConfigurable.java
+++ sdk-updates/src/com/android/tools/idea/updater/configure/SdkUpdaterConfigurable.java
@@ -124,7 +124,7 @@ public class SdkUpdaterConfigurable implements SearchableConfigurable {
   }
 
   RepoManager getRepoManager() {
-    return getSdkHandler().getRepoManagerAndLoadSynchronously(new StudioLoggerProgressIndicator(getClass()));
+    return getSdkHandler().getRepoManager(new StudioLoggerProgressIndicator(getClass()));
   }
 
   @Override
