38286ee7cf662df7dab69cf14c1faed87a3954cd
Yuriy Solodkyy <solodkyy@google.com>
2025-03-14 14:33:11 +0000

    Regenerate workspace model metadata files
    Bug: 374810387
    Test: n/a
    Change-Id: I7ae743f28e5f72d8005a43aea6d32a2253a0467a
    
    GitOrigin-RevId: df07152c966d72fcc5c131f7d3d3e4ba754acf6b

--- aswb/base/src/com/google/idea/blaze/base/qsync/entity/BazelEntitySource.kt
+++ aswb/base/src/com/google/idea/blaze/base/qsync/entity/BazelEntitySource.kt
@@ -15,23 +15,12 @@
  */
 package com.google.idea.blaze.base.qsync.entity
 
-import com.google.idea.blaze.base.sync.data.BlazeDataStorage.WORKSPACE_MODULE_NAME
-import com.google.idea.blaze.base.util.UrlUtil
-import com.google.idea.blaze.qsync.project.ProjectPath
-import com.google.idea.blaze.qsync.project.ProjectProto
-import com.intellij.openapi.vfs.VirtualFileManager
-import com.intellij.platform.workspace.jps.entities.DependencyScope
-import com.intellij.platform.workspace.jps.entities.InheritedSdkDependency
-import com.intellij.platform.workspace.jps.entities.LibraryDependency
-import com.intellij.platform.workspace.jps.entities.LibraryEntity
-import com.intellij.platform.workspace.jps.entities.LibraryRoot
-import com.intellij.platform.workspace.jps.entities.LibraryRootTypeId
-import com.intellij.platform.workspace.jps.entities.LibraryTableId
-import com.intellij.platform.workspace.jps.entities.ModuleEntity
-import com.intellij.platform.workspace.jps.entities.ModuleSourceDependency
 import com.intellij.platform.workspace.storage.EntitySource
-import com.intellij.platform.workspace.storage.MutableEntityStorage
-import com.intellij.platform.workspace.storage.url.VirtualFileUrlManager
-import java.nio.file.Path
 
 object BazelEntitySource : EntitySource
+
+@Suppress("unused")
+/**
+ * Workspace model code generation is not triggered unless there is at least one class in the file.
+ */
+class AnchorForCodeGeneration
