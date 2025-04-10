745121435f11cf1d8db679b8067e4553ecbb0221
Suryanarayana Mudunuri <smudunuri@google.com>
2025-04-09 21:33:58 -0700

    UI Test: Update compose preview test to use sdk 36
    Use complete sdk 36 for compose preview tests.
    
    Bug: 408057613
    Test: This is a test
    Change-Id: Ie92702cabddf015e339afc8cdb64ece0b2feecce
    
    GitOrigin-RevId: 62187e688ba98ccfbe389b754c154a238649478f

--- android-uitests/testSrc/com/android/tools/idea/tests/gui/compose/ComposePreviewTest.kt
+++ android-uitests/testSrc/com/android/tools/idea/tests/gui/compose/ComposePreviewTest.kt
@@ -52,11 +52,11 @@ import org.junit.runner.RunWith
 
 @RunWith(GuiTestRemoteRunner::class)
 class ComposePreviewTest {
-  @get:Rule val guiTest = ComposeDesignerTestDataRule().withTimeout(10, TimeUnit.MINUTES)
+  @get:Rule val guiTest = ComposeDesignerTestDataRule().withTimeout(15, TimeUnit.MINUTES)
 
   @get:Rule val renderTaskLeakCheckRule = RenderTaskLeakCheckRule()
 
-  @get:Rule val adbRule: FakeAdbTestRule = FakeAdbTestRule("35")
+  @get:Rule val adbRule: FakeAdbTestRule = FakeAdbTestRule("36")
 
   private fun openComposePreview(
     fixture: IdeFrameFixture,
