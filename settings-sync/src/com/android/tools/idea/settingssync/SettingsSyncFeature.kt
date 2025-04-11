fbb8d171a608b08916c7699d6cb56fe061cd6d97
Kun Shen <kunshen@google.com>
2025-04-11 21:52:18 +0000

    [settings sync] Do not open a new settings dialog
    ...when clicking "Go to Backup and Sync".
    
    Bug: n/a
    Test: manual
    Change-Id: Idfa15cd70f5d5085e1e1ef13b744ba01bfa78406
    
    GitOrigin-RevId: eedd292c49979b66113cee75ad1fd95c7edb9ea4

--- settings-sync/src/com/android/tools/idea/settingssync/SettingsSyncFeature.kt
+++ settings-sync/src/com/android/tools/idea/settingssync/SettingsSyncFeature.kt
@@ -26,8 +26,7 @@ import com.google.gct.login2.LoginFeature
 import com.google.gct.wizard.WizardPage
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
-import com.intellij.openapi.options.ShowSettingsUtil
-import com.intellij.settingsSync.core.SettingsSyncBundle
+import com.intellij.openapi.options.ex.Settings
 import icons.StudioIllustrations
 import icons.StudioIllustrationsCompose
 import javax.swing.Icon
@@ -47,8 +46,8 @@ class SettingsSyncFeature : LoginFeature {
   override val settingsAction: AnAction =
     object : AnAction("Go to Backup and Sync") {
       override fun actionPerformed(e: AnActionEvent) {
-        ShowSettingsUtil.getInstance()
-          .showSettingsDialog(e.project, SettingsSyncBundle.message("title.settings.sync"))
+        val settings = e.getData(Settings.KEY)
+        settings?.select(settings.find("settings.sync"))
       }
     }
 
