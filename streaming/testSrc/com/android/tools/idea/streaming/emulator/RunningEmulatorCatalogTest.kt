62ac9f5f3280317de77359697a09b830f79238ba
Sergey Prigogin <sprigogin@google.com>
2025-04-14 09:39:30 -0700

    Increase timeout to reduce test flakiness
    Bug: 409854551
    Test: adjusted
    Change-Id: I364fceb9425893db6324b2351359bc9b8d2b66f0
    
    GitOrigin-RevId: bdf992c7bf8f950e200a41f37d0bfcc0dbb1fb76

--- streaming/testSrc/com/android/tools/idea/streaming/emulator/RunningEmulatorCatalogTest.kt
+++ streaming/testSrc/com/android/tools/idea/streaming/emulator/RunningEmulatorCatalogTest.kt
@@ -23,6 +23,7 @@ import org.junit.Rule
 import org.junit.Test
 import java.util.concurrent.LinkedBlockingDeque
 import java.util.concurrent.TimeUnit.MILLISECONDS
+import kotlin.test.fail
 
 /**
  * Tests for [RunningEmulatorCatalog].
@@ -58,7 +59,7 @@ class RunningEmulatorCatalogTest {
     // Start the first Emulator and check that it is reflected in the catalog after an explicit update.
     emulator1.start(standalone = false)
     val emulators = runBlocking { catalog.updateNow().await() }
-    val event1: CatalogEvent = eventQueue.poll(50, MILLISECONDS) ?: throw AssertionError("Listener was not called")
+    val event1: CatalogEvent = eventQueue.poll(200, MILLISECONDS) ?: fail("Listener was not called")
     assertThat(event1.type).isEqualTo(EventType.ADDED)
     assertThat(event1.emulator.emulatorId.grpcPort).isEqualTo(emulator1.grpcPort)
     assertThat(event1.emulator.emulatorId.avdFolder).isEqualTo(emulator1.avdFolder)
@@ -67,7 +68,7 @@ class RunningEmulatorCatalogTest {
 
     // Start the second Emulator and check that a listener gets notified.
     emulator2.start(standalone = false)
-    val event2 = eventQueue.poll(1500, MILLISECONDS) ?: throw AssertionError("Listener was not called")
+    val event2 = eventQueue.poll(1500, MILLISECONDS) ?: fail("Listener was not called")
     assertThat(event2.type).isEqualTo(EventType.ADDED)
     assertThat(event2.emulator.emulatorId.grpcPort).isEqualTo(emulator2.grpcPort)
     assertThat(event2.emulator.emulatorId.avdFolder).isEqualTo(emulator2.avdFolder)
@@ -75,27 +76,27 @@ class RunningEmulatorCatalogTest {
 
     // Stop the first Emulator and check that a listener gets notified.
     emulator1.stop()
-    val event3 = eventQueue.poll(1500, MILLISECONDS) ?: throw AssertionError("Listener was not called")
+    val event3 = eventQueue.poll(1500, MILLISECONDS) ?: fail("Listener was not called")
     assertThat(event3.type).isEqualTo(EventType.REMOVED)
     assertThat(event3.emulator).isEqualTo(event1.emulator)
     assertThat(catalog.emulators).containsExactly(event2.emulator)
 
     // Stop the second Emulator and check that a listener gets notified.
     emulator2.stop()
-    val event4 = eventQueue.poll(1500, MILLISECONDS) ?: throw AssertionError("Listener was not called")
+    val event4 = eventQueue.poll(1500, MILLISECONDS) ?: fail("Listener was not called")
     assertThat(event4.type).isEqualTo(EventType.REMOVED)
     assertThat(event4.emulator).isEqualTo(event2.emulator)
     assertThat(catalog.emulators).isEmpty()
 
     // Start a standalone emulator and check that it is reflected in the catalog after an explicit update.
     emulator3.start(standalone = true)
-    val event5: CatalogEvent = eventQueue.poll(1500, MILLISECONDS) ?: throw AssertionError("Listener was not called")
+    val event5: CatalogEvent = eventQueue.poll(1500, MILLISECONDS) ?: fail("Listener was not called")
     assertThat(event5.type).isEqualTo(EventType.ADDED)
     assertThat(catalog.emulators).containsExactly(event5.emulator)
     assertThat(event5.emulator.emulatorId.isEmbedded).isFalse()
 
     emulator3.stop()
-    val event6 = eventQueue.poll(1500, MILLISECONDS) ?: throw AssertionError("Listener was not called")
+    val event6 = eventQueue.poll(1500, MILLISECONDS) ?: fail("Listener was not called")
     assertThat(event6.type).isEqualTo(EventType.REMOVED)
     assertThat(event6.emulator).isEqualTo(event5.emulator)
     assertThat(runBlocking { catalog.updateNow().await() }).isEmpty()
