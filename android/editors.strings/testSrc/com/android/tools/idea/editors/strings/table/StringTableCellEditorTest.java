72b77fef7ae1767ef022e4f367886a17d1a49533
Jens Ole Lauridsen <jlauridsen@google.com>
2025-04-22 13:37:08 -0700

    Add key validation in translation table editor
    Fixes: 401180035
    Test: StringTableCellEditorTest
    Change-Id: Ibe0eba12e4870bd883996852e0a12bc21d4407a4
    
    GitOrigin-RevId: 14f9ffae5f1d124791b8cc10ced6bfdbc8173e1d

--- android/editors.strings/testSrc/com/android/tools/idea/editors/strings/table/StringTableCellEditorTest.java
+++ android/editors.strings/testSrc/com/android/tools/idea/editors/strings/table/StringTableCellEditorTest.java
@@ -15,39 +15,79 @@
  */
 package com.android.tools.idea.editors.strings.table;
 
-import com.intellij.ui.JBColor;
-import org.junit.Before;
-import org.junit.Test;
-import org.mockito.ArgumentMatchers;
-import org.mockito.Mockito;
-
-import javax.swing.*;
-import javax.swing.border.LineBorder;
-import java.awt.event.ActionEvent;
-
+import static com.google.common.truth.Truth.assertThat;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import com.android.tools.adtui.swing.laf.HeadlessTableUI;
+import com.intellij.testFramework.ApplicationRule;
+import com.intellij.ui.JBColor;
+import javax.swing.border.LineBorder;
+import javax.swing.table.DefaultTableModel;
+import org.jetbrains.annotations.NotNull;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
 public final class StringTableCellEditorTest {
-  private JTextField myComponent;
+  @Rule
+  public ApplicationRule myRule = new ApplicationRule();
+
+  private FrozenColumnTable<DefaultTableModel> myFrozenColumnTable = null;
+  private StringTableCellEditor myEditor = null;
 
   @Before
-  public void mockComponent() {
-    myComponent = Mockito.mock(JTextField.class);
+  public void setUp() {
+    DefaultTableModel model = getModel();
+    myFrozenColumnTable = new FrozenColumnTable<>(model, 4);
+    myFrozenColumnTable.createDefaultColumnsFromModel();
+    myFrozenColumnTable.getFrozenTable().setUI(new HeadlessTableUI());
+    myFrozenColumnTable.getScrollableTable().setUI(new HeadlessTableUI());
+    myEditor = new StringTableCellEditor();
+    myFrozenColumnTable.setDefaultEditor(Object.class, myEditor);
+  }
+
+  private static @NotNull DefaultTableModel getModel() {
+    Object[][] data = new Object[][] {
+      new Object[]{"east", "app/src/main/res", false, "east", "est", "øst"},
+      new Object[]{"west", "app/src/main/res", false, "west", "ouest", "vest"},
+      new Object[]{"north", "app/src/main/res", false, "north", "nord", "nord"},
+      new Object[]{"south", "app/src/main/res", false, "south", "sud", "syd"}
+    };
+    Object[] columns = new Object[]{"Key", "Resource Folder", "Untranslatable", "Default Value", "French (fr)", "Danish (da)"};
+    return new DefaultTableModel(data, columns);
   }
 
   @Test
   public void stopCellEditing() {
-    Mockito.when(myComponent.getText()).thenReturn("https://oslobysykkel.no/_app/options/terms?locale=%1$s&product_id=%2$s");
-
-    assertFalse(new StringTableCellEditor(myComponent).stopCellEditing());
-
-    Mockito.verify(myComponent).setBorder(ArgumentMatchers.<LineBorder>argThat(
-      actual -> JBColor.RED.equals(actual.getLineColor()) && 1 == actual.getThickness() && !actual.getRoundedCorners()));
+    myFrozenColumnTable.getFrozenTable().editCellAt(1, 3);
+    myEditor.setCellEditorValue("Hello World");
+    assertTrue(myEditor.stopCellEditing());
+    LineBorder border = (LineBorder)myEditor.getComponent().getBorder();
+    assertThat(border.getLineColor()).isEqualTo(JBColor.BLACK);
+    assertThat(border.getThickness()).isEqualTo(1);
+    assertThat(myEditor.getComponent().getToolTipText()).isNull();
   }
 
   @Test
-  public void isCellEditable() {
-    assertTrue(new StringTableCellEditor(myComponent).isCellEditable(new ActionEvent(Mockito.mock(JTable.class), 0, null)));
+  public void stopCellEditingWithInvalidValue() {
+    myFrozenColumnTable.getScrollableTable().editCellAt(1, 0);
+    myEditor.setCellEditorValue("https://oslobysykkel.no/_app/options/terms?locale=%1$s&product_id=%2$s");
+    assertFalse(myEditor.stopCellEditing());
+    LineBorder border = (LineBorder)myEditor.getComponent().getBorder();
+    assertThat(border.getLineColor()).isEqualTo(JBColor.RED);
+    assertThat(border.getThickness()).isEqualTo(1);
+    assertThat(myEditor.getComponent().getToolTipText()).isEqualTo("Invalid value");
+  }
+
+  @Test
+  public void stopCellEditingWithInvalidKeyValue() {
+    myFrozenColumnTable.getFrozenTable().editCellAt(1, 0);
+    myEditor.setCellEditorValue("a b c");
+    assertFalse(myEditor.stopCellEditing());
+    LineBorder border = (LineBorder)myEditor.getComponent().getBorder();
+    assertThat(border.getLineColor()).isEqualTo(JBColor.RED);
+    assertThat(border.getThickness()).isEqualTo(1);
+    assertThat(myEditor.getComponent().getToolTipText()).isEqualTo("' ' is not a valid resource name character");
   }
 }
