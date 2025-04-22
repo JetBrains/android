5da4e82fe564f6ec3d630eefad91b927951026f0
Paul Milian <paulmilian@google.com>
2025-04-22 08:59:45 -0700

    Fix flaky test
    The flakiness was caused by kotlin classes not being present in the
    PSI class index. Because of this, we were not creating an expected
    number of run configurations. The issue only happens on Windows.
    
    I was not able to reproduce this issue when creating
    an isolated test and when running the flaky test case in an isolated
    environment. This leads me to believe that the flakiness is not caused
    by a platform issue but rather by the test infrastructure.
    
    The CL fixes the flakiness by converting the kotlin classes into java
    to ensure they are present in the index when creating the run
    configurations.
    
    Fixes: 410256906
    Test: c:\src\studio-main\tools\base\bazel\bazel.cmd test  \
      --runs_per_test=1000 --config=remote \
      //tools/adt/idea/android/project:intellij.android.core.project_tests \
      --test_filter=AndroidRunConfigurationsTest
    Change-Id: I8e0f4ac25a29361428cd55184f463227b61c7488
    
    GitOrigin-RevId: b3a1493889c285f6ebdb912099a990a6320d3f0a

---
+++ android/testData/projects/wearWithTileComplicationAndWatchFace/app/src/main/java/com/example/complication/MyComplicationService.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.complication;
+
+import androidx.wear.watchface.complications.data.ComplicationData;
+import androidx.wear.watchface.complications.data.ComplicationType;
+import androidx.wear.watchface.complications.datasource.ComplicationDataSourceService;
+import androidx.wear.watchface.complications.datasource.ComplicationRequest;
+
+public class MyComplicationService extends ComplicationDataSourceService {
+
+  @Override
+  public ComplicationData getPreviewData(ComplicationType type) {
+    return null;
+  }
+
+  @Override
+  public void onComplicationRequest(
+    ComplicationRequest request,
+    ComplicationRequestListener listener
+  ) {}
+}
