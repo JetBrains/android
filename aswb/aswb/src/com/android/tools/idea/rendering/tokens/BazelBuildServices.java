e7b87f98a3f70a001101a4febd922b0f291107ef
Juan C Nuno <juancnuno@google.com>
2025-04-22 14:57:27 -0700

    Build the dependencies
    Bug: 409388814
    Bug: 412450450
    Test: Not applicable
    Change-Id: Id7683796d09ba1b861f90c2b22164682636e669d
    
    GitOrigin-RevId: 4c975f22bddc197cf8db906d380eae6386d92b8b

--- aswb/aswb/src/com/android/tools/idea/rendering/tokens/BazelBuildServices.java
+++ aswb/aswb/src/com/android/tools/idea/rendering/tokens/BazelBuildServices.java
@@ -23,15 +23,20 @@ import com.android.tools.idea.rendering.tokens.BuildSystemFilePreviewServices.Bu
 import com.google.common.collect.MoreCollectors;
 import com.google.idea.blaze.base.logging.utils.querysync.QuerySyncActionStatsScope;
 import com.google.idea.blaze.base.model.primitives.WorkspaceRoot;
+import com.google.idea.blaze.base.qsync.DependencyTracker.DependencyBuildRequest;
 import com.google.idea.blaze.base.qsync.QuerySyncManager;
 import com.google.idea.blaze.base.qsync.QuerySyncManager.TaskOrigin;
 import com.google.idea.blaze.base.qsync.action.BuildDependenciesHelper;
 import com.google.idea.blaze.base.qsync.action.BuildDependenciesHelperSelectTargetPopup;
 import com.google.idea.blaze.base.qsync.action.TargetDisambiguationAnchors.WorkingSet;
 import com.google.idea.blaze.base.scope.BlazeContext;
+import com.google.idea.blaze.common.Label;
+import com.google.idea.blaze.exception.BuildException;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.project.Project;
+import java.io.IOException;
 import java.util.Collection;
+import java.util.Set;
 import kotlinx.coroutines.Deferred;
 import kotlinx.coroutines.guava.ListenableFutureKt;
 import org.jetbrains.annotations.NotNull;
@@ -67,25 +72,34 @@ final class BazelBuildServices implements BuildServices<BazelBuildTargetReferenc
                                     popup -> popup.showCenteredInCurrentWindow(project)),
                                   new WorkingSet(helper),
                                   scope,
-                                  labels -> buildAndRefresh(project, scope));
+                                  labels -> buildAndRefresh(project, scope, labels));
   }
 
   /**
    * Executed by the EDT
    */
-  // TODO: b/412450450 - Consume the labels
-  private static @NotNull Deferred<@NotNull Boolean> buildAndRefresh(@NotNull Project project, @NotNull QuerySyncActionStatsScope scope) {
+  private static @NotNull Deferred<@NotNull Boolean> buildAndRefresh(@NotNull Project project,
+                                                                     @NotNull QuerySyncActionStatsScope scope,
+                                                                     @NotNull Set<@NotNull Label> labels) {
     var manager = QuerySyncManager.getInstance(project);
-    var future = manager.runBuild("Build & Refresh", null, scope, BazelBuildServices::buildAndRefresh, TaskOrigin.USER_ACTION);
 
-    return ListenableFutureKt.asDeferred(future);
+    return ListenableFutureKt.asDeferred(
+      manager.runBuild("Build & Refresh", null, scope, context -> buildAndRefresh(manager, context, labels), TaskOrigin.USER_ACTION));
   }
 
   /**
    * Executed by the Blaze executor
    */
-  private static void buildAndRefresh(@NotNull BlazeContext context) {
-    // TODO: b/409388814 - Implement this
-    throw new UnsupportedOperationException();
+  private static void buildAndRefresh(@NotNull QuerySyncManager manager,
+                                      @NotNull BlazeContext context,
+                                      @NotNull Set<@NotNull Label> labels) throws BuildException {
+    try {
+      // TODO: b/413108575 - Build the runtime artifacts as well
+      manager.getLoadedProject().orElseThrow().getDependencyTracker()
+        .buildDependenciesForTargets(context, DependencyBuildRequest.multiTarget(labels));
+    }
+    catch (IOException exception) {
+      throw new BuildException(exception);
+    }
   }
 }
