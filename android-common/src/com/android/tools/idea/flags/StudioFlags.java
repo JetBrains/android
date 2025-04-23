3a835bb490534317424cb114a2326ec07c457db3
Sergio Murguia <srmurguia@google.com>
2025-04-23 13:51:41 -0700

    Add flag for default Project View settings UI
    Bug: 399814788
    Test: N/A only adding a new flag to be used in an incoming change
    Change-Id: I28b8429a2ddb7f33cbc725f7ccccf03cc63eb3a0
    
    GitOrigin-RevId: 6c8b8a749b35d4127654560e049d4adfd14a31b9

--- android-common/src/com/android/tools/idea/flags/StudioFlags.java
+++ android-common/src/com/android/tools/idea/flags/StudioFlags.java
@@ -2477,5 +2477,16 @@ public final class StudioFlags {
       false);
   // endregion Settings sync
 
+  // region PROJECT_TOOL_WINDOW
+  private static final FlagGroup PROJECT_TOOL_WINDOW = new FlagGroup(FLAGS, "project.tool.window", "Project Toolwindow");
+  public static final Flag<Boolean> SHOW_DEFAULT_PROJECT_VIEW_SETTINGS =
+    new BooleanFlag(
+      PROJECT_TOOL_WINDOW,
+      "default.project.view",
+      "Show UI for default project view in settings",
+      "Show UI for default project view in settings",
+      enabledUpTo(CANARY));
+  // endregion PROJECT_TOOL_WINDOW
+
   private StudioFlags() { }
 }
