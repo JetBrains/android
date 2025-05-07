7332799cbcadb0a0ff438058e0655d172be81556
Sergey Prigogin <sprigogin@google.com>
2025-05-07 16:38:46 -0700

    Logging for investigation of b/415832959
    Bug: 415832959
    Test: existing
    Change-Id: I8a0ca54fee75da69db260077308a388dde5aac75
    
    GitOrigin-RevId: 8e39c2fdbedf3fcc3263e44934e2b9fc95f8549f

--- streaming/src/com/android/tools/idea/streaming/actions/StreamingHardwareInputAction.kt
+++ streaming/src/com/android/tools/idea/streaming/actions/StreamingHardwareInputAction.kt
@@ -18,6 +18,8 @@ package com.android.tools.idea.streaming.actions
 import com.android.tools.idea.actions.enableRichTooltip
 import com.android.tools.idea.streaming.core.DeviceId
 import com.android.tools.idea.streaming.xr.XrInputMode
+import com.android.tools.idea.streaming.xr.b415832959Logger
+import com.android.utils.TraceUtils.simpleId
 import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.ToggleAction
@@ -25,6 +27,7 @@ import com.intellij.openapi.components.Service
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.DumbAware
 import com.intellij.util.containers.ContainerUtil.createConcurrentList
+import java.lang.Thread.currentThread
 
 /**
  * ToggleAction for hardware input.
@@ -70,10 +73,13 @@ internal class HardwareInputStateStorage {
   private val enabledDevices = createConcurrentList<String>()
 
   fun isHardwareInputEnabled(deviceId: DeviceId): Boolean {
-    return enabledDevices.contains(deviceId.storageKey)
+    val result = enabledDevices.contains(deviceId.storageKey)
+    b415832959Logger?.info("${currentThread()} $simpleId.isHardwareInputEnabled($deviceId) returning $result")
+    return result
   }
 
   fun setHardwareInputEnabled(deviceId: DeviceId, enabled: Boolean) {
+    b415832959Logger?.info("${currentThread()} $simpleId.setHardwareInputEnabled($deviceId, $enabled)")
     if (enabled) {
       enabledDevices.add(deviceId.storageKey)
     } else {
