ea3dafbc1e8ee245404f35de931d9ef248c8d073
Sergey Prigogin <sprigogin@google.com>
2025-06-10 16:23:05 -0700

    Make use of AdbDeviceServices.screenRecord
    ...in ShellCommandRecordingProvider.
    
    Bug: 422078208
    Test: ShellCommandRecordingProviderTest
    Change-Id: Ib1d120419b6e4eccac2ef24b972eda04ae667403
    
    GitOrigin-RevId: aa8bd0cd7e489be596adb736987eaaff0970f181

--- android-adb-ui/testSrc/com/android/tools/idea/ui/screenrecording/ShellCommandRecordingProviderTest.kt
+++ android-adb-ui/testSrc/com/android/tools/idea/ui/screenrecording/ShellCommandRecordingProviderTest.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2022 The Android Open Source Project
+ * Copyright (C) 2025 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,30 +16,65 @@
 package com.android.tools.idea.ui.screenrecording
 
 import com.android.SdkConstants.PRIMARY_DISPLAY_ID
-import com.google.common.truth.Truth.assertThat
+import com.android.adblib.testingutils.CoroutineTestUtils.runBlockingWithTimeout
+import com.android.adblib.testingutils.CoroutineTestUtils.yieldUntil
+import com.android.adblib.testingutils.FakeAdbServerProviderRule
+import com.android.adblib.tools.testutils.waitForOnlineConnectedDevice
+import com.android.fakeadbserver.DeviceState
+import com.android.fakeadbserver.DeviceState.HostConnectionType
+import com.android.fakeadbserver.shellcommandhandlers.ScreenRecordCommandHandler
+import com.android.sdklib.AndroidApiLevel
+import com.android.testutils.truth.PathSubject.assertThat
+import com.android.tools.idea.testing.disposable
+import com.intellij.testFramework.ProjectRule
+import com.intellij.testFramework.RuleChain
+import com.intellij.testFramework.TemporaryDirectory
+import org.junit.Rule
 import org.junit.Test
 
-/**
- * Tests for [ShellCommandRecordingProvider]
- */
+/** Tests for [ShellCommandRecordingProvider]. */
 class ShellCommandRecordingProviderTest {
+
+  private val projectRule = ProjectRule()
+  private val fakeAdbRule = FakeAdbServerProviderRule { installDefaultCommandHandlers() }
+  private val temporaryDirectoryRule = TemporaryDirectory()
+
+  @get:Rule
+  val ruleChain = RuleChain(projectRule, fakeAdbRule, temporaryDirectoryRule)
+
+  private val fakeAdb get() =
+      fakeAdbRule.fakeAdb
+  private val deviceServices get() =
+      fakeAdbRule.adbSession.deviceServices
+
   @Test
-  fun getScreenRecordCommand() {
+  fun testRecording(): Unit = runBlockingWithTimeout {
+    // Prepare.
+    val device = createFakeDevice()
     val options = ScreenRecorderOptions(
-        displayId = PRIMARY_DISPLAY_ID, width = 600, height = 400, bitrateMbps = 6, showTouches = true, timeLimitSec = 300)
+        displayId = PRIMARY_DISPLAY_ID, width = 600, height = 400, bitrateMbps = 6, showTouches = false, timeLimitSec = 300)
+    val provider = ShellCommandRecordingProvider(projectRule.disposable, device.deviceId, RECODING_FILE, options, fakeAdbRule.adbSession)
 
-    val command = ShellCommandRecordingProvider.getScreenRecordCommand(0, options, "/sdcard/foo.mp4")
+    // Act.
+    val done = provider.startRecording()
+    // Wait until the recording file is created on the device.
+    yieldUntil { device.getFile(RECODING_FILE) != null || done.isCompleted }
+    // Stop the screen recording operation and wait for it to finish.
+    provider.stopRecording()
+    done.await()
+    val file = temporaryDirectoryRule.newPath("screen-recording.mp4")
+    provider.pullRecording(file)
 
-    assertThat(command).isEqualTo("screenrecord --size 600x400 --bit-rate 6000000 --time-limit 300 /sdcard/foo.mp4")
+    // Assert.
+    assertThat(file).hasContents(ScreenRecordCommandHandler.FINISHED_RECORDING_CONTENTS)
   }
 
-  @Test
-  fun getScreenRecordCommandDefaultTimeLimit() {
-    val options = ScreenRecorderOptions(
-        displayId = 1, width = 600, height = 400, bitrateMbps = 6, showTouches = true, timeLimitSec = 0)
-
-    val command = ShellCommandRecordingProvider.getScreenRecordCommand(1234567890123456, options, "/sdcard/foo.mp4")
-
-    assertThat(command).isEqualTo("screenrecord --display-id 1234567890123456 --size 600x400 --bit-rate 6000000 /sdcard/foo.mp4")
+  private suspend fun createFakeDevice(): DeviceState {
+    val device = fakeAdb.connectDevice("1234", "Google", "Pixel 9", "Baklava", AndroidApiLevel(36), HostConnectionType.USB)
+    device.deviceStatus = DeviceState.DeviceStatus.ONLINE
+    deviceServices.session.waitForOnlineConnectedDevice(device.deviceId)
+    return device
   }
 }
+
+private const val RECODING_FILE = "/sdcard/screen-recording.mp4"
