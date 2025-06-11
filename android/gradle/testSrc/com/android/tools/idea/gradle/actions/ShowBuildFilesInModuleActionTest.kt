6e36b7b6ea5a9b0f5e26322fb625aafd08a9c670
Paige Mcauliffe <paigemca@google.com>
2025-06-11 19:58:27 +0000

    Move setting to display build files in module to Appearance submenu
    This also removes the Project Tool Window settings page, since it is
    empty without this setting.
    
    Bug: 421907923
    Test: test added
    Change-Id: Ibf1c6642aeed0438e8fd0f828e8d25734cc21fca
    
    GitOrigin-RevId: 4b53f1ec1fe6b1c7ece42a6da2e7060929d6d3e5

---
+++ android/gradle/testSrc/com/android/tools/idea/gradle/actions/ShowBuildFilesInModuleActionTest.kt
@@ -0,0 +1,94 @@
+/*
+ * Copyright (C) 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.gradle.actions
+
+import com.android.tools.idea.flags.StudioFlags
+import com.android.tools.idea.gradle.projectView.ProjectToolWindowSettings
+import com.android.tools.idea.navigator.ANDROID_VIEW_ID
+import com.intellij.ide.projectView.ProjectView
+import com.intellij.ide.projectView.impl.AbstractProjectViewPane
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.Presentation
+import com.intellij.testFramework.HeavyPlatformTestCase
+import org.mockito.Mock
+import org.mockito.MockedStatic
+import org.mockito.Mockito.mockStatic
+import org.mockito.Mockito.`when`
+import org.mockito.MockitoAnnotations.initMocks
+
+/**
+ * Tests for [ShowBuildFilesInModuleAction].
+ */
+class ShowBuildFilesInModuleActionTest : HeavyPlatformTestCase() {
+
+  @Mock
+  private lateinit var myEvent: AnActionEvent
+
+  @Mock
+  private lateinit var myViewPane: AbstractProjectViewPane
+
+  @Mock lateinit var myProjectView: ProjectView
+
+  private var myPresentation: Presentation? = null
+
+  @Throws(Exception::class)
+  override fun setUp() {
+    super.setUp()
+    initMocks(this)
+
+     myPresentation = Presentation()
+    `when`(myEvent.presentation).thenReturn(myPresentation)
+    `when`(myEvent.project).thenReturn(myProject)
+  }
+
+  fun testActionVisibility() {
+    val mockedStaticProjectView: MockedStatic<ProjectView> = mockStatic(ProjectView::class.java)
+    mockedStaticProjectView.`when`<ProjectView> { ProjectView.getInstance(project) }
+      .thenReturn(myProjectView)
+    `when`(myViewPane.id).thenReturn(ANDROID_VIEW_ID)
+    `when`(myProjectView.currentProjectViewPane).thenReturn(myViewPane)
+    val action = ShowBuildFilesInModuleAction()
+
+    // Visible when flag enabled and android view is open
+    StudioFlags.SHOW_BUILD_FILES_IN_MODULE_SETTINGS.override(true)
+    `when`(myViewPane.id).thenReturn(ANDROID_VIEW_ID)
+    action.update(myEvent)
+    assert(myEvent.presentation.isEnabledAndVisible)
+
+    // Hidden when flag disabled and android view is open
+    StudioFlags.SHOW_BUILD_FILES_IN_MODULE_SETTINGS.override(false)
+    action.update(myEvent)
+    assertFalse(myEvent.presentation.isEnabledAndVisible)
+
+    // Hidden when flag enabled and android view is not open
+    StudioFlags.SHOW_BUILD_FILES_IN_MODULE_SETTINGS.override(true)
+    `when`(myViewPane.id).thenReturn("SomeOtherViewId")
+    action.update(myEvent)
+    assertFalse(myEvent.presentation.isEnabledAndVisible)
+  }
+
+  fun testSettingShowBuildFilesInModuleSetting() {
+    val settings = ProjectToolWindowSettings.Companion.getInstance()
+    assertFalse(settings.showBuildFilesInModule)
+
+    val action = ShowBuildFilesInModuleAction()
+    action.setSelected(myEvent, true)
+    assert(settings.showBuildFilesInModule)
+
+    action.setSelected(myEvent, false)
+    assertFalse(settings.showBuildFilesInModule)
+  }
+}
---
+++ project-system-gradle/src/com/android/tools/idea/gradle/actions/ShowBuildFilesInModuleAction.kt
@@ -0,0 +1,59 @@
+/*
+ * Copyright (C) 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.gradle.actions
+
+import com.android.tools.idea.flags.StudioFlags
+import com.android.tools.idea.gradle.projectView.ProjectToolWindowSettings
+import com.android.tools.idea.navigator.ANDROID_VIEW_ID
+import com.intellij.ide.projectView.ProjectView
+import com.intellij.openapi.actionSystem.ActionUpdateThread
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.ToggleAction
+import com.intellij.openapi.project.ProjectManager
+import org.jetbrains.kotlin.idea.gradleTooling.get
+
+class ShowBuildFilesInModuleAction: ToggleAction("Display Build Files In Module") {
+  private val settings = ProjectToolWindowSettings.Companion.getInstance()
+
+  override fun isSelected(e: AnActionEvent) = settings.showBuildFilesInModule
+
+  override fun setSelected(e: AnActionEvent, state: Boolean) {
+    if (settings.showBuildFilesInModule != state) {
+      settings.showBuildFilesInModule = state
+      ProjectManager.getInstance().openProjects
+        .filter { !it.isDisposed }
+        .forEach{ ProjectView.getInstance(it)?.refresh() }
+    }
+  }
+
+  override fun getActionUpdateThread(): ActionUpdateThread = ActionUpdateThread.BGT
+
+  override fun update(e: AnActionEvent) {
+    // Display this action only when flag is enabled and Android view is visible
+    var showAction = false
+    if (StudioFlags.SHOW_BUILD_FILES_IN_MODULE_SETTINGS.get()) {
+      val project = e.project
+      if (project != null) {
+        val projectView = ProjectView.getInstance(project)
+        showAction = projectView.currentProjectViewPane.id == ANDROID_VIEW_ID
+      } else {
+        showAction = false
+      }
+    }
+    e.presentation.isEnabledAndVisible = showAction
+    super.update(e)
+  }
+}
