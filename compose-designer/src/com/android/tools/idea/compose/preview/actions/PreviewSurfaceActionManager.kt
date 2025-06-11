530e27b39f241cf09139ef8f20af7b81d7b65bb5
Polina Koval <kovalp@google.com>
2025-06-11 11:46:06 +0100

    Make transformPreviewAction available only in static preview
    Fixes: 422316014
    Test: manually
    Change-Id: I4fec086c9d17acd985815a9d0291bb69606071e3
    
    GitOrigin-RevId: fc6ad923058e78c1e969cb44594a214dd3c36ff1

--- compose-designer/src/com/android/tools/idea/compose/preview/actions/PreviewSurfaceActionManager.kt
+++ compose-designer/src/com/android/tools/idea/compose/preview/actions/PreviewSurfaceActionManager.kt
@@ -65,7 +65,9 @@ internal class PreviewSurfaceActionManager(
     // Add action to transform UI with AI
     if (StudioFlags.COMPOSE_PREVIEW_TRANSFORM_UI_WITH_AI.get()) {
       ComposeStudioBotActionFactory.EP_NAME.extensionList.firstOrNull()?.let {
-        it.transformPreviewAction()?.let { action -> actionGroup.add(action) }
+        it.transformPreviewAction()?.let { action ->
+          actionGroup.add(action.visibleOnlyInStaticPreview())
+        }
       }
     }
 
