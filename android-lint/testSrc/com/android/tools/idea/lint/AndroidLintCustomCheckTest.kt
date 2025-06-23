d4896ec35926ec9d92a4ab1690fe29bed3decd57
Paul Thomson <paulthomson@google.com>
2025-06-23 13:10:27 +0100

    Add test for registering lint jars
    Add a test for checking that third party lint checks are registered
    when doing a global Lint run (like a global inspection / Inspect
    Code...) in Studio.
    
    Bug: 424430560
    Test: this is it
    Change-Id: I75e2ad563b66e35881fe6499a51cf353c395c2a5
    
    GitOrigin-RevId: 6cb3fff544efe8817b99458a439fdb9115cd9fec

--- android-lint/testSrc/com/android/tools/idea/lint/AndroidLintCustomCheckTest.kt
+++ android-lint/testSrc/com/android/tools/idea/lint/AndroidLintCustomCheckTest.kt
@@ -17,15 +17,19 @@ package com.android.tools.idea.lint
 
 import com.android.tools.idea.gradle.project.build.invoker.GradleBuildInvoker
 import com.android.tools.idea.lint.common.AndroidLintInspectionBase
+import com.android.tools.idea.lint.common.forceRegisterThirdPartyIssues
 import com.android.tools.idea.testing.AndroidGradleProjectRule
 import com.intellij.lang.annotation.HighlightSeverity
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.command.WriteCommandAction
 import com.intellij.openapi.fileEditor.FileDocumentManager
 import com.intellij.openapi.module.ModuleManager
+import com.intellij.profile.codeInspection.InspectionProjectProfileManager
 import org.hamcrest.CoreMatchers.hasItem
 import org.junit.After
+import org.junit.Assert.assertFalse
 import org.junit.Assert.assertThat
+import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -104,5 +108,32 @@ class AndroidLintCustomCheckTest {
       "app/src/main/java/com/example/app/MySet.java",
       "Do not implement java.util.Set directly",
     )
+  }
+
+  @Test
+  fun globalInspectionRunFindsCompiledLintChecks() {
+    // Regression test for b/424430560
+    //
+    // Finding lint check jars in compiled dependencies stopped working in Studio when using
+    // "Inspect Code..." and not restricting the scope to a subset of files. The test code below
+    // does not invoke "Inspect Code...", but the Lint code path is similar enough that the test
+    // fails in the same way (without the fix).
+    val project = myProjectRule.project
+    val currentProfile = InspectionProjectProfileManager.getInstance(project).currentProfile
+
+    fun foundLintCheck(): Boolean {
+      val toolWrappers = currentProfile.getInspectionTools(null)
+      for (toolWrapper in toolWrappers) {
+        val tool = toolWrapper.tool
+        if (tool is AndroidLintInspectionBase) {
+          if (tool.issue.id == "UnitTestLintCheck3") return true
+        }
+      }
+      return false
+    }
+
+    assertFalse("Should not find third party lint check yet", foundLintCheck())
+    forceRegisterThirdPartyIssues(project, currentProfile)
+    assertTrue("Should now find third party lint check", foundLintCheck())
   }
 }
