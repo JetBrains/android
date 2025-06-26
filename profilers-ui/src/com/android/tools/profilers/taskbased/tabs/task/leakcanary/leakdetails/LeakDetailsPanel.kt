cecedf9d0c2c0e392e858e5843d7499d86e86494
Pushkar Mourya <pushkarmourya@google.com>
2025-06-26 07:24:03 +0000

    Changed text color
    Updated text color for better visibility in light theme
    
    Bug: 427567271
    Test: Existing
    Change-Id: Ie127b49b8773c54c396c2c83a874eb999464ebee
    
    GitOrigin-RevId: 211c02f55f6f0f258f3a8cb37b8a009cdfc51ba8

--- profilers-ui/src/com/android/tools/profilers/taskbased/tabs/task/leakcanary/leakdetails/LeakDetailsPanel.kt
+++ profilers-ui/src/com/android/tools/profilers/taskbased/tabs/task/leakcanary/leakdetails/LeakDetailsPanel.kt
@@ -37,7 +37,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.SpanStyle
@@ -185,6 +184,6 @@ private fun AnnotatedString.Builder.appendClassAndStatusText(previousNode: Node?
                                                                                        referringDisplayName.length) else ""
   append(AnnotatedString(text = node.className))
   append(AnnotatedString(" ${node.nodeType} \n"))
-  append(AnnotatedString(firstSection, spanStyle = SpanStyle(color = Color.White)))
+  append(AnnotatedString(firstSection))
   append(AnnotatedString(lastSection, spanStyle = SpanStyle(color = getLeakStatusColor(node.leakingStatus))))
 }
