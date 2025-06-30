96b69b016d364befe80e3809d0cd8d0442d2a901
xinruiy <xinruiy@google.com>
2025-06-30 07:14:30 +0000

    Update bzl library deps and lint issue
    For bzl library, it's according to google3 build_cleaner's suggestion. But it does not affect any build result.
    For copyright, just make sure all copyright have same format.
    For android, fix one resource lint suggestion.
    
    Bug: b/b/377865954
    Test: existing tests
    Change-Id: I7079360dcc6dc7b32a5c5488fc92ae91b69cd46c
    
    GitOrigin-RevId: 140eb699964e7e58f9de700615d50cae24aadfdb

--- aswb/testing/test_deps/projects/bzl_tests/android/main/java/com/basicapp/BasicActivity.java
+++ aswb/testing/test_deps/projects/bzl_tests/android/main/java/com/basicapp/BasicActivity.java
@@ -1,16 +1,18 @@
-// Copyright 2022 The Bazel Authors. All rights reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//    http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+/*
+ * Copyright 2022 The Bazel Authors. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package com.basicapp;
 
@@ -42,9 +44,9 @@ public class BasicActivity extends Activity {
             public void onClick(View v) {
               TextView tv = findViewById(R.id.text_hello);
               if (v.getId() == R.id.button_id_fizz) {
-                tv.setText("fizz");
+                tv.setText(R.string.fizz);
               } else if (v.getId() == R.id.button_id_buzz) {
-                tv.setText("buzz");
+                tv.setText(R.string.buzz);
               }
             }
           });
