f36654e241319e2ed21629b067aa3c0cf45aca9c
Alon Albert <aalbert@google.com>
2025-07-15 08:31:41 -0700

    Try to Detect Content Type if Unknown
    Bug: NA
    Test: NA
    Change-Id: Id0c9a89661a6c002f6610101a11b1c300f2c03da
    
    GitOrigin-RevId: bcb585775f9a2520c7392e6f7ba982fb126de9e0

--- app-inspection/inspectors/network/ide/src/com/android/tools/idea/appinspection/inspectors/network/ide/DefaultUiComponentsProvider.kt
+++ app-inspection/inspectors/network/ide/src/com/android/tools/idea/appinspection/inspectors/network/ide/DefaultUiComponentsProvider.kt
@@ -22,13 +22,20 @@ import com.android.tools.inspectors.common.ui.dataviewer.DataViewer
 import com.android.tools.inspectors.common.ui.dataviewer.IntellijDataViewer
 import com.android.tools.inspectors.common.ui.dataviewer.IntellijImageDataViewer
 import com.android.tools.inspectors.common.ui.stacktrace.StackTraceGroup
+import com.google.gson.Gson
+import com.google.gson.JsonObject
 import com.intellij.openapi.Disposable
 import com.intellij.openapi.project.Project
+import java.io.ByteArrayInputStream
+import javax.xml.parsers.DocumentBuilderFactory
 
 class DefaultUiComponentsProvider(
   private val project: Project,
   private val parentDisposable: Disposable,
 ) : UiComponentsProvider {
+  private val gson = Gson()
+  private val xml = DocumentBuilderFactory.newInstance().newDocumentBuilder()
+
   override fun createDataViewer(
     bytes: ByteArray,
     contentType: ContentType,
@@ -38,9 +45,20 @@ class DefaultUiComponentsProvider(
     return when {
       contentType.isSupportedImageType -> IntellijImageDataViewer(bytes, parentDisposable)
       contentType.isMultipart -> IntellijDataViewer.createRawTextViewer(bytes)
-      !contentType.isSupportedTextType -> IntellijDataViewer.createInvalidViewer()
-      styleHint == DataViewer.Style.RAW -> IntellijDataViewer.createRawTextViewer(bytes)
-      styleHint == DataViewer.Style.PRETTY ->
+      contentType.isSupportedTextType -> createViewer(bytes, contentType, styleHint, formatted)
+      else -> handleUnsupportedContentType(bytes, styleHint, formatted)
+    }
+  }
+
+  private fun createViewer(
+    bytes: ByteArray,
+    contentType: ContentType,
+    styleHint: DataViewer.Style,
+    formatted: Boolean,
+  ): IntellijDataViewer {
+    return when (styleHint) {
+      DataViewer.Style.RAW -> IntellijDataViewer.createRawTextViewer(bytes)
+      DataViewer.Style.PRETTY ->
         IntellijDataViewer.createPrettyViewerIfPossible(
           project,
           bytes,
@@ -48,11 +66,47 @@ class DefaultUiComponentsProvider(
           formatted,
           parentDisposable,
         )
-      else -> throw RuntimeException("DataViewer style is invalid.")
+      DataViewer.Style.INVALID -> throw RuntimeException("DataViewer style is invalid.")
+    }
+  }
+
+  /*
+   * Some web services may not specify content correctly so we try detecting it.
+   */
+  private fun handleUnsupportedContentType(
+    bytes: ByteArray,
+    styleHint: DataViewer.Style,
+    formatted: Boolean,
+  ): DataViewer {
+    return when {
+      bytes.isJson() -> createViewer(bytes, ContentType.JSON, styleHint, formatted)
+      bytes.isXml() -> createViewer(bytes, ContentType.XML, styleHint, formatted)
+      else -> IntellijDataViewer.createInvalidViewer()
     }
   }
 
   override fun createStackGroup(): StackTraceGroup {
     return IntelliJStackTraceGroup(project, parentDisposable)
+  }
+
+  // TODO(b/235501148): Detect partial JSON. See LintSyntaxHighlighter#tokenizeXml() for a starting
+  // point.
+  private fun ByteArray.isJson(): Boolean {
+    return try {
+      ByteArrayInputStream(this).reader().use { gson.fromJson(it, JsonObject::class.java) != null }
+    } catch (_: Exception) {
+      false
+    }
+  }
+
+  // TODO(b/235501148): Detect partial XML. See LintSyntaxHighlighter#tokenizeXml() for a starting
+  // point.
+  private fun ByteArray.isXml(): Boolean {
+    return try {
+      ByteArrayInputStream(this).use { xml.parse(it) }
+      true
+    } catch (_: Exception) {
+      false
+    }
   }
 }
