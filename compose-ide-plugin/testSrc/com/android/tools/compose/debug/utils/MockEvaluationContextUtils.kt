0e63117af0f0a077a125c374990def7bf873c0cc
Madalin Valceleanu <vmadalin@google.com>
2025-07-15 12:29:27 +0000

    Fix mockEvaluationContext with IntelliJ 2025.2
    With IntelliJ 2025.2 the implementation of BatchEvaluator has changed,
    using now 'putUserDataIfAbsent' instead of 'putUserData'. Given that
    mock is used for VirtualMachineProxyImpl this results on
    NullPointerException.
    
    To address this, MockEvaluationContextUtils.mockEvaluationContext
    now uses MockVirtualMachineProxy stub for VirtualMachineProxyImpl.
    
    Bug: 431970247
    Test: Existing
    Change-Id: I319d9445026861f2c5915507e0c54e26536ba835
    
    GitOrigin-RevId: be2bd9685c0811972145f26d3efb629b356bf954

--- compose-ide-plugin/testSrc/com/android/tools/compose/debug/utils/MockEvaluationContextUtils.kt
+++ compose-ide-plugin/testSrc/com/android/tools/compose/debug/utils/MockEvaluationContextUtils.kt
@@ -19,7 +19,6 @@ import com.intellij.debugger.engine.DebugProcessImpl
 import com.intellij.debugger.engine.SuspendContextImpl
 import com.intellij.debugger.engine.evaluation.EvaluationContextImpl
 import com.intellij.debugger.jdi.StackFrameProxyImpl
-import com.intellij.debugger.jdi.VirtualMachineProxyImpl
 import com.sun.jdi.ObjectReference
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.whenever
@@ -30,7 +29,7 @@ internal fun mockEvaluationContext(
 ): EvaluationContextImpl {
   val mockSuspendContext = mock<SuspendContextImpl>()
   whenever(mockSuspendContext.debugProcess).thenReturn(debugProcess)
-  whenever(mockSuspendContext.virtualMachineProxy).thenReturn(mock<VirtualMachineProxyImpl>())
+  whenever(mockSuspendContext.virtualMachineProxy).thenReturn(debugProcess.virtualMachineProxy)
   whenever(mockSuspendContext.managerThread).thenReturn(debugProcess.managerThread)
 
   val mockFrameProxyImpl = mock<StackFrameProxyImpl>()
