0e68339843289c7d7ce684dcc266ab6b080662da
Sergey Prigogin <sprigogin@google.com>
2025-07-22 17:49:05 -0700

    Make XR floating toolbar slightly more transparent
    Bug: N/A
    Test: existing
    Change-Id: I51167c826c0380227d87a6016ecb9659826a283c
    
    GitOrigin-RevId: 017efdbe3195a7d8521de2f627c438ef87dd823f

--- streaming/src/com/android/tools/idea/streaming/core/AbstractDisplayPanel.kt
+++ streaming/src/com/android/tools/idea/streaming/core/AbstractDisplayPanel.kt
@@ -137,7 +137,7 @@ abstract class AbstractDisplayPanel<T : AbstractDisplayView>(
   protected fun createFloatingToolbar() {
     floatingToolbarLayerPane.removeAll()
     if (deviceType == DeviceType.XR_HEADSET) {
-      val toolbar = FloatingToolbarContainer(horizontal = false, inactiveAlpha = 0.8).apply {
+      val toolbar = FloatingToolbarContainer(horizontal = false, inactiveAlpha = 0.7).apply {
         val actionManager = ActionManager.getInstance()
         val inputModeGroup = actionManager.getAction("android.streaming.xr.input.mode.group") as? ActionGroup
         if (inputModeGroup != null) {
