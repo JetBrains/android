a6b283971defbe5d1af59a41339ed73093c26c05
Yuriy Solodkyy <solodkyy@google.com>
2025-09-03 16:25:53 +0100

    Fix AndroidIntegrationTestSetupRule
    to allow running `bazel test` locally.
    
    Bug: n/a
    Test: n/a
    Change-Id: I9250a29ab91276405b665341fa246fb5d5d369f4
    
    GitOrigin-RevId: f59f2f2a73f48daa74d4b070f8754cbb7ffd73af

--- aswb/testing/studio/com/google/idea/blaze/android/AndroidIntegrationTestSetupRule.java
+++ aswb/testing/studio/com/google/idea/blaze/android/AndroidIntegrationTestSetupRule.java
@@ -17,18 +17,46 @@ package com.google.idea.blaze.android;
 
 import com.android.tools.analytics.AnalyticsSettings;
 import com.android.tools.analytics.AnalyticsSettingsData;
+import com.google.idea.testing.runfiles.Runfiles;
+import com.intellij.openapi.Disposable;
+import com.intellij.openapi.util.Disposer;
+import com.intellij.openapi.vfs.newvfs.impl.VfsRootAccess;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import org.junit.rules.ExternalResource;
 
 /** Runs before Android Studio integration tests. */
 public class AndroidIntegrationTestSetupRule extends ExternalResource {
+  private final Disposable testRootDisposable = Disposer.newDisposable();
+
   @Override
   protected void before() throws Throwable {
     System.setProperty("android.studio.sdk.manager.disabled", "true");
+    Path runfilesPath = Runfiles.runfilesPath();
+    final var runFilesRoot = runfilesPath.toRealPath().toString();
+    VfsRootAccess.allowRootAccess(testRootDisposable, runFilesRoot);
+    // NOTE: This file matters when running test in a bazel sandbox (i.e. not with --config=remote).
+    // Please do not remove just because the test passes in pre-submit.
+    Path mcpKotlinSdkPath =
+      runfilesPath.resolve("prebuilts/tools/common/m2/mcp-sdk-libraries.jar").toRealPath();
+    if (!Files.exists(mcpKotlinSdkPath)) {
+      // Comment out this and allowRootAccess below and run baze test without --config=remote to
+      // find out whether it is still needed and if so which file does not pass the allow list
+      // filter.
+      throw new IllegalStateException("prebuilts/tools/common/m2/mcp-sdk-libraries.jar not found");
+    }
+    VfsRootAccess.allowRootAccess(testRootDisposable, mcpKotlinSdkPath.toString());
+
 
     // AS 3.3 and higher requires analytics to be initialized, or it'll try to create a file in
     // the home directory (~/.android/...)
     AnalyticsSettingsData analyticsSettings = new AnalyticsSettingsData();
     analyticsSettings.setOptedIn(false);
     AnalyticsSettings.setInstanceForTest(analyticsSettings);
+  }
+
+  @Override
+  protected void after() {
+    Disposer.dispose(testRootDisposable);
   }
 }
