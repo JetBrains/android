dd78d152897ae8ada20cde04fcf0ac37bd6a4a09
Yuki Hamada <hummer@google.com>
2025-09-18 14:37:41 -0700

    Replace emulator snapshot migration with removal
    The `failureRetention` and `emulatorSnapshots` DSL blocks in
    `testOptions` are no longer supported.
    
    This patch updates the AGP Upgrade Assistant to reflect this. It removes
    the previous
    `MigrateFailureRetentionToEmulatorSnapshotsRefactoringProcessor`, which
    migrated properties from one deprecated block to the other.
    
    It introduces a new `RemoveEmulatorSnapshotsRefactoringProcessor` that
    completely removes both the `failureRetention` and `emulatorSnapshots`
    DSL blocks from build files.
    
    All related tests, test data, and UI messages in
    `AgpUpgradeBundle.properties` are updated to align with this new removal
    behavior.
    
    Bug: 328748781, 328748875, 328748995, 328749036
    Test: updated
    Change-Id: I95d20acaaf5d6468eaf47e9e5a417dd1b73e2b18
    
    GitOrigin-RevId: d06071f1a66780b35f30a22538f5077ac1bb74da
    (cherry picked from commit 1c4c798703dd9ac6236cd9bcc14027f25baa37e1)
    
    Space-RevId: 69ddc46fff74659c8f55f80befde3b4705c5f7c2

---
+++ project-system-gradle-upgrade/src/com/android/tools/idea/gradle/project/upgrade/RemoveEmulatorSnapshotsRefactoringProcessor.kt
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.gradle.project.upgrade
+
+import com.android.ide.common.repository.AgpVersion
+import com.android.tools.idea.gradle.dsl.api.GradleBuildModel
+import com.google.wireless.android.sdk.stats.UpgradeAssistantComponentInfo.UpgradeAssistantComponentKind
+import com.intellij.usages.impl.rules.UsageType
+
+val REMOVE_EMULATOR_SNAPSHOTS =
+  PropertiesOperationsRefactoringInfo(
+    optionalFromVersion = AgpVersion.parse("7.0.0-alpha13"),
+    requiredFromVersion = AgpVersion.parse("9.0.0-alpha03"),
+    commandNameSupplier = AgpUpgradeBundle.messagePointer("removeEmulatorSnapshotsRefactoringProcessor.commandName"),
+    shortDescriptionSupplier = { """
+        emulatorSnapshots block is no longer supported and deleted.
+      """.trimIndent()
+    },
+    processedElementsHeaderSupplier = AgpUpgradeBundle.messagePointer("removeEmulatorSnapshotsRefactoringProcessor.usageView.header"),
+    componentKind = UpgradeAssistantComponentKind.REMOVE_EMULATOR_SNAPSHOTS,
+    propertiesOperationInfos = listOf(
+      RemovePropertiesInfo(
+        propertyModelListGetter = { listOf(failureRetention(), emulatorSnapshots()) },
+        tooltipTextSupplier = AgpUpgradeBundle.messagePointer("removeEmulatorSnapshotsRefactoringProcessor.remove.tooltipText"),
+        usageType = UsageType(AgpUpgradeBundle.messagePointer("removeEmulatorSnapshotsRefactoringProcessor.remove.usageType"))
+      )
+    ),
+  )
+
+private fun GradleBuildModel.failureRetention() = android().testOptions().failureRetention()
+private fun GradleBuildModel.emulatorSnapshots() = android().testOptions().emulatorSnapshots()
