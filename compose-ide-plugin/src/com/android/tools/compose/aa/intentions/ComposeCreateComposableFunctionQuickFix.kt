19b20147f88062998534b75ddd088a48d95f9c44
Andrey Cherkasov <andrey.cherkasov@jetbrains.com>
2025-09-20 22:17:27 +0400

    [kotlin] KTIJ-35381 Simplify `mapIndexed` calls by replacing with `List` generator where applicable
    GitOrigin-RevId: 278b435a1c96872422c9cd6215cd3962d2b5f493

--- compose-ide-plugin/src/com/android/tools/compose/aa/intentions/ComposeCreateComposableFunctionQuickFix.kt
+++ compose-ide-plugin/src/com/android/tools/compose/aa/intentions/ComposeCreateComposableFunctionQuickFix.kt
@@ -125,7 +125,7 @@ class ComposeCreateComposableFunctionQuickFix(
           KtPsiFactory.CallableBuilder(KtPsiFactory.CallableBuilder.Target.FUNCTION)
             .apply {
               modifier("@$COMPOSABLE_ANNOTATION_NAME")
-              typeParams(unresolvedCall.typeArguments.mapIndexed { index, _ -> "T$index" })
+              typeParams(List(unresolvedCall.typeArguments.size) { index -> "T$index" })
               name(unresolvedName)
               val lastIndex = unresolvedCall.valueArguments.lastIndex
               unresolvedCall.valueArguments.forEachIndexed { index, arg ->
