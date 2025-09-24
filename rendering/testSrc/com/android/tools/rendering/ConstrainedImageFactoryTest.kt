562f2c5a6e72c248fc7778403f353ce789691966
Luis Santiago Re <lsantire@google.com>
2025-09-24 15:14:02 +0000

    Improve image factory logic in RenderTask
    Before this change, when quality was 1f, an unbounded image was
    being created, which is dangerous. This change makes sure that all
    images created by the factory in RenderTask are bounded to a limit.
    
    Before this change, there was a bug in the constraints calculations.
    Quality and max size constrains were being multiplied sometimes, now
    the tighter one of them is used.
    
    Before this change, different device types would get different image
    size limits. This logic hasn't been updated for a long time, and there
    are new big device types that are not being considered as special
    cases. Also sometimes a custom device defined by a user may be some
    custom deskptop or similar device, which should then be treated as
    a desktop. This change addresses these issues by using a unique max
    image size limit.
    
    Bug: 425743229
    Test: ConstrainedImageFactoryTest
    Change-Id: I3097ab58855f9ea44d1d81a2c2b97d2829a3ea1f
    
    GitOrigin-RevId: f95ffdcd04d36ddb8d1e039bd89ba3d1716ebccb
    (cherry picked from commit 7a5c29cdd0849c015852b8fae44a7d021a43f0a7)
    
    Space-RevId: f3b4bf30ddf1fd91f992c1aff5739bec1e2e4077

---
+++ rendering/testSrc/com/android/tools/rendering/ConstrainedImageFactoryTest.kt
@@ -0,0 +1,126 @@
+/*
+ * Copyright (C) 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.rendering
+
+import com.android.ide.common.rendering.api.IImageFactory
+import java.awt.image.BufferedImage
+import kotlin.random.Random
+import org.junit.Test
+import org.mockito.Mockito.mock
+
+class ConstrainedImageFactoryTest {
+  private val fakeImageFactory = FakeImageFactory()
+
+  @Test
+  fun testImageSize() {
+    val maxSize = 128L * 128L
+    var currentQuality = 1f
+
+    val myFactory = ConstrainedImageFactory(maxSize, { currentQuality }, fakeImageFactory)
+
+    // Quality = 1
+    // wanted < max
+    myFactory.getImage(64, 64)
+    assert(fakeImageFactory.getImageCalls.size == 1)
+    assert(fakeImageFactory.getImageCalls.last() == 64 to 64)
+    // wanted = max
+    myFactory.getImage(128, 128)
+    assert(fakeImageFactory.getImageCalls.size == 2)
+    assert(fakeImageFactory.getImageCalls.last() == 128 to 128)
+    myFactory.getImage(64, 256)
+    assert(fakeImageFactory.getImageCalls.size == 3)
+    assert(fakeImageFactory.getImageCalls.last() == 64 to 256)
+    // wanted > max
+    myFactory.getImage(256, 256)
+    assert(fakeImageFactory.getImageCalls.size == 4)
+    assert(fakeImageFactory.getImageCalls.last() == 128 to 128)
+    myFactory.getImage(128, 512)
+    assert(fakeImageFactory.getImageCalls.size == 5)
+    assert(fakeImageFactory.getImageCalls.last() == 64 to 256)
+
+    // Quality > 1 (same behavior as 1 expected)
+    currentQuality = Random.nextFloat() + Random.nextInt(1, 1000)
+    // wanted < max
+    myFactory.getImage(64, 64)
+    assert(fakeImageFactory.getImageCalls.size == 6)
+    assert(fakeImageFactory.getImageCalls.last() == 64 to 64)
+    // wanted = max
+    myFactory.getImage(128, 128)
+    assert(fakeImageFactory.getImageCalls.size == 7)
+    assert(fakeImageFactory.getImageCalls.last() == 128 to 128)
+    myFactory.getImage(64, 256)
+    assert(fakeImageFactory.getImageCalls.size == 8)
+    assert(fakeImageFactory.getImageCalls.last() == 64 to 256)
+    // wanted > max
+    myFactory.getImage(256, 256)
+    assert(fakeImageFactory.getImageCalls.size == 9)
+    assert(fakeImageFactory.getImageCalls.last() == 128 to 128)
+    myFactory.getImage(128, 512)
+    assert(fakeImageFactory.getImageCalls.size == 10)
+    assert(fakeImageFactory.getImageCalls.last() == 64 to 256)
+
+    // Quality = 0.25
+    currentQuality = 0.25f
+    // wanted < max
+    myFactory.getImage(64, 64)
+    assert(fakeImageFactory.getImageCalls.size == 11)
+    assert(fakeImageFactory.getImageCalls.last() == 32 to 32)
+    // wanted = max
+    myFactory.getImage(128, 128)
+    assert(fakeImageFactory.getImageCalls.size == 12)
+    assert(fakeImageFactory.getImageCalls.last() == 64 to 64)
+    myFactory.getImage(64, 256)
+    assert(fakeImageFactory.getImageCalls.size == 13)
+    assert(fakeImageFactory.getImageCalls.last() == 32 to 128)
+    // wanted > max, and wanted * quality < max
+    myFactory.getImage(256, 128)
+    assert(fakeImageFactory.getImageCalls.size == 14)
+    assert(fakeImageFactory.getImageCalls.last() == 128 to 64)
+    // wanted > max, and wanted * quality = max
+    myFactory.getImage(256, 256)
+    assert(fakeImageFactory.getImageCalls.size == 15)
+    assert(fakeImageFactory.getImageCalls.last() == 128 to 128)
+    // wanted > max, and wanted * quality > max
+    myFactory.getImage(512, 512)
+    assert(fakeImageFactory.getImageCalls.size == 16)
+    assert(fakeImageFactory.getImageCalls.last() == 128 to 128)
+    myFactory.getImage(256, 1024)
+    assert(fakeImageFactory.getImageCalls.size == 17)
+    assert(fakeImageFactory.getImageCalls.last() == 64 to 256)
+
+    // Quality = 0
+    currentQuality = 0f
+    myFactory.getImage(1024, 1024)
+    assert(fakeImageFactory.getImageCalls.size == 18)
+    assert(fakeImageFactory.getImageCalls.last() == 1 to 1)
+
+    // Huge image should not cause problems like overflow during calculation
+    currentQuality = 1f
+    myFactory.getImage(Int.MAX_VALUE, Int.MAX_VALUE)
+    assert(fakeImageFactory.getImageCalls.size == 19)
+    assert(fakeImageFactory.getImageCalls.last() == 128 to 128)
+  }
+}
+
+private class FakeImageFactory() : IImageFactory {
+  val getImageCalls = mutableListOf<Pair<Int, Int>>()
+  val fakeImage: BufferedImage = mock()
+
+  override fun getImage(width: Int, height: Int): BufferedImage {
+    getImageCalls.add(width to height)
+    return fakeImage
+  }
+}
