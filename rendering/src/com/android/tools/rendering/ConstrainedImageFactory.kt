502b33d91315087d5e3e27448cf7402668dcc563
Luis Santiago Re <lsantire@google.com>
2025-09-24 15:14:02 +0000

    Improve image factory logic in RenderTask
    Before this change, when quality was 1f, an unbounded image was
    being created, which is dangerous. This change makes sure that all
    images created by the factory in RenderTask are bounded to a limit.
    
    Before this change, there was a bug in the constraints calculations.
    Quality and max size constrains were being multiplied sometimes, now
    the tighter one of them is used.
    
    Before this change, different device types would get different image
    size limits. This logic hasn't been updated for a long time, and there
    are new big device types that are not being considered as special
    cases. Also sometimes a custom device defined by a user may be some
    custom deskptop or similar device, which should then be treated as
    a desktop. This change addresses these issues by using a unique max
    image size limit.
    
    Bug: 425743229
    Test: ConstrainedImageFactoryTest
    Change-Id: I3097ab58855f9ea44d1d81a2c2b97d2829a3ea1f
    
    GitOrigin-RevId: 012255814914a3dd1b04f3004ca4bcf7ead8adb4
    (cherry picked from commit 7a5c29cdd0849c015852b8fae44a7d021a43f0a7)
    
    Space-RevId: f3b4bf30ddf1fd91f992c1aff5739bec1e2e4077

---
+++ rendering/src/com/android/tools/rendering/ConstrainedImageFactory.kt
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.rendering
+
+import com.android.ide.common.rendering.api.IImageFactory
+import java.awt.image.BufferedImage
+import kotlin.math.max
+import kotlin.math.min
+import kotlin.math.sqrt
+
+class ConstrainedImageFactory(
+  private val maxImageSize: Long,
+  private val qualityProvider: () -> Float,
+  private val delegate: IImageFactory,
+) : IImageFactory {
+  override fun getImage(width: Int, height: Int): BufferedImage {
+    val wantedImageSize: Long = width.toLong() * height.toLong()
+    // First adjust dimensions according to quality multiplier. Each dimension is scaled using
+    // `sqrt(quality)`, so that  the whole image is scaled with `quality` as a consequence.
+    val quality = qualityProvider().toDouble().coerceIn(1e-6, 1.0)
+    val qualityDimensionScale: Double = sqrt(quality)
+
+    // Also use maxImageSize as absolute upper bound
+    val maxSizeDimensionScale: Double = sqrt(maxImageSize.toDouble() / wantedImageSize)
+
+    // Use the tighter bound (quality vs max size)
+    val dimensionScale: Double = min(qualityDimensionScale, maxSizeDimensionScale)
+    val downscaleWidth: Double = width * dimensionScale
+    val downscaleHeight: Double = height * dimensionScale
+
+    // Make sure both dimensions are positive integers
+    val w = max(1, downscaleWidth.toInt())
+    val h = max(1, downscaleHeight.toInt())
+    return delegate.getImage(w, h)
+  }
+}
