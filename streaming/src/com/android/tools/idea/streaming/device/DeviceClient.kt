dc63613463c8ad12e37fa7fe211fe9076a2c5879
Sergey Prigogin <sprigogin@google.com>
2025-09-26 12:35:22 -0700

    Move agent's exit codes to a separate file
    Bug: N/A
    Test: existing
    Change-Id: I1738be11cbab9b48d79edaa69ddb3067756bc91d
    
    GitOrigin-RevId: cfb154dd1ede844ad27ff6eb53fce6fc1846996b
    (cherry picked from commit 3886a45a263cb239b60478afcc17ecdd5bb16567)
    
    Space-RevId: ceb78810d2e062680744967ebe8e0ef1a8d727e1

--- streaming/src/com/android/tools/idea/streaming/device/DeviceClient.kt
+++ streaming/src/com/android/tools/idea/streaming/device/DeviceClient.kt
@@ -82,33 +82,6 @@ import kotlinx.coroutines.sync.Semaphore
 import kotlinx.coroutines.withTimeout
 import org.jetbrains.android.facet.AndroidFacet
 
-// Predefined agent's exit codes. Other exit codes are possible.
-internal const val AGENT_GENERIC_FAILURE = 1
-internal const val AGENT_INVALID_COMMAND_LINE = 2
-internal const val AGENT_SOCKET_CONNECTIVITY_ERROR = 10
-internal const val AGENT_SOCKET_IO_ERROR = 11
-internal const val AGENT_INVALID_CONTROL_MESSAGE = 12
-internal const val AGENT_NULL_POINTER = 20
-internal const val AGENT_CLASS_NOT_FOUND = 21
-internal const val AGENT_METHOD_NOT_FOUND = 22
-internal const val AGENT_CONSTRUCTOR_NOT_FOUND = 23
-internal const val AGENT_FIELD_NOT_FOUND = 24
-internal const val AGENT_JAVA_EXCEPTION = 25
-internal const val AGENT_VIDEO_ENCODER_NOT_FOUND = 30
-internal const val AGENT_VIDEO_ENCODER_INITIALIZATION_ERROR = 31
-internal const val AGENT_VIDEO_ENCODER_CONFIGURATION_ERROR = 32
-internal const val AGENT_WEAK_VIDEO_ENCODER = 33
-internal const val AGENT_REPEATED_VIDEO_ENCODER_ERRORS = 34
-internal const val AGENT_VIDEO_ENCODER_START_ERROR = 35
-internal const val AGENT_VIRTUAL_DISPLAY_CREATION_ERROR = 50
-internal const val AGENT_INPUT_SURFACE_CREATION_ERROR = 51
-internal const val AGENT_SERVICE_NOT_FOUND = 52
-internal const val AGENT_KEY_CHARACTER_MAP_ERROR = 53
-internal const val AGENT_XR_DEVICE_IS_NOT_CONFIGURED_FOR_MIRRORING = 54
-internal const val AGENT_SIGABORT = 134
-internal const val AGENT_SIGKILL = 137
-internal const val AGENT_SIGSEGV = 139
-
 internal const val SCREEN_SHARING_AGENT_JAR_NAME = "screen-sharing-agent.jar"
 internal const val SCREEN_SHARING_AGENT_SO_NAME = "libscreen-sharing-agent.so"
 internal const val SCREEN_SHARING_AGENT_SOURCE_PATH = "tools/adt/idea/streaming/screen-sharing-agent"
