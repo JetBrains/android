1631821fa43034537b3eecca72eb1e28c48cb60e
Jinseong Jeon <jsjeon@google.com>
2025-09-30 22:52:56 -0700

    Re-enable androidRImport test in K2 mode
    We better use this to detect the regression of K2's import quickfix
    as per b/448377698.
    
    Also, see b/304360782#comment9 for more details of the rationale.
    
    Bug: 304360782
    Bug: 448377698
    Test: AndroidQuickFixMultiFileTest.AutoImports#testAndroidRImport
    Change-Id: I1d65793acee31b3312098bf97fb03ff585f75fbb
    
    GitOrigin-RevId: 526561b89f7eb5c8437e4068d1f529c1ad68850b
    (cherry picked from commit 7c2bfc3e82917075c9239fbd87627213533c6ee8)
    
    Space-RevId: c26d1e8f6068142e997e6387851e7853f4300f87

--- android-kotlin/idea-android/testData/android/quickfix/autoImports/androidRImport.after.kt
+++ android-kotlin/idea-android/testData/android/quickfix/autoImports/androidRImport.after.kt
@@ -1,11 +1,12 @@
 // "Import class 'R'" "true"
-// ERROR: Unresolved reference: R
+// K1-ERROR: Unresolved reference: R
+// K2-ERROR: Unresolved reference 'R'.
 // DO_NOT_IMPORT: android.support.v7.appcompat.R
-// SKIP-K2 (b/304360782)
+// SKIP-K1 (b/304360782)
 
 package com.myapp.activity
 
-import com.myapp.R
+import android.R
 
 fun test() {
     val a = <caret>R.layout.activity_test_kotlin
