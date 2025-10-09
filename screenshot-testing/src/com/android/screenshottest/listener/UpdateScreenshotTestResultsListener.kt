606d4052d1c6d76d57d76bc4ace957a4629f127d
vathsalyap <vathsalyap@google.com>
2025-10-09 09:35:45 +0000

    Refactor UpdateReferenceImagesAction to use default run configuration
    Bug: 450484053
    Test: Manually tested on Android Studio
    Change-Id: I292c37601fbb3e12270d62ad9c215aea90661599
    
    GitOrigin-RevId: 60f67acef53dea5d2d9e4634b01c9275db6efa9b
    (cherry picked from commit 61a42081d938e19ea51eaba8035f225b0f5e7bf7)
    
    Space-RevId: b6ca0ea1b774a491c2abb17e4fb710a55bdc3a12

---
+++ screenshot-testing/src/com/android/screenshottest/listener/UpdateScreenshotTestResultsListener.kt
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.screenshottest.listener
+
+import com.android.tools.idea.testartifacts.instrumented.testsuite.api.AndroidTestResultListener
+import com.android.tools.idea.testartifacts.instrumented.testsuite.model.AndroidDevice
+import com.android.tools.idea.testartifacts.instrumented.testsuite.model.AndroidTestCase
+import com.android.tools.idea.testartifacts.instrumented.testsuite.model.AndroidTestSuite
+
+class UpdateScreenshotTestResultsListener : AndroidTestResultListener {
+  override fun onTestCaseStarted(device: AndroidDevice, testSuite: AndroidTestSuite, testCase: AndroidTestCase) {
+    TODO("Not yet implemented")
+  }
+
+  override fun onTestCaseFinished(device: AndroidDevice, testSuite: AndroidTestSuite, testCase: AndroidTestCase) {
+    TODO("Not yet implemented")
+  }
+}
