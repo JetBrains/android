56b2d7b13c9bee2302f8d782678ec8199c49a3f3
Yuriy Solodkyy <solodkyy@google.com>
2025-10-10 10:33:50 +0100

    Turn cc1 into Android app using JNI
    as it is the only way ASwB supports C++ code.
    
    Disable SyncedInBazelProjectTest.basic_cpp as it now reproduces
    b/397976423.
    
    Bug: 397976423
    Test: n/a
    Change-Id: I509369cde869d54a4f196a006aea9479af357bf3
    
    GitOrigin-RevId: 3445a4d011a035c110da4f78f53d6c56383a69ba
    (cherry picked from commit e0b806fc95891fb2dc5658a8f2fe7295ca53cda5)
    
    Space-RevId: 1112e458c0bbb5403c1abd6e488b603a88ce6dc2

---
+++ aswb/testing/test_deps/projects/cc1/project/hello-api.h
@@ -0,0 +1,6 @@
+#pragma once
+#include <string>
+
+// A C++ API that returns an architecture-specific message.
+// This is the C++ API that we're going to wrap in a Java method.
+std::string HelloJniMessage();
--- aswb/testing/test_deps/projects/cc1/project/hello-jni.cc
+++ aswb/testing/test_deps/projects/cc1/project/hello-jni.cc
@@ -1,25 +1,26 @@
 /*
- * Copyright 2022 The Bazel Authors. All rights reserved.
+ * Copyright (C) 2009 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
  */
 
-#pragma once
-#include <string>
+#include <jni.h>
 
-class TestClass {
-  std::string testString;
+#include "hello-api.h"
 
- public:
-  TestClass();
-};
+extern "C" jstring
+Java_com_jniexample_HelloJni_stringFromJNI( JNIEnv* env, jobject thiz )
+{
+  return env->NewStringUTF(HelloJniMessage().c_str());
+}
