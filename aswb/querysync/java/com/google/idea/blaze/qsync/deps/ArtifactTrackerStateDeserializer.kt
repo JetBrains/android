8df30650f1da5558cf68d7f615f5943dedee15cb
Yuriy Solodkyy <solodkyy@google.com>
2025-10-10 11:25:22 +0100

    Convert artifact tracker serialization to kt
    to ensure sealed classes and interfaces are respected.
    
    Bug: n/a
    Test: existing
    Change-Id: Ib340292127ef347a05f6dab8b328fd946603890a
    
    GitOrigin-RevId: af71889559cb8b20beec46e39d4aace45d16077f
    (cherry picked from commit f9efdaa678b35ce87f36c4879ff871e3d5897d8a)
    
    Space-RevId: 19d2f18382f59840b4bd79633a0745d33053c513

---
+++ aswb/querysync/java/com/google/idea/blaze/qsync/deps/ArtifactTrackerStateDeserializer.kt
@@ -0,0 +1,140 @@
+/*
+ * Copyright 2024 The Bazel Authors. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.idea.blaze.qsync.deps
+
+import com.google.common.collect.ImmutableList
+import com.google.common.collect.ImmutableMap
+import com.google.idea.blaze.common.Label
+import com.google.idea.blaze.qsync.artifacts.ArtifactMetadata
+import com.google.idea.blaze.qsync.artifacts.BuildArtifact
+import com.google.idea.blaze.qsync.java.ArtifactTrackerProto
+import com.google.idea.blaze.qsync.project.ProjectPath
+import com.google.idea.blaze.qsync.project.ProjectPath.Companion.absolute
+import com.google.idea.blaze.qsync.project.ProjectPath.Companion.projectRelative
+import com.google.idea.blaze.qsync.project.ProjectPath.Companion.workspaceRelative
+import java.nio.file.Path
+import java.time.Instant
+
+/** Deserializes [NewArtifactTracker] state from a proto.  */
+class ArtifactTrackerStateDeserializer(private val metadataFactory: ArtifactMetadata.Factory) {
+  val builtDepsMap: Map<Label, TargetBuildInfo> get() = _depsMap
+  val ccToolchainMap: Map<String, CcToolchain> get() = _ccToolchainMap
+
+  private val _depsMap = mutableMapOf<Label, TargetBuildInfo>()
+  private val _ccToolchainMap = mutableMapOf<String, CcToolchain>()
+  private val buildContexts = mutableMapOf<String, DependencyBuildContext>()
+
+  fun visit(proto: ArtifactTrackerProto.ArtifactTrackerState) {
+    if (proto.version != ArtifactTrackerStateSerializer.VERSION) {
+      // Skip loading older versions.
+      return
+    }
+    proto.buildContextsList.forEach { visitBuildContext(it) }
+    proto.builtDepsMap.entries.forEach { visitTargetBuildInfo(it) }
+    proto.ccToolchainsMap.forEach { visitCcToolchain(it.key, it.value) }
+  }
+
+  private fun visitBuildContext(buildContext: ArtifactTrackerProto.BuildContext) {
+    buildContexts[buildContext.getBuildIdForLogging()] =
+      DependencyBuildContext.create(
+        buildContext.getBuildIdForLogging(),
+        Instant.ofEpochMilli(buildContext.startTimeMillis)
+      )
+  }
+
+  private fun visitTargetBuildInfo(entry: Map.Entry<String, ArtifactTrackerProto.TargetBuildInfo>) {
+    val proto = entry.value
+    val builder = TargetBuildInfo.builder().buildContext(buildContexts[proto.getBuildId()])
+    val owner = Label.of(entry.key)
+    if (proto.hasJavaArtifacts()) {
+      builder.javaInfo(convertJavaArtifactInfo(owner, proto.javaArtifacts))
+    }
+    if (proto.hasCcInfo()) {
+      builder.ccInfo(convertCcCompilationInfo(owner, proto.ccInfo))
+    }
+    _depsMap[owner] = builder.build()
+  }
+
+  private fun visitCcToolchain(id: String, proto: ArtifactTrackerProto.CcToolchain) {
+    _ccToolchainMap[id] = CcToolchain.builder()
+      .id(id)
+      .compiler(proto.getCompiler())
+      .compilerExecutable(projectPathFrom(proto.compilerExecutable))
+      .cpu(proto.getCpu())
+      .targetGnuSystemName(proto.getTargetGnuSystemName())
+      .builtInIncludeDirectories(ImmutableList.copyOf(proto.builtInIncludeDirectoriesList.map { projectPathFrom(it) }))
+      .cOptions(ImmutableList.copyOf(proto.cOptionsList))
+      .cppOptions(ImmutableList.copyOf(proto.cppOptionsList))
+      .build()
+  }
+
+  private fun projectPathFrom(p: ArtifactTrackerProto.ProjectPath): ProjectPath {
+    return when (p.getBase()) {
+      ArtifactTrackerProto.ProjectPath.Base.UNSPECIFIED -> throw IllegalStateException("Unexpected value: $p")
+      ArtifactTrackerProto.ProjectPath.Base.WORKSPACE -> workspaceRelative(Path.of(p.getPath()))
+      ArtifactTrackerProto.ProjectPath.Base.PROJECT -> projectRelative(Path.of(p.getPath()))
+      ArtifactTrackerProto.ProjectPath.Base.ABSOLUTE -> absolute(Path.of(p.getPath()))
+      ArtifactTrackerProto.ProjectPath.Base.UNRECOGNIZED -> throw IllegalStateException("Unexpected value: $p")
+    }
+  }
+
+  private fun convertJavaArtifactInfo(owner: Label, proto: ArtifactTrackerProto.JavaArtifacts): JavaArtifactInfo {
+    return JavaArtifactInfo.builder()
+      .setLabel(owner)
+      .setIsExternalDependency(proto.isExternalDependency)
+      .setJars(toArtifactList(proto.jarsList, owner))
+      .setOutputJars(toArtifactList(proto.outputJarsList, owner))
+      .setIdeAar(if (proto.hasIdeAar()) toArtifact(proto.ideAar, owner) else null)
+      .setGenSrcs(toArtifactList(proto.genSrcsList, owner))
+      .setSources(proto.sourcesList.map { Path.of(it) }.toSet())
+      .setSrcJars(proto.srcJarsList.map { Path.of(it) }.toSet())
+      .setAndroidResourcesPackage(proto.getAndroidResourcesPackage())
+      .build()
+  }
+
+  private fun convertCcCompilationInfo(owner: Label, proto: ArtifactTrackerProto.CcCompilationInfo): CcCompilationInfo {
+    return CcCompilationInfo.builder()
+      .target(owner)
+      .defines(proto.definesList)
+      .includeDirectories(proto.includeDirectoriesList.map { projectPathFrom(it) })
+      .quoteIncludeDirectories(proto.quoteIncludeDirectoriesList.map { projectPathFrom(it) })
+      .systemIncludeDirectories(
+        proto.sysytemIncludeDirectoriesList.map { projectPathFrom(it) })
+      .frameworkIncludeDirectories(proto.frameworkIncludeDirectoriesList.map { projectPathFrom(it) })
+      .genHeaders(toArtifactList(proto.genHeadersList, owner))
+      .toolchainId(proto.getToolchainId())
+      .build()
+  }
+
+  private fun toArtifact(a: ArtifactTrackerProto.Artifact, owner: Label): BuildArtifact {
+    return BuildArtifact.create(
+      a.getDigest(),
+      Path.of(a.getArtifactPath()),
+      owner,
+      ImmutableMap.copyOf(toArtifactMap(a.metadataList))
+    )
+  }
+
+  private fun toArtifactList(protos: List<ArtifactTrackerProto.Artifact>, owner: Label): List<BuildArtifact> {
+    return protos.map { toArtifact(it, owner) }
+  }
+
+  private fun toArtifactMap(protoList: List<ArtifactTrackerProto.Metadata>): Map<Class<out ArtifactMetadata>, ArtifactMetadata> {
+    return protoList
+      .mapNotNull { metadataFactory.create(it) }
+      .associateBy { it.javaClass }
+  }
+}
---
+++ aswb/querysync/java/com/google/idea/blaze/qsync/deps/ArtifactTrackerStateSerializer.kt
@@ -0,0 +1,144 @@
+/*
+ * Copyright 2024 The Bazel Authors. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.idea.blaze.qsync.deps
+
+import com.google.common.collect.Sets
+import com.google.errorprone.annotations.CanIgnoreReturnValue
+import com.google.idea.blaze.common.Label
+import com.google.idea.blaze.qsync.artifacts.BuildArtifact
+import com.google.idea.blaze.qsync.java.ArtifactTrackerProto
+import com.google.idea.blaze.qsync.project.ProjectPath
+import com.google.idea.blaze.qsync.project.ProjectPath.AbsoluteProjectPath
+import com.google.idea.blaze.qsync.project.ProjectPath.ProjectRelativeProjectPath
+import com.google.idea.blaze.qsync.project.ProjectPath.WorkspaceRelativeProjectPath
+
+/** Serializes [NewArtifactTracker] state to a proto.  */
+class ArtifactTrackerStateSerializer {
+  companion object {
+    const val VERSION: Int = 4
+  }
+  private val proto = ArtifactTrackerProto.ArtifactTrackerState.newBuilder().setVersion(VERSION)
+  private val buildIdsSeen: MutableSet<String> = Sets.newHashSet()
+
+  @CanIgnoreReturnValue
+  fun visitDepsMap(builtDeps: Map<Label, TargetBuildInfo>): ArtifactTrackerStateSerializer {
+    builtDeps.entries.forEach { visitTargetBuildInfo(it.key, it.value) }
+    return this
+  }
+
+  @CanIgnoreReturnValue
+  fun visitToolchainMap(toolchainMap: Map<String, CcToolchain>): ArtifactTrackerStateSerializer {
+    toolchainMap.values.forEach { visitCcToolchain(it) }
+    return this
+  }
+
+  fun toProto(): ArtifactTrackerProto.ArtifactTrackerState {
+    return proto.build()
+  }
+
+  private fun visitTargetBuildInfo(target: Label, targetBuildInfo: TargetBuildInfo) {
+    visitBuildContext(targetBuildInfo.buildContext())
+
+    val builder = ArtifactTrackerProto.TargetBuildInfo.newBuilder()
+    builder.setBuildId(targetBuildInfo.buildContext().buildIdForLogging())
+    targetBuildInfo.javaInfo().ifPresent { visitJavaInfo(it, builder) }
+    targetBuildInfo.ccInfo().ifPresent { visitCcInfo(it, builder) }
+    proto.putBuiltDeps(target.toString(), builder.build())
+  }
+
+  private fun visitBuildContext(buildContext: DependencyBuildContext) {
+    if (buildIdsSeen.add(buildContext.buildIdForLogging())) {
+      proto
+        .addBuildContextsBuilder()
+        .setStartTimeMillis(buildContext.startTime().toEpochMilli())
+        .setBuildIdForLogging(buildContext.buildIdForLogging())
+    }
+  }
+
+  private fun visitJavaInfo(javaInfo: JavaArtifactInfo, builder: ArtifactTrackerProto.TargetBuildInfo.Builder) {
+    val artifactTrackerProtoBuilder = builder.getJavaArtifactsBuilder()
+    javaInfo.ideAar()?.let {artifactTrackerProtoBuilder.setIdeAar(toProto(it))}
+    artifactTrackerProtoBuilder
+      .addAllGenSrcs(toProtos(javaInfo.genSrcs()))
+      .addAllJars(toProtos(javaInfo.jars()))
+      .addAllSources(javaInfo.sources().map { it.toString() })
+      .addAllSrcJars(javaInfo.srcJars().map { it.toString() })
+      .setAndroidResourcesPackage(javaInfo.androidResourcesPackage())
+  }
+
+  private fun toProtos(artifacts: Collection<BuildArtifact>): List<ArtifactTrackerProto.Artifact> {
+    return artifacts.map { toProto(it) }
+  }
+
+  private fun toProto(artifact: BuildArtifact): ArtifactTrackerProto.Artifact {
+    return ArtifactTrackerProto.Artifact.newBuilder()
+      .setDigest(artifact.digest())
+      .setArtifactPath(artifact.artifactPath().toString())
+      .addAllMetadata(artifact.metadata().values.map { it.toProto() })
+      .build()
+  }
+
+  private fun visitCcInfo(ccInfo: CcCompilationInfo, builder: ArtifactTrackerProto.TargetBuildInfo.Builder) {
+    builder
+      .getCcInfoBuilder()
+      .addAllDefines(ccInfo.defines())
+      .addAllIncludeDirectories(ccInfo.includeDirectories().map { projectPathToProto(it) })
+      .addAllQuoteIncludeDirectories(ccInfo.quoteIncludeDirectories().map { projectPathToProto(it) })
+      .addAllSysytemIncludeDirectories(ccInfo.systemIncludeDirectories().map { projectPathToProto(it) })
+      .addAllFrameworkIncludeDirectories(ccInfo.frameworkIncludeDirectories().map { projectPathToProto(it) })
+      .addAllGenHeaders(toProtos(ccInfo.genHeaders()))
+      .setToolchainId(ccInfo.toolchainId())
+  }
+
+  private fun projectPathToProto(projectPath: ProjectPath): ArtifactTrackerProto.ProjectPath {
+    when (projectPath) {
+      is WorkspaceRelativeProjectPath -> {
+        return ArtifactTrackerProto.ProjectPath.newBuilder()
+          .setBase(ArtifactTrackerProto.ProjectPath.Base.WORKSPACE)
+          .setPath(projectPath.relativePath.toString())
+          .build()
+      }
+
+      is ProjectRelativeProjectPath -> {
+        return ArtifactTrackerProto.ProjectPath.newBuilder()
+          .setBase(ArtifactTrackerProto.ProjectPath.Base.PROJECT)
+          .setPath(projectPath.relativePath.toString())
+          .build()
+      }
+
+      is AbsoluteProjectPath -> {
+        return ArtifactTrackerProto.ProjectPath.newBuilder()
+          .setBase(ArtifactTrackerProto.ProjectPath.Base.ABSOLUTE)
+          .setPath(projectPath.absolutePath.toString())
+          .build()
+      }
+    }
+  }
+
+  private fun visitCcToolchain(toolchain: CcToolchain) {
+    proto.putCcToolchains(
+      toolchain.id(),
+      ArtifactTrackerProto.CcToolchain.newBuilder()
+        .setCompiler(toolchain.compiler())
+        .setCompilerExecutable(projectPathToProto(toolchain.compilerExecutable()))
+        .setCpu(toolchain.cpu())
+        .setTargetGnuSystemName(toolchain.targetGnuSystemName())
+        .addAllBuiltInIncludeDirectories(toolchain.builtInIncludeDirectories().map { projectPathToProto(it) })
+        .addAllCOptions(toolchain.cOptions())
+        .addAllCppOptions(toolchain.cppOptions())
+        .build())
+  }
+}
