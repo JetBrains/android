3c4dc7a25ebf1a7d672c6f6d60b78d7cc3455072
David Belcher <davidbelcher@google.com>
2025-10-10 16:46:30 +0100

    Move NotationStrategy inner classes to standalone classes
    The IDE refactoring tools were used to move the inner classes to
    top-level classes, as they will be re-used in a follow-up CL.
    
    Bug: 446141700
    Test: Existing
    Change-Id: I6a18400dfbf1ccdabbf7e48647cdfdf1d6b391df
    
    GitOrigin-RevId: a62f49bbc2e4fd106dd1c6e33f172d0a0a142bb7
    (cherry picked from commit 6dd73cd1e68e7e00dc6c0eb5af1705768e178797)
    
    Space-RevId: 8103caaccedfbd5d749a39b700f62db9c069e413

---
+++ gradle-dsl/src/com/android/tools/idea/gradle/dsl/model/dependencies/CompactNotationStrategy.java
@@ -0,0 +1,114 @@
+/*
+ * Copyright (C) 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.gradle.dsl.model.dependencies;
+
+import static com.android.tools.idea.gradle.dsl.api.ext.GradlePropertyModel.ValueType.NONE;
+import static com.android.tools.idea.gradle.dsl.model.ext.PropertyUtil.resolveElement;
+
+import com.android.tools.idea.gradle.dsl.api.dependencies.ArtifactDependencySpec;
+import com.android.tools.idea.gradle.dsl.api.ext.ResolvedPropertyModel;
+import com.android.tools.idea.gradle.dsl.model.ext.GradlePropertyModelBuilder;
+import com.android.tools.idea.gradle.dsl.model.ext.transforms.CompactToMapCatalogDependencyTransform;
+import com.android.tools.idea.gradle.dsl.model.ext.transforms.FakeElementTransform;
+import com.android.tools.idea.gradle.dsl.model.ext.transforms.PropertyTransform;
+import com.android.tools.idea.gradle.dsl.parser.dependencies.FakeArtifactElement;
+import com.android.tools.idea.gradle.dsl.parser.elements.FakeElement;
+import com.android.tools.idea.gradle.dsl.parser.elements.GradleDslSimpleExpression;
+import com.android.tools.idea.gradle.dsl.parser.elements.GradleNameElement;
+import java.util.function.BiConsumer;
+import java.util.function.Function;
+import org.jetbrains.annotations.NotNull;
+
+class CompactNotationStrategy implements NotationStrategy {
+  @NotNull private GradleDslSimpleExpression myDslExpression;
+  private boolean mySetThrough;
+
+  CompactNotationStrategy(@NotNull GradleDslSimpleExpression dslExpression,
+                          boolean setThrough) {
+    myDslExpression = dslExpression;
+    mySetThrough = setThrough;
+  }
+
+  @NotNull
+  public ResolvedPropertyModel createModelFor(@NotNull String name,
+                                              @NotNull Function<ArtifactDependencySpec, String> getFunc,
+                                              @NotNull BiConsumer<ArtifactDependencySpecImpl, String> setFunc,
+                                              boolean canDelete,
+                                              PropertyTransform additionalTransformer
+  ) {
+    GradleDslSimpleExpression element = mySetThrough ? resolveElement(myDslExpression) : myDslExpression;
+    FakeElement fakeElement =
+      new FakeArtifactElement(element.getParent(), GradleNameElement.fake(name), element, getFunc, setFunc, canDelete);
+    GradlePropertyModelBuilder builder = GradlePropertyModelBuilder.create(fakeElement);
+    if (additionalTransformer != null) {
+      builder = builder.addTransform(additionalTransformer);
+    }
+    return builder.addTransform(new FakeElementTransform()).buildResolved();
+  }
+
+  @NotNull
+  public ResolvedPropertyModel createModelFor(@NotNull String name,
+                                              @NotNull Function<ArtifactDependencySpec, String> getFunc,
+                                              @NotNull BiConsumer<ArtifactDependencySpecImpl, String> setFunc,
+                                              boolean canDelete) {
+    return createModelFor(name, getFunc, setFunc, canDelete, null);
+  }
+
+
+  @Override
+  public boolean isValidDSL() {
+    String value = myDslExpression.getValue(String.class);
+    if (value == null || value.trim().isEmpty()) {
+      return false;
+    }
+    // Check if the notation is valid i.e. it has a name
+    return name().getValueType() != NONE;
+  }
+
+  @Override
+  @NotNull
+  public ResolvedPropertyModel name() {
+    return createModelFor("name", ArtifactDependencySpec::getName, ArtifactDependencySpecImpl::setName, false);
+  }
+
+  @Override
+  @NotNull
+  public ResolvedPropertyModel group() {
+    return createModelFor("group", ArtifactDependencySpec::getGroup, ArtifactDependencySpecImpl::setGroup, true);
+  }
+
+  @Override
+  @NotNull
+  public ResolvedPropertyModel version() {
+    return createModelFor("version",
+                          ArtifactDependencySpec::getVersion,
+                          ArtifactDependencySpecImpl::setVersion,
+                          true,
+                          new CompactToMapCatalogDependencyTransform());
+  }
+
+  @Override
+  @NotNull
+  public ResolvedPropertyModel classifier() {
+    return createModelFor("classifier", ArtifactDependencySpec::getClassifier, ArtifactDependencySpecImpl::setClassifier, true);
+  }
+
+  @Override
+  @NotNull
+  public ResolvedPropertyModel extension() {
+    return createModelFor("extension", ArtifactDependencySpec::getExtension, ArtifactDependencySpecImpl::setExtension, true);
+  }
+}
