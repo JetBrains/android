804e4b1121112812b659cb29e9797d3275b493e6
Amaury Medeiros <amaurym@google.com>
2025-10-14 17:58:27 +0100

    Use smartReadAction when checking if
    ... ComposePreviewRepresentationProvider can create a reprsentation.
    It's not enough to check for DumbService.isDumb(), because the project
    can return to DumbMode between this check and when we actually rely on
    the indexes, causing an IndexNotReadyException to be thrown. In addition
    to the DumbService.isDumb() early return, we should wrap the read action
    into a smartReadAction instead. This way, we will suspend until it's
    possible to obtain the read lock in smart mode and only then we'll run
    the action.
    
    Fixes: 451626256
    Test: N/A
    Change-Id: I370c1a3311dbe545badeb32e4dd904480f431344
    
    GitOrigin-RevId: d5d18773e1d96e20cfaff259766b351c755f731b
    (cherry picked from commit 390e6c73d1897c4b9cc1db6d8cfb18e87fc959aa)
    
    Space-RevId: 25ae13a93274c47fe61e37f9f7bf57c15fc9eb54

--- compose-designer/src/com/android/tools/idea/compose/preview/ComposePreviewRepresentationProvider.kt
+++ compose-designer/src/com/android/tools/idea/compose/preview/ComposePreviewRepresentationProvider.kt
@@ -61,7 +61,7 @@ import com.intellij.openapi.actionSystem.DataKey
 import com.intellij.openapi.actionSystem.DefaultActionGroup
 import com.intellij.openapi.actionSystem.Separator
 import com.intellij.openapi.application.ApplicationManager
-import com.intellij.openapi.application.readAction
+import com.intellij.openapi.application.smartReadAction
 import com.intellij.openapi.diagnostic.debug
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.module.ModuleUtilCore
@@ -153,7 +153,7 @@ class ComposePreviewRepresentationProvider(
     // We need to be in smart mode to be able to access the index for the annotations.
     if (DumbService.isDumb(project)) return false
     return psiFile.virtualFile.isKotlinFileType() &&
-      (readAction {
+      (smartReadAction(project) {
         (psiFile.getModuleSystem()?.usesCompose == true ||
           isCompatibleComposableClassAvailable(psiFile)) && !psiFile.isInLibrary()
       })
