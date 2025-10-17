89dda61e235e44d821423cfe45a7b9af81ed64e5
Karima Ichou <karimai@google.com>
2025-10-17 14:55:29 +0100

    Update comparison benchmark Kotlin version
    to 2.3.20-dev-825
    
    Bug: 365509858
    Test: N/A
    Change-Id: I695cf39593c40eb4b2de1b6f689571e72591c48b
    
    GitOrigin-RevId: 4e217f36f10a2ae15f3a88c4b5d3f54effead6ad
    (cherry picked from commit 834d3e91ec97c164f799435670c90262bf3467d7)
    
    Space-RevId: 1f5c33b8749cf8b84aecad259bb7660dd1f2eb0e

--- android-test-framework/testSrc/com/android/tools/idea/testing/AgpIntegrationTests.kt
+++ android-test-framework/testSrc/com/android/tools/idea/testing/AgpIntegrationTests.kt
@@ -27,7 +27,7 @@ import com.intellij.openapi.projectRoots.JavaSdkVersion.JDK_1_8
 const val GRADLE_SNAPSHOT_VERSION = "9.2.0-20251017013220+0000"
 const val GRADLE_DECLARATIVE_SNAPSHOT_VERSION = "9.1.0-20250726001724+0000"
 // For available versions: https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev/org/jetbrains/kotlin/kotlin-compiler/maven-metadata.xml
-const val KOTLIN_SNAPSHOT_VERSION = "2.3.0-dev-9885"
+const val KOTLIN_SNAPSHOT_VERSION = "2.3.20-dev-825"
 
 /**
  * An AGP Version definition to be used in AGP integration tests.
