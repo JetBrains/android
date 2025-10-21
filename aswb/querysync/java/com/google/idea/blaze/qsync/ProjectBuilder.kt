ca4cab911a1e8d981dbf7d9dadf757d3b8642bc7
Arvind Ranganathan <arvindanekal@google.com>
2025-10-21 19:56:14 +0000

    Convert ProjectBuilder to Kotlin
    Bug: 448702414
    Test: NA
    Change-Id: I0cb82b93e5d6d5984fb8bdaf8abed16f3fd5a0d4
    
    GitOrigin-RevId: cf121498f89746314bcb7a537fbdcfd94723f31c
    (cherry picked from commit f74e7402311a06e8da9a1e6f4ca8ad0cf3ffdee1)
    
    Space-RevId: 02b5c22c6b74363294a1f6e1e21eca8a717514ea

---
+++ aswb/querysync/java/com/google/idea/blaze/qsync/ProjectBuilder.kt
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2023 The Bazel Authors. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.idea.blaze.qsync
+
+import com.google.common.util.concurrent.ListeningExecutorService
+import com.google.idea.blaze.common.Context
+import com.google.idea.blaze.exception.BuildException
+import com.google.idea.blaze.qsync.deps.ArtifactTracker
+import com.google.idea.blaze.qsync.java.PackageReader
+import com.google.idea.blaze.qsync.java.WorkspaceResolvingPackageReader
+import com.google.idea.blaze.qsync.project.BuildGraphData
+import com.google.idea.blaze.qsync.project.PostQuerySyncData
+import com.google.idea.blaze.qsync.project.ProjectPath
+import com.google.idea.blaze.qsync.project.ProjectProto
+import com.google.idea.blaze.qsync.project.update.ProjectProtoUpdate
+import com.google.idea.blaze.qsync.project.update.ProjectProtoUpdateOperation
+import java.nio.file.Path
+
+/**
+ * Project refresher creates an appropriate [RefreshOperation] based on the project and
+ * current VCS state.
+ */
+class ProjectBuilder(
+  private val executor: ListeningExecutorService,
+  private val packageReader: PackageReader,
+  private val parallelPackageReader: PackageReader.ParallelReader,
+  private val workspaceRoot: Path,
+) {
+
+  /**
+   * Creates a [QuerySyncProjectSnapshot], which includes an expected IDE project structure,
+   * from the `postQuerySyncData` and a function `applyBuiltDependenciesTransform` that
+   * applies transformations required to account for any currently synced(i.e. built) dependencies.
+   */
+  @Throws(BuildException::class)
+  fun createBlazeProjectStructure(
+    context: Context<*>,
+    postQuerySyncData: PostQuerySyncData,
+    graph: BuildGraphData,
+    artifactTrackerState: ArtifactTracker.State,
+    projectProtoUpdates: Collection<ProjectProtoUpdateOperation>,
+  ): ProjectProto.Project {
+    val effectiveWorkspaceRoot =
+      postQuerySyncData.vcsState().flatMap { it.workspaceSnapshotPath }.orElse(workspaceRoot)
+    val packageReader = WorkspaceResolvingPackageReader(effectiveWorkspaceRoot, this.packageReader)
+    val javaPackagePrefixReader: JavaPackagePrefixReader =
+      JavaPackagePrefixReaderImpl(workspaceRoot, packageReader, parallelPackageReader)
+
+    val graphToProjectConverter =
+      GraphToProjectConverter(
+        javaPackagePrefixReader = javaPackagePrefixReader,
+        context = context,
+        projectDefinition = postQuerySyncData.projectDefinition()
+      )
+    val externalRepositoryFinder = ProjectPath.ExternalRepositoryFinder.createAndPrepare(workspaceRoot)
+
+    val update = ProjectProtoUpdate(ProjectProto.Project.getDefaultInstance())
+    graphToProjectConverter.createProject(graph, externalRepositoryFinder, update)
+    for (updateOperation in projectProtoUpdates) {
+      updateOperation.update(update, graph, artifactTrackerState, context, externalRepositoryFinder)
+    }
+    return update.build()
+  }
+}
