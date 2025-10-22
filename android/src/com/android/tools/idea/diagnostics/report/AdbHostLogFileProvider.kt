667d167454f0eb8b5ddf03c67d2dd4d58fc22b38
Sherif Eid <sherifeid@google.com>
2025-10-22 15:27:13 +0100

    Add output of server-status to a file in Diagnostic data
    The server-status output can change by changing some environment
    variables (e.g. ADB_TRACE). We fetch new server-status whenever adb
    changes.
    
    Test: Help -> Collect Logs and Diagnostic data
    Bug: 412571872
    Change-Id: Ie852c97af650506aa9f56c646f756e36ae176b77
    
    GitOrigin-RevId: 913db19b5e6e3e082dd164dc09fe4c0457299b17
    
    Space-RevId: 84255f3c2df121f89a0c9e6f28d0892692629525
    (cherry picked from commit 08d56dc02aa729859b63e4afb7f1d3fe5e0594db)

--- android/src/com/android/tools/idea/diagnostics/report/AdbHostLogFileProvider.kt
+++ android/src/com/android/tools/idea/diagnostics/report/AdbHostLogFileProvider.kt
@@ -15,21 +15,35 @@
  */
 package com.android.tools.idea.diagnostics.report
 
-import com.android.tools.idea.adb.AdbHostLog
+import com.android.tools.idea.adb.AdbServerStatusRetriever
+import com.android.tools.idea.flags.StudioFlags
+import com.intellij.openapi.application.PathManager
 import com.intellij.openapi.project.Project
+import java.nio.file.Files
+import java.nio.file.Paths
 import kotlin.io.path.Path
 
 /** A [DiagnosticsSummaryFileProvider] for adb host log. */
 object AdbHostLogFileProvider : DiagnosticsSummaryFileProvider {
 
+  private const val SERVER_STATUS_FILE: String = "server-status.log"
+
   override val name: String
     get() = "Adb Host"
 
   override fun getFiles(project: Project?): List<FileInfo> {
+    if (!StudioFlags.ADB_HOST_LOGS_ENABLED.get()) {
+      return emptyList()
+    }
     if (project == null) {
       return emptyList()
     }
-    val logPath = AdbHostLog.getInstance(project).path ?: return emptyList()
-    return listOf(FileInfo(Path(logPath), Path(logPath).fileName))
+    val serverStatus = AdbServerStatusRetriever.getInstance(project).serverStatus.value ?: return emptyList()
+    val serverStatusOutputFile =
+      DiagnosticsSummaryFileProvider.getDiagnosticsDirectoryPath(PathManager.getLogPath())
+        .resolve(SERVER_STATUS_FILE)
+    Files.writeString(serverStatusOutputFile, serverStatus.toString())
+    return listOf(FileInfo(Path(serverStatus.absoluteLogPath), Path(serverStatus.absoluteLogPath).fileName),
+                  FileInfo(serverStatusOutputFile, Paths.get(SERVER_STATUS_FILE)))
   }
 }
