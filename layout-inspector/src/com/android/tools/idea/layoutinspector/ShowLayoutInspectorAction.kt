0a75e1802f0e63cdbab928011e7f03df03ccc184
Pierfrancesco Soffritti <psoffritti@google.com>
2025-10-24 16:08:10 +0100

    Refactor ShowLayoutInspectorAction
    Test: existing
    Bug: NA
    Change-Id: I625dff9fa8b3ee3b9cd4c5ac82ba4d89955fae01
    
    GitOrigin-RevId: b981493ebea1391406efa233bc4589814bbbb4b1
    
    Space-RevId: 4cffe38dd6ab69979949047fc922c330b4ba51b3
    (cherry picked from commit 8edfee5045c5f7689a8eca4955cef2efba6cf999)

--- layout-inspector/src/com/android/tools/idea/layoutinspector/ShowLayoutInspectorAction.kt
+++ layout-inspector/src/com/android/tools/idea/layoutinspector/ShowLayoutInspectorAction.kt
@@ -15,6 +15,7 @@
  */
 package com.android.tools.idea.layoutinspector
 
+import com.android.tools.idea.layoutinspector.runningdevices.actions.TOGGLE_EMBEDDED_LAYOUT_INSPECTOR_ACTION_ID
 import com.android.tools.idea.layoutinspector.settings.LayoutInspectorSettings
 import com.android.tools.idea.streaming.RUNNING_DEVICES_TOOL_WINDOW_ID
 import com.android.tools.idea.util.CommonAndroidUtil
@@ -24,6 +25,7 @@ import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.project.DumbAwareAction
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowManager
 import icons.StudioIcons
 import org.jetbrains.android.util.AndroidBundle
@@ -47,30 +49,40 @@ class ShowLayoutInspectorAction :
   override fun actionPerformed(e: AnActionEvent) {
     val project = e.project ?: return
     if (LayoutInspectorSettings.getInstance().embeddedLayoutInspectorEnabled) {
-      activateToolWindow(project, RUNNING_DEVICES_TOOL_WINDOW_ID)
+      activateEmbeddedLayoutInspectorToolWindow(project)
     } else {
-      activateToolWindow(project, LAYOUT_INSPECTOR_TOOL_WINDOW_ID)
+      activateStandaloneLayoutInspectorToolWindow(project)
     }
   }
+}
 
-  private fun activateToolWindow(project: Project, toolWindowId: String) {
-    val toolWindow = ToolWindowManager.getInstance(project).getToolWindow(toolWindowId)
-    toolWindow?.activate {
-      if (toolWindowId == LAYOUT_INSPECTOR_TOOL_WINDOW_ID) {
-        // We don't need to trigger toggle action for standalone layout inspector.
-        return@activate
-      }
-      val toggleLayoutInspectorAction =
-        ActionManager.getInstance()
-          .getAction("com.android.tools.idea.layoutinspector.toggle.layout.inspector.action")
-      ActionManager.getInstance()
-        .tryToExecute(
-          toggleLayoutInspectorAction,
-          null,
-          toolWindow.contentManager.selectedContent?.component,
-          ActionPlaces.TOOLBAR,
-          true,
-        )
-    }
+private fun activateEmbeddedLayoutInspectorToolWindow(project: Project) {
+  activateToolWindow(project, toolWindowId = RUNNING_DEVICES_TOOL_WINDOW_ID) { toolWindow ->
+    val toggleLayoutInspectorAction =
+      ActionManager.getInstance().getAction(TOGGLE_EMBEDDED_LAYOUT_INSPECTOR_ACTION_ID)
+
+    ActionManager.getInstance()
+      // If there is a selected device tab in Running Devices, enable embedded Layout Inspector for
+      // it
+      .tryToExecute(
+        toggleLayoutInspectorAction,
+        null,
+        toolWindow.contentManager.selectedContent?.component,
+        ActionPlaces.TOOLBAR,
+        true,
+      )
   }
+}
+
+private fun activateStandaloneLayoutInspectorToolWindow(project: Project) {
+  activateToolWindow(project, toolWindowId = LAYOUT_INSPECTOR_TOOL_WINDOW_ID)
+}
+
+private fun activateToolWindow(
+  project: Project,
+  toolWindowId: String,
+  action: (ToolWindow) -> Unit = {},
+) {
+  val toolWindow = ToolWindowManager.getInstance(project).getToolWindow(toolWindowId)
+  toolWindow?.activate { action(toolWindow) }
 }
