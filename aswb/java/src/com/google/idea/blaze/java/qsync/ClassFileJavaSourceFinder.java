233827cbd8d79aebe5b5b15a0d52257694ce8d47
xinruiy <xinruiy@google.com>
2025-10-25 19:28:39 +0000

    Refactor ClassFileJavaSourceFinder
    Move the common util functions into a base class, so that our next
    source finder class can reuse them
    
    Bug: b/447640817
    Test: to be added
    Change-Id: I6e1c43714ed0c3c009d27a1b8115d498773491dc
    
    GitOrigin-RevId: 02827a2e4e810bc56218de44156fc55fad376222
    
    Space-RevId: 2fca13eedffcd242c319ce4b5228aa25d167b46b
    (cherry picked from commit 67d932dd6a073e96ba7a161f311b7d1fb8b0ea19)

--- aswb/java/src/com/google/idea/blaze/java/qsync/ClassFileJavaSourceFinder.java
+++ aswb/java/src/com/google/idea/blaze/java/qsync/ClassFileJavaSourceFinder.java
@@ -14,21 +14,14 @@
  * limitations under the License.
  */
 package com.google.idea.blaze.java.qsync;
-
 import static com.google.common.collect.ImmutableSet.toImmutableSet;
 import static java.util.Arrays.stream;
-
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.collect.ImmutableSet;
-import com.google.idea.blaze.base.model.primitives.WorkspaceRoot;
 import com.google.idea.blaze.base.qsync.QuerySyncManager;
-import com.google.idea.blaze.base.qsync.ReadonlyQuerySyncProject;
-import com.google.idea.blaze.qsync.QuerySyncProjectSnapshot;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.vfs.JarFileSystem;
 import com.intellij.openapi.vfs.LocalFileSystem;
-import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiClass;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiManager;
@@ -38,42 +31,25 @@ import java.nio.file.Path;
 import java.util.Objects;
 import java.util.Set;
 import javax.annotation.Nullable;
-
 /**
  * Finds java source files corresponding to a class file inside a jar file belonging to a
  * dependency.
  */
-public class ClassFileJavaSourceFinder {
-
+public class ClassFileJavaSourceFinder extends CompiledJavaSourceFinderBase {
   private static final Logger logger = Logger.getInstance(ClassFileJavaSourceFinder.class);
 
-  private final Project project;
-  private final QuerySyncManager querySyncManager;
-  private final Path workspaceRoot;
-  private final Path projectPath;
-  private final ClsFileImpl clsFile;
-
   public ClassFileJavaSourceFinder(ClsFileImpl clsFile) {
-    this(
-        clsFile.getProject(),
-        QuerySyncManager.getInstance(clsFile.getProject()),
-        WorkspaceRoot.fromProject(clsFile.getProject()).path(),
-        Path.of(clsFile.getProject().getBasePath()),
-        clsFile);
+    super(clsFile);
   }
 
   @VisibleForTesting
   public ClassFileJavaSourceFinder(
-      Project project,
-      QuerySyncManager querySyncManager,
-      Path workspaceRoot,
-      Path projectPath,
-      ClsFileImpl clsFile) {
-    this.project = project;
-    this.querySyncManager = querySyncManager;
-    this.workspaceRoot = workspaceRoot;
-    this.projectPath = projectPath;
-    this.clsFile = clsFile;
+    Project project,
+    QuerySyncManager querySyncManager,
+    Path workspaceRoot,
+    Path projectPath,
+    ClsFileImpl clsFile) {
+    super(project, querySyncManager, workspaceRoot, projectPath, clsFile);
   }
 
   @Nullable
@@ -89,12 +65,12 @@ public class ClassFileJavaSourceFinder {
     }
     // convert from workspace relative path to PsiFile:
     ImmutableSet<PsiFile> matchingPsiFiles =
-        matchedJarSrcs.stream()
-            .map(workspaceRoot::resolve)
-            .map(LocalFileSystem.getInstance()::findFileByNioFile)
-            .filter(Objects::nonNull)
-            .map(PsiManager.getInstance(project)::findFile)
-            .collect(toImmutableSet());
+      matchedJarSrcs.stream()
+        .map(workspaceRoot::resolve)
+        .map(LocalFileSystem.getInstance()::findFileByNioFile)
+        .filter(Objects::nonNull)
+        .map(PsiManager.getInstance(project)::findFile)
+        .collect(toImmutableSet());
     if (matchingPsiFiles.isEmpty()) {
       return null;
     }
@@ -103,16 +79,16 @@ public class ClassFileJavaSourceFinder {
     // that we select the right source file in case there are files of the same name in multiple
     // packages.
     ImmutableSet<String> qualifiedClassNames =
-        stream(clsFile.getClasses()).map(PsiClass::getQualifiedName).collect(toImmutableSet());
+      stream(clsFile.getClasses()).map(PsiClass::getQualifiedName).collect(toImmutableSet());
     matchingPsiFiles =
-        matchingPsiFiles.stream()
-            .filter(c -> containsClass(c, qualifiedClassNames))
-            .collect(toImmutableSet());
+      matchingPsiFiles.stream()
+        .filter(c -> containsClass(c, qualifiedClassNames))
+        .collect(toImmutableSet());
     if (matchingPsiFiles.size() > 1) {
       logger.warn(
-          String.format(
-              "Warning: found more than 1 matching source file for %s: %s",
-              clsFile, matchingPsiFiles));
+        String.format(
+          "Warning: found more than 1 matching source file for %s: %s",
+          clsFile, matchingPsiFiles));
     }
     return matchingPsiFiles.stream().findAny().orElse(null);
   }
@@ -120,57 +96,22 @@ public class ClassFileJavaSourceFinder {
   private ImmutableSet<Path> findMatchingSourcePathsByFilename(Set<Path> workspaceSourcesForJar) {
     // Find the original source file name(s) for the classes in the file:
     ImmutableSet<String> sourceNamesFromClasses =
-        stream(clsFile.getClasses())
-            .filter(ClsClassImpl.class::isInstance)
-            .map(ClsClassImpl.class::cast)
-            .map(ClsClassImpl::getSourceFileName)
-            .collect(toImmutableSet());
+      stream(clsFile.getClasses())
+        .filter(ClsClassImpl.class::isInstance)
+        .map(ClsClassImpl.class::cast)
+        .map(ClsClassImpl::getSourceFileName)
+        .collect(toImmutableSet());
     // Match the original source file name(s) against the sources for the library jar:
     return workspaceSourcesForJar.stream()
-        .filter(p -> sourceNamesFromClasses.contains(p.getFileName().toString()))
-        .collect(toImmutableSet());
+      .filter(p -> sourceNamesFromClasses.contains(p.getFileName().toString()))
+      .collect(toImmutableSet());
   }
 
   private ImmutableSet<Path> getWorkspaceSources() {
-    VirtualFile file = clsFile.getContainingFile().getVirtualFile();
-    // We expect a .class file inside a .jar file. Get the outer jar file:
-    VirtualFile jar = JarFileSystem.getInstance().getLocalVirtualFileFor(file);
-    if (jar == null) {
-      // Not a jar file - ignore it
-      return ImmutableSet.of();
-    }
-    Path jarPath = jar.toNioPath();
-    if (!jarPath.startsWith(projectPath)) {
-      return ImmutableSet.of();
-    }
-    jarPath = projectPath.relativize(jarPath);
-
-    QuerySyncProjectSnapshot snapshot =
-        querySyncManager
-            .getCurrentSnapshot()
-            .orElse(null);
-    if (snapshot == null) {
-      return ImmutableSet.of();
-    }
-
     final var pathResolver = querySyncManager.assertProjectLoaded().getProjectPathResolver();
-
-    return snapshot
-        .getArtifactIndex()
-        .getInfoForJarArtifact(jarPath)
-        .stream()
-        .flatMap(it -> it.sources().stream())
-        .map(pathResolver::resolve)
-        .collect(toImmutableSet());
-  }
-
-  // private ImmutableSet<PsiFile> filterByExpectedQualified
-
-  private static boolean containsClass(PsiFile file, Set<String> qualifiedClassName) {
-    return stream(file.getChildren())
-        .filter(PsiClass.class::isInstance)
-        .map(PsiClass.class::cast)
-        .map(PsiClass::getQualifiedName)
-        .anyMatch(qualifiedClassName::contains);
+    return getJavaArtifactInfos().stream()
+      .flatMap(it -> it.sources().stream())
+      .map(pathResolver::resolve)
+      .collect(toImmutableSet());
   }
 }
