8db1aff61f89cd3ccb3081a697ef2f3e505faa27
Alexander Demurov <ademurov@google.com>
2025-10-28 20:27:33 +0000

    Stop using ddmlib `FakeAdbRule` in `adt/idea/streaming`
    Bug: 443319528
    Test: these are tests
    Change-Id: I03c301f857ad38ec390f4f33317d7a643bc36dff
    
    GitOrigin-RevId: bd3757dd066d038eba10ed040b7d5e18a9496e2f
    
    Space-RevId: 8d2cb34b1bcb510d577b7aa7d55690edb0b268ee
    (cherry picked from commit 7aa8dfc9a1aa426efa5124634e03f8b12ff6792d)

---
+++ android-adb/testUtil/com/android/adblib/ddmlibcompatibility/testutils/AdbLibApplicationServiceRule.kt
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.adblib.ddmlibcompatibility.testutils
+
+import com.android.adblib.testingutils.FakeAdbServerRule
+import com.android.fakeadbserver.FakeAdbServer
+import com.android.tools.idea.adblib.AdbLibApplicationService
+import org.junit.runner.Description
+import org.junit.runners.model.Statement
+
+/**
+ * The test rule that combines [FakeAdbServerRule] and [InitAndroidDebugBridgeRule] to allow
+ * `AdbLibApplicationService` to be used in tests.
+ */
+class AdbLibApplicationServiceRule(
+  configure: (FakeAdbServer.Builder.() -> FakeAdbServer.Builder)? = null
+) : FakeAdbServerRule(TODO()) { // TODO android merge
+
+  override fun before() {
+    super.before()
+    // Instantiate `AdbLibApplicationService`, which initializes adblib's application-level
+    // components such as `AdbSession` and `ChannelProvider`.
+    AdbLibApplicationService.instance
+  }
+
+  override fun apply(base: Statement, description: Description): Statement {
+    return super.apply(
+      InitAndroidDebugBridgeRule(alsoCreateBridge = true) { adbServer.port }
+        .apply(base, description),
+      description,
+    )
+  }
+}
