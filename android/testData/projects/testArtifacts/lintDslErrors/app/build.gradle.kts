d1359bd106cf091010d1d42e37f7eb42f7da8513
Madalin Valceleanu <vmadalin@google.com>
2025-10-28 18:03:59 +0000

    Update to KGP 2.3.0-Beta2 for tests (tools/adt/idea)
    Bug: N/A (Regular update)
    Test: Updated
    Change-Id: Ie448d2da9883c9f612eb8af51e482d2c7c6ca661
    
    GitOrigin-RevId: 5686699fffcc2e438f58f01e40fe653cc40579ec
    
    Space-RevId: f2b0904162b807a3859647de2e4e5d781bc099dd
    (cherry picked from commit cc96b484e8192851864e9562c570523fdab675f2)

--- android/testData/projects/testArtifacts/lintDslErrors/app/build.gradle.kts
+++ android/testData/projects/testArtifacts/lintDslErrors/app/build.gradle.kts
@@ -4,6 +4,12 @@ plugins {
     alias(libs.plugins.compose.compiler)
 }
 
+kotlin {
+  compilerOptions {
+    jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_1_8
+  }
+}
+
 android {
     namespace = "com.example.myapplication"
     compileSdkVersion(34)
@@ -53,9 +59,6 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
     }
     buildFeatures {
         compose = true
