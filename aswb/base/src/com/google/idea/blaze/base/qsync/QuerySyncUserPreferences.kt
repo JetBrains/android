afa6966614d5285c885af12c4b9e5dd020179303
Yuriy Solodkyy <solodkyy@google.com>
2025-10-29 11:36:59 +0000

    Introduce QuerySyncUserPreferences
    and begin moving  fetching of user preferences to
    `QuerySyncUserPreferencesProvider`. This is to: (1) avoid cluttering
    query sync with preference loading implementation details and (2) to
    allow changing sources of truth for user preferences related to the
    query sync.
    
    Bug: n/a
    Test: n/a (existing)
    Change-Id: I460e8dca8a4d5740206099e07c7a910194b52583
    
    GitOrigin-RevId: 58567a9e409e1f54a054f51c60f8afafc421ebe2
    
    Space-RevId: bdcc837d15064e1e0bf98e65657984ac5b409c4f
    (cherry picked from commit 3dd8f59919fe778c413e24e228cbe9079c44f118)

---
+++ aswb/base/src/com/google/idea/blaze/base/qsync/QuerySyncUserPreferences.kt
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2025 The Bazel Authors. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.idea.blaze.base.qsync
+
+import com.google.idea.blaze.base.projectview.ProjectViewManager
+import com.google.idea.blaze.base.projectview.section.sections.EnableCodeAnalysisOnSyncSection
+import com.intellij.openapi.components.Service
+import com.intellij.openapi.components.service
+import com.intellij.openapi.project.Project
+import kotlin.jvm.optionals.getOrDefault
+
+interface QuerySyncUserPreferences {
+  val enableCodeAnalysisOnSync: Boolean
+}
+
+@Service(Service.Level.PROJECT)
+class QuerySyncUserPreferencesProvider(private val project: Project) {
+  val userPreferences: QuerySyncUserPreferences = object : QuerySyncUserPreferences {
+    override val enableCodeAnalysisOnSync: Boolean
+      get() =
+        ProjectViewManager.getInstance(project).projectViewSet?.getScalarValue(EnableCodeAnalysisOnSyncSection.KEY)?.getOrDefault(false)
+        ?: false
+  }
+
+  companion object {
+    @JvmStatic
+    fun getInstance(project: Project): QuerySyncUserPreferencesProvider = project.service()
+  }
+}
