b7fe7892bdb2c2c4da0addb4bef3599b259b4ee5
David Belcher <davidbelcher@google.com>
2025-11-04 13:40:02 +0000

    Keep min AGP version shown for Journey file template in sync with latest
    Test suites in AGP are supported from AGP 9.0.0-alpha13 onwards.
    However, if there is a more recent alpha version available, or a stable
    9.0.0 version, we should use that instead.
    
    Bug: 448352524
    Test: Added
    Change-Id: Iba71b902d8dc1d3c84a9a32aada084c67f573155
    
    GitOrigin-RevId: 6bc61cbd1be54e60636ac7e255af244801f7ad6a
    
    Space-RevId: 9f0dd9fc8774829b1cd5b53249d02c5bc404e07a
    (cherry picked from commit bf783a770ba660517812027b900f2688901e5291)

---
+++ android-npw/testSrc/com/android/tools/idea/npw/NpwUiUtilsTest.kt
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.npw
+
+import com.android.ide.common.repository.AgpVersion
+import kotlin.test.assertEquals
+import org.junit.Test
+
+class NpwUiUtilsTest {
+
+  @Test
+  fun getMinimumAgpVersionForTestSuiteSupport_returns_9_0_0_whenVersionIsLatestKnown() {
+    assertEquals("9.0.0", getMinimumAgpVersionForTestSuiteSupport(AgpVersion.parse("9.0.0")))
+  }
+
+  @Test
+  fun getMinimumAgpVersionForTestSuiteSupport_returns_9_0_0_whenNewerVersionAvailable() {
+    assertEquals("9.0.0", getMinimumAgpVersionForTestSuiteSupport(AgpVersion.parse("9.0.1")))
+  }
+
+  @Test
+  fun getMinimumAgpVersionForTestSuiteSupport_returnsLatestAlphaVersion_ifLatestVersionIsAlpha() {
+    assertEquals(
+      "9.0.0-alpha14",
+      getMinimumAgpVersionForTestSuiteSupport(AgpVersion.parse("9.0.0-alpha14")),
+    )
+  }
+
+  @Test
+  fun getMinimumAgpVersionForTestSuiteSupport_returnsDefaultAlphaVersion_ifLatestVersionIsOlder() {
+    assertEquals(
+      "9.0.0-alpha13",
+      getMinimumAgpVersionForTestSuiteSupport(AgpVersion.parse("8.13.0")),
+    )
+  }
+}
