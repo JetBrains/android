bd51642c3f7f5684693691b764cc53a887c4b1d1
Denalex Orakwue <corakwue@google.com>
2025-11-09 04:52:26 +0000

    Add implementation for GlassesPairingUsageTracker
    Test: n/a
    Bug: b/36852542
    Change-Id: I9cf672aa08271f254684ab5c94bb8332de97146c
    
    GitOrigin-RevId: cdedf4db08ef0f53a3ea58133e8f0c016ab804ea
    
    Space-RevId: a20143a2c9e7a3b56a226a8fb90e1108ee43563c
    (cherry picked from commit f2b12304b789429859e64851f0413587f257523e)

--- glasses-pairing/src/com/android/tools/idea/glassespairing/GlassesPairingUsageTracker.kt
+++ glasses-pairing/src/com/android/tools/idea/glassespairing/GlassesPairingUsageTracker.kt
@@ -15,6 +15,20 @@
  */
 package com.android.tools.idea.glassespairing
 
-class GlassesPairingUsageTracker {
-  fun log() {}
-}
+import com.android.tools.analytics.UsageTracker
+import com.google.wireless.android.sdk.stats.AndroidStudioEvent
+import com.google.wireless.android.sdk.stats.GlassesPairingEvent
+
+// TODO android merge
+//object GlassesPairingUsageTracker {
+//  fun log(eventKind: GlassesPairingEvent.EventKind) {
+//    val event = GlassesPairingEvent.newBuilder().setKind(eventKind).build()
+//
+//    val builder =
+//      AndroidStudioEvent.newBuilder()
+//        .setKind(AndroidStudioEvent.EventKind.GLASSES_PAIRING_EVENT)
+//        .setGlassesPairingEvent(event)
+//
+//    UsageTracker.log(builder)
+//  }
+//}
