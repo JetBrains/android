c257a426f94e89bdca2967a80be586ccfcaeb504
Matthew Gharrity <gharrma@google.com>
2025-11-14 14:20:10 -0800

    Defer to platform for isAllowed() in analytics
    Background: we have a platform patch that intercepts calls to
    UsageStatisticsPersistenceComponent.isAllowed() and redirects them to
    AndroidStudioAnalyticsImpl.isAllowed(). However, this platform patch
    is unnecessary because the platform consents and the Android plugin
    consents are already kept in sync at all times. The syncing happens in
    AndroidStudioAnalyticsImpl.initialize*() at app startup and continues
    in AndroidStudioAnalyticsImpl.update*() whenever the platform consents
    are modified by the user in the Data Sharing settings dialog. To be
    extra sure, I've added a new unit test which passes both before and
    after this change.
    
    Side note: the platform consents are treated as the source of truth
    (ever since ag/4768793). One could wonder whether the syncing should
    happen in the opposite direction eventually, but that is outside the
    scope of this change.
    
    Bug: 460518368
    Test: AnalyticsSettingsUiTest.testConsistencyWithPlatformConsents()
    Change-Id: I1ae19c12c5fc9d67aca4be83cf7a3652cbca738c
    
    GitOrigin-RevId: ddab146244fa93cf343d6f137f19997a0640ff53
    
    Space-RevId: ea90110008a55c9aec95e8ff232ca7eb31db70d1
    (cherry picked from commit 57620a286d40dd7c35569fea48e03561de1fc378)

--- android/testSrc/com/android/tools/idea/analytics/AnalyticsSettingsUiTest.kt
+++ android/testSrc/com/android/tools/idea/analytics/AnalyticsSettingsUiTest.kt
@@ -22,31 +22,31 @@ import com.android.tools.idea.IdeInfo
 import com.android.tools.idea.testing.AndroidProjectRule
 import com.google.common.truth.Truth.assertThat
 //import com.intellij.analytics.AndroidStudioAnalytics
+import com.intellij.ide.ConsentOptionsProvider
 import com.intellij.ide.gdpr.ConsentConfigurable
+import com.intellij.openapi.components.service
+import com.intellij.testFramework.ApplicationRule
 import com.intellij.testFramework.EdtRule
 import com.intellij.testFramework.RunsInEdt
+import javax.swing.JCheckBox
 import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.RuleChain
-import javax.swing.JCheckBox
 
 /**
  * Tests the data sharing checkbox in the settings/preferences ui.
  */
 @RunsInEdt
 class AnalyticsSettingsUiTest {
-  private val projectRule = AndroidProjectRule.inMemory()
-
   @get:Rule
-  val ruleChain: RuleChain = RuleChain.outerRule(projectRule).around(EdtRule())
+  val ruleChain: RuleChain = RuleChain.outerRule(ApplicationRule()).around(EdtRule())
 
   @Test
   fun testSettingsUi() {
     if (!IdeInfo.getInstance().isAndroidStudio) return
 
-    AnalyticsSettings.setInstanceForTest(AnalyticsSettingsData().apply {
-      optedIn = false
-    })
+    AnalyticsSettings.setInstanceForTest(AnalyticsSettingsData())
+    service<ConsentOptionsProvider>().isSendingUsageStatsAllowed = false
 
     val configurable = ConsentConfigurable()
     val component = configurable.createComponent()
@@ -60,6 +60,38 @@ class AnalyticsSettingsUiTest {
 
     checkbox.isSelected = false
     configurable.apply()
+    assertThat(AnalyticsSettings.optedIn).isFalse()
+  }
+
+  @Test
+  fun testConsistencyWithPlatformConsents() {
+    // Background: we reuse the platform "Data Sharing" UI and consent ID, but we also have our own
+    // mechanism for storing and querying the data-sharing consent (see AnalyticsSettings.optedIn).
+    // That means there are two different places where the data-sharing consent gets stored:
+    //
+    //   1. ConsentOptions (platform) => $IDE_CONFIG_DIR/consentOptions/accepted
+    //   2. AnalyticsSettings (Android plugin) => $USER_HOME/.android/analytics.settings
+    //
+    // It is important that these two stay in sync, otherwise the Data Sharing UI would not
+    // correctly reflect the opt-in status on the Android plugin side.
+
+    // Begin in a state where the platform consent as granted but the Studio consent as not.
+    val platformConsentProvider = service<ConsentOptionsProvider>()
+    platformConsentProvider.isSendingUsageStatsAllowed = true
+    AnalyticsSettings.setInstanceForTest(AnalyticsSettingsData())
+    assertThat(AnalyticsSettings.optedIn).isFalse()
+
+    // When AnalyticsSettings gets initialized (normally during app startup), its state should
+    // reset to the platform source-of-truth. This behavior was established in 2018 with
+    // http://ag/4768793 (Change I34cbf26df). One could imagine syncing the opt-in status
+    // in the other direction, but the current behavior works well enough as long as Android Studio
+    // is the only surface in which the opt-in status can be changed.
+
+    //AndroidStudioAnalytics.getInstance().initializeAndroidStudioUsageTrackerAndPublisher()
+    assertThat(AnalyticsSettings.optedIn).isTrue()
+
+    // The Android plugin opt-in status should adapt to changes in the platform opt-in status.
+    platformConsentProvider.isSendingUsageStatsAllowed = false
     assertThat(AnalyticsSettings.optedIn).isFalse()
   }
 }
