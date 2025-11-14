0ff5c3b8e03e0b78a133770c452cd4c2d236ec09
Matthew Gharrity <gharrma@google.com>
2025-11-14 13:19:31 -0800

    Add test coverage for HighlightingStats
    Bug: 460518368
    Test: added
    Change-Id: I099dad7414874bf52f6992ceca3cb2f3a847bc41
    
    GitOrigin-RevId: 99645328bd7e2d1919952a75607c31bb963ea6c1
    
    Space-RevId: 22f28eba09065ef347807f7d7a11b353a826497b
    (cherry picked from commit 3c146560f647083808d5c8d4276ca465c86ac66a)

---
+++ android/testSrc/com/android/tools/idea/analytics/HighlightingStatsTest.kt
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.analytics
+
+import com.android.testutils.VirtualTimeScheduler
+import com.android.tools.analytics.HighlightingStats
+import com.android.tools.analytics.TestUsageTracker
+import com.android.tools.analytics.UsageTracker
+import com.google.common.truth.Truth.assertThat
+import com.google.wireless.android.sdk.stats.AndroidStudioEvent
+import com.google.wireless.android.sdk.stats.EditorFileType
+import com.intellij.testFramework.fixtures.BasePlatformTestCase
+
+// This test should ideally live in the same module as HighlightingStats, but it cannot currently
+// because it needs AndroidStudioAnalyticsImpl to be active from intellij.android.core.
+class HighlightingStatsTest : BasePlatformTestCase() {
+
+  fun testSimple() {
+    val usageTracker = TestUsageTracker(VirtualTimeScheduler())
+    val getHighlightingEvents = {
+      HighlightingStats.getInstance().reportHighlightingStats() // Flush pending histogram.
+      usageTracker.usages.filter { usage ->
+        usage.studioEvent.kind == AndroidStudioEvent.EventKind.EDITOR_HIGHLIGHTING_STATS
+      }
+    }
+    UsageTracker.setWriterForTest(usageTracker)
+    try {
+      // The latency histogram should be empty initially.
+      assertThat(getHighlightingEvents()).isEmpty()
+
+      myFixture.configureByText("Main.kt", "fun main() {}")
+      myFixture.checkHighlighting()
+
+      // The latency histogram should have 1 datapoint for the Kotlin file.
+      val events = getHighlightingEvents()
+      assertThat(events).hasSize(1)
+      val stats = events.single().studioEvent.editorHighlightingStats
+      assertThat(stats.byFileTypeList).hasSize(1)
+      assertThat(stats.byFileTypeList.single().fileType).isEqualTo(EditorFileType.KOTLIN)
+
+      // No new events should be logged now that the latency histogram is empty again.
+      assertThat(getHighlightingEvents()).hasSize(1)
+    }
+    finally {
+      UsageTracker.cleanAfterTesting()
+    }
+  }
+}
