6b24897a4fe3a39b4cdb289631063fed2b5df468
venkicodes <venkicodes@google.com>
2025-11-19 13:24:45 +0000

    Add unit tests for PreviewItemPanel
    This CL adds a new test class PreviewItemPanelTest to verify the UI logic of PreviewItemPanel.
    
    Bug: N/A
    Test: PreviewItemPanelTest
    Change-Id: Iaf9228a446688c9dc50c6d709267dcfc242b2a76
    
    GitOrigin-RevId: d94aa8763976413e0f33997f4b4c2bb75893d7f0
    
    Space-RevId: 7201afe272c7cfe6adcdbcc800b5b18e45e2c20f
    (cherry picked from commit a29c46f1fef6e909c7b5d8bdc42b7c1d25cd0f5a)

---
+++ screenshot-testing/testSrc/com/android/screenshottest/ui/PreviewItemPanelTest.kt
@@ -0,0 +1,184 @@
+/*
+ * Copyright (C) 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.screenshottest.ui
+
+import com.android.tools.idea.testartifacts.instrumented.testsuite.model.AndroidTestCaseResult
+import com.android.tools.idea.testartifacts.instrumented.testsuite.view.ScreenshotViewType
+import com.android.tools.idea.testing.AndroidProjectRule
+import com.intellij.testFramework.PlatformTestUtil
+import com.intellij.testFramework.runInEdtAndWait
+import com.intellij.ui.components.JBLabel
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
+import org.junit.Rule
+import org.junit.Test
+import java.awt.Container
+
+class PreviewItemPanelTest {
+
+  @get:Rule
+  val projectRule = AndroidProjectRule.inMemory()
+
+  @Test
+  fun verifyInitialization() = runInEdtAndWait {
+    val details = PreviewDetails(
+      testId = "test.id",
+      className = "TestClass",
+      methodName = "testMethod",
+      previewName = "preview",
+      testResult = AndroidTestCaseResult.PASSED
+    )
+    val panel = PreviewItemPanel(details)
+    assertEquals(details, panel.previewData)
+  }
+
+  @Test
+  fun verifyDetailsPanelHidden() = runInEdtAndWait {
+    val details = PreviewDetails(
+      testId = "test.id",
+      className = "TestClass",
+      methodName = "testMethod",
+      previewName = "preview",
+      testResult = AndroidTestCaseResult.PASSED
+    )
+    val panel = PreviewItemPanel(details, showDetails = false)
+
+    // Expect only 1 component (the image panel)
+    assertEquals(1, panel.componentCount)
+  }
+
+  @Test
+  fun verifyDetailsLabels() = runInEdtAndWait {
+    val details = PreviewDetails(
+      testId = "test.id",
+      className = "TestClass",
+      methodName = "testMethod",
+      previewName = "MyPreview",
+      testResult = AndroidTestCaseResult.FAILED,
+      diffPercent = "0.01" // 99% match
+    )
+    val panel = PreviewItemPanel(details, showDetails = true)
+
+    val labels = findAllLabels(panel)
+
+    val matchLabel = labels.find { it.text == "99.00%" }
+    val nameLabel = labels.find { it.text == "MyPreview" }
+
+    assertNotNull("Match label '99.00%' should exist", matchLabel)
+    assertNotNull("Name label 'MyPreview' should exist", nameLabel)
+  }
+
+  @Test
+  fun verifyPlaceholderForMissingNewImage() = runInEdtAndWait {
+    val details = PreviewDetails(
+      testId = "test.id",
+      className = "TestClass",
+      methodName = "testMethod",
+      previewName = "preview",
+      testResult = AndroidTestCaseResult.FAILED,
+      srcImagePath = null
+    )
+    val panel = PreviewItemPanel(details)
+    panel.showImageForView(ScreenshotViewType.NEW)
+
+    PlatformTestUtil.dispatchAllEventsInIdeEventQueue()
+
+    val label = findLabel(panel)
+    assertEquals("No New Image", label?.text)
+  }
+
+  @Test
+  fun verifyPlaceholderForPassedDiff() = runInEdtAndWait {
+    val details = PreviewDetails(
+      testId = "test.id",
+      className = "TestClass",
+      methodName = "testMethod",
+      previewName = "preview",
+      testResult = AndroidTestCaseResult.PASSED,
+      diffImagePath = null
+    )
+    val panel = PreviewItemPanel(details)
+    panel.showImageForView(ScreenshotViewType.DIFF)
+
+    PlatformTestUtil.dispatchAllEventsInIdeEventQueue()
+
+    val label = findLabel(panel)
+    assertEquals("No Difference", label?.text)
+  }
+
+  @Test
+  fun verifyPlaceholderForFailedDiff() = runInEdtAndWait {
+    val details = PreviewDetails(
+      testId = "test.id",
+      className = "TestClass",
+      methodName = "testMethod",
+      previewName = "preview",
+      testResult = AndroidTestCaseResult.FAILED,
+      diffImagePath = null
+    )
+    val panel = PreviewItemPanel(details)
+    panel.showImageForView(ScreenshotViewType.DIFF)
+
+    PlatformTestUtil.dispatchAllEventsInIdeEventQueue()
+
+    val label = findLabel(panel)
+    assertEquals("No Diff Image", label?.text)
+  }
+
+  @Test
+  fun verifyPlaceholderForReferenceImage() = runInEdtAndWait {
+    val details = PreviewDetails(
+      testId = "test.id",
+      className = "TestClass",
+      methodName = "testMethod",
+      previewName = "preview",
+      testResult = AndroidTestCaseResult.PASSED,
+      destImagePath = null
+    )
+    val panel = PreviewItemPanel(details)
+    panel.showImageForView(ScreenshotViewType.REFERENCE)
+
+    PlatformTestUtil.dispatchAllEventsInIdeEventQueue()
+
+    val label = findLabel(panel)
+    assertEquals("No Reference Image", label?.text)
+  }
+
+  private fun findLabel(container: Container): JBLabel? {
+    for (component in container.components) {
+      if (component is JBLabel) {
+        return component
+      }
+      if (component is Container) {
+        val label = findLabel(component)
+        if (label != null) return label
+      }
+    }
+    return null
+  }
+
+  private fun findAllLabels(container: Container): List<JBLabel> {
+    val result = mutableListOf<JBLabel>()
+    for (component in container.components) {
+      if (component is JBLabel) {
+        result.add(component)
+      } else if (component is Container) {
+        result.addAll(findAllLabels(component))
+      }
+    }
+    return result
+  }
+}
