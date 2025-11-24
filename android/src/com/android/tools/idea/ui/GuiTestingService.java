95155d50160e07937406ccae9db49ed72d9ade84
Matthew Gharrity <gharrma@google.com>
2025-11-24 10:41:24 -0800

    Convert GuiTestingService to light service
    This is an alternative fix to whatever issue JetBrains was solving in
    commit https://github.com/JetBrains/android/commit/af88889156.
    
    Bug: not filed
    Test: existing
    Change-Id: I314a579ca32a50d77d9ae4341bf297b037e461e0
    
    GitOrigin-RevId: f5502423570798642ae2ad69a8d6aa8397873b19
    
    Space-RevId: 8501ba04a309daa16d3a3b13a27b16fbaf4ff47e
    (cherry picked from commit 6f29ad0894c2d00cef334d62b07043f3c69cf453)

--- android/src/com/android/tools/idea/ui/GuiTestingService.java
+++ android/src/com/android/tools/idea/ui/GuiTestingService.java
@@ -17,10 +17,12 @@ package com.android.tools.idea.ui;
 
 import com.android.annotations.concurrency.GuardedBy;
 import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.components.Service;
 import com.intellij.openapi.extensions.ExtensionPointName;
 import com.intellij.openapi.util.Key;
 
-public class GuiTestingService {
+@Service
+public final class GuiTestingService {
   public static final Key<Runnable> EXECUTE_BEFORE_PROJECT_BUILD_IN_GUI_TEST_KEY = Key.create("gui.test.execute.before.build");
   public static final Key<String> GRADLE_BUILD_OUTPUT_IN_GUI_TEST_KEY = Key.create("gui.test.gradle.build.output");
 
