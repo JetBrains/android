96d325ab76fddfe955bf03cde81d4cfa3f5a5b47
Sergey Prigogin <sprigogin@google.com>
2025-11-25 12:29:15 -0800

    Move HumanReadableTest to the same package as HumanReadable
    Bug: N/A
    Test: existing
    Change-Id: I63f45959c65f95d3cc8533f0706c2ca190b9ff48
    
    GitOrigin-RevId: 868338d1d898d6c3f3c4fa832b34c378af2b2333
    
    Space-RevId: 3b231bb31cfb31f8d60a8c6f86254aed12ceed54
    (cherry picked from commit 38ddaac0fc141cf772c03f25a7ae99bb9b5a6945)

--- adt-ui/src/test/java/com/android/tools/adtui/util/HumanReadableTest.kt
+++ adt-ui/src/test/java/com/android/tools/adtui/util/HumanReadableTest.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2020 The Android Open Source Project
+ * Copyright (C) 2025 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,15 +13,12 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.android.tools.adtui
+package com.android.tools.adtui.util
 
-import com.android.tools.adtui.util.getHumanizedSize
 import com.google.common.truth.Truth.assertThat
 import org.junit.Test
 
-/**
- * Tests for functions defined in `HumanReadable.kt`
- */
+/** Tests for functions defined in `HumanReadable.kt`. */
 class HumanReadableTest {
   @Test
   fun testGetHumanizedSize() {
