9ec35c381b502ff0ce1d09695232eb52af6f5261
venkicodes <venkicodes@google.com>
2025-11-28 14:56:46 +0000

    Refactor PreviewItemPanel and PreviewDetailsPanel
    Extracts hardcoded string literals and dimensions into private constants in PreviewItemPanel and PreviewDetailsPanel.
    This improves code maintainability and reduces duplication of placeholder text and error messages.
    
    Bug: N/A
    Test: Existing. PreviewItemPanelTest and PreviewDetailsPanelTest
    Change-Id: I7012276c3c06fb981497cf705b6e990948d3cd9f
    
    GitOrigin-RevId: bc2fed68ec9c81c03e05c9bf39b1e7ff0c1426ad
    
    Space-RevId: 5b1eb8398a8eaf6d029b37b65af872aad6c79663
    (cherry picked from commit 2057846af990b135846a8a0b8147f7158d00e5d7)

---
+++ screenshot-testing/src/com/android/screenshottest/ui/UiConstants.kt
@@ -0,0 +1,26 @@
+/*
+ * Copyright (C) 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.screenshottest.ui
+
+// User-facing text for various image loading states.
+internal const val NO_NEW_IMAGE_TEXT = "No New Image"
+internal const val NO_DIFF_IMAGE_TEXT = "No Diff Image"
+internal const val NO_REF_IMAGE_TEXT = "No Reference Image"
+internal const val NO_DIFFERENCE_TEXT = "No Difference"
+internal const val WAITING_FOR_IMAGE_TEXT = "Waiting for image..."
+internal const val COULD_NOT_LOAD_IMAGE_TEXT = "Couldn't load image"
+internal const val DEFAULT_MATCH_PERCENTAGE = "0.00%"
+internal const val UNNAMED_FUNCTION_TEXT = "Unnamed Function"
