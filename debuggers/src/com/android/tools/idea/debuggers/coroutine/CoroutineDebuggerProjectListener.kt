6b38d37f3a0ec4454f8402fd4248194b392b0608
Maksim Zuev <maksim.zuev@jetbrains.com>
2025-12-02 16:37:42 +0100

    [rd debugger] IJPL-221120 LUX coroutines tab
    GitOrigin-RevId: a34c02e316247329a2a726732f5c576271684872

--- debuggers/src/com/android/tools/idea/debuggers/coroutine/CoroutineDebuggerProjectListener.kt
+++ debuggers/src/com/android/tools/idea/debuggers/coroutine/CoroutineDebuggerProjectListener.kt
@@ -22,8 +22,6 @@ import com.intellij.openapi.startup.ProjectActivity
 import com.intellij.xdebugger.XDebugProcess
 import com.intellij.xdebugger.XDebuggerManager
 import com.intellij.xdebugger.XDebuggerManagerListener
-import com.intellij.xdebugger.impl.XDebugSessionImpl
-import com.intellij.platform.debugger.impl.shared.proxy.XDebugManagerProxy
 import org.jetbrains.android.AndroidStartupManager.ProjectDisposableScope
 import org.jetbrains.kotlin.idea.debugger.coroutine.DebuggerConnection
 
@@ -55,12 +53,9 @@ private class CoroutineDebuggerListener(private val project: Project) : XDebugge
 
     val debuggerConnection = DebuggerConnection(project, null, null, shouldAttachCoroutineAgent = false, alwaysShowPanel = true)
 
-    val sessionId = (debugProcess.session as XDebugSessionImpl).id
-    val sessionProxy = XDebugManagerProxy.getInstance().findSessionProxy(project, sessionId) ?: return
-
     // creating the [DebuggerConnection] object does nothing on its own. In order for the panel to be created
     // we need to forward the "processStarted" call to the Kotlin plugin DebuggerConnection component,
     // which is responsible for creating the Coroutines Debugger panel
-    debuggerConnection.sessionStarted(sessionProxy)
+    debuggerConnection.processStarted(debugProcess)
   }
 }
