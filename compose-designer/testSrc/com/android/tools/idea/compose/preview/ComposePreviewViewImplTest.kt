fa6ebfc1f0d21a872b179d14f6955db576b08350
Leyla Mizrahi <leylamizrahi@google.com>
2025-12-04 04:39:53 -0800

    Revert "Lowercase C in composable"
    This reverts commit 751aec78ea76ae92012bc8c9eb9702e0b659ff73.
    
    Reason for revert: should remain capitilized
    
    Bug: 463949278
    Change-Id: I480e688d3fce0cb85ea12a3716c7473c5c22341e
    
    GitOrigin-RevId: cf6f8b82ac49554ebac996174632a4358df069e1
    
    Space-RevId: 20e59331421f8b90bf384330224d33741857c9b6
    (cherry picked from commit a3bf3a19ae3a1a9499cf1da7ee27dc0af62feead)

--- compose-designer/testSrc/com/android/tools/idea/compose/preview/ComposePreviewViewImplTest.kt
+++ compose-designer/testSrc/com/android/tools/idea/compose/preview/ComposePreviewViewImplTest.kt
@@ -497,7 +497,7 @@ class ComposePreviewViewImplTest(generatePreviewFlag: Boolean, screenshotToCodeF
     checkEmptyPreviewState(
       showAutoGenerateAction = true,
       showScreenshotToAction = StudioFlags.COMPOSE_PREVIEW_SCREENSHOT_TO_CODE.get(),
-      expectedAutoGenerateActionText = "Generate Preview for composable",
+      expectedAutoGenerateActionText = "Generate Preview for Composable",
     )
   }
 
@@ -516,7 +516,7 @@ class ComposePreviewViewImplTest(generatePreviewFlag: Boolean, screenshotToCodeF
   private fun checkEmptyPreviewState(
     showAutoGenerateAction: Boolean,
     showScreenshotToAction: Boolean,
-    expectedAutoGenerateActionText: String = "Generate Preview for composable",
+    expectedAutoGenerateActionText: String = "Generate Preview for Composable",
   ) = runBlocking {
     previewView.hasRendered = true
     previewView.hasContent = false
