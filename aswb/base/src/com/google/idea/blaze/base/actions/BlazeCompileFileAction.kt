695c52bcf49fcc92b0386489ce27d3ae03e86220
Yuriy Solodkyy <solodkyy@google.com>
2025-12-08 16:34:18 +0000

    Remove more query sync checks
    Bug: n/a
    Test: n/a
    Change-Id: Ic9145bccf39ed23095d732b61c365b68af37d370
    
    GitOrigin-RevId: 009675bea001f663f176716c9ef983c792f8395f
    
    Space-RevId: 9d5dc11b671bdfff4eea9db033c390ca3941657a
    (cherry picked from commit 060fca25adb12cad590c80d070df8f0c88536215)

--- aswb/base/src/com/google/idea/blaze/base/actions/BlazeCompileFileAction.kt
+++ aswb/base/src/com/google/idea/blaze/base/actions/BlazeCompileFileAction.kt
@@ -24,11 +24,8 @@ import com.google.idea.blaze.base.model.primitives.Label
 import com.google.idea.blaze.base.model.primitives.WorkspaceRoot
 import com.google.idea.blaze.base.qsync.QuerySyncManager
 import com.google.idea.blaze.base.qsync.action.BuildDependenciesHelper
-import com.google.idea.blaze.base.qsync.action.BuildDependenciesHelperSelectTargetPopup
 import com.google.idea.blaze.base.qsync.action.BuildDependenciesHelperSelectTargetPopup.createDisambiguateTargetPrompt
 import com.google.idea.blaze.base.qsync.action.TargetDisambiguationAnchors
-import com.google.idea.blaze.base.settings.Blaze
-import com.google.idea.blaze.base.settings.BlazeImportSettings
 import com.google.idea.blaze.base.targetmaps.SourceToTargetMap
 import com.google.idea.common.actions.ActionPresentationHelper
 import com.intellij.openapi.actionSystem.AnActionEvent
@@ -53,41 +50,33 @@ internal class BlazeCompileFileAction : BlazeProjectAction() {
   }
 
   private fun isEnabled(project: Project, e: AnActionEvent): Boolean {
-    if (Blaze.getProjectType(project) == BlazeImportSettings.ProjectType.QUERY_SYNC) {
-      val vfs = e.getData(CommonDataKeys.VIRTUAL_FILE_ARRAY)?.takeUnless { it.isEmpty() }?.toList() ?: return false
-      val querySyncManager = QuerySyncManager.getInstance(project)
-      return querySyncManager.getLoadedProject().isPresent
-        &&
-             // TODO: b/411054914 - Build dependencies actions should not get disabled when not in sync/not in a project target and instead
-             // they should automatically trigger sync.
-             querySyncManager
-               .getTargetsToBuildByPaths(WorkspaceRoot.virtualFilesToWorkspaceRelativePaths(project, vfs))
-          .asSequence()
-          .flatMap { it.targets }.any()
-    }
-    return getTargets(e).isNotEmpty()
+    val vfs = e.getData(CommonDataKeys.VIRTUAL_FILE_ARRAY)?.takeUnless { it.isEmpty() }?.toList() ?: return false
+    val querySyncManager = QuerySyncManager.getInstance(project)
+    return querySyncManager.getLoadedProject().isPresent
+      &&
+           // TODO: b/411054914 - Build dependencies actions should not get disabled when not in sync/not in a project target and instead
+           // they should automatically trigger sync.
+           querySyncManager
+             .getTargetsToBuildByPaths(WorkspaceRoot.virtualFilesToWorkspaceRelativePaths(project, vfs))
+        .asSequence()
+        .flatMap { it.targets }.any()
   }
 
   override fun actionPerformedInBlazeProject(project: Project, e: AnActionEvent) {
     val files = e.getData(CommonDataKeys.VIRTUAL_FILE_ARRAY)?.filterNotNull()?.takeUnless { it.isEmpty() } ?: return
 
-    if (Blaze.getProjectType(project) == BlazeImportSettings.ProjectType.QUERY_SYNC) {
-      val buildDependenciesHelper = BuildDependenciesHelper(project)
-      val querySyncActionStats = QuerySyncActionStatsScope.createForFiles(project, javaClass, e, ImmutableList.copyOf(files))
-      buildDependenciesHelper.determineTargetsAndRun(
-        workspaceRelativePaths = WorkspaceRoot.virtualFilesToWorkspaceRelativePaths(project, files),
-        disambiguateTargetPrompt = createDisambiguateTargetPrompt({ popup -> popup.showCenteredInCurrentWindow(project) }),
-        targetDisambiguationAnchors = TargetDisambiguationAnchors.NONE,
-        querySyncActionStats = querySyncActionStats,
-      ) { labels ->
-        BlazeBuildService.getInstance(project)
-          .buildFileForLabels(files.joinToString(", ", limit = 2), ImmutableSet.copyOf(labels))
-          .asDeferred()
-      }
-      return
+    val buildDependenciesHelper = BuildDependenciesHelper(project)
+    val querySyncActionStats = QuerySyncActionStatsScope.createForFiles(project, javaClass, e, ImmutableList.copyOf(files))
+    buildDependenciesHelper.determineTargetsAndRun(
+      workspaceRelativePaths = WorkspaceRoot.virtualFilesToWorkspaceRelativePaths(project, files),
+      disambiguateTargetPrompt = createDisambiguateTargetPrompt({ popup -> popup.showCenteredInCurrentWindow(project) }),
+      targetDisambiguationAnchors = TargetDisambiguationAnchors.NONE,
+      querySyncActionStats = querySyncActionStats,
+    ) { labels ->
+      BlazeBuildService.getInstance(project)
+        .buildFileForLabels(files.joinToString(", ", limit = 2), ImmutableSet.copyOf(labels))
+        .asDeferred()
     }
-
-    BlazeBuildService.getInstance(project).buildFile(files[0].name, getTargets(e))
   }
 
   private fun getTargets(e: AnActionEvent): ImmutableCollection<Label> {
