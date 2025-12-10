09364f331b9c316d5bda0a4b82cb242d13c15ea7
Paige Mcauliffe <paigemca@google.com>
2025-12-10 22:25:54 +0000

    Hide Kotlin JVM Target field in project structure dialog
    The Kotlin JVM Target field of the PSD currently sets android.kotlinOptions DSL which has been removed.
    
    Fixes: 463567263
    Test: manual
    Change-Id: I209cb68e34e4ee1184e4c0e3c3603b52cf9b2cd3
    
    GitOrigin-RevId: c3a66a5a4b32bf9dbe4a9ad78c1228da2162daf1
    
    Space-RevId: c385c0e6af660e343924d6ed2686f981ec402934
    (cherry picked from commit 972c033b6ba711ceb17dfafd741304cea955cb84)

--- project-system-gradle-psd/src/com/android/tools/idea/gradle/structure/configurables/android/modules/AndroidModuleRootConfigurable.kt
+++ project-system-gradle-psd/src/com/android/tools/idea/gradle/structure/configurables/android/modules/AndroidModuleRootConfigurable.kt
@@ -69,8 +69,6 @@ fun androidModulePropertiesModel(context: PsContext, module: PsAndroidModule) =
                  PSDEvent.PSDField.PROJECT_STRUCTURE_DIALOG_FIELD_MODULE_PROPERTIES_SOURCE_COMPATIBILITY),
       uiProperty(AndroidModuleDescriptors.targetCompatibility, ::simplePropertyEditor,
                  PSDEvent.PSDField.PROJECT_STRUCTURE_DIALOG_FIELD_MODULE_PROPERTIES_TARGET_COMPATIBILITY),
-      uiProperty(AndroidModuleDescriptors.kotlinJvmTarget, ::simplePropertyEditor,
-                 null)
     )
     + when (viewBindingPresent(context, module)) {
       true -> listOf(uiProperty(AndroidModuleDescriptors.viewBindingEnabled, ::simplePropertyEditor, null))
