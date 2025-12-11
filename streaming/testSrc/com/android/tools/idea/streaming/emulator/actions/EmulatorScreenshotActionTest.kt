5cf7206b990f8d9471eaf7460847e38ee638c98d
Sergey Prigogin <sprigogin@google.com>
2025-12-11 10:30:25 -0800

    Re-enable temporarily disabled test code
    Bug: 462824863
    Test: this
    Change-Id: Ie5bb29fee268296422bdc0faa41c34c7da7e34a1
    
    GitOrigin-RevId: e69f48380bdc76e90b0b563da5bbd560e3af2dab
    
    Space-RevId: 0c708bfdf466eafcc9f60ecf5f32d5aaca4efacb
    (cherry picked from commit 85c44f2d5e679ab0fd26a632eccb685f97fe14eb)

--- streaming/testSrc/com/android/tools/idea/streaming/emulator/actions/EmulatorScreenshotActionTest.kt
+++ streaming/testSrc/com/android/tools/idea/streaming/emulator/actions/EmulatorScreenshotActionTest.kt
@@ -43,7 +43,6 @@ import com.android.tools.idea.ui.screenshot.ScreenshotViewer
 import com.google.common.truth.Truth.assertThat
 import com.intellij.openapi.actionSystem.PlatformCoreDataKeys
 import com.intellij.openapi.actionSystem.impl.ActionButton
-import com.intellij.openapi.application.ApplicationInfo
 import com.intellij.openapi.components.service
 import com.intellij.openapi.ui.DialogWrapper.CLOSE_EXIT_CODE
 import com.intellij.openapi.vfs.VirtualFile
@@ -119,10 +118,8 @@ class EmulatorScreenshotActionTest {
     assertThat(clipComboBox.selectedItem?.toString()).isEqualTo("Rectangular")
     assertAppearance(screenshotViewer.waitForUpdateAndGetImage(), "WithoutFrame")
 
-    if (ApplicationInfo.getInstance().build.baselineVersion != 253) { // TODO: Remove the condition (b/462824863)
-      clipComboBox.selectFirstMatch("Show Device Frame")
-      assertAppearance(screenshotViewer.waitForUpdateAndGetImage(), "WithFrame")
-    }
+    clipComboBox.selectFirstMatch("Show Device Frame")
+    assertAppearance(screenshotViewer.waitForUpdateAndGetImage(), "WithFrame")
   }
 
   @Test
