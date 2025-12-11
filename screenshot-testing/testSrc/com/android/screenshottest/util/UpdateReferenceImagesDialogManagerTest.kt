3cfaeccbe6f375b3cd977d418a10ff22d078f640
rkdubey <rkdubey@google.com>
2025-12-11 22:57:32 +0000

    Move dialog manager to main module and add tests
    Moves UpdateReferenceImagesDialogManager from the gradle submodule
    to the main screenshot-testing module, as it is a UI utility with
    no Gradle dependencies.
    
    Adds unit tests to verify the single-instance dialog logic.
    
    Bug: 468093553
    Test: UpdateReferenceImagesDialogManagerTest. Verified the behavior
    manually.
    
    Change-Id: I14fb35c86d267e18e9f87b5dad43e717eab27af5
    
    GitOrigin-RevId: fdd0ec961c98317e3fa9e43f781d08938c4d2be7
    
    Space-RevId: dc7e2efe595926e347f3c714f20c3b6805b28066
    (cherry picked from commit 72595f4a154317d1f34d3eb492a6e53af65fa5af)

---
+++ screenshot-testing/testSrc/com/android/screenshottest/util/UpdateReferenceImagesDialogManagerTest.kt
@@ -0,0 +1,114 @@
+/*
+ * Copyright (C) 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.screenshottest.util
+
+import com.android.screenshottest.ui.UpdateReferenceImagesDialog
+import com.android.tools.idea.testing.AndroidProjectRule
+import com.intellij.openapi.util.Disposer
+import com.intellij.testFramework.PlatformTestUtil
+import com.intellij.testFramework.runInEdtAndWait
+import org.junit.Assert.assertNotNull
+import org.junit.Assert.assertNull
+import org.junit.Assert.assertNotSame
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.verify
+
+class UpdateReferenceImagesDialogManagerTest {
+
+    @get:Rule
+    val projectRule = AndroidProjectRule.inMemory()
+
+    @Test
+    fun testOnlyOneDialogActiveAtATime() = runInEdtAndWait {
+        val manager = UpdateReferenceImagesDialogManager.getInstance(projectRule.project)
+
+        // Mock the dialog
+        val mockDialog1 = mock(UpdateReferenceImagesDialog::class.java)
+        val mockDialog2 = mock(UpdateReferenceImagesDialog::class.java)
+
+        // Create disposables for the mocks to return, so Disposer.register works
+        val disposable1 = Disposer.newDisposable("MockDialog1")
+        val disposable2 = Disposer.newDisposable("MockDialog2")
+
+        `when`(mockDialog1.disposable).thenReturn(disposable1)
+        `when`(mockDialog2.disposable).thenReturn(disposable2)
+
+        var factoryCallCount = 0
+        manager.dialogFactory = {
+            factoryCallCount++
+            if (factoryCallCount == 1) mockDialog1 else mockDialog2
+        }
+
+        // 1. First call
+        // The dialog is NOT visible initially (default behavior)
+        `when`(mockDialog1.isVisible).thenReturn(false)
+
+        val dialog1 = manager.showOrGetDialog()
+        assertNotNull("First call should return a new dialog", dialog1)
+
+        // 2. Second call while dialog1 is "visible"
+        // Now we tell the mock it is visible
+        `when`(mockDialog1.isVisible).thenReturn(true)
+
+        val dialog2 = manager.showOrGetDialog()
+        assertNull("Second call should return null", dialog2)
+        verify(mockDialog1).toFront() // Verify it was brought to front
+
+        // 3. Close the first dialog
+        Disposer.dispose(disposable1)
+        PlatformTestUtil.dispatchAllEventsInIdeEventQueue()
+
+        // 4. Third call after closing
+        // The manager should have cleared the ref to dialog1
+        // We expect dialog2 to be returned now
+        `when`(mockDialog2.isVisible).thenReturn(false) // New dialog is not visible yet
+
+        val dialog3 = manager.showOrGetDialog()
+        assertNotNull("Should return a new dialog", dialog3)
+        assertNotSame("Should be different instance", dialog1, dialog3)
+
+        // Cleanup
+        Disposer.dispose(disposable2)
+    }
+
+    @Test
+    fun testManagerDisposalClearsDialog() = runInEdtAndWait {
+        val manager = UpdateReferenceImagesDialogManager.getInstance(projectRule.project)
+
+        val mockDialog = mock(UpdateReferenceImagesDialog::class.java)
+        val disposable = Disposer.newDisposable("MockDialog")
+        `when`(mockDialog.disposable).thenReturn(disposable)
+
+        manager.dialogFactory = { mockDialog }
+
+        val dialog = manager.showOrGetDialog()
+        assertNotNull(dialog)
+
+        // Dispose manager
+        manager.dispose()
+
+        // Check state by asking for dialog again
+        val dialogAfterDispose = manager.showOrGetDialog()
+        // It should create a new one (or return the factory result again)
+        // Since our factory returns the same mock, it will return mockDialog again
+        assertNotNull(dialogAfterDispose)
+
+        Disposer.dispose(disposable)
+    }
+}
