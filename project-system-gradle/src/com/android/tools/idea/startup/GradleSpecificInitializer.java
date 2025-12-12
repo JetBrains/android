075990f4936620a8237f67452d37d5a767742be6
Ahmet Kerim Senol <akerim@google.com>
2025-12-12 10:59:17 +0000

    Delete AndroidGradleBridgeProjectDataService
    Relevant behaviour controlled by the registry flag
    `gradle.phased.sync.bridge.disabled` instead.
    
    Bug: 384022658
    Test: n/a
    Change-Id: I3919ce4c6bae488b0fbd96eda124baf11db4ab3e
    
    GitOrigin-RevId: f061b76e7570c993413359fc14a3a1d801caa93d
    
    Space-RevId: 43bcf55577f6792d2bf9b8e1215ae4870c2590fc
    (cherry picked from commit 2e7f818577349eb44bfdc57d84680b5704628efe)

--- project-system-gradle/src/com/android/tools/idea/startup/GradleSpecificInitializer.java
+++ project-system-gradle/src/com/android/tools/idea/startup/GradleSpecificInitializer.java
@@ -58,7 +58,6 @@ public class GradleSpecificInitializer implements AppLifecycleListener {
     initializePhasedSync();
   }
 
-
   @VisibleForTesting
   public static void initializePhasedSync() {
     Registry.get("gradle.phased.sync.enabled").setValue(StudioFlags.PHASED_SYNC_ENABLED.get());
