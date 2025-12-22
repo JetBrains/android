0f5626e752562117af3c572e826114a9421bae4a
Max Medvedev <maxim.medvedev@jetbrains.com>
2025-12-22 19:46:22 +0100

    cleanup [completion]: nullability for CharFilters
    GitOrigin-RevId: f0338967098d9d1e1c52ff147e9c9cddf729c9e3

--- android/src/org/jetbrains/android/dom/AndroidXmlCharFilter.java
+++ android/src/org/jetbrains/android/dom/AndroidXmlCharFilter.java
@@ -1,15 +1,17 @@
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.android.dom;
 
 import com.intellij.codeInsight.lookup.CharFilter;
 import com.intellij.codeInsight.lookup.Lookup;
 import com.intellij.psi.PsiFile;
 import org.jetbrains.android.facet.AndroidFacet;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class AndroidXmlCharFilter extends CharFilter {
   @Nullable
   @Override
-  public Result acceptChar(char c, int prefixLength, Lookup lookup) {
+  public Result acceptChar(char c, int prefixLength, @NotNull Lookup lookup) {
     if (c != '|') {
       return null;
     }
