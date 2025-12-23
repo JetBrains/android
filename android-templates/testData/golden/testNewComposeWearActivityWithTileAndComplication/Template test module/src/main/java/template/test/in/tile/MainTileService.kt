783af6be7196977bd79fad019c8e465f7484f388
Paul Milian <paulmilian@google.com>
2025-12-23 16:08:40 +0000

    Update Wear templates
    Fixes: 459427532
    Test: TemplateDiffTest, ComposeWearActivityTemplateTest
    Change-Id: Ibdd9fbbbdc7f7eee2340f22b7a10dc57fcc5f4ee
    
    GitOrigin-RevId: 4ce295aa7196785eebef06829e273ca4e85d26e7
    
    Space-RevId: 16a81f47e570073267825d1ace4eb87b6dcd3726
    (cherry picked from commit 8865591fddafea3b3e1eaecfad4fd7e3b5a10b0f)

--- android-templates/testData/golden/testNewComposeWearActivityWithTileAndComplication/Template test module/src/main/java/template/test/in/tile/MainTileService.kt
+++ android-templates/testData/golden/testNewComposeWearActivityWithTileAndComplication/Template test module/src/main/java/template/test/in/tile/MainTileService.kt
@@ -1,43 +1,36 @@
 package template.test.`in`.tile
 
 import android.content.Context
-import androidx.wear.protolayout.ColorBuilders.argb
-import androidx.wear.protolayout.LayoutElementBuilders
-import androidx.wear.protolayout.ResourceBuilders
+import androidx.wear.protolayout.ResourceBuilders.Resources
 import androidx.wear.protolayout.TimelineBuilders
-import androidx.wear.protolayout.material.Colors
-import androidx.wear.protolayout.material.Text
-import androidx.wear.protolayout.material.Typography
-import androidx.wear.protolayout.material.layouts.PrimaryLayout
+import androidx.wear.protolayout.material3.Typography.BODY_LARGE
 import androidx.wear.tiles.RequestBuilders
 import androidx.wear.tiles.TileBuilders
+import androidx.wear.tiles.TileService
+import com.google.common.util.concurrent.Futures
+import androidx.wear.protolayout.material3.materialScope
+import androidx.wear.protolayout.material3.primaryLayout
+import androidx.wear.protolayout.material3.text
+import androidx.wear.protolayout.types.layoutString
+import androidx.wear.tiles.RequestBuilders.ResourcesRequest
 import androidx.wear.tiles.tooling.preview.Preview
 import androidx.wear.tiles.tooling.preview.TilePreviewData
 import androidx.wear.tooling.preview.devices.WearDevices
-import com.google.android.horologist.annotations.ExperimentalHorologistApi
-import com.google.android.horologist.tiles.SuspendingTileService
+import template.test.`in`.R
+import com.google.common.util.concurrent.ListenableFuture
 
 private const val RESOURCES_VERSION = "0"
 
-/**
- * Skeleton for a tile with no images.
- */
-@OptIn(ExperimentalHorologistApi::class)
-class MainTileService : SuspendingTileService() {
+class MainTileService : TileService() {
+    override fun onTileRequest(requestParams: RequestBuilders.TileRequest): ListenableFuture<TileBuilders.Tile> =
+        Futures.immediateFuture(tile(requestParams, this))
 
-    override suspend fun resourcesRequest(
-        requestParams: RequestBuilders.ResourcesRequest
-    ) = resources(requestParams)
-
-    override suspend fun tileRequest(
-        requestParams: RequestBuilders.TileRequest
-    ) = tile(requestParams, this)
+    override fun onTileResourcesRequest(requestParams: ResourcesRequest): ListenableFuture<Resources> =
+        Futures.immediateFuture(resources(requestParams))
 }
 
-private fun resources(
-    requestParams: RequestBuilders.ResourcesRequest
-): ResourceBuilders.Resources {
-    return ResourceBuilders.Resources.Builder()
+private fun resources(requestParams: ResourcesRequest): Resources {
+    return Resources.Builder()
         .setVersion(RESOURCES_VERSION)
         .build()
 }
@@ -46,36 +39,23 @@ private fun tile(
     requestParams: RequestBuilders.TileRequest,
     context: Context,
 ): TileBuilders.Tile {
-    val singleTileTimeline = TimelineBuilders.Timeline.Builder()
-        .addTimelineEntry(
-            TimelineBuilders.TimelineEntry.Builder()
-                .setLayout(
-                    LayoutElementBuilders.Layout.Builder()
-                        .setRoot(tileLayout(requestParams, context))
-                        .build()
-                )
-                .build()
-        )
-        .build()
-
     return TileBuilders.Tile.Builder()
         .setResourcesVersion(RESOURCES_VERSION)
-        .setTileTimeline(singleTileTimeline)
+        .setTileTimeline(
+            TimelineBuilders.Timeline.fromLayoutElement(
+                materialScope(context, requestParams.deviceConfiguration) {
+                    primaryLayout(
+                        mainSlot = {
+                            text(
+                                context.getString(R.string.hello_world, "Tile").layoutString,
+                                typography = BODY_LARGE
+                            )
+                        }
+                    )
+                }
+            )
+        )
         .build()
-}
-
-private fun tileLayout(
-    requestParams: RequestBuilders.TileRequest,
-    context: Context,
-): LayoutElementBuilders.LayoutElement {
-    return PrimaryLayout.Builder(requestParams.deviceConfiguration)
-        .setResponsiveContentInsetEnabled(true)
-        .setContent(
-            Text.Builder(context, "Hello World!")
-                .setColor(argb(Colors.DEFAULT.onSurface))
-                .setTypography(Typography.TYPOGRAPHY_CAPTION1)
-                .build()
-        ).build()
 }
 
 @Preview(device = WearDevices.SMALL_ROUND)
