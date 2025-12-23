7def04f85c5c2ad6c8b3ccc0878e0efad4ff5ea6
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2025-12-23 11:19:14 +0200

    [kotlin] Adapt to changes in `IrGenerationExtension` extension point registration
    ^KT-83341
    
    Space-RevId: 7b7dcd974873e6ff2cfb72208c94cdb7be29f179
    
    GitOrigin-RevId: 6d608619b992549cd4f73bac35d1fcf5b89e2c77

--- android/testSrc/com/android/tools/idea/run/deployment/liveedit/ComposeRuntimeTestUtil.kt
+++ android/testSrc/com/android/tools/idea/run/deployment/liveedit/ComposeRuntimeTestUtil.kt
@@ -28,6 +28,7 @@ import com.intellij.openapi.vfs.newvfs.impl.VfsRootAccess
 import com.intellij.testFramework.PsiTestUtil
 import com.intellij.testFramework.fixtures.CodeInsightTestFixture
 import com.intellij.testFramework.runInEdtAndWait
+import org.jetbrains.kotlin.analysis.api.impl.base.extensions.IrGenerationExtensionPointDescriptor
 import org.jetbrains.kotlin.analysis.api.platform.projectStructure.KotlinCompilerPluginsProvider
 import org.jetbrains.kotlin.analysis.api.projectStructure.KaModule
 import org.jetbrains.kotlin.backend.common.extensions.IrGenerationExtension
@@ -101,8 +102,8 @@ fun registerComposeCompilerPlugin(project: Project) {
                                     composeCompilerPluginProviderForTest, project)
     return
   }
-  if (IrGenerationExtension.getInstances(project).find { it is ComposePluginIrGenerationExtension } == null) {
-    IrGenerationExtension.registerExtension(project, ComposePluginIrGenerationExtension())
+  if (IrGenerationExtensionPointDescriptor.getInstances(project).find { it is ComposePluginIrGenerationExtension } == null) {
+    IrGenerationExtensionPointDescriptor.registerExtension(project, ComposePluginIrGenerationExtension())
   }
 }
 
