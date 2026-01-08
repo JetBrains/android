a58516f777385e9752757f0e04d53c407168a30b
Joe Baker-Malone <jbakermalone@google.com>
2026-01-07 16:57:57 -0800

    Remove commons-lang
    It was included needlessly in data-binding
    
    Test: updated DuplicateClassesTest
    Fixes: 476501574
    Change-Id: I6a7f8f5209304328673b84472ac768e848810b7a
    
    GitOrigin-RevId: 5c01503765cd9b53f4327d17d3488d40d8912dcf
    (cherry picked from commit a8e4b3128ab64610d7b0ef57da80311f3366dc73)

--- android/src/com/android/tools/idea/actions/internal/TestDuplicateClassesAction.kt
+++ android/src/com/android/tools/idea/actions/internal/TestDuplicateClassesAction.kt
@@ -50,14 +50,6 @@ class TestDuplicateClassesAction : DumbAwareAction("Test Duplicate Classes") {
           "com.android.annotations",
           "com.android.tools.instrumentation.threading.agent.callback",
           "javax.inject",
-          // b/476501574 below here
-          "org.apache.commons.lang.builder",
-          "org.apache.commons.lang.exception",
-          "org.apache.commons.lang.math",
-          "org.apache.commons.lang.text",
-          "org.apache.commons.lang.time",
-          "org.apache.commons.lang",
-          // end b/476501574
           "org.objectweb.asm.commons",
           "org.objectweb.asm.signature",
           "org.objectweb.asm.tree.analysis",
