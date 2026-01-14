a533aa1fdbd67b63db0aeea775338342e0230976
Hisham Haque <hhaque@google.com>
2026-01-14 17:30:04 +0000

    Enable OldAgpResolvedDependenciesTreeRootNodeTest for AGP 7.1.0
    #pdxai
    
    Bug: 439789826
    Test: updated
    Change-Id: I72742f43af4cdb8b98b1cca1b810d8b8685b85d2
    
    GitOrigin-RevId: 987072337e356967ecfb6d97b0282c7286de698d
    
    Space-RevId: 0e23c13682f11f455b5af5b8351f167675f3a90b
    (cherry picked from commit 5d03b2b930429371ed6bd211d77d505e6eaaa16b)

--- old-agp-tests/testSrc/com/android/tools/idea/gradle/project/psd/OldAgpResolvedDependenciesTreeRootNodeTest.kt
+++ old-agp-tests/testSrc/com/android/tools/idea/gradle/project/psd/OldAgpResolvedDependenciesTreeRootNodeTest.kt
@@ -18,18 +18,23 @@ package com.android.tools.idea.gradle.project.psd
 import com.android.testutils.junit4.OldAgpTest
 import com.android.tools.idea.gradle.project.sync.snapshots.AndroidCoreTestProject
 import com.android.tools.idea.gradle.project.sync.snapshots.TestProjectDefinition.Companion.prepareTestProject
+import com.android.tools.idea.gradle.structure.configurables.dependencies.treeview.ResolvedDependenciesTreeRootNode
 import com.android.tools.idea.gradle.structure.configurables.dependencies.treeview.ResolvedDependenciesTreeRootNodeTest
+import com.android.tools.idea.gradle.structure.configurables.ui.PsUISettings
+import com.android.tools.idea.gradle.structure.configurables.ui.testStructure
+import com.android.tools.idea.gradle.structure.model.PsProjectImpl
+import com.android.tools.idea.gradle.structure.model.android.PsAndroidModule
 import com.android.tools.idea.gradle.structure.model.android.psTestWithProject
 import com.android.tools.idea.testing.AgpVersionSoftwareEnvironmentDescriptor
 import com.intellij.testFramework.RunsInEdt
-import org.junit.Ignore
+import org.hamcrest.CoreMatchers.equalTo
+import org.junit.Assert.assertThat
 
 /**
  * This test covers the same case as [ResolvedDependenciesTreeRootNodeTest] but for older versions of AGP where transitive dependency
  * information is not available in the mode. There should be no change in the resulting dependency tree apart from where the information
  * is being obtained from.
  */
-@Ignore("b/439789826 - Enable this once the implementation is fixed")
 @OldAgpTest(agpVersions = ["7.1.0"], gradleVersions = ["7.2"])
 @RunsInEdt
 class OldAgpResolvedDependenciesTreeRootNodeTest : ResolvedDependenciesTreeRootNodeTest() {
@@ -37,7 +42,95 @@ class OldAgpResolvedDependenciesTreeRootNodeTest : ResolvedDependenciesTreeRootN
     val preparedProject = projectRule.prepareTestProject(AndroidCoreTestProject.PSD_DEPENDENCY,
                                                          agpVersion = AgpVersionSoftwareEnvironmentDescriptor.AGP_71)
     projectRule.psTestWithProject(preparedProject) {
-      project.runTestAndroidTreeStructure()
+      project.runOldAgpTestAndroidTreeStructure()
     }
+  }
+
+  private fun PsProjectImpl.runOldAgpTestAndroidTreeStructure() {
+    val appModule = findModuleByGradlePath(":app") as PsAndroidModule
+    val node = ResolvedDependenciesTreeRootNode(appModule, PsUISettings())
+
+    // Note: indentation matters!
+    val expectedProjectStructure = """
+    app
+        freeDebug
+            mainModule
+                lib1:1.0 (com.example.libs)
+                    lib2:1.0 (com.example.libs)
+                        lib3:1.0 (com.example.jlib)
+                            lib4:1.0 (com.example.jlib)
+                lib3:0.6→1.0 (com.example.jlib)
+                    lib4:1.0 (com.example.jlib)
+        freeDebugAndroidTest
+            freeDebug
+                mainModule
+                    lib1:1.0 (com.example.libs)
+                        lib2:1.0 (com.example.libs)
+                            lib3:1.0 (com.example.jlib)
+                                lib4:1.0 (com.example.jlib)
+                    lib3:0.6→1.0 (com.example.jlib)
+                        lib4:1.0 (com.example.jlib)
+        freeDebugUnitTest
+            freeDebug
+                mainModule
+                    lib1:1.0 (com.example.libs)
+                        lib2:1.0 (com.example.libs)
+                            lib3:1.0 (com.example.jlib)
+                                lib4:1.0 (com.example.jlib)
+                    lib3:0.6→1.0 (com.example.jlib)
+                        lib4:1.0 (com.example.jlib)
+        freeRelease
+            mainModule
+                lib1:1.0,0.9.1→1.0 (com.example.libs)
+                    lib2:1.0 (com.example.libs)
+                        lib3:1.0 (com.example.jlib)
+                            lib4:1.0 (com.example.jlib)
+                lib3:0.6→1.0 (com.example.jlib)
+                    lib4:1.0 (com.example.jlib)
+        freeReleaseUnitTest
+            freeRelease
+                mainModule
+                    lib1:1.0,0.9.1→1.0 (com.example.libs)
+                        lib2:1.0 (com.example.libs)
+                            lib3:1.0 (com.example.jlib)
+                                lib4:1.0 (com.example.jlib)
+                    lib3:0.6→1.0 (com.example.jlib)
+                        lib4:1.0 (com.example.jlib)
+        paidDebug
+            mainModule
+                lib1:1.0 (com.example.libs)
+                    lib2:1.0 (com.example.libs)
+                        lib3:1.0 (com.example.jlib)
+                            lib4:1.0 (com.example.jlib)
+        paidDebugAndroidTest
+            paidDebug
+                mainModule
+                    lib1:1.0 (com.example.libs)
+                        lib2:1.0 (com.example.libs)
+                            lib3:1.0 (com.example.jlib)
+                                lib4:1.0 (com.example.jlib)
+        paidDebugUnitTest
+            paidDebug
+                mainModule
+                    lib1:1.0 (com.example.libs)
+                        lib2:1.0 (com.example.libs)
+                            lib3:1.0 (com.example.jlib)
+                                lib4:1.0 (com.example.jlib)
+        paidRelease
+            mainModule
+                lib1:1.0,0.9.1→1.0 (com.example.libs)
+                    lib2:1.0 (com.example.libs)
+                        lib3:1.0 (com.example.jlib)
+                            lib4:1.0 (com.example.jlib)
+        paidReleaseUnitTest
+            paidRelease
+                mainModule
+                    lib1:1.0,0.9.1→1.0 (com.example.libs)
+                        lib2:1.0 (com.example.libs)
+                            lib3:1.0 (com.example.jlib)
+                                lib4:1.0 (com.example.jlib)""".trimIndent()
+    val treeStructure = node.testStructure { !it.name.startsWith("appcompat-v7") }
+    // Note: If fails see a nice diff by clicking <Click to see difference> in the IDEA output window.
+    assertThat(treeStructure.toString(), equalTo(expectedProjectStructure))
   }
 }
