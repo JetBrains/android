5baae6cb823f9916665c3d9630aa6c6373e73292
Sergey Prigogin <sprigogin@google.com>
2026-01-20 15:21:17 -0800

    Update getDesktopDirectory code from IntelliJ platform
    Bug: N/A
    Test: existing
    Change-Id: I74efbf4470ba5c71114098b7fa41eef2eb39f579
    
    GitOrigin-RevId: 18b8948de44247c83d3d6109cf5790b6f69665d4
    (cherry picked from commit 8c192683c4b7f019b80c739a79bf8fb3d187b889)

--- android-common/src/com/android/tools/idea/io/IdeFileUtils.java
+++ android-common/src/com/android/tools/idea/io/IdeFileUtils.java
@@ -18,12 +18,23 @@ package com.android.tools.idea.io;
 import com.intellij.execution.configurations.GeneralCommandLine;
 import com.intellij.execution.configurations.PathEnvironmentVariableUtil;
 import com.intellij.execution.util.ExecUtil;
+import com.intellij.jna.JnaLoader;
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.vfs.LocalFileSystem;
 import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.ui.mac.foundation.Foundation;
+import com.intellij.ui.mac.foundation.ID;
 import com.intellij.util.SystemProperties;
+import com.intellij.util.system.OS;
+import com.sun.jna.platform.win32.Shell32;
+import com.sun.jna.platform.win32.ShlObj;
+import com.sun.jna.platform.win32.WinDef;
+import com.sun.jna.platform.win32.WinError;
+import com.sun.jna.platform.win32.WinNT;
 import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
-import java.nio.file.Paths;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 /**
@@ -31,29 +42,56 @@ import org.jetbrains.annotations.Nullable;
  */
 public class IdeFileUtils {
   /**
-   * Returns the Desktop directory for the current user, or null if it doesn't exist.
+   * Returns the Desktop directory for the current user. The directory may not exist.
    * Copied from {@link GotoDesktopDirAction} with minor changes.
    */
-  @Nullable
-  public static Path getDesktopDirectory() {
-    Path desktop = Paths.get(SystemProperties.getUserHome(), "Desktop");
-
-    if (!Files.isDirectory(desktop) && PathEnvironmentVariableUtil.isOnPath("xdg-user-dir")) {
-      String path = ExecUtil.execAndReadLine(new GeneralCommandLine("xdg-user-dir", "DESKTOP"));
+  public static @NotNull Path getDesktopDirectory() {
+    if (OS.CURRENT == OS.Windows && JnaLoader.isLoaded()) {
+      char[] path = new char[WinDef.MAX_PATH];
+      WinNT.HRESULT res = Shell32.INSTANCE.SHGetFolderPath(null, ShlObj.CSIDL_DESKTOP, null, ShlObj.SHGFP_TYPE_CURRENT, path);
+      if (WinError.S_OK.equals(res)) {
+        int len = 0;
+        while (len < path.length && path[len] != 0) len++;
+        return Path.of(new String(path, 0, len));
+      }
+    }
+    else if (OS.CURRENT == OS.macOS && JnaLoader.isLoaded()) {
+      ID manager = Foundation.invoke(Foundation.getObjcClass("NSFileManager"), "defaultManager");
+      ID url = Foundation.invoke(manager, "URLForDirectory:inDomain:appropriateForURL:create:error:",
+                                 12 /*NSDesktopDirectory*/, 1 /*NSUserDomainMask*/, null, false, null);
+      String path = Foundation.toStringViaUTF8(Foundation.invoke(url, "path"));
       if (path != null) {
-        desktop = Paths.get(path);
+        return Path.of(path);
+      }
+    }
+    else if (PathEnvironmentVariableUtil.isOnPath("xdg-user-dir")) {
+      var path = ExecUtil.execAndReadLine(new GeneralCommandLine("xdg-user-dir", "DESKTOP"));
+      if (path != null && !path.isBlank()) {
+        try {
+          return Path.of(path);
+        }
+        catch (InvalidPathException e) {
+          Logger.getInstance(IdeFileUtils.class).error("str='" + path + "' JNU=" + System.getProperty("sun.jnu.encoding"), e);
+        }
       }
     }
 
-    return Files.isDirectory(desktop) ? desktop : null;
+    return Path.of(SystemProperties.getUserHome(), "Desktop");
   }
 
   /**
    * Returns the Desktop directory for the current user, or null if it doesn't exist.
    */
-  @Nullable
-  public static VirtualFile getDesktopDirectoryVirtualFile() {
-    Path desktop = getDesktopDirectory();
+  public static @Nullable Path getDesktopDirectoryIfExists() {
+    Path dir = getDesktopDirectory();
+    return Files.isDirectory(dir) ? dir : null;
+  }
+
+  /**
+   * Returns the Desktop directory for the current user, or null if it doesn't exist.
+   */
+  public static @Nullable VirtualFile getDesktopDirectoryVirtualFile() {
+    Path desktop = getDesktopDirectoryIfExists();
     return desktop == null ? null : LocalFileSystem.getInstance().refreshAndFindFileByIoFile(desktop.toFile());
   }
 }
