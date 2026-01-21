a084e4f0f06d453636f7afef8ed5791163b07a6f
Sergey Prigogin <sprigogin@google.com>
2026-01-20 16:27:21 -0800

    Make use of the getDesktopDirectory function
    Bug: 472222693
    Test: SaveConfigurationResolverTest, SaveConfigurationPanelTest
    Change-Id: I0dc688cf9125f210b5b04dd2833410745f1b95db
    
    GitOrigin-RevId: 1fdf5cb88b8b7097c276e49dbe69c935385800d5
    (cherry picked from commit a3aaeaf6d3017ac5f61086859e0fbbe548d8541a)

--- android-adb-ui/src/com/android/tools/idea/ui/save/SaveConfigurationResolver.kt
+++ android-adb-ui/src/com/android/tools/idea/ui/save/SaveConfigurationResolver.kt
@@ -15,10 +15,11 @@
  */
 package com.android.tools.idea.ui.save
 
+import com.android.tools.idea.io.IdeFileUtils.getDesktopDirectory
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.guessProjectDir
-import org.jetbrains.annotations.NonNls
+import com.intellij.util.SystemProperties
 import java.io.File
 import java.nio.file.InvalidPathException
 import java.nio.file.Paths
@@ -26,12 +27,13 @@ import java.time.Instant
 import java.time.ZoneId
 import java.time.temporal.ChronoField
 import java.util.Locale
+import org.jetbrains.annotations.NonNls
 
 /** Provides methods operating on save location and filename template. */
 @Service(Service.Level.PROJECT)
 internal class SaveConfigurationResolver(private val project: Project) {
 
-  private val userHome: @NonNls String = System.getProperty("user.home")
+  private val userHome: @NonNls String = SystemProperties.getUserHome()
 
   fun expandFilenamePattern(
       saveLocation: String, filenameTemplate: String, fileExtension: String, timestamp: Instant, sequentialNumber: Int): String {
@@ -58,34 +60,39 @@ internal class SaveConfigurationResolver(private val project: Project) {
   fun generalizeSaveLocation(saveLocation: String): String {
     val dir = Paths.get(userHome).resolve(saveLocation).normalize()
     val projectDir = project.guessProjectDir()?.toNioPath()
-    return when {
-      projectDir != null && dir.startsWith(projectDir) ->
-          dir.toString().replaceRange(0, projectDir.toString().length, PROJECT_DIR_MACRO)
-      dir.startsWith(Paths.get(userHome)) ->
-          dir.toString().replaceRange(0, userHome.length, USER_HOME_MACRO)
-      else -> saveLocation
-    }.replace(File.separatorChar, '/')
+    // Iterate over possible prefix substitutions from the longest to the shortest one.
+    val substitutions = mutableListOf(
+      Pair(projectDir, PROJECT_DIR_MACRO), Pair(getDesktopDirectory(), DESKTOP_DIR_MACRO), Pair(Paths.get(userHome), USER_HOME_MACRO)
+    ).sortedByDescending { it.first?.nameCount }
+    for (substitution in substitutions) {
+      val prefixPath = substitution.first ?: continue
+      if (dir.startsWith(prefixPath)) {
+        return dir.toString().replaceRange(0, prefixPath.toString().length, substitution.second).replace(File.separatorChar, '/')
+      }
+    }
+    return saveLocation.replace(File.separatorChar, '/')
   }
 
   fun expandSaveLocation(saveLocation: String): String {
     val dir = saveLocation.replace(File.separatorChar, '/')
-    return when {
+    val base = when {
       saveLocation.startsWithFollowedBySeparator(PROJECT_DIR_MACRO) ->
-          project.guessProjectDir()?.toNioPath()?.resolve(dir.substringAfter('/', ""))?.toString() ?:
-          "$userHome/Desktop"
-      saveLocation.startsWithFollowedBySeparator(USER_HOME_MACRO) -> "$userHome/${dir.substringAfter('/', "")}"
-      else -> saveLocation
-    }.replace('/', File.separatorChar)
+          project.guessProjectDir()?.toNioPath() ?: getDesktopDirectory()
+      saveLocation.startsWithFollowedBySeparator(DESKTOP_DIR_MACRO) -> getDesktopDirectory()
+      saveLocation.startsWithFollowedBySeparator(USER_HOME_MACRO) -> Paths.get(userHome)
+      else -> null
+    }
+    return (base?.resolve(dir.substringAfter('/', "")) ?: saveLocation).toString()
   }
 
   companion object {
     const val USER_HOME_MACRO: @NonNls String = "\$USER_HOME$"
-    const val DEFAULT_SAVE_LOCATION = "$USER_HOME_MACRO/Desktop"
+    const val DESKTOP_DIR_MACRO: @NonNls String = "\$DESKTOP_DIR$"
     const val PROJECT_DIR_MACRO: @NonNls String = "\$PROJECT_DIR$"
+    const val DEFAULT_SAVE_LOCATION = "\$DESKTOP_DIR$"
 
     @JvmStatic
-    private fun String.startsWithFollowedBySeparator(prefix: String): Boolean {
-      return startsWith(prefix) && (length == prefix.length || this[prefix.length] == '/')
-    }
+    private fun String.startsWithFollowedBySeparator(prefix: String): Boolean =
+        startsWith(prefix) && (length == prefix.length || this[prefix.length] == '/')
   }
 }
