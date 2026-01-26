7b39eb55a5cdcbd66186bed2caf9719a38674ed3
Christophe Rhodes <xof@google.com>
2026-01-26 17:10:56 +0000

    Add a test project
    Bug: 119942511
    Test: in forthcoming CLs
    Change-Id: Ibc9a614a44790deee4d373eb627905e2f93b6100
    
    GitOrigin-RevId: 6f0e5e3ca16026b0415ceb7dddd7415fa16630c8
    (cherry picked from commit c37c683cc42f83673fe149b98af7b914855ca6ec)

---
+++ android/testData/projects/testOnlyVisibility/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.annotation.VisibleForTesting
+import androidx.annotation.VisibleForTesting.Companion.PRIVATE
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.myapplication.ui.theme.MyApplicationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MyApplicationTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MyApplicationTheme {
+        Greeting("Android")
+    }
+}
+
+@VisibleForTesting(PRIVATE)
+fun foo(x: Int): Int {
+    return x + 1
+}
---
+++ android/testData/projects/testOnlyVisibility/settings.gradle.kts
@@ -0,0 +1,14 @@
+pluginManagement {
+  repositories {
+    // This will be populated by AndroidGradleTestCase
+  }
+}
+dependencyResolutionManagement {
+  repositories {
+    // This will be populated by AndroidGradleTestCase
+  }
+}
+
+rootProject.name = "My Application"
+include(":app")
+include(":test")
