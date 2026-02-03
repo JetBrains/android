361b444bf0fe55ce105c8aa5def987249ba93e7b
Ayush Jain <iayushj@google.com>
2026-02-03 13:02:02 +0530

    [Tests] CAL Basic App Template
    Add tests for CAL Basic App Template.
    Update golden files for the newly added tests.
    
    Fixes: 477798658
    Test: Covered under TemplateDiffTest for both Kotlin and Java cases.
    Change-Id: I3a65b3c11f91ffd369666e838904581df5b4c977
    
    GitOrigin-RevId: 68d5c9029ecf715cc788dc7dee298806fed794ba
    (cherry picked from commit 651389e39dfe9bed4135f90d7d127158502af195)

--- android-templates/testSrc/com/android/tools/idea/templates/diff/activity/TemplateDiffTest.kt
+++ android-templates/testSrc/com/android/tools/idea/templates/diff/activity/TemplateDiffTest.kt
@@ -750,6 +750,11 @@ class TemplateDiffTest(private val testMode: TestMode) {
     checkCreateTemplate("Media Service", withKotlin())
   }
 
+  @Test
+  fun testEmptyCarAppLibraryAppWithKotlin() {
+    checkCreateTemplate("Empty Car App Library App", withKotlin())
+  }
+
   @Ignore("b/418047552")
   @Test
   fun testXRBasicHeadsetActivity() {
