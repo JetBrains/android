5f74a6f64367ab4b709a28bdca9725fd2f9e70fc
Amaury Medeiros <amaurym@google.com>
2026-02-18 15:33:45 +0000

    Rename render quality Perfgate tests accordingly
    Since the render quality flag was permanently enabled, these tests need
    to be renamed to reflect the new behavior.
    
    Bug: N/A
    Test: PerfgateComposeRenderQualityGradleTest,
     PerfgateComposeStandardGradleTest
    Change-Id: I8d88de7b771f91093e3e7e9f174e1b6a1f3f0395
    
    GitOrigin-RevId: f0dee557c09670868fd04d664cc5bc2e7c8d6ad6
    (cherry picked from commit fd915ebe975cf2ea6ccb2e6388f24fb7b380ae17)

--- designer-perf-tests/testSrc/com/android/tools/idea/rendering/gradle/PerfgateComposeRenderQualityGradleTest.kt
+++ designer-perf-tests/testSrc/com/android/tools/idea/rendering/gradle/PerfgateComposeRenderQualityGradleTest.kt
@@ -28,7 +28,7 @@ import org.junit.Test
 
 class PerfgateComposeRenderQualityGradleTest : PerfgateComposeGradleTestBase() {
   @Test
-  fun renderQualityEnabled_5Previews() = runBlocking {
+  fun qualityRefresh_5Previews() = runBlocking {
     Assert.assertEquals(1, composePreviewRepresentation.renderedPreviewElementsInstancesFlowForTest().value.asCollection().size)
     addPreviewsAndMeasure(
       4,
@@ -36,23 +36,19 @@ class PerfgateComposeRenderQualityGradleTest : PerfgateComposeGradleTestBase() {
       listOf(
         // Measures the full rendering time, including ModuleClassLoader instantiation, inflation
         // and render.
-        ElapsedTimeMeasurement(Metric("renderQualityEnabled_5_previews_refresh_time")),
-        HeapSnapshotMemoryUseMeasurement("android:designTools", null, Metric("renderQualityEnabled_5_previews_total_memory")),
-        HeapSnapshotMemoryUseMeasurement("android:designTools", "rendering", Metric("renderQualityEnabled_5_previews_rendering_memory")),
-        HeapSnapshotMemoryUseMeasurement(
-          "android:designTools",
-          "layoutEditor",
-          Metric("renderQualityEnabled_5_previews_layoutEditor_memory"),
-        ),
-        HeapSnapshotMemoryUseMeasurement("android:designTools", "layoutlib", Metric("renderQualityEnabled_5_previews_layoutlib_memory")),
-        LayoutlibNativeMemoryMeasurement(Metric("renderQualityEnabled_5_previews_layoutlib_native_memory")),
+        ElapsedTimeMeasurement(Metric("qualityRefresh_5_previews_refresh_time")),
+        HeapSnapshotMemoryUseMeasurement("android:designTools", null, Metric("qualityRefresh_5_previews_total_memory")),
+        HeapSnapshotMemoryUseMeasurement("android:designTools", "rendering", Metric("qualityRefresh_5_previews_rendering_memory")),
+        HeapSnapshotMemoryUseMeasurement("android:designTools", "layoutEditor", Metric("qualityRefresh_5_previews_layoutEditor_memory")),
+        HeapSnapshotMemoryUseMeasurement("android:designTools", "layoutlib", Metric("qualityRefresh_5_previews_layoutlib_memory")),
+        LayoutlibNativeMemoryMeasurement(Metric("qualityRefresh_5_previews_layoutlib_native_memory")),
       ),
       measuredRunnable = ::qualityRefresh,
     )
   }
 
   @Test
-  fun renderQualityEnabled_30Previews() = runBlocking {
+  fun qualityRefresh_30Previews() = runBlocking {
     Assert.assertEquals(1, composePreviewRepresentation.renderedPreviewElementsInstancesFlowForTest().value.asCollection().size)
     addPreviewsAndMeasure(
       29,
@@ -60,23 +56,19 @@ class PerfgateComposeRenderQualityGradleTest : PerfgateComposeGradleTestBase() {
       listOf(
         // Measures the full rendering time, including ModuleClassLoader instantiation, inflation
         // and render.
-        ElapsedTimeMeasurement(Metric("renderQualityEnabled_30_previews_refresh_time")),
-        HeapSnapshotMemoryUseMeasurement("android:designTools", null, Metric("renderQualityEnabled_30_previews_total_memory")),
-        HeapSnapshotMemoryUseMeasurement("android:designTools", "rendering", Metric("renderQualityEnabled_30_previews_rendering_memory")),
-        HeapSnapshotMemoryUseMeasurement(
-          "android:designTools",
-          "layoutEditor",
-          Metric("renderQualityEnabled_30_previews_layoutEditor_memory"),
-        ),
-        HeapSnapshotMemoryUseMeasurement("android:designTools", "layoutlib", Metric("renderQualityEnabled_30_previews_layoutlib_memory")),
-        LayoutlibNativeMemoryMeasurement(Metric("renderQualityEnabled_30_previews_layoutlib_native_memory")),
+        ElapsedTimeMeasurement(Metric("qualityRefresh_30_previews_refresh_time")),
+        HeapSnapshotMemoryUseMeasurement("android:designTools", null, Metric("qualityRefresh_30_previews_total_memory")),
+        HeapSnapshotMemoryUseMeasurement("android:designTools", "rendering", Metric("qualityRefresh_30_previews_rendering_memory")),
+        HeapSnapshotMemoryUseMeasurement("android:designTools", "layoutEditor", Metric("qualityRefresh_30_previews_layoutEditor_memory")),
+        HeapSnapshotMemoryUseMeasurement("android:designTools", "layoutlib", Metric("qualityRefresh_30_previews_layoutlib_memory")),
+        LayoutlibNativeMemoryMeasurement(Metric("qualityRefresh_30_previews_layoutlib_native_memory")),
       ),
       measuredRunnable = ::qualityRefresh,
     )
   }
 
   @Test
-  fun renderQualityEnabled_200Previews() = runBlocking {
+  fun qualityRefresh_200Previews() = runBlocking {
     Assert.assertEquals(1, composePreviewRepresentation.renderedPreviewElementsInstancesFlowForTest().value.asCollection().size)
     addPreviewsAndMeasure(
       199,
@@ -84,16 +76,12 @@ class PerfgateComposeRenderQualityGradleTest : PerfgateComposeGradleTestBase() {
       listOf(
         // Measures the full rendering time, including ModuleClassLoader instantiation, inflation
         // and render.
-        ElapsedTimeMeasurement(Metric("renderQualityEnabled_200_previews_refresh_time")),
-        HeapSnapshotMemoryUseMeasurement("android:designTools", null, Metric("renderQualityEnabled_200_previews_total_memory")),
-        HeapSnapshotMemoryUseMeasurement("android:designTools", "rendering", Metric("renderQualityEnabled_200_previews_rendering_memory")),
-        HeapSnapshotMemoryUseMeasurement(
-          "android:designTools",
-          "layoutEditor",
-          Metric("renderQualityEnabled_200_previews_layoutEditor_memory"),
-        ),
-        HeapSnapshotMemoryUseMeasurement("android:designTools", "layoutlib", Metric("renderQualityEnabled_200_previews_layoutlib_memory")),
-        LayoutlibNativeMemoryMeasurement(Metric("renderQualityEnabled_200_previews_layoutlib_native_memory")),
+        ElapsedTimeMeasurement(Metric("qualityRefresh_200_previews_refresh_time")),
+        HeapSnapshotMemoryUseMeasurement("android:designTools", null, Metric("qualityRefresh_200_previews_total_memory")),
+        HeapSnapshotMemoryUseMeasurement("android:designTools", "rendering", Metric("qualityRefresh_200_previews_rendering_memory")),
+        HeapSnapshotMemoryUseMeasurement("android:designTools", "layoutEditor", Metric("qualityRefresh_200_previews_layoutEditor_memory")),
+        HeapSnapshotMemoryUseMeasurement("android:designTools", "layoutlib", Metric("qualityRefresh_200_previews_layoutlib_memory")),
+        LayoutlibNativeMemoryMeasurement(Metric("qualityRefresh_200_previews_layoutlib_native_memory")),
       ),
       measuredRunnable = ::qualityRefresh,
     )
