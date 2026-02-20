babcd5966c63b4c06ed367e2b7acfd625d510155
Karima Ichou <karimai@google.com>
2026-02-20 12:13:27 +0000

    Update comparison benchmark Gradle version
    to 9.4.0-20260220074159+0000
    
    Bug: 365508657
    Test: N/A
    Change-Id: Ice6f527d853f2c2fb9152ffe292d19b3ebe1d019
    
    GitOrigin-RevId: 511144971da7422bca3641ae05ad608dbe715a51
    (cherry picked from commit 38b004f9cd699f924f2aeb746ab717cc537e43c6)

--- android-test-framework/testSrc/com/android/tools/idea/testing/AgpIntegrationTests.kt
+++ android-test-framework/testSrc/com/android/tools/idea/testing/AgpIntegrationTests.kt
@@ -26,7 +26,7 @@ import org.jetbrains.kotlin.util.capitalizeDecapitalize.capitalizeAsciiOnly
 
 // This Gradle version is exclusively used for the Sync Comparison Benchmarks and gets updated
 // frequently. Please do not use for other purposes
-const val GRADLE_SNAPSHOT_VERSION = "9.4.0-rc-1"
+const val GRADLE_SNAPSHOT_VERSION = "9.4.0-20260220074159+0000"
 const val GRADLE_DECLARATIVE_SNAPSHOT_VERSION = "9.4.0-rc-1"
 // For available versions:
 // https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev/org/jetbrains/kotlin/kotlin-compiler/maven-metadata.xml
