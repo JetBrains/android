4f7d34d18801ec8787a086bad3a3e65e24c1a46a
Hung Nguyen <hungnv@google.com>
2026-02-20 15:37:33 +0000

    Update to KGP 2.3.20-RC for tests (tools/adt/idea)
    Bug: N/A (Regular update)
    Test: Updated
    Change-Id: I2eb110da55bdd4e5f672cb28e2d533d26c02eaa0
    
    GitOrigin-RevId: bba5b033a13d53e9af80ce22a127ac24ae25541b
    (cherry picked from commit e889b93e958199342c8a065412db68dc2bc8a11d)

--- project-system-gradle/src/com/android/tools/idea/gradle/project/sync/internal/IdeModelDumper.kt
+++ project-system-gradle/src/com/android/tools/idea/gradle/project/sync/internal/IdeModelDumper.kt
@@ -264,7 +264,7 @@ private val jbModelDumpers =
  * Note: Other tests in the IDE (e.g., templates, editor, UI tools, deployment) should not use this constant as they don't need to test
  * against the latest preview version of Kotlin.
  */
-const val KOTLIN_VERSION_FOR_TESTS = "2.3.20-Beta1"
+const val KOTLIN_VERSION_FOR_TESTS = "2.3.20-RC"
 
 fun String.replaceKotlinVersionForTests(): String = replace(KOTLIN_VERSION_FOR_TESTS, "<KOTLIN_VERSION_FOR_TESTS>")
 
