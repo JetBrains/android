2e12d3fa00d4fc82c600f36ab8ef72111bf85ba1
Jinseong Jeon <jsjeon@google.com>
2026-02-24 11:43:51 -0800

    Drop K1 mode usages in android-kotlin
    Bug: 448273436
    Test: presubmit
    Change-Id: I8b29b6f618e5f12085bb794473e387d68c26b343
    
    GitOrigin-RevId: 500502e4435e7756bb0fe000fa5a492522d4bd2e
    (cherry picked from commit ad5a59df0ef440e297aa7dc085d052767a52ba7e)

--- android-kotlin/idea-android/testData/android/quickfix/autoImports/androidRImport.after.kt
+++ android-kotlin/idea-android/testData/android/quickfix/autoImports/androidRImport.after.kt
@@ -1,8 +1,6 @@
 // "Import class 'R'" "true"
-// K1-ERROR: UNRESOLVED_REFERENCE: R
 // K2-ERROR: UNRESOLVED_REFERENCE: R
 // DO_NOT_IMPORT: android.support.v7.appcompat.R
-// SKIP-K1 (b/304360782)
 
 package com.myapp.activity
 
