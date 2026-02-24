10d5fec2bc51f32b07619a86b006743f74b78ea3
Paul Milian <paulmilian@google.com>
2026-02-24 11:56:51 +0000

    Replace blocking readAction with non-blocking version
    ModuleUtilCore.findModuleForFile(file, project) uses a blocking read
    action under the hood. This calls the ProjectFileIndex directly in order
    to use a non-blocking read version.
    
    Bug: 487042085
    Bug: 486397330
    Test: existing
    Change-Id: I7d153bf00da7e4173162437beed0f54477a4a0b3
    
    GitOrigin-RevId: f0d1f78eb4a26d7cb314599077bc9c17d29c9b05
    (cherry picked from commit 688576b4deb20dc87cdf48151cfb3263b1c759bb)

--- android/src/com/android/tools/idea/stats/EditorStatsUtil.kt
+++ android/src/com/android/tools/idea/stats/EditorStatsUtil.kt
@@ -18,7 +18,6 @@
 package com.android.tools.idea.stats
 
 import com.android.SdkConstants.ANDROID_MANIFEST_XML
-import com.android.annotations.concurrency.WorkerThread
 import com.android.resources.ResourceFolderType
 import com.android.resources.ResourceFolderType.ANIM
 import com.android.resources.ResourceFolderType.ANIMATOR
@@ -70,9 +69,11 @@ import com.google.wireless.android.sdk.stats.EditorFileType.XML_RES_RAW
 import com.google.wireless.android.sdk.stats.EditorFileType.XML_RES_TRANSITION
 import com.google.wireless.android.sdk.stats.EditorFileType.XML_RES_VALUES
 import com.google.wireless.android.sdk.stats.EditorFileType.XML_RES_XML
-import com.intellij.openapi.application.runReadAction
+import com.intellij.openapi.application.readAction
+import com.intellij.openapi.module.Module
 import com.intellij.openapi.module.ModuleUtilCore
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.roots.ProjectFileIndex
 import com.intellij.openapi.vfs.VirtualFile
 import java.util.Locale
 import kotlinx.coroutines.Dispatchers
@@ -107,7 +108,7 @@ suspend fun getEditorFileTypeForAnalytics(file: VirtualFile, project: Project?):
     "Kotlin" ->
       when {
         file.extension == "kts" -> KOTLIN_SCRIPT
-        withContext(Dispatchers.Default) { isComposeEnabled(file, project) } -> KOTLIN_COMPOSE
+        isComposeEnabled(file, project) -> KOTLIN_COMPOSE
         else -> KOTLIN
       }
     "Groovy" -> GROOVY
@@ -157,12 +158,17 @@ suspend fun getEditorFileTypeForAnalytics(file: VirtualFile, project: Project?):
   }
 
 /**
- * This method is not expected to be slow, but it's possible the call to find the file's module could take longer in some circumstances. As
- * such, it's marked with [WorkerThread] to make sure there are no long calls on the UI thread.
+ * This method will perform a non-blocking read action to get the module attached to the file [file] in order to check if compose is being
+ * used.
  */
-@WorkerThread
-private fun isComposeEnabled(file: VirtualFile, project: Project?): Boolean {
+private suspend fun isComposeEnabled(file: VirtualFile, project: Project?): Boolean {
   if (project == null) return false
-  val module = runReadAction { ModuleUtilCore.findModuleForFile(file, project) } ?: return false
+  val module = findModuleForFileNonBlocking(file, project) ?: return false
   return module.getModuleSystem().usesCompose
+}
+
+/** Copy of [ModuleUtilCore.findModuleForFile] that uses a non-blocking read instead of a blocking read */
+private suspend fun findModuleForFileNonBlocking(file: VirtualFile, project: Project): Module? {
+  if (project.isDefault) return null
+  return withContext(Dispatchers.Default) { readAction { ProjectFileIndex.getInstance(project).getModuleForFile(file) } }
 }
