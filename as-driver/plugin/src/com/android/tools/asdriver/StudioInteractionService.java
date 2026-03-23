52e765429237600d0ffb3099ec2ba98d3bcc26c0
Rohan Kumar <rohanrko@google.com>
2026-03-23 19:11:59 +0000

    Enabled invokeComponentByXpath to handle modal button clicks
    Replaced invokeAndWait with invokeLater to prevent deadlocks when interacting with buttons that open modal dialogs
    
    Test: Tested with pre-submit
    Bug: 452789102
    
    Fusing Link for 500 runs:
    https://fusion2.corp.google.com/invocations/720e3fd8-e082-4ecb-a9b5-f6234b8780ce
     https://fusion2.corp.google.com/invocations/efa8f495-da38-43db-aff3-b51bdf9df4ec
     https://fusion2.corp.google.com/invocations/70f5498e-71bb-4460-83f9-ce3fb171e950
    
    Change-Id: I646b69948cf2302d49bc7e57fb0e0a29ba1e7b56
    
    GitOrigin-RevId: 35da9261f428364e0ecc5d7d7a4cae18b2f86e82
    (cherry picked from commit 76d7d0bae0c275c79e347a2eba98e605d041b391)

--- as-driver/plugin/src/com/android/tools/asdriver/StudioInteractionService.java
+++ as-driver/plugin/src/com/android/tools/asdriver/StudioInteractionService.java
@@ -716,16 +716,19 @@ public class StudioInteractionService {
     long msBetweenRetries = 300;
     long startTime = System.currentTimeMillis();
     long elapsedTime = 0;
-    final AtomicBoolean clicked = new AtomicBoolean(false);
+
+    final AtomicBoolean foundComponent = new AtomicBoolean(false);
+    final AtomicBoolean invokedComponent = new AtomicBoolean(false);
+    Application app = ApplicationManager.getApplication();
 
     while (elapsedTime < timeoutMillis) {
-      ApplicationManager.getApplication().invokeAndWait(() -> {
+      app.invokeLater(() -> {
         try {
           XpathDataModelCreator creator = new XpathDataModelCreator(true);
           org.w3c.dom.Document doc = creator.create(null, false, null);
 
           XPathFactory xPathFactory = XPathFactory.newInstance();
-          NodeList result = (NodeList)xPathFactory.newXPath().compile(xpath).evaluate(doc, XPathConstants.NODESET);
+          NodeList result = (NodeList) xPathFactory.newXPath().compile(xpath).evaluate(doc, XPathConstants.NODESET);
 
           Component componentToClick = null;
           for (int i = 0; i < result.getLength(); i++) {
@@ -733,7 +736,7 @@ public class StudioInteractionService {
               Object userObj = element.getUserData("component");
 
               if (userObj instanceof Component) {
-                Component c = (Component)userObj;
+                Component c = (Component) userObj;
                 if (c.isShowing()) {
                   componentToClick = c;
                   break;
@@ -743,25 +746,34 @@ public class StudioInteractionService {
           }
 
           if (componentToClick != null && componentToClick.isEnabled()) {
-            if (tryClickStrategies(componentToClick)) {
-              clicked.set(true);
-            }
+            foundComponent.set(true);
+            Component finalComponent = componentToClick;
+
+            Runnable execComponent = () -> tryClickStrategies(finalComponent);
+            ((TransactionGuardImpl) TransactionGuard.getInstance()).performUserActivity(execComponent);
           }
-        }
-        catch (Throwable e) {
+        } catch (Throwable e) {
           // Retry
         }
       }, ModalityState.any());
 
-      if (clicked.get()) {
-        return;
+      app.invokeAndWait(() -> {
+        if (foundComponent.get()) {
+          invokedComponent.set(true);
+        }
+      }, ModalityState.any());
+
+      if (invokedComponent.get()) {
+        break;
       }
 
       Thread.sleep(msBetweenRetries);
       elapsedTime = System.currentTimeMillis() - startTime;
     }
 
-    throw new TimeoutException("Timed out after " + elapsedTime + "ms trying to click component by xpath: " + xpath);
+    if (elapsedTime >= timeoutMillis) {
+      throw new TimeoutException("Timed out after " + elapsedTime + "ms trying to click component by xpath: " + xpath);
+    }
   }
 
   /**
