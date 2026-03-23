7b7212f141233f336d75752950bf1420ef4b288a
Yuriy Solodkyy <solodkyy@google.com>
2026-03-23 20:50:50 +0000

    Migrate RunLineMarkerContributor.Info constructor
    Simplifies `BuildFileRunLineMarkerContributor` by adopting the updated
    `RunLineMarkerContributor.Info` constructor signature, removing the
    deprecated lambda tool-tip provider.
    
    Bug: n/a
    Test: n/a
    Change-Id: I4583d97e38e270722e3109fcd5cea0b08ef3a112
    
    GitOrigin-RevId: 8a26f64f5352313d12e929fc4003fada14898769
    (cherry picked from commit 9c3b306c15618abc76f34fa37b3a30245e3dd6c5)

--- aswb/base/src/com/google/idea/blaze/base/run/producers/BuildFileRunLineMarkerContributor.java
+++ aswb/base/src/com/google/idea/blaze/base/run/producers/BuildFileRunLineMarkerContributor.java
@@ -32,13 +32,11 @@ import com.intellij.execution.lineMarker.ExecutorAction;
 import com.intellij.execution.lineMarker.RunLineMarkerContributor;
 import com.intellij.icons.AllIcons;
 import com.intellij.openapi.actionSystem.AnAction;
-import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.psi.PsiComment;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiWhiteSpace;
 import com.intellij.psi.impl.source.tree.LeafElement;
-import com.intellij.util.containers.ContainerUtil;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeoutException;
 import javax.annotation.Nullable;
@@ -63,12 +61,7 @@ public class BuildFileRunLineMarkerContributor extends RunLineMarkerContributor 
       return null;
     }
     AnAction[] actions = ExecutorAction.getActions();
-    return new Info(
-        AllIcons.RunConfigurations.TestState.Run,
-        actions,
-        psiElement ->
-            StringUtil.join(
-                ContainerUtil.mapNotNull(actions, action -> getText(action, psiElement)), "\n"));
+    return new Info(AllIcons.RunConfigurations.TestState.Run, actions);
   }
 
   private static boolean isRunContext(PsiElement element) {
@@ -89,7 +82,8 @@ public class BuildFileRunLineMarkerContributor extends RunLineMarkerContributor 
     // - dirty file status somehow?
     // - override RunLineMarkerProvider, supporting collectSlowLineMarkers
     ListenableFuture<TargetInfo> future =
-        TargetFinder.findTargetInfoFuture(element.getProject(), com.google.idea.blaze.common.Label.of(data.label.toString()));
+        TargetFinder.findTargetInfoFuture(
+            element.getProject(), com.google.idea.blaze.common.Label.of(data.label.toString()));
     try {
       TargetInfo target = future.get(2, SECONDS);
       return target != null && HANDLED_RULE_TYPES.contains(target.getRuleType());
