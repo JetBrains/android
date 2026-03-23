542775581024dd43e62db8a92dd387d19c382e58
Arvind <arvindtech@google.com>
2026-03-23 07:21:41 +0000

    Use local variable for StatLabel  in CapturePanel
    When updating the `totalLeakLabel` and `totalBitmapDuplicatesLabel` in
    `CapturePanel`, the UI checks if `numValue > 0` to display a warning
    icon. However, `StatLabel.numValue` is not designed for state retrieval
    after setting; reading `numValue` immediately after setting it returns
    the initial constructor value (0), causing the icon condition to always
    evaluate to false.
    
    This change fixes the issue by storing the calculated instance count in
    a local variable `count` and using that for both setting `numValue` and
    evaluating the icon condition.
    
    Bug: 494286580
    Test: Manual
    Change-Id: I7b5840484621c872e143c4892076bfdd418b6137
    
    GitOrigin-RevId: 9d3dd1443cbe0c8f77ce20ad4d79683665b030f4
    (cherry picked from commit 0846088db3608346e9f07bc79f69d19a6b67d293)

--- profilers-ui/src/com/android/tools/profilers/memory/CapturePanel.kt
+++ profilers-ui/src/com/android/tools/profilers/memory/CapturePanel.kt
@@ -255,18 +255,15 @@ private class CapturePanelUi(
             return@let
           }
 
-          totalLeakLabel.apply {
-            val leakFilter = capture.activityFragmentLeakFilter
-            numValue = getFilteredInstanceCount(heap, selection.selectedClassTypeFilter, leakFilter)
-            isVisible = true
-            icon = if (numValue > 0) StudioIcons.Common.WARNING else null
+          fun updateLabel(label: StatLabel, filter: CaptureObjectInstanceFilter) {
+            val count = getFilteredInstanceCount(heap, selection.selectedClassTypeFilter, filter)
+            label.numValue = count
+            label.isVisible = true
+            label.icon = if (count > 0) StudioIcons.Common.WARNING else null
           }
-          totalBitmapDuplicatesLabel.apply {
-            val dupeFilter = capture.bitmapDuplicationFilter
-            numValue = getFilteredInstanceCount(heap, selection.selectedClassTypeFilter, dupeFilter)
-            isVisible = true
-            icon = if (numValue > 0) StudioIcons.Common.WARNING else null
-          }
+
+          updateLabel(totalLeakLabel, capture.activityFragmentLeakFilter)
+          updateLabel(totalBitmapDuplicatesLabel, capture.bitmapDuplicationFilter)
         }
       }
 
