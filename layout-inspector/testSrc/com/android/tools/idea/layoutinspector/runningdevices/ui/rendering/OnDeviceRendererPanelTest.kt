a9d84d0a3193938155977cadcd3942082f0c516c
Jens Ole Lauridsen <jlauridsen@google.com>
2026-03-25 19:15:23 +0000

    Re-enable testDisposeCancelsScope
    This test doesn't seem to fail even after running the layout tests
    200 times.
    
    Fixes: 459769542
    Test: OnDeviceRendererPanelTest
    
    Change-Id: I1259102242b4c4ea92e0d24e778ab8ab93c1f282
    
    GitOrigin-RevId: 0948d6edc69c5874fcc4faa68944e5811fa9f9b0
    (cherry picked from commit 78a50e823e35ba45d219e765e5f804ea83d3bdd9)

--- layout-inspector/testSrc/com/android/tools/idea/layoutinspector/runningdevices/ui/rendering/OnDeviceRendererPanelTest.kt
+++ layout-inspector/testSrc/com/android/tools/idea/layoutinspector/runningdevices/ui/rendering/OnDeviceRendererPanelTest.kt
@@ -57,7 +57,6 @@ import kotlinx.coroutines.flow.emptyFlow
 import kotlinx.coroutines.test.StandardTestDispatcher
 import kotlinx.coroutines.test.runTest
 import org.junit.Before
-import org.junit.Ignore
 import org.junit.Rule
 import org.junit.Test
 
@@ -582,7 +581,6 @@ class OnDeviceRendererPanelTest {
   }
 
   @Test
-  @Ignore("b/459769542")
   fun testDisposeCancelsScope() = runTest {
     val (_, messenger) = buildMessenger()
     val onDeviceRenderingClient = OnDeviceRenderingClient(messenger = messenger)
