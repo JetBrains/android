29c2f85b291febbdeb8a61046a8c5cc8e5aa65c8
Phil Nguyen <philnguyen@google.com>
2026-03-26 10:44:02 -0700

    Remove now obsolete `WrongThread` suppressions
    This change also fixes the `UiThread` annotation on
    `invokeLaterIfNeeded`'s parameter to the syntax that Lint supports.
    
    Bug: 496344769
    Test: n/a
    Change-Id: I59a42f083b439c18f775ef4a15b01dc8cbee3ea0
    
    GitOrigin-RevId: 7a08398984ed9964f9b5a479cd4dcc5e922707fe
    (cherry picked from commit c34a5018f89aeab3cd8ef99363bd1eee23f14c9f)

--- streaming/src/com/android/tools/idea/streaming/core/StreamingToolWindowManager.kt
+++ streaming/src/com/android/tools/idea/streaming/core/StreamingToolWindowManager.kt
@@ -297,7 +297,6 @@ internal class StreamingToolWindowManager @AnyThread constructor(private val too
       @AnyThread
       override fun connectionStateChanged(emulator: EmulatorController, connectionState: ConnectionState) {
         if (connectionState == ConnectionState.DISCONNECTED) {
-          @Suppress("WrongThread") // b/496344769
           invokeLaterIfNeeded {
             if (removeEmulatorPanel(emulator)) {
               emulators.remove(emulator)
@@ -387,7 +386,6 @@ internal class StreamingToolWindowManager @AnyThread constructor(private val too
   @AnyThread
   private fun onDeviceHeadsUp(serialNumber: String, activation: ActivationLevel, project: Project) {
     if (project == toolWindow.project) {
-      @Suppress("WrongThread") // b/496344769
       invokeLaterIfNeeded {
         val excludedDevice = devicesExcludedFromMirroring.remove(serialNumber)
         when {
@@ -1016,7 +1014,6 @@ internal class StreamingToolWindowManager @AnyThread constructor(private val too
   @AnyThread
   private fun deviceConnected(serialNumber: String, device: ConnectedDevice) {
     val config = DeviceConfiguration(device.state.properties, useTitleAsName = isLocalEmulator(serialNumber))
-    @Suppress("WrongThread") // b/496344769
     invokeLaterIfNeeded { deviceConnected(serialNumber, device.handle, config) }
   }
 
@@ -1166,7 +1163,8 @@ internal class StreamingToolWindowManager @AnyThread constructor(private val too
   }
 
   @AnyThread
-  private fun invokeLaterIfNeeded(block: @UiThread () -> Unit) {
+  @Suppress("WrongThread") // b/379742474
+  private fun invokeLaterIfNeeded(@UiThread block: () -> Unit) {
     if (EDT.isCurrentThreadEdt()) {
       block()
     } else {
@@ -1183,7 +1181,6 @@ internal class StreamingToolWindowManager @AnyThread constructor(private val too
 
     override fun userInvolvementRequired(device1SerialNumber: String, device2SerialNumber: String, project: Project) {
       if (project == toolWindow.project) {
-        @Suppress("WrongThread") // b/496344769
         invokeLaterIfNeeded { showInSplitView(device1SerialNumber, device2SerialNumber) }
       }
     }
@@ -1279,7 +1276,6 @@ internal class StreamingToolWindowManager @AnyThread constructor(private val too
       coroutineScope = createCoroutineScope(executor.asCoroutineDispatcher())
       coroutineScope.launch {
         deviceProvisioner.mirrorableDevicesBySerialNumber().collect { newOnlineDevices ->
-          @Suppress("WrongThread") // b/496344769
           invokeLaterIfNeeded {
             onlineDevices = newOnlineDevices
             onlineDevicesChanged()
