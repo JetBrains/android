c47e1055921e4bbf657fc70ada564b0f7252cedf
Madalin Valceleanu <vmadalin@google.com>
2026-03-31 16:55:50 +0000

    Fix missing settings.gradle VFS discovery in Composite Build tests
    With IntelliJ 2026.1 as part of IJPL-214620 an optimization was
    introduced to the VFS in `CacheAvoidingVirtualFileWrapper` which
    now returns children directly from the VFS cache, instead of
    falling back to the physical disk.
    
    The TestProject.COMPOSITE_BUILD test manipulates the settings.gradle
    file on disk after the parent directory may have been initially
    accessed. Because the VFS cache considers the directory already
    cached, it bypasses the physical disk check entirely and fails to
    discover the newly modified or created settings.gradle files in
    the composite build directories. This leads to missing configuration
    when GradleBuildConfigurationSourceProvider when attempts to find
    Gradle Scripts.
    
    This issue only affects tests since in production an asynchronous
    file watcher automatically detects the physical file changes and
    triggers a VFS refresh.
    
    To address this, the VfsUtilCore.processFilesRecursively() is called
    to force the IDE to instantiate and load all VirtualFile objects in
    the VFS.
    
    Bug: 493857612
    Test: This
    Change-Id: If7b447262a5d8d5f5083b9a20074230912d15284
    
    GitOrigin-RevId: 83223246d2b4eb2203a8d5e33807cfea064d2f20
    (cherry picked from commit e29f6d8d3b472e76547e7b2d1232488061100cef)

--- project-system-gradle/testSrc/com/android/tools/idea/gradle/project/sync/snapshots/TestProject.kt
+++ project-system-gradle/testSrc/com/android/tools/idea/gradle/project/sync/snapshots/TestProject.kt
@@ -31,10 +31,12 @@ import com.android.tools.idea.testing.ModelVersion
 import com.android.tools.idea.testing.TestProjectPaths
 import com.android.tools.idea.testing.TestProjectToSnapshotPaths
 import com.android.tools.idea.testing.resolve
+import com.android.tools.idea.util.toVirtualFile
 import com.google.common.truth.Expect
 import com.google.common.truth.Truth.assertThat
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VfsUtil
+import com.intellij.openapi.vfs.VfsUtilCore
 import com.intellij.util.PathUtil
 import java.io.File
 import java.nio.file.Files
@@ -215,6 +217,11 @@ enum class TestProject(
     patch = { projectRoot ->
       if (modelVersion == ModelVersion.V2) {
         truncateForV2(projectRoot.resolve("settings.gradle"))
+
+        // Synchronize the file modification with VFS
+        projectRoot.toVirtualFile()?.let {
+          VfsUtilCore.processFilesRecursively(it) { true }
+        }
       }
     },
   ),
