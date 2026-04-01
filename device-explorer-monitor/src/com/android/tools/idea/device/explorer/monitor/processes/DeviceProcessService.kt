2562bc040b69aa746673d712e5073892649dd190
Alexander Demurov <ademurov@google.com>
2026-04-01 13:44:14 -0700

    Add `pm clear` support to `AdbPackageManagerServices`
    Adds `clear` method to `AdbPackageManagerServices` and
    `ConnectedDevice.packageManager`.
    Switches `DeviceProcessServices` to use this method instead of running
    a `shell` command directly.
    
    Bug: n/a
    Test: added
    Change-Id: If991afa8eda1f7018cbe4280c914beb1fc5a2a51
    
    GitOrigin-RevId: df420e79c1e8a91c9cb368b5ee1e010051bde1a6
    (cherry picked from commit 42882374e5f2b594c1235c94f1238d2b7c8ba60b)

--- device-explorer-monitor/src/com/android/tools/idea/device/explorer/monitor/processes/DeviceProcessService.kt
+++ device-explorer-monitor/src/com/android/tools/idea/device/explorer/monitor/processes/DeviceProcessService.kt
@@ -15,15 +15,12 @@
  */
 package com.android.tools.idea.device.explorer.monitor.processes
 
-// TODO: android-merge; AdbPackageManagerException is not in the studio-platform jar, see uninstallApp below
-//import com.android.adblib.AdbPackageManagerException
+import com.android.adblib.AdbPackageManagerException
 import com.android.adblib.ConnectedDevice
 import com.android.adblib.activityManager
 import com.android.adblib.ddmlibcompatibility.debugging.associatedIDevice
-// TODO: android-merge; ConnectedDevice.packageManager is not in the studio-platform jar, see uninstallApp below
-//import com.android.adblib.packageManager
+import com.android.adblib.packageManager
 import com.android.adblib.serialNumber
-import com.android.adblib.shell
 import com.android.adblib.tools.debugging.jdwpProcessTracker
 import com.android.adblib.tools.debugging.sendDdmsExit
 import com.android.annotations.concurrency.UiThread
@@ -130,9 +127,10 @@ constructor(
       withContext(workerThreadDispatcher) {
         val packageName = process.packageName
         if (packageName != null) {
-          val result = device.shell.executeAsText("pm clear $packageName")
-          if (result.stdout.trim() != "Success") {
-            thisLogger().info("Clear App Data $packageName failed with output: ${result.stdout} ${result.stderr}")
+          try {
+            device.packageManager.clear(packageName)
+          } catch (e: AdbPackageManagerException) {
+            thisLogger().info("Clear App Data $packageName failed with output: ${e.errorOutput}")
             withContext(uiThreadDispatcher) { reportError("clear app data", "Failed to clear app data.") }
           }
         } else {
