27670d65658764602dbecd6a13f94097ba36cdba
Jens Ole Lauridsen <jlauridsen@google.com>
2026-04-02 20:47:34 +0000

    Increase the timeout for waitForPopup
    Fixes: 498969804
    Test: EmulatorUiSettingsIntegrationTest
    Change-Id: I76fafa8d154fde842cb2ecaabb39b7bb42a2767f
    
    GitOrigin-RevId: 367fe44813a12212051fdcc43aa6b39128da8f9d
    (cherry picked from commit 803276ac3470859a4013d104ea4d1035e51590f5)

--- streaming/integration/com/android/tools/idea/streaming/uisettings/UiSettingsIntegrationRule.kt
+++ streaming/integration/com/android/tools/idea/streaming/uisettings/UiSettingsIntegrationRule.kt
@@ -249,7 +249,7 @@ internal class UiSettingsIntegrationRule : ExternalResource() {
   }
 
   private fun waitForPopup(): JBPopup {
-    waitForCondition(3.seconds) { popupRule.fakePopupFactory.popupCount > 0 }
+    waitForCondition(10.seconds) { popupRule.fakePopupFactory.popupCount > 0 }
     return findPopup()
   }
 
