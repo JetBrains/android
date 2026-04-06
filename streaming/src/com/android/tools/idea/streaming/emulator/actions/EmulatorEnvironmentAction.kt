7fe08f2f58950a906537c9faa063ec398edc4675
Sergey Prigogin <sprigogin@google.com>
2026-04-06 10:05:54 -0700

    Set minimum emulator version for EmulatorEnvironmentAction
    Bug: 488039949
    Test: existing
    Change-Id: I9935e159d804ece762918d5d28bec39df2ce1fc3
    
    GitOrigin-RevId: 2e8f9db37bb864b3d3f6100bd5fb27a6d5b43258
    (cherry picked from commit 9c60c9e0145e26809ba638dee3de5275ad926fe7)

--- streaming/src/com/android/tools/idea/streaming/emulator/actions/EmulatorEnvironmentAction.kt
+++ streaming/src/com/android/tools/idea/streaming/emulator/actions/EmulatorEnvironmentAction.kt
@@ -87,7 +87,6 @@ internal sealed class EmulatorEnvironmentAction : AbstractEmulatorAction(configF
     // TODO: Remove emulator version check after 2026-09-01.
     val emulatorSupported =
       ApplicationManager.getApplication().isUnitTestMode ||
-        // TODO: Replace with emulator version that contains fixes for b/499028965 and b/499030315.
-        AvdManagerConnection.getDefaultAvdManagerConnection().emulator?.version?.let { it >= Revision(99, 9, 9) } ?: false
+        AvdManagerConnection.getDefaultAvdManagerConnection().emulator?.version?.let { it >= Revision(36, 6, 4) } ?: false
   }
 }
