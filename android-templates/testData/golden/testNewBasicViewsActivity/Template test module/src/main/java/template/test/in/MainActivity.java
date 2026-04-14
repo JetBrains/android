ea644fb78094447dfe1701375871820411354e7e
Ayush Jain <iayushj@google.com>
2026-04-14 11:53:04 +0530

    Update golden files for Basic Views Activity
    Test: TemplateDiffTest
    Bug: 438069971
    Change-Id: I6aae1e28379a9df391b83118388a0c05999b56bd
    
    GitOrigin-RevId: 9a4d57bb2d70e1ae746ecace255d56c0160e6458
    (cherry picked from commit 600f13e05509d74c5b674c130310ac2e010fa9d7)

--- android-templates/testData/golden/testNewBasicViewsActivity/Template test module/src/main/java/template/test/in/MainActivity.java
+++ android-templates/testData/golden/testNewBasicViewsActivity/Template test module/src/main/java/template/test/in/MainActivity.java
@@ -17,6 +17,7 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+import androidx.navigation.fragment.NavHostFragment;
 
 import template.test.in.databinding.ActivityMainBinding;
 
@@ -45,9 +46,15 @@ public class MainActivity extends AppCompatActivity {
         });
         setSupportActionBar(binding.toolbar);
 
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.nav_host_fragment_content_main);
+
+        if (navHostFragment != null) {
+            NavController navController = navHostFragment.getNavController();
+
+            appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+            NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        }
 
         binding.fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -83,7 +90,9 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.nav_host_fragment_content_main);
+        NavController navController = navHostFragment.getNavController();
         return NavigationUI.navigateUp(navController, appBarConfiguration)
                 || super.onSupportNavigateUp();
     }
