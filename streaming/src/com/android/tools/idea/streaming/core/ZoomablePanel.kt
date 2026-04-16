c9740acd68dff40f39391bfe722dbaaba6878c0b
Sergey Prigogin <sprigogin@google.com>
2026-04-15 18:51:54 -0700

    Remove a redundant qualifier
    Bug: N/A
    Test: existing
    Change-Id: Id899e01badfe6008a16e6a1e97537254716ee1ad
    
    GitOrigin-RevId: 03ea97742a9e9bc87f7cb442b7e572701b7b9b00
    (cherry picked from commit 921f0e4bb699201a348a8307b038a36314b8c6d0)

--- streaming/src/com/android/tools/idea/streaming/core/ZoomablePanel.kt
+++ streaming/src/com/android/tools/idea/streaming/core/ZoomablePanel.kt
@@ -31,7 +31,7 @@ private val ZOOM_LEVELS = doubleArrayOf(0.0625, 0.125, 0.25, 0.5, 1.0, 2.0, 4.0)
 internal abstract class ZoomablePanel : BorderLayoutPanel(), Zoomable {
 
   override val screenScalingFactor: Double
-    get() = if (JreHiDpiUtil.isJreHiDPI(this.graphicsConfiguration)) JBUIScale.sysScale(this).toDouble() else 1.0
+    get() = if (JreHiDpiUtil.isJreHiDPI(graphicsConfiguration)) JBUIScale.sysScale(this).toDouble() else 1.0
 
   /** Width in physical pixels. */
   protected val physicalWidth
