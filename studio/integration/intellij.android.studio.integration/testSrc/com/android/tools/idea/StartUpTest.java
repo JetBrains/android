f5b3604e6b7c30f9918d080e5c96bfe5bde645b5
Matthew Gharrity <gharrma@google.com>
2026-04-21 00:29:39 +0000

    Update generated files
    Change-Id: I4a5e77c740b0ad0041d9b7748e544d886bf4d09a
    
    GitOrigin-RevId: da5e3699e2f05a6ef7a26d18db5820b679cc07f6
    (cherry picked from commit 22331f4b695feee67e6276f71c8b28174d61f65d)

--- studio/integration/intellij.android.studio.integration/testSrc/com/android/tools/idea/StartUpTest.java
+++ studio/integration/intellij.android.studio.integration/testSrc/com/android/tools/idea/StartUpTest.java
@@ -148,7 +148,6 @@ public class StartUpTest {
                                     "TestNG",
                                     "TextMate Bundles",
                                     "Toml",
-                                    "Turbo Complete",
                                     "Visual Studio Keymap",
                                     "WebP Support",
                                     "YAML"));
