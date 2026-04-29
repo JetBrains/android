155a51ca702f57be82574d642ebe317af5cd9505
Vishal Gupta <vishalga@google.com>
2026-04-29 05:19:00 +0000

    Add a "manual edit warning" comment to generated golden files
    This change updates the GoldenFileGenerator to automatically
    prepend a warning header to all generated golden files. The
    comment "This file should not be edited manually! See
    go/template-diff-tests" aims to prevent direct modifications
    to these test artifacts.
    
    The comment syntax is adapted based on the file extension:
    - `<!-- ... -->` for .xml files
    - `// ...` for .cpp, .gradle, .h, .java, .kt files
    - `# ...` for .properties, .txt, .toml files
    
    For XML files, the comment is intelligently placed after the
    `<?xml ... ?>` declaration if it exists.
    
    Bug: 302711575
    Test: Verified by generating golden files in other CLs.
    Change-Id: Iefe1786af066c196c559f40018a1578bb9f19981
    
    GitOrigin-RevId: 8447863aa204b1e867d584ef57f632e3a3392031
    (cherry picked from commit 527855258c42c933a2181eac535fb805dd276513)

--- android-templates/testSrc/com/android/tools/idea/templates/diff/activity/GoldenFileGenerator.kt
+++ android-templates/testSrc/com/android/tools/idea/templates/diff/activity/GoldenFileGenerator.kt
@@ -21,6 +21,8 @@ import com.android.utils.FileUtils
 import java.io.File
 import java.nio.file.Path
 
+const val GOLDEN_FILE_HEADER_COMMENT = "This file should not be edited manually! See go/template-diff-tests"
+
 /**
  * Generates files from a template and performs checks on them to ensure they're valid and can be checked in as golden files, then copies
  * the validated files to the output directory.
@@ -33,7 +35,47 @@ class GoldenFileGenerator(template: Template, goldenDirName: String) : ProjectRe
 
     FileUtils.deleteRecursivelyIfExists(outputDir.toFile())
     FileUtils.copyDirectory(projectDir.toFile(), outputDir.toFile())
+
+    outputDir.toFile().walk().forEach { file ->
+      if (file.isFile) {
+        addHeaderComment(file)
+      }
+    }
+
     FILES_TO_IGNORE.forEach { FileUtils.deleteRecursivelyIfExists(goldenDir.resolve(it).toFile()) }
+  }
+
+  private fun addHeaderComment(file: File) {
+    val extension = file.extension
+    val comment =
+      when (extension) {
+        "xml" -> "<!-- $GOLDEN_FILE_HEADER_COMMENT -->"
+        "cpp",
+        "gradle",
+        "h",
+        "java",
+        "kt" -> "// $GOLDEN_FILE_HEADER_COMMENT"
+        "properties",
+        "txt",
+        "toml" -> "# $GOLDEN_FILE_HEADER_COMMENT"
+        else -> return // Skip unknown types
+      }
+    val content = file.readText()
+    val newContent =
+      if (extension == "xml" && content.startsWith("<?xml")) {
+        if (content.contains("?><!--")) {
+          // Formatter bug detected! Put the warning at the bottom to avoid breaking the file.
+          if (content.endsWith("\n")) content + comment else "$content\n$comment"
+        } else {
+          // No collision. Safe to put the warning right after the XML header.
+          val headerEnd = content.indexOf("?>")
+          content.substring(0, headerEnd + 2) + "\n" + comment + content.substring(headerEnd + 2)
+        }
+      } else {
+        // Non-XML files or XML files without headers
+        "$comment\n$content"
+      }
+    file.writeText(newContent)
   }
 
   override fun prepareProject(projectRoot: File) {
