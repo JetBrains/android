msft-mirror-aosp/platform.tools.adt.idea

Extract GradleAndroidDependencyModel into its file
To make upcoming change a bit more clean.

Bug: 384022658
Test: n/a
Change-Id: I3f6460f4133e7f0351495ae44d2d3950cf20fabe

project-system-gradle/src/com/android/tools/idea/gradle/project/model/GradleAndroidDependencyModel.kt
@@ -1,0 +1,96 @@
+/*
+ * Copyright (C) 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.gradle.project.model
+
+import com.android.tools.idea.gradle.model.IdeAndroidArtifact
+import com.android.tools.idea.gradle.model.IdeAndroidProjectType
+import com.android.tools.idea.gradle.model.IdeArtifactName
+import com.android.tools.idea.gradle.model.IdeDependencies
+import com.android.tools.idea.gradle.model.IdeJavaArtifact
+import com.android.tools.idea.gradle.model.IdeLibraryModelResolver
+import com.android.tools.idea.gradle.model.IdeVariant
+import com.android.tools.idea.gradle.model.impl.IdeAndroidArtifactImpl
+import com.android.tools.idea.gradle.model.impl.IdeJavaArtifactImpl
+import com.android.tools.idea.gradle.model.impl.IdeLibraryModelResolverImpl
+import com.android.tools.idea.gradle.model.impl.IdeVariantImpl
+import com.android.tools.idea.model.AndroidModel
+import com.intellij.openapi.module.Module
+import com.intellij.openapi.project.Project
+import com.jetbrains.rd.util.getOrCreate
+import org.jetbrains.android.facet.AndroidFacet
+import org.jetbrains.annotations.VisibleForTesting
+
+sealed interface GradleAndroidDependencyModel: GradleAndroidModel {
+  fun getArtifactForScreenshotTest(): IdeJavaArtifact?
+  val selectedAndroidTestCompileDependencies: IdeDependencies?
+  val mainArtifactWithDependencies: IdeAndroidArtifact
+  val selectedVariantWithDependencies: IdeVariant
+  val variantsWithDependencies: List<IdeVariant>
+  fun getArtifactForAndroidTest(): IdeAndroidArtifact?
+
+  @VisibleForTesting
+  fun containsTheSameDataAs(that: GradleAndroidDependencyModel): Boolean
+
+  companion object {
+    @JvmStatic
+    fun get(module: Module): GradleAndroidDependencyModel? = AndroidModel.get(
+      module) as? GradleAndroidDependencyModel
+
+    @JvmStatic
+    fun get(androidFacet: AndroidFacet): GradleAndroidDependencyModel? = AndroidModel.get(
+      androidFacet) as? GradleAndroidDependencyModel
+
+    @JvmStatic
+    fun createFactory(project: Project, libraryResolver: IdeLibraryModelResolver): (GradleAndroidModelData) -> GradleAndroidDependencyModel {
+      val models = mutableMapOf<GradleAndroidModelData, GradleAndroidDependencyModel>()
+      return fun(data: GradleAndroidModelData): GradleAndroidDependencyModel {
+        return models.getOrCreate(data) { GradleAndroidDependencyModelImpl(GradleAndroidModel.create(project, data) as GradleAndroidModelImpl,
+                                                                           libraryResolver as IdeLibraryModelResolverImpl
+        ) }
+      }
+    }
+  }
+}
+
+@VisibleForTesting
+class GradleAndroidDependencyModelImpl(
+  val gradleAndroidModel: GradleAndroidModelImpl,
+  private val ideLibraryModelResolver: IdeLibraryModelResolverImpl
+): GradleAndroidDependencyModel, GradleAndroidModelImpl(gradleAndroidModel) {
+  private val myCachedResolvedVariantsByName: Map<String, IdeVariantImpl> =
+    variants.associate { it.name to IdeVariantImpl(it, ideLibraryModelResolver) }
+  override val selectedVariantWithDependencies: IdeVariantImpl get () = myCachedResolvedVariantsByName[selectedVariantName] ?: unknownSelectedVariant()
+  override val variantsWithDependencies: List<IdeVariantImpl>
+    get() = myCachedResolvedVariantsByName.values.toList()
+  /** Returns the artifact used for instrumented testing. For test-only modules this is the main artifact. */
+  override fun getArtifactForAndroidTest(): IdeAndroidArtifactImpl? {
+    return when (androidProject.projectType) {
+      IdeAndroidProjectType.PROJECT_TYPE_TEST -> selectedVariantWithDependencies.mainArtifact
+      else -> selectedVariantWithDependencies.deviceTestArtifacts.find { it.name == IdeArtifactName.ANDROID_TEST }
+    }
+  }
+  /** Returns the artifact used for screenshot testing. For screenshot test-only modules this is the main artifact. */
+  override fun getArtifactForScreenshotTest(): IdeJavaArtifactImpl? {
+    return selectedVariantWithDependencies.hostTestArtifacts.find { it.name == IdeArtifactName.SCREENSHOT_TEST }
+  }
+
+  override val selectedAndroidTestCompileDependencies: IdeDependencies? get() = getArtifactForAndroidTest()?.compileClasspath
+
+  override val mainArtifactWithDependencies: IdeAndroidArtifactImpl get() = selectedVariantWithDependencies.mainArtifact
+
+  @VisibleForTesting
+  override fun containsTheSameDataAs(that: GradleAndroidDependencyModel) = gradleAndroidModel.containsTheSameDataAs((that as GradleAndroidDependencyModelImpl).gradleAndroidModel)
+}

JetBrains/android

GitOrigin-RevId: 7d056b813cf6168e0777a1a2faf76683795d7ea3
(cherry picked from commit 6a2546384288b407e9580bbe81429f89a1b93c5b)
Space-RevId: f34eb2cc63644af6ed9d3936fbd8e6259cf65af6

project-system-gradle/src/com/android/tools/idea/gradle/project/model/GradleAndroidDependencyModel.kt
@@ -1,0 +1,94 @@
+/*
+ * Copyright (C) 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.gradle.project.model
+
+import com.android.tools.idea.gradle.model.IdeAndroidArtifact
+import com.android.tools.idea.gradle.model.IdeAndroidProjectType
+import com.android.tools.idea.gradle.model.IdeArtifactName
+import com.android.tools.idea.gradle.model.IdeDependencies
+import com.android.tools.idea.gradle.model.IdeJavaArtifact
+import com.android.tools.idea.gradle.model.IdeLibraryModelResolver
+import com.android.tools.idea.gradle.model.IdeVariant
+import com.android.tools.idea.gradle.model.impl.IdeAndroidArtifactImpl
+import com.android.tools.idea.gradle.model.impl.IdeJavaArtifactImpl
+import com.android.tools.idea.gradle.model.impl.IdeLibraryModelResolverImpl
+import com.android.tools.idea.gradle.model.impl.IdeVariantImpl
+import com.android.tools.idea.model.AndroidModel
+import com.intellij.openapi.module.Module
+import com.intellij.openapi.project.Project
+import org.jetbrains.android.facet.AndroidFacet
+import org.jetbrains.annotations.VisibleForTesting
+
+sealed interface GradleAndroidDependencyModel: GradleAndroidModel {
+  fun getArtifactForScreenshotTest(): IdeJavaArtifact?
+  val selectedAndroidTestCompileDependencies: IdeDependencies?
+  val mainArtifactWithDependencies: IdeAndroidArtifact
+  val selectedVariantWithDependencies: IdeVariant
+  val variantsWithDependencies: List<IdeVariant>
+  fun getArtifactForAndroidTest(): IdeAndroidArtifact?
+
+  @VisibleForTesting
+  fun containsTheSameDataAs(that: GradleAndroidDependencyModel): Boolean
+
+  companion object {
+    @JvmStatic
+    fun get(module: Module): GradleAndroidDependencyModel? = AndroidModel.get(
+      module) as? GradleAndroidDependencyModel
+
+    @JvmStatic
+    fun get(androidFacet: AndroidFacet): GradleAndroidDependencyModel? = AndroidModel.get(
+      androidFacet) as? GradleAndroidDependencyModel
+
+    @JvmStatic
+    fun createFactory(project: Project, libraryResolver: IdeLibraryModelResolver): (GradleAndroidModelData) -> GradleAndroidDependencyModel {
+      val models = mutableMapOf<GradleAndroidModelData, GradleAndroidDependencyModel>()
+      return fun(data: GradleAndroidModelData): GradleAndroidDependencyModel {
+        return models.computeIfAbsent(data) { GradleAndroidDependencyModelImpl(GradleAndroidModel.create(project, data) as GradleAndroidModelImpl,
+                                                                               libraryResolver as IdeLibraryModelResolverImpl) }
+      }
+    }
+  }
+}
+
+@VisibleForTesting
+class GradleAndroidDependencyModelImpl(
+  val gradleAndroidModel: GradleAndroidModelImpl,
+  private val ideLibraryModelResolver: IdeLibraryModelResolverImpl
+): GradleAndroidDependencyModel, GradleAndroidModelImpl(gradleAndroidModel) {
+  private val myCachedResolvedVariantsByName: Map<String, IdeVariantImpl> =
+    variants.associate { it.name to IdeVariantImpl(it, ideLibraryModelResolver) }
+  override val selectedVariantWithDependencies: IdeVariantImpl get () = myCachedResolvedVariantsByName[selectedVariantName] ?: unknownSelectedVariant()
+  override val variantsWithDependencies: List<IdeVariantImpl>
+    get() = myCachedResolvedVariantsByName.values.toList()
+  /** Returns the artifact used for instrumented testing. For test-only modules this is the main artifact. */
+  override fun getArtifactForAndroidTest(): IdeAndroidArtifactImpl? {
+    return when (androidProject.projectType) {
+      IdeAndroidProjectType.PROJECT_TYPE_TEST -> selectedVariantWithDependencies.mainArtifact
+      else -> selectedVariantWithDependencies.deviceTestArtifacts.find { it.name == IdeArtifactName.ANDROID_TEST }
+    }
+  }
+  /** Returns the artifact used for screenshot testing. For screenshot test-only modules this is the main artifact. */
+  override fun getArtifactForScreenshotTest(): IdeJavaArtifactImpl? {
+    return selectedVariantWithDependencies.hostTestArtifacts.find { it.name == IdeArtifactName.SCREENSHOT_TEST }
+  }
+
+  override val selectedAndroidTestCompileDependencies: IdeDependencies? get() = getArtifactForAndroidTest()?.compileClasspath
+
+  override val mainArtifactWithDependencies: IdeAndroidArtifactImpl get() = selectedVariantWithDependencies.mainArtifact
+
+  @VisibleForTesting
+  override fun containsTheSameDataAs(that: GradleAndroidDependencyModel) = gradleAndroidModel.containsTheSameDataAs((that as GradleAndroidDependencyModelImpl).gradleAndroidModel)
+}