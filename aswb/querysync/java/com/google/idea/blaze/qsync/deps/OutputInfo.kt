msft-mirror-aosp/platform.tools.adt.idea

Add qs_external_transitive_runtime_jars
Bug: 443056614
Test: ExternalTransitiveRuntimeJarsTest
Change-Id: I603ebaee64049b4c6f5f94084d4f6abc98cea819

aswb/querysync/java/com/google/idea/blaze/qsync/deps/OutputInfo.kt
@@ -21,6 +21,7 @@ import com.google.common.collect.Multimap
 import com.google.devtools.build.lib.view.proto.Deps
 import com.google.idea.blaze.common.Label
 import com.google.idea.blaze.common.artifact.OutputArtifact
+import com.google.idea.blaze.common.artifact.OutputArtifactInfo
 import com.google.idea.blaze.qsync.java.JavaTargetInfo.JavaArtifacts
 import com.google.idea.blaze.qsync.java.cc.CcCompilationInfoOuterClass.CcCompilationInfo
 import com.google.idea.blaze.qsync.java.cc.CcCompilationInfoOuterClass.CcTargetInfo
@@ -36,6 +37,7 @@ interface OutputInfo {
   val ccTargets: Map<Label, CcTargetInfo>
   val ccToolchains: Map<String, CcToolchainInfo>
   val jars: List<OutputArtifact>
+  val externalTransitiveRuntimeJars: List<OutputArtifactInfo>
   val aars: List<OutputArtifact>
   val generatedSources: List<OutputArtifact>
   val allJavaArtifacts: Collection<OutputArtifact>
@@ -71,6 +73,8 @@ interface OutputInfo {
       get() = OutputGroup.entries.filter { it.usedBySymbolResolution }.flatMap { artifacts[it].orEmpty() }
     override val jars: List<OutputArtifact>
       get() = artifacts[OutputGroup.JARS].orEmpty()
+    override val externalTransitiveRuntimeJars: List<OutputArtifact>
+      get() = artifacts[OutputGroup.EXTERNAL_TRANSITIVE_RUNTIME_JARS].orEmpty()
     override val aars: List<OutputArtifact>
       get() = artifacts[OutputGroup.AARS].orEmpty()
     override val generatedSources: List<OutputArtifact>

JetBrains/android

Bug: 459565102
Change-Id: Ie45c67a5ae2cd1b612702d0f4d7a652075b32fad
GitOrigin-RevId: 9cc8c23789e9fc9a2f3aa125cb4b7626c54b17bb
Space-RevId: 63b72fb0a8ed8ba429351919f6e27240c6d71d72
(cherry picked from commit 583675a2bdfc2eaa69b3eb0bb483b8e4bc5eaab2)

aswb/querysync/java/com/google/idea/blaze/qsync/deps/OutputInfo.kt
@@ -36,10 +36,12 @@ interface OutputInfo {
   val ccTargets: Map<Label, CcTargetInfo>
   val ccToolchains: Map<String, CcToolchainInfo>
   val jars: List<OutputArtifact>
+  val externalTransitiveRuntimeJars: List<OutputArtifact>
   val aars: List<OutputArtifact>
   val generatedSources: List<OutputArtifact>
   val allJavaArtifacts: Collection<OutputArtifact>
   val ccGeneratedHeaders: List<OutputArtifact>
+  val ccInfoFile: List<OutputArtifact>
 
   val isEmpty: Boolean
   val exitCode: Int
@@ -71,12 +73,16 @@ interface OutputInfo {
       get() = OutputGroup.entries.filter { it.usedBySymbolResolution }.flatMap { artifacts[it].orEmpty() }
     override val jars: List<OutputArtifact>
       get() = artifacts[OutputGroup.JARS].orEmpty()
+    override val externalTransitiveRuntimeJars: List<OutputArtifact>
+      get() = artifacts[OutputGroup.EXTERNAL_TRANSITIVE_RUNTIME_JARS].orEmpty()
     override val aars: List<OutputArtifact>
       get() = artifacts[OutputGroup.AARS].orEmpty()
     override val generatedSources: List<OutputArtifact>
       get() = artifacts[OutputGroup.GENSRCS].orEmpty()
     override val ccGeneratedHeaders: List<OutputArtifact>
       get() = artifacts[OutputGroup.CC_GEN_HEADERS].orEmpty()
+    override val ccInfoFile: List<OutputArtifact>
+      get() = artifacts[OutputGroup.CC_INFO_FILE].orEmpty()
     override val isEmpty: Boolean
       get() = artifacts.isEmpty() && ccTargets.isEmpty() && ccToolchains.isEmpty()
 