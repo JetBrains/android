msft-mirror-aosp/platform.tools.adt.idea

Enable a background for glasses by default
While we're at it, add an end-to-end device creation test.

Bug: 458554409
Test: AddDeviceWizardTest
Change-Id: I5c7814047c2e07e828b934cd35ecfd49e9f78855

avd/testSrc/com/android/tools/idea/avd/AddDeviceWizardTest.kt
@@ -22,28 +22,36 @@ import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.assertIsEnabled
 import androidx.compose.ui.test.assertIsSelected
 import androidx.compose.ui.test.hasContentDescription
+import androidx.compose.ui.test.hasParent
+import androidx.compose.ui.test.hasTestTag
 import androidx.compose.ui.test.hasText
 import androidx.compose.ui.test.onAllNodesWithText
+import androidx.compose.ui.test.onFirst
 import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.onRoot
 import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.performKeyInput
 import androidx.compose.ui.test.pressKey
+import com.android.flags.junit.FlagRule
 import com.android.sdklib.AndroidVersion
 import com.android.sdklib.PathFileWrapper
 import com.android.sdklib.SystemImageTags
 import com.android.sdklib.devices.Device
 import com.android.sdklib.internal.avd.AvdManager
 import com.android.sdklib.internal.avd.ConfigKey
+import com.android.sdklib.internal.avd.EnvironmentKey
 import com.android.tools.adtui.compose.TestComposeWizard
 import com.android.tools.adtui.compose.utils.StudioComposeTestRule.Companion.createStudioComposeTestRule
 import com.android.tools.idea.avdmanager.AccelerationErrorCode
+import com.android.tools.idea.flags.StudioFlags
 import com.google.common.truth.Truth.assertThat
 import com.intellij.testFramework.ApplicationRule
 import com.intellij.testFramework.EdtRule
 import com.intellij.testFramework.RunsInEdt
 import java.nio.file.Files
+import kotlin.io.path.createTempDirectory
+import kotlin.io.path.listDirectoryEntries
 import org.junit.Rule
 import org.junit.Test
 
@@ -52,6 +60,7 @@ class AddDeviceWizardTest {
   @get:Rule val edtRule = EdtRule()
   @get:Rule val applicationRule = ApplicationRule()
   @get:Rule val composeTestRule = createStudioComposeTestRule()
+  @get:Rule val flagRule = FlagRule(StudioFlags.AI_GLASSES_DEVICE_SUPPORT_ENABLED, true)
 
   /**
    * Pick a device, advance, and then finish (using default system image and settings). Verify that
@@ -136,6 +145,58 @@ class AddDeviceWizardTest {
           )
         )
       assertThat(properties[ConfigKey.CAMERA_FRONT]).isEqualTo("none")
+    }
+  }
+
+  @Test
+  fun addAiGlassesDevice() {
+    // The AVD needs to be on a real filesystem for the copy of the default environment to work.
+    val fixture = SdkFixture(avdRoot = createTempDirectory("AddAiGlassesDeviceTest"))
+    with(fixture) {
+      val api36Glasses =
+        createLocalSystemImage(
+          "ai-glasses",
+          listOf(SystemImageTags.AI_GLASSES_TAG),
+          AndroidVersion(36, null, 9, false),
+        )
+      repoPackages.setLocalPkgInfos(listOf(api36Glasses))
+
+      val source = createLocalVirtualDeviceSource()
+      val wizard = createTestAddDeviceWizard(source)
+
+      composeTestRule.setContentWithSdkLocals { wizard.Content() }
+
+      composeTestRule.onNodeWithText("XR").performClick()
+      composeTestRule.onAllNodesWithText("AI Glasses", substring = true).onFirst().performClick()
+      composeTestRule.waitForIdle()
+
+      wizard.performAction(wizard.nextAction)
+
+      composeTestRule.onNodeWithText(api36Glasses.displayName).assertIsSelected()
+      composeTestRule.onNodeWithText("Additional settings").performClick()
+      // Glasses have background, not skin.
+      composeTestRule.onNodeWithText("Skin").assertDoesNotExist()
+      composeTestRule.onNodeWithText("Background").assertIsDisplayed()
+      // We need to disable the external storage, since we can't run mksdcard.
+      composeTestRule
+        .onNode(hasText("None") and hasParent(hasTestTag("StorageGroup")))
+        .performClick()
+
+      composeTestRule.waitForIdle()
+
+      wizard.performAction(wizard.finishAction)
+      wizard.awaitClose()
+
+      val avdFolder = avdRoot.listDirectoryEntries("*.avd").single()
+      val properties =
+        checkNotNull(
+          AvdManager.parseIniFile(PathFileWrapper(avdFolder.resolve("config.ini")), null)
+        )
+      assertThat(properties[ConfigKey.LCD_TRANSPARENT]).isEqualTo("yes")
+
+      val userSettings = AvdManager.parseEnvironmentFile(avdFolder, null)
+      assertThat(userSettings[EnvironmentKey.IMAGE])
+        .isEqualTo(defaultEnvironments().first().fileName)
     }
   }
 

JetBrains/android

GitOrigin-RevId: 47200234b99858972ecb94b7d28b47ab940ff0ba
Space-RevId: 4273b3e7edb1f540530d9e4f7c14bbd8872098e8
(cherry picked from commit 15409f3ffbaaa6c702eaff63bbedc1815fc6cc4f)

avd/testSrc/com/android/tools/idea/avd/AddDeviceWizardTest.kt
@@ -22,8 +22,11 @@ import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.assertIsEnabled
 import androidx.compose.ui.test.assertIsSelected
 import androidx.compose.ui.test.hasContentDescription
+import androidx.compose.ui.test.hasParent
+import androidx.compose.ui.test.hasTestTag
 import androidx.compose.ui.test.hasText
 import androidx.compose.ui.test.onAllNodesWithText
+import androidx.compose.ui.test.onFirst
 import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.onRoot
@@ -37,6 +40,7 @@ import com.android.sdklib.SystemImageTags
 import com.android.sdklib.devices.Device
 import com.android.sdklib.internal.avd.AvdManager
 import com.android.sdklib.internal.avd.ConfigKey
+import com.android.sdklib.internal.avd.EnvironmentKey
 import com.android.tools.adtui.compose.TestComposeWizard
 import com.android.tools.adtui.compose.utils.StudioComposeTestRule.Companion.createStudioComposeTestRule
 import com.android.tools.idea.avdmanager.AccelerationErrorCode
@@ -47,6 +51,8 @@ import com.intellij.testFramework.ApplicationRule
 import com.intellij.testFramework.EdtRule
 import com.intellij.testFramework.RunsInEdt
 import java.nio.file.Files
+import kotlin.io.path.createTempDirectory
+import kotlin.io.path.listDirectoryEntries
 import org.junit.Rule
 import org.junit.Test
 
@@ -55,7 +61,8 @@ class AddDeviceWizardTest {
   @get:Rule val edtRule = EdtRule()
   @get:Rule val applicationRule = ApplicationRule()
   @get:Rule val composeTestRule = createStudioComposeTestRule()
-  @get:Rule val flagRule = FlagRule(StudioFlags.XR_DEVICE_SUPPORT_ENABLED, true)
+  @get:Rule val xrFlagRule = FlagRule(StudioFlags.XR_DEVICE_SUPPORT_ENABLED, true)
+  @get:Rule val aiGlassesFlagRule = FlagRule(StudioFlags.AI_GLASSES_DEVICE_SUPPORT_ENABLED, true)
 
   /**
    * Pick a device, advance, and then finish (using default system image and settings). Verify that
@@ -141,6 +148,58 @@ class AddDeviceWizardTest {
           )
         )
       assertThat(properties[ConfigKey.CAMERA_FRONT]).isEqualTo("none")
+    }
+  }
+
+  @Test
+  fun addAiGlassesDevice() {
+    // The AVD needs to be on a real filesystem for the copy of the default environment to work.
+    val fixture = SdkFixture(avdRoot = createTempDirectory("AddAiGlassesDeviceTest"))
+    with(fixture) {
+      val api36Glasses =
+        createLocalSystemImage(
+          "ai-glasses",
+          listOf(SystemImageTags.AI_GLASSES_TAG),
+          AndroidVersion(36, null, 9, false),
+        )
+      repoPackages.setLocalPkgInfos(listOf(api36Glasses))
+
+      val source = createLocalVirtualDeviceSource()
+      val wizard = createTestAddDeviceWizard(source)
+
+      composeTestRule.setContentWithSdkLocals { wizard.Content() }
+
+      composeTestRule.onNodeWithText("XR").performClick()
+      composeTestRule.onAllNodesWithText("AI Glasses", substring = true).onFirst().performClick()
+      composeTestRule.waitForIdle()
+
+      wizard.performAction(wizard.nextAction)
+
+      composeTestRule.onNodeWithText(api36Glasses.displayName).assertIsSelected()
+      composeTestRule.onNodeWithText("Additional settings").performClick()
+      // Glasses have background, not skin.
+      composeTestRule.onNodeWithText("Skin").assertDoesNotExist()
+      composeTestRule.onNodeWithText("Background").assertIsDisplayed()
+      // We need to disable the external storage, since we can't run mksdcard.
+      composeTestRule
+        .onNode(hasText("None") and hasParent(hasTestTag("StorageGroup")))
+        .performClick()
+
+      composeTestRule.waitForIdle()
+
+      wizard.performAction(wizard.finishAction)
+      wizard.awaitClose()
+
+      val avdFolder = avdRoot.listDirectoryEntries("*.avd").single()
+      val properties =
+        checkNotNull(
+          AvdManager.parseIniFile(PathFileWrapper(avdFolder.resolve("config.ini")), null)
+        )
+      assertThat(properties[ConfigKey.LCD_TRANSPARENT]).isEqualTo("yes")
+
+      val userSettings = AvdManager.parseEnvironmentFile(avdFolder, null)
+      assertThat(userSettings[EnvironmentKey.IMAGE])
+        .isEqualTo(defaultEnvironments().first().fileName)
     }
   }
 