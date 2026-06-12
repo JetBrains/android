msft-mirror-aosp/platform.tools.adt.idea

Compile AndroidStudioPreferences in IJ 2025.3
ScriptDefinitionProvider had some changes that needed adapted to.

Change-Id: Ib36a2d86bf8ccdabd5106f00835bbc9aec70b8e9

project-system-gradle/src/com/android/tools/idea/gradle/util/AndroidStudioPreferences.java
@@ -16,8 +16,6 @@
 package com.android.tools.idea.gradle.util;
 
 import static com.intellij.openapi.options.Configurable.PROJECT_CONFIGURABLE;
-import static org.jetbrains.kotlin.idea.core.script.ScriptUtilsKt.getAllDefinitions;
-
 import com.intellij.compiler.CompilerWorkspaceConfiguration;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.extensions.ExtensionPoint;
@@ -28,8 +26,10 @@ import com.intellij.openapi.project.Project;
 import com.intellij.ui.EditorNotificationProvider;
 import java.util.Arrays;
 import java.util.List;
+import kotlin.sequences.SequencesKt;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.kotlin.idea.core.script.settings.KotlinScriptingSettingsStorage;
+import org.jetbrains.kotlin.idea.core.script.v1.settings.KotlinScriptingSettingsStorage;
+import org.jetbrains.kotlin.scripting.definitions.ScriptDefinitionProvider;
 
 public final class AndroidStudioPreferences {
   private static final List<String> PROJECT_PREFERENCES_TO_REMOVE = Arrays.asList(
@@ -64,13 +64,17 @@ public final class AndroidStudioPreferences {
 
     // Tests do not rely on this but it causes test flakiness as it can be executed after test finish during project disposal.
     if (!ApplicationManager.getApplication().isUnitTestMode()) {
-      // Disable KotlinScriptingSettings.autoReloadConfigurations flag, avoiding unexpected re-sync project with kotlin scripts
-      getAllDefinitions(project).forEach(scriptDefinition -> {
-        var settings = KotlinScriptingSettingsStorage.Companion.getInstance(project);
-        if (settings.isScriptDefinitionEnabled(scriptDefinition) && settings.autoReloadConfigurations(scriptDefinition)) {
-          settings.setAutoReloadConfigurations(scriptDefinition, false);
-        }
-      });
+      var scriptDefinitionProvider = ScriptDefinitionProvider.Companion.getInstance(project);
+      if (scriptDefinitionProvider != null) {
+        SequencesKt.asIterable(scriptDefinitionProvider.getCurrentDefinitions()).forEach(
+          scriptDefinitions -> {
+            var settings = KotlinScriptingSettingsStorage.Companion.getInstance(project);
+            if (settings.isScriptDefinitionEnabled(scriptDefinitions) && settings.autoReloadConfigurations(scriptDefinitions)) {
+              settings.setAutoReloadConfigurations(scriptDefinitions, false);
+            }
+          }
+        );
+      }
     }
 
     // Note: This unregisters the extensions when the predicate returns False.

JetBrains/android

GitOrigin-RevId: d5f772ebed094094cdca892d5c71fb171e5c6865
Space-RevId: a18bedf36d461b8d93e73098ea949570b428faa9
(cherry picked from commit d971a8f321d56d0867716dafdf2b0bb1e60b9ec7)

project-system-gradle/src/com/android/tools/idea/gradle/util/AndroidStudioPreferences.java
@@ -27,8 +27,10 @@ import com.intellij.openapi.project.Project;
 import com.intellij.ui.EditorNotificationProvider;
 import java.util.Arrays;
 import java.util.List;
+import kotlin.sequences.SequencesKt;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.kotlin.idea.core.script.v1.settings.KotlinScriptingSettings;
+import org.jetbrains.kotlin.scripting.definitions.ScriptDefinitionProvider;
 import org.jetbrains.kotlin.scripting.definitions.ScriptDefinitionProvider;
 
 public final class AndroidStudioPreferences {
@@ -65,13 +67,16 @@ public final class AndroidStudioPreferences {
     // Tests do not rely on this but it causes test flakiness as it can be executed after test finish during project disposal.
     if (!ApplicationManager.getApplication().isUnitTestMode()) {
       // Disable KotlinScriptingSettings.autoReloadConfigurations flag, avoiding unexpected re-sync project with kotlin scripts
-      var iterator = ScriptDefinitionProvider.Companion.getInstance(project).getCurrentDefinitions().iterator();
-      var settings = KotlinScriptingSettings.getInstance(project);
-      while (iterator.hasNext()) {
-        var scriptDefinition = iterator.next();
-        if (settings.isScriptDefinitionEnabled(scriptDefinition) && settings.autoReloadConfigurations(scriptDefinition)) {
-          settings.setAutoReloadConfigurations(scriptDefinition, false);
-        }
+      var scriptDefinitionProvider = ScriptDefinitionProvider.Companion.getInstance(project);
+      if (scriptDefinitionProvider != null) {
+        SequencesKt.asIterable(scriptDefinitionProvider.getCurrentDefinitions()).forEach(
+          scriptDefinitions -> {
+            var settings = KotlinScriptingSettings.Companion.getInstance(project);
+            if (settings.isScriptDefinitionEnabled(scriptDefinitions) && settings.autoReloadConfigurations(scriptDefinitions)) {
+              settings.setAutoReloadConfigurations(scriptDefinitions, false);
+            }
+          }
+        );
       }
     }
 