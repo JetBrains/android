msft-mirror-aosp/platform.tools.adt.idea

[KMP] Add androidLibrary dsl model
This commit adds supports for namespace, compileSdk and minSdk.

Bug: 474315059
Test: added KmpAndroidLibraryTest
Change-Id: I84c9b4716a2343142efc877a13ef4b6c4ee6f1e3

gradle-dsl-android/testSrc/com/android/tools/idea/gradle/dsl/model/android/KmpAndroidLibraryTest.kt
@@ -1,0 +1,98 @@
+/*
+ * Copyright (C) 2026 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.gradle.dsl.model.android
+
+import com.android.tools.idea.gradle.dsl.TestFileName
+import com.android.tools.idea.gradle.dsl.api.GradleBuildModel
+import com.android.tools.idea.gradle.dsl.api.android.CompileSdkPropertyModel.Companion.COMPILE_SDK_BLOCK_VERSION
+import com.android.tools.idea.gradle.dsl.api.android.CompileSdkReleaseModel
+import com.android.tools.idea.gradle.dsl.api.android.KmpAndroidLibraryModel
+import com.android.tools.idea.gradle.dsl.api.ext.GradlePropertyModel
+import com.android.tools.idea.gradle.dsl.api.kotlin.KotlinModel
+import com.android.tools.idea.gradle.dsl.model.AndroidGradleFileModelTestCase
+import com.android.tools.idea.gradle.dsl.parser.semantics.AndroidGradlePluginVersion
+import com.google.common.truth.Truth.assertThat
+import java.io.File
+import org.jetbrains.annotations.SystemDependent
+import org.junit.Ignore
+import org.junit.Test
+
+class KmpAndroidLibraryTest : AndroidGradleFileModelTestCase() {
+
+  @Test
+  fun testParseKmpAndroidLibrary() {
+    isIrrelevantForGroovy("Kotlin Multiplatform modules are not available for Groovy")
+
+    writeToBuildFile(TestFile.KMP_ANDROID_LIBRARY)
+    val kmpAndroidLibraryModel = gradleBuildModel.kotlin().androidLibrary()
+
+    assertEquals("namespace", "abc", kmpAndroidLibraryModel.namespace())
+
+    val compileSdkVersion = kmpAndroidLibraryModel.compileSdkVersion()
+    assertThat(compileSdkVersion).isNotNull()
+
+    val config = compileSdkVersion.toCompileSdkConfig()
+    assertThat(config).isNotNull()
+
+    val version = config!!.getVersion()
+    assertThat(version).isNotNull()
+
+    val release = (version as CompileSdkReleaseModel)
+
+    assertThat(release.getVersion().getValue(
+      GradlePropertyModel.INTEGER_TYPE)).isEqualTo(36)
+    assertThat(release.getMinorApiLevel().getValue(
+      GradlePropertyModel.INTEGER_TYPE)).isEqualTo(1)
+
+    assertThat(kmpAndroidLibraryModel.minSdkVersion().getValue(
+      GradlePropertyModel.INTEGER_TYPE)).isEqualTo(30)
+  }
+
+  @Test
+  fun testRemoveAndApply() {
+    isIrrelevantForGroovy("Kotlin Multiplatform modules are not available for Groovy")
+
+    writeToBuildFile(TestFile.KMP_ANDROID_LIBRARY)
+    val buildModel = gradleBuildModel
+    buildModel.context.agpVersion = AndroidGradlePluginVersion.parse(COMPILE_SDK_BLOCK_VERSION)
+
+    buildModel.kotlin().androidLibrary().delete()
+
+    applyChangesAndReparse(buildModel)
+    verifyFileContents(myBuildFile, "")
+
+    buildModel.kotlin().androidLibrary().namespace().setValue("abc")
+
+    val config = buildModel.kotlin().androidLibrary().compileSdkVersion().toCompileSdkConfig()
+    assertThat(config).isNotNull()
+
+    config!!.setReleaseVersion(36,1, null)
+
+    buildModel.kotlin().androidLibrary().minSdkVersion().setValue(30)
+
+    applyChangesAndReparse(buildModel)
+    verifyFileContents(myBuildFile, TestFile.KMP_ANDROID_LIBRARY)
+  }
+
+  enum class TestFile(val path: @SystemDependent String) : TestFileName {
+    KMP_ANDROID_LIBRARY("block")
+    ;
+
+    override fun toFile(basePath: @SystemDependent String, extension: String): File {
+      return super.toFile("$basePath/kotlinMultiplatform/$path", extension)
+    }
+  }
+}

JetBrains/android

GitOrigin-RevId: c45e5f586b2b328027c71f581d59404e1e8b304e
Space-RevId: af5a666d11eeeffec967f8fe5eb16e63d0bfbee5
(cherry picked from commit f741eaec1fe91d30faa329df53cc93ba2f9c2b67)

gradle-dsl-android/testSrc/com/android/tools/idea/gradle/dsl/android/model/android/KmpAndroidLibraryTest.kt
@@ -1,0 +1,98 @@
+/*
+ * Copyright (C) 2026 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.tools.idea.gradle.dsl.android.model.android
+
+import com.android.tools.idea.gradle.dsl.TestFileName
+import com.android.tools.idea.gradle.dsl.api.GradleBuildModel
+import com.android.tools.idea.gradle.dsl.android.api.android.CompileSdkPropertyModel.Companion.COMPILE_SDK_BLOCK_VERSION
+import com.android.tools.idea.gradle.dsl.android.api.android.CompileSdkReleaseModel
+import com.android.tools.idea.gradle.dsl.android.api.android.KmpAndroidLibraryModel
+import com.android.tools.idea.gradle.dsl.api.ext.GradlePropertyModel
+import com.android.tools.idea.gradle.dsl.api.kotlin.KotlinModel
+import com.android.tools.idea.gradle.dsl.android.model.AndroidGradleFileModelTestCase
+import com.android.tools.idea.gradle.dsl.parser.semantics.AndroidGradlePluginVersion
+import com.google.common.truth.Truth.assertThat
+import java.io.File
+import org.jetbrains.annotations.SystemDependent
+import org.junit.Ignore
+import org.junit.Test
+
+class KmpAndroidLibraryTest : AndroidGradleFileModelTestCase() {
+
+  @Test
+  fun testParseKmpAndroidLibrary() {
+    isIrrelevantForGroovy("Kotlin Multiplatform modules are not available for Groovy")
+
+    writeToBuildFile(TestFile.KMP_ANDROID_LIBRARY)
+    val kmpAndroidLibraryModel = gradleBuildModel.kotlin().androidLibrary()
+
+    assertEquals("namespace", "abc", kmpAndroidLibraryModel.namespace())
+
+    val compileSdkVersion = kmpAndroidLibraryModel.compileSdkVersion()
+    assertThat(compileSdkVersion).isNotNull()
+
+    val config = compileSdkVersion.toCompileSdkConfig()
+    assertThat(config).isNotNull()
+
+    val version = config!!.getVersion()
+    assertThat(version).isNotNull()
+
+    val release = (version as CompileSdkReleaseModel)
+
+    assertThat(release.getVersion().getValue(
+      GradlePropertyModel.INTEGER_TYPE)).isEqualTo(36)
+    assertThat(release.getMinorApiLevel().getValue(
+      GradlePropertyModel.INTEGER_TYPE)).isEqualTo(1)
+
+    assertThat(kmpAndroidLibraryModel.minSdkVersion().getValue(
+      GradlePropertyModel.INTEGER_TYPE)).isEqualTo(30)
+  }
+
+  @Test
+  fun testRemoveAndApply() {
+    isIrrelevantForGroovy("Kotlin Multiplatform modules are not available for Groovy")
+
+    writeToBuildFile(TestFile.KMP_ANDROID_LIBRARY)
+    val buildModel = gradleBuildModel
+    buildModel.context.agpVersion = AndroidGradlePluginVersion.parse(COMPILE_SDK_BLOCK_VERSION)
+
+    buildModel.kotlin().androidLibrary().delete()
+
+    applyChangesAndReparse(buildModel)
+    verifyFileContents(myBuildFile, "")
+
+    buildModel.kotlin().androidLibrary().namespace().setValue("abc")
+
+    val config = buildModel.kotlin().androidLibrary().compileSdkVersion().toCompileSdkConfig()
+    assertThat(config).isNotNull()
+
+    config!!.setReleaseVersion(36,1, null)
+
+    buildModel.kotlin().androidLibrary().minSdkVersion().setValue(30)
+
+    applyChangesAndReparse(buildModel)
+    verifyFileContents(myBuildFile, TestFile.KMP_ANDROID_LIBRARY)
+  }
+
+  enum class TestFile(val path: @SystemDependent String) : TestFileName {
+    KMP_ANDROID_LIBRARY("block")
+    ;
+
+    override fun toFile(basePath: @SystemDependent String, extension: String): File {
+      return super.toFile("$basePath/kotlinMultiplatform/$path", extension)
+    }
+  }
+}