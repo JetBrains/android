5a9a7de2d80cab6e4c90d245570887ccdbd71afc
Slava Trenogin <slava.trenogin@jetbrains.com>
2026-08-04 19:26:23 +0200

    MRI-4576 followup for NET-MR-18905
    GitOrigin-RevId: de56f46a46196b00a274a142f741ba687c750f18

--- adt-ui-compose/build.gradle.kts
+++ adt-ui-compose/build.gradle.kts
@@ -26,7 +26,7 @@ plugins {
 
 repositories {
   maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
-  maven("https://packages.jetbrains.team/maven/p/kpm/public/")
+  maven("https://cache-redirector.jetbrains.com/packages.jetbrains.team/maven/p/kpm/public/")
   google()
   mavenCentral()
   intellijPlatform {
