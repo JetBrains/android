ff3e0966828f21992bf51d62e8b9ca49805eea3a
Kirill Bochkarev <kirill.bochkarev@jetbrains.com>
2026-08-14 16:55:32 +0000

    IJPL-250478 IJPL-214121 Workspace Model: move reference setters and getters to WorkspaceEntityBase
    - Update child symbolicId when parent changes. Previously changes in parent would not affect child and consistency would break.
    - regen IntelliJ workspace code v25
    
    
    Merge-request: IJ-MR-218271
    Merged-by: Kirill Bochkarev <kirill.bochkarev@jetbrains.com>
    
    GitOrigin-RevId: 62956268c1a8342971d64ebd94dbd35e6dba5490

--- project-system-gradle/gen/com/android/tools/idea/gradle/project/entities/impl/GradleAndroidModelEntityImpl.kt
+++ project-system-gradle/gen/com/android/tools/idea/gradle/project/entities/impl/GradleAndroidModelEntityImpl.kt
@@ -35,7 +35,6 @@ import com.intellij.platform.workspace.storage.impl.ModifiableWorkspaceEntityBas
 import com.intellij.platform.workspace.storage.impl.WorkspaceEntityBase
 import com.intellij.platform.workspace.storage.impl.WorkspaceEntityData
 import com.intellij.platform.workspace.storage.instrumentation.EntityStorageInstrumentationApi
-import com.intellij.platform.workspace.storage.instrumentation.MutableEntityStorageInstrumentation
 import com.intellij.platform.workspace.storage.instrumentation.instrumentation
 import com.intellij.platform.workspace.storage.metadata.model.EntityMetadata
 
@@ -118,35 +117,9 @@ internal class GradleAndroidModelEntityImpl(private val dataSource: GradleAndroi
         changedProperty.add("entitySource")
       }
     override var module: ModuleEntityBuilder
-      get() {
-        val _diff = diff
-        return if (_diff != null) {
-          ((_diff as MutableEntityStorageInstrumentation).getParentBuilder(MODULE_CONNECTION_ID, this) as? ModuleEntityBuilder)
-          ?: (this.entityLinks[EntityLink(false, MODULE_CONNECTION_ID)] as? ModuleEntityBuilder) ?: error(
-            "module is null for GradleAndroidModelEntity")
-        }
-        else {
-          (this.entityLinks[EntityLink(false, MODULE_CONNECTION_ID)] as? ModuleEntityBuilder) ?: error(
-            "module is null for GradleAndroidModelEntity")
-        }
-      }
+      get() = getParent(MODULE_CONNECTION_ID) as? ModuleEntityBuilder ?: error("module is null for GradleAndroidModelEntity")
       set(value) {
-        checkModificationAllowed()
-        val _diff = diff
-        if (_diff != null && value is ModifiableWorkspaceEntityBase<*, *> && value.diff == null) {
-          value.entityLinks[EntityLink(true, MODULE_CONNECTION_ID)] = this
-          @Suppress("UNCHECKED_CAST")
-          _diff.addEntity(value as ModifiableWorkspaceEntityBase<WorkspaceEntity, *>)
-        }
-        if (_diff != null && (value !is ModifiableWorkspaceEntityBase<*, *> || value.diff != null)) {
-          _diff.instrumentation.addChild(MODULE_CONNECTION_ID, value, this)
-        }
-        else {
-          if (value is ModifiableWorkspaceEntityBase<*, *>) {
-            value.entityLinks[EntityLink(true, MODULE_CONNECTION_ID)] = this
-          }
-          this.entityLinks[EntityLink(false, MODULE_CONNECTION_ID)] = value
-        }
+        changeParent(value, MODULE_CONNECTION_ID)
         changedProperty.add("module")
       }
     override var gradleAndroidModel: GradleAndroidModelImpl
