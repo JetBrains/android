msft-mirror-aosp/platform.tools.adt.idea

Improve way of handling disposables
Wrap disposer registration with tryRegister to prevent trying to use an
already disposed Disposable as parent. Also add some early returns when
the parent is already disposed before registering a child. Finally, this
change adds a direct call to Disposer.dispose() when the SceneManager is
not registered in the Disposable tree because its parent was already
disposed by the time of its creation. SceneManager (and subclasses) have
some clean up logic in the dispose() method, so it needs to be called
if the SceneManager is not registered in the disposable tree.

This fixes flakiness in ComposePreviewRepresentationTest and re-enables
the test class.
testInteractivePreviewManagerFpsLimitIsUpdatedWhenEssentialsModeChanges
is still causing issues, so this individual test method is kept ignored.

Bug: 488139872
Test: ComposePreviewRepresentationTest (--runs_per_test=500)
Change-Id: Id3db211f5373a44a65214dc8b419ffae9e69ed34

preview-designer/src/com/android/tools/idea/preview/SurfacePreviewsUpdate.kt
@@ -201,6 +201,9 @@ suspend fun <T : PsiPreviewElement> NlDesignSurface.updatePreviewsAndRefresh(
         previewElement to newModel
       }
       .let { elementModelList ->
+        if (this@NlDesignSurface.isDisposed()) {
+          return@let emptyList()
+        }
         // Reorder existing models and add placeholders altogether to improve performance and UX in
         // comparison with adding/reordering them one by one.
         this@NlDesignSurface.addModelsWithoutRender(elementModelList.map { it.second }).mapIndexed { idx, sceneManager ->

JetBrains/android

GitOrigin-RevId: a11f0c3a1d172d2cf175dbd42bd93106482bfc45
(cherry picked from commit 843667d35cd60224e327009ec4941260efc700d5)

preview-designer/src/com/android/tools/idea/preview/SurfacePreviewsUpdate.kt
@@ -201,6 +201,9 @@ suspend fun <T : PsiPreviewElement> NlDesignSurface.updatePreviewsAndRefresh(
         previewElement to newModel
       }
       .let { elementModelList ->
+        if (this@updatePreviewsAndRefresh.isDisposed()) {
+          return@let emptyList()
+        }
         // Reorder existing models and add placeholders altogether to improve performance and UX in
         // comparison with adding/reordering them one by one.
         this.addModelsWithoutRender(elementModelList.map { it.second }).mapIndexed { idx, sceneManager ->