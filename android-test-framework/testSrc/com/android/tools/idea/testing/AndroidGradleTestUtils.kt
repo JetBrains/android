a298ed3aacaddc5573a8cec9894a19953be13c12
Vlad Koshkin <vladislav.koshkin@jetbrains.com>
2026-09-11 11:24:49 +0200

    [kotlin] KTIJ-40197 consolidated scripting packages, inline interfaces, cleanup
    GitOrigin-RevId: d8e072940f40a05a81c1ebe0ecacf3a8a336075b

--- android-test-framework/testSrc/com/android/tools/idea/testing/AndroidGradleTestUtils.kt
+++ android-test-framework/testSrc/com/android/tools/idea/testing/AndroidGradleTestUtils.kt
@@ -239,7 +239,7 @@ import org.jetbrains.android.facet.AndroidFacet
 import org.jetbrains.annotations.SystemDependent
 import org.jetbrains.annotations.SystemIndependent
 import org.jetbrains.kotlin.idea.base.externalSystem.findAll
-import org.jetbrains.kotlin.idea.core.script.k2.KotlinScriptWorkspaceFileIndexContributor
+import org.jetbrains.kotlin.idea.core.script.KotlinScriptWorkspaceFileIndexContributor
 import org.jetbrains.plugins.gradle.model.DefaultGradleExtension
 import org.jetbrains.plugins.gradle.model.DefaultGradleExtensions
 import org.jetbrains.plugins.gradle.model.ExternalProject
