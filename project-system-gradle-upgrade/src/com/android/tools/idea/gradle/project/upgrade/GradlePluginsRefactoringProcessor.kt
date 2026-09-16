msft-mirror-aosp/platform.tools.adt.idea

Normalize kotlin-stdlib version in IDE model snapshots
so that these snapshots are not impacted by future Kotlin version
updates.

Bug: 483083119
Test: Updated
Change-Id: Iea656277ab8e3a10e65ac384e7fe9aebe72a3a1a

project-system-gradle-upgrade/src/com/android/tools/idea/gradle/project/upgrade/GradlePluginsRefactoringProcessor.kt
@@ -22,6 +22,7 @@ import com.android.tools.idea.gradle.dsl.api.dependencies.ArtifactDependencyMode
 import com.android.tools.idea.gradle.dsl.api.dependencies.CommonConfigurationNames
 import com.android.tools.idea.gradle.dsl.api.ext.GradlePropertyModel
 import com.android.tools.idea.gradle.dsl.parser.dependencies.FakeArtifactElement
+import com.android.tools.idea.gradle.util.AGP_BUILT_IN_KOTLIN_VERSION
 import com.android.tools.idea.gradle.util.CompatibleGradleVersion
 import com.android.tools.idea.gradle.util.CompatibleGradleVersion.Companion.getCompatibleGradleVersion
 import com.android.tools.idea.gradle.util.CompatibleGradleVersion.VERSION_4_10_1
@@ -174,8 +175,8 @@ class GradlePluginsRefactoringProcessor : AgpUpgradeComponentRefactoringProcesso
         VERSION_8_10_2 -> Version.parse("1.6.21")
         VERSION_8_11_1 -> Version.parse("1.6.21")
         VERSION_8_13 -> Version.parse("1.6.21")
-        VERSION_9_1_0,
-        VERSION_FOR_DEV -> Version.parse("2.2.10")
+        VERSION_9_1_0 -> Version.parse("2.2.10")
+        VERSION_FOR_DEV -> Version.parse(AGP_BUILT_IN_KOTLIN_VERSION)
       }
 
     fun `androidx-navigation-safeargs-gradle-plugin-compatibility-info`(compatibleGradleVersion: CompatibleGradleVersion): Version =

JetBrains/android

GitOrigin-RevId: ed8edbb7432770a0e3aa20c5a0392b43968e4d8c
(cherry picked from commit 00af370356b724a01d9e70563a11737823497820)

project-system-gradle-upgrade/src/com/android/tools/idea/gradle/project/upgrade/GradlePluginsRefactoringProcessor.kt
@@ -22,6 +22,7 @@ import com.android.tools.idea.gradle.dsl.api.dependencies.ArtifactDependencyMode
 import com.android.tools.idea.gradle.dsl.api.dependencies.CommonConfigurationNames
 import com.android.tools.idea.gradle.dsl.api.ext.GradlePropertyModel
 import com.android.tools.idea.gradle.dsl.parser.dependencies.FakeArtifactElement
+import com.android.tools.idea.gradle.util.AGP_BUILT_IN_KOTLIN_VERSION
 import com.android.tools.idea.gradle.util.CompatibleGradleVersion
 import com.android.tools.idea.gradle.util.CompatibleGradleVersion.Companion.getCompatibleGradleVersion
 import com.android.tools.idea.gradle.util.CompatibleGradleVersion.VERSION_4_10_1
@@ -176,8 +177,8 @@ class GradlePluginsRefactoringProcessor : AgpUpgradeComponentRefactoringProcesso
         VERSION_8_11_1 -> Version.parse("1.6.21")
         VERSION_8_13 -> Version.parse("1.6.21")
         VERSION_9_1_0,
-        VERSION_9_3_1,
-        VERSION_FOR_DEV -> Version.parse("2.2.10")
+        VERSION_9_3_1 -> Version.parse("2.2.10")
+        VERSION_FOR_DEV -> Version.parse(AGP_BUILT_IN_KOTLIN_VERSION)
       }
 
     fun `androidx-navigation-safeargs-gradle-plugin-compatibility-info`(compatibleGradleVersion: CompatibleGradleVersion): Version =